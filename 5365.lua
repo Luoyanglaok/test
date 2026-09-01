050492fdde89ff92489b9032a0bfc0763d0ac166
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:35:32 +0800

    add 5365.lua

---
+++ 5365.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5365.lua
+--Gamename Assassins Creed 2 Developer Diary
+addappid(5365) --Mainappid Assassins Creed 2 Developer Diary
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Assassins Creed 2 Developer Diary
+setManifestid(910, "1332084816209327708", 0)
