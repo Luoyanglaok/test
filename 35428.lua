c35e30fe6440145959bfae06d0c5d35169cd35ed
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:38:38 +0800

    add 35428.lua

---
+++ 35428.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 35428.lua
+--Gamename Killing Floor Hungarian Depot
+addappid(35428, 1, "55245614dca5ba68071421dcea7c967d952a2b9b5e753b6d89d497ffff1d4c5c") --Mainappid Killing Floor Hungarian Depot
