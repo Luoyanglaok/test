e4d0cd30cc3f90bce4b5837c3e31665e4aab8f9b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:19:58 +0800

    add 7780.lua

---
+++ 7780.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7780.lua
+--Gamename MLB® Front Office Manager
+addappid(7780) --Mainappid MLB® Front Office Manager
+addappid(7781, 1, "ff765c3a0c4d3c3e81cc479213b3778c4f767ae5ca8001076d8b312b5a1748d0") --Main Windows Depot MLB® Front Office Manager
+setManifestid(7781, "2783731858875605896", 0)
