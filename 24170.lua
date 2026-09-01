759c89bd1a76d74e3c51e96c08dfc00a63d71809
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:10:43 +0800

    add 24170.lua

---
+++ 24170.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24170.lua
+--Gamename Star Wars Galaxies™: The Complete Online Adventures
+addappid(24170) --Mainappid Star Wars Galaxies™: The Complete Online Adventures
+addappid(24171, 1, "bf6c072c07bbcba02d59c33b91bc4d79fcf6a4ab36022c034b5dd2b703bdd62f") --Main Windows Depot Star Wars Galaxies™: The Complete Online Adventures
