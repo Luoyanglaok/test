1174b5638a2023e2753b91263fd5cb301cf8434b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:43:24 +0800

    add 37310.lua

---
+++ 37310.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37310.lua
+--Gamename DinerTown Detective Agency™
+addappid(37310) --Mainappid DinerTown Detective Agency™
+addappid(37311, 1, "65821d37478d32111fa71d3a78b6f492db520792da61429b34e274b062f7b096") --Main Windows Depot DinerTown Detective Agency™
+setManifestid(37311, "6610020189793925970", 0)
