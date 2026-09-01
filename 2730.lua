2895211e5c298305d96b75fd097d6167d6269689
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:23:32 +0800

    add 2730.lua

---
+++ 2730.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2730.lua
+--Gamename ThreadSpace: Hyperbol Demo
+addappid(2730) --Mainappid ThreadSpace: Hyperbol Demo
+addappid(2722, 1, "7fbd9497a98176bca021543adba8dbbe6a7a796e6015626a1991f2d9a4fbec8a") --Main Windows Depot ThreadSpace: Hyperbol Demo
+addappid(2723, 1, "1611d3893c5eeef840ef0a464bbd9c2120d2d05a09117882a6d5ac058e3f2e57") --Main Windows Depot ThreadSpace: Hyperbol Demo
+addappid(2731, 1, "7ee954366da91f176bc4fa221107fda308503cd97629fbd04efc1e658c37bf8d") --Main Windows Depot ThreadSpace: Hyperbol Demo
+setManifestid(2731, "846842587961117731", 0)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Main Windows Depot ThreadSpace: Hyperbol Demo
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot ThreadSpace: Hyperbol Demo
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Main Windows Depot ThreadSpace: Hyperbol Demo
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Main Windows Depot ThreadSpace: Hyperbol Demo
