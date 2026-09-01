62988811dc508ca772c097fda7bb568d1a1d78d6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:36:39 +0800

    add 13140.lua

---
+++ 13140.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 13140.lua
+--Gamename America's Army 3
+addappid(13140) --Mainappid America's Army 3
+addappid(13141, 1, "d279575d0c3ddfa831f5b1ae5fe4bb8a2914bdc69a1c2c1982e230cf9d9c9f79") --Main Windows Depot America's Army 3
+setManifestid(13141, "5610684473385955954", 4170339136)
