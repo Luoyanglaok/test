395663df3caaea375b9de607757658fb295434ec
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:23:16 +0800

    add 33933.lua

---
+++ 33933.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33933.lua
+--Gamename Arma 2: Operation Arrowhead
+addappid(33933) --Mainappid Arma 2: Operation Arrowhead
+addappid(33934, 1, "e76a2c7fff33e0185e60063b7dadb06d7ecbac7eb586b3cf54bdbbd387afed56") --Main Windows Depot Arma 2: Operation Arrowhead
+setManifestid(33934, "1218581768977878334", 0)
+--Dlcname Arma 2: Operation Arrowhead
+addappid(33930) --Dlcname Arma 2: Operation Arrowhead
+addappid(33931, 1, "3b324ca60026d263ac2c391af625442d40c7675e5f1894a9e340c0c435d1499e") --Dlc Windows Depot Arma 2: Operation Arrowhead
+setManifestid(33931, "7739883071566525791", 7269165760)
+addappid(33932, 1, "07e07b7a6d5f04dbf42162d9ebb710c7ab43b98ec52af69e10b6becb72eda6fa") --Dlc Windows Depot Arma 2: Operation Arrowhead
+addappid(33937, 1, "309019a4078b23b7d63eb7869152b006789ba736c1badd1eeb6be1773d86c04c") --Dlc Windows Depot Arma 2: Operation Arrowhead
+setManifestid(33937, "6080133772376467329", 0)
