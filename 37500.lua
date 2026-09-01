f03f5c693171013fa9cd8ec40f843dd0fa94e702
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:44:28 +0800

    add 37500.lua

---
+++ 37500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37500.lua
+--Gamename Magnetis
+addappid(37500) --Mainappid Magnetis
+addappid(37501, 1, "3131063ce411c00e6fba6173effc04342fa3f61f43e0596668805e9e82bc2f1e") --Main Windows Depot Magnetis
+setManifestid(37501, "1024921423414283721", 0)
