932cd2e413596c4b775eec3a1e4aa3aecd24f511
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:16:12 +0800

    add 6370.lua

---
+++ 6370.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6370.lua
+--Gamename Bloodline Champions
+addappid(6370) --Mainappid Bloodline Champions
+addappid(6371, 1, "685e0acf5535c8b0bba97862fb1eb9771e58d7a7cfa678be3f0db419d60503f0") --Main Windows Depot Bloodline Champions
+setManifestid(6371, "8315850012655897675", 447009920)
+addappid(223031) --Dlcname Bloodline Champions - Beginners Pack
+addappid(223032) --Dlcname Bloodline Champions - Supreme Pack
+addappid(338200) --Dlcname Bloodline Champions - Huntress Pack
+addappid(338201) --Dlcname Bloodline Champions - Warrior Pack
+addappid(338202) --Dlcname Bloodline Champions - Warchief Pack
