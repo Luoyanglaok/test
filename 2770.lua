2068b156d82aa58398b0f5bb71726a783e4b407c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:24:15 +0800

    add 2770.lua

---
+++ 2770.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2770.lua
+--Gamename Neverwinter Nights 2: Platinum - Map Editor
+addappid(2770) --Mainappid Neverwinter Nights 2: Platinum - Map Editor
+addappid(2762, 1, "b5abcdbc85148e570df3495c0b57232eb47914b3e2b675ec1c8942fb57af3376") --Main Windows Depot Neverwinter Nights 2: Platinum - Map Editor
+setManifestid(2762, "3967009828914626100", 0)
