2597cd529a588e11bfca24fdbf97cadb3e0c8bd8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:27:16 +0800

    add 3322.lua

---
+++ 3322.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3322.lua
+--Gamename Insaniquarium! Deluxe Demo
+addappid(3322) --Mainappid Insaniquarium! Deluxe Demo
+addappid(3321, 1, "26234aa7a8ed8885d4b33589933cc80eb2791003f63b325e8b7aa7b5b81af115") --Main Windows Depot Insaniquarium! Deluxe Demo
+addappid(3323, 1, "cedbdc0297b63f8916b4040931443823f486b5bd224ae00583ea38c871049f2d") --Main Windows Language Depot French Insaniquarium! Deluxe Demo
+addappid(3324, 1, "0ab82fcd718a9462618aaf3bc33c0c7376d101bc69fef4d8478cd14fdf2319fd") --Main Windows Language Depot German Insaniquarium! Deluxe Demo
+addappid(3325, 1, "4133100689e8ddfbaed547829631e786ce9ad11434b1800d613a05116368bc35") --Main Windows Language Depot Italian Insaniquarium! Deluxe Demo
+addappid(3326, 1, "493d55f9dfa8ea06975a94646cb892a4a8649706db69c933b5bb64a89654b046") --Main Windows Language Depot Spanish Insaniquarium! Deluxe Demo
