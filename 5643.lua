91d4db5e0dc06652f65c667b4c8836a6b37a5257
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:45:21 +0800

    add 5643.lua

---
+++ 5643.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5643.lua
+--Gamename Sam & Max The Devils Playhouse
+addappid(5643) --Mainappid Sam & Max The Devils Playhouse
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Sam & Max The Devils Playhouse
+setManifestid(910, "1332084816209327708", 0)
