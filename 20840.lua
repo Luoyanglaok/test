67ab0e972d0bba5e48498c78d69d5430ae192a23
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:53:30 +0800

    add 20840.lua

---
+++ 20840.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 20840.lua
+--Gamename Rugby Challenge
+addappid(20840) --Mainappid Rugby Challenge
+addappid(20841, 1, "06aca3285c6c38904d90fe677f9fb5d2671f4e1b77bd8f489cde8330fd01553e") --Main Windows Depot Rugby Challenge
+setManifestid(20841, "3167825830124569836", 0)
