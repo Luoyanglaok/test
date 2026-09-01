2213b26f4c54d07de07e327db0e3db5e64c62bbe
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:30:20 +0800

    add 5303.lua

---
+++ 5303.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5303.lua
+--Gamename Tales of Monkey Island Trailer
+addappid(5303) --Mainappid Tales of Monkey Island Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Tales of Monkey Island Trailer
+setManifestid(910, "1332084816209327708", 0)
