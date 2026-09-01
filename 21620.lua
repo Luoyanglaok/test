14f2c754580807771523c7c9fe19a3f1bd6074f8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:55:24 +0800

    add 21620.lua

---
+++ 21620.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21620.lua
+--Gamename Neopets: Puzzle Adventure
+addappid(21620) --Mainappid Neopets: Puzzle Adventure
+addappid(21621, 1, "b477c3f47133d56594cf36ec0ed97f16b8c1c17a50ed07252166ec6584f5718b") --Main Windows Depot Neopets: Puzzle Adventure
+setManifestid(21621, "5027990431446612597", 0)
