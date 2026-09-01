8d623121e4c5857377cf18ab86121b8456ab3167
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:25:26 +0800

    add 9360.lua

---
+++ 9360.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9360.lua
+--Gamename ValveTestApp9360
+addappid(9360) --Mainappid ValveTestApp9360
+addappid(9361, 1, "3cf514f342b9ec876c566730d17b318c5215cd95c8f8805ca714ad1b1d798ff1") --Main Windows Depot ValveTestApp9360
