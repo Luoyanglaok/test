1ece01482dd673905bd8023b30c83e87c94531cb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:52:48 +0800

    add 19940.lua

---
+++ 19940.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19940.lua
+--Gamename The Settlers 6 - Gold Editor
+addappid(19940) --Mainappid The Settlers 6 - Gold Editor
+addappid(19931, 1, "b4d49eef28263e76c486fcd8cf5981d7c9017a83209ac0a7827f85b04a29aeb4") --Main Windows Depot The Settlers 6 - Gold Editor
+setManifestid(19931, "8981027791565835241", 0)
