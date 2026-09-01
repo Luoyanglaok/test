6448d88372afe09a99bb230063bf93d72ca15d42
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:42:41 +0800

    add 16510.lua

---
+++ 16510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16510.lua
+--Gamename Chaos Theory Demo
+addappid(16510) --Mainappid Chaos Theory Demo
+addappid(16511, 1, "8a389a6e0bc9a633ecfa10e82949dbc262de0708cac710c94549f537a9ef2ca0") --Main Windows Depot Chaos Theory Demo
+setManifestid(16511, "7311163870521240657", 0)
