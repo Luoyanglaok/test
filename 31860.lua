8847b7d0bcfc95b096f793916f9ea68c3cf16444
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:17:40 +0800

    add 31860.lua

---
+++ 31860.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 31860.lua
+--Gamename Nancy Drew®: Legend of the Crystal Skull
+addappid(31860) --Mainappid Nancy Drew®: Legend of the Crystal Skull
+addappid(31861, 1, "aa341a65ef80421b47c1a4fb0c448276638f77d859aa5f65d508df3b071a332c") --Main Windows Depot Nancy Drew®: Legend of the Crystal Skull
+setManifestid(31861, "2401367520319054641", 1020867648)
