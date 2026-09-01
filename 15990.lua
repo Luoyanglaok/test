b555f90703827c0805482c21c8a8887e6c6a5444
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:40:55 +0800

    add 15990.lua

---
+++ 15990.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15990.lua
+--Gamename Elf Bowling: Hawaiian Vacation
+addappid(15990) --Mainappid Elf Bowling: Hawaiian Vacation
+addappid(15991, 1, "fe7b660816121e8580394c3be6ed1bcae5484728a8deb9c852d8cfab032152a7") --Main Windows Depot Elf Bowling: Hawaiian Vacation
+setManifestid(15991, "905718427627219468", 0)
