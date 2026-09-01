b3ec4335ea65807064678903427abde92f5a9e9f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:54:31 +0800

    add 219.lua

---
+++ 219.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 219.lua
+--Gamename Half-Life 2: Demo
+addappid(219) --Mainappid Half-Life 2: Demo
+addappid(220, 1, "c9adc9c18fccfe63c2ea938ddce901fff46d8bbdc78f4799623a656698df312c") --Main Windows Depot Half-Life 2: Demo
+addappid(221, 1, "f5e105e91fb4354535c3cf3c8951788ab552be70223608c6694363a226133b85") --Main Windows Depot Half-Life 2: Demo
+addappid(222, 1, "3ab884cba0fadaca0c1fd679674ded49751376c9d31601cbf5024df1f82d4506") --Main Windows Depot Half-Life 2: Demo
+addappid(223, 1, "9bdd77e77514c4ab1868ccf58a8d8664e65f69ecbe2cfb770769034477b8c86e") --Main Macos Depot Half-Life 2: Demo
+addappid(224, 1, "918d917917c2b39588108c8009673f885025a339340afc19a5ef1941d5925595") --Main Linux Depot Half-Life 2: Demo
+addappid(225, 1, "60e635e4daf91e2fd671ffc26e4941dab0988ae3f3a2328dfc04ffe4117e26b5") --Main Windows Language Depot Russian Half-Life 2: Demo
+addappid(226, 1, "1bd20044a82e9539ac742ac3691df05152904d82342d9255f45b52c0c57e54c6") --Main Windows Language Depot Spanish Half-Life 2: Demo
+addappid(227, 1, "f6aa27353df328897edbd51b5a825d70f7c358807397a93eb5abfcb94b183572") --Main Windows Language Depot French Half-Life 2: Demo
+addappid(228, 1, "d75d5d119b336a64745a13676bd7006d972c3de556e771e0009661ff7d4dd955") --Main Windows Language Depot German Half-Life 2: Demo
+addappid(229, 1, "18ca38da67160ae26a413b19f3f9b43dacb40373d5327f676a4278aa32e26203") --Main Windows Language Depot Koreana Half-Life 2: Demo
+addappid(230, 1, "ea712d5794d15e57eb2ba5ba0b5ecf3341086b43701e6e6973ff92ddf03de683") --Main Windows Language Depot Tchinese Half-Life 2: Demo
+addappid(231, 1, "c25bd10ea1f4d8727710046873443ff79da609575d5d5e98450334c9fb3a98a6") --Main Windows Language Depot Schinese Half-Life 2: Demo
+addappid(232, 1, "894ec100daaf7781542c2fa06826fe7b1f22fa4ab74b8a61bf23da586ee42367") --Main Windows Language Depot Italian Half-Life 2: Demo
+addappid(233, 1, "4c903543278080fadf911204001bb474a80561a9c74b66138e327b84ad28e769") --Main Windows Depot Half-Life 2: Demo
+addappid(234, 1, "5daa4d2379f218e856f6d8c7ef665d6d70887883f4ca9cd3afbd10d1fe2ec2d0") --Main Windows Depot Half-Life 2: Demo
