8a2571680891c03ad481363d6d036dc464943b48
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:50:38 +0800

    add 18610.lua

---
+++ 18610.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 18610.lua
+--Gamename Mayhem Intergalactic Demo
+addappid(18610) --Mainappid Mayhem Intergalactic Demo
+addappid(18611, 1, "12d118b0a697c61bbebd87e7c55dd2c3977d730c23dad7729aeaf477ae1cd36b") --Main Windows Depot Mayhem Intergalactic Demo
+setManifestid(18611, "8671268900995491889", 0)
