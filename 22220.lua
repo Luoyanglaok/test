df7eb5820befbccb2d6b1100942a2101c28e2b63
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:57:37 +0800

    add 22220.lua

---
+++ 22220.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 22220.lua
+--Gamename Zeno Clash Demo
+addappid(22220) --Mainappid Zeno Clash Demo
+addappid(22221, 1, "6787b659fcf574ab33d478c1f482bd8262f413c32c789b3b69e7a13c02c669cb") --Main Windows Depot Zeno Clash Demo
+setManifestid(22221, "7361414104554023991", 0)
+addappid(22222, 1, "0851d3c0d45e3b38fbd62490a3047cc293de39a3aedd69a3d4247f6d137e5e91") --Main Windows Depot Zeno Clash Demo
+setManifestid(22222, "5475075691271548880", 0)
+addappid(22223, 1, "a3343aabdff5543dfcf95b0f101f341559470e333c4caf77fee82fce5b8c2759") --Main Windows Depot Zeno Clash Demo
+setManifestid(22223, "451077167010873081", 0)
+addappid(22224, 1, "3edd243b43aeedc24d3776f38a8308c47420711832ff8d6230aa3bc0569a5333") --Main Windows Depot Zeno Clash Demo
+setManifestid(22224, "3024717336069488213", 0)
