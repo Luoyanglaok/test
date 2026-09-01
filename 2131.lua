4be8086454c67cd62affb942cbc254552d339c4f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:21:41 +0800

    add 2131.lua

---
+++ 2131.lua
@@ -0,0 +1,29 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2131.lua
+--Gamename Dark Messiah of Might & Magic
+addappid(2131, 1, "e32cc6357aa4d89d8a51d4d9c198da50a1b2680d7490ff531fda563d6779d092") --Mainappid Dark Messiah of Might & Magic
+setManifestid(2131, "3768977838608906338", 0)
+addappid(2132, 1, "feaa43e6fb2ffb462eb7992381cbaa8436cfbb22eefc321ed55d8f1eb83f2c8e") --Main Windows Depot Dark Messiah of Might & Magic
+setManifestid(2132, "6598208797721037185", 0)
+--Dlcname Dark Messiah of Might & Magic Multi-Player
+addappid(2130) --Dlcname Dark Messiah of Might & Magic Multi-Player
+addappid(1004, 1, "f21faa4f29ecee9d192fc1784d1701376eb87e13521824ceaf0116b10c558a81") --Dlc Windows Depot Dark Messiah of Might & Magic Multi-Player
+setManifestid(1004, "3578600867548439549", 0)
+addappid(2133, 1, "90709d9ae1e3b6d32da9bbcfd4c7e893ce32a7dcb57a3419d9428817924685a7") --Dlc Windows Depot Dark Messiah of Might & Magic Multi-Player
+setManifestid(2133, "4035822256194243656", 0)
+addappid(2134, 1, "1f2c065f543cd43489619670329e815def0d791f444bd758a4ddef58fdd082d0") --Dlc Windows Depot Dark Messiah of Might & Magic Multi-Player
+setManifestid(2134, "6970001795950006478", 0)
+addappid(2135, 1, "1c8ad478d3eec6ba7564e202b0d7a7ba84168656c43523b9126efd4b85830c9d") --Dlc Windows Depot Dark Messiah of Might & Magic Multi-Player
+setManifestid(2135, "6776152709533059089", 0)
+addappid(2136, 1, "46dbf59a2a6c1523332368c6b939fbc9dda57889ee10a9714b6ffda5b3bed24a") --Dlc Windows Depot Dark Messiah of Might & Magic Multi-Player
+setManifestid(2136, "8840957310590959220", 0)
+addappid(2137, 1, "a300430b332e5e85b54d6eed89ead3bcdc99e87ba0d858a012d3a529843536fa") --Dlc Windows Language Depot French Dark Messiah of Might & Magic Multi-Player
+setManifestid(2137, "6076359142485839011", 0)
+addappid(2138, 1, "a87532a7029d50b9871f60cdecdb093927f0f928508a0fd7514c3976d08fd743") --Dlc Windows Language Depot German Dark Messiah of Might & Magic Multi-Player
+setManifestid(2138, "7703710409727016128", 0)
+addappid(2139, 1, "eb17ff1d6135e0e1f538fb1dc08bdbe56f2665bf31e161ab74c63866f24a6d6e") --Dlc Windows Language Depot Spanish Dark Messiah of Might & Magic Multi-Player
+setManifestid(2139, "190191596735838729", 0)
+addappid(2140, 1, "1850a9ac494ba89b46992eeb5717e248f134752009d2940e92c529d72ace4cf3") --Dlc Windows Language Depot Italian Dark Messiah of Might & Magic Multi-Player
+setManifestid(2140, "3560601814023723699", 0)
+addappid(2141, 1, "3cb74da92d10f9a14db9a87712b831f4fb5a192ceb54852f38de572ab4179621") --Dlc Windows Language Depot Russian Dark Messiah of Might & Magic Multi-Player
+setManifestid(2141, "8403837732232495019", 0)
