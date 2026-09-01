e14911327530fcf752ec3d4b0d9b15ff950d851e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:34:05 +0800

    add 12420.lua

---
+++ 12420.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12420.lua
+--Gamename Perimeter 2: New Earth
+addappid(12420) --Mainappid Perimeter 2: New Earth
+addappid(12421, 1, "7ffdfa6b126384eab680a54225071dd11ab8e74fc2ee21405f9fd1565ba884f8") --Main Windows Depot Perimeter 2: New Earth
+setManifestid(12421, "7162772054419936105", 0)
