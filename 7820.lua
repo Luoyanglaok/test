b54fad2eb346f8b6bf1c5750f0a5f73bbf6a4864
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:20:19 +0800

    add 7820.lua

---
+++ 7820.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7820.lua
+--Gamename Stubbs The Zombie Demo
+addappid(7820) --Mainappid Stubbs The Zombie Demo
+addappid(7821, 1, "6ab956014bc3609c7cc834a19c16eaf4a06b3fd94a00f419c271a621515cbd4d") --Main Windows Depot Stubbs The Zombie Demo
+setManifestid(7821, "2046557095989118596", 0)
