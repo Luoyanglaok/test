ce4df83344b7e3c4ae85da9cf91832605d2b08ad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:47:42 +0800

    add 972.lua

---
+++ 972.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 972.lua
+--Gamename Sam & Max 105: Reality 2.0 Trailer
+addappid(972) --Mainappid Sam & Max 105: Reality 2.0 Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Sam & Max 105: Reality 2.0 Trailer
+setManifestid(910, "1332084816209327708", 0)
