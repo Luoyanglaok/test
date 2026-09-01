26d67a11d61f2f7e36b167792d062e6fe775c8c8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:46:27 +0800

    add 959.lua

---
+++ 959.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 959.lua
+--Gamename Infernal Trailer 1
+addappid(959) --Mainappid Infernal Trailer 1
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Infernal Trailer 1
+setManifestid(910, "1332084816209327708", 0)
+addappid(960, 1, "ab8a2881a92559b7b05560a9a9ad01ea60158371e8b2dcad089042dab02a9618") --Main Windows Depot Infernal Trailer 1
