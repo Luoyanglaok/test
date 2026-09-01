d1fe3a6e9900bde670d4f4c702ef2957a2f52410
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:26:16 +0800

    add 3190.lua

---
+++ 3190.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3190.lua
+--Gamename King's Bounty Armored Princess Demo
+addappid(3190) --Mainappid King's Bounty Armored Princess Demo
+addappid(3191, 1, "7c1df147be514d34f314517b8c0ce832d1fb118e02ece3df91a2f8e98b77e789") --Main Windows Depot King's Bounty Armored Princess Demo
+setManifestid(3191, "4791659239147113601", 0)
