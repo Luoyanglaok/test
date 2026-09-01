6d755739f1f8cbf36924293cf6487f5616bccdb4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:22:39 +0800

    add 33710.lua

---
+++ 33710.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33710.lua
+--Gamename Paws and Claws: Pet School
+addappid(33710) --Mainappid Paws and Claws: Pet School
+addappid(33711, 1, "e6300c863cde5d378b36726d595263b648949452d7225d8ed6351392316a7d2d") --Main Windows Depot Paws and Claws: Pet School
+setManifestid(33711, "7614307489570186356", 0)
