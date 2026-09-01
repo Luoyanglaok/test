3efc1d52846eddaaf97b62eb7112e257df2c17d7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:36:26 +0800

    add 5012.lua

---
+++ 5012.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5012.lua
+--Gamename Speedball 2 - Tournament Tutorial
+addappid(5012) --Mainappid Speedball 2 - Tournament Tutorial
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Speedball 2 - Tournament Tutorial
+setManifestid(910, "1332084816209327708", 0)
