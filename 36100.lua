9dedccc0364ed1568a16f70342dc63f9720aa81c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:40:40 +0800

    add 36100.lua

---
+++ 36100.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 36100.lua
+--Gamename Tradewinds Classic
+addappid(36100) --Mainappid Tradewinds Classic
+addappid(36101, 1, "4da9e23ecf735a36ccda24e4a5aecd271b42e3895fa352c957aff5186b3d1c52") --Main Windows Depot Tradewinds Classic
+setManifestid(36101, "5627751337054577512", 0)
