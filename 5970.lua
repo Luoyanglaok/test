b79bf7bb9477fb33508b61afc08589c67e6507c2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:13:28 +0800

    add 5970.lua

---
+++ 5970.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5970.lua
+--Gamename GWEN
+addappid(5970) --Mainappid GWEN
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot GWEN
+setManifestid(910, "1332084816209327708", 0)
