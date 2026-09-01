d57ab798c3a2c7c558f53d41e0db4645537c0d7c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:41:36 +0800

    add 16170.lua

---
+++ 16170.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16170.lua
+--Gamename Fish Tycoon Demo
+addappid(16170) --Mainappid Fish Tycoon Demo
+addappid(16171, 1, "ccb5fe6061cce1968a60c8a2c4233b305b8cdc21d5e5208582801dee7282436a") --Main Windows Depot Fish Tycoon Demo
+setManifestid(16171, "7230485743738849319", 0)
