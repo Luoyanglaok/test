e236199840ea180801c406a0fd75a82e136f680b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:43:14 +0800

    add 5076.lua

---
+++ 5076.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5076.lua
+--Gamename Far Cry 2: E3 Gameplay
+addappid(5076) --Mainappid Far Cry 2: E3 Gameplay
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Far Cry 2: E3 Gameplay
+setManifestid(910, "1332084816209327708", 0)
