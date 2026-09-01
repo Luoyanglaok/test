d191805589e8918c0488fe46ab885aa78dd59501
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:54:28 +0800

    add 21080.lua

---
+++ 21080.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21080.lua
+--Gamename Terminator Salvation
+addappid(21080) --Mainappid Terminator Salvation
+addappid(21081, 1, "158d56b2b89c3623a8bf033520dc49b7657785047a687563a2c67d650b8ca15c") --Main Windows Depot Terminator Salvation
+setManifestid(21081, "8767388101298574668", 0)
