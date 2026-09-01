f08ca9ad86f84eb185bf0f71b4879eb5ba370020
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:21:50 +0800

    add 5183.lua

---
+++ 5183.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5183.lua
+--Gamename Pirates of the Burning Sea Trailer
+addappid(5183) --Mainappid Pirates of the Burning Sea Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Pirates of the Burning Sea Trailer
+setManifestid(910, "1332084816209327708", 0)
