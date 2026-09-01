c9892900427417cc3868757fe2c982a260eaca61
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:30:22 +0800

    add 34470.lua

---
+++ 34470.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34470.lua
+--Gamename Sid Meier's Civilization IV: Colonization
+addappid(34470) --Mainappid Sid Meier's Civilization IV: Colonization
+addappid(16812, 1, "deba2bf70e820bf1225cb2c31820245b8a0e106a452605af4e8b4f3f7685dbd4") --Main Windows Language Depot English Sid Meier's Civilization IV: Colonization
+setManifestid(16812, "8216847508664978347", 0)
+addappid(16813, 1, "a978ccd2728487636bc4ece2380d11e8a797f4d4534d823626068974b4899fed") --Main Windows Language Depot French Sid Meier's Civilization IV: Colonization
+setManifestid(16813, "3205752415808184622", 0)
+addappid(16814, 1, "5feb1fd92ce940a7c9266e0a8cc3f7e8d315220f87f66c41959eacc45d0972d3") --Main Windows Language Depot German Sid Meier's Civilization IV: Colonization
+setManifestid(16814, "2933854372902432126", 0)
+addappid(16815, 1, "ac02a2e0237eb1b02106b1d4344f1697b46614d8d99a89cf20ec748f547a9e8d") --Main Windows Language Depot Italian Sid Meier's Civilization IV: Colonization
+setManifestid(16815, "2955027371324891156", 0)
+addappid(16816, 1, "bccce273554bbdcddf844d385cf45a0a4a1f98bdfbbbcadeb9a542073b8169b1") --Main Windows Language Depot Spanish Sid Meier's Civilization IV: Colonization
+setManifestid(16816, "7965551106205470389", 0)
+addappid(34471, 1, "916deb73a865e862f519c88638a3adb6279dc28541b6f7251301917ceaf312ed") --Main Macos Depot Sid Meier's Civilization IV: Colonization
+setManifestid(34471, "812493725833215527", 640677984)
