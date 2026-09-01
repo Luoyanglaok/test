bc43c016d7fc19752728a1a678741bda35615d17
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:18:32 +0800

    add 5135.lua

---
+++ 5135.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5135.lua
+--Gamename Tomb Raider: Undeworld - Mexico
+addappid(5135) --Mainappid Tomb Raider: Undeworld - Mexico
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Tomb Raider: Undeworld - Mexico
+setManifestid(910, "1332084816209327708", 0)
