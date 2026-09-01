c655efff8d67700a4734fd6f39dd9ffcc20e770f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:48:36 +0800

    add 38830.lua

---
+++ 38830.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38830.lua
+--Gamename CrimeCraft GangWars
+addappid(38830) --Mainappid CrimeCraft GangWars
+addappid(38801, 1, "db0bf9d5de10f1ff124c84b68680de2b520aa3070a6acc0b83cab27ffaa4c8f0") --Main Windows Depot CrimeCraft GangWars
+setManifestid(38801, "3191111342085009798", 5037417697043)
