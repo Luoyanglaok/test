057ab220ba643b8f8537b335b2f33755fd84dad8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:47:50 +0800

    add 973.lua

---
+++ 973.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 973.lua
+--Gamename Sam & Max 106: Bright Side of the Moon Trailer
+addappid(973) --Mainappid Sam & Max 106: Bright Side of the Moon Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Sam & Max 106: Bright Side of the Moon Trailer
+setManifestid(910, "1332084816209327708", 0)
