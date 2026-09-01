213d2f115be63013b7686650b735b1c56de86f08
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:19:46 +0800

    add 7630.lua

---
+++ 7630.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7630.lua
+--Gamename Sid Meier's Railroads Demo
+addappid(7630) --Mainappid Sid Meier's Railroads Demo
+addappid(7631, 1, "e15772ce43caedff9825428d00e73a2f84b9f0d4c0ad3fc1b69f83ba9ec02ef4") --Main Windows Depot Sid Meier's Railroads Demo
+setManifestid(7631, "8291374991315761675", 0)
