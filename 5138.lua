8a5467206d64fb3df1cbdac85e4b4d4e5c85eaa6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:18:41 +0800

    add 5138.lua

---
+++ 5138.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5138.lua
+--Gamename Left 4 Dead TV Spot 1 - 1080p
+addappid(5138) --Mainappid Left 4 Dead TV Spot 1 - 1080p
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Left 4 Dead TV Spot 1 - 1080p
+setManifestid(910, "1332084816209327708", 0)
