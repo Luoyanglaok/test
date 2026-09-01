1eedc8ae979f46a8d07ed0b9d3c5c3036be144fa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:26:46 +0800

    add 3220.lua

---
+++ 3220.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3220.lua
+--Gamename SpaceForce: Rogue Universe
+addappid(3220) --Mainappid SpaceForce: Rogue Universe
+addappid(3221, 1, "9a1ad7dc7c7858073c9ad4e78c59a89128f33a4043ec521171bfe62e0b749296") --Main Windows Depot SpaceForce: Rogue Universe
+setManifestid(3221, "2753156873649825661", 0)
+addappid(3222, 1, "7a13ac81d5ff5953c80084039daa397aee5f17d46df30e16c13ccda5864c6aec") --Main Windows Depot SpaceForce: Rogue Universe
+setManifestid(3222, "9127767504529290733", 0)
+addappid(3223, 1, "fbb71fd5e64ee5ac43dbfef089483fe069d25b89c8f78cf1c52f3b77b20727e1") --Main Windows Language Depot French SpaceForce: Rogue Universe
+setManifestid(3223, "5425083716955672051", 0)
+addappid(3224, 1, "ba27b0b5f4adc7eacb77612a3105b26ea425f4803763ee8795a10aa721d4cb87") --Main Windows Language Depot German SpaceForce: Rogue Universe
+setManifestid(3224, "3947616121577758684", 0)
+addappid(3225, 1, "b34c71764b68e8c542a95238160278223eabb1d2934b53006d65641f81ed5c08") --Main Windows Language Depot Italian SpaceForce: Rogue Universe
+setManifestid(3225, "6541249676862322144", 0)
+addappid(3226, 1, "6cf1a9dbbf87ca16c508ba41eb7bb1ed9a9b4a1782c1665d374175a514f2660f") --Main Windows Language Depot Spanish SpaceForce: Rogue Universe
+setManifestid(3226, "3400388038204480799", 0)
