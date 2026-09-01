3db294f74555b26101c1b9788e3c469f03989e9a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:35:06 +0800

    add 12570.lua

---
+++ 12570.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12570.lua
+--Gamename Hot Dish
+addappid(12570) --Mainappid Hot Dish
+addappid(12571, 1, "9c6650ef2190c3af938ab4b330de85161f61ca523a5a5e21c37c0f95dee863f9") --Main Windows Depot Hot Dish
+setManifestid(12571, "5862990992561368112", 0)
