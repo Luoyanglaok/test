77f6e71da478c58377bc03dddc9077db9a19a2cc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:34:10 +0800

    add 5343.lua

---
+++ 5343.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5343.lua
+--Gamename R.U.S.E. E3 Demo Part 2
+addappid(5343) --Mainappid R.U.S.E. E3 Demo Part 2
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot R.U.S.E. E3 Demo Part 2
+setManifestid(910, "1332084816209327708", 0)
