694ee4cdbef301cc1eaefce8f71a59221310aa88
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:57:29 +0800

    add 22160.lua

---
+++ 22160.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 22160.lua
+--Gamename Europa Universalis: Rome - Demo
+addappid(22160) --Mainappid Europa Universalis: Rome - Demo
+addappid(22161, 1, "a1bc4ce8dac0c120f4342e98d2f8de0ecfbf8152cbe269a4d73802e4754299a2") --Main Windows Depot Europa Universalis: Rome - Demo
+setManifestid(22161, "4735078530794964731", 0)
