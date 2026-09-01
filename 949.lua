34740fd1a52f60db4effb17504412b3a73649763
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:45:10 +0800

    add 949.lua

---
+++ 949.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 949.lua
+--Gamename Joint Task Force Trailer
+addappid(949) --Mainappid Joint Task Force Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Joint Task Force Trailer
+setManifestid(910, "1332084816209327708", 0)
