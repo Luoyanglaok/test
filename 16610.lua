ec692025835754c2dd8615b958dd67e630637dd1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:42:52 +0800

    add 16610.lua

---
+++ 16610.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16610.lua
+--Gamename Trials 2: Second Edition Demo
+addappid(16610) --Mainappid Trials 2: Second Edition Demo
+addappid(16611, 1, "b538f822b08c7af298c17386f4e9053c0e7e0fabf4933b5d2f74d802c6a74069") --Main Windows Depot Trials 2: Second Edition Demo
+setManifestid(16611, "6736468297629873591", 0)
