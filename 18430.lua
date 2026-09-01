89b5312a9e7c38f5aa25dfa13c367e4ed51c8d63
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:48:53 +0800

    add 18430.lua

---
+++ 18430.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 18430.lua
+--Gamename Crazy Machines 1.5
+addappid(18430) --Mainappid Crazy Machines 1.5
+addappid(18431, 1, "fb1d9a26469c089501d8a6cab5967733f189c4d5ce55b1b868f10b9123a0380d") --Main Windows Depot Crazy Machines 1.5
+setManifestid(18431, "3961109105394109464", 0)
