b32fedd9e4b23d3b0a50ac108cdcaefe6c4c1353
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:42:07 +0800

    add 36900.lua

---
+++ 36900.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 36900.lua
+--Gamename Angle of Attack
+addappid(36900) --Mainappid Angle of Attack
+addappid(36901, 1, "23ebbe1cb12478ce624544a59e0b3d9365ecbd672366e7deb19a4b084fc9a8ba") --Main Windows Language Depot English Angle of Attack
+setManifestid(36901, "8169854862430203268", 306084608)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Main Windows Depot Angle of Attack
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Main Windows Depot Angle of Attack
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Angle of Attack
