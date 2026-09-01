de68974b00a840881b8c4a26a3eccd0a24c5e65c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:34:19 +0800

    add 12430.lua

---
+++ 12430.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12430.lua
+--Gamename SlamIt Pinball Big Score
+addappid(12430) --Mainappid SlamIt Pinball Big Score
+addappid(12431, 1, "e044175eae515b4a88a06d06c415ae10d92ce6b70c9c9d41c82e7208c5161392") --Main Windows Depot SlamIt Pinball Big Score
+setManifestid(12431, "7576817827656540470", 0)
