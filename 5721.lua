b13171d77fa187815e9f1d35e34a1f969d8e70bb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:51:22 +0800

    add 5721.lua

---
+++ 5721.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5721.lua
+--Gamename Napoleon: Total War - Peninsular Campaign
+addappid(5721) --Mainappid Napoleon: Total War - Peninsular Campaign
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Napoleon: Total War - Peninsular Campaign
+setManifestid(910, "1332084816209327708", 0)
