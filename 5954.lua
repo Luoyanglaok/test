6a1493ac9948b3cc2f7ded2bb1ca34d5027e001f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:12:25 +0800

    add 5954.lua

---
+++ 5954.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5954.lua
+--Gamename Governor of Poker 2
+addappid(5954) --Mainappid Governor of Poker 2
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Governor of Poker 2
+setManifestid(910, "1332084816209327708", 0)
