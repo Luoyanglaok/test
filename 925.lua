111427b4e101bc5811ecf5bf46bd72532ba4aac2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:42:41 +0800

    add 925.lua

---
+++ 925.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 925.lua
+--Gamename Red Orchestra Vehicle Tutorial
+addappid(925) --Mainappid Red Orchestra Vehicle Tutorial
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Red Orchestra Vehicle Tutorial
+setManifestid(910, "1332084816209327708", 0)
