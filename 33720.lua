8eced4ba3d44ca84cd42cdfe17b2be49b7a8b7d1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:22:46 +0800

    add 33720.lua

---
+++ 33720.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33720.lua
+--Gamename Paws and Claws: Pet Vet
+addappid(33720) --Mainappid Paws and Claws: Pet Vet
+addappid(33721, 1, "8e473f1c4ee5d6b22e511bb9a022f5211c02fb18fcffcefc0c639e9bac002f78") --Main Windows Depot Paws and Claws: Pet Vet
+setManifestid(33721, "2955338211240053401", 0)
