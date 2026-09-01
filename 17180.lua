5d050574eee166bab3542f03cd6623f415b3d89a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:45:53 +0800

    add 17180.lua

---
+++ 17180.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 17180.lua
+--Gamename Mosby's Confederacy
+addappid(17180) --Mainappid Mosby's Confederacy
+addappid(17181, 1, "218a49cf928600782cf0361f0bc7ed01c2f17b73399fb3ec65185ead1ea4c85d") --Main Windows Depot Mosby's Confederacy
+setManifestid(17181, "1109522393060157528", 0)
