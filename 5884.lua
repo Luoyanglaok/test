8005911382a3bdc452e2bd6da6d068b4a1fea5cc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:08:05 +0800

    add 5884.lua

---
+++ 5884.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5884.lua
+--Gamename Football Manager 2011 Dev Diary
+addappid(5884) --Mainappid Football Manager 2011 Dev Diary
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Football Manager 2011 Dev Diary
+setManifestid(910, "1332084816209327708", 0)
