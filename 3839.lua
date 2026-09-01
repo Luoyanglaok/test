cad75e7b4c8b99aea40c1ff5ee18d4477d1288f5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:29:08 +0800

    add 3839.lua

---
+++ 3839.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3839.lua
+--Gamename Psychonauts Original Score
+addappid(3839, 1, "fb90a41f2b4753baee201a045fefadb96cdddfd5e45e905d39eecfbd29678ae2") --Mainappid Psychonauts Original Score
+setManifestid(3839, "7723707352790636229", 58557616)
+addappid(3840, 1, "17170ae4984b940300dd09fa40dc96ad9e433e29b4246aaba81dce9b78a1bb29") --Main Windows Depot Psychonauts Original Score
