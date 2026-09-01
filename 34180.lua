7d312bf9eb2b958546ff5f232656d48e3e478fc1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:24:06 +0800

    add 34180.lua

---
+++ 34180.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34180.lua
+--Gamename Vancouver 2010™ - The Official Video Game of the Olympic Winter Games
+addappid(34180) --Mainappid Vancouver 2010™ - The Official Video Game of the Olympic Winter Games
+addappid(34181, 1, "28b74143a633589a1f8d8794ce43c35601667975e757c64bda8a558ae64854ca") --Main Windows Depot Vancouver 2010™ - The Official Video Game of the Olympic Winter Games
