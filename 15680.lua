7481979359d06a993e3ee4864138c604ee5f290c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:39:19 +0800

    add 15680.lua

---
+++ 15680.lua
@@ -0,0 +1,24 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15680.lua
+--Gamename Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+addappid(15680) --Mainappid Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+addappid(15681, 1, "a9f6b9b25d31555b39ed27eaaae5370b556e59cf677b221190a1dde28f4e9ffd") --Main Windows Depot Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15681, "8022621943138449622", 0)
+addappid(15682, 1, "a151f42b49e96fb5de817e2bbe71936e9c1c87737a4e58cbe0e180595f2d16c7") --Main Windows Language Depot English Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15682, "1749794635800549393", 0)
+addappid(15683, 1, "bdb18505a45148e12a3e5a2ac3deedeedeefd55b09fbcc22f22c52bf909cebd7") --Main Windows Language Depot French Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15683, "773549247558730259", 0)
+addappid(15684, 1, "f48504c3334e9d147c0a542ef312e556bd0bdef4b8b1946b5ad937921aa8e0f5") --Main Windows Language Depot Italian Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15684, "6558630740497914028", 0)
+addappid(15685, 1, "67ec7684f8fdfd5858fed715c586b0669a1be3d1f22d1ee0b76c4d6b2273ee10") --Main Windows Language Depot German Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15685, "3982679155043022925", 0)
+addappid(15686, 1, "87c292e5370b4e5839cdf402329736ed29ba1efe65889eb754049604be8b607f") --Main Windows Language Depot Spanish Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15686, "5516621295451615395", 0)
+addappid(15687, 1, "7fe77ce5b3ef12fd4a989d4befc1baf23712ac09b52481f9a9aa902f804102ed") --Main Windows Language Depot Russian Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15687, "6730308113253587244", 0)
+addappid(15688, 1, "a35ad87189438870079f235cbedf645c8a9357de49535658f3d7bab52f2fe2e6") --Main Windows Language Depot Czech Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15688, "5388436478426121750", 0)
+addappid(15689, 1, "0d172c9e8324ac26355930625e086751997d63b9834bc6b9bf9815bf1865ba05") --Main Windows Language Depot Polish Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15689, "7518187670216889810", 0)
+addappid(15690, 1, "e9cdba384afe12f74fbead46d940141190087af4c623b100e823aed1fb256246") --Main Windows Language Depot Hungarian Warhammer® 40,000™: Dawn of War® II - Single Player Demo
+setManifestid(15690, "8566469447407757656", 0)
