ba101d4a8342fe4087e249efe6602568eec065aa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:28:32 +0800

    add 34311.lua

---
+++ 34311.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34311.lua
+--Gamename Kid Chameleon™
+addappid(34311, 1, "299b996c7ca601015a8a989714099647c26e09661823b4a705e113b6b7f924cc") --Mainappid Kid Chameleon™
+addappid(34312, 1, "de22b2fe4da6cba04d5d7d361a15a99bea02a25c3d482f8b84458dcfa3ca8497") --Main Windows Depot Kid Chameleon™
