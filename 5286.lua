6520c35a13c91443e5fd8f442b20b573b4ae949c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:28:30 +0800

    add 5286.lua

---
+++ 5286.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5286.lua
+--Gamename Oblivion Game of the Year Edition Trailer
+addappid(5286) --Mainappid Oblivion Game of the Year Edition Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Oblivion Game of the Year Edition Trailer
+setManifestid(910, "1332084816209327708", 0)
