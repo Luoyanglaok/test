aa69df3dc71a3a1bac0363c1bbdd00102dd22b0e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:46:44 +0800

    add 962.lua

---
+++ 962.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 962.lua
+--Gamename Tomb Raider: Anniversary Trailer
+addappid(962) --Mainappid Tomb Raider: Anniversary Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Tomb Raider: Anniversary Trailer
+setManifestid(910, "1332084816209327708", 0)
