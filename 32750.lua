560e1e5c78f0317a816a1f6cca93e6e30abbacd5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:20:10 +0800

    add 32750.lua

---
+++ 32750.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 32750.lua
+--Gamename Comanche 4
+addappid(32750) --Mainappid Comanche 4
+addappid(32751, 1, "c1caa70d3925608e56cb72e954da6f3905cbe4e5f1235e7912bbaf5297ec3ecc") --Main Windows Depot Comanche 4
+setManifestid(32751, "4355813920335948014", 0)
