453d5e4df75ec1707ed8eb5884378bb92e5074b8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:37:52 +0800

    add 5030.lua

---
+++ 5030.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5030.lua
+--Gamename Assassin's Creed Trailer
+addappid(5030) --Mainappid Assassin's Creed Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Assassin's Creed Trailer
+setManifestid(910, "1332084816209327708", 0)
