be4e089a44fd5b23cb8ec6963347da57936396e6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:41:20 +0800

    add 16060.lua

---
+++ 16060.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16060.lua
+--Gamename Samantha Swift and the Golden Touch
+addappid(16060) --Mainappid Samantha Swift and the Golden Touch
+addappid(16061, 1, "3cbf3eff98f6cdb07a379f0b93183df6311cdbdc351d56b4fb9e7aa2b3a9cdd5") --Main Windows Depot Samantha Swift and the Golden Touch
+setManifestid(16061, "5391433640275103276", 0)
