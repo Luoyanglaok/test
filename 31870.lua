2da44591796fc4413154a37f0b95df5b4c2e98ac
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:17:50 +0800

    add 31870.lua

---
+++ 31870.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 31870.lua
+--Gamename Nancy Drew® Dossier: Lights, Camera, Curses!
+addappid(31870) --Mainappid Nancy Drew® Dossier: Lights, Camera, Curses!
+addappid(31871, 1, "d9709246e19e9dff67ab8283e03b184113ebe4b2bcb83efdc52958b26a1dd871") --Main Windows Depot Nancy Drew® Dossier: Lights, Camera, Curses!
+setManifestid(31871, "6552789473567776902", 279919456)
