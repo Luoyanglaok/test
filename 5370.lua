bdf09fc7897b2ca9335b3e0e84d1fb21e11f7203
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:36:07 +0800

    add 5370.lua

---
+++ 5370.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5370.lua
+--Gamename Heroes Over Europe - Environments
+addappid(5370) --Mainappid Heroes Over Europe - Environments
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Heroes Over Europe - Environments
