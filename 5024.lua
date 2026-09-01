8274f18bffcdb3bbd2fe48fefde41ded6c61df6b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:37:41 +0800

    add 5024.lua

---
+++ 5024.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5024.lua
+--Gamename Rainbox Six: Vegas 2 Trailer
+addappid(5024) --Mainappid Rainbox Six: Vegas 2 Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Rainbox Six: Vegas 2 Trailer
+setManifestid(910, "1332084816209327708", 0)
