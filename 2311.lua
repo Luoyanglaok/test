31d2b6661c30c254c0e69057dd910701bb2a548c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:22:01 +0800

    add 2311.lua

---
+++ 2311.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2311.lua
+--Gamename Steam Store
+addappid(2311, 1, "d854f0dbf7aef388219ae3af1c437d156dc6ecd64ef81439e276abbec1872689") --Mainappid Steam Store
