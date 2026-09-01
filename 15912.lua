7421b5329ab336af85d495e132753abed169fd0d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:40:05 +0800

    add 15912.lua

---
+++ 15912.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15912.lua
+--Gamename Luxor Amun Rising Demo
+addappid(15912) --Mainappid Luxor Amun Rising Demo
+addappid(15911, 1, "d0042db6da385f64367291ed4af0143d7abbc08adc1ad8f6007049ad9235437e") --Main Windows Depot Luxor Amun Rising Demo
+setManifestid(15911, "9203417229911749715", 0)
+addappid(15913, 1, "4A364E3E58C9E482E30A409A52C106343F657FCDCA161DAA84FCBA328D6D2456") --Main Windows Depot Luxor Amun Rising Demo
