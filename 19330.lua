6414df524dc508f93f3e2b19b23aa9810b70a920
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:51:54 +0800

    add 19330.lua

---
+++ 19330.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19330.lua
+--Gamename Space Ark - Demo
+addappid(19330) --Mainappid Space Ark - Demo
+addappid(19331, 1, "a9ff22fbb4287fca1160c64aae71d4069fb053ddd63ddff93232d686f08c9c32") --Main Windows Depot Space Ark - Demo
+setManifestid(19331, "902804322302626829", 0)
