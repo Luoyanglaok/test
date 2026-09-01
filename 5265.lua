d223d1c30f38d25291a6b02828a121c8c3ea801d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:26:25 +0800

    add 5265.lua

---
+++ 5265.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5265.lua
+--Gamename RFG Tools of Destruction 6
+addappid(5265) --Mainappid RFG Tools of Destruction 6
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot RFG Tools of Destruction 6
+setManifestid(910, "1332084816209327708", 0)
