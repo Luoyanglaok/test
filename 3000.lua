a3b013bab6ac6d49cf2d755bf5433fdd7293a35e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:25:41 +0800

    add 3000.lua

---
+++ 3000.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3000.lua
+--Gamename GTI Racing
+addappid(3000) --Mainappid GTI Racing
+addappid(3001, 1, "5647792bba4b6f2c82372ad0fc3784ca56f7d7a17c1b5b093a62a28d2cf7a645") --Main Windows Depot GTI Racing
+setManifestid(3001, "5732367675251728661", 0)
+addappid(3002, 1, "73c6e884aed7595ed18b6ffce30de9bc1dc0c7747af6b25df8df67267a971a81") --Main Windows Depot GTI Racing
+setManifestid(3002, "1764763485413967301", 0)
