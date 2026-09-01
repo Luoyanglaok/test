41b229fa35cbe63f725bb8f89d72ff6d1c1f1813
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:25:28 +0800

    add 5250.lua

---
+++ 5250.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5250.lua
+--Gamename RFG Dev Diary
+addappid(5250) --Mainappid RFG Dev Diary
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot RFG Dev Diary
+setManifestid(910, "1332084816209327708", 0)
