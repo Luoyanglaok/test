1d82c2c6a24f1471610e2c15500295da511c2d3d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:57:36 +0800

    add 42712.lua

---
+++ 42712.lua
@@ -0,0 +1,37 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42712.lua
+--Gamename Steam Store
+addappid(42712, 1, "5368e07809c82a9de5bdd7d4367711a499bf3c5aaa8cda2a74eed0ec6692f953") --Mainappid Steam Store
+addappid(42713, 1, "a1c22f61cab5fbe228ca1e586d0995d1c3ce0443ae89a1b69868fb63b237b459") --Main Windows Language Depot German Steam Store
+--Dlcname Call of Duty: Black Ops - Multiplayer
+addappid(42710) --Dlcname Call of Duty: Black Ops - Multiplayer
+addappid(42702, 1, "0a06c6a7e3239813ec8eaa68b3f825792e50e8f9766d7218440f95cafc414df2") --Dlc Windows Depot Call of Duty: Black Ops - Multiplayer
+addappid(42703, 1, "48e008cbcb32413a3e7645bb624908b69f3c45a1e1bd2d288d710120e7db2872") --Dlc Windows Language Depot English Call of Duty: Black Ops - Multiplayer
+addappid(42704, 1, "ef154528aeaf6541d4b637fa5e7820e12f45f66844c66bae380dd91921e73e23") --Dlc Windows Depot Call of Duty: Black Ops - Multiplayer
+setManifestid(42704, "2396700113547373447", 0)
+addappid(42705, 1, "12ace44104d1b7194c091d35ced64b2eaa19e0fbc3c21c4c6b0196624408ee3a") --Dlc Windows Language Depot French Call of Duty: Black Ops - Multiplayer
+addappid(42706, 1, "01c5172e023bfeda1a5441d6bd640e5b8b8c70b1882ac68d214e1a6ada4a85b8") --Dlc Windows Language Depot German Call of Duty: Black Ops - Multiplayer
+addappid(42707, 1, "4239d3b38cd64b5d20dba2b00d0b170a008574a779a2233da2bf9a8405e165d1") --Dlc Windows Language Depot Italian Call of Duty: Black Ops - Multiplayer
+addappid(42708, 1, "6977051f55833a3bb91c8ca4df029767d7069a9aa15ee46ad2c2233c159b5c86") --Dlc Windows Language Depot Spanish Call of Duty: Black Ops - Multiplayer
+addappid(42709, 1, "765bedbba80d60fb32bd6ece8b768bbcfd0e3c06046aad7f551613c1f485f5cd") --Dlc Windows Language Depot Russian Call of Duty: Black Ops - Multiplayer
+addappid(42711, 1, "f47b3121d84acb57cf36e8a31252de7aaa36d46cabb5025606c25701af1fc9ff") --Dlc Windows Language Depot Polish Call of Duty: Black Ops - Multiplayer
+addappid(42715, 1, "dd7c7b2cb4e026a6797f04fcb6794dd0b344ecbb922aa2e155f19dcb57e2bc8d") --Dlc Windows Depot Call of Duty: Black Ops - Multiplayer
+setManifestid(42715, "9002586187758155798", 0)
+addappid(42717, 1, "5fda7eac8f2937f0902fdee5e4557d58b5d44c0387b46815ca4c68a13a5be40a") --Dlc Windows Depot Call of Duty: Black Ops - Multiplayer
+setManifestid(42717, "5077930234431075609", 0)
+addappid(42722, 1, "41ff5db29a861668118f45abadc21bc18576a07e8f08da29528b5eb7167c62a3") --Dlc Windows Depot Call of Duty: Black Ops - Multiplayer
+setManifestid(42722, "5149070859526644479", 0)
+addappid(214632, 1, "481c1e943f158a88f14c744f2881f80e324eadabcf226af94a694ab798245b8a") --Dlc Macos Depot Call of Duty: Black Ops - Multiplayer
+addappid(214633, 1, "1827c09ead4857e3d99ed07af58f7c32f468381b3594c2be3f0de7c441e239ad") --Dlc Macos Language Depot English Call of Duty: Black Ops - Multiplayer
+addappid(214634, 1, "06082554b254b4f2109aa1af5abe42d001cb05f5fff01ffc38d20339ad9cd0d3") --Dlc Macos Depot Call of Duty: Black Ops - Multiplayer
+setManifestid(214634, "2189911851910115687", 0)
+addappid(214635, 1, "2ec4aac7f7b604c205cf17bfbff40afc739beb79ba4493e1db6c98151ff4b98e") --Dlc Macos Language Depot French Call of Duty: Black Ops - Multiplayer
+addappid(214636, 1, "ef90f2445f2f952ba7f1dbf12ada65d4c5b7a4f23a2ea0abe0f029ba17a9d797") --Dlc Macos Language Depot German Call of Duty: Black Ops - Multiplayer
+addappid(214637, 1, "6383ae668b25aa3614f8ba35f8ccf6e643a4a6dda535c4683999d6f2450b89ee") --Dlc Macos Language Depot Italian Call of Duty: Black Ops - Multiplayer
+addappid(214638, 1, "f87d21994f2c02b7117e4087e5a2bb480a2bd4454214d173df7d122a85469d0f") --Dlc Macos Language Depot Spanish Call of Duty: Black Ops - Multiplayer
+addappid(214645, 1, "6b22dcf0120eec6c5325356e6f881820c99d077ce7a358b2bf7023447f51f672") --Dlc Macos Depot Call of Duty: Black Ops - Multiplayer
+setManifestid(214645, "1177190134541019008", 0)
+addappid(214647, 1, "6cb4d4a4e558b3d83d5f40d0493e8cb1ede9c0b83ea32dab40c3751916aee923") --Dlc Macos Depot Call of Duty: Black Ops - Multiplayer
+setManifestid(214647, "2999362337012302648", 0)
+addappid(214652, 1, "f4afb093c41f9fe2042ea32801413120187ac714bcbfe128ecda7222364e8f9b") --Dlc Macos Depot Call of Duty: Black Ops - Multiplayer
+setManifestid(214652, "6602885893100293948", 0)
