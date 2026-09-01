046845a87692b50ca05bfb83d6fab4b48c2a2e43
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:07:08 +0800

    add 5867.lua

---
+++ 5867.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5867.lua
+--Gamename Kane & Lynch 2 - You Think You Can Kill Me (FR)
+addappid(5867) --Mainappid Kane & Lynch 2 - You Think You Can Kill Me (FR)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Kane & Lynch 2 - You Think You Can Kill Me (FR)
+setManifestid(910, "1332084816209327708", 0)
