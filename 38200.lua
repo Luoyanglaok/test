f4d67f5e2277ee844a7c855822c6eb53d43241c8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:47:27 +0800

    add 38200.lua

---
+++ 38200.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38200.lua
+--Gamename Raven Squad
+addappid(38200) --Mainappid Raven Squad
+addappid(38201, 1, "6cf4009e749e8fd6ea060f138e38de5a838bbe3ab08e263b31f929f821e4ba3d") --Main Windows Depot Raven Squad
+setManifestid(38201, "7778113703968971653", 0)
+addappid(38202, 1, "944431e21dd5c90a3c20527874f2a788bac3f5b094fec94bab45c297de13c9ea") --Main Windows Language Depot German Raven Squad
+setManifestid(38202, "5366160734489009219", 0)
+addappid(38203, 1, "0d299d3a19dff8bd20049dda8640a4721ff1b1424547a3527ffb8a64c44489d0") --Main Windows Language Depot French Raven Squad
+setManifestid(38203, "3867770713652128892", 0)
