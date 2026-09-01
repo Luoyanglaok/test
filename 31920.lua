6f5e71436219c4be96304a734f7e001bdc44392e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:18:17 +0800

    add 31920.lua

---
+++ 31920.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 31920.lua
+--Gamename Nancy Drew®: The Phantom of Venice
+addappid(31920) --Mainappid Nancy Drew®: The Phantom of Venice
+addappid(31921, 1, "ea8250ba7af367cc20ff2622b3f8e111665fb53767ad3e9328bf12efffe77592") --Main Windows Depot Nancy Drew®: The Phantom of Venice
+setManifestid(31921, "9099478439120854252", 879497984)
+addappid(31922, 1, "c4881e7fe6e4407d3079a2ccaeddf8a0aa34921350f876f7f5fe3e8b2a018840") --Main Windows Language Depot German Nancy Drew®: The Phantom of Venice
+setManifestid(31922, "7251132614163057535", 855361312)
