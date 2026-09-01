d4d12c7af51863a0ec9f61ee1414b8f77ed9ade6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:23:46 +0800

    add 8840.lua

---
+++ 8840.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8840.lua
+--Gamename Major League Baseball 2K9
+addappid(8840) --Mainappid Major League Baseball 2K9
+addappid(8841, 1, "7ab6187126c92ed41c08a98072e6e5b5e3acb68a84a7ec5bd15bf522079fcddc") --Main Windows Depot Major League Baseball 2K9
+setManifestid(8841, "3812202803054315661", 0)
