71f7e49bce4e2f057062c5b853021cebc286e898
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:48:21 +0800

    add 18310.lua

---
+++ 18310.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 18310.lua
+--Gamename Spectraball - Demo
+addappid(18310) --Mainappid Spectraball - Demo
+addappid(18311, 1, "86ba858b5c567dea514438ccfc9b198094138d1aca289fdcb61660756aec1c11") --Main Windows Depot Spectraball - Demo
+setManifestid(18311, "7698656506513204966", 0)
