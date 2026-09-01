958e12183be6695703a6fff694cac343db00aeca
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:12:16 +0800

    add 24500.lua

---
+++ 24500.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24500.lua
+--Gamename Dreamkiller
+addappid(24500) --Mainappid Dreamkiller
+addappid(24501, 1, "ae7fbe87c6daa4bbfb10d448ca0dc38dfa11b3f5947184e3fe20313f60e7edb0") --Main Windows Depot Dreamkiller
+setManifestid(24501, "7835176674188168091", 0)
+addappid(24502, 1, "bdc734f1fc091df84c96839661da851892ef159334001d1ea7f00c2ae35a7f90") --Main Windows Depot Dreamkiller
+setManifestid(24502, "8887519577181900368", 0)
+addappid(24503, 1, "96c30163a88e0df82019d3273575de2b463f917f89a3efbfe91281390cca1648") --Main Windows Depot Dreamkiller
+setManifestid(24503, "7652908797036988006", 0)
