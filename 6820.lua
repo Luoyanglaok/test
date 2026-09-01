c10a85fa195bf284a1de93ddcbe04f53b854702b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:17:17 +0800

    add 6820.lua

---
+++ 6820.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6820.lua
+--Gamename Commandos: Strike Force
+addappid(6820) --Mainappid Commandos: Strike Force
+addappid(6821, 1, "9f9688e05cc4035be926a92eea4acdd5e2002d96ec246bfdef43af81fca563a9") --Main Windows Depot Commandos: Strike Force
+setManifestid(6821, "3235465230557981047", 0)
+addappid(6822, 1, "1d3f3ef6d128da90021b0e0fab91cf1a713ba742e8e975cdfd91e3496f111da9") --Main Windows Depot Commandos: Strike Force
+setManifestid(6822, "7549619815695350589", 0)
+addappid(6823, 1, "cfb60e1ac1a4a6f993213ac3c051dd39c84edc8445983bcb7aab5f4676f3021e") --Main Windows Depot Commandos: Strike Force
+setManifestid(6823, "3395194365119781465", 0)
+addappid(6824, 1, "82440faa3be86211da758e3ff56d4996d02f23073e71c1393d934cc5fce9b74a") --Main Windows Language Depot French Commandos: Strike Force
+setManifestid(6824, "7446902111605236168", 0)
+addappid(6825, 1, "8846aac12868812226b84f2a210098d07528de8d80b0b5199bdf7f41712c5f89") --Main Windows Language Depot German Commandos: Strike Force
+setManifestid(6825, "533570160815417789", 0)
+addappid(6826, 1, "224f8910ed722654a51ba54cfbee5cb3f47311add38e9b9a58911de527130a12") --Main Windows Language Depot Italian Commandos: Strike Force
+setManifestid(6826, "949790423749685773", 0)
+addappid(6827, 1, "3291a0f9fb9d1abd4c02ca9decb9b5b40d7b3573cdb8ebe3aa5e11ca34b17463") --Main Windows Language Depot Spanish Commandos: Strike Force
+setManifestid(6827, "5266929655564079930", 0)
