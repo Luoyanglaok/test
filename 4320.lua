b1222f3f2dd68dbc61f8d03e8217c26d88196ba7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:05:42 +0800

    add 4320.lua

---
+++ 4320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4320.lua
+--Gamename Star Trek: D-A-C
+addappid(4320) --Mainappid Star Trek: D-A-C
+addappid(4321, 1, "71b60c905eada32fcd2fff701ae448801ac3b263dbd2e1dcb7f3331316be408c") --Main Windows Depot Star Trek: D-A-C
+setManifestid(4321, "6798936272522660630", 0)
