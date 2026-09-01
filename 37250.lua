b9959ee52d281aad0431a5bed66a922537d8cd41
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:42:43 +0800

    add 37250.lua

---
+++ 37250.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37250.lua
+--Gamename Dream Chronicles: The Chosen Child
+addappid(37250) --Mainappid Dream Chronicles: The Chosen Child
+addappid(37251, 1, "1a6a903ba1980929fd799d4eac0400274cca390a1a0480b8ae72ae200136de97") --Main Windows Depot Dream Chronicles: The Chosen Child
+setManifestid(37251, "3792938188266874187", 0)
+addappid(37252, 1, "f3e9f76bb7730671346bece8804db717afb45f8d00735236510b2f342d9496a9") --Main Macos Depot Dream Chronicles: The Chosen Child
+setManifestid(37252, "9078135232294023985", 0)
