e6443ec872778f5450f3607970d217faa7ebb8d5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:25:31 +0800

    add 2930.lua

---
+++ 2930.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2930.lua
+--Gamename Birth Of America
+addappid(2930) --Mainappid Birth Of America
+addappid(2931, 1, "0b8d906a416a015ad2ae7270b7c076949b232c4186518b7d3ef3cd5a834629b4") --Main Windows Depot Birth Of America
+setManifestid(2931, "6073555075989129719", 0)
