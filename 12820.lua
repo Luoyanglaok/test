6e1aa3a6633015603aedfd5eaf3ef14e3b0ab768
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:36:15 +0800

    add 12820.lua

---
+++ 12820.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12820.lua
+--Gamename Overlord II Demo
+addappid(12820) --Mainappid Overlord II Demo
+addappid(12821, 1, "7679585f922fb529a64b546f9d33a6bc847d235620abc04fdb88cceccb42de23") --Main Windows Depot Overlord II Demo
+setManifestid(12821, "489963017048869494", 0)
+addappid(12822, 1, "dc8e8f2e61ab3a3af511ae41f2ff6da0238e4801c67fb29e6af59e5c1325bf94") --Main Windows Language Depot Spanish Overlord II Demo
+setManifestid(12822, "4212975807090202278", 0)
+addappid(12823, 1, "b3952c2d77f3ca3c5afbb23f76ffd668c0a8ce83ce3b0ec8580f1d41787ccbb6") --Main Windows Language Depot German Overlord II Demo
+setManifestid(12823, "5645709456728569958", 0)
+addappid(12824, 1, "c24348dcef6f5437b295b0a9fe3aeda60b476259767ee446f3fca30a2b0a7141") --Main Windows Language Depot French Overlord II Demo
+setManifestid(12824, "8076145187232185305", 0)
+addappid(12825, 1, "83ff7b3291b93d7c2020b65564f6f6c1beeae2deba0e8eacb08506b3d4c3ba6f") --Main Windows Language Depot Italian Overlord II Demo
+setManifestid(12825, "1579961855035992923", 0)
