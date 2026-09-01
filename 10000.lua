8218f78f198dbc7b045d45f1a1045aa53618b037
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:26:18 +0800

    add 10000.lua

---
+++ 10000.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10000.lua
+--Gamename Enemy Territory: Quake Wars
+addappid(10000) --Mainappid Enemy Territory: Quake Wars
+addappid(10001, 1, "990e9c13a9fe8072d18efc1c69fbe2d36ee85ed64c274218a1d414d8a9ac67f0") --Main Windows Depot Enemy Territory: Quake Wars
+setManifestid(10001, "5477813184068131469", 0)
+addappid(10002, 1, "9f7b06e197f6c7bd1c91c0920484e696fe096962b2f2a34a75657e0e97462e3b") --Main Windows Depot Enemy Territory: Quake Wars
+setManifestid(10002, "5089600357930497585", 0)
+addappid(10003, 1, "14e6ea5b4a97b5c4652f62fb1b352f1fc0f12ebee7234dbce2eab105014b4b13") --Main Windows Depot Enemy Territory: Quake Wars
+setManifestid(10003, "6954294664838145245", 0)
+addappid(10004, 1, "29dbe482078c72e0c1fbb960a602c97f4229c80b6015d2e97395271a0964d35c") --Main Windows Language Depot French Enemy Territory: Quake Wars
+setManifestid(10004, "5633547836704586446", 0)
+addappid(10006, 1, "36b8d741f05213f8ab72da879c1e1482464795605f16a4a80d89b3abe169a4cb") --Main Windows Language Depot Spanish Enemy Territory: Quake Wars
+setManifestid(10006, "1856538235062813871", 0)
+addappid(10007, 1, "fa2f568646e0eea96fbc91d0c8ac8c09ea9ce7c21781c99b541c4defba8ab866") --Main Windows Depot Enemy Territory: Quake Wars
+setManifestid(10007, "606507097212639045", 0)
