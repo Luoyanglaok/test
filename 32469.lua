d93aa93e530dabb855c1d4f216bebb49340c3a4d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:19:14 +0800

    add 32469.lua

---
+++ 32469.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 32469.lua
+--Gamename Monkey Island 2 Special Edition: LeChuck's Revenge Pre-Order
+addappid(32469) --Mainappid Monkey Island 2 Special Edition: LeChuck's Revenge Pre-Order
+addappid(32470, 1, "319d364e1954ba9405bf5d9bf0fb08de085422c9ba81ba2d157e6f8dd4fc1571") --Main Windows Depot Monkey Island 2 Special Edition: LeChuck's Revenge Pre-Order
