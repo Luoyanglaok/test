b2e96bfc7bb4ca295b20438743d3723f2a520eb0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:02:00 +0800

    add 5818.lua

---
+++ 5818.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5818.lua
+--Gamename Medal of Honor - WOLF
+addappid(5818) --Mainappid Medal of Honor - WOLF
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Medal of Honor - WOLF
+setManifestid(910, "1332084816209327708", 0)
