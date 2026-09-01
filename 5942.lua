eac5cfc768d973d079ec95c52055023d8d89fecc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:11:14 +0800

    add 5942.lua

---
+++ 5942.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5942.lua
+--Gamename MOH - Part 2
+addappid(5942) --Mainappid MOH - Part 2
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot MOH - Part 2
+setManifestid(910, "1332084816209327708", 0)
