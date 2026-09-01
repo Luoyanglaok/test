510a97b4813145753f5c5971c5f6cabb8f3ae5f4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:46:35 +0800

    add 960.lua

---
+++ 960.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 960.lua
+--Gamename Team Fortress 2: Meet the Heavy
+addappid(960, 1, "ab8a2881a92559b7b05560a9a9ad01ea60158371e8b2dcad089042dab02a9618") --Mainappid Team Fortress 2: Meet the Heavy
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Team Fortress 2: Meet the Heavy
+setManifestid(910, "1332084816209327708", 0)
+setManifestid(960, "8574850627098073741", 0)
