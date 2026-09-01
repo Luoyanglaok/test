24022871e7505f6795d6b8c235669848b655aef1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:40:26 +0800

    add 35800.lua

---
+++ 35800.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 35800.lua
+--Gamename BRAINPIPE: A Plunge to Unhumanity
+addappid(35800) --Mainappid BRAINPIPE: A Plunge to Unhumanity
+addappid(35801, 1, "029c4e49a8513a1e69090085988dae0c0e3feec969327987930946332e1ce7eb") --Main Windows Depot BRAINPIPE: A Plunge to Unhumanity
+setManifestid(35801, "2869467151599580318", 0)
+addappid(35802, 1, "add7150d52095e2ec03f493616ed1b0a9b09b5a319c7905e71a08df5718973c1") --Main Macos Depot BRAINPIPE: A Plunge to Unhumanity
+setManifestid(35802, "8211351398154358441", 0)
