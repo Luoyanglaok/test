6dc1b91811c08b89949d130dd29bfed487c04a2d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:52:20 +0800

    add 5739.lua

---
+++ 5739.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5739.lua
+--Gamename Portal 2 Teaser
+addappid(5739) --Mainappid Portal 2 Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Portal 2 Teaser
+setManifestid(910, "1332084816209327708", 0)
