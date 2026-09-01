16b109db3e5336ad77c753fdf1ba45e32004b7c4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:32:38 +0800

    add 5333.lua

---
+++ 5333.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5333.lua
+--Gamename EVE Online Butterfly Effect
+addappid(5333) --Mainappid EVE Online Butterfly Effect
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot EVE Online Butterfly Effect
+setManifestid(910, "1332084816209327708", 0)
