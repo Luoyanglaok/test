9671ebabf28fec308b9c371db8944e921a7104a1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:22:50 +0800

    add 8630.lua

---
+++ 8630.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8630.lua
+--Gamename Attack on Pearl Harbor Demo
+addappid(8630) --Mainappid Attack on Pearl Harbor Demo
+addappid(8631, 1, "f492e144874a35d6e3f30dd0bdab99081aad052abeb2d9104ad3042c471888c5") --Main Windows Depot Attack on Pearl Harbor Demo
+setManifestid(8631, "6018993693106689939", 0)
