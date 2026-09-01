31105a88cde6baca74b639d19db9d087414c4fc1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:44:09 +0800

    add 37370.lua

---
+++ 37370.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37370.lua
+--Gamename TriJinx: A Kristine Kross Mystery™
+addappid(37370) --Mainappid TriJinx: A Kristine Kross Mystery™
+addappid(37371, 1, "07c9f9d110542aeef50279217d73dfdc50cc5441ff634842929c2f5b0df0188f") --Main Windows Depot TriJinx: A Kristine Kross Mystery™
+setManifestid(37371, "6758135618545950961", 0)
