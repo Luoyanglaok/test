64c9a943cfb57517bda39031d00716c6071e94b8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:48:19 +0800

    add 5686.lua

---
+++ 5686.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5686.lua
+--Gamename shatteredHorizon_firepower
+addappid(5686) --Mainappid shatteredHorizon_firepower
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot shatteredHorizon_firepower
+setManifestid(910, "1332084816209327708", 0)
