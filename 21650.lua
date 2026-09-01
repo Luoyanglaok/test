fb78252e031aab0ee4292bd729995bc999fb4811
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:55:34 +0800

    add 21650.lua

---
+++ 21650.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21650.lua
+--Gamename FLOCK! Demo
+addappid(21650) --Mainappid FLOCK! Demo
+addappid(21651, 1, "f0e50447c3c284f8466b121f2f77e98c55efbb3fc2bbc557749adf5efb1f6454") --Main Windows Depot FLOCK! Demo
+setManifestid(21651, "1641567183030836043", 0)
