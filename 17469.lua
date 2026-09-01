c1be4b3ffd7c7147128905b40ad7c80dc6fc612a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:46:41 +0800

    add 17469.lua

---
+++ 17469.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 17469.lua
+--Gamename Steam Store
+addappid(17469) --Mainappid Steam Store
+addappid(17470, 1, "a002c74e28525f01cb8638947aefc2c348ef2b0621881a336b61af380a5d4091") --Main Windows Depot Steam Store
+--Dlcname Dead Space (2008)
+addappid(17471, 1, "37c09725566a2f37a7e099db99334638a3bd9a5e0baf7d79d67039cab768535c") --Dlc Windows Depot Dead Space (2008)
+setManifestid(17471, "2334814020589705387", 0)
+addappid(17472, 1, "bb179e217854555d2ad5686797567923b79d9ca2f8bc1cc821327220afa6b493") --Dlc Windows Language Depot English Dead Space (2008)
+setManifestid(17472, "4696807564043505398", 0)
+addappid(17473, 1, "eb3cbc31e17f7f645f2c9261acdf60aa52f6bdfc4d37d2605bd7e70f6bb48b0c") --Dlc Windows Language Depot French Dead Space (2008)
+setManifestid(17473, "4576600954076636304", 0)
+addappid(17474, 1, "4592e52066a840fa4fef21406c8b2c6674c8eda00575e8d1e4077770e8d262cb") --Dlc Windows Language Depot German Dead Space (2008)
+setManifestid(17474, "1240696795312330974", 0)
+addappid(17475, 1, "e6b7525043e7caadea7344e0ef880b3ee706138a5ab04a06d7f769743cb65cde") --Dlc Windows Language Depot Italian Dead Space (2008)
+setManifestid(17475, "1757382782543725383", 0)
+addappid(17476, 1, "6220e7fb29844d167ad7f4142fecc275decbf522be1b2bfca5afc8f180d83e29") --Dlc Windows Language Depot Spanish Dead Space (2008)
+setManifestid(17476, "4237117223104214246", 0)
