6c11d8f90fc75a4ba4d2c743a5641453d7c90117
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:26:53 +0800

    add 34282.lua

---
+++ 34282.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34282.lua
+--Gamename Shadow Dancer™
+addappid(34282, 1, "ab900a58c24adca08b5bce11185e973585cc8b79036622e43e15ec0af5b49208") --Mainappid Shadow Dancer™
+addappid(34283, 1, "015b1baae157b15f7802babfe3a931a6ac9aa6bd29788573fc1b958b8fb7159f") --Main Windows Depot Shadow Dancer™
