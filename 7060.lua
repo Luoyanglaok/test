73e425dcba1cf9e4697719a6c390d5933f8db781
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:17:35 +0800

    add 7060.lua

---
+++ 7060.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7060.lua
+--Gamename Infernal
+addappid(7060) --Mainappid Infernal
+addappid(7061, 1, "0c8854658e5dd01a46a65bff14707ea14556ec9f4f1ad075d4311cb72017e7cc") --Main Windows Depot Infernal
+setManifestid(7061, "2247027158640745850", 0)
+addappid(7062, 1, "4ba4fabd86b7a1497382d829eba8e6a2e92e79dae4b7e1a9c2fdd7b80869b724") --Main Windows Depot Infernal
+setManifestid(7062, "6085038734054334461", 0)
+addappid(7063, 1, "3e126e4d5fde940fc0c68757dc3ef2859c02b539d752b949a5f5429e5811cb41") --Main Windows Depot Infernal
+setManifestid(7063, "6818865357424416895", 0)
