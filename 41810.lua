45594f141e1fd2af22dfeec29a30b1fd0a2bda1e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:53:33 +0800

    add 41810.lua

---
+++ 41810.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 41810.lua
+--Gamename Gratuitous Space Battles - Demo
+addappid(41810) --Mainappid Gratuitous Space Battles - Demo
+addappid(41811, 1, "c595338cfa2168a14ea760e11c59e44a69a678e848cbe188c746b2bb4172384b") --Main Windows Depot Gratuitous Space Battles - Demo
+setManifestid(41811, "565009581334426916", 0)
