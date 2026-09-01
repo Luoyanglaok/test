246ad545ca1cc7016a2f2fd406e2f4d59fdd0e82
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:52:11 +0800

    add 19510.lua

---
+++ 19510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19510.lua
+--Gamename Eternity's Child Demo
+addappid(19510) --Mainappid Eternity's Child Demo
+addappid(19511, 1, "5d0bb4a2e6d248e7dddcb509e3f2850d73c8a0ce4d7db6116d35f7d70fea6f17") --Main Windows Depot Eternity's Child Demo
+setManifestid(19511, "7516516288327376062", 0)
