c40dfac94c4a36d22e843e4f5da921fd1c3f4bed
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:20:31 +0800

    add 1522.lua

---
+++ 1522.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1522.lua
+--Gamename DEFCON Demo
+addappid(1522) --Mainappid DEFCON Demo
+addappid(1521, 1, "c3d037dde2d61d13f18390480d788d37b51e6970543e8142cb05d284bec37f0e") --Main Windows Depot DEFCON Demo
+addappid(1523, 1, "251fc988abdc830d11c03c40efa19601b729e6b333463cc21d025d1d1c1a48a4") --Main Linux Depot DEFCON Demo
+addappid(1527, 1, "eda1c9403d5431539e1245cdc6f080b89e539c53d97eaa0fb76f735b418ef036") --Main Macos Depot DEFCON Demo
