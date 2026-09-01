45ab218054997de83c5ea45a8c667e86fb5296bb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:52:54 +0800

    add 5744.lua

---
+++ 5744.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5744.lua
+--Gamename KL2 Arcade Mode English (PEGI)
+addappid(5744) --Mainappid KL2 Arcade Mode English (PEGI)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot KL2 Arcade Mode English (PEGI)
+setManifestid(910, "1332084816209327708", 0)
