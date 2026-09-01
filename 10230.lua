dac54bde6a87c245448c4a287470a5975121544a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:28:05 +0800

    add 10230.lua

---
+++ 10230.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10230.lua
+--Gamename Dead Mountaineer's Hotel
+addappid(10230) --Mainappid Dead Mountaineer's Hotel
+addappid(10231, 1, "246384f96c6bc3174b1ace87b7e91cd048a712696f0bf079e8ba56d853dc600b") --Main Windows Depot Dead Mountaineer's Hotel
+setManifestid(10231, "3278558586691774099", 0)
+addappid(10232, 1, "a53928c5a6347e95f6d2972e28fa27c4f7fcccb4aa9c95a438379dde337f1971") --Main Windows Language Depot English Dead Mountaineer's Hotel
+setManifestid(10232, "7172234357081880279", 0)
+addappid(10233, 1, "3c3f9ff6af84a489647ef5e06b09140f1dc2288c6e7947dfd78216b5edde7c48") --Main Windows Language Depot German Dead Mountaineer's Hotel
+setManifestid(10233, "207954740335786001", 0)
+addappid(10234, 1, "cb2ab52a9a30caa30f358fd13c255e2a42aeb0c94799ef8a5e36a184903b4545") --Main Windows Language Depot Russian Dead Mountaineer's Hotel
+setManifestid(10234, "1445282062678950021", 0)
