8516f6e1f559b12b966ce8eac0ef382a07e254b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:59:49 +0800

    add 5794.lua

---
+++ 5794.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5794.lua
+--Gamename Arma 2 - Operation Arrowhead Trailer 2
+addappid(5794) --Mainappid Arma 2 - Operation Arrowhead Trailer 2
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Arma 2 - Operation Arrowhead Trailer 2
+setManifestid(910, "1332084816209327708", 0)
