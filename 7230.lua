eb15cc5a8435c26cc624ab558006c60eb6f967eb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:17:59 +0800

    add 7230.lua

---
+++ 7230.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7230.lua
+--Gamename Runaway, The Dream of the Turtle Demo
+addappid(7230) --Mainappid Runaway, The Dream of the Turtle Demo
+addappid(7231, 1, "3db0995282d6d3e76466a23563192bdf5981c9d2d0a659366fed1fa6bc46ccf1") --Main Windows Depot Runaway, The Dream of the Turtle Demo
+setManifestid(7231, "3490436311511967047", 0)
