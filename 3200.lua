2a0ffa6380fda82b18d7c55bc447762aba0a6cbc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:26:26 +0800

    add 3200.lua

---
+++ 3200.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3200.lua
+--Gamename Painkiller: Gold Edition
+addappid(3200) --Mainappid Painkiller: Gold Edition
+addappid(3201, 1, "f35b79aad7f09dd55b35f7673458112d91805ab4c3a3a2ee19625226cb9d8ebe") --Main Windows Depot Painkiller: Gold Edition
+setManifestid(3201, "7496443897475719248", 0)
+addappid(3202, 1, "3909cc6b22264b10966b0154559f9578b04fec872b765ba1a552015bd371b38e") --Main Windows Depot Painkiller: Gold Edition
+setManifestid(3202, "5743764891669388074", 0)
+addappid(3203, 1, "a888f1c5e923445b8a8c0f5085dd4f0c130f3b7f6b515df985f2322f9a7cbb1a") --Main Windows Depot Painkiller: Gold Edition
+setManifestid(3203, "8582863964722191222", 0)
+addappid(3204, 1, "71f3357fabcc218352f352a55f6ad73201addbf2b63771a7d168e453da7e51ec") --Main Windows Depot Painkiller: Gold Edition
+setManifestid(3204, "2021216167458254025", 0)
