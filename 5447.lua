96238791452cc317bed4222d9c8e2b1fa36264af
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:38:46 +0800

    add 5447.lua

---
+++ 5447.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5447.lua
+--Gamename Dragon Age Origins Stone Prisoner Trailer
+addappid(5447) --Mainappid Dragon Age Origins Stone Prisoner Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Dragon Age Origins Stone Prisoner Trailer
+setManifestid(910, "1332084816209327708", 0)
