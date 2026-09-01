add08b3d0f0c22534ca0c6cb6932b0a187b0dc19
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:41:36 +0800

    add 36210.lua

---
+++ 36210.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 36210.lua
+--Gamename Monster Mash
+addappid(36210) --Mainappid Monster Mash
+addappid(36211, 1, "730f538f66764cbb811baa2108a95d8a1a06d127f943ac0cdcc84b1ef8131037") --Main Windows Depot Monster Mash
