e703daf0cf2b01c0ecf57cd48112f0b71a59f46a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:54:26 +0800

    add 42210.lua

---
+++ 42210.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42210.lua
+--Gamename Nancy Drew®: Secrets Can Kill REMASTERED
+addappid(42210) --Mainappid Nancy Drew®: Secrets Can Kill REMASTERED
+addappid(42211, 1, "8a51d659d9805b333d52177d935ff3852eb0122d261d27ca867e7fc1fec19f40") --Main Windows Depot Nancy Drew®: Secrets Can Kill REMASTERED
+setManifestid(42211, "275374762248783113", 579501136)
+addappid(42212, 1, "31a09da00558ea3dd283a5bf762c0b5af2e0b9f3cfdd5a149ef061dce9677fc5") --Main Macos Depot Nancy Drew®: Secrets Can Kill REMASTERED
+setManifestid(42212, "8105037185654449545", 0)
