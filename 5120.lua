7a6357fe436c720c829986f9011440202e825d7c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:15:20 +0800

    add 5120.lua

---
+++ 5120.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5120.lua
+--Gamename Fallout 3 Gameplay 2: Megaton
+addappid(5120) --Mainappid Fallout 3 Gameplay 2: Megaton
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Fallout 3 Gameplay 2: Megaton
+setManifestid(910, "1332084816209327708", 0)
