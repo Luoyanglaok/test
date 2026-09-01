541915a480031f2b2a946b4ffe111181e0a46395
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:37:19 +0800

    add 439.lua

---
+++ 439.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 439.lua
+--Gamename Steam Store
+addappid(439) --Mainappid Steam Store
+addappid(440, 1, "44ce5c5297a415a1a6f69c856037a5a2fdd82cd474fa659edfb4d59b2abc55fc") --Main Windows Depot Steam Store
