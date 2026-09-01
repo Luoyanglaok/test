e5b973ff8dba361a71c67a1e0239439fcf00d97c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:22:32 +0800

    add 5187.lua

---
+++ 5187.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5187.lua
+--Gamename Empire: Total War - Campaign
+addappid(5187) --Mainappid Empire: Total War - Campaign
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Empire: Total War - Campaign
+setManifestid(910, "1332084816209327708", 0)
