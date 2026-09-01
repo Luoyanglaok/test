5b11ff37ab492b12e98cc5ee1256dac565ee0924
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:21:29 +0800

    add 1945.lua

---
+++ 1945.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1945.lua
+--Gamename Steam Store
+addappid(1945, 1, "565ef4a78b1e5dfaca9b93ac9dc8291c4c4bce838b2e84ac9ebbbee13ebcac8e") --Mainappid Steam Store
+addappid(1946, 1, "5972504b42a1509acfa1bf9465d30425e76b7fae90e288c951f555a084ef13e3") --Main Windows Depot Steam Store
