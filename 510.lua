1bb684a13cbf89fc73550cac4f8120a1673422c2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:37:40 +0800

    add 510.lua

---
+++ 510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 510.lua
+--Gamename Left 4 Dead Dedicated Server
+addappid(510) --Mainappid Left 4 Dead Dedicated Server
+addappid(501, 1, "7a0b21eed084464fd2efb03ecc44116224b150f9377551ca7901ead966f21c26") --Main Windows Depot Left 4 Dead Dedicated Server
+addappid(502, 1, "54d51378fea25647cfe0c7d77804f29abd538c2606e4bf27b972d02816f34487") --Main Windows Depot Left 4 Dead Dedicated Server
