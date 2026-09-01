b65f711012e8fb5126f1041a73e0d8696cf7bdc8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:29:38 +0800

    add 3960.lua

---
+++ 3960.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3960.lua
+--Gamename Shattered Union
+addappid(3960) --Mainappid Shattered Union
+addappid(3961, 1, "6e6e028b1c743f6a81fffdf73b4565771aa4d3913169548d507ec6dcd61d5a1d") --Main Windows Depot Shattered Union
+setManifestid(3961, "8509874771188498790", 0)
