2352c18e82687ba59b964c22a41aecef841a1406
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:46:40 +0800

    add 38160.lua

---
+++ 38160.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38160.lua
+--Gamename Farm Frenzy 3: American Pie
+addappid(38160) --Mainappid Farm Frenzy 3: American Pie
+addappid(38161, 1, "9e76d3e073cbc2cd78cc86b4b50814261dfd44d5b1acbd54d4f7a83c51a92702") --Main Windows Depot Farm Frenzy 3: American Pie
+setManifestid(38161, "1823274278738598722", 0)
