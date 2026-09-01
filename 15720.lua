f02ac92755fcdcc3da0ca4cbaf4944e39a23e37d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:39:30 +0800

    add 15720.lua

---
+++ 15720.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15720.lua
+--Gamename Oddworld: Abe's Exoddus® Demo
+addappid(15720) --Mainappid Oddworld: Abe's Exoddus® Demo
+addappid(15721, 1, "9793cba508ba1c3dff91793a189c5f26aa35192061577604fca1f59ebab4c098") --Main Windows Depot Oddworld: Abe's Exoddus® Demo
+setManifestid(15721, "2820789973615578127", 0)
