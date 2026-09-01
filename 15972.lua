fb35f9e9b7fcac717194889f52eb6b0dfeac6f09
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:40:41 +0800

    add 15972.lua

---
+++ 15972.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15972.lua
+--Gamename Luxor Demo
+addappid(15972) --Mainappid Luxor Demo
+addappid(15971, 1, "25be5039f73086d9e940113040b2f794aafc16faf0544e26d40f0d40b7fceb14") --Main Windows Depot Luxor Demo
+addappid(15973, 1, "CCDF94FF89DD7B737CDCE24C8F88690B6F1D97F2664D5782091DF85D2375DB9A") --Main Windows Depot Luxor Demo
