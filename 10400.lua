4173c9c84127c32cbb346dbc5f2d1b03bf37e23d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:28:34 +0800

    add 10400.lua

---
+++ 10400.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10400.lua
+--Gamename Sega Rally
+addappid(10400) --Mainappid Sega Rally
+addappid(10401, 1, "ba48757ac8a82af51407a47d3eaa23d87167495e2b569ddbe6aca901b03d47c7") --Main Windows Depot Sega Rally
+setManifestid(10401, "7300392463568651378", 0)
+addappid(10402, 1, "ac4208f609b26e945d1b964a806f3cf1355c1b88ca2825ed8a65b010863485b6") --Main Windows Depot Sega Rally
+setManifestid(10402, "8102568656524144008", 0)
+addappid(10403, 1, "70ebf1b172a1348793dec061d7deaff6c35dccbc2541442d6737e4470bf39e73") --Main Windows Depot Sega Rally
+setManifestid(10403, "2042689554454415164", 0)
