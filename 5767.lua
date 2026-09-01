00bec2d6cbdecfd7a45649d4b282ac4e0b03e884
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:57:24 +0800

    add 5767.lua

---
+++ 5767.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5767.lua
+--Gamename KL2 She Better Be Worth It (Spanish)
+addappid(5767) --Mainappid KL2 She Better Be Worth It (Spanish)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot KL2 She Better Be Worth It (Spanish)
+setManifestid(910, "1332084816209327708", 0)
