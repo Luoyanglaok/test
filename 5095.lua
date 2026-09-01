48da71fcde9980ef5b59423a3476e2a2f88b4fa1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:14:04 +0800

    add 5095.lua

---
+++ 5095.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5095.lua
+--Gamename STCC: The Game Teaser
+addappid(5095) --Mainappid STCC: The Game Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot STCC: The Game Teaser
+setManifestid(910, "1332084816209327708", 0)
