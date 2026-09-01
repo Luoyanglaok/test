e3cb3c5c73f9f1558c2c266b3a5f02d296fd7364
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:55:44 +0800

    add 21730.lua

---
+++ 21730.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21730.lua
+--Gamename CSI: Crime Scene Investigation™: Hard Evidence
+addappid(21730) --Mainappid CSI: Crime Scene Investigation™: Hard Evidence
+addappid(21731, 1, "f92a2fea1f7cee29010e0bc93d5e5a7d6828022d4d0d1060ef8e14f559245e7e") --Main Windows Depot CSI: Crime Scene Investigation™: Hard Evidence
+setManifestid(21731, "4361698035985189276", 0)
+addappid(21732, 1, "1a1375ad187afec0081133fe7c8543c013929848692e35e83f3d01556cfe545c") --Main Windows Language Depot Spanish CSI: Crime Scene Investigation™: Hard Evidence
+setManifestid(21732, "2746906375090051442", 0)
+addappid(21733, 1, "2b8aac5e7f5dfdea348572b3f294cff341f13c148809f5a117a3ae5e84534508") --Main Windows Language Depot German CSI: Crime Scene Investigation™: Hard Evidence
+setManifestid(21733, "6590608662069669962", 0)
+addappid(21734, 1, "62c96b65d190dcb6d5fbdad74a0e8081a39b4fe3da9529d0f83a61b32551fa65") --Main Windows Language Depot French CSI: Crime Scene Investigation™: Hard Evidence
+setManifestid(21734, "2529267561711879695", 0)
+addappid(21735, 1, "5703c68c1dbf8aafdc4f3418ba3e9bbf1a9a7ba6857cb918432ab559004383db") --Main Windows Language Depot Italian CSI: Crime Scene Investigation™: Hard Evidence
+setManifestid(21735, "1835759737226319938", 0)
