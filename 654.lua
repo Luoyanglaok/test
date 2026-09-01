ec2d0817a0037ccd90a36b667dc689e0c4240639
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:47:36 +0800

    add 654.lua

--- 654.lua
+++ 654.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 654.lua
-addappid(654, 0, "1da4390487b9a94877bb2ec856dbc8a57934b4a84117ebe85425c039524cfe5a")
+addappid(654, 0, "9b2c2146b653f405bab693659a4ac652ee0c632ebd3b24fa1d65f7580f341bb5")
