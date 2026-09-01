5867bf11ea1c170bc06cafca36aaf8a55f074ab6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:22:13 +0800

    add 5185.lua

---
+++ 5185.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5185.lua
+--Gamename Empire: Total War - Naval Battles
+addappid(5185) --Mainappid Empire: Total War - Naval Battles
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Empire: Total War - Naval Battles
+setManifestid(910, "1332084816209327708", 0)
