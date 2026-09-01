370a708e2c003793ee988f8faa0010afe2649a2c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:24:21 +0800

    add 5231.lua

---
+++ 5231.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5231.lua
+--Gamename F.E.A.R 2: Project Origin - Toy Soldiers
+addappid(5231) --Mainappid F.E.A.R 2: Project Origin - Toy Soldiers
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot F.E.A.R 2: Project Origin - Toy Soldiers
+setManifestid(910, "1332084816209327708", 0)
