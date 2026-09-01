465fd6634c9ee800ca263192f59fbe12164da605
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:46:04 +0800

    add 38090.lua

---
+++ 38090.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38090.lua
+--Gamename Build-A-Lot 2: Town of the Year
+addappid(38090) --Mainappid Build-A-Lot 2: Town of the Year
+addappid(38091, 1, "da2f2ac022477747ffd3fcc858aefbb6f0e826a953c6b78448cbfc39336f8404") --Main Windows Depot Build-A-Lot 2: Town of the Year
+setManifestid(38091, "2426261417135859544", 0)
