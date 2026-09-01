781db78280ad113dad4b782d7923b654c960dafa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:29:18 +0800

    add 3840.lua

---
+++ 3840.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3840.lua
+--Gamename Psychonauts Demo
+addappid(3840, 1, "17170ae4984b940300dd09fa40dc96ad9e433e29b4246aaba81dce9b78a1bb29") --Mainappid Psychonauts Demo
+addappid(3841, 1, "f8d108236831e27556b2c9f97a9962c24e646ffc646dfd82959974a2dedb634e") --Main Windows Depot Psychonauts Demo
+setManifestid(3841, "7218750677087905256", 0)
