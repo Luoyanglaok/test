ddbd437e53c051638a8e781dcbfa80f2f92b73c7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:21:38 +0800

    add 5181.lua

---
+++ 5181.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5181.lua
+--Gamename Everquest II: The Shadow Odyssey Trailer
+addappid(5181) --Mainappid Everquest II: The Shadow Odyssey Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Everquest II: The Shadow Odyssey Trailer
+setManifestid(910, "1332084816209327708", 0)
