cf578bf38423f3901658b2434ded6e1af533d92d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:08:38 +0800

    add 5891.lua

---
+++ 5891.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5891.lua
+--Gamename ArcaniA Trailer2
+addappid(5891) --Mainappid ArcaniA Trailer2
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot ArcaniA Trailer2
+setManifestid(910, "1332084816209327708", 0)
