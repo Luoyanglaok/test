7b6f732c08c1679d8d015df0e6d071775becf461
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:44:40 +0800

    add 37700.lua

---
+++ 37700.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37700.lua
+--Gamename Darkest of Days
+addappid(37700) --Mainappid Darkest of Days
+addappid(37701, 1, "51f6419505a29a14a686df4acd34e9f0e5faf1c6c1d3878e27f2833ff4017877") --Main Windows Depot Darkest of Days
