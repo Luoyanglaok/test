5edc572ad6543c82146381afcafea5016afe1840
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:52:25 +0800

    add 41680.lua

---
+++ 41680.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 41680.lua
+--Gamename Death and the Fly
+addappid(41680) --Mainappid Death and the Fly
+addappid(41681, 1, "8aac58b1a85bacf2e51a631bcbcb4a63598bac7ab47494f0d6b0583a6a3db131") --Main Windows Depot Death and the Fly
+setManifestid(41681, "344827768334267264", 0)
