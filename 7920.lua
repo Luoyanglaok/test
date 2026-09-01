5ce5e27e9806ba0d0d9a71db6d92cd9026e7530b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:20:55 +0800

    add 7920.lua

---
+++ 7920.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7920.lua
+--Gamename The Movies Demo
+addappid(7920) --Mainappid The Movies Demo
+addappid(7921, 1, "5db80543e89e3fc0367c5592f0df4ee535d26b944b4ed7b6a122a681f92bfc50") --Main Windows Depot The Movies Demo
+setManifestid(7921, "4460664102125786758", 0)
