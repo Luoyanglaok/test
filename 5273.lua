500cb2f148e923afbae949dfd57d32d21231f563
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:27:06 +0800

    add 5273.lua

---
+++ 5273.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5273.lua
+--Gamename Splinter Cell Conviction E3 Gameplay
+addappid(5273) --Mainappid Splinter Cell Conviction E3 Gameplay
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Splinter Cell Conviction E3 Gameplay
+setManifestid(910, "1332084816209327708", 0)
