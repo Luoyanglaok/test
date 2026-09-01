65a6480502e80ca266574e626df0c342c0cd51e5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:43:45 +0800

    add 37330.lua

---
+++ 37330.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37330.lua
+--Gamename Nightshift Legacy: The Jaguar's Eye™
+addappid(37330) --Mainappid Nightshift Legacy: The Jaguar's Eye™
+addappid(37331, 1, "a73afa6ef1ddc24a509fe33645091dc9014776d0235576fc09979a475e376543") --Main Windows Depot Nightshift Legacy: The Jaguar's Eye™
+setManifestid(37331, "1090206699779235943", 0)
