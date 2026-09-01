0a5c6c00209cf6b7e37e81457a5b6a7ad8a7a8a2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:34:31 +0800

    add 12450.lua

---
+++ 12450.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12450.lua
+--Gamename Ankh 3: Battle of the Gods
+addappid(12450) --Mainappid Ankh 3: Battle of the Gods
+addappid(12451, 1, "7d877a21857dc80c95087a017c34762930f9888c4c83b45b0ca0951dbc6e37d9") --Main Windows Depot Ankh 3: Battle of the Gods
+setManifestid(12451, "5998957619956579574", 0)
