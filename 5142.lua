9aaa155b80069ef9fb80f0b6633ca421dee04395
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:19:17 +0800

    add 5142.lua

---
+++ 5142.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5142.lua
+--Gamename Left 4 Dead Intro (French)
+addappid(5142) --Mainappid Left 4 Dead Intro (French)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Left 4 Dead Intro (French)
+setManifestid(910, "1332084816209327708", 0)
