39c142b920092dd710519558d7fe58aff5bc1384
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:22:38 +0800

    add 8610.lua

---
+++ 8610.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8610.lua
+--Gamename RACE 07 Dedicated Server
+addappid(8610) --Mainappid RACE 07 Dedicated Server
+addappid(1004, 1, "f21faa4f29ecee9d192fc1784d1701376eb87e13521824ceaf0116b10c558a81") --Main Windows Depot RACE 07 Dedicated Server
+addappid(8601, 1, "fd988e7496d1ad29e9c6184247db971604fb1182d9d2f4a63f8cbe9bb771cd52") --Main Windows Depot RACE 07 Dedicated Server
+addappid(8602, 1, "258bbc1baa7e13399d9f1714e14dc28fc56e7afe8105e799b8ffd2d86955a047") --Main Windows Depot RACE 07 Dedicated Server
+addappid(8611, 1, "3a6565659098ee2e07f010abcd0b11526ce229aa1fe293ebded6543255c8b02d") --Main Windows Depot RACE 07 Dedicated Server
