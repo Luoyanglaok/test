5b9d4c69ed4ce5dfe7a4ccf8f8ce191e7fb0aab9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:26:38 +0800

    add 34281.lua

---
+++ 34281.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34281.lua
+--Gamename Altered Beast™
+addappid(34281, 1, "ea5067e3e53af403d91543f3dc8e82553a1f2b79cf194bec9f82e972f28af0eb") --Mainappid Altered Beast™
+addappid(34282, 1, "ab900a58c24adca08b5bce11185e973585cc8b79036622e43e15ec0af5b49208") --Main Windows Depot Altered Beast™
