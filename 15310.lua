698d29a53ef75df87a102abea206a18a7fef92d8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:38:03 +0800

    add 15310.lua

---
+++ 15310.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15310.lua
+--Gamename The Settlers: Heritage of Kings
+addappid(15310) --Mainappid The Settlers: Heritage of Kings
+addappid(15311, 1, "350bc3522d628cb83506f67b220716f191f5fc1c9d87a6b9d95212ea20a0da76") --Main Windows Depot The Settlers: Heritage of Kings
+setManifestid(15311, "219058603185412304", 0)
