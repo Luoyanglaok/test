2c202610b00deb2d13ea10f8e2c16c2f3f9d60bc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:28:24 +0800

    add 3602.lua

---
+++ 3602.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3602.lua
+--Gamename Escape Rosecliff Island Demo
+addappid(3602) --Mainappid Escape Rosecliff Island Demo
+addappid(3601, 1, "43ef6f91d4901a73cd4a34c1cd367f8a528adecc1f5dc3a9edb0221b0cd99dfb") --Main Windows Depot Escape Rosecliff Island Demo
+addappid(3603, 1, "cca8929dcb8313330603c5556d9cf1a6edf0899479ef7f34dc947943af499671") --Main Windows Depot Escape Rosecliff Island Demo
