c1261b19b3794315fe4522e995d3e5fa25159bb7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:47:53 +0800

    add 38530.lua

---
+++ 38530.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38530.lua
+--Gamename Top Gun
+addappid(38530) --Mainappid Top Gun
+addappid(38531, 1, "c06a9730729115bbc0823ae318be815816601e550a88f3f75d0599aa6d894c44") --Main Windows Depot Top Gun
+setManifestid(38531, "3242523932695138817", 0)
