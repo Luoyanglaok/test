b9bcac1bef3d63777473602dd371384b8426c4bb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:21:52 +0800

    add 8090.lua

---
+++ 8090.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8090.lua
+--Gamename Conflict: Denied Ops Demo
+addappid(8090) --Mainappid Conflict: Denied Ops Demo
+addappid(8091, 1, "ab475fdc50385e6a48e4c8d423232338cec4a1c1dfbfbd3a9f725ddb31e3966d") --Main Windows Depot Conflict: Denied Ops Demo
+setManifestid(8091, "4259624416059069872", 0)
