8a5af4f39db7d58ac4190de1b11929b5ecc79b9c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:45:19 +0800

    add 950.lua

---
+++ 950.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 950.lua
+--Gamename Hitman: Blood Money Trailer
+addappid(950) --Mainappid Hitman: Blood Money Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Hitman: Blood Money Trailer
+setManifestid(910, "1332084816209327708", 0)
