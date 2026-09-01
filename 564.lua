fb4edaf9f5508b249970e20031092c5403b54130
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:37:59 +0800

    add 564.lua

---
+++ 564.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 564.lua
+--Gamename Left 4 Dead 2 Add-on Support
+addappid(564) --Mainappid Left 4 Dead 2 Add-on Support
+addappid(551, 1, "133d1039d438364b5569e91221c10fcd640f141d7700c921c02721ebe87c0459") --Main Windows Depot Left 4 Dead 2 Add-on Support
+setManifestid(551, "314878501352141828", 5228951552)
+addappid(552, 1, "2585a22b53c3e040982897d140378d942a22994ffae3ae2085eddf36af94f84d") --Main Windows Depot Left 4 Dead 2 Add-on Support
+setManifestid(552, "8555994540167139077", 3308506288)
