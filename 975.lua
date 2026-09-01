03fe20975c09f074410456f82bcac82ab5c4aea4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:48:00 +0800

    add 975.lua

---
+++ 975.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 975.lua
+--Gamename Kane & Lynch Trailer 2
+addappid(975) --Mainappid Kane & Lynch Trailer 2
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Kane & Lynch Trailer 2
+setManifestid(910, "1332084816209327708", 0)
