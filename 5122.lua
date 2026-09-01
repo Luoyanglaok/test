684a7ef4a10f3e79ece0aec16f1dba209057d63e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:15:41 +0800

    add 5122.lua

---
+++ 5122.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5122.lua
+--Gamename Fallout 3 Gameplay 4: Super-Duper Mart
+addappid(5122) --Mainappid Fallout 3 Gameplay 4: Super-Duper Mart
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Fallout 3 Gameplay 4: Super-Duper Mart
+setManifestid(910, "1332084816209327708", 0)
