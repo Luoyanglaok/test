c349224a13933083d6669e63e02757eeecb017d7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:46:52 +0800

    add 17481.lua

---
+++ 17481.lua
@@ -0,0 +1,31 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 17481.lua
+--Gamename Steam Store
+addappid(17481, 1, "b13a658a73dc214dd4c12fd62a81428ab5e925cc66549b1cc224dd0a393c9ae8") --Mainappid Steam Store
+setManifestid(17481, "7399110944044859959", 7152793632)
+addappid(17482, 1, "4ab4dc0bbb1fc053e3e48fa3d1d4e48806771853f5cd43631d3da56cc0c0bf47") --Main Windows Language Depot Spanish Steam Store
+setManifestid(17482, "5337273780141890863", 1887701456)
+--Dlcname Command & Conquer™ Red Alert™ 3
+addappid(17480, 1, "de8be15c66d36a88392a76c9067536d470d61a61d464edbb0cc51860e7a5d6a9") --Dlcname Command & Conquer™ Red Alert™ 3
+addappid(17483, 1, "e285f53920127758596e725c4df1c09686e7681e4661117ebce4dfd9afccc7c0") --Dlc Windows Language Depot Koreana Command & Conquer™ Red Alert™ 3
+setManifestid(17483, "7382175596500191863", 6230464)
+addappid(17484, 1, "cf2b01f2a74ae4e5e8f711580a5ed6d19935d5b7214fbb9c891d9f758c19f563") --Dlc Windows Language Depot Czech Command & Conquer™ Red Alert™ 3
+setManifestid(17484, "8224907084858076022", 146483168)
+addappid(17485, 1, "de1605528ba55a7b330b589a250717f5b9dfcba21cb50afdb7c763c729c756f2") --Dlc Windows Language Depot French Command & Conquer™ Red Alert™ 3
+setManifestid(17485, "577067934581235304", 1880607504)
+addappid(17486, 1, "cd91de02d5457477a2c9254d4ebfa0c1b8853576b15c309bc92c79d7dc50f14c") --Dlc Windows Language Depot German Command & Conquer™ Red Alert™ 3
+setManifestid(17486, "1881178721620356443", 1893126544)
+addappid(17487, 1, "46311b08e8fef5e899ea043424faa884a9f8ffb7d8986ce457774baf12773b82") --Dlc Windows Language Depot Hungarian Command & Conquer™ Red Alert™ 3
+setManifestid(17487, "263977213759137039", 146377120)
+addappid(17488, 1, "bdd4807f56d6525ddeba3570d44a23d511d80f880ec912db1ddacfb2895f3826") --Dlc Windows Language Depot Italian Command & Conquer™ Red Alert™ 3
+setManifestid(17488, "8442844672739422442", 1838921840)
+addappid(17489, 1, "f8c00b3c11213cc7e74f83bde645ba16e774a87c16f52d365fb9b60883eb7f3a") --Dlc Windows Language Depot Tchinese Command & Conquer™ Red Alert™ 3
+setManifestid(17489, "544620771775941168", 12004832)
+addappid(17490, 1, "af3865859ef3abda7e34c8c859bf5bbf3e3eda22b20bdca9cf2412826633d230") --Dlc Windows Language Depot Polish Command & Conquer™ Red Alert™ 3
+setManifestid(17490, "6887882775092064817", 146450688)
+addappid(17491, 1, "30b51adab9bd5b4367a3afc73392db52a8d6912b7ce21e3004462507612a88cd") --Dlc Windows Language Depot Russian Command & Conquer™ Red Alert™ 3
+setManifestid(17491, "5501621558289431925", 1916835520)
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Dlc Windows Depot Command & Conquer™ Red Alert™ 3
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Dlc Windows Depot Command & Conquer™ Red Alert™ 3
+addappid(3314380, 1, "130ae012b6a41a1ef16c03e0ba1e14ad814fe83b5ebccca5150e122ac50a18cf") --Dlc Windows Language Depot Thai Command & Conquer™ Red Alert™ 3
+setManifestid(3314380, "2812775176019743365", 5942592)
