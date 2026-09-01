1e067c33360c6704386552511642b09919f4bf8c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:19:29 +0800

    add 32690.lua

---
+++ 32690.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 32690.lua
+--Gamename Joint Operations: Typhoon Rising
+addappid(32690) --Mainappid Joint Operations: Typhoon Rising
+addappid(32691, 1, "44184de2bf92156526940373a4eb17c20440ddb3acd9d0880718b22b484233e9") --Main Windows Depot Joint Operations: Typhoon Rising
+setManifestid(32691, "3328923870620639996", 0)
