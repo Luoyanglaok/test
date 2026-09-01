d459412de9e0b27124f10ea1b093e958a0db1afc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:47:09 +0800

    add 38180.lua

---
+++ 38180.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38180.lua
+--Gamename Cradle of Persia
+addappid(38180) --Mainappid Cradle of Persia
+addappid(38181, 1, "afa5ebf66bd0c0fc86e2e88e0839750349a30b20591833c1c7ff1c3dff767cc2") --Main Windows Depot Cradle of Persia
+setManifestid(38181, "194671197935550365", 0)
