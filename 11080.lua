0a803a575bb9dd4ede9960de7400f21050607353
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:31:17 +0800

    add 11080.lua

---
+++ 11080.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 11080.lua
+--Gamename Sherlock Holmes: Nemesis Demo
+addappid(11080) --Mainappid Sherlock Holmes: Nemesis Demo
+addappid(11081, 1, "abd9cf45ffe41cc55cd476737b39b67033ff31a45da6609d8b04138617f8827f") --Main Windows Depot Sherlock Holmes: Nemesis Demo
+setManifestid(11081, "7745325600681924083", 0)
