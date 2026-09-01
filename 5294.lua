413416ad916140e8adbbc6a4b30943444856bed8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:29:08 +0800

    add 5294.lua

---
+++ 5294.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5294.lua
+--Gamename ARMA 2 In Game Footage
+addappid(5294) --Mainappid ARMA 2 In Game Footage
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot ARMA 2 In Game Footage
+setManifestid(910, "1332084816209327708", 0)
