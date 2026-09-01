046de694328ca350a9be20172ea5e0691ab3444b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:53:11 +0800

    add 20560.lua

---
+++ 20560.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 20560.lua
+--Gamename World of Zoo: Animal Creator Demo
+addappid(20560) --Mainappid World of Zoo: Animal Creator Demo
+addappid(20561, 1, "7f89956d112174d3479a7832cc7d76af820b4954f12a0d650ea23737b0cc5eb8") --Main Windows Depot World of Zoo: Animal Creator Demo
+setManifestid(20561, "448784580534994457", 0)
