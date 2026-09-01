877f1d2faa0a0611b3c55123c1057fd7a5d90e53
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:55:59 +0800

    add 21760.lua

---
+++ 21760.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21760.lua
+--Gamename World In Conflict
+addappid(21760) --Mainappid World In Conflict
+addappid(21761, 1, "995d285d258f9453b7da5558d233b3a9ace6d3251ed67f8dc5e495dd2b8ac48b") --Main Windows Depot World In Conflict
+setManifestid(21761, "4303884799797378015", 0)
+addappid(21762, 1, "e029270844895ff6aa7b116348b7f5cdf37e3a9501c53709f919535ecbb7b2c6") --Main Windows Language Depot French World In Conflict
+setManifestid(21762, "7267823916012351291", 0)
+addappid(21763, 1, "b502644ae35dd23de247e572b20e9d1ab0cf83f9030ec3b81966fa9761e149b6") --Main Windows Language Depot Spanish World In Conflict
+setManifestid(21763, "5114686484471953843", 0)
+addappid(21764, 1, "4f37680b1d79d55cec6415aba531ac6e2ef1e90cf1242dc4e769dee9b175a702") --Main Windows Language Depot German World In Conflict
+setManifestid(21764, "2470265608918171935", 0)
+addappid(21765, 1, "e98f7e9443a800009a5a92faf38c3868652ec4b4bd112566b1322fa151998e41") --Main Windows Language Depot Italian World In Conflict
+setManifestid(21765, "1822286209696908418", 0)
+addappid(21911, 1, "22aecea17c17d5ce9e58a71f15c59158df7216fc625ada08c9c12cb81e1e52b5") --Main Windows Depot World In Conflict
