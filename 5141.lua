b79b46e1917e204f86a3fcefd5452aa05551a9bd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:19:07 +0800

    add 5141.lua

---
+++ 5141.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5141.lua
+--Gamename Left 4 Dead Intro
+addappid(5141) --Mainappid Left 4 Dead Intro
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Left 4 Dead Intro
+setManifestid(910, "1332084816209327708", 0)
