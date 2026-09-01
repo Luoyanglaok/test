09cac158d48d21d664bb111877d74614cc39e8a2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:53:14 +0800

    add 5746.lua

---
+++ 5746.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5746.lua
+--Gamename KL2 Arcade Mode (German)
+addappid(5746) --Mainappid KL2 Arcade Mode (German)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot KL2 Arcade Mode (German)
+setManifestid(910, "1332084816209327708", 0)
