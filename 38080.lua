1aebd28a9ab45352d5e0cb256ec1839df21758e4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:45:53 +0800

    add 38080.lua

---
+++ 38080.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38080.lua
+--Gamename Build-A-Lot
+addappid(38080) --Mainappid Build-A-Lot
+addappid(38081, 1, "24f2aa09b6046dd5698fa1a5c04ee25f9dae57458c10e17e5304ada7e28d149c") --Main Windows Depot Build-A-Lot
+setManifestid(38081, "7424581584885332169", 0)
