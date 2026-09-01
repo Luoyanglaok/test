b10d18d60f167f9409053678cce0fc3158e8a6de
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:15:01 +0800

    add 5118.lua

---
+++ 5118.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5118.lua
+--Gamename Fallout 3 Teaser
+addappid(5118) --Mainappid Fallout 3 Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Fallout 3 Teaser
+setManifestid(910, "1332084816209327708", 0)
