657d6a0e024bf74f1e95375052193f4aef3c701d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:22:59 +0800

    add 8650.lua

---
+++ 8650.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8650.lua
+--Gamename RACE 07: Andy Priaulx Crowne Plaza Raceway
+addappid(8650) --Mainappid RACE 07: Andy Priaulx Crowne Plaza Raceway
+addappid(1004, 1, "f21faa4f29ecee9d192fc1784d1701376eb87e13521824ceaf0116b10c558a81") --Main Windows Depot RACE 07: Andy Priaulx Crowne Plaza Raceway
+addappid(8604, 1, "47e811dd68d4dc79ee87939d0acd6db567ddc6fad31bd7717d48bb76490b4200") --Main Windows Depot RACE 07: Andy Priaulx Crowne Plaza Raceway
+setManifestid(8604, "7963520296699615963", 0)
+addappid(8619, 1, "e2a0fe22927b05d5d4a6b3720432e3e7826f976481b37e963c9d6da5268306a5") --Main Windows Depot RACE 07: Andy Priaulx Crowne Plaza Raceway
