25194b66ae09aac7393c5a154edf8bce04f03d36
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:27:27 +0800

    add 5276.lua

---
+++ 5276.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5276.lua
+--Gamename America's Army 3 Authenticy
+addappid(5276) --Mainappid America's Army 3 Authenticy
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot America's Army 3 Authenticy
+setManifestid(910, "1332084816209327708", 0)
