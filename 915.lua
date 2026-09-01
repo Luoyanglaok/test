05260d9262f820efa1b28b8d3a23dffd0ee026f1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:41:31 +0800

    add 915.lua

---
+++ 915.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 915.lua
+--Gamename Half-Life 2: Episode One Launch Teaser 4
+addappid(915) --Mainappid Half-Life 2: Episode One Launch Teaser 4
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Half-Life 2: Episode One Launch Teaser 4
+setManifestid(910, "1332084816209327708", 0)
