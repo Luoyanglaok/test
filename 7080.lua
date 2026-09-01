3d448c1542ea91179ae3e5cc8ce76c40747740f0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:17:44 +0800

    add 7080.lua

---
+++ 7080.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7080.lua
+--Gamename Infernal Demo
+addappid(7080) --Mainappid Infernal Demo
+addappid(7081, 1, "73c733c8487c996b528bcfca8f8c62d80416772b3163403682f00dc32d48c3af") --Main Windows Depot Infernal Demo
+setManifestid(7081, "6069593399396453176", 0)
