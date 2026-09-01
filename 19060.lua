bc7378ba7d3e509e884dba4ae67f819f43144e7a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:51:10 +0800

    add 19060.lua

---
+++ 19060.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19060.lua
+--Gamename Rocket Knight Demo
+addappid(19060) --Mainappid Rocket Knight Demo
+addappid(19061, 1, "8382bbbd0f5c75dc50fb53afd4d40c3841bbf371ccaeb679db852eab6c42bac4") --Main Windows Depot Rocket Knight Demo
+setManifestid(19061, "8925751776045607058", 0)
