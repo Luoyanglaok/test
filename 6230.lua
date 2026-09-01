eb2f27618a19c29372f5724287a6944e75c9bc4d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:15:11 +0800

    add 6230.lua

---
+++ 6230.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6230.lua
+--Gamename FlatOut Demo
+addappid(6230) --Mainappid FlatOut Demo
+addappid(6231, 1, "86f78526717062be16ccdaf7f5ccda4d36209e301107e4815e402c528fa62403") --Main Windows Depot FlatOut Demo
+setManifestid(6231, "8749496458000798934", 0)
