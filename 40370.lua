a9b38a4b2a32f2dae6d74341eb69a9331ae23191
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:49:55 +0800

    add 40370.lua

---
+++ 40370.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 40370.lua
+--Gamename Emergency 2012
+addappid(40370) --Mainappid Emergency 2012
+addappid(40371, 1, "969843a47e0ea315b8c693668b970e0ea6f14543c539997679d48e3f008a58d9") --Main Windows Depot Emergency 2012
+setManifestid(40371, "7584138299981198673", 4488693344)
+addappid(40372, 1, "d677fb1af73f3922e9d6aed7e20574e77daea28354eb5f8744ff69f0b86f5a29") --Main Windows Language Depot German Emergency 2012
+setManifestid(40372, "9077971555828314690", 0)
+addappid(40373, 1, "9d0062d3f1aec4221074611ca177b2a75dcf3ff641dda6f45e402f6274af4e1b") --Main Windows Language Depot Spanish Emergency 2012
+setManifestid(40373, "1422388555178482539", 0)
+addappid(40374, 1, "b0173ba7a88ba2347605f0ff774af23788186e61625b6b2b1c7237c37f9d25be") --Main Windows Language Depot French Emergency 2012
+setManifestid(40374, "2985667783807942830", 0)
+addappid(40375, 1, "46af1fccd60e97d80fa50ba67fc86d9b0a039c0c1e68f4b9a67fe86c397eec01") --Main Windows Language Depot English Emergency 2012
+setManifestid(40375, "701502744990122654", 0)
