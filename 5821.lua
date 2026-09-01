f3bdd6ab982d8bda1bd87ecbfe055c60a3acc800
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:02:39 +0800

    add 5821.lua

---
+++ 5821.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5821.lua
+--Gamename Medal of Honor - T6E3SP
+addappid(5821) --Mainappid Medal of Honor - T6E3SP
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Medal of Honor - T6E3SP
+setManifestid(910, "1332084816209327708", 0)
