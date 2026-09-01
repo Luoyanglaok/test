f70abbb7b8394788742798fd785285cb055a4da6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:20:45 +0800

    add 1523.lua

---
+++ 1523.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1523.lua
+--Gamename DEFCON Beta Demo
+addappid(1523, 1, "251fc988abdc830d11c03c40efa19601b729e6b333463cc21d025d1d1c1a48a4") --Mainappid DEFCON Beta Demo
