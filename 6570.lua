8ea8df968a2f35dee0a3bf1608cb75e7d7840c30
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:16:54 +0800

    add 6570.lua

---
+++ 6570.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6570.lua
+--Gamename Onimusha 3: Demon Siege
+addappid(6570) --Mainappid Onimusha 3: Demon Siege
+addappid(6571, 1, "2aa9802aaaa6fdf1d609c852bd2e6f4bf83e8b273210ebb415e52c593fe2cb40") --Main Windows Depot Onimusha 3: Demon Siege
+setManifestid(6571, "8283916300943883690", 0)
