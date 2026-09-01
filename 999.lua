e24eaa5adba531cc6689377fb56b4a80235138e0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:51:16 +0800

    add 999.lua

---
+++ 999.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 999.lua
+--Gamename Painkiller Overdose Trailer
+addappid(999) --Mainappid Painkiller Overdose Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Painkiller Overdose Trailer
+setManifestid(910, "1332084816209327708", 0)
+addappid(1000, 1, "2493380d1b7607e54439bfea560a5384b2f7e7a8b8cf9934fd1abd50286982c1") --Main Windows Depot Painkiller Overdose Trailer
