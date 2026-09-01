2f677a2768e62029a91561eae9d410739c6724a0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:35:20 +0800

    add 5362.lua

---
+++ 5362.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5362.lua
+--Gamename Mini Ninjas - Suzume Trailer (EU)
+addappid(5362) --Mainappid Mini Ninjas - Suzume Trailer (EU)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Mini Ninjas - Suzume Trailer (EU)
+setManifestid(910, "1332084816209327708", 0)
