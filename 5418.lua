5f97c9d277e28424b734249931a43abb1c1ffdad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:37:54 +0800

    add 5418.lua

---
+++ 5418.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5418.lua
+--Gamename NBA 2K10
+addappid(5418) --Mainappid NBA 2K10
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot NBA 2K10
+setManifestid(910, "1332084816209327708", 0)
