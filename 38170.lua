f8b1d1aae19fed0a4f2812b48b181b24883ba286
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:46:54 +0800

    add 38170.lua

---
+++ 38170.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38170.lua
+--Gamename Cradle of Rome
+addappid(38170) --Mainappid Cradle of Rome
+addappid(38171, 1, "48c0f95a97667d6599db1744d214085f6885e41347a142a5e26952a94ac4b5da") --Main Windows Depot Cradle of Rome
+setManifestid(38171, "2493856805305512526", 0)
