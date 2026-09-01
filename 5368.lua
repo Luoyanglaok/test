8b4f2f725c3e5f28ffa072f4d92ae8aaca5cd027
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:35:51 +0800

    add 5368.lua

---
+++ 5368.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5368.lua
+--Gamename Dawn of War 2 - Last Stand
+addappid(5368) --Mainappid Dawn of War 2 - Last Stand
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Dawn of War 2 - Last Stand
+setManifestid(910, "1332084816209327708", 0)
