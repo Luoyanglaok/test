4078009889ecf52e2aee80d4eee9084b359c955c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:19:02 +0800

    add 7401.lua

---
+++ 7401.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7401.lua
+--Gamename Steam Store
+addappid(7401, 1, "c3396a088e89a01f5b3cdab64e2b4086e69e535fc651cf3aac458200c0cff0fc") --Mainappid Steam Store
