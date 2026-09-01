38686fe07486b221a2426fad1a4584580e026069
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:00:28 +0800

    add 23500.lua

---
+++ 23500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 23500.lua
+--Gamename Puzzle Quest: Galactrix
+addappid(23500) --Mainappid Puzzle Quest: Galactrix
+addappid(23501, 1, "a07a0ebfea4e3bba943b75f38ed68b4846f351c7ecfe146fd602695ab3f02896") --Main Windows Depot Puzzle Quest: Galactrix
+setManifestid(23501, "7038657996657044759", 0)
