e0f7fe950ff2fc83be5fe2a052b6132329d03dce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:15:10 +0800

    add 5119.lua

---
+++ 5119.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5119.lua
+--Gamename Fallout 3 Gameplay 1: Escape
+addappid(5119) --Mainappid Fallout 3 Gameplay 1: Escape
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Fallout 3 Gameplay 1: Escape
+setManifestid(910, "1332084816209327708", 0)
