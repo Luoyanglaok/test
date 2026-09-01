7215299e739f9150cb215d9cefde1f5a47f3c0d2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:17:03 +0800

    add 6590.lua

---
+++ 6590.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6590.lua
+--Gamename Lost Planet: Extreme Condition DirectX10 Trial
+addappid(6590) --Mainappid Lost Planet: Extreme Condition DirectX10 Trial
+addappid(6591, 1, "9d491b4fa520ee1775e49e70e7bf1994817d8a20372ed3fa2bab1a8affa0a341") --Main Windows Depot Lost Planet: Extreme Condition DirectX10 Trial
+setManifestid(6591, "554158937993613009", 0)
