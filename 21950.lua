d2b092493a358d5269f013720ffe53c46d318664
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:56:32 +0800

    add 21950.lua

---
+++ 21950.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21950.lua
+--Gamename Grey's Anatomy: The Video Game
+addappid(21950) --Mainappid Grey's Anatomy: The Video Game
+addappid(21951, 1, "e3d768088c4445a50c3ca126e86f5490f201c54289b614a10b82143a0e4f3b84") --Main Windows Depot Grey's Anatomy: The Video Game
+setManifestid(21951, "6817826879496606897", 0)
