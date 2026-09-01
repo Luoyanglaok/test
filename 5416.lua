206b5f0674e67de0807aa3c6c11ae63a7da15dab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:37:41 +0800

    add 5416.lua

---
+++ 5416.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5416.lua
+--Gamename MOHA Trailer
+addappid(5416) --Mainappid MOHA Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot MOHA Trailer
+setManifestid(910, "1332084816209327708", 0)
