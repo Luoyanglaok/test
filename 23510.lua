f7e1b931f78249f413ee11b4d004a0d5709a5312
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:00:36 +0800

    add 23510.lua

---
+++ 23510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 23510.lua
+--Gamename Texas Cheat'Em
+addappid(23510) --Mainappid Texas Cheat'Em
+addappid(23511, 1, "2afde845517029ce2af39f0ec787211a8f7b29f6a3278d7adc9eee48f7250e84") --Main Windows Depot Texas Cheat'Em
+setManifestid(23511, "7203551104132285324", 0)
