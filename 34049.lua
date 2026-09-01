149cb95d6694b4eb47c03097e36beb8064a2eed3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:23:54 +0800

    add 34049.lua

---
+++ 34049.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34049.lua
+--Gamename Napoleon: Total War
+addappid(34049, 1, "69a3676f1206f554c2942ab829d441a08930c6a57b8ef14144bf22e86e2e5275") --Mainappid Napoleon: Total War
