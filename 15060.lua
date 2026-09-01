fdf089a2ed7f8dc7bc1e0820e4d23eb38c90b165
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:37:01 +0800

    add 15060.lua

---
+++ 15060.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15060.lua
+--Gamename Rayman 2 - The Great Escape
+addappid(15060) --Mainappid Rayman 2 - The Great Escape
+addappid(15061, 1, "a53b1c5330d0f3084dbf4abde0e2144963b23d8c35f677be631650bd2500bfd6") --Main Windows Depot Rayman 2 - The Great Escape
+setManifestid(15061, "7412792547056707860", 0)
