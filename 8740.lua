95b67bff87f5063df6fe595dc5f591aff7c78ec2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:23:10 +0800

    add 8740.lua

---
+++ 8740.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8740.lua
+--Gamename Puzzlegeddon
+addappid(8740) --Mainappid Puzzlegeddon
+addappid(8741, 1, "176353a431cbd05f78e0a5f0216fd47987a0566ce109f238ca9c7bfd192730a2") --Main Windows Depot Puzzlegeddon
+setManifestid(8741, "7652915241833712568", 0)
