277a36297423f3cf96403633dbcaa6cb3f4ec7dd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:41:57 +0800

    add 36630.lua

---
+++ 36630.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 36630.lua
+--Gamename Rusty Hearts
+addappid(36630) --Mainappid Rusty Hearts
+addappid(36631, 1, "41078cc846c6c1fb3ea248312a10376265e879730a43949303f550c498c9268c") --Main Windows Depot Rusty Hearts
+setManifestid(36631, "4812308084960354390", 0)
+addappid(36632) --Dlcname Rusty Hearts Meilin Starter Pack
+addappid(36633) --Dlcname Rusty Hearts Roselle Starter Pack
