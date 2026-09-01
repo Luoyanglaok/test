a9c6dfe623e63d6c31a1cc371feafc78efdc9189
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:56:06 +0800

    add 21900.lua

---
+++ 21900.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21900.lua
+--Gamename Tom Clancy's H.A.W.X™
+addappid(21900) --Mainappid Tom Clancy's H.A.W.X™
+addappid(21901, 1, "58cbd92918e15732ffd0fe18b60c30d86dc57d4a2850860db71aa417b44c3907") --Main Windows Depot Tom Clancy's H.A.W.X™
+setManifestid(21901, "4423971803632828017", 0)
