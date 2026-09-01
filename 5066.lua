f81983dc22fda343fe3bd9c55e34d2e6e670c8f1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:42:09 +0800

    add 5066.lua

---
+++ 5066.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5066.lua
+--Gamename Pro Cycling Manager 2008 Trailer
+addappid(5066) --Mainappid Pro Cycling Manager 2008 Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Pro Cycling Manager 2008 Trailer
+setManifestid(910, "1332084816209327708", 0)
