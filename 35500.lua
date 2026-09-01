aa7bde68c947e9ca03a5a6c66559d1387769174a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:38:48 +0800

    add 35500.lua

---
+++ 35500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 35500.lua
+--Gamename Cities XL - Limited Edition
+addappid(35500) --Mainappid Cities XL - Limited Edition
+addappid(35501, 1, "c750713896ae5f6784643acdc6fd294dbed040ecc0fef16c6263d6e56caec025") --Main Windows Depot Cities XL - Limited Edition
+setManifestid(35501, "8515775492305103844", 0)
