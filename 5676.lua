1f53155b5e7084e2e6cd83e00f841b5e61cdf34b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:47:11 +0800

    add 5676.lua

---
+++ 5676.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5676.lua
+--Gamename Trine Path To New Dawn Trailer
+addappid(5676) --Mainappid Trine Path To New Dawn Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Trine Path To New Dawn Trailer
+setManifestid(910, "1332084816209327708", 0)
