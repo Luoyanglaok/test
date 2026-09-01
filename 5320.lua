3152e4049778537f2ece76cc9ef3db889ab21eeb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:31:27 +0800

    add 5320.lua

---
+++ 5320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5320.lua
+--Gamename Batman Arkham Asylum Invisible
+addappid(5320) --Mainappid Batman Arkham Asylum Invisible
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Batman Arkham Asylum Invisible
+setManifestid(910, "1332084816209327708", 0)
