1bfb20b48cf3fc79075d09a4feb630b50cc2dc70
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:12:35 +0800

    add 24640.lua

---
+++ 24640.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24640.lua
+--Gamename Trainz: Classic Cabon City
+addappid(24640) --Mainappid Trainz: Classic Cabon City
+addappid(24631, 1, "90b92b1630fa1fc26400da330418abc3b721c0d0991fdf7361cb692833868793") --Main Windows Depot Trainz: Classic Cabon City
+setManifestid(24631, "196455006608489102", 0)
+addappid(24641, 1, "10ad8d65f3c8b49943e45df96e93f677df89e840f20e369b0bfcc9c4c0ced59c") --Main Windows Depot Trainz: Classic Cabon City
+setManifestid(24641, "8791572549680993688", 0)
