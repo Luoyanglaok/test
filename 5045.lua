b285df2b98a688ffde7fba71fc6111f523676a4c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:10:17 +0800

    add 5045.lua

---
+++ 5045.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5045.lua
+--Gamename Trials 2: Second Edition: Rollin and Tumblin
+addappid(5045) --Mainappid Trials 2: Second Edition: Rollin and Tumblin
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Trials 2: Second Edition: Rollin and Tumblin
+setManifestid(910, "1332084816209327708", 0)
