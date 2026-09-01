7876d6fd9cd88f3261cb0eeaf4fc60fe00a0bf0f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:18:08 +0800

    add 31900.lua

---
+++ 31900.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 31900.lua
+--Gamename Nancy Drew®: The Creature of Kapu Cave
+addappid(31900) --Mainappid Nancy Drew®: The Creature of Kapu Cave
+addappid(31901, 1, "1e7840dd5eb9e0253e638d70f01fd8b1624a59eb5cfaa7c48bf56b0bdd574faf") --Main Windows Depot Nancy Drew®: The Creature of Kapu Cave
+setManifestid(31901, "6149706206686542196", 0)
