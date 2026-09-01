7555ddbee116d0d99e8a78eb34471c01b2af0159
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:28:10 +0800

    add 5284.lua

---
+++ 5284.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5284.lua
+--Gamename Mini Ninjas HIRO Vignette
+addappid(5284) --Mainappid Mini Ninjas HIRO Vignette
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Mini Ninjas HIRO Vignette
+setManifestid(910, "1332084816209327708", 0)
