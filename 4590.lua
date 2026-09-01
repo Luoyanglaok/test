ba87512d8dda5d4e0e192fee6d0385e7daf14809
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:06:14 +0800

    add 4590.lua

---
+++ 4590.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4590.lua
+--Gamename Titan Quest Demo
+addappid(4590) --Mainappid Titan Quest Demo
+addappid(4591, 1, "96baea92085571a6985bc651871d49eb852e01de314457fc79039cba8aea7481") --Main Windows Depot Titan Quest Demo
+setManifestid(4591, "2365052288003786737", 0)
