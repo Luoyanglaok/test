a1b14669e5aed44f9321fa10a10f7e2738b7a860
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:07:09 +0800

    add 4932.lua

--- 4932.lua
+++ 4932.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 4932.lua
-addappid(4932, 0, "324530e1e9d55e7a996812dcc5cf40fc680c1d40dd23616cbf646432634db9c9")
+addappid(4932, 0, "cfb981ea002657403e62fbb609e1d066e07ceac6677fdc6d105f27b5c460c8a9")
