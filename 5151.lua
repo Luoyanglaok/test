3e7192fb76c6c6a60c4520e5d15d41ccdf25fff3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:20:50 +0800

    add 5151.lua

---
+++ 5151.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5151.lua
+--Gamename Eve Online: Quantum Rise Trailer
+addappid(5151) --Mainappid Eve Online: Quantum Rise Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Eve Online: Quantum Rise Trailer
+setManifestid(910, "1332084816209327708", 0)
