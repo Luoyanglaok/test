7b46172db0221b6232ad309afd29bf5e4780d858
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:52:04 +0800

    add 41100.lua

---
+++ 41100.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 41100.lua
+--Gamename Hammerfight
+addappid(41100) --Mainappid Hammerfight
+addappid(41101, 1, "d6e033d7b05099b4574323ac4afaa50d2ed4e2913ba34385be75fe8345e85d44") --Main Windows Depot Hammerfight
+setManifestid(41101, "9099976178542377286", 0)
+addappid(41102, 1, "d704cd788d2837bc4ed83e759c68d01c1b725c14e1e62b4b573e9313284258d5") --Main Linux Depot Hammerfight
+setManifestid(41102, "6879580539895862765", 0)
+addappid(41103, 1, "94d2058575d13757f974163ff90b9c896c584e9b005c9b4932e4e536dfa141f7") --Main Macos Depot Hammerfight
+setManifestid(41103, "6094076177698590545", 0)
