c0ce4892767012e9fca8a4553958b246b18e3d38
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:25:12 +0800

    add 2828.lua

---
+++ 2828.lua
@@ -0,0 +1,63 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2828.lua
+--Gamename Steam Store
+addappid(2828, 1, "5420f3b8be7899820e802e277e845dedcfb46e152e6580825b199653873edc44") --Mainappid Steam Store
+setManifestid(2828, "828302822341641812", 451629152)
+addappid(2829, 1, "e9ca1a9a4e41bac8a8e998d6c6cf73571ca18518146d88e93784825fd9a15c44") --Main Windows Language Depot Spanish Steam Store
+setManifestid(2829, "3606314388738915297", 261700416)
+--Dlcname X3: Terran Conflict
+addappid(2820) --Dlcname X3: Terran Conflict
+addappid(2821, 1, "c888d31f86fadbe042ac724312f0063083dab3a5fce6c1a56cecdba75ebc7d15") --Dlc Windows Depot X3: Terran Conflict
+setManifestid(2821, "4571596043939966618", 6350936464)
+addappid(2822, 1, "af4d4ece2933b111e2fbb938d0b76133ea5800b2eddf185eff0a0c09678e48e6") --Dlc Windows Language Depot German X3: Terran Conflict
+setManifestid(2822, "5904076903935520543", 302057936)
+addappid(2823, 1, "7b88f428c7480c60faef7928d37df249ef3e5542d1943e66203fe07ef36dc47c") --Dlc Windows Language Depot French X3: Terran Conflict
+setManifestid(2823, "6944571675475707313", 252535584)
+addappid(2824, 1, "e9b2fad7ae2e3e24cc41aaf51193d6285c93993aafadc3cec57cc32c5e2db21a") --Dlc Windows Language Depot Italian X3: Terran Conflict
+setManifestid(2824, "4487446513069688130", 261700416)
+addappid(2825, 1, "55dfc08c2ac8d7777dcedd7d4aa25e75a46ac50f126cc8cce5597bf17c0eb1c9") --Dlc Windows Language Depot English X3: Terran Conflict
+setManifestid(2825, "419643549810218030", 261700416)
+addappid(2826, 1, "d74b6d4a1af193772e295cda6927b3e6a392d8ac7e44ef95898ae3ca852f6f9f") --Dlc Windows Depot X3: Terran Conflict
+setManifestid(2826, "2922034356428210668", 539728)
+addappid(2827, 1, "98329506663c0685a6fdfd84402991dcf69408df02c11256b1eff9cf5a24dadd") --Dlc Windows Depot X3: Terran Conflict
+setManifestid(2827, "8903563702817739294", 1872)
+addappid(207311, 1, "a700717e49d242d306923280d53fbcb06f6561ec1997d1293e0318b9a1a3c02e") --Dlc Windows Language Depot Polish X3: Terran Conflict
+setManifestid(207311, "2601193916248056330", 261700416)
+addappid(207312, 1, "5cdadc4e28eb661e4f8c256d91d9f384a78f75e9ae8192260acfc2355f063252") --Dlc Windows Language Depot Czech X3: Terran Conflict
+setManifestid(207312, "7395632196623364940", 261700416)
+addappid(207313, 1, "7b93962c40ada9fa93820024a12d2c2551c4add1c2641f7f728b88967600afdd") --Dlc Windows Language Depot Schinese X3: Terran Conflict
+setManifestid(207313, "2934857449112272061", 264451616)
+addappid(207314, 1, "06ed9b196b226a3e8c0e892eca4b6fb3f0b22630125db2efb2496c7de80ca09b") --Dlc Windows Language Depot Tchinese X3: Terran Conflict
+setManifestid(207314, "6646266648853799100", 264451616)
+addappid(207315, 1, "065b7b2a635d3f60f8c8efe0ee5fd4d6c88e1241f784784c9e1bf8f996c07496") --Dlc Windows Language Depot Japanese X3: Terran Conflict
+setManifestid(207315, "4814546802455153787", 263208816)
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Dlc Windows Depot X3: Terran Conflict
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Dlc Windows Depot X3: Terran Conflict
+addappid(229381, 1, "a81e9b0069cefea741141beb090e1e8c42a3604ca051a5533c40a3f71397c10d") --Dlc Linux/Macos Language Depot English X3: Terran Conflict
+setManifestid(229381, "3757522022214182985", 299276384)
+addappid(229382, 1, "6ae5c4a5e3d187e3d4c13c69586ee8da2deb8201e2b0b34cf57377cd3fe62d77") --Dlc Linux/Macos Language Depot Russian X3: Terran Conflict
+setManifestid(229382, "621247179248362105", 346898080)
+addappid(229383, 1, "160bdd854f63c70a103d35b4c8602b57946a1e31e35aa6b5e69bb22499254f3f") --Dlc Linux/Macos Language Depot Spanish X3: Terran Conflict
+setManifestid(229383, "4262111734254535056", 299276384)
+addappid(229384, 1, "09534debdcd244bce85a6ada2fe927acf287e43350e52a44da27cc2030a8d4a9") --Dlc Linux/Macos Language Depot Polish X3: Terran Conflict
+setManifestid(229384, "4747656007006527623", 299276384)
+addappid(229385, 1, "32912e2d3dd0f2e6422ea30a04351232d6f0d06e60e555864d229a3f88b890b8") --Dlc Linux/Macos Language Depot Czech X3: Terran Conflict
+setManifestid(229385, "37094450773341160", 299276384)
+addappid(229386, 1, "dda73e2697e4747ed5d3d2827d6429d619f0872e214ea30320c59d91073e1d84") --Dlc Linux/Macos Language Depot Schinese X3: Terran Conflict
+setManifestid(229386, "1706518292678967950", 299276384)
+addappid(229387, 1, "da8dc0dce6c80f31b330c3a28f0429c6d4d69b986cb4b73f45b6560758a3d58d") --Dlc Linux/Macos Language Depot Tchinese X3: Terran Conflict
+setManifestid(229387, "5315881311567316279", 299276384)
+addappid(229388, 1, "873649af16d85816a456ac7a163f93f88e4122dca946358fba2625597793bbf4") --Dlc Linux/Macos Language Depot Japanese X3: Terran Conflict
+setManifestid(229388, "3306788036035193002", 299276384)
+addappid(229389, 1, "baba7085881044e062f07b20d5bd3cacb370f6ac58dd56e5c7d1206285665990") --Dlc Linux Depot X3: Terran Conflict
+setManifestid(229389, "9001090755008083245", 278817808)
+addappid(229390, 1, "5e878f55540ff6fb3fbd2901189e4f362e10610dcbd5c05788b6e8c36aa0f8f9") --Dlc Linux/Macos Language Depot German X3: Terran Conflict
+setManifestid(229390, "860322241960964237", 350262704)
+addappid(229391, 1, "d3f5c08711495497dc6313ef9e5cca4f5ddfd96e3d4c1b32c7d45540a61dc9e3") --Dlc Linux/Macos Language Depot French X3: Terran Conflict
+setManifestid(229391, "878659847836389323", 287749904)
+addappid(229392, 1, "01bc2cb82209fc51236ccd984736882f47895cc47bb6d970e0906d1200760ea1") --Dlc Linux/Macos Language Depot Italian X3: Terran Conflict
+setManifestid(229392, "3691373940361021004", 299276384)
+addappid(235940, 1, "5daab6238239618c9e04cc1b1eb50b619a5abd034bec9c08b8fad47339556b39") --Dlc Windows Depot X3: Terran Conflict
+setManifestid(235940, "2216595307647535683", 1239408)
+addappid(235941, 1, "1d234f2e2b511d3891b44af46ec0ac9be0a2f66ee9e193b852ed7f471a1ba388") --Dlc Macos Depot X3: Terran Conflict
+setManifestid(235941, "936852204344306804", 575840464)
