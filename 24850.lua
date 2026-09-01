3b7acf4409044ffab7ea6db7af339bf2436ef1b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:13:58 +0800

    add 24850.lua

---
+++ 24850.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24850.lua
+--Gamename FIFA Manager 10
+addappid(24850) --Mainappid FIFA Manager 10
+addappid(24851, 1, "a52991118dc7ac799a590274f989efd08961d350c20240a01ea38d45e302b70e") --Main Windows Depot FIFA Manager 10
+setManifestid(24851, "3195028868850090844", 0)
+addappid(24852, 1, "95d598462ada092a4418e28ba69c9e88cbecfac5413d8a21c0f364ce33e2aac4") --Main Windows Language Depot French FIFA Manager 10
+setManifestid(24852, "6300388058212276110", 0)
+addappid(24853, 1, "f50799989a5043881dced20514af89b68558479f1427ab35e298c4802258ea71") --Main Windows Language Depot German FIFA Manager 10
+setManifestid(24853, "4063793439173217513", 0)
+addappid(24854, 1, "241c2b68638f960b2d4011ac40ad6a34fdfda33751b72fb3dea6e15285adb874") --Main Windows Language Depot Italian FIFA Manager 10
+setManifestid(24854, "1526318095380747303", 0)
+addappid(24855, 1, "729457d61448e99d71fa817791dcba7b941ef29e82a75a257d08129547a196e7") --Main Windows Language Depot Polish FIFA Manager 10
+setManifestid(24855, "3237811698365121299", 0)
+addappid(24856, 1, "1160281b8e74b28d111347ce4029f34eb9ff1fc1f048c82c97c26034123ad3ea") --Main Windows Language Depot Russian FIFA Manager 10
+setManifestid(24856, "6895522348432573227", 0)
+addappid(24857, 1, "13d52a97a99e733d0eb712133b25fef027ecfcb5dc420236659c7a8d94a67e75") --Main Windows Language Depot Spanish FIFA Manager 10
+setManifestid(24857, "9001724470964031720", 0)
+addappid(24858, 1, "37231702f804c6e5214c22017c06e2665c9bad646644f35a4950c925369c07dd") --Main Windows Depot FIFA Manager 10
+setManifestid(24858, "7132773370207516640", 0)
