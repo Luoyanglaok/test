7743447bdbd38de00eb1d19a6102310d1cdee408
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:57:48 +0800

    add 22319.lua

---
+++ 22319.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 22319.lua
+--Gamename Rogue Warrior
+addappid(22319) --Mainappid Rogue Warrior
+addappid(22320, 1, "6a7a777a8b36cd4e888ed7c0a5f6ebb42bd92eb53182cbc3bd76729f77e3433f") --Main Windows Depot Rogue Warrior
