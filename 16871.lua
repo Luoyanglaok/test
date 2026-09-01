ba0710ddfa476d8faeb858a5df01942a285a5045
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:44:44 +0800

    add 16871.lua

---
+++ 16871.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16871.lua
+--Gamename Sid Meier's Civilization V - XP2
+addappid(16871, 1, "56ec0aa4e2db20db9a72cff513170723b9d65f9156eb3ed3bf195726dd6eda19") --Mainappid Sid Meier's Civilization V - XP2
