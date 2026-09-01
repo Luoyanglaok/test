5d5e46f5e33145c5e97432a771bf6f7ec3afd5f2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:23:54 +0800

    add 8859.lua

---
+++ 8859.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8859.lua
+--Gamename Bioshock 2 Pre-order
+addappid(8859, 1, "38b255872f2158733719031fdaf5daa73aee3e6ca1d994b57183ef2fbf499460") --Mainappid Bioshock 2 Pre-order
+addappid(8860, 1, "350d124af3d677e07c5ffd9775a93a65e162c1d6977cc4473d94a8a7e9fd11b8") --Main Windows Depot Bioshock 2 Pre-order
