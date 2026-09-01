7afc2cc3a4e08e3284e820f25b25e4c4f6047da3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:29:30 +0800

    add 10490.lua

---
+++ 10490.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10490.lua
+--Gamename Iron Man
+addappid(10490) --Mainappid Iron Man
+addappid(10491, 1, "b3888c4ae8aa1787b3169641e4db260909433dda7d6e035e2956cc7728c51e29") --Main Windows Depot Iron Man
+setManifestid(10491, "8124736344014823098", 0)
+addappid(10492, 1, "cb2de8aba5ae1197509f4e31901caff6e581ab6802c1b47800ce338c18191f32") --Main Windows Depot Iron Man
+setManifestid(10492, "6314958001951609406", 0)
