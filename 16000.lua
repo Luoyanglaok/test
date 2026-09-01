bc0a2bdfdffeaaf640bf4eb0b5fc1c222a4288f7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:41:07 +0800

    add 16000.lua

---
+++ 16000.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16000.lua
+--Gamename Discovery! A Seek and Find Adventure
+addappid(16000) --Mainappid Discovery! A Seek and Find Adventure
+addappid(16001, 1, "c0a2f25a51657804f403a3e6c504788b987b88279b06f2408c2b6783139b2cdb") --Main Windows Depot Discovery! A Seek and Find Adventure
+setManifestid(16001, "4031222449610861303", 0)
