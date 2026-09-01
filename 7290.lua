c20184cdc6a9e45d7d22cb2563556bdbdd10bc54
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:18:31 +0800

    add 7290.lua

---
+++ 7290.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7290.lua
+--Gamename Sherlock Holmes: The Awakened Demo
+addappid(7290) --Mainappid Sherlock Holmes: The Awakened Demo
+addappid(7291, 1, "1bb9f7dd14194eec676af7b8795bfaad51aa32f7f9a985a194865dccc34f6418") --Main Windows Depot Sherlock Holmes: The Awakened Demo
+setManifestid(7291, "254576263663989259", 0)
