68b95888011df76dee7ddcf86f6e7339767b54b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:25:42 +0800

    add 9510.lua

---
+++ 9510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9510.lua
+--Gamename Gish Demo
+addappid(9510) --Mainappid Gish Demo
+addappid(9511, 1, "9f898037403862dd83c280ddcf50fa4dc55ad41af1b522361617533bcc694735") --Main Windows Depot Gish Demo
+setManifestid(9511, "3730757904737145976", 0)
