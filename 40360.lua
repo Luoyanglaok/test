2e8ed3f867b90a674be529c0f3506eaf7a36916b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:49:48 +0800

    add 40360.lua

---
+++ 40360.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 40360.lua
+--Gamename Lost Horizon Demo
+addappid(40360) --Mainappid Lost Horizon Demo
+addappid(40361, 1, "0b98f9c541c879f2e2eb391e82065dae4140500cd2f341b36a9607b67470c211") --Main Windows Depot Lost Horizon Demo
+setManifestid(40361, "2799510147302440916", 0)
+addappid(40362, 1, "a20a5567d7be46d233aca8dd44f7909cb7f88b5ed7726703b86fbba27b065648") --Main Windows Language Depot French Lost Horizon Demo
+setManifestid(40362, "6905451520117506435", 0)
+addappid(40363, 1, "5bb0b10dbb605e2f10c80fad1066fb217670e64b173859f4e2a711600a679f7c") --Main Windows Language Depot German Lost Horizon Demo
+setManifestid(40363, "1646576224331138931", 0)
+addappid(40364, 1, "f4db6e89454c3663c6f0667b9576c1f07f12b476f021a6979d67f1a75c01590b") --Main Windows Language Depot Italian Lost Horizon Demo
+setManifestid(40364, "3307027338990742236", 0)
+addappid(40365, 1, "a03090484ea3fdb75670ca675d1c8b042b26c4a6c81058acd3fa3fde6fe72a36") --Main Windows Language Depot Spanish Lost Horizon Demo
+setManifestid(40365, "6106202916714483726", 0)
