85e71aa798ae7a81b61a1fef72e70aad0db67b3d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:12:51 +0800

    add 5087.lua

---
+++ 5087.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5087.lua
+--Gamename Multiwinia: Rocket Riot
+addappid(5087) --Mainappid Multiwinia: Rocket Riot
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Multiwinia: Rocket Riot
+setManifestid(910, "1332084816209327708", 0)
