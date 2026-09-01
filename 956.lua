7c35f494b542c9af88ba31164e5769072931ab88
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:45:48 +0800

    add 956.lua

---
+++ 956.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 956.lua
+--Gamename Project: Snowblind Trailer
+addappid(956) --Mainappid Project: Snowblind Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Project: Snowblind Trailer
+setManifestid(910, "1332084816209327708", 0)
