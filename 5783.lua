306006be23548c6111b6347ac36e50fdaaca32eb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:59:35 +0800

    add 5783.lua

---
+++ 5783.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5783.lua
+--Gamename Guild2AO_No2
+addappid(5783) --Mainappid Guild2AO_No2
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Guild2AO_No2
