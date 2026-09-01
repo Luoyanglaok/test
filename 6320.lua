eb7687abcb2efadc14f76c1e447167c7cf5dd108
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:15:42 +0800

    add 6320.lua

---
+++ 6320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6320.lua
+--Gamename The Longest Journey Demo
+addappid(6320) --Mainappid The Longest Journey Demo
+addappid(6321, 1, "b552e78567fefed60652f56f2f277e7a384b2d77fdfdfd0d9b84d6479f394d15") --Main Windows Depot The Longest Journey Demo
+setManifestid(6321, "8393188558464763665", 0)
