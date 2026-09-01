5db05898b19a87cbad5eacf567c468e6d6722254
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:52:20 +0800

    add 19810.lua

---
+++ 19810.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19810.lua
+--Gamename The Sum of All Fears
+addappid(19810) --Mainappid The Sum of All Fears
+addappid(19811, 1, "127ec6d1c862c654c02289ed155545e6b501e8b83ebe760adefa793cc2bd1b8d") --Main Windows Depot The Sum of All Fears
+setManifestid(19811, "3296809662105095565", 0)
