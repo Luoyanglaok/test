f2ea2ac78145d2ed81b841c5f689ed15870aa976
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:40:46 +0800

    add 909.lua

---
+++ 909.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 909.lua
+--Gamename Sin Episodes: Emergence Trailer
+addappid(909) --Mainappid Sin Episodes: Emergence Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Sin Episodes: Emergence Trailer
+setManifestid(910, "1332084816209327708", 0)
