6021b6044e46552243f9e623cb86043cf38a8241
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:33:26 +0800

    add 34810.lua

---
+++ 34810.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34810.lua
+--Gamename Project Freedom
+addappid(34810) --Mainappid Project Freedom
+addappid(34811, 1, "d391c03441196490f2d75845950c5d4de871bae7d74a7747d018357e6687c92b") --Main Windows Depot Project Freedom
+setManifestid(34811, "7350366154313929699", 0)
+addappid(34812, 1, "ac21765e1032b3790fac6aff464e82b99ab2b14780a53ee6a7cf891d73b2bbba") --Main Windows Language Depot French Project Freedom
+setManifestid(34812, "2465405639831113018", 0)
+addappid(34813, 1, "606d5db91597c3bfb9e06e7fbdf470f274b5b44094411c7c79a96c09477d7456") --Main Windows Language Depot German Project Freedom
+setManifestid(34813, "5296819047380472001", 0)
+addappid(34814, 1, "809422b691d3342c55f19929eec68672629342f8187050e652c00dd527b10dc2") --Main Windows Language Depot Italian Project Freedom
+setManifestid(34814, "8113772118401774109", 0)
