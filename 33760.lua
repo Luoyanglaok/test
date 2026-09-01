994c6ece63ddf341ec005630d94b5b17cab498e1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:22:54 +0800

    add 33760.lua

---
+++ 33760.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33760.lua
+--Gamename The Search for Amelia Earhart
+addappid(33760) --Mainappid The Search for Amelia Earhart
+addappid(33761, 1, "497c6eb0996e387f0c2c2c66fb105c3bff09bfa5c115cf17d17ede17a6237d98") --Main Windows Depot The Search for Amelia Earhart
+setManifestid(33761, "6432550248502405489", 0)
