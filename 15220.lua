73393b2500d5432fea3a55790364cb2871e5d5f1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:37:26 +0800

    add 15220.lua

---
+++ 15220.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15220.lua
+--Gamename Blazing Angels® Squadrons of WWII
+addappid(15220) --Mainappid Blazing Angels® Squadrons of WWII
+addappid(15221, 1, "10790d6601a2869c0bc6210c8559a1d8f7bf8c05c4210898a2843cd53d50fa63") --Main Windows Depot Blazing Angels® Squadrons of WWII
+setManifestid(15221, "5802111715784687297", 0)
