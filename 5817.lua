6a7099da85a0952dd3a47b1b01f303fa59c747ad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:01:49 +0800

    add 5817.lua

---
+++ 5817.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5817.lua
+--Gamename Medal of Honor - Limited Edition
+addappid(5817) --Mainappid Medal of Honor - Limited Edition
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Medal of Honor - Limited Edition
+setManifestid(910, "1332084816209327708", 0)
