0a3b8d939c7f3317d86a429d8c053b0b56d05ca6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:52:31 +0800

    add 19910.lua

---
+++ 19910.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19910.lua
+--Gamename Tom Clancy's Rainbow Six 2: Rogue Spear - Urban Operations
+addappid(19910) --Mainappid Tom Clancy's Rainbow Six 2: Rogue Spear - Urban Operations
+addappid(13661, 1, "0751f074c0aec5069570a431f3f05f25c4ca707e63c24790d3b8b1d021cd59d9") --Main Windows Depot Tom Clancy's Rainbow Six 2: Rogue Spear - Urban Operations
+addappid(19911, 1, "570b240d09965ea5ec15872f9d682605043e84fd9b2d565fb14f17fa092c817e") --Main Windows Depot Tom Clancy's Rainbow Six 2: Rogue Spear - Urban Operations
+setManifestid(19911, "5399668217721805262", 0)
