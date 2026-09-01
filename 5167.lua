263a78ec19799e5a64e34b6d76a7636144394e52
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:21:17 +0800

    add 5167.lua

---
+++ 5167.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5167.lua
+--Gamename Strong Bad Episode 5: 8-Bit Is Enough Trailer
+addappid(5167) --Mainappid Strong Bad Episode 5: 8-Bit Is Enough Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Strong Bad Episode 5: 8-Bit Is Enough Trailer
+setManifestid(910, "1332084816209327708", 0)
