0b3f6ee323ea2b2d7d95a984ca4ec808276996a4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:57:00 +0800

    add 21979.lua

---
+++ 21979.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21979.lua
+--Gamename R.U.S.E.
+addappid(21979, 1, "dd9cc83fdae46f0073ea9325a330bd8802d1f4f17ac00856cd9cfb09fad93f90") --Mainappid R.U.S.E.
