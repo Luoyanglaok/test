07c873cb9083700b763e32bf8e77d68866bd0bbf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:14:50 +0800

    add 6090.lua

---
+++ 6090.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6090.lua
+--Gamename Armed and Dangerous
+addappid(6090) --Mainappid Armed and Dangerous
+addappid(6091, 1, "9c616c1e647e025f3a6688910224c4ae15843b106ca2bb2f331d66d5364a34fa") --Main Windows Depot Armed and Dangerous
+setManifestid(6091, "5563098898668404336", 0)
+addappid(6092, 1, "f59bd2328d4d8d5d74617334a19e7ad641bfcbedd4a810bd4db11ed2578288a4") --Main Windows Language Depot French Armed and Dangerous
+setManifestid(6092, "1384470052935031482", 0)
+addappid(6093, 1, "44172fea79b01fcffcf4764b35d3ea689c696f1c64af56dc5644dca67c09e539") --Main Windows Language Depot German Armed and Dangerous
+setManifestid(6093, "8761955353458920280", 0)
+addappid(6094, 1, "f9d606821612083cd573ea6149b166cbd9fb7674bf64f85a5082f41a469183d0") --Main Windows Language Depot Italian Armed and Dangerous
+setManifestid(6094, "1697949650417000934", 0)
+addappid(6095, 1, "95ec18e7844dee4b778ecd8f9f14dab95c300138037775e6b68387ab9b77b14e") --Main Windows Language Depot Spanish Armed and Dangerous
+setManifestid(6095, "5977527614239288900", 0)
