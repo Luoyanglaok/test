b07bbba5a5756326bb2b13bba31a7f632dc0fdf1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:44:18 +0800

    add 37390.lua

---
+++ 37390.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37390.lua
+--Gamename Gemini Lost™
+addappid(37390) --Mainappid Gemini Lost™
+addappid(37391, 1, "f11e0b0dd1c086e0aaa9b846ac469284ce4842807bfaa1ec72c79bcf329c4a15") --Main Windows Depot Gemini Lost™
+setManifestid(37391, "8124150991574371806", 0)
