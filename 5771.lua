504d83fe13b25199c9de957d845c5b05fa504cda
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:58:13 +0800

    add 5771.lua

---
+++ 5771.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5771.lua
+--Gamename KL2 Undercover Cop Mode (Italian)
+addappid(5771) --Mainappid KL2 Undercover Cop Mode (Italian)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot KL2 Undercover Cop Mode (Italian)
+setManifestid(910, "1332084816209327708", 0)
