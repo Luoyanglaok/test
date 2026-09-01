4d7761ef997ebe01f4147081a467095e41057e1b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:28:15 +0800

    add 10250.lua

---
+++ 10250.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10250.lua
+--Gamename PT Boats: Knights of the Sea
+addappid(10250) --Mainappid PT Boats: Knights of the Sea
+addappid(10251, 1, "aacb83167571790993fec751edf4a5db365d4afae0a12d8587e73679eb2fc833") --Main Windows Depot PT Boats: Knights of the Sea
+setManifestid(10251, "1977902023899756359", 0)
+addappid(10252, 1, "960c25a7a28d1b4e346144db934532e22404a07937c443343c7decd267d737b7") --Main Windows Language Depot English PT Boats: Knights of the Sea
+setManifestid(10252, "8246586604621610930", 0)
+addappid(10253, 1, "7443e47bfd5877cbac1bf2107e60e711d754c4ba8def3ffb97e550d23198ab1b") --Main Windows Language Depot Russian PT Boats: Knights of the Sea
+setManifestid(10253, "7591848700232825309", 0)
