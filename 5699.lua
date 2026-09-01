0a5abaa0325e83636e6ed95cf256ce49860e8075
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:49:32 +0800

    add 5699.lua

---
+++ 5699.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5699.lua
+--Gamename All Points Bulletin - Trailer
+addappid(5699) --Mainappid All Points Bulletin - Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot All Points Bulletin - Trailer
