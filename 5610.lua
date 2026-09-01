c2dfdd70222d94a3e7b7da0a82d058f57f72c92d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:44:06 +0800

    add 5610.lua

---
+++ 5610.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5610.lua
+--Gamename Supreme Commander 2 Pulinsmash
+addappid(5610) --Mainappid Supreme Commander 2 Pulinsmash
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Supreme Commander 2 Pulinsmash
+setManifestid(910, "1332084816209327708", 0)
