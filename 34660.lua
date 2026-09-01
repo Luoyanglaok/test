3e4e0ebb3e89c00cef382d24284cd69f1228b0fb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:32:43 +0800

    add 34660.lua

---
+++ 34660.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34660.lua
+--Gamename Massive Assault Network 2 - Demo
+addappid(34660) --Mainappid Massive Assault Network 2 - Demo
+addappid(34661, 1, "9139cd4c11ef128c982d7db6c9692ef877858c2960bc6d2d66823462c629d786") --Main Windows Depot Massive Assault Network 2 - Demo
+setManifestid(34661, "629231820418078485", 0)
