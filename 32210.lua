c552a054cf594a5693cc6fe1bc3c7fef4d582318
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:19:05 +0800

    add 32210.lua

---
+++ 32210.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 32210.lua
+--Gamename Metal Drift - Demo
+addappid(32210) --Mainappid Metal Drift - Demo
+addappid(32211, 1, "b19277434a7df019dd948ba85a5a88d0035fca5362846f082f59d413522fece8") --Main Windows Depot Metal Drift - Demo
+setManifestid(32211, "997122050527354100", 0)
