ea1a5428dff2c9d18dd6bf36380a9e12d2cf2925
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:28:54 +0800

    add 3838.lua

---
+++ 3838.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3838.lua
+--Gamename Psychonauts Original Soundtrack
+addappid(3838, 1, "85a23853a54945d9ab440c563e0f37476ece9727fbe71e5a47e80035a009d775") --Mainappid Psychonauts Original Soundtrack
+setManifestid(3838, "5400981561268233613", 98212592)
+addappid(3839, 1, "fb90a41f2b4753baee201a045fefadb96cdddfd5e45e905d39eecfbd29678ae2") --Main Windows Depot Psychonauts Original Soundtrack
