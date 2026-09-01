63b022d21c735744805f036412230cdba437c981
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:54:18 +0800

    add 42200.lua

---
+++ 42200.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42200.lua
+--Gamename Nancy Drew® Dossier: Resorting to Danger!
+addappid(42200) --Mainappid Nancy Drew® Dossier: Resorting to Danger!
+addappid(42201, 1, "4efcb3848c314c77249813b10816f41c88416cc1588c63acd2bfba3eccf38334") --Main Windows Depot Nancy Drew® Dossier: Resorting to Danger!
+setManifestid(42201, "1016366587515554934", 279380336)
