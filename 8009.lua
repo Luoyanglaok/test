8ae7f19a793286e20b80573e80de533814fe83e7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:21:15 +0800

    add 8009.lua

---
+++ 8009.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8009.lua
+--Gamename Tomb Raider: Anniversary
+addappid(8009) --Mainappid Tomb Raider: Anniversary
+addappid(8001, 1, "4e1307023822badbbf24311cd61f7a64e6e36f9b31d3548cad45ecf53e5c2b64") --Main Windows Depot Tomb Raider: Anniversary
+addappid(8002, 1, "566e41ba63d701ebf6f145e2c0836504c38a36c476806f81de5b80ea2b253223") --Main Windows Depot Tomb Raider: Anniversary
+addappid(8003, 1, "d4e8812e53cb24d184fa2f43f3628cb4cbf34292801a6cee8c466e516bf78199") --Main Windows Depot Tomb Raider: Anniversary
