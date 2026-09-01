3f825916d2e450df3187fe2863b94ad87844a782
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:43:04 +0800

    add 5075.lua

---
+++ 5075.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5075.lua
+--Gamename Dawn of War II: The Tyranids
+addappid(5075) --Mainappid Dawn of War II: The Tyranids
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Dawn of War II: The Tyranids
+setManifestid(910, "1332084816209327708", 0)
