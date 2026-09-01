ea2a3b0ff5ee7749f8cb493180bc6edf93b0c1a4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:31:40 +0800

    add 11090.lua

---
+++ 11090.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 11090.lua
+--Gamename Dracula Origin Demo
+addappid(11090) --Mainappid Dracula Origin Demo
+addappid(11091, 1, "99f110fb406c6927d8e390060dd5b4ed53bc62f98e227f14b4976198167d9e7f") --Main Windows Depot Dracula Origin Demo
+setManifestid(11091, "8053641233618806344", 0)
