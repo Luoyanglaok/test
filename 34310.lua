aaf105874e6cc0f0201a9d2e7c34e7df14ff8378
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:28:13 +0800

    add 34310.lua

---
+++ 34310.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34310.lua
+--Gamename Galaxy Force II™
+addappid(34310, 1, "05c82a11eeb3453b7afdfcb6a3187d2c340f5a9adaf7f3ac7355a68282cd13bf") --Mainappid Galaxy Force II™
+addappid(34311, 1, "299b996c7ca601015a8a989714099647c26e09661823b4a705e113b6b7f924cc") --Main Windows Depot Galaxy Force II™
