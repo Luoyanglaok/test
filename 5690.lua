010a4c756f79d7de3962697e3cb8dbec9ef655ff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:49:01 +0800

    add 5690.lua

---
+++ 5690.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5690.lua
+--Gamename Sid Meiers Civilization V Teaser
+addappid(5690) --Mainappid Sid Meiers Civilization V Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Sid Meiers Civilization V Teaser
+setManifestid(910, "1332084816209327708", 0)
