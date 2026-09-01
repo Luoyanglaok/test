351dab18fb7a7504125c3eb1af1deb7b194ba159
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:56:01 +0800

    add 5759.lua

---
+++ 5759.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5759.lua
+--Gamename KL2 Fragile Alliance Mode (Italian)
+addappid(5759) --Mainappid KL2 Fragile Alliance Mode (Italian)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot KL2 Fragile Alliance Mode (Italian)
+setManifestid(910, "1332084816209327708", 0)
