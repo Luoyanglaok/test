d1cb2ff94734d8467750b0d297a8e47435f94b81
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:21:15 +0800

    add 1942.lua

---
+++ 1942.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1942.lua
+--Gamename Steam Store
+addappid(1942, 1, "976a942775d851a322d28f3b637806e8738ef92b0b15be4ec0ec299b9561fc01") --Mainappid Steam Store
+addappid(1943, 1, "67f56f52b7b18542d7aa22da4e832efa9c5fad93a18e28e9c59bbf295669130e") --Main Windows Depot Steam Store
