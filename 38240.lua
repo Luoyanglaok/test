3748fea8e3e021dafea37509274c875df946dc76
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:47:36 +0800

    add 38240.lua

---
+++ 38240.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38240.lua
+--Gamename Horrid Henry
+addappid(38240) --Mainappid Horrid Henry
+addappid(38241, 1, "2b1d404f6b08597afaa327d2d00645ca4b26a9f869783be9da639593f0201e95") --Main Windows Depot Horrid Henry
+setManifestid(38241, "1862693231085980245", 0)
