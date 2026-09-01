c80c02bc59408a5152bba5ef5ab88156cc8634ce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:30:19 +0800

    add 10640.lua

---
+++ 10640.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10640.lua
+--Gamename Empire: Total War Naval Demo
+addappid(10640) --Mainappid Empire: Total War Naval Demo
+addappid(10641, 1, "a945c6b34c4158cebb88bcb97ec704298e4d9847442a3966dd03734ab1b07865") --Main Windows Depot Empire: Total War Naval Demo
+setManifestid(10641, "3282906553535799177", 0)
+addappid(10644, 1, "bc43ca40055bcd57e416cef30d241c0e597b595465d6544b26f6681e5f9e85d4") --Main Windows Depot Empire: Total War Naval Demo
+setManifestid(10644, "468634281255520281", 0)
+addappid(10645, 1, "d4da0e3c2362eb43c5d132cafe96949c18b23da71fe7001284e00c75d522a664") --Main Windows Depot Empire: Total War Naval Demo
+setManifestid(10645, "6130104982103199204", 0)
+addappid(10646, 1, "f75e7e98f315608ea68f41e2e5f03f54977895c5f6896f8d36f8f9a2973cecc2") --Main Windows Language Depot English Empire: Total War Naval Demo
+setManifestid(10646, "840047749170703993", 0)
+addappid(10647, 1, "c0c7b64c27f5cc72919e592d641eab7ef36ab5591a70b1c6cc6861ba0c0828cc") --Main Windows Language Depot French Empire: Total War Naval Demo
+setManifestid(10647, "1799393227372258150", 0)
