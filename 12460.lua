51606b93ff226fca5d3da1186eff62023897bcf2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:34:42 +0800

    add 12460.lua

---
+++ 12460.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12460.lua
+--Gamename BC Kings
+addappid(12460) --Mainappid BC Kings
+addappid(12461, 1, "0c3ffcc8828bd510b7ab8be994be3006c8bd50a61a32dafe74d148fd6e8b0424") --Main Windows Depot BC Kings
+setManifestid(12461, "3222366408943250134", 0)
