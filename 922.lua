fc6872ce32d912225e7f1483b563448aa42eef9f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:42:22 +0800

    add 922.lua

---
+++ 922.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 922.lua
+--Gamename Portal Trailer
+addappid(922) --Mainappid Portal Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Portal Trailer
+setManifestid(910, "1332084816209327708", 0)
