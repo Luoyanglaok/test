df214fc23d888968361db536dd6a83867b453dea
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:20:59 +0800

    add 5152.lua

---
+++ 5152.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5152.lua
+--Gamename Grand Theft Auto IV
+addappid(5152) --Mainappid Grand Theft Auto IV
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Grand Theft Auto IV
+setManifestid(910, "1332084816209327708", 0)
