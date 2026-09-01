4420211f7a5bfcae841074eb23d7f5aeed502894
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:38:35 +0800

    add 15510.lua

---
+++ 15510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15510.lua
+--Gamename The Wonderful End of the World Demo
+addappid(15510) --Mainappid The Wonderful End of the World Demo
+addappid(15511, 1, "566e31827d15536edc6f0e8927bad7a6fd6f2f9e4afe8947f6c5b3ba43f67ba8") --Main Windows Depot The Wonderful End of the World Demo
+setManifestid(15511, "4807818357003875003", 0)
