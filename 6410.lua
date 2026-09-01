d73038f1cf6f2a360df1b9c27a86aad573946fe2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:16:26 +0800

    add 6410.lua

---
+++ 6410.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6410.lua
+--Gamename Joint Task Force Demo
+addappid(6410) --Mainappid Joint Task Force Demo
+addappid(6411, 1, "b74bb5995db1cd35813c78d3ac28f93dd24d722cfe7629500a2efce807df70b2") --Main Windows Depot Joint Task Force Demo
+setManifestid(6411, "2462292438259161462", 0)
