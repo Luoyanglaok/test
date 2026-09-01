116b81d3701189dc76b16cadcfe69971abf9fa4a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:43:05 +0800

    add 16630.lua

---
+++ 16630.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16630.lua
+--Gamename Monster Trucks Nitro Demo
+addappid(16630) --Mainappid Monster Trucks Nitro Demo
+addappid(16631, 1, "9a5d7c921cb80c0979e2a11556ba7e8cdab12f78e3cc1045618ac066f0ef8ca7") --Main Windows Depot Monster Trucks Nitro Demo
+setManifestid(16631, "8471325838320286528", 0)
+addappid(16632, 1, "72c882e965842c686e4ef19079c3b0be664d0694b27abeb18ac98aeba4783282") --Main Macos Depot Monster Trucks Nitro Demo
+setManifestid(16632, "1924964703774764018", 0)
