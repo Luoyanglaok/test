0d4a0838b4aad048103bf39833832f8a96cdc71d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:57:40 +0800

    add 1256.lua

--- 1256.lua
+++ 1256.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 1256.lua
-addappid(1256, 0, "1e5adaebd6013559e34d8a015b504ab8bfe7ca8f644a53f524c2da13854b2d56")
+addappid(1256, 0, "4bb808dd44c2be1c737630cc59a18b55a640929eb0f011247182ef1deb0ef5f1")
