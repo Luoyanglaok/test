45be96390b0656293f2aa644844315fc1b8aca74
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:32:48 +0800

    add 5337.lua

---
+++ 5337.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5337.lua
+--Gamename Darkest of Days Time Periods
+addappid(5337) --Mainappid Darkest of Days Time Periods
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Darkest of Days Time Periods
+setManifestid(910, "1332084816209327708", 0)
