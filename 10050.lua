f798f81adeb56444645ef1626f6c4321eebcbe44
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:27:05 +0800

    add 10050.lua

---
+++ 10050.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10050.lua
+--Gamename Enemy Territory: QUAKE Wars Demo 2.0
+addappid(10050) --Mainappid Enemy Territory: QUAKE Wars Demo 2.0
+addappid(10051, 1, "a32602823d1b6f19b803392187b36711a02b4163b6693a810e8c157e5e523a54") --Main Windows Depot Enemy Territory: QUAKE Wars Demo 2.0
+setManifestid(10051, "6155882605441277350", 0)
