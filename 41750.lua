b1100d02f0ff7de477b819a64ec35cf62c0b3211
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:52:36 +0800

    add 41750.lua

---
+++ 41750.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 41750.lua
+--Gamename The Rockin' Dead
+addappid(41750) --Mainappid The Rockin' Dead
+addtoken(41750, "14337621510783806007")
+addappid(41751, 1, "16dd4631b6efab1e2cc976d6caf1a522bbb9efc298ac3dd5a190994026ab67a1") --Main Windows Depot The Rockin' Dead
