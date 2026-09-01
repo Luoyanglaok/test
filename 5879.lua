48982cbf27c2181cda11b4dce5043e86761296f3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:07:46 +0800

    add 5879.lua

---
+++ 5879.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5879.lua
+--Gamename Kane & Lynch 2 - Most Notorious Criminals (DE)
+addappid(5879) --Mainappid Kane & Lynch 2 - Most Notorious Criminals (DE)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Kane & Lynch 2 - Most Notorious Criminals (DE)
+setManifestid(910, "1332084816209327708", 0)
