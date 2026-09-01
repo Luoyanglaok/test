f4f54a6efd40bb8ceed096eee9f315f2987f26e6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:55:05 +0800

    add 21600.lua

---
+++ 21600.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21600.lua
+--Gamename Age of Booty™
+addappid(21600) --Mainappid Age of Booty™
+addappid(21601, 1, "6aacfd8826d8a2380f82d2d3d9935f0413d9615006dfa9705b855bd0c57ececd") --Main Windows Depot Age of Booty™
