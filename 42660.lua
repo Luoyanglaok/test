d3b09dcabe690200bba62a2206084fca8c44f70b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:54:42 +0800

    add 42660.lua

---
+++ 42660.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42660.lua
+--Gamename Shrek Forever After
+addappid(42660) --Mainappid Shrek Forever After
+addappid(42661, 1, "983614ed38c995cc441d6748868840143c0b725bdd576a05abcd01e6968bf98c") --Main Windows Depot Shrek Forever After
+setManifestid(42661, "6783701044101096205", 0)
+addappid(42662, 1, "b7db9bd1cc31934675dca51655a395080c0412da3af45dbf372a50d8d66ba1a6") --Main Windows Language Depot English Shrek Forever After
+setManifestid(42662, "5382936552259045205", 0)
