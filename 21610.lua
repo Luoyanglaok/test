0f8c7ddc96f59af67221a8d8a85571defbe50913
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:55:15 +0800

    add 21610.lua

---
+++ 21610.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21610.lua
+--Gamename MotoGP 08
+addappid(21610) --Mainappid MotoGP 08
+addappid(21611, 1, "856dcd75aba74603635b6239815df4d8b7541f3956059594ef07cfe11b6906a8") --Main Windows Depot MotoGP 08
+setManifestid(21611, "1987966562772558699", 0)
