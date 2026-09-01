e87649eb4bf264f0e3828492d4b66f21b18f39a6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:35:29 +0800

    add 12630.lua

---
+++ 12630.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12630.lua
+--Gamename Legend: Hand of God
+addappid(12630) --Mainappid Legend: Hand of God
+addappid(12631, 1, "3483fb907dcd25042c111bd9086801d6aff0583dc444812da445f637eb0c5bb9") --Main Windows Depot Legend: Hand of God
+setManifestid(12631, "1951146421639676808", 0)
+addappid(12632, 1, "8ec49dbada9ed67cc01f207a08d46866008a918a9f0adce751b53fb7a734ce0b") --Main Windows Language Depot French Legend: Hand of God
+setManifestid(12632, "1433661650788488669", 0)
+addappid(12633, 1, "8c26227fa9ea5b37eae3943a819068703680cdca0968987da43b4d6ee9bb9106") --Main Windows Language Depot German Legend: Hand of God
+setManifestid(12633, "724281995629767953", 0)
+addappid(12634, 1, "1b55287451ce5118e10b06b257bc4b856b5df0e3e7d2775966fbd680c71e8053") --Main Windows Language Depot Italian Legend: Hand of God
+setManifestid(12634, "2057922377981138960", 0)
