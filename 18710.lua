b498c3ba90f65d092aecb665f3d85b07b90ca346
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:50:52 +0800

    add 18710.lua

---
+++ 18710.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 18710.lua
+--Gamename And Yet It Moves Demo
+addappid(18710) --Mainappid And Yet It Moves Demo
+addappid(18711, 1, "64d5c0b95e5b5b9f85fadc470d787b3c56de84fde216d23aabb321cf2b9a0146") --Main Windows Depot And Yet It Moves Demo
+setManifestid(18711, "4115040158675790953", 0)
+addappid(18712, 1, "2f226d5d636f85e3492845143a88ffc05879a2a9db706a2784c5b1c16c976c1f") --Main Macos Depot And Yet It Moves Demo
+setManifestid(18712, "6965901959854589313", 0)
