a26c252dc2fe22d0f1c53091072beeae001aa1f1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:27:50 +0800

    add 3542.lua

---
+++ 3542.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3542.lua
+--Gamename Peggle Nights Demo
+addappid(3542) --Mainappid Peggle Nights Demo
+addappid(3541, 1, "1bd8c41e16d4706b60d57a2728fdb4736c28571510b62f7c71ac902786fa0460") --Main Windows Depot Peggle Nights Demo
+addappid(3543, 1, "53483efcd99bf7f442fc05384b59d4f1f5eeee4c8c07da524354bfa8b19c9a7e") --Main Windows Depot Peggle Nights Demo
