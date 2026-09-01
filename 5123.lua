d4af3d7b8d9ff625dded967c6e73c9e71b2c5fcf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:15:59 +0800

    add 5123.lua

---
+++ 5123.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5123.lua
+--Gamename Fallout 3 Gameplay 5: Tenpenny Tower
+addappid(5123) --Mainappid Fallout 3 Gameplay 5: Tenpenny Tower
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Fallout 3 Gameplay 5: Tenpenny Tower
+setManifestid(910, "1332084816209327708", 0)
