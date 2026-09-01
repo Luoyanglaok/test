2b3917e5de68966898282d7452e29d1392deedfc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:11:41 +0800

    add 5945.lua

---
+++ 5945.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5945.lua
+--Gamename MOH - Part 5
+addappid(5945) --Mainappid MOH - Part 5
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot MOH - Part 5
+setManifestid(910, "1332084816209327708", 0)
