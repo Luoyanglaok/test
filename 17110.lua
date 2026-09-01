15de1f63e8320d2d0de57d75d1615aeafc1db95b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:45:45 +0800

    add 17110.lua

---
+++ 17110.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 17110.lua
+--Gamename Children of the Nile: Enhanced Edition Demo
+addappid(17110) --Mainappid Children of the Nile: Enhanced Edition Demo
+addappid(17111, 1, "cb35d403996fb0d6af1e957894965d0c2a43b35ca5814a516ddeff22c281ab8e") --Main Windows Depot Children of the Nile: Enhanced Edition Demo
+setManifestid(17111, "8843129635180547810", 0)
+addappid(17112, 1, "d4cd9c234eeb95ceb0850a14fa8cc1790bb2ef105a231c25fd22ff265d1b9993") --Main Windows Language Depot English Children of the Nile: Enhanced Edition Demo
+setManifestid(17112, "6387194817456290840", 0)
+addappid(17113, 1, "dd1bdc0eef7a7a23793caf235d29c6e14e00b941db71662dcce4aadb9ca2300f") --Main Windows Language Depot French Children of the Nile: Enhanced Edition Demo
+setManifestid(17113, "1725954516324334872", 0)
+addappid(17114, 1, "948a2339d94659e5eaf0464455ca100f11933fc2cec6e6e9faac7b58ba278844") --Main Windows Language Depot German Children of the Nile: Enhanced Edition Demo
+setManifestid(17114, "474858275770905588", 0)
+addappid(17115, 1, "05a24983d7ae34470fc0988b73a815a51bf4504dc2dc38e4fa464d25e6605dbf") --Main Windows Language Depot Italian Children of the Nile: Enhanced Edition Demo
+setManifestid(17115, "4324174963719037830", 0)
+addappid(17116, 1, "51c51a4b438a100802dc225db32f5cbbc60768b46bfa2a59872f34b55cf450c8") --Main Windows Language Depot Spanish Children of the Nile: Enhanced Edition Demo
+setManifestid(17116, "1461151777270979355", 0)
