d6d01240e9be4705bd85fd9bd0d213528c3716e4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:18:50 +0800

    add 7400.lua

---
+++ 7400.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7400.lua
+--Gamename Ricochet: Lost Worlds
+addappid(7400) --Mainappid Ricochet: Lost Worlds
+addappid(7401, 1, "c3396a088e89a01f5b3cdab64e2b4086e69e535fc651cf3aac458200c0cff0fc") --Main Windows Depot Ricochet: Lost Worlds
+setManifestid(7401, "4262265008227020456", 0)
