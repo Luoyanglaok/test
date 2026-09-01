0d901ea7204d61eb9b58d9e8c5e57442e5cce797
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:08:14 +0800

    add 5886.lua

---
+++ 5886.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5886.lua
+--Gamename Aura2 Trailer
+addappid(5886) --Mainappid Aura2 Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Aura2 Trailer
+setManifestid(910, "1332084816209327708", 0)
