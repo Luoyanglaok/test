e29b13530b4c806c377601f75ee4b665c82fa7e3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:00:05 +0800

    add 23180.lua

---
+++ 23180.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 23180.lua
+--Gamename Fluttabyes - Demo
+addappid(23180) --Mainappid Fluttabyes - Demo
+addappid(23181, 1, "032fa1f348895e1d8b8776a4bdbaf0b440c82b7301c325251e5ea82301def3ab") --Main Windows Depot Fluttabyes - Demo
+setManifestid(23181, "2458231538486532596", 0)
