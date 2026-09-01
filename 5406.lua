cf2de640723b0ced909dbf29c6bd5b1ca7b8a084
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:37:26 +0800

    add 5406.lua

---
+++ 5406.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5406.lua
+--Gamename cities xl
+addappid(5406) --Mainappid cities xl
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot cities xl
+setManifestid(910, "1332084816209327708", 0)
