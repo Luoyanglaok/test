7f596d95dcd365a172b5d18bdee88ba68d9e306b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:19:53 +0800

    add 32730.lua

---
+++ 32730.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 32730.lua
+--Gamename F-22 Lightning 3
+addappid(32730) --Mainappid F-22 Lightning 3
+addappid(32731, 1, "a705f53b54ee0a169a56b40fec21ab1a0deda53aad460b2294546cf1187f0ff2") --Main Windows Depot F-22 Lightning 3
+setManifestid(32731, "813312471384694409", 0)
