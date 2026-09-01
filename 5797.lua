4be2581ae6b58b14443997522ef709e32b5cfa92
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:00:00 +0800

    add 5797.lua

---
+++ 5797.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5797.lua
+--Gamename Sniper Ghost Warrior Basic Tactics
+addappid(5797) --Mainappid Sniper Ghost Warrior Basic Tactics
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Sniper Ghost Warrior Basic Tactics
+setManifestid(910, "1332084816209327708", 0)
