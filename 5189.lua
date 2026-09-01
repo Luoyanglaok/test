537404ed95f6d77c60c36a3a9342b9374a382f56
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:22:50 +0800

    add 5189.lua

---
+++ 5189.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5189.lua
+--Gamename Empire: Total War - Road to Indepdence
+addappid(5189) --Mainappid Empire: Total War - Road to Indepdence
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Empire: Total War - Road to Indepdence
+setManifestid(910, "1332084816209327708", 0)
