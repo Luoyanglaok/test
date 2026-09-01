70d0dbef4496dc3d45fc3b07b3934295d4815d5d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:17:26 +0800

    add 7050.lua

---
+++ 7050.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7050.lua
+--Gamename Project: Snowblind Demo
+addappid(7050) --Mainappid Project: Snowblind Demo
+addappid(7051, 1, "8ab27e83b59781f55bea9f7944e5cf67c23670ffabe8da654cb5e95bac01e2bb") --Main Windows Depot Project: Snowblind Demo
+setManifestid(7051, "4799426275009059803", 0)
