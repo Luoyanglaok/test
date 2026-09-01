f828be1337a43a3c11b230fe0fc433be308f2fce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:23:27 +0800

    add 8804.lua

---
+++ 8804.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8804.lua
+--Gamename Sid Meier's Civilization IV: Beyond the Sword
+addappid(8804, 1, "e071eefdf549dfb671a80e6ec69e090b8e994b31e03aeb46e84c2ee378358858") --Mainappid Sid Meier's Civilization IV: Beyond the Sword
