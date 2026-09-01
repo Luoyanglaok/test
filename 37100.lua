c578b32bba1ac43a510f0d44be6a4cbfde947b04
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:42:32 +0800

    add 37100.lua

---
+++ 37100.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37100.lua
+--Gamename Aztaka
+addappid(37100) --Mainappid Aztaka
+addappid(37101, 1, "7e21d5b74fbb3be165d832d3e7fe15292f2d9b3cc17a68e18eb0a2e3de0c5809") --Main Windows Depot Aztaka
