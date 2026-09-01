66f41d380e4445b0e1278b36362525c1ff7f1562
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:24:58 +0800

    add 9300.lua

---
+++ 9300.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9300.lua
+--Gamename Company of Heroes Singleplayer Demo
+addappid(9300) --Mainappid Company of Heroes Singleplayer Demo
+addappid(9301, 1, "531775759ea5db6b13ecd0cdd663edb94ea69e8b1939797035c41ea5533bf811") --Main Windows Depot Company of Heroes Singleplayer Demo
+setManifestid(9301, "3923944702831562025", 0)
+addappid(9302, 1, "fe202822cc3180554e846261cdafe5c7dca28b97d3ed04c5bc91d9501148d26b") --Main Windows Depot Company of Heroes Singleplayer Demo
+setManifestid(9302, "1314715809371895066", 0)
