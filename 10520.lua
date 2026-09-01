3b8e173f090d0ed4265b97e292980839a7abed61
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:29:49 +0800

    add 10520.lua

---
+++ 10520.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10520.lua
+--Gamename Beijing 2008™ - The Official Video Game of the Olympic Games
+addappid(10520) --Mainappid Beijing 2008™ - The Official Video Game of the Olympic Games
+addappid(10521, 1, "d39c39febbdf90e42b7f739d01e526038418fb7ac6e2d3976d1ebaa7419761dc") --Main Windows Depot Beijing 2008™ - The Official Video Game of the Olympic Games
+setManifestid(10521, "8729699854468853678", 0)
