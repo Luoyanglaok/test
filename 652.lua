2647278cbf7fa9b2ea18afd19d61684932e66ce1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:47:23 +0800

    add 652.lua

--- 652.lua
+++ 652.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 652.lua
-addappid(652, 0, "c7f0d10b15e3b93f569b05903043842b268011f704484b7c3c678bb47149716d")
+addappid(652, 0, "f68c7839c5b5762255cf77b38294d06f7899e4153583a5a34abb549607c4cb53")
