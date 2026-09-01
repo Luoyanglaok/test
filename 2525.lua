a0c0900cbd6ec1ae0a6fbaf04c447f8f91115df2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:22:31 +0800

    add 2525.lua

---
+++ 2525.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2525.lua
+--Gamename Gumboy Crazy Features
+addappid(2525) --Mainappid Gumboy Crazy Features
+addappid(2524, 1, "ccf62f32972e62931348b901aabdf0563387658426a641088e6f76529fd9db1c") --Main Windows Depot Gumboy Crazy Features
+setManifestid(2524, "8922207881111331691", 0)
+addappid(2526, 1, "5e4c7abd5f9192f99bd6941cd2d9260da1345b47dd05b27235744ec1b5f4fbac") --Main Windows Language Depot Polish Gumboy Crazy Features
+setManifestid(2526, "8876506559584917987", 0)
+addappid(2527, 1, "9bfca447d1759b97b59326d46d69540e43cd1750fbf04c3eba1905b711982250") --Main Windows Language Depot Russian Gumboy Crazy Features
+setManifestid(2527, "5041173822604299429", 0)
