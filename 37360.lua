9bad1701c7aabcde68a6e82cde8bb6d71f721e0b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:43:57 +0800

    add 37360.lua

---
+++ 37360.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37360.lua
+--Gamename Mahjong Roadshow™
+addappid(37360) --Mainappid Mahjong Roadshow™
+addappid(37361, 1, "25400c3ce02f1cb27ec710541e250636838b64ab5028f05e17469868b73f6bf3") --Main Windows Depot Mahjong Roadshow™
+setManifestid(37361, "6682743243868974256", 0)
