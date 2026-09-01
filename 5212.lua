715ee74728155b69050aad5b83113787640abef9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:23:30 +0800

    add 5212.lua

---
+++ 5212.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5212.lua
+--Gamename Wanted Teaser
+addappid(5212) --Mainappid Wanted Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Wanted Teaser
+setManifestid(910, "1332084816209327708", 0)
