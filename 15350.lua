814a53035bd1384b2e388f827dff20bce71961b8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:38:22 +0800

    add 15350.lua

---
+++ 15350.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15350.lua
+--Gamename Brain Spa
+addappid(15350) --Mainappid Brain Spa
+addappid(15351, 1, "520a7f555ad45fb9bc823b7ec11637294c055212de021e9a61c6a86a15af0871") --Main Windows Depot Brain Spa
+setManifestid(15351, "7072022877920714743", 0)
