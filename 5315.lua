93974cd82b0dce28d23290626571a8ffe02c5a9d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:31:02 +0800

    add 5315.lua

---
+++ 5315.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5315.lua
+--Gamename X3 Terran Conflict 2.0 Trailer
+addappid(5315) --Mainappid X3 Terran Conflict 2.0 Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot X3 Terran Conflict 2.0 Trailer
+setManifestid(910, "1332084816209327708", 0)
