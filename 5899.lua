388e001efdce794cb4ff2f0aabf5b7853d6723a2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:08:47 +0800

    add 5899.lua

---
+++ 5899.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5899.lua
+--Gamename Patrician IV Teaser
+addappid(5899) --Mainappid Patrician IV Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Patrician IV Teaser
+setManifestid(910, "1332084816209327708", 0)
