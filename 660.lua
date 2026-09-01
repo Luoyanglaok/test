0a61e8663c842c2fa0c0aec9d6fb57545220d172
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:56:34 +0800

    add 660.lua

--- 660.lua
+++ 660.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 660.lua
-addappid(660, 0, "9e9898c09e1fc9a3c56e45815d07e34710dcc6b976021346e82171de86284a8a")
+addappid(660, 0, "f37c1a3f7280b12824024cc1d66a051476898554500b8ed50b05c459995c9bef")
