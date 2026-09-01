5589d4331cae7e8088ee967a714e167c48091fda
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:41:14 +0800

    add 36180.lua

---
+++ 36180.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 36180.lua
+--Gamename Cake Mania
+addappid(36180) --Mainappid Cake Mania
+addappid(36181, 1, "75dae49a035b521b62de6445b192e9e39304c435c01a2b52583e722c6de7e59b") --Main Windows Depot Cake Mania
