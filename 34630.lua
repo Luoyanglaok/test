2a9d5062bf66388acfaa75f038df066f12a8c5b4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:32:14 +0800

    add 34630.lua

---
+++ 34630.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34630.lua
+--Gamename Massive Assault
+addappid(34630) --Mainappid Massive Assault
+addappid(34631, 1, "4778d557e373b98c8dae5f721c5377517b9e55b07f86355b69910bc5a4871060") --Main Windows Depot Massive Assault
+setManifestid(34631, "2893931564860063738", 0)
