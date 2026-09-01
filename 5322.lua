1dd4d94e5dc8d59427265c03689cd37348b3d103
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:31:58 +0800

    add 5322.lua

---
+++ 5322.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5322.lua
+--Gamename Divinity 2 - Trailer 1 (English)
+addappid(5322) --Mainappid Divinity 2 - Trailer 1 (English)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Divinity 2 - Trailer 1 (English)
+setManifestid(910, "1332084816209327708", 0)
