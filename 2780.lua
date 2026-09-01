bd1de81399e0966d9db4ed6bff823bd12e351a26
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:24:26 +0800

    add 2780.lua

---
+++ 2780.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2780.lua
+--Gamename ARMA: Combat Operations
+addappid(2780) --Mainappid ARMA: Combat Operations
+addappid(2781, 1, "9446ac5961bbcc22d77b725179763f290ab494b9e9e7b5ba1a5ef8c355317ad5") --Main Windows Depot ARMA: Combat Operations
+setManifestid(2781, "6854069796673810862", 0)
+addappid(2782, 1, "e468636c3d3155fe7f1c14be39b8f6608211f00abca62ed0772f761e61bc4913") --Main Windows Depot ARMA: Combat Operations
+setManifestid(2782, "7950207257683185050", 0)
+addappid(2783, 1, "001cd91afa24f0029d06023e4108f9c7f6d4f9b6dfd86fef5835456e62c59ee8") --Main Windows Depot ARMA: Combat Operations
+setManifestid(2783, "9064131329186873909", 0)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Main Windows Depot ARMA: Combat Operations
+addappid(200460) --Dlcname AppID 200460
