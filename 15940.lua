e27d7531d2967a7a6b6c519ab7dc2f6c6630b903
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:40:30 +0800

    add 15940.lua

---
+++ 15940.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15940.lua
+--Gamename The Office
+addappid(15940) --Mainappid The Office
+addappid(15941, 1, "68418d568c9ba08c18241a84a70c6b42ec32912188fe61932a04aa74cbd75995") --Main Windows Depot The Office
+setManifestid(15941, "245802633863176637", 0)
