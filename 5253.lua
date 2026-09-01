9e403111d7960eaadfc407c2c1d35ea088eef906
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:25:38 +0800

    add 5253.lua

---
+++ 5253.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5253.lua
+--Gamename Call of Duty: World at War - Map Pack
+addappid(5253) --Mainappid Call of Duty: World at War - Map Pack
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Call of Duty: World at War - Map Pack
+setManifestid(910, "1332084816209327708", 0)
