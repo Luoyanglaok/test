43f402a9eac6da01b8eb6ea8fc54aa4ff8962731
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:06:21 +0800

    add 570.lua

--- 570.lua
+++ 570.lua
@@ -1,42 +1,100 @@
-addappid(570)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(373301,0,"279c88be13d9b20cc225ac6a493938250bd3c466a10be0510ee63f3fee466e59")
-setManifestid(373301,"7327918771545452700")
-addappid(381451,0,"c45d21d2567e6bbbaabd5d8c510cfc3df5c51ba614e8147758361b095d4fa385")
-setManifestid(381451,"3099179070422680664")
-addappid(381452,0,"dbca512c4195d0acb381cb58961227a2389591eb8ef830fe0b2075034915429f")
-setManifestid(381452,"5559429930528020871")
-addappid(381453,0,"7579e5293fe4e936f8c9e91699ad8331d317aef3fd772926c1fc5f6b3775d15e")
-setManifestid(381453,"5723651394550524659")
-addappid(381454,0,"dad51b312f4226160bc7d83b4e5959f6b98aeccd7a2fd96040dc2bd91e93e006")
-setManifestid(381454,"3653281149075627225")
-addappid(381455,0,"bce388e5d35bce02105c0a8de97259e0187df8220f13994a85546241c5cb46c7")
-setManifestid(381455,"1379877685178272800")
-addappid(373302,0,"1bfcb4b8a591a7dcde8ebae166815a3881252a90420d8d86a6925d63ec728e1c")
-setManifestid(373302,"2258884513790295561")
-addappid(373303,0,"005943def146b997e82e3d47d24cc7aeb993a35d3b5667b7085d22adc883af08")
-setManifestid(373303,"2518563807909310866")
-addappid(373304,0,"5aab0fdef9c8de1f871c67c0de61014bf541b726392f89caf1a288764b277edf")
-setManifestid(373304,"7166575635598811404")
-addappid(373305,0,"322d1a4f7c1a278ebac8dd32abc6c68e3846c750840335829767f5bb61a9c20a")
-setManifestid(373305,"8903775600571334631")
-addappid(373306,0,"84158152dd9c4100de7a63a6d8a0e78fed18291e599a5c533c3303b32399d45e")
-setManifestid(373306,"5764620914835367791")
-addappid(373307,0,"11f3d9ff5db607675832505f1db661d511d7379fef7896eb2e12bff54a1655a4")
-setManifestid(373307,"2636379485241555173")
-addappid(373308,0,"8624a7548d76e91640c6d312115a0d42d3a7eef930123df8888a9f1bd2d1349f")
-setManifestid(373308,"8126429463115692983")
-addappid(373309,0,"84260bb289a869883362af02ffccd7a1cd6fa16337f747bff65ab7bfb576329b")
-setManifestid(373309,"8243194691725980100")
-addappid(381456,0,"9f5ad847483a839ed9baf76434be1ddc183cb997433d124eb61480b1ff847144")
-setManifestid(381456,"8954184990626614529")
-addappid(381450,0,"23009db3578e9bd5f750f2cbcfe897efdf07572b455ec53bb0cd02d9d09f0a39")
-setManifestid(381450,"2086719151889130007")
-addappid(401531,0,"77d161ec79f984f0fcd75bf5448aaca4def24808131a932e75bbbe3265458a08")
-setManifestid(401531,"3442758096700779883")
-addappid(401535,0,"3a8d99407e5e63bcf7357af8a2262fa8228b5eaa6e8dca73d9a65ceda0fb7115")
-setManifestid(401535,"818256195465990382")
-addappid(401536,0,"0c3e321b5203db650a2905168dc251639fd5c398f304de8e02a3b862b782f2e0")
-setManifestid(401536,"8626830787987928686")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 570.lua
+--Gamename Dota 2
+addappid(570, 1, "d1c0e5cee633870d25ece907f9836bb6005a09ac6442ac0228f3667ffca7ca78") --Mainappid Dota 2
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Main Windows Depot Dota 2
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Dota 2
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Dota 2
+addappid(373301, 1, "279c88be13d9b20cc225ac6a493938250bd3c466a10be0510ee63f3fee466e59") --Main Windows Depot Dota 2
+setManifestid(373301, "3761617624612040941", 3200021408)
+addappid(373302, 1, "1bfcb4b8a591a7dcde8ebae166815a3881252a90420d8d86a6925d63ec728e1c") --Main Windows Depot Dota 2
+setManifestid(373302, "6949368311956215785", 130601984)
+addappid(373303, 1, "005943def146b997e82e3d47d24cc7aeb993a35d3b5667b7085d22adc883af08") --Main Windows Depot Dota 2
+setManifestid(373303, "1528413665739005935", 192834976)
+addappid(373304, 1, "5aab0fdef9c8de1f871c67c0de61014bf541b726392f89caf1a288764b277edf") --Main Macos Depot Dota 2
+setManifestid(373304, "5513073761855626240", 293643568)
+addappid(373305, 1, "322d1a4f7c1a278ebac8dd32abc6c68e3846c750840335829767f5bb61a9c20a") --Main Windows Depot Dota 2
+setManifestid(373305, "4435851250675935801", 0)
+addappid(373306, 1, "84158152dd9c4100de7a63a6d8a0e78fed18291e599a5c533c3303b32399d45e") --Main Linux Depot Dota 2
+setManifestid(373306, "3993506464080429043", 314402608)
+addappid(373307, 1, "11f3d9ff5db607675832505f1db661d511d7379fef7896eb2e12bff54a1655a4") --Main Windows Depot Dota 2
+setManifestid(373307, "7660638834009368395", 99325008)
+addappid(373308, 1, "8624a7548d76e91640c6d312115a0d42d3a7eef930123df8888a9f1bd2d1349f") --Main Windows Language Depot Koreana Dota 2
+setManifestid(373308, "8126429463115692983", 727964672)
+addappid(373309, 1, "84260bb289a869883362af02ffccd7a1cd6fa16337f747bff65ab7bfb576329b") --Main Windows Language Depot Schinese Dota 2
+setManifestid(373309, "261442499468665220", 3681416896)
+addappid(381450, 1, "23009db3578e9bd5f750f2cbcfe897efdf07572b455ec53bb0cd02d9d09f0a39") --Main Windows Depot Dota 2
+setManifestid(381450, "3284923945180297004", 1629667200)
+addappid(381451, 1, "c45d21d2567e6bbbaabd5d8c510cfc3df5c51ba614e8147758361b095d4fa385") --Main Windows Depot Dota 2
+setManifestid(381451, "1644205904362630105", 948348208)
+addappid(381452, 1, "dbca512c4195d0acb381cb58961227a2389591eb8ef830fe0b2075034915429f") --Main Windows Depot Dota 2
+setManifestid(381452, "1790151512922507464", 1317209360)
+addappid(381453, 1, "7579e5293fe4e936f8c9e91699ad8331d317aef3fd772926c1fc5f6b3775d15e") --Main Windows Depot Dota 2
+setManifestid(381453, "2098768841322990910", 1174250512)
+addappid(381454, 1, "dad51b312f4226160bc7d83b4e5959f6b98aeccd7a2fd96040dc2bd91e93e006") --Main Windows Depot Dota 2
+setManifestid(381454, "3653281149075627225", 858336640)
+addappid(381455, 1, "bce388e5d35bce02105c0a8de97259e0187df8220f13994a85546241c5cb46c7") --Main Windows Depot Dota 2
+setManifestid(381455, "7802627126899494189", 27411778704)
+addappid(381456, 1, "9f5ad847483a839ed9baf76434be1ddc183cb997433d124eb61480b1ff847144") --Main Windows Language Depot Russian Dota 2
+setManifestid(381456, "8954184990626614529", 531016032)
+addappid(401531, 1, "77d161ec79f984f0fcd75bf5448aaca4def24808131a932e75bbbe3265458a08") --Main Windows Depot Dota 2
+setManifestid(401531, "3442758096700779883", 80)
+addappid(401535, 1, "3a8d99407e5e63bcf7357af8a2262fa8228b5eaa6e8dca73d9a65ceda0fb7115") --Main Windows Depot Dota 2
+setManifestid(401535, "1990577232290008990", 181173328)
+addappid(401536, 1, "0c3e321b5203db650a2905168dc251639fd5c398f304de8e02a3b862b782f2e0") --Main Windows Depot Dota 2
+setManifestid(401536, "8626830787987928686", 0)
+addappid(576) --Dlcname Dota 2 - SteelSeries DLC
+addappid(581) --Dlcname Dota 2 - Murrissey the Smeevil
+addappid(582) --Dlcname Dota 2 - Lockjaw the Boxhound
+addappid(583) --Dlcname Dota 2 - Drodo the Druffin
+addappid(584) --Dlcname Dota 2 - Inflatable Donkey Courier
+addappid(585) --Dlcname Dota 2 - Plush Donkey Courier
+addappid(586) --Dlcname Dota 2 - International Axe
+addappid(218571) --Dlcname Dota 2 - Axe Shirt
+addappid(218572) --Dlcname Dota 2 - Axe shirt Jinx
+addappid(218573) --Dlcname Dota 2 - Drow shirt
+addappid(218574) --Dlcname Dota 2 - Drow shirt Jinx
+addappid(218575) --Dlcname AppID 218575
+addappid(218576) --Dlcname AppID 218576
+addtoken(218576, "14740863126040210741")
+addappid(218577) --Dlcname AppID 218577
+addappid(218578) --Dlcname AppID 218578
+addappid(218579) --Dlcname Dota 2 - Portal 2 Soundtrack Bundle
+addappid(218582) --Dlcname Dota 2 - Winter Sale 2012
+addappid(313250) --Dlcname AppID 313250
+addtoken(313250, "7007036278857765184")
+addappid(318940) --Dlcname Dota 2 - Premium DLC
+addappid(373300) --Dlcname AppID 373300
+addtoken(373300, "7620803809741004454")
+addappid(401530) --Dlcname Dota 2 - OpenGL support for Windows
+addappid(476580) --Dlcname AppID 476580
+addtoken(476580, "12051381034871427382")
+addappid(480610) --Dlcname AppID 480610
+addappid(539760) --Dlcname AppID 539760
+addtoken(539760, "3069829048022854314")
+addappid(633890) --Dlcname AppID 633890
+addappid(635080) --Dlcname AppID 635080
+addappid(652720) --Dlcname The Dota 2 Remixes EP
+addtoken(652720, "1840098022108016194")
+addappid(665530) --Dlcname The International 2017 Battle Level Bundle
+addappid(859040) --Dlcname The International 2018 Battle Pass - Level 1
+addappid(859041) --Dlcname The International 2018 Battle Pass - Level 75
+addappid(1082350) --Dlcname The International 2019 Battle Pass
+addappid(1082351) --Dlcname The International 2019 Battle Pass - Level 50
+addappid(1082352) --Dlcname The International 2019 Battle Pass - Level 100
+addappid(1082353) --Dlcname The International 2019 Battle Level Bundle
+addappid(1082354) --Dlcname The International 10 Battle Pass
+addappid(1082355) --Dlcname AppID 1082355
+addappid(1082356) --Dlcname AppID 1082356
+addappid(1082357) --Dlcname Nemestice 2021 Battle Pass
+addappid(1082358) --Dlcname AppID 1082358
+addappid(1082359) --Dlcname AppID 1082359
+addappid(1126980) --Dlcname AppID 1126980
+addtoken(1126980, "10471891316556182886")
+addappid(1126981) --Dlcname AppID 1126981
+addtoken(1126981, "13253038459313459640")
+addappid(1241930) --Dlcname The DOTA 2 Official Soundtrack
+addtoken(1241930, "9867088503339864765")
+addappid(1838090) --Dlcname The Aghanim’s Labyrinth Battle Pass
+addappid(1838091) --Dlcname AppID 1838091
+addappid(1838092) --Dlcname AppID 1838092
