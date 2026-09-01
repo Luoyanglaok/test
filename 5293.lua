5d799add7a1c5cf7b506d27d9bc47e519a2dcd70
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:28:59 +0800

    add 5293.lua

---
+++ 5293.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5293.lua
+--Gamename ARMA 2 USMC Trailer
+addappid(5293) --Mainappid ARMA 2 USMC Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot ARMA 2 USMC Trailer
+setManifestid(910, "1332084816209327708", 0)
