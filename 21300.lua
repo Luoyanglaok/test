3d63a891f9be9995fc5bc666eda3989bf8c5f7bc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:54:49 +0800

    add 21300.lua

---
+++ 21300.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21300.lua
+--Gamename Gravitron 2
+addappid(21300) --Mainappid Gravitron 2
+addappid(21301, 1, "9dfc6d3e487cfb85054c7e5bde2504a8fcdbfa9b5be326d5b2e7a9fca90126f3") --Main Windows Depot Gravitron 2
+setManifestid(21301, "5976210143045546132", 0)
