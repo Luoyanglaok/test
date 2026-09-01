9cf3149ac6145dffea14164fe46021b4d30cc1b1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:38:13 +0800

    add 15330.lua

---
+++ 15330.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15330.lua
+--Gamename The Settlers: Rise of an Empire
+addappid(15330) --Mainappid The Settlers: Rise of an Empire
+addappid(15331, 1, "aa7efa8a4a20de3989d12eebb4b55eda6b41a0523e2e9a18492bb6086cd72b1c") --Main Windows Depot The Settlers: Rise of an Empire
+setManifestid(15331, "3751127306550112642", 0)
+addappid(15332, 1, "2a584135839a9e7d27d59aa5658b2f19aae0109c5cad3ab33df3cc3d7e3fe0b4") --Main Windows Language Depot French The Settlers: Rise of an Empire
+setManifestid(15332, "4773036064343682270", 0)
+addappid(15333, 1, "3bd56a5354c44fa04b7ee8db5aef36dd14c8aa094e5b67e43ff54b6817430c7c") --Main Windows Language Depot Spanish The Settlers: Rise of an Empire
+setManifestid(15333, "1436144815565486985", 0)
