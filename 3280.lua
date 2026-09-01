ebea50abf3c961a06bd70dc06a42cab1f0c05a1c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:26:56 +0800

    add 3280.lua

---
+++ 3280.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3280.lua
+--Gamename Painkiller Overdose Demo
+addappid(3280) --Mainappid Painkiller Overdose Demo
+addappid(3281, 1, "ed11c2d98a00b6b1521cd26cfa1499c14f3aeeacd71e1fdc5b5a600e075a054d") --Main Windows Depot Painkiller Overdose Demo
+setManifestid(3281, "4540658263351738071", 0)
