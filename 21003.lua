7941bae0e607fa21b03ed6ad58b7e6e262389705
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:53:52 +0800

    add 21003.lua

---
+++ 21003.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21003.lua
+--Gamename Steam Store
+addappid(21003, 1, "6d85ee42b0015812987b32922ef66079a69eb2c25046e6c33533b81e7dbcb43a") --Mainappid Steam Store
+setManifestid(21003, "4999633951566687241", 0)
+addappid(21004, 1, "86236dcb06a29123ec6d8b5533ae4c271c88335074e225cf027fda1a356d33c7") --Main Windows Language Depot Spanish Steam Store
+setManifestid(21004, "7397377424455615958", 0)
+--Dlcname LEGO® Batman™: The Videogame
+addappid(21000, 1, "14b34623da65e14be2f92286d0dd2dd5f8673a449b50f1f911492553ef5c6be5") --Dlcname LEGO® Batman™: The Videogame
+addappid(21001, 1, "41f749df849f89a6dfccc2ad784a023add7467c5b5222b04ff7b00baae20ac98") --Dlc Windows Depot LEGO® Batman™: The Videogame
+setManifestid(21001, "5871323374027822051", 0)
+addappid(21002, 1, "63d732c46e22671a3751b82c54652749be5b8d47e4dccb0bd1a1f9e870b4d252") --Dlc Windows Language Depot English LEGO® Batman™: The Videogame
+setManifestid(21002, "302603787906994898", 0)
+addappid(21005, 1, "58b09a0a856a3b01ed7fc4fac702fee674be30f5162d26d8030e7897862ebe3f") --Dlc Macos Depot LEGO® Batman™: The Videogame
+setManifestid(21005, "4019824351870444877", 0)
+addappid(21006, 1, "cdf455be4ee9b4752f8a6231175e6d882313479b9b9bac940b0c08ac3cc39c32") --Dlc Windows Depot LEGO® Batman™: The Videogame
+setManifestid(21006, "1027077621092336532", 0)
+addappid(21007, 1, "b6c97168a1cafde4fada8f1503890edf2a324f7b34a8223590b23825a9b9e68a") --Dlc Windows Depot LEGO® Batman™: The Videogame
+setManifestid(21007, "6690420575247744391", 2408832)
