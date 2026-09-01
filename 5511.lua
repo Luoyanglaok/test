a118130bfc59924ebf1f7d66a616db46b9db329c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:41:06 +0800

    add 5511.lua

---
+++ 5511.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5511.lua
+--Gamename James Camerons Avatar - The Game - Developer Diary 3
+addappid(5511) --Mainappid James Camerons Avatar - The Game - Developer Diary 3
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot James Camerons Avatar - The Game - Developer Diary 3
+setManifestid(910, "1332084816209327708", 0)
