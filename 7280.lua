fdbddfeeb77ac1a88a93ce9ef1305f4712c221b0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:18:22 +0800

    add 7280.lua

---
+++ 7280.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7280.lua
+--Gamename Loki Egyptian Demo
+addappid(7280) --Mainappid Loki Egyptian Demo
+addappid(7281, 1, "103add94c678e425d5d9b85a3fa30abdde7b7abed753777f934af02867abd25a") --Main Windows Depot Loki Egyptian Demo
+setManifestid(7281, "8814909178098550977", 0)
+addappid(7282, 1, "9a0a806cb1d93662824a54d4a71d809dc0f84de542e73f824c27effd46dd6bd3") --Main Windows Depot Loki Egyptian Demo
+setManifestid(7282, "2022867562669038997", 0)
