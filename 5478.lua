9e77a40b40ab831f783007a861f3b9f9d88593d0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:39:25 +0800

    add 5478.lua

---
+++ 5478.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5478.lua
+--Gamename Command and Conquer 3 Tiberium Wars Trailer
+addappid(5478) --Mainappid Command and Conquer 3 Tiberium Wars Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Command and Conquer 3 Tiberium Wars Trailer
+setManifestid(910, "1332084816209327708", 0)
