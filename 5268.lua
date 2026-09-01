dcc52e0e2735b42439555b20445e275c66476bd2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:26:56 +0800

    add 5268.lua

---
+++ 5268.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5268.lua
+--Gamename Left 4 Dead 2: E3 Teaser
+addappid(5268) --Mainappid Left 4 Dead 2: E3 Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Left 4 Dead 2: E3 Teaser
+setManifestid(910, "1332084816209327708", 0)
