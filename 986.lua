52ad4dcb696123407cc230dfbfbff35c6946dc65
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:48:55 +0800

    add 986.lua

---
+++ 986.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 986.lua
+--Gamename Race07 Trailer
+addappid(986) --Mainappid Race07 Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Race07 Trailer
+setManifestid(910, "1332084816209327708", 0)
+addappid(987, 1, "8bafa43775fa15ba72ae82eff4c4e36e42fa9a6701b4ee877ffb9e6db3206442") --Main Windows Depot Race07 Trailer
