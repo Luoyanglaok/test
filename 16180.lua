279e6d845d895b30257f14e46583531cbd1a0fb0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:41:46 +0800

    add 16180.lua

---
+++ 16180.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16180.lua
+--Gamename Virtual Villagers - The Secret City
+addappid(16180) --Mainappid Virtual Villagers - The Secret City
+addappid(16181, 1, "51158830e4e1a11fabbe0702a179d572a13362ea1ff3181c02350ab92450e70a") --Main Windows Depot Virtual Villagers - The Secret City
+setManifestid(16181, "6537836562402652536", 0)
