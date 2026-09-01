fb2996d4cfcbc99689095daf912a473f3426ae3c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:26:06 +0800

    add 3160.lua

---
+++ 3160.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3160.lua
+--Gamename A Farewell to Dragons
+addappid(3160) --Mainappid A Farewell to Dragons
+addappid(3161, 1, "650c584a7862d5ad820fd03275c56e570bcb31449a56aa15e20ec25294069a3b") --Main Windows Depot A Farewell to Dragons
+setManifestid(3161, "5921005195378108082", 0)
+addappid(3162, 1, "6dd26edd6e9b415dc03f8bce47d843c0d781b9bf236dd280841a56c816996ae7") --Main Windows Language Depot Russian A Farewell to Dragons
+setManifestid(3162, "5398450677200127409", 0)
