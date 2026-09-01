182c3ad88d348ced39b0ffec7bb306cf0b9a3875
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:16:21 +0800

    add 5125.lua

---
+++ 5125.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5125.lua
+--Gamename Far Cry 2: Whos Who
+addappid(5125) --Mainappid Far Cry 2: Whos Who
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Far Cry 2: Whos Who
+setManifestid(910, "1332084816209327708", 0)
