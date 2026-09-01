a01e715995e6cc7c91d005f655af99a4386389b9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:56:23 +0800

    add 21930.lua

---
+++ 21930.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21930.lua
+--Gamename Tom Clancy's H.A.W.X. - Demo
+addappid(21930) --Mainappid Tom Clancy's H.A.W.X. - Demo
+addappid(21931, 1, "5a20f054aed03c4062d75e492cf3c60b85c5e41f010f8b51873516b06ac7f8cf") --Main Windows Depot Tom Clancy's H.A.W.X. - Demo
+setManifestid(21931, "3061887499413659141", 0)
