d9908d324df8522454a4f5092a460de4cab85b05
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:37:43 +0800

    add 15260.lua

---
+++ 15260.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15260.lua
+--Gamename Chessmaster: Grandmaster Edition
+addappid(15260) --Mainappid Chessmaster: Grandmaster Edition
+addappid(15261, 1, "d6f1aad3f4c7272b3bc39aa7fd40a75418cfacf857d1798bbf00bd8a30a37db5") --Main Windows Depot Chessmaster: Grandmaster Edition
+setManifestid(15261, "4418186591797155251", 0)
+addappid(15262, 1, "0751c50dba7f662d6373af57db291f8e18f714d925049e48db154a4af2dde285") --Main Windows Language Depot French Chessmaster: Grandmaster Edition
+setManifestid(15262, "7443316801313775277", 0)
