509fbffa73e2cc5dd973431c558db1a0bb682291
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:54:01 +0800

    add 21020.lua

---
+++ 21020.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21020.lua
+--Gamename Watchmen: The End Is Nigh Demo
+addappid(21020) --Mainappid Watchmen: The End Is Nigh Demo
+addappid(21021, 1, "b162b48b8b9f23e0c2e5e020cfac99a808f56d78aa4543a35b82eb13d1b2a65c") --Main Windows Depot Watchmen: The End Is Nigh Demo
+setManifestid(21021, "7850910209521974226", 0)
