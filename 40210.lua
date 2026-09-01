abe79b049c06602ff6e7da6067d22180f01828f5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:49:39 +0800

    add 40210.lua

---
+++ 40210.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 40210.lua
+--Gamename ItzaZoo
+addappid(40210) --Mainappid ItzaZoo
+addappid(40211, 1, "cc9f638f3d542d7f351c3e48729606b5322f645dc6d444d7b64087e4a8c27572") --Main Windows Depot ItzaZoo
+setManifestid(40211, "3997558430825975580", 0)
