b0004f9178ea391fb505e596c28e344f20e64a1f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:00:10 +0800

    add 5799.lua

---
+++ 5799.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5799.lua
+--Gamename Front Mission Evolved E3 Trailer
+addappid(5799) --Mainappid Front Mission Evolved E3 Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Front Mission Evolved E3 Trailer
+setManifestid(910, "1332084816209327708", 0)
