4796d3874b08cf2bb2a6212eccace6150618fc96
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:21:54 +0800

    add 33510.lua

---
+++ 33510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33510.lua
+--Gamename Officers: World War II
+addappid(33510) --Mainappid Officers: World War II
+addappid(33511, 1, "596aa102037f605012b5103e5cdb8fb53e99d548bb64dd839bc78e1423a65a87") --Main Windows Depot Officers: World War II
+setManifestid(33511, "4319558498184467567", 0)
