f98077fe08628f43ac7e1df7464c51bae4acb7c0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:36:32 +0800

    add 5388.lua

---
+++ 5388.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5388.lua
+--Gamename Heroes Over Europe - Multiplayer Trailer
+addappid(5388) --Mainappid Heroes Over Europe - Multiplayer Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Heroes Over Europe - Multiplayer Trailer
+setManifestid(910, "1332084816209327708", 0)
