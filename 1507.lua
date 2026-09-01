bc50faa777724c8ad5d0053b1303006d837535a6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:58:17 +0800

    add 1507.lua

--- 1507.lua
+++ 1507.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 1507.lua
-addappid(1507, 0, "62be7a019fc136709d1960632069f6175d87cc9c2e56ef7afe97c3bfe49d60b6")
+addappid(1507, 0, "a1285e33db47fd694263c76c70eaa6299c40b4d835622de9e2529a2eb9a1b457")
