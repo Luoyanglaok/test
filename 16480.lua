8e8935724ea2e8924ff750b93ff5c447536de724
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:42:22 +0800

    add 16480.lua

---
+++ 16480.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16480.lua
+--Gamename F.E.A.R. 2: Project Origin JP
+addappid(16480) --Mainappid F.E.A.R. 2: Project Origin JP
+addappid(16481, 1, "a5c16ddb73dbb4d7f8d4c1f17cc5bc64aa90ada1bcc6809650460cfd26bb2d44") --Main Windows Depot F.E.A.R. 2: Project Origin JP
+setManifestid(16481, "8051457159537185674", 0)
+addappid(16482, 1, "ac468652e2f8fcee53da5ede43909adb13974f363000933621b1e39fec69ac2d") --Main Windows Depot F.E.A.R. 2: Project Origin JP
+setManifestid(16482, "6518262916618976774", 0)
