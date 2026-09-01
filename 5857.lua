14c70e8874e5f1736558b430d48ccafc0cc84447
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:05:14 +0800

    add 5857.lua

---
+++ 5857.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5857.lua
+--Gamename Kane & Lynch 2 - Focus on the Job (IT)
+addappid(5857) --Mainappid Kane & Lynch 2 - Focus on the Job (IT)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Kane & Lynch 2 - Focus on the Job (IT)
+setManifestid(910, "1332084816209327708", 0)
