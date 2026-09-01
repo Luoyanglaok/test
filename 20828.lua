e4894fc510f081e7d0bc6a767dfcfb6f9c831722
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:53:22 +0800

    add 20828.lua

---
+++ 20828.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 20828.lua
+--Gamename Shatter - Original Soundtrack
+addappid(20828) --Mainappid Shatter - Original Soundtrack
+addappid(20829, 1, "f2bca478a27edc34dbe66cd3b408d1b4b1ef96505662a591cc80a7decc6425d2") --Main Windows Depot Shatter - Original Soundtrack
