725c6122b9cf400ff2a75c5040cf2c7fe1620bbf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:35:53 +0800

    add 12660.lua

---
+++ 12660.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12660.lua
+--Gamename Miss Popularity
+addappid(12660) --Mainappid Miss Popularity
+addappid(12661, 1, "c4f37b8cb77627dcba5d3a6e29b4839c87c9de1c7a86eeb820c31b2863488548") --Main Windows Depot Miss Popularity
+setManifestid(12661, "4878743558575289654", 0)
