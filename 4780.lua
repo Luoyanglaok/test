875120b9b78a3c94175090f2032126a03027d31f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:32:11 +0800

    add 4780.lua

---
+++ 4780.lua
@@ -0,0 +1,32 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4780.lua
+--Gamename Medieval II: Total War™ Kingdoms
+addappid(4780) --Mainappid Medieval II: Total War™ Kingdoms
+addappid(4701, 1, "a11ab5cc8b05d2504d4750a44662bd53c68a369b9169450a43abdc68d71f1df9") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(4702, 1, "b6056e40be63c6294ad3aac31e1d4a0ad8bc79bbb996f413e68822a9492d406b") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(4703, 1, "d598e906c0da1f405218e95f3783ab4f13c4695d077d0901dca7ca51a5522069") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(4704, 1, "44bd28b86c4b4133c695bbd55424a0a1748bbb319aa4a828a3c0a6e033577e3d") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(4705, 1, "aad8473adf26b9b7c318b33d1300152356d16f7875bf27e488ad6cdb24a41205") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(4706, 1, "e946c8351295178b6513be5b6064df81e5ad0bde0dcc35a94a1b6c79f0639ba4") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(4707, 1, "a5c712f250de5294a9c592851617354ad2759b21f8df06c416f57978e05897e3") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(4708, 1, "8bd3bf9367fcc0a4791e8751c57422395f908478f3bb180fcf7f08528fd0c7e4") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(4781, 1, "0aefad813b96543def357268d45103b06d3d4f3a01dd815efbfdcd19293c7d26") --Main Windows Depot Medieval II: Total War™ Kingdoms
+setManifestid(4781, "5925923973988402922", 0)
+addappid(4782, 1, "d8a0b643b7b1c50e354ecc8133135854c777d26a6d59a12568e55afd8eb13c88") --Main Windows Depot Medieval II: Total War™ Kingdoms
+setManifestid(4782, "8719040043663002357", 0)
+addappid(4783, 1, "db730ab33a07fb3a0ef60266b8bc1c1662d052721a3c80bfd98eeda4d062e1d1") --Main Windows Depot Medieval II: Total War™ Kingdoms
+setManifestid(4783, "8274108944591298343", 0)
+addappid(4784, 1, "091ae59f5797b60750952849608a04097e1fea4084ef9402e9c43cdc376ed4b2") --Main Windows Depot Medieval II: Total War™ Kingdoms
+setManifestid(4784, "7036624566569314994", 0)
+addappid(305550, 1, "99b2a2a73340b958a7ffd54075ddd9c938c4abe1564100d3ca4d45195b6a57e6") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(305551, 1, "70a2ad26abbb534cd72c58a30428995976f5a16f02376dfa11098715034f4251") --Main Windows Language Depot English Medieval II: Total War™ Kingdoms
+addappid(305552, 1, "bce1356453018d887991286685fcbb9a84900f8385f18c994ea61656928f92b3") --Main Windows Language Depot French Medieval II: Total War™ Kingdoms
+addappid(305553, 1, "eb0339ae4a8e85e82c1efd5ec46cc622a2522aa68e16de08c12f0c84911dfc73") --Main Windows Language Depot Italian Medieval II: Total War™ Kingdoms
+addappid(305554, 1, "06b5f5a9919553431fe3ef8223d905632a26c00d1db87892aeb246e9384e7ecb") --Main Windows Language Depot German Medieval II: Total War™ Kingdoms
+addappid(305555, 1, "5f2609231c34316070f792708c69c33385a6c4c533e545c8aa61168c20e9d89b") --Main Windows Language Depot Spanish Medieval II: Total War™ Kingdoms
+addappid(305560, 1, "f85651966d56043e57f3f04d46fb981fa314db39d204c1d78822b527a4b0b1bc") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(305561, 1, "69c547fde236d9c4b3ee9cc2c6e484afbf48f2f8ce0ce8386c5dfe1cfe1aadb1") --Main Windows Depot Medieval II: Total War™ Kingdoms
+addappid(305562, 1, "aca67e4550012a83407f9eb4500dc4d7a4c3fc4245f4a4c3889fce50cd767cc9") --Main Windows Language Depot French Medieval II: Total War™ Kingdoms
+addappid(305563, 1, "daea54b81a19c68ce153131d8ce66553e65e153d8c490b54e008554d3c4694d0") --Main Windows Language Depot Italian Medieval II: Total War™ Kingdoms
+addappid(305564, 1, "0b8acd280c2e955e01798364c6f676c2c9d82f4a120e7a9db3f34b479a0ff9c0") --Main Windows Language Depot German Medieval II: Total War™ Kingdoms
+addappid(305565, 1, "10a0556fa8854643756c172e48cf065c6b775c43f241b0a557989026510092e2") --Main Windows Language Depot Spanish Medieval II: Total War™ Kingdoms
