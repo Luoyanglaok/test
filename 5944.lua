37c34dbaf96e00e88e2b88d8feb309c5497c439c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:11:32 +0800

    add 5944.lua

---
+++ 5944.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5944.lua
+--Gamename KB Crossworlds 1
+addappid(5944) --Mainappid KB Crossworlds 1
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot KB Crossworlds 1
+setManifestid(910, "1332084816209327708", 0)
