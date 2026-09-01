18b53283d9c86af3bee97f948a1c12a126c53169
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:45:32 +0800

    add 5648.lua

---
+++ 5648.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5648.lua
+--Gamename Serious Sam HD The Second Encounter Announcement Video
+addappid(5648) --Mainappid Serious Sam HD The Second Encounter Announcement Video
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Serious Sam HD The Second Encounter Announcement Video
+setManifestid(910, "1332084816209327708", 0)
