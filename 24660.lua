9219e2a8a0a7e799a84fff4d1d0e2230aff3c863
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:12:54 +0800

    add 24660.lua

---
+++ 24660.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24660.lua
+--Gamename Trainz: Murchison 2
+addappid(24660) --Mainappid Trainz: Murchison 2
+addappid(24631, 1, "90b92b1630fa1fc26400da330418abc3b721c0d0991fdf7361cb692833868793") --Main Windows Depot Trainz: Murchison 2
+setManifestid(24631, "196455006608489102", 0)
+addappid(24661, 1, "b7edc927fb686ba9269636e1e69e439c043eae1bdbfdaf219c9992c9501021e2") --Main Windows Depot Trainz: Murchison 2
+setManifestid(24661, "460319246807463535", 0)
