87a0f0bf585999c7daea0d2d12ed12ca89656522
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:27:27 +0800

    add 3332.lua

---
+++ 3332.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3332.lua
+--Gamename Zuma Deluxe Demo
+addappid(3332) --Mainappid Zuma Deluxe Demo
+addappid(3331, 1, "ee29d66bb30053c1ff08964ca6b256fc482c3ee0aeeeaa5bca902805859c9af4") --Main Windows Depot Zuma Deluxe Demo
+addappid(3333, 1, "3f2567df36ec438057609c9fbd830c03a461a6553f6ac5b82cf61d8e1da1b9bc") --Main Windows Language Depot German Zuma Deluxe Demo
+addappid(3334, 1, "2f2f369e198f09aa041d8bdef9314836680de2ca9e4bd0b48cd9dab3b4695509") --Main Windows Language Depot French Zuma Deluxe Demo
+addappid(3335, 1, "d0de2859c7047d53964fabcf0e86a762ccb940d7ed05986879785a1004a3b25a") --Main Windows Language Depot Italian Zuma Deluxe Demo
+addappid(3336, 1, "2855ed6624a5b0f9ed48c93d928bed31c14fec354edd7844e2cc3dc351757abc") --Main Windows Language Depot Spanish Zuma Deluxe Demo
