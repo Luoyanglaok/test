bd1e9c3f092c36048f8c6ddf198ac54e8b6cd067
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:25:07 +0800

    add 5241.lua

---
+++ 5241.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5241.lua
+--Gamename RFG Tools of Destruction 4
+addappid(5241) --Mainappid RFG Tools of Destruction 4
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot RFG Tools of Destruction 4
+setManifestid(910, "1332084816209327708", 0)
