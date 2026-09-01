a22033305305460d70248f9f0d42cd22f9e0851f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:40:55 +0800

    add 5503.lua

---
+++ 5503.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5503.lua
+--Gamename James Camerons Avatar - The Game - Navi Gameplay Trailer
+addappid(5503) --Mainappid James Camerons Avatar - The Game - Navi Gameplay Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot James Camerons Avatar - The Game - Navi Gameplay Trailer
+setManifestid(910, "1332084816209327708", 0)
