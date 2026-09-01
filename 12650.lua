acfb7c7284f12bc628aa712306399fedfd032e67
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:35:42 +0800

    add 12650.lua

---
+++ 12650.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12650.lua
+--Gamename Fetch It Again
+addappid(12650) --Mainappid Fetch It Again
+addappid(12651, 1, "24f5d73fce693a9b75d00f5827064fd86da784f2ee8430aacea1f9295b869f36") --Main Windows Depot Fetch It Again
+setManifestid(12651, "8123502746131274021", 0)
