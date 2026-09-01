87134653ee8f80de690721de724f549a7ff63dde
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:14:34 +0800

    add 5981.lua

---
+++ 5981.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5981.lua
+--Gamename Cities XL 2011 - Trailer (French)
+addappid(5981) --Mainappid Cities XL 2011 - Trailer (French)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Cities XL 2011 - Trailer (French)
+setManifestid(910, "1332084816209327708", 0)
