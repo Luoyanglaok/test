e8be5b50d347be4a146a32fac900a08d233177ad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:45:33 +0800

    add 17080.lua

---
+++ 17080.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 17080.lua
+--Gamename Tribes: Ascend
+addappid(17080) --Mainappid Tribes: Ascend
+addappid(17082, 1, "5699fbbe5eb47560883f0d3622221b24ea231d57fff07b41db652d2901a97857") --Main Windows Depot Tribes: Ascend
+setManifestid(17082, "6850038347814825998", 4407810848)
+addappid(17095) --Dlcname Tribes: Ascend - Steam Starter Pack
+addappid(17096) --Dlcname Tribes: Ascend - Steam Premium Pack
+addappid(17097) --Dlcname Tribes: Ascend - Ultimate Weapons Pack
