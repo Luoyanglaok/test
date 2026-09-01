d8f731cf8eaee063e041f179b365be46f08a4a21
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:47:18 +0800

    add 38190.lua

---
+++ 38190.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38190.lua
+--Gamename Romance of Rome
+addappid(38190) --Mainappid Romance of Rome
+addappid(38191, 1, "9986e20a269845d8fa67d66e3b623cf942484ac7476fdd826f8858dccb20057c") --Main Windows Depot Romance of Rome
+setManifestid(38191, "1686909123361245611", 0)
