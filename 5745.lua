cf3697340380d0da0104bdc667ef8770bb0e1b49
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:53:06 +0800

    add 5745.lua

---
+++ 5745.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5745.lua
+--Gamename KL2 Arcade Mode (French)
+addappid(5745) --Mainappid KL2 Arcade Mode (French)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot KL2 Arcade Mode (French)
+setManifestid(910, "1332084816209327708", 0)
