408a1210b9dfb7d3d2217b90e85e2961187c8736
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:45:06 +0800

    add 37990.lua

---
+++ 37990.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37990.lua
+--Gamename Jewel Quest Mysteries
+addappid(37990) --Mainappid Jewel Quest Mysteries
+addappid(37991, 1, "0e0cfca38469cc7440b760d83db489fd2784eeb30e8d359b45a811a217419ef1") --Main Windows Depot Jewel Quest Mysteries
+setManifestid(37991, "8809650493240822275", 0)
