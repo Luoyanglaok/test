d6f450eb10d53e1a7c49e887247ed2b918ed946d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:49:05 +0800

    add 987.lua

---
+++ 987.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 987.lua
+--Gamename Team Fortress 2: Meet The Engineer
+addappid(987, 1, "8bafa43775fa15ba72ae82eff4c4e36e42fa9a6701b4ee877ffb9e6db3206442") --Mainappid Team Fortress 2: Meet The Engineer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Team Fortress 2: Meet The Engineer
+setManifestid(910, "1332084816209327708", 0)
+setManifestid(987, "3953354576832214065", 0)
