851405b46305c1c315849f20cff7dc10d2960183
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:01:00 +0800

    add 24020.lua

---
+++ 24020.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24020.lua
+--Gamename Steam Store
+addappid(24020) --Mainappid Steam Store
+addappid(24021, 1, "8acb61fa3e2d5d1e6a7dcc0f1a53c19552c3dc9424668090b71fdbe48b2cba35") --Main Windows Depot Steam Store
