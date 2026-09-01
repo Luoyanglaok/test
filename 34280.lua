979ee1f35c6afaa7f7de3ef640934546319e04e9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:26:25 +0800

    add 34280.lua

---
+++ 34280.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34280.lua
+--Gamename VectorMan™
+addappid(34280, 1, "ee66d5a7e21be3001440df14214f2469ee7028b253851205f2388aaf1c291e09") --Mainappid VectorMan™
+addappid(34281, 1, "ea5067e3e53af403d91543f3dc8e82553a1f2b79cf194bec9f82e972f28af0eb") --Main Windows Depot VectorMan™
