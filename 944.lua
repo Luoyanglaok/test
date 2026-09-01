ca3f17f8eb88c65478b2ef6d30ecee312fbf317a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:44:33 +0800

    add 944.lua

---
+++ 944.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 944.lua
+--Gamename Left4Dead Teaser
+addappid(944) --Mainappid Left4Dead Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Left4Dead Teaser
+setManifestid(910, "1332084816209327708", 0)
