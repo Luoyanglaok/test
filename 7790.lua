13c692aac7292965b253912fe6f228eee4b7afc6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:20:07 +0800

    add 7790.lua

---
+++ 7790.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7790.lua
+--Gamename NBA 2K9 Demo
+addappid(7790) --Mainappid NBA 2K9 Demo
+addappid(7791, 1, "fc7dd053ffb9d01d84c30a2df52d31b4f31a7b0673d6fcd71ff40057c5854939") --Main Windows Depot NBA 2K9 Demo
+setManifestid(7791, "2557799510825293930", 0)
