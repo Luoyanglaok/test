ccfeea8169ab348f3409ebed44426e9e950fc4d4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:36:17 +0800

    add 210.lua

---
+++ 210.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 210.lua
+--Gamename Source Dedicated Server
+addappid(210) --Mainappid Source Dedicated Server
+addappid(211, 1, "45b0f10ae3a5f73b71eb456eae343bd2318b0966e300401cd0feabc23185d212") --Main Windows Depot Source Dedicated Server
