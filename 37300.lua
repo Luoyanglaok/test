d5edc2c8566f9486835e242b6a9e4b0b1a46d8bf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:43:15 +0800

    add 37300.lua

---
+++ 37300.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37300.lua
+--Gamename DinerTown Tycoon
+addappid(37300) --Mainappid DinerTown Tycoon
+addappid(37301, 1, "175b4ef90b00e9ea7f8c400fbfa9979cab3a67086bb7c8babfcbe890f45cb6e4") --Main Windows Depot DinerTown Tycoon
+setManifestid(37301, "2716353604725498610", 0)
