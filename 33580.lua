ecfbf19fede2bd10e896786e9d7b0c9149a20498
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:22:01 +0800

    add 33580.lua

---
+++ 33580.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33580.lua
+--Gamename The Great Art Race
+addappid(33580) --Mainappid The Great Art Race
+addappid(33581, 1, "0fcaf81ed1d22edaab133c43805946b8c1502c61cf6ae95cfe53e02eadb98f47") --Main Windows Depot The Great Art Race
+setManifestid(33581, "1681894769081304106", 0)
