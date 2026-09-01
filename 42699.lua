b29c891a6177101b056beb516f63dd003de12874
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:55:33 +0800

    add 42699.lua

---
+++ 42699.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42699.lua
+--Gamename Call of Duty: Modern Warfare 3
+addappid(42699) --Mainappid Call of Duty: Modern Warfare 3
+addappid(42700, 1, "f15c6af8ba661ac92e674238a47ffb183335b824a29cdc294e22c832672303a8") --Main Windows Depot Call of Duty: Modern Warfare 3
