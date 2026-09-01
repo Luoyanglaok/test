3e367b909ac17e34196357172bb74c142af7e89c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:34:40 +0800

    add 5345.lua

---
+++ 5345.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5345.lua
+--Gamename CSI Deadly Intent
+addappid(5345) --Mainappid CSI Deadly Intent
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot CSI Deadly Intent
+setManifestid(910, "1332084816209327708", 0)
