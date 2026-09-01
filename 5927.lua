047db347cb3b8246eaf42ff1a0af71b27fefa0a3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:10:20 +0800

    add 5927.lua

---
+++ 5927.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5927.lua
+--Gamename Chime01
+addappid(5927) --Mainappid Chime01
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Chime01
+setManifestid(910, "1332084816209327708", 0)
