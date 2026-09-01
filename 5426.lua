bb77a46cfd8a2ad8ee26ec3141f6a92931858191
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:38:36 +0800

    add 5426.lua

---
+++ 5426.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5426.lua
+--Gamename cities game play
+addappid(5426) --Mainappid cities game play
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot cities game play
+setManifestid(910, "1332084816209327708", 0)
