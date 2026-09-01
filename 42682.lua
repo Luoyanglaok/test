26b0161cda7d73c6d02e5b0f91ea5d41efbded9a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:54:52 +0800

    add 42682.lua

---
+++ 42682.lua
@@ -0,0 +1,26 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42682.lua
+--Gamename Steam Store
+addappid(42682, 1, "a7f0704e4a61083d1ef176c1efe2666a1e694ca4a2e40c17dda9f6c7a9ecceb1") --Mainappid Steam Store
+addappid(42683, 1, "252c3bce60c362e7fdb65dabb24a8f0f6d3098ef341315df1a9f2208c8550c9f") --Main Windows Language Depot English Steam Store
+--Dlcname Call of Duty®: Modern Warfare® 3 (2011)
+addappid(42680, 1, "4bf2d9294f477df1327e22e5efe65d98eca4d4592f0dc59fa451ab8ca8608fde") --Dlcname Call of Duty®: Modern Warfare® 3 (2011)
+addappid(42681, 1, "b7003c5ea7e370881499daaf5ba52ea4f3a1e76843fb37ab4d97753d94df8f95") --Dlc Windows Depot Call of Duty®: Modern Warfare® 3 (2011)
+setManifestid(42681, "5651167211650965131", 0)
+addappid(42684, 1, "7f904784b8e0755738fd4e3a4ade829396e449060bfd09275dd380dcff422703") --Dlc Windows Language Depot Spanish Call of Duty®: Modern Warfare® 3 (2011)
+addappid(42685, 1, "55cf38352373d9669395acabad606ab665e82d0421b28dc2c39003055f91ac5f") --Dlc Windows Language Depot German Call of Duty®: Modern Warfare® 3 (2011)
+addappid(42686, 1, "1a676d3458091a765385fd997ed2171aeb13045296927c26c6a7bdebba7a3504") --Dlc Windows Language Depot French Call of Duty®: Modern Warfare® 3 (2011)
+addappid(42687, 1, "308eb88b20769f215eb317cc8cae34f4e7a4cc74d74e8a04262275dbe41f5eca") --Dlc Windows Language Depot Italian Call of Duty®: Modern Warfare® 3 (2011)
+addappid(42688, 1, "a86e1ec84e71aad3a8c1b3fc2f3a237906eb6de447510da925db20173fb6b724") --Dlc Windows Language Depot Russian Call of Duty®: Modern Warfare® 3 (2011)
+addappid(42689, 1, "5562719fd8e66322c45af3e1b327211c2dd815edd340bd6969a0dac1ebad018f") --Dlc Windows Language Depot Japanese Call of Duty®: Modern Warfare® 3 (2011)
+addappid(42692, 1, "9c172487a1111e79908452b7cbbb680a070c482754b41be716278f44d681586d") --Dlc Windows Language Depot Polish Call of Duty®: Modern Warfare® 3 (2011)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Dlc Windows Depot Call of Duty®: Modern Warfare® 3 (2011)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Dlc Windows Depot Call of Duty®: Modern Warfare® 3 (2011)
+addappid(278310, 1, "c79da57207ccf933258ced455e9793b8ca10fdcd38300181226173ee705fe71b") --Dlc Macos Depot Call of Duty®: Modern Warfare® 3 (2011)
+addappid(278312, 1, "ab950813ce8448c02aad504c486513a86c03aa55bde084c7c6be8f6ca2b71966") --Dlc Macos Language Depot English Call of Duty®: Modern Warfare® 3 (2011)
+addappid(278313, 1, "fac16c89e3e4731296e3017b59a98ba16fa82ac1eca6b432fb0134639fbcc721") --Dlc Macos Language Depot Spanish Call of Duty®: Modern Warfare® 3 (2011)
+addappid(278314, 1, "bba43dbf20aca5543940151e8cf815cc535967d8091a6e2a777a4486bb5410a4") --Dlc Macos Language Depot German Call of Duty®: Modern Warfare® 3 (2011)
+addappid(278315, 1, "ab5bc1898a3c4805ce627fada0b379a4854a291ac920846e1505fcd7ace6b422") --Dlc Macos Language Depot French Call of Duty®: Modern Warfare® 3 (2011)
+addappid(278316, 1, "654ea6858de9296fc3d260a92db3c6c51acfdda38afd672784f6b5907a2b9ab6") --Dlc Macos Language Depot Italian Call of Duty®: Modern Warfare® 3 (2011)
+addappid(278331, 1, "0e8c2e3857ffb4d0cca47f5cf07ccbe8ee9e2ccc66be7ac744d1fa5167745310") --Dlc Macos Depot Call of Duty®: Modern Warfare® 3 (2011)
+setManifestid(278331, "4380871370494427217", 18630608)
