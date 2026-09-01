3326eba19013836f12368e76acc4f282c38e1f61
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:06:03 +0800

    add 4440.lua

---
+++ 4440.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4440.lua
+--Gamename Silverfall Demo
+addappid(4440) --Mainappid Silverfall Demo
+addappid(4441, 1, "b5aefb7cf40ba351dfe07c75a96f4558ccddffe8f855426bc297778b48c58998") --Main Windows Depot Silverfall Demo
+setManifestid(4441, "8795543899015992758", 0)
+addappid(4442, 1, "6526f3739286d593f927d32b7cef15b5e0210e87c6f42821a1bbe23c81bf46a2") --Main Windows Depot Silverfall Demo
+setManifestid(4442, "270387079283349277", 0)
