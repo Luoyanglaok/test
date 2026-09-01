dc09c387310aaa11ed8acb1f69db249b7fe35967
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:32:15 +0800

    add 11170.lua

---
+++ 11170.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 11170.lua
+--Gamename Blood Bowl: Dark Elves Edition
+addappid(11170, 1, "711cd97f98faafbe4bc4fd1c435d881ffadb8d1f108737faafbe59cfd06ce5ba") --Mainappid Blood Bowl: Dark Elves Edition
+addappid(11171, 1, "4a079815a34dd7cd10191d845b03bcf3e3880513f0f5d1bdddb063d707e60713") --Main Windows Depot Blood Bowl: Dark Elves Edition
+setManifestid(11171, "1633565889598334729", 0)
+addappid(11172, 1, "cf22dd4e256961d83974fb926f109d65df9be45e95caa36ff2c9ef598de2c1de") --Main Windows Depot Blood Bowl: Dark Elves Edition
+setManifestid(11172, "3125784489380760808", 0)
+addappid(11173, 1, "5109f5458eb859f64b61d5ff1ff21c395f238227fe3641ab292a02bb9972357a") --Main Windows Language Depot English Blood Bowl: Dark Elves Edition
+setManifestid(11173, "3766865567757766336", 0)
+addappid(11174, 1, "844ad8ddfbca02f48a86e8d4115e2b0f2986788251470f178372e7ce97b1f51b") --Main Windows Language Depot French Blood Bowl: Dark Elves Edition
+setManifestid(11174, "5508174778916296795", 0)
+addappid(11175, 1, "7a1120eca9f4ceb0d33d1b4046245d5e3461c3c915497a27771a373230b66313") --Main Windows Language Depot German Blood Bowl: Dark Elves Edition
+setManifestid(11175, "291700117707420421", 0)
+addappid(11176, 1, "ef2498591c8a6a1e0629f24784464089053eeb7913acf4d75e05d08157e056b1") --Main Windows Language Depot Italian Blood Bowl: Dark Elves Edition
+setManifestid(11176, "737265453700775298", 0)
+addappid(11177, 1, "58fa61824671b2794bb53014a919bc188bf719588fcd489b2569b12278f19813") --Main Windows Language Depot Spanish Blood Bowl: Dark Elves Edition
+setManifestid(11177, "6024339045959466715", 0)
+--Dlcname Bloodbowl Activation Key
