3fe63e5eeea1fee4eb45cd194b60bff22d2ce389
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:14:57 +0800

    add 25500.lua

---
+++ 25500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 25500.lua
+--Gamename Cricket Revolution
+addappid(25500) --Mainappid Cricket Revolution
+addappid(25501, 1, "b18ce142477acc83c85b2adba2d756f229a2bf49ef8176468ab8e34e29f080b8") --Main Windows Depot Cricket Revolution
+setManifestid(25501, "4788219314614237046", 0)
