b1d8617b31e72fc91146f118a6bc3a303e741429
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:39:26 +0800

    add 659.lua

---
+++ 659.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 659.lua
+--Gamename Portal 2 - Pre-order
+addappid(659) --Mainappid Portal 2 - Pre-order
+addappid(660, 1, "759d2251fd0bb050f088c166dc269ea869ddafde525032db3095fbf06cc0dfc3") --Main Windows Depot Portal 2 - Pre-order
+--Dlcname Portal 2 Soundtrack
+addappid(323180, 1, "3845d6afedc1ae0879cd7af31df2bf42724e94bc07e672023ee4d12c422befee") --Dlcname Portal 2 Soundtrack
+addappid(323184, 1, "e20344b3d3b8542dd688df9c0ea6c68482dabe0d49774a297fc06782acd1d952") --Dlc Windows Depot Portal 2 Soundtrack
+setManifestid(323184, "658642952988970250", 1149287072)
+addappid(323185, 1, "33658cb0a5a5c9d74b8bb94b2307030da8b8925b41edc06674458a1d094cabba") --Dlc Windows Depot Portal 2 Soundtrack
+setManifestid(323185, "8466984105877234385", 450495456)
