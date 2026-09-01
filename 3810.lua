e2c98acfea65c28163606aac0b572323cfa7a567
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:28:45 +0800

    add 3810.lua

---
+++ 3810.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3810.lua
+--Gamename BloodRayne (Legacy)
+addappid(3810) --Mainappid BloodRayne (Legacy)
+addappid(3811, 1, "3a818a8e22dbde320d2036c9b2155767929f631cf05719a02a449dc677445e3f") --Main Windows Depot BloodRayne (Legacy)
+setManifestid(3811, "5177721170901481981", 0)
