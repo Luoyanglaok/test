2ddfc885be40b63ba14e0b0efa043ea52cbc8636
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:02:48 +0800

    add 5822.lua

---
+++ 5822.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5822.lua
+--Gamename Medal of Honor - E3MP
+addappid(5822) --Mainappid Medal of Honor - E3MP
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Medal of Honor - E3MP
+setManifestid(910, "1332084816209327708", 0)
