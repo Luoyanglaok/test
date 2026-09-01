477b9f7dbc24f0f277083c8df3fac538f3509fd4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:48:08 +0800

    add 38800.lua

---
+++ 38800.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38800.lua
+--Gamename CrimeCraft: Premium Edition
+addappid(38800) --Mainappid CrimeCraft: Premium Edition
+addappid(38801, 1, "db0bf9d5de10f1ff124c84b68680de2b520aa3070a6acc0b83cab27ffaa4c8f0") --Main Windows Depot CrimeCraft: Premium Edition
+setManifestid(38801, "8732964066783267507", 0)
