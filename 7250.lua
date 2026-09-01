b9f53a9749b0965ab10d279805450598b2c86df0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:18:12 +0800

    add 7250.lua

---
+++ 7250.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7250.lua
+--Gamename Sherlock Holmes: The Awakened
+addappid(7250) --Mainappid Sherlock Holmes: The Awakened
+addappid(7251, 1, "17eef4570b69f638e148b76e32b4f9701320cbcf168f4f11ea105a64a8717f91") --Main Windows Depot Sherlock Holmes: The Awakened
+setManifestid(7251, "3987829563246826049", 0)
+addappid(7252, 1, "61304cec0ff1a323d21bffd92578697b2b000c2ffb166cf70270e2a543d68ca3") --Main Windows Depot Sherlock Holmes: The Awakened
+setManifestid(7252, "3868054117817093553", 0)
