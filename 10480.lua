b430d69804484ed6c58224d2169685b527cb28b1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:29:18 +0800

    add 10480.lua

---
+++ 10480.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10480.lua
+--Gamename Football Manager 2008
+addappid(10480) --Mainappid Football Manager 2008
+addappid(10481, 1, "8d3cef435f68a2ba8f8e75fee492fcb6cce112cec76cb72bed32d8b9644f3716") --Main Windows Depot Football Manager 2008
+setManifestid(10481, "8008625906986730180", 0)
