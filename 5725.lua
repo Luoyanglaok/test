62187dd606242abb9101a42d7954d427419c9876
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:51:55 +0800

    add 5725.lua

---
+++ 5725.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5725.lua
+--Gamename Eve Online - Tyrannis
+addappid(5725) --Mainappid Eve Online - Tyrannis
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Eve Online - Tyrannis
+setManifestid(910, "1332084816209327708", 0)
