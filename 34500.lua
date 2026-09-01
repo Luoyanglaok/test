6e4decbfcacb1813da500e8d5e2a4f2517110919
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:32:02 +0800

    add 34500.lua

---
+++ 34500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34500.lua
+--Gamename Light of Altair
+addappid(34500) --Mainappid Light of Altair
+addappid(34501, 1, "0e30215991af22dfc3371769250072c0859c83a41f3c37ebf577a0138c589df7") --Main Windows Depot Light of Altair
+setManifestid(34501, "7517925367576373785", 0)
