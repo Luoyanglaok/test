d5e481af40a8235ce95e36cda6189f83ca3fb570
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:15:30 +0800

    add 6290.lua

---
+++ 6290.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6290.lua
+--Gamename Alien Shooter: Vengeance
+addappid(6290) --Mainappid Alien Shooter: Vengeance
+addappid(6291, 1, "bcd508f80f108b8b8536013a697ced396f13af6fe5d66290d918bf60f47be088") --Main Windows Depot Alien Shooter: Vengeance
+setManifestid(6291, "7883498744817367364", 0)
