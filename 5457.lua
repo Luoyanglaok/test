c040096fe78e206176fa7762acef9d2fdebaf120
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:38:55 +0800

    add 5457.lua

---
+++ 5457.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5457.lua
+--Gamename Dawn of War II - The Last Stand Developer Diary
+addappid(5457) --Mainappid Dawn of War II - The Last Stand Developer Diary
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Dawn of War II - The Last Stand Developer Diary
+setManifestid(910, "1332084816209327708", 0)
