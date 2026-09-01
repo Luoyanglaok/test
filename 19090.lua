6e301759527dd6a2a38aa16e431c273bf4afe479
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:51:37 +0800

    add 19090.lua

---
+++ 19090.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19090.lua
+--Gamename Yard Sale Hidden Treasures Sunnyville
+addappid(19090) --Mainappid Yard Sale Hidden Treasures Sunnyville
+addappid(19091, 1, "5ceec8dc999a30333a1cb3de95dcfedfc9f771dd63cc285a2e22600634b5f4f9") --Main Windows Depot Yard Sale Hidden Treasures Sunnyville
+setManifestid(19091, "9100623599611252900", 0)
+addappid(19092, 1, "2f2f59c5b24cf86f89f5a92ffaa33fd17c7d371b42320aec53ba62e058e1acd4") --Main Windows Language Depot English Yard Sale Hidden Treasures Sunnyville
+setManifestid(19092, "2922495362390653427", 0)
+addappid(19093, 1, "b9fc7fef3a4ec855200d1345a3b505e6b2334c23a9fcda687b1702e22066e69e") --Main Windows Language Depot French Yard Sale Hidden Treasures Sunnyville
+setManifestid(19093, "3306321971602359044", 0)
+addappid(19094, 1, "34070ad2ba6ae26a8e4dc244935969437865cdeb965a8573524b50909bcc3a27") --Main Windows Language Depot German Yard Sale Hidden Treasures Sunnyville
+setManifestid(19094, "2183099845820807271", 0)
+addappid(19095, 1, "e3ae5f2a289dde4dcaef8ffde866183f115d9a8af87108759fcd8f5f35102492") --Main Windows Language Depot Italian Yard Sale Hidden Treasures Sunnyville
+setManifestid(19095, "6749873145327521944", 0)
+addappid(19096, 1, "f7b326ed498d2b1627a26b5a0bf0f911db4673ef2b0fe45417eae1b720de1a7f") --Main Windows Language Depot Spanish Yard Sale Hidden Treasures Sunnyville
+setManifestid(19096, "2430836792223856322", 0)
