e2f657c8704f065bb1bd11b0dc9b85d19106a065
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:42:00 +0800

    add 16200.lua

---
+++ 16200.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 16200.lua
+--Gamename Virtual Families
+addappid(16200) --Mainappid Virtual Families
+addappid(16201, 1, "39ab8f671616cd23d41392d0977c86b693a17d4ea5888da6d44d9871c66cd598") --Main Windows Depot Virtual Families
+setManifestid(16201, "4473437896133150765", 0)
