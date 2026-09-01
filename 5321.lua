5498563fe613d81d31ec088f43e7ea3cdad70df6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:31:46 +0800

    add 5321.lua

---
+++ 5321.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5321.lua
+--Gamename Batman Arkham Asylum Joker
+addappid(5321) --Mainappid Batman Arkham Asylum Joker
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Batman Arkham Asylum Joker
+setManifestid(910, "1332084816209327708", 0)
