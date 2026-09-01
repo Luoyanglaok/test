391f21d805d68cf094850a543dd130a62c6f500a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:21:25 +0800

    add 8010.lua

---
+++ 8010.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8010.lua
+--Gamename Ancient Wars: Sparta
+addappid(8010) --Mainappid Ancient Wars: Sparta
+addappid(8011, 1, "c09e81d9970a381b950f530cad287e04590ddb1cc2e2e2882d4456131475c472") --Main Windows Depot Ancient Wars: Sparta
+setManifestid(8011, "5346687693255090513", 0)
+addappid(8012, 1, "8d5fa7e20013913088691339d67964f81670e4b0a7761701938b5595a804e7b5") --Main Windows Depot Ancient Wars: Sparta
+setManifestid(8012, "4646115218617992164", 0)
+addappid(8013, 1, "be67aa1b47b40a7a58052bbff874466a95531da1c3c6e0e94be1fb817ca30bcf") --Main Windows Depot Ancient Wars: Sparta
+setManifestid(8013, "6584034897601976613", 0)
+addappid(8014, 1, "578310d0509ecdffe4228ce8d92980f3fd1d79a840c307934b2ecc54169e8659") --Main Windows Language Depot German Ancient Wars: Sparta
+setManifestid(8014, "3313350838343220361", 0)
+addappid(8015, 1, "4f9f1464180058745c7afa03547c2f84d6cb613c3bb4b366046064de74d99837") --Main Windows Language Depot French Ancient Wars: Sparta
+setManifestid(8015, "2703531139752317248", 0)
