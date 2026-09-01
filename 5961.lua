09f3e9c889b418019780c65c8350afb934aa8be9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:13:16 +0800

    add 5961.lua

---
+++ 5961.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5961.lua
+--Gamename Shank 2 - PEGI
+addappid(5961) --Mainappid Shank 2 - PEGI
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Shank 2 - PEGI
