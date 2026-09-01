6b2deb313e6372150f534eaf5ee8a7de00943e09
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:25:14 +0800

    add 9330.lua

---
+++ 9330.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9330.lua
+--Gamename Dawn of War Winter Assault Demo
+addappid(9330) --Mainappid Dawn of War Winter Assault Demo
+addappid(9331, 1, "a9a09a7b5de69ef6fb43f69d8f1474fa53a26231385089ffa5b54133a4c5ad5c") --Main Windows Depot Dawn of War Winter Assault Demo
+setManifestid(9331, "9195368990007606223", 0)
