e0afb538df37890a3c7ef58ee5efac2fbab776f3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:44:14 +0800

    add 941.lua

---
+++ 941.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 941.lua
+--Gamename Red Orchestra Fall Update Trailer
+addappid(941) --Mainappid Red Orchestra Fall Update Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Red Orchestra Fall Update Trailer
+setManifestid(910, "1332084816209327708", 0)
