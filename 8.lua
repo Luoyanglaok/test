0583da26006037eb5fdf2897b15725a3211a8b9e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:26:12 +0800

    add 8.lua

---
+++ 8.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8.lua
+--Gamename winui2
+addappid(8, 1, "20af1043db56d6408ddd965580e1269005b6e04f81c3676d155174f73099da95") --Mainappid winui2
+addappid(9, 1, "68c92cf2532cdc6e0b7a9d2783b473b6d16bac72218badf68d7be9b033076096") --Main Windows Depot winui2
