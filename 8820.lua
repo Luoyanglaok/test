c1573583906f54e004887a2e7c7ffa007d24a77a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:23:38 +0800

    add 8820.lua

---
+++ 8820.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8820.lua
+--Gamename Sid Meier's Civilization IV: Beyond the Sword - Final Frontier Demo
+addappid(8820) --Mainappid Sid Meier's Civilization IV: Beyond the Sword - Final Frontier Demo
+addappid(8821, 1, "c1397718baf2c1bf535b6635f633bac87af8170f6eaeb3f9875a0732e75b03d2") --Main Windows Depot Sid Meier's Civilization IV: Beyond the Sword - Final Frontier Demo
+setManifestid(8821, "919952758073993924", 0)
