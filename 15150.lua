e9db31d81aa1756f4a848da0dc8f7d003b07e4e7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:37:17 +0800

    add 15150.lua

---
+++ 15150.lua
@@ -0,0 +1,22 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15150.lua
+--Gamename Petz Catz 2
+addappid(15150) --Mainappid Petz Catz 2
+addappid(15151, 1, "f81c4d60b87f0ed9d4fc14fe0a5e3cb064bb57c7bb5c12ca2c127221ec48298e") --Main Windows Depot Petz Catz 2
+setManifestid(15151, "16352386376731670", 0)
+addappid(15152, 1, "2b962d8c8622a10ef7f5107384ab75a8672402a835bd680ea0bcb1583ba0908a") --Main Windows Language Depot Danish Petz Catz 2
+setManifestid(15152, "263460216981467744", 0)
+addappid(15153, 1, "a5c0d922827a88ee27609ad51f44be172c031b088d3d4b6b6bf93a7aec75573d") --Main Windows Language Depot Dutch Petz Catz 2
+setManifestid(15153, "219301637653023647", 0)
+addappid(15154, 1, "7743897df0901ddfe0a7ca1fa7549d75c5ef698850978deefd1adcc3c6e5d89e") --Main Windows Language Depot French Petz Catz 2
+setManifestid(15154, "1756599032033005473", 0)
+addappid(15155, 1, "bcc6c33dd04aac378f9057da0765eab36a230932467a019a359b66bec72da4f8") --Main Windows Language Depot German Petz Catz 2
+setManifestid(15155, "5509261243828890494", 0)
+addappid(15156, 1, "36b44f12c606c541c5a388c9827a139ca2937990a06875dfbf8ff73925f0afcc") --Main Windows Language Depot Italian Petz Catz 2
+setManifestid(15156, "7082108754884787168", 0)
+addappid(15157, 1, "904aae71be07d27d8fe884d28510d885cca47c9e3e3444de5e8dbe6c0346280b") --Main Windows Language Depot Norwegian Petz Catz 2
+setManifestid(15157, "8296873113973133486", 0)
+addappid(15158, 1, "190ff8f23dc515051243c804dce4865f4900337ced8a4799a07ea37552b473aa") --Main Windows Language Depot Spanish Petz Catz 2
+setManifestid(15158, "7594353776002834820", 0)
+addappid(15159, 1, "16b86e1e5f2789d660344424cebf160186ce87755b36a76786e419ce78d70a8d") --Main Windows Language Depot Swedish Petz Catz 2
+setManifestid(15159, "4527162649816814534", 0)
