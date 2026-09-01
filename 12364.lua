86822ec995fea1f2a5797f1d15de170ea3a588ae
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:33:48 +0800

    add 12364.lua

---
+++ 12364.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12364.lua
+--Gamename Big Mutha Truckers
+addappid(12364) --Mainappid Big Mutha Truckers
+addappid(12365, 1, "a6400c456f5c87af99f150b722c829c7d623acff03e8960a8633b21f611dfa17") --Main Windows Depot Big Mutha Truckers
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Big Mutha Truckers
+addappid(2682930, 1, "7a96e4e12b3fc89930fcb001433c370d3215fb35ee4dbaa316eed2f44259ba49") --Main Windows Depot Big Mutha Truckers
+setManifestid(2682930, "753194071788851254", 473058592)
