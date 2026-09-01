207002e355eebf7f33aeb08f3f839a589df01035
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:36:53 +0800

    add 13660.lua

---
+++ 13660.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 13660.lua
+--Gamename Tom Clancy's Rainbow Six 2: Rogue Spear
+addappid(13660) --Mainappid Tom Clancy's Rainbow Six 2: Rogue Spear
+addappid(13661, 1, "0751f074c0aec5069570a431f3f05f25c4ca707e63c24790d3b8b1d021cd59d9") --Main Windows Depot Tom Clancy's Rainbow Six 2: Rogue Spear
+setManifestid(13661, "9179039500634459294", 0)
