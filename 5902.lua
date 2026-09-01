edd4434e97f3b1f315f53e861a5cfb313714a9aa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:09:19 +0800

    add 5902.lua

---
+++ 5902.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5902.lua
+--Gamename BladeKittenTrailer 2
+addappid(5902) --Mainappid BladeKittenTrailer 2
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot BladeKittenTrailer 2
+setManifestid(910, "1332084816209327708", 0)
