ecf7d456b46d595f746a41a2566ba4043f3a9d18
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:21:34 +0800

    add 8060.lua

---
+++ 8060.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8060.lua
+--Gamename Championship Manager 2008
+addappid(8060) --Mainappid Championship Manager 2008
+addappid(8061, 1, "b8daadb928048fecf04f5012cb093a50e20ecfd2f1d1507281f8318b3169347a") --Main Windows Depot Championship Manager 2008
+setManifestid(8061, "7425192378276973525", 0)
+addappid(8062, 1, "a6a8d72a68d1fcbd3c6de97e4ade417d3de1beecdb8d0aa826708297a92cdcdd") --Main Windows Language Depot English Championship Manager 2008
+setManifestid(8062, "2503863739431178417", 0)
+addappid(8063, 1, "38663f846b716f313a2b1aac8640eed5ba8220c05d4700541aa68e4819943e85") --Main Windows Language Depot French Championship Manager 2008
+setManifestid(8063, "4299448872056455252", 0)
+addappid(8064, 1, "0ae253a754c1823a56905f8954274ef9bc8324f952e51d869af9fc090a37c0c4") --Main Windows Language Depot Italian Championship Manager 2008
+setManifestid(8064, "1380990717764572562", 0)
+addappid(8065, 1, "4115736a5a0cdffb291e311a22a34f40ac8dd26f52f2f37367bea710924f3c17") --Main Windows Language Depot Spanish Championship Manager 2008
+setManifestid(8065, "5099548855046231276", 0)
