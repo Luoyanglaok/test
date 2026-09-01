db9fd8a76caec51c803e6bf3bac8b61e5a8447d7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:40:50 +0800

    add 36130.lua

---
+++ 36130.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 36130.lua
+--Gamename Tradewinds Caravans + Odyssey Pack
+addappid(36130) --Mainappid Tradewinds Caravans + Odyssey Pack
+addappid(36131, 1, "96ec2c2d6818243e33272d0ad46d13fc6746408642c965341079c712fb07ef0c") --Main Windows Depot Tradewinds Caravans + Odyssey Pack
+setManifestid(36131, "6612615646476287167", 0)
