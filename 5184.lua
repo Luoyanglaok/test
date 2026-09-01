6d9fbf7fac6aa8cb64bbdd08af753fde45efd6b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:22:02 +0800

    add 5184.lua

---
+++ 5184.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5184.lua
+--Gamename Monster Trucks Nitro Trailer
+addappid(5184) --Mainappid Monster Trucks Nitro Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Monster Trucks Nitro Trailer
+setManifestid(910, "1332084816209327708", 0)
