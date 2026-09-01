2c7e8a4a462bfb9b2f6dbbbaf082619a56150665
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:26:15 +0800

    add 5263.lua

---
+++ 5263.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5263.lua
+--Gamename Prototype Story Montage
+addappid(5263) --Mainappid Prototype Story Montage
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Prototype Story Montage
+setManifestid(910, "1332084816209327708", 0)
