f3bd74ab88fe7de389fcf05100c440eb9eb5b08b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:00:55 +0800

    add 5806.lua

---
+++ 5806.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5806.lua
+--Gamename Black _Shark_Trailer1
+addappid(5806) --Mainappid Black _Shark_Trailer1
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Black _Shark_Trailer1
+setManifestid(910, "1332084816209327708", 0)
