2f9eaffaec70f676d978573a8cad761e3195b2a6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:42:53 +0800

    add 37270.lua

---
+++ 37270.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37270.lua
+--Gamename Wandering Willows
+addappid(37270) --Mainappid Wandering Willows
+addappid(37271, 1, "2834916a273e769f13fbdd4be2348cb2ae8e53dfaff65be830d0c479166617a2") --Main Windows Depot Wandering Willows
+setManifestid(37271, "8661737666474847565", 0)
+addappid(37272, 1, "a9376df770ab9e8188ca6ed0336e11882c7971b44f7194857398abeafdad3e35") --Main Macos Depot Wandering Willows
+setManifestid(37272, "587748432756670796", 0)
