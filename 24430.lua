a1fd486d1e560bccaa8cb250d493bee9eb452b5d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:11:13 +0800

    add 24430.lua

---
+++ 24430.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24430.lua
+--Gamename King Arthur - The Role-playing Wargame Demo
+addappid(24430) --Mainappid King Arthur - The Role-playing Wargame Demo
+addappid(24431, 1, "da656765b046768be43521b9d8593379f2916e7bbbbd04477a5fa9ed51d2a8f6") --Main Windows Depot King Arthur - The Role-playing Wargame Demo
+setManifestid(24431, "1277071286262700376", 0)
