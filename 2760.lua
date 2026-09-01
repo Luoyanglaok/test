8ef0d17ec61e337a5c49bb4abac341d2dd9245ef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:23:47 +0800

    add 2760.lua

---
+++ 2760.lua
@@ -0,0 +1,22 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2760.lua
+--Gamename Neverwinter Nights™ 2 Platinum
+addappid(2760) --Mainappid Neverwinter Nights™ 2 Platinum
+addappid(2761, 1, "5e17c2431c7caadd279b339772942bf4e5e9a45e1296aa01ba35e5c092d46ce4") --Main Windows Depot Neverwinter Nights™ 2 Platinum
+setManifestid(2761, "417065396441547239", 0)
+addappid(2763, 1, "370db8901788f61c50e86ab24e2b5c9bdda3c372c7da85e8a8a3f8173f6af186") --Main Windows Language Depot French Neverwinter Nights™ 2 Platinum
+setManifestid(2763, "3740583565086440865", 0)
+addappid(2764, 1, "40d9017cdf5121a58fe0bf414fda24d9212304d4c74a03c0fcd144b444b19fbd") --Main Windows Language Depot German Neverwinter Nights™ 2 Platinum
+setManifestid(2764, "1915941584196772687", 0)
+addappid(2765, 1, "b1b8733c4f41bb4e0ede0405e548d43732b8a3291e944f850a9aed2976e52f44") --Main Windows Language Depot English Neverwinter Nights™ 2 Platinum
+setManifestid(2765, "2297614638630342428", 0)
+addappid(2766, 1, "bc0c68c13fa31ac46b6afb7ec98b604c61b6c2ce4d081cd40959e90120ba0285") --Main Windows Language Depot Spanish Neverwinter Nights™ 2 Platinum
+setManifestid(2766, "2655512818314402057", 0)
+addappid(2767, 1, "459fac79c7b7c454e4769169a67610635887affb9d003c6f02178829a1a31879") --Main Windows Depot Neverwinter Nights™ 2 Platinum
+setManifestid(2767, "8057023001991090276", 0)
+addappid(2768, 1, "976266c71e2a450da154d891e8cb476677252d688fc1a193098a03ee74454e94") --Main Windows Depot Neverwinter Nights™ 2 Platinum
+setManifestid(2768, "2930198314680165052", 0)
+addappid(2769, 1, "e3afe77db4e4cb7e4581a16c7af34eecbd1ba97c1b6088a85c0d6c8ec0706227") --Main Windows Language Depot Italian Neverwinter Nights™ 2 Platinum
+setManifestid(2769, "5198570373182371647", 0)
+--Dlcname Neverwinter Nights 2 Expansion 1
+--Dlcname Neverwinter Nights 2 Expansion 2
