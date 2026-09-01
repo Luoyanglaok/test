e1b6367ea0ac891c36414131e42ed4ca76d07f71
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:36:49 +0800

    add 364.lua

---
+++ 364.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 364.lua
+--Gamename HL1:MP Linux client
+addappid(364, 1, "73096ee7654c146172e69a0aecedce5e96a965ba1086a6dfad55d6b6476afcb4") --Mainappid HL1:MP Linux client
