4ed5ffccfea56d240d46652ba514d85fe597ee53
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:50:14 +0800

    add 5706.lua

---
+++ 5706.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5706.lua
+--Gamename Guns Of Icarus 1
+addappid(5706) --Mainappid Guns Of Icarus 1
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Guns Of Icarus 1
