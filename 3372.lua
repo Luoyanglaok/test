37328a172f6024a73604ade86a7038e79a6dab66
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:27:37 +0800

    add 3372.lua

---
+++ 3372.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3372.lua
+--Gamename BookWorm Deluxe Demo
+addappid(3372) --Mainappid BookWorm Deluxe Demo
+addappid(3371, 1, "561a17d0e047d0be1c8055ce985acf93dd10553b2e4dd0b2ee6d3017c031d77c") --Main Windows Depot BookWorm Deluxe Demo
+addappid(3373, 1, "60a68bafa70c76309753640094ba01e409a991ea5941721154852cc686b96440") --Main Windows Language Depot German BookWorm Deluxe Demo
+addappid(3374, 1, "e243fe949081ba1c4e917646e7c41b8679b9f1648ed76110f4c4f978ed96bbc9") --Main Windows Language Depot Spanish BookWorm Deluxe Demo
+addappid(3375, 1, "7dafce7cb66b90f3490876854e775cfd08a0878b68ab630c8d5b3e27f5e6b0a9") --Main Windows Language Depot Italian BookWorm Deluxe Demo
+addappid(3376, 1, "a1a1866a1b3c06423a8dc87eadb79820720af61204ddcb38f338101759a845f5") --Main Windows Language Depot French BookWorm Deluxe Demo
