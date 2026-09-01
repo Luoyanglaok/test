d1136a9c413ad136215558d6dd3a844f36ad9a7e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:25:21 +0800

    add 2920.lua

---
+++ 2920.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2920.lua
+--Gamename Sub Command
+addappid(2920) --Mainappid Sub Command
+addappid(2921, 1, "a93e00ba03fcb724de7a7b8959d5503fb74f5a51dc7d25f6fd85e76930c5d597") --Main Windows Depot Sub Command
+setManifestid(2921, "5435813013862110404", 0)
