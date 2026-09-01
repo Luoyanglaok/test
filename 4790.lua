fd6e0fb6579d3e627e6da62c9ffaa0fa0e4780ad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:33:12 +0800

    add 4790.lua

---
+++ 4790.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4790.lua
+--Gamename SEGA Rally Revo
+addappid(4790) --Mainappid SEGA Rally Revo
+addappid(4791, 1, "1bcd0646aa1203aedb2118e85b7f0688efc750dd13fff2cf8fc0454a14005e69") --Main Windows Depot SEGA Rally Revo
+setManifestid(4791, "4448578495158846788", 0)
+addappid(4792, 1, "d0d1a3cba5e8bd84ef805a2d5f93471300a6fd57794c3d76f87cad374828b5e2") --Main Windows Depot SEGA Rally Revo
+setManifestid(4792, "7865950457767639603", 0)
+addappid(4793, 1, "82c926ef4b7d1111758237f4d5269e44a824a54c1747455e6e152c047e315f8f") --Main Windows Depot SEGA Rally Revo
+setManifestid(4793, "4572497640416027057", 0)
