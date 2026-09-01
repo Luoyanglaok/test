c20c49b3a900358e06ff41c663664a275341869c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:43:24 +0800

    add 5077.lua

---
+++ 5077.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5077.lua
+--Gamename Far Cry 2: Enemies
+addappid(5077) --Mainappid Far Cry 2: Enemies
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Far Cry 2: Enemies
+setManifestid(910, "1332084816209327708", 0)
