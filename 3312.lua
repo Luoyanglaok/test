6a6318e9d669953daee65db50f346aff56d72e36
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:27:06 +0800

    add 3312.lua

---
+++ 3312.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3312.lua
+--Gamename Chuzzle Deluxe Demo
+addappid(3312) --Mainappid Chuzzle Deluxe Demo
+addappid(3311, 1, "a43fb836c6313a0cc6158ffc959f55c733141be9f6d4d16f3c5a5ed935ddbebd") --Main Windows Depot Chuzzle Deluxe Demo
+addappid(3313, 1, "14c2b9e345b44d5aff4a5d8e30cf288e53253307b45319b312608a8f1374c734") --Main Windows Language Depot French Chuzzle Deluxe Demo
+addappid(3314, 1, "532167b35dd2cbb1c6a8be473b3c6199070d11b5021088cefb9240762d2c847e") --Main Windows Language Depot German Chuzzle Deluxe Demo
+addappid(3315, 1, "26b1afc709df677c817da72e9ce0c32c324fb8e984a92861486037860898b224") --Main Windows Language Depot Italian Chuzzle Deluxe Demo
+addappid(3316, 1, "b3a868a92b506b38e3a5c8b200fa4fa3ae32967c38a3e62a0fa83d9a6abd5cb6") --Main Windows Language Depot Spanish Chuzzle Deluxe Demo
