cc2c46a020aaf5bb66eb691280f87616cc9e72ee
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:30:03 +0800

    add 10570.lua

---
+++ 10570.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10570.lua
+--Gamename Football Manager 2009 Demo
+addappid(10570) --Mainappid Football Manager 2009 Demo
+addappid(10571, 1, "a0c20dd1d79623c8c67d493373b1c1e82f71135a50839d870065d85857610e5e") --Main Windows Depot Football Manager 2009 Demo
+setManifestid(10571, "6964329265439322754", 0)
