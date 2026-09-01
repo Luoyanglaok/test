c6bb326b3f15e1a318968ff866c7f68f63c4dfa4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:43:35 +0800

    add 37320.lua

---
+++ 37320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37320.lua
+--Gamename The Nightshift Code™
+addappid(37320) --Mainappid The Nightshift Code™
+addappid(37321, 1, "6c9b32d1c1860772e47775451fcb6553e0efa59100897a51ed36520c4cc7b398") --Main Windows Depot The Nightshift Code™
+setManifestid(37321, "3682666470545754282", 0)
