330d306bd4c82b3c94b01f5638cefb6866ad0ff2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:29:08 +0800

    add 10470.lua

---
+++ 10470.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10470.lua
+--Gamename Happy Tree Friends: False Alarm
+addappid(10470) --Mainappid Happy Tree Friends: False Alarm
+addappid(10471, 1, "4fd3636eb7a3b691b397c3ac652923ba63deab6ee59a679f3ad8e05054d2d4cf") --Main Windows Depot Happy Tree Friends: False Alarm
+setManifestid(10471, "8028834019902588336", 0)
