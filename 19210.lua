5c9677b94d257d467a2e209da79ad9a3dae60cff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:51:45 +0800

    add 19210.lua

---
+++ 19210.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19210.lua
+--Gamename Tank Universal Demo
+addappid(19210) --Mainappid Tank Universal Demo
+addappid(19211, 1, "36d6746381f8caaed79e9f7bfc9dc81102e1bf64614021dc0ff89741730d5c73") --Main Windows Depot Tank Universal Demo
+setManifestid(19211, "6501190454308177222", 0)
