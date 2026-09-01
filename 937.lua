e0aba90441ae214555e8951814c592a00745df70
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:43:51 +0800

    add 937.lua

---
+++ 937.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 937.lua
+--Gamename Half-Life 2: Episode Two Gameplay Movie 5
+addappid(937) --Mainappid Half-Life 2: Episode Two Gameplay Movie 5
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Half-Life 2: Episode Two Gameplay Movie 5
+setManifestid(910, "1332084816209327708", 0)
