55ef9317d8f084df21cb6e2d584e9b7355863b53
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:48:16 +0800

    add 38810.lua

---
+++ 38810.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38810.lua
+--Gamename Crimecraft: Standard Edition
+addappid(38810) --Mainappid Crimecraft: Standard Edition
+addappid(38801, 1, "db0bf9d5de10f1ff124c84b68680de2b520aa3070a6acc0b83cab27ffaa4c8f0") --Main Windows Depot Crimecraft: Standard Edition
