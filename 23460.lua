5c539227e438d5af3c8cd95acbf36ebbd107890b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:00:20 +0800

    add 23460.lua

---
+++ 23460.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 23460.lua
+--Gamename Ceville
+addappid(23460) --Mainappid Ceville
+addappid(23461, 1, "4f50ce3c9e2637042ac14306eb25eeddf406d1315485c99cce8536b633481fc7") --Main Windows Depot Ceville
+setManifestid(23461, "9047215271172436034", 0)
+addappid(23462, 1, "ea722222144a807d3c42180e28f5b79e30effb603377833c1b6108b8b99acd72") --Main Windows Language Depot German Ceville
+setManifestid(23462, "4437578440069638709", 0)
+addappid(23463, 1, "e0ad30a396492eeb872c19cd28d2735b8cb37bbfd04a01d640abd827ca7a599b") --Main Windows Language Depot English Ceville
+setManifestid(23463, "2146253658657730101", 0)
