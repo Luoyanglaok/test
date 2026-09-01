f74cd5a67d5291bbb220ebe21465db767fa19391
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:37:34 +0800

    add 15230.lua

---
+++ 15230.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15230.lua
+--Gamename Blazing Angels 2: Secret Missions of WWII
+addappid(15230) --Mainappid Blazing Angels 2: Secret Missions of WWII
+addappid(15231, 1, "3302e01671b87dff0768fe192e2844d57a44d13183f26874fa88aa68c6024f9d") --Main Windows Depot Blazing Angels 2: Secret Missions of WWII
+setManifestid(15231, "3455984667935609142", 0)
