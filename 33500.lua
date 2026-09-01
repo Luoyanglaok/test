44d6527a3f84600e6e02816cb61132e13c7af2ee
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:21:45 +0800

    add 33500.lua

---
+++ 33500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33500.lua
+--Gamename Hired Guns: The Jagged Edge
+addappid(33500) --Mainappid Hired Guns: The Jagged Edge
+addappid(33501, 1, "89ad9a608d188bc8f238783cf8ffe492182d6fcb95ec0cf77521d2b39e05c187") --Main Windows Depot Hired Guns: The Jagged Edge
+setManifestid(33501, "4018469912304762325", 0)
