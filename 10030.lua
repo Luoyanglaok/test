5a48ee9756c8e5c58546ebe53abe1e14bc10236c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:26:44 +0800

    add 10030.lua

---
+++ 10030.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10030.lua
+--Gamename Enemy Territory: QUAKE Wars Dedicated Server
+addappid(10030) --Mainappid Enemy Territory: QUAKE Wars Dedicated Server
+addappid(10001, 1, "990e9c13a9fe8072d18efc1c69fbe2d36ee85ed64c274218a1d414d8a9ac67f0") --Main Windows Depot Enemy Territory: QUAKE Wars Dedicated Server
+setManifestid(10001, "5477813184068131469", 0)
+addappid(10004, 1, "29dbe482078c72e0c1fbb960a602c97f4229c80b6015d2e97395271a0964d35c") --Main Windows Language Depot French Enemy Territory: QUAKE Wars Dedicated Server
+setManifestid(10004, "5633547836704586446", 0)
+addappid(10006, 1, "36b8d741f05213f8ab72da879c1e1482464795605f16a4a80d89b3abe169a4cb") --Main Windows Language Depot Spanish Enemy Territory: QUAKE Wars Dedicated Server
+setManifestid(10006, "1856538235062813871", 0)
+addappid(10031, 1, "e7158c6adcaf2f895a6b298f7a51da5686eac5aae481edbaa102425db5b83e20") --Main Windows Depot Enemy Territory: QUAKE Wars Dedicated Server
+setManifestid(10031, "1124709354692861233", 0)
