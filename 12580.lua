12a625b85bd8866dbc868f7be9f26b0800058652
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:35:21 +0800

    add 12580.lua

---
+++ 12580.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12580.lua
+--Gamename Dr. Daisy Pet Vet
+addappid(12580) --Mainappid Dr. Daisy Pet Vet
+addappid(12581, 1, "a3302f4e04e94b5d528764f101e756249ecb7bdb74a73db5d859b4e2bb55177c") --Main Windows Depot Dr. Daisy Pet Vet
+setManifestid(12581, "4384482415012191865", 0)
