615c922c6c7e55c547464f74a4711c7ec323e038
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:51:44 +0800

    add 5724.lua

---
+++ 5724.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5724.lua
+--Gamename Free Yourself - 5.26.2010
+addappid(5724) --Mainappid Free Yourself - 5.26.2010
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Free Yourself - 5.26.2010
+setManifestid(910, "1332084816209327708", 0)
