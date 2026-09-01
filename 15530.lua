ee6fbe23a3a97394ffe1b4b6cdedd8111d48abb8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:38:56 +0800

    add 15530.lua

---
+++ 15530.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15530.lua
+--Gamename AaaaaAAaaaAAAaaAAAAaAAAAA!!! - A Reckless Disregard for Gravity - Demo
+addappid(15530) --Mainappid AaaaaAAaaaAAAaaAAAAaAAAAA!!! - A Reckless Disregard for Gravity - Demo
+addappid(15531, 1, "0f215d8fca31759ca6519964db8ae343e2882091d5f316b2d7d9e17ae7e78083") --Main Windows Depot AaaaaAAaaaAAAaaAAAAaAAAAA!!! - A Reckless Disregard for Gravity - Demo
+setManifestid(15531, "8757947028701168938", 0)
