3000748ad2e569eae874b7619c3e1d0a9cf8bd14
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:46:55 +0800

    add 963.lua

---
+++ 963.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 963.lua
+--Gamename TrackMania United Webisode 1: Coppers
+addappid(963) --Mainappid TrackMania United Webisode 1: Coppers
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot TrackMania United Webisode 1: Coppers
+setManifestid(910, "1332084816209327708", 0)
