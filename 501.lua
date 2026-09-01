50ea0b5c1c18fd4f4c6899fc90f88beb2355f810
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:37:31 +0800

    add 501.lua

---
+++ 501.lua
@@ -0,0 +1,25 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 501.lua
+--Gamename Steam Store
+addappid(501, 1, "7a0b21eed084464fd2efb03ecc44116224b150f9377551ca7901ead966f21c26") --Mainappid Steam Store
+setManifestid(501, "1832333851347898090", 110481984)
+addappid(502, 1, "54d51378fea25647cfe0c7d77804f29abd538c2606e4bf27b972d02816f34487") --Main Windows Depot Steam Store
+setManifestid(502, "1346199116149879810", 2045022352)
+--Dlcname Left 4 Dead
+addappid(500, 1, "b17ac8c5e383af45264bd21caaac5f45e1f40e9883bbef39cb25d308b4083ab7") --Dlcname Left 4 Dead
+addappid(503, 1, "81d91ffef65bb0b38b50fe8ba06fe690aae9a0afd2c65c3bbc9ec55ba5a203c4") --Dlc Windows Depot Left 4 Dead
+setManifestid(503, "5156732173895269546", 2736000)
+addappid(504, 1, "f7671db1d3c622e18dd1245e2d3f173e8ea71ad425f82e37f5d4027a60416c1a") --Dlc Windows Depot Left 4 Dead
+setManifestid(504, "8212197441078172924", 1851249808)
+addappid(505, 1, "606977fdfcd55d1a781656ef5db7811bc9ab107d7604c68291a971aaa2a8c5b9") --Dlc Windows Language Depot French Left 4 Dead
+setManifestid(505, "5045000053553889222", 995611984)
+addappid(506, 1, "ff83a268db47060bbe676d22168de551715015faa70d962f3aa46368bb083bf8") --Dlc Windows Language Depot German Left 4 Dead
+setManifestid(506, "3612476309264296188", 972623088)
+addappid(507, 1, "d3a82b031014aa7c72b7c9ce029b2a15d8bfbcbb52ca29a56ac212f5bf1653db") --Dlc Windows Language Depot Russian Left 4 Dead
+setManifestid(507, "3651932563014144986", 1162486576)
+addappid(508, 1, "1f37e4f4b59d43a25a36ab2b174899f36a063cd7a80b743604d71f1c7c61015c") --Dlc Windows Language Depot Spanish Left 4 Dead
+setManifestid(508, "3774429000353462406", 943738160)
+addappid(509, 1, "4bbb633eb8a92ee086da33ea940e3f185d1ed584872826f7d4dcc6d012c9e729") --Dlc Windows Depot Left 4 Dead
+setManifestid(509, "4309044654752076632", 645961040)
+addappid(515, 1, "f09a2fa191a845e1274cf6abb8e5bc7d6ffd6c04cb4a114ddf6a98f8967877e0") --Dlc Macos Depot Left 4 Dead
+setManifestid(515, "2369701142506601604", 577800016)
