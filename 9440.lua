c510f573f0b76cdb299cb2d1e8e6935cd55dac9e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:25:35 +0800

    add 9440.lua

---
+++ 9440.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9440.lua
+--Gamename Warhammer 40,000: Dawn of War – Soulstorm Demo
+addappid(9440) --Mainappid Warhammer 40,000: Dawn of War – Soulstorm Demo
+addappid(9441, 1, "e80ead616668a9585b23cdee19a23c7e450829f67e62595a0c03fa2b4390971a") --Main Windows Depot Warhammer 40,000: Dawn of War – Soulstorm Demo
+setManifestid(9441, "1207668052658970844", 0)
