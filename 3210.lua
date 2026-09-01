153d9ebeac8f92df1718689f4e72790cd6711518
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:26:35 +0800

    add 3210.lua

---
+++ 3210.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3210.lua
+--Gamename Painkiller Demo
+addappid(3210) --Mainappid Painkiller Demo
+addappid(3211, 1, "7f39571991dacd52851ea1cd3c439338f9ddda269119eb857ebabf5d3a979b08") --Main Windows Depot Painkiller Demo
+setManifestid(3211, "5278195698989979394", 0)
