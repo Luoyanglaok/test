82e02c47b956ed8497970f987fa1ba4568479e23
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:46:44 +0800

    add 5662.lua

---
+++ 5662.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5662.lua
+--Gamename Luftwaffe_teaser
+addappid(5662) --Mainappid Luftwaffe_teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Luftwaffe_teaser
+setManifestid(910, "1332084816209327708", 0)
