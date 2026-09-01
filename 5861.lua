681cb5c12c97f0c0962970ac7310c644ee2ba8fb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:05:53 +0800

    add 5861.lua

---
+++ 5861.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5861.lua
+--Gamename Lara Croft Guardian of Light Trailer 2 (ESRB)
+addappid(5861) --Mainappid Lara Croft Guardian of Light Trailer 2 (ESRB)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Lara Croft Guardian of Light Trailer 2 (ESRB)
+setManifestid(910, "1332084816209327708", 0)
