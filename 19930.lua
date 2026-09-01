e72622868741bb1307e0b976bdac835b9f0ce453
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:52:39 +0800

    add 19930.lua

---
+++ 19930.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19930.lua
+--Gamename The Settlers: Rise of an Empire Gold Edition
+addappid(19930) --Mainappid The Settlers: Rise of an Empire Gold Edition
+addappid(19931, 1, "b4d49eef28263e76c486fcd8cf5981d7c9017a83209ac0a7827f85b04a29aeb4") --Main Windows Depot The Settlers: Rise of an Empire Gold Edition
+setManifestid(19931, "5161287270536190729", 0)
+addappid(19932, 1, "8b41d00c8b39e8cf86a82c2e7f0c214ab5cfc4306facc7f8b68ba4a6fad22783") --Main Windows Language Depot Spanish The Settlers: Rise of an Empire Gold Edition
+setManifestid(19932, "4182341608814660137", 0)
+addappid(19933, 1, "0cb156700d8f77569f76161f8b55fab213268762c909e1fdb105246dea38dfd6") --Main Windows Language Depot French The Settlers: Rise of an Empire Gold Edition
+setManifestid(19933, "5721189374154742833", 0)
