bc5e7f3592dc06bff14bce6d68ea5d45a390addd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:29:20 +0800

    add 5295.lua

---
+++ 5295.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5295.lua
+--Gamename ARMA 2 Citizens Trailer
+addappid(5295) --Mainappid ARMA 2 Citizens Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot ARMA 2 Citizens Trailer
+setManifestid(910, "1332084816209327708", 0)
