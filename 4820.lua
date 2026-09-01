b8c262f8eeca2b5608c79172bbf7124483a2edf3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:34:01 +0800

    add 4820.lua

---
+++ 4820.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4820.lua
+--Gamename Heroes of Annihilated Empires Multiplayer Demo
+addappid(4820) --Mainappid Heroes of Annihilated Empires Multiplayer Demo
+addappid(4821, 1, "5d28ea3084b976ef34506f034cd7bd32d65a3b0e6c6b9bb49240ffc60a423cc4") --Main Windows Depot Heroes of Annihilated Empires Multiplayer Demo
+setManifestid(4821, "2465835998030143719", 0)
