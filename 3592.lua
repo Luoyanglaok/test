84575b2f25938ba5d7e476d46a0f78f326050a95
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:28:01 +0800

    add 3592.lua

---
+++ 3592.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3592.lua
+--Gamename Plants vs. Zombies Demo
+addappid(3592) --Mainappid Plants vs. Zombies Demo
+addappid(3591, 1, "1625fdac425750d0736c420f3877d3b4b4119d1d361629c2d12b360eb3689839") --Main Windows Depot Plants vs. Zombies Demo
+addappid(3593, 1, "2fec966bebbf74b5fad937c61ad06d393d521b64f27d97992ae7ef3775c50c10") --Main Windows Depot Plants vs. Zombies Demo
