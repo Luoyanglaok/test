ec1a95d652856d58d55e2fab19ea12e7a0e779bc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:54:19 +0800

    add 21070.lua

---
+++ 21070.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21070.lua
+--Gamename Wanted: Weapons of Fate
+addappid(21070) --Mainappid Wanted: Weapons of Fate
+addappid(21071, 1, "61cbe596c360972871e64fdfe1d99e65e0da5547a9e441c92140f9eaff38924a") --Main Windows Depot Wanted: Weapons of Fate
+setManifestid(21071, "6732668385752941174", 0)
+addappid(21072, 1, "8bcd5b25ade5936d6a8b367f409a512a3dd28ff8798ba639c29093206dc6f89d") --Main Windows Depot Wanted: Weapons of Fate
+setManifestid(21072, "3614472722547748563", 0)
