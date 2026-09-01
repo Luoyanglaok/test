b3f916b146a53e398c4ea00fc29ca679847f0991
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:25:06 +0800

    add 9320.lua

---
+++ 9320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9320.lua
+--Gamename Dawn of War Demo
+addappid(9320) --Mainappid Dawn of War Demo
+addappid(9321, 1, "dfd2ee01ce3f648a73d4454acdc4812c98c9d8b2736348f0e944448dfdda1b6a") --Main Windows Depot Dawn of War Demo
+setManifestid(9321, "1953017994359085235", 0)
