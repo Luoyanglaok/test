c3ad0d296bb4302ae44ac1fd2c7bb7a869dde088
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:42:20 +0800

    add 36910.lua

---
+++ 36910.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 36910.lua
+--Gamename All Aspect Warfare
+addappid(36910) --Mainappid All Aspect Warfare
+addappid(36911, 1, "6b146acdaa8ec26558b1d8685edc17bc7b952008e4c259008e85c48abf0aaaa4") --Main Windows Language Depot English All Aspect Warfare
+setManifestid(36911, "7670984058295111759", 387824304)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Main Windows Depot All Aspect Warfare
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Main Windows Depot All Aspect Warfare
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot All Aspect Warfare
