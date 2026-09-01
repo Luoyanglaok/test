6e2e6c1c8e191a15714ebdd4629f1537ed4c6800
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:16:36 +0800

    add 6530.lua

---
+++ 6530.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6530.lua
+--Gamename Lost Planet: Extreme Conditions Demo
+addappid(6530) --Mainappid Lost Planet: Extreme Conditions Demo
+addappid(6531, 1, "6459d850864ca9d959805f325d78ad25686e4561d9bf9d65568ca93bb64898ba") --Main Windows Depot Lost Planet: Extreme Conditions Demo
+setManifestid(6531, "4735891655115823026", 0)
