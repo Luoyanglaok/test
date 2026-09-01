30277aedef3d1bb31ae357b9ddb3879dca21359a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:26:00 +0800

    add 7.lua

---
+++ 7.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7.lua
+--Gamename Steam Client
+addappid(7) --Mainappid Steam Client
+addappid(8, 1, "20af1043db56d6408ddd965580e1269005b6e04f81c3676d155174f73099da95") --Main Windows Depot Steam Client
