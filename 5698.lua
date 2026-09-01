89f42e4ce5cca3dfa5fe84ca211d4c4df56d1752
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:49:22 +0800

    add 5698.lua

---
+++ 5698.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5698.lua
+--Gamename Max and the Magic Marker
+addappid(5698) --Mainappid Max and the Magic Marker
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Max and the Magic Marker
+setManifestid(910, "1332084816209327708", 0)
