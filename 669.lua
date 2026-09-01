0b58d9599b57a597cf8b511bf635df950ac87908
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:47:57 +0800

    add 669.lua

--- 669.lua
+++ 669.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 669.lua
-addappid(669, 0, "1809a6d00574715c89580593ff84caeba350810dd44f2f9bdaf57ffc63fb4548")
+addappid(669, 0, "4dc0bb90f996b5c51ee07de3b18f725be0e2a0beb123a9f04fce2342874ad236")
