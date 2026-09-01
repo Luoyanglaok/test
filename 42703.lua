27ed9ca9ed597c29cb591740f8dbf579f91c9dcd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:56:02 +0800

    add 42703.lua

---
+++ 42703.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42703.lua
+--Gamename Steam Store
+addappid(42703, 1, "48e008cbcb32413a3e7645bb624908b69f3c45a1e1bd2d288d710120e7db2872") --Mainappid Steam Store
+addappid(42704, 1, "ef154528aeaf6541d4b637fa5e7820e12f45f66844c66bae380dd91921e73e23") --Main Windows Depot Steam Store
