e55ada3dd3e99dae6e916942d98c71ee2b75a033
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:20:47 +0800

    add 7910.lua

---
+++ 7910.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7910.lua
+--Gamename The Movies: Stunts and Effects
+addappid(7910) --Mainappid The Movies: Stunts and Effects
+addappid(7901, 1, "AFE027A14C3DE2ED81993507D4BAD2E7DF096B7F4303F9FAF648B3C876934233") --Main Windows Depot The Movies: Stunts and Effects
+addappid(7902, 1, "4D2379B8106F3278ACA23DBA638670075B10583006FA004EB5C3FE8DD584400A") --Main Windows Depot The Movies: Stunts and Effects
+addappid(7911, 1, "77FB0636DECF3EA77558A28943E16EB5D43E30E47B2856B86CABEDBB7F9B7515") --Main Windows Depot The Movies: Stunts and Effects
+setManifestid(7911, "3527089021339152166", 0)
