8396688dacbdc34133c6de1fcf7bb67edb71296d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:24:16 +0800

    add 8863.lua

---
+++ 8863.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8863.lua
+--Gamename Steam Store
+addappid(8863, 1, "c22e104d1941ca7182d6f90e42cf3821bfb1ad513fea14aacea960b8ef9daac7") --Mainappid Steam Store
+addappid(8864, 1, "51a0ee58f457db9588d7d97f250840e7e078248292a2ed368453aa8173eefd87") --Main Windows Depot Steam Store
