8e81a687ed93f8c5f14fb2c142373638f5ff791e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:38:32 +0800

    add 644.lua

---
+++ 644.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 644.lua
+--Gamename Portal 2 Publishing Tool
+addappid(644) --Mainappid Portal 2 Publishing Tool
+addappid(209, 1, "65597f95b601e6c3f4b97d9044ab4c12ac772121cb0c7fb6c09495881e6421c6") --Main Windows Depot Portal 2 Publishing Tool
