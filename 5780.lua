7092d948fe75c0bf42c81f8cb1b3f52ec25ae118
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:59:08 +0800

    add 5780.lua

---
+++ 5780.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5780.lua
+--Gamename Numen_Eng
+addappid(5780) --Mainappid Numen_Eng
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Numen_Eng
