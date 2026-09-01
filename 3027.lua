82cb5826d79f029a07b2a7f092be82db2ec8baa0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:25:51 +0800

    add 3027.lua

---
+++ 3027.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3027.lua
+--Gamename Steam Store
+addappid(3027, 1, "0c92d52b172d18c6bee0da8106bfe9ee8226373f3e80b7633f5000f19c715da1") --Mainappid Steam Store
