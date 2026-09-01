a80eeccdcaa6d2720d684700702e92283d584113
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:39:43 +0800

    add 15800.lua

---
+++ 15800.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15800.lua
+--Gamename Planet Busters
+addappid(15800) --Mainappid Planet Busters
+addappid(15801, 1, "3dbfe60d0d0f74d502ea8c439cab3b6b321f7124e616b68124bbfd7e11debeee") --Main Windows Depot Planet Busters
+setManifestid(15801, "2579845417927983900", 0)
