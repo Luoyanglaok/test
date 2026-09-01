9e76c3f64871c0c9b4f34cafeae64d5acb147d31
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:18:40 +0800

    add 32000.lua

---
+++ 32000.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 32000.lua
+--Gamename Raycatcher
+addappid(32000) --Mainappid Raycatcher
+addappid(32001, 1, "083eb1c657913e3407a4d7f58e16d5e1590ea81f48eec39e8964aca558bdd70e") --Main Windows Depot Raycatcher
+setManifestid(32001, "1907574149466468854", 0)
