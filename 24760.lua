cd9c9e97a9494c51f94efaf98d87c29e8428723c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:13:42 +0800

    add 24760.lua

---
+++ 24760.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24760.lua
+--Gamename BattleForge
+addappid(24760) --Mainappid BattleForge
+addappid(24761, 1, "34d4f081839a33425cf5ecf588580b3524825231a827469f9a04605d0c6639b6") --Main Windows Depot BattleForge
+setManifestid(24761, "4117710660500721799", 0)
