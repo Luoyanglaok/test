d6125a57c8c601ab44707c9be671a473e59edef4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:28:42 +0800

    add 5289.lua

---
+++ 5289.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5289.lua
+--Gamename Prototype Intro
+addappid(5289) --Mainappid Prototype Intro
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Prototype Intro
+setManifestid(910, "1332084816209327708", 0)
