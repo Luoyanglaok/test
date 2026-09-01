e01d64911f3a45ce8fac868846364791e2849a8d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:18:49 +0800

    add 32010.lua

---
+++ 32010.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 32010.lua
+--Gamename Raycatcher Demo
+addappid(32010) --Mainappid Raycatcher Demo
+addappid(32011, 1, "fcf7c08afb10da75f3aae53174d1bb983129f6723db87abdddc16ebbf3067ed1") --Main Windows Depot Raycatcher Demo
+setManifestid(32011, "8767895372624017333", 0)
