3cb56a83b67adda23ea7b9ea0f71de7ce95c5cf2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:30:12 +0800

    add 34400.lua

---
+++ 34400.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34400.lua
+--Gamename NecroVisioN - Demo
+addappid(34400) --Mainappid NecroVisioN - Demo
+addappid(34401, 1, "6813ceb8ee00c2cc34e8966af1fe1b0bc9da33444d25430c57bc5f3db730eade") --Main Windows Depot NecroVisioN - Demo
+setManifestid(34401, "4312560960424125787", 0)
