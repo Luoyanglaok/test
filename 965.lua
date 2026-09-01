c84565e360206886d4d0c67bf2c1b816b4c6ca2d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:47:14 +0800

    add 965.lua

---
+++ 965.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 965.lua
+--Gamename TrackMania United Webisode 3: Maniazones
+addappid(965) --Mainappid TrackMania United Webisode 3: Maniazones
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot TrackMania United Webisode 3: Maniazones
+setManifestid(910, "1332084816209327708", 0)
