531f210437d72d18a60f7eb307f5181b37824212
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:13:51 +0800

    add 24830.lua

---
+++ 24830.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24830.lua
+--Gamename Godfather 2
+addappid(24830) --Mainappid Godfather 2
+addappid(24831, 1, "372b0e03c877c6b41a30b59efa111b3d71f907ae3d0d1b13ceddab55f833271c") --Main Windows Depot Godfather 2
+setManifestid(24831, "1667126148023322059", 0)
+addappid(24832, 1, "4b4a754d1a42c00c298d53e97216255f8bf483984d12aff74314b5d6f8cc3a53") --Main Windows Language Depot French Godfather 2
+setManifestid(24832, "994338303023666667", 0)
+addappid(24833, 1, "e60eb1b115b1b41db16fcd425c95181b826d9466638a65c8c37cec6f2a4cd419") --Main Windows Language Depot German Godfather 2
+setManifestid(24833, "3213367695191143066", 0)
+addappid(24834, 1, "810c030867f4026aaea4c3aeffd06c103663e4f346016713067d8700b4da0971") --Main Windows Language Depot Italian Godfather 2
+setManifestid(24834, "2385182819521791463", 0)
+addappid(24835, 1, "fdb936425bbebc43aa8a710f2bccf7397f1f2c39aa418e2431bf01c719f3d6f7") --Main Windows Language Depot Spanish Godfather 2
+setManifestid(24835, "1190738926504262359", 0)
