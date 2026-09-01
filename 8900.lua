f6e4dd7185b7509ce894aab2e3e46f6649968ac8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:24:24 +0800

    add 8900.lua

---
+++ 8900.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8900.lua
+--Gamename Freedom Force Demo
+addappid(8900) --Mainappid Freedom Force Demo
+addappid(8901, 1, "feb9c8389d8d5f563268749cacfed08c039b8e37275c5c4c17fa012c1f2a99ef") --Main Windows Depot Freedom Force Demo
+setManifestid(8901, "7328420897486641567", 0)
