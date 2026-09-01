8f937802aa2eef4ab0ad58fca70d76aaee23cc62
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:28:13 +0800

    add 3593.lua

---
+++ 3593.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3593.lua
+--Gamename Steam Store
+addappid(3593, 1, "2fec966bebbf74b5fad937c61ad06d393d521b64f27d97992ae7ef3775c50c10") --Mainappid Steam Store
