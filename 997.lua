6cbed26662857e8f85921df3114e43c4240bc5e3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:50:43 +0800

    add 997.lua

---
+++ 997.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 997.lua
+--Gamename Team Fortress 2: Meet the Demoman
+addappid(997, 1, "bf8c4e2dcf6b5aa991c4e693d884f9086b0da1056c72da175511b9fcf7138371") --Mainappid Team Fortress 2: Meet the Demoman
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Team Fortress 2: Meet the Demoman
+setManifestid(910, "1332084816209327708", 0)
+setManifestid(997, "3786203338061260159", 0)
