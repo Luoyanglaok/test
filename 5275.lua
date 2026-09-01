7977c3a3465bc256517b514272a88de6cb25de06
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:27:16 +0800

    add 5275.lua

---
+++ 5275.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5275.lua
+--Gamename America's Army 3 Teaser
+addappid(5275) --Mainappid America's Army 3 Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot America's Army 3 Teaser
+setManifestid(910, "1332084816209327708", 0)
