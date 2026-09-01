df469e03ad1e8d1c6cfefd8dd74edd59077a4db5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:19:37 +0800

    add 32700.lua

---
+++ 32700.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 32700.lua
+--Gamename Joint Operations: Escalation
+addappid(32700) --Mainappid Joint Operations: Escalation
+addappid(32691, 1, "44184de2bf92156526940373a4eb17c20440ddb3acd9d0880718b22b484233e9") --Main Windows Depot Joint Operations: Escalation
+setManifestid(32691, "3328923870620639996", 0)
+addappid(32701, 1, "6add74d44c5a816910b2bbc3b25d4fb9bbc768a4f9bb83ed265327f12c4eea38") --Main Windows Depot Joint Operations: Escalation
+setManifestid(32701, "5470257770221558010", 0)
