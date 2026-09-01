2b5c77bb748902a803dd003d2e7dedc6f33a4496
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:45:20 +0800

    add 17020.lua

---
+++ 17020.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 17020.lua
+--Gamename Global Agenda
+addappid(17020) --Mainappid Global Agenda
+addappid(17021, 1, "76ba95e02dba51b9c4c278d00255d4dce62ed68a078d3f496769db3bb94ca311") --Main Windows Depot Global Agenda
+setManifestid(17021, "6398637513017447820", 0)
