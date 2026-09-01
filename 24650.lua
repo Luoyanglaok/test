776d0d3fbff0a12dd2051497da9f60f25c0200d3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:12:47 +0800

    add 24650.lua

---
+++ 24650.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24650.lua
+--Gamename Trainz Settle and Carlisle
+addappid(24650) --Mainappid Trainz Settle and Carlisle
+addappid(24631, 1, "90b92b1630fa1fc26400da330418abc3b721c0d0991fdf7361cb692833868793") --Main Windows Depot Trainz Settle and Carlisle
+setManifestid(24631, "196455006608489102", 0)
+addappid(24651, 1, "a8dfe051c140aa6f72e025dd18ecae1c611ec453535f21478dd2b0a4e76aefce") --Main Windows Depot Trainz Settle and Carlisle
+setManifestid(24651, "7230325568310989750", 0)
