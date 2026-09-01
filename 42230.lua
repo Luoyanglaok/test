6fa04f0846945e8f95f710a54023dda9acea4cb3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:54:34 +0800

    add 42230.lua

---
+++ 42230.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42230.lua
+--Gamename Nancy Drew®: Trail of the Twister
+addappid(42230) --Mainappid Nancy Drew®: Trail of the Twister
+addappid(42231, 1, "234e9b5ead31c65b5c4176e1d29e49f84337d5b4201102868d279ee4fa06e2fd") --Main Windows Depot Nancy Drew®: Trail of the Twister
+setManifestid(42231, "7778475104119233482", 944614272)
+addappid(42232, 1, "c67d8e07168861edccc9e8f6899429386961aed38f5510a9b37bbf9c29522066") --Main Macos Depot Nancy Drew®: Trail of the Twister
+setManifestid(42232, "9202824122408515723", 0)
