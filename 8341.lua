9057fd1014d9775effa713fc1d35872f3f28ae38
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:22:14 +0800

    add 8341.lua

---
+++ 8341.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8341.lua
+--Gamename Steam Store
+addappid(8341, 1, "d3c32c56e8de0fa05024762233481727e814986ec6d910de252cda83cd890dbc") --Mainappid Steam Store
+setManifestid(8341, "1785508718323614451", 0)
+addappid(8342, 1, "f381b12c9083ec6cfbbe73c2b0d47c15685d9d99d6e0fe0cd18ecfcb46e6c162") --Main Macos Depot Steam Store
+setManifestid(8342, "6200335863558219789", 0)
+--Dlcname Strong Bad Episode 1: Homestar Ruiner
+addappid(8340) --Dlcname Strong Bad Episode 1: Homestar Ruiner
