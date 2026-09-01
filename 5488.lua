6ce508cd058efa3fa6d2bc0694c2dc9684c8617b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:40:01 +0800

    add 5488.lua

---
+++ 5488.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5488.lua
+--Gamename Devil May Cry 4
+addappid(5488) --Mainappid Devil May Cry 4
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Devil May Cry 4
+setManifestid(910, "1332084816209327708", 0)
