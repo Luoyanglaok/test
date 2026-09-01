890ddbc240612e2014d921c1ba93eebc129bfb6c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:29:28 +0800

    add 3850.lua

---
+++ 3850.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3850.lua
+--Gamename BloodRayne 2 Demo
+addappid(3850) --Mainappid BloodRayne 2 Demo
+addappid(3851, 1, "b21df7773c07b95249490a63c915c5d91fa52e97253ddb4a72fcd378d1d3aca2") --Main Windows Depot BloodRayne 2 Demo
+setManifestid(3851, "4164659898830557299", 0)
