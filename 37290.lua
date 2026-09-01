08e2226e40d4f82a8cd8265f5b41a268262aef04
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:43:05 +0800

    add 37290.lua

---
+++ 37290.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37290.lua
+--Gamename Zenerchi®
+addappid(37290) --Mainappid Zenerchi®
+addappid(37291, 1, "ef65b4b14b84406c289749f8d30ea79cafd35b98e88d26c9aa2919e6e9bc882f") --Main Windows Depot Zenerchi®
+setManifestid(37291, "3297199749378962102", 0)
