65823a3f247739d689fbbe57b23e44149f1233fc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:24:32 +0800

    add 8910.lua

---
+++ 8910.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8910.lua
+--Gamename Freedom Force vs. the Third Reich Demo
+addappid(8910) --Mainappid Freedom Force vs. the Third Reich Demo
+addappid(8911, 1, "33db293eda00a0afeab313bf83b75bb9f9fc843967d5813934498cfea94cceba") --Main Windows Depot Freedom Force vs. the Third Reich Demo
+setManifestid(8911, "2505720806089776446", 0)
