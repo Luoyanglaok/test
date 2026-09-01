8bbbb8cb1fe8118c75bfe0bdc041140380b5725c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:41:40 +0800

    add 5521.lua

---
+++ 5521.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5521.lua
+--Gamename Virtua Tennis 2009 Final Trailer
+addappid(5521) --Mainappid Virtua Tennis 2009 Final Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Virtua Tennis 2009 Final Trailer
+setManifestid(910, "1332084816209327708", 0)
