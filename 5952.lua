77172b4dd3bee14dac92a50845e38a18ded826fb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:12:17 +0800

    add 5952.lua

---
+++ 5952.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5952.lua
+--Gamename Left 4 Dead - The Sacrifice Teaser
+addappid(5952) --Mainappid Left 4 Dead - The Sacrifice Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Left 4 Dead - The Sacrifice Teaser
+setManifestid(910, "1332084816209327708", 0)
