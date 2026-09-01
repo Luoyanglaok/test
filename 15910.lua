f8679a96b3ef65548d99bb983ef4e3d2724cd2b2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:39:52 +0800

    add 15910.lua

---
+++ 15910.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15910.lua
+--Gamename Luxor Amun Rising
+addappid(15910) --Mainappid Luxor Amun Rising
+addappid(15911, 1, "d0042db6da385f64367291ed4af0143d7abbc08adc1ad8f6007049ad9235437e") --Main Windows Depot Luxor Amun Rising
+setManifestid(15911, "9203417229911749715", 0)
+addappid(15913, 1, "4A364E3E58C9E482E30A409A52C106343F657FCDCA161DAA84FCBA328D6D2456") --Main Windows Language Depot German Luxor Amun Rising
+setManifestid(15913, "4642045882336091709", 0)
+addappid(15914, 1, "EE23E1FC15FD9DEC8FBB9E7C8A1274A9263DF46E32E2AD838330E32C45B974FD") --Main Windows Language Depot French Luxor Amun Rising
+setManifestid(15914, "2885210921744872118", 0)
+addappid(15915, 1, "FE9DA7B295AA36096F7053A4DF8ADD69CB6B41C0A0FD1FF0EC47A5A8D2E2B137") --Main Windows Language Depot Swedish Luxor Amun Rising
+setManifestid(15915, "1863150900561600960", 0)
+addappid(15916, 1, "c8931e55433cc6b461e5b87d278466438c99363d839d7600de284b8173dc49f0") --Main Macos Depot Luxor Amun Rising
+setManifestid(15916, "2269828251841500024", 0)
