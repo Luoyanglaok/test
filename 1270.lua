eb142d58efb45d9e6a39f2accb17ebad59aaca77
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:19:42 +0800

    add 1270.lua

---
+++ 1270.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1270.lua
+--Gamename Killing Floor Beta
+addappid(1270) --Mainappid Killing Floor Beta
+addappid(1271, 1, "7b336f657f13dfbfec5f51cf4413a4f949511c089d713ab1fa279568cc0e4ac9") --Main Windows Depot Killing Floor Beta
