a262b3b0fdc9cf8c52c16636e965cf6ae55bd168
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:27:47 +0800

    add 5278.lua

---
+++ 5278.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5278.lua
+--Gamename America's Army 3 New Features
+addappid(5278) --Mainappid America's Army 3 New Features
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot America's Army 3 New Features
+setManifestid(910, "1332084816209327708", 0)
