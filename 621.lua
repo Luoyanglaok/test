71683d9813f2f4c527a097a42529b599217e209e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:38:22 +0800

    add 621.lua

---
+++ 621.lua
@@ -0,0 +1,25 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 621.lua
+--Gamename Steam Store
+addappid(621, 1, "d2718f2424c94832a7112661774d98ffb3d52f454312f543a71418d253100d1d") --Mainappid Steam Store
+setManifestid(621, "9122696443005314333", 7509295520)
+addappid(622, 1, "0be76bef0b464b2d58b6ced70c86e608708c18c1204836cb1bb5d209776f8a0e") --Main Windows Depot Steam Store
+setManifestid(622, "916560128540961379", 27379104)
+--Dlcname Portal 2
+addappid(620, 1, "a33ddcda802d5d2db32b70ce263fe29efdcef1f2e534c5d4e7f515a20b31b6a5") --Dlcname Portal 2
+addappid(623, 1, "6a9fe60f83046355da81ea12f8fe895e3f59f1223d1689cc7e1ba327358e99fb") --Dlc Macos Depot Portal 2
+setManifestid(623, "2276453204441793614", 31994448)
+addappid(624, 1, "48fcb7e647902c2d9467ef3f4405e1b6308981949705cb2b7489bb3d22aead66") --Dlc Windows Depot Portal 2
+setManifestid(624, "8207314901980262131", 7690320)
+addappid(625, 1, "d120d4c37faea11be77c787dc48000f297aaf8200a4265969a4f5dbeebc0326c") --Dlc Windows Language Depot French Portal 2
+setManifestid(625, "1128685895801277145", 690630480)
+addappid(626, 1, "50611154e0550112cb9f9edcc44cf32bb90a7fe8d2f5738f6f24b7ced43cf7c2") --Dlc Windows Language Depot German Portal 2
+setManifestid(626, "6439173920052159773", 687653744)
+addappid(627, 1, "4ff7fb0bde51bae4f2c7a9bb220fd2da9bd9b3b8210d222c615acf28a8fdd821") --Dlc Windows Language Depot Spanish Portal 2
+setManifestid(627, "4705364648410025995", 686661072)
+addappid(628, 1, "7f299ff4eec6b9de24869bb482a325b72f792798c87268f03feda25d4f3361c5") --Dlc Windows Language Depot Russian Portal 2
+setManifestid(628, "1134745004760172192", 716027584)
+addappid(660, 1, "759d2251fd0bb050f088c166dc269ea869ddafde525032db3095fbf06cc0dfc3") --Dlc Windows Depot Portal 2
+setManifestid(660, "2679694482538379167", 444528944)
+addappid(661, 1, "5e338dd8d07e4cda5dfbcc7eb03418d9b1d93192bc20d5aac2174b851b11c1a6") --Dlc Linux Depot Portal 2
+setManifestid(661, "8164832528872470690", 27596896)
