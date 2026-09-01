171d0cc942cb8489c9cf04e4d62fc84a9fa48d41
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:24:35 +0800

    add 2790.lua

---
+++ 2790.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2790.lua
+--Gamename Atari 80 Classics in One
+addappid(2790) --Mainappid Atari 80 Classics in One
+addappid(2791, 1, "672c931fac1d769b87044adb7d2094f8e3c4598c620d8a0351acd5d422906b02") --Main Windows Depot Atari 80 Classics in One
+setManifestid(2791, "3803613861856895051", 0)
