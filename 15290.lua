d09aa13ce5558bcc771554d528c65de779de00ab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:37:56 +0800

    add 15290.lua

---
+++ 15290.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15290.lua
+--Gamename Lost: Via Domus
+addappid(15290) --Mainappid Lost: Via Domus
+addappid(15291, 1, "8436a00bccf7e0649d9b3a2cd89658cffd7c6e8fcd4145230e31a983767abcc7") --Main Windows Depot Lost: Via Domus
+setManifestid(15291, "5904449495271107723", 0)
