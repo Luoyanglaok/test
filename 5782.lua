ccbadc29d028e2e8f7c1f250deddfc8dafb51020
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:59:24 +0800

    add 5782.lua

---
+++ 5782.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5782.lua
+--Gamename TheGuild2
+addappid(5782) --Mainappid TheGuild2
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot TheGuild2
