231deb8600ae56dee49ad7406ae412aff5be7a67
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:59:36 +0800

    add 42900.lua

---
+++ 42900.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42900.lua
+--Gamename Hearts of Iron III: Semper Fi
+addappid(42900, 1, "4ee92618aa09d0df1ff03ec58d6c5de7c75947aacb563050efc1c27c51a8cdb6") --Mainappid Hearts of Iron III: Semper Fi
+addappid(42901, 1, "311b56025117c376222b13b0f5425f4aba74268f89f9997c5c62a7501a817bc6") --Main Windows Depot Hearts of Iron III: Semper Fi
+--Dlcname Hearts of Iron III Semper Fi: Dies Irae Gotterdammerung
+addappid(42902, 1, "1de38982535bd81734642c6070c9030024dce5abebceda0f3d3a3ee0182f3257") --Dlcname Hearts of Iron III Semper Fi: Dies Irae Gotterdammerung
+--Dlcname Hearts of Iron III: For the Motherland
+addappid(42903, 1, "e8db585d12413c01d5be30477e88a5d4777a85319fd5b4abeae5649dc6a8b779") --Dlcname Hearts of Iron III: For the Motherland
+--Dlcname Hearts of Iron III: Stars and Stripes
+addappid(42905, 1, "ecfb3e96d427ebbbe63fc27f79b81deaed5ecba856a65e44289a7e4a416db935") --Dlcname Hearts of Iron III: Stars and Stripes
