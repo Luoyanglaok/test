31b556e98825822bc2f3cd04630d1820f9651fb0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:38:11 +0800

    add 5033.lua

---
+++ 5033.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5033.lua
+--Gamename Team Fortress 2: Meet the Scout (French)
+addappid(5033) --Mainappid Team Fortress 2: Meet the Scout (French)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Team Fortress 2: Meet the Scout (French)
+setManifestid(910, "1332084816209327708", 0)
