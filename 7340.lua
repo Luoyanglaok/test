0a616325441e4b4d6efd41b8eb7583f953d2f519
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:18:41 +0800

    add 7340.lua

---
+++ 7340.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7340.lua
+--Gamename Azada
+addappid(7340) --Mainappid Azada
+addappid(7341, 1, "2e93f523ed2cc87a579da95ce2e7e40f4f584e0a83b8743bc16ad66774470432") --Main Windows Depot Azada
+setManifestid(7341, "1733506316105306298", 48484464)
