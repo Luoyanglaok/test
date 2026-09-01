3a2adc4eb0eb83a58811051ba6f5c602d8823de4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:22:12 +0800

    add 2510.lua

---
+++ 2510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2510.lua
+--Gamename Shadowgrounds Demo
+addappid(2510) --Mainappid Shadowgrounds Demo
+addappid(2511, 1, "1e039fc965d2a6df67ef5b2703f089b201215de73454ff3a94d0e38028f316a2") --Main Windows Depot Shadowgrounds Demo
+setManifestid(2511, "2154011757532778564", 0)
