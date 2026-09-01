f96268c9d0f36a0cd52096da4d5d37d3394cf19a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:50:23 +0800

    add 40402.lua

---
+++ 40402.lua
@@ -0,0 +1,27 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 40402.lua
+--Gamename Steam Store
+addappid(40402, 1, "0a8cda83307c0745467144c478af3722593b072d1cb24208a97f5f339f94c180") --Mainappid Steam Store
+setManifestid(40402, "4589889734460397170", 25441664)
+addappid(40403, 1, "8947f3d36ad485e20e8d52c4f2d54d43f39c7412a91748981a92920395eb03ba") --Main Windows Depot Steam Store
+setManifestid(40403, "3857346319108786759", 159221360)
+--Dlcname AI War: Fleet Command
+addappid(40400) --Dlcname AI War: Fleet Command
+addappid(40401, 1, "df1b8782b087c746b1c9085d6faba9c80c79ece0ca5b704daddcfade41bfa3f7") --Dlc Windows Depot AI War: Fleet Command
+setManifestid(40401, "1153555666216009501", 267661760)
+addappid(40404, 1, "a1c8521cf15b06e1777c0f76c8cfb5600ce469aacab6b914e7dbd95571643f2e") --Dlc Windows Depot AI War: Fleet Command
+setManifestid(40404, "9051064516144152399", 15195472)
+addappid(40405, 1, "105e86f804745968ced7f545424bc6ed06ac8628aebd730034fbcf01b529de01") --Dlc Macos Depot AI War: Fleet Command
+setManifestid(40405, "6500706439988356202", 31117280)
+addappid(40406, 1, "988d656000e1008d552c2452960b3acb80582182540145cba254a45ade4f60ba") --Dlc Windows Depot AI War: Fleet Command
+setManifestid(40406, "5201540245363576347", 56632688)
+addappid(40407, 1, "9ef2f848607a5a282561f424eb428dc52e1dab5f147930e020101adc86072fa8") --Dlc Windows Depot AI War: Fleet Command
+setManifestid(40407, "384718767628109285", 157372672)
+addappid(40408, 1, "3885296205efa3b5bc66253a09a8ce1f4adc5104417ca4dc64ae02a77e12eeeb") --Dlc Windows Depot AI War: Fleet Command
+setManifestid(40408, "2303977033692632760", 131691264)
+addappid(40409, 1, "4cea6a84984623bfaf9e02c6eadcbcb8f3f5713b69fa8662608bbf9eaafb5798") --Dlc Windows Depot AI War: Fleet Command
+setManifestid(40409, "7462447101832519099", 1536)
+addappid(238950, 1, "9ba0509f6fc21fdfe80fa47f50122a580803f67e4f8cc595e840028bcc03a506") --Dlc Windows Depot AI War: Fleet Command
+setManifestid(238950, "901474767367031186", 97989376)
+addappid(317150, 1, "dc67f75ba4593188812393c72e8b35ba45ac1bff2e52d228a17e151997b32fa6") --Dlc Windows Depot AI War: Fleet Command
+setManifestid(317150, "3748021006022689622", 55365600)
