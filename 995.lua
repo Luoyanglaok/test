b0fd0f74bdba8b3e2c5e015b585605c37de938f4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:50:02 +0800

    add 995.lua

---
+++ 995.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 995.lua
+--Gamename The Orange Box Commercial
+addappid(995) --Mainappid The Orange Box Commercial
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot The Orange Box Commercial
+setManifestid(910, "1332084816209327708", 0)
