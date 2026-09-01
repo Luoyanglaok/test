900b1526a7e2b8b4fcb760ee9af159a6a803e863
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:56:15 +0800

    add 21920.lua

---
+++ 21920.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21920.lua
+--Gamename Wheelman
+addappid(21920) --Mainappid Wheelman
+addappid(21921, 1, "48212a872a3462d3d62704b6314b9e3e44fa77ee31d56cb4042743c21e291d88") --Main Windows Depot Wheelman
+setManifestid(21921, "5240734268059455158", 0)
+addappid(21922, 1, "28db537036f8d59eec322f9e472894a11027292b29c151a0dc5fee8824cab54b") --Main Windows Language Depot French Wheelman
+setManifestid(21922, "5765270274356118766", 0)
+addappid(21923, 1, "7008213e20a0a01a2ac14acb741a9a468823d5c495fc5dc5cc23ed2ff3a2c983") --Main Windows Language Depot German Wheelman
+setManifestid(21923, "1664002394732485146", 0)
+addappid(21924, 1, "2bba4d0380ec3ee63d7dbd5eb390d2f6ac535fc807da6d732dc2a8403646d29b") --Main Windows Language Depot Italian Wheelman
+setManifestid(21924, "5010521207910469847", 0)
+addappid(21925, 1, "22e13531da63edbcd516eb2c37373bfcd39c06c900f10203f448ce6dd6300504") --Main Windows Language Depot Spanish Wheelman
+setManifestid(21925, "4542299219311909258", 0)
