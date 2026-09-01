932f60f2a4f9b5814d9348e63ce09cd5eda3c4ba
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:54:51 +0800

    add 5754.lua

---
+++ 5754.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5754.lua
+--Gamename KL2 Cops Robbers Mode (English)
+addappid(5754) --Mainappid KL2 Cops Robbers Mode (English)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot KL2 Cops Robbers Mode (English)
+setManifestid(910, "1332084816209327708", 0)
