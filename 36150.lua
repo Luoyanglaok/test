eafde673b464085a64861e0c18870b6068d39f3e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:41:02 +0800

    add 36150.lua

---
+++ 36150.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 36150.lua
+--Gamename Westward Collection
+addappid(36150) --Mainappid Westward Collection
+addappid(36151, 1, "2e37ac8ce2cb2ba0c750893ae6ac592ac0ed6eae298bc19991c3156ae4cff002") --Main Windows Depot Westward Collection
+setManifestid(36151, "7976705157602283843", 0)
