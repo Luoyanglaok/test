b1b6d3928d97d701447f00e4634fc2a2bba3162c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:29:40 +0800

    add 10510.lua

---
+++ 10510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10510.lua
+--Gamename The Incredible Hulk: The Official Videogame
+addappid(10510) --Mainappid The Incredible Hulk: The Official Videogame
+addappid(10511, 1, "1ee8523ea6cccbb77331f1c4f4119d46c6cec6f04f55d8c06ae619f2aa60b1b6") --Main Windows Depot The Incredible Hulk: The Official Videogame
+setManifestid(10511, "2455476488834638434", 0)
