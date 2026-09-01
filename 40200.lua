9e22fec411ec348eeec4482feb9dab878defb1ca
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:49:31 +0800

    add 40200.lua

---
+++ 40200.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 40200.lua
+--Gamename ItzaBitza
+addappid(40200) --Mainappid ItzaBitza
+addappid(40201, 1, "b848b1b3d5c8dac0089c397d98ad5269a8b2adb512b34291efe5cd61ef43bfa4") --Main Windows Depot ItzaBitza
+setManifestid(40201, "6848816745063250363", 0)
