5bf218594f151056ee3270fff1b53ad318530bfa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:51:01 +0800

    add 18800.lua

---
+++ 18800.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 18800.lua
+--Gamename Zero Gear Demo
+addappid(18800) --Mainappid Zero Gear Demo
+addappid(18801, 1, "a27556136e3fdf2ff903479d5e6ae97accfd7be69d68dd239e8c8495db259dde") --Main Windows Depot Zero Gear Demo
