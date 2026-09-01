76e6a31766664d87bb3b78435a80db62bd8af4a8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:57:14 +0800

    add 42709.lua

---
+++ 42709.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42709.lua
+--Gamename Steam Store
+addappid(42709, 1, "765bedbba80d60fb32bd6ece8b768bbcfd0e3c06046aad7f551613c1f485f5cd") --Mainappid Steam Store
