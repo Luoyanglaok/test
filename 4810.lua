453e0e403ff0f5ba4bac26927433704dad02b872
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:33:51 +0800

    add 4810.lua

---
+++ 4810.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4810.lua
+--Gamename Heroes of Annihilated Empires Demo
+addappid(4810) --Mainappid Heroes of Annihilated Empires Demo
+addappid(4811, 1, "ab2c11c0edfe05766153cf81259eb60d2c571099a9713da57e77652dda762022") --Main Windows Depot Heroes of Annihilated Empires Demo
+setManifestid(4811, "1126807805064559813", 0)
+addappid(4812, 1, "8aaa5cdef0c33e5707c67c2993834a51338c8faf73e368a3e4199fa4de8339f0") --Main Windows Language Depot French Heroes of Annihilated Empires Demo
+setManifestid(4812, "6883274969935337384", 0)
+addappid(4813, 1, "f37e768c210e099183ac7beb601e4849b17eeda566b3a64ad2675ba3700eb5c8") --Main Windows Language Depot German Heroes of Annihilated Empires Demo
+setManifestid(4813, "6320886720101474415", 0)
+addappid(4814, 1, "1957ecc29ab92c2d88232bafc121c04da07187ec8736bf79ce040f30f4cf068c") --Main Windows Language Depot Russian Heroes of Annihilated Empires Demo
+setManifestid(4814, "6966492981428853069", 0)
