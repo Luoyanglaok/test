07eff707ccc7c5247e27bb1516ede29122155e34
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:46:06 +0800

    add 565.lua

--- 565.lua
+++ 565.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 565.lua
-addappid(565, 0, "6131efb09daf3c59dfe74ddbb441f22341434afe4b4056226d0f948a6f62df1e")
+addappid(565, 0, "83c03096d680ebefba1ff6f4b8d0979ce3ae64f44e42f4d5c6945dae31802688")
