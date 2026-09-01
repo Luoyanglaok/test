503bd5f1ecbdd78a0b673ad31c043265c21287a0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:19:24 +0800

    add 7450.lua

---
+++ 7450.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 7450.lua
+--Gamename Ricochet Infinity
+addappid(7450) --Mainappid Ricochet Infinity
+addappid(7451, 1, "a65ce87a6f89c60acabfce373c43f54ba2647e7975000f0849dadae0e4f14582") --Main Windows Depot Ricochet Infinity
+setManifestid(7451, "7387428238757223841", 0)
