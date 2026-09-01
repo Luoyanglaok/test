f9ed5cfe058437d8b504ad9fba412bcc78f09224
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:57:47 +0800

    add 1257.lua

--- 1257.lua
+++ 1257.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 1257.lua
-addappid(1257, 0, "d83cd320227281f6530773c725a765eb79cdb224760e36e4c9f6dace6c4b2f6e")
+addappid(1257, 0, "8d9e83f4119e0fee05156fab9b538c82a16209e081bb218496362f310a83f9a0")
