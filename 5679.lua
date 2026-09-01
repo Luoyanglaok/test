b377a916baa1da8573d991d00e93bd9e3229a355
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:47:48 +0800

    add 5679.lua

---
+++ 5679.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5679.lua
+--Gamename BLUR3a_trailer
+addappid(5679) --Mainappid BLUR3a_trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot BLUR3a_trailer
