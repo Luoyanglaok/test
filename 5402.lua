d11ec94fdb0d5dd189acb7321502e257c365f107
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:36:57 +0800

    add 5402.lua

---
+++ 5402.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5402.lua
+--Gamename Napoleon: Total War (Russian) Trailer
+addappid(5402) --Mainappid Napoleon: Total War (Russian) Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Napoleon: Total War (Russian) Trailer
+setManifestid(910, "1332084816209327708", 0)
