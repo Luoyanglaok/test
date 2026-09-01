c67b48558d6aca86a5e2f7d287e9c2beffc34be4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:55:51 +0800

    add 21750.lua

---
+++ 21750.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21750.lua
+--Gamename CSI: NY - The Game
+addappid(21750) --Mainappid CSI: NY - The Game
+addappid(21751, 1, "6b91a83f044a4e84fa191a5205110562854b1f259973d3970a547a8ea14a155b") --Main Windows Depot CSI: NY - The Game
+setManifestid(21751, "2534001674724255884", 0)
