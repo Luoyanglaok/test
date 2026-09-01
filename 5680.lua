267968a689df6b2554d26d51538f232f77a349fd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:47:58 +0800

    add 5680.lua

---
+++ 5680.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5680.lua
+--Gamename BLUR4_trailer
+addappid(5680) --Mainappid BLUR4_trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot BLUR4_trailer
