f89653a517508183ce99ea935a4786cec79d8f1b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:13:42 +0800

    add 5091.lua

---
+++ 5091.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5091.lua
+--Gamename Team Fortress 2: Meet the Demoman (Russian)
+addappid(5091) --Mainappid Team Fortress 2: Meet the Demoman (Russian)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Team Fortress 2: Meet the Demoman (Russian)
+setManifestid(910, "1332084816209327708", 0)
