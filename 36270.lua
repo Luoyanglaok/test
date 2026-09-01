64d4301619359d7848cfd37c8c53f1ed18b3ccd5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:41:46 +0800

    add 36270.lua

---
+++ 36270.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 36270.lua
+--Gamename Granny in Paradise
+addappid(36270) --Mainappid Granny in Paradise
+addappid(36271, 1, "f64351b07d76c2744073637e8a1592f7e8f36c9385ecef8bb46becc01ce5e747") --Main Windows Depot Granny in Paradise
+setManifestid(36271, "6993949268932906100", 0)
