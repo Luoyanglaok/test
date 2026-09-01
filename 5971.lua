7a8cfc4347c618e4f963fa075fefc032d12a0148
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:13:41 +0800

    add 5971.lua

---
+++ 5971.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5971.lua
+--Gamename MOH - Launch Trailer
+addappid(5971) --Mainappid MOH - Launch Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot MOH - Launch Trailer
+setManifestid(910, "1332084816209327708", 0)
