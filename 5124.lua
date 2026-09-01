5171ddf166fe9e9f99f429eed9144f0e28465206
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:16:11 +0800

    add 5124.lua

---
+++ 5124.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5124.lua
+--Gamename Far Cry 2: Anything Goes
+addappid(5124) --Mainappid Far Cry 2: Anything Goes
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Far Cry 2: Anything Goes
+setManifestid(910, "1332084816209327708", 0)
