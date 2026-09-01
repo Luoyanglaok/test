a4f384fe5b18dff49070be2acd75338ae50b68dc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:40:43 +0800

    add 5500.lua

---
+++ 5500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5500.lua
+--Gamename Shattered Horizon Silent Running Trailer
+addappid(5500) --Mainappid Shattered Horizon Silent Running Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Shattered Horizon Silent Running Trailer
+setManifestid(910, "1332084816209327708", 0)
