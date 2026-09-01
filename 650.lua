20f9f2a8f45e2ea9bdf4d48cbd1bb4a7fe6597c6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:10:08 +0800

    add 650.lua

--- 650.lua
+++ 650.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 650.lua
-addappid(650, 0, "d732467971831c1cb0f0621c561ab6b4aa046f838a6e2fa0dcddb50ada3f2137")
+addappid(650, 0, "9a0fc84e3f77ef92fcd9636cce61aab256ff2cdbd0b169d6b0a085c37a700aee")
