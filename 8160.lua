74aee65c81988f427d583224ea80f5b4200a7406
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:22:01 +0800

    add 8160.lua

---
+++ 8160.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8160.lua
+--Gamename Shellshock 2: Blood Trails
+addappid(8160) --Mainappid Shellshock 2: Blood Trails
+addappid(8161, 1, "8df2eb867be2b5a975c956224bb87735377508a43631283713758d8ab75bbd0c") --Main Windows Depot Shellshock 2: Blood Trails
+setManifestid(8161, "5594730511899216480", 0)
+addappid(8162, 1, "d5a4c6c9a1513fe7b11fc2601c48cc1af072b68145528ce5626bf44157a30bca") --Main Windows Language Depot English Shellshock 2: Blood Trails
+setManifestid(8162, "5427926646642091851", 0)
+addappid(8163, 1, "a325d02d1d5e3f9f399ff84ec80414ccce0ca14fd544d936ee5d4d43dd915d50") --Main Windows Language Depot French Shellshock 2: Blood Trails
+setManifestid(8163, "7883460018705716160", 0)
+addappid(8164, 1, "2136f76a4436dd9a1ceb2bf2773822392927f6655c4e79b76ffb2ff8f1dc6739") --Main Windows Language Depot German Shellshock 2: Blood Trails
+setManifestid(8164, "2872645734094018354", 0)
+addappid(8165, 1, "eb19792b2db522bf0e869b483c542bd1d768ccf4cc97c08ac73a59210cac401c") --Main Windows Language Depot Italian Shellshock 2: Blood Trails
+setManifestid(8165, "5646215501931774541", 0)
+addappid(8166, 1, "c861d3aeedebe5ba35c4c874d3dc4bcbc708c4209be03b8123a125715da2e8a5") --Main Windows Language Depot Spanish Shellshock 2: Blood Trails
+setManifestid(8166, "7503433325602547222", 0)
