2d260df81c620826382e158766feb80421757a9d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:12:26 +0800

    add 24630.lua

---
+++ 24630.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 24630.lua
+--Gamename Trainz Simulator 2010: Engineers Edition
+addappid(24630) --Mainappid Trainz Simulator 2010: Engineers Edition
+addappid(24631, 1, "90b92b1630fa1fc26400da330418abc3b721c0d0991fdf7361cb692833868793") --Main Windows Depot Trainz Simulator 2010: Engineers Edition
+setManifestid(24631, "196455006608489102", 0)
+addappid(24637) --Dlcname Trainz Sim - PRRT1
