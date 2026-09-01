7bca3e5c08b147ffcbcc7ed10aeb127e5ca06758
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:39:01 +0800

    add 35510.lua

---
+++ 35510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 35510.lua
+--Gamename Cities XL
+addappid(35510) --Mainappid Cities XL
+addappid(35501, 1, "c750713896ae5f6784643acdc6fd294dbed040ecc0fef16c6263d6e56caec025") --Main Windows Depot Cities XL
+setManifestid(35501, "723942185696290766", 0)
