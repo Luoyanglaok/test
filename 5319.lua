90f65aa09862b74fdc82bb22094dd44354362456
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:31:11 +0800

    add 5319.lua

---
+++ 5319.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5319.lua
+--Gamename Batman Arkham Asylum Combat
+addappid(5319) --Mainappid Batman Arkham Asylum Combat
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Batman Arkham Asylum Combat
+setManifestid(910, "1332084816209327708", 0)
