637fb3378105c5137b59ede3ba451d407257f131
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:00:33 +0800

    add 5804.lua

---
+++ 5804.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5804.lua
+--Gamename 4elements_moive
+addappid(5804) --Mainappid 4elements_moive
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot 4elements_moive
+setManifestid(910, "1332084816209327708", 0)
