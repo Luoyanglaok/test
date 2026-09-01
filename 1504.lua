006c8b3168d1a547f12f5d6aa88c7ecbcd511110
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:20:03 +0800

    add 1504.lua

---
+++ 1504.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1504.lua
+--Gamename Darwinia Press Review
+addappid(1504) --Mainappid Darwinia Press Review
+addappid(1505, 1, "c41060ed9c303d9834b8a880531a3742f955633991a6bab971ad6ad7111513a9") --Main Windows Depot Darwinia Press Review
