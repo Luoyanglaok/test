34892116f8576d078c4b6cb60bdce17415a90487
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:47:39 +0800

    add 18059.lua

---
+++ 18059.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 18059.lua
+--Gamename Deathspank: Thongs of Virtue
+addappid(18059) --Mainappid Deathspank: Thongs of Virtue
+addappid(18060, 1, "1c7d2db1cffa30c068beb249628e7ad169b7926188423cc99bcae5c3741405c5") --Main Windows Depot Deathspank: Thongs of Virtue
