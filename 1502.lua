00cca4a364e8100266f4a135c1eff7d9cfd718f2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:19:51 +0800

    add 1502.lua

---
+++ 1502.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1502.lua
+--Gamename Darwinia Demo
+addappid(1502) --Mainappid Darwinia Demo
+addappid(1503, 1, "324f5b7b6105a1a716ba4624c3713d44597d36bf912f01b62e1b762957a6d36a") --Main Windows Depot Darwinia Demo
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Darwinia Demo
