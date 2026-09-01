31815301951da51b5d7681f8b936e8f34ea940fe
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:32:26 +0800

    add 5332.lua

---
+++ 5332.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5332.lua
+--Gamename Dawn of Discovery Intro
+addappid(5332) --Mainappid Dawn of Discovery Intro
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Dawn of Discovery Intro
+setManifestid(910, "1332084816209327708", 0)
