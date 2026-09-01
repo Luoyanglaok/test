0a0728c036799fa8ed540baf46778d7d2406f1b2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:18:49 +0800

    add 996.lua

---
+++ 996.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 996.lua
+--Gamename Two Worlds Trailer
+addappid(996) --Mainappid Two Worlds Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Two Worlds Trailer
+setManifestid(910, "1332084816209327708", 0)
+addappid(997, 1, "bf8c4e2dcf6b5aa991c4e693d884f9086b0da1056c72da175511b9fcf7138371") --Main Windows Depot Two Worlds Trailer
