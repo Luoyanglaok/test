de123bdf43b97c6b597344e09b1701c77e4e2754
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:48:24 +0800

    add 38820.lua

---
+++ 38820.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38820.lua
+--Gamename Crimecraft: BLEEDOUT
+addappid(38820) --Mainappid Crimecraft: BLEEDOUT
+addappid(38801, 1, "db0bf9d5de10f1ff124c84b68680de2b520aa3070a6acc0b83cab27ffaa4c8f0") --Main Windows Depot Crimecraft: BLEEDOUT
+setManifestid(38801, "5430714950973300575", 0)
