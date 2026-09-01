60f097d3f25451b0001509287cc01dbb3c42eda5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:14:15 +0800

    add 5099.lua

---
+++ 5099.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5099.lua
+--Gamename Age of Conan: Hyborian Adventures Trailer
+addappid(5099) --Mainappid Age of Conan: Hyborian Adventures Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Age of Conan: Hyborian Adventures Trailer
+setManifestid(910, "1332084816209327708", 0)
