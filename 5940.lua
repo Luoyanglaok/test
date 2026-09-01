736b4cbd18633a188cf139fab4cde26f7821f3bb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:10:54 +0800

    add 5940.lua

---
+++ 5940.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5940.lua
+--Gamename MOH - Part 4
+addappid(5940) --Mainappid MOH - Part 4
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot MOH - Part 4
+setManifestid(910, "1332084816209327708", 0)
