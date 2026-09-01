fe634eba9f6cb71655d6fd3e8e69726f8e5f7f33
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:54:57 +0800

    add 21500.lua

---
+++ 21500.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 21500.lua
+--Gamename Pyroblazer®
+addappid(21500) --Mainappid Pyroblazer®
+addappid(21501, 1, "0823d0bf6b22045650113bdd386da26dece430bc8a90e29d29bc78f8ced2b361") --Main Windows Depot Pyroblazer®
+setManifestid(21501, "4447093319965896875", 0)
