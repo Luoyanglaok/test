72784fb74c87c8f571df581acc24e9ebd9ad6db6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:13:29 +0800

    add 5090.lua

---
+++ 5090.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5090.lua
+--Gamename Team Fortress 2: Meet the Soldier (Russian)
+addappid(5090) --Mainappid Team Fortress 2: Meet the Soldier (Russian)
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Team Fortress 2: Meet the Soldier (Russian)
+setManifestid(910, "1332084816209327708", 0)
