17f32180281a0e04d094379f09ef777ec70125ab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:51:01 +0800

    add 40801.lua

---
+++ 40801.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 40801.lua
+--Gamename Steam Store
+addappid(40801, 1, "cdba07a99af206aeb16f77b2fb63ef6d5a7b9323bb7a92e48244fd597013dcca") --Mainappid Steam Store
+setManifestid(40801, "5234313430832523474", 361892576)
+addappid(40802, 1, "64a9ded61e049fd8ab73451c519acf2321c6419b6924603516861486a2df1982") --Main Linux Depot Steam Store
+setManifestid(40802, "6556596646716197166", 171903328)
+--Dlcname Super Meat Boy
+addappid(40800, 1, "6ed3de52261ae52111aab821a4a94ac63102846f18f37d75368f2296e62727c6") --Dlcname Super Meat Boy
+addappid(98011, 1, "b54a8be91d6223e22beaa4c5415f00aef71770d682068b7999855d6ba1e543ff") --Dlc Windows Depot Super Meat Boy
+setManifestid(98011, "915545047853675884", 0)
+addappid(202941, 1, "e81a93a26c224c42af1e8b013dc148809b870d1718fd970fca8be9afda5ac806") --Dlc Windows Depot Super Meat Boy
+setManifestid(202941, "7909665330530849751", 0)
+addappid(202942, 1, "7456550b7c6035613e2dce4b1e5330096bd2949510d0174327e0456092632dbf") --Dlc Macos Depot Super Meat Boy
+setManifestid(202942, "5927010178477174585", 172035408)
+addappid(969710, 1, "a04242b1f5cb97808ad527ea154a7b29e7d24a133cb943e832ebc57c356aa1a2") --Dlc Windows Depot Super Meat Boy
+setManifestid(969710, "9030929104429239407", 1374816)
