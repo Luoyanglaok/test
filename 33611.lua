91517a21e1de01f886a416f7cba0e1c3696548a3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:22:23 +0800

    add 33611.lua

---
+++ 33611.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33611.lua
+--Gamename Steam Store
+addappid(33611, 1, "b63689e558ba8a1bb31130cef59669bf26e259b4790a57cc1562b67983774b4a") --Mainappid Steam Store
+setManifestid(33611, "9072431506502567755", 0)
+addappid(33612, 1, "9a72c1845da9fb125a7f52206b01f78294cdb40de4ea844b05cd1ab672641708") --Main Windows Language Depot French Steam Store
+setManifestid(33612, "6968415016739457295", 0)
+--Dlcname Broken Sword 3 - the Sleeping Dragon (2003)
+addappid(33610) --Dlcname Broken Sword 3 - the Sleeping Dragon (2003)
+addappid(33613, 1, "3003286e82c22da2d10870993c651c125e2e59e832978a2f39e3979df17742f6") --Dlc Windows Language Depot German Broken Sword 3 - the Sleeping Dragon (2003)
+setManifestid(33613, "101464306410912037", 0)
+addappid(33614, 1, "06330b767eda842fc2d8a4ca9efcc67a80a50179de29f147f200ad0f9d407e30") --Dlc Windows Language Depot Italian Broken Sword 3 - the Sleeping Dragon (2003)
+setManifestid(33614, "3948075899233562804", 0)
+addappid(33615, 1, "b260e681672d837701825640fc4e72210ad4c6daa48fa8af654a95ed8a8fd612") --Dlc Windows Language Depot Spanish Broken Sword 3 - the Sleeping Dragon (2003)
+setManifestid(33615, "8045337047491811568", 0)
