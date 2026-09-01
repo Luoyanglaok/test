224e886326e83120d5a92880e4a1284ca18f07d0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:51:21 +0800

    add 19080.lua

---
+++ 19080.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 19080.lua
+--Gamename Biozone
+addappid(19080) --Mainappid Biozone
+addappid(19081, 1, "d1fe336698d5660f2200e7739d493ad8c72ef749cf16f05062b0ec93c2e44ee2") --Main Windows Depot Biozone
+setManifestid(19081, "5232621543727230389", 0)
