2c2b2ac8a36161e01cd42769ebc848ed81da31d9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:41:17 +0800

    add 5512.lua

---
+++ 5512.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5512.lua
+--Gamename Left 4 Dead 2 Final TV Spot
+addappid(5512) --Mainappid Left 4 Dead 2 Final TV Spot
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Left 4 Dead 2 Final TV Spot
+setManifestid(910, "1332084816209327708", 0)
