899fcd015504bac4f8422c20c6025c4d71448dc0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:36:40 +0800

    add 304.lua

---
+++ 304.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 304.lua
+--Gamename Day of Defeat: Source Press Review
+addappid(304) --Mainappid Day of Defeat: Source Press Review
+addappid(206, 1, "15a624d389f3b12993eafaea17f4304957d13aa116f561faf36c4dd97a664123") --Main Windows Depot Day of Defeat: Source Press Review
+addappid(207, 1, "cc9eeb827bbf37b90172eb6a19d04568a00bcb01ac03a168b3f09a2e4ca48be0") --Main Windows Depot Day of Defeat: Source Press Review
+addappid(208, 1, "f5da5520e5d11228bb9e146324c9dc2b2387d00eb0df7829ad787bc7d299066a") --Main Windows Depot Day of Defeat: Source Press Review
+addappid(212, 1, "eeb4885ef329d5477a06274b9ead4904aa9f79aab9aff6a83254d910ec9150fa") --Main Windows Depot Day of Defeat: Source Press Review
+addappid(305, 1, "87f5b172a392e11d80facf1239bc315d21f903af56ddd6da5d6cb497c78c7bdb") --Main Windows Depot Day of Defeat: Source Press Review
