4077fa80b0060f86cef3137475258254e0140e36
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:31:00 +0800

    add 11000.lua

---
+++ 11000.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 11000.lua
+--Gamename Loki Demo - Norse Warrior
+addappid(11000) --Mainappid Loki Demo - Norse Warrior
+addappid(11001, 1, "cd9cb40c57a023c964a25434dc1279a9c0b05dc6eebe7c8d21513d1abede6d3d") --Main Windows Depot Loki Demo - Norse Warrior
+setManifestid(11001, "6280046852278598856", 0)
+addappid(11002, 1, "e33a3a04f20efa127d5e9f5a8ca13de178f5dff99dc355cc794fb306b754d8ef") --Main Windows Depot Loki Demo - Norse Warrior
+setManifestid(11002, "1526597139395217783", 0)
