0ef387151e71d95bc6e926f99152a97dfd7bb4f1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:45:18 +0800

    add 38010.lua

---
+++ 38010.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38010.lua
+--Gamename Mahjong Quest 2
+addappid(38010) --Mainappid Mahjong Quest 2
+addappid(38011, 1, "22c5ecc225b6cd3e1e4f71501e657c912aebadee008eb25e61eb042a01f99be5") --Main Windows Depot Mahjong Quest 2
+setManifestid(38011, "5682322445238885355", 0)
