a1f90fb0954f224bda66adfff544de3d03c906d0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:01:17 +0800

    add 5812.lua

---
+++ 5812.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5812.lua
+--Gamename Blacklight Tango Down Teaser
+addappid(5812) --Mainappid Blacklight Tango Down Teaser
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Blacklight Tango Down Teaser
+setManifestid(910, "1332084816209327708", 0)
