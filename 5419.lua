47649a17a8f10d558064d915dd7b2fb2f2600814
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:38:09 +0800

    add 5419.lua

---
+++ 5419.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5419.lua
+--Gamename Tom Clancy's Splinter Cell Conviction - Numbers
+addappid(5419) --Mainappid Tom Clancy's Splinter Cell Conviction - Numbers
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Tom Clancy's Splinter Cell Conviction - Numbers
+setManifestid(910, "1332084816209327708", 0)
