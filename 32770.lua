d7333041125e6cdb8ca14b327ffac645a930891a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:20:19 +0800

    add 32770.lua

---
+++ 32770.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 32770.lua
+--Gamename Joint Operations: Combined Arms Gold
+addappid(32770) --Mainappid Joint Operations: Combined Arms Gold
+addappid(32771, 1, "0b29186655efcb2c4bc6892361e468720f0487505714a82f70c264bde1b68768") --Main Windows Depot Joint Operations: Combined Arms Gold
+setManifestid(32771, "8600140539539463080", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Joint Operations: Combined Arms Gold
