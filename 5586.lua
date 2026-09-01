018ffbefdf4fa30cd6f6472f4539ea17fd2a734b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:42:32 +0800

    add 5586.lua

---
+++ 5586.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5586.lua
+--Gamename Lost Planet Extreme Condition Colonies Edition
+addappid(5586) --Mainappid Lost Planet Extreme Condition Colonies Edition
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Lost Planet Extreme Condition Colonies Edition
+setManifestid(910, "1332084816209327708", 0)
