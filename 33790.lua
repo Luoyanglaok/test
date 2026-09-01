d6d98efb75fd320cafc7ed6f613adde8a821b1f1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:23:04 +0800

    add 33790.lua

---
+++ 33790.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33790.lua
+--Gamename Elizabeth Find M.D. - Diagnosis Mystery - Season 2
+addappid(33790) --Mainappid Elizabeth Find M.D. - Diagnosis Mystery - Season 2
+addappid(33791, 1, "1d30aee50a86d64a468c1aaf92fd47c25c47a8145874a91b59046ed7a170fb71") --Main Windows Depot Elizabeth Find M.D. - Diagnosis Mystery - Season 2
+setManifestid(33791, "6456687309942391316", 0)
+addappid(33792, 1, "2383ed148cdbc024aa424f7d76a3da8d6ddcb72f3937e7d8382bfb0865630e6f") --Main Macos Depot Elizabeth Find M.D. - Diagnosis Mystery - Season 2
+setManifestid(33792, "2100729914924537967", 0)
