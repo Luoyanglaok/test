9bbe6e29a47cd8c9f995891f1b0b5d2c9391feca
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:12:08 +0800

    add 5951.lua

---
+++ 5951.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5951.lua
+--Gamename Shank Trailer
+addappid(5951) --Mainappid Shank Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Shank Trailer
+setManifestid(910, "1332084816209327708", 0)
