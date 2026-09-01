8244f1d8b62ceb9664492d3cb47989ec36c584ea
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:15:20 +0800

    add 6260.lua

---
+++ 6260.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 6260.lua
+--Gamename Making History: The Calm & The Storm Demo
+addappid(6260) --Mainappid Making History: The Calm & The Storm Demo
+addappid(6261, 1, "601ac8ddbe280416c49d3dfbf12cfb77424ee9d290af4d7751f117318ae7fbf9") --Main Windows Depot Making History: The Calm & The Storm Demo
+setManifestid(6261, "2248795358878805722", 0)
