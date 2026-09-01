da87c0de72d9440f28e1fe5c073266c69174f977
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:00:20 +0800

    add 5803.lua

---
+++ 5803.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5803.lua
+--Gamename Darwinia_ESRB
+addappid(5803) --Mainappid Darwinia_ESRB
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Darwinia_ESRB
+setManifestid(910, "1332084816209327708", 0)
