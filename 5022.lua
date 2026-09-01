fd0e0c011f4fb547a71831c165c07d7a14d3399c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 22:37:30 +0800

    add 5022.lua

---
+++ 5022.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5022.lua
+--Gamename Overlord: Raising Hell Trailer
+addappid(5022) --Mainappid Overlord: Raising Hell Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Overlord: Raising Hell Trailer
+setManifestid(910, "1332084816209327708", 0)
