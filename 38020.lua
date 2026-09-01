396561a3c527170b57af0fd9a5184f5ebc52b3b2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:45:27 +0800

    add 38020.lua

---
+++ 38020.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38020.lua
+--Gamename Mahjong Quest 3
+addappid(38020) --Mainappid Mahjong Quest 3
+addappid(38021, 1, "bc8762db0504165e17efcc2704aa479fde7d28718fa23d2e90cc9d3f6e28f260") --Main Windows Depot Mahjong Quest 3
+setManifestid(38021, "8206622815018494455", 0)
