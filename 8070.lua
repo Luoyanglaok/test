a0d6730dd0d2335dc98a249b006a77bcbe765667
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:21:43 +0800

    add 8070.lua

---
+++ 8070.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8070.lua
+--Gamename Championship Manager 2008 Demo
+addappid(8070) --Mainappid Championship Manager 2008 Demo
+addappid(8071, 1, "2435f9cd63faf506dc5fcfc5d2104dea9d60437c5b95661fef234258901c2caf") --Main Windows Depot Championship Manager 2008 Demo
+setManifestid(8071, "8005902325592867110", 0)
