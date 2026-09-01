1061e86aeb1f7c8c01a155ab790694279bec3320
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:42:51 +0800

    add 926.lua

---
+++ 926.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 926.lua
+--Gamename Red Orchestra Lyes Krovy Trailer
+addappid(926) --Mainappid Red Orchestra Lyes Krovy Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Red Orchestra Lyes Krovy Trailer
+setManifestid(910, "1332084816209327708", 0)
