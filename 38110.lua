d8b9ca1ea7f2e8b41b3bdd7a71ab8cd77126393e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:46:26 +0800

    add 38110.lua

---
+++ 38110.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 38110.lua
+--Gamename Build-A-Lot 4: Power Source
+addappid(38110) --Mainappid Build-A-Lot 4: Power Source
+addappid(38111, 1, "7c236a6a8e0a31a6a2bbc5cf569c35af2755c00d8421d45acf4f89f4053dec37") --Main Windows Depot Build-A-Lot 4: Power Source
+setManifestid(38111, "6318559532066070537", 0)
