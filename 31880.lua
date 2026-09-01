7169788e55cc0e394cef6c963443b0c5e424bd62
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:18:00 +0800

    add 31880.lua

---
+++ 31880.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 31880.lua
+--Gamename Nancy Drew®: Secret of the Old Clock
+addappid(31880) --Mainappid Nancy Drew®: Secret of the Old Clock
+addappid(31881, 1, "7afbb93534bed14c16e52d0f99b652c2a06efb58c8e82fe4977621ff22961417") --Main Windows Depot Nancy Drew®: Secret of the Old Clock
+setManifestid(31881, "1026742316257624780", 0)
