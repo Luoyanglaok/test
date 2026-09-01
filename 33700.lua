df9613199864f00740557a0c47020e573f5af911
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:22:31 +0800

    add 33700.lua

---
+++ 33700.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 33700.lua
+--Gamename Paws and Claws: Pampered Pets
+addappid(33700) --Mainappid Paws and Claws: Pampered Pets
+addappid(33701, 1, "0d16e0d8cd8c226e2f35435571a73bd1126e9ed2d7ae279b6bd01bc839041816") --Main Windows Depot Paws and Claws: Pampered Pets
+setManifestid(33701, "5655639109068933226", 0)
