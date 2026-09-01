6e53d0a37c89244fc741ce3a72cb6225e28bb9c1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:37:50 +0800

    add 515.lua

---
+++ 515.lua
@@ -0,0 +1,4 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 515.lua
+--Gamename Steam Store
+addappid(515, 1, "f09a2fa191a845e1274cf6abb8e5bc7d6ffd6c04cb4a114ddf6a98f8967877e0") --Mainappid Steam Store
