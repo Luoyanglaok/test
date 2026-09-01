e955fa0d4cd7cca140626676430fe6b3ff52c930
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 21:41:41 +0800

    add 917.lua

---
+++ 917.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 917.lua
+--Gamename Day of Defeat: Jagd Trailer
+addappid(917) --Mainappid Day of Defeat: Jagd Trailer
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot Day of Defeat: Jagd Trailer
+setManifestid(910, "1332084816209327708", 0)
