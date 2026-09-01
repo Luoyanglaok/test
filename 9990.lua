4b311ad1f24cf99ff5b4327e27b6d97d0b9b031a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 00:26:09 +0800

    add 9990.lua

---
+++ 9990.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9990.lua
+--Gamename Ghostbusters: Sanctum of Slime
+addappid(9990) --Mainappid Ghostbusters: Sanctum of Slime
+addappid(9991, 1, "5e02f09462e7bceaaabea713a36cb5cc64674cde0d48de4e6df24ff9e6d13436") --Main Windows Depot Ghostbusters: Sanctum of Slime
+setManifestid(9991, "6573600818039967594", 0)
+addappid(9992) --Dlcname Ghostbusters: Sanctum of Slime Challenge Pack DLC
