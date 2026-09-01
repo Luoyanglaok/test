96544c4135e54e07b93112cf7d93c6344c7932ab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 01:59:15 +0800

    add 42830.lua

---
+++ 42830.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 42830.lua
+--Gamename Achtung Panzer - Kharkov 1943
+addappid(42830) --Mainappid Achtung Panzer - Kharkov 1943
+addappid(42831, 1, "dec3fc1faf80f45440ce84cda793b2ca731408dd358421c7ab3cd100f840fef6") --Main Windows Depot Achtung Panzer - Kharkov 1943
+setManifestid(42831, "2924714975177712200", 0)
