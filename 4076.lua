712f15342438faeb29de1f6441c47960b86d3998
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-01 23:05:31 +0800

    add 4076.lua

---
+++ 4076.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4076.lua
+--Gamename Steam Store
+addappid(4076) --Mainappid Steam Store
+addtoken(4076, "7163375852442353764")
