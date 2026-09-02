d7383e229bf3ce2f2e73d733622b4f21e8a57e6c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 17:51:07 +0800

    add 1000090.lua

--- 1000090.lua
+++ 1000090.lua
@@ -1,7 +1,12 @@
-addappid(1000090)
-addappid(1004,0,"f21faa4f29ecee9d192fc1784d1701376eb87e13521824ceaf0116b10c558a81")
-setManifestid(1004,"8904491048620934740")
-addappid(1005,0,"ffa980484d510ce8d7bfcdfde8545a438acc894accb6750711fdb75c0ee92ad9")
-setManifestid(1005,"2135359612286175146")
-addappid(1006,0,"0a6d85ecbae0b6b89cf23244f6b4a69fe14e3e64b128473e93f9a2ed3d835b1c")
-setManifestid(1006,"5587033981095108078")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1000090.lua
+--Gamename Meal of the Worms Dedicated Server
+addappid(1000090) --Mainappid Meal of the Worms Dedicated Server
+addappid(1004, 1, "f21faa4f29ecee9d192fc1784d1701376eb87e13521824ceaf0116b10c558a81") --Main Windows Depot Meal of the Worms Dedicated Server
+setManifestid(1004, "5612541580377302256", 22986368)
+addappid(1005, 1, "ffa980484d510ce8d7bfcdfde8545a438acc894accb6750711fdb75c0ee92ad9") --Main Macos Depot Meal of the Worms Dedicated Server
+setManifestid(1005, "2135359612286175146", 0)
+addappid(1006, 1, "0a6d85ecbae0b6b89cf23244f6b4a69fe14e3e64b128473e93f9a2ed3d835b1c") --Main Linux Depot Meal of the Worms Dedicated Server
+setManifestid(1006, "6403079453713498174", 35551408)
+addappid(1000091, 1, "6067192149f9a23b5e3b1c955ed2bfa07e887ed4312e0ab09aa1968e76dae753") --Main Windows Depot Meal of the Worms Dedicated Server
+setManifestid(1000091, "5990957705625501682", 189936496)
