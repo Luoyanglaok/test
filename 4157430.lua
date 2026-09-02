689ffcb08d73027d4f37e500c3445972ed94dc60
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-02 21:01:34 +0800

    add 4157430.lua

--- 4157430.lua
+++ 4157430.lua
@@ -1,3 +1,6 @@
-addappid(4157430)
-addappid(4157431,1,"50ef8fc0c8357d34c59cbed6c89430a705c3c10ffc20637616058a320ea93684")
-setManifestid(4157431,"566579178082928293")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4157430.lua
+--Gamename Diggin
+addappid(4157430, 1, "8897eccd6a29e2ab0c20c82892fd89cdbdf79365c1c9bbced5b6dbef5ef8d5e6") --Mainappid Diggin
+addappid(4157431, 1, "50ef8fc0c8357d34c59cbed6c89430a705c3c10ffc20637616058a320ea93684") --Main Windows Depot Diggin
+setManifestid(4157431, "566579178082928293", 177803840)
