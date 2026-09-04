c439ddafcec7bb09083652f825eb4262980d095e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-04 20:25:24 +0800

    add 4627110.lua

---
+++ 4627110.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4627110.lua
+--Gamename Fell & Sell
+addappid(4627110) --Mainappid Fell & Sell
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot Fell & Sell
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot Fell & Sell
+addappid(4627111, 1, "28d630c42b8cba00c7b125084e4b7882fd121590cc2e1696e72ad68d6193d0e4") --Main Windows Depot Fell & Sell
+setManifestid(4627111, "3698263106305675710", 556078352)
+--Missing Dlcs
+--Dlcname Fell & Sell Demo
