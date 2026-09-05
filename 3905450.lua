9cf68a5f3a46fa2c7ae54dc26951587fb90af0b8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-05 16:26:27 +0800

    add 3905450.lua

---
+++ 3905450.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3905450.lua
+--Gamename WheelMates
+addappid(3905450) --Mainappid WheelMates
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Main Windows Depot WheelMates
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Main Windows Depot WheelMates
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Main Windows Depot WheelMates
+addappid(3905451, 1, "65e41de63569430ea95de665bca8b6a2254b980cbf6514cde1b107c420f53626") --Main Windows Depot WheelMates
+setManifestid(3905451, "7633994413648298872", 6919714672)
+addappid(5108990) --Dlcname WheelMates - Supporter Pack
