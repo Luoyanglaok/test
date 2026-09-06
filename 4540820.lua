060850ccdf4f12d71f45cfb85b0bb2524ad7620b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-06 13:43:36 +0800

    Update 4540820.lua

--- 4540820.lua
+++ 4540820.lua
@@ -1,3 +1,7 @@
-﻿addappid(4540820)
-addappid(4540821)
-setManifestid(4540821,"5142713827073505022")
+addappid(4540820) --Mainappid Kin and Conquest
+addappid(4540821, 1, "eaaad65590522e8c055755f43c02db9041e786eefc36c040fc0f0c0248ca71d2") --Main Windows Depot Kin and Conquest
+setManifestid(4540821, "5142713827073505022", 191880288)
+--Dlcname Kin and Conquest Soundtrack
+addappid(4962100) --Dlcname Kin and Conquest Soundtrack
+addappid(4962101, 1, "dcb0d080f4669a2aabdc1680c30328829078ea8183f16c6f32cc4e0a0d5eb6ef") --Dlc Windows Depot Kin and Conquest Soundtrack
+setManifestid(4962101, "8913611738756991186", 33219184)
