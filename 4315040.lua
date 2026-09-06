c3f7f61d51dd8de8aacba3c863058eba234a841d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-06 18:10:29 +0800

    add 4315040.lua

---
+++ 4315040.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4315040.lua
+--Gamename Hearth and Hamlet
+addappid(4315040, 1, "fd14d389c658a2eacbeebec2c04dd28856fecbbcb2de377c921ce888b1d128c5") --Mainappid Hearth and Hamlet
+addappid(4315041, 1, "9a026c36ec2e999340ac2f8abfb6b3618b8ab5222b430be7fad4f6ffd3111ab0") --Main Windows Depot Hearth and Hamlet
+setManifestid(4315041, "6950687788351987081", 852357440)
+--Missing Dlcs
+--Dlcname Hearth and Hamlet Playtest
