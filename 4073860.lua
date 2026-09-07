24b89d5b19ad7a4772cae5bd4f825b021ad56f28
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-07 08:42:01 +0800

    add 4073860.lua

---
+++ 4073860.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4073860.lua
+--Gamename Sludgineers
+addappid(4073860, 1, "727a00b6e623e92543b7ce515f973560301a457e5f697e1b1f00d8c0373e5b4f") --Mainappid Sludgineers
+addappid(4073861, 1, "da852f0a1697419dd86d310348f4bff6cad6d38e28b73bb29e76545d1973e0cb") --Main Windows Depot Sludgineers
+setManifestid(4073861, "6407871746993850449", 187174240)
+addappid(4073862, 1, "a58683ff4262867e752529f9b9d532f43fd5d611f2f767f1cfe249707cb88bdb") --Main Macos Depot Sludgineers
+setManifestid(4073862, "1494660652528405798", 214353360)
+addappid(4073863, 1, "08b8b3717233c2ce8780bde42a587c1ef9b36a9f41e8760089b42e972d313e14") --Main Linux Depot Sludgineers
+setManifestid(4073863, "3943428481252335523", 191489936)
