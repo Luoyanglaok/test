addf07090ba432d95897ce90df33795139b07642
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-07 14:03:30 +0800

    add 3321460.lua

---
+++ 3321460.lua
@@ -0,0 +1,22 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3321460.lua
+--Gamename Crimson Desert Enhanced
+addappid(3321460, 1, "dc014470e47064263f3003feb526a87cc64784292e2a608455d18fa70ec2c1e0") --Mainappid Crimson Desert Enhanced
+addappid(3321461, 1, "e2e570c84eb8b4cfe9c20268cd30b84f8c1c7c076691c92ae8c5ba7b510b8a95") --Main Windows Depot Crimson Desert Enhanced
+setManifestid(3321461, "3540302611239512787", 115755508848)
+addappid(3321462, 1, "6eb3bfe7d16b9eb6441dba4f7d18b341fe83ab2f1e9385d042ce622f38a600cb") --Main Macos Depot Crimson Desert Enhanced
+setManifestid(3321462, "304563384328720229", 123690696416)
+--Dlcname Crimson Desert Original Soundtrack Volume 1
+addappid(4572870) --Dlcname Crimson Desert Original Soundtrack Volume 1
+addappid(4572871, 1, "fd5557767303d9a63b0b31b6a22cd1368e5d82a692e64d041890100ea0c330c4") --Dlc Windows Depot Crimson Desert Original Soundtrack Volume 1
+setManifestid(4572871, "6173128065949955413", 705188272)
+addappid(4572872, 1, "e071efcdac5de6bf1b6620a3110cb3ab7799943ee634eed2970db8732eeb817e") --Dlc Windows Depot Crimson Desert Original Soundtrack Volume 1
+setManifestid(4572872, "1021886354207583209", 5084474448)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4024620) --Dlcname Crimson Desert - Deluxe Pack
+addappid(4024630) --Dlcname Crimson Desert - Pre Order Pack
+addtoken(4024630, "17135069641874014023")
+addappid(4193060) --Dlcname Crimson Desert - Ultimate Pack
+addappid(5001840) --Dlcname Crimson Desert Enhanced: Charting the Unknown
