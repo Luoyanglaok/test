caea9f073f54de8d5f2a723665c8698b0778fa75
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-08 20:23:55 +0800

    add 2287220.lua

--- 2287220.lua
+++ 2287220.lua
@@ -1,4 +1,11 @@
-addappid(2287220)
-setManifestid(228989,"550968249685141759")
-addappid(2287221,0,"f3bbc703b528bc21ea60de0dfbd5eb17f2b5697a6dcebc73b2a287fc01853882")
-setManifestid(2287221,"2850106475000837370")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2287220.lua
+--Gamename F1® Manager 2023
+addappid(2287220) --Mainappid F1® Manager 2023
+addappid(2287221, 1, "f3bbc703b528bc21ea60de0dfbd5eb17f2b5697a6dcebc73b2a287fc01853882") --Main Windows Depot F1® Manager 2023
+setManifestid(2287221, "2850106475000837370", 20142334688)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2386730) --Dlcname Deluxe
+addappid(2428720) --Dlcname Pre-order Content
