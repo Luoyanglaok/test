eda1eaa9f28542e1c72e692db7b8a2d86b9c40c7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-08 20:21:17 +0800

    add 3669570.lua

---
+++ 3669570.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3669570.lua
+--Gamename Alchemy Factory
+addappid(3669570, 1, "c2328e8ae04075227a47bf917ff3a18f9726021d9539e86b5d32a0dd350d27a3") --Mainappid Alchemy Factory
+addappid(3669571, 1, "fe5844e8b931f2da4fecf3d1663f9fff18c91ec3d6834f9a9ae792a28b4af523") --Main Windows Depot Alchemy Factory
+setManifestid(3669571, "7632684833950630476", 3615616656)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
