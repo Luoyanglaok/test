d3ca77f0af6de9165d669b0141ae1bd0cd64e6e1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-08 21:24:02 +0800

    add 4243990.lua

---
+++ 4243990.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4243990.lua
+--Gamename Path of Idle: Old Gods Rising
+addappid(4243990) --Mainappid Path of Idle: Old Gods Rising
+addappid(4243991, 1, "623ed85953c63973a1304b6251c1f4ae8ec579582fafa15adf440db37d696852") --Main Windows Depot Path of Idle: Old Gods Rising
+setManifestid(4243991, "4812394426858028006", 170350848)
+addappid(4243992, 1, "f98fc8450d889cf7c2b73a25f834a6f0f3c5839fe383771816440bf41d0fdcc6") --Main Macos Depot Path of Idle: Old Gods Rising
+setManifestid(4243992, "1675960471145491864", 196185280)
+addappid(4985510) --Dlcname Path of Idle: Old Gods Rising - Supporter Pack
