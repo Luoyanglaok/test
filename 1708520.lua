68647490361aaaa7ed126fb434992db2581b2d79
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-08 20:22:41 +0800

    add 1708520.lua

--- 1708520.lua
+++ 1708520.lua
@@ -1,7 +1,11 @@
-addappid(1708520)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1708521,0,"cfe691d2a98318144412d25c85741599738732ab007a32803fe9a6f449e689ed")
-setManifestid(1708521,"7023775666497361782")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1708520.lua
+--Gamename F1® Manager 2022
+addappid(1708520) --Mainappid F1® Manager 2022
+addappid(1708521, 1, "cfe691d2a98318144412d25c85741599738732ab007a32803fe9a6f449e689ed") --Main Windows Depot F1® Manager 2022
+setManifestid(1708521, "7023775666497361782", 20284845136)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
