54f69e9232ebfff42e3dcf2f9a4fd45ea8503a87
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 04:37:58 +0800

    add 1020790.lua

--- 1020790.lua
+++ 1020790.lua
@@ -1,8 +1,28 @@
-addappid(1020790)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229003,"8740933542064151477")
-addappid(1020791,0,"20b22cbbc47231bd29023b51d8a6fa2c019ad774840d245064799912ea48b105")
-setManifestid(1020791,"1061767113913208859")
-addappid(2542820,0,"290c05f9538952fbafab1381ad8516cb900a835310db3701bd21461afa343090")
-setManifestid(2542820,"2745812014487487415")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1020790.lua
+--Gamename NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS
+addappid(1020790, 1, "9f68fbfc5c958fc71ebbfd872f331b90ea558aaa7c36ac423503c0f55233f347") --Mainappid NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS
+addappid(1020791, 1, "20b22cbbc47231bd29023b51d8a6fa2c019ad774840d245064799912ea48b105") --Main Windows Depot NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS
+setManifestid(1020791, "7503379920390129484", 28108267968)
+--Dlcname NBUNSC - Nostalgic Anime Song & Item Pack
+addappid(2542820, 1, "290c05f9538952fbafab1381ad8516cb900a835310db3701bd21461afa343090") --Dlcname NBUNSC - Nostalgic Anime Song & Item Pack
+setManifestid(2542820, "2745812014487487415", 14123920)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229003, "8740933542064151477", 42914928)
+addappid(2171320) --Dlcname NBUNSC - Costume & Accessory Pack
+addappid(2171321) --Dlcname NBUNSC - Pre-Order Pack
+addappid(2171324) --Dlcname NBUNSC - Kakashi Hatake (Maskless)
+addappid(2171325) --Dlcname NBUNSC - Naruto Uzumaki (The Great Ninja War: End)
+addappid(2171326) --Dlcname NBUNSC - DLC Pack 1
+addappid(2171327) --Dlcname NBUNSC - DLC Pack 2
+addappid(2171328) --Dlcname NBUNSC - DLC Pack 3
+addappid(2171329) --Dlcname NBUNSC - DLC Pack 4
+addappid(2171330) --Dlcname NBUNSC - DLC Pack 5
+addappid(2171332) --Dlcname NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS - Season Pass
+addappid(2594760) --Dlcname NARUTO X BORUTO Ultimate Ninja Storm Connections - Ultimate Edition Upgrade Pack
+addappid(2674900) --Dlcname Asics x Naruto Shippuden Special Sneakers
