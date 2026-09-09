f89b7ca68cb9a3b468c6b68d93fdb1aac8e9dd88
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 00:35:42 +0800

    add 2291680.lua

--- 2291680.lua
+++ 2291680.lua
@@ -1,3 +1,9 @@
-addappid(2291680)
-addappid(2291681,0,"d5f249db247fe7b9d64a53304732e9775a8ffc470308d03c4e96fc84f8775f71")
-setManifestid(2291681,"1585438778104121488")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2291680.lua
+--Gamename The Arrogant Kaiju Princess & The Detective Servant
+addappid(2291680, 1, "14a51b81f58e18c0558c035d022e2180e58f6e8c0ec757f967eba4c2ae64eda4") --Mainappid The Arrogant Kaiju Princess & The Detective Servant
+addappid(2291681, 1, "d5f249db247fe7b9d64a53304732e9775a8ffc470308d03c4e96fc84f8775f71") --Main Windows Depot The Arrogant Kaiju Princess & The Detective Servant
+setManifestid(2291681, "1585438778104121488", 1133623184)
+--Dlcname The Arrogant Kaiju Princess and The Detective Servant ArtBook
+addappid(2714820, 1, "232e0645e594dbe740dbb0bf1557d765dd0444558df6d159de8734b3fb51e255") --Dlcname The Arrogant Kaiju Princess and The Detective Servant ArtBook
+setManifestid(2714820, "4057397915279419981", 144017440)
