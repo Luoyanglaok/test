a1843c780f3c03b120ea1766ba6622c8323bf2f1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-09 23:47:14 +0800

    add 3192340.lua

---
+++ 3192340.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3192340.lua
+--Gamename Steamy Livestreams with My Girlfriend SS
+addappid(3192340, 1, "730d182d55396739742d1d946d3cb233dcff47bb1dfc21cb66c3225c8d8e01a6") --Mainappid Steamy Livestreams with My Girlfriend SS
+addappid(3192341, 1, "09f43f1aa3973ed13bce58f80a4742d9da74999d1f294bfe062c3dc608d98166") --Main Windows Depot Steamy Livestreams with My Girlfriend SS
+setManifestid(3192341, "5367448805166823446", 1045153792)
+addappid(3192342, 1, "3afad89993c7e84840b657461589902978a75cf61e6ae14afd42f73e92279f57") --Main Windows Language Depot Japanese Steamy Livestreams with My Girlfriend SS
+setManifestid(3192342, "4420443335212668336", 1045286240)
+addappid(3192343, 1, "fd6dc564cd8e5c218e4c2e60e09e05be72fb5590beb5aa331a9d0570e043a910") --Main Windows Language Depot Tchinese Steamy Livestreams with My Girlfriend SS
+setManifestid(3192343, "6805774248024875851", 1044814080)
+addappid(3192344, 1, "4b98b30d1a00d7dd5ddbab928a25e31fa44a35b1c241a4880c8878bbd9e7246d") --Main Windows Language Depot Schinese Steamy Livestreams with My Girlfriend SS
+setManifestid(3192344, "3104027518966913021", 1044784208)
+addappid(3192345, 1, "269625ec6b0b6697018bc26f360eded6aa3a2c79619030d3f7f8317a88049806") --Main Windows Language Depot Koreana Steamy Livestreams with My Girlfriend SS
+setManifestid(3192345, "1389514755096778565", 1044727040)
