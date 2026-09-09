673c9ceafb861b28f896f62454ffea5affa73c10
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 06:57:51 +0800

    add 3207780.lua

--- 3207780.lua
+++ 3207780.lua
@@ -1,8 +1,2 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 3207780.lua
---Gamename Love Mansion
-addappid(3207780, 1, "8d1e6f9681cda17d074678ac97586d3854a3739b9678795f7d9ce25ae76a070f") --Mainappid Love Mansion
-addappid(3207781, 1, "3551564b59e26ffe321d5d8dcb72136f1d1fc8b7144ace3ac38d2647f123164c") --Main Windows Depot Love Mansion
-setManifestid(3207781, "8274789120095252639", 3553660768)
---Missing Dlcs
---Dlcname Love Mansion Soundtrack
+addappid(3207780)
+setManifestid(3207781,"8274789120095252639")
