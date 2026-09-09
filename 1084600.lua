ed4005ef91ce809dca41ef575055411178ed38f0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 04:41:36 +0800

    add 1084600.lua

--- 1084600.lua
+++ 1084600.lua
@@ -1,10 +1,39 @@
-addappid(1084600)
-setManifestid(228989,"3514306556860204959")
-addappid(1084602,0,"262b20ffd5fbd41eb4e6993b44dba982695731cf82c7f787236a1bc1b82109aa")
-setManifestid(1084602,"5014485731095115828")
-addappid(1084601,0,"7af65c0378d727ff872dcf1bfc511faa6bda56ddb31f4d891ba88a15e5540d17")
-setManifestid(1084601,"6033883375651283552")
-addappid(2182080,0,"8052cc32bd4cd71ff86ef8c4db68c2dc5ab50884feea0cc90db814a73eb3dcf7")
-setManifestid(2182080,"6000569290448241032")
-addappid(1084603,0,"5298981b009169e714731cbf957866f073237c264807e27cc5ddc061d41efb5d")
-setManifestid(1084603,"6318924155206180160")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1084600.lua
+--Gamename My Time at Sandrock
+addappid(1084600, 1, "a7928b9ab58be8e7c7a5a4bb0819746faf8fb81bb9c1767b3f734ccf57b73dac") --Mainappid My Time at Sandrock
+addappid(1084601, 1, "7af65c0378d727ff872dcf1bfc511faa6bda56ddb31f4d891ba88a15e5540d17") --Main Windows Depot My Time at Sandrock
+setManifestid(1084601, "6033883375651283552", 12875415280)
+addappid(1084602, 1, "262b20ffd5fbd41eb4e6993b44dba982695731cf82c7f787236a1bc1b82109aa") --Main Windows Depot My Time at Sandrock
+setManifestid(1084602, "5014485731095115828", 8055044272)
+addappid(1084603, 1, "5298981b009169e714731cbf957866f073237c264807e27cc5ddc061d41efb5d") --Main Windows Depot My Time at Sandrock
+setManifestid(1084603, "6318924155206180160", 12875032912)
+--Dlcname My Time At Sandrock - OST
+addappid(1720400) --Dlcname My Time At Sandrock - OST
+addappid(1720401, 1, "364495165d936ac2da68067973c99e913edf26ee8e8ef51e220ce33e95fab33d") --Dlc Windows Depot My Time At Sandrock - OST
+setManifestid(1720401, "4650324154701246762", 1920446448)
+addappid(1720402, 1, "6af6a4d4adfddd5e7d518de22f243dbd4187f1c6bea5aad0deddde0e8d15b387") --Dlc Windows Depot My Time At Sandrock - OST
+setManifestid(1720402, "7182821150993537114", 318038640)
+--Dlcname AppID 2090470
+addtoken(2090470, "6523541594591311016")
+addappid(2090470, 1, "171040ef17e40051ff077c37fcfef7510a669fd9d81549403028bc15e0c22015") --Dlcname AppID 2090470
+--Dlcname My Time at Sandrock - Digital Artbook
+addappid(2182080, 1, "8052cc32bd4cd71ff86ef8c4db68c2dc5ab50884feea0cc90db814a73eb3dcf7") --Dlcname My Time at Sandrock - Digital Artbook
+setManifestid(2182080, "6000569290448241032", 272255552)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2181522) --Dlcname My Time at Sandrock - Builder's Beach and Ball Clothing Pack
+addappid(2181528) --Dlcname My Time at Sandrock - Summer Heat Collection 1
+addappid(2595600) --Dlcname My Time at Sandrock - Summer Heat Collection 2
+addappid(2595610) --Dlcname My Time at Sandrock - Summer Heat Collection 3
+addappid(2595620) --Dlcname My Time at Sandrock - Starlight Collection 1
+addappid(2595710) --Dlcname My Time at Sandrock - Starlight Collection 2
+addappid(2595720) --Dlcname My Time at Sandrock - Starlight Collection 3
+addappid(2595730) --Dlcname My Time at Sandrock - Pet Pack: Lil Gecko
+addappid(2595740) --Dlcname My Time at Sandrock - Interior Decorator Pack
+addappid(2595750) --Dlcname My Time at Sandrock - Far East Furniture Pack
+addappid(2595760) --Dlcname My Time at Sandrock - Cute Critter Furniture Pack
+addappid(2622210) --Dlcname My Time at Sandrock - Alienware Gift
+addappid(2808740) --Dlcname My Time at Sandrock - Monster Whisperer
+addappid(2969170) --Dlcname My Time at Sandrock - Love Chronicles
