e7b5514bf2f538a4f0842933bc90a7b197096013
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 02:22:07 +0800

    add 3172060.lua

--- 3172060.lua
+++ 3172060.lua
@@ -1,3 +1,6 @@
-addappid(3172060)
-addappid(3172061,0,"d66c7efadc63e9260aa599f569707a81a0981a2ac669d64fa1149f1ec7c9f0f1")
-setManifestid(3172061,"1282672771582943466")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3172060.lua
+--Gamename Sex Interview: Ice Cream
+addappid(3172060, 1, "efa294224bb2f7f5aea5bd739fd1a097d9d5474df6b5a0c111167ec635c219b4") --Mainappid Sex Interview: Ice Cream
+addtoken(3172060, "9591979397466884227")
+addappid(3172061, 1, "d66c7efadc63e9260aa599f569707a81a0981a2ac669d64fa1149f1ec7c9f0f1") --Main Windows Depot Sex Interview: Ice Cream
