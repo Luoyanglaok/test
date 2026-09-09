dbeb33db26bd6d21b7edfcc444fb086ea442c332
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 02:08:51 +0800

    add 1677040.lua

--- 1677040.lua
+++ 1677040.lua
@@ -1,3 +1,6 @@
-addappid(1677040)
-addappid(1677041,0,"d6a47630f2976e56daf7e1e494e518f25131e4d833f9947a74bdabb3cc1f28bd")
-setManifestid(1677041,"3934479534630562533")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1677040.lua
+--Gamename The Clinic of Depravity - A Wife Reveals Her True Nature in Front of Her Husband - Demo
+addappid(1677040) --Mainappid The Clinic of Depravity - A Wife Reveals Her True Nature in Front of Her Husband - Demo
+addappid(1677041, 1, "d6a47630f2976e56daf7e1e494e518f25131e4d833f9947a74bdabb3cc1f28bd") --Main Windows Depot The Clinic of Depravity - A Wife Reveals Her True Nature in Front of Her Husband - Demo
+setManifestid(1677041, "3934479534630562533", 106794336)
