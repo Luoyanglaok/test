a9b73a6e5fcbdab085fd00e26ed0431751215944
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 04:35:49 +0800

    add 1004490.lua

--- 1004490.lua
+++ 1004490.lua
@@ -1,11 +1,18 @@
-addappid(1004490)
-addappid(1004491,0,"1293fb9fd26fdaf27c67385704ccc255168787779beb45a309b36725506f778e")
-setManifestid(1004491,"7683747812532859775")
-addappid(1415430,0,"b830a4da289437fc1c3ebb420d7756aa8e5effef5ad0418456dc52c97657686b")
-setManifestid(1415430,"4237597664100428700")
-addappid(1415440,0,"a60ea3c7b87ab327b7535028f03fbd04708bade9885a64d4d564ba5d5a9ccf28")
-setManifestid(1415440,"340514984383026726")
-addappid(1415450,0,"4048f396cd7081d42d0c676d2b8638563dad10a46309dd84d93049fbb9911870")
-setManifestid(1415450,"8836853490041576741")
-addappid(1408620,0,"41a759a74c136baa213c5512785cd204cb0d9c54d927f5a73443c434769992e2")
-setManifestid(1408620,"5904890169833865056")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1004490.lua
+--Gamename Tools Up!
+addappid(1004490) --Mainappid Tools Up!
+addappid(1004491, 1, "1293fb9fd26fdaf27c67385704ccc255168787779beb45a309b36725506f778e") --Main Windows Depot Tools Up!
+setManifestid(1004491, "2366897851602559848", 602058304)
+--Dlcname Tools Up! Garden Party – Season Pass
+addappid(1408620, 1, "41a759a74c136baa213c5512785cd204cb0d9c54d927f5a73443c434769992e2") --Dlcname Tools Up! Garden Party – Season Pass
+setManifestid(1408620, "4409994996217151360", 677353568)
+--Dlcname Tools Up! Garden Party - Episode 1: The Tree House
+addappid(1415430, 1, "b830a4da289437fc1c3ebb420d7756aa8e5effef5ad0418456dc52c97657686b") --Dlcname Tools Up! Garden Party - Episode 1: The Tree House
+setManifestid(1415430, "4873241157228530827", 203702432)
+--Dlcname Tools Up! Garden Party - Episode 2: Tunnel Vision
+addappid(1415440, 1, "a60ea3c7b87ab327b7535028f03fbd04708bade9885a64d4d564ba5d5a9ccf28") --Dlcname Tools Up! Garden Party - Episode 2: Tunnel Vision
+setManifestid(1415440, "436093717187981521", 216735216)
+--Dlcname Tools Up! Garden Party - Episode 3: Home Sweet Home
+addappid(1415450, 1, "4048f396cd7081d42d0c676d2b8638563dad10a46309dd84d93049fbb9911870") --Dlcname Tools Up! Garden Party - Episode 3: Home Sweet Home
+setManifestid(1415450, "4610744902234520332", 256915920)
