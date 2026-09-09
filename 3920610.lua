78242ea1fabfa03ee9cf045170391c33514d553b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 02:38:34 +0800

    add 3920610.lua

---
+++ 3920610.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3920610.lua
+--Gamename FATAL FRAME II: Crimson Butterfly REMAKE
+addappid(3920610, 1, "8c9b8ce81e2921eea49b102eed1821aa883ad85645ff92115830bb24cbfa0bfc") --Mainappid FATAL FRAME II: Crimson Butterfly REMAKE
+addappid(3920611, 1, "12bffaedca2f7ee446211ba60b97b8262a19e282d803fde20b020cdd0d8ab780") --Main Windows Depot FATAL FRAME II: Crimson Butterfly REMAKE
+setManifestid(3920611, "1113019213164715548", 33289236384)
+--Dlcname FATAL FRAME II: Crimson Butterfly REMAKE Digital Art Book & Digital Soundtrack
+addappid(4123380, 1, "373ac671ea014e7cf29c347bbb4f27efa8fb93238f339504fa412757e9bd6b0a") --Dlcname FATAL FRAME II: Crimson Butterfly REMAKE Digital Art Book & Digital Soundtrack
+setManifestid(4123380, "2859176425516100251", 1191449696)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(4122450) --Dlcname FATAL FRAME II: Crimson Butterfly REMAKE Pre-Order Bonus Set
+addappid(4123350) --Dlcname FATAL FRAME II: Crimson Butterfly REMAKE Early Purchase Bonus Set
+addappid(4123360) --Dlcname FATAL FRAME II: Crimson Butterfly REMAKE Digital Deluxe Bonus Set
+addappid(4123370) --Dlcname FATAL FRAME II: Crimson Butterfly REMAKE Digital Deluxe Upgrade
+addappid(4423570) --Dlcname FATAL FRAME II: Crimson Butterfly REMAKE x SILENT HILL f Costume Set
