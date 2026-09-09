2cd03561b849aba965fe56855d0c8d05a1e26f58
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 05:52:22 +0800

    add 3606380.lua

---
+++ 3606380.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3606380.lua
+--Gamename Summer Days in the Joyful Countryside
+addappid(3606380, 1, "ffb73463722a5a8284329f31dec84f701742cc3514d5c891b592fce17d3c6435") --Mainappid Summer Days in the Joyful Countryside
+addappid(3606381, 1, "27dc0f5a02bfc74a349307e709f01cdd02a7a6e347f5db04e4e23ddc58d29d32") --Main Windows Depot Summer Days in the Joyful Countryside
+setManifestid(3606381, "81280967321966747", 125299504)
+addappid(3606382, 1, "22abe5802de495fd4a365612dcf1aacd18e4341ae98761d954393030c2297ef1") --Main Windows Language Depot Schinese Summer Days in the Joyful Countryside
+setManifestid(3606382, "3083404472717250589", 123107456)
+addappid(3606383, 1, "882b5f8e7f80b619f0ce137325e2d9a7d1fa061d34806f1df0a3652d908e1b9d") --Main Windows Language Depot Japanese Summer Days in the Joyful Countryside
+setManifestid(3606383, "9055061829151871812", 124024224)
