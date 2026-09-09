6e89294e17d1467ef5110dc07bcff385b749cac0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 02:55:20 +0800

    add 3357650.lua

---
+++ 3357650.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3357650.lua
+--Gamename PRAGMATA
+addappid(3357650, 1, "7d3fd8efe5fba21ded0c274905926cc56b6a26da1060d76462721945a8ef328a") --Mainappid PRAGMATA
+addappid(3357651, 1, "18e75bfb3f8c26b7a59d9e3a7013e74e1c59fe563aa00ee15f54a35b17691766") --Main Windows Depot PRAGMATA
+setManifestid(3357651, "6330832861176696160", 35462966992)
+addappid(3357652, 1, "7421326d63bf53701559733582fe45877e218f41683ebc6d614905727f5ba144") --Main Windows Depot PRAGMATA
+setManifestid(3357652, "5639705324720691310", 452115088)
+--Dlcname PRAGMATA: Outfit Set - Neo Bushido & Neo Kunoichi
+addappid(3859920, 1, "1fb811843160e2a11211f5bba90ece0ef2dac5994c19936e50ff85faf2f80423") --Dlcname PRAGMATA: Outfit Set - Neo Bushido & Neo Kunoichi
+setManifestid(3859920, "4731286747379700304", 89996576)
+--Dlcname PRAGMATA: Shelter Variety Pack
+addappid(3859930, 1, "14001799a6877a14751af43adbd59ca3cc037cf202aa5615e0c70e057090ddce") --Dlcname PRAGMATA: Shelter Variety Pack
+setManifestid(3859930, "6714427611547107917", 298515120)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
