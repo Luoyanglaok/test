3cd4a644d28288b55a24052097c61449b690a90e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 06:35:30 +0800

    add 4489930.lua

---
+++ 4489930.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4489930.lua
+--Gamename Memory Brew: Tales of Underworld
+addappid(4489930) --Mainappid Memory Brew: Tales of Underworld
+addappid(4489931, 1, "c6871211a6900f42f729755d89005f541d0c6a9eb1efd63c432417ced6e330e4") --Main Windows Depot Memory Brew: Tales of Underworld
+setManifestid(4489931, "6342295986500042226", 940002704)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
