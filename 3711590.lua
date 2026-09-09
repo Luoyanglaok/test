4747615ac0f1794a0feac7aa43a41370f55bbacb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 01:10:44 +0800

    add 3711590.lua

---
+++ 3711590.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3711590.lua
+--Gamename Train 45
+addappid(3711590, 1, "d0fbcba33e35102ec47709617020d7bc9b402c89fa0f1e92369a5972a8738a3b") --Mainappid Train 45
+addappid(3711591, 1, "66e440ffab28f63a155822137b57433dad7a9ddf50009e8d61ff1d48af37019c") --Main Windows Depot Train 45
+setManifestid(3711591, "8882598814287590726", 1302374736)
+--Dlcname Train 45 Artbook
+addappid(4612910) --Dlcname Train 45 Artbook
+addappid(4612911, 1, "45f012daa170c726e8d1234119150130a703e293a9513f461608700811726e63") --Dlc Windows Depot Train 45 Artbook
+setManifestid(4612911, "1415573599890324727", 125187520)
