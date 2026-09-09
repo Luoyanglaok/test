33d0afef5068221d0b1db6d76c40c393cad1a7c1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 02:55:38 +0800

    add 1659420.lua

--- 1659420.lua
+++ 1659420.lua
@@ -1,8 +1,13 @@
-addappid(1659420)
-setManifestid(228988,"6645201662696499616")
-addappid(1659421,0,"54f1e37db693643220de1240ebc50a0c00d4bc3a53ffdd91cf3f52cc71c91382")
-setManifestid(1659421,"7819112604911333886")
-addappid(1659422,0,"513eb93ddc7de41caabd6dfc7f8b2f5da716cc75ca9df1df674e4e339fceada2")
-setManifestid(1659422,"9007195248264946670")
-addappid(1659425,0,"6bc8b76c2fb8d3e01a2bb8698af8b91196dbcb08fe353fcf64b55649ae766043")
-setManifestid(1659425,"5288094124393013920")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1659420.lua
+--Gamename UNCHARTED™: Legacy of Thieves Collection
+addappid(1659420, 1, "10ca449663b7a1487266c15581817f160c914dba69e464d63504fc1071ab13ee") --Mainappid UNCHARTED™: Legacy of Thieves Collection
+addappid(1659421, 1, "54f1e37db693643220de1240ebc50a0c00d4bc3a53ffdd91cf3f52cc71c91382") --Main Windows Depot UNCHARTED™: Legacy of Thieves Collection
+setManifestid(1659421, "7819112604911333886", 67838369072)
+addappid(1659422, 1, "513eb93ddc7de41caabd6dfc7f8b2f5da716cc75ca9df1df674e4e339fceada2") --Main Windows Depot UNCHARTED™: Legacy of Thieves Collection
+setManifestid(1659422, "9007195248264946670", 28438571728)
+addappid(1659425, 1, "6bc8b76c2fb8d3e01a2bb8698af8b91196dbcb08fe353fcf64b55649ae766043") --Main Windows Depot UNCHARTED™: Legacy of Thieves Collection
+setManifestid(1659425, "5288094124393013920", 4707285216)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
