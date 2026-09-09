994745d7e67676eea3460094a49d161c42ece634
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 02:37:30 +0800

    add 1265920.lua

--- 1265920.lua
+++ 1265920.lua
@@ -1,5 +1,12 @@
-addappid(1265920)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1265921,0,"1d3906ef8fa5305f9430e0cdb7db5a577fe38da35a49dba3ec73ad9468f65807")
-setManifestid(1265921,"9135202032808388294")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1265920.lua
+--Gamename Life is Strange Remastered
+addappid(1265920, 1, "6385344305a3db4ba8ed9d30bb69f09ccebf561f7c3a64fc664c2c165c3c2f43") --Mainappid Life is Strange Remastered
+addappid(1265921, 1, "1d3906ef8fa5305f9430e0cdb7db5a577fe38da35a49dba3ec73ad9468f65807") --Main Windows Depot Life is Strange Remastered
+setManifestid(1265921, "9135202032808388294", 17239294128)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1537870) --Dlcname Life is Strange Remastered Collection DLC 'Zombie Crypt' Outfit
