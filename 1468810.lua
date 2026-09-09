ccab83fe6fce8e7c46642aaab4338b71b2a9e8c8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 06:48:29 +0800

    add 1468810.lua

--- 1468810.lua
+++ 1468810.lua
@@ -1,13 +1,8 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 1468810.lua
---Gamename 鬼谷八荒 Tale of Immortal
-addappid(1468810, 1, "947a1353f497af3a44dd791bbc5add8463d0714e94f78e608ca135ec6ae698c5") --Mainappid 鬼谷八荒 Tale of Immortal
-addtoken(1468810, "2905683081384123386")
-addappid(1468811, 1, "57880f9288973b73552c08af505e7d3e21db84ff579c2b5d08a34f092f652349") --Main Windows Depot 鬼谷八荒 Tale of Immortal
---Dlcname 鬼谷八荒 Soundtrack
-addappid(1536810) --Dlcname 鬼谷八荒 Soundtrack
-addappid(1536811, 1, "bf3164a85aaeb30ee465259766421af6d24cbe525d3ac405943888c56e3a7e98") --Dlc Windows Depot 鬼谷八荒 Soundtrack
-setManifestid(1536811, "7684741116744970612", 299370656)
-addappid(2408730) --Dlcname 鬼谷八荒 - 不归玄境
-addappid(3294480) --Dlcname Tale of Immortal - BaHuang Chronicles Digital Art Book
-addappid(3326620) --Dlcname Tale of Immortal - Five Blooms
+addappid(1468810)
+addappid(2408730)
+addappid(1536810)
+addappid(3294480)
+addappid(3326620)
+setManifestid(1468811,"5275750898890021375")
+setManifestid(228988,"6645201662696499616")
+setManifestid(229006,"1784011429307107530")
