f64212cc427ecf491f7027e2dbbe90695159fc50
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 02:26:10 +0800

    add 436090.lua

--- 436090.lua
+++ 436090.lua
@@ -1,5 +1,7 @@
-addappid(436090)
-addappid(436090,0,"23238f54eaeb9a42690c8335c61a6b00ea7d1cb65421b6f7e24f3c7134888de4")
-setManifestid(436090,"1380443608017988433")
-addappid(436091,0,"72edbb2c0d316fd9b5f39aa3d480ecf62bddadd611a728a5ccd95b66a5200ba5")
-setManifestid(436091,"1142415089460811828")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 436090.lua
+--Gamename Factorio - Soundtrack
+addappid(436090, 1, "23238f54eaeb9a42690c8335c61a6b00ea7d1cb65421b6f7e24f3c7134888de4") --Mainappid Factorio - Soundtrack
+setManifestid(436090, "1380443608017988433", 115639104)
+addappid(436091, 1, "72edbb2c0d316fd9b5f39aa3d480ecf62bddadd611a728a5ccd95b66a5200ba5") --Main Windows Depot Factorio - Soundtrack
+setManifestid(436091, "1142415089460811828", 346307360)
