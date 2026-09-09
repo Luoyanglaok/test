4bcb2a28773e5262ac5c8e4c7c55110966131215
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 03:01:01 +0800

    add 2860190.lua

--- 2860190.lua
+++ 2860190.lua
@@ -1,3 +1,6 @@
-addappid(2860190)
-addappid(2860191,0,"9204c19dbbf6f8a7cb49e838dbaa6a8c6e6fb31bf37a7a53ae15b7514fa284b7")
-setManifestid(2860191,"9040623639093436585")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2860190.lua
+--Gamename 西游记 幻想西游释厄传
+addappid(2860190) --Mainappid 西游记 幻想西游释厄传
+addappid(2860191, 1, "9204c19dbbf6f8a7cb49e838dbaa6a8c6e6fb31bf37a7a53ae15b7514fa284b7") --Main Windows Depot 西游记 幻想西游释厄传
+setManifestid(2860191, "9040623639093436585", 1694117856)
