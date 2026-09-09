3db6dc923d7449a2ae0bedc10a7924abd5efc37b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 03:02:01 +0800

    add 4570720.lua

---
+++ 4570720.lua
@@ -0,0 +1,36 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4570720.lua
+--Gamename DragonSword : Awakening
+addappid(4570720, 1, "1479788b29b18f4924dc12f1ac3c2c7a8afbc734c3b8a5774d4ba64995651725") --Mainappid DragonSword : Awakening
+addappid(4570721, 1, "bcfd9b5aa6729364b267af933d75e064c35ed2dfee9d3529186bed1713221251") --Main Windows Depot DragonSword : Awakening
+setManifestid(4570721, "7070000222387465389", 17217536640)
+addappid(4570722, 1, "3061e4e1f78023461c0fb8fdf5e9368efd7b0d77c114546e038ebd6749408ab9") --Main Windows Depot DragonSword : Awakening
+setManifestid(4570722, "41935765332657826", 13028314048)
+--Dlcname DragonSword : Awakening - Deluxe Pack
+addappid(4742640) --Dlcname DragonSword : Awakening - Deluxe Pack
+addappid(4742641, 1, "1b66d546a7e756f5dbdf86fefdc35f338217b02efbe352c2f85518d9398a4835") --Dlc Windows Depot DragonSword : Awakening - Deluxe Pack
+setManifestid(4742641, "4473994521257316453", 512010656)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4742660) --Dlcname DragonSword : Awakening - Familiar : Abyssal Direwolf
+addappid(4785980) --Dlcname DragonSword : Awakening - Familiar : Viola Flying Squirrel
+addappid(4785990) --Dlcname DragonSword : Awakening - Familiar : Cotton Flying Squirrel
+addappid(4786000) --Dlcname DragonSword : Awakening - Familiar : Orchid Wing Cat
+addappid(4786010) --Dlcname DragonSword : Awakening - Familiar : Timid Hummingbird
+addappid(4786020) --Dlcname DragonSword : Awakening - Familiar : Gale Sky Wolf
+addappid(4786030) --Dlcname DragonSword : Awakening - Familiar : Frost Sky Wolf
+addappid(4786040) --Dlcname DragonSword : Awakening - Familiar : Exploration Squad Griffon
+addappid(4786050) --Dlcname DragonSword : Awakening - Familiar : Holy Knightage Griffon
+addappid(4786220) --Dlcname DragonSword : Awakening - Costume : Ophelia's Oath
+addappid(4786230) --Dlcname DragonSword : Awakening - Costume : Roderick Royal Uniform
+addappid(4786240) --Dlcname DragonSword : Awakening - Costume : Phantom Thief of Dawn
+addappid(4786250) --Dlcname DragonSword : Awakening - Costume : Black-Dyed Vestment
+addappid(4786270) --Dlcname DragonSword : Awakening - Costume : Wild Strawberry Waitress
+addappid(4872640) --Dlcname DragonSword : Awakening - Familiar : Flame Spirit Fox
+addappid(4872650) --Dlcname DragonSword : Awakening - Familiar : War Direwolf
+addappid(4911300) --Dlcname DragonSword : Awakening - Costume : Coral Summer Mischief
+addappid(4911310) --Dlcname DragonSword : Awakening - Costume : White Flower of the Blue Waves
+addappid(5000050) --Dlcname DragonSword : Awakening - Costume : Moonlight on the Night Sea
