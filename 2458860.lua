6d853c8f4e769ca8d0f532ffcc9e19b161b902ec
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-09 23:29:14 +0800

    add 2458860.lua

--- 2458860.lua
+++ 2458860.lua
@@ -1,5 +1,9 @@
-addappid(2458860)
-addappid(2458861,0,"3e20430ed7318a56fac69f79ed704aa4e7c820ad27f78481e5b31fcd6bb4fd87")
-setManifestid(2458861,"4472995105930506671")
-addappid(3008220,0,"edd0c6d426f35323011c803abd710e9946c91ef04f58aaa64cc6b6d55b39ddec")
-setManifestid(3008220,"8646972810412396678")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2458860.lua
+--Gamename Summer Clover
+addappid(2458860, 1, "6a1c634d72ac6ceb1373601c4205e644053c435d6b09fb32f6ceea9da379b783") --Mainappid Summer Clover
+addappid(2458861, 1, "3e20430ed7318a56fac69f79ed704aa4e7c820ad27f78481e5b31fcd6bb4fd87") --Main Windows Depot Summer Clover
+setManifestid(2458861, "4472995105930506671", 4103715232)
+--Dlcname Summer Clover -Artbook-
+addappid(3008220, 1, "edd0c6d426f35323011c803abd710e9946c91ef04f58aaa64cc6b6d55b39ddec") --Dlcname Summer Clover -Artbook-
+setManifestid(3008220, "8646972810412396678", 179387072)
