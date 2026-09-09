e9ebc49695e983d9aabb9151196ffe9bd8139eda
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 02:14:23 +0800

    add 3521860.lua

--- 3521860.lua
+++ 3521860.lua
@@ -1,3 +1,6 @@
-addappid(3521860)
-addappid(3521861,0,"ad72b1afd0ffbd442ab1437a9c0874efd5d6c08e1bf9c5920c79ee839f251af2")
-setManifestid(3521861,"8453476761058067006")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3521860.lua
+--Gamename Wolf on Rail
+addappid(3521860, 1, "736de9302b25e434924cd87cbc2501af410ba6415583ec3a280a6be63a3cd25e") --Mainappid Wolf on Rail
+addappid(3521861, 1, "ad72b1afd0ffbd442ab1437a9c0874efd5d6c08e1bf9c5920c79ee839f251af2") --Main Windows Depot Wolf on Rail
+setManifestid(3521861, "8453476761058067006", 1999235008)
