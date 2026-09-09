1a6e601b004cf9988aededf6068641156d153fab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 01:10:42 +0800

    add 2897760.lua

--- 2897760.lua
+++ 2897760.lua
@@ -1,4 +1,15 @@
-addappid(2897760)
-setManifestid(228990,"1829726630299308803")
-addappid(2897761,0,"d309294d9f28f61bc9fd632ec2133bc496ef6fb2c353ef57e9af630ea77ac693")
-setManifestid(2897761,"8754151215226782444")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2897760.lua
+--Gamename Romantic Escapades
+addappid(2897760, 1, "7389a2c9d5eb3e5f06dc2d9ee37ed9979eec1e027cb308273e2ea1ee29bc569f") --Mainappid Romantic Escapades
+addappid(2897761, 1, "d309294d9f28f61bc9fd632ec2133bc496ef6fb2c353ef57e9af630ea77ac693") --Main Windows Depot Romantic Escapades
+setManifestid(2897761, "7784528564798080034", 7383790624)
+--Dlcname Romantic Escapades Soundtrack
+addappid(3929550) --Dlcname Romantic Escapades Soundtrack
+addappid(3929551, 1, "3713bf6a11e1b17831377fde1a19ae9f33e1dcf7365566c115733588f9a203f0") --Dlc Windows Depot Romantic Escapades Soundtrack
+setManifestid(3929551, "8025783756294781317", 104154528)
+addappid(3929552, 1, "185f7d52ca87254f7802c80ee5e87779f86cf7a5492c102f21411dd52cb2cc87") --Dlc Windows Depot Romantic Escapades Soundtrack
+setManifestid(3929552, "2299769207102751128", 690212080)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
