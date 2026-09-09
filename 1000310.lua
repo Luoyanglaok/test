0d008dc2e8fc1c88402777f20d69d8feaef92eab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 04:42:57 +0800

    add 1000310.lua

--- 1000310.lua
+++ 1000310.lua
@@ -1,3 +1,6 @@
-addappid(1000310)
-addappid(1000311,0,"852760afc099c680dd944fbbdcb467ebe2c3f68e4399091d2ade46f306dfd402")
-setManifestid(1000311,"8448870456805172263")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1000310.lua
+--Gamename 人气动漫大乱斗
+addappid(1000310) --Mainappid 人气动漫大乱斗
+addappid(1000311, 1, "852760afc099c680dd944fbbdcb467ebe2c3f68e4399091d2ade46f306dfd402") --Main Windows Depot 人气动漫大乱斗
+setManifestid(1000311, "6016477133199262546", 919983904)
