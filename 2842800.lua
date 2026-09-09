1479244d5fb33ca8ff604a2d25114764512ed01e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 06:55:33 +0800

    add 2842800.lua

--- 2842800.lua
+++ 2842800.lua
@@ -1,9 +1,3 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 2842800.lua
---Gamename 尸姬之梦
-addappid(2842800, 1, "9724c7694b935986ddf5f3c2f439475ba20d3b26ae14e102be0515f25ef2d05a") --Mainappid 尸姬之梦
-addappid(2842801, 1, "ff61e4f261e08ac99710b14666f720fdc9a3e577d65e65654bc60ca5f25482c7") --Main Windows Depot 尸姬之梦
-setManifestid(2842801, "8053485807408555788", 2316381968)
-addappid(4661130) --Dlcname 尸姬之梦-神明的三尸
---Missing Dlcs
---Dlcname 尸姬之梦 音乐集
+addappid(2842800)
+addappid(4661130)
+addappid(4346830)
