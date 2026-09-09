a2a9f601d15f68ca4111af88e879cb4893de7206
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 07:52:11 +0800

    add 2651410.lua

---
+++ 2651410.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2651410.lua
+--Gamename The Endless Abyss: A Busty Cop’s Corrupted Soul
+addappid(2651410, 1, "35c7058b791808ea1d1ca43ae08cd5c8fbc7765a139af1f1c7d969d466290a99") --Mainappid The Endless Abyss: A Busty Cop’s Corrupted Soul
+addappid(2651411, 1, "bfed488dd748d6b586605ec0e9b8a5bafdbe2b4d4d193ee2e6c9d3e93e3d90a1") --Main Windows Depot The Endless Abyss: A Busty Cop’s Corrupted Soul
+setManifestid(2651411, "4179475202283467201", 9169467184)
