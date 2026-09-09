d2e379cf21fd768556a2457c29b57ce75c15f1b9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 00:01:45 +0800

    add 1582680.lua

--- 1582680.lua
+++ 1582680.lua
@@ -1,5 +1,11 @@
-addappid(1582680)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1582681,0,"6777aac904aa3af99ba8147dc462a6ffd184255b8fba3a0ec5a9fc171bf2dd6c")
-setManifestid(1582681,"7007998724030429617")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1582680.lua
+--Gamename Trolley Problem, Inc.
+addappid(1582680) --Mainappid Trolley Problem, Inc.
+addappid(1582681, 1, "6777aac904aa3af99ba8147dc462a6ffd184255b8fba3a0ec5a9fc171bf2dd6c") --Main Windows Depot Trolley Problem, Inc.
+setManifestid(1582681, "7007998724030429617", 471363568)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
