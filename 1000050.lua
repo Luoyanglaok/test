d4a08a29cc745488d8d18983ab1793989df55334
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 04:44:20 +0800

    add 1000050.lua

--- 1000050.lua
+++ 1000050.lua
@@ -1,3 +1,6 @@
-addappid(1000050)
-addappid(1000051,0,"3accd906f5b479163db6b3aac72a36ac5da9646bdf01edada5baf0554a1c0b73")
-setManifestid(1000051,"6358551512135168976")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1000050.lua
+--Gamename 我的一个多重人格朋友
+addappid(1000050) --Mainappid 我的一个多重人格朋友
+addappid(1000051, 1, "3accd906f5b479163db6b3aac72a36ac5da9646bdf01edada5baf0554a1c0b73") --Main Windows Language Depot Schinese 我的一个多重人格朋友
+setManifestid(1000051, "6358551512135168976", 351424560)
