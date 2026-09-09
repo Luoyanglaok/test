27692cc867f0cb0dc2ce542de007526cb83aaead
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-09 10:12:51 +0800

    add 3405690.lua

---
+++ 3405690.lua
@@ -0,0 +1,72 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3405690.lua
+--Gamename EA SPORTS FC™ 26
+addappid(3405690, 1, "d72f742e665d75526611fdff936f05ab1703820ecf6f6c5764f3173e5b6a401d") --Mainappid EA SPORTS FC™ 26
+addappid(3405691, 1, "12a93ecb44c6b853d762578ffef21df60aa702f723232d87f81f485f7e636684") --Main Windows Depot EA SPORTS FC™ 26
+setManifestid(3405691, "1432614305134452702", 56088664080)
+addappid(3405692, 1, "c6ec306084ddbc34166f66738b51806ae01d351b007df9dbfb5a17531575b5b7") --Main Windows Language Depot English EA SPORTS FC™ 26
+setManifestid(3405692, "5738979875848121619", 3654282784)
+addappid(3405693, 1, "684e872bb91267d94cb6b604f3851b322d7f3097347d27a21ba119c822c80ac1") --Main Windows Language Depot German EA SPORTS FC™ 26
+setManifestid(3405693, "4449537283497051323", 5023063104)
+addappid(3405694, 1, "807a1450e4e3200b6fa0ee63b41651b467484775b8b7cd64e1fdd3f32e2ff466") --Main Windows Language Depot French EA SPORTS FC™ 26
+setManifestid(3405694, "7708960131740705031", 4724000672)
+addappid(3405695, 1, "9d8e39a63134b325186f44ee79df6023adbd4585f926ac27d99fd20d2b4689b1") --Main Windows Language Depot Italian EA SPORTS FC™ 26
+setManifestid(3405695, "567537300398711155", 5022630720)
+addappid(3405696, 1, "b48a48cfacf2e2e24f564c56bea0624d00744fb79f94bc6470e039dbf783b0fb") --Main Windows Language Depot Koreana EA SPORTS FC™ 26
+setManifestid(3405696, "7670306672842937546", 4819778224)
+addappid(3405697, 1, "05c30614464586cbdaa5cc6d2420004ed85be6113860ed3092acba46190d3e10") --Main Windows Language Depot Spanish EA SPORTS FC™ 26
+setManifestid(3405697, "1653017136779184063", 5087453200)
+addappid(3405698, 1, "d4c795b81851da1cdb29d739abcde82d3af4972dde9e4cfb47afe6f602132713") --Main Windows Language Depot Schinese EA SPORTS FC™ 26
+setManifestid(3405698, "7844111839133819995", 4885631008)
+addappid(3405699, 1, "4bd67983492c731beceea51679510dc6a8f3b8dd30f42b307b61803d305082a7") --Main Windows Language Depot Tchinese EA SPORTS FC™ 26
+setManifestid(3405699, "360529476766717131", 3654288128)
+addappid(3405701, 1, "34125f2eddef4bd7192c97bc6e5daa2280c2f8a2bf449efb544c557bf0c64345") --Main Windows Language Depot Russian EA SPORTS FC™ 26
+setManifestid(3405701, "7500160448109390177", 3664963984)
+addappid(3405702, 1, "280968368ef276032b87f704f89e4e72c508265b1941279a0fd917cce852ccbb") --Main Windows Language Depot Japanese EA SPORTS FC™ 26
+setManifestid(3405702, "1675460796885309467", 5136887680)
+addappid(3405703, 1, "b4952b326fd29fbc8305521132204db66bf42da6f2a1762ba749fc0dfb19fd51") --Main Windows Language Depot Portuguese EA SPORTS FC™ 26
+setManifestid(3405703, "4871905346881452284", 3654291184)
+addappid(3405704, 1, "a9646252767ad1e1b4dadb53f0b3e55aa3f47b24194411c4936119bc38292e13") --Main Windows Language Depot Polish EA SPORTS FC™ 26
+setManifestid(3405704, "3606116345272469468", 5165445888)
+addappid(3405705, 1, "6beb7e95413777101eb6bcc85a1d4b3cb8944742ac13fd0b763fd767ad45a2bf") --Main Windows Language Depot Danish EA SPORTS FC™ 26
+setManifestid(3405705, "323745656453383837", 3654290464)
+addappid(3405706, 1, "49e4266b919686a1e009092bebae33ef34e56a40da196671f5caf22f8a0f10fd") --Main Windows Language Depot Dutch EA SPORTS FC™ 26
+setManifestid(3405706, "3954198720303542359", 5195596752)
+addappid(3405707, 1, "474261f80ec08ec0f21ce91b7ce542b3b9a16e0b716191a81d8dc66683b74cdd") --Main Windows Language Depot Norwegian EA SPORTS FC™ 26
+setManifestid(3405707, "2792763074705464558", 3654289872)
+addappid(3405708, 1, "5b4b9d2954b602a49dc87189e660332a7b8c81c88fee10b6350dacfd14704572") --Main Windows Language Depot Swedish EA SPORTS FC™ 26
+setManifestid(3405708, "3518844953722649", 3654290992)
+addappid(3405709, 1, "efcad7fae359a3dbcb0caa0e2a2f21a3f2996e9d90a94b58e40b635a87a85244") --Main Windows Language Depot Czech EA SPORTS FC™ 26
+setManifestid(3405709, "7586108621946051268", 3654296608)
+addappid(3405711, 1, "a47af223a991d72c48d1af1aa4232265ee7c65ad78a651f5bbd8a7b773a2618a") --Main Windows Language Depot Turkish EA SPORTS FC™ 26
+setManifestid(3405711, "6815406496707485583", 3657392704)
+addappid(3405712, 1, "1af2e8bc3d90234bfd55dd6d7273b412568c2cea29e93dbaa1148cb3ea7426fe") --Main Windows Language Depot Arabic EA SPORTS FC™ 26
+setManifestid(3405712, "352367862036374069", 5335206976)
+addappid(3405713, 1, "9a264107b9f1925d8e91afb72a2d7f8c706ad8db2ac6aab2cdfc64d2c88ee3ab") --Main Windows Language Depot Brazilian EA SPORTS FC™ 26
+setManifestid(3405713, "4483739851889895290", 5276291440)
+addappid(3405714, 1, "3d05b44725b01a4b2777e069e593450f655d45a0768d7b25590a71c70a68c64b") --Main Windows Language Depot Latam EA SPORTS FC™ 26
+setManifestid(3405714, "6155538128257137927", 5209786928)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") --Share Windows Depot EA APP Installer
+setManifestid(3340991, "5874704072306837744", 246482576)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6") --Share Windows Depot EA Javelin Anticheat Installer
+setManifestid(3893181, "3685412901435514107", 318033232)
+addappid(3405710) --Dlcname EA SPORTS FC 26 - EA Play Trial Key
+addappid(3405720) --Dlcname EA SPORTS FC 26 - Press Offer Key
+addappid(3405830) --Dlcname EA SPORTS FC 26 - Standard Edition Key
+addappid(3484390) --Dlcname EA SPORTS FC™ 26 Ultimate Edition Pre-Purchase content
+addappid(3484440) --Dlcname EA SPORTS FC 26 - Ultimate Preorder Edition Key
+addappid(3484530) --Dlcname EA SPORTS FC 26 - Ultimate Edition Key
+addappid(3707320) --Dlcname EA SPORTS FC™ 26 - FC Points
+addappid(3765110) --Dlcname EA SPORTS FC™ 25 Football Ultimate Team™ rewards
+addappid(3922840) --Dlcname EA SPORTS FC™ 26
+addtoken(3922840, "605253596113519784")
+addappid(4146820) --Dlcname EA SPORTS FC™ 26 TOTY Edition
+addappid(4328110) --Dlcname EA SPORTS FC™ 26 ICONS Edition
+addappid(4488060) --Dlcname EA SPORTS FC™ 26 - The World's Game Edition Catch-Up Pack
+addappid(4637600) --Dlcname EA SPORTS FC™ 26 Football Ultimate Team™ 92+ OVR ICON Player Item
+addappid(4637610) --Dlcname EA SPORTS FC™ 26 Football Ultimate Team™ rewards
+addappid(4637620) --Dlcname EA SPORTS FC™ 26 Football Ultimate Team™ rewards
+addappid(4637630) --Dlcname EA SPORTS FC™ 26 Football Ultimate Team™ rewards
