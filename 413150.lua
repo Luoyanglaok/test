f06fe718b4ae336fab45fea8a81adb51f127c112
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 03:22:07 +0800

    add 413150.lua

--- 413150.lua
+++ 413150.lua
@@ -1,9 +1,19 @@
-addappid(413150)
-setManifestid(229002,"7260605429366465749")
-setManifestid(229012,"4353723233161159493")
-addappid(413151,0,"ff71699a17787b798d901cb27398556eb69a498b690b4392b2ffedcacc1019ff")
-setManifestid(413151,"4278718763097142923")
-addappid(413152,0,"c65e7919242bdcb29efd9551af381e9b6f5d828ac7382cec83f1d8459aaf3fdf")
-setManifestid(413152,"2618652301291810021")
-addappid(413153,0,"d1e04015717ef68e43064bd29a027c3309b47bd156d0494c7d4dff56a971e3c7")
-setManifestid(413153,"6005910083361727734")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 413150.lua
+--Gamename Stardew Valley
+addappid(413150, 1, "2352cc75feedbdb8e9a6d3dd1f6a71af9d38068a6369af6ca83e4301abee4100") --Mainappid Stardew Valley
+addappid(413151, 1, "ff71699a17787b798d901cb27398556eb69a498b690b4392b2ffedcacc1019ff") --Main Windows Depot Stardew Valley
+setManifestid(413151, "4278718763097142923", 453699840)
+addappid(413152, 1, "c65e7919242bdcb29efd9551af381e9b6f5d828ac7382cec83f1d8459aaf3fdf") --Main Macos Depot Stardew Valley
+setManifestid(413152, "2618652301291810021", 456794576)
+addappid(413153, 1, "d1e04015717ef68e43064bd29a027c3309b47bd156d0494c7d4dff56a971e3c7") --Main Linux Depot Stardew Valley
+setManifestid(413153, "6005910083361727734", 457681328)
+--Dlcname Stardew Valley Soundtrack
+addappid(440820, 1, "ed2360d39a75d023e9cb2b89cb5e96f9843f575ae8223776874b13a21ca231dd") --Dlcname Stardew Valley Soundtrack
+addappid(440821, 1, "b7326aa9312d03f8acabdcb537792cd63cf4e5b611a7a17a5ae5c3055ccea894") --Dlc Windows Depot Stardew Valley Soundtrack
+setManifestid(440821, "5706279157015252834", 332305904)
+--Share Depots
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229012, "4353723233161159493", 6387264)
