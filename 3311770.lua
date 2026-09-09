5fbef81a5c3061c8a89275386b8e822eb43881b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 02:26:02 +0800

    add 3311770.lua

--- 3311770.lua
+++ 3311770.lua
@@ -1,5 +1,8 @@
-addappid(3311770)
-addappid(3311771,0,"0565f14b3a4c063e57b257b849a52b2778d79106d0a3dacc462dabf6dc7bf0e7")
-setManifestid(3311771,"7297438335085052778")
-addappid(3311772,0,"a990370a5957f13aa9ffa191a2f9fdb2bce9723697971288d0cdb65f2779cfa2")
-setManifestid(3311772,"9105904954403112113")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3311770.lua
+--Gamename Factorio: Space Age - Soundtrack
+addappid(3311770) --Mainappid Factorio: Space Age - Soundtrack
+addappid(3311771, 1, "0565f14b3a4c063e57b257b849a52b2778d79106d0a3dacc462dabf6dc7bf0e7") --Main Windows Depot Factorio: Space Age - Soundtrack
+setManifestid(3311771, "7297438335085052778", 1510326736)
+addappid(3311772, 1, "a990370a5957f13aa9ffa191a2f9fdb2bce9723697971288d0cdb65f2779cfa2") --Main Windows Depot Factorio: Space Age - Soundtrack
+setManifestid(3311772, "9105904954403112113", 2925115424)
