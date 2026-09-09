76a1e4efef0168eecb295c758078d0ba5995777c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 03:06:36 +0800

    add 3929330.lua

---
+++ 3929330.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3929330.lua
+--Gamename Relief
+addappid(3929330) --Mainappid Relief
+addappid(3929331, 1, "17ded237a6fb72f1fbc3a819e4a05690e49ca77c2f2bdc585ba578ffcd7056e5") --Main Windows Depot Relief
+setManifestid(3929331, "164763032894257649", 1130978272)
+--Missing Dlcs
+--Dlcname 释厄传 Soundtrack
