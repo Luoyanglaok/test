37e5cae0a6ba2100207b9c14edc351a42ccd7763
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:37:29 +0800

    add 3819640.lua

---
+++ 3819640.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3819640.lua
+--Gamename Megastore Simulator
+addappid(3819640, 1, "c5ca5045f5114cca24f02d4de5da563ad6097c7bfb1dd698c0be703c24c47be9") --Mainappid Megastore Simulator
+addappid(3819641, 1, "cd3a3b0b3ca76fc3fd11c8c5426cc433df4d406d30e4d4fce18621a4742cdd64") --Main Windows Depot Megastore Simulator
+setManifestid(3819641, "5445863887271753549", 3027660032)
