b9a98875f980bd12a5e5c9ff5d755199dce300cc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:42:11 +0800

    add 1086940.lua

--- 1086940.lua
+++ 1086940.lua
@@ -1,12 +1,119 @@
-addappid(1086940)
-addappid(2378500)
-addappid(2956320)
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(1086941,"4288948197859959444")
-setManifestid(1086947,"4701297036193868114")
-setManifestid(1419652,"7005275183208636774")
-setManifestid(1419653,"2098612237190800348")
-setManifestid(2378500,"6453229909780803137")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1086940.lua
+--Gamename Baldur's Gate 3
+addappid(1086940, 1, "b67aeb033de6a4692edc568edb48a6cd6e11592f5adcaacf9465d4a459b04bd2") --Mainappid Baldur's Gate 3
+addappid(1086941, 1, "4876e089a6bb7a4d8da586306d4040c7d4bd5fb7c05da6c5441a760aa0b2bef6") --Main Windows Depot Baldur's Gate 3
+setManifestid(1086941, "4288948197859959444", 127375426624)
+addappid(1086942, 1, "5313c9d372b30e73c15606d9792e5f23314c21559d35f54eb7b5e8369e28b036") --Main Windows Language Depot English Baldur's Gate 3
+setManifestid(1086942, "17875152607970122", 304)
+addappid(1086943, 1, "8e611b984e4bee3d57f719cb5f73a69d2935cd826ab0cef2185e5d3bfe0dc272") --Main Windows Language Depot French Baldur's Gate 3
+setManifestid(1086943, "2118230943991643525", 14340688)
+addappid(1086944, 1, "c5ba980103bb168497ff98d0faca3bdef34d8328098d43aee0dce6461468daa6") --Main Windows Language Depot German Baldur's Gate 3
+setManifestid(1086944, "3423097641402863301", 12926096)
+addappid(1086945, 1, "5bfe4c9e909a98b53a970a1cab2bc67cafdea68b169c3f585b8b3b0762282b74") --Main Windows Language Depot Polish Baldur's Gate 3
+setManifestid(1086945, "9002272171910189584", 14049168)
+addappid(1086946, 1, "febd9048a518edadea98e5c5daa5cbaf3e9e0e5920154007f97111cbf487ef52") --Main Windows Language Depot Russian Baldur's Gate 3
+setManifestid(1086946, "2409481873063333868", 15378224)
+addappid(1086947, 1, "1e9c89eb1395c0ae168407fbcf33ea43b0f8937033d45485ee2a17737bd6beeb") --Main Windows Language Depot Schinese Baldur's Gate 3
+setManifestid(1086947, "4701297036193868114", 21935104)
+addappid(1086948, 1, "0b7198f84d1c683b5c4670d13cbad273d50896bb16424423814d15a4be629923") --Main Windows Language Depot Tchinese Baldur's Gate 3
+setManifestid(1086948, "8505682662122387972", 29185232)
+addappid(1086949, 1, "2bd5f702d5349ecff8f4d5a2d575c883939b435d0081671954314a093a24acd4") --Main Windows Language Depot Spanish Baldur's Gate 3
+setManifestid(1086949, "4043484420852339008", 13047104)
+addappid(1419650, 1, "8ff19ec3475e30f649dbd9afdeec7805c60162b66d348bf06fe9b0ebf31f8095") --Main Windows Language Depot Italian Baldur's Gate 3
+setManifestid(1419650, "6250389178731869788", 13835072)
+addappid(1419651, 1, "cb6dcb0c6b59caa4d14f208f4c91dda0db49dcb244487f8d2939e4d0df1a0fa2") --Main Windows Language Depot Turkish Baldur's Gate 3
+setManifestid(1419651, "9051166838150188236", 12272832)
+addappid(1419652, 1, "43c13e12030aed1fdb00bd1c630bd68f0d22cb23289c879b00d7904aa05fcc89") --Main Windows Depot Baldur's Gate 3
+setManifestid(1419652, "7005275183208636774", 133133984)
+addappid(1419653, 1, "dc9bee49d256e78104b71d0bf90e9818631b069dce3b576a86fa6345d3b94896") --Main Windows Depot Baldur's Gate 3
+setManifestid(1419653, "2098612237190800348", 109616016)
+addappid(1419657, 1, "93e82e826849110eafaab1cbce34013b629c83ae54b721b04bd63972768f5864") --Main Macos Language Depot Turkish Baldur's Gate 3
+setManifestid(1419657, "4548607990756240099", 304)
+addappid(1419658, 1, "78d07ca5c99bdbaba2167c7c6fbe4ebae6ed0abb76e65e207209cce9eff9d084") --Main Windows Language Depot Brazilian Baldur's Gate 3
+setManifestid(1419658, "916483664598379515", 12721712)
+addappid(1419659, 1, "6cb8cd5bf7992d1a73f8c457ee5f5ee287b01822132084291522c132ebea1f6e") --Main Macos Language Depot Koreana Baldur's Gate 3
+setManifestid(1419659, "2290135083393841644", 304)
+addappid(1419660, 1, "d43be075ab476e7f895098958154afeb0c5982041685354104a4bf8347df1f48") --Main Macos Depot Baldur's Gate 3
+setManifestid(1419660, "7960684881345890822", 128671813280)
+addappid(1419661, 1, "f0abb3e52786d3d5d6c0788d8e5fe6855b1520f5034d75618265726a370cc3ce") --Main Macos Language Depot English Baldur's Gate 3
+setManifestid(1419661, "4191530919188121973", 304)
+addappid(1419662, 1, "d692cbc14df4524227c601f481987bec2c22bcf56a944040ed254405b4f3bb95") --Main Macos Language Depot French Baldur's Gate 3
+setManifestid(1419662, "1875852885473929394", 304)
+addappid(1419663, 1, "4ff33f8dbf2f891668d22bd1260f2bdaf5900124c67995c506265695ade438bd") --Main Macos Language Depot German Baldur's Gate 3
+setManifestid(1419663, "4691743920644356019", 304)
+addappid(1419664, 1, "00d4de0f8f717ab8a08bbb01a737d9af341c164d9f5c44b840ec3e6235852fe9") --Main Macos Language Depot Russian Baldur's Gate 3
+setManifestid(1419664, "3234554884097886807", 304)
+addappid(1419665, 1, "473f4742a5cf82f9ba639d7e657c84e116493b30b0e7aa8be56cc7bbb297b391") --Main Macos Language Depot Polish Baldur's Gate 3
+setManifestid(1419665, "7659581228351622955", 304)
+addappid(1419666, 1, "08c2d7418efd3e58ff1d2d7ebbb5b5f6e86a1756093b3ac1fec27bdf6b5a12f8") --Main Macos Language Depot Spanish Baldur's Gate 3
+setManifestid(1419666, "6495500843315473226", 304)
+addappid(1419667, 1, "d20164301f37c4ff691e7f9e5881cfa57ef7f65447ec317fd3098594c08cd8ee") --Main Macos Language Depot Schinese Baldur's Gate 3
+setManifestid(1419667, "4852131502867675361", 304)
+addappid(1419668, 1, "abd8a66e97a01588a93c4e77a4a246a771b76b15db1c16e2a08bc2870f587819") --Main Windows Language Depot Japanese Baldur's Gate 3
+setManifestid(1419668, "6176479858889274652", 18961456)
+addappid(1419669, 1, "9bcfd7785faa9f9c46555e4c214e8d4a3445e88d3ba8708e3717c6078fc38801") --Main Windows Language Depot Koreana Baldur's Gate 3
+setManifestid(1419669, "9081827886419290237", 18214064)
+addappid(2330350, 1, "e29448a5a803bac897d89c21a54271b93cad4793263caeae3e19c0b963524614") --Main Windows Language Depot Ukrainian Baldur's Gate 3
+setManifestid(2330350, "1205466515600447451", 14619664)
+addappid(2330351, 1, "00106a111eb560b12b684a5b635d7966762ac65f3b3830ddc40b7b09febc82ae") --Main Windows Language Depot Latam Baldur's Gate 3
+setManifestid(2330351, "7646694850759131929", 12977296)
+addappid(2330352, 1, "e7f759c9f39f104d8f5bb49153caac2195d006bb4f23b40254199f16ecd8cfc6") --Main Macos Language Depot Tchinese Baldur's Gate 3
+setManifestid(2330352, "3405764980446251715", 304)
+addappid(2330353, 1, "f74356cba683ca39ecc7d21a5853bf3b6571875c0dc058ab0ecfb817c9a9d41e") --Main Macos Language Depot Italian Baldur's Gate 3
+setManifestid(2330353, "4279847977508632405", 304)
+addappid(2330354, 1, "5b198327f5564ecfc10d6151138f69c766456916feb7958a363ab3959aff9de2") --Main Macos Language Depot Ukrainian Baldur's Gate 3
+setManifestid(2330354, "7630747791077435590", 304)
+addappid(2330355, 1, "69df36ae1000c3da76a7a4b0dad51657dc450a972e53f1fef589df34e48a0885") --Main Macos Language Depot Latam Baldur's Gate 3
+setManifestid(2330355, "8819605045630475220", 304)
+addappid(2330356, 1, "05ab4c7489dac005d12b294f688338c89699983da4003781842e4669d7201331") --Main Macos Language Depot Brazilian Baldur's Gate 3
+setManifestid(2330356, "8907182068373780168", 304)
+addappid(2330357, 1, "31db647b6387beb2f8b15ee459b6e1c99c3e935108209edf3c40c0ac327759b1") --Main Macos Language Depot Japanese Baldur's Gate 3
+setManifestid(2330357, "1060371666470234022", 304)
+addappid(2330359, 1, "2BEDE26D81B7FE150B073F04A95733E1377000D00BDC8E08CA8DE665ECF4A082") --Main Linux Depot Baldur's Gate 3
+setManifestid(2330359, "7645484789604960346", 94517984)
+addappid(2378501, 1, "f0f133d646308c77d52f6e88b456946322b6aae5ca5a35ff3ff6130664e37838") --Main Linux Depot Baldur's Gate 3
+setManifestid(2378501, "3713387847141825843", 127825325840)
+addappid(2378503, 1, "DE057F959221009E3A900F7D33A1F53C8CEDC7BAEAB7508ACAB681927E3D81E8") --Main Linux Language Depot English Baldur's Gate 3
+setManifestid(2378503, "1723944792970805542", 304)
+addappid(2378504, 1, "0A420360DD2456B671470E2F185302FF348A4CCF34A09593E2E084A5B9671429") --Main Linux Language Depot French Baldur's Gate 3
+setManifestid(2378504, "6634482063811342712", 14335552)
+addappid(2378505, 1, "371FE64C652717475E28AE5379B4C1B20470FA048CCACED97B2252041F2AA87B") --Main Linux Language Depot German Baldur's Gate 3
+setManifestid(2378505, "2316304865434133652", 12921488)
+addappid(2378506, 1, "d599bf1a2f0ae979c23dc6454de009fd2c030ac365c76c833b7cc485e7b443ae") --Main Linux Language Depot Italian Baldur's Gate 3
+setManifestid(2378506, "1977521336839809758", 13835072)
+addappid(2378507, 1, "1E08BD9184E734F38E2FA9BCC9FEB90D25AD74F0C12691B02FE86FA1219228A5") --Main Linux Language Depot Polish Baldur's Gate 3
+setManifestid(2378507, "8560748397770015641", 14049168)
+addappid(2378508, 1, "B5EADEC11DFC10648ADF712733B76A8C63A1F8BF33FD1EEDCF69326681BC9633") --Main Linux Language Depot Russian Baldur's Gate 3
+setManifestid(2378508, "4852989022982685062", 15377568)
+addappid(2378509, 1, "5881FF4E584A83D684B3854AC059E5889E7448A6FDBF420CB6F23D1F35F4584F") --Main Linux Language Depot Schinese Baldur's Gate 3
+setManifestid(2378509, "4960005248455228591", 21935104)
+addappid(2378511, 1, "DB11319FE425485132079FD1F19DAF3EF691637D3535537E6F6CB5C4F1A4C46B") --Main Linux Language Depot Tchinese Baldur's Gate 3
+setManifestid(2378511, "1528341115338226880", 29185232)
+addappid(2378512, 1, "5B55082029F179A949F8B7A729DD648033CB1450027A7D806AFA9656A9F9BB3A") --Main Linux Language Depot Spanish Baldur's Gate 3
+setManifestid(2378512, "8915217382870164872", 13045040)
+addappid(2378513, 1, "aa77fbcb4e88742e3e6e4c8765aa03cf3c6e2eb04e819771dd5e12b822a97316") --Main Linux Language Depot Turkish Baldur's Gate 3
+setManifestid(2378513, "6533824461881214544", 12272752)
+addappid(2378514, 1, "c29e2661c5dbc9b7b014208fa2eacd775a4b8b360fcf577ada7d38688df0ca41") --Main Linux Language Depot Japanese Baldur's Gate 3
+setManifestid(2378514, "1768558830505910161", 18953408)
+addappid(2378515, 1, "b81aab9f1a0a751bed094fa1bb5a47b3ef796bc61d19381f67be2a22c8fc1f1e") --Main Linux Language Depot Koreana Baldur's Gate 3
+setManifestid(2378515, "8246445589683895301", 18214464)
+addappid(2378516, 1, "74EE8561A8A00645DAA5ECCE5EE5A09CF2415FF664AA00D1FE9CC28279621CDB") --Main Linux Language Depot Ukrainian Baldur's Gate 3
+setManifestid(2378516, "3468707769721696067", 14619664)
+addappid(2378517, 1, "4F5C5A2A9D700FFADD349C67364EC78029C48057194BD5A4A177A40145E0DF41") --Main Linux Language Depot Latam Baldur's Gate 3
+setManifestid(2378517, "6648954009968723469", 12977296)
+addappid(2378518, 1, "D6E9DFC242A95A4CD44BF29DC01EC12B9DBAD4533CDAB9DE47C98F30B8D34C56") --Main Linux Language Depot Brazilian Baldur's Gate 3
+setManifestid(2378518, "2428320560666820462", 12721440)
+--Dlcname Digital Deluxe Edition DLC
+addappid(2378500, 1, "77dfde8ac009c5a510843210ffa1343976be2cc3a303122c9c635fcba725d9cb") --Dlcname Digital Deluxe Edition DLC
+setManifestid(2378500, "6453229909780803137", 2728905872)
+--Dlcname BG3 Toolkit Data
+addappid(2956320) --Dlcname BG3 Toolkit Data
+addappid(2330358, 1, "21915761f183b3b468040d6cc77d6c1e08827d1e3db7aa89c0cf72c6a77a1472") --Dlc Windows Depot BG3 Toolkit Data
+setManifestid(2330358, "7444994150796633975", 1036455408)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2378510) --Dlcname Collector's Edition DLC
