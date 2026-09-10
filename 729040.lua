ec71684822e0841b84c3792105a680aaeccec2d2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:40:28 +0800

    add 729040.lua

--- 729040.lua
+++ 729040.lua
@@ -1,7 +1,15 @@
-addappid(729040)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229002,"7260605429366465749")
-addappid(729041,0,"6bf645f76a001bb548c0f52d99fb5a7f5c7130c9a7ed3d6518318c0bdea77f8d")
-setManifestid(729041,"3930773085502141992")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 729040.lua
+--Gamename Borderlands Game of the Year Enhanced
+addappid(729040, 1, "6b0632c9d06e9d6046f80d98a9e3fd6515d35f11fbba9aa53cdfb01e7fe46034") --Mainappid Borderlands Game of the Year Enhanced
+addappid(729041, 1, "6bf645f76a001bb548c0f52d99fb5a7f5c7130c9a7ed3d6518318c0bdea77f8d") --Main Windows Depot Borderlands Game of the Year Enhanced
+setManifestid(729041, "3930773085502141992", 13818791712)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
