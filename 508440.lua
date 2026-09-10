250583935d5fe833fd7ea392320263472e65d7cf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:54:55 +0800

    add 508440.lua

--- 508440.lua
+++ 508440.lua
@@ -1,5 +1,12 @@
-addappid(508440)
-addappid(508443,0,"6de3434d18886e0ff07614c02781c1f625e189a4a9833e36f4287f77443ce44c")
-setManifestid(508443,"3005476685836115021")
-addappid(508444,0,"c831d684196d21677fc279e01f064dd989c00c2f5c4975b0c58793fd71c1f3f9")
-setManifestid(508444,"6531249309224663181")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 508440.lua
+--Gamename Totally Accurate Battle Simulator
+addappid(508440, 1, "5b7e684bff1e4800cd621ab1dcbddefe6befe0c557601fa6a7dc3170234f6037") --Mainappid Totally Accurate Battle Simulator
+addappid(508442, 1, "1ce344084592b51c732ae49f9ace3a58f5ab718c6e95e3721fbd20a3a0ecf026") --Main Windows Depot Totally Accurate Battle Simulator
+setManifestid(508442, "8242906715970617352", 772821824)
+addappid(508443, 1, "6de3434d18886e0ff07614c02781c1f625e189a4a9833e36f4287f77443ce44c") --Main Windows Depot Totally Accurate Battle Simulator
+setManifestid(508443, "7032160763023751725", 1402201968)
+addappid(508444, 1, "c831d684196d21677fc279e01f064dd989c00c2f5c4975b0c58793fd71c1f3f9") --Main Macos Depot Totally Accurate Battle Simulator
+setManifestid(508444, "1380846934100563863", 1389623344)
+--Missing Dlcs
+--Dlcname Totally Accurate Battle Simulator - BUG DLC
