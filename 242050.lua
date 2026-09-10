14d0052cffeac8b8e32b95f3bd085d9c79afbcff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:46:19 +0800

    add 242050.lua

--- 242050.lua
+++ 242050.lua
@@ -1,13 +1,54 @@
-addappid(242050)
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229004,"5220958916987797232")
-addappid(242051,0,"6b4004c38a5c8c1a869d90c381726c7d5651f578753c316c2dcd372fa13ef609")
-setManifestid(242051,"30755846185611447")
-addappid(260476,0,"c6245eb9d4c77c2f2204ab40754a0d50402bb78f9de10b495e2f9df94d0c051d")
-setManifestid(260476,"3697863006561544635")
-addappid(264803,0,"28fe23f7e6ef14d6190c4b1646a87d9d7aa6d22d7a0f89936abfe3ebd293fd80")
-setManifestid(264803,"6005226780702341878")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 242050.lua
+--Gamename Assassin’s Creed® IV Black Flag™
+addappid(242050, 1, "cafa8e00c75d44d7e693fab3646f3e5803ada60a2dd26534ead5874dccdc4869") --Mainappid Assassin’s Creed® IV Black Flag™
+addappid(242051, 1, "6b4004c38a5c8c1a869d90c381726c7d5651f578753c316c2dcd372fa13ef609") --Main Windows Depot Assassin’s Creed® IV Black Flag™
+setManifestid(242051, "30755846185611447", 23185804256)
+--Dlcname Assassin's Creed Black Flag - Freedom Cry (SP)
+addappid(260476, 1, "c6245eb9d4c77c2f2204ab40754a0d50402bb78f9de10b495e2f9df94d0c051d") --Dlcname Assassin's Creed Black Flag - Freedom Cry (SP)
+setManifestid(260476, "3697863006561544635", 4877131646102)
+--Dlcname AppID 264803
+addtoken(264803, "7841055440138038446")
+addappid(264803, 1, "28fe23f7e6ef14d6190c4b1646a87d9d7aa6d22d7a0f89936abfe3ebd293fd80") --Dlcname AppID 264803
+setManifestid(264803, "6005226780702341878", 2494320141224)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(260470) --Dlcname Assassin's Creed Black Flag - Resources Pack
+addtoken(260470, "18445699559667956713")
+addappid(260471) --Dlcname Assassin's Creed Black Flag - Collectibles Pack
+addappid(260472) --Dlcname Assassin's Creed Black Flag - Activities Pack
+addappid(260473) --Dlcname Assassin's Creed Black Flag - Death Vessel Pack (SP)
+addtoken(260473, "303721168538640280")
+addappid(260474) --Dlcname Assassin's Creed Black Flag - Crusader & Florentine Pack (SP)
+addtoken(260474, "16547570126041288549")
+addappid(260477) --Dlcname Assassin's Creed Black Flag - Illustrious Pirates Pack (SP)
+addappid(260478) --Dlcname Assassin's Creed Black Flag - Guild of Rogues (SP)
+addappid(260950) --Dlcname Assassin's Creed Black Flag - Season Pass
+addtoken(260950, "4004010671453213299")
+addappid(264750) --Dlcname Assassin's Creed Black Flag - Technology Pack
+addappid(264800) --Dlcname Assassin's Creed Black Flag - Death Vessel Pack Activation Key
+addappid(264801) --Dlcname AppID 264801
+addtoken(264801, "7991568246044981123")
+addappid(264802) --Dlcname Assassin's Creed Black Flag - Blackbeard's Wrath Pack Activation Key
+addappid(264804) --Dlcname Assassin's Creed Black Flag - Illustrious Pirates Pack Activation Key
+addappid(264805) --Dlcname Assassin's Creed Black Flag - Guild of Rogues Pack Activation Key
+addappid(264810) --Dlcname AppID 264810
+addtoken(264810, "9041864999908290061")
+addappid(264811) --Dlcname AppID 264811
+addtoken(264811, "308158585488475138")
+addappid(264812) --Dlcname AppID 264812
+addtoken(264812, "3422936916222565870")
+addappid(264813) --Dlcname AppID 264813
+addtoken(264813, "1017937411464545343")
+addappid(265320) --Dlcname AppID 265320
+addtoken(265320, "9693741348087304153")
+addappid(1650420) --Dlcname Assassin’s Creed IV Black Flag - Gold Uplay activation
