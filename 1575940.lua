a9226c458e2500414cf1b576d33c68b551ae9b07
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:44:50 +0800

    add 1575940.lua

--- 1575940.lua
+++ 1575940.lua
@@ -1,9 +1,19 @@
-addappid(1575940)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1575941,0,"4e2e571147e77d0cadda071bc6cb8aea54d2c0e2c076b1dbf4d5fea94482ff09")
-setManifestid(1575941,"4151072988243715910")
-addappid(1575942,0,"cc619e6e9c7f306decb6005a71df177bcb1d9980484676461f25cbf4bcc52e76")
-setManifestid(1575942,"430366403103823297")
-addappid(1575943,0,"8fa4ce7002d306e317d40c1629f5a14edfc0d578ce1a253a52a6cca27e4b35a4")
-setManifestid(1575943,"4029621161901050013")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1575940.lua
+--Gamename Sins of a Solar Empire II
+addappid(1575940, 1, "ff5639159c1feae66a35ad44d9270e06425f6fe30a0cf897d21610997d911f3d") --Mainappid Sins of a Solar Empire II
+addappid(1575941, 1, "4e2e571147e77d0cadda071bc6cb8aea54d2c0e2c076b1dbf4d5fea94482ff09") --Main Windows Depot Sins of a Solar Empire II
+setManifestid(1575941, "7857741933321908940", 14746072048)
+addappid(1575942, 1, "cc619e6e9c7f306decb6005a71df177bcb1d9980484676461f25cbf4bcc52e76") --Main Windows Language Depot Japanese Sins of a Solar Empire II
+setManifestid(1575942, "430366403103823297", 46098752)
+addappid(1575943, 1, "8fa4ce7002d306e317d40c1629f5a14edfc0d578ce1a253a52a6cca27e4b35a4") --Main Windows Depot Sins of a Solar Empire II
+setManifestid(1575943, "5679576359147181550", 78698240)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3195270) --Dlcname Sins of a Solar Empire II - Paths to Power
+addappid(3195280) --Dlcname Sins of a Solar Empire II - Reinforcements
+addappid(3195290) --Dlcname Sins of a Solar Empire II - Times of War
+addappid(3195300) --Dlcname Sins of a Solar Empire II: Harbinger
