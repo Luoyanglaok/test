96e8a3c1905ee96b54e35475fca20bc8a6ad4a8b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:36:48 +0800

    add 513710.lua

--- 513710.lua
+++ 513710.lua
@@ -1,11 +1,38 @@
-addappid(513710)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(513711,0,"a4217026617fd23bbc1b544c3160c7c7e5157bc3cfcd825b8dbd1c6d0513c3b4")
-setManifestid(513711,"364501993041715454")
-addappid(919680,0,"903d006bb149ccbe1ab4f9dbbad34150079380811c9c78b3cfa5c1505ad889ae")
-setManifestid(919680,"6252294094855435400")
-addappid(1643210,0,"9e747e8f5cf60151ff0c48ce9eb2bb1977b88052fbb610f1a0ae88d28d001194")
-setManifestid(1643210,"8526987047835682522")
-addappid(3596770,0,"ca8672e87bb66a5095fa0d3bd263d23d78ee88ee808938b6b0df4c0abbbb0cd2")
-setManifestid(3596770,"4482977239842637557")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 513710.lua
+--Gamename SCUM
+addappid(513710, 1, "490737566db1c843800690df604ab8b78b767a46299e85da9971068c309e5447") --Mainappid SCUM
+addappid(513711, 1, "a4217026617fd23bbc1b544c3160c7c7e5157bc3cfcd825b8dbd1c6d0513c3b4") --Main Windows Depot SCUM
+setManifestid(513711, "6355811951485736134", 33676366640)
+--Dlcname SCUM Deluxe
+addappid(919680, 1, "903d006bb149ccbe1ab4f9dbbad34150079380811c9c78b3cfa5c1505ad889ae") --Dlcname SCUM Deluxe
+setManifestid(919680, "6252294094855435400", 25373104)
+--Dlcname SCUM Deluxe 2
+addappid(1643210, 1, "9e747e8f5cf60151ff0c48ce9eb2bb1977b88052fbb610f1a0ae88d28d001194") --Dlcname SCUM Deluxe 2
+setManifestid(1643210, "8526987047835682522", 94680896)
+--Dlcname SCUM Deluxe 3
+addappid(3596770, 1, "ca8672e87bb66a5095fa0d3bd263d23d78ee88ee808938b6b0df4c0abbbb0cd2") --Dlcname SCUM Deluxe 3
+setManifestid(3596770, "4482977239842637557", 59134144)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1990860) --Dlcname SCUM Danny Trejo
+addappid(2321800) --Dlcname SCUM Hair Deluxe 1
+addappid(2371620) --Dlcname SCUM Luis Moncada
+addappid(2517160) --Dlcname SCUM Charms 1
+addappid(2517170) --Dlcname SCUM Weapon Skins 1
+addappid(2651720) --Dlcname SCUM Car Skins 1
+addappid(2651740) --Dlcname SCUM Man Hair Deluxe 1
+addappid(2651750) --Dlcname SCUM Dances 1
+addappid(2841840) --Dlcname SCUM Raymond Cruz
+addappid(4062860) --Dlcname SCUM Base Building Decor Pack
+addappid(4139450) --Dlcname SCUM Squad Emblem Pack
+addappid(4369750) --Dlcname SCUM Eastern Furniture Pack
+addappid(4529160) --Dlcname SCUM Woodland Hunter Pack
+addappid(4635190) --Dlcname SCUM Woodland Furniture Pack
+addappid(4635200) --Dlcname SCUM Apex Predator Pack
+addappid(4635240) --Dlcname SCUM Specialist Archer Pack
+addappid(4635250) --Dlcname SCUM Specialist Scout Pack
+addappid(5006300) --Dlcname SCUM Stalker Outfit Pack
