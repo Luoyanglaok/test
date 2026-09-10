a22462b89445bc31bdcfaa536df939ec18efed5e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 22:57:47 +0800

    add 1385380.lua

--- 1385380.lua
+++ 1385380.lua
@@ -1,9 +1,28 @@
-addappid(1385380)
-addappid(1385381,0,"212b4c54a7dadad3bb6a4a55088868dde54c128e58d7ce4afab3363c77ba1383")
-setManifestid(1385381,"1746566356481089212")
-addappid(1385382,0,"18b37f64253b8d3e91a2b06f6a1e6446857b10fc0c3ef5dc99fda4e626054e9b")
-setManifestid(1385382,"1941848329367503973")
-addappid(1385383,0,"0671ea1b8cec86cac27391f005068b3758b9c4bf96d10f781893e87127a6b0c0")
-setManifestid(1385383,"1721418848022472513")
-addappid(1385384,0,"5f65b73ec54888e940b46ea6f2de6b59069f8242617e44dde98d89adbf9d67a1")
-setManifestid(1385384,"3997200357442911965")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1385380.lua
+--Gamename Across the Obelisk
+addappid(1385380, 1, "b90c65b51feefc708420cc008f541f887553123113c5c7b0d91c9056e84f2982") --Mainappid Across the Obelisk
+addappid(1385381, 1, "212b4c54a7dadad3bb6a4a55088868dde54c128e58d7ce4afab3363c77ba1383") --Main Windows Depot Across the Obelisk
+setManifestid(1385381, "1675949572694733849", 1687386192)
+addappid(1385382, 1, "18b37f64253b8d3e91a2b06f6a1e6446857b10fc0c3ef5dc99fda4e626054e9b") --Main Windows Depot Across the Obelisk
+setManifestid(1385382, "3857998236208101264", 1684758640)
+addappid(1385383, 1, "0671ea1b8cec86cac27391f005068b3758b9c4bf96d10f781893e87127a6b0c0") --Main Macos Depot Across the Obelisk
+setManifestid(1385383, "4994360581965455576", 1882375248)
+addappid(1385384, 1, "5f65b73ec54888e940b46ea6f2de6b59069f8242617e44dde98d89adbf9d67a1") --Main Linux Depot Across the Obelisk
+setManifestid(1385384, "4103121397594935301", 1686918672)
+addappid(2168960) --Dlcname Across The Obelisk: Spooky Night in Senenthia
+addappid(2325780) --Dlcname Across The Obelisk: The Wolf Wars
+addappid(2511580) --Dlcname Across The Obelisk: Sands of Ulminin
+addappid(2666340) --Dlcname Across the Obelisk: Amelia, the Queen
+addappid(2879680) --Dlcname Across the Obelisk: Nenukil, the Engineer
+addappid(2879690) --Dlcname Across the Obelisk: The Obsidian Uprising
+addappid(3185630) --Dlcname Across the Obelisk: Shores of Sahti
+addappid(3185640) --Dlcname Across the Obelisk: Bernard, the Alchemist
+addappid(3185650) --Dlcname Across the Obelisk: Sigrun, the Valkyrie
+addappid(3473700) --Dlcname Across the Obelisk: Tulah, the Spider Queen
+addappid(3473720) --Dlcname Across the Obelisk: The Sunken Temple
+addappid(3795420) --Dlcname Across the Obelisk - Spiders & Temples
+addappid(3875470) --Dlcname Across the Obelisk: Necropolis Of The Damned
+addappid(3930240) --Dlcname Across the Obelisk: The Shattered Remnant
+addappid(4013420) --Dlcname Across the Obelisk: Golden Season Pack
+addappid(4194030) --Dlcname Across the Obelisk: Frostforged Season Pack
