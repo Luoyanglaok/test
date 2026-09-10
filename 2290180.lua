685492664cf7f3875251acf8fda7b2f4a4120136
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:26:05 +0800

    add 2290180.lua

--- 2290180.lua
+++ 2290180.lua
@@ -1,7 +1,42 @@
-addappid(2290180)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-addappid(2290181,0,"87bb928cf12682614e38b33c1fdbcfe71c6473b5bad0c278583e2478ccf34f36")
-setManifestid(2290181,"1067016915466219233")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2290180.lua
+--Gamename Riders Republic
+addappid(2290180, 1, "f4dc4dfa72e4a27abc58f753127c9574426a2ea4c2daeede7a14338535f6df5d") --Mainappid Riders Republic
+addappid(2290181, 1, "87bb928cf12682614e38b33c1fdbcfe71c6473b5bad0c278583e2478ccf34f36") --Main Windows Depot Riders Republic
+setManifestid(2290181, "5056873641546631185", 31065172848)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(2389390) --Dlcname Riders Republic - Deluxe Edition Ubisoft Activation
+addappid(2389391) --Dlcname Riders Republic - Gold Edition Ubisoft Activation
+addappid(2389392) --Dlcname Riders Republic - Ultimate Edition Ubisoft Activation
+addappid(2389393) --Dlcname AppID 2389393
+addappid(2389394) --Dlcname Riders Republic Season 6 Welcome Pack Ubisoft Activation
+addappid(2400940) --Dlcname Riders Republic Ubisoft Activation
+addappid(2466100) --Dlcname Riders Republic - Year One Pass
+addappid(2466110) --Dlcname Riders Republic - Year One Pass Ubisoft Activation
+addappid(2468280) --Dlcname AppID 2468280
+addappid(2468281) --Dlcname Riders Republic Season 7 Welcome Pack Ubisoft Activation
+addappid(2550510) --Dlcname Riders Republic Season 8 Welcome Pack
+addappid(2550520) --Dlcname Riders Republic Season 8 Welcome Pack Ubisoft Activation
+addappid(2550540) --Dlcname Riders Republic Skate Edition Ubisoft Activation
+addappid(2550550) --Dlcname Riders Republic 360 Edition Ubisoft Activation
+addappid(2550560) --Dlcname Riders Republic Complete Edition Ubisoft Activation
+addappid(2605010) --Dlcname Riders Republic Skate Plus Pack
+addappid(2605020) --Dlcname Riders Republic Skate Plus Pack Ubisoft Activation
+addappid(2735800) --Dlcname AppID 2735800
+addappid(2735810) --Dlcname Riders Republic Season 9 Welcome Pack Ubisoft Activation
+addappid(4508860) --Dlcname Riders Republic Deluxe Edition 2026 - Ubisoft Activation
+addappid(4508870) --Dlcname Riders Republic Premium Edition 2026 - Ubisoft Activation
+addappid(4508890) --Dlcname Riders Republic Careers Pack
+addappid(4508900) --Dlcname Riders Republic Frenzy Pack
+addappid(4508910) --Dlcname Riders Republic X-TREME Board Pack
+addappid(4508920) --Dlcname Riders Republic Motor Pack
+addappid(4508930) --Dlcname Riders Republic - Careers Pack - Ubisoft Activation
+addappid(4508940) --Dlcname Riders Republic - Frenzy Pack - Ubisoft Activation
+addappid(4508950) --Dlcname Riders Republic - Xtreme Pack - Ubisoft Activation
+addappid(4508960) --Dlcname Riders Republic - Motor Pack - Ubisoft Activation
