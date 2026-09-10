0ce89c0601f6584ba2ee0d2cf205c501421cd564
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:44:31 +0800

    add 2413950.lua

---
+++ 2413950.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2413950.lua
+--Gamename Final Sentence
+addappid(2413950) --Mainappid Final Sentence
+addappid(2413951, 1, "3ad7272d45fc2e119edbee37c6ba3717db9e05e3f86ef888f322187368c078e3") --Main Windows Depot Final Sentence
+setManifestid(2413951, "4359476531449613829", 534212800)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4556350) --Dlcname Final Sentence Supporter Pack
