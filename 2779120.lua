af4eba67d8895cdecfcc6aae595df293e70ff30d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:44:47 +0800

    add 2779120.lua

---
+++ 2779120.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2779120.lua
+--Gamename Modulus: Factory Automation
+addappid(2779120, 1, "b370e477fb5f48154bcef1e5a781f1fe72b50960704e9bdbbba283fa0f6dca00") --Mainappid Modulus: Factory Automation
+addappid(2779121, 1, "a31f71a108c3146f0f6b394879d19557153bea224f7db12818aa0dbd5a5c00c1") --Main Windows Depot Modulus: Factory Automation
+setManifestid(2779121, "8204126685010731379", 2069028592)
+--Dlcname Modulus: Digital Artbook
+addappid(4428400, 1, "4ba54950c9c4b0b9a8ea652b6d1c4950fca97f7ea0d224d811f7bf9080866b93") --Dlcname Modulus: Digital Artbook
+setManifestid(4428400, "4729507790709378811", 39295792)
+addappid(4013900) --Dlcname Modulus: Factory Automation Deluxe Upgrade Pack
