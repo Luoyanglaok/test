78b2e95c821fb760e56ffc3020a1e62af28eb991
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:36:43 +0800

    add 1432050.lua

--- 1432050.lua
+++ 1432050.lua
@@ -1,5 +1,12 @@
-addappid(1432050)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1432052,0,"6b92b195e94eda5f196d54c2c8c92900e8acbd81445885e66b5490772ba61a23")
-setManifestid(1432052,"38746912287548060")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1432050.lua
+--Gamename Nobody Saves the World
+addappid(1432050) --Mainappid Nobody Saves the World
+addappid(1432052, 1, "6b92b195e94eda5f196d54c2c8c92900e8acbd81445885e66b5490772ba61a23") --Main Windows Depot Nobody Saves the World
+setManifestid(1432052, "38746912287548060", 887878272)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1951590) --Dlcname Nobody Saves the World - Frozen Hearth
