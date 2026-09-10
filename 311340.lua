f2764834a8c8fcb1ff2b4f56bef34b3435e68811
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:55:17 +0800

    add 311340.lua

--- 311340.lua
+++ 311340.lua
@@ -1,7 +1,15 @@
-addappid(311340)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(311341,0,"e88d146d5d2a23261b9315001e2ab7344adac66e00194985e1f00b847e2e9dbc")
-setManifestid(311341,"3088848911239715671")
-addappid(311343,0,"5498c3c90715a35f8e208cd6b380a16901cae634d9c22cb0f8a1cfed3f70b9cf")
-setManifestid(311343,"1427650686339053933")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 311340.lua
+--Gamename METAL GEAR SOLID V: GROUND ZEROES
+addappid(311340, 1, "5057539fa26099216ef1a32d4f2f486170b9321a8d904a4454694b7abd6e7a93") --Mainappid METAL GEAR SOLID V: GROUND ZEROES
+addappid(311341, 1, "e88d146d5d2a23261b9315001e2ab7344adac66e00194985e1f00b847e2e9dbc") --Main Windows Language Depot English METAL GEAR SOLID V: GROUND ZEROES
+setManifestid(311341, "3088848911239715671", 3009832960)
+addappid(311343, 1, "5498c3c90715a35f8e208cd6b380a16901cae634d9c22cb0f8a1cfed3f70b9cf") --Main Windows Language Depot Japanese METAL GEAR SOLID V: GROUND ZEROES
+setManifestid(311343, "1427650686339053933", 3054274064)
+addappid(311344, 1, "b00a15399f6f765acf2f0f20d06bcfa10766cc6e2cdcfa4ed3e5731fa7cfbb5f") --Main Windows Language Depot English METAL GEAR SOLID V: GROUND ZEROES
+setManifestid(311344, "2543357782612220046", 3009693520)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
