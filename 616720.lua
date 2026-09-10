b418d71765205e5efbca54e634b5826f532a45e0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:16:38 +0800

    add 616720.lua

--- 616720.lua
+++ 616720.lua
@@ -1,9 +1,22 @@
-addappid(616720)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-addappid(616721,0,"34f7b93e924c5d236bba8833fd04d9489e5a55fc76bda6992d5e65eb4cfce6c3")
-setManifestid(616721,"8758180511637625136")
-addappid(616722,0,"9e382fdb223d32adfcb889f34734eb3e9d6097b029872159427c233d04928ff3")
-setManifestid(616722,"5917523308048401619")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 616720.lua
+--Gamename Live2DViewerEX
+addappid(616720, 1, "99529225ccd19a436d52b19894a9a5d24f0559ec79e9384570a58f11d9871727") --Mainappid Live2DViewerEX
+addappid(616721, 1, "34f7b93e924c5d236bba8833fd04d9489e5a55fc76bda6992d5e65eb4cfce6c3") --Main Windows Depot Live2DViewerEX
+setManifestid(616721, "973820133284247421", 450116864)
+addappid(616722, 1, "9e382fdb223d32adfcb889f34734eb3e9d6097b029872159427c233d04928ff3") --Main Macos Depot Live2DViewerEX
+setManifestid(616722, "86255310388151035", 313491504)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(641880) --Dlcname Live2D Wallpaper - [Effect] Rain and Snow
+addappid(642270) --Dlcname Live2D Wallpaper - [Widget] Digital Clock
+addappid(652840) --Dlcname Live2D Wallpaper - [Widget] Analog Clock
+addappid(1732010) --Dlcname Live2DViewerEX - Spine Expansion
+addappid(1350680) --Dlcname AppID 1350680
