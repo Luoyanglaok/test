afafd73fa0d1d751fdb665ee0373608af1c22b97
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 22:05:21 +0800

    add 1310410.lua

--- 1310410.lua
+++ 1310410.lua
@@ -1,13 +1,23 @@
-addappid(1310410)
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-addappid(1310411,0,"2d39fddfd2e106911e6b2163a3bad7f70ba00af107fe1e777f8768c28d16205a")
-setManifestid(1310411,"1599423929991799385")
-addappid(2450980,0,"830385766c9907cd940ae7ed43a699dc2a0b711e2ee4010cb7ee2266083b7980")
-setManifestid(2450980,"4188049365323835729")
-addappid(2450982,0,"863e33498a4c3aeb954f799e797789e5964aa5f8072d77236637bd57c4d914ab")
-setManifestid(2450982,"3337194034610531235")
-addappid(2450981,0,"4727b48ab9d30b5fb95ce96910bfcae1e51964b4415eabbb4d129b15497c77dd")
-setManifestid(2450981,"4074926775292582049")
-addappid(2450983,0,"1e3b4a0692e673e1db221cd86f537a88bff72856f78a90ab2f7b8eb1021c401d")
-setManifestid(2450983,"6929282419867829082")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1310410.lua
+--Gamename Alone in the Dark
+addappid(1310410) --Mainappid Alone in the Dark
+addappid(1310411, 1, "2d39fddfd2e106911e6b2163a3bad7f70ba00af107fe1e777f8768c28d16205a") --Main Windows Depot Alone in the Dark
+setManifestid(1310411, "1599423929991799385", 30457314336)
+--Dlcname Alone in the Dark - Vintage Horror Filter Pack
+addappid(2450980, 1, "830385766c9907cd940ae7ed43a699dc2a0b711e2ee4010cb7ee2266083b7980") --Dlcname Alone in the Dark - Vintage Horror Filter Pack
+setManifestid(2450980, "4188049365323835729", 0)
+--Dlcname Alone in the Dark - Derceto 1992 Costume Pack
+addappid(2450981, 1, "4727b48ab9d30b5fb95ce96910bfcae1e51964b4415eabbb4d129b15497c77dd") --Dlcname Alone in the Dark - Derceto 1992 Costume Pack
+setManifestid(2450981, "4074926775292582049", 0)
+--Dlcname Alone in the Dark - Director's Commentary Mode
+addappid(2450982, 1, "863e33498a4c3aeb954f799e797789e5964aa5f8072d77236637bd57c4d914ab") --Dlcname Alone in the Dark - Director's Commentary Mode
+setManifestid(2450982, "3337194034610531235", 0)
+--Dlcname Alone in the Dark - Digital Artbook
+addappid(2450983, 1, "1e3b4a0692e673e1db221cd86f537a88bff72856f78a90ab2f7b8eb1021c401d") --Dlcname Alone in the Dark - Digital Artbook
+setManifestid(2450983, "6929282419867829082", 19957904)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
