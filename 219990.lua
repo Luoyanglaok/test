ebf3f429f1e14e92d168299031e747a7367649c8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 22:29:07 +0800

    add 219990.lua

--- 219990.lua
+++ 219990.lua
@@ -1,19 +1,42 @@
-addappid(219990)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229003,"8740933542064151477")
-addappid(219991,0,"24ba5f38bc31d02b7075c9464e178e08c9552de08a183056fbf1aa79b405e4c6")
-setManifestid(219991,"4819409569099254837")
-addappid(483840,0,"25fb87a62fad42ff98bde06d9be0995cc02349c65bc47637fc8e713ed6d9d3e1")
-setManifestid(483840,"6040899517575526785")
-addappid(642280,0,"af5387c55c30f622c2cb2aafa8bb289785c75d67ee675b5a2db9a1dc40b0e55d")
-setManifestid(642280,"4954397063195706843")
-addappid(642281,0,"40263ca158fa164ef993b52a491845a7508f4b9a784e0a4b1f6a78fb1109e9f9")
-setManifestid(642281,"3342450113394097507")
-addappid(897670,0,"9e9fdd83057e0d41385fd8bb3461125aecaed7e3ff3cc7890834639322e00e30")
-setManifestid(897670,"8712345113464518732")
-addappid(897671,0,"deefcab56ed487ca3ff25be38f578ead6650ae4e1b1fbe39450cdd9ccd737333")
-setManifestid(897671,"6781241713105194020")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 219990.lua
+--Gamename Grim Dawn
+addappid(219990, 1, "f526be8add966b86be32ef5a4b61c78a5cd1f1621dfcd168368922397abc39cb") --Mainappid Grim Dawn
+addappid(219991, 1, "24ba5f38bc31d02b7075c9464e178e08c9552de08a183056fbf1aa79b405e4c6") --Main Windows Depot Grim Dawn
+setManifestid(219991, "7668750451172825438", 3634046944)
+--Dlcname Grim Dawn - Crucible Mode DLC
+addappid(483840, 1, "25fb87a62fad42ff98bde06d9be0995cc02349c65bc47637fc8e713ed6d9d3e1") --Dlcname Grim Dawn - Crucible Mode DLC
+setManifestid(483840, "2736791586213455031", 103894384)
+--Dlcname Grim Dawn - Ashes of Malmouth Expansion
+addappid(642280, 1, "af5387c55c30f622c2cb2aafa8bb289785c75d67ee675b5a2db9a1dc40b0e55d") --Dlcname Grim Dawn - Ashes of Malmouth Expansion
+setManifestid(642280, "5052231144302612075", 2027164880)
+addappid(642281, 1, "40263ca158fa164ef993b52a491845a7508f4b9a784e0a4b1f6a78fb1109e9f9") --Dlc Windows Depot Grim Dawn - Ashes of Malmouth Expansion
+setManifestid(642281, "1964064799284640879", 12090544)
+--Dlcname Grim Dawn - Forgotten Gods Expansion
+addappid(897670, 1, "9e9fdd83057e0d41385fd8bb3461125aecaed7e3ff3cc7890834639322e00e30") --Dlcname Grim Dawn - Forgotten Gods Expansion
+setManifestid(897670, "4413745635411486049", 2613622256)
+addappid(897671, 1, "deefcab56ed487ca3ff25be38f578ead6650ae4e1b1fbe39450cdd9ccd737333") --Dlc Windows Depot Grim Dawn - Forgotten Gods Expansion
+setManifestid(897671, "7604532180778486959", 2487216)
+--Dlcname Grim Dawn - Fangs of Asterkarn
+addappid(2699230, 1, "4843e2630d30639c182670a20fcd1a15b455f6917c345f7824fc2062fdc46a11") --Dlcname Grim Dawn - Fangs of Asterkarn
+setManifestid(2699230, "1509728266051519769", 4928958864)
+addappid(2699231, 1, "336117c2e4cd9ad52a8bdd10e432814d6ac7e4ffb6ac21fc8ba7872a9e825a7d") --Dlc Windows Depot Grim Dawn - Fangs of Asterkarn
+setManifestid(2699231, "7322348115871238818", 14321712)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229003, "8740933542064151477", 42914928)
+addappid(565610) --Dlcname Grim Dawn - Steam Loyalist Upgrade
+addappid(1088290) --Dlcname Grim Dawn - Steam Loyalist 2 DLC
+addappid(2701090) --Dlcname Grim Dawn - Steam Loyalist 3 DLC
+--Missing Dlcs
+--Dlcname Grim Dawn Soundtrack
