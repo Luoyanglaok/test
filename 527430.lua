3226cb12a53c865db3c55e6b2de143d4c0645aea
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:50:09 +0800

    add 527430.lua

--- 527430.lua
+++ 527430.lua
@@ -1,7 +1,44 @@
-addappid(527430)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(527431,0,"a22368d0a73cc74b4ba8445b12620a5f2662822d0fec23a38cbd5afedfe3d44d")
-setManifestid(527431,"2687237330542327071")
-addappid(527432,0,"e72027c8b7ab04a562b16333a23afd70dd1b1d6151dcadcabe70b7d5b886b34b")
-setManifestid(527432,"6474134563617630190")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 527430.lua
+--Gamename Warhammer 40,000: Inquisitor - Martyr
+addappid(527430) --Mainappid Warhammer 40,000: Inquisitor - Martyr
+addappid(527431, 1, "a22368d0a73cc74b4ba8445b12620a5f2662822d0fec23a38cbd5afedfe3d44d") --Main Windows Depot Warhammer 40,000: Inquisitor - Martyr
+setManifestid(527431, "2687237330542327071", 30448271520)
+addappid(527432, 1, "e72027c8b7ab04a562b16333a23afd70dd1b1d6151dcadcabe70b7d5b886b34b") --Main Windows Depot Warhammer 40,000: Inquisitor - Martyr
+setManifestid(527432, "6474134563617630190", 98652432)
+addappid(878701, 1, "efc39fc0443206cc8f951bae151691e9d27ee895634437d9ad4da5f287bcd6a1") --Dlcname AppID 878701
+setManifestid(878701, "724798824679318308", 429945984)
+addappid(929620, 1, "b5d895763771df60d506587108ed5b1e3cd50addbb84b405792cf1eb41f59dfc") --Dlcname AppID 929620
+setManifestid(929620, "2723965874293278542", 556924176)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(722130) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Seeds of Corruption
+addappid(808140) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Purgation Beacon
+addappid(808150) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Cherub
+addappid(938400) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Mind Plague
+addappid(939050) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Heresy Emote
+addappid(939051) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Poisoned Souls
+addappid(945830) --Dlcname Warhammer 40,000: Inquisitor - Martyr - City of Suffering
+addappid(945831) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Faith Undone
+addappid(945832) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Monotask Servoskull
+addappid(995650) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Occult Siege
+addappid(995651) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Corrosive Footprints
+addappid(995652) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Grim Penance
+addappid(995653) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Servo Commissar-skull
+addappid(995654) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Discordant Choir
+addappid(995670) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Forgotten Arsenal
+addappid(998500) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Grieving Cherub
+addappid(1028890) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Maelstrom of Carnage
+addappid(1028891) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Seal of Inquisition Footprints
+addappid(1028892) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Hollow Bliss
+addappid(1028893) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Reverence Emote
+addappid(1028894) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Desperate Crusade
+addappid(1028895) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Herald Cherub Pet
+addappid(1351990) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Charybdis Outpost
+addappid(2138210) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Sororitas
+addappid(3003800) --Dlcname Warhammer 40,000: Inquisitor - Martyr - Hierophant
+--Missing Dlcs
+--Dlcname Warhammer 40,000: Inquisitor - Martyr - Servo-skull
