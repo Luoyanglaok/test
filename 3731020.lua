5e3382bc38b812a419c437ac509f796dfa6988e5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:30:42 +0800

    add 3731020.lua

---
+++ 3731020.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3731020.lua
+--Gamename Five Hearts Under One Roof season 2
+addappid(3731020, 1, "41a21e3a16c908d66e37893a0c4afc9602871369943d05b4045fbb833fbd9e88") --Mainappid Five Hearts Under One Roof season 2
+addappid(3731021, 1, "6c8a7201c4ca604359838797d37767cef71d5a105fa1c69bd3049c2320646947") --Main Windows Depot Five Hearts Under One Roof season 2
+setManifestid(3731021, "4634443937917573470", 25967548656)
+addappid(3731022, 1, "5d4c8a8e068df7858e0d251b09176989427f7d8a1dea3a51504bc212c6a7bfb8") --Main Macos Depot Five Hearts Under One Roof season 2
+setManifestid(3731022, "7368016707050734069", 25969278160)
+addappid(3731023, 1, "78c4a80de6a91458000fe0446749ac0292901895d5d98c712da2fdfba9d6b3c0") --Main Linux Depot Five Hearts Under One Roof season 2
+setManifestid(3731023, "1957700352921458078", 27573978064)
+--Dlcname Five Hearts Under One Roof season2 - Soundtrack
+addappid(3994370) --Dlcname Five Hearts Under One Roof season2 - Soundtrack
+addappid(3994371, 1, "9af044152f1ae5a909b05c146587e84a822ddfd19acda6264359eee283e0a810") --Dlc Windows Depot Five Hearts Under One Roof season2 - Soundtrack
+setManifestid(3994371, "7584353851468313637", 1012571088)
+--Dlcname Five Hearts Under One Roof season2 Photobook
+addappid(4054090, 1, "a8181a87732e029df7a081f3c88557a7ff70410aa33046867eb281dc34604c48") --Dlcname Five Hearts Under One Roof season2 Photobook
+setManifestid(4054090, "859826515876099767", 539173776)
+addappid(4745180) --Dlcname Five Hearts Under One Roof season2 : Winter Trip DLC
