e3cb64b471ebc07a56ecee8f523dbd672cf0b91a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:53:07 +0800

    add 3934250.lua

---
+++ 3934250.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3934250.lua
+--Gamename Village in the Shade
+addappid(3934250, 1, "f9281487b86ca135f62af617288f2361d498e1fe0e3c787341865caceda33869") --Mainappid Village in the Shade
+addappid(3934251, 1, "75e066e06a1f091ad443715367fae2bbaebe3bfca3404740d369ce7bd0502686") --Main Windows Depot Village in the Shade
+setManifestid(3934251, "4300272778606369373", 5659345392)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
