e32f0a7495c6b8ec19488f8d73fcf827bb0b2dda
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:47:13 +0800

    add 1850570.lua

--- 1850570.lua
+++ 1850570.lua
@@ -1,6 +1,12 @@
-addappid(1850570)
-setManifestid(228988,"6645201662696499616")
-addappid(1850571,0,"f0c67b235585f625ae33882c4547f9ef4404e96c5ad0a1fb4dbf43da61e04e24")
-setManifestid(1850571,"5054031739229969224")
-addappid(1946740,0,"e01735660cdb310784cc00ba796a0b48c48605b9b94cf3e4d473b3a5ae97af32")
-setManifestid(1946740,"8878465371266993543")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1850570.lua
+--Gamename DEATH STRANDING DIRECTOR'S CUT
+addappid(1850570, 1, "e77a76cf5a135d53fe856b43991c87d66be9c7e7319498d6af1143386899832a") --Mainappid DEATH STRANDING DIRECTOR'S CUT
+addappid(1850571, 1, "f0c67b235585f625ae33882c4547f9ef4404e96c5ad0a1fb4dbf43da61e04e24") --Main Windows Depot DEATH STRANDING DIRECTOR'S CUT
+setManifestid(1850571, "5054031739229969224", 80331031456)
+--Dlcname DEATH STRANDING Digital Artbook
+addappid(1946740, 1, "e01735660cdb310784cc00ba796a0b48c48605b9b94cf3e4d473b3a5ae97af32") --Dlcname DEATH STRANDING Digital Artbook
+setManifestid(1946740, "8878465371266993543", 97007440)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
