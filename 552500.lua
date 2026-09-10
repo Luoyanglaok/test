605661c6c12d0b64e0b01ed857482c4e47245016
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:47:28 +0800

    add 552500.lua

--- 552500.lua
+++ 552500.lua
@@ -1,16 +1,67 @@
-addappid(552500)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228987,"4302102680580581867")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229004,"5220958916987797232")
-addappid(552501,0,"a7a75811b296249cb955e09881a66f2d253d083995ab0a749c6448e5ee6beee2")
-setManifestid(552501,"7992702276935981436")
-addappid(552502,0,"dda8511217a15f192cf48148ee19fc6176fd0d432342131bcb39a6187d16bc22")
-setManifestid(552502,"4642656104001527764")
-addappid(552503,0,"dedd3fd14201a662940967a122b5f016d671e484a6b5e916669062f82007426a")
-setManifestid(552503,"4036638607872741551")
-addappid(737040,0,"e0bb967ef64ac01918a6d98f7a1a7967925a7c34455bc35fe9c7e2ef0e9b1840")
-setManifestid(737040,"8568200941940119519")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 552500.lua
+--Gamename Warhammer: Vermintide 2
+addappid(552500, 1, "1826b2a614f6f7cc91260278470651bbf674e69c2cb9a45349d387b89698d61d") --Mainappid Warhammer: Vermintide 2
+addappid(552501, 1, "a7a75811b296249cb955e09881a66f2d253d083995ab0a749c6448e5ee6beee2") --Main Windows Depot Warhammer: Vermintide 2
+setManifestid(552501, "2514070045912892045", 42680014144)
+addappid(552502, 1, "dda8511217a15f192cf48148ee19fc6176fd0d432342131bcb39a6187d16bc22") --Main Windows Depot Warhammer: Vermintide 2
+setManifestid(552502, "4642656104001527764", 62529040)
+addappid(552503, 1, "dedd3fd14201a662940967a122b5f016d671e484a6b5e916669062f82007426a") --Main Windows Depot Warhammer: Vermintide 2
+setManifestid(552503, "386497584549338894", 71492896)
+addappid(552504, 1, "075599f7dea5ccb70915b8a84c44bad6a75be4a75df3675ebb10af2436ddf2a3") --Main Windows Depot Warhammer: Vermintide 2
+setManifestid(552504, "6203603397448415466", 82659696)
+addappid(552505, 1, "247a1e0fb3fb2eda8ea6bf24db6b942b20c55c17077e34c6dbd68c5f368ae2d1") --Main Windows Depot Warhammer: Vermintide 2
+--Dlcname Warhammer: Vermintide 2 - Collector's Edition
+addappid(737040, 1, "e0bb967ef64ac01918a6d98f7a1a7967925a7c34455bc35fe9c7e2ef0e9b1840") --Dlcname Warhammer: Vermintide 2 - Collector's Edition
+setManifestid(737040, "8568200941940119519", 2528584576)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(828790) --Dlcname Warhammer: Vermintide 2 - Shadows Over Bögenhafen
+addappid(975400) --Dlcname Warhammer: Vermintide 2 - Back to Ubersreik
+addappid(1033060) --Dlcname Warhammer: Vermintide 2 - Winds of Magic
+addappid(1196780) --Dlcname AppID 1196780
+addappid(1196781) --Dlcname AppID 1196781
+addappid(1196782) --Dlcname AppID 1196782
+addappid(1196783) --Dlcname AppID 1196783
+addappid(1196784) --Dlcname AppID 1196784
+addappid(1196785) --Dlcname AppID 1196785
+addappid(1196786) --Dlcname AppID 1196786
+addappid(1196787) --Dlcname AppID 1196787
+addappid(1196788) --Dlcname AppID 1196788
+addappid(1196789) --Dlcname AppID 1196789
+addappid(1196790) --Dlcname AppID 1196790
+addappid(1196791) --Dlcname AppID 1196791
+addappid(1196792) --Dlcname AppID 1196792
+addappid(1196793) --Dlcname AppID 1196793
+addappid(1196794) --Dlcname AppID 1196794
+addappid(1282490) --Dlcname AppID 1282490
+addappid(1343500) --Dlcname Warhammer: Vermintide 2 - Grail Knight Career
+addappid(1345990) --Dlcname Warhammer: Vermintide 2 - Grail Knight Cosmetic Upgrade
+addappid(1443780) --Dlcname Warhammer: Vermintide 2 - Outcast Engineer
+addappid(1443790) --Dlcname Warhammer: Vermintide 2 - Outcast Engineer Cosmetic Upgrade
+addappid(1592630) --Dlcname Warhammer: Vermintide 2 - Forgotten Relics Pack
+addappid(1601550) --Dlcname Warhammer: Vermintide 2 - Chaos Wastes
+addappid(1629000) --Dlcname Warhammer: Vermintide 2 - Sister of the Thorn
+addappid(1629010) --Dlcname Warhammer: Vermintide 2 - Sister of the Thorn Cosmetic Upgrade
+addappid(1782450) --Dlcname Warhammer: Vermintide 2 - Warrior Priest Career
+addappid(1782451) --Dlcname Warhammer: Vermintide 2 - Warrior Priest Cosmetic Upgrade
+addappid(2199500) --Dlcname Warhammer: Vermintide 2 - A Treacherous Adventure
+addappid(2445960) --Dlcname Warhammer: Vermintide 2 - Karak Azgaraz
+addappid(2585630) --Dlcname Warhammer: Vermintide 2 - Necromancer Career
+addappid(2585640) --Dlcname Warhammer: Vermintide 2 - Necromancer Cosmetic Upgrade
+addappid(3283440) --Dlcname Warhammer: Vermintide 2 - Verminous Dreams
+addappid(3283450) --Dlcname Warhammer: Vermintide 2 - Versus
+addappid(4040200) --Dlcname Warhammer: Vermintide 2 - Return to the Reik
