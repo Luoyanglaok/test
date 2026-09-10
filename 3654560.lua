e9db2dc6dfe214d024cef7688ed9075003d513df
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:58:07 +0800

    add 3654560.lua

---
+++ 3654560.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3654560.lua
+--Gamename Plants vs. Zombies™: Replanted
+addappid(3654560, 1, "b9481b0f70088fde115847d19925fc7669580cdbfc79c5a5674e924f9894c437") --Mainappid Plants vs. Zombies™: Replanted
+addappid(3654561, 1, "6fc167060f8d59e04b1336aa831531599eba25b5d81a6d659408e1d69b65b456") --Main Windows Depot Plants vs. Zombies™: Replanted
+setManifestid(3654561, "58628256224447820", 1192626496)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3802680) --Dlcname Retro Peashooter Skin Pre-Order Bonus
