0eed188ede60413f5d3d6221c12237d44a5710a8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:04:25 +0800

    add 1113560.lua

--- 1113560.lua
+++ 1113560.lua
@@ -1,3 +1,44 @@
-addappid(1113560)
-addappid(1113561,0,"a8237e96d9b70952bee1f0bc216c2148ba663ef656ca44d6e2057878f4494eb3")
-setManifestid(1113561,"2447286908863323105")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1113560.lua
+--Gamename NieR Replicant™ ver.1.22474487139...
+addappid(1113560, 1, "22f756f649158815c93c2b7cfe8ed95936e7d72ce000a1ec927a39d50c99cd18") --Mainappid NieR Replicant™ ver.1.22474487139...
+addappid(1113561, 1, "a8237e96d9b70952bee1f0bc216c2148ba663ef656ca44d6e2057878f4494eb3") --Main Windows Depot NieR Replicant™ ver.1.22474487139...
+setManifestid(1113561, "2447286908863323105", 19366363120)
+--Dlcname NieR Replicant 4 YoRHa
+addappid(1408390, 1, "30f495faf8482a92d1ec6e2338de1579fa4ee5d2b49f793de32254466c169aa6") --Dlcname NieR Replicant 4 YoRHa
+setManifestid(1408390, "2190712455577141025", 791531152)
+--Dlcname AppID 1565100
+addtoken(1565100, "2840617736922173174")
+addappid(1565100, 1, "de81ee58a951bcd9dbfed04bea0c628e28e688f761aacd2a4866cd885847005b") --Dlcname AppID 1565100
+--Dlcname NieR Replicant ver.1.22474487139... Original Soundtrack
+addappid(3514150) --Dlcname NieR Replicant ver.1.22474487139... Original Soundtrack
+addappid(3514151, 1, "73032d6c163eeb8b15bc1392f932fb263b0640faa0d8e4f009274b26781cbeaa") --Dlc Windows Depot NieR Replicant ver.1.22474487139... Original Soundtrack
+setManifestid(3514151, "3684556428335853003", 416172640)
+--Dlcname NieR Gestalt & Replicant 15 Nightmares & Arrange Tracks
+addappid(3514160) --Dlcname NieR Gestalt & Replicant 15 Nightmares & Arrange Tracks
+addappid(3514161, 1, "f9775a55f8d12399d6dcbe445a7c22dbc594e4dd7f974733a22ada93022e4377") --Dlc Windows Depot NieR Gestalt & Replicant 15 Nightmares & Arrange Tracks
+setManifestid(3514161, "4139157156987135328", 118815872)
+--Dlcname NieR Gestalt & Replicant Orchestral Arrangement Album
+addappid(3514180) --Dlcname NieR Gestalt & Replicant Orchestral Arrangement Album
+addappid(3514181, 1, "375318ff46011229c57c3c645ebd275d0b141372338247c1e93592a566a80836") --Dlc Windows Depot NieR Gestalt & Replicant Orchestral Arrangement Album
+setManifestid(3514181, "8177608886567312240", 111214112)
+--Dlcname NieR Gestalt & Replicant Original Soundtrack
+addappid(3514190) --Dlcname NieR Gestalt & Replicant Original Soundtrack
+addappid(3514191, 1, "76fee7f23c4999c5d0728702e171f48728f1f07067a16c1d992e733d638a79ec") --Dlc Windows Depot NieR Gestalt & Replicant Original Soundtrack
+setManifestid(3514191, "7145350750741863248", 327503152)
+--Dlcname NieR Replicant ver.1.22474487139... Choir Arrangement Album
+addappid(3514250) --Dlcname NieR Replicant ver.1.22474487139... Choir Arrangement Album
+addappid(3514251, 1, "5d8c5ca7c45afce4748401ba8967d97266a08e62a7fbc4da6b4cc03a0a35bd8c") --Dlc Windows Depot NieR Replicant ver.1.22474487139... Choir Arrangement Album
+setManifestid(3514251, "5693149912827660770", 101752848)
+--Dlcname NieR Tribute Album -echo-
+addappid(3514260) --Dlcname NieR Tribute Album -echo-
+addappid(3514261, 1, "ff9ec5bca1241c68e28edb41af3bd20c4fa8048b46a912bd9e32a51ab46a8c53") --Dlc Windows Depot NieR Tribute Album -echo-
+setManifestid(3514261, "852854135828031974", 132976128)
+--Dlcname Piano Collections NieR Gestalt & Replicant
+addappid(3514290) --Dlcname Piano Collections NieR Gestalt & Replicant
+addappid(3514291, 1, "d13260b0102cc44a8ee911a6198987931e97e6c223c7ef75b3900157a6bb7687") --Dlc Windows Depot Piano Collections NieR Gestalt & Replicant
+setManifestid(3514291, "7712689375725388743", 98270032)
+--Dlcname NieR Replicant -10+1 Years-
+addappid(3516770) --Dlcname NieR Replicant -10+1 Years-
+addappid(3516771, 1, "830aa08f123164cdfdc84b83786f696a30dd9d575c4868c82066c69613c7ad07") --Dlc Windows Depot NieR Replicant -10+1 Years-
+setManifestid(3516771, "8333729988532872308", 43855312)
