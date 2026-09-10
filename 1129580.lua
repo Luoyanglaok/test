dc3c00f5cffff2f0e231c8f6ca58e3d4dee62bb5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:53:26 +0800

    add 1129580.lua

--- 1129580.lua
+++ 1129580.lua
@@ -1,13 +1,33 @@
-addappid(1129580)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-addappid(1129581,0,"1046c854c37b8fbffc2463ed255b11644eb113572b68ab4d509a5e09a9f47098")
-setManifestid(1129581,"7509126062782675113")
-addappid(1418980,0,"4725ba16d1f74610293041fa477eedfb35cad844c9c064efb54a7ff031a89e3d")
-setManifestid(1418980,"8707615711707866919")
-addappid(1766020,0,"ca76a7535ac3f0665611290b40894720e2d8ddcfbae6330301f0bf49cef0c64b")
-setManifestid(1766020,"4528843924608308590")
-addappid(1766021,0,"e63723c50d9ecdb7093d2f318c5ce1418817e4fc2148706bba820c94f83de162")
-setManifestid(1766021,"7860095987267006881")
-addappid(1766022,0,"42d607c1515fec47b5c19b04778c4c52bbfe10cac2d0acb459d0f61a02eebacf")
-setManifestid(1766022,"9195790049611468482")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1129580.lua
+--Gamename Medieval Dynasty
+addappid(1129580, 1, "878911f0cb5b2d6d6bf64a1538c3a8445acbf57f498dfd71d1dc045da3a621ab") --Mainappid Medieval Dynasty
+addappid(1129581, 1, "1046c854c37b8fbffc2463ed255b11644eb113572b68ab4d509a5e09a9f47098") --Main Windows Depot Medieval Dynasty
+setManifestid(1129581, "7565454548429046356", 13671716240)
+--Dlcname Medieval Dynasty - Digital Supporter Pack
+addappid(1418980, 1, "4725ba16d1f74610293041fa477eedfb35cad844c9c064efb54a7ff031a89e3d") --Dlcname Medieval Dynasty - Digital Supporter Pack
+setManifestid(1418980, "8707615711707866919", 5443089968)
+--Dlcname Medieval Dynasty - Official Guide
+addappid(1766020, 1, "ca76a7535ac3f0665611290b40894720e2d8ddcfbae6330301f0bf49cef0c64b") --Dlcname Medieval Dynasty - Official Guide
+setManifestid(1766020, "4528843924608308590", 246575408)
+--Dlcname Medieval Dynasty - Official Cookbook
+addappid(1766021, 1, "e63723c50d9ecdb7093d2f318c5ce1418817e4fc2148706bba820c94f83de162") --Dlcname Medieval Dynasty - Official Cookbook
+setManifestid(1766021, "7860095987267006881", 49754768)
+--Dlcname Medieval Dynasty - Map Pack
+addappid(1766022, 1, "42d607c1515fec47b5c19b04778c4c52bbfe10cac2d0acb459d0f61a02eebacf") --Dlcname Medieval Dynasty - Map Pack
+setManifestid(1766022, "9195790049611468482", 130719200)
+--Dlcname Medieval Dynasty - Original Soundtrack Vol. 2
+addappid(3214760) --Dlcname Medieval Dynasty - Original Soundtrack Vol. 2
+addappid(3214761, 1, "eca4676503d291016a05d1eba062320bcb338ccf294d0b8ea55d2fb69c344b10") --Dlc Windows Depot Medieval Dynasty - Original Soundtrack Vol. 2
+setManifestid(3214761, "4750399889280094829", 408185520)
+addappid(3214762, 1, "bb842d206582aea08c09e1e6288567829067db21c049c89f23959a731a9dfa9b") --Dlc Windows Depot Medieval Dynasty - Original Soundtrack Vol. 2
+setManifestid(3214762, "2075981207196233190", 1293418960)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3774330) --Dlcname Medieval Dynasty - Echoes of Nature
+addappid(4159030) --Dlcname Medieval Dynasty - Exquisite Pack
+addappid(4754550) --Dlcname Medieval Dynasty - The Backwood
+addappid(4997500) --Dlcname Medieval Dynasty - Hunting Set
