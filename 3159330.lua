f3edf9d447edee5b6d1e113a6bfdd979579c6c7c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:37:43 +0800

    add 3159330.lua

--- 3159330.lua
+++ 3159330.lua
@@ -1,16 +1,39 @@
-addappid(3159330)
-setManifestid(228989,"3514306556860204959")
-addappid(3159331,0,"a1a2865a35a1aa7255e0d1a8d6cedab23f2b34346e4a359a283f1a5ceb826ddf")
-setManifestid(3159331,"2627174286402456816")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
-addappid(3159333,0,"24bbf4e14d616c7df7c56696e444569bcf5689554a6a79f731426e4fac7d66ea")
-setManifestid(3159333,"2890497842286120105")
-addappid(3159334,0,"919bdf5d6752a3dfc2cf5f76c6800e25059aeab0489c2ec656e1bb938fef0f13")
-setManifestid(3159334,"4089506801826452583")
-addappid(3159335,0,"843ebde3282fd10fe2204126d7be3c471c849e01618fd2419fd56be6a2e4fc72")
-setManifestid(3159335,"7433144042915746485")
-addappid(3159336,0,"10867627410a4ed3756f1263fe75741717e2c6f7797ff9af89cd7c5749a14f2b")
-setManifestid(3159336,"1120211533521827360")
-addappid(3159337,0,"e4e59c05f264ffc35d4c0629f2f887bee3dc7848b240411d65a8f2e2d793843b")
-setManifestid(3159337,"8152455803407100798")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3159330.lua
+--Gamename Assassin’s Creed Shadows
+addappid(3159330, 1, "87c39548810f05cc0cf12c9a6a9ff7ae58bbba17ba6fc67d463491b8381b3772") --Mainappid Assassin’s Creed Shadows
+addappid(3159331, 1, "a1a2865a35a1aa7255e0d1a8d6cedab23f2b34346e4a359a283f1a5ceb826ddf") --Main Windows Depot Assassin’s Creed Shadows
+setManifestid(3159331, "8085519646687009160", 128700694736)
+addappid(3159333, 1, "24bbf4e14d616c7df7c56696e444569bcf5689554a6a79f731426e4fac7d66ea") --Main Windows Language Depot French Assassin’s Creed Shadows
+setManifestid(3159333, "8283686636322166472", 1562758864)
+addappid(3159334, 1, "919bdf5d6752a3dfc2cf5f76c6800e25059aeab0489c2ec656e1bb938fef0f13") --Main Windows Language Depot Italian Assassin’s Creed Shadows
+setManifestid(3159334, "7350092371372350439", 1821487904)
+addappid(3159335, 1, "843ebde3282fd10fe2204126d7be3c471c849e01618fd2419fd56be6a2e4fc72") --Main Windows Language Depot German Assassin’s Creed Shadows
+setManifestid(3159335, "1102932231740758169", 1692604832)
+addappid(3159336, 1, "10867627410a4ed3756f1263fe75741717e2c6f7797ff9af89cd7c5749a14f2b") --Main Windows Language Depot Spanish Assassin’s Creed Shadows
+setManifestid(3159336, "4931157239490777827", 1723970080)
+addappid(3159337, 1, "e4e59c05f264ffc35d4c0629f2f887bee3dc7848b240411d65a8f2e2d793843b") --Main Windows Language Depot Brazilian Assassin’s Creed Shadows
+setManifestid(3159337, "175917902438003276", 1631279136)
+--Dlcname Assassin's Creed Shadows - Claws of Awaji
+addappid(3362550) --Dlcname Assassin's Creed Shadows - Claws of Awaji
+addappid(3159339, 1, "bf213689d9d9dbd0ec6f827e3af29ed5783bb4da152bbefe251a4af6197755f5") --Dlc Windows Depot Assassin's Creed Shadows - Claws of Awaji
+setManifestid(3159339, "740575329311058756", 16525729088)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(3159340) --Dlcname Assassin's Creed Shadows - Ubisoft Activation
+addappid(3362520) --Dlcname Assassin's Creed Shadows - Standard Edition - JP (Ubisoft Activation)
+addappid(3362530) --Dlcname Assassin's Creed Shadows - Digital Deluxe Edition - WW (Ubisoft Activation)
+addappid(3362540) --Dlcname Assassin's Creed Shadows - Digital Deluxe Edition - JP (Ubisoft Activation)
+addappid(3362570) --Dlcname Assassin's Creed Shadows - Premium Starter Pack
+addappid(3362580) --Dlcname Assassin's Creed Shadows - Premium Starter Pack (Ubisoft Activation)
+addappid(3362610) --Dlcname Assassin's Creed Shadows - Standard Edition - WW Prepurchase - Ubisoft Activation
+addappid(3362620) --Dlcname Assassin's Creed Shadows - Digital Deluxe Edition - WW Prepurchase - Ubisoft Activation
+addappid(3362630) --Dlcname Assassin's Creed Shadows - Standard Edition - JP Prepurchase - Ubisoft Activation
+addappid(3362640) --Dlcname Assassin's Creed Shadows - Digital Deluxe Edition - JP Prepurchase - Ubisoft Activation
+addappid(3931580) --Dlcname Assassin's Creed Shadows - Premium Edition - JP (Ubisoft Activation)
+addappid(3931590) --Dlcname Assassin's Creed Shadows - Premium Edition - WW (Ubisoft Activation)
+--Missing Dlcs
+--Dlcname Assassin's Creed Shadows - Claws of Awaji - DLC - (Ubisoft Activation)
