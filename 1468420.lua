66d5760162d389543c315df8ad91695791104c43
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:39:05 +0800

    add 1468420.lua

--- 1468420.lua
+++ 1468420.lua
@@ -1,7 +1,14 @@
-addappid(1468420)
-addappid(1468421,0,"bdec2e35940816de1bd97c8619a27a15349891a71719064348c1c28e9d4e1546")
-setManifestid(1468421,"4778338969000011304")
-addappid(1743292,0,"74158bd2ee7e63a5273e4543fa22fc85d70738835536d74ea9184a80380cf6fb")
-setManifestid(1743292,"3586770671052073684")
-addappid(1743293,0,"9ad516bd9893ea2367d9460f7324a21b506d2d95a0dd46c84478ab14794d1cc1")
-setManifestid(1743293,"390708997453445578")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1468420.lua
+--Gamename Sexcraft - Sofiya and the Lewd Clan
+addappid(1468420) --Mainappid Sexcraft - Sofiya and the Lewd Clan
+addappid(1468421, 1, "bdec2e35940816de1bd97c8619a27a15349891a71719064348c1c28e9d4e1546") --Main Windows Depot Sexcraft - Sofiya and the Lewd Clan
+setManifestid(1468421, "4778338969000011304", 95954976)
+--Dlcname Sexcraft - Walkthrough
+addappid(1743290) --Dlcname Sexcraft - Walkthrough
+addappid(1743292, 1, "74158bd2ee7e63a5273e4543fa22fc85d70738835536d74ea9184a80380cf6fb") --Dlc Windows Depot Sexcraft - Walkthrough
+setManifestid(1743292, "3586770671052073684", 2895840)
+--Dlcname Sexcraft - Adult Art Pack
+addappid(1743291) --Dlcname Sexcraft - Adult Art Pack
+addappid(1743293, 1, "9ad516bd9893ea2367d9460f7324a21b506d2d95a0dd46c84478ab14794d1cc1") --Dlc Windows Depot Sexcraft - Adult Art Pack
+setManifestid(1743293, "390708997453445578", 1669248)
