d33b80ecfa74b3c6ebdaeae4f6a389b49479f045
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:14:48 +0800

    add 2532340.lua

--- 2532340.lua
+++ 2532340.lua
@@ -1,6 +1,18 @@
-addappid(2532340)
-setManifestid(228990,"1829726630299308803")
-addappid(2532341,0,"44781234023af7d32ac61583c9b9b3be400b89530e2a73bc1e6162ebf79a82cd")
-setManifestid(2532341,"2503475685046550490")
-addappid(3312450,0,"e8c80dbbd711db602977b32a384c9fd3b33b8e3dd8c2a593ba5c0f186af63976")
-setManifestid(3312450,"6875649704926279435")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2532340.lua
+--Gamename Unaware in The City
+addappid(2532340, 1, "c79f9339735bc55ddda5ab4f1f57e58df3dabd41808d5e7e416ed7f4fc57291a") --Mainappid Unaware in The City
+addappid(2532341, 1, "44781234023af7d32ac61583c9b9b3be400b89530e2a73bc1e6162ebf79a82cd") --Main Windows Depot Unaware in The City
+setManifestid(2532341, "8620713852197038721", 424051456)
+addappid(2532342, 1, "f80e5801436b03d3f1903a25b292e65415a4965e123a1d3cad81ad4c3a32270a") --Main Macos Depot Unaware in The City
+setManifestid(2532342, "3887534614094448067", 424035232)
+addappid(2532343, 1, "17ce5978e9ef0b5b297118912b9c927005344409da2d2a325bccc06409e2a2d7") --Main Linux Depot Unaware in The City
+setManifestid(2532343, "6220405343081953977", 425951104)
+--Dlcname Unaware in The City - Extended
+addappid(3312450, 1, "e8c80dbbd711db602977b32a384c9fd3b33b8e3dd8c2a593ba5c0f186af63976") --Dlcname Unaware in The City - Extended
+setManifestid(3312450, "3660314346329595459", 424068192)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Unaware in The City Soundtrack
