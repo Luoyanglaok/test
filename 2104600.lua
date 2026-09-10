5e2333fdccb9b4df061163af3c07407a81e7baa8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:02:45 +0800

    add 2104600.lua

--- 2104600.lua
+++ 2104600.lua
@@ -1,3 +1,6 @@
-addappid(2104600)
-addappid(2104601,0,"efa36dee8abb195b8ecb81cf8584f4228aca7d64482487d700f68c437955fa66")
-setManifestid(2104601,"1219495480173382210")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2104600.lua
+--Gamename Sex Adventures - The Pool Party
+addappid(2104600) --Mainappid Sex Adventures - The Pool Party
+addappid(2104601, 1, "efa36dee8abb195b8ecb81cf8584f4228aca7d64482487d700f68c437955fa66") --Main Windows Depot Sex Adventures - The Pool Party
+setManifestid(2104601, "1219495480173382210", 663496320)
