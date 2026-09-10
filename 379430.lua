015db520c47834688cdd975b254e0741aafee09f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:05:09 +0800

    add 379430.lua

--- 379430.lua
+++ 379430.lua
@@ -1,25 +1,45 @@
-addappid(379430)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(379432,0,"108f95cce2562576671a8d1deb159b583397b664cafabd96f622beaae2a38655")
-setManifestid(379432,"426485591477717928")
-addappid(379433,0,"b73a679f54e714c4f1faf2c054cb3acaf90fcdd998e96984742b9f5f20d5325e")
-setManifestid(379433,"5353900087755850263")
-addappid(379431,0,"28025aa521351af4da4b05336ce0cdeb467cf54ab15af771b6e96e777e6e11ef")
-setManifestid(379431,"3231236415084706454")
-addappid(836890,0,"7e4b67dfff0691c120ae20411dd923d92fe00e434fc66529e528c431dcacf0ef")
-setManifestid(836890,"1375049741468394017")
-addappid(836900,0,"d131517918735f795946cf9898da1e5f69df47f9e2f4a6f572f74ab375f3152d")
-setManifestid(836900,"6125976842225591303")
-addappid(836910,0,"fde946d787d448a9ad59c6b49c4c8ed56da692d161669d585a82129d4c74be1e")
-setManifestid(836910,"1961281874941694896")
-addappid(836920,0,"abb26620bd532d9519f5c3359a4995bd7def223ec7ddef791fe0b487ecf6eff3")
-setManifestid(836920,"2096891146739122223")
-addappid(836930,0,"479474ba710c10d7b528343e0471cb4e979ad174ef2fefe437dbc0c0f1da8b43")
-setManifestid(836930,"4537521641197841292")
-addappid(921950,0,"7a9065cf0a6bab53fc81761cd1c3ccdd67e1516403d4a59d0a272de7b0c876ff")
-setManifestid(921950,"3621959063097269656")
-addappid(977420,0,"aafa5c0edc670ba1c9b40669ccbff37939d12a3484df559cda3a4fa671fdf72f")
-setManifestid(977420,"4585083368905346922")
-addappid(1033890,0,"4e8ad98af79c5565ecb6850e4f7a582e875c939630f61980a886f6a2e9168c81")
-setManifestid(1033890,"3972128807530539973")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 379430.lua
+--Gamename Kingdom Come: Deliverance
+addappid(379430, 1, "fdb320571e09066114da93da67a1874b282dc8db0d61419a72b89381894b3363") --Mainappid Kingdom Come: Deliverance
+addappid(379432, 1, "108f95cce2562576671a8d1deb159b583397b664cafabd96f622beaae2a38655") --Main Windows Depot Kingdom Come: Deliverance
+setManifestid(379432, "5694983311934705492", 56003403552)
+addappid(379433, 1, "b73a679f54e714c4f1faf2c054cb3acaf90fcdd998e96984742b9f5f20d5325e") --Main Windows Depot Kingdom Come: Deliverance
+setManifestid(379433, "717100730472011277", 19910688)
+addappid(379436, 1, "5a4593b96f530a9d8280e22d4ec8b80af706a8bfe9a073b0e6fafc1f5e10e2c0") --Main Windows Depot Kingdom Come: Deliverance
+setManifestid(379436, "6811928840428249954", 80)
+--Dlcname Kingdom Come: Deliverance - Artbook
+addappid(801980) --Dlcname Kingdom Come: Deliverance - Artbook
+addappid(379431, 1, "28025aa521351af4da4b05336ce0cdeb467cf54ab15af771b6e96e777e6e11ef") --Dlc Windows Depot Kingdom Come: Deliverance - Artbook
+setManifestid(379431, "3231236415084706454", 123027184)
+--Dlcname Kingdom Come: Deliverance - HD Texture Pack
+addappid(836890, 1, "7e4b67dfff0691c120ae20411dd923d92fe00e434fc66529e528c431dcacf0ef") --Dlcname Kingdom Come: Deliverance - HD Texture Pack
+setManifestid(836890, "1375049741468394017", 7097750192)
+--Dlcname Kingdom Come: Deliverance - HD Sound Pack
+addappid(836900, 1, "d131517918735f795946cf9898da1e5f69df47f9e2f4a6f572f74ab375f3152d") --Dlcname Kingdom Come: Deliverance - HD Sound Pack
+setManifestid(836900, "6319390312926829540", 431195360)
+--Dlcname Kingdom Come: Deliverance - HD Voice Pack - English
+addappid(836910, 1, "fde946d787d448a9ad59c6b49c4c8ed56da692d161669d585a82129d4c74be1e") --Dlcname Kingdom Come: Deliverance - HD Voice Pack - English
+setManifestid(836910, "1961281874941694896", 3644413040)
+--Dlcname Kingdom Come: Deliverance - HD Voice Pack - German
+addappid(836920, 1, "abb26620bd532d9519f5c3359a4995bd7def223ec7ddef791fe0b487ecf6eff3") --Dlcname Kingdom Come: Deliverance - HD Voice Pack - German
+setManifestid(836920, "2096891146739122223", 3772268224)
+--Dlcname Kingdom Come: Deliverance - HD Voice Pack - French
+addappid(836930, 1, "479474ba710c10d7b528343e0471cb4e979ad174ef2fefe437dbc0c0f1da8b43") --Dlcname Kingdom Come: Deliverance - HD Voice Pack - French
+setManifestid(836930, "4537521641197841292", 3689489888)
+--Dlcname Kingdom Come: Deliverance – The Amorous Adventures of Bold Sir Hans Capon
+addappid(921950, 1, "7a9065cf0a6bab53fc81761cd1c3ccdd67e1516403d4a59d0a272de7b0c876ff") --Dlcname Kingdom Come: Deliverance – The Amorous Adventures of Bold Sir Hans Capon
+setManifestid(921950, "1621685670664125200", 433568128)
+--Dlcname Kingdom Come: Deliverance – Band of Bastards
+addappid(977420, 1, "aafa5c0edc670ba1c9b40669ccbff37939d12a3484df559cda3a4fa671fdf72f") --Dlcname Kingdom Come: Deliverance – Band of Bastards
+setManifestid(977420, "2391667617405966423", 333395552)
+--Dlcname Kingdom Come: Deliverance - A Woman's Lot
+addappid(1033890, 1, "4e8ad98af79c5565ecb6850e4f7a582e875c939630f61980a886f6a2e9168c81") --Dlcname Kingdom Come: Deliverance - A Woman's Lot
+setManifestid(1033890, "4286493706647649295", 6265023424)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(768530) --Dlcname Kingdom Come: Deliverance - Treasures of the Past
+addappid(883150) --Dlcname Kingdom Come: Deliverance – From the Ashes
