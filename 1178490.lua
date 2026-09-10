0fee8ecfbdbfddf2abc22babeba52079fa51ff95
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:40:04 +0800

    add 1178490.lua

--- 1178490.lua
+++ 1178490.lua
@@ -1,6 +1,13 @@
-addappid(1178490)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-addappid(1178491,0,"05de198526b71687f617c6a476b9829f9deaff99b590c33599e4cc1ba63ef987")
-setManifestid(1178491,"4256647170631983340")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1178490.lua
+--Gamename 港詭實錄ParanormalHK
+addappid(1178490, 1, "ea57da631125d93336e873e1152ea4f9c07ffc328b37558414253cb7e7ab39a5") --Mainappid 港詭實錄ParanormalHK
+addappid(1178491, 1, "05de198526b71687f617c6a476b9829f9deaff99b590c33599e4cc1ba63ef987") --Main Windows Depot 港詭實錄ParanormalHK
+setManifestid(1178491, "4256647170631983340", 5052276768)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
