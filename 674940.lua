a3a512c90b37ff00deb6d7aba70139f720f6101b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:04:37 +0800

    add 674940.lua

--- 674940.lua
+++ 674940.lua
@@ -1,11 +1,15 @@
-addappid(674940)
-addappid(674941,0,"b6184320732c4df9365dc179616f688a3da2bb9334c80267f54d47e49ab84969")
-setManifestid(674941,"6963720474772919938")
-addappid(726050,0,"3866668b1a85adef60c3470be739a60c4764d0fcbd9fd7f7813b703233f9548e")
-setManifestid(726050,"987934740586805841")
-addappid(674942,0,"0d4f44eb9a21c3d76b3ae3ad2cf2d1445c3132c443d4a63957f6ccf89b6a9cd8")
-setManifestid(674942,"6708735043648025786")
-addappid(674943)
-addappid(674944,0,"4f8739acea8e537672897925fdae67d2e6519846299b9289c0e12c90416c845a")
-setManifestid(674944,"5454866201526278118")
-addappid(674945)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 674940.lua
+--Gamename Stick Fight: The Game
+addappid(674940, 1, "1a031daa727c81e12f3d00c23a4c634f8c79bf1e428c7b3e3879da2138eb1be6") --Mainappid Stick Fight: The Game
+addappid(674941, 1, "b6184320732c4df9365dc179616f688a3da2bb9334c80267f54d47e49ab84969") --Main Windows Depot Stick Fight: The Game
+setManifestid(674941, "5790730271752808752", 91428208)
+addappid(674942, 1, "0d4f44eb9a21c3d76b3ae3ad2cf2d1445c3132c443d4a63957f6ccf89b6a9cd8") --Main Macos Depot Stick Fight: The Game
+setManifestid(674942, "5528644822636694017", 94952320)
+addappid(674944, 1, "4f8739acea8e537672897925fdae67d2e6519846299b9289c0e12c90416c845a") --Main Macos Depot Stick Fight: The Game
+setManifestid(674944, "6080791344802767910", 95598384)
+addappid(674945, 1, "dafa52a6bf95ec2f3a178468d0c54fc9ca22b01a5b562338dbb1b310c4a8d79b") --Main Windows Depot Stick Fight: The Game
+setManifestid(674945, "8864248428190094911", 100743760)
+--Dlcname Stick Fight: The Game OST
+addappid(726050, 1, "3866668b1a85adef60c3470be739a60c4764d0fcbd9fd7f7813b703233f9548e") --Dlcname Stick Fight: The Game OST
+setManifestid(726050, "5969593727816611135", 260589216)
