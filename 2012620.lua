bb082c475c0a7903660df414de759c76aa3476c8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:43:29 +0800

    add 2012620.lua

--- 2012620.lua
+++ 2012620.lua
@@ -1,3 +1,6 @@
-addappid(2012620)
-addappid(2012621,0,"dc24016b37c05f1bbd48f0bd914c86fec41db1b4eac7ae460063801f7bb7c706")
-setManifestid(2012621,"3702653814802826919")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2012620.lua
+--Gamename FURRY CYBERSEX
+addappid(2012620) --Mainappid FURRY CYBERSEX
+addappid(2012621, 1, "dc24016b37c05f1bbd48f0bd914c86fec41db1b4eac7ae460063801f7bb7c706") --Main Windows Depot FURRY CYBERSEX
+setManifestid(2012621, "3702653814802826919", 92282320)
