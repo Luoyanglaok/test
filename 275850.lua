330af4a2d2284802cdf3696411dce5abda6037a4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:09:00 +0800

    add 275850.lua

--- 275850.lua
+++ 275850.lua
@@ -1,8 +1,18 @@
-addappid(275850)
-addappid(228983,1,"77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b")
-addappid(228985,1,"21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c")
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-setManifestid(275851,"1377104383665151456")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228989,"5753583882400741046")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 275850.lua
+--Gamename No Man's Sky
+addappid(275850, 1, "bb6b75fde9bed39524f38b15f4421448e580935048bc0105a7943f893e20974a") --Mainappid No Man's Sky
+addappid(275851, 1, "a5ce5217e5d5d20115ea81209764d4e89f37bed55b84690951d5df23e73852a6") --Main Windows Depot No Man's Sky
+setManifestid(275851, "1377104383665151456", 32157341424)
+addappid(275852, 1, "1f8fb10eb2401484d32af8e6c0d3b7e4148e78e4cf8f632afe1ab6600fb7e57d") --Main Macos Depot No Man's Sky
+setManifestid(275852, "7554462894173576070", 29024893968)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(513130) --Dlcname AppID 513130
+addtoken(513130, "3901477932064680203")
+addappid(3380990) --Dlcname AppID 3380990
