be1986143b0c42f41556d5fe12cf4ced4b0a14a2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:58:59 +0800

    add 1659040.lua

--- 1659040.lua
+++ 1659040.lua
@@ -1,3 +1,54 @@
-addappid(1659040)
-addappid(1659041,0,"891bd9670c03fa74065659fe448680b55b5279707dfe40493d5f00372e02b42f")
-setManifestid(1659041,"6711704740148543453")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1659040.lua
+--Gamename HITMAN World of Assassination
+addappid(1659040, 1, "5390a1942e4032c44876f6896fa8169bff61161a13f379433575053c4fe38c02") --Mainappid HITMAN World of Assassination
+addappid(1659041, 1, "891bd9670c03fa74065659fe448680b55b5279707dfe40493d5f00372e02b42f") --Main Windows Depot HITMAN World of Assassination
+setManifestid(1659041, "5408453574472182733", 74869444464)
+addappid(1659042, 1, "a4a03eaeb99e5848f2074a8902ce896268489ec313300285a521885c807e263c") --Main Macos Depot HITMAN World of Assassination
+setManifestid(1659042, "6944284808996353456", 66295702048)
+addappid(1829587, 1, "7f139e3a632fe7e782856995965d4b28185d6ef5fd190966386f65c31accf0ad") --Main Windows Depot HITMAN World of Assassination
+addappid(1829590, 1, "99a8ea96a1d00ee787cd4f481782f717833343c132908a8328e78fcd5102e50e") --Main Windows Depot HITMAN World of Assassination
+addappid(2184790, 1, "e967cde3a53c43dd7dfcc769cd0162719e5b054b9a7729e0854002a3f740e91c") --Main Windows Depot HITMAN World of Assassination
+addappid(2184791, 1, "7691f8610f6da192dbefc32af82e092c47b32fc36dc9fd885329b06e35140ad9") --Main Windows Depot HITMAN World of Assassination
+addappid(2475260, 1, "ea115bccc3c6c1d2e926f2d2cd17b64b42e86b57a5ad0a963f38f806e7633524") --Main Windows Depot HITMAN World of Assassination
+addappid(2828470, 1, "9c5066dc34ee90448a8c7cef18ffed03001c6804637c78b6b8ae512423ba8a3e") --Main Windows Depot HITMAN World of Assassination
+addappid(3110360, 1, "edac69b8f5df91b5d10ce1c8f1516328fd8273dd568aa3c157504dba5215345d") --Main Windows Depot HITMAN World of Assassination
+addappid(3254350, 1, "8e66ebec2afe768640cdbc679b4819214ba3864bd84e181cdad887c2236f7573") --Main Windows Depot HITMAN World of Assassination
+addappid(4328240, 1, "8f3b52abd940a6157aad14fa2f94aa57d11c2cfa63a0ee87746ffbb2e6080dab") --Main Windows Depot HITMAN World of Assassination
+--Dlcname HITMAN 3 - Seven Deadly Sins Collection
+--Dlcname HITMAN 3 Access Pass: HITMAN 2 Expansion
+--Dlcname HITMAN WOA - Street Art Pack
+--Dlcname HITMAN WOA - Makeshift Pack
+--Dlcname HITMAN WOA - Sarajevo Six Campaign Pack
+--Dlcname HITMAN WOA - The Undying Pack
+--Dlcname HITMAN WOA - The Drop Pack
+--Dlcname HITMAN WOA - The Splitter Pack
+--Dlcname HITMAN WOA - Patient Zero Requiem Pack
+addappid(1829580) --Dlcname HITMAN 3 - Seven Deadly Sins Act 1: Greed
+addappid(1829581) --Dlcname HITMAN 3 - Seven Deadly Sins Act 2: Pride
+addappid(1829582) --Dlcname HITMAN 3 - Seven Deadly Sins Act 3: Sloth
+addappid(1829583) --Dlcname HITMAN 3 - Seven Deadly Sins Act 4: Lust
+addappid(1829584) --Dlcname HITMAN 3 - Seven Deadly Sins Act 5: Gluttony
+addappid(1829585) --Dlcname HITMAN 3 - Seven Deadly Sins Act 6: Envy
+addappid(1829586) --Dlcname HITMAN 3 - Seven Deadly Sins Act 7: Wrath
+addappid(1829591) --Dlcname HITMAN 3 - Deluxe Pack
+addappid(1829592) --Dlcname HITMAN 3 Access Pass: HITMAN 2 Standard
+addappid(1829593) --Dlcname HITMAN 3 Access Pass: HITMAN 1 Complete First Season
+addappid(1829594) --Dlcname HITMAN WOA - VR Access
+addappid(1829595) --Dlcname HITMAN 3 Access Pass: HITMAN 1 GOTY Upgrade
+addappid(1829596) --Dlcname HITMAN WOA - Trinity Pack
+addappid(1829600) --Dlcname HITMAN 3 - Carpathian Mountains
+addappid(1829601) --Dlcname HITMAN 3 - Mendoza
+addappid(1829602) --Dlcname HITMAN 3 - Chongqing
+addappid(1829603) --Dlcname HITMAN 3 - Berlin
+addappid(1829604) --Dlcname HITMAN 3 - Dartmoor
+addappid(1829605) --Dlcname HITMAN 3 - Dubai
+addappid(1843460) --Dlcname HITMAN 3 Access Pass: HITMAN 1 GOTY Edition
+addappid(2973650) --Dlcname HITMAN 3 - The Disruptor Pack
+addappid(3711140) --Dlcname HITMAN WOA - The Banker Pack
+addappid(3957470) --Dlcname HITMAN WOA - The Bruce Lee Pack
+addappid(4097630) --Dlcname HITMAN WOA - The Eminem vs. Slim Shady Pack
+addappid(4542910) --Dlcname HITMAN WOA - World Champions Pack
+addappid(4621250) --Dlcname HITMAN WOA - The Wizard Pack
+addappid(4911210) --Dlcname HITMAN WOA - The Herbalist Pack
+addappid(4944070) --Dlcname HITMAN WOA - The Getaway Pack
