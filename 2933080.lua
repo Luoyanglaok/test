9fcd463fc1fe1dfda98adbd4a386dd8fda67b98d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:27:14 +0800

    add 2933080.lua

--- 2933080.lua
+++ 2933080.lua
@@ -1,8 +1,27 @@
-addappid(2933080)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229006,"1784011429307107530")
-addappid(2933081,0,"c19e67580e16bb198a0208353b4ae25e66eee4cb16421cc24f593925c2cd41ec")
-setManifestid(2933081,"199567002226581427")
-addappid(2933083,0,"33064194c736b017c0d6d0a4b5c01816f9f5153631464c677b5dd445ff27e374")
-setManifestid(2933083,"9091999647342646565")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2933080.lua
+--Gamename Crime Boss: Rockay City
+addappid(2933080, 1, "787c8ee112c3831358b74842deca936b0ab2e378420a5bd5ef3a03b2dba2f901") --Mainappid Crime Boss: Rockay City
+addappid(2933081, 1, "c19e67580e16bb198a0208353b4ae25e66eee4cb16421cc24f593925c2cd41ec") --Main Windows Depot Crime Boss: Rockay City
+setManifestid(2933081, "4047762858265146131", 89952890464)
+addappid(2933083, 1, "33064194c736b017c0d6d0a4b5c01816f9f5153631464c677b5dd445ff27e374") --Main Windows Depot Crime Boss: Rockay City
+setManifestid(2933083, "5624905045649714780", 0)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(2934440) --Dlcname Crime Boss: Rockay City - Tactical Weapon Pack
+addappid(2934450) --Dlcname Crime Boss: Rockay City - Dragon's Gold Cup
+addappid(2934460) --Dlcname Crime Boss: Rockay City - Heavy Hitters Pack
+addappid(2939860) --Dlcname Crime Boss: Rockay City - Cagnali's Order
+addappid(3115300) --Dlcname Crime Boss: Rockay City - Baker's Bundle
+addappid(3245230) --Dlcname Crime Boss: Rockay City - Jupiter Character Pack
+addappid(3337580) --Dlcname Crime Boss: Rockay City - Cracker Character Pack
+addappid(3634240) --Dlcname Crime Boss: Rockay City - Hielo Character Pack
+addappid(3787350) --Dlcname Crime Boss: Rockay City - Wiz Character Pack
+addappid(3916000) --Dlcname Crime Boss: Rockay City - Smoke Character Pack
+addappid(4053730) --Dlcname Crime Boss: Rockay City - Shaman Chase Character Pack
+addappid(4391860) --Dlcname Crime Boss: Rockay City - Auction House
