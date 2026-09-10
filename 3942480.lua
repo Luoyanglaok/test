7d29fbef822ff7fbd746cf397a0cfe9fe06b7835
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:01:19 +0800

    add 3942480.lua

---
+++ 3942480.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3942480.lua
+--Gamename The NOexistenceN of Morphean Paradox : The Forest of Silver Shallots
+addappid(3942480, 1, "4511582c8d63d9fd3dae1f19a4450e9deee0c7743555cea761ada30d202d0193") --Mainappid The NOexistenceN of Morphean Paradox : The Forest of Silver Shallots
+addappid(3942481, 1, "b45fb23c7cd70e0b1489d1cb6218a78ce90fb901c8c25141e7037dfbd0f73edf") --Main Windows Depot The NOexistenceN of Morphean Paradox : The Forest of Silver Shallots
+setManifestid(3942481, "8611057924587875372", 1139743344)
