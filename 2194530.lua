9f41a8cf84b1c2af91e50eca62d4bdd5c5051c13
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:00:36 +0800

    add 2194530.lua

--- 2194530.lua
+++ 2194530.lua
@@ -1,4 +1,13 @@
-addappid(2194530)
-addappid(2194531,0,"6e4d968905352d244ba2c7560e36a41444e533a61d2cc9d5fcd571eb62621e63")
-setManifestid(2194531,"4349817306687406736")
-setManifestid(2699860,"9126834133194351588")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2194530.lua
+--Gamename Yog-Sothoth’s Yard
+addappid(2194530, 1, "347b510a66d577bdcddf9e93841c8a15f63bd43dbef3391917c1604a22bca187") --Mainappid Yog-Sothoth’s Yard
+addappid(2194531, 1, "6e4d968905352d244ba2c7560e36a41444e533a61d2cc9d5fcd571eb62621e63") --Main Windows Depot Yog-Sothoth’s Yard
+setManifestid(2194531, "6858613814422579269", 600055360)
+addappid(2194532, 1, "b8785c0c90a8021385fbd748d26715e618b69c29c5ff88e3350fc06e9410c40c") --Main Windows Depot Yog-Sothoth’s Yard
+addappid(2194533, 1, "4CF8FA27963F538A756AE0F4F2711AF5D36CDB5709AC08434D502809EF3A21FD") --Main Windows Depot Yog-Sothoth’s Yard
+--Dlcname Yog-Sothoth's Yard - Official Picture Package
+addappid(2699860, 1, "55bb7dd855c944ff13e73fa28ad5b13afed5f181be237051938200102ab88985") --Dlcname Yog-Sothoth's Yard - Official Picture Package
+setManifestid(2699860, "9126834133194351588", 51294640)
+addappid(2699861, 1, "dc1387bdccecdee9d31a56f610f064b897eb03cabae90dccc9f14674211bd8fc") --Dlc Windows Depot Yog-Sothoth's Yard - Official Picture Package
+setManifestid(2699861, "8231958956662741069", 51294640)
