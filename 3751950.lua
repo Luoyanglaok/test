041c29042f02a9f2d9af4135f3dee4f48e74fcf9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:40:26 +0800

    add 3751950.lua

--- 3751950.lua
+++ 3751950.lua
@@ -1,15 +1,49 @@
-addappid(3751950)
-addappid(4496490)
-addappid(4496510)
-addappid(4496530)
-addappid(4496550)
-addappid(4496580)
-addappid(4496600)
-addappid(4496620)
-addappid(4496640)
-addappid(4496660)
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-setManifestid(3751951,"6244593628330629913")
-setManifestid(1716751,"8936186649444731600")
-setManifestid(3751959,"6456623308878089663")
-setManifestid(228989,"5753583882400741046")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3751950.lua
+--Gamename Assassin's Creed Black Flag Resynced
+addappid(3751950, 1, "e6d96386c77349411f5be39f2955ee0649a9a80abd22c3d69752bfc6e3302539") --Mainappid Assassin's Creed Black Flag Resynced
+addappid(3751951, 1, "0495628add2f29892c7a7930e1ed5332f68e374d7621e3338f23492fcd8f23db") --Main Windows Depot Assassin's Creed Black Flag Resynced
+setManifestid(3751951, "5857658919173276733", 56277348336)
+addappid(3751953, 1, "f96dc68fd865006d30d7d804bab42593f8bb6a5250624df45771cbc2a9c7aa45") --Main Windows Language Depot French Assassin's Creed Black Flag Resynced
+setManifestid(3751953, "3022415893432196011", 232286352)
+addappid(3751954, 1, "b61e5b022604f5f47312b82b8489487c5b1dc57159c8ee09ecac0c7959631762") --Main Windows Language Depot Italian Assassin's Creed Black Flag Resynced
+setManifestid(3751954, "6892818699399123643", 243613152)
+addappid(3751955, 1, "f2ae043d5d7111cfd549ed1bd3f0611cd26711aa4a55e1af56889e43e8d0fb1c") --Main Windows Language Depot German Assassin's Creed Black Flag Resynced
+setManifestid(3751955, "4309823939662694323", 240346368)
+addappid(3751956, 1, "c6c9dba85bf9b4a6f19551cc87b047ab631cd88317027eb33e30174e4bbe41fc") --Main Windows Language Depot Spanish Assassin's Creed Black Flag Resynced
+setManifestid(3751956, "6870137386025150758", 236988112)
+addappid(3751957, 1, "c98ba5f83bc7269be83bedef8689d8a68ff2bb4ad1d6f7ba221178305af557c4") --Main Windows Language Depot Brazilian Assassin's Creed Black Flag Resynced
+setManifestid(3751957, "4086000294114352953", 237528848)
+addappid(3751958, 1, "17b0d6cff686c37972334878f48648f8982f40f290fdaeddc022e1a795e6452c") --Main Windows Language Depot Japanese Assassin's Creed Black Flag Resynced
+setManifestid(3751958, "5424793249443767029", 237982368)
+addappid(3751959, 1, "6011c824af6fe4db62d3154d751de76d49422e4a56218d54d8ceb716cac7fa6c") --Main Windows Language Depot Schinese Assassin's Creed Black Flag Resynced
+setManifestid(3751959, "6456623308878089663", 247051200)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(4496490) --Dlcname Assassin's Creed Black Flag Resynced - Master Assassin Character Pack
+addappid(4496500) --Dlcname Assassin's Creed Black Flag Resynced - Master Assassin Character Pack - Ubisoft Activation
+addappid(4496510) --Dlcname Assassin's Creed Black Flag Resynced - Master Assassin Naval Pack
+addappid(4496520) --Dlcname Assassin's Creed Black Flag Resynced - Master Assassin Naval Pack - Ubisoft Activation
+addappid(4496530) --Dlcname Assassin's Creed Black Flag Resynced - Hellfire Character Pack
+addappid(4496540) --Dlcname Assassin's Creed Black Flag Resynced - Hellfire Character Pack - Ubisoft Activation
+addappid(4496550) --Dlcname Assassin's Creed Black Flag Resynced - Hellfire Naval Pack
+addappid(4496560) --Dlcname Assassin's Creed Black Flag Resynced - Hellfire Naval Pack - Ubisoft Activation
+addappid(4496580) --Dlcname Assassin's Creed Black Flag Resynced - Sea Serpent Character Pack
+addappid(4496590) --Dlcname Assassin's Creed Black Flag Resynced - Sea Serpent Character Pack - Ubisoft Activation
+addappid(4496600) --Dlcname Assassin's Creed Black Flag Resynced - Sea Serpent Naval Pack
+addappid(4496610) --Dlcname Assassin's Creed Black Flag Resynced - Sea Serpent Naval Pack - Ubisoft Activation
+addappid(4496620) --Dlcname Assassin's Creed Black Flag Resynced - Dragon Storm Character Pack
+addappid(4496630) --Dlcname Assassin's Creed Black Flag Resynced - Dragon Storm Character Pack - Ubisoft Activation
+addappid(4496640) --Dlcname Assassin's Creed Black Flag Resynced - Dragon Storm Naval Pack
+addappid(4496650) --Dlcname Assassin's Creed Black Flag Resynced - Dragon Storm Naval Pack - Ubisoft Activation
+addappid(4496660) --Dlcname Assassin's Creed Black Flag Resynced - Map Pack
+addappid(4496670) --Dlcname Assassin's Creed Black Flag Resynced - MAP PACK - Ubisoft Activation
+addappid(4496720) --Dlcname Assassin's Creed Black Flag Resynced - Standard Edition - Ubisoft Activation
+addappid(4496730) --Dlcname Assassin's Creed Black Flag Resynced - Deluxe Edition - Ubisoft Activation
+addappid(4519940) --Dlcname Assassin's Creed Black Flag Resynced - Standard Edition - Prepurchase - Ubisoft Activation
+addappid(4519950) --Dlcname Assassin's Creed Black Flag Resynced - Deluxe Edition - Prepurchase - Ubisoft Activation
+addappid(4872930) --Dlcname Assassin's Creed Black Flag Resynced - Standard Edition - PREVIEW - Ubisoft Activation
+addappid(4892480) --Dlcname Assassin's Creed Black Flag Resynced - Deluxe Edition - PREVIEW - Ubisoft Activation
