853eea087557e839cd99256c002ea702886e1299
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:44:25 +0800

    add 1004750.lua

--- 1004750.lua
+++ 1004750.lua
@@ -1,13 +1,22 @@
-addappid(1004750)
-addappid(228983)
-setManifestid(228983,"8124929965194586177")
-addappid(228984)
-setManifestid(228984,"2547553897526095397")
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(228987)
-setManifestid(228987,"4302102680580581867")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1004751,0,"1634a0db5e059e9e0eb5c72533baa526a769d66fda7602f5c7cb893adf3ef935")
-setManifestid(1004751,"1343133403813617173")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1004750.lua
+--Gamename WRC 8 FIA World Rally Championship
+addappid(1004750) --Mainappid WRC 8 FIA World Rally Championship
+addappid(1004751, 1, "1634a0db5e059e9e0eb5c72533baa526a769d66fda7602f5c7cb893adf3ef935") --Main Windows Depot WRC 8 FIA World Rally Championship
+setManifestid(1004751, "1343133403813617173", 18935510912)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1101810) --Dlcname WRC 8 - RWD Legends
+addappid(1101830) --Dlcname WRC 8 - Alpine A110 (1973)
+addappid(1101870) --Dlcname WRC 8 - Ford Escort MkII 1800 (1979)
+addappid(1101880) --Dlcname WRC 8 - Lancia Delta HF Integrale Evoluzione (1992)
+addappid(1101890) --Dlcname WRC 8 - Senior Staff Members Unlock
