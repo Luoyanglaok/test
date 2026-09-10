025efac89843bb3272c6b65c92f0d317f6651352
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:07:00 +0800

    add 2285550.lua

--- 2285550.lua
+++ 2285550.lua
@@ -1,4 +1,15 @@
-addappid(2285550)
-addtoken(2285550,"11000334821660453025")
-addappid(2285551,0,"afe8a5179c5f8472cac3ca41c6b35eab7bb08d22b24bb4c034c74a1a02bd8d12")
-setManifestid(2285551,"88553667655263082")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2285550.lua
+--Gamename Doloc Town
+addappid(2285550, 1, "774a2b5b30c583e2b7d626e70586aa34eee95173f317e39add0c569687a1cf97") --Mainappid Doloc Town
+addtoken(2285550, "11000334821660453025")
+addappid(2285551, 1, "afe8a5179c5f8472cac3ca41c6b35eab7bb08d22b24bb4c034c74a1a02bd8d12") --Main Windows Depot Doloc Town
+addappid(3617061, 1, "94747ab93c00b6844e40f510421ea87cc0883d964b2518756d951f003c986f8b") --Main Windows Depot Doloc Town
+setManifestid(3617061, "928537901197974004", 144730736)
+addappid(3617062, 1, "07c7fe42fd628520c795d546bee914496d40ecffbbe6ec753a4fa70968f2206a") --Main Windows Depot Doloc Town
+setManifestid(3617062, "6247867880497680031", 1363313440)
+--Dlcname Doloc Town Soundtrack
+addtoken(3617060, "15110001161586445678")
+addappid(3617060) --Dlcname Doloc Town Soundtrack
+--Dlcname Doloc Town - Digital Artbook
+addappid(4864710, 1, "c34fa204d78f384e6f7ec57a29104b92733a43a34859328efd6163ac77e8b4e8") --Dlcname Doloc Town - Digital Artbook
