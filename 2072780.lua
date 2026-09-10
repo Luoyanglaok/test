abd2feae869f6fd6ad2b958f56bdfd2e1f109434
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:01:11 +0800

    add 2072780.lua

--- 2072780.lua
+++ 2072780.lua
@@ -1,3 +1,6 @@
-addappid(2072780)
-addappid(2072781,0,"0239e4055c121e361f95e06ec942425dcc9c9943a2bc11f483ac8c0c0fa7d132")
-setManifestid(2072781,"3788355963962780298")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2072780.lua
+--Gamename Sex Adventures - The Office Slut
+addappid(2072780) --Mainappid Sex Adventures - The Office Slut
+addappid(2072781, 1, "0239e4055c121e361f95e06ec942425dcc9c9943a2bc11f483ac8c0c0fa7d132") --Main Windows Depot Sex Adventures - The Office Slut
+setManifestid(2072781, "3788355963962780298", 813248048)
