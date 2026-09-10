da6c4a52984389d701bfc223e891f59b898fa016
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:18:02 +0800

    add 1326470.lua

--- 1326470.lua
+++ 1326470.lua
@@ -1,3 +1,6 @@
-addappid(1326470)
-addappid(1326471,0,"dcc8b76a7563c836c1aed54a46abf85e7fec5fccabc0dd8e176d2151173e508e")
-setManifestid(1326471,"2631942613378601299")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1326470.lua
+--Gamename Sons Of The Forest
+addappid(1326470, 1, "c553d89fa378bd6b5f4a82e9f61b9b085561e8481150c7ab1f2f5744af1d018c") --Mainappid Sons Of The Forest
+addappid(1326471, 1, "dcc8b76a7563c836c1aed54a46abf85e7fec5fccabc0dd8e176d2151173e508e") --Main Windows Depot Sons Of The Forest
+setManifestid(1326471, "8162016777027728069", 10569505744)
