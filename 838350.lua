9a6d3fd51e401db390f76c45b381e414ea2f3bc0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:10:18 +0800

    add 838350.lua

--- 838350.lua
+++ 838350.lua
@@ -1,24 +1,49 @@
-addappid(838350)
-addappid(4528730)
-addappid(4834440)
-addappid(4834450)
-addappid(4395170)
-addappid(3464590)
-addappid(946970)
-addappid(2819320)
-addappid(2764950)
-addappid(2764960)
-addappid(2241120)
-addappid(2172690)
-setManifestid(838351,"5390397077101914833")
-setManifestid(946970,"2089095199798162434")
-setManifestid(4834450,"9070015990007851062")
-setManifestid(2172690,"5595148215465884713")
-setManifestid(2241120,"8929325035393384592")
-setManifestid(2764950,"6279348019110604381")
-setManifestid(2764960,"4096172026402743863")
-setManifestid(3464590,"2170511017358734763")
-setManifestid(4395171,"6990289780772373997")
-setManifestid(4528730,"4456568609758365763")
-setManifestid(4834440,"6304815620934302627")
-setManifestid(228988,"6645201662696499616")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 838350.lua
+--Gamename The Scroll of Taiwu : Beyond The Dome
+addappid(838350, 1, "bf086ed374abbdc30220a8965af14256dd7f48cb48cfe8f47d4f1eaedc79fd2e") --Mainappid The Scroll of Taiwu : Beyond The Dome
+addtoken(838350, "15146427787086570964")
+addappid(838351, 1, "7de3577779d63fd52db715a1898f8353eb0f5f4096ab3391f77b582621e1adb8") --Main Windows Depot The Scroll of Taiwu : Beyond The Dome
+setManifestid(838351, "5390397077101914833", 15161125952)
+--Dlcname Scroll Of Taiwu - OST
+addtoken(946970, "5933735890991512877")
+addappid(946970, 1, "6eb479723fd905892e55add18fe531a67b7e3c42595302faa5685e47fd7603c3") --Dlcname Scroll Of Taiwu - OST
+setManifestid(946970, "2089095199798162434", 1087279040)
+--Dlcname Scroll Of Taiwu - 宛渠之民的赠礼之二
+addtoken(2172690, "7035753016401393565")
+addappid(2172690, 1, "e456ab9135bdfdaae17d9235af62db550990ed406c0f7e116fe237793c0b3cd0") --Dlcname Scroll Of Taiwu - 宛渠之民的赠礼之二
+setManifestid(2172690, "5595148215465884713", 1174592)
+--Dlcname Scroll Of Taiwu - 宛渠之民的赠礼之一
+addtoken(2241120, "1649741745593958494")
+addappid(2241120, 1, "306f5087d837883ad86f8b4feb42058723cbb7b7701724c3bb2098df278272d1") --Dlcname Scroll Of Taiwu - 宛渠之民的赠礼之一
+setManifestid(2241120, "8929325035393384592", 3543984)
+--Dlcname Scroll Of Taiwu - 五方神龙
+addtoken(2764950, "9097792362658511678")
+addappid(2764950, 1, "c4c231a528c0f4c95a95fceae57a4530982bd307bd8f07c47070e4a7adebff0b") --Dlcname Scroll Of Taiwu - 五方神龙
+setManifestid(2764950, "6279348019110604381", 192421136)
+--Dlcname Scroll Of Taiwu - 新衣贺春
+addtoken(2764960, "10039634942498840576")
+addappid(2764960, 1, "00140a77be9fb80581f68904948537eb5ba3ca04ffc9b2c024d128d98cbf9365") --Dlcname Scroll Of Taiwu - 新衣贺春
+setManifestid(2764960, "4096172026402743863", 1540416)
+--Dlcname Scroll Of Taiwu - 碧霄蛇影
+addtoken(3464590, "17260488847850872888")
+addappid(3464590, 1, "34a15558cad10dbf91bbb3f82e56f56bfdce3e80ca105b90fb69296f4c94d94f") --Dlcname Scroll Of Taiwu - 碧霄蛇影
+setManifestid(3464590, "2170511017358734763", 23531136)
+--Dlcname Scroll Of Taiwu - 跃马听香
+addtoken(4395170, "6007455422738143747")
+addappid(4395170, 1, "32426e879f3458c36c7827712232c988b375f53b006c967cade6b279b82719f8") --Dlcname Scroll Of Taiwu - 跃马听香
+setManifestid(4395170, "8007211957462256197", 2345232)
+addappid(4395171, 1, "cd96a820b2c9245d21083263f9571fc227fd0fd087dd6abf6f5b740f2ce6ce47") --Dlc Windows Depot Scroll Of Taiwu - 跃马听香
+setManifestid(4395171, "6990289780772373997", 4645504)
+--Dlcname Scroll Of Taiwu - 促织化影
+addappid(4528730, 1, "011923e062968acb50f9d2a992c286143858510fc261adedbc079513df981791") --Dlcname Scroll Of Taiwu - 促织化影
+setManifestid(4528730, "4456568609758365763", 62839264)
+--Dlcname Scroll Of Taiwu - 八载同舟
+addappid(4834440, 1, "f8e3794187a1e35aa42731bcea4507938766a7d8a75db3ca6c73e0a0dfb83ece") --Dlcname Scroll Of Taiwu - 八载同舟
+setManifestid(4834440, "6304815620934302627", 3882080)
+--Dlcname Scroll Of Taiwu - 青山依旧
+addappid(4834450, 1, "8ff4143536007a730a9eb1deb44329ab822ca0b193ce85943b230cf658929096") --Dlcname Scroll Of Taiwu - 青山依旧
+setManifestid(4834450, "9070015990007851062", 6934704)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
