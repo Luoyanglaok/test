981905d9d5813b037a1bc23e7b52371e43a817bd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:32:08 +0800

    add 3768760.lua

--- 3768760.lua
+++ 3768760.lua
@@ -1,4 +1,30 @@
-addappid(3768760)
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-setManifestid(3768761,"3282166900805425338")
-setManifestid(228989,"5753583882400741046")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3768760.lua
+--Gamename 007 First Light
+addappid(3768760, 1, "6f7c66b16f0ee1cfe2e337a85cf54359c812b65f322a21fbd1c9725197e60062") --Mainappid 007 First Light
+addappid(3768761, 1, "8284c5e8921ec5acbe53f4d45af0260905841dc698ca179dd218ce5a2657ef09") --Main Windows Depot 007 First Light
+setManifestid(3768761, "3282166900805425338", 53043933312)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3950810) --Dlcname 007 First Light - Gleaming Earphones (Gadget Skin)
+addappid(3950820) --Dlcname 007 First Light - Gleaming Lighter (Gadget Skin)
+addappid(3950830) --Dlcname 007 First Light - Gleaming Pen (Gadget Skin)
+addappid(3950850) --Dlcname 007 First Light - Gleaming Phone (Gadget Skin)
+addappid(3950870) --Dlcname 007 First Light - Day of the Dead (Exclusive Outfit)
+addappid(3950880) --Dlcname 007 First Light - Desert Explorer (Exclusive Outfit)
+addappid(3950900) --Dlcname 007 First Light - Gentleman Operator (Exclusive Outfit)
+addappid(3950910) --Dlcname 007 First Light - Silent Anchor (Exclusive Outfit)
+addappid(3950930) --Dlcname 007 First Light - Agent's Mark (Weapon Skin)
+addappid(3954230) --Dlcname 007 First Light - Golden Gun (Weapon Skin)
+addappid(3954250) --Dlcname 007 First Light - Obsidian Suit (Exclusive Outfit)
+addappid(3954260) --Dlcname 007 First Light - Classic Tuxedo (Exclusive Outfit)
+addappid(3986520) --Dlcname 007 First Light - Deluxe Edition Upgrade
+addappid(4521800) --Dlcname 007 First Light - First Class (Outfit)
+addappid(4521810) --Dlcname 007 First Light - Forest Operative (Exclusive Outfit)
+addappid(4521820) --Dlcname 007 First Light - Observant Spy (Outfit)
+addappid(4521830) --Dlcname 007 First Light - Leisure Beam (Outfit)
+addappid(4521840) --Dlcname 007 First Light - One More (Outfit)
+addappid(4601250) --Dlcname 007 First Light - Diamond in the Rough (Outfit)
+addappid(4626160) --Dlcname 007 First Light - Standard
+addappid(4707780) --Dlcname 007 First Light - 00 Pack
