d1429ae8a32cf97d6608d3449b67c35d165572ce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:28:22 +0800

    add 2399830.lua

--- 2399830.lua
+++ 2399830.lua
@@ -1,5 +1,54 @@
-addappid(2399830)
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-addappid(2399831,0,"320e0bcc46f1e7d88e18488c73990878eedbc06c3284015e1d0af97abe143e23")
-setManifestid(2399831,"4735635872933062882")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2399830.lua
+--Gamename ARK: Survival Ascended
+addappid(2399830, 1, "b1ea599fef75fb79ad1302210f84313f0e014f3dc079f2af31ae524390ddf59d") --Mainappid ARK: Survival Ascended
+addappid(2399831, 1, "320e0bcc46f1e7d88e18488c73990878eedbc06c3284015e1d0af97abe143e23") --Main Windows Depot ARK: Survival Ascended
+setManifestid(2399831, "5279637775352331674", 208696352320)
+--Dlcname ARK: The Center Ascended
+addappid(2827030, 1, "ca47571a30440cd0d827bee6a57a945cf56d25fd4dbc19435d608994b021302a") --Dlcname ARK: The Center Ascended
+setManifestid(2827030, "1130108437390701715", 9268328432)
+--Dlcname ARK: Scorched Earth Ascended
+addappid(2849450, 1, "06a4aa068a1a3940e0c7eff34508ad6474806f4b46520ef9ea12b4b7f77a3d0e") --Dlcname ARK: Scorched Earth Ascended
+setManifestid(2849450, "6419239003871496352", 6657255824)
+--Dlcname ARK: Aberration Ascended
+addappid(3059650, 1, "84b1a413568c28ea0226bbd983fbf08741ead907cc7cd9cdd447056405850bd2") --Dlcname ARK: Aberration Ascended
+setManifestid(3059650, "1948120393611429977", 6942411568)
+--Dlcname ARK: Extinction Ascended
+addappid(3349320, 1, "e0cbc7f932b6b5ffa7e36b9fe8ccf5b2317d5073f520b8a357a7bb8e32d7a181") --Dlcname ARK: Extinction Ascended
+setManifestid(3349320, "88944860403328183", 5445900464)
+--Dlcname ARK: Astraeos
+addappid(3483400, 1, "8f2706fe64f86426091cbb9a23dedbc3ec0c53a78877abb9b549edded5f436ea") --Dlcname ARK: Astraeos
+setManifestid(3483400, "8537552427520024462", 9417128752)
+--Dlcname ARK: Lost Colony
+addappid(3583650, 1, "e7d5b3c53aabefd8ec1f24038e55c6e614c31d7e5c6237c443e83c4eb1fc5a83") --Dlcname ARK: Lost Colony
+setManifestid(3583650, "8615758654606748278", 38582577056)
+--Dlcname ARK: Ragnarok Ascended
+addappid(3675020, 1, "5028ee914f2c74aba43cc91d7f2f49fd5ad0ec96dcb76372ed26deec22b0e88c") --Dlcname ARK: Ragnarok Ascended
+setManifestid(3675020, "4685250889310099161", 12165310048)
+--Dlcname ARK: Valguero Ascended
+addappid(3982310, 1, "184547e9bf5059e77f7971af6c7a064a5a7eb7a76a3b210fbeed6ebb00df991c") --Dlcname ARK: Valguero Ascended
+setManifestid(3982310, "2211942404099222304", 19750040400)
+--Dlcname ARK: Lost Colony Original Soundtrack
+addappid(4274580) --Dlcname ARK: Lost Colony Original Soundtrack
+addappid(4274581, 1, "8420f95f14b885bdce8089b09dc84ed09086a34281e12c3f53a7e9a7879412cb") --Dlc Windows Depot ARK: Lost Colony Original Soundtrack
+setManifestid(4274581, "3676056276399030608", 285771344)
+--Dlcname ARK: Genesis Ascended Part 1
+addappid(4558470, 1, "4b8207f4622bf4e9eb35a0da27d875f87b29ea9081d4cf4188ebf38cbbf1fde3") --Dlcname ARK: Genesis Ascended Part 1
+setManifestid(4558470, "2192039426397195906", 18528402624)
+--Dlcname ARK: Dragontopia
+addappid(4558490, 1, "d20fe0649f9a014dbb0feb29ecd21514ec3b7743f4d49d3cb8885afb1b22e020") --Dlcname ARK: Dragontopia
+setManifestid(4558490, "5157510924718883041", 64)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2881150) --Dlcname ARK: Bob's Tall Tales
+addappid(2972680) --Dlcname ARK Fantastic Tames - Pyromane
+addappid(3282470) --Dlcname ARK Fantastic Tames - Dreadmare
+addappid(3571730) --Dlcname ARK Animated Series 109-Costumes Pack
+addappid(3720100) --Dlcname ARK: Lost Colony Expansion Pass
+addappid(3720200) --Dlcname ARK Fantastic Tames - Drakelings
+addappid(3982300) --Dlcname ARK Fantastic Tames - Elderclaw
+addappid(4558480) --Dlcname ARK Tides of Fortune
+addappid(4558510) --Dlcname ARK Fantastic Tames Season 1
