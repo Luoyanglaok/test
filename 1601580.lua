f4125577b43bedda61ff25283a5511b825fd840b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:41:07 +0800

    add 1601580.lua

--- 1601580.lua
+++ 1601580.lua
@@ -1,15 +1,37 @@
-addappid(1601580)
-addtoken(1601580,"9776222650124242927")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1601581,0,"7372485e88b89813dc5250dca3be72fcfa499604514bea173703fbb7bac1f78f")
-setManifestid(1601581,"6694642185819303478")
-addappid(2791500,0,"647f22682ffa05f21b1ea12779b04414782ddf4f2456565761b6ee0f863204ee")
-setManifestid(2791500,"62191551857984617")
-addappid(1601582,0,"430c4ff745d427259e5934cada320f38cfadad961bdb56a35f4403f2ffeaea6b")
-setManifestid(1601582,"742568876558121323")
-addappid(1601583,0,"be06bc27eb3b8e788a06bae787211c5e406cf846a3aeceffbac54475a447d036")
-setManifestid(1601583,"3503695293846071892")
-addappid(2791490,0,"3f4b6f54645a56bc7991b93bed90155b81354f90e7fb4113e92ed9378980e36c")
-setManifestid(2791490,"5534266034783623960")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1601580.lua
+--Gamename Frostpunk 2
+addappid(1601580, 1, "b188b56eb26beef5cc8940334590f6202b960b404f64fc45efd499e5b8fa8446") --Mainappid Frostpunk 2
+addtoken(1601580, "9776222650124242927")
+addappid(1601581, 1, "7372485e88b89813dc5250dca3be72fcfa499604514bea173703fbb7bac1f78f") --Main Windows Depot Frostpunk 2
+setManifestid(1601581, "1112786033110687412", 17231123184)
+addappid(1601582, 1, "430c4ff745d427259e5934cada320f38cfadad961bdb56a35f4403f2ffeaea6b") --Main Macos Depot Frostpunk 2
+setManifestid(1601582, "1620030169262753711", 17263835552)
+--Dlcname Frostpunk 2: Digital Artbook & “Warm Flesh” Novella
+addtoken(2791490, "2460618430796516304")
+addappid(2791490, 1, "3f4b6f54645a56bc7991b93bed90155b81354f90e7fb4113e92ed9378980e36c") --Dlcname Frostpunk 2: Digital Artbook & “Warm Flesh” Novella
+setManifestid(2791490, "5534266034783623960", 246494896)
+--Dlcname Frostpunk 2: “Captain's Hall” In-game Item
+addtoken(2791500, "17920453012318309804")
+addappid(2791500, 1, "647f22682ffa05f21b1ea12779b04414782ddf4f2456565761b6ee0f863204ee") --Dlcname Frostpunk 2: “Captain's Hall” In-game Item
+addappid(1601583, 1, "be06bc27eb3b8e788a06bae787211c5e406cf846a3aeceffbac54475a447d036") --Dlc Macos Depot Frostpunk 2: “Captain's Hall” In-game Item
+setManifestid(1601583, "1556554617951712131", 17259513376)
+setManifestid(2791500, "4473289431149296091", 106553792)
+--Dlcname Frostpunk 2: Fractured Utopias
+addappid(2791510, 1, "ffad03667e8754d84e3dc4638c128fab65e760710bb056d2296e53c6c6de50bd") --Dlcname Frostpunk 2: Fractured Utopias
+addappid(1601584, 1, "36e9a475072dc9722c31a1fb0e453b8059561f32f293e4fe0b23016f5243ee2d") --Dlc Macos Depot Frostpunk 2: Fractured Utopias
+setManifestid(1601584, "6328326313015819129", 0)
+setManifestid(2791510, "8755351809277204459", 870832896)
+--Dlcname Frostpunk 2: Breach of Trust
+addappid(4124750, 1, "019eed088b74a71ab2144e9bd30f2b2c49a8c2337683ea8c5e5b3c378da5d2f9") --Dlcname Frostpunk 2: Breach of Trust
+setManifestid(4124750, "3410021992439246931", 621610320)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4065430) --Dlcname Frostpunk 2: Deluxe Edition Upgrade
+--Missing Dlcs
+--Dlcname Frostpunk 2: Original Soundtrack
