a4bd160d0381ca7cbffe9ebd216a82b9fdf13a8b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:01:16 +0800

    add 2289750.lua

---
+++ 2289750.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2289750.lua
+--Gamename Super Fantasy Kingdom
+addappid(2289750, 1, "d9f24360174522fcb3d9f1d042634358f1ef823565062eb565add4fad25f166d") --Mainappid Super Fantasy Kingdom
+addappid(2289751, 1, "7c55ae6c9ad8d57caf5c869d0646221c0175dd371a933d2c9edabd34ab38c835") --Main Windows Depot Super Fantasy Kingdom
+setManifestid(2289751, "6572719122002941960", 273713024)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
