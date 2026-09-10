42a36de028cb1fe88514b57b6b3477f90fa95d99
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:54:32 +0800

    add 261550.lua

--- 261550.lua
+++ 261550.lua
@@ -1,7 +1,29 @@
-addappid(261550)
-setManifestid(228988,"6645201662696499616")
-setManifestid(229006,"1784011429307107530")
-addappid(261552,0,"b6cfe4c61b9f19a7399a290c50b3dc18052f63b095401ffb3aa8286277597365")
-setManifestid(261552,"9186208735541562301")
-addappid(261551,0,"16a71c423df683e98af58e40c60a6fc8087dc360f977a73c7d138e8d26b99062")
-setManifestid(261551,"6992419040320091598")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 261550.lua
+--Gamename Mount & Blade II: Bannerlord
+addappid(261550, 1, "979c4a097d819ee61862a7ad898e1a20dea3708a8dcbeaaa00a54c4975c1b107") --Mainappid Mount & Blade II: Bannerlord
+addappid(261551, 1, "16a71c423df683e98af58e40c60a6fc8087dc360f977a73c7d138e8d26b99062") --Main Windows Depot Mount & Blade II: Bannerlord
+setManifestid(261551, "2130492496439118726", 49594484976)
+addappid(261552, 1, "b6cfe4c61b9f19a7399a290c50b3dc18052f63b095401ffb3aa8286277597365") --Main Windows Depot Mount & Blade II: Bannerlord
+setManifestid(261552, "652956066029874796", 3832992864)
+addappid(2240110, 1, "305559a9a6ef58d7981b6e10beb7e104acb806407314b6a2b7cbc3cabb239fcf") --Main Windows Depot Mount & Blade II: Bannerlord
+addappid(4456490, 1, "77bd4b511a0c5823331372155f4cc406df167f9a3181fe3cbfec32d41b5183ed") --Main Windows Depot Mount & Blade II: Bannerlord
+setManifestid(4456490, "1015157637537524692", 14716764640)
+--Dlcname Mount & Blade II: Bannerlord Soundtrack
+addappid(2194520) --Dlcname Mount & Blade II: Bannerlord Soundtrack
+addappid(2194521, 1, "37b0e0ec7e17d59a6134c8537144e8a03d3b659627a6f8cb219f6318c5daef66") --Dlc Windows Depot Mount & Blade II: Bannerlord Soundtrack
+setManifestid(2194521, "6400901825798180743", 176229008)
+addappid(2194523, 1, "3ca163dc7ae2e8b921eaf3da66b9d60d253a7e031689202dbb830ff20511c4c2") --Dlc Windows Depot Mount & Blade II: Bannerlord Soundtrack
+setManifestid(2194523, "8721140348870055028", 809172992)
+--Dlcname Mount & Blade II: Bannerlord - Digital Companion
+addappid(2240111, 1, "172d8d7fb867c14dfe3251829c334dd13bd3fa452eca4f34a3c583ed9928efea") --Dlc Windows Depot Mount & Blade II: Bannerlord - Digital Companion
+setManifestid(2240111, "6194812840481947105", 978224800)
+--Dlcname Mount & Blade II: Bannerlord - War Sails
+addappid(2927200, 1, "27f86f83a59f156011599d935b8f3f25a5472b9030cd56a2a91aa0597f939512") --Dlcname Mount & Blade II: Bannerlord - War Sails
+setManifestid(2927200, "4993282495083223470", 26147759328)
+--Dlcname Mount & Blade II: Bannerlord - War Sails Modding Kit
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
