caab863484e11111c52224190bf6616fe16e081e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:21:55 +0800

    add 2162800.lua

--- 2162800.lua
+++ 2162800.lua
@@ -1,10 +1,18 @@
-addappid(2162800)
-addtoken(2162800,"18007773361721752868")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2162802,0,"163ae6ae731b47cb94170aee0ed8a4d4ecc62c6082bc319cc270e20c5eb0f759")
-setManifestid(2162802,"4645733043704051650")
-addappid(2162803,0,"9b7c2e9a0f8d2a28b522233286436b360fffb4830d45a69a5a0bf35573da8647")
-setManifestid(2162803,"5810959838591075632")
-addappid(2162804,0,"aba21c81613972e41bf5e0131f382113d13fbdee073a02f7065cfc4256630232")
-setManifestid(2162804,"3161757862671664441")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2162800.lua
+--Gamename shapez 2 - Factory
+addappid(2162800, 1, "615a243c485abe945a4ff4a2a3e6a1663325ef15432b30f2f772d747cb1a0d2f") --Mainappid shapez 2 - Factory
+addtoken(2162800, "18007773361721752868")
+addappid(2162802, 1, "163ae6ae731b47cb94170aee0ed8a4d4ecc62c6082bc319cc270e20c5eb0f759") --Main Windows Depot shapez 2 - Factory
+setManifestid(2162802, "4008747342886200441", 2090975424)
+addappid(2162803, 1, "9b7c2e9a0f8d2a28b522233286436b360fffb4830d45a69a5a0bf35573da8647") --Main Linux Depot shapez 2 - Factory
+setManifestid(2162803, "3903618799325724812", 2095199024)
+addappid(2162804, 1, "aba21c81613972e41bf5e0131f382113d13fbdee073a02f7065cfc4256630232") --Main Macos Depot shapez 2 - Factory
+setManifestid(2162804, "7686725161086972918", 2090261168)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3075620) --Dlcname shapez 2 Supporter Edition Upgrade
+addappid(4746220) --Dlcname Shapez 2 - Hyper Skin DLC
