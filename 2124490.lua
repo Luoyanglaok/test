304e8a192edcdb8699a92dbc5b24a384b09a7a8f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:29:03 +0800

    add 2124490.lua

--- 2124490.lua
+++ 2124490.lua
@@ -1,6 +1,19 @@
-addappid(2124490)
-addtoken(2124490,"2486523437417765568")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2124491,0,"1a14569bdc22983489a37bb81ed420c6230f43e84548d0ec0d4a3e9687861955")
-setManifestid(2124491,"4138456104249046245")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2124490.lua
+--Gamename SILENT HILL 2
+addappid(2124490, 1, "0ea5165430cb3b8baf9e0944b998ad40e910c6763ac7adf3ffb2e10c01d979c9") --Mainappid SILENT HILL 2
+addtoken(2124490, "2486523437417765568")
+addappid(2124491, 1, "1a14569bdc22983489a37bb81ed420c6230f43e84548d0ec0d4a3e9687861955") --Main Windows Depot SILENT HILL 2
+setManifestid(2124491, "4138456104249046245", 35299455152)
+--Dlcname SILENT HILL 2 - Artbook
+addtoken(2951540, "3447079396028723161")
+addappid(2951540) --Dlcname SILENT HILL 2 - Artbook
+addappid(2951541, 1, "e089852efbe924984e137b7c02cf0f4d4481f091625dd22b61be2e5ffd02b994") --Dlc Windows Depot SILENT HILL 2 - Artbook
+setManifestid(2951541, "4181790107554818304", 224601584)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3097490) --Dlcname SILENT HILL 2 - Mira Mask
+addappid(3097500) --Dlcname SILENT HILL 2 - Pyramid Head Mask
