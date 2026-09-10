baf5e02ac0da2a99df8ccac315b0dd6d5d085732
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:20:17 +0800

    add 3099660.lua

--- 3099660.lua
+++ 3099660.lua
@@ -1,6 +1,15 @@
-addappid(3099660)
-setManifestid(228989,"3514306556860204959")
-addappid(3099661,0,"8307ffd73510ab48dec77577266458532f5d90033565f1234fedc768ceef12e4")
-setManifestid(3099661,"5358850037068768664")
-addappid(3099662,0,"e0b56e1d2f42b2742e5c77cb4cfc5cc56cb1a774b4612aee8919d88582b10ff4")
-setManifestid(3099662,"5518153978984336586")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3099660.lua
+--Gamename 完蛋！我被美女包围了！前传
+addappid(3099660) --Mainappid 完蛋！我被美女包围了！前传
+addappid(3099661, 1, "8307ffd73510ab48dec77577266458532f5d90033565f1234fedc768ceef12e4") --Main Windows Depot 完蛋！我被美女包围了！前传
+setManifestid(3099661, "5358850037068768664", 24923713936)
+addappid(3099662, 1, "e0b56e1d2f42b2742e5c77cb4cfc5cc56cb1a774b4612aee8919d88582b10ff4") --Main Macos Depot 完蛋！我被美女包围了！前传
+setManifestid(3099662, "5518153978984336586", 24954800704)
+--Dlcname 完蛋！我被美女包围了！前传-2K视频
+addappid(3228000) --Dlcname 完蛋！我被美女包围了！前传-2K视频
+addappid(3228001, 1, "6bca35027f9f987dda7200a0cf8dc7ce25121ce6e4a0843db55984287e63f56f") --Dlc Windows Depot 完蛋！我被美女包围了！前传-2K视频
+setManifestid(3228001, "3360008553192908515", 39065998704)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
