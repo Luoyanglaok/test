2c1ec27defad17474550ab2f4d08036840126072
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:04:46 +0800

    add 3489700.lua

--- 3489700.lua
+++ 3489700.lua
@@ -1,9 +1,22 @@
-addappid(3489700)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(3489701,0,"2266ca31f6f2666ebe75906201e72f9398694030ff1c8214aacebcf74dc94ee3")
-setManifestid(3489701,"4947667343661089824")
-addappid(3596180,0,"a55f79607c32a7e24a388464ddc3cfbc91b2dcee6691061bca3fb32d231ccb32")
-setManifestid(3596180,"6319771942931301862")
-addappid(3596190,0,"1e65f8baa259f487dcdc1d3dce1e198dd23c9f7b77236e4e56fcc9f6b915a449")
-setManifestid(3596190,"3119456928663392571")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3489700.lua
+--Gamename Stellar Blade™
+addappid(3489700, 1, "8ffda038a0cf6e1f5af8fc2022bc307e2e02a92da4001d7a3b6e93efcbbe6bd8") --Mainappid Stellar Blade™
+addappid(3489701, 1, "2266ca31f6f2666ebe75906201e72f9398694030ff1c8214aacebcf74dc94ee3") --Main Windows Depot Stellar Blade™
+setManifestid(3489701, "3495742353909195412", 61044815568)
+--Dlcname Stellar Blade™ x NieR:Automata
+addappid(3596180, 1, "a55f79607c32a7e24a388464ddc3cfbc91b2dcee6691061bca3fb32d231ccb32") --Dlcname Stellar Blade™ x NieR:Automata
+setManifestid(3596180, "2215861949587335617", 266479808)
+--Dlcname Stellar Blade™ x GODDESS OF VICTORY: NIKKE
+addappid(3596190, 1, "1e65f8baa259f487dcdc1d3dce1e198dd23c9f7b77236e4e56fcc9f6b915a449") --Dlcname Stellar Blade™ x GODDESS OF VICTORY: NIKKE
+setManifestid(3596190, "6588499701851943951", 25543120)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3596170) --Dlcname Stellar Blade™ Deluxe Pack
+addappid(3596200) --Dlcname Stellar Blade™ NIKKE Code Box
+addappid(3596210) --Dlcname Stellar Blade™ NIKKE Code Verification
+addappid(3596220) --Dlcname Stellar Blade™ Pre-purchase Rewards
+addappid(3662250) --Dlcname Stellar Blade™: Tachyon Pack
