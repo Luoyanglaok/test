31dae80a7276bf6e141a73caa5caaaafb3438ef1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:57:09 +0800

    add 1948980.lua

--- 1948980.lua
+++ 1948980.lua
@@ -1,9 +1,23 @@
-addappid(1948980)
-addtoken(1948980,"15007247341427304635")
-setManifestid(228990,"1829726630299308803")
-addappid(1948981,0,"e24e21853fcf27393c4424978740b631785a618ee8356eb9fa135070ade87f1b")
-setManifestid(1948981,"2681712572823768641")
-addappid(2924120,0,"1ee9dbc5a2205fa53b8290d7b9444ba5bcbee983bf5c04a92851b83c8edca2ce")
-setManifestid(2924120,"8670192923045755531")
-addappid(3332620,0,"ad493ff72c24f4f74d94a5255ec284fcf42f1717927f548cd65dc879a0c97855")
-setManifestid(3332620,"2332558840217941735")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1948980.lua
+--Gamename 大侠立志传
+addappid(1948980, 1, "6d0e4344aa93ffe73f7fe7bb24d37aed4024fb6abc21e376e1e551157551e56c") --Mainappid 大侠立志传
+addtoken(1948980, "15007247341427304635")
+addappid(1948981, 1, "e24e21853fcf27393c4424978740b631785a618ee8356eb9fa135070ade87f1b") --Main Windows Depot 大侠立志传
+setManifestid(1948981, "1096125930821706249", 1688271440)
+--Dlcname 大侠立志传：碧血丹心 Soundtrack
+addappid(2680270) --Dlcname 大侠立志传：碧血丹心 Soundtrack
+addappid(2680271, 1, "fbe9342c087a8749040fef7d66fc818dec7c79787b96c98b326d21511605fc51") --Dlc Windows Depot 大侠立志传：碧血丹心 Soundtrack
+setManifestid(2680271, "7004318294318620163", 85003536)
+--Dlcname 大侠立志传：碧血丹心 - DLC
+addappid(2924120, 1, "1ee9dbc5a2205fa53b8290d7b9444ba5bcbee983bf5c04a92851b83c8edca2ce") --Dlcname 大侠立志传：碧血丹心 - DLC
+setManifestid(2924120, "6211561909410193898", 188554768)
+--Dlcname 大侠立志传：碧血丹心 - 新颜绘梦
+addappid(3332620, 1, "ad493ff72c24f4f74d94a5255ec284fcf42f1717927f548cd65dc879a0c97855") --Dlcname 大侠立志传：碧血丹心 - 新颜绘梦
+setManifestid(3332620, "4516441096558599586", 299355088)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3128600) --Dlcname 大侠立志传：碧血丹心 - 倩影萍踪
+addappid(3393130) --Dlcname 大侠立志传：碧血丹心 - 祸起蓬莱
+addappid(3731090) --Dlcname 大侠立志传：碧血丹心 - 清风遗痕
