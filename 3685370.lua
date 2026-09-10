7a95a0347e83f3b9461337f3f80425067a2c768f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:17:01 +0800

    add 3685370.lua

---
+++ 3685370.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3685370.lua
+--Gamename 黑山羊BlackGoat
+addappid(3685370) --Mainappid 黑山羊BlackGoat
+addappid(3685371, 1, "73d5a910f6195e96c697edf323d1076fc81b775c7653170d41d829029c81c1b7") --Main Windows Depot 黑山羊BlackGoat
+setManifestid(3685371, "181715241798138025", 713911984)
