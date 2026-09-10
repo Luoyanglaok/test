3d041d1d5da5daa3c23f8610d42f314226fdc808
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:14:58 +0800

    add 1267540.lua

--- 1267540.lua
+++ 1267540.lua
@@ -1,6 +1,18 @@
-addappid(1267540)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(1267541,0,"af197905780602fd49b05bccf6e8f59a872a943619b0aa7c563b2bc9e48e3ee7")
-setManifestid(1267541,"5783580886174022879")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1267540.lua
+--Gamename WRC 9 FIA World Rally Championship
+addappid(1267540) --Mainappid WRC 9 FIA World Rally Championship
+addappid(1267541, 1, "af197905780602fd49b05bccf6e8f59a872a943619b0aa7c563b2bc9e48e3ee7") --Main Windows Depot WRC 9 FIA World Rally Championship
+setManifestid(1267541, "5783580886174022879", 20336934976)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1530500) --Dlcname WRC 9 Audi Quattro A2 1984
+addappid(1530501) --Dlcname WRC 9 Toyota Corolla 1999
+addappid(1530502) --Dlcname WRC 9 Barcelona SSS
+addappid(1530503) --Dlcname WRC 9 Career Starter Upgrades
+addappid(1530504) --Dlcname WRC 9 FIA Rally Star
