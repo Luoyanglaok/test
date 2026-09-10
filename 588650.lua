6ebf2afcac240950459512315f82406290317794
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 22:08:58 +0800

    add 588650.lua

--- 588650.lua
+++ 588650.lua
@@ -10,12 +10,25 @@ addappid(588653, 1, "2336789903ae91cb11227bcf1b373363713f81c832eac5a009f4a34f0df
 setManifestid(588653, "325905742812183616", 1960649088)
 addappid(588654, 1, "d3869ea6e5650f8be36180bcfab511f05b89d33279c4fbcdcb636c53335585ad") --Main Windows Depot Dead Cells
 setManifestid(588654, "1964813582676736440", 11481264)
+--Dlcname Dead Cells: Soundtrack
+addappid(665380, 1, "213ca7ba6466a2d79fc8837b3e50c5f71198e3954c2dae90479e941ff26b137d") --Dlcname Dead Cells: Soundtrack
+setManifestid(665380, "6627368678362331930", 462160336)
+addappid(665381, 1, "a792bf015c0d133e2219891cded6b206019225bdc5032349f9a35e4b029ece63") --Dlc Windows Depot Dead Cells: Soundtrack
+setManifestid(665381, "450335502342766306", 4812217360)
+--Dlcname Dead Cells: Demake Soundtrack
+addappid(1393790) --Dlcname Dead Cells: Demake Soundtrack
+addappid(1393791, 1, "5a8a09600f41a03eb45fb86d7d29b10c3c603bc3a0653823c5dde9fd5b3da3d2") --Dlc Windows Depot Dead Cells: Demake Soundtrack
+setManifestid(1393791, "4455791514475589825", 484768208)
+addappid(1393792, 1, "a711bd988fcaf7ca9f156edee67d7a5a1acd69ac77099eda1d4f472eb56082d4") --Dlc Windows Depot Dead Cells: Demake Soundtrack
+setManifestid(1393792, "8909614192517987309", 1497202256)
+--Dlcname Dead Cells: Return to Castlevania Soundtrack
+addappid(2502260) --Dlcname Dead Cells: Return to Castlevania Soundtrack
+addappid(2502261, 1, "8e730b3155bdffa43eb6216bf37c63452595f22337f9f6ed912de9659cd9e156") --Dlc Windows Depot Dead Cells: Return to Castlevania Soundtrack
+setManifestid(2502261, "5656702432843011231", 202465952)
+addappid(2502262, 1, "6c0bc30e0f03d5da08398e4c9ade8db945e6e27e29fe9504a4f36c78a7e62151") --Dlc Windows Depot Dead Cells: Return to Castlevania Soundtrack
+setManifestid(2502262, "7745291905639148961", 977597856)
 addappid(1046440) --Dlcname Dead Cells: Rise of the Giant
 addappid(1204130) --Dlcname Dead Cells: The Bad Seed
 addappid(1451460) --Dlcname Dead Cells: Fatal Falls
 addappid(1580050) --Dlcname Dead Cells: The Queen and the Sea
 addappid(2101430) --Dlcname Dead Cells: Return to Castlevania
---Missing Dlcs
---Dlcname Dead Cells: Soundtrack
---Dlcname Dead Cells: Demake Soundtrack
---Dlcname Dead Cells: Return to Castlevania Soundtrack
