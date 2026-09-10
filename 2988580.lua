eb330f756bb4287043749ee400c92e8e86d79e2b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:32:40 +0800

    add 2988580.lua

---
+++ 2988580.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2988580.lua
+--Gamename Yakuza 0 Director's Cut
+addappid(2988580, 1, "628009f28e3943f4407e8823eb37646e46ec9b8222cb17bbcc2504cf762809d0") --Mainappid Yakuza 0 Director's Cut
+addappid(2988581, 1, "5dd1da6fa3ee7c2ba5ff647beb5b4fd9899f1ed4c421a6af79eb65b7ce38cb6e") --Main Windows Depot Yakuza 0 Director's Cut
+setManifestid(2988581, "7346982656604945933", 75204935392)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
