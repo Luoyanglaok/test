7669cb14f65aa22edbb6ff6ca0e69df651bce992
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:07:19 +0800

    add 2322010.lua

--- 2322010.lua
+++ 2322010.lua
@@ -1,4 +1,14 @@
-addappid(2322010)
-setManifestid(228989,"3514306556860204959")
-addappid(2322011,0,"78758c85a2a8b04770fb4fe430ac7a215a959b72e496d94f6a0ec5ac38066f49")
-setManifestid(2322011,"3011634702942262327")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2322010.lua
+--Gamename God of War Ragnarök
+addappid(2322010, 1, "13d2e9ad8a1b887be0b84504b3ca15dd92617a090c9751c180f8aa04986f3871") --Mainappid God of War Ragnarök
+addappid(2322011, 1, "78758c85a2a8b04770fb4fe430ac7a215a959b72e496d94f6a0ec5ac38066f49") --Main Windows Depot God of War Ragnarök
+setManifestid(2322011, "151255971557114067", 107627197504)
+--Dlcname God of War Ragnarök - Digital Artbook
+addtoken(2974340, "14621696841799701455")
+addappid(2974340, 1, "b54ec4dbd1528067329c6ce102d3dc0501401e41b08a6cabc0c947d0c9541baf") --Dlcname God of War Ragnarök - Digital Artbook
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2974320) --Dlcname God of War Ragnarök - Pre-purchase Entitlement
+addappid(2974330) --Dlcname God of War Ragnarök - Digital Deluxe Edition Upgrade
