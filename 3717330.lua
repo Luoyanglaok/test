716c5e3e6ca7ca474138a245a9c7d5dd0a546329
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:30:36 +0800

    add 3717330.lua

---
+++ 3717330.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3717330.lua
+--Gamename Yakuza Kiwami
+addappid(3717330, 1, "1a3204b9e9acaff1be13f257805f5aece4c12c8c6c5d62b5ac9a514913370f2c") --Mainappid Yakuza Kiwami
+addappid(3717331, 1, "0ae165b84122aca403f33db5776efe0b7470e19c5dd5f3049ad9b0a149bebc86") --Main Windows Depot Yakuza Kiwami
+setManifestid(3717331, "8313693570548550289", 22527027616)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
