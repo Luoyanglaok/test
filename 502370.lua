08ed2489b9e9e9f299af8a7ffbea849fb9591436
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:50:07 +0800

    add 502370.lua

--- 502370.lua
+++ 502370.lua
@@ -1,3 +1,9 @@
-addappid(502370)
-addappid(502371,0,"19996d49b389b4203817159379b7a56684311727a0525fdd45a7d1e8ee924c3d")
-setManifestid(502371,"8160221183836148520")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 502370.lua
+--Gamename Warhammer 40,000: Sanctus Reach (Classic)
+addappid(502370) --Mainappid Warhammer 40,000: Sanctus Reach (Classic)
+addappid(502371, 1, "19996d49b389b4203817159379b7a56684311727a0525fdd45a7d1e8ee924c3d") --Main Windows Depot Warhammer 40,000: Sanctus Reach (Classic)
+setManifestid(502371, "8160221183836148520", 1468162784)
+addappid(623140) --Dlcname Warhammer 40,000: Sanctus Reach - Legacy of the Weirdboy (Classic)
+addappid(716660) --Dlcname Warhammer 40,000: Sanctus Reach - Sons of Cadia (Classic)
+addappid(841150) --Dlcname Warhammer 40,000: Sanctus Reach - Horrors of the Warp (Classic)
