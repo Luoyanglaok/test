e40b38d2cc8a0120a1068614afd1294e66589df0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:35:02 +0800

    add 3971950.lua

--- 3971950.lua
+++ 3971950.lua
@@ -1,3 +1,11 @@
-addappid(3971950)
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3971950.lua
+--Gamename In Falsus
+addappid(3971950) --Mainappid In Falsus
+addappid(3971951, 1, "a4c52a8c29eaa0924805618903820a93efa6148d1942b1e2abaab445b92e3f62") --Main Windows Depot In Falsus
+setManifestid(3971951, "479563667435485719", 7192978320)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
