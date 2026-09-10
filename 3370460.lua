986cd9232536e9f71b803a7b272e52c762d2174a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:39:39 +0800

    add 3370460.lua

--- 3370460.lua
+++ 3370460.lua
@@ -1,3 +1,6 @@
-addappid(3370460)
-addappid(3370461,0,"3b64166aa24ce934dc47a4c177a3b4dbf9dd53068715f6ee784515245a65281b")
-setManifestid(3370461,"6857070280437590099")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3370460.lua
+--Gamename 幼馴染とイチャラブえっち!! ～初々しかったあの子がド〇乱セッ〇ス中毒に～
+addappid(3370460, 1, "3acca059618c07d7b23623859393ab7c39d1fc4760068c95759431bb3ec722fa") --Mainappid 幼馴染とイチャラブえっち!! ～初々しかったあの子がド〇乱セッ〇ス中毒に～
+addappid(3370461, 1, "3b64166aa24ce934dc47a4c177a3b4dbf9dd53068715f6ee784515245a65281b") --Main Windows Depot 幼馴染とイチャラブえっち!! ～初々しかったあの子がド〇乱セッ〇ス中毒に～
+setManifestid(3370461, "6857070280437590099", 65313072)
