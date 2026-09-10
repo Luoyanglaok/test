95b8d794ba3130c54e8c1b39440f5f42cd07d892
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:50:15 +0800

    add 2005010.lua

--- 2005010.lua
+++ 2005010.lua
@@ -1,5 +1,12 @@
-addappid(2005010)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2005011,0,"a0d26058a3aee26facb0319102648ee8172819bfba6358f4f375b89dd4070d36")
-setManifestid(2005011,"7274678156380217851")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2005010.lua
+--Gamename Warhammer 40,000: Boltgun
+addappid(2005010) --Mainappid Warhammer 40,000: Boltgun
+addappid(2005011, 1, "a0d26058a3aee26facb0319102648ee8172819bfba6358f4f375b89dd4070d36") --Main Windows Depot Warhammer 40,000: Boltgun
+setManifestid(2005011, "7274678156380217851", 6200181504)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2668290) --Dlcname Warhammer 40,000: Boltgun - Forges Of Corruption Expansion
