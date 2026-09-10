b9b6a411511ccbe8dddc131e14f4bf9ab9b31490
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:59:38 +0800

    add 1732190.lua

--- 1732190.lua
+++ 1732190.lua
@@ -1,13 +1,24 @@
-addappid(1732190)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1732191,0,"41fbca52fbea25bd47005f1c64a88877acf4621890040a4dd2f54ee07b05e8dd")
-setManifestid(1732191,"5426184032901443170")
-addappid(1751200,0,"5ad283afb5d2525d5f7bf5c1c95ad9847aa9be16a9df4f98015ef0adc76b81e5")
-setManifestid(1751200,"775183834941695855")
-addappid(1751201,0,"b89827dae7308c51c72ff8c69f724f1db76dfefae10b759f846106bb90446bb7")
-setManifestid(1751201,"747587657479076756")
-addappid(1751203,0,"4adafabbc0acd89e419fa3d001b3e5145f9c1610b20ea41c589f71c86630359f")
-setManifestid(1751203,"109097717630190306")
-addappid(1751204,0,"07ff8fbe297d5350d7cf72088d7c4363bc7f39f8039601e30a222ab5548c8129")
-setManifestid(1751204,"547659163536929531")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1732190.lua
+--Gamename FATAL FRAME / PROJECT ZERO: Maiden of Black Water
+addappid(1732190) --Mainappid FATAL FRAME / PROJECT ZERO: Maiden of Black Water
+addappid(1732191, 1, "41fbca52fbea25bd47005f1c64a88877acf4621890040a4dd2f54ee07b05e8dd") --Main Windows Depot FATAL FRAME / PROJECT ZERO: Maiden of Black Water
+setManifestid(1732191, "5426184032901443170", 12182806288)
+--Dlcname FATAL FRAME / PROJECT ZERO: MOBW - Yuri/Miu/Ren Exclusive Costume: Camera Obscura Hat & Yuri/Miu Exclusive Costume: Witch's Hat
+addappid(1751200, 1, "5ad283afb5d2525d5f7bf5c1c95ad9847aa9be16a9df4f98015ef0adc76b81e5") --Dlcname FATAL FRAME / PROJECT ZERO: MOBW - Yuri/Miu/Ren Exclusive Costume: Camera Obscura Hat & Yuri/Miu Exclusive Costume: Witch's Hat
+setManifestid(1751200, "775183834941695855", 9608944)
+--Dlcname FATAL FRAME / PROJECT ZERO: MOBW - Yuri Exclusive Costume: Ryza Outfit, Ryza Hat
+addappid(1751201, 1, "b89827dae7308c51c72ff8c69f724f1db76dfefae10b759f846106bb90446bb7") --Dlcname FATAL FRAME / PROJECT ZERO: MOBW - Yuri Exclusive Costume: Ryza Outfit, Ryza Hat
+setManifestid(1751201, "747587657479076756", 21629024)
+--Dlcname FATAL FRAME / PROJECT ZERO: MOBW - 20th Anniversary Digital Artbook
+addappid(1751203, 1, "4adafabbc0acd89e419fa3d001b3e5145f9c1610b20ea41c589f71c86630359f") --Dlcname FATAL FRAME / PROJECT ZERO: MOBW - 20th Anniversary Digital Artbook
+setManifestid(1751203, "109097717630190306", 1564703520)
+--Dlcname FATAL FRAME / PROJECT ZERO: MOBW - Past Protagonists Costume Set
+addappid(1751204, 1, "07ff8fbe297d5350d7cf72088d7c4363bc7f39f8039601e30a222ab5548c8129") --Dlcname FATAL FRAME / PROJECT ZERO: MOBW - Past Protagonists Costume Set
+setManifestid(1751204, "547659163536929531", 92439088)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1751202) --Dlcname FATAL FRAME / PROJECT ZERO: MOBW - FATAL FRAME / PROJECT ZERO 20th Anniversary Celebration DLC
