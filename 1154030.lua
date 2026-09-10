24129e27d854723981f1c6432004c7f5653426f8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 23:14:18 +0800

    add 1154030.lua

---
+++ 1154030.lua
@@ -0,0 +1,21 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1154030.lua
+--Gamename Titan Quest II
+addappid(1154030, 1, "4f807f0c5039d652149b0b3b5e807b3e5df82e52152fc127018534e7b2ff5573") --Mainappid Titan Quest II
+addappid(1154031, 1, "e4c24aa5ff3a15fba8dcb2762211078a3f284a840b9514a64e6694e2acbb33b3") --Main Windows Depot Titan Quest II
+setManifestid(1154031, "5735141294807476733", 27128457088)
+addappid(1154032, 1, "43500c93da407fa7209d086b870b5e21bf2fe23eb5b4222593992edd340ca26e") --Main Windows Depot Titan Quest II
+setManifestid(1154032, "134163191811643539", 243237456)
+addappid(1154033, 1, "fd8d7e7db90c144288acda1e77897ffd3ecc1acc26408079c1c8f54c095d3f49") --Main Windows Depot Titan Quest II
+setManifestid(1154033, "1280571733929986813", 0)
+--Dlcname Titan Quest II Soundtrack
+addappid(2671260) --Dlcname Titan Quest II Soundtrack
+addappid(2671261, 1, "eebf14bcceeb99ae7a93de7acea81a05a70922deabce003baa5f3e68c1f0ea78") --Dlc Windows Depot Titan Quest II Soundtrack
+setManifestid(2671261, "8607334647171911587", 184191616)
+addappid(2671262, 1, "35f24f7e185a7dc68335cd0b09056c3f9216b96294938f6e1d83e3d50b98f769") --Dlc Windows Depot Titan Quest II Soundtrack
+setManifestid(2671262, "3203051862930621914", 881899568)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
