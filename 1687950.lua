d2516002a4b91fc5fd88b477fc1828339c834634
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:18:51 +0800

    add 1687950.lua

--- 1687950.lua
+++ 1687950.lua
@@ -1,23 +1,30 @@
-addappid(1687950)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1687951,0,"63d197a9660d17f1f93004f842b51a99590dcb140cfa4b33b67f04b66e3ef2dc")
-setManifestid(1687951,"179949564195054526")
-addappid(1687952,0,"786ef5979ef545b2322dcda59e5f0ccaaadd321a9f6ac7522b6a55198edff0b6")
-setManifestid(1687952,"3167861682612256520")
-addappid(1687953,0,"0d1a567ea2ea013eb35cae7c62deac6973987d8f0cf5585326fc9f0cec3f0868")
-setManifestid(1687953,"676724932591096292")
-addappid(1687954,0,"26d7d88b05209c16a6ca45297dcaaa774b2185b6a74970bac121c1f5cb37b1d9")
-setManifestid(1687954,"4383202142358403441")
-addappid(1687955,0,"987640b74d60f9c60cbf8d2899469cd1d552f00df1da5a3aba3dfa44ea6f375a")
-setManifestid(1687955,"1440355225172460061")
-addappid(1687956,0,"c7b8a0e3c11d039f34b797ce0ce792926c0cb6e70c5cdac969785c85d7d29133")
-setManifestid(1687956,"7521686342464165318")
-addappid(1687957,0,"cd181cf543f85bc275f52e8836e5090293530f895c6109be61bbf8735fd1fa60")
-setManifestid(1687957,"748315061441883037")
-addappid(1687958,0,"bcd88777efb6608bbd36b99c813acfda482f0a0ae1ebbef6844e9f9bb46c5872")
-setManifestid(1687958,"4651383591850403589")
-addappid(1687959,0,"4ed788043739d211ea18eaf3837f9739527b60da089c778b896d9baa05a16415")
-setManifestid(1687959,"223453434450812259")
-addappid(1753980,0,"5373b843f2355df32399e1130a468df8893039961569bde75192c1be9e05f0d6")
-setManifestid(1753980,"3263965511551036653")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1687950.lua
+--Gamename Persona 5 Royal
+addappid(1687950, 1, "213f5a64fac8e551f7588d59a4a32e22feaf3b9118544503f555b3949f5db67f") --Mainappid Persona 5 Royal
+addappid(1687951, 1, "63d197a9660d17f1f93004f842b51a99590dcb140cfa4b33b67f04b66e3ef2dc") --Main Windows Depot Persona 5 Royal
+setManifestid(1687951, "179949564195054526", 35662637744)
+addappid(1687952, 1, "786ef5979ef545b2322dcda59e5f0ccaaadd321a9f6ac7522b6a55198edff0b6") --Main Windows Language Depot English Persona 5 Royal
+setManifestid(1687952, "3167861682612256520", 565022480)
+addappid(1687953, 1, "0d1a567ea2ea013eb35cae7c62deac6973987d8f0cf5585326fc9f0cec3f0868") --Main Windows Language Depot French Persona 5 Royal
+setManifestid(1687953, "676724932591096292", 565337840)
+addappid(1687954, 1, "26d7d88b05209c16a6ca45297dcaaa774b2185b6a74970bac121c1f5cb37b1d9") --Main Windows Language Depot Italian Persona 5 Royal
+setManifestid(1687954, "4383202142358403441", 561992112)
+addappid(1687955, 1, "987640b74d60f9c60cbf8d2899469cd1d552f00df1da5a3aba3dfa44ea6f375a") --Main Windows Language Depot German Persona 5 Royal
+setManifestid(1687955, "1440355225172460061", 564392128)
+addappid(1687956, 1, "c7b8a0e3c11d039f34b797ce0ce792926c0cb6e70c5cdac969785c85d7d29133") --Main Windows Language Depot Spanish Persona 5 Royal
+setManifestid(1687956, "7521686342464165318", 564265776)
+addappid(1687957, 1, "cd181cf543f85bc275f52e8836e5090293530f895c6109be61bbf8735fd1fa60") --Main Windows Language Depot Japanese Persona 5 Royal
+setManifestid(1687957, "748315061441883037", 565116368)
+addappid(1687958, 1, "bcd88777efb6608bbd36b99c813acfda482f0a0ae1ebbef6844e9f9bb46c5872") --Main Windows Language Depot Tchinese Persona 5 Royal
+setManifestid(1687958, "4651383591850403589", 569619168)
+addappid(1687959, 1, "4ed788043739d211ea18eaf3837f9739527b60da089c778b896d9baa05a16415") --Main Windows Language Depot Schinese Persona 5 Royal
+setManifestid(1687959, "223453434450812259", 576057712)
+addappid(1753980, 1, "5373b843f2355df32399e1130a468df8893039961569bde75192c1be9e05f0d6") --Main Windows Language Depot Koreana Persona 5 Royal
+setManifestid(1753980, "3263965511551036653", 564030768)
+addappid(1753981, 1, "4dcbb31f1942de2662bb26d9c7350be54b7bbb3be8d0d2ae35ee93dd601b3871") --Main Windows Depot Persona 5 Royal
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
