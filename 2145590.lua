19895d523a9fe15dd3d79ecae1faaa27d26d9098
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:52:14 +0800

    add 2145590.lua

--- 2145590.lua
+++ 2145590.lua
@@ -1,4 +1,9 @@
-addappid(2145590)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(2145591,0,"dbd72a152acfa2eeefbedd078aad9db33ef14b0d13f928236fb9f2ac417085c7")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2145590.lua
+--Gamename 太吾绘卷 The Scroll Of Taiwu Playtest
+addappid(2145590) --Mainappid 太吾绘卷 The Scroll Of Taiwu Playtest
+addappid(2145591, 1, "dbd72a152acfa2eeefbedd078aad9db33ef14b0d13f928236fb9f2ac417085c7") --Main Windows Depot 太吾绘卷 The Scroll Of Taiwu Playtest
+setManifestid(2145591, "8298000417186519435", 3246468752)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
