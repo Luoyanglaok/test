f047f01d5b71749e58357c8bb1e802750497364a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 01:32:37 +0800

    add 2456740.lua

--- 2456740.lua
+++ 2456740.lua
@@ -1,7 +1,18 @@
-addappid(2456740)
-addappid(3887680)
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-setManifestid(2456741,"5943431838427810183")
-setManifestid(3887680,"8602510422366396616")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2456740.lua
+--Gamename inZOI
+addappid(2456740, 1, "3539dc3e61ab7546b1030d8a8a44145f1ebcc241b65eb601af0d015c3b7a12a4") --Mainappid inZOI
+addappid(2456741, 1, "e91cba2d1d471bdfb115bee76ac997858f84e6ab0a82af5f17d0054f8a3c9686") --Main Windows Depot inZOI
+setManifestid(2456741, "6913867244256278777", 35896465520)
+addappid(2456742, 1, "7c9a756f88682f7ae0913792f29b1c2ad7c702cb41859ecb9965e8c8760aff0d") --Main Macos Depot inZOI
+setManifestid(2456742, "7362610486089539182", 31058391520)
+--Dlcname inZOI : Island Getaway
+addappid(3887680, 1, "25ed1bd4d03ac4fd1a739171c82248fe6db511beea87dccfe33721091039e9c8") --Dlcname inZOI : Island Getaway
+setManifestid(3887680, "7555355012698923774", 1035110592)
+addappid(3887681, 1, "443b555d0c838bd99b995ed172ed1d49f028e6347a1f27e3d74e648cd5718bc2") --Dlc Macos Depot inZOI : Island Getaway
+setManifestid(3887681, "4281404175402743879", 1033555168)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
