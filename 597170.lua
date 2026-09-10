44ab3a83f23d2ce34b75809e8e1d6e7486bbb655
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:05:42 +0800

    add 597170.lua

--- 597170.lua
+++ 597170.lua
@@ -1,5 +1,11 @@
-addappid(597170)
-addappid(597171,0,"54a12fbbe95ba3b3a1ce8580f41e3d3b36b0e88b0780b2a9377f9b8768eb3bd2")
-setManifestid(597171,"330883778744703387")
-addappid(597172,0,"9b6d15086bc7fcc9f9663e9ddcb98328595627032d3c906a333f9b86ae97ac15")
-setManifestid(597172,"2701051849677709049")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 597170.lua
+--Gamename Clone Drone in the Danger Zone
+addappid(597170, 1, "854dbedc56dd80e20fd91e8dd77eb2928d7d24c8acfbf2e009544ef6195901e5") --Mainappid Clone Drone in the Danger Zone
+addappid(597171, 1, "54a12fbbe95ba3b3a1ce8580f41e3d3b36b0e88b0780b2a9377f9b8768eb3bd2") --Main Windows Depot Clone Drone in the Danger Zone
+setManifestid(597171, "2685253951088188559", 1217635344)
+addappid(597172, 1, "9b6d15086bc7fcc9f9663e9ddcb98328595627032d3c906a333f9b86ae97ac15") --Main Macos Depot Clone Drone in the Danger Zone
+setManifestid(597172, "2053901458254836568", 1240224304)
+addappid(4824790) --Dlcname Clone Drone in the Danger Zone - Fast Lane Pack
+addappid(4833350) --Dlcname Clone Drone in the Danger Zone - Horse Power Pack
+addappid(4833360) --Dlcname Clone Drone in the Danger Zone - Night Mode Pack
