221bf3a086c3dc41a8d2a8ceaa08cac5d1368b22
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:28:31 +0800

    add 2014780.lua

--- 2014780.lua
+++ 2014780.lua
@@ -1,10 +1,178 @@
-addappid(2014780)
-setManifestid(228989,"3514306556860204959")
-addappid(2014781,0,"d434a3a2f8e493e4e5aba69d96015b4148060f784373d7a0d366377d79fa109d")
-setManifestid(2014781,"7018607862242250013")
-addappid(2014782,0,"a2bf3327753266ca2e73e7e6c5dfe46a5ba12b3fba4ee1a856e20c13eb878c17")
-setManifestid(2014782,"7428429371777666919")
-addappid(2014783,0,"8604e88b7e76537580a5a3f7c67b914394a68a621a270b800ee64951069a7559")
-setManifestid(2014783,"5345354023428859447")
-addappid(2014784,0,"858fa2504e8bf5101dd680378e1c7a6333239030f84b6a4446cab9b48791c0a9")
-setManifestid(2014784,"8331286439243672428")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2014780.lua
+--Gamename X-Plane 12
+addappid(2014780, 1, "d76e761f2350d719fc159655a51ce9b04ade63fc4c54d01fadb24e27a9f0ceca") --Mainappid X-Plane 12
+addappid(2014781, 1, "d434a3a2f8e493e4e5aba69d96015b4148060f784373d7a0d366377d79fa109d") --Main Windows Depot X-Plane 12
+setManifestid(2014781, "4666255656697409799", 18134081200)
+addappid(2014782, 1, "a2bf3327753266ca2e73e7e6c5dfe46a5ba12b3fba4ee1a856e20c13eb878c17") --Main Windows Depot X-Plane 12
+setManifestid(2014782, "6724559683807017880", 29439184)
+addappid(2014783, 1, "8604e88b7e76537580a5a3f7c67b914394a68a621a270b800ee64951069a7559") --Main Macos Depot X-Plane 12
+setManifestid(2014783, "9185536978135554841", 213263856)
+addappid(2014784, 1, "858fa2504e8bf5101dd680378e1c7a6333239030f84b6a4446cab9b48791c0a9") --Main Linux Depot X-Plane 12
+setManifestid(2014784, "358439959204543622", 65606880)
+--Dlcname Global Scenery: North America
+addappid(2126100, 1, "316aad671d9560fc08df579689455ce826de2d37e01cd82e3261619d28078d54") --Dlcname Global Scenery: North America
+setManifestid(2126100, "1268996111267296633", 18569835920)
+--Dlcname Global Scenery: South America
+addappid(2126101, 1, "bc983cd4ccc0cf1b8b9e4215ad596c825c4d2798223e739dec9944aa2dbe7249") --Dlcname Global Scenery: South America
+setManifestid(2126101, "4119121700675998058", 6892163824)
+--Dlcname Global Scenery: Europe
+addappid(2126102, 1, "034019461186f88625bdc10f7238179c52131946443b8606042676525758a3b6") --Dlcname Global Scenery: Europe
+setManifestid(2126102, "8000978764251497009", 9244065680)
+--Dlcname Global Scenery: Africa
+addappid(2126103, 1, "f732a17dbc8b3b744b456cf8f6d81fca28428bd199c0f6076dcc1846481d5326") --Dlcname Global Scenery: Africa
+setManifestid(2126103, "5739872633844092018", 12049799904)
+--Dlcname Global Scenery: Asia
+addappid(2126104, 1, "b3a749cde58bf9259f02cb4bd01e1f99340d3f6fb518e84d8df124ff93d648d9") --Dlcname Global Scenery: Asia
+setManifestid(2126104, "7046260749035765478", 22150335136)
+--Dlcname Global Scenery: Australia, Oceania, Pacific
+addappid(2126105, 1, "bcd7afa11f663c96da29723b920eed7e32eeef9482967f8b8b12a4a219f85563") --Dlcname Global Scenery: Australia, Oceania, Pacific
+setManifestid(2126105, "6663860979281983921", 4336217888)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Milano Malpensa
+addappid(2259160, 1, "20f25289ec5ac85d719f2e54c06a274c57cd20203137089481245def88b44ed3") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Milano Malpensa
+setManifestid(2259160, "6124047307623537265", 1650143648)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Menorca
+addappid(2259161, 1, "8cf7ad1677ed239c72c6b03e3e38e341e93abde44ffdc448c712e49ccd4addb8") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Menorca
+setManifestid(2259161, "2749528653453186092", 2696208544)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Seychelles XP
+addappid(2259162, 1, "cd2cb7718330be39b5dcaffab964b16ece074400b72ee7aebcce9886ebe2d455") --Dlcname X-Plane 12 Add-on: Aerosoft - Seychelles XP
+setManifestid(2259162, "7209100915163835642", 2245340944)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Society Islands XP - Bora Bora & Leeward Islands
+addappid(2259163, 1, "2d68a6dcb0c82a89bf131c6d27dbfaaad67da2622a9a93a1d10389d1985aeeec") --Dlcname X-Plane 12 Add-on: Aerosoft - Society Islands XP - Bora Bora & Leeward Islands
+setManifestid(2259163, "3740220865607016858", 4391128800)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Marseille
+addappid(2427060, 1, "53110e8ce2688d55eca61770b8f72041d6d62e988c3b5879a0cbac21cab1a86a") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Marseille
+setManifestid(2427060, "9042753859792655006", 2370558656)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Saint Croix XP
+addappid(2427061, 1, "98e2b9c19f040cb486d202578e41847249e1f82b546ccba650d752e75baa4d8d") --Dlcname X-Plane 12 Add-on: Aerosoft - Saint Croix XP
+setManifestid(2427061, "3942137026773788789", 3729427568)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Faroe Islands XP
+addappid(2436890, 1, "03aded20b3dec79881a1a5b0e3e6d449c97012823a09f28b0d29649ccf8f9061") --Dlcname X-Plane 12 Add-on: Aerosoft - Faroe Islands XP
+setManifestid(2436890, "6024201289334657729", 1251731232)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Friedrichshafen
+addappid(2436900, 1, "1305d4e6681200261e31e788740f266f224e577c1041f1f1e364e5d6c0c2990d") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Friedrichshafen
+setManifestid(2436900, "1128523632333876925", 328761920)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Manchester
+addappid(2436901, 1, "565f4f48fb8fb5b5a47b74c0743032928c4fd9b920645c331e53e76d770f6cfd") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Manchester
+setManifestid(2436901, "2879447980409910135", 1305905152)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Madrid
+addappid(2436902, 1, "3a31bdaeaf3255a5586311dbf1bf694ed2757f556192fdd33d697bafdf495411") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Madrid
+setManifestid(2436902, "8092517157590789113", 3769119088)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Berlin-Tegel
+addappid(2436904, 1, "e25ebae0fece5831d9ac46a4abfcd55ba5859a28f311748a38679ef91b3adb83") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Berlin-Tegel
+setManifestid(2436904, "9144131596714854745", 650668336)
+addappid(2437030, 1, "3d8629cd436059b78764ff21b5c14d6a39e04ba6514b7d35adb0ec633b97d043") --Dlcname AppID 2437030
+--Dlcname X-Plane 12 Add-on: FSDesigns - Alliance Municipal Airport
+addappid(2437031, 1, "c3043863ca880062b3d9e2c81bf7bcea3587892e69eabd907a15a1afae0baf7f") --Dlcname X-Plane 12 Add-on: FSDesigns - Alliance Municipal Airport
+setManifestid(2437031, "1750645741849837975", 1706008464)
+--Dlcname X-Plane 12 Add-on: FSDesigns - Jacksonville International Airport
+addappid(2437032, 1, "d68428d70fa74e7c7a58327ef390ae9bea18924301cb4613b5c0d99153b04cfe") --Dlcname X-Plane 12 Add-on: FSDesigns - Jacksonville International Airport
+setManifestid(2437032, "7933084128765343787", 1436668896)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Svalbard XP
+addappid(2437033, 1, "91967880149da6732c3863ed7ced26726aac24aaa5c1d9e48d1fa97d2ba9a6f8") --Dlcname X-Plane 12 Add-on: Aerosoft - Svalbard XP
+setManifestid(2437033, "7480573670697167125", 2277016416)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Society Islands XP - Tahiti & Windward Islands
+addappid(2437034, 1, "61dae1e9dc6fc498f1c9d1a2f86d06ec9bae8d73bc5cb82072afa6fed6496c7d") --Dlcname X-Plane 12 Add-on: Aerosoft - Society Islands XP - Tahiti & Windward Islands
+setManifestid(2437034, "6606893830231619453", 4441800912)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Zurich V2.0
+addappid(2437035, 1, "63e9dc7b45ad882e2769ebb3e60b64c121b38c6f65f9631b32f7c1f125de61d3") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Zurich V2.0
+setManifestid(2437035, "1703220056061655285", 1463926112)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Berlin Brandenburg V2
+addappid(2437036, 1, "d12c11c1af13a3fd8a826cfadb529812ff88a60df1183a392320ca59458ded53") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Berlin Brandenburg V2
+setManifestid(2437036, "6958660961108890623", 1853327712)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Helgoland
+addappid(2437037, 1, "65e8322f92746fcf136375a99917770a4dda731f4d6ad80af88e49f3251a20a6") --Dlcname X-Plane 12 Add-on: Aerosoft - Helgoland
+setManifestid(2437037, "658250369484826434", 2498260928)
+--Dlcname X-Plane 12 Add-on: Aerosoft - St. Tropez
+addappid(2437038, 1, "b0cef4baf5088098bad884ccab84677a6dbd5b413f9b0e5f47bef7a7962fc38f") --Dlcname X-Plane 12 Add-on: Aerosoft - St. Tropez
+setManifestid(2437038, "644071958543830003", 1325947072)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Tromsø
+addappid(2437039, 1, "92804ec8c718b649d9036f2bd6e9584cd20daebda824682df920c21b9230c189") --Dlcname X-Plane 12 Add-on: Aerosoft - Tromsø
+setManifestid(2437039, "5319135919256334020", 797482592)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Stuttgart
+addappid(2969750, 1, "ae9e8ffbba4d5df70f49fe34a51ead7f3a081cb76453911db779e4ecf03a02e8") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Stuttgart
+setManifestid(2969750, "7741273583181325127", 3581560976)
+--Dlcname X-Plane 12 Add-on: FACO Simulations - F-15C Eagle
+addappid(2969770, 1, "b59508418d27721bfc4e2e2077daffb93e05d1e5c7c2ecd3d195f411a66f2a1f") --Dlcname X-Plane 12 Add-on: FACO Simulations - F-15C Eagle
+setManifestid(2969770, "179006954659789828", 965163264)
+--Dlcname X-Plane 12 Add-on: Axonos - Varna Airport
+addappid(2969790, 1, "c0a69d0c9e05defb48b0e969c9ee717f2c921f33cd823becff6a12c8708fcd0d") --Dlcname X-Plane 12 Add-on: Axonos - Varna Airport
+setManifestid(2969790, "2262034601427369546", 3517521264)
+--Dlcname X-Plane 12 Add-on: Axonos - Owen Roberts International Airport
+addappid(2969810, 1, "a978562fd573a1f43b6bb2bca889cfd1f49dc188bd41bf920e88d3691a8a26cf") --Dlcname X-Plane 12 Add-on: Axonos - Owen Roberts International Airport
+setManifestid(2969810, "728328904369324528", 645616768)
+--Dlcname X-Plane 12 Add-on: 29 Palms/Captain7 - Airport Nuremberg
+addappid(2969830, 1, "7521e0a90b3185fff6103173abb40c6c333899f5a59a2e4d303434ff753e3fb8") --Dlcname X-Plane 12 Add-on: 29 Palms/Captain7 - Airport Nuremberg
+setManifestid(2969830, "5874981498781879614", 3901979392)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Kassel
+addappid(2969860, 1, "b4bdda6665a5de020b87e6da718719e64d9420c29e55dbea0822756080cddd25") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Kassel
+setManifestid(2969860, "8751472057273191878", 1863292960)
+--Dlcname X-Plane 12 Add-on: FSDG - Djerba
+addappid(2969880, 1, "fdf6a9190383204878c45767352104e646ba6421da0ce7caf29123396099a5be") --Dlcname X-Plane 12 Add-on: FSDG - Djerba
+setManifestid(2969880, "3481066492867573405", 370550432)
+--Dlcname X-Plane 12 Add-on: FSDG - Teruel
+addappid(2969900, 1, "0f81b41ef5f3cfc4d4e228ceaf594961e86756586f8105927cfab44755d622f5") --Dlcname X-Plane 12 Add-on: FSDG - Teruel
+setManifestid(2969900, "237218513987995169", 739230944)
+--Dlcname X-Plane 12 Add-on: FSDG - Hurghada
+addappid(2969910, 1, "f34c3a85c15803924b62b3c2a043a550b5bafdb6ef751428fe178eb6734f1f22") --Dlcname X-Plane 12 Add-on: FSDG - Hurghada
+setManifestid(2969910, "1555113950770324969", 994532016)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Zagreb
+addappid(2969920, 1, "fefb3f75d51be11482ec482ee2dd0406d3c4fcf54c6f8754de166c60cfa4987f") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Zagreb
+setManifestid(2969920, "2243725394529873152", 953190384)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Melilla
+addappid(2970590, 1, "a6c865e7eb1b283e2635c7de9973342bb0613374172b2f64b371e53bf1449560") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Melilla
+setManifestid(2970590, "4337239042366258788", 1430859680)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Greater Moncton International
+addappid(2970600, 1, "bd805f35c8029fabdf9ee1255c1ec991b70145a052228a7dadc28ebbef18fcb5") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Greater Moncton International
+setManifestid(2970600, "2430878779935165102", 1746635280)
+--Dlcname X-Plane 12 Add-on: FSDG - Malediven
+addappid(2970640, 1, "e0900d6383e2cd7e822dd4e1489592cf886d764aabe5a679031dc3d8f477359a") --Dlcname X-Plane 12 Add-on: FSDG - Malediven
+setManifestid(2970640, "929800832851444243", 1420483216)
+--Dlcname X-Plane 12 Add-on: Skyline Simulations - Long Beach Airport
+addappid(2970650, 1, "c75d4323bd230f059bc0f4aaf47518acc5dfaaf1b93b553eb6e7c831e3183ac9") --Dlcname X-Plane 12 Add-on: Skyline Simulations - Long Beach Airport
+setManifestid(2970650, "3425052040613666910", 1710953984)
+--Dlcname X-Plane 12 Add-on: Skyline Simulations - Billy Bishop Toronto City Airport
+addappid(2970670, 1, "a8fe15dbd63ed93c32f1f0eb6c522f0ab2783704fb08ff25088cabb06ad00e22") --Dlcname X-Plane 12 Add-on: Skyline Simulations - Billy Bishop Toronto City Airport
+setManifestid(2970670, "5644463711086092681", 1245553472)
+--Dlcname X-Plane 12 Add-on: Skyline Simulations - John Wayne Airport
+addappid(2970690, 1, "0ee1277052af26106ca6b0338c238715485c5f874e4e7fc43e6faeb68d080378") --Dlcname X-Plane 12 Add-on: Skyline Simulations - John Wayne Airport
+setManifestid(2970690, "7477715683563556851", 767567312)
+--Dlcname X-Plane 12 Add-on: Aerosoft - Airport Hamburg
+addappid(2970710, 1, "3261d99e1a8fd7da44a6939cc16c637bd2fb85ab772aca890938b6760b9ce7e4") --Dlcname X-Plane 12 Add-on: Aerosoft - Airport Hamburg
+setManifestid(2970710, "5385757923530163716", 1190127760)
+--Dlcname X-Plane 12 Add-on: FSDG - Monastir
+addappid(2970730, 1, "ee6832520b20b47d493fb48a9be89620f17a6b048e259b38cf986a5db1bf1674") --Dlcname X-Plane 12 Add-on: FSDG - Monastir
+setManifestid(2970730, "938205445044681996", 1843513056)
+--Dlcname X-Plane 12 Add-on: DarkBlue Scenery - Kansai Intl Airport
+addappid(2970740, 1, "15aaec6ae9bd85afd1482f8d906ce1e2b546fe716b0728143de33516e965c992") --Dlcname X-Plane 12 Add-on: DarkBlue Scenery - Kansai Intl Airport
+setManifestid(2970740, "3112018900111101544", 1707747168)
+--Dlcname X-Plane 12 Add-on: DarkBlue Scenery - Tokyo Intl Airport
+addappid(2970850, 1, "5db92b29ca731de1d38c628897168c58ab723036831a1652e59d6fa0d927ce2c") --Dlcname X-Plane 12 Add-on: DarkBlue Scenery - Tokyo Intl Airport
+setManifestid(2970850, "3403351854708646311", 2750471776)
+--Dlcname X-Plane 12 Add-on: DarkBlue Scenery - Naha Airport
+addappid(2970870, 1, "2d7cd7cedd4b4fdec4e1aab3324868823117e19dc604625e416c78fed3f27756") --Dlcname X-Plane 12 Add-on: DarkBlue Scenery - Naha Airport
+setManifestid(2970870, "4094895384518875189", 2076265568)
+--Dlcname X-Plane 12 Add-on: DarkBlue Scenery - Chubu Centrair Intl Airport
+addappid(2970890, 1, "807b08a06e5e41a736943d9fbc0a0dfb82732d320a21e94b91eb93bbb68f7804") --Dlcname X-Plane 12 Add-on: DarkBlue Scenery - Chubu Centrair Intl Airport
+setManifestid(2970890, "2655837248317975761", 881476336)
+--Dlcname X-Plane 12 Add-on: FASimulations - Helsinki Metropolitan VFR 2025 Scenery
+addappid(2970900, 1, "c7c1f99166fe960ed40e6c5717f45adfed1b5eacccd751a35b5674d9224723f8") --Dlcname X-Plane 12 Add-on: FASimulations - Helsinki Metropolitan VFR 2025 Scenery
+setManifestid(2970900, "2709959501644136779", 33414413968)
+--Dlcname X-Plane 12 Add-on: Globall Art - Nashville International Airport
+addappid(2970930, 1, "b2740742b3379bf96bdfd1e8dc855188951a2f9ac39c52f01a4eb8bf150795ca") --Dlcname X-Plane 12 Add-on: Globall Art - Nashville International Airport
+setManifestid(2970930, "6520009396607242707", 1616602608)
+--Dlcname X-Plane 12 Add-on: Globall Art - Vancouver International Airport
+addappid(2970940, 1, "85e0a84057729ec9b08727583d6eb9e3576d602f604c0a735a25bb1a64a092a7") --Dlcname X-Plane 12 Add-on: Globall Art - Vancouver International Airport
+setManifestid(2970940, "4083154998645083483", 1940679440)
+--Dlcname X-Plane 12 Add-on: FSDG - Kinshasa
+addappid(2970950, 1, "356bd3120290c30a88c85cdccafdabda2f0ec7fa54c67995faa5960259742483") --Dlcname X-Plane 12 Add-on: FSDG - Kinshasa
+setManifestid(2970950, "6653524496170526896", 1645011200)
+--Dlcname X-Plane 12 Add-on: DarkBlue Scenery - New Chitose Intl Airport
+addappid(2970960, 1, "237fecefa4b774da3b8f688e3a5e9f6517bd5c87c68b283a7471048539d0462d") --Dlcname X-Plane 12 Add-on: DarkBlue Scenery - New Chitose Intl Airport
+setManifestid(2970960, "5831999819405662204", 2732461936)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+--Missing Dlcs
+--Dlcname X-Plane 12 Add-on: Globall Art - Toronto Pearson International
+--Dlcname X-Plane 12 Add-on: Aerosoft - Culebra XP
