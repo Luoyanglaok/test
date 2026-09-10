6175089b932008bb55ddb7c398db7f5e7786548c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:50:59 +0800

    add 55150.lua

--- 55150.lua
+++ 55150.lua
@@ -1,25 +1,49 @@
-addappid(55150)
-addappid(55152,0,"74c1f39841f51827a14de70a58541709448f922375ce843a7bdf8d123b6e00e4")
-setManifestid(55152,"7732626057366896266")
-addappid(55153,0,"850b25c4072739708bdca7603923024a80c5fe87af4984b142e4f39f22c6cf46")
-setManifestid(55153,"5316515551330539091")
-addappid(55155,0,"ff98724747f01ec92ed6c8e62bd0bf8e11ce276e5f909d5ff7975fe0ebe7e3f2")
-setManifestid(55155,"8940353249204101203")
-addappid(55156,0,"d20c26f8b66e6e171ff5541e4904d9cea8a8b45c848f8592c63fe1a600723701")
-setManifestid(55156,"1028619944509671662")
-addappid(55157,0,"d6bdbd0f6bbd5ded437d1ab06542f7999309d1bb8807113d0cf23c70fe29459b")
-setManifestid(55157,"8570175942921542109")
-addappid(55158,0,"0589ec93eaacdccf08c08bc486e0628b191a3f5b9816b1984bcebe65ee9d460f")
-setManifestid(55158,"59163885033298122")
-addappid(55159,0,"6ffd446547e3c0b5a88ae1f178e6056d4e4f6ead0490e1e441fbc0c23acb5f5a")
-setManifestid(55159,"1948134711496472537")
-addappid(55350,0,"2689a6046c0e45173f9f81caae5665aff0d6bdbdac55a5860de1928953d254fd")
-setManifestid(55350,"7934593195949255214")
-addappid(55351,0,"3ccf6393fc34579c34a59d552874d5df809d92739696265a93f0e13d3dc19a67")
-setManifestid(55351,"2597776270728213154")
-addappid(55352,0,"28fd2935fe1fd88e6137f97473791e92be6fa9b48c89f57327cc313a1b64c976")
-setManifestid(55352,"6951698301628534302")
-addappid(55353,0,"5e9b829e0440fb56a0f7275d56e4efc3a959210117839afbbaf56abf059e679f")
-setManifestid(55353,"1415144130525641305")
-addappid(1769500,0,"e48a10e5dd12fd29d4bf0e838aac1bc7b79dbfbc61cebb0280e79e31015687d0")
-setManifestid(1769500,"1661938459010781164")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 55150.lua
+--Gamename Warhammer 40,000: Space Marine - Anniversary Edition
+addappid(55150, 1, "c662dca44ac73307c28aace217411880dd88fc9aea274b1889167ebd03189dcd") --Mainappid Warhammer 40,000: Space Marine - Anniversary Edition
+addappid(55152, 1, "74c1f39841f51827a14de70a58541709448f922375ce843a7bdf8d123b6e00e4") --Main Windows Depot Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55152, "7732626057366896266", 10838668559494)
+addappid(55153, 1, "850b25c4072739708bdca7603923024a80c5fe87af4984b142e4f39f22c6cf46") --Main Windows Depot Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55153, "5316515551330539091", 116051830335)
+addappid(55154, 1, "e1d2f56ee02e67cd0064c261886cf10d6b8141c123bdf6ffacc9bb79a59e3917") --Main Windows Language Depot Japanese Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55154, "868675989080103967", 15653)
+addappid(55155, 1, "ff98724747f01ec92ed6c8e62bd0bf8e11ce276e5f909d5ff7975fe0ebe7e3f2") --Main Windows Language Depot Czech Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55155, "8940353249204101203", 18917)
+addappid(55156, 1, "d20c26f8b66e6e171ff5541e4904d9cea8a8b45c848f8592c63fe1a600723701") --Main Windows Language Depot Dutch Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55156, "1028619944509671662", 17765)
+addappid(55157, 1, "d6bdbd0f6bbd5ded437d1ab06542f7999309d1bb8807113d0cf23c70fe29459b") --Main Windows Language Depot English Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55157, "8570175942921542109", 16597)
+addappid(55158, 1, "0589ec93eaacdccf08c08bc486e0628b191a3f5b9816b1984bcebe65ee9d460f") --Main Windows Language Depot French Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55158, "59163885033298122", 60697600)
+addappid(55159, 1, "6ffd446547e3c0b5a88ae1f178e6056d4e4f6ead0490e1e441fbc0c23acb5f5a") --Main Windows Language Depot German Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55159, "1948134711496472537", 60698176)
+addappid(55350, 1, "2689a6046c0e45173f9f81caae5665aff0d6bdbdac55a5860de1928953d254fd") --Main Windows Language Depot Italian Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55350, "7934593195949255214", 19973)
+addappid(55351, 1, "3ccf6393fc34579c34a59d552874d5df809d92739696265a93f0e13d3dc19a67") --Main Windows Language Depot Polish Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55351, "2597776270728213154", 60696448)
+addappid(55352, 1, "28fd2935fe1fd88e6137f97473791e92be6fa9b48c89f57327cc313a1b64c976") --Main Windows Language Depot Russian Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55352, "6951698301628534302", 21669)
+addappid(55353, 1, "5e9b829e0440fb56a0f7275d56e4efc3a959210117839afbbaf56abf059e679f") --Main Windows Language Depot Spanish Warhammer 40,000: Space Marine - Anniversary Edition
+setManifestid(55353, "1415144130525641305", 60697360)
+--Dlcname Warhammer 40,000: Space Marine - 10th Anniversary Extras Pack
+addappid(1769500, 1, "e48a10e5dd12fd29d4bf0e838aac1bc7b79dbfbc61cebb0280e79e31015687d0") --Dlcname Warhammer 40,000: Space Marine - 10th Anniversary Extras Pack
+setManifestid(1769500, "1661938459010781164", 1903328560)
+addappid(55340) --Dlcname Warhammer 40,000: Space Marine - Emperor's Elite Pack
+addappid(55341) --Dlcname Warhammer 40,000: Space Marine - Traitor Legions Pack
+addappid(55342) --Dlcname Warhammer 40,000: Space Marine - Blood Raven's Chapter Pack
+addappid(55343) --Dlcname Warhammer 40,000: Space Marine - Golden Relic Bolter
+addappid(55344) --Dlcname Warhammer 40,000: Space Marine - Golden Relic Chainsword
+addappid(55345) --Dlcname Warhammer 40,000: Space Marine - Power Sword
+addappid(55346) --Dlcname ValveTestApp55150 DLC1 - PL
+addappid(55347) --Dlcname Warhammer 40,000: Space Marine - Chaos Unleashed Map Pack
+addappid(55348) --Dlcname Warhammer 40,000: Space Marine - Dreadnought Assault
+addappid(55349) --Dlcname Warhammer 40,000: Space Marine - 55349 DLC
+addappid(55470) --Dlcname Warhammer 40,000: Space Marine - Legion of the Damned Armour Set
+addappid(55471) --Dlcname Warhammer 40,000: Space Marine - Alpha Legion Champion Armour Set
+addappid(55472) --Dlcname Warhammer 40,000: Space Marine - Blood Angels Veteran Armour Set
+addappid(55473) --Dlcname Warhammer 40,000: Space Marine - Salamanders Veteran Armour Set
+addappid(55474) --Dlcname Warhammer 40,000: Space Marine - Future MapPack
+addtoken(55474, "10174988491003783350")
+addappid(55475) --Dlcname Warhammer 40,000: Space Marine - Iron Hands Chapter Pack DLC
+addappid(55476) --Dlcname Warhammer 40,000: Space Marine - Death Guard Champion Chapter Pack DLC
