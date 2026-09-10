05595cc67b78fbfc4795d85e8aea62aae65a5876
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:07:42 +0800

    add 1593500.lua

--- 1593500.lua
+++ 1593500.lua
@@ -1,4 +1,9 @@
-addappid(1593500)
-setManifestid(228988,"6645201662696499616")
-addappid(1593501,0,"77f7390a115063bccc41240f71e0bf27d42c6a7c0424657756fe3f99f26a287a")
-setManifestid(1593501,"2535153423663269664")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1593500.lua
+--Gamename God of War
+addappid(1593500, 1, "93b91503fb7aaf54f61ae3197cf1790f9889d142399e30f9bd439e7c6a7dc9f8") --Mainappid God of War
+addappid(1593501, 1, "77f7390a115063bccc41240f71e0bf27d42c6a7c0424657756fe3f99f26a287a") --Main Windows Depot God of War
+setManifestid(1593501, "2535153423663269664", 37134232112)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
