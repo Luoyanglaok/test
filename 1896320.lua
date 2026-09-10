478e7d8d26b2156ce5bfe988a5915d10755c0c14
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 13:58:42 +0800

    add 1896320.lua

--- 1896320.lua
+++ 1896320.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 1896320.lua
-addappid(1896320, 0, "55b10ecff1252ef80f877268f7a53c313f76a068b946b916e1ac9de2bc3a3c47")
+addappid(1896320, 0, "fe396a2a6e1bb9a3232a1af30b2212797f929f63d6765d9f84a04c9f996c69c1")
