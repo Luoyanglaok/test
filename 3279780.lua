b30a9b98ff9b942d481d8335d40b41d546a4220c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:14:52 +0800

    add 3279780.lua

---
+++ 3279780.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3279780.lua
+--Gamename Horizon Walker
+addappid(3279780) --Mainappid Horizon Walker
+addappid(3279781, 1, "5D0FF26265BC8400969A8F5DEA7059B368663CE774F578849BE1FCFC092836EC") --Main Windows Depot Horizon Walker
+setManifestid(3279781, "5926060043838881536", 12913033760)
