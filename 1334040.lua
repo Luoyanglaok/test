9ac819800718df0d3a5968bf6b31f97ccf6b3bb7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:38:36 +0800

    add 1334040.lua

--- 1334040.lua
+++ 1334040.lua
@@ -4,12 +4,6 @@
 addappid(1334040, 1, "32b91cdbb2bb6d706a0c1164e47cc8985679805872d426ca1a2acf8b07e5c613") --Mainappid Paper Dolls 2 纸人贰
 addappid(1334041, 1, "597e814feb7f6fd2584d5f51e05f08b325ab4c0325e79ec83dbafb1b61568937") --Main Windows Depot Paper Dolls 2 纸人贰
 setManifestid(1334041, "3370083081851074381", 7777690544)
---Dlcname Paper Dolls Soundtrack
-addappid(1376680) --Dlcname Paper Dolls Soundtrack
-addappid(1376681, 1, "1e106944b2ebcdeefec6333c446f1b359ef1d12f76993bab390ac90f48cdb7ff") --Dlc Windows Depot Paper Dolls Soundtrack
-setManifestid(1376681, "5044526373721490663", 70909024)
-addappid(1376682, 1, "fb7e1bb62a7b882ebb151b808f47733b6f89fe1cb0ac71a68a25e734f4209e82") --Dlc Windows Depot Paper Dolls Soundtrack
-setManifestid(1376682, "4715463273232950816", 235032160)
 --Dlcname Paper Dolls 2 逃出生天
 addappid(1453650, 1, "205f3bfd33dab5bd606c4959fa858e6046c33db2a1b86ec84ff73d62c6b6d6da") --Dlcname Paper Dolls 2 逃出生天
 setManifestid(1453650, "5162287514018279639", 223189808)
