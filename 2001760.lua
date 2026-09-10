f4288eb81842eb2b8e459b43375657314f514246
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:43:36 +0800

    add 2001760.lua

---
+++ 2001760.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2001760.lua
+--Gamename Beast of Reincarnation
+addappid(2001760, 1, "5a7d287208fe812255055e80d0e5f02b15836607d90c100f2760a85cbf70cb33") --Mainappid Beast of Reincarnation
+addappid(2001761, 1, "0d843c3fd4bba4400b829693c665e6394ad8311f3f7f6c0acf31c71eab887c0c") --Main Windows Depot Beast of Reincarnation
+setManifestid(2001761, "5820605326682716309", 31561982976)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4146810) --Dlcname Beast of Reincarnation Pre-Order DLC
+addappid(4146830) --Dlcname Beast of Reincarnation Deluxe DLC
