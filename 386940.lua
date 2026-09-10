66a15f1f84f8a87d65e41def1eb29a5768aea11e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:07:07 +0800

    add 386940.lua

--- 386940.lua
+++ 386940.lua
@@ -1,14 +1,26 @@
-addappid(386940)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228990,"1829726630299308803")
-addappid(386941,0,"468fd966fb446f6751ea67a25687b0e423fea61638f6b942a41ed6f8eb7f073a")
-setManifestid(386941,"819494070590847490")
-addappid(386942,0,"6050a40c785f778d78655ff43408f770ba44ed0f0b066bc88591e84746c3820e")
-setManifestid(386942,"2806442111850230345")
-addappid(386943,0,"eda580fb43750c674790357a3d78df444c272da9003b5a6638e44a102a8b3b63")
-setManifestid(386943,"8460242137328080823")
-addappid(386944,0,"7e59c8157c276f6ef0cbccb7ba16cad1cbe5d0a40e8938aec309c71b07715321")
-setManifestid(386944,"5280474380969321166")
-addappid(386945,0,"836fe7f1c616c04e425ce816245609bfb5cbe8ea683172027557902b9e0ddbcb")
-setManifestid(386945,"6226733280434829419")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 386940.lua
+--Gamename Ultimate Chicken Horse
+addappid(386940, 1, "7f13a3c2874df71a67907165f98b238534688d9860e14d8c06ab4e0dfe334a04") --Mainappid Ultimate Chicken Horse
+addappid(386941, 1, "468fd966fb446f6751ea67a25687b0e423fea61638f6b942a41ed6f8eb7f073a") --Main Windows Depot Ultimate Chicken Horse
+setManifestid(386941, "819494070590847490", 64)
+addappid(386942, 1, "6050a40c785f778d78655ff43408f770ba44ed0f0b066bc88591e84746c3820e") --Main Windows Depot Ultimate Chicken Horse
+setManifestid(386942, "6866382348743602766", 780129728)
+addappid(386943, 1, "eda580fb43750c674790357a3d78df444c272da9003b5a6638e44a102a8b3b63") --Main Macos Depot Ultimate Chicken Horse
+setManifestid(386943, "425613942289430858", 788600080)
+addappid(386944, 1, "7e59c8157c276f6ef0cbccb7ba16cad1cbe5d0a40e8938aec309c71b07715321") --Main Linux Depot Ultimate Chicken Horse
+setManifestid(386944, "1154853445036469476", 779016960)
+addappid(386945, 1, "836fe7f1c616c04e425ce816245609bfb5cbe8ea683172027557902b9e0ddbcb") --Main Windows Depot Ultimate Chicken Horse
+setManifestid(386945, "5006566939340776772", 776879072)
+--Dlcname Ultimate Chicken Horse Soundtrack
+addappid(493070, 1, "6a0e5e881dd787aa2aa2080e09a96c7596ce4a71fce1c2c595dcea57a1351f63") --Dlcname Ultimate Chicken Horse Soundtrack
+setManifestid(493070, "8650004532875876756", 268189264)
+addappid(493071, 1, "62e1a518fba65462826ea59943e0fd9de0a238f87dbfcc3ffe723678d836fe13") --Dlc Windows Depot Ultimate Chicken Horse Soundtrack
+setManifestid(493071, "59952338851560596", 1624461968)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
