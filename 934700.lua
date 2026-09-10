d355d1d543085d754a33b8bcce9825e8e43a45ba
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:04:56 +0800

    add 934700.lua

--- 934700.lua
+++ 934700.lua
@@ -1,9 +1,35 @@
-addappid(934700)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(934701,0,"9f01bb4de4308fcda3524d8e086fb30c2cee5a647bc41b8ca77afad561bde375")
-setManifestid(934701,"4520157714470513458")
-addappid(934702,0,"74f4653f5c8cd21938bd4a269765ee3310d860bbf57e7fcc44dd02751f277a49")
-setManifestid(934702,"2858245023909888152")
-addappid(934703,0,"58de2f171f82b74cb47ade0cbc87eb4a5046814bdf0bf1b6d359625a63543852")
-setManifestid(934703,"1968873050790886925")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 934700.lua
+--Gamename Dead Island 2
+addappid(934700, 1, "24a48fbdcbb1375b8f5f4a4a78e0ac341d79d00f2ccebf6718aad84e1166a2cf") --Mainappid Dead Island 2
+addappid(934701, 1, "9f01bb4de4308fcda3524d8e086fb30c2cee5a647bc41b8ca77afad561bde375") --Main Windows Depot Dead Island 2
+setManifestid(934701, "1106721445574208449", 67099113296)
+addappid(934702, 1, "74f4653f5c8cd21938bd4a269765ee3310d860bbf57e7fcc44dd02751f277a49") --Main Windows Depot Dead Island 2
+setManifestid(934702, "8002347731515535596", 35509920)
+addappid(934703, 1, "58de2f171f82b74cb47ade0cbc87eb4a5046814bdf0bf1b6d359625a63543852") --Main Windows Depot Dead Island 2
+setManifestid(934703, "4895842858546976242", 35517792)
+addappid(934704, 1, "F8CA25B03C428B9C8997D0C77220099CBEE1019FFA63D3AAAB329CFB89A63AE4") --Main Macos Depot Dead Island 2
+setManifestid(934704, "6462954258249328049", 51052480)
+addappid(934705, 1, "899975E301E3F57AB965E0C7C5881EBB7E3DB0A5E6927F1EC63EBCAF26152620") --Main Macos Depot Dead Island 2
+setManifestid(934705, "4741377670085898559", 77035973456)
+addappid(934706, 1, "1f0c2415b2eace5f969ff25f1339c91e82fe3ba7ec0bd78d7ba3f991fc4dcb8a") --Main Macos Depot Dead Island 2
+setManifestid(934706, "679450087945263863", 51052464)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2667910) --Dlcname Dead Island 2 - Kingdom Come: Deliverance II Weapons Pack
+addappid(2813000) --Dlcname Dead Island 2 - Memories of Banoi Pack
+addappid(2813010) --Dlcname Dead Island 2 - Golden Weapons Pack
+addappid(2813020) --Dlcname Dead Island 2 - Pulp Weapons Pack
+addappid(2813030) --Dlcname Dead Island 2 - Red’s Demise Katana
+addappid(2813040) --Dlcname Dead Island 2 - Character Pack: Silver Star Jacob
+addappid(2813050) --Dlcname Dead Island 2 - Character Pack: Cyber Slayer Amy
+addappid(2813060) --Dlcname Dead Island 2 - Character Pack: Gaelic Queen Dani
+addappid(2813070) --Dlcname Dead Island 2 - Character Pack: Jungle Fantasy Ryan
+addappid(2813080) --Dlcname Dead Island 2 - Character Pack: Steel Horse Carla
+addappid(2813090) --Dlcname Dead Island 2 - Character Pack: Venice Vogue Bruno
+addappid(2813100) --Dlcname Dead Island 2 - Haus
+addappid(2813110) --Dlcname Dead Island 2 - Expansion Pass
+addappid(2813310) --Dlcname Dead Island 2 - SoLA
