3eb40830b59ee59deff0004b58c689f75b1b430f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:36:36 +0800

    add 3101040.lua

--- 3101040.lua
+++ 3101040.lua
@@ -1,3 +1,16 @@
-addappid(3101040)
-addappid(3101041,0,"bc30bed29c59a3d5f0fd1ce9ec95897294403c5f89f2eb2cb08b903cf872246a")
-setManifestid(3101041,"5377937694606161068")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3101040.lua
+--Gamename Magical Girl Witch Trials (魔法少女ノ魔女裁判)
+addappid(3101040, 1, "cabcd6641194a65849564a95229bfd69272d635d75fb45e8aaa94fc470a11341") --Mainappid Magical Girl Witch Trials (魔法少女ノ魔女裁判)
+addappid(3101041, 1, "bc30bed29c59a3d5f0fd1ce9ec95897294403c5f89f2eb2cb08b903cf872246a") --Main Windows Depot Magical Girl Witch Trials (魔法少女ノ魔女裁判)
+setManifestid(3101041, "2495811167861611119", 6716828016)
+addappid(3101042, 1, "03cc78f678cda0957f1afd2f489c892e613dc8a547b0d619be680e1bd298f1bc") --Main Macos Depot Magical Girl Witch Trials (魔法少女ノ魔女裁判)
+setManifestid(3101042, "3915779634405154820", 6774333520)
+addappid(3423771, 1, "225e5f4a15f7f2a6da919191753552d9a7e6cd1ff604549860ea19bd5a93601e") --Main Windows Depot Magical Girl Witch Trials (魔法少女ノ魔女裁判)
+setManifestid(3423771, "2854611272629104707", 574698496)
+addappid(3423772, 1, "da195d5396320d39a6570b674d3ea62e6d6ee67c2c51958cfa996faad9e83e9b") --Main Windows Depot Magical Girl Witch Trials (魔法少女ノ魔女裁判)
+setManifestid(3423772, "5923232314017348539", 1496599680)
+--Dlcname 魔法少女ノ魔女裁判 Digital Original Soundtrack
+addappid(3423770) --Dlcname 魔法少女ノ魔女裁判 Digital Original Soundtrack
+--Missing Dlcs
+--Dlcname 魔法少女ノ魔女裁判 電子設定資料集
