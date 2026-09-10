b17be205f731d1334d8b2ed938dd9186a8005d2c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:51:52 +0800

    add 3668560.lua

---
+++ 3668560.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3668560.lua
+--Gamename Happy Island Fantasy
+addappid(3668560, 1, "b8ef045400b51d405e25654062870d7f44851d5fcbe1bc5c1e097c84215c4997") --Mainappid Happy Island Fantasy
+addappid(3668561, 1, "2fba95b2f05878acc5d5b57f1429cc236645a6aed513ae6d733ed589422235ec") --Main Windows Depot Happy Island Fantasy
+setManifestid(3668561, "3286017332291051475", 7044488240)
+addappid(3668562, 1, "4e174fc55441d6c22cea85197ccc692e37ce48737a3cd8c00858a45439e02dbe") --Main Macos Depot Happy Island Fantasy
+setManifestid(3668562, "5905347203176948914", 7326794240)
+--Dlcname Happy Island Fantasy - Olivia&Sophia
+addappid(4306470, 1, "91e409e2d7a38234508c772677400d2c3b4a9669b5e575ec2de3e00a04b836a7") --Dlcname Happy Island Fantasy - Olivia&Sophia
+setManifestid(4306470, "6867262603850589435", 1360)
+addappid(4306471, 1, "492c0ed40c8f106f540608640a47e1c5710e89bbac411d82251d5a5fe2b74526") --Dlc Macos Depot Happy Island Fantasy - Olivia&Sophia
+setManifestid(4306471, "2114830450670775927", 1360)
+--Dlcname Happy Island Fantasy - Sexual Bliss
+addappid(4603500, 1, "3651a8c1f7e94be15db12a30553a774d8b526a7787544dce0e27cc2b651f94b2") --Dlcname Happy Island Fantasy - Sexual Bliss
+setManifestid(4603500, "7959890016306506922", 1360)
+addappid(4603501, 1, "fbb92f95587a59063770534a59572b96fd088cfd985808e18fd0212f78d238b2") --Dlc Macos Depot Happy Island Fantasy - Sexual Bliss
+setManifestid(4603501, "3323827336338836456", 1360)
