ee1c10f2bad35a81a8f57aa15e687512387f987b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:36:38 +0800

    add 2638890.lua

--- 2638890.lua
+++ 2638890.lua
@@ -1,20 +1,91 @@
-addappid(2638890)
-addappid(3935850)
-addappid(3935810)
-addappid(3935880)
-addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
-setManifestid(2638891,"5940078787069851749")
-setManifestid(3932690,"2538750768099769850")
-setManifestid(3935590,"1614209865751367010")
-setManifestid(3935610,"3639640272087483898")
-setManifestid(3935620,"9072135417618014548")
-setManifestid(3935630,"849292827146697774")
-setManifestid(3935640,"7869318232535607947")
-setManifestid(3935650,"2139237229059626757")
-setManifestid(3935660,"6690501137643375729")
-setManifestid(3935670,"5198353972838508166")
-setManifestid(3935680,"5668231829930088188")
-setManifestid(3935710,"3057918296414170091")
-setManifestid(3935740,"5614209616409865935")
-setManifestid(3935760,"457899910034771138")
-setManifestid(228988,"6645201662696499616")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2638890.lua
+--Gamename Onimusha: Way of the Sword
+addappid(2638890, 1, "f6dbd80432a5e344e291ffbf52ec20c2b63dcfce030fbd1fb06074dcbb8c64f3") --Mainappid Onimusha: Way of the Sword
+addappid(2638891, 1, "65930a28ef13fdb1576424deec5b6f0d117f3690f244de208b9429b5d43439a4") --Main Windows Depot Onimusha: Way of the Sword
+setManifestid(2638891, "5940078787069851749", 55816623856)
+--Dlcname Onimusha: WotS - Charm: Lion Dog
+addappid(3932690, 1, "ae623b8dacaeb8d21cb86d567aa86ff6d0d0d82a19a2ba49a9c58507270763da") --Dlcname Onimusha: WotS - Charm: Lion Dog
+setManifestid(3932690, "2538750768099769850", 759856)
+--Dlcname Onimusha: WotS - Charm: White Monkey
+addappid(3935590, 1, "28d2e6001c9eaf98494d9b4b5323962de3f881f781f69797e052911637dc8788") --Dlcname Onimusha: WotS - Charm: White Monkey
+setManifestid(3935590, "1614209865751367010", 9116096)
+--Dlcname Onimusha: WotS - Charm: Oni Lady
+addappid(3935610, 1, "56f161208df4af3a7e0e7129cff1dee1f38c81b01003d07933c6d4fb218a9adf") --Dlcname Onimusha: WotS - Charm: Oni Lady
+setManifestid(3935610, "3639640272087483898", 1751920)
+--Dlcname Onimusha: WotS - Charm: Little Dancer
+addappid(3935620, 1, "594fdb52abb5badd92059b30ea8b3b9ac2fa4fcc6e212b84a8c27f34f45ce183") --Dlcname Onimusha: WotS - Charm: Little Dancer
+setManifestid(3935620, "9072135417618014548", 895776)
+--Dlcname Onimusha: WotS - Charm: Mentor
+addappid(3935630, 1, "747b6ea35a366feb022e5b00f381a6c4ba9f81f329e33ba38a4b91f517178a7d") --Dlcname Onimusha: WotS - Charm: Mentor
+setManifestid(3935630, "849292827146697774", 3481600)
+--Dlcname Onimusha: WotS - Sword Appearance: Sealed Curse
+addappid(3935640, 1, "103212b8b602e4f57ffbbed3c9226324a4b11af450b6460f47e102ebeef255f4") --Dlcname Onimusha: WotS - Sword Appearance: Sealed Curse
+setManifestid(3935640, "7869318232535607947", 22081376)
+--Dlcname Onimusha: WotS - Sword Appearance: White Lion
+addappid(3935650, 1, "94b74229ec8e6d08f801029276ea2227551d439fefa1f0960274b8b71bf75d23") --Dlcname Onimusha: WotS - Sword Appearance: White Lion
+setManifestid(3935650, "2139237229059626757", 25626752)
+--Dlcname Onimusha: WotS - Sword Appearance: Bamboo & Panda
+addappid(3935660, 1, "013c1a41111cee6ca8f7a0c988086f862b3af6155acbf5ce1cc75147556be898") --Dlcname Onimusha: WotS - Sword Appearance: Bamboo & Panda
+setManifestid(3935660, "6690501137643375729", 38446400)
+--Dlcname Onimusha: WotS - Sword Appearance: Whittled Oar
+addappid(3935670, 1, "d5561c19597467b03d24a2a10640960491668c917c784a502be7368d798a6ca6") --Dlcname Onimusha: WotS - Sword Appearance: Whittled Oar
+setManifestid(3935670, "5198353972838508166", 21900112)
+--Dlcname Onimusha: WotS - Sword Appearance: Raizan
+addappid(3935680, 1, "2c7352f8ac7c65b66c089300945fb844986d0972ba720bf5815f0f4dfe437fd1") --Dlcname Onimusha: WotS - Sword Appearance: Raizan
+setManifestid(3935680, "5668231829930088188", 23802288)
+--Dlcname Onimusha: WotS - Sword Appearance: Enryuu
+addappid(3935710, 1, "c4d54e9c901231dfd6365fd8028113a21ff3b1498ffd3de42b18322657ad163e") --Dlcname Onimusha: WotS - Sword Appearance: Enryuu
+setManifestid(3935710, "3057918296414170091", 36335600)
+--Dlcname Onimusha: WotS - Hellfire Haori
+addappid(3935720, 1, "794c08444318a21509843545dc1563a3f26f31bf365d29491dcbb230a547a610") --Dlcname Onimusha: WotS - Hellfire Haori
+setManifestid(3935720, "1598997428423100521", 18477536)
+--Dlcname Onimusha: WotS - Moonlight Haori
+addappid(3935730, 1, "c69391d546d04417e6981742ad9aeae6876f5840d6876bfa4188aac85d013234") --Dlcname Onimusha: WotS - Moonlight Haori
+setManifestid(3935730, "916022852999675622", 22618288)
+--Dlcname Onimusha: WotS - Outfit for Musashi: Red Armor
+addappid(3935740, 1, "4bba0113b4c1ddfc79e5542a436219348d5e10d3f10be5db38b69c0bb2aca625") --Dlcname Onimusha: WotS - Outfit for Musashi: Red Armor
+setManifestid(3935740, "5614209616409865935", 86939840)
+--Dlcname Onimusha: WotS - Oni Gauntlet: Crimson Lotus
+addappid(3935760, 1, "463bb96fe33c8de4118c782c9d13955e0e0ce00e7add9efe942ed8233d58b6e0") --Dlcname Onimusha: WotS - Oni Gauntlet: Crimson Lotus
+setManifestid(3935760, "457899910034771138", 21791328)
+--Dlcname Onimusha: WotS - Outfit for Okuni: Flower Dance
+addappid(3935770, 1, "8138baf0266d451706216719aa126ce98cc06d32ad32f92753f886b4818d6be6") --Dlcname Onimusha: WotS - Outfit for Okuni: Flower Dance
+setManifestid(3935770, "3345379029419501372", 96522512)
+--Dlcname Onimusha: WotS - Outfit for Okuni: Goldfish Dance
+addappid(3935780, 1, "dc474fa75d698f9aada881fb9149175b3e7ff175768390adc147eaf5af3cf8d4") --Dlcname Onimusha: WotS - Outfit for Okuni: Goldfish Dance
+setManifestid(3935780, "1905616808444006027", 98116208)
+--Dlcname Onimusha: WotS - Outfit for Okuni: Oni Dance
+addappid(3935790, 1, "f80c521e9ef6a61b9d5b12c30487237c5b9d0ba5596e3de9d5b288581d3d81c4") --Dlcname Onimusha: WotS - Outfit for Okuni: Oni Dance
+setManifestid(3935790, "2636126909798155901", 191056576)
+--Dlcname Onimusha: WotS - Outfit for Okuni: Yomogi's Dance
+addappid(3935810, 1, "d157590e4c02a3b93726fe8c10c37dae8629fcd3b8cd0cf13bccf66c7a23711f") --Dlcname Onimusha: WotS - Outfit for Okuni: Yomogi's Dance
+setManifestid(3935810, "2155604707961816536", 90973808)
+--Dlcname Onimusha: WotS - Outfit for the Oni Lady: Bird Call
+addappid(3935820, 1, "3ba87767f5e77a9edc993a83f3a903d0a16e4edbd84e631b5afce889a61df78a") --Dlcname Onimusha: WotS - Outfit for the Oni Lady: Bird Call
+setManifestid(3935820, "7604312708712906953", 65199296)
+--Dlcname Onimusha: WotS - Outfit for the Oni Lady: Snow Camellia
+addappid(3935830, 1, "6cdc9ce5edfe83a926788ee6a3c6630bf45c50cd3c4d327978dc00a26d734ac7") --Dlcname Onimusha: WotS - Outfit for the Oni Lady: Snow Camellia
+setManifestid(3935830, "6920370433013846765", 65272032)
+--Dlcname Onimusha: WotS - Outfit for the Oni Lady: Elegance
+addappid(3935840, 1, "c841af808b50e24f2edde0ebec5b4584ef66fbf02f426018e606187791793c38") --Dlcname Onimusha: WotS - Outfit for the Oni Lady: Elegance
+setManifestid(3935840, "7976919454910969079", 88952688)
+--Dlcname Onimusha: WotS - Outfit for the Oni Lady: Hinoa
+addappid(3935850, 1, "9637f83231589a04634fb7540ab434bd9564738ac4935f6d336165b764f334c5") --Dlcname Onimusha: WotS - Outfit for the Oni Lady: Hinoa
+setManifestid(3935850, "6864373056384121676", 91369696)
+--Dlcname Onimusha: WotS - Materials Pack: Red Souls I
+addappid(3935880, 1, "9dd129eb50fbfced37a7a85d8af63228eb7e33170d9637ecfb72da05cf087497") --Dlcname Onimusha: WotS - Materials Pack: Red Souls I
+setManifestid(3935880, "8349835451433793566", 992)
+--Dlcname Onimusha: WotS - In-Game Mini Digital Soundtrack
+addappid(3936020, 1, "6d68fa8fc4c1743ef07f48eedaba7e6ab457fbb586dad87a5a93dfeabff8a7cc") --Dlcname Onimusha: WotS - In-Game Mini Digital Soundtrack
+setManifestid(3936020, "4059330835345272885", 22674400)
+--Dlcname Onimusha: WotS - Cloudbiter Haori
+addappid(4053020, 1, "0108bc0b7499b66168061f073d9604600f6caf26bb5e361722c9f5216d1a4bd0") --Dlcname Onimusha: WotS - Cloudbiter Haori
+setManifestid(4053020, "3952155057430317179", 14536304)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+--Missing Dlcs
+--Dlcname Onimusha: WotS - Materials Pack: Personal Effects I
+--Dlcname Onimusha: WotS - Materials Pack: Red Souls II
+--Dlcname Onimusha: WotS - Materials Pack: Enhancements III
