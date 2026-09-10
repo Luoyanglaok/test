671721bdaaa3e6fd1fee6662f8e65fc52096ba80
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:37:29 +0800

    add 391730.lua

--- 391730.lua
+++ 391730.lua
@@ -1,6 +1,11 @@
-addappid(391730)
-setManifestid(228990,"1829726630299308803")
-addappid(391731,0,"31db48045139ccd5e4b431c76c2d9831552e9b3877879501bac6b0ea8130551a")
-setManifestid(391731,"5702919251111799161")
-addappid(391732,0,"42943e1707294ffbb6e5e87b3e3627a37d39edb7b10039a4781119eab9716f96")
-setManifestid(391732,"5160663558593528303")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 391730.lua
+--Gamename Crashlands
+addappid(391730) --Mainappid Crashlands
+addappid(391731, 1, "31db48045139ccd5e4b431c76c2d9831552e9b3877879501bac6b0ea8130551a") --Main Windows/Linux Depot Crashlands
+setManifestid(391731, "8455360675739640188", 81053328)
+addappid(391732, 1, "42943e1707294ffbb6e5e87b3e3627a37d39edb7b10039a4781119eab9716f96") --Main Macos Depot Crashlands
+setManifestid(391732, "5160663558593528303", 76010288)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
