38a212a1fe0dd6f69a7f369c92f6c00568dd3663
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:13:14 +0800

    add 12120.lua

--- 12120.lua
+++ 12120.lua
@@ -1,10 +1,15 @@
-addappid(12120)
-setManifestid(228990,"1829726630299308803")
-addappid(12123,0,"b6246a66a369124f35879ade188971257e9827d14f178abace4d759001e6191a")
-setManifestid(12123,"3229936372288409207")
-addappid(12122,0,"eaa6ab82216553e3e53f1de147e88096b378cbc8b0cd488fd2c4d047ff263389")
-setManifestid(12122,"5085199700668523983")
-addappid(12121,0,"d89825855fd6f4b802378b6110effadaea8db612d89de6cf00f75dc92f898bf0")
-setManifestid(12121,"242870450180196986")
-addappid(12124,0,"020b11d09c31b51ddf1f3de13d5a2e420501f31de6638914b7ecfee4b15e3fc8")
-setManifestid(12124,"1305660839026218242")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12120.lua
+--Gamename Grand Theft Auto: San Andreas
+addappid(12120, 1, "0bba607170939d09fce1ee7a25d512c7d3e799b061784bc20a573ecb1664e38e") --Mainappid Grand Theft Auto: San Andreas
+addappid(12121, 1, "d89825855fd6f4b802378b6110effadaea8db612d89de6cf00f75dc92f898bf0") --Main Windows Depot Grand Theft Auto: San Andreas
+setManifestid(12121, "242870450180196986", 872971296)
+addappid(12122, 1, "eaa6ab82216553e3e53f1de147e88096b378cbc8b0cd488fd2c4d047ff263389") --Main Windows Depot Grand Theft Auto: San Andreas
+setManifestid(12122, "5085199700668523983", 1600166400)
+addappid(12123, 1, "b6246a66a369124f35879ade188971257e9827d14f178abace4d759001e6191a") --Main Windows Depot Grand Theft Auto: San Andreas
+setManifestid(12123, "3229936372288409207", 1562506320)
+addappid(12124, 1, "020b11d09c31b51ddf1f3de13d5a2e420501f31de6638914b7ecfee4b15e3fc8") --Main Windows Depot Grand Theft Auto: San Andreas
+setManifestid(12124, "1305660839026218242", 622092576)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
