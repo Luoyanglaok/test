5a16d1f0b73dbb6c3f7c9b3d5f4fc62f201d8c77
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:17:39 +0800

    add 2254740.lua

--- 2254740.lua
+++ 2254740.lua
@@ -1,12 +1,21 @@
-addappid(2254740)
-setManifestid(228989,"3514306556860204959")
-addappid(2254741,0,"025ff3d951f39f6737173ca90de98b40c56f8ea0f1d0be4d59d11328d74f409b")
-setManifestid(2254741,"3990155621538580453")
-addappid(2377460,0,"5a06313ad2816cf0f1b8ce9c8bd060f695c66259a49f08f003471511a72ba659")
-setManifestid(2377460,"8726855442506987686")
-addappid(2377440,0,"2ea9621c7e11c0a35a39310c5ddebd9027c6a7405504ca40a8d5f5d95551c86f")
-setManifestid(2377440,"8974252781239956130")
-addappid(2377461,0,"0c5872695516b4b3882fb14552a849ef52d2a9c98b8928cf91da7a708c73fce1")
-setManifestid(2377461,"8665887035189640330")
-addappid(2377462,0,"69212faea0ab119a38451aaa3e6205bd3630aea21ec29283eb8be7e756d95a33")
-setManifestid(2377462,"8693556843502666168")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2254740.lua
+--Gamename Persona 5 Tactica
+addappid(2254740, 1, "86489b315af1d44f012ed6c37f2af91615a7a660837c9cab8deff39b450d065d") --Mainappid Persona 5 Tactica
+addappid(2254741, 1, "025ff3d951f39f6737173ca90de98b40c56f8ea0f1d0be4d59d11328d74f409b") --Main Windows Depot Persona 5 Tactica
+setManifestid(2254741, "6791489377519143618", 15710466192)
+--Dlcname Persona 5 Tactica: Orpheus Picaro & Izanagi Picaro
+addappid(2377440, 1, "2ea9621c7e11c0a35a39310c5ddebd9027c6a7405504ca40a8d5f5d95551c86f") --Dlcname Persona 5 Tactica: Orpheus Picaro & Izanagi Picaro
+setManifestid(2377440, "8974252781239956130", 0)
+--Dlcname Persona 5 Tactica: Repaint Your Heart Challenge Pack
+addappid(2377460, 1, "5a06313ad2816cf0f1b8ce9c8bd060f695c66259a49f08f003471511a72ba659") --Dlcname Persona 5 Tactica: Repaint Your Heart Challenge Pack
+setManifestid(2377460, "8726855442506987686", 96956864)
+--Dlcname Persona 5 Tactica: Weapon Pack
+addappid(2377461, 1, "0c5872695516b4b3882fb14552a849ef52d2a9c98b8928cf91da7a708c73fce1") --Dlcname Persona 5 Tactica: Weapon Pack
+setManifestid(2377461, "8665887035189640330", 0)
+--Dlcname Persona 5 Tactica: Picaro Summoning Pack + Raoul Persona
+addappid(2377462, 1, "69212faea0ab119a38451aaa3e6205bd3630aea21ec29283eb8be7e756d95a33") --Dlcname Persona 5 Tactica: Picaro Summoning Pack + Raoul Persona
+setManifestid(2377462, "8693556843502666168", 0)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
