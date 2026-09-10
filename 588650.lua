0ffaaad286b79e1cc8767ac31207051c20ff31d2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:25:29 +0800

    add 588650.lua

--- 588650.lua
+++ 588650.lua
@@ -1,9 +1,17 @@
-addappid(588650)
-addappid(588651,0,"b1571411c76b5560172122534e5f36b33665aa1ae5337216fb74816b8c06a9cb")
-setManifestid(588651,"1424459526839357792")
-addappid(588653,0,"2336789903ae91cb11227bcf1b373363713f81c832eac5a009f4a34f0dfbf340")
-setManifestid(588653,"5814907161645516281")
-addappid(588652,0,"98f088e6b86104bf373ec16bf0eddfc7f5228918b7e4c1dddae39f4ab26b0dd3")
-setManifestid(588652,"2808220399414699720")
-addappid(588654,0,"d3869ea6e5650f8be36180bcfab511f05b89d33279c4fbcdcb636c53335585ad")
-setManifestid(588654,"1964813582676736440")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 588650.lua
+--Gamename Dead Cells
+addappid(588650, 1, "9dd9ead18fafa93ccb47172cd88174b9348e9fde0db0b6ca4a36283844111f6f") --Mainappid Dead Cells
+addappid(588651, 1, "b1571411c76b5560172122534e5f36b33665aa1ae5337216fb74816b8c06a9cb") --Main Windows Depot Dead Cells
+setManifestid(588651, "6188834083785799549", 1950386128)
+addappid(588652, 1, "98f088e6b86104bf373ec16bf0eddfc7f5228918b7e4c1dddae39f4ab26b0dd3") --Main Macos Depot Dead Cells
+setManifestid(588652, "620007880138288496", 1954495120)
+addappid(588653, 1, "2336789903ae91cb11227bcf1b373363713f81c832eac5a009f4a34f0dfbf340") --Main Linux Depot Dead Cells
+setManifestid(588653, "325905742812183616", 1960649088)
+addappid(588654, 1, "d3869ea6e5650f8be36180bcfab511f05b89d33279c4fbcdcb636c53335585ad") --Main Windows Depot Dead Cells
+setManifestid(588654, "1964813582676736440", 11481264)
+addappid(1046440) --Dlcname Dead Cells: Rise of the Giant
+addappid(1204130) --Dlcname Dead Cells: The Bad Seed
+addappid(1451460) --Dlcname Dead Cells: Fatal Falls
+addappid(1580050) --Dlcname Dead Cells: The Queen and the Sea
+addappid(2101430) --Dlcname Dead Cells: Return to Castlevania
