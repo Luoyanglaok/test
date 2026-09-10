a20ed6033b6770b696ea48f1a2974bcf9d9e1f67
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:55:09 +0800

    add 2340520.lua

--- 2340520.lua
+++ 2340520.lua
@@ -1,3 +1,6 @@
-addappid(2340520)
-addappid(2340521,0,"087fe8526494d1976a8829148eef69b423316b13c45a469e31a3fc5165a78824")
-setManifestid(2340521,"3328031941408372137")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2340520.lua
+--Gamename Tales of Seikyu
+addappid(2340520, 1, "5bce975ffa92f986a9d45fce63c2816fc07cb686126bd114b7e9039be86e6cdd") --Mainappid Tales of Seikyu
+addappid(2340521, 1, "087fe8526494d1976a8829148eef69b423316b13c45a469e31a3fc5165a78824") --Main Windows Depot Tales of Seikyu
+setManifestid(2340521, "749902812065379480", 9050507888)
