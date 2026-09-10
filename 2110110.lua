05fbe9a7d2abf4eda4e92fcec9f0c11340bbd8d8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 00:31:01 +0800

    add 2110110.lua

--- 2110110.lua
+++ 2110110.lua
@@ -1,3 +1,44 @@
-addappid(2110110)
-addappid(2110111,0,"6431489b1541bc67ac1f67b0f891dc75be8446657c444bfc5837ae0dd619d162")
-setManifestid(2110111,"6703275736217866069")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2110110.lua
+--Gamename Motherless - Season 1
+addappid(2110110, 1, "876bea4d56f1f69680925c3c9aaad4632b29595abb85f69f63b629a0e312a535") --Mainappid Motherless - Season 1
+addappid(2110111, 1, "6431489b1541bc67ac1f67b0f891dc75be8446657c444bfc5837ae0dd619d162") --Main Windows/Macos Depot Motherless - Season 1
+setManifestid(2110111, "3812688693943324006", 4621160384)
+--Dlcname Motherless - Season 1: Support DLC
+addappid(2128150, 1, "078d18896964eca0ce0ee13802f0db0ff6b32cd67397e2989ae1a7354c0d2cd9") --Dlcname Motherless - Season 1: Support DLC
+setManifestid(2128150, "7459171863111034682", 123199120)
+--Dlcname Motherless - Season 1: Support DLC 2.0
+addappid(2156780, 1, "cd9ad64b03d42adc8fb941bdd7457d131c847d32c42a44c8876528b9e4ddc1eb") --Dlcname Motherless - Season 1: Support DLC 2.0
+setManifestid(2156780, "8330702248911343574", 84812368)
+--Dlcname Motherless - Season 2: Chapter 12 DLC
+addappid(2157990, 1, "faa9b59c4b0275b5ed7c46b8151702d6d07b095d10bdee22b94c561787b8cecb") --Dlcname Motherless - Season 2: Chapter 12 DLC
+setManifestid(2157990, "6246137084348464446", 346740976)
+--Dlcname Motherless - Season 2: Chapter 13 DLC
+addappid(2158000, 1, "4b78c8ac2c5c9ede26e105832b61c6752f9f50548b592f05b2ae82c2395e10e5") --Dlcname Motherless - Season 2: Chapter 13 DLC
+setManifestid(2158000, "6200451737273618841", 374965696)
+--Dlcname Motherless - Season 2: Chapter 14 DLC
+addappid(2158010, 1, "640beeb91a6e93252a8ffb755372ae5bd4762c9eb3bad212691e96c836da5db2") --Dlcname Motherless - Season 2: Chapter 14 DLC
+setManifestid(2158010, "9181437526404228462", 884919408)
+--Dlcname Motherless - Season 2: Chapter 15 DLC
+addappid(2158011, 1, "4f685867e8dc7c407f40dcdf6a8b05fa09a9c96f17defd545eb11e4b29b0b8f0") --Dlcname Motherless - Season 2: Chapter 15 DLC
+setManifestid(2158011, "2525028614428325023", 956758880)
+--Dlcname Motherless - Season 2: Chapter 16 DLC
+addappid(2204890, 1, "d4e13b127093dd59261491de40777050990f79c095f9627d553fa2c4b41eed3e") --Dlcname Motherless - Season 2: Chapter 16 DLC
+setManifestid(2204890, "6511494178803439612", 768274192)
+--Dlcname Motherless - Season 3
+addappid(2261470, 1, "a9a11ef5545f9c3662149d4f186464b378340e64e46a7d0f3aa777c0d664c98e") --Dlcname Motherless - Season 3
+setManifestid(2261470, "9221163178471144896", 2332661872)
+--Dlcname Motherless - What If? - Anamarija DLC
+addappid(2611790, 1, "5324a841c53d9d447b4f2d5ab6a34d175cc642e57371e1bd51196b3de1f911bd") --Dlcname Motherless - What If? - Anamarija DLC
+setManifestid(2611790, "7502312027817717088", 1146810176)
+--Dlcname Motherless - What If? - Valerie DLC
+addappid(3195550, 1, "ebf72c1ec3bd45e322a80f98a7f93197c6e5a71377dda636acfbc875a4580dc5") --Dlcname Motherless - What If? - Valerie DLC
+setManifestid(3195550, "6718414939350147656", 222170368)
+--Dlcname Motherless - Vacation Special: Episode 1
+addappid(3649120, 1, "d2cfe698014705a192e2c1ded54e8586eb03df785cd9a2641e3264d42a6b6d69") --Dlcname Motherless - Vacation Special: Episode 1
+setManifestid(3649120, "3509418854832605886", 816516368)
+--Dlcname Motherless - Vacation Special: Episode 2
+addappid(4509180, 1, "948839ff1c4dc0b34220b81a224c988cda10eead08f19da2b04c090f6c62b2df") --Dlcname Motherless - Vacation Special: Episode 2
+setManifestid(4509180, "6742632935343152304", 330573536)
+--Missing Dlcs
+--Dlcname Motherless - What If? - Gissell DLC
