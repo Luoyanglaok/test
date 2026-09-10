f3ff036d63000afdd30671ae6cc88def479bde06
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 13:46:53 +0800

    add 3201300.lua

---
+++ 3201300.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3201300.lua
+--Gamename AppID_3201300
+addappid(3201300) --Mainappid AppID_3201300
+addtoken(3201300, "15177105963939341920")
+addappid(3201301, 1, "4b1bbc40d807285e6e8f424e68311c76eaf193dc146fd672f24b6bbe9963196f") --Main Windows Depot AppID_3201300
