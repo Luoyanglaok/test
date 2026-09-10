cf9869b9e817aa1bd3133948aff5785907f0650b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:48:47 +0800

    add 427290.lua

--- 427290.lua
+++ 427290.lua
@@ -1,5 +1,15 @@
-addappid(427290)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(427291,0,"cdc71f514e81cdcdf2205f20a95908bd1e90d12bad79d5089096ba9966113524")
-setManifestid(427291,"1028370181203077487")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 427290.lua
+--Gamename Vampyr
+addappid(427290, 1, "1a2b070187a7685c449ed27092ff3ddd59f0b71b5fa75cbc3438cd700fc3e206") --Mainappid Vampyr
+addappid(427291, 1, "cdc71f514e81cdcdf2205f20a95908bd1e90d12bad79d5089096ba9966113524") --Main Windows Depot Vampyr
+setManifestid(427291, "1028370181203077487", 15093090640)
+addappid(427292, 1, "80a79c5f1d29c5c401c0a4cd953c789be75bff497c14e21b8a578d21a354f0d5") --Main Windows Depot Vampyr
+setManifestid(427292, "6200167262384707298", 109624224)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Vampyr - The Hunters Heirlooms
