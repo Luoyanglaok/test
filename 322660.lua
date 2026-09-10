831c7633f777d0172d8fe008cb35d3fa956e4a63
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:37:34 +0800

    add 322660.lua

--- 322660.lua
+++ 322660.lua
@@ -1,5 +1,7 @@
-addappid(322660)
-addappid(322660,0,"cc56b394152b0d78bb4d6e147716a463b70ae573e8cf6f2ef28ebe359f97860d")
-setManifestid(322660,"5499930042188336604")
-addappid(322661,0,"6c8f5e0423cd4e9f34a9ff9b9f4bc95464e3fd93f03bb699be87ec06402cefc0")
-setManifestid(322661,"4078660603895799229")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 322660.lua
+--Gamename The Binding of Isaac: Rebirth - Soundtrack
+addappid(322660, 1, "cc56b394152b0d78bb4d6e147716a463b70ae573e8cf6f2ef28ebe359f97860d") --Mainappid The Binding of Isaac: Rebirth - Soundtrack
+setManifestid(322660, "5499930042188336604", 349532032)
+addappid(322661, 1, "6c8f5e0423cd4e9f34a9ff9b9f4bc95464e3fd93f03bb699be87ec06402cefc0") --Main Windows Depot The Binding of Isaac: Rebirth - Soundtrack
+setManifestid(322661, "4078660603895799229", 186090208)
