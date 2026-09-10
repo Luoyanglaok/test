bfa364956eb47212db8bd62c98eef09ef211ffdd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:40:45 +0800

    add 4356430.lua

---
+++ 4356430.lua
@@ -0,0 +1,27 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4356430.lua
+--Gamename NBA 2K27
+addappid(4356430, 1, "da0457cd701ea98a184543bf867722ae607be456e2fd7da486ff49e8e7275c09") --Mainappid NBA 2K27
+addtoken(4356430, "13716260120817756824")
+addappid(4356431, 1, "a1ccdda87579dfa269b9015cc7ba18b7eb8339848b5eb408bfea8bdfb31eb8a4") --Main Windows Depot NBA 2K27
+setManifestid(4356431, "2271080181003010218", 79258324384)
+--Dlcname NBA 2K27 - High Resolution Textures
+addtoken(4687050, "7614811867666966860")
+addappid(4687050, 1, "c96a0954f3836680afb31e3b837546bb92606ccaebcc3e1bf8cd3761b8221c25") --Dlcname NBA 2K27 - High Resolution Textures
+setManifestid(4687050, "4767089068737929486", 29541459872)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(4357560) --Dlcname NBA 2K27 Pro Pass: Season 1
+addtoken(4357560, "11490798856557241078")
+addappid(4357570) --Dlcname NBA 2K27 Hall of Fame Pass: Season 1
+addappid(4357580) --Dlcname NBA 2K27 MyCAREER Bonus Offer: Season 1
+addappid(4357590) --Dlcname NBA 2K27 MyTEAM Bonus Offer: Season 1
+addappid(4691140) --Dlcname NBA 2K27 Bonus Pack
+addtoken(4691140, "137551546670174222")
+addappid(4691150) --Dlcname NBA 2K27 Limited-Time Offer Bonus Pack
+addtoken(4691150, "17233423616215550412")
+addappid(4691180) --Dlcname NBA 2K27 Deluxe Bonus
+addtoken(4691180, "11900496500442026758")
+addappid(4691190) --Dlcname NBA 2K27 Ultra Bonus
+addtoken(4691190, "354420848406686800")
