9d0b7b5d35446e86e62dbe3061350ddf0885e751
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:46:49 +0800

    add 2015270.lua

--- 2015270.lua
+++ 2015270.lua
@@ -1,4 +1,9 @@
-addappid(2015270)
-setManifestid(228989,"3514306556860204959")
-addappid(2015271,0,"52e97957aab04bacd50fec8149cc4c447b6cfdf455482b37ab728cb7d14779fe")
-setManifestid(2015271,"2783645635724920218")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2015270.lua
+--Gamename Rotwood
+addappid(2015270, 1, "256d70eace742a20b4ee30690205b3c12b8b1aa7555febefff2c21904dc316ac") --Mainappid Rotwood
+addappid(2015271, 1, "52e97957aab04bacd50fec8149cc4c447b6cfdf455482b37ab728cb7d14779fe") --Main Windows Depot Rotwood
+setManifestid(2015271, "6908691748284527848", 1865258592)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
