87cf71c0187ffb708cfe56dff9bb3642d33f235c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:58:44 +0800

    add 1432860.lua

--- 1432860.lua
+++ 1432860.lua
@@ -1,7 +1,63 @@
-addappid(1432860)
-addappid(1432861,0,"699136c056b2ebdf81900c0ca6bffabdf5b83899c1d451837b3fbc945c986d86")
-setManifestid(1432861,"1078850793895706526")
-addappid(1432862,0,"26c2186d937d6a9271998f3740326a40582ea51a565d46f1aa620cb228c104e2")
-setManifestid(1432862,"791064351886411741")
-addappid(2305662,0,"fd3834626fe0fb254f567e19c07831ea4684b5b1dc4360fdd0fd76a99491acd5")
-setManifestid(2305662,"5013926395044398502")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1432860.lua
+--Gamename Sun Haven
+addappid(1432860, 1, "1c4c95617cace6c87b862d88f8caf88757ce5976ede0edbb3bef6bda4fae0a20") --Mainappid Sun Haven
+addappid(1432861, 1, "699136c056b2ebdf81900c0ca6bffabdf5b83899c1d451837b3fbc945c986d86") --Main Windows Language Depot English Sun Haven
+setManifestid(1432861, "2994138885476124310", 1218425696)
+addappid(1432862, 1, "26c2186d937d6a9271998f3740326a40582ea51a565d46f1aa620cb228c104e2") --Main Macos Language Depot English Sun Haven
+setManifestid(1432862, "4552976601978599813", 1214727072)
+--Dlcname Sun Haven Soundtrack Vol. 1
+addappid(2305660) --Dlcname Sun Haven Soundtrack Vol. 1
+addappid(2305662, 1, "fd3834626fe0fb254f567e19c07831ea4684b5b1dc4360fdd0fd76a99491acd5") --Dlc Windows Depot Sun Haven Soundtrack Vol. 1
+setManifestid(2305662, "5013926395044398502", 241214944)
+--Dlcname Sun Haven Soundtrack Vol. 2
+addappid(2375000) --Dlcname Sun Haven Soundtrack Vol. 2
+addappid(2375001, 1, "9e35d8896c45172937a1b2ffe2f95285b7be1a250c0e89ce0d9bdcf4cca754d5") --Dlc Windows Depot Sun Haven Soundtrack Vol. 2
+setManifestid(2375001, "7475523022309329780", 176604336)
+--Share Depots
+addappid(2375130) --Dlcname Sun Haven: Dreamy Pet Pack
+addappid(2375140) --Dlcname Sun Haven: Spooky Pet Pack
+addappid(2401500) --Dlcname Sun Haven: Sweet Mounts
+addappid(2401501) --Dlcname Sun Haven: Wicked Mounts Pack
+addappid(2488150) --Dlcname Sun Haven: Ocean Shores Pack
+addappid(2488151) --Dlcname Sun Haven: Set Sail Pack
+addappid(2488152) --Dlcname Sun Haven: Mushy Pack
+addappid(2488153) --Dlcname Sun Haven: Free Snaccoon Costume
+addappid(2659990) --Dlcname Sun Haven: Dreamy Ram Pack
+addappid(2660000) --Dlcname Sun Haven: Funky Monkey Pack
+addappid(2660010) --Dlcname Sun Haven: Cyberpop Pack
+addappid(2660020) --Dlcname Sun Haven: Rock 'n' Roll Pack
+addappid(2660030) --Dlcname Sun Haven: Spirit Petal Pack
+addappid(2660040) --Dlcname Sun Haven: Trick or Treat Pack
+addappid(2743900) --Dlcname Sun Haven: Pop Sensation Pack
+addappid(2743910) --Dlcname Sun Haven: Starlight Pack
+addappid(2743920) --Dlcname Sun Haven: Toy Pack
+addappid(2743930) --Dlcname Sun Haven: Snow Day Pack
+addappid(2743940) --Dlcname Sun Haven: Tis the Season Pack
+addappid(2743950) --Dlcname Sun Haven: Sugar Rush Pack
+addappid(2952640) --Dlcname Sun Haven: Deep Sea Pack
+addappid(2952650) --Dlcname Sun Haven: Celestial Pack
+addappid(2952660) --Dlcname Sun Haven: Bloom and Doom Pack
+addappid(2952670) --Dlcname Sun Haven: Claws and Paws Pack
+addappid(3139200) --Dlcname Sun Haven: Tea Temple Pack
+addappid(3139210) --Dlcname Sun Haven: Emerald Elegance Pack
+addappid(3139220) --Dlcname Sun Haven: Flames and Ashes Pack
+addappid(3139230) --Dlcname Sun Haven: Squeaky Clean Pack
+addappid(3139240) --Dlcname Sun Haven: Snow Yeti Pack
+addappid(3819940) --Dlcname Sun Haven: Once Upon a Time Pack
+addappid(3819950) --Dlcname Sun Haven: Cyberknight Pack
+addappid(3819960) --Dlcname Sun Haven: Radiant Royalty Pack
+addappid(3819970) --Dlcname Sun Haven: Astral Pack
+addappid(3819980) --Dlcname Sun Haven: Myths and Muses Pack
+addappid(4209190) --Dlcname Sun Haven: Gearwork Glamour Pack
+addappid(4209200) --Dlcname Sun Haven: Día de Delights Pack
+addappid(4209210) --Dlcname Sun Haven: Cozy Cottage Pack
+addappid(4209220) --Dlcname Sun Haven: Garden Party Pack
+addappid(4534970) --Dlcname Sun Haven: Sakura Pet Pack
+addappid(4534980) --Dlcname Sun Haven: Snack Pet Pack
+addappid(4534990) --Dlcname Sun Haven: Sweet Pet Pack
+addappid(1559320) --Dlcname AppID 1559320
+addappid(1667600) --Dlcname AppID 1667600
+addappid(1667610) --Dlcname AppID 1667610
+addappid(1667620) --Dlcname AppID 1667620
+addappid(1667660) --Dlcname AppID 1667660
