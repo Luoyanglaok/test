b5ddfcfe964fc71df600eb940c6ad31394aa7365
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:40:43 +0800

    add 2460070.lua

--- 2460070.lua
+++ 2460070.lua
@@ -4,12 +4,6 @@
 addappid(2460070, 1, "d48238379879b59354a5be33ed48764c0e413f4a8bb2baef7e0d072aca71081c") --Mainappid Half-Demon Shinobi
 addappid(2460071, 1, "93a04e611c871594f0084a3866fb12bdebd033ac2cdecc94d596b9020313d7f3") --Main Windows Depot Half-Demon Shinobi
 setManifestid(2460071, "7979384062751592402", 3503944896)
---Dlcname Half-Demon Shinobi Soundtrack
-addappid(3331470) --Dlcname Half-Demon Shinobi Soundtrack
-addappid(3331471, 1, "fa0f4a7243419084f247574033cbd118c40d37114e0d91dd87331d80c4da49da") --Dlc Windows Depot Half-Demon Shinobi Soundtrack
-setManifestid(3331471, "837945450337986816", 160980896)
-addappid(3331472, 1, "1b9524cf6e42545496bcb5b737663450b35ba58b6c5cd4dba84560c29add81dc") --Dlc Windows Depot Half-Demon Shinobi Soundtrack
-setManifestid(3331472, "8307966969248000721", 898909376)
 --Dlcname Half-Demon Shinobi Artbook
 addappid(3331480) --Dlcname Half-Demon Shinobi Artbook
 addappid(3331481, 1, "4645bdde41aca3d181b1c41abe7bc8fe7eec43cb2a640b13110ed1494cd29f7a") --Dlc Windows Depot Half-Demon Shinobi Artbook
