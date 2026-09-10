71ce442a2dd1471e89b4cc42742235965bfeb83d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:25:13 +0800

    add 1293830.lua

--- 1293830.lua
+++ 1293830.lua
@@ -1,9 +1,76 @@
-addappid(1293830)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1293831,0,"c6be4a0719d73ba5201e778b7e44d6c9c0e1b4baf33104df4d0da1729751cf7f")
-setManifestid(1293831,"7960876371477493357")
-addappid(1443091,0,"cacce7495ef657d5a7e8ae0f89e83fcdbb3f7146e25f8dcf9a4843a74e711aba")
-setManifestid(1443091,"1638530373648163875")
-addappid(1443092,0,"bff2e67bf6bdf9001760367fcb99b7d1fba41f17441c648568895843464e2dd3")
-setManifestid(1443092,"2636644777724144367")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1293830.lua
+--Gamename Forza Horizon 4
+addappid(1293830, 1, "38cac36fb9b491054ccc33f95bd2b7b11b41db73d65e58b1744cb394cb3c017b") --Mainappid Forza Horizon 4
+addappid(1293831, 1, "c6be4a0719d73ba5201e778b7e44d6c9c0e1b4baf33104df4d0da1729751cf7f") --Main Windows Depot Forza Horizon 4
+setManifestid(1293831, "7960876371477493357", 83952363680)
+--Dlcname Forza Horizon 4: Fortune Island
+addappid(1443091, 1, "cacce7495ef657d5a7e8ae0f89e83fcdbb3f7146e25f8dcf9a4843a74e711aba") --Dlcname Forza Horizon 4: Fortune Island
+setManifestid(1443091, "1638530373648163875", 7223477360)
+--Dlcname Forza Horizon 4: LEGO® Speed Champions
+addappid(1443092, 1, "bff2e67bf6bdf9001760367fcb99b7d1fba41f17441c648568895843464e2dd3") --Dlcname Forza Horizon 4: LEGO® Speed Champions
+setManifestid(1443092, "2636644777724144367", 4423992688)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1443010) --Dlcname AppID 1443010
+addappid(1443011) --Dlcname AppID 1443011
+addappid(1443015) --Dlcname Forza Horizon 4: 1938 MG TA Midget
+addappid(1443016) --Dlcname Forza Horizon 4: 2017 Koenigsegg Agera RS
+addappid(1443017) --Dlcname Forza Horizon 4: 2016 Honda Civic Coupe GRC
+addappid(1443018) --Dlcname Forza Horizon 4: 2018 Ford Deberti Design Mustang Fastback
+addappid(1443019) --Dlcname Forza Horizon 4: 2019 Porsche 911 Carrera S
+addappid(1443020) --Dlcname Forza Horizon 4: 1993 Porsche 968 Turbo S
+addappid(1443021) --Dlcname Forza Horizon 4: 2005 Honda NSX-R GT
+addappid(1443022) --Dlcname Forza Horizon 4: 1966 Hillman Imp
+addappid(1443023) --Dlcname Forza Horizon 4: 1968 Ford Mustang GT 2+2 Fastback
+addappid(1443024) --Dlcname Forza Horizon 4: Ford Transit
+addappid(1443025) --Dlcname Forza Horizon 4: 1959 Cadillac Eldorado Biarritz Convertible
+addappid(1443026) --Dlcname Forza Horizon 4: 2018 TVR Griffith
+addappid(1443027) --Dlcname Forza Horizon 4: 2019 BMW i8 Roadster
+addappid(1443028) --Dlcname Forza Horizon 4: 2002 Mazda RX-7 Spirit R Type-A
+addappid(1443029) --Dlcname Forza Horizon 4: 1962 Triumph TR3B
+addappid(1443040) --Dlcname Forza Horizon 4: 2010 Vauxhall Insignia VXR
+addappid(1443041) --Dlcname Forza Horizon 4: 2014 McLaren 650 Super Sport Spyder
+addappid(1443042) --Dlcname Forza Horizon 4: 1953 Jaguar C-Type
+addappid(1443043) --Dlcname Forza Horizon 4: 1974 Honda CivicRS
+addappid(1443044) --Dlcname Forza Horizon 4: 2018 Chevrolet Deberti Design DriftTruck
+addappid(1443045) --Dlcname Forza Horizon 4: 2018 Aston Martin Vantage
+addappid(1443046) --Dlcname Forza Horizon 4: 1965 Peel Trident
+addappid(1443047) --Dlcname Forza Horizon 4: 2018 Chevrolet Camaro ZL1 1LE
+addappid(1443048) --Dlcname Forza Horizon 4: 2019 Chevrolet Corvette ZR1
+addappid(1443049) --Dlcname Forza Horizon 4: 2018 Morgan Aero GT
+addappid(1443050) --Dlcname Forza Horizon 4: 1970 Triumph TR6 PI
+addappid(1443051) --Dlcname Forza Horizon 4: 2017 Ferrari GTC4Lusso
+addappid(1443052) --Dlcname Forza Horizon 4: 1972 Lamborghini Jarama S
+addappid(1443053) --Dlcname Forza Horizon 4: 1966 Volkswagen Double Cab Pick-Up
+addappid(1443054) --Dlcname Forza Horizon 4: 2018 Can-Am Maverick X3 X RS Turbo R
+addappid(1443055) --Dlcname Forza Horizon 4: 1929 Mercedes-Benz SSK
+addappid(1443056) --Dlcname Forza Horizon 4: 1985 Porsche #186 959 Paris-Dakar
+addappid(1443057) --Dlcname Forza Horizon 4: 2018 Nissan SentraNismo
+addappid(1443058) --Dlcname Forza Horizon 4: 2018 Alfa Romeo Stelvio Quadrifoglio
+addappid(1443059) --Dlcname Forza Horizon 4: 2005 Ferrari FXX
+addappid(1443060) --Dlcname Forza Horizon 4: 2004 Vauxhall VX220
+addappid(1443061) --Dlcname Forza Horizon 4: 1993 Hoonigan Ford Escort Cosworth Group A
+addappid(1443062) --Dlcname Forza Horizon 4: Lamborghini Gallardo LP570-4 Spyder Performante
+addappid(1443063) --Dlcname Forza Horizon 4: 1959 Porsche 356A Coupe
+addappid(1443064) --Dlcname Forza Horizon 4: 1963 Opel Kadett A
+addappid(1443065) --Dlcname Forza Horizon 4: 1977 Hoonigan Ford Gymkhana 10 F-150
+addappid(1443066) --Dlcname Forza Horizon 4: 2003 Honda S2000
+addappid(1443080) --Dlcname Forza Horizon 4: Treasure Map
+addappid(1443081) --Dlcname AppID 1443081
+addappid(1443082) --Dlcname AppID 1443082
+addappid(1443083) --Dlcname Forza Horizon 4: Best of Bond Car Pack
+addappid(1443084) --Dlcname Forza Horizon 4: Barrett-Jackson Car Pack
+addappid(1443085) --Dlcname AppID 1443085
+addappid(1443086) --Dlcname AppID 1443086
+addappid(1443087) --Dlcname AppID 1443087
+addappid(1443088) --Dlcname AppID 1443088
+addappid(1443089) --Dlcname AppID 1443089
+addappid(1443090) --Dlcname AppID 1443090
+addappid(1443093) --Dlcname AppID 1443093
+addappid(1530010) --Dlcname AppID 1530010
+addappid(1530011) --Dlcname AppID 1530011
+addappid(1530012) --Dlcname AppID 1530012
