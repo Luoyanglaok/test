60717583317145a0a5e3a0eef0f4f22e5c43836a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:41:37 +0800

    add 3618390.lua

---
+++ 3618390.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3618390.lua
+--Gamename Gamblers Table
+addappid(3618390, 1, "a4ed71e9cece916bc3a74c744d76a557a2d3b0e967f770bc7dad12b181b3ae28") --Mainappid Gamblers Table
+addappid(3618391, 1, "af524ae28cb634f861e43a8dc0f9dbc6002bb303ec8e8167e02a8c92fa57d1e9") --Main Windows Depot Gamblers Table
+setManifestid(3618391, "1425048336041053119", 67868608)
+addappid(3618392, 1, "0f1983ef9df338918e261c22b47c0bfdb8940a902f198be8f0e872fa7d0083ce") --Main Linux Depot Gamblers Table
+setManifestid(3618392, "5859985964098726193", 64795120)
+addappid(4218840) --Dlcname Gamblers Table - Supporter Pack
