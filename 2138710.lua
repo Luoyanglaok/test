0f8d28b087d3961647b13dbec2220ed7dc31ec0a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:12:55 +0800

    add 2138710.lua

--- 2138710.lua
+++ 2138710.lua
@@ -1,7 +1,15 @@
-addappid(2138710)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2138711,0,"640993870daafc721632280e9e612a93f957d22bd8a16dcc526c3b8f73be0e33")
-setManifestid(2138711,"5545504035024010382")
-addappid(2253841,0,"27dc031b34fbcd34f1a8d7fd5658680f030c63aa34e8cfeb1a6c982888552d7f")
-setManifestid(2253841,"3893356194676043314")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2138710.lua
+--Gamename Sifu
+addappid(2138710, 1, "74d4a05e74098d220d35e2bbba100a19cbadaeed357bb503d152a1b4294219ca") --Mainappid Sifu
+addappid(2138711, 1, "640993870daafc721632280e9e612a93f957d22bd8a16dcc526c3b8f73be0e33") --Main Windows Depot Sifu
+setManifestid(2138711, "5545504035024010382", 22697896976)
+--Dlcname Sifu Digital Official Artbook
+addappid(2253841, 1, "27dc031b34fbcd34f1a8d7fd5658680f030c63aa34e8cfeb1a6c982888552d7f") --Dlcname Sifu Digital Official Artbook
+setManifestid(2253841, "3893356194676043314", 31170208)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2231950) --Dlcname Sifu - Deluxe Cosmetic Pack
