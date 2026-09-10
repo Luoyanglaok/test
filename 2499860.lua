c05cbb71514de1af52cb5ea6d766143d3841490d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:56:45 +0800

    add 2499860.lua

---
+++ 2499860.lua
@@ -0,0 +1,27 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2499860.lua
+--Gamename DRAGON QUEST VII Reimagined
+addappid(2499860, 1, "1e58a20f0aa0e0d214a207103d0d8042cd3d432a0ecf06e62a381366aa724dbe") --Mainappid DRAGON QUEST VII Reimagined
+addappid(2499861, 1, "78d85bb7957e80d4fac0655a65ff9bee61dafc06c753de2713726e9860d6a860") --Main Windows Depot DRAGON QUEST VII Reimagined
+setManifestid(2499861, "6283106044578104469", 6585212992)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3257600) --Dlcname DRAGON QUEST VII Reimagined: The Road of Regal Wretches
+addtoken(3257600, "619892886756003504")
+addappid(3378680) --Dlcname DRAGON QUEST VII Reimagined: Luminary's Livery
+addtoken(3378680, "1991433107608930159")
+addappid(3378690) --Dlcname DRAGON QUEST VII Reimagined: Jam-Packed Swag Bag
+addtoken(3378690, "10576518879816565189")
+addappid(3378700) --Dlcname DRAGON QUEST VII Reimagined: Early Bird Bonus: Trodain Togs, Seed of Proficiency x3
+addtoken(3378700, "9317591769083609258")
+addappid(3378720) --Dlcname DRAGON QUEST VII Reimagined: Silver Slime Shield
+addtoken(3378720, "1107469435969586498")
+addappid(3378750) --Dlcname AppID 3378750
+addappid(3378760) --Dlcname DRAGON QUEST VII Reimagined: Item Set: Gold Bracer, Seed of Magic
+addappid(3543080) --Dlcname DRAGON QUEST VII Reimagined: White Wolf Costume
+addtoken(3543080, "2047583276778140402")
+addappid(3995580) --Dlcname DRAGON QUEST VII Reimagined: Additional DLC Sets
+addtoken(3995580, "14307469858210407774")
