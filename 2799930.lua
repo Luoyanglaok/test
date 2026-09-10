e17dadbb2f8df5ba71d5258ae2cf8e69fbee5fa4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:35:31 +0800

    add 2799930.lua

--- 2799930.lua
+++ 2799930.lua
@@ -1,7 +1,15 @@
-addappid(2799930)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229007,"4477590687906973371")
-setManifestid(229033,"2059065101492814639")
-addappid(2799931,0,"96045378ae72057e53d00b38e6f124e34b6376bffbdbae9b3ce42ecb17804e30")
-setManifestid(2799931,"7053851393523483437")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2799930.lua
+--Gamename 灵境·修仙界
+addappid(2799930, 1, "20a9a69e13a3dee0f494ca678d9eeb055b974ecb4b0808ec8ee82db94559f071") --Mainappid 灵境·修仙界
+addappid(2799931, 1, "96045378ae72057e53d00b38e6f124e34b6376bffbdbae9b3ce42ecb17804e30") --Main Windows Depot 灵境·修仙界
+setManifestid(2799931, "5302570184008067617", 24235145712)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
