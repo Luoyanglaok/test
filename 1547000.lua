0b2cb78350e44318c9b118f546acee6e84486437
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:13:21 +0800

    add 1547000.lua

--- 1547000.lua
+++ 1547000.lua
@@ -1,7 +1,13 @@
-addappid(1547000)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1547001,0,"625ab457b256e557dc28337fec8a01b39731624ee6008b92670ac977c2691806")
-setManifestid(1547001,"6814041191190748315")
-addappid(1899671,0,"b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b")
-setManifestid(1899671,"1313721849634300954")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1547000.lua
+--Gamename Grand Theft Auto: San Andreas – The Definitive Edition
+addappid(1547000, 1, "ca7f99c2b9d94c16db93def0b731d526d31af35942c4737a9706cfdb3849a9ad") --Mainappid Grand Theft Auto: San Andreas – The Definitive Edition
+addappid(1547001, 1, "625ab457b256e557dc28337fec8a01b39731624ee6008b92670ac977c2691806") --Main Windows Depot Grand Theft Auto: San Andreas – The Definitive Edition
+setManifestid(1547001, "6814041191190748315", 19385204112)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1899671, 1, "b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b") --Share Windows Depot RGL/SC
+setManifestid(1899671, "3914969219305676290", 209058416)
