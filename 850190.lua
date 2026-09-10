557e3d179bddeb948874cd83fc0b06d12770a562
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:16:33 +0800

    add 850190.lua

--- 850190.lua
+++ 850190.lua
@@ -1,5 +1,17 @@
-addappid(850190)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(850191,0,"05fc8de1333cce70dc8b8f7f2e79735da8ba77bc8d8a05a2ab711bdda428c90f")
-setManifestid(850191,"6891584188642398164")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 850190.lua
+--Gamename Goat Simulator 3
+addappid(850190, 1, "243ce3059f2b83879ca2ad1fbcca40a41ea4f75349082d44beee4f36e5dacd57") --Mainappid Goat Simulator 3
+addappid(850191, 1, "05fc8de1333cce70dc8b8f7f2e79735da8ba77bc8d8a05a2ab711bdda428c90f") --Main Windows Depot Goat Simulator 3
+setManifestid(850191, "7315100551114631352", 6003784512)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2666450) --Dlcname Goat Simulator 3 - Digital Downgrade
+addappid(2809180) --Dlcname Goat Simulator 3 - Multiverse of Nonsense
+addappid(3204290) --Dlcname Goat Simulator 3 - Hocus Pocus Pack
+addappid(3299590) --Dlcname Goat Simulator 3 - Super Duper Pack
+addappid(3708030) --Dlcname Goat Simulator 3 – Baadlands: Furry Road
+addappid(4370220) --Dlcname Goat Simulator 3 – Buck to the Future Pack
