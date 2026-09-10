59356642a22637efbecc2ade1f1b65adc82b91a3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 23:53:04 +0800

    add 1623730.lua

--- 1623730.lua
+++ 1623730.lua
@@ -1,7 +1,11 @@
-addappid(1623730)
-addappid(2771110)
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(1623731,"868868087024202254")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1623730.lua
+--Gamename Palworld
+addappid(1623730, 1, "214e5bcf1036d2bc62193cf2bc15e148899db0ed4799c3a2c0d41f2490d136f5") --Mainappid Palworld
+addappid(1623731, 1, "7a422988135296505205dd489945d4b17ed7414734dd627744adac84aade7b73") --Main Windows Depot Palworld
+setManifestid(1623731, "868868087024202254", 35943136912)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
