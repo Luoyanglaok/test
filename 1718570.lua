dac24ef4e0df517070c6bbcb65d47567b6ab4158
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:04:11 +0800

    add 1718570.lua

--- 1718570.lua
+++ 1718570.lua
@@ -1,5 +1,10 @@
-addappid(1718570)
-addappid(1718571,0,"5340c7c9ef95f91f6e844d2e4f42b9b9a18b873a71a20516b0a1f23cb65c86db")
-setManifestid(1718571,"3282200606953518320")
-addappid(2669361,0,"d0945427a75897ebf551e5d57660b66c67e276511b1e0bef90039bf01c0e3de9")
-setManifestid(2669361,"1245237997762892723")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1718570.lua
+--Gamename ASTLIBRA Revision
+addappid(1718570, 1, "34543c2be766f4100c9927bb49b7aa7ec18ad1dcd9605bc95015a2482e1172cb") --Mainappid ASTLIBRA Revision
+addappid(1718571, 1, "5340c7c9ef95f91f6e844d2e4f42b9b9a18b873a71a20516b0a1f23cb65c86db") --Main Windows Depot ASTLIBRA Revision
+setManifestid(1718571, "100192253820083066", 2711647712)
+--Dlcname ASTLIBRA Gaiden: The Cave of Phantom Mist
+addappid(2669360) --Dlcname ASTLIBRA Gaiden: The Cave of Phantom Mist
+addappid(2669361, 1, "d0945427a75897ebf551e5d57660b66c67e276511b1e0bef90039bf01c0e3de9") --Dlc Windows Depot ASTLIBRA Gaiden: The Cave of Phantom Mist
+setManifestid(2669361, "8833947623743784631", 2050690960)
