df1f9c5dd933e4fe2251cf39e7dddfb4a0a41a41
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:55:47 +0800

    add 753640.lua

--- 753640.lua
+++ 753640.lua
@@ -1,5 +1,19 @@
-addappid(753640)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(753641,0,"57452658900ae11ac7c0e91c17d64353fa62936759ca202f4ecbbc9f29cf4b60")
-setManifestid(753641,"5293038641177186185")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 753640.lua
+--Gamename Outer Wilds
+addappid(753640, 1, "71c9b9745ab2ff71228d1fa444fb74b9f9f04b129ed339af354bb409fc820c33") --Mainappid Outer Wilds
+addappid(753641, 1, "57452658900ae11ac7c0e91c17d64353fa62936759ca202f4ecbbc9f29cf4b60") --Main Windows Depot Outer Wilds
+setManifestid(753641, "2759471679100125861", 3576069440)
+--Dlcname Outer Wilds - Original Soundtrack
+addappid(1286760) --Dlcname Outer Wilds - Original Soundtrack
+addappid(1286761, 1, "fe1f5c1aa43c2b20ced2cd057779325ecce557cb042e9e7b0495f4121835f040") --Dlc Windows Depot Outer Wilds - Original Soundtrack
+setManifestid(1286761, "2113840346735518444", 149562688)
+addappid(1286762, 1, "072b2e0718e046f8123748103b16f4429876b372296e1a1f1abaa03131ca6ec8") --Dlc Windows Depot Outer Wilds - Original Soundtrack
+setManifestid(1286762, "261450155689233405", 1109471184)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Outer Wilds - Echoes of the Eye
