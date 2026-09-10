a9cbc4d2a9baa5461ab1c7ae9a5455c68d72a3eb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:15:49 +0800

    add 2835570.lua

--- 2835570.lua
+++ 2835570.lua
@@ -1,5 +1,8 @@
-addappid(2835570)
-addappid(2835572,0,"d62599196078743d30d665ddd351d433f153e6e3ed12cdc8a4021ee8b1fc8b30")
-setManifestid(2835572,"7472777116297885824")
-addappid(2835573,0,"9c48f34a0e831c56623c49c66403647f8c08682d26ee6bee1d56c470bb97f6cb")
-setManifestid(2835573,"9007644155059772192")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2835570.lua
+--Gamename Buckshot Roulette
+addappid(2835570, 1, "7593ca54f5e185d4066792db3db8590ea97bc6654ef6507a4320b1d566f0afc5") --Mainappid Buckshot Roulette
+addappid(2835572, 1, "d62599196078743d30d665ddd351d433f153e6e3ed12cdc8a4021ee8b1fc8b30") --Main Windows Depot Buckshot Roulette
+setManifestid(2835572, "7472777116297885824", 900308448)
+addappid(2835573, 1, "9c48f34a0e831c56623c49c66403647f8c08682d26ee6bee1d56c470bb97f6cb") --Main Linux Depot Buckshot Roulette
+setManifestid(2835573, "9007644155059772192", 906276960)
