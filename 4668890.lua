6c76bed494ab4de5dfab16ac2f715f69e2c9c3fb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:02:42 +0800

    add 4668890.lua

---
+++ 4668890.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4668890.lua
+--Gamename Mall Together
+addappid(4668890) --Mainappid Mall Together
+addappid(4668891, 1, "9dc3bedf92fa9b0a20fcd0fd74bd85b4985af941ea8db23d8a6ead97d7845a78") --Main Windows Depot Mall Together
+setManifestid(4668891, "7931750230780161745", 2968009136)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
