7465163ef9873a66025153118dd8424ed8abd152
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:16:36 +0800

    add 2875140.lua

--- 2875140.lua
+++ 2875140.lua
@@ -1,3 +1,6 @@
-addappid(2875140)
-addappid(2875141,0,"8b3e91f1ed5f67b93b42edabf455c00936c84e3d5c1159b9ac55434e91146630")
-setManifestid(2875141,"7018735392280437917")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2875140.lua
+--Gamename 失业了，我获得了亿万游戏财产！
+addappid(2875140) --Mainappid 失业了，我获得了亿万游戏财产！
+addappid(2875141, 1, "8b3e91f1ed5f67b93b42edabf455c00936c84e3d5c1159b9ac55434e91146630") --Main Windows Depot 失业了，我获得了亿万游戏财产！
+setManifestid(2875141, "7018735392280437917", 43613247680)
