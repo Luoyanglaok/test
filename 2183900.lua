6ea3ab5f05650da3d7f19aaf3a711924a501ff78
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:53:43 +0800

    add 2183900.lua

--- 2183900.lua
+++ 2183900.lua
@@ -1,9 +1,47 @@
-addappid(2183900)
-addtoken(2183900,"12779763340560861411")
-setManifestid(228989,"550968249685141759")
-addappid(2183901,0,"8df236a6a5084039477362b31157e7e0eb66554a744762d6782c544ab64d0c4e")
-setManifestid(2183901,"1402240209931868812")
-addappid(3202690,0,"00c47f82b08192958b11307abb87c755003eb8dfae75063373e379c296b26083")
-setManifestid(3202690,"7038592287560796420")
-addappid(3212040,0,"e3818ce0f203d78eed2647e6364bd19a702fd8d7a01230f77a038e0fd26b0ffb")
-setManifestid(3212040,"7457655780363166458")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2183900.lua
+--Gamename Warhammer 40,000: Space Marine 2
+addappid(2183900, 1, "227e48320b1cf15063a473da684cea19ebcd76548c0d50a505cdd0a78864ed0a") --Mainappid Warhammer 40,000: Space Marine 2
+addtoken(2183900, "12779763340560861411")
+addappid(2183901, 1, "8df236a6a5084039477362b31157e7e0eb66554a744762d6782c544ab64d0c4e") --Main Windows Depot Warhammer 40,000: Space Marine 2
+setManifestid(2183901, "6015590042991520040", 76969260304)
+--Dlcname Warhammer 40,000: Space Marine 2 - 4K Texture Pack
+addappid(3202690, 1, "00c47f82b08192958b11307abb87c755003eb8dfae75063373e379c296b26083") --Dlcname Warhammer 40,000: Space Marine 2 - 4K Texture Pack
+setManifestid(3202690, "8603083004664865563", 128903793728)
+--Dlcname Warhammer 40,000: Space Marine 2 Soundtrack
+addappid(3212020) --Dlcname Warhammer 40,000: Space Marine 2 Soundtrack
+addappid(3212021, 1, "01e7d7bd4abcb8946df7226afc08075b26c702659cf81018580d5f8e597b6c93") --Dlc Windows Depot Warhammer 40,000: Space Marine 2 Soundtrack
+setManifestid(3212021, "4575728820807956562", 175900656)
+addappid(3212022, 1, "924f67387bb12cfc2988a7decd11c8ace7b5b38ae9d07397e246b36d01c1e5c5") --Dlc Windows Depot Warhammer 40,000: Space Marine 2 Soundtrack
+setManifestid(3212022, "2274340000490854200", 671289856)
+addappid(3212023, 1, "ad43f8052145e7fad816f6b2299a1ae217fc78bd8e1c769cb0ad3e0272d843d0") --Dlc Windows Depot Warhammer 40,000: Space Marine 2 Soundtrack
+setManifestid(3212023, "4686517121168409350", 403437856)
+--Dlcname Warhammer 40,000: Space Marine 2 - Digital Artbook
+addappid(3212040, 1, "e3818ce0f203d78eed2647e6364bd19a702fd8d7a01230f77a038e0fd26b0ffb") --Dlcname Warhammer 40,000: Space Marine 2 - Digital Artbook
+setManifestid(3212040, "7457655780363166458", 112129136)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2486150) --Dlcname Warhammer 40,000: Space Marine 2 - Macragge’s Chosen
+addappid(2792140) --Dlcname Warhammer 40,000: Space Marine 2 - Ultramarines Champion Pack
+addappid(2792150) --Dlcname Warhammer 40,000: Space Marine 2 - Ultramarines Cosmetic Pack
+addappid(2792170) --Dlcname Warhammer 40,000: Space Marine 2 - Dark Angels Chapter Pack
+addappid(2792180) --Dlcname Warhammer 40,000: Space Marine 2 - Space Wolves Chapter Pack
+addappid(2792190) --Dlcname Warhammer 40,000: Space Marine 2 - White Scars Chapter Pack
+addappid(2792280) --Dlcname Warhammer 40,000: Space Marine 2 - Season Pass
+addappid(3274400) --Dlcname Warhammer 40,000: Space Marine 2 - Raven Guard Cosmetic Pack
+addappid(3274410) --Dlcname Warhammer 40,000: Space Marine 2 - Salamanders Champion Pack
+addappid(3274430) --Dlcname Warhammer 40,000: Space Marine 2 - Imperial Fists Champion Pack
+addappid(3274440) --Dlcname Warhammer 40,000: Space Marine 2 - Blood Angels Cosmetic Pack
+addappid(3871320) --Dlcname Warhammer 40,000: Space Marine 2 - Season Pass 2
+addappid(3871330) --Dlcname Warhammer 40,000: Space Marine 2 - Black Templars Champion Pack
+addappid(3871340) --Dlcname Warhammer 40,000: Space Marine 2 - Imperial Fists Cosmetic Pack
+addappid(3871350) --Dlcname Warhammer 40,000: Space Marine 2 - Blood Angels Champion Pack
+addappid(3871360) --Dlcname Warhammer 40,000: Space Marine 2 - Salamanders Cosmetic Pack
+addappid(3871370) --Dlcname Warhammer 40,000: Space Marine 2 - Raven Guard Champion Pack
+addappid(3871380) --Dlcname Warhammer 40,000: Space Marine 2 - Carcharodons Cosmetic Pack
+addappid(3871390) --Dlcname Warhammer 40,000: Space Marine 2 - Iron Hands Chapter Pack
+addtoken(3871390, "15555146775881414615")
+addappid(3871400) --Dlcname Warhammer 40,000: Space Marine 2 - Salamanders Champion Pack 2
+addappid(3871410) --Dlcname Warhammer 40,000: Space Marine 2 - Raptors Cosmetic Pack
+addappid(4247850) --Dlcname Warhammer 40,000: Space Marine 2 - Chapter Voice Pack 1
