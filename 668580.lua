171842320adf6a378922fec4923dbd448c942bcd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:46:22 +0800

    add 668580.lua

--- 668580.lua
+++ 668580.lua
@@ -1,5 +1,26 @@
-addappid(668580)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(668581,0,"b331448756a6614f7b9b52f56b71e329928370afb8034b8474e264ace0048928")
-setManifestid(668581,"1673467964514218551")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 668580.lua
+--Gamename Atomic Heart
+addappid(668580, 1, "5a1ed8d8d2a8110fde9be6e0ec543d96db83954f381710c70e330b5c010841e7") --Mainappid Atomic Heart
+addappid(668581, 1, "b331448756a6614f7b9b52f56b71e329928370afb8034b8474e264ace0048928") --Main Windows Depot Atomic Heart
+setManifestid(668581, "1879284477080962633", 85777880992)
+--Dlcname Atomic Heart - Original Soundtrack
+addappid(2303120) --Dlcname Atomic Heart - Original Soundtrack
+addappid(2303121, 1, "6fd3db1eba411d9ecc68741d7ebf12f38bfb738c8578dd71419af57f7b2f73c4") --Dlc Windows Depot Atomic Heart - Original Soundtrack
+setManifestid(2303121, "3587340790320923187", 835147072)
+addappid(2303122, 1, "49808584cc51f185a98e76678db9045fcfccc7474b9bccb847a92aa223ed8aff") --Dlc Windows Depot Atomic Heart - Original Soundtrack
+setManifestid(2303122, "41228753438295057", 5180338176)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2214820) --Dlcname Atomic Heart - Midas Glove Skin
+addappid(2214821) --Dlcname Atomic Heart - Annihilation Instinct
+addappid(2214822) --Dlcname Atomic Heart - Trapped in Limbo
+addappid(2214823) --Dlcname Atomic Heart - Enchantment Under the Sea
+addappid(2214824) --Dlcname Atomic Heart - Blood On Crystal
+addappid(2214825) --Dlcname Atomic Heart - Golden Age Weapon Skin Pack
+addtoken(2214825, "5039941091422940824")
+addappid(2214826) --Dlcname Atomic Heart - Digital Artbook
+addappid(2214830) --Dlcname Atomic Heart - Labor & Science Weapon Skin Pack
