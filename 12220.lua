af0dcac8a760ca46229378f9c44764a63c34a200
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:12:58 +0800

    add 12220.lua

--- 12220.lua
+++ 12220.lua
@@ -1,9 +1,18 @@
-addappid(12220)
-addappid(12222,0,"b5b5eda30f092696af90c38dececf04ce645b6c719190650068bddce5c49de1e")
-setManifestid(12222,"4873137157423983930")
-addappid(12221,0,"81ddb2112e643bbd3b6ba2afb6cb5befc29e6af279417b1fe869a913e6fb2131")
-setManifestid(12221,"3724368639462495704")
-addappid(12223,0,"982592ab99db3254cf53eba6b7cc8fb969d6bb444701a5b69d9c4013980153e6")
-setManifestid(12223,"5864218123665864372")
-addappid(12224,0,"eafa2330ab55d5ac009bc764b5a743ca0f8b066908589f75687b2f72ba2d5466")
-setManifestid(12224,"5159865615262522368")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12220.lua
+--Gamename Grand Theft Auto: Episodes from Liberty City
+addappid(12220, 1, "4f3323ebbd0b8a771afc0e44a381ef46ab01168da53f8b667c622e24198d8b99") --Mainappid Grand Theft Auto: Episodes from Liberty City
+addappid(12221, 1, "81ddb2112e643bbd3b6ba2afb6cb5befc29e6af279417b1fe869a913e6fb2131") --Main Windows Depot Grand Theft Auto: Episodes from Liberty City
+setManifestid(12221, "3724368639462495704", 14952916224)
+addappid(12222, 1, "b5b5eda30f092696af90c38dececf04ce645b6c719190650068bddce5c49de1e") --Main Windows Depot Grand Theft Auto: Episodes from Liberty City
+setManifestid(12222, "4873137157423983930", 139711360)
+addappid(12223, 1, "982592ab99db3254cf53eba6b7cc8fb969d6bb444701a5b69d9c4013980153e6") --Main Windows Language Depot English Grand Theft Auto: Episodes from Liberty City
+setManifestid(12223, "5864218123665864372", 0)
+addappid(12224, 1, "eafa2330ab55d5ac009bc764b5a743ca0f8b066908589f75687b2f72ba2d5466") --Main Windows Language Depot French Grand Theft Auto: Episodes from Liberty City
+setManifestid(12224, "5159865615262522368", 0)
+addappid(12225, 1, "17e188ec9aeb7eeee849a65b009787a8cc415781466a517ce5f6c27cbc9e4136") --Main Windows Language Depot Italian Grand Theft Auto: Episodes from Liberty City
+setManifestid(12225, "2410949247245578384", 0)
+addappid(12226, 1, "b9c2812297b896cbe17d5d14a9b74e93b2b99b1f86dd3b4e21342f7a61b32fca") --Main Windows Language Depot German Grand Theft Auto: Episodes from Liberty City
+setManifestid(12226, "1134841128184410844", 0)
+addappid(12227, 1, "dc52bbe1b3329b150a53b9cad3c71ee6ab502b81e18b87b293f98a811a600515") --Main Windows Language Depot Spanish Grand Theft Auto: Episodes from Liberty City
+setManifestid(12227, "1563319131398911718", 0)
