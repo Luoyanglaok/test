acb19cb5340243b94b5bb028081bf2fcdebba800
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:54:32 +0800

    add 287700.lua

--- 287700.lua
+++ 287700.lua
@@ -1,7 +1,30 @@
-addappid(287700)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(287701,0,"f1c3bbf22c23c0777bf23c71491e2398e216ade65fbd5597762b7fb64165dd4f")
-setManifestid(287701,"2518831260221836800")
-addappid(287702,0,"5fff13d6b2e316f81e7fbce5e82cc9e77fdcc6c1daa0f723a3f347c1f2017287")
-setManifestid(287702,"5656962073680694207")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 287700.lua
+--Gamename METAL GEAR SOLID V: THE PHANTOM PAIN
+addappid(287700, 1, "96a637d7740f26b2a43cc7d0c250d595d86d3bc03c81481df01cadd8680bd0c5") --Mainappid METAL GEAR SOLID V: THE PHANTOM PAIN
+addappid(287701, 1, "f1c3bbf22c23c0777bf23c71491e2398e216ade65fbd5597762b7fb64165dd4f") --Main Windows Language Depot English METAL GEAR SOLID V: THE PHANTOM PAIN
+setManifestid(287701, "1506669297791974057", 28235501168)
+addappid(287702, 1, "5fff13d6b2e316f81e7fbce5e82cc9e77fdcc6c1daa0f723a3f347c1f2017287") --Main Windows Language Depot Japanese METAL GEAR SOLID V: THE PHANTOM PAIN
+setManifestid(287702, "2786532116982545317", 28220172544)
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - Jumpsuit (EVA)
+addappid(406600, 1, "5f4aa56440a0b958c1a3cf50ca76373fae5263407cbbfabf5bf600ad908c4683") --Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - Jumpsuit (EVA)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(437232) --Dlcname AppID 437232
+addtoken(437232, "17207916683457290835")
+addappid(437233) --Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - MB Coin 2000
+--Missing Dlcs
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - Fatigues (Naked Snake)
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - Sneaking Suit (Naked Snake)
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - Tuxedo
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - Sneaking Suit (The Boss)
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - Western Tack
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - Parade Tack
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - MGO DLC Pack 1
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - MGO Appeal Action Pack 1
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - MGO Appeal Action Pack 2
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - MGO Appeal Action Pack 3
+--Dlcname METAL GEAR SOLID V: THE PHANTOM PAIN - MGO Appeal Action Pack 4
