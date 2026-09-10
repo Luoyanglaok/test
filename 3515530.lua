dfcc1ecb4240e1983315079349b4efd99396d766
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:16:42 +0800

    add 3515530.lua

---
+++ 3515530.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3515530.lua
+--Gamename Ice Cream Simulator
+addappid(3515530) --Mainappid Ice Cream Simulator
+addappid(3515531, 1, "8721ca94ac600fb7dd0d888249c92b19f18ab29a40d64e6268a0fc24597b1c49") --Main Windows Depot Ice Cream Simulator
+setManifestid(3515531, "2131926516736607220", 8714168240)
+addappid(3515532, 1, "106cde3444d7cf039b83411e0a7a3ad81ebca4f3b80d09a3ba54cac8794419fc") --Main Macos Depot Ice Cream Simulator
+setManifestid(3515532, "6710446902618922599", 8661699632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
