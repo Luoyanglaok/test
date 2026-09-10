aae18c562da2c7b8f290592305561c0fab7c7c75
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:38:18 +0800

    add 4251610.lua

---
+++ 4251610.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4251610.lua
+--Gamename 交叠之夏
+addappid(4251610, 1, "5c6dc0ec1130cfbcb7668baf1645d1ff972371b20dfacc3cac5eb3d730c2e8ae") --Mainappid 交叠之夏
+addappid(4251611, 1, "72fcd539b029a0beb03fed94918bc096d5966c5e5504cf3acaa1ffdc55883101") --Main Windows Depot 交叠之夏
+setManifestid(4251611, "3657431390970986969", 1387049104)
