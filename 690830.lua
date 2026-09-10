2c3a25b58118540921764062e2a86f2252a8c319
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:56:32 +0800

    add 690830.lua

--- 690830.lua
+++ 690830.lua
@@ -1,6 +1,13 @@
-addappid(690830)
-setManifestid(228988,"6645201662696499616")
-addappid(690831,0,"a0df1cd93c1681e0839c3d7a86e7de7b7af07d07b00cc495896697b1fda49961")
-setManifestid(690831,"8436991491819773886")
-addappid(3482250,0,"c10e61648bc4b16350ebfac52a6d56f7282c1442a4f3c49e04bb9a7dd4693717")
-setManifestid(3482250,"8199326432290963373")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 690830.lua
+--Gamename Foundation
+addappid(690830, 1, "ec0f4ec6b9dc4bdcab5a696ea43103e83bd531cc49ae6a47fb4d30852241cbb0") --Mainappid Foundation
+addappid(690831, 1, "a0df1cd93c1681e0839c3d7a86e7de7b7af07d07b00cc495896697b1fda49961") --Main Windows Depot Foundation
+setManifestid(690831, "8425790433988916696", 2903173488)
+--Dlcname Foundation Guivre Garden
+addappid(3482250, 1, "c10e61648bc4b16350ebfac52a6d56f7282c1442a4f3c49e04bb9a7dd4693717") --Dlcname Foundation Guivre Garden
+setManifestid(3482250, "8572113907474119714", 8661376)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4713280) --Dlcname Foundation: Harvest Festival
