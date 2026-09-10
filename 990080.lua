7982824098c830b5bb50c67b1d146e35e99baf2c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:48:06 +0800

    add 990080.lua

--- 990080.lua
+++ 990080.lua
@@ -1,23 +1,36 @@
-addappid(990080)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-addappid(990081,0,"99edd7f3221635d93481770b28ebac240488e1f42cf16f01e5a4e744b0944da6")
-setManifestid(990081,"7657511114935696635")
-addappid(990082,0,"ac55581fbdef1f6687f91654d435e2d4c395d2fa56eb5aba4fa52d708118157e")
-setManifestid(990082,"2131911399630779386")
-addappid(990083,0,"2326f907ccd6ebfc8235495fbb2abd7b12d814a3228cd3b0bf480f7c602c87f5")
-setManifestid(990083,"5520381994780129747")
-addappid(990084,0,"f043f2ca0cfbbaf5fcb6e30d4beb1a20ba5ef293db11dc4e5521bdfa8a7fdfe0")
-setManifestid(990084,"4298775494378965915")
-addappid(990085,0,"b7d07ff314786df30f9d32266a2e88cd026fabaffbd2567dbc1637cd319ef996")
-setManifestid(990085,"8141128345180509732")
-addappid(990086,0,"d9814846faaa5f83e45151bec5190062e320c6b9a6842e7722ed51293a29a667")
-setManifestid(990086,"8889891247215635520")
-addappid(990087,0,"42c46e0e1520cefc9f4120f00fca640bc83f48b322e7b19ad9530538d9cbee75")
-setManifestid(990087,"8956076021982921820")
-addappid(990088,0,"81c364d84279c2fafb7960d0aabd75e7e371ad27f275484d99ef5734e4c00d95")
-setManifestid(990088,"7612209164717793334")
-addappid(990089,0,"21886ab063a78ccf9b676f311597b895c73a1410cd9f7e0e3e7b4e99b82f1e6b")
-setManifestid(990089,"5406006200511624287")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 990080.lua
+--Gamename Hogwarts Legacy
+addappid(990080, 1, "da8234fa54a6758153c90b4d013b5a5e41930ef8a22c1e42729949bb890e0c55") --Mainappid Hogwarts Legacy
+addappid(990081, 1, "99edd7f3221635d93481770b28ebac240488e1f42cf16f01e5a4e744b0944da6") --Main Windows Depot Hogwarts Legacy
+setManifestid(990081, "5198899101792588169", 70605758608)
+addappid(990082, 1, "ac55581fbdef1f6687f91654d435e2d4c395d2fa56eb5aba4fa52d708118157e") --Main Windows Depot Hogwarts Legacy
+setManifestid(990082, "7167310505439807086", 2292093440)
+addappid(990083, 1, "2326f907ccd6ebfc8235495fbb2abd7b12d814a3228cd3b0bf480f7c602c87f5") --Main Windows Language Depot French Hogwarts Legacy
+setManifestid(990083, "6256883361539505915", 2496109520)
+addappid(990084, 1, "f043f2ca0cfbbaf5fcb6e30d4beb1a20ba5ef293db11dc4e5521bdfa8a7fdfe0") --Main Windows Language Depot German Hogwarts Legacy
+setManifestid(990084, "4878463996609114776", 2488506304)
+addappid(990085, 1, "b7d07ff314786df30f9d32266a2e88cd026fabaffbd2567dbc1637cd319ef996") --Main Windows Language Depot Spanish Hogwarts Legacy
+setManifestid(990085, "109909038347739340", 2588590272)
+addappid(990086, 1, "d9814846faaa5f83e45151bec5190062e320c6b9a6842e7722ed51293a29a667") --Main Windows Language Depot Latam Hogwarts Legacy
+setManifestid(990086, "7173744932268868207", 2593457120)
+addappid(990087, 1, "42c46e0e1520cefc9f4120f00fca640bc83f48b322e7b19ad9530538d9cbee75") --Main Windows Language Depot Italian Hogwarts Legacy
+setManifestid(990087, "4408608444924365738", 2574284928)
+addappid(990088, 1, "81c364d84279c2fafb7960d0aabd75e7e371ad27f275484d99ef5734e4c00d95") --Main Windows Language Depot Japanese Hogwarts Legacy
+setManifestid(990088, "4209668305361446309", 2506164128)
+addappid(990089, 1, "21886ab063a78ccf9b676f311597b895c73a1410cd9f7e0e3e7b4e99b82f1e6b") --Main Windows Language Depot Brazilian Hogwarts Legacy
+setManifestid(990089, "7833413172600852139", 2463150592)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(1880830) --Dlcname Hogwarts Legacy: Onyx Hippogriff Mount
+addappid(1880831) --Dlcname Hogwarts Legacy: Astronomer's Hat
+addappid(1880832) --Dlcname Hogwarts Legacy: Dark Arts Pack
+addappid(1880833) --Dlcname Hogwarts Legacy: Kelpie Robe
+addappid(1880837) --Dlcname Hogwarts Legacy: Dark Arts Garrison Hat
