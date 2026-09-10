2abf059c20d97f9d5b0d09d9a6c3bf7b918f5ac6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:39:09 +0800

    add 1962700.lua

---
+++ 1962700.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1962700.lua
+--Gamename Subnautica 2
+addappid(1962700, 1, "f1218dc455570758f1829f86bbfab5e00806e8a4063b13c786c37ff641e8053f") --Mainappid Subnautica 2
+addappid(1962701, 1, "96feb03fc707b975cf8271da4659fdb16c14c4b1a5c5bf8f94bd66e39edd4d96") --Main Windows Depot Subnautica 2
+setManifestid(1962701, "2218551816517627922", 14381006992)
+addappid(3803322, 1, "7eaea8f9d28c829c78f341808f18830cbd44ce705c0a0f73de3d2bda53571058") --Main Windows Depot Subnautica 2
+setManifestid(3803322, "8650163981997321126", 703979936)
+--Dlcname Subnautica 2 Soundtrack
+addappid(3803320) --Dlcname Subnautica 2 Soundtrack
+addappid(3803321, 1, "838cddad95174d4eba833bbd3194cdb121a6c59a0b5fea1ce8b5c796e2d86449") --Dlc Windows Depot Subnautica 2 Soundtrack
+setManifestid(3803321, "5719658270261268016", 167098064)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
