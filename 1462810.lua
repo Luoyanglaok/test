45824b100d02b1371f3426f31cb0847553c66073
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:18:05 +0800

    add 1462810.lua

--- 1462810.lua
+++ 1462810.lua
@@ -1,7 +1,19 @@
-addappid(1462810)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1462811,0,"4d32d8e4437fd7b8a78ade88d36e020a1b25551ae1bd3da6768dcb764cf661c1")
-setManifestid(1462811,"3647501222326679630")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1462810.lua
+--Gamename WRC 10 FIA World Rally Championship
+addappid(1462810) --Mainappid WRC 10 FIA World Rally Championship
+addappid(1462811, 1, "4d32d8e4437fd7b8a78ade88d36e020a1b25551ae1bd3da6768dcb764cf661c1") --Main Windows Depot WRC 10 FIA World Rally Championship
+setManifestid(1462811, "3647501222326679630", 35023483248)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1654720) --Dlcname WRC 10 Subaru Impreza WRC 1997
+addappid(1654721) --Dlcname WRC 10 Arena Panzerplatte SSS
+addappid(1654722) --Dlcname WRC 10 Mitsubishi Lancer Evo V 1998
+addappid(1654723) --Dlcname WRC 10 Career Starter Pack
