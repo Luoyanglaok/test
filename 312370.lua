4adcb120b966495ed7fd58fd504ddef3d3632489
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:52:47 +0800

    add 312370.lua

--- 312370.lua
+++ 312370.lua
@@ -1,8 +1,21 @@
-addappid(312370)
-setManifestid(228990,"1829726630299308803")
-addappid(312371,0,"4845b52c22b02fa0e35db3fce0c087504f90e4b6ef6d3f69585fee6de7448550")
-setManifestid(312371,"6776733795176741733")
-addappid(312372,0,"b5bb1887c9ac92f1effbce4ca2c636dc9c084f52df3f781b2e52463dfd4af9b9")
-setManifestid(312372,"6862694679219973019")
-addappid(437110,0,"1cec5197cb7ba133a2365a057ae306b00c982b06b760b8ed843f24d702f0d6f9")
-setManifestid(437110,"843163003281780195")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 312370.lua
+--Gamename Warhammer 40,000: Armageddon (Classic)
+addappid(312370) --Mainappid Warhammer 40,000: Armageddon (Classic)
+addappid(312371, 1, "4845b52c22b02fa0e35db3fce0c087504f90e4b6ef6d3f69585fee6de7448550") --Main Windows Depot Warhammer 40,000: Armageddon (Classic)
+setManifestid(312371, "6776733795176741733", 937069056)
+addappid(312372, 1, "b5bb1887c9ac92f1effbce4ca2c636dc9c084f52df3f781b2e52463dfd4af9b9") --Main Macos Depot Warhammer 40,000: Armageddon (Classic)
+setManifestid(312372, "6862694679219973019", 972882176)
+--Dlcname Warhammer 40,000: Armageddon - Soundtrack
+addappid(437110, 1, "1cec5197cb7ba133a2365a057ae306b00c982b06b760b8ed843f24d702f0d6f9") --Dlcname Warhammer 40,000: Armageddon - Soundtrack
+setManifestid(437110, "843163003281780195", 80412768)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(405520) --Dlcname Warhammer 40,000: Armageddon - Ork Hunters (Classic)
+addappid(437140) --Dlcname Warhammer 40,000: Armageddon - Golgotha (Classic)
+--Missing Dlcs
+--Dlcname Warhammer 40,000: Armageddon - Untold Battles (Classic)
+--Dlcname Warhammer 40,000: Armageddon - Vulkan's Wrath (Classic)
+--Dlcname Warhammer 40,000: Armageddon - Angels of Death (Classic)
+--Dlcname Warhammer 40,000: Armageddon - Glory of Macragge (Classic)
