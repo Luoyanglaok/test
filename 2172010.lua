c93283ef9b4744b3ee8759e1d6295debafbcf935
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 23:16:36 +0800

    add 2172010.lua

--- 2172010.lua
+++ 2172010.lua
@@ -1,7 +1,13 @@
-addappid(2172010)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2172011,0,"b8955754339fd50012ea607b7095692a80fdcb5b93df0e9dd837dfa5c6e3aabf")
-setManifestid(2172011,"8034387937407970920")
-addappid(2172012,0,"3dee1125193274da66e531b4c674f7787d08405c3f39309b0f72d9fe96d6ba74")
-setManifestid(2172012,"5223895161202821946")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2172010.lua
+--Gamename Until Dawn™
+addappid(2172010, 1, "1c69067a2cb0ff7e2bebf164ee4a304c9c3b3fca2695cb0cf7b84756f18f8edd") --Mainappid Until Dawn™
+addappid(2172011, 1, "b8955754339fd50012ea607b7095692a80fdcb5b93df0e9dd837dfa5c6e3aabf") --Main Windows Depot Until Dawn™
+setManifestid(2172011, "943096621366092792", 61732899408)
+addappid(2172012, 1, "3dee1125193274da66e531b4c674f7787d08405c3f39309b0f72d9fe96d6ba74") --Main Windows Depot Until Dawn™
+setManifestid(2172012, "5005699011104635783", 61779386528)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
