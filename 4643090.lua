c164c70368e6f96353d493c075844f2664fd24a8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:00:34 +0800

    add 4643090.lua

---
+++ 4643090.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4643090.lua
+--Gamename The NOexistenceN of Lilith
+addappid(4643090, 1, "c40f59e41f1b2f697776810afc742ddd15a8dfc2bd123552ae27a120d7b92691") --Mainappid The NOexistenceN of Lilith
+addappid(4643091, 1, "24dc6d333abfee90ce1032977f8fbce3aab7ffcaef2babc048cfecc79d93d749") --Main Windows Depot The NOexistenceN of Lilith
+setManifestid(4643091, "3294049618375270151", 221804976)
+addappid(4643092, 1, "93785f5726976cb5a3fad7d5604ad272ff131a57f8ead118f0f46c19a2b48153") --Main Macos Depot The NOexistenceN of Lilith
+setManifestid(4643092, "6277190356977587807", 249844848)
