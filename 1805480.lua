257f051c0e957f7fc4fff29c02bca0517e3c5a2e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:36:09 +0800

    add 1805480.lua

--- 1805480.lua
+++ 1805480.lua
@@ -1,5 +1,22 @@
-addappid(1805480)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1805481,0,"b6865a3777287943bedd9ec78a50d638f0eea4d711441035e656bcc633a83765")
-setManifestid(1805481,"3104969324904098578")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1805480.lua
+--Gamename Like a Dragon: Ishin!
+addappid(1805480) --Mainappid Like a Dragon: Ishin!
+addappid(1805481, 1, "b6865a3777287943bedd9ec78a50d638f0eea4d711441035e656bcc633a83765") --Main Windows Depot Like a Dragon: Ishin!
+setManifestid(1805481, "3104969324904098578", 59993274240)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2104730) --Dlcname Like a Dragon: Ishin! - Shinsengumi Captain's Set
+addappid(2104731) --Dlcname Like a Dragon: Ishin! - Ryoma Growth Support Kit
+addappid(2104732) --Dlcname Like a Dragon: Ishin! - Sword Upgrade Materials Kit
+addappid(2104733) --Dlcname Like a Dragon: Ishin! - Gun Upgrade Materials Kit
+addappid(2104734) --Dlcname Like a Dragon: Ishin! - Third Division Armament Expansion Kit
+addappid(2104735) --Dlcname Like a Dragon: Ishin! - Dragon of Dojima Skin
+addappid(2104736) --Dlcname Like a Dragon: Ishin! - Japanese Swords & Cannon Set (Pre-Purchase)
+addappid(2163170) --Dlcname Like a Dragon: Ishin! - Bonechewer Katana
+addappid(2163171) --Dlcname Like a Dragon: Ishin! - Waterdrop Katana
+addappid(2202540) --Dlcname Like a Dragon: Ishin! - Elite Generals Trooper Card Bundle
+addappid(2223050) --Dlcname Like a Dragon: Ishin! - Dragon’s Clutch Katana
