e4bdee99f374396ea298b3b8b342941423cbcd3b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:27:07 +0800

    add 1017900.lua

--- 1017900.lua
+++ 1017900.lua
@@ -1,5 +1,11 @@
-addappid(1017900)
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-addappid(1017901,0,"d28682965acebec2e120b391343fe1f369835501696e82b33a6312037eb0b0d5")
-setManifestid(1017901,"410572971259215218")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1017900.lua
+--Gamename Age of Empires: Definitive Edition
+addappid(1017900) --Mainappid Age of Empires: Definitive Edition
+addappid(1017901, 1, "d28682965acebec2e120b391343fe1f369835501696e82b33a6312037eb0b0d5") --Main Windows Depot Age of Empires: Definitive Edition
+setManifestid(1017901, "410572971259215218", 10148359520)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
