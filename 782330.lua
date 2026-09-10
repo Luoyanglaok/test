57cba1c6d140b600bbbc9f5bc12753cb9c88f49a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:53:55 +0800

    add 782330.lua

--- 782330.lua
+++ 782330.lua
@@ -1,21 +1,45 @@
-addappid(782330)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(782332,0,"50c0780a4d2fe21b3bbdf3e70f7705e07544725ddb3b65c32580d2f8c8652801")
-setManifestid(782332,"3719882324993485765")
-addappid(782333,0,"ae331c10946ec0147a027c970cc531ce101105b4a761c835d01d500a02f15cf9")
-setManifestid(782333,"7790151103401207847")
-addappid(782334,0,"a444c49bffe4e0ea2fde35c1cf7ada5b2282711c72fcd2ac88f89abbe40f61d7")
-setManifestid(782334,"2305285962735833965")
-addappid(782335,0,"3b4cff3ce45f5fe2f10c0ec4ddd80ccc514fb48df6c0cce946bfba8f9e138400")
-setManifestid(782335,"4277233946804126868")
-addappid(782338,0,"5bed7bb7b23d1d00ec8ec0db60fa7b92f4cbfe02e12dbc481c89c0b627933216")
-setManifestid(782338,"8642366102070535855")
-addappid(782336,0,"8e8398764c2ed958096c29c49abad7be5160a272889979f6de1d38d2e3f8b39c")
-setManifestid(782336,"3442068440108528889")
-addappid(782337,0,"173e7ffd69ef2f81a69d99858c80cd6947693b247645da2a3caa3bb7593c4c61")
-setManifestid(782337,"7595167059955801744")
-addappid(782339,0,"eda659b514b48a17be8e2619052fae93c7b37578ee6bfc3fd648a8844246e86b")
-setManifestid(782339,"5334830772540664008")
-addappid(1098294,0,"4853f1060dba902f3a474dbd6710c8e8f8db4fbec981d3fc90da7dc51ec85d62")
-setManifestid(1098294,"6180500623984810207")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 782330.lua
+--Gamename DOOM Eternal
+addappid(782330, 1, "61e90dcafecdebcbaa7bf4a80d452aab69a16b2c71d44926f1df45dba2404145") --Mainappid DOOM Eternal
+addappid(782332, 1, "50c0780a4d2fe21b3bbdf3e70f7705e07544725ddb3b65c32580d2f8c8652801") --Main Windows Depot DOOM Eternal
+setManifestid(782332, "2367662674907098364", 39265984)
+addappid(782333, 1, "ae331c10946ec0147a027c970cc531ce101105b4a761c835d01d500a02f15cf9") --Main Windows Depot DOOM Eternal
+setManifestid(782333, "2893512595648799481", 352)
+addappid(782334, 1, "a444c49bffe4e0ea2fde35c1cf7ada5b2282711c72fcd2ac88f89abbe40f61d7") --Main Windows Depot DOOM Eternal
+setManifestid(782334, "8272489020521907930", 3062070640)
+addappid(782335, 1, "3b4cff3ce45f5fe2f10c0ec4ddd80ccc514fb48df6c0cce946bfba8f9e138400") --Main Windows Depot DOOM Eternal
+setManifestid(782335, "4277233946804126868", 1833392352)
+addappid(782336, 1, "8e8398764c2ed958096c29c49abad7be5160a272889979f6de1d38d2e3f8b39c") --Main Windows Depot DOOM Eternal
+setManifestid(782336, "7701371671209160942", 70645451040)
+addappid(782337, 1, "173e7ffd69ef2f81a69d99858c80cd6947693b247645da2a3caa3bb7593c4c61") --Main Windows Depot DOOM Eternal
+setManifestid(782337, "7595167059955801744", 9206805168)
+addappid(782338, 1, "5bed7bb7b23d1d00ec8ec0db60fa7b92f4cbfe02e12dbc481c89c0b627933216") --Main Windows Depot DOOM Eternal
+setManifestid(782338, "8642366102070535855", 304)
+addappid(782339, 1, "eda659b514b48a17be8e2619052fae93c7b37578ee6bfc3fd648a8844246e86b") --Main Windows Depot DOOM Eternal
+setManifestid(782339, "7935278702241685666", 161095632)
+addappid(1098294, 1, "4853f1060dba902f3a474dbd6710c8e8f8db4fbec981d3fc90da7dc51ec85d62") --Main Windows Depot DOOM Eternal
+setManifestid(1098294, "8086652904798375734", 79712992)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1098290) --Dlcname DOOM Eternal (Campaign)
+addappid(1098291) --Dlcname DOOM Eternal Year One Pass
+addappid(1098292) --Dlcname Doom Eternal - The Ancient Gods - Part One Cosmic DLC
+addappid(1098293) --Dlcname DOOM Eternal: The Ancient Gods - Part Two
+addtoken(1098293, "16795730995712719561")
+addappid(1098300) --Dlcname DOOM Eternal: Rip and Tear Pack
+addappid(1098301) --Dlcname DOOM Eternal - Deluxe Edition Content
+addappid(1460020) --Dlcname DOOM Eternal: Series One Cosmetic Pack
+addappid(1460021) --Dlcname DOOMicorn Master Collection Cosmetic Pack
+addappid(1626000) --Dlcname DOOM Eternal: Series Two Cosmetic Pack
+addappid(1626001) --Dlcname DOOM Eternal: Series Three Cosmetic Pack
+addappid(1626002) --Dlcname Mullet Slayer Master Collection Cosmetic Pack
+addappid(1626003) --Dlcname DOOM Eternal: Cosplay Slayer Master Collection Cosmetic Pack
+addappid(1749100) --Dlcname DOOM Eternal: Series Four Cosmetic Pack
+addappid(1749101) --Dlcname DOOM Eternal: Series Five Cosmetic Pack
+addappid(1749102) --Dlcname DOOM Eternal: Series Six Cosmetic Pack
+addappid(1749103) --Dlcname DOOM Eternal: Series Seven Cosmetic Pack
+addappid(4764470) --Dlcname DOOM Eternal: Complete Cosmetic Pack
