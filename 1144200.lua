42d0e275d8b6806693ce69062a78e4e402581861
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:09:37 +0800

    add 1144200.lua

--- 1144200.lua
+++ 1144200.lua
@@ -1,5 +1,23 @@
-addappid(1144200)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1144201,0,"b9236894ba071d12dd117ce16220fefeb20574d7c6f7d2170e3113346867f3d6")
-setManifestid(1144201,"6091457157396211072")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1144200.lua
+--Gamename Ready or Not
+addappid(1144200, 1, "fb29b3865255ec0e9230c029502cd87b8626fd12f9bdb2fa3df6cddf34e17179") --Mainappid Ready or Not
+addappid(1144201, 1, "b9236894ba071d12dd117ce16220fefeb20574d7c6f7d2170e3113346867f3d6") --Main Windows Language Depot English Ready or Not
+setManifestid(1144201, "4381209963403495092", 40287443200)
+--Dlcname Ready or Not: Official Soundtrack
+addappid(3089910) --Dlcname Ready or Not: Official Soundtrack
+addappid(3089911, 1, "ad6e74ede75aba32203a011af40c506a771be84256f15775efce310170c4676c") --Dlc Windows Depot Ready or Not: Official Soundtrack
+setManifestid(3089911, "6229163073173979005", 269993776)
+addappid(3089912, 1, "cd045276bbdda167270fd46f0b129a781e705003f2af095fe71bec217d065b95") --Dlc Windows Depot Ready or Not: Official Soundtrack
+setManifestid(3089912, "1071123769281934173", 1479900080)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(1844910) --Dlcname Ready or Not: Supporter Edition
+addappid(3015760) --Dlcname Ready or Not: Home Invasion
+addappid(3174120) --Dlcname Ready or Not: Dark Waters
+addappid(4224910) --Dlcname Ready or Not: Boiling Point
