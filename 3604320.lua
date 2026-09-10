91a2f09e7e443685c6d65b4b5d3159dffd85f16f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:18:57 +0800

    add 3604320.lua

--- 3604320.lua
+++ 3604320.lua
@@ -1,3 +1,6 @@
-addappid(3604320)
-addappid(3604321,0,"9253308044c28fa78ab9e22135c527b2f61a6367a2bb012bc8f3051ce8c38deb")
-setManifestid(3604321,"1988638181616573396")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3604320.lua
+--Gamename 女神异闻录：夜幕魅影
+addappid(3604320, 1, "0ec30ef992b1eb60c17e513b7d1d23a115c584e6dc0a54595bc2435e80076ffa") --Mainappid 女神异闻录：夜幕魅影
+addappid(3604321, 1, "9253308044c28fa78ab9e22135c527b2f61a6367a2bb012bc8f3051ce8c38deb") --Main Windows Depot 女神异闻录：夜幕魅影
+setManifestid(3604321, "1988638181616573396", 139424848)
