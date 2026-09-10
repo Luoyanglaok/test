6126c262b61ef244370411b2fe1798fcdc3c6079
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:28:22 +0800

    add 3937550.lua

---
+++ 3937550.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3937550.lua
+--Gamename Yakuza Kiwami 3 & Dark Ties
+addappid(3937550, 1, "4aebad04b4889dfd496ffce6b97ecd30c7bcbaff1519d464c73e7d05dead1573") --Mainappid Yakuza Kiwami 3 & Dark Ties
+addappid(3937551, 1, "ce44bd72b4308b8793812099f3fc2005dbdd1cf8465b7dd7391cae39a7512801") --Main Windows Depot Yakuza Kiwami 3 & Dark Ties
+setManifestid(3937551, "663491988918308505", 53283640368)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3948230) --Dlcname Yakuza Kiwami 3 & Dark Ties - Legendary Outfit Pack
+addappid(3948240) --Dlcname Yakuza Kiwami 3 & Dark Ties - Legendary Lads & Gals Set
+addappid(3948250) --Dlcname Yakuza Kiwami 3 & Dark Ties - Ryukyu Gal Gang: Customization Pack
+addappid(3948260) --Dlcname Yakuza Kiwami 3 & Dark Ties - Flip Phone Customization Pack
+addappid(3948270) --Dlcname Yakuza Kiwami 3 & Dark Ties - Legendary BGM CD Pack
+addappid(3948280) --Dlcname Yakuza Kiwami 3 & Dark Ties - Ryukyu Gal Gang: Ichiban Legendary Lad
+addappid(3957290) --Dlcname Yakuza Kiwami 3 & Dark Ties - Ichiban Kasuga Legendary Outfit Pack
