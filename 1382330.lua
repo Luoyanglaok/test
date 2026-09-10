a01cfa5a5c03228a0fd4160a39490dd3db9f40a5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:17:38 +0800

    add 1382330.lua

--- 1382330.lua
+++ 1382330.lua
@@ -1,11 +1,21 @@
-addappid(1382330)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1382331,0,"cb4d47b8650bd74db3b51a92cf07ad418d78b96bd67133d2b4b8b2a3d0ba9538")
-setManifestid(1382331,"541657970914493992")
-addappid(1482080,0,"9bc21e38df537abe2d78ec1c094aa47ba429892dde2f0104efc9fd1bc5bc4ea1")
-setManifestid(1482080,"213556939175564631")
-addappid(1482081,0,"5fccb5605a330f3cf82a4a14924f83ea10e8a1264f86cf3dd89284f3e06abb03")
-setManifestid(1482081,"180884191516666770")
-addappid(1382332,0,"2d11674fc11c6ebfab0a1ec662058d2ff40d80e4b968698c2e4fb9c2b5973ae7")
-setManifestid(1382332,"2454132547387187838")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1382330.lua
+--Gamename Persona® 5 Strikers
+addappid(1382330, 1, "b6c302b69e06243ddc7ce804432bc7e5d5e6cf7d4f981198dd5a29cef67a45b6") --Mainappid Persona® 5 Strikers
+addappid(1382331, 1, "cb4d47b8650bd74db3b51a92cf07ad418d78b96bd67133d2b4b8b2a3d0ba9538") --Main Windows Depot Persona® 5 Strikers
+setManifestid(1382331, "541657970914493992", 22242769936)
+--Dlcname Persona 5 Strikers - Legacy BGM Pack
+addappid(1482080, 1, "9bc21e38df537abe2d78ec1c094aa47ba429892dde2f0104efc9fd1bc5bc4ea1") --Dlcname Persona 5 Strikers - Legacy BGM Pack
+setManifestid(1482080, "213556939175564631", 64)
+--Dlcname Persona 5 Strikers - All-Out Attack Pack
+addappid(1482081, 1, "5fccb5605a330f3cf82a4a14924f83ea10e8a1264f86cf3dd89284f3e06abb03") --Dlcname Persona 5 Strikers - All-Out Attack Pack
+setManifestid(1482081, "180884191516666770", 64)
+--Dlcname Persona 5 Strikers - Bonus Content
+addappid(1523810) --Dlcname Persona 5 Strikers - Bonus Content
+addappid(1382332, 1, "2d11674fc11c6ebfab0a1ec662058d2ff40d80e4b968698c2e4fb9c2b5973ae7") --Dlc Windows Depot Persona 5 Strikers - Bonus Content
+setManifestid(1382332, "1217603562634354986", 2917576032)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
