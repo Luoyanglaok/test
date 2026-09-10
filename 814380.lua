b7120c834972ae4a63ceb175883a7968eb204563
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:15:28 +0800

    add 814380.lua

--- 814380.lua
+++ 814380.lua
@@ -1,13 +1,22 @@
-addappid(814380)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(814381,0,"84db6d0cd0648c885d471541b83416e460f07ad6b71e1b86b2fa26926514d874")
-setManifestid(814381,"7598923580425802222")
-addappid(814382,0,"742e0660742d90e8f827db3e00bcbf594970e97e76f8fb90ec74a54562ef5502")
-setManifestid(814382,"8495754623934990551")
-addappid(814383,0,"3f8022efca09175e6bef77385ece0b5693e94d829d50baab66f9e249b996992b")
-setManifestid(814383,"1996592192490629382")
-addappid(814385,0,"8689daaad42496510eb7b2fc6f6de095af54aae85e481998eb4746d2fad6b9af")
-setManifestid(814385,"1613271962634429538")
-addappid(1039230,0,"fdf0783fc1f1f3ee832c2912beffe0982e4f6d2abb1b63c8266fade7ad7f4493")
-setManifestid(1039230,"1077830899885356469")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 814380.lua
+--Gamename Sekiro™: Shadows Die Twice - GOTY Edition
+addappid(814380, 1, "aa4d7f62b340049a0798ece9a8f027f2aedd339bcb5e5ad2c1a0a78881ab4707") --Mainappid Sekiro™: Shadows Die Twice - GOTY Edition
+addappid(814381, 1, "84db6d0cd0648c885d471541b83416e460f07ad6b71e1b86b2fa26926514d874") --Main Windows Depot Sekiro™: Shadows Die Twice - GOTY Edition
+setManifestid(814381, "7598923580425802222", 48655104)
+addappid(814382, 1, "742e0660742d90e8f827db3e00bcbf594970e97e76f8fb90ec74a54562ef5502") --Main Windows Depot Sekiro™: Shadows Die Twice - GOTY Edition
+setManifestid(814382, "8495754623934990551", 12623533008)
+addappid(814383, 1, "3f8022efca09175e6bef77385ece0b5693e94d829d50baab66f9e249b996992b") --Main Windows Depot Sekiro™: Shadows Die Twice - GOTY Edition
+setManifestid(814383, "1996592192490629382", 576)
+addappid(814384, 1, "cd263eaba39f96f151278b4f7f4f60601959371d5f55781fe6b393ec3cd447f6") --Main Windows Depot Sekiro™: Shadows Die Twice - GOTY Edition
+setManifestid(814384, "147690030473406272", 576)
+addappid(814385, 1, "8689daaad42496510eb7b2fc6f6de095af54aae85e481998eb4746d2fad6b9af") --Main Windows Depot Sekiro™: Shadows Die Twice - GOTY Edition
+setManifestid(814385, "1613271962634429538", 576)
+--Dlcname Sekiro - DIGITAL ARTWORK & MINI SOUNDTRACK
+addappid(1039230, 1, "fdf0783fc1f1f3ee832c2912beffe0982e4f6d2abb1b63c8266fade7ad7f4493") --Dlcname Sekiro - DIGITAL ARTWORK & MINI SOUNDTRACK
+setManifestid(1039230, "1077830899885356469", 327749616)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
