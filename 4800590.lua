8247bacd1de623d1d6869769640b13e691e84b27
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:15:00 +0800

    add 4800590.lua

---
+++ 4800590.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4800590.lua
+--Gamename Supermarket Chaos
+addappid(4800590, 1, "e9335e53c789d938158eedb8a5c66e174fb270ecd3139813b644779b17bb0f82") --Mainappid Supermarket Chaos
+addappid(4800591, 1, "96914473ba48921d6f31b7753bec7dbeaac2e5ba8f983227bd6b51b8ebbed306") --Main Windows Depot Supermarket Chaos
+setManifestid(4800591, "7544505283011053352", 1146769296)
