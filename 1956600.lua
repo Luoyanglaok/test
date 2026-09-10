f575bcad2673d2c936d541cbeead1415a15afd48
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:37:14 +0800

    add 1956600.lua

--- 1956600.lua
+++ 1956600.lua
@@ -1,5 +1,9 @@
-addappid(1956600)
-addappid(1956601,0,"9bb0762a66fb0500a17d6c9ec6db35ea7f9d98adc3fcf77851f87b3c99d8bba6")
-setManifestid(1956601,"786206625583612178")
-addappid(2095530,0,"326526d579e8e8dd232e4c114c989e866d14949d7c82c7a7b67cc6e153f09dcf")
-setManifestid(2095530,"3616188022168217743")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1956600.lua
+--Gamename 融雪
+addappid(1956600, 1, "c143e9914646412955c069d38b5563dd20e5b3494f18036ba21f2f7df03d31db") --Mainappid 融雪
+addappid(1956601, 1, "9bb0762a66fb0500a17d6c9ec6db35ea7f9d98adc3fcf77851f87b3c99d8bba6") --Main Windows Language Depot English 融雪
+setManifestid(1956601, "786206625583612178", 1042043088)
+--Dlcname Yusetsu-Patch
+addappid(2095530, 1, "326526d579e8e8dd232e4c114c989e866d14949d7c82c7a7b67cc6e153f09dcf") --Dlcname Yusetsu-Patch
+setManifestid(2095530, "3616188022168217743", 208924160)
