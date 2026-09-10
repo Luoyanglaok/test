6fdf52a483e528f7cf93ef823af28d970d849c2d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:23:27 +0800

    add 2713690.lua

--- 2713690.lua
+++ 2713690.lua
@@ -1,3 +1,6 @@
-addappid(2713690)
-addappid(2713692,0,"b78eff7b11ce3c5061e4fc8f455c25d1d7ea57fbfe0889f1d1268a6ff56b66df")
-setManifestid(2713692,"3891156152409949149")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2713690.lua
+--Gamename Shattered Minds
+addappid(2713690, 1, "89c3dad35bf25a8bc98a177c69f5eeaf62f9cf3e01e9043badbcc652aceaf41b") --Mainappid Shattered Minds
+addappid(2713692, 1, "b78eff7b11ce3c5061e4fc8f455c25d1d7ea57fbfe0889f1d1268a6ff56b66df") --Main Windows Depot Shattered Minds
+setManifestid(2713692, "2792745434795124536", 5485624608)
