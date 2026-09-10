705fe44b1665c6a0515d10d86ff15fb07b07c8e7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:41:19 +0800

    add 2889660.lua

--- 2889660.lua
+++ 2889660.lua
@@ -1,3 +1,6 @@
-addappid(2889660)
-addappid(2889661,0,"8de65c7822383b2d93b5dc0b44e946ab936809a2da93c49d043f289e50f957a7")
-setManifestid(2889661,"5736633611681511718")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2889660.lua
+--Gamename Sex Formula
+addappid(2889660, 1, "d3523390fd2bf3fd32365e3e35baa658babf67003c3c0d5f04dabe420f4611d1") --Mainappid Sex Formula
+addappid(2889661, 1, "8de65c7822383b2d93b5dc0b44e946ab936809a2da93c49d043f289e50f957a7") --Main Windows Depot Sex Formula
+setManifestid(2889661, "3578466596962271269", 6198640576)
