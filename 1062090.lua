81e2474221be68051b415053e49655adf5e34b85
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:53:16 +0800

    add 1062090.lua

--- 1062090.lua
+++ 1062090.lua
@@ -1,7 +1,13 @@
-addappid(1062090)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1062091,0,"e6291a6d488f1366425014f5742483a513e8771437644a531111ffc9e9674be4")
-setManifestid(1062091,"8428754161272443208")
-addappid(1062092,0,"07decf196b134aeb2362a7dcdf1ebce51004f759e44961e838189f59f9943267")
-setManifestid(1062092,"2432352389034748122")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1062090.lua
+--Gamename Timberborn
+addappid(1062090, 1, "ae26e4878b346db42adc3555b618ae1bc88ccd1cca9758d27b4e7c6e41ea64df") --Mainappid Timberborn
+addappid(1062091, 1, "e6291a6d488f1366425014f5742483a513e8771437644a531111ffc9e9674be4") --Main Windows Depot Timberborn
+setManifestid(1062091, "556962574473221825", 1211865168)
+addappid(1062092, 1, "07decf196b134aeb2362a7dcdf1ebce51004f759e44961e838189f59f9943267") --Main Macos Depot Timberborn
+setManifestid(1062092, "1820628757349473286", 1221294096)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
