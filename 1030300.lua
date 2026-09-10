283d7b65d428d2353ad3af6fa9609ad08464b217
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:01:28 +0800

    add 1030300.lua

---
+++ 1030300.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1030300.lua
+--Gamename Hollow Knight: Silksong
+addappid(1030300, 1, "a33af0e3d40507ddbcce0f87bd8647020d74f5775c60c5f57833c78d1d0be546") --Mainappid Hollow Knight: Silksong
+addappid(1030301, 1, "f4a253f96969f93b8a9392cddf6f1b779886ea10a160e7ca148b92bae09abd28") --Main Windows Depot Hollow Knight: Silksong
+setManifestid(1030301, "4421626056705534276", 2252028704)
+addappid(1030302, 1, "BC736FB6E74601F627B4890559822B4B8E786E36E949B881DCD25419E8A15E9E") --Main Macos Depot Hollow Knight: Silksong
+setManifestid(1030302, "4136280015582261500", 2265968544)
+addappid(1030303, 1, "EA761F41BD28AE29FD22375E81702EADC64796099FD9DCF01EA2A190536CC7C3") --Main Linux Depot Hollow Knight: Silksong
+setManifestid(1030303, "7921642076658611197", 2274106352)
