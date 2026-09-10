9dc6ec69b8edb927e89a581960a939bdb98f5f16
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:37:41 +0800

    add 2375550.lua

--- 2375550.lua
+++ 2375550.lua
@@ -1,4 +1,10 @@
-addappid(2375550)
-setManifestid(228989,"3514306556860204959")
-addappid(2375551,0,"98c80a1e378de4c0c534551cba0c53e74326cd8ff2f9360ad6fa4f5588a4729e")
-setManifestid(2375551,"357163534041889933")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2375550.lua
+--Gamename Like a Dragon Gaiden: The Man Who Erased His Name
+addappid(2375550, 1, "aa6250ced91007be0a081e942217b34e48e5e23e947beed70fcbc06b3636a090") --Mainappid Like a Dragon Gaiden: The Man Who Erased His Name
+addappid(2375551, 1, "98c80a1e378de4c0c534551cba0c53e74326cd8ff2f9360ad6fa4f5588a4729e") --Main Windows Depot Like a Dragon Gaiden: The Man Who Erased His Name
+setManifestid(2375551, "357163534041889933", 70517459824)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2438750) --Dlcname Like a Dragon Gaiden: The Man Who Erased His Name - Legendary Fighter Pack
