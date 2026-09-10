2da6acd81e7bf8ca44d10bed9a05510167f071c1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:01:32 +0800

    add 493840.lua

--- 493840.lua
+++ 493840.lua
@@ -1,3 +1,52 @@
-addappid(493840)
-addappid(493841,0,"377c6811b90f0807dd4ab4c98f0b46f3ca19ff8b2a974286018d41c8f89589ca")
-setManifestid(493841,"7735204537352731644")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 493840.lua
+--Gamename Marvel vs. Capcom: Infinite
+addappid(493840) --Mainappid Marvel vs. Capcom: Infinite
+addappid(493841, 1, "377c6811b90f0807dd4ab4c98f0b46f3ca19ff8b2a974286018d41c8f89589ca") --Main Windows Depot Marvel vs. Capcom: Infinite
+setManifestid(493841, "7735204537352731644", 63237960288)
+addappid(622990) --Dlcname AppID 622990
+addtoken(622990, "16624500403512656266")
+addappid(622991) --Dlcname AppID 622991
+addtoken(622991, "13595829997172150562")
+addappid(623000) --Dlcname AppID 623000
+addtoken(623000, "4147311097826625792")
+addappid(623010) --Dlcname AppID 623010
+addtoken(623010, "13847114958878837060")
+addappid(623020) --Dlcname AppID 623020
+addtoken(623020, "386434365575729803")
+addappid(721100) --Dlcname Marvel vs. Capcom: Infinite - Venom
+addappid(721101) --Dlcname Marvel vs. Capcom: Infinite - Black Panther
+addappid(721102) --Dlcname Marvel vs. Capcom: Infinite - Winter Soldier
+addappid(721103) --Dlcname Marvel vs. Capcom: Infinite - Black Widow
+addappid(721104) --Dlcname Marvel vs. Capcom: Infinite - Sigma
+addappid(721105) --Dlcname Marvel vs. Capcom: Infinite - Monster Hunter
+addappid(721106) --Dlcname Marvel vs. Capcom: Infinite - Captain Marvel Warbird Costume
+addappid(721107) --Dlcname Marvel vs. Capcom: Infinite - Superior Spider-Man Costume
+addappid(721108) --Dlcname Marvel vs. Capcom: Infinite - Joe Fixit Costume
+addappid(721109) --Dlcname Marvel vs. Capcom: Infinite - Captain America Gladiator Costume
+addappid(721110) --Dlcname Marvel vs. Capcom: Infinite - Superior Iron Man Costume
+addappid(721111) --Dlcname Marvel vs. Capcom: Infinite - Ultimate Thor Costume
+addappid(721112) --Dlcname Marvel vs. Capcom: Infinite - Doctor Strange Illuminati Costume
+addappid(721113) --Dlcname Marvel vs. Capcom: Infinite - Space Suit Costume
+addappid(721114) --Dlcname Marvel vs. Capcom: Infinite - Nova Prime Costume
+addappid(721115) --Dlcname Marvel vs. Capcom: Infinite - Ghost Rider Outlaw Costume
+addappid(721116) --Dlcname Marvel vs. Capcom: Infinite - Ultimate Hawkeye Costume
+addappid(721117) --Dlcname Marvel vs. Capcom: Infinite - Dormammu Molten Costume
+addappid(721118) --Dlcname Marvel vs. Capcom: Infinite - Thanos Annihilation Costume
+addappid(721119) --Dlcname Marvel vs. Capcom: Infinite - Ultron Conquest Costume
+addappid(721130) --Dlcname Marvel vs. Capcom: Infinite - Gamora Classic Costume
+addappid(721131) --Dlcname Marvel vs. Capcom: Infinite - Ryu Wanderer Costume
+addappid(721132) --Dlcname Marvel vs. Capcom: Infinite - Dante Nephilim Costume
+addappid(721133) --Dlcname Marvel vs. Capcom: Infinite - Morrigan Night Warrior Costume
+addappid(721140) --Dlcname Marvel vs. Capcom: Infinite - Strider Hien Costume
+addappid(721141) --Dlcname Marvel vs. Capcom: Infinite - Chun-Li Casual Costume
+addappid(721142) --Dlcname Marvel vs. Capcom: Infinite - Firebrand Ultimate Costume
+addappid(721143) --Dlcname Marvel vs. Capcom: Infinite - Chris Classic Costume
+addappid(721144) --Dlcname Marvel vs. Capcom: Infinite - Frank West Proto Man Costume
+addappid(721145) --Dlcname Marvel vs. Capcom: Infinite - Special Zero Costume
+addappid(721146) --Dlcname Marvel vs. Capcom: Infinite - Haggar Metro City Mayor Costume
+addappid(721147) --Dlcname Marvel vs. Capcom: Infinite - Nemesis Tyrant Costume
+addappid(721148) --Dlcname Marvel vs. Capcom: Infinite - Spencer Gottfried Costume
+addappid(721149) --Dlcname Marvel vs. Capcom: Infinite - Arthur Fallen Angel Armor Costume
+addappid(721150) --Dlcname Marvel vs. Capcom: Infinite - Special X Costume
+addappid(721151) --Dlcname Marvel vs. Capcom: Infinite - Jedah Makai Messiah Costume
