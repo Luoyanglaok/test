786893a974f42926f3ca46e0d33f002a9921a677
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:45:23 +0800

    add 241540.lua

--- 241540.lua
+++ 241540.lua
@@ -1,13 +1,19 @@
-addappid(241540)
-addappid(228983)
-setManifestid(228983,"8124929965194586177")
-addappid(228984)
-setManifestid(228984,"2547553897526095397")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(241541,0,"0920be735ac293e7239fd59281947688658aca80975371b55bb0f9731860f642")
-setManifestid(241541,"2191603663082571190")
-addappid(259040,0,"aa233e11a648c9a6265c6de6622fd80cac25a2818fce0747ffeeb9290bfd8702")
-setManifestid(259040,"3714156624226948039")
-addappid(259041,0,"d2b4149580106f83ac63ca91e6debdb7f6c9486cf33912d182cdcee13fc7f09f")
-setManifestid(259041,"2737385718303827107")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 241540.lua
+--Gamename State of Decay
+addappid(241540) --Mainappid State of Decay
+addappid(241541, 1, "0920be735ac293e7239fd59281947688658aca80975371b55bb0f9731860f642") --Main Windows Depot State of Decay
+setManifestid(241541, "2191603663082571190", 0)
+--Dlcname State of Decay - Breakdown
+addappid(259040, 1, "aa233e11a648c9a6265c6de6622fd80cac25a2818fce0747ffeeb9290bfd8702") --Dlcname State of Decay - Breakdown
+setManifestid(259040, "3714156624226948039", 0)
+--Dlcname State of Decay - Lifeline
+addappid(259041, 1, "d2b4149580106f83ac63ca91e6debdb7f6c9486cf33912d182cdcee13fc7f09f") --Dlcname State of Decay - Lifeline
+setManifestid(259041, "2737385718303827107", 0)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
