c90d8b324102ab196f2299beca30b437fc925acc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:49:55 +0800

    add 1844380.lua

--- 1844380.lua
+++ 1844380.lua
@@ -1,5 +1,17 @@
-addappid(1844380)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1844381,0,"34ab542f6d7b2eb7aa9e7623c37b81259208343c7989509c7dc2e559405fa22d")
-setManifestid(1844381,"6369046589173515865")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1844380.lua
+--Gamename Warhammer Age of Sigmar: Realms of Ruin
+addappid(1844380, 1, "9396dc4eaa05ffc9f68b32bbc68e7e07c14fb63d877934e85ae3490796c6c1f1") --Mainappid Warhammer Age of Sigmar: Realms of Ruin
+addappid(1844381, 1, "34ab542f6d7b2eb7aa9e7623c37b81259208343c7989509c7dc2e559405fa22d") --Main Windows Depot Warhammer Age of Sigmar: Realms of Ruin
+setManifestid(1844381, "6369046589173515865", 14855007984)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2531100) --Dlcname Warhammer Age of Sigmar: Realms of Ruin - The Yndrasta, Celestial Spear Pack
+addappid(2531110) --Dlcname Warhammer Age of Sigmar: Realms of Ruin - The Gobsprakk, The Mouth of Mork Pack
+addappid(2531120) --Dlcname Warhammer Age of Sigmar: Realms of Ruin - Preorder Bonus
+addappid(2531130) --Dlcname Warhammer Age of Sigmar: Realms of Ruin Deluxe Upgrade Pack
+addappid(2744270) --Dlcname Warhammer Age of Sigmar: Realms of Ruin - Kurdoss Valentian, The Craven King
+addappid(2744280) --Dlcname Warhammer Age of Sigmar: Realms of Ruin - Gaunt Summoner
