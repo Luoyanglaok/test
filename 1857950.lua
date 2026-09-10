f08b5f82144720cfd4f51bce6efe13248b4743c7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:57:03 +0800

    add 1857950.lua

---
+++ 1857950.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1857950.lua
+--Gamename VEIN
+addappid(1857950, 1, "14814ce04f771b51d99954d9dc14ecb75f1ef2c70879cffa4045314a233c2b15") --Mainappid VEIN
+addappid(1857951, 1, "e414cf1e5b3b3a2034a90ccb1b067f244b73e91fd8fe331fde0149260b69cb62") --Main Linux Depot VEIN
+setManifestid(1857951, "4069034771383874312", 27469968416)
+addappid(1857952, 1, "0f09efc4c8566ce587f3329874cc02f750a58ad24fa6d30c8154b1b95cf2f050") --Main Windows Depot VEIN
+setManifestid(1857952, "5423777580105299402", 27570388896)
+--Dlcname VEIN Soundtrack
+addappid(4001710) --Dlcname VEIN Soundtrack
+addappid(4001711, 1, "921f5bf17662c176cadeaf48f2aefdfc387dfa0379ec993d322c232961d1cfbe") --Dlc Windows Depot VEIN Soundtrack
+setManifestid(4001711, "4993372614874082457", 178907008)
+addappid(4001712, 1, "5a1439fbed750126a04128bfa74f54c97622ec72404317d05fe67486e6c3e50d") --Dlc Windows Depot VEIN Soundtrack
+setManifestid(4001712, "3391712251978246752", 1231110192)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
