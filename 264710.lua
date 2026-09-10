73b34be03ecbcc54cbe79605bb26efaaa957d68c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:52:35 +0800

    add 264710.lua

--- 264710.lua
+++ 264710.lua
@@ -1,6 +1,15 @@
-addappid(264710)
-setManifestid(228985,"3966345552745568756")
-addappid(264712,0,"d764b2a41e9bdbf338f04948309614ea6f8d462073a610940aaf10cbfc35aa85")
-setManifestid(264712,"3638894940716012854")
-addappid(264713,0,"6da396b527d2b570a039778566cef5c1e94ba486fefa835553a1831e40353dec")
-setManifestid(264713,"6406809907156750445")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 264710.lua
+--Gamename Subnautica
+addappid(264710, 1, "d8528d3d73339cb0ac1f14dcfb2410e30927f473d6121df434f0a7c31eda1568") --Mainappid Subnautica
+addappid(264712, 1, "d764b2a41e9bdbf338f04948309614ea6f8d462073a610940aaf10cbfc35aa85") --Main Windows Depot Subnautica
+setManifestid(264712, "4495119477327785570", 3164035872)
+addappid(264713, 1, "6da396b527d2b570a039778566cef5c1e94ba486fefa835553a1831e40353dec") --Main Macos Depot Subnautica
+setManifestid(264713, "6303477024342771846", 3162273552)
+--Dlcname Subnautica Soundtrack
+addappid(1619300) --Dlcname Subnautica Soundtrack
+addappid(1619301, 1, "462060463aa5916ef318a23bb44c53ce357929744fcbf66ac08bbf6a70edf846") --Dlc Windows Depot Subnautica Soundtrack
+setManifestid(1619301, "8891334160387689213", 340017488)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
