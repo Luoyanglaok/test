d0eebecf91e992738c5034f9f94b4ec4633a09ef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:06:05 +0800

    add 3892270.lua

---
+++ 3892270.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3892270.lua
+--Gamename Gamble With Your Friends
+addappid(3892270, 1, "792fb0e3dedf02f8756774da38656b5b7212250cf8e33822e6b27199b2530385") --Mainappid Gamble With Your Friends
+addappid(3892271, 1, "90695be6010d5ec2e8890f1d742357316f4d5f74a8a9d22b2ae076b184797167") --Main Windows Depot Gamble With Your Friends
+setManifestid(3892271, "4322338465787944513", 857538992)
