b8211c763f9dbfb0177ad51ea2c8f02428797010
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:57:02 +0800

    add 1072420.lua

--- 1072420.lua
+++ 1072420.lua
@@ -1,7 +1,15 @@
-addappid(1072420)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(1072421,0,"ea3e24679754c7e32fd45950b725fab07df1573119307b7c85950f38fc293ecc")
-setManifestid(1072421,"8016507788492773105")
-addappid(1162620,0,"7c87d006544dc296935202dbe2c8041875f6d5652b052f610098412bf718a3e9")
-setManifestid(1162620,"57645546756447385")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1072420.lua
+--Gamename DRAGON QUEST BUILDERS™ 2
+addappid(1072420) --Mainappid DRAGON QUEST BUILDERS™ 2
+addappid(1072421, 1, "ea3e24679754c7e32fd45950b725fab07df1573119307b7c85950f38fc293ecc") --Main Windows Depot DRAGON QUEST BUILDERS™ 2
+setManifestid(1072421, "8016507788492773105", 4106064960)
+--Dlcname AppID 1162620
+addtoken(1162620, "17437710400044353331")
+addappid(1162620, 1, "7c87d006544dc296935202dbe2c8041875f6d5652b052f610098412bf718a3e9") --Dlcname AppID 1162620
+setManifestid(1162620, "57645546756447385", 0)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
