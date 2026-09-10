0fc7ae817a33047d3dd1c6cf05d59397eb69640f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:58:49 +0800

    add 2654540.lua

--- 2654540.lua
+++ 2654540.lua
@@ -1,3 +1,9 @@
-addappid(2654540)
-addappid(2654541,0,"593e4681bf6fca95ddd5ce89407908fa324bdbeb85e7a28dfb1356afb44dc55f")
-setManifestid(2654541,"1839822331184078594")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2654540.lua
+--Gamename 无休仙途
+addappid(2654540) --Mainappid 无休仙途
+addappid(2654541, 1, "593e4681bf6fca95ddd5ce89407908fa324bdbeb85e7a28dfb1356afb44dc55f") --Main Windows Depot 无休仙途
+setManifestid(2654541, "4150250272371322538", 586040160)
+addappid(3367330) --Dlcname 無休仙途/EndlessJourney - 天命玄女
+addappid(3367340) --Dlcname 無休仙途/EndlessJourney - 七星奕者
+addappid(4148560) --Dlcname 無休仙途/EndlessJourney - 雷罚天君
