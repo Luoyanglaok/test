173bb598be67e48d9dd03ae15954baaebdb92ca8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:08:29 +0800

    add 218620.lua

--- 218620.lua
+++ 218620.lua
@@ -1,38 +1,162 @@
-addappid(218620)
-setManifestid(228982,"6413394087650432851")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229031,"7746630274301172884")
-addappid(218621,0,"2d0de203756909d9b2b65506594648ce773a0428741fcae5208616bd9c79d3ea")
-setManifestid(218621,"5646129736559894459")
-addappid(218623,0,"125afa058982488c55e77fc1ed4e1a4f160786f8383db3db5ed5510d760611b6")
-setManifestid(218623,"7700458186423454139")
-addappid(218624,0,"deab327ada094059df67910f69c19cfffeb79aff089df65433f1760ad23bf639")
-setManifestid(218624,"6736605256120450039")
-addappid(218625,0,"af7ebe5bfa557f52f7d9993481d10385f5eb32adf77936bc8d9a4f66a9d80fdc")
-setManifestid(218625,"3325538660884987003")
-addappid(218626,0,"90e5e5d05e46661b2c26e7194d6531796a4017bca0f5e5900cd79f01dd45537b")
-setManifestid(218626,"8111024477834407195")
-addappid(218627,0,"5baf56fb99687983be6e6c22a7be8889468866a4eda552bbe3dc5ffc212081b7")
-setManifestid(218627,"5146760485964231618")
-addappid(218628,0,"bf4eac2b5b55bdc1264f00e3f24193e6c82c01284b02f3af2137ab8915211e86")
-setManifestid(218628,"2507768204192281825")
-addappid(218629,0,"26cbf329bd73db8f9e7beefe23857b1e0c76b3aef5d3e9e4e6b5b3f31be0ddc5")
-setManifestid(218629,"6307434253818453593")
-addappid(218631,0,"1c394d148b286577ab96453164cb03ded6619ccfad3da0f01aac23d71302d1e2")
-setManifestid(218631,"2916203722723946867")
-addappid(218630,0,"80aa89abaa061beb1ccf5cb5f1ab79eb7cf11f79c2767f5477e2ae92675f9e55")
-setManifestid(218630,"5536271309787494781")
-addappid(254260,0,"685f9ea20bba151bc1afd38d564fbeb79714127d8860e6e3709b42597942586e")
-setManifestid(254260,"7885621803433237728")
-addappid(368870,0,"88e548381947ae497f4412b47a2fb962ef3a6c85ac20bff4e3ebcce1b608e5f8")
-setManifestid(368870,"1337488452970909033")
-addappid(267381,0,"6eb9e5fd6c12890fb9bc3130a6e968b9f8e369df554af53e073e8dae99f40c7f")
-setManifestid(267381,"8983085739249833412")
-addappid(2076560,0,"107c80c6f6e8200462fb697af71577ad703e4a640b1f64850e60e7fae6226f9b")
-setManifestid(2076560,"5245040824892464413")
-addappid(218632,0,"2851e3544784ade4486f7cd973ac593c8b7232e951adb1ab11037c750e05c560")
-setManifestid(218632,"7553784073178780379")
-addappid(974460,0,"876285ae8e877eddef056b17241e282dc5b3e09c99fa91986425309be9782add")
-setManifestid(974460,"7712954533225333834")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 218620.lua
+--Gamename PAYDAY 2
+addappid(218620, 1, "b005cdff158b60e65c0c26498b91d145f8e68cfc09c793ba62716d87c1a4369e") --Mainappid PAYDAY 2
+addappid(218621, 1, "2d0de203756909d9b2b65506594648ce773a0428741fcae5208616bd9c79d3ea") --Main Windows Depot PAYDAY 2
+setManifestid(218621, "6226081920218254228", 32495987568)
+addappid(218623, 1, "125afa058982488c55e77fc1ed4e1a4f160786f8383db3db5ed5510d760611b6") --Main Windows Language Depot Russian PAYDAY 2
+setManifestid(218623, "7700458186423454139", 0)
+addappid(218624, 1, "deab327ada094059df67910f69c19cfffeb79aff089df65433f1760ad23bf639") --Main Windows Language Depot Dutch PAYDAY 2
+setManifestid(218624, "6736605256120450039", 0)
+addappid(218625, 1, "af7ebe5bfa557f52f7d9993481d10385f5eb32adf77936bc8d9a4f66a9d80fdc") --Main Windows Language Depot French PAYDAY 2
+setManifestid(218625, "3325538660884987003", 0)
+addappid(218626, 1, "90e5e5d05e46661b2c26e7194d6531796a4017bca0f5e5900cd79f01dd45537b") --Main Windows Language Depot German PAYDAY 2
+setManifestid(218626, "8111024477834407195", 0)
+addappid(218627, 1, "5baf56fb99687983be6e6c22a7be8889468866a4eda552bbe3dc5ffc212081b7") --Main Windows Language Depot Italian PAYDAY 2
+setManifestid(218627, "5146760485964231618", 0)
+addappid(218628, 1, "bf4eac2b5b55bdc1264f00e3f24193e6c82c01284b02f3af2137ab8915211e86") --Main Windows Language Depot Spanish PAYDAY 2
+setManifestid(218628, "2507768204192281825", 0)
+addappid(218629, 1, "26cbf329bd73db8f9e7beefe23857b1e0c76b3aef5d3e9e4e6b5b3f31be0ddc5") --Main Windows Language Depot Japanese PAYDAY 2
+setManifestid(218629, "6307434253818453593", 656)
+addappid(218631, 1, "1c394d148b286577ab96453164cb03ded6619ccfad3da0f01aac23d71302d1e2") --Main Windows Language Depot Schinese PAYDAY 2
+setManifestid(218631, "2916203722723946867", 672)
+--Dlcname PAYDAY 2: Career Criminal Content
+addappid(218630, 1, "80aa89abaa061beb1ccf5cb5f1ab79eb7cf11f79c2767f5477e2ae92675f9e55") --Dlcname PAYDAY 2: Career Criminal Content
+setManifestid(218630, "5536271309787494781", 0)
+--Dlcname PAYDAY 2: The Official Soundtrack
+addappid(254260, 1, "685f9ea20bba151bc1afd38d564fbeb79714127d8860e6e3709b42597942586e") --Dlcname PAYDAY 2: The Official Soundtrack
+setManifestid(254260, "7885621803433237728", 2309672112)
+--Dlcname PAYDAY 2: A Merry Payday Christmas Soundtrack
+addappid(267381, 1, "6eb9e5fd6c12890fb9bc3130a6e968b9f8e369df554af53e073e8dae99f40c7f") --Dlcname PAYDAY 2: A Merry Payday Christmas Soundtrack
+setManifestid(267381, "8983085739249833412", 407782656)
+--Dlcname PAYDAY 2: The B-Sides Soundtrack
+addappid(368870, 1, "88e548381947ae497f4412b47a2fb962ef3a6c85ac20bff4e3ebcce1b608e5f8") --Dlcname PAYDAY 2: The B-Sides Soundtrack
+setManifestid(368870, "7190612300832133696", 296481648)
+--Dlcname PAYDAY 2: Tijuana Music Pack
+addappid(2076560, 1, "107c80c6f6e8200462fb697af71577ad703e4a640b1f64850e60e7fae6226f9b") --Dlcname PAYDAY 2: Tijuana Music Pack
+setManifestid(2076560, "5811614798887510953", 528610048)
+--Dlcname PAYDAY 2: Chinatown Music Pack
+addappid(2353511) --Dlcname PAYDAY 2: Chinatown Music Pack
+addappid(2418070, 1, "9a46ff4127b070013ea0189940cec49551f33a7e20b2c6b612565be0502a5a78") --Dlc Windows Depot PAYDAY 2: Chinatown Music Pack
+setManifestid(2418070, "3463837528970200612", 134618240)
+addappid(2418071, 1, "294731a01d179f2db60818570da445503017dd973ac474291fbc855e20502be3") --Dlc Windows Depot PAYDAY 2: Chinatown Music Pack
+setManifestid(2418071, "3577794892998287564", 433469184)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229031, 1, "98d01f1eaa90ae08f978994e043bcbbf1f23fc72a325528ca7d722c827c0eb54") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229031, "7746630274301172884", 25905472)
+addappid(247450) --Dlcname PAYDAY 2: Pre-order Bonus
+addappid(259380) --Dlcname PAYDAY 2: Gage Sniper Pack
+addappid(259381) --Dlcname PAYDAY 2: Gage Mod Courier
+addappid(264610) --Dlcname PAYDAY 2: Armored Transport
+addappid(267380) --Dlcname PAYDAY 2: Gage Weapon Pack #01
+addappid(267382) --Dlcname PAYDAY 2: The Shadow Raid Heist
+addappid(271110) --Dlcname PAYDAY 2: Charlie Santa Heist
+addappid(274160) --Dlcname PAYDAY 2: The Diamond Store Heist
+addappid(274161) --Dlcname PAYDAY 2: Free Content #2
+addappid(275590) --Dlcname PAYDAY 2: Gage Weapon Pack #02
+addappid(284430) --Dlcname PAYDAY 2: Death Wish Update
+addappid(288900) --Dlcname PAYDAY 2: The Election Day Heist
+addappid(306110) --Dlcname PAYDAY 2: Humble Mask Pack
+addappid(306690) --Dlcname PAYDAY 2: The Big Bank Heist
+addappid(311050) --Dlcname PAYDAY 2: Gage Shotgun Pack
+addappid(318720) --Dlcname PAYDAY 2: Alienware Alpha Mauler
+addappid(320030) --Dlcname PAYDAY 2: Gage Assault Pack
+addappid(323500) --Dlcname PAYDAY 2: Hotline Miami
+addappid(328860) --Dlcname PAYDAY 2: Bobblehead DLC
+addappid(328861) --Dlcname PAYDAY 2: Alienware Alpha Mask Pack
+addappid(330010) --Dlcname PAYDAY 2: John Wick
+addtoken(330010, "16514420279682145073")
+addappid(330490) --Dlcname AppID 330490
+addtoken(330490, "9615560963107885536")
+addappid(330491) --Dlcname AppID 330491
+addtoken(330491, "7774797695476584217")
+addappid(331040) --Dlcname AppID 331040
+addtoken(331040, "12497427485668692480")
+addappid(337660) --Dlcname PAYDAY 2: The Diamond Heist
+addappid(337661) --Dlcname PAYDAY 2: Clover Character Pack
+addappid(338950) --Dlcname PAYDAY 2: The PAYDAYCON 2015 Mask Pack
+addappid(338951) --Dlcname AppID 338951
+addappid(339480) --Dlcname PAYDAY 2: The Bomb Heists
+addappid(344140) --Dlcname PAYDAY 2: Dragan Character Pack
+addappid(348090) --Dlcname PAYDAY 2: The OVERKILL Pack
+addappid(348091) --Dlcname PAYDAY 2: The COMPLETELY OVERKILL Pack
+addappid(349830) --Dlcname PAYDAY 2: The Butcher's Western Pack
+addappid(351890) --Dlcname PAYDAY 2: The Butcher's AK/CAR Mod Pack
+addappid(358150) --Dlcname PAYDAY 2: The Butcher's BBQ Pack
+addappid(366660) --Dlcname PAYDAY 2: The Alesso Heist
+addappid(374300) --Dlcname PAYDAY 2: The Golden Grin Casino Heist
+addappid(374301) --Dlcname PAYDAY 2: Sokol Character Pack
+addappid(375380) --Dlcname PAYDAY 2: Humble Mask Pack 3
+addappid(375381) --Dlcname PAYDAY 2: Humble Mask Pack 4
+addappid(375382) --Dlcname PAYDAY 2: The Jack Mask Pack
+addappid(375383) --Dlcname PAYDAY 2: The Queen Mask Pack
+addappid(375384) --Dlcname PAYDAY 2: The King Mask Pack
+addappid(375385) --Dlcname PAYDAY 2: The Joker Mask Pack
+addappid(384020) --Dlcname PAYDAY 2: Yakuza Character Pack
+addappid(384021) --Dlcname PAYDAY 2: Gage Ninja Pack
+addappid(401650) --Dlcname PAYDAY 2: Gage Chivalry Pack
+addappid(422400) --Dlcname PAYDAY 2: The Point Break Heists
+addappid(433730) --Dlcname PAYDAY 2: The Goat Simulator Heist
+addappid(441600) --Dlcname PAYDAY 2: The Wolf Pack
+addappid(450660) --Dlcname PAYDAY 2: Biker Character Pack
+addappid(468410) --Dlcname PAYDAY 2: Sydney Character Pack
+addappid(486610) --Dlcname PAYDAY 2: Humble Mask Pack 5
+addappid(487210) --Dlcname PAYDAY 2: The Biker Heist
+addappid(518760) --Dlcname PAYDAY 2: Community Safe Reward 1
+addappid(532810) --Dlcname PAYDAY 2: Housewarming Party
+addappid(545100) --Dlcname PAYDAY 2: John Wick Weapon Pack
+addappid(548420) --Dlcname PAYDAY 2: Gage Spec Ops Pack
+addappid(548421) --Dlcname PAYDAY 2: Scarface Heist
+addappid(612900) --Dlcname PAYDAY 2: Gage Russian Weapon Pack
+addappid(758420) --Dlcname PAYDAY 2: h3h3 Character Pack
+addappid(826090) --Dlcname PAYDAY 2: VR
+addappid(1184410) --Dlcname PAYDAY 2: Tailor Pack 1
+addappid(1184411) --Dlcname PAYDAY 2: Border Crossing Heist
+addappid(1184412) --Dlcname PAYDAY 2: Cartel Optics Mod Pack
+addappid(1252200) --Dlcname PAYDAY 2: San Martín Bank Heist
+addappid(1255150) --Dlcname PAYDAY 2: Tailor Pack 2
+addappid(1255151) --Dlcname PAYDAY 2: Federales Weapon Pack
+addappid(1255152) --Dlcname PAYDAY 2: Weapon Color Pack 1
+addappid(1347750) --Dlcname PAYDAY 2: Breakfast in Tijuana Heist
+addappid(1347751) --Dlcname PAYDAY 2: Weapon Color Pack 2
+addappid(1351060) --Dlcname PAYDAY 2: Fugitive Weapon Pack
+addappid(1449440) --Dlcname PAYDAY 2: Tailor Pack 3
+addappid(1449441) --Dlcname PAYDAY 2: Gunslinger Weapon Pack
+addappid(1449442) --Dlcname PAYDAY 2: Weapon Color Pack 3
+addappid(1449450) --Dlcname PAYDAY 2: Buluc's Mansion Heist
+addappid(1555040) --Dlcname PAYDAY 2: Dragon Pack
+addappid(1555050) --Dlcname PAYDAY 2: Jiu Feng Smuggler Pack
+addappid(1619070) --Dlcname PAYDAY 2: Jiu Feng Smuggler Pack 2
+addappid(1654480) --Dlcname PAYDAY 2: The Ukrainian Prisoner Heist
+addappid(1654481) --Dlcname PAYDAY 2: Guardians Tailor Pack
+addappid(1746580) --Dlcname PAYDAY 2: Jiu Feng Smuggler Pack 3
+addappid(1778790) --Dlcname PAYDAY 2: Black Cat Heist
+addappid(1778791) --Dlcname PAYDAY 2: Mega City Tailor Pack
+addappid(1824880) --Dlcname PAYDAY 2: Winter Ghosts Tailor Pack
+addappid(1901420) --Dlcname PAYDAY 2: Jiu Feng Smuggler Pack 4
+addappid(1906240) --Dlcname PAYDAY 2: Mountain Master Heist
+addappid(1907080) --Dlcname PAYDAY 2: Golden Dagger Tailor Pack
+addappid(1945680) --Dlcname PAYDAY 2: Southbound Tailor Pack
+addappid(1945681) --Dlcname PAYDAY 2: Midland Ranch Heist
+addappid(1945682) --Dlcname PAYDAY 2: McShay Weapon Pack
+addappid(2074220) --Dlcname PAYDAY 2: High Octane Tailor Pack
+addappid(2074230) --Dlcname PAYDAY 2: McShay Weapon Pack 2
+addappid(2074240) --Dlcname PAYDAY 2: Lost in Transit Heist
+addappid(2200970) --Dlcname PAYDAY 2: McShay Weapon Pack 3
+addappid(2200971) --Dlcname PAYDAY 2: Street Smart Tailor Pack
+addappid(2215010) --Dlcname PAYDAY 2: Hostile Takeover Heist
+addappid(2218650) --Dlcname PAYDAY 2: McShay Mod Pack
+addappid(2352300) --Dlcname PAYDAY 2: Lawless Tailor Pack
+addappid(2353510) --Dlcname PAYDAY 2: McShay Weapon Pack 4
+addappid(2353512) --Dlcname PAYDAY 2: Crude Awakening Heist
+addappid(3188980) --Dlcname PAYDAY 2: Jacket Character Pack
+addappid(3847540) --Dlcname PAYDAY 2: Subscription
+addappid(4543600) --Dlcname PAYDAY 2: Espionage Weapon Pack
+--Missing Dlcs
+--Dlcname PAYDAY 2: Gage Historical Pack
