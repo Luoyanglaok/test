b5e29e5b9cf5c3f4f9099dbe588b3efab0a9ede6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:38:32 +0800

    add 368500.lua

--- 368500.lua
+++ 368500.lua
@@ -1,32 +1,75 @@
-addappid(368500)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(368501,0,"d1bdef91b5eaec4f3e423ca91fcfdf9431091873ffb54f6f72c040cc953b97e4")
-setManifestid(368501,"3950444377332639251")
-addappid(368502,0,"104abcec788393b8fdd109808cdd448282321f2ca2a2841bd144bf714a6c49c0")
-setManifestid(368502,"7245430940335893091")
-addappid(368503,0,"a141f6692cc60dbbf88df0cb80c64887a85c1ab11b4f8c6b5f32c58443d5be37")
-setManifestid(368503,"8445202823339236077")
-addappid(405080,0,"0b90af327ed0210d31b63ebaf92971acbc3dd0d28ce213b5c7da0915a041b48a")
-setManifestid(405080,"7130849894187701516")
-addappid(368504,0,"fe94c08ddf5cca80f132536cc1fc78f52bb83920c7d7c25a065b1e2af798fdfc")
-setManifestid(368504,"2992954383828961684")
-addappid(368505,0,"8bb3f40ef7ed048b09cb108388bf7a67b3c488bfb865e592a87bf9cc3a4e7854")
-setManifestid(368505,"2445555025373299607")
-addappid(407470,0,"1c2aa26252e28e027a4ef7e686d7557f66d9d1a558c3daeb6a6c4eab6552acfd")
-setManifestid(407470,"3707348865287667255")
-addappid(407471,0,"4fb128097f83ca038a9ca86bf80ca2d030e47c314f274568906d2586ca2b8dd6")
-setManifestid(407471,"1267306434745333523")
-addappid(407472,0,"4ada1b0ac4eaf0c9ddf28bdde57963e0e1bae66413d4c5262aadd3e8764e07c6")
-setManifestid(407472,"6785341095949687424")
-addappid(407473,0,"faec6593cb6d2e6b32626b52677f86bb5201875eee76b2e27cf5b2d00bf5caaa")
-setManifestid(407473,"2255042779327026176")
-addappid(407475,0,"c1506ebebe02f663559ca77f6031bac61c86ea9dede7a44d22e391553d54afda")
-setManifestid(407475,"298960376516436252")
-addappid(407476,0,"c09880ee8e9d7f43f503b453d621475d21260f808c2a8ea3dcad8c84321e6e0a")
-setManifestid(407476,"753387291892218228")
-addappid(407477,0,"2ed345356b1c7f23d41c73dde724b24b97210e4071f17a0014895b3b31c5435f")
-setManifestid(407477,"2631881794433412166")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 368500.lua
+--Gamename Assassin's Creed® Syndicate
+addappid(368500, 1, "57e0d9f6fa7ecb541d39e95abf187a40f428bcd549cd9808393cd3d225f344f3") --Mainappid Assassin's Creed® Syndicate
+addappid(368501, 1, "d1bdef91b5eaec4f3e423ca91fcfdf9431091873ffb54f6f72c040cc953b97e4") --Main Windows Depot Assassin's Creed® Syndicate
+setManifestid(368501, "4815907272273797379", 30799400160)
+addappid(368502, 1, "104abcec788393b8fdd109808cdd448282321f2ca2a2841bd144bf714a6c49c0") --Main Windows Depot Assassin's Creed® Syndicate
+setManifestid(368502, "7245430940335893091", 4007813824)
+addappid(368503, 1, "a141f6692cc60dbbf88df0cb80c64887a85c1ab11b4f8c6b5f32c58443d5be37") --Main Windows Depot Assassin's Creed® Syndicate
+setManifestid(368503, "2273470268360003424", 1047108416)
+--Dlcname Assassin's Creed Syndicate - Jack The Ripper
+addappid(405080, 1, "0b90af327ed0210d31b63ebaf92971acbc3dd0d28ce213b5c7da0915a041b48a") --Dlcname Assassin's Creed Syndicate - Jack The Ripper
+addappid(368504, 1, "fe94c08ddf5cca80f132536cc1fc78f52bb83920c7d7c25a065b1e2af798fdfc") --Dlc Windows Depot Assassin's Creed Syndicate - Jack The Ripper
+setManifestid(368504, "2992954383828961684", 525932800)
+addappid(368505, 1, "8bb3f40ef7ed048b09cb108388bf7a67b3c488bfb865e592a87bf9cc3a4e7854") --Dlc Windows Depot Assassin's Creed Syndicate - Jack The Ripper
+setManifestid(368505, "2445555025373299607", 61211088)
+setManifestid(405080, "7130849894187701516", 8643518272)
+--Dlcname Assassin's Creed Syndicate - Steampunk Pack
+addtoken(407470, "14313500131502997464")
+addappid(407470, 1, "1c2aa26252e28e027a4ef7e686d7557f66d9d1a558c3daeb6a6c4eab6552acfd") --Dlcname Assassin's Creed Syndicate - Steampunk Pack
+setManifestid(407470, "3707348865287667255", 1019378528)
+--Dlcname Assassin's Creed Syndicate - Victorian Legends pack
+addtoken(407471, "11906388779735550720")
+addappid(407471, 1, "4fb128097f83ca038a9ca86bf80ca2d030e47c314f274568906d2586ca2b8dd6") --Dlcname Assassin's Creed Syndicate - Victorian Legends pack
+setManifestid(407471, "1267306434745333523", 1018791824)
+--Dlcname Assassin's Creed Syndicate - Steampunk Outfit for Evie
+addtoken(407472, "10523805908066337608")
+addappid(407472, 1, "4ada1b0ac4eaf0c9ddf28bdde57963e0e1bae66413d4c5262aadd3e8764e07c6") --Dlcname Assassin's Creed Syndicate - Steampunk Outfit for Evie
+setManifestid(407472, "6785341095949687424", 521698448)
+--Dlcname Assassin's Creed Syndicate - Victorian Legends Outfit for Jacob
+addtoken(407473, "9995795017656206850")
+addappid(407473, 1, "faec6593cb6d2e6b32626b52677f86bb5201875eee76b2e27cf5b2d00bf5caaa") --Dlcname Assassin's Creed Syndicate - Victorian Legends Outfit for Jacob
+setManifestid(407473, "2255042779327026176", 528025776)
+--Dlcname Assassin's Creed Syndicate - The Last Maharaja
+addappid(407475, 1, "c1506ebebe02f663559ca77f6031bac61c86ea9dede7a44d22e391553d54afda") --Dlcname Assassin's Creed Syndicate - The Last Maharaja
+setManifestid(407475, "298960376516436252", 2788018064)
+addappid(407476, 1, "c09880ee8e9d7f43f503b453d621475d21260f808c2a8ea3dcad8c84321e6e0a") --Dlc Windows Depot Assassin's Creed Syndicate - The Last Maharaja
+setManifestid(407476, "753387291892218228", 186254208)
+addappid(407477, 1, "2ed345356b1c7f23d41c73dde724b24b97210e4071f17a0014895b3b31c5435f") --Dlc Windows Depot Assassin's Creed Syndicate - The Last Maharaja
+setManifestid(407477, "2631881794433412166", 21244672)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(371020) --Dlcname AppID 371020
+addtoken(371020, "11342798832795940306")
+addappid(407474) --Dlcname Assassin's Creed Syndicate - Streets of London Pack
+addappid(417960) --Dlcname AppID 417960
+addtoken(417960, "15614361081499913217")
+addappid(417961) --Dlcname Assassin's Creed Syndicate - WW Gold - Uplay Activation
+addappid(417962) --Dlcname Assassin's Creed Syndicate - WW - Uplay Activation
+addappid(417963) --Dlcname Assassin's Creed Syndicate - WW Pre-order - Uplay Activation
+addappid(417964) --Dlcname Assassin's Creed Syndicate - RU Pre-order Gold - Uplay Activation
+addappid(417965) --Dlcname Assassin's Creed Syndicate - RU Gold - Uplay Activation
+addappid(417966) --Dlcname Assassin's Creed Syndicate - RU - Uplay Activation
+addappid(417967) --Dlcname Assassin's Creed Syndicate - RU Pre-order - Uplay Activation
+addappid(417968) --Dlcname Assassin's Creed Syndicate - Season Pass - Uplay Activation
+addappid(417970) --Dlcname AppID 417970
+addtoken(417970, "12523998094966539042")
+addappid(417971) --Dlcname Assassin's Creed Syndicate - Steampunk Pack - Uplay Activation
+addappid(417972) --Dlcname Assassin's Creed Syndicate - Victorian Legends Pack - Uplay Activation
+addappid(417973) --Dlcname Assassin's Creed Syndicate - Steampunk Outfit for Evie - Uplay Activation
+addappid(417974) --Dlcname Assassin's Creed Syndicate - Victorian Legends Outfit for Jacob - Uplay Activation
+addappid(417975) --Dlcname AppID 417975
+addtoken(417975, "1860660477798642737")
+addappid(417976) --Dlcname AppID 417976
+addtoken(417976, "15027263869546695518")
+addappid(452960) --Dlcname Assassin's Creed Syndicate - The Dreadful Crimes
+addappid(456770) --Dlcname AppID 456770
+addtoken(456770, "8782124308583257683")
