16395e3343ae0f5eb9905a06b48857c814461930
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:08:32 +0800

    add 1406990.lua

--- 1406990.lua
+++ 1406990.lua
@@ -1,4 +1,15 @@
-addappid(1406990)
-setManifestid(228990,"1829726630299308803")
-addappid(1406991,0,"3a788925e3104dac2a37bd06c1889d4de6ed05d62e672b9f9283f07fd45e3f4c")
-setManifestid(1406991,"334699367672931861")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1406990.lua
+--Gamename NEKOPARA Vol. 4
+addappid(1406990, 1, "1b251dec67aa3454039a85756ce0a541356db129d74674faaf2d08af37ea8625") --Mainappid NEKOPARA Vol. 4
+addappid(1406991, 1, "3a788925e3104dac2a37bd06c1889d4de6ed05d62e672b9f9283f07fd45e3f4c") --Main Windows Depot NEKOPARA Vol. 4
+setManifestid(1406991, "334699367672931861", 2472599616)
+--Dlcname NEKOPARA Vol. 4 - Theme Song
+addappid(1480360) --Dlcname NEKOPARA Vol. 4 - Theme Song
+addappid(1480361, 1, "d0824119fd5746d21e4b7d692a84daa93d04919652954e63a69b1582b284cb91") --Dlc Windows Depot NEKOPARA Vol. 4 - Theme Song
+setManifestid(1480361, "2250345937062015730", 29354096)
+addappid(1480362, 1, "de5d6f817d7753223b10c99bcf402b035618606b29db977f66d7cba4885ef5ba") --Dlc Windows Depot NEKOPARA Vol. 4 - Theme Song
+setManifestid(1480362, "1906404078237090288", 226398032)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
