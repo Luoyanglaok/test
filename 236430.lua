bc9beb91f9a5f9a9ddba137ca45c2b214a16203c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:11:09 +0800

    add 236430.lua

--- 236430.lua
+++ 236430.lua
@@ -1,16 +1,42 @@
-addappid(236430)
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(236432,0,"eda018a11af0a6a37b59a60dadcbfca12bd721227868fcff4053b8b85e67bd05")
-setManifestid(236432,"4547260915165328308")
-addappid(271940,0,"591c3310fabae8ea378bcd7de0d2d6e51021ff65de92140c0869f2a90df0e3ce")
-setManifestid(271940,"3404732443660876107")
-addappid(271941,0,"e6c617c30eb9d95791592e9fb5200627d32cb74264e74e58d55ffd04d01b0ff6")
-setManifestid(271941,"8479411515924661579")
-addappid(271942,0,"a1965b62e56b19e4ff236ec31cef3b501ea253fd4d0f958b9f3db502e7e92e86")
-setManifestid(271942,"1308165959807529893")
-addappid(271943,0,"03066e42170eb2798cc95b53972b74fc6ad4e994b1f0ad1c62720136abbd954a")
-setManifestid(271943,"7128319083619969881")
-addappid(271944,0,"cce41eba79f1afc8fe13f0c97724e9bf9e1fbe22782a2eb17582fdc3a8b5780e")
-setManifestid(271944,"633989536178537058")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 236430.lua
+--Gamename DARK SOULS™ II
+addappid(236430, 1, "3b01703a979f8e646f85a896dfcf285c5b7dc1132a7a8e5dcb4212b82e267246") --Mainappid DARK SOULS™ II
+addappid(236432, 1, "eda018a11af0a6a37b59a60dadcbfca12bd721227868fcff4053b8b85e67bd05") --Main Windows Depot DARK SOULS™ II
+setManifestid(236432, "4547260915165328308", 10633517344)
+addappid(236434, 1, "0b32f571e0c05f8fb40ea9d3d4fb2281c9b5431dc2da21a8cc799f96bd6304f6") --Main Windows Depot DARK SOULS™ II
+setManifestid(236434, "683459295384639511", 10578614448)
+--Dlcname Dark Souls II - Pre-Order DLC ROW
+addappid(271940, 1, "591c3310fabae8ea378bcd7de0d2d6e51021ff65de92140c0869f2a90df0e3ce") --Dlcname Dark Souls II - Pre-Order DLC ROW
+setManifestid(271940, "3404732443660876107", 160)
+--Dlcname Dark Souls II - Digital Extras
+addappid(271941, 1, "e6c617c30eb9d95791592e9fb5200627d32cb74264e74e58d55ffd04d01b0ff6") --Dlcname Dark Souls II - Digital Extras
+setManifestid(271941, "8479411515924661579", 218137936)
+--Dlcname Dark Souls™ II Crown of the Sunken King
+addappid(271942, 1, "a1965b62e56b19e4ff236ec31cef3b501ea253fd4d0f958b9f3db502e7e92e86") --Dlcname Dark Souls™ II Crown of the Sunken King
+setManifestid(271942, "1308165959807529893", 160)
+--Dlcname DARK SOULS™ II Crown of the Old Iron King
+addappid(271943, 1, "03066e42170eb2798cc95b53972b74fc6ad4e994b1f0ad1c62720136abbd954a") --Dlcname DARK SOULS™ II Crown of the Old Iron King
+setManifestid(271943, "7128319083619969881", 160)
+--Dlcname DARK SOULS™ II Crown of the Ivory King
+addappid(271944, 1, "cce41eba79f1afc8fe13f0c97724e9bf9e1fbe22782a2eb17582fdc3a8b5780e") --Dlcname DARK SOULS™ II Crown of the Ivory King
+setManifestid(271944, "633989536178537058", 160)
+--Dlcname Darks Souls II JP Retail Pre-Order DLC 5
+addappid(289122, 1, "b48ee6ed45aa5e4caa03670599af7090b3a522984ceb8dc86e9f9094e5c2e0f4") --Dlcname Darks Souls II JP Retail Pre-Order DLC 5
+setManifestid(289122, "3397237352207390949", 160)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(284450) --Dlcname DARK SOULS™ II - Season Pass
+addappid(355700) --Dlcname Dark Souls II Upgrade to DX11 (no content)
+addtoken(355700, "11688840643228370216")
+--Missing Dlcs
+--Dlcname Dark Souls II - Pre-Order DLC JP
+--Dlcname Darks Souls II JP Retail Pre-Order DLC 1
+--Dlcname Darks Souls II JP Retail Pre-Order DLC 2
+--Dlcname Darks Souls II JP Retail Pre-Order DLC 3
+--Dlcname Darks Souls II JP Retail Pre-Order DLC 4
