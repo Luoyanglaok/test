ff64144b85aa184e4180fcc9873528d9468cdbb0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:57:55 +0800

    add 2527500.lua

--- 2527500.lua
+++ 2527500.lua
@@ -1,3 +1,6 @@
-addappid(2527500)
-addappid(3404450)
-setManifestid(2527501,"5992599505980992417")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2527500.lua
+--Gamename MiSide
+addappid(2527500, 1, "319708828feb58fa2ed02fb00975af201afbd79269c7f91a3a5e69efcf51f4c0") --Mainappid MiSide
+addappid(2527501, 1, "ffef2d1f42cd2438f5b7d029d771a9f941fd224250c3184f595773fb4926e1cd") --Main Windows Depot MiSide
+setManifestid(2527501, "5992599505980992417", 2205630880)
