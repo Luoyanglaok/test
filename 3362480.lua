450d6e0bb55c8bee7058cba2b0084e115436f1d7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:54:23 +0800

    add 3362480.lua

--- 3362480.lua
+++ 3362480.lua
@@ -1,3 +1,15 @@
-addappid(3362480)
-addappid(3362481,0,"048a088dec4738e3d2e1643145aef5055fc6de3718f370b526fa3ae18b79250a")
-setManifestid(3362481,"5288102714809328197")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3362480.lua
+--Gamename Sex Apartment 💫
+addappid(3362480) --Mainappid Sex Apartment 💫
+addappid(3362481, 1, "048a088dec4738e3d2e1643145aef5055fc6de3718f370b526fa3ae18b79250a") --Main Windows Depot Sex Apartment 💫
+setManifestid(3362481, "5288102714809328197", 299681488)
+--Dlcname Sex Apartment 💫 - Digital Artbook
+addappid(3495760, 1, "69e2392b881b5da5883254c9297349ac89e05b301112be173caafdb90177440e") --Dlcname Sex Apartment 💫 - Digital Artbook
+setManifestid(3495760, "376460977206387774", 79142224)
+--Dlcname Sex Apartment 💫 - Wallpapers Pack
+addappid(3495770, 1, "738016fd9bf298bf67c21a717944bdd2d6e568e13c1c6b9189bc67f6e333a03e") --Dlcname Sex Apartment 💫 - Wallpapers Pack
+setManifestid(3495770, "136676638714465394", 79125632)
+--Dlcname Sex Apartment 💫 - Animation Pack
+addappid(3495780, 1, "fcda5130bdc8a1086dd591a1cd69aecdb9005062db06fb55351d51d0e840ad81") --Dlcname Sex Apartment 💫 - Animation Pack
+setManifestid(3495780, "5406825720536430156", 519686784)
