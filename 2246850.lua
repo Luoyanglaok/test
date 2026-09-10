4c424185ea7e8c2d1af48a5a3e4a1eb1bc9ef6c6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:43:56 +0800

    add 2246850.lua

--- 2246850.lua
+++ 2246850.lua
@@ -1,3 +1,6 @@
-addappid(2246850)
-addappid(2246851,0,"c1afb26983c4c7afe285ab916f092dc89c4da470d0f40c22d8a327609aed1cb7")
-setManifestid(2246851,"7855156361388606481")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2246850.lua
+--Gamename SEX Room [18+]
+addappid(2246850) --Mainappid SEX Room [18+]
+addappid(2246851, 1, "c1afb26983c4c7afe285ab916f092dc89c4da470d0f40c22d8a327609aed1cb7") --Main Windows Depot SEX Room [18+]
+setManifestid(2246851, "7855156361388606481", 207072512)
