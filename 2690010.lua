0cb7b70738951f9241094e86a6cd75f549c9fc77
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:56:46 +0800

    add 2690010.lua

--- 2690010.lua
+++ 2690010.lua
@@ -1,5 +1,9 @@
-addappid(2690010)
-addappid(2690011,0,"946fef02fba52477b88e087972a3ae4e300ea926c25fcc7ea0d6be4e8477fe8f")
-setManifestid(2690011,"7795381997969274913")
-addappid(3557260,0,"ad120b9f38c2f7b3830f03119e96844f4ef5ee5a462e19e2dac8adfbea559e91")
-setManifestid(3557260,"3231325698451431824")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2690010.lua
+--Gamename Handyman Fantasy
+addappid(2690010, 1, "f333d7aaf4766b9280ccef3fab44c379e5f8833719fadf7a04e788fed872436c") --Mainappid Handyman Fantasy
+addappid(2690011, 1, "946fef02fba52477b88e087972a3ae4e300ea926c25fcc7ea0d6be4e8477fe8f") --Main Windows Depot Handyman Fantasy
+setManifestid(2690011, "5651562076729746107", 136740960)
+--Dlcname Handyman Fantasy - Digital Art book
+addappid(3557260, 1, "ad120b9f38c2f7b3830f03119e96844f4ef5ee5a462e19e2dac8adfbea559e91") --Dlcname Handyman Fantasy - Digital Art book
+setManifestid(3557260, "3231325698451431824", 39880208)
