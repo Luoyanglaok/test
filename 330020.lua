294940c55661e0a4fc6d986326a8428d58a6d3e7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:46:16 +0800

    add 330020.lua

--- 330020.lua
+++ 330020.lua
@@ -1,17 +1,28 @@
-addappid(330020)
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-addappid(330021,0,"08ae65e452cbf086b1185be193446421485214e63e705cbc139de687c906dc15")
-setManifestid(330021,"4606985130426915900")
-addappid(330022,0,"d67d4cef4af612190a78a42d3af9df70988474b78c9f1de68d65cf711cc09be4")
-setManifestid(330022,"4076267414139895734")
-addappid(330023,0,"47799065e21c92aab38943ad2b7312f5719f9b3e92bc8029e19216741a650cde")
-setManifestid(330023,"7753140511382431254")
-addappid(330024,0,"57ee867e033c4216af875908ec959e0d91ae5c1ef5493dc3a5a7c06b4a6c7317")
-setManifestid(330024,"7281937126606251578")
-addappid(1317160,0,"9977ce8fa90309f0f0c337720d0ab3ec775f1a6f02d6034e6ad8a18157e514ef")
-setManifestid(1317160,"9017478250715199651")
-addappid(330026,0,"3d470a3c0b7fc703b8e780dc3f087babecd65d940c96bc92ee202f61acea9113")
-setManifestid(330026,"6822150715540450746")
-addappid(330027,0,"7e53bfaacab4472e6918491409733d7490edc7f25e0797f374e498118da1e0c5")
-setManifestid(330027,"2686645784613825353")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 330020.lua
+--Gamename Children of Morta
+addappid(330020) --Mainappid Children of Morta
+addappid(330021, 1, "08ae65e452cbf086b1185be193446421485214e63e705cbc139de687c906dc15") --Main Windows Depot Children of Morta
+setManifestid(330021, "4606985130426915900", 1433705056)
+addappid(330022, 1, "d67d4cef4af612190a78a42d3af9df70988474b78c9f1de68d65cf711cc09be4") --Main Macos Depot Children of Morta
+setManifestid(330022, "4076267414139895734", 1434312384)
+addappid(330023, 1, "47799065e21c92aab38943ad2b7312f5719f9b3e92bc8029e19216741a650cde") --Main Linux Depot Children of Morta
+setManifestid(330023, "7753140511382431254", 1336724272)
+addappid(330024, 1, "57ee867e033c4216af875908ec959e0d91ae5c1ef5493dc3a5a7c06b4a6c7317") --Main Windows Depot Children of Morta
+setManifestid(330024, "7281937126606251578", 4128)
+--Dlcname Children of Morta: Paws and Claws
+addappid(1317160, 1, "9977ce8fa90309f0f0c337720d0ab3ec775f1a6f02d6034e6ad8a18157e514ef") --Dlcname Children of Morta: Paws and Claws
+addappid(330026, 1, "3d470a3c0b7fc703b8e780dc3f087babecd65d940c96bc92ee202f61acea9113") --Dlc Macos Depot Children of Morta: Paws and Claws
+setManifestid(330026, "6822150715540450746", 0)
+addappid(330027, 1, "7e53bfaacab4472e6918491409733d7490edc7f25e0797f374e498118da1e0c5") --Dlc Linux Depot Children of Morta: Paws and Claws
+setManifestid(330027, "2686645784613825353", 0)
+setManifestid(1317160, "9017478250715199651", 0)
+--Dlcname Children of Morta: Family Fireside Fables
+addappid(1556830, 1, "5b1335f05514071257d0d502e96929535068d10c72824ffe3698e01cc05ed218") --Dlcname Children of Morta: Family Fireside Fables
+setManifestid(1556830, "1525705846671816247", 1873673696)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1648880) --Dlcname Children of Morta: Ancient Spirits
