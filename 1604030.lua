656eed158a1e26a8eefc69fc2263cb172cd0bfa9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:25:23 +0800

    add 1604030.lua

--- 1604030.lua
+++ 1604030.lua
@@ -1,6 +1,25 @@
-addappid(1604030)
-setManifestid(228989,"3514306556860204959")
-addappid(1604031,0,"e51d4f407e118a5477cab33cf9cd3340057f5aa111772951f47d76b05b594606")
-setManifestid(1604031,"5333584901924489448")
-addappid(1604032,0,"05dc87d45e23f53bfdc06512480bf3a8d84941a929034772fba892bf252d9587")
-setManifestid(1604032,"1496685958615597028")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1604030.lua
+--Gamename V Rising
+addappid(1604030, 1, "56c40ea98866f38fd5632717e524396c74c9764a7daeaf525d71ba5539e1bd0a") --Mainappid V Rising
+addappid(1604031, 1, "e51d4f407e118a5477cab33cf9cd3340057f5aa111772951f47d76b05b594606") --Main Windows Depot V Rising
+setManifestid(1604031, "2621767958913415677", 5050462560)
+addappid(1604032, 1, "05dc87d45e23f53bfdc06512480bf3a8d84941a929034772fba892bf252d9587") --Main Windows Depot V Rising
+setManifestid(1604032, "8225175356744277948", 4321140624)
+--Dlcname V Rising Soundtrack
+addappid(1999270) --Dlcname V Rising Soundtrack
+addappid(1999271, 1, "018b653b9b20cb999529b75c5893c842325de70b50a9d294e1425f2461089604") --Dlc Windows Depot V Rising Soundtrack
+setManifestid(1999271, "873845939608994832", 282881712)
+addappid(1999272, 1, "bc8a0725a46c16535d2b0ea0ca3671bc100e16c5feb496ba92fa7bbfacc89061") --Dlc Windows Depot V Rising Soundtrack
+setManifestid(1999272, "5505416664520345647", 1986502960)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(1983900) --Dlcname V Rising - Dracula's Relics Pack
+addappid(1983901) --Dlcname V Rising - Founder's Pack: Eldest Bloodline
+addappid(1997210) --Dlcname AppID 1997210
+addtoken(1997210, "15813987473860297923")
+addappid(2169180) --Dlcname V Rising - Haunted Nights Castle Pack
+addappid(2358430) --Dlcname V Rising - Sinister Evolution Pack
+addappid(2833600) --Dlcname V Rising - Legacy of Castlevania Premium Pack
+addappid(3604340) --Dlcname V Rising - Eternal Dominance Pack
