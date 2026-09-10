a1738b6ef8e790cab0bfcea939a0cf539d5f111e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:43:57 +0800

    add 752590.lua

--- 752590.lua
+++ 752590.lua
@@ -1,4 +1,10 @@
-addappid(752590)
-setManifestid(228987,"4302102680580581867")
-addappid(752591,0,"31c0eb1a38b3926e0e589c25300225886ac653a8415d3612864cd26664681460")
-setManifestid(752591,"476939438164423828")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 752590.lua
+--Gamename A Plague Tale: Innocence
+addappid(752590, 1, "6e5aec0c1e73711e58985b074bb63f6fe15e352eba8a039ead765ef9093d8409") --Mainappid A Plague Tale: Innocence
+addappid(752591, 1, "31c0eb1a38b3926e0e589c25300225886ac653a8415d3612864cd26664681460") --Main Windows Depot A Plague Tale: Innocence
+setManifestid(752591, "476939438164423828", 20960952368)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(967380) --Dlcname A Plague Tale: Innocence - Coats of Arms DLC
