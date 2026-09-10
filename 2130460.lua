42eebe2dc3b1f981823669de91e1374d1c4d251a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:09:40 +0800

    add 2130460.lua

--- 2130460.lua
+++ 2130460.lua
@@ -1,13 +1,24 @@
-addappid(2130460)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2130461,0,"ebca96a0f4137d2bb98654148efb453f475b6f091226288bdf31d232a41b7c01")
-setManifestid(2130461,"3008367655729204016")
-addappid(2257211,0,"2b8fa2b81702ac842fbabc262af0a425b1cbd3d4cc2517b33217d654b3a2e19f")
-setManifestid(2257211,"10251861383826610")
-addappid(2257212,0,"6a3c1c642b13e73002c765f0411d7162b6684697950c82f12b0364706ba6deab")
-setManifestid(2257212,"207626751715885023")
-addappid(2257213,0,"08baa8a0787b02119e8c8723cb9696d781d91c742f1f0bb8b425a41567aee835")
-setManifestid(2257213,"421178101224505117")
-addappid(2257214,0,"1cf2f80cdfae6511f1e0b872b9c1b16349c20f4cb9ee6c846a70cefdc520b723")
-setManifestid(2257214,"740608742303706834")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2130460.lua
+--Gamename FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse
+addappid(2130460) --Mainappid FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse
+addappid(2130461, 1, "ebca96a0f4137d2bb98654148efb453f475b6f091226288bdf31d232a41b7c01") --Main Windows Depot FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse
+setManifestid(2130461, "3008367655729204016", 6739322400)
+--Dlcname FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse Digital Artbook
+addappid(2257211, 1, "2b8fa2b81702ac842fbabc262af0a425b1cbd3d4cc2517b33217d654b3a2e19f") --Dlcname FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse Digital Artbook
+setManifestid(2257211, "10251861383826610", 353405440)
+--Dlcname FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse Rogetsu Isle Dinner Party Costume Set
+addappid(2257212, 1, "6a3c1c642b13e73002c765f0411d7162b6684697950c82f12b0364706ba6deab") --Dlcname FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse Rogetsu Isle Dinner Party Costume Set
+setManifestid(2257212, "207626751715885023", 64001104)
+--Dlcname FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse Ruka Exclusive Costume "Marie Rose Outfit"
+addappid(2257213, 1, "08baa8a0787b02119e8c8723cb9696d781d91c742f1f0bb8b425a41567aee835") --Dlcname FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse Ruka Exclusive Costume "Marie Rose Outfit"
+setManifestid(2257213, "421178101224505117", 12013968)
+--Dlcname FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse Ruka/Misaki/Choshiro-Exclusive Accessory (Spirit Stone Flashlight Hat & Fox Mask)
+addappid(2257214, 1, "1cf2f80cdfae6511f1e0b872b9c1b16349c20f4cb9ee6c846a70cefdc520b723") --Dlcname FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse Ruka/Misaki/Choshiro-Exclusive Accessory (Spirit Stone Flashlight Hat & Fox Mask)
+setManifestid(2257214, "740608742303706834", 7062912)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2257210) --Dlcname FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse Digital Deluxe Upgrade Pack
