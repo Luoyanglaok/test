ad5011bebbfcb83ed83c3113c1f33616792bb016
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:43:57 +0800

    add 543870.lua

--- 543870.lua
+++ 543870.lua
@@ -1,3 +1,6 @@
-addappid(543870)
-addappid(543871,0,"3c04bda9a8a328aa4d425eb8c6858a4f7b108a9fa13e390d5e628b949ecb083d")
-setManifestid(543871,"523331981350778192")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 543870.lua
+--Gamename NARUTO SHIPPUDEN: Ultimate Ninja STORM 2
+addappid(543870) --Mainappid NARUTO SHIPPUDEN: Ultimate Ninja STORM 2
+addappid(543871, 1, "3c04bda9a8a328aa4d425eb8c6858a4f7b108a9fa13e390d5e628b949ecb083d") --Main Windows Depot NARUTO SHIPPUDEN: Ultimate Ninja STORM 2
+setManifestid(543871, "523331981350778192", 6290481088)
