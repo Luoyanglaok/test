17334e7ee51847f572e1fba222367dc8fe240236
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:53:13 +0800

    add 9310.lua

--- 9310.lua
+++ 9310.lua
@@ -1,10 +1,21 @@
-addappid(9310)
-setManifestid(4572,"4281044843760781397")
-setManifestid(4571,"8794460804132575282")
-setManifestid(4576,"2914002567962819400")
-setManifestid(4573,"451017054915260593")
-setManifestid(4574,"3096403505290929983")
-setManifestid(4575,"3526581488074967244")
-setManifestid(4577,"496159478834953471")
-addappid(9311,0,"2dbf6bdc31f2049defab4b7da6a8bce08eebd54fa7abf92cae1fc4d7ae17288a")
-setManifestid(9311,"7301405401172982142")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9310.lua
+--Gamename Warhammer 40,000: Dawn of War - Winter Assault
+addappid(9310) --Mainappid Warhammer 40,000: Dawn of War - Winter Assault
+addappid(9311, 1, "2dbf6bdc31f2049defab4b7da6a8bce08eebd54fa7abf92cae1fc4d7ae17288a") --Main Windows Depot Warhammer 40,000: Dawn of War - Winter Assault
+setManifestid(9311, "7301405401172982142", 2464640)
+--Share Depots
+addappid(4571, 1, "cd29e710bc768c891460d4b29858905d8a9c25eb3c11c16912d7296fe9ac2270") --Share Windows Depot Warhammer 40,000: Dawn of War - Anniversary Edition
+setManifestid(4571, "8794460804132575282", 988870144)
+addappid(4572, 1, "7f5ef5d88b2ca50cf1279f3185ac95b1255c09a29b3c2a4af7325deecee926f1") --Share Windows Depot Warhammer 40,000: Dawn of War - Anniversary Edition
+setManifestid(4572, "4281044843760781397", 843151968)
+addappid(4573, 1, "ef7b8a5b2b3732cd1284ef1004272a2c6d1c6ae60160860b09f50004cb43e141") --Share Windows Language Depot Spanish Warhammer 40,000: Dawn of War - Anniversary Edition
+setManifestid(4573, "451017054915260593", 317299424)
+addappid(4574, 1, "b6363d215e674b4b4f1f51023e2aa223deff4471eb4662464c2bc8a74a826f3c") --Share Windows Language Depot French Warhammer 40,000: Dawn of War - Anniversary Edition
+setManifestid(4574, "3096403505290929983", 334611920)
+addappid(4575, 1, "d53eef39d2d9daf87e8c0442388792ccc181d627d73284afcad7b68151c8fd49") --Share Windows Language Depot Italian Warhammer 40,000: Dawn of War - Anniversary Edition
+setManifestid(4575, "3526581488074967244", 317301104)
+addappid(4576, 1, "ef666ae4e383c0f0ca02b56189bab8e6120cd6bd905c0d3fb172a479db11517c") --Share Windows Language Depot German Warhammer 40,000: Dawn of War - Anniversary Edition
+setManifestid(4576, "2914002567962819400", 376967904)
+addappid(4577, 1, "6a170b9c3f47d515d32af19775049e22c4bf0f9e67edd3a8fa0de67162ae7178") --Share Windows Language Depot English Warhammer 40,000: Dawn of War - Anniversary Edition
+setManifestid(4577, "496159478834953471", 317304032)
