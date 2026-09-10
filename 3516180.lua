7cef3dea9c509d6720a89717cb49797dbcf3fbe6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:04:07 +0800

    add 3516180.lua

--- 3516180.lua
+++ 3516180.lua
@@ -1,3 +1,10 @@
-addappid(3516180)
-addappid(3516181,0,"dc747063c492b3c7c598538d95e4a8f61cb724864be1a6eb46e07103bce91942")
-setManifestid(3516181,"4445228082601084974")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3516180.lua
+--Gamename Don't FOOL ME, Beauties!
+addappid(3516180, 1, "b743a79f05dbfa025bae4f987097f7a3ad2c84403cf10b2bb6e5248e5d15d2c3") --Mainappid Don't FOOL ME, Beauties!
+addappid(3516181, 1, "dc747063c492b3c7c598538d95e4a8f61cb724864be1a6eb46e07103bce91942") --Main Windows Depot Don't FOOL ME, Beauties!
+setManifestid(3516181, "4445228082601084974", 34753995776)
+addappid(3516182, 1, "f2af1892671d65f076bd963b5c3b54afb428e277bea567ea50d61f7e59c4b791") --Main Macos Depot Don't FOOL ME, Beauties!
+setManifestid(3516182, "156921443675158534", 34759020320)
+addappid(3516183, 1, "5ca7fc8093bc710beb38d20d7f4041986e01e75cc57b8de92f840c7d4ec83ac5") --Main Linux Depot Don't FOOL ME, Beauties!
+setManifestid(3516183, "3916418263937033063", 16742040016)
