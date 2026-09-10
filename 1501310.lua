2928beb72b81ea11b0ef817d834668fd25c59b2e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:01:10 +0800

    add 1501310.lua

---
+++ 1501310.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1501310.lua
+--Gamename The Ranchers
+addappid(1501310, 1, "8a9a42c3112111804ca31e1bffe9b003bd7324e9aa69c49076b4e6c3f08b9a33") --Mainappid The Ranchers
+addappid(1501311, 1, "088b0440005c5c7d768fd5220477fcb73b0079cb37329030b0ec4f6fad0ecb81") --Main Windows Depot The Ranchers
+setManifestid(1501311, "7922543973007647452", 3882317808)
+addappid(1501312, 1, "e1d68451c228dfd2156bdfc7939f4b93c8f80ab0722f1273a4e59ba753069d62") --Main Macos Depot The Ranchers
+setManifestid(1501312, "8504987099854719168", 3890795616)
+addappid(1501313, 1, "8f6bb1d3a05d63514ad294130250def11eb9ebb5303cf10d822a2c5a5734eb00") --Main Linux Depot The Ranchers
+setManifestid(1501313, "5507188592058770609", 3683825440)
