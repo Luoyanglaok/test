b2797db416d5585c93c28f567383559e87058952
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:55:37 +0800

    add 1465460.lua

--- 1465460.lua
+++ 1465460.lua
@@ -1,3 +1,9 @@
-addappid(1465460)
-addappid(1465461,0,"e2a62ed18d9479e7bca56d425a8c527bacdfc7435666810eafeea55fe3e7746f")
-setManifestid(1465461,"1192505900662512114")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1465460.lua
+--Gamename Infection Free Zone
+addappid(1465460, 1, "86d798c9de12740c68e9f88ad9799466c2fa68547751f1ed7778a246b696742a") --Mainappid Infection Free Zone
+addappid(1465461, 1, "e2a62ed18d9479e7bca56d425a8c527bacdfc7435666810eafeea55fe3e7746f") --Main Windows Depot Infection Free Zone
+setManifestid(1465461, "4761718897411589775", 4020248128)
+--Dlcname Infection Free Zone - Supporter Pack
+addappid(3905440, 1, "e07375c16602232b10c3e0ace0249fd00186f3b2d77ad89f19f45d7a5626453d") --Dlcname Infection Free Zone - Supporter Pack
+setManifestid(3905440, "1617389351499270440", 564185888)
