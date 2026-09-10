d23e112d60f73e46010cd0fd5137a9fb58712b2b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:35:41 +0800

    add 1498120.lua

--- 1498120.lua
+++ 1498120.lua
@@ -1,5 +1,9 @@
-addappid(1498120)
-addappid(1498121,0,"4b1fb40e9b9ab6cb4c500769f4497fee34fa66eb4570a914560228949733e058")
-setManifestid(1498121,"7685898043738231187")
-addappid(2230970,0,"e0a08a843365c0a402a7470d88ad0a4ec886470b3e2641761fdefcb0baf0b672")
-setManifestid(2230970,"338605730905472429")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1498120.lua
+--Gamename Lewd Island
+addappid(1498120, 1, "559bba70a944bafe8ab38577227740acde4ebc55b554b86568a7701d232539c6") --Mainappid Lewd Island
+addappid(1498121, 1, "4b1fb40e9b9ab6cb4c500769f4497fee34fa66eb4570a914560228949733e058") --Main Windows Depot Lewd Island
+setManifestid(1498121, "7685898043738231187", 3404207056)
+--Dlcname Lewd Island - Official Walkthrough
+addappid(2230970, 1, "e0a08a843365c0a402a7470d88ad0a4ec886470b3e2641761fdefcb0baf0b672") --Dlcname Lewd Island - Official Walkthrough
+setManifestid(2230970, "338605730905472429", 698016)
