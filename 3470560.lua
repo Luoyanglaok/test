dadbe3692b63b93e91d9e01aa56259c787d8bf11
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 23:28:40 +0800

    add 3470560.lua

---
+++ 3470560.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3470560.lua
+--Gamename Millennium Dream
+addappid(3470560, 1, "30aff7c58a0ee23cf75a2f81ddb1f73e0fefbb081de9c5da3ee889340ea2d951") --Mainappid Millennium Dream
+addappid(3470561, 1, "c400239376934280aa7af1c97f0cc31bc639a354de507ad0f319a11714a4e23d") --Main Windows Depot Millennium Dream
+setManifestid(3470561, "2421132335801984964", 26223099632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
