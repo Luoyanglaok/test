766387c931d72dc972e19e8852aa5ddbf03f58d8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:05:21 +0800

    add 433340.lua

--- 433340.lua
+++ 433340.lua
@@ -1,11 +1,17 @@
-addappid(433340)
-addappid(433341,0,"02404d4d6e218a526def6e05f322a0510bcc1199addaa52610cc21258bf0a574")
-setManifestid(433341,"6712318380892906767")
-addappid(433342,0,"064d5b0bceb825d1a92db63ac6df94ab7765a45943098ea6af90bd09f5305498")
-setManifestid(433342,"4496584778521264766")
-addappid(433343,0,"5e5f51850ece04988ceff04c7b3bedd6f2e617e1a7e707e9cba06a89f8d3570d")
-setManifestid(433343,"1110279965060068321")
-addappid(433344,0,"3fb5cef87b8f8a4684a476b8d7429b100117e1bd76a4961e168c98d036422e10")
-setManifestid(433344,"7144887116487152453")
-addappid(939480,0,"791322ae7ba59474f3e7faea093783d42597e0dc27c5dd3d1c5325f57cccc6b6")
-setManifestid(939480,"3789258136107597671")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 433340.lua
+--Gamename Slime Rancher
+addappid(433340, 1, "acfe87b27a08f20b14eef4c88b25eb44442f2f6c7a35c3e21cc1ead2464a3509") --Mainappid Slime Rancher
+addappid(433341, 1, "02404d4d6e218a526def6e05f322a0510bcc1199addaa52610cc21258bf0a574") --Main Windows Depot Slime Rancher
+setManifestid(433341, "6712318380892906767", 532302112)
+addappid(433342, 1, "064d5b0bceb825d1a92db63ac6df94ab7765a45943098ea6af90bd09f5305498") --Main Windows Depot Slime Rancher
+setManifestid(433342, "2849989913182793155", 536043456)
+addappid(433343, 1, "5e5f51850ece04988ceff04c7b3bedd6f2e617e1a7e707e9cba06a89f8d3570d") --Main Macos Depot Slime Rancher
+setManifestid(433343, "1110279965060068321", 536827744)
+addappid(433344, 1, "3fb5cef87b8f8a4684a476b8d7429b100117e1bd76a4961e168c98d036422e10") --Main Linux Depot Slime Rancher
+setManifestid(433344, "7144887116487152453", 539577280)
+--Dlcname Slime Rancher: VR Playground
+addappid(939480, 1, "791322ae7ba59474f3e7faea093783d42597e0dc27c5dd3d1c5325f57cccc6b6") --Dlcname Slime Rancher: VR Playground
+setManifestid(939480, "3789258136107597671", 254688352)
+addappid(982310) --Dlcname Slime Rancher: Galactic Bundle
+addappid(1079180) --Dlcname Slime Rancher: Secret Style Pack
