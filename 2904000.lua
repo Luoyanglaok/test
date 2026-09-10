55a83da944cc217a3a6d8beac454fcc77d4a77d2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:42:27 +0800

    add 2904000.lua

--- 2904000.lua
+++ 2904000.lua
@@ -1,5 +1,17 @@
-addappid(2904000)
-addappid(2904001,0,"1cea3f47529b0e53e7add1f7e63dcdf14634adfde24faf952128ad1034ee90e8")
-setManifestid(2904001,"7862405843757934183")
-addappid(2904002,0,"de0044598316af4e08b7d4552491ebb9a7e3ad4e36d1705fc82b0510f1c01acc")
-setManifestid(2904002,"5173833102161842258")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2904000.lua
+--Gamename The Spell Brigade
+addappid(2904000, 1, "53f59ea61a3ed7df3bc6f763f4269312f95253c67f375a1778fdda6e3f35344b") --Mainappid The Spell Brigade
+addappid(2904001, 1, "1cea3f47529b0e53e7add1f7e63dcdf14634adfde24faf952128ad1034ee90e8") --Main Windows Depot The Spell Brigade
+setManifestid(2904001, "8402072233658259859", 993548160)
+--Dlcname The Spell Brigade - Digital Artbook
+addappid(4457550) --Dlcname The Spell Brigade - Digital Artbook
+addappid(4457551, 1, "28b7307eedd29b4f32201952a9ded776d14e53d1a3e88a0db4af253431d2c373") --Dlc Windows Depot The Spell Brigade - Digital Artbook
+setManifestid(4457551, "9056838936422561624", 172615584)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3194200) --Dlcname The Spell Brigade - Supporter Pack
+addappid(4235530) --Dlcname The Spell Brigade - Disciples of Sol'phish Skin Pack
+--Missing Dlcs
+--Dlcname The Spell Brigade - Original Soundtrack
