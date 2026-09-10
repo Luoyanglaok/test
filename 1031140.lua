bdc115596275400504b89ac6688a65156d68fdf6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:49:27 +0800

    add 1031140.lua

--- 1031140.lua
+++ 1031140.lua
@@ -1,5 +1,13 @@
-addappid(1031140)
-addappid(1031141,0,"8d89873705504eddc8005dbbd2be1b80b924c00facc0b22b50d9d6806322e4e1")
-setManifestid(1031141,"733488004208390033")
-addappid(1031142,0,"6ac3ef89142913d63db869672977bdbb9179e227f1562668104fdcbc3a521b43")
-setManifestid(1031142,"2611134843735526656")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1031140.lua
+--Gamename Warhammer The Horus Heresy: Legions
+addappid(1031140) --Mainappid Warhammer The Horus Heresy: Legions
+addappid(1031141, 1, "8d89873705504eddc8005dbbd2be1b80b924c00facc0b22b50d9d6806322e4e1") --Main Windows Depot Warhammer The Horus Heresy: Legions
+setManifestid(1031141, "2568665210696800887", 168392752)
+addappid(1031142, 1, "6ac3ef89142913d63db869672977bdbb9179e227f1562668104fdcbc3a521b43") --Main Macos Depot Warhammer The Horus Heresy: Legions
+setManifestid(1031142, "3137144759437378635", 228252160)
+addappid(1321580) --Dlcname Horus Heresy: Legions - Starter bundle
+addappid(1985190) --Dlcname Horus Heresy: Legions - Expansion bundle
+addappid(2436700) --Dlcname Warhammer Horus Heresy: Legions - World Eaters Skulls bundle
+addappid(2961910) --Dlcname Warhammer Horus Heresy: Legions - Space Wolves bundle
+addappid(3697830) --Dlcname Warhammer Horus Heresy: Legions - Shattered Legions bundle
