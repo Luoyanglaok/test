a021d9082edaebf4ee006e4b766fddaa95b39269
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:32:57 +0800

    add 3994370.lua

---
+++ 3994370.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3994370.lua
+--Gamename Five Hearts Under One Roof season2 - Soundtrack
+addappid(3994370) --Mainappid Five Hearts Under One Roof season2 - Soundtrack
+addappid(3994371, 1, "9af044152f1ae5a909b05c146587e84a822ddfd19acda6264359eee283e0a810") --Main Windows Depot Five Hearts Under One Roof season2 - Soundtrack
+setManifestid(3994371, "7584353851468313637", 1012571088)
