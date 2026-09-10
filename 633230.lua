442e3d693c6f251fe866c70e2c671ebbe1770a75
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:42:58 +0800

    add 633230.lua

--- 633230.lua
+++ 633230.lua
@@ -1,5 +1,110 @@
-addappid(633230)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(633231,0,"94df9bad960ed6e04e89edf50a1b7a953c09cd7be9027ee380e41ba0a22e142b")
-setManifestid(633231,"3531692746006237471")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 633230.lua
+--Gamename NARUTO TO BORUTO: SHINOBI STRIKER
+addappid(633230, 1, "d2f850ad46f2968990ed22f74d0911bbf5e3e46de48b689eeb501a5be487cec5") --Mainappid NARUTO TO BORUTO: SHINOBI STRIKER
+addappid(633231, 1, "94df9bad960ed6e04e89edf50a1b7a953c09cd7be9027ee380e41ba0a22e142b") --Main Windows Depot NARUTO TO BORUTO: SHINOBI STRIKER
+setManifestid(633231, "3317547665511308200", 17731716528)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(888850) --Dlcname NTBSS: Master Character Training Pack - Jiraiya
+addappid(888851) --Dlcname NTBSS: Master Character Training Pack - Hiruzen Sarutobi
+addappid(888852) --Dlcname NTBSS: Master Character Training Pack - Orochimaru
+addappid(888853) --Dlcname NTBSS: Master Character Training Pack - Tobirama Senju
+addappid(888854) --Dlcname NTBSS: Master Character Training Pack - Obito Uchiha
+addappid(888855) --Dlcname NTBSS: Master Character Training Pack - Minato Namikaze
+addappid(888856) --Dlcname NTBSS: Master Character Training Pack - Tsunade
+addappid(888857) --Dlcname NTBSS: Master Character Training Pack - Hashirama Senju
+addappid(888858) --Dlcname NTBSS: Master Character Training Pack - Madara Uchiha
+addappid(888859) --Dlcname NTBSS: Seventh Hokage Costume (Gender-Neutral)
+addappid(888870) --Dlcname NTBSS: Master Character Pain Early Unlock
+addappid(888871) --Dlcname NTBSS: Shinobi Strikers Coat: White (Gender-Neutral)
+addappid(888872) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season Pass
+addappid(915390) --Dlcname NTBSS: Shinobi Strikers Coat: Black (Gender-Neutral)
+addappid(1087420) --Dlcname NTBSS: Master Character Training Pack - Zabuza Momochi
+addappid(1087421) --Dlcname NTBSS: Master Character Training Pack - Haku
+addappid(1087422) --Dlcname NTBSS: Master Character Training Pack - Ohnoki
+addappid(1087423) --Dlcname NTBSS: Master Character Training Pack - Eight Tails Jinchuriki
+addappid(1087424) --Dlcname NTBSS: Master Character Training Pack - Sasuke Uchiha (Boruto)
+addappid(1087425) --Dlcname NTBSS: Master Character Training Pack - Ino Yamanaka
+addappid(1087426) --Dlcname NTBSS: Master Character Training Pack - Might Guy
+addappid(1087427) --Dlcname NTBSS: Master Character Training Pack - Mei Terumi
+addappid(1087428) --Dlcname NTBSS: Master Character Training Pack Naruto Uzumaki (BORUTO)
+addappid(1087429) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season Pass 2
+addappid(1087450) --Dlcname NTBSS: Master Character Training Pack - Kakashi Hatake (Double Sharingan)
+addappid(1087451) --Dlcname NTBSS: Master Character Training Pack - Neji Hyuga
+addappid(1090130) --Dlcname NTBSS: Master Character Training Pack - Shisui Uchiha
+addappid(1090140) --Dlcname NTBSS: Master Character Training Pack - Naruto Uzumaki (Last Battle)
+addappid(1090141) --Dlcname AppID 1090141
+addtoken(1090141, "1060160398196400131")
+addappid(1090160) --Dlcname NTBSS: Lobby Action - Lightning Blade
+addappid(1090161) --Dlcname NTBSS Top Secret Training Set - Base Game Characters
+addappid(1090162) --Dlcname NTBSS Top Secret Training Set - Season Pass 1 Characters
+addappid(1090163) --Dlcname NTBSS Top Secret Training Set - Season Pass 2 Characters
+addappid(1090164) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season Pass 3
+addappid(1090165) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season Pass 4
+addappid(1090166) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Secret Technique: Super Uzumaki Rasengan
+addappid(1090167) --Dlcname NTBSS: Master Character Training Pack - Sakura Haruno (Great Ninja War)
+addappid(1090180) --Dlcname NTBSS: Master Character Training Pack - Nagato (Reanimation)
+addappid(1090181) --Dlcname NTBSS Master Character Training Pack - Itachi Uchiha (Reanimation)
+addappid(1090182) --Dlcname NTBSS Master Character Training Pack - Sasuke Uchiha (Last Battle)
+addappid(1090183) --Dlcname NTBSS Master Character Training Pack - Kawaki
+addappid(1090184) --Dlcname NTBSS Top Secret Training Set - Season Pass 3 Characters
+addappid(1090241) --Dlcname AppID 1090241
+addtoken(1090241, "7250152614800142454")
+addappid(1983240) --Dlcname NTBSS Master Character Training Pack - Madara Uchiha (Six Paths)
+addappid(1983241) --Dlcname NTBSS Master Character Training Pack - Temari
+addappid(1983242) --Dlcname NTBSS Master Character Training Pack - Konohamaru Sarutobi (BORUTO)
+addappid(1983243) --Dlcname NTBSS Master Character Training Pack - Naruto Uzumaki (Baryon Mode)
+addappid(1983244) --Dlcname NTBSS Master Character Training Pack - Isshiki Otsutsuki
+addappid(1983245) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season Pass 5
+addappid(1983246) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Secret Technique Rasengan Unison
+addappid(1983247) --Dlcname NTBSS Top Secret Training Set - Season Pass 4 Characters
+addappid(2185750) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER - Starter Pack
+addappid(2321090) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Secret Technique Kurama Taikyoku Rasengan
+addappid(2321091) --Dlcname NTBSS: Master Character Training Pack - Minato Namikaze (Reanimation)
+addappid(2321092) --Dlcname NTBSS Top Secret Training Set - Season Pass 5 Characters
+addappid(2321093) --Dlcname NTBSS Special Ninjutsu - The Piercing One-Fingered Thrust of Hell
+addappid(2321094) --Dlcname NTBSS: Master Character Training Pack - Obito Uchiha (Ten Tails)
+addappid(2321095) --Dlcname NTBSS: Special Ninjutsu - Flying Raijin Slice
+addappid(2321096) --Dlcname NTBSS: Master Character Training Pack - Kaguya Otsutsuki
+addappid(2321097) --Dlcname NTBSS: Special Ninjutsu - Steam Imp
+addappid(2321098) --Dlcname NTBSS: Special Ninjutsu - Particle Style: Atomic Dismantling Jutsu - Pillar
+addappid(2321099) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Secret Technique Amaterasu: Dual Layer
+addappid(2321100) --Dlcname NTBSS: Master Character Training Pack - Boruto Uzumaki (Karma Progression)
+addappid(2321101) --Dlcname NTBSS Top Secret Training Set - Season Pass 6 Characters
+addappid(2321102) --Dlcname NTBSS: Special Ninjutsu - Magnet Style: Gold Dust Great Burial
+addappid(2321103) --Dlcname NTBSS: Master Character Training Pack - Gaara (Young Ver.)
+addappid(2321104) --Dlcname NTBSS: Special Ninjutsu - Wood Style: Wood Golem Jutsu
+addappid(2321105) --Dlcname NTBSS: Master Character Training Pack - Sasori
+addappid(2321110) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season Pass 6
+addappid(2321111) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season Pass 7
+addappid(2737440) --Dlcname NTBSS: Moonlight Scroll x15 - 15-Million User Celebration
+addappid(2903750) --Dlcname NTBSS: Master Character Training Pack - Kawaki (Karma Progression)
+addappid(2903760) --Dlcname NTBSS Top Secret Training Set - Season Pass 7 Characters
+addappid(2903770) --Dlcname NTBSS: Special Ninjutsu - Host Transformation: Two Tails
+addappid(3020720) --Dlcname NTBSS: Special Ninjutsu - Host Transformation: Six Tails
+addappid(3020730) --Dlcname NTBSS: Special Ninjutsu - Host Transformation: Three Tails
+addappid(3020740) --Dlcname NTBSS: Special Ninjutsu - Host Transformation: Seven Tails
+addappid(3020750) --Dlcname NTBSS: Master Character Training Pack - Shisui Uchiha (Perfect Susano'o)
+addappid(3020760) --Dlcname NTBSS: Master Character Training Pack - Naruto Uzumaki (Young Ver.)
+addappid(3020770) --Dlcname NTBSS: Master Character Training Pack - Sasuke Uchiha (Young Ver. Final Battle)
+addappid(3244580) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER - Special Ninjutsu: An Elder Reborn Kage Pack
+addappid(3244590) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER - Naruto Uzumaki Starter Pack
+addappid(3244600) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER - Sasuke Uchiha Starter Pack
+addappid(3601230) --Dlcname NTBSS: Master Character Training Pack - Hidan
+addappid(3601240) --Dlcname NTBSS: Master Character Training Pack - Kakuzu
+addappid(3601250) --Dlcname NTBSS: Master Character Training Pack - Momoshiki Otsutsuki
+addappid(3601260) --Dlcname NTBSS: Master Character Training Pack - Kimimaro
+addappid(3601280) --Dlcname NTBSS Top Secret Training Set - Season 8 Characters
+addappid(3601290) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season Pass 9
+addappid(3623160) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season 8 Character Pack
+addappid(3888520) --Dlcname NTBSS: Special Ninjutsu: Awakened "Tailed Beast" Complete Pack
+addappid(4467460) --Dlcname NTBSS: Master Character Training Pack - Jiraiya (Sage Mode)
+addappid(4467470) --Dlcname NTBSS: Master Character Training Pack - Naruto Uzumaki (Sage Mode)
+addappid(4467510) --Dlcname NTBSS Top Secret Training Set - Season Pass 9 Characters
+addappid(4467520) --Dlcname NARUTO TO BORUTO: SHINOBI STRIKER Season Pass 10
+addappid(887640) --Dlcname AppID 887640
+addtoken(887640, "271819680309519740")
