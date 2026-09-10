e178944a0a827c2364bb0d0f2b862a1b9434c1cc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:23:18 +0800

    add 2344520.lua

--- 2344520.lua
+++ 2344520.lua
@@ -1,35 +1,63 @@
-addappid(2344520)
-addappid(2344521,0,"bf58e60bf2aca37197c7bc279a6b48b726131fa8b6d07bd12909d8a251252c65")
-setManifestid(2344521,"8317771000308526451")
-addappid(2344522,0,"8b885b87e01a772da363b1cd92f8fb0faac6f727acb321aef2ae9c4ce938a4bf")
-setManifestid(2344522,"6119733914981539496")
-addappid(2344523,0,"95a541fb3a2967fb73875c96ad8f56e3cbb5e3eafb742a4a1671847e722b543a")
-setManifestid(2344523,"7435362687459974044")
-addappid(2344524,0,"5eab48ada2fbd0681f2548f19de28ffb437ab06f912c4adef37b7d4a1d82df40")
-setManifestid(2344524,"1791005504593600266")
-addappid(2344525,0,"222f83dd58e78880080c90ac47ec8b18598682e8d9cf1eb5d9c971838ae9982a")
-setManifestid(2344525,"455952894560026898")
-addappid(2344526,0,"36773de6a67ec1ab03be4f3608e84d6065093969f89f13a78938d568c64dbcdc")
-setManifestid(2344526,"4133517338832106586")
-addappid(2344527,0,"e9ca21ab9bc1f84a13c3ab8c2d16770896c62bded180208739d12966d0a5a74e")
-setManifestid(2344527,"7548478969542582688")
-addappid(2344528,0,"8208caaf454b35c95674eea8f70a58cc4daf109424ce89ff87f15a06e0c57002")
-setManifestid(2344528,"4868334574415422129")
-addappid(2344529,0,"cc7acfe91d9730e4bf1d3d18e3c3c508c138f3308b65e1b5430171c037ad1b72")
-setManifestid(2344529,"8522123038762035142")
-addappid(2452970,0,"02d4526ba2550dee890eb72533f01be38dbfa301a86b4c9ce95bf1a00cb4320f")
-setManifestid(2452970,"613821691196489505")
-addappid(2452971,0,"26b03b20e224538d7f7d02d44f1b6e6a64dc028b2950ae50570a6871607a168d")
-setManifestid(2452971,"4405682680161286413")
-addappid(2452972,0,"d7bf30f74044cedc899e1a38a4ade22b52a682d843b76a530652c0b4a7510a29")
-setManifestid(2452972,"4127206478451286924")
-addappid(2452973,0,"8e3ae2409f103be46cbd55a941b370d6a19438516d0cf68c4fea29a7e2a0c018")
-setManifestid(2452973,"3094510568614274782")
-addappid(2452974,0,"6db8a9ba8df12b66ed9f14a1e18c176f6a44522ab7c4ca3b37e9964ea0a3eb5b")
-setManifestid(2452974,"868214670449482918")
-addappid(2452975,0,"22c898c54175ad295a2c2794c9ea4b44278e36f2a4f65362d48b6cbe284388ea")
-setManifestid(2452975,"1432517896829171846")
-addappid(2452976,0,"88ea785161f2912b8884373d4e8cdb42c8105a3f689cd44c89116d9b38abd476")
-setManifestid(2452976,"6143367495984190722")
-addappid(2453170,0,"962a85322d266e4dc3ca0de3fe2d065dd2c270c3322c73ceb0d742f960ceb77e")
-setManifestid(2453170,"3527468562417127813")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2344520.lua
+--Gamename Diablo® IV
+addappid(2344520, 1, "48dc14a13742dad6c951eebee02c84d74ed52e46d93b4413943d321791a9dcf1") --Mainappid Diablo® IV
+addappid(2344521, 1, "bf58e60bf2aca37197c7bc279a6b48b726131fa8b6d07bd12909d8a251252c65") --Main Windows Depot Diablo® IV
+setManifestid(2344521, "3508466073762777374", 90615963840)
+addappid(2344522, 1, "8b885b87e01a772da363b1cd92f8fb0faac6f727acb321aef2ae9c4ce938a4bf") --Main Windows Depot Diablo® IV
+setManifestid(2344522, "5904049010260482557", 235737040)
+addappid(2344523, 1, "95a541fb3a2967fb73875c96ad8f56e3cbb5e3eafb742a4a1671847e722b543a") --Main Windows Language Depot English Diablo® IV
+setManifestid(2344523, "7050594136262446625", 2152844096)
+addappid(2344524, 1, "5eab48ada2fbd0681f2548f19de28ffb437ab06f912c4adef37b7d4a1d82df40") --Main Windows Language Depot German Diablo® IV
+setManifestid(2344524, "717728225594241867", 2698118016)
+addappid(2344525, 1, "222f83dd58e78880080c90ac47ec8b18598682e8d9cf1eb5d9c971838ae9982a") --Main Windows Language Depot Spanish Diablo® IV
+setManifestid(2344525, "2441314052010983439", 2737339808)
+addappid(2344526, 1, "36773de6a67ec1ab03be4f3608e84d6065093969f89f13a78938d568c64dbcdc") --Main Windows Language Depot Latam Diablo® IV
+setManifestid(2344526, "7907022764160474042", 2463521424)
+addappid(2344527, 1, "e9ca21ab9bc1f84a13c3ab8c2d16770896c62bded180208739d12966d0a5a74e") --Main Windows Language Depot French Diablo® IV
+setManifestid(2344527, "3788100744525370933", 2560185680)
+addappid(2344528, 1, "8208caaf454b35c95674eea8f70a58cc4daf109424ce89ff87f15a06e0c57002") --Main Windows Language Depot Italian Diablo® IV
+setManifestid(2344528, "3136077628413433896", 2527252224)
+addappid(2344529, 1, "cc7acfe91d9730e4bf1d3d18e3c3c508c138f3308b65e1b5430171c037ad1b72") --Main Windows Language Depot Japanese Diablo® IV
+setManifestid(2344529, "3336864774806435829", 2769638512)
+addappid(2452970, 1, "02d4526ba2550dee890eb72533f01be38dbfa301a86b4c9ce95bf1a00cb4320f") --Main Windows Language Depot Koreana Diablo® IV
+setManifestid(2452970, "2248991882092342495", 2629755360)
+addappid(2452971, 1, "26b03b20e224538d7f7d02d44f1b6e6a64dc028b2950ae50570a6871607a168d") --Main Windows Language Depot Polish Diablo® IV
+setManifestid(2452971, "8953756419004183993", 2639440256)
+addappid(2452972, 1, "d7bf30f74044cedc899e1a38a4ade22b52a682d843b76a530652c0b4a7510a29") --Main Windows Language Depot Brazilian Diablo® IV
+setManifestid(2452972, "1775853573834724961", 2469516304)
+addappid(2452973, 1, "8e3ae2409f103be46cbd55a941b370d6a19438516d0cf68c4fea29a7e2a0c018") --Main Windows Language Depot Russian Diablo® IV
+setManifestid(2452973, "1251702690847874529", 2674536592)
+addappid(2452974, 1, "6db8a9ba8df12b66ed9f14a1e18c176f6a44522ab7c4ca3b37e9964ea0a3eb5b") --Main Windows Language Depot Turkish Diablo® IV
+setManifestid(2452974, "5138690085156398967", 2152844096)
+addappid(2452975, 1, "22c898c54175ad295a2c2794c9ea4b44278e36f2a4f65362d48b6cbe284388ea") --Main Windows Language Depot Schinese Diablo® IV
+setManifestid(2452975, "78874252183741509", 2410670368)
+addappid(2452976, 1, "88ea785161f2912b8884373d4e8cdb42c8105a3f689cd44c89116d9b38abd476") --Main Windows Language Depot Tchinese Diablo® IV
+setManifestid(2452976, "150799551373092020", 2430839824)
+--Dlcname Diablo® IV - High-Resolution Assets
+addappid(2453170, 1, "962a85322d266e4dc3ca0de3fe2d065dd2c270c3322c73ceb0d742f960ceb77e") --Dlcname Diablo® IV - High-Resolution Assets
+setManifestid(2453170, "3767424134030614753", 75080189552)
+--Dlcname Diablo® IV: Vessel of Hatred™
+addappid(3043530, 1, "c82a86d812fde68fbdc2c2776a7abb9c72442001ebc4b38d84b413215facd76e") --Dlcname Diablo® IV: Vessel of Hatred™
+--Dlcname Diablo® IV: Lord of Hatred™
+addappid(3958690, 1, "ba77c26633983b980c502912080daf5684cc60e7043e71791f4a5d89339dac2b") --Dlcname Diablo® IV: Lord of Hatred™
+addappid(2453190) --Dlcname Diablo® IV - Crypt Hunter Pack
+addappid(2572010) --Dlcname Diablo® IV - Standard Edition
+addappid(2572020) --Dlcname Diablo® IV - Digital Deluxe Edition
+addappid(2572030) --Dlcname Diablo® IV - Ultimate Edition
+addappid(2575620) --Dlcname AppID 2575620
+addappid(2575630) --Dlcname Diablo® IV - Shards of Vulcan Pack
+addappid(2575640) --Dlcname AppID 2575640
+addappid(2721550) --Dlcname AppID 2721550
+addappid(2721560) --Dlcname AppID 2721560
+addappid(2721570) --Dlcname AppID 2721570
+addappid(2941030) --Dlcname Diablo® IV: Vessel of Hatred™ - Standard Edition
+addappid(2941050) --Dlcname Diablo® IV: Vessel of Hatred™ - Deluxe Edition
+addappid(2941060) --Dlcname Diablo® IV: Vessel of Hatred™ - Ultimate Edition
+addappid(3184520) --Dlcname Diablo® IV - Rebel's Cache Platinum Pack
+addappid(3559880) --Dlcname AppID 3559880
+addappid(3958700) --Dlcname Diablo® IV: Lord of Hatred™ - Standard Edition
+addappid(3958710) --Dlcname Diablo® IV: Lord of Hatred™ - Deluxe Edition
+addappid(3958720) --Dlcname Diablo® IV: Lord of Hatred™ - Ultimate Edition
+--Missing Dlcs
+--Dlcname Diablo® IV - Soundtrack
