600fefb83906fd4b44fbbe52a8161b7a2fa543ac
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:57:08 +0800

    add 1228520.lua

--- 1228520.lua
+++ 1228520.lua
@@ -1,3 +1,6 @@
-addappid(1228520)
-addappid(1228521,0,"ffd0fe65430f74a66f3416ff21bf0da1fb359bff5f5fdf17f617f1badcc532fa")
-setManifestid(1228521,"8687110034829659799")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1228520.lua
+--Gamename [Chilla's Art] The Convenience Store | 夜勤事件
+addappid(1228520) --Mainappid [Chilla's Art] The Convenience Store | 夜勤事件
+addappid(1228521, 1, "ffd0fe65430f74a66f3416ff21bf0da1fb359bff5f5fdf17f617f1badcc532fa") --Main Windows Depot [Chilla's Art] The Convenience Store | 夜勤事件
+setManifestid(1228521, "8687110034829659799", 983371168)
