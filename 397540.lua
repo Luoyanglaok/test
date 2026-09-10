ff30f12f1b990244ae703245bb12826ce2c03248
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:12:58 +0800

    add 397540.lua

--- 397540.lua
+++ 397540.lua
@@ -1,20 +1,54 @@
-addappid(397540)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229033,"2059065101492814639")
-addappid(397541,0,"88f77a801ca63cd0492b040858a57b62b748c774c5a1046d397b58b0c2cf836c")
-setManifestid(397541,"2108173713249823877")
-addappid(397542,0,"64915689531d1c5e242ad493e198ee1a61a5757f84de8ed77e09f08a76c30847")
-setManifestid(397542,"1221292089630287862")
-addappid(1232256,0,"7de46f8b2674a72ce84a910d86a255f02e6225ba00490119d3281e7dc2a4bcfa")
-setManifestid(1232256,"9116600370569954686")
-addappid(1232257,0,"201e40a8cc715029d3b43ea2fc07f83de91ddf73484115ed3225014584d6e6b4")
-setManifestid(1232257,"132348831915272916")
-addappid(1233060,0,"f9f0b5e50d586c47ec01453408ce945c3038a22649a7b87505bf8bc91bc90486")
-setManifestid(1233060,"350750707932598048")
-addappid(1233061,0,"72210c059f9f275f8f1c144209d5aa3a37c079f001ab59cabb1a114e606773cf")
-setManifestid(1233061,"5627198090468988485")
-addappid(1361830,0,"b601b7eb94614a5eb2b6f877b86e12c6fb79f7527ff4b1b7bf59d0a8b5b088aa")
-setManifestid(1361830,"988244885201248028")
-addappid(1361831,0,"4a0968e321be58b271250dc3ab2ada1f3917b543d06f70e18f6da4a425844e5c")
-setManifestid(1361831,"8226097640942912719")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 397540.lua
+--Gamename Borderlands 3
+addappid(397540, 1, "84264bc307b39fdecf116e4a25ff19499eb3c70de7c24d7b0ee03cfd82d740b7") --Mainappid Borderlands 3
+addappid(397541, 1, "88f77a801ca63cd0492b040858a57b62b748c774c5a1046d397b58b0c2cf836c") --Main Windows Depot Borderlands 3
+setManifestid(397541, "2108173713249823877", 102013572800)
+addappid(397542, 1, "64915689531d1c5e242ad493e198ee1a61a5757f84de8ed77e09f08a76c30847") --Main Windows Depot Borderlands 3
+setManifestid(397542, "1221292089630287862", 30083200)
+--Dlcname Borderlands 3: Moxxi's Heist of the Handsome Jackpot
+addappid(1232256, 1, "7de46f8b2674a72ce84a910d86a255f02e6225ba00490119d3281e7dc2a4bcfa") --Dlcname Borderlands 3: Moxxi's Heist of the Handsome Jackpot
+setManifestid(1232256, "9116600370569954686", 8745388672)
+--Dlcname Borderlands 3: Guns, Love, and Tentacles
+addappid(1232257, 1, "201e40a8cc715029d3b43ea2fc07f83de91ddf73484115ed3225014584d6e6b4") --Dlcname Borderlands 3: Guns, Love, and Tentacles
+setManifestid(1232257, "132348831915272916", 5579288960)
+--Dlcname Borderlands 3: Bounty of Blood
+addappid(1233060, 1, "f9f0b5e50d586c47ec01453408ce945c3038a22649a7b87505bf8bc91bc90486") --Dlcname Borderlands 3: Bounty of Blood
+setManifestid(1233060, "350750707932598048", 11888558944)
+--Dlcname Borderlands 3: Psycho Krieg and the Fantastic Fustercluck
+addappid(1233061, 1, "72210c059f9f275f8f1c144209d5aa3a37c079f001ab59cabb1a114e606773cf") --Dlcname Borderlands 3: Psycho Krieg and the Fantastic Fustercluck
+setManifestid(1233061, "5627198090468988485", 7073511984)
+--Dlcname Borderlands 3: Designer's Cut
+addappid(1361830, 1, "b601b7eb94614a5eb2b6f877b86e12c6fb79f7527ff4b1b7bf59d0a8b5b088aa") --Dlcname Borderlands 3: Designer's Cut
+setManifestid(1361830, "988244885201248028", 2133311328)
+--Dlcname Borderlands 3: Director's Cut
+addappid(1361831, 1, "4a0968e321be58b271250dc3ab2ada1f3917b543d06f70e18f6da4a425844e5c") --Dlcname Borderlands 3: Director's Cut
+setManifestid(1361831, "8226097640942912719", 11616681968)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
+addappid(1232240) --Dlcname Borderlands 3 Boost Mods Pack
+addtoken(1232240, "7496093302550835937")
+addappid(1232250) --Dlcname Borderlands 3: Butt Stallion Pack
+addtoken(1232250, "5063846210627113052")
+addappid(1232251) --Dlcname Borderlands 3: Gearbox Cosmetic Pack
+addappid(1232252) --Dlcname Borderlands 3: Gold Weapons Skins Pack
+addappid(1232253) --Dlcname Borderlands 3: Neon Cosmetic Pack
+addappid(1232254) --Dlcname Borderlands 3: Retro Cosmetic Pack
+addappid(1232255) --Dlcname Borderlands 3: Toy Box Weapons Pack
+addappid(1233062) --Dlcname Borderlands 3: Season Pass
+addappid(1256890) --Dlcname Borderlands 3: Digital Deluxe Extras
+addtoken(1256890, "11568047079294441594")
+addappid(1396470) --Dlcname Borderlands 3: Amara Final Form Pack
+addappid(1396471) --Dlcname Borderlands 3: Zane Final Form Pack
+addappid(1396472) --Dlcname Borderlands 3: Moze Final Form Pack
+addappid(1396473) --Dlcname Borderlands 3: FL4K Final Form Pack
+addappid(1396474) --Dlcname Borderlands 3: Season Pass 2
+addappid(1537990) --Dlcname Disciples of the Vault Amara Cosmetic Pack
+addappid(1537991) --Dlcname Disciples of the Vault FL4K Cosmetic Pack
+addappid(1537992) --Dlcname Disciples of the Vault Moze Cosmetic Pack
+addappid(1537993) --Dlcname Disciples of the Vault Zane Cosmetic Pack
