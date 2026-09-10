6479738c4ce14dad709a817d30f2696a068ec360
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:09:28 +0800

    add 3407070.lua

--- 3407070.lua
+++ 3407070.lua
@@ -1,3 +1,6 @@
-addappid(3407070)
-addappid(3407071,0,"624ef68205bc8a50f5b7008d4478abb7a56723ece293adc96f4760e6a59915ce")
-setManifestid(3407071,"5160349369714215363")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3407070.lua
+--Gamename Sex With Friends
+addappid(3407070) --Mainappid Sex With Friends
+addappid(3407071, 1, "624ef68205bc8a50f5b7008d4478abb7a56723ece293adc96f4760e6a59915ce") --Main Windows Depot Sex With Friends
+setManifestid(3407071, "4617763615190712398", 487265280)
