b8b866057e6c54b56f3816bfb16e221037238701
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:38:25 +0800

    add 2144740.lua

--- 2144740.lua
+++ 2144740.lua
@@ -1,7 +1,24 @@
-addappid(2144740)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-addappid(2144741,0,"d8c29346c26cded430d2454682d4c10c4b3ae1605e872fd1a8a829ecb79e1de4")
-setManifestid(2144741,"5236436374264945035")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2144740.lua
+--Gamename Ghostrunner 2
+addappid(2144740) --Mainappid Ghostrunner 2
+addappid(2144741, 1, "d8c29346c26cded430d2454682d4c10c4b3ae1605e872fd1a8a829ecb79e1de4") --Main Windows Depot Ghostrunner 2
+setManifestid(2144741, "5236436374264945035", 29046790896)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(2573290) --Dlcname Ghostrunner 2 - Pre Order Content
+addappid(2573300) --Dlcname Ghostrunner 2 - Deluxe Content
+addappid(2573310) --Dlcname Ghostrunner 2 - Brutal Content
+addappid(2574920) --Dlcname Ghostrunner 2 - Season Pass
+addappid(2691130) --Dlcname Ghostrunner 2 - Ice Pack
+addappid(2769320) --Dlcname Ghostrunner 2 - Dragon Pack
+addappid(2988270) --Dlcname Ghostrunner 2 - Heat Pack
+addappid(3092910) --Dlcname Ghostrunner 2 - Endless Moto Mode
+addappid(3232490) --Dlcname Ghostrunner 2 - Anniversary Pack
