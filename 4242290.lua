79d26a855f123940129554db7ff662dae2b4f701
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:04:31 +0800

    add 4242290.lua

---
+++ 4242290.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4242290.lua
+--Gamename SPITE: Taking the Boss's Wife, Sister and Mother
+addappid(4242290, 1, "c08a0ec4a1c1e7ef348fbf139936a09bae91f8062f132765446b70b2244f2ab4") --Mainappid SPITE: Taking the Boss's Wife, Sister and Mother
+addappid(4242291, 1, "193f712b8c3e06b1d21b9073523678087406578590945666d73079a628e08f6c") --Main Windows Depot SPITE: Taking the Boss's Wife, Sister and Mother
+setManifestid(4242291, "4510034553234773417", 1237885312)
