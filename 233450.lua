5903cc9c1fa079aad07d5090aff9886fd60baca9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:05:48 +0800

    add 233450.lua

--- 233450.lua
+++ 233450.lua
@@ -1,13 +1,34 @@
-addappid(233450)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(233451,0,"27eb5fe0ce5772f90c109ac60918afc58a5870bbd1cdaef4b6c3919b0b63ef83")
-setManifestid(233451,"214199605922079846")
-addappid(233452,0,"0313b1a3064addbc9f94a29d64e52a2db1f9da529598fe6402af74482b81876d")
-setManifestid(233452,"8796105605411077743")
-addappid(233453,0,"ba72480b04c6b8e3a27a7dab0ad8aae496aa36f4420a20e9b8aa8cb6c38b8b30")
-setManifestid(233453,"6703655944775788476")
-addappid(234432,0,"79c2572c2aea51e5da076c4656f66287663658804187cd5d78dba3a7eb801814")
-setManifestid(234432,"548000939191343126")
-addappid(234433,0,"60f2761aca08c5325e4b42c01f8b4196a3046e895062c3e60c7f916ea9541acb")
-setManifestid(234433,"836138921998834207")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 233450.lua
+--Gamename Prison Architect
+addappid(233450, 1, "809221fc5d7dd4c438a0cf50d5d5c10e6cd8bf86d9dd14d3e3f24e3a47f68dd9") --Mainappid Prison Architect
+addappid(233451, 1, "27eb5fe0ce5772f90c109ac60918afc58a5870bbd1cdaef4b6c3919b0b63ef83") --Main Windows Depot Prison Architect
+setManifestid(233451, "214199605922079846", 518561136)
+addappid(233452, 1, "0313b1a3064addbc9f94a29d64e52a2db1f9da529598fe6402af74482b81876d") --Main Macos Depot Prison Architect
+setManifestid(233452, "8796105605411077743", 530924368)
+addappid(233453, 1, "ba72480b04c6b8e3a27a7dab0ad8aae496aa36f4420a20e9b8aa8cb6c38b8b30") --Main Linux Depot Prison Architect
+setManifestid(233453, "6703655944775788476", 524977728)
+--Dlcname Prison Architect Art Book
+addappid(234432, 1, "79c2572c2aea51e5da076c4656f66287663658804187cd5d78dba3a7eb801814") --Dlcname Prison Architect Art Book
+setManifestid(234432, "548000939191343126", 34019392)
+--Dlcname Prison Architect Soundtrack
+addappid(234433, 1, "60f2761aca08c5325e4b42c01f8b4196a3046e895062c3e60c7f916ea9541acb") --Dlcname Prison Architect Soundtrack
+setManifestid(234433, "836138921998834207", 639328800)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(234430) --Dlcname Prison Architect Standard Edition Key
+addappid(234431) --Dlcname Prison Architect Name in Game Edition Key
+addappid(1143650) --Dlcname Prison Architect - Psych Ward: Warden's Edition
+addappid(1195240) --Dlcname Prison Architect - Aficionado
+addappid(1244780) --Dlcname Prison Architect - Cleared for Transfer
+addappid(1244781) --Dlcname Prison Architect - Island Bound
+addappid(1493470) --Dlcname Prison Architect - Going Green
+addappid(1601350) --Dlcname Prison Architect - Second Chances
+addappid(1760030) --Dlcname Prison Architect - Perfect Storm
+addtoken(1760030, "787658480463838767")
+addappid(1948120) --Dlcname Prison Architect - Gangs
+addappid(2085570) --Dlcname Prison Architect - Free For Life
+addappid(2154010) --Dlcname Prison Architect - Undead
+addappid(2184800) --Dlcname Prison Architect - Future Tech Pack
+addappid(2237250) --Dlcname Prison Architect - Jungle Pack
