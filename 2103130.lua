c0a03246e0d10749b2a121965854422b298ecb55
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:01:30 +0800

    add 2103130.lua

--- 2103130.lua
+++ 2103130.lua
@@ -1,7 +1,10 @@
-addappid(2103130)
-addappid(2103131,0,"dc5fff8db171d37fd68a34ac05d9d32f8f64ebe21a86fb659f3e09ccb09bd2d0")
-setManifestid(2103131,"8187683453723081834")
-addappid(2103132,0,"3745abbe0bcf561e1e90782a74196a11b4c2280fe3b5b8441ca3b14efcf0fedd")
-setManifestid(2103132,"4625666622977554017")
-addappid(2103133,0,"0c3fd9dbe825f5a3e35f2c0db940708e6daa569d1896609efa11617518a13f87")
-setManifestid(2103133,"2776895266109649659")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2103130.lua
+--Gamename Matchmaking Inc.
+addappid(2103130, 1, "a30e123cb7ce725e1c8c07605e2ec0713d3311daeb986a5168f7fee587aacd12") --Mainappid Matchmaking Inc.
+addappid(2103131, 1, "dc5fff8db171d37fd68a34ac05d9d32f8f64ebe21a86fb659f3e09ccb09bd2d0") --Main Windows Depot Matchmaking Inc.
+setManifestid(2103131, "3998646564959998082", 2332452384)
+addappid(2103132, 1, "3745abbe0bcf561e1e90782a74196a11b4c2280fe3b5b8441ca3b14efcf0fedd") --Main Macos Depot Matchmaking Inc.
+setManifestid(2103132, "6360042479473369855", 2339681152)
+addappid(2103133, 1, "0c3fd9dbe825f5a3e35f2c0db940708e6daa569d1896609efa11617518a13f87") --Main Linux Depot Matchmaking Inc.
+setManifestid(2103133, "8170919075696542380", 2331175600)
