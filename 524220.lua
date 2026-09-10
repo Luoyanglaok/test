2319a4f2f28b69b4e4459385a12883478d7b4e33
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 13:40:45 +0800

    add 524220.lua

--- 524220.lua
+++ 524220.lua
@@ -1,20 +1,62 @@
-addappid(524220)
-setManifestid(228990,"1829726630299308803")
-addappid(524221,0,"710eaf838fca522739fd6f0741fc539dbfb3e69df0bca4f0daed6d9789cac571")
-setManifestid(524221,"4633942970953135000")
-addappid(580600,0,"67169947379fca33e6a1fd52fd5d4c6f45234151a71bfd9f5e6cda8bdcfab349")
-setManifestid(580600,"6352626735786606959")
-addappid(580610,0,"b6542835e2fd44948c08a553cab57020dce3620f6677ff161f908160be1de0dd")
-setManifestid(580610,"70539499618618565")
-addappid(583990,0,"cfe4dbbe0d5622ff101a177b2bd7e089e1fb1afd241cda936d0fc3ac7d4010d1")
-setManifestid(583990,"7857600567806192582")
-addappid(583991,0,"6d4ffe017d1c3b9a876f16684204feb1f303a3b6f8327a8cd5246c74799587f0")
-setManifestid(583991,"7822405453448242645")
-addappid(583992,0,"8f903ba92f0e6eb40ae27b57eb1a1ba3672599687013ec0a1685bffb8cf78a93")
-setManifestid(583992,"7955785643780797608")
-addappid(583993,0,"8feaf96bd01fb95f5a288544933692972ad398cf76355a6589a6c85475f7cc95")
-setManifestid(583993,"3440919711813183915")
-addappid(583994,0,"8b754108a01dcdb855841b04bdac5882b22d07844e6d364adbe5efd46d8ecdaa")
-setManifestid(583994,"6837545457254028528")
-addappid(1017430,0,"3f639279ce3e6f19d6e4093f0af4032334e31a0e1203288a5da6419f82d04720")
-setManifestid(1017430,"1820157406263693639")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 524220.lua
+--Gamename NieR:Automata™
+addappid(524220, 1, "3f4a2b25dffae830b22167c9543e21d9afeee5c0c0c4a5e03dee5864582838ab") --Mainappid NieR:Automata™
+addappid(524221, 1, "710eaf838fca522739fd6f0741fc539dbfb3e69df0bca4f0daed6d9789cac571") --Main Windows Depot NieR:Automata™
+setManifestid(524221, "4633942970953135000", 25297983696)
+--Dlcname NieR:Automata™ - 3C3C1D119440927
+addappid(580600, 1, "67169947379fca33e6a1fd52fd5d4c6f45234151a71bfd9f5e6cda8bdcfab349") --Dlcname NieR:Automata™ - 3C3C1D119440927
+setManifestid(580600, "6352626735786606959", 2788684640)
+--Dlcname NieR:Automata™ - Grimoire Weiss Pod
+addappid(580610, 1, "b6542835e2fd44948c08a553cab57020dce3620f6677ff161f908160be1de0dd") --Dlcname NieR:Automata™ - Grimoire Weiss Pod
+setManifestid(580610, "70539499618618565", 940272)
+--Dlcname NieR:Automata™ - Machine Mask Accessory
+addappid(583990, 1, "cfe4dbbe0d5622ff101a177b2bd7e089e1fb1afd241cda936d0fc3ac7d4010d1") --Dlcname NieR:Automata™ - Machine Mask Accessory
+setManifestid(583990, "7857600567806192582", 296128)
+--Dlcname NieR:Automata™ - Valve Character Accessory
+addappid(583991, 1, "6d4ffe017d1c3b9a876f16684204feb1f303a3b6f8327a8cd5246c74799587f0") --Dlcname NieR:Automata™ - Valve Character Accessory
+setManifestid(583991, "7822405453448242645", 144160)
+--Dlcname NieR:Automata™ - Cardboard Pod Skin
+addappid(583992, 1, "8f903ba92f0e6eb40ae27b57eb1a1ba3672599687013ec0a1685bffb8cf78a93") --Dlcname NieR:Automata™ - Cardboard Pod Skin
+setManifestid(583992, "7955785643780797608", 1343920)
+--Dlcname NieR:Automata™ - Retro Red Pod Skin
+addappid(583993, 1, "8feaf96bd01fb95f5a288544933692972ad398cf76355a6589a6c85475f7cc95") --Dlcname NieR:Automata™ - Retro Red Pod Skin
+setManifestid(583993, "3440919711813183915", 1024384)
+--Dlcname NieR:Automata™ - Retro Grey Pod Skin
+addappid(583994, 1, "8b754108a01dcdb855841b04bdac5882b22d07844e6d364adbe5efd46d8ecdaa") --Dlcname NieR:Automata™ - Retro Grey Pod Skin
+setManifestid(583994, "6837545457254028528", 1021600)
+--Dlcname AppID 1017430
+addtoken(1017430, "1808551213462367460")
+addappid(1017430, 1, "3f639279ce3e6f19d6e4093f0af4032334e31a0e1203288a5da6419f82d04720") --Dlcname AppID 1017430
+setManifestid(1017430, "1820157406263693639", 16207504)
+--Dlcname NieR:Automata Original Soundtrack
+addappid(3514130) --Dlcname NieR:Automata Original Soundtrack
+addappid(3514131, 1, "a7c343b68119cbafd502126c39bcac4c3e44bc3deaddfb0a050815a575c9fa6d") --Dlc Windows Depot NieR:Automata Original Soundtrack
+setManifestid(3514131, "3619695901002137478", 469542480)
+--Dlcname NieR Orchestral Arrangement Album - Addendum
+addappid(3514210) --Dlcname NieR Orchestral Arrangement Album - Addendum
+addappid(3514211, 1, "383835575355f7d435bd6e96dbc7bfa4ddaec0f6da0b22baa5a0290b9071b64c") --Dlc Windows Depot NieR Orchestral Arrangement Album - Addendum
+setManifestid(3514211, "6587449000233746566", 112824560)
+--Dlcname NieR:Automata Arranged & Unreleased Tracks
+addappid(3514270) --Dlcname NieR:Automata Arranged & Unreleased Tracks
+addappid(3514271, 1, "74a6d5af8f7912a270a42e76331e9ced787f32089f9b469f70500766cbb81752") --Dlc Windows Depot NieR:Automata Arranged & Unreleased Tracks
+setManifestid(3514271, "8247361657920876053", 228639392)
+--Dlcname NieR:Automata Orchestral Arrangement Album
+addappid(3514280) --Dlcname NieR:Automata Orchestral Arrangement Album
+addappid(3514281, 1, "215d3d1c1591b55f764135c014c87f3b5ec406be3a6f385c659f780901b56c57") --Dlc Windows Depot NieR:Automata Orchestral Arrangement Album
+setManifestid(3514281, "3239115518060299221", 108626704)
+--Dlcname Piano Collections NieR:Automata
+addappid(3514300) --Dlcname Piano Collections NieR:Automata
+addappid(3514301, 1, "5049979f26229e43132a32293ce6d091ad4111640f3da4c6229d0a9c1b439134") --Dlc Windows Depot Piano Collections NieR:Automata
+setManifestid(3514301, "5727053876295354879", 130772048)
+--Dlcname NieR:Piano Journeys
+addappid(3518710) --Dlcname NieR:Piano Journeys
+addappid(3518711, 1, "9afad0e47752127621377caa2ccd43789006934beff0513df4decc4bd5933867") --Dlc Windows Depot NieR:Piano Journeys
+setManifestid(3518711, "3821622139177109291", 108830528)
+--Dlcname NieR:Orchestra Concert re:12024 [ the end of data ] Music CD
+addappid(4267420) --Dlcname NieR:Orchestra Concert re:12024 [ the end of data ] Music CD
+addappid(4267421, 1, "c4aca6e568c2e3f5ae4a164745fc044e0f8d31336388bff841447dfed9d45285") --Dlc Windows Depot NieR:Orchestra Concert re:12024 [ the end of data ] Music CD
+setManifestid(4267421, "2998556727204715887", 226636752)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
