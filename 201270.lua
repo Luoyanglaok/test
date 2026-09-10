d8239bccd7728ce0c0254549943b8dc5f2ddc65f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:59:43 +0800

    add 201270.lua

---
+++ 201270.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 201270.lua
+addappid(201270, 0, "e9360c35467cb671426e983bab7c8ffbc1ad40c7a5873817c613fbbc3557c86f")
+addappid(34342) --Dlcname Total War: SHOGUN 2 - Sengoku Jidai Unit Pack DLC
+addappid(34343) --Dlcname Total War: SHOGUN 2 - Rise of the Samurai Campaign DLC
+addappid(34345) --Dlcname Total War: SHOGUN 2 - Hattori Clan Pack DLC
+addappid(34348) --Dlcname Total War: SHOGUN 2 - Ikko Ikki Clan Pack DLC
+addappid(201272) --Dlcname Total War: SHOGUN 2 - Blood Pack DLC
+addappid(201277) --Dlcname Total War: SHOGUN 2 - Dragon War Battle Pack
+addappid(201279) --Dlcname Total War: SHOGUN 2 - Saints and Heroes DLC
+addappid(223180) --Dlcname Total War: SHOGUN 2 - Otomo Clan Pack
