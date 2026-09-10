6c9fba4d15366d5f867b49811f0f21b10ac617c1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:53:36 +0800

    add 2380410.lua

--- 2380410.lua
+++ 2380410.lua
@@ -1,3 +1,6 @@
-addappid(2380410)
-addappid(2380411,0,"3ede6f8229cd02c5a5aec8fda7e6d6d290335c4beaea23aca867ed1af40c2246")
-setManifestid(2380411,"4862676469043397786")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2380410.lua
+--Gamename SEX Tentacles [18+]
+addappid(2380410) --Mainappid SEX Tentacles [18+]
+addappid(2380411, 1, "3ede6f8229cd02c5a5aec8fda7e6d6d290335c4beaea23aca867ed1af40c2246") --Main Windows Depot SEX Tentacles [18+]
+setManifestid(2380411, "4862676469043397786", 839131968)
