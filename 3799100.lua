818d73f2e0295dbed1ae2e7cd787bfd66f446387
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:43:57 +0800

    add 3799100.lua

---
+++ 3799100.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3799100.lua
+--Gamename SIDE EFFECTS
+addappid(3799100) --Mainappid SIDE EFFECTS
+addappid(3799101, 1, "cb02a7ff0425aed0f5a9ea9376e2c2f0fb2a59cc14d14b7f23792e63fc6d0aa7") --Main Windows Depot SIDE EFFECTS
+setManifestid(3799101, "5112222304742848737", 845060592)
