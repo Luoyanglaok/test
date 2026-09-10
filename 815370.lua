180c3921fb21dd5de93adb78043d2a0a6f7083ce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:06:56 +0800

    add 815370.lua

--- 815370.lua
+++ 815370.lua
@@ -1,5 +1,11 @@
-addappid(815370)
-addappid(2564710)
-setManifestid(815371,"3938349524363268753")
-setManifestid(815372,"1441393091310007518")
-setManifestid(2990430,"1413785688298351171")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 815370.lua
+--Gamename Green Hell
+addappid(815370, 1, "c7fad83c696c4585abbd6d334a696814b452895badf6f5b087373f96be68dfe6") --Mainappid Green Hell
+addappid(815371, 1, "2d729703a7a551530f0fadf02b8c1da3cb944c4a898295bef1c19722f9f9f3cd") --Main Windows Depot Green Hell
+setManifestid(815371, "3938349524363268753", 360848)
+addappid(815372, 1, "03a5e9ecc1760fada61452368cefd3bb2234f78fb25bb7dc481ba9660d781dc7") --Main Windows Depot Green Hell
+setManifestid(815372, "1441393091310007518", 5867971680)
+--Dlcname Green Hell - Anniversary Edition Pack
+addappid(2990430, 1, "cc1e64d590434473930b6edd90d09027aa939a0d8b1fcc7d3ccd30f86ea21e1f") --Dlcname Green Hell - Anniversary Edition Pack
+setManifestid(2990430, "1413785688298351171", 1944543040)
