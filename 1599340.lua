f321e03d1a6a5f8e9b7f330cfcedd67289a79f46
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:24:48 +0800

    add 1599340.lua

--- 1599340.lua
+++ 1599340.lua
@@ -1,6 +1,40 @@
-addappid(1599340)
-setManifestid(228988,"6645201662696499616")
-addappid(1599341,0,"a92b3dd6eedf5645374d5361557211ba7628c6f23185ea14b47bbaa46722b993")
-setManifestid(1599341,"5046038054045525426")
-addappid(1877840,0,"c912c072640806601f0d8959272feb7dd6a7eb97f5bea6afcce84c6b16a3c6a6")
-setManifestid(1877840,"6005745777581938788")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1599340.lua
+--Gamename Lost Ark
+addappid(1599340, 1, "2af38adfd29205eae5133e82c6c0cc59360febc513e5029194aa25d024650b74") --Mainappid Lost Ark
+addappid(1599341, 1, "a92b3dd6eedf5645374d5361557211ba7628c6f23185ea14b47bbaa46722b993") --Main Windows Depot Lost Ark
+setManifestid(1599341, "4291071413787952759", 128768565120)
+--Dlcname Lost Ark - Korean VO Pack
+addappid(1877840, 1, "c912c072640806601f0d8959272feb7dd6a7eb97f5bea6afcce84c6b16a3c6a6") --Dlcname Lost Ark - Korean VO Pack
+setManifestid(1877840, "6553265410110055050", 0)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(1656220) --Dlcname Lost Ark Platinum Founder's Pack
+addappid(1656221) --Dlcname Lost Ark Gold Founder's Pack
+addappid(1656222) --Dlcname Lost Ark Silver Founder's Pack
+addappid(1697950) --Dlcname Lost Ark Bronze Founder's Pack
+addappid(1869700) --Dlcname Lost Ark Apprentice Starter Pack
+addappid(1869701) --Dlcname Lost Ark Explorer Starter Pack
+addappid(1869702) --Dlcname Lost Ark Vanquisher Starter Pack
+addappid(2283590) --Dlcname Lost Ark Mokoko Starter Pack
+addappid(2283591) --Dlcname Lost Ark Shandi Starter Pack
+addappid(2283592) --Dlcname Lost Ark Thirain Starter Pack
+addappid(2471310) --Dlcname AppID 2471310
+addappid(2471311) --Dlcname AppID 2471311
+addappid(2471312) --Dlcname AppID 2471312
+addappid(2612160) --Dlcname Lost Ark: Essential Starter Pack
+addappid(2612170) --Dlcname Lost Ark: Premium Starter Pack
+addappid(2612180) --Dlcname Lost Ark: Ultimate Starter Pack
+addappid(2638590) --Dlcname AppID 2638590
+addappid(2968750) --Dlcname AppID 2968750
+addappid(3166380) --Dlcname Lost Ark: Crystalline Aura Plus Subscription
+addappid(3200700) --Dlcname Lost Ark T4 Founder’s Pack: Platinum
+addappid(3200710) --Dlcname Lost Ark T4 Founder's Pack: Gold
+addappid(3200720) --Dlcname Lost Ark T4 Founder's Pack: Silver
+addappid(3200730) --Dlcname Lost Ark T4 Founder's Pack: Bronze
+addappid(3346540) --Dlcname Lost Ark: Royal Crystal Pack - Silver
+addappid(3346550) --Dlcname Lost Ark: Royal Crystal Pack - Oreha
+addappid(3346560) --Dlcname Lost Ark: Royal Crystal Pack - Leapstone
+addappid(3346570) --Dlcname Lost Ark: Royal Crystal Pack - Shard
+addappid(3735360) --Dlcname Lost Ark: Abyssal Support Pack
