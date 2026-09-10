68cb7f45c6106804979a330db56a1ad6821664ce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:57:25 +0800

    add 2967800.lua

--- 2967800.lua
+++ 2967800.lua
@@ -1,5 +1,19 @@
-addappid(2967800)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2967801,0,"b7a46e12d41bc63d19fa4e59b973a2ead9e218c69c1e40a79033112a34d899a2")
-setManifestid(2967801,"969030386747480281")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2967800.lua
+--Gamename Sex on the beach
+addappid(2967800) --Mainappid Sex on the beach
+addappid(2967801, 1, "b7a46e12d41bc63d19fa4e59b973a2ead9e218c69c1e40a79033112a34d899a2") --Main Windows Depot Sex on the beach
+setManifestid(2967801, "7987460592984170234", 24451169920)
+--Dlcname Sex on the beach Soundtrack
+addappid(3854070) --Dlcname Sex on the beach Soundtrack
+addappid(3854071, 1, "22CD4760E579235D8C12BE58ECA6F7666A3A384421025F860A0606ED56393186") --Dlc Windows Depot Sex on the beach Soundtrack
+setManifestid(3854071, "4017634049707749828", 7815024)
+--Dlcname Sex on the beach (Latin Pop, Rock, Tropical, Soft Adult, Contemporary)
+addappid(3877640) --Dlcname Sex on the beach (Latin Pop, Rock, Tropical, Soft Adult, Contemporary)
+addappid(3877641, 1, "1B02F521B53E9EF9F149B568D953F97FCD141C5C320F1A4E2DCC3D8271E49A83") --Dlc Windows Depot Sex on the beach (Latin Pop, Rock, Tropical, Soft Adult, Contemporary)
+setManifestid(3877641, "108502950761880042", 17278768)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
