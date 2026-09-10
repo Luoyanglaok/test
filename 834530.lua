97c80d1da2fe558cc9d6f0a3bd180f9b63d33621
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:20:07 +0800

    add 834530.lua

--- 834530.lua
+++ 834530.lua
@@ -1,13 +1,23 @@
-addappid(834530)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-addappid(834531,0,"64ebec3e0178f27e5d27d7cc4c91382ce5653dca2966989a8ee26574a83383d5")
-setManifestid(834531,"6343115858572417905")
-addappid(834532,0,"00ac0f337bbd03f1d6b4e25cbe12b68f9567b85b234bd69822a7cb22cfecb100")
-setManifestid(834532,"8056939067145069014")
-addappid(834533,0,"84c25831d0a0900169b6bd7540ec8ecb569e08ec283a37391d404c60a798d0c9")
-setManifestid(834533,"5566686960884841902")
-addappid(834535,0,"f9e85ff7b9d2d862bc853c14bc4e7e6773efdcdfd9028d7a196296a4926a63e0")
-setManifestid(834535,"8036566944547967721")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 834530.lua
+--Gamename Yakuza Kiwami (Legacy)
+addappid(834530, 1, "1c6b37c633ad603055aa67efc1924ca9a34753b5d32af692d9a595e84f2951b1") --Mainappid Yakuza Kiwami (Legacy)
+addappid(834531, 1, "64ebec3e0178f27e5d27d7cc4c91382ce5653dca2966989a8ee26574a83383d5") --Main Windows Depot Yakuza Kiwami (Legacy)
+setManifestid(834531, "6343115858572417905", 8747102768)
+addappid(834532, 1, "00ac0f337bbd03f1d6b4e25cbe12b68f9567b85b234bd69822a7cb22cfecb100") --Main Windows Language Depot English Yakuza Kiwami (Legacy)
+setManifestid(834532, "8056939067145069014", 8083669008)
+addappid(834533, 1, "84c25831d0a0900169b6bd7540ec8ecb569e08ec283a37391d404c60a798d0c9") --Main Windows Language Depot Japanese Yakuza Kiwami (Legacy)
+setManifestid(834533, "5566686960884841902", 8455882208)
+addappid(834534, 1, "39d69ecbb762ccd9af1f9bfa47aec669eb104efdb8cc6f92d263c1b6f1fe094a") --Main Windows Depot Yakuza Kiwami (Legacy)
+setManifestid(834534, "210304784564462656", 0)
+addappid(834535, 1, "f9e85ff7b9d2d862bc853c14bc4e7e6773efdcdfd9028d7a196296a4926a63e0") --Main Windows Depot Yakuza Kiwami (Legacy)
+setManifestid(834535, "8036566944547967721", 149098944)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
