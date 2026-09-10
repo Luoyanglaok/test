f66460b8d1b6063982737a27d3ef5e31aa56024a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:00:16 +0800

    add 346110.lua

--- 346110.lua
+++ 346110.lua
@@ -1,65 +1,123 @@
-addappid(346110)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228990,"1829726630299308803")
-addappid(346111,0,"5edb307972cd5bf1e3081aedc986ef721dfd27cae16d0a976c6b7ea6e8ff2089")
-setManifestid(346111,"2937727572174572127")
-addappid(346113,0,"3cde616ad9b9f9def5512691fe0a3ba1dfe8a6d773e68ea3b01f73b5f4f5a6bd")
-setManifestid(346113,"8339919800688829995")
-addappid(346114,0,"d8d49ab98f0f7530c0c2926213c7ac64625712a2efbbc96e6b3f06943741f806")
-setManifestid(346114,"3420388955259693525")
-addappid(346115,0,"32f03ae1bf21e71fbd0361230a6442e2a221bbfced8fdb4fd37b7ab0225cb99c")
-setManifestid(346115,"7211460979435225614")
-addappid(346117,0,"5a2bbc7508560b4d6f663c0db1923379ce85a1d08ac9750e64bdb821968e5245")
-setManifestid(346117,"2507825098602328137")
-addappid(346118,0,"8cffc3c5d721b966af7b470e8ed392cb0440ca975c4f28c6b20bbb0ae437d903")
-setManifestid(346118,"1999006499798962652")
-addappid(375351,0,"80573113a7bf294555b4c8e4e041c69e7a5a69527a4b65d3ce6f47c2382488a7")
-setManifestid(375351,"3704844677068939578")
-addappid(375352,0,"4165ab1d0803718ec4e69bdea9e03cdb00d9be97d60761600f69aa02635a6818")
-setManifestid(375352,"2239387445239950885")
-addappid(375354,0,"e2fa71e01810bbd554e94713e84efb084e0a99ff78f21fba3c201410f74e9a19")
-setManifestid(375354,"3333791656814885758")
-addappid(375355,0,"538aa57c154732fc995904719ff0ae3ebde300e4f4d6be40ec1241b6abef9c54")
-setManifestid(375355,"5324252023372007884")
-addappid(375357,0,"9bc45b9b59f3f32154ee76b47ff8b6015e92d5a6342c444bed3eb210bcd24b60")
-setManifestid(375357,"1840083954536771524")
-addappid(375358,0,"380b1dd0c8d2a98996c20021513e4af8768d5c1c19b0299d428c149ea6419be0")
-setManifestid(375358,"426338121519536361")
-addappid(473851,0,"99fb597107882942ca94e2038215f03f8707d57b059460ec6bde8265bf020733")
-setManifestid(473851,"7524216447960639216")
-addappid(473852,0,"cf96b8769d1734315042b0239dd110a83d831f4da2cfcc6066d52dd26e294069")
-setManifestid(473852,"2616742401864906431")
-addappid(473854,0,"dac71f84f6dd940b7bfe12e5df2bb98eff8ba622d76c30091c52ecfd60bbf334")
-setManifestid(473854,"3837423723924729672")
-addappid(473855,0,"f4ab0847fc2c5a996d0dd7e62142319b91e057364458f627065a679042de5cf2")
-setManifestid(473855,"9041462482401519649")
-addappid(473858,0,"41f8a645e812903465d0edae936402edbec301d6e0704cddc3192e6a661dc133")
-setManifestid(473858,"1929691095877787242")
-addappid(473857,0,"c9bb21644270fd6883bc3f007aa66f625ea3281ad2196900dfbbdf12ceb58ce2")
-setManifestid(473857,"2185564502675862595")
-addappid(1318685,0,"323f243a7ced4f90628ba9da901ca96302350c23c625153d21425d93df0a62cf")
-setManifestid(1318685,"6801959511350633211")
-addappid(1318686,0,"dd79aabbad1a2c16599b9c57ea58081e50c7ebd49a68a445823000c509361a43")
-setManifestid(1318686,"2905223544564987419")
-addappid(1691801,0,"db89fe99710be8d80b85377d37210487dbce8b9bfb8e90767f97e138d3e0206e")
-setManifestid(1691801,"4898454577078487217")
-addappid(1691802,0,"c8ad6983cc292e1bacdb1b413aef29dd704430d075258337a58edf62a8d04fd6")
-setManifestid(1691802,"8855925838913262422")
-addappid(1887561,0,"a3f5aa53c16af324c6b57326578ce7a4341c7aef7a14ff52ccfa35cba9fccbc7")
-setManifestid(1887561,"6339579471589569293")
-addappid(1887562,0,"9412a47f669490322d818e4b5d4d184fae7b3ac2ee0db526751f9da74ecd8901")
-setManifestid(1887562,"2849859682584703966")
-addappid(1318680,0,"4d062d18a7fe719d0a98a1025f680ac6ef26cb347f030a51453c02dcd8351916")
-addappid(512541,0,"267b9da1ca97290bf9311789c2c78a0226dc149b33b708f638318bffb9466d07")
-addappid(642251,0,"000551f44eb27848ac700992f1cc63b8647dd6ff487f61be75d715690f7ffaa8")
-addappid(708771,0,"7a010e1818b19197008fec7530f81134496e048b22799ab8dbaa15afc934640b")
-addappid(887381,0,"9ee1587cedb1f2530eedff959f611469b80c52a19d671aaf5ca90c9b8cf2923f")
-addappid(1100811,0,"b79b9927e9dc5b5e4a01d8bd53ca4fd403aa3e00a6d9b328df9517e17eb5b741")
-addappid(1113411,0,"fabb760fd389664584fd71e1f32755adcae789c57c7080d91f8a46e1a660bae3")
-addappid(1270831,0,"9db4e7056d0b077245b0193c7efee5978c425e8a3e4bf2946fdca79a09358829")
-addappid(1691805,0,"f926b2b5de3cc1a5f85b15e97ea6bac7a123a0a986d32cf3f77669e4b65ebc44")
-addappid(1887565,0,"1cfee4900783d7c2b56c8aa3288ade97d875296edb363cf38f307d1706c6da1b")
-addappid(3537070,0,"9a07e220390d63d22b6fca95fca32f71bcad2d28494d72ee2c79b5afcb84c473")
-setManifestid(3537070,"2775816645345839354")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 346110.lua
+--Gamename ARK: Survival Evolved
+addappid(346110, 1, "e16ff7f08225d9ae3635a98833a1c63acc58b2a104b0b37a9605d494683787be") --Mainappid ARK: Survival Evolved
+addappid(346111, 1, "5edb307972cd5bf1e3081aedc986ef721dfd27cae16d0a976c6b7ea6e8ff2089") --Main Windows Depot ARK: Survival Evolved
+setManifestid(346111, "6154025194991279746", 61597325312)
+addappid(346113, 1, "3cde616ad9b9f9def5512691fe0a3ba1dfe8a6d773e68ea3b01f73b5f4f5a6bd") --Main Macos Depot ARK: Survival Evolved
+setManifestid(346113, "8339919800688829995", 42016573776)
+addappid(1318680, 1, "4d062d18a7fe719d0a98a1025f680ac6ef26cb347f030a51453c02dcd8351916") --Main Linux Depot ARK: Survival Evolved
+setManifestid(1318680, "8060274748931292766", 42008643344)
+--Dlcname The Center - ARK Expansion Map
+addappid(473850) --Dlcname The Center - ARK Expansion Map
+addappid(346114, 1, "d8d49ab98f0f7530c0c2926213c7ac64625712a2efbbc96e6b3f06943741f806") --Dlc Windows Depot The Center - ARK Expansion Map
+setManifestid(346114, "3763053575682314024", 2306214720)
+addappid(346115, 1, "32f03ae1bf21e71fbd0361230a6442e2a221bbfced8fdb4fd37b7ab0225cb99c") --Dlc Macos Depot The Center - ARK Expansion Map
+setManifestid(346115, "7211460979435225614", 820646832)
+addappid(473851, 1, "99fb597107882942ca94e2038215f03f8707d57b059460ec6bde8265bf020733") --Dlc Windows Depot The Center - ARK Expansion Map
+setManifestid(473851, "4803031605764221686", 3198905824)
+--Dlcname Primitive+ - ARK Total Conversion
+addappid(508150) --Dlcname Primitive+ - ARK Total Conversion
+addappid(346117, 1, "5a2bbc7508560b4d6f663c0db1923379ce85a1d08ac9750e64bdb821968e5245") --Dlc Windows Depot Primitive+ - ARK Total Conversion
+setManifestid(346117, "2507825098602328137", 844806048)
+addappid(346118, 1, "8cffc3c5d721b966af7b470e8ed392cb0440ca975c4f28c6b20bbb0ae437d903") --Dlc Macos Depot Primitive+ - ARK Total Conversion
+setManifestid(346118, "1999006499798962652", 820649264)
+--Dlcname ARK: Scorched Earth - Expansion Pack
+addappid(512540) --Dlcname ARK: Scorched Earth - Expansion Pack
+addappid(375351, 1, "80573113a7bf294555b4c8e4e041c69e7a5a69527a4b65d3ce6f47c2382488a7") --Dlc Windows Depot ARK: Scorched Earth - Expansion Pack
+setManifestid(375351, "4395836124878524913", 1884497520)
+addappid(375352, 1, "4165ab1d0803718ec4e69bdea9e03cdb00d9be97d60761600f69aa02635a6818") --Dlc Macos Depot ARK: Scorched Earth - Expansion Pack
+setManifestid(375352, "2239387445239950885", 371891696)
+addappid(512541, 1, "267b9da1ca97290bf9311789c2c78a0226dc149b33b708f638318bffb9466d07") --Dlc Linux Depot ARK: Scorched Earth - Expansion Pack
+setManifestid(512541, "3511445840272695131", 371864240)
+--Dlcname Ragnarok - ARK Expansion Map
+addappid(642250) --Dlcname Ragnarok - ARK Expansion Map
+addappid(375354, 1, "e2fa71e01810bbd554e94713e84efb084e0a99ff78f21fba3c201410f74e9a19") --Dlc Windows Depot Ragnarok - ARK Expansion Map
+setManifestid(375354, "3831237975753864887", 3972479136)
+addappid(375355, 1, "538aa57c154732fc995904719ff0ae3ebde300e4f4d6be40ec1241b6abef9c54") --Dlc Macos Depot Ragnarok - ARK Expansion Map
+setManifestid(375355, "5324252023372007884", 903893456)
+addappid(642251, 1, "000551f44eb27848ac700992f1cc63b8647dd6ff487f61be75d715690f7ffaa8") --Dlc Linux Depot Ragnarok - ARK Expansion Map
+setManifestid(642251, "5233120494394645986", 903814816)
+--Dlcname ARK: Aberration - Expansion Pack
+addappid(708770) --Dlcname ARK: Aberration - Expansion Pack
+addappid(375357, 1, "9bc45b9b59f3f32154ee76b47ff8b6015e92d5a6342c444bed3eb210bcd24b60") --Dlc Windows Depot ARK: Aberration - Expansion Pack
+setManifestid(375357, "6791072184851719971", 2522437904)
+addappid(375358, 1, "380b1dd0c8d2a98996c20021513e4af8768d5c1c19b0299d428c149ea6419be0") --Dlc Macos Depot ARK: Aberration - Expansion Pack
+setManifestid(375358, "426338121519536361", 418724016)
+addappid(708771, 1, "7a010e1818b19197008fec7530f81134496e048b22799ab8dbaa15afc934640b") --Dlc Linux Depot ARK: Aberration - Expansion Pack
+setManifestid(708771, "6493885717147978831", 418735712)
+--Dlcname ARK: Extinction - Expansion Pack
+addappid(887380) --Dlcname ARK: Extinction - Expansion Pack
+addappid(473852, 1, "cf96b8769d1734315042b0239dd110a83d831f4da2cfcc6066d52dd26e294069") --Dlc Macos Depot ARK: Extinction - Expansion Pack
+setManifestid(473852, "2616742401864906431", 551445632)
+addappid(887381, 1, "9ee1587cedb1f2530eedff959f611469b80c52a19d671aaf5ca90c9b8cf2923f") --Dlc Linux Depot ARK: Extinction - Expansion Pack
+setManifestid(887381, "1054470338340920835", 551822848)
+--Dlcname Valguero - ARK Expansion Map
+addappid(1100810) --Dlcname Valguero - ARK Expansion Map
+addappid(473854, 1, "dac71f84f6dd940b7bfe12e5df2bb98eff8ba622d76c30091c52ecfd60bbf334") --Dlc Windows Depot Valguero - ARK Expansion Map
+setManifestid(473854, "3892036305094432143", 3758304608)
+addappid(473855, 1, "f4ab0847fc2c5a996d0dd7e62142319b91e057364458f627065a679042de5cf2") --Dlc Macos Depot Valguero - ARK Expansion Map
+setManifestid(473855, "9041462482401519649", 1075944960)
+addappid(1100811, 1, "b79b9927e9dc5b5e4a01d8bd53ca4fd403aa3e00a6d9b328df9517e17eb5b741") --Dlc Linux Depot Valguero - ARK Expansion Map
+setManifestid(1100811, "7695993562551442119", 1076083264)
+--Dlcname ARK: Genesis Season Pass
+addappid(1113410) --Dlcname ARK: Genesis Season Pass
+addappid(473857, 1, "c9bb21644270fd6883bc3f007aa66f625ea3281ad2196900dfbbdf12ceb58ce2") --Dlc Windows Depot ARK: Genesis Season Pass
+setManifestid(473857, "3988040328692901254", 11120262160)
+addappid(473858, 1, "41f8a645e812903465d0edae936402edbec301d6e0704cddc3192e6a661dc133") --Dlc Macos Depot ARK: Genesis Season Pass
+setManifestid(473858, "1929691095877787242", 2060161008)
+addappid(1113411, 1, "fabb760fd389664584fd71e1f32755adcae789c57c7080d91f8a46e1a660bae3") --Dlc Linux Depot ARK: Genesis Season Pass
+setManifestid(1113411, "2598227911265107161", 2060117104)
+--Dlcname Crystal Isles - ARK Expansion Map
+addappid(1270830) --Dlcname Crystal Isles - ARK Expansion Map
+addappid(1270831, 1, "9db4e7056d0b077245b0193c7efee5978c425e8a3e4bf2946fdca79a09358829") --Dlc Linux Depot Crystal Isles - ARK Expansion Map
+setManifestid(1270831, "1641301694859193503", 2119029440)
+addappid(1318685, 1, "323f243a7ced4f90628ba9da901ca96302350c23c625153d21425d93df0a62cf") --Dlc Windows Depot Crystal Isles - ARK Expansion Map
+setManifestid(1318685, "1956361019326166695", 7090950880)
+addappid(1318686, 1, "dd79aabbad1a2c16599b9c57ea58081e50c7ebd49a68a445823000c509361a43") --Dlc Macos Depot Crystal Isles - ARK Expansion Map
+setManifestid(1318686, "2905223544564987419", 2119353280)
+--Dlcname Lost Island - ARK Expansion Map
+addappid(1691800) --Dlcname Lost Island - ARK Expansion Map
+addappid(1691801, 1, "db89fe99710be8d80b85377d37210487dbce8b9bfb8e90767f97e138d3e0206e") --Dlc Windows Depot Lost Island - ARK Expansion Map
+setManifestid(1691801, "7582104043070509624", 14492899568)
+addappid(1691802, 1, "c8ad6983cc292e1bacdb1b413aef29dd704430d075258337a58edf62a8d04fd6") --Dlc Macos Depot Lost Island - ARK Expansion Map
+setManifestid(1691802, "8855925838913262422", 4068742432)
+addappid(1691805, 1, "f926b2b5de3cc1a5f85b15e97ea6bac7a123a0a986d32cf3f77669e4b65ebc44") --Dlc Linux Depot Lost Island - ARK Expansion Map
+setManifestid(1691805, "4699391172317574864", 4068174336)
+--Dlcname Fjordur - ARK Expansion Map
+addappid(1887560) --Dlcname Fjordur - ARK Expansion Map
+addappid(1887561, 1, "a3f5aa53c16af324c6b57326578ce7a4341c7aef7a14ff52ccfa35cba9fccbc7") --Dlc Windows Depot Fjordur - ARK Expansion Map
+setManifestid(1887561, "1479180173861153414", 7623968800)
+addappid(1887562, 1, "9412a47f669490322d818e4b5d4d184fae7b3ac2ee0db526751f9da74ecd8901") --Dlc Macos Depot Fjordur - ARK Expansion Map
+setManifestid(1887562, "2849859682584703966", 2855775216)
+addappid(1887565, 1, "1cfee4900783d7c2b56c8aa3288ade97d875296edb363cf38f307d1706c6da1b") --Dlc Linux Depot Fjordur - ARK Expansion Map
+setManifestid(1887565, "8813640760145155438", 2855771120)
+--Dlcname Aquatica - ARK Expansion Map
+addappid(3537070, 1, "9a07e220390d63d22b6fca95fca32f71bcad2d28494d72ee2c79b5afcb84c473") --Dlcname Aquatica - ARK Expansion Map
+setManifestid(3537070, "4062438905818018280", 5276023840)
+--Dlcname ARK: Aquatica - Luminati Suns Song
+addappid(3761020) --Dlcname ARK: Aquatica - Luminati Suns Song
+addappid(3761021, 1, "014f2af8839a4964cf5c957f5fd49833d9b22717887e02c132266b3ae74951b1") --Dlc Windows Depot ARK: Aquatica - Luminati Suns Song
+setManifestid(3761021, "6417303618500064550", 6609392)
+--Dlcname ARK: Aquatica Original Soundtrack
+addappid(3820060) --Dlcname ARK: Aquatica Original Soundtrack
+addappid(3820061, 1, "83C4455101B8E21BB22C295E7F0B0C9B5B7530624BB3AD3B1BC53B933746D049") --Dlc Windows Depot ARK: Aquatica Original Soundtrack
+setManifestid(3820061, "8402848240653047855", 36537584)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(696680) --Dlcname ARK: Survival Evolved Season Pass
+addappid(1646550) --Dlcname ARK: Survival Evolved Original Soundtrack
+addtoken(1646550, "2789243375787517774")
+addappid(1646690) --Dlcname ARK: Expansion Packs Original Soundtrack
+addtoken(1646690, "5000450121281393136")
+addappid(1646700) --Dlcname ARK: Genesis Part 1 Original Soundtrack
+addtoken(1646700, "17188204630189073134")
+addappid(1646720) --Dlcname ARK: Genesis Part 2 Original Soundtrack
+addtoken(1646720, "3965590415632377873")
