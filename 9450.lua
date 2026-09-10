df77dd0609e5de2c9b38d0bb8763d40e21dc2e26
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:52:57 +0800

    add 9450.lua

--- 9450.lua
+++ 9450.lua
@@ -1,16 +1,21 @@
-addappid(9450)
-setManifestid(228981,"7613356809904826842")
-addappid(9454,0,"3330a269eb0bfc01b9c2d12cc4e9cf598d9b347b99852004acd3056a1813ac85")
-setManifestid(9454,"1775370173744485268")
-addappid(9453,0,"c286f05c5b6967c03d8db959764a67f3319414dde66370c257709c571dd8ad61")
-setManifestid(9453,"414038789054871101")
-addappid(9452,0,"04888da06c535124dd51c4f0ce5e0eaf55a180b141d079fc3e5a6f05429bb2a7")
-setManifestid(9452,"4494728472524602230")
-addappid(9451,0,"477db55d6c15b28de148b66ce7ddeec414186bdc346977abab98037eaeeb4cac")
-setManifestid(9451,"3070633727290409394")
-addappid(9455,0,"169c41e2641c6d2d64f42694e7be06195f9e10c54724f3a8855bc7b9b0057601")
-setManifestid(9455,"4220012594067707071")
-addappid(9456,0,"ec86938425d5b7f7358ad2bb4298c9b976878c43b0a3d97935b5ce828ccf5e4c")
-setManifestid(9456,"7840161698729712018")
-addappid(9457,0,"439ed8865876be5486a742d02b86ecfdcb258d6d1bf59467d63a67cda0709667")
-setManifestid(9457,"3158835098303731254")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9450.lua
+--Gamename Warhammer 40,000: Dawn of War - Soulstorm
+addappid(9450, 1, "8332a02d0ac70c6cc5b8b7aaa45330edf3dea797c407ea4f079e760070c14296") --Mainappid Warhammer 40,000: Dawn of War - Soulstorm
+addappid(9451, 1, "477db55d6c15b28de148b66ce7ddeec414186bdc346977abab98037eaeeb4cac") --Main Windows Depot Warhammer 40,000: Dawn of War - Soulstorm
+setManifestid(9451, "3070633727290409394", 1003046272)
+addappid(9452, 1, "04888da06c535124dd51c4f0ce5e0eaf55a180b141d079fc3e5a6f05429bb2a7") --Main Windows Depot Warhammer 40,000: Dawn of War - Soulstorm
+setManifestid(9452, "4494728472524602230", 1365848064)
+addappid(9453, 1, "c286f05c5b6967c03d8db959764a67f3319414dde66370c257709c571dd8ad61") --Main Windows Depot Warhammer 40,000: Dawn of War - Soulstorm
+setManifestid(9453, "414038789054871101", 1176149584)
+addappid(9454, 1, "3330a269eb0bfc01b9c2d12cc4e9cf598d9b347b99852004acd3056a1813ac85") --Main Windows Depot Warhammer 40,000: Dawn of War - Soulstorm
+setManifestid(9454, "1775370173744485268", 528782464)
+addappid(9455, 1, "169c41e2641c6d2d64f42694e7be06195f9e10c54724f3a8855bc7b9b0057601") --Main Windows Language Depot English Warhammer 40,000: Dawn of War - Soulstorm
+setManifestid(9455, "4220012594067707071", 521734800)
+addappid(9456, 1, "ec86938425d5b7f7358ad2bb4298c9b976878c43b0a3d97935b5ce828ccf5e4c") --Main Windows Language Depot French Warhammer 40,000: Dawn of War - Soulstorm
+setManifestid(9456, "7840161698729712018", 545301568)
+addappid(9457, 1, "439ed8865876be5486a742d02b86ecfdcb258d6d1bf59467d63a67cda0709667") --Main Windows Language Depot German Warhammer 40,000: Dawn of War - Soulstorm
+setManifestid(9457, "3158835098303731254", 624054528)
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
