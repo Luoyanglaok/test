95cf25d3e6a63671b59f454310ccc4e9ac076811
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:26:17 +0800

    add 2639100.lua

--- 2639100.lua
+++ 2639100.lua
@@ -1,3 +1,6 @@
-addappid(2639100)
-addappid(2639101,0,"21f31c8adf14db935b09d4d1a9ea8e781df503db08605f5daa84515f3dd6f10c")
-setManifestid(2639101,"4161348225931473101")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2639100.lua
+--Gamename Inari
+addappid(2639100, 1, "3501fe45b06f7c3a2f81a9c1f9eeb56c57096aded6a9a9d2c54a52e10ee761ff") --Mainappid Inari
+addappid(2639101, 1, "21f31c8adf14db935b09d4d1a9ea8e781df503db08605f5daa84515f3dd6f10c") --Main Windows Depot Inari
+setManifestid(2639101, "4161348225931473101", 1805528688)
