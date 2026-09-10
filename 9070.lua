6552353c8d5ae4fcf686c6662d0d8ef10fd9386d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:03:52 +0800

    add 9070.lua

--- 9070.lua
+++ 9070.lua
@@ -1,15 +1,19 @@
-addappid(9070)
-addappid(9051)
-setManifestid(9051,"8286038173018646412")
-addappid(9071,0,"ff4f9c0f456fbdf13bbac2511951fbea8fa2e81fac9416dbcee5008845be6b91")
-setManifestid(9071,"4235632539331474360")
-addappid(9052)
-setManifestid(9052,"234565912299936290")
-addappid(9053)
-setManifestid(9053,"1051348798445073166")
-addappid(9054)
-setManifestid(9054,"6013538961173469274")
-addappid(9055)
-setManifestid(9055,"8209211353725172101")
-addappid(9057)
-setManifestid(9057,"5681427843027684907")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 9070.lua
+--Gamename DOOM 3: Resurrection of Evil
+addappid(9070, 1, "b0b03c22fbcc0865132395ff9e576efa64a117131936c35d2d533a29cf16b997") --Mainappid DOOM 3: Resurrection of Evil
+addappid(9071, 1, "ff4f9c0f456fbdf13bbac2511951fbea8fa2e81fac9416dbcee5008845be6b91") --Main Windows Depot DOOM 3: Resurrection of Evil
+setManifestid(9071, "4235632539331474360", 0)
+--Share Depots
+addappid(9051, 1, "a6b3510995bf7cc4e56b42c020d52546ab9ecc6ff1bf04bc28e05ab28af78ff5") --Share Windows Depot DOOM 3
+setManifestid(9051, "8286038173018646412", 0)
+addappid(9052, 1, "0bddb46cb1153bbb6b7d38c18eb032f8866566caf51b17b1a5230b9f273f3993") --Share Windows Language Depot English DOOM 3
+setManifestid(9052, "234565912299936290", 0)
+addappid(9053, 1, "94a8f12d066cde33d47a0ad640e4478a59e367a08a21bb6061c5d5ee24a46697") --Share Windows Language Depot Spanish DOOM 3
+setManifestid(9053, "1051348798445073166", 0)
+addappid(9054, 1, "c6119bfdb230bbaf193f988ed17bd1a6c77956369e05b0d55c7e68e91d488463") --Share Windows Language Depot German DOOM 3
+setManifestid(9054, "6013538961173469274", 0)
+addappid(9055, 1, "9c0cdfa9d2cd8d82f641efe69dd99fba4562dbf2c98b47585e928b6fbcbd2c15") --Share Windows Language Depot Italian DOOM 3
+setManifestid(9055, "8209211353725172101", 0)
+addappid(9057, 1, "ef243538908734ad17979cd9d77f9b615a535bdb7b37313f49b7a7b35bc830aa") --Share Windows Language Depot French DOOM 3
+setManifestid(9057, "5681427843027684907", 0)
