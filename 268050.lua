c9539823fd72681ac38f8ce4f467732fcc36b498
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:53:48 +0800

    add 268050.lua

--- 268050.lua
+++ 268050.lua
@@ -1,12 +1,27 @@
-addappid(268050)
-setManifestid(228990,"1829726630299308803")
-addappid(268053,0,"84748307b8aaef4518f3657e9d2250072898e781824478f78f33197b5c157d10")
-setManifestid(268053,"3063496017212715396")
-addappid(268051,0,"93ee80eca527bcfb7d775351a682fee43cb7e507105958ade0b022e19a59f4d0")
-setManifestid(268051,"133536520792078154")
-addappid(268052,0,"2f35291eae1dedeb25ab35a40987c05dee10afd39883acdd1eb86f411ffbdc01")
-setManifestid(268052,"1263818844251762323")
-addappid(359930,0,"bad4fe532d4b0af4716ada1632250ca9a4993e693615aee74b8a5cd50872b8e2")
-setManifestid(359930,"5306620168622236739")
-addappid(446950,0,"bf30aa6a29252e5fedf5bb424228fa5549eb907f08ffebe3769e98c9d32e36cd")
-setManifestid(446950,"8864889076466158208")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 268050.lua
+--Gamename The Evil Within
+addappid(268050, 1, "217a485f3ff7ca992a54feb9d8deb926854fe87de5349ef07f5a464f64daef9d") --Mainappid The Evil Within
+addappid(268053, 1, "84748307b8aaef4518f3657e9d2250072898e781824478f78f33197b5c157d10") --Main Windows Depot The Evil Within
+setManifestid(268053, "3063496017212715396", 33855692592)
+--Dlcname The Evil Within: The Assignment
+addappid(345980) --Dlcname The Evil Within: The Assignment
+addappid(268051, 1, "93ee80eca527bcfb7d775351a682fee43cb7e507105958ade0b022e19a59f4d0") --Dlc Windows Depot The Evil Within: The Assignment
+setManifestid(268051, "133536520792078154", 6511984272)
+--Dlcname The Evil Within: The Consequence
+addtoken(354710, "4391196372988051497")
+addappid(354710) --Dlcname The Evil Within: The Consequence
+addappid(268052, 1, "2f35291eae1dedeb25ab35a40987c05dee10afd39883acdd1eb86f411ffbdc01") --Dlc Windows Depot The Evil Within: The Consequence
+setManifestid(268052, "1263818844251762323", 6699941072)
+--Dlcname The Evil Within: The Executioner
+addappid(359930, 1, "bad4fe532d4b0af4716ada1632250ca9a4993e693615aee74b8a5cd50872b8e2") --Dlcname The Evil Within: The Executioner
+setManifestid(359930, "5306620168622236739", 4240685008)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(446950, 1, "bf30aa6a29252e5fedf5bb424228fa5549eb907f08ffebe3769e98c9d32e36cd") --Share Windows Depot The Evil Within - Soundtrack
+setManifestid(446950, "8864889076466158208", 105196448)
+addappid(318750) --Dlcname AppID 318750
+addtoken(318750, "6843027824690960282")
+addappid(318751) --Dlcname AppID 318751
+addtoken(318751, "815011547124599054")
