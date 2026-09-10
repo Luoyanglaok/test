e8c8cb8e9e248c33f4d963d56e281315b1a25770
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:01:16 +0800

    add 568220.lua

--- 568220.lua
+++ 568220.lua
@@ -1,5 +1,9 @@
-addappid(568220)
-addappid(568221,0,"4b83656636360e659df7b8f983d8436fe8216360fe0c6edf14f819a8622d70c7")
-setManifestid(568221,"8058397718201736202")
-addappid(896000,0,"76d7260e37893f7d0e8100d73d3685b1ebe2a0c8e71be8034d6f4c2d6596374b")
-setManifestid(896000,"4668171197035314231")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 568220.lua
+--Gamename Lobotomy Corporation | Monster Management Simulation
+addappid(568220, 1, "fa653f2dc25ab3fc344e141d5179f2fa7daae1455414ed988a0ca8d7c0d72e04") --Mainappid Lobotomy Corporation | Monster Management Simulation
+addappid(568221, 1, "4b83656636360e659df7b8f983d8436fe8216360fe0c6edf14f819a8622d70c7") --Main Windows Depot Lobotomy Corporation | Monster Management Simulation
+setManifestid(568221, "8058397718201736202", 1629881056)
+--Dlcname LobotomyCorporation_ArtBook
+addappid(896000, 1, "76d7260e37893f7d0e8100d73d3685b1ebe2a0c8e71be8034d6f4c2d6596374b") --Dlcname LobotomyCorporation_ArtBook
+setManifestid(896000, "4668171197035314231", 352246080)
