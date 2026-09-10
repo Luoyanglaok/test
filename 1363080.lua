345e3c93f840343863d3da5186cb90e5d67023c7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:52:35 +0800

    add 1363080.lua

--- 1363080.lua
+++ 1363080.lua
@@ -1,7 +1,18 @@
-addappid(1363080)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229007,"4477590687906973371")
-setManifestid(229033,"2059065101492814639")
-addappid(1363081,0,"28763f0001125b10d1d269c0655d8d3f9e6a7678dd9fed0af3f4a37701443e70")
-setManifestid(1363081,"7778699477787378160")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1363080.lua
+--Gamename Manor Lords
+addappid(1363080, 1, "e5bf8cd0faa28d9d63f560b028dca5bc8045f07979973e57536826644f2f4f43") --Mainappid Manor Lords
+addappid(1363081, 1, "28763f0001125b10d1d269c0655d8d3f9e6a7678dd9fed0af3f4a37701443e70") --Main Windows Depot Manor Lords
+setManifestid(1363081, "7662659300316410169", 7247134960)
+--Dlcname Manor Lords - Artbook
+addappid(2932660, 1, "1adba25f4fa5ce03145e033f1f027e5d411e8db2042fbad6317352153d507bfe") --Dlcname Manor Lords - Artbook
+setManifestid(2932660, "7430712793949389372", 443300640)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
