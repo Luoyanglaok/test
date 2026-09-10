f635f221b029e9853ee1fcaa4f0dbcb048d69926
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:55:18 +0800

    add 534380.lua

--- 534380.lua
+++ 534380.lua
@@ -1,76 +1,120 @@
-addappid(534380)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(534381,0,"fec776617825a7f3e61dd191f407bd23c6f60ba1056a506f1facb4fd74d9f7e2")
-setManifestid(534381,"2431211655003768534")
-addappid(534382,0,"cf933660206b892b3d32060682d4063e7a42482428b1cbb3f0b8fed43cdfee45")
-setManifestid(534382,"1973955231117206288")
-addappid(534383,0,"1a2b6e67c39a9803c80ab38c1be8b25b112ae32eaa1872b6cd42db109049e007")
-setManifestid(534383,"6639748655531310770")
-addappid(534384,0,"b5e4f819a4ac1b9e78bca8293db8428a4eea8a95b6913fc32842010060db425e")
-setManifestid(534384,"900395428038630535")
-addappid(534385,0,"c7a22e2cab3e656efbdaceb8e2a7b76748c1790517faf3283beaa2cf761f5dc0")
-setManifestid(534385,"4975424511675893864")
-addappid(534386,0,"0f124b03d74178f173d00c78c84573dc7cef743f0d0835edea668a5ed3ba116e")
-setManifestid(534386,"1344544806272816313")
-addappid(534387,0,"d3fcdef424d1ec4c1a72bc78a8ae5364b74a5abd23ef7cbeca64996cad28b24b")
-setManifestid(534387,"1935456551746027043")
-addappid(534388,0,"9af548a883576aa8eb98d69ceef4d0a50b7e5beebe14744e078cd2f47ace909b")
-setManifestid(534388,"7734306966367034259")
-addappid(534389,0,"3e6c48fa5a81522f18e7633fa89b6ea95f05d991c626ee9ceaebecf711cd6e72")
-setManifestid(534389,"1300212907823593285")
-addappid(1741780,0,"56a0e9c235e8305b7e1f1e722252b52918ea0003a341b5901c42ec0d39cd449c")
-setManifestid(1741780,"6540041772615006948")
-addappid(1741781,0,"42678fed970f2d33723e75c88c27c4d19278d40ddbea2be652c02d7dfb41404f")
-setManifestid(1741781,"7370229488720708177")
-addappid(1741782,0,"26d52711c65b1d4c96eb06fb7fba2a68e80984a9828a074bf8a5384c16e86433")
-setManifestid(1741782,"5111549044329075420")
-addappid(1741783,0,"f1fd93f6cd43dbde9dfecda6beceaec208b0c3842cd34ded16b3f31ae7984f0b")
-setManifestid(1741783,"3489386982795606791")
-addappid(1741784,0,"56ca0a485646ce4d5fb8163100b5f62610f743c214fabd9133f57b3dc3720d44")
-setManifestid(1741784,"4250953824362817398")
-addappid(1741785,0,"377954647d348be60b76ce49cae9b13763ad66aba3ce9ec5e1b0ba1f9f6c7a49")
-setManifestid(1741785,"6811508315827086568")
-addappid(1741786,0,"19a6afcb6a47753b8b8d5f8a0febea931845a56103db941a3cedc57fa4fa3632")
-setManifestid(1741786,"5745490560579869331")
-addappid(1741787,0,"a2dcee88dfd62b92c47a6260dd083eefd70bb504b41012881ef45e10d3ef31e4")
-setManifestid(1741787,"7269720910837587775")
-addappid(1741788,0,"5647c59ff805db90b39f2286a138774c318579addeb0ae3cd67cb37be8391794")
-setManifestid(1741788,"1029618011306315267")
-addappid(1741789,0,"d45069f6f74b78a88a6d5bd30c9e7e56cb00c84e80957adc473e0815581f8ca5")
-setManifestid(1741789,"6643666169597174051")
-addappid(1741790,0,"ffb3557cf402c7a2847a17a705d05cc02b1c921cedb1a4e33c0965c3ef3b8ead")
-setManifestid(1741790,"6412698918778375817")
-addappid(1741791,0,"83833d3a3c1d59757bd4fd32fe79b0498cfdbd4ba1a4c770319bf58734d3220c")
-setManifestid(1741791,"7985642131435435878")
-addappid(1741792,0,"eec928bf10da86f55bc03800c52b9292406c09c72e9d5ef99373c846e960f5ca")
-setManifestid(1741792,"5667776213858150255")
-addappid(1741793,0,"4dd7ff0cb01db2bf5c574054353078bef596a1cd611a8268bd5a023c65022278")
-setManifestid(1741793,"7712872807986339616")
-addappid(1741794,0,"17d125330d6f175e6c28c56630312084fe07a44a06bcbffc530c3f590d01667c")
-setManifestid(1741794,"4000949483817469060")
-addappid(1741795,0,"8b0095c3dc40776199884af8ac54987c23277a352c03f79acdbf51eea7ef6191")
-setManifestid(1741795,"5708377914858544403")
-addappid(1741796,0,"6d55d6c6f95126badb8fc4d39fafcd9abfe6470847f8c3a81057aa9b260c6107")
-setManifestid(1741796,"2673483473009527883")
-addappid(1741797,0,"b3c0705864ffb87b5be6890de5ecd919959de996b6a9a60ec6f84a3bea7bd768")
-setManifestid(1741797,"2086682394900066193")
-addappid(1741798,0,"dac1cd4282c31bca5e08c68b37b3939433252e9716025c74c0eef121dcc56938")
-setManifestid(1741798,"3842467555925097725")
-addappid(1741799,0,"962cef0a55b1ae29a391f43431ee4b98a4f54bb40cab9b0a047491df31d5b6c2")
-setManifestid(1741799,"8557508875599521571")
-addappid(1741800,0,"85cbd9ea07b0212def7701851f906cfc2015697823afa00849e0ffadacb4ee2c")
-setManifestid(1741800,"6931320953933785693")
-addappid(1741801,0,"8d26e4e39b3d4f7beec6b01e44a9c7caf7d5e9eae6e499ef414cde2d79caa94b")
-setManifestid(1741801,"2724688667461108035")
-addappid(1741802,0,"0452622d330096a7e928803be80e7f859a861c522eebb67fc338824122ea90cb")
-setManifestid(1741802,"8369068266057563647")
-addappid(1741803,0,"921360aefff71e9ec8ab8a2ee251e4b718ff615f3183c5fe0e0080bf7f434766")
-setManifestid(1741803,"9165989432588229945")
-addappid(1741804,0,"6d1931aa3987c3bf777944cd3240584ed15e169dc8ad97da57449f72730911cb")
-setManifestid(1741804,"2382853488395355170")
-addappid(1741805,0,"7fd30a7a706af1dc2c94010cf7486ebf1d49af69f2aebe663075b6810d1dd527")
-setManifestid(1741805,"5373479986815706731")
-addappid(1263581,0,"b7e959a2f431a626db99f0f9d3e4f9da7b5f725308214738c3f93ecc70aec314")
-setManifestid(1263581,"4576148900009433954")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 534380.lua
+--Gamename Dying Light 2 Stay Human: Reloaded Edition
+addappid(534380, 1, "da5060467c4b9199432fb5d9ba41ae2896a9c4a4705bc8e8f426155dc8efae83") --Mainappid Dying Light 2 Stay Human: Reloaded Edition
+addappid(534381, 1, "fec776617825a7f3e61dd191f407bd23c6f60ba1056a506f1facb4fd74d9f7e2") --Main Windows Depot Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(534381, "6047889557930655330", 20902308064)
+addappid(534382, 1, "cf933660206b892b3d32060682d4063e7a42482428b1cbb3f0b8fed43cdfee45") --Main Windows Depot Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(534382, "5735164629807342085", 169879344)
+addappid(534383, 1, "1a2b6e67c39a9803c80ab38c1be8b25b112ae32eaa1872b6cd42db109049e007") --Main Windows Depot Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(534383, "8136690296726921687", 26293744992)
+addappid(534384, 1, "b5e4f819a4ac1b9e78bca8293db8428a4eea8a95b6913fc32842010060db425e") --Main Windows Depot Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(534384, "7460841048279528617", 4806246656)
+addappid(534385, 1, "c7a22e2cab3e656efbdaceb8e2a7b76748c1790517faf3283beaa2cf761f5dc0") --Main Windows Depot Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(534385, "7717590928254460278", 513945200)
+addappid(534387, 1, "d3fcdef424d1ec4c1a72bc78a8ae5364b74a5abd23ef7cbeca64996cad28b24b") --Main Windows Language Depot Arabic Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(534387, "7555660583696724859", 3023104)
+addappid(534388, 1, "9af548a883576aa8eb98d69ceef4d0a50b7e5beebe14744e078cd2f47ace909b") --Main Windows Language Depot Brazilian Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(534388, "2167658038064718984", 4649037088)
+addappid(534389, 1, "3e6c48fa5a81522f18e7633fa89b6ea95f05d991c626ee9ceaebecf711cd6e72") --Main Windows Language Depot Brazilian Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(534389, "18138001885843337", 427318080)
+addappid(1741780, 1, "56a0e9c235e8305b7e1f1e722252b52918ea0003a341b5901c42ec0d39cd449c") --Main Windows Language Depot Schinese Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741780, "4663332200780026069", 4558160416)
+addappid(1741781, 1, "42678fed970f2d33723e75c88c27c4d19278d40ddbea2be652c02d7dfb41404f") --Main Windows Language Depot Schinese Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741781, "8797685774151594368", 441810272)
+addappid(1741782, 1, "26d52711c65b1d4c96eb06fb7fba2a68e80984a9828a074bf8a5384c16e86433") --Main Windows Language Depot Tchinese Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741782, "6542748555918287759", 3748400)
+addappid(1741783, 1, "f1fd93f6cd43dbde9dfecda6beceaec208b0c3842cd34ded16b3f31ae7984f0b") --Main Windows Language Depot Czech Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741783, "5836237907410473330", 2223472)
+addappid(1741784, 1, "56ca0a485646ce4d5fb8163100b5f62610f743c214fabd9133f57b3dc3720d44") --Main Windows Language Depot German Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741784, "6461171604122033892", 4802110176)
+addappid(1741785, 1, "377954647d348be60b76ce49cae9b13763ad66aba3ce9ec5e1b0ba1f9f6c7a49") --Main Windows Language Depot German Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741785, "1410127441701920489", 438851824)
+addappid(1741786, 1, "19a6afcb6a47753b8b8d5f8a0febea931845a56103db941a3cedc57fa4fa3632") --Main Windows Language Depot Latam Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741786, "7656732264300752744", 4828174288)
+addappid(1741787, 1, "a2dcee88dfd62b92c47a6260dd083eefd70bb504b41012881ef45e10d3ef31e4") --Main Windows Language Depot Latam Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741787, "1415079305717038115", 454346128)
+addappid(1741788, 1, "5647c59ff805db90b39f2286a138774c318579addeb0ae3cd67cb37be8391794") --Main Windows Language Depot Spanish Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741788, "7472575343308253578", 4787393392)
+addappid(1741789, 1, "d45069f6f74b78a88a6d5bd30c9e7e56cb00c84e80957adc473e0815581f8ca5") --Main Windows Language Depot Spanish Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741789, "2279241920317545539", 476871024)
+addappid(1741790, 1, "ffb3557cf402c7a2847a17a705d05cc02b1c921cedb1a4e33c0965c3ef3b8ead") --Main Windows Language Depot French Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741790, "3851379681756359611", 4630932672)
+addappid(1741791, 1, "83833d3a3c1d59757bd4fd32fe79b0498cfdbd4ba1a4c770319bf58734d3220c") --Main Windows Language Depot French Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741791, "2578560080327108121", 453136992)
+addappid(1741792, 1, "eec928bf10da86f55bc03800c52b9292406c09c72e9d5ef99373c846e960f5ca") --Main Windows Language Depot Italian Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741792, "3775539584642866948", 2159056)
+addappid(1741793, 1, "4dd7ff0cb01db2bf5c574054353078bef596a1cd611a8268bd5a023c65022278") --Main Windows Language Depot Japanese Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741793, "8997627425899583033", 4735447360)
+addappid(1741794, 1, "17d125330d6f175e6c28c56630312084fe07a44a06bcbffc530c3f590d01667c") --Main Windows Language Depot Japanese Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741794, "1714715461487638694", 448089696)
+addappid(1741795, 1, "8b0095c3dc40776199884af8ac54987c23277a352c03f79acdbf51eea7ef6191") --Main Windows Language Depot Koreana Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741795, "180884095722922854", 2585344)
+addappid(1741796, 1, "6d55d6c6f95126badb8fc4d39fafcd9abfe6470847f8c3a81057aa9b260c6107") --Main Windows Language Depot Polish Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741796, "21487293048659539", 5015954544)
+addappid(1741797, 1, "b3c0705864ffb87b5be6890de5ecd919959de996b6a9a60ec6f84a3bea7bd768") --Main Windows Language Depot Polish Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741797, "5198374207807073799", 480562816)
+addappid(1741798, 1, "dac1cd4282c31bca5e08c68b37b3939433252e9716025c74c0eef121dcc56938") --Main Windows Language Depot Portuguese Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741798, "5773674298437918400", 2142064)
+addappid(1741799, 1, "962cef0a55b1ae29a391f43431ee4b98a4f54bb40cab9b0a047491df31d5b6c2") --Main Windows Language Depot Russian Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741799, "5972659885492754783", 4941085136)
+addappid(1741800, 1, "85cbd9ea07b0212def7701851f906cfc2015697823afa00849e0ffadacb4ee2c") --Main Windows Language Depot Russian Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741800, "7458648730821514672", 463452704)
+addappid(1741801, 1, "8d26e4e39b3d4f7beec6b01e44a9c7caf7d5e9eae6e499ef414cde2d79caa94b") --Main Windows Language Depot Turkish Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741801, "9182520194521440882", 2137744)
+addappid(1741802, 1, "0452622d330096a7e928803be80e7f859a861c522eebb67fc338824122ea90cb") --Main Windows Language Depot Arabic Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741802, "8369068266057563647", 1277424)
+addappid(1741803, 1, "921360aefff71e9ec8ab8a2ee251e4b718ff615f3183c5fe0e0080bf7f434766") --Main Windows Language Depot Tchinese Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741803, "5316067805805557199", 3671392)
+addappid(1741804, 1, "6d1931aa3987c3bf777944cd3240584ed15e169dc8ad97da57449f72730911cb") --Main Windows Language Depot Koreana Dying Light 2 Stay Human: Reloaded Edition
+setManifestid(1741804, "8188302794449075690", 952304)
+--Dlcname AppID 1263580
+addappid(1263580) --Dlcname AppID 1263580
+addappid(534386, 1, "0f124b03d74178f173d00c78c84573dc7cef743f0d0835edea668a5ed3ba116e") --Dlc Windows Depot AppID 1263580
+setManifestid(534386, "1344544806272816313", 48)
+--Dlcname Dying Light 2 Stay Human: Bloody Ties
+addappid(1537624) --Dlcname Dying Light 2 Stay Human: Bloody Ties
+addappid(1741805, 1, "7fd30a7a706af1dc2c94010cf7486ebf1d49af69f2aebe663075b6810d1dd527") --Dlc Windows Depot Dying Light 2 Stay Human: Bloody Ties
+setManifestid(1741805, "3685828062432473187", 96258928)
+--Dlcname Dying Light 2 - Developer Tools
+addappid(2217480, 1, "860118abb9d1e13ef7e22c102734eadaa2e6a742ef7c32b792ba82e1b58b8a19") --Dlcname Dying Light 2 - Developer Tools
+addappid(1263581, 1, "b7e959a2f431a626db99f0f9d3e4f9da7b5f725308214738c3f93ecc70aec314") --Dlc Windows Depot Dying Light 2 - Developer Tools
+setManifestid(1263581, "5166833660420409932", 744164912)
+setManifestid(2217480, "5444126781536642757", 0)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1537620) --Dlcname Dying Light 2 - Deluxe Pack
+addappid(1537621) --Dlcname Dying Light 2 - Ultimate pack
+addappid(1537622) --Dlcname Dying Light 2 - Artbook
+addappid(1537623) --Dlcname Dying Light 2 - Pre-order pack
+addappid(1864100) --Dlcname Dying Light 2 - Peacekeeper 01
+addappid(1864200) --Dlcname Dying Light 2 - Peacekeeper 02
+addappid(1864230) --Dlcname Dying Light 2 - Peacekeeper 03
+addappid(1864240) --Dlcname Dying Light 2 - Survivor 01
+addappid(1864250) --Dlcname Dying Light 2 - Survivor 02
+addappid(1864260) --Dlcname Dying Light 2 - Survivor 03
+addappid(1888510) --Dlcname Dying Light 2 - Soundtrack
+addappid(1937530) --Dlcname AppID 1937530
+addappid(1937531) --Dlcname AppID 1937531
+addappid(2108090) --Dlcname AppID 2108090
+addtoken(2108090, "7241572958295469293")
+addappid(2156750) --Dlcname Dying Light 2 - Dying Laugh Bundle
+addappid(2210190) --Dlcname Dying Light 2 - Nutcracker Bundle
+addappid(2269810) --Dlcname Dying Light 2 - Rais Skin Bundle
+addappid(2269820) --Dlcname Dying Light 2 - Brecken Skin Bundle
+addappid(2330980) --Dlcname Dying Light 2 - Chicken Bundle
+addappid(2370460) --Dlcname Dying Light 2 - Rahim Bundle
+addappid(2370480) --Dlcname Dying Light 2 - Gunslinger Bundle
+addappid(2370700) --Dlcname Dying Light 2 - Post-apo Bundle
+addappid(2370710) --Dlcname Dying Light 2 - Nightrunner Bundle
+addappid(2400900) --Dlcname Dying Light 2 Stay Human: The Walking Dead Bundle
+addappid(2456710) --Dlcname Dying Light 2 - Hakon Bundle
+addappid(3453200) --Dlcname Dying Light 2 Stay Human: G.R.E. Hazmat Bundle
+addappid(3818860) --Dlcname Dying Light 2: Stay Human - 2300 DLP For Summer Edition
+addappid(3833090) --Dlcname Dying Light 2 - 1500 DLP One-off Offer
+addappid(4612850) --Dlcname Dying Light 2 Stay Human - Welcome Offer: Essential Starter Kit
