1ad0fd4c1260a16cb7c22965e85de67ca1a95b59
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:04:41 +0800

    add 1771300.lua

--- 1771300.lua
+++ 1771300.lua
@@ -1,18 +1,33 @@
-addappid(1771300)
-setManifestid(228989,"3514306556860204959")
-addappid(1771302,0,"41bc41c39ce79db318e98b533bab1fe71bfd294c4e8c90f90e4f8efa5fcc2a91")
-setManifestid(1771302,"3544488985586875723")
-addappid(1771303,0,"397ab2e5c59bbb75f716c30dba3374be8af7ff49a2fc45c621e0bc23bafbd5f3")
-setManifestid(1771303,"7417037405751139280")
-addappid(1771305,0,"66d5a42e0171363eac9b79352c78401952d616880979d26967c385d2d7a88368")
-setManifestid(1771305,"4646920070999016242")
-addappid(1771306,0,"120aed0e767afa1ac85539240704d613b82c13473fcd7136d3cea555a784783e")
-setManifestid(1771306,"725648561472919950")
-addappid(1771307,0,"8d47328b0b82d9b9ba25bfb199c69ea2dd005954298bd2aff2b448321569b727")
-setManifestid(1771307,"1176397216058495539")
-addappid(1771308,0,"b457fdf0e78f3465b9ccbfed5f8ecbb85f22428ba321534ee0df4a0d08754d02")
-setManifestid(1771308,"8664781500674004426")
-addappid(1771309,0,"1f8b55f9f4ac27eac8ffa310ca46163ee5aaad0937438035274a30b78de4e81a")
-setManifestid(1771309,"7822220232979156168")
-addappid(3118101,0,"69d3aecd16754fea50cf46182e56be5d29647e571e45c1dd1ef3294b966d0ee4")
-setManifestid(3118101,"284584102760554243")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1771300.lua
+--Gamename Kingdom Come: Deliverance II
+addappid(1771300, 1, "3caed047b77bd7be8e68f818a3a6729b75371e0f67c871728c7a19de18aa6f53") --Mainappid Kingdom Come: Deliverance II
+addappid(1771302, 1, "41bc41c39ce79db318e98b533bab1fe71bfd294c4e8c90f90e4f8efa5fcc2a91") --Main Windows Depot Kingdom Come: Deliverance II
+setManifestid(1771302, "257127247679847518", 60587600)
+addappid(1771303, 1, "397ab2e5c59bbb75f716c30dba3374be8af7ff49a2fc45c621e0bc23bafbd5f3") --Main Windows Depot Kingdom Come: Deliverance II
+setManifestid(1771303, "1999516201078544527", 79429590304)
+addappid(1771305, 1, "66d5a42e0171363eac9b79352c78401952d616880979d26967c385d2d7a88368") --Main Windows Language Depot Czech Kingdom Come: Deliverance II
+setManifestid(1771305, "877277304945139034", 14157250256)
+addappid(1771306, 1, "120aed0e767afa1ac85539240704d613b82c13473fcd7136d3cea555a784783e") --Main Windows Language Depot English Kingdom Come: Deliverance II
+setManifestid(1771306, "3329997247863059841", 12518875056)
+addappid(1771307, 1, "8d47328b0b82d9b9ba25bfb199c69ea2dd005954298bd2aff2b448321569b727") --Main Windows Language Depot French Kingdom Come: Deliverance II
+setManifestid(1771307, "1779956914792969310", 10789056560)
+addappid(1771308, 1, "b457fdf0e78f3465b9ccbfed5f8ecbb85f22428ba321534ee0df4a0d08754d02") --Main Windows Language Depot German Kingdom Come: Deliverance II
+setManifestid(1771308, "7843156561372221832", 11788347216)
+addappid(1771309, 1, "1f8b55f9f4ac27eac8ffa310ca46163ee5aaad0937438035274a30b78de4e81a") --Main Windows Language Depot Japanese Kingdom Come: Deliverance II
+setManifestid(1771309, "8473341515705503999", 11188903776)
+addappid(3118101, 1, "69d3aecd16754fea50cf46182e56be5d29647e571e45c1dd1ef3294b966d0ee4") --Main Windows Language Depot Spanish Kingdom Come: Deliverance II
+setManifestid(3118101, "4032169301022912555", 11440973808)
+--Dlcname Kingdom Come: Deliverance II Artbook
+addappid(4075290, 1, "5719ab17163fb31ff02aedc90c95c0b06a0a0d66b687706cbef92a539a773b1b") --Dlcname Kingdom Come: Deliverance II Artbook
+setManifestid(4075290, "7365651604166421259", 335265472)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3118100) --Dlcname Kingdom Come: Deliverance II The Lion's Crest
+addappid(3118110) --Dlcname Kingdom Come: Deliverance II Gallant Huntsman's Kit
+addappid(3118120) --Dlcname Kingdom Come: Deliverance II Shields of Seasons Passing
+addappid(3119920) --Dlcname Kingdom Come: Deliverance II Expansion Pass
+addappid(3368600) --Dlcname Kingdom Come: Deliverance II Brushes with Death
+addappid(3368610) --Dlcname Kingdom Come: Deliverance II Legacy of the Forge
+addappid(3368620) --Dlcname Kingdom Come: Deliverance II Mysteria Ecclesiae
