2a21e388c8958b5d1457eb6dfc27b332e37ed4cf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 04:26:52 +0800

    add 2536490.lua

--- 2536490.lua
+++ 2536490.lua
@@ -1,3 +1,8 @@
-addappid(2536490)
-addappid(2536491,0,"028814ed8b11b2e4fbf0e49ea3f734242caaf1f9f27c09ff1ba80df50491d52f")
-setManifestid(2536491,"6913711619984046465")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2536490.lua
+--Gamename Cartel Simulator
+addappid(2536490) --Mainappid Cartel Simulator
+addappid(2536491, 1, "028814ed8b11b2e4fbf0e49ea3f734242caaf1f9f27c09ff1ba80df50491d52f") --Main Windows Depot Cartel Simulator
+setManifestid(2536491, "5246532992565845648", 6211074624)
+addappid(2536492, 1, "f2192a2015a98e9782f36fa83c58eddcd8291d49a9a177b72226edcd0de8ceab") --Main Macos Depot Cartel Simulator
+setManifestid(2536492, "7383326907827329773", 2899534032)
