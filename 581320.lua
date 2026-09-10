eba7d7d250b8e02cd4e650d083cce7ba537f675e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 00:22:12 +0800

    add 581320.lua

--- 581320.lua
+++ 581320.lua
@@ -1,7 +1,77 @@
-addappid(581320)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(581322,0,"1cf7a7b78cda78f21064d61cbb2ac22836872b85b7dc263a8bd13ca32e17a814")
-setManifestid(581322,"4429625405889302323")
-addappid(1575020,0,"8d80c42ad2d2240671b19cde2cd7f80e7109c5403ed0373b6d0e84999ddc808c")
-setManifestid(1575020,"4734817636402277757")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 581320.lua
+--Gamename Insurgency: Sandstorm
+addappid(581320, 1, "51f0814199b00735b3d635cacbb0ed5441072fc99db1083b2738f7e54db01b6a") --Mainappid Insurgency: Sandstorm
+addappid(581322, 1, "1cf7a7b78cda78f21064d61cbb2ac22836872b85b7dc263a8bd13ca32e17a814") --Main Windows Depot Insurgency: Sandstorm
+setManifestid(581322, "4267157659164256387", 28714522464)
+--Dlcname Insurgency: Sandstorm - High Resolution Texture Pack
+addappid(1575020, 1, "8d80c42ad2d2240671b19cde2cd7f80e7109c5403ed0373b6d0e84999ddc808c") --Dlcname Insurgency: Sandstorm - High Resolution Texture Pack
+setManifestid(1575020, "7529054639636037665", 8609254016)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(996220) --Dlcname Insurgency: Sandstorm - Bug Hunter Tattoo
+addappid(1308180) --Dlcname Insurgency: Sandstorm - Red Dark Weapon Skin Set
+addappid(1308181) --Dlcname Insurgency: Sandstorm - Midnight Blue Weapon Skin Set
+addappid(1308182) --Dlcname Insurgency: Sandstorm - Nightstalker Set
+addappid(1308183) --Dlcname Insurgency: Sandstorm - Ghillie Gear Set
+addappid(1409140) --Dlcname Insurgency: Sandstorm - Desert Hex Weapon Skin Set
+addappid(1409141) --Dlcname Insurgency: Sandstorm - Urban Digital Weapon Skin Set
+addappid(1409142) --Dlcname Insurgency: Sandstorm - Rogue Spec Ops Gear Set
+addappid(1409143) --Dlcname Insurgency: Sandstorm - Urban Warden Gear Set
+addappid(1481110) --Dlcname Insurgency: Sandstorm - Sasquatch Gear Set
+addappid(1481111) --Dlcname Insurgency: Sandstorm - Yeti Gear Set
+addappid(1481112) --Dlcname Insurgency: Sandstorm - Hunter Weapon Skin Set
+addappid(1481113) --Dlcname Insurgency: Sandstorm - Whiteout Weapon Skin Set
+addappid(1590710) --Dlcname Insurgency: Sandstorm - Upriser Gear Set
+addappid(1590711) --Dlcname Insurgency: Sandstorm - Bad Day Gear Set
+addappid(1590712) --Dlcname Insurgency: Sandstorm - True Grit Weapon Skin Set
+addappid(1590713) --Dlcname Insurgency: Sandstorm - Bear Claw Weapon Skin Set
+addappid(1711180) --Dlcname Insurgency: Sandstorm - Chrome Weapon Skin Set
+addappid(1711181) --Dlcname Insurgency: Sandstorm - Two-Tone Weapon Skin Set
+addappid(1711182) --Dlcname Insurgency: Sandstorm - PMC Gear Set
+addappid(1711183) --Dlcname Insurgency: Sandstorm - Gray Man Gear Set
+addappid(1759140) --Dlcname Insurgency: Sandstorm - The Peacemaker Gear Set
+addappid(1760280) --Dlcname Insurgency: Sandstorm - Warlord Gear Set
+addappid(1763080) --Dlcname Insurgency: Sandstorm - The Assault Pack
+addappid(1763081) --Dlcname Insurgency: Sandstorm - Carbon Fiber Weapon Skin Set
+addappid(1763082) --Dlcname Insurgency: Sandstorm - Woodland Weapon Skin Set
+addappid(1763083) --Dlcname Insurgency: Sandstorm - Brute Gear Set
+addappid(1836470) --Dlcname Insurgency: Sandstorm - S.O.R.T. Gear Set
+addappid(2062370) --Dlcname Insurgency: Sandstorm - Damascus Weapon Skin Set
+addappid(2062371) --Dlcname Insurgency: Sandstorm - Black Powder Weapon Skin Set
+addappid(2062372) --Dlcname Insurgency: Sandstorm - Dealer Gear Set
+addappid(2062373) --Dlcname Insurgency: Sandstorm - Technician Gear Set
+addappid(2062374) --Dlcname Insurgency: Sandstorm - Biker Gear Set
+addappid(2062375) --Dlcname Insurgency: Sandstorm - Pilot Gear Set
+addappid(2062376) --Dlcname Insurgency: Sandstorm - Rust & Wrap Weapon Skin Set
+addappid(2062377) --Dlcname Insurgency: Sandstorm - Worn Veteran Weapon Skin Set
+addappid(2432520) --Dlcname Insurgency: Sandstorm - Mountain Tactical Gear Set
+addappid(2432521) --Dlcname Insurgency: Sandstorm - Mountain Nomad Gear Set
+addappid(2432522) --Dlcname Insurgency: Sandstorm - Digital Splatter Weapon Skin Set
+addappid(2432523) --Dlcname Insurgency: Sandstorm - Woodburn Weapon Skin Set
+addappid(2606030) --Dlcname Insurgency: Sandstorm - Chemical Combat Gear Set
+addappid(2606040) --Dlcname Insurgency: Sandstorm - Protective Gear Set
+addappid(2606050) --Dlcname Insurgency: Sandstorm - Desert Veteran Weapon Skin Set
+addappid(2606060) --Dlcname Insurgency: Sandstorm - Dusty Weapon Skin Set
+addappid(2606070) --Dlcname Insurgency: Sandstorm - Tactical Doc Gear Set
+addappid(2606080) --Dlcname Insurgency: Sandstorm - Desert Corpsman Gear Set
+addappid(2606090) --Dlcname Insurgency: Sandstorm - Night Raven Weapon Skin Set
+addappid(2606100) --Dlcname Insurgency: Sandstorm - Duct Taped Weapon Skin Set
+addappid(2606110) --Dlcname Insurgency: Sandstorm - Phantom Gear Set
+addappid(2606120) --Dlcname Insurgency: Sandstorm - Forest Warden Gear Set
+addappid(2606130) --Dlcname Insurgency: Sandstorm - Green Recon Weapon Skin Set
+addappid(2606140) --Dlcname Insurgency: Sandstorm - Bloodwood Weapon Skin Set
+addappid(3801220) --Dlcname Insurgency: Sandstorm - Vandal Gear Set
+addappid(3801230) --Dlcname Insurgency: Sandstorm - Climber Gear Set
+addappid(3801240) --Dlcname Insurgency: Sandstorm - Dragon Skin Weapon Skin Set
+addappid(3801250) --Dlcname Insurgency: Sandstorm - Wheat Weapon Skin Set
+addappid(3951900) --Dlcname Insurgency: Sandstorm - The Jackal Gear Set
+addappid(3951910) --Dlcname Insurgency: Sandstorm - GRS Operator Gear Set
+addappid(3951920) --Dlcname Insurgency: Sandstorm - Jezzail Weapon Skin Set
+addappid(3951930) --Dlcname Insurgency: Sandstorm - Canyon Weapon Skin Set
+addappid(4210880) --Dlcname Insurgency: Sandstorm - Rapid Assault Gear Set
+addappid(4210890) --Dlcname Insurgency: Sandstorm - Scavenger Gear Set
+addappid(4210900) --Dlcname Insurgency: Sandstorm - Endurance Weapon Skin Set
