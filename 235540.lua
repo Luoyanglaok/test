29ddeae9cda079141f9479304925f1575ca3236f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:52:04 +0800

    add 235540.lua

--- 235540.lua
+++ 235540.lua
@@ -1,15 +1,48 @@
-addappid(235540)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229004,"5220958916987797232")
-addappid(235541,0,"4bbee43e456e56921a35f988865155aa4f518229000db6d56aaf98d118134d9a")
-setManifestid(235541,"7983608034794906357")
-addappid(235542,0,"f64ffed41a8b3776d43946910c5b43921553400e092da49cb38b169f8604c82f")
-setManifestid(235542,"3756960812244037923")
-addappid(235543,0,"21a1c3b6dfcf0d12bca8d530bef13721529f81d7fafef4bab743299cb84521d6")
-setManifestid(235543,"8374750442656094942")
-addappid(235544,0,"d60767d0f76f6e41506ab7002054cb69cac1a55c24a4287a412c20cd97f532c1")
-setManifestid(235544,"8753288419425204928")
-addappid(373540,0,"f2f1744decba1820bab329a30c6c3709cde101e36c1d6580ebfa26cc3f5f696b")
-setManifestid(373540,"8367289550886265290")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 235540.lua
+--Gamename Warhammer: End Times - Vermintide
+addappid(235540, 1, "5f8697be096748ebd0d6e757768d22024664bcc3d2e47f914645b580ecbd4dde") --Mainappid Warhammer: End Times - Vermintide
+addappid(235541, 1, "4bbee43e456e56921a35f988865155aa4f518229000db6d56aaf98d118134d9a") --Main Windows Depot Warhammer: End Times - Vermintide
+setManifestid(235541, "7983608034794906357", 45129474448)
+addappid(235542, 1, "f64ffed41a8b3776d43946910c5b43921553400e092da49cb38b169f8604c82f") --Main Windows Depot Warhammer: End Times - Vermintide
+setManifestid(235542, "3756960812244037923", 5470160)
+addappid(235543, 1, "21a1c3b6dfcf0d12bca8d530bef13721529f81d7fafef4bab743299cb84521d6") --Main Windows Depot Warhammer: End Times - Vermintide
+setManifestid(235543, "8374750442656094942", 10351328)
+addappid(235544, 1, "d60767d0f76f6e41506ab7002054cb69cac1a55c24a4287a412c20cd97f532c1") --Main Windows Depot Warhammer: End Times - Vermintide
+setManifestid(235544, "8753288419425204928", 89268256)
+--Dlcname Warhammer: End Times - Vermintide Collector's Edition Content
+addtoken(373540, "10753879721991375249")
+addappid(373540, 1, "f2f1744decba1820bab329a30c6c3709cde101e36c1d6580ebfa26cc3f5f696b") --Dlcname Warhammer: End Times - Vermintide Collector's Edition Content
+setManifestid(373540, "8367289550886265290", 1409229072)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(410400) --Dlcname AppID 410400
+addtoken(410400, "14500096624399745319")
+addappid(412500) --Dlcname AppID 412500
+addappid(412501) --Dlcname AppID 412501
+addtoken(412501, "10545325791837064901")
+addappid(412502) --Dlcname AppID 412502
+addappid(412503) --Dlcname AppID 412503
+addtoken(412503, "13195213357581770380")
+addappid(419070) --Dlcname Warhammer: End Times - Vermintide Sigmar's Blessing
+addappid(437070) --Dlcname Warhammer: End Times - Vermintide Schluesselschloss
+addappid(440980) --Dlcname Warhammer: End Times - Vermintide Last Stand
+addappid(453280) --Dlcname Warhammer: End Times - Vermintide Drachenfels
+addappid(463790) --Dlcname Warhammer Vermintide - Saltzpyre ' Estalian Leather Coat' Skin
+addappid(463791) --Dlcname Warhammer: End Times - Vermintide Karak Azgaraz
+addappid(463792) --Dlcname Warhammer: End Times - Vermintide Lorebook
+addappid(463794) --Dlcname Warhammer: End Times - Vermintide Quests and Contracts
+addappid(463795) --Dlcname Warhammer: End Times - Vermintide Stromdorf
+addappid(463796) --Dlcname Warhammer Vermintide - Sienna 'Wyrmscales' Skin
+addappid(463797) --Dlcname Warhammer Vermintide - Kruber 'Carroburg Livery' Skin
+addappid(463798) --Dlcname Warhammer Vermintide - Kerillian 'Tirsyth Garment' Skin
+addappid(463799) --Dlcname Warhammer Vermintide - Bardin 'Studded Leather' Skin
+addappid(569790) --Dlcname AppID 569790
+addappid(741420) --Dlcname Warhammer: End Times - Vermintide Death on the Reik
