bc67872e7855e6e9080ac7a1deae6bbe19b7dd69
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:17:45 +0800

    add 242760.lua

--- 242760.lua
+++ 242760.lua
@@ -1,3 +1,6 @@
-addappid(242760)
-addappid(242761,0,"786dfb9db41be351c888b4a930d5295dfe6c8c4bbc4e442f0e1ced301c4d24df")
-setManifestid(242761,"5529756524060062456")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 242760.lua
+--Gamename The Forest
+addappid(242760, 1, "a6f4c5b8e1c5dabd49aeaa734eddcfce5b54092657062e128f9134946efbf327") --Mainappid The Forest
+addappid(242761, 1, "786dfb9db41be351c888b4a930d5295dfe6c8c4bbc4e442f0e1ced301c4d24df") --Main Windows Language Depot English The Forest
+setManifestid(242761, "1816573600225453267", 2129681056)
