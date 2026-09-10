000cc9a427aaff564158701f07e8a7c6d1cd5608
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:50:26 +0800

    add 1022310.lua

--- 1022310.lua
+++ 1022310.lua
@@ -1,3 +1,22 @@
-addappid(1022310)
-addappid(1022311,0,"a86e5fa13916762d1764abd2dd82c7f21b8627efc9cc6973a2f1fde8b7ece843")
-setManifestid(1022311,"6105728632745217265")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1022310.lua
+--Gamename Warhammer Underworlds - Shadespire Edition (Classic)
+addappid(1022310) --Mainappid Warhammer Underworlds - Shadespire Edition (Classic)
+addappid(1022311, 1, "a86e5fa13916762d1764abd2dd82c7f21b8627efc9cc6973a2f1fde8b7ece843") --Main Windows Depot Warhammer Underworlds - Shadespire Edition (Classic)
+setManifestid(1022311, "6105728632745217265", 12273194480)
+addappid(1320850) --Dlcname AppID 1320850
+addappid(1320851) --Dlcname AppID 1320851
+addappid(1375190) --Dlcname AppID 1375190
+addappid(1421580) --Dlcname AppID 1421580
+addappid(1471160) --Dlcname AppID 1471160
+addappid(1471161) --Dlcname AppID 1471161
+addappid(1489230) --Dlcname AppID 1489230
+addappid(1489231) --Dlcname AppID 1489231
+addappid(1489232) --Dlcname AppID 1489232
+addappid(1489233) --Dlcname AppID 1489233
+addappid(1489240) --Dlcname AppID 1489240
+addappid(1574120) --Dlcname AppID 1574120
+addappid(1626390) --Dlcname Warhammer Underworlds: Online - Cosmetics: Relics of the Founders
+addappid(1639840) --Dlcname AppID 1639840
+addappid(1639841) --Dlcname AppID 1639841
+addappid(1824460) --Dlcname AppID 1824460
