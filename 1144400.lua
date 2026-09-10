1a7b99c18f7ed8c3138f58c9f0f2b17e7a6fd184
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:49:27 +0800

    add 1144400.lua

--- 1144400.lua
+++ 1144400.lua
@@ -1,5 +1,11 @@
-addappid(1144400)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(1144401,0,"0a108eb0b9a0eda8f2877f8445935437a7a9e9e62de17813cf34e0535593eb3a")
-setManifestid(1144401,"2666301483748955992")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1144400.lua
+--Gamename Senren＊Banka
+addappid(1144400, 1, "c1918578d64d425d8b3be4f029af1a804df9aa1f58f4f82cd04da438a0163443") --Mainappid Senren＊Banka
+addappid(1144401, 1, "0a108eb0b9a0eda8f2877f8445935437a7a9e9e62de17813cf34e0535593eb3a") --Main Windows Depot Senren＊Banka
+setManifestid(1144401, "2666301483748955992", 7093029936)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
