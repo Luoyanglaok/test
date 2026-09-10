8f8f85cd66d775c6880748843f6b98e4d64f19fb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:03:00 +0800

    add 2408920.lua

--- 2408920.lua
+++ 2408920.lua
@@ -1,6 +1,13 @@
-addappid(2408920)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-addappid(2408921,0,"75744141562a3fb10eda0de8ea4cf7cb2b9f3fbdac79e2813824dc13e30f296f")
-setManifestid(2408921,"3031506306953429049")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2408920.lua
+--Gamename 森林之子
+addappid(2408920) --Mainappid 森林之子
+addappid(2408921, 1, "75744141562a3fb10eda0de8ea4cf7cb2b9f3fbdac79e2813824dc13e30f296f") --Main Windows Depot 森林之子
+setManifestid(2408921, "3031506306953429049", 11693651088)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
