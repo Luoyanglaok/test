4511ff054a40e454133706595afce26152a87673
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:20:35 +0800

    add 2406770.lua

--- 2406770.lua
+++ 2406770.lua
@@ -1,5 +1,11 @@
-addappid(2406770)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2406771,0,"0456324ba6c023dafc26bedc6005e6145a7b03bfe59a6d2806bf7e07ef54e6c2")
-setManifestid(2406771,"100473726127154370")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2406770.lua
+--Gamename Bodycam
+addappid(2406770, 1, "70969ed45a39a813850ed9d81a50af9aabc5fc4ebd8fc1216ed260e04487cb50") --Mainappid Bodycam
+addappid(2406771, 1, "0456324ba6c023dafc26bedc6005e6145a7b03bfe59a6d2806bf7e07ef54e6c2") --Main Windows Depot Bodycam
+setManifestid(2406771, "1912806340035433358", 53264478080)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
