a6a966f9c4a37b945466b427502dd67308dc520f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:16:54 +0800

    add 1762930.lua

--- 1762930.lua
+++ 1762930.lua
@@ -1,5 +1,11 @@
-addappid(1762930)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1762931,0,"612ee27641d14fa8b5a84c3aa603d8f3bd28a1228ea0dc1bd92bb2339df8cef7")
-setManifestid(1762931,"1000445308206844405")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1762930.lua
+--Gamename Goat Simulator: Remastered
+addappid(1762930) --Mainappid Goat Simulator: Remastered
+addappid(1762931, 1, "612ee27641d14fa8b5a84c3aa603d8f3bd28a1228ea0dc1bd92bb2339df8cef7") --Main Windows Depot Goat Simulator: Remastered
+setManifestid(1762931, "1000445308206844405", 13253991280)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
