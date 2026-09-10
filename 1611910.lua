dcb8f93563b6e8edd390a05c660071e5bf65d0fb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:49:30 +0800

    add 1611910.lua

--- 1611910.lua
+++ 1611910.lua
@@ -1,4 +1,13 @@
-addappid(1611910)
-setManifestid(228990,"1829726630299308803")
-addappid(1611911,0,"86237e0895ee119ad5f6adfe0435ea25f199a14eb12f10f69cd84330b6d22720")
-setManifestid(1611911,"635211419555788352")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1611910.lua
+--Gamename Warhammer 40,000: Chaos Gate - Daemonhunters
+addappid(1611910, 1, "1a19c8ed4136347327549702b7aa5c58281cdbf7446948b59036dfe6766da1c7") --Mainappid Warhammer 40,000: Chaos Gate - Daemonhunters
+addappid(1611911, 1, "86237e0895ee119ad5f6adfe0435ea25f199a14eb12f10f69cd84330b6d22720") --Main Windows Depot Warhammer 40,000: Chaos Gate - Daemonhunters
+setManifestid(1611911, "7150054391983782129", 14366368384)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1804350) --Dlcname Warhammer 40,000: Chaos Gate - Daemonhunters Relics of Titan
+addappid(1883510) --Dlcname Warhammer 40,000: Chaos Gate - Daemonhunters Castellan Champion Upgrade Pack
+addappid(2126610) --Dlcname Warhammer 40,000: Chaos Gate - Daemonhunters - Duty Eternal
+addappid(2421890) --Dlcname Warhammer 40,000: Chaosgate - Daemonhunters - Execution Force
