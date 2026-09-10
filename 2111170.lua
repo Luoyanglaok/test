3bab83fe7860ea75865ddc0083604a1aa821fb51
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:57:56 +0800

    add 2111170.lua

--- 2111170.lua
+++ 2111170.lua
@@ -1,6 +1,21 @@
-addappid(2111170)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-addappid(2111171,0,"2d9d2b9615e3f11f5256bf67cffb29afba507e75ece6760e7f8e8d8c63f6b8c9")
-setManifestid(2111171,"6538834553344203744")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2111170.lua
+--Gamename STORY OF SEASONS: A Wonderful Life
+addappid(2111170) --Mainappid STORY OF SEASONS: A Wonderful Life
+addappid(2111171, 1, "2d9d2b9615e3f11f5256bf67cffb29afba507e75ece6760e7f8e8d8c63f6b8c9") --Main Windows Depot STORY OF SEASONS: A Wonderful Life
+setManifestid(2111171, "6538834553344203744", 1235348704)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(2282580) --Dlcname STORY OF SEASONS: A Wonderful Life - Spring Cow-digan
+addappid(2282581) --Dlcname STORY OF SEASONS: A Wonderful Life - Summer Raincoat
+addappid(2282582) --Dlcname STORY OF SEASONS: A Wonderful Life - Autumn Aviator Jacket
+addappid(2282583) --Dlcname STORY OF SEASONS: A Wonderful Life - Winter Peacoat
+addappid(2282584) --Dlcname STORY OF SEASONS: A Wonderful Life - Mukumuku Outfit
+addappid(2282585) --Dlcname STORY OF SEASONS: A Wonderful Life - Red Happi Coat
+addappid(2282586) --Dlcname STORY OF SEASONS: A Wonderful Life - Pumpkin Patch Costume
+addappid(2446830) --Dlcname STORY OF SEASONS: A Wonderful Life - Seasonal Outfits Set
