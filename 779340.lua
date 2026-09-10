f654611c883d88e5c50dace308fa5f58138cca64
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 04:31:58 +0800

    add 779340.lua

--- 779340.lua
+++ 779340.lua
@@ -1,19 +1,153 @@
-addappid(779340)
-addappid(228983,1,"77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b")
-addappid(228985,1,"21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c")
-addappid(228986,1,"51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5")
-addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(779341,"6805036328918787539")
-setManifestid(779342,"701257731915716276")
-setManifestid(799374,"8970394105904486266")
-setManifestid(874310,"621956972607272441")
-setManifestid(853360,"6426854821231808348")
-setManifestid(372531,"1128077100884847891")
-setManifestid(1102310,"4713270222318393168")
-setManifestid(1493250,"7425816379686603859")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 779340.lua
+--Gamename Total War: THREE KINGDOMS
+addappid(779340, 1, "3b8f8502e227a5ed9d8ffe754a6b5f35bd016e606e815e8304c7305da8da5b86") --Mainappid Total War: THREE KINGDOMS
+addappid(779341, 1, "26509b7ee264e6455f001a33acbc0d4dfcffd04bd84cecfc1d758532bda5d2d3") --Main Windows Depot Total War: THREE KINGDOMS
+setManifestid(779341, "6805036328918787539", 169102112)
+addappid(779342, 1, "3731a2e539a56985c559d353c0ebd3bc05c7f20f634eda2c31447d25594be73f") --Main Windows Depot Total War: THREE KINGDOMS
+setManifestid(779342, "701257731915716276", 37774553744)
+addappid(779343, 1, "b10d804e86fc7f87e9ed344436510dfd697c46c265b1c1bdeb5d5899d13c17f3") --Main Windows Language Depot English Total War: THREE KINGDOMS
+setManifestid(779343, "6811352873591035718", 48)
+addappid(779344, 1, "2247f386f028d849203150d827ce46008ebf938593cdaae9bf2391fa988180d1") --Main Windows Language Depot Brazilian Total War: THREE KINGDOMS
+setManifestid(779344, "6226708278104984996", 48)
+addappid(779345, 1, "e8d08b48083d125a7e60c18b1fb0d03a4293b9662b3550c8ef4b7e3c8a760692") --Main Windows Language Depot Czech Total War: THREE KINGDOMS
+setManifestid(779345, "7811988152330787045", 48)
+addappid(779346, 1, "c8295bd1d775da4202ef2fad2bd8cd67364d92fc2b10daef97dd1de5dafe7b38") --Main Windows Language Depot German Total War: THREE KINGDOMS
+setManifestid(779346, "6439895821707944426", 48)
+addappid(779347, 1, "5d6dd8b63b47efe245962529e3cccd0f34c3db40fcb168ff261d1a3564f3bd14") --Main Windows Language Depot Spanish Total War: THREE KINGDOMS
+setManifestid(779347, "7607284941095542694", 48)
+addappid(779348, 1, "b0606589d7843a2d9a92f65c18af6bae601583d17c7e336a95d307a9f0060520") --Main Windows Language Depot French Total War: THREE KINGDOMS
+setManifestid(779348, "2727002083010010970", 48)
+addappid(779349, 1, "1f72563ab744f508c6ab4214cd7bad617e110d95cbe84cf6b34beae37c807f6c") --Main Windows Language Depot Italian Total War: THREE KINGDOMS
+setManifestid(779349, "5728834785887145782", 48)
+addappid(799370, 1, "5fa211f183218141f1e2e1fb1bac8fb3fe0fb320265c638d8c05d7bf486b7c05") --Main Windows Language Depot Koreana Total War: THREE KINGDOMS
+setManifestid(799370, "3367773559547756101", 48)
+addappid(799371, 1, "8f5ffa57b5724d32c5ab7033ff08d322a58521745f8d6d0aecd8399dfbb63fda") --Main Windows Language Depot Polish Total War: THREE KINGDOMS
+setManifestid(799371, "2821369045636723031", 48)
+addappid(799372, 1, "e48978bb972fe97b75dcd90853df38ff668962b8af2b5351b7465a3a4d362035") --Main Windows Language Depot Russian Total War: THREE KINGDOMS
+setManifestid(799372, "8317842917173132408", 48)
+addappid(799373, 1, "414a04e48834fb20afbd5b1db11f20f9c7ba1686fa7ad1fedf570c2b69b741c1") --Main Windows Language Depot Turkish Total War: THREE KINGDOMS
+setManifestid(799373, "6188527233134904275", 48)
+addappid(799374, 1, "48d62c1b65bcc6466a13d0f9deaecac7392a283f04ae9df00cecee7cae1e1188") --Main Windows Language Depot Schinese Total War: THREE KINGDOMS
+setManifestid(799374, "8970394105904486266", 48)
+addappid(799375, 1, "4b5593e2674bff578d2444380da6ffd22e7025cb85c6094c4a47c180ffbdcf7b") --Main Windows Language Depot Tchinese Total War: THREE KINGDOMS
+setManifestid(799375, "3027834449937705053", 48)
+addappid(799376, 1, "4111f4a602da7defe0794657f4b1896571ff96a5eb4b643c708c9d0e5d8f879f") --Main Macos Depot Total War: THREE KINGDOMS
+setManifestid(799376, "6876137406832576760", 90944032)
+addappid(799377, 1, "1006a23a044bb8f4dfec20ffc14554d56743dabee9e4fcf0a12c25b302163723") --Main Macos Depot Total War: THREE KINGDOMS
+setManifestid(799377, "8711706353855482991", 23081642144)
+addappid(799378, 1, "5ed449c303c4fce119a8ea02be414a93726d42430808137f3e239ae4f09d7035") --Main Macos Language Depot English Total War: THREE KINGDOMS
+setManifestid(799378, "5192346604235148876", 166507904)
+addappid(799379, 1, "15d68ffbd53f51d1936c0e5bb5f23e0da29a927a82f5551f0d0953129d2d7501") --Main Macos Language Depot Brazilian Total War: THREE KINGDOMS
+setManifestid(799379, "2040901913039453251", 210353568)
+addappid(799380, 1, "d008ea1131cf513451cdd59d1f1e1e6e155998923222dca7cfd37adfc58eb77a") --Main Macos Language Depot Czech Total War: THREE KINGDOMS
+setManifestid(799380, "3896225693388319693", 210485936)
+addappid(799381, 1, "0ca5cdf926d3111e852914d36ffcde5de47fcb39e887a3fd13b914157e54d070") --Main Macos Language Depot German Total War: THREE KINGDOMS
+setManifestid(799381, "7694239069183276370", 2470566736)
+addappid(799382, 1, "855716e4edcfa7802a5623497504757a6a6bf00cac32d173b19e8b4c384b7484") --Main Macos Language Depot Spanish Total War: THREE KINGDOMS
+setManifestid(799382, "6210872980235670105", 2359688528)
+addappid(799383, 1, "50586e45dfa2fa31f45f331c5d09745e6cbb1794dc1db95642265cf3d7cdd84f") --Main Macos Language Depot French Total War: THREE KINGDOMS
+setManifestid(799383, "270722091443656251", 2241576720)
+addappid(799384, 1, "6051ea58f701d215dea87c3e594d5b2754c008a83b3a02bbff7a5cf2df52628a") --Main Macos Language Depot Italian Total War: THREE KINGDOMS
+setManifestid(799384, "7594287316373494366", 2357662576)
+addappid(799385, 1, "a23d6ed5118587f34e9c54d8a372d4f177b311ab96452ba766c8ba44d69eaacd") --Main Macos Language Depot Koreana Total War: THREE KINGDOMS
+setManifestid(799385, "8368816413155866835", 2417639120)
+addappid(799386, 1, "e2a23bd5a42795ec23d89f8f420401ff06ba28f8c19c6b6015981de42d2719cb") --Main Macos Language Depot Polish Total War: THREE KINGDOMS
+setManifestid(799386, "5142423819021804033", 2335887680)
+addappid(799387, 1, "a6719b7b9c023f1b3fc89e9531c80bf60e9f00bebe7fab60f8b25aa1e576cf81") --Main Macos Language Depot Russian Total War: THREE KINGDOMS
+setManifestid(799387, "643707233512071167", 2532921104)
+addappid(799388, 1, "1c30bf81d1af9b340806a6b89d21d378139f0db7333b5bfee8d49bc38b171d70") --Main Macos Language Depot Schinese Total War: THREE KINGDOMS
+setManifestid(799388, "3447090649279020429", 141545584)
+addappid(799389, 1, "b84abd43d50dcb08223617c2cc0ff6046c1891db4acdefdc9ec1fdb3cc1ff838") --Main Macos Language Depot Tchinese Total War: THREE KINGDOMS
+setManifestid(799389, "7818916934745391663", 142938640)
+addappid(853364, 1, "e0f867831de0234d6459f0f3cccbb983c80956576d016d5b598123e97938034a") --Main Linux Depot Total War: THREE KINGDOMS
+setManifestid(853364, "1790793131171414262", 154846528)
+addappid(853365, 1, "7a2480d5762ee065b9c7f9d2c709831b9aeacc700da7d4c1bf4b9e8abe9343c8") --Main Linux Depot Total War: THREE KINGDOMS
+setManifestid(853365, "6874476711803023460", 23081642144)
+addappid(853366, 1, "aff83922a08f0fb4f4b335f89608dfbebe23860804ddba31ca22d6837eb3daf5") --Main Linux Language Depot English Total War: THREE KINGDOMS
+setManifestid(853366, "267303604192653197", 166507904)
+addappid(853367, 1, "1d8b8be7fc1166d0b3ee87af40535e13a13b6d5ff3bd2bd1f9a3a04cc9a86c4c") --Main Linux Language Depot Brazilian Total War: THREE KINGDOMS
+setManifestid(853367, "288396738835583599", 210353568)
+addappid(853368, 1, "6efa9e2213407fc0db394edff76cf977538084a1e52af943be0da01c589bae44") --Main Linux Language Depot Czech Total War: THREE KINGDOMS
+setManifestid(853368, "4025852931841648048", 210485936)
+addappid(853369, 1, "ea618cab6daf992c9ce9c410cb1fb02973be4916f334eab984adf5e2026f7608") --Main Linux Language Depot German Total War: THREE KINGDOMS
+setManifestid(853369, "5780446731479300544", 2470566736)
+addappid(874311, 1, "91f44e61e006b85134eaa29c3a733d0a2d2a15b56b0ca7ce45735ee28e4286cc") --Main Linux Language Depot Spanish Total War: THREE KINGDOMS
+setManifestid(874311, "720808102507720586", 2359688528)
+addappid(874312, 1, "2453026962dfe78ac1168ee11cff07ab22c0bed130cea34b414dceb3408d6311") --Main Linux Language Depot French Total War: THREE KINGDOMS
+setManifestid(874312, "1911963028686984877", 2241576720)
+addappid(874313, 1, "d4a62640cc9720f0187a353aac5cc4a580563a18e54547f23c26782c9e28b931") --Main Linux Language Depot Italian Total War: THREE KINGDOMS
+setManifestid(874313, "6329876529700719423", 2357662576)
+addappid(874314, 1, "cfa8ad544751064afdcf9fb64a3e4e91b3c4d35f0e62f2b5d61cfc4005ffbf1b") --Main Linux Language Depot Koreana Total War: THREE KINGDOMS
+setManifestid(874314, "6864785333814126911", 2417639120)
+addappid(874315, 1, "7784e691f362d4b6bd3105e9b2d3945b7224237f05d0301dd489bc7a9e94b3ea") --Main Linux Language Depot Polish Total War: THREE KINGDOMS
+setManifestid(874315, "1734128328112285351", 2335887680)
+addappid(874316, 1, "237727f9764fff6a03f3c956737fc6194e759d8cd745093fbb4ac51d15dc0ffd") --Main Linux Language Depot Russian Total War: THREE KINGDOMS
+setManifestid(874316, "6084825952699408594", 2532921104)
+addappid(874317, 1, "8aee7d25bf98faf81a6aa5755895deacbd882f1f837c3244f697ccd6b143820c") --Main Linux Language Depot Schinese Total War: THREE KINGDOMS
+setManifestid(874317, "3462161394127247211", 141545584)
+addappid(874318, 1, "334de1283b0e65b6c7bd4c7530127f51dd3e0eca3c2794ec2be837a143530e88") --Main Linux Language Depot Tchinese Total War: THREE KINGDOMS
+setManifestid(874318, "4814841122602316343", 142938640)
+addappid(1030850, 1, "fffa934ed450ea325f96a6420a4ea8fd527285db221875c169609d1adf93d65a") --Main Macos Language Depot Turkish Total War: THREE KINGDOMS
+setManifestid(1030850, "7547307803489972892", 210484288)
+addappid(1030851, 1, "450c94a4ee260d29e14f3710bc9d3fe0aa34958282f72ac5403c64528d2d68d8") --Main Linux Language Depot Turkish Total War: THREE KINGDOMS
+setManifestid(1030851, "413153163459188821", 210509520)
+--Dlcname Total War: THREE KINGDOMS - Yellow Turban Rebellion
+addappid(853360, 1, "0480271ac8b34a46d79a327e1142565eea365a7a6d0125c1a34b4397714ed560") --Dlcname Total War: THREE KINGDOMS - Yellow Turban Rebellion
+setManifestid(853360, "6426854821231808348", 403698240)
+addappid(853362, 1, "83afabf2249d029d8a7ef8a1bfaf9b2fbb2e2d9a8b55293ca403d83cf3d7294e") --Dlc Macos Depot Total War: THREE KINGDOMS - Yellow Turban Rebellion
+setManifestid(853362, "2321975253441450575", 403698240)
+addappid(853363, 1, "45dccc29d9352e840ee16d9164c35f7569cc28da1ce207613c38845907fa535d") --Dlc Linux Depot Total War: THREE KINGDOMS - Yellow Turban Rebellion
+setManifestid(853363, "7552786998773460784", 403698240)
+--Dlcname Total War: THREE KINGDOMS - Reign of Blood
+addappid(874310, 1, "0c2ea8f20daa3f4f321620b660e47159163d9a4ae8e137da2fd074e508293258") --Dlcname Total War: THREE KINGDOMS - Reign of Blood
+addappid(853361, 1, "85917877a7393b8e2f7470c901557edc427d30ef231814293ff04f15af7cd26f") --Dlc Macos Depot Total War: THREE KINGDOMS - Reign of Blood
+setManifestid(853361, "1744637256336652692", 67804976)
+setManifestid(874310, "621956972607272441", 67806464)
+addappid(874319, 1, "f0c55d3bbb20ed764f9f15f2fff6b603c2cf3c278221b26a6e81ea17e2a2b598") --Dlc Linux Depot Total War: THREE KINGDOMS - Reign of Blood
+setManifestid(874319, "69339887191085045", 67804976)
+--Dlcname Total War: THREE KINGDOMS - Eight Princes
+addappid(1102310, 1, "2754f9db99e84d959972754b17523d37f5f8f77d3f74885d36a1da20266183e2") --Dlcname Total War: THREE KINGDOMS - Eight Princes
+addappid(1030852, 1, "1ca79e95330df4044583338f893e940962dc67a0080c331891f1b76f0bb72067") --Dlc Macos Depot Total War: THREE KINGDOMS - Eight Princes
+setManifestid(1030852, "289192701057883370", 354358400)
+addappid(1030853, 1, "f2759346b856508481a3da893463c1187ba4de72d1eb292a5c9027d30de860ee") --Dlc Linux Depot Total War: THREE KINGDOMS - Eight Princes
+setManifestid(1030853, "8360673456096362470", 354358400)
+setManifestid(1102310, "4713270222318393168", 354358384)
+--Dlcname Total War: THREE KINGDOMS - Fates Divided
+addtoken(1493250, "3875493348049827274")
+addappid(1493250, 1, "c9edab1317ed8640a178ce68802fc7be5d928b7a78ae5ebe879f035eedfd5eb1") --Dlcname Total War: THREE KINGDOMS - Fates Divided
+setManifestid(1493250, "7425816379686603859", 1501408)
+--Dlcname Total War: THREE KINGDOMS – The Furious Wild Original Soundtrack
+addappid(1637330) --Dlcname Total War: THREE KINGDOMS – The Furious Wild Original Soundtrack
+addappid(1637331, 1, "786118195045d416578fb71a1b78e876b024a85a1f4c19ff61e674f37154d616") --Dlc Windows Depot Total War: THREE KINGDOMS – The Furious Wild Original Soundtrack
+setManifestid(1637331, "3527068674572545898", 239719536)
+addappid(1637332, 1, "c4334e821101742d5f52bac5928f789616d332f15f4eae18995a2c0c00e82ac6") --Dlc Windows Depot Total War: THREE KINGDOMS – The Furious Wild Original Soundtrack
+setManifestid(1637332, "3641362735100326937", 84248000)
+--Dlcname Total War: THREE KINGDOMS - Original Soundtrack
+addappid(1970200) --Dlcname Total War: THREE KINGDOMS - Original Soundtrack
+addappid(1970202, 1, "f31e16d1e4c61b405b4e56aef49c2284523eb109e07117fa6f14a7307e750266") --Dlc Windows Depot Total War: THREE KINGDOMS - Original Soundtrack
+setManifestid(1970202, "7010346969812327271", 187783792)
+addappid(1970203, 1, "39f46069fff85011c27527f2d1306f230029625ca07e23b930821e09facbb521") --Dlc Windows Depot Total War: THREE KINGDOMS - Original Soundtrack
+setManifestid(1970203, "9107919310165550256", 556031264)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(372531, 1, "85fd9e7e0a0f07b17dcb14263b94e436306270f02200d7a6669de0ba70602504") --Share Windows Depot AppID 372530
+setManifestid(372531, "1128077100884847891", 57465616)
+addappid(372532, 1, "2bdc97c7a635c04ecd75e8896ba7cb9297b6acde69b9ece3b8f9ba651425ae12") --Share Windows Depot AppID 372530
+setManifestid(372532, "8058135659253913348", 0)
+addappid(1180600) --Dlcname Total War: THREE KINGDOMS - Tao Qian
+addappid(1209110) --Dlcname Total War: THREE KINGDOMS - A World Betrayed
+addappid(1244640) --Dlcname Total War: THREE KINGDOMS - White Tiger Yan
+--Missing Dlcs
+--Dlcname Total War: THREE KINGDOMS - Mandate of Heaven
+--Dlcname Total War: THREE KINGDOMS – Shi Xie
+--Dlcname Total War: THREE KINGDOMS - The Furious Wild
