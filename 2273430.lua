ec581a023185b246444e8713eee84578004d6698
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 23:05:29 +0800

    add 2273430.lua

--- 2273430.lua
+++ 2273430.lua
@@ -1,9 +1,20 @@
-addappid(2273430)
-addtoken(2273430,"16677277415782867963")
-setManifestid(228989,"3514306556860204959")
-addappid(2273431,0,"8208c2f3da1d2334a1bdfdce064948df43608a9dce229fd006defc11ec0bb5ff")
-setManifestid(2273431,"9077258417775296772")
-addappid(2273432,0,"61cf08549f627caaf2793f6324710aa2bc1e9ce5b15ddd5f91a07fe323c89cef")
-setManifestid(2273432,"7663657295629746010")
-addappid(2771130,0,"9252ee34ddef99b9842cf9b953a265b23f119aac46977475999deac055a2aa24")
-setManifestid(2771130,"8440413603119081723")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2273430.lua
+--Gamename BlazBlue Entropy Effect
+addappid(2273430, 1, "74c56cf61e5a97b6195c5ffbb90ff0e2d2fef3c6db31611a575cfca82250ef25") --Mainappid BlazBlue Entropy Effect
+addtoken(2273430, "16677277415782867963")
+addappid(2273431, 1, "8208c2f3da1d2334a1bdfdce064948df43608a9dce229fd006defc11ec0bb5ff") --Main Windows Depot BlazBlue Entropy Effect
+setManifestid(2273431, "9014643508758778659", 13973535632)
+addappid(2273432, 1, "61cf08549f627caaf2793f6324710aa2bc1e9ce5b15ddd5f91a07fe323c89cef") --Main Macos Depot BlazBlue Entropy Effect
+setManifestid(2273432, "9153299841968279599", 14090728032)
+--Dlcname BlazBlue Entropy Effect - Launch Exclusive Content
+addappid(2771130, 1, "9252ee34ddef99b9842cf9b953a265b23f119aac46977475999deac055a2aa24") --Dlcname BlazBlue Entropy Effect - Launch Exclusive Content
+setManifestid(2771130, "3519795565238480803", 959557664)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3085820) --Dlcname BlazBlue Entropy Effect - Rachel Character Pack
+addappid(3422870) --Dlcname BlazBlue Entropy Effect - Hazama Character Pack
+addappid(3681910) --Dlcname BlazBlue Entropy Effect - Bullet Character Pack
+addappid(4212180) --Dlcname BlazBlue Entropy Effect - Naoto Character Pack
+addappid(4372140) --Dlcname BlazBlue Entropy Effect [X-File] Palette Set
