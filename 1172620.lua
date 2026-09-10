e751b20702ebb1f6203e57be862968ec9f2bd567
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:49:55 +0800

    add 1172620.lua

--- 1172620.lua
+++ 1172620.lua
@@ -1,5 +1,41 @@
-addappid(1172620)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-addappid(1172621,0,"d0a7c81c3c4ec69d5646531c500f6f8e46c7a42a74c6baf68a210c8db2f82117")
-setManifestid(1172621,"7757046792856570309")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1172620.lua
+--Gamename Sea of Thieves: 2026 Edition
+addappid(1172620, 1, "5826335db117775ac35debfae05a7afeff3331ad99059fa57939358a06fa0f6c") --Mainappid Sea of Thieves: 2026 Edition
+addappid(1172621, 1, "d0a7c81c3c4ec69d5646531c500f6f8e46c7a42a74c6baf68a210c8db2f82117") --Main Windows Depot Sea of Thieves: 2026 Edition
+setManifestid(1172621, "790542234133104130", 144182473504)
+--Dlcname AppID 2309050
+addappid(2309050, 1, "e496bd343a45a22ca0148b587298f7701885f32f944ce4217de1c0ba4fb0f786") --Dlcname AppID 2309050
+--Dlcname Sea of Thieves: Athena's Fortune Audiobook
+addappid(2838630) --Dlcname Sea of Thieves: Athena's Fortune Audiobook
+addappid(2838631, 1, "398266b3450812ecd97e3f428e6546c939c5ebda28899a391cafa3ff2311f2cf") --Dlc Windows Depot Sea of Thieves: Athena's Fortune Audiobook
+setManifestid(2838631, "739321582875573975", 1068621200)
+addappid(2838632, 1, "bf2d7c38224c5b155dce8a0857ecc24110e9afe71c7f4205e51330b8f99373a5") --Dlc Windows Depot Sea of Thieves: Athena's Fortune Audiobook
+setManifestid(2838632, "8106968498466588924", 5130352352)
+--Dlcname The Rough Guide to Sea of Thieves eBook
+addappid(2838640, 1, "68937ad2c3fcbdd9f020ab2a0bf862931c990fafd559f2fd67a362a76f4569ec") --Dlcname The Rough Guide to Sea of Thieves eBook
+setManifestid(2838640, "502403447961499835", 13183168)
+--Dlcname Sea of Thieves Original Soundtrack – 2026 Edition
+addappid(4373290) --Dlcname Sea of Thieves Original Soundtrack – 2026 Edition
+addappid(4373291, 1, "c32296fcec362f75df2b240ea1c5d26fb683f93c5668c5c8120387361065763c") --Dlc Windows Depot Sea of Thieves Original Soundtrack – 2026 Edition
+setManifestid(4373291, "8620163104098356392", 183080352)
+addappid(4373292, 1, "3a4cb6b30796b03b7589a53c8e577bc23cfa15094e36c392cc59f6b41f4b07d8") --Dlc Windows Depot Sea of Thieves Original Soundtrack – 2026 Edition
+setManifestid(4373292, "2028579537499628571", 2091518256)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(1453980) --Dlcname Sea of Thieves - Spooky Bonus Pack
+addappid(1467220) --Dlcname Sea of Thieves - Black Friday Special Edition
+addappid(1594030) --Dlcname AppID 1594030
+addappid(1781320) --Dlcname AppID 1781320
+addappid(1928780) --Dlcname AppID 1928780
+addappid(2307680) --Dlcname Sea of Thieves - Deluxe Edition Pack
+addappid(2838650) --Dlcname AppID 2838650
+addappid(2838670) --Dlcname AppID 2838670
+addappid(3444920) --Dlcname Sea of Thieves: 2025 Premium Bundle
+addappid(3444930) --Dlcname Sea of Thieves: 2025 Deluxe Bundle
+addappid(3937210) --Dlcname Sea of Thieves - Embody Audio
+addappid(4373360) --Dlcname Sea of Thieves: 2026 Deluxe Bundle
+addappid(4373370) --Dlcname Sea of Thieves: 2026 Premium Bundle
