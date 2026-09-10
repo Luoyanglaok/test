15476c5df731f2cde9d18dc7473c09f213a62fa7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:45:49 +0800

    add 1607130.lua

--- 1607130.lua
+++ 1607130.lua
@@ -1,5 +1,9 @@
-addappid(1607130)
-addappid(1607131,0,"34a2e19836c47d58fea934474d66310da4b8c9cbaddee722d66827c7e1997e58")
-setManifestid(1607131,"2278182849236490751")
-addappid(2177670,0,"26933dd3e70666a27b8ed940ac1e940807d9189b896990bceaa4c0a2a18edb82")
-setManifestid(2177670,"3776234604314363349")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1607130.lua
+--Gamename Lust Theory - Season 1
+addappid(1607130, 1, "625cc77fbcb36a1c6e6de3337d9ba9b260f585376fa88e754b0e0358e6382e83") --Mainappid Lust Theory - Season 1
+addappid(1607131, 1, "34a2e19836c47d58fea934474d66310da4b8c9cbaddee722d66827c7e1997e58") --Main Windows Language Depot English Lust Theory - Season 1
+setManifestid(1607131, "592953502344588732", 5849623616)
+--Dlcname Lust Theory High Quality 4K Wallpapers
+addappid(2177670, 1, "26933dd3e70666a27b8ed940ac1e940807d9189b896990bceaa4c0a2a18edb82") --Dlcname Lust Theory High Quality 4K Wallpapers
+setManifestid(2177670, "3776234604314363349", 711409600)
