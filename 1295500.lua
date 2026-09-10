10815d288fa1ab0b042476dfb3889faa0c6cdce0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:49:26 +0800

    add 1295500.lua

--- 1295500.lua
+++ 1295500.lua
@@ -1,6 +1,22 @@
-addappid(1295500)
-setManifestid(229006,"1784011429307107530")
-addappid(1295501,0,"d12f0bcfa736438c73d5829cd28d41cd9bbc7be4f401af778ec81c0cf9d16ae9")
-setManifestid(1295501,"3538030925073934310")
-addappid(1295502,0,"e4bca82705ce54ce78394160a3222f708214c7dbd198a07b320f63f6d9dce0aa")
-setManifestid(1295502,"5209198779971351930")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1295500.lua
+--Gamename Warhammer 40,000: Battlesector
+addappid(1295500, 1, "0baf20c17fef23bd2e9a7039fbaf4fb089fb0907e47c37ba1ebba0cdb8abfdf9") --Mainappid Warhammer 40,000: Battlesector
+addappid(1295501, 1, "d12f0bcfa736438c73d5829cd28d41cd9bbc7be4f401af778ec81c0cf9d16ae9") --Main Windows Depot Warhammer 40,000: Battlesector
+setManifestid(1295501, "3211032245227978257", 13112747120)
+addappid(1295502, 1, "e4bca82705ce54ce78394160a3222f708214c7dbd198a07b320f63f6d9dce0aa") --Main Windows Depot Warhammer 40,000: Battlesector
+setManifestid(1295502, "6356118931423404886", 48911760)
+--Share Depots
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(1718640) --Dlcname Warhammer 40,000: Battlesector - Blood Angels Elites Pack
+addappid(1793050) --Dlcname Warhammer 40,000: Battlesector - Tyranid Elites Pack
+addappid(1793051) --Dlcname Warhammer 40,000: Battlesector - Necrons Faction Pack
+addappid(2011980) --Dlcname Warhammer 40,000: Battlesector - Sisters of Battle
+addappid(2199370) --Dlcname Warhammer 40,000: Battlesector - Daemons of Khorne
+addappid(2305830) --Dlcname Warhammer 40,000: Battlesector - Orks
+addappid(2583910) --Dlcname Warhammer 40,000: Battlesector - T'au
+addappid(3202720) --Dlcname Warhammer 40,000: Battlesector - Astra Militarum
+addappid(3577600) --Dlcname Warhammer 40,000: Battlesector - Deeds of the Fallen
+addappid(3577620) --Dlcname Warhammer 40,000: Battlesector - Black Legion
+addappid(3577690) --Dlcname Warhammer 40,000: Battlesector - Ultramarines
