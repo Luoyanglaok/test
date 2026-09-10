23a46780c50065947a63a2fe4c4d69494dcf4e31
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:19:12 +0800

    add 1113000.lua

--- 1113000.lua
+++ 1113000.lua
@@ -1,7 +1,13 @@
-addappid(1113000)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(1113001,0,"0f306d31499478d6f1a6971240693c21891f3887103f5f2c92766c20a91cc0ec")
-setManifestid(1113001,"4737529339202475845")
-addappid(1113002,0,"b10005d38f9cc7dd78f58cb736b8b8f2c9401087b8ac0a5386737bae70fe3a46")
-setManifestid(1113002,"2520901896865483548")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1113000.lua
+--Gamename Persona 4 Golden
+addappid(1113000, 1, "d4a76dbb43089ce85ba57e6da0a0a7559044598746f7a0d134ad71f79cc77a1b") --Mainappid Persona 4 Golden
+addappid(1113001, 1, "0f306d31499478d6f1a6971240693c21891f3887103f5f2c92766c20a91cc0ec") --Main Windows Depot Persona 4 Golden
+setManifestid(1113001, "4737529339202475845", 8799850176)
+addappid(1113002, 1, "b10005d38f9cc7dd78f58cb736b8b8f2c9401087b8ac0a5386737bae70fe3a46") --Main Windows Depot Persona 4 Golden
+setManifestid(1113002, "2520901896865483548", 94147712)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
