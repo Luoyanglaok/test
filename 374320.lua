5597a64c50aa8371ec8680ceb4f612543c2c35ef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:10:57 +0800

    add 374320.lua

--- 374320.lua
+++ 374320.lua
@@ -1,11 +1,24 @@
-addappid(374320)
-addappid(442010)
-addappid(506970)
-addappid(506971)
-addappid(228984,1,"df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(374321,"6652575487689169560")
-setManifestid(506970,"2441039608905796121")
-setManifestid(506971,"266039425715139370")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 374320.lua
+--Gamename DARK SOULS™ III
+addappid(374320, 1, "e5f7bec9ae6e7f8ad0a875a225ae10055236b0aebad8ca7cf395f8aa7e8102b2") --Mainappid DARK SOULS™ III
+addappid(374321, 1, "1e1f904451f44a8549008ac8152430dc18288dd22c8edda058afa86b926a4162") --Main Windows Depot DARK SOULS™ III
+setManifestid(374321, "6652575487689169560", 20757267040)
+addappid(374322, 1, "2e8b33176e957bf7c6ed4ad3c34847d1eef1ee846e900e1b4a2b4fc3a51b6c09") --Main Windows Language Depot Japanese DARK SOULS™ III
+setManifestid(374322, "6130303443397132373", 644208448)
+--Dlcname DARK SOULS™ III - Original Soundtrack
+addappid(455380, 1, "ee1942ee70f7707eb40415a87cd933fedf4de04aeab6f69421e106ff4b8d3228") --Dlcname DARK SOULS™ III - Original Soundtrack
+setManifestid(455380, "398045694410355072", 99805488)
+--Dlcname DARK SOULS™ III - Ashes of Ariandel
+addappid(506970, 1, "2dc303f9e1fc1f45f385b5d262b3b481ed10f11d38e5ca44edf7096a4f280385") --Dlcname DARK SOULS™ III - Ashes of Ariandel
+setManifestid(506970, "2441039608905796121", 1627146864)
+--Dlcname DARK SOULS™ III - The Ringed City
+addappid(506971, 1, "0ce07245c46df4c8b1c5d7bef627f59f34b9fde6ea903ec21ea879cf6d9c10d5") --Dlcname DARK SOULS™ III - The Ringed City
+setManifestid(506971, "266039425715139370", 3074862160)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(442010) --Dlcname DARK SOULS™ III - Season Pass
+addappid(435470) --Dlcname AppID 435470
