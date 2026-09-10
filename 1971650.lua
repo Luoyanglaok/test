45c550403ef610561caf3628ba57bdeb687f873d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:12:50 +0800

    add 1971650.lua

--- 1971650.lua
+++ 1971650.lua
@@ -1,5 +1,12 @@
-addappid(1971650)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1971651,0,"b27023dd7c9a1894c98a3a8a507f86f2e56bf2f7e611addb95eba15ea538ff5a")
-setManifestid(1971651,"1880363709147535524")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1971650.lua
+--Gamename OCTOPATH TRAVELER II
+addappid(1971650) --Mainappid OCTOPATH TRAVELER II
+addappid(1971651, 1, "b27023dd7c9a1894c98a3a8a507f86f2e56bf2f7e611addb95eba15ea538ff5a") --Main Windows Depot OCTOPATH TRAVELER II
+setManifestid(1971651, "1880363709147535524", 7374720416)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2133280) --Dlcname Travel Provisions
