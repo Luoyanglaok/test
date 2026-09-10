6441f6122747686680a6a83744c3ed5a20003660
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:39:19 +0800

    add 2537590.lua

--- 2537590.lua
+++ 2537590.lua
@@ -1,4 +1,20 @@
-addappid(2537590)
-setManifestid(228989,"3514306556860204959")
-addappid(2537591,0,"a149759cc259406fd0f2255297e089d9deb63fb59c263b7f2d1b7fcd7aded1a4")
-setManifestid(2537591,"4129732293635838112")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2537590.lua
+--Gamename Microsoft Flight Simulator 2024
+addappid(2537590, 1, "f264fcd2580ab71934f29046dc415c3a83163882e768246533ed1f20f1730835") --Mainappid Microsoft Flight Simulator 2024
+addappid(2537591, 1, "a149759cc259406fd0f2255297e089d9deb63fb59c263b7f2d1b7fcd7aded1a4") --Main Windows Depot Microsoft Flight Simulator 2024
+setManifestid(2537591, "4036856429109772592", 6620691680)
+--Dlcname Microsoft Flight Simulator 2024 - Premium Deluxe Upgrade
+addappid(3341960, 1, "e8a61a15c5d03ec83b72cc98dd7f7c6d620961eb241c5ec55987a94ac759fd7c") --Dlcname Microsoft Flight Simulator 2024 - Premium Deluxe Upgrade
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3227340) --Dlcname Limitless - Preorder Bonus
+addappid(3245690) --Dlcname Microsoft Flight Simulator 2024 - Aviator
+addappid(3245700) --Dlcname Microsoft Flight Simulator 2024 Deluxe
+addappid(3245710) --Dlcname Microsoft Flight Simulator 2024 - Premium Deluxe
+addappid(5068380) --Dlcname Microsoft Flight Simulator 2024 Local Legends Pack 1
+addappid(5068390) --Dlcname Microsoft Flight Simulator 2024 Local Legends Pack 2
+addappid(5068400) --Dlcname Microsoft Flight Simulator 2024 Famous Flyers Pack 3
+--Missing Dlcs
+--Dlcname Microsoft Flight Simulator Suite: Themes Reimagined
