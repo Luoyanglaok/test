06d5282fbd30151b4242594172d278fb4e1193a0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 23:24:49 +0800

    add 3116700.lua

---
+++ 3116700.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3116700.lua
+--Gamename Reap and Rush
+addappid(3116700) --Mainappid Reap and Rush
+addappid(3116701, 1, "59e214b6551875afe40dc6b443eb79edd0708241d372252991ec7c145cd584b8") --Main Windows Depot Reap and Rush
+setManifestid(3116701, "3662292737085861777", 2803945888)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4942620) --Dlcname 地府有点忙 - 开发者支持包
