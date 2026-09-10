88b7c812b0625a9a09cbe7b83f9ec615742194b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:37:39 +0800

    add 2593900.lua

--- 2593900.lua
+++ 2593900.lua
@@ -1,3 +1,6 @@
-addappid(2593900)
-addappid(2593901,0,"593686724a16591b656424bffd25e371cc7a324b1ae8aa953357b045573888e5")
-setManifestid(2593901,"4652944724218541661")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2593900.lua
+--Gamename [Chilla's Art] The Kidnap | 誘拐事件
+addappid(2593900) --Mainappid [Chilla's Art] The Kidnap | 誘拐事件
+addappid(2593901, 1, "593686724a16591b656424bffd25e371cc7a324b1ae8aa953357b045573888e5") --Main Windows Depot [Chilla's Art] The Kidnap | 誘拐事件
+setManifestid(2593901, "4652944724218541661", 2383512160)
