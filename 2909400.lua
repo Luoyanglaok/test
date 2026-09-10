dfb9499b402b5a0e3b6cf30a44e98ce4cbc052b9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 23:24:10 +0800

    add 2909400.lua

--- 2909400.lua
+++ 2909400.lua
@@ -1,15 +1,34 @@
-addappid(2909400)
-addappid(3361450)
-addappid(3399450)
-addappid(4270160)
-addappid(4270120)
-addappid(4270130)
-addappid(4270150)
-addappid(2909401,1,"7ab31662f5cf76b4197f27c340a2a0589dc4713c137aadfd22b281df42c07a56")
-addappid(3260630,1,"39c8633805b12cb219e18d1665139d9ff5dd2a8ad30517ce3b29f1f935641f61")
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(2909401,"6351449259010459507")
-setManifestid(3260630,"2312058563762645934")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2909400.lua
+--Gamename FINAL FANTASY VII REBIRTH
+addappid(2909400, 1, "cfbd29b2bfe9c0615e5d8fcf42c056ceed75322c90337e4478054d1cee3555ad") --Mainappid FINAL FANTASY VII REBIRTH
+addappid(2909401, 1, "7ab31662f5cf76b4197f27c340a2a0589dc4713c137aadfd22b281df42c07a56") --Main Windows Depot FINAL FANTASY VII REBIRTH
+setManifestid(2909401, "73573156178669865", 156751034928)
+addappid(2909402, 1, "fd15686ace81b2e7a839997e1bcc40d7dd9c15f49ce21714252771aa357fd67f") --Main Windows Depot FINAL FANTASY VII REBIRTH
+setManifestid(2909402, "5084108683872765851", 22069031744)
+--Dlcname FINAL FANTASY VII REBIRTH Digital Mini Soundtrack & Digital Art Book
+addappid(3260630, 1, "39c8633805b12cb219e18d1665139d9ff5dd2a8ad30517ce3b29f1f935641f61") --Dlcname FINAL FANTASY VII REBIRTH Digital Mini Soundtrack & Digital Art Book
+setManifestid(3260630, "2312058563762645934", 96)
+--Dlcname FINAL FANTASY VII REBIRTH Original Soundtrack
+addappid(3399450) --Dlcname FINAL FANTASY VII REBIRTH Original Soundtrack
+addappid(3399451, 1, "77fc07cf815274bb5488f717bef898b48d14d3144441905bdccdfd0b98c7642c") --Dlc Windows Depot FINAL FANTASY VII REBIRTH Original Soundtrack
+setManifestid(3399451, "8141321127507237435", 1195580768)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3260650) --Dlcname FINAL FANTASY VII REBIRTH Summon materia: Moogle trio
+addappid(3260660) --Dlcname FINAL FANTASY VII REBIRTH Summon materia: Magic Pot
+addappid(3260670) --Dlcname FINAL FANTASY VII REBIRTH Accessory: Reclaimant Choker
+addappid(3260680) --Dlcname FINAL FANTASY VII REBIRTH Armor: Orchid Bracelet
+addappid(3312260) --Dlcname FINAL FANTASY VII REBIRTH Posh Chocobo Summoning Materia
+addappid(3312270) --Dlcname FINAL FANTASY VII REBIRTH Armor: Shinra Bangle Mk. II
+addappid(3312280) --Dlcname FINAL FANTASY VII REBIRTH Armor: Midgar Bangle Mk. II
+addappid(3361450) --Dlcname FINAL FANTASY VII REBIRTH Digital Deluxe Edition Upgrade
+addappid(3376810) --Dlcname FINAL FANTASY VII REBIRTH Accessory: Kupo Charm & Survival Set
+--Missing Dlcs
+--Dlcname FINAL FANTASY VII REBIRTH Orchestral Arrangement Album
+--Dlcname FINAL FANTASY VII REBIRTH Orchestra World Tour LIVE 2024.09.08
+--Dlcname FINAL FANTASY VII REBIRTH ACOUSTIC ARRANGEMENTS
+--Dlcname FINAL FANTASY VII REBIRTH Original Soundtrack Plus
