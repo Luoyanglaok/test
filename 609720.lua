cdecdb2ced62c0f839376d0532dd9dc0a623010f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:07:30 +0800

    add 609720.lua

---
+++ 609720.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 609720.lua
+--Gamename Tyr: Chains of Valhalla
+addappid(609720) --Mainappid Tyr: Chains of Valhalla
+addappid(609722, 1, "258c67e9cf59ec244460dcf8d08697fd12a04ded87ecc80447a62ad14ede2819") --Main Windows Depot Tyr: Chains of Valhalla
+setManifestid(609722, "5202140755325612542", 482706864)
+--Missing Dlcs
+--Dlcname Tyr: Chains of Valhalla OST
