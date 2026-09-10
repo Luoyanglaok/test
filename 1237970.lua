f97c12d2a65a5ad99c8e828c23beb62742e360b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:35:02 +0800

    add 1237970.lua

--- 1237970.lua
+++ 1237970.lua
@@ -1,30 +1,101 @@
-addappid(1237970)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(1237971,0,"22aeb051a8e530542cbb99bd7c78cbd283bfa6f65c010a98efb20a97bc86aa16")
-setManifestid(1237971,"2801113779391264796")
-addappid(1237972,0,"1d9462a7d7c94ffa925137129e0d07155cec5786d601ecc5e9dd46b96e9dcf4f")
-setManifestid(1237972,"5076953189658140889")
-addappid(1237973,0,"a6695112317ed9e5f7e6787f0a5993fb27cc5b814533bc7e1fe03fbc5a233cc8")
-setManifestid(1237973,"7846189424213860451")
-addappid(1237974,0,"700b101f5341fc3838393cfa779eea0c102b5f8475a3d291e404edab244da2e8")
-setManifestid(1237974,"4918993176297451540")
-addappid(1237975,0,"16820ddfb43fcb0b1216a8aaba999f8f81babbba13583daff40de1706e172fc4")
-setManifestid(1237975,"6149384324018133109")
-addappid(1237976,0,"ef53756e02e0bc170d3f3ebaa729f6e196e2a75a4aec7ab88abdae33402bfd35")
-setManifestid(1237976,"6093231552820258157")
-addappid(1237977,0,"386502850c1881ca4598319b7de7cfc05380fa06d0ba4b94584cd2bee9143e21")
-setManifestid(1237977,"666533038113272042")
-addappid(1237978,0,"af4f8e6b536c27584c137b2e000bc626ecc02baa9d8c90bbaa31d678390bb192")
-setManifestid(1237978,"1147887117574715547")
-addappid(1237979,0,"2196528e90e15287581f7e1ec7a6fa7e0f2aa8c4662536795ea606641abb83db")
-setManifestid(1237979,"4200522030241028353")
-addappid(1238106,0,"3dd6726943280308ae1f43edfb95d9cec6def09c0792791f8c7791701cdc1e67")
-setManifestid(1238106,"6657923150700234666")
-addappid(1238107,0,"3a9aed574fd2b18d063b9dff4cad6f8c81e83abc4406613a99445d46fa3a57fd")
-setManifestid(1238107,"5340648545387345990")
-addappid(1238108,0,"700772775e8368324e2801763da29fd3a6fda344d3d2c1132c5a452ee41aa563")
-setManifestid(1238108,"5218246798794462392")
-addappid(3340991,0,"023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491")
-setManifestid(3340991,"1318423300601359782")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1237970.lua
+--Gamename Titanfall® 2
+addappid(1237970, 1, "122d4c732e5a62474555aaa81e115e257281117ab3cfee68ff8d5182f685ece7") --Mainappid Titanfall® 2
+addappid(1237971, 1, "22aeb051a8e530542cbb99bd7c78cbd283bfa6f65c010a98efb20a97bc86aa16") --Main Windows Depot Titanfall® 2
+setManifestid(1237971, "2801113779391264796", 31644383184)
+addappid(1237972, 1, "1d9462a7d7c94ffa925137129e0d07155cec5786d601ecc5e9dd46b96e9dcf4f") --Main Windows Language Depot English Titanfall® 2
+setManifestid(1237972, "5076953189658140889", 724186400)
+addappid(1237973, 1, "a6695112317ed9e5f7e6787f0a5993fb27cc5b814533bc7e1fe03fbc5a233cc8") --Main Windows Language Depot French Titanfall® 2
+setManifestid(1237973, "7846189424213860451", 652405776)
+addappid(1237974, 1, "700b101f5341fc3838393cfa779eea0c102b5f8475a3d291e404edab244da2e8") --Main Windows Language Depot German Titanfall® 2
+setManifestid(1237974, "4918993176297451540", 654718928)
+addappid(1237975, 1, "16820ddfb43fcb0b1216a8aaba999f8f81babbba13583daff40de1706e172fc4") --Main Windows Language Depot Italian Titanfall® 2
+setManifestid(1237975, "6149384324018133109", 670206672)
+addappid(1237976, 1, "ef53756e02e0bc170d3f3ebaa729f6e196e2a75a4aec7ab88abdae33402bfd35") --Main Windows Language Depot Spanish Titanfall® 2
+setManifestid(1237976, "6093231552820258157", 688924448)
+addappid(1237977, 1, "386502850c1881ca4598319b7de7cfc05380fa06d0ba4b94584cd2bee9143e21") --Main Windows Language Depot Polish Titanfall® 2
+setManifestid(1237977, "666533038113272042", 658989536)
+addappid(1237978, 1, "af4f8e6b536c27584c137b2e000bc626ecc02baa9d8c90bbaa31d678390bb192") --Main Windows Language Depot Russian Titanfall® 2
+setManifestid(1237978, "1147887117574715547", 686877328)
+addappid(1237979, 1, "2196528e90e15287581f7e1ec7a6fa7e0f2aa8c4662536795ea606641abb83db") --Main Windows Language Depot Latam Titanfall® 2
+setManifestid(1237979, "4200522030241028353", 688457808)
+addappid(1238106, 1, "3dd6726943280308ae1f43edfb95d9cec6def09c0792791f8c7791701cdc1e67") --Main Windows Language Depot Japanese Titanfall® 2
+setManifestid(1238106, "6657923150700234666", 641006752)
+addappid(1238107, 1, "3a9aed574fd2b18d063b9dff4cad6f8c81e83abc4406613a99445d46fa3a57fd") --Main Windows Language Depot Brazilian Titanfall® 2
+setManifestid(1238107, "5340648545387345990", 670817456)
+addappid(1238108, 1, "700772775e8368324e2801763da29fd3a6fda344d3d2c1132c5a452ee41aa563") --Main Windows Language Depot Tchinese Titanfall® 2
+setManifestid(1238108, "5218246798794462392", 617040960)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") --Share Windows Depot EA APP Installer
+setManifestid(3340991, "5874704072306837744", 246482576)
+addappid(1238100) --Dlcname Titanfall® 2: Monarch's Reign Bundle
+addappid(1238101) --Dlcname Titanfall® 2: Jump Start Pack
+addappid(1238102) --Dlcname Titanfall® 2: Colony Reborn Bundle
+addappid(1238103) --Dlcname Titanfall® 2: Prime Titan Bundle
+addappid(1238104) --Dlcname Titanfall® 2: Angel City's Most Wanted Bundle
+addappid(1238105) --Dlcname Titanfall® 2 Nitro Scorch Pack
+addappid(1288681) --Dlcname Titanfall™ 2 Deluxe Edition - Key
+addappid(1288682) --Dlcname Titanfall™ 2: Ultimate Edition - Key
+addappid(1288683) --Dlcname Titanfall™ 2: Underground R-201 Carbine
+addappid(1288684) --Dlcname Titanfall™ 2 Deluxe Edition Content
+addappid(1288685) --Dlcname Titanfall™ 2: Jump Start Pack - UE
+addappid(1304960) --Dlcname Titanfall™ 2: Colony Reborn Tone Art Pack
+addappid(1305010) --Dlcname Titanfall™ 2: Crimson Fury CAR
+addappid(1315800) --Dlcname Titanfall™ 2: Ion Prime
+addappid(1315801) --Dlcname Titanfall™ 2: Scorch Prime
+addappid(1315802) --Dlcname Titanfall™ 2: Ion Art Pack 1
+addappid(1315803) --Dlcname Titanfall™ 2: Tone Art Pack 1
+addappid(1315804) --Dlcname Titanfall™ 2: Scorch Art Pack 1
+addappid(1315805) --Dlcname Titanfall™ 2: Legion Art Pack 1
+addappid(1315806) --Dlcname Titanfall™ 2: Northstar Art Pack 1
+addappid(1315807) --Dlcname Titanfall™ 2: Ronin Art Pack 1
+addappid(1315808) --Dlcname Titanfall™ 2: Angel City Callsign Pack
+addappid(1315809) --Dlcname Titanfall™ 2: Angel City Camo Pack
+addappid(1315870) --Dlcname Titanfall™ 2: Legion Prime
+addappid(1315871) --Dlcname Titanfall™ 2: Northstar Prime
+addappid(1315872) --Dlcname Titanfall™ 2: Colony Reborn Ion Art Pack
+addappid(1315873) --Dlcname Titanfall™ 2: Colony Reborn Scorch Art Pack
+addappid(1315874) --Dlcname Titanfall™ 2: Colony Reborn Legion Art Pack
+addappid(1315875) --Dlcname Titanfall™ 2: Colony Reborn Northstar Art Pack
+addappid(1315876) --Dlcname Titanfall™ 2: Colony Reborn Ronin Art Pack
+addappid(1315877) --Dlcname Titanfall™ 2: Colony Reborn Callsign Pack
+addappid(1315878) --Dlcname Titanfall™ 2: Colony Reborn Camo Pack
+addappid(1316020) --Dlcname Titanfall™ 2: Tone Prime
+addappid(1316021) --Dlcname Titanfall™ 2: Ronin Prime
+addappid(1316022) --Dlcname Titanfall™ 2: Monarch's Reign Ion Art Pack
+addappid(1316023) --Dlcname Titanfall™ 2: Monarch's Reign Tone Art Pack
+addappid(1316024) --Dlcname Titanfall™ 2: Monarch's Reign Scorch Art Pack
+addappid(1316025) --Dlcname Titanfall™ 2: Monarch's Reign Legion Art Pack
+addappid(1316026) --Dlcname Titanfall™ 2: Monarch's Reign Northstar Art Pack
+addappid(1316027) --Dlcname Titanfall™ 2: Monarch's Reign Ronin Art Pack
+addappid(1316028) --Dlcname Titanfall™ 2: Monarch's Reign Callsign Pack
+addappid(1316029) --Dlcname Titanfall™ 2: Monarch's Reign Camo Pack
+addappid(1316070) --Dlcname Titanfall™ 2: Masterwork G2A5
+addappid(1316071) --Dlcname Titanfall™ 2: Blue Fade Flatline
+addappid(1316072) --Dlcname Titanfall™ 2: Frontier Patriot Alternator
+addappid(1316073) --Dlcname Titanfall™ 2: Badlands EVA-8 Auto
+addappid(1316074) --Dlcname Titanfall™ 2: Aqua Fade B3 Wingman
+addappid(1316075) --Dlcname Titanfall™ 2: Phantom Archer
+addappid(1316076) --Dlcname Titanfall™ 2: Mochi Hemlok BF-R
+addappid(1316140) --Dlcname Titanfall™ 2: Purple Fade R-97
+addappid(1316141) --Dlcname Titanfall™ 2: Masterwork Kraber-AP Sniper
+addappid(1316142) --Dlcname Titanfall™ 2: Crimson Fury SA-3 Mozambique
+addappid(1316143) --Dlcname Titanfall™ 2: Lead Farmer Spitfire
+addappid(1316144) --Dlcname Titanfall™ 2: RSPN Customs X-55 Devotion
+addappid(1316145) --Dlcname Titanfall™ 2: 8-Bit LG-97 Thunderbolt
+addappid(1316146) --Dlcname Titanfall™ 2: Frontier Patriot R-201 Carbine
+addappid(1316240) --Dlcname Titanfall™ 2: Crimson Fury Mastiff
+addappid(1316241) --Dlcname Titanfall™ 2: Masterwork Sidewinder SMR
+addappid(1316242) --Dlcname Titanfall™ 2: Heat Sink L-STAR
+addappid(1316260) --Dlcname Titanfall™ 2: Badlands Flatline
+addappid(1316261) --Dlcname Titanfall™ 2: Heat Sink Volt
+addappid(1316262) --Dlcname Titanfall™ 2: MRVN EPG
+addappid(1316263) --Dlcname Titanfall™ 2: Headhunter Alternator
+addappid(1316264) --Dlcname Titanfall™ 2: Masterwork R-6P Softball
