3e3dd6ce2dd0e12d118ceddc3d21b9853d6018b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:46:15 +0800

    add 495420.lua

--- 495420.lua
+++ 495420.lua
@@ -1,11 +1,13 @@
-addappid(495420)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(495422,0,"82873486e64ba05260f583a4a80f88755f1a2422e37db46396fca015a35e966d")
-setManifestid(495422,"440247711752488453")
-addappid(495423,0,"ae5ec7c2bc2d211ef82c94e26d5fb9ac2e75be1d90fd5d3cf9bcaee2d5159a53")
-setManifestid(495423,"9135075435278811908")
-addappid(495421)
-addappid(495424)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 495420.lua
+--Gamename State of Decay 2: Juggernaut Edition
+addappid(495420, 1, "ed41614622b8ca9d92c9efcc6ef9505cc7e8db503507a9daf823a6c6a0cda9dc") --Mainappid State of Decay 2: Juggernaut Edition
+addappid(495422, 1, "82873486e64ba05260f583a4a80f88755f1a2422e37db46396fca015a35e966d") --Main Windows Depot State of Decay 2: Juggernaut Edition
+setManifestid(495422, "440247711752488453", 17965645728)
+addappid(495423, 1, "ae5ec7c2bc2d211ef82c94e26d5fb9ac2e75be1d90fd5d3cf9bcaee2d5159a53") --Main Windows Depot State of Decay 2: Juggernaut Edition
+setManifestid(495423, "9135075435278811908", 109527120)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
