d87f6611473980d92d6ec5427ceb02de2d2e7bba
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:17:51 +0800

    add 3009300.lua

---
+++ 3009300.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3009300.lua
+--Gamename Teamfight Manager 2
+addappid(3009300, 1, "959ccbd7f66f8670fc1d6b9124421955f1f7c31b953050f12bc234ec7ee7128b") --Mainappid Teamfight Manager 2
+addappid(3009301, 1, "b23fe424cc9f269898756472a513ce0395b175590259861bb8a68429fdc53fd2") --Main Windows Depot Teamfight Manager 2
+setManifestid(3009301, "2954263401846710061", 959064688)
+addappid(3009302, 1, "1fe745e6e17d2a1756a1141166288b75230a135dcace909747b21a7b3b3d9e5c") --Main Macos Depot Teamfight Manager 2
+setManifestid(3009302, "7325928956037365935", 954642784)
+addappid(3009303, 1, "7560f11036a03541c398147f2d257f514fc1c3b21e85bda7e30024f0103e1913") --Main Linux Depot Teamfight Manager 2
+setManifestid(3009303, "5650811284623115416", 957705456)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
