b63d9fb1f70da7406d391d4b074ca6ef9f7b3b75
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:54:33 +0800

    add 2417610.lua

---
+++ 2417610.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2417610.lua
+--Gamename METAL GEAR SOLID Δ: SNAKE EATER
+addappid(2417610, 1, "14b48d0210efd54e1d10ebf9167022f7e7e4618491c84746f64de8b474505681") --Mainappid METAL GEAR SOLID Δ: SNAKE EATER
+addappid(2417611, 1, "9a9b7b9956ecfecde761c3ec639b3c9efcc0e38d3b11a153d66014a015a8004d") --Main Windows Depot METAL GEAR SOLID Δ: SNAKE EATER
+setManifestid(2417611, "7817157983409754902", 106196941776)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2881290) --Dlcname METAL GEAR SOLID Δ: SNAKE EATER - WHITE TUXEDO
+addtoken(2881290, "17676168364810778620")
+addappid(3364840) --Dlcname METAL GEAR SOLID Δ: SNAKE EATER - Sneaking DLC Pack
+addtoken(3364840, "6792383297371870615")
