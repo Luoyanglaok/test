a0729dd1ea51819c7bc352af2a5f8852869eaa5b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 22:31:04 +0800

    add 1195590.lua

---
+++ 1195590.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1195590.lua
+--Gamename AppID_1195590
+addappid(1195590) --Mainappid AppID_1195590
+addappid(1195591, 1, "d716bef2da4fa005d0a268ed40ae5c45fcc08fad918c46ce4fe1651e6fa14b95") --Main Windows Depot AppID_1195590
+setManifestid(1195591, "5810733762559896321", 2765533440)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
