b0cc17e6b9e8fc52b9de6bc22c7e0de412a237c8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 00:30:34 +0800

    add 553850.lua

--- 553850.lua
+++ 553850.lua
@@ -1,25 +1,50 @@
-addappid(553850)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(553851,0,"6b40282b237b0cafc4d89014b5ec5f925f9aaa02f2d37f4d956acffdde2e0219")
-setManifestid(553851,"4333239040659935267")
-addappid(553853,0,"5422d63966f1d6febad8f7e2d1bc3a1640844ae17bae7fc242dc6c1f97b99f3a")
-setManifestid(553853,"4217421247306298529")
-addappid(553854,0,"43e8d494c23afa2317d5826d1ee2d87cf5657f1ce353df53190884b375a35ddd")
-setManifestid(553854,"1927836759628678344")
-addappid(2506220,0,"7ad81ca560ac5347639df776f2ac89be13e39105f7413a162fc1fd87ba07daf7")
-setManifestid(2506220,"254234794722414555")
-addappid(2506210,0,"05abf775a5f6aa84691b609ba4f72b7145a525d3ad96f40a370fedb0ca56e18e")
-setManifestid(2506210,"8012213004791051152")
-addappid(2506280,0,"15458a3b5a528df081b8c9c7702c90c4ccdcad2901e001c4adf7dae4c7229c4d")
-setManifestid(2506280,"2296416453264305306")
-addappid(2506270,0,"f80e19955c82b4f235d52f44da578439b0f93d36f489db81e9fcc8b88dd56fab")
-setManifestid(2506270,"8437189185293530538")
-addappid(2506250,0,"cdea2884539afaec7b221d45ee7692d076b02fd50763c20a094d58c05cba9574")
-setManifestid(2506250,"1450233528102364877")
-addappid(2506260,0,"85c9cfc1c96f01987c5bbf0b9be3cb58b760e512494cd167600197309d544526")
-setManifestid(2506260,"4456146330466267396")
-addappid(2506240,0,"55e8d00f742f81908503ca513f319ee3232b0d879972ea6c37106b59d72e9228")
-setManifestid(2506240,"6979138493630637638")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 553850.lua
+--Gamename HELLDIVERS™ 2
+addappid(553850, 1, "7383127ec54af70c43f24a0f4026210541378dbf5d40a51aa740e886394653c5") --Mainappid HELLDIVERS™ 2
+addappid(553851, 1, "6b40282b237b0cafc4d89014b5ec5f925f9aaa02f2d37f4d956acffdde2e0219") --Main Windows Depot HELLDIVERS™ 2
+setManifestid(553851, "5937460960958490081", 164881856)
+addappid(553853, 1, "5422d63966f1d6febad8f7e2d1bc3a1640844ae17bae7fc242dc6c1f97b99f3a") --Main Windows Depot HELLDIVERS™ 2
+setManifestid(553853, "9020691380707780829", 22077875216)
+addappid(553854, 1, "43e8d494c23afa2317d5826d1ee2d87cf5657f1ce353df53190884b375a35ddd") --Main Windows Depot HELLDIVERS™ 2
+setManifestid(553854, "6929803021334100650", 292714832)
+--Dlcname HELLDIVERS™ 2 - Português-Brasil (Portuguese - Brazil)
+addappid(2506210, 1, "05abf775a5f6aa84691b609ba4f72b7145a525d3ad96f40a370fedb0ca56e18e") --Dlcname HELLDIVERS™ 2 - Português-Brasil (Portuguese - Brazil)
+setManifestid(2506210, "837647062170379478", 293320928)
+--Dlcname HELLDIVERS™ 2 - Deutsch (German)
+addappid(2506220, 1, "7ad81ca560ac5347639df776f2ac89be13e39105f7413a162fc1fd87ba07daf7") --Dlcname HELLDIVERS™ 2 - Deutsch (German)
+setManifestid(2506220, "6916690753983708143", 298149840)
+--Dlcname HELLDIVERS™ 2 - Español-España (Spanish - Spain)
+addappid(2506240, 1, "55e8d00f742f81908503ca513f319ee3232b0d879972ea6c37106b59d72e9228") --Dlcname HELLDIVERS™ 2 - Español-España (Spanish - Spain)
+setManifestid(2506240, "5039770487947346137", 304106240)
+--Dlcname HELLDIVERS™ 2 - Français (French)
+addappid(2506250, 1, "cdea2884539afaec7b221d45ee7692d076b02fd50763c20a094d58c05cba9574") --Dlcname HELLDIVERS™ 2 - Français (French)
+setManifestid(2506250, "3825743383596942863", 289507088)
+--Dlcname HELLDIVERS™ 2 - Italiano (Italian)
+addappid(2506260, 1, "85c9cfc1c96f01987c5bbf0b9be3cb58b760e512494cd167600197309d544526") --Dlcname HELLDIVERS™ 2 - Italiano (Italian)
+setManifestid(2506260, "8901884106685008539", 294743104)
+--Dlcname HELLDIVERS™ 2 - 日本語 (Japanese)
+addappid(2506270, 1, "f80e19955c82b4f235d52f44da578439b0f93d36f489db81e9fcc8b88dd56fab") --Dlcname HELLDIVERS™ 2 - 日本語 (Japanese)
+setManifestid(2506270, "7258631991920304792", 281656096)
+--Dlcname HELLDIVERS™ 2 - Español-Latinoamérica (Spanish - Latin America)
+addappid(2506280, 1, "15458a3b5a528df081b8c9c7702c90c4ccdcad2901e001c4adf7dae4c7229c4d") --Dlcname HELLDIVERS™ 2 - Español-Latinoamérica (Spanish - Latin America)
+setManifestid(2506280, "2235691478097112013", 292196048)
+--Dlcname HELLDIVERS™ 2 - 中文 (Chinese)
+addappid(4199890, 1, "e08bc75d58f64b27a7f66e0ed0e048bb04d912c3f3d273c216366714e4e2299c") --Dlcname HELLDIVERS™ 2 - 中文 (Chinese)
+setManifestid(4199890, "6509021360920651876", 252395952)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2506300) --Dlcname HELLDIVERS™ 2 - Русский (Russian)
+addappid(2514120) --Dlcname HELLDIVERS™ 2 - Pre-purchase Entitlement
+addappid(2514130) --Dlcname HELLDIVERS™ 2 - Upgrade to Super Citizen Edition
+addappid(2521690) --Dlcname HELLDIVERS™ 2 - ‘Steeled Veterans’ Premium Warbond
+addappid(2765950) --Dlcname HELLDIVERS™ 2 - TR-117 Alpha Commander
+addappid(3118530) --Dlcname HELLDIVERS™ 2 - TR-40 Gold Eagle
+addappid(3165780) --Dlcname HELLDIVERS™ 2 - Warbond Token
