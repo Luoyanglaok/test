2bd03de8c2a7e680f4504d4be16fbd697d9b9d7b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:54:46 +0800

    add 1627720.lua

--- 1627720.lua
+++ 1627720.lua
@@ -1,8 +1,27 @@
-addappid(1627720)
-addtoken(1627720,"16027423782916783116")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1627721,0,"d847ff3494ad5b72d69b7b38c52474fdb22d437421b569013dd1508d8879f7c1")
-setManifestid(1627721,"2864050144902464475")
-addappid(1627722,0,"b2550c254da0b229ff5f5bb765b809d2fac5bd74a98ff3611b8cb8870e02aa6a")
-setManifestid(1627722,"6836272904728643256")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1627720.lua
+--Gamename Lies of P
+addappid(1627720, 1, "3b74c91ab7657b50731863962dcf8d62ba04f0c9c5990cdaaeb7e7ae45c91dcd") --Mainappid Lies of P
+addtoken(1627720, "16027423782916783116")
+addappid(1627721, 1, "d847ff3494ad5b72d69b7b38c52474fdb22d437421b569013dd1508d8879f7c1") --Main Windows/Linux Depot Lies of P
+setManifestid(1627721, "2432660753175592861", 50881630752)
+addappid(1627722, 1, "b2550c254da0b229ff5f5bb765b809d2fac5bd74a98ff3611b8cb8870e02aa6a") --Main Macos Depot Lies of P
+setManifestid(1627722, "959564121581773315", 51136014480)
+--Dlcname Lies of P : Soundtrack
+addappid(1938420) --Dlcname Lies of P : Soundtrack
+addappid(1938421, 1, "3a598dabbe2783774bd506a68f7da2189eabe7234c03bdfce88cefed14bab3d8") --Dlc Windows Depot Lies of P : Soundtrack
+setManifestid(1938421, "3546175310487101172", 352460464)
+addappid(1938422, 1, "ababb5e54025724a4a56f8f0f08d33282cd3ef1296cb746627419bf317d9baa4") --Dlc Windows Depot Lies of P : Soundtrack
+setManifestid(1938422, "5377334227263562822", 1730874112)
+addappid(1938423, 1, "f1a270c1b76e7723c154a571d3d0e70521e697e67dd1e75fab4d6de0e1199a5d") --Dlc Windows Depot Lies of P : Soundtrack
+setManifestid(1938423, "1904840400598274064", 242003632)
+addappid(1938424, 1, "27a7a3f53278e69bd81334632b6db38ed9d5b16f4865d48e46bbe78e45543ee0") --Dlc Windows Depot Lies of P : Soundtrack
+setManifestid(1938424, "7265438420520470710", 2052030224)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2325280) --Dlcname Lies of P : Pre-Order Bonus
+addappid(2325281) --Dlcname Lies of P: Prince of Krat Cosmetics Pack
+addappid(2848330) --Dlcname Lies of P: Overture
