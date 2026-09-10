53bd66d618e6e20341442044e7cd7b14df134949
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:37:10 +0800

    add 3222890.lua

---
+++ 3222890.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3222890.lua
+--Gamename 醉风酒 Taste of the Wind
+addappid(3222890) --Mainappid 醉风酒 Taste of the Wind
+addappid(3222891, 1, "ce8d93f8abcfc40901372f3289e93d66d306d53e451db73d23ac2f5502c3674e") --Main Windows/Linux Depot 醉风酒 Taste of the Wind
+setManifestid(3222891, "6759558493891664368", 1015877328)
+addappid(3222892, 1, "287a710b74d5e8a09a6b686b2ecd37e73722bb124d8c3bb7163475a8a3feca6a") --Main Macos Depot 醉风酒 Taste of the Wind
+setManifestid(3222892, "1256358689803086256", 1023408048)
