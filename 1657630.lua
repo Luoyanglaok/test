23ca0188adbdcfab61676a14b1bdee34889e0621
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:05:07 +0800

    add 1657630.lua

--- 1657630.lua
+++ 1657630.lua
@@ -4,15 +4,3 @@
 addappid(1657630, 1, "3618218c6d7e06b80d06f3b06e2d66b96012de6769860e72badf747497977535") --Mainappid Slime Rancher 2
 addappid(1657631, 1, "a37c1409dc2c7880fc988d25d9fa333438bab756f17f7d0fc27015c8ef2faed3") --Main Windows Depot Slime Rancher 2
 setManifestid(1657631, "8733226813009597637", 2874506128)
---Dlcname Slime Rancher 2 Soundtrack
-addappid(2117970) --Dlcname Slime Rancher 2 Soundtrack
-addappid(2117971, 1, "9e5f1f261249a7635d8b594259a463ef44d0c50658445492bf48951d0223faf0") --Dlc Windows Depot Slime Rancher 2 Soundtrack
-setManifestid(2117971, "4405624980261655057", 326521984)
-addappid(2117972, 1, "8f1d483cea319575e627b91883cc9e2750ea0177c68b42dc9cb683ffc806a854") --Dlc Windows Depot Slime Rancher 2 Soundtrack
-setManifestid(2117972, "532255500344259410", 988982544)
---Dlcname Slime Rancher 2: Original Soundtrack, Vol 2
-addappid(3922910) --Dlcname Slime Rancher 2: Original Soundtrack, Vol 2
-addappid(3922911, 1, "476afdd2c2204166ee3b1a0adc1cd7d60e33ce9ffb285c54da99d1063ebbd202") --Dlc Windows Depot Slime Rancher 2: Original Soundtrack, Vol 2
-setManifestid(3922911, "4249749382686594985", 473481120)
-addappid(3922912, 1, "b8944b758fe2574f8946ba79d29d3c8dd102e35b0592da9dcae82d7520d96c73") --Dlc Windows Depot Slime Rancher 2: Original Soundtrack, Vol 2
-setManifestid(3922912, "4053339426284940568", 1324442240)
