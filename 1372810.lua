29a2cfbc1c292387176d224172301b57735951d5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:49:30 +0800

    add 1372810.lua

--- 1372810.lua
+++ 1372810.lua
@@ -1,5 +1,12 @@
-addappid(1372810)
-addappid(1372811,0,"df6596100d8c79e5b47567902f61ab4351ac02aefa8fdccc6382ce0b44684423")
-setManifestid(1372811,"2027851026347150373")
-addappid(1372813,0,"8d10044d66eae146f1f587e09fd3969e350626d48c277a2f01cc44e6f5a460da")
-setManifestid(1372813,"9027546050920686557")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1372810.lua
+--Gamename Teamfight Manager
+addappid(1372810) --Mainappid Teamfight Manager
+addappid(1372811, 1, "df6596100d8c79e5b47567902f61ab4351ac02aefa8fdccc6382ce0b44684423") --Main Windows Depot Teamfight Manager
+setManifestid(1372811, "2027851026347150373", 235650128)
+addappid(1372813, 1, "8d10044d66eae146f1f587e09fd3969e350626d48c277a2f01cc44e6f5a460da") --Main Macos Depot Teamfight Manager
+setManifestid(1372813, "9027546050920686557", 228630256)
+addappid(1565922) --Dlcname Teamfight Manager - Donationware Tier 3
+--Missing Dlcs
+--Dlcname Teamfight Manager - Donationware Tier 1
+--Dlcname Teamfight Manager - Donationware Tier 2
