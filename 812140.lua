c458cb8b4d4aea33a8d98c1bdbf9f9f41d6a71d2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:38:48 +0800

    add 812140.lua

--- 812140.lua
+++ 812140.lua
@@ -1,31 +1,60 @@
-addappid(812140)
-addappid(812141,0,"0d69b986695af676b593ce6d217a24a47bceeb630dbd8ac61eaba88b72a17819")
-setManifestid(812141,"7369859942329036273")
-addappid(812142,0,"500a7b45788ab393ea848fa1f13f8c94450e34112be4f429c609035df4d6e1e9")
-setManifestid(812142,"8328691686092249446")
-addappid(812143,0,"ef150f7bac4d1776428bdbf22cbc466a706c94ca3fad3e52fc3f04640d9e6445")
-setManifestid(812143,"7207703417476497678")
-addappid(812144,0,"a3c5e18d19e59d43ac841bfb47696527a5df28c7f00bc487ad1209bdf62c919a")
-setManifestid(812144,"7568168263952342688")
-addappid(812145,0,"e7987355e75005a3427ea9f492931e4d3dcc3fead917205b3f8559e2d5460db6")
-setManifestid(812145,"8450177789687634985")
-addappid(812146,0,"e6a983014e43104c1ce2cba18939b39f80e80c1d4c38dbaaf00867d3f6f244a6")
-setManifestid(812146,"6975128233689020437")
-addappid(812147,0,"1f216a1ed4582aedcd215d44fd522d5712e7c4078d336e9fddf229439cb1ac15")
-setManifestid(812147,"8756573802222983569")
-addappid(812148,0,"c86367c01d5724d576682c66d4ba21c57a3524633d185a5bf0cbf3fa9629c545")
-setManifestid(812148,"3408766981981870757")
-addappid(857154,0,"83d52bd1470d31e15675f56ebe3ebcc33aca78389e03609658453f0bbab533bd")
-setManifestid(857154,"6682327947305864699")
-addappid(857155,0,"3090e98acd394b0fd68186d871834bf36b3f01f272c974a9f18c7b0d89289750")
-setManifestid(857155,"3258680875590795254")
-addappid(857156,0,"955d65e60433b66a7635ab199e725571f2c649893471f79840cde33a687cf9e8")
-setManifestid(857156,"2300917583245418327")
-addappid(857157,0,"322b49c10e633b78fd5e37cc8ef9cf5f8eb4b000472ab38228b068a9921eceb2")
-setManifestid(857157,"5671725767919675728")
-addappid(857158,0,"8be9361fa307daedc4a4162a88f195938dce6a248d9fe399ceea1fa2d66a1dd4")
-setManifestid(857158,"2111558703979014825")
-addappid(857159,0,"e2d4b3e65d688551d5b3a00e3f337d4f3d4ba560523a57ce9333c03d26219f19")
-setManifestid(857159,"4985956972222534239")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 812140.lua
+--Gamename Assassin's Creed® Odyssey
+addappid(812140, 1, "a3ae6196e6418b650f1dc3be61564d5a6eb35222d212bd25d3caf8c6d51bdeb7") --Mainappid Assassin's Creed® Odyssey
+addappid(812141, 1, "0d69b986695af676b593ce6d217a24a47bceeb630dbd8ac61eaba88b72a17819") --Main Windows Depot Assassin's Creed® Odyssey
+setManifestid(812141, "7369859942329036273", 60488424144)
+addappid(812142, 1, "500a7b45788ab393ea848fa1f13f8c94450e34112be4f429c609035df4d6e1e9") --Main Windows Language Depot French Assassin's Creed® Odyssey
+setManifestid(812142, "8328691686092249446", 1957556880)
+addappid(812143, 1, "ef150f7bac4d1776428bdbf22cbc466a706c94ca3fad3e52fc3f04640d9e6445") --Main Windows Language Depot Brazilian Assassin's Creed® Odyssey
+setManifestid(812143, "7207703417476497678", 2462703440)
+addappid(812144, 1, "a3c5e18d19e59d43ac841bfb47696527a5df28c7f00bc487ad1209bdf62c919a") --Main Windows Language Depot German Assassin's Creed® Odyssey
+setManifestid(812144, "7568168263952342688", 1918841856)
+addappid(812145, 1, "e7987355e75005a3427ea9f492931e4d3dcc3fead917205b3f8559e2d5460db6") --Main Windows Language Depot Italian Assassin's Creed® Odyssey
+setManifestid(812145, "8450177789687634985", 1975810720)
+addappid(812146, 1, "e6a983014e43104c1ce2cba18939b39f80e80c1d4c38dbaaf00867d3f6f244a6") --Main Windows Language Depot Russian Assassin's Creed® Odyssey
+setManifestid(812146, "6975128233689020437", 1809208224)
+addappid(812147, 1, "1f216a1ed4582aedcd215d44fd522d5712e7c4078d336e9fddf229439cb1ac15") --Main Windows Language Depot Spanish Assassin's Creed® Odyssey
+setManifestid(812147, "8756573802222983569", 1958131600)
+addappid(812148, 1, "c86367c01d5724d576682c66d4ba21c57a3524633d185a5bf0cbf3fa9629c545") --Main Windows Language Depot Japanese Assassin's Creed® Odyssey
+setManifestid(812148, "3408766981981870757", 1858715488)
+--Dlcname Assassin's Creed Odyssey - Legacy of the First Blade
+addappid(937891) --Dlcname Assassin's Creed Odyssey - Legacy of the First Blade
+addappid(857154, 1, "83d52bd1470d31e15675f56ebe3ebcc33aca78389e03609658453f0bbab533bd") --Dlc Windows Depot Assassin's Creed Odyssey - Legacy of the First Blade
+setManifestid(857154, "6682327947305864699", 2544201952)
+addappid(857155, 1, "3090e98acd394b0fd68186d871834bf36b3f01f272c974a9f18c7b0d89289750") --Dlc Windows Depot Assassin's Creed Odyssey - Legacy of the First Blade
+setManifestid(857155, "3258680875590795254", 1067526560)
+addappid(857156, 1, "955d65e60433b66a7635ab199e725571f2c649893471f79840cde33a687cf9e8") --Dlc Windows Depot Assassin's Creed Odyssey - Legacy of the First Blade
+setManifestid(857156, "2300917583245418327", 1179359648)
+--Dlcname Assassin's Creed Odyssey - The Fate of Atlantis
+addtoken(937892, "15850730603638221170")
+addappid(937892) --Dlcname Assassin's Creed Odyssey - The Fate of Atlantis
+addappid(857158, 1, "8be9361fa307daedc4a4162a88f195938dce6a248d9fe399ceea1fa2d66a1dd4") --Dlc Windows Depot Assassin's Creed Odyssey - The Fate of Atlantis
+setManifestid(857158, "2111558703979014825", 6219671984)
+addappid(857159, 1, "e2d4b3e65d688551d5b3a00e3f337d4f3d4ba560523a57ce9333c03d26219f19") --Dlc Windows Depot Assassin's Creed Odyssey - The Fate of Atlantis
+setManifestid(857159, "4985956972222534239", 10036508256)
+--Dlcname Assassin's Creed Odyssey - Fields of Elysium
+addappid(1109770) --Dlcname Assassin's Creed Odyssey - Fields of Elysium
+addappid(857157, 1, "322b49c10e633b78fd5e37cc8ef9cf5f8eb4b000472ab38228b068a9921eceb2") --Dlc Windows Depot Assassin's Creed Odyssey - Fields of Elysium
+setManifestid(857157, "5671725767919675728", 6776838688)
+--Share Depots
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(857150) --Dlcname Assassin's Creed Odyssey - Preorder Standard - Uplay Activation
+addappid(857151) --Dlcname Assassin's Creed Odyssey - Preorder Deluxe - Uplay Activation
+addappid(857152) --Dlcname Assassin's Creed Odyssey - Preorder Gold - Uplay Activation
+addappid(857153) --Dlcname Assassin's Creed Odyssey - Preorder Ultimate - Uplay Activation
+addappid(907610) --Dlcname Assassin's Creed Odyssey - Standard - Uplay Activation
+addappid(907611) --Dlcname Assassin's Creed Odyssey - Deluxe - Uplay Activation
+addappid(907613) --Dlcname Assassin's Creed Odyssey - Gold - Uplay Activation
+addappid(907614) --Dlcname Assassin's Creed Odyssey - Ultimate - Uplay Activation
+addappid(933010) --Dlcname Assassin's Creed Odyssey - Season Pass – Uplay Activation
+addappid(933011) --Dlcname AppID 933011
+addtoken(933011, "11715314837577335280")
+addappid(933012) --Dlcname AppID 933012
+addtoken(933012, "6760411481530335580")
+addappid(937890) --Dlcname Assassin's Creed Odyssey - Season Pass
+addappid(1113150) --Dlcname Assassin's Creed Odyssey - Free DLC 2.1 - Uplay Activation
+addappid(1254000) --Dlcname AppID 1254000
+addappid(1254010) --Dlcname Assassin's Creed Odyssey - Full Game ownership
+addtoken(1254010, "16051304276878572945")
