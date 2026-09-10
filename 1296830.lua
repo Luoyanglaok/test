7e57f04fb8809a8baa20903281d51687a335d08d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:08:45 +0800

    add 1296830.lua

--- 1296830.lua
+++ 1296830.lua
@@ -1,5 +1,24 @@
-addappid(1296830)
-addappid(1296831,0,"cf5d2cd621a67cb4a476d6390fe2d9e4506dcc772b691c35acc5bcc6c4a7e976")
-setManifestid(1296831,"5191741389914849473")
-addappid(1296832,0,"d5aa12207c07cf9f8ba98b9e6fdc9b08c0883ff5f80e0da3d720c6bdd2cb5c43")
-setManifestid(1296832,"2934943906401015285")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1296830.lua
+--Gamename 暖雪 Warm Snow
+addappid(1296830) --Mainappid 暖雪 Warm Snow
+addappid(1296831, 1, "cf5d2cd621a67cb4a476d6390fe2d9e4506dcc772b691c35acc5bcc6c4a7e976") --Main Windows Depot 暖雪 Warm Snow
+setManifestid(1296831, "5191741389914849473", 2239968848)
+addappid(1296832, 1, "d5aa12207c07cf9f8ba98b9e6fdc9b08c0883ff5f80e0da3d720c6bdd2cb5c43") --Main Windows Depot 暖雪 Warm Snow
+setManifestid(1296832, "2934943906401015285", 1368531696)
+--Dlcname 暖雪 Warm Snow - FirstDLC
+addappid(2147220, 1, "d9be6d05aa5eca0530a97338b5ce85d0aa680ea87d3cf330e242af3d908a44ef") --Dlcname 暖雪 Warm Snow - FirstDLC
+setManifestid(2147220, "1125160281922234017", 64)
+addappid(2147222, 1, "f90956526a139781a45069ee419ce8aed9d0642bdbd267ca0cec80f2e86ac481") --Dlc Windows Depot 暖雪 Warm Snow - FirstDLC
+setManifestid(2147222, "6068627155823496470", 2231308448)
+--Dlcname 暖雪 Warm Snow - DLC2
+addappid(2612950, 1, "e72f0b985b64245d58130d6953c964ec1a19e15cbcf1bea48e69fb419f39a0f1") --Dlcname 暖雪 Warm Snow - DLC2
+setManifestid(2612950, "5923179117909807892", 2239792448)
+addappid(2612951, 1, "8836b7eab3392d6916577bff52e2b164064115d58d59696306e1933c342c8a4c") --Dlc Windows Depot 暖雪 Warm Snow - DLC2
+setManifestid(2612951, "1629603847288100961", 2237720656)
+addappid(2612952, 1, "a0e808e703d416af24ea88a613a8ff91a92e5508669aeba93c54581e55715569") --Dlc Windows Language Depot Bulgarian 暖雪 Warm Snow - DLC2
+setManifestid(2612952, "5973679343989166898", 2240354464)
+--Dlcname 暖雪 Warm Snow - 美术设定集
+addtoken(2936000, "15755738597420251343")
+addappid(2936000, 1, "3f9660fd9153125226d5a791c896e95e7f91196474916e114a6b5a7cc6bc82ed") --Dlcname 暖雪 Warm Snow - 美术设定集
+setManifestid(2936000, "1496916447034636149", 367431120)
