f88c1bad2212e20c4a168dbed1b4d6b306deba3c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:16:36 +0800

    add 300380.lua

--- 300380.lua
+++ 300380.lua
@@ -1,24 +1,44 @@
-addappid(300380)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-setManifestid(229001,"4049573910112143457")
-setManifestid(229002,"7260605429366465749")
-setManifestid(229003,"8740933542064151477")
-setManifestid(229004,"5220958916987797232")
-addappid(300382,0,"d6fa1b92e0b5716e57b04973493c4974423db7e7b84981965be53fc2638ca620")
-setManifestid(300382,"4407793480085078994")
-addappid(300385,0,"93af33495aa32c25253d66705a23f1951c26fa64b175cc1008bcc45383edb333")
-setManifestid(300385,"7027597161995680235")
-addappid(300386,0,"0cccbda95beae17dc8a69a846c3c4b30ff5c1c905e63a99f7df3d52b09e4c946")
-setManifestid(300386,"6706157212336714672")
-addappid(300387,0,"99ccddb494b1509668cdcc23d5253331454e46dfdc64ca4652c09d1666103ae7")
-setManifestid(300387,"6656347611638919898")
-addappid(725880,0,"4782b6fe6bafc37455e4e121ebaa8b9d038db5796df8434d12fb12e1e1b67733")
-setManifestid(725880,"6015472939352733004")
-addappid(727050,0,"5c08a0fcfd1620810543e1c6f254f63b333e1f4639964b268e442a95c5ace094")
-setManifestid(727050,"1911146224113644893")
-addappid(727440,0,"8e9373e3ab2b5de40a00dd5853abea47b0042d97720258a0f3e16004d964d437")
-setManifestid(727440,"9036764687940851995")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 300380.lua
+--Gamename Road Redemption
+addappid(300380) --Mainappid Road Redemption
+addappid(300381, 1, "569251966c7f451ccf9ee0e549fe83d74989796633f9fa0d7ac1b68f0bdc4a5b") --Main Windows Depot Road Redemption
+addappid(300382, 1, "d6fa1b92e0b5716e57b04973493c4974423db7e7b84981965be53fc2638ca620") --Main Windows Depot Road Redemption
+setManifestid(300382, "4407793480085078994", 2682787008)
+addappid(300385, 1, "93af33495aa32c25253d66705a23f1951c26fa64b175cc1008bcc45383edb333") --Main Macos Depot Road Redemption
+setManifestid(300385, "7027597161995680235", 2688471264)
+addappid(300386, 1, "0cccbda95beae17dc8a69a846c3c4b30ff5c1c905e63a99f7df3d52b09e4c946") --Main Windows Depot Road Redemption
+setManifestid(300386, "6706157212336714672", 1742426240)
+addappid(300387, 1, "99ccddb494b1509668cdcc23d5253331454e46dfdc64ca4652c09d1666103ae7") --Main Linux Depot Road Redemption
+setManifestid(300387, "6656347611638919898", 2679575808)
+--Dlcname Road Redemption - Art Book
+addappid(725880, 1, "4782b6fe6bafc37455e4e121ebaa8b9d038db5796df8434d12fb12e1e1b67733") --Dlcname Road Redemption - Art Book
+setManifestid(725880, "6015472939352733004", 499629520)
+--Dlcname Road Redemption - Early Prototype
+addappid(727050, 1, "5c08a0fcfd1620810543e1c6f254f63b333e1f4639964b268e442a95c5ace094") --Dlcname Road Redemption - Early Prototype
+setManifestid(727050, "1911146224113644893", 633270240)
+--Dlcname Road Redemption - Developer Commentary Video
+addappid(727440, 1, "8e9373e3ab2b5de40a00dd5853abea47b0042d97720258a0f3e16004d964d437") --Dlcname Road Redemption - Developer Commentary Video
+setManifestid(727440, "9036764687940851995", 4113023184)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229001, 1, "ec23e1a1e1da6205722291f91bf72d1989cb347245e5a14d8d514c4ad5ba4e97") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229001, "4049573910112143457", 268006912)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229003, "8740933542064151477", 42914928)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(1180010) --Dlcname Road Redemption - Revengers Assemble
+--Missing Dlcs
+--Dlcname Road Redemption: Name A Character
