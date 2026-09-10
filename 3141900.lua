d95ff902c5e2328c9a33794f64596a246ba2267e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:40:53 +0800

    add 3141900.lua

--- 3141900.lua
+++ 3141900.lua
@@ -1,5 +1,12 @@
-addappid(3141900)
-addappid(3141903,0,"074244f0a8e28327ff8281168ce84e96ab27cde591331e6cd76973c97279455d")
-setManifestid(3141903,"6642829085694435462")
-addappid(3141904,0,"96ef6885f9b0bc7a8289c3a7dc92788fa84d7bc218b4be5de6f5c1a4372336fc")
-setManifestid(3141904,"8803848270975066170")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3141900.lua
+--Gamename Saintess Eruna and the Lustful Book of Corruption ~Busty Sister's H Services~
+addappid(3141900, 1, "8444cbd3c01805fa87667acf4b59c05fc0d6938a24ddd0a622a6f05dfe8beb30") --Mainappid Saintess Eruna and the Lustful Book of Corruption ~Busty Sister's H Services~
+addappid(3141901, 1, "3ca8abdfd3db93a319b9132c97664f4a930a300662ae1a5bfc93ddea581f9293") --Main Windows Language Depot Japanese Saintess Eruna and the Lustful Book of Corruption ~Busty Sister's H Services~
+setManifestid(3141901, "1367824178122121060", 740797008)
+addappid(3141902, 1, "0eb53395c49a4a930b50c69c66e45798642f39a1fbf29a4cb45c7cf39cc0f91d") --Main Windows Language Depot English Saintess Eruna and the Lustful Book of Corruption ~Busty Sister's H Services~
+setManifestid(3141902, "5690852239894504603", 758746784)
+addappid(3141903, 1, "074244f0a8e28327ff8281168ce84e96ab27cde591331e6cd76973c97279455d") --Main Windows Language Depot Tchinese Saintess Eruna and the Lustful Book of Corruption ~Busty Sister's H Services~
+setManifestid(3141903, "6642829085694435462", 758770608)
+addappid(3141904, 1, "96ef6885f9b0bc7a8289c3a7dc92788fa84d7bc218b4be5de6f5c1a4372336fc") --Main Windows Language Depot Schinese Saintess Eruna and the Lustful Book of Corruption ~Busty Sister's H Services~
+setManifestid(3141904, "8803848270975066170", 762490592)
