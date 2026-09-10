aaf7fb59a7ebdbd8991a20b3e4ae1f934cf1d488
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:00:27 +0800

    add 357190.lua

--- 357190.lua
+++ 357190.lua
@@ -1,4 +1,9 @@
-addappid(357190)
-setManifestid(228990,"1829726630299308803")
-addappid(357191,0,"c855d2a2a022b2f35add948a015356e8fe3c3802195b5507e2bedc3acab1c6bb")
-setManifestid(357191,"7549865494669531118")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 357190.lua
+--Gamename ULTIMATE MARVEL VS. CAPCOM 3
+addappid(357190, 1, "97b2d3cc9da0f8851f848372d620a023fd2ea96d856f5e83e83153119c87941c") --Mainappid ULTIMATE MARVEL VS. CAPCOM 3
+addappid(357191, 1, "c855d2a2a022b2f35add948a015356e8fe3c3802195b5507e2bedc3acab1c6bb") --Main Windows Depot ULTIMATE MARVEL VS. CAPCOM 3
+setManifestid(357191, "7549865494669531118", 3385497136)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
