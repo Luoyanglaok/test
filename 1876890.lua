5c40d56393fc550f4c746aa509142994a49edee9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:15:30 +0800

    add 1876890.lua

--- 1876890.lua
+++ 1876890.lua
@@ -1,6 +1,16 @@
-addappid(1876890)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1876891,0,"40985dfc0fbcd23c2b692a036c22fad2ad18d03794b58ea33fd079acbfb53c5d")
-setManifestid(1876891,"9087759129847348343")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1876890.lua
+--Gamename Wandering Sword
+addappid(1876890, 1, "17330e11dd131d13006d8e8df286ee717fd284f940bb652d3ce3d2390e258964") --Mainappid Wandering Sword
+addappid(1876891, 1, "40985dfc0fbcd23c2b692a036c22fad2ad18d03794b58ea33fd079acbfb53c5d") --Main Windows Depot Wandering Sword
+setManifestid(1876891, "1253249806920162062", 5595865024)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3027840) --Dlcname Wandering Sword - Mount Pack: Majestic Steeds
+addappid(3600640) --Dlcname Wandering Sword - Secrets of the Eastern Sea
+addappid(3600650) --Dlcname Wandering Sword - Hero's Adventure Collaboration DLC
