af1d9013c64a19a66e26820354db847d575c34e5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:31:52 +0800

    add 1149460.lua

--- 1149460.lua
+++ 1149460.lua
@@ -1,5 +1,31 @@
-addappid(1149460)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1149461,0,"c71fc47ef9e1e718df06677c0d6cf06fc0f01145d8481e0db444a26eed261296")
-setManifestid(1149461,"2486877982870437883")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1149460.lua
+--Gamename ICARUS
+addappid(1149460, 1, "fecfa8dae0f009276447d67ac2d4aad77ce51ed669d1cad33af05676f6d99699") --Mainappid ICARUS
+addappid(1149461, 1, "c71fc47ef9e1e718df06677c0d6cf06fc0f01145d8481e0db444a26eed261296") --Main Windows Depot ICARUS
+setManifestid(1149461, "1648123548408384195", 42990325712)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1648532) --Dlcname Icarus: New Frontiers Expansion
+addappid(1648533) --Dlcname Icarus: Dangerous Horizons Expansion
+addappid(1648540) --Dlcname Icarus: Holdfast Outpost
+addappid(1648541) --Dlcname Icarus: Iceholm Outpost
+addappid(1995690) --Dlcname Icarus: Styx Expansion
+addappid(2445280) --Dlcname Icarus: Interior Decorations Pack
+addappid(2536360) --Dlcname Icarus: Tecton Outpost
+addappid(2536370) --Dlcname Icarus: Everbark Outpost
+addappid(2536380) --Dlcname Icarus: Cactus Outpost
+addappid(2536390) --Dlcname Icarus: Arcticus Outpost
+addappid(2679990) --Dlcname Icarus: Industrial Furniture Pack
+addappid(2686540) --Dlcname Icarus: Art Deco Furniture Pack
+addappid(2790990) --Dlcname Icarus: Creature Comforts Pack
+addappid(2791000) --Dlcname Icarus: Homestead Content Pack
+addappid(2923820) --Dlcname Icarus: Pet Companions Pack
+addappid(2923830) --Dlcname Icarus: Zebra Rescue Mission
+addappid(3052290) --Dlcname Icarus: Matariki Holiday Mission
+addappid(3315290) --Dlcname Icarus: Great Hunts Campaigns
+addappid(3364360) --Dlcname Icarus: Frostfall Outpost
+addappid(3984230) --Dlcname Icarus: Aquatica Isle Outpost
