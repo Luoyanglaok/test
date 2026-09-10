8f49f071838b9d68100e1fba318542aac2de4f28
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:18:38 +0800

    add 3431040.lua

--- 3431040.lua
+++ 3431040.lua
@@ -1,7 +1,14 @@
-addappid(3431040)
-addappid(3431043,0,"deb5b59ba0936b3ae251e701a13be2d2f24a8cbc021be1bc13e7925fda72745c")
-setManifestid(3431043,"8870970973599912143")
-addappid(3431041,0,"cc51fcec3d8dda778605464eef88ae266cdbc612a843106a170839a7060289fc")
-setManifestid(3431041,"5213632607014064158")
-addappid(3431042,0,"5bd556fdd5836d1e3abc4e4e086fd6f9fba21afbf7280e427b1e0c2865de4d3a")
-setManifestid(3431042,"376615255908550684")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3431040.lua
+--Gamename That's not my Neighbor
+addappid(3431040, 1, "e11ff1bef0f8523be9e1a62e1446b577bae26367d2c67887f002ba7350451c4f") --Mainappid That's not my Neighbor
+addappid(3431041, 1, "cc51fcec3d8dda778605464eef88ae266cdbc612a843106a170839a7060289fc") --Main Windows Depot That's not my Neighbor
+setManifestid(3431041, "5213632607014064158", 301892368)
+addappid(3431042, 1, "5bd556fdd5836d1e3abc4e4e086fd6f9fba21afbf7280e427b1e0c2865de4d3a") --Main Linux Depot That's not my Neighbor
+setManifestid(3431042, "376615255908550684", 304097072)
+addappid(3431043, 1, "deb5b59ba0936b3ae251e701a13be2d2f24a8cbc021be1bc13e7925fda72745c") --Main Windows Depot That's not my Neighbor
+setManifestid(3431043, "8870970973599912143", 300952608)
+addappid(3431045, 1, "b508cdfe4e9f20dc8e655da4aa5cd1e41833a3421485f79fc8039f69b1655ee3") --Main Windows Depot That's not my Neighbor
+setManifestid(3431045, "591281361236477397", 301889264)
+addappid(3431047, 1, "5e7961ef262adf64c0b82ed8f8f7527db9e6f65f716068d6a9320bbe744c43e3") --Main Windows Depot That's not my Neighbor
+setManifestid(3431047, "3131244617883416287", 301889392)
