a1370f21a48d1e82fe3b052ddc6120cbfb082bfa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:11:25 +0800

    add 335300.lua

--- 335300.lua
+++ 335300.lua
@@ -1,8 +1,15 @@
-addappid(335300)
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(335301,0,"cf0e82e9ee3c3dd620a31184d8fb9d60c9e6641be15cdd5cc6da9960e822384a")
-setManifestid(335301,"2962083928887153929")
-addappid(335302,0,"7a10a8968977653c95ac43dc8e7799f02750283f840618910466e264e85f97c1")
-setManifestid(335302,"6445867072010600238")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 335300.lua
+--Gamename DARK SOULS™ II: Scholar of the First Sin
+addappid(335300, 1, "f6c61c789e294ecdcc7629d6efffd2fe0a454edfd941c8935419a460b04b8e7f") --Mainappid DARK SOULS™ II: Scholar of the First Sin
+addappid(335301, 1, "cf0e82e9ee3c3dd620a31184d8fb9d60c9e6641be15cdd5cc6da9960e822384a") --Main Windows Depot DARK SOULS™ II: Scholar of the First Sin
+setManifestid(335301, "2962083928887153929", 12591415360)
+addappid(335302, 1, "7a10a8968977653c95ac43dc8e7799f02750283f840618910466e264e85f97c1") --Main Windows Depot DARK SOULS™ II: Scholar of the First Sin
+setManifestid(335302, "6445867072010600238", 12588346960)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
