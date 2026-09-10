50401bd55e1a6b9b58ded386baf5c9ae3e490b8c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 13:56:32 +0800

    add 2899050.lua

--- 2899050.lua
+++ 2899050.lua
@@ -1,3 +1,6 @@
-addappid(2899050)
-addappid(2899051,0,"a6cff3e9d965f5096403263df0125bd18d78589c82e7642e33f2f3ab3c2be3e1")
-setManifestid(2899051,"361248128083373848")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2899050.lua
+--Gamename Desert Stalker
+addappid(2899050, 1, "3bd1034b5a6b7717b0ab1657691ef003df8568b75b9e5e3db834f56396876ce6") --Mainappid Desert Stalker
+addappid(2899051, 1, "a6cff3e9d965f5096403263df0125bd18d78589c82e7642e33f2f3ab3c2be3e1") --Main Windows Depot Desert Stalker
+setManifestid(2899051, "4119041597343114332", 7834318000)
