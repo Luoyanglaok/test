1feb25defa8e52e68c6a8d5db7c6d76b6d217f7e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:36:48 +0800

    add 1401730.lua

--- 1401730.lua
+++ 1401730.lua
@@ -1,3 +1,11 @@
-addappid(1401730)
-addappid(1401731,0,"9feb5500bb35f27058f1c86e3a465a1f4904b2f5abe5ff891a9928cb702fea64")
-setManifestid(1401731,"4909139820887088013")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1401730.lua
+--Gamename Crashlands 2
+addappid(1401730, 1, "0bdf2ef71917e76968fcf96f04ae46b6e69a3c89826f8cc899e3873ed26254cd") --Mainappid Crashlands 2
+addappid(1401731, 1, "9feb5500bb35f27058f1c86e3a465a1f4904b2f5abe5ff891a9928cb702fea64") --Main Windows Depot Crashlands 2
+setManifestid(1401731, "6212194981627031861", 281461664)
+--Dlcname Crashlands 2 - Digital Art and Design Book
+addappid(3665380) --Dlcname Crashlands 2 - Digital Art and Design Book
+addappid(3665381, 1, "5081b78d2530d0ce043516c1fead2a7b5e41450f15e5910c1ad20e0e3d8dd28c") --Dlc Windows Depot Crashlands 2 - Digital Art and Design Book
+setManifestid(3665381, "6792396351188340778", 114968864)
+--Share Depots
