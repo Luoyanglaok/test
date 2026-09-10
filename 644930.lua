67f4befe80e89c688ebc933799f32817ef56b42b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:58:02 +0800

    add 644930.lua

--- 644930.lua
+++ 644930.lua
@@ -1,7 +1,14 @@
-addappid(644930)
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-addappid(644931,0,"44dfb497f7adb842912ea368c2535be6c163e2d25e1a4a9b06f682f60508a673")
-setManifestid(644931,"5324599310992417746")
-addappid(1100130,0,"58b125475d5c892070bc9b783a235987f0838d35e87719d52be3a3036b15955e")
-setManifestid(1100130,"287668829472044349")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 644930.lua
+--Gamename They Are Billions
+addappid(644930, 1, "4fd3e48b43b5c2f4a31038f55ce533a9bfe9f59175ffdc3b8da2084d0954a9bc") --Mainappid They Are Billions
+addappid(644931, 1, "44dfb497f7adb842912ea368c2535be6c163e2d25e1a4a9b06f682f60508a673") --Main Windows Depot They Are Billions
+setManifestid(644931, "8644553349467774858", 2197836448)
+--Dlcname They Are Billions - Soundtrack & Art Book
+addappid(1100130, 1, "58b125475d5c892070bc9b783a235987f0838d35e87719d52be3a3036b15955e") --Dlcname They Are Billions - Soundtrack & Art Book
+setManifestid(1100130, "3000532913243472563", 524273904)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
