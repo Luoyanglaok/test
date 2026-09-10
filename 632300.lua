25222b40d1c1514d0d1dd1aa78ccc67869287e14
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:30:22 +0800

    add 632300.lua

--- 632300.lua
+++ 632300.lua
@@ -1,13 +1,24 @@
-addappid(632300)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228987,"4302102680580581867")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229006,"1784011429307107530")
-addappid(632301,0,"f1e3666450afb59a43038cb7afb583a66b9fe73e79e1d99db59e02c91abda29e")
-setManifestid(632301,"2746440012569640965")
-addappid(632302,0,"a527ac8279aef11cc243871b0fc7ccd2a75f4824c0a1fbb1dbc2cf42e6e365b4")
-setManifestid(632302,"1658555888011903808")
-addappid(1255390,0,"cdf7211589ca4e6b4f300e61100324b6d46d63f067d323047ddeb5a34249f879")
-setManifestid(1255390,"7398123464269630519")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 632300.lua
+--Gamename Hobo: Tough Life
+addappid(632300) --Mainappid Hobo: Tough Life
+addappid(632301, 1, "f1e3666450afb59a43038cb7afb583a66b9fe73e79e1d99db59e02c91abda29e") --Main Windows Depot Hobo: Tough Life
+setManifestid(632301, "2746440012569640965", 8654535456)
+addappid(632302, 1, "a527ac8279aef11cc243871b0fc7ccd2a75f4824c0a1fbb1dbc2cf42e6e365b4") --Main Linux Depot Hobo: Tough Life
+setManifestid(632302, "1658555888011903808", 8625780704)
+--Dlcname Hobo: Tough Life - Soundtrack & Wallpapers
+addappid(1255390, 1, "cdf7211589ca4e6b4f300e61100324b6d46d63f067d323047ddeb5a34249f879") --Dlcname Hobo: Tough Life - Soundtrack & Wallpapers
+setManifestid(1255390, "7398123464269630519", 902166800)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
