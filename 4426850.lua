931e0317cd00b190e0983ac8080d69f4b1f392ae
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:08:59 +0800

    add 4426850.lua

---
+++ 4426850.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4426850.lua
+--Gamename Little Girl On Earth Asking Why She Asking Why
+addappid(4426850) --Mainappid Little Girl On Earth Asking Why She Asking Why
+addappid(4426851, 1, "0e3b21c27abef9f8afdc819c7703175cce9499a789cfe281d5e5932e2997bd8f") --Main Windows Language Depot English Little Girl On Earth Asking Why She Asking Why
+setManifestid(4426851, "4882433045543237851", 131349040)
