4d5a9e981c80094e432af7bb534a3bcb0493dbfa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:28:11 +0800

    add 2710480.lua

--- 2710480.lua
+++ 2710480.lua
@@ -1,6 +1,1 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 2710480.lua
---Gamename The Wife Next Door
-addappid(2710480, 1, "6021f76c5d3db206a898fe4abf64eead00ec420eb17bbb22deb76c9bbc9b75e6") --Mainappid The Wife Next Door
-addappid(2710481, 1, "7765451235157fe8932eeebf00a41af0f17b2267a35e3795492dd9b7b3931572") --Main Windows Depot The Wife Next Door
-setManifestid(2710481, "5762499936092396568", 12054611904)
+addappid(2710480)
