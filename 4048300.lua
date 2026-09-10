ae7a7efbd8bb77aae965d8a8e65fd749135ebdd4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:52:01 +0800

    add 4048300.lua

---
+++ 4048300.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4048300.lua
+--Gamename 緋月仙行錄
+addappid(4048300, 1, "995b3aa86a20f18d936d49b35c84de587a1975457b6b7458142419fdb7c15b14") --Mainappid 緋月仙行錄
+addappid(4048301, 1, "3cd301e2477af9d8360795892389f0a1ad3b953513271435f6ce27ad62c9ed03") --Main Windows Depot 緋月仙行錄
+setManifestid(4048301, "1276792124480682490", 2844059120)
+--Dlcname 緋月仙行錄 美術設定集
+addappid(4579960) --Dlcname 緋月仙行錄 美術設定集
+addappid(4579961, 1, "0263bac6a0758341a7b546e074566da3e9a74fcb74b1099374ec102971309092") --Dlc Windows Depot 緋月仙行錄 美術設定集
+setManifestid(4579961, "2360373448587687651", 462965888)
+--Missing Dlcs
+--Dlcname 緋月仙行錄 原聲帶
