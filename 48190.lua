1852bb9870343a947a4ec4cbbcbb0b98b77260db
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:59:35 +0800

    add 48190.lua

--- 48190.lua
+++ 48190.lua
@@ -1,22 +1,37 @@
-addappid(48190)
-setManifestid(228981,"7613356809904826842")
-setManifestid(228982,"6413394087650432851")
-setManifestid(228990,"1829726630299308803")
-addappid(48193,0,"92734ca4ad50878769fab352e45b48131110f53494ecc4708db34b7e0ce942c1")
-setManifestid(48193,"3500960106426649477")
-addappid(48192,0,"8cabeaf38cd688184bab6bdbbd34d0dbce4c751eaaf7c7f0878847359bc6543a")
-setManifestid(48192,"8303807335979433554")
-addappid(48191,0,"07b66cd56c14adc4368fc36622308a39eb66af717af87f63a706fb5095cc1058")
-setManifestid(48191,"6641652349099836879")
-addappid(48194,0,"46f9e1c0a5284ed561b7f88c1105921c5be7d5e281a1b4d6436c7ab5420ec029")
-setManifestid(48194,"3001580851652474584")
-addappid(48199,0,"5484ca1e7b74d883623916099c96dc87402fcd62247952d2fcef3e18633eadc3")
-setManifestid(48199,"1129412330587023805")
-addappid(48200,0,"74773d98cb00b55c6bf498196ef57756d25f070ab28a25eb5dd8c998930dded6")
-setManifestid(48200,"883912898399357051")
-addappid(48201,0,"68693fb573b07160d0ea85fb3683c682d46e810a16a0013e22777dea8e4e843d")
-setManifestid(48201,"4071795358417878418")
-addappid(48202,0,"1cf3d330157141da11d9dc1ffaafaa9b72ad42a1f9418c9d994a916d1a8e5c15")
-setManifestid(48202,"639386459569464708")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 48190.lua
+--Gamename Assassin’s Creed® Brotherhood
+addappid(48190, 1, "0b4bc97148f0e7fb6e1ff087036c4c820dad1ad562adae57d8317cea6b64a0c1") --Mainappid Assassin’s Creed® Brotherhood
+addappid(48191, 1, "07b66cd56c14adc4368fc36622308a39eb66af717af87f63a706fb5095cc1058") --Main Windows Depot Assassin’s Creed® Brotherhood
+setManifestid(48191, "6641652349099836879", 8140332416)
+addappid(48192, 1, "8cabeaf38cd688184bab6bdbbd34d0dbce4c751eaaf7c7f0878847359bc6543a") --Main Windows Depot Assassin’s Creed® Brotherhood
+setManifestid(48192, "8303807335979433554", 0)
+addappid(48193, 1, "92734ca4ad50878769fab352e45b48131110f53494ecc4708db34b7e0ce942c1") --Main Macos Depot Assassin’s Creed® Brotherhood
+setManifestid(48193, "3500960106426649477", 0)
+addappid(48194, 1, "46f9e1c0a5284ed561b7f88c1105921c5be7d5e281a1b4d6436c7ab5420ec029") --Main Windows Depot Assassin’s Creed® Brotherhood
+setManifestid(48194, "3001580851652474584", 7992141920)
+addappid(48199, 1, "5484ca1e7b74d883623916099c96dc87402fcd62247952d2fcef3e18633eadc3") --Main Windows Language Depot Koreana Assassin’s Creed® Brotherhood
+setManifestid(48199, "1129412330587023805", 64)
+addappid(48200, 1, "74773d98cb00b55c6bf498196ef57756d25f070ab28a25eb5dd8c998930dded6") --Main Windows Language Depot Schinese Assassin’s Creed® Brotherhood
+setManifestid(48200, "883912898399357051", 64)
+addappid(48201, 1, "68693fb573b07160d0ea85fb3683c682d46e810a16a0013e22777dea8e4e843d") --Main Windows Language Depot Tchinese Assassin’s Creed® Brotherhood
+setManifestid(48201, "4071795358417878418", 64)
+addappid(48202, 1, "1cf3d330157141da11d9dc1ffaafaa9b72ad42a1f9418c9d994a916d1a8e5c15") --Main Windows Language Depot Polish Assassin’s Creed® Brotherhood
+setManifestid(48202, "639386459569464708", 64)
+--Dlcname Assassin's Creed Brotherhood - Standard CD Key
+addappid(48195, 1, "11fe276e9f8b098a5453da29e0331bc5282b7bc9ac1945d1ea183b75623a23dc") --Dlcname Assassin's Creed Brotherhood - Standard CD Key
+--Dlcname Assassin's Creed Brotherhood - Deluxe CD Key
+addappid(48196, 1, "c4077afce9a381d77c988d5f8722fe3978ce65e1644988b1548c8c679344c89a") --Dlcname Assassin's Creed Brotherhood - Deluxe CD Key
+--Dlcname Assassin's Creed Brotherhood - Standard CD Key Mac
+addappid(48197, 1, "0439d964c85aca7239738d403be2d1020e1fcb409efc6293205dca94740ba5a6") --Dlcname Assassin's Creed Brotherhood - Standard CD Key Mac
+--Dlcname Assassin's Creed Brotherhood - Deluxe CD Key Mac
+addappid(48198, 1, "1d7e61001768d53ad029220e86e8a146e3ef81dd476222cdbe97291078090f98") --Dlcname Assassin's Creed Brotherhood - Deluxe CD Key Mac
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
