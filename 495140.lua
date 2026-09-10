aa5ede43e15ea40aa44fb2b9961671d78ae80a0d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:43:25 +0800

    add 495140.lua

--- 495140.lua
+++ 495140.lua
@@ -1,3 +1,6 @@
-addappid(495140)
-addappid(495141,0,"78542431af43146b9f53910d3bf2450a1cebf5ce5166f7d05cf474135be186ea")
-setManifestid(495141,"8686507798773437685")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 495140.lua
+--Gamename NARUTO: Ultimate Ninja STORM
+addappid(495140, 1, "b5c12e551b65fd282ddd39b19d60687741141316e21312dd093490b46d02c3cb") --Mainappid NARUTO: Ultimate Ninja STORM
+addappid(495141, 1, "78542431af43146b9f53910d3bf2450a1cebf5ce5166f7d05cf474135be186ea") --Main Windows Depot NARUTO: Ultimate Ninja STORM
+setManifestid(495141, "8686507798773437685", 6503569328)
