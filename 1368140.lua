f2b769a41126dd55711bc27aad21ab46b006e782
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:55:43 +0800

    add 1368140.lua

--- 1368140.lua
+++ 1368140.lua
@@ -1,5 +1,11 @@
-addappid(1368140)
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-setManifestid(1368141,"8565312755185007464")
-setManifestid(1368142,"4050571179734322575")
-setManifestid(228989,"5753583882400741046")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1368140.lua
+--Gamename Corsair Cove
+addappid(1368140) --Mainappid Corsair Cove
+addappid(1368141, 1, "622125856d179fd026496cefcabadac4e6a40520051ae9ed5cb210a6f05d364d") --Main Windows Depot Corsair Cove
+setManifestid(1368141, "8565312755185007464", 17351813472)
+addappid(1368142, 1, "6a84f1a9c026c5fd7bbdf63013d95d0ec71fd06876a0703d56a4e2b55a98e587") --Main Windows Depot Corsair Cove
+setManifestid(1368142, "4050571179734322575", 0)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
