0998ef5fc7129d1c5863165f4346e81e85a65547
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:14:11 +0800

    add 1051310.lua

--- 1051310.lua
+++ 1051310.lua
@@ -1,9 +1,13 @@
-addappid(1051310)
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(228987)
-setManifestid(228987,"4302102680580581867")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1051311,0,"7703818b7761602a9381f5fc9e6aaa500b7497414ecd56d40ce1ceed9485157e")
-setManifestid(1051311,"3351318383076816840")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1051310.lua
+--Gamename Paper Dolls: Original / 纸人
+addappid(1051310, 1, "75bbb913ea742d0616148a6f8c7c333624bd2c39ad2891bb8f356842223373c4") --Mainappid Paper Dolls: Original / 纸人
+addappid(1051311, 1, "7703818b7761602a9381f5fc9e6aaa500b7497414ecd56d40ce1ceed9485157e") --Main Windows Depot Paper Dolls: Original / 纸人
+setManifestid(1051311, "3351318383076816840", 1969684848)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
