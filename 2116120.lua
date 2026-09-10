4fbacfea7a0a97f8978bd0b21a8fd8a400422319
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 13:07:42 +0800

    add 2116120.lua

--- 2116120.lua
+++ 2116120.lua
@@ -1,5 +1,14 @@
-addappid(2116120)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2116121,0,"8f80a1590d1be6bfdde9c800499b420fe786838c27c69a6572bbb75c85c48007")
-setManifestid(2116121,"2484602995622303249")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2116120.lua
+--Gamename Incursion Red River
+addappid(2116120, 1, "e8d8de54516b28bf4d096a77c70aa05add05a82eea917680858a79e1f3c04f53") --Mainappid Incursion Red River
+addappid(2116121, 1, "8f80a1590d1be6bfdde9c800499b420fe786838c27c69a6572bbb75c85c48007") --Main Windows Depot Incursion Red River
+setManifestid(2116121, "6672703203196539514", 30432157584)
+--Dlcname Incursion Red River - Supporter Edition
+addappid(3405150, 1, "66466595306f5647e00fca5435234afc25e2a2f757c1ab2d550917767e80b71e") --Dlcname Incursion Red River - Supporter Edition
+setManifestid(3405150, "1887001357310105880", 12880516272)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
