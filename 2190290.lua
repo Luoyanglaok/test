7691a9f21910e7a150e54602f592fec3b6dc1b09
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:41:16 +0800

    add 2190290.lua

--- 2190290.lua
+++ 2190290.lua
@@ -1,5 +1,9 @@
-addappid(2190290)
-addappid(2190291,0,"71d020d54704794ac8d42c08d087112ae12660a00337bac1132684a202a3cd69")
-setManifestid(2190291,"8683591363854769839")
-addappid(2208180,0,"aed056f91092aad76d4f85af3d5dbab6130ff7c5790bfcb6c5f94e364169aedd")
-setManifestid(2208180,"7719716198521518733")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2190290.lua
+--Gamename To Be or Not to Be
+addappid(2190290, 1, "da3cba3b454008ed3de6f92f5c4e1f09d8697e6ec56a62be957639dd39c51fbb") --Mainappid To Be or Not to Be
+addappid(2190291, 1, "71d020d54704794ac8d42c08d087112ae12660a00337bac1132684a202a3cd69") --Main Windows Depot To Be or Not to Be
+setManifestid(2190291, "8683591363854769839", 1498124336)
+--Dlcname To Be or Not to Be-Patch
+addappid(2208180, 1, "aed056f91092aad76d4f85af3d5dbab6130ff7c5790bfcb6c5f94e364169aedd") --Dlcname To Be or Not to Be-Patch
+setManifestid(2208180, "7719716198521518733", 397263520)
