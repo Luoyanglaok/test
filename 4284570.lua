75e59f21e834369753afee831f27d7f72124972f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:03:48 +0800

    add 4284570.lua

---
+++ 4284570.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4284570.lua
+--Gamename Blind Box Shop Simulator
+addappid(4284570) --Mainappid Blind Box Shop Simulator
+addappid(4284571, 1, "a9f6faa9cbb373355bf2dbf22dff47d653b97931f4217d85be439c201b8fc844") --Main Windows Depot Blind Box Shop Simulator
+setManifestid(4284571, "8129995218561214021", 1363885312)
+addappid(4284572, 1, "b771c82da93e97d53df74fe6002054cadbf5a545a1888c7842f4e3efa7b877fa") --Main Macos Depot Blind Box Shop Simulator
+setManifestid(4284572, "7335532120057122106", 1453652512)
