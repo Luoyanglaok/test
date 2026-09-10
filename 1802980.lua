f12efee42fbe66d683200e4daf836b1e6348f75c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 13:24:29 +0800

    add 1802980.lua

--- 1802980.lua
+++ 1802980.lua
@@ -1,5 +1,9 @@
-addappid(1802980)
-addappid(1802981,0,"ce79d48911d88af9e08e9e2ac469e6a7d51ef3b8c7f980846fc70fa3544f65cb")
-setManifestid(1802981,"8398967445980781695")
-addappid(2247300,0,"c8c394dd9b6596ad31644470cf6dbbd5084890ea500050059d7cdcb2551dd2c2")
-setManifestid(2247300,"7605012916581791084")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1802980.lua
+--Gamename Yihongyuan
+addappid(1802980, 1, "5b785e84cf52607ac29c66abd19a06abb528a0ae86872227ff7a138f75c771e1") --Mainappid Yihongyuan
+addappid(1802981, 1, "ce79d48911d88af9e08e9e2ac469e6a7d51ef3b8c7f980846fc70fa3544f65cb") --Main Windows Depot Yihongyuan
+setManifestid(1802981, "8398967445980781695", 258575520)
+--Dlcname Yihongyuan - patch
+addappid(2247300, 1, "c8c394dd9b6596ad31644470cf6dbbd5084890ea500050059d7cdcb2551dd2c2") --Dlcname Yihongyuan - patch
+setManifestid(2247300, "7605012916581791084", 141929376)
