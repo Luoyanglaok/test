70b5bdae2a71186b5f4e84899351bed04efc58ce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:43:50 +0800

    add 234670.lua

--- 234670.lua
+++ 234670.lua
@@ -1,14 +1,22 @@
-addappid(234670)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229002,"7260605429366465749")
-addappid(234671,0,"132185278774455f9ac417b8e8938131b4d9d8821ebd9c2bc0d02d9883069405")
-setManifestid(234671,"284665897609485612")
-addappid(234672,0,"393c2dd43be41a8598a40b492a0ad8a41baf91379b6fa948de855f7f94db31f3")
-setManifestid(234672,"2059070709147005474")
-addappid(234673,0,"68c6e38f4e45def7ae51cb544f9d433a7c0e8a21141dfeada88fec37b5b2f462")
-setManifestid(234673,"7628913490051072341")
-addappid(234674,0,"500459fca252625c182791972170e28807a7095bc745b9503a56cf5e5adea361")
-setManifestid(234674,"231737632292320580")
-addappid(543890,0,"025485ca898026fa8c5bb1088c516831d0c299276661cd847c0f8e5b8ed0bb05")
-setManifestid(543890,"1432168299579796780")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 234670.lua
+--Gamename NARUTO SHIPPUDEN: Ultimate Ninja STORM 3 Full Burst HD
+addappid(234670) --Mainappid NARUTO SHIPPUDEN: Ultimate Ninja STORM 3 Full Burst HD
+addappid(234671, 1, "132185278774455f9ac417b8e8938131b4d9d8821ebd9c2bc0d02d9883069405") --Main Windows Depot NARUTO SHIPPUDEN: Ultimate Ninja STORM 3 Full Burst HD
+setManifestid(234671, "284665897609485612", 0)
+addappid(234672, 1, "393c2dd43be41a8598a40b492a0ad8a41baf91379b6fa948de855f7f94db31f3") --Main Windows Depot NARUTO SHIPPUDEN: Ultimate Ninja STORM 3 Full Burst HD
+setManifestid(234672, "2059070709147005474", 0)
+addappid(234673, 1, "68c6e38f4e45def7ae51cb544f9d433a7c0e8a21141dfeada88fec37b5b2f462") --Main Windows Depot NARUTO SHIPPUDEN: Ultimate Ninja STORM 3 Full Burst HD
+setManifestid(234673, "7628913490051072341", 0)
+addappid(234674, 1, "500459fca252625c182791972170e28807a7095bc745b9503a56cf5e5adea361") --Main Windows Depot NARUTO SHIPPUDEN: Ultimate Ninja STORM 3 Full Burst HD
+setManifestid(234674, "231737632292320580", 0)
+--Dlcname NARUTO SHIPPUDEN: Ultimate Ninja STORM 3 Full Burst - HD -
+addappid(543890, 1, "025485ca898026fa8c5bb1088c516831d0c299276661cd847c0f8e5b8ed0bb05") --Dlcname NARUTO SHIPPUDEN: Ultimate Ninja STORM 3 Full Burst - HD -
+setManifestid(543890, "1432168299579796780", 8893890832)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
