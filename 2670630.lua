3013b1d77e2f914c6875a1c6e96f9eaaba7d6d71
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 02:00:02 +0800

    add 2670630.lua

--- 2670630.lua
+++ 2670630.lua
@@ -1,5 +1,15 @@
-addappid(2670630)
-addappid(2670631,0,"72fc26c211612ffaa7b30cdfea152be9628668260d7f8c375980e0f96e445701")
-setManifestid(2670631,"8104257649776192521")
-addappid(2670632,0,"05a026749d66983d1a8b484cee9c7e2e320926d365ae5fe9f916ca8e96c00e4a")
-setManifestid(2670632,"2770086332180486396")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2670630.lua
+--Gamename Supermarket Simulator
+addappid(2670630, 1, "506e8fb6ac803f12d68d4f15299eb63557c21ecada3e6e5a4081de6d1f05140e") --Mainappid Supermarket Simulator
+addappid(2670631, 1, "72fc26c211612ffaa7b30cdfea152be9628668260d7f8c375980e0f96e445701") --Main Windows/Linux Depot Supermarket Simulator
+setManifestid(2670631, "761419252710164832", 1195170160)
+addappid(2670632, 1, "05a026749d66983d1a8b484cee9c7e2e320926d365ae5fe9f916ca8e96c00e4a") --Main Macos Depot Supermarket Simulator
+setManifestid(2670632, "8789528567955695806", 1260135952)
+addappid(4053820) --Dlcname Supermarket Simulator - Clothing DLC
+addappid(4078560) --Dlcname Supermarket Simulator - Electronics DLC
+addappid(4078570) --Dlcname Supermarket Simulator - Hardware DLC
+addappid(4196060) --Dlcname Supermarket Simulator - Bakery DLC
+addappid(4196070) --Dlcname Supermarket Simulator - Ice Cream DLC
+addappid(4196080) --Dlcname Supermarket Simulator - Essentials DLC
+addappid(4199610) --Dlcname Supermarket Simulator - Vending Machine DLC
