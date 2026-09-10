f9c2e82bf94acd669c770ef4c72a10732fc03c38
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:04:09 +0800

    add 1010800.lua

--- 1010800.lua
+++ 1010800.lua
@@ -1,4 +1,9 @@
-addappid(1010800)
-setManifestid(229004,"5220958916987797232")
-addappid(1010801,0,"0db2fbe8820e5e607759b4b89fd60ad57486231bf88e4d7782557aff037fafc4")
-setManifestid(1010801,"4244151322939768084")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1010800.lua
+--Gamename Wallpaper Maker （造物主视频桌面）
+addappid(1010800) --Mainappid Wallpaper Maker （造物主视频桌面）
+addappid(1010801, 1, "0db2fbe8820e5e607759b4b89fd60ad57486231bf88e4d7782557aff037fafc4") --Main Windows Depot Wallpaper Maker （造物主视频桌面）
+setManifestid(1010801, "4244151322939768084", 265451360)
+--Share Depots
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
