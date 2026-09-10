71074b91715d71c6986808d58ff64c042db3c873
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 23:12:16 +0800

    add 1222140.lua

--- 1222140.lua
+++ 1222140.lua
@@ -1,3 +1,6 @@
-addappid(1222140)
-addappid(1222141,0,"d87af32872df297ec519d0e79b6e0afc327b09048c3414622606b54efc46f519")
-setManifestid(1222141,"7324084008489949045")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1222140.lua
+--Gamename Detroit: Become Human
+addappid(1222140, 1, "c86eb22d1ee19339760143055ce73a32b48e96f7dbd06d56db43b97a5fea5f30") --Mainappid Detroit: Become Human
+addappid(1222141, 1, "d87af32872df297ec519d0e79b6e0afc327b09048c3414622606b54efc46f519") --Main Windows Depot Detroit: Become Human
+setManifestid(1222141, "7324084008489949045", 54953473904)
