26b6074d83e4fb3e7da6369a17ae9caa5cab6a3c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 21:01:53 +0800

    add 1256670.lua

--- 1256670.lua
+++ 1256670.lua
@@ -1,5 +1,11 @@
-addappid(1256670)
-addappid(1256671,0,"00c735126632c6eadd65b68ea7eda8c5b53533b7667a651747c006bd102491fb")
-setManifestid(1256671,"96563032411238562")
-addappid(1735680,0,"7eec92553b09b2fd957f4be63eae32b05f0a496d42c8b311aea081606362b81c")
-setManifestid(1735680,"8865581939697287877")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1256670.lua
+--Gamename Library Of Ruina
+addappid(1256670, 1, "355c4d26e35405d2a7824b7a237c77b03f2da7856671bcb62383ead42108f5f9") --Mainappid Library Of Ruina
+addappid(1256671, 1, "00c735126632c6eadd65b68ea7eda8c5b53533b7667a651747c006bd102491fb") --Main Windows Depot Library Of Ruina
+setManifestid(1256671, "96563032411238562", 3411346480)
+--Dlcname ArtBook
+addappid(1735680, 1, "7eec92553b09b2fd957f4be63eae32b05f0a496d42c8b311aea081606362b81c") --Dlcname ArtBook
+setManifestid(1735680, "8865581939697287877", 1734381632)
+--Missing Dlcs
+--Dlcname Library Of Ruina Soundtrack
