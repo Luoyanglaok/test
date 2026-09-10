faa3859043cd3202e7242fbaf9da8df80ba9128d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:54:10 +0800

    add 3180070.lua

---
+++ 3180070.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3180070.lua
+--Gamename No, I'm not a Human
+addappid(3180070, 1, "1346713ff575420c8698cd7ea5b83fbe137e5bff8bfbe921f5a24b9ffaa46f3a") --Mainappid No, I'm not a Human
+addappid(3180071, 1, "46e2cd1b157fec728146480948f03addd84b05ad244310eea46c81c3ebb47971") --Main Windows Depot No, I'm not a Human
+setManifestid(3180071, "4459994972108349428", 901420464)
+--Dlcname No, I'm not a Human Soundtrack
+addappid(4514440) --Dlcname No, I'm not a Human Soundtrack
+addappid(4514441, 1, "5c7ebc2dc06f23fd99904c84c9a9477d767fdaae80c89c20ca992bc809a8104d") --Dlc Windows Depot No, I'm not a Human Soundtrack
+setManifestid(4514441, "4310637138269678385", 168581408)
+addappid(4514442, 1, "93366126aa47ba758dbf40fdf31a4997aaab72b4573cbdde99c6ae01d37a76b9") --Dlc Windows Depot No, I'm not a Human Soundtrack
+setManifestid(4514442, "8212616603800579006", 686678416)
+--Dlcname No, I'm not a Human - Digital Artbook
+addappid(4521370, 1, "3f3c9a236136947d6cde7ed40e08b1bd5ae08f26575fed762fac8b510dce0501") --Dlcname No, I'm not a Human - Digital Artbook
+setManifestid(4521370, "2742753597642686133", 194600464)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
