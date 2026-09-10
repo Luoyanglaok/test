32f0528dc42d1f23ee1f6159db7a879629b5751c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:59:04 +0800

    add 1929290.lua

---
+++ 1929290.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1929290.lua
+--Gamename Bits & Bops
+addappid(1929290, 1, "77e4135ba1847c076ce998aeb8d2c093cc84707b3e51e4f6444a3e7850bc677c") --Mainappid Bits & Bops
+addappid(1929291, 1, "91ae978375944c164d52183a42b80bcba7be0c3e9862e50167fe9820c43ac287") --Main Windows Depot Bits & Bops
+setManifestid(1929291, "4836465726223256507", 961810816)
+addappid(1929292, 1, "5c41ab13934489b51a533de03142ca6b8f47a1c5a53a51e10ad09083678729af") --Main Windows Depot Bits & Bops
+setManifestid(1929292, "7576994429001691570", 958112976)
+addappid(1929293, 1, "8ed9a633f088e50d10c11fedf9b4780cbbede6974f96d9502322ae84a2becc8c") --Main Macos Depot Bits & Bops
+setManifestid(1929293, "7591245809968537285", 969345328)
+addappid(1929294, 1, "7c1731d6590b9e7c43e6bfb3a413657bc494a857f00c283f40e18f0ab4a88a5c") --Main Linux Depot Bits & Bops
+setManifestid(1929294, "1010890559940960030", 957998512)
+--Dlcname Bits & Bops Soundtrack
+addappid(4201430) --Dlcname Bits & Bops Soundtrack
+addappid(4201431, 1, "4f2ceb46c3acc37920986223c3d900316adfba4f4c2cea96c43be0b7f45af5c5") --Dlc Windows Depot Bits & Bops Soundtrack
+setManifestid(4201431, "2257519196349875391", 51099472)
+addappid(4201432, 1, "66497b241bbc6fd84d0529ce4bae222bf239cd360613b13cf428ea6cc4523115") --Dlc Windows Depot Bits & Bops Soundtrack
+setManifestid(4201432, "516231066668844307", 688277104)
