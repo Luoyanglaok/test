e7f2587d429be2c77f3d801ab0fa273fd9f70488
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:43:03 +0800

    add 341800.lua

--- 341800.lua
+++ 341800.lua
@@ -1,13 +1,17 @@
-addappid(341800)
-addappid(228985)
-setManifestid(228985,"3966345552745568756")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(341804,0,"39749b7fbb22631d93260af41062aba00d0490e5aeff02e7c9f0258978f8be85")
-setManifestid(341804,"3201216360315509764")
-addappid(341801,0,"7a3268e19c324c33a765f5d6411b261b4025d30a399dc9cc1e279ec8d64f33c6")
-setManifestid(341801,"8711988109837842559")
-addappid(341802,0,"e2113a997e3ff5b772be46811bab8ec43ed7e423dc2737fc54626fbe2c2b49ef")
-setManifestid(341802,"5369102617851565549")
-addappid(341803,0,"8b152cd65ba73c1b340af9e6476ae1e2e9f288dcec76123797c0271bcdf89c5c")
-setManifestid(341803,"1012396589549340219")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 341800.lua
+--Gamename Keep Talking and Nobody Explodes
+addappid(341800, 1, "1affcd096e6e13525be1a4d54d0365223e983b2dd9e4af35e0e4198de31fae3f") --Mainappid Keep Talking and Nobody Explodes
+addappid(341801, 1, "7a3268e19c324c33a765f5d6411b261b4025d30a399dc9cc1e279ec8d64f33c6") --Main Windows Depot Keep Talking and Nobody Explodes
+setManifestid(341801, "4834851516304994678", 236131664)
+addappid(341802, 1, "e2113a997e3ff5b772be46811bab8ec43ed7e423dc2737fc54626fbe2c2b49ef") --Main Macos Depot Keep Talking and Nobody Explodes
+setManifestid(341802, "5369102617851565549", 147124864)
+addappid(341803, 1, "8b152cd65ba73c1b340af9e6476ae1e2e9f288dcec76123797c0271bcdf89c5c") --Main Linux Depot Keep Talking and Nobody Explodes
+setManifestid(341803, "1012396589549340219", 160432128)
+addappid(341804, 1, "39749b7fbb22631d93260af41062aba00d0490e5aeff02e7c9f0258978f8be85") --Main Windows Depot Keep Talking and Nobody Explodes
+setManifestid(341804, "322469379001954668", 233922800)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
