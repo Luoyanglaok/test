a1566bb02efa139d1858b3976d2a281a987199a9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:24:16 +0800

    add 858820.lua

--- 858820.lua
+++ 858820.lua
@@ -1,5 +1,14 @@
-addappid(858820)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(858821,0,"396cc73b4dbae399058e283549bd4e3a305081d85808c286ab9989f1c9d0bade")
-setManifestid(858821,"4523459070732657435")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 858820.lua
+--Gamename Tribes of Midgard
+addappid(858820) --Mainappid Tribes of Midgard
+addappid(858821, 1, "396cc73b4dbae399058e283549bd4e3a305081d85808c286ab9989f1c9d0bade") --Main Windows Depot Tribes of Midgard
+setManifestid(858821, "7381483353528618062", 6287205264)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1663080) --Dlcname Tribes of Midgard - Deluxe Content
+addappid(1669720) --Dlcname Tribes of Midgard - Pre-Order Content
+addtoken(1669720, "4477921538126104330")
