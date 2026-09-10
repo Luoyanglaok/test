a91e84d3d2b867fbae6127e0a909b95a97e933ff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:59:58 +0800

    add 383150.lua

--- 383150.lua
+++ 383150.lua
@@ -1,18 +1,25 @@
-addappid(383150)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(383151,0,"848765cbc4ad068b9676d33d38c720ad28c690b3aceceb467947e4564b95e9e4")
-setManifestid(383151,"8159924524727733546")
-addappid(383152,0,"2bbe10937bfd3798ca64c41702a970237c6316d80544d2f8636f344bd9f0f1f5")
-setManifestid(383152,"7318972889813881710")
-addappid(383153,0,"00b123ed897e65d3ed13036cbc16e867bfb0e31c14af87a2b3b35b425cef47d2")
-setManifestid(383153,"3530186727903563836")
-addappid(383154,0,"cfc88d94c4e825a0367280ca7395214ab6e6e73987ea66368b275b80a727b170")
-setManifestid(383154,"6391798167256944570")
-addappid(383155,0,"545717ce30503c9738098d4e5450da026acc272a575fe815d87d0cbcc600f1d9")
-setManifestid(383155,"5936660404927479705")
-addappid(383156,0,"1ef83354cda51c9e87f7901171d8ab5224fabf69e2605f550d47ed3ad1620bfd")
-setManifestid(383156,"7736023019209677579")
-addappid(383157,0,"eed0d30f6698a7562af48e6b88cd246befc2e76aa6fba153b63b952af19f41fb")
-setManifestid(383157,"6357865423433303878")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 383150.lua
+--Gamename Dead Island Definitive Edition
+addappid(383150, 1, "445d6c4614872cdb764fe89343820108b50d20aba18ed13f72ca937a50ddb82b") --Mainappid Dead Island Definitive Edition
+addappid(383151, 1, "848765cbc4ad068b9676d33d38c720ad28c690b3aceceb467947e4564b95e9e4") --Main Windows Depot Dead Island Definitive Edition
+setManifestid(383151, "8159924524727733546", 12387616)
+addappid(383152, 1, "2bbe10937bfd3798ca64c41702a970237c6316d80544d2f8636f344bd9f0f1f5") --Main Linux Depot Dead Island Definitive Edition
+setManifestid(383152, "7318972889813881710", 16163664)
+addappid(383153, 1, "00b123ed897e65d3ed13036cbc16e867bfb0e31c14af87a2b3b35b425cef47d2") --Main Windows Depot Dead Island Definitive Edition
+setManifestid(383153, "3530186727903563836", 2535116208)
+addappid(383154, 1, "cfc88d94c4e825a0367280ca7395214ab6e6e73987ea66368b275b80a727b170") --Main Windows Depot Dead Island Definitive Edition
+setManifestid(383154, "6391798167256944570", 2543586624)
+addappid(383155, 1, "545717ce30503c9738098d4e5450da026acc272a575fe815d87d0cbcc600f1d9") --Main Linux Depot Dead Island Definitive Edition
+setManifestid(383155, "5936660404927479705", 2549111104)
+addappid(383156, 1, "1ef83354cda51c9e87f7901171d8ab5224fabf69e2605f550d47ed3ad1620bfd") --Main Windows Depot Dead Island Definitive Edition
+setManifestid(383156, "7736023019209677579", 774802736)
+addappid(383157, 1, "eed0d30f6698a7562af48e6b88cd246befc2e76aa6fba153b63b952af19f41fb") --Main Windows Language Depot Russian Dead Island Definitive Edition
+setManifestid(383157, "6357865423433303878", 745454512)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
