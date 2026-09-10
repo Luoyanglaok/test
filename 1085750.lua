499b1ea4a1ef83fdcc501ee1b1bdd6c5d356d440
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:46:25 +0800

    add 1085750.lua

--- 1085750.lua
+++ 1085750.lua
@@ -1,3 +1,10 @@
-addappid(1085750)
-addappid(1085751,0,"accc6ade0d89c0229c7c9b2d5887fd67bdd4879979fdd516ae9535e3849d8c47")
-setManifestid(1085751,"4820627852967652881")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1085750.lua
+--Gamename Sex with Stalin
+addappid(1085750) --Mainappid Sex with Stalin
+addappid(1085751, 1, "accc6ade0d89c0229c7c9b2d5887fd67bdd4879979fdd516ae9535e3849d8c47") --Main Windows Depot Sex with Stalin
+setManifestid(1085751, "4820627852967652881", 11602333488)
+--Dlcname Sex with Stalin Soundtrack
+addappid(1458660) --Dlcname Sex with Stalin Soundtrack
+addappid(1458661, 1, "b960ff9ce0d8e0ff9007df72c6066b49be1db9e7f07c4385d3dc0d626095ac07") --Dlc Windows Depot Sex with Stalin Soundtrack
+setManifestid(1458661, "1072508444178109484", 130504960)
