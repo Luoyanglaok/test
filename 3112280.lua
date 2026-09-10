062f9f42e80095b656db7180996af3468fed7552
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:50:49 +0800

    add 3112280.lua

--- 3112280.lua
+++ 3112280.lua
@@ -1,2 +1,6 @@
-addappid(3112280)
-setManifestid(3112281,"6697050108002742371")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3112280.lua
+--Gamename Reclaiming the Lost
+addappid(3112280, 1, "99a12b2be7c8762fd44354b6658013759934a65da37fb0737d8fa0a75349a245") --Mainappid Reclaiming the Lost
+addappid(3112281, 1, "8a1787ce374cb2ba72de205cdebd655424fbba6db8a652615f408c873ff9f5c9") --Main Windows Language Depot English Reclaiming the Lost
+setManifestid(3112281, "6697050108002742371", 9722182784)
