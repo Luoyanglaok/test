08240cc6ee9cc9317f7fe2ba7d7608144acea554
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:41:11 +0800

    add 1139900.lua

--- 1139900.lua
+++ 1139900.lua
@@ -1,9 +1,23 @@
-addappid(1139900)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-addappid(1139901,0,"b1a4614bd74ba48ab2546570b5d66bc0d1380a4b1dce45acc370be5fc5fb9c1f")
-setManifestid(1139901,"732659811379795654")
-addappid(1741380,0,"f8bc115f6677008301df4a15b02a362d568e2b6e5e6b1443de1d146cb01892ba")
-setManifestid(1741380,"2198570130799634824")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1139900.lua
+--Gamename Ghostrunner
+addappid(1139900, 1, "b5628f62a987d51d0b00fa2e44ee9b7350875840b8eca368f0f3483ab0382520") --Mainappid Ghostrunner
+addappid(1139901, 1, "b1a4614bd74ba48ab2546570b5d66bc0d1380a4b1dce45acc370be5fc5fb9c1f") --Main Windows Depot Ghostrunner
+setManifestid(1139901, "732659811379795654", 16471087360)
+--Dlcname Ghostrunner Digital Artbook
+addappid(1741380, 1, "f8bc115f6677008301df4a15b02a362d568e2b6e5e6b1443de1d146cb01892ba") --Dlcname Ghostrunner Digital Artbook
+setManifestid(1741380, "2198570130799634824", 14196960)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(1480500) --Dlcname Ghostrunner - Winter Pack
+addappid(1542040) --Dlcname Ghostrunner - Metal OX Pack
+addappid(1647450) --Dlcname Ghostrunner - Neon Pack
+addappid(1760130) --Dlcname Ghostrunner - Halloween Pack
+addappid(1760190) --Dlcname Ghostrunner - Project_Hel
