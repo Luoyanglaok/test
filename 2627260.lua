42b8d2be6e16aa1be79da82a84547d13ea3771c9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 23:20:53 +0800

    add 2627260.lua

---
+++ 2627260.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2627260.lua
+--Gamename NINJA GAIDEN 4
+addappid(2627260, 1, "0a892e3e526a787e9c75452994764a88a34e66b7eac52146fd250a7223dfb182") --Mainappid NINJA GAIDEN 4
+addappid(2627261, 1, "1dbbfa4a1302ac713af5d572bd94e7d0708ba581ca184c4124588ff3fdc06481") --Main Windows Depot NINJA GAIDEN 4
+setManifestid(2627261, "6644170988571432626", 65493522032)
+addappid(3717100) --Dlcname NINJA GAIDEN 4 Deluxe Edition Content
+addappid(3717110) --Dlcname NINJA GAIDEN 4: Dark Dragon Descendant Yakumo Skin
+addappid(4191490) --Dlcname NINJA GAIDEN 4 The Two Masters
