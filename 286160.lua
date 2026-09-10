ed64d7ab3d8d4641d05a3c79c2a66af3c4c3c0ad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:11:23 +0800

    add 286160.lua

--- 286160.lua
+++ 286160.lua
@@ -1,13 +1,78 @@
-addappid(286160)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-addappid(286161,0,"44c1a2d71883ae73e086c3a9635ce91ce552c7f967b577cdfe7a371fbbe5a454")
-setManifestid(286161,"1722066204200192106")
-addappid(286164,0,"384bd8817c99571c89493aff2c944cdabfa94c61e03b976afc07ca9d3aafa3c6")
-setManifestid(286164,"8329441041205144090")
-addappid(286162,0,"1a643eb767c6bf74e7a80ce613d74244d133e1c53607e20d44ce9ca75740776b")
-setManifestid(286162,"1131220859142294814")
-addappid(286163,0,"cc860200bf120e6acf8e7b8ae107bf5c80a427cb0ef48911a954af6ac9593e0a")
-setManifestid(286163,"8052780359676064230")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 286160.lua
+--Gamename Tabletop Simulator
+addappid(286160, 1, "fa9a2dc0b376e08e7a782bab4154bbb5c4c28787750c4c6cf113fc75d0499fa0") --Mainappid Tabletop Simulator
+addappid(286161, 1, "44c1a2d71883ae73e086c3a9635ce91ce552c7f967b577cdfe7a371fbbe5a454") --Main Windows Depot Tabletop Simulator
+setManifestid(286161, "7369335255803394753", 1234074496)
+addappid(286162, 1, "1a643eb767c6bf74e7a80ce613d74244d133e1c53607e20d44ce9ca75740776b") --Main Macos Depot Tabletop Simulator
+setManifestid(286162, "5510635935292062034", 1221416464)
+addappid(286163, 1, "cc860200bf120e6acf8e7b8ae107bf5c80a427cb0ef48911a954af6ac9593e0a") --Main Linux Depot Tabletop Simulator
+setManifestid(286163, "4591767591742417822", 1358123312)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(367330) --Dlcname Tabletop Simulator - Superfight
+addappid(405130) --Dlcname Tabletop Simulator - Cosmic Encounter Connector
+addappid(417300) --Dlcname AppID 417300
+addappid(418880) --Dlcname Tabletop Simulator - Superfight: The Orange Deck
+addappid(418881) --Dlcname Tabletop Simulator - Cosmic Incursion
+addappid(425960) --Dlcname Tabletop Simulator - Mistfall
+addappid(425961) --Dlcname Tabletop Simulator - Superfight: The Red Deck
+addappid(433520) --Dlcname Tabletop Simulator - Darkest Night
+addappid(437590) --Dlcname Tabletop Simulator - Tiny Epic Galaxies
+addappid(438980) --Dlcname Tabletop Simulator - Battle For Souls
+addappid(443240) --Dlcname Tabletop Simulator - Cosmic Conflict
+addappid(446980) --Dlcname Tabletop Simulator - Scythe
+addappid(457180) --Dlcname Tabletop Simulator - RARRR!!
+addappid(458910) --Dlcname Tabletop Simulator - Tiny Epic Kingdoms
+addappid(468720) --Dlcname Tabletop Simulator - In the Name of Odin
+addappid(468721) --Dlcname Tabletop Simulator - Indonesia
+addappid(468722) --Dlcname Tabletop Simulator - Scuttle!
+addappid(468723) --Dlcname Tabletop Simulator - Warfighter
+addappid(468724) --Dlcname Tabletop Simulator - Spirits of the Rice Paddy
+addappid(468725) --Dlcname Tabletop Simulator - The Captain Is Dead
+addappid(468726) --Dlcname Tabletop Simulator - Zombicide
+addappid(468727) --Dlcname Tabletop Simulator - Mr. Game!
+addappid(468728) --Dlcname Tabletop Simulator - Cosmic Alliance
+addappid(468729) --Dlcname Tabletop Simulator - Simurgh
+addappid(468730) --Dlcname Tabletop Simulator - Wizard's Academy
+addappid(468731) --Dlcname Tabletop Simulator - Three Cheers For Master
+addappid(468732) --Dlcname Tabletop Simulator - Cosmic Storm
+addappid(468733) --Dlcname Tabletop Simulator - Viticulture
+addappid(468734) --Dlcname Tabletop Simulator - Tiny Epic Defenders
+addappid(468735) --Dlcname Tabletop Simulator - Darkrock Ventures
+addappid(468736) --Dlcname Tabletop Simulator - Tiny Epic Western
+addappid(468737) --Dlcname Tabletop Simulator - Xia: Legends of a Drift System
+addappid(468738) --Dlcname Tabletop Simulator - Cosmic Dominion
+addtoken(468738, "10353632270774983304")
+addappid(468739) --Dlcname Tabletop Simulator - Abraca...What?
+addappid(610690) --Dlcname Tabletop Simulator - Khronos Hunter
+addappid(610691) --Dlcname Tabletop Simulator - Cavern Tavern
+addappid(610692) --Dlcname Tabletop Simulator - Three Kingdoms Redux
+addappid(610693) --Dlcname Tabletop Simulator - The Red Dragon Inn: Battle For Greyport
+addappid(610694) --Dlcname Tabletop Simulator - The Great Dinosaur Rush
+addappid(610695) --Dlcname Tabletop Simulator - Pillars of Eternity: Lords of the Eastern Reach
+addappid(610696) --Dlcname Tabletop Simulator - Draco Magi
+addappid(610697) --Dlcname Tabletop Simulator - Tortuga 1667
+addappid(610698) --Dlcname Tabletop Simulator - Tiny Epic Quest
+addappid(610699) --Dlcname Tabletop Simulator - Deck Quest
+addappid(610700) --Dlcname Tabletop Simulator - Blood Rage
+addappid(610701) --Dlcname Tabletop Simulator - Boss Monster
+addappid(610702) --Dlcname Tabletop Simulator - Unearth
+addappid(610703) --Dlcname AppID 610703
+addappid(610704) --Dlcname Tabletop Simulator - Dungeon Drop
+addappid(610705) --Dlcname Tabletop Simulator - Dawn of the Zeds
+addappid(610706) --Dlcname Tabletop Simulator - Down in Flames: Locked-On
+addappid(610707) --Dlcname Tabletop Simulator - Mistborn: House War
+addappid(610708) --Dlcname Tabletop Simulator - One Night Ultimate Werewolf
+addappid(610709) --Dlcname Tabletop Simulator - Rise of Tribes
+addappid(1621110) --Dlcname Tabletop Simulator - Adventure Mart
+addappid(1621112) --Dlcname Tabletop Simulator - Bristol 1350
+addappid(1621113) --Dlcname Tabletop Simulator - Tiny Epic Dinosaurs
+addappid(1621114) --Dlcname Tabletop Simulator - Widget's Workshop
