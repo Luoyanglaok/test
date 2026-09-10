f9e6e5179c1b9665c617f65df3e2ffa417f2dd69
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 09:38:36 +0800

    add 1868140.lua

--- 1868140.lua
+++ 1868140.lua
@@ -1,6 +1,31 @@
-addappid(1868140)
-setManifestid(1868141,"1490153896524122226")
-setManifestid(2492320,"3605329027721964993")
-setManifestid(2677020,"1640036140298738282")
-setManifestid(2841140,"4277352553098425670")
-setManifestid(3543180,"7053194086829219553")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1868140.lua
+--Gamename DAVE THE DIVER
+addappid(1868140, 1, "c283d28f5a647e86857c71bbcd79f63db35febefefe20b121df6dc6a8721e673") --Mainappid DAVE THE DIVER
+addappid(1868141, 1, "dec1482ee88e2a62d7fddedf0e75c6abdef55d2884440de6508bef83044198d8") --Main Windows Depot DAVE THE DIVER
+setManifestid(1868141, "1490153896524122226", 4636283008)
+addappid(1868142, 1, "f55cec1c353af17ba294e0bc3c1af6766a91b76e97d59d44425873918058d6c5") --Main Macos Depot DAVE THE DIVER
+setManifestid(1868142, "7107957488412882591", 4460891520)
+--Dlcname DAVE THE DIVER Digital Extra
+addappid(2492320, 1, "80467121a86d6b38971889737a91b309bb3aa329fee8e93ac63f2748f43fa063") --Dlcname DAVE THE DIVER Digital Extra
+setManifestid(2492320, "3605329027721964993", 186146912)
+--Dlcname DAVE THE DIVER - DREDGE Content Pack
+addappid(2677020, 1, "0f12757b1a8cc78265828ba98e12b673ff97e942bbfd3ae07eca6d10f6b8dfcf") --Dlcname DAVE THE DIVER - DREDGE Content Pack
+setManifestid(2677020, "1640036140298738282", 22173664)
+addappid(2677021, 1, "794d4476c6913ad8568af8bf1303f216b1f16b3d6041f62834536fe460d661dd") --Dlc Macos Depot DAVE THE DIVER - DREDGE Content Pack
+setManifestid(2677021, "4629321012438313755", 22149632)
+--Dlcname DAVE THE DIVER - Godzilla Content Pack
+addappid(2841140, 1, "a5e4472216eb53c5abc48c617b42bc740a1fac4adf25659c183015a27f8849fb") --Dlcname DAVE THE DIVER - Godzilla Content Pack
+setManifestid(2841140, "4277352553098425670", 66864480)
+addappid(2841141, 1, "a6863e71aca974796ecc6423cb6d8b9820fd11e9df7f06bcbaa7a1926227f77d") --Dlc Macos Depot DAVE THE DIVER - Godzilla Content Pack
+setManifestid(2841141, "415104347584392154", 66792208)
+--Dlcname DAVE THE DIVER - Ichiban's Holiday Content Pack
+addappid(3543180, 1, "5dbf7b425cf6ac47d95a2abe30926e2c845f6b9fbf05903c27837624a56b6b66") --Dlcname DAVE THE DIVER - Ichiban's Holiday Content Pack
+setManifestid(3543180, "7053194086829219553", 195537120)
+addappid(3543181, 1, "fda6ac6e3da3645d6c445ad4c68bffd11640df814da698f3fb85db7ada2a388b") --Dlc Macos Depot DAVE THE DIVER - Ichiban's Holiday Content Pack
+setManifestid(3543181, "6888995605330603482", 192970032)
+--Dlcname DAVE THE DIVER - In the Jungle Content Pack
+addappid(4394810, 1, "3c8afacd8366ec6d0872b9f95be7bcc55784514c88e92d69b2b5ead5228bd9c8") --Dlcname DAVE THE DIVER - In the Jungle Content Pack
+setManifestid(4394810, "6080006835337499181", 0)
+addappid(4394811, 1, "07d0577ef5b0833c904eb284e46eeea83cfc030963793fdf43aac48c95cdbf1f") --Dlc Macos Depot DAVE THE DIVER - In the Jungle Content Pack
+setManifestid(4394811, "2071559694774481152", 664637152)
