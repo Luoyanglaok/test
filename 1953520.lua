382c9b2846a170023e898374d3589146ac7fd9b7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:07:18 +0800

    add 1953520.lua

--- 1953520.lua
+++ 1953520.lua
@@ -1,11 +1,20 @@
-addappid(1953520)
-addappid(228983)
-setManifestid(228983,"8124929965194586177")
-addappid(228984)
-setManifestid(228984,"2547553897526095397")
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1953521,0,"22a77bd226c9a356f60e7c333c8eba478eab6a48a4f312794e5d5a3ee118651a")
-setManifestid(1953521,"5461488580969915647")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1953520.lua
+--Gamename WRC Generations – The FIA WRC Official Game
+addappid(1953520) --Mainappid WRC Generations – The FIA WRC Official Game
+addappid(1953521, 1, "22a77bd226c9a356f60e7c333c8eba478eab6a48a4f312794e5d5a3ee118651a") --Main Windows Depot WRC Generations – The FIA WRC Official Game
+setManifestid(1953521, "5461488580969915647", 34529365168)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2074060) --Dlcname WRC Generations - Peugeot 206 WRC 2002
+addappid(2074070) --Dlcname WRC Generations - Citroën C4 WRC 2010
+addappid(2074080) --Dlcname WRC Generations - Career Starter Pack
+addappid(2074090) --Dlcname WRC Generations - Porsche 911 GT3 RS RGT Extra liveries
+addappid(2074100) --Dlcname WRC Generations - Livery editor extra items
