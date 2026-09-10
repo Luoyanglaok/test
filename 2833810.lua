f236a41ff166419a5709c73f49c9aa7f00e95fd6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:08:51 +0800

    add 2833810.lua

--- 2833810.lua
+++ 2833810.lua
@@ -1,3 +1,6 @@
-addappid(2833810)
-addappid(2833811,0,"af92047e19f01f59be907395bd1543cab4b9cdcf059f9c3bb8e75614ac182d83")
-setManifestid(2833811,"3941218627913802157")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2833810.lua
+--Gamename SEX Campus🔞
+addappid(2833810) --Mainappid SEX Campus🔞
+addappid(2833811, 1, "af92047e19f01f59be907395bd1543cab4b9cdcf059f9c3bb8e75614ac182d83") --Main Windows Depot SEX Campus🔞
+setManifestid(2833811, "3941218627913802157", 1679494592)
