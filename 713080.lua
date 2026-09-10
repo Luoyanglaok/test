739d89a05b66b0f89f8909ff28262e8d8e5912ed
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:09:24 +0800

    add 713080.lua

--- 713080.lua
+++ 713080.lua
@@ -1,9 +1,19 @@
-addappid(713080)
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229004,"5220958916987797232")
-setManifestid(229006,"1784011429307107530")
-addappid(713081,0,"2fdd224573f1c9e32cdf0bd014f845c6d115686eeeff4aa75c11bb82acbda963")
-setManifestid(713081,"6410339957515431688")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 713080.lua
+--Gamename President Evil
+addappid(713080) --Mainappid President Evil
+addappid(713081, 1, "2fdd224573f1c9e32cdf0bd014f845c6d115686eeeff4aa75c11bb82acbda963") --Main Windows Depot President Evil
+setManifestid(713081, "6410339957515431688", 256705008)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
