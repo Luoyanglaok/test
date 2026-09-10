af4ccf981e5ddb094216f71e39838191e37aedfa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 14:06:07 +0800

    add 285900.lua

--- 285900.lua
+++ 285900.lua
@@ -1,7 +1,15 @@
-addappid(285900)
-addappid(285901,0,"417b624d1798155e5d9bd1fe0e54fb15f6c418fe23caacc03e76866a6ef5d2f6")
-setManifestid(285901,"7232511415277304551")
-addappid(285902,0,"0c325cb5864b8f034bc692fa8a2741674d1b30c51d51acb91ff331498133435c")
-setManifestid(285902,"2524541732585644999")
-addappid(285903,0,"47dbec3990eb35d1b9f0e30306b05e5de90de70b1e134a4bbf651ca195de085f")
-setManifestid(285903,"751080414778357312")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 285900.lua
+--Gamename Gang Beasts
+addappid(285900, 1, "d5a3784e280306f122da4ab0a05de37374dbdb0f652b5aab6d614a145ee4ff28") --Mainappid Gang Beasts
+addappid(285901, 1, "417b624d1798155e5d9bd1fe0e54fb15f6c418fe23caacc03e76866a6ef5d2f6") --Main Windows Language Depot English Gang Beasts
+setManifestid(285901, "4905063432706063925", 946908960)
+addappid(285902, 1, "0c325cb5864b8f034bc692fa8a2741674d1b30c51d51acb91ff331498133435c") --Main Macos Language Depot English Gang Beasts
+setManifestid(285902, "3264082452678762042", 947689776)
+addappid(285903, 1, "47dbec3990eb35d1b9f0e30306b05e5de90de70b1e134a4bbf651ca195de085f") --Main Linux Language Depot English Gang Beasts
+setManifestid(285903, "1815274778430318286", 864293056)
+--Dlcname Gang Beasts Soundtrack
+addappid(1003800, 1, "bb91c8306d8c387437df923dc41555bf8dd18b89313125e483ee022fa1a77c73") --Dlcname Gang Beasts Soundtrack
+setManifestid(1003800, "5824623063958402171", 135859840)
+--Missing Dlcs
+--Dlcname Gang Beasts - Yogscast Charity Drive 2015
