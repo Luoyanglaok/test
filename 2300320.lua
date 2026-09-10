c301d2036a897210078c28290ff0e9ed0f0b8866
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:00:16 +0800

    add 2300320.lua

--- 2300320.lua
+++ 2300320.lua
@@ -1,59 +1,92 @@
-addappid(2300320)
-addappid(2300321,0,"e5e857b4939e3993f78ae1bfa1857aa4dca14c90680aec0e63f48565c885c2ae")
-setManifestid(2300321,"6058333694498242201")
-addappid(2300322,0,"e60dffe1f9b3a6f6fb4475045e0d66ec6a40410ffb036ce2dd37fd88c3acd6a5")
-setManifestid(2300322,"5979330087026300633")
-addappid(2300323,0,"3de49a7eee583d841ab3194df793f8db9fe186930c8623d2a864a81ff5586afd")
-setManifestid(2300323,"1550107122794757569")
-addappid(2300324,0,"f8938589cbd4ca4f7c8031839fe15759ac1c8c79ef2eaef69e6a00241703a2c5")
-setManifestid(2300324,"7082434029875972807")
-addappid(2300325,0,"f946e8f4ea8420aed7edd1f5918e81d77ce171e78ea11c54b7adeae8a4669a4d")
-setManifestid(2300325,"2519322278117707135")
-addappid(2300326,0,"cca076ed972ab800d4c8c6efafe281be83d39290660fe3a2b27603c21151da6c")
-setManifestid(2300326,"2699845844463245190")
-addappid(2300327,0,"639dd3cbaa8cbc16ccedb39098b2b8a039da4a992920ff2e368bc2c041b28cba")
-setManifestid(2300327,"8747416787258114339")
-addappid(2300328,0,"c121e9f50b5801a92cc456d3211620c4d920f3a0e75686f6184cb10bb562d904")
-setManifestid(2300328,"4898221753086484171")
-addappid(2300329,0,"f80f185138acffeef2e34fa3571b351e5551c4d705483aaabd9b5d15e74e75fb")
-setManifestid(2300329,"5932979496095422232")
-addappid(2981091,0,"968fe6780cc09c4baeda840f0411a94b5f1bab38dfca0f36c89fbe2331c294a8")
-setManifestid(2981091,"5292817651936743538")
-addappid(2981092,0,"e0288911696b43f3458119ec84de121e35abfe2d2322504ce777e37a018b4a33")
-setManifestid(2981092,"3986813650063916220")
-addappid(2981093,0,"29d185a1ecc003c97c77f0e8b70151d2f4ce3499db9e54ca2e85cbe1b2ff9070")
-setManifestid(2981093,"934605271199172290")
-addappid(2981094,0,"ea0932e50bea6317bc6a512361e03c2e454e9733c0a9a65a5290879eee89bf6b")
-setManifestid(2981094,"4467306760700940625")
-addappid(2981095,0,"ade1eac77aba845dd5816f2573f47b7ac39f5599834dc69c3b71af8f59bee8ec")
-setManifestid(2981095,"8694346490421832324")
-addappid(2981096,0,"863228b07a13e8d3eba1639ebaab3cc538e34672e7b3672c40197ffb6c896ba2")
-setManifestid(2981096,"8727505838908121317")
-addappid(2981097,0,"1ced5c6691d6aa866ca70e4708a6617186dc447bc154699a8ec16682c9cf165f")
-setManifestid(2981097,"6937195163401650241")
-addappid(2981098,0,"d1f521d8b6ae2b013a7e2a9f4c44766e025a4e0a6112ee90016c6f2968da9e24")
-setManifestid(2981098,"8829242856986735896")
-addappid(2981099,0,"655b7d0f6e8e38251a8eeec6f60e4ed47d5ddc3a50b3cacb2faca871417c9dc0")
-setManifestid(2981099,"3980285498065046994")
-addappid(2981101,0,"9f00d6bfe26a671b716b22bcfacf0ffa17e67b9960a7627a9c6d6a5448f93fd8")
-setManifestid(2981101,"6096155025109753201")
-addappid(2981102,0,"9eb91152a6aa2d434131b3551ccd34e79cdcd87baa77972f930e2e49f2727c16")
-setManifestid(2981102,"6773764835871137388")
-addappid(2981103,0,"9a3b1f5733c28c3ac4529ab6f6ae7c3c35639bd8adc5518066c1e9db0ac6a9d8")
-setManifestid(2981103,"2996502837207591963")
-addappid(2981104,0,"ae85b8e8c5a5ed430feea1df6a23f3bccca41ee70b4b77592b6f6551c956be81")
-setManifestid(2981104,"8290737112462894023")
-addappid(2981105,0,"41b3097f70f8b302773f6d473d438df683eba85bc6fedfca26e4b55f00388cec")
-setManifestid(2981105,"6806530335208221837")
-addappid(2981106,0,"8f6f1b07d0ff2a282fbaa971eb841ec96e4cea021abb7fdbb8b788a0c2bdd556")
-setManifestid(2981106,"2141179524058091071")
-addappid(2981107,0,"402e70f1a56080cfdac842c33ef75226c38fd20e3e390c6e67dcae0b01e28a95")
-setManifestid(2981107,"782481009953151439")
-addappid(2981090,0,"0651e56ae954c27ffee937a46ec72d5a6d9838157153e18713a5f5367b6655dd")
-setManifestid(2981090,"7847067090206129125")
-addappid(2981110,0,"c8311e383c31827910b45fd0ab017594f10b8c6395251b28c6c4fa0d57a8e7be")
-setManifestid(2981110,"2426592928577282527")
-addappid(3450870,0,"9a83c658cb248afaef38c4df9015a9c6e0bf9b2397e9dfff8dca12aa6e4d3a81")
-setManifestid(3450870,"4372243593270383365")
-addappid(3453950,0,"467c83ead3c8b40f90c044575fc18c1cd49f0acdd23423219f7ab35f19e30b02")
-setManifestid(3453950,"4479790015664922032")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2300320.lua
+--Gamename Farming Simulator 25
+addappid(2300320, 1, "7c0ac74f428b6c8d7c059b22347a2bcc3b6cc11ae5f142dba0327533ce49b52c") --Mainappid Farming Simulator 25
+addappid(2300321, 1, "e5e857b4939e3993f78ae1bfa1857aa4dca14c90680aec0e63f48565c885c2ae") --Main Windows Depot Farming Simulator 25
+setManifestid(2300321, "2942346977020221913", 23926248368)
+addappid(2300322, 1, "e60dffe1f9b3a6f6fb4475045e0d66ec6a40410ffb036ce2dd37fd88c3acd6a5") --Main Macos Depot Farming Simulator 25
+setManifestid(2300322, "8752823399378312131", 22375269312)
+addappid(2300323, 1, "3de49a7eee583d841ab3194df793f8db9fe186930c8623d2a864a81ff5586afd") --Main Windows Language Depot English Farming Simulator 25
+setManifestid(2300323, "1550107122794757569", 0)
+addappid(2300324, 1, "f8938589cbd4ca4f7c8031839fe15759ac1c8c79ef2eaef69e6a00241703a2c5") --Main Windows Language Depot German Farming Simulator 25
+setManifestid(2300324, "7082434029875972807", 0)
+addappid(2300325, 1, "f946e8f4ea8420aed7edd1f5918e81d77ce171e78ea11c54b7adeae8a4669a4d") --Main Windows Language Depot French Farming Simulator 25
+setManifestid(2300325, "2519322278117707135", 0)
+addappid(2300326, 1, "cca076ed972ab800d4c8c6efafe281be83d39290660fe3a2b27603c21151da6c") --Main Windows Language Depot Italian Farming Simulator 25
+setManifestid(2300326, "2699845844463245190", 0)
+addappid(2300327, 1, "639dd3cbaa8cbc16ccedb39098b2b8a039da4a992920ff2e368bc2c041b28cba") --Main Windows Language Depot Koreana Farming Simulator 25
+setManifestid(2300327, "8747416787258114339", 0)
+addappid(2300328, 1, "c121e9f50b5801a92cc456d3211620c4d920f3a0e75686f6184cb10bb562d904") --Main Windows Language Depot Spanish Farming Simulator 25
+setManifestid(2300328, "4898221753086484171", 0)
+addappid(2300329, 1, "f80f185138acffeef2e34fa3571b351e5551c4d705483aaabd9b5d15e74e75fb") --Main Windows Language Depot Schinese Farming Simulator 25
+setManifestid(2300329, "5932979496095422232", 0)
+addappid(2981091, 1, "968fe6780cc09c4baeda840f0411a94b5f1bab38dfca0f36c89fbe2331c294a8") --Main Windows Language Depot Tchinese Farming Simulator 25
+setManifestid(2981091, "5292817651936743538", 0)
+addappid(2981092, 1, "e0288911696b43f3458119ec84de121e35abfe2d2322504ce777e37a018b4a33") --Main Windows Language Depot Russian Farming Simulator 25
+setManifestid(2981092, "3986813650063916220", 0)
+addappid(2981093, 1, "29d185a1ecc003c97c77f0e8b70151d2f4ce3499db9e54ca2e85cbe1b2ff9070") --Main Windows Language Depot Japanese Farming Simulator 25
+setManifestid(2981093, "934605271199172290", 0)
+addappid(2981094, 1, "ea0932e50bea6317bc6a512361e03c2e454e9733c0a9a65a5290879eee89bf6b") --Main Windows Language Depot Portuguese Farming Simulator 25
+setManifestid(2981094, "4467306760700940625", 0)
+addappid(2981095, 1, "ade1eac77aba845dd5816f2573f47b7ac39f5599834dc69c3b71af8f59bee8ec") --Main Windows Language Depot Polish Farming Simulator 25
+setManifestid(2981095, "8694346490421832324", 0)
+addappid(2981096, 1, "863228b07a13e8d3eba1639ebaab3cc538e34672e7b3672c40197ffb6c896ba2") --Main Windows Language Depot Danish Farming Simulator 25
+setManifestid(2981096, "8727505838908121317", 0)
+addappid(2981097, 1, "1ced5c6691d6aa866ca70e4708a6617186dc447bc154699a8ec16682c9cf165f") --Main Windows Language Depot Dutch Farming Simulator 25
+setManifestid(2981097, "6937195163401650241", 0)
+addappid(2981098, 1, "d1f521d8b6ae2b013a7e2a9f4c44766e025a4e0a6112ee90016c6f2968da9e24") --Main Windows Language Depot Finnish Farming Simulator 25
+setManifestid(2981098, "8829242856986735896", 0)
+addappid(2981099, 1, "655b7d0f6e8e38251a8eeec6f60e4ed47d5ddc3a50b3cacb2faca871417c9dc0") --Main Windows Language Depot Norwegian Farming Simulator 25
+setManifestid(2981099, "3980285498065046994", 0)
+addappid(2981101, 1, "9f00d6bfe26a671b716b22bcfacf0ffa17e67b9960a7627a9c6d6a5448f93fd8") --Main Windows Language Depot Swedish Farming Simulator 25
+setManifestid(2981101, "6096155025109753201", 0)
+addappid(2981102, 1, "9eb91152a6aa2d434131b3551ccd34e79cdcd87baa77972f930e2e49f2727c16") --Main Windows Language Depot Hungarian Farming Simulator 25
+setManifestid(2981102, "6773764835871137388", 0)
+addappid(2981103, 1, "9a3b1f5733c28c3ac4529ab6f6ae7c3c35639bd8adc5518066c1e9db0ac6a9d8") --Main Windows Language Depot Czech Farming Simulator 25
+setManifestid(2981103, "2996502837207591963", 0)
+addappid(2981104, 1, "ae85b8e8c5a5ed430feea1df6a23f3bccca41ee70b4b77592b6f6551c956be81") --Main Windows Language Depot Romanian Farming Simulator 25
+setManifestid(2981104, "8290737112462894023", 0)
+addappid(2981105, 1, "41b3097f70f8b302773f6d473d438df683eba85bc6fedfca26e4b55f00388cec") --Main Windows Language Depot Turkish Farming Simulator 25
+setManifestid(2981105, "6806530335208221837", 0)
+addappid(2981106, 1, "8f6f1b07d0ff2a282fbaa971eb841ec96e4cea021abb7fdbb8b788a0c2bdd556") --Main Windows Language Depot Brazilian Farming Simulator 25
+setManifestid(2981106, "2141179524058091071", 0)
+addappid(2981107, 1, "402e70f1a56080cfdac842c33ef75226c38fd20e3e390c6e67dcae0b01e28a95") --Main Windows Language Depot Latam Farming Simulator 25
+setManifestid(2981107, "782481009953151439", 0)
+--Dlcname Farming Simulator 25: MacDon Pack
+addappid(2981090, 1, "0651e56ae954c27ffee937a46ec72d5a6d9838157153e18713a5f5367b6655dd") --Dlcname Farming Simulator 25: MacDon Pack
+setManifestid(2981090, "4030937932501187334", 104629600)
+--Dlcname Farming Simulator 25: New Holland CR11 Gold Edition
+addappid(2981110, 1, "c8311e383c31827910b45fd0ab017594f10b8c6395251b28c6c4fa0d57a8e7be") --Dlcname Farming Simulator 25: New Holland CR11 Gold Edition
+setManifestid(2981110, "2426592928577282527", 132608)
+--Dlcname Farming Simulator 25: NEXAT Pack
+addappid(3450870, 1, "9a83c658cb248afaef38c4df9015a9c6e0bf9b2397e9dfff8dca12aa6e4d3a81") --Dlcname Farming Simulator 25: NEXAT Pack
+setManifestid(3450870, "8018742123442168696", 491246640)
+--Dlcname Farming Simulator 25: Plains & Prairies Pack
+addappid(3453950, 1, "467c83ead3c8b40f90c044575fc18c1cd49f0acdd23423219f7ab35f19e30b02") --Dlcname Farming Simulator 25: Plains & Prairies Pack
+setManifestid(3453950, "9034799208246814175", 824254336)
+--Dlcname Farming Simulator 25: Mercedes-Benz Trucks Pack
+addappid(3453960, 1, "2316b4442225a84b1c071368fbca8259d035c27a00d6bd261001f6434429b8cc") --Dlcname Farming Simulator 25: Mercedes-Benz Trucks Pack
+setManifestid(3453960, "7125736555787811725", 599379040)
+--Dlcname Farming Simulator 25: Highlands Fishing Expansion
+addappid(3453970, 1, "bda31b60d517b56988250720f32f8e205ae01b8e0137f6e0c65150a0d7ff4a02") --Dlcname Farming Simulator 25: Highlands Fishing Expansion
+setManifestid(3453970, "7870183526494872168", 4830886896)
+--Dlcname Farming Simulator 25: Straw Harvest Pack
+addappid(3816190, 1, "c4bb89ebe43f8f1e8d233156a2b7ded8c2696eb186afbdfdbe7a58b9c53e9f97") --Dlcname Farming Simulator 25: Straw Harvest Pack
+setManifestid(3816190, "1491330788882535021", 255879200)
+--Dlcname Farming Simulator 25: JCB - WFT
+addappid(4054370, 1, "c184b40eca33e8c19bd297e07790abb95aaf1420d694646707ca7bedbd8cd6a7") --Dlcname Farming Simulator 25: JCB - WFT
+setManifestid(4054370, "2097302948663994711", 132640)
+--Dlcname Farming Simulator 25: Vredo Pack
+addappid(4348790, 1, "057b03766cf1beaefd052d018f8e29d180a83a7ba2db94f80b1b327c8e87f36a") --Dlcname Farming Simulator 25: Vredo Pack
+setManifestid(4348790, "7121350103423823809", 387373216)
+--Dlcname Farming Simulator 25: SKY Agriculture Pack
+addappid(4348800, 1, "df5f2f04b2da14360ec44a1038cead463089b72d6a30612b25bf3449558ceb5b") --Dlcname Farming Simulator 25: SKY Agriculture Pack
+setManifestid(4348800, "1988638044360980890", 276622032)
+--Dlcname Farming Simulator 25: Emergency Pack
+addappid(4651570, 1, "01db07db648105515fd656c7dba1789dab4308b0faeeb1c1ab9b69d3376cbd6e") --Dlcname Farming Simulator 25: Emergency Pack
+setManifestid(4651570, "818651558017765456", 334118912)
+addappid(2981100) --Dlcname Farming Simulator 25: Year 1 Season Pass
+addappid(3453810) --Dlcname Farming Simulator 25: Year 2 Season Pass
+addappid(4404070) --Dlcname Farming Simulator 25: Beans & Alpacas Expansion
+--Missing Dlcs
+--Dlcname Farming Simulator 25: Pumps n' Hoses Pack
