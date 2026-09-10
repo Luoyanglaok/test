bb00078c0143c43d54bd027f4a63d0627825e5ed
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:52:49 +0800

    add 3375110.lua

---
+++ 3375110.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3375110.lua
+--Gamename Tiny Eden
+addappid(3375110, 1, "0931ebd61f9fbe10c85642adfd885d77b3b0be04ab8a50e374522d647b007e24") --Mainappid Tiny Eden
+addappid(3375111, 1, "bba143af40bfa92b1a34d0912b4a57b125ef18f6016debcc54cfd592a5cb145b") --Main Windows Depot Tiny Eden
+setManifestid(3375111, "6925709232545477616", 3321407792)
+addappid(3375112, 1, "86b4af037855951e69e205b86cdfe1535be7b2c1c35bb6b2bb4bf942ef16648b") --Main Linux Depot Tiny Eden
+setManifestid(3375112, "8835714655946399819", 3533917872)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Tiny Eden Soundtrack
+--Dlcname Tiny Eden Goodies Pack
