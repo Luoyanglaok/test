d6b9ef22d1eaf878fa23b46e465687540c5850c5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:18:33 +0800

    add 2784470.lua

--- 2784470.lua
+++ 2784470.lua
@@ -1,5 +1,8 @@
-addappid(2784470)
-addappid(2784471,0,"3f1be54c32850828ed38015c593c4596fdec0af8820dc3e517efba88374d235e")
-setManifestid(2784471,"1063234433465552852")
-addappid(2784472,0,"cea0ff84008944b1cbc251283a8ded658adf149382bfaf80d3959d28ebb5734e")
-setManifestid(2784472,"124692137673302752")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2784470.lua
+--Gamename 9 Kings
+addappid(2784470, 1, "0e6ad66a31b96244a72f102bd71e14cb296805559d3fb374c7b440c24139c31a") --Mainappid 9 Kings
+addappid(2784471, 1, "3f1be54c32850828ed38015c593c4596fdec0af8820dc3e517efba88374d235e") --Main Windows Depot 9 Kings
+setManifestid(2784471, "8807414749283878166", 111999568)
+addappid(2784472, 1, "cea0ff84008944b1cbc251283a8ded658adf149382bfaf80d3959d28ebb5734e") --Main Macos Depot 9 Kings
+setManifestid(2784472, "3688702212760187539", 121605216)
