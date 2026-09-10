570da370f3b59111bda5ce4fb7bdbd2122b82957
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:09:21 +0800

    add 34330.lua

--- 34330.lua
+++ 34330.lua
@@ -1,84 +1,112 @@
-addappid(34330)
-setManifestid(228986,"8782296191957114623")
-addappid(34333,0,"940ffb223c110eb32e04fa8c97ed5514d9404540bdba0a1eb54621376d0741b7")
-setManifestid(34333,"1972056557494830740")
-addappid(34332,0,"ae8ff60604d56d148c2491b100dd0d90572a0d2f218aa7b616d12fed2f35fcbf")
-setManifestid(34332,"5881356124441497033")
-addappid(34331,0,"331d98adf1f18ee81fafd99f9bc1217c5e9cde84c74d1995aecaa5f020dcd4a1")
-setManifestid(34331,"190308019507430064")
-addappid(201278,0,"07e144f81158f5f1dfc5a12328f9b4d281a1e1eceb94835110df4a8f7a1a03e4")
-setManifestid(201278,"1545335483322603358")
-addappid(201271,0,"d9c1f6504fc1063881c3d328b6c231251e6cb5a567a37dc599d1991958ffafc1")
-setManifestid(201271,"3803329044277082064")
-addappid(34334,0,"0c50a3378b3489d46b8fa596974699e72201b403d91a9671b18de15057d6cd97")
-setManifestid(34334,"1890103565405039456")
-addappid(34335,0,"7dec20909cba1c48e881b2adf5947dd0dca0f6832ffc87bc8b2c4be9e7477928")
-setManifestid(34335,"7279334387996785775")
-addappid(34336,0,"be07942b438d659f16920e2288f7e881f4b9da7acad9b58c143c85fc3b1b309b")
-setManifestid(34336,"7236751957725420295")
-addappid(34337,0,"03e859576ca29a73fc4000e3eb729dc8cc62aeeba54575dabe637c6b0175d124")
-setManifestid(34337,"6104384797188190875")
-addappid(34338,0,"e639988f2850a5ed361758885dee750effc626a8ad28bcccd68d8bca60f7d5a0")
-setManifestid(34338,"2019551163166850947")
-addappid(34339,0,"af670eeb71b75c233b51759222a20e8fe03a7b2c53c315aa8c9707457f21cb2e")
-setManifestid(34339,"8741815696204403390")
-addappid(34340,0,"7001a192a51af7254a614ecf4ab4e3ca4d95d28570b9775ccdab99619aa2576c")
-setManifestid(34340,"8582852140904295544")
-addappid(34341,0,"b04cc3eb897c57f6e0e1488458911bf583db9b4cb64b6aa17eeaff7c0262354b")
-setManifestid(34341,"4726807682131071446")
-addappid(34362,0,"33b5f85834b61432e68195fd322d6d42ff585ec91e45e4310543460c9a1e80e2")
-setManifestid(34362,"2019728393033869948")
-addappid(34349,0,"5a60b1d4d4c5ae2bbec58750f4b7089e5c6f1ae432956d56b2c39cd26128a2f9")
-setManifestid(34349,"7434482738042123192")
-addappid(203711,0,"4b73f690b1c86e732c067e0dfecf42f96feb7b17a50d8accbca0ebfd4a2e1cc4")
-setManifestid(203711,"585301899018748156")
-addappid(203712,0,"9ea52337e7702cc76a642f19c540b862db744bb3e0360be87e38f65f17ea3df8")
-setManifestid(203712,"8760837523389306295")
-addappid(203713,0,"fe152f1ba73f301708ab728e300e84168aff955f6bcf1ad6bfed340f800bb74a")
-setManifestid(203713,"7294532923074008469")
-addappid(203714,0,"54c7ac1c049d8da4f7b4561a07224edf666a369bdc158730bf48e44d8b4da81f")
-setManifestid(203714,"518326888580160621")
-addappid(203715,0,"212fd0d7856e4ea82d6a9cf12a6dddf97cf7e371b1e25838bf372defb7e92621")
-setManifestid(203715,"1056051627441399654")
-addappid(203716,0,"a8c6f1c80cf18ab33527be8dea3855410606c9040506d023c9f594573bb93c9c")
-setManifestid(203716,"1925710679528494771")
-addappid(203717,0,"1657a36b02cf3531ed74c8ddf0a4053134e9bd169aa64b630a9fa0a59294be76")
-setManifestid(203717,"3935614286324550775")
-addappid(203718,0,"f3d16b521bab079af3811459de29a13e636933f7706dd3bf8f6c74004cb043f1")
-setManifestid(203718,"7093356102196200004")
-addappid(203719,0,"58a5182f2ae80c73ffec51dbd161fa60380e6e2f5925b85038b285143265b98a")
-setManifestid(203719,"14936802368121482")
-addappid(203720,0,"dafa2a9bd663c866c4b708e2592288dd3416801e001c0a5dbe3f142e8247c74a")
-setManifestid(203720,"8777279047211202439")
-addappid(203721,0,"1344d17cd26c43d849f1ba429c1aac2b10308703de3094bee7cc39df8cbb25d6")
-setManifestid(203721,"4196835143774225587")
-addappid(203722,0,"5ee59c1af92f6465272f8c5e0881d0c61be4cab151d8348c8b04243e03ffdcea")
-setManifestid(203722,"1317818465072882453")
-addappid(203723,0,"16e06bb282d3df7c391dde49701fd9ed4834274b6d5768e65d597bae2a158f57")
-setManifestid(203723,"4675046982445241787")
-addappid(203724,0,"655c88a48273c76d575a68e6f7399fead34eebf83f64791214c78f073ab2cfc4")
-setManifestid(203724,"930061958965971308")
-addappid(203726,0,"cb5f59b89fbe7abeb2d78ef4324cf757f7f5d1ede2ea8f8a2ff38fad624af488")
-setManifestid(203726,"3089071817864978857")
-addappid(203727,0,"ac818ac96960f30d137514a48c355667eff1876beb7b07a028ddc327e54579dc")
-setManifestid(203727,"306841927912146962")
-addappid(203728,0,"0d1996856e41c7ecd1156eda87fd24d5634d18b964bcd17af2a6bb3a314831e4")
-setManifestid(203728,"3266502893584101")
-addappid(203729,0,"38342020bab4615065666ff949c637aee6fb2f09e08a3f6d6dd18f8398771160")
-setManifestid(203729,"1017658135622700246")
-addappid(223182,0,"41bbf8b442b6d1963c6df75e35767131e5bb7a1dd84a2abb012b5739f7d5017d")
-setManifestid(223182,"496316355454709144")
-addappid(223184,0,"c3528ad73b32e277e66b751112033a892794eca22baff4655a87c3d9099713be")
-setManifestid(223184,"682921555830121021")
-addappid(223185,0,"7a3d3c700267f014d6c60f061189f49886baca6294abe0a79f903bec913f97c6")
-setManifestid(223185,"1109516620999867060")
-addappid(223186,0,"645a0f186d6892cc27ebaab56da1b4360300107200e8b014fbe10053102631c2")
-setManifestid(223186,"635506733625013845")
-addappid(223187,0,"c099e44efdc1c821541613847a77f97125cc084a858a80bc0f505d4eebf6f606")
-setManifestid(223187,"66859539443567871")
-addappid(223188,0,"4b5375a21105dd5f3e7e40986787c95b431b794628693b384263019427044d14")
-setManifestid(223188,"3594582976460501426")
-addappid(223183,0,"3c8b07f1e05541d51302fe250645d11ab3512413d17b6d00f47b0e84475c9a39")
-setManifestid(223183,"7739883907705681489")
-addappid(372531,0,"85fd9e7e0a0f07b17dcb14263b94e436306270f02200d7a6669de0ba70602504")
-setManifestid(372531,"1128077100884847891")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 34330.lua
+--Gamename Total War: SHOGUN 2
+addappid(34330, 1, "db752e6cae43e9d2d14ba61659d82c6484792aa8ca8d1fe37e386fed68195a22") --Mainappid Total War: SHOGUN 2
+addappid(34331, 1, "331d98adf1f18ee81fafd99f9bc1217c5e9cde84c74d1995aecaa5f020dcd4a1") --Main Windows Depot Total War: SHOGUN 2
+setManifestid(34331, "190308019507430064", 41269712)
+addappid(34332, 1, "ae8ff60604d56d148c2491b100dd0d90572a0d2f218aa7b616d12fed2f35fcbf") --Main Windows Depot Total War: SHOGUN 2
+setManifestid(34332, "5881356124441497033", 12703578928)
+addappid(34333, 1, "940ffb223c110eb32e04fa8c97ed5514d9404540bdba0a1eb54621376d0741b7") --Main Windows Depot Total War: SHOGUN 2
+setManifestid(34333, "1972056557494830740", 0)
+addappid(34334, 1, "0c50a3378b3489d46b8fa596974699e72201b403d91a9671b18de15057d6cd97") --Main Windows Language Depot English Total War: SHOGUN 2
+setManifestid(34334, "1890103565405039456", 518010992)
+addappid(34335, 1, "7dec20909cba1c48e881b2adf5947dd0dca0f6832ffc87bc8b2c4be9e7477928") --Main Windows Language Depot Czech Total War: SHOGUN 2
+setManifestid(34335, "7279334387996785775", 506268240)
+addappid(34336, 1, "be07942b438d659f16920e2288f7e881f4b9da7acad9b58c143c85fc3b1b309b") --Main Windows Language Depot French Total War: SHOGUN 2
+setManifestid(34336, "7236751957725420295", 476098480)
+addappid(34337, 1, "03e859576ca29a73fc4000e3eb729dc8cc62aeeba54575dabe637c6b0175d124") --Main Windows Language Depot German Total War: SHOGUN 2
+setManifestid(34337, "6104384797188190875", 482521152)
+addappid(34338, 1, "e639988f2850a5ed361758885dee750effc626a8ad28bcccd68d8bca60f7d5a0") --Main Windows Language Depot Italian Total War: SHOGUN 2
+setManifestid(34338, "2019551163166850947", 527765856)
+addappid(34339, 1, "af670eeb71b75c233b51759222a20e8fe03a7b2c53c315aa8c9707457f21cb2e") --Main Windows Language Depot Polish Total War: SHOGUN 2
+setManifestid(34339, "8741815696204403390", 506026256)
+addappid(34340, 1, "7001a192a51af7254a614ecf4ab4e3ca4d95d28570b9775ccdab99619aa2576c") --Main Windows Language Depot Russian Total War: SHOGUN 2
+setManifestid(34340, "8582852140904295544", 515199888)
+addappid(34341, 1, "b04cc3eb897c57f6e0e1488458911bf583db9b4cb64b6aa17eeaff7c0262354b") --Main Windows Language Depot Spanish Total War: SHOGUN 2
+setManifestid(34341, "4726807682131071446", 477190016)
+addappid(34349, 1, "5a60b1d4d4c5ae2bbec58750f4b7089e5c6f1ae432956d56b2c39cd26128a2f9") --Main Macos Depot Total War: SHOGUN 2
+setManifestid(34349, "7434482738042123192", 13913982944)
+addappid(34362, 1, "33b5f85834b61432e68195fd322d6d42ff585ec91e45e4310543460c9a1e80e2") --Main Windows Language Depot Japanese Total War: SHOGUN 2
+setManifestid(34362, "2019728393033869948", 0)
+addappid(203711, 1, "4b73f690b1c86e732c067e0dfecf42f96feb7b17a50d8accbca0ebfd4a2e1cc4") --Main Macos Depot Total War: SHOGUN 2
+setManifestid(203711, "585301899018748156", 50304768)
+addappid(203712, 1, "9ea52337e7702cc76a642f19c540b862db744bb3e0360be87e38f65f17ea3df8") --Main Macos Depot Total War: SHOGUN 2
+setManifestid(203712, "8760837523389306295", 193568192)
+addappid(203713, 1, "fe152f1ba73f301708ab728e300e84168aff955f6bcf1ad6bfed340f800bb74a") --Main Macos Depot Total War: SHOGUN 2
+setManifestid(203713, "7294532923074008469", 1325062080)
+addappid(203714, 1, "54c7ac1c049d8da4f7b4561a07224edf666a369bdc158730bf48e44d8b4da81f") --Main Macos Language Depot English Total War: SHOGUN 2
+setManifestid(203714, "518326888580160621", 515683504)
+addappid(203715, 1, "212fd0d7856e4ea82d6a9cf12a6dddf97cf7e371b1e25838bf372defb7e92621") --Main Macos Language Depot Czech Total War: SHOGUN 2
+setManifestid(203715, "1056051627441399654", 5428976)
+addappid(203716, 1, "a8c6f1c80cf18ab33527be8dea3855410606c9040506d023c9f594573bb93c9c") --Main Macos Language Depot French Total War: SHOGUN 2
+setManifestid(203716, "1925710679528494771", 4938544)
+addappid(203717, 1, "1657a36b02cf3531ed74c8ddf0a4053134e9bd169aa64b630a9fa0a59294be76") --Main Macos Language Depot German Total War: SHOGUN 2
+setManifestid(203717, "3935614286324550775", 5084016)
+addappid(203718, 1, "f3d16b521bab079af3811459de29a13e636933f7706dd3bf8f6c74004cb043f1") --Main Macos Language Depot Italian Total War: SHOGUN 2
+setManifestid(203718, "7093356102196200004", 5133136)
+addappid(203719, 1, "58a5182f2ae80c73ffec51dbd161fa60380e6e2f5925b85038b285143265b98a") --Main Macos Language Depot Polish Total War: SHOGUN 2
+setManifestid(203719, "14936802368121482", 5361024)
+addappid(203720, 1, "dafa2a9bd663c866c4b708e2592288dd3416801e001c0a5dbe3f142e8247c74a") --Main Macos Language Depot Russian Total War: SHOGUN 2
+setManifestid(203720, "8777279047211202439", 2953072)
+addappid(203721, 1, "1344d17cd26c43d849f1ba429c1aac2b10308703de3094bee7cc39df8cbb25d6") --Main Macos Language Depot Spanish Total War: SHOGUN 2
+setManifestid(203721, "4196835143774225587", 4895776)
+addappid(203722, 1, "5ee59c1af92f6465272f8c5e0881d0c61be4cab151d8348c8b04243e03ffdcea") --Main Macos Language Depot Japanese Total War: SHOGUN 2
+setManifestid(203722, "1317818465072882453", 17762177424)
+addappid(203723, 1, "16e06bb282d3df7c391dde49701fd9ed4834274b6d5768e65d597bae2a158f57") --Main Linux Depot Total War: SHOGUN 2
+setManifestid(203723, "4675046982445241787", 13680311968)
+addappid(203724, 1, "655c88a48273c76d575a68e6f7399fead34eebf83f64791214c78f073ab2cfc4") --Main Linux Depot Total War: SHOGUN 2
+setManifestid(203724, "930061958965971308", 96017040)
+addappid(203725, 1, "14528b86d3399b0834f821e3b89cc07634d384b34bc18716f6b56e9616a2ca3b") --Main Linux Depot Total War: SHOGUN 2
+addappid(203726, 1, "cb5f59b89fbe7abeb2d78ef4324cf757f7f5d1ede2ea8f8a2ff38fad624af488") --Main Linux Depot Total War: SHOGUN 2
+setManifestid(203726, "3089071817864978857", 1581893712)
+addappid(203727, 1, "ac818ac96960f30d137514a48c355667eff1876beb7b07a028ddc327e54579dc") --Main Linux Language Depot English Total War: SHOGUN 2
+setManifestid(203727, "306841927912146962", 514523744)
+addappid(203728, 1, "0d1996856e41c7ecd1156eda87fd24d5634d18b964bcd17af2a6bb3a314831e4") --Main Linux Language Depot Czech Total War: SHOGUN 2
+setManifestid(203728, "3266502893584101", 501310736)
+addappid(203729, 1, "38342020bab4615065666ff949c637aee6fb2f09e08a3f6d6dd18f8398771160") --Main Linux Language Depot French Total War: SHOGUN 2
+setManifestid(203729, "1017658135622700246", 468590528)
+addappid(223182, 1, "41bbf8b442b6d1963c6df75e35767131e5bb7a1dd84a2abb012b5739f7d5017d") --Main Linux Language Depot German Total War: SHOGUN 2
+setManifestid(223182, "496316355454709144", 476663072)
+addappid(223183, 1, "3c8b07f1e05541d51302fe250645d11ab3512413d17b6d00f47b0e84475c9a39") --Main Linux Depot Total War: SHOGUN 2
+setManifestid(223183, "7739883907705681489", 5744)
+addappid(223184, 1, "c3528ad73b32e277e66b751112033a892794eca22baff4655a87c3d9099713be") --Main Linux Language Depot Italian Total War: SHOGUN 2
+setManifestid(223184, "682921555830121021", 523809216)
+addappid(223185, 1, "7a3d3c700267f014d6c60f061189f49886baca6294abe0a79f903bec913f97c6") --Main Linux Language Depot Polish Total War: SHOGUN 2
+setManifestid(223185, "1109516620999867060", 501240688)
+addappid(223186, 1, "645a0f186d6892cc27ebaab56da1b4360300107200e8b014fbe10053102631c2") --Main Linux Language Depot Russian Total War: SHOGUN 2
+setManifestid(223186, "635506733625013845", 510629744)
+addappid(223187, 1, "c099e44efdc1c821541613847a77f97125cc084a858a80bc0f505d4eebf6f606") --Main Linux Language Depot Spanish Total War: SHOGUN 2
+setManifestid(223187, "66859539443567871", 474294224)
+addappid(223188, 1, "4b5375a21105dd5f3e7e40986787c95b431b794628693b384263019427044d14") --Main Linux Language Depot Japanese Total War: SHOGUN 2
+setManifestid(223188, "3594582976460501426", 0)
+--Dlcname Total War: SHOGUN 2
+addappid(201270, 1, "3b99b6d7cb570d07430876f99f9cf3f1e8a2112d176821d607fd5eb5c6f19b21") --Dlcname Total War: SHOGUN 2
+--Dlcname A Total War Saga: FALL OF THE SAMURAI
+addappid(201271, 1, "d9c1f6504fc1063881c3d328b6c231251e6cb5a567a37dc599d1991958ffafc1") --Dlcname A Total War Saga: FALL OF THE SAMURAI
+setManifestid(201271, "3803329044277082064", 0)
+--Dlcname Total War: SHOGUN 2 - Fall of the Samurai Soundtrack
+addappid(201278, 1, "07e144f81158f5f1dfc5a12328f9b4d281a1e1eceb94835110df4a8f7a1a03e4") --Dlcname Total War: SHOGUN 2 - Fall of the Samurai Soundtrack
+setManifestid(201278, "1545335483322603358", 0)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(372531, 1, "85fd9e7e0a0f07b17dcb14263b94e436306270f02200d7a6669de0ba70602504") --Share Windows Depot AppID 372530
+setManifestid(372531, "1128077100884847891", 57465616)
+addappid(34342) --Dlcname Total War: SHOGUN 2 - Sengoku Jidai Unit Pack DLC
+addappid(34343) --Dlcname Total War: SHOGUN 2 - Rise of the Samurai Campaign DLC
+addappid(34344) --Dlcname Total War: Shogun 2 - Unused DLC
+addappid(34345) --Dlcname Total War: SHOGUN 2 - Hattori Clan Pack DLC
+addappid(34346) --Dlcname Total War: SHOGUN 2 - Battle Bonus DLC
+addappid(34347) --Dlcname Total War: SHOGUN 2 - Currency Bonus DLC
+addappid(34348) --Dlcname Total War: SHOGUN 2 - Ikko Ikki Clan Pack DLC
+addappid(201272) --Dlcname Total War: SHOGUN 2 - Blood Pack DLC
+addappid(201273) --Dlcname Total War: SHOGUN 2 - Fall of the Samurai - Saga Faction Pack DLC
+addappid(201274) --Dlcname Total War: SHOGUN 2 - Fall of the Samurai - Obama Faction Pack DLC
+addappid(201275) --Dlcname Total War: SHOGUN 2 - Fall of the Samurai - Tsu Faction Pack DLC
+addappid(201276) --Dlcname Total War: SHOGUN 2 - Fall of the Samurai - Sendai Faction Pack DLC
+addappid(201277) --Dlcname Total War: SHOGUN 2 - Dragon War Battle Pack
+addappid(201279) --Dlcname Total War: SHOGUN 2 - Saints and Heroes DLC
+addappid(203710) --Dlcname AppID 203710
+addappid(223180) --Dlcname Total War: SHOGUN 2 - Otomo Clan Pack
+addappid(223181) --Dlcname SHOGUN 2 Otomo Clan Pack DLC Pre-Purchase
+addappid(2028023) --Dlcname A Total War Saga: FALL OF THE SAMURAI – Blood Pack
