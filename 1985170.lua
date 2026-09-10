807fc5f7ef027371195cab5d9b938d4f33799b50
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:49:37 +0800

    add 1985170.lua

--- 1985170.lua
+++ 1985170.lua
@@ -1,5 +1,11 @@
-addappid(1985170)
-addappid(1985171,0,"bf9c88dd8eaed9fee6529f71c7b823f921af2bbb78a3294b32b684dff3be59fd")
-setManifestid(1985171,"1227390310174900652")
-addappid(1985172,0,"69b09e52d4beb8b1a3b9473bd63cda34d661a692f2d7a6cee2f481ce14e99fd2")
-setManifestid(1985172,"5737655050487543361")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1985170.lua
+--Gamename Warhammer 40,000: Warpforge
+addappid(1985170) --Mainappid Warhammer 40,000: Warpforge
+addappid(1985171, 1, "bf9c88dd8eaed9fee6529f71c7b823f921af2bbb78a3294b32b684dff3be59fd") --Main Windows Depot Warhammer 40,000: Warpforge
+setManifestid(1985171, "3250194520209525594", 1665242512)
+addappid(1985172, 1, "69b09e52d4beb8b1a3b9473bd63cda34d661a692f2d7a6cee2f481ce14e99fd2") --Main Macos Depot Warhammer 40,000: Warpforge
+setManifestid(1985172, "4011473470443165103", 1721615264)
+addappid(2961950) --Dlcname Warhammer 40,000: Warpforge - Sororitas starter bundle
+addappid(3694510) --Dlcname Warhammer 40,000:Warpforge - Emperor's Children starter bundle
+addappid(4053510) --Dlcname Warhammer 40,000: Warpforge - Space Wolves starter bundle
