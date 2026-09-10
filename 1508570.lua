9f1d88e0a91090863aab60d8458792a27ec8a2be
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:52:12 +0800

    add 1508570.lua

--- 1508570.lua
+++ 1508570.lua
@@ -1,5 +1,11 @@
-addappid(1508570)
-addappid(1508571,0,"0fc8bbfd68f345b69c6afb453fb86b722789fb96fa1e3656964dac2a63f61e4d")
-setManifestid(1508571,"4400295007839308007")
-addappid(2107850,0,"19d4a80e52b5f6ab4bcbec10fc570b877a01decc50fba435f37fdc47f5afe98a")
-setManifestid(2107850,"3665604206449223863")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1508570.lua
+--Gamename Summer~Life in the Countryside~
+addappid(1508570, 1, "df2feb3f323ae7d76177fa2353b899c44235f73187634c4986cd159c68603ee8") --Mainappid Summer~Life in the Countryside~
+addappid(1508571, 1, "0fc8bbfd68f345b69c6afb453fb86b722789fb96fa1e3656964dac2a63f61e4d") --Main Windows Depot Summer~Life in the Countryside~
+setManifestid(1508571, "4400295007839308007", 225774048)
+--Dlcname Summer~Life in the Countryside~ +Outing
+addappid(2107850, 1, "19d4a80e52b5f6ab4bcbec10fc570b877a01decc50fba435f37fdc47f5afe98a") --Dlcname Summer~Life in the Countryside~ +Outing
+setManifestid(2107850, "3665604206449223863", 143960112)
+--Missing Dlcs
+--Dlcname 【Summer~Life in the Countryside~ ASMR】Lewd Days with My Longtime Friend
