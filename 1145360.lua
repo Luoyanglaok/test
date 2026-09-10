b8d14ceb6622d26e51b28e2fb9878ebb503c2785
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:49:57 +0800

    add 1145360.lua

--- 1145360.lua
+++ 1145360.lua
@@ -1,9 +1,20 @@
-addappid(1145360)
-setManifestid(228988,"6645201662696499616")
-setManifestid(229006,"1784011429307107530")
-addappid(1145361,0,"e7db1a4f43363b9d751ed9e888334353425704ea0db26c8434015e516a482f4d")
-setManifestid(1145361,"5173085471687919135")
-addappid(1145363,0,"b88219eb2aee190d115e532ef9feea08c69bbc53bb1f959a2c48f3739f871b31")
-setManifestid(1145363,"2572656066874346283")
-addappid(1145364,0,"c70c6707a01507e124d35ac0e1644ee6210f383a1ae759aa252478cbd1ca7e0e")
-setManifestid(1145364,"6896671991921324741")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1145360.lua
+--Gamename Hades
+addappid(1145360, 1, "2b98bb3c217e78aa086dec7c513fa38143633e1f5b041f12f4a72fe3b921be7c") --Mainappid Hades
+addappid(1145361, 1, "e7db1a4f43363b9d751ed9e888334353425704ea0db26c8434015e516a482f4d") --Main Windows Depot Hades
+setManifestid(1145361, "5173085471687919135", 10694072752)
+addappid(1145363, 1, "b88219eb2aee190d115e532ef9feea08c69bbc53bb1f959a2c48f3739f871b31") --Main Windows Depot Hades
+setManifestid(1145363, "2572656066874346283", 78070992)
+addappid(1145364, 1, "c70c6707a01507e124d35ac0e1644ee6210f383a1ae759aa252478cbd1ca7e0e") --Main Macos Depot Hades
+setManifestid(1145364, "6896671991921324741", 10705927552)
+--Dlcname Hades Original Soundtrack
+addappid(1206340, 1, "c9345233ddbba0dd102b637212b8a892325701e165ffdd994cf90c6794551d6a") --Dlcname Hades Original Soundtrack
+setManifestid(1206340, "1347596415684508815", 365702976)
+addappid(1206341, 1, "d0cbd567d11fca873af7c65482965f6ef164ad4b58b208408ba7a05e61ee49a6") --Dlc Windows Depot Hades Original Soundtrack
+setManifestid(1206341, "5338171915262780986", 1066937936)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
