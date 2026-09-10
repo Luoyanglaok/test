d540e1512c2b7505a2f18d00e02ac5bd956f7379
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:13:55 +0800

    add 262060.lua

--- 262060.lua
+++ 262060.lua
@@ -1,45 +1,59 @@
-addappid(262060)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228989,"3514306556860204959")
-addappid(262061,0,"80ffcb74d1590f74ea3d6bf442f54d2a0d21327bae63d6e0cfdad768c7c7b7c2")
-setManifestid(262061,"4634735682857893604")
-addappid(262062,0,"c573ff0fbec6f00c8ff86aafc2085945c6a0b934b6396b0c135574e1326f7470")
-setManifestid(262062,"5085728985686214661")
-addappid(262063,0,"29e00db53e36359014c7975e1991156a35c28410f92643c66fcab5a98239f741")
-setManifestid(262063,"1331318350316094833")
-addappid(262064,0,"2658baced519789ef83fa5c378667f2d6fa3e62c1c1901b3bc5d357cee4ecb78")
-setManifestid(262064,"4063048565598821932")
-addappid(262065,0,"1ed93be3fc9b6d8911781b16ee38ea20b01a601089cc8ea554a0acb19ef80a1e")
-setManifestid(262065,"6595151068437416797")
-addappid(262066,0,"5ac9e3783153c138b4fbe2ca2fad3c3fde61883c56b8a4a6945fd186c3503e1c")
-setManifestid(262066,"2780779887000175382")
-addappid(445700,0,"f49dbb1bf10058d543c577196757756572d32615971242e63d9a5a945903741b")
-setManifestid(445700,"560769545274183569")
-addappid(445701,0,"a2f4ee66ab860ba416c504648d2fdc24b25877abed72abc6a80bd5d80db45c58")
-setManifestid(445701,"1365831417421820010")
-addappid(445702,0,"06f8bafa494c20f0eaa74cfc34a237f907789911f7226d207b719ad12810d075")
-setManifestid(445702,"5301635625866171440")
-addappid(580100,0,"326ca1f3e2c2a5b128d19b9ea4cb0eeb4dd71767ba4649beb35b4964a2f3036d")
-setManifestid(580100,"6222557203718607465")
-addappid(580101,0,"ee5837a6d587a663372bb57cf2ab4527e8d590470905ab6c252ab2810641e0a4")
-setManifestid(580101,"1565657565947939877")
-addappid(580102,0,"1830572dced421bddf06c78c75d178d4c10185652567a7a527787c8db22fff2f")
-setManifestid(580102,"3978850777272547237")
-addappid(702540,0,"a072f106d99256a1419ba3b11cbcfe2b88690ebb337e0013b6658ecf70443c5f")
-setManifestid(702540,"5141506331153784611")
-addappid(702541,0,"23a51aba543af134313157cd260951b978d7fc9c7324ed2970a3d1a3981b093a")
-setManifestid(702541,"2273224435470672990")
-addappid(702542,0,"4ede4a6602034a69b581069259516b7e22bfffaa8ef452b07688ecbfa4bfe9b1")
-setManifestid(702542,"6448535821289548124")
-addappid(735730,0,"5aea239e9d2d39fb207060d004d83521f96e2c2376c7ee3292544057f3953333")
-setManifestid(735730,"621686432886124292")
-addappid(735731,0,"961483d44b60822f029a8a6f399aa9881c3b10bb551c55fc6cf2ef0907f878fa")
-setManifestid(735731,"4790693839981705963")
-addappid(735732,0,"ee6a70a5c052a74214bfe1965668567dc83abe1e87647d38c0dc676df1b385e0")
-setManifestid(735732,"6684691611074244159")
-addappid(1117860,0,"eade86a7d3e1fb1164eb5a1886ae5d93db775162aa411c3744c8a27aee496c9f")
-setManifestid(1117860,"3265268534229067774")
-addappid(1117861,0,"a50099d9d81bd78ea990cdae6108db35cc59dbe777dfca43db5e42c332d5c129")
-setManifestid(1117861,"7933257625620854203")
-addappid(1117862,0,"888d380eaa1cf07630bc75e55d54618a00649f08c5a3309ee49ec839ccc0cff6")
-setManifestid(1117862,"5402419707475563686")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 262060.lua
+--Gamename Darkest Dungeon®
+addappid(262060, 1, "74a1007ed0ae35bd91851091c3748151ab4bf964a3786929e9c9cbce2b9a72af") --Mainappid Darkest Dungeon®
+addappid(262061, 1, "80ffcb74d1590f74ea3d6bf442f54d2a0d21327bae63d6e0cfdad768c7c7b7c2") --Main Windows Depot Darkest Dungeon®
+setManifestid(262061, "6061527905072634926", 1540374368)
+addappid(262062, 1, "c573ff0fbec6f00c8ff86aafc2085945c6a0b934b6396b0c135574e1326f7470") --Main Windows Depot Darkest Dungeon®
+setManifestid(262062, "2752702469922723321", 40967344)
+addappid(262063, 1, "29e00db53e36359014c7975e1991156a35c28410f92643c66fcab5a98239f741") --Main Macos Depot Darkest Dungeon®
+setManifestid(262063, "654433740633253024", 21479456)
+addappid(262064, 1, "2658baced519789ef83fa5c378667f2d6fa3e62c1c1901b3bc5d357cee4ecb78") --Main Macos Depot Darkest Dungeon®
+setManifestid(262064, "2842070835785182415", 1613417824)
+addappid(262065, 1, "1ed93be3fc9b6d8911781b16ee38ea20b01a601089cc8ea554a0acb19ef80a1e") --Main Linux Depot Darkest Dungeon®
+setManifestid(262065, "8201359037676991345", 48715536)
+addappid(262066, 1, "5ac9e3783153c138b4fbe2ca2fad3c3fde61883c56b8a4a6945fd186c3503e1c") --Main Linux Depot Darkest Dungeon®
+setManifestid(262066, "3206384093720084", 1641905840)
+--Dlcname Darkest Dungeon®: The Musketeer
+addappid(445700, 1, "f49dbb1bf10058d543c577196757756572d32615971242e63d9a5a945903741b") --Dlcname Darkest Dungeon®: The Musketeer
+setManifestid(445700, "1910576351267588826", 13538784)
+addappid(445701, 1, "a2f4ee66ab860ba416c504648d2fdc24b25877abed72abc6a80bd5d80db45c58") --Dlc Macos Depot Darkest Dungeon®: The Musketeer
+setManifestid(445701, "8167459794238687528", 13538784)
+addappid(445702, 1, "06f8bafa494c20f0eaa74cfc34a237f907789911f7226d207b719ad12810d075") --Dlc Linux Depot Darkest Dungeon®: The Musketeer
+setManifestid(445702, "303813516494152901", 13538784)
+--Dlcname Darkest Dungeon®: The Crimson Court
+addappid(580100, 1, "326ca1f3e2c2a5b128d19b9ea4cb0eeb4dd71767ba4649beb35b4964a2f3036d") --Dlcname Darkest Dungeon®: The Crimson Court
+setManifestid(580100, "3379207342614393360", 322587936)
+addappid(580101, 1, "ee5837a6d587a663372bb57cf2ab4527e8d590470905ab6c252ab2810641e0a4") --Dlc Macos Depot Darkest Dungeon®: The Crimson Court
+setManifestid(580101, "604327354194205489", 322587936)
+addappid(580102, 1, "1830572dced421bddf06c78c75d178d4c10185652567a7a527787c8db22fff2f") --Dlc Linux Depot Darkest Dungeon®: The Crimson Court
+setManifestid(580102, "8310052446613253636", 322587840)
+--Dlcname Darkest Dungeon®: The Shieldbreaker
+addappid(702540, 1, "a072f106d99256a1419ba3b11cbcfe2b88690ebb337e0013b6658ecf70443c5f") --Dlcname Darkest Dungeon®: The Shieldbreaker
+setManifestid(702540, "385638166620788848", 29843456)
+addappid(702541, 1, "23a51aba543af134313157cd260951b978d7fc9c7324ed2970a3d1a3981b093a") --Dlc Macos Depot Darkest Dungeon®: The Shieldbreaker
+setManifestid(702541, "6965914485267238717", 29843456)
+addappid(702542, 1, "4ede4a6602034a69b581069259516b7e22bfffaa8ef452b07688ecbfa4bfe9b1") --Dlc Linux Depot Darkest Dungeon®: The Shieldbreaker
+setManifestid(702542, "4258374143576351227", 29843456)
+--Dlcname Darkest Dungeon®: The Color of Madness
+addappid(735730, 1, "5aea239e9d2d39fb207060d004d83521f96e2c2376c7ee3292544057f3953333") --Dlcname Darkest Dungeon®: The Color of Madness
+setManifestid(735730, "3908148387796449307", 500973168)
+addappid(735731, 1, "961483d44b60822f029a8a6f399aa9881c3b10bb551c55fc6cf2ef0907f878fa") --Dlc Macos Depot Darkest Dungeon®: The Color of Madness
+setManifestid(735731, "4499433260725311056", 500973168)
+addappid(735732, 1, "ee6a70a5c052a74214bfe1965668567dc83abe1e87647d38c0dc676df1b385e0") --Dlc Linux Depot Darkest Dungeon®: The Color of Madness
+setManifestid(735732, "2077455249768182085", 500973088)
+--Dlcname Darkest Dungeon©: The Butcher's Circus
+addappid(1117860, 1, "eade86a7d3e1fb1164eb5a1886ae5d93db775162aa411c3744c8a27aee496c9f") --Dlcname Darkest Dungeon©: The Butcher's Circus
+setManifestid(1117860, "6376649910526554716", 380102720)
+addappid(1117861, 1, "a50099d9d81bd78ea990cdae6108db35cc59dbe777dfca43db5e42c332d5c129") --Dlc Macos Depot Darkest Dungeon©: The Butcher's Circus
+setManifestid(1117861, "2183525769666721026", 380098752)
+addappid(1117862, 1, "888d380eaa1cf07630bc75e55d54618a00649f08c5a3309ee49ec839ccc0cff6") --Dlc Linux Depot Darkest Dungeon©: The Butcher's Circus
+setManifestid(1117862, "5150525322187587414", 380098752)
+--Dlcname Darkest Dungeon®: The Fire's Edge
+addappid(4964110, 1, "fd09bbb4fd3ace258fdb9974422b5a31e11da17c43def2811fa61e293a6cc391") --Dlcname Darkest Dungeon®: The Fire's Edge
+setManifestid(4964110, "735598114277414029", 43429184)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
