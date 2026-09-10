04fbe52e3cdb985c8e873d17644204cfbcd678ef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:36:29 +0800

    add 704850.lua

--- 704850.lua
+++ 704850.lua
@@ -1,7 +1,19 @@
-addappid(704850)
-addappid(704851,0,"c6eb71b65618938bcf35d976cdd952c08d6fc53d1617ec8a408d9e8aedf5fb6f")
-setManifestid(704851,"6883475723616659058")
-addappid(704852,0,"7fdb57788d74395af527b98fb77483e1f82f93204f6dcd1d4ae5a424c4aa006b")
-setManifestid(704852,"6408742455698385061")
-addappid(1840650,0,"0c60b1aec120a0a8e452e02c449781b389f1fa7fa872611e338ad9a952c7bf1b")
-setManifestid(1840650,"6887987143828794476")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 704850.lua
+--Gamename Thief Simulator
+addappid(704850, 1, "feb0a17499266c95256522deb820d82bc0b2d7e028b864be8388eedea5df545b") --Mainappid Thief Simulator
+addappid(704851, 1, "c6eb71b65618938bcf35d976cdd952c08d6fc53d1617ec8a408d9e8aedf5fb6f") --Main Windows Depot Thief Simulator
+setManifestid(704851, "661864796988121626", 4656932096)
+addappid(704852, 1, "7fdb57788d74395af527b98fb77483e1f82f93204f6dcd1d4ae5a424c4aa006b") --Main Macos Depot Thief Simulator
+setManifestid(704852, "6408742455698385061", 3683657536)
+--Dlcname Thief Simulator - Luxury Houses DLC
+addappid(1840650, 1, "0c60b1aec120a0a8e452e02c449781b389f1fa7fa872611e338ad9a952c7bf1b") --Dlcname Thief Simulator - Luxury Houses DLC
+setManifestid(1840650, "1723464614183593186", 32939968)
+addappid(1840651, 1, "5df03d9500373d4270d8843f9afc0ade952c7a5aaa9cbabd9fea12ff94e7bf57") --Dlc Windows Depot Thief Simulator - Luxury Houses DLC
+setManifestid(1840651, "9136042689085792584", 32939968)
+addappid(1840652, 1, "458c5e1329ce6b9316da4a2571f6314809c7488855a14751e3881ba2307643a9") --Dlc Macos Depot Thief Simulator - Luxury Houses DLC
+setManifestid(1840652, "8451773674094920410", 32939968)
+--Dlcname Thief Simulator - Shopping Center DLC
+addappid(2518240) --Dlcname Thief Simulator - Shopping Center DLC
+addappid(2518242, 1, "e62e8d87295d6ae84bdaca81214762d03705ff7d09824166bf3edb48f923b455") --Dlc Windows Depot Thief Simulator - Shopping Center DLC
+setManifestid(2518242, "7350979375399521797", 15877312)
