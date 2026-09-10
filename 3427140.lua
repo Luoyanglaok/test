36c3bcb9ba90525383fa9c1bb17e2fa530de4f95
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:58:12 +0800

    add 3427140.lua

--- 3427140.lua
+++ 3427140.lua
@@ -1,3 +1,10 @@
-addappid(3427140)
-addappid(3427141,0,"08ac3f01aecb09eb5d0d50ca76895bcdc13381e17bc12cceddf487f67f0f41c2")
-setManifestid(3427141,"1919516440368383987")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3427140.lua
+--Gamename Aquilon Lust: Sex Arcana
+addappid(3427140) --Mainappid Aquilon Lust: Sex Arcana
+addappid(3427141, 1, "08ac3f01aecb09eb5d0d50ca76895bcdc13381e17bc12cceddf487f67f0f41c2") --Main Windows Depot Aquilon Lust: Sex Arcana
+setManifestid(3427141, "1919516440368383987", 184775824)
+--Missing Dlcs
+--Dlcname Aquilon Lust - Wallpapers pack 📰
+--Dlcname Aquilon Lust - Animation pack ▶️
+--Dlcname Aquilon Lust - Digital artbook 📖
