ffa556a8e1ccc01f55fdb5f6c2cb9c10c2468144
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:20:57 +0800

    add 2553870.lua

--- 2553870.lua
+++ 2553870.lua
@@ -1,7 +1,10 @@
-addappid(2553870)
-addappid(2553871,0,"7eb6f1e8beddbfaeb434476e4d1d7c2a53c9c3b799888085d89703ee3e4ec36a")
-setManifestid(2553871,"134942620736849302")
-addappid(2553872,0,"afe6f6bc2bb24e9465165b6d86b8b674125673c0a0fbccac0d978b8ed742832f")
-setManifestid(2553872,"3225395902680997725")
-addappid(2553873,0,"4f7903e435bc33dca3742bbe6899391caf0b3d0b8021fadd90d6c077b0d34fd3")
-setManifestid(2553873,"5853633659359000000")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2553870.lua
+--Gamename The Genesis Order
+addappid(2553870) --Mainappid The Genesis Order
+addappid(2553871, 1, "7eb6f1e8beddbfaeb434476e4d1d7c2a53c9c3b799888085d89703ee3e4ec36a") --Main Windows Depot The Genesis Order
+setManifestid(2553871, "134942620736849302", 12064712448)
+addappid(2553872, 1, "afe6f6bc2bb24e9465165b6d86b8b674125673c0a0fbccac0d978b8ed742832f") --Main Linux Depot The Genesis Order
+setManifestid(2553872, "3225395902680997725", 12066829344)
+addappid(2553873, 1, "4f7903e435bc33dca3742bbe6899391caf0b3d0b8021fadd90d6c077b0d34fd3") --Main Macos Depot The Genesis Order
+setManifestid(2553873, "5853633659359000000", 12071075104)
