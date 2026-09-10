0ce3eabfe2c7b566fb36079530a161f59e180053
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:41:47 +0800

    add 427100.lua

--- 427100.lua
+++ 427100.lua
@@ -1,71 +1,135 @@
-addappid(427100)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-addappid(427101,0,"565d1b14d850d4be0ee5a4304e7e23552107ff4279deb62d28a3b8d8bb279750")
-setManifestid(427101,"6209185531376152440")
-addappid(427102,0,"b7e3efea77ad621fb4c4c81b0aebe935f81c085bbaab5772290e98349b626736")
-setManifestid(427102,"3039167669064314363")
-addappid(427103,0,"7097279c0c5cae6cabe195457e63660ebdc0385e5ffa2310e76d884de55b8e71")
-setManifestid(427103,"3230596282498798904")
-addappid(427104,0,"9f5d13c02e8b037cb2f341f387c8e42d84bf91714632427072f21ae95a2b2a4d")
-setManifestid(427104,"299553215479527120")
-addappid(427105,0,"ab1d52e3740785f9ad45a4499b95669b1c926d2f799e33cf7017f2a16485c749")
-setManifestid(427105,"4068641150062983901")
-addappid(427106,0,"752c488131363f07898c253bf1144f453c458891be3d265d8d1daa2f3e30d054")
-setManifestid(427106,"4030910872037196629")
-addappid(427107,0,"2eb5875553aeb58b06eeb2be236551e9d13193ca40985f29b0d6ce34eaefe84f")
-setManifestid(427107,"5356177378879402684")
-addappid(427108,0,"aacb4339b64782a0053b45bffbd3953814cf60f6fc19d35814d5c010fc555a4a")
-setManifestid(427108,"6451832587382769975")
-addappid(427109,0,"19604c5550435da3b7f8eaa97bb1e477f77cb2877fa2b22a89e896c57b78df3b")
-setManifestid(427109,"576903352774981856")
-addappid(427110,0,"8ed03c8a144e76ff3248ead1d4bb29fa2cf4417d264482ef9365c0200f39916a")
-setManifestid(427110,"7766338427850198013")
-addappid(427111,0,"5936952c4a8fe4b0aa299d7cca335d0e856eefacbf10560f1f6f3abe62984d35")
-setManifestid(427111,"1100020261178760872")
-addappid(427112,0,"e6a182f13fa091ec5db7647abc76097700eda1588b990ffa87019c3b05691b56")
-setManifestid(427112,"6745388767416594167")
-addappid(427113,0,"a4d6953bc9b84551963e5793464cbfd9414ce824d0b695d183744e406a66e13d")
-setManifestid(427113,"3246510030392480116")
-addappid(427114,0,"42ac453f629862a02eff7597f5d607417aa0e2ce5f24d7d3f0663e3c0537b4bc")
-setManifestid(427114,"2108548880410610523")
-addappid(427115,0,"5ad0196361913422bfd5a777f6ebc19f0ecb8635aff5ecb5629a29feb58b6040")
-setManifestid(427115,"8129091546651496475")
-addappid(427116,0,"4b6f0229290c208601c15ac5e67013ba4eb6e0ecf2976dec8528a939ee35fd6b")
-setManifestid(427116,"1673878876618330691")
-addappid(427117,0,"35c397ce4c08698037401672c5754a3ef6b16536f4ea75e9ee93c525583323c8")
-setManifestid(427117,"921255585315331232")
-addappid(427118,0,"02eb02c4417a0cc7053dff6f8aab3e9dd02edeb775fbe698dda06bd7b8befc94")
-setManifestid(427118,"8609301819124552363")
-addappid(427119,0,"b7d98a195c431fee4c2a5369322a9b542a65d08f5d6aeed85060109a792a4dc5")
-setManifestid(427119,"1128993684715194911")
-addappid(569701,0,"7aeaaf11c528b61e7409aee2fb979e63a4cdd56b0dea3ce0c8e84f0967afdae4")
-setManifestid(569701,"8497909674161473596")
-addappid(569702,0,"e0de57ed2ef2e66e6fe3f59afcbaad4537bb32df5d447113aa0e676b32470462")
-setManifestid(569702,"2278123645898777675")
-addappid(569703,0,"962bb32ccc8db03a8fda8ac3d536fe09ba8db2e41c39ffe84af3fed8777aaf63")
-setManifestid(569703,"1588070790492004896")
-addappid(569704,0,"e393848b39a1a20f8f1ec171232f7a74d3cbc96ce94a5976b6252de12d42764a")
-setManifestid(569704,"5569204347930712098")
-addappid(569705,0,"309174c6e512a5264217f9b3fb739302ed6831acd51d20c1597382cba9c04b48")
-setManifestid(569705,"5188401913187414929")
-addappid(569706,0,"d67e720a9f88d76b5246aa6a6c9defa91b5377a6175b95b9f0802c6f1e710df6")
-setManifestid(569706,"7097420164809987061")
-addappid(569707,0,"b12b8fbdb9d5abb84106e9b52a71f45a8fd7877f58824722a4b31ed3f251f73f")
-setManifestid(569707,"7899180655766344622")
-addappid(569708,0,"710bbe21f5309cfb8b1fd454a425c163e2f8aca8eb9b747c327ed30ef3352224")
-setManifestid(569708,"8063992118282527498")
-addappid(602731,0,"fd84f9c4485c037310152b9ba1f30d1bc7606f7cf264e9cf8e4dda7d2370f902")
-setManifestid(602731,"5949501301683070979")
-addappid(602732,0,"189064a9e581545f4501cd9c6925a373452a8c31bdc8fdfc4d87223622b836a7")
-setManifestid(602732,"5838210037125358651")
-addappid(602733,0,"c10105523bf33022d83ee2308934a95686e2e77bc050482b74cf0944ce8f5955")
-setManifestid(602733,"2696001778401728315")
-addappid(602734,0,"8ffb4606d92321fda89368fb588256e832c67c8c7d9e501d49953c38ebdbf41f")
-setManifestid(602734,"2485359697634522798")
-addappid(602735,0,"e4dfd5eea5b8ac67bc1744d18cb44ece3baf51505a3978610c6514eb39fd1614")
-setManifestid(602735,"3648565053815618997")
-addappid(602736,0,"a74901d1de5f62541d4107cbb6134e35a56ee1af88774af451cc6005fd9241a0")
-setManifestid(602736,"6643946220833519592")
-addappid(602737,0,"3315af8e64235a41862a31842c1f822ac680ca69db1f37b19aafb5c68a8e17f3")
-setManifestid(602737,"1595810466924213972")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 427100.lua
+--Gamename Fernbus Simulator
+addappid(427100, 1, "6a8aab109ff6b1a3dfa8c0ee0b317ca59bf8ddaf98fcf14d1fd17784813c817b") --Mainappid Fernbus Simulator
+addappid(427101, 1, "565d1b14d850d4be0ee5a4304e7e23552107ff4279deb62d28a3b8d8bb279750") --Main Windows Depot Fernbus Simulator
+setManifestid(427101, "4248377372074980947", 30865011344)
+addappid(427118, 1, "02eb02c4417a0cc7053dff6f8aab3e9dd02edeb775fbe698dda06bd7b8befc94") --Main Windows Depot Fernbus Simulator
+setManifestid(427118, "6109109852265004324", 0)
+addappid(569708, 1, "710bbe21f5309cfb8b1fd454a425c163e2f8aca8eb9b747c327ed30ef3352224") --Main Windows Depot Fernbus Simulator
+setManifestid(569708, "8063992118282527498", 0)
+addappid(602731, 1, "fd84f9c4485c037310152b9ba1f30d1bc7606f7cf264e9cf8e4dda7d2370f902") --Main Windows Depot Fernbus Simulator
+setManifestid(602731, "5949501301683070979", 0)
+addappid(602732, 1, "189064a9e581545f4501cd9c6925a373452a8c31bdc8fdfc4d87223622b836a7") --Main Windows Depot Fernbus Simulator
+setManifestid(602732, "2491055244755427402", 138768544)
+addappid(602733, 1, "c10105523bf33022d83ee2308934a95686e2e77bc050482b74cf0944ce8f5955") --Main Windows Depot Fernbus Simulator
+setManifestid(602733, "311262967224024245", 135678864)
+addappid(602734, 1, "8ffb4606d92321fda89368fb588256e832c67c8c7d9e501d49953c38ebdbf41f") --Main Windows Depot Fernbus Simulator
+setManifestid(602734, "5537542002262606876", 135468384)
+addappid(602735, 1, "e4dfd5eea5b8ac67bc1744d18cb44ece3baf51505a3978610c6514eb39fd1614") --Main Windows Depot Fernbus Simulator
+setManifestid(602735, "2572297305743733873", 75548144)
+addappid(602736, 1, "a74901d1de5f62541d4107cbb6134e35a56ee1af88774af451cc6005fd9241a0") --Main Windows Depot Fernbus Simulator
+setManifestid(602736, "4013615258050169983", 89174576)
+addappid(602737, 1, "3315af8e64235a41862a31842c1f822ac680ca69db1f37b19aafb5c68a8e17f3") --Main Windows Depot Fernbus Simulator
+setManifestid(602737, "8287699278480207386", 200327264)
+--Dlcname Fernbus Simulator - Neoplan Skyliner
+addappid(569700) --Dlcname Fernbus Simulator - Neoplan Skyliner
+addappid(427102, 1, "b7e3efea77ad621fb4c4c81b0aebe935f81c085bbaab5772290e98349b626736") --Dlc Windows Depot Fernbus Simulator - Neoplan Skyliner
+setManifestid(427102, "3039167669064314363", 0)
+addappid(602730) --Dlcname AppID 602730
+addappid(427103, 1, "7097279c0c5cae6cabe195457e63660ebdc0385e5ffa2310e76d884de55b8e71") --Dlc Windows Depot AppID 602730
+setManifestid(427103, "504300840283808385", 67520)
+--Dlcname Fernbus Simulator - Anniversary Repaint Package
+addappid(612260) --Dlcname Fernbus Simulator - Anniversary Repaint Package
+addappid(427104, 1, "9f5d13c02e8b037cb2f341f387c8e42d84bf91714632427072f21ae95a2b2a4d") --Dlc Windows Depot Fernbus Simulator - Anniversary Repaint Package
+setManifestid(427104, "7633734006381174586", 0)
+--Dlcname Fernbus Simulator - Austria / Switzerland
+addappid(612261) --Dlcname Fernbus Simulator - Austria / Switzerland
+addappid(427105, 1, "ab1d52e3740785f9ad45a4499b95669b1c926d2f799e33cf7017f2a16485c749") --Dlc Windows Depot Fernbus Simulator - Austria / Switzerland
+setManifestid(427105, "7246446506076052446", 1011573296)
+--Dlcname Fernbus Simulator - France
+addappid(654120) --Dlcname Fernbus Simulator - France
+addappid(427106, 1, "752c488131363f07898c253bf1144f453c458891be3d265d8d1daa2f3e30d054") --Dlc Windows Depot Fernbus Simulator - France
+setManifestid(427106, "3132423170255669086", 5364357328)
+--Dlcname Fernbus Simulator - Usedom
+addappid(686280) --Dlcname Fernbus Simulator - Usedom
+addappid(427107, 1, "2eb5875553aeb58b06eeb2be236551e9d13193ca40985f29b0d6ce34eaefe84f") --Dlc Windows Depot Fernbus Simulator - Usedom
+setManifestid(427107, "7128356027763383957", 7459696)
+--Dlcname Fernbus Simulator - Comfort Class HD
+addappid(724420) --Dlcname Fernbus Simulator - Comfort Class HD
+addappid(427108, 1, "aacb4339b64782a0053b45bffbd3953814cf60f6fc19d35814d5c010fc555a4a") --Dlc Windows Depot Fernbus Simulator - Comfort Class HD
+setManifestid(427108, "6451832587382769975", 0)
+--Dlcname Fernbus Simulator - VDL Futura FHD2
+addappid(724440) --Dlcname Fernbus Simulator - VDL Futura FHD2
+addappid(427109, 1, "19604c5550435da3b7f8eaa97bb1e477f77cb2877fa2b22a89e896c57b78df3b") --Dlc Windows Depot Fernbus Simulator - VDL Futura FHD2
+setManifestid(427109, "576903352774981856", 0)
+--Dlcname Fernbus Simulator - Fußball Mannschaftsbus
+addappid(729490) --Dlcname Fernbus Simulator - Fußball Mannschaftsbus
+addappid(427110, 1, "8ed03c8a144e76ff3248ead1d4bb29fa2cf4417d264482ef9365c0200f39916a") --Dlc Windows Depot Fernbus Simulator - Fußball Mannschaftsbus
+setManifestid(427110, "4611760430510035333", 91761264)
+--Dlcname Fernbus Simulator - Rennsteig
+addappid(937720) --Dlcname Fernbus Simulator - Rennsteig
+addappid(427111, 1, "5936952c4a8fe4b0aa299d7cca335d0e856eefacbf10560f1f6f3abe62984d35") --Dlc Windows Depot Fernbus Simulator - Rennsteig
+setManifestid(427111, "4189120779132897904", 5116640)
+--Dlcname Fernbus Simulator - MAN Lion's Intercity
+addappid(1026740) --Dlcname Fernbus Simulator - MAN Lion's Intercity
+addappid(427112, 1, "e6a182f13fa091ec5db7647abc76097700eda1588b990ffa87019c3b05691b56") --Dlc Windows Depot Fernbus Simulator - MAN Lion's Intercity
+setManifestid(427112, "6745388767416594167", 0)
+--Dlcname Fernbus Simulator - BB40
+addappid(1121030) --Dlcname Fernbus Simulator - BB40
+addappid(427113, 1, "a4d6953bc9b84551963e5793464cbfd9414ce824d0b695d183744e406a66e13d") --Dlc Windows Depot Fernbus Simulator - BB40
+setManifestid(427113, "3246510030392480116", 0)
+--Dlcname Fernbus Simulator - Rhine Gorge
+addappid(1121090) --Dlcname Fernbus Simulator - Rhine Gorge
+addappid(427114, 1, "42ac453f629862a02eff7597f5d607417aa0e2ce5f24d7d3f0663e3c0537b4bc") --Dlc Windows Depot Fernbus Simulator - Rhine Gorge
+setManifestid(427114, "5641694344674979531", 138782256)
+--Dlcname Fernbus Simulator - Belgium
+addappid(1140660) --Dlcname Fernbus Simulator - Belgium
+addappid(427115, 1, "5ad0196361913422bfd5a777f6ebc19f0ecb8635aff5ecb5629a29feb58b6040") --Dlc Windows Depot Fernbus Simulator - Belgium
+setManifestid(427115, "1775679843697920144", 927304976)
+--Dlcname Fernbus Simulator - Netherlands
+addappid(1140661) --Dlcname Fernbus Simulator - Netherlands
+addappid(427116, 1, "4b6f0229290c208601c15ac5e67013ba4eb6e0ecf2976dec8528a939ee35fd6b") --Dlc Windows Depot Fernbus Simulator - Netherlands
+setManifestid(427116, "421168391668892853", 822442384)
+--Dlcname Fernbus Simulator - Luxembourg
+addappid(1140662) --Dlcname Fernbus Simulator - Luxembourg
+addappid(427117, 1, "35c397ce4c08698037401672c5754a3ef6b16536f4ea75e9ee93c525583323c8") --Dlc Windows Depot Fernbus Simulator - Luxembourg
+setManifestid(427117, "5885232301577337616", 149557360)
+--Dlcname Fernbus Simulator - MAN Lion's Coach 3rd Gen
+addappid(1154770) --Dlcname Fernbus Simulator - MAN Lion's Coach 3rd Gen
+addappid(569702, 1, "e0de57ed2ef2e66e6fe3f59afcbaad4537bb32df5d447113aa0e676b32470462") --Dlc Windows Depot Fernbus Simulator - MAN Lion's Coach 3rd Gen
+setManifestid(569702, "2278123645898777675", 0)
+--Dlcname Fernbus Simulator - Scania Touring
+addappid(1158120) --Dlcname Fernbus Simulator - Scania Touring
+addappid(427119, 1, "b7d98a195c431fee4c2a5369322a9b542a65d08f5d6aeed85060109a792a4dc5") --Dlc Windows Depot Fernbus Simulator - Scania Touring
+setManifestid(427119, "1128993684715194911", 0)
+--Dlcname Fernbus Simulator - W906
+addappid(1250730) --Dlcname Fernbus Simulator - W906
+addappid(569701, 1, "7aeaaf11c528b61e7409aee2fb979e63a4cdd56b0dea3ce0c8e84f0967afdae4") --Dlc Windows Depot Fernbus Simulator - W906
+setManifestid(569701, "8497909674161473596", 0)
+--Dlcname Fernbus Simulator - VDL Futura FDD2
+addtoken(1370010, "5186298009897529827")
+addappid(1370010) --Dlcname Fernbus Simulator - VDL Futura FDD2
+addappid(569703, 1, "962bb32ccc8db03a8fda8ac3d536fe09ba8db2e41c39ffe84af3fed8777aaf63") --Dlc Windows Depot Fernbus Simulator - VDL Futura FDD2
+setManifestid(569703, "1588070790492004896", 0)
+--Dlcname Fernbus Simulator - Denmark
+addappid(1446990) --Dlcname Fernbus Simulator - Denmark
+addappid(569704, 1, "e393848b39a1a20f8f1ec171232f7a74d3cbc96ce94a5976b6252de12d42764a") --Dlc Windows Depot Fernbus Simulator - Denmark
+setManifestid(569704, "9015109917612817746", 432443456)
+--Dlcname Fernbus Simulator - Czech
+addappid(1446991) --Dlcname Fernbus Simulator - Czech
+addappid(569705, 1, "309174c6e512a5264217f9b3fb739302ed6831acd51d20c1597382cba9c04b48") --Dlc Windows Depot Fernbus Simulator - Czech
+setManifestid(569705, "2502193686655789171", 826882000)
+--Dlcname Fernbus Simulator - Bavarian Castle
+addappid(1471310) --Dlcname Fernbus Simulator - Bavarian Castle
+addappid(569706, 1, "d67e720a9f88d76b5246aa6a6c9defa91b5377a6175b95b9f0802c6f1e710df6") --Dlc Windows Depot Fernbus Simulator - Bavarian Castle
+setManifestid(569706, "6830497696611338611", 3505232)
+--Dlcname Fernbus Simulator - Poland
+addappid(1635100) --Dlcname Fernbus Simulator - Poland
+addappid(569707, 1, "b12b8fbdb9d5abb84106e9b52a71f45a8fd7877f58824722a4b31ed3f251f73f") --Dlc Windows Depot Fernbus Simulator - Poland
+setManifestid(569707, "6312066808780020279", 2084001536)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(1847550) --Dlcname Fernbus Simulator - Top Class DD
+addappid(2294410) --Dlcname Fernbus Simulator - Interlink HD
+addappid(2294411) --Dlcname Fernbus Simulator - Altano TX
+addappid(2294412) --Dlcname Fernbus Simulator - Altano TDX
+addappid(2294413) --Dlcname Fernbus Simulator - Astromega
+addappid(2407420) --Dlcname Fernbus Simulator - Top Class HDH
+addappid(2407421) --Dlcname Fernbus Simulator - Neoplan Cityliner
+--Missing Dlcs
+--Dlcname Fernbus Simulator - England
+--Dlcname Fernbus Simulator - Tourism W410
