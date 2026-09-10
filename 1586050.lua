e4f6aa035f8c5945afad27d7559b2f2ba6745eb0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:37:53 +0800

    add 1586050.lua

--- 1586050.lua
+++ 1586050.lua
@@ -1,5 +1,9 @@
-addappid(1586050)
-addappid(1586051,0,"39536b0709ed31b391600612f165deb4d3c02da5d4eab0439b94c703c081da7d")
-setManifestid(1586051,"3279903303538025014")
-addappid(1652530,0,"ff9477ccea48c466beb2cc438464ad36dbd1acdb7f0c107046258eef39fcdedf")
-setManifestid(1652530,"7326891779087502082")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1586050.lua
+--Gamename CyberSex 2069
+addappid(1586050) --Mainappid CyberSex 2069
+addappid(1586051, 1, "39536b0709ed31b391600612f165deb4d3c02da5d4eab0439b94c703c081da7d") --Main Windows Depot CyberSex 2069
+setManifestid(1586051, "3279903303538025014", 648180368)
+--Dlcname CyberSex 2069 - Art Collection
+addappid(1652530, 1, "ff9477ccea48c466beb2cc438464ad36dbd1acdb7f0c107046258eef39fcdedf") --Dlcname CyberSex 2069 - Art Collection
+setManifestid(1652530, "7326891779087502082", 231259536)
