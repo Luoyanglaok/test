b89f94662eab3956d8752a6dbf4032cf54458c2b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 17:41:30 +0800

    add 2103140.lua

--- 2103140.lua
+++ 2103140.lua
@@ -1,3 +1,14 @@
-addappid(2103140)
-addappid(2103141,0,"f0ba119c48fc700dae769f7087e735d40dc7f3a1f379c01ff163a8386ae1b3ce")
-setManifestid(2103141,"8237188497619952451")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2103140.lua
+--Gamename Magicraft
+addappid(2103140, 1, "75d08dedb57cc72b24a757b7bbf4333017efe35b1d5735ac5b5266532802661e") --Mainappid Magicraft
+addappid(2103141, 1, "f0ba119c48fc700dae769f7087e735d40dc7f3a1f379c01ff163a8386ae1b3ce") --Main Windows Depot Magicraft
+setManifestid(2103141, "3374724404491369027", 627595504)
+addappid(2103144, 1, "8c28f65b28131414846d93c38ddcec659512404bccd93c58163f4b62cdc5a6c4") --Main Windows Depot Magicraft
+setManifestid(2103144, "5383460820466663275", 599913360)
+addappid(4099830) --Dlcname 魔法工艺 潜水员戴夫 Content Pack
+addtoken(4099830, "17599615563912186545")
+addappid(4100710) --Dlcname 魔法工艺 - 万圣派对 Halloween Costume
+addtoken(4100710, "14941122213920764900")
+addappid(4293030) --Dlcname 魔法工艺 - 新春贺岁 Spring Festival Costume
+addappid(4838370) --Dlcname 魔法工艺 - 清爽夏日 Cool Summer Costume
