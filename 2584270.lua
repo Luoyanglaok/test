fb26bcc7daffe1780bdf2ae910acae40a8918f95
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:52:24 +0800

    add 2584270.lua

---
+++ 2584270.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2584270.lua
+--Gamename Mortal Shell II
+addappid(2584270) --Mainappid Mortal Shell II
+addappid(2584271, 1, "bb59e6dc30df8728a4bfcc3129ca9b8284ed3808d42a9a272e65d4904b4bc445") --Main Windows Depot Mortal Shell II
+setManifestid(2584271, "4010694604290439344", 69803772768)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4711720) --Dlcname Mortal Shell II: Devout Edition Upgrade
+addtoken(4711720, "14235145898889595086")
+addappid(4711730) --Dlcname Mortal Shell II - Pre-order Bonus
+addtoken(4711730, "2594855405444440804")
