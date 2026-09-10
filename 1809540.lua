2122f76f31878cad8fa378c50552d688b3e6fb08
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 19:52:32 +0800

    add 1809540.lua

--- 1809540.lua
+++ 1809540.lua
@@ -1,5 +1,19 @@
-addappid(1809540)
-addappid(1809541,0,"6c41504f601faddc7ec40aff4688b9ee9e34c37f2ab46b882880cbcc76a8eb40")
-setManifestid(1809541,"9212361079301621773")
-addappid(1809542,0,"39cff11ed94d5fb43612916d75ca9d971b35caeca4fa3bda91610e1118927ec3")
-setManifestid(1809542,"2663029198830131753")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1809540.lua
+--Gamename Nine Sols
+addappid(1809540, 1, "6579f7e948e92fa5e2e745f8114d468ce0da84db9b05392996ab61dcf5b7a643") --Mainappid Nine Sols
+addappid(1809541, 1, "6c41504f601faddc7ec40aff4688b9ee9e34c37f2ab46b882880cbcc76a8eb40") --Main Windows Depot Nine Sols
+setManifestid(1809541, "4170341253080557508", 4074822512)
+addappid(1809542, 1, "39cff11ed94d5fb43612916d75ca9d971b35caeca4fa3bda91610e1118927ec3") --Main Macos Depot Nine Sols
+setManifestid(1809542, "6968920301698375133", 4084142976)
+--Dlcname Nine Sols Soundtrack
+addappid(1913930) --Dlcname Nine Sols Soundtrack
+addappid(1913931, 1, "c42d1d308472f2bdfb55854a826148354354932fa0dc80fcb9fbc3bdbd805c40") --Dlc Windows Depot Nine Sols Soundtrack
+setManifestid(1913931, "197660531224712960", 528430912)
+addappid(1913932, 1, "cf359cf7ac3fc6baee747e7e03d62fe991ec60c75ca8d9630fa173c13b9f4f76") --Dlc Windows Depot Nine Sols Soundtrack
+setManifestid(1913932, "8315689725284345744", 2363045280)
+--Dlcname Nine Sols Art Book
+addappid(3362590, 1, "8393593da990f6cd827af13ecb9d42c653dabc0101e53dd927e7e55ae2fa4aa1") --Dlcname Nine Sols Art Book
+setManifestid(3362590, "2032132760684014067", 375944816)
+addappid(3362591, 1, "0d84cc89dc3f66b203521c09e53075478305ba8702859948d892ba9c3bf0d9d0") --Dlc Windows Language Depot English Nine Sols Art Book
+setManifestid(3362591, "5837835346821903939", 375944816)
