d86d6c0fe67840eb85dbf2dc7d5135609d4dc339
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:26:52 +0800

    add 3751260.lua

---
+++ 3751260.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3751260.lua
+--Gamename The Blood of Dawnwalker
+addappid(3751260, 1, "bfb8982d4aeb1b3b81978c18fb93b66cbc707cf9530ab92769d00c50e90c4031") --Mainappid The Blood of Dawnwalker
+addappid(3751261, 1, "fe44cc16911c3d5dabb2b862a70f1b8b58ee79e1da1f47b8bf03af487c17e8fc") --Main Windows Depot The Blood of Dawnwalker
+setManifestid(3751261, "6667172545766883229", 52466845504)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4417540) --Dlcname AppID 4417540
+addappid(4417550) --Dlcname The Blood of Dawnwalker - Eclipse Edition Content
