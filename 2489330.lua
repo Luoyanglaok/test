1ec3dfaea0c7580041115f8dbd85c449351df6ff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:51:21 +0800

    add 2489330.lua

---
+++ 2489330.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2489330.lua
+--Gamename Whiskerwood
+addappid(2489330, 1, "83e1125a9ee59e054a471647a3a548a629c99664c91200a62e579b1ad2cae973") --Mainappid Whiskerwood
+addappid(2489331, 1, "480ca1efcc3cec10d331e77610da0a69b91cd36364ebff34f181ca0e36e63eb6") --Main Windows Depot Whiskerwood
+setManifestid(2489331, "4881486203988886377", 2208709984)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
