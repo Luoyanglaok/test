3d10d294320de61798dfba1b59163809d48cf9ef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 01:51:37 +0800

    add 1623730.lua

--- 1623730.lua
+++ 1623730.lua
@@ -4,6 +4,12 @@
 addappid(1623730, 1, "214e5bcf1036d2bc62193cf2bc15e148899db0ed4799c3a2c0d41f2490d136f5") --Mainappid Palworld
 addappid(1623731, 1, "7a422988135296505205dd489945d4b17ed7414734dd627744adac84aade7b73") --Main Windows Depot Palworld
 setManifestid(1623731, "868868087024202254", 35943136912)
+--Dlcname Palworld - Soundtrack
+addappid(2771110) --Dlcname Palworld - Soundtrack
+addappid(2771111, 1, "802dd0505be5518464fe77ed7afa2c28611463bf00ec5ef400643b323d09c4c7") --Dlc Windows Depot Palworld - Soundtrack
+setManifestid(2771111, "6155944524181178373", 296450800)
+addappid(2771112, 1, "c385e52ae8ad3b8756ab3caa03e50b105c0157fb797bb5acbff1bbb8bb27692f") --Dlc Windows Depot Palworld - Soundtrack
+setManifestid(2771112, "20864656196700675", 1691155504)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
