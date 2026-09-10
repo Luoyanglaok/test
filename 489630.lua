f7a6f903adf73f6be7a44bd962f9298247beb53c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:50:00 +0800

    add 489630.lua

--- 489630.lua
+++ 489630.lua
@@ -1,46 +1,79 @@
-addappid(489630)
-setManifestid(229006,"1784011429307107530")
-addappid(489633,0,"6bfeeddc0c7c9ab4c84e53df522ca4caf4b1613a43e564d36b986ec55f5463f8")
-setManifestid(489633,"1715800054631963256")
-addappid(489631,0,"98cad733831ba87cf9c07a92272afbcfcf98b22daea917ab778b0f46ac6473b8")
-setManifestid(489631,"3646769651338185448")
-addappid(489632,0,"2181cc760c11eec2b2088e83099b893bbef7db3b725ab0f437942b2f6a6042b6")
-setManifestid(489632,"660970934787565220")
-addappid(870530,0,"4e12760f62e2227d48295b0f3b0422870e19f58cb4fb97f5a845b2b36f4aa30a")
-setManifestid(870530,"8903843273179332056")
-addappid(870550,0,"8f35ba8a63d84bb32165ffd1c934a166e581becffc9e3908a561c927f605caac")
-setManifestid(870550,"7813621408118342533")
-addappid(870540,0,"6e0cd7069ff1bb92e841c6045af49df24f0012760cbfa2b36a6baea8e8648e33")
-setManifestid(870540,"442301573797662394")
-addappid(961840,0,"e04d4e06fffc382f6c245d339f0c0b761d3813b94f38e9a69135b828ecd1ed6f")
-setManifestid(961840,"7296323810134405176")
-addappid(995860,0,"2c8c2a78b1d4c6d478c7de8f44bd9fae8f9199cc5fd003a203005136d53ce006")
-setManifestid(995860,"4144015961261623394")
-addappid(1084820,0,"ee313c687835e5166562e269b36572e89de3a45cafbeaf1879db587d5681bd49")
-setManifestid(1084820,"4214815001978544993")
-addappid(1170010,0,"78a8474c3c6a3d4d0437cd808aeaa18fc4e4e923f0f58988c2887c12b6e1e949")
-setManifestid(1170010,"8986672640384167430")
-addappid(1239450,0,"06a825d11874fb9ccec98d4720b93362b1401cd05e30f240b6c64d1cdcd302dc")
-setManifestid(1239450,"1412725218929693043")
-addappid(1330480,0,"dc33d113a2f943b9be884c2817828603859f93972619902dab7c39d0ecb8b649")
-setManifestid(1330480,"8560968486342641530")
-addappid(1425820,0,"be8694de13023b51c4cad8d672e68571f20dd0072714424f5fbf97dc9e0ac4a7")
-setManifestid(1425820,"7181811712912349053")
-addappid(1640850,0,"d69f24faedae1efb5695169b70140cc649be555e18e2be643ee324a617fe7294")
-setManifestid(1640850,"9212576765883138281")
-addappid(1788210,0,"85a2c84a029166be6e595e081b6bf9f39a8f98652b0c8c01d0bc35e7d91f9a60")
-setManifestid(1788210,"6718914060557802407")
-addappid(2006870,0,"541a5c3ccb66295018e90753817508bd11691d17aa4d894379b0dfdbf433f2ab")
-setManifestid(2006870,"4076941040674822771")
-addappid(2130960,0,"2e41d3eb51b475a717f4526395d8cf7bbbbc09ee871cd5ecc6efdfafdf2481b6")
-setManifestid(2130960,"8813317415370242721")
-addappid(2399890,0,"2d8288c197415cced529152df9e87700f628d56e35db913c1549eaecca674086")
-setManifestid(2399890,"4892800493497001049")
-addappid(2668040,0,"d994cddf0661503e891556f8a8dc28f74586be3c16c9520fa5ba392dd2dbcea2")
-setManifestid(2668040,"7709192808205830515")
-addappid(2932380,0,"cbe017581e8b542a1262cbd153e731d294da59db188801a36647944a080b9d52")
-setManifestid(2932380,"8151899288165800345")
-addappid(3326080,0,"5a032138ec68be28197d6e03a29eb1c99e8c7f392838b8ea170ac329111040cc")
-setManifestid(3326080,"7375851679082319866")
-addappid(3743110,0,"126dd0c29b9af92ee931d376f40a6b77e15602a53c6c4ff3e4963c427853d1f5")
-setManifestid(3743110,"8622599070272747958")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 489630.lua
+--Gamename Warhammer 40,000: Gladius - Relics of War
+addappid(489630, 1, "de35a1a75ebf92e40ea43bb11248f578291c59f64eea9582935a1206eee600cc") --Mainappid Warhammer 40,000: Gladius - Relics of War
+addappid(489631, 1, "98cad733831ba87cf9c07a92272afbcfcf98b22daea917ab778b0f46ac6473b8") --Main Windows Depot Warhammer 40,000: Gladius - Relics of War
+setManifestid(489631, "2758880504343920108", 43427744)
+addappid(489632, 1, "2181cc760c11eec2b2088e83099b893bbef7db3b725ab0f437942b2f6a6042b6") --Main Linux Depot Warhammer 40,000: Gladius - Relics of War
+setManifestid(489632, "5866847354160841914", 42818432)
+addappid(489633, 1, "6bfeeddc0c7c9ab4c84e53df522ca4caf4b1613a43e564d36b986ec55f5463f8") --Main Windows Depot Warhammer 40,000: Gladius - Relics of War
+setManifestid(489633, "2520582596086416009", 2223100768)
+addappid(870530, 1, "4e12760f62e2227d48295b0f3b0422870e19f58cb4fb97f5a845b2b36f4aa30a") --Main Windows Depot Warhammer 40,000: Gladius - Relics of War
+setManifestid(870530, "8903843273179332056", 1290081888)
+--Dlcname Warhammer 40,000: Gladius - Relics of War - Soundtrack
+addtoken(870530, "5016130886635041473")
+--Dlcname AppID 870540
+addtoken(870540, "3316042410707366797")
+addappid(870540, 1, "6e0cd7069ff1bb92e841c6045af49df24f0012760cbfa2b36a6baea8e8648e33") --Dlcname AppID 870540
+setManifestid(870540, "442301573797662394", 64)
+--Dlcname AppID 870550
+addtoken(870550, "7595109318970487055")
+addappid(870550, 1, "8f35ba8a63d84bb32165ffd1c934a166e581becffc9e3908a561c927f605caac") --Dlcname AppID 870550
+setManifestid(870550, "7813621408118342533", 25170448)
+--Dlcname Warhammer 40,000: Gladius - Reinforcement Pack
+addappid(961840, 1, "e04d4e06fffc382f6c245d339f0c0b761d3813b94f38e9a69135b828ecd1ed6f") --Dlcname Warhammer 40,000: Gladius - Reinforcement Pack
+setManifestid(961840, "7296323810134405176", 64)
+--Dlcname Warhammer 40,000: Gladius - Tyranids
+addappid(995860, 1, "2c8c2a78b1d4c6d478c7de8f44bd9fae8f9199cc5fd003a203005136d53ce006") --Dlcname Warhammer 40,000: Gladius - Tyranids
+setManifestid(995860, "4144015961261623394", 64)
+--Dlcname Warhammer 40,000: Gladius - Chaos Space Marines
+addappid(1084820, 1, "ee313c687835e5166562e269b36572e89de3a45cafbeaf1879db587d5681bd49") --Dlcname Warhammer 40,000: Gladius - Chaos Space Marines
+setManifestid(1084820, "1359616362294346870", 64)
+--Dlcname Warhammer 40,000: Gladius - Fortification Pack
+addappid(1170010, 1, "78a8474c3c6a3d4d0437cd808aeaa18fc4e4e923f0f58988c2887c12b6e1e949") --Dlcname Warhammer 40,000: Gladius - Fortification Pack
+setManifestid(1170010, "8986672640384167430", 64)
+--Dlcname Warhammer 40,000: Gladius - T'au
+addappid(1239450, 1, "06a825d11874fb9ccec98d4720b93362b1401cd05e30f240b6c64d1cdcd302dc") --Dlcname Warhammer 40,000: Gladius - T'au
+setManifestid(1239450, "1412725218929693043", 64)
+--Dlcname Warhammer 40,000: Gladius - Assault Pack
+addappid(1330480, 1, "dc33d113a2f943b9be884c2817828603859f93972619902dab7c39d0ecb8b649") --Dlcname Warhammer 40,000: Gladius - Assault Pack
+setManifestid(1330480, "8560968486342641530", 64)
+--Dlcname Warhammer 40,000: Gladius - Craftworld Aeldari
+addappid(1425820, 1, "be8694de13023b51c4cad8d672e68571f20dd0072714424f5fbf97dc9e0ac4a7") --Dlcname Warhammer 40,000: Gladius - Craftworld Aeldari
+setManifestid(1425820, "1299365935980391543", 64)
+--Dlcname Warhammer 40,000: Gladius - Specialist Pack
+addappid(1640850, 1, "d69f24faedae1efb5695169b70140cc649be555e18e2be643ee324a617fe7294") --Dlcname Warhammer 40,000: Gladius - Specialist Pack
+setManifestid(1640850, "6872846984314783191", 192)
+--Dlcname Warhammer 40,000: Gladius - Adeptus Mechanicus
+addappid(1788210, 1, "85a2c84a029166be6e595e081b6bf9f39a8f98652b0c8c01d0bc35e7d91f9a60") --Dlcname Warhammer 40,000: Gladius - Adeptus Mechanicus
+setManifestid(1788210, "4574783087363847923", 192)
+--Dlcname Warhammer 40,000: Gladius - Escalation Pack
+addappid(2006870, 1, "541a5c3ccb66295018e90753817508bd11691d17aa4d894379b0dfdbf433f2ab") --Dlcname Warhammer 40,000: Gladius - Escalation Pack
+setManifestid(2006870, "1665716313743754605", 192)
+--Dlcname Warhammer 40,000: Gladius - Adepta Sororitas
+addappid(2130960, 1, "2e41d3eb51b475a717f4526395d8cf7bbbbc09ee871cd5ecc6efdfafdf2481b6") --Dlcname Warhammer 40,000: Gladius - Adepta Sororitas
+setManifestid(2130960, "3422755477659509337", 192)
+--Dlcname Warhammer 40,000: Gladius - Firepower Pack
+addappid(2399890, 1, "2d8288c197415cced529152df9e87700f628d56e35db913c1549eaecca674086") --Dlcname Warhammer 40,000: Gladius - Firepower Pack
+setManifestid(2399890, "4892800493497001049", 192)
+--Dlcname Warhammer 40,000: Gladius - Drukhari
+addappid(2668040, 1, "d994cddf0661503e891556f8a8dc28f74586be3c16c9520fa5ba392dd2dbcea2") --Dlcname Warhammer 40,000: Gladius - Drukhari
+setManifestid(2668040, "4099249179098026115", 192)
+--Dlcname Warhammer 40,000: Gladius - Demolition Pack
+addappid(2932380, 1, "cbe017581e8b542a1262cbd153e731d294da59db188801a36647944a080b9d52") --Dlcname Warhammer 40,000: Gladius - Demolition Pack
+setManifestid(2932380, "4278394078308930362", 192)
+--Dlcname Warhammer 40,000: Gladius - Ultima Founding
+addappid(3326080, 1, "5a032138ec68be28197d6e03a29eb1c99e8c7f392838b8ea170ac329111040cc") --Dlcname Warhammer 40,000: Gladius - Ultima Founding
+setManifestid(3326080, "1554606525873451708", 192)
+--Dlcname Warhammer 40,000: Gladius - Onslaught Pack
+addappid(3743110, 1, "126dd0c29b9af92ee931d376f40a6b77e15602a53c6c4ff3e4963c427853d1f5") --Dlcname Warhammer 40,000: Gladius - Onslaught Pack
+setManifestid(3743110, "8622599070272747958", 192)
+--Dlcname Warhammer 40,000: Gladius - Rampage Pack
+addappid(4084640, 1, "9e5b9dc7d9920571946a3aa1d9b7426c42111e5859ff73280b8229e5c7969d21") --Dlcname Warhammer 40,000: Gladius - Rampage Pack
+setManifestid(4084640, "4967570762675787809", 192)
+--Dlcname Warhammer 40,000: Gladius - Eradication pack
+addappid(4660150, 1, "b5cc02320ee2b1d091ed3b5e1107e7126de8470683499937cab4e4c4b430e981") --Dlcname Warhammer 40,000: Gladius - Eradication pack
+setManifestid(4660150, "5764432161256486450", 192)
+--Share Depots
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
