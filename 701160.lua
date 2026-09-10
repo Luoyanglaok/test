ceac238b3c6919acc2fcfe17d98bb7537ebcfaf7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 16:35:59 +0800

    add 701160.lua

--- 701160.lua
+++ 701160.lua
@@ -1,10 +1,24 @@
-addappid(701160)
-setManifestid(228990,"1829726630299308803")
-addappid(701161,0,"5aae8ca5c454854bdf1b25248fe5f3c01a97c983489767c17930cc8a02156ac3")
-setManifestid(701161,"3161061016134672988")
-addappid(701162,0,"17174ac183441ce4785eb189f7a80fc0fd9a202494cad02f1ddf4bc3d87aade4")
-setManifestid(701162,"1282176972241264473")
-addappid(701163,0,"88068941a333be0784b8fd4fe2c414237610909671e189f3068177fc84e0352e")
-setManifestid(701163,"4566154827935506379")
-addappid(988810,0,"09f7b9d014059b08ea21838c59e9edadb8d14238c0e97a6cbb6d597d98f49174")
-setManifestid(988810,"1333726444267560544")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 701160.lua
+--Gamename Kingdom Two Crowns
+addappid(701160, 1, "a870b8cb617fd6f06ec9d1b0a89f514f8611794aa043288467001423108182c7") --Mainappid Kingdom Two Crowns
+addappid(701161, 1, "5aae8ca5c454854bdf1b25248fe5f3c01a97c983489767c17930cc8a02156ac3") --Main Windows Depot Kingdom Two Crowns
+setManifestid(701161, "6639004889238773204", 1395568064)
+addappid(701162, 1, "17174ac183441ce4785eb189f7a80fc0fd9a202494cad02f1ddf4bc3d87aade4") --Main Macos Depot Kingdom Two Crowns
+setManifestid(701162, "8199715182987815586", 1425463552)
+addappid(701163, 1, "88068941a333be0784b8fd4fe2c414237610909671e189f3068177fc84e0352e") --Main Linux Depot Kingdom Two Crowns
+setManifestid(701163, "2442055016837937796", 1378192624)
+--Dlcname Kingdom Two Crowns: OST
+addappid(988810, 1, "09f7b9d014059b08ea21838c59e9edadb8d14238c0e97a6cbb6d597d98f49174") --Dlcname Kingdom Two Crowns: OST
+setManifestid(988810, "9153904362077354978", 2149279024)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(980450) --Dlcname Kingdom Two Crowns: Shogun
+addappid(1299270) --Dlcname Kingdom Two Crowns: Dead Lands
+addappid(2736340) --Dlcname Kingdom Two Crowns: Call of Olympus
+addappid(3062350) --Dlcname Kingdom Two Crowns: Regent's Royal Wardrobe
+addappid(3062360) --Dlcname Kingdom Two Crowns: Archon's Royal Wardrobe
+addappid(3939430) --Dlcname Kingdom Two Crowns: Decennial Royal Wardrobe
+--Missing Dlcs
+--Dlcname Kingdom Two Crowns: Norse Lands
