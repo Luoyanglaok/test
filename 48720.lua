2219f776e32f93f0fe1731380272f15347d07bfe
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:58:00 +0800

    add 48720.lua

--- 48720.lua
+++ 48720.lua
@@ -1,3 +1,6 @@
-addappid(48720)
-addappid(48721,0,"d324596215ef1b8090849ba8de83581948e186747079b8fa4bac51999f5ead22")
-setManifestid(48721,"3141280427055525072")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 48720.lua
+--Gamename Mount & Blade: With Fire & Sword
+addappid(48720) --Mainappid Mount & Blade: With Fire & Sword
+addappid(48721, 1, "d324596215ef1b8090849ba8de83581948e186747079b8fa4bac51999f5ead22") --Main Windows Depot Mount & Blade: With Fire & Sword
+setManifestid(48721, "3141280427055525072", 898696960)
