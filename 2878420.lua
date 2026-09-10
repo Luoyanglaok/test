d4cfcbbe60e87fe6ac3aa9904269588997444b36
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:12:34 +0800

    add 2878420.lua

--- 2878420.lua
+++ 2878420.lua
@@ -1,4 +1,7 @@
-addappid(2878420)
-addtoken(2878420,"15180986674925960148")
-addappid(2878421,0,"4576f91c200141b0609fc6250aec2ddf4604ec5031f650f201d4b8114744ef0b")
-setManifestid(2878421,"5784716990056895669")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2878420.lua
+--Gamename Old Market Simulator
+addappid(2878420) --Mainappid Old Market Simulator
+addtoken(2878420, "15180986674925960148")
+addappid(2878421, 1, "4576f91c200141b0609fc6250aec2ddf4604ec5031f650f201d4b8114744ef0b") --Main Windows Depot Old Market Simulator
+setManifestid(2878421, "393038957838246265", 856907456)
