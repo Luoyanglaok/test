801db780688ff8e9ac7da289330358a179153087
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:15:53 +0800

    add 407530.lua

--- 407530.lua
+++ 407530.lua
@@ -1,11 +1,19 @@
-addappid(407530)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228990,"1829726630299308803")
-addappid(407531,0,"3b2ecd725e715acbe4f0faece4804881627c6b44756f8ab21b7ea7ef265d8790")
-setManifestid(407531,"3150260920024472953")
-addappid(407532,0,"7890a0784b70376002ee9a6324fc0316a0708d0885963ea04e19eb8ff39154ec")
-setManifestid(407532,"8413944083207386605")
-addappid(407533,0,"ff34a171aebbf864adebe441b8a24188fb40b5bd0a62645427132c63ca28803f")
-setManifestid(407533,"130193075946215246")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 407530.lua
+--Gamename ARK: The Survival Of The Fittest
+addappid(407530, 1, "7774931bb45e7bced607ab9b3b7f8160b5e3d08720b1d9842addb3c424a22b8d") --Mainappid ARK: The Survival Of The Fittest
+addappid(407531, 1, "3b2ecd725e715acbe4f0faece4804881627c6b44756f8ab21b7ea7ef265d8790") --Main Windows Depot ARK: The Survival Of The Fittest
+setManifestid(407531, "3150260920024472953", 20277026544)
+addappid(407532, 1, "7890a0784b70376002ee9a6324fc0316a0708d0885963ea04e19eb8ff39154ec") --Main Linux Depot ARK: The Survival Of The Fittest
+setManifestid(407532, "8413944083207386605", 7522803408)
+addappid(407533, 1, "ff34a171aebbf864adebe441b8a24188fb40b5bd0a62645427132c63ca28803f") --Main Macos Depot ARK: The Survival Of The Fittest
+setManifestid(407533, "130193075946215246", 7520941328)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
