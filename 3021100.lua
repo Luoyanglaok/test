e626ce7f46373ee7a611fea8e6a472beafbceef6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:07:46 +0800

    add 3021100.lua

--- 3021100.lua
+++ 3021100.lua
@@ -1,7 +1,10 @@
-addappid(3021100)
-addappid(3021101,0,"7e7c0976f19ebf717f8bdf5b661b1bec4163b9972058f6b40f37f41ea75a5ad5")
-setManifestid(3021101,"2784147648951140968")
-addappid(3021102,0,"b823062bd11419706eb109653048db1167267957791dbd3cd9de278b1df22fd4")
-setManifestid(3021102,"7827774043635703013")
-addappid(3021103,0,"c2efb6191fffcccf257712a9903c4ec9c3b6dbd1785540d1a747756791f3eb29")
-setManifestid(3021103,"1454603167080144696")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3021100.lua
+--Gamename Five Hearts Under One Roof
+addappid(3021100) --Mainappid Five Hearts Under One Roof
+addappid(3021101, 1, "7e7c0976f19ebf717f8bdf5b661b1bec4163b9972058f6b40f37f41ea75a5ad5") --Main Windows Depot Five Hearts Under One Roof
+setManifestid(3021101, "7793127310082659738", 27803391904)
+addappid(3021102, 1, "b823062bd11419706eb109653048db1167267957791dbd3cd9de278b1df22fd4") --Main Macos Depot Five Hearts Under One Roof
+setManifestid(3021102, "935125386564644069", 28923479664)
+addappid(3021103, 1, "c2efb6191fffcccf257712a9903c4ec9c3b6dbd1785540d1a747756791f3eb29") --Main Linux Depot Five Hearts Under One Roof
+setManifestid(3021103, "8114336949438709065", 28912520912)
