cc3073e31233dfcdf156cd67f7dd8450eb5e3f2d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:38:30 +0800

    add 2713000.lua

---
+++ 2713000.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2713000.lua
+--Gamename Resonance: A Plague Tale Legacy
+addappid(2713000) --Mainappid Resonance: A Plague Tale Legacy
+addtoken(2713000, "17223300556103744127")
+addappid(2713001, 1, "7085af990ca726e5a5e97ac1acd51bf4f32ba387d0f3d5edb114720e52b536b4") --Main Windows Depot Resonance: A Plague Tale Legacy
+setManifestid(2713001, "4228535050004172075", 58298064688)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3837640) --Dlcname Resonance: A Plague Tale Legacy - Heritage Pack DLC
