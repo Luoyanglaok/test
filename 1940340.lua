896f65601a00db09909de7ddfa8c1978810fa876
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:16:30 +0800

    add 1940340.lua

--- 1940340.lua
+++ 1940340.lua
@@ -1,17 +1,28 @@
-addappid(1940340)
-addappid(1940341,0,"4266c37ee75bce9e4b3c5d16b0cffd8e21ed9ca98a95eed54900689ab5242d23")
-setManifestid(1940341,"7126773135450601497")
-addappid(2598510,0,"235f932f22e943699a9dd3de36d77beb99ce3e99b25eea7d1467da814906bbde")
-setManifestid(2598510,"5125800756120033427")
-addappid(1940342,0,"e9c92bd06c91103e6d6f07ae4be80184bb8a28fcb63018f3941d222048c45287")
-setManifestid(1940342,"8527322850386493919")
-addappid(2598511,0,"dbaaed9071a3364932fffa0e960be801e3abb368b91a3ebacb3bb0657e0cc6ca")
-setManifestid(2598511,"6215306164053669060")
-addappid(3205310,0,"36bbc8f34a0af50c71728ea98f60e80969087846308d321308eeacf180c72b81")
-setManifestid(3205310,"3753132147703713544")
-addappid(3205311,0,"b1b962b48dd30d0bca610ea61a17f745d8e60bf227f5b60c93ce056739650224")
-setManifestid(3205311,"834742674505427799")
-addappid(3591040,0,"bcb4dce50f021f662a8f4dc7765e5952186fdd6f4faad47e379af0b95202bebb")
-setManifestid(3591040,"8468142998457560582")
-addappid(3715610,0,"8509167b054fa6acac9084802a5cfe75c7e4956395a4b64adb307cef93bf2740")
-setManifestid(3715610,"3662574011577989633")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1940340.lua
+--Gamename Darkest Dungeon® II
+addappid(1940340, 1, "813ac2fc24ad2efd41c269d79360b3514c9f55e28d0a1d6695425875b5aae0f3") --Mainappid Darkest Dungeon® II
+addappid(1940341, 1, "4266c37ee75bce9e4b3c5d16b0cffd8e21ed9ca98a95eed54900689ab5242d23") --Main Windows Depot Darkest Dungeon® II
+setManifestid(1940341, "2804281716747385054", 5052409728)
+addappid(1940342, 1, "e9c92bd06c91103e6d6f07ae4be80184bb8a28fcb63018f3941d222048c45287") --Main Macos Depot Darkest Dungeon® II
+setManifestid(1940342, "7256003679686235436", 5071639600)
+--Dlcname Darkest Dungeon® II: The Binding Blade
+addappid(2598510, 1, "235f932f22e943699a9dd3de36d77beb99ce3e99b25eea7d1467da814906bbde") --Dlcname Darkest Dungeon® II: The Binding Blade
+setManifestid(2598510, "3821771715023000420", 190852720)
+addappid(2598511, 1, "dbaaed9071a3364932fffa0e960be801e3abb368b91a3ebacb3bb0657e0cc6ca") --Dlc Macos Depot Darkest Dungeon® II: The Binding Blade
+setManifestid(2598511, "1121923413959543457", 190873120)
+--Dlcname Darkest Dungeon® II: Inhuman Bondage
+addappid(3205310, 1, "36bbc8f34a0af50c71728ea98f60e80969087846308d321308eeacf180c72b81") --Dlcname Darkest Dungeon® II: Inhuman Bondage
+setManifestid(3205310, "5558201690709026218", 343905952)
+addappid(3205311, 1, "b1b962b48dd30d0bca610ea61a17f745d8e60bf227f5b60c93ce056739650224") --Dlc Macos Depot Darkest Dungeon® II: Inhuman Bondage
+setManifestid(3205311, "4975300004145726605", 343928176)
+--Dlcname Darkest Dungeon® II: Infernal Supporter Pack
+addappid(3591040, 1, "bcb4dce50f021f662a8f4dc7765e5952186fdd6f4faad47e379af0b95202bebb") --Dlcname Darkest Dungeon® II: Infernal Supporter Pack
+setManifestid(3591040, "1451016740324191040", 36196704)
+addappid(3591041, 1, "b1d7efce3eca72ac0fb6a744e7a9969bc6f97bc90669d70d05da021ad21d603e") --Dlc Macos Depot Darkest Dungeon® II: Infernal Supporter Pack
+setManifestid(3591041, "7378219750337745171", 36193392)
+--Dlcname Darkest Dungeon® II: Hero Origin Pack
+addappid(3715610, 1, "8509167b054fa6acac9084802a5cfe75c7e4956395a4b64adb307cef93bf2740") --Dlcname Darkest Dungeon® II: Hero Origin Pack
+setManifestid(3715610, "3150497762967237829", 33266592)
+addappid(3715611, 1, "D838B99999D23DE2DF264680D5C9D50ADB82978A334DEDC0D515885A3D467425") --Dlc Macos Depot Darkest Dungeon® II: Hero Origin Pack
+setManifestid(3715611, "7434935669169429389", 33260512)
