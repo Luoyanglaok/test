9afbb7b2491bb3ba11ee647e815fa706958020e8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 18:09:56 +0800

    add 2904040.lua

---
+++ 2904040.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2904040.lua
+--Gamename Cheap Car Repair
+addappid(2904040) --Mainappid Cheap Car Repair
+addappid(2904041, 1, "5f2fc48115edd9e7bfc1546f9dc43c8d900013aaf3d4f5035fb69ff28899f050") --Main Windows Depot Cheap Car Repair
+setManifestid(2904041, "2110048212733887519", 3872445840)
