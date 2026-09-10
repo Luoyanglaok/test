e214c3640d84379966de8a95f623ee7b22f6db58
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 10:42:49 +0800

    add 1140410.lua

--- 1140410.lua
+++ 1140410.lua
@@ -1,3 +1,6 @@
-addappid(1140410)
-addappid(1140411,0,"e53566a0b1566a3b39e8170bfedcb697ca00e5b59c024479f9acba223beb74e7")
-setManifestid(1140411,"2172874710527121773")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1140410.lua
+--Gamename Sex City
+addappid(1140410) --Mainappid Sex City
+addappid(1140411, 1, "e53566a0b1566a3b39e8170bfedcb697ca00e5b59c024479f9acba223beb74e7") --Main Windows Depot Sex City
+setManifestid(1140411, "2172874710527121773", 48419168)
