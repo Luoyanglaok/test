be84dac7527f81f62508494d5903dbfb2bea9ed3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 20:18:29 +0800

    add 1283230.lua

--- 1283230.lua
+++ 1283230.lua
@@ -1,5 +1,9 @@
-addappid(1283230)
-addappid(1283231,0,"578da163a6889657d25ae77647b853543c386f98b887c29aec3845255d378ea3")
-setManifestid(1283231,"4072493460832741759")
-addappid(3017520,0,"bf5308dd1c5f9bec7c93a631d0ff95730de681d7a940f9433cc48903db285647")
-setManifestid(3017520,"5621270297597602334")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1283230.lua
+--Gamename Autopsy Simulator
+addappid(1283230) --Mainappid Autopsy Simulator
+addappid(1283231, 1, "578da163a6889657d25ae77647b853543c386f98b887c29aec3845255d378ea3") --Main Windows Depot Autopsy Simulator
+setManifestid(1283231, "4485818646241165075", 8606978176)
+--Dlcname Autopsy Simulator - Digital Artbook
+addappid(3017520, 1, "bf5308dd1c5f9bec7c93a631d0ff95730de681d7a940f9433cc48903db285647") --Dlcname Autopsy Simulator - Digital Artbook
+setManifestid(3017520, "5621270297597602334", 49145680)
