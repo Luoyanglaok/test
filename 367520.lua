0e64691e158b8b0e159a18436a905a0174f622e2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 12:01:12 +0800

    add 367520.lua

--- 367520.lua
+++ 367520.lua
@@ -1,7 +1,10 @@
-addappid(367520)
-addappid(367521,0,"de801b9b61e3385b333259cc0f04d2e5d976c4cb8f933cb859d75a4d0125a2a8")
-setManifestid(367521,"3820178394837330515")
-addappid(367522,0,"c7e67c6bf87d7128302920e88b17cb6ee322e69585472cbcd2a3d913c7d3579a")
-setManifestid(367522,"8410255336209704559")
-addappid(367523,0,"61dd4c0fd624c81feb31fc22cb61a228c4d8077cf3927e9058f736026d9dd838")
-setManifestid(367523,"5829533265112705522")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 367520.lua
+--Gamename Hollow Knight
+addappid(367520, 1, "94a0a4b4c50ce3065ce9d4d21eecfdb8bae52f99b16c4eea1649ebcce9ac2e15") --Mainappid Hollow Knight
+addappid(367521, 1, "de801b9b61e3385b333259cc0f04d2e5d976c4cb8f933cb859d75a4d0125a2a8") --Main Windows Depot Hollow Knight
+setManifestid(367521, "257781644874438846", 1091282960)
+addappid(367522, 1, "c7e67c6bf87d7128302920e88b17cb6ee322e69585472cbcd2a3d913c7d3579a") --Main Macos Depot Hollow Knight
+setManifestid(367522, "6809616861563627670", 1107565712)
+addappid(367523, 1, "61dd4c0fd624c81feb31fc22cb61a228c4d8077cf3927e9058f736026d9dd838") --Main Linux Depot Hollow Knight
+setManifestid(367523, "708613018541602983", 1101974432)
