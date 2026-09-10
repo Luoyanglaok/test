9742f026f44c0ee9dd925cf26400c04d9664b348
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 15:54:55 +0800

    add 1594320.lua

--- 1594320.lua
+++ 1594320.lua
@@ -1,5 +1,12 @@
-addappid(1594320)
-addappid(1594321,0,"4ff3d26a9ec89b9d17aa43ee05ea46cb83cd638a31b36a385b43fc810ccc6703")
-setManifestid(1594321,"3166734453949857378")
-addappid(2004740,0,"99e976dbccd9e46b15b201695ea57868cd42725d13bb6fc4512e5f3a7d620687")
-setManifestid(2004740,"1630681541187985989")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1594320.lua
+--Gamename Captain of Industry
+addappid(1594320, 1, "05671966acb0423953d9ccc1db5981a7d6f00bce411bddda621eb4959f9d2123") --Mainappid Captain of Industry
+addappid(1594321, 1, "4ff3d26a9ec89b9d17aa43ee05ea46cb83cd638a31b36a385b43fc810ccc6703") --Main Windows Depot Captain of Industry
+setManifestid(1594321, "6265590129150821136", 3329792496)
+--Dlcname Captain of Industry - Supporter pack
+addappid(2004740, 1, "99e976dbccd9e46b15b201695ea57868cd42725d13bb6fc4512e5f3a7d620687") --Dlcname Captain of Industry - Supporter pack
+setManifestid(2004740, "5026221012306694212", 16482000)
+--Dlcname Captain of Industry - Trains expanded
+addappid(4349830, 1, "06605130aff9e40092bf43024ef429e6fd4c60c5da7f397898598e936d14e8a6") --Dlcname Captain of Industry - Trains expanded
+setManifestid(4349830, "3588577377178619199", 384101440)
