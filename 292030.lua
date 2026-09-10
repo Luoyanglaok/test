6e87d86374168be77b88fb0b03aa12a681c4e09e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-10 11:36:50 +0800

    add 292030.lua

--- 292030.lua
+++ 292030.lua
@@ -1,45 +1,160 @@
-addappid(292030)
-addappid(5006530)
-addappid(1229320)
-addappid(1233280)
-addappid(1233340)
-addappid(228984,1,"df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068")
-addappid(228985,1,"21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c")
-addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(292031,"1714062795524118335")
-setManifestid(292032,"3490723872095446153")
-setManifestid(292034,"8082988454426507412")
-setManifestid(292036,"3491360665020245284")
-setManifestid(292042,"99458089550221039")
-setManifestid(370000,"1458194560961361295")
-setManifestid(370001,"5979725543926398369")
-setManifestid(370003,"5453197123234356101")
-setManifestid(370002,"484236434286524845")
-setManifestid(292033,"5639169236463762487")
-setManifestid(373950,"7457326912183925734")
-setManifestid(373951,"1015260686331095979")
-setManifestid(376390,"4380995726432289642")
-setManifestid(376391,"6437141662912268782")
-setManifestid(378642,"2210049775439183344")
-setManifestid(378640,"5318422306227358494")
-setManifestid(378641,"4647447917504831785")
-setManifestid(378643,"459508221271235268")
-setManifestid(378644,"2928442289493234239")
-setManifestid(378645,"6056630807565191690")
-setManifestid(378646,"7313260968752741219")
-setManifestid(378647,"6347498022277232982")
-setManifestid(378648,"5677496677655330654")
-setManifestid(378649,"9216322386755119484")
-setManifestid(292044,"908607634780300143")
-setManifestid(310611,"9087403118674428852")
-setManifestid(310613,"2970969303001438150")
-setManifestid(310619,"4855315366624356663")
-setManifestid(370008,"3305949701387885154")
-setManifestid(370009,"3498696915194240584")
-setManifestid(373952,"7987515327562995934")
-setManifestid(373954,"9189601712632867387")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 292030.lua
+--Gamename The Witcher 3: Wild Hunt - Complete Edition
+addappid(292030, 1, "9cee38a2013555e62de550c2d53e86dd747dd60d6ca81fda65ff7711d517e322") --Mainappid The Witcher 3: Wild Hunt - Complete Edition
+addappid(292031, 1, "57538aae18fccf308ea67da213c57b2635d53d14b59ade4ebb068dca33328eee") --Main Windows Depot The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292031, "1714062795524118335", 34642186960)
+addappid(292032, 1, "f6f8cb024f592fa85a1e86523011ed889893386f235624a5a76e12428be6fbac") --Main Windows Depot The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292032, "3490723872095446153", 95563856)
+addappid(292033, 1, "11b857e28f6db3a43762d6756bfb09d701c922ec27ea12cbb8bad1104a184466") --Main Windows Depot The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292033, "5639169236463762487", 3724906608)
+addappid(292034, 1, "876688c365d3ed8b11a0c1454d8e64234a22dde3e7798ca8e2aa70d55ad5082b") --Main Windows Depot The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292034, "8082988454426507412", 9245568)
+addappid(292035, 1, "a29f657f2ef95293a55076c120ac3814e8145c1253455c6eb162bf8f03aeb705") --Main Windows Language Depot Polish The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292035, "5593726038514618994", 2441660608)
+addappid(292036, 1, "a382ab2faaacad1573c079e6ce74400decd37e6e1a388d67ca242a3aa7af8e0c") --Main Windows Depot The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292036, "3491360665020245284", 2497801216)
+addappid(292037, 1, "9d4d27d17de980de7a96d2a810f649f2195deeb47ac6aae8f31ca09e17543581") --Main Windows Language Depot German The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292037, "3817923221097690740", 2560085360)
+addappid(292038, 1, "1b451418b38bac4816cbb838bc7fe6e9273a6228f4498e86c4ed54a0ab9a1156") --Main Windows Language Depot French The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292038, "1227805940310364169", 2413199680)
+addappid(292039, 1, "2625b539cdd037cdf02cc7a414ce0d12fa3f1266653e97303319ec472c7875fe") --Main Windows Language Depot Russian The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292039, "5480433155299939110", 2658626912)
+addappid(292040, 1, "ce852c61d460e3e716ca8e755d1e1485bb467944f96c8b0509fae32f9b2a6392") --Main Windows Language Depot Japanese The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292040, "8125275595906123923", 2597443040)
+addappid(292041, 1, "7dbc791cd60110b1d08e49d871b15d2356ce70224ca7d950b3ea36617cfbe8bb") --Main Windows Language Depot Brazilian The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292041, "4823902935523134760", 2520803104)
+addappid(292042, 1, "47e959135bc89e032628b3ed3a21c3cd18719d5e111c8489f131d0b166422995") --Main Windows Depot The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(292042, "99458089550221039", 29282448)
+addappid(370007, 1, "e501823568fd98483bacffd2f32510ba9aabaf2d012ff8e9f8280eedfbd4986c") --Main Windows Language Depot Koreana The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(370007, "6165918066648822292", 2532214704)
+addappid(370008, 1, "dba9227ccbfd4748acec6b60fa6cc450ff9303df51c1d319c9c0ac8d87000798") --Main Windows Language Depot Schinese The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(370008, "3305949701387885154", 2600379664)
+addappid(370009, 1, "2b8ebd860f519578e0269e5b20a1ce13d76652bd066501087b6c3c1f77f5efc8") --Main Windows Depot The Witcher 3: Wild Hunt - Complete Edition
+setManifestid(370009, "3498696915194240584", 653952688)
+--Dlcname The Witcher 3: Wild Hunt - Temerian Armor Set
+addtoken(370000, "662041442504756376")
+addappid(370000, 1, "cc779551805a59df011c692726377ef65f9505362d482c96df5387856f3ea584") --Dlcname The Witcher 3: Wild Hunt - Temerian Armor Set
+setManifestid(370000, "1458194560961361295", 64712912)
+--Dlcname The Witcher 3: Wild Hunt - Beard and Hairstyle Set
+addappid(370001, 1, "62f05882d6c750c73e7dbb09c38b4107aa9791f63bc847800ce8b8cb90aa0231") --Dlcname The Witcher 3: Wild Hunt - Beard and Hairstyle Set
+setManifestid(370001, "5979725543926398369", 17805328)
+--Dlcname The Witcher 3: Wild Hunt - Alternative Look for Yennefer
+addappid(370002, 1, "a66d950990c695f3297c531ceaffcf6ac5ff949761132dd280943e8dcebeb8f8") --Dlcname The Witcher 3: Wild Hunt - Alternative Look for Yennefer
+setManifestid(370002, "484236434286524845", 17246080)
+--Dlcname The Witcher 3: Wild Hunt - New Quest 'Contract: Missing Miners'
+addappid(370003, 1, "26138bf02e93588cd63346a8f3bb943ab5080c7bb71e1f7b4058afd57bb90f23") --Dlcname The Witcher 3: Wild Hunt - New Quest 'Contract: Missing Miners'
+setManifestid(370003, "5453197123234356101", 30763984)
+--Dlcname The Witcher 3: Wild Hunt - Nilfgaardian Armor Set
+addappid(373950, 1, "5c927c7af29a8d017b12cfd585b32987a0934ef7c4a6bc15564ea70846d0df83") --Dlcname The Witcher 3: Wild Hunt - Nilfgaardian Armor Set
+setManifestid(373950, "7457326912183925734", 62724336)
+--Dlcname The Witcher 3: Wild Hunt - Elite Crossbow Set
+addappid(373951, 1, "d94415a879eb6f7eb0bd2c382900fc1cc57afd85f4e7a7a3f91d17b184bc8d7a") --Dlcname The Witcher 3: Wild Hunt - Elite Crossbow Set
+setManifestid(373951, "1015260686331095979", 5817888)
+--Dlcname The Witcher 3: Wild Hunt - New Quest 'Fool's Gold'
+addappid(376390, 1, "35ac02e7fb00857f601c8b9a9c3fad17932cf2c7a301a2d0fedef61c350c911c") --Dlcname The Witcher 3: Wild Hunt - New Quest 'Fool's Gold'
+setManifestid(376390, "4380995726432289642", 92192704)
+--Dlcname The Witcher 3: Wild Hunt - 'Ballad Heroes' Neutral Gwent Card Set
+addappid(376391, 1, "a691c6929ba1ae81132b4baf31fd81cb87158c03e926d3310acf71d5a1286009") --Dlcname The Witcher 3: Wild Hunt - 'Ballad Heroes' Neutral Gwent Card Set
+setManifestid(376391, "6437141662912268782", 905408)
+--Dlcname The Witcher 3: Wild Hunt - New Quest: 'Scavenger Hunt: Wolf School Gear'
+addtoken(378640, "8056110346062720087")
+addappid(378640, 1, "13ddf115043ce87ceb0e52ec64938c102a665a483d48414db54425e52617cf35") --Dlcname The Witcher 3: Wild Hunt - New Quest: 'Scavenger Hunt: Wolf School Gear'
+setManifestid(378640, "5318422306227358494", 192381040)
+--Dlcname The Witcher 3: Wild Hunt - Alternative Look for Triss
+addtoken(378641, "13137250378595674538")
+addappid(378641, 1, "2a06dfffcdc645e88483b274bab505803dea0a498dcf0874efcca8d633b52c9d") --Dlcname The Witcher 3: Wild Hunt - Alternative Look for Triss
+setManifestid(378641, "4647447917504831785", 12234400)
+--Dlcname The Witcher 3: Wild Hunt - New Quest 'Contract: Skellige's Most Wanted'
+addappid(378642, 1, "8e82aa6c124304e5aafaf6319eb9cd5b84df9c2fd74f257f7936b58b7fa4a93e") --Dlcname The Witcher 3: Wild Hunt - New Quest 'Contract: Skellige's Most Wanted'
+setManifestid(378642, "2210049775439183344", 138835984)
+--Dlcname The Witcher 3: Wild Hunt - Skellige Armor Set
+addappid(378643, 1, "7165cd00ca2393e84e109e5f36ce0442707be4d472d573f9eecbea30c361f8de") --Dlcname The Witcher 3: Wild Hunt - Skellige Armor Set
+setManifestid(378643, "459508221271235268", 65087008)
+--Dlcname The Witcher 3: Wild Hunt - Alternative Look for Ciri
+addappid(378644, 1, "6f6e38d2efd42182a86e14afa5a2bf6f39c93e52fc173651a50e741c23379e15") --Dlcname The Witcher 3: Wild Hunt - Alternative Look for Ciri
+setManifestid(378644, "2928442289493234239", 62617776)
+--Dlcname The Witcher 3: Wild Hunt - New Quest 'Where the Cat and Wolf Play...'
+addappid(378645, 1, "b7f1a0a26896dd7f0f9d29334b5439afd695df215ca63bfb2f118a203dc63130") --Dlcname The Witcher 3: Wild Hunt - New Quest 'Where the Cat and Wolf Play...'
+setManifestid(378645, "6056630807565191690", 142404768)
+--Dlcname The Witcher 3: Wild Hunt - New Finisher Animations
+addappid(378646, 1, "7321f1ae7091f395e6f0854c10db5159d777bdc2697b9e6bd1f9ea74e218210a") --Dlcname The Witcher 3: Wild Hunt - New Finisher Animations
+setManifestid(378646, "7313260968752741219", 1596176)
+--Dlcname The Witcher 3: Wild Hunt - NEW GAME +
+addappid(378647, 1, "98e972b141076c8a6d72a292915cd5ef1d30eb73983070d8aa3f0aa77858f52a") --Dlcname The Witcher 3: Wild Hunt - NEW GAME +
+setManifestid(378647, "6347498022277232982", 7472)
+--Dlcname The Witcher 3: Wild Hunt - Blood and Wine
+addappid(378648, 1, "a3ef8bcc9af9edbd00aa55beadd796dfe0c980143c871b631291b05637113c1a") --Dlcname The Witcher 3: Wild Hunt - Blood and Wine
+addappid(310612, 1, "2778da309fe302e77a1578645da0f2f5cb1096822dab53c7ba456e829961d86f") --Dlc Windows Language Depot Polish The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(310612, "5455214373061651405", 1043090592)
+addappid(310613, 1, "e721336157973be351a375485bde985c8d5ad1821ef3d70f5072064c324309e8") --Dlc Windows Depot The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(310613, "2970969303001438150", 978842096)
+addappid(310614, 1, "41d483e998e4e5a4a256e08ea3cabde7c88cc81d0745015d064978b7f402ff07") --Dlc Windows Language Depot German The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(310614, "1512711109507003050", 987831552)
+addappid(310615, 1, "23336e7ee6b781dd4b9b9e2f071dde6bbc19a6dbb03908a24aa02915d680ecef") --Dlc Windows Language Depot French The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(310615, "8649692936145776844", 988453072)
+addappid(310616, 1, "49c227a33232cda12a88a053437fede2921516814540d309cf237e9e8404bc2f") --Dlc Windows Language Depot Russian The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(310616, "81296909978179260", 1041681248)
+addappid(310617, 1, "bd584800dd1556df065305414c425d43efd38b45fc614e722bfe5decd8f97bff") --Dlc Windows Language Depot Japanese The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(310617, "2115415390192780692", 1048750112)
+addappid(310618, 1, "20293d6cbcec4b428015a68110d3055b8f5909f1e669d999baadcc8eb24b12dd") --Dlc Windows Language Depot Brazilian The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(310618, "36196449033815259", 991929392)
+addappid(310619, 1, "1a710d9b03d4e5bbb6e1e57c0f3f322e52d2740b11803e4f718653b114af83ec") --Dlc Windows Depot The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(310619, "4855315366624356663", 628167680)
+addappid(373954, 1, "32b895886f889ecad112d5bc901e501e5173ca081595b3adbf528bc111aac3c2") --Dlc Windows Language Depot Schinese The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(373954, "9189601712632867387", 1010292736)
+addappid(373955, 1, "118abd22ef378fd5c62d2205668aa8fc4d713c3ce5c22c8e952695d5c79c0723") --Dlc Windows Language Depot Koreana The Witcher 3: Wild Hunt - Blood and Wine
+setManifestid(373955, "4126613168484560251", 997685696)
+setManifestid(378648, "5677496677655330654", 8847144064)
+--Dlcname The Witcher 3: Wild Hunt - Hearts of Stone
+addappid(378649, 1, "7c934ed4aa4f0e7a1a5ea204d411bebf48377f7b7e6fa0a114d011107d04684b") --Dlcname The Witcher 3: Wild Hunt - Hearts of Stone
+addappid(292043, 1, "eec6e98154b0e5cbc27742c27f9214034ac6ee649697ea9ecc087c7ec396f4ef") --Dlc Windows Language Depot Polish The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(292043, "2863314232165890237", 394720240)
+addappid(292044, 1, "f4b1aac8fe1d8c3d1b9fd4ed0766acc4be49037251cda4092b148ffbcb3422a9") --Dlc Windows Depot The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(292044, "908607634780300143", 371060592)
+addappid(292045, 1, "cbf6ed38ba7e79ed057715f18ea171ba7f7bed4a6728285d204113558db7ff4a") --Dlc Windows Language Depot German The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(292045, "4620621685241046742", 383524704)
+addappid(292046, 1, "649d7f21dfad2ba0674aab102c196e6ac3c53780138773a693a06cabeece070e") --Dlc Windows Language Depot French The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(292046, "1699589270478816105", 369120448)
+addappid(292047, 1, "47e36cc0c65f98ee521bada7a7418b7cd7f67e873fb012eec08276ff331994b3") --Dlc Windows Language Depot Russian The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(292047, "4188887637226014223", 382080608)
+addappid(292048, 1, "c365f8a91132be9140d40aff760898195077139810cf6c0d1c1e7cf8845dbb02") --Dlc Windows Language Depot Japanese The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(292048, "5283427417482321933", 390781072)
+addappid(292049, 1, "397dfb734f5a7b42a09b95f0c2d9cad324a1e34ddd9c0bc583c1bfb64c49dc85") --Dlc Windows Language Depot Brazilian The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(292049, "7686086885054708771", 381909584)
+addappid(310611, 1, "330dbf44f635b1b504b0bca11e0fc33fd8240e52f822083ef70cb65868c55b81") --Dlc Windows Depot The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(310611, "9087403118674428852", 258006176)
+addappid(373952, 1, "31a69f76a7088a6cb382e0ad8c5866456df9bcc8df53bacc9f878eb16af59c02") --Dlc Windows Language Depot Schinese The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(373952, "7987515327562995934", 385395312)
+addappid(373953, 1, "2b8b7f1f6d49cea5915d446c63860d74f36b43b1958a8da09df550a545a9e5dd") --Dlc Windows Language Depot Koreana The Witcher 3: Wild Hunt - Hearts of Stone
+setManifestid(373953, "2993703975089049690", 380910144)
+setManifestid(378649, "9216322386755119484", 1743391952)
+--Dlcname The Witcher 3: Wild Hunt Soundtrack
+addappid(1229320) --Dlcname The Witcher 3: Wild Hunt Soundtrack
+addappid(1229321, 1, "aabffdf335260ba3a1609a699afe267f038bd26f104d5080c6c84f4c2f954c5e") --Dlc Windows Depot The Witcher 3: Wild Hunt Soundtrack
+setManifestid(1229321, "2956293257306390314", 216737456)
+addappid(1229322, 1, "c8f0226c60c667cc2115ac95cc5b2fd10b11f3c924812ed419147a480bfc5e74") --Dlc Windows Depot The Witcher 3: Wild Hunt Soundtrack
+setManifestid(1229322, "951359575970231120", 579639456)
+--Dlcname The Witcher 3: Wild Hunt - Hearts of Stone Soundtrack
+addappid(1233280) --Dlcname The Witcher 3: Wild Hunt - Hearts of Stone Soundtrack
+addappid(1233281, 1, "4a5e8e0f4729f38afb17748f565f7a342968b6345f622d9031d51370acd5175a") --Dlc Windows Depot The Witcher 3: Wild Hunt - Hearts of Stone Soundtrack
+setManifestid(1233281, "7944135146154053598", 58619344)
+addappid(1233282, 1, "c496f66738b7af1b4fd7c233acf5b28d8b408aa255df02cf8d811b179bcf63dd") --Dlc Windows Depot The Witcher 3: Wild Hunt - Hearts of Stone Soundtrack
+setManifestid(1233282, "8832847868008237537", 163451312)
+--Dlcname The Witcher 3: Wild Hunt - Blood and Wine Soundtrack
+addappid(1233340) --Dlcname The Witcher 3: Wild Hunt - Blood and Wine Soundtrack
+addappid(1233341, 1, "a33c68d0a6b2a32c5ec9e5ed21783cd10e74fea9c71f94069fbee96e580dc9a3") --Dlc Windows Depot The Witcher 3: Wild Hunt - Blood and Wine Soundtrack
+setManifestid(1233341, "8748510798880361623", 133957200)
+addappid(1233342, 1, "4a4d2b29d5ca7726300c3bf228bf951bb4d2da5bcde3b206c6417d1f4a5ce40c") --Dlc Windows Depot The Witcher 3: Wild Hunt - Blood and Wine Soundtrack
+setManifestid(1233342, "5094747183155722711", 414754384)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(355880) --Dlcname The Witcher 3: Wild Hunt - Expansion Pass
+addappid(5006530) --Dlcname The Witcher 3: Wild Hunt — Songs of the Past
