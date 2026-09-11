8bd631eee084cbff32b9990e7ad7452e34f514fc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:15:15 +0800

    add 2477270.lua

--- 2477270.lua
+++ 2477270.lua
@@ -1,6 +1,2 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 2477270.lua
---Gamename Lust's Cupid
-addappid(2477270, 1, "5aca11b8bd166eef883183382d847649988b9d61772728cb64e8c1e92fcc2d34") --Mainappid Lust's Cupid
-addappid(2477271, 1, "1d68c2af57678e17e48935daee51d4fa41f10d47b531e6129d1d890d0436f2a5") --Main Windows Depot Lust's Cupid
-setManifestid(2477271, "6955632220301156321", 1135421776)
+addappid(2477270)
+setManifestid(2477271,"1418832092790912035")
