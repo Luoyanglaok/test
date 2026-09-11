bcdea1658db439ee83346d4ff7d911f3852225ac
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:16:32 +0800

    add 2193650.lua

--- 2193650.lua
+++ 2193650.lua
@@ -1,3 +1,6 @@
-addappid(2193650)
-addappid(2193651,0,"c95545f8720bb8fcf0fcd40a109b9bd27d04d6eec29640aaefca38d4c290ac3b")
-setManifestid(2193651,"8199114649059989072")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2193650.lua
+--Gamename SEX HELL 👹
+addappid(2193650) --Mainappid SEX HELL 👹
+addappid(2193651, 1, "c95545f8720bb8fcf0fcd40a109b9bd27d04d6eec29640aaefca38d4c290ac3b") --Main Windows Depot SEX HELL 👹
+setManifestid(2193651, "8199114649059989072", 1130473776)
