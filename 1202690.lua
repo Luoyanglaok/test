e204d64e7d81b3f38f11acd8e505c66b74783831
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 12:03:17 +0800

    add 1202690.lua

--- 1202690.lua
+++ 1202690.lua
@@ -1,7 +1,14 @@
-addappid(1202690)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1202691,0,"3da30e69657b2f3b2ab434ce2a86f8bb331691ff615436cd27314fc716f246db")
-setManifestid(1202691,"3084890007373336513")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1202690.lua
+--Gamename Sex with the Devil
+addappid(1202690, 1, "c9b70940310effe536a2c140a2a9e47b99f645ea5ca3f0492f2ca0b599c3c71a") --Mainappid Sex with the Devil
+addappid(1202691, 1, "3da30e69657b2f3b2ab434ce2a86f8bb331691ff615436cd27314fc716f246db") --Main Windows Depot Sex with the Devil
+setManifestid(1202691, "3084890007373336513", 3162016592)
+--Dlcname Sex with the Devil: VR Sex Scenes
+addappid(1757780, 1, "16f714ddfd12984ca758839cc66d0e83a142d8cce3f15d25bc91f64b5d01400c") --Dlcname Sex with the Devil: VR Sex Scenes
+setManifestid(1757780, "5670212906856995284", 922186480)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
