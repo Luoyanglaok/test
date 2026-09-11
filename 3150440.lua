2c3633608d81cd50bc49689752b9276ed0c8ff77
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:30:57 +0800

    add 3150440.lua

--- 3150440.lua
+++ 3150440.lua
@@ -1,5 +1,11 @@
-addappid(3150440)
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-addappid(3150441,0,"d6e3e5cb657c1847009f837d80ebfbf6a8bba4a76a8b2b5f3f4937762606aa61")
-setManifestid(3150441,"1397917422715362000")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3150440.lua
+--Gamename Laundry Store Simulator
+addappid(3150440) --Mainappid Laundry Store Simulator
+addappid(3150441, 1, "d6e3e5cb657c1847009f837d80ebfbf6a8bba4a76a8b2b5f3f4937762606aa61") --Main Windows Depot Laundry Store Simulator
+setManifestid(3150441, "1220747541305528781", 753281504)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
