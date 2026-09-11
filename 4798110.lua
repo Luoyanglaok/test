3d4aebb18cdd2a56027ceef133535a7225c84441
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:31:22 +0800

    add 4798110.lua

--- 4798110.lua
+++ 4798110.lua
@@ -3,6 +3,6 @@
 --Gamename Order Picker Simulator
 addappid(4798110) --Mainappid Order Picker Simulator
 addappid(4798111, 1, "e5fb88020a6deb355a2498af9a3841e68481918db66a48899e4321d0f5b97bcb") --Main Windows Depot Order Picker Simulator
-setManifestid(4798111, "1804844755693873210", 1042502992)
+setManifestid(4798111, "1270062528691934854", 1045629488)
 --Missing Dlcs
 --Dlcname Order Picker Simulator Demo
