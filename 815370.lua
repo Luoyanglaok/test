8d3ebbaccc2e4d37fa258a6f907abc825c6106c9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:26:34 +0800

    add 815370.lua

--- 815370.lua
+++ 815370.lua
@@ -1,17 +1,5 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 815370.lua
---Gamename Green Hell
-addappid(815370, 1, "c7fad83c696c4585abbd6d334a696814b452895badf6f5b087373f96be68dfe6") --Mainappid Green Hell
-addappid(815371, 1, "2d729703a7a551530f0fadf02b8c1da3cb944c4a898295bef1c19722f9f9f3cd") --Main Windows Depot Green Hell
-setManifestid(815371, "3938349524363268753", 360848)
-addappid(815372, 1, "03a5e9ecc1760fada61452368cefd3bb2234f78fb25bb7dc481ba9660d781dc7") --Main Windows Depot Green Hell
-setManifestid(815372, "1441393091310007518", 5867971680)
---Dlcname Green Hell Soundtrack
-addappid(2564710) --Dlcname Green Hell Soundtrack
-addappid(2564711, 1, "8e8b4f7a71597ed2490fd32c490c7a8e79ed9835e66fef328bd70c243c8bbbd3") --Dlc Windows Depot Green Hell Soundtrack
-setManifestid(2564711, "1327866468695058909", 169030032)
-addappid(2564712, 1, "29594e10b2246ce527414b2f9811969fa7fff90bc8a0f2c1b019c0292918b1cf") --Dlc Windows Depot Green Hell Soundtrack
-setManifestid(2564712, "3055971783223078804", 702433968)
---Dlcname Green Hell - Anniversary Edition Pack
-addappid(2990430, 1, "cc1e64d590434473930b6edd90d09027aa939a0d8b1fcc7d3ccd30f86ea21e1f") --Dlcname Green Hell - Anniversary Edition Pack
-setManifestid(2990430, "1413785688298351171", 1944543040)
+addappid(815370)
+addappid(2564710)
+setManifestid(815371,"3938349524363268753")
+setManifestid(815372,"1441393091310007518")
+setManifestid(2990430,"1413785688298351171")
