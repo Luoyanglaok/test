108cb9f60a5ddfcb99147ad3ff583e0c651c97ec
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:12:55 +0800

    add 440900.lua

--- 440900.lua
+++ 440900.lua
@@ -1,11 +1,42 @@
-addappid(440900)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(440901,0,"57ff2e2d0f7c2740c0c36426359060ef90b3367394dae24b861efff0752010fd")
-setManifestid(440901,"2657474047503583744")
-addappid(440902,0,"c4039510426aa98899efbe92e3777309401d95e2b341f6605db10afee7e2f7a9")
-setManifestid(440902,"6720935780644509559")
-addappid(577840,0,"5de478d01266d514b190336ec2af425bffc9016ca5c27869a17d38b216294fdc")
-setManifestid(577840,"1847681229230730152")
-addappid(440903,0,"55bc314377497f1dc799ba2accf9643afc380ad197155bca6dfb5326e8760270")
-setManifestid(440903,"8179650484142684528")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 440900.lua
+--Gamename Conan Exiles Enhanced
+addappid(440900, 1, "aab1dec96efdfe1f4aa13e2f009151c55e5f45df529f0b52a05d541e9d48bc64") --Mainappid Conan Exiles Enhanced
+addappid(440901, 1, "57ff2e2d0f7c2740c0c36426359060ef90b3367394dae24b861efff0752010fd") --Main Windows Depot Conan Exiles Enhanced
+setManifestid(440901, "3271588147291787679", 51618374800)
+addappid(440902, 1, "c4039510426aa98899efbe92e3777309401d95e2b341f6605db10afee7e2f7a9") --Main Windows Depot Conan Exiles Enhanced
+setManifestid(440902, "8374080680790355237", 116542976)
+addappid(440903, 1, "55bc314377497f1dc799ba2accf9643afc380ad197155bca6dfb5326e8760270") --Main Windows Depot Conan Exiles Enhanced
+setManifestid(440903, "4117553855113091513", 98276992)
+--Dlcname Conan Exiles Enhanced - Barbarian Edition
+addtoken(577840, "6316486689997394878")
+addappid(577840, 1, "5de478d01266d514b190336ec2af425bffc9016ca5c27869a17d38b216294fdc") --Dlcname Conan Exiles Enhanced - Barbarian Edition
+setManifestid(577840, "1847681229230730152", 541488448)
+--Dlcname Conan Exiles Enhanced: Isle of Siptah
+addappid(1206200, 1, "634db241a3f4000ce8cde76f15b56686f7dcd983aacae9869864dbdbc9afa4a0") --Dlcname Conan Exiles Enhanced: Isle of Siptah
+setManifestid(1206200, "8589589656543250479", 3507669744)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(760730) --Dlcname Conan Exiles Enhanced - The Imperial East Pack
+addappid(785120) --Dlcname Conan's Royal Armor
+addtoken(785120, "1246279920682173101")
+addappid(788220) --Dlcname Conan's Atlantean Sword
+addtoken(788220, "8776177628982512827")
+addappid(887120) --Dlcname Conan Exiles Enhanced - Jewel of the West Pack
+addappid(919390) --Dlcname Conan Exiles Enhanced - The Savage Frontier Pack
+addappid(972370) --Dlcname Conan Exiles Enhanced - Seekers of the Dawn Pack
+addappid(1040930) --Dlcname Conan Exiles Enhanced - Treasures of Turan Pack
+addappid(1066190) --Dlcname Conan Exiles Enhanced - Year 2 Season Pass
+addappid(1066250) --Dlcname Conan Exiles Enhanced - Complete Edition
+addappid(1076150) --Dlcname Conan Exiles Enhanced - The Riddle of Steel
+addappid(1134010) --Dlcname Conan Exiles Enhanced - Blood and Sand Pack
+addappid(1147100) --Dlcname Conan Exiles Enhanced - Debaucheries of Derketo Pack
+addappid(1173900) --Dlcname Conan Exiles Enhanced - Riders of Hyboria Pack
+addappid(1265640) --Dlcname Conan Exiles Enhanced - Architects of Argos Pack
+addappid(1410760) --Dlcname Conan Exiles Enhanced - Exclusive Early Access War-torn Rhino
+addappid(1668110) --Dlcname Conan Exiles Enhanced - People of the Dragon Pack
