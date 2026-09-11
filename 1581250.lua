1641d9004f3d4ae0742c19fdc763400820ad47f3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:08:22 +0800

    add 1581250.lua

--- 1581250.lua
+++ 1581250.lua
@@ -1,3 +1,6 @@
-addappid(1581250)
-addappid(1581251,0,"abc5aa0880775499054d048622c94e5d113def0b0f3e10e26365f13acb7ca973")
-setManifestid(1581251,"4384666357524486956")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1581250.lua
+--Gamename A Nerd who has sex with girls in "Another world" has become a HERO
+addappid(1581250) --Mainappid A Nerd who has sex with girls in "Another world" has become a HERO
+addappid(1581251, 1, "abc5aa0880775499054d048622c94e5d113def0b0f3e10e26365f13acb7ca973") --Main Windows Language Depot English A Nerd who has sex with girls in "Another world" has become a HERO
+setManifestid(1581251, "6359145371476004097", 478856000)
