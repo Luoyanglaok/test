ce26af56428d019a8184132e730ae666cfa02942
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:42:19 +0800

    add 2109460.lua

--- 2109460.lua
+++ 2109460.lua
@@ -1,7 +1,14 @@
-addappid(2109460)
-setManifestid(228988,"6645201662696499616")
-setManifestid(229006,"1784011429307107530")
-addappid(2109461,0,"f77ae6d25c175811951ff2fb68f85fb653d802e950393094175b1f84598999a1")
-setManifestid(2109461,"2220123672812714340")
-addappid(2179250,0,"f5a7fa1b530cd3dc9345d00c4222701be82991ddaff54d552c7fa29915bfd428")
-setManifestid(2179250,"191588528380038495")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2109460.lua
+--Gamename 乡村狂想曲
+addappid(2109460, 1, "d0865eaf21ec8e0177b947101bd0f45e4f745c99f12c901111b6a5296a4745d6") --Mainappid 乡村狂想曲
+addappid(2109461, 1, "f77ae6d25c175811951ff2fb68f85fb653d802e950393094175b1f84598999a1") --Main Windows Depot 乡村狂想曲
+setManifestid(2109461, "2220123672812714340", 1022583472)
+--Dlcname AppID 2179250
+addappid(2179250, 1, "f5a7fa1b530cd3dc9345d00c4222701be82991ddaff54d552c7fa29915bfd428") --Dlcname AppID 2179250
+setManifestid(2179250, "191588528380038495", 57697664)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
