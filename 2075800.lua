ea33aeabfdc79485ccee384cc0636fd77c6b61d5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 17:00:01 +0800

    add 2075800.lua

---
+++ 2075800.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2075800.lua
+--Gamename STAR WARS Zero Company™
+addappid(2075800, 1, "8739458192bb08e37151785656da5812d94e6372b0f59181874d85e39e7124e5") --Mainappid STAR WARS Zero Company™
+addappid(2075801, 1, "aa9607f71f9a605eca85df732f1e983f862efa0e5a2042765c056168b09511aa") --Main Windows Depot STAR WARS Zero Company™
+setManifestid(2075801, "6996140628888084124", 36678647392)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3701350) --Dlcname STAR WARS Zero Company™ Deluxe Edition Upgrade
+addappid(3701360) --Dlcname STAR WARS Zero Company™ Preorder Bonus
