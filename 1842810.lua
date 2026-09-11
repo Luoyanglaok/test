cdc34fd43531152f14087528ff822844f1f0106a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:51:41 +0800

    add 1842810.lua

--- 1842810.lua
+++ 1842810.lua
@@ -1,10 +1,16 @@
-addappid(1842810)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1842811,0,"45813a348477949c95bcc99acb76af79586f80853c6b91069277c1bd5e82f3e0")
-setManifestid(1842811,"2345313088801476794")
-addappid(1842812)
-addappid(1912160,0,"b8db1256184134521d31218a1ed50c22938ca4f38113849332c687d7c43fc784")
-setManifestid(1912160,"621021471606106286")
-addappid(1933920,0,"8ae22f1e8d887984debd24bb0b384bab5d1ff1824e4f44dffb0b28a3520d8621")
-setManifestid(1933920,"7150679046168922129")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1842810.lua
+--Gamename Taiko Risshiden V DX
+addappid(1842810) --Mainappid Taiko Risshiden V DX
+addappid(1842811, 1, "45813a348477949c95bcc99acb76af79586f80853c6b91069277c1bd5e82f3e0") --Main Windows Depot Taiko Risshiden V DX
+setManifestid(1842811, "2345313088801476794", 1521047936)
+--Dlcname Taiko Risshiden V DX - Cat Officer Mode
+addappid(1912160, 1, "b8db1256184134521d31218a1ed50c22938ca4f38113849332c687d7c43fc784") --Dlcname Taiko Risshiden V DX - Cat Officer Mode
+setManifestid(1912160, "621021471606106286", 28156176)
+--Dlcname AppID 1933920
+addtoken(1933920, "7293093011600907121")
+addappid(1933920, 1, "8ae22f1e8d887984debd24bb0b384bab5d1ff1824e4f44dffb0b28a3520d8621") --Dlcname AppID 1933920
+setManifestid(1933920, "7150679046168922129", 1089188128)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
