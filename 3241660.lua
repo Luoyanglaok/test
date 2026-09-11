f1bdb13040fd81139fa044eed134775e8e6c9406
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:56:53 +0800

    add 3241660.lua

--- 3241660.lua
+++ 3241660.lua
@@ -1,2 +1,6 @@
-addappid(3241660)
-setManifestid(3241661,"8225435692675993447")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3241660.lua
+--Gamename R.E.P.O.
+addappid(3241660, 1, "f647b73aa575d1ab056b68c1922a108a7bdeb79f6d472db883bde0a2ff307f48") --Mainappid R.E.P.O.
+addappid(3241661, 1, "0a0fcffb2de6fb0fa6caff076a5a8cb960903d17320968d5c042fd5a4f9ad2ad") --Main Windows Depot R.E.P.O.
+setManifestid(3241661, "8225435692675993447", 598508256)
