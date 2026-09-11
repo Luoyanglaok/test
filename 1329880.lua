328731b33557b2cf3388eb158003f2ee6f95bdec
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:46:23 +0800

    add 1329880.lua

--- 1329880.lua
+++ 1329880.lua
@@ -1,7 +1,18 @@
-addappid(1329880)
-addappid(1329881,0,"e7c4f5cdf6d3910ea059e38dcd706a986c99bc16b14ab0b9faeb43446a8e311c")
-setManifestid(1329881,"5410813116437150644")
-addappid(2286820,0,"372d09cda5bc99a6145d99a6824925f8f786dd6ab58f1e199b66b94d370c77c5")
-setManifestid(2286820,"553792975535920546")
-addappid(2286821,0,"792b344ea973101bf07334cf243731ebf1410ff3f20cf23f817d3ff8bfa62472")
-setManifestid(2286821,"1852584131706941833")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1329880.lua
+--Gamename Wild West Legacy
+addappid(1329880) --Mainappid Wild West Legacy
+addappid(1329881, 1, "e7c4f5cdf6d3910ea059e38dcd706a986c99bc16b14ab0b9faeb43446a8e311c") --Main Windows Depot Wild West Legacy
+setManifestid(1329881, "4811604193567914331", 5203483840)
+--Dlcname Wild West Legacy - Digital Supporter Pack
+addappid(2286820, 1, "372d09cda5bc99a6145d99a6824925f8f786dd6ab58f1e199b66b94d370c77c5") --Dlcname Wild West Legacy - Digital Supporter Pack
+setManifestid(2286820, "7916911749684037536", 194733984)
+--Dlcname Wild West Legacy - A Settlers Guide to the Wild West
+addappid(2286821, 1, "792b344ea973101bf07334cf243731ebf1410ff3f20cf23f817d3ff8bfa62472") --Dlcname Wild West Legacy - A Settlers Guide to the Wild West
+setManifestid(2286821, "7873715754099310326", 49838096)
+--Dlcname Wild West Legacy - Original Soundtrack
+addappid(2289710) --Dlcname Wild West Legacy - Original Soundtrack
+addappid(2289711, 1, "668b602b52fca2cea38319fc50eb03f2ffcd601ce4079920deb2cd272cd3d9ef") --Dlc Windows Depot Wild West Legacy - Original Soundtrack
+setManifestid(2289711, "7077996861017940965", 257653760)
+addappid(2289712, 1, "bb2009b6da33cc63b97031742a95a0ad386d980319ddfcd2af6efe3d753f3291") --Dlc Windows Depot Wild West Legacy - Original Soundtrack
+setManifestid(2289712, "5265882760031387017", 784089152)
