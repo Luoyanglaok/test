964d3d1ec392ed2f6f4a3467084bcad31019330e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:38:34 +0800

    add 2189450.lua

---
+++ 2189450.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2189450.lua
+--Gamename Boba Tea Shop Simulator
+addappid(2189450) --Mainappid Boba Tea Shop Simulator
+addappid(2189451, 1, "315cf43079cea68c82138d494b2f67d1a557d771733332891a28be526f6b8abb") --Main Windows Depot Boba Tea Shop Simulator
+setManifestid(2189451, "4974779204480190087", 530311440)
