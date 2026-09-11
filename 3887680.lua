20ebc0a012646cace892662af351f16cbd851eb5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:26:00 +0800

    add 3887680.lua

--- 3887680.lua
+++ 3887680.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 3887680.lua
-addappid(3887680, 0, "3bb1408b76e31d736595c5f7eb28a2ea0730b18301d747a98527376473902624")
+addappid(3887680, 0, "81df38ccd42e8f66ab6655d71d16096c445f57e3cc7eac041a1603a1758804fa")
