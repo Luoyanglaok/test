ea725b431c14a718f41d22047edeaf05b2c015e9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:16:10 +0800

    add 3920190.lua

---
+++ 3920190.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3920190.lua
+--Gamename SEX Guild Simulator ⚔🔞
+addappid(3920190) --Mainappid SEX Guild Simulator ⚔🔞
+addappid(3920191, 1, "1b7685d48061cd813bc8305fc3cd6a9179a1ce3a963716631fe14af4bee42e6a") --Main Windows Depot SEX Guild Simulator ⚔🔞
+setManifestid(3920191, "1890732921137296753", 440723904)
+--Dlcname SEX Guild Simulator ⚔️🔞 - Wallpapers Pack
+addappid(4450510, 1, "376df249ee3f184dc5940905716d2511c9176070f41ea9ffa953dd400db02268") --Dlcname SEX Guild Simulator ⚔️🔞 - Wallpapers Pack
+setManifestid(4450510, "3857043841253101337", 477865280)
