94abe4cf18dd63b28108cb0ac87972ff3707a991
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 01:54:21 +0800

    add 2198150.lua

--- 2198150.lua
+++ 2198150.lua
@@ -1,6 +1,19 @@
-addappid(2198150)
-setManifestid(228988,"6645201662696499616")
-addappid(2198151,0,"a64cc2160b07e8fd86f1a2f001987b46e6760e59b3e0ac07915743a4eb695bac")
-setManifestid(2198151,"2693167547300432593")
-addappid(2198152,0,"ef668181df894c351bdd06264513360aebd32539049befc1f36deae429957b8c")
-setManifestid(2198152,"3114081683394066473")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2198150.lua
+--Gamename Tiny Glade
+addappid(2198150, 1, "e3664316121f90e5be70e8c56eb118f18f210795301a46ebb8b3686372603375") --Mainappid Tiny Glade
+addappid(2198151, 1, "a64cc2160b07e8fd86f1a2f001987b46e6760e59b3e0ac07915743a4eb695bac") --Main Windows Depot Tiny Glade
+setManifestid(2198151, "4958143249067362694", 1578790448)
+addappid(2198152, 1, "ef668181df894c351bdd06264513360aebd32539049befc1f36deae429957b8c") --Main Linux Depot Tiny Glade
+setManifestid(2198152, "976796174160723395", 1579968304)
+addappid(2198153, 1, "61febbd9d716de077197dc892ba4a12a7d2c64e1c2889a80672ff5a3938c0c2f") --Main Macos Depot Tiny Glade
+setManifestid(2198153, "8898327245825255910", 1564372320)
+--Dlcname Tiny Glade Soundtrack
+addappid(3226080) --Dlcname Tiny Glade Soundtrack
+addappid(3226081, 1, "2375ec4e71066e6fe59299ff72cbe7c9ef9e20c400869a12471a32bbe1745925") --Dlc Windows Depot Tiny Glade Soundtrack
+setManifestid(3226081, "1456892718890955075", 84494240)
+addappid(3226082, 1, "5d1352f1b768186c33afa9a6904d74dd96bdd6e28f3564061dd59a3c3be9734a") --Dlc Windows Depot Tiny Glade Soundtrack
+setManifestid(3226082, "7498053464605565597", 1035827008)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
