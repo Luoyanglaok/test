a754ffee24df6de77f15f24d114c2c6278eeeb75
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:15:17 +0800

    add 1475810.lua

--- 1475810.lua
+++ 1475810.lua
@@ -1,7 +1,27 @@
-addappid(1475810)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1475811,0,"6044597d5ce5e2a639d6651712701e816115dca897c115e8cd756c0b2cae4454")
-setManifestid(1475811,"4910392376862342847")
-addappid(1475812,0,"3943ef5d4b3a97877847fe80d0e0662bf583777800d70f80a04946dd3ff39143")
-setManifestid(1475812,"679915731015348045")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1475810.lua
+--Gamename Ghostwire: Tokyo
+addappid(1475810, 1, "84260bb289a869883362af02ffccd7a1cd6fa16337f747bff65ab7bfb576329b") --Mainappid Ghostwire: Tokyo
+addappid(1475811, 1, "6044597d5ce5e2a639d6651712701e816115dca897c115e8cd756c0b2cae4454") --Main Windows Depot Ghostwire: Tokyo
+setManifestid(1475811, "4910392376862342847", 24023264336)
+addappid(1475812, 1, "3943ef5d4b3a97877847fe80d0e0662bf583777800d70f80a04946dd3ff39143") --Main Windows Depot Ghostwire: Tokyo
+setManifestid(1475812, "679915731015348045", 166121808)
+--Dlcname Ghostwire: Tokyo Original Game Soundtrack
+addappid(1944840) --Dlcname Ghostwire: Tokyo Original Game Soundtrack
+addappid(1944841, 1, "306ed82629f34a45c6abafdb09fd0385625b8afd4087b834826947245eb51547") --Dlc Windows Depot Ghostwire: Tokyo Original Game Soundtrack
+setManifestid(1944841, "3553460826576368769", 350603232)
+addappid(1944842, 1, "7d11294d04f326ed0614226e5090997ba9356f101ae86eeff881fdc2b3f0d591") --Dlc Windows Depot Ghostwire: Tokyo Original Game Soundtrack
+setManifestid(1944842, "8289528075359631245", 2436307312)
+--Dlcname Ghostwire: Tokyo - Spider's Thread Soundtrack
+addappid(2650130) --Dlcname Ghostwire: Tokyo - Spider's Thread Soundtrack
+addappid(2650131, 1, "f0179dffec41bcd4566fc7672f9e0830fa31ca35eb8c57151c214052139c9fe5") --Dlc Windows Depot Ghostwire: Tokyo - Spider's Thread Soundtrack
+setManifestid(2650131, "325716342498306703", 44650672)
+addappid(2650132, 1, "7cd509081aff2f9616fd4454aa9011005b41cde4f99c011904480fc7cdcc22ae") --Dlc Windows Depot Ghostwire: Tokyo - Spider's Thread Soundtrack
+setManifestid(2650132, "8379864717705943998", 297353632)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1702820) --Dlcname Ghostwire: Tokyo - Deluxe Upgrade
+addappid(1702821) --Dlcname GhostWire: Tokyo - Preorder Outfits Pack
