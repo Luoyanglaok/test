8e5195c41509ca25ad6aea350523bab63bde4b57
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 01:17:28 +0800

    add 2651570.lua

--- 2651570.lua
+++ 2651570.lua
@@ -1,7 +1,10 @@
-addappid(2651570)
-addappid(2651571,0,"59f80dedd0a2710cd83d050ba933cf31bbcc23a680d006685dd2659b36e9fb6e")
-setManifestid(2651571,"7946351646398154976")
-addappid(2651572,0,"e85c173c130e25ce9564ce01c6f8fddb1ef39ff1007655bda707dd573fdbb943")
-setManifestid(2651572,"7692520352080898622")
-addappid(2651573,0,"2261ab898902cb6909c51504052d6c0d3dda6c4ccd7878cafe3d16204d2dc0ec")
-setManifestid(2651573,"9051908932925258890")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2651570.lua
+--Gamename Lilim wants to Lv up♥
+addappid(2651570, 1, "dbbb5cf31b33a8852757dbf375a64dfabfbc4c80d8f71be291ca6858ca3376c0") --Mainappid Lilim wants to Lv up♥
+addappid(2651571, 1, "59f80dedd0a2710cd83d050ba933cf31bbcc23a680d006685dd2659b36e9fb6e") --Main Windows Depot Lilim wants to Lv up♥
+setManifestid(2651571, "7946351646398154976", 741436096)
+addappid(2651572, 1, "e85c173c130e25ce9564ce01c6f8fddb1ef39ff1007655bda707dd573fdbb943") --Main Macos Depot Lilim wants to Lv up♥
+setManifestid(2651572, "7692520352080898622", 740812176)
+addappid(2651573, 1, "2261ab898902cb6909c51504052d6c0d3dda6c4ccd7878cafe3d16204d2dc0ec") --Main Linux Depot Lilim wants to Lv up♥
+setManifestid(2651573, "9051908932925258890", 749325424)
