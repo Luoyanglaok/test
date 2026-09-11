db7a8ac29421ce6113f0d8d2328b0f8e1d147ccd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:02:20 +0800

    add 1465360.lua

--- 1465360.lua
+++ 1465360.lua
@@ -1,6 +1,60 @@
-addappid(1465360)
-setManifestid(228989,"3514306556860204959")
-addappid(1465361,0,"0dd93272d1d47d5943b93081730b61e39d20a49e42873ca743dcd30322161fb0")
-setManifestid(1465361,"3518876189924674658")
-addappid(1465362,0,"924ebaf5547a976d7080e139e3ce420ce10a76bdcd89d74fa74a98fb7312b7fc")
-setManifestid(1465362,"8996226671340099453")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1465360.lua
+--Gamename SnowRunner
+addappid(1465360, 1, "73f8f030255f23904e82de2afed639196197db79e1dc6e22d0d7e8eeb220fbd3") --Mainappid SnowRunner
+addappid(1465361, 1, "0dd93272d1d47d5943b93081730b61e39d20a49e42873ca743dcd30322161fb0") --Main Windows Depot SnowRunner
+setManifestid(1465361, "1017218816943865737", 52188593792)
+addappid(1465362, 1, "924ebaf5547a976d7080e139e3ce420ce10a76bdcd89d74fa74a98fb7312b7fc") --Main Macos Depot SnowRunner
+setManifestid(1465362, "8920584982304117109", 34545982944)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(1533000) --Dlcname SnowRunner - Sabertooth Livery
+addappid(1533001) --Dlcname SnowRunner - Navistar 5000-MV Tractor
+addappid(1533002) --Dlcname SnowRunner - High Roller Pack
+addappid(1533003) --Dlcname SnowRunner - Loaded Dice Vinyl Wrap
+addappid(1533004) --Dlcname SnowRunner - Scorched Vinyl Wrap
+addappid(1533005) --Dlcname SnowRunner - True Colors Vinyl Wrap
+addappid(1533006) --Dlcname SnowRunner - Season 1: Search & Recover
+addappid(1533007) --Dlcname SnowRunner - Season 2: Explore & Expand
+addappid(1533008) --Dlcname SnowRunner - Season 3: Locate & Deliver
+addappid(1533009) --Dlcname SnowRunner - Season 4: New Frontiers
+addappid(1533010) --Dlcname SnowRunner - Western Star 49X
+addappid(1533011) --Dlcname SnowRunner - Clasico Pack
+addappid(1585810) --Dlcname SnowRunner - Anniversary DLC
+addappid(1585811) --Dlcname SnowRunner - TATRA Dual Pack
+addappid(1585812) --Dlcname SnowRunner - Season 5: Build & Dispatch
+addappid(1585813) --Dlcname SnowRunner - Season 6: Haul & Hustle
+addappid(1585814) --Dlcname SnowRunner - Season 7: Compete & Conquer
+addappid(1585815) --Dlcname SnowRunner - Season 8: Grand Harvest
+addappid(1585816) --Dlcname SnowRunner - GMC Brigadier
+addappid(1613110) --Dlcname SnowRunner - Jeep Dual Pack
+addappid(1613111) --Dlcname SnowRunner - Burning Bright Vinyl Wrap Pack
+addappid(1785850) --Dlcname SnowRunner - Land Rover Dual Pack
+addappid(1838500) --Dlcname SnowRunner - Crocodile Pack
+addappid(2058880) --Dlcname SnowRunner - Rezvani Hercules
+addappid(2058881) --Dlcname SnowRunner - Western Star Wolf Pack
+addappid(2154170) --Dlcname SnowRunner - Save the Day Vinyl Wrap Pack
+addappid(2154171) --Dlcname SnowRunner - Season 9: Renew & Rebuild
+addappid(2154172) --Dlcname SnowRunner - Season 10: Fix & Connect
+addappid(2154173) --Dlcname SnowRunner - Season 11: Lights & Cameras
+addappid(2154174) --Dlcname SnowRunner - Season 12: Public Energy
+addappid(2353720) --Dlcname SnowRunner - The Mastodon
+addappid(2353721) --Dlcname SnowRunner - Kenworth W990
+addappid(2353722) --Dlcname SnowRunner - Jack of All Treads Tire Pack
+addappid(2353723) --Dlcname SnowRunner - Kenworth Dual Pack
+addappid(2709430) --Dlcname SnowRunner - Salt of the Earth Vinyl Wrap Pack
+addappid(2709450) --Dlcname SnowRunner - Season 13: Dig & Drill
+addappid(2709460) --Dlcname SnowRunner - Season 14: Reap & Sow
+addappid(2709470) --Dlcname SnowRunner - Season 15: Oil & Dirt
+addappid(2709480) --Dlcname SnowRunner - Season 16: High Voltage
+addappid(2856380) --Dlcname SnowRunner - The Atom
+addappid(3023640) --Dlcname SnowRunner - Rezvani Tank
+addappid(3398280) --Dlcname SnowRunner - Mack Dual Pack
+addappid(3623240) --Dlcname SnowRunner - Freightliner & Western Star Dual Pack
+addappid(3623250) --Dlcname SnowRunner - Dragon Wagons Dual Pack
+addappid(3777600) --Dlcname SnowRunner – Pathfinders Vinyl Wrap Pack
+addappid(3777610) --Dlcname SnowRunner - Season 17: Repair & Rescue
+addappid(3777620) --Dlcname SnowRunner - Season 18: Patch & Power
+addappid(3777650) --Dlcname SnowRunner - Mercedes-Benz Trucks Dual Pack 1
+addappid(3777660) --Dlcname SnowRunner - Mercedes-Benz Trucks Dual Pack 2
