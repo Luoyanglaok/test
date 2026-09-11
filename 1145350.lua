b342ab16e3205827bb86de4c1cf7d3243c43b13c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:51:25 +0800

    add 1145350.lua

--- 1145350.lua
+++ 1145350.lua
@@ -1,8 +1,19 @@
-addappid(1145350)
-setManifestid(228988,"6645201662696499616")
-addappid(1145352,0,"ff02298666a5470ddca19f89593983136f6d1727fc82957ca6b847133783b4e7")
-setManifestid(1145352,"1213040274271553936")
-addappid(1145354,0,"71f0364cb7bd2b7d7755605c7983bf2b73101a6e2e0ef6b7082d275a4650f7e5")
-setManifestid(1145354,"4006996105029385842")
-addappid(1145355,0,"67def0278f458945e71a75914c686e5bf4d3f90c8b4320795c3b78ad999afc56")
-setManifestid(1145355,"7779338011844125043")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1145350.lua
+--Gamename Hades II
+addappid(1145350, 1, "b4b04fea10664cb3ea0d6e6f8876e3350ed60657be95ed215f7d9795036dd11f") --Mainappid Hades II
+addappid(1145352, 1, "ff02298666a5470ddca19f89593983136f6d1727fc82957ca6b847133783b4e7") --Main Windows Depot Hades II
+setManifestid(1145352, "4474613126080913417", 10515236912)
+addappid(1145354, 1, "71f0364cb7bd2b7d7755605c7983bf2b73101a6e2e0ef6b7082d275a4650f7e5") --Main Windows Depot Hades II
+setManifestid(1145354, "3523419235077190492", 44571408)
+addappid(1145355, 1, "67def0278f458945e71a75914c686e5bf4d3f90c8b4320795c3b78ad999afc56") --Main Macos Depot Hades II
+setManifestid(1145355, "4358145244193201792", 10537006496)
+--Dlcname Hades II Original Soundtrack
+addappid(2950840) --Dlcname Hades II Original Soundtrack
+addappid(2950841, 1, "e3caba4f78ee373466d01e82c8533e6d858847bd48fefb2cba4724554537a206") --Dlc Windows Depot Hades II Original Soundtrack
+setManifestid(2950841, "4161983320866568293", 5105678976)
+addappid(2950842, 1, "fca5e03ba1607d809bb6410da5a466f2e3c9456b3ef75a1fa4f3b0427ae9fc34") --Dlc Windows Depot Hades II Original Soundtrack
+setManifestid(2950842, "6358127939422595540", 493302096)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
