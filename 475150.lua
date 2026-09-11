e637cac8e476802653ac8f0392ffa3d96467b313
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 17:42:21 +0800

    add 475150.lua

--- 475150.lua
+++ 475150.lua
@@ -1,17 +1,30 @@
-addappid(475150)
-setManifestid(228981,"7613356809904826842")
-setManifestid(228982,"6413394087650432851")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(475152,0,"aa44d00ff5b62b6c416f4ce7b9fc8fb3247c35edc0c67f4284effe5cebc9fbb7")
-setManifestid(475152,"6427448782216844262")
-addappid(475151,0,"c1c3edb704d43929929fa371f6e02864b265e7d85a33430f789a2e6a4d6869f8")
-setManifestid(475151,"5684991738681127654")
-addappid(741350,0,"58951f90a73c082db0d47f5b217fd4026c5b6bb5a21a0e537bd069e5d7078d26")
-setManifestid(741350,"1505176620565420855")
-addappid(760270,0,"cc33840e6436454777bf485a4bc8807cd79883b8ec34d72d814704e68ba07c5e")
-setManifestid(760270,"2843544694026273287")
-addappid(1071200,0,"05f1bf6799fc248979458614b61b543dedb3bb085232409f294c70210e5c61e1")
-setManifestid(1071200,"4240267134625557697")
-addappid(1804460,0,"0088e414403d323b326c3335d3467cc5d112baa680f869d978a08853f0b0d7d7")
-setManifestid(1804460,"3938875257587627587")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 475150.lua
+--Gamename Titan Quest Anniversary Edition
+addappid(475150, 1, "f59aa5c950b96da3b2bcbd71e8714048ed9ff198e587d57218e02f6a7afb3ba9") --Mainappid Titan Quest Anniversary Edition
+addappid(475151, 1, "c1c3edb704d43929929fa371f6e02864b265e7d85a33430f789a2e6a4d6869f8") --Main Windows Depot Titan Quest Anniversary Edition
+setManifestid(475151, "5684991738681127654", 13499713344)
+addappid(475152, 1, "aa44d00ff5b62b6c416f4ce7b9fc8fb3247c35edc0c67f4284effe5cebc9fbb7") --Main Windows Depot Titan Quest Anniversary Edition
+setManifestid(475152, "6147791973602619787", 27394144)
+--Dlcname Titan Quest: Ragnarök
+addappid(741350, 1, "58951f90a73c082db0d47f5b217fd4026c5b6bb5a21a0e537bd069e5d7078d26") --Dlcname Titan Quest: Ragnarök
+setManifestid(741350, "1505176620565420855", 2571227344)
+--Dlcname AppID 760270
+addtoken(760270, "16566657934259524556")
+addappid(760270, 1, "cc33840e6436454777bf485a4bc8807cd79883b8ec34d72d814704e68ba07c5e") --Dlcname AppID 760270
+setManifestid(760270, "2843544694026273287", 299182928)
+--Dlcname Titan Quest: Atlantis
+addappid(1071200, 1, "05f1bf6799fc248979458614b61b543dedb3bb085232409f294c70210e5c61e1") --Dlcname Titan Quest: Atlantis
+setManifestid(1071200, "4240267134625557697", 2236882528)
+--Dlcname Titan Quest: Eternal Embers
+addappid(1804460, 1, "0088e414403d323b326c3335d3467cc5d112baa680f869d978a08853f0b0d7d7") --Dlcname Titan Quest: Eternal Embers
+setManifestid(1804460, "3938875257587627587", 2474743776)
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
