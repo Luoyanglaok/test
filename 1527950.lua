69f252bb926d5108debaec552e55b4f8c4de0f1a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:06:26 +0800

    add 1527950.lua

--- 1527950.lua
+++ 1527950.lua
@@ -1,4 +1,23 @@
-addappid(1527950)
-setManifestid(228988,"6645201662696499616")
-addappid(1527951,0,"95bd24e7b06dd6b363b41cd5735569fe66f9d030ec1b11d30e46c13171a17852")
-setManifestid(1527951,"7047736929626325014")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1527950.lua
+--Gamename Wartales
+addappid(1527950, 1, "df3fb79e39a23369ab2a734aa3168b065638b173cb7cc3842a16f676adbfbeee") --Mainappid Wartales
+addappid(1527951, 1, "95bd24e7b06dd6b363b41cd5735569fe66f9d030ec1b11d30e46c13171a17852") --Main Windows Depot Wartales
+setManifestid(1527951, "703474146168077072", 16271262560)
+--Dlcname Wartales - Music: The Official Soundtrack
+addappid(3533110) --Dlcname Wartales - Music: The Official Soundtrack
+addappid(3533111, 1, "3354e8e7365b10273c0c78b9cd7518719e8363fd3a2b09af934059b80307d624") --Dlc Windows Depot Wartales - Music: The Official Soundtrack
+setManifestid(3533111, "1590043208140971743", 113068064)
+addappid(3533112, 1, "ac370f744d81811d0f2349a8f98ddbaa19edad8c1d9771965373b165c78db5aa") --Dlc Windows Depot Wartales - Music: The Official Soundtrack
+setManifestid(3533112, "1840744157618419814", 1756218592)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(2692400) --Dlcname Wartales - Expansion: Pirates of Belerion
+addappid(2903960) --Dlcname Wartales - Contract: The Tavern Opens!
+addappid(3112270) --Dlcname Wartales - Contract: The Pits
+addappid(3335020) --Dlcname Wartales - Expansion: The Skelmar Invasion
+addappid(3559560) --Dlcname Wartales - Contract: The Beast Hunt
+addappid(3899740) --Dlcname Wartales - Contract: The Fief
+addappid(4165520) --Dlcname Wartales - Expansion: The Curse of Rigel
+addappid(4490120) --Dlcname Wartales - Contract: Fires in the Capital
