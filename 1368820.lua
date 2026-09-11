19e249b5adc4a45cadca132e0ab9840ea451b965
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 01:50:44 +0800

    add 1368820.lua

--- 1368820.lua
+++ 1368820.lua
@@ -1,9 +1,15 @@
-addappid(1368820)
-addappid(228984)
-setManifestid(228984,"2547553897526095397")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1368821,0,"f0743d378ea2b0ec9a037ce889330cec3dbfe17982142a7ab26f82427d081faf")
-setManifestid(1368821,"3464953341930095826")
-addappid(1368822,0,"8015b2ef9ac9ed50d5029b603d2d802cf5244ac035c4a109e4f95ca47f7fc604")
-setManifestid(1368822,"4572733481233066561")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1368820.lua
+--Gamename RollerCoaster Tycoon® 3: Complete Edition
+addappid(1368820, 1, "194959e87a7d1dfb3a392790bf2bbd2a71b59f71e2d2ed0aac54a8419564e41a") --Mainappid RollerCoaster Tycoon® 3: Complete Edition
+addappid(1368821, 1, "f0743d378ea2b0ec9a037ce889330cec3dbfe17982142a7ab26f82427d081faf") --Main Windows Depot RollerCoaster Tycoon® 3: Complete Edition
+setManifestid(1368821, "6085498102233317421", 556869456)
+addappid(1368822, 1, "8015b2ef9ac9ed50d5029b603d2d802cf5244ac035c4a109e4f95ca47f7fc604") --Main Macos Depot RollerCoaster Tycoon® 3: Complete Edition
+setManifestid(1368822, "4572733481233066561", 539402608)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
