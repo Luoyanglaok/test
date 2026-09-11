d34576778a823919de75a5882072088582146528
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:58:45 +0800

    add 350310.lua

--- 350310.lua
+++ 350310.lua
@@ -1,5 +1,13 @@
-addappid(350310)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(350311,0,"7d4182ace264a26383af8733bfa078a4388bc7a6a40d3ad3aabb0376a770d954")
-setManifestid(350311,"5373087412483143381")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 350310.lua
+--Gamename BLADESTORM: Nightmare
+addappid(350310) --Mainappid BLADESTORM: Nightmare
+addappid(350311, 1, "7d4182ace264a26383af8733bfa078a4388bc7a6a40d3ad3aabb0376a770d954") --Main Windows Depot BLADESTORM: Nightmare
+setManifestid(350311, "5373087412483143381", 9518370736)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(363910) --Dlcname GAMECITYオンラインユーザー登録シリアルナンバー
+addappid(363911) --Dlcname my GAMECITY GCコインシリアルナンバー
