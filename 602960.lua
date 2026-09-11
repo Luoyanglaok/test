a4e9627d098e3161268837286dd3e41ead2bfcbb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:24:27 +0800

    add 602960.lua

--- 602960.lua
+++ 602960.lua
@@ -1,20 +1,6 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 602960.lua
---Gamename Barotrauma
-addappid(602960, 1, "78724f00a8538c7548a770db37d02b6d5872204c12aa66972ddf965bd6b45792") --Mainappid Barotrauma
-addappid(602961, 1, "9548ba939f90ad1341ef0ea0125642dabf42d425fed0a4a5b2ad3d40a88af1d2") --Main Windows Depot Barotrauma
-setManifestid(602961, "1417043812903529152", 1071868768)
-addappid(602962, 1, "aa05725821345dfdedd93eaef1f0f4fb6e399b95cffc47dbabf9134a7eeca1d9") --Main Linux Depot Barotrauma
-setManifestid(602962, "1060615628637975021", 1073693760)
-addappid(602963, 1, "9420d15bdd3429b9391b475f1d1c59499535e408b59dcc0053cfa369cdbb8609") --Main Macos Depot Barotrauma
-setManifestid(602963, "32495984955377582", 1075149120)
---Dlcname Barotrauma - Supporter Pack
-addappid(1197650, 1, "179a1e7986e8029892a8442b25794a7e3e6ca59e33a87f8b00233ab4a8fe5ddb") --Dlcname Barotrauma - Supporter Pack
-setManifestid(1197650, "4582887644478638575", 432726192)
---Dlcname Barotrauma - Soundtrack
-addappid(1197651, 1, "880374e21d84e1b54d094f73147f62e9aef49552b1e0a9a2f0bbfc04ae2ac069") --Dlcname Barotrauma - Soundtrack
-setManifestid(1197651, "6346527442254387436", 431963696)
-addappid(1670640, 1, "1e35785f229c02d68d16234645fcccb600afc2be5783df82a77896c835d4759b") --Dlc Windows Depot Barotrauma - Soundtrack
-setManifestid(1670640, "6625677740856929075", 1861930416)
---Missing Dlcs
---Dlcname Barotrauma - Home & Harbor Expansion
+addappid(602960)
+addappid(3714450)
+addappid(1197650)
+addappid(1197651)
+setManifestid(602961,"1417043812903529152")
+setManifestid(1197650,"4582887644478638575")
