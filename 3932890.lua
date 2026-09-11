a071b14adbd9b6c77c9ac692ee9d1dfee34b3be0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:13:34 +0800

    add 3932890.lua

---
+++ 3932890.lua
@@ -0,0 +1,45 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3932890.lua
+--Gamename Escape from Tarkov
+addappid(3932890, 1, "a43bd6d20ab68d15d5edc9aec0debd22d72fdec082c14b60593dc6e16cf79aac") --Mainappid Escape from Tarkov
+addappid(3932891, 1, "8cf57d9d3c35a5371e82341a2a0aa834b0f4edc929ae700566d6b2ebe66e7210") --Main Windows Depot Escape from Tarkov
+setManifestid(3932891, "3078757278777809523", 48436561568)
+addappid(3932892, 1, "6a03571f28db62fb25aa43c1c39bf870b8848c97b3624dd0e846023438411904") --Main Windows Depot Escape from Tarkov
+setManifestid(3932892, "7527847050651808918", 173277552)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(4090950) --Dlcname Escape from Tarkov: Left Behind - Expansion Pack
+addappid(4090970) --Dlcname Escape from Tarkov: Prepare for Escape - Expansion Pack
+addappid(4090980) --Dlcname Escape From Tarkov: The Unheard - Expansion Pack
+addappid(4091290) --Dlcname Escape from Tarkov: PVE Zone
+addappid(4229530) --Dlcname Escape from Tarkov: USEC - Gold Squadron
+addappid(4229540) --Dlcname Escape from Tarkov: BEAR - Senezh
+addappid(4407420) --Dlcname Escape from Tarkov: BEAR - Arktika
+addappid(4407430) --Dlcname Escape from Tarkov: USEC - Fahrenheit
+addappid(4490930) --Dlcname Escape from Tarkov: TarCoin Ingame Currency
+addappid(4531790) --Dlcname Escape from Tarkov: BEAR - Classic
+addappid(4531800) --Dlcname Escape from Tarkov: BEAR - Cyclone
+addappid(4531810) --Dlcname Escape from Tarkov: BEAR - G99
+addappid(4531820) --Dlcname Escape from Tarkov: BEAR - Instructor
+addappid(4531830) --Dlcname Escape from Tarkov: BEAR - Morozko
+addappid(4531840) --Dlcname Escape from Tarkov: BEAR - Nord
+addappid(4531850) --Dlcname Escape from Tarkov: BEAR - Oldschool
+addappid(4531860) --Dlcname Escape from Tarkov: BEAR - Vacation
+addappid(4531870) --Dlcname Escape from Tarkov: BEAR - Zaslon
+addappid(4531890) --Dlcname Escape from Tarkov: USEC - Day off
+addappid(4531900) --Dlcname Escape from Tarkov: USEC - Deadly Frog
+addappid(4531910) --Dlcname Escape from Tarkov: USEC - Night Patrol
+addappid(4531920) --Dlcname Escape from Tarkov: USEC - Woodland Infiltrator
+addappid(4531930) --Dlcname Escape from Tarkov: USEC - Urban Responder
+addappid(4531940) --Dlcname Escape from Tarkov: USEC - AC Ranger Green
+addappid(4531950) --Dlcname Escape from Tarkov: USEC - Vanguard
+addappid(4531960) --Dlcname Escape from Tarkov: USEC - Special Ops
+addappid(4531970) --Dlcname Escape from Tarkov: USEC - Scadi
+addappid(4589090) --Dlcname Escape from Tarkov: USEC - ScavTac
+addappid(4589100) --Dlcname Escape from Tarkov: BEAR - Three Stripes
+addappid(4764340) --Dlcname Escape from Tarkov: BEAR - Slavianka
+addappid(4764350) --Dlcname Escape from Tarkov: USEC - GEN4
+addappid(4898900) --Dlcname Escape from Tarkov: TarCoin Special Offer
