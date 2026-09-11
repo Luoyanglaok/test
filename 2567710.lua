24f42842ae309915560dbb8e370e534a8708f31e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 17:19:26 +0800

    add 2567710.lua

--- 2567710.lua
+++ 2567710.lua
@@ -1,3 +1,6 @@
-addappid(2567710)
-addappid(2567711,0,"0119167cd08cd0262abea5e687f962954f53253040d63bb516afe99f6a1f5660")
-setManifestid(2567711,"1653881811312102815")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2567710.lua
+--Gamename SEXY LADIES
+addappid(2567710) --Mainappid SEXY LADIES
+addappid(2567711, 1, "0119167cd08cd0262abea5e687f962954f53253040d63bb516afe99f6a1f5660") --Main Windows Depot SEXY LADIES
+setManifestid(2567711, "1653881811312102815", 141200080)
