7465a7ac94e8c928f05ed130b0e4ad187d235f70
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:06:43 +0800

    add 3446240.lua

---
+++ 3446240.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3446240.lua
+--Gamename 宗门起源(Eastern Era)
+addappid(3446240, 1, "72025f6589adbe085fe83773d0423225dadb0c32b0eeb2f865984c8d71e5c736") --Mainappid 宗门起源(Eastern Era)
+addtoken(3446240, "12366859424522752049")
+addappid(3446241, 1, "ddbae286524f900e39fb2c0e6d35401e7a94444b87a43825104ae7a128279543") --Main Windows Depot 宗门起源(Eastern Era)
+setManifestid(3446241, "8499296820765998330", 14286897872)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
