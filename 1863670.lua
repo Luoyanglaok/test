d5c611984240fe6ec5fb52d0f71cf985e1a4971d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 03:02:47 +0800

    add 1863670.lua

--- 1863670.lua
+++ 1863670.lua
@@ -1,8 +1,14 @@
-addappid(1863670)
-setManifestid(228989,"3514306556860204959")
-addappid(1863671,0,"48f0861f50d491d54e04a73a1c18681e5ac8ab1f8d2f7e5ee3e8be1e3d6de26d")
-setManifestid(1863671,"8302486163873169172")
-addappid(1863672,0,"5a3d6efc6d8f5e6692f2f7f3ceb6658c43853455ffbf7ed757838b49d7ed9361")
-setManifestid(1863672,"5088588288551430055")
-addappid(2170070,0,"074c968debe442b6e36b663082160a3a772eac1dbb79a66dab92fce4fd916533")
-setManifestid(2170070,"3485108790842822217")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1863670.lua
+--Gamename Mystwood Manor
+addappid(1863670, 1, "d625ee13e5ffd5a72f37a7d4edbf6cb8170d2e2123711fd14d8d68bbb2a43e15") --Mainappid Mystwood Manor
+addappid(1863671, 1, "48f0861f50d491d54e04a73a1c18681e5ac8ab1f8d2f7e5ee3e8be1e3d6de26d") --Main Windows Depot Mystwood Manor
+setManifestid(1863671, "692297371650304270", 1720009200)
+addappid(1863672, 1, "5a3d6efc6d8f5e6692f2f7f3ceb6658c43853455ffbf7ed757838b49d7ed9361") --Main Macos Depot Mystwood Manor
+setManifestid(1863672, "5088588288551430055", 1719211184)
+--Dlcname Mystwood Manor - High-res character wallpapers
+addappid(2170070, 1, "074c968debe442b6e36b663082160a3a772eac1dbb79a66dab92fce4fd916533") --Dlcname Mystwood Manor - High-res character wallpapers
+setManifestid(2170070, "3485108790842822217", 542996256)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
