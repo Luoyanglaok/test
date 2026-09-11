9b2f509c937db17c8cee9a3b95971487dd34735d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:02:35 +0800

    add 3172280.lua

--- 3172280.lua
+++ 3172280.lua
@@ -1,3 +1,6 @@
-addappid(3172280)
-addappid(3172281,0,"1646a77a092a374a2be5cbe417ef498dbd02bd3ea4567fb3d6907e14b46bb04e")
-setManifestid(3172281,"4170851070200970074")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3172280.lua
+--Gamename SEX Amnesia - Lover Sim 🔞
+addappid(3172280) --Mainappid SEX Amnesia - Lover Sim 🔞
+addappid(3172281, 1, "1646a77a092a374a2be5cbe417ef498dbd02bd3ea4567fb3d6907e14b46bb04e") --Main Windows Depot SEX Amnesia - Lover Sim 🔞
+setManifestid(3172281, "4170851070200970074", 3175590800)
