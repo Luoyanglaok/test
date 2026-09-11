45b20a8463861ff6cfb153c4b8a9d838b4b2df17
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:17:45 +0800

    add 322330.lua

--- 322330.lua
+++ 322330.lua
@@ -1,67 +1,58 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 322330.lua
---Gamename Don't Starve Together
-addappid(322330, 1, "c947e0b06f2977593e083d4a0b02bac9de29ab99d49478d07dabef37265f2bf8") --Mainappid Don't Starve Together
-addtoken(322330, "4367778632350439069")
-addappid(322331, 1, "3ee94d4c47554fc580de37cdeb4bb41020ece2d5ab195d51d8b87f5e438e9f23") --Main Windows Depot Don't Starve Together
-setManifestid(322331, "3356367044060314490", 3088200480)
-addappid(322332, 1, "8f36286253d1b2e61f2e7bfec57510d6ec34da84a9d126dc4adf19f6cff117be") --Main Linux Depot Don't Starve Together
-setManifestid(322332, "2756614310977922724", 2974564432)
-addappid(322333, 1, "065e9fc3141e45c895786254045560d2af8860c513d0a6077fceaca5fda4c32f") --Main Macos Depot Don't Starve Together
-setManifestid(322333, "6957262084090609678", 2968589152)
---Share Depots
-addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
-setManifestid(228982, "6413394087650432851", 9650016)
-addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
-setManifestid(228988, "6645201662696499616", 22411856)
-addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
-setManifestid(228990, "1829726630299308803", 100658080)
-addappid(798740) --Dlcname Don't Starve Together: Beating Heart Chest
-addappid(798750) --Dlcname Don't Starve Together: Original Verdant Chest
-addappid(840750) --Dlcname Don't Starve Together: Victorian Belongings Chest
-addappid(840760) --Dlcname Don't Starve Together: Original Survivors Victorian Chest
-addappid(840770) --Dlcname Don't Starve Together: Hallowed Nights Survivors Chest
-addappid(962960) --Dlcname Don't Starve Together: Hallowed Nights Belongings Chest
-addappid(974740) --Dlcname Don't Starve Together: All Survivors Magmatic Chest
-addappid(981700) --Dlcname Don't Starve Together: All Survivors Gladiator Chest
-addappid(981701) --Dlcname Don't Starve Together: Forge Weapons Chest
-addappid(981702) --Dlcname Don't Starve Together: Forge Armor Chest
-addappid(987340) --Dlcname Don't Starve Together: Snowfallen Survivors Chest
-addappid(1051700) --Dlcname Don't Starve Together: Wortox Deluxe Chest
-addappid(1084430) --Dlcname Don't Starve Together: Wormwood Deluxe Chest
-addappid(1167330) --Dlcname Don't Starve Together: Wurt Deluxe Chest
-addappid(1167510) --Dlcname Don't Starve Together: Hallowed Nights Survivors Chest, Part II
-addappid(1172860) --Dlcname Don't Starve Together: Starter Pack 2019
-addappid(1205500) --Dlcname Don't Starve Together: Merrymaker Survivors Chest
-addappid(1205501) --Dlcname Don't Starve Together: Merrymaker Belongings Chest
-addappid(1230830) --Dlcname Don't Starve Together: Bounteous Bundle
-addappid(1272440) --Dlcname Don't Starve Together: Starter Pack 2020
-addappid(1289760) --Dlcname Don't Starve Together: Spring Cleaning Bundle
-addappid(1338540) --Dlcname Don't Starve Together: Walter Deluxe Wardrobe
-addappid(1447360) --Dlcname Don't Starve Together: Hallowed Nights Survivors Chest, Part III
-addappid(1485060) --Dlcname Don't Starve Together: Snowfallen Survivors Chest, Part II
-addappid(1522880) --Dlcname Don't Starve Together: Victorian Antiques Chest
-addappid(1558300) --Dlcname Don't Starve Together: Latecomers' Victorian Chest
-addappid(1612410) --Dlcname Don't Starve Together: Cottage Cache Chest
-addappid(1694780) --Dlcname Don't Starve Together: Seaside Chest
-addappid(1698560) --Dlcname Don't Starve Together: Fresh Verdant Chest
-addappid(1710240) --Dlcname Don't Starve Together: Starter Pack 2021
-addappid(1710241) --Dlcname Don't Starve Together: Wanda Deluxe Chest
-addappid(1836630) --Dlcname Don't Starve Together: Merrymaker Survivors Chest, Part II
-addappid(1874370) --Dlcname Don't Starve Together: Fantasmical Chest
-addappid(1899050) --Dlcname Don't Starve Together: Gothic Belongings Chest
-addappid(2007370) --Dlcname Don't Starve Together: Midsummer Cawnival Chest
-addappid(2058280) --Dlcname Don't Starve Together: Inventor's Excursion Chest
-addappid(2058281) --Dlcname Don't Starve Together: Swashbuckler Survivors Chest
-addappid(2132800) --Dlcname Don't Starve Together: Moonbound Survivors Chest
-addappid(2170670) --Dlcname Don't Starve Together: Fantasmical Chest, Part II
-addappid(2170671) --Dlcname Don't Starve Together: Gothic Belongings Chest, Part II
-addappid(2236180) --Dlcname Don't Starve Together: Snowfallen Survivors Chest, Part III
-addappid(2241030) --Dlcname Don't Starve Together: Starter Pack 2023
-addappid(2377640) --Dlcname Don't Starve Together: Blooming Verdant Chest
-addappid(2711510) --Dlcname Don't Starve Together: Starter Pack 2024
-addappid(2769680) --Dlcname Don't Starve Together: Merrymaker Survivors Chest, Part III
-addappid(3155070) --Dlcname Don't Starve Together: Complete Roseate Chest
-addappid(3373360) --Dlcname Don't Starve Together: Merrymaker Survivors Chest, Part IV
-addappid(3373370) --Dlcname Don't Starve Together: Starter Pack 2025
-addappid(4211520) --Dlcname Don't Starve Together: Starter Pack 2026
+addappid(322330)
+addappid(4211520)
+addappid(1084430)
+addappid(1167330)
+addappid(1051700)
+addappid(1710241)
+addappid(840750)
+addappid(1522880)
+addappid(840760)
+addappid(1558300)
+addappid(3373370)
+addappid(2711510)
+addappid(2241030)
+addappid(1710240)
+addappid(1272440)
+addappid(1172860)
+addappid(1447360)
+addappid(1167510)
+addappid(840770)
+addappid(962960)
+addappid(1230830)
+addappid(2236180)
+addappid(1485060)
+addappid(987340)
+addappid(3373360)
+addappid(2769680)
+addappid(1205500)
+addappid(1836630)
+addappid(1205501)
+addappid(2170670)
+addappid(1874370)
+addappid(1289760)
+addappid(2170671)
+addappid(1899050)
+addappid(981701)
+addappid(981702)
+addappid(798740)
+addappid(974740)
+addappid(981700)
+addappid(2132800)
+addappid(2007370)
+addappid(2377640)
+addappid(798750)
+addappid(1698560)
+addappid(1612410)
+addappid(1694780)
+addappid(2058280)
+addappid(2058281)
+addappid(3155070)
+addappid(1338540)
+addappid(322331,1,"3ee94d4c47554fc580de37cdeb4bb41020ece2d5ab195d51d8b87f5e438e9f23")
+addappid(228982,1,"fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5")
+addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
+addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+setManifestid(322331,"3356367044060314490")
+setManifestid(228982,"6413394087650432851")
+setManifestid(228988,"6645201662696499616")
+setManifestid(228990,"1829726630299308803")
