5a9b4b4b472b6f2bca674506a5e892890c9e13e1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:11:59 +0800

    add 1007040.lua

--- 1007040.lua
+++ 1007040.lua
@@ -1,7 +1,31 @@
-addappid(1007040)
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(1007041,0,"9e97f916bd62116e247cbac00bdde6ee17ef433c1f275e1bf91ced2f4bec5106")
-setManifestid(1007041,"1745282649550539254")
-addappid(1007042,0,"59820074274e4bba44d87dab26511cbfe7dec04cb3cf08690641dc57ca9f75cf")
-setManifestid(1007042,"8440089052034728143")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1007040.lua
+--Gamename EARTH DEFENSE FORCE 5
+addappid(1007040) --Mainappid EARTH DEFENSE FORCE 5
+addappid(1007041, 1, "9e97f916bd62116e247cbac00bdde6ee17ef433c1f275e1bf91ced2f4bec5106") --Main Windows Depot EARTH DEFENSE FORCE 5
+setManifestid(1007041, "1745282649550539254", 14346302080)
+addappid(1007042, 1, "59820074274e4bba44d87dab26511cbfe7dec04cb3cf08690641dc57ca9f75cf") --Main Windows Depot EARTH DEFENSE FORCE 5
+setManifestid(1007042, "8440089052034728143", 6545168)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(1057270) --Dlcname EARTH DEFENSE FORCE 5 - Mission Pack 1: Extra Challenge
+addappid(1057271) --Dlcname EARTH DEFENSE FORCE 5 - Mission Pack 2: Super Challenge
+addappid(1057272) --Dlcname EARTH DEFENSE FORCE 5 - Ranger Piloted Weapon: Omega Freesia
+addappid(1057273) --Dlcname EARTH DEFENSE FORCE 5 - Ranger Vehicle: Light Truck
+addappid(1057274) --Dlcname EARTH DEFENSE FORCE 5 - Ranger Support Device: Detector S-Type
+addappid(1057275) --Dlcname EARTH DEFENSE FORCE 5 - Ranger Piloted Weapon: Blacker Number 4.1
+addappid(1057276) --Dlcname EARTH DEFENSE FORCE 5 - Wing Diver Support Device: Reverse Core S-Type
+addappid(1057277) --Dlcname EARTH DEFENSE FORCE 5 - Fencer Weapon: Grim Reaper Shield
+addappid(1057278) --Dlcname EARTH DEFENSE FORCE 5 - Fencer Weapon: Happy Body Pillow
+addappid(1057279) --Dlcname EARTH DEFENSE FORCE 5 - Fencer Support Device: Wild Skeleton
+addappid(1057280) --Dlcname EARTH DEFENSE FORCE 5 - Air Raider Weapon: FZ-GUN
+addappid(1057281) --Dlcname EARTH DEFENSE FORCE 5 - Air Raider Piloted Weapon: Combat Frame Phantasia Shield Nyx
+addappid(1057282) --Dlcname EARTH DEFENSE FORCE 5 - Air Raider Piloted Weapon: Blacker Number 5
+addappid(1057283) --Dlcname EARTH DEFENSE FORCE 5 - Air Raider Vehicle: Caliban Armored Ambulance Joyful Marking
+addappid(1076010) --Dlcname EARTH DEFENSE FORCE 5 - Ranger Weapon: Decoy Launcher (Pale Wing)
+addappid(1076011) --Dlcname EARTH DEFENSE FORCE 5 - Air Raider Piloted Weapon: Combat Frame Gold Nyx
+addappid(1076012) --Dlcname EARTH DEFENSE FORCE 5 - Wing Diver Weapon: Vulcaneer Cracker
+addappid(1076013) --Dlcname EARTH DEFENSE FORCE 5 - Wing Diver Weapon: Starburst
+addappid(1076014) --Dlcname EARTH DEFENSE FORCE 5 - Ranger Weapon: Singing and Dancing Pure Decoy Launcher Set of 8
+addappid(1076015) --Dlcname EARTH DEFENSE FORCE 5 - Air Raider Weapon: Singing and Dancing Pure Decoy Launcher Set of 8
