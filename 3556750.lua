365ff492705e9b0c722ad29efccc29d4d4d1d946
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:38:32 +0800

    add 3556750.lua

--- 3556750.lua
+++ 3556750.lua
@@ -1,6 +1,11 @@
-addappid(3556750)
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(3556751,"4763316343917524540")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3556750.lua
+--Gamename Warhammer 40,000: Dawn of War - Definitive Edition
+addappid(3556750, 1, "e0de1899c12dfaefe9eddb247993141c1c6b06e63ce4acdb0fde3a7a95fe95fa") --Mainappid Warhammer 40,000: Dawn of War - Definitive Edition
+addappid(3556751, 1, "35bac2dd94bb8e01c8f92ca142f307b26f07bbd36f213c409d5cafdbea7db478") --Main Windows Depot Warhammer 40,000: Dawn of War - Definitive Edition
+setManifestid(3556751, "4763316343917524540", 20266062768)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
