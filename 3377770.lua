cd53d4c52503cb2779b01b41df1cd1aeafafadbb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:54:44 +0800

    add 3377770.lua

--- 3377770.lua
+++ 3377770.lua
@@ -1,3 +1,6 @@
-addappid(3377770)
-addappid(3377771,0,"66d9fcae2691745f33f9d4c65e97e53e6b10a4923be5ec52b6930ae92f65f758")
-setManifestid(3377771,"3164541798822091607")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3377770.lua
+--Gamename Haunting Havens
+addappid(3377770, 1, "f147c6cf194e4066acdb5dc680d59a1927365ec0ca58a55c86ebfe99240b8ab8") --Mainappid Haunting Havens
+addappid(3377771, 1, "66d9fcae2691745f33f9d4c65e97e53e6b10a4923be5ec52b6930ae92f65f758") --Main Windows Depot Haunting Havens
+setManifestid(3377771, "3022941472106940854", 9987800800)
