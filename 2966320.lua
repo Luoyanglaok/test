0c18ead3e9db01c74494376525131e86178a20fa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:50:26 +0800

    add 2966320.lua

---
+++ 2966320.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2966320.lua
+--Gamename Starsand Island
+addappid(2966320, 1, "208ea6f9923a37a7b011f392e50f7f7ab2952497337e184dbd00448c63bd1a50") --Mainappid Starsand Island
+addtoken(2966320, "1152859757700171993")
+addappid(2966321, 1, "ce86c628c5c525587d2aea36d5a745c980584e22eca6389d8bfd7a63c0db1c4c") --Main Windows Depot Starsand Island
+setManifestid(2966321, "7271785676686400459", 7880356400)
+addappid(4391920) --Dlcname 星砂岛 春节国风家具和服装DLC
+addtoken(4391920, "4278437199386008459")
+addappid(4395490) --Dlcname 星砂岛 欧式梦幻家具、服装和载具DLC
+addtoken(4395490, "10139208446148904551")
+addappid(4424260) --Dlcname 星砂岛 呱呱假日DLC
+addtoken(4424260, "18153694763478506076")
+addappid(4460980) --Dlcname 星砂岛 林间小筑系列家具DLC
+addappid(4621220) --Dlcname 星砂岛 闪耀之星DLC
+addtoken(4621220, "985130833684389911")
+--Missing Dlcs
+--Dlcname Starsandisland Modkit
