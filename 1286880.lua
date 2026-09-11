6cfc231137c929eb1d4edf351e6a6e98263b1243
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:41:43 +0800

    add 1286880.lua

--- 1286880.lua
+++ 1286880.lua
@@ -1,5 +1,12 @@
-addappid(1286880)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1286881,0,"73fbc4ec2441db925364ee7921204361cfb62431d36dcc0bfa9bbb50267aea2d")
-setManifestid(1286881,"2593066669097147307")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1286880.lua
+--Gamename Ship Graveyard Simulator
+addappid(1286880) --Mainappid Ship Graveyard Simulator
+addappid(1286881, 1, "73fbc4ec2441db925364ee7921204361cfb62431d36dcc0bfa9bbb50267aea2d") --Main Windows Depot Ship Graveyard Simulator
+setManifestid(1286881, "2593066669097147307", 7581801888)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1918190) --Dlcname Ship Graveyard Simulator - Submarines DLC
