d15002bed63be2a5aa8f3af9f88bddcf96102193
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:24:23 +0800

    add 3844970.lua

---
+++ 3844970.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3844970.lua
+--Gamename Burglin' Gnomes
+addappid(3844970, 1, "caa3d9c9e3d34e7188d88ab4e24228fc47178896a739dc2a5a0ff484fa9c5a7b") --Mainappid Burglin' Gnomes
+addappid(3844971, 1, "6d3197ad9feb781dfe4dfd67f09fb9e834c9bbb158108a27c31e31d3001ec326") --Main Windows Depot Burglin' Gnomes
+setManifestid(3844971, "2233065720977923709", 3552860768)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
