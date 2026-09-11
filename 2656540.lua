5dbb20b25b0f0c08331b03073d2b24e7f8352a79
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:33:20 +0800

    add 2656540.lua

--- 2656540.lua
+++ 2656540.lua
@@ -1,3 +1,14 @@
-addappid(2656540)
-addappid(2656541,0,"226ea8fc6fbf56203b2e433de14ecb9db7e75f1bfc44cbd12fdbcb7dd4d37e62")
-setManifestid(2656541,"2258454976848242687")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2656540.lua
+--Gamename 东方：平野孤鸿
+addappid(2656540, 1, "554afe3f5a22813bbcd478706953f93fcc34f4af19e59988d63c75373a11f687") --Mainappid 东方：平野孤鸿
+addappid(2656541, 1, "226ea8fc6fbf56203b2e433de14ecb9db7e75f1bfc44cbd12fdbcb7dd4d37e62") --Main Windows Depot 东方：平野孤鸿
+setManifestid(2656541, "2258454976848242687", 34021860256)
+--Dlcname 东方：平野孤鸿 数字豪华版升级包
+addappid(3298900, 1, "047fe37656b447580e81aee0dddb145f926b096789eda141652c89359908a786") --Dlcname 东方：平野孤鸿 数字豪华版升级包
+setManifestid(3298900, "243632004459158116", 1919345024)
+addappid(3332130) --Dlcname 东方：平野孤鸿 建筑外观扩展包（一）
+addappid(3333150) --Dlcname 东方：平野孤鸿 建筑外观扩展包（三）
+addappid(3410990) --Dlcname 东方：平野孤鸿 薪火长燃计划-山西飞云楼
+--Missing Dlcs
+--Dlcname 东方：平野孤鸿 建筑外观扩展包（二）
