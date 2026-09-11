4a6977bf0b9be9a4e560d36047c7732e9cb70e17
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:51:17 +0800

    add 2487150.lua

---
+++ 2487150.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2487150.lua
+--Gamename Deconstruction Simulator
+addappid(2487150, 1, "74bd2cac3087267e9905b6e3fcc0db2d351adda777799fb7484ff3400aef82ec") --Mainappid Deconstruction Simulator
+addappid(2487151, 1, "734BFF9AED757224CE32FDD52D7EF669C41D5E8D0C6F972006E554F1B8E0BBC1") --Main Windows Depot Deconstruction Simulator
+setManifestid(2487151, "6412176256665855563", 10242467072)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
