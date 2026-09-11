9a5c49eb5b57c41591e4d13ca1b1d1ebd9b4f552
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:21:59 +0800

    add 457140.lua

--- 457140.lua
+++ 457140.lua
@@ -1,28 +1,71 @@
-addappid(457140)
-setManifestid(228986,"8782296191957114623")
-addappid(457141,0,"1f77f3d9ef1b69fc77d28031a5ed6d04cc279101faa6ffe5f1e0915b4baab79c")
-setManifestid(457141,"276963429592180720")
-addappid(457142,0,"16847f270c721a9adf7256bc88d288af678193f74c533f2cd44bccff1ceccdf4")
-setManifestid(457142,"2340255509647081195")
-addappid(457143,0,"3199b2ed10129547be0ba6efa9fbb4aa5c740f09fe158c1cf9b8879fc7aba8e4")
-setManifestid(457143,"4507298824457801678")
-addappid(1452491,0,"65272efbc87a5b051b30af08129c729e7069bf5fb8152d68d21fa0588bdbaa42")
-setManifestid(1452491,"4506704908037636701")
-addappid(1452492,0,"0b9f63b77607bdc6f37c6bdb210dae04b2b0c754c15f23d35f821faedde16c18")
-setManifestid(1452492,"7453797904245530621")
-addappid(1452493,0,"c9535e00cac8b8233875f729e30bd590afc2e07204f78d4ba08391d690279210")
-setManifestid(1452493,"8558015493122010007")
-addappid(2952301,0,"0aac13130570a4cb9a749ceff82c255feee2a1bf65b45505327eed962cb26157")
-setManifestid(2952301,"6808368032254829666")
-addappid(2952302,0,"52efd7282b10c02fdbed47f79ab3fee394692e140b4a4f6772bed739e17ce4a1")
-setManifestid(2952302,"8169744682045356820")
-addappid(2952303,0,"734a56752faf870ec6a0658210b60aac3f13de253df76e86374e6682c65026af")
-setManifestid(2952303,"1918864595757588815")
-addappid(3302471,0,"f8bc05dcd9e0ee42b25f6aa97c7f85fc741c4d6ad98d739bdf60e771b00a3b8c")
-setManifestid(3302471,"2368012642942227266")
-addappid(3302472,0,"65bd88c7ebed08e9724c1a238b1f2ac37402acad5347a83dcd5ba1cc0305d452")
-setManifestid(3302472,"4781468463339437744")
-addappid(3302473,0,"48e8fc6672f49bfa71934a13a2f53e4846315340e22d7be691de099e7db4d87d")
-setManifestid(3302473,"3481451596590066604")
-addappid(3655421,0,"1e19f1c5c9d64d9892acde57b6b210141f37d8b0c605605eed1e8728be887dae")
-setManifestid(3655421,"5778969697634757174")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 457140.lua
+--Gamename Oxygen Not Included
+addappid(457140, 1, "bd3469b4af420f761c8bc3b61e72d81466e5d75b5236e63471909ffc70656d69") --Mainappid Oxygen Not Included
+addappid(457141, 1, "1f77f3d9ef1b69fc77d28031a5ed6d04cc279101faa6ffe5f1e0915b4baab79c") --Main Windows Depot Oxygen Not Included
+setManifestid(457141, "4837644823067158760", 1039157824)
+addappid(457142, 1, "16847f270c721a9adf7256bc88d288af678193f74c533f2cd44bccff1ceccdf4") --Main Macos Depot Oxygen Not Included
+setManifestid(457142, "3419067905610246730", 1030262624)
+addappid(457143, 1, "3199b2ed10129547be0ba6efa9fbb4aa5c740f09fe158c1cf9b8879fc7aba8e4") --Main Linux Depot Oxygen Not Included
+setManifestid(457143, "3743790893999086910", 1033721248)
+--Dlcname Oxygen Not Included Soundtrack
+addappid(1244160) --Dlcname Oxygen Not Included Soundtrack
+addappid(1244163, 1, "8b521846a9d7d24fbb6a5d9a24a3540a42fa2120c58f3e7c253bacb140d9539a") --Dlc Windows Depot Oxygen Not Included Soundtrack
+setManifestid(1244163, "8363155395009446104", 1669365888)
+addappid(1244164, 1, "f1911e2f9590008059c71addaa1787bf804a7bff30a744186df249db161f3892") --Dlc Windows Depot Oxygen Not Included Soundtrack
+setManifestid(1244164, "6900117664300329874", 164417008)
+addappid(1244165, 1, "445d97e9622fb68a1cc79e7ef62a43194b06df0546b1b2025a1c69c7d634ebb6") --Dlc Windows Depot Oxygen Not Included Soundtrack
+setManifestid(1244165, "690398342775806013", 1282942656)
+addappid(1244166, 1, "ec0a00b692e4f21ca4ef688cd616f60a8d68580e0e479c1fd1de4dba67e9a0b6") --Dlc Windows Depot Oxygen Not Included Soundtrack
+setManifestid(1244166, "4261369924288007851", 126718096)
+--Dlcname Oxygen Not Included - Spaced Out!
+addappid(1452490) --Dlcname Oxygen Not Included - Spaced Out!
+addappid(1452491, 1, "65272efbc87a5b051b30af08129c729e7069bf5fb8152d68d21fa0588bdbaa42") --Dlc Windows Depot Oxygen Not Included - Spaced Out!
+setManifestid(1452491, "8396750184523323038", 286868880)
+addappid(1452492, 1, "0b9f63b77607bdc6f37c6bdb210dae04b2b0c754c15f23d35f821faedde16c18") --Dlc Macos Depot Oxygen Not Included - Spaced Out!
+setManifestid(1452492, "5063491960860573058", 286863632)
+addappid(1452493, 1, "c9535e00cac8b8233875f729e30bd590afc2e07204f78d4ba08391d690279210") --Dlc Linux Depot Oxygen Not Included - Spaced Out!
+setManifestid(1452493, "7820242504201654785", 286864128)
+--Dlcname Oxygen Not Included: The Frosty Planet Pack
+addappid(2952300) --Dlcname Oxygen Not Included: The Frosty Planet Pack
+addappid(2952301, 1, "0aac13130570a4cb9a749ceff82c255feee2a1bf65b45505327eed962cb26157") --Dlc Windows Depot Oxygen Not Included: The Frosty Planet Pack
+setManifestid(2952301, "1335370103027876731", 72859264)
+addappid(2952302, 1, "52efd7282b10c02fdbed47f79ab3fee394692e140b4a4f6772bed739e17ce4a1") --Dlc Macos Depot Oxygen Not Included: The Frosty Planet Pack
+setManifestid(2952302, "1630477759227119941", 72861760)
+addappid(2952303, 1, "734a56752faf870ec6a0658210b60aac3f13de253df76e86374e6682c65026af") --Dlc Linux Depot Oxygen Not Included: The Frosty Planet Pack
+setManifestid(2952303, "4453749253650801169", 72861760)
+--Dlcname Oxygen Not Included: The Bionic Booster Pack
+addappid(3302470) --Dlcname Oxygen Not Included: The Bionic Booster Pack
+addappid(3302471, 1, "f8bc05dcd9e0ee42b25f6aa97c7f85fc741c4d6ad98d739bdf60e771b00a3b8c") --Dlc Windows Depot Oxygen Not Included: The Bionic Booster Pack
+setManifestid(3302471, "1822568966096086841", 26976080)
+addappid(3302472, 1, "65bd88c7ebed08e9724c1a238b1f2ac37402acad5347a83dcd5ba1cc0305d452") --Dlc Macos Depot Oxygen Not Included: The Bionic Booster Pack
+setManifestid(3302472, "6215920908828744077", 26978512)
+addappid(3302473, 1, "48e8fc6672f49bfa71934a13a2f53e4846315340e22d7be691de099e7db4d87d") --Dlc Linux Depot Oxygen Not Included: The Bionic Booster Pack
+setManifestid(3302473, "7813832438596765983", 26979808)
+--Dlcname Oxygen Not Included: The Prehistoric Planet Pack
+addappid(3655420) --Dlcname Oxygen Not Included: The Prehistoric Planet Pack
+addappid(3655421, 1, "1e19f1c5c9d64d9892acde57b6b210141f37d8b0c605605eed1e8728be887dae") --Dlc Windows Depot Oxygen Not Included: The Prehistoric Planet Pack
+setManifestid(3655421, "4848920761121622416", 32982096)
+addappid(3655422, 1, "71fcc2ab0243b59a4258e8417c3018e4e57caf88df6027f367a215d5f756875c") --Dlc Macos Depot Oxygen Not Included: The Prehistoric Planet Pack
+setManifestid(3655422, "4433085393215100788", 32980496)
+addappid(3655423, 1, "beb86fc4b7407f862c2263bbdd011cd6e7e49728d5adb2353cbbc02ef9185ff6") --Dlc Linux Depot Oxygen Not Included: The Prehistoric Planet Pack
+setManifestid(3655423, "5716210815088746446", 32982096)
+--Dlcname Oxygen Not Included: Neutronium Cosmetics Pack
+addappid(4157740) --Dlcname Oxygen Not Included: Neutronium Cosmetics Pack
+addappid(4157741, 1, "04c156d9e73616823c80c5634127217788cbb95922569e51a3e873c38636e466") --Dlc Windows Depot Oxygen Not Included: Neutronium Cosmetics Pack
+setManifestid(4157741, "7269192979546626522", 4442096)
+addappid(4157742, 1, "8fa03d1af4ac72fc16c0927fb26ac8a0d0eba62ced20d18087bbd54838f46bbd") --Dlc Macos Depot Oxygen Not Included: Neutronium Cosmetics Pack
+setManifestid(4157742, "2972396247022102742", 4442048)
+addappid(4157743, 1, "5e9a9d5edb45852df67aefd8454d049c2c6413343a33793a7e2dffd09a64673a") --Dlc Linux Depot Oxygen Not Included: Neutronium Cosmetics Pack
+setManifestid(4157743, "2888959302683286004", 4442128)
+--Dlcname Oxygen Not Included: The Aquatic Planet Pack
+addappid(4310080) --Dlcname Oxygen Not Included: The Aquatic Planet Pack
+addappid(4310081, 1, "69ad525535865eb4f4d299cf857ce34c5a03d9a7d3c7c0650188727434d30f27") --Dlc Windows Depot Oxygen Not Included: The Aquatic Planet Pack
+setManifestid(4310081, "7843994958119989828", 203321568)
+addappid(4310082, 1, "b93eda89a01fc823363bc727480225676a34d3f5d254a56dc605cc583483c4b9") --Dlc Macos Depot Oxygen Not Included: The Aquatic Planet Pack
+setManifestid(4310082, "4229541529705433357", 203321568)
+addappid(4310083, 1, "6890afaf54c40e6d8a14a021174846341a0d47ddca554775f5fc2a96830f98af") --Dlc Linux Depot Oxygen Not Included: The Aquatic Planet Pack
+setManifestid(4310083, "4950998344286956405", 203321568)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
