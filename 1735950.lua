1ef57713d4e400d27218767ebfc7c9051c7a145c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:49:23 +0800

    add 1735950.lua

--- 1735950.lua
+++ 1735950.lua
@@ -1,7 +1,10 @@
-addappid(1735950)
-addappid(1735951,0,"57baa5a39952bf5b01598310566b4ace8a5f97fec03e21b7c4ba580cc4c69af7")
-setManifestid(1735951,"7760395770867840515")
-addappid(1735952,0,"81543e692154ae8398bd8c1064b1868f7ae4f0bd4b9294e54a51de3eb468ace4")
-setManifestid(1735952,"5601909260979355887")
-addappid(1735953,0,"c648de0001ca2da0bfb3c6100e50cb23f495a64f6d310c096ad9cf7eec0449e4")
-setManifestid(1735953,"9208830750023627647")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1735950.lua
+--Gamename 纸嫁衣
+addappid(1735950) --Mainappid 纸嫁衣
+addappid(1735951, 1, "57baa5a39952bf5b01598310566b4ace8a5f97fec03e21b7c4ba580cc4c69af7") --Main Windows Depot 纸嫁衣
+setManifestid(1735951, "8788107440043237701", 75667008)
+addappid(1735952, 1, "81543e692154ae8398bd8c1064b1868f7ae4f0bd4b9294e54a51de3eb468ace4") --Main Windows Depot 纸嫁衣
+setManifestid(1735952, "9061856357014411031", 77673488)
+addappid(1735953, 1, "c648de0001ca2da0bfb3c6100e50cb23f495a64f6d310c096ad9cf7eec0449e4") --Main Windows Depot 纸嫁衣
+setManifestid(1735953, "4847940518412599673", 81657424)
