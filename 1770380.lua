1cdd87b8af01c89b9aca6b933660db7131ee93ee
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:15:32 +0800

    add 1770380.lua

--- 1770380.lua
+++ 1770380.lua
@@ -1,3 +1,6 @@
-addappid(1770380)
-addappid(1770381,0,"a7a6822c30d46344b3186a5d279d49eb532df37d7937abe927ec12a031974255")
-setManifestid(1770381,"1882861819283052166")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1770380.lua
+--Gamename SEX Prison [18+]
+addappid(1770380) --Mainappid SEX Prison [18+]
+addappid(1770381, 1, "a7a6822c30d46344b3186a5d279d49eb532df37d7937abe927ec12a031974255") --Main Windows Depot SEX Prison [18+]
+setManifestid(1770381, "1882861819283052166", 695383328)
