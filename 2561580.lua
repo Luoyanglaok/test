2d28e30963c31ea070df8a02c195ee5e43307818
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:04:12 +0800

    add 2561580.lua

--- 2561580.lua
+++ 2561580.lua
@@ -1,30 +1,36 @@
-addappid(2561580)
-setManifestid(228989,"3514306556860204959")
-addappid(2561581,0,"8e1a9148f817c83b0869213d79cba88820071c6c04b83d90cbacaf54d9f2a71b")
-setManifestid(2561581,"7465240662519965185")
-addappid(2561582,0,"d13854c1d6ac633f3d788d3dd4dc120a6f17a140afc307f83a58a852ed398fb8")
-setManifestid(2561582,"265552515056369382")
-addappid(2561584,0,"da0a904fca9229fa415915fa6afc05f317700c5680b10a2ef10ec3b4b71de705")
-setManifestid(2561584,"4149824663914320759")
-addappid(2561585,0,"45ae912f07bedbcc51c66e195ccc5b4ec76cc36dda26455da454791634af9855")
-setManifestid(2561585,"2495579293854622973")
-addappid(2561586,0,"82d9e8b2d5717969c66973c6c285b423e9924d14650541f6b8ffbd0e250ad17d")
-setManifestid(2561586,"3297516423133445931")
-addappid(2561587,0,"3bbbe805bf9f774ce2a375cd923de91e08961307cf57d828270df9ea2374b676")
-setManifestid(2561587,"3174005451803957562")
-addappid(2561588,0,"d2938490f5acf48f658919f3788ab0cf3580bc38a48fefd60a9f24198a468662")
-setManifestid(2561588,"4791143133681149741")
-addappid(2561589,0,"48ca3569f851b2d8821adf4e2ddb8cdcb1df5daa549b355d7687dcae1058c5c3")
-setManifestid(2561589,"2203801687096253528")
-addappid(2632101,0,"57ef037853c0ebb8b918a1d914631f6c7c686bb8d51417fe043a9b18762e6f63")
-setManifestid(2632101,"3653271140047671175")
-addappid(2632102,0,"16b4861960cee59e05f720421c63be2aa944dea4e24a6d2085a6aec16a9f5206")
-setManifestid(2632102,"4896625360655140230")
-addappid(2632103,0,"997ddb200403a4eb12bd22506e78426ca33aa5dfc3a0590966f4934999e8ee89")
-setManifestid(2632103,"1609059493216400047")
-addappid(2632104,0,"4572ee6d9b6859f84446f08aa79f684e27e2da6b5a5bba240d5dad65c3398cf5")
-setManifestid(2632104,"1539313718481784453")
-addappid(2632105,0,"f87ad549329bbf6236d645abfa60f07c32371c6d7f755aad57070963364cf35a")
-setManifestid(2632105,"2190630327115764663")
-addappid(2632106,0,"9102c8df6f5322bdbc8eaba62a0ccd538551585e7d0c2659f2a0ac4690fe48e8")
-setManifestid(2632106,"2176013819849316133")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2561580.lua
+--Gamename Horizon Zero Dawn™ Remastered
+addappid(2561580, 1, "b5ebadc3aa9c0bda6645c2a0548f89afe8ad4825f845c84fa4acfb56314f9bbd") --Mainappid Horizon Zero Dawn™ Remastered
+addappid(2561581, 1, "8e1a9148f817c83b0869213d79cba88820071c6c04b83d90cbacaf54d9f2a71b") --Main Windows Depot Horizon Zero Dawn™ Remastered
+setManifestid(2561581, "7465240662519965185", 89931274480)
+addappid(2561582, 1, "d13854c1d6ac633f3d788d3dd4dc120a6f17a140afc307f83a58a852ed398fb8") --Main Windows Depot Horizon Zero Dawn™ Remastered
+setManifestid(2561582, "265552515056369382", 466229952)
+addappid(2561584, 1, "da0a904fca9229fa415915fa6afc05f317700c5680b10a2ef10ec3b4b71de705") --Main Windows Depot Horizon Zero Dawn™ Remastered
+setManifestid(2561584, "4149824663914320759", 1594998448)
+addappid(2561585, 1, "45ae912f07bedbcc51c66e195ccc5b4ec76cc36dda26455da454791634af9855") --Main Windows Language Depot French Horizon Zero Dawn™ Remastered
+setManifestid(2561585, "2495579293854622973", 1473353088)
+addappid(2561586, 1, "82d9e8b2d5717969c66973c6c285b423e9924d14650541f6b8ffbd0e250ad17d") --Main Windows Language Depot Spanish Horizon Zero Dawn™ Remastered
+setManifestid(2561586, "3297516423133445931", 1482900320)
+addappid(2561587, 1, "3bbbe805bf9f774ce2a375cd923de91e08961307cf57d828270df9ea2374b676") --Main Windows Language Depot German Horizon Zero Dawn™ Remastered
+setManifestid(2561587, "3174005451803957562", 1668184032)
+addappid(2561588, 1, "d2938490f5acf48f658919f3788ab0cf3580bc38a48fefd60a9f24198a468662") --Main Windows Language Depot Italian Horizon Zero Dawn™ Remastered
+setManifestid(2561588, "4791143133681149741", 1588146272)
+addappid(2561589, 1, "48ca3569f851b2d8821adf4e2ddb8cdcb1df5daa549b355d7687dcae1058c5c3") --Main Windows Language Depot Portuguese Horizon Zero Dawn™ Remastered
+setManifestid(2561589, "2203801687096253528", 1579859504)
+addappid(2632101, 1, "57ef037853c0ebb8b918a1d914631f6c7c686bb8d51417fe043a9b18762e6f63") --Main Windows Language Depot Russian Horizon Zero Dawn™ Remastered
+setManifestid(2632101, "3653271140047671175", 1673745408)
+addappid(2632102, 1, "16b4861960cee59e05f720421c63be2aa944dea4e24a6d2085a6aec16a9f5206") --Main Windows Language Depot Polish Horizon Zero Dawn™ Remastered
+setManifestid(2632102, "4896625360655140230", 1415501344)
+addappid(2632103, 1, "997ddb200403a4eb12bd22506e78426ca33aa5dfc3a0590966f4934999e8ee89") --Main Windows Language Depot Japanese Horizon Zero Dawn™ Remastered
+setManifestid(2632103, "1609059493216400047", 1571176032)
+addappid(2632104, 1, "4572ee6d9b6859f84446f08aa79f684e27e2da6b5a5bba240d5dad65c3398cf5") --Main Windows Language Depot Latam Horizon Zero Dawn™ Remastered
+setManifestid(2632104, "1539313718481784453", 1444012752)
+addappid(2632105, 1, "f87ad549329bbf6236d645abfa60f07c32371c6d7f755aad57070963364cf35a") --Main Windows Language Depot Brazilian Horizon Zero Dawn™ Remastered
+setManifestid(2632105, "2190630327115764663", 1615760608)
+addappid(2632106, 1, "9102c8df6f5322bdbc8eaba62a0ccd538551585e7d0c2659f2a0ac4690fe48e8") --Main Windows Language Depot Arabic Horizon Zero Dawn™ Remastered
+setManifestid(2632106, "2176013819849316133", 1675261760)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3260530) --Dlcname Horizon Zero Dawn™ Remastered Digital Artbook
