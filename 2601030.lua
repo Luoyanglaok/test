99b0faadd40b690bf549c854db7a0d14125297e5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:50:54 +0800

    add 2601030.lua

--- 2601030.lua
+++ 2601030.lua
@@ -1,5 +1,11 @@
-addappid(2601030)
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-addappid(2601031,0,"63406cc408fa86ef14ef634a8b9a37974e44c030fb6e5e248768c38d8ac0e8d0")
-setManifestid(2601031,"8394692636571473229")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2601030.lua
+--Gamename Exanimum: The Silent Call
+addappid(2601030) --Mainappid Exanimum: The Silent Call
+addappid(2601031, 1, "63406cc408fa86ef14ef634a8b9a37974e44c030fb6e5e248768c38d8ac0e8d0") --Main Windows Depot Exanimum: The Silent Call
+setManifestid(2601031, "8394692636571473229", 10852130704)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
