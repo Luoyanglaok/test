7825d73930d1bfff99d5d7edd4b1a0e5694717e7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:58:46 +0800

    add 3017860.lua

--- 3017860.lua
+++ 3017860.lua
@@ -1,17 +1,30 @@
-addappid(3017860)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(3017862,0,"b537907a613fd29911d2e311eb41fd2f0b309324601b0ef164c8520562eac290")
-setManifestid(3017862,"3460224106981726721")
-addappid(3017863,0,"0c010bdf1623542793fb5765875c715494c4a6c78856f75c124aa7d31f3021cd")
-setManifestid(3017863,"6962243758063524023")
-addappid(3017864,0,"35f0d11b6d2b729dd8e5f50a21e7b0300e8d6b79fc6ba487f4198ca6d3402c0d")
-setManifestid(3017864,"4394267145978638751")
-addappid(3017865,0,"558521d6ef4cac7a1318bb55268dc878b72482482691e7791ea81bff06c73694")
-setManifestid(3017865,"9108580810918908470")
-addappid(3017866,0,"cc9112a06785fd0d7f6e24a50702f1f71e655e35caf8fb3ea09f6036680c0fe4")
-setManifestid(3017866,"9217731964535062300")
-addappid(3017867,0,"8ffa78f10846725bed8d48d1a182cf4088f770691c44561b2d167f84fdc60789")
-setManifestid(3017867,"3503398939794266883")
-addappid(3017868,0,"fb3bda82e733857d05419dce15179a1aa642b840af21ef69520346fe8a00e20e")
-setManifestid(3017868,"6380569489411096461")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3017860.lua
+--Gamename DOOM: The Dark Ages
+addappid(3017860, 1, "ce570e5aa3168b1e9c8d2004a3b93db4c11a44cf8b9a99158ae4c1baf9384dbb") --Mainappid DOOM: The Dark Ages
+addappid(3017862, 1, "b537907a613fd29911d2e311eb41fd2f0b309324601b0ef164c8520562eac290") --Main Windows Depot DOOM: The Dark Ages
+setManifestid(3017862, "7466449196753546769", 167062032)
+addappid(3017863, 1, "0c010bdf1623542793fb5765875c715494c4a6c78856f75c124aa7d31f3021cd") --Main Windows Depot DOOM: The Dark Ages
+setManifestid(3017863, "3860369781994465240", 352)
+addappid(3017864, 1, "35f0d11b6d2b729dd8e5f50a21e7b0300e8d6b79fc6ba487f4198ca6d3402c0d") --Main Windows Depot DOOM: The Dark Ages
+setManifestid(3017864, "7054043744589518599", 11825466528)
+addappid(3017865, 1, "558521d6ef4cac7a1318bb55268dc878b72482482691e7791ea81bff06c73694") --Main Windows Depot DOOM: The Dark Ages
+setManifestid(3017865, "6508691013012284956", 0)
+addappid(3017866, 1, "cc9112a06785fd0d7f6e24a50702f1f71e655e35caf8fb3ea09f6036680c0fe4") --Main Windows Depot DOOM: The Dark Ages
+setManifestid(3017866, "51450828051076476", 288)
+addappid(3017867, 1, "8ffa78f10846725bed8d48d1a182cf4088f770691c44561b2d167f84fdc60789") --Main Windows Depot DOOM: The Dark Ages
+setManifestid(3017867, "7998384974670192918", 90173392752)
+addappid(3017868, 1, "fb3bda82e733857d05419dce15179a1aa642b840af21ef69520346fe8a00e20e") --Main Windows Depot DOOM: The Dark Ages
+setManifestid(3017868, "8320539347038966589", 20571120)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3363930) --Dlcname DOOM: The Dark Ages - Preorder Content
+addtoken(3363930, "3317585234173034770")
+addappid(3363940) --Dlcname DOOM: The Dark Ages - Premium Content
+addtoken(3363940, "4257299237979279538")
+addappid(3469450) --Dlcname DOOM: The Dark Ages | Revelations
+addappid(3698350) --Dlcname DOOM: The Dark Ages - Premium Upgrade
+addtoken(3698350, "553470768921816577")
