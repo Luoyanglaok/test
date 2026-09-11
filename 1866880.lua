40210d9ba427e96ac83d6fca017741d95d5a9f8a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 17:45:09 +0800

    add 1866880.lua

--- 1866880.lua
+++ 1866880.lua
@@ -1,3 +1,6 @@
-addappid(1866880)
-addappid(1866881,0,"030d43aa16163f1360a017db00e68adaf3a1953a22de98b7ba99862192e33e04")
-setManifestid(1866881,"7614619103936372801")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1866880.lua
+--Gamename 修仙故事: 轮回 Cultivation Story: Reincarnation
+addappid(1866880) --Mainappid 修仙故事: 轮回 Cultivation Story: Reincarnation
+addappid(1866881, 1, "030d43aa16163f1360a017db00e68adaf3a1953a22de98b7ba99862192e33e04") --Main Windows Depot 修仙故事: 轮回 Cultivation Story: Reincarnation
+setManifestid(1866881, "8268326801819863086", 87012976)
