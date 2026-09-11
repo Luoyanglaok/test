e71aa8a4a22b92038139055fa1de42839b5430b7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:15:20 +0800

    add 761600.lua

--- 761600.lua
+++ 761600.lua
@@ -1,5 +1,13 @@
-addappid(761600)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(761601,0,"a9cebe81198dc3bda033a39cb706ee6de1b8e574c0ea442ec1e611e2247764de")
-setManifestid(761601,"5936204945665114510")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 761600.lua
+--Gamename Onimusha: Warlords
+addappid(761600) --Mainappid Onimusha: Warlords
+addappid(761601, 1, "a9cebe81198dc3bda033a39cb706ee6de1b8e574c0ea442ec1e611e2247764de") --Main Windows Depot Onimusha: Warlords
+setManifestid(761601, "5936204945665114510", 10137770672)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(961480) --Dlcname AppID 961480
+addtoken(961480, "3569995046521784826")
