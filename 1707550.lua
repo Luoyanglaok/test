efd175b97aeba4d0561d6b240106bc88ee646dc5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:08:35 +0800

    add 1707550.lua

--- 1707550.lua
+++ 1707550.lua
@@ -1,3 +1,6 @@
-addappid(1707550)
-addappid(1707551,0,"f4410bc90ecd9cb0290cb8844063c55ea74deb00a789cd359633fd0790a6f742")
-setManifestid(1707551,"9048089881719302736")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1707550.lua
+--Gamename Granny 3
+addappid(1707550, 1, "abd7a930a0edf52b0577d2a6ba38bbe513a7b264c13b3bfd817ea090f719c27c") --Mainappid Granny 3
+addappid(1707551, 1, "f4410bc90ecd9cb0290cb8844063c55ea74deb00a789cd359633fd0790a6f742") --Main Windows Depot Granny 3
+setManifestid(1707551, "5572416366286417751", 476643264)
