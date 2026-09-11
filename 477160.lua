696058243ba2d94e9e7755b34576bd4a3017e16f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 03:42:12 +0800

    add 477160.lua

--- 477160.lua
+++ 477160.lua
@@ -1,15 +1,21 @@
-addappid(477160)
-addappid(477161,0,"8bc8773aa491fcab031c973d15bae81a5139cd9d28bdbe82776e9f39dba071d1")
-setManifestid(477161,"6402861719820014798")
-addappid(477162,0,"3e1b0a746178eda39e69d130aac41e225a61f7777ccd8907b3b81f02918242ac")
-setManifestid(477162,"3788656093835144143")
-addappid(477163,0,"48a9d63f348dcd848df0af0fe2eb01efddbb549c03d113bfca9d88015d8b6b7d")
-setManifestid(477163,"129240740414445083")
-addappid(632870,0,"292790c00ead9bdc3e2da82f59687f73b21c059eb0e98e3d6d55a9218b594ae9")
-setManifestid(632870,"6110178810839816362")
-addappid(477164,0,"d05522bd5e3f6e7f6bc455253be55a9d3057b94b95da110d6365a246121a5fdc")
-setManifestid(477164,"803940719847356966")
-addappid(477165,0,"91b02c0aecb8688c5844032fe30518f25ac754a1b32ca68c91a6021623d06ebd")
-setManifestid(477165,"1411043900457074403")
-addappid(477166,0,"3efd94cbca4e47344be62115da0240c6b360356e1634b47f47dbbd82b4de329d")
-setManifestid(477166,"2111907740247976469")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 477160.lua
+--Gamename Human Fall Flat
+addappid(477160, 1, "6183f2ec8046e4576180f01e2e88d7934ca49dc5db95f178bc343e50c852b770") --Mainappid Human Fall Flat
+addappid(477161, 1, "8bc8773aa491fcab031c973d15bae81a5139cd9d28bdbe82776e9f39dba071d1") --Main Windows Depot Human Fall Flat
+setManifestid(477161, "8546845114557955722", 2337703888)
+addappid(477162, 1, "3e1b0a746178eda39e69d130aac41e225a61f7777ccd8907b3b81f02918242ac") --Main Macos Depot Human Fall Flat
+setManifestid(477162, "8083533861768738269", 2346418944)
+addappid(477163, 1, "48a9d63f348dcd848df0af0fe2eb01efddbb549c03d113bfca9d88015d8b6b7d") --Main Linux Depot Human Fall Flat
+setManifestid(477163, "129240740414445083", 616680464)
+addappid(477164, 1, "d05522bd5e3f6e7f6bc455253be55a9d3057b94b95da110d6365a246121a5fdc") --Main Windows Depot Human Fall Flat
+setManifestid(477164, "6577834523877167712", 336643824)
+addappid(477165, 1, "91b02c0aecb8688c5844032fe30518f25ac754a1b32ca68c91a6021623d06ebd") --Main Windows Depot Human Fall Flat
+setManifestid(477165, "7673690209049765361", 2902858640)
+addappid(477166, 1, "3efd94cbca4e47344be62115da0240c6b360356e1634b47f47dbbd82b4de329d") --Main Windows Depot Human Fall Flat
+setManifestid(477166, "2111907740247976469", 304)
+addappid(632870, 1, "292790c00ead9bdc3e2da82f59687f73b21c059eb0e98e3d6d55a9218b594ae9") --Main Windows Depot Human Fall Flat
+setManifestid(632870, "6110178810839816362", 59201904)
+--Dlcname Human Fall Flat Official Soundtrack
+addappid(632871, 1, "bb0872c2ea6d9a04bbf262213c345fb98a4b40db0accda9a7587acb823bd6133") --Dlc Windows Depot Human Fall Flat Official Soundtrack
+setManifestid(632871, "6557631332683396479", 130318960)
