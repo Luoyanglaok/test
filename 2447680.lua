e36a82e7414a95c47ad9e10589d324f0d778f584
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:05:46 +0800

    add 2447680.lua

---
+++ 2447680.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2447680.lua
+--Gamename Island Notes
+addappid(2447680) --Mainappid Island Notes
+addappid(2447681, 1, "46c2a8a75923881e79934768d412e79838452504b275b9f17e8d9f86d4cea573") --Main Windows Depot Island Notes
+setManifestid(2447681, "5862791892701172267", 6853342192)
