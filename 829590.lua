9f61a5f7d4672da8c14a1f501bb6c30728b40bb9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:44:38 +0800

    add 829590.lua

--- 829590.lua
+++ 829590.lua
@@ -1,6 +1,12 @@
-addappid(829590)
-setManifestid(228988,"6645201662696499616")
-addappid(829591,0,"5a3688082c17e1eeb38542df01cc984d5d3e1ad294135bc3cb1109c34d321c96")
-setManifestid(829591,"4099247285123110628")
-addappid(1178321,0,"b87fb51f621665ab1ac25cb6d95844e72f59fbf1de4308a09fb63b40c5ea6e5e")
-setManifestid(1178321,"3915988355857129570")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 829590.lua
+--Gamename CryoFall
+addappid(829590, 1, "c0695e36e1dd54b58f2c14c9bb5ebbec67d76cca4717bac058371bed4c014d0a") --Mainappid CryoFall
+addappid(829591, 1, "5a3688082c17e1eeb38542df01cc984d5d3e1ad294135bc3cb1109c34d321c96") --Main Windows Depot CryoFall
+setManifestid(829591, "4099247285123110628", 533196560)
+--Dlcname CryoFall - Supporter Pack
+addappid(1178321, 1, "b87fb51f621665ab1ac25cb6d95844e72f59fbf1de4308a09fb63b40c5ea6e5e") --Dlcname CryoFall - Supporter Pack
+setManifestid(1178321, "3915988355857129570", 60752640)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
