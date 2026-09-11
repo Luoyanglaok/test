3359ac8ae26d9173141c8b8205e2d4a51c1d36ab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 12:03:17 +0800

    add 1033120.lua

--- 1033120.lua
+++ 1033120.lua
@@ -1,3 +1,6 @@
-addappid(1033120)
-addappid(1033121,0,"b2e33da01c71fd9c1c24fe7ad3b84543850e5e299efedee8ea88d3181aa0d8fc")
-setManifestid(1033121,"4719312213114284046")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1033120.lua
+--Gamename SEX MOTEL
+addappid(1033120) --Mainappid SEX MOTEL
+addappid(1033121, 1, "b2e33da01c71fd9c1c24fe7ad3b84543850e5e299efedee8ea88d3181aa0d8fc") --Main Windows Depot SEX MOTEL
+setManifestid(1033121, "4719312213114284046", 484580064)
