4e57a7cbe882f0b7c7979ffaf74f1b27ad46c1d5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:12:36 +0800

    add 3172670.lua

--- 3172670.lua
+++ 3172670.lua
@@ -1,3 +1,15 @@
-addappid(3172670)
-addappid(3172671,0,"5af754b4d0fc021eb39db5a02fd4e703a3cf7a7c0c66f35c83294a7f669cb70e")
-setManifestid(3172671,"5897545056764793688")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3172670.lua
+--Gamename Sex Arena: Passion of Aquilon 💋⚔
+addappid(3172670) --Mainappid Sex Arena: Passion of Aquilon 💋⚔
+addappid(3172671, 1, "5af754b4d0fc021eb39db5a02fd4e703a3cf7a7c0c66f35c83294a7f669cb70e") --Main Windows Language Depot English Sex Arena: Passion of Aquilon 💋⚔
+setManifestid(3172671, "5897545056764793688", 277794736)
+--Dlcname XXX Motion Pack – Sex Arena 🎬
+addappid(3668750, 1, "d7c43b8e21161c326f8f03e272621042c0101e51f4c71f41a79ee24b4ff5bfdc") --Dlcname XXX Motion Pack – Sex Arena 🎬
+setManifestid(3668750, "1736418237939953179", 494482080)
+--Dlcname Carnal Archives – Sex Arena 🖌️
+addappid(3668760, 1, "84f184304e61490990a3d766d41e0c99ab6f9d09aada14c04468b6f892079830") --Dlcname Carnal Archives – Sex Arena 🖌️
+setManifestid(3668760, "9038966144136096915", 80866832)
+--Dlcname Desktop Desires – Sex Arena 🖥️
+addappid(3668770, 1, "64bbdd6bab2889d8d3416d719d1b81825e742002225cff10e069820d7fb73132") --Dlcname Desktop Desires – Sex Arena 🖥️
+setManifestid(3668770, "7032950245739067370", 88612992)
