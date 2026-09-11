3936660d6c4581c67003e99873afd15b58a7e601
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:58:54 +0800

    add 263300.lua

--- 263300.lua
+++ 263300.lua
@@ -1,4 +1,9 @@
-addappid(263300)
-setManifestid(228990,"1829726630299308803")
-addappid(263301,0,"7697b7e83a550c2d028f063eb6d7e86cdcf158a48efd1a9c7ee08b3de077b1e9")
-setManifestid(263301,"5598002421519384928")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 263300.lua
+--Gamename BlazBlue: Calamity Trigger
+addappid(263300) --Mainappid BlazBlue: Calamity Trigger
+addappid(263301, 1, "7697b7e83a550c2d028f063eb6d7e86cdcf158a48efd1a9c7ee08b3de077b1e9") --Main Windows Depot BlazBlue: Calamity Trigger
+setManifestid(263301, "5598002421519384928", 0)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
