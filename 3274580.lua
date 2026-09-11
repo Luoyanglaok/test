f7f41c602b46cda90bf7aa3fd666df2af1db7ca0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:12:50 +0800

    add 3274580.lua

---
+++ 3274580.lua
@@ -0,0 +1,32 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3274580.lua
+--Gamename Anno 117: Pax Romana
+addappid(3274580, 1, "353c6ea14a59554350687fbc679379bf5094473ae8792b1324bb55536ae4d88e") --Mainappid Anno 117: Pax Romana
+addappid(3274581, 1, "29f1b2f2b84b5981fe5e3ebadc14b925e747eea87b6556a95001d576acb9beb9") --Main Windows Depot Anno 117: Pax Romana
+setManifestid(3274581, "5845007920181389140", 69215831760)
+addappid(3274582, 1, "f04b6ff10311fb1d95b43528e4681da8a1b615a31d69275af308d77f93602a4f") --Main Windows Depot Anno 117: Pax Romana
+setManifestid(3274582, "2323456942143318593", 334050784)
+addappid(3274583, 1, "dc97a69d99aeaea85bf8ec545b63e9a140606870c2f17f9fec7967c463060618") --Main Windows Depot Anno 117: Pax Romana
+setManifestid(3274583, "4939646312621748083", 371903584)
+addappid(3274584, 1, "e8679d4318f2f44fc44ff183048c09321b4b84000680a1675fe1dd5463d2f24e") --Main Windows Depot Anno 117: Pax Romana
+setManifestid(3274584, "3648053476570262086", 386455264)
+addappid(3274585, 1, "b08a4e3e71782afe4166e047a91bed2a236df33c2b0d0fe02a7ca3dba1a15912") --Main Windows Depot Anno 117: Pax Romana
+setManifestid(3274585, "174882112856049611", 355312800)
+--Share Depots
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(3749050) --Dlcname Anno 117: Pax Romana – Year 1 Pass
+addappid(3749060) --Dlcname Anno 117: Pax Romana – Year 1 Pass - Ubisoft activation
+addappid(3749080) --Dlcname Anno 117: Pax Romana Standard Edition - Ubisoft Activation
+addappid(3749090) --Dlcname Anno 117: Pax Romana Gold Edition - Ubisoft Activation
+addappid(3858750) --Dlcname Anno 117: Pax Romana – Prophecies of Ash
+addappid(3858760) --Dlcname Anno 117: Pax Romana – Prophecies of Ash Ubisoft Activation
+addappid(3858770) --Dlcname Anno 117: Pax Romana – The Hippodrome
+addappid(3858780) --Dlcname Anno 117: Pax Romana – The Hippodrome Ubisoft Activation
+addappid(3874230) --Dlcname Anno 117 - Marvellous Mosaic Pack
+addappid(3874240) --Dlcname Anno 117 - CDLC 01 Ubisoft Activation
+addappid(3874260) --Dlcname Anno 117: Pax Romana – Blooming Cities Pack
+addappid(3874270) --Dlcname Anno 117: Pax Romana – Blooming Cities Pack Ubisoft Activation
+addappid(3875910) --Dlcname Anno 117 - Closed Beta Ubisoft Activation
+addappid(4168120) --Dlcname Anno 117: Pax Romana Standard Edition Launch - Ubisoft Activation
+addappid(4168130) --Dlcname Anno 117: Pax Romana Gold Edition Launch - Ubisoft Activation
