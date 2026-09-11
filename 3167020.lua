e0155deb61d910465a8c9244f1ff871d9ff66993
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:04:37 +0800

    add 3167020.lua

---
+++ 3167020.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3167020.lua
+--Gamename Escape from Duckov 逃离鸭科夫
+addappid(3167020, 1, "868bad77591803956d03d9863f4775a7d402330362cbd41c1c764c994dfae9fa") --Mainappid Escape from Duckov 逃离鸭科夫
+addappid(3167021, 1, "6a1377569bff7441f985074b6ed8692c0c8f6681b80a75e020dd1a10419b5dea") --Main Windows Depot Escape from Duckov 逃离鸭科夫
+setManifestid(3167021, "177545053407069080", 1179934192)
+addappid(3167022, 1, "862fd0e5628b2423c67e71daa4e95147265f746e4f4adf57350eb171f822c616") --Main Macos Depot Escape from Duckov 逃离鸭科夫
+setManifestid(3167022, "2670200030787709548", 1271953248)
+--Dlcname 逃离鸭科夫 Soundtrack
+addappid(4240540) --Dlcname 逃离鸭科夫 Soundtrack
+addappid(4240541, 1, "8dbb2783f59841e880aca25d8a4b25416d0e348a3c21b928e66ab92c1df4c481") --Dlc Windows Depot 逃离鸭科夫 Soundtrack
+setManifestid(4240541, "9196431939025440725", 111880912)
+addappid(4240542, 1, "6c9e381a31330d89ea460449c4fb3b99caa8d77412330d4d559f3d3f43a6cd7b") --Dlc Windows Depot 逃离鸭科夫 Soundtrack
+setManifestid(4240542, "3220741354592645442", 10381584)
