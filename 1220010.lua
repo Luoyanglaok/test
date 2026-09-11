593279afadcf3b7dbd5cd47854ea8eb4fc96903c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:08:55 +0800

    add 1220010.lua

--- 1220010.lua
+++ 1220010.lua
@@ -1,9 +1,17 @@
-addappid(1220010)
-addappid(1220011,0,"c282e5aea5508168d72b73189b916ba008b9c13e82fad2a3e0cc00ab1dac1b7e")
-setManifestid(1220011,"7859147108674617922")
-addappid(1220012,0,"0d6a07561c79cb1e2158055e6aae53a8562325b93819ca782fd9567b14ad15ae")
-setManifestid(1220012,"984439740782699351")
-addappid(1220013,0,"ded94c0129efe3e30efc76ebdd7bb1a952abbc9e8ab9f5ddba23174abbe48ec9")
-setManifestid(1220013,"450423315144445665")
-addappid(1220014,0,"5d1c6ff6bb56f51088116ec62f51e8b428b7ecfec5d892c323bbd22e09bfed95")
-setManifestid(1220014,"4124646638841245346")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1220010.lua
+--Gamename Home Behind 2
+addappid(1220010) --Mainappid Home Behind 2
+addappid(1220011, 1, "c282e5aea5508168d72b73189b916ba008b9c13e82fad2a3e0cc00ab1dac1b7e") --Main Windows Depot Home Behind 2
+setManifestid(1220011, "4207158257719004359", 793916288)
+addappid(1220012, 1, "0d6a07561c79cb1e2158055e6aae53a8562325b93819ca782fd9567b14ad15ae") --Main Windows Depot Home Behind 2
+setManifestid(1220012, "943602423086190937", 796124144)
+addappid(1220013, 1, "ded94c0129efe3e30efc76ebdd7bb1a952abbc9e8ab9f5ddba23174abbe48ec9") --Main Macos Depot Home Behind 2
+setManifestid(1220013, "450423315144445665", 798178064)
+addappid(1220014, 1, "5d1c6ff6bb56f51088116ec62f51e8b428b7ecfec5d892c323bbd22e09bfed95") --Main Linux Depot Home Behind 2
+setManifestid(1220014, "4124646638841245346", 811825200)
+--Dlcname Home Behind 2 Soundtrack
+addappid(1410460, 1, "09302a9340f1af9debc8f326e038ee2fac241b1c72661076d31f5320dd18868a") --Dlcname Home Behind 2 Soundtrack
+setManifestid(1410460, "3675795718833313531", 52258512)
+addappid(1410461, 1, "e75213e1750a8f4a3c89b56955fb87cd23b71b679da8501eb54a22dbab04e9d4") --Dlc Windows Depot Home Behind 2 Soundtrack
+setManifestid(1410461, "7519132492502343241", 106154128)
