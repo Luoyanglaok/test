f2b0f265fadb447c632d2613d735a07a9932f264
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:27:15 +0800

    add 4838500.lua

---
+++ 4838500.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4838500.lua
+--Gamename 天亮之后
+addappid(4838500) --Mainappid 天亮之后
+addappid(4838501, 1, "56bd0c85867ee5e671b21fbdd3c484e5507e5127961189a68e7ebbe3d8e70e13") --Main Windows Depot 天亮之后
+setManifestid(4838501, "8165648506143148619", 8756525904)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
