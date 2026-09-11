9317e4b4b29592c341aa72e11c38d664ff42ef0e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:59:33 +0800

    add 1017180.lua

--- 1017180.lua
+++ 1017180.lua
@@ -1,5 +1,11 @@
-addappid(1017180)
-setManifestid(228986,"8782296191957114623")
-addappid(1017181,0,"244a982ca42ee3d2f9489b8debf008732bea45f9a499101041e26bd0dccfa4ed")
-setManifestid(1017181,"7731485613881054580")
-addappid(1017182,0,"7efbc11f6c4f97523abaf2aac374158ac23c3348938680d9f7c88632889446e9")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1017180.lua
+--Gamename The Long Drive
+addappid(1017180, 1, "f794bf137087c34994cfc1b3573ecc9c6b0d04f0290b6cfcc63b12f62b09fca1") --Mainappid The Long Drive
+addappid(1017181, 1, "244a982ca42ee3d2f9489b8debf008732bea45f9a499101041e26bd0dccfa4ed") --Main Windows Depot The Long Drive
+setManifestid(1017181, "7731485613881054580", 337576592)
+addappid(1017182, 1, "7efbc11f6c4f97523abaf2aac374158ac23c3348938680d9f7c88632889446e9") --Main Windows Depot The Long Drive
+setManifestid(1017182, "6000646271089049071", 262409104)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
