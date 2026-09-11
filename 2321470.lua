c2e4e4730d9b060a2faaedeba6234c24f0b1523a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:50:12 +0800

    add 2321470.lua

--- 2321470.lua
+++ 2321470.lua
@@ -1,3 +1,9 @@
-addappid(2321470)
-addappid(2321471,0,"18154d412c1c3c3719d8f05c119354536a5228cdd169ca75488708a215c1cddf")
-setManifestid(2321471,"2442071377255205983")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2321470.lua
+--Gamename Deep Rock Galactic: Survivor
+addappid(2321470, 1, "1b437453d839af65b6e35906a8fc5d3164e50eccfd31493fdd4868e1958c7df1") --Mainappid Deep Rock Galactic: Survivor
+addappid(2321471, 1, "18154d412c1c3c3719d8f05c119354536a5228cdd169ca75488708a215c1cddf") --Main Windows Depot Deep Rock Galactic: Survivor
+setManifestid(2321471, "2170332834079432460", 2222198976)
+addappid(2782850) --Dlcname Deep Rock Galactic: Survivor - Supporter Pack
+addappid(3818560) --Dlcname Deep Rock Galactic: Survivor - After Hours Pack
+addappid(4395500) --Dlcname Deep Rock Galactic: Survivor - Heavy Duty Expansion
