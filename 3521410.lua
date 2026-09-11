86ac2122e0e33daf50d2808208fc7453882d3f4e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:04:47 +0800

    add 3521410.lua

---
+++ 3521410.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3521410.lua
+--Gamename Cats & Cups
+addappid(3521410) --Mainappid Cats & Cups
+addappid(3521411, 1, "357127f4ccbf3e31313b143ba33611f7d67f1965dcfa8959f3bb507aee81b031") --Main Windows Depot Cats & Cups
+setManifestid(3521411, "1300916154333220204", 333126352)
