92af34b8b285116899e9d5911e62e2cc70a56568
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:09:21 +0800

    add 3886200.lua

---
+++ 3886200.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3886200.lua
+--Gamename BrokenLore: DON’T LIE
+addappid(3886200) --Mainappid BrokenLore: DON’T LIE
+addappid(3886201, 1, "7ac0ab5351d199aef423516459d7f4efebea1adca05e229a7b6e70445130e6ec") --Main Windows Depot BrokenLore: DON’T LIE
+setManifestid(3886201, "1646569095749950351", 6078138496)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
