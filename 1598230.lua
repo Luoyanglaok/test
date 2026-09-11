d1a974745563bc8dd1468ac31f63611281118c6e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:17:51 +0800

    add 1598230.lua

---
+++ 1598230.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1598230.lua
+--Gamename KILLER INN
+addappid(1598230) --Mainappid KILLER INN
+addappid(1598231, 1, "f7759ab7595d3c7e6d6f4665b515e43036aa27eef8264326078f1c0acd94cb55") --Main Windows Depot KILLER INN
+setManifestid(1598231, "9122393182245473786", 26493119744)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4179300) --Dlcname KILLER INN Special Supporter Upgrade
+addappid(4794360) --Dlcname KILLER INN Purple Resonance Gift Bundle
