7ff9b396295d5ee12ea3ebfe6aa7c5f56f007141
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:12:21 +0800

    add 4132330.lua

---
+++ 4132330.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4132330.lua
+--Gamename Pizza House Simulator🍕
+addappid(4132330, 1, "30a655c7b301177576fc1072673bb9c5744de0f281229d44c77c40a37c659259") --Mainappid Pizza House Simulator🍕
+addappid(4132331, 1, "51035a357a681b45f9b70402c409d25181faea5a48999aaf9eb13536d0a203e9") --Main Windows Depot Pizza House Simulator🍕
+setManifestid(4132331, "5310101576199142599", 4221334752)
+--Dlcname Pizza House Simulator🍕 Demo
+addappid(4776950) --Dlcname Pizza House Simulator🍕 Demo
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Dlc Windows Depot Pizza House Simulator🍕 Demo
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Dlc Windows Depot Pizza House Simulator🍕 Demo
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4776951, 1, "48995cde115b11a70099b023ec7d049a62dd131ecfda9350b0db4ed277fe9923") --Dlc Windows Depot Pizza House Simulator🍕 Demo
+setManifestid(4776951, "8634600811978362198", 3415149856)
+--Share Depots
