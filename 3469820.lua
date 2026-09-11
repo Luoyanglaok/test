8c12060ec5afed56bc9fec7d40a2ed6107482efa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:16:07 +0800

    add 3469820.lua

---
+++ 3469820.lua
@@ -0,0 +1,41 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3469820.lua
+--Gamename Cipheur
+addappid(3469820) --Mainappid Cipheur
+addappid(3469821, 1, "5FD94A0F216305815D4C2149A68120ED05133C1C7860B57B4A1C1AA18287CC59") --Main Windows Depot Cipheur
+setManifestid(3469821, "1332398187416076611", 94641952)
+addappid(3469822, 1, "2DFF794BE00732599951DF792CD5012C2D0CF24611579EEE8D11BDEDC4D3946E") --Main Macos Depot Cipheur
+setManifestid(3469822, "3925904228958551884", 115813264)
+addappid(3469823, 1, "A2478C7119C4DF65B0F4CE23F3C2905142D337BB977721CB6C64190334A85A8C") --Main Linux Depot Cipheur
+setManifestid(3469823, "5271109967214443954", 98549248)
+addappid(3559270) --Dlcname Cipheur - Famous People Quotes 2
+addappid(3559280) --Dlcname Cipheur - Famous People Quotes 3
+addappid(3559290) --Dlcname Cipheur - Famous People Quotes 4
+addappid(3559300) --Dlcname Cipheur - Famous People Quotes 5
+addappid(3570970) --Dlcname Cipheur - Proverbs
+addappid(3570980) --Dlcname Cipheur - Proverbs 2
+addappid(3570990) --Dlcname Cipheur - Proverbs 3
+addappid(3571000) --Dlcname Cipheur - Proverbs 4
+addappid(3571090) --Dlcname Cipheur - Videogame Quotes
+addappid(3571100) --Dlcname Cipheur - Videogame Quotes 2
+addappid(3571230) --Dlcname Cipheur - Stupid Laws
+addappid(3571250) --Dlcname Cipheur - Famous Ads
+addappid(3571260) --Dlcname Cipheur - Famous Ads 2
+addappid(3571270) --Dlcname Cipheur - Uncommon Words
+addappid(3571280) --Dlcname Cipheur - Uncommon Words 2
+addappid(3571290) --Dlcname Cipheur - Uncommon Words 3
+addappid(3580550) --Dlcname Cipheur - Famous Last Words
+addappid(3580560) --Dlcname Cipheur - Famous Last Words 2
+addappid(3580570) --Dlcname Cipheur - Famous Last Words 3
+addappid(3580590) --Dlcname Cipheur - Stupid Laws 2
+addappid(3650870) --Dlcname Cipheur - Цитаты из мультфильмов
+addappid(3650880) --Dlcname Cipheur - Цитаты из советских кинофильмов 1
+addappid(3650890) --Dlcname Cipheur - Цитаты из советских кинофильмов 2
+addappid(3865260) --Dlcname Cipheur - Trivia: Geography
+addappid(3865270) --Dlcname Cipheur - Trivia: Physics
+addappid(3865280) --Dlcname Cipheur - Trivia: Biology
+addappid(3865290) --Dlcname Cipheur - Trivia: Astronomy
+addappid(3874840) --Dlcname Cipheur - Trivia: Chemistry
+addappid(3954190) --Dlcname Cipheur - Trivia: Biology 2
+addappid(4254030) --Dlcname AppID 4254030
+addappid(4254040) --Dlcname Cipheur - Trivia: Food
