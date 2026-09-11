9b65316832f9612927ddc94fe985585ca9f9b413
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:01:17 +0800

    add 588430.lua

--- 588430.lua
+++ 588430.lua
@@ -1,5 +1,14 @@
-addappid(588430)
-addappid(588431,0,"81d996a10800cbb00139fb9543976314983bc892251111299329bbf277e87865")
-setManifestid(588431,"1344652083257557844")
-addappid(588432,0,"8716ccb13bc85d5d209f9f7614e6d6bb3ae06667410c7cea5192234273f94c0f")
-setManifestid(588432,"8637710508957144818")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 588430.lua
+--Gamename Fallout Shelter
+addappid(588430, 1, "50daa28a8d454b4071dc4478cf3e9b2fcfc4c041b560f0ae978a906ccfdcabc5") --Mainappid Fallout Shelter
+addappid(588431, 1, "81d996a10800cbb00139fb9543976314983bc892251111299329bbf277e87865") --Main Windows Depot Fallout Shelter
+setManifestid(588431, "7036583868162733800", 581701712)
+addappid(588432, 1, "8716ccb13bc85d5d209f9f7614e6d6bb3ae06667410c7cea5192234273f94c0f") --Main Windows Depot Fallout Shelter
+setManifestid(588432, "6498690623362296326", 190704)
+addappid(615510) --Dlcname AppID 615510
+addappid(617490) --Dlcname AppID 617490
+addappid(617500) --Dlcname AppID 617500
+addappid(617510) --Dlcname AppID 617510
+addappid(617520) --Dlcname AppID 617520
+addappid(617530) --Dlcname AppID 617530
