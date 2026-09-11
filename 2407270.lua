fb85ea1186f52ab636ec76ea0a6b04ee160241d4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:34:45 +0800

    add 2407270.lua

--- 2407270.lua
+++ 2407270.lua
@@ -1,3 +1,9 @@
-addappid(2407270)
-addappid(2407271,0,"7fbec0c07311645df5048184f930dab908082e6a96be2f0814a3159eaa8de5a6")
-setManifestid(2407271,"8048381649970259673")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2407270.lua
+--Gamename AI LIMIT
+addappid(2407270, 1, "2615d909118b737d77236fb50a3dd435f355cc2190c1a6ad30a54a3bd11955cc") --Mainappid AI LIMIT
+addappid(2407271, 1, "7fbec0c07311645df5048184f930dab908082e6a96be2f0814a3159eaa8de5a6") --Main Windows Depot AI LIMIT
+setManifestid(2407271, "118013794508952302", 19998821456)
+addappid(3391010) --Dlcname AI LIMIT - Deluxe Edition Upgrade Pack
+--Missing Dlcs
+--Dlcname AI LIMIT - Eirene's Furnace of War
