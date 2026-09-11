d7cb30bea76c4dfc31685bc14e277ffb2100eb6a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:05:51 +0800

    add 1571690.lua

--- 1571690.lua
+++ 1571690.lua
@@ -1,3 +1,8 @@
-addappid(1571690)
-addappid(1571691,0,"b4fd79e95fc359fa05eb3227444147aea72f2b4de139bf7d74397bd3dc3c2b68")
-setManifestid(1571691,"3349929223445410038")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1571690.lua
+--Gamename Sexorcist Academy
+addappid(1571690, 1, "6960d39bc4d79751a2b26a51f3a531d4b28ab05e84742f947c8540651bae5c6c") --Mainappid Sexorcist Academy
+addappid(1571691, 1, "b4fd79e95fc359fa05eb3227444147aea72f2b4de139bf7d74397bd3dc3c2b68") --Main Windows Depot Sexorcist Academy
+setManifestid(1571691, "3349929223445410038", 1471466352)
+addappid(1571692, 1, "9672c4d3d30e36023143a88cc282b03d08f02f381c8325132781de9a6c6d83f5") --Main Macos Depot Sexorcist Academy
+setManifestid(1571692, "4546450731521282625", 1449072784)
