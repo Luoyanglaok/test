c070a82bf851523cdb97dd9d9691b838c55b52a9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:54:04 +0800

    add 1336490.lua

--- 1336490.lua
+++ 1336490.lua
@@ -1,3 +1,11 @@
-addappid(1336490)
-addappid(1336491,0,"c62157594f44e2a3087998f70ca9dfe4736a0a98d9d1eca658ecaaf102f17dec")
-setManifestid(1336491,"5641147592114689776")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1336490.lua
+--Gamename Against the Storm
+addappid(1336490, 1, "66eeeb696a07fb6d6e9b309366dcfb7fac46f85aeb1f28bb161f58817b3cd9ae") --Mainappid Against the Storm
+addappid(1336491, 1, "c62157594f44e2a3087998f70ca9dfe4736a0a98d9d1eca658ecaaf102f17dec") --Main Windows Depot Against the Storm
+setManifestid(1336491, "7639203643718080630", 4521413040)
+--Dlcname Against the Storm - Supporter Pack
+addappid(2712950, 1, "e87c2fd0103b135d75ea1e7ab9961328a7d9bdd04bc07c98c60539ac490017fb") --Dlcname Against the Storm - Supporter Pack
+setManifestid(2712950, "2460933230736712672", 482518784)
+addappid(3075500) --Dlcname Against the Storm - Keepers of the Stone
+addappid(3725110) --Dlcname Against the Storm - Nightwatchers
