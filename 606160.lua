870dee7bbdc796d07235009702d894fe622ff50d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 11:36:50 +0800

    add 606160.lua

---
+++ 606160.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 606160.lua
+--Gamename ROUTINE
+addappid(606160, 1, "368986b48cf7cf15a2876422b0ce68c5f7ec1436badd957b0193db563ca7bb63") --Mainappid ROUTINE
+addappid(606161, 1, "f1cd8fe75a865cf2333d06c14f644b5ef8dd9fc4425aac2a35ae7a75eb4da0b1") --Main Windows Language Depot English ROUTINE
+setManifestid(606161, "5082450058729967054", 10467465744)
