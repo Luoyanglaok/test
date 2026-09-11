bac647c1accee298fa9289801337db84819df396
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:22:44 +0800

    add 3115840.lua

--- 3115840.lua
+++ 3115840.lua
@@ -1,4 +1,6 @@
-addappid(3115840)
-addappid(3115841,0,"3180f7ad45376d2df8da7e98444b2ae2b7434f57b731ca208afc9079867f4ef4")
-setManifestid(3115841,"6338001687414724217")
-addappid(3115842)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3115840.lua
+--Gamename Sexy Girl Next Door - Virtual Valentine Sex
+addappid(3115840) --Mainappid Sexy Girl Next Door - Virtual Valentine Sex
+addappid(3115841, 1, "3180f7ad45376d2df8da7e98444b2ae2b7434f57b731ca208afc9079867f4ef4") --Main Windows Depot Sexy Girl Next Door - Virtual Valentine Sex
+setManifestid(3115841, "6338001687414724217", 1673911088)
