fc8ece956f46fc777c47ec4b8f8de8ac26ccba36
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:34:14 +0800

    add 1574580.lua

--- 1574580.lua
+++ 1574580.lua
@@ -1,3 +1,8 @@
-addappid(1574580)
-addappid(1574581,1,"855676fed2c8660b9bc487411cf69d3ea10a505a2a3b1abc4d76b0433098f7fc")
-setManifestid(1574581,"5132607692592855863")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1574580.lua
+--Gamename Strange Horticulture
+addappid(1574580) --Mainappid Strange Horticulture
+addappid(1574581, 1, "855676fed2c8660b9bc487411cf69d3ea10a505a2a3b1abc4d76b0433098f7fc") --Main Windows Depot Strange Horticulture
+setManifestid(1574581, "5132607692592855863", 234064560)
+addappid(1574582, 1, "d7d7dfe8d8d85953f24146ddb4bd34b9f661e54a7ccc74803eb7134c97745484") --Main Macos Depot Strange Horticulture
+setManifestid(1574582, "8404410664450384564", 226929328)
