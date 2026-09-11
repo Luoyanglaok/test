c5b4b5b4b349e2fa6c44900f58e3c04d06d9af46
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:31:21 +0800

    add 1190970.lua

--- 1190970.lua
+++ 1190970.lua
@@ -1,5 +1,13 @@
-addappid(1190970)
-addappid(1190971,0,"a6202777d416dcf4d2f36cc5f7a69f90cca6e1bfbde6e27eaa4e97e86c7d3196")
-setManifestid(1190971,"9077091668481121453")
-addappid(2649200,0,"75c666e7e7f011eddb178036bbb4b16446378ef945ecf6811f32e2a4f96bfcc7")
-setManifestid(2649200,"2894455407820860002")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1190970.lua
+--Gamename House Flipper 2
+addappid(1190970, 1, "f2f1aaf6899f899ad07cb2da7594595d1acc24d732a23f7a4a16cc959e5c697b") --Mainappid House Flipper 2
+addappid(1190971, 1, "a6202777d416dcf4d2f36cc5f7a69f90cca6e1bfbde6e27eaa4e97e86c7d3196") --Main Windows Depot House Flipper 2
+setManifestid(1190971, "6230646019910921279", 11099243456)
+--Dlcname House Flipper 2 - Supporter Pack
+addappid(2649200, 1, "75c666e7e7f011eddb178036bbb4b16446378ef945ecf6811f32e2a4f96bfcc7") --Dlcname House Flipper 2 - Supporter Pack
+setManifestid(2649200, "2894455407820860002", 3798965904)
+addappid(3363370) --Dlcname House Flipper 2 - Co-op DLC
+addappid(3672960) --Dlcname House Flipper 2 - Scooby-Doo DLC
+addappid(3680100) --Dlcname House Flipper 2 - Pets DLC
+addappid(4351410) --Dlcname House Flipper 2 - Sakura DLC
