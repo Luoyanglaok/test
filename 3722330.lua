946025c55f657da97497a34fd89b607ee7fd5fae
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:02:18 +0800

    add 3722330.lua

--- 3722330.lua
+++ 3722330.lua
@@ -1,6 +1,3 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 3722330.lua
---Gamename Shift At Midnight
-addappid(3722330, 1, "837512e9ffbe1435f39a77a9010a70a3285befe9b540f084e1757623c7bd324e") --Mainappid Shift At Midnight
-addappid(3722331, 1, "241b88aeb27a250cbfff336e4e5ec6f28a60d136f4431f8bdcbf84a0506e5ed4") --Main Windows Depot Shift At Midnight
-setManifestid(3722331, "6738424999942008887", 384056032)
+addappid(3722330)
+addappid(3722331,1,"241b88aeb27a250cbfff336e4e5ec6f28a60d136f4431f8bdcbf84a0506e5ed4")
+setManifestid(3722331,"6738424999942008887")
