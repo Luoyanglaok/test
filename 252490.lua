ee61156e5410ec1f0151e6525466598a1a9f4f23
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:20:26 +0800

    add 252490.lua

--- 252490.lua
+++ 252490.lua
@@ -1,9 +1,28 @@
-addappid(252490)
-setManifestid(228990,"1829726630299308803")
-setManifestid(229002,"7260605429366465749")
-addappid(252494,0,"5d5f72c0460cb7598666696c1b2f612f22efb43fb87cf4322e596bfec8a231ec")
-setManifestid(252494,"605709908456049009")
-addappid(252492,0,"96b750328197855f4311ba6ad6d86e9051866cc6b9a6c851de55cc41b8ad9a24")
-setManifestid(252492,"6504446108943851284")
-addappid(252495,0,"728fcdc1c6af4454f3c86e5bc62a75875af5ba790cc8a2623620e216086e034d")
-setManifestid(252495,"1428864948072679040")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 252490.lua
+--Gamename Rust
+addappid(252490, 1, "070d35c4cac50d6b14881a6fd630bf1c58a061bec585e899a48c813972638108") --Mainappid Rust
+addappid(252492, 1, "96b750328197855f4311ba6ad6d86e9051866cc6b9a6c851de55cc41b8ad9a24") --Main Macos Depot Rust
+setManifestid(252492, "471795495274323010", 3142770656)
+addappid(252494, 1, "5d5f72c0460cb7598666696c1b2f612f22efb43fb87cf4322e596bfec8a231ec") --Main Windows Depot Rust
+setManifestid(252494, "8351861777439364232", 31440298048)
+addappid(252495, 1, "728fcdc1c6af4454f3c86e5bc62a75875af5ba790cc8a2623620e216086e034d") --Main Windows Depot Rust
+setManifestid(252495, "3016265514889343492", 666168960)
+--Dlcname Rust Soundtrack
+addappid(1701720) --Dlcname Rust Soundtrack
+addappid(1701721, 1, "bb2d77fa0840435c4f482147731f6e1050acf3695f83ddb23eb3fa5e39b489b0") --Dlc Windows Depot Rust Soundtrack
+setManifestid(1701721, "912468135639080873", 235393904)
+addappid(1701722, 1, "031921c4cd796f5a4d6de63473f1c6f3adfe37f0b6ce7e1ce98f9d66836d7085") --Dlc Windows Depot Rust Soundtrack
+setManifestid(1701722, "4671480673282610353", 474905776)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
+addappid(494560) --Dlcname AppID 494560
+addtoken(494560, "9612755471074374815")
+addappid(1174370) --Dlcname Instrument Pack
+addappid(1353060) --Dlcname Rust Sunburn Pack
+addappid(1374000) --Dlcname AppID 1374000
+addappid(1670430) --Dlcname Rust Voice Props Pack
+addappid(3891560) --Dlcname Rust Warhammer Pack
