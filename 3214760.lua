675193c81c12d9088d52974d3c770fc8fc74101a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:24:48 +0800

    add 3214760.lua

--- 3214760.lua
+++ 3214760.lua
@@ -1,5 +1,8 @@
-addappid(3214760)
-addappid(3214761,0,"eca4676503d291016a05d1eba062320bcb338ccf294d0b8ea55d2fb69c344b10")
-setManifestid(3214761,"4750399889280094829")
-addappid(3214762,0,"bb842d206582aea08c09e1e6288567829067db21c049c89f23959a731a9dfa9b")
-setManifestid(3214762,"2075981207196233190")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3214760.lua
+--Gamename Medieval Dynasty - Original Soundtrack Vol. 2
+addappid(3214760) --Mainappid Medieval Dynasty - Original Soundtrack Vol. 2
+addappid(3214761, 1, "eca4676503d291016a05d1eba062320bcb338ccf294d0b8ea55d2fb69c344b10") --Main Windows Depot Medieval Dynasty - Original Soundtrack Vol. 2
+setManifestid(3214761, "4750399889280094829", 408185520)
+addappid(3214762, 1, "bb842d206582aea08c09e1e6288567829067db21c049c89f23959a731a9dfa9b") --Main Windows Depot Medieval Dynasty - Original Soundtrack Vol. 2
+setManifestid(3214762, "2075981207196233190", 1293418960)
