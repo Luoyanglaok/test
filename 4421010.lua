b3c7a6a8bb4a3f451ccdd6963aa3ce836167a2b7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:22:35 +0800

    add 4421010.lua

--- 4421010.lua
+++ 4421010.lua
@@ -1,9 +1,3 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 4421010.lua
---Gamename Bills Must Be Paid
-addappid(4421010, 1, "d4498a5e8f21f1b5d64df4737a56df8d5dcb372982757215170308a33112d914") --Mainappid Bills Must Be Paid
-addappid(4421011, 1, "11e5390201ab27a5ff5ccd0a0c03136ba3b4e50f0c08b187d14b36a2a23896d6") --Main Windows Depot Bills Must Be Paid
-setManifestid(4421011, "6848976417670468762", 211379280)
-addappid(4421012, 1, "59e5144afe91bf9c7914f4df2297cdb42b37b584ee885cbbc7f8b4fd6155a156") --Main Macos Depot Bills Must Be Paid
-setManifestid(4421012, "4644916239143317778", 243595504)
-addappid(5020040) --Dlcname Bills Must Be Paid - Supporter Pack
+addappid(4421010)
+addappid(5020040)
+setManifestid(4421011,"6848976417670468762")
