1ffbce0d20d8c9bfb62e46be71334a1e73a2059e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:48:38 +0800

    add 3044740.lua

--- 3044740.lua
+++ 3044740.lua
@@ -1,3 +1,10 @@
-addappid(3044740)
-addappid(3044741,0,"7fe1736dcd0fdddc507d4692a06a7610a694c6875eeeca84ae09158c2ddf2165")
-setManifestid(3044741,"6864847101588814309")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3044740.lua
+--Gamename V-LOVER!
+addappid(3044740, 1, "002d6b01936631994440387fc0ce48d9dd44fae13b1a84330c4d0e64ed653939") --Mainappid V-LOVER!
+addappid(3044741, 1, "7fe1736dcd0fdddc507d4692a06a7610a694c6875eeeca84ae09158c2ddf2165") --Main Windows Depot V-LOVER!
+setManifestid(3044741, "2671468225850350297", 2574886128)
+--Dlcname V-LOVER！ArtBook
+addappid(3780670) --Dlcname V-LOVER！ArtBook
+addappid(3780671, 1, "2a16fbc9fcdc2b7b9070a75c785401d3d2c93f751ac3c16a2fb1c649d67a0ad1") --Dlc Windows Depot V-LOVER！ArtBook
+setManifestid(3780671, "3887514347642553427", 1517069280)
