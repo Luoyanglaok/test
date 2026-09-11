b4f9a56b3461412851d3ceeed85c0c785e1066aa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:48:59 +0800

    add 1631080.lua

--- 1631080.lua
+++ 1631080.lua
@@ -1,3 +1,6 @@
-addappid(1631080)
-addappid(1631081,0,"01c82c96527d7e375cfdbfea412814279f1d24510b9e34e863c0ec1225da59f7")
-setManifestid(1631081,"8912587589440368098")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1631080.lua
+--Gamename HoneySelect2Libido DX
+addappid(1631080, 1, "c13feb3ae1faa32ffbb81871558fa67c12a703c0f873a5b5f11d42542fa71d75") --Mainappid HoneySelect2Libido DX
+addappid(1631081, 1, "01c82c96527d7e375cfdbfea412814279f1d24510b9e34e863c0ec1225da59f7") --Main Windows Depot HoneySelect2Libido DX
+setManifestid(1631081, "8912587589440368098", 10234158896)
