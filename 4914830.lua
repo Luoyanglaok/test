2ab5ef3e2fad13c78e2bec5ad337487c41f466b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:55:55 +0800

    add 4914830.lua

---
+++ 4914830.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4914830.lua
+--Gamename Toy Shop Tidy Up
+addappid(4914830, 1, "b3e21080f232c07e7399da4395c40963223aff25ac9b1dc06944824eb9aab386") --Mainappid Toy Shop Tidy Up
+addtoken(4914830, "9031495605152820117")
+addappid(4914831, 1, "6b44b57453155927f2b1cf9f56bf7669169a7a88d067d2683873aacbb1b1849f") --Main Windows Depot Toy Shop Tidy Up
+setManifestid(4914831, "393217525249537260", 1000522448)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
