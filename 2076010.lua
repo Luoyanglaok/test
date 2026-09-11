a0813fb04e5bc3a04bd08d1e067bc1036ca5f15b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:57:19 +0800

    add 2076010.lua

--- 2076010.lua
+++ 2076010.lua
@@ -1,5 +1,16 @@
-addappid(2076010)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2076011,0,"5bbbf95f9bbd30168e03337a4ca557593e1d32f76a9df1f47ac731425d66ceb8")
-setManifestid(2076011,"3589624785577451940")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2076010.lua
+--Gamename UNDER NIGHT IN-BIRTH II Sys:Celes
+addappid(2076010) --Mainappid UNDER NIGHT IN-BIRTH II Sys:Celes
+addappid(2076011, 1, "5bbbf95f9bbd30168e03337a4ca557593e1d32f76a9df1f47ac731425d66ceb8") --Main Windows Depot UNDER NIGHT IN-BIRTH II Sys:Celes
+setManifestid(2076011, "3166128254051169420", 4048139168)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2353330) --Dlcname UNDER NIGHT IN-BIRTH II Sys:Celes - 25 Announcer Characters
+addappid(2441370) --Dlcname UNDER NIGHT IN-BIRTH II Sys:Celes - Season Pass
+addappid(2677080) --Dlcname UNI2 Character Unlock: Uzuki
+addappid(2677090) --Dlcname UNI2 Character Unlock: Ogre
+addappid(2677100) --Dlcname UNI2 Character Unlock: Izumi
