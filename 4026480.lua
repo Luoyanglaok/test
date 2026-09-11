e30c6fd0cda59ab98816c48176184dd32497eccb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:22:25 +0800

    add 4026480.lua

---
+++ 4026480.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4026480.lua
+--Gamename Cafe Business Manager
+addappid(4026480) --Mainappid Cafe Business Manager
+addappid(4026481, 1, "88a5cd641810cebf1b7d440abb9fbcbb677a99aeb9a018cecd520109c3d01d3a") --Main Windows Depot Cafe Business Manager
+setManifestid(4026481, "3584145531411538289", 4266763392)
