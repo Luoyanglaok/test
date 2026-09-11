e04f33a044871c0a788e6a7ccf1353823bc14890
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:24:04 +0800

    add 3314790.lua

---
+++ 3314790.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3314790.lua
+--Gamename CloverPit
+addappid(3314790, 1, "7f32b9cda5b8cffd32bfc4147e1264c1c25525c40196a30762eec09a85f97d83") --Mainappid CloverPit
+addappid(3314791, 1, "01f256d876a29ffa69ec7fefed5588393e85c3e5326497d7da92427e011759ff") --Main Windows Depot CloverPit
+setManifestid(3314791, "735106840404360210", 122952816)
+addappid(4341120) --Dlcname CloverPit: Unholy Fusion
