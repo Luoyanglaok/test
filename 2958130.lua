996e58654c32ce136c2d09e282cce8652bb1ea94
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:01:49 +0800

    add 2958130.lua

---
+++ 2958130.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2958130.lua
+--Gamename Jurassic World Evolution 3
+addappid(2958130, 1, "28c45829d1af12623b4fead679e384291bfb040d0241fc6be9d05f7d4cdbbd62") --Mainappid Jurassic World Evolution 3
+addappid(2958131, 1, "dc7cb4cb8d7d76fdcab84fed34fa1f799e7459cf1be5ed041116ed846211889d") --Main Windows Depot Jurassic World Evolution 3
+setManifestid(2958131, "5993154482072028239", 16521608224)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3651190) --Dlcname Jurassic World Evolution 3: Badlands Set
+addappid(3651200) --Dlcname Jurassic World Evolution 3: Deluxe Upgrade Pack
+addappid(4131660) --Dlcname Jurassic World Evolution 3: Wetlands
+addappid(4131670) --Dlcname Jurassic World Evolution 3: Rebirth Expansion
+addappid(4553980) --Dlcname Jurassic World Evolution 3: Crocodilia Coast
