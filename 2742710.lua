f36af238cf19cea54a5de67784ddd715987e4f64
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:22:47 +0800

    add 2742710.lua

---
+++ 2742710.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2742710.lua
+--Gamename Cozy Cleaner
+addappid(2742710) --Mainappid Cozy Cleaner
+addappid(2742711, 1, "fa679db3ee25fd5bf046ab8c7004d9dfe98c18c17f44972843fbcf0c66a79695") --Main Windows/Linux Depot Cozy Cleaner
+setManifestid(2742711, "568298799174989342", 465731136)
+addappid(2742712, 1, "f115ae36ec3e619126f6f6de9c0de0273e6d420c28bd8887c016a1cfee6d3b10") --Main Macos Depot Cozy Cleaner
+setManifestid(2742712, "2842443140266778100", 475005472)
