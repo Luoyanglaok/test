03de748cd89ffac762e80f334f5edc45ba728ded
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:07:30 +0800

    add 962400.lua

--- 962400.lua
+++ 962400.lua
@@ -1,3 +1,6 @@
-addappid(962400)
-addappid(962401,0,"62b8ae3bb8c880952382aaaeeeac3d2f87bc304a2e24d0a07709ab9124f6548d")
-setManifestid(962401,"4116798528376785568")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 962400.lua
+--Gamename Granny
+addappid(962400, 1, "71b8e0edb3adae35fe9502c96aa1669b736e466473d87a59acf14b70edb9b907") --Mainappid Granny
+addappid(962401, 1, "62b8ae3bb8c880952382aaaeeeac3d2f87bc304a2e24d0a07709ab9124f6548d") --Main Windows Depot Granny
+setManifestid(962401, "3073919976832489549", 683106864)
