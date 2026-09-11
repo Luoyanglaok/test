ca1b863f914c88836cbe67eb756357cc5b5b18c8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 12:59:19 +0800

    add 1386200.lua

--- 1386200.lua
+++ 1386200.lua
@@ -1,3 +1,6 @@
-addappid(1386200)
-addappid(1386201,0,"7a12b7bd57d0887f826269ac0ae5fec429c3607da86e017f3736cf361e4b31f0")
-setManifestid(1386201,"5229329378059509646")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1386200.lua
+--Gamename Sex Lesson
+addappid(1386200) --Mainappid Sex Lesson
+addappid(1386201, 1, "7a12b7bd57d0887f826269ac0ae5fec429c3607da86e017f3736cf361e4b31f0") --Main Windows Depot Sex Lesson
+setManifestid(1386201, "5229329378059509646", 96428560)
