1546e49f4dab4c65582bbcd94e9981dc2f827b70
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:23:24 +0800

    add 1042800.lua

--- 1042800.lua
+++ 1042800.lua
@@ -1,5 +1,11 @@
-addappid(1042800)
-addappid(1042801,0,"1e5d9df64c85c7259e5764bc91970185aae825bae7e8bc0ad41b029e2a140782")
-setManifestid(1042801,"5110347786538620700")
-setManifestid(527431,"2687237330542327071")
-setManifestid(527432,"6474134563617630190")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1042800.lua
+--Gamename Warhammer 40,000: Inquisitor - Prophecy
+addappid(1042800) --Mainappid Warhammer 40,000: Inquisitor - Prophecy
+addappid(1042801, 1, "1e5d9df64c85c7259e5764bc91970185aae825bae7e8bc0ad41b029e2a140782") --Main Windows Depot Warhammer 40,000: Inquisitor - Prophecy
+setManifestid(1042801, "5110347786538620700", 145216)
+--Share Depots
+addappid(527431, 1, "a22368d0a73cc74b4ba8445b12620a5f2662822d0fec23a38cbd5afedfe3d44d") --Share Windows Depot Warhammer 40,000: Inquisitor - Martyr
+setManifestid(527431, "2687237330542327071", 30448271520)
+addappid(527432, 1, "e72027c8b7ab04a562b16333a23afd70dd1b1d6151dcadcabe70b7d5b886b34b") --Share Windows Depot Warhammer 40,000: Inquisitor - Martyr
+setManifestid(527432, "6474134563617630190", 98652432)
