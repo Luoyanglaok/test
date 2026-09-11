9789b3a3cc0a95abe98f15fc142b54cbf035acaa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:25:30 +0800

    add 3072120.lua

---
+++ 3072120.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3072120.lua
+--Gamename Backrooms Lost Runners
+addappid(3072120, 1, "94a301ef9c246527844d967f115d3893979e61b054507f366bef2e97adc00a72") --Mainappid Backrooms Lost Runners
+addappid(3072121, 1, "3893e74f509714a02402663b2218789ed8bd7f0e3c3575fff0685903934394c8") --Main Windows Depot Backrooms Lost Runners
+setManifestid(3072121, "6848901854606390279", 3623089760)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
