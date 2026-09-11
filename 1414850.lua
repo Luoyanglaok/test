41fd6ae2f90c611eae75895459515902d6528652
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:18:37 +0800

    add 1414850.lua

--- 1414850.lua
+++ 1414850.lua
@@ -1,8 +1,29 @@
-addappid(1414850)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1414851,0,"53e7f3936af23e3ded4233db79608e2ed462e5ee21cda975efd9925d88210af3")
-setManifestid(1414851,"1138761226996891155")
-addappid(1414852)
-addappid(1414853,0,"f4705a3c9653ce0c59bfee78c7f817e574367b44c1878a1d3feeec8b161f494d")
-setManifestid(1414853,"1201417681265517313")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1414850.lua
+--Gamename Nickelodeon All-Star Brawl
+addappid(1414850, 1, "b5d2f808d451ef891fdc0e1df44ec7ca264c5e62431b6d1fb71d2b157b94c44c") --Mainappid Nickelodeon All-Star Brawl
+addappid(1414851, 1, "53e7f3936af23e3ded4233db79608e2ed462e5ee21cda975efd9925d88210af3") --Main Windows Depot Nickelodeon All-Star Brawl
+setManifestid(1414851, "1138761226996891155", 4514516272)
+addappid(1414853, 1, "f4705a3c9653ce0c59bfee78c7f817e574367b44c1878a1d3feeec8b161f494d") --Main Windows Depot Nickelodeon All-Star Brawl
+setManifestid(1414853, "1201417681265517313", 4513466400)
+--Dlcname Nickelodeon All-Star Brawl Soundtrack
+addappid(1786560) --Dlcname Nickelodeon All-Star Brawl Soundtrack
+addappid(1786561, 1, "d330f521527bec8affbb4fbd04292479a1d064f513330142c3d2c3f1bd2507e6") --Dlc Windows Depot Nickelodeon All-Star Brawl Soundtrack
+setManifestid(1786561, "70000836029065208", 55499376)
+addappid(1786562, 1, "7afdfb1db201ab66920800e0ee23e7afdd7ffdb62e5d61a69ef6a4965be292e6") --Dlc Windows Depot Nickelodeon All-Star Brawl Soundtrack
+setManifestid(1786562, "3505183013279251929", 533010352)
+--Dlcname Nickelodeon All-Star Brawl - Jenny Brawler Pack
+addappid(1944980, 1, "f6628ee429cdf1918d0025901b2e6ed14d2342611bfc754faa1ff26bff58d36b") --Dlcname Nickelodeon All-Star Brawl - Jenny Brawler Pack
+setManifestid(1944980, "476324730900976908", 1088)
+--Dlcname Nickelodeon All-Star Brawl - Universe Pack
+addappid(1946940, 1, "e0ce221d03b8cd6506f746983bb3efc9c5da87a34230bf7ac17ababdeb6ceaef") --Dlcname Nickelodeon All-Star Brawl - Universe Pack
+setManifestid(1946940, "1658123263440757119", 1088)
+--Dlcname Nickelodeon All-Star Brawl - Hugh Neutron Brawler Pack
+addappid(1967790, 1, "4456fb88af61c6e7de66d357e1e47a4aff6faf491b779110fbbae688f4d23ec0") --Dlcname Nickelodeon All-Star Brawl - Hugh Neutron Brawler Pack
+setManifestid(1967790, "1154266019984040849", 1088)
+--Dlcname Nickelodeon All-Star Brawl - Rocko Brawler Pack
+addappid(1967791, 1, "57d2329935af8661ee58824010bbd2321ea463b7ff5a12c88ebcb39ba6570160") --Dlcname Nickelodeon All-Star Brawl - Rocko Brawler Pack
+setManifestid(1967791, "6273449517690274043", 1088)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
