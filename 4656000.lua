bff66f7f4879e83f054d2cbd422439250c39eb16
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:28:56 +0800

    add 4656000.lua

---
+++ 4656000.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4656000.lua
+--Gamename BOMBANANA!
+addappid(4656000) --Mainappid BOMBANANA!
+addappid(4656001, 1, "d4ca035dd638ad12e68cdacffbe742c7c3200ca84ddaf9ef21dfdbcbb1ab83fc") --Main Macos Depot BOMBANANA!
+setManifestid(4656001, "5626122186073031522", 349189200)
+addappid(4656002, 1, "4253533257321c05573a2a4681e7bc10885e1338e0dc46bfe93b9a08863c9346") --Main Windows Depot BOMBANANA!
+setManifestid(4656002, "9159914856202370713", 284389760)
