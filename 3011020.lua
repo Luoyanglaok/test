437e2e8792f285b12cd1a5a2dda6a4df82141233
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 11:38:56 +0800

    add 3011020.lua

--- 3011020.lua
+++ 3011020.lua
@@ -1,3 +1,6 @@
-addappid(3011020)
-addappid(3011021,0,"3328538be6b9ce98c1ea16559db904e9665226dce38b2ff010f382be6517fa91")
-setManifestid(3011021,"99879118041311081")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3011020.lua
+--Gamename Dreamless
+addappid(3011020) --Mainappid Dreamless
+addappid(3011021, 1, "3328538be6b9ce98c1ea16559db904e9665226dce38b2ff010f382be6517fa91") --Main Windows Depot Dreamless
+setManifestid(3011021, "8466246577334120372", 4291679648)
