54cc2ef23490e36dbfc8d64ed41cb75d56028f63
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:59:08 +0800

    add 1571820.lua

--- 1571820.lua
+++ 1571820.lua
@@ -1,3 +1,6 @@
-addappid(1571820)
-addappid(1571821,0,"2377df26b7c9fa43a0cc970a08b1f87cac98193aa3c49ec12502b24472ae6207")
-setManifestid(1571821,"8131016762618998511")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1571820.lua
+--Gamename Life With College Girl
+addappid(1571820, 1, "30cfc2787f9ef5462fa8206cc378a35260b42f625771d6d85ce04f5ec733ed65") --Mainappid Life With College Girl
+addappid(1571821, 1, "2377df26b7c9fa43a0cc970a08b1f87cac98193aa3c49ec12502b24472ae6207") --Main Windows Depot Life With College Girl
+setManifestid(1571821, "8131016762618998511", 598219792)
