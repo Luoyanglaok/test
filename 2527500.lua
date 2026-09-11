92ed968ed58a184f86f38e6d8e4f864b40734829
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:27:19 +0800

    add 2527500.lua

--- 2527500.lua
+++ 2527500.lua
@@ -4,3 +4,9 @@
 addappid(2527500, 1, "319708828feb58fa2ed02fb00975af201afbd79269c7f91a3a5e69efcf51f4c0") --Mainappid MiSide
 addappid(2527501, 1, "ffef2d1f42cd2438f5b7d029d771a9f941fd224250c3184f595773fb4926e1cd") --Main Windows Depot MiSide
 setManifestid(2527501, "5992599505980992417", 2205630880)
+--Dlcname MiSide Soundtrack
+addappid(3404450) --Dlcname MiSide Soundtrack
+addappid(3404451, 1, "401cfca91341369711b16540df763140e99621c2331d8ebfc736045c290dbf98") --Dlc Windows Depot MiSide Soundtrack
+setManifestid(3404451, "6733290632493676009", 70953952)
+addappid(3404452, 1, "272b49fcc2141cc4a67c20d84a8acb7ac1b1673a7c51af65b60615490d9c419f") --Dlc Windows Depot MiSide Soundtrack
+setManifestid(3404452, "9044251031765134423", 330904288)
