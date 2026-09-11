2b798be6ddc80a957adf04ffa7b914e2ef226c2f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:15:55 +0800

    add 2530470.lua

--- 2530470.lua
+++ 2530470.lua
@@ -1,12 +1,2 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 2530470.lua
---Gamename Garden of Witches
-addappid(2530470) --Mainappid Garden of Witches
-addappid(2530471, 1, "a978c4dc2cbf61036edb2e812dc4d23ab3ef9162446c75c0dd2bd87f384c49af") --Main Windows Depot Garden of Witches
-setManifestid(2530471, "3489760729689456126", 996542288)
---Dlcname Garden of Witches Soundtrack
-addappid(3624950) --Dlcname Garden of Witches Soundtrack
-addappid(3624951, 1, "2fa12b55190f7c23817d3be03f766e81a91a2334e5b0466688591321a652584d") --Dlc Windows Depot Garden of Witches Soundtrack
-setManifestid(3624951, "5638383876616489211", 105984288)
-addappid(3624952, 1, "9192993f77e9ea4539b2e38bcaf68e2fc986cefabe3ab3d2a43b1e909fa1fe4d") --Dlc Windows Depot Garden of Witches Soundtrack
-setManifestid(3624952, "6109329788469528677", 698710864)
+addappid(2530470)
+addappid(3624950)
