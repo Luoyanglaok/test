143239b305a47e6dcae1c3eec0b2b7a25391ef81
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:34:42 +0800

    add 5027520.lua

---
+++ 5027520.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5027520.lua
+--Gamename Megastore: Tidy Up Together
+addappid(5027520) --Mainappid Megastore: Tidy Up Together
+addappid(5027521, 1, "0b4bece51fa05789c4172e6faed43d360d0db528622312bc27b424ae4d608651") --Main Windows Depot Megastore: Tidy Up Together
+setManifestid(5027521, "4802343023408455639", 2663703376)
