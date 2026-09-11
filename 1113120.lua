2ae399620c9966d04e9e9a9af6d57599a5f56584
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:07:09 +0800

    add 1113120.lua

--- 1113120.lua
+++ 1113120.lua
@@ -1,7 +1,15 @@
-addappid(1113120)
-addappid(1113121,0,"fa3f3b4a6b929572b908f7a0558a4055471173f4ca30ac3a068c4e0a89096414")
-setManifestid(1113121,"6706069456693339826")
-addappid(1113122)
-addappid(1113123)
-addappid(2115950,0,"9f890ff771b770c3b90cfff818b2597ae05d9a74f59b518bd95ec854ad2a2ae5")
-setManifestid(2115950,"3393214506738470140")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1113120.lua
+--Gamename IXION
+addappid(1113120) --Mainappid IXION
+addappid(1113121, 1, "fa3f3b4a6b929572b908f7a0558a4055471173f4ca30ac3a068c4e0a89096414") --Main Windows Depot IXION
+setManifestid(1113121, "6706069456693339826", 3600660864)
+--Dlcname IXION - Original Soundtrack
+addappid(2115940) --Dlcname IXION - Original Soundtrack
+addappid(2115942, 1, "b25abec17b242fcbaa1ffe8b74199862019e2204421e75c49bfee2913b57616a") --Dlc Windows Depot IXION - Original Soundtrack
+setManifestid(2115942, "2080889467947827316", 1503047600)
+addappid(2115943, 1, "dcea2ae4f6e9e6783c8f0c020bca24fa73634b488d0c0eba49d44b12a2438843") --Dlc Windows Depot IXION - Original Soundtrack
+setManifestid(2115943, "9006275125470353768", 970610784)
+--Dlcname IXION - Artbook
+addappid(2115950, 1, "9f890ff771b770c3b90cfff818b2597ae05d9a74f59b518bd95ec854ad2a2ae5") --Dlcname IXION - Artbook
+setManifestid(2115950, "3393214506738470140", 140989216)
