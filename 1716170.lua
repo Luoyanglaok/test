6169c7ced5f15c15a66c2fc13b911af817bb9a99
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:13:57 +0800

    add 1716170.lua

--- 1716170.lua
+++ 1716170.lua
@@ -1,3 +1,10 @@
-addappid(1716170)
-addappid(1716171,0,"1dd4f58a4443ba289d01dbe06d1f6c8807164c577cd812c69751e60b16cf08d0")
-setManifestid(1716171,"5912990837898814522")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1716170.lua
+--Gamename Zoey: My Hentai Sex Doll
+addappid(1716170) --Mainappid Zoey: My Hentai Sex Doll
+addappid(1716171, 1, "1dd4f58a4443ba289d01dbe06d1f6c8807164c577cd812c69751e60b16cf08d0") --Main Windows Depot Zoey: My Hentai Sex Doll
+setManifestid(1716171, "5912990837898814522", 410431776)
+addappid(2138950) --Dlcname Zoey: My Hentai Sex Doll - Fellatio Fun DLC
+addappid(2138951) --Dlcname Zoey: My Hentai Sex Doll - Futa Potion DLC
+addappid(2138952) --Dlcname Zoey: My Hentai Sex Doll - Deep Cleaning DLC
+addappid(2195420) --Dlcname Zoey: My Hentai Sex Doll - Magic Paizuri
