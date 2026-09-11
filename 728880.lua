8d5fc3f636ebbde8b246eb64a00cc04963f679f5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:40:44 +0800

    add 728880.lua

--- 728880.lua
+++ 728880.lua
@@ -1,7 +1,16 @@
-addappid(728880)
-addappid(728881,0,"6ab9f32746295a38fc0a75db089afc2394e0ced25ca436201027ed360890e5bc")
-setManifestid(728881,"8562236934291228162")
-addappid(728882,0,"3693ed13535f104258a7425e20b9e1fae3f30ec4d78a09f0b89cd55350991d51")
-setManifestid(728882,"433550811082520169")
-addappid(728883,0,"67ac3a681ded025499180d68f3753ce53f4934976c75af86ab9a6287056dcdae")
-setManifestid(728883,"604448138896078557")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 728880.lua
+--Gamename Overcooked! 2
+addappid(728880, 1, "21a370467ff28d51b2ca206260688bfaf12b6a03ff409a21f1cf173bb0c44cb7") --Mainappid Overcooked! 2
+addappid(728881, 1, "6ab9f32746295a38fc0a75db089afc2394e0ced25ca436201027ed360890e5bc") --Main Windows Depot Overcooked! 2
+setManifestid(728881, "9029823169690910983", 6246352800)
+addappid(728882, 1, "3693ed13535f104258a7425e20b9e1fae3f30ec4d78a09f0b89cd55350991d51") --Main Linux Depot Overcooked! 2
+setManifestid(728882, "433550811082520169", 6382912672)
+addappid(728883, 1, "67ac3a681ded025499180d68f3753ce53f4934976c75af86ab9a6287056dcdae") --Main Macos Depot Overcooked! 2
+setManifestid(728883, "8876331237899748277", 6269071600)
+addappid(858240) --Dlcname Overcooked! 2 - Too Many Cooks Pack
+addappid(909720) --Dlcname Overcooked! 2 - Surf 'n' Turf
+addappid(1013300) --Dlcname Overcooked! 2 - Campfire Cook Off
+addappid(1017510) --Dlcname Overcooked! 2 - Night of the Hangry Horde
+addappid(1067770) --Dlcname Overcooked! 2 - Season Pass
+addappid(1138400) --Dlcname Overcooked! 2 - Carnival of Chaos
