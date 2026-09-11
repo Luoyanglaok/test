78321adb4861c50b6926abbade78db24193c16d1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:52:10 +0800

    add 1543030.lua

--- 1543030.lua
+++ 1543030.lua
@@ -1,5 +1,21 @@
-addappid(1543030)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1543031,0,"80c82f692c1dccb0e5de735f6b7513dc62421d6f9bfdf44af13992f5de4367cd")
-setManifestid(1543031,"1328293710815739891")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1543030.lua
+--Gamename Sword and Fairy 7
+addappid(1543030) --Mainappid Sword and Fairy 7
+addappid(1543031, 1, "80c82f692c1dccb0e5de735f6b7513dc62421d6f9bfdf44af13992f5de4367cd") --Main Windows Depot Sword and Fairy 7
+setManifestid(1543031, "1328293710815739891", 32049930080)
+--Dlcname Sword and Fairy 7 Soundtrack
+addappid(1867890) --Dlcname Sword and Fairy 7 Soundtrack
+addappid(1867891, 1, "3f301b765cbacde2590bec1ef5a1d1b51108d13dd5c1b29ba09b562fcd6b995e") --Dlc Windows Depot Sword and Fairy 7 Soundtrack
+setManifestid(1867891, "2059192692005900934", 488335200)
+addappid(1867892, 1, "6b32f373adbc55bf32d79707d497ac1c450c7c74737173b0c501a507fdab9060") --Dlc Windows Depot Sword and Fairy 7 Soundtrack
+setManifestid(1867892, "7849036732852795147", 2599124144)
+--Dlcname Sword and Fairy 7 - Dreamlike World Expansion
+addappid(2223240) --Dlcname Sword and Fairy 7 - Dreamlike World Expansion
+addappid(2223241, 1, "c7d3ba2342020f382cb7e6c7f202bbb7a136283e87870d04190562e15966f398") --Dlc Windows Depot Sword and Fairy 7 - Dreamlike World Expansion
+setManifestid(2223241, "8900201496308069518", 12677404816)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
