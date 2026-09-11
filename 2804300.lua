174a9f75c5d61634e3a97c8417393d073553fd8a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:24:35 +0800

    add 2804300.lua

--- 2804300.lua
+++ 2804300.lua
@@ -1,3 +1,6 @@
-addappid(2804300)
-addappid(2804301,0,"bbf20d63fa0571a5ce945f65ec497dc2cdf90af792170f5db7b654bf70b7f1f5")
-setManifestid(2804301,"6903759024988443581")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2804300.lua
+--Gamename Sex with a Vampire 🧛♂❤
+addappid(2804300) --Mainappid Sex with a Vampire 🧛♂❤
+addappid(2804301, 1, "bbf20d63fa0571a5ce945f65ec497dc2cdf90af792170f5db7b654bf70b7f1f5") --Main Windows/Linux Depot Sex with a Vampire 🧛♂❤
+setManifestid(2804301, "6903759024988443581", 301592928)
