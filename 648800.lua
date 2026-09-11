f4e6d8c9059871fc2086fcfa883ffdc735ef618f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 04:09:10 +0800

    add 648800.lua

--- 648800.lua
+++ 648800.lua
@@ -1,4 +1,9 @@
-addappid(648800)
-setManifestid(228990,"1829726630299308803")
-addappid(648801,0,"69b674ffb72cbe77ce081b29070056279accc29956cad5d38d31e0d3e2090632")
-setManifestid(648801,"577422474383592005")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 648800.lua
+--Gamename Raft
+addappid(648800, 1, "fcc79fc7fbc5d5fefdb2fc4b0bffef695b803edc0612e656861e6b362f2869ca") --Mainappid Raft
+addappid(648801, 1, "69b674ffb72cbe77ce081b29070056279accc29956cad5d38d31e0d3e2090632") --Main Windows Depot Raft
+setManifestid(648801, "4830832075480360897", 2241578592)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
