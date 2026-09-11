e6213dbd90cd180e1ee6b9fa6f22f5b09ec53dfb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:02:09 +0800

    add 1448440.lua

--- 1448440.lua
+++ 1448440.lua
@@ -1,8 +1,24 @@
-addappid(1448440)
-setManifestid(228988,"6645201662696499616")
-addappid(1448441,0,"ef3b4eaabfb5d6bd17666007dda635b99375509180b267fbba485e2df18b4ed0")
-setManifestid(1448441,"5746039873445988019")
-addappid(1448442,0,"4e508b0bc29665862ed41a6e37c28fe63fc758dca12297e0237f28b2df4b2fd7")
-setManifestid(1448442,"1386628775558457295")
-addappid(2232890,0,"c9d303abb2711b7678880b74da95738ae983c74990967e0607993544a12c5087")
-setManifestid(2232890,"4304578856547169949")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1448440.lua
+--Gamename Wo Long: Fallen Dynasty
+addappid(1448440, 1, "eb976df4cb1dde3afdefb3d24ff6485e769d904cbfdc5fa5a4b58de7435bb817") --Mainappid Wo Long: Fallen Dynasty
+addappid(1448441, 1, "ef3b4eaabfb5d6bd17666007dda635b99375509180b267fbba485e2df18b4ed0") --Main Windows Depot Wo Long: Fallen Dynasty
+setManifestid(1448441, "537498482973662688", 51684712016)
+addappid(1448442, 1, "4e508b0bc29665862ed41a6e37c28fe63fc758dca12297e0237f28b2df4b2fd7") --Main Windows Depot Wo Long: Fallen Dynasty
+setManifestid(1448442, "4349854675463499286", 111130032)
+--Dlcname Wo Long: Fallen Dynasty Digital Art Book & Digital Mini Soundtrack
+addappid(2232890, 1, "c9d303abb2711b7678880b74da95738ae983c74990967e0607993544a12c5087") --Dlcname Wo Long: Fallen Dynasty Digital Art Book & Digital Mini Soundtrack
+setManifestid(2232890, "4304578856547169949", 5534798800)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(2205890) --Dlcname Wo Long: Fallen Dynasty Baihu Armor
+addappid(2205891) --Dlcname Wo Long: Fallen Dynasty Zhuque Armor
+addappid(2205892) --Dlcname Wo Long: Fallen Dynasty Qinglong Armor
+addappid(2205893) --Dlcname Wo Long: Fallen Dynasty Season Pass
+addappid(2205894) --Dlcname Wo Long: Fallen Dynasty Battle of Zhongyuan
+addappid(2205895) --Dlcname Wo Long: Fallen Dynasty Conqueror of Jiangdong
+addappid(2205896) --Dlcname Wo Long: Fallen Dynasty Upheaval in Jingxiang
+addappid(2612470) --Dlcname Wo Long: Fallen Dynasty Indigo Dual Swords
+addappid(2612490) --Dlcname Wo Long: Fallen Dynasty Indigo Garment Set
+addappid(2654720) --Dlcname Wo Long: Fallen Dynasty Records of the Grand Historian
