2e3164e14b33819d317c86e4404911ab830b87eb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:24:32 +0800

    add 921570.lua

--- 921570.lua
+++ 921570.lua
@@ -1,7 +1,15 @@
-addappid(921570)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(921571,0,"d68c52ab730016257e72ad98554e2a7d8cb6131790cc4490fff16873b73132b9")
-setManifestid(921571,"5896441090656338684")
-addappid(1083770,0,"233d3fdec4d0f87dbcbd2a2d77bb6d277ac034050c67ef93f7930a1952816eaf")
-setManifestid(1083770,"639595591076631777")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 921570.lua
+--Gamename OCTOPATH TRAVELER™
+addappid(921570) --Mainappid OCTOPATH TRAVELER™
+addappid(921571, 1, "d68c52ab730016257e72ad98554e2a7d8cb6131790cc4490fff16873b73132b9") --Main Windows Depot OCTOPATH TRAVELER™
+setManifestid(921571, "5896441090656338684", 3063594528)
+--Dlcname AppID 1083770
+addtoken(1083770, "5048866756202704070")
+addappid(1083770, 1, "233d3fdec4d0f87dbcbd2a2d77bb6d277ac034050c67ef93f7930a1952816eaf") --Dlcname AppID 1083770
+setManifestid(1083770, "639595591076631777", 184458800)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
