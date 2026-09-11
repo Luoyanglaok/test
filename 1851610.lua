e7f9f57f24e6daea7519c3f4ae066ca4fcf909b2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:41:02 +0800

    add 1851610.lua

--- 1851610.lua
+++ 1851610.lua
@@ -1,3 +1,13 @@
-addappid(1851610)
-addappid(1851611,0,"0433750bab63572df54610d462657fb0ac98292cccb61c9198384c77954148d8")
-setManifestid(1851611,"8255106886576059186")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1851610.lua
+--Gamename River Tails: Stronger Together
+addappid(1851610) --Mainappid River Tails: Stronger Together
+addappid(1851611, 1, "0433750bab63572df54610d462657fb0ac98292cccb61c9198384c77954148d8") --Main Windows Depot River Tails: Stronger Together
+setManifestid(1851611, "4477222858874794596", 2796695968)
+addappid(1851613, 1, "9B83C5B5B0AEFB40E1ABB34B072EEC66890416104923810E49F061639CD8A494") --Main Windows Depot River Tails: Stronger Together
+setManifestid(1851613, "2910388989396821359", 2791723312)
+--Dlcname River Tails: Stronger Together - The Art of River Tails: Stronger Together
+addappid(2691690, 1, "be7f80a49422e4e05e6f8e299812759a908823727f088e9772ce77180659c752") --Dlcname River Tails: Stronger Together - The Art of River Tails: Stronger Together
+setManifestid(2691690, "703931705990089991", 336351008)
+--Missing Dlcs
+--Dlcname River Tails: Stronger Together - Original Soundtrack
