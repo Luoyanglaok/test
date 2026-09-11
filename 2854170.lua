f4d16ad1bf162b76bb9992aae18e2e01a31ee297
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:50:57 +0800

    add 2854170.lua

--- 2854170.lua
+++ 2854170.lua
@@ -1,8 +1,18 @@
-addappid(2854170)
-setManifestid(228990,"1829726630299308803")
-addappid(2854171,0,"6b7d17a5dce248ea62e596854cc58b84d88914c3a82c711f8236161ffaae0d4b")
-setManifestid(2854171,"9030982401366557913")
-addappid(2854172,0,"4da70b5bbc7d3eb5633f9168b9a90f203dbf04ed23703ccfe0eaa6777aaa86a6")
-setManifestid(2854172,"1151403447120079937")
-addappid(2854173,0,"7b7aa932798149b7bb19fcc23494eaf9e3e48194eb34fbe53b48fb653a75ed2b")
-setManifestid(2854173,"1927113972884324546")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2854170.lua
+--Gamename OPPAI Succubus Academy Sucky and Busty, Demonic and Lusty!
+addappid(2854170, 1, "62efb946d762949a6636717473fd0e5f9f44b61bc9f13b0805c6994373bbf1bb") --Mainappid OPPAI Succubus Academy Sucky and Busty, Demonic and Lusty!
+addappid(2854171, 1, "6b7d17a5dce248ea62e596854cc58b84d88914c3a82c711f8236161ffaae0d4b") --Main Windows Depot OPPAI Succubus Academy Sucky and Busty, Demonic and Lusty!
+setManifestid(2854171, "9030982401366557913", 7513778336)
+addappid(2854172, 1, "4da70b5bbc7d3eb5633f9168b9a90f203dbf04ed23703ccfe0eaa6777aaa86a6") --Main Windows Depot OPPAI Succubus Academy Sucky and Busty, Demonic and Lusty!
+setManifestid(2854172, "1151403447120079937", 72640)
+addappid(2854173, 1, "7b7aa932798149b7bb19fcc23494eaf9e3e48194eb34fbe53b48fb653a75ed2b") --Main Windows Depot OPPAI Succubus Academy Sucky and Busty, Demonic and Lusty!
+setManifestid(2854173, "1735514639827656095", 5250752)
+--Dlcname OPPAI Succubus Academy Sucky and Busty, Demonic and Lusty! Special Mosaic Removal DLC
+addappid(2998620, 1, "3be4ee99b429788778e1b2ec3729397ef5af709bf583b70deb9c92a175925ebe") --Dlcname OPPAI Succubus Academy Sucky and Busty, Demonic and Lusty! Special Mosaic Removal DLC
+setManifestid(2998620, "2151973514726607372", 4847692160)
+addappid(2998621, 1, "c2eed429e1f9ac6f55005790e365c2238b0dfaf432a147c2d3dd39d4d2342859") --Dlc Windows Depot OPPAI Succubus Academy Sucky and Busty, Demonic and Lusty! Special Mosaic Removal DLC
+setManifestid(2998621, "3360276239077028568", 2768192)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
