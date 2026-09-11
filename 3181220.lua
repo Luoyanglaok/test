d84486cd8a3c609105eb140122fe342d71ded356
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:38:48 +0800

    add 3181220.lua

---
+++ 3181220.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3181220.lua
+--Gamename AV Director Life!
+addappid(3181220, 1, "85434bba5eb3fd06d1e51affc0ee05bea485cb3ab812319b91c7c2b949796105") --Mainappid AV Director Life!
+addappid(3181221, 1, "4568d834b5706f7dbb46e9c2962f9b2fd643d8709ebda0e428c84b83d7757356") --Main Windows Language Depot Japanese AV Director Life!
+setManifestid(3181221, "3471053361802884981", 3591112736)
+addappid(3181222, 1, "5961bc414641c16c702484d3f4cf3766cf313190006888cd9b8e1896f174c2f0") --Main Windows Depot AV Director Life!
+setManifestid(3181222, "6258463783727347341", 3601965104)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
