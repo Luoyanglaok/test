e107a6c19786f3a55a41d66ff7ff139321c003c9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:54:50 +0800

    add 2185060.lua

--- 2185060.lua
+++ 2185060.lua
@@ -1,22 +1,53 @@
-addappid(2185060)
-setManifestid(228989,"3514306556860204959")
-addappid(2185061,0,"a98101165ba3b99b3d98c5866360aa7cb08eeffd31ec95a9d024a824ff57c792")
-setManifestid(2185061,"7779277124017584250")
-addappid(2185062,0,"50e06e5a50d397f8af97494176ecfb9f75bb4082443bbe1fb111f44715c18d61")
-setManifestid(2185062,"3539297810650017225")
-addappid(2185063,0,"e68d0e65f0ced4a31e4932edecd8ff65ad2f1b503bff1b0e74faa05ae4d5356c")
-setManifestid(2185063,"2488791012006590353")
-addappid(2999841,0,"17c8f80c3b69386aa95d085d8d9acf78ded50c7d234c3fe760843b1854e57d5d")
-setManifestid(2999841,"5449128149226694238")
-addappid(2999842,0,"c17024c21617ad2a2c48ab1ef147fa5631840f4a8e92735f4ceddc9bcf4aa2cb")
-setManifestid(2999842,"8927889988331454670")
-addappid(2999843,0,"e9ef0ab3c1526da46248fa2d6b362fe47d752025d6737c04217e276330a0f57f")
-setManifestid(2999843,"7534205379407041785")
-addappid(2999851,0,"08a857d0c27299f066279413cf450e11262163e9fdd33ed8146402834a75a318")
-setManifestid(2999851,"8098462697722779185")
-addappid(2999852,0,"6a30c41e89a00e6dccd56cde848599d00d6f98edb86f68d8ad3ca11a7772fbb7")
-setManifestid(2999852,"4740776227837110299")
-addappid(2999853,0,"02e1edb27de1b992bbac282fe43c00b39aced6b5b1ff1e6dce02a711d64adbe4")
-setManifestid(2999853,"985337764462346047")
-addappid(3592601,0,"8fc6c6248dc796e6fd5cc6834560bebbe05cd43fca1209ce06cd28d42a3421b1")
-setManifestid(3592601,"6851228627857910475")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2185060.lua
+--Gamename Two Point Museum
+addappid(2185060, 1, "2340a2fa4b9bb44e786eb62e5866d2f8b2b4c5d73c0173273064b5a6424c5d1d") --Mainappid Two Point Museum
+addappid(2185061, 1, "a98101165ba3b99b3d98c5866360aa7cb08eeffd31ec95a9d024a824ff57c792") --Main Windows Depot Two Point Museum
+setManifestid(2185061, "8733114788911726434", 4832103888)
+addappid(2185062, 1, "50e06e5a50d397f8af97494176ecfb9f75bb4082443bbe1fb111f44715c18d61") --Main Macos Depot Two Point Museum
+setManifestid(2185062, "3582170520357180116", 4718272160)
+addappid(2185063, 1, "e68d0e65f0ced4a31e4932edecd8ff65ad2f1b503bff1b0e74faa05ae4d5356c") --Main Linux Depot Two Point Museum
+setManifestid(2185063, "1139392003525860915", 4783657856)
+--Dlcname Two Point Museum: Sonic Pre-Purchase Pack
+addappid(2999840) --Dlcname Two Point Museum: Sonic Pre-Purchase Pack
+addappid(2999841, 1, "17c8f80c3b69386aa95d085d8d9acf78ded50c7d234c3fe760843b1854e57d5d") --Dlc Windows Depot Two Point Museum: Sonic Pre-Purchase Pack
+setManifestid(2999841, "4056095540777983282", 47070160)
+addappid(2999842, 1, "c17024c21617ad2a2c48ab1ef147fa5631840f4a8e92735f4ceddc9bcf4aa2cb") --Dlc Macos Depot Two Point Museum: Sonic Pre-Purchase Pack
+setManifestid(2999842, "1630215263507499927", 47453952)
+addappid(2999843, 1, "e9ef0ab3c1526da46248fa2d6b362fe47d752025d6737c04217e276330a0f57f") --Dlc Linux Depot Two Point Museum: Sonic Pre-Purchase Pack
+setManifestid(2999843, "7898887487327155824", 47045888)
+--Dlcname Two Point Museum: Explorer Upgrade Pack
+addappid(2999850) --Dlcname Two Point Museum: Explorer Upgrade Pack
+addappid(2999851, 1, "08a857d0c27299f066279413cf450e11262163e9fdd33ed8146402834a75a318") --Dlc Windows Depot Two Point Museum: Explorer Upgrade Pack
+setManifestid(2999851, "9204244846157769899", 21155776)
+addappid(2999852, 1, "6a30c41e89a00e6dccd56cde848599d00d6f98edb86f68d8ad3ca11a7772fbb7") --Dlc Macos Depot Two Point Museum: Explorer Upgrade Pack
+setManifestid(2999852, "8132875642203899476", 21706624)
+addappid(2999853, 1, "02e1edb27de1b992bbac282fe43c00b39aced6b5b1ff1e6dce02a711d64adbe4") --Dlc Linux Depot Two Point Museum: Explorer Upgrade Pack
+setManifestid(2999853, "4203121068208814481", 21150080)
+--Dlcname Two Point Museum: Fantasy Finds
+addappid(3592600) --Dlcname Two Point Museum: Fantasy Finds
+addappid(3592601, 1, "8fc6c6248dc796e6fd5cc6834560bebbe05cd43fca1209ce06cd28d42a3421b1") --Dlc Windows Depot Two Point Museum: Fantasy Finds
+setManifestid(3592601, "7129815126137922867", 109630064)
+addappid(3592602, 1, "f176a616fceffd4977533324e8cbecec75de625ac741be1563f35bc159674382") --Dlc Macos Depot Two Point Museum: Fantasy Finds
+setManifestid(3592602, "3390468595923474015", 116563504)
+addappid(3592603, 1, "d0e5329d3415a09f040f4c36085ad1ecbe427e72f8505095a0798f4d0068b0e9") --Dlc Linux Depot Two Point Museum: Fantasy Finds
+setManifestid(3592603, "5076644500685787158", 109641008)
+--Dlcname Two Point Museum: Zooseum
+addappid(3804850) --Dlcname Two Point Museum: Zooseum
+addappid(3804851, 1, "245b7148e9c97035fcc19d3bb6286994fe4bbfec32c17a6803f915ce8704d421") --Dlc Windows Depot Two Point Museum: Zooseum
+setManifestid(3804851, "3265263831790862078", 107167936)
+addappid(3804852, 1, "a583d14bddd9f6fc992424ce67bf9b15784a5f4955af01eb3cd14f80a12d606d") --Dlc Macos Depot Two Point Museum: Zooseum
+setManifestid(3804852, "1647911251712906912", 112835552)
+addappid(3804853, 1, "1e6e361d4b7c2b23303f99e11c058d5e82c3112aa3f0111e1ae7966c40e0070b") --Dlc Linux Depot Two Point Museum: Zooseum
+setManifestid(3804853, "7482442005166367503", 107194480)
+--Dlcname Two Point Museum: Arty-Facts
+addappid(4114070) --Dlcname Two Point Museum: Arty-Facts
+addappid(4114071, 1, "a1d52a9452a4435e6c078aee4682ae820a960536c98fa626d6e5e61893c378c8") --Dlc Windows Depot Two Point Museum: Arty-Facts
+setManifestid(4114071, "7641393193992402875", 45534096)
+addappid(4114072, 1, "b1870e3cbcf57e05514e6f01a40edd9ec1d180a6d845c3688f2c308f13727389") --Dlc Macos Depot Two Point Museum: Arty-Facts
+setManifestid(4114072, "2434779693430300563", 46150944)
+addappid(4114073, 1, "327160d1fe32acf1828908f9ba8a7bd8093de255be16c02f2f41186f5b95884b") --Dlc Linux Depot Two Point Museum: Arty-Facts
+setManifestid(4114073, "7962553413517880080", 45530560)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
