7fa2ecbe2915f95178196a0944d62bfbbda353ce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:21:55 +0800

    add 750130.lua

--- 750130.lua
+++ 750130.lua
@@ -1,14 +1,34 @@
-addappid(750130)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-addappid(750133,0,"da21083938e32867b6b9190a5f7ae54c1b716b6d74df27516a589f9f0cb9d4f5")
-setManifestid(750133,"6058490855972658563")
-addappid(750131,0,"47a72920d4c72af7e8407a617881be6fa81aaaf4f4334c27bc6ce0844890add5")
-setManifestid(750131,"7624002377498776081")
-addappid(750132,0,"33dc940870d31a3fff549c22816fa9a2170b5a3c87d60ea2d4d66565fa1520b7")
-setManifestid(750132,"3304446528767190259")
-addappid(750134,0,"b261bbaefbb88b1aa01e939cd69c807fa2d63383f8971c96a563b816db403e92")
-setManifestid(750134,"5466644149342743634")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 750130.lua
+--Gamename The Sinking City Remastered
+addappid(750130, 1, "de30cb51d7a0076dfe262223a6b58c37814b42c5a578dccbd12f813a4780a9ad") --Mainappid The Sinking City Remastered
+addappid(750131, 1, "47a72920d4c72af7e8407a617881be6fa81aaaf4f4334c27bc6ce0844890add5") --Main Windows Depot The Sinking City Remastered
+setManifestid(750131, "7624002377498776081", 15663584)
+addappid(750132, 1, "33dc940870d31a3fff549c22816fa9a2170b5a3c87d60ea2d4d66565fa1520b7") --Main Windows Depot The Sinking City Remastered
+setManifestid(750132, "3304446528767190259", 18887561856)
+addappid(750133, 1, "da21083938e32867b6b9190a5f7ae54c1b716b6d74df27516a589f9f0cb9d4f5") --Main Windows Depot The Sinking City Remastered
+setManifestid(750133, "6058490855972658563", 408976)
+addappid(750134, 1, "b261bbaefbb88b1aa01e939cd69c807fa2d63383f8971c96a563b816db403e92") --Main Windows Depot The Sinking City Remastered
+setManifestid(750134, "5466644149342743634", 31179498944)
+--Dlcname The Sinking City Artbook & OST Bundle
+addappid(2019880) --Dlcname The Sinking City Artbook & OST Bundle
+addappid(2019881, 1, "ab7a748712f283b854e2ceaf6e5ee1edad8099175ab0cfc5d88c8ebb20c2938e") --Dlc Windows Depot The Sinking City Artbook & OST Bundle
+setManifestid(2019881, "5175849185549774047", 1019301104)
+addappid(2019882, 1, "a19e5fd0cadb0dd470aa93678bc0599b4312cbf32767b66243d599b461d24dad") --Dlc Windows Depot The Sinking City Artbook & OST Bundle
+setManifestid(2019882, "8599911425468607295", 250550384)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(887080) --Dlcname The Sinking City Remastered - Investigator Pack
+addappid(975180) --Dlcname The Sinking City Remastered - Worshippers of the Necronomicon
+addappid(990470) --Dlcname The Sinking City Remastered - Chicago Organ Grinder
+addappid(990620) --Dlcname The Sinking City Remastered - Experience Boost
+addappid(2765310) --Dlcname The Sinking City Remastered - Merciful Madness
