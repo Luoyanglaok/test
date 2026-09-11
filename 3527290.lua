d9a44c23d9139d3d73b002017773bb54c24ee545
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:05:01 +0800

    add 3527290.lua

--- 3527290.lua
+++ 3527290.lua
@@ -1,3 +1,6 @@
-addappid(3527290)
-addappid(3527291,0,"f55585bb6a00ba332421256f80ba05e27afc38fec6170b9a763138244c506f05")
-setManifestid(3527291,"548744499804961861")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3527290.lua
+--Gamename PEAK
+addappid(3527290, 1, "499eb049b24a30730d5926e9cf72bfa9237d1a7f6de06e94a05e9c2c01348478") --Mainappid PEAK
+addappid(3527291, 1, "f55585bb6a00ba332421256f80ba05e27afc38fec6170b9a763138244c506f05") --Main Windows Depot PEAK
+setManifestid(3527291, "269859723287817937", 1559371008)
