e165654131e18c17d73b5ad0c9c213e7f4767a7b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:01:18 +0800

    add 3998800.lua

---
+++ 3998800.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3998800.lua
+--Gamename 恋爱吗？我家猫会后空翻
+addappid(3998800) --Mainappid 恋爱吗？我家猫会后空翻
+addappid(3998801, 1, "f338475c0e275d289d04ec416d7ab6b6c5127619d5ceae3022bdeef6eef2a2bb") --Main Windows Language Depot Schinese 恋爱吗？我家猫会后空翻
+setManifestid(3998801, "2031126748492586048", 26555896624)
+--Dlcname 恋爱吗？我家猫会后空翻 - 4K资产包
+addappid(4223320, 1, "ce4ec9d433975e3b8557f8a016315342c36def7d126ed860c41c3ac280de06d3") --Dlcname 恋爱吗？我家猫会后空翻 - 4K资产包
+setManifestid(4223320, "5952934530510481780", 80415958176)
