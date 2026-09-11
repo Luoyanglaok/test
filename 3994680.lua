29aaa3329a2b04690dd7b995f2057f46550995b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:26:33 +0800

    add 3994680.lua

---
+++ 3994680.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3994680.lua
+--Gamename Meat Grinder: Hotdog Simulator
+addappid(3994680) --Mainappid Meat Grinder: Hotdog Simulator
+addappid(3994681, 1, "f15d02c88c4e81f4bc4c3daeab2c9ec445d7406d71c9c247fd6a3daee32f4a0f") --Main Windows Depot Meat Grinder: Hotdog Simulator
+setManifestid(3994681, "6269995567672920898", 158956416)
