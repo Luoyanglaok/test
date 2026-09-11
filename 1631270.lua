1aca50978ee9719b5f852cf60700e8b6f5bb8fa9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:48:15 +0800

    add 1631270.lua

---
+++ 1631270.lua
@@ -0,0 +1,22 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1631270.lua
+--Gamename StarRupture
+addappid(1631270, 1, "58df0d0c7d2375066687753516dd994c4171bd5e33a9d304dc72bceea96e1bf0") --Mainappid StarRupture
+addappid(1631272, 1, "cbb99a831607efa2d9529979e09ca1e3b1692bf978684330d6db62a558c30b52") --Main Windows Depot StarRupture
+setManifestid(1631272, "5950584203207177574", 16221509600)
+--Dlcname StarRupture - Original Soundtrack
+addappid(3795340) --Dlcname StarRupture - Original Soundtrack
+addappid(3795342, 1, "dbc2c1f0943b674c0fdda26f332f24ce8ca1e98366cc2316f823c513d35a1d79") --Dlc Windows Depot StarRupture - Original Soundtrack
+setManifestid(3795342, "5174713112577954211", 148354112)
+addappid(3795343, 1, "a5978b176505cad67d83f0c2aaaec56b84635c3c1a16188ba727c1e9d0ab8684") --Dlc Windows Depot StarRupture - Original Soundtrack
+setManifestid(3795343, "1255235654127924330", 394050176)
+addappid(3795344, 1, "1ff48a80653c31a9e9417f44ed5340c6c6494f08209de9bf74c8a25798ab2ef6") --Dlc Windows Depot StarRupture - Original Soundtrack
+setManifestid(3795344, "8010805355589142150", 618403280)
+--Dlcname StarRupture - Supporter Pack
+addappid(4167250, 1, "5ea58a4526ce19f7edf2448015e940043fc2da5d61a837f34dfdf2e2966a2f8c") --Dlcname StarRupture - Supporter Pack
+setManifestid(4167250, "9015653026638990134", 1513598352)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
