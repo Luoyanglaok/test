5acac37bf8597f7b39ba02ea876f3553f12e2d42
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:01:15 +0800

    add 2246340.lua

--- 2246340.lua
+++ 2246340.lua
@@ -7,6 +7,10 @@ setManifestid(2246341, "4844075769269645714", 109384001824)
 --Dlcname Monster Hunter Wilds - High Resolution Texture Pack
 addappid(3308900, 1, "86e9ff2249c193631e8e04855d9c6154071b06491dced87279c1c72b1662556d") --Dlcname Monster Hunter Wilds - High Resolution Texture Pack
 setManifestid(3308900, "2311240512518968412", 31415547440)
+--Dlcname Monster Hunter Wilds Original Soundtrack
+addappid(3885660) --Dlcname Monster Hunter Wilds Original Soundtrack
+addappid(3885661, 1, "25073952ac95672044fee8fda7b6157f0f00d418f666e53243ace246cd9b0850") --Dlc Windows Depot Monster Hunter Wilds Original Soundtrack
+setManifestid(3885661, "2085525327031658274", 838950720)
 --Share Depots
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228988, "6645201662696499616", 22411856)
@@ -76,3 +80,144 @@ addappid(3289760) --Dlcname Monster Hunter Wilds - Makeup/Face Paint: Flames
 addappid(3289770) --Dlcname Monster Hunter Wilds - Pendant: Strigible: Amstrigian
 addappid(3289780) --Dlcname Monster Hunter Wilds - Pendant: Strigible: Hymstrigian
 addappid(3289790) --Dlcname Monster Hunter Wilds - Pendant: Strigible: Cypurrpunk
+addappid(3289800) --Dlcname Monster Hunter Wilds - Pendant: Strigible: Gold
+addappid(3289810) --Dlcname Monster Hunter Wilds - Pendant: Strigible: Love
+addappid(3289820) --Dlcname Monster Hunter Wilds - Pendant: Strigible: Color
+addappid(3289830) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 4
+addappid(3289840) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 5
+addappid(3289850) --Dlcname Monster Hunter Wilds - Sticker Set: Whimsical Wudwuds
+addappid(3289860) --Dlcname Monster Hunter Wilds - Hairstyle: Gemma
+addappid(3289870) --Dlcname Monster Hunter Wilds - Hairstyle: Alma
+addappid(3289880) --Dlcname Monster Hunter Wilds - Hairstyle: Nata
+addappid(3289890) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 6
+addappid(3289900) --Dlcname Monster Hunter Wilds - Alma Outfit: Summer Poncho
+addappid(3289910) --Dlcname Monster Hunter Wilds - Seikret Decoration: Phoenix Caparison
+addappid(3289920) --Dlcname Monster Hunter Wilds - Pendant: Red Flamefete Spirit
+addappid(3289930) --Dlcname Monster Hunter Wilds - Pendant: Blue Flamefete Spirit
+addappid(3289940) --Dlcname Monster Hunter Wilds - Pendant: Violet Flamefete Spirit
+addappid(3289950) --Dlcname Monster Hunter Wilds - Pendant: Green Flamefete Spirit
+addappid(3289960) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 7
+addappid(3289980) --Dlcname Monster Hunter Wilds - Sticker Set: Flamefete
+addappid(3336850) --Dlcname Monster Hunter Wilds - Palico Edit Voucher: Three-Voucher Pack
+addappid(3336860) --Dlcname Monster Hunter Wilds - Palico Edit Voucher: Single Voucher (Free Trial Version)
+addappid(3336890) --Dlcname Monster Hunter Wilds - Character & Palico Edit Voucher: Three-Voucher Pack
+addappid(3336910) --Dlcname Monster Hunter Wilds - Pendant: Felyne Mint Chocolate
+addappid(3336920) --Dlcname Monster Hunter Wilds - Pendant: Felyne Strawberry
+addappid(3336930) --Dlcname Monster Hunter Wilds - Pendant: Felyne Caramel
+addappid(3360450) --Dlcname Monster Hunter Wilds - Pendant: Dancing White Cat
+addappid(3360460) --Dlcname Monster Hunter Wilds - Pendant: Stretched White Cat
+addappid(3360470) --Dlcname Monster Hunter Wilds - Pendant: Dancing Black Cat
+addappid(3360480) --Dlcname Monster Hunter Wilds - Pendant: Stretched Black Cat
+addappid(3360490) --Dlcname Monster Hunter Wilds - Alma Outfit: Chun-Li Outfit - SF6
+addappid(3360500) --Dlcname Monster Hunter Wilds - Alma Outfit: Cammy Outfit - SF6
+addappid(3360510) --Dlcname Monster Hunter Wilds - Gesture: SF6 Hadoken
+addappid(3360520) --Dlcname Monster Hunter Wilds - Gesture: SF6 Shoryuken
+addappid(3360530) --Dlcname Monster Hunter Wilds - Sticker Set: Street Fighter 6 Set
+addappid(3360540) --Dlcname Monster Hunter Wilds - Gemma Outfit: Summer Coveralls
+addappid(3360550) --Dlcname Monster Hunter Wilds - Erik Outfit: Summer Hat
+addappid(3410840) --Dlcname Monster Hunter Wilds - Hunter Help: Welcome Pack
+addappid(3410850) --Dlcname Monster Hunter Wilds - Pendant: Blanka-Chan Doll
+addappid(3502660) --Dlcname Monster Hunter Wilds - Gesture: SF6 Tatsumaki Senpu-kyaku
+addappid(3619120) --Dlcname Monster Hunter Wilds - Pendant: Fluttering Quatreflies
+addappid(3619130) --Dlcname Monster Hunter Wilds - Pendant: Trot-along Crake
+addappid(3619140) --Dlcname Monster Hunter Wilds - Pendant: Floating Hearts
+addappid(3619150) --Dlcname Monster Hunter Wilds - Pendant: Floating Doodles
+addappid(3619160) --Dlcname Monster Hunter Wilds - Pendant: Floating Sparkles
+addappid(3619170) --Dlcname Monster Hunter Wilds - Pendant: Floating Potions
+addappid(3619180) --Dlcname Monster Hunter Wilds - Pendant: Floating Well-done Steaks
+addappid(3619190) --Dlcname Monster Hunter Wilds - Pendant: Floating Honey
+addappid(3619200) --Dlcname Monster Hunter Wilds - Pendant: Pink Flamefete Spirit
+addappid(3619210) --Dlcname Monster Hunter Wilds - Pendant: White Flamefete Spirit
+addappid(3619220) --Dlcname Monster Hunter Wilds - Pendant: Lagiacrus Plushie
+addappid(3696220) --Dlcname Monster Hunter Wilds - Hunter Layered Armor: Fluffy Ears
+addappid(3696230) --Dlcname Monster Hunter Wilds - Hunter Layered Armor: Fluffy Tail
+addappid(3696240) --Dlcname Monster Hunter Wilds - Seikret Decoration: Cute Critter Caparison
+addappid(3696250) --Dlcname Monster Hunter Wilds - Quest Completion Animation: Handler Set
+addappid(3696260) --Dlcname Monster Hunter Wilds - Quest Completion Animation: Smithy Set
+addappid(3696270) --Dlcname Monster Hunter Wilds - Felyne Layered Armor Set: Felyne Gawkie
+addappid(3696280) --Dlcname Monster Hunter Wilds - Pendant: Red Lala Dress Doll
+addappid(3696290) --Dlcname Monster Hunter Wilds - Pendant: Blue Lala Dress Doll
+addappid(3696300) --Dlcname Monster Hunter Wilds - Pendant: Pink Lala Dress Doll
+addappid(3696310) --Dlcname Monster Hunter Wilds - Tent Customization Set Vol. 2
+addappid(3696340) --Dlcname Monster Hunter Wilds - Camp Gear Set Vol. 2
+addappid(3696350) --Dlcname Monster Hunter Wilds - BGM Set Vol. 2
+addappid(3696360) --Dlcname Monster Hunter Wilds - Sticker Set: Endearing Endemic Life
+addappid(3696370) --Dlcname Monster Hunter Wilds - Alma Outfit: Autumn Witch
+addappid(3696380) --Dlcname Monster Hunter Wilds - Erik Outfit: Autumn Therian
+addappid(3696390) --Dlcname Monster Hunter Wilds - Seikret Decoration: Spooky Doll Caparison
+addappid(3696400) --Dlcname Monster Hunter Wilds - Gesture: Hocus Palicosus
+addappid(3696420) --Dlcname Monster Hunter Wilds - Gesture: Flying Broomstick
+addappid(3696430) --Dlcname Monster Hunter Wilds - Pendant: White Poltercat
+addappid(3696440) --Dlcname Monster Hunter Wilds - Pendant: Orange Poltercat
+addappid(3696450) --Dlcname Monster Hunter Wilds - Pendant: Violet Poltercat
+addappid(3696460) --Dlcname Monster Hunter Wilds - Pendant: Blue Poltercat
+addappid(3696470) --Dlcname Monster Hunter Wilds - Sticker Set: Pillopod Pathos
+addappid(3696480) --Dlcname Monster Hunter Wilds - Camp Gear Set Vol. 3
+addappid(3696490) --Dlcname Monster Hunter Wilds - Pendant: Rolling Poogie: Two-tone
+addappid(3696500) --Dlcname Monster Hunter Wilds - Pendant: Rolling Poogie: Monochrome
+addappid(3696510) --Dlcname Monster Hunter Wilds - Pendant: Rolling Poogie: Takoyaki
+addappid(3696610) --Dlcname Monster Hunter Wilds - Hunter Layered Armor Set: Cosmoloid
+addappid(3696620) --Dlcname Monster Hunter Wilds - Seikret Decoration: Machina Caparison
+addappid(3696630) --Dlcname Monster Hunter Wilds - Layered Sword & Shield: Cosmoloid Saber
+addappid(3696640) --Dlcname Monster Hunter Wilds - Layered Great Sword: Cosmoloid Blade
+addappid(3696750) --Dlcname Monster Hunter Wilds - Layered Long Sword: Cosmoloid Edge
+addappid(3696760) --Dlcname Monster Hunter Wilds - Layered Dual Blades: Cosmoloid Wings
+addappid(3696770) --Dlcname Monster Hunter Wilds - Layered Lance: Cosmoloid Piercer
+addappid(3696780) --Dlcname Monster Hunter Wilds - Layered Gunlance: Cosmoloid Burst
+addappid(3696790) --Dlcname Monster Hunter Wilds - Layered Switch Axe: Cosmoloid Axe
+addappid(3696800) --Dlcname Monster Hunter Wilds - Layered Charge Blade: Cosmoloid Arms
+addappid(3696810) --Dlcname Monster Hunter Wilds - Layered Hammer: Cosmoloid Collapse
+addappid(3696820) --Dlcname Monster Hunter Wilds - Layered Insect Glaive: Cosmoloid Glaive
+addappid(3696830) --Dlcname Monster Hunter Wilds - Layered Hunting Horn: Cosmoloid Gjallar
+addappid(3696840) --Dlcname Monster Hunter Wilds - Layered Light Bowgun: Cosmoloid Shooter
+addappid(3696850) --Dlcname Monster Hunter Wilds - Layered Heavy Bowgun: Cosmoloid Buster
+addappid(3696860) --Dlcname Monster Hunter Wilds - Layered Bow: Cosmoloid Arrow
+addappid(3696870) --Dlcname Monster Hunter Wilds - Pendant: Cosmoloid Core
+addappid(3696890) --Dlcname Monster Hunter Wilds - Photo Pose: Seikret Battle-Ready Pose
+addappid(3696900) --Dlcname Monster Hunter Wilds - Sticker Set: Everyday Eastlands
+addappid(3696910) --Dlcname Monster Hunter Wilds - Hairstyle: Olivia
+addappid(3696920) --Dlcname Monster Hunter Wilds - Hairstyle: Erik
+addappid(3696930) --Dlcname Monster Hunter Wilds - Hairstyle: Werner
+addappid(3696940) --Dlcname Monster Hunter Wilds - Pendant: White Rock-a-Baby
+addappid(3696950) --Dlcname Monster Hunter Wilds - Pendant: Pink Rock-a-Baby
+addappid(3696960) --Dlcname Monster Hunter Wilds - Pendant: Blue Rock-a-Baby
+addappid(3696970) --Dlcname Monster Hunter Wilds - Pendant: Green Rock-a-Baby
+addappid(3696980) --Dlcname Monster Hunter Wilds - Alma Outfit: Featherskirt Seikret Dress
+addappid(3696990) --Dlcname Monster Hunter Wilds - Erik Outfit: Crestcollar Seikret Suit
+addappid(3697010) --Dlcname Monster Hunter Wilds - Gemma Outfit: Redveil Seikret Dress
+addappid(3697020) --Dlcname Monster Hunter Wilds - Seikret Decoration: Whitewing Caparison
+addappid(3697030) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 9
+addappid(3697050) --Dlcname Monster Hunter Wilds - Sticker Set: Avis Unit Greetings
+addappid(3697060) --Dlcname Monster Hunter Wilds - Pendant: Rolling Poogie: Seasonal
+addappid(3747820) --Dlcname Monster Hunter Wilds - Pendant: Felyne Duna Bobble
+addappid(3747830) --Dlcname Monster Hunter Wilds - Pendant: Felyne Duna γ Bobble
+addappid(3747840) --Dlcname Monster Hunter Wilds - Pendant: Fluttering Myriads
+addappid(3747850) --Dlcname Monster Hunter Wilds - Gesture: Resting Seikret
+addappid(3747860) --Dlcname Monster Hunter Wilds - Gesture: Tea Time
+addappid(3767510) --Dlcname Monster Hunter Wilds - Pendant: Orange Lala Dress Doll
+addappid(3767520) --Dlcname Monster Hunter Wilds - Pendant: Violet Lala Dress Doll
+addappid(3767530) --Dlcname Monster Hunter Wilds - Pendant: Green Poltercat
+addappid(3767550) --Dlcname Monster Hunter Wilds - Pendant: KFC Colonel Felyne
+addappid(3767560) --Dlcname Monster Hunter Wilds - Pendant: KFC Bucket Felyne
+addappid(3767570) --Dlcname Monster Hunter Wilds - Pendant: Happy-Go-Congakart
+addappid(3767580) --Dlcname Monster Hunter Wilds - Felyne Layered Armor Set: Azure Fantasy Wudwud
+addappid(3772970) --Dlcname Monster Hunter Wilds - Pendant: Red Poltercat
+addappid(3801040) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 8
+addappid(3932400) --Dlcname Monster Hunter Wilds - Pendant: Trot-along Fledgling
+addappid(3932410) --Dlcname Monster Hunter Wilds - Pendant: White Turntail
+addappid(3932420) --Dlcname Monster Hunter Wilds - Pendant: White Tubby Turntail
+addappid(3932430) --Dlcname Monster Hunter Wilds - Pendant: Black Turntail
+addappid(3932440) --Dlcname Monster Hunter Wilds - Pendant: Black Tubby Turntail
+addappid(3932450) --Dlcname Monster Hunter Wilds - Pendant: Floating Rath Balloon
+addappid(3932460) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 10
+addappid(3932490) --Dlcname Monster Hunter Wilds - Pendant: Purple Rock-a-Baby
+addappid(3932500) --Dlcname Monster Hunter Wilds - Pendant: Black Rock-a-Baby
+addappid(3959310) --Dlcname Monster Hunter Wilds - Pendant: F Hope Balloon
+addappid(3959320) --Dlcname Monster Hunter Wilds - Pendant: F Arkveld Balloon
+addappid(3959330) --Dlcname Monster Hunter Wilds - Pendant: F Rompopolo Balloon
+addappid(3959340) --Dlcname Monster Hunter Wilds - Pendant: F Barina Balloon
+addappid(3959350) --Dlcname Monster Hunter Wilds - Pendant: Athos Balloon
+addappid(3968920) --Dlcname Monster Hunter Wilds - Character & Palico Edit Voucher: Single Voucher (Free Gift Version)
+addappid(3968930) --Dlcname Monster Hunter Wilds - Character & Palico Edit Voucher: Single Voucher (Free Gift Version 2)
+addappid(4123270) --Dlcname Monster Hunter Wilds - Pendant: Rolling Poogie: Sakura
+addappid(4829700) --Dlcname Monster Hunter Wilds: Ascendance
