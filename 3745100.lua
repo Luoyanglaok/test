1cf2069d01afdd94755cad722e0d42afdf59dc10
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:50:29 +0800

    add 3745100.lua

--- 3745100.lua
+++ 3745100.lua
@@ -1,3 +1,6 @@
-addappid(3745100)
-addappid(3745101,0,"01bb3b56f157d8262f6ce99cf1eea5e3ff866cf9e3ab0f81f7b1ad965ee69572")
-setManifestid(3745101,"4511105023215181145")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3745100.lua
+--Gamename Mirai's Midnight Stream Soundtrack
+addappid(3745100) --Mainappid Mirai's Midnight Stream Soundtrack
+addappid(3745101, 1, "01bb3b56f157d8262f6ce99cf1eea5e3ff866cf9e3ab0f81f7b1ad965ee69572") --Main Windows Depot Mirai's Midnight Stream Soundtrack
+setManifestid(3745101, "4511105023215181145", 153361984)
