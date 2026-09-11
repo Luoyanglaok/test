11ab11a80ea5fadac58d1895a2946ff86e818d62
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:46:15 +0800

    add 1426210.lua

--- 1426210.lua
+++ 1426210.lua
@@ -1,7 +1,14 @@
-addappid(1426210)
-addappid(4110940)
-addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(1426211,"5290425664756095500")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1426210.lua
+--Gamename It Takes Two
+addappid(1426210, 1, "ec7aca2b989b0dad7b2f7880268887d67d9656bfd0f47146e9cad7d411785250") --Mainappid It Takes Two
+addappid(1426211, 1, "c450c44e032e27d0308cf864259fb2af91bcfc58401a0caf33bb8efba85bb323") --Main Windows Depot It Takes Two
+setManifestid(1426211, "5290425664756095500", 47190674656)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1426390) --Dlcname It Takes Two – Full Game Unlock
+--Missing Dlcs
+--Dlcname It Takes Two - Original Soundtrack
