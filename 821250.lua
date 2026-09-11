7aa7516f3e32eb09330d67a5230ab15b8a9628ec
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:26:41 +0800

    add 821250.lua

--- 821250.lua
+++ 821250.lua
@@ -1,7 +1,4 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 821250.lua
---Gamename Flotsam
-addappid(821250, 1, "87e07f81bf85fe07b7e5ea224272ebf3050a7e5c6c41b1b73e9e928bdc1ab0c9") --Mainappid Flotsam
-addappid(821251, 1, "56d2732aafc44c77401e0dfc97a9374d19b16610b85b9446aadcbaccdfaf7c20") --Main Windows Depot Flotsam
-setManifestid(821251, "2343332648804177982", 575084240)
-addappid(4661240) --Dlcname Support Pack
+addappid(821250)
+addappid(4661240)
+addappid(821251,1,"56d2732aafc44c77401e0dfc97a9374d19b16610b85b9446aadcbaccdfaf7c20")
+setManifestid(821251,"2343332648804177982")
