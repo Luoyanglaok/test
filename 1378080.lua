0442c05a2977a23be8e34fc8745d18ebc2b39232
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 12:58:07 +0800

    add 1378080.lua

--- 1378080.lua
+++ 1378080.lua
@@ -1,3 +1,6 @@
-addappid(1378080)
-addappid(1378081,0,"193a905e272eb0c0e70ff9925ee4bdde7b580e7921dda5745b9bcc3ba8325c8f")
-setManifestid(1378081,"5187865168340687852")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1378080.lua
+--Gamename Sex Lady Sonia
+addappid(1378080) --Mainappid Sex Lady Sonia
+addappid(1378081, 1, "193a905e272eb0c0e70ff9925ee4bdde7b580e7921dda5745b9bcc3ba8325c8f") --Main Windows Depot Sex Lady Sonia
+setManifestid(1378081, "5187865168340687852", 955936304)
