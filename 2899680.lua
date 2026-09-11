01ae971352d46d296b5b4bfdaab52188c92ae41e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:06:33 +0800

    add 2899680.lua

---
+++ 2899680.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2899680.lua
+--Gamename Island Of Enchantment
+addappid(2899680, 1, "437f3141edd8c01766f93186586b6e3f43807c9b618bc2c848bff4837d128373") --Mainappid Island Of Enchantment
+addappid(2899681, 1, "cbd4ba1aefb8d1c285946ac969136fe0a51c282edc2e251afcdd8c607da9a59e") --Main Windows Depot Island Of Enchantment
+setManifestid(2899681, "4532322964017171580", 12544948032)
+--Dlcname Island Of Enchantment - Mystery DLC
+addappid(4298120, 1, "a30e64ba3bb3db2ba72fe984d0791fe35818186bb60efea7c064bcdbd3379bf6") --Dlcname Island Of Enchantment - Mystery DLC
+setManifestid(4298120, "6463958859787498540", 1810640832)
