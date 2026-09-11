437e0e6f87e3db3d23910ee11824c40eebe45657
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:14:44 +0800

    add 4003800.lua

---
+++ 4003800.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4003800.lua
+--Gamename PRAGMATA SKETCHBOOK - DEMO
+addappid(4003800) --Mainappid PRAGMATA SKETCHBOOK - DEMO
+addappid(4003801, 1, "cd288076d00d8bc408af69b86e2774a1e56d16ba9416c36b22f28b9d206e6d3d") --Main Windows Depot PRAGMATA SKETCHBOOK - DEMO
+setManifestid(4003801, "1363403386003317303", 7415008512)
+addappid(4003802, 1, "aeb4ac23c3505b8375f53b136770e7f5576d335d3f3d9f89757d6b0a4b4bb904") --Main Windows Depot PRAGMATA SKETCHBOOK - DEMO
+setManifestid(4003802, "4670877062663028430", 287819648)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
