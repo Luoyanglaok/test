ced61f03375ba9c656bcd9680a831abfb4aefa8f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:40:36 +0800

    add 2374190.lua

--- 2374190.lua
+++ 2374190.lua
@@ -1,5 +1,13 @@
-addappid(2374190)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2374191,0,"6824d28cc3d5b903cd58d0828d1d25ecf7062324b07b5aba906dbf36b9a7a1e7")
-setManifestid(2374191,"1995635572448571857")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2374190.lua
+--Gamename Nikoderiko: The Magical World — Director’s Cut
+addappid(2374190) --Mainappid Nikoderiko: The Magical World — Director’s Cut
+addappid(2374191, 1, "6824d28cc3d5b903cd58d0828d1d25ecf7062324b07b5aba906dbf36b9a7a1e7") --Main Windows Depot Nikoderiko: The Magical World — Director’s Cut
+setManifestid(2374191, "1995635572448571857", 12409989264)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Nikoderiko: The Magical World — Director’s Cut Soundtrack + Artbook
