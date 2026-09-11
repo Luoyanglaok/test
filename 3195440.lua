0fe575f1d2ed56d23793154339aa6de296cebf5d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:36:10 +0800

    add 3195440.lua

---
+++ 3195440.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3195440.lua
+--Gamename Welcome to Elderfield
+addappid(3195440, 1, "b7089da309310d2ec5694fe51cdf1e6cb458e1da14203c10d9666c1238ca9602") --Mainappid Welcome to Elderfield
+addappid(3195441, 1, "1ab0aa8318b70bbf603af7110d658bc0cb4de84b5cc932c6ad937f3019712873") --Main Windows Depot Welcome to Elderfield
+setManifestid(3195441, "953612272409548332", 1001362544)
+addappid(5145771, 1, "5f0c409d53eed32beab8d04a962fbd782a9972eec429bac79e83f903f1faa1b6") --Main Windows Depot Welcome to Elderfield
+setManifestid(5145771, "2458095708455477064", 180285408)
+--Dlcname Welcome to Elderfield Soundtrack
+addappid(5145770) --Dlcname Welcome to Elderfield Soundtrack
