446d81523e0f1fa8924b11caddfaf2d3a4872420
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:12:28 +0800

    add 1683950.lua

--- 1683950.lua
+++ 1683950.lua
@@ -1,3 +1,6 @@
-addappid(1683950)
-addappid(1683951,0,"664a5b9c37d8b87b89370b1be1ea7396753738e0778532926ee2dd8388115fb1")
-setManifestid(1683951,"9054306269633740043")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1683950.lua
+--Gamename The Witch's Sexual Prison
+addappid(1683950, 1, "3e8e1b5b6b6c5b71ff37445f2f9ba0ff437862379d8582b780e4d0c707356cae") --Mainappid The Witch's Sexual Prison
+addappid(1683951, 1, "664a5b9c37d8b87b89370b1be1ea7396753738e0778532926ee2dd8388115fb1") --Main Windows Depot The Witch's Sexual Prison
+setManifestid(1683951, "9054306269633740043", 340705024)
