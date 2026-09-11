6de0c3c8f16fcd455fdb71553a812940d45edfce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:01:41 +0800

    add 2741360.lua

--- 2741360.lua
+++ 2741360.lua
@@ -1,7 +1,14 @@
-addappid(2741360)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(2741361,0,"bdf619a0696d63c3f117ca0ee19d9cafd50b5f42396506f674aa947b728c8d57")
-setManifestid(2741361,"7060065011170823408")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2741360.lua
+--Gamename Valiant Hearts: Coming Home
+addappid(2741360) --Mainappid Valiant Hearts: Coming Home
+addappid(2741361, 1, "bdf619a0696d63c3f117ca0ee19d9cafd50b5f42396506f674aa947b728c8d57") --Main Windows Depot Valiant Hearts: Coming Home
+setManifestid(2741361, "7060065011170823408", 1526710208)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(2741370) --Dlcname Valiant Hearts: Coming Home Ubisoft Activation
