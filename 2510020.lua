c9dd16b77507a515e0028cf0c500c72db93d2a3c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 11:45:58 +0800

    add 2510020.lua

---
+++ 2510020.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2510020.lua
+--Gamename Placid Plastic Deck - A Quiet Quest
+addappid(2510020, 1, "f46bee50e7a7a5a14295d0164796b2a081638f8adaecb2a5419b3c754f63c585") --Mainappid Placid Plastic Deck - A Quiet Quest
+addappid(2510021, 1, "d96c6ab15d92dd3f659a46627554a6861b02406ba995033a9613981c09b94bda") --Main Windows Depot Placid Plastic Deck - A Quiet Quest
+setManifestid(2510021, "6284003859173922651", 1063912480)
