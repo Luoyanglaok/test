9680bf1c2af5a1ac7cd7146038fb20d6fcac0765
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:09:58 +0800

    add 2273430.lua

--- 2273430.lua
+++ 2273430.lua
@@ -10,6 +10,24 @@ setManifestid(2273432, "9153299841968279599", 14090728032)
 --Dlcname BlazBlue Entropy Effect - Launch Exclusive Content
 addappid(2771130, 1, "9252ee34ddef99b9842cf9b953a265b23f119aac46977475999deac055a2aa24") --Dlcname BlazBlue Entropy Effect - Launch Exclusive Content
 setManifestid(2771130, "3519795565238480803", 959557664)
+--Dlcname 苍翼：混沌效应 Soundtrack - A
+addappid(2771140) --Dlcname 苍翼：混沌效应 Soundtrack - A
+addappid(2771141, 1, "4ea322c9278e847cc773c832e891c5d181c97706fe6327de5dcb50376c1be9c1") --Dlc Windows Depot 苍翼：混沌效应 Soundtrack - A
+setManifestid(2771141, "5359888352168889722", 167486432)
+addappid(2771142, 1, "f79ddf0111ff5d536aff589dfda6390b06a13d28f116f9eae97bc36fac3f8a6a") --Dlc Windows Depot 苍翼：混沌效应 Soundtrack - A
+setManifestid(2771142, "7333669365228745470", 927484832)
+--Dlcname 苍翼：混沌效应 Soundtrack - B
+addappid(2872920) --Dlcname 苍翼：混沌效应 Soundtrack - B
+addappid(2872921, 1, "1d9b78c5d9de65e82553135803e1141a63c04e20028de8376ec9398ef67ae727") --Dlc Windows Depot 苍翼：混沌效应 Soundtrack - B
+setManifestid(2872921, "1140987421356250847", 160583824)
+addappid(2872922, 1, "46574a10735fbe8a7d678a380cdab1dd5618849d50fc82f3273df2cc86e9f88c") --Dlc Windows Depot 苍翼：混沌效应 Soundtrack - B
+setManifestid(2872922, "2184427327520918167", 757559760)
+--Dlcname BlazBlue Entropy Effect - Soundtrack C
+addappid(4372020) --Dlcname BlazBlue Entropy Effect - Soundtrack C
+addappid(4372021, 1, "44d71eb126cc41c7552730a4cf953683e327b39e0b6786b3e6ba59da23e14f94") --Dlc Windows Depot BlazBlue Entropy Effect - Soundtrack C
+setManifestid(4372021, "3828367568886025592", 90845424)
+addappid(4372022, 1, "5da00964d20bafd2aa6b823e097356277a14122f8f5b2ede194315b22e2e4c60") --Dlc Windows Depot BlazBlue Entropy Effect - Soundtrack C
+setManifestid(4372022, "2174479067396996711", 656741344)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
