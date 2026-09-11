e6d3f76e0888b5e461723d7ae59cab770ef41e91
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:00:16 +0800

    add 2922020.lua

--- 2922020.lua
+++ 2922020.lua
@@ -1,3 +1,6 @@
-addappid(2922020)
-addappid(2922021,0,"81ff12ac23baf7f0be63af101da9da0e931ef9bb1225a5f01f45a081842df2be")
-setManifestid(2922021,"5292510264088698530")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2922020.lua
+--Gamename Sex Ghoul Fling🧟❤
+addappid(2922020) --Mainappid Sex Ghoul Fling🧟❤
+addappid(2922021, 1, "81ff12ac23baf7f0be63af101da9da0e931ef9bb1225a5f01f45a081842df2be") --Main Windows Depot Sex Ghoul Fling🧟❤
+setManifestid(2922021, "5292510264088698530", 409677168)
