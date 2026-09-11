3e7e1346ed03887e187efd34490bea1c85e680a9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:33:50 +0800

    add 1602000.lua

--- 1602000.lua
+++ 1602000.lua
@@ -1,5 +1,11 @@
-addappid(1602000)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1602001,0,"0eae087f0db9be78952d7fae0ef01ab55a34cc7d8bae7f18ccc3379d89476fe7")
-setManifestid(1602001,"5910110795845710736")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1602000.lua
+--Gamename Hotel Architect
+addappid(1602000, 1, "a17e0c50e72b96a8be98c8d7702bb79628b78e3d9532b1cc3bf7b34949259636") --Mainappid Hotel Architect
+addappid(1602001, 1, "0eae087f0db9be78952d7fae0ef01ab55a34cc7d8bae7f18ccc3379d89476fe7") --Main Windows Depot Hotel Architect
+setManifestid(1602001, "5737379820367698382", 1200575408)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
