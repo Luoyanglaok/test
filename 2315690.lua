52fd615f6998bd7b041f55c7faf4aba83156d7a4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:34:14 +0800

    add 2315690.lua

--- 2315690.lua
+++ 2315690.lua
@@ -1,5 +1,21 @@
-addappid(2315690)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(2315691,0,"23e097a5dcfd505419d23c66434881e466ae025183332849a90469800dd3d074")
-setManifestid(2315691,"1030431831166830840")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2315690.lua
+--Gamename WWE 2K24
+addappid(2315690, 1, "cacfb2414682e48897adc63a602cd8e7c52a2cc811fc7d1608aab6bf1608ab7e") --Mainappid WWE 2K24
+addappid(2315691, 1, "23e097a5dcfd505419d23c66434881e466ae025183332849a90469800dd3d074") --Main Windows Depot WWE 2K24
+setManifestid(2315691, "1030431831166830840", 99961311408)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(2528460) --Dlcname WWE 2K24 ECW Punk Pack
+addappid(2544390) --Dlcname WWE 2K24 SuperCharger
+addappid(2584660) --Dlcname WWE 2K24 MyRISE Mega-Boost
+addappid(2768160) --Dlcname WWE 2K24 40 years of WrestleMania Pack
+addappid(2768170) --Dlcname WWE 2K24 Nightmare Family Pack
+addappid(2768180) --Dlcname WWE 2K24 Deluxe Content Pack
+addappid(2768190) --Dlcname WWE 2K24 Season Pass
+addappid(2855610) --Dlcname WWE 2K24 Post Malone & Friends Pack
+addappid(2855620) --Dlcname WWE 2K24 Pat McAfee Show Pack
+addappid(2855630) --Dlcname WWE 2K24 Global Superstars Pack
+addappid(2855640) --Dlcname WWE 2K24 WCW Pack
+addappid(3180050) --Dlcname WWE 2K24 Bray Wyatt Edition Pack
