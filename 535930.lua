7348728111330e34b750468c909b5b8fecd6e2e6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 01:42:29 +0800

    add 535930.lua

--- 535930.lua
+++ 535930.lua
@@ -1,9 +1,30 @@
-addappid(535930)
-addappid(535931,0,"038f6bc45efe9b8bb282557291689a07867e95b7088cfcd700b9820f03f2cff7")
-setManifestid(535931,"8772648359442339687")
-addappid(535932,0,"e96023d14598781d7557a326368c151f5344f0f07908048976c2d936538a40c9")
-setManifestid(535932,"3339250538333425032")
-addappid(535933,0,"afd24b32446d595e1a9eb6a92f86efe39a810967276c60eac67ab013ff812226")
-setManifestid(535933,"8053895649383227012")
-addappid(535934,0,"5081596c4bc39082ba05cdd66845272d699f624a790f5019f479c77cfe32baca")
-setManifestid(535934,"7740562474794154195")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 535930.lua
+--Gamename Two Point Hospital
+addappid(535930, 1, "4211e6a13a80d340fb5badee9f3ca036ad3d150c5cf8a6b05720ca4a241daaa2") --Mainappid Two Point Hospital
+addappid(535931, 1, "038f6bc45efe9b8bb282557291689a07867e95b7088cfcd700b9820f03f2cff7") --Main Windows Depot Two Point Hospital
+setManifestid(535931, "7503888200462323972", 3748399280)
+addappid(535932, 1, "e96023d14598781d7557a326368c151f5344f0f07908048976c2d936538a40c9") --Main Macos Depot Two Point Hospital
+setManifestid(535932, "8907883893101955548", 3759039680)
+addappid(535933, 1, "afd24b32446d595e1a9eb6a92f86efe39a810967276c60eac67ab013ff812226") --Main Linux Depot Two Point Hospital
+setManifestid(535933, "8053895649383227012", 3748380912)
+addappid(535934, 1, "5081596c4bc39082ba05cdd66845272d699f624a790f5019f479c77cfe32baca") --Main Windows Depot Two Point Hospital
+setManifestid(535934, "4660623737327035626", 3479675904)
+addappid(898770) --Dlcname Hospital Pass Rewards
+addappid(963240) --Dlcname Two Point Hospital - Convention-al Rug
+addtoken(963240, "14016073498584215671")
+addappid(966690) --Dlcname Two Point Hospital: Bigfoot
+addappid(1003310) --Dlcname Two Point Hospital: Pebberley Island
+addappid(1035020) --Dlcname Two Point Hospital: Retro Items pack
+addappid(1096580) --Dlcname Two Point Hospital: Close Encounters
+addtoken(1096580, "7350915962678311902")
+addappid(1144500) --Dlcname Two Point Hospital: Exhibition Items Pack
+addappid(1185850) --Dlcname Two Point Hospital: Jingle Jam Jingle Jimjams
+addappid(1196150) --Dlcname Two Point Hospital: Off The Grid
+addappid(1376920) --Dlcname Two Point Hospital: Fancy Dress Pack
+addappid(1377080) --Dlcname Two Point Hospital: Culture Shock
+addappid(1424940) --Dlcname Two Point Hospital: SEGA 60th Items
+addtoken(1424940, "14425829004195746129")
+addappid(1440200) --Dlcname Two Point Hospital: Free Zombie Costume
+addappid(1498090) --Dlcname Two Point Hospital: A Stitch in Time
+addappid(1901750) --Dlcname Two Point Hospital: Speedy Recovery
