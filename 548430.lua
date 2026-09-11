d1ee03f6abb4dd931709284845319ca701e4baa4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:24:14 +0800

    add 548430.lua

--- 548430.lua
+++ 548430.lua
@@ -1,34 +1,21 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 548430.lua
---Gamename Deep Rock Galactic
-addappid(548430, 1, "35e74cda02258a1dc29466af2d3c05f2030830981ead0936e3eb6c9d59284e7d") --Mainappid Deep Rock Galactic
-addappid(548431, 1, "73ef10dc344cb7fb34be23aea35b20fbb2b32b0db50650919c7e3d731e759aad") --Main Windows Depot Deep Rock Galactic
-setManifestid(548431, "5775322591771794477", 3600352304)
---Dlcname Deep Rock Galactic - Original Soundtrack Volume I + II
-addappid(802640) --Dlcname Deep Rock Galactic - Original Soundtrack Volume I + II
-addappid(802641, 1, "4a25bdb0198768cfc3277d4892ad41f45d19762c101711c73f7fd229b60e1df2") --Dlc Windows Depot Deep Rock Galactic - Original Soundtrack Volume I + II
-setManifestid(802641, "3260459802955998328", 239409152)
-addappid(802642, 1, "d448c4f4a1f48362382656c111c5dc7e5f18a01b98d86d4dc9f821781d563ba5") --Dlc Windows Depot Deep Rock Galactic - Original Soundtrack Volume I + II
-setManifestid(802642, "1312881002778069876", 555620304)
-addappid(802643, 1, "3f7c047aaeda45352662542387c5b0c6012529258d3a4ee7bfb72da9395b55f8") --Dlc Windows Depot Deep Rock Galactic - Original Soundtrack Volume I + II
-setManifestid(802643, "2206979629215810349", 160881952)
-addappid(802644, 1, "fb00dd61b77af7a766e93dec241b8d04576c0e1768cff0677448d00e965ce75a") --Dlc Windows Depot Deep Rock Galactic - Original Soundtrack Volume I + II
-setManifestid(802644, "5048846235105489207", 380614720)
---Share Depots
-addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
-setManifestid(228986, "8782296191957114623", 23045488)
-addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
-setManifestid(228988, "6645201662696499616", 22411856)
-addappid(801860) --Dlcname Deep Rock Galactic - Supporter Upgrade
-addappid(1283090) --Dlcname Deep Rock Galactic - Dark Future Pack
-addappid(1283091) --Dlcname Deep Rock Galactic - MegaCorp Pack
-addappid(1430610) --Dlcname Deep Rock Galactic - Roughneck Pack
-addappid(1530640) --Dlcname Deep Rock Galactic - Dawn of the Dread Pack
-addappid(1797340) --Dlcname Deep Rock Galactic - Rival Tech Pack
-addappid(1946960) --Dlcname Deep Rock Galactic - Robot Rebellion Pack
-addappid(2165590) --Dlcname Deep Rock Galactic - Biohazard Pack
-addappid(2291670) --Dlcname Deep Rock Galactic - Supporter II Upgrade
-addappid(2416900) --Dlcname Deep Rock Galactic - Decontaminator Pack
-addappid(2934390) --Dlcname Deep Rock Galactic - Order of the Deep Pack
-addappid(3481460) --Dlcname Deep Rock Galactic - Skullcrusher Pack
-addappid(4207910) --Dlcname Deep Rock Galactic - Relic Raider Pack
+addappid(548430)
+addappid(4207910)
+addappid(3481460)
+addappid(2934390)
+addappid(2416900)
+addappid(2291670)
+addappid(2165590)
+addappid(1946960)
+addappid(1797340)
+addappid(801860)
+addappid(1283090)
+addappid(1283091)
+addappid(1430610)
+addappid(1530640)
+addappid(802640)
+addappid(548431,1,"73ef10dc344cb7fb34be23aea35b20fbb2b32b0db50650919c7e3d731e759aad")
+addappid(228986,1,"51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5")
+addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
+setManifestid(548431,"5775322591771794477")
+setManifestid(228986,"8782296191957114623")
+setManifestid(228988,"6645201662696499616")
