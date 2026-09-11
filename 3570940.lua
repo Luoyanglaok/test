42f52dad86a5c3a9980150a075265d6b66b534ef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:26:24 +0800

    add 3570940.lua

---
+++ 3570940.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3570940.lua
+--Gamename Isekai Bistro
+addappid(3570940, 1, "d9df87a82504fe07effb0872756434d6e7287fc8519c6552e127c496f361d91e") --Mainappid Isekai Bistro
+addappid(3570941, 1, "fda6e87cead70aa7bd022e1e4fa314b383fe72dc4f6ebf4bbe5c9a367e3055f0") --Main Windows Depot Isekai Bistro
+setManifestid(3570941, "4188761212187936928", 1001452368)
+--Dlcname Isekai Bistro Artbook
+addappid(4591780) --Dlcname Isekai Bistro Artbook
+addappid(4591781, 1, "d279897475d270125126e3d22f62f9773ebc3955ea222f8b414685ab94f046fc") --Dlc Windows Depot Isekai Bistro Artbook
+setManifestid(4591781, "9114846704921011404", 194608896)
