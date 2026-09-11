7323774ba85d7110eedefd48d8596e2b21f23e02
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:19:29 +0800

    add 1129540.lua

--- 1129540.lua
+++ 1129540.lua
@@ -1,3 +1,6 @@
-addappid(1129540)
-addappid(1129541,0,"b1352d91104d7a3c4a4402d664ca5eaf0471b69d4483c5e37ca40d47416ae6ae")
-setManifestid(1129541,"762733069851296617")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1129540.lua
+--Gamename Orc Massage
+addappid(1129540, 1, "0d80d3da846a66fefb93dacf1ea3e517f9906a88ce066f367b1084408c0033f7") --Mainappid Orc Massage
+addappid(1129541, 1, "b1352d91104d7a3c4a4402d664ca5eaf0471b69d4483c5e37ca40d47416ae6ae") --Main Windows Depot Orc Massage
+setManifestid(1129541, "762733069851296617", 8184673824)
