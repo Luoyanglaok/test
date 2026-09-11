c50ce40c46b50884352a0614abd5b186f2dbe57d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:47:03 +0800

    add 3085910.lua

---
+++ 3085910.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3085910.lua
+--Gamename Feastopia
+addappid(3085910, 1, "c77fb2d4c25e8654e3f702c6e8634a3982e55c27c0de7aa27fd1aaf7d3d90cc4") --Mainappid Feastopia
+addtoken(3085910, "5387819681192250210")
+addappid(3085911, 1, "abcabc8a18b9a4e7718872d5d72ddc6984132ab33add00e086bca32cd91fa6b2") --Main Windows Depot Feastopia
+--Missing Dlcs
+--Dlcname Feastopia Soundtrack
