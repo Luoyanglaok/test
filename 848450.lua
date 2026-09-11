7cdb281ea5b69caac0249c84c5e1527e35998268
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 01:46:52 +0800

    add 848450.lua

--- 848450.lua
+++ 848450.lua
@@ -1,7 +1,17 @@
-addappid(848450)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-addappid(848452,0,"2faf06aaa2ceea761e8a38f7f8f72d4c64e3768d5e94c600e3c671f2eeffa17e")
-setManifestid(848452,"3183944276780659315")
-addappid(848453,0,"396d4aced9c9e5f7fa7f07f8971b1f0d673438dc324831ebd79a9cce5ae693b8")
-setManifestid(848453,"4691675320476539755")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 848450.lua
+--Gamename Subnautica: Below Zero
+addappid(848450, 1, "9124a368510557262677476613a2f852f3c6fa6b2de59fd46093edeeda2ae65c") --Mainappid Subnautica: Below Zero
+addappid(848452, 1, "2faf06aaa2ceea761e8a38f7f8f72d4c64e3768d5e94c600e3c671f2eeffa17e") --Main Windows Depot Subnautica: Below Zero
+setManifestid(848452, "3284616936916893028", 5087642528)
+addappid(848453, 1, "396d4aced9c9e5f7fa7f07f8971b1f0d673438dc324831ebd79a9cce5ae693b8") --Main Macos Depot Subnautica: Below Zero
+setManifestid(848453, "5213898712116806574", 5098747184)
+--Dlcname Subnautica: Below Zero Soundtrack
+addappid(1619010) --Dlcname Subnautica: Below Zero Soundtrack
+addappid(1619011, 1, "56679c2d6ebb4022ef53ac1185c5ffda3dff3b178c5dfb930c314c5292f8d5f9") --Dlc Windows Depot Subnautica: Below Zero Soundtrack
+setManifestid(1619011, "4333404235044384381", 346707936)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
