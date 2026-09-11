89a55fd7c9386108d8835c01382113cd61293a4e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:49:03 +0800

    add 3044760.lua

--- 3044760.lua
+++ 3044760.lua
@@ -1,3 +1,6 @@
-addappid(3044760)
-addappid(3044761,0,"9255c287c06e02d6d2ae424221b8291c831a513ca4c38a828bb95d03f2a91c33")
-setManifestid(3044761,"8173496851412115899")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3044760.lua
+--Gamename Taiwan Love Story⁵ Demo
+addappid(3044760) --Mainappid Taiwan Love Story⁵ Demo
+addappid(3044761, 1, "9255c287c06e02d6d2ae424221b8291c831a513ca4c38a828bb95d03f2a91c33") --Main Windows Depot Taiwan Love Story⁵ Demo
+setManifestid(3044761, "8173496851412115899", 1355376800)
