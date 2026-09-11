93a285f8dc5927f53c5beaaebd8938d5decfa161
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:01:12 +0800

    add 260230.lua

--- 260230.lua
+++ 260230.lua
@@ -1,7 +1,15 @@
-addappid(260230)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(260231,0,"6c41bd4691a0b3d82697e2b41689bb5b94b99f6d250f162227b1cd98bd902c4a")
-setManifestid(260231,"4720306033921821350")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 260230.lua
+--Gamename Valiant Hearts: The Great War™ / Soldats Inconnus : Mémoires de la Grande Guerre™
+addappid(260230) --Mainappid Valiant Hearts: The Great War™ / Soldats Inconnus : Mémoires de la Grande Guerre™
+addappid(260231, 1, "6c41bd4691a0b3d82697e2b41689bb5b94b99f6d250f162227b1cd98bd902c4a") --Main Windows Depot Valiant Hearts: The Great War™ / Soldats Inconnus : Mémoires de la Grande Guerre™
+setManifestid(260231, "4720306033921821350", 1111854032)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(406181, 1, "419e422f533fe69e81eaec351533ea335f44eaa94535e1c50e9ef31ac08e081f") --Share Windows Depot AppID 406180
+setManifestid(406181, "314289967746074236", 66760192)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
