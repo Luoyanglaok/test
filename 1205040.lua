f50353cc28ad451fab31f21ca78ae93595787800
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:07:58 +0800

    add 1205040.lua

--- 1205040.lua
+++ 1205040.lua
@@ -1,3 +1,6 @@
-addappid(1205040)
-addappid(1205041,0,"4c4bbe19ea7a183ac419f32c6c140bd98938407f07a00718eddeee9d493b24da")
-setManifestid(1205041,"319351010876076017")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1205040.lua
+--Gamename Granny: Chapter Two
+addappid(1205040, 1, "84a2eb9cd4d6818547f7e129c641840cae4a8ef092c5c37c900e02d47829aee6") --Mainappid Granny: Chapter Two
+addappid(1205041, 1, "4c4bbe19ea7a183ac419f32c6c140bd98938407f07a00718eddeee9d493b24da") --Main Windows Depot Granny: Chapter Two
+setManifestid(1205041, "4294412516828222739", 591611536)
