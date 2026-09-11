4e1c1834e3841c90424a8c4358e40d375c637d54
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:39:38 +0800

    add 1781160.lua

---
+++ 1781160.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1781160.lua
+--Gamename Electrician Simulator - First Shock
+addappid(1781160) --Mainappid Electrician Simulator - First Shock
+addappid(1781161, 1, "c66293b85faa5fd1a1510993df867a4277d7036667fd370fb8a859032039c4a4") --Main Windows Depot Electrician Simulator - First Shock
+setManifestid(1781161, "4631871077652485125", 2198850512)
