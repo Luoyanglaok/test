c59ba11c7cd0a65e67e682759dadd382d83bb2c0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:57:43 +0800

    add 2101960.lua

---
+++ 2101960.lua
@@ -0,0 +1,28 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2101960.lua
+--Gamename Cronos: The New Dawn
+addappid(2101960, 1, "ec96530e1a837a72f0812565ce111a52f7a5a2815456bcd0d1d0ea9663834afd") --Mainappid Cronos: The New Dawn
+addtoken(2101960, "2704160378812211529")
+addappid(2101961, 1, "7f2c42a3171b51a0ea7463a8acbf143d1341f27c8e7babad3fa9eeef66f133eb") --Main Windows Depot Cronos: The New Dawn
+setManifestid(2101961, "7413665617495231317", 22972219056)
+addappid(2101962, 1, "d3f3ea72d940025825a5859f4747ec70d37c6b3df38f8ec038c1c70911d4e0ac") --Main Linux Depot Cronos: The New Dawn
+setManifestid(2101962, "8662209173710427597", 20623403664)
+addappid(2101965, 1, "30689a8a7fd4b8afac63815d91719c1a0b9bf47b1487b2ab8c6397a823ba0c27") --Main Macos Depot Cronos: The New Dawn
+setManifestid(2101965, "8692998544696887968", 22701249568)
+--Dlcname Cronos: The New Dawn - Artbook
+addtoken(3748550, "13073764574007953713")
+addappid(3748550) --Dlcname Cronos: The New Dawn - Artbook
+addappid(3748551, 1, "a00d5fc8b73f7250ff62ca6a67441ad191eb2a2415db2d0db193285a7bb92780") --Dlc Windows Depot Cronos: The New Dawn - Artbook
+setManifestid(3748551, "2053467992832422812", 70882896)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(3748530) --Dlcname Cronos: The New Dawn - Preorder Bonus
+addtoken(3748530, "2558675309268600323")
+addappid(3748540) --Dlcname Cronos: The New Dawn - Deluxe Edition Upgrade
+addtoken(3748540, "7354826723082080627")
+addappid(4768180) --Dlcname Cronos: Lazarus
