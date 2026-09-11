38f8baa5407ba1afafcdf36110fa923123f09c48
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 12:49:34 +0800

    add 1000360.lua

--- 1000360.lua
+++ 1000360.lua
@@ -1,3 +1,6 @@
-addappid(1000360)
-addappid(1000361,0,"b5b97451061159abe6184f5c46e24d7d3a1898c26611b7dce09483c6b5843f79")
-setManifestid(1000361,"1877935547319009574")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1000360.lua
+--Gamename Hellish Quart
+addappid(1000360, 1, "ab45ce3d322dfac23e9432062cb49e9a28b52ae3b2b6588325c3a226dca36a2c") --Mainappid Hellish Quart
+addappid(1000361, 1, "b5b97451061159abe6184f5c46e24d7d3a1898c26611b7dce09483c6b5843f79") --Main Windows Depot Hellish Quart
+setManifestid(1000361, "8336076695197766858", 8289721120)
