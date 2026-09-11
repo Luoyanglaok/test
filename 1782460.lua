d158981d29cade16904386309a75306d98ce308f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:36:53 +0800

    add 1782460.lua

--- 1782460.lua
+++ 1782460.lua
@@ -1,3 +1,14 @@
-addappid(1782460)
-addappid(1782461,0,"b87981e090ab2ad89c7a18d14898061157db6a1ed5fb03d5e9f0546d734399ce")
-setManifestid(1782461,"8917802360475921388")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1782460.lua
+--Gamename Hell Clock
+addappid(1782460, 1, "c9cc29b2f929f03d1f9aa5ae84866cd3caafa4b581de971c5599e953eced891d") --Mainappid Hell Clock
+addappid(1782461, 1, "b87981e090ab2ad89c7a18d14898061157db6a1ed5fb03d5e9f0546d734399ce") --Main Windows Depot Hell Clock
+setManifestid(1782461, "1358312951495311820", 1409903248)
+--Dlcname Hell Clock Soundtrack
+addappid(3893120) --Dlcname Hell Clock Soundtrack
+addappid(3893122, 1, "92fa4198711af9cbff1be1f3f7a6b4e9a60f9988b3df20f104e8a693444288e4") --Dlc Windows Depot Hell Clock Soundtrack
+setManifestid(3893122, "9052655112804694111", 380349296)
+addappid(3774890) --Dlcname Hell Clock: Supporter Pack Edition
+addappid(3774910) --Dlcname Hell Clock: Cursed War
+--Missing Dlcs
+--Dlcname Hell Clock: Supporter Pack Upgrade
