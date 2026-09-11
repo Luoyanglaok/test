26d1770cf615c0803c46955d8c091671832b645e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:59:33 +0800

    add 3059520.lua

--- 3059520.lua
+++ 3059520.lua
@@ -1,11 +1,30 @@
-addappid(3059520)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(3059521,0,"69b468d1904c13ad60ff37077c6c6350439f3e70a1df3afcfc075f41325f6b8b")
-setManifestid(3059521,"4158072094888339010")
-addappid(3059522,0,"136b91fa8f220638f0a95077f481dbcb9c6f8d5a936401ec0222ddc2cc73de68")
-setManifestid(3059522,"3406045659755292342")
-addappid(3059523,0,"0fefd33650cc16731c9eaa7582b567f5ab2e1dfe22480ba457f704740cb59275")
-setManifestid(3059523,"5500027339946641612")
-addappid(3059524,0,"0f267a629c6d00a85bbea9fcadd7a8031d68da2504ea8e24821967b15c695e01")
-setManifestid(3059524,"3729989372449045086")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3059520.lua
+--Gamename F1® 25
+addappid(3059520, 1, "74712f7ff33f8367396d6daccbdb2683ce1a5d574dee4bc060144d5b9db95bc4") --Mainappid F1® 25
+addappid(3059521, 1, "69b468d1904c13ad60ff37077c6c6350439f3e70a1df3afcfc075f41325f6b8b") --Main Windows Depot F1® 25
+setManifestid(3059521, "855660325865551352", 37510652608)
+addappid(3059522, 1, "136b91fa8f220638f0a95077f481dbcb9c6f8d5a936401ec0222ddc2cc73de68") --Main Windows Depot F1® 25
+setManifestid(3059522, "8368152309781879498", 44875392224)
+addappid(3059523, 1, "0fefd33650cc16731c9eaa7582b567f5ab2e1dfe22480ba457f704740cb59275") --Main Windows Depot F1® 25
+setManifestid(3059523, "1607970825015496875", 5003576080)
+addappid(3059524, 1, "0f267a629c6d00a85bbea9fcadd7a8031d68da2504ea8e24821967b15c695e01") --Main Windows Depot F1® 25
+setManifestid(3059524, "5162663448467389238", 572707024)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6") --Share Windows Depot EA Javelin Anticheat Installer
+setManifestid(3893181, "3685412901435514107", 318033232)
+addappid(3335200) --Dlcname F1® 25 Iconic Bumper Pack
+addappid(3335210) --Dlcname F1® 25 Iconic Content Pack
+addappid(3335220) --Dlcname F1® 25: F1® The Movie - Chapter Scenarios
+addappid(3335250) --Dlcname F1® 25 Standard Starter Pack
+addappid(3335260) --Dlcname F1® 25: F1® 75 Celebration Pack
+addappid(3493790) --Dlcname F1® TV Pro 1-Month Subscription
+addappid(3493820) --Dlcname F1® 25 Iconic Edition Upgrade
+addappid(3493830) --Dlcname F1® 25 Iconic Edition Upgrade
+addappid(3546340) --Dlcname F1® 25 APXGP Team Pack
+addappid(3601380) --Dlcname F1 25 - EA Play Trial Key
+addappid(4148930) --Dlcname F1® 25: 2026 Season Pack
