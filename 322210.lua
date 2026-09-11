bf0eb753907a28c00ecf584ec57cb051632cb624
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:39:29 +0800

    add 322210.lua

--- 322210.lua
+++ 322210.lua
@@ -1,5 +1,8 @@
-addappid(322210)
-addappid(322212,0,"6cc2551f8d640dcef22e3b8e42ef73ecc99eab7746cef840452c81402a803885")
-setManifestid(322212,"5203239241664335591")
-addappid(322213,0,"6ae067adc26cb651c142b5d607c4133a8421286b4a97bb1924d0d6549087b34c")
-setManifestid(322213,"7183844994724458647")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 322210.lua
+--Gamename Guns, Gore & Cannoli
+addappid(322210) --Mainappid Guns, Gore & Cannoli
+addappid(322212, 1, "6cc2551f8d640dcef22e3b8e42ef73ecc99eab7746cef840452c81402a803885") --Main Windows Depot Guns, Gore & Cannoli
+setManifestid(322212, "5203239241664335591", 1349919072)
+addappid(322213, 1, "6ae067adc26cb651c142b5d607c4133a8421286b4a97bb1924d0d6549087b34c") --Main Macos Depot Guns, Gore & Cannoli
+setManifestid(322213, "7183844994724458647", 1351035392)
