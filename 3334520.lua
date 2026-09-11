11cad4350fed7ee1d3728147244c32f0ea7622c3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:38:51 +0800

    add 3334520.lua

---
+++ 3334520.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3334520.lua
+--Gamename 奶茶店大亨 - MilkTea Time
+addappid(3334520) --Mainappid 奶茶店大亨 - MilkTea Time
+addappid(3334521, 1, "fc715d73516736621df69aacd9e55a8d1a451adb99b047b43a69a128cc1484b8") --Main Windows Depot 奶茶店大亨 - MilkTea Time
+setManifestid(3334521, "73685918550491606", 1226718464)
