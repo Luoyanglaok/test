eb5e9609cde1498408998ebf4e63130871df4e23
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:36:40 +0800

    add 532790.lua

---
+++ 532790.lua
@@ -0,0 +1,23 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 532790.lua
+--Gamename Vampire: The Masquerade® - Bloodlines™ 2
+addappid(532790, 1, "90523a607f979ac7e71f9fb19c614b4751a8b4f9a7e7efcd5ad6fd72e8380b89") --Mainappid Vampire: The Masquerade® - Bloodlines™ 2
+addappid(532791, 1, "5031e75920df33014e680cad671ff0895f9e439d33eedb5b5878aa407d5cb177") --Main Windows Depot Vampire: The Masquerade® - Bloodlines™ 2
+setManifestid(532791, "1112797178864699279", 21438309440)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
+addappid(1048510) --Dlcname Vampire: The Masquerade® - Bloodlines™ 2 - Bloodlines Nostalgia Jukebox
+addappid(1048511) --Dlcname Vampire: The Masquerade® - Bloodlines™ 2 - Santa Monica Memories
+addappid(1048512) --Dlcname Vampire: The Masquerade® - Bloodlines™ 2 - Shadows & Silk
+addappid(3992600) --Dlcname Vampire: The Masquerade® - Bloodlines™ 2 - Expansion Pass
+addappid(3996800) --Dlcname Vampire: The Masquerade® - Bloodlines™ 2 - Loose Cannon
+addappid(3996820) --Dlcname Vampire: The Masquerade® - Bloodlines™ 2 - The Flower & the Flame
