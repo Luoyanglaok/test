eefed0250c3e0c89c2b681bf262b989c4f455cf3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:01:56 +0800

    add 2129530.lua

---
+++ 2129530.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2129530.lua
+--Gamename REANIMAL
+addappid(2129530, 1, "ea029714a30e4f9e526b5aa68792282d1ab1e7922fbba70da3983c9f958dcacb") --Mainappid REANIMAL
+addappid(2129531, 1, "8c619e3fd74002747f22974390275782dc2b568bc92b52859f0c5688a4a663f7") --Main Windows Depot REANIMAL
+setManifestid(2129531, "4146659007008728705", 14375976016)
+--Dlcname REANIMAL - Full Game
+addtoken(4351720, "7437698755938420611")
+addappid(4351720, 1, "6dba1be22c2161793226a2567e1ab5d6f153799494d7b26684d00e692ec041be") --Dlcname REANIMAL - Full Game
+setManifestid(4351720, "6551131955355540364", 14380518912)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3954470) --Dlcname REANIMAL - Foxhead and Muttonhead Masks
+addappid(4112550) --Dlcname REANIMAL – Season Pass
+addappid(4733900) --Dlcname REANIMAL - The Prisoner
