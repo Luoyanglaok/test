959e4ffc736b354bca81856bfbf6dbdd16293f8c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 08:13:30 +0800

    add 3755860.lua

---
+++ 3755860.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3755860.lua
+--Gamename BLACK SOULS
+addappid(3755860, 1, "98c27504893677355eb05d7b3390d2c3eca2b279ba4f2643ffcfca2da016a66d") --Mainappid BLACK SOULS
+addappid(3755861, 1, "1e7edb84c54a75b892289bf1f527430096987c8014060ed0f5a31431c19a9985") --Main Windows Language Depot Japanese BLACK SOULS
+setManifestid(3755861, "6682752774339849402", 680619792)
+addappid(3755862, 1, "d33a7f927ce836afe9da58b68f405435737ca5d9f3e20770b67880a57dded151") --Main Windows Language Depot Schinese BLACK SOULS
+setManifestid(3755862, "8702760606186730306", 798053184)
+addappid(3755863, 1, "284e12d82c8696e2c3d4fd500c0da44ddca49794c6ccd0682e0ec75b18ba43d7") --Main Windows Language Depot English BLACK SOULS
+setManifestid(3755863, "5145146131198925425", 788158688)
+addappid(3755864, 1, "c224ef7b659eeb66e7b48b84ce121c49fb721e84db25c92f0bfe8769e4a72c12") --Main Windows Language Depot Tchinese BLACK SOULS
+setManifestid(3755864, "2826478471919248830", 800623296)
