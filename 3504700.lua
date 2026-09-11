ef51f2e3ff1f223fe07849a9afc6d72dbe0a9cec
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:03:49 +0800

    add 3504700.lua

---
+++ 3504700.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3504700.lua
+--Gamename 4Wheel Challenge
+addappid(3504700) --Mainappid 4Wheel Challenge
+addappid(3504701, 1, "71bb8381920b2c0c1df20a19ba5ad8afd3921df2073b3feda3ae8da0f7b500bc") --Main Windows Depot 4Wheel Challenge
+setManifestid(3504701, "5850614362507821193", 1162256384)
+addappid(4570550) --Dlcname 4Wheel Challenge - ZRogueLike: The Last Wheelchair User
+addappid(4867090) --Dlcname 4Wheel Challenge - Only Up
