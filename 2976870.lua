01468d0b6f2080cf9a239e86a1b7298ec89fa2f9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:44:15 +0800

    add 2976870.lua

--- 2976870.lua
+++ 2976870.lua
@@ -8,12 +8,6 @@ setManifestid(2976871, "2943770608701930525", 2532014912)
 addappid(2982460) --Dlcname 台灣戀愛物語⁵ - 免費成人內容
 addappid(2982461, 1, "a1d18026a7276941fdc62c6e6c0abc9181a3a5e8d1fb6ce57c2b974a6704ecb1") --Dlc Windows Depot 台灣戀愛物語⁵ - 免費成人內容
 setManifestid(2982461, "6709507911070544379", 1221331232)
---Dlcname Taiwan Love Story⁵ Soundtrack
-addappid(3033250) --Dlcname Taiwan Love Story⁵ Soundtrack
-addappid(3033251, 1, "3c3a5a42cf400e9c55ce64cc990ad26a35f5cd36e7ffba80853001e3fc2ef12c") --Dlc Windows Depot Taiwan Love Story⁵ Soundtrack
-setManifestid(3033251, "1902738102402146137", 130127728)
-addappid(3033252, 1, "1e9dfcfdf5b4240d191352afa3eef13b913f3775ced111a36760ed5ca5dedff2") --Dlc Windows Depot Taiwan Love Story⁵ Soundtrack
-setManifestid(3033252, "1436573855261952709", 1397309040)
 --Dlcname Taiwan Love Story⁵ Artbook
 addappid(3346870) --Dlcname Taiwan Love Story⁵ Artbook
 addappid(3346871, 1, "44c94c69a119fccdf16968acf55d9a009d53fbd0f43ad58d5a7ba5eca1b4c90c") --Dlc Windows Depot Taiwan Love Story⁵ Artbook
