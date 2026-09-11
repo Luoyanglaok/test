fe6ec1c128c503ebd15aa46cb47a865b2982f2de
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:07:35 +0800

    add 3959530.lua

---
+++ 3959530.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3959530.lua
+--Gamename Landscaper Simulator
+addappid(3959530) --Mainappid Landscaper Simulator
+addappid(3959531, 1, "381af5364ad26fcb30552cd60904593ad13cc1fc45fc5ff310833699227f6756") --Main Windows Depot Landscaper Simulator
+setManifestid(3959531, "6010541172239843477", 284811472)
