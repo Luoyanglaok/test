3593cd519eea96eab3ce99cdd5336e583b5d210f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:15:10 +0800

    add 1368030.lua

--- 1368030.lua
+++ 1368030.lua
@@ -1,5 +1,15 @@
-addappid(1368030)
-addappid(1368031,0,"3332efc299cec15a93d1602555631c48f6272e69159b6cdd85a42aae0c5c5c22")
-setManifestid(1368031,"9199278987100224176")
-addappid(1909960,0,"07a45b03b0c6f5bf92fb6f84ed9877e0908f29f89663a644d6cb3438188281d8")
-setManifestid(1909960,"8722771999527886706")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1368030.lua
+--Gamename ANNO: Mutationem
+addappid(1368030, 1, "8e5ee1a4d2f48cc19a81efe114633f8aec9be2bc1cb22e7bd07ff004f90ccc89") --Mainappid ANNO: Mutationem
+addtoken(1368030, "6936581589546791641")
+addappid(1368031, 1, "3332efc299cec15a93d1602555631c48f6272e69159b6cdd85a42aae0c5c5c22") --Main Windows Depot ANNO: Mutationem
+--Dlcname Anno: Mutationem Original Game Soundtrack Complete Edition
+addappid(1823560) --Dlcname Anno: Mutationem Original Game Soundtrack Complete Edition
+addappid(1823561, 1, "ef12706289534d19f04e607c8ea78da1ca5f8fe1749f80ea0da212855f4094d6") --Dlc Windows Depot Anno: Mutationem Original Game Soundtrack Complete Edition
+setManifestid(1823561, "3822919914988398668", 491618736)
+addappid(1823562, 1, "81c8b95ba8985277b143abfa3519b878e3963fdbcf4ac92226d8e767aa1cdbdc") --Dlc Windows Depot Anno: Mutationem Original Game Soundtrack Complete Edition
+setManifestid(1823562, "5597401790031103004", 1254232224)
+--Dlcname ANNO: Mutationem - Collectors Upgrade Pack
+addtoken(1909960, "3026245640165213879")
+addappid(1909960, 1, "07a45b03b0c6f5bf92fb6f84ed9877e0908f29f89663a644d6cb3438188281d8") --Dlcname ANNO: Mutationem - Collectors Upgrade Pack
