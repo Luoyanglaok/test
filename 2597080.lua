59c1dbb39c39b57d275bc078d21d110c24ce0f26
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:16:01 +0800

    add 2597080.lua

--- 2597080.lua
+++ 2597080.lua
@@ -1,9 +1,2 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 2597080.lua
---Gamename Realm of Ink
-addappid(2597080, 1, "04b432ab57a87689ee15e32998283d45a9791293dc7c4f20450eae95bfc65600") --Mainappid Realm of Ink
-addtoken(2597080, "13497506901935481773")
-addappid(2597081, 1, "30b38a65aea230571a237b595825b0e4500f6c3717546e14cf300291af6b4891") --Main Windows Depot Realm of Ink
-setManifestid(2597081, "6991211255193703581", 5240598928)
-addappid(2597082, 1, "823d9917bc63d8708be7167f2c46eb5f3bf528bf4cccffa5b1a5efa13336a429") --Main Windows Depot Realm of Ink
-setManifestid(2597082, "6987573082333451708", 5204495264)
+addappid(2597080)
+setManifestid(2597081,"6991211255193703581")
