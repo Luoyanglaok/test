aa63cd66c5867a02ff19bff9fd1335cc1a10808c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:06:33 +0800

    add 518790.lua

--- 518790.lua
+++ 518790.lua
@@ -1,4 +1,85 @@
-addappid(518790)
-setManifestid(228988,"6645201662696499616")
-addappid(518791,0,"c56059a25b086342342b536c815b9903ee3faa35b8358fe7556346fe60a9081e")
-setManifestid(518791,"7056211137777624643")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 518790.lua
+--Gamename theHunter: Call of the Wild™
+addappid(518790, 1, "e05cc7be14786efefc664724a6358b5e1a54532838a2eb508538c87fdd682f9c") --Mainappid theHunter: Call of the Wild™
+addappid(518791, 1, "c56059a25b086342342b536c815b9903ee3faa35b8358fe7556346fe60a9081e") --Main Windows Depot theHunter: Call of the Wild™
+setManifestid(518791, "7347212052527316668", 67018291264)
+addappid(518792, 1, "a50ac267e9bb6647ae465bebbe952989e1e6b3dc7f9e20c5a2a96fd2addf2788") --Main Windows Depot theHunter: Call of the Wild™
+setManifestid(518792, "3125085761261358713", 499885472)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(579670) --Dlcname AppID 579670
+addappid(579671) --Dlcname AppID 579671
+addappid(579672) --Dlcname AppID 579672
+addappid(602310) --Dlcname theHunter: Call of the Wild™ - Bearclaw Lite CB-60
+addappid(619790) --Dlcname theHunter: Call of the Wild™ - Facing the Wild 1
+addappid(622450) --Dlcname theHunter: Call of the Wild™ - Tents & Ground Blinds
+addappid(630940) --Dlcname theHunter: Call of the Wild™ - ATV
+addappid(640230) --Dlcname theHunter: Call of the Wild™ - Shooting Range
+addappid(695680) --Dlcname theHunter: Call of the Wild™ - Medved-Taiga
+addappid(706150) --Dlcname theHunter: Call of the Wild™ - Backpacks
+addappid(766870) --Dlcname theHunter: Call of the Wild™ - Weapon Pack 1
+addappid(767380) --Dlcname theHunter: Call of the Wild™ - New Species 2018
+addappid(854070) --Dlcname theHunter: Call of the Wild™ - Wild Goose Chase Gear
+addappid(894600) --Dlcname theHunter: Call of the Wild™ - Vurhonga Savanna
+addappid(939840) --Dlcname theHunter: Call of the Wild™ - Duck and Cover Pack
+addappid(986850) --Dlcname theHunter: Call of the Wild™ - Parque Fernando
+addappid(1011280) --Dlcname theHunter: Call of the Wild™ - New Species 2019
+addappid(1028270) --Dlcname theHunter: Call of the Wild™ - Weapon Pack 2
+addappid(1052690) --Dlcname theHunter: Call of the Wild™ - Trophy Lodge Spring Creek Manor
+addappid(1070170) --Dlcname theHunter: Call of the Wild™ - TruRACS
+addappid(1076850) --Dlcname theHunter: Call of the Wild™ - Treestand & Tripod Pack
+addappid(1093700) --Dlcname theHunter: Call of the Wild™ - Yukon Valley
+addappid(1108410) --Dlcname theHunter: Call of the Wild™ - Weapon Pack 3
+addappid(1146830) --Dlcname theHunter: Call of the Wild™ - High-Tech Hunting Pack
+addappid(1164110) --Dlcname theHunter: Call of the Wild™ - Saseka Safari Trophy Lodge
+addappid(1179710) --Dlcname theHunter: Call of the Wild™ - Cuatro Colinas Game Reserve
+addappid(1252750) --Dlcname theHunter: Call of the Wild™ - Smoking Barrels Weapon Pack
+addappid(1316840) --Dlcname theHunter: Call of the Wild™ - Silver Ridge Peaks
+addappid(1433620) --Dlcname theHunter: Call of the Wild™ - Remi Warren
+addappid(1450350) --Dlcname theHunter: Call of the Wild™ - Free Species: European Rabbit
+addappid(1463090) --Dlcname theHunter: Call of the Wild™ - Te Awaroa National Park
+addappid(1546670) --Dlcname theHunter: Call of the Wild™ - Bloodhound
+addappid(1608440) --Dlcname theHunter: Call of the Wild™ - Rancho del Arroyo
+addappid(1776430) --Dlcname theHunter: Call of the Wild™ - Mississippi Acres Preserve
+addappid(1843410) --Dlcname theHunter: Call of the Wild™ - Modern Rifle Pack
+addappid(1931910) --Dlcname theHunter: Call of the Wild™ - Revontuli Coast
+addappid(2064720) --Dlcname theHunter: Call of the Wild™ - Assorted Sidearms Pack
+addappid(2167630) --Dlcname theHunter: Call of the Wild™ - New England Mountains
+addappid(2197810) --Dlcname theHunter: Call of the Wild™ - Emerald Coast Cosmetic Pack
+addappid(2197811) --Dlcname theHunter: Call of the Wild™ - Sundarpatan Cosmetic Pack
+addappid(2197812) --Dlcname theHunter: Call of the Wild™ - Layton Lake Cosmetic Pack
+addappid(2197822) --Dlcname theHunter: Call of the Wild™ - Medved-Taiga Cosmetic Pack
+addappid(2198012) --Dlcname theHunter: Call of the Wild™ - Vurhonga Savanna Cosmetic Pack
+addappid(2198015) --Dlcname theHunter: Call of the Wild™ - Parque Fernando Cosmetic Pack
+addappid(2198025) --Dlcname theHunter: Call of the Wild™ - Yukon Valley Cosmetic Pack
+addappid(2198092) --Dlcname theHunter: Call of the Wild™ - Cuatro Colinas Cosmetic Pack
+addappid(2198095) --Dlcname theHunter: Call of the Wild™ - Silver Ridge Peaks Cosmetic Pack
+addappid(2198870) --Dlcname theHunter Call of the Wild™ - New England Scout Cosmetic Pack
+addappid(2198872) --Dlcname theHunter Call of the Wild™ - New England Veteran Cosmetic Pack
+addappid(2198874) --Dlcname theHunter Call of the Wild™ - Hirschfelden Veteran Cosmetic Pack
+addappid(2199012) --Dlcname theHunter: Call of the Wild™ - Te Awaroa Cosmetic Pack
+addappid(2199015) --Dlcname theHunter: Call of the Wild™ - Rancho del Arroyo Cosmetic Pack
+addappid(2199072) --Dlcname theHunter: Call of the Wild™ - Mississippi Acres Cosmetic Pack
+addappid(2199075) --Dlcname theHunter: Call of the Wild™ - Revontuli Coast Cosmetic Pack
+addappid(2299780) --Dlcname theHunter: Call of the Wild™ - Hunter Power Pack
+addappid(2383430) --Dlcname theHunter: Call of the Wild™ - Emerald Coast Australia
+addappid(2532370) --Dlcname theHunter: Call of the Wild™ - Ambusher Pack
+addappid(2631430) --Dlcname theHunter: Call of the Wild™ - Labrador Retriever
+addappid(2701860) --Dlcname theHunter: Call of the Wild™ - High Caliber Weapon Pack
+addappid(2869680) --Dlcname theHunter: Call of the Wild™ - Sundarpatan Hunting Reserve
+addappid(3106720) --Dlcname theHunter: Call of the Wild™ - Scopes and Crosshairs Pack
+addappid(3244800) --Dlcname theHunter: Call of the Wild™ - German Shorthaired Pointer
+addappid(3244810) --Dlcname theHunter: Call of the Wild™ - Salzwiesen Park
+addappid(3244820) --Dlcname theHunter: Call of the Wild™ - Hunters' Choice: Bolt-Action Rifle Pack
+addappid(3447720) --Dlcname theHunter: Call of the Wild™ - Rapid Hunt Rifle Pack
+addappid(3570290) --Dlcname theHunter: Call of the Wild™ - Alberta Hunting Preserve
+addappid(3750770) --Dlcname theHunter: Call of the Wild™ – Pump, Flip and Fire Pack
+addappid(3992110) --Dlcname theHunter: Call of the Wild™ - Game Feeder Pack
+addappid(4010590) --Dlcname theHunter: Call of the Wild™ - Scotland Hunting Reserve
+addappid(4089880) --Dlcname theHunter: Call of the Wild™ - Premium Trophy Mount Pack
+addappid(4182010) --Dlcname theHunter: Call of the Wild™ - Game Feeder Pack 2
+addappid(4424610) --Dlcname theHunter: Call of the Wild™ - Multi-Class Weapon Pack
+addappid(4467980) --Dlcname theHunter: Call of the Wild™ - Peru Hunting Reserve
+addappid(4868870) --Dlcname theHunter: Call of the Wild™ - Old West Weapon Pack
