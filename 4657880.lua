1e53c265c1d534cb98b1536073fc1ff155737c38
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:24:11 +0800

    add 4657880.lua

---
+++ 4657880.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4657880.lua
+--Gamename Symphony of the Serpent Demo
+addappid(4657880) --Mainappid Symphony of the Serpent Demo
+addappid(4657881, 1, "094c85cbee969f7eb56c8ce18f337e03428924855b6d630a7a400baecc96b444") --Main Windows Depot Symphony of the Serpent Demo
+setManifestid(4657881, "4713330542872686157", 3399488192)
