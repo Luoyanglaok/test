d5455ac5924cd101d3033687f976bf09f1028014
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:43:55 +0800

    add 4158870.lua

---
+++ 4158870.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4158870.lua
+--Gamename Japanese Rural Life Adventure
+addappid(4158870, 1, "e3a7aa174b5b3b29ef17c830fc859f467bbfd4a7d6ccda56b7bfafbbca73cfce") --Mainappid Japanese Rural Life Adventure
+addappid(4158871, 1, "3572d23e28fe810f631002260bf811383939e1ef9203b5203b93dd0e65375fb0") --Main Windows Depot Japanese Rural Life Adventure
+setManifestid(4158871, "5187776214282891429", 128243040)
