2b26592574957fd77014b1812e971bbfbafa019e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:47:54 +0800

    add 3700780.lua

---
+++ 3700780.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3700780.lua
+--Gamename Wild West Pioneers Demo
+addappid(3700780) --Mainappid Wild West Pioneers Demo
+addappid(3700781, 1, "8292d4406066880c7652b799f3bbcbe7a214a983e774243fec30b0efc7ec6716") --Main Windows Depot Wild West Pioneers Demo
+setManifestid(3700781, "3147432179485688409", 5332628192)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
