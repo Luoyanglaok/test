a561677ed9d1cbcb90d9784e2497eb65abb19820
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:37:02 +0800

    add 2193540.lua

--- 2193540.lua
+++ 2193540.lua
@@ -1,5 +1,11 @@
-addappid(2193540)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2193541,0,"0b38693b47350ed1df704c576d67dd2d05ce6e286543e940c52a95a6f7b3b812")
-setManifestid(2193541,"9013949346201226547")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2193540.lua
+--Gamename Wizard of Legend 2
+addappid(2193540, 1, "2269c0cb2840f90262978a2eca978b358ac3d5a4e3d1e78ef2a973f29b0904a7") --Mainappid Wizard of Legend 2
+addappid(2193541, 1, "0b38693b47350ed1df704c576d67dd2d05ce6e286543e940c52a95a6f7b3b812") --Main Windows Depot Wizard of Legend 2
+setManifestid(2193541, "6508668046492795301", 3300096400)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
