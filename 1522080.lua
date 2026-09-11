44a298d670b6261888706e40a4f9d13f0f1172a0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:29:13 +0800

    add 1522080.lua

--- 1522080.lua
+++ 1522080.lua
@@ -1,3 +1,6 @@
-addappid(1522080)
-addappid(1522081,0,"81a741e78e1a125f41d7a850b3f71bc2a5161ee358ca5910cef69e6082516aa7")
-setManifestid(1522081,"4586069640638762127")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1522080.lua
+--Gamename Jitsu Squad
+addappid(1522080) --Mainappid Jitsu Squad
+addappid(1522081, 1, "81a741e78e1a125f41d7a850b3f71bc2a5161ee358ca5910cef69e6082516aa7") --Main Windows Language Depot English Jitsu Squad
+setManifestid(1522081, "4586069640638762127", 1597231552)
