b2628fc10ae75ad2644018a1b2155e81f405d3b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:03:00 +0800

    add 2104890.lua

--- 2104890.lua
+++ 2104890.lua
@@ -1,5 +1,16 @@
-addappid(2104890)
-addappid(2104891,0,"7aae8e21e13a76d7d261e23592399805131dbb2dc4678b5c40b335e6f131b3a7")
-setManifestid(2104891,"4145879645757748482")
-addappid(3510890,0,"fed6ac5a7ee3d94e10e924fe19f30acfcbcfdb56a5835b080efede124d44b8a0")
-setManifestid(3510890,"958223831529350072")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2104890.lua
+--Gamename RoadCraft
+addappid(2104890, 1, "b07e322e1f1bc0a15e4d7abe0e9abd0a04f22ffbf084774a1a252bf455048388") --Mainappid RoadCraft
+addappid(2104891, 1, "7aae8e21e13a76d7d261e23592399805131dbb2dc4678b5c40b335e6f131b3a7") --Main Windows Depot RoadCraft
+setManifestid(2104891, "5603132051103311041", 36192926304)
+--Dlcname RoadCraft - 4K Texture Pack
+addappid(3510890, 1, "fed6ac5a7ee3d94e10e924fe19f30acfcbcfdb56a5835b080efede124d44b8a0") --Dlcname RoadCraft - 4K Texture Pack
+setManifestid(3510890, "1705142645359025405", 34962219536)
+addappid(2806770) --Dlcname RoadCraft – Aramatsu Bowhead 30T
+addappid(3470710) --Dlcname RoadCraft – Invictus Type A Scout
+addappid(3470720) --Dlcname RoadCraft - Rebuild DLC
+addappid(3470730) --Dlcname RoadCraft - Rebuild Expansion
+addappid(4124160) --Dlcname RoadCraft – Timberworks Pack
+addappid(4159460) --Dlcname RoadCraft - Reclaim Expansion
+addappid(4511610) --Dlcname RoadCraft – Crawler Pack
