9913509ffcd9f0a10860e5738d50ed47d2aa85e4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:49:51 +0800

    add 3241760.lua

--- 3241760.lua
+++ 3241760.lua
@@ -1,3 +1,10 @@
-addappid(3241760)
-addappid(3241761,0,"877ea4fd65b7150aa3944c78c3840696cec669b06fee87e9965fa6665daaff1a")
-setManifestid(3241761,"5974818572347200081")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3241760.lua
+--Gamename Mirai's Midnight Stream
+addappid(3241760, 1, "98e6d74e0ff089a782df744e3bafc4974acc35f0c65e25ca43beb2b8cbb08e84") --Mainappid Mirai's Midnight Stream
+addappid(3241761, 1, "877ea4fd65b7150aa3944c78c3840696cec669b06fee87e9965fa6665daaff1a") --Main Windows Depot Mirai's Midnight Stream
+setManifestid(3241761, "4311711827188964597", 12627632848)
+--Dlcname Mirai's Midnight Stream - Collector's Edition
+addappid(3745130) --Dlcname Mirai's Midnight Stream - Collector's Edition
+addappid(3745131, 1, "7ef6759f6a487c1f1e91ce704482274f14d1b359c43c78c915a233572a8ed01f") --Dlc Windows Depot Mirai's Midnight Stream - Collector's Edition
+setManifestid(3745131, "8555071299811963269", 3579266912)
