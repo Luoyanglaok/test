45b3791f144954ca6e6ddaf6b15056fda10ee837
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:35:19 +0800

    add 1325200.lua

--- 1325200.lua
+++ 1325200.lua
@@ -1,7 +1,15 @@
-addappid(1325200)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1325201,0,"20da81589e0a73461a18d3f42426068e48228b81ea86664c8b93ffdf7285762a")
-setManifestid(1325201,"114676460224695371")
-addappid(1325202,0,"e644223a7b80d05032b8d93def9d0473b80c7430da7e55e3540eefbf0961d0e1")
-setManifestid(1325202,"3674821973469504854")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1325200.lua
+--Gamename Nioh 2 – The Complete Edition
+addappid(1325200, 1, "8eb55cac45a68934d7f485b2757c1c49d98713faf8c1e48839ad1bf8afb6f40d") --Mainappid Nioh 2 – The Complete Edition
+addappid(1325201, 1, "20da81589e0a73461a18d3f42426068e48228b81ea86664c8b93ffdf7285762a") --Main Windows Depot Nioh 2 – The Complete Edition
+setManifestid(1325201, "114676460224695371", 59735055472)
+addappid(1325202, 1, "e644223a7b80d05032b8d93def9d0473b80c7430da7e55e3540eefbf0961d0e1") --Main Windows Depot Nioh 2 – The Complete Edition
+setManifestid(1325202, "3674821973469504854", 24958704)
+addappid(1325203, 1, "9c164a5820759db569bf273ebbd44f4d5d48df4cc884899e99a9586fa7269330") --Main Windows Depot Nioh 2 – The Complete Edition
+setManifestid(1325203, "7274020265141192108", 24956704)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
