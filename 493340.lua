7187feb463839d0bbfaabda8f5decd38ab711739
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 01:50:28 +0800

    add 493340.lua

--- 493340.lua
+++ 493340.lua
@@ -1,11 +1,42 @@
-addappid(493340)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(493341,0,"09a4f06e0ef8de8d1caf20ff08c96103f36c42117bfbada84788bbbb1e6fb540")
-setManifestid(493341,"1860998967709107590")
-addappid(553770,0,"89104dad5e5c9353db7da2a99242c14b9da5fa15e5e838953b4c2ca13e9cf43a")
-setManifestid(553770,"85056478682462288")
-addappid(553771,0,"8f009b27cac943465ce5619ce1fb47db51d461407158065897ee58150490f9d1")
-setManifestid(553771,"2666834127152107450")
-addappid(493359,0,"38c45d793c46bac67e42f1a20ba64fc3d92edeb7dd9fda970d75db772a664520")
-setManifestid(493359,"5359596109481587651")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 493340.lua
+--Gamename Planet Coaster
+addappid(493340, 1, "ca45add22809fbfd4f2e9a8393edc799cc5c7c6a693fb1aacb78141c08fa51d5") --Mainappid Planet Coaster
+addappid(493341, 1, "09a4f06e0ef8de8d1caf20ff08c96103f36c42117bfbada84788bbbb1e6fb540") --Main Windows Depot Planet Coaster
+setManifestid(493341, "1860998967709107590", 12077557504)
+addappid(493359, 1, "38c45d793c46bac67e42f1a20ba64fc3d92edeb7dd9fda970d75db772a664520") --Main Macos Depot Planet Coaster
+setManifestid(493359, "5359596109481587651", 11852813808)
+--Dlcname AppID 553770
+addtoken(553770, "9338851922907917429")
+addappid(553770, 1, "89104dad5e5c9353db7da2a99242c14b9da5fa15e5e838953b4c2ca13e9cf43a") --Dlcname AppID 553770
+setManifestid(553770, "85056478682462288", 745018160)
+--Dlcname AppID 553771
+addtoken(553771, "4827531281155771420")
+addappid(553771, 1, "8f009b27cac943465ce5619ce1fb47db51d461407158065897ee58150490f9d1") --Dlcname AppID 553771
+setManifestid(553771, "2666834127152107450", 30930624)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(520710) --Dlcname Planet Coaster: Thrillseeker Edition
+addappid(540750) --Dlcname AppID 540750
+addappid(540751) --Dlcname AppID 540751
+addappid(540752) --Dlcname AppID 540752
+addappid(540760) --Dlcname AppID 540760
+addtoken(540760, "16535175624377149277")
+addappid(611400) --Dlcname Planet Coaster - The Munsters® Munster Koach Construction Kit
+addappid(611420) --Dlcname Planet Coaster - Knight Rider™ K.I.T.T. Construction Kit
+addappid(611421) --Dlcname Planet Coaster - Back to the Future™ Time Machine Construction Kit
+addappid(710490) --Dlcname Planet Coaster - Spooky Pack
+addappid(710500) --Dlcname Planet Coaster - FX17 Digital Goodies
+addappid(710530) --Dlcname Planet Coaster - FX17 Digital Goodies (Founder Level)
+addappid(768830) --Dlcname Planet Coaster - Adventure Pack
+addappid(821090) --Dlcname Planet Coaster - Studios Pack
+addappid(891230) --Dlcname Planet Coaster - Vintage Pack
+addappid(953660) --Dlcname Planet Coaster - World's Fair Pack
+addappid(995780) --Dlcname Planet Coaster - Magnificent Rides Collection
+addappid(1061350) --Dlcname Planet Coaster - Classic Rides Collection
+addappid(1092880) --Dlcname AppID 1092880
+addappid(1094160) --Dlcname Planet Coaster - Quick Draw Interactive Shooting Ride
+addappid(2171430) --Dlcname Planet Coaster: Subscription Pass
