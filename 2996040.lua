4f65e659bdb3b89617f255ca119f51f4cb7316bc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:38:21 +0800

    add 2996040.lua

--- 2996040.lua
+++ 2996040.lua
@@ -1,6 +1,23 @@
-addappid(2996040)
-setManifestid(228988,"6645201662696499616")
-addappid(2996041,0,"e93c99408425422103d746643c24b3bc8c717cd5110c9f0b876225db50cc6671")
-setManifestid(2996041,"4266532081604388589")
-addappid(3255900,0,"200f77802a15d4f75f7441e2bb4dbbdbd90e6419e42bd8e66715b12bb0452284")
-setManifestid(3255900,"4587529972681717957")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2996040.lua
+--Gamename Teenage Mutant Ninja Turtles: Splintered Fate
+addappid(2996040, 1, "96891d9b2870a84c54bb996262f721b1299c79885486e6feb249653388a2d127") --Mainappid Teenage Mutant Ninja Turtles: Splintered Fate
+addappid(2996041, 1, "e93c99408425422103d746643c24b3bc8c717cd5110c9f0b876225db50cc6671") --Main Windows Depot Teenage Mutant Ninja Turtles: Splintered Fate
+setManifestid(2996041, "138817717960842889", 1977383104)
+--Dlcname TMNT: Splintered Fate Soundtrack
+addappid(3247420) --Dlcname TMNT: Splintered Fate Soundtrack
+addappid(3247421, 1, "e0fa92800392485535c849929c56e0f55d0fc0f57ede311b0794448ae9564d69") --Dlc Windows Depot TMNT: Splintered Fate Soundtrack
+setManifestid(3247421, "1508636023682389250", 1297424160)
+addappid(3247422, 1, "83a069ffe8d6e373c15ea06e16652993927d88b27e9846ec9a09221b1d44ed1b") --Dlc Windows Depot TMNT: Splintered Fate Soundtrack
+setManifestid(3247422, "1233438326065711824", 208700480)
+--Dlcname TMNT Splintered Fate - Artbook
+addappid(3255900, 1, "200f77802a15d4f75f7441e2bb4dbbdbd90e6419e42bd8e66715b12bb0452284") --Dlcname TMNT Splintered Fate - Artbook
+setManifestid(3255900, "4587529972681717957", 541521232)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(3316670) --Dlcname TMNT: Splintered Fate - Casey Jones & the Junkyard Jam
+addappid(3784190) --Dlcname TMNT: Splintered Fate - Metalhead
+addappid(4152390) --Dlcname TMNT: Splintered Fate - Alopex
+addappid(4611360) --Dlcname TMNT: Splintered Fate - Slash Character DLC
+addappid(4611370) --Dlcname TMNT: Splintered Fate - Samurai Armor
