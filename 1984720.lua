66c7a8bfe92a3ff8dd472c83efd6ec8636ed1963
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:41:06 +0800

    add 1984720.lua

--- 1984720.lua
+++ 1984720.lua
@@ -1,4 +1,6 @@
-addappid(1984720)
-addtoken(1984720,"15062086920492002569")
-addappid(1984721,0,"ab4edfd9e483e8ec0298e01cd8fd21bfbfae98d9f60fae744c4fa7648a15ff11")
-setManifestid(1984721,"8359958036363949317")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1984720.lua
+--Gamename 逐鹿
+addappid(1984720) --Mainappid 逐鹿
+addtoken(1984720, "15062086920492002569")
+addappid(1984721, 1, "ab4edfd9e483e8ec0298e01cd8fd21bfbfae98d9f60fae744c4fa7648a15ff11") --Main Windows Depot 逐鹿
