6a3a8f4c9108b0a29a1b851af0733f16caaac787
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:32:51 +0800

    add 1273400.lua

--- 1273400.lua
+++ 1273400.lua
@@ -1,36 +1,78 @@
-addappid(1273400)
-setManifestid(228989,"3514306556860204959")
-addappid(1273401,0,"7ac6b6d000ec93fdd41c708fdaa554b3b457d5e9c07bb1947f2d555502c12b28")
-setManifestid(1273401,"3680529257743237412")
-addappid(1944341,0,"fff00853888f489491f77e33f4b0abfdb9e4f274f7abb2db7e4a473c75710ad2")
-setManifestid(1944341,"8807678063382063768")
-addappid(1944340,0,"9ada72090f09e32fe5de61dc7f3d27dfa65bbf00c10951f5e171fef1d54467f4")
-setManifestid(1944340,"322555549106022213")
-addappid(2203420,0,"936a535f06583e664a84071a3e5ef1442c263230dfccb079ed73cf6b1632531f")
-setManifestid(2203420,"8739100311331388793")
-addappid(2291424,0,"aa1e4588ba76e6e1723d055f0ac651747fd4f70363d870bfd3bfcc0a9b0641d6")
-setManifestid(2291424,"8453066766368453847")
-addappid(2295781,0,"e934f73594337e0e6026594ced95237670b45cb3b0a4a8afe5afb168cf7c96b9")
-setManifestid(2295781,"685349866155445181")
-addappid(2295811,0,"81149cf5bfb357c4d5e9ca09e28b9bb871218513bf57671f45b4db19322e3987")
-setManifestid(2295811,"3383914678723808175")
-addappid(2295821,0,"0238a85d92c1efd94f87882d80f94f274a9642622803775ac612597cc3900a37")
-setManifestid(2295821,"8580441773329478911")
-addappid(2322991,0,"e6cef62d582f8932a2108656f1f9bd325b154bfbfd77bc4e334d201f50bd367f")
-setManifestid(2322991,"7520794540910599885")
-addappid(2322981,0,"5b18f6aa61fc834076274cf99d042aaf7a103e39ae6c2e916b2ca6c157969162")
-setManifestid(2322981,"5743210225651998743")
-addappid(2322971,0,"e259ebb0698e11fea131e2432dc3fb90a1deb703381e24c6f86da0a5b873260c")
-setManifestid(2322971,"320280581201002583")
-addappid(2657421,0,"44b4871b93c1c2532a5d33b57cba0f4741d9a76c64cb63c4678044b9d8eec00a")
-setManifestid(2657421,"8791015684642513912")
-addappid(2657381,0,"1197196a9b63ed09d0fe34a639f3613f84174fd77cd34ebd8c94092c1618a6ef")
-setManifestid(2657381,"6016383179637761921")
-addappid(2657391,0,"57b63b80e8017b3ba57d81a99bf46013281784cf8337fae9f22c99188903283e")
-setManifestid(2657391,"3197796519729678759")
-addappid(2657401,0,"f4203866785afe4f52c192b96551976c3921fdb35b2b4c82cc2129b0d0a4f46c")
-setManifestid(2657401,"8114831874000182204")
-addappid(2657411,0,"6d0061174c36857cf29b71b91831fc9e2ae1d6b0e8e1095248975c2f7c29c28a")
-setManifestid(2657411,"3557864438957203195")
-addappid(2800781,0,"e4fd402d9c5abf02cceaad103495cd1c30d7fe3c777d312788a808fe719e0701")
-setManifestid(2800781,"5044123825545457794")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1273400.lua
+--Gamename Construction Simulator
+addappid(1273400, 1, "e312b76595b21d01382d54e8ecda7ac8f0744a64274fec9114a01086d825b927") --Mainappid Construction Simulator
+addappid(1273401, 1, "7ac6b6d000ec93fdd41c708fdaa554b3b457d5e9c07bb1947f2d555502c12b28") --Main Windows Depot Construction Simulator
+setManifestid(1273401, "3537032707906149374", 11269727760)
+--Dlcname Construction Simulator - Car & Bobblehead Pack
+addappid(1944340, 1, "9ada72090f09e32fe5de61dc7f3d27dfa65bbf00c10951f5e171fef1d54467f4") --Dlcname Construction Simulator - Car & Bobblehead Pack
+setManifestid(1944340, "322555549106022213", 48)
+--Dlcname Construction Simulator - Customization Kit
+addappid(1944341, 1, "fff00853888f489491f77e33f4b0abfdb9e4f274f7abb2db7e4a473c75710ad2") --Dlcname Construction Simulator - Customization Kit
+setManifestid(1944341, "8807678063382063768", 48)
+--Dlcname Construction Simulator - Official Guide
+addappid(2153870) --Dlcname Construction Simulator - Official Guide
+addappid(2153871, 1, "35ec66577bad165531ed3faa7d57db174dc5c411d7fa334cfac933a406119c7b") --Dlc Windows Language Depot German Construction Simulator - Official Guide
+setManifestid(2153871, "8605968922934877579", 29105136)
+addappid(2153872, 1, "8d457689bea1359a61b8bc676238db49b96aec8e56ad39efd92741e9d2db04a6") --Dlc Windows Depot Construction Simulator - Official Guide
+setManifestid(2153872, "425545223924338255", 36947952)
+--Dlcname Construction Simulator - Kramer Pack
+addappid(2203420, 1, "936a535f06583e664a84071a3e5ef1442c263230dfccb079ed73cf6b1632531f") --Dlcname Construction Simulator - Kramer Pack
+setManifestid(2203420, "8739100311331388793", 48)
+--Dlcname Construction Simulator - JCB Pack
+addappid(2291420) --Dlcname Construction Simulator - JCB Pack
+addappid(2291424, 1, "aa1e4588ba76e6e1723d055f0ac651747fd4f70363d870bfd3bfcc0a9b0641d6") --Dlc Windows Depot Construction Simulator - JCB Pack
+setManifestid(2291424, "8453066766368453847", 48)
+--Dlcname Construction Simulator - Airfield Expansion
+addappid(2295780) --Dlcname Construction Simulator - Airfield Expansion
+addappid(2295781, 1, "e934f73594337e0e6026594ced95237670b45cb3b0a4a8afe5afb168cf7c96b9") --Dlc Windows Depot Construction Simulator - Airfield Expansion
+setManifestid(2295781, "685349866155445181", 48)
+--Dlcname Construction Simulator - SANY Pack
+addappid(2295810) --Dlcname Construction Simulator - SANY Pack
+addappid(2295811, 1, "81149cf5bfb357c4d5e9ca09e28b9bb871218513bf57671f45b4db19322e3987") --Dlc Windows Depot Construction Simulator - SANY Pack
+setManifestid(2295811, "3383914678723808175", 48)
+--Dlcname Construction Simulator - Spaceport Expansion
+addappid(2295820) --Dlcname Construction Simulator - Spaceport Expansion
+addappid(2295821, 1, "0238a85d92c1efd94f87882d80f94f274a9642622803775ac612597cc3900a37") --Dlc Windows Depot Construction Simulator - Spaceport Expansion
+setManifestid(2295821, "8580441773329478911", 48)
+--Dlcname Construction Simulator - Cosmetic Pack #1
+addappid(2322970) --Dlcname Construction Simulator - Cosmetic Pack #1
+addappid(2322971, 1, "e259ebb0698e11fea131e2432dc3fb90a1deb703381e24c6f86da0a5b873260c") --Dlc Windows Depot Construction Simulator - Cosmetic Pack #1
+setManifestid(2322971, "320280581201002583", 48)
+--Dlcname Construction Simulator - Cosmetic Pack #2
+addappid(2322980) --Dlcname Construction Simulator - Cosmetic Pack #2
+addappid(2322981, 1, "5b18f6aa61fc834076274cf99d042aaf7a103e39ae6c2e916b2ca6c157969162") --Dlc Windows Depot Construction Simulator - Cosmetic Pack #2
+setManifestid(2322981, "5743210225651998743", 48)
+--Dlcname Construction Simulator - Year 1 Season Pass Helmet
+addappid(2322990) --Dlcname Construction Simulator - Year 1 Season Pass Helmet
+addappid(2322991, 1, "e6cef62d582f8932a2108656f1f9bd325b154bfbfd77bc4e334d201f50bd367f") --Dlc Windows Depot Construction Simulator - Year 1 Season Pass Helmet
+setManifestid(2322991, "7520794540910599885", 48)
+--Dlcname Construction Simulator - Liebherr Pack
+addappid(2657380) --Dlcname Construction Simulator - Liebherr Pack
+addappid(2657381, 1, "1197196a9b63ed09d0fe34a639f3613f84174fd77cd34ebd8c94092c1618a6ef") --Dlc Windows Depot Construction Simulator - Liebherr Pack
+setManifestid(2657381, "6016383179637761921", 48)
+--Dlcname Construction Simulator - Stadium Expansion
+addappid(2657390) --Dlcname Construction Simulator - Stadium Expansion
+addappid(2657391, 1, "57b63b80e8017b3ba57d81a99bf46013281784cf8337fae9f22c99188903283e") --Dlc Windows Depot Construction Simulator - Stadium Expansion
+setManifestid(2657391, "3197796519729678759", 48)
+--Dlcname Construction Simulator - Dynapac Pack
+addappid(2657400) --Dlcname Construction Simulator - Dynapac Pack
+addappid(2657401, 1, "f4203866785afe4f52c192b96551976c3921fdb35b2b4c82cc2129b0d0a4f46c") --Dlc Windows Depot Construction Simulator - Dynapac Pack
+setManifestid(2657401, "8114831874000182204", 48)
+--Dlcname Construction Simulator - DAF & MEILLER Pack
+addappid(2657410) --Dlcname Construction Simulator - DAF & MEILLER Pack
+addappid(2657411, 1, "6d0061174c36857cf29b71b91831fc9e2ae1d6b0e8e1095248975c2f7c29c28a") --Dlc Windows Depot Construction Simulator - DAF & MEILLER Pack
+setManifestid(2657411, "3557864438957203195", 48)
+--Dlcname Construction Simulator - Year 2 Season Pass Pickup Truck
+addappid(2657420) --Dlcname Construction Simulator - Year 2 Season Pass Pickup Truck
+addappid(2657421, 1, "44b4871b93c1c2532a5d33b57cba0f4741d9a76c64cb63c4678044b9d8eec00a") --Dlc Windows Depot Construction Simulator - Year 2 Season Pass Pickup Truck
+setManifestid(2657421, "8791015684642513912", 48)
+--Dlcname Construction Simulator - Cosmetic Pack #3
+addappid(2800780) --Dlcname Construction Simulator - Cosmetic Pack #3
+addappid(2800781, 1, "e4fd402d9c5abf02cceaad103495cd1c30d7fe3c777d312788a808fe719e0701") --Dlc Windows Depot Construction Simulator - Cosmetic Pack #3
+setManifestid(2800781, "5044123825545457794", 48)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2323000) --Dlcname Construction Simulator - Year 1 Season Pass
+addappid(2666240) --Dlcname Construction Simulator - Year 2 Season Pass
