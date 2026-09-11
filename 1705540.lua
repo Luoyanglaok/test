3a99b5dc00e677f5acfd6495a120600a3b0d1bb9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 12:53:54 +0800

    add 1705540.lua

--- 1705540.lua
+++ 1705540.lua
@@ -1,3 +1,14 @@
-addappid(1705540)
-addappid(1705541,0,"87eea2c7f361da91b40be9df43ad7b3808556d67158485a394cdd4803f0d0306")
-setManifestid(1705541,"9073694816372111910")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1705540.lua
+--Gamename Dungeon Arena
+addappid(1705540) --Mainappid Dungeon Arena
+addappid(1705541, 1, "87eea2c7f361da91b40be9df43ad7b3808556d67158485a394cdd4803f0d0306") --Main Windows Depot Dungeon Arena
+setManifestid(1705541, "9073694816372111910", 5748848)
+addappid(1707910) --Dlcname Dungeon Arena - Class Ninja
+addappid(1707920) --Dlcname Dungeon Arena - Class Dancer
+addappid(1707921) --Dlcname Dungeon Arena - Class Bard
+addappid(1707922) --Dlcname Dungeon Arena - Class Gnome
+addappid(1707930) --Dlcname Dungeon Arena - Arena Snowy plain
+addappid(1707931) --Dlcname Dungeon Arena - Arena Alien planet
+addappid(1707932) --Dlcname Dungeon Arena - Arena Pirates
+addappid(1707933) --Dlcname Dungeon Arena - Arena King's palace
