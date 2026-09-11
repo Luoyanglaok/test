9bb5b20daeb2671baa437e06ce793072de1eb91c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:59:03 +0800

    add 938380.lua

--- 938380.lua
+++ 938380.lua
@@ -1,13 +1,18 @@
-addappid(938380)
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(228987)
-setManifestid(228987,"4302102680580581867")
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(229020)
-setManifestid(229020,"5799761707845834510")
-addappid(938381,0,"e2343bc799cc70578f3f833d189447740e9824c230493046aec23b8698c5b545")
-setManifestid(938381,"3407797103975419192")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 938380.lua
+--Gamename Townsmen - A Kingdom Rebuilt
+addappid(938380, 1, "ba2b69928c79529db66fa65ee1aba84c3fdf1b10a993bb796f40f4512570e690") --Mainappid Townsmen - A Kingdom Rebuilt
+addappid(938381, 1, "e2343bc799cc70578f3f833d189447740e9824c230493046aec23b8698c5b545") --Main Windows Depot Townsmen - A Kingdom Rebuilt
+setManifestid(938381, "3407797103975419192", 695109968)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
+addappid(1176670) --Dlcname Townsmen - A Kingdom Rebuilt: The Seaside Empire
