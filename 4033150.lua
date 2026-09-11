7818bce02c7f3de5eba15b05fb8e35d8cd0dbfcc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:20:08 +0800

    add 4033150.lua

--- 4033150.lua
+++ 4033150.lua
@@ -1,6 +1,2 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 4033150.lua
---Gamename もかラブReLive♪
-addappid(4033150, 1, "a62a63a72e7e1f706d61e3dd08cc5735965638e68079623e9595eff0ff5fb781") --Mainappid もかラブReLive♪
-addappid(4033151, 1, "fe31ea59809a1f1ee69c84de51b8b8d8a291bc222b12293b98407991fa4e30bf") --Main Windows Depot もかラブReLive♪
-setManifestid(4033151, "3451479663643172639", 211506960)
+addappid(4033150)
+setManifestid(4033151,"3451479663643172639")
