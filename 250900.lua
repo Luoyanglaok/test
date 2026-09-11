5d26b8c627ab4f4e04ab515bc9920665280fcb0e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 07:15:27 +0800

    add 250900.lua

--- 250900.lua
+++ 250900.lua
@@ -1,42 +1,16 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 250900.lua
---Gamename The Binding of Isaac: Rebirth
-addappid(250900, 1, "cb1c1ac5bf12aa887c898dcfcb962eca6ddb2dc729d5e49ee4e41bb24a95ea39") --Mainappid The Binding of Isaac: Rebirth
-addappid(250902, 1, "ad8a374561fd8769aac243917372237482caa7a94aafbcdaa45adc4208916b7a") --Main Windows Depot The Binding of Isaac: Rebirth
-setManifestid(250902, "4994611894646808503", 324246784)
-addappid(250903, 1, "40e55a223f52c70c427117df402eee6152118a11d9c17f28bc849252cde5337b") --Main Linux Depot The Binding of Isaac: Rebirth
-setManifestid(250903, "367036646469636831", 326571712)
-addappid(250904, 1, "210036fb298fd6b8c2d227c9a803614edb0a6e2d9949968b46321c25d298155d") --Main Macos Depot The Binding of Isaac: Rebirth
-setManifestid(250904, "1942665726573884808", 322282208)
-addappid(1426300, 1, "996de252a588d3f62ddd98a03e5880bb322e3b00b09b3f3997cb087908952a85") --Main Windows Depot The Binding of Isaac: Rebirth
---Dlcname The Binding of Isaac: Rebirth - Soundtrack
-addappid(322660, 1, "cc56b394152b0d78bb4d6e147716a463b70ae573e8cf6f2ef28ebe359f97860d") --Dlcname The Binding of Isaac: Rebirth - Soundtrack
-setManifestid(322660, "5499930042188336604", 349532032)
-addappid(322661, 1, "6c8f5e0423cd4e9f34a9ff9b9f4bc95464e3fd93f03bb699be87ec06402cefc0") --Dlc Windows Depot The Binding of Isaac: Rebirth - Soundtrack
-setManifestid(322661, "4078660603895799229", 186090208)
---Dlcname The Binding of Isaac: Afterbirth
-addappid(401920) --Dlcname The Binding of Isaac: Afterbirth
-addappid(250905, 1, "42e1eb359f37040d6a198d7b0aef616b100b9ac5405667f89c15aeaf217a47ed") --Dlc Windows Depot The Binding of Isaac: Afterbirth
-setManifestid(250905, "1709017229885880564", 164204560)
-addappid(250906, 1, "d83d1ccc6ec53ebb8724d9dbaa3fd1b7d417fef3dcf9f9260d5fcd49b3dc771e") --Dlc Linux Depot The Binding of Isaac: Afterbirth
-setManifestid(250906, "274330258716671855", 168261968)
-addappid(250907, 1, "748829017a20ec0570ef232ad6b5b12539ebe511fcf85c494ac7856e5015e48a") --Dlc Macos Depot The Binding of Isaac: Afterbirth
-setManifestid(250907, "1258401244940465981", 175833872)
---Dlcname The Binding of Isaac: Afterbirth+
-addappid(570660, 1, "b6faa9c559ffe718a3bc7b046e5a5e8268961c50e6a6e1561cb9441405f098d9") --Dlcname The Binding of Isaac: Afterbirth+
-addappid(250908, 1, "5a6d9cc4f7864cadb061e3c6f91f8c3f2ee7509f31db0c926f7c4066ff737366") --Dlc Windows Depot The Binding of Isaac: Afterbirth+
-setManifestid(250908, "7333987924869605149", 118565376)
-addappid(250909, 1, "80ee47ebd3ec21428656ac288e189aeb671034dbed479347ac2b553f822d5899") --Dlc Linux Depot The Binding of Isaac: Afterbirth+
-setManifestid(250909, "5041024818858406088", 115206208)
-addappid(250910, 1, "6624d0874bc231a99b0f107bd111eaaaac393405c68773a6641e4e82f1232172") --Dlc Macos Depot The Binding of Isaac: Afterbirth+
-setManifestid(250910, "1079085999450784617", 115829936)
---Dlcname The Binding of Isaac: Repentance
-addappid(250911, 1, "91f532dc0f4bd27b98705c41e9c5b1171df5c8254c92409b9faf40bbe8708639") --Dlc Windows Depot The Binding of Isaac: Repentance
-setManifestid(250911, "7652847940910762229", 599198896)
---Dlcname The Binding of Isaac: Repentance+
-addappid(3353470) --Dlcname The Binding of Isaac: Repentance+
-addappid(3353471, 1, "b1b260b7116c50497bfe54b1df79bc30dbca77c3d8d7d2698f96657fdb2829e8") --Dlc Windows Depot The Binding of Isaac: Repentance+
-setManifestid(3353471, "229910742625134068", 694662480)
---Share Depots
-addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
-setManifestid(228986, "8782296191957114623", 23045488)
+addappid(250900)
+addappid(3353470)
+addappid(1426300)
+addappid(401920)
+addappid(570660)
+addappid(322660)
+addappid(250902,1,"ad8a374561fd8769aac243917372237482caa7a94aafbcdaa45adc4208916b7a")
+addappid(250905,1,"42e1eb359f37040d6a198d7b0aef616b100b9ac5405667f89c15aeaf217a47ed")
+addappid(250908,1,"5a6d9cc4f7864cadb061e3c6f91f8c3f2ee7509f31db0c926f7c4066ff737366")
+addappid(250911,1,"91f532dc0f4bd27b98705c41e9c5b1171df5c8254c92409b9faf40bbe8708639")
+addappid(228986,1,"51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5")
+setManifestid(250902,"4994611894646808503")
+setManifestid(250905,"1709017229885880564")
+setManifestid(250908,"7333987924869605149")
+setManifestid(250911,"7652847940910762229")
+setManifestid(228986,"8782296191957114623")
