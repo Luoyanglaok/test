8bbc650a9741e2a205f3b8152bf1fbe1179cf52c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:56:54 +0800

    add 1829980.lua

--- 1829980.lua
+++ 1829980.lua
@@ -1,3 +1,6 @@
-addappid(1829980)
-addappid(1829981,0,"bb26c855c621e80e9adfa9262b4e8c01d781f9710a03756159fd699a0e64c264")
-setManifestid(1829981,"9039669875738497158")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1829980.lua
+--Gamename Café Stella and the Reaper's Butterflies
+addappid(1829980, 1, "8476ca5f8c7ab6108b5d3c0efcc6c4130631c2944aa438bb57b0cdcc0bebb939") --Mainappid Café Stella and the Reaper's Butterflies
+addappid(1829981, 1, "bb26c855c621e80e9adfa9262b4e8c01d781f9710a03756159fd699a0e64c264") --Main Windows Depot Café Stella and the Reaper's Butterflies
+setManifestid(1829981, "9039669875738497158", 6767314128)
