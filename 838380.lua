4ec28ac9f4902d7a67f2e2c48c24cfe9994b07ab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:21:18 +0800

    add 838380.lua

--- 838380.lua
+++ 838380.lua
@@ -1,7 +1,108 @@
-addappid(838380)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(838381,0,"92fb6d22798bdbc124409edb818c41c9c3fd9d61ecbe63da822938138e9c402f")
-setManifestid(838381,"4735425669633752101")
-addappid(838382,0,"260f44b05ec60bbf5e68d67ddd4ae78e648bb292eae12f9da97193fc00d5ffbe")
-setManifestid(838382,"574111364706793777")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 838380.lua
+--Gamename DEAD OR ALIVE 6
+addappid(838380, 1, "3954db49a58b078fc001215df5238df61508f6378227a2850de379c95c8aaddb") --Mainappid DEAD OR ALIVE 6
+addappid(838381, 1, "92fb6d22798bdbc124409edb818c41c9c3fd9d61ecbe63da822938138e9c402f") --Main Windows Depot DEAD OR ALIVE 6
+setManifestid(838381, "4735425669633752101", 47329273056)
+addappid(838382, 1, "260f44b05ec60bbf5e68d67ddd4ae78e648bb292eae12f9da97193fc00d5ffbe") --Main Windows Depot DEAD OR ALIVE 6
+setManifestid(838382, "574111364706793777", 33464656)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1033460) --Dlcname DOA6 Character: Ayane
+addtoken(1033460, "13360269898889445960")
+addappid(1033470) --Dlcname DOA6 Character: Tina
+addtoken(1033470, "15238142599178128341")
+addappid(1033471) --Dlcname DOA6 Character: Leifang
+addtoken(1033471, "5361349451755515067")
+addappid(1033472) --Dlcname DOA6 Character: Helena
+addtoken(1033472, "9045571180509599884")
+addappid(1033473) --Dlcname DOA6 Character: Christie
+addtoken(1033473, "5439780443710653218")
+addappid(1033474) --Dlcname DOA6 Character: Kokoro
+addtoken(1033474, "15499212216936538789")
+addappid(1033475) --Dlcname DOA6 Character: La Mariposa
+addtoken(1033475, "17663208581071504597")
+addappid(1033476) --Dlcname DOA6 Character: Mila
+addtoken(1033476, "7740829313818471305")
+addappid(1033477) --Dlcname DOA6 Character: Marie Rose
+addtoken(1033477, "12771691015090539907")
+addappid(1033478) --Dlcname DOA6 Character: Honoka
+addtoken(1033478, "8959876260361863512")
+addappid(1033479) --Dlcname DOA6 Character: NiCO
+addtoken(1033479, "12509055989826031019")
+addappid(1033480) --Dlcname DOA6 Character: Hayabusa
+addtoken(1033480, "8305488345013854960")
+addappid(1033481) --Dlcname DOA6 Character: Hayate
+addtoken(1033481, "15785577573015868638")
+addappid(1033482) --Dlcname DOA6 Character: Jann Lee
+addtoken(1033482, "1640183750935869019")
+addappid(1033483) --Dlcname DOA6 Character: Zack
+addtoken(1033483, "12274165726822007132")
+addappid(1033484) --Dlcname DOA6 Character: Bayman
+addtoken(1033484, "5130655573315924963")
+addappid(1033485) --Dlcname DOA6 Character: Brad Wong
+addtoken(1033485, "16375660702228433484")
+addappid(1033486) --Dlcname DOA6 Character: Eliot
+addtoken(1033486, "16674356462863386661")
+addappid(1033487) --Dlcname DOA6 Character: Rig
+addtoken(1033487, "13341369112030840651")
+addappid(1033488) --Dlcname DOA6 Character: Raidou
+addtoken(1033488, "3022472106970559785")
+addappid(1033489) --Dlcname DEAD OR ALIVE 6: Core Fighters 20 Character Set
+addtoken(1033489, "15000446146281457064")
+addappid(1033490) --Dlcname DOA6 Season Pass 1
+addappid(1033491) --Dlcname DOA6 Season Pass 1 Bonus Costume: NiCO
+addtoken(1033491, "14124309683048313497")
+addappid(1033492) --Dlcname DOA6 Season Pass 1 Bonus Costume: Nyotengu
+addtoken(1033492, "15443703180519279047")
+addappid(1034470) --Dlcname DOA6 Story Unlock Key
+addtoken(1034470, "12926584720767900251")
+addappid(1034471) --Dlcname DOA6 Character: Nyotengu
+addtoken(1034471, "6158735455868223595")
+addappid(1034480) --Dlcname DOA6 Character: Phase4
+addtoken(1034480, "8946727556844140187")
+addappid(1034481) --Dlcname DOA6 Early Purchase Bonus Costume - Kasumi
+addtoken(1034481, "11326258888400566827")
+addappid(1034482) --Dlcname DOA6 Digital Deluxe Edition Bonus Costume - Kasumi
+addtoken(1034482, "17136858179616171006")
+addappid(1034483) --Dlcname DOA6 Deluxe BGM (3 tracks)
+addtoken(1034483, "4490832916617145503")
+addappid(1034484) --Dlcname DOA6 Deluxe Costume Set
+addtoken(1034484, "9382758591433291531")
+addappid(1042850) --Dlcname DOA6 Happy Wedding Costume Vol.1 Set
+addappid(1042851) --Dlcname DOA6 Happy Wedding Costume Vol.2 Set
+addappid(1048380) --Dlcname DEAD OR ALIVE 6: Core Fighters - Male Fighters Set
+addappid(1048381) --Dlcname DEAD OR ALIVE 6: Core Fighters - Female Fighters Set
+addappid(1054330) --Dlcname DEAD OR ALIVE 6 - Full Game
+addtoken(1054330, "9632757680028342101")
+addappid(1058120) --Dlcname DOA6 Pirates of the 7 Seas Costumes Vol.1 Set
+addappid(1058121) --Dlcname DOA6 Pirates of the 7 Seas Costumes Vol.2 Set
+addappid(1074260) --Dlcname DOA6 Character: Mai Shiranui
+addappid(1074261) --Dlcname DOA6 Happy Wedding Costume - Mai Shiranui
+addappid(1074262) --Dlcname DOA6 Pirates of the 7 Seas Costume - Mai Shiranui
+addappid(1074263) --Dlcname DOA6 Nurse Costume - Mai Shiranui
+addappid(1074264) --Dlcname DOA6 Maid Costume - Mai Shiranui
+addappid(1074265) --Dlcname DOA6 Bunny Costume - Mai Shiranui
+addappid(1074266) --Dlcname DOA6 Mai Shiranui Debut Costume Set
+addappid(1074267) --Dlcname DOA6 Mai Shiranui + Debut Costume Set
+addappid(1074270) --Dlcname DOA6 Character: Kula Diamond
+addappid(1074271) --Dlcname DOA6 Happy Wedding Costume - Kula Diamond
+addappid(1074272) --Dlcname DOA6 Pirates of the 7 Seas Costume - Kula Diamond
+addappid(1074273) --Dlcname DOA6 Nurse Costume - Kula Diamond
+addappid(1074274) --Dlcname DOA6 Maid Costume - Kula Diamond
+addappid(1074275) --Dlcname DOA6 Bunny Costume - Kula Diamond
+addappid(1074276) --Dlcname DOA6 Kula Diamond Debut Costume Set
+addappid(1074277) --Dlcname DOA6 Kula Diamond + Debut Costume Set
+addappid(1092291) --Dlcname DOA6 THE KING OF FIGHTERS XIV Mashup Content Set
+addappid(1099190) --Dlcname DOA6 Happy Wedding Costume Vol.1 - Kasumi
+addappid(1099191) --Dlcname DOA6 Happy Wedding Costume Vol.1 - Kokoro
+addappid(1099200) --Dlcname DOA6 Happy Wedding Costume Vol.1 - Leifang
+addappid(1099201) --Dlcname DOA6 Happy Wedding Costume Vol.1 - Hitomi
+addappid(1099202) --Dlcname DOA6 Happy Wedding Costume Vol.1 - Marie Rose
+addappid(1099203) --Dlcname DOA6 Happy Wedding Costume Vol.1 - Nyotengu
+addappid(1099210) --Dlcname DOA6 Happy Wedding Costume Vol.1 - NiCO
+addappid(1099211) --Dlcname DOA6 Happy Wedding Costume Vol.1 - Jann Lee
+addappid(1099212) --Dlcname DOA6 Happy Wedding Costume Vol.1 - Hayabusa
