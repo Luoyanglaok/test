79635cdfdbcd553818845256e03ff04f7122a01a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:18:32 +0800

    add 525510.lua

--- 525510.lua
+++ 525510.lua
@@ -1,5 +1,8 @@
-addappid(525510)
-addappid(525512,0,"bf4bc2a7d03327bcb1bc2e7889348c38818eb196785c6f684bdba8c986fa366e")
-setManifestid(525512,"6384266413431872935")
-addappid(525513,0,"69f77227ffbf3c0b3cfeacc62b69818e74849f267d4a503bd08850c15951eb60")
-setManifestid(525513,"299401870113337811")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 525510.lua
+--Gamename Guns, Gore and Cannoli 2
+addappid(525510) --Mainappid Guns, Gore and Cannoli 2
+addappid(525512, 1, "bf4bc2a7d03327bcb1bc2e7889348c38818eb196785c6f684bdba8c986fa366e") --Main Windows Depot Guns, Gore and Cannoli 2
+setManifestid(525512, "6384266413431872935", 1357646688)
+addappid(525513, 1, "69f77227ffbf3c0b3cfeacc62b69818e74849f267d4a503bd08850c15951eb60") --Main Macos Depot Guns, Gore and Cannoli 2
+setManifestid(525513, "299401870113337811", 1353183712)
