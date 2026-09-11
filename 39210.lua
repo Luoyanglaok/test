9b69229084ff9fab8457a63b7cbf4b2654e9f2b8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:47:56 +0800

    add 39210.lua

--- 39210.lua
+++ 39210.lua
@@ -1,3 +1,128 @@
-addappid(39210)
-addappid(39211,0,"d0c623233763e6f93f011efe694adc0da08240833523dce4dd7c33452faefd98")
-setManifestid(39211,"8665979780740027235")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 39210.lua
+--Gamename FINAL FANTASY XIV Online
+addappid(39210, 1, "c61ef3c549c9baf199512ffc455c3f14fd8ddac725cc5f3630085ff481140239") --Mainappid FINAL FANTASY XIV Online
+addappid(39211, 1, "d0c623233763e6f93f011efe694adc0da08240833523dce4dd7c33452faefd98") --Main Windows Depot FINAL FANTASY XIV Online
+setManifestid(39211, "8665979780740027235", 15993216)
+--Dlcname Before Meteor: FINAL FANTASY XIV Original Soundtrack
+addappid(3372220) --Dlcname Before Meteor: FINAL FANTASY XIV Original Soundtrack
+addappid(3372221, 1, "be64eda4f2dc8f9b44b54e3bf49be5687cbf0079ff96b70b985c1045551c460f") --Dlc Windows Depot Before Meteor: FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372221, "5299962713414247375", 814190960)
+--Dlcname A REALM REBORN : FINAL FANTASY XIV Original Soundtrack
+addappid(3372240) --Dlcname A REALM REBORN : FINAL FANTASY XIV Original Soundtrack
+addappid(3372241, 1, "f202db7298fe0db750b8841b286d6fec20ad24822a2488a62a9f4377046f61d9") --Dlc Windows Depot A REALM REBORN : FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372241, "2437015434136078112", 893639696)
+--Dlcname Before the Fall: FINAL FANTASY XIV Original Soundtrack
+addappid(3372250) --Dlcname Before the Fall: FINAL FANTASY XIV Original Soundtrack
+addappid(3372251, 1, "32b6fe84c9cc1b0f68156bd4c955afc916ff3a5eaef623fa80c70a5ae8808b84") --Dlc Windows Depot Before the Fall: FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372251, "6685731106221800785", 557638512)
+--Dlcname Heavensward: FINAL FANTASY XIV Original Soundtrack
+addappid(3372260) --Dlcname Heavensward: FINAL FANTASY XIV Original Soundtrack
+addappid(3372261, 1, "c955bb15682d3473e4fb5e1f183e666f41524a0c75ee4d7fffa153c3a3dcc5f1") --Dlc Windows Depot Heavensward: FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372261, "2294518922579656109", 624976640)
+--Dlcname THE FAR EDGE OF FATE:FINAL FANTASY XIV Original Soundtrack
+addappid(3372270) --Dlcname THE FAR EDGE OF FATE:FINAL FANTASY XIV Original Soundtrack
+addappid(3372271, 1, "fe42766b6819fb450db2a2b579f62d1d1bbd1fe3c759951d4b9c15c191c7b95e") --Dlc Windows Depot THE FAR EDGE OF FATE:FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372271, "1235280584580413088", 474259296)
+--Dlcname STORMBLOOD: FINAL FANTASY XIV Original Soundtrack
+addappid(3372280) --Dlcname STORMBLOOD: FINAL FANTASY XIV Original Soundtrack
+addappid(3372281, 1, "238914f54210c1e50aa7a04bc4cda8df17cda8a00a464d52dffa505e0d8a7846") --Dlc Windows Depot STORMBLOOD: FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372281, "7616212957150627920", 800575456)
+--Dlcname SHADOWBRINGERS: FINAL FANTASY XIV Original Soundtrack
+addappid(3372290) --Dlcname SHADOWBRINGERS: FINAL FANTASY XIV Original Soundtrack
+addappid(3372291, 1, "8fe472261d51ec3029c2b708f37a2fa96218875d6defbf377308c1823b4a5829") --Dlc Windows Depot SHADOWBRINGERS: FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372291, "8249913286899729272", 808117488)
+--Dlcname DEATH UNTO DAWN: FINAL FANTASY XIV Original Soundtrack
+addappid(3372300) --Dlcname DEATH UNTO DAWN: FINAL FANTASY XIV Original Soundtrack
+addappid(3372301, 1, "f4e83bdab52983439158ed3757c976f72bd0857222842bfa797d808715e4ea04") --Dlc Windows Depot DEATH UNTO DAWN: FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372301, "3841455640523541432", 742761712)
+--Dlcname ENDWALKER: FINAL FANTASY XIV Original Soundtrack
+addappid(3372310) --Dlcname ENDWALKER: FINAL FANTASY XIV Original Soundtrack
+addappid(3372311, 1, "f8b31fc992717147e3a460e85ac13c58c34d9d1c56d319178cbec630f7de4ebd") --Dlc Windows Depot ENDWALKER: FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372311, "1868241463138512796", 613401968)
+--Dlcname GROWING LIGHT: FINAL FANTASY XIV Original Soundtrack
+addappid(3372320) --Dlcname GROWING LIGHT: FINAL FANTASY XIV Original Soundtrack
+addappid(3372321, 1, "c216cb62410d5b9602ebe6e99742d5db16dff3aed6f670b97723c833b3f48b02") --Dlc Windows Depot GROWING LIGHT: FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372321, "4971628679217837777", 872341440)
+--Dlcname DAWNTRAIL: FINAL FANTASY XIV Original Soundtrack
+addappid(3372330) --Dlcname DAWNTRAIL: FINAL FANTASY XIV Original Soundtrack
+addappid(3372331, 1, "cdb17111854df71a0876717ddccaf0a9ae3155e49d71e745fae1036fb159df8d") --Dlc Windows Depot DAWNTRAIL: FINAL FANTASY XIV Original Soundtrack
+setManifestid(3372331, "9119777665321781675", 636573440)
+addappid(262340) --Dlcname FINAL FANTASY XIV Online Starter Edition (JP)
+addappid(262341) --Dlcname FINAL FANTASY XIV Online Starter Edition (NA)
+addappid(262342) --Dlcname FINAL FANTASY XIV Online Starter Edition (PAL)
+addappid(267510) --Dlcname ファイナルファンタジーXIV: 新生エオルゼア CE (JP version)
+addappid(267511) --Dlcname FINAL FANTASY XIV: A Realm Reborn CE (NA version)
+addappid(267512) --Dlcname FINAL FANTASY XIV: A Realm Reborn CE (PAL version)
+addappid(359480) --Dlcname FINAL FANTASY XIV: Heavensward
+addappid(376010) --Dlcname FINAL FANTASY XIV: Heavensward - Standard Edition
+addappid(376011) --Dlcname FINAL FANTASY XIV: Heavensward - Collector's Edition
+addappid(376020) --Dlcname FINAL FANTASY XIV: Heavensward - Pre-purchase Bonus Content
+addappid(582380) --Dlcname FINAL FANTASY XIV: Stormblood
+addappid(582381) --Dlcname AppID 582381
+addtoken(582381, "860119013166593544")
+addappid(582382) --Dlcname AppID 582382
+addappid(596480) --Dlcname AppID 596480
+addappid(1016870) --Dlcname AppID 1016870
+addtoken(1016870, "12900292854322353734")
+addappid(1016871) --Dlcname AppID 1016871
+addtoken(1016871, "868237004055700247")
+addappid(1016872) --Dlcname AppID 1016872
+addtoken(1016872, "17901663096186668832")
+addappid(1016873) --Dlcname AppID 1016873
+addtoken(1016873, "291415382909089227")
+addappid(1592500) --Dlcname AppID 1592500
+addtoken(1592500, "10366468884747889829")
+addappid(1592510) --Dlcname AppID 1592510
+addtoken(1592510, "10968808886194957886")
+addappid(1592511) --Dlcname AppID 1592511
+addtoken(1592511, "14326895339431661425")
+addappid(1592512) --Dlcname AppID 1592512
+addtoken(1592512, "12731267973603691771")
+addappid(1939460) --Dlcname FINAL FANTASY XIV Online Complete Edition (JP)
+addappid(1939461) --Dlcname FINAL FANTASY XIV Online Complete Edition (NA)
+addappid(1939462) --Dlcname FINAL FANTASY XIV Online Complete Edition (PAL)
+addappid(2649240) --Dlcname FINAL FANTASY XIV: Dawntrail
+addappid(2649250) --Dlcname AppID 2649250
+addtoken(2649250, "7738449258934532724")
+addappid(2649260) --Dlcname AppID 2649260
+addtoken(2649260, "11269238478097573012")
+addappid(2649270) --Dlcname AppID 2649270
+addtoken(2649270, "10842003436864836722")
+addappid(2649330) --Dlcname FINAL FANTASY XIV Online Complete Edition (EX5 JP)
+addappid(2649340) --Dlcname FINAL FANTASY XIV Online Complete Edition (EX5 NA)
+addtoken(2649340, "3755804144696363366")
+addappid(2649350) --Dlcname FINAL FANTASY XIV Online Complete Edition (EX5 PAL)
+--Missing Dlcs
+--Dlcname FINAL FANTASY XIV Online (NA)
+--Dlcname FINAL FANTASY XIV Online (JP)
+--Dlcname FINAL FANTASY XIV Online (PAL)
+--Dlcname THE PRIMALS
+--Dlcname THE PRIMALS Zepp Tour 2018 - Trial By Shadow
+--Dlcname THE PRIMALS Live in Japan - Beyond the Shadow
+--Dlcname THE PRIMALS - Riding Home
+--Dlcname THE PRIMALS Live in Japan - Darkest Before Dawn
+--Dlcname THE PRIMALS - Dark Decades
+--Dlcname Pulse: FINAL FANTASY XIV Remix Album Vol. 2 (Session 1)
+--Dlcname Pulse: FINAL FANTASY XIV Remix Album Vol. 2 (Session 2)
+--Dlcname From Astral to Umbral: FINAL FANTASY XIV 〜 Arrangement Album 〜
+--Dlcname FINAL FANTASY XIV Duality ～ Arrangement Album ～
+--Dlcname Eorzean Symphony: FINAL FANTASY XIV Orchestral Album
+--Dlcname Eorzean Symphony: FINAL FANTASY XIV Orchestral Album (Concert version)
+--Dlcname Journeys: FINAL FANTASY XIV ～ Arrangement Album ～
+--Dlcname Eorzean Symphony: FINAL FANTASY XIV Orchestral Album Vol. 2
+--Dlcname Eorzean Symphony: FINAL FANTASY XIV Orchestral Album Vol. 2 (Concert version)
+--Dlcname Pulse: FINAL FANTASY XIV Remix Album
+--Dlcname Scions & Sinners: FINAL FANTASY XIV ～ Arrangement Album ～
+--Dlcname Sanctuary's Heart: FINAL FANTASY XIV Chill Arrangement Album
+--Dlcname Eorzean Symphony: FINAL FANTASY XIV Orchestral Album Vol. 3
+--Dlcname Eorzean Symphony: FINAL FANTASY XIV Orchestral Album Vol. 3 (Concert version)
+--Dlcname Sanctuary's Heart: FINAL FANTASY XIV Chill Arrangement Album (B-sides)
+--Dlcname Forge Ahead: FINAL FANTASY XIV ～ Arrangement Album ～
+--Dlcname And Back Again: Live Performances from the FINAL FANTASY XIV Fan Festival 2024
+--Dlcname Pulse: FINAL FANTASY XIV Remix Album Vol. 2 (B-sides)
+--Dlcname Piano Collections FINAL FANTASY XIV
+--Dlcname THE PRIMALS Dark Decades Tour BUDOKAN
+--Dlcname Eorzean Symphony: FINAL FANTASY XIV Orchestral Album Vol. 4
+--Dlcname Eorzean Symphony: FINAL FANTASY XIV Orchestral Album Vol. 4 (Concert version)
+--Dlcname THE PRIMALS - Back in the Ring
