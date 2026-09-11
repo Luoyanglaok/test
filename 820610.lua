9dca073365a69e79081c056d37f70d5809c53108
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:39:17 +0800

    add 820610.lua

--- 820610.lua
+++ 820610.lua
@@ -1,4 +1,6 @@
-addappid(820610)
-addappid(820611,0,"80ff45eeeb4ad0ad882e59950dd4c351bd8303c390f6ceb085a05764818b79c0")
-setManifestid(820611,"8022253782055468892")
-addappid(820612)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 820610.lua
+--Gamename Ages of Mages: The last keeper
+addappid(820610) --Mainappid Ages of Mages: The last keeper
+addappid(820611, 1, "80ff45eeeb4ad0ad882e59950dd4c351bd8303c390f6ceb085a05764818b79c0") --Main Windows Depot Ages of Mages: The last keeper
+setManifestid(820611, "8022253782055468892", 719617648)
