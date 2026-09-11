9d02abb1d4773fcfc4f9240908a28012b53c919e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:08:42 +0800

    add 2798330.lua

--- 2798330.lua
+++ 2798330.lua
@@ -1,1 +1,6 @@
-addappid(2798330)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2798330.lua
+--Gamename Dungeon Settlers
+addappid(2798330) --Mainappid Dungeon Settlers
+addappid(2798331, 1, "e9e6cfc71635a146bb6da13c0f181d3466b7ddcd63665ebf1138d568ca5b5622") --Main Windows Depot Dungeon Settlers
+setManifestid(2798331, "1347387287156377341", 547651312)
