296566e4ba6febc55fa1724e50a06a5322daff29
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:58:34 +0800

    add 1362010.lua

--- 1362010.lua
+++ 1362010.lua
@@ -1,3 +1,6 @@
-addappid(1362010)
-addappid(1362011,0,"e00549b0f005f864bcc508cbd627167c8663d19524ff07cba94759f5fee8ab84")
-setManifestid(1362011,"3570371357466796270")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1362010.lua
+--Gamename SEX, Drugs and CYBERPUNK
+addappid(1362010) --Mainappid SEX, Drugs and CYBERPUNK
+addappid(1362011, 1, "e00549b0f005f864bcc508cbd627167c8663d19524ff07cba94759f5fee8ab84") --Main Windows Depot SEX, Drugs and CYBERPUNK
+setManifestid(1362011, "3570371357466796270", 3248091024)
