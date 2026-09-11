f79f8796941cf6a7692eede5c96dc5aa276c3285
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:11:08 +0800

    add 2749000.lua

--- 2749000.lua
+++ 2749000.lua
@@ -1,3 +1,6 @@
-addappid(2749000)
-addappid(2749001,0,"867b444c2eb8519774232a1a354854de141eede681d32d518f61f766c501df4a")
-setManifestid(2749001,"7705132234326123143")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2749000.lua
+--Gamename Inn Tycoon
+addappid(2749000) --Mainappid Inn Tycoon
+addappid(2749001, 1, "867b444c2eb8519774232a1a354854de141eede681d32d518f61f766c501df4a") --Main Windows Depot Inn Tycoon
+setManifestid(2749001, "7735248895170448840", 2793448624)
