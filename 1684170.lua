8ef7165665d1c1b7d8d36acb045f79c4fe07efdf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 03:02:35 +0800

    add 1684170.lua

--- 1684170.lua
+++ 1684170.lua
@@ -1,5 +1,15 @@
-addappid(1684170)
-addappid(1684171,0,"9e8c76fd782e5ae2fdc981a6fb4713ac041aa58509de543ba6843fe56b6422c2")
-setManifestid(1684171,"2404973488917024007")
-addappid(1805600,0,"de244818877047e116733eb4a3b7968055ffb5e17384448940eb6b80159e5d3f")
-setManifestid(1805600,"7277906200917373012")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1684170.lua
+--Gamename Fetish Locator Week Two
+addappid(1684170, 1, "29f58ddbb7056b36e812e7282d5745d49fde256abf8d5473f32b9f0a5de7d5a1") --Mainappid Fetish Locator Week Two
+addappid(1684171, 1, "9e8c76fd782e5ae2fdc981a6fb4713ac041aa58509de543ba6843fe56b6422c2") --Main Windows Depot Fetish Locator Week Two
+setManifestid(1684171, "4521078807838460332", 4931833472)
+--Dlcname Fetish Locator Week Two Soundtrack
+addappid(1798440) --Dlcname Fetish Locator Week Two Soundtrack
+addappid(1798441, 1, "3ea0861ed6a367ee7b24c9f69973121efdf6c0210e3f8b273e7082f6eac5ca4e") --Dlc Windows Depot Fetish Locator Week Two Soundtrack
+setManifestid(1798441, "5018414049401672869", 73158352)
+addappid(1798442, 1, "b8ada4218c4ba707180e3fdd1fe2232b8a7180575d7182e49d10b6b7df668910") --Dlc Windows Depot Fetish Locator Week Two Soundtrack
+setManifestid(1798442, "2748830488439643145", 256887392)
+--Dlcname Fetish Locator Week Two - Walkthrough DLC
+addappid(1805600, 1, "de244818877047e116733eb4a3b7968055ffb5e17384448940eb6b80159e5d3f") --Dlcname Fetish Locator Week Two - Walkthrough DLC
+setManifestid(1805600, "7277906200917373012", 257616)
