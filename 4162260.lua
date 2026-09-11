febc0842ff66345e1d67cae5d9b39e1963d7780f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:33:50 +0800

    add 4162260.lua

---
+++ 4162260.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4162260.lua
+--Gamename Pizza Man Simulator
+addappid(4162260) --Mainappid Pizza Man Simulator
+addappid(4162261, 1, "b1bc69f89400a468e63653b5f29f5d71f3841d914b6dbc1460565c8c4a425bc9") --Main Windows Depot Pizza Man Simulator
+setManifestid(4162261, "4559852093425498739", 2619635760)
