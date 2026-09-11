c89d6403dda2784727e17b39f9ffd038cc43bf2e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 11:41:57 +0800

    add 2515020.lua

--- 2515020.lua
+++ 2515020.lua
@@ -1,5 +1,18 @@
-addappid(2515020)
-addappid(2515021,0,"a1bc4011c581932ab613727be50494068bd3d57d59fa0f60bef081401902a046")
-setManifestid(2515021,"8473560965998297727")
-addappid(2515022,0,"3bf28fa5738f1732e7e31ca3de269b6690be6a325d2ce76da6f924ddf812b90b")
-setManifestid(2515022,"8458672795933688766")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2515020.lua
+--Gamename FINAL FANTASY XVI
+addappid(2515020, 1, "2cde97a0aa99f50beb1ea949762d856a5ea6852541819cadd10e6624aff5dd4d") --Mainappid FINAL FANTASY XVI
+addappid(2515021, 1, "a1bc4011c581932ab613727be50494068bd3d57d59fa0f60bef081401902a046") --Main Windows Depot FINAL FANTASY XVI
+setManifestid(2515021, "8473560965998297727", 121860338640)
+addappid(2515022, 1, "3bf28fa5738f1732e7e31ca3de269b6690be6a325d2ce76da6f924ddf812b90b") --Main Windows Depot FINAL FANTASY XVI
+setManifestid(2515022, "8458672795933688766", 8450858048)
+--Dlcname FINAL FANTASY XVI Echoes of the Fallen
+addappid(2744050, 1, "bc72920eacff6d82dae14ecb5d72a42f4ecbba95f5aab7db874e4cea87e8801e") --Dlcname FINAL FANTASY XVI Echoes of the Fallen
+setManifestid(2744050, "4732525787723924801", 10888258032)
+--Dlcname FINAL FANTASY XVI The Rising Tide
+addappid(2744060, 1, "e57aab17d957ce339fc8bfa53c929fdaa17fa91f3355b40c9bd25e8ba1f1ff83") --Dlcname FINAL FANTASY XVI The Rising Tide
+setManifestid(2744060, "8940147577278755183", 15077195216)
+addappid(2886900) --Dlcname FINAL FANTASY XVI Expansion Pass
+addappid(3142080) --Dlcname FINAL FANTASY XVI - Brave Blade
+addappid(3142090) --Dlcname FINAL FANTASY XVI - Cait Sith Charm
+addappid(3142100) --Dlcname FINAL FANTASY XVI - Sixteen Bells Orchestrion Roll
