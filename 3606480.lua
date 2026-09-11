cf445d7c741ea177cb35fe787ba9f1611a7c90cb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:20:53 +0800

    add 3606480.lua

---
+++ 3606480.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3606480.lua
+--Gamename Call of Duty®: Black Ops 7
+addappid(3606480) --Mainappid Call of Duty®: Black Ops 7
+addtoken(3606480, "9695571449896312778")
