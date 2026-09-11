56e362029867f5414d0ae08d6048f90e4c174790
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:24:55 +0800

    add 3014320.lua

---
+++ 3014320.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3014320.lua
+--Gamename OCTOPATH TRAVELER 0
+addappid(3014320, 1, "f4fd915fb13264ea1005edb8f7beee8699ea3a94262534f617592669ee815ab8") --Mainappid OCTOPATH TRAVELER 0
+addappid(3014321, 1, "376135a423323c017c43031fbf0c4048cf6ea9a6ca94311cd04879ad9a043491") --Main Windows Depot OCTOPATH TRAVELER 0
+setManifestid(3014321, "6649250304437060125", 4961045328)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3576180) --Dlcname オクトパストラベラー0 旅立ち応援パック
+addappid(3718180) --Dlcname オクトパストラベラー0 旅立ち祝福パック
+addappid(3825480) --Dlcname OCTOPATH TRAVELER 0 Digital Deluxe Upgrade
