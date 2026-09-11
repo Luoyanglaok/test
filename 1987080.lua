ff1cd1c0e97b1588fd06fafde8958efdd91918b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:33:33 +0800

    add 1987080.lua

--- 1987080.lua
+++ 1987080.lua
@@ -1,2 +1,6 @@
-addappid(1987080)
-setManifestid(1987081,"5039846724399082316")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1987080.lua
+--Gamename Inside the Backrooms
+addappid(1987080, 1, "f9fc3eab74c49a38fbcf7f9ead2bb6a4e8ace57c135c79f3a743eedde8f9c79f") --Mainappid Inside the Backrooms
+addappid(1987081, 1, "6118f908f294c8f78dfe8b12bdf321b389553909a48dc2283805d8c3ecf0eca3") --Main Windows Depot Inside the Backrooms
+setManifestid(1987081, "5039846724399082316", 11944600432)
