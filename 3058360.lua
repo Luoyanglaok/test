2092dbd3e5ed5f4dd5516e3cf3fe5922ac3b17ce
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 22:13:00 +0800

    add 3058360.lua

---
+++ 3058360.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3058360.lua
+--Gamename Parisian Bistro Simulator
+addappid(3058360) --Mainappid Parisian Bistro Simulator
+addappid(3058361, 1, "26406a7886eee2c23db04271be53ba949b04ea41e7f2626113e76c244f89f9b1") --Main Windows Depot Parisian Bistro Simulator
+setManifestid(3058361, "3686906771159532846", 3518540544)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
