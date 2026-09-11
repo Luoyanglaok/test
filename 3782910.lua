189881d88fad361be523d8e1a25b20368dab6f44
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:51:55 +0800

    add 3782910.lua

---
+++ 3782910.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3782910.lua
+--Gamename Goblin Nest
+addappid(3782910) --Mainappid Goblin Nest
+addappid(3782912, 1, "75d15fb72e9dae9778af3d9bef60148e919910a369c89c0990ba9889e0a96ca7") --Main Windows Depot Goblin Nest
+setManifestid(3782912, "4101927951714747039", 44235984)
