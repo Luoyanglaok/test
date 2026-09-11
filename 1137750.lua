d0e8edc6a51d7935a7dd1809b74956675d1e78d8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 11:13:45 +0800

    add 1137750.lua

--- 1137750.lua
+++ 1137750.lua
@@ -1,3 +1,9 @@
-addappid(1137750)
-addappid(1137751,0,"cb893aa124304bb89a0f140bbd3e10f801fb060b671460c585e7e0befab4ac03")
-setManifestid(1137751,"2183465527816667271")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1137750.lua
+--Gamename Farmer's Life
+addappid(1137750) --Mainappid Farmer's Life
+addappid(1137751, 1, "cb893aa124304bb89a0f140bbd3e10f801fb060b671460c585e7e0befab4ac03") --Main Windows Depot Farmer's Life
+setManifestid(1137751, "7765517645745940469", 4770878528)
+addappid(2926840) --Dlcname Farmer's Life - Pimp my Cottage DLC
+addappid(3468940) --Dlcname Farmer's Life: Spread the Bread DLC
+addappid(3967800) --Dlcname Farmer's Life - Booze Bites Bets DLC
