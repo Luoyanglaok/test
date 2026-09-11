7adbbef768c6f9b4ff53c1f0ddec205b4e703dd0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:35:38 +0800

    add 1599660.lua

--- 1599660.lua
+++ 1599660.lua
@@ -1,5 +1,34 @@
-addappid(1599660)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1599661,0,"d759c1903ae656936c6564d2148c69c553fb408d0234a6604c01916699b2b482")
-setManifestid(1599661,"5076959587775286666")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1599660.lua
+--Gamename Sackboy™: A Big Adventure
+addappid(1599660) --Mainappid Sackboy™: A Big Adventure
+addappid(1599661, 1, "d759c1903ae656936c6564d2148c69c553fb408d0234a6604c01916699b2b482") --Main Windows Depot Sackboy™: A Big Adventure
+setManifestid(1599661, "5076959587775286666", 44228429872)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2131310) --Dlcname Sackboy Pre-Purchase Entitlements
+addappid(2193240) --Dlcname Sackboy™: A Big Adventure – Nathan Drake Costume
+addappid(2193241) --Dlcname Sackboy™: A Big Adventure – Chloe Frazer Costume
+addappid(2206300) --Dlcname Sackboy™: A Big Adventure - Kratos Costume
+addappid(2206301) --Dlcname Sackboy™: A Big Adventure - Atreus Costume
+addappid(2206302) --Dlcname Sackboy™: A Big Adventure - Freya Costume
+addappid(2216230) --Dlcname Sackboy™: A Big Adventure - Football Costume
+addappid(2228240) --Dlcname Sackboy™: A Big Adventure - Emote Pack
+addappid(2228241) --Dlcname Sackboy™: A Big Adventure - New Year's Eve Costume
+addappid(2232410) --Dlcname Sackboy™: A Big Adventure - Monkey King Costume
+addappid(2232470) --Dlcname Sackboy™: A Big Adventure - Fancy Clothing Pack
+addappid(2232480) --Dlcname Sackboy™: A Big Adventure - Celebrations Emote Pack
+addappid(2232490) --Dlcname Sackboy™: A Big Adventure – Gran Turismo® Racing Suit Costume
+addappid(2232500) --Dlcname Sackboy™: A Big Adventure - Video Game Costume
+addappid(2232501) --Dlcname Sackboy™: A Big Adventure - Chinese New Year Costume
+addappid(2232502) --Dlcname Sackboy™: A Big Adventure - Valentine's Costume
+addappid(2232510) --Dlcname Sackboy™: A Big Adventure - Emotions Emote Pack
+addappid(2232520) --Dlcname Sackboy™: A Big Adventure - ASTRO BOT Costume
+addappid(2232530) --Dlcname Sackboy™: A Big Adventure - Sporty Clothing Pack
+addappid(2232540) --Dlcname Sackboy™: A Big Adventure - Matte Paint Pack
+addappid(2232550) --Dlcname Sackboy™: A Big Adventure - Jellybean Paint Pack
+addappid(2232560) --Dlcname Sackboy™: A Big Adventure - Daisy Costume
+addappid(2232580) --Dlcname Sackboy™: A Big Adventure - Casual Clothing Pack
