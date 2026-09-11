426dbcc8b4fc19185aa462b79a6b646935b59249
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:13:34 +0800

    add 1324780.lua

--- 1324780.lua
+++ 1324780.lua
@@ -1,7 +1,25 @@
-addappid(1324780)
-addappid(1324781,0,"8a121832d20574941b7d7e7eb4a34323c9ebe72338fc859080aac679b74bd78c")
-setManifestid(1324781,"5878951315918373970")
-addappid(1324782,0,"4540b21d37c237bc72c6a5b2c3c1202a48b56a5e2bd9b7e8aed3e2112f87042c")
-setManifestid(1324782,"3785266338701351451")
-addappid(1324783,0,"3e04baca8a855c9d1936a84667b80691d3a021d9b24927ff2f32a45f46d18b99")
-setManifestid(1324783,"8664554017968135465")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1324780.lua
+--Gamename Easy Red 2
+addappid(1324780, 1, "9c2fd69454514ddde97c26fb0212ca728b182427f101e9d4f5cdf8ac40462f29") --Mainappid Easy Red 2
+addappid(1324781, 1, "8a121832d20574941b7d7e7eb4a34323c9ebe72338fc859080aac679b74bd78c") --Main Windows Depot Easy Red 2
+setManifestid(1324781, "4648600550357060626", 9263502848)
+addappid(1324782, 1, "4540b21d37c237bc72c6a5b2c3c1202a48b56a5e2bd9b7e8aed3e2112f87042c") --Main Linux Depot Easy Red 2
+setManifestid(1324782, "178019297474602878", 9273124256)
+addappid(1324783, 1, "3e04baca8a855c9d1936a84667b80691d3a021d9b24927ff2f32a45f46d18b99") --Main Macos Depot Easy Red 2
+setManifestid(1324783, "4619565493586982505", 9182113520)
+--Dlcname Easy Red 2: Stalingrad
+addappid(1888350, 1, "7a538b9f79ceb1d7b31d5bdba4d29076dcb7de6dd202a0b9eedb57706ad5f4ab") --Dlcname Easy Red 2: Stalingrad
+setManifestid(1888350, "978066862228458846", 80)
+--Dlcname Easy Red 2: Normandy
+addappid(2317930, 1, "abd13ac0def91994aa63d469d8606fe414d99506f540f683af7807eef45cf1dc") --Dlcname Easy Red 2: Normandy
+setManifestid(2317930, "5612018561843790201", 80)
+--Dlcname Easy Red 2: Ardennes 1940 & 1944
+addappid(2617770, 1, "e69446c7b51abdb0dfece40ed6fa4bcbdb82a7e824906cbdff23f22fbb31dd81") --Dlcname Easy Red 2: Ardennes 1940 & 1944
+setManifestid(2617770, "5016301745773547561", 80)
+--Dlcname Easy Red 2: Shanghai - Nanking
+addappid(3499380, 1, "562ad0c0a2753f1aaf4cae8e67c3d2326d4709031c2a1f3d7d37fa3a1f24ea7c") --Dlcname Easy Red 2: Shanghai - Nanking
+setManifestid(3499380, "3665159405217490266", 80)
+--Dlcname Easy Red 2: Blood on the Danube
+addappid(4563790, 1, "6880ada2f0786d214767b1c45b0740f39b474520c3ad37abdf67042130ebe77c") --Dlcname Easy Red 2: Blood on the Danube
+setManifestid(4563790, "8560462454081719751", 96)
