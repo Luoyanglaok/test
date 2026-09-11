5ba2403f47b1bde007cb15985583da473272878e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:04:20 +0800

    add 492720.lua

--- 492720.lua
+++ 492720.lua
@@ -1,33 +1,50 @@
-addappid(492720)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(492721,0,"dd1294f56dcac65ce85f51bb16a22e48be2ae8a6ad5d4ca90c6dab91f11d11e0")
-setManifestid(492721,"2482048476725152063")
-addappid(881962,0,"17f8cb3323a7e98acdaf4db2607701917ed439757bc8adb8e81f69272de34077")
-setManifestid(881962,"3177376426867696405")
-addappid(881963,0,"fa30a245f1c36ba45e97fe5366ffcc2d17c2b22f510ddc1c788b4260099e5af6")
-setManifestid(881963,"6053701911850756550")
-addappid(492722,0,"3f6d0842f19aa9a1e925efa65af938a7c0787a40772209f04f85b642390a7e7d")
-setManifestid(492722,"4964144065790125117")
-addappid(492723,0,"a7acf29ed791bbbbeab7db9de495da7a7a8de56210c1ecbfc1a136fd5164bafc")
-setManifestid(492723,"4671638683689531000")
-addappid(492724,0,"ab2df80459c17b89abb8dc0b273fc0428a0032b6ebc2c5027c98588279068a37")
-setManifestid(492724,"8446271955596560256")
-addappid(492725,0,"8efc4cf21f7207cea9c32f259680080dac5f5f942aaec8868cfbee4e8de36029")
-setManifestid(492725,"3757073033091442221")
-addappid(492726,0,"9020266ef836d81e3607a7ef8c032c5d154101593ca7819008fe12e390b2b738")
-setManifestid(492726,"7807448400604552744")
-addappid(492727,0,"553570b3d73881c17534d6e148cf70cae386b3329d85e5b102865c6abe9b5780")
-setManifestid(492727,"6683266592988829118")
-addappid(492729,0,"4188d6855228eedcaf3acee2a1177b56d9cadde1f6564d1fcc8b2d5e8fd07dd4")
-setManifestid(492729,"8641782660136044329")
-addappid(881961,0,"4acb95226bd1bae883783022f7021263752f0ad2c41657310320ef2252167fb8")
-setManifestid(881961,"7548479188371965043")
-addappid(881964,0,"cdcb5067d4bd9abc27cdf0c5bc5e389b5c04cd3d9b864539e4e5ef12c45b6e25")
-setManifestid(881964,"5477568930720155518")
-addappid(881965,0,"e8c37b294ead4c0599777edbd97dd724ceb7c1140fb8a6cf165a0ac83cd140a9")
-setManifestid(881965,"3182330635426176422")
-addappid(492728,0,"b45380cb07f37f378d256c7db47227896c9fdac77516b840e8290a25d8b59a2f")
-setManifestid(492728,"3444659887075232254")
-addappid(881960,0,"9a205eb5d636da4082eef0bd4c83938e1eaf7f313cc8f19e3daaae78989b2b24")
-setManifestid(881960,"7166363854253545793")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 492720.lua
+--Gamename Tropico 6
+addappid(492720, 1, "3d5a3467d900e030ecfe1b205db124966a55bec80fe529a0610d96892ee59e50") --Mainappid Tropico 6
+addappid(492721, 1, "dd1294f56dcac65ce85f51bb16a22e48be2ae8a6ad5d4ca90c6dab91f11d11e0") --Main Windows Depot Tropico 6
+setManifestid(492721, "2482048476725152063", 8424379920)
+addappid(492722, 1, "3f6d0842f19aa9a1e925efa65af938a7c0787a40772209f04f85b642390a7e7d") --Main Windows Language Depot English Tropico 6
+setManifestid(492722, "4964144065790125117", 2755417152)
+addappid(492723, 1, "a7acf29ed791bbbbeab7db9de495da7a7a8de56210c1ecbfc1a136fd5164bafc") --Main Windows Language Depot German Tropico 6
+setManifestid(492723, "4671638683689531000", 3126318592)
+addappid(492724, 1, "ab2df80459c17b89abb8dc0b273fc0428a0032b6ebc2c5027c98588279068a37") --Main Windows Language Depot French Tropico 6
+setManifestid(492724, "8446271955596560256", 2800993872)
+addappid(492725, 1, "8efc4cf21f7207cea9c32f259680080dac5f5f942aaec8868cfbee4e8de36029") --Main Windows Language Depot Spanish Tropico 6
+setManifestid(492725, "3757073033091442221", 2821812512)
+addappid(492726, 1, "9020266ef836d81e3607a7ef8c032c5d154101593ca7819008fe12e390b2b738") --Main Windows Language Depot Italian Tropico 6
+setManifestid(492726, "7807448400604552744", 2760921824)
+addappid(492727, 1, "553570b3d73881c17534d6e148cf70cae386b3329d85e5b102865c6abe9b5780") --Main Windows Language Depot Russian Tropico 6
+setManifestid(492727, "6683266592988829118", 3029564848)
+addappid(492728, 1, "b45380cb07f37f378d256c7db47227896c9fdac77516b840e8290a25d8b59a2f") --Main Windows Language Depot Japanese Tropico 6
+setManifestid(492728, "3444659887075232254", 5380475136)
+addappid(492729, 1, "4188d6855228eedcaf3acee2a1177b56d9cadde1f6564d1fcc8b2d5e8fd07dd4") --Main Windows Language Depot Schinese Tropico 6
+setManifestid(492729, "8641782660136044329", 2747392720)
+addappid(881961, 1, "4acb95226bd1bae883783022f7021263752f0ad2c41657310320ef2252167fb8") --Main Windows Language Depot Koreana Tropico 6
+setManifestid(881961, "7548479188371965043", 2755417152)
+addappid(881962, 1, "17f8cb3323a7e98acdaf4db2607701917ed439757bc8adb8e81f69272de34077") --Main Macos Depot Tropico 6
+setManifestid(881962, "3177376426867696405", 10822629152)
+addappid(881963, 1, "fa30a245f1c36ba45e97fe5366ffcc2d17c2b22f510ddc1c788b4260099e5af6") --Main Linux Depot Tropico 6
+setManifestid(881963, "6053701911850756550", 8792592304)
+addappid(881964, 1, "cdcb5067d4bd9abc27cdf0c5bc5e389b5c04cd3d9b864539e4e5ef12c45b6e25") --Main Windows Language Depot Arabic Tropico 6
+setManifestid(881964, "5477568930720155518", 3578037504)
+addappid(881965, 1, "e8c37b294ead4c0599777edbd97dd724ceb7c1140fb8a6cf165a0ac83cd140a9") --Main Windows Language Depot Brazilian Tropico 6
+setManifestid(881965, "3182330635426176422", 2755417152)
+--Dlcname Tropico 6 El Prez Edition Pack
+addtoken(881960, "16001553025725405273")
+addappid(881960, 1, "9a205eb5d636da4082eef0bd4c83938e1eaf7f313cc8f19e3daaae78989b2b24") --Dlcname Tropico 6 El Prez Edition Pack
+setManifestid(881960, "7166363854253545793", 514288896)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1100250) --Dlcname Tropico 6 - The Llama of Wall Street
+addappid(1259750) --Dlcname Tropico 6 - Spitter
+addappid(1282350) --Dlcname Tropico 6 - Lobbyistico
+addappid(1449310) --Dlcname Tropico 6 - Caribbean Skies
+addappid(1631380) --Dlcname Tropico 6 - Festival
+addappid(2186250) --Dlcname Tropico 6 - New Frontiers
+addappid(2594010) --Dlcname Tropico 6 - Going Viral
+addappid(2938080) --Dlcname Tropico 6 - Tropican Shores
+addappid(3391380) --Dlcname Tropico 6 - Return to Nature
