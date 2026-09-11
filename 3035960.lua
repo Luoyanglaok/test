35e1ec941311146b95f09c1f1de7085e4c4ac2a0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 15:14:12 +0800

    add 3035960.lua

---
+++ 3035960.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3035960.lua
+--Gamename Augment Anthem
+addappid(3035960) --Mainappid Augment Anthem
+addappid(3035969, 1, "fa7f135eb24d884dbad747d4715cba056b2dab308805b8523b6d5d985e5b8939") --Main Windows Depot Augment Anthem
+setManifestid(3035969, "2644358238979635871", 241824448)
