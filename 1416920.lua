8fbe53b68b6ff3ed90e96a5e61b28678780c34cc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 10:57:10 +0800

    add 1416920.lua

--- 1416920.lua
+++ 1416920.lua
@@ -1,5 +1,13 @@
-addappid(1416920)
-addappid(1416921,0,"baa863b7295b6249af3fd91f6f55956ca6f8b5185529106ae13f60c51ca21988")
-setManifestid(1416921,"8957170505581065647")
-addappid(3193590,0,"d20a4d95eebfeb18d60aff959a5c584787ab9793bcfa5ba415dcc7b3dffc3acf")
-setManifestid(3193590,"5342148085172353443")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1416920.lua
+--Gamename 中国式网游
+addappid(1416920) --Mainappid 中国式网游
+addappid(1416921, 1, "baa863b7295b6249af3fd91f6f55956ca6f8b5185529106ae13f60c51ca21988") --Main Windows Depot 中国式网游
+setManifestid(1416921, "8957170505581065647", 2945427424)
+--Dlcname 中国式网游 - 爽文
+addappid(3193590, 1, "d20a4d95eebfeb18d60aff959a5c584787ab9793bcfa5ba415dcc7b3dffc3acf") --Dlcname 中国式网游 - 爽文
+setManifestid(3193590, "5342148085172353443", 3003960880)
+addappid(3334700) --Dlcname 中国式网游 - 彩票模式
+addappid(3356630) --Dlcname 中国式网游 - 氪佬专属礼包2
+--Missing Dlcs
+--Dlcname 中国式网游 - 氪佬专属礼包
