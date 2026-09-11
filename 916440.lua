f91246a5aa58aa22fb7da97e916c895160de7815
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:11:30 +0800

    add 916440.lua

--- 916440.lua
+++ 916440.lua
@@ -1,13 +1,115 @@
-addappid(916440)
-addappid(916441,0,"fdf2da2bad8516bd58704e87cdd2101173aa1330d9328186184ff6445a945e19")
-setManifestid(916441,"6124783162666472131")
-addappid(916442,0,"eb8f0f30eafebbbaf8c5870c365708e8a973fd5d817e79d08db91170da90284b")
-setManifestid(916442,"1322343137084862164")
-addappid(916443,0,"070c294380877e9c7d8f2daab4a07fe0b2194473c79502b71e66b11d5ec4bbf0")
-setManifestid(916443,"2181072315111797237")
-addappid(916444,0,"5f6372b7a1ed235b555d05aa54ec7bea9f3d4b08906633514b058b5bd14f4e6c")
-setManifestid(916444,"7565422157144661605")
-addappid(916445,0,"2bb378d0ec6043f34d63cbf2c37a06e9d61c9c197188638cdd24b6fccc0ccbbf")
-setManifestid(916445,"2803529792524411023")
-addappid(916446,0,"96ca058661170b58b95d8745bf9a352795ee003dfbf96c622825b88063eada12")
-setManifestid(916446,"5306902030699936255")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 916440.lua
+--Gamename Anno 1800
+addappid(916440, 1, "889f4cfd8180a1fdc3df4831d25fdf6cdef9ce57dfe9c60ed6fc145ab532c383") --Mainappid Anno 1800
+addappid(916441, 1, "fdf2da2bad8516bd58704e87cdd2101173aa1330d9328186184ff6445a945e19") --Main Windows Depot Anno 1800
+setManifestid(916441, "6124783162666472131", 43699765536)
+addappid(916442, 1, "eb8f0f30eafebbbaf8c5870c365708e8a973fd5d817e79d08db91170da90284b") --Main Windows Depot Anno 1800
+setManifestid(916442, "1322343137084862164", 946730528)
+addappid(916443, 1, "070c294380877e9c7d8f2daab4a07fe0b2194473c79502b71e66b11d5ec4bbf0") --Main Windows Depot Anno 1800
+setManifestid(916443, "2181072315111797237", 1012184144)
+addappid(916444, 1, "5f6372b7a1ed235b555d05aa54ec7bea9f3d4b08906633514b058b5bd14f4e6c") --Main Windows Depot Anno 1800
+setManifestid(916444, "7565422157144661605", 900101152)
+addappid(916445, 1, "2bb378d0ec6043f34d63cbf2c37a06e9d61c9c197188638cdd24b6fccc0ccbbf") --Main Windows Depot Anno 1800
+setManifestid(916445, "2803529792524411023", 856603696)
+--Dlcname Anno 1800 - Deluxe Pack
+addtoken(1066852, "13854832133522835162")
+addappid(1066852) --Dlcname Anno 1800 - Deluxe Pack
+addappid(916446, 1, "96ca058661170b58b95d8745bf9a352795ee003dfbf96c622825b88063eada12") --Dlc Windows Depot Anno 1800 - Deluxe Pack
+setManifestid(916446, "5306902030699936255", 239831504)
+addappid(916451) --Dlcname Anno 1800 - Uplay Activation Standard Preorder
+addappid(916452) --Dlcname Anno 1800 - Uplay Activation Deluxe Preorder
+addappid(1066850) --Dlcname Anno 1800 - Season Pass
+addtoken(1066850, "12111271149057402995")
+addappid(1066851) --Dlcname Anno 1800 - Season Pass Uplay Activation
+addappid(1066853) --Dlcname Anno 1800 - Deluxe Pack Uplay Activation
+addappid(1125790) --Dlcname Anno 1800 - Sunken Treasure
+addappid(1125791) --Dlcname Anno 1800 - Sunken Treasure Uplay Activation
+addappid(1125792) --Dlcname AppID 1125792
+addtoken(1125792, "15363863492004126802")
+addappid(1125793) --Dlcname AppID 1125793
+addtoken(1125793, "15696604020445804298")
+addappid(1125794) --Dlcname Anno 1800 - The Passage
+addtoken(1125794, "10348292758142420166")
+addappid(1125795) --Dlcname AppID 1125795
+addtoken(1125795, "285244861000477955")
+addappid(1210040) --Dlcname AppID 1210040
+addtoken(1210040, "16759723954527412761")
+addappid(1210041) --Dlcname Anno 1800 – Holyday pack Uplay activation
+addappid(1247680) --Dlcname Anno 1800 - Seat of Power
+addtoken(1247680, "3197839846494379136")
+addappid(1247681) --Dlcname AppID 1247681
+addtoken(1247681, "10907565467632324824")
+addappid(1247710) --Dlcname Anno 1800 - Year 2 Pass
+addtoken(1247710, "8973042785894684627")
+addappid(1247711) --Dlcname Anno 1800 - Year 2 Pass Uplay Activation
+addappid(1314950) --Dlcname Anno 1800 - Bright Harvest
+addtoken(1314950, "14151021687858726039")
+addappid(1314951) --Dlcname AppID 1314951
+addtoken(1314951, "9050655666304366063")
+addappid(1383240) --Dlcname AppID 1383240
+addtoken(1383240, "6035183030212088646")
+addappid(1383241) --Dlcname AppID 1383241
+addtoken(1383241, "3247136012154016126")
+addappid(1442810) --Dlcname Anno 1800 - Land of Lions
+addappid(1442811) --Dlcname Anno 1800 - Land of Lions Uplay Activation
+addappid(1465650) --Dlcname Anno 1800 - City Lights Pack
+addappid(1465651) --Dlcname AppID 1465651
+addtoken(1465651, "8510601613906666037")
+addappid(1530880) --Dlcname Anno 1800 - Year 3 Pass
+addtoken(1530880, "10032381193074420280")
+addappid(1530881) --Dlcname AppID 1530881
+addtoken(1530881, "10333676926568764571")
+addappid(1530882) --Dlcname Anno 1800 - Docklands
+addappid(1530883) --Dlcname AppID 1530883
+addtoken(1530883, "14469224751097083876")
+addappid(1626970) --Dlcname Anno 1800 - Tourist Season
+addtoken(1626970, "14431331289985871920")
+addappid(1626971) --Dlcname AppID 1626971
+addtoken(1626971, "15660085937845016189")
+addappid(1626972) --Dlcname AppID 1626972
+addtoken(1626972, "7686359769432141679")
+addappid(1626973) --Dlcname AppID 1626973
+addtoken(1626973, "1001413596776433140")
+addappid(1704680) --Dlcname Anno 1800 - The High Life
+addappid(1704681) --Dlcname AppID 1704681
+addtoken(1704681, "8134497571007803230")
+addappid(1704690) --Dlcname AppID 1704690
+addtoken(1704690, "4766422470377779440")
+addappid(1704691) --Dlcname AppID 1704691
+addtoken(1704691, "4237534615968270310")
+addappid(1803740) --Dlcname AppID 1803740
+addappid(1803741) --Dlcname AppID 1803741
+addtoken(1803741, "13092192723117200999")
+addappid(1803742) --Dlcname Anno 1800 - Vibrant Cities Pack
+addappid(1803743) --Dlcname AppID 1803743
+addtoken(1803743, "10834241906534394363")
+addappid(1881100) --Dlcname Anno 1800 - Seasonal Decorations Pack
+addappid(1881101) --Dlcname AppID 1881101
+addtoken(1881101, "7186953762791114175")
+addappid(1933240) --Dlcname Anno 1800 - Year 4 Season Pass
+addappid(1933241) --Dlcname Anno 1800 - Year 4 Season Pass Ubisoft Activation
+addappid(1933242) --Dlcname Anno 1800 - Cosmetic Pack Bundle
+addappid(1933243) --Dlcname Anno 1800 - Cosmetic Pack Bundle Ubisoft Activation
+addappid(1933244) --Dlcname Anno 1800 - Seeds of Change
+addappid(1933245) --Dlcname Anno 1800 - Seeds of Change Ubisoft Activation
+addappid(2055760) --Dlcname Anno 1800 - Industrial Zone Pack
+addappid(2055761) --Dlcname Anno 1800 - Industrial Zone Pack Ubisoft Activation
+addappid(2074410) --Dlcname Anno 1800 - Empire of the Skies Pack
+addappid(2074411) --Dlcname Anno 1800 - Empire of the Skies Pack Ubisoft Connect Activation
+addappid(2222640) --Dlcname Anno 1800 – New World Rising Pack
+addappid(2222641) --Dlcname Anno 1800 – New World Rising Ubisoft Activation
+addappid(2222650) --Dlcname Anno 1800 - Old Town Pack
+addappid(2222651) --Dlcname Anno 1800 - Old Town Pack Ubisoft Activation
+addappid(2231361) --Dlcname Anno 1800 Ubisoft Activation
+addappid(2235740) --Dlcname Anno 1800 - Year 4 Complete Edition Ubisoft Activation
+addappid(2235741) --Dlcname Anno 1800 - Year 4 Gold Edition Ubisoft Activation
+addappid(2277980) --Dlcname Anno 1800 – Dragon Garden Pack
+addappid(2277981) --Dlcname Anno 1800 – Dragon Garden Pack Ubisoft Activation
+addappid(2400950) --Dlcname Anno 1800 - Fiesta Pack
+addappid(2504950) --Dlcname Anno 1800 - National Park Pack
+addappid(2622020) --Dlcname Anno 1800 - Cosmetic Bundle #2
+addappid(2622040) --Dlcname Anno 1800 - Eldritch Pack
+addappid(2830580) --Dlcname Anno 1800 - Steampunk Pack
+addappid(3113620) --Dlcname Anno 1800 - Pirate Cove Pack
+addappid(3256930) --Dlcname Anno 1800 - End of an Era Pack
