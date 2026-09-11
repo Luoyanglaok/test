884173304d297424ae378293d5520618717fb4cf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:48:17 +0800

    add 3033250.lua

--- 3033250.lua
+++ 3033250.lua
@@ -1,5 +1,8 @@
-addappid(3033250)
-addappid(3033251,0,"3c3a5a42cf400e9c55ce64cc990ad26a35f5cd36e7ffba80853001e3fc2ef12c")
-setManifestid(3033251,"1902738102402146137")
-addappid(3033252,0,"1e9dfcfdf5b4240d191352afa3eef13b913f3775ced111a36760ed5ca5dedff2")
-setManifestid(3033252,"1436573855261952709")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3033250.lua
+--Gamename Taiwan Love Story⁵ Soundtrack
+addappid(3033250) --Mainappid Taiwan Love Story⁵ Soundtrack
+addappid(3033251, 1, "3c3a5a42cf400e9c55ce64cc990ad26a35f5cd36e7ffba80853001e3fc2ef12c") --Main Windows Depot Taiwan Love Story⁵ Soundtrack
+setManifestid(3033251, "1902738102402146137", 130127728)
+addappid(3033252, 1, "1e9dfcfdf5b4240d191352afa3eef13b913f3775ced111a36760ed5ca5dedff2") --Main Windows Depot Taiwan Love Story⁵ Soundtrack
+setManifestid(3033252, "1436573855261952709", 1397309040)
