6b5a4580f8b375f9efede58eed40f33368c11424
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:50:30 +0800

    add 3375110.lua

--- 3375110.lua
+++ 3375110.lua
@@ -12,4 +12,5 @@ setManifestid(228989, "5753583882400741046", 25108528)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228990, "1829726630299308803", 100658080)
 --Missing Dlcs
+--Dlcname Tiny Eden Soundtrack
 --Dlcname Tiny Eden Goodies Pack
