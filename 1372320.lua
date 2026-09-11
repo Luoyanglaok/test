3900ba027ba0278c06586517c2b55e5e2ac86dba
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 01:56:29 +0800

    add 1372320.lua

--- 1372320.lua
+++ 1372320.lua
@@ -1,5 +1,8 @@
-addappid(1372320)
-addappid(1372321,0,"c6048f5203ffadde7e04256cee879b17a7f1b97f01cc849404e940ee0715f264")
-setManifestid(1372321,"2874586199382021433")
-addappid(1372322,0,"b013288636d1c029efe42290a797e7ad5133cd28a83804250ddd5f5986a48d9a")
-setManifestid(1372322,"6596031682601610953")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1372320.lua
+--Gamename Cloud Gardens
+addappid(1372320) --Mainappid Cloud Gardens
+addappid(1372321, 1, "c6048f5203ffadde7e04256cee879b17a7f1b97f01cc849404e940ee0715f264") --Main Windows Depot Cloud Gardens
+setManifestid(1372321, "8338177975486494320", 138824064)
+addappid(1372322, 1, "b013288636d1c029efe42290a797e7ad5133cd28a83804250ddd5f5986a48d9a") --Main Macos Depot Cloud Gardens
+setManifestid(1372322, "4176734135631037017", 144021424)
