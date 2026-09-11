8f81e33a32d195228d169ec9383274d29155f45d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 13:54:59 +0800

    add 1822120.lua

--- 1822120.lua
+++ 1822120.lua
@@ -1,3 +1,6 @@
-addappid(1822120)
-addappid(1822121,0,"bd96fd89afd5496aeb0da997cee9953435052dc8dabe32bdc42ed75e2e69acfa")
-setManifestid(1822121,"7813976726222400017")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1822120.lua
+--Gamename Life and Sex in Men's Сollege - Season 1 💕🔞
+addappid(1822120, 1, "47d044a14c737484cbaa8d4de08d0aed1e0abb9df515c57115d069b98ab3919a") --Mainappid Life and Sex in Men's Сollege - Season 1 💕🔞
+addappid(1822121, 1, "bd96fd89afd5496aeb0da997cee9953435052dc8dabe32bdc42ed75e2e69acfa") --Main Windows Depot Life and Sex in Men's Сollege - Season 1 💕🔞
+setManifestid(1822121, "7813976726222400017", 73800016)
