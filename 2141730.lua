186dda8e8ca94ae62ef1c65d7ff4fa49a7b4b827
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 17:54:29 +0800

    add 2141730.lua

--- 2141730.lua
+++ 2141730.lua
@@ -1,7 +1,13 @@
-addappid(2141730)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2141731,0,"cfcbb1b05a371faabeb03500c308dbae345a2c7d9a45417a910283c9201e084d")
-setManifestid(2141731,"553615528304740248")
-addappid(2141732,0,"1038f744a70ce5b445cdbfc9ce77601ae45a36279d0a9ad02b59b714acfba762")
-setManifestid(2141732,"6866044377255604924")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2141730.lua
+--Gamename Backrooms: Escape Together
+addappid(2141730, 1, "aaf3b1238f920baec05279c2de2478fbf37c5380c19660e6b4a2a09f7e176e66") --Mainappid Backrooms: Escape Together
+addappid(2141731, 1, "cfcbb1b05a371faabeb03500c308dbae345a2c7d9a45417a910283c9201e084d") --Main Windows Depot Backrooms: Escape Together
+setManifestid(2141731, "2167984175935939828", 13663896832)
+addappid(2141732, 1, "1038f744a70ce5b445cdbfc9ce77601ae45a36279d0a9ad02b59b714acfba762") --Main Macos Depot Backrooms: Escape Together
+setManifestid(2141732, "6866044377255604924", 7801338224)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
