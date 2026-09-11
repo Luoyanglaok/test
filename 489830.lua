f5f93315c3222f1128ac1d169e023df8d9c5f664
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 11:57:18 +0800

    add 489830.lua

--- 489830.lua
+++ 489830.lua
@@ -1,25 +1,34 @@
-addappid(489830)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(489831,0,"a26da9ff1f2af2345ebfad1bd62bcb9ed1cbe6afcf4ced92d1a5fa944242b825")
-setManifestid(489831,"8442952117333549665")
-addappid(489832,0,"c6c5e29627073060ccf9fc8c6c9c7ec86dfdf16b51b3d1fbb0a2e7c1ad9f533e")
-setManifestid(489832,"8042843504692938467")
-addappid(489833,0,"7c5e75890b4f59b4be2d3b68c88abad8b23b159a23bf25a00c5c3a4748752f36")
-setManifestid(489833,"1914580699073641964")
-addappid(489834,0,"eeb89a10bdac7b9b83b6d03cffd9c6225d9041a8130ea52ff0f96b7f6174e857")
-setManifestid(489834,"8562891139598763344")
-addappid(489835,0,"ed764444d4ab7b8d2f98100ddd951659a57f5df3c8825b2d14df99f8ab1b6447")
-setManifestid(489835,"9152699258833967999")
-addappid(489836,0,"1ebc81b82a20b369c9c0335ed96a206b81b46846f2dc9a9f457e748d1ec48045")
-setManifestid(489836,"2360664012913025776")
-addappid(489837,0,"b2cf2cc8cadda88f33e707b79fb2b95b458553a6d768f7d1ab9ef657a18fa90c")
-setManifestid(489837,"8402094395440824039")
-addappid(489838,0,"807c9db0782a0d03abbe9f123179b7428bb952dcb3c127abcb0455d65d772cdd")
-setManifestid(489838,"3873588632592923754")
-addappid(489839,0,"19028a477ba63d9a671eed7c6590e607370062aa3bf59446cc8b79022ab64de4")
-setManifestid(489839,"176511637554234126")
-addappid(544860,0,"b0cdbf6da886a9083687985cffde7be07f2183afe615b10af508ff8ded2f8408")
-setManifestid(544860,"8476536650815851202")
-addappid(544861,0,"586061c4ba80f7bf1fcf1da170aeffeacdc20869d4d5d622e69c935f42c26630")
-setManifestid(544861,"3494476046078906882")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 489830.lua
+--Gamename The Elder Scrolls V: Skyrim Special Edition
+addappid(489830, 1, "6a555c63251bd00d634428e538ae4b14e1443b71950e17cf1822a9a4efebaa4c") --Mainappid The Elder Scrolls V: Skyrim Special Edition
+addappid(489831, 1, "a26da9ff1f2af2345ebfad1bd62bcb9ed1cbe6afcf4ced92d1a5fa944242b825") --Main Windows Depot The Elder Scrolls V: Skyrim Special Edition
+setManifestid(489831, "4940892828028256588", 5102624736)
+addappid(489832, 1, "c6c5e29627073060ccf9fc8c6c9c7ec86dfdf16b51b3d1fbb0a2e7c1ad9f533e") --Main Windows Depot The Elder Scrolls V: Skyrim Special Edition
+setManifestid(489832, "5728778377666085157", 7624290176)
+addappid(489833, 1, "7c5e75890b4f59b4be2d3b68c88abad8b23b159a23bf25a00c5c3a4748752f36") --Main Windows Depot The Elder Scrolls V: Skyrim Special Edition
+setManifestid(489833, "4886117324142477814", 13605104)
+addappid(489834, 1, "eeb89a10bdac7b9b83b6d03cffd9c6225d9041a8130ea52ff0f96b7f6174e857") --Main Windows Language Depot French The Elder Scrolls V: Skyrim Special Edition
+setManifestid(489834, "3443400614252193371", 1708134400)
+addappid(489835, 1, "ed764444d4ab7b8d2f98100ddd951659a57f5df3c8825b2d14df99f8ab1b6447") --Main Windows Language Depot Italian The Elder Scrolls V: Skyrim Special Edition
+setManifestid(489835, "8608174573130432573", 1758726944)
+addappid(489836, 1, "1ebc81b82a20b369c9c0335ed96a206b81b46846f2dc9a9f457e748d1ec48045") --Main Windows Language Depot German The Elder Scrolls V: Skyrim Special Edition
+setManifestid(489836, "2757270758137158257", 1861391600)
+addappid(489837, 1, "b2cf2cc8cadda88f33e707b79fb2b95b458553a6d768f7d1ab9ef657a18fa90c") --Main Windows Language Depot Spanish The Elder Scrolls V: Skyrim Special Edition
+setManifestid(489837, "1257737911412624232", 1743243424)
+addappid(489838, 1, "807c9db0782a0d03abbe9f123179b7428bb952dcb3c127abcb0455d65d772cdd") --Main Windows Language Depot Russian The Elder Scrolls V: Skyrim Special Edition
+setManifestid(489838, "6810738189465173322", 1896099968)
+addappid(489839, 1, "19028a477ba63d9a671eed7c6590e607370062aa3bf59446cc8b79022ab64de4") --Main Windows Language Depot Polish The Elder Scrolls V: Skyrim Special Edition
+setManifestid(489839, "2751807503705497425", 1906243968)
+addappid(544860, 1, "b0cdbf6da886a9083687985cffde7be07f2183afe615b10af508ff8ded2f8408") --Main Windows Language Depot Tchinese The Elder Scrolls V: Skyrim Special Edition
+setManifestid(544860, "5386271571563273695", 944)
+addappid(544861, 1, "586061c4ba80f7bf1fcf1da170aeffeacdc20869d4d5d622e69c935f42c26630") --Main Windows Language Depot Japanese The Elder Scrolls V: Skyrim Special Edition
+setManifestid(544861, "4913356797939551438", 1836567296)
+addappid(1746860, 1, "4ec81ebbdba15133502f0810d762a508e2a91c18c0c2ba6a4dda9be6b339abc2") --Main Windows Depot The Elder Scrolls V: Skyrim Special Edition
+--Dlcname The Elder Scrolls V: Skyrim Anniversary Upgrade
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(598120) --Dlcname The Elder Scrolls V: Skyrim Special Edition - Creations
