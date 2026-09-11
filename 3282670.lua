7513be5caa9f4072cee5ea95e443e3a9ef86ca15
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:19:52 +0800

    add 3282670.lua

--- 3282670.lua
+++ 3282670.lua
@@ -1,3 +1,6 @@
-addappid(3282670)
-addappid(3282671,0,"ca04f5512f9def9d894fb8fa00d77dc3b58dc52bca901fcdf7ad02f358118d79")
-setManifestid(3282671,"2887382280080373722")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3282670.lua
+--Gamename SEX Police 🔞🚨
+addappid(3282670) --Mainappid SEX Police 🔞🚨
+addappid(3282671, 1, "ca04f5512f9def9d894fb8fa00d77dc3b58dc52bca901fcdf7ad02f358118d79") --Main Windows Depot SEX Police 🔞🚨
+setManifestid(3282671, "2887382280080373722", 2755978224)
