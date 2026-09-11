2b8f4c197b97f53ed4e22fb7a4c1d762da6095d9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:17:27 +0800

    add 3044550.lua

--- 3044550.lua
+++ 3044550.lua
@@ -1,3 +1,8 @@
-addappid(3044550)
-addappid(3044551,0,"0e2d106c15c99f7de40b6c155b15b1efbf330fc9d39caa929949b6af82b97c6d")
-setManifestid(3044551,"5754558447758921740")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3044550.lua
+--Gamename Eldrea: SEX Saga 🔞
+addappid(3044550, 1, "34aea6c78b1b8bf0764727ad220ff973d2915376ed81de2c1b8c91ad7688dd51") --Mainappid Eldrea: SEX Saga 🔞
+addappid(3044551, 1, "0e2d106c15c99f7de40b6c155b15b1efbf330fc9d39caa929949b6af82b97c6d") --Main Windows/Linux Depot Eldrea: SEX Saga 🔞
+setManifestid(3044551, "5754558447758921740", 2239521760)
+addappid(3044552, 1, "d8eefeffeab71c428c9d9ad1d6387992232762c2fc16574b9e1e29ee8be27c1a") --Main Macos Depot Eldrea: SEX Saga 🔞
+setManifestid(3044552, "372776007079121461", 2233950032)
