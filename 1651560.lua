b948b3e4602c6949e28610e5c894d2b5e142b41e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:08:13 +0800

    add 1651560.lua

--- 1651560.lua
+++ 1651560.lua
@@ -1,3 +1,6 @@
-addappid(1651560)
-addappid(1651561,0,"dbaf69374e2fa99d18be9c0ee9b803630ac5fdb318fff5a83637397db2576b99")
-setManifestid(1651561,"2922201590293598515")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1651560.lua
+--Gamename Fabledom
+addappid(1651560, 1, "c6329467bff9d2906cce3e97340d0e18525fc2cc4b69cbf65fd3dcbcaf9692b3") --Mainappid Fabledom
+addtoken(1651560, "4483984494128195992")
+addappid(1651561, 1, "dbaf69374e2fa99d18be9c0ee9b803630ac5fdb318fff5a83637397db2576b99") --Main Windows Depot Fabledom
