86d16adaca3fe930f98c24cc52442d1a95f349d4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:13:11 +0800

    add 2696050.lua

--- 2696050.lua
+++ 2696050.lua
@@ -1,7 +1,18 @@
-addappid(2696050)
-addappid(3171570,0,"006677b4d7eb93ef97bd8ade42a0c84efe2ca2a7983f406b1f79b4bcfeabe666")
-setManifestid(3171570,"6273957131003220084")
-addappid(2696055,0,"25043be4f82f396d723a71516561347bda08810502afdc20dea5cbe5b75a2bc8")
-setManifestid(2696055,"1478240780007961904")
-addappid(3593651,0,"cdac6a93224f65139edcce3b7fb21a294db38b14f5cf1b5e5f96687a45f35f0b")
-setManifestid(3593651,"1488515107206078954")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2696050.lua
+--Gamename Yarimono
+addappid(2696050, 1, "f71b6990b32f1363c0982ef9b17898c3cdff4c174a0474e70d62d110419c0b18") --Mainappid Yarimono
+addappid(2696051, 1, "6e24313174f32bc32cb4a48c6bbfc4718078603d843c22968b25fa1d7faefd3b") --Main Windows Depot Yarimono
+addappid(2696055, 1, "25043be4f82f396d723a71516561347bda08810502afdc20dea5cbe5b75a2bc8") --Main Windows Depot Yarimono
+setManifestid(2696055, "4797632950615304316", 701023440)
+--Dlcname Yarimono ~ DLC 1
+addappid(3171570, 1, "006677b4d7eb93ef97bd8ade42a0c84efe2ca2a7983f406b1f79b4bcfeabe666") --Dlcname Yarimono ~ DLC 1
+setManifestid(3171570, "6273957131003220084", 15589792)
+addappid(3171571, 1, "3ec0a8d4b8c0f87b6df335032a93b2d866d94819cce90ef8deb6123dc6e602e7") --Dlc Windows Depot Yarimono ~ DLC 1
+setManifestid(3171571, "3309968121601332384", 17543216)
+--Dlcname Yarimono ~ DLC 2
+addappid(3593650, 1, "1b4a0cba8eb75228cf19b2d132a8a887137dbb37f81dd95c0b2698ac8ae1166b") --Dlcname Yarimono ~ DLC 2
+setManifestid(3593650, "7850549861192466480", 100441440)
+addappid(3593651, 1, "cdac6a93224f65139edcce3b7fb21a294db38b14f5cf1b5e5f96687a45f35f0b") --Dlc Windows Depot Yarimono ~ DLC 2
+setManifestid(3593651, "8139483366419726494", 99527248)
+--Share Depots
