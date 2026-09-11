d1996874fdfce9c6e11d7bdf76c7ec790e8ea427
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:58:00 +0800

    add 1462040.lua

--- 1462040.lua
+++ 1462040.lua
@@ -8,8 +8,23 @@ addappid(1462042, 1, "ac6881e562951d6369653f3e05bfbb32cb022208f25a950fd31e5045af
 setManifestid(1462042, "4652159271987585380", 64)
 addappid(1462043, 1, "85dd87a3f14448b8659faa0acf1ec19c4213ea1096601adec258a00910b31cbb") --Main Windows Depot FINAL FANTASY VII REMAKE INTERGRADE
 setManifestid(1462043, "2245432882606591716", 48)
+--Dlcname FINAL FANTASY VII REMAKE Original Soundtrack
+addappid(3365650) --Dlcname FINAL FANTASY VII REMAKE Original Soundtrack
+addappid(3365651, 1, "6ae2f0df4ac4f2599d26e91be198cd60486c19d92fa1549e79d83ee96e41156c") --Dlc Windows Depot FINAL FANTASY VII REMAKE Original Soundtrack
+setManifestid(3365651, "5610006288920982081", 1118982304)
+--Dlcname FINAL FANTASY VII REMAKE Original Soundtrack Plus
+addappid(3365670) --Dlcname FINAL FANTASY VII REMAKE Original Soundtrack Plus
+addappid(3365671, 1, "87bf49cbb0d9a54f8b064181471f67550cf246c1715f6e484f169d68df01d6be") --Dlc Windows Depot FINAL FANTASY VII REMAKE Original Soundtrack Plus
+setManifestid(3365671, "5317826766882873609", 680598048)
+--Dlcname FINAL FANTASY VII REMAKE INTERGRADE Original Soundtrack
+addappid(3365690) --Dlcname FINAL FANTASY VII REMAKE INTERGRADE Original Soundtrack
+addappid(3365691, 1, "595b8379fecbb70f577632cd4551268aae9b01dffbf90fd224950d8d884a3ce6") --Dlc Windows Depot FINAL FANTASY VII REMAKE INTERGRADE Original Soundtrack
+setManifestid(3365691, "4219983033002616196", 498604128)
 --Share Depots
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228988, "6645201662696499616", 22411856)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname FINAL FANTASY VII REMAKE Orchestral Arrangement Album
+--Dlcname FINAL FANTASY VII REMAKE ACOUSTIC ARRANGEMENTS
