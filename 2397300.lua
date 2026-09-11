012f06ea50617f4427315950ec3118d01440f4d0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:05:25 +0800

    add 2397300.lua

---
+++ 2397300.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2397300.lua
+--Gamename Half Sword
+addappid(2397300, 1, "524c672a6125d66225973b18d8f1b53631b38000295384082e5feca5ec3071c3") --Mainappid Half Sword
+addappid(2397301, 1, "0d49aa268c5fc78babcfe220c4828bd7bbc422ae09419958a82240f57b82e161") --Main Windows Depot Half Sword
+setManifestid(2397301, "2093926066177713714", 9787721808)
+--Dlcname Half Sword - Supporter Edition
+addappid(4355970, 1, "a1a3a30f2d8cabc41059d866af336784462840865d6bcbc5eb9962fcb794dc42") --Dlcname Half Sword - Supporter Edition
+setManifestid(4355970, "1233827490267229814", 1764090656)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
