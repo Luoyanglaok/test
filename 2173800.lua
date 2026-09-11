6ad30c1d7e6c1540822b65c56335bd304858f316
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 02:55:07 +0800

    add 2173800.lua

--- 2173800.lua
+++ 2173800.lua
@@ -1,5 +1,9 @@
-addappid(2173800)
-addappid(2173801,0,"5e2bd10d255b09afeab30becbaa1ae93d8f3eb5b2f6696de8e91356c029fc65f")
-setManifestid(2173801,"3902752399534244356")
-addappid(2447610,0,"e48ffd8e3cc28345310847e93e7749d97b5eda4838608ed7a2d7904c382021c4")
-setManifestid(2447610,"5528681308163853672")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2173800.lua
+--Gamename Projekt: Passion - Season 1
+addappid(2173800, 1, "765c17ca9780fffea778c7415bec4c478c833142200df8956ef3afe6efe589a9") --Mainappid Projekt: Passion - Season 1
+addappid(2173801, 1, "5e2bd10d255b09afeab30becbaa1ae93d8f3eb5b2f6696de8e91356c029fc65f") --Main Windows Language Depot English Projekt: Passion - Season 1
+setManifestid(2173801, "3902752399534244356", 2869686688)
+--Dlcname Projekt Passion — Season 1 Walkthrough
+addappid(2447610, 1, "e48ffd8e3cc28345310847e93e7749d97b5eda4838608ed7a2d7904c382021c4") --Dlcname Projekt Passion — Season 1 Walkthrough
+setManifestid(2447610, "5528681308163853672", 6008544)
