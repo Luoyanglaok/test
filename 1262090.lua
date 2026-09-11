956acdf435f0796024201dd99494930d2827cc26
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:02:08 +0800

    add 1262090.lua

---
+++ 1262090.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1262090.lua
+--Gamename 风暴岛MOD工具
+addappid(1262090) --Mainappid 风暴岛MOD工具
+addappid(1262091, 1, "5015248294ed4660c8abf9a9d13c4ca7bc0cec8573668761336085c4f17423e2") --Main Windows Depot 风暴岛MOD工具
+setManifestid(1262091, "8288438253502611741", 13698240)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
