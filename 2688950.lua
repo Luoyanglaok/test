74a28dc7ee525dd1732d1eeb3c65b201234ea9db
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 01:51:07 +0800

    add 2688950.lua

--- 2688950.lua
+++ 2688950.lua
@@ -1,5 +1,26 @@
-addappid(2688950)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2688951,0,"d1fc4044f8dbec56ccd7ccac0a38810470c5fecf00dedc36a294c39453f9adb0")
-setManifestid(2688951,"6052203423923721560")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2688950.lua
+--Gamename Planet Coaster 2
+addappid(2688950, 1, "db80e514e0e23f3ac57084d4e47af67ec4d0d5d3c223d5b918daf8fdc9e73eeb") --Mainappid Planet Coaster 2
+addappid(2688951, 1, "d1fc4044f8dbec56ccd7ccac0a38810470c5fecf00dedc36a294c39453f9adb0") --Main Windows Depot Planet Coaster 2
+setManifestid(2688951, "5564607911436696205", 13889792832)
+--Dlcname The Music of Planet Coaster 2: You, Me & Fluidity
+addappid(3299720) --Dlcname The Music of Planet Coaster 2: You, Me & Fluidity
+addappid(3299721, 1, "223e20c632bc0347c13db5bf878f388c34d94dbb0ca4f37f1f0f8e2f344535cf") --Dlc Windows Depot The Music of Planet Coaster 2: You, Me & Fluidity
+setManifestid(3299721, "544182693317952126", 681987120)
+addappid(3299722, 1, "8b9708acdf921f3e7152104befd3820d0aaa021112e28516c5a819f3560c460b") --Dlc Windows Depot The Music of Planet Coaster 2: You, Me & Fluidity
+setManifestid(3299722, "7769414947378223114", 1468536976)
+addappid(3299723, 1, "88ca1484366b066d82034f67795683103f3e80c8230b9ca98c9bcdb5b98ff867") --Dlc Windows Depot The Music of Planet Coaster 2: You, Me & Fluidity
+setManifestid(3299723, "2377508422711440983", 1346388528)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3091180) --Dlcname Planet Coaster 2: Bonus Ride Collection
+addappid(3091190) --Dlcname Planet Coaster 2: Vintage Funfair Ride Pack
+addappid(3337040) --Dlcname Planet Coaster 2: Thrill-Seekers Ride Pack
+addappid(3902620) --Dlcname Planet Coaster 2: Sorcery Pack
+addappid(4129920) --Dlcname Planet Coaster 2: Toybox Pack
+addappid(4492160) --Dlcname Planet Coaster 2: Parades Pack
+addappid(4961870) --Dlcname Planet Coaster 2: Silver Screen Pack
