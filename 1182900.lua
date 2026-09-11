1509cea3bdf839fe41c688d22686defdb748ca6b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 06:34:48 +0800

    add 1182900.lua

--- 1182900.lua
+++ 1182900.lua
@@ -1,4 +1,10 @@
-addappid(1182900)
-setManifestid(228988,"6645201662696499616")
-addappid(1182901,0,"89056a553dcf6ee12c8c1a92b728c89e8457fb95ec117f931d7115aa918cbd18")
-setManifestid(1182901,"3367036266289852265")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1182900.lua
+--Gamename A Plague Tale: Requiem
+addappid(1182900, 1, "004f1f2a81aa2a3fb95f08a87f8ce5d5b172d797d3eaefc40e39f6a47ff84cf0") --Mainappid A Plague Tale: Requiem
+addappid(1182901, 1, "89056a553dcf6ee12c8c1a92b728c89e8457fb95ec117f931d7115aa918cbd18") --Main Windows Depot A Plague Tale: Requiem
+setManifestid(1182901, "3367036266289852265", 46350194912)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(1906530) --Dlcname A Plague Tale: Requiem - Protector Pack
