861a3a134aafee14c50b5ee4c11f5f10797dd875
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 17:36:15 +0800

    add 1966720.lua

--- 1966720.lua
+++ 1966720.lua
@@ -1,4 +1,9 @@
-addappid(1966720)
-setManifestid(228988,"6645201662696499616")
-addappid(1966721,0,"b79233b678dae6d3f928ff9c6c7674d559427776a1337dee238773a4fec60949")
-setManifestid(1966721,"4861510547912001926")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1966720.lua
+--Gamename Lethal Company
+addappid(1966720, 1, "b1b18f9400b7b45bb7d3223ca7951a73cc2bdf0cb311e5b1a337e9c776c7ac7d") --Mainappid Lethal Company
+addappid(1966721, 1, "b79233b678dae6d3f928ff9c6c7674d559427776a1337dee238773a4fec60949") --Main Windows Depot Lethal Company
+setManifestid(1966721, "6423525044216269478", 594479696)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
