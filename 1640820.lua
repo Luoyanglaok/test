ee3d24aecb8179f20b014baf97a7fb6e04e285b2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 10:55:29 +0800

    add 1640820.lua

--- 1640820.lua
+++ 1640820.lua
@@ -1,7 +1,13 @@
-addappid(1640820)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1640821,0,"2de4c341c69a5caece120eed798bbd441bada826bdf8b1ea784ad8d1964397b9")
-setManifestid(1640821,"618843688828427295")
-addappid(1640822,0,"523e44823dd5e6b046a2f63a9d4c6cdea4d21188c21f153da271138c00b1685e")
-setManifestid(1640822,"2110041086799325647")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1640820.lua
+--Gamename Chinese Frontiers
+addappid(1640820) --Mainappid Chinese Frontiers
+addappid(1640821, 1, "2de4c341c69a5caece120eed798bbd441bada826bdf8b1ea784ad8d1964397b9") --Main Windows Depot Chinese Frontiers
+setManifestid(1640821, "6802840241679956785", 14308041216)
+addappid(1640822, 1, "523e44823dd5e6b046a2f63a9d4c6cdea4d21188c21f153da271138c00b1685e") --Main Windows Depot Chinese Frontiers
+setManifestid(1640822, "2110041086799325647", 64)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
