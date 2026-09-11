665d1789168e59c33bc43a6eb4501e42bd466b26
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:00:43 +0800

    add 1970580.lua

--- 1970580.lua
+++ 1970580.lua
@@ -1,7 +1,16 @@
-addappid(1970580)
-addappid(1970581,0,"7bae37e1619e3eaf89e2689a47977fe767293487db2dc87eeea151deb59b4e2a")
-setManifestid(1970581,"3178627114856544901")
-addappid(1970582,0,"47cefda381bf839c765b093c9d27e09a805d90509379e96950ab2b9baf93bfb4")
-setManifestid(1970582,"176513439720981009")
-addappid(1970583,0,"6218e47340a54721ed75b10918487fc70956b4005f56d5464f48dacf4ad4ef1d")
-setManifestid(1970583,"8957818974901801959")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1970580.lua
+--Gamename Backpack Hero
+addappid(1970580, 1, "f520ab5adecff123f4a796fd9bf559bceec91bbba7c47536c0335599b1fe79b9") --Mainappid Backpack Hero
+addappid(1970581, 1, "7bae37e1619e3eaf89e2689a47977fe767293487db2dc87eeea151deb59b4e2a") --Main Windows Depot Backpack Hero
+setManifestid(1970581, "3178627114856544901", 183321008)
+addappid(1970582, 1, "47cefda381bf839c765b093c9d27e09a805d90509379e96950ab2b9baf93bfb4") --Main Macos Depot Backpack Hero
+setManifestid(1970582, "176513439720981009", 195898864)
+addappid(1970583, 1, "6218e47340a54721ed75b10918487fc70956b4005f56d5464f48dacf4ad4ef1d") --Main Linux Depot Backpack Hero
+setManifestid(1970583, "8957818974901801959", 180242784)
+--Dlcname Backpack Hero Soundtrack
+addappid(2109040) --Dlcname Backpack Hero Soundtrack
+addappid(2109041, 1, "a29772d165d49b2ddf71ccc2c2e76d2cfcabf6143c08ac9f7dde1b42319fd48f") --Dlc Windows Depot Backpack Hero Soundtrack
+setManifestid(2109041, "3927063234213911161", 147109056)
+addappid(2109042, 1, "4702a7144541d0acf9240c490b24a26853bd80218d919fa8872e2ccc2eb22a2f") --Dlc Windows Depot Backpack Hero Soundtrack
+setManifestid(2109042, "3615941928059586614", 503701568)
