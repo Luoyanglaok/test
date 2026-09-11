62e20eba2d73a43e6d2a96625dc45bebd4410c67
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:00:37 +0800

    add 1000280.lua

--- 1000280.lua
+++ 1000280.lua
@@ -1,3 +1,18 @@
-addappid(1000280)
-addappid(1000281,0,"75d8d966b1159c52c3807539077824574e719b2aa2f4c17fc1a1d28bf8ba191f")
-setManifestid(1000281,"2507006329102947433")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1000280.lua
+--Gamename Tower of Origin2-Worm's Nest
+addappid(1000280) --Mainappid Tower of Origin2-Worm's Nest
+addappid(1000281, 1, "75d8d966b1159c52c3807539077824574e719b2aa2f4c17fc1a1d28bf8ba191f") --Main Windows Language Depot Schinese Tower of Origin2-Worm's Nest
+setManifestid(1000281, "2507006329102947433", 462877584)
+addappid(1000282, 1, "614fe72128b3d93b19374cbf674c20fdc2efea33d49c882fa0e703b8fda2c7cb") --Main Windows Language Depot English Tower of Origin2-Worm's Nest
+setManifestid(1000282, "3147797639535336698", 462879104)
+addappid(1000283, 1, "f9c47c84d9cf21f679e89069e4c4bc2ccc216dda948157896af11a62361f453f") --Main Windows Language Depot Tchinese Tower of Origin2-Worm's Nest
+setManifestid(1000283, "4469889477783455148", 462887920)
+--Dlcname 起源之塔2-虫巢【r18扩展包 】
+addappid(1757640) --Dlcname 起源之塔2-虫巢【r18扩展包 】
+addappid(1757641, 1, "2db3dcfdebdb61de007f96f815b2763dea289f1961bb8c66ab9e75c4f2df06fe") --Dlc Windows Language Depot Schinese 起源之塔2-虫巢【r18扩展包 】
+setManifestid(1757641, "4531531928502553674", 629247536)
+addappid(1757642, 1, "5d5ec6ebfbb394195e9d9a1d005db48b6ef2453d60b4e6085222a21757c6e21e") --Dlc Windows Language Depot English 起源之塔2-虫巢【r18扩展包 】
+setManifestid(1757642, "519284624362358028", 629251808)
+addappid(1757643, 1, "4ee3ea5ee18247ec67b32dc88e106e48e9e4e1ab01a94b8d46e5fbdd7893e30f") --Dlc Windows Language Depot Tchinese 起源之塔2-虫巢【r18扩展包 】
+setManifestid(1757643, "2739295844232099252", 629267200)
