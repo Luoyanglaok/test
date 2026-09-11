69b08e880da7469572db8e59241f13e685de607f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:40:55 +0800

    add 242550.lua

--- 242550.lua
+++ 242550.lua
@@ -1,7 +1,14 @@
-addappid(242550)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(242551,0,"686bb8789459e4e3030ad32b9375d784ae80215a36f25a6e2f7d9ca04a37adcd")
-setManifestid(242551,"8839431118021240556")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 242550.lua
+--Gamename Rayman® Legends
+addappid(242550, 1, "7726c65822b6070b546ea2f2d04af56b0cc57fcf0b3a03e13aa7e49119a537d7") --Mainappid Rayman® Legends
+addappid(242551, 1, "686bb8789459e4e3030ad32b9375d784ae80215a36f25a6e2f7d9ca04a37adcd") --Main Windows Depot Rayman® Legends
+setManifestid(242551, "8839431118021240556", 4838974640)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(247811) --Dlcname Rayman Legends - Game Key
