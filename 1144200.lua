39b320cfc13baf68d606c7422084ec99de1d8e8d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 14:36:08 +0800

    add 1144200.lua

--- 1144200.lua
+++ 1144200.lua
@@ -4,12 +4,6 @@
 addappid(1144200, 1, "fb29b3865255ec0e9230c029502cd87b8626fd12f9bdb2fa3df6cddf34e17179") --Mainappid Ready or Not
 addappid(1144201, 1, "b9236894ba071d12dd117ce16220fefeb20574d7c6f7d2170e3113346867f3d6") --Main Windows Language Depot English Ready or Not
 setManifestid(1144201, "4381209963403495092", 40287443200)
---Dlcname Ready or Not: Official Soundtrack
-addappid(3089910) --Dlcname Ready or Not: Official Soundtrack
-addappid(3089911, 1, "ad6e74ede75aba32203a011af40c506a771be84256f15775efce310170c4676c") --Dlc Windows Depot Ready or Not: Official Soundtrack
-setManifestid(3089911, "6229163073173979005", 269993776)
-addappid(3089912, 1, "cd045276bbdda167270fd46f0b129a781e705003f2af095fe71bec217d065b95") --Dlc Windows Depot Ready or Not: Official Soundtrack
-setManifestid(3089912, "1071123769281934173", 1479900080)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
