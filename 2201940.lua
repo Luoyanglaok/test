1399916793976f9e38b8f1da3610ba1f20fec8b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:42:26 +0800

    add 2201940.lua

--- 2201940.lua
+++ 2201940.lua
@@ -1,11 +1,29 @@
-addappid(2201940)
-setManifestid(228981,"7613356809904826842")
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2201941,0,"786baefb7662e006fd710680e9ad8ad20bfc6b6ec4ce089d528438f1ae7b480a")
-setManifestid(2201941,"1556849090050811681")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2201940.lua
+--Gamename Ship Graveyard Simulator 2
+addappid(2201940) --Mainappid Ship Graveyard Simulator 2
+addappid(2201941, 1, "786baefb7662e006fd710680e9ad8ad20bfc6b6ec4ce089d528438f1ae7b480a") --Main Windows Depot Ship Graveyard Simulator 2
+setManifestid(2201941, "7505975130084486348", 13662772848)
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2504670) --Dlcname Ship Graveyard Simulator 2 - Steel Giants DLC
+addappid(2628300) --Dlcname Ship Graveyard Simulator 2 - Warships DLC
+addappid(2909000) --Dlcname Ship Graveyard Simulator 2 - Submarines DLC
+addappid(3142330) --Dlcname Ship Graveyard Simulator 2 - Floating Cities DLC
+addappid(3550840) --Dlcname Ship Graveyard Simulator 2 - Steam Ships DLC
+addappid(3826010) --Dlcname Ship Graveyard Simulator 2 - Fishing Boats DLC
