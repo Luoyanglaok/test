8813aaa43ca7a1a37aab061a85a874ae8c77c9e3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 19:54:14 +0800

    add 3227760.lua

--- 3227760.lua
+++ 3227760.lua
@@ -1,11 +1,16 @@
-addappid(3227760)
-addappid(3227761,0,"aea5ec3b5d98a199b1648d8e8ecf86e1c81701ddca706e7d0fc97f8415a6f46f")
-setManifestid(3227761,"874557219778769599")
-addappid(3227762,0,"2552bf98b66adcedb6153e4d4294ee95bb124281a667843fbc66f5397bb1ec3d")
-setManifestid(3227762,"8861727928832313771")
-addappid(3227763,0,"fcd90f845c8f9207cc7a6dff64b174c3d1c987eeccf67ff4d83d18bfbaaa1bf0")
-setManifestid(3227763,"7778064901107986")
-addappid(3283000,0,"7067bd44fff2bb3a55e9223dabc1d1a00a6336ec747fb3859a8f5d27a0693fc6")
-setManifestid(3283000,"4877437083952888248")
-addappid(3284820,0,"f56b357bf5eca79547a270eb8b527e97905dd22aa14a5c03ea293eb545bcff90")
-setManifestid(3284820,"1617866985731512989")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3227760.lua
+--Gamename 纸嫁衣6千秋魇
+addappid(3227760) --Mainappid 纸嫁衣6千秋魇
+addappid(3227761, 1, "aea5ec3b5d98a199b1648d8e8ecf86e1c81701ddca706e7d0fc97f8415a6f46f") --Main Windows Depot 纸嫁衣6千秋魇
+setManifestid(3227761, "5959323662908354834", 77590656)
+addappid(3227762, 1, "2552bf98b66adcedb6153e4d4294ee95bb124281a667843fbc66f5397bb1ec3d") --Main Windows Depot 纸嫁衣6千秋魇
+setManifestid(3227762, "7564816117602541524", 79647680)
+addappid(3227763, 1, "fcd90f845c8f9207cc7a6dff64b174c3d1c987eeccf67ff4d83d18bfbaaa1bf0") --Main Windows Depot 纸嫁衣6千秋魇
+setManifestid(3227763, "7664581494230801369", 790416048)
+--Dlcname 纸嫁衣6千秋魇-原声集
+addappid(3283000, 1, "7067bd44fff2bb3a55e9223dabc1d1a00a6336ec747fb3859a8f5d27a0693fc6") --Dlcname 纸嫁衣6千秋魇-原声集
+setManifestid(3283000, "4877437083952888248", 47845184)
+--Dlcname 纸嫁衣6千秋魇-原画与设定集
+addappid(3284820, 1, "f56b357bf5eca79547a270eb8b527e97905dd22aa14a5c03ea293eb545bcff90") --Dlcname 纸嫁衣6千秋魇-原画与设定集
+setManifestid(3284820, "1617866985731512989", 122741168)
