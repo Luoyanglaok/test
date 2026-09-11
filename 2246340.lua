81a236a84440533214e3d41a0f52a6ec7481e73c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 09:14:08 +0800

    add 2246340.lua

--- 2246340.lua
+++ 2246340.lua
@@ -1,208 +1,78 @@
-addappid(2246340)
-addappid(4829700)
-addappid(3289630)
-addappid(3336860)
-addappid(3116990)
-addappid(3410840)
-addappid(3117090)
-addappid(3117100)
-addappid(3117110)
-addappid(3117120)
-addappid(3117130)
-addappid(3117140)
-addappid(3117170)
-addappid(3157720)
-addappid(3157730)
-addappid(3117180)
-addappid(3117190)
-addappid(3117200)
-addappid(3117210)
-addappid(3117220)
-addappid(3117230)
-addappid(3289620)
-addappid(3336850)
-addappid(3336890)
-addappid(3117250)
-addappid(3117260)
-addappid(3117280)
-addappid(3117290)
-addappid(3117370)
-addappid(3117430)
-addappid(3117440)
-addappid(3117450)
-addappid(3117460)
-addappid(3117500)
-addappid(3117300)
-addappid(3117310)
-addappid(3117320)
-addappid(3117330)
-addappid(3117340)
-addappid(3117350)
-addappid(3117520)
-addappid(3117530)
-addappid(3117540)
-addappid(3117550)
-addappid(3117510)
-addappid(3117560)
-addappid(3360450)
-addappid(3360460)
-addappid(3360470)
-addappid(3360480)
-addappid(3289640)
-addappid(3289650)
-addappid(3289700)
-addappid(3289710)
-addappid(3289660)
-addappid(3289670)
-addappid(3289680)
-addappid(3289690)
-addappid(3360490)
-addappid(3360500)
-addappid(3360510)
-addappid(3360520)
-addappid(3502660)
-addappid(3360530)
-addappid(3410850)
-addappid(3289720)
-addappid(3289730)
-addappid(3289740)
-addappid(3289750)
-addappid(3289760)
-addappid(3289830)
-addappid(3289840)
-addappid(3289850)
-addappid(3289770)
-addappid(3289780)
-addappid(3289790)
-addappid(3289800)
-addappid(3289810)
-addappid(3289820)
-addappid(3289860)
-addappid(3289870)
-addappid(3289880)
-addappid(3289890)
-addappid(3619120)
-addappid(3619130)
-addappid(3619140)
-addappid(3619150)
-addappid(3619160)
-addappid(3619170)
-addappid(3619180)
-addappid(3619190)
-addappid(3289900)
-addappid(3289910)
-addappid(3360540)
-addappid(3360550)
-addappid(3289960)
-addappid(3289980)
-addappid(3289920)
-addappid(3289930)
-addappid(3289940)
-addappid(3289950)
-addappid(3619200)
-addappid(3619210)
-addappid(3619220)
-addappid(3696220)
-addappid(3696230)
-addappid(3696240)
-addappid(3696250)
-addappid(3696260)
-addappid(3696270)
-addappid(3696280)
-addappid(3696290)
-addappid(3696300)
-addappid(3767510)
-addappid(3767520)
-addappid(3696310)
-addappid(3696340)
-addappid(3696350)
-addappid(3696360)
-addappid(3696480)
-addappid(3747820)
-addappid(3747830)
-addappid(3747840)
-addappid(3747850)
-addappid(3747860)
-addappid(3801040)
-addappid(3336910)
-addappid(3336920)
-addappid(3336930)
-addappid(3696370)
-addappid(3696380)
-addappid(3696390)
-addappid(3696400)
-addappid(3696420)
-addappid(3696430)
-addappid(3696440)
-addappid(3696450)
-addappid(3696460)
-addappid(3767530)
-addappid(3772970)
-addappid(3696470)
-addappid(3696610)
-addappid(3696620)
-addappid(3696630)
-addappid(3696640)
-addappid(3696750)
-addappid(3696760)
-addappid(3696770)
-addappid(3696780)
-addappid(3696790)
-addappid(3696800)
-addappid(3696810)
-addappid(3696820)
-addappid(3696830)
-addappid(3696840)
-addappid(3696850)
-addappid(3696860)
-addappid(3696870)
-addappid(3696890)
-addappid(3696900)
-addappid(3696910)
-addappid(3696920)
-addappid(3696930)
-addappid(3932400)
-addappid(3932410)
-addappid(3932420)
-addappid(3932430)
-addappid(3932440)
-addappid(3932450)
-addappid(3959310)
-addappid(3959320)
-addappid(3959330)
-addappid(3959340)
-addappid(3959350)
-addappid(3932460)
-addappid(3117390)
-addappid(3117400)
-addappid(3968920)
-addappid(3696940)
-addappid(3696950)
-addappid(3696960)
-addappid(3696970)
-addappid(3932490)
-addappid(3932500)
-addappid(3696980)
-addappid(3696990)
-addappid(3697010)
-addappid(3697020)
-addappid(3697030)
-addappid(3697050)
-addappid(3885660)
-addappid(3968930)
-addappid(3767550)
-addappid(3767560)
-addappid(3696510)
-addappid(3767570)
-addappid(3767580)
-addappid(3696490)
-addappid(3696500)
-addappid(3697060)
-addappid(4123270)
-addappid(3308900)
-addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-setManifestid(2246341,"4276490724741861191")
-setManifestid(3308900,"5891069512660502042")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"5753583882400741046")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2246340.lua
+--Gamename Monster Hunter Wilds
+addappid(2246340, 1, "3376b3ba2a4e55044a3116230ebeafcc862380c70ed5329384fcbbeeca7beaf3") --Mainappid Monster Hunter Wilds
+addappid(2246341, 1, "15f418b98b8ade3be07b095f07a9ffc62ab918cf659a2fd7465fcb41c462f0af") --Main Windows Depot Monster Hunter Wilds
+setManifestid(2246341, "4844075769269645714", 109384001824)
+--Dlcname Monster Hunter Wilds - High Resolution Texture Pack
+addappid(3308900, 1, "86e9ff2249c193631e8e04855d9c6154071b06491dced87279c1c72b1662556d") --Dlcname Monster Hunter Wilds - High Resolution Texture Pack
+setManifestid(3308900, "2311240512518968412", 31415547440)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3116950) --Dlcname Monster Hunter Wilds - Hunter Layered Armor Set: Guild Knight
+addappid(3116960) --Dlcname Monster Hunter Wilds - Talisman: Hope Charm
+addappid(3116990) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 1
+addappid(3117030) --Dlcname Monster Hunter Wilds - Hunter Layered Armor: Wyverian Ears
+addappid(3117050) --Dlcname Monster Hunter Wilds - Premium Bonus Hunter Profile Set
+addappid(3117070) --Dlcname Monster Hunter Wilds - BGM: Proof of a Hero (2025 Recording)
+addappid(3117090) --Dlcname Monster Hunter Wilds - Hunter Layered Armor Set: Feudal Soldier
+addappid(3117100) --Dlcname Monster Hunter Wilds - Hunter Layered Armor: Fencer's Eyepatch
+addappid(3117110) --Dlcname Monster Hunter Wilds - Hunter Layered Armor: Oni Horns Wig
+addappid(3117120) --Dlcname Monster Hunter Wilds - Seikret Decoration: Soldier's Caparison
+addappid(3117130) --Dlcname Monster Hunter Wilds - Seikret Decoration: General's Caparison
+addappid(3117140) --Dlcname Monster Hunter Wilds - Felyne Layered Armor Set: Felyne Ashigaru
+addappid(3117170) --Dlcname Monster Hunter Wilds - Pendant: Avian Wind Chime
+addappid(3117180) --Dlcname Monster Hunter Wilds - Hairstyle: Hero's Topknot
+addappid(3117190) --Dlcname Monster Hunter Wilds - Hairstyle: Refined Warrior
+addappid(3117200) --Dlcname Monster Hunter Wilds - Makeup/Face Paint: Hunter's Kumadori
+addappid(3117210) --Dlcname Monster Hunter Wilds - Makeup/Face Paint: Special Bloom
+addappid(3117220) --Dlcname Monster Hunter Wilds - Sticker Set: Avis Unit
+addappid(3117230) --Dlcname Monster Hunter Wilds - Sticker Set: Monsters of the Windward Plains
+addappid(3117240) --Dlcname Monster Hunter Wilds - Nameplate: Extra Frame — Russet Dawn
+addappid(3117250) --Dlcname Monster Hunter Wilds - Hunter Layered Armor Set: Noblesse
+addappid(3117260) --Dlcname Monster Hunter Wilds - Hunter Layered Armor: Florescent Circlet
+addappid(3117280) --Dlcname Monster Hunter Wilds - Seikret Decoration: Purehorn Caparison
+addappid(3117290) --Dlcname Monster Hunter Wilds - Seikret Decoration: Duskhorn Caparison
+addappid(3117300) --Dlcname Monster Hunter Wilds - Pendant: White Fairy Light
+addappid(3117310) --Dlcname Monster Hunter Wilds - Pendant: Red Fairy Light
+addappid(3117320) --Dlcname Monster Hunter Wilds - Pendant: Green Fairy Light
+addappid(3117330) --Dlcname Monster Hunter Wilds - Pendant: Blue Fairy Light
+addappid(3117340) --Dlcname Monster Hunter Wilds - Pendant: Violet Fairy Light
+addappid(3117350) --Dlcname Monster Hunter Wilds - Pendant: Pink Fairy Light
+addappid(3117370) --Dlcname Monster Hunter Wilds - Photo Pose Set Vol. 1
+addappid(3117390) --Dlcname Monster Hunter Wilds - Photo Pose: Adventure Begins Pose 1 & 2
+addappid(3117400) --Dlcname Monster Hunter Wilds - Photo Pose: Adventure Begins Pose 3 & 4
+addappid(3117430) --Dlcname Monster Hunter Wilds - Makeup/Face Paint: Monoblos Heart
+addappid(3117440) --Dlcname Monster Hunter Wilds - Tent Customization Set Vol. 1
+addappid(3117450) --Dlcname Monster Hunter Wilds - Camp Gear Set Vol. 1
+addappid(3117460) --Dlcname Monster Hunter Wilds - BGM Set Vol. 1
+addappid(3117500) --Dlcname Monster Hunter Wilds - Sticker Set: Expedition Pals
+addappid(3117510) --Dlcname Monster Hunter Wilds - Alma Outfit: Scrivener's Coat
+addappid(3117520) --Dlcname Monster Hunter Wilds - Alma Outfit: New World Commission
+addappid(3117530) --Dlcname Monster Hunter Wilds - Photo Pose Set Vol. 2
+addappid(3117540) --Dlcname Monster Hunter Wilds - Gesture: Celebratory Knee Slide
+addappid(3117550) --Dlcname Monster Hunter Wilds - Gesture: Rearing To Go
+addappid(3117560) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 2
+addappid(3157720) --Dlcname Monster Hunter Wilds - Gesture: Battle Cry
+addappid(3157730) --Dlcname Monster Hunter Wilds - Gesture: Uchiko
+addappid(3289620) --Dlcname Monster Hunter Wilds - Character Edit Voucher: Three-Voucher Pack
+addappid(3289630) --Dlcname Monster Hunter Wilds - Character Edit Voucher: Single Voucher (Free Trial Version)
+addappid(3289640) --Dlcname Monster Hunter Wilds - Alma Outfit: Spring Blossom Kimono
+addappid(3289650) --Dlcname Monster Hunter Wilds - Seikret Decoration: Majestic Dragon's Caparison
+addappid(3289660) --Dlcname Monster Hunter Wilds - Pendant: Sakura Posy: White
+addappid(3289670) --Dlcname Monster Hunter Wilds - Pendant: Sakura Posy: Colorful
+addappid(3289680) --Dlcname Monster Hunter Wilds - Pendant: White Cat Bell
+addappid(3289690) --Dlcname Monster Hunter Wilds - Pendant: Black Cat Bell
+addappid(3289700) --Dlcname Monster Hunter Wilds - Gesture Set Vol. 3
+addappid(3289710) --Dlcname Monster Hunter Wilds - Sticker Set: Blossomdance
+addappid(3289720) --Dlcname Monster Hunter Wilds - Hunter Layered Armor Set: Cypurrpunk
+addappid(3289730) --Dlcname Monster Hunter Wilds - Hairstyle: Wild Braids
+addappid(3289740) --Dlcname Monster Hunter Wilds - Hairstyle: Futuristic Crop
+addappid(3289750) --Dlcname Monster Hunter Wilds - Makeup/Face Paint: Drawn Eyelashes
+addappid(3289760) --Dlcname Monster Hunter Wilds - Makeup/Face Paint: Flames
+addappid(3289770) --Dlcname Monster Hunter Wilds - Pendant: Strigible: Amstrigian
+addappid(3289780) --Dlcname Monster Hunter Wilds - Pendant: Strigible: Hymstrigian
+addappid(3289790) --Dlcname Monster Hunter Wilds - Pendant: Strigible: Cypurrpunk
