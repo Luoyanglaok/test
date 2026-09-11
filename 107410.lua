4519bb534b02e33552786ae58d65f32dd1af78b2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 17:06:30 +0800

    add 107410.lua

--- 107410.lua
+++ 107410.lua
@@ -1,43 +1,148 @@
-addappid(107410)
-addappid(4198200)
-addappid(2647830)
-addappid(2647760)
-addappid(395180)
-addappid(1021790)
-addappid(601670)
-addappid(304380)
-addappid(288520)
-addappid(332350)
-addappid(571710)
-addappid(639600)
-addappid(275700)
-addappid(744950)
-addappid(249860)
-addappid(798390)
-addappid(1042220)
-addappid(1227700)
-addappid(1616620)
-addappid(1294440)
-addappid(1175380)
-addappid(1681170)
-addappid(1325500)
-addappid(228983,1,"77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b")
-addappid(228984,1,"df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068")
-addappid(228985,1,"21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c")
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-addappid(229004,1,"56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0")
-addappid(229006,1,"9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416")
-setManifestid(107411,"1149770872896707278")
-setManifestid(107413,"4517156068945193185")
-setManifestid(107420,"7053381975926971498")
-setManifestid(249510,"6620680010375519103")
-setManifestid(395180,"3163523818171719496")
-setManifestid(1681170,"2600990344855222521")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229004,"5220958916987797232")
-setManifestid(229006,"1784011429307107530")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 107410.lua
+--Gamename Arma 3
+addappid(107410, 1, "41ff2badbff9351171ac6dd5b518595760f066656bcc157cdd77aa28046e176e") --Mainappid Arma 3
+addappid(107411, 1, "833b10a4156402ff5a874c5fe740af6294d055f92fc25c69607b3351ffbfe679") --Main Windows Depot Arma 3
+setManifestid(107411, "1149770872896707278", 34753459760)
+addappid(107413, 1, "44faf57ebfee712ae9316d3f7ec77e3a763190dc8325e63cc8aa23235718f798") --Main Windows Depot Arma 3
+setManifestid(107413, "4517156068945193185", 50109088)
+addappid(107414, 1, "9e0b330c18485bdf6a3444b5ec24c0f56f2234669f54ba15d8640af0b62fa9d3") --Main Windows Depot Arma 3
+setManifestid(107414, "7587485463657503368", 17289264)
+addappid(107415, 1, "74e3e7e31e761aa6fa007fc703408e88a980512d3bc0f737c2b724bcef1ccc38") --Main Windows Depot Arma 3
+setManifestid(107415, "10973010387952070", 19463504)
+addappid(107416, 1, "748d4dde34f8d535cfc2306c110fd9646c6644d814c0fff1dc853dd70953b353") --Main Windows Depot Arma 3
+setManifestid(107416, "3085402364414286678", 19378432)
+addappid(107417, 1, "9ebe30dbeca6377c4d81a2d7b181eb449164ac5b9975f02c12efeb808af3b7f1") --Main Windows Depot Arma 3
+setManifestid(107417, "7042899986204108462", 19496544)
+addappid(107418, 1, "b20b8b302fd775dd5f510f4c6d92303e5e9e91a5c1dd63b49d5fba5c73a16e15") --Main Windows Depot Arma 3
+setManifestid(107418, "6202893213668425446", 19598624)
+addappid(107419, 1, "ae1c1fd9b03805ae6f6e4a1d1356142422c19353b5acc12ede23ad710b1a4ed2") --Main Windows Depot Arma 3
+setManifestid(107419, "5730151752640405886", 25430448)
+addappid(107420, 1, "721d759ad3199299425a057d3075e0bf1078ea624d65faae71bd11254617a966") --Main Windows Depot Arma 3
+setManifestid(107420, "7053381975926971498", 8356416)
+addappid(107421, 1, "965c797f05169d0a632cea851fab1d418d19ba8557a54af8f32667658782dca1") --Main Windows Language Depot French Arma 3
+setManifestid(107421, "2479538286611674481", 7952256)
+addappid(107422, 1, "5dc537b4337aaafa4d100493e59e8a7ee84cbcedb16777622685a2047d91a306") --Main Windows Language Depot German Arma 3
+setManifestid(107422, "694831486858131928", 7953520)
+addappid(107423, 1, "4a4d56da6c332cd0be710e0e7b3117dab1f73ed51810d800a7e747011ab69a85") --Main Windows Language Depot Italian Arma 3
+setManifestid(107423, "5294990231226172562", 7950320)
+addappid(107424, 1, "f9348a2559cf47ded3a0f49e4dcb54281587c1f5725499091856cf1866c49fba") --Main Windows Language Depot Spanish Arma 3
+setManifestid(107424, "3395489159717928684", 7955904)
+addappid(107425, 1, "398f6319a28f9570857894591a9c2e2168669f521529f30bc42d00b73b9a89dc") --Main Windows Language Depot Czech Arma 3
+setManifestid(107425, "4906889931917921096", 8054576)
+addappid(107426, 1, "6c45afa220059d2deb0b16d7ee972cc58e158f8e55b885bf09296867ba76e3ab") --Main Windows Language Depot Russian Arma 3
+setManifestid(107426, "4684404531445583131", 7983904)
+addappid(107427, 1, "2e127da445a329f971cb2da6f95a042f20ea9b14829fb74179f34841e710a815") --Main Windows Language Depot Polish Arma 3
+setManifestid(107427, "8406091474147147301", 7957744)
+addappid(107428, 1, "055d57fcc12ff12b49a329f177eba80af24ec36b377ba03148e2d9d4172effa7") --Main Windows Language Depot Brazilian Arma 3
+setManifestid(107428, "5514411074045516159", 7953984)
+addappid(107429, 1, "5f3b60be0c035cec10f8fb64490071b14f4a6ca0375f7495e2b2dab864f8904c") --Main Windows Language Depot Koreana Arma 3
+setManifestid(107429, "9165345853969700754", 8689200)
+addappid(249501, 1, "37f6a5a690ef6ecccf4eb4197d2256b611bf80cc95338ff552f8f09c100086a5") --Main Windows Language Depot Japanese Arma 3
+setManifestid(249501, "8720942636409428537", 8808896)
+addappid(249502, 1, "277ba94f2fa9a7f293ee635051aecd51bf5b7580fbde96927600d9f9f7fdf903") --Main Windows Depot Arma 3
+setManifestid(249502, "2855841879143950386", 43907696)
+addappid(249503, 1, "5957e2f2c177f505edfedc9170b8ca215df87c63267843d424bcda30eaf0beb0") --Main Windows Depot Arma 3
+setManifestid(249503, "662814218937232349", 60698064)
+addappid(249504, 1, "7a5c9a04f58f3f86cbdef4e9f2817629d08bbf84a1f4321e13f917f6e92887d6") --Main Windows Depot Arma 3
+setManifestid(249504, "1015968845214793196", 11838952160)
+addappid(249505, 1, "f77130c5d488aced7c009e872beb263ef9a845463aed4372c5968c310cc6565a") --Main Macos Depot Arma 3
+setManifestid(249505, "1737072357190833783", 34685421760)
+addappid(249506, 1, "720c4414485a1bd6aad38fb825f7455fc6f41d61176a6eab369d339a64601ff5") --Main Macos Depot Arma 3
+setManifestid(249506, "2099091726510050690", 19351728)
+addappid(249508, 1, "f384303deb8d08f3dcadcf71d670a0bae23e6e46f72b2e9426152a725c48a40b") --Main Macos Depot Arma 3
+setManifestid(249508, "2578592443209347791", 96920512)
+addappid(249510, 1, "af7b2fba5a503e59c847039be22b27bde7385d837b8a4211cb46d77e70715022") --Main Windows Language Depot Schinese Arma 3
+setManifestid(249510, "6620680010375519103", 5821376)
+addappid(249511, 1, "cfd49c5a7dd4d67a3cebfaa501b735cef12ca4dbca4e21e5fe5df3984a4fe7c3") --Main Windows Language Depot Tchinese Arma 3
+setManifestid(249511, "2843568687622074286", 5957888)
+addappid(249512, 1, "e88d176e948e932b3f6682bc73e9836ee83bfbdb65ca649efdbc49e833c1cdc5") --Main Windows Language Depot Turkish Arma 3
+setManifestid(249512, "4734283135240260657", 4155120)
+addappid(249860, 1, "b1d6aa1d14f16b1f383aec21c0931c12832a42239eb026f5afaa6d8e970de449") --Main Windows Depot Arma 3
+setManifestid(249860, "7431003676632113359", 2187390224)
+addappid(1616621, 1, "55621765272f1ec180fb42a84aaab5c2e8779b05f73b2dc570b0a823c71d475a") --Main Windows Depot Arma 3
+setManifestid(1616621, "3397792532337084452", 216284176)
+addappid(1616622, 1, "9110e2a79a3a6fc7c9506845d9ad0c656cfd2959b576009c703b5338e140d4ff") --Main Windows Depot Arma 3
+setManifestid(1616622, "7363967124564038601", 495298480)
+--Dlcname Arma 3 Soundtrack
+addtoken(249860, "16194985672234201971")
+--Dlcname Arma 3 Maps
+addtoken(249861, "6943354596756530945")
+addappid(249861, 1, "352a937f53f6ec85eae8f2755e3b1609dca20c54af30799cd956e6f7d046c932") --Dlcname Arma 3 Maps
+setManifestid(249861, "4518665814873472898", 420719152)
+--Dlcname Arma 3 Tactical Guide
+addtoken(249862, "16685319281903082284")
+addappid(249862, 1, "8e31c7702caf35ce3aaedd6b0e2033ca5465b072c4de869482f8010f4d4b3117") --Dlcname Arma 3 Tactical Guide
+setManifestid(249862, "2738283997064954949", 75877344)
+--Dlcname Arma 3 Apex
+addappid(395180, 1, "96c966f67619cb8112c41edd7b20dc6ae55e1fa5fd6de3b7e503e8951c6e43ef") --Dlcname Arma 3 Apex
+addappid(249509, 1, "eabd6887679e6f371df7c224fbebb6387e6be162c349c64d943a3cfaa09e4639") --Dlc Macos Depot Arma 3 Apex
+setManifestid(249509, "4555863940473377516", 0)
+setManifestid(395180, "3163523818171719496", 1696)
+--Dlcname Arma 3 Contact
+addappid(1021790, 1, "2a1a84bef4bca331a78e022e133ca531af3062be29de09bd05934c9ce49e733f") --Dlcname Arma 3 Contact
+setManifestid(1021790, "655405770170985748", 982532400)
+--Dlcname Arma 3 Creator DLC: Global Mobilization - Cold War Germany
+addappid(1042220, 1, "d2d2b4ba7701e9067b5d6b00d91c57839c42a7032a985a69c57612b16ce61e21") --Dlcname Arma 3 Creator DLC: Global Mobilization - Cold War Germany
+setManifestid(1042220, "5561821618542621196", 38875959808)
+addappid(1042221, 1, "285b4b9b2bd5ad724cdf3997240446475ac631a84ff726879ed717cf5ed635b9") --Dlc Macos Depot Arma 3 Creator DLC: Global Mobilization - Cold War Germany
+setManifestid(1042221, "2053062434401462647", 38875959808)
+--Dlcname Arma 3 Creator DLC: Spearhead 1944
+addappid(1175380, 1, "1527e79ee970b83c9e29530acf0ad8c557fec87abcd7cdfafe10a9163ccc6896") --Dlcname Arma 3 Creator DLC: Spearhead 1944
+setManifestid(1175380, "8749914837138314128", 31547420816)
+addappid(1175381, 1, "6173300769cbd0edac4a68b33bf2333f3bc062c9b5b1da31f9c79549ae8e947e") --Dlc Macos Depot Arma 3 Creator DLC: Spearhead 1944
+setManifestid(1175381, "6576197082193427041", 31184863104)
+--Dlcname Arma 3 Creator DLC: S.O.G. Prairie Fire
+addappid(1227700, 1, "62492374d9e872dec77870f24170bb85cc1b28a1d792c8f081be2d5d3d167951") --Dlcname Arma 3 Creator DLC: S.O.G. Prairie Fire
+setManifestid(1227700, "1889876263343541672", 38898963648)
+addappid(1227701, 1, "70adde1421b521bb699fb2344e414ae08c97c56ea1a22342359778f40465302f") --Dlc Macos Depot Arma 3 Creator DLC: S.O.G. Prairie Fire
+setManifestid(1227701, "8778927882579535691", 38897578080)
+--Dlcname Arma 3 Creator DLC: CSLA Iron Curtain
+addappid(1294440, 1, "5b1e01c821d9d3707104d4faf25d64faac508a1304a8324f048e56566bb9821f") --Dlcname Arma 3 Creator DLC: CSLA Iron Curtain
+setManifestid(1294440, "8320117063324338394", 10623241056)
+addappid(1294441, 1, "c2c39942b00fd547c6e3613a863ad9cb60eeb74e1208742302b4ba06309f7cfe") --Dlc Macos Depot Arma 3 Creator DLC: CSLA Iron Curtain
+setManifestid(1294441, "5407594059006667059", 10623241056)
+--Dlcname Arma 3 Creator DLC: S.O.G. Prairie Fire Soundtrack
+addappid(1616620) --Dlcname Arma 3 Creator DLC: S.O.G. Prairie Fire Soundtrack
+--Dlcname Arma 3 Creator DLC: Western Sahara
+addappid(1681170, 1, "89aa19de49b63e33c462859290ffc3e0a36b3168627208399bf4b8d1f160d8e3") --Dlcname Arma 3 Creator DLC: Western Sahara
+setManifestid(1681170, "2600990344855222521", 5327120864)
+addappid(1681171, 1, "2b42cb143feee60349f0ddd52e0f275210a36b13db67378295d48e24ace805e0") --Dlc Macos Depot Arma 3 Creator DLC: Western Sahara
+setManifestid(1681171, "6303306766811849415", 5311918240)
+--Dlcname Arma 3 Creator DLC: Reaction Forces
+addappid(2647760, 1, "ea7053053da76cf6d68b4561d05eeba1736c31409b4e4e67afd56fff5533add1") --Dlcname Arma 3 Creator DLC: Reaction Forces
+setManifestid(2647760, "8773700064943194686", 4257997840)
+addappid(2647761, 1, "d3823ed85d445668a83045544c6c20c0aae5a6e98a99d06fe56dc6ceb3474f99") --Dlc Macos Depot Arma 3 Creator DLC: Reaction Forces
+setManifestid(2647761, "2576413575760136050", 4251103216)
+--Dlcname Arma 3 Creator DLC: Expeditionary Forces
+addappid(2647830, 1, "e5e2d7d83696d653ec6a4893b702afadf9978113d4c0f565d16a53fa04b3289a") --Dlcname Arma 3 Creator DLC: Expeditionary Forces
+setManifestid(2647830, "7181533284673180153", 3692471440)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(275700) --Dlcname Arma 3 Zeus
+addappid(288520) --Dlcname Arma 3 Karts
+addappid(304380) --Dlcname Arma 3 Helicopters
+addappid(304400) --Dlcname AppID 304400
+addtoken(304400, "2907071240555921015")
+addappid(332350) --Dlcname Arma 3 Marksmen
+addappid(571710) --Dlcname Arma 3 Laws of War
+addappid(601670) --Dlcname Arma 3 Jets
+addappid(612480) --Dlcname Arma 3 DLC Bundle 2
+addappid(639600) --Dlcname Arma 3 Malden
+addappid(744950) --Dlcname Arma 3 Tac-Ops Mission Pack
+addappid(798390) --Dlcname Arma 3 Tanks
+addappid(1325500) --Dlcname Arma 3 Art of War
+addappid(4198200) --Dlcname Arma 3 Pass
