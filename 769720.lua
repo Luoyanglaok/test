ac52df4b0c1264140049df021895edfe7c5d8078
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:59:26 +0800

    add 769720.lua

--- 769720.lua
+++ 769720.lua
@@ -1,11 +1,18 @@
-addappid(769720)
-addappid(228983)
-setManifestid(228983,"8124929965194586177")
-addappid(228985)
-setManifestid(228985,"3966345552745568756")
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(769721,0,"0c4130a7e64c1d025fb58d10c6b6285ff85fbc2b2c95c0c64ae18a05bad65e62")
-setManifestid(769721,"4854039244090262281")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 769720.lua
+--Gamename Island of Deception
+addappid(769720, 1, "41cb434de998f3ff19d1d5116c89f0e8d4af002e972abe5712514fa6356691cd") --Mainappid Island of Deception
+addappid(769721, 1, "0c4130a7e64c1d025fb58d10c6b6285ff85fbc2b2c95c0c64ae18a05bad65e62") --Main Windows Depot Island of Deception
+setManifestid(769721, "4854039244090262281", 1292776640)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1345630) --Dlcname 风暴岛-正式版DLC
+--Missing Dlcs
+--Dlcname 风暴岛 - 新角色：朱迪
