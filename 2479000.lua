bc76ff93a524cc1f6f1b5b0f77f24c89cd109563
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 21:36:39 +0800

    add 2479000.lua

--- 2479000.lua
+++ 2479000.lua
@@ -1,4 +1,9 @@
-addappid(2479000)
-setManifestid(228989,"3514306556860204959")
-addappid(2479001,0,"f99f8b999e8d4e38a3d473f98e8a2e82918be24e42e446735bd2f89be509fe80")
-setManifestid(2479001,"6429320794903314615")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2479000.lua
+--Gamename Noise-o-matic
+addappid(2479000, 1, "ba601ee92b076f5f90242392f65b3e9eeb71855619f343afb34f09ced6d82a4c") --Mainappid Noise-o-matic
+addappid(2479001, 1, "f99f8b999e8d4e38a3d473f98e8a2e82918be24e42e446735bd2f89be509fe80") --Main Windows Depot Noise-o-matic
+setManifestid(2479001, "2154931086049951658", 27985232)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
