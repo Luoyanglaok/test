bfe8f560324664f51a7bbd0c5f3bc655f2c28162
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 01:53:04 +0800

    add 3293260.lua

---
+++ 3293260.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3293260.lua
+--Gamename Waterpark Simulator
+addappid(3293260, 1, "1c76571d2ad12cc3b27e52ee7293568e87fd1e9eab2d765faf87d0df7b83c16e") --Mainappid Waterpark Simulator
+addappid(3293261, 1, "2ae10ca475da07718687c9d3078d27cd93464eb539d53ed314d60fc261929fd7") --Main Windows Depot Waterpark Simulator
+setManifestid(3293261, "3425168740030409028", 7059087360)
+addappid(4811281, 1, "9d95d318dd2fb848f2903d9ad834fed8b4d3fc44e4d5d89e9569cc2ffa95b00d") --Main Windows Depot Waterpark Simulator
+setManifestid(4811281, "4112534515767971870", 134368384)
+--Dlcname Waterpark Simulator Soundtrack
+addappid(4811280) --Dlcname Waterpark Simulator Soundtrack
