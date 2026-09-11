1748c046eed9e8d575b9bb29bedc189911c4ebf5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 00:19:28 +0800

    add 1663440.lua

--- 1663440.lua
+++ 1663440.lua
@@ -1,3 +1,6 @@
-addappid(1663440)
-addappid(1663441,0,"00ae3f06f2830d99a0c32e9f3b7f9a6c23e99bbfe03cdedfeab1594e028357e5")
-setManifestid(1663441,"7116700483183422903")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1663440.lua
+--Gamename Hot Cam
+addappid(1663440) --Mainappid Hot Cam
+addappid(1663441, 1, "00ae3f06f2830d99a0c32e9f3b7f9a6c23e99bbfe03cdedfeab1594e028357e5") --Main Windows Depot Hot Cam
+setManifestid(1663441, "7116700483183422903", 205998608)
