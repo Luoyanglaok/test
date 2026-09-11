08786b5bdf0431429159fa4514aedc9fed5f8855
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 18:27:05 +0800

    add 1606180.lua

--- 1606180.lua
+++ 1606180.lua
@@ -1,5 +1,31 @@
-addappid(1606180)
-addappid(1606181,0,"78085baa1a1489cac9f2593fc792421a3c773edf3317721f5b02ccb7b9c07898")
-setManifestid(1606181,"436281305868505834")
-addappid(2905470,0,"5e0e8e4e9b25bd36951858ffd7fe45086c92ca50d89481201f01b39e18aff304")
-setManifestid(2905470,"2357287501546393262")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1606180.lua
+--Gamename 下一站江湖Ⅱ
+addappid(1606180, 1, "8ac605401cca020b8849769c24cbdab583d22e6aea9f5a5394d3df1334cd2582") --Mainappid 下一站江湖Ⅱ
+addappid(1606181, 1, "78085baa1a1489cac9f2593fc792421a3c773edf3317721f5b02ccb7b9c07898") --Main Windows Depot 下一站江湖Ⅱ
+setManifestid(1606181, "5753319134771590542", 15029568320)
+--Dlcname 下一站江湖Ⅱ-纯外观DLC《江湖如梦》
+addappid(2905470, 1, "5e0e8e4e9b25bd36951858ffd7fe45086c92ca50d89481201f01b39e18aff304") --Dlcname 下一站江湖Ⅱ-纯外观DLC《江湖如梦》
+setManifestid(2905470, "2357287501546393262", 190107376)
+addappid(2998780) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《逃婚千金》
+addappid(3007440) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《江湖霸图：开宗立派》
+addappid(3099590) --Dlcname 下一站江湖Ⅱ-纯外观DLC《舞弄太极》
+addappid(3137140) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《江湖霸图：江湖霸业》
+addappid(3169410) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《霸业飞花》
+addappid(3255940) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《寒门书生》
+addappid(3323010) --Dlcname 下一站江湖Ⅱ-纯外观DLC《日月同辉》
+addappid(3349590) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《市井江湖》
+addappid(3386320) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《百业待兴》
+addappid(3402520) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《江湖浪子》
+addappid(3422810) --Dlcname 下一站江湖Ⅱ-纯外观DLC《传世降龙》
+addappid(3552810) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《模拟江湖》
+addappid(3627630) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《武林纷争》
+addappid(3636310) --Dlcname 下一站江湖Ⅱ-纯外观DLC《剑指六脉》
+addappid(3759070) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《塞外风云》
+addappid(3978160) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《侠骨柔情》
+addappid(4110930) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《黑榜风云》
+addappid(4276540) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《天剑龙刀》
+addappid(4330480) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《人在江湖》
+addappid(4330490) --Dlcname 下一站江湖Ⅱ-纯外观DLC《剑过无痕》
+addappid(4587320) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《威压东瀛》
+addappid(4763140) --Dlcname 下一站江湖Ⅱ-纯玩法DLC（免费）《绝代双骄》
