4b3db7d37bc20bdb12dc20bfbbc2b9e69dff49a0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:15:07 +0800

    add 1594320.lua

--- 1594320.lua
+++ 1594320.lua
@@ -3,10 +3,16 @@
 --Gamename Captain of Industry
 addappid(1594320, 1, "05671966acb0423953d9ccc1db5981a7d6f00bce411bddda621eb4959f9d2123") --Mainappid Captain of Industry
 addappid(1594321, 1, "4ff3d26a9ec89b9d17aa43ee05ea46cb83cd638a31b36a385b43fc810ccc6703") --Main Windows Depot Captain of Industry
-setManifestid(1594321, "6265590129150821136", 3329792496)
+setManifestid(1594321, "7114462889834617875", 3329823552)
+--Dlcname Captain of Industry Soundtrack
+addappid(2004660) --Dlcname Captain of Industry Soundtrack
+addappid(2004661, 1, "d5f6bd754404f2b19465e5d4611487dba3e338d3ad20738c582e8f4a5ba0062b") --Dlc Windows Depot Captain of Industry Soundtrack
+setManifestid(2004661, "5745558198071497822", 255060960)
+addappid(2004663, 1, "4d438c4923cd4a3f5884316f0fd3a575a2c26209fac9cb9c7216c79ec27fa423") --Dlc Windows Depot Captain of Industry Soundtrack
+setManifestid(2004663, "2312159052591282248", 710077712)
 --Dlcname Captain of Industry - Supporter pack
 addappid(2004740, 1, "99e976dbccd9e46b15b201695ea57868cd42725d13bb6fc4512e5f3a7d620687") --Dlcname Captain of Industry - Supporter pack
-setManifestid(2004740, "5026221012306694212", 16482000)
+setManifestid(2004740, "7031609665574253041", 16481984)
 --Dlcname Captain of Industry - Trains expanded
 addappid(4349830, 1, "06605130aff9e40092bf43024ef429e6fd4c60c5da7f397898598e936d14e8a6") --Dlcname Captain of Industry - Trains expanded
-setManifestid(4349830, "3588577377178619199", 384101440)
+setManifestid(4349830, "4269206658089884517", 384101536)
