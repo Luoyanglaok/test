8bc968f29e696e5ecad0a059beba809cef009aeb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 23:58:33 +0800

    add 388750.lua

--- 388750.lua
+++ 388750.lua
@@ -1,4 +1,9 @@
-addappid(388750)
-setManifestid(228990,"1829726630299308803")
-addappid(388751,0,"2b953b431190646fdb936d3d5214f9c6c6f5d81635f0107b0a787af034cb8285")
-setManifestid(388751,"1630397797710183597")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 388750.lua
+--Gamename BlazBlue: Chronophantasma Extend
+addappid(388750) --Mainappid BlazBlue: Chronophantasma Extend
+addappid(388751, 1, "2b953b431190646fdb936d3d5214f9c6c6f5d81635f0107b0a787af034cb8285") --Main Windows Depot BlazBlue: Chronophantasma Extend
+setManifestid(388751, "1630397797710183597", 9593455072)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
