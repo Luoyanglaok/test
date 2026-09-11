471baec3570f585bcfc1342fe5602e4bb67e6a68
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 11:23:19 +0800

    add 3183280.lua

--- 3183280.lua
+++ 3183280.lua
@@ -1,7 +1,17 @@
-addappid(3183280)
-addappid(3183281,0,"68dce48db7977a0ea03b654ede2a578765ee4320c93955e29aeae0ef9bb43c9e")
-setManifestid(3183281,"2431580331874032394")
-addappid(3183282,0,"7fda33f42919c3fececa548528f54b0514a220b324f6a87377d20f63a8d7982f")
-setManifestid(3183282,"1460512481227690666")
-addappid(3477690,0,"09ad1044265abc607d130258e11d192531c0471b4755edbe711911048d6bd94f")
-setManifestid(3477690,"6790709864012105538")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3183280.lua
+--Gamename Game of Thrones: Kingsroad
+addappid(3183280, 1, "89ec42b7af958bd580011abadad61dd574f9f5483ca77baee3d85d77eb0c8b4c") --Mainappid Game of Thrones: Kingsroad
+addappid(3183281, 1, "68dce48db7977a0ea03b654ede2a578765ee4320c93955e29aeae0ef9bb43c9e") --Main Windows Depot Game of Thrones: Kingsroad
+setManifestid(3183281, "3109619855288264408", 633939760)
+addappid(3183282, 1, "7fda33f42919c3fececa548528f54b0514a220b324f6a87377d20f63a8d7982f") --Main Windows Depot Game of Thrones: Kingsroad
+setManifestid(3183282, "1828672081728776753", 27223939808)
+--Dlcname Game of Thrones: Kingsroad - Standard Pack
+addappid(3477690, 1, "09ad1044265abc607d130258e11d192531c0471b4755edbe711911048d6bd94f") --Dlcname Game of Thrones: Kingsroad - Standard Pack
+setManifestid(3477690, "6790709864012105538", 0)
+--Dlcname AppID 3508440
+addappid(3508440, 1, "397f159408f47e87b0ea008558a4b02c701c09cffbb5be3686ca0e6f05188f93") --Dlcname AppID 3508440
+setManifestid(3508440, "7793818489679416633", 0)
+--Dlcname AppID 3516830
+addappid(3516830, 1, "b3d00ce7c8f96ddaa9645e315ef5dcebc10ed2bb234eebd33ed32157f897f343") --Dlcname AppID 3516830
+setManifestid(3516830, "2386730318353588602", 0)
