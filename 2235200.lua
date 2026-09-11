d267b9392f0f9733675e2d076a28010101cf590e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 16:18:44 +0800

    add 2235200.lua

--- 2235200.lua
+++ 2235200.lua
@@ -1,3 +1,14 @@
-addappid(2235200)
-addappid(2235201,0,"64b6a7741dff52341709bb548d844f5cbfb4d551a165eba90db90d49ca88586c")
-setManifestid(2235201,"7104842568583203622")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2235200.lua
+--Gamename Neon Abyss 2
+addappid(2235200, 1, "8649ef76e1d2ecec1ac588ba6a2f342134b33e3f79d204cf4e9e86a0a6576fff") --Mainappid Neon Abyss 2
+addappid(2235201, 1, "64b6a7741dff52341709bb548d844f5cbfb4d551a165eba90db90d49ca88586c") --Main Windows Depot Neon Abyss 2
+setManifestid(2235201, "6394426800303818273", 1061097920)
+--Dlcname Neon Abyss 2 Original Soundtrack
+addappid(3821760) --Dlcname Neon Abyss 2 Original Soundtrack
+addappid(3821761, 1, "c57c0122afbabc657c82653a8495573dd24b6d33e55d84170bfd141aea2f42ed") --Dlc Windows Depot Neon Abyss 2 Original Soundtrack
+setManifestid(3821761, "8952598267255126367", 192555440)
+addappid(3821762, 1, "7fc06e1dc2844d845d5834882ef3d0d919a38092057958337a9683abf2ebff3d") --Dlc Windows Depot Neon Abyss 2 Original Soundtrack
+setManifestid(3821762, "4747296344409624490", 1190898048)
+addappid(3821980) --Dlcname Neon Abyss 2 - Supporter Pack
+addappid(4060090) --Dlcname Neon Abyss 2 - Archenemy Returns
