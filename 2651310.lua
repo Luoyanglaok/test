c67256f12b548556e24f05ba46272ea0be2661c7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-11 20:34:21 +0800

    add 2651310.lua

--- 2651310.lua
+++ 2651310.lua
@@ -1,3 +1,6 @@
-addappid(2651310)
-addappid(2651311,0,"bde9a862771b76a267d498de86955fabad96e78a8e6e113a1ab66e04a7aa3352")
-setManifestid(2651311,"4910389341011220534")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2651310.lua
+--Gamename 脱出哥布林巢穴
+addappid(2651310) --Mainappid 脱出哥布林巢穴
+addappid(2651311, 1, "bde9a862771b76a267d498de86955fabad96e78a8e6e113a1ab66e04a7aa3352") --Main Windows Depot 脱出哥布林巢穴
+setManifestid(2651311, "823131275329963923", 135636272)
