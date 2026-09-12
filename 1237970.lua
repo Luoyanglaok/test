f51b1b6a46b53f5306cae07de577f78c4682baa2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 21:37:44 +0800

    add 1237970.lua

--- 1237970.lua
+++ 1237970.lua
@@ -34,7 +34,7 @@ setManifestid(228984, "2547553897526095397", 13436144)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228990, "1829726630299308803", 100658080)
 addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") --Share Windows Depot EA APP Installer
-setManifestid(3340991, "5874704072306837744", 246482576)
+setManifestid(3340991, "3755387105551220105", 246722480)
 addappid(1238100) --Dlcname Titanfall® 2: Monarch's Reign Bundle
 addappid(1238101) --Dlcname Titanfall® 2: Jump Start Pack
 addappid(1238102) --Dlcname Titanfall® 2: Colony Reborn Bundle
