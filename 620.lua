84b057150f74adcb992307a1a6c6cfab74c16f2a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:01:52 +0800

    add 620.lua

--- 620.lua
+++ 620.lua
@@ -1,21 +1,37 @@
-addappid(620)
-addappid(660,0,"759d2251fd0bb050f088c166dc269ea869ddafde525032db3095fbf06cc0dfc3")
-setManifestid(660,"2679694482538379167")
-addappid(624,0,"48fcb7e647902c2d9467ef3f4405e1b6308981949705cb2b7489bb3d22aead66")
-setManifestid(624,"8323957709516641347")
-addappid(623,0,"6a9fe60f83046355da81ea12f8fe895e3f59f1223d1689cc7e1ba327358e99fb")
-setManifestid(623,"2276453204441793614")
-addappid(622,0,"0be76bef0b464b2d58b6ced70c86e608708c18c1204836cb1bb5d209776f8a0e")
-setManifestid(622,"5403637970672788159")
-addappid(621,0,"d2718f2424c94832a7112661774d98ffb3d52f454312f543a71418d253100d1d")
-setManifestid(621,"3111897107378205370")
-addappid(625,0,"d120d4c37faea11be77c787dc48000f297aaf8200a4265969a4f5dbeebc0326c")
-setManifestid(625,"1128685895801277145")
-addappid(626,0,"50611154e0550112cb9f9edcc44cf32bb90a7fe8d2f5738f6f24b7ced43cf7c2")
-setManifestid(626,"6439173920052159773")
-addappid(627,0,"4ff7fb0bde51bae4f2c7a9bb220fd2da9bd9b3b8210d222c615acf28a8fdd821")
-setManifestid(627,"4705364648410025995")
-addappid(628,0,"7f299ff4eec6b9de24869bb482a325b72f792798c87268f03feda25d4f3361c5")
-setManifestid(628,"1134745004760172192")
-addappid(661,0,"5e338dd8d07e4cda5dfbcc7eb03418d9b1d93192bc20d5aac2174b851b11c1a6")
-setManifestid(661,"8787172582039049173")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 620.lua
+--Gamename Portal 2
+addappid(620, 1, "a33ddcda802d5d2db32b70ce263fe29efdcef1f2e534c5d4e7f515a20b31b6a5") --Mainappid Portal 2
+addappid(621, 1, "d2718f2424c94832a7112661774d98ffb3d52f454312f543a71418d253100d1d") --Main Windows Depot Portal 2
+setManifestid(621, "9122696443005314333", 7509295520)
+addappid(622, 1, "0be76bef0b464b2d58b6ced70c86e608708c18c1204836cb1bb5d209776f8a0e") --Main Windows Depot Portal 2
+setManifestid(622, "916560128540961379", 27379104)
+addappid(623, 1, "6a9fe60f83046355da81ea12f8fe895e3f59f1223d1689cc7e1ba327358e99fb") --Main Macos Depot Portal 2
+setManifestid(623, "2276453204441793614", 31994448)
+addappid(624, 1, "48fcb7e647902c2d9467ef3f4405e1b6308981949705cb2b7489bb3d22aead66") --Main Windows Depot Portal 2
+setManifestid(624, "8207314901980262131", 7690320)
+addappid(625, 1, "d120d4c37faea11be77c787dc48000f297aaf8200a4265969a4f5dbeebc0326c") --Main Windows Language Depot French Portal 2
+setManifestid(625, "1128685895801277145", 690630480)
+addappid(626, 1, "50611154e0550112cb9f9edcc44cf32bb90a7fe8d2f5738f6f24b7ced43cf7c2") --Main Windows Language Depot German Portal 2
+setManifestid(626, "6439173920052159773", 687653744)
+addappid(627, 1, "4ff7fb0bde51bae4f2c7a9bb220fd2da9bd9b3b8210d222c615acf28a8fdd821") --Main Windows Language Depot Spanish Portal 2
+setManifestid(627, "4705364648410025995", 686661072)
+addappid(628, 1, "7f299ff4eec6b9de24869bb482a325b72f792798c87268f03feda25d4f3361c5") --Main Windows Language Depot Russian Portal 2
+setManifestid(628, "1134745004760172192", 716027584)
+addappid(661, 1, "5e338dd8d07e4cda5dfbcc7eb03418d9b1d93192bc20d5aac2174b851b11c1a6") --Main Linux Depot Portal 2
+setManifestid(661, "8164832528872470690", 27596896)
+--Dlcname Portal 2 Sixense MotionPack
+addappid(660, 1, "759d2251fd0bb050f088c166dc269ea869ddafde525032db3095fbf06cc0dfc3") --Dlcname Portal 2 Sixense MotionPack
+setManifestid(660, "2679694482538379167", 444528944)
+--Dlcname Portal 2 Soundtrack
+addappid(323180, 1, "3845d6afedc1ae0879cd7af31df2bf42724e94bc07e672023ee4d12c422befee") --Dlcname Portal 2 Soundtrack
+addappid(323184, 1, "e20344b3d3b8542dd688df9c0ea6c68482dabe0d49774a297fc06782acd1d952") --Dlc Windows Depot Portal 2 Soundtrack
+setManifestid(323184, "658642952988970250", 1149287072)
+addappid(323185, 1, "33658cb0a5a5c9d74b8bb94b2307030da8b8925b41edc06674458a1d094cabba") --Dlc Windows Depot Portal 2 Soundtrack
+setManifestid(323185, "8466984105877234385", 450495456)
+addappid(650) --Dlcname Portal 2 - Gamestop PS3 DLC
+addappid(651) --Dlcname Portal 2 - Bot Paint Job DLC
+addappid(652) --Dlcname Portal 2 - Bot Roll Cage DLC
+addappid(653) --Dlcname Portal 2 - Bot Antenna Topper DLC
+addappid(654) --Dlcname Portal 2 - Summer 2011 DLC
+addappid(669) --Dlcname Portal 2 - Sixense Motion Preorder
