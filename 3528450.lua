3e6b20a53086b4690ef1472dad473858fee2b6f3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 18:57:56 +0800

    add 3528450.lua

---
+++ 3528450.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3528450.lua
+--Gamename 纸房子 Sweet Home
+addappid(3528450) --Mainappid 纸房子 Sweet Home
+addappid(3528451, 1, "cde617a40d47bab5dca680dda42717a7cfcffe17e1131d7925d27b93667962b3") --Main Windows Depot 纸房子 Sweet Home
+setManifestid(3528451, "5951062554738536919", 4141254944)
