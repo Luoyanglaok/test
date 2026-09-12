e7d69441cd774d592b188c1a83a687f3c4d16cf7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:14:22 +0800

    add 1674010.lua

---
+++ 1674010.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1674010.lua
+--Gamename Nin Online
+addappid(1674010, 1, "c56d2c4ac988e4f18e654719bc00a38f7ef5219fd5697395f9226a006b5445c7") --Mainappid Nin Online
+addappid(1674011, 1, "9E86D04027BE438323B361661DA5E98012C6642BAA1139FC5DD2350A7709A120") --Main Windows Depot Nin Online
+setManifestid(1674011, "33962785755915033", 257719200)
+addappid(1674012, 1, "9d814f536dec545efdb7324baec1673dc55d40519bff02a4fc0ecd6d1d3d19f5") --Main Windows Depot Nin Online
+setManifestid(1674012, "3467058633206820544", 859166336)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
