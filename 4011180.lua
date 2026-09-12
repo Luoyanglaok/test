f576e40f33951907e2e26a3dee5a3fbb49fb0923
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 01:26:16 +0800

    add 4011180.lua

---
+++ 4011180.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4011180.lua
+--Gamename NTR Idol: Déjà Vu
+addappid(4011180, 1, "58562ec8f76be79ed1a229669918948dd9cf53e25bb33a21142935c460856549") --Mainappid NTR Idol: Déjà Vu
+addappid(4011181, 1, "984dbfea7fafeba01c1c2a44fd4b89f20d38c54bf67395ecc67bcb6bced1f7c4") --Main Windows Depot NTR Idol: Déjà Vu
+setManifestid(4011181, "6069859317959917275", 3381802928)
+--Dlcname NTR Idol: Déjà Vu Artbook
+addappid(4340680) --Dlcname NTR Idol: Déjà Vu Artbook
+addappid(4340681, 1, "3a30173dd61441e7b251f08fc0cecce9e086dfc78205c50ea22376003af38bcc") --Dlc Windows Depot NTR Idol: Déjà Vu Artbook
+setManifestid(4340681, "6007369577231587917", 373393680)
