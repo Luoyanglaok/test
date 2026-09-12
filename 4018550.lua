c84386d397e98cd36d791862f3baf1c70d45a154
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 11:38:24 +0800

    add 4018550.lua

---
+++ 4018550.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4018550.lua
+--Gamename Cat Isle
+addappid(4018550) --Mainappid Cat Isle
+addappid(4018551, 1, "fdd99a76ae0c605a08b9e78b09b69596dff0dabf19041f50de387b8deee8d05e") --Main Windows Depot Cat Isle
+setManifestid(4018551, "457878844946090784", 88848672)
+addappid(4018552, 1, "4a6751d2e0f24a4d793773a9f2a5101f215157549dd3d5d4f1c8f65cac5afce9") --Main Macos Depot Cat Isle
+setManifestid(4018552, "258111665347693676", 115794048)
