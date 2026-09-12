26b1d4b9ede77305ce68dda9745fb57b3ade36c0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:07:16 +0800

    add 4659620.lua

---
+++ 4659620.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4659620.lua
+--Gamename Touhou Koumakyou: New Classic - the Embodiment of Scarlet Devil
+addappid(4659620) --Mainappid Touhou Koumakyou: New Classic - the Embodiment of Scarlet Devil
+addappid(4659621, 1, "6f796158c99d7c5d5f0c5d8d72861591d792e43523557c0d64e7142961d367c4") --Main Windows Depot Touhou Koumakyou: New Classic - the Embodiment of Scarlet Devil
+setManifestid(4659621, "4177228129538833157", 932140944)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+--Missing Dlcs
+--Dlcname 東方紅魔郷：New Classic ～ the Embodiment of Scarlet Devil. Soundtrack
