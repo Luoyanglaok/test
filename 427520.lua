dfc42e0758ff2461fe39c18cd57bc62552e8df3d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:05:11 +0800

    add 427520.lua

--- 427520.lua
+++ 427520.lua
@@ -14,6 +14,11 @@ addappid(427525, 1, "4cf6a64823e9240f302d20a5428fdade384b312b0f55c87aaebd14bf62b
 setManifestid(427525, "2157676814121005612", 1621678976)
 addappid(427526, 1, "b53e12d3afaa53f49e8aea389862fb07f212a62db254c1c6a5db9f3c2ee29006") --Main Windows Depot Factorio
 addappid(645390, 1, "8526cd1dd11bbe44bbc6ce0c045a6878d7b54bd5a39e84b466062528e977e3df") --Main Windows Depot Factorio
+--Dlcname Factorio - Soundtrack
+addappid(436090, 1, "23238f54eaeb9a42690c8335c61a6b00ea7d1cb65421b6f7e24f3c7134888de4") --Dlcname Factorio - Soundtrack
+setManifestid(436090, "1380443608017988433", 115639104)
+addappid(436091, 1, "72edbb2c0d316fd9b5f39aa3d480ecf62bddadd611a728a5ccd95b66a5200ba5") --Dlc Windows Depot Factorio - Soundtrack
+setManifestid(436091, "1142415089460811828", 346307360)
 --Dlcname Factorio: Space Age
 addappid(645391, 1, "e1e840cb840b8d382a84e6cc0443393d76f99ab19ee8afd3a61a0b414fc5ac9f") --Dlc Linux Depot Factorio: Space Age
 setManifestid(645391, "5217534433594943675", 4276014000)
@@ -21,3 +26,9 @@ addappid(645392, 1, "a7b9074d3a2634bc9163847fe1ae9cfb0ad55a286a6c32b67289f2a07e2
 setManifestid(645392, "935401243988284740", 4379026224)
 addappid(645393, 1, "df5808d5e8ae3a9cce0454d6f6968c6f200e4d28a4cd2a4ba4f8be1426ad7ff4") --Dlc Windows Depot Factorio: Space Age
 setManifestid(645393, "4313719932916563912", 4316980864)
+--Dlcname Factorio: Space Age - Soundtrack
+addappid(3311770) --Dlcname Factorio: Space Age - Soundtrack
+addappid(3311771, 1, "0565f14b3a4c063e57b257b849a52b2778d79106d0a3dacc462dabf6dc7bf0e7") --Dlc Windows Depot Factorio: Space Age - Soundtrack
+setManifestid(3311771, "7297438335085052778", 1510326736)
+addappid(3311772, 1, "a990370a5957f13aa9ffa191a2f9fdb2bce9723697971288d0cdb65f2779cfa2") --Dlc Windows Depot Factorio: Space Age - Soundtrack
+setManifestid(3311772, "9105904954403112113", 2925115424)
