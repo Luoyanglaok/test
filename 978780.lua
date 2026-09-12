848e6d8c13f78f6719d632af31c4dbc341660c4a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 17:40:06 +0800

    add 978780.lua

--- 978780.lua
+++ 978780.lua
@@ -1,6 +1,18 @@
-addappid(978780)
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-addappid(978781,0,"6c1f00459c6e87fccd19c861a3905933c304256f5f3e7ffea2b870ce0c521b0d")
-setManifestid(978781,"4175510262569195929")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 978780.lua
+--Gamename STORY OF SEASONS: Friends of Mineral Town
+addappid(978780) --Mainappid STORY OF SEASONS: Friends of Mineral Town
+addappid(978781, 1, "6c1f00459c6e87fccd19c861a3905933c304256f5f3e7ffea2b870ce0c521b0d") --Main Windows Depot STORY OF SEASONS: Friends of Mineral Town
+setManifestid(978781, "4175510262569195929", 1168639840)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(1292300) --Dlcname STORY OF SEASONS: Friends of Mineral Town - Cow Costume
+addappid(1292301) --Dlcname STORY OF SEASONS: Friends of Mineral Town - Sheep Costume
+addappid(1292302) --Dlcname STORY OF SEASONS: Friends of Mineral Town - Dog Costume
+addappid(1292303) --Dlcname STORY OF SEASONS: Friends of Mineral Town - Chick Costume
+addappid(1292304) --Dlcname STORY OF SEASONS: Friends of Mineral Town - Chicken Costume
