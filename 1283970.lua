98f84755e0bfa0a0c2d8f49b2b37f0abafaf8920
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 19:01:36 +0800

    add 1283970.lua

--- 1283970.lua
+++ 1283970.lua
@@ -1,3 +1,8 @@
-addappid(1283970)
-addappid(1283971,0,"cc3e8638081fb7bb6f0a2b461db9232fa3521c984d47cb33aed0f59c8eadd303")
-setManifestid(1283971,"7299813230997459070")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1283970.lua
+--Gamename YoloMouse - Cursor Changer
+addappid(1283970, 1, "090778e689ec1b0da2c1a9de97eb18811ed42fc768d995ec77a9ec87ddff8142") --Mainappid YoloMouse - Cursor Changer
+addappid(1283971, 1, "cc3e8638081fb7bb6f0a2b461db9232fa3521c984d47cb33aed0f59c8eadd303") --Main Windows Depot YoloMouse - Cursor Changer
+setManifestid(1283971, "6945317349049247381", 51564576)
+addappid(1283972, 1, "6c50e2907df9edffe3e252121e541935263a21825e414ad821c5378eb19fbdd2") --Main Linux Depot YoloMouse - Cursor Changer
+setManifestid(1283972, "1640986703873381372", 56896816)
