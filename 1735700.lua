73cec5bb3431fba6c9e3c216b1f81e9a191ff7fe
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 05:23:43 +0800

    add 1735700.lua

--- 1735700.lua
+++ 1735700.lua
@@ -1,5 +1,23 @@
-addappid(1735700)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1735701,0,"f2bbd7e59cb9050efe964f132c6edcb6059ffefaa0995f9b658989c95a85d7df")
-setManifestid(1735701,"2461480398419448207")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1735700.lua
+--Gamename Back to the Dawn
+addappid(1735700, 1, "113c82b120b76ed1b42a4cec94e104fec92fb04156aee028698ce1fe161f5fa9") --Mainappid Back to the Dawn
+addappid(1735701, 1, "f2bbd7e59cb9050efe964f132c6edcb6059ffefaa0995f9b658989c95a85d7df") --Main Windows Depot Back to the Dawn
+setManifestid(1735701, "3293226214379797659", 1238568576)
+--Dlcname Back to the Dawn - Original Soundtrack
+addappid(2646420) --Dlcname Back to the Dawn - Original Soundtrack
+addappid(2646421, 1, "10604d3dec843e1666b3fee920169e1c9758852911109b08f24d9783d8e07398") --Dlc Windows Depot Back to the Dawn - Original Soundtrack
+setManifestid(2646421, "1621856909135907349", 132333072)
+addappid(2646422, 1, "126aa0345576d873662ac1e268c5ad98f102a41a45561155e8a4ef640e976277") --Dlc Windows Depot Back to the Dawn - Original Soundtrack
+setManifestid(2646422, "598068047997893263", 941563328)
+--Dlcname Back to the Dawn - Supporter Pack
+addappid(3700590) --Dlcname Back to the Dawn - Supporter Pack
+addappid(3700591, 1, "999ea4a5f7c7406dcf832770bb051d0ba6d4dbe8df09e6412ccdf3464379b120") --Dlc Windows Depot Back to the Dawn - Supporter Pack
+setManifestid(3700591, "2270776739023884576", 804346304)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3700610) --Dlcname Back to the Dawn - Game Modifier Toolkit
+addappid(3713320) --Dlcname Back to the Dawn - Thank You Letter for EA players
