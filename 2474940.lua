d806cea2d2f062cfa45c830071478629459e09f9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 17:52:29 +0800

    add 2474940.lua

--- 2474940.lua
+++ 2474940.lua
@@ -1,4 +1,9 @@
-addappid(2474940)
-setManifestid(228990,"1829726630299308803")
-addappid(2474941,0,"21ee99ef5fc00ffd56eebe7540b9ce39bf861e3bba1e23605cdb87f74ea4f5b1")
-setManifestid(2474941,"7108223534191367456")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2474940.lua
+--Gamename Nope Nope Nope Nope Nurses
+addappid(2474940, 1, "7a7b8273b54ef1c7645cba10ef12b8baf84bd01280526044b8da712040b5118b") --Mainappid Nope Nope Nope Nope Nurses
+addappid(2474941, 1, "21ee99ef5fc00ffd56eebe7540b9ce39bf861e3bba1e23605cdb87f74ea4f5b1") --Main Windows Depot Nope Nope Nope Nope Nurses
+setManifestid(2474941, "7108223534191367456", 716627792)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
