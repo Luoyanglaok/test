48e8ee3e50a8a6271672120d1c917d2b3575f6a9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 10:57:50 +0800

    add 2560240.lua

---
+++ 2560240.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2560240.lua
+--Gamename Biped 2
+addappid(2560240, 1, "0fef022272438eb1ccfa5c245f74175a6af37003860a2e249a32d008f715d4ec") --Mainappid Biped 2
+addappid(2560241, 1, "e2316d00922f6f267842f1e95fe47ce413cf57346e675fde73f7e10f5616f5cf") --Main Windows Depot Biped 2
+setManifestid(2560241, "4255865106111709018", 8430432256)
+--Missing Dlcs
+--Dlcname Biped 2 - Full Entitlement
