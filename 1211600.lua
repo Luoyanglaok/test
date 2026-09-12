680de2bc97d9e9f06e2a2d9d8172599828b806d6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:17:29 +0800

    add 1211600.lua

--- 1211600.lua
+++ 1211600.lua
@@ -1,3 +1,6 @@
-addappid(1211600)
-addappid(1211602,0,"0378532b7715d88cc41c1a7f96a42d2ac9ab565ff5818653cdbbfe54548f87db")
-setManifestid(1211602,"8940120057938597380")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1211600.lua
+--Gamename ZED ZONE
+addappid(1211600, 1, "e08f906b845219f80f043cd93cdada602514e246305aeaab8ecfef8a9ed12aae") --Mainappid ZED ZONE
+addappid(1211602, 1, "0378532b7715d88cc41c1a7f96a42d2ac9ab565ff5818653cdbbfe54548f87db") --Main Windows Depot ZED ZONE
+setManifestid(1211602, "5881288754680579047", 320441312)
