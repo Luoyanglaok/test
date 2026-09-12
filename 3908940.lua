f8c03e217aef59ae5f755b8a16d2edecf353c9ae
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:24:12 +0800

    add 3908940.lua

---
+++ 3908940.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3908940.lua
+--Gamename Inferno Protocol
+addappid(3908940) --Mainappid Inferno Protocol
+addappid(3908941, 1, "fd06408fa3a6b4deaa40d6304be8b88b2fef63a1ed05f9010b029df56ebab95d") --Main Windows Depot Inferno Protocol
+setManifestid(3908941, "9194534717582409351", 2694786160)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
