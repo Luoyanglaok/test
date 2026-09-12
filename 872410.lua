828432fb028d538edfb104cc5405ad5a966404c6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 10:10:42 +0800

    add 872410.lua

---
+++ 872410.lua
@@ -0,0 +1,171 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 872410.lua
+--Gamename ROMANCE OF THE THREE KINGDOMS XIV
+addappid(872410, 1, "b35001f8360a070729e1ed0aa5f7c1f2ff3a86b4bedb1dcfff94275330480ac8") --Mainappid ROMANCE OF THE THREE KINGDOMS XIV
+addappid(872411, 1, "7816f4f3dc21fd070c471c51bd6edb0f1875e0dcd71a5f8225d7543186b677e4") --Main Windows Depot ROMANCE OF THE THREE KINGDOMS XIV
+setManifestid(872411, "247060224827963761", 107680432)
+addappid(872412, 1, "a7a9b2aa07ea6f65c6e11c82e528819858b22b723696ee0bc7f62b567cc55602") --Main Windows Depot ROMANCE OF THE THREE KINGDOMS XIV
+setManifestid(872412, "1821258790151557907", 11752857392)
+addappid(872413, 1, "64FB6A1A445D7541305BEBD0BDF500B61FAD204978FC76A4E74C5DE12E5A925F") --Main Windows Depot ROMANCE OF THE THREE KINGDOMS XIV
+setManifestid(872413, "2708382053934172341", 0)
+addappid(872414, 1, "182b089b1582dd47ba82e8efa49e4be9ae1d2d8cc6427fb409e59fb0fa9189f6") --Main Windows Depot ROMANCE OF THE THREE KINGDOMS XIV
+setManifestid(872414, "8553357306661913417", 558249472)
+--Dlcname RTK14: Scenario [Battle of Yiling]
+addappid(1198830, 1, "edd2d961ed4a31223af649b16eba8fc4e3c7f1057b352cae94acc5b262cb9a7e") --Dlcname RTK14: Scenario [Battle of Yiling]
+setManifestid(1198830, "4537773435072559451", 1758864)
+--Dlcname RTK14: Scenario [Autumn Winds in Wuzhangyuan]
+addappid(1198831, 1, "FDA99AC90D09602F2CF30F845C5D1EFBD762A6E04F638A41AA8921F204A1592D") --Dlcname RTK14: Scenario [Autumn Winds in Wuzhangyuan]
+setManifestid(1198831, "4773667971732112719", 1741664)
+--Dlcname RTK14: Scenario [The Start of Change]
+addappid(1198832, 1, "cbfb276398ff7639cf91e26db7cc00aee7869d1c9a63fa8d7d285e2d1de32a75") --Dlcname RTK14: Scenario [The Start of Change]
+setManifestid(1198832, "4729672140666924474", 1210624)
+--Dlcname RTK14: "ROMANCE OF THE THREE KINGDOMS XIII" Officer CG Set
+addappid(1198834, 1, "a0992c2727dbf46e5000f4f52046afd0d88b981df28950a85ab51b2a82104025") --Dlcname RTK14: "ROMANCE OF THE THREE KINGDOMS XIII" Officer CG Set
+setManifestid(1198834, "6577578762064609762", 1072)
+--Dlcname RTK14: "Ryza and Her Friends" Officer Data
+addappid(1198835, 1, "d17f92f99ba024fecc1214bda7e6ed2491839e83b169b6c7d31a36d9c48fcf2e") --Dlcname RTK14: "Ryza and Her Friends" Officer Data
+setManifestid(1198835, "4073196032626802279", 5648)
+--Dlcname RTK14: Scenario [Battle of Tong Gate] & Event Set
+addappid(1198836, 1, "e24b013fba513d43565c333162eb4ae8191b026911dc65fb30a197527689e7af") --Dlcname RTK14: Scenario [Battle of Tong Gate] & Event Set
+setManifestid(1198836, "5276450411441047934", 226542176)
+--Dlcname RTK14: Editor - 1st Wave
+addappid(1198837, 1, "66c79c3b617319e737394f00d04ae5e006184348bc9ba16462550ce3462d496a") --Dlcname RTK14: Editor - 1st Wave
+setManifestid(1198837, "5549346714551848346", 64)
+--Dlcname RTK14: Scenario [The Lu Bu Campaign] & Event Set
+addappid(1198839, 1, "ec2d745a25043146e0ee861c751de8ee9a49272424d3840f4a155c04f93d1a90") --Dlcname RTK14: Scenario [The Lu Bu Campaign] & Event Set
+setManifestid(1198839, "9106118020399216637", 233952816)
+--Dlcname RTK14: Editor - 2nd Wave
+addappid(1198840, 1, "c8daa30e6e9825c5cd88f43844083bdf2847abd24d495fa2edebdb7b1b4ee0e8") --Dlcname RTK14: Editor - 2nd Wave
+setManifestid(1198840, "5183827118423278820", 800)
+--Dlcname RTK14: Difficulty [Veteran] & Scenario Settings Set
+addappid(1198841, 1, "eaba90ed4bc4be7084672609ae66bc9d65cfd3b4e02cca1974901c65a641fb1d") --Dlcname RTK14: Difficulty [Veteran] & Scenario Settings Set
+setManifestid(1198841, "64168457231799406", 64)
+--Dlcname RTK14: Scenario [Hebei Conflict] & Event Set
+addappid(1198843, 1, "806d06b24013c22a6be32b93ebf62fff1d54e4cbcdca99734ebaca4dd84f9506") --Dlcname RTK14: Scenario [Hebei Conflict] & Event Set
+setManifestid(1198843, "5967214586019959915", 230690576)
+--Dlcname RTK14: Scenario [The Battle of Hefei] & Event Set
+addappid(1198844, 1, "c33aa81c5ce6287bfe765569cd63d72297e03f59a654fd5cb580667b9a98d23e") --Dlcname RTK14: Scenario [The Battle of Hefei] & Event Set
+setManifestid(1198844, "5929110711180847402", 238825440)
+--Dlcname RTK14: Editor - 3rd Wave
+addappid(1198845, 1, "9a9b9d33ab432d1c3bc302956877fbad0c9220f0fb688f90a2d60438624438cf") --Dlcname RTK14: Editor - 3rd Wave
+setManifestid(1198845, "5535988802195645348", 64)
+--Dlcname RTK14: Scenario [Conflict among Clans]
+addappid(1198846, 1, "132489cac647654ee4a1d0590340ef4e10a959954a78d57d52436c31a1ee294f") --Dlcname RTK14: Scenario [Conflict among Clans]
+setManifestid(1198846, "995050871963497002", 1208704)
+--Dlcname RTK14: Scenario [A Yellow Sky]
+addappid(1198847, 1, "20ebe79ca5a3df37f2c4aae793a0a3d4c8dd8cd9df5afbba37e8fa8e012b9255") --Dlcname RTK14: Scenario [A Yellow Sky]
+setManifestid(1198847, "1175004702111808016", 1216336)
+--Dlcname RTK14: Scenario [Gongsun Zan: the Hero of Hebei]
+addappid(1198848, 1, "0329a3f1faf757194645d0bc4bae8804b998016f89e9e1f7937d4b0a5356736a") --Dlcname RTK14: Scenario [Gongsun Zan: the Hero of Hebei]
+setManifestid(1198848, "196977068528133134", 1224336)
+--Dlcname RTK14: [Extreme] Difficulty & Scenario Settings Set
+addappid(1198849, 1, "c9070e70a15b2593c6762ff92f1cadbe910c05b250fed5121b2ec9d88d22e4ff") --Dlcname RTK14: [Extreme] Difficulty & Scenario Settings Set
+setManifestid(1198849, "6753092835371492704", 64)
+--Dlcname 三國志14: 「つくもたん」武将顔CG
+addappid(1213610, 1, "F729A4424E98C530265A80DB395EA98EE6CD75EEDDB1A6DBFCCD70FF47D41767") --Dlcname 三國志14: 「つくもたん」武将顔CG
+setManifestid(1213610, "2306631369019405132", 64)
+--Dlcname RTK14: "Legend of the Galactic Heroes" Collab: Reinhard & Yang
+addappid(1213611, 1, "f632a2bf03751fb514c81b9e638aa730a841a7828b4e92298f34dfe3e1ab511b") --Dlcname RTK14: "Legend of the Galactic Heroes" Collab: Reinhard & Yang
+setManifestid(1213611, "7696882174603873275", 3792)
+addtoken(1213612, "2832950080961635342")
+addappid(1213612, 1, "d19a7c3807c1380ac5fec0f056b126977f5c901e64259fdf5d57e1bb3ce73280") --Dlcname AppID 1213612
+setManifestid(1213612, "8905828104802641966", 3792)
+--Dlcname RTK14: "Ancient Officers" Officer Data Set
+addappid(1213613, 1, "b2795353ca91386276f93549c71dfcb1926c9c82cdb33f6099923fc76768d7ca") --Dlcname RTK14: "Ancient Officers" Officer Data Set
+setManifestid(1213613, "7478584307335246570", 352)
+addtoken(1258150, "2158062538924847222")
+addappid(1258150, 1, "051eb674da763cec75818618e39fb766e2a0db18928edaa1fa035d21f3135cb3") --Dlcname AppID 1258150
+setManifestid(1258150, "8411790377032961673", 3792)
+addtoken(1258151, "15234304398983561282")
+addappid(1258151, 1, "26a864e3e358ef783144a45ea62cf9b86399c6f99d4bb0e003d6b478602b5b1e") --Dlcname AppID 1258151
+setManifestid(1258151, "1486913277627246249", 3792)
+addtoken(1258152, "11484763141663210829")
+addappid(1258152, 1, "9291779e848113da1b7de59663c0473a1ed7dc7970a584d37207f8a6a2e0ffc2") --Dlcname AppID 1258152
+setManifestid(1258152, "1609349454990144848", 7520)
+--Dlcname RTK14: "Legend of the Galactic Heroes" Collab Scenario "In the Midst of an Endless Dream" & Reinhard & Yang Officer Data Set
+addappid(1258154, 1, "18c6dbffd52866092690b6e676249d6edbc95a653f3719db4062ee7c50fb248c") --Dlcname RTK14: "Legend of the Galactic Heroes" Collab Scenario "In the Midst of an Endless Dream" & Reinhard & Yang Officer Data Set
+setManifestid(1258154, "225361991569734678", 1730448)
+--Dlcname RTK14: Tie-up Officer "Zhuge Liang" Data
+addappid(1258155, 1, "6d4ffdf50180101da5d16051e93e2a213587dbb4d404738c09851063c8989313") --Dlcname RTK14: Tie-up Officer "Zhuge Liang" Data
+setManifestid(1258155, "3090505027647891977", 992)
+--Dlcname RTK14: "Sasshu Sangokushi" Collab: Specialty Data
+addappid(1293030, 1, "7ceb140944c881964cb62d26752c0ee5f45de84123b712bd16136a7902791d7d") --Dlcname RTK14: "Sasshu Sangokushi" Collab: Specialty Data
+setManifestid(1293030, "8429122104998380227", 80)
+--Dlcname ROMANCE OF THE THREE KINGDOMS XIV: Diplomacy and Strategy Expansion Pack
+addappid(1427650, 1, "b1d1308079c2208633cea7b2e9d4f4bef64d3635d80917c59c8fff7f0645972d") --Dlcname ROMANCE OF THE THREE KINGDOMS XIV: Diplomacy and Strategy Expansion Pack
+setManifestid(1427650, "6260143535053874155", 4908146176)
+--Dlcname RTK14 EP: Scenario [The Fall of Shu Han]
+addappid(1431480, 1, "76ff1f00483ca80b75580c6021650cee27320215d2a7dac9c4fc46b20a569a15") --Dlcname RTK14 EP: Scenario [The Fall of Shu Han]
+setManifestid(1431480, "537728245166833856", 1591600)
+--Dlcname RTK14: "Ishin no Arashi" Officer Data Set
+addappid(1431481, 1, "C8DF32063CB76912D161683F0CF64BC14C00154DE4939B48225B187428B882BE") --Dlcname RTK14: "Ishin no Arashi" Officer Data Set
+setManifestid(1431481, "807548417110382181", 176)
+--Dlcname RTK14: Scenario [The Wavering Han Dynasty] & Event Set
+addappid(1431483, 1, "e98eff31397af5e5b66e807d1af9f6a81da1c61f3f9abe2485623462cbdf3aa1") --Dlcname RTK14: Scenario [The Wavering Han Dynasty] & Event Set
+setManifestid(1431483, "218718109270039859", 222238880)
+--Dlcname RTK14 EP: Editor - 4th Wave
+addappid(1431484, 1, "7f6da7829047dbdca745409c795c7928b8fbc86e138902c2742a455951f41d0e") --Dlcname RTK14 EP: Editor - 4th Wave
+setManifestid(1431484, "7305709814624911096", 64)
+--Dlcname RTK14 EP: Scenario for War Chronicles Mode - 1st Wave: "Liu Bei Enters Shu"
+addappid(1431485, 1, "aa9a880dbd4edc735767bff9c77e356d803c66934dcc9c846ca35635a2e76635") --Dlcname RTK14 EP: Scenario for War Chronicles Mode - 1st Wave: "Liu Bei Enters Shu"
+setManifestid(1431485, "1404895364427829799", 634080)
+--Dlcname RTK14: "DEAD OR ALIVE Xtreme Venus Vacation" Officer Data
+addappid(1431486, 1, "8ff2877b883499fcb75b22fa5a9ffae4a09a7db41e590a563f21d6ab3972c4fa") --Dlcname RTK14: "DEAD OR ALIVE Xtreme Venus Vacation" Officer Data
+setManifestid(1431486, "7336033176794171761", 3792)
+--Dlcname RTK14: Scenario [The Rise of Cao Cao] & Event Set
+addappid(1431487, 1, "8d0285f29c8150dda19f93be24d6c13467125b617e9cb3a18a2fa67731050dbb") --Dlcname RTK14: Scenario [The Rise of Cao Cao] & Event Set
+setManifestid(1431487, "1380717195442474747", 214997792)
+--Dlcname RTK14 EP: Scenario for War Chronicles Mode - 2nd Wave: "Clash of the Warmongers"
+addappid(1431488, 1, "f9116cdf920c2a3ac25e9b54e0e19ba64958effe17036737cb96676020edb763") --Dlcname RTK14 EP: Scenario for War Chronicles Mode - 2nd Wave: "Clash of the Warmongers"
+setManifestid(1431488, "794422081024784071", 626192)
+--Dlcname RTK14: "ROMANCE OF THE THREE KINGDOMS XII" Officer CG Set
+addappid(1431489, 1, "e5d61bd2c3b9cba27c7e00a19ceaacabe20dfc6c9f9ee362a0082b94766a74bb") --Dlcname RTK14: "ROMANCE OF THE THREE KINGDOMS XII" Officer CG Set
+setManifestid(1431489, "1736589144793819341", 115331232)
+--Dlcname RTK14 EP: Scenario for War Chronicles Mode - 3rd Wave: "The Battle for Hebei"
+addappid(1431490, 1, "17fdfb9cc66114584695c81c4e4a1c603aa0b0dd6a32cafb00ce3a95ef332f60") --Dlcname RTK14 EP: Scenario for War Chronicles Mode - 3rd Wave: "The Battle for Hebei"
+setManifestid(1431490, "235836961760836053", 629296)
+--Dlcname RTK14: "Sun Ce Pushing Forward" Event Set
+addappid(1431491, 1, "c525205219f6eb523549c36cf8279ba269b1a24a17452e454980ecc2a14852f6") --Dlcname RTK14: "Sun Ce Pushing Forward" Event Set
+setManifestid(1431491, "7699693027733289254", 112)
+--Dlcname RTK14: Watercolor Painting Style Officer CG Set for Wei, Wu, and Shu
+addappid(1431492, 1, "63e6749014ed8e00a78af173a79115f2f98b447699394d28222ac59b904f312e") --Dlcname RTK14: Watercolor Painting Style Officer CG Set for Wei, Wu, and Shu
+setManifestid(1431492, "93829382814809718", 21538000)
+--Dlcname RTK14: Scenario "The Subjugation of Nanman" & Event Set
+addappid(1431493, 1, "adaafd23eea02ade840316e1a06f5c3190cee3e153057e8d47581dc0799970bc") --Dlcname RTK14: Scenario "The Subjugation of Nanman" & Event Set
+setManifestid(1431493, "5005967991735548235", 226408976)
+--Dlcname RTK14 EP: Scenario for War Chronicles Mode - 4th Wave: "The Battle for Wu"
+addappid(1431494, 1, "d0a2b4cc1adcad4927cc4d6c47231927a92bc343c2ebf6b380ee0c8524ec629e") --Dlcname RTK14 EP: Scenario for War Chronicles Mode - 4th Wave: "The Battle for Wu"
+setManifestid(1431494, "2677268141824279676", 628144)
+--Dlcname RTK14: "Zhuge Liang's Northern Campaign" Event Set
+addappid(1431495, 1, "e678c265071cdfb319817e6b594c3391674010a26ba080e01a9adb6035122561") --Dlcname RTK14: "Zhuge Liang's Northern Campaign" Event Set
+setManifestid(1431495, "8737854741584879346", 112)
+--Dlcname RTK14: "ROMANCE OF THE THREE KINGDOMS XIII" Officer CG Set - 2nd Wave
+addappid(1431496, 1, "2c02f6a1505d64724c674fcfca53b9363ff0b8950fdc6e7821b05322a9a77a67") --Dlcname RTK14: "ROMANCE OF THE THREE KINGDOMS XIII" Officer CG Set - 2nd Wave
+setManifestid(1431496, "7305801351318220012", 125758768)
+--Dlcname RTK14 EP: Scenario for War Chronicles Mode - 5th Wave: "The Battle for Yan Province"
+addappid(1431497, 1, "8049c08d4677e2e9a494a0e1e827013290f349314c64c73619267c7023fb6845") --Dlcname RTK14 EP: Scenario for War Chronicles Mode - 5th Wave: "The Battle for Yan Province"
+setManifestid(1431497, "8423485004548809111", 633440)
+--Dlcname RTK14: "The Subjugation of Southern Jing Province" Event Set
+addappid(1431498, 1, "e34873be5efcbdeaa3e7c6460734d91b8fd53deeacf9580d71d7347752d91aa2") --Dlcname RTK14: "The Subjugation of Southern Jing Province" Event Set
+setManifestid(1431498, "4590861573312572986", 112)
+--Dlcname RTK14: "Ryza and Her New Friends" Officer Data
+addappid(1543950, 1, "863cf8b8e1e7b7f69b16f8fb2c229b7ece17d441e71db2d22736a06219608ab5") --Dlcname RTK14: "Ryza and Her New Friends" Officer Data
+setManifestid(1543950, "19626315043741332", 6592)
+--Dlcname RTK14: Nijisanji VTubers: "Kuzuha, Sister Claire, and Debidebi Debiru" Collaboration Officer Data
+addappid(1638310, 1, "525e2e67ee57220e157995550f311e4f08566127d1367c2fef749a17afbbdfc3") --Dlcname RTK14: Nijisanji VTubers: "Kuzuha, Sister Claire, and Debidebi Debiru" Collaboration Officer Data
+setManifestid(1638310, "4519985131428191727", 2864)
+--Dlcname RTK14: KT's ROMANCE OF THE THREE KINGDOMS Series App Officer CG Set
+addappid(1638311, 1, "edd94190771b3fbebd9620bf176354ec340ed9c886b1636f5e1a50f48c726a91") --Dlcname RTK14: KT's ROMANCE OF THE THREE KINGDOMS Series App Officer CG Set
+setManifestid(1638311, "355277098172238882", 34612416)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1198833) --Dlcname ROMANCE OF THE THREE KINGDOMS XIV Season Pass
+addappid(1258153) --Dlcname AppID 1258153
+addappid(1431482) --Dlcname ROMANCE OF THE THREE KINGDOMS XIV Season Pass 2
+--Missing Dlcs
+--Dlcname ROMANCE OF THE THREE KINGDOMS XIV Complete Edition Upgrade Pack
+--Dlcname RTK14CE: Scenario [Cao Cao's Miscalculation]
+--Dlcname RTK14CE: Scenario [Bold Ambition]
