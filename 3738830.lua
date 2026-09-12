97c1795accd9febb8901013bb4dedf5b96d4833c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 23:19:06 +0800

    add 3738830.lua

---
+++ 3738830.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3738830.lua
+--Gamename FIRE STATE
+addappid(3738830) --Mainappid FIRE STATE
+addtoken(3738830, "16207906861055397030")
+addappid(3738831, 1, "f7cfa2e0e1a6b6940b155923613b25849fd38a098f80a6876b0f124b76e6be86") --Main Windows Depot FIRE STATE
+setManifestid(3738831, "422572294592502023", 27265701184)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
