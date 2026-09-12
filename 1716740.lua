b8741f1494fc8ef7fe51fdde3cab5533bdf51d93
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 10:28:03 +0800

    add 1716740.lua

--- 1716740.lua
+++ 1716740.lua
@@ -1,33 +1,62 @@
-addappid(1716740)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1716741,0,"14c0bdb25d858d05a7381ba67a373aa99b50c5bd4cf78f1895751d418de29ceb")
-setManifestid(1716741,"6270867041624635278")
-addappid(1716742,0,"f7ba0bf1fd70db2958682bc87fcab6dfe9a6f8ca774321df79b1d595d5be303b")
-setManifestid(1716742,"9055339130498423803")
-addappid(2401180,0,"abb5f01b77e2f81e4b06049a0a17302bd7c9dacc7715b9536f6f3462ad51dd89")
-setManifestid(2401180,"1350450549444461803")
-addappid(2401181,0,"9382f0b0fee3c8ccb0cf9fc2389a52eab89fdf1c4e6f408635a82b30037aed9e")
-setManifestid(2401181,"9009164480135609609")
-addappid(1716743,0,"d0a17417a778577c9edb2b58f5352ad326ba2def007a1e3eacd058ac4293907a")
-setManifestid(1716743,"7908035178130303133")
-addappid(1716744,0,"a2c8d4541f82fe48dd2252b967d0af91f36454f141840c7f18ce4904a1300a54")
-setManifestid(1716744,"7835038652843152485")
-addappid(1716745,0,"409f827297a516a03a1619f1f9c419da22001bf78297e4dcf3884af93b4b50c0")
-setManifestid(1716745,"2728823519993102844")
-addappid(1716746,0,"03e63b3a66513fc8232b0675af0269ae351370508e1f988cbca4f6324ab7d86e")
-setManifestid(1716746,"8256719953704350900")
-addappid(1716747,0,"bf59310131ad2859544a15359fc90c7dbdbd7c2fb1efb9e633a9044d6b07975f")
-setManifestid(1716747,"1299082767590167920")
-addappid(2721670,0,"34f5ffb0f43b8e1e3de155a0cea2f5e9f53c87b19431cf7e1bfe08e4d3806ff9")
-setManifestid(2721670,"3372380558904479288")
-addappid(2721671,0,"ccfca2d801690b92437b56892cc3f331c77d8e1688cb75485d6979d179e6aaa2")
-setManifestid(2721671,"7840312422557329126")
-addappid(2721672,0,"5e3835e229258b76be58c76fa52698064aaf2b04f7786a674ed7912b6556cf96")
-setManifestid(2721672,"1504624849949367816")
-addappid(2721673,0,"22841c1a633eccea581ca7b7c5fcf8b0e719cb757e2805d15fabd3ec6789267c")
-setManifestid(2721673,"1691563649532467395")
-addappid(2721674,0,"2444b0653ae7af96c558260ce936805ecda73dfe0a8647e5ff9c1e19d2669ecb")
-setManifestid(2721674,"4251571192310551992")
-addappid(2721675,0,"9c9897d73ba01838de4bad44e2487bd6f103fe70ae45c088c56aeda7e8b3e9e0")
-setManifestid(2721675,"2326521295832541938")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1716740.lua
+--Gamename Starfield
+addappid(1716740, 1, "aadd49bba583a51f17cf343fa4ff71c77f30a51cc417490f7a35b8a3c2dc59ea") --Mainappid Starfield
+addappid(1716741, 1, "14c0bdb25d858d05a7381ba67a373aa99b50c5bd4cf78f1895751d418de29ceb") --Main Windows Depot Starfield
+setManifestid(1716741, "6026043903923231328", 115991472)
+addappid(1716742, 1, "f7ba0bf1fd70db2958682bc87fcab6dfe9a6f8ca774321df79b1d595d5be303b") --Main Windows Depot Starfield
+setManifestid(1716742, "3130703038371677921", 102216324416)
+addappid(1716743, 1, "d0a17417a778577c9edb2b58f5352ad326ba2def007a1e3eacd058ac4293907a") --Main Windows Language Depot German Starfield
+setManifestid(1716743, "7908035178130303133", 6134201264)
+addappid(1716744, 1, "a2c8d4541f82fe48dd2252b967d0af91f36454f141840c7f18ce4904a1300a54") --Main Windows Language Depot Japanese Starfield
+setManifestid(1716744, "7835038652843152485", 6664268096)
+addappid(1716745, 1, "409f827297a516a03a1619f1f9c419da22001bf78297e4dcf3884af93b4b50c0") --Main Windows Language Depot French Starfield
+setManifestid(1716745, "8082676786766068854", 5648166560)
+addappid(1716746, 1, "03e63b3a66513fc8232b0675af0269ae351370508e1f988cbca4f6324ab7d86e") --Main Windows Language Depot Latam Starfield
+setManifestid(1716746, "8256719953704350900", 6317819216)
+addappid(1716747, 1, "bf59310131ad2859544a15359fc90c7dbdbd7c2fb1efb9e633a9044d6b07975f") --Main Windows Language Depot Spanish Starfield
+setManifestid(1716747, "1299082767590167920", 6317819216)
+--Dlcname STARFIELD DIGITAL PREMIUM EDITION UPGRADE
+addappid(2401180, 1, "abb5f01b77e2f81e4b06049a0a17302bd7c9dacc7715b9536f6f3462ad51dd89") --Dlcname STARFIELD DIGITAL PREMIUM EDITION UPGRADE
+setManifestid(2401180, "1350450549444461803", 51092320)
+--Dlcname Starfield - Preorder
+addappid(2401181, 1, "9382f0b0fee3c8ccb0cf9fc2389a52eab89fdf1c4e6f408635a82b30037aed9e") --Dlcname Starfield - Preorder
+setManifestid(2401181, "9009164480135609609", 14823792)
+--Dlcname STARFIELD OFFICIAL SOUNDTRACK
+addappid(2561880) --Dlcname STARFIELD OFFICIAL SOUNDTRACK
+addappid(2561881, 1, "ef3e5627630f1223639cace742a3a0cb21039e71ed9fdde4cc8fa62d41b64a6d") --Dlc Windows Depot STARFIELD OFFICIAL SOUNDTRACK
+setManifestid(2561881, "1792993759553594235", 572171136)
+addappid(2561882, 1, "8c9b474435c4bc69fc3c16004dca803ad314128372ef670bbdf25d5828bf0beb") --Dlc Windows Depot STARFIELD OFFICIAL SOUNDTRACK
+setManifestid(2561882, "1759119827406415985", 4940815744)
+--Dlcname Starfield - Shattered Space
+addappid(2721670, 1, "34f5ffb0f43b8e1e3de155a0cea2f5e9f53c87b19431cf7e1bfe08e4d3806ff9") --Dlcname Starfield - Shattered Space
+setManifestid(2721670, "8614401350451913562", 6811938640)
+addappid(2721671, 1, "ccfca2d801690b92437b56892cc3f331c77d8e1688cb75485d6979d179e6aaa2") --Dlc Windows Language Depot German Starfield - Shattered Space
+setManifestid(2721671, "2022570483820521890", 1089497856)
+addappid(2721672, 1, "5e3835e229258b76be58c76fa52698064aaf2b04f7786a674ed7912b6556cf96") --Dlc Windows Language Depot Japanese Starfield - Shattered Space
+setManifestid(2721672, "5199964521116328049", 1107173440)
+addappid(2721673, 1, "22841c1a633eccea581ca7b7c5fcf8b0e719cb757e2805d15fabd3ec6789267c") --Dlc Windows Language Depot French Starfield - Shattered Space
+setManifestid(2721673, "1551232352573742085", 1081696208)
+addappid(2721674, 1, "2444b0653ae7af96c558260ce936805ecda73dfe0a8647e5ff9c1e19d2669ecb") --Dlc Windows Language Depot Spanish Starfield - Shattered Space
+setManifestid(2721674, "2803893024827218007", 1120476320)
+addappid(2721675, 1, "9c9897d73ba01838de4bad44e2487bd6f103fe70ae45c088c56aeda7e8b3e9e0") --Dlc Windows Language Depot Latam Starfield - Shattered Space
+setManifestid(2721675, "6454078205026451051", 1120476320)
+--Dlcname Starfield - Terran Armada
+addappid(3763700, 1, "2797c7863691541ceccebc7d44902968a889cb785cba5d945dc3a3d401bb813d") --Dlcname Starfield - Terran Armada
+setManifestid(3763700, "8831470945722778174", 3262398304)
+addappid(3763701, 1, "87fc0e1a6e04ccdaf6ba7032e260e714909066c3b004ded9e888381841190992") --Dlc Windows Language Depot German Starfield - Terran Armada
+setManifestid(3763701, "6731229789289957506", 260727776)
+addappid(3763702, 1, "293949da123110d321949e93f49176fb4f7a67705f81b31f5a12febad68143ae") --Dlc Windows Language Depot Japanese Starfield - Terran Armada
+setManifestid(3763702, "8952354822615737755", 273335680)
+addappid(3763703, 1, "b0aa567554d37d5c075d428ccf1d9da7f8146073a6533829481a4f588f0f7f56") --Dlc Windows Language Depot French Starfield - Terran Armada
+setManifestid(3763703, "8941449898558361829", 508480)
+addappid(3763704, 1, "ddfd631e1b163e7d1e824c4516d317c73fc8e3a2e4313316b9e615651b2ab94d") --Dlc Windows Language Depot Spanish Starfield - Terran Armada
+setManifestid(3763704, "5973845605985235256", 260697920)
+addappid(3763705, 1, "f181f528c945f2317e1d9a29d82f2411c6979ddab73f7d6965c590a37c9bfa30") --Dlc Windows Language Depot Latam Starfield - Terran Armada
+setManifestid(3763705, "3958790910375919829", 260697920)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2792440) --Dlcname Starfield - Creations
