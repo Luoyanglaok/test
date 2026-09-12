ecf6b94c2d9d521252ceb223c9a84847db11d297
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:13:08 +0800

    add 4131730.lua

---
+++ 4131730.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4131730.lua
+--Gamename VHOLUME
+addappid(4131730) --Mainappid VHOLUME
+addappid(4131731, 1, "434664fa58d5cf2b3058734a738903a03b70148afe8f2fac9e04043808fe8ede") --Main Windows Depot VHOLUME
+setManifestid(4131731, "6252397139616441924", 5686486576)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname VHOLUME Soundtrack
