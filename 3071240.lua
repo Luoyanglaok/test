37a3acb1d47342a6f932ea528fa2595cb3ae5611
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 11:54:03 +0800

    add 3071240.lua

---
+++ 3071240.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3071240.lua
+--Gamename Voodoo Fishin'
+addappid(3071240, 1, "6fdd18162d00843f1f1930fd56d5e6eeb250623ec2943c538e3939a6b1e670a3") --Mainappid Voodoo Fishin'
+addappid(3071241, 1, "f1cf0cc34cc7625855cbd7da469cd67c7a7b280328d6338359898ed9cd2d525c") --Main Windows Depot Voodoo Fishin'
+setManifestid(3071241, "7653018351439482803", 2841225280)
