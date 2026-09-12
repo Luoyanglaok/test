298d3b4df841a97625fbef3d71b3ad87ad758da8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 18:52:17 +0800

    add 4246640.lua

---
+++ 4246640.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4246640.lua
+--Gamename Dungeon Brawls
+addappid(4246640, 1, "7d3012ec04087077f25a64d6cb4bb1515af0429361ce69baae0d4476c217f8d3") --Mainappid Dungeon Brawls
+addappid(4246641, 1, "38a27266a1ba3a515e0f81afd6604545fc8493c1434a6dc17f3b4e45b836e23a") --Main Windows Depot Dungeon Brawls
+setManifestid(4246641, "8064491275415869058", 1013466992)
+addappid(4817070) --Dlcname Dungeon Brawls - Supporter Pack
+addappid(4817080) --Dlcname Dungeon Brawls - Wings Pack
