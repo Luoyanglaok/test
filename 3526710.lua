167d78a516933088056ff5471747b1df1a4b9b43
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 06:24:38 +0800

    add 3526710.lua

--- 3526710.lua
+++ 3526710.lua
@@ -1,4 +1,7 @@
-addappid(3526710)
-addappid(4540950)
-addappid(3526711,1,"ed639283b231e8c9cf3f2809bafb053eca94f9edd0d68f2678b1b9ecbc100e83")
-setManifestid(3526711,"4745247098709093529")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3526710.lua
+--Gamename Everything is Crab: The Animal Evolution Roguelite
+addappid(3526710) --Mainappid Everything is Crab: The Animal Evolution Roguelite
+addappid(3526711, 1, "ed639283b231e8c9cf3f2809bafb053eca94f9edd0d68f2678b1b9ecbc100e83") --Main Windows Depot Everything is Crab: The Animal Evolution Roguelite
+setManifestid(3526711, "4745247098709093529", 213583856)
+addappid(4540950) --Dlcname Everything is Crab Supporter Pack
