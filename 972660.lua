ea4f5cd30426de8c1cc3f55aca560bd546458aad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 05:37:39 +0800

    add 972660.lua

--- 972660.lua
+++ 972660.lua
@@ -1,9 +1,19 @@
-addappid(972660)
-addappid(972661,0,"187ac179ffc1b8c1f070554d4e13861b9bdbef7ecd1c2e5b53a1413e48e54883")
-setManifestid(972661,"6269447316531223781")
-addappid(972662,0,"7ca518a09e4deb74c6dc6fc2e9de6d80bf74af17db13d5319b9467603aa25d88")
-setManifestid(972662,"8058226904025998493")
-addappid(972663,0,"299c32fb774806b7671917661d5f3f5eb761c728ea22b717a6395d1f2b759543")
-setManifestid(972663,"4029588228379842087")
-addappid(1398770,0,"05e8f50eb4a5b8466f8ea94e8f7c97cb9edd1431b29f8d87012ae9161c1710e9")
-setManifestid(1398770,"4330632686706330779")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 972660.lua
+--Gamename Spiritfarer®: Farewell Edition
+addappid(972660, 1, "262714a8f30961be8c4d1ce1f55ba82b697c58563021127c8e526ad68171b849") --Mainappid Spiritfarer®: Farewell Edition
+addappid(972661, 1, "187ac179ffc1b8c1f070554d4e13861b9bdbef7ecd1c2e5b53a1413e48e54883") --Main Windows Depot Spiritfarer®: Farewell Edition
+setManifestid(972661, "6269447316531223781", 1572665792)
+addappid(972662, 1, "7ca518a09e4deb74c6dc6fc2e9de6d80bf74af17db13d5319b9467603aa25d88") --Main Macos Depot Spiritfarer®: Farewell Edition
+setManifestid(972662, "8058226904025998493", 1565562080)
+addappid(972663, 1, "299c32fb774806b7671917661d5f3f5eb761c728ea22b717a6395d1f2b759543") --Main Linux Depot Spiritfarer®: Farewell Edition
+setManifestid(972663, "4029588228379842087", 1564158944)
+--Dlcname Spiritfarer Soundtrack
+addappid(1395720) --Dlcname Spiritfarer Soundtrack
+addappid(1395721, 1, "a6bfc4d74a6b15f279bec20e552c895ecfebd3f65a195154a4578ce0dc9b2629") --Dlc Windows Depot Spiritfarer Soundtrack
+setManifestid(1395721, "1548922401604070666", 396369744)
+addappid(1395722, 1, "bea02899e5127d712b7455da964dfc474fde00323b4894acddc2232d139dd192") --Dlc Windows Depot Spiritfarer Soundtrack
+setManifestid(1395722, "8993206909832146657", 790334576)
+--Dlcname Spiritfarer - Digital Artbook
+addappid(1398770, 1, "05e8f50eb4a5b8466f8ea94e8f7c97cb9edd1431b29f8d87012ae9161c1710e9") --Dlcname Spiritfarer - Digital Artbook
+setManifestid(1398770, "4330632686706330779", 54597360)
