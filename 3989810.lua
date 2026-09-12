d663427628b678d374d89f4813f89aff3b320650
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:03:26 +0800

    add 3989810.lua

---
+++ 3989810.lua
@@ -0,0 +1,22 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3989810.lua
+--Gamename FUTA FUCKS FEMBOYS
+addappid(3989810, 1, "846496d313800d76ae5e8243a59f69791190096542412a76d07a5fee364c0ec7") --Mainappid FUTA FUCKS FEMBOYS
+addappid(3989811, 1, "f6e8f5535ace4378147ca42a0a7522dda75edff837b68f3dc656f4c95fcebb99") --Main Windows Depot FUTA FUCKS FEMBOYS
+setManifestid(3989811, "4105552577530775311", 1377943728)
+addappid(3989812, 1, "3a4a91f1962fb8c5a4262927397f35c5a953e4720071ca8cae01bf2b215abf9b") --Main Macos Depot FUTA FUCKS FEMBOYS
+setManifestid(3989812, "9171179676351658915", 1388435616)
+--Dlcname FUTA FUCKS FEMBOYS - Animations Pack
+addappid(4395420, 1, "3b1bcd39ead4d3af921a5476214c3d65a8becd99b90500468eaa6ce5ec4f6ac2") --Dlcname FUTA FUCKS FEMBOYS - Animations Pack
+setManifestid(4395420, "753274894698518315", 6416246576)
+--Dlcname FUTA FUCKS FEMBOYS - Digital Artbook
+addappid(4395430, 1, "ab9ebc2c100bed0a80c02e3086345d23ad0b9a8f394fefe6b8dea0934746a6a8") --Dlcname FUTA FUCKS FEMBOYS - Digital Artbook
+setManifestid(4395430, "4475992030326862069", 801336304)
+--Dlcname FUTA FUCKS FEMBOYS - Wallpapers Pack
+addappid(4395440, 1, "3b1aff0141c2611775dac4ab574539c090ce0c0f98c464d6cac5026636882192") --Dlcname FUTA FUCKS FEMBOYS - Wallpapers Pack
+setManifestid(4395440, "263653302676348636", 907136048)
+addappid(4925520) --Dlcname FUTA FUCKS FEMBOYS - How Onyx Stole Kissmass DLC
+--Missing Dlcs
+--Dlcname FUTA FUCKS FEMBOYS - Soundtrack
+--Dlcname FUTA FUCKS FEMBOYS - XXXL Skins Pack
+--Dlcname FUTA FUCKS FEMBOYS - Harley's Adventures DLC
