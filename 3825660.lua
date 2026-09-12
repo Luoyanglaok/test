293e10bfc1761cee99a6a2e5551f174d3efc0e14
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 19:06:57 +0800

    add 3825660.lua

---
+++ 3825660.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3825660.lua
+--Gamename Isekai Big Two
+addappid(3825660, 1, "d19d293d08999e561460698073ff306a90881f852e2405264c8259410c2c5a38") --Mainappid Isekai Big Two
+addappid(3825661, 1, "5106a11c11738158f0a8fa8e312fb8f3e5cb831c70bd2f329326b6b5f2014272") --Main Windows Depot Isekai Big Two
+setManifestid(3825661, "4154473957206136358", 1343838080)
