9b574611b0fd289f4cc9a39f63c61425e3c9fb22
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 18:07:10 +0800

    add 1282760.lua

--- 1282760.lua
+++ 1282760.lua
@@ -1,3 +1,6 @@
-addappid(1282760)
-addappid(1282761,0,"5321960cdea41e3e93e37be21b0ff8446ca360073ac57e667330ceaecb4dd5a4")
-setManifestid(1282761,"3483535207374547737")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1282760.lua
+--Gamename Mad Streets
+addappid(1282760) --Mainappid Mad Streets
+addappid(1282761, 1, "5321960cdea41e3e93e37be21b0ff8446ca360073ac57e667330ceaecb4dd5a4") --Main Windows Depot Mad Streets
+setManifestid(1282761, "3483535207374547737", 1030633888)
