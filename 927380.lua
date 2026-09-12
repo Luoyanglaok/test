f760f94e75e14cd944bcc613113919dd8608c892
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 21:51:52 +0800

    add 927380.lua

--- 927380.lua
+++ 927380.lua
@@ -1,5 +1,9 @@
-addappid(927380)
-addappid(927381,0,"23ab2918d89de8bcea11ba1517ea120e1c8bc8ef188f22abefeb07765362193c")
-setManifestid(927381,"8687551471438537642")
-addappid(927382,0,"5fab821dbe4cb51d6cede5ab01323beb934e97d7234cd006748536c657693c54")
-setManifestid(927382,"8846117159153844266")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 927380.lua
+--Gamename Yakuza Kiwami 2 (Legacy)
+addappid(927380, 1, "7109a41b4d24b94103fca55fbbc1f78e2cdc67247ab5cfc4c198c71541fa37a0") --Mainappid Yakuza Kiwami 2 (Legacy)
+addappid(927381, 1, "23ab2918d89de8bcea11ba1517ea120e1c8bc8ef188f22abefeb07765362193c") --Main Windows Depot Yakuza Kiwami 2 (Legacy)
+setManifestid(927381, "8687551471438537642", 37686901344)
+addappid(927382, 1, "5fab821dbe4cb51d6cede5ab01323beb934e97d7234cd006748536c657693c54") --Main Windows Depot Yakuza Kiwami 2 (Legacy)
+setManifestid(927382, "8846117159153844266", 14499280)
+addappid(1030500) --Dlcname Yakuza Kiwami 2 - Clan Creator Bundle
