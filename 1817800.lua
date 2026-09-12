b50892e9e87781e8b3fd9bb8732a446def1bd410
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 02:44:41 +0800

    add 1817800.lua

--- 1817800.lua
+++ 1817800.lua
@@ -1,6 +1,19 @@
-addappid(1817800)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229007,"4477590687906973371")
-addappid(1817801,0,"b59ebe145d953e10adb462fd930cbf0da37483e64c0896babf1861638f87b6a5")
-setManifestid(1817801,"5854655456476323598")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1817800.lua
+--Gamename Oddsparks: An Automation Adventure
+addappid(1817800, 1, "5b713a6995db88c78277eda45ff02db74276d6f0eb4b594e939217a23970a6e3") --Mainappid Oddsparks: An Automation Adventure
+addappid(1817801, 1, "b59ebe145d953e10adb462fd930cbf0da37483e64c0896babf1861638f87b6a5") --Main Windows Depot Oddsparks: An Automation Adventure
+setManifestid(1817801, "4056015573089703636", 3171234992)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(3073500) --Dlcname Oddsparks: An Automation Adventure - Deluxe Upgrade
+addappid(3678620) --Dlcname Oddsparks - Tailor's Animal Costumes Pack
+addappid(3678630) --Dlcname Oddsparks - Craftsman's Elemental Decorations Pack
+addappid(3678640) --Dlcname Oddsparks: An Automation Adventure - Making Music
+addappid(3678650) --Dlcname Oddsparks: An Automation Adventure - Hats & Helmets
+addappid(3916680) --Dlcname Oddsparks: An Automation Adventure - Coaster Rush
