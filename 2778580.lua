50c144e8a9a98d93a8f1f70e7ed50b3efe33894b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 10:29:28 +0800

    add 2778580.lua

--- 2778580.lua
+++ 2778580.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 2778580.lua
-addappid(2778580, 0, "bbe759343bb9cf47834e56828c22aa15bc18bedfc68f36879666b64b38d1a634")
+addappid(2778580, 0, "880e2971be8d9f6bb244a4d977bdf04280f9594d3a495562da9edbdd704e430d")
