d85e297a9b21de4e1700c236697e4621aeff7e62
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 17:14:42 +0800

    add 1641700.lua

--- 1641700.lua
+++ 1641700.lua
@@ -1,5 +1,16 @@
-addappid(1641700)
-addappid(1641701,0,"87425c1557b863d4091a52a61a146d1634d5de17464e6e2107c05b2806ffd4ce")
-setManifestid(1641701,"2956349555802483894")
-addappid(2502500,0,"4f8bd8296b8af0d840f53c6d1f93d57613db76ade437a0105e5c4fabc4475410")
-setManifestid(2502500,"3137666977931661277")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1641700.lua
+--Gamename Moving Out 2
+addappid(1641700) --Mainappid Moving Out 2
+addappid(1641701, 1, "87425c1557b863d4091a52a61a146d1634d5de17464e6e2107c05b2806ffd4ce") --Main Windows Depot Moving Out 2
+setManifestid(1641701, "9050846351133172747", 1084523040)
+--Dlcname Moving Out 2 - Soundtrack
+addappid(2403460) --Dlcname Moving Out 2 - Soundtrack
+addappid(2403462, 1, "e87ba56bb37d28f109e08dbd8dbdc0e13debb44dda2cfa9e216c3d1cd59e9b2b") --Dlc Windows Depot Moving Out 2 - Soundtrack
+setManifestid(2403462, "3430841551306650820", 143608576)
+addappid(2403463, 1, "892b3094972b94cea317e6fd6f0bfd3a9efa4470bc3a8003a77d7bffb928419c") --Dlc Windows Depot Moving Out 2 - Soundtrack
+setManifestid(2403463, "3409976433453799852", 1039984064)
+--Dlcname Moving Out 2 - Digital Artbook
+addappid(2502500, 1, "4f8bd8296b8af0d840f53c6d1f93d57613db76ade437a0105e5c4fabc4475410") --Dlcname Moving Out 2 - Digital Artbook
+setManifestid(2502500, "3137666977931661277", 154576608)
+addappid(2410580) --Dlcname Moving Out 2 - F.A.R.Tastic Four DLC
