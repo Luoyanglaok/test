fe7e1c5a276b8256b66a3120bbdc5844244e7aa5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 18:40:00 +0800

    add 1836590.lua

--- 1836590.lua
+++ 1836590.lua
@@ -1,3 +1,6 @@
-addappid(1836590)
-addappid(1836591,0,"e8c4a7f54b846e765081a272884ee8fcc05b9a083040b8970c3cef6a73773bdc")
-setManifestid(1836591,"8153988727994941053")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1836590.lua
+--Gamename Sex Dating Trip
+addappid(1836590) --Mainappid Sex Dating Trip
+addappid(1836591, 1, "e8c4a7f54b846e765081a272884ee8fcc05b9a083040b8970c3cef6a73773bdc") --Main Windows Depot Sex Dating Trip
+setManifestid(1836591, "8153988727994941053", 405410464)
