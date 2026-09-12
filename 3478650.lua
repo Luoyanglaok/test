c01016aeb932bcb69275dbfc02bd6c1150dfb3b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 15:22:41 +0800

    add 3478650.lua

--- 3478650.lua
+++ 3478650.lua
@@ -1,5 +1,13 @@
-addappid(3478650)
-setManifestid(3478651,"8297209302547635228")
-setManifestid(3930100,"8769246009326709623")
-setManifestid(3478652,"991123513066954076")
-setManifestid(3478653,"577570399620717255")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3478650.lua
+--Gamename FreshWomen - Season 2
+addappid(3478650, 1, "bae1c2f2922a2615da15651ab9306f3b4314b0765de99d37318ac1f366b68735") --Mainappid FreshWomen - Season 2
+addappid(3478651, 1, "ac18510eeb2d0b975c7fb8019572de8c568179c25d765f22223bce123eff775b") --Main Windows Depot FreshWomen - Season 2
+setManifestid(3478651, "8297209302547635228", 13960638608)
+addappid(3478652, 1, "2dd992e4f3dadcf439e1de3232f0ff86e042edae9980d6eb94c6db9b4ff46e64") --Main Windows Depot FreshWomen - Season 2
+setManifestid(3478652, "991123513066954076", 4280366272)
+addappid(3478653, 1, "4e696364f3a9cd07331e22c1db769659d48971618d88a580402f9f61a9c88626") --Main Windows Depot FreshWomen - Season 2
+setManifestid(3478653, "577570399620717255", 4413309376)
+--Dlcname FreshWomen - Season 2 - Supporter Pack
+addappid(3930100, 1, "4cfc083bd45f19ea36197cdeafdf100cb186c1e56d447b80f94517f1fa9c60bd") --Dlcname FreshWomen - Season 2 - Supporter Pack
+setManifestid(3930100, "8769246009326709623", 1247239488)
