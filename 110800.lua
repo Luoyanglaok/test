a279c7a2e754e7588a961c18c1784fb86c6987cc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 18:32:56 +0800

    add 110800.lua

--- 110800.lua
+++ 110800.lua
@@ -1,9 +1,14 @@
-addappid(110800)
-addappid(110802,0,"74851338b906da06cd9e860d075ed5d8eb654b326d152a518ef3b62bea4baae6")
-setManifestid(110802,"3234208118368148040")
-addappid(110810,0,"d5ffeca506e20ff835c9f9e6146e7163845408c264596f8d314712855d238f68")
-setManifestid(110810,"239200575577592374")
-addappid(110801,0,"34360e9d27bff94c1c6135d12ed4818d28e60082b3f7f984ed7f0c0eeecfc3b6")
-setManifestid(110801,"7802845176688782381")
-addappid(1899671,0,"b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b")
-setManifestid(1899671,"1313721849634300954")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 110800.lua
+--Gamename L.A. Noire
+addappid(110800, 1, "4164ca93339aea96e138751a0460e1a7367c5b576741ba80c34524412694f9bb") --Mainappid L.A. Noire
+addappid(110801, 1, "34360e9d27bff94c1c6135d12ed4818d28e60082b3f7f984ed7f0c0eeecfc3b6") --Main Windows Depot L.A. Noire
+setManifestid(110801, "7802845176688782381", 13787094928)
+addappid(110802, 1, "74851338b906da06cd9e860d075ed5d8eb654b326d152a518ef3b62bea4baae6") --Main Windows Depot L.A. Noire
+setManifestid(110802, "3234208118368148040", 10448112)
+--Dlcname L.A. Noire: DLC Bundle
+addappid(110810, 1, "d5ffeca506e20ff835c9f9e6146e7163845408c264596f8d314712855d238f68") --Dlcname L.A. Noire: DLC Bundle
+setManifestid(110810, "239200575577592374", 176)
+--Share Depots
+addappid(1899671, 1, "b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b") --Share Windows Depot RGL/SC
+setManifestid(1899671, "3914969219305676290", 209058416)
