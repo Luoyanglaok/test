b9ebfd436b3fe6ad843db2df12b64bb546cca073
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:12:29 +0800

    add 4920100.lua

---
+++ 4920100.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4920100.lua
+--Gamename 列车怪谈：虚像之境 The Wraith Train: Illusory Domain
+addappid(4920100) --Mainappid 列车怪谈：虚像之境 The Wraith Train: Illusory Domain
+addappid(4920101, 1, "f7f4f2b276656840026dddd2aae2bb50ba3e26865d6f7f2935c4acf1ed9d9d51") --Main Windows Depot 列车怪谈：虚像之境 The Wraith Train: Illusory Domain
+setManifestid(4920101, "6303150204858558743", 15460943824)
