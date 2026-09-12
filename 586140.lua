11c3bbe0f14d807232cba683801348f868e71482
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 16:47:43 +0800

    add 586140.lua

--- 586140.lua
+++ 586140.lua
@@ -1,5 +1,12 @@
-addappid(586140)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(586141,0,"c99c2fc9ec1bf1a45fc4f91bbc02b59692e46294454be80955658b710f9659ed")
-setManifestid(586141,"7510438104584471661")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 586140.lua
+--Gamename BlazBlue Centralfiction
+addappid(586140, 1, "4bf84db94f54b179a00cb6141ac5e308588f9d5aa55b899cc618f8dd876f9d12") --Mainappid BlazBlue Centralfiction
+addappid(586141, 1, "c99c2fc9ec1bf1a45fc4f91bbc02b59692e46294454be80955658b710f9659ed") --Main Windows Depot BlazBlue Centralfiction
+setManifestid(586141, "7510438104584471661", 16477993056)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(708880) --Dlcname BlazBlue Centralfiction - Additional Playable Character JUBEI
