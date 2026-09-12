ac10b4ad179fb01890d754dd9319b1a8cbf9d958
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:27:28 +0800

    add 2592160.lua

---
+++ 2592160.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2592160.lua
+--Gamename Dispatch
+addappid(2592160, 1, "94ae1de1f6d1f0fb70fe57e5fbcfd7761611372ed8325748dc0f011bcc3781f5") --Mainappid Dispatch
+addappid(2592161, 1, "5b4a3a4477e47584b99200004c6f4ef8cf41be05df586de9e1771e160fc7527d") --Main Windows Depot Dispatch
+setManifestid(2592161, "3163669758231840767", 14574251760)
+--Dlcname Dispatch Soundtrack
+addappid(3920290) --Dlcname Dispatch Soundtrack
+addappid(3920291, 1, "44e7eea22514cc0624e270947cc4000eceaf4a9c91bf4b5b0f1dbf014f8352a3") --Dlc Windows Depot Dispatch Soundtrack
+setManifestid(3920291, "4806059628382357785", 543968960)
+addappid(3920292, 1, "1773afcb82620efc3f45e98fd78108afab20252d50e931e88020f680c6449cd3") --Dlc Windows Depot Dispatch Soundtrack
+setManifestid(3920292, "5916346750942358801", 2193955552)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3920280) --Dlcname Dispatch - Deluxe Edition Upgrade
