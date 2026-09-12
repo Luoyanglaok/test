9edea55f2c041172e9282ee1f3d29e25ab0b5c93
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 17:19:31 +0800

    add 2438110.lua

---
+++ 2438110.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2438110.lua
+--Gamename Emergency Room Simulator
+addappid(2438110) --Mainappid Emergency Room Simulator
+addappid(2438111, 1, "419aca58b5376b3ea49d013450b85a805a708cb1fe429ba7b0cd846534a85b4f") --Main Windows Depot Emergency Room Simulator
+setManifestid(2438111, "6302828478794278746", 4938121760)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
