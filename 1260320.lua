6b71c9b39e368bf6f08bf6143ea6a5bd6030d038
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:55:43 +0800

    add 1260320.lua

--- 1260320.lua
+++ 1260320.lua
@@ -1,6 +1,12 @@
-addappid(1260320)
-addappid(2342040)
-addappid(1260321,1,"9623dafeee383701a837274d2547c7a5cbae7334393d64e82b4d2cdc01ef679e")
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-setManifestid(1260321,"7166497126909714168")
-setManifestid(228989,"3514306556860204959")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1260320.lua
+--Gamename Party Animals
+addappid(1260320, 1, "bc1f6dc33cc56a820c67ef2a4e6a6a601077f693cc39530bbed01b96a2acdfcb") --Mainappid Party Animals
+addtoken(1260320, "7641392735724118585")
+addappid(1260321, 1, "9623dafeee383701a837274d2547c7a5cbae7334393d64e82b4d2cdc01ef679e") --Main Windows Depot Party Animals
+setManifestid(1260321, "7166497126909714168", 12434367632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2342040) --Dlcname Party Animals - Deluxe Pack
+addappid(2545150) --Dlcname Party Animals - Preorder Pack
