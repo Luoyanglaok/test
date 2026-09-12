58afaa9f25ffc5991212fcd55247c7cb91b25c0e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 19:02:54 +0800

    add 628070.lua

--- 628070.lua
+++ 628070.lua
@@ -1,7 +1,11 @@
-addappid(628070)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(628071,0,"8dc6b735d02d38ee6622843378ed3b020e7b215b0a686a6e11e2ea53b898f7ac")
-setManifestid(628071,"1454871840120194639")
-addappid(628072,0,"b3abca25c927403a9625b28878837092bea054c7fbdf652d740de7eb0a32d5ac")
-setManifestid(628072,"3611590611992064954")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 628070.lua
+--Gamename Romance of the Three Kingdoms XI with Power Up Kit
+addappid(628070) --Mainappid Romance of the Three Kingdoms XI with Power Up Kit
+addappid(628071, 1, "8dc6b735d02d38ee6622843378ed3b020e7b215b0a686a6e11e2ea53b898f7ac") --Main Windows Depot Romance of the Three Kingdoms XI with Power Up Kit
+setManifestid(628071, "1454871840120194639", 1345322928)
+addappid(628072, 1, "b3abca25c927403a9625b28878837092bea054c7fbdf652d740de7eb0a32d5ac") --Main Windows Language Depot Tchinese Romance of the Three Kingdoms XI with Power Up Kit
+setManifestid(628072, "3611590611992064954", 1379744176)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
