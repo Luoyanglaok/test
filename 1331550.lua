ebdc2bb747f21e81b48c2ed5af76813da8a62ebb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 01:24:07 +0800

    add 1331550.lua

--- 1331550.lua
+++ 1331550.lua
@@ -1,5 +1,8 @@
-addappid(1331550)
-addappid(1331551,0,"f3b44da48f4335a1dad963250d348f21e38cb27fbe8264e2cd0ebf217849a089")
-setManifestid(1331551,"1430793071555567569")
-addappid(1331552,0,"1547279019dc793ecffc735c9690f060ed6a8c2fd0145e1e4c605b795cc423d9")
-setManifestid(1331552,"5846712122292133977")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1331550.lua
+--Gamename Big Ambitions
+addappid(1331550, 1, "159ee5940ca12f8daef8eccc0d528f6dff71e38da7b5d9fec465ed4fdd63e401") --Mainappid Big Ambitions
+addappid(1331551, 1, "f3b44da48f4335a1dad963250d348f21e38cb27fbe8264e2cd0ebf217849a089") --Main Windows Depot Big Ambitions
+setManifestid(1331551, "7001139920346164058", 3095729088)
+addappid(1331552, 1, "1547279019dc793ecffc735c9690f060ed6a8c2fd0145e1e4c605b795cc423d9") --Main Macos Depot Big Ambitions
+setManifestid(1331552, "8124138447764310195", 2947319664)
