8945ae557898aca0aaca2316e514efe4e07d8bea
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 05:23:40 +0800

    add 3362750.lua

---
+++ 3362750.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3362750.lua
+--Gamename Stormbridge
+addappid(3362750) --Mainappid Stormbridge
+addappid(3362751, 1, "ad8f7c810723c11621e18fccd7ecdae177bde913cdc806b421aecd830ab075b1") --Main Windows Depot Stormbridge
+setManifestid(3362751, "181037262468826792", 271565696)
