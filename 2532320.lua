7eadea21bd02968f49c60947b934beaa35f3ce75
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 15:51:01 +0800

    add 2532320.lua

--- 2532320.lua
+++ 2532320.lua
@@ -1,3 +1,6 @@
-addappid(2532320)
-addappid(2532321,0,"9c790156c8c42b90b120f4a5e47cac25dc66410750581a500d4bc651aa382a16")
-setManifestid(2532321,"2637793463713005580")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2532320.lua
+--Gamename 樱花校园之恋模拟器
+addappid(2532320) --Mainappid 樱花校园之恋模拟器
+addappid(2532321, 1, "9c790156c8c42b90b120f4a5e47cac25dc66410750581a500d4bc651aa382a16") --Main Windows Language Depot Schinese 樱花校园之恋模拟器
+setManifestid(2532321, "2637793463713005580", 135014512)
