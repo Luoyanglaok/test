8ff822814c4f93e6d3e3fca23203860bf3fc7db5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:05:23 +0800

    add 868360.lua

--- 868360.lua
+++ 868360.lua
@@ -1,9 +1,43 @@
-addappid(868360)
-addappid(868362,0,"4f5081e288327046084fa377c67cd5aef2b6307d23a63f139938563ef44e5dab")
-setManifestid(868362,"7178818363705911350")
-addappid(868363,0,"d373db0fd7c1ee5ebf860648b90f34da40e3c9144b5c83a52abc4dc4cd51bcd5")
-setManifestid(868363,"1555285924497261809")
-addappid(868364,0,"f31e148df5675a1886aff8250ad2090f7713b0249f9ca221b9a696c3af9714a7")
-setManifestid(868364,"3250394954169298989")
-addappid(1149760,0,"5fb70dc38e24afb9ba35f48053c7e183732c87980a10715e6fdeba77bc3c7fc5")
-setManifestid(1149760,"8548966852256690719")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 868360.lua
+--Gamename Project Hospital
+addappid(868360, 1, "ef496df548ac2ac235ec9f63c171bd7fe0c6654780bf83aa26a14d829cefd73e") --Mainappid Project Hospital
+addappid(868362, 1, "4f5081e288327046084fa377c67cd5aef2b6307d23a63f139938563ef44e5dab") --Main Windows Depot Project Hospital
+setManifestid(868362, "1524862661757441836", 205839712)
+addappid(868363, 1, "d373db0fd7c1ee5ebf860648b90f34da40e3c9144b5c83a52abc4dc4cd51bcd5") --Main Linux Depot Project Hospital
+setManifestid(868363, "4005820175882538502", 206612288)
+addappid(868364, 1, "f31e148df5675a1886aff8250ad2090f7713b0249f9ca221b9a696c3af9714a7") --Main Macos Depot Project Hospital
+setManifestid(868364, "2643269129776459190", 206813808)
+--Dlcname Project Hospital - Doctor Mode
+addappid(1149760, 1, "5fb70dc38e24afb9ba35f48053c7e183732c87980a10715e6fdeba77bc3c7fc5") --Dlcname Project Hospital - Doctor Mode
+setManifestid(1149760, "8548966852256690719", 0)
+addappid(1149761, 1, "c0ca13ce571f95368cfbb70b2f382380b7237eb3be164af332a4610e70d1ae1b") --Dlc Windows Depot Project Hospital - Doctor Mode
+setManifestid(1149761, "905555328422869814", 722416)
+addappid(1149762, 1, "26D8B148D0F548D8CBD3D95D3191C4CDE217536B306DBDD1C6D2CC70F9CE2957") --Dlc Macos Depot Project Hospital - Doctor Mode
+setManifestid(1149762, "5257291705244729811", 722416)
+addappid(1149763, 1, "AEA81BF29A5F89B6076375E95AF7FE5CE0A4EB08BF110353686EFA34FCA92E09") --Dlc Linux Depot Project Hospital - Doctor Mode
+setManifestid(1149763, "3234842771297980719", 722416)
+--Dlcname Project Hospital - Hospital Services
+addappid(1282550) --Dlcname Project Hospital - Hospital Services
+addappid(1282551, 1, "1194d8920c53c77a582f8eded1efddd58dff69f1feeef03abc3b70a40cb8f450") --Dlc Windows Depot Project Hospital - Hospital Services
+setManifestid(1282551, "6228204657866527077", 611680)
+addappid(1282552, 1, "7c8b8f513073691ae4ce239a42e79fc3b923c5f48908d8d31761c1396b51be96") --Dlc Macos Depot Project Hospital - Hospital Services
+setManifestid(1282552, "3858146742842737906", 611680)
+addappid(1282553, 1, "0e14341c052c873c0b6a29f72f90af6260806d52c3289664f71a25fea30190ba") --Dlc Linux Depot Project Hospital - Hospital Services
+setManifestid(1282553, "3279470454629538544", 611680)
+--Dlcname Project Hospital - Department of Infectious Diseases
+addappid(1368210) --Dlcname Project Hospital - Department of Infectious Diseases
+addappid(1368211, 1, "e8304490307d54a9c5092ff598919fa9363613103f751e246690ba90c0f2be88") --Dlc Macos Depot Project Hospital - Department of Infectious Diseases
+setManifestid(1368211, "15049201019964635", 1529904)
+addappid(1368212, 1, "bbd712271ed4977df01f2509d461b7148f7a0f81da7e00ba3fec257db9b2187d") --Dlc Linux Depot Project Hospital - Department of Infectious Diseases
+setManifestid(1368212, "7352467108305907491", 1529904)
+addappid(1368213, 1, "f814e39cc322cccf287e3a140d5ca26c8047ac3c37ce5ff0660e9ff08cb22a38") --Dlc Windows Depot Project Hospital - Department of Infectious Diseases
+setManifestid(1368213, "3984309746366752956", 1529904)
+--Dlcname Project Hospital - Traumatology Department
+addappid(1423900) --Dlcname Project Hospital - Traumatology Department
+addappid(1423901, 1, "e769b7cf6049294e299b483c76c9526892d71e9ac191975d7d4abf680aa4bdc1") --Dlc Windows Depot Project Hospital - Traumatology Department
+setManifestid(1423901, "6461724812251058930", 1670864)
+addappid(1423902, 1, "8c80b060cfcfd64baa71e2ca772c88730a554cf024959c037176498ca49b7acc") --Dlc Macos Depot Project Hospital - Traumatology Department
+setManifestid(1423902, "7484351723154814162", 1670864)
+addappid(1423903, 1, "03273afc355f0e967cd1d9048f2e8a54ebfb8be1a0f1f68da036b575a2100852") --Dlc Linux Depot Project Hospital - Traumatology Department
+setManifestid(1423903, "501993010057503915", 1670864)
