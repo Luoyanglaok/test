b3243db11dc00051e7c071ed9feb3c0ae80fdcc2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 06:08:32 +0800

    add 3115220.lua

---
+++ 3115220.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3115220.lua
+--Gamename Town to City
+addappid(3115220, 1, "2dca5bdc5b7be5a765eaa8693ccc21391d40685099f898aa747fcc728d4f9279") --Mainappid Town to City
+addappid(3115221, 1, "2bee0d6f4424d4ab2e390fe00b111e2c779f83a34f8a9fd16984e6edba09f8be") --Main Windows Depot Town to City
+setManifestid(3115221, "1649931187295680809", 2174011584)
+--Dlcname Town To City Soundtrack
+addappid(3829160) --Dlcname Town To City Soundtrack
+addappid(3829161, 1, "954c99ff3984c730fa122651d57fb99a3aeaf62201dd125ebd7956f6c05677aa") --Dlc Windows Depot Town To City Soundtrack
+setManifestid(3829161, "3985961639671305986", 111237792)
+addappid(3829162, 1, "af437bf57d5d05460afb6373c72491bc3dcfbc638020dd060a8e2842e55fb597") --Dlc Windows Depot Town To City Soundtrack
+setManifestid(3829162, "2836445132662355587", 849613056)
+--Dlcname Town to City Digital Artbook
+addappid(3873380, 1, "0d30c479951e8bf4f4f5f50e5c9412f92f4608184fb459a04871939c13067219") --Dlcname Town to City Digital Artbook
+setManifestid(3873380, "8326927651341020770", 59579456)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
