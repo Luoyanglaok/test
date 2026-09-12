bd6fcda72f6cec765d137ef5ed666f83d64e3f96
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:39:53 +0800

    add 3065360.lua

---
+++ 3065360.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3065360.lua
+--Gamename Chop Chop Together
+addappid(3065360) --Mainappid Chop Chop Together
+addtoken(3065360, "3964656790747603711")
+addappid(3065361, 1, "28bbb3f114756dcdfa465afc08aab902df7bef49d902b7cafdcf0c3884e56a1c") --Main Windows Depot Chop Chop Together
+setManifestid(3065361, "2961126354710301469", 1304951696)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
