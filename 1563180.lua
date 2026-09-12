caec349369f8e803d529b9058f785e386a715b5a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 22:09:21 +0800

    add 1563180.lua

--- 1563180.lua
+++ 1563180.lua
@@ -1,3 +1,6 @@
-addappid(1563180)
-addappid(1563181,0,"deb015c5f355a865f557ef2511c27ed07fed2214540bad2c41e5567e65cd2c62")
-setManifestid(1563181,"3099060878986052939")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1563180.lua
+--Gamename Internet Cafe Simulator 2
+addappid(1563180) --Mainappid Internet Cafe Simulator 2
+addappid(1563181, 1, "deb015c5f355a865f557ef2511c27ed07fed2214540bad2c41e5567e65cd2c62") --Main Windows Depot Internet Cafe Simulator 2
+setManifestid(1563181, "3099060878986052939", 2263405504)
