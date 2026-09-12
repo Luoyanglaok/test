d0a89a186cd68c6793cdd2385b2779cc0f7604ef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 16:20:00 +0800

    add 846770.lua

--- 846770.lua
+++ 846770.lua
@@ -1,6 +1,14 @@
-addappid(846770)
-setManifestid(228989,"3514306556860204959")
-addappid(846771,0,"c345ec3e46cd18e15a4f18b0c8e1c099ab995829330b6a66d5a8f47894be732f")
-setManifestid(846771,"8404607684717552108")
-addappid(846772,0,"a0fcc1b3487aacf7ed5abbd1859ee12a145b61fb902bb71961426d4921e9a0ae")
-setManifestid(846772,"3996830895741847660")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 846770.lua
+--Gamename DYSMANTLE
+addappid(846770, 1, "e62c47c969d5072004230043978c146f4b1ccfec300b7d1f73a86ab637d560d5") --Mainappid DYSMANTLE
+addappid(846771, 1, "c345ec3e46cd18e15a4f18b0c8e1c099ab995829330b6a66d5a8f47894be732f") --Main Windows Depot DYSMANTLE
+setManifestid(846771, "5075101902608519471", 1449569584)
+addappid(846772, 1, "a0fcc1b3487aacf7ed5abbd1859ee12a145b61fb902bb71961426d4921e9a0ae") --Main Macos Depot DYSMANTLE
+setManifestid(846772, "8514681507973902214", 1162714848)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2004110) --Dlcname DYSMANTLE: Underworld
+addappid(2147250) --Dlcname DYSMANTLE: Doomsday
+addappid(2439690) --Dlcname DYSMANTLE: Pets & Dungeons
