e6542a78e063ae408dc40f4d0a967bf10796103a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 11:55:51 +0800

    add 699130.lua

--- 699130.lua
+++ 699130.lua
@@ -1,4 +1,31 @@
-addappid(699130)
-setManifestid(228988,"6645201662696499616")
-addappid(699131,0,"8e1c37df6ec32e30f09ea256f8732eb98b7955ec82c51c49960f0d078d057fbb")
-setManifestid(699131,"4637816073556083019")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 699130.lua
+--Gamename World War Z
+addappid(699130, 1, "cea3b55dec0169ec0c0c08823356b8c1ad1b3b5e5b56f00dd2bd9459abff53dd") --Mainappid World War Z
+addappid(699131, 1, "8e1c37df6ec32e30f09ea256f8732eb98b7955ec82c51c49960f0d078d057fbb") --Main Windows Depot World War Z
+setManifestid(699131, "3615152122155594123", 49648517392)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(1640650) --Dlcname World War Z: Explorer Weapons Pack
+addappid(1708020) --Dlcname World War Z: Zeke Hunter Weapons Pack
+addappid(2170270) --Dlcname World War Z: Raven Weapons Skin Pack
+addappid(2226070) --Dlcname World War Z: Aftermath - Deadly Vice Weapons Skin Pack
+addappid(2243210) --Dlcname World War Z: Aftermath Upgrade
+addappid(2313390) --Dlcname World War Z: The Rat Packs Weapon Skins Bundle
+addappid(2475060) --Dlcname World War Z: Victory Lap Weapons Skin Pack
+addappid(2540380) --Dlcname World War Z: Burning Skulls Weapons Skin Pack
+addappid(2614650) --Dlcname World War Z: Valley of the Zeke Episode
+addappid(2614660) --Dlcname World War Z: Pre-Apocalypse Character Skins Pack
+addappid(2962180) --Dlcname World War Z: Aftermath - Desert Defenders Weapons Pack
+addappid(3100200) --Dlcname World War Z: Aftermath - Golden Bloom Weapon Skins Pack
+addappid(3245120) --Dlcname World War Z: Sin City Apocalypse Episode
+addappid(3245130) --Dlcname World War Z: Vegas Skin Pack
+addappid(3699420) --Dlcname World War Z - Chibi Kimiko Skin Pack
+addappid(3699430) --Dlcname World War Z: Chibi Izzy Skin Pack
+addappid(3930030) --Dlcname World War Z: Vibrant Halloween Skin Pack
+addappid(3930040) --Dlcname World War Z: Raging Phoenix Skin Pack
+addappid(4057370) --Dlcname World War Z x The Walking Dead
+addappid(4197300) --Dlcname World War Z - Lunar New Year Skin Pack
+addappid(4383600) --Dlcname World War Z - Chibi Bunko Skin Pack
+addappid(4383610) --Dlcname World War Z - Chibi Daniela Skin Pack
