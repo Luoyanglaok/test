8d373acd7f6423c80d45c4f06e9113ca7b77b8a8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 19:15:17 +0800

    add 107430.lua

--- 107430.lua
+++ 107430.lua
@@ -1,3 +1,6 @@
-addappid(107430)
-addappid(107431,0,"0b2041d33294e3ecbfa89c4fa307364aaa615658e4c279504dc62d582630dfbd")
-setManifestid(107431,"4167607710212522747")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 107430.lua
+--Gamename Arma X: Anniversary Edition
+addappid(107430) --Mainappid Arma X: Anniversary Edition
+addappid(107431, 1, "0b2041d33294e3ecbfa89c4fa307364aaa615658e4c279504dc62d582630dfbd") --Main Windows Depot Arma X: Anniversary Edition
+setManifestid(107431, "4167607710212522747", 0)
