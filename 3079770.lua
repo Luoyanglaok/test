c19d5c3ca204e8bc948e9b11d23ff0beba42ba6a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 06:02:04 +0800

    add 3079770.lua

---
+++ 3079770.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3079770.lua
+--Gamename Kokoro Kitchen
+addappid(3079770, 1, "a202571922f2d676afeb57e389cbea8b5147f5f3997bbff801cd132b8d539a5c") --Mainappid Kokoro Kitchen
+addappid(3079771, 1, "f9ea963cec199d9ee13f968c251bbb63788e333fba890abec68a4d11581ae754") --Main Windows Depot Kokoro Kitchen
+setManifestid(3079771, "3485109954347964423", 322820240)
+addappid(3079772, 1, "f0bfe0641b3f0c4fcf59e646d2c426003f809ee8b3142e80620def262d80eba3") --Main Macos Depot Kokoro Kitchen
+setManifestid(3079772, "3902970031570351068", 335320224)
