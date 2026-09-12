2210fe590dbceaac37be29e45ec141fccea4bbec
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:02:22 +0800

    add 400.lua

--- 400.lua
+++ 400.lua
@@ -1,21 +1,48 @@
-addappid(400)
-addappid(402,0,"1d9699c90f13a7549a97e5935b9eb152502aa664ce6944a56573c5f9a5c6d2f1")
-setManifestid(402,"7655374749121348667")
-addappid(403,0,"1349997dfe5e13cb91d43d7076e499e92da34710020fb4d3d58c385e5092c837")
-setManifestid(403,"8974907495363134881")
-addappid(404,0,"552f270a24f114f8eb9ccaad3502d0f30f8950c67fcb62b2752f1ae5bd9eb2e1")
-setManifestid(404,"1961797017312074949")
-addappid(405,0,"a727634c8c0d6f750077fb397fb5d4848b1a31f7cefcb0e3295bbbed05af1b6a")
-setManifestid(405,"453200351187428393")
-addappid(406,0,"3bfe4846e06d664c531bb61e2f35737653d9099497522b38fd15a355acd5339c")
-setManifestid(406,"8904830885129684315")
-addappid(407,0,"e9eafe69caa4384a1b69193f12c7f4dbbe65a71a76e178b988260524b2a4c3ba")
-setManifestid(407,"8242589767738537927")
-addappid(408,0,"a6254ee5cef5797432b6ae404018d028c955596cbfa5ff3fece97a9cd6534d1b")
-setManifestid(408,"8794244784218856360")
-addappid(409,0,"28cf5b133c63b6f5f1fab9d376b3ce35c7dd327505459288ae8d772a262c48f4")
-setManifestid(409,"5466869569815608374")
-addappid(410,0,"6bb84a4ff29b21f5f3e52a121c1293ad75c92ba8ad0b050fa691a9c15164a5e9")
-setManifestid(410,"106212190778449766")
-addappid(401,0,"3ed55d26ffdf7f6c5e228ec60127ddc1712f341cfb082cadb9cebbd0f64d0d7d")
-setManifestid(401,"3169084482272098670")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 400.lua
+--Gamename Portal
+addappid(400, 1, "3c3c47f502a7d112bfb98a81c469df7add3b758b225b28310b1b0a8c9b255454") --Mainappid Portal
+addappid(401, 1, "3ed55d26ffdf7f6c5e228ec60127ddc1712f341cfb082cadb9cebbd0f64d0d7d") --Main Windows Depot Portal
+setManifestid(401, "3169084482272098670", 2522547296)
+addappid(402, 1, "1d9699c90f13a7549a97e5935b9eb152502aa664ce6944a56573c5f9a5c6d2f1") --Main Windows Depot Portal
+setManifestid(402, "7655374749121348667", 64607136)
+addappid(403, 1, "1349997dfe5e13cb91d43d7076e499e92da34710020fb4d3d58c385e5092c837") --Main Macos Depot Portal
+setManifestid(403, "8974907495363134881", 33780816)
+addappid(404, 1, "552f270a24f114f8eb9ccaad3502d0f30f8950c67fcb62b2752f1ae5bd9eb2e1") --Main Linux Depot Portal
+setManifestid(404, "1961797017312074949", 51396928)
+addappid(405, 1, "a727634c8c0d6f750077fb397fb5d4848b1a31f7cefcb0e3295bbbed05af1b6a") --Main Windows Language Depot Russian Portal
+setManifestid(405, "453200351187428393", 76108208)
+addappid(406, 1, "3bfe4846e06d664c531bb61e2f35737653d9099497522b38fd15a355acd5339c") --Main Windows Language Depot Spanish Portal
+setManifestid(406, "8904830885129684315", 103692368)
+addappid(407, 1, "e9eafe69caa4384a1b69193f12c7f4dbbe65a71a76e178b988260524b2a4c3ba") --Main Windows Language Depot French Portal
+setManifestid(407, "8242589767738537927", 112107920)
+addappid(408, 1, "a6254ee5cef5797432b6ae404018d028c955596cbfa5ff3fece97a9cd6534d1b") --Main Windows Language Depot German Portal
+setManifestid(408, "8794244784218856360", 114173696)
+addappid(409, 1, "28cf5b133c63b6f5f1fab9d376b3ce35c7dd327505459288ae8d772a262c48f4") --Main Windows Depot Portal
+setManifestid(409, "5466869569815608374", 2355896560)
+addappid(410, 1, "6bb84a4ff29b21f5f3e52a121c1293ad75c92ba8ad0b050fa691a9c15164a5e9") --Main Windows Depot Portal
+setManifestid(410, "106212190778449766", 3104)
+--Dlcname Portal Soundtrack
+addappid(323170) --Dlcname Portal Soundtrack
+addappid(323171, 1, "ef5954989cfc0754ad15c4727a521500630ea5091e24da9e4247a4c30dceead9") --Dlc Windows Depot Portal Soundtrack
+setManifestid(323171, "6566049664008006432", 210016544)
+addappid(323172, 1, "361a107003b168bddbc4d14382306eecea86ad4f1606b016160fd57112b2a3a8") --Dlc Windows Depot Portal Soundtrack
+setManifestid(323172, "6846887992426215867", 97846976)
+--Dlcname Portal with RTX
+addappid(2012840, 1, "8eb4d817266ae691ab891333ae50d85eae740f523b7efe7c1de38410c4b0f2fb") --Dlcname Portal with RTX
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Dlc Windows Depot Portal with RTX
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2012841, 1, "1e1cfddfae65b4dcb58dc80af760acf0c80c793d178c7e7406637f82a5f076cb") --Dlc Windows Depot Portal with RTX
+setManifestid(2012841, "7304250410936673472", 2097828176)
+addappid(2012842, 1, "b5eb4d08602ad47b81cfb91e638702b1bfd51b94c80652c805e939fb4147f1dd") --Dlc Windows Depot Portal with RTX
+setManifestid(2012842, "1433176604007698919", 147470208)
+addappid(2012845, 1, "d7bdddc10ad83ab3d46c8403ff2a8d83688bda6ef6b3feda2ce7062ebea78b52") --Dlc Windows Language Depot Russian Portal with RTX
+setManifestid(2012845, "7675679219148045300", 71814096)
+addappid(2012846, 1, "06f05b85e3171898d6816a21a8d823640276c6cb77478681e98926017d961186") --Dlc Windows Language Depot Spanish Portal with RTX
+setManifestid(2012846, "6970396092531288017", 98171920)
+addappid(2012847, 1, "0b9fea6807eb580ba9adf0e2dcba4ea5b9a599beb6b43aa09f920116125af90c") --Dlc Windows Depot Portal with RTX
+setManifestid(2012847, "9064669986084165273", 107880560)
+addappid(2012848, 1, "7be3bd7eb9fb012c9b0f2d83a4f837e5161a9989ea7255a66d79045a35de7ada") --Dlc Windows Depot Portal with RTX
+setManifestid(2012848, "5396582583170272694", 109351744)
+addappid(2012849, 1, "f9635127d02200459f36233f31e7c2f2b55679eeb168d83c5e7824b339f2e935") --Dlc Windows Depot Portal with RTX
+setManifestid(2012849, "4618112704901940572", 12413923760)
