e73ee8e1a969bf592ac97add28506146e7806c0c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 06:48:22 +0800

    add 1272290.lua

---
+++ 1272290.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1272290.lua
+--Gamename Space Chef
+addappid(1272290) --Mainappid Space Chef
+addappid(1272291, 1, "69039d72a13cb65f3fcaf548fa3323845bedf66803eae38545bb847e1c9fb637") --Main Windows Depot Space Chef
+setManifestid(1272291, "9160841221289123451", 420414672)
+addappid(4091311, 1, "fb1599db24ce9024174611aac57991a1c47ff8d829d0c2ff2757e72aa04d3ad4") --Main Windows Depot Space Chef
+setManifestid(4091311, "2204555214869748018", 64444912)
+addappid(4091312, 1, "1f3a87ed10b73591189f45fc2504f810f39a13fd5360427f494e69fec18e8ab4") --Main Windows Depot Space Chef
+setManifestid(4091312, "6383718418237801018", 293854064)
+--Dlcname Space Chef Soundtrack
+addappid(4091310) --Dlcname Space Chef Soundtrack
