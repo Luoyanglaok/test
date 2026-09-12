c8eff47e7916da4d6db8631cfc545cc3eb836cc0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 17:18:35 +0800

    add 2212410.lua

--- 2212410.lua
+++ 2212410.lua
@@ -1,3 +1,8 @@
-addappid(2212410)
-addappid(2212413,0,"17924b6636c39f867982238d5563ba38ea8115c67defb232f5ba11b55d35ec33")
-setManifestid(2212413,"5379719614147267043")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2212410.lua
+--Gamename Shadowkin
+addappid(2212410) --Mainappid Shadowkin
+addappid(2212413, 1, "17924b6636c39f867982238d5563ba38ea8115c67defb232f5ba11b55d35ec33") --Main Windows Depot Shadowkin
+setManifestid(2212413, "5379719614147267043", 188823456)
+--Missing Dlcs
+--Dlcname Shadowkin Soundtrack
