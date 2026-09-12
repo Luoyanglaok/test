5b425a7f0cd35c4023c4d7448591b100b082424e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 15:58:01 +0800

    add 4704690.lua

--- 4704690.lua
+++ 4704690.lua
@@ -1,6 +1,11 @@
-addappid(4704690)
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(4704691,"5414522052898934799")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4704690.lua
+--Gamename MECCHA CHAMELEON
+addappid(4704690, 1, "ddf5e06be47f8470e5735390673244b191e5ab5562310db7bdbcb539a69a4900") --Mainappid MECCHA CHAMELEON
+addappid(4704691, 1, "04f85383091217f3b9735b5f34a09e0fbcbcb9afe68e33c25244d37bf45933f8") --Main Windows Depot MECCHA CHAMELEON
+setManifestid(4704691, "5414522052898934799", 3229259104)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
