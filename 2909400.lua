d2f0faed7f8745e410016846736f2b03019e8fcf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 04:42:45 +0800

    add 2909400.lua

--- 2909400.lua
+++ 2909400.lua
@@ -13,6 +13,18 @@ setManifestid(3260630, "2312058563762645934", 96)
 addappid(3399450) --Dlcname FINAL FANTASY VII REBIRTH Original Soundtrack
 addappid(3399451, 1, "77fc07cf815274bb5488f717bef898b48d14d3144441905bdccdfd0b98c7642c") --Dlc Windows Depot FINAL FANTASY VII REBIRTH Original Soundtrack
 setManifestid(3399451, "8141321127507237435", 1195580768)
+--Dlcname FINAL FANTASY VII REBIRTH Orchestral Arrangement Album
+addappid(4270120) --Dlcname FINAL FANTASY VII REBIRTH Orchestral Arrangement Album
+addappid(4270121, 1, "48057643f5c52d9966cdad44d26faa7fe7ce5659d31522f5f74028458bf01c66") --Dlc Windows Depot FINAL FANTASY VII REBIRTH Orchestral Arrangement Album
+setManifestid(4270121, "4828599390278757995", 118750896)
+--Dlcname FINAL FANTASY VII REBIRTH Orchestra World Tour LIVE 2024.09.08
+addappid(4270130) --Dlcname FINAL FANTASY VII REBIRTH Orchestra World Tour LIVE 2024.09.08
+addappid(4270131, 1, "7333cdb2a71b807a15f09cb24e2c42242c376b7e74de47c7deeeeb7208ab5a05") --Dlc Windows Depot FINAL FANTASY VII REBIRTH Orchestra World Tour LIVE 2024.09.08
+setManifestid(4270131, "2188760043812857999", 225095056)
+--Dlcname FINAL FANTASY VII REBIRTH ACOUSTIC ARRANGEMENTS
+addappid(4270150) --Dlcname FINAL FANTASY VII REBIRTH ACOUSTIC ARRANGEMENTS
+addappid(4270151, 1, "d8b0f1be79f001c761884333227ee076dd5b34170da6e1512ee1e5a80d3554cb") --Dlc Windows Depot FINAL FANTASY VII REBIRTH ACOUSTIC ARRANGEMENTS
+setManifestid(4270151, "9033807406605207280", 92904864)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
@@ -28,7 +40,4 @@ addappid(3312280) --Dlcname FINAL FANTASY VII REBIRTH Armor: Midgar Bangle Mk. I
 addappid(3361450) --Dlcname FINAL FANTASY VII REBIRTH Digital Deluxe Edition Upgrade
 addappid(3376810) --Dlcname FINAL FANTASY VII REBIRTH Accessory: Kupo Charm & Survival Set
 --Missing Dlcs
---Dlcname FINAL FANTASY VII REBIRTH Orchestral Arrangement Album
---Dlcname FINAL FANTASY VII REBIRTH Orchestra World Tour LIVE 2024.09.08
---Dlcname FINAL FANTASY VII REBIRTH ACOUSTIC ARRANGEMENTS
 --Dlcname FINAL FANTASY VII REBIRTH Original Soundtrack Plus
