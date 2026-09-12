ddd19ecbd021490da052751464c153f3390f692e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 05:50:25 +0800

    add 2464280.lua

--- 2464280.lua
+++ 2464280.lua
@@ -4,12 +4,6 @@
 addappid(2464280, 1, "b88f19c06483bf20aeee143ffdadb0e517e4d861ea8f11c7cd104a867c1578c4") --Mainappid Tormented Souls 2
 addappid(2464281, 1, "35ad79ec9649e9b4e9adf4595a0cf6a5f154af8dba7cd93afef6a346dea7002d") --Main Windows Depot Tormented Souls 2
 setManifestid(2464281, "3722878081501838183", 13956518784)
---Dlcname Tormented Souls 2 - Original Soundtrack
-addappid(4084230) --Dlcname Tormented Souls 2 - Original Soundtrack
-addappid(4084231, 1, "d7841414d7e3df962ad04c93399da740bfbd53eaad05ef939fb726013a293e54") --Dlc Windows Depot Tormented Souls 2 - Original Soundtrack
-setManifestid(4084231, "5678209043779426421", 392632928)
-addappid(4084232, 1, "60c21c3a247d76ac4389ca1152ef8e394c5d2a7fd1622f401a2fdeca4421b38b") --Dlc Windows Depot Tormented Souls 2 - Original Soundtrack
-setManifestid(4084232, "1561059259237294163", 2705265648)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
