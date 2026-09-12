1d4fe914507e3ba3cdb2d3f8bfff325577805f0f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 13:27:21 +0800

    add 3929470.lua

---
+++ 3929470.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3929470.lua
+--Gamename Warhounds
+addappid(3929470) --Mainappid Warhounds
+addappid(3929471, 1, "52f4a1effcf7e1f1c151b4134820bc29a4cd95f614a5bca7e369d33595d5ab59") --Main Windows Depot Warhounds
+setManifestid(3929471, "8028775473114099706", 30146653184)
+addappid(4962080) --Dlcname Warhounds - Action Heroes Pack
+--Missing Dlcs
+--Dlcname Warhounds Soundtrack
+--Dlcname Warhounds - Digital Art Book
