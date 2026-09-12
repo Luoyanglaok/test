32777b5cb89c0584bd45d63600d9f65cf8cec3bc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 16:17:39 +0800

    add 3583540.lua

---
+++ 3583540.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3583540.lua
+--Gamename Water Margin Scrolls
+addappid(3583540) --Mainappid Water Margin Scrolls
+addappid(3583541, 1, "1b7da874adb9bcb160bfbf7b91e12c3e04f7a2f71e4cf3831843341fa4b6eac8") --Main Windows Depot Water Margin Scrolls
+setManifestid(3583541, "1620930071245363929", 155962752)
