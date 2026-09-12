4210fbf508234264dc3a230e7d5fd72f31fda2a8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 13:24:46 +0800

    add 2054520.lua

---
+++ 2054520.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2054520.lua
+--Gamename Gawr Gura: Quest for Bread
+addappid(2054520, 1, "72e595a18103d57f003fbafad72c2d9c57bb91f99b87354d4583b77505b49035") --Mainappid Gawr Gura: Quest for Bread
+addappid(2054521, 1, "54725a9e1ba9fc369d5858d3e7c6f68cae69a47c164b8d1cb1bc2098890224e7") --Main Windows Depot Gawr Gura: Quest for Bread
+setManifestid(2054521, "7604242978084609550", 291681312)
+--Missing Dlcs
+--Dlcname Gawr Gura: Quest for Bread Demo
