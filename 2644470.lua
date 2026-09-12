a5b94186b09bbfd39b1ca95d57b04e1b5ab075d7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:56:08 +0800

    add 2644470.lua

--- 2644470.lua
+++ 2644470.lua
@@ -1,5 +1,14 @@
-addappid(2644470)
-addappid(2644471,0,"742488856b65c1f5c8c1d8c3d8b64f0f47fe89887d5308427c354d9ef5902e51")
-setManifestid(2644471,"2924012056785307079")
-addappid(2644472,0,"b53539a4f7c49575c61018fe492c2d05dbb6ca16267bc077043797ef23f427f6")
-setManifestid(2644472,"6634242654530814647")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2644470.lua
+--Gamename PICO PARK 2
+addappid(2644470, 1, "6458f0223dca0f5e2fb63b23896d04abe8b17453432cfd691a658c92de46f999") --Mainappid PICO PARK 2
+addappid(2644471, 1, "742488856b65c1f5c8c1d8c3d8b64f0f47fe89887d5308427c354d9ef5902e51") --Main Windows/Linux Depot PICO PARK 2
+setManifestid(2644471, "977062023767142031", 114132624)
+addappid(2644472, 1, "b53539a4f7c49575c61018fe492c2d05dbb6ca16267bc077043797ef23f427f6") --Main Macos Depot PICO PARK 2
+setManifestid(2644472, "880294164879156752", 137512144)
+--Dlcname PICO PARK 2 Soundtrack
+addappid(3228750) --Dlcname PICO PARK 2 Soundtrack
+addappid(3228751, 1, "1ce75bd4ec604ed2c5df384bcdc404884c1137cda40f8193de696bc2d8c064b6") --Dlc Windows Depot PICO PARK 2 Soundtrack
+setManifestid(3228751, "7521323224473043392", 13386352)
+addappid(3228752, 1, "e4aa0cc5b5bf2263966950a3d93e1b78f9622f69a106dcfe55f7652ff08d9dcc") --Dlc Windows Depot PICO PARK 2 Soundtrack
+setManifestid(3228752, "3579112180188329689", 62070320)
