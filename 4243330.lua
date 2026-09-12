7f1016913be81d927081c5ee9d22c64b72153f22
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 15:26:45 +0800

    add 4243330.lua

---
+++ 4243330.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4243330.lua
+--Gamename 全网有点怪
+addappid(4243330) --Mainappid 全网有点怪
+addappid(4243331, 1, "f4d18fd70c356e0e6bc3255aeeb3fe079f62f58e1870220ff875c209fca42918") --Main Windows Depot 全网有点怪
+setManifestid(4243331, "2675355588384096102", 2574373424)
