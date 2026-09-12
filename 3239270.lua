11fbc95e299f074c59cbe1467718a1452a4a27f7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 17:34:39 +0800

    add 3239270.lua

--- 3239270.lua
+++ 3239270.lua
@@ -1,3 +1,6 @@
-addappid(3239270)
-addappid(3239271,0,"94c30ac47ad9c694a516a45cbc89f73e179cf4456d6aec9a1c0c95b1378084b3")
-setManifestid(3239271,"229176277399209504")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3239270.lua
+--Gamename Night Shift Nurses
+addappid(3239270, 1, "bbd852f304f60b71dfb7160f3a5a357bfce3215d4bb77220edea90e1a3ae604a") --Mainappid Night Shift Nurses
+addappid(3239271, 1, "94c30ac47ad9c694a516a45cbc89f73e179cf4456d6aec9a1c0c95b1378084b3") --Main Windows Depot Night Shift Nurses
+setManifestid(3239271, "229176277399209504", 680590752)
