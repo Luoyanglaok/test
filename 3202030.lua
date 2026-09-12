786bab620736e3bd6bb6d2f0640150f4d3531b88
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 18:32:38 +0800

    add 3202030.lua

---
+++ 3202030.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3202030.lua
+--Gamename 龙胤立志传
+addappid(3202030) --Mainappid 龙胤立志传
+addappid(3202031, 1, "7f64c950b5a6bd56e9f3fb9272a80f75a04f0a36b99f540e529f467d1f3584da") --Main Windows Depot 龙胤立志传
+setManifestid(3202031, "6609390178268415730", 1107221008)
+addappid(3202032, 1, "5e3ef45e8310fefde2bcbe47277fae96fd5a6301115acc02db78f0570273588e") --Main Windows Depot 龙胤立志传
+setManifestid(3202032, "8964470425825936066", 1221516192)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4383960) --Dlcname 龙胤立志传 - 支持者包
