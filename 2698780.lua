dcf25eee3a59d9c48edcae8c7c2b2eff7b7b85fa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 15:45:51 +0800

    add 2698780.lua

--- 2698780.lua
+++ 2698780.lua
@@ -1,7 +1,20 @@
-addappid(2698780)
-setManifestid(228982,"6413394087650432851")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2698781,0,"828d5f7224a4a0900737bb184f8ebcb2f7db10894724f2ca7d7d06c9904cf1cd")
-setManifestid(2698781,"5932387771058343484")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2698780.lua
+--Gamename ContractVille
+addappid(2698780) --Mainappid ContractVille
+addappid(2698781, 1, "828d5f7224a4a0900737bb184f8ebcb2f7db10894724f2ca7d7d06c9904cf1cd") --Main Windows Depot ContractVille
+setManifestid(2698781, "5059187893259448596", 9127494000)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3969000) --Dlcname ContractVille - Medieval Furniture Pack
+addappid(4075310) --Dlcname ContractVille - Japanese Furniture Pack
+addappid(4207210) --Dlcname ContractVille - Sci-Fi Furniture Pack
+addappid(4376710) --Dlcname ContractVille - Valentine's Day Furniture Pack
+addappid(5112520) --Dlcname ContractVille - Rent Expansion
