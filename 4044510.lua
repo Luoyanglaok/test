7a624495ae24e6aeaa5fce0ee65b861075c98106
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:53:00 +0800

    add 4044510.lua

---
+++ 4044510.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4044510.lua
+--Gamename Breezy Bakes Simulator
+addappid(4044510) --Mainappid Breezy Bakes Simulator
+addappid(4044511, 1, "ae4d4d1809ff4effc5e9709263d563a73ad181c625aeb848f7bf48932b0224eb") --Main Windows Depot Breezy Bakes Simulator
+setManifestid(4044511, "7638446022592626456", 1847919520)
