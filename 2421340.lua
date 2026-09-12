63fa0d048cea9af993a2b456e6b5627b253b4455
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 02:57:52 +0800

    add 2421340.lua

---
+++ 2421340.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2421340.lua
+--Gamename 不倫婚姻審判官
+addappid(2421340, 1, "c5bfbc4fe5f4ec051bd37a53044064a2508306357a0e6580864a739d5daef39a") --Mainappid 不倫婚姻審判官
+addappid(2421341, 1, "e5ec407b5b6857bfdaedc00107fa636c88ae84f3d4745e7f39dc34ba1e943a00") --Main Windows Depot 不倫婚姻審判官
+setManifestid(2421341, "173502785099525060", 884442336)
+addappid(2421342, 1, "ada118a5745579078e08c953b9c3b9ad670b34eb9115a0040160a040b074c73c") --Main Macos Depot 不倫婚姻審判官
+setManifestid(2421342, "5824513969601833216", 664680304)
