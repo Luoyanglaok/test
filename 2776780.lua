bcd37f3ee57afce5aeb5d579f9efcd740f6be815
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 07:03:13 +0800

    add 2776780.lua

--- 2776780.lua
+++ 2776780.lua
@@ -1,5 +1,10 @@
-addappid(2776780)
-addappid(2776781,0,"afa10f600fdbcfa388448072896e37c7d362a5efbbf2f49e6e85493d0e90aabe")
-setManifestid(2776781,"777856739921686770")
-addappid(2776782,0,"e018e66dc491c66e51db146b43901006c0de6494db6cb7be71c2c8aa0098b19f")
-setManifestid(2776782,"4972220916305845385")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2776780.lua
+--Gamename Blood Bar Tycoon
+addappid(2776780) --Mainappid Blood Bar Tycoon
+addappid(2776781, 1, "afa10f600fdbcfa388448072896e37c7d362a5efbbf2f49e6e85493d0e90aabe") --Main Windows/Linux Depot Blood Bar Tycoon
+setManifestid(2776781, "8301452533099928834", 4012524992)
+addappid(2776782, 1, "e018e66dc491c66e51db146b43901006c0de6494db6cb7be71c2c8aa0098b19f") --Main Macos Depot Blood Bar Tycoon
+setManifestid(2776782, "9179996216241977472", 4039546048)
+--Missing Dlcs
+--Dlcname Blood Bar Tycoon - Supporter Pack
