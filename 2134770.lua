6b2d9a762fdcf221002c3a472f81e77c9a94539c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 05:20:05 +0800

    add 2134770.lua

--- 2134770.lua
+++ 2134770.lua
@@ -1,3 +1,7 @@
-addappid(2134770)
-addappid(2134771,0,"bbda61a192b954b2b25cfe27ca50a6a5e1cd9b6ecf41b9cd42b62d78bd2eace5")
-setManifestid(2134771,"5245599283315680962")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2134770.lua
+--Gamename SteamWorld Build
+addappid(2134770, 1, "9c6b519f49edbb50b640ec6aa75fca5bfebb1202765cba076f54b4831e9aaf2c") --Mainappid SteamWorld Build
+addappid(2134771, 1, "bbda61a192b954b2b25cfe27ca50a6a5e1cd9b6ecf41b9cd42b62d78bd2eace5") --Main Windows Depot SteamWorld Build
+setManifestid(2134771, "5245599283315680962", 1200416160)
+addappid(2744630) --Dlcname SteamWorld Build Mechanized DLC
