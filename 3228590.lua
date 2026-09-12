93ce0e5124e75d872c3cfe11efc6b50cebdd21eb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:03:43 +0800

    add 3228590.lua

--- 3228590.lua
+++ 3228590.lua
@@ -1,5 +1,19 @@
-addappid(3228590)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(3228591,0,"ff50f696309f1bdaedc819a0dfc5ad20497d9884179b19683f5ceba047094567")
-setManifestid(3228591,"7244435533429567867")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3228590.lua
+--Gamename Deadzone: Rogue
+addappid(3228590, 1, "c3c097ac79d244f9b988ee5f10b318ef649dd9e6445e6fee5266898c0ef700b0") --Mainappid Deadzone: Rogue
+addappid(3228591, 1, "ff50f696309f1bdaedc819a0dfc5ad20497d9884179b19683f5ceba047094567") --Main Windows Depot Deadzone: Rogue
+setManifestid(3228591, "7449610133526036205", 9573220672)
+--Dlcname Deadzone: Rogue - Original Soundtrack
+addappid(3883890) --Dlcname Deadzone: Rogue - Original Soundtrack
+addappid(3883891, 1, "1aa3cb53ddaf573313e911f6e1b93cdd94b20d3e1fafa57f19d262d894b8baf9") --Dlc Windows Depot Deadzone: Rogue - Original Soundtrack
+setManifestid(3883891, "7410862965380130954", 56951280)
+addappid(3883892, 1, "b3a8828b6e3bc539e968cf7842638563d5dc8c1f1385fa8eeef46d85ea1b3bd5") --Dlc Windows Depot Deadzone: Rogue - Original Soundtrack
+setManifestid(3883892, "5626925813669035130", 631999216)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3897660) --Dlcname Deadzone: Rogue - Operative Pack
+addappid(4208730) --Dlcname Deadzone: Rogue - MARK II Pack
