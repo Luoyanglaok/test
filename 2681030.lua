f59611fc0a9dc580112e578344f61d18b948cdb2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 01:35:27 +0800

    add 2681030.lua

---
+++ 2681030.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2681030.lua
+--Gamename Outbound
+addappid(2681030, 1, "212c0a7b5c03882e040badb68ae28f5fed348c96e52b78942ab003e09f13700f") --Mainappid Outbound
+addappid(2681031, 1, "feac3f781af42ac4e65cdb05e24f482c33e5660d23b377e86d744be6cc53a4bd") --Main Windows Depot Outbound
+setManifestid(2681031, "365168129000575614", 6642124448)
+addappid(4148860) --Dlcname Outbound | School Bus Adventures
