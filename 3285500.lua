86298f8e70a6f9d912f595167be96ec4ecc99d8f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 16:16:56 +0800

    add 3285500.lua

--- 3285500.lua
+++ 3285500.lua
@@ -1,5 +1,15 @@
-addappid(3285500)
-addappid(3285501,0,"d540ff5a3e564eb42dd7bc7fcd2767ba878e869bde88b3b99f21372cf43fe116")
-setManifestid(3285501,"6915443875258383764")
-addappid(3786750,0,"8ea8b93dc597304b744fb21727c260bb3f2940dde0fc68d4cce2c9c1a2cdbecf")
-setManifestid(3786750,"3374101838050016339")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3285500.lua
+--Gamename Three Kingdoms Mushouden
+addappid(3285500, 1, "a72e9be4f0a2395e9f01c8fc97a29f9ed4d8345a0604f8e66151de58ce08a588") --Mainappid Three Kingdoms Mushouden
+addappid(3285501, 1, "d540ff5a3e564eb42dd7bc7fcd2767ba878e869bde88b3b99f21372cf43fe116") --Main Windows Depot Three Kingdoms Mushouden
+setManifestid(3285501, "9129263997121899207", 3763419760)
+--Dlcname 猛将三国 数字豪华版升级包
+addappid(3786750, 1, "8ea8b93dc597304b744fb21727c260bb3f2940dde0fc68d4cce2c9c1a2cdbecf") --Dlcname 猛将三国 数字豪华版升级包
+setManifestid(3786750, "2102585071659237461", 8835296)
+--Dlcname Three Kingdoms Mushouden - Mushouden GUANYU
+addappid(4243340, 1, "57752722713cf916021e2f04f6b6defc1e7f5aa24f70b51a6ac22f976c87b40a") --Dlcname Three Kingdoms Mushouden - Mushouden GUANYU
+setManifestid(4243340, "7585080945568092154", 439269408)
+--Dlcname Three Kingdoms Mushouden - Mushouden LIAO
+addappid(4243370, 1, "ba963f04f7a4ba05544c87cae97abf780b1406d2a7e4906183e04a17e5d0ad1e") --Dlcname Three Kingdoms Mushouden - Mushouden LIAO
+setManifestid(4243370, "8251698704868364907", 435922240)
