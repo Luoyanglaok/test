ccda4a89b984b81e9aee366b1fc0f2f012b37819
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 07:16:19 +0800

    add 3755320.lua

---
+++ 3755320.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3755320.lua
+--Gamename BakeryDiary
+addappid(3755320) --Mainappid BakeryDiary
+addappid(3755328, 1, "35ea6296aaea8d125c82702bb9da38c6dbb6ffeffbb28c85bdd9d7b2860439ca") --Main Windows Depot BakeryDiary
+setManifestid(3755328, "2638779978532340352", 138400112)
+addappid(3755329, 1, "7869ab30fea9ae95947be156c91fb23ac06b73e1261ff46acdd00192b91a88f1") --Main Windows Depot BakeryDiary
+setManifestid(3755329, "3262294646520287311", 90272288)
