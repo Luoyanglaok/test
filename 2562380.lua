e4edadc0e18462ca6c51ac5f651de6142356a253
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:14:15 +0800

    add 2562380.lua

---
+++ 2562380.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2562380.lua
+--Gamename ENFYNYTY
+addappid(2562380) --Mainappid ENFYNYTY
+addappid(2562388, 1, "f4b84e6f48eab528a405a300718fcdd9ddc77f51e6c572df97ae1db24bca384f") --Main Windows Depot ENFYNYTY
+setManifestid(2562388, "4365007008215829886", 1472631856)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
