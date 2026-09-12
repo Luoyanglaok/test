cde4508c862c72c5df1fe9617715ebf2a9a968f9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 19:21:50 +0800

    add 3484850.lua

---
+++ 3484850.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3484850.lua
+--Gamename Scam Center Simulator: UnderKingdom
+addappid(3484850, 1, "f74b0087c302de52d0633b201e82664acc68565bb45a7e2d5ace6fc53a5c5d43") --Mainappid Scam Center Simulator: UnderKingdom
+addappid(3484851, 1, "d3f006a17610929854d256761a03e2ed2f17d6b0cd5f555cc91d7c8e0a07d095") --Main Windows Depot Scam Center Simulator: UnderKingdom
+setManifestid(3484851, "4655502004762065324", 2953711024)
