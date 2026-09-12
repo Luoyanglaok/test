eb43c831834e372881a1aed2a3384e4e84150e93
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 01:09:57 +0800

    add 2753900.lua

--- 2753900.lua
+++ 2753900.lua
@@ -1,5 +1,12 @@
-addappid(2753900)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2753901,0,"416d49c23e9d45fbd519129ae945ff52e8e57b2e8a0170688a8623c6ca0d3941")
-setManifestid(2753901,"117752305295599952")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2753900.lua
+--Gamename The King is Watching
+addappid(2753900, 1, "9c508ce17fc01c7952b90ead8e86425ba705f9f8c34bfe2077230b58c1b6062e") --Mainappid The King is Watching
+addappid(2753901, 1, "416d49c23e9d45fbd519129ae945ff52e8e57b2e8a0170688a8623c6ca0d3941") --Main Windows Depot The King is Watching
+setManifestid(2753901, "4723183641092796383", 496987040)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3951490) --Dlcname The King is Watching: Crowns of History
