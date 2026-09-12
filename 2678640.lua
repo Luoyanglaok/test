494099cd89e4ab978c21e4c8fd24082347728302
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:05:09 +0800

    add 2678640.lua

---
+++ 2678640.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2678640.lua
+--Gamename 朝露：境界旅程
+addappid(2678640) --Mainappid 朝露：境界旅程
+addappid(2678641, 1, "2d51e5c831cd6d9b124f42a99cfffbfeb80b49d8a4515e7e06a626abe1db0ae7") --Main Windows Depot 朝露：境界旅程
+setManifestid(2678641, "1232835973936137667", 1005546512)
+addappid(4214831, 1, "22e1c8f12827f4128325b5dea8616a76a8456c8a1277def227cea2ad0acb06ba") --Main Windows Depot 朝露：境界旅程
+setManifestid(4214831, "7719419167399058808", 1052500272)
+--Dlcname Dawn Dew - Realm Heart
+addappid(4214830) --Dlcname Dawn Dew - Realm Heart
