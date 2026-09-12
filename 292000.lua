1395ba8923bfaa41c109e9960460d0dcbbc8b4e9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 08:24:54 +0800

    add 292000.lua

--- 292000.lua
+++ 292000.lua
@@ -1,5 +1,15 @@
-addappid(292000)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(292001,0,"2bb3dfcc7245e9a85e6adcb82c91257dfd6e3ca66bdc188476820020395cc812")
-setManifestid(292001,"8535289037092316471")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 292000.lua
+--Gamename No More Room in Hell 2
+addappid(292000, 1, "4a7cdda160ead514fee5cc6330491cea03bcfd7a56ff3696ccbf51568cf18069") --Mainappid No More Room in Hell 2
+addappid(292001, 1, "2bb3dfcc7245e9a85e6adcb82c91257dfd6e3ca66bdc188476820020395cc812") --Main Windows Depot No More Room in Hell 2
+setManifestid(292001, "5742437652247501692", 21153429504)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2089150) --Dlcname No More Room in Hell 2 - Veteran Survivor Pack
+addappid(4250540) --Dlcname No More Room in Hell 2 - First Responder Pack
+addappid(4250550) --Dlcname No More Room in Hell 2 - Punk is Life Pack
+addappid(4250630) --Dlcname No More Room in Hell 2 - Surplus Officer Pack
