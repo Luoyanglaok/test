8e27cee0417cba349f41f370e56a21cd4db0e4aa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:48:06 +0800

    add 1044620.lua

--- 1044620.lua
+++ 1044620.lua
@@ -1,13 +1,29 @@
-addappid(1044620)
-addappid(1044621,0,"e4de2e5ad93821687c7cc71aec870246915e61b66bec007091abb381da5454a1")
-setManifestid(1044621,"1124321592740122839")
-addappid(1162110,0,"75cada30e8bfa481e7d923acc24e1593c5e1cf3f4df79ec9b81690af8f97a670")
-setManifestid(1162110,"5804530054537620033")
-addappid(1219130,0,"7b39c1cbdc7abf346f2879d68ddf26e62080a42daf5ca6da87d0889ef27093c8")
-setManifestid(1219130,"3496350920010610244")
-addappid(1219150,0,"205e9eaa30e63e10a340ad5057e4dba01087016109bb4065215bfb62b473a6dc")
-setManifestid(1219150,"1624156722750849597")
-addappid(1966540,0,"9d67124c756a85a8c6115c471b6694bec604df1ccfa9ea8ecc44744daacfc25d")
-setManifestid(1966540,"5093831643860917657")
-addappid(1966541,0,"2c2d64c11ec16a8d7f980ced69d635c558e1fc79f0b9f66df3f7a41fc79a182f")
-setManifestid(1966541,"6849251102774804024")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1044620.lua
+--Gamename Aokana - Four Rhythms Across the Blue
+addappid(1044620) --Mainappid Aokana - Four Rhythms Across the Blue
+addappid(1044621, 1, "e4de2e5ad93821687c7cc71aec870246915e61b66bec007091abb381da5454a1") --Main Windows Depot Aokana - Four Rhythms Across the Blue
+setManifestid(1044621, "1124321592740122839", 9766039424)
+--Dlcname Aokana - Four Rhythms Across the Blue Soundtrack
+addappid(1162110, 1, "75cada30e8bfa481e7d923acc24e1593c5e1cf3f4df79ec9b81690af8f97a670") --Dlcname Aokana - Four Rhythms Across the Blue Soundtrack
+setManifestid(1162110, "5804530054537620033", 1933728944)
+--Dlcname Aokana - Drama CD 1
+addappid(1219130, 1, "7b39c1cbdc7abf346f2879d68ddf26e62080a42daf5ca6da87d0889ef27093c8") --Dlcname Aokana - Drama CD 1
+setManifestid(1219130, "3496350920010610244", 780268096)
+--Dlcname Aokana - Drama CD Vol 2
+addappid(1219150, 1, "205e9eaa30e63e10a340ad5057e4dba01087016109bb4065215bfb62b473a6dc") --Dlcname Aokana - Drama CD Vol 2
+setManifestid(1219150, "1624156722750849597", 728897696)
+--Dlcname Aokana - Drama CD Vol 3
+addappid(1966540, 1, "9d67124c756a85a8c6115c471b6694bec604df1ccfa9ea8ecc44744daacfc25d") --Dlcname Aokana - Drama CD Vol 3
+setManifestid(1966540, "5093831643860917657", 558170448)
+--Dlcname Aokana - Drama CD Vol 4
+addappid(1966541, 1, "2c2d64c11ec16a8d7f980ced69d635c558e1fc79f0b9f66df3f7a41fc79a182f") --Dlcname Aokana - Drama CD Vol 4
+setManifestid(1966541, "6849251102774804024", 439908592)
+--Dlcname Aokana - Four Rhythms Across the Blue Piano Album#1
+addappid(2022800) --Dlcname Aokana - Four Rhythms Across the Blue Piano Album#1
+addappid(2022801, 1, "d6ce053fcf077b0d563bfbfb0097cf11a0381bb280d0a5af8bdb16ebb27ed9d1") --Dlc Windows Depot Aokana - Four Rhythms Across the Blue Piano Album#1
+setManifestid(2022801, "3395569265615405461", 100115872)
+addappid(2022802, 1, "83d392ef0b19831bdf81e01e5503a71eb897d2be8d3b698a89dc43cc39fd0506") --Dlc Windows Depot Aokana - Four Rhythms Across the Blue Piano Album#1
+setManifestid(2022802, "288261122734015286", 660804112)
+--Missing Dlcs
+--Dlcname Aokana - Four Rhythms Across the Blue Piano Album#2
