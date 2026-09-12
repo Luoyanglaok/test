cc9a30f84256883d59a9df92d9f79089b63ada85
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 02:32:22 +0800

    add 2451100.lua

---
+++ 2451100.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2451100.lua
+--Gamename Little Rocket Lab
+addappid(2451100, 1, "f19acd3a1dd472c06294e4ac24dd8285ca1ac100381ed8261ddfb934b538d981") --Mainappid Little Rocket Lab
+addappid(2451101, 1, "3817c5f556bafe1583c04eda47709f40345c13b9f42096ff4bf67378421144ff") --Main Windows Depot Little Rocket Lab
+setManifestid(2451101, "6539731065218576192", 1091866848)
+--Dlcname Little Rocket Lab Soundtrack
+addappid(4007960) --Dlcname Little Rocket Lab Soundtrack
+addappid(4007961, 1, "46dd4d4b6eb870ab116ffdbe16896f028864f94f8b9be9004227105abccbb3d7") --Dlc Windows Depot Little Rocket Lab Soundtrack
+setManifestid(4007961, "1481014849668157100", 350841984)
+addappid(4007962, 1, "c74411a0b4c03f51120627a8070ec49726c4689656490f0dfc2910493ed88e0c") --Dlc Windows Depot Little Rocket Lab Soundtrack
+setManifestid(4007962, "8068533640721927691", 1479974016)
