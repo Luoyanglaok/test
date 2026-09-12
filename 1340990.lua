3518857207afcc270badbd4d07c5d4198499d1c3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:21:22 +0800

    add 1340990.lua

--- 1340990.lua
+++ 1340990.lua
@@ -1,20 +1,26 @@
-addappid(1340990)
-setManifestid(228989,"3514306556860204959")
-addappid(1340995,0,"e57b64d9b5f9e584a87a9832529c0e1f901f699e690b4b5637fec2b2e1fbf2aa")
-setManifestid(1340995,"1876216873872086392")
-addappid(1340996,0,"2aca517d8dece95e0cbfce99e8e6b937dd56dee06fc84b510bcfe0fa09538d9d")
-setManifestid(1340996,"3841686798722160883")
-addappid(1340997,0,"37138487eef65143ea189a388085037e84de8519cef8b35cf5486474b5df15d4")
-setManifestid(1340997,"2331755264652654819")
-addappid(1340991,0,"6fdac6ccd9ebadf5c61e1c34ec225a8bc687cab591d28cc5ade262e9a65ae393")
-setManifestid(1340991,"5032043067359059522")
-addappid(1340992,0,"7c7e86cec8a1f874213799a3eb2db858af7a8e28c72d7792330cc7b28f580793")
-setManifestid(1340992,"8097777051056268960")
-addappid(1340993,0,"50234d9ac0cda0a23d385d3c086560656cc037de4dcb3d743e960f416ed5c69d")
-setManifestid(1340993,"9075053198374038770")
-addappid(1340994,0,"747729b8ffcd2e18673252e55284cc050dbecbff88ab7ef91b69cd71d941084f")
-setManifestid(1340994,"7767196483143147196")
-addappid(1340998,0,"7e685022588dcde7a3b5ee6627e0a250e8641a8bbd123bf7377b2e9e990c42fe")
-setManifestid(1340998,"8952393605010619144")
-addappid(1340999,0,"bb4b2b9bca194610993a7f32ee52f074460b0d0b54fb37099580e2d512e35728")
-setManifestid(1340999,"9058506243337526886")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1340990.lua
+--Gamename Rise of the Ronin
+addappid(1340990, 1, "30901833789edbe371383f32587d0b87174bf816c2c46c237d26adafab5a2d2a") --Mainappid Rise of the Ronin
+addappid(1340991, 1, "6fdac6ccd9ebadf5c61e1c34ec225a8bc687cab591d28cc5ade262e9a65ae393") --Main Windows Language Depot French Rise of the Ronin
+setManifestid(1340991, "5032043067359059522", 2438227680)
+addappid(1340992, 1, "7c7e86cec8a1f874213799a3eb2db858af7a8e28c72d7792330cc7b28f580793") --Main Windows Language Depot German Rise of the Ronin
+setManifestid(1340992, "8097777051056268960", 2390945920)
+addappid(1340993, 1, "50234d9ac0cda0a23d385d3c086560656cc037de4dcb3d743e960f416ed5c69d") --Main Windows Language Depot Italian Rise of the Ronin
+setManifestid(1340993, "9075053198374038770", 2510570992)
+addappid(1340994, 1, "747729b8ffcd2e18673252e55284cc050dbecbff88ab7ef91b69cd71d941084f") --Main Windows Language Depot Spanish Rise of the Ronin
+setManifestid(1340994, "7767196483143147196", 2437882688)
+addappid(1340995, 1, "e57b64d9b5f9e584a87a9832529c0e1f901f699e690b4b5637fec2b2e1fbf2aa") --Main Windows Depot Rise of the Ronin
+setManifestid(1340995, "943949358035404765", 97508684320)
+addappid(1340996, 1, "2aca517d8dece95e0cbfce99e8e6b937dd56dee06fc84b510bcfe0fa09538d9d") --Main Windows Depot Rise of the Ronin
+setManifestid(1340996, "3841686798722160883", 11549241312)
+addappid(1340997, 1, "37138487eef65143ea189a388085037e84de8519cef8b35cf5486474b5df15d4") --Main Windows Depot Rise of the Ronin
+setManifestid(1340997, "2178159606363813413", 202325440)
+addappid(1340998, 1, "7e685022588dcde7a3b5ee6627e0a250e8641a8bbd123bf7377b2e9e990c42fe") --Main Windows Language Depot Brazilian Rise of the Ronin
+setManifestid(1340998, "8952393605010619144", 2296661920)
+addappid(1340999, 1, "bb4b2b9bca194610993a7f32ee52f074460b0d0b54fb37099580e2d512e35728") --Main Windows Language Depot Latam Rise of the Ronin
+setManifestid(1340999, "9058506243337526886", 2290559664)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3355660) --Dlcname Rise of the Ronin - Early Purchase Bonus
