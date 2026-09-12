eaa2b0140456b47f1aefce1a9515633af64583d5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:12:52 +0800

    add 3264850.lua

---
+++ 3264850.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3264850.lua
+--Gamename That Time I Got Reincarnated as a Demon Hand
+addappid(3264850, 1, "e36b1e605f0cefe7c78e93e3b72db5ede3e41c4896a9bdf8de78a52ee28c55b3") --Mainappid That Time I Got Reincarnated as a Demon Hand
+addappid(3264851, 1, "0c3939bfeb0779a83002b99b9dbafae53d694dbc5966fdf6c07a90b54372f604") --Main Windows Depot That Time I Got Reincarnated as a Demon Hand
+setManifestid(3264851, "7043431320971278751", 1819672272)
