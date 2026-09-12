28259cf338cf2d5299dbdff319e1db9cb397038b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 02:26:50 +0800

    add 1158160.lua

--- 1158160.lua
+++ 1158160.lua
@@ -1,11 +1,28 @@
-addappid(1158160)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1158161,0,"e9303b2124c42a0c9cfc882e92469d8ceb392056ec283045ac876c31df4f5e65")
-setManifestid(1158161,"2516381922549577623")
-addappid(2273380,0,"f2946bf1df6964993d9597e6f2ac42e9aca3ef22d2f7c7145c3823b241966bf2")
-setManifestid(2273380,"8577500605315709614")
-addappid(2471180,0,"61aa13efb38e3f9ffaf22c018bde687a97b95b559d21406a6bbc47da860be1ab")
-setManifestid(2471180,"1643256457515370317")
-addappid(2537800,0,"2c18bb7df0d4f51d0d81f95eefaa22e3966f628e7f4defbc9d7d1978476a8b0b")
-setManifestid(2537800,"9022386239993204092")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1158160.lua
+--Gamename Coral Island
+addappid(1158160, 1, "52bf499bc7b8b8e9ac5da6b84aa5e4447bd57fd287e6a634ea6177a3a3439701") --Mainappid Coral Island
+addappid(1158161, 1, "e9303b2124c42a0c9cfc882e92469d8ceb392056ec283045ac876c31df4f5e65") --Main Windows Depot Coral Island
+setManifestid(1158161, "7332034312925916231", 13296755152)
+--Dlcname Kickstarter Exclusive "Farmer Outfit" Set
+addappid(2273380, 1, "f2946bf1df6964993d9597e6f2ac42e9aca3ef22d2f7c7145c3823b241966bf2") --Dlcname Kickstarter Exclusive "Farmer Outfit" Set
+setManifestid(2273380, "8577500605315709614", 64)
+--Dlcname Coral Island Soundtrack
+addappid(2331830) --Dlcname Coral Island Soundtrack
+addappid(2331831, 1, "b714d5edaab8aba71bc4b81af48e8ec301dfea16ee0e9bb587cc61dd9277b4e0") --Dlc Windows Depot Coral Island Soundtrack
+setManifestid(2331831, "6831515917586636038", 1127323632)
+addappid(2331832, 1, "f9939100b45ae905fa17be35e850cf2682de424fe4b0ec44b6c7c0d535c3e458") --Dlc Windows Depot Coral Island Soundtrack
+setManifestid(2331832, "6574698724966683545", 4950649280)
+addappid(2471180, 1, "61aa13efb38e3f9ffaf22c018bde687a97b95b559d21406a6bbc47da860be1ab") --Dlcname AppID 2471180
+setManifestid(2471180, "1643256457515370317", 64)
+addappid(2537800, 1, "2c18bb7df0d4f51d0d81f95eefaa22e3966f628e7f4defbc9d7d1978476a8b0b") --Dlcname AppID 2537800
+setManifestid(2537800, "9022386239993204092", 64)
+--Dlcname Kickstarter Exclusive "Mythical Pet"
+addappid(2626420, 1, "8ace641f5b3a2f94eb13ef67916c370cf42e520b76a567c72413c3788fd02a3f") --Dlcname Kickstarter Exclusive "Mythical Pet"
+setManifestid(2626420, "6493947519583149268", 64)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2561700) --Dlcname AppID 2561700
