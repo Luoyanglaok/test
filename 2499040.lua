c13cf8a13876eb554f0aea041119dd0ef7e18e6b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:10:45 +0800

    add 2499040.lua

---
+++ 2499040.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2499040.lua
+--Gamename Volkolak: The Will of Gods
+addappid(2499040, 1, "485103cd9421ac7965f06ef6072a740fd7d72a56ca43e9c1096a433d65869c6f") --Mainappid Volkolak: The Will of Gods
+addappid(2499041, 1, "17f83c8b8f6a55c15d2257a1cbc51986fe6454bd77c2a8c9ac392306afdcd9c8") --Main Windows Depot Volkolak: The Will of Gods
+setManifestid(2499041, "7168142011465007463", 1518386496)
+--Missing Dlcs
+--Dlcname Volkolak: The Will of Gods Soundtrack
