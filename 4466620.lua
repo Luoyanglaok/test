1956a9db1aa54ed2e80deac6663cbb91ba1a3bae
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 23:55:06 +0800

    add 4466620.lua

---
+++ 4466620.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4466620.lua
+--Gamename SiNiSistar 2
+addappid(4466620, 1, "de887f3565af8aa5f3b2cfbf3cecc87912c521ae8e8bd441eafd7d8693715564") --Mainappid SiNiSistar 2
+addappid(4466621, 1, "240a8767c6fcb561914f7c4495d7e5a9bf4dc0a2793a40d75351764b1eafa2f6") --Main Windows Depot SiNiSistar 2
+setManifestid(4466621, "4073389734174195398", 1821768416)
+addappid(4466622, 1, "a1afb0bb838f24c8e1b9dd2d8a242c5f9f60f6c3ada589ff1020e8542b06a2d7") --Main Macos Depot SiNiSistar 2
+setManifestid(4466622, "7121909639395693581", 1988024496)
