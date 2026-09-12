19dd29651d162a32b1bead0b58814ca928235858
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 05:25:29 +0800

    add 1466060.lua

--- 1466060.lua
+++ 1466060.lua
@@ -1,5 +1,17 @@
-addappid(1466060)
-addappid(1466062,0,"b164844c9d1dcef4202a397a747eff0fc24b75b5553e21bebbd7e86042d94bca")
-setManifestid(1466062,"455114081535859025")
-addappid(3687050,0,"8a5843ffcee408b2a16680531955ed57dd20d577be56ebb42d7c99dd653a3b2e")
-setManifestid(3687050,"3877787421677591921")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1466060.lua
+--Gamename Tainted Grail: The Fall of Avalon
+addappid(1466060, 1, "f53acb7f143a62f46c302975a2d5cdb7d58fb41a5a7d9a0a03f722a39493e160") --Mainappid Tainted Grail: The Fall of Avalon
+addappid(1466062, 1, "b164844c9d1dcef4202a397a747eff0fc24b75b5553e21bebbd7e86042d94bca") --Main Windows Depot Tainted Grail: The Fall of Avalon
+setManifestid(1466062, "7923749585628793804", 23429870656)
+--Dlcname Tainted Grail: The Fall of Avalon - Supporters Pack
+addappid(3687050, 1, "8a5843ffcee408b2a16680531955ed57dd20d577be56ebb42d7c99dd653a3b2e") --Dlcname Tainted Grail: The Fall of Avalon - Supporters Pack
+setManifestid(3687050, "3877787421677591921", 746529104)
+addappid(3681950) --Dlcname Tainted Grail: The Fall of Avalon - Horse Armor DLC
+addappid(4132150) --Dlcname Tainted Grail: The Fall of Avalon - Sanctuary of Sarras DLC
+addappid(4183140) --Dlcname Tainted Grail: The Fall of Avalon - Bonus Content #1
+addappid(4492250) --Dlcname Tainted Grail: The Fall of Avalon - Bonus Content #2
+addappid(4514980) --Dlcname Tainted Grail: The Fall of Avalon - Merlin's Tomb DLC
+addappid(4639800) --Dlcname Tainted Grail: The Fall of Avalon - Challenge Mode DLC
+addappid(4704460) --Dlcname Tainted Grail: The Fall of Avalon - Bonus Content #3: Anniversary
+addappid(4831420) --Dlcname Tainted Grail: The Fall of Avalon - Bonus Content #4
