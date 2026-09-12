0768f369f32f975b9d74ff5859e1e4b57cf236ef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 03:13:39 +0800

    add 1121640.lua

--- 1121640.lua
+++ 1121640.lua
@@ -1,11 +1,23 @@
-addappid(1121640)
-addappid(1121641,0,"589062032126cb115e44bf22b1f1a7e84ac07202db9d7e297972cc12130c256b")
-setManifestid(1121641,"1451792186089875823")
-addappid(1121642,0,"bbb6ebc0721460dbc0155d9b26c55231535405d93f38fb6e50872d0803db0fed")
-setManifestid(1121642,"4995161417299647411")
-addappid(1121643,0,"89b00047847f1d8ba2672409d955a16b87f2fba17d5104e7d52f2eb93b0ef7d2")
-setManifestid(1121643,"7472532716004145644")
-addappid(1121644,0,"bdd41c80e89da770cf45077f31fabce93992c2a99fde9378afdc725375bdee4e")
-setManifestid(1121644,"8463665611685739828")
-addappid(2108240,0,"3bab56d802407d29977f89e57f59dbddd3fbdffb45f22df1832cf0e363bb7a13")
-setManifestid(2108240,"1599426698477333693")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1121640.lua
+--Gamename The Wandering Village
+addappid(1121640) --Mainappid The Wandering Village
+addappid(1121641, 1, "589062032126cb115e44bf22b1f1a7e84ac07202db9d7e297972cc12130c256b") --Main Windows Depot The Wandering Village
+setManifestid(1121641, "8713801311289334407", 778634144)
+addappid(1121642, 1, "bbb6ebc0721460dbc0155d9b26c55231535405d93f38fb6e50872d0803db0fed") --Main Macos Depot The Wandering Village
+setManifestid(1121642, "4215698338820371729", 785947024)
+addappid(1121643, 1, "89b00047847f1d8ba2672409d955a16b87f2fba17d5104e7d52f2eb93b0ef7d2") --Main Windows Depot The Wandering Village
+setManifestid(1121643, "4064919891605506965", 773901920)
+addappid(1121644, 1, "bdd41c80e89da770cf45077f31fabce93992c2a99fde9378afdc725375bdee4e") --Main Linux Depot The Wandering Village
+setManifestid(1121644, "536215934951096141", 779385040)
+--Dlcname The Wandering Village: Artbook, Wallpapers and Goodies Pack
+addappid(2108240, 1, "3bab56d802407d29977f89e57f59dbddd3fbdffb45f22df1832cf0e363bb7a13") --Dlcname The Wandering Village: Artbook, Wallpapers and Goodies Pack
+setManifestid(2108240, "1599426698477333693", 215118672)
+--Dlcname The Wandering Village Soundtrack
+addappid(2108250) --Dlcname The Wandering Village Soundtrack
+addappid(2108251, 1, "d6fb0ed010c06dfdca387061f1bb4892e0482bf705aee95f2c71ed9d70eb2eab") --Dlc Windows Depot The Wandering Village Soundtrack
+setManifestid(2108251, "398426517631679072", 169414160)
+addappid(2108252, 1, "ee894065f752a31854dd17794a896892c26017992ce7610165edeaf3adf6a338") --Dlc Windows Depot The Wandering Village Soundtrack
+setManifestid(2108252, "6558190687885558422", 1000924000)
+addappid(4162120) --Dlcname The Wandering Village: Onbu Skin Pack
+addappid(4498320) --Dlcname The Wandering Village: Leviathan of the Seas
