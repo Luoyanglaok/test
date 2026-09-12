03bed81b502a648b0ae42f0cd4cf473f886d2e95
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 08:18:47 +0800

    add 2479810.lua

--- 2479810.lua
+++ 2479810.lua
@@ -1,5 +1,14 @@
-addappid(2479810)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2479811,0,"c89db92b0e186d8c1e9db781270fc739b23f152b3e11ed7733f93bba19ce6716")
-setManifestid(2479811,"4491652062652053115")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2479810.lua
+--Gamename Gray Zone Warfare
+addappid(2479810, 1, "49ada9d51358b95a5416b3f17c6c2ead1b8171c2fe362c9266ed1489ce6a0576") --Mainappid Gray Zone Warfare
+addappid(2479811, 1, "c89db92b0e186d8c1e9db781270fc739b23f152b3e11ed7733f93bba19ce6716") --Main Windows Depot Gray Zone Warfare
+setManifestid(2479811, "1773837463786738408", 20415624368)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2633470) --Dlcname Gray Zone Warfare - Tactical Edition
+addappid(2914230) --Dlcname Gray Zone Warfare - Elite Edition
+addappid(2955580) --Dlcname Gray Zone Warfare - Supporter Edition
