91e801e424437f3fe2b6607585f13081dfdb47b0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 06:40:11 +0800

    add 1637400.lua

--- 1637400.lua
+++ 1637400.lua
@@ -1,5 +1,11 @@
-addappid(1637400)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1637401,0,"ea14270e5247cec7afc3693c00273dffde99fc4c6da67730618e043be3d696fa")
-setManifestid(1637401,"9060627398430202775")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1637400.lua
+--Gamename Let's Cook Together 2
+addappid(1637400) --Mainappid Let's Cook Together 2
+addappid(1637401, 1, "ea14270e5247cec7afc3693c00273dffde99fc4c6da67730618e043be3d696fa") --Main Windows Depot Let's Cook Together 2
+setManifestid(1637401, "9060627398430202775", 1255821744)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
