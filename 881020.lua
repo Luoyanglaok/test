3b4977f76a438f3ebc39365f4752491af01f2a9a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 17:23:23 +0800

    add 881020.lua

--- 881020.lua
+++ 881020.lua
@@ -1,3 +1,45 @@
-addappid(881020)
-addappid(881021,0,"87a89e2a3e05613b1fa823d2a8d3796cb8e38972563f929ba9bee212ece2a40f")
-setManifestid(881021,"96435969733811286")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 881020.lua
+--Gamename Granblue Fantasy: Relink
+addappid(881020, 1, "e2a0cc7ae9b6f90364955f1c48562defddb892aa14c44328482a925d4054ad3a") --Mainappid Granblue Fantasy: Relink
+addappid(881021, 1, "87a89e2a3e05613b1fa823d2a8d3796cb8e38972563f929ba9bee212ece2a40f") --Main Windows Depot Granblue Fantasy: Relink
+setManifestid(881021, "938976331214223510", 94124411008)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2183570) --Dlcname Granblue Fantasy: Relink - Color Pack 1
+addappid(2694640) --Dlcname Granblue Fantasy: Relink - Color Pack 2
+addappid(2694650) --Dlcname Granblue Fantasy: Relink - Color Pack 3
+addappid(2694660) --Dlcname Granblue Fantasy: Relink - Starter Items Pack
+addappid(2694670) --Dlcname Granblue Fantasy: Relink - Sword of the False Apocalypse
+addappid(2710000) --Dlcname Granblue Fantasy: Relink - DayOneDLC
+addappid(2782160) --Dlcname Granblue Fantasy: Relink - Emote Expansion Set: Let's Chat
+addappid(2782170) --Dlcname Granblue Fantasy: Relink - Self-Improvement Pack 1
+addappid(2782180) --Dlcname Granblue Fantasy: Relink - Sigil Upgrade Items Pack 1
+addappid(2782190) --Dlcname Granblue Fantasy: Relink - Weapon Uncap Items Pack 1
+addappid(2782200) --Dlcname Granblue Fantasy: Relink - Weapon Upgrade Items Pack 1
+addappid(2799840) --Dlcname Granblue Fantasy: Relink - Color Pack 1/2/3
+addappid(2799850) --Dlcname Granblue Fantasy: Relink - Upgrade pack
+addappid(2853950) --Dlcname Granblue Fantasy: Relink - Character Expansion Set: Seofon
+addappid(2853960) --Dlcname Granblue Fantasy: Relink - Character Expansion Set: Tweyen
+addappid(2853970) --Dlcname Granblue Fantasy: Relink - Color Pack 4
+addappid(2853980) --Dlcname Granblue Fantasy: Relink - Emote Expansion Set: I Work Out
+addappid(2853990) --Dlcname Granblue Fantasy: Relink - Self-Improvement Pack 2
+addappid(2854000) --Dlcname Granblue Fantasy: Relink - Weapon Uncap Items Pack 2
+addappid(2854010) --Dlcname Granblue Fantasy: Relink - Weapon Upgrade Items Pack 2
+addappid(2854020) --Dlcname Granblue Fantasy: Relink - Sigil Upgrade Items Pack 2
+addappid(2944840) --Dlcname Granblue Fantasy: Relink - Character Expansion Set: Sandalphon
+addappid(2944850) --Dlcname Granblue Fantasy: Relink - Color Pack 5
+addappid(2944860) --Dlcname Granblue Fantasy: Relink - Emote Expansion Set: Grab Bag
+addappid(2944870) --Dlcname Granblue Fantasy: Relink - Self-Improvement Pack 3
+addappid(2944880) --Dlcname Granblue Fantasy: Relink - Weapon Uncap Items Pack 3
+addappid(2944890) --Dlcname Granblue Fantasy: Relink - Weapon Upgrade Items Pack 3
+addappid(2944900) --Dlcname Granblue Fantasy: Relink - Sigil Upgrade Items Pack 3
+addappid(3839790) --Dlcname Granblue Fantasy: Relink - Endless Ragnarok Upgrade Kit (Standard Edition)
+addappid(4306890) --Dlcname Granblue Fantasy: Relink - Endless Ragnarok Upgrade Kit (Special Edition)
+addappid(4528180) --Dlcname Granblue Fantasy: Relink - Endless Ragnarok Guts III & Autorevive III Sigil Set
+addappid(4528190) --Dlcname Granblue Fantasy: Relink - Endless Ragnarok Supplementary Damage V & War Elemental Sigil Set
+addappid(4528200) --Dlcname Granblue Fantasy: Relink - Endless Ragnarok Extra Items Pack
+addappid(4528540) --Dlcname Granblue Fantasy: Relink - Endless Ragnarok Special Pack
+addappid(4579010) --Dlcname Granblue Fantasy: Relink - Endless Ragnarok Character Expansion Set: Fraux
+addappid(4579020) --Dlcname Granblue Fantasy: Relink - Endless Ragnarok Character Expansion Set: Fediel
