cf95c8f883d8a8028ca698b10cd4355a98c57e66
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:03:35 +0800

    add 1080020.lua

--- 1080020.lua
+++ 1080020.lua
@@ -1,5 +1,9 @@
-addappid(1080020)
-addappid(1080021,0,"2a785b96ae4d7f957ce7987495ffe9cec7ff7a456600763ed68b6bd0ed111dad")
-setManifestid(1080021,"6542913283129182451")
-addappid(2329870,0,"50c259e5299223757808b3956cdeda82fb3d3c49cb6e0ea21e1f6ffda10e42d7")
-setManifestid(2329870,"6944148590576550617")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1080020.lua
+--Gamename Electrician Simulator
+addappid(1080020) --Mainappid Electrician Simulator
+addappid(1080021, 1, "2a785b96ae4d7f957ce7987495ffe9cec7ff7a456600763ed68b6bd0ed111dad") --Main Windows Depot Electrician Simulator
+setManifestid(1080021, "6542913283129182451", 6918912240)
+--Dlcname Electrician Simulator - Smart Devices
+addappid(2329870, 1, "50c259e5299223757808b3956cdeda82fb3d3c49cb6e0ea21e1f6ffda10e42d7") --Dlcname Electrician Simulator - Smart Devices
+setManifestid(2329870, "6944148590576550617", 1032329552)
