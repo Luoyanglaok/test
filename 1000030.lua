0ce2ca361e92390a9b49b28767ce63232361fe78
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 18:51:14 +0800

    add 1000030.lua

--- 1000030.lua
+++ 1000030.lua
@@ -1,5 +1,14 @@
-addappid(1000030)
-addappid(1000031,0,"b3168631ed68f079a4f8387a3cada0af96fd440ad9d3761f73ff1dd945254484")
-setManifestid(1000031,"4294986155902772539")
-addappid(1000032,0,"270510d613e91deab852b14f372431ddda63fb935c720243b809bb7a91463a64")
-setManifestid(1000032,"6984747278248773429")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1000030.lua
+--Gamename Cook, Serve, Delicious! 3?!
+addappid(1000030) --Mainappid Cook, Serve, Delicious! 3?!
+addappid(1000031, 1, "b3168631ed68f079a4f8387a3cada0af96fd440ad9d3761f73ff1dd945254484") --Main Windows Depot Cook, Serve, Delicious! 3?!
+setManifestid(1000031, "4294986155902772539", 1273423472)
+addappid(1000032, 1, "270510d613e91deab852b14f372431ddda63fb935c720243b809bb7a91463a64") --Main Macos Depot Cook, Serve, Delicious! 3?!
+setManifestid(1000032, "6984747278248773429", 1259097904)
+--Dlcname Cook, Serve, Delicious! 3?! Soundtrack
+addappid(1231250) --Dlcname Cook, Serve, Delicious! 3?! Soundtrack
+addappid(1231251, 1, "6a9ad2c1923c4aab3ab47feae072061c94a9d4439979538228a566c1fec5ba74") --Dlc Windows Depot Cook, Serve, Delicious! 3?! Soundtrack
+setManifestid(1231251, "4417352155764693770", 142155888)
+addappid(1231252, 1, "2663e1f39f137c8faa0cf28f3612b3e92cd9102d23f5d455e91747285c57daf5") --Dlc Windows Depot Cook, Serve, Delicious! 3?! Soundtrack
+setManifestid(1231252, "64529730040387549", 642321296)
