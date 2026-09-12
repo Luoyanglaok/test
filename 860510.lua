714dd885a7598d0ea1064315c3371b01386bcf03
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 17:09:03 +0800

    add 860510.lua

--- 860510.lua
+++ 860510.lua
@@ -1,10 +1,21 @@
-addappid(860510)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-addappid(860511,0,"b4785c13c7b51e8a08e3f16332ef4839c28d6e7544230b06535bd5ee180cbfea")
-setManifestid(860511,"1340413818607528556")
-addappid(1282531,0,"1bf86c1151867753629f54ffb559c02482ff71f34f59476a0a058093e0635ddd")
-setManifestid(1282531,"4972151890057558212")
-addappid(1413420,0,"8c10dd752f9e566421902b5f300bfc5098b40beaf62ba20c7ab8d223c1423dbf")
-setManifestid(1413420,"2584754337176686908")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 860510.lua
+--Gamename Little Nightmares II
+addappid(860510, 1, "7c19e104b37e48eae6442039e13f902a29ca43da03639cb64ce7bf904f3386ac") --Mainappid Little Nightmares II
+addappid(860511, 1, "b4785c13c7b51e8a08e3f16332ef4839c28d6e7544230b06535bd5ee180cbfea") --Main Windows Depot Little Nightmares II
+setManifestid(860511, "1340413818607528556", 3328244800)
+--Dlcname Little Nightmares II Digital Content Bundle
+addappid(1282531, 1, "1bf86c1151867753629f54ffb559c02482ff71f34f59476a0a058093e0635ddd") --Dlcname Little Nightmares II Digital Content Bundle
+setManifestid(1282531, "4972151890057558212", 1025101920)
+--Dlcname Little Nightmares II Enhanced Edition
+addappid(1413420, 1, "8c10dd752f9e566421902b5f300bfc5098b40beaf62ba20c7ab8d223c1423dbf") --Dlcname Little Nightmares II Enhanced Edition
+setManifestid(1413420, "2584754337176686908", 4632045296)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(1282530) --Dlcname Little Nightmares II – Mokujin Hat
+addappid(1282532) --Dlcname Little Nightmares II – The Nome’s Attic
