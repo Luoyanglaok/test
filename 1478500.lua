6a48c4f7b27d83d7fdf5fc5b65064d1b03479423
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 23:20:20 +0800

    add 1478500.lua

---
+++ 1478500.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1478500.lua
+--Gamename Big Walk
+addappid(1478500, 1, "1174edcf23ff4903776c928407704c67bf13b9f75a546c3587fe5413055b2ca8") --Mainappid Big Walk
+addappid(1478501, 1, "f1f21164a742a9a2ce6614fc29dbe3fda4a2365c1b73e88a2738bec5cca99d53") --Main Windows Depot Big Walk
+setManifestid(1478501, "3254088315844693061", 3360916192)
+addappid(1478502, 1, "595e8e546355fab56cf985a75b0c428014f60327b70dc584cba7a3bfbebe3c71") --Main Macos Depot Big Walk
+setManifestid(1478502, "7661277211048595624", 3422879792)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
