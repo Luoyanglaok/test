79c91124020c384eb0d9f172d7d424795b7c659d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 11:18:47 +0800

    add 2172260.lua

--- 2172260.lua
+++ 2172260.lua
@@ -1,5 +1,11 @@
-addappid(2172260)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2172261,0,"83d83ec58cff9c5a8d4a6c3d49ff5bf3bbd73d72cf9f5fd84508d69d716c5748")
-setManifestid(2172261,"6918848690941247083")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2172260.lua
+--Gamename The Complex: Expedition
+addappid(2172260) --Mainappid The Complex: Expedition
+addappid(2172261, 1, "83d83ec58cff9c5a8d4a6c3d49ff5bf3bbd73d72cf9f5fd84508d69d716c5748") --Main Windows Depot The Complex: Expedition
+setManifestid(2172261, "1195880747854417819", 6902020048)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
