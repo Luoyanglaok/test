9caf3866d5bf0312c76e6795569bc674f1769366
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 18:02:35 +0800

    add 2750080.lua

--- 2750080.lua
+++ 2750080.lua
@@ -1,7 +1,19 @@
-addappid(2750080)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2750081,0,"33ee22467ac12ac1254685a95378f2f831405511a55fff1054a6b8e0bfb0165e")
-setManifestid(2750081,"6001544124993483548")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2750080.lua
+--Gamename The Settlers®: New Allies
+addappid(2750080) --Mainappid The Settlers®: New Allies
+addappid(2750081, 1, "33ee22467ac12ac1254685a95378f2f831405511a55fff1054a6b8e0bfb0165e") --Main Windows Depot The Settlers®: New Allies
+setManifestid(2750081, "6001544124993483548", 15200194640)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(2880540) --Dlcname The Settlers: New Allies - Starter Pack
+addappid(2880550) --Dlcname The Settlers: New Allies - Starter Pack - Ubisoft Activation
+addappid(2880560) --Dlcname The Settlers: New Allies - Deluxe Pack
+addappid(2880570) --Dlcname The Settlers: New Allies - Deluxe Pack - Ubisoft Activation
+addappid(2880590) --Dlcname The Settlers: New Allies - Deluxe Edition - Ubisoft Activation
+addappid(2888500) --Dlcname The Settlers: New Allies - Ubisoft Activation
