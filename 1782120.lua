d7642dd91c02202a1f239f51517d59c07eaf515c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 02:22:48 +0800

    add 1782120.lua

--- 1782120.lua
+++ 1782120.lua
@@ -1,2 +1,6 @@
-addappid(1782120)
-setManifestid(1782121,"4794845599353366448")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1782120.lua
+--Gamename ZERO Sievert
+addappid(1782120, 1, "859081face538d716778d1af6b2128c2fee9768e71487407cdc393f4be7f969c") --Mainappid ZERO Sievert
+addappid(1782121, 1, "0bc5588f4c3b046a032c9a572cad93842f26607606bbfa76dbf4468046feabec") --Main Windows Depot ZERO Sievert
+setManifestid(1782121, "5279262738150654146", 464518944)
