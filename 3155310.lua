29ca7fe10c7b7198cdb49f52dcd71cc18ec6059c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:09:56 +0800

    add 3155310.lua

---
+++ 3155310.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3155310.lua
+--Gamename The Three Kingdoms of Shadows: Xuzhou
+addappid(3155310) --Mainappid The Three Kingdoms of Shadows: Xuzhou
+addappid(3155311, 1, "6cd54e75b1718c2a3493feb9cdb4af57cbdb7badfd71b596ad6118e84ce030ca") --Main Windows Depot The Three Kingdoms of Shadows: Xuzhou
+setManifestid(3155311, "47741212435947827", 768402848)
