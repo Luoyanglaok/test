4d9914f736d1316eaf6096c9349307a55fe0ca81
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 03:09:55 +0800

    add 1845250.lua

---
+++ 1845250.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1845250.lua
+--Gamename Rubinite
+addappid(1845250) --Mainappid Rubinite
+addappid(1845251, 1, "25598ffd90253ab700d539a8bff576b1cb82853b6dea1bc4ca8d74e368b07f3c") --Main Windows Depot Rubinite
+setManifestid(1845251, "5250094833713414212", 663237056)
+--Missing Dlcs
+--Dlcname Rubinite Soundtrack
