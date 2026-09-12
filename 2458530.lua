eb9737479833d31dcb55b9e29d68310ee0187ac3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 23:43:40 +0800

    add 2458530.lua

--- 2458530.lua
+++ 2458530.lua
@@ -1,5 +1,11 @@
-addappid(2458530)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2458531,0,"a112ff17726a4eadb397f7d89ea34e081ba3891aa001fe4de40b3b3f868bb1a7")
-setManifestid(2458531,"643445481308705647")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2458530.lua
+--Gamename 魔女的夜宴
+addappid(2458530, 1, "18dcc1eaaceb4c582700b94685d723b36ab5d1a61b9ea104ae223c015a462689") --Mainappid 魔女的夜宴
+addappid(2458531, 1, "a112ff17726a4eadb397f7d89ea34e081ba3891aa001fe4de40b3b3f868bb1a7") --Main Windows Depot 魔女的夜宴
+setManifestid(2458531, "643445481308705647", 2953620544)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
