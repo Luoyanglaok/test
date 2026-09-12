d307a9a260308395fa76faae613daa545437f2f7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 01:57:58 +0800

    add 494730.lua

--- 494730.lua
+++ 494730.lua
@@ -1,5 +1,11 @@
-addappid(494730)
-addappid(494731,0,"7d9789588ba66be2326f70ccb84b168862bb5f68c9ff0b2450cce71b68770f02")
-setManifestid(494731,"1381025711095905799")
-addappid(2859040,0,"74b280ce43cc5a1076dcfdcd3629a86d2409c4db38a885662705a3a775360f4b")
-setManifestid(2859040,"4787824631112050022")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 494730.lua
+--Gamename Galacticare
+addappid(494730) --Mainappid Galacticare
+addappid(494731, 1, "7d9789588ba66be2326f70ccb84b168862bb5f68c9ff0b2450cce71b68770f02") --Main Windows Depot Galacticare
+setManifestid(494731, "8162690487333425404", 6845926992)
+--Dlcname Galacticare - Supporter Pack
+addappid(2859040, 1, "74b280ce43cc5a1076dcfdcd3629a86d2409c4db38a885662705a3a775360f4b") --Dlcname Galacticare - Supporter Pack
+setManifestid(2859040, "4787824631112050022", 781528528)
+addappid(3089140) --Dlcname Galacticare - Challenge Mode
+addappid(3334930) --Dlcname Galacticare - The Voice Beyond
