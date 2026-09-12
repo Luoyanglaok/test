d87cb384ef3246e046878e13478bdac6afc7ed06
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 05:28:16 +0800

    add 4294490.lua

---
+++ 4294490.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4294490.lua
+--Gamename KOTAMON: My Sis Found A Super-Rare Card In Her Cereal Box, So I Became A Garbage Man To Find The Entire Collection And Earn $1,000,000
+addappid(4294490, 1, "641359621dca9cdc6350136563224c4d46a4bff91f576279ae7f2f4914be5f88") --Mainappid KOTAMON: My Sis Found A Super-Rare Card In Her Cereal Box, So I Became A Garbage Man To Find The Entire Collection And Earn $1,000,000
+addappid(4294491, 1, "e83098292c96c3780bd4db1e95c7b37fccfada8df56629ed81142179291ce66c") --Main Windows Depot KOTAMON: My Sis Found A Super-Rare Card In Her Cereal Box, So I Became A Garbage Man To Find The Entire Collection And Earn $1,000,000
+setManifestid(4294491, "5558755450958333057", 604605248)
+addappid(5015110, 1, "48d3bcb60b7b68187a801a58c622f5db4ed74090647842985c8f93ff360b53e4") --Main Windows Depot KOTAMON: My Sis Found A Super-Rare Card In Her Cereal Box, So I Became A Garbage Man To Find The Entire Collection And Earn $1,000,000
+setManifestid(5015110, "6708488317890647463", 144752336)
+--Dlcname KOTAMON - Supporter Pack
+--Missing Dlcs
+--Dlcname KOTAMON: My Sis Found A Super-Rare Card In Her Cereal Box, So I Became A Garbage Man To Find The Entire Collection And Earn $1,000,000 Soundtrack
