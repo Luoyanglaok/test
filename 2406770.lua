718267eff9f03591c29fd85008759c9118ff9db9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:17:49 +0800

    add 2406770.lua

--- 2406770.lua
+++ 2406770.lua
@@ -3,7 +3,7 @@
 --Gamename Bodycam
 addappid(2406770, 1, "70969ed45a39a813850ed9d81a50af9aabc5fc4ebd8fc1216ed260e04487cb50") --Mainappid Bodycam
 addappid(2406771, 1, "0456324ba6c023dafc26bedc6005e6145a7b03bfe59a6d2806bf7e07ef54e6c2") --Main Windows Depot Bodycam
-setManifestid(2406771, "1912806340035433358", 53264478080)
+setManifestid(2406771, "5191741311888999039", 53237762288)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
