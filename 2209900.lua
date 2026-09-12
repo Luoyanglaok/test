3540f690011babe09f99df5c0fd1d245a5bd93ff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 03:31:38 +0800

    add 2209900.lua

---
+++ 2209900.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2209900.lua
+--Gamename Moonlight Peaks
+addappid(2209900, 1, "6ea39bc5800a363b407f920c57705e010df4b9139c0ed5e89ee36e9d607b546a") --Mainappid Moonlight Peaks
+addappid(2209901, 1, "01d7e2553cfbb6e74ba7875bc496c21df179b68c79905bef6f24dfbc33180749") --Main Windows Depot Moonlight Peaks
+setManifestid(2209901, "1155604627167905323", 11060930640)
+addappid(2209902, 1, "257820bd2045d17b29194d5e5b2a02d4098540bf93e11100b8cdea336125c75a") --Main Macos Depot Moonlight Peaks
+setManifestid(2209902, "3114262743365489619", 11081202064)
+--Dlcname Moonlight Peaks - Art Book
+addappid(4892990, 1, "f23cf3c15a3c6e18a5409540488640be30002f5056b9a6a65aa2e60df19950f7") --Dlcname Moonlight Peaks - Art Book
+setManifestid(4892990, "3628439518757480652", 68119440)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+--Missing Dlcs
+--Dlcname Moonlight Peaks Original Soundtrack
