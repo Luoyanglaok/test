0ac0f5d7400e9a6aaa39840edb85f3261288a2c6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:57:35 +0800

    add 3548580.lua

---
+++ 3548580.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3548580.lua
+--Gamename Chill with You : Lo-Fi Story
+addappid(3548580, 1, "8625b461a430eb0ae6b7502b6ae9e753153f4a4506b62990e9d5f40d41acf6e2") --Mainappid Chill with You : Lo-Fi Story
+addappid(3548581, 1, "4bd410e2ecc28dd07ee6d887a275a6b32edaeca1e6d401ccab204fe35bd9b99d") --Main Windows Depot Chill with You : Lo-Fi Story
+setManifestid(3548581, "3106235381077599058", 719924608)
+addappid(3548582, 1, "7f9420fcff9ed1d597a17183b7253b3cb1be15c4247e4f1894fa52b455c035fe") --Main Macos Depot Chill with You : Lo-Fi Story
+setManifestid(3548582, "468494362528011541", 747900800)
+--Dlcname Chill with You Lo-Fi Story Soundtrack
+addappid(4161250) --Dlcname Chill with You Lo-Fi Story Soundtrack
+addappid(4161251, 1, "10df7d4924fef06323492b85378b6dfa0b5b4110139745ffb254df08e7480289") --Dlc Windows Depot Chill with You Lo-Fi Story Soundtrack
+setManifestid(4161251, "3534731718328250104", 1408787056)
+addappid(4161252, 1, "b5b2ad9c8c13a4e2255bc969008fcfd0ec190c7e6ecf1051e69d33219558ab14") --Dlc Windows Depot Chill with You Lo-Fi Story Soundtrack
+setManifestid(4161252, "8870852150410581663", 109489856)
