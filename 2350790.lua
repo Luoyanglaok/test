62ffe1fdd65567ca1345e8eb01da23a38de428b8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 23:14:11 +0800

    add 2350790.lua

---
+++ 2350790.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2350790.lua
+--Gamename Moonlighter 2: The Endless Vault
+addappid(2350790, 1, "7c01755933fc3578562800de1e4f630f201b840b836fc2539b9b18836859fb3f") --Mainappid Moonlighter 2: The Endless Vault
+addappid(2350792, 1, "794eca19bb31db7e79bbc69f303feb210c8e3efca02f89ff7d38d9e0fed8705f") --Main Windows Depot Moonlighter 2: The Endless Vault
+setManifestid(2350792, "2919996153714733012", 4769081664)
+--Missing Dlcs
+--Dlcname Moonlighter 2: The Endless Vault - Zen Patio Pack
+--Dlcname Moonlighter 2: The Endless Vault - Original Artbook
+--Dlcname Moonlighter 2: The Endless Vault Official Soundtrack
