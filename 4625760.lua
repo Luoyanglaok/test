518bde99f8f34c3061096864f71c5fa7184f46e4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 11:24:14 +0800

    add 4625760.lua

---
+++ 4625760.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4625760.lua
+--Gamename 山上那个猴儿
+addappid(4625760) --Mainappid 山上那个猴儿
+addappid(4625761, 1, "def855560c72b275f39ea2f79c37259491ae354cc5a2d76192301a18b6d9fedf") --Main Windows Depot 山上那个猴儿
+setManifestid(4625761, "4553005218594867926", 87410240)
