5e8a2406ce3a52c1351c8493c2b955ec241b8510
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 19:46:53 +0800

    add 1016920.lua

--- 1016920.lua
+++ 1016920.lua
@@ -1,12 +1,26 @@
-addappid(1016920)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-setManifestid(229006,"1784011429307107530")
-addappid(1016921,0,"b82ffaa19bdd8d11584b97453a59514dff8b19275df764530fe0b4ff903d307e")
-setManifestid(1016921,"8078967623883027128")
-addappid(1016922,0,"177bb2c5aca577c8ed6613d81bd29757f03258799dd18fb4297fc00d49804d01")
-setManifestid(1016922,"4769734068859162748")
-addappid(1016923,0,"4135b3d9d81843c6672277bd25d0e0a80d50f333f93ed5f508970ff02b9de4c7")
-setManifestid(1016923,"7974558925845671853")
-addappid(1150940,0,"d7ead7d64b81dfa09d9e5dfab17ff7688f3cb0d9e5a2d0d7ec4ee733f44fd86b")
-setManifestid(1150940,"1819341891265115519")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1016920.lua
+--Gamename Unrailed!
+addappid(1016920, 1, "9a03020befb09ea1474f400fab1f448ad3be517aa84fbadb7b36dfb8be09fd83") --Mainappid Unrailed!
+addappid(1016921, 1, "b82ffaa19bdd8d11584b97453a59514dff8b19275df764530fe0b4ff903d307e") --Main Windows Depot Unrailed!
+setManifestid(1016921, "8078967623883027128", 405382848)
+addappid(1016922, 1, "177bb2c5aca577c8ed6613d81bd29757f03258799dd18fb4297fc00d49804d01") --Main Macos Depot Unrailed!
+setManifestid(1016922, "4769734068859162748", 397951328)
+addappid(1016923, 1, "4135b3d9d81843c6672277bd25d0e0a80d50f333f93ed5f508970ff02b9de4c7") --Main Linux Depot Unrailed!
+setManifestid(1016923, "7974558925845671853", 388053936)
+--Dlcname Unrailed! - Supporter Pack
+addappid(1150940, 1, "d7ead7d64b81dfa09d9e5dfab17ff7688f3cb0d9e5a2d0d7ec4ee733f44fd86b") --Dlcname Unrailed! - Supporter Pack
+setManifestid(1150940, "1819341891265115519", 53792576)
+--Dlcname Unrailed! - Soundtrack: Underwater EP
+addappid(2352250) --Dlcname Unrailed! - Soundtrack: Underwater EP
+addappid(2352251, 1, "2bae20726d09958edf89ac11d8ad0963c65f0f1970bcd1a740782b0e3363ba35") --Dlc Windows Depot Unrailed! - Soundtrack: Underwater EP
+setManifestid(2352251, "6747011711965273570", 17296560)
+addappid(2352252, 1, "80b09078d653f550ccd39b2e0ca0d9713ae284a1738b43dbd184254e5f631ce4") --Dlc Windows Depot Unrailed! - Soundtrack: Underwater EP
+setManifestid(2352252, "2450305453708342668", 79165952)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
