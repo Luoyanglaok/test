3fd863f367adf6a5bb0f171359740ac6512f8b6e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:34:04 +0800

    add 641990.lua

--- 641990.lua
+++ 641990.lua
@@ -1,11 +1,19 @@
-addappid(641990)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(641992,0,"8c3af5e78c682bcf051a7eb58d709dff6af4aad4a97be9f80484a81ecd10d577")
-setManifestid(641992,"1586345527194870116")
-addappid(641993,0,"29057c4876f7ba3fcef935f9d0d5697f98c12481e81325e6e67c47b5dc77228a")
-setManifestid(641993,"1064841895391973752")
-addappid(641994,0,"1af8fc14e0c6fcbc4e13186ff8c04538029382e544aae3b31ed1145993143311")
-setManifestid(641994,"3855406764481817315")
-addappid(666350)
-addappid(795970)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 641990.lua
+--Gamename The Escapists 2
+addappid(641990, 1, "3cad74989afb503f1fb211678e7e72510b12bf1cd75f2d69e75f30476a6d74e5") --Mainappid The Escapists 2
+addappid(641992, 1, "8c3af5e78c682bcf051a7eb58d709dff6af4aad4a97be9f80484a81ecd10d577") --Main Windows Depot The Escapists 2
+setManifestid(641992, "1586345527194870116", 1449916240)
+addappid(641993, 1, "29057c4876f7ba3fcef935f9d0d5697f98c12481e81325e6e67c47b5dc77228a") --Main Macos Depot The Escapists 2
+setManifestid(641993, "1064841895391973752", 1184497760)
+addappid(641994, 1, "1af8fc14e0c6fcbc4e13186ff8c04538029382e544aae3b31ed1145993143311") --Main Linux Depot The Escapists 2
+setManifestid(641994, "3855406764481817315", 1358091616)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(701180) --Dlcname The Escapists 2 - Season Pass
+addappid(716580) --Dlcname The Escapists 2 - Wicked Ward
+addappid(784930) --Dlcname The Escapists 2 - Big Top Breakout
+addappid(821170) --Dlcname The Escapists 2 - Dungeons and Duct Tape
+--Missing Dlcs
+--Dlcname The Escapists 2 - The Glorious Regime Prison
