8b66396c97b68e1bf24e00d194ea5351101d2ec7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 13:47:08 +0800

    add 1267170.lua

--- 1267170.lua
+++ 1267170.lua
@@ -1,3 +1,6 @@
-addappid(1267170)
-addappid(1267171,0,"c40d7690134433624afdd5f0743753d308c850ff67f8b04d491c56729d4df600")
-setManifestid(1267171,"8280122595960712644")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1267170.lua
+--Gamename Carestia
+addappid(1267170) --Mainappid Carestia
+addappid(1267171, 1, "c40d7690134433624afdd5f0743753d308c850ff67f8b04d491c56729d4df600") --Main Windows Depot Carestia
+setManifestid(1267171, "8280122595960712644", 27256416)
