6abf255516d45800267d553b13034c2ce606fcca
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 23:34:31 +0800

    add 2362060.lua

---
+++ 2362060.lua
@@ -0,0 +1,23 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2362060.lua
+--Gamename CODE VEIN II
+addappid(2362060, 1, "43d95e744211ec4d1671e74682b1c9d069e0564e51d304135a18c948f2df3367") --Mainappid CODE VEIN II
+addtoken(2362060, "10049253451395720733")
+addappid(2362061, 1, "b01b93b4fd055fc2f23e77e6797ee933d5d83fbb293cf55442e567407723aa4b") --Main Windows Depot CODE VEIN II
+setManifestid(2362061, "7568595339859464045", 74493533472)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3559210) --Dlcname CODE VEIN II - Custom Outfit Pack
+addtoken(3559210, "9580235972559335095")
+addappid(3559220) --Dlcname CODE VEIN II - Mask of Idris
+addappid(3559230) --Dlcname CODE VEIN II - CODE VEIN Character Costume Set
+addtoken(3559230, "7040880122102985044")
+addappid(3559240) --Dlcname CODE VEIN II - Stylized Forma Set
+addtoken(3559240, "7796102293365901577")
+addappid(3559250) --Dlcname CODE VEIN II - Deluxe Upgrade Pack
+addtoken(3559250, "2371691821057026299")
+addappid(3559260) --Dlcname CODE VEIN II - Ultimate Upgrade Pack
+addtoken(3559260, "14728694277007258415")
