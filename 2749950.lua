aaf3cbf680ea72b61142e11b643b491bdc19bc77
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 19:40:05 +0800

    add 2749950.lua

---
+++ 2749950.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2749950.lua
+--Gamename Esports Manager 2026
+addappid(2749950, 1, "979f73860d30018b9c7b049f4ca610e8f3a522d886624394bd0287d82a5d624d") --Mainappid Esports Manager 2026
+addappid(2749951, 1, "bbca891b03feb6b30b61373f1fdb51376656cf5ab5fee47855c742e6878b0598") --Main Windows Depot Esports Manager 2026
+setManifestid(2749951, "4464971437996255543", 2490789200)
