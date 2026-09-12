acc29aa6cf0a9b7cadec89b6c1feaf628ba73c95
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 15:47:29 +0800

    add 992300.lua

--- 992300.lua
+++ 992300.lua
@@ -1,43 +1,94 @@
-addappid(992300)
-addappid(992301,0,"025ade4ccdf49c31d20804c3c13f557cee53f57918687f445ba72726be1d74d9")
-setManifestid(992301,"6624159818833752098")
-addappid(1092241,0,"906e51251613474a848e638bafd78e16cf75e93eca137889302c3ac12b83db55")
-setManifestid(1092241,"719557481206015257")
-addappid(1092242,0,"def49b4ff45124331aa7c503b00639895e7361146c361baa5f70d897602bf132")
-setManifestid(1092242,"8982678042413115055")
-addappid(1092251,0,"a476da7278da09feba7b25741880a9f4b4c4dc34223fa5e1bfcb9b63c7c019c9")
-setManifestid(1092251,"7268623418775322540")
-addappid(1092252,0,"895ecda5fd4879f95692237a70cbc1e557149a5c2e3a68c2d28c5e4153acbbeb")
-setManifestid(1092252,"5283265706769032290")
-addappid(1092245,0,"f752e705494dc1409b8c5e90371e15fac75534cea1cfeaf87573fde9e6fe3a32")
-setManifestid(1092245,"6663481203318735997")
-addappid(1092253,0,"2dcc9ae778a3a81eeba4bd653a0716a06f8c5b6dd480734984a0b9f6d53f7394")
-setManifestid(1092253,"7559710706729855893")
-addappid(1092254,0,"a2aeb9da2fec41a4ce885a1d49356944f7b525ee0b20bc4c94142edf4afc1811")
-setManifestid(1092254,"8482099537849106159")
-addappid(1092243,0,"0938c04b0c297f447b9356cc744100d7812717c55e34a6a90e7f19f1dad6913e")
-setManifestid(1092243,"8903272106672811657")
-addappid(1092255,0,"1c3b08f0b2d9ca94d826dccf1ddc3df17fb6d401e136a14ef633317ddda0e6c3")
-setManifestid(1092255,"4823537886590791209")
-addappid(1111366,0,"2ceb1ac2db88d8e080663b6fe58ce2669cc370f6e20c70bd8fe49c205dad8ea5")
-setManifestid(1111366,"5332432184562754835")
-addappid(1111367,0,"fe6e26de89f5e5e8ed571a07df12a30da64b9c56eea24aa04a99dffdc20475e0")
-setManifestid(1111367,"5867597616274363549")
-addappid(1111368,0,"52e9c4a7822ed59c5406998a3da82421afb100bae0236958b79ea874d1e8fab8")
-setManifestid(1111368,"3867133735132606218")
-addappid(1111509,0,"87ed0b9e6d24af0bc333976cd26914471dddca5efaaf25f20a64a7912711bc80")
-setManifestid(1111509,"8663740365495894446")
-addappid(1111508,0,"2ba49932a2770d674bda1ea61e1834f36694341a81626a8c7971109d77835c5f")
-setManifestid(1111508,"7214670273163154969")
-addappid(1111501,0,"f324bd4c40a219a4d8269d21a68865dffdc1cc4d59c1c3e50227b40a56b379ee")
-setManifestid(1111501,"7728946628286717958")
-addappid(1111502,0,"0ac9d88598c69c98daf6f0e384fef586d2b86514374eb95085a96992d58625a7")
-setManifestid(1111502,"5662622916299276537")
-addappid(1111503,0,"317a04b277d45569dc4c62cd9a8065a5055a91777edfdce39071b0ac8a114154")
-setManifestid(1111503,"8841255678152723341")
-addappid(1092258,0,"ab758ef03df424ff104dbf6f475f809ea512d5824766a8aa78ec674a400e0990")
-setManifestid(1092258,"5899632139299318803")
-addappid(2314361,0,"b39a0a2053b55ffe867039264ed6ad9f943fc29b18c1d0824b0d707b779c7b16")
-setManifestid(2314361,"2956947773599199466")
-addappid(3056011,0,"b08cdd478dbc27d9a73e29a7cec06d0651b1cb215f9adee19b5ee98a6a590796")
-setManifestid(3056011,"191448634917634852")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 992300.lua
+--Gamename 嗜血印 Bloody Spell
+addappid(992300, 1, "482d67024d0766c90f42ac1a489e41766a03eacff9535c99623b3c00e0c96fe7") --Mainappid 嗜血印 Bloody Spell
+addappid(992301, 1, "025ade4ccdf49c31d20804c3c13f557cee53f57918687f445ba72726be1d74d9") --Main Windows Depot 嗜血印 Bloody Spell
+setManifestid(992301, "7860129337286926182", 15959258064)
+--Dlcname BloodySpell dlc20015 剑仙
+addappid(1092250) --Dlcname BloodySpell dlc20015 剑仙
+addappid(1092258, 1, "ab758ef03df424ff104dbf6f475f809ea512d5824766a8aa78ec674a400e0990") --Dlc Windows Depot BloodySpell dlc20015 剑仙
+setManifestid(1092258, "5899632139299318803", 64)
+--Dlcname BloodySpell dlc10001 女装大佬
+addappid(1096030) --Dlcname BloodySpell dlc10001 女装大佬
+addappid(1092241, 1, "906e51251613474a848e638bafd78e16cf75e93eca137889302c3ac12b83db55") --Dlc Windows Depot BloodySpell dlc10001 女装大佬
+setManifestid(1092241, "719557481206015257", 64)
+--Dlcname BloodySpell dlc20001 清凉肚兜
+addappid(1096040) --Dlcname BloodySpell dlc20001 清凉肚兜
+addappid(1092251, 1, "a476da7278da09feba7b25741880a9f4b4c4dc34223fa5e1bfcb9b63c7c019c9") --Dlc Windows Depot BloodySpell dlc20001 清凉肚兜
+setManifestid(1092251, "7268623418775322540", 64)
+--Dlcname BloodySpell dlc30001 原生音乐集
+addappid(1111360) --Dlcname BloodySpell dlc30001 原生音乐集
+addappid(1092245, 1, "f752e705494dc1409b8c5e90371e15fac75534cea1cfeaf87573fde9e6fe3a32") --Dlc Windows Depot BloodySpell dlc30001 原生音乐集
+setManifestid(1092245, "6663481203318735997", 75776032)
+--Dlcname BloodySpell dlc20003 猫咪制服诱惑
+addappid(1111500) --Dlcname BloodySpell dlc20003 猫咪制服诱惑
+addappid(1092253, 1, "2dcc9ae778a3a81eeba4bd653a0716a06f8c5b6dd480734984a0b9f6d53f7394") --Dlc Windows Depot BloodySpell dlc20003 猫咪制服诱惑
+setManifestid(1092253, "7559710706729855893", 64)
+--Dlcname BloodySpell dlc20002 僵尸角色扮演
+addappid(1121960) --Dlcname BloodySpell dlc20002 僵尸角色扮演
+addappid(1092252, 1, "895ecda5fd4879f95692237a70cbc1e557149a5c2e3a68c2d28c5e4153acbbeb") --Dlc Windows Depot BloodySpell dlc20002 僵尸角色扮演
+setManifestid(1092252, "5283265706769032290", 64)
+--Dlcname BloodySpell dlc20004 一眉道长
+addappid(1121970) --Dlcname BloodySpell dlc20004 一眉道长
+addappid(1092254, 1, "a2aeb9da2fec41a4ce885a1d49356944f7b525ee0b20bc4c94142edf4afc1811") --Dlc Windows Depot BloodySpell dlc20004 一眉道长
+setManifestid(1092254, "8482099537849106159", 64)
+--Dlcname BloodySpell dlc10002 女祭司
+addappid(1155760) --Dlcname BloodySpell dlc10002 女祭司
+addappid(1092242, 1, "def49b4ff45124331aa7c503b00639895e7361146c361baa5f70d897602bf132") --Dlc Windows Depot BloodySpell dlc10002 女祭司
+setManifestid(1092242, "8982678042413115055", 64)
+--Dlcname BloodySpell dlc10003 小鲤
+addappid(1215220) --Dlcname BloodySpell dlc10003 小鲤
+addappid(1092243, 1, "0938c04b0c297f447b9356cc744100d7812717c55e34a6a90e7f19f1dad6913e") --Dlc Windows Depot BloodySpell dlc10003 小鲤
+setManifestid(1092243, "8903272106672811657", 64)
+--Dlcname BloodySpell dlc20005 新春旗袍
+addappid(1215230) --Dlcname BloodySpell dlc20005 新春旗袍
+addappid(1092255, 1, "1c3b08f0b2d9ca94d826dccf1ddc3df17fb6d401e136a14ef633317ddda0e6c3") --Dlc Windows Depot BloodySpell dlc20005 新春旗袍
+setManifestid(1092255, "4823537886590791209", 64)
+--Dlcname BloodySpell dlc 20006 黄金甲
+addappid(1295130) --Dlcname BloodySpell dlc 20006 黄金甲
+addappid(1111366, 1, "2ceb1ac2db88d8e080663b6fe58ce2669cc370f6e20c70bd8fe49c205dad8ea5") --Dlc Windows Depot BloodySpell dlc 20006 黄金甲
+setManifestid(1111366, "5332432184562754835", 64)
+--Dlcname BloodySpell dlc20007 宫廷刺绣睡袍
+addappid(1295180) --Dlcname BloodySpell dlc20007 宫廷刺绣睡袍
+addappid(1111367, 1, "fe6e26de89f5e5e8ed571a07df12a30da64b9c56eea24aa04a99dffdc20475e0") --Dlc Windows Depot BloodySpell dlc20007 宫廷刺绣睡袍
+setManifestid(1111367, "5867597616274363549", 64)
+--Dlcname BloodySpell dlc20008 人气制服系列
+addappid(1295190) --Dlcname BloodySpell dlc20008 人气制服系列
+addappid(1111368, 1, "52e9c4a7822ed59c5406998a3da82421afb100bae0236958b79ea874d1e8fab8") --Dlc Windows Depot BloodySpell dlc20008 人气制服系列
+setManifestid(1111368, "3867133735132606218", 64)
+--Dlcname BloodySpell dlc20009 清凉泳装系列
+addappid(1329260) --Dlcname BloodySpell dlc20009 清凉泳装系列
+addappid(1111509, 1, "87ed0b9e6d24af0bc333976cd26914471dddca5efaaf25f20a64a7912711bc80") --Dlc Windows Depot BloodySpell dlc20009 清凉泳装系列
+setManifestid(1111509, "8663740365495894446", 64)
+--Dlcname BloodySpell dlc20010 女神旗袍
+addappid(1329261) --Dlcname BloodySpell dlc20010 女神旗袍
+addappid(1111508, 1, "2ba49932a2770d674bda1ea61e1834f36694341a81626a8c7971109d77835c5f") --Dlc Windows Depot BloodySpell dlc20010 女神旗袍
+setManifestid(1111508, "7214670273163154969", 64)
+--Dlcname BloodySpell dlc20011 婚纱系列
+addappid(1329262) --Dlcname BloodySpell dlc20011 婚纱系列
+addappid(1111501, 1, "f324bd4c40a219a4d8269d21a68865dffdc1cc4d59c1c3e50227b40a56b379ee") --Dlc Windows Depot BloodySpell dlc20011 婚纱系列
+setManifestid(1111501, "7728946628286717958", 64)
+--Dlcname BloodySpell dlc20012 邪神装
+addappid(1329263) --Dlcname BloodySpell dlc20012 邪神装
+addappid(1111502, 1, "0ac9d88598c69c98daf6f0e384fef586d2b86514374eb95085a96992d58625a7") --Dlc Windows Depot BloodySpell dlc20012 邪神装
+setManifestid(1111502, "5662622916299276537", 64)
+--Dlcname BloodySpell dlc20013 女神蕾丝裙
+addappid(1329264) --Dlcname BloodySpell dlc20013 女神蕾丝裙
+addappid(1111503, 1, "317a04b277d45569dc4c62cd9a8065a5055a91777edfdce39071b0ac8a114154") --Dlc Windows Depot BloodySpell dlc20013 女神蕾丝裙
+setManifestid(1111503, "8841255678152723341", 64)
+--Dlcname BloodySpell dlc20016 魅魔
+addappid(2314360) --Dlcname BloodySpell dlc20016 魅魔
+addappid(2314361, 1, "b39a0a2053b55ffe867039264ed6ad9f943fc29b18c1d0824b0d707b779c7b16") --Dlc Windows Depot BloodySpell dlc20016 魅魔
+setManifestid(2314361, "2956947773599199466", 64)
+--Dlcname BloodySpell dlc20018 汉服
+addappid(3056010) --Dlcname BloodySpell dlc20018 汉服
+addappid(3056011, 1, "b08cdd478dbc27d9a73e29a7cec06d0651b1cb215f9adee19b5ee98a6a590796") --Dlc Windows Depot BloodySpell dlc20018 汉服
+setManifestid(3056011, "191448634917634852", 64)
+--Dlcname BloodySpell dlc20019 赛博朋克
+addappid(4863610) --Dlcname BloodySpell dlc20019 赛博朋克
+addappid(4863615, 1, "ae4c4107f3c9dcf380e454f2c3c496e3db07dae5ae6786742d22396be6573bc0") --Dlc Windows Depot BloodySpell dlc20019 赛博朋克
+setManifestid(4863615, "4476474726287058899", 64)
+--Dlcname BloodySpell dlc20020 萌宠Cosplayer
+addappid(4863620) --Dlcname BloodySpell dlc20020 萌宠Cosplayer
+addappid(4863625, 1, "14452481e9883bcf66feac31f6537c921c3d74ca22867cefd6b3719371ee5058") --Dlc Windows Depot BloodySpell dlc20020 萌宠Cosplayer
+setManifestid(4863625, "7573320165185016316", 64)
