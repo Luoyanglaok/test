09c9d7f6f684d0d89f6b39db477704b274a8da25
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 05:53:36 +0800

    add 2426530.lua

---
+++ 2426530.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2426530.lua
+--Gamename Nova Roma
+addappid(2426530, 1, "c716304b91291bd4069b1528c278220a06931ec2d6456708fd05e4ec1469f996") --Mainappid Nova Roma
+addtoken(2426530, "9829129124425507037")
+addappid(2426533, 1, "11e7b17e52efc229e337671d604d6cda7f84bd02ab665901a7be41d3ec929aba") --Main Windows/Linux Depot Nova Roma
+setManifestid(2426533, "8627334944690660040", 320293776)
+addappid(2426534, 1, "19fe1ac6be7f66c75ba7337adfcaf9202925f0609b396ff0d71c82f53fba3f38") --Main Macos Depot Nova Roma
+setManifestid(2426534, "489199394397758439", 346093968)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
