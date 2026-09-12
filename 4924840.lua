6070f0f8ab27647cd3112bd6573fb65702d87b2a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:08:52 +0800

    add 4924840.lua

---
+++ 4924840.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4924840.lua
+--Gamename Touhou Koumakyou: Classic - the Embodiment of Scarlet Devil
+addappid(4924840) --Mainappid Touhou Koumakyou: Classic - the Embodiment of Scarlet Devil
+addappid(4924841, 1, "e8b1950f8e7efa3cf7e02c9eeecc2b843f691e0e802d826fd0320fcec0ab20ed") --Main Windows Depot Touhou Koumakyou: Classic - the Embodiment of Scarlet Devil
+setManifestid(4924841, "6538821459679913664", 326390976)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
