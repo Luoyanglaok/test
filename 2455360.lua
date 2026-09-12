7df7cdeb12264c3d1af076d038a20990d6aac585
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:48:37 +0800

    add 2455360.lua

---
+++ 2455360.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2455360.lua
+--Gamename Cooking Simulator 2: Better Together
+addappid(2455360, 1, "31871cafdd2378b19ec444d53d84af25082cf94873b39c3a0c3af21419896d76") --Mainappid Cooking Simulator 2: Better Together
+addappid(2455361, 1, "246c514beec3f2192841f0ad37f41d13200f24a0b56edf2d21111c6a688e0a09") --Main Windows Depot Cooking Simulator 2: Better Together
+setManifestid(2455361, "6732268365383013569", 9367134192)
