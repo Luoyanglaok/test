3c4fff69322e60586afe881b33f95f47754286fc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 14:55:05 +0800

    add 3376500.lua

---
+++ 3376500.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3376500.lua
+--Gamename Little Chef: Cozy Cooking
+addappid(3376500) --Mainappid Little Chef: Cozy Cooking
+addappid(3376501, 1, "56de40f8978e1f6c4ab825b583eb7ba9b5ebeb4f8b7eadc965bfe7709aeb6d0a") --Main Windows Depot Little Chef: Cozy Cooking
+setManifestid(3376501, "5176466168833141938", 159999408)
+addappid(3376502, 1, "c42bc3fc705162985cc0a1010f6dc0c3dfa2b3d8eae25b7d4774cf11e5e8cef0") --Main Macos Depot Little Chef: Cozy Cooking
+setManifestid(3376502, "7961487547900125089", 174805952)
