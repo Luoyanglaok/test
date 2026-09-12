9941e093f3f242804f17d96d139451790088c31b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 12:11:19 +0800

    add 2949950.lua

--- 2949950.lua
+++ 2949950.lua
@@ -1,5 +1,11 @@
-addappid(2949950)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2949951,0,"b549f031671f1a048c06dec9d8e12b824e59166dd6fdd592e2bfbafad6983785")
-setManifestid(2949951,"3264618890305934017")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2949950.lua
+--Gamename HAELE 3D - Portrait Studio Lite - Drawing References
+addappid(2949950) --Mainappid HAELE 3D - Portrait Studio Lite - Drawing References
+addappid(2949951, 1, "b549f031671f1a048c06dec9d8e12b824e59166dd6fdd592e2bfbafad6983785") --Main Windows Depot HAELE 3D - Portrait Studio Lite - Drawing References
+setManifestid(2949951, "5272016027653893889", 2997772816)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
