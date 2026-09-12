826b6cbacd2b3a34f6049c3561247b33c232af15
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 15:34:17 +0800

    add 1909950.lua

---
+++ 1909950.lua
@@ -0,0 +1,38 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1909950.lua
+--Gamename SUPER ROBOT WARS Y
+addappid(1909950, 1, "11922dc6c02510397696f1c4ce60915fc924a729c8adc02274dbf968b67cbc13") --Mainappid SUPER ROBOT WARS Y
+addappid(1909951, 1, "77fcc720c77a9d762fca608c74046980979badaf8ce497d8494b3c2da40d3ddb") --Main Windows Depot SUPER ROBOT WARS Y
+setManifestid(1909951, "1065763862836035722", 18084569472)
+--Dlcname SUPER ROBOT WARS Y - Bonus Mission Pack
+addappid(3390820, 1, "d22f81e1d50c71064d093e98fd8d79be8fe2443c109def325c1589235ddee9f2") --Dlcname SUPER ROBOT WARS Y - Bonus Mission Pack
+setManifestid(3390820, "7112124880795295060", 3248704)
+--Dlcname SUPER ROBOT WARS Y - Pre-Order Bonus Pack
+addappid(3483830, 1, "e32b0227a39c74cf84056450d0d93a70f6c229494b783b3a81935018c7ba5f31") --Dlcname SUPER ROBOT WARS Y - Pre-Order Bonus Pack
+setManifestid(3483830, "9089033282010469365", 463024)
+--Dlcname SUPER ROBOT WARS Y - Premium Sound & Data Pack
+addappid(3483840, 1, "c7108fbc58e82ebc3b10935a5048e2da2860b20e89c11f063d9ffe3cd1f6f638") --Dlcname SUPER ROBOT WARS Y - Premium Sound & Data Pack
+setManifestid(3483840, "1145738050661138623", 163286608)
+--Dlcname SUPER ROBOT WARS Y - DLC 1
+addappid(3483850, 1, "ba95e3ed923a31dee2ca44f450589db4ee2d645691892123d45db80a92358a1e") --Dlcname SUPER ROBOT WARS Y - DLC 1
+setManifestid(3483850, "4527382040117238039", 443776864)
+--Dlcname SUPER ROBOT WARS Y - DLC 2
+addappid(3483860, 1, "36d2080cd439b6e5db903a5cc34bc10bbaa4c69167a983ff3522237c56edb427") --Dlcname SUPER ROBOT WARS Y - DLC 2
+setManifestid(3483860, "2332045968270933307", 550388688)
+--Dlcname SUPER ROBOT WARS Y - DLC 1 & 2 Set Bonus
+addappid(3483870, 1, "9cbfb79f2b6e617b33b8011be6be19e88089ae8343b6b83b7087a59cb1507f7d") --Dlcname SUPER ROBOT WARS Y - DLC 1 & 2 Set Bonus
+setManifestid(3483870, "9219054550646773475", 265272480)
+--Dlcname SUPER ROBOT WARS Y - Expansion Pack
+addappid(3483880, 1, "5ba41954600e424f26e9e438ec848fe20c66de2daa77bf8f7d77152134f3ba34") --Dlcname SUPER ROBOT WARS Y - Expansion Pack
+setManifestid(3483880, "8308793943337114811", 806882832)
+--Dlcname SUPER ROBOT WARS Y - Deluxe Edition Bonus
+addappid(3483900, 1, "109130b59df37285a9a1e75cdb50516fb303e6694d4ae1d72e1c41cc7ef31cec") --Dlcname SUPER ROBOT WARS Y - Deluxe Edition Bonus
+setManifestid(3483900, "2408797858890454844", 46672)
+--Dlcname SUPER ROBOT WARS Y - Ultimate Edition Bonus
+addappid(3483910, 1, "4a871e042cdbc9309625b25d1178c4a4b761e122b26e96f50a31cb4c80031be3") --Dlcname SUPER ROBOT WARS Y - Ultimate Edition Bonus
+setManifestid(3483910, "7610336536250456307", 46688)
+--Dlcname SUPER ROBOT WARS Y - Anniversary Expansion Pack
+addappid(4126740, 1, "e30f0e1409dcba2cf144878b9e7a30c328af2aefd0881a43159dedcecf61076d") --Dlcname SUPER ROBOT WARS Y - Anniversary Expansion Pack
+setManifestid(4126740, "7022359798124941881", 1653060336)
+--Missing Dlcs
+--Dlcname SUPER ROBOT WARS Y - DLC 1 & 2 Set
