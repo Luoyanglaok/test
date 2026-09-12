d66ab7c905190755d7fed1b53b161836c0728907
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 13:37:32 +0800

    add 1371980.lua

--- 1371980.lua
+++ 1371980.lua
@@ -1,6 +1,21 @@
-addappid(1371980)
-setManifestid(228989,"3514306556860204959")
-addappid(1371981,0,"9598cfe06b948f9eca5cbd915b40bec41730f61f6349bbd41d5ee6e86c47a3f6")
-setManifestid(1371981,"6786745793025270417")
-addappid(1371982,0,"d2a7d91fcd80393c3bf0c9e55165cba9c37c264dbe3eddb717215de20508e607")
-setManifestid(1371982,"6654993460787894975")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1371980.lua
+--Gamename No Rest for the Wicked
+addappid(1371980, 1, "baf4a4496a90c673e65c0458177cfa9ff7edf6480002cbcafa70b2a2f1d944c9") --Mainappid No Rest for the Wicked
+addappid(1371981, 1, "9598cfe06b948f9eca5cbd915b40bec41730f61f6349bbd41d5ee6e86c47a3f6") --Main Windows Depot No Rest for the Wicked
+setManifestid(1371981, "510898126581031226", 18160294592)
+addappid(1371982, 1, "d2a7d91fcd80393c3bf0c9e55165cba9c37c264dbe3eddb717215de20508e607") --Main Windows Depot No Rest for the Wicked
+setManifestid(1371982, "8466480832192743958", 388860000)
+addappid(1371985, 1, "9f273c98ebab602f1cf76be43e4834f934f44f098537271c9c04c9fbb449004c") --Main Windows/Macos Depot No Rest for the Wicked
+setManifestid(1371985, "5949813554702470180", 2451948080)
+addappid(1371986, 1, "f63741bbbae5e090d2c6cb14d8cb10351d16ad159acdeb6aa051da9425702c67") --Main Linux Depot No Rest for the Wicked
+setManifestid(1371986, "1521315808141174752", 2461096096)
+--Dlcname No Rest for the Wicked Soundtrack
+addappid(4600710) --Dlcname No Rest for the Wicked Soundtrack
+addappid(4600711, 1, "b302ddeb3a4191deb45db0a87ccd7d73e7e036300406e410cdbd90cdcc6943a2") --Dlc Windows Depot No Rest for the Wicked Soundtrack
+setManifestid(4600711, "2675903473317953662", 540291360)
+addappid(4600713, 1, "8dcd902472e730efd7e0b43ee1bcf7b5e55732b63f4faf185bb7b9192f972da4") --Dlc Windows Depot No Rest for the Wicked Soundtrack
+setManifestid(4600713, "2456873168904838085", 117399872)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
