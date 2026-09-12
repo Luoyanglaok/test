25f4e753fe42ccb6085581f1068135fc80c72152
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 07:56:25 +0800

    add 2149010.lua

--- 2149010.lua
+++ 2149010.lua
@@ -1,5 +1,14 @@
-addappid(2149010)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2149011,0,"05e1f184ed87ed48da9f886fc92f683a735242e8265dc6ff8a289deaeddc7ba2")
-setManifestid(2149011,"2983995807411843964")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2149010.lua
+--Gamename Little Nightmares Enhanced Edition
+addappid(2149010, 1, "bd9d25a7ac48150e03c713d4a4f5d2490754b1e1b1f2ccc0b4fefd85224d663e") --Mainappid Little Nightmares Enhanced Edition
+addappid(2149011, 1, "05e1f184ed87ed48da9f886fc92f683a735242e8265dc6ff8a289deaeddc7ba2") --Main Windows Depot Little Nightmares Enhanced Edition
+setManifestid(2149011, "3370379334482300592", 4599503968)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2187770) --Dlcname AppID 2187770
+addappid(2187780) --Dlcname AppID 2187780
+addappid(2187800) --Dlcname AppID 2187800
