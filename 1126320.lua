a6935dd41fbd0740ba097079ab51852fd751249e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 15:32:39 +0800

    add 1126320.lua

--- 1126320.lua
+++ 1126320.lua
@@ -1,10 +1,22 @@
-addappid(1126320)
-setManifestid(228988,"6645201662696499616")
-addappid(1126321,0,"b487bb317f141b1ea8c7f43473ae7192dc9c3ddabf47e75c0b8ea091ea83f5dd")
-setManifestid(1126321,"4158248342270159261")
-addappid(1215820,0,"0890c98fa0aa823afe57e4bfdbdbfa87a06f0532f447baa16117044c87eee172")
-setManifestid(1215820,"1689708494354971282")
-addappid(1223490,0,"1d2d2ea68cb27690634e2ff82a64426f397f85aaf001bbf2c14cff8b4bd551a9")
-setManifestid(1223490,"5978801628945559663")
-addappid(1631620,0,"11aa46b31524868c0f6390fba205305425c13bae710c8a44f309281fe43a88b7")
-setManifestid(1631620,"5931159407815891390")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1126320.lua
+--Gamename Being a DIK - Season 1
+addappid(1126320, 1, "d8008bebfc5dcd66a109007d2238b7074a3417874a199e919c05d9a91711668a") --Mainappid Being a DIK - Season 1
+addappid(1126321, 1, "b487bb317f141b1ea8c7f43473ae7192dc9c3ddabf47e75c0b8ea091ea83f5dd") --Main Windows Depot Being a DIK - Season 1
+setManifestid(1126321, "5048437209491747633", 6497076448)
+--Dlcname Being a DIK - Season 2
+addappid(1215820, 1, "0890c98fa0aa823afe57e4bfdbdbfa87a06f0532f447baa16117044c87eee172") --Dlcname Being a DIK - Season 2
+setManifestid(1215820, "2681026102112447956", 13494691936)
+--Dlcname Being a DIK: Season 1 - The complete official guide
+addappid(1223490, 1, "1d2d2ea68cb27690634e2ff82a64426f397f85aaf001bbf2c14cff8b4bd551a9") --Dlcname Being a DIK: Season 1 - The complete official guide
+setManifestid(1223490, "8841051450723478944", 53686720)
+--Dlcname Being a DIK: Season 2 - The complete official guide
+addappid(1631620, 1, "11aa46b31524868c0f6390fba205305425c13bae710c8a44f309281fe43a88b7") --Dlcname Being a DIK: Season 2 - The complete official guide
+setManifestid(1631620, "4049727502880215072", 78039408)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+--Missing Dlcs
+--Dlcname Being a DIK - Season 3
+--Dlcname Being a DIK: Season 3 - The complete official guide
+--Dlcname Being a DIK: Season 3 - 4K Resolution Mode
