5e1be17a5b53571f74824d1140c232630dbb2024
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:00:39 +0800

    add 1011420.lua

--- 1011420.lua
+++ 1011420.lua
@@ -1,9 +1,13 @@
-addappid(1011420)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(229006)
-setManifestid(229006,"1784011429307107530")
-addappid(1011421,0,"ff10b73cbc06a7339d5ca1d141f0a1f04c45aea81ff15c0afc783e8728fc2e08")
-setManifestid(1011421,"3782575814715309544")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1011420.lua
+--Gamename Bus Mechanic Simulator
+addappid(1011420) --Mainappid Bus Mechanic Simulator
+addappid(1011421, 1, "ff10b73cbc06a7339d5ca1d141f0a1f04c45aea81ff15c0afc783e8728fc2e08") --Main Windows Depot Bus Mechanic Simulator
+setManifestid(1011421, "3782575814715309544", 3112385984)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
