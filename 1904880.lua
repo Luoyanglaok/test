034446911e79c54ab252f2dc247d7552bbbdd701
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 05:21:33 +0800

    add 1904880.lua

--- 1904880.lua
+++ 1904880.lua
@@ -1,5 +1,15 @@
-addappid(1904880)
-addappid(1904881,0,"3129bb61eb9a39fa8f4d4142993675b18e9f0e0b56b80f4144aa70f3d5301b27")
-setManifestid(1904881,"6151366278347332673")
-addappid(3283050,0,"7028a7810941a57217583d6c134ad780cf24a6f876bc62efb5748b0422d3e478")
-setManifestid(3283050,"6203990076447370502")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1904880.lua
+--Gamename Once Upon A Puppet
+addappid(1904880) --Mainappid Once Upon A Puppet
+addappid(1904881, 1, "3129bb61eb9a39fa8f4d4142993675b18e9f0e0b56b80f4144aa70f3d5301b27") --Main Windows Depot Once Upon A Puppet
+setManifestid(1904881, "6151366278347332673", 11635400192)
+--Dlcname Once Upon A Puppet - Supporter Pack
+addappid(3283050, 1, "7028a7810941a57217583d6c134ad780cf24a6f876bc62efb5748b0422d3e478") --Dlcname Once Upon A Puppet - Supporter Pack
+setManifestid(3283050, "6203990076447370502", 453677104)
+--Dlcname Once Upon A Puppet - Soundtrack
+addappid(3314130) --Dlcname Once Upon A Puppet - Soundtrack
+addappid(3314131, 1, "d74f923c166194be6519149b83f327f46028bfb53912901031c4a825ef64cf5a") --Dlc Windows Depot Once Upon A Puppet - Soundtrack
+setManifestid(3314131, "6370808341026169737", 133243584)
+addappid(3314132, 1, "d165f912c1028580042b90b1f2f5f1e53a7043f3fc01c0d5bdd412a6dae92f0a") --Dlc Windows Depot Once Upon A Puppet - Soundtrack
+setManifestid(3314132, "5821809723555983502", 539232896)
