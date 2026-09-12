43903875c3d086c0253f648650edfb0d5136f939
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 18:23:09 +0800

    add 2737070.lua

--- 2737070.lua
+++ 2737070.lua
@@ -1,3 +1,7 @@
-addappid(2737070)
-addappid(2737071,0,"36e272c2274c8d6dbf4a697d1faa618de36bbacf8439215d60cca4350601aedb")
-setManifestid(2737071,"4561550877213799178")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2737070.lua
+--Gamename Crime Simulator
+addappid(2737070, 1, "370ab87affc026ee4e6cdf067e3a5484e0bd4a5c38f97c9a07b03540bf21ecef") --Mainappid Crime Simulator
+addappid(2737071, 1, "36e272c2274c8d6dbf4a697d1faa618de36bbacf8439215d60cca4350601aedb") --Main Windows Depot Crime Simulator
+setManifestid(2737071, "3216220369565337393", 8392848720)
+addappid(4794830) --Dlcname Crime Simulator - Ashen Creek DLC
