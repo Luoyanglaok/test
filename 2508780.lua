ed5d7ffbb8df621fc54793085e0e5688a7208408
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 14:43:20 +0800

    add 2508780.lua

---
+++ 2508780.lua
@@ -0,0 +1,48 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2508780.lua
+--Gamename STORY OF SEASONS: Grand Bazaar
+addappid(2508780, 1, "34db92710a920fab17ef00ef9f41849d414e79a09d8e173a8b5d17cf06a44893") --Mainappid STORY OF SEASONS: Grand Bazaar
+addappid(2508781, 1, "d3d2cecbf425b32af43928d0b54fb991edc1a74dfac4d72d31a47b22d078ec14") --Main Windows Depot STORY OF SEASONS: Grand Bazaar
+setManifestid(2508781, "1557678708717129949", 4397782064)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Pastel Flower Set
+addappid(3051770, 1, "48425c228628d91f98d2dfbccacf0c990949331ae7831686b75272939cd409b5") --Dlcname STORY OF SEASONS: Grand Bazaar - Pastel Flower Set
+setManifestid(3051770, "4781979651439660224", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Honey Bee Set
+addappid(3051780, 1, "20ad8f70645e50600b24aa5b5c34748031628880b54a4341d0f7efc0caffdfa3") --Dlcname STORY OF SEASONS: Grand Bazaar - Honey Bee Set
+setManifestid(3051780, "6142504203826130606", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Turnip Set
+addappid(3051790, 1, "301e21e1002634db24ac7ea0bf6a7aebf90a11c6d08e8337a8239ead450139be") --Dlcname STORY OF SEASONS: Grand Bazaar - Turnip Set
+setManifestid(3051790, "7233756548924847260", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Head Chef Set
+addappid(3051800, 1, "2a0efec563aabcf8baf9162677f331803971d4eebfbba9a09e1a9adc4bfb27b8") --Dlcname STORY OF SEASONS: Grand Bazaar - Head Chef Set
+setManifestid(3051800, "4141453086276865299", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Pumpkin Set
+addappid(3051810, 1, "f2ed3c0541419292069873bf33c7dc8be660db6b8a5e6355ef5e09febc626cbf") --Dlcname STORY OF SEASONS: Grand Bazaar - Pumpkin Set
+setManifestid(3051810, "8010863435161125787", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Paw Print Set
+addappid(3051820, 1, "ce0bbf0be49a40ff8b3e544fd42c62751d4a1f228464e2bc1e1ad2767438d30d") --Dlcname STORY OF SEASONS: Grand Bazaar - Paw Print Set
+setManifestid(3051820, "6092932639233683545", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Cow Set
+addappid(3051830, 1, "11061a1ed5036b24ad17351d518ca68411d4fe283cf6156176f5d161b9044ebc") --Dlcname STORY OF SEASONS: Grand Bazaar - Cow Set
+setManifestid(3051830, "7047648226728925275", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Trunk of Transformation
+addappid(3248040, 1, "d671f9d0718c441e13b212891d9003f5260e69433455f44bcf2c768e61173336") --Dlcname STORY OF SEASONS: Grand Bazaar - Trunk of Transformation
+setManifestid(3248040, "2588098502636584215", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Rancher Set
+addappid(3406540, 1, "cbd89f777e21da2a0046f09b6b6aa1625ae6491140fce422c8f6070672427754") --Dlcname STORY OF SEASONS: Grand Bazaar - Rancher Set
+setManifestid(3406540, "9076925937056610569", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Pine Hoverfly Set
+addappid(3482230, 1, "0789e54527d096025f42b5c9e7196eed567c1e7b5a83a8466121fe86a3b6a8bb") --Dlcname STORY OF SEASONS: Grand Bazaar - Pine Hoverfly Set
+setManifestid(3482230, "498202292068430120", 1024)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Art Book
+addappid(3847830, 1, "418b5c8fd01c80869d13b37d6ddff62844ed8fa89b0c9c3803a9846c36a96c3f") --Dlcname STORY OF SEASONS: Grand Bazaar - Art Book
+setManifestid(3847830, "726245139049856328", 532213136)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Bachelor Sweetheart Set
+addappid(4426530, 1, "014faffc51b17dabb1ef21d485acaa1b08412bae7612442afc0af5e392467539") --Dlcname STORY OF SEASONS: Grand Bazaar - Bachelor Sweetheart Set
+setManifestid(4426530, "4110460564971939453", 1056)
+--Dlcname STORY OF SEASONS: Grand Bazaar - Bachelorette Sweetheart Set
+addappid(4426540, 1, "42347e364b2e4a8764dcd902e4a258d48f87db30ca44cf40866dbcd7a9268b47") --Dlcname STORY OF SEASONS: Grand Bazaar - Bachelorette Sweetheart Set
+setManifestid(4426540, "1309594991992385732", 1040)
+addappid(3847810) --Dlcname STORY OF SEASONS: Grand Bazaar - Super Digital Deluxe DLC Set
+addappid(3847820) --Dlcname STORY OF SEASONS: Grand Bazaar - New You Outfit Set
+addappid(4695490) --Dlcname STORY OF SEASONS: Grand Bazaar - Complete Sweetheart Set
