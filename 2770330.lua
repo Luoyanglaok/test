80e3d5e412a4b7867045f46ee61103840ce664f7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 20:07:26 +0800

    add 2770330.lua

---
+++ 2770330.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2770330.lua
+--Gamename Chaos Front
+addappid(2770330) --Mainappid Chaos Front
+addappid(2770331, 1, "7cf881fc5becc6e57416f2f5e954c7756b6a2e70dcf95a04988c27f23504709d") --Main Windows Depot Chaos Front
+setManifestid(2770331, "2285607310715011221", 87867136)
