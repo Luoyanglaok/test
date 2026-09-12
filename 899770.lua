774172fc883987358a078c4699935cd3ee66bc71
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 15:19:27 +0800

    add 899770.lua

--- 899770.lua
+++ 899770.lua
@@ -1,3 +1,41 @@
-addappid(899770)
-addappid(899771,0,"c2e2078a202c652383c595f86a2661e2c0bf747e070ab942890d958aaecb06d9")
-setManifestid(899771,"4171989203661091517")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 899770.lua
+--Gamename Last Epoch
+addappid(899770, 1, "7067141c355e1f204ba26265c813da0307742bfbdb077d43153a4b4650b4ba76") --Mainappid Last Epoch
+addappid(899771, 1, "c2e2078a202c652383c595f86a2661e2c0bf747e070ab942890d958aaecb06d9") --Main Windows Depot Last Epoch
+setManifestid(899771, "7042486888779928584", 19639428784)
+addappid(899772, 1, "936cd1b5632d2079bebbac0103a6a01a8eea9890ebf2797efb1ea8b041892f26") --Main Windows Depot Last Epoch
+addappid(899773, 1, "260dbdc56b50136f96e7fc61e14929ea03295274fe433a3a87e53e0066c6a465") --Main Windows Depot Last Epoch
+--Dlcname Last Epoch - Digital Sound Track
+addappid(2768220, 1, "d15b6fec494066fa9ecfbd19f48042c6db781c7db9336752a23b066d645c1607") --Dlcname Last Epoch - Digital Sound Track
+setManifestid(2768220, "2377843085109067424", 786639072)
+addappid(2303580) --Dlcname AppID 2303580
+addappid(2303581) --Dlcname AppID 2303581
+addappid(2303582) --Dlcname AppID 2303582
+addappid(2303583) --Dlcname AppID 2303583
+addappid(2744750) --Dlcname Last Epoch - "Golden Guppy" the Baby Chronowyrm
+addappid(2744760) --Dlcname Last Epoch - 100 Epoch Points (EP)
+addappid(2744770) --Dlcname Last Epoch - Adolescent Chronowyrm
+addappid(2744780) --Dlcname Last Epoch - Fallen Ronin
+addappid(2744790) --Dlcname Last Epoch - Firefly's Refuge
+addappid(2744800) --Dlcname Last Epoch - Temporal Guardian
+addappid(2744810) --Dlcname Last Epoch - Twilight Fox
+addappid(2744820) --Dlcname Last Epoch - Adult Chronowyrm
+addappid(2744830) --Dlcname Last Epoch - Celestial Way
+addappid(2768230) --Dlcname Last Epoch - 50 Epoch Points (EP)
+addappid(3080440) --Dlcname AppID 3080440
+addappid(3080450) --Dlcname AppID 3080450
+addappid(3080460) --Dlcname AppID 3080460
+addappid(3080470) --Dlcname Last Epoch - Abyssal Legend Supporter Pack
+addappid(3512570) --Dlcname Last Epoch - Woven Traveler Supporter Pack
+addappid(3512580) --Dlcname Last Epoch - Woven Templar Supporter Pack
+addappid(3512590) --Dlcname Last Epoch - Woven Vanquisher Supporter Pack
+addappid(3512600) --Dlcname Last Epoch - Woven Legend Supporter Pack
+addappid(3865720) --Dlcname Last Epoch - Primal Traveler Supporter Pack
+addappid(3865730) --Dlcname Last Epoch - Primal Templar Supporter Pack
+addappid(3865740) --Dlcname Last Epoch - Primal Vanquisher Supporter Pack
+addappid(3865750) --Dlcname Last Epoch - Primal Legend Supporter Pack
+addappid(4322970) --Dlcname Last Epoch - Fractured Traveler Supporter Pack
+addappid(4322980) --Dlcname Last Epoch - Fractured Templar Supporter Pack
+addappid(4322990) --Dlcname Last Epoch - Fractured Vanquisher Supporter Pack
+addappid(4323000) --Dlcname Last Epoch - Fractured Legend Supporter Pack
