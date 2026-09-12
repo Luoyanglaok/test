57b28f04658558ebfd6e8809010c9ef619b0a36c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 23:21:07 +0800

    add 1672810.lua

---
+++ 1672810.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1672810.lua
+--Gamename MIO: Memories in Orbit
+addappid(1672810) --Mainappid MIO: Memories in Orbit
+addappid(1672811, 1, "cb73bead2ca7cc72a2ad3fb210203ba2e0732db40cf3700d95b3fc62b825e490") --Main Windows Depot MIO: Memories in Orbit
+setManifestid(1672811, "4711570043897766739", 3228616624)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
