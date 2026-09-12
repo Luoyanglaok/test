fba79ab4d3fea25f266f0f6eeb223ba3cc27a881
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-12 22:46:59 +0800

    add 1297900.lua

---
+++ 1297900.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1297900.lua
+--Gamename Gothic 1 Remake
+addappid(1297900, 1, "f6f83cf86d7118ceede3e9d60492ccf6771d379ec14324b2bf990fc97855605a") --Mainappid Gothic 1 Remake
+addappid(1297901, 1, "63fe67b675132ec13ab1579e4a9b13f877167ef3b2a7fb4f101e85ca42c42814") --Main Windows Depot Gothic 1 Remake
+setManifestid(1297901, "382135126159906494", 32964740096)
+--Dlcname Gothic 1 Remake Soundtrack
+addappid(2671240) --Dlcname Gothic 1 Remake Soundtrack
+addappid(2671241, 1, "169a2e0c25b7e6a417ebc08c95881a7c1399043d6b46905b17f44721454f9822") --Dlc Windows Depot Gothic 1 Remake Soundtrack
+setManifestid(2671241, "1244025951154317983", 180792672)
+addappid(2671242, 1, "cc16ec5ce1ab0c451d5440c42859e77c001cce01af7a39dd5170f0d408989eca") --Dlc Windows Depot Gothic 1 Remake Soundtrack
+setManifestid(2671242, "850254086776421475", 711019344)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
