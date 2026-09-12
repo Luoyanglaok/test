b7556c00035fac87f6fc34e4691d7734a3f129f3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 01:27:10 +0800

    add 2774040.lua

---
+++ 2774040.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2774040.lua
+--Gamename The Boss Gangster: Criminal Empire
+addappid(2774040) --Mainappid The Boss Gangster: Criminal Empire
+addappid(2774041, 1, "a448ae83061f402b52fa69cff70f81e6271ccfc66329a66b80a8248a0fea7dad") --Main Windows Depot The Boss Gangster: Criminal Empire
+setManifestid(2774041, "8438311517957294907", 1396619152)
