f40257877d53c265336f0faa60d96f0d04de067c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 00:44:42 +0800

    add 4453300.lua

---
+++ 4453300.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4453300.lua
+--Gamename Cup and Counter: Coffee Shop Simulator
+addappid(4453300) --Mainappid Cup and Counter: Coffee Shop Simulator
+addappid(4453301, 1, "7673f352c60d0b2124b3dbb54b8e068f3fefa34733018c1c7ff8bf1a28332c5b") --Main Windows/Linux Depot Cup and Counter: Coffee Shop Simulator
+setManifestid(4453301, "7677692564254012250", 1204607664)
