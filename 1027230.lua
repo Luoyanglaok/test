caf7228f89d90f66cc2c77e6b8c1b352baef2ae4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 14:56:08 +0800

    add 1027230.lua

--- 1027230.lua
+++ 1027230.lua
@@ -1,3 +1,6 @@
-addappid(1027230)
-addappid(1027231,0,"9b6c68ff2c687fed89b163c331e727641e3082b1038bd2212a716d42d417d323")
-setManifestid(1027231,"6949963446432261654")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1027230.lua
+--Gamename Woman's body: For adults
+addappid(1027230) --Mainappid Woman's body: For adults
+addappid(1027231, 1, "9b6c68ff2c687fed89b163c331e727641e3082b1038bd2212a716d42d417d323") --Main Windows Depot Woman's body: For adults
+setManifestid(1027231, "6949963446432261654", 11834736)
