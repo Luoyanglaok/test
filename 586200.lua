a55e39f0da8c577c4187df5610db6635a23af6ab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 09:18:16 +0800

    add 586200.lua

--- 586200.lua
+++ 586200.lua
@@ -1,8 +1,15 @@
-addappid(586200)
-setManifestid(228986,"8782296191957114623")
-addappid(586201,0,"db4277b4f48376b54dad95c88fdaf5754f4b373872d0b63fcc2cba2428cb6638")
-setManifestid(586201,"6872899991876066797")
-addappid(586202,0,"bab43ae9edf922438c44acdb633b3a7458ad57742925b172e1d9c545ef8d4461")
-setManifestid(586202,"3960767494968070627")
-addappid(586203,0,"97ce5507f8e9dc8a11cf3ba6d30bd29ff2f483bb09ba7a5d3a0f21d8b1c982d2")
-setManifestid(586203,"4689750818252803957")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 586200.lua
+--Gamename Street Fighter 30th Anniversary Collection
+addappid(586200, 1, "1cd30699210be67351917d4d497b44ba8bbfb785e610baf4adc6cfa169a95819") --Mainappid Street Fighter 30th Anniversary Collection
+addappid(586201, 1, "db4277b4f48376b54dad95c88fdaf5754f4b373872d0b63fcc2cba2428cb6638") --Main Windows Depot Street Fighter 30th Anniversary Collection
+setManifestid(586201, "6872899991876066797", 4999816016)
+addappid(586202, 1, "bab43ae9edf922438c44acdb633b3a7458ad57742925b172e1d9c545ef8d4461") --Main Windows Depot Street Fighter 30th Anniversary Collection
+setManifestid(586202, "3960767494968070627", 5001019184)
+addappid(586203, 1, "97ce5507f8e9dc8a11cf3ba6d30bd29ff2f483bb09ba7a5d3a0f21d8b1c982d2") --Main Windows Depot Street Fighter 30th Anniversary Collection
+setManifestid(586203, "4689750818252803957", 5028665824)
+addappid(586204, 1, "14bc5016cdb1f7d330d68032b52e48673f843562c0007722fdb3431017c6f0e7") --Main Windows Depot Street Fighter 30th Anniversary Collection
+setManifestid(586204, "3246656311099709929", 5027438432)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
