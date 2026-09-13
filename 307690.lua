3d8a737bd8a9251dbae946d9d4b8d3441c6d0d40
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 08:35:51 +0800

    add 307690.lua

--- 307690.lua
+++ 307690.lua
@@ -1,15 +1,21 @@
-addappid(307690)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(307691,0,"16e218fadcf1fd663c320861c008944503ae0e2725445797fd5043efb3806809")
-setManifestid(307691,"1259773366995347002")
-addappid(307692,0,"ccb1da56b95d82ffb9c3d29e4c19b6a782988c14c610a8ca674ead32d0f6cc8d")
-setManifestid(307692,"8969342104525223947")
-addappid(307693,0,"9deece342bce4e677c7c91c18a4e137aaa45e689533db5aa669ba83ea6cbc8cb")
-setManifestid(307693,"521771309705279162")
-addappid(307694,0,"dd8ea35584589906ffef100d5d805f816c71b9dc99efe76a4e4eab647baceda4")
-setManifestid(307694,"5203513487252783908")
-addappid(307695,0,"1c80d5c2e7555bfabf795cfbbe8cb7449132c279f20bfbaa43351e72fe09b2d4")
-setManifestid(307695,"1186231055532829370")
-addappid(307696,0,"a265aef4fa6f4034eff3b39b421a9405aa44454654e8c7778847df6bb3684c59")
-setManifestid(307696,"7419791888443965893")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 307690.lua
+--Gamename Sleeping Dogs: Definitive Edition
+addappid(307690, 1, "9e72d6ce581ccfbe8447dca7ced57bc5ea87c35db639b7d3d9f8cf8162df892a") --Mainappid Sleeping Dogs: Definitive Edition
+addappid(307691, 1, "16e218fadcf1fd663c320861c008944503ae0e2725445797fd5043efb3806809") --Main Windows Depot Sleeping Dogs: Definitive Edition
+setManifestid(307691, "1259773366995347002", 19547874013219)
+addappid(307692, 1, "ccb1da56b95d82ffb9c3d29e4c19b6a782988c14c610a8ca674ead32d0f6cc8d") --Main Windows Depot Sleeping Dogs: Definitive Edition
+setManifestid(307692, "8969342104525223947", 10646154136)
+addappid(307693, 1, "9deece342bce4e677c7c91c18a4e137aaa45e689533db5aa669ba83ea6cbc8cb") --Main Windows Depot Sleeping Dogs: Definitive Edition
+setManifestid(307693, "521771309705279162", 2009541826)
+addappid(307694, 1, "dd8ea35584589906ffef100d5d805f816c71b9dc99efe76a4e4eab647baceda4") --Main Windows Depot Sleeping Dogs: Definitive Edition
+setManifestid(307694, "5203513487252783908", 2582022576)
+addappid(307695, 1, "1c80d5c2e7555bfabf795cfbbe8cb7449132c279f20bfbaa43351e72fe09b2d4") --Main Macos Depot Sleeping Dogs: Definitive Edition
+setManifestid(307695, "1186231055532829370", 15683965376)
+addappid(307696, 1, "a265aef4fa6f4034eff3b39b421a9405aa44454654e8c7778847df6bb3684c59") --Main Macos Depot Sleeping Dogs: Definitive Edition
+setManifestid(307696, "7419791888443965893", 36166576)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
