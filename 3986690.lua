1bff3ee3244fad80541e6f5dd7c32a8519937bff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 18:00:31 +0800

    add 3986690.lua

---
+++ 3986690.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3986690.lua
+--Gamename Ariana and the Elder Codex
+addappid(3986690, 1, "de0ff31df35ce9fea0b3de07063888d760bb24e8575b2525217ba6acd1ea933e") --Mainappid Ariana and the Elder Codex
+addappid(3986691, 1, "a4d9c62943e8881dd3339f75b865e5da3384c232df2cd3bba96d13309628f767") --Main Windows Depot Ariana and the Elder Codex
+setManifestid(3986691, "1870852140323422772", 1400190640)
+addappid(4152631, 1, "0ae951ee0c34902c9767a244e134a79616e2993dad2553b6dfb14ec55ef3123e") --Main Windows Depot Ariana and the Elder Codex
+setManifestid(4152631, "7474660752256974600", 335278800)
+addappid(4211251, 1, "b715ab5590cfcd7aee888e3df70cd80fc6df5ca6d1e741554f3de4f74b57cc35") --Main Windows Depot Ariana and the Elder Codex
+setManifestid(4211251, "6114791043707492052", 1510796880)
+addappid(4356790, 1, "b9427368674542dd2db2d13119a9b431888f5129aebe760f21bfc3cf70373937") --Main Windows Depot Ariana and the Elder Codex
+setManifestid(4356790, "8308051710605086203", 26146816)
+--Dlcname Ariana and the Elder Codex Digital Soundtrack
+addappid(4152630) --Dlcname Ariana and the Elder Codex Digital Soundtrack
+--Dlcname Ariana and the Elder Codex Digital Art Book
+addappid(4211250) --Dlcname Ariana and the Elder Codex Digital Art Book
+--Dlcname Ariana and the Elder Codex - PC and Mobile Wallpapers
+addappid(4131000) --Dlcname Ariana and the Elder Codex - Ocean Bookmark
+addappid(4131010) --Dlcname Ariana and the Elder Codex - Rainfall Ring
+addappid(4131030) --Dlcname Ariana and the Elder Codex - Mothers' Blessing
