8c6778a2d951f3b7bd70b1ffd05557b85e1c6513
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 13:02:54 +0800

    add 1691970.lua

--- 1691970.lua
+++ 1691970.lua
@@ -1,3 +1,6 @@
-addappid(1691970)
-addappid(1691971,0,"ef865324c35579e6b37b60fded989943e391b4e2cc69b4ca1de12bdd1bd759b5")
-setManifestid(1691971,"6803312782285633044")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1691970.lua
+--Gamename Danganronpa S: Ultimate Summer Camp
+addappid(1691970) --Mainappid Danganronpa S: Ultimate Summer Camp
+addappid(1691971, 1, "ef865324c35579e6b37b60fded989943e391b4e2cc69b4ca1de12bdd1bd759b5") --Main Windows Depot Danganronpa S: Ultimate Summer Camp
+setManifestid(1691971, "6803312782285633044", 1357906576)
