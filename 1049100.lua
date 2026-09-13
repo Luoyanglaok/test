2f224597d30d38271343dcbec69df73e5e78617e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 11:07:26 +0800

    add 1049100.lua

--- 1049100.lua
+++ 1049100.lua
@@ -1,9 +1,20 @@
-addappid(1049100)
-addappid(1049101,0,"c71eca367a831a038455f535c347297f4c321b91bb6db15918db9e557737eec0")
-setManifestid(1049101,"7887703637414174973")
-addappid(1049102,0,"8a2b27144f91509b38a2e0fa2ba9af5d7f7bbdafd14ff348b000772b87994181")
-setManifestid(1049102,"9194050912565999716")
-addappid(1049103,0,"579fb5ecbb28623db8380b22bcc78770d91c4000ae03aa00a184691c8759f712")
-setManifestid(1049103,"3870198695716252603")
-addappid(1049104,0,"40bf682876ddf4178357f89f9875ed8194f379dd1092e0947a82350b52c82d5c")
-setManifestid(1049104,"3258648389874837977")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1049100.lua
+--Gamename 泡沫冬景
+addappid(1049100) --Mainappid 泡沫冬景
+addappid(1049101, 1, "c71eca367a831a038455f535c347297f4c321b91bb6db15918db9e557737eec0") --Main Windows Depot 泡沫冬景
+setManifestid(1049101, "7887703637414174973", 1696156592)
+addappid(1049102, 1, "8a2b27144f91509b38a2e0fa2ba9af5d7f7bbdafd14ff348b000772b87994181") --Main Windows Language Depot Tchinese 泡沫冬景
+setManifestid(1049102, "9194050912565999716", 1597740608)
+addappid(1049103, 1, "579fb5ecbb28623db8380b22bcc78770d91c4000ae03aa00a184691c8759f712") --Main Windows Language Depot Japanese 泡沫冬景
+setManifestid(1049103, "3870198695716252603", 1520576656)
+addappid(1049104, 1, "40bf682876ddf4178357f89f9875ed8194f379dd1092e0947a82350b52c82d5c") --Main Windows Language Depot English 泡沫冬景
+setManifestid(1049104, "3258648389874837977", 1031142240)
+--Dlcname Christmas Tina -泡沫冬景- 设定增量补充包
+addappid(4317510) --Dlcname Christmas Tina -泡沫冬景- 设定增量补充包
+addappid(4317511, 1, "a538f6e570c428bee1562aa8781c2a623b79281f91e3301731f9839362ab50cb") --Dlc Windows Depot Christmas Tina -泡沫冬景- 设定增量补充包
+setManifestid(4317511, "326823898496663608", 630409216)
+addappid(4317512, 1, "fc47edf112d3fef0c4c9335bc9685547ebd0eb741ed72e9e513493063d653e0e") --Dlc Windows Depot Christmas Tina -泡沫冬景- 设定增量补充包
+setManifestid(4317512, "4840910830709760684", 850815616)
+addappid(1424830) --Dlcname 泡沫冬景：樱色零落&景色萧然
+addappid(1506340) --Dlcname 泡沫冬景：和平默示录
