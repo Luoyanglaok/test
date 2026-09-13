0351a89e25cacb1877145e55902129d96d5f59ab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 13:02:22 +0800

    add 1324340.lua

--- 1324340.lua
+++ 1324340.lua
@@ -1,7 +1,11 @@
-addappid(1324340)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1324341,0,"9eaf4b7ea8830e817a0347e7d9579f940c5368a0759b9f2ecdf4443466eada7e")
-setManifestid(1324341,"5853099990832539946")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1324340.lua
+--Gamename Made in Abyss: Binary Star Falling into Darkness
+addappid(1324340) --Mainappid Made in Abyss: Binary Star Falling into Darkness
+addappid(1324341, 1, "9eaf4b7ea8830e817a0347e7d9579f940c5368a0759b9f2ecdf4443466eada7e") --Main Windows Depot Made in Abyss: Binary Star Falling into Darkness
+setManifestid(1324341, "5853099990832539946", 11536682288)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
