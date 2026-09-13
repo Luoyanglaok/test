c46b3f8991b6b0f57b4564691ccd23d0d58f8f23
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 19:11:27 +0800

    add 1842620.lua

--- 1842620.lua
+++ 1842620.lua
@@ -1,3 +1,6 @@
-addappid(1842620)
-addappid(1842621,0,"e381094a9a17f36b1ce9e1040f5bb5ec19b56dc6a59b75473d932693480fbf31")
-setManifestid(1842621,"4733792379017130551")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1842620.lua
+--Gamename Trepang2 - Soundtrack
+addappid(1842620) --Mainappid Trepang2 - Soundtrack
+addappid(1842621, 1, "e381094a9a17f36b1ce9e1040f5bb5ec19b56dc6a59b75473d932693480fbf31") --Main Windows Depot Trepang2 - Soundtrack
+setManifestid(1842621, "4733792379017130551", 2565629312)
