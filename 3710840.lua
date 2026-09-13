01892b13c7c6f0cfab78ad971f062212fd5f0f64
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 10:05:32 +0800

    add 3710840.lua

---
+++ 3710840.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3710840.lua
+--Gamename House Flipper Remastered Collection
+addappid(3710840, 1, "a71ad4a1b3fbe9f459b53e904e4d69521907dae9f83ae79a0f429027343cab64") --Mainappid House Flipper Remastered Collection
+addappid(3710841, 1, "488fc8ff042da5fbec1bbf2ace38baed5d94b2ca030a4d5da53c90366e489bc4") --Main Windows Depot House Flipper Remastered Collection
+setManifestid(3710841, "8201944931468931059", 12885549648)
+addappid(3710842, 1, "4ac9eb8ffd1dbaf7f1e0854c65fbee712878183417b817be2a5ddea9133910aa") --Main Macos Depot House Flipper Remastered Collection
+setManifestid(3710842, "8222876920177606981", 12886873728)
