19973c96e7374eef05d017d8ba83085920e39659
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 17:55:03 +0800

    add 4588700.lua

---
+++ 4588700.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4588700.lua
+--Gamename Monster Girls: You Can't Say No 🔞
+addappid(4588700) --Mainappid Monster Girls: You Can't Say No 🔞
+addappid(4588701, 1, "39d3ce7afbfbac82a7a7fb7a12f9cf32ca44c3c0aa2fc298f16aba4259e47f48") --Main Windows Depot Monster Girls: You Can't Say No 🔞
+setManifestid(4588701, "8261394324198423215", 6711693232)
+addappid(4588702, 1, "dd40ac300073e1d01945bbd7cab7b9cbbb59fd7f7768cc3c0e21b216566a4042") --Main Macos Depot Monster Girls: You Can't Say No 🔞
+setManifestid(4588702, "8577466386383605165", 6718133648)
+addappid(4588703, 1, "c20f0e48a640f1d07759ad054c8b8000ce569e1be196288d65db95f0cad4a73d") --Main Linux Depot Monster Girls: You Can't Say No 🔞
+setManifestid(4588703, "632559694938030529", 6708888944)
+--Dlcname Monster Girls: You Can't Say No - Wallpapers🔞
+addappid(5054000, 1, "11998b2b4b69d8901039b2d2053942713338a338ce1d736583747e3c174f7f41") --Dlcname Monster Girls: You Can't Say No - Wallpapers🔞
+setManifestid(5054000, "1920668810363759483", 219278416)
