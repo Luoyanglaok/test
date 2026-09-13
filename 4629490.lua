5a85c453554e42945a553da40887ced7f1ce65e6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 12:09:40 +0800

    add 4629490.lua

---
+++ 4629490.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4629490.lua
+--Gamename 魔王的后宫~异世界温柔乡~
+addappid(4629490, 1, "92e61ad5086dd9b10c47e0e3a20086493801a0c76d5e3d4208e31b461de9eb9e") --Mainappid 魔王的后宫~异世界温柔乡~
+addappid(4629491, 1, "743af18c254f870331f5a91463d13eea904371d6d61f868825373bb9a46a6abc") --Main Windows Depot 魔王的后宫~异世界温柔乡~
+setManifestid(4629491, "1402216925308112695", 1483706928)
+--Dlcname 魔王的后宫 -【成人内容扩展包】
+addappid(4795100, 1, "0c59d55de6c40c2ac02faaa7287b25fdce37521a6a6ebb1c3e9b658c60dcedd8") --Dlcname 魔王的后宫 -【成人内容扩展包】
+setManifestid(4795100, "3848410127966297163", 1483706944)
