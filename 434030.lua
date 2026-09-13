4e297861961d6351eb09d471697510fb959a3298
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 11:49:12 +0800

    add 434030.lua

--- 434030.lua
+++ 434030.lua
@@ -1,37 +1,52 @@
-addappid(434030)
-addappid(434031,0,"175c430691e521db8fe3ca4e9c0e7471a0ced159a324a77faa349ab1fec3ab8a")
-setManifestid(434031,"9190671642806032411")
-addappid(434032,0,"bc3c122d047b0325ec444882f25f32be59b6d711188e8f9f52f7f3728d9f1cb9")
-setManifestid(434032,"2972015323168424498")
-addappid(434033,0,"676dcd2f2777f6e5df9d01e5fab20b1e359a0eaa60c988514bc0913fded566d7")
-setManifestid(434033,"5605887546063720616")
-addappid(434034)
-addappid(486110,0,"0d78cb63689cb549d65a331cf0ac394a419332057d1511a0d9f6a0a39d1bbfe4")
-setManifestid(486110,"3855555389078697043")
-addappid(500620)
-addappid(570310,0,"c7e19959b7f2db49cbfd3c29f2ab1de049b1bd895657cf7417611c5e8b99f36e")
-setManifestid(570310,"1232778421756566370")
-addappid(684610,0,"ef8d153c2cb302f162e17d385971fe875f23a7212ceae008885134bf007e5fd0")
-setManifestid(684610,"570442096687578638")
-addappid(611210,0,"726aecba90cc8e3493ef831c06f383324a044d325c4ceac757fc82bc7329e732")
-setManifestid(611210,"5122814516827376917")
-addappid(742410,0,"fbde2ddf388673293c8c8494c45ac201a108a0aacc1ae33d54792fb269de79ab")
-setManifestid(742410,"6063692397948215423")
-addappid(898700)
-addappid(643730)
-addappid(643731)
-addappid(932760)
-addappid(771830)
-addappid(794140)
-addappid(794141)
-addappid(820940)
-addappid(1101750,0,"6f2e0afaba5d3442474b5aaeea047054d6337bad12a25f3601b35374371b3bf5")
-setManifestid(1101750,"4279539059056828042")
-addappid(794143)
-addappid(1121620)
-addappid(1121621)
-addappid(1121622)
-addappid(1214170)
-addappid(1312980)
-addappid(1478120)
-addappid(1523430)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 434030.lua
+--Gamename Aerofly FS 2 Flight Simulator
+addappid(434030, 1, "582d26be4fd8cdb8ff7e7c51962dbf50726ab76a25ebc380e154e8066512aa15") --Mainappid Aerofly FS 2 Flight Simulator
+addappid(434031, 1, "175c430691e521db8fe3ca4e9c0e7471a0ced159a324a77faa349ab1fec3ab8a") --Main Windows Depot Aerofly FS 2 Flight Simulator
+setManifestid(434031, "9190671642806032411", 34346067584)
+addappid(434032, 1, "bc3c122d047b0325ec444882f25f32be59b6d711188e8f9f52f7f3728d9f1cb9") --Main Windows Depot Aerofly FS 2 Flight Simulator
+setManifestid(434032, "2972015323168424498", 13529856)
+addappid(434033, 1, "676dcd2f2777f6e5df9d01e5fab20b1e359a0eaa60c988514bc0913fded566d7") --Main Linux Depot Aerofly FS 2 Flight Simulator
+setManifestid(434033, "5605887546063720616", 5136896)
+addappid(434034, 1, "413690fe7af048437449018a10b70431380a5e7382624c3b24e7b15cbbc8aa16") --Main Macos Depot Aerofly FS 2 Flight Simulator
+setManifestid(434034, "2736089199280333755", 10137408)
+--Dlcname Aerofly FS 2 - High Resolution Texture Pack for Southwestern USA
+addappid(486110, 1, "0d78cb63689cb549d65a331cf0ac394a419332057d1511a0d9f6a0a39d1bbfe4") --Dlcname Aerofly FS 2 - High Resolution Texture Pack for Southwestern USA
+setManifestid(486110, "3855555389078697043", 62601164464)
+--Dlcname Aerofly FS 2 - Switzerland
+addappid(500620, 1, "a7bca14448ed0d04fe9e65bca3203b4af0fbb37c5dd952e9de58c2133787e014") --Dlcname Aerofly FS 2 - Switzerland
+setManifestid(500620, "1062310461572177534", 16112063568)
+--Dlcname Aerofly FS 2 - Northeastern USA
+addappid(570310, 1, "c7e19959b7f2db49cbfd3c29f2ab1de049b1bd895657cf7417611c5e8b99f36e") --Dlcname Aerofly FS 2 - Northeastern USA
+setManifestid(570310, "1232778421756566370", 7775281376)
+--Dlcname Aerofly FS 2 - USA Utah
+addappid(611210, 1, "726aecba90cc8e3493ef831c06f383324a044d325c4ceac757fc82bc7329e732") --Dlcname Aerofly FS 2 - USA Utah
+setManifestid(611210, "5122814516827376917", 16863231616)
+--Dlcname Aerofly FS 2 - Aircraft - Q400
+addappid(684610, 1, "ef8d153c2cb302f162e17d385971fe875f23a7212ceae008885134bf007e5fd0") --Dlcname Aerofly FS 2 - Aircraft - Q400
+setManifestid(684610, "570442096687578638", 66315440)
+--Dlcname Aerofly FS 2 - USA Colorado
+addappid(742410, 1, "fbde2ddf388673293c8c8494c45ac201a108a0aacc1ae33d54792fb269de79ab") --Dlcname Aerofly FS 2 - USA Colorado
+setManifestid(742410, "6063692397948215423", 18677416128)
+--Dlcname Aerofly FS 2 - Orbx - Eagle County Colorado
+addappid(794140, 1, "9ba14f3e4469683d5db6491659bd4c2dc4ad995d83efc5aefd38650343020f34") --Dlcname Aerofly FS 2 - Orbx - Eagle County Colorado
+setManifestid(794140, "5742472031932396089", 2222320192)
+--Dlcname Aerofly FS 2 - Aerosoft - Lukla
+addappid(1101750, 1, "6f2e0afaba5d3442474b5aaeea047054d6337bad12a25f3601b35374371b3bf5") --Dlcname Aerofly FS 2 - Aerosoft - Lukla
+setManifestid(1101750, "4279539059056828042", 7400812288)
+--Missing Dlcs
+--Dlcname Aerofly FS 2 - Orbx - Innsbruck Airport
+--Dlcname Aerofly FS 2 - Orbx - Chicago Meigs Field
+--Dlcname Aerofly FS 2 - Orbx - Monterey Regional Airport
+--Dlcname Aerofly FS 2 - Orbx - Palm Springs International Airport
+--Dlcname Aerofly FS 2 - Aerosoft - Germany Helgoland
+--Dlcname Aerofly FS 2 - USA South Florida
+--Dlcname Aerofly FS 2 - Orbx - Netherlands TrueEarth
+--Dlcname Aerofly FS 2 - Just Flight - Duchess
+--Dlcname Aerofly FS 2 - Just Flight - Arrow
+--Dlcname Aerofly FS 2 - Just Flight - C152
+--Dlcname Aerofly FS 2 - Just Flight - Tomahawk
+--Dlcname Aerofly FS 2 - Just Flight - Falke SF25
+--Dlcname Aerofly FS 2 - Just Flight - Hawk
+addappid(794143) --Dlcname AppID 794143
+addappid(1523430) --Dlcname AppID 1523430
