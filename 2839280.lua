8bb2f118235bab9c0a2c794c6e6c4b535366950e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 13:00:49 +0800

    add 2839280.lua

--- 2839280.lua
+++ 2839280.lua
@@ -1,5 +1,14 @@
-addappid(2839280)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2839281,0,"0f3c4a4638a09ca39ece60e8b7daa705bf0a70d0d9836cf7f109b5c324720537")
-setManifestid(2839281,"25185765069974839")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2839280.lua
+--Gamename Natsu-Mon: 20th Century Summer Kid
+addappid(2839280) --Mainappid Natsu-Mon: 20th Century Summer Kid
+addappid(2839281, 1, "0f3c4a4638a09ca39ece60e8b7daa705bf0a70d0d9836cf7f109b5c324720537") --Main Windows Depot Natsu-Mon: 20th Century Summer Kid
+setManifestid(2839281, "25185765069974839", 2242363616)
+--Dlcname Natsu-Mon: 20th Century Summer Kid: Broadcast Over Sunset
+addappid(2913330, 1, "77217da454f17cb71823d83ccab084c20356dfe54ce50434be0d486ef697034c") --Dlcname Natsu-Mon: 20th Century Summer Kid: Broadcast Over Sunset
+setManifestid(2913330, "3687264469890870101", 53788032)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
