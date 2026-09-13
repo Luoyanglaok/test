d581c4e7d9bc62d9acf00b66dcd317864cd7d840
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 11:20:37 +0800

    add 1449690.lua

--- 1449690.lua
+++ 1449690.lua
@@ -1,5 +1,11 @@
-addappid(1449690)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1449691,0,"ebfb9f62016b22f81e1323ccfc885d47a554513a3aec0310ea08e4043e4a84a7")
-setManifestid(1449691,"7544099611846363881")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1449690.lua
+--Gamename The Walking Dead: The Telltale Definitive Series
+addappid(1449690, 1, "fcd7ea3158090a5530207a9e27bc69ade8a22fefa3773c951e2166651779e5d2") --Mainappid The Walking Dead: The Telltale Definitive Series
+addappid(1449691, 1, "ebfb9f62016b22f81e1323ccfc885d47a554513a3aec0310ea08e4043e4a84a7") --Main Windows Depot The Walking Dead: The Telltale Definitive Series
+setManifestid(1449691, "7544099611846363881", 47877808144)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
