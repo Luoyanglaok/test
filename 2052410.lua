2c75f8794c885cdd7aeb794589176e435b06e287
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 12:53:05 +0800

    add 2052410.lua

--- 2052410.lua
+++ 2052410.lua
@@ -1,3 +1,6 @@
-addappid(2052410)
-addappid(2052411,0,"6ab2d901d86a9f777078ad2d3310ba844676045d51fce0e67d2ad6ec7bb3c39d")
-setManifestid(2052411,"2393599331048596479")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2052410.lua
+--Gamename WITCH ON THE HOLY NIGHT
+addappid(2052410) --Mainappid WITCH ON THE HOLY NIGHT
+addappid(2052411, 1, "6ab2d901d86a9f777078ad2d3310ba844676045d51fce0e67d2ad6ec7bb3c39d") --Main Windows Depot WITCH ON THE HOLY NIGHT
+setManifestid(2052411, "2393599331048596479", 11599174768)
