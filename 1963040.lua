252211be0c8584dd2053aceefc03200ac21a9478
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 09:26:44 +0800

    add 1963040.lua

--- 1963040.lua
+++ 1963040.lua
@@ -1,9 +1,47 @@
-addappid(1963040)
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-setManifestid(229012,"4353723233161159493")
-setManifestid(229020,"5799761707845834510")
-setManifestid(229030,"1043465440436835055")
-addappid(1963041,0,"0960237e0bdfa0375c4556be4c342956e5f6d446657bc4d0a9644f32316b9c76")
-setManifestid(1963041,"3036330452574176109")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1963040.lua
+--Gamename The Lost Village
+addappid(1963040) --Mainappid The Lost Village
+addappid(1963041, 1, "0960237e0bdfa0375c4556be4c342956e5f6d446657bc4d0a9644f32316b9c76") --Main Windows Depot The Lost Village
+setManifestid(1963041, "7151174632794435826", 5961430080)
+addappid(1963042, 1, "61e5e1650a0b0a2ee690399ead28cc4d11ac3a1058dfd64ceb46f82bc9ce948f") --Main Windows Depot The Lost Village
+setManifestid(1963042, "5729363257035477947", 4736772144)
+--Dlcname TheLostVillage - 扩展包
+addtoken(2850340, "13026027230159238598")
+addappid(2850340, 1, "423f4ec9aa4edff9616f9a065bf6d4a7b3a50e7a5989eca2eba1fe2e8c232af2") --Dlcname TheLostVillage - 扩展包
+setManifestid(2850340, "3581378244241433097", 69265456)
+--Dlcname TheLostVillage-我的江湖扩展包
+addtoken(2917210, "4197742415902708585")
+addappid(2917210, 1, "bdeb1eccedd69002ef23f347d2ef3b65a79887c96a4996cbe1fe7c53a79c2c2f") --Dlcname TheLostVillage-我的江湖扩展包
+setManifestid(2917210, "4002384482655671079", 2474096)
+--Dlcname TheLostVillage-万物有灵扩展包
+addtoken(3227480, "16129377367153834198")
+addappid(3227480, 1, "ba1dbc2707196bda0ba7d451b6a81412ed21c6d4f581d5c411674292da812ef3") --Dlcname TheLostVillage-万物有灵扩展包
+setManifestid(3227480, "3971885926465697198", 217936)
+--Dlcname TheLostVillage-异族再起扩展包
+addtoken(3348000, "10419493762087925175")
+addappid(3348000, 1, "137af1b6a775c42a78e9b1386d97ed46074f1296114ef4983770b48ab2b7653a") --Dlcname TheLostVillage-异族再起扩展包
+setManifestid(3348000, "3423465725393360040", 253840)
+--Dlcname 山门与幻境 Soundtrack
+addappid(3532770) --Dlcname 山门与幻境 Soundtrack
+addappid(3532771, 1, "e19cb41cee301fbec021ad10d5e6621e16cda119a906633c2cc16ddeef57344e") --Dlc Windows Depot 山门与幻境 Soundtrack
+setManifestid(3532771, "2260432553433671948", 62169776)
+addappid(3532772, 1, "31a9c5b573fb849cd644bc04e57107411d996b4ab4864406881d11c9c9ed25f1") --Dlc Windows Depot 山门与幻境 Soundtrack
+setManifestid(3532772, "2568639386340840861", 793680336)
+--Dlcname TheLostVillage-绝世宗门扩展包
+addtoken(3600860, "17269303264250646776")
+addappid(3600860, 1, "0a7688ca5818fb37401d2e52695c01778e352644ff6e5f37c434fbc42bb93784") --Dlcname TheLostVillage-绝世宗门扩展包
+setManifestid(3600860, "3557495147778647459", 1588800)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229012, "4353723233161159493", 6387264)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
+addappid(229030, 1, "92f0f88867c8d5f351090eb2d27aeb50e2b621ae8a34708265d2c20c36fbcf5e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229030, "1043465440436835055", 50071472)
