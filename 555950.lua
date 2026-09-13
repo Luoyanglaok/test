c8d0f995a9283d2c52a303884a4f5a02f3835276
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 13:06:05 +0800

    add 555950.lua

--- 555950.lua
+++ 555950.lua
@@ -1,10 +1,15 @@
-addappid(555950)
-setManifestid(228990,"1829726630299308803")
-addappid(555952,0,"2e9a60957d79f2cc3fbdfcf4b2a852364493e626daef3d3b4f702c24e2d305a0")
-setManifestid(555952,"3451890528442011371")
-addappid(555951,0,"372a12a1d96aa1014642e4fe55b82a77887e22246cf3266402e7096ef3aae5b5")
-setManifestid(555951,"9114703985677743048")
-addappid(555953,0,"3d5b471314018f6a9a16c0d4ec7b98ccb93fbe70bbf4626bf43e60c1323ac4cc")
-setManifestid(555953,"6936498450357279184")
-addappid(555954,0,"021aca3dfcf2206b2a4ed988642fcb09d11eea690e48d4abed4c508493bbfcf3")
-setManifestid(555954,"8421517668918607840")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 555950.lua
+--Gamename Danganronpa Another Episode: Ultra Despair Girls
+addappid(555950, 1, "70035acd4ae49f40af62258f989d3589db459ac91741109d82bc550f8b43b31c") --Mainappid Danganronpa Another Episode: Ultra Despair Girls
+addappid(555951, 1, "372a12a1d96aa1014642e4fe55b82a77887e22246cf3266402e7096ef3aae5b5") --Main Windows Language Depot Japanese Danganronpa Another Episode: Ultra Despair Girls
+setManifestid(555951, "9114703985677743048", 9068847600)
+addappid(555952, 1, "2e9a60957d79f2cc3fbdfcf4b2a852364493e626daef3d3b4f702c24e2d305a0") --Main Windows Language Depot English Danganronpa Another Episode: Ultra Despair Girls
+setManifestid(555952, "3451890528442011371", 15401190208)
+addappid(555953, 1, "3d5b471314018f6a9a16c0d4ec7b98ccb93fbe70bbf4626bf43e60c1323ac4cc") --Main Windows Language Depot Tchinese Danganronpa Another Episode: Ultra Despair Girls
+setManifestid(555953, "6936498450357279184", 9319385232)
+addappid(555954, 1, "021aca3dfcf2206b2a4ed988642fcb09d11eea690e48d4abed4c508493bbfcf3") --Main Windows Language Depot Koreana Danganronpa Another Episode: Ultra Despair Girls
+setManifestid(555954, "8421517668918607840", 9307905776)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
