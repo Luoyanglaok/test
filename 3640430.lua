ed8ead05ae5c20217d1a4321f26750bda16dc7ff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 10:37:00 +0800

    add 3640430.lua

---
+++ 3640430.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3640430.lua
+--Gamename Worlds Upon The Wind
+addappid(3640430, 1, "a406b3666a35788bbe6a90c5c5ab30c98e2f5e8caea9e5f87ee801e57fafd39e") --Mainappid Worlds Upon The Wind
+addappid(3640431, 1, "00c2e1366be59bc370d7b5026522b2fac3cbb1fa513a24d3a553f06342bbadbd") --Main Windows Depot Worlds Upon The Wind
+setManifestid(3640431, "2844801531372396948", 2577481120)
+addappid(3640432, 1, "1925060ea9c61bc63adcea517b6506173811371250e30923f793f1ddcc864473") --Main Linux Depot Worlds Upon The Wind
+setManifestid(3640432, "4252424752803258145", 2572086800)
+addappid(3640433, 1, "2edc25a33aadafda0b997ca8471a82fcb5c063c6143578d9cac802d32db54525") --Main Macos Depot Worlds Upon The Wind
+setManifestid(3640433, "5149210312810641549", 2606526128)
