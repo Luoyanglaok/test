1e4d9576901fd04acb8e8249198a38ed961e41d4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 08:48:42 +0800

    add 2833580.lua

---
+++ 2833580.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2833580.lua
+--Gamename BRAVELY DEFAULT FLYING FAIRY HD Remaster
+addappid(2833580, 1, "b03ec9f374b8fa512d685bee50b2a4ccb2deac206c71f406400d3ab8d8cd9123") --Mainappid BRAVELY DEFAULT FLYING FAIRY HD Remaster
+addappid(2833581, 1, "5ff21e6a3f7dfdd86e8ed49a695b5c520086a36bb2f76d190222a18d2fda5f96") --Main Windows Depot BRAVELY DEFAULT FLYING FAIRY HD Remaster
+setManifestid(2833581, "4882646503320637764", 8648403712)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
