7f8f81cf283c06ae8bf57e1a728ad56c870360ed
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 07:26:48 +0800

    add 3498270.lua

--- 3498270.lua
+++ 3498270.lua
@@ -1,5 +1,11 @@
-addappid(3498270)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(3498271,0,"f9a8672f59ce9964e872dfb6e6bfaff3baa16dad3ddc7f2b2fb12bd5b1e66ad3")
-setManifestid(3498271,"1457586427043927570")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3498270.lua
+--Gamename Better Mart Simulator
+addappid(3498270, 1, "9eca33cf6dc5d3734b39f7e936555526020b0d3a669250534e422d7d10d8a999") --Mainappid Better Mart Simulator
+addappid(3498271, 1, "f9a8672f59ce9964e872dfb6e6bfaff3baa16dad3ddc7f2b2fb12bd5b1e66ad3") --Main Windows Depot Better Mart Simulator
+setManifestid(3498271, "6088298367273174741", 5556573200)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
