ce78447d52ef1af69b530e09f0919e6395365e64
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 19:08:40 +0800

    add 1311700.lua

--- 1311700.lua
+++ 1311700.lua
@@ -1,36 +1,108 @@
-addappid(1311700)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1311701,0,"26e1b002b8fa347b38c1e63240641b748d755290a8208c14a9b4cc012dd275bc")
-setManifestid(1311701,"3662394563886754966")
-addappid(1414540,0,"e5e127e2a851a0be5184502cbc6c8193ef1aa37f9de8e60ce895da07c6b15dc4")
-setManifestid(1414540,"6796435408167361766")
-addappid(1417810,0,"e62faba73d461c829c8b0567a69ec1b8a60510bb8261c4a12f94746c1427e43a")
-setManifestid(1417810,"5692429604758009249")
-addappid(1418710,0,"76e27cd04c5a10f0e09e3eca66ea690f0c1fcfc1cd8d17adfe41746955f18c96")
-setManifestid(1418710,"2863427958183994901")
-addappid(2419400,0,"5f2301211980a01874bc242813bc3a43a4c2f3b079dd8fa480e881b21b23a6a9")
-setManifestid(2419400,"4592640076181216830")
-addappid(2509760,0,"7cef29d59a4b73a340de8a6e6c2f354df45764ab4eb58aea0e8afd9ca13f294f")
-setManifestid(2509760,"4855984171804785535")
-addappid(2509770,0,"7ae435292928f397b0466646406b5d328e737c6c9925a891aa87753ea6567bd1")
-setManifestid(2509770,"5670267496673178614")
-addappid(3095470,0,"d1b1b99c5df4468df638f2e74fcf50828b9b5337837a0fb823c786aa56830985")
-setManifestid(3095470,"3872302842777983444")
-addappid(3594630,0,"c2a0d1087f283ad5d35a68cb69b55bffa794b829529ae5dd28a5829d971c2ed6")
-setManifestid(3594630,"6114578488581410897")
-addappid(3594640,0,"7878d873a46f3dd6cc6ef3bb28dff171197ae3f226dc43d319a8bc6beadbbc9a")
-setManifestid(3594640,"3862457746359805261")
-addappid(3607760,0,"9ee3fd63156c19fa17a563b7e34f8dcc304cffde7e721bbec54469947c9d56c8")
-setManifestid(3607760,"310324433665678485")
-addappid(3615570,0,"b676cdc325705829036088bcce255efd28958577890a25053b3aae83596b532c")
-setManifestid(3615570,"2879675458631472167")
-addappid(3615930,0,"d15496b60ed129484df1d7945826c99f00277b163584b5d8c29d9ea53af644d8")
-setManifestid(3615930,"3093490433548909446")
-addappid(3638460,0,"cdcd00117f8002c4439291b9b7170b07d598a53a4ff0bfa983f230bbac4918df")
-setManifestid(3638460,"8885020991214573060")
-addappid(3638470,0,"970438c1ca0c114d328d3fbbf6aebfcb45a4c20b6d3d113ed20533078bb734ba")
-setManifestid(3638470,"4993256269102892597")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1311700.lua
+--Gamename 汉武大帝传-经典策略战棋
+addappid(1311700, 1, "d4076fbf50f704241dc1abd87d4911d6cfba470aeb267f7f556dc8a7ac780da3") --Mainappid 汉武大帝传-经典策略战棋
+addappid(1311701, 1, "26e1b002b8fa347b38c1e63240641b748d755290a8208c14a9b4cc012dd275bc") --Main Windows Depot 汉武大帝传-经典策略战棋
+setManifestid(1311701, "5827054865915147035", 205709472)
+--Dlcname 汉武大帝传 - 内容追加福利大礼包（最高性价比）
+addappid(1414540, 1, "e5e127e2a851a0be5184502cbc6c8193ef1aa37f9de8e60ce895da07c6b15dc4") --Dlcname 汉武大帝传 - 内容追加福利大礼包（最高性价比）
+setManifestid(1414540, "7401269068339507217", 194361568)
+--Dlcname （非必需购买项）神龙天御礼包
+addappid(1417810, 1, "e62faba73d461c829c8b0567a69ec1b8a60510bb8261c4a12f94746c1427e43a") --Dlcname （非必需购买项）神龙天御礼包
+setManifestid(1417810, "5692429604758009249", 27760)
+--Dlcname 汉武大帝传-盖世英雄礼包（最具价值-五星DLC）
+addappid(1418710, 1, "76e27cd04c5a10f0e09e3eca66ea690f0c1fcfc1cd8d17adfe41746955f18c96") --Dlcname 汉武大帝传-盖世英雄礼包（最具价值-五星DLC）
+setManifestid(1418710, "2863427958183994901", 23856)
+--Dlcname 汉武大帝传-奖励
+addappid(2419400, 1, "5f2301211980a01874bc242813bc3a43a4c2f3b079dd8fa480e881b21b23a6a9") --Dlcname 汉武大帝传-奖励
+setManifestid(2419400, "4592640076181216830", 8480)
+--Dlcname 汉武大帝传-御·帝尧套装
+addappid(2509750, 1, "159dee3cdeb01eeb8e5e7dc3ebce5fac3640af5aaf869b646e34185136dd7a69") --Dlcname 汉武大帝传-御·帝尧套装
+setManifestid(2509750, "8656812437192293338", 17184)
+--Dlcname 汉武大帝传-(新人限量福利)军神心法礼包
+addappid(2509760, 1, "7cef29d59a4b73a340de8a6e6c2f354df45764ab4eb58aea0e8afd9ca13f294f") --Dlcname 汉武大帝传-(新人限量福利)军神心法礼包
+setManifestid(2509760, "4855984171804785535", 17184)
+--Dlcname 汉武大帝传-凶萌兽宠礼包（新手必备）
+addappid(2509770, 1, "7ae435292928f397b0466646406b5d328e737c6c9925a891aa87753ea6567bd1") --Dlcname 汉武大帝传-凶萌兽宠礼包（新手必备）
+setManifestid(2509770, "5670267496673178614", 17184)
+--Dlcname 汉武大帝传-超级DLC合集
+addappid(2524840, 1, "f6295857aaf39fe564387b38c511f1d81aab4bf278d5c4963f630129245a0152") --Dlcname 汉武大帝传-超级DLC合集
+setManifestid(2524840, "8449604875137942949", 17184)
+--Dlcname 汉武大帝传-鬼神莫测DLC
+addappid(3095470, 1, "d1b1b99c5df4468df638f2e74fcf50828b9b5337837a0fb823c786aa56830985") --Dlcname 汉武大帝传-鬼神莫测DLC
+setManifestid(3095470, "3872302842777983444", 17184)
+--Dlcname 汉武- 女将备用DLC2
+addappid(3594630, 1, "c2a0d1087f283ad5d35a68cb69b55bffa794b829529ae5dd28a5829d971c2ed6") --Dlcname 汉武- 女将备用DLC2
+setManifestid(3594630, "6114578488581410897", 20416)
+--Dlcname 汉武- 女将备用DLC3
+addappid(3594640, 1, "7878d873a46f3dd6cc6ef3bb28dff171197ae3f226dc43d319a8bc6beadbbc9a") --Dlcname 汉武- 女将备用DLC3
+setManifestid(3594640, "3862457746359805261", 17792)
+--Dlcname 汉武- 女将备用DLC4
+addappid(3607760, 1, "9ee3fd63156c19fa17a563b7e34f8dcc304cffde7e721bbec54469947c9d56c8") --Dlcname 汉武- 女将备用DLC4
+setManifestid(3607760, "310324433665678485", 20400)
+--Dlcname 汉武大帝传-绝代佳人DLC
+addappid(3615570, 1, "b676cdc325705829036088bcce255efd28958577890a25053b3aae83596b532c") --Dlcname 汉武大帝传-绝代佳人DLC
+setManifestid(3615570, "2879675458631472167", 17184)
+--Dlcname 汉武- 女将备用DLC1
+addappid(3615930, 1, "d15496b60ed129484df1d7945826c99f00277b163584b5d8c29d9ea53af644d8") --Dlcname 汉武- 女将备用DLC1
+setManifestid(3615930, "3093490433548909446", 20400)
+--Dlcname 汉武专用定制DLC-D度空间新（非卖品）
+addappid(3638460, 1, "cdcd00117f8002c4439291b9b7170b07d598a53a4ff0bfa983f230bbac4918df") --Dlcname 汉武专用定制DLC-D度空间新（非卖品）
+setManifestid(3638460, "8885020991214573060", 38224)
+--Dlcname 汉武专用定制DLC-Jack新（非卖品）
+addappid(3638470, 1, "970438c1ca0c114d328d3fbbf6aebfcb45a4c20b6d3d113ed20533078bb734ba") --Dlcname 汉武专用定制DLC-Jack新（非卖品）
+setManifestid(3638470, "4993256269102892597", 30864)
+--Dlcname 汉武大帝传-备用定制DLC1
+addappid(3679640, 1, "9fcafdc6c0d7ab39ab812fe6aba13043ea2247322ff17737122eb85d46701052") --Dlcname 汉武大帝传-备用定制DLC1
+setManifestid(3679640, "2888409000940348658", 42352)
+--Dlcname 汉武大帝传-虞姬专属飒装DLC
+addappid(3762850, 1, "1520a3ec7e3e28d09347418338f5404c780272c52d9244e613774a76f6f2977b") --Dlcname 汉武大帝传-虞姬专属飒装DLC
+setManifestid(3762850, "6647076096344503168", 31088)
+--Dlcname 汉武大帝传-月氏女王专属飒装DLC
+addappid(3762860, 1, "d0155d7d22bd388d1391307233e9c65c9b7698f0dad5af86d8eed173fd19c7ab") --Dlcname 汉武大帝传-月氏女王专属飒装DLC
+setManifestid(3762860, "7895779001958916666", 31088)
+--Dlcname 汉武大帝传-项羽专属霸装DLC
+addappid(3835310, 1, "42bcce27174d1be44d83ff06dde38bb623223869938237f4828b8b9ec6933c47") --Dlcname 汉武大帝传-项羽专属霸装DLC
+setManifestid(3835310, "1824135367291067063", 37280)
+--Dlcname 汉武大帝传-平阳公主专属飒装DLC
+addappid(3835320, 1, "84ef98a862aaf0232cdcad0d6de867cc5f073981a5bb420fcf246c00c245c4f0") --Dlcname 汉武大帝传-平阳公主专属飒装DLC
+setManifestid(3835320, "8740969425609941253", 37280)
+--Dlcname 汉武大帝传-卓文君专属飒装DLC
+addappid(3926780, 1, "7b2aa67b601cc95af26f52bbde234cb059d396b71c9bad2aecd214d64b0a95da") --Dlcname 汉武大帝传-卓文君专属飒装DLC
+setManifestid(3926780, "5706033075185918493", 39328)
+--Dlcname 汉武大帝传-刘陵专属飒装DLC
+addappid(3926790, 1, "8c46e4edd5ec6cbc364b23a409a6bc0fc87afd0f3b086d2344fa221e8cdb03f2") --Dlcname 汉武大帝传-刘陵专属飒装DLC
+setManifestid(3926790, "1589042410187676595", 39328)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname 汉武大帝传-神之秘术礼包
+--Dlcname 汉武大帝传-国士无双礼包
+--Dlcname 汉武大帝传-宿命之地（超级副本）DLC
+--Dlcname 汉武大帝传-仙丹盛宴第一弹（限购1次）
+--Dlcname 汉武大帝传-仙丹盛宴第二弹（限购1次）
+--Dlcname 汉武大帝传-仙丹盛宴第三弹（限购1次）
+--Dlcname 汉武大帝传-仙丹盛宴超级合集（限购1次）
+--Dlcname 汉帝-定制迁移预留1-D（非卖品）
+--Dlcname 汉帝-定制迁移预留2-J（非卖品）
+--Dlcname 汉武大帝传-仙丹盛宴超级合集二（限购1次）
+--Dlcname 汉武大帝传-新手超值补给包（策略战棋）
+--Dlcname 汉武大帝传-史诗级特技书 · 吸血攻击60%（非必购项）
+--Dlcname 汉武大帝传-史诗级特技书 · 穿透6人（非必购项）
+--Dlcname 汉武大帝传-史诗级特技书 · 一夫当关30%（非必购项）
+--Dlcname 汉武大帝传-定制DLC2叛逆的猴子
+--Dlcname 汉武大帝传-经典策略战棋 - 钩弋夫人专属飒装DLC
+--Dlcname 汉武大帝传-李夫人专属飒装DLC
+--Dlcname 汉武大帝传-倚华专属飒装DLC
+--Dlcname 汉武大帝传-2026新春至尊典藏包
+--Dlcname 汉武大帝传-金马贺岁礼包
+addappid(4205930) --Dlcname AppID 4205930
