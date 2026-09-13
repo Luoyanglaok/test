32eea6e69cfa47dfcdd3518080e2d549cc4e67d6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 12:55:27 +0800

    add 413420.lua

--- 413420.lua
+++ 413420.lua
@@ -1,17 +1,27 @@
-addappid(413420)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-setManifestid(229001,"4049573910112143457")
-addappid(413421,0,"65dd2d598e63ea5ac5c2ccd8a31a71ccb73169f4117c991fa40a8d0fa7f02bdb")
-setManifestid(413421,"865716286440512742")
-addappid(413422,0,"a315983577505ccbbff926c252bc06912f1487dd68b7cef7f9eb420c5d199944")
-setManifestid(413422,"73890315654914570")
-addappid(413423,0,"eda8cf7512bf7cc489c2bfa82878b18813f77f593e926059b5f709cafa6d8fcf")
-setManifestid(413423,"4176999508874191731")
-addappid(413424,0,"9477631711d08c528c8b5d021afdac892141f0fdd9228157884aaa2c128862fb")
-setManifestid(413424,"2944879051336645197")
-addappid(413425,0,"9e198ebff66a43ab19b0ea470fb54f3d13df1c07c50165ade299b54a3848b44d")
-setManifestid(413425,"3641548101996708686")
-addappid(413426,0,"ac7959cd7d6badec27457439489adf9c8c3803afd22b9ebc331196ded49b3d57")
-setManifestid(413426,"2621488385565004742")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 413420.lua
+--Gamename Danganronpa 2: Goodbye Despair
+addappid(413420, 1, "f40c412d51bbfcb3f018d7d7c0cfcf9a110a75d141146babfb389d4e06610516") --Mainappid Danganronpa 2: Goodbye Despair
+addappid(413421, 1, "65dd2d598e63ea5ac5c2ccd8a31a71ccb73169f4117c991fa40a8d0fa7f02bdb") --Main Windows Depot Danganronpa 2: Goodbye Despair
+setManifestid(413421, "865716286440512742", 2155713376)
+addappid(413422, 1, "a315983577505ccbbff926c252bc06912f1487dd68b7cef7f9eb420c5d199944") --Main Macos Depot Danganronpa 2: Goodbye Despair
+setManifestid(413422, "73890315654914570", 2799836000)
+addappid(413423, 1, "eda8cf7512bf7cc489c2bfa82878b18813f77f593e926059b5f709cafa6d8fcf") --Main Linux Depot Danganronpa 2: Goodbye Despair
+setManifestid(413423, "4176999508874191731", 2165837968)
+addappid(413425, 1, "9e198ebff66a43ab19b0ea470fb54f3d13df1c07c50165ade299b54a3848b44d") --Main Windows Language Depot Japanese Danganronpa 2: Goodbye Despair
+setManifestid(413425, "3641548101996708686", 750409568)
+addappid(413426, 1, "ac7959cd7d6badec27457439489adf9c8c3803afd22b9ebc331196ded49b3d57") --Main Windows Language Depot Tchinese Danganronpa 2: Goodbye Despair
+setManifestid(413426, "2621488385565004742", 758318544)
+--Dlcname Danganronpa 2: Goodbye Despair Mini-OST
+addappid(453490) --Dlcname Danganronpa 2: Goodbye Despair Mini-OST
+addappid(413424, 1, "9477631711d08c528c8b5d021afdac892141f0fdd9228157884aaa2c128862fb") --Dlc Windows Depot Danganronpa 2: Goodbye Despair Mini-OST
+setManifestid(413424, "2944879051336645197", 274468560)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229001, 1, "ec23e1a1e1da6205722291f91bf72d1989cb347245e5a14d8d514c4ad5ba4e97") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229001, "4049573910112143457", 268006912)
