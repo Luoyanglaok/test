be5fe3706dfde162ecb67d16fce3e42b3da88c1c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 12:47:29 +0800

    add 4134650.lua

---
+++ 4134650.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4134650.lua
+--Gamename Bones and Coins
+addappid(4134650) --Mainappid Bones and Coins
+addappid(4134651, 1, "3354b7ae17e8d332ff240b9a165d5460c4981f7d2b7aa1838108d894b1b38073") --Main Windows Depot Bones and Coins
+setManifestid(4134651, "1067726486105043532", 180579584)
