96d4edf20e859ee6b3a142f7346b0ad71e65b4d3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 08:13:08 +0800

    add 825630.lua

--- 825630.lua
+++ 825630.lua
@@ -1,12 +1,17 @@
-addappid(825630)
-setManifestid(228990,"1829726630299308803")
-addappid(825631,0,"7272d6df7ae168e82d805f977c206d71014577137fdbee034e37d72263bf7356")
-setManifestid(825631,"6416888918427989549")
-addappid(825632,0,"57b3859a5400960dfd2f28cde339bc763997eb76e5020644ec5760d0e836bca3")
-setManifestid(825632,"29171768538633351")
-addappid(825633,0,"6e52df2d665a0a86727a0ac82152d4d3a29fb09e9d1331bd644fc1d7b6bfeb62")
-setManifestid(825633,"989290602639422364")
-addappid(825634,0,"6a322f897cfdd28ab04750b2547f554513251aa769ff828723c40e8907a241aa")
-setManifestid(825634,"934975517331353754")
-addappid(825635,0,"efcbbd8adef0b300f121cb3e2dc2981b0384bbb5108525c6e66a7e3b4126290c")
-setManifestid(825635,"9197845446988398808")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 825630.lua
+--Gamename STEINS;GATE 0
+addappid(825630, 1, "29c3b0fab8d4a1b214c066a9ddca0b92465e592eb3faa9f7f7c4c3723b86d49d") --Mainappid STEINS;GATE 0
+addappid(825631, 1, "7272d6df7ae168e82d805f977c206d71014577137fdbee034e37d72263bf7356") --Main Windows Depot STEINS;GATE 0
+setManifestid(825631, "6416888918427989549", 6914959968)
+addappid(825632, 1, "57b3859a5400960dfd2f28cde339bc763997eb76e5020644ec5760d0e836bca3") --Main Windows Language Depot English STEINS;GATE 0
+setManifestid(825632, "29171768538633351", 6914959968)
+addappid(825633, 1, "6e52df2d665a0a86727a0ac82152d4d3a29fb09e9d1331bd644fc1d7b6bfeb62") --Main Windows Language Depot Japanese STEINS;GATE 0
+setManifestid(825633, "989290602639422364", 6894990032)
+addappid(825634, 1, "6a322f897cfdd28ab04750b2547f554513251aa769ff828723c40e8907a241aa") --Main Windows Language Depot Tchinese STEINS;GATE 0
+setManifestid(825634, "934975517331353754", 6990098944)
+addappid(825635, 1, "efcbbd8adef0b300f121cb3e2dc2981b0384bbb5108525c6e66a7e3b4126290c") --Main Windows Language Depot Schinese STEINS;GATE 0
+setManifestid(825635, "9197845446988398808", 6989627984)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
