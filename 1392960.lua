21a03bae9ee40c775989f49e4ea4b67ada8352b5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 15:46:23 +0800

    add 1392960.lua

--- 1392960.lua
+++ 1392960.lua
@@ -1,6 +1,28 @@
-addappid(1392960)
-setManifestid(229005,"7992454656023763365")
-addappid(1392961,0,"7929038c4f0eecc945a4c729b402a052d67647fe9b5ebf6a8a9e62900ad0d621")
-setManifestid(1392961,"2545591170410592883")
-addappid(1392962,0,"59af10455ece1d3b11e9be890adf7127588ed728d20a8a62763ec27a6b513598")
-setManifestid(1392962,"330912230820413784")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1392960.lua
+--Gamename STORY OF SEASONS: Pioneers of Olive Town
+addappid(1392960) --Mainappid STORY OF SEASONS: Pioneers of Olive Town
+addappid(1392961, 1, "7929038c4f0eecc945a4c729b402a052d67647fe9b5ebf6a8a9e62900ad0d621") --Main Windows Depot STORY OF SEASONS: Pioneers of Olive Town
+setManifestid(1392961, "3589441553948795944", 1015872464)
+addappid(1392962, 1, "59af10455ece1d3b11e9be890adf7127588ed728d20a8a62763ec27a6b513598") --Main Windows Depot STORY OF SEASONS: Pioneers of Olive Town
+setManifestid(1392962, "330912230820413784", 440912)
+--Dlcname STORY OF SEASONS: Pioneers of Olive Town - Expansion Pass
+addappid(1567290, 1, "6fcc8cf39f5b1e1d7ad584df0bdeb8130e4619bc79964cde3ff93311d208ee76") --Dlcname STORY OF SEASONS: Pioneers of Olive Town - Expansion Pass
+setManifestid(1567290, "8988459350891342259", 174888992)
+--Dlcname STORY OF SEASONS: Pioneers of Olive Town - Buffalo Costume
+addappid(1567291, 1, "0a7133ecbc4404f93f5eba4e17f9dfd37c9b89b970477a118fbc5a6e46895d4c") --Dlcname STORY OF SEASONS: Pioneers of Olive Town - Buffalo Costume
+setManifestid(1567291, "2616295593368494655", 436480)
+--Dlcname STORY OF SEASONS: Pioneers of Olive Town - Fox Costume
+addappid(1567292, 1, "883865e1f6436d2e6c7d7961ca1f63d5cb36e9209e3026781236d55f18961216") --Dlcname STORY OF SEASONS: Pioneers of Olive Town - Fox Costume
+setManifestid(1567292, "6875697304556710042", 553184)
+--Dlcname STORY OF SEASONS: Pioneers of Olive Town - Neil's Jacket
+addappid(1567293, 1, "112b71b1b5c076a4c228c552afdc6989a44337cbbb060c172699e489b42f16d4") --Dlcname STORY OF SEASONS: Pioneers of Olive Town - Neil's Jacket
+setManifestid(1567293, "1864083631730606019", 1085392)
+--Dlcname AppID 1567294
+addappid(1567294, 1, "fea36c9f627298d47f12b1e0a95f65246942c71357fdcdaa9df4c16a29177bb2") --Dlcname AppID 1567294
+--Dlcname STORY OF SEASONS: Pioneers of Olive Town - Panda Costume
+addappid(1692490, 1, "6cada7b57fc4a593853d1b8ca2dfe61551955f18ffcdf8c3e6c2a6dba64b77de") --Dlcname STORY OF SEASONS: Pioneers of Olive Town - Panda Costume
+setManifestid(1692490, "5945672132180549946", 444096)
+--Share Depots
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
