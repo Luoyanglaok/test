9f07231e26867fe7fc25a65b93d21a5ac69052ef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 18:36:10 +0800

    add 2744880.lua

--- 2744880.lua
+++ 2744880.lua
@@ -1,3 +1,9 @@
-addappid(2744880)
-addappid(2744881,0,"9fd7118ee51abbec444753ac08e8c7b7f6a304d646e13948d07333d6881aa58a")
-setManifestid(2744881,"6753953480199824698")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2744880.lua
+--Gamename Climber Animals: Together
+addappid(2744880) --Mainappid Climber Animals: Together
+addappid(2744881, 1, "9fd7118ee51abbec444753ac08e8c7b7f6a304d646e13948d07333d6881aa58a") --Main Windows Depot Climber Animals: Together
+setManifestid(2744881, "3220279233571548313", 377710048)
+--Dlcname Climber Animals Together : Asian Pack
+addappid(4656990, 1, "c2255ec3ed1309fe69a00523a771253025e2fc490935bbdb37ae3429bfae5b46") --Dlcname Climber Animals Together : Asian Pack
+setManifestid(4656990, "6055406831008129681", 383055312)
