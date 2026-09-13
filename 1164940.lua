baee6f49297c3f4e4c1b1294a3f358094ff2430e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 18:58:18 +0800

    add 1164940.lua

--- 1164940.lua
+++ 1164940.lua
@@ -1,5 +1,18 @@
-addappid(1164940)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1164944,0,"e7cf384fd9cc9adec7e85fe5751ed3bb5fe89e2cf1c3c1135df0dbd421580c3c")
-setManifestid(1164944,"2586365741953940983")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1164940.lua
+--Gamename Trepang2
+addappid(1164940, 1, "1680cafaa35e3e03a0f585cbc57232a0452a5c2c43d8ef0f8cf4a4b792abf699") --Mainappid Trepang2
+addappid(1164944, 1, "e7cf384fd9cc9adec7e85fe5751ed3bb5fe89e2cf1c3c1135df0dbd421580c3c") --Main Windows Depot Trepang2
+setManifestid(1164944, "2586365741953940983", 15395539056)
+--Dlcname Trepang2 - Soundtrack
+addappid(1842620) --Dlcname Trepang2 - Soundtrack
+addappid(1842621, 1, "e381094a9a17f36b1ce9e1040f5bb5ec19b56dc6a59b75473d932693480fbf31") --Dlc Windows Depot Trepang2 - Soundtrack
+setManifestid(1842621, "4733792379017130551", 2565629312)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2615910) --Dlcname Trepang2 - Survival Mode DLC
+addappid(2615920) --Dlcname Trepang2 - Bladekisser DLC
+addappid(2656880) --Dlcname Trepang2 - Season Pass
