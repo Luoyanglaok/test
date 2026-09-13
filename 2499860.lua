1d00d7414ae5cbb0733268eb86c50636c7b637e0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 18:09:24 +0800

    add 2499860.lua

--- 2499860.lua
+++ 2499860.lua
@@ -2,6 +2,7 @@
 -- Original file: 2499860.lua
 --Gamename DRAGON QUEST VII Reimagined
 addappid(2499860, 1, "1e58a20f0aa0e0d214a207103d0d8042cd3d432a0ecf06e62a381366aa724dbe") --Mainappid DRAGON QUEST VII Reimagined
+addtoken(2499860, "8549649642535805549")
 addappid(2499861, 1, "78d85bb7957e80d4fac0655a65ff9bee61dafc06c753de2713726e9860d6a860") --Main Windows Depot DRAGON QUEST VII Reimagined
 setManifestid(2499861, "6283106044578104469", 6585212992)
 --Share Depots
