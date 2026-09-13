88324d298fce17999d2f3674164267f6ad74df51
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 15:44:46 +0800

    add 1995890.lua

--- 1995890.lua
+++ 1995890.lua
@@ -1,17 +1,24 @@
-addappid(1995890)
-addappid(1995891,0,"6b3fc84d7a24d7f4f5800f84e73fc89f7275ff8bfa94b69a314bbbe76ac6d0aa")
-setManifestid(1995891,"3438272076824159257")
-addappid(1995892,0,"c9b76fb78a8d79befbc40588f17cc0a75f7e285632757b8e95896d92d8da32fd")
-setManifestid(1995892,"932312921450931410")
-addappid(1995893,0,"4473da7c62418bbac093942a6089fa73c8a5eb1d51eee20e35668cf1a2346134")
-setManifestid(1995893,"6364645736988201203")
-addappid(1995894,0,"f43b4945fa95abbeea9a3ddeccbcbff1b482820b371e4eaa6d47ddc070d86060")
-setManifestid(1995894,"8672151080057663479")
-addappid(2011820,0,"a3aafe378aacd12baecdc6110da2ccb468a4d6b811baa533b4e45b2a414746e2")
-setManifestid(2011820,"6593923523394007235")
-addappid(2011821,0,"63de99f8ff14b6e07d0222b0ad3f695b5de557160e985d6d2788d1e47c64f417")
-setManifestid(2011821,"7132256737695010488")
-addappid(2011823,0,"216bb04f7293134916c04d64440c4196d6c6554b9ad796ac8052e8f3bffdaef0")
-setManifestid(2011823,"1942695654996373341")
-addappid(2011824,0,"c56b60b3da1ca6c3e381453f98850e96ea4050ef93950d15c11db2135c629629")
-setManifestid(2011824,"8426134100309667358")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1995890.lua
+--Gamename Aerofly FS 4 Flight Simulator
+addappid(1995890, 1, "8de44cb9960616ea8c82ca76135670398d985d46fc69084e2a4d3c6dd73f1a04") --Mainappid Aerofly FS 4 Flight Simulator
+addappid(1995891, 1, "6b3fc84d7a24d7f4f5800f84e73fc89f7275ff8bfa94b69a314bbbe76ac6d0aa") --Main Windows Depot Aerofly FS 4 Flight Simulator
+setManifestid(1995891, "7661515345289955934", 63469227632)
+addappid(1995892, 1, "c9b76fb78a8d79befbc40588f17cc0a75f7e285632757b8e95896d92d8da32fd") --Main Windows Depot Aerofly FS 4 Flight Simulator
+setManifestid(1995892, "5396105394873160581", 24303024)
+addappid(1995893, 1, "4473da7c62418bbac093942a6089fa73c8a5eb1d51eee20e35668cf1a2346134") --Main Linux Depot Aerofly FS 4 Flight Simulator
+setManifestid(1995893, "4705618611780720166", 9065200)
+addappid(1995894, 1, "f43b4945fa95abbeea9a3ddeccbcbff1b482820b371e4eaa6d47ddc070d86060") --Main Macos Depot Aerofly FS 4 Flight Simulator
+setManifestid(1995894, "7386951162297146338", 21001392)
+--Dlcname Aerofly FS 4 Flight Simulator - Aircraft AddOn
+addappid(2011820, 1, "a3aafe378aacd12baecdc6110da2ccb468a4d6b811baa533b4e45b2a414746e2") --Dlcname Aerofly FS 4 Flight Simulator - Aircraft AddOn
+setManifestid(2011820, "949036774004341235", 3592175312)
+--Dlcname Aerofly FS 4 - Scenery: USA high resolution aerial images Part 1
+addappid(2011821, 1, "63de99f8ff14b6e07d0222b0ad3f695b5de557160e985d6d2788d1e47c64f417") --Dlcname Aerofly FS 4 - Scenery: USA high resolution aerial images Part 1
+setManifestid(2011821, "7132256737695010488", 72077594752)
+--Dlcname Aerofly FS 4 - Scenery: Europe Part 1
+addappid(2011823, 1, "216bb04f7293134916c04d64440c4196d6c6554b9ad796ac8052e8f3bffdaef0") --Dlcname Aerofly FS 4 - Scenery: Europe Part 1
+setManifestid(2011823, "3531773450017850175", 57309602960)
+--Dlcname Aerofly FS 4 - Scenery: Europe Part 2
+addappid(2011824, 1, "c56b60b3da1ca6c3e381453f98850e96ea4050ef93950d15c11db2135c629629") --Dlcname Aerofly FS 4 - Scenery: Europe Part 2
+setManifestid(2011824, "7227891170105556256", 44015607136)
