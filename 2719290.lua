8acdcb3eede4bae8a85401a4fcb3b9b9ed769697
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 04:51:30 +0800

    add 2719290.lua

--- 2719290.lua
+++ 2719290.lua
@@ -1,6 +1,2 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 2719290.lua
---Gamename Night Raider
-addappid(2719290) --Mainappid Night Raider
-addappid(2719291, 1, "3a2948db3b7201c3bdd69857dec4c6c844c1505dc1d3ff59cd05e2cdebb3f832") --Main Windows Depot Night Raider
-setManifestid(2719291, "7643714319915551536", 107093120)
+addappid(2719290)
+setManifestid(2719291,"3602557759348903097")
