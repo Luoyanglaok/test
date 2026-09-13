5d44aa8008014438682e7921703bb7085fab2d1d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 11:50:11 +0800

    add 1304930.lua

--- 1304930.lua
+++ 1304930.lua
@@ -1,6 +1,29 @@
-addappid(1304930)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1304931,0,"81611074b6fd0d2e108d34562aca73115d01d6bea3f08092b8d98aad42c2f0fb")
-setManifestid(1304931,"428272836408249701")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1304930.lua
+--Gamename The Outlast Trials
+addappid(1304930, 1, "7e1bcb0ff8a4d9f5b812334dcb1b6683cef2c631a9bd65d41ec64e6264cc1f8c") --Mainappid The Outlast Trials
+addappid(1304931, 1, "81611074b6fd0d2e108d34562aca73115d01d6bea3f08092b8d98aad42c2f0fb") --Main Windows Depot The Outlast Trials
+setManifestid(1304931, "5388239427091944033", 57015340960)
+--Dlcname The Outlast Trials - Reageant starter pack
+addappid(2328320, 1, "e910a7544e5001ac7650a8acbfc5180e3be595d70c4331b2c8ad9e6b4f511843") --Dlcname The Outlast Trials - Reageant starter pack
+--Dlcname The Outlast Trials - Exotica Pack
+addappid(3030370, 1, "c97e7a515c89d936618002382be5617a49f06e8a3ecf07a33d4d16bb9e602920") --Dlcname The Outlast Trials - Exotica Pack
+--Dlcname The Outlast Trials - Unholy Night Pack
+addappid(3307910, 1, "13d3bd4c2fb2a9eb4b1795e12700df2398d68ed31f86d24345eade316ae5566e") --Dlcname The Outlast Trials - Unholy Night Pack
+--Dlcname The Outlast Trials - Sweet Release Pack
+addappid(3518320, 1, "b6daebe776eefcde87b83b2ccc6b51edcefb7a7a2d82b335b08e3d6fe46ea8ae") --Dlcname The Outlast Trials - Sweet Release Pack
+--Dlcname The Outlast Trials - Porcelain Observer Pack
+addappid(3805230, 1, "08de1574f56dee9a58cf691fdcffb658eedf91b23f33e38dec36c00cf0c24eb4") --Dlcname The Outlast Trials - Porcelain Observer Pack
+--Dlcname The Outlast Trials - Terrified Toddler Pack
+addappid(3805250, 1, "1b5757e608555db0cbee22d289513de2321ca9e94ac0ef42fea3ddc31d3d5621") --Dlcname The Outlast Trials - Terrified Toddler Pack
+--Dlcname The Outlast Trials - World of Heavyweights Pack
+addappid(3805260, 1, "da826c01baf72bb630b627f9c29c9b0447399ebf8400da211739adae1519d55c") --Dlcname The Outlast Trials - World of Heavyweights Pack
+--Dlcname The Outlast Trials - Spelunking For Your Life Pack
+addappid(3805270, 1, "319b2ce55b43e34269abac7621a9b090dc6bf561f544be8d466648f426b6686c") --Dlcname The Outlast Trials - Spelunking For Your Life Pack
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
