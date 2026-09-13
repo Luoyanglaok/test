82f5a9ba0ca9c9c10b0b333bcc42be4c382c9d6c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 15:38:09 +0800

    add 1536810.lua

--- 1536810.lua
+++ 1536810.lua
@@ -1,3 +1,6 @@
-addappid(1536810)
-addappid(1536811,0,"bf3164a85aaeb30ee465259766421af6d24cbe525d3ac405943888c56e3a7e98")
-setManifestid(1536811,"7684741116744970612")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1536810.lua
+--Gamename 鬼谷八荒 Soundtrack
+addappid(1536810) --Mainappid 鬼谷八荒 Soundtrack
+addappid(1536811, 1, "bf3164a85aaeb30ee465259766421af6d24cbe525d3ac405943888c56e3a7e98") --Main Windows Depot 鬼谷八荒 Soundtrack
+setManifestid(1536811, "7684741116744970612", 299370656)
