0509f9e06428f9314d625bb0b497edbe808829f7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 08:17:31 +0800

    add 412020.lua

--- 412020.lua
+++ 412020.lua
@@ -1,19 +1,32 @@
-addappid(412020)
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-addappid(412021,0,"f06003bf9aba2630c97f3f84b80f14067b0df94fb9bfabadcbaea93e1ae4c6f7")
-setManifestid(412021,"8743909117511884672")
-addappid(412022,0,"fdaa0cc6fe8207b1742e3d1fe9b0c62fc848da589cba441eef409d816d6aa775")
-setManifestid(412022,"4169096906348758341")
-addappid(889922,0,"8bed7c94e8395e6a090d72ce60b7048ef9995126be4d7c04896c25ed74382608")
-setManifestid(889922,"2273948263506713647")
-addappid(889923,0,"2853b589418e3e2cdbe78e9e851ac23e54a9f14bef73641a0309f59f4d4f37d1")
-setManifestid(889923,"1970836693290980708")
-addappid(889920,0,"0c8376c99ff46438fef56701e400b882822dd57b5d292398e222228f628849a6")
-setManifestid(889920,"361436782434919800")
-addappid(889921,0,"ba6a1b0ca33f2370d28d58adc6b53ed79d5fb88d1931130c96ecf95df0530ae2")
-setManifestid(889921,"181613428803252911")
-addappid(412023,0,"2d1188fda4fe6c142425d0a9c34f0e6353e5b09eaf0f9967995ecc2948af10f8")
-setManifestid(412023,"4901698992981806692")
-addappid(412024,0,"45161252e073acc402c8cb3c9227484c10aa4f205f1e709294d2c220f6241ded")
-setManifestid(412024,"2770016577399373364")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 412020.lua
+--Gamename Metro Exodus
+addappid(412020, 1, "451aeeb55696fcd66f30d193102e9bb702eb8fa196553ddfaef2d46279ca3f81") --Mainappid Metro Exodus
+addappid(412021, 1, "f06003bf9aba2630c97f3f84b80f14067b0df94fb9bfabadcbaea93e1ae4c6f7") --Main Windows Depot Metro Exodus
+setManifestid(412021, "8743909117511884672", 61722074640)
+addappid(412022, 1, "fdaa0cc6fe8207b1742e3d1fe9b0c62fc848da589cba441eef409d816d6aa775") --Main Windows Depot Metro Exodus
+setManifestid(412022, "4169096906348758341", 26188416)
+addappid(412023, 1, "2d1188fda4fe6c142425d0a9c34f0e6353e5b09eaf0f9967995ecc2948af10f8") --Main Linux Depot Metro Exodus
+setManifestid(412023, "4901698992981806692", 61852751328)
+addappid(412024, 1, "45161252e073acc402c8cb3c9227484c10aa4f205f1e709294d2c220f6241ded") --Main Macos Depot Metro Exodus
+setManifestid(412024, "2770016577399373364", 69515488912)
+--Dlcname Metro Exodus - The Two Colonels
+addappid(889920, 1, "0c8376c99ff46438fef56701e400b882822dd57b5d292398e222228f628849a6") --Dlcname Metro Exodus - The Two Colonels
+setManifestid(889920, "361436782434919800", 1195819840)
+--Dlcname Metro Exodus - Sam's Story
+addappid(889921, 1, "ba6a1b0ca33f2370d28d58adc6b53ed79d5fb88d1931130c96ecf95df0530ae2") --Dlcname Metro Exodus - Sam's Story
+setManifestid(889921, "181613428803252911", 6556070928)
+--Dlcname AppID 889922
+addtoken(889922, "18063194108132517671")
+addappid(889922, 1, "8bed7c94e8395e6a090d72ce60b7048ef9995126be4d7c04896c25ed74382608") --Dlcname AppID 889922
+setManifestid(889922, "2273948263506713647", 169916224)
+--Dlcname AppID 889923
+addtoken(889923, "10381871630932097954")
+addappid(889923, 1, "2853b589418e3e2cdbe78e9e851ac23e54a9f14bef73641a0309f59f4d4f37d1") --Dlcname AppID 889923
+setManifestid(889923, "1970836693290980708", 52665008)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(924220) --Dlcname Metro Exodus Season Pass
