a108ce22e5e8402a719c7cacb8a986db5ed165fd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 12:08:07 +0800

    add 3987810.lua

---
+++ 3987810.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3987810.lua
+--Gamename 欲望之轮
+addappid(3987810, 1, "142966f1cf7a981814f0ca997a6504ed367aa7f621522b7cd8f8e1b636c40a0b") --Mainappid 欲望之轮
+addappid(3987811, 1, "a592e62a04b5bda67cc83501968d0813dc0c3a95fc888be755504470b70de452") --Main Windows Depot 欲望之轮
+setManifestid(3987811, "1653104926795289400", 786319504)
+--Dlcname Pleasure Cruise - Adult DLC
+addappid(4155780, 1, "7b46a973940609d0ae3a67576c455d229a6a6248cd76eda386cda8ca55813e35") --Dlcname Pleasure Cruise - Adult DLC
+setManifestid(4155780, "8028359355591949307", 786319520)
