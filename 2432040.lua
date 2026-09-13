59595394fe292d85e0eeff2918b45ffb3d10fc0a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 07:39:10 +0800

    add 2432040.lua

--- 2432040.lua
+++ 2432040.lua
@@ -1,3 +1,14 @@
-addappid(2432040)
-addappid(2432041,0,"6fd5fe18b06aeec912f746e80798a55e62d86ff4cc32966de2d85b47aebfd5a4")
-setManifestid(2432041,"2385198634890780004")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2432040.lua
+--Gamename ネジ込みシミュレーターTMA02
+addappid(2432040) --Mainappid ネジ込みシミュレーターTMA02
+addappid(2432041, 1, "6fd5fe18b06aeec912f746e80798a55e62d86ff4cc32966de2d85b47aebfd5a4") --Main Windows Depot ネジ込みシミュレーターTMA02
+setManifestid(2432041, "9098249619684591025", 327639056)
+--Dlcname ネジ込みシミュレーター: あまねのドラマ01 -魔法少女は敗北しました-
+addappid(2950240) --Dlcname ネジ込みシミュレーター: あまねのドラマ01 -魔法少女は敗北しました-
+addappid(2950241, 1, "752dd30f53e39e02b177b333bb8ad4790023cd507315a5cb391a61d7b66ed0b8") --Dlc Windows Depot ネジ込みシミュレーター: あまねのドラマ01 -魔法少女は敗北しました-
+setManifestid(2950241, "8683844536433494152", 2575325008)
+addappid(2950242, 1, "7f05a14371d2d82f9670eaa8fba2557b54c4ee36d1cda80a13e971f70a739150") --Dlc Windows Depot ネジ込みシミュレーター: あまねのドラマ01 -魔法少女は敗北しました-
+setManifestid(2950242, "4380094316116907074", 440591088)
+addappid(2432050) --Dlcname NejicomiSimulator TMA02 -Unlock All Settings Pack-
+addappid(3109790) --Dlcname ネジ込みシミュレーターTMA02 DLC
