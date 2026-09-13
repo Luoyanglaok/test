2bd9b9d79a118bf3a18ec61dd79eae98c04e6cfb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 12:59:23 +0800

    add 2631250.lua

--- 2631250.lua
+++ 2631250.lua
@@ -1,7 +1,13 @@
-addappid(2631250)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2631251,0,"35b65353c889458eccf51a3307b040e48e83aff12bddd5bfeb96a787392be005")
-setManifestid(2631251,"150359154658006113")
-addappid(2631252,0,"bb6e2fdcea1188e89b4fbbc62a18fad23a4580ab83074a3bee8bb5fee9c8eadf")
-setManifestid(2631252,"431009321496640377")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2631250.lua
+--Gamename Slitterhead
+addappid(2631250) --Mainappid Slitterhead
+addappid(2631251, 1, "35b65353c889458eccf51a3307b040e48e83aff12bddd5bfeb96a787392be005") --Main Windows Depot Slitterhead
+setManifestid(2631251, "150359154658006113", 27811675344)
+addappid(2631252, 1, "bb6e2fdcea1188e89b4fbbc62a18fad23a4580ab83074a3bee8bb5fee9c8eadf") --Main Windows Depot Slitterhead
+setManifestid(2631252, "431009321496640377", 27818260848)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
