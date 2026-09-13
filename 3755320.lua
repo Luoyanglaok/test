d2275327268d24da4bdfab26571cf93664a4b45d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 07:17:45 +0800

    add 3755320.lua

--- 3755320.lua
+++ 3755320.lua
@@ -5,4 +5,4 @@ addappid(3755320) --Mainappid BakeryDiary
 addappid(3755328, 1, "35ea6296aaea8d125c82702bb9da38c6dbb6ffeffbb28c85bdd9d7b2860439ca") --Main Windows Depot BakeryDiary
 setManifestid(3755328, "2638779978532340352", 138400112)
 addappid(3755329, 1, "7869ab30fea9ae95947be156c91fb23ac06b73e1261ff46acdd00192b91a88f1") --Main Windows Depot BakeryDiary
-setManifestid(3755329, "3262294646520287311", 90272288)
+setManifestid(3755329, "9056154993454288261", 90272144)
