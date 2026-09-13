7ef96565ee4f0b503775303fd57475566b30b29d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 17:57:44 +0800

    add 3732310.lua

---
+++ 3732310.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3732310.lua
+--Gamename Dark Succubus
+addappid(3732310, 1, "b9927c741842e6a7ad541a8b873ca8e16c8fbf5186c46e98624d76d79d1cff46") --Mainappid Dark Succubus
+addappid(3732311, 1, "810ac05957947c9ed2104adae094b5eb949c2a13a6cdf26241899e9e8b7e9ee0") --Main Windows Depot Dark Succubus
+setManifestid(3732311, "6180719968666616052", 2027813328)
