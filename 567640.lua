a923cc38e3a632bedde05d30dbc112c1b491a2e4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 13:05:13 +0800

    add 567640.lua

--- 567640.lua
+++ 567640.lua
@@ -1,18 +1,27 @@
-addappid(567640)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229004,"5220958916987797232")
-addappid(567641,0,"9970cbb9f5643bcca6f9a4a79fd477c623bd86b920e15e1b5d305928b99b6940")
-setManifestid(567641,"2894713093757374682")
-addappid(567642,0,"12906a228da73773ee68b88cb8dda37c3a385f65cab8e7ef355e53bbb648ff20")
-setManifestid(567642,"3393536351451183646")
-addappid(567643,0,"523db6fe1fbeca55f4d7c4b053f5ccb95fbb155a23d6b1d67fac210b04fcc102")
-setManifestid(567643,"457799388131997244")
-addappid(567644,0,"c61bb68902b01ef6e3d5d1ccf5514fdbb153ddb9df5e2ea6abf67939d6162723")
-setManifestid(567644,"535232999877171488")
-addappid(567645,0,"69c35716aa9c4bd28a5f5dcec3fffa15922690b8b1b612e81992871b50c1d937")
-setManifestid(567645,"6189875144592621093")
-addappid(567646,0,"996f6d53be10b91a1e3c54a9ffc6688732de2da64f7701b242cfc0865d6ae178")
-setManifestid(567646,"693090757913722021")
-addappid(567649,0,"ce16809a2ece4721017e47e7835a0158561444aab4053f41d1ea7bb904a673dc")
-setManifestid(567649,"7100102831510686799")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 567640.lua
+--Gamename Danganronpa V3: Killing Harmony
+addappid(567640) --Mainappid Danganronpa V3: Killing Harmony
+addappid(567641, 1, "9970cbb9f5643bcca6f9a4a79fd477c623bd86b920e15e1b5d305928b99b6940") --Main Windows Depot Danganronpa V3: Killing Harmony
+setManifestid(567641, "2894713093757374682", 10663321408)
+addappid(567642, 1, "12906a228da73773ee68b88cb8dda37c3a385f65cab8e7ef355e53bbb648ff20") --Main Windows Language Depot Japanese Danganronpa V3: Killing Harmony
+setManifestid(567642, "3393536351451183646", 2855058032)
+addappid(567643, 1, "523db6fe1fbeca55f4d7c4b053f5ccb95fbb155a23d6b1d67fac210b04fcc102") --Main Windows Language Depot English Danganronpa V3: Killing Harmony
+setManifestid(567643, "457799388131997244", 5775228864)
+addappid(567644, 1, "c61bb68902b01ef6e3d5d1ccf5514fdbb153ddb9df5e2ea6abf67939d6162723") --Main Windows Language Depot French Danganronpa V3: Killing Harmony
+setManifestid(567644, "535232999877171488", 5808829680)
+addappid(567645, 1, "69c35716aa9c4bd28a5f5dcec3fffa15922690b8b1b612e81992871b50c1d937") --Main Windows Language Depot Schinese Danganronpa V3: Killing Harmony
+setManifestid(567645, "6189875144592621093", 5857782032)
+addappid(567646, 1, "996f6d53be10b91a1e3c54a9ffc6688732de2da64f7701b242cfc0865d6ae178") --Main Windows Language Depot Tchinese Danganronpa V3: Killing Harmony
+setManifestid(567646, "693090757913722021", 5827876000)
+--Dlcname Danganronpa V3: Killing Harmony OST
+addappid(707390) --Dlcname Danganronpa V3: Killing Harmony OST
+addappid(567649, 1, "ce16809a2ece4721017e47e7835a0158561444aab4053f41d1ea7bb904a673dc") --Dlc Windows Depot Danganronpa V3: Killing Harmony OST
+setManifestid(567649, "7100102831510686799", 293354624)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
