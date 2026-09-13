8da4ee795961cb14b73c714daa11f0a153db65b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 10:15:17 +0800

    add 4354460.lua

---
+++ 4354460.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4354460.lua
+--Gamename 幸存者笔记
+addappid(4354460) --Mainappid 幸存者笔记
+addappid(4354462, 1, "ed97435798a123a15a1d10cce5b19923d4fbb3a254fdfa9220edfca31f458e2a") --Main Windows Depot 幸存者笔记
+setManifestid(4354462, "2916029916063945821", 36338742400)
