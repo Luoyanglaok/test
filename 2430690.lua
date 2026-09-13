ae66053a2fedf733ec53d6a5ccc56350236ba571
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 13:09:46 +0800

    add 2430690.lua

--- 2430690.lua
+++ 2430690.lua
@@ -1,7 +1,11 @@
-addappid(2430690)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(2430691,0,"8b2540b4208868f8d243b13cd346addb345d8a9ed7fef271884389f0547b37db")
-setManifestid(2430691,"536267691152687196")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2430690.lua
+--Gamename 幸运草的约定
+addappid(2430690, 1, "9e72726747d589f2ebf4ffe9a4c129e81e8a7d27b3c55766b17b71505dad42e6") --Mainappid 幸运草的约定
+addappid(2430691, 1, "8b2540b4208868f8d243b13cd346addb345d8a9ed7fef271884389f0547b37db") --Main Windows Depot 幸运草的约定
+setManifestid(2430691, "536267691152687196", 6618037472)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
