e8be4de77e1a044bc9ba15fd29d1f6d121333798
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 22:17:53 +0800

    add 3988410.lua

---
+++ 3988410.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3988410.lua
+--Gamename Phantom Tower
+addappid(3988410) --Mainappid Phantom Tower
+addappid(3988411, 1, "516fd0b8046ddeb7c18f9c841aaacca2c13dbc5c1cf1224eb1ee73ae9be0d969") --Main Windows Depot Phantom Tower
+setManifestid(3988411, "1478871880015991415", 1208710800)
