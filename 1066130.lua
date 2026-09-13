375a732e859c6bde5b4b631b896e949c0a835e11
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 11:46:14 +0800

    add 1066130.lua

--- 1066130.lua
+++ 1066130.lua
@@ -1,3 +1,12 @@
-addappid(1066130)
-addappid(1066132,0,"90f07c607592aa3b31b2e7bf53daa561245e251e758f5362398dc20e0a422849")
-setManifestid(1066132,"2808553445115395261")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1066130.lua
+--Gamename Truck Life
+addappid(1066130) --Mainappid Truck Life
+addappid(1066132, 1, "90f07c607592aa3b31b2e7bf53daa561245e251e758f5362398dc20e0a422849") --Main Windows Depot Truck Life
+setManifestid(1066132, "2808553445115395261", 4067759520)
+--Dlcname Truck Life-Gansu Province
+addappid(1130600, 1, "697d5528eac57d580c39643293967b35b7b41fcff5e3f6bb9b59167dfd5df5e0") --Dlcname Truck Life-Gansu Province
+setManifestid(1130600, "7528700781805273840", 0)
+addappid(1287490) --Dlcname TruckLife-Special items（Guangdong,Fujian)
+addappid(1385070) --Dlcname Truck Life-TaiWan
+addappid(1468080) --Dlcname Truck Life-Hainan
