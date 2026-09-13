9ab0e5a18725d94515454a7f9cf85055ffedb972
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 17:45:14 +0800

    add 1332720.lua

--- 1332720.lua
+++ 1332720.lua
@@ -1,3 +1,7 @@
-addappid(1332720)
-addappid(1332721,0,"fa8baa603d576daf280269ec6d1581afea7eb9d1940f593ff8d3bbfb78339e14")
-setManifestid(1332721,"6652561021038637825")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1332720.lua
+--Gamename Thief Simulator 2
+addappid(1332720, 1, "061697e4c9e1c835e8e39f8afc3b269c1a0b86ab93283d61c66b3190d1047a28") --Mainappid Thief Simulator 2
+addappid(1332721, 1, "fa8baa603d576daf280269ec6d1581afea7eb9d1940f593ff8d3bbfb78339e14") --Main Windows Depot Thief Simulator 2
+setManifestid(1332721, "6652561021038637825", 5697476144)
+addappid(3027860) --Dlcname Thief Simulator 2: Cruise Ship DLC
