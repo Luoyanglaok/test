1d6d07aa3f05220142ba6e522592e456bb29c431
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 01:31:31 +0800

    add 1583520.lua

---
+++ 1583520.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1583520.lua
+--Gamename Under The Island
+addappid(1583520) --Mainappid Under The Island
+addappid(1583521, 1, "933f8d4241e3c605f0ccbfd390df7d6d4dd779c93db37a8f8b65e4e19e02fddc") --Main Windows Depot Under The Island
+setManifestid(1583521, "8687400541981630159", 410934384)
+--Missing Dlcs
+--Dlcname Under the Island (Selections from the Original Game Soundtrack)
