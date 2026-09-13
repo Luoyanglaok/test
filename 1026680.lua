2235802368011e4821b9a77839e761ffd870678c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 10:36:41 +0800

    add 1026680.lua

--- 1026680.lua
+++ 1026680.lua
@@ -1,8 +1,25 @@
-addappid(1026680)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229004,"5220958916987797232")
-addappid(1026681,0,"2a49f44a8153e4e505ded49c5844e54320aa082a9a1812f987644e6dac5863f2")
-setManifestid(1026681,"5673647156759806571")
-addappid(1152240,0,"85f2fe46d3930f2ec95b072cdfd11ede2ba8e33bd11fa8e8dc2836db2a880073")
-setManifestid(1152240,"1993331997466795726")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1026680.lua
+--Gamename FINAL FANTASY VIII - REMASTERED
+addappid(1026680, 1, "80747b4c9428b0b67c8654fb478b4d16ddbd74345f5334572bd9add03055779a") --Mainappid FINAL FANTASY VIII - REMASTERED
+addappid(1026681, 1, "2a49f44a8153e4e505ded49c5844e54320aa082a9a1812f987644e6dac5863f2") --Main Windows Depot FINAL FANTASY VIII - REMASTERED
+setManifestid(1026681, "5673647156759806571", 2319190672)
+addappid(4425231, 1, "7bd3c0b90cbc324beb5339e06255080365099d8406f2e31ac34d2340a83af01a") --Main Windows Depot FINAL FANTASY VIII - REMASTERED
+setManifestid(4425231, "1105295842015947296", 104167840)
+--Dlcname AppID 1152240
+addtoken(1152240, "15286880836748985791")
+addappid(1152240, 1, "85f2fe46d3930f2ec95b072cdfd11ede2ba8e33bd11fa8e8dc2836db2a880073") --Dlcname AppID 1152240
+setManifestid(1152240, "1993331997466795726", 18847248)
+--Dlcname FINAL FANTASY VIII Original Soundtrack
+addappid(3365700) --Dlcname FINAL FANTASY VIII Original Soundtrack
+addappid(3365701, 1, "99f8544ea87302e3a1e6521d12669f8ca4659134ff480a1f7819d49daeb1fe38") --Dlc Windows Depot FINAL FANTASY VIII Original Soundtrack
+setManifestid(3365701, "2875869071946574645", 521513536)
+--Dlcname Piano Collections FINAL FANTASY VIII
+addappid(4425230) --Dlcname Piano Collections FINAL FANTASY VIII
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
