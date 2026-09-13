09d0e5dcd66afb772af62ff3c354f49a4567bc54
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 01:29:46 +0800

    add 3198890.lua

--- 3198890.lua
+++ 3198890.lua
@@ -1,3 +1,8 @@
-addappid(3198890)
-addappid(3198891,1,"51f594d6d9bf65f6fc859dd65ab3da9bc1131f58eb6a910625b2e7d42f190271")
-setManifestid(3198891,"1533140906856477934")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3198890.lua
+--Gamename Scale the Depths
+addappid(3198890, 1, "581f3d9809e0064f79037c7002fc3983736a0f6f833eb032efcac60aa24770f7") --Mainappid Scale the Depths
+addappid(3198891, 1, "51f594d6d9bf65f6fc859dd65ab3da9bc1131f58eb6a910625b2e7d42f190271") --Main Windows Depot Scale the Depths
+setManifestid(3198891, "1533140906856477934", 225683520)
+addappid(3198892, 1, "68178134e51d991e567d4b0d190883f0e13e2a68a22de8a299243040e1630684") --Main Macos Depot Scale the Depths
+setManifestid(3198892, "8205987516893465684", 254065104)
