b89c92cdc8df68cbec995dac7549caa76a22745d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 04:52:14 +0800

    add 3576350.lua

--- 3576350.lua
+++ 3576350.lua
@@ -1,9 +1,3 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 3576350.lua
---Gamename Sex, Secrets & Used Tech
-addappid(3576350, 1, "d4bb8b7a72a844686faebb4edc5582ec0c88891db163944a62d6db0b4dab123d") --Mainappid Sex, Secrets & Used Tech
-addappid(3576351, 1, "a81a90790f79399b98a8563d1cb099b157662333dd4bac8c1a0060086797e56b") --Main Windows Depot Sex, Secrets & Used Tech
-setManifestid(3576351, "1654315165680704507", 1567635632)
---Dlcname Sex, Secrets & Used Tech - Digital Artbook
-addappid(4687240, 1, "744448e72018b51db6274ce8a99abdce9989f5d69f64c6da501a77e970363d7f") --Dlcname Sex, Secrets & Used Tech - Digital Artbook
-setManifestid(4687240, "2351578736436094779", 32883120)
+addappid(3576350)
+setManifestid(3576351,"1654315165680704507")
+setManifestid(4687240,"2351578736436094779")
