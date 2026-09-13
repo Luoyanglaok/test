316015290136447ed49f5f7674e21641cfbc5f24
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 01:30:32 +0800

    add 1375750.lua

---
+++ 1375750.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1375750.lua
+--Gamename Regions of Ruin: Runegate
+addappid(1375750) --Mainappid Regions of Ruin: Runegate
+addappid(1375752, 1, "af95fcca9c379f1d71fd424952bb1692e14f1214d52a3c25e85d4a4b60b7b61a") --Main Windows Depot Regions of Ruin: Runegate
+setManifestid(1375752, "968121247695557892", 697624352)
+addappid(1375753, 1, "b5b89890195a3fd0d315013dca681714ef671a4ca9643423179cb7a476cbccea") --Main Macos Depot Regions of Ruin: Runegate
+setManifestid(1375753, "4285063225518362613", 715054672)
+addappid(4338371, 1, "d2a6d10bc9ed75e5e77fefee8950328b73f303ca98bec80e7fdfa1a39ac7042e") --Main Windows Depot Regions of Ruin: Runegate
+setManifestid(4338371, "4643517060216401116", 180220928)
+addappid(4338372, 1, "f1df03b6ccf680843bbf8e86b02ba81d516ce433ab08c61993e415a4300b9b1d") --Main Windows Depot Regions of Ruin: Runegate
+setManifestid(4338372, "5390269076734523245", 326766736)
+--Dlcname Regions of Ruin: Runegate Original Soundtrack
+addappid(4338370) --Dlcname Regions of Ruin: Runegate Original Soundtrack
+addappid(4446470) --Dlcname Regions of Ruin: Runegate - The Merchant
