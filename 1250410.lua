669a058be7ff9d5ad8dead9497f40083ad3f1c1c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 15:45:37 +0800

    add 1250410.lua

--- 1250410.lua
+++ 1250410.lua
@@ -1,4 +1,16 @@
-addappid(1250410)
-setManifestid(228988,"6645201662696499616")
-addappid(1250411,0,"dbf9d5a5cae7eab44d452bfe53856e4b08b448ac7c224124316ca8cf3f91b3a8")
-setManifestid(1250411,"6114479443081241185")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1250410.lua
+--Gamename Microsoft Flight Simulator (2020) 40th Anniversary Edition
+addappid(1250410, 1, "8a8884d351f799545f675d56310da7e1749995d652576641056ee35cba44fa4f") --Mainappid Microsoft Flight Simulator (2020) 40th Anniversary Edition
+addappid(1250411, 1, "dbf9d5a5cae7eab44d452bfe53856e4b08b448ac7c224124316ca8cf3f91b3a8") --Main Windows Depot Microsoft Flight Simulator (2020) 40th Anniversary Edition
+setManifestid(1250411, "4980418302200182124", 1202067504)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(1265910) --Dlcname DLC2
+addtoken(1265910, "14285234326855878141")
+addappid(1265911) --Dlcname Microsoft Flight Simulator Deluxe
+addtoken(1265911, "10866014101006598775")
+addappid(5068460) --Dlcname Microsoft Flight Simulator (2020) Local Legends Pack 1
+addappid(5068470) --Dlcname Microsoft Flight Simulator (2020) Local Legends Pack 2
+addappid(5068480) --Dlcname Microsoft Flight Simulator (2020) Famous Flyers Pack 3
