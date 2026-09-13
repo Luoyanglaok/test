1f72bf215bb3f34fa3be31f65badc1e4a581f193
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 10:55:45 +0800

    add 952070.lua

--- 952070.lua
+++ 952070.lua
@@ -1,5 +1,17 @@
-addappid(952070)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(952071,0,"be4c331b420fccf7ed6e4ed528a8f004574c409dfc941ad3e7004fb936679301")
-setManifestid(952071,"2124586286221352257")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 952070.lua
+--Gamename RESIDENT EVIL RESISTANCE
+addappid(952070, 1, "990bfab2b62fccd8a91a20579a65b8578b6b583785cbd89264bc1ca6c9b2947a") --Mainappid RESIDENT EVIL RESISTANCE
+addappid(952071, 1, "be4c331b420fccf7ed6e4ed528a8f004574c409dfc941ad3e7004fb936679301") --Main Windows Depot RESIDENT EVIL RESISTANCE
+setManifestid(952071, "2124586286221352257", 23480641456)
+--Dlcname Resident Evil Resistance - Male Survivor Costume: Leon S. Kennedy
+addappid(1108280, 1, "2c80f9e638cea673510ec5562456040bb00c6ef138fb7be77c691ecab6118199") --Dlcname Resident Evil Resistance - Male Survivor Costume: Leon S. Kennedy
+setManifestid(1108280, "118105767515922548", 272)
+--Dlcname Resident Evil Resistance - Female Survivor Costume: Claire Redfield
+addappid(1295350, 1, "dbe021a1825f0f53da71272f5fd0f4e37669b27d0e00393fcc472b70f515dce7") --Dlcname Resident Evil Resistance - Female Survivor Costume: Claire Redfield
+setManifestid(1295350, "4318883917755845120", 272)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
