d7f95af85032e21bed033b59a5561c7486176d40
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 10:58:59 +0800

    add 1658280.lua

--- 1658280.lua
+++ 1658280.lua
@@ -1,9 +1,25 @@
-addappid(1658280)
-addappid(1658281,0,"61d12d4ec2b9e888bfe67fae7c70f6d76bfe64bbef403294b7375cc326523bb4")
-setManifestid(1658281,"931861435176872698")
-addappid(2694060,0,"c0aa52099fb10cf9bb72bb177c12fb2e52918d0bf6d39dc3dfc85eaf07e5d07a")
-setManifestid(2694060,"2814350827779174299")
-addappid(2694070,0,"1bf7c8888c8f8eff07d289aaebb430c7ea9bbd4ef1a3f0a1eb1ff1f158f557a6")
-setManifestid(2694070,"5303487694956715357")
-addappid(2694080,0,"93f4886b80544255a37428bee906326f84974c434b3439009fdfb1f90d9bb7e4")
-setManifestid(2694080,"7839505989973759128")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1658280.lua
+--Gamename Eiyuden Chronicle: Hundred Heroes
+addappid(1658280) --Mainappid Eiyuden Chronicle: Hundred Heroes
+addappid(1658281, 1, "61d12d4ec2b9e888bfe67fae7c70f6d76bfe64bbef403294b7375cc326523bb4") --Main Windows Depot Eiyuden Chronicle: Hundred Heroes
+setManifestid(1658281, "931861435176872698", 19109315680)
+--Dlcname Eiyuden Chronicle: Hundred Heroes - Story Expansion: The Chapter of Seign
+addappid(2694060, 1, "c0aa52099fb10cf9bb72bb177c12fb2e52918d0bf6d39dc3dfc85eaf07e5d07a") --Dlcname Eiyuden Chronicle: Hundred Heroes - Story Expansion: The Chapter of Seign
+setManifestid(2694060, "2814350827779174299", 115330656)
+--Dlcname Eiyuden Chronicle: Hundred Heroes - Story Expansion: The Chapter of Marisa
+addappid(2694070, 1, "1bf7c8888c8f8eff07d289aaebb430c7ea9bbd4ef1a3f0a1eb1ff1f158f557a6") --Dlcname Eiyuden Chronicle: Hundred Heroes - Story Expansion: The Chapter of Marisa
+setManifestid(2694070, "5303487694956715357", 23963968)
+--Dlcname Eiyuden Chronicle: Hundred Heroes - Story Expansion: The Chapter of Markus
+addappid(2694080, 1, "93f4886b80544255a37428bee906326f84974c434b3439009fdfb1f90d9bb7e4") --Dlcname Eiyuden Chronicle: Hundred Heroes - Story Expansion: The Chapter of Markus
+setManifestid(2694080, "7839505989973759128", 114923408)
+--Dlcname Eiyuden Chronicle: Hundred Heroes - Allraan Artistry Pack
+addappid(2843320) --Dlcname Eiyuden Chronicle: Hundred Heroes - Allraan Artistry Pack
+addappid(2843321, 1, "ec7326b14a67a35d5ee44ef9dae9ee6d3b7f84729bf34e3e25f58d01a36e4eb4") --Dlc Windows Depot Eiyuden Chronicle: Hundred Heroes - Allraan Artistry Pack
+setManifestid(2843321, "1169070756800470315", 68769680)
+addappid(2694030) --Dlcname Eiyuden Chronicle: Hundred Heroes - Pioneer Pack
+addappid(2694050) --Dlcname Eiyuden Chronicle: Hundred Heroes - Easy Journey Pack
+addappid(2694090) --Dlcname Eiyuden Chronicle: Hundred Heroes - HQ Makeover Pack
+addappid(2694110) --Dlcname Eiyuden Chronicle: Hundred Heroes - Season Pass
+addappid(2786940) --Dlcname Eiyuden Chronicle: Hundred Heroes - Hope of the Alliance - Special HQ Statue
+addappid(2786950) --Dlcname Eiyuden Chronicle: Hundred Heroes - 3x Original Wallpapers by Junko Kawano
