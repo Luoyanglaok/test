6c0af777d2a0e60f2eb317b3784e4a22491d5ffc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 17:53:37 +0800

    add 4414250.lua

---
+++ 4414250.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4414250.lua
+--Gamename Sisters in Sin 🔞
+addappid(4414250) --Mainappid Sisters in Sin 🔞
+addappid(4414251, 1, "c83f8b37e4b98049fa4416b945ce8eb674b6be6cf16f35a528b38d3c7b0a2b4e") --Main Windows Depot Sisters in Sin 🔞
+setManifestid(4414251, "7732044961251016019", 3169010736)
+--Dlcname Sisters in Sin🔞-Wallpapers 18+
+addappid(4613200, 1, "000948e41fe7357cd12dc809bae2da6c6737260908931277e6b3e4a309dfd3b2") --Dlcname Sisters in Sin🔞-Wallpapers 18+
+setManifestid(4613200, "8334617565677923042", 312642944)
