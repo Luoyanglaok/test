1c030e757afa716e62fd526b74a88361e90d382a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 16:27:05 +0800

    add 4836540.lua

---
+++ 4836540.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4836540.lua
+--Gamename Company of Heroes 3: Final Stand
+addappid(4836540) --Mainappid Company of Heroes 3: Final Stand
+addappid(4836541, 1, "7515c0af49aad4cf3ae885c6eef73087d477cef8b7383d891d439082c36b0e86") --Main Windows Depot Company of Heroes 3: Final Stand
+setManifestid(4836541, "2556183309939186578", 528)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(1677281, 1, "946c1091854e4b36a1468dea11adb8fe4fe6db6485976132a50781a70d5a206e") --Share Windows Depot Company of Heroes 3
+setManifestid(1677281, "5724534741138488832", 28242247408)
+addappid(1677282, 1, "2dbe642c09395dcb0cfab68ff34eca066c3f47e38d8511d59509fb60cf7c665a") --Share Windows Depot Company of Heroes 3
+setManifestid(1677282, "83851306951263697", 1008)
+addappid(1677283, 1, "532412e59c494bd36e8efc9d4e257208e7412c8a70b16f206103f3913d4add50") --Share Windows Depot Company of Heroes 3
+setManifestid(1677283, "6058592591375980183", 3504)
