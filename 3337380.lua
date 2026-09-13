65c78454682267c1e939fa8f3f285ba6cba748cf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 17:51:45 +0800

    add 3337380.lua

---
+++ 3337380.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3337380.lua
+--Gamename Tabletop Tavern
+addappid(3337380, 1, "42c99ecf0da79c0ace1c7b96e1cc3c05c294f4212fbe49ccdd19075d26ed3aae") --Mainappid Tabletop Tavern
+addappid(3337382, 1, "31e627b11c7ebb087b53481732fc2b3c8fde3a48fe11744a988ab6739d56e9f6") --Main Windows Depot Tabletop Tavern
+setManifestid(3337382, "7218291198263426857", 2206106752)
