13fa0cceea0e3fccf278d777dbca73166f84c27d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 10:42:11 +0800

    add 2022670.lua

--- 2022670.lua
+++ 2022670.lua
@@ -1,5 +1,42 @@
-addappid(2022670)
-setManifestid(228988,"6645201662696499616")
-setManifestid(229007,"4477590687906973371")
-addappid(2022671,0,"f2c1f41d9127af994a3cd878921ff5b89db85334ee15711d98ecac202e0b2c54")
-setManifestid(2022671,"6653124279603243936")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2022670.lua
+--Gamename SONIC SUPERSTARS
+addappid(2022670, 1, "d81511a4ac6bd583c05cbd15c8d5b3da281db6c44edaf62abd2cda910bb81baa") --Mainappid SONIC SUPERSTARS
+addappid(2022671, 1, "f2c1f41d9127af994a3cd878921ff5b89db85334ee15711d98ecac202e0b2c54") --Main Windows Depot SONIC SUPERSTARS
+setManifestid(2022671, "7909756545997353030", 10141658048)
+--Dlcname SONIC SUPERSTARS - Shadow Costume for Sonic
+addappid(2376680, 1, "6730eaccec954f07d577f306eb359a3f71288e5b3059632a7ce9b8d85a986c50") --Dlcname SONIC SUPERSTARS - Shadow Costume for Sonic
+setManifestid(2376680, "3173335243830424535", 36399712)
+--Dlcname Sonic Superstars - LEGO Fun Pack
+addappid(2376681, 1, "0c92a1adfef01e648935444d21387d4210e9f8f9e44ce9540dd74fc366520cce") --Dlcname Sonic Superstars - LEGO Fun Pack
+setManifestid(2376681, "1536858451744743258", 132021184)
+--Dlcname Sonic Superstars - LEGO Eggman
+addappid(2376682, 1, "d6cda1ed45d3181f448124fafa140b64a9b3980145720d0b44d98abd4da5df7d") --Dlcname Sonic Superstars - LEGO Eggman
+setManifestid(2376682, "2167741500057019530", 1627840)
+--Dlcname Sonic Superstars - LEGO Sonic
+addappid(2376683, 1, "4341168c3fe2c4ba640375fa50482728135b6fcf4c92d7563fc21753355ddd2d") --Dlcname Sonic Superstars - LEGO Sonic
+setManifestid(2376683, "1226875788894383256", 40419872)
+--Dlcname SONIC SUPERSTARS – Retro Diner Style Amy Costume
+addappid(2376684, 1, "6e52b3de3a359892d79332af8f7a699108a53088cbd5a4bedbc73c3e13fbd3b9") --Dlcname SONIC SUPERSTARS – Retro Diner Style Amy Costume
+setManifestid(2376684, "46072427903571440", 23794208)
+--Dlcname SONIC SUPERSTARS - Sonic Holiday Costume
+addappid(2376685, 1, "505e7d01852a3491ba27f47a39654981b5d226637188c38cc6ab624441e74e09") --Dlcname SONIC SUPERSTARS - Sonic Holiday Costume
+setManifestid(2376685, "1841909521232151900", 63352256)
+--Dlcname Sonic Superstars - Digital Artbook and mini-OS
+addappid(2376686, 1, "5966b1059b51f7a0848e75844f5bf15635d7add8d093eeb9f8a8c1e10c2a1465") --Dlcname Sonic Superstars - Digital Artbook and mini-OS
+setManifestid(2376686, "8865037080950302454", 479510768)
+--Dlcname Sonic Superstars - Comic Book Skin Pack
+addappid(2378680, 1, "ceb1ec12c9e4865ad35f179ba80e76726cac61d5b9b47c7aa40c2209575e76e3") --Dlcname Sonic Superstars - Comic Book Skin Pack
+setManifestid(2378680, "8828860786451589385", 72550704)
+--Dlcname SONIC SUPERSTARS - Modern Amy Costume
+addappid(2504600) --Dlcname SONIC SUPERSTARS - Modern Amy Costume
+addappid(2504601, 1, "748a138e138f241d762920fadfcc7161080d43c424d5c0aed4a5232b366db48a") --Dlc Windows Depot SONIC SUPERSTARS - Modern Amy Costume
+setManifestid(2504601, "5130416525580453851", 25620144)
+--Dlcname Sonic Superstars - Extra Content Pack
+addappid(2549530, 1, "1df5d5fc27599c6099bf21263adf57547fa1e3bd827da9094e8d40210599d386") --Dlcname Sonic Superstars - Extra Content Pack
+setManifestid(2549530, "6651777336508063901", 39268384)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
