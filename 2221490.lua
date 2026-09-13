eb473ba20d73009c67c035e0ac2ca7c7627f7e95
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 10:25:29 +0800

    add 2221490.lua

--- 2221490.lua
+++ 2221490.lua
@@ -1,25 +1,96 @@
-addappid(2221490)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2221491,0,"c1bcc3effbf8f9a08114de2d5d1aa3566e3b6d1598cf0575f174c8a515253d52")
-setManifestid(2221491,"5276946066805714368")
-addappid(2221493,0,"4a6ca4d7aba34c6bd5610356b9376b677f46bc51626d709798bc62bb259ce9ec")
-setManifestid(2221493,"3569630540591168750")
-addappid(2221494,0,"e24ec08305a5c93445186239740fbb352895b8d8cc21bf45c896c3022abff8c2")
-setManifestid(2221494,"8860932936618331339")
-addappid(2221495,0,"98e9fa9338ab3a9281c906e23368fdbe20981ae5e4cfa8f2e75dd7ccba4d9145")
-setManifestid(2221495,"6977800260336078576")
-addappid(2221496,0,"aa48fd4ef9a8e1ac4441333f7663c2bf89b03ce8ebcea88eda787494395a6d1d")
-setManifestid(2221496,"1604297294206615824")
-addappid(2221497,0,"cc92f847586fe819ba20318130af261e168540f20df523954079c316f8e2d22d")
-setManifestid(2221497,"3095912839045575933")
-addappid(2221498,0,"a69e9ccf948e6813ed344faa6a164e5721b2d301bdb7d3b8901aa2dd68a875ff")
-setManifestid(2221498,"1231632894842306046")
-addappid(2221499,0,"bfb3ec4c927afbda0ccc1e5a5707e6695d618f0e3f6ad4168686782d4c8e317d")
-setManifestid(2221499,"3544195792160309541")
-addappid(2222561,0,"31b6f8f7aa32fa310f71c9638ac76179d1d87affaec821f64ef5c5fcc6081ef6")
-setManifestid(2222561,"1640466823563625962")
-addappid(2222562,0,"0518808cf00b8c1264b30464d91e336d50175ddddf2ca301d6a09fd79b2ac8ca")
-setManifestid(2222562,"6456314514203095052")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2221490.lua
+--Gamename Tom Clancy’s The Division® 2
+addappid(2221490, 1, "ad795f09fbf6f1b18b6d355ac86eeedf4ab361ff65253bdf126f85e3b7bf1b1f") --Mainappid Tom Clancy’s The Division® 2
+addappid(2221491, 1, "c1bcc3effbf8f9a08114de2d5d1aa3566e3b6d1598cf0575f174c8a515253d52") --Main Windows Depot Tom Clancy’s The Division® 2
+setManifestid(2221491, "1544422626416534340", 97332244928)
+addappid(2221493, 1, "4a6ca4d7aba34c6bd5610356b9376b677f46bc51626d709798bc62bb259ce9ec") --Main Windows Language Depot French Tom Clancy’s The Division® 2
+setManifestid(2221493, "3850305554152431132", 2777330320)
+addappid(2221494, 1, "e24ec08305a5c93445186239740fbb352895b8d8cc21bf45c896c3022abff8c2") --Main Windows Language Depot Italian Tom Clancy’s The Division® 2
+setManifestid(2221494, "5400730481639066889", 2894787328)
+addappid(2221495, 1, "98e9fa9338ab3a9281c906e23368fdbe20981ae5e4cfa8f2e75dd7ccba4d9145") --Main Windows Language Depot German Tom Clancy’s The Division® 2
+setManifestid(2221495, "7377644914114760879", 2797289712)
+addappid(2221496, 1, "aa48fd4ef9a8e1ac4441333f7663c2bf89b03ce8ebcea88eda787494395a6d1d") --Main Windows Language Depot Spanish Tom Clancy’s The Division® 2
+setManifestid(2221496, "5287551336326926766", 2903356800)
+addappid(2221497, 1, "cc92f847586fe819ba20318130af261e168540f20df523954079c316f8e2d22d") --Main Windows Language Depot Latam Tom Clancy’s The Division® 2
+setManifestid(2221497, "4558789785581152364", 2840146720)
+addappid(2221498, 1, "a69e9ccf948e6813ed344faa6a164e5721b2d301bdb7d3b8901aa2dd68a875ff") --Main Windows Language Depot Brazilian Tom Clancy’s The Division® 2
+setManifestid(2221498, "2620768349240435505", 2794939040)
+addappid(2221499, 1, "bfb3ec4c927afbda0ccc1e5a5707e6695d618f0e3f6ad4168686782d4c8e317d") --Main Windows Language Depot Russian Tom Clancy’s The Division® 2
+setManifestid(2221499, "2183711803563981353", 2837374432)
+addappid(2222561, 1, "31b6f8f7aa32fa310f71c9638ac76179d1d87affaec821f64ef5c5fcc6081ef6") --Main Windows Language Depot Japanese Tom Clancy’s The Division® 2
+setManifestid(2222561, "1669587881289747002", 2731305040)
+addappid(2222562, 1, "0518808cf00b8c1264b30464d91e336d50175ddddf2ca301d6a09fd79b2ac8ca") --Main Windows Language Depot Arabic Tom Clancy’s The Division® 2
+setManifestid(2222562, "4105951339075836853", 2943835696)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2222560) --Dlcname Tom Clancy's The Division 2 Ubisoft Activation
+addappid(2222570) --Dlcname Tom Clancy's The Division 2 – Warlords Of New York – Expansion
+addappid(2222571) --Dlcname Tom Clancy's The Division 2 Warlords Of New York Expansion Ubisoft Activation
+addappid(2222580) --Dlcname Tom Clancy's The Division 2 Warlords of New York Edition Ubisoft Activation
+addappid(2222730) --Dlcname Tom Clancy's The Division 2 Warlords of New York Ultimate Edition Ubisoft Activation
+addappid(2225120) --Dlcname The Division 2 – One-Time Offer Pack
+addappid(2225121) --Dlcname The Division 2 – One-Time Offer Pack Ubisoft Activation
+addappid(2424600) --Dlcname Tom Clancy's The Division 2 - Full Game Ownership
+addappid(2424601) --Dlcname Tom Clancy's The Division 2 - Free Weekend Ownership
+addappid(2560550) --Dlcname AppID 2560550
+addappid(2560560) --Dlcname The Division 2 – One-Time Offer Pack 3 Ubisoft Activation
+addappid(2927620) --Dlcname The Division 2 - One time offer Wrapped Pack (2024)
+addappid(2927630) --Dlcname The Division 2 - One time offer Wrapped Pack (2024) - Ubisoft Activation
+addappid(2928970) --Dlcname AppID 2928970
+addappid(2928980) --Dlcname The Division 2 - First Rogue Value Pack - Ubisoft Activation
+addappid(3182470) --Dlcname The Division 2 - AUGURY PACK - Ubisoft Activation
+addappid(3182480) --Dlcname AppID 3182480
+addappid(3182490) --Dlcname The Division 2 - SHADES OF RED SEASON PASS BUNDLE - Ubisoft Activation
+addappid(3182500) --Dlcname AppID 3182500
+addappid(3268800) --Dlcname Tom Clancy's The Division 2 – Activation Bundle
+addappid(3268810) --Dlcname The Division 2 Bargain Bundle - Ubisoft Activation
+addappid(3398640) --Dlcname AppID 3398640
+addappid(3398650) --Dlcname Tom Clancy’s The Division 2 – Clawed Pack - Ubisoft Activation
+addappid(3398660) --Dlcname AppID 3398660
+addappid(3398680) --Dlcname Tom Clancy’s The Division 2 – Burden of Truth Season Pass Bundle - Ubisoft Activation
+addappid(3572100) --Dlcname Tom Clancy’s The Division 2 - Gold Edition Ubisoft Activation
+addappid(3572110) --Dlcname Tom Clancy’s The Division 2 - Ultimate Edition Ubisoft Activation
+addappid(3572140) --Dlcname Tom Clancy's The Division 2 – Battle for Brooklyn DLC
+addappid(3572150) --Dlcname Tom Clancy's The Division 2 Battle for Brooklyn DLC Ubisoft Activation
+addappid(3572160) --Dlcname Tom Clancy's The Division 2 Battle for Brooklyn Deluxe DLC
+addappid(3572170) --Dlcname Tom Clancy's The Division 2 Battle for Brooklyn Deluxe DLC Ubisoft Activation
+addappid(3588070) --Dlcname AppID 3588070
+addappid(3588080) --Dlcname The Division 2 Crossroads Season Bundle Ubisoft Activation
+addappid(3588090) --Dlcname AppID 3588090
+addappid(3588100) --Dlcname The Division 2 Bedlam Pack Ubisoft Activation
+addappid(3827270) --Dlcname Y7S2 The Pact Season Bundle
+addappid(3827280) --Dlcname Y7S2 The Pact Season Bundle Ubisoft Activation
+addappid(3827290) --Dlcname Y7S2 Red Eye Pack - One time offer
+addappid(3827300) --Dlcname Y7S2 Red Eye Pack - One time offer Ubisoft Activation
+addappid(4064190) --Dlcname Tom Clancy’s The Division 2 – Mutiny Season Bundle
+addappid(4064200) --Dlcname Y7S3 Mutiny Season Bundle Ubisoft Activation
+addappid(4064210) --Dlcname Tom Clancy’s The Division 2 – Fang Pack
+addappid(4064220) --Dlcname [One Time Offer] Fang Pack 2025 Ubisoft Activation
+addappid(4351500) --Dlcname Tom Clancy’s The Division 2 – Shady Pack
+addappid(4351510) --Dlcname AppID 4351510
+addappid(4351520) --Dlcname Bug Mask Pack - Tom Clancy’s The Division 2
+addappid(4351530) --Dlcname Bug Mask Pack - Ubisoft Activation
+addappid(4351540) --Dlcname Rise Up Bundle - Tom Clancy’s The Division 2
+addappid(4351550) --Dlcname Rise Up Bundle - Ubisoft Activation
+addappid(4378130) --Dlcname Rise Up Supply Drop - Ubisoft Activation
+addappid(4378150) --Dlcname Rise Up Supply Drop - Tom Clancy’s The Division 2
+addappid(4661970) --Dlcname Tom Clancy’s The Division 2 – CBRN Mask Pack
+addappid(4661980) --Dlcname Tom Clancy’s The Division 2 – CBRN Mask Pack Ubisoft Activation
+addappid(4661990) --Dlcname Tom Clancy’s The Division 2 – Into the Dark Bundle
+addappid(4662000) --Dlcname Tom Clancy’s The Division 2 – Into the Dark Bundle Ubisoft Activation
+addappid(4662010) --Dlcname Tom Clancy’s The Division 2 – Into the Dark Supply Drop
+addappid(4662020) --Dlcname Tom Clancy’s The Division 2 – Into the Dark Supply Drop Ubisoft Activation
+addappid(4973980) --Dlcname Tom Clancy’s The Division 2 – Red Horizon Bundle
+addappid(4973990) --Dlcname Tom Clancy’s The Division 2 – Red Horizon Bundle Ubisoft Activation
+addappid(4974000) --Dlcname Tom Clancy’s The Division 2 – Red Horizon Supply Drop
+addappid(4974010) --Dlcname Tom Clancy’s The Division 2 – Red Horizon Supply Drop Ubisoft Activation
+addappid(4974020) --Dlcname Tom Clancy’s The Division 2 – Classic Mask Pack
+addappid(4974030) --Dlcname Tom Clancy’s The Division 2 – Classic Mask Pack Ubisoft Activation
