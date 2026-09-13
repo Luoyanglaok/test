2086bac6074930e992d50076010a64c659afbf1d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 13:00:12 +0800

    add 2903950.lua

--- 2903950.lua
+++ 2903950.lua
@@ -1,5 +1,22 @@
-addappid(2903950)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2903951,0,"a12bd5bdeb914bcfc0be723089d2e8765ae48f4c1ebf378f37da0ba758b6c6c3")
-setManifestid(2903951,"4285774256745523215")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2903950.lua
+--Gamename Master Detective Archives: RAIN CODE Plus
+addappid(2903950, 1, "202d33c5b71d4f3f9b7940240620a24855207814db4deca5777bfd6c06f5739b") --Mainappid Master Detective Archives: RAIN CODE Plus
+addappid(2903951, 1, "a12bd5bdeb914bcfc0be723089d2e8765ae48f4c1ebf378f37da0ba758b6c6c3") --Main Windows Depot Master Detective Archives: RAIN CODE Plus
+setManifestid(2903951, "4285774256745523215", 47250477840)
+--Dlcname Master Detective Archives: RAIN CODE Plus - Noise of Neon
+addappid(2903970) --Dlcname Master Detective Archives: RAIN CODE Plus - Noise of Neon
+addappid(2903971, 1, "6f7fcbc9b9468a4c0ea66a59b8dc0296e5596c00f7f08a349d33fbcdd4137b1d") --Dlc Windows Depot Master Detective Archives: RAIN CODE Plus - Noise of Neon
+setManifestid(2903971, "5195112998359873892", 41482272)
+addappid(2903973, 1, "15ecdec9ff64ca256da2f4a25c5dcd6b7c761fc5e252e9957b2c0d88ec00295f") --Dlc Windows Language Depot Tchinese Master Detective Archives: RAIN CODE Plus - Noise of Neon
+setManifestid(2903973, "4531356983783757817", 41483008)
+addappid(2903974, 1, "bc06172bae0c8d3f98e2127c0f621545333e9021bafcf922a678ab7696b93290") --Dlc Windows Language Depot Schinese Master Detective Archives: RAIN CODE Plus - Noise of Neon
+setManifestid(2903974, "654880889028368774", 41483056)
+--Dlcname Master Detective Archives: RAIN CODE Plus - The Book of Death
+addappid(2903990, 1, "867085a2d7296a081e93e6bdfece17b32035a08712e5e761edea8bed259d7231") --Dlcname Master Detective Archives: RAIN CODE Plus - The Book of Death
+setManifestid(2903990, "4742908682597040890", 2516416576)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
