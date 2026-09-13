a4ef6c009bd6a8e6d9316d8c01e5f2bea3bbf93f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 09:04:21 +0800

    add 3447040.lua

--- 3447040.lua
+++ 3447040.lua
@@ -1,72 +1,83 @@
-addappid(3447040)
-addappid(3965850)
-addappid(3945660)
-addappid(3945670)
-addappid(3945680)
-addappid(3944980)
-addappid(3944990)
-addappid(3945000)
-addappid(3945010)
-addappid(3945020)
-addappid(3945030)
-addappid(3945040)
-addappid(3945050)
-addappid(3945060)
-addappid(3945070)
-addappid(3945080)
-addappid(3945090)
-addappid(3945100)
-addappid(3945110)
-addappid(3945120)
-addappid(3945130)
-addappid(3945140)
-addappid(3945150)
-addappid(3945160)
-addappid(3945170)
-addappid(3945180)
-addappid(3945190)
-addappid(3945200)
-addappid(3945210)
-addappid(3945220)
-addappid(3945230)
-addappid(3945240)
-addappid(3945250)
-addappid(3945260)
-addappid(3945270)
-addappid(3945280)
-addappid(3945290)
-addappid(3945300)
-addappid(3945310)
-addappid(3945320)
-addappid(3945330)
-addappid(3945340)
-addappid(3945360)
-addappid(3945370)
-addappid(3945380)
-addappid(3945390)
-addappid(3945400)
-addappid(3945420)
-addappid(3945430)
-addappid(3945440)
-addappid(3945450)
-addappid(3945460)
-addappid(3945470)
-addappid(3945480)
-addappid(3945490)
-addappid(3945500)
-addappid(3945510)
-addappid(3945520)
-addappid(3945550)
-addappid(3945560)
-addappid(3945570)
-addappid(3945580)
-addappid(3945590)
-addappid(3945600)
-addappid(3945610)
-addappid(3945620)
-addappid(3945630)
-addappid(3945640)
-addappid(3945650)
-setManifestid(3447041,"5940653132800016886")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3447040.lua
+--Gamename Sora no Kiseki the 1st
+addappid(3447040, 1, "6f2278893cf29640194421d9638abfb3a4eaa81d78eeae513436274aafc18e19") --Mainappid Sora no Kiseki the 1st
+addappid(3447041, 1, "1094c51be1905bc44cf99ef97fa418199a8907734568008f95c100371af927f7") --Main Windows Depot Sora no Kiseki the 1st
+setManifestid(3447041, "6141595829747460974", 18453173488)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3944910) --Dlcname Sora no Kiseki the 1st - Costume: Plushie Estelle/Plushie Joshua
+addappid(3944950) --Dlcname Sora no Kiseki the 1st - Costume: Summer Vacation (Estelle Ver.)
+addappid(3944960) --Dlcname Sora no Kiseki the 1st - Costume: Summer Vacation (Joshua Ver.)
+addappid(3944970) --Dlcname Sora no Kiseki the 1st - Costume: NEO Bracer
+addappid(3944980) --Dlcname Sora no Kiseki the 1st - Estelle & Joshua Swimwear: Pure Resort
+addappid(3944990) --Dlcname Sora no Kiseki the 1st - Jenis Royal Academy Uniform Set
+addappid(3945000) --Dlcname Sora no Kiseki the 1st - Matching Diner Costume Set: Fine Red Diner Vol.1
+addappid(3945010) --Dlcname Sora no Kiseki the 1st - Matching Diner Costume Set: Fine Red Diner Vol.2
+addappid(3945020) --Dlcname Sora no Kiseki the 1st - Matching Diner Costume Set: Fine Red Diner Vol.3
+addappid(3945030) --Dlcname Sora no Kiseki the 1st - Unique Diner Costume Set Vol.1
+addappid(3945040) --Dlcname Sora no Kiseki the 1st - Unique Diner Costume Set Vol.2
+addappid(3945050) --Dlcname Sora no Kiseki the 1st - Unique Diner Costume Set Vol.3
+addappid(3945060) --Dlcname Sora no Kiseki the 1st - Glasses Set A
+addappid(3945070) --Dlcname Sora no Kiseki the 1st - Glasses Set B
+addappid(3945080) --Dlcname Sora no Kiseki the 1st - Go for it! Headgear Set
+addappid(3945090) --Dlcname Sora no Kiseki the 1st - Head-Pom Set A
+addappid(3945100) --Dlcname Sora no Kiseki the 1st - Head-Pom Set B
+addappid(3945110) --Dlcname Sora no Kiseki the 1st - Chocosta Set
+addappid(3945120) --Dlcname Sora no Kiseki the 1st - Pretty Backpack Set
+addappid(3945130) --Dlcname Sora no Kiseki the 1st - Archangel Costume Set
+addappid(3945140) --Dlcname Sora no Kiseki the 1st - Fallen Angel Costume Set
+addappid(3945150) --Dlcname Sora no Kiseki the 1st - Demon King Costume Set
+addappid(3945160) --Dlcname Sora no Kiseki the 1st - Lively Effect Set
+addappid(3945170) --Dlcname Sora no Kiseki the 1st - Banner Set A
+addappid(3945180) --Dlcname Sora no Kiseki the 1st - Banner Set B
+addappid(3945190) --Dlcname Sora no Kiseki the 1st - Banner Set C
+addappid(3945200) --Dlcname Sora no Kiseki the 1st - Two-Tone Hair Color Set
+addappid(3945210) --Dlcname Sora no Kiseki the 1st - Unique Hair Color Set
+addappid(3945220) --Dlcname Sora no Kiseki the 1st - Orbment Cover Set
+addappid(3945230) --Dlcname Sora no Kiseki the 1st - Chibi Character Cover Set
+addappid(3945240) --Dlcname Sora no Kiseki the 1st - Sepith Set (1)
+addappid(3945250) --Dlcname Sora no Kiseki the 1st - Sepith Set (2)
+addappid(3945260) --Dlcname Sora no Kiseki the 1st - Sepith Set (3)
+addappid(3945270) --Dlcname Sora no Kiseki the 1st - Sepith Set (4)
+addappid(3945280) --Dlcname Sora no Kiseki the 1st - Sepith Set (5)
+addappid(3945290) --Dlcname Sora no Kiseki the 1st - U-Material Set (1)
+addappid(3945300) --Dlcname Sora no Kiseki the 1st - U-Material Set (2)
+addappid(3945310) --Dlcname Sora no Kiseki the 1st - U-Material Set (3)
+addappid(3945320) --Dlcname Sora no Kiseki the 1st - U-Material Set (4)
+addappid(3945330) --Dlcname Sora no Kiseki the 1st - U-Material Set (5)
+addappid(3945340) --Dlcname Sora no Kiseki the 1st - Monster Ingredients Set A (1)
+addappid(3945360) --Dlcname Sora no Kiseki the 1st - Monster Ingredients Set A (2)
+addappid(3945370) --Dlcname Sora no Kiseki the 1st - Monster Ingredients Set A (3)
+addappid(3945380) --Dlcname Sora no Kiseki the 1st - Monster Ingredients Set B (1)
+addappid(3945390) --Dlcname Sora no Kiseki the 1st - Monster Ingredients Set B (2)
+addappid(3945400) --Dlcname Sora no Kiseki the 1st - Monster Ingredients Set B (3)
+addappid(3945420) --Dlcname Sora no Kiseki the 1st - Advanced Recovery Medicine Set (1)
+addappid(3945430) --Dlcname Sora no Kiseki the 1st - Advanced Recovery Medicine Set (2)
+addappid(3945440) --Dlcname Sora no Kiseki the 1st - Advanced Recovery Medicine Set (3)
+addappid(3945450) --Dlcname Sora no Kiseki the 1st - Advanced Recovery Medicine Set (4)
+addappid(3945460) --Dlcname Sora no Kiseki the 1st - Advanced Recovery Medicine Set (5)
+addappid(3945470) --Dlcname Sora no Kiseki the 1st - Zelam Powder Set (1)
+addappid(3945480) --Dlcname Sora no Kiseki the 1st - Zelam Powder Set (2)
+addappid(3945490) --Dlcname Sora no Kiseki the 1st - Zelam Powder Set (3)
+addappid(3945500) --Dlcname Sora no Kiseki the 1st - Zelam Capsule Set (1)
+addappid(3945510) --Dlcname Sora no Kiseki the 1st - Zelam Capsule Set (2)
+addappid(3945520) --Dlcname Sora no Kiseki the 1st - Zelam Capsule Set (3)
+addappid(3945550) --Dlcname Sora no Kiseki the 1st - Golden Pom Fruit Set (1)
+addappid(3945560) --Dlcname Sora no Kiseki the 1st - Golden Pom Fruit Set (2)
+addappid(3945570) --Dlcname Sora no Kiseki the 1st - Golden Pom Fruit Set (3)
+addappid(3945580) --Dlcname Sora no Kiseki the 1st - Golden Pom Fruit Value Pack (1)
+addappid(3945590) --Dlcname Sora no Kiseki the 1st - Golden Pom Fruit Value Pack (2)
+addappid(3945600) --Dlcname Sora no Kiseki the 1st - Golden Pom Fruit Value Pack (3)
+addappid(3945610) --Dlcname Sora no Kiseki the 1st - Droplet Set (1)
+addappid(3945620) --Dlcname Sora no Kiseki the 1st - Droplet Set (2)
+addappid(3945630) --Dlcname Sora no Kiseki the 1st - Droplet Set (3)
+addappid(3945640) --Dlcname Sora no Kiseki the 1st - Combat Boost Pack: Physical
+addappid(3945650) --Dlcname Sora no Kiseki the 1st - Combat Boost Pack: Magical
+addappid(3945660) --Dlcname Sora no Kiseki the 1st - Free Sample Set Vol.1
+addappid(3945670) --Dlcname Sora no Kiseki the 1st - Free Sample Set Vol.2
+addappid(3945680) --Dlcname Sora no Kiseki the 1st - Free Sample Set Vol.3
+addappid(3965850) --Dlcname Sora no Kiseki the 1st Season Pass
