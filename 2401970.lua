2a9d6ee3f9e9f72f31e18ff4bc4147ec8efd604d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-13 16:10:02 +0800

    add 2401970.lua

--- 2401970.lua
+++ 2401970.lua
@@ -1,3 +1,7 @@
-addappid(2401970)
-addappid(2401971,0,"6207c375742bbad4314f7dd29c0875182fdcb82428b948c4ae9205f86a920a3c")
-setManifestid(2401971,"5676289079352931341")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2401970.lua
+--Gamename Ace Attorney Investigations Collection
+addappid(2401970, 1, "ed7414f95ad7a756d33cd6b9b45befdde117bbddecdd31bb9b2f1b7617326b92") --Mainappid Ace Attorney Investigations Collection
+addappid(2401971, 1, "6207c375742bbad4314f7dd29c0875182fdcb82428b948c4ae9205f86a920a3c") --Main Windows Depot Ace Attorney Investigations Collection
+setManifestid(2401971, "2813677523471918258", 6807153280)
+addappid(2708760) --Dlcname Ace Attorney Investigations Collection In-Game Music (Arranged) - 5 Tracks Set
