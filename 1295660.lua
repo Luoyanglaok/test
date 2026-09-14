185a47ca1f794c8ac2108343746fddca51ae7475
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 01:11:00 +0800

    add 1295660.lua

--- 1295660.lua
+++ 1295660.lua
@@ -1,12 +1,68 @@
-addappid(1295660)
-addtoken(1295660,"16771268434109846988")
-addappid(1295661,0,"59cf929fe0be7af1bba7498247138b359bcc8141d6921f65f06f5d3c4e604430")
-setManifestid(1295661,"3443115785256834029")
-addappid(1295662,0,"2d91bb0ac40f79f21d133921c8092cee1bbc5622c1e429d5d5e65a5543a440eb")
-setManifestid(1295662,"8604623926511869991")
-addappid(1295663,0,"129446ed0c86a3cecf211aca2aca7c0ef0b14248fe2e7229afb79e90f2b07531")
-setManifestid(1295663,"2879663200719946650")
-addappid(1295665,0,"ad00cf603a7538a190b6f7e523a9f9b1ff48759aacb39f2b35a70c5302fcef37")
-setManifestid(1295665,"5696017506834416946")
-addappid(1295666,0,"68e1686bc8f5a23173a86f6a5c3fb61ae2714081bf956976f7f837a1f03a9779")
-setManifestid(1295666,"6185944872203192343")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1295660.lua
+--Gamename Sid Meier's Civilization® VII
+addappid(1295660, 1, "f23c385f4f13e1385d24f0f6033c70e9dd9fbbbbc955227df88693a31db9411b") --Mainappid Sid Meier's Civilization® VII
+addtoken(1295660, "16771268434109846988")
+addappid(1295661, 1, "59cf929fe0be7af1bba7498247138b359bcc8141d6921f65f06f5d3c4e604430") --Main Windows Depot Sid Meier's Civilization® VII
+setManifestid(1295661, "2169683721877823435", 20010120576)
+addappid(1295662, 1, "2d91bb0ac40f79f21d133921c8092cee1bbc5622c1e429d5d5e65a5543a440eb") --Main Windows Depot Sid Meier's Civilization® VII
+setManifestid(1295662, "4956434543758512240", 1230545344)
+addappid(1295663, 1, "129446ed0c86a3cecf211aca2aca7c0ef0b14248fe2e7229afb79e90f2b07531") --Main Macos Depot Sid Meier's Civilization® VII
+setManifestid(1295663, "1081703995254963718", 19425926112)
+addappid(1295665, 1, "ad00cf603a7538a190b6f7e523a9f9b1ff48759aacb39f2b35a70c5302fcef37") --Main Linux Depot Sid Meier's Civilization® VII
+setManifestid(1295665, "1138836125388027140", 40628096)
+addappid(1295666, 1, "68e1686bc8f5a23173a86f6a5c3fb61ae2714081bf956976f7f837a1f03a9779") --Main Linux Depot Sid Meier's Civilization® VII
+setManifestid(1295666, "2119085612154290477", 19107730784)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2778060) --Dlcname Sid Meier's Civilization VII: Deluxe Cosmetics Pack
+addappid(3004650) --Dlcname Sid Meier's Civilization® VII: Founders Cosmetics Pack
+addappid(3004660) --Dlcname Sid Meier's Civilization® VII: Crossroads of the World Collection
+addtoken(3004660, "908074924523561409")
+addappid(3087220) --Dlcname Sid Meier's Civilization VII: Xerxes (The Achaemenid) Persona
+addappid(3087240) --Dlcname Sid Meier's Civilization® VII: Friedrich (Baroque) Persona
+addappid(3087250) --Dlcname Sid Meier's Civilization® VII: Deluxe Content Pack
+addappid(3087260) --Dlcname Sid Meier's Civilization VII: Himiko (High Shaman) Persona
+addappid(3087270) --Dlcname Sid Meier's Civilization VII: Ashoka (World Conqueror) Persona
+addappid(3087280) --Dlcname Sid Meier's Civilization® VII: Carthage Pack
+addappid(3087290) --Dlcname Sid Meier's Civilization® VII: Ada Lovelace Pack
+addappid(3087300) --Dlcname Sid Meier's Civilization® VII: Great Britain Pack
+addappid(3087310) --Dlcname Sid Meier's Civilization® VII: Simón Bolívar Pack
+addappid(3087320) --Dlcname Sid Meier's Civilization® VII: Bulgaria Pack
+addappid(3087330) --Dlcname Sid Meier's Civilization® VII: Nepal Pack
+addappid(3087350) --Dlcname Sid Meier's Civilization® VII: Crossroads of the World Collection - Wonder Pack
+addappid(3087370) --Dlcname Sid Meier's Civilization® VII: Genghis Khan Pack
+addtoken(3087370, "17615123042722017763")
+addappid(3087380) --Dlcname Sid Meier's Civilization® VII: Dai Viet Pack
+addtoken(3087380, "7871402320395250466")
+addappid(3087390) --Dlcname Sid Meier's Civilization® VII: Lakshmibai Pack
+addappid(3087400) --Dlcname Sid Meier's Civilization® VII: Silla Pack
+addappid(3087410) --Dlcname Sid Meier's Civilization® VII: Assyria Pack
+addtoken(3087410, "16879705066946463605")
+addappid(3087420) --Dlcname Sid Meier's Civilization® VII: Qajar Pack
+addappid(3087430) --Dlcname Sid Meier's Civilization® VII: Right to Rule Collection
+addappid(3087440) --Dlcname Sid Meier's Civilization® VII: Right to Rule Collection - Wonder Pack
+addtoken(3087440, "14302846944858302509")
+addappid(3087460) --Dlcname Sid Meier's Civilization VII: Tecumseh and Shawnee Pack
+addappid(3369860) --Dlcname Sid Meier's Civilization® VII: Tides of Power Collection
+addappid(3490290) --Dlcname Sid Meier's Civilization® VII Upgrade to Founders Edition
+addtoken(3490290, "6861301468872530534")
+addappid(3502600) --Dlcname Sid Meier's Civilization® VII Upgrade to Deluxe Edition
+addtoken(3502600, "2609124707744154156")
+addappid(3958320) --Dlcname Sid Meier's Civilization® VII: Edward Teach Pack
+addappid(3958330) --Dlcname Sid Meier's Civilization® VII: Republic of Pirates Pack
+addappid(3958340) --Dlcname Sid Meier's Civilization® VII: Tonga Pack
+addappid(3958350) --Dlcname Sid Meier's Civilization® VII: Sayyida al Hurra Pack
+addappid(3958360) --Dlcname Sid Meier's Civilization® VII: Ottomans Pack
+addappid(3958370) --Dlcname Sid Meier's Civilization® VII: Iceland Pack
+addappid(3958380) --Dlcname Civilization VII - CC3 Natural Wonders + Narrative Pack
+addappid(4657090) --Dlcname Sid Meier's Civilization® VII: Brush and Blade Collection
+addappid(4657100) --Dlcname Sid Meier's Civilization® VII: Toyotomi Hideyoshi Pack
+addappid(4657110) --Dlcname Sid Meier's Civilization® VII: Heian Japan Pack
+addappid(4657120) --Dlcname Sid Meier's Civilization® VII: Sengoku Japan Pack
+addappid(4657130) --Dlcname Sid Meier's Civilization® VII: Yi Sun-sin Pack
+addappid(4657140) --Dlcname Sid Meier's Civilization® VII: Goryeo
+addappid(4657150) --Dlcname Sid Meier's Civilization® VII: Joseon Pack
+addappid(4657160) --Dlcname Sid Meier's Civilization® VII: Brush and Blade Collection - Wonder Pack
+addappid(5032500) --Dlcname Sid Meier's Civilization® VII: Earthrise
