9a1a6c32ce7d26731ad572c35160a8c4c67d8df9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 22:30:20 +0800

    add 8190.lua

--- 8190.lua
+++ 8190.lua
@@ -1,54 +1,93 @@
-addappid(8190)
-setManifestid(228981,"7613356809904826842")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(35103,0,"c0b11848f18f441348c49ecd3764804b70e018b3212f6ae0a5bae9ebbffc83bb")
-setManifestid(35103,"1055634404789415346")
-addappid(35102,0,"a0d77da3123f04996d9724b0c01926e1f93be749cb2cbdafd30f5ba18539a696")
-setManifestid(35102,"2605655751645253389")
-addappid(35101,0,"635cb78da22a6de46f57350d256691e6f6393dec6c363acd2bdef34f75c411d5")
-setManifestid(35101,"7380670225511713154")
-addappid(35100,0,"a5f25b4137f4add89eac06c668675321e334e0d23222d49b79b9a7dcc68e5373")
-setManifestid(35100,"558213615192367173")
-addappid(35099,0,"c0988fa0a88f6a2a0e09037f83662494d3f1478dc66328d049ab33d2eb512d4d")
-setManifestid(35099,"2606628488225202364")
-addappid(35098,0,"dd2bec9e6e4f00ffdb46d3a008f55c1f356c17a2afb0cebeb1c0e16d34d8c8cc")
-setManifestid(35098,"6162536311124061831")
-addappid(35097,0,"d21687154de439a8c36a9bdcb8eb2397ee30e4b5f51dc94598f08cc0f23d94f4")
-setManifestid(35097,"1529224188873178393")
-addappid(35096,0,"de5009d6ecd33c86dbee1827004a760f0153045a515b809c5f554f16ed374f6e")
-setManifestid(35096,"5631831470696328720")
-addappid(35095,0,"05999201770ffdb5adf16c9312b66ad31c0e4a2514e0bf8c9d07384fce27d554")
-setManifestid(35095,"4355965730796584520")
-addappid(35094,0,"53fd40ab5b6d662b604ffb84775b64af4230feb9915b6423313ddab4673fb379")
-setManifestid(35094,"1086138972707405937")
-addappid(35093,0,"61c78b06aabdf84dc4c737833d25413d1f607b8fa25aca9437b99821e282f012")
-setManifestid(35093,"8946393391238309221")
-addappid(35092,0,"64e0f629a1bf1d98636b43b184487f8bed8e5cf095e089c94c54bbb67dc7995d")
-setManifestid(35092,"7082310017476453632")
-addappid(35091,0,"a24ca3bd73b0cef5b408ca837a93b0bb1edf02e17c27a623a93a032020c9a429")
-setManifestid(35091,"8336502279671675996")
-addappid(35090,0,"8d9a539c9e53424c8322948b61596fcf15c27810e89262839b09361cae757f09")
-setManifestid(35090,"243679482731703317")
-addappid(35089,0,"dcead30cf50bdaf3a2a5e223980dcc2ca58c96c10ff6fc26576888998b3c1d1d")
-setManifestid(35089,"2410819095638531909")
-addappid(35088,0,"8c9e528db1ef2f1029ba3d57fae3bd24099cfa85a9e1a94a06211e0377c326b6")
-setManifestid(35088,"995384485784707921")
-addappid(35087,0,"71fef3ad51331217cba602108dcf4707dbd30aff06155046ff3b2043080026eb")
-setManifestid(35087,"1154837240653038200")
-addappid(35086,0,"8c51070017226c6b1d2f23919907ad8e8968fb12eef964edec5aeea9e7eb89f1")
-setManifestid(35086,"8569651332701617496")
-addappid(35085,0,"2943904254d37f1f408182f06e54242d98377e7a718d0962c8218f2176a2994f")
-setManifestid(35085,"4841511043315316629")
-addappid(35084,0,"cede5e02e50f6bf459e568b6f06715f885afec21f91b96372437de1c17d93d6d")
-setManifestid(35084,"796682772375627998")
-addappid(35083,0,"4d16b3584224c2a428315316fd261185db22081eae7255c4131ebd9c51da146a")
-setManifestid(35083,"7993100068291742119")
-addappid(35082,0,"00b1e05653c1d3e292088e39d0cd666c5e08096e37684230105702ae90bc9079")
-setManifestid(35082,"3030356907128474855")
-addappid(35081,0,"c5b22bb45f4419da776c63b99fb71b38756b08e7c65d01804ba71f231be01beb")
-setManifestid(35081,"7740178380003749361")
-addappid(35080,0,"2db5c4ed264cf64da1513516b7354ebb14072e2baa9d601db20aa68fabd784b2")
-setManifestid(35080,"4684465936996631403")
-addappid(8191,0,"39f7ed47874422226e5b68b9f58beaafaefc42e5daebdf5151095954230ef13e")
-setManifestid(8191,"4328963383977882793")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8190.lua
+--Gamename Just Cause 2
+addappid(8190, 1, "18eed42f97c3ff754e3f9416d9ad718fc1fcfc6ca7288da7b7a4a1f22d03a1a0") --Mainappid Just Cause 2
+addappid(8191, 1, "39f7ed47874422226e5b68b9f58beaafaefc42e5daebdf5151095954230ef13e") --Main Windows Depot Just Cause 2
+setManifestid(8191, "4328963383977882793", 0)
+--Dlcname Just Cause 2: Bull's Eye Rifle DLC
+addappid(35080, 1, "2db5c4ed264cf64da1513516b7354ebb14072e2baa9d601db20aa68fabd784b2") --Dlcname Just Cause 2: Bull's Eye Rifle DLC
+setManifestid(35080, "4684465936996631403", 0)
+--Dlcname Just Cause 2: Air Propulsion Gun DLC
+addappid(35081, 1, "c5b22bb45f4419da776c63b99fb71b38756b08e7c65d01804ba71f231be01beb") --Dlcname Just Cause 2: Air Propulsion Gun DLC
+setManifestid(35081, "7740178380003749361", 0)
+--Dlcname Just Cause 2: Cluster Bomb Launcher DLC
+addappid(35082, 1, "00b1e05653c1d3e292088e39d0cd666c5e08096e37684230105702ae90bc9079") --Dlcname Just Cause 2: Cluster Bomb Launcher DLC
+setManifestid(35082, "3030356907128474855", 0)
+--Dlcname Just Cause 2: Rico's Signature Gun DLC
+addappid(35083, 1, "4d16b3584224c2a428315316fd261185db22081eae7255c4131ebd9c51da146a") --Dlcname Just Cause 2: Rico's Signature Gun DLC
+setManifestid(35083, "7993100068291742119", 0)
+--Dlcname Just Cause 2: Quad Rocket Launcher DLC
+addappid(35084, 1, "cede5e02e50f6bf459e568b6f06715f885afec21f91b96372437de1c17d93d6d") --Dlcname Just Cause 2: Quad Rocket Launcher DLC
+setManifestid(35084, "796682772375627998", 0)
+--Dlcname Just Cause 2: Multi-lock Missile Launcher DLC
+addappid(35085, 1, "2943904254d37f1f408182f06e54242d98377e7a718d0962c8218f2176a2994f") --Dlcname Just Cause 2: Multi-lock Missile Launcher DLC
+setManifestid(35085, "4841511043315316629", 0)
+--Dlcname Just Cause 2: Tuk Tuk Boom Boom DLC
+addappid(35086, 1, "8c51070017226c6b1d2f23919907ad8e8968fb12eef964edec5aeea9e7eb89f1") --Dlcname Just Cause 2: Tuk Tuk Boom Boom DLC
+setManifestid(35086, "8569651332701617496", 0)
+--Dlcname Just Cause 2: Chevalier Ice Breaker DLC
+addappid(35087, 1, "71fef3ad51331217cba602108dcf4707dbd30aff06155046ff3b2043080026eb") --Dlcname Just Cause 2: Chevalier Ice Breaker DLC
+setManifestid(35087, "1154837240653038200", 0)
+--Dlcname Just Cause 2: F-33 DragonFly Jet Fighter DLC
+addappid(35088, 1, "8c9e528db1ef2f1029ba3d57fae3bd24099cfa85a9e1a94a06211e0377c326b6") --Dlcname Just Cause 2: F-33 DragonFly Jet Fighter DLC
+setManifestid(35088, "995384485784707921", 0)
+--Dlcname Just Cause 2: Chevalier Classic DLC
+addappid(35089, 1, "dcead30cf50bdaf3a2a5e223980dcc2ca58c96c10ff6fc26576888998b3c1d1d") --Dlcname Just Cause 2: Chevalier Classic DLC
+setManifestid(35089, "2410819095638531909", 0)
+--Dlcname Just Cause 2: Agency Hovercraft DLC
+addappid(35090, 1, "8d9a539c9e53424c8322948b61596fcf15c27810e89262839b09361cae757f09") --Dlcname Just Cause 2: Agency Hovercraft DLC
+setManifestid(35090, "243679482731703317", 0)
+--Dlcname Just Cause 2: Monster Truck DLC
+addappid(35091, 1, "a24ca3bd73b0cef5b408ca837a93b0bb1edf02e17c27a623a93a032020c9a429") --Dlcname Just Cause 2: Monster Truck DLC
+setManifestid(35091, "8336502279671675996", 0)
+--Dlcname Just Cause 2: Dual Parachute Thrusters DLC
+addappid(35092, 1, "64e0f629a1bf1d98636b43b184487f8bed8e5cf095e089c94c54bbb67dc7995d") --Dlcname Just Cause 2: Dual Parachute Thrusters DLC
+setManifestid(35092, "7082310017476453632", 0)
+--Dlcname Just Cause 2: Dare Devil Parachute DLC
+addappid(35093, 1, "61c78b06aabdf84dc4c737833d25413d1f607b8fa25aca9437b99821e282f012") --Dlcname Just Cause 2: Dare Devil Parachute DLC
+setManifestid(35093, "8946393391238309221", 0)
+--Dlcname Just Cause 2: Chaos Parachute DLC
+addappid(35094, 1, "53fd40ab5b6d662b604ffb84775b64af4230feb9915b6423313ddab4673fb379") --Dlcname Just Cause 2: Chaos Parachute DLC
+setManifestid(35094, "1086138972707405937", 0)
+--Dlcname Just Cause 2: Camo Parachute DLC
+addappid(35095, 1, "05999201770ffdb5adf16c9312b66ad31c0e4a2514e0bf8c9d07384fce27d554") --Dlcname Just Cause 2: Camo Parachute DLC
+setManifestid(35095, "4355965730796584520", 0)
+--Dlcname Just Cause 2: Tiger Parachute DLC
+addappid(35096, 1, "de5009d6ecd33c86dbee1827004a760f0153045a515b809c5f554f16ed374f6e") --Dlcname Just Cause 2: Tiger Parachute DLC
+setManifestid(35096, "5631831470696328720", 0)
+--Dlcname Just Cause 2: Scorpion Parachute DLC
+addappid(35097, 1, "d21687154de439a8c36a9bdcb8eb2397ee30e4b5f51dc94598f08cc0f23d94f4") --Dlcname Just Cause 2: Scorpion Parachute DLC
+setManifestid(35097, "1529224188873178393", 0)
+--Dlcname Just Cause 2: Firestorm Parachute DLC
+addappid(35098, 1, "dd2bec9e6e4f00ffdb46d3a008f55c1f356c17a2afb0cebeb1c0e16d34d8c8cc") --Dlcname Just Cause 2: Firestorm Parachute DLC
+setManifestid(35098, "6162536311124061831", 0)
+--Dlcname Just Cause 2: Rico's Standard Parachute DLC
+addappid(35099, 1, "c0988fa0a88f6a2a0e09037f83662494d3f1478dc66328d049ab33d2eb512d4d") --Dlcname Just Cause 2: Rico's Standard Parachute DLC
+setManifestid(35099, "2606628488225202364", 0)
+--Dlcname Just Cause 2: Black Market Chaos Pack DLC
+addappid(35100, 1, "a5f25b4137f4add89eac06c668675321e334e0d23222d49b79b9a7dcc68e5373") --Dlcname Just Cause 2: Black Market Chaos Pack DLC
+setManifestid(35100, "558213615192367173", 0)
+--Dlcname Just Cause 2: Black Market Aerial Pack DLC
+addappid(35101, 1, "635cb78da22a6de46f57350d256691e6f6393dec6c363acd2bdef34f75c411d5") --Dlcname Just Cause 2: Black Market Aerial Pack DLC
+setManifestid(35101, "7380670225511713154", 0)
+--Dlcname Just Cause 2: Black Market Boom Pack DLC
+addappid(35102, 1, "a0d77da3123f04996d9724b0c01926e1f93be749cb2cbdafd30f5ba18539a696") --Dlcname Just Cause 2: Black Market Boom Pack DLC
+setManifestid(35102, "2605655751645253389", 0)
+--Dlcname Just Cause 2: Ricos Signature Gun and Monster Truck Pack DLC
+addappid(35103, 1, "c0b11848f18f441348c49ecd3764804b70e018b3212f6ae0a5bae9ebbffc83bb") --Dlcname Just Cause 2: Ricos Signature Gun and Monster Truck Pack DLC
+setManifestid(35103, "1055634404789415346", 0)
+--Dlcname Just Cause 2: Multiplayer Mod
+addappid(259080) --Dlcname Just Cause 2: Multiplayer Mod
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Dlc Windows Depot Just Cause 2: Multiplayer Mod
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Dlc Windows Depot Just Cause 2: Multiplayer Mod
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(259081, 1, "2291d5589d16735839216456b9f70bf6fc47a843051e21e663b8533c50a86530") --Dlc Windows Depot Just Cause 2: Multiplayer Mod
+setManifestid(259081, "2816355392406302341", 7626240)
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
