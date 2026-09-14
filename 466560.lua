45e99b3f43320409ff700d467d60e6235e974c29
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 14:06:00 +0800

    add 466560.lua

--- 466560.lua
+++ 466560.lua
@@ -1,12 +1,37 @@
-addappid(466560)
-setManifestid(228989,"3514306556860204959")
-addappid(466561,0,"073598adfc95cad9b9866fb7b6625dc6ab9ff54d5f00dff91733906ab3da0042")
-setManifestid(466561,"2743093440625530244")
-addappid(466562,0,"18aa806b07e8263d3ddfb7adfef61370fec1d963ccdac5661cc60635dd17251c")
-setManifestid(466562,"2234529236490095339")
-addappid(466563,0,"7ca18b39eccf8746ad6f9eb347ba3aa9f34b7a84457dd1f68330df695df5613f")
-setManifestid(466563,"8076485880214558017")
-addappid(466564,0,"9dc832b17369b422c11784a8553ad9101ecabd9fbc8ae4f00a2011736317dc31")
-setManifestid(466564,"2376866793294585779")
-addappid(466565,0,"bb7151f689032a72537d03a88738c298310558b9b87554aa697a430b3d35ae87")
-setManifestid(466565,"2444457629100233845")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 466560.lua
+--Gamename Northgard: Definitive Edition
+addappid(466560, 1, "63356d7c9c945c769d323f9e826744e72e5cca5365b63d7f95c927edd5577769") --Mainappid Northgard: Definitive Edition
+addappid(466561, 1, "073598adfc95cad9b9866fb7b6625dc6ab9ff54d5f00dff91733906ab3da0042") --Main Windows Depot Northgard: Definitive Edition
+setManifestid(466561, "2239118191755178721", 2341946208)
+addappid(466562, 1, "18aa806b07e8263d3ddfb7adfef61370fec1d963ccdac5661cc60635dd17251c") --Main Linux Depot Northgard: Definitive Edition
+setManifestid(466562, "5197198618982432103", 7628912)
+addappid(466563, 1, "7ca18b39eccf8746ad6f9eb347ba3aa9f34b7a84457dd1f68330df695df5613f") --Main Macos Depot Northgard: Definitive Edition
+setManifestid(466563, "2855087865280393601", 4749312)
+addappid(466564, 1, "9dc832b17369b422c11784a8553ad9101ecabd9fbc8ae4f00a2011736317dc31") --Main Windows Depot Northgard: Definitive Edition
+setManifestid(466564, "2376866793294585779", 3382192)
+addappid(466565, 1, "bb7151f689032a72537d03a88738c298310558b9b87554aa697a430b3d35ae87") --Main Windows Depot Northgard: Definitive Edition
+setManifestid(466565, "6184948222963244156", 170552928)
+--Dlcname Northgard Soundtrack
+addappid(1236370) --Dlcname Northgard Soundtrack
+addappid(1236371, 1, "5df18af649cbbe0facf2c0b6d17150b3003f03b1a9bd5bf3b1cbff38837fef74") --Dlc Windows Depot Northgard Soundtrack
+setManifestid(1236371, "4808349631386335890", 57212368)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(820560) --Dlcname AppID 820560
+addappid(888650) --Dlcname Northgard - Sváfnir, Clan of the Snake
+addappid(971630) --Dlcname Northgard - Nidhogg, Clan of the Dragon
+addappid(1036760) --Dlcname Northgard - Svardilfari, Clan of the Horse
+addappid(1088640) --Dlcname Northgard - Lyngbakr, Clan of the Kraken
+addappid(1223060) --Dlcname Northgard - Himminbrjotir, Clan of the Ox
+addappid(1401490) --Dlcname Northgard - Brundr & Kaelinn, Clan of the Lynx
+addappid(1644210) --Dlcname Northgard - Ratatoskr, Clan of the Squirrel Official
+addappid(1876990) --Dlcname Northgard - Dodsvagr, Clan of the Rat
+addappid(2138500) --Dlcname Northgard - Hræsvelg, Clan of the Eagle
+addappid(2228200) --Dlcname Northgard - Cross of Vidar Expansion Pack
+addappid(2515460) --Dlcname Northgard - Kernev, Clan of the Stoat
+addappid(2658730) --Dlcname Northgard - Vordr, Clan of the Owl
+addappid(2946220) --Dlcname Northgard - Garm, Clan of the Hounds
+addappid(3352480) --Dlcname Northgard - Hafgufa, Clan of the Turtle
+addappid(3691650) --Dlcname Northgard - Gardariki, Clan of the Hippogriff
