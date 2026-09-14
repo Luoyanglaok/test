59292b23c6c853809c5ca12f1fabaf4eacf655ff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 18:45:17 +0800

    add 1089090.lua

--- 1089090.lua
+++ 1089090.lua
@@ -1,29 +1,65 @@
-addappid(1089090)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(1089091,0,"49964ca34e77b3aee8889132d8de8da22521d34a3cdfbfda9c74bdfb995a6bd3")
-setManifestid(1089091,"3066870969032248219")
-addappid(1130840,0,"139786567849d65b72a9e95a65a33c440aab4f87e84f16c5967eece9f002c31a")
-setManifestid(1130840,"382959669369915622")
-addappid(1203040,0,"23721734da64a0e0eed9e25288becfacbd86526db03272187e504b5a454ce809")
-setManifestid(1203040,"1037522985104359193")
-addappid(1203041,0,"bf69c2c5c04fd1c592fc1d9ff6100309b370feb381996dfc7586d5a0fbc5cc3e")
-setManifestid(1203041,"3289703452675459640")
-addappid(1203042,0,"daa2e5d518b63ac6b8878c5ea4c540165ffdc4f7dfa078bfc99e165da8803a65")
-setManifestid(1203042,"8487133234868163393")
-addappid(2452120,0,"b54bcf33357770b5d13a1e145d50d48aaa4dfed4be53b630f92cb1e4864838be")
-setManifestid(2452120,"7603190901144942853")
-addappid(2449250,0,"cb4ad443ede1955f0390dfd14006088a10213c6a97269acc9454b933b2395d30")
-setManifestid(2449250,"3459187442548351688")
-addappid(2449171,0,"045e54225fe6c77786bfcd2e7a99f704190372cc286e260d2ec38e4a597cebd0")
-setManifestid(2449171,"4274761634327527371")
-addappid(2449170,0,"676e606a6905bec5553299076d2e2cec4d11ccf9cd6f669e7c3d3277e732441d")
-setManifestid(2449170,"2372859754295510374")
-addappid(2449140,0,"a5b90a998cb9fd64fa11da5687004b175f17d545d9b5b2d68e8631c6bc2a841d")
-setManifestid(2449140,"4992870228369364071")
-addappid(2469232,0,"4db2e09ac0239d465a9b16bbc8a751c66cb387980ce813c30b1773a0ef26c58c")
-setManifestid(2469232,"2820906211075097558")
-addappid(2617310,0,"8c03ebaf59f386bc358b416d055f137dce85c2baf4603c07a179b3d6c669f96e")
-setManifestid(2617310,"4136459436573473184")
-addappid(2617330,0,"a5a15512c615494b0ac80cd28d8aaf459e3b5ea8c2b8071caac2ade8b61d6b45")
-setManifestid(2617330,"2413082009446378633")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1089090.lua
+--Gamename ONE PIECE: PIRATE WARRIORS 4
+addappid(1089090, 1, "710228b049046b0de02d331ad48aae083520027713d32afd7053768d212532d1") --Mainappid ONE PIECE: PIRATE WARRIORS 4
+addappid(1089091, 1, "49964ca34e77b3aee8889132d8de8da22521d34a3cdfbfda9c74bdfb995a6bd3") --Main Windows Depot ONE PIECE: PIRATE WARRIORS 4
+setManifestid(1089091, "8456438783315868429", 25505514592)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Whole Cake Island Pack
+addappid(1130840, 1, "139786567849d65b72a9e95a65a33c440aab4f87e84f16c5967eece9f002c31a") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Whole Cake Island Pack
+setManifestid(1130840, "382959669369915622", 288)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 The Worst Generation Pack
+addappid(1203040, 1, "23721734da64a0e0eed9e25288becfacbd86526db03272187e504b5a454ce809") --Dlcname ONE PIECE: PIRATE WARRIORS 4 The Worst Generation Pack
+setManifestid(1203040, "1037522985104359193", 288)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Land of Wano Pack
+addappid(1203041, 1, "bf69c2c5c04fd1c592fc1d9ff6100309b370feb381996dfc7586d5a0fbc5cc3e") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Land of Wano Pack
+setManifestid(1203041, "3289703452675459640", 288)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Anime Song Pack
+addappid(1203042, 1, "daa2e5d518b63ac6b8878c5ea4c540165ffdc4f7dfa078bfc99e165da8803a65") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Anime Song Pack
+setManifestid(1203042, "8487133234868163393", 377454320)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 The Battle of Onigashima Pack
+addappid(2449140, 1, "a5b90a998cb9fd64fa11da5687004b175f17d545d9b5b2d68e8631c6bc2a841d") --Dlcname ONE PIECE: PIRATE WARRIORS 4 The Battle of Onigashima Pack
+setManifestid(2449140, "4992870228369364071", 288)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 One Piece Film: Red Pack
+addappid(2449170, 1, "676e606a6905bec5553299076d2e2cec4d11ccf9cd6f669e7c3d3277e732441d") --Dlcname ONE PIECE: PIRATE WARRIORS 4 One Piece Film: Red Pack
+setManifestid(2449170, "2372859754295510374", 672)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Legend Dawn Pack
+addappid(2449171, 1, "045e54225fe6c77786bfcd2e7a99f704190372cc286e260d2ec38e4a597cebd0") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Legend Dawn Pack
+setManifestid(2449171, "4274761634327527371", 288)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Yamato's Grand Tour Logbook & Soul Map 1
+addappid(2449250, 1, "cb4ad443ede1955f0390dfd14006088a10213c6a97269acc9454b933b2395d30") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Yamato's Grand Tour Logbook & Soul Map 1
+setManifestid(2449250, "3459187442548351688", 96)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Onigashima Battle Law Costume
+addappid(2452120, 1, "b54bcf33357770b5d13a1e145d50d48aaa4dfed4be53b630f92cb1e4864838be") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Onigashima Battle Law Costume
+setManifestid(2452120, "7603190901144942853", 960)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 One Piece Film: Red Anime Song Pack
+addappid(2469232, 1, "4db2e09ac0239d465a9b16bbc8a751c66cb387980ce813c30b1773a0ef26c58c") --Dlcname ONE PIECE: PIRATE WARRIORS 4 One Piece Film: Red Anime Song Pack
+setManifestid(2469232, "2820906211075097558", 107978304)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Koby's Combat Chronicle & Soul Map 2
+addappid(2617310, 1, "8c03ebaf59f386bc358b416d055f137dce85c2baf4603c07a179b3d6c669f96e") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Koby's Combat Chronicle & Soul Map 2
+setManifestid(2617310, "4136459436573473184", 96)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Path to the King of the Pirates & Soul Map 3
+addappid(2617330, 1, "a5a15512c615494b0ac80cd28d8aaf459e3b5ea8c2b8071caac2ade8b61d6b45") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Path to the King of the Pirates & Soul Map 3
+setManifestid(2617330, "2413082009446378633", 96)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Shanks Additional Special Move "Divine Departure"
+addappid(3798480, 1, "d2ef4e6b851e5ac5cbb5538730ca14e8dd37e1eebe81f4debf9b0b71a356bad5") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Shanks Additional Special Move "Divine Departure"
+setManifestid(3798480, "3613911204377030935", 96)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Character Pack 7 Future Island Egghead Pack
+addappid(3846080, 1, "bf9e74c7282f3322f89ecf01ab0a507ae6db747f612e7753813d5afcbedb2922") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Character Pack 7 Future Island Egghead Pack
+setManifestid(3846080, "6052221571313314111", 288)
+--Dlcname ONE PIECE: PIRATE WARRIORS 4 Character Pack 8 Special Selection Pack
+addappid(3846090, 1, "c1c033f3a5b121486e5dd1a33e9157b7b3859dba5372c4dbeb1627cbd6d9e9e4") --Dlcname ONE PIECE: PIRATE WARRIORS 4 Character Pack 8 Special Selection Pack
+setManifestid(3846090, "7463743725217791757", 288)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1203043) --Dlcname ONE PIECE: PIRATE WARRIORS 4 Month 1 Bundle
+addtoken(1203043, "316931438837035431")
+addappid(1203044) --Dlcname ONE PIECE: PIRATE WARRIORS 4 - Charlotte Katakuri Early Unlock
+addtoken(1203044, "16499482579822565625")
+addappid(1203045) --Dlcname ONE PIECE: PIRATE WARRIORS 4 - Character Pass
+addtoken(1203045, "4729094100341584419")
+addappid(2458610) --Dlcname ONE PIECE: PIRATE WARRIORS 4 Character Pass 2
+addappid(2469230) --Dlcname ONE PIECE: PIRATE WARRIORS 4 Additional Episodes Pack
+addappid(3798490) --Dlcname ONE PIECE: PIRATE WARRIORS 4 Character Pass 3
