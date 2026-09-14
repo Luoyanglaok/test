72dda1598bfcae91913284379da8807864f84adf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:44:53 +0800

    add 2555430.lua

--- 2555430.lua
+++ 2555430.lua
@@ -1,4 +1,11 @@
-addappid(2555430)
-setManifestid(228990,"1829726630299308803")
-addappid(2555431,0,"18aecd28f1a2e9ac4ba577111bd5c963487b23b817f4b2210363f1bd00b08064")
-setManifestid(2555431,"4700721018590490225")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2555430.lua
+--Gamename TRADESMAN: Deal to Dealer
+addappid(2555430) --Mainappid TRADESMAN: Deal to Dealer
+addappid(2555431, 1, "18aecd28f1a2e9ac4ba577111bd5c963487b23b817f4b2210363f1bd00b08064") --Main Windows Depot TRADESMAN: Deal to Dealer
+setManifestid(2555431, "2871317548550278734", 98613088)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3751590) --Dlcname TRADESMAN: Deal to Dealer - HATS Supporter Pack
+addappid(5097380) --Dlcname TRADESMAN: Deal to Dealer - Donkey Support Pack
