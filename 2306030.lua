d2870e54f9f56ea5e3b4829e81cff07abd0bc217
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:53:23 +0800

    add 2306030.lua

--- 2306030.lua
+++ 2306030.lua
@@ -1,5 +1,11 @@
-addappid(2306030)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2306031,0,"b717b1927d90220e4e426ec6f87854f67e2b94fa99b29aafbab8568e0eacb1db")
-setManifestid(2306031,"6874040602280068120")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2306030.lua
+--Gamename EBOLA VILLAGE
+addappid(2306030) --Mainappid EBOLA VILLAGE
+addappid(2306031, 1, "b717b1927d90220e4e426ec6f87854f67e2b94fa99b29aafbab8568e0eacb1db") --Main Windows Depot EBOLA VILLAGE
+setManifestid(2306031, "6874040602280068120", 25820776640)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
