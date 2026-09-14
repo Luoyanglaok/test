79c46280b91f61219e053ea5e87a559477ffdbcb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 22:52:07 +0800

    add 2903970.lua

--- 2903970.lua
+++ 2903970.lua
@@ -1,9 +1,10 @@
-addappid(2903970)
-addappid(2903971,0,"6f7fcbc9b9468a4c0ea66a59b8dc0296e5596c00f7f08a349d33fbcdd4137b1d")
-setManifestid(2903971,"5195112998359873892")
-addappid(2903972)
-addappid(2903973,0,"15ecdec9ff64ca256da2f4a25c5dcd6b7c761fc5e252e9957b2c0d88ec00295f")
-setManifestid(2903973,"4531356983783757817")
-addappid(2903974,0,"bc06172bae0c8d3f98e2127c0f621545333e9021bafcf922a678ab7696b93290")
-setManifestid(2903974,"654880889028368774")
-addappid(2903975)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2903970.lua
+--Gamename Master Detective Archives: RAIN CODE Plus - Noise of Neon
+addappid(2903970) --Mainappid Master Detective Archives: RAIN CODE Plus - Noise of Neon
+addappid(2903971, 1, "6f7fcbc9b9468a4c0ea66a59b8dc0296e5596c00f7f08a349d33fbcdd4137b1d") --Main Windows Depot Master Detective Archives: RAIN CODE Plus - Noise of Neon
+setManifestid(2903971, "5195112998359873892", 41482272)
+addappid(2903973, 1, "15ecdec9ff64ca256da2f4a25c5dcd6b7c761fc5e252e9957b2c0d88ec00295f") --Main Windows Language Depot Tchinese Master Detective Archives: RAIN CODE Plus - Noise of Neon
+setManifestid(2903973, "4531356983783757817", 41483008)
+addappid(2903974, 1, "bc06172bae0c8d3f98e2127c0f621545333e9021bafcf922a678ab7696b93290") --Main Windows Language Depot Schinese Master Detective Archives: RAIN CODE Plus - Noise of Neon
+setManifestid(2903974, "654880889028368774", 41483056)
