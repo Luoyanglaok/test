e52432f278d3ce42453c0458bd2bee3fa27e60ba
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 09:02:18 +0800

    add 2195120.lua

--- 2195120.lua
+++ 2195120.lua
@@ -1,3 +1,6 @@
-addappid(2195120)
-addappid(2195121,0,"94d3a57270a0d1f85e33c7108de0457196cf7b7fd4ee7c45b233972ba6d3b962")
-setManifestid(2195121,"4743709943272306940")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2195120.lua
+--Gamename Go-Go Town!
+addappid(2195120, 1, "a11b317ebb377da13222e6e6ada384a8dd41fe74b8b722736e9ca7b21d15ebfd") --Mainappid Go-Go Town!
+addappid(2195121, 1, "94d3a57270a0d1f85e33c7108de0457196cf7b7fd4ee7c45b233972ba6d3b962") --Main Windows Depot Go-Go Town!
+setManifestid(2195121, "8259704422523530930", 899591232)
