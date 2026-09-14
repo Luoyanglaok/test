aa25e6116efa6a30e543df957cdeed180389b0dd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 18:09:58 +0800

    add 2336830.lua

--- 2336830.lua
+++ 2336830.lua
@@ -1,3 +1,6 @@
-addappid(2336830)
-addappid(2336831,0,"32daa9ea254b6c2d68464d99888d25b67a4a9a4f073da0261889c18244f0e2e5")
-setManifestid(2336831,"5241657079789787825")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2336830.lua
+--Gamename Rogue Blight Demo
+addappid(2336830) --Mainappid Rogue Blight Demo
+addappid(2336831, 1, "32daa9ea254b6c2d68464d99888d25b67a4a9a4f073da0261889c18244f0e2e5") --Main Windows Depot Rogue Blight Demo
+setManifestid(2336831, "1251140182609569905", 1032648464)
