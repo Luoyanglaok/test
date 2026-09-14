a1cdbb58513a31c4f611e967edab860f53a66f6b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:48:33 +0800

    add 3312250.lua

---
+++ 3312250.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3312250.lua
+--Gamename FEAR OF FAITH
+addappid(3312250) --Mainappid FEAR OF FAITH
+addappid(3312251, 1, "28ec0a94413d71bbda3b65aa9fd49bec3fd52d783aa03379b2975a0eca96c8f0") --Main Windows Depot FEAR OF FAITH
+setManifestid(3312251, "7579823678352615912", 15123895888)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(5055480) --Dlcname FEAR OF FAITH - Supporter Pack
+--Missing Dlcs
+--Dlcname FEAR OF FAITH - Original Soundtrack
