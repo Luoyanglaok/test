e6d89057364f2032dba86fcbef79dc81e2a1800f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 09:45:19 +0800

    add 4005220.lua

---
+++ 4005220.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4005220.lua
+--Gamename Deep Blue Sushi
+addappid(4005220) --Mainappid Deep Blue Sushi
+addappid(4005221, 1, "a6d69f88f05a5cdd8e5a895a9bb7c771ddf8cb52bdd683b7042e95adbb945a1b") --Main Windows Depot Deep Blue Sushi
+setManifestid(4005221, "1848778697134699748", 2430447152)
+--Missing Dlcs
+--Dlcname Deep Blue Sushi Playtest
