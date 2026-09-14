94eb95bf8d068de4495e08f1937665c4d076b9d8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:17:57 +0800

    add 931060.lua

---
+++ 931060.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 931060.lua
+--Gamename Tormentum II
+addappid(931060) --Mainappid Tormentum II
+addappid(931061, 1, "84fc879027a2114ec39eb42ecfc8ac4faa2b5be9062dc88bdf96714931e8b114") --Main Linux Depot Tormentum II
+setManifestid(931061, "6160964775965352685", 1911558192)
+addappid(931062, 1, "49742768f1091f1c81a9115b5f95013d13c4678689e15d0c914c753d7d6f236d") --Main Windows Depot Tormentum II
+setManifestid(931062, "8181485031778981598", 1903979280)
+addappid(931063, 1, "ffcff5c9d57debcd2b9b97221ed1916af7ccaf54ad1522a5a77cfe59582a7c45") --Main Macos Depot Tormentum II
+setManifestid(931063, "1000330692366394280", 1918300384)
+addappid(931064, 1, "bae82f887195849ccdc4ea3a82fb7582da6082b799f813d863366ca5f7d7e5f5") --Main Windows Depot Tormentum II
+setManifestid(931064, "2961097175029339018", 1908098544)
