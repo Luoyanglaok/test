48bef7e993c8ff9b6260f712df6169643eda5779
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:44:56 +0800

    add 3929550.lua

---
+++ 3929550.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3929550.lua
+--Gamename Romantic Escapades Soundtrack
+addappid(3929550) --Mainappid Romantic Escapades Soundtrack
+addappid(3929551, 1, "3713bf6a11e1b17831377fde1a19ae9f33e1dcf7365566c115733588f9a203f0") --Main Windows Depot Romantic Escapades Soundtrack
+setManifestid(3929551, "8025783756294781317", 104154528)
+addappid(3929552, 1, "185f7d52ca87254f7802c80ee5e87779f86cf7a5492c102f21411dd52cb2cc87") --Main Windows Depot Romantic Escapades Soundtrack
+setManifestid(3929552, "2299769207102751128", 690212080)
