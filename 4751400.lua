93f7b776ca5a014c0ae7cb7024cbf25ba61cc00b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 09:38:04 +0800

    add 4751400.lua

---
+++ 4751400.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4751400.lua
+--Gamename GUTTED: Infested Crypts Demo
+addappid(4751400) --Mainappid GUTTED: Infested Crypts Demo
+addappid(4751401, 1, "95d41e151cff64ca4027a4ba9c9f3e3f76bdfe2af3f02d517f60df94e7097d9c") --Main Windows Depot GUTTED: Infested Crypts Demo
+setManifestid(4751401, "8454775395767712464", 8319738128)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
