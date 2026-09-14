a421083bc2aba774370063b4e9f7fb7861ae1324
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 07:14:01 +0800

    add 2760580.lua

---
+++ 2760580.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2760580.lua
+--Gamename Mars Attracts
+addappid(2760580) --Mainappid Mars Attracts
+addappid(2760581, 1, "db681438192b6f3d7ea13050140a1056a1dcf7f9b9883ea46536f94420f9d474") --Main Windows Depot Mars Attracts
+setManifestid(2760581, "3071741486921053861", 1225299232)
+--Dlcname Mars Attracts Soundtrack
+addappid(3888500) --Dlcname Mars Attracts Soundtrack
+addappid(3888501, 1, "7426f3c6785e0b35a21a5055ecbb62ecea19070932b9247a754b2166018c7328") --Dlc Windows Depot Mars Attracts Soundtrack
+setManifestid(3888501, "3241949579371107868", 33842272)
+--Dlcname Mars Attracts - Artbook
+addappid(3888510, 1, "f55cd0d1086e93a9d7fe4d79acb255f0701501ae6fea8a3270ec8e5ba60750b3") --Dlcname Mars Attracts - Artbook
+setManifestid(3888510, "8926686536884428842", 112581680)
+--Dlcname Mars Attracts - Making The Game Featurette
+addappid(3959660, 1, "e17dc46c919a9677a1a44866a00369a2a2711bb011f840f057397eabe11846db") --Dlcname Mars Attracts - Making The Game Featurette
+setManifestid(3959660, "4026315217561317299", 1835031120)
+--Missing Dlcs
+--Dlcname Mars Attracts - Starter Pack
