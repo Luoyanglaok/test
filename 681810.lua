03eb87f22d7983699062dcaa6a193f84ce52ef47
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 13:49:34 +0800

    add 681810.lua

--- 681810.lua
+++ 681810.lua
@@ -1,5 +1,10 @@
-addappid(681810)
-addtoken(681810,"6493438147039780840")
-setManifestid(228983,"8124929965194586177")
-addappid(681811,0,"bb8d4a38f1769172b56dd4237f9a9855937bd1093978123fa987706511bf003a")
-setManifestid(681811,"1861590445548192812")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 681810.lua
+--Gamename 仙剑奇侠传六
+addappid(681810) --Mainappid 仙剑奇侠传六
+addtoken(681810, "6493438147039780840")
+addappid(681811, 1, "bb8d4a38f1769172b56dd4237f9a9855937bd1093978123fa987706511bf003a") --Main Windows Depot 仙剑奇侠传六
+setManifestid(681811, "1861590445548192812", 12815093344)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
