ca3f56efe25141aff3ff6d8a3744ae1e700d4d9c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 20:44:31 +0800

    add 2169200.lua

--- 2169200.lua
+++ 2169200.lua
@@ -1,48 +1,63 @@
-addappid(2169200)
-setManifestid(228989,"3514306556860204959")
-addappid(2169201,0,"f0442f7884739333bfdcc7544a60a639ccf65a344fb7e5312d81b71d53f3483f")
-setManifestid(2169201,"7391481385941074802")
-addappid(2169202,0,"01734f2ec3ffff33c42ef6f7337642950f08f3d7251d72eead052011085536f6")
-setManifestid(2169202,"1865254937317479303")
-addappid(2169203,0,"e6c06bbeb1a3f7316704546bd26db1ec93406963a3d84d402a9e31956f8b7cc7")
-setManifestid(2169203,"7139550291347173555")
-addappid(2169204,0,"d11132ae9b8705ec81f29311b269dda0811ffb55d25dbf766eadaac3660ad7f5")
-setManifestid(2169204,"372530551937361395")
-addappid(2169205,0,"cfdf61799a0c9e8ec83eb607ab4fd3482ee99d700d99eb6d6d194f0f41360faa")
-setManifestid(2169205,"4461957118771003967")
-addappid(2169206,0,"58856b5fdc9f0115eeef7fa218522ef3f74c223a794906e53b01541c82b9f32e")
-setManifestid(2169206,"1047435328961232841")
-addappid(2169207,0,"f2266e1c1d7f21bcf969ad780c66debeaf32eb3d564ed083458f9d64ef89d1c6")
-setManifestid(2169207,"3300678429426280478")
-addappid(2169208,0,"7130b3aa6ad5e700afca9674a124616dc9d04c226ebab6453d98cab4d8dfa2d1")
-setManifestid(2169208,"5843923210413641436")
-addappid(2169209,0,"d722416d284d1778f0911b4c0577c3e6864b72100d39bdea21056ade0129251b")
-setManifestid(2169209,"3901507899860555469")
-addappid(2561511,0,"ed396bd5a04dcb8cbf85bfb20018ce8905c58a3f245459ca2c4f18f036a5d932")
-setManifestid(2561511,"3740802027707569957")
-addappid(2561512,0,"edd809a64000a91fb3ac08691ba68bbcb629ea6f693c9811c3c32679ce936be9")
-setManifestid(2561512,"6751910486874420582")
-addappid(2561513,0,"fc8eca9e12e0f3bf1fb24524a1703fcc77a1f3f636afe8ac954922d202d3989d")
-setManifestid(2561513,"2873633407686130884")
-addappid(2561514,0,"81aa2260b9d0d8a0a4e194dee301d61e683d6aa6aa151e20ad351f579b9ce991")
-setManifestid(2561514,"6889488596835225992")
-addappid(2561515,0,"db078358a476eb1b8549563936329da856dcdf7f2dc1b5deeec74eaf78d679c6")
-setManifestid(2561515,"3195328302612406793")
-addappid(2561516,0,"1c448e10003fbe6f2be8716ca09189c53b2bd8dbfc36e6abe243d7736679a458")
-setManifestid(2561516,"1189735114395280860")
-addappid(2561517,0,"444cee8f5e54fc5971ae18ec553fd532307d6ba3b9d11036cc8a143b36f32f38")
-setManifestid(2561517,"6263522995165149636")
-addappid(2561518,0,"6439c73ba8f70236c8e73c167af4b04bbc5f2476ffab4554ccc1340e466a4c2d")
-setManifestid(2561518,"3608856189875224965")
-addappid(2561519,0,"6a940a58a5b3e5fe4b41c87e7d1e8fe5339025a1934e46835f0146b2f2a4e0ab")
-setManifestid(2561519,"1191642172033623765")
-addappid(2561521,0,"9f465c45d74c657d4af3adf7d4048c896868b411ec45d6dde4cc057d5fb0c7ae")
-setManifestid(2561521,"622305630631083236")
-addappid(2561520,0,"e9782dcff591c25264d47b0a83c5d9738ae22a2456e4d7fd5eba2ce17858d21c")
-setManifestid(2561520,"4740185705261078409")
-addappid(2729150,0,"9008f4414e442c17ceb0d7b181d9da5a763bb6407bef14f212f915e57c7e921c")
-setManifestid(2729150,"7327088656667926825")
-addappid(2800240,0,"28250feeef899032c0de5ae470b5a9b880ec93ddfd6afe2ed52ed6604ae09ea9")
-setManifestid(2800240,"3684063861755512692")
-addappid(2800250,0,"27f94daae928259b037667abc464aaa7cac0901503e2fea07fe5e7abc114468d")
-setManifestid(2800250,"1513378400177448501")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2169200.lua
+--Gamename Sniper Elite: Resistance
+addappid(2169200, 1, "ca36f7363a4c2e3ec2fd8a3c500ecb2e4f2c4d976d587a9160899dfba3532846") --Mainappid Sniper Elite: Resistance
+addappid(2169201, 1, "f0442f7884739333bfdcc7544a60a639ccf65a344fb7e5312d81b71d53f3483f") --Main Windows Depot Sniper Elite: Resistance
+setManifestid(2169201, "7391481385941074802", 300765888)
+addappid(2169202, 1, "01734f2ec3ffff33c42ef6f7337642950f08f3d7251d72eead052011085536f6") --Main Windows Depot Sniper Elite: Resistance
+setManifestid(2169202, "1865254937317479303", 18328331536)
+addappid(2169203, 1, "e6c06bbeb1a3f7316704546bd26db1ec93406963a3d84d402a9e31956f8b7cc7") --Main Windows Depot Sniper Elite: Resistance
+setManifestid(2169203, "7139550291347173555", 68146336)
+addappid(2169204, 1, "d11132ae9b8705ec81f29311b269dda0811ffb55d25dbf766eadaac3660ad7f5") --Main Windows Depot Sniper Elite: Resistance
+setManifestid(2169204, "372530551937361395", 80)
+addappid(2169205, 1, "cfdf61799a0c9e8ec83eb607ab4fd3482ee99d700d99eb6d6d194f0f41360faa") --Main Windows Depot Sniper Elite: Resistance
+setManifestid(2169205, "4461957118771003967", 2195530576)
+addappid(2169206, 1, "58856b5fdc9f0115eeef7fa218522ef3f74c223a794906e53b01541c82b9f32e") --Main Windows Depot Sniper Elite: Resistance
+setManifestid(2169206, "1047435328961232841", 41244107392)
+addappid(2169207, 1, "f2266e1c1d7f21bcf969ad780c66debeaf32eb3d564ed083458f9d64ef89d1c6") --Main Windows Depot Sniper Elite: Resistance
+setManifestid(2169207, "3300678429426280478", 18292725488)
+addappid(2169208, 1, "7130b3aa6ad5e700afca9674a124616dc9d04c226ebab6453d98cab4d8dfa2d1") --Main Windows Depot Sniper Elite: Resistance
+setManifestid(2169208, "5843923210413641436", 80)
+addappid(2169209, 1, "d722416d284d1778f0911b4c0577c3e6864b72100d39bdea21056ade0129251b") --Main Windows Language Depot French Sniper Elite: Resistance
+setManifestid(2169209, "3901507899860555469", 80)
+addappid(2561511, 1, "ed396bd5a04dcb8cbf85bfb20018ce8905c58a3f245459ca2c4f18f036a5d932") --Main Windows Language Depot German Sniper Elite: Resistance
+setManifestid(2561511, "3740802027707569957", 80)
+addappid(2561512, 1, "edd809a64000a91fb3ac08691ba68bbcb629ea6f693c9811c3c32679ce936be9") --Main Windows Language Depot Spanish Sniper Elite: Resistance
+setManifestid(2561512, "6751910486874420582", 80)
+addappid(2561513, 1, "fc8eca9e12e0f3bf1fb24524a1703fcc77a1f3f636afe8ac954922d202d3989d") --Main Windows Language Depot Brazilian Sniper Elite: Resistance
+setManifestid(2561513, "2873633407686130884", 80)
+addappid(2561514, 1, "81aa2260b9d0d8a0a4e194dee301d61e683d6aa6aa151e20ad351f579b9ce991") --Main Windows Language Depot Russian Sniper Elite: Resistance
+setManifestid(2561514, "6889488596835225992", 80)
+addappid(2561515, 1, "db078358a476eb1b8549563936329da856dcdf7f2dc1b5deeec74eaf78d679c6") --Main Windows Language Depot Polish Sniper Elite: Resistance
+setManifestid(2561515, "3195328302612406793", 80)
+addappid(2561516, 1, "1c448e10003fbe6f2be8716ca09189c53b2bd8dbfc36e6abe243d7736679a458") --Main Windows Language Depot Italian Sniper Elite: Resistance
+setManifestid(2561516, "1189735114395280860", 80)
+addappid(2561517, 1, "444cee8f5e54fc5971ae18ec553fd532307d6ba3b9d11036cc8a143b36f32f38") --Main Windows Language Depot Japanese Sniper Elite: Resistance
+setManifestid(2561517, "6263522995165149636", 80)
+addappid(2561518, 1, "6439c73ba8f70236c8e73c167af4b04bbc5f2476ffab4554ccc1340e466a4c2d") --Main Windows Language Depot Koreana Sniper Elite: Resistance
+setManifestid(2561518, "3608856189875224965", 80)
+addappid(2561519, 1, "6a940a58a5b3e5fe4b41c87e7d1e8fe5339025a1934e46835f0146b2f2a4e0ab") --Main Windows Language Depot Schinese Sniper Elite: Resistance
+setManifestid(2561519, "1191642172033623765", 80)
+addappid(2561521, 1, "9f465c45d74c657d4af3adf7d4048c896868b411ec45d6dde4cc057d5fb0c7ae") --Main Windows Language Depot Tchinese Sniper Elite: Resistance
+setManifestid(2561521, "622305630631083236", 80)
+--Dlcname Sniper Elite: Resistance - Target Führer - Lights, Camera, Achtung
+addappid(2561520, 1, "e9782dcff591c25264d47b0a83c5d9738ae22a2456e4d7fd5eba2ce17858d21c") --Dlcname Sniper Elite: Resistance - Target Führer - Lights, Camera, Achtung
+setManifestid(2561520, "4740185705261078409", 3139100384)
+--Dlcname Sniper Elite: Resistance - Vercors Vendetta Mission and Weapons Pack
+addappid(2729150, 1, "9008f4414e442c17ceb0d7b181d9da5a763bb6407bef14f212f915e57c7e921c") --Dlcname Sniper Elite: Resistance - Vercors Vendetta Mission and Weapons Pack
+setManifestid(2729150, "7327088656667926825", 2459652480)
+--Dlcname Sniper Elite: Resistance - Striking Range Mission, Weapons and Skins Pack
+addappid(2800240, 1, "28250feeef899032c0de5ae470b5a9b880ec93ddfd6afe2ed52ed6604ae09ea9") --Dlcname Sniper Elite: Resistance - Striking Range Mission, Weapons and Skins Pack
+setManifestid(2800240, "3684063861755512692", 2792178288)
+--Dlcname Sniper Elite: Resistance - Mud and Thunder Mission, Weapons and Skin Pack
+addappid(2800250, 1, "27f94daae928259b037667abc464aaa7cac0901503e2fea07fe5e7abc114468d") --Dlcname Sniper Elite: Resistance - Mud and Thunder Mission, Weapons and Skin Pack
+setManifestid(2800250, "1513378400177448501", 2166133712)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3003600) --Dlcname Sniper Elite: Resistance - M1911 pistol
+addappid(3003610) --Dlcname Sniper Elite: Resistance - Karabiner 98 Rifle
+addappid(3003620) --Dlcname Sniper Elite: Resistance Fairburne's Armoury Weapons and Skins Pack
+addappid(3003630) --Dlcname Sniper Elite: Resistance - Commando Weapon and Skin Pack
+addappid(3258690) --Dlcname Sniper Elite: Resistance Season Pass
+addappid(3618300) --Dlcname Sniper Elite: Resistance - Wrath of the Resistance Weapon and Skins Pack
