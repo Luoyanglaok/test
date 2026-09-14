c9a1c20c427d35015cb3e7141f8de826a3b24409
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 16:02:38 +0800

    add 3391510.lua

---
+++ 3391510.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3391510.lua
+--Gamename Island Market Simulator
+addappid(3391510, 1, "1487c52328f47c9cfd6496057b9c327f25c317fac4f0eec3e2c140c983a50cb4") --Mainappid Island Market Simulator
+addtoken(3391510, "8328235256214163507")
+addappid(3391511, 1, "386b71bed1a9faeeda969d8a3c0269c2a2f9877c50dcae805b20ccf9da4d737d") --Main Windows Depot Island Market Simulator
+setManifestid(3391511, "4636763628847992287", 2501309840)
+addappid(3391512, 1, "f9bf622623f034b5db33bac7b30c14666e6bf1ef9f3ae635abe5809750009e97") --Main Linux Depot Island Market Simulator
+setManifestid(3391512, "264483965193824974", 2728705216)
+--Missing Dlcs
+--Dlcname Island Market Simulator Soundtrack
