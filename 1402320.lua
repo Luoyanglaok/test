903491506eef0e2d46e2d5e6a7d07068a528e385
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:43:27 +0800

    add 1402320.lua

--- 1402320.lua
+++ 1402320.lua
@@ -1,9 +1,13 @@
-addappid(1402320)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(229005)
-setManifestid(229005,"7992454656023763365")
-addappid(1402321,0,"59551c0e72daf61029457f14cdfddda74bdf53df1b099d0356bbd79bb7c511ed")
-setManifestid(1402321,"9154068945774264997")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1402320.lua
+--Gamename Medal of Honor™: Above and Beyond
+addappid(1402320) --Mainappid Medal of Honor™: Above and Beyond
+addappid(1402321, 1, "59551c0e72daf61029457f14cdfddda74bdf53df1b099d0356bbd79bb7c511ed") --Main Windows Depot Medal of Honor™: Above and Beyond
+setManifestid(1402321, "9154068945774264997", 177970131504)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
