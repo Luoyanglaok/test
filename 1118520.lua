7f76a84ecab97b9920c039add75cc5e449fe2497
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 08:49:32 +0800

    add 1118520.lua

---
+++ 1118520.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1118520.lua
+--Gamename Paralives
+addappid(1118520, 1, "e52365d9e88efee525a3978c4991cad62a7f1bf6a53054dc6b38df387bc8dece") --Mainappid Paralives
+addappid(1118521, 1, "20543823281f3ef968fcd728fafb6786a33991bb918fbce88cd0a5c7fa49eb5a") --Main Windows Depot Paralives
+setManifestid(1118521, "5921896997568131685", 5418333536)
+addappid(1118522, 1, "e4e1d8088155be0d69686a0d236bb8693beede9db841019973b75aa8c8677ecd") --Main Macos Depot Paralives
+setManifestid(1118522, "4062677690466617525", 5488924336)
