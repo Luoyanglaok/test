8006ff7b6d86abe00c640412f48accc44cdf65f5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 09:29:15 +0800

    add 2203010.lua

---
+++ 2203010.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2203010.lua
+--Gamename AppID_2203010
+addappid(2203010) --Mainappid AppID_2203010
+addappid(2203011, 1, "075c349ce8d2d291644e87ce313e2496ece6d0a5ff016eef387905f200b525b8") --Main Windows Depot AppID_2203010
+setManifestid(2203011, "400001204044675540", 3029236224)
