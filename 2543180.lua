842812537574b0c981d26a663efd0cff7d40e764
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 03:33:35 +0800

    add 2543180.lua

--- 2543180.lua
+++ 2543180.lua
@@ -1,5 +1,12 @@
-addappid(2543180)
-addappid(2543181,0,"710ea94ede6b7341fc677cfed501f4a587ea3d576e9cecd917f056937548d455")
-setManifestid(2543181,"2810056950634253184")
-addappid(2543182,0,"02f5079b97e8a2548f5ae2e69794714c27fd0e1629ab5e911a659e5b773f3c23")
-setManifestid(2543182,"3685726511047664267")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2543180.lua
+--Gamename POPUCOM
+addappid(2543180, 1, "1f4ab702036f5e5ccc194451658a6691acbcda3fd204e1a0d9e752d2ec7377f6") --Mainappid POPUCOM
+addappid(2543181, 1, "710ea94ede6b7341fc677cfed501f4a587ea3d576e9cecd917f056937548d455") --Main Windows Depot POPUCOM
+setManifestid(2543181, "5396537959274998798", 9312424080)
+addappid(2543182, 1, "02f5079b97e8a2548f5ae2e69794714c27fd0e1629ab5e911a659e5b773f3c23") --Main Windows Depot POPUCOM
+setManifestid(2543182, "2066177807817503772", 9303069312)
+addappid(3929360) --Dlcname POPUCOM X ARKNIGHTS Collab Outfit Pack
+--Missing Dlcs
+--Dlcname POPUCOM - Too Many Clothes Pack
+--Dlcname POPUCOM - Rabbids Chic Pack
