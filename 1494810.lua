00abbc540390f6b071be4872513e8d96a3cbf022
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 09:51:22 +0800

    add 1494810.lua

--- 1494810.lua
+++ 1494810.lua
@@ -1,3 +1,12 @@
-addappid(1494810)
-addappid(1494811,0,"0a511e83da83f4164929f78b0220da9474d343cbf334e814f37b5731de7196be")
-setManifestid(1494811,"432728880845182673")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1494810.lua
+--Gamename Mortal Sin
+addappid(1494810, 1, "2c1fbb2c090610669295c79d3658b920275cb5c23866fbbb96aaca75109165c5") --Mainappid Mortal Sin
+addappid(1494811, 1, "0a511e83da83f4164929f78b0220da9474d343cbf334e814f37b5731de7196be") --Main Windows Depot Mortal Sin
+setManifestid(1494811, "6585777941221035892", 1684996480)
+--Dlcname Mortal Sin Soundtrack
+addappid(2493860) --Dlcname Mortal Sin Soundtrack
+addappid(2493861, 1, "c7a227cc59727a2deda52a4cd7001a1fa51a23281bef6d40939d01c194c26b15") --Dlc Windows Depot Mortal Sin Soundtrack
+setManifestid(2493861, "5686127106679030469", 163328448)
+addappid(2493862, 1, "8fba208a81976c5ba06ff23e68c888e5af084f7a04afb229db23c63bf8740648") --Dlc Windows Depot Mortal Sin Soundtrack
+setManifestid(2493862, "4911058005679779626", 1107236400)
