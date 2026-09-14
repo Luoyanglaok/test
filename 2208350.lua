b78b31a8964f7339b8cf2b74e0a710d8e2c3c370
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:51:57 +0800

    add 2208350.lua

---
+++ 2208350.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2208350.lua
+--Gamename Total Chaos
+addappid(2208350) --Mainappid Total Chaos
+addappid(2208351, 1, "f89145057d0fc47ccfafc6f127d9a151704248d818381e1fd71c03e5124b81fb") --Main Windows Depot Total Chaos
+setManifestid(2208351, "4427544612897027169", 12433447808)
