92a95bb8ae80d8802c4a0d6562d9af1ea0963d10
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 15:18:58 +0800

    add 617160.lua

--- 617160.lua
+++ 617160.lua
@@ -1,5 +1,11 @@
-addappid(617160)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(617161,0,"83ffafe3da1cb09d9a2865d2e53b4ee8f7306fddc7420b9aeb18bec559426e84")
-setManifestid(617161,"129600654875059209")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 617160.lua
+--Gamename Home Sweet Home
+addappid(617160) --Mainappid Home Sweet Home
+addappid(617161, 1, "83ffafe3da1cb09d9a2865d2e53b4ee8f7306fddc7420b9aeb18bec559426e84") --Main Windows Depot Home Sweet Home
+setManifestid(617161, "129600654875059209", 4613033040)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
