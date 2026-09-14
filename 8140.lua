1c08bfc2754a4c87f4d9fae510fe21d8d0a60ed4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:00:11 +0800

    add 8140.lua

--- 8140.lua
+++ 8140.lua
@@ -1,3 +1,6 @@
-addappid(8140)
-addappid(8141,0,"cea2c65903e50bec294289d441b22b0f9c65f6e6aad83700c30c174b312fe36e")
-setManifestid(8141,"495933899943708274")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 8140.lua
+--Gamename Tomb Raider: Underworld
+addappid(8140, 1, "7fba33396b153a540c41b802892b0eb7e5f3d4481b8ee73dc58c2872127ee1f0") --Mainappid Tomb Raider: Underworld
+addappid(8141, 1, "cea2c65903e50bec294289d441b22b0f9c65f6e6aad83700c30c174b312fe36e") --Main Windows Depot Tomb Raider: Underworld
+setManifestid(8141, "495933899943708274", 0)
