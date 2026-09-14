5a191d7e2ac38690cf501ea09fa115431434f8bb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 09:43:12 +0800

    add 976310.lua

--- 976310.lua
+++ 976310.lua
@@ -1,12 +1,87 @@
-addappid(976310)
-setManifestid(228986,"8782296191957114623")
-addappid(976311,0,"ba51ae236d415e34a5aa7366e15b74cd96587031f70d2acaf46ac5b390b0f4d9")
-setManifestid(976311,"6512752024476768865")
-addappid(976312,0,"37130205b90f533304b81a7d155c3f74576759eaab5e9f7d4e92478404205f60")
-setManifestid(976312,"2372612785967955744")
-addappid(976313,0,"b8e08c7b5961c5daa928f561f46d5c8438ca9c016ed0c5e9bbe5827d0b7dfdb7")
-setManifestid(976313,"4245947103270175452")
-addappid(1316183,0,"ad7b646a99d28411d65e8f72de9a641edd4c778f7bfb98681eb63f2e0e19cfb1")
-setManifestid(1316183,"2020495199252320891")
-addappid(1461160,0,"d8a15e3cfcd6d44dd3cb30fb1477f61b3b5e6f5cdbf54c2148e2aab61d609ba1")
-setManifestid(1461160,"5735050710340606220")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 976310.lua
+--Gamename Mortal Kombat11
+addappid(976310, 1, "b55a8bb9b15f963446c6555e564422e54ea199cd933c33ea19461c0ccf3ea34b") --Mainappid Mortal Kombat11
+addappid(976311, 1, "ba51ae236d415e34a5aa7366e15b74cd96587031f70d2acaf46ac5b390b0f4d9") --Main Windows Depot Mortal Kombat11
+setManifestid(976311, "6512752024476768865", 107459050464)
+addappid(976312, 1, "37130205b90f533304b81a7d155c3f74576759eaab5e9f7d4e92478404205f60") --Main Windows Depot Mortal Kombat11
+setManifestid(976312, "1715536566262804393", 63126880)
+addappid(976313, 1, "b8e08c7b5961c5daa928f561f46d5c8438ca9c016ed0c5e9bbe5827d0b7dfdb7") --Main Windows Depot Mortal Kombat11
+setManifestid(976313, "4245947103270175452", 144)
+--Dlcname Aftermath Story
+addtoken(1316183, "17258017834634481351")
+addappid(1316183, 1, "ad7b646a99d28411d65e8f72de9a641edd4c778f7bfb98681eb63f2e0e19cfb1") --Dlcname Aftermath Story
+setManifestid(1316183, "2020495199252320891", 7028296032)
+--Dlcname 4K Cinematic Pack
+addappid(1461160, 1, "d8a15e3cfcd6d44dd3cb30fb1477f61b3b5e6f5cdbf54c2148e2aab61d609ba1") --Dlcname 4K Cinematic Pack
+setManifestid(1461160, "5735050710340606220", 39102839360)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(1051620) --Dlcname Shao Kahn
+addappid(1051621) --Dlcname Frost
+addappid(1054480) --Dlcname Kombat Pack 1
+addappid(1054870) --Dlcname Kold War Skarlet
+addappid(1054871) --Dlcname Cangaceiro Kano
+addappid(1054872) --Dlcname Gold Demon Scorpion
+addappid(1054873) --Dlcname Ninja Mime Johnny Cage
+addappid(1095030) --Dlcname Shang Tsung
+addappid(1095031) --Dlcname Klassic Ninja Skins
+addappid(1102090) --Dlcname Shang Tsung Movie Skins
+addappid(1116620) --Dlcname Nightwolf
+addtoken(1116620, "12493272622588703308")
+addappid(1116621) --Dlcname Klassic Arcade Fighter Pack
+addtoken(1116621, "14663220391896737013")
+addappid(1116622) --Dlcname War Games Sonya Blade
+addtoken(1116622, "15842350001407264177")
+addappid(1160240) --Dlcname Terminator T-800
+addtoken(1160240, "17128396078543704387")
+addappid(1160241) --Dlcname Double Feature Skin Pack
+addtoken(1160241, "17223401320969048271")
+addappid(1160242) --Dlcname Cassie Quinn
+addtoken(1160242, "11207879261656686706")
+addappid(1160243) --Dlcname Masquerade Skin Pack
+addappid(1180720) --Dlcname Sindel
+addtoken(1180720, "7803015090967595566")
+addappid(1180721) --Dlcname Gothic Horror Skin Pack
+addtoken(1180721, "5781167843207166001")
+addappid(1180722) --Dlcname Klassic Cassie
+addtoken(1180722, "3557708058148800682")
+addappid(1200550) --Dlcname The Joker
+addappid(1200551) --Dlcname Time Lord of Apokolips Geras
+addappid(1200552) --Dlcname DC Elseworlds Skin Pack
+addappid(1253300) --Dlcname Spawn
+addtoken(1253300, "15979675729663044800")
+addappid(1253301) --Dlcname Hellspawn Jacqui
+addtoken(1253301, "7313392218119549452")
+addappid(1253302) --Dlcname Matinee Skin Pack
+addtoken(1253302, "7121575206995700928")
+addappid(1273970) --Dlcname Mortal Kombat 11: Aftermath + Kombat Pack Bundle
+addappid(1273971) --Dlcname Mortal Kombat 11: Aftermath Expansion
+addappid(1316180) --Dlcname Fujin
+addtoken(1316180, "7385712625288293082")
+addappid(1316181) --Dlcname Sheeva
+addtoken(1316181, "8505132378874468315")
+addappid(1316182) --Dlcname Robocop
+addtoken(1316182, "12221699817818724261")
+addappid(1316184) --Dlcname AppID 1316184
+addtoken(1316184, "14438177027151725629")
+addappid(1316185) --Dlcname Thanks A Million Johnny Cage
+addtoken(1316185, "9749570368331811933")
+addappid(1375580) --Dlcname Summer Heat Skin Pack
+addtoken(1375580, "17246012852030160350")
+addappid(1396460) --Dlcname Klassic Femme Fatale Skin Pack
+addtoken(1396460, "17334866501163965620")
+addappid(1415130) --Dlcname All Hallows' Eve Skin Pack
+addtoken(1415130, "9108617708585246192")
+addappid(1449880) --Dlcname Kombat Pack 2
+addappid(1449881) --Dlcname Rain
+addtoken(1449881, "10025843513073481141")
+addappid(1449882) --Dlcname Mileena
+addtoken(1449882, "8738007944097354560")
+addappid(1449883) --Dlcname Rambo
+addtoken(1449883, "12180670094604275504")
+addappid(1449890) --Dlcname AppID 1449890
+addtoken(1449890, "5779223872582353032")
+addappid(1471870) --Dlcname Ultimate Add-On Bundle
+addappid(1480230) --Dlcname Klassic MK Movie Skin Pack
