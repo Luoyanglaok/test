a3d9b786a18a70c928cb25b617c5f27700bb62ae
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:45:49 +0800

    add 434730.lua

--- 434730.lua
+++ 434730.lua
@@ -1,5 +1,9 @@
-addappid(434730)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(434731,0,"dfa1506ae9e0870abc1a16a85dfbdd420b7b31fe27c2745ae1b788c6c1b15c6a")
-setManifestid(434731,"8184719463490173049")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 434730.lua
+--Gamename Half Dead
+addappid(434730, 1, "a7414994e6c32a312c72d23a75ca20a0c895a9027183a2bc4abd0ef617dabbd5") --Mainappid Half Dead
+addappid(434731, 1, "dfa1506ae9e0870abc1a16a85dfbdd420b7b31fe27c2745ae1b788c6c1b15c6a") --Main Windows Depot Half Dead
+setManifestid(434731, "8184719463490173049", 266484176)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
