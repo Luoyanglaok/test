33feb53e7826b0aa7bf94dc23faa9c17e217ee75
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:17:39 +0800

    add 2677660.lua

--- 2677660.lua
+++ 2677660.lua
@@ -1,25 +1,38 @@
-addappid(2677660)
-addtoken(2677660,"11743623575295112529")
-setManifestid(228989,"3514306556860204959")
-addappid(2677661,0,"0abf0860b7a1d53e5ae7b3ed8d7e97e668fa5619be6a6ad74c140c53871868bd")
-setManifestid(2677661,"5042195164182342064")
-addappid(2677662,0,"b4089fc44110008f2b07bc97f30e298c1ae457fa69964cdb239f480259baec84")
-setManifestid(2677662,"6831917657948528277")
-addappid(2830501,0,"43cf88c0f48b755f8cb8d54bf6f3a04a2ed5551120d29b81c8ae098f26edbb0e")
-setManifestid(2830501,"6662355324070304373")
-addappid(2830502,0,"c9fbb0008c00893bad5a3815793a4439d6a7d3ebcdc9bc6fe187a30f2ab326c7")
-setManifestid(2830502,"5448201813523494044")
-addappid(2830503,0,"02d24f8070e356ac90fac5df5692432ee3e0ea0c1558e6bb78e76aade721cf68")
-setManifestid(2830503,"1458159576047134417")
-addappid(2830504,0,"ab46ce36b2648707946bc7ff2e1562491060503e25487e60fcc60e4a692f5c7b")
-setManifestid(2830504,"7266227290161751726")
-addappid(2830505,0,"3a2de50a20a1204b2d32c00deb6b5f5082560e4e7ef80e78236b4a25299a5486")
-setManifestid(2830505,"1342774415612904380")
-addappid(2830506,0,"1de09055224b1a241e117533dccab28c234008abe60653c30cd44f9ebf48e9f5")
-setManifestid(2830506,"693980898691867769")
-addappid(2830507,0,"fbde4f908358404a6544359f9c8c402bb8a40a336fb00fa92074cf4bf30143b2")
-setManifestid(2830507,"640811313163609149")
-addappid(2830508,0,"f1808e29d18c4de5461e51d4e7d3f65b9a7eee466b91359150ad4588bdbbabe6")
-setManifestid(2830508,"7390468751589638582")
-addappid(2830509,0,"7d4266463917726e639937e41582b2ba629c710a3a415dfaea90997ffd35e241")
-setManifestid(2830509,"5248135763137395051")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2677660.lua
+--Gamename Indiana Jones and the Great Circle
+addappid(2677660, 1, "4b22dac540d24fab8fe0ce68b7166d192bd13bb6488c9a18de875706c9b32011") --Mainappid Indiana Jones and the Great Circle
+addtoken(2677660, "11743623575295112529")
+addappid(2677661, 1, "0abf0860b7a1d53e5ae7b3ed8d7e97e668fa5619be6a6ad74c140c53871868bd") --Main Windows Depot Indiana Jones and the Great Circle
+setManifestid(2677661, "6237523072047006667", 115989415664)
+addappid(2677662, 1, "b4089fc44110008f2b07bc97f30e298c1ae457fa69964cdb239f480259baec84") --Main Windows Depot Indiana Jones and the Great Circle
+setManifestid(2677662, "5084568945809971997", 190599088)
+addappid(2830501, 1, "43cf88c0f48b755f8cb8d54bf6f3a04a2ed5551120d29b81c8ae098f26edbb0e") --Main Windows Depot Indiana Jones and the Great Circle
+setManifestid(2830501, "6601360459876353050", 196362160)
+addappid(2830502, 1, "c9fbb0008c00893bad5a3815793a4439d6a7d3ebcdc9bc6fe187a30f2ab326c7") --Main Windows Language Depot German Indiana Jones and the Great Circle
+setManifestid(2830502, "8007413738434142917", 273747008)
+addappid(2830503, 1, "02d24f8070e356ac90fac5df5692432ee3e0ea0c1558e6bb78e76aade721cf68") --Main Windows Language Depot Spanish Indiana Jones and the Great Circle
+setManifestid(2830503, "830671202293683776", 284307872)
+addappid(2830504, 1, "ab46ce36b2648707946bc7ff2e1562491060503e25487e60fcc60e4a692f5c7b") --Main Windows Language Depot Latam Indiana Jones and the Great Circle
+setManifestid(2830504, "6629109004288775421", 282389936)
+addappid(2830505, 1, "3a2de50a20a1204b2d32c00deb6b5f5082560e4e7ef80e78236b4a25299a5486") --Main Windows Language Depot French Indiana Jones and the Great Circle
+setManifestid(2830505, "2755672558638797836", 273512576)
+addappid(2830506, 1, "1de09055224b1a241e117533dccab28c234008abe60653c30cd44f9ebf48e9f5") --Main Windows Language Depot Italian Indiana Jones and the Great Circle
+setManifestid(2830506, "3017367679564545429", 287652160)
+addappid(2830507, 1, "fbde4f908358404a6544359f9c8c402bb8a40a336fb00fa92074cf4bf30143b2") --Main Windows Language Depot Japanese Indiana Jones and the Great Circle
+setManifestid(2830507, "278507716317809228", 272964496)
+addappid(2830508, 1, "f1808e29d18c4de5461e51d4e7d3f65b9a7eee466b91359150ad4588bdbbabe6") --Main Windows Language Depot Polish Indiana Jones and the Great Circle
+setManifestid(2830508, "6454515309235511181", 272292912)
+addappid(2830509, 1, "7d4266463917726e639937e41582b2ba629c710a3a415dfaea90997ffd35e241") --Main Windows Language Depot Brazilian Indiana Jones and the Great Circle
+setManifestid(2830509, "6972604076386304594", 269712784)
+--Dlcname Indiana Jones and the Great Circle™: The Order of Giants
+addtoken(2830500, "27111668089869839")
+addappid(2830500, 1, "c51fae5748ae85fe9a754c5e7cb2bb783d4602005897d3401b00104492820eef") --Dlcname Indiana Jones and the Great Circle™: The Order of Giants
+setManifestid(2830500, "4724397213563997330", 9376664352)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2830510) --Dlcname Indiana Jones and the Great Circle - Traveling Suit Outfit
+addappid(2830520) --Dlcname Indiana Jones and the Great Circle - Lion Tamer Whip
+addappid(2832880) --Dlcname Indiana Jones and the Great Circle - Temple of Doom™ Outfit
+addappid(3138470) --Dlcname Indiana Jones and the Great Circle: Digital Premium Upgrade
