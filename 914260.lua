67ae8df760ae04d5f9c0d6509c5494f72b3f33fa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:45:15 +0800

    add 914260.lua

--- 914260.lua
+++ 914260.lua
@@ -1,3 +1,6 @@
-addappid(914260)
-addappid(914261,0,"4831236796b051436e983b2b810e4e35773fc3287ca6873312d55da2bb42c662")
-setManifestid(914261,"739285980813446567")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 914260.lua
+--Gamename HALF DEAD 2
+addappid(914260) --Mainappid HALF DEAD 2
+addappid(914261, 1, "4831236796b051436e983b2b810e4e35773fc3287ca6873312d55da2bb42c662") --Main Windows Depot HALF DEAD 2
+setManifestid(914261, "739285980813446567", 1287800640)
