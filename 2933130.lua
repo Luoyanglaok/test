c8d935286a377bb502a674801f2e548cdc4704b1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:12:59 +0800

    add 2933130.lua

--- 2933130.lua
+++ 2933130.lua
@@ -1,7 +1,26 @@
-addappid(2933130)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2933131,0,"571136626b31c8951f3afe863e98bd7d8c833657d08311b65aa528309cb9b6ef")
-setManifestid(2933131,"5765961238007899173")
-addappid(3184500,0,"f496d95994c7faaae378d2296549e6dcf8caba08288406472ede9bf5c3cdf2ef")
-setManifestid(3184500,"9124949127565843574")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2933130.lua
+--Gamename The Lord of the Rings: Return to Moria™
+addappid(2933130, 1, "8fbaa3222c785223df5be6d58a4a7a0c8b5bd3c70973c2d6c72e6d8308193687") --Mainappid The Lord of the Rings: Return to Moria™
+addappid(2933131, 1, "571136626b31c8951f3afe863e98bd7d8c833657d08311b65aa528309cb9b6ef") --Main Windows Depot The Lord of the Rings: Return to Moria™
+setManifestid(2933131, "8240638781215522520", 18460727904)
+--Dlcname The Lord of the Rings: Return to Moria™ Soundtrack
+addappid(3080090) --Dlcname The Lord of the Rings: Return to Moria™ Soundtrack
+addappid(3080092, 1, "73414b93eecf0b11ce364c40a1d76593ba247c2edf1de4df2ccdea1d4d324875") --Dlc Windows Depot The Lord of the Rings: Return to Moria™ Soundtrack
+setManifestid(3080092, "8912566162116638582", 107327872)
+addappid(3080093, 1, "19a5dd5c25d3c01a77d5cb7d8701a93d46e11f41c50bb491b689f8084f637171") --Dlc Windows Depot The Lord of the Rings: Return to Moria™ Soundtrack
+setManifestid(3080093, "6932548281361082675", 334542608)
+--Dlcname The Lord of the Rings: Return to Moria™ Digital Art Exhibition
+addappid(3184500, 1, "f496d95994c7faaae378d2296549e6dcf8caba08288406472ede9bf5c3cdf2ef") --Dlcname The Lord of the Rings: Return to Moria™ Digital Art Exhibition
+setManifestid(3184500, "9124949127565843574", 16812080)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3308250) --Dlcname The Lord of the Rings: Return to Moria™ Rohan Pack DLC
+addappid(3349540) --Dlcname The Lord of the Rings: Return to Moria™ Yule-Tide Pack DLC
+addappid(3560550) --Dlcname The Lord of the Rings: Return to Moria™ Ent-craft Pack
+addappid(3739100) --Dlcname The Lord of the Rings: Return to Moria™ Orc Hunter Pack DLC
+addappid(3739110) --Dlcname The Lord of the Rings: Return to Moria™ Beorn's Lodge Pack DLC
+addappid(3874660) --Dlcname The Lord of the Rings: Return to Moria™ Durin's Folk Expansion
