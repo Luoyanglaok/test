dcdae70c72a527f693dac390c3106ffffe2e97bc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 14:43:21 +0800

    add 1374490.lua

--- 1374490.lua
+++ 1374490.lua
@@ -1,5 +1,19 @@
-addappid(1374490)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1374491,0,"e1a3b5ad6a0fbda32b1fceadce8be5fe365e6c98dcc654bfdef62b6aa01f48d5")
-setManifestid(1374491,"7660121480306734561")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1374490.lua
+--Gamename RuneScape: Dragonwilds
+addappid(1374490, 1, "735dc070f781ea410ecf7ffbdf051b5fdb8d98ea6054aa34aea78625b4a3e594") --Mainappid RuneScape: Dragonwilds
+addappid(1374491, 1, "e1a3b5ad6a0fbda32b1fceadce8be5fe365e6c98dcc654bfdef62b6aa01f48d5") --Main Windows Depot RuneScape: Dragonwilds
+setManifestid(1374491, "6800995088898146166", 19123242400)
+addappid(3501791, 1, "64c6c73071b1288fd80153527a20ae1d0d1467ea9f9539aef7c8245345e2f1f3") --Main Windows Depot RuneScape: Dragonwilds
+--Dlcname RuneScape: Dragonwilds Early Adopter Soundtrack
+addappid(3637470) --Dlcname RuneScape: Dragonwilds Early Adopter Soundtrack
+addappid(3637471, 1, "263c8802f36928dec41a97d9928d32e02d03586113185704040f3b41f14d13d6") --Dlc Windows Depot RuneScape: Dragonwilds Early Adopter Soundtrack
+setManifestid(3637471, "8659772693087272583", 18141808)
+addappid(3637472, 1, "e2eb8af830733a56f38d33af3efd1a36f12b294b39f727830471099458bcfd43") --Dlc Windows Depot RuneScape: Dragonwilds Early Adopter Soundtrack
+setManifestid(3637472, "7005123597752440638", 99730512)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3501790) --Dlcname RuneScape: Dragonwilds Early Adopter DLC
