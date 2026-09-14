17b0a7b771351cf500e63884704db5ca4639b6a1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:20:47 +0800

    add 3687790.lua

---
+++ 3687790.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3687790.lua
+--Gamename Deadliest Pigeon
+addappid(3687790) --Mainappid Deadliest Pigeon
+addappid(3687791, 1, "fcaf346fd5ed144336541d7ba2fb8200ad9b56294e1bc66d769af20d382b7774") --Main Windows Depot Deadliest Pigeon
+setManifestid(3687791, "8211257119694890785", 1147697456)
+addappid(5031640) --Dlcname Deadliest Pigeon - Supporter Pack
