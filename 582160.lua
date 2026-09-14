1c2fadf22269920e601dd06485e82680a89ab479
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 22:10:34 +0800

    add 582160.lua

--- 582160.lua
+++ 582160.lua
@@ -1,29 +1,74 @@
-addappid(582160)
-addappid(582161,0,"8a326c0b4da80f5c0fceff46bb180dcec85f3a7bfccbbe886f72b76554158bae")
-setManifestid(582161,"899343151187474389")
-addappid(582163,0,"8b73cf590b16f3ddd4b6a765ab5e7db92eca028f0ca011231a07689e1090c903")
-setManifestid(582163,"8719200316416197317")
-addappid(582164,0,"ea4d3242ce02b2007b8a5c1689ad1b91fe540d379da53645d7e854622d586e08")
-setManifestid(582164,"2665968185911542813")
-addappid(582165,0,"d7eae89df7ae009b38f2c5f816c6e4b95e3132dfbf7d17318d36d44baf44c312")
-setManifestid(582165,"5204817986383847126")
-addappid(582166,0,"7b16ae306a4b429f7a7bd082987732b197e2a0e5c20da339ca4e869137caf82d")
-setManifestid(582166,"5281665035933672010")
-addappid(582167,0,"94e3348b5f53858df08d295feeea54fdd2bdc699dbfa4d02229397e13dfc40f7")
-setManifestid(582167,"2360141554958804953")
-addappid(582168,0,"fed6f556c223abf9d56ee8353ea704ca7e2ffb8178cdc618941252bc3c2e91c3")
-setManifestid(582168,"319055195560046919")
-addappid(582169,0,"e87fde80316a793368f114df1d49b86a00d9cc4f7d857923160900ff97b4a4be")
-setManifestid(582169,"5694571365631700045")
-addappid(660081,0,"617237848e1792b936aa6f862d9203a1cc6ebe59162923bda191681e773c66a5")
-setManifestid(660081,"1246424898343738572")
-addappid(660082,0,"256948a62c4e67514eba700c0081ae366b428052e026c1b967e5f19c7da09355")
-setManifestid(660082,"6873063169339276628")
-addappid(660083,0,"9f84ead601362489c2465704a97b6aac7f91a1195dede5847556b51379313056")
-setManifestid(660083,"5816920034497408068")
-addappid(660084,0,"fba661c6ee698f367011611cfbf4edefc4613fcf2b90c077643e08bda518127d")
-setManifestid(660084,"2134409271002682996")
-addappid(660085,0,"d962d765069eb30826be3a4401f9f746a8793082b4a48dac32a87e4c19074dc0")
-setManifestid(660085,"4329630249127241901")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 582160.lua
+--Gamename Assassin's Creed® Origins
+addappid(582160, 1, "06770e006a0d3cb3cb1052ddc24bee833203db699098c891c5474247dfb1b175") --Mainappid Assassin's Creed® Origins
+addappid(582161, 1, "8a326c0b4da80f5c0fceff46bb180dcec85f3a7bfccbbe886f72b76554158bae") --Main Windows Depot Assassin's Creed® Origins
+setManifestid(582161, "899343151187474389", 41801873840)
+addappid(582163, 1, "8b73cf590b16f3ddd4b6a765ab5e7db92eca028f0ca011231a07689e1090c903") --Main Windows Language Depot Brazilian Assassin's Creed® Origins
+setManifestid(582163, "8719200316416197317", 462707424)
+addappid(582164, 1, "ea4d3242ce02b2007b8a5c1689ad1b91fe540d379da53645d7e854622d586e08") --Main Windows Language Depot French Assassin's Creed® Origins
+setManifestid(582164, "2665968185911542813", 451921824)
+addappid(582165, 1, "d7eae89df7ae009b38f2c5f816c6e4b95e3132dfbf7d17318d36d44baf44c312") --Main Windows Language Depot German Assassin's Creed® Origins
+setManifestid(582165, "5204817986383847126", 457586096)
+addappid(582166, 1, "7b16ae306a4b429f7a7bd082987732b197e2a0e5c20da339ca4e869137caf82d") --Main Windows Language Depot Italian Assassin's Creed® Origins
+setManifestid(582166, "5281665035933672010", 474265216)
+addappid(582167, 1, "94e3348b5f53858df08d295feeea54fdd2bdc699dbfa4d02229397e13dfc40f7") --Main Windows Language Depot Russian Assassin's Creed® Origins
+setManifestid(582167, "2360141554958804953", 420654480)
+addappid(582168, 1, "fed6f556c223abf9d56ee8353ea704ca7e2ffb8178cdc618941252bc3c2e91c3") --Main Windows Language Depot Spanish Assassin's Creed® Origins
+setManifestid(582168, "319055195560046919", 471689104)
+addappid(582169, 1, "e87fde80316a793368f114df1d49b86a00d9cc4f7d857923160900ff97b4a4be") --Main Windows Language Depot Japanese Assassin's Creed® Origins
+setManifestid(582169, "5694571365631700045", 451026032)
+--Dlcname Assassin's Creed Origins - The Hidden Ones
+addappid(662350) --Dlcname Assassin's Creed Origins - The Hidden Ones
+addappid(660084, 1, "fba661c6ee698f367011611cfbf4edefc4613fcf2b90c077643e08bda518127d") --Dlc Windows Depot Assassin's Creed Origins - The Hidden Ones
+setManifestid(660084, "2134409271002682996", 6615225872)
+--Dlcname Assassin's Creed Origins - The Curse Of The Pharaohs
+addtoken(662351, "4707066081058569263")
+addappid(662351) --Dlcname Assassin's Creed Origins - The Curse Of The Pharaohs
+addappid(660085, 1, "d962d765069eb30826be3a4401f9f746a8793082b4a48dac32a87e4c19074dc0") --Dlc Windows Depot Assassin's Creed Origins - The Curse Of The Pharaohs
+setManifestid(660085, "4329630249127241901", 13275358608)
+--Dlcname Assassin's Creed Origins - Roman Centurion Pack
+addappid(662352) --Dlcname Assassin's Creed Origins - Roman Centurion Pack
+addappid(660081, 1, "617237848e1792b936aa6f862d9203a1cc6ebe59162923bda191681e773c66a5") --Dlc Windows Depot Assassin's Creed Origins - Roman Centurion Pack
+setManifestid(660081, "1246424898343738572", 269775040)
+--Dlcname Assassin's Creed Origins - Horus Pack
+addappid(662353) --Dlcname Assassin's Creed Origins - Horus Pack
+addappid(660082, 1, "256948a62c4e67514eba700c0081ae366b428052e026c1b967e5f19c7da09355") --Dlc Windows Depot Assassin's Creed Origins - Horus Pack
+setManifestid(660082, "6873063169339276628", 278114288)
+--Dlcname AppID 722300
+addtoken(722300, "8399803014141064048")
+addappid(722300) --Dlcname AppID 722300
+addappid(660083, 1, "9f84ead601362489c2465704a97b6aac7f91a1195dede5847556b51379313056") --Dlc Windows Depot AppID 722300
+setManifestid(660083, "5816920034497408068", 269642592)
+--Share Depots
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(648280) --Dlcname AppID 648280
+addtoken(648280, "15197689054693081407")
+addappid(648281) --Dlcname AppID 648281
+addtoken(648281, "7708301591599340839")
+addappid(648282) --Dlcname AppID 648282
+addtoken(648282, "18199489657176655610")
+addappid(648283) --Dlcname AppID 648283
+addappid(648284) --Dlcname AppID 648284
+addtoken(648284, "17370715451325527879")
+addappid(648285) --Dlcname AppID 648285
+addtoken(648285, "10033086959743765839")
+addappid(660080) --Dlcname Assassin's Creed Origins - Season Pass
+addappid(662354) --Dlcname AppID 662354
+addtoken(662354, "16691004918125099330")
+addappid(662355) --Dlcname Assassin's Creed Origins - Roman Centurion Pack Uplay Activation
+addappid(662356) --Dlcname Assassin's Creed Origins - The Curse Of The Pharaohs Uplay Activation
+addappid(662357) --Dlcname AppID 662357
+addtoken(662357, "15998500631813457808")
+addappid(683850) --Dlcname Assassin's Creed Origins - Deluxe Pack
+addappid(683860) --Dlcname AppID 683860
+addtoken(683860, "16247908316574337737")
+addappid(736940) --Dlcname Assassin's Creed Origins - Standard - Uplay Activation
+addtoken(736940, "5319322542343312665")
+addappid(736941) --Dlcname AppID 736941
+addtoken(736941, "1717028301038615064")
+addappid(736942) --Dlcname AppID 736942
+addtoken(736942, "17894768497514246640")
+addappid(737850) --Dlcname AppID 737850
+addtoken(737850, "12349559016455404101")
