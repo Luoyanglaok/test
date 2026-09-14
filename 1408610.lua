d719847842a5cf773ebcea97f8764d004a27ee29
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 22:09:20 +0800

    add 1408610.lua

--- 1408610.lua
+++ 1408610.lua
@@ -1,8 +1,25 @@
-addappid(1408610)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(1408612,0,"0df254da7c6699200feea9dd9b69b97e7275867dbc594c76c55f4324cc0597e5")
-setManifestid(1408612,"4499768014919057944")
-addappid(1408613,0,"8b019c5dac3541d498213ef63801f591cb9b1fdf3cd28716f3d5ebd9e03bd03a")
-setManifestid(1408613,"6424194900758736514")
-addappid(1408614)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1408610.lua
+--Gamename Call of the Wild: The Angler™
+addappid(1408610) --Mainappid Call of the Wild: The Angler™
+addappid(1408612, 1, "0df254da7c6699200feea9dd9b69b97e7275867dbc594c76c55f4324cc0597e5") --Main Windows Depot Call of the Wild: The Angler™
+setManifestid(1408612, "4499768014919057944", 30571099600)
+addappid(1408613, 1, "8b019c5dac3541d498213ef63801f591cb9b1fdf3cd28716f3d5ebd9e03bd03a") --Main Windows Depot Call of the Wild: The Angler™
+setManifestid(1408613, "6424194900758736514", 0)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(2125860) --Dlcname Call of the Wild: The Angler™ - Classic Tackle Pack
+addappid(2125861) --Dlcname Call of the Wild: The Angler™ - Wilderness Cosmetics Pack
+addappid(2165500) --Dlcname Call of the Wild: The Angler™ – Norway Reserve
+addappid(2288430) --Dlcname Call of the Wild: The Angler™ - Spain Reserve
+addappid(2388130) --Dlcname Call of the Wild: The Angler™ - Fiskespro Gear Pack
+addappid(2500360) --Dlcname Call of the Wild: The Angler™ - T-Shirt Pack
+addappid(2500370) --Dlcname Call of the Wild: The Angler™ - South Africa Reserve
+addappid(2633140) --Dlcname Call of the Wild: The Angler™ - Winter Blast Style Pack
+addappid(2659060) --Dlcname Call of the Wild: The Angler™ - Ultra Cruiser Boat Pack
+addappid(2659070) --Dlcname Call of the Wild: The Angler™ - Winter Vehicle Cosmetics Pack
+addappid(2659080) --Dlcname Call of the Wild: The Angler™ - Rushy Gear Pack
+addappid(2936210) --Dlcname Call of the Wild: The Angler™ - Mountain Range Pack
+addappid(2997100) --Dlcname Call of the Wild: The Angler™ – LakePal Bait Boat Pack
+addappid(3046830) --Dlcname Call of the Wild The Angler™ - Japan Fishing Reserve
