a87104b7493b81c4cf269343882f2ce799d6e838
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 01:49:27 +0800

    add 1249770.lua

--- 1249770.lua
+++ 1249770.lua
@@ -1,5 +1,8 @@
-addappid(1249770)
-addappid(1249771,0,"9bb094a138f2936862b184d0605830936d584b2cc8bcbaa02c97fbcb60e91077")
-setManifestid(1249771,"8631284653544930525")
-addappid(1249772,0,"35a48dcb7ff6b78975584767b116cd598a853e23aa42a41c95ad90ad1c371769")
-setManifestid(1249772,"7309528406805440479")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1249770.lua
+--Gamename State of Decay 2 Soundtrack
+addappid(1249770) --Mainappid State of Decay 2 Soundtrack
+addappid(1249771, 1, "9bb094a138f2936862b184d0605830936d584b2cc8bcbaa02c97fbcb60e91077") --Main Windows Depot State of Decay 2 Soundtrack
+setManifestid(1249771, "8631284653544930525", 308955568)
+addappid(1249772, 1, "35a48dcb7ff6b78975584767b116cd598a853e23aa42a41c95ad90ad1c371769") --Main Windows Depot State of Decay 2 Soundtrack
+setManifestid(1249772, "7309528406805440479", 815811632)
