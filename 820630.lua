4c043062cdeb717a3138e2db24a3050901602cd0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 21:44:30 +0800

    add 820630.lua

--- 820630.lua
+++ 820630.lua
@@ -1,4 +1,15 @@
-addappid(820630)
-setManifestid(228986,"8782296191957114623")
-addappid(820631,0,"00d6ce478f0900edcaa81b3538aff08bcf28c5d6178f4030420e531c65b26902")
-setManifestid(820631,"28837882827987332")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 820630.lua
+--Gamename Metal Wolf Chaos XD
+addappid(820630) --Mainappid Metal Wolf Chaos XD
+addappid(820631, 1, "00d6ce478f0900edcaa81b3538aff08bcf28c5d6178f4030420e531c65b26902") --Main Windows Depot Metal Wolf Chaos XD
+setManifestid(820631, "28837882827987332", 3646539552)
+--Dlcname Metal Wolf Chaos XD: Pre-Order Suit
+addappid(1111140) --Dlcname Metal Wolf Chaos XD: Pre-Order Suit
+addappid(820632, 1, "72d50e38d62910061e586eeb74e309b27d03047ec6e132337cd277878a1111aa") --Dlc Windows Depot Metal Wolf Chaos XD: Pre-Order Suit
+setManifestid(820632, "3835390384513479923", 23638480)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+--Missing Dlcs
+--Dlcname Metal Wolf Chaos XD: Soundtrack
