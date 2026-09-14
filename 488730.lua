a92b3a9c869e471460b1888ebdb83ea81ec77eb4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:26:35 +0800

    add 488730.lua

--- 488730.lua
+++ 488730.lua
@@ -1,7 +1,13 @@
-addappid(488730)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(488731,0,"1766cbc8b15799108d3e13a1e28dbccb70c28be0c46bd53c60ba19ac1f433856")
-setManifestid(488731,"1741066744098976839")
-addappid(1042870,0,"c66240353eeba793248bedea3e1e7c87d7494888006d134ea726f83da5db9324")
-setManifestid(1042870,"2218262079349135664")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 488730.lua
+--Gamename God's Trigger
+addappid(488730) --Mainappid God's Trigger
+addappid(488731, 1, "1766cbc8b15799108d3e13a1e28dbccb70c28be0c46bd53c60ba19ac1f433856") --Main Windows Depot God's Trigger
+setManifestid(488731, "1741066744098976839", 3610972800)
+--Dlcname AppID 1042870
+addtoken(1042870, "14129398227345631905")
+addappid(1042870, 1, "c66240353eeba793248bedea3e1e7c87d7494888006d134ea726f83da5db9324") --Dlcname AppID 1042870
+setManifestid(1042870, "2218262079349135664", 869292224)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
