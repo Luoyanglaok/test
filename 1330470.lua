56549b6ddbe3e92760ba488a297037a4f998a857
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:57:09 +0800

    add 1330470.lua

--- 1330470.lua
+++ 1330470.lua
@@ -1,7 +1,19 @@
-addappid(1330470)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1330471,0,"90bde5ea2666baa460c9476da114f0a78a6b63f314fcfb7456c0f499841e5dfb")
-setManifestid(1330471,"7692433695764366161")
-addappid(1330472,0,"2ee73d4dd8b85b4d1601563faf22a7a679a33f4fafe1766746e8e448e4a79bcb")
-setManifestid(1330472,"1202866964769506825")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1330470.lua
+--Gamename F.I.S.T.: Forged In Shadow Torch
+addappid(1330470) --Mainappid F.I.S.T.: Forged In Shadow Torch
+addappid(1330471, 1, "90bde5ea2666baa460c9476da114f0a78a6b63f314fcfb7456c0f499841e5dfb") --Main Windows Depot F.I.S.T.: Forged In Shadow Torch
+setManifestid(1330471, "7692433695764366161", 22796910704)
+addappid(1330472, 1, "2ee73d4dd8b85b4d1601563faf22a7a679a33f4fafe1766746e8e448e4a79bcb") --Main Windows Depot F.I.S.T.: Forged In Shadow Torch
+setManifestid(1330472, "1202866964769506825", 22949248128)
+--Dlcname F.I.S.T.: Soundtrack
+addappid(1772090) --Dlcname F.I.S.T.: Soundtrack
+addappid(1772093, 1, "254ae5580673e5021de75fcf3bcdf18c272ae430653eab36c811d2efe292630f") --Dlc Windows Depot F.I.S.T.: Soundtrack
+setManifestid(1772093, "4088426863326847162", 263163088)
+addappid(1772094, 1, "c03f9d99c9c3d987dfbc8b9ca810aa76cfe40eca3f8ce4a65625b03cc1f950c3") --Dlc Windows Depot F.I.S.T.: Soundtrack
+setManifestid(1772094, "6160231013928468689", 1256174256)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
