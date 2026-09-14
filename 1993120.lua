6af4355509e27f3e23b2a95e6576d6a5b097b63b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:08:33 +0800

    add 1993120.lua

--- 1993120.lua
+++ 1993120.lua
@@ -1,6 +1,13 @@
-addappid(1993120)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1993121,0,"308823ab2d5fe7bb2b63dfc44de8526d47a3ff157687dff8282e11ae1669a4bf")
-setManifestid(1993121,"8285457995932926528")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1993120.lua
+--Gamename Death Below
+addappid(1993120) --Mainappid Death Below
+addappid(1993121, 1, "308823ab2d5fe7bb2b63dfc44de8526d47a3ff157687dff8282e11ae1669a4bf") --Main Windows Depot Death Below
+setManifestid(1993121, "8285457995932926528", 11383635712)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
