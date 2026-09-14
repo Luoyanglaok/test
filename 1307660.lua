b6d803b8faaa7948bcc4797b0ef794fbad565379
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 22:05:44 +0800

    add 1307660.lua

--- 1307660.lua
+++ 1307660.lua
@@ -1,6 +1,11 @@
-addappid(1307660)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(1307661,0,"80a2d108c008202648fe42f3e33563339592701b34d78fb420e17bb2dfb42f2a")
-setManifestid(1307661,"7368543662503221621")
-addappid(1307662)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1307660.lua
+--Gamename Ravenbound
+addappid(1307660) --Mainappid Ravenbound
+addappid(1307661, 1, "80a2d108c008202648fe42f3e33563339592701b34d78fb420e17bb2dfb42f2a") --Main Windows Depot Ravenbound
+setManifestid(1307661, "7368543662503221621", 19375065312)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(2356650) --Dlcname Ravenbound - Trailblazer's Arms DLC
+addappid(2447860) --Dlcname Ravenbound - Hammers of Ávalt
