626f09acb24b770a09c7c794034e4dc61e0e3be0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:00:58 +0800

    add 224960.lua

--- 224960.lua
+++ 224960.lua
@@ -1,4 +1,9 @@
-addappid(224960)
-setManifestid(228988,"6645201662696499616")
-addappid(224961,0,"000cf5fca54372f6c9a80bc4bd91927221a8966a0862fbdde7b22a41fa50b2d5")
-setManifestid(224961,"1018039351833983427")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 224960.lua
+--Gamename Tomb Raider I (1996)
+addappid(224960, 1, "28e52adc2084646d81c4256ac8a63d1c5f5a4119beacf26b2b6e09b8d3793977") --Mainappid Tomb Raider I (1996)
+addappid(224961, 1, "000cf5fca54372f6c9a80bc4bd91927221a8966a0862fbdde7b22a41fa50b2d5") --Main Windows Depot Tomb Raider I (1996)
+setManifestid(224961, "1018039351833983427", 232068720)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
