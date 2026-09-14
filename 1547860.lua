26c4896845cde2d5fed2c5dc8d9ed9f1108341ad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:50:57 +0800

    add 1547860.lua

--- 1547860.lua
+++ 1547860.lua
@@ -1,7 +1,11 @@
-addappid(1547860)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1547861,0,"ad0ebbb5aff9844ab24765057734a545b0d65016903e4043612d4e3d94cdf551")
-setManifestid(1547861,"4367433705465923914")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1547860.lua
+--Gamename EBOLA 3
+addappid(1547860) --Mainappid EBOLA 3
+addappid(1547861, 1, "ad0ebbb5aff9844ab24765057734a545b0d65016903e4043612d4e3d94cdf551") --Main Windows Depot EBOLA 3
+setManifestid(1547861, "4367433705465923914", 24573574256)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
