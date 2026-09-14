54377e51483fb65d286d7e62b4490b8c0d4a5f65
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 22:16:02 +0800

    add 548570.lua

--- 548570.lua
+++ 548570.lua
@@ -1,6 +1,12 @@
-addappid(548570)
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(548572,0,"1492bc687a1a552be15c6dcb0c7c677d21a0a5253a4f8845c808f0a9037ae5b2")
-setManifestid(548572,"897964294657539321")
-addappid(548573)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 548570.lua
+--Gamename RAGE 2
+addappid(548570, 1, "ec340fa82a327017d604bd08a915a36e16d58ba1dd8840e450b44a327a3b69cc") --Mainappid RAGE 2
+addappid(548572, 1, "1492bc687a1a552be15c6dcb0c7c677d21a0a5253a4f8845c808f0a9037ae5b2") --Main Windows Depot RAGE 2
+setManifestid(548572, "897964294657539321", 41309181616)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(1054920) --Dlcname RAGE 2 - Pre Order Bonus
+addappid(1054921) --Dlcname RAGE 2 - Deluxe Edition Pack
+addappid(1067980) --Dlcname RAGE 2 - RAGE Coins
