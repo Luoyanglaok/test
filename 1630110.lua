ca571ead0a3a91aa9970fcbba5a07df6e8a27b88
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:37:44 +0800

    add 1630110.lua

--- 1630110.lua
+++ 1630110.lua
@@ -1,15 +1,28 @@
-addappid(1630110)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1630111,0,"de7837e7f8f02bb4f6318a72ac8bd90ea58a6f54dd0d0c15b8fb16ce01b687c8")
-setManifestid(1630111,"8951152658736829360")
-addappid(2144690,0,"f97524694c112f9bbf31395d2a44c13d97d260e676fd671820b418be725d4235")
-setManifestid(2144690,"6513270308616875429")
-addappid(2144691,0,"b228e7a1bf8baa323c565d325b946b47ac5f881f97cb71f97e2f0a1024e82ab3")
-setManifestid(2144691,"3059617026358021726")
-addappid(3072820,0,"5244848c79a3c14e638384f14b0da4178ec771be9f18472b3cc22728bfc7117a")
-setManifestid(3072820,"8296232795360648797")
-addappid(1875760,0,"aa183566d1c2bdb8ffd52c53202cff36d969890921a39b76d31cb9dff4ae79e4")
-setManifestid(1875760,"2088483007732639747")
-addappid(3072830,0,"18717481ef30c96d9761266cf4e7f8374e6672a303a5fe16df2f3f3bcd65ae82")
-setManifestid(3072830,"4216706781397012985")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1630110.lua
+--Gamename Gungrave G.O.R.E
+addappid(1630110) --Mainappid Gungrave G.O.R.E
+addappid(1630111, 1, "de7837e7f8f02bb4f6318a72ac8bd90ea58a6f54dd0d0c15b8fb16ce01b687c8") --Main Windows Depot Gungrave G.O.R.E
+setManifestid(1630111, "8951152658736829360", 36613673280)
+--Dlcname Gungrave G.O.R.E - Street Grave
+addappid(1875760, 1, "aa183566d1c2bdb8ffd52c53202cff36d969890921a39b76d31cb9dff4ae79e4") --Dlcname Gungrave G.O.R.E - Street Grave
+setManifestid(1875760, "2088483007732639747", 64)
+--Dlcname AppID 2144690
+addtoken(2144690, "9099762904193055400")
+addappid(2144690, 1, "f97524694c112f9bbf31395d2a44c13d97d260e676fd671820b418be725d4235") --Dlcname AppID 2144690
+setManifestid(2144690, "6513270308616875429", 64)
+--Dlcname AppID 2144691
+addtoken(2144691, "18248181076385435271")
+addappid(2144691, 1, "b228e7a1bf8baa323c565d325b946b47ac5f881f97cb71f97e2f0a1024e82ab3") --Dlcname AppID 2144691
+setManifestid(2144691, "3059617026358021726", 64)
+--Dlcname Gungrave G.O.R.E - Rocket Billy Red Cadillac
+addappid(3072820, 1, "5244848c79a3c14e638384f14b0da4178ec771be9f18472b3cc22728bfc7117a") --Dlcname Gungrave G.O.R.E - Rocket Billy Red Cadillac
+setManifestid(3072820, "8296232795360648797", 64)
+--Dlcname Gungrave G.O.R.E - Harry MacDowell
+addappid(3072830, 1, "18717481ef30c96d9761266cf4e7f8374e6672a303a5fe16df2f3f3bcd65ae82") --Dlcname Gungrave G.O.R.E - Harry MacDowell
+setManifestid(3072830, "4216706781397012985", 64)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
