966fc0b508e843d031633e24099a21862232f403
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:24:48 +0800

    add 3100310.lua

---
+++ 3100310.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3100310.lua
+--Gamename Arms of God
+addappid(3100310, 1, "45d9e702a85f59b282fd11ac3de5329c43d503d91d096ac03494f3e6c869e020") --Mainappid Arms of God
+addappid(3100311, 1, "4f6806b594a4f6b601cbb79736c7004672bff04b8e0c273c0883d73839fe436c") --Main Windows Depot Arms of God
+setManifestid(3100311, "8370780378487370197", 10244565184)
+addappid(4635791, 1, "78f9759a07fc0a75e9823f0581a81f27dbf76435dae65b407b12a3c965974d40") --Main Windows Depot Arms of God
+setManifestid(4635791, "332876335214116926", 27715184)
+addappid(4635792, 1, "255dc339b7712544566d71b64c002300948ba5c221eda72bdae7a9821e363ac8") --Main Windows Depot Arms of God
+setManifestid(4635792, "2371784233355757093", 316045952)
+--Dlcname Arms of God Soundtrack
+addappid(4635790) --Dlcname Arms of God Soundtrack
+--Dlcname Arms of God - Supporter Pack
+addappid(4639880, 1, "cf806ec4dea7b88e82eb05b29e22309affec9c0f9eec14bdd8f705006aa9d04d") --Dlcname Arms of God - Supporter Pack
+setManifestid(4639880, "5470362654090727784", 1073338656)
