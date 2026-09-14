dff12460dadef46bbb1fc50560be4cd010a3830b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 14:37:47 +0800

    add 3549020.lua

---
+++ 3549020.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3549020.lua
+--Gamename Caribbean Legend: Age of Pirates
+addappid(3549020, 1, "4f7f973e7c556497e26830bb453bf127459632d5d14b57a0e234fb4080f2712a") --Mainappid Caribbean Legend: Age of Pirates
+addappid(3549021, 1, "160945e93b5d049b7bff421558fed9b170f6cae8a60ab4b01942837731bbc6ff") --Main Windows Depot Caribbean Legend: Age of Pirates
+setManifestid(3549021, "8795204892425721977", 24480174224)
+addappid(3549022, 1, "86a067e810534fe43250e86d71700ac719f575efe91399780077b5b4737eac21") --Main Windows Depot Caribbean Legend: Age of Pirates
+setManifestid(3549022, "663016823467430445", 886144)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4238730) --Dlcname Caribbean Legend: Age of Pirates - Supporter Pack
+addappid(4631460) --Dlcname Caribbean Legend: Age of Pirates - Sea Dogs
+addappid(4969720) --Dlcname Caribbean Legend: Age of Pirates - Buccaneers
