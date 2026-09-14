b6c4c35e51f9a160a7955050ca07361051dd2352
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 02:03:13 +0800

    add 495420.lua

--- 495420.lua
+++ 495420.lua
@@ -6,6 +6,12 @@ addappid(495422, 1, "82873486e64ba05260f583a4a80f88755f1a2422e37db46396fca015a35
 setManifestid(495422, "440247711752488453", 17965645728)
 addappid(495423, 1, "ae5ec7c2bc2d211ef82c94e26d5fb9ac2e75be1d90fd5d3cf9bcaee2d5159a53") --Main Windows Depot State of Decay 2: Juggernaut Edition
 setManifestid(495423, "9135075435278811908", 109527120)
+--Dlcname State of Decay 2 Soundtrack
+addappid(1249770) --Dlcname State of Decay 2 Soundtrack
+addappid(1249771, 1, "9bb094a138f2936862b184d0605830936d584b2cc8bcbaa02c97fbcb60e91077") --Dlc Windows Depot State of Decay 2 Soundtrack
+setManifestid(1249771, "8631284653544930525", 308955568)
+addappid(1249772, 1, "35a48dcb7ff6b78975584767b116cd598a853e23aa42a41c95ad90ad1c371769") --Dlc Windows Depot State of Decay 2 Soundtrack
+setManifestid(1249772, "7309528406805440479", 815811632)
 --Share Depots
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228988, "6645201662696499616", 22411856)
