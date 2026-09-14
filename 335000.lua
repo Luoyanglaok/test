c87a4f3a04815addbe9aaff209968ed64602766f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:18:37 +0800

    add 335000.lua

--- 335000.lua
+++ 335000.lua
@@ -1,4 +1,10 @@
-addappid(335000)
-addappid(335001,0,"e8062888fc0d10745bd745056c1711e238935006069395d1e9a6e29b24058745")
-setManifestid(335001,"2546909470092090819")
-addappid(335002)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 335000.lua
+--Gamename Tormentum - Dark Sorrow
+addappid(335000, 1, "7dc5b2d7b7c1cb2295c613fe0402f9505dda2509cbd241d7e6442ba1bbfaf860") --Mainappid Tormentum - Dark Sorrow
+addappid(335001, 1, "e8062888fc0d10745bd745056c1711e238935006069395d1e9a6e29b24058745") --Main Windows Depot Tormentum - Dark Sorrow
+setManifestid(335001, "2546909470092090819", 320673200)
+addappid(335002, 1, "27f37d22e72816abfba5d7db63909051e20a6cc93e0671c56ed1c848dc39e474") --Main Macos Depot Tormentum - Dark Sorrow
+setManifestid(335002, "792992174696066779", 357608208)
+--Missing Dlcs
+--Dlcname Tormentum: Artbook
