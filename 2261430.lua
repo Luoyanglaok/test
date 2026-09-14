567dc8b3f8e12158586c1cc87e58c8ed2afe83aa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:56:56 +0800

    add 2261430.lua

--- 2261430.lua
+++ 2261430.lua
@@ -1,6 +1,16 @@
-addappid(2261430)
-addappid(3976090)
-addappid(3976110)
-addappid(3976120)
-setManifestid(2261431,"2157818637787125834")
-setManifestid(3976111,"6313863028101471717")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2261430.lua
+--Gamename LumenTale: Memories of Trey
+addappid(2261430, 1, "375d10df228aa91e4676f4a6ed3c035e146a928cdeebae79f633f9e004767283") --Mainappid LumenTale: Memories of Trey
+addappid(2261431, 1, "c37596c858c344da52f712140ec94665adcc3aee9d5b990901bbd3be121aaab8") --Main Windows/Linux Depot LumenTale: Memories of Trey
+setManifestid(2261431, "2157818637787125834", 2671682176)
+addappid(2261432, 1, "e945dac3ce70daffed46d0aa7cf192691d3b0867eef8c298add542e8d72b5888") --Main Macos Depot LumenTale: Memories of Trey
+setManifestid(2261432, "5439657358734060514", 1371360112)
+--Dlcname LumenTale - Artbook
+addappid(3976110) --Dlcname LumenTale - Artbook
+addappid(3976111, 1, "6bfa22d9e38c6ee62331e151a0ba6f089b65cb4ce2d563e27c42cd371dbf680f") --Dlc Windows Depot LumenTale - Artbook
+setManifestid(3976111, "6313863028101471717", 402349312)
+addappid(3976090) --Dlcname LumenTale: Memories of Trey - Deluxe DLC
+addappid(3976100) --Dlcname Lumentale - Early Adopters Bonus Pack
+--Missing Dlcs
+--Dlcname LumenTale: Memories of Trey Soundtrack
