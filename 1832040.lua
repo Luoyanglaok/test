a6b282645f7f89a312063a748325c7bccbb843cb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:28:49 +0800

    add 1832040.lua

--- 1832040.lua
+++ 1832040.lua
@@ -1,5 +1,12 @@
-addappid(1832040)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1832041,0,"c1c5cdc2dfdfd7c552d99cd75df6df269211866648a1602567d62350e7fd4145")
-setManifestid(1832041,"1206818263585481516")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1832040.lua
+--Gamename Flintlock: The Siege of Dawn
+addappid(1832040, 1, "16dd83bada876d4d8cca911cdd02dbe19be14e94ce12e61b1804d1fbb594a435") --Mainappid Flintlock: The Siege of Dawn
+addappid(1832041, 1, "c1c5cdc2dfdfd7c552d99cd75df6df269211866648a1602567d62350e7fd4145") --Main Windows Depot Flintlock: The Siege of Dawn
+setManifestid(1832041, "1206818263585481516", 15853025392)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2783020) --Dlcname Flintlock – Deluxe Edition Upgrade
