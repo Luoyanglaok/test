ce0493e868c712dc724b1f5b4cf0202509c71678
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:12:21 +0800

    add 212500.lua

---
+++ 212500.lua
@@ -0,0 +1,34 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 212500.lua
+--Gamename The Lord of the Rings Online™
+addappid(212500) --Mainappid The Lord of the Rings Online™
+addappid(212501, 1, "f33fb64f9f6573e0da084d433e7c692e6d5149e3cd7f0e2c94931a09c47ea9f1") --Main Windows Depot The Lord of the Rings Online™
+setManifestid(212501, "8836528461813210164", 28147632896)
+addappid(212502, 1, "69becd47b73b1fa55bfa4121601e515641fe99f0bae2d939882f8d9efe2a9ac9") --Main Windows Language Depot English The Lord of the Rings Online™
+setManifestid(212502, "1319296929740556713", 3271290240)
+addappid(212503, 1, "ac35dde3ef6bc4e78001ae97cce7ef8a00cec4749449dd9a10866eb1ec252425") --Main Windows Language Depot French The Lord of the Rings Online™
+setManifestid(212503, "9111936205846700570", 3136750688)
+addappid(212504, 1, "b47d30015a2b2ab8383e27378a67e5793c04cd2b64564560a7f6cdecfc8a9086") --Main Windows Language Depot German The Lord of the Rings Online™
+setManifestid(212504, "5997078198381010133", 3952660928)
+addappid(212505, 1, "d61ea3bb936024dd8f09864e66fb6427e6aa1496a47d36d5cd4879c6269cdc31") --Main Macos Depot The Lord of the Rings Online™
+setManifestid(212505, "8385128744054315523", 1676336)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(213170) --Dlcname AppID 213170
+addappid(213171) --Dlcname AppID 213171
+addappid(213172) --Dlcname AppID 213172
+addappid(213173) --Dlcname AppID 213173
+addappid(213174) --Dlcname The Lord of the Rings Online™ Steam Pack
+addappid(213175) --Dlcname AppID 213175
+addappid(213176) --Dlcname The Lord of the Rings Online™: Riders of Rohan™ Heroic Edition Live
+addappid(213177) --Dlcname AppID 213177
+addappid(213178) --Dlcname The Lord of the Rings Online: Triple Pack
+addappid(213179) --Dlcname The Lord of the Rings Online™: Steely Dawn Starter Pack
+addappid(258800) --Dlcname AppID 258800
+addappid(258803) --Dlcname AppID 258803
+addappid(258804) --Dlcname The Lord of the Rings Online - Helm’s Deep Premium Edition
