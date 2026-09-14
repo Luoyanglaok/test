1e2d6874c348f94f52047c6868ff2643aaa98dda
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 01:12:17 +0800

    add 774171.lua

--- 774171.lua
+++ 774171.lua
@@ -4,3 +4,4 @@
 addappid(774171, 1, "334d4f66db94f122ff3175db91062b8efbdcc9d0a3bf0a569888132d4aa70f6e") --Mainappid Muse Dash
 addtoken(774171, "1032167890812251798")
 addappid(774172, 1, "c4fdeb37adb91f019b5f8f1378c0a63c3a3b90ffec89c7e0394b32a28206d2ec") --Main Windows Depot Muse Dash
+addappid(2593750) --Dlcname Muse Plus
