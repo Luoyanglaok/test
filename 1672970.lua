f2aae0317e62c5d1cfc5fd20744ff7816f9f0886
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:02:35 +0800

    add 1672970.lua

--- 1672970.lua
+++ 1672970.lua
@@ -1,5 +1,30 @@
-addappid(1672970)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1672971,0,"b42295e44d98b2ed35338c9f96b4c65ad99a839023e9e4ddef8e19ddc7addac4")
-setManifestid(1672971,"4325055371153456789")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1672970.lua
+--Gamename Minecraft Dungeons
+addappid(1672970, 1, "10ef1f37796e917fa6b2109aa8c4ab1baaf3f1dd69f9d8e4a5ce194a615d5dc8") --Mainappid Minecraft Dungeons
+addappid(1672971, 1, "b42295e44d98b2ed35338c9f96b4c65ad99a839023e9e4ddef8e19ddc7addac4") --Main Windows Depot Minecraft Dungeons
+setManifestid(1672971, "4325055371153456789", 2316929296)
+--Dlcname Minecraft Dungeons Ultimate Edition Soundtrack
+addappid(1717450) --Dlcname Minecraft Dungeons Ultimate Edition Soundtrack
+addappid(1717451, 1, "537a4254a9eb6f712e387077aea3440baed45fecc5d77804bbae0186e98ba92b") --Dlc Windows Depot Minecraft Dungeons Ultimate Edition Soundtrack
+setManifestid(1717451, "1493749821264086954", 1639013904)
+addappid(1717452, 1, "e24b4a2f0fc286cc4d2c8589889bccdf8735e4b9f76d4ee5bd9200e78baf25d0") --Dlc Windows Depot Minecraft Dungeons Ultimate Edition Soundtrack
+setManifestid(1717452, "1157092614209266083", 7514593760)
+--Dlcname Minecraft Dungeons Ultimate Edition Digital Artwork
+addappid(1726570, 1, "0e89af5042b93fdac6bf07b8155a9addf8bc19a556fed00a326ece6e7c9e7a14") --Dlcname Minecraft Dungeons Ultimate Edition Digital Artwork
+setManifestid(1726570, "2582615933734748879", 43718240)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1672980) --Dlcname Minecraft Dungeons Jungle Awakens
+addappid(1672981) --Dlcname Minecraft Dungeons Creeping Winter
+addappid(1672982) --Dlcname Minecraft Dungeons Howling Peaks
+addappid(1672983) --Dlcname Minecraft Dungeons Flames of the Nether
+addappid(1672984) --Dlcname Minecraft Dungeons Hidden Depths
+addappid(1672985) --Dlcname Minecraft Dungeons Echoing Void
+addappid(1672986) --Dlcname Minecraft Dungeons Hero DLC
+addappid(1776550) --Dlcname Minecraft Dungeons: Cloudy Climb Adventure Pass
+addappid(1850660) --Dlcname Minecraft Dungeons: Luminous Night Adventure Pass
+addappid(1979530) --Dlcname Minecraft Dungeons: Fauna Faire Adventure Pass
