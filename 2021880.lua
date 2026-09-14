df74bc72d8610c209db97e994f5390e6fc82b70c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 19:33:50 +0800

    add 2021880.lua

--- 2021880.lua
+++ 2021880.lua
@@ -1,7 +1,26 @@
-addappid(2021880)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2021881,0,"2ee32023245e16a98b624134e65f2cb58176b2ec23c8cccf202cc305d57d864e")
-setManifestid(2021881,"6450195582978987289")
-addappid(3144830,0,"46ec9fab404dcd3a5e0b2862b1f9c1a0410157b5071f35a086bd4f57f6b79beb")
-setManifestid(3144830,"1036188934286123835")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2021880.lua
+--Gamename Ara History Untold: Anniversary Edition
+addappid(2021880) --Mainappid Ara History Untold: Anniversary Edition
+addappid(2021881, 1, "2ee32023245e16a98b624134e65f2cb58176b2ec23c8cccf202cc305d57d864e") --Main Windows Depot Ara History Untold: Anniversary Edition
+setManifestid(2021881, "2028503611726990938", 17822927568)
+--Dlcname Ara: History Untold Complete OST
+addappid(3144790) --Dlcname Ara: History Untold Complete OST
+addappid(3144791, 1, "c61b244d062bbd0134123562396d4422cfb720588d1f0d750d4df98ee1227211") --Dlc Windows Depot Ara: History Untold Complete OST
+setManifestid(3144791, "7370174555807335211", 811930880)
+--Dlcname AppID 3144830
+addappid(3144830, 1, "46ec9fab404dcd3a5e0b2862b1f9c1a0410157b5071f35a086bd4f57f6b79beb") --Dlcname AppID 3144830
+setManifestid(3144830, "1036188934286123835", 51147104)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3143380) --Dlcname AppID 3143380
+addappid(3143390) --Dlcname Ara: History Untold, Modern Leader Skin Pack
+addappid(3143400) --Dlcname AppID 3143400
+addappid(3143410) --Dlcname AppID 3143410
+addappid(3143420) --Dlcname AppID 3143420
+addappid(3143430) --Dlcname AppID 3143430
+addappid(3158790) --Dlcname Ara: History Untold, Exclusive Leader Collection
+addappid(3495280) --Dlcname Ara: History Untold - Untold Scenarios
