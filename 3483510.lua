33ec38b9d556063ad358a3bc551ba8ee56c7ee60
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 19:53:51 +0800

    add 3483510.lua

--- 3483510.lua
+++ 3483510.lua
@@ -1,8 +1,15 @@
-addappid(3483510)
-addappid(4031020)
-addappid(4612130)
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(3483511,"362222618243114503")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3483510.lua
+--Gamename The Adventures of Elliot: The Millennium Tales
+addappid(3483510, 1, "a17d2e0760baf6b66e3ed3253087b686cf224be40f3975685c6115c67b8bfc0e") --Mainappid The Adventures of Elliot: The Millennium Tales
+addappid(3483511, 1, "ee7fd8a235ebc8f1301e13d7f127fe3a64d1ea1c15fcd9ef6385d2e3285b52f3") --Main Windows Depot The Adventures of Elliot: The Millennium Tales
+setManifestid(3483511, "2042765930477868769", 16419961632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4031020) --Dlcname The Adventures of Elliot: The Millennium Tales Digital Deluxe Upgrade
+addappid(4031030) --Dlcname Elliot’s Departure Pack
+--Missing Dlcs
+--Dlcname The Adventures of Elliot: The Millennium Tales Original Soundtrack
