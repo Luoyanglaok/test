0d3fa93abe795ea3fae9d7a2374eda37deb05661
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:27:24 +0800

    add 3214610.lua

--- 3214610.lua
+++ 3214610.lua
@@ -1,4 +1,13 @@
-addappid(3214610)
-addappid(4559030)
-addappid(3214611,1,"11606ac2e0d357064a1b121a23ed7409bcc466ceefa174aa3a08b275753d6115")
-setManifestid(3214611,"3142613456264705535")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3214610.lua
+--Gamename Cinderia
+addappid(3214610, 1, "ad78c2c95377bc0b6665cfcf37b4255da0a85ca5d8825e180a5171dbb08c7b0c") --Mainappid Cinderia
+addtoken(3214610, "16472338112919955715")
+addappid(3214611, 1, "11606ac2e0d357064a1b121a23ed7409bcc466ceefa174aa3a08b275753d6115") --Main Windows Depot Cinderia
+setManifestid(3214611, "3142613456264705535", 2558537232)
+addappid(4559031, 1, "4db9b2ac1c795b31cb2225866eb70f3f84740a810d5348b5be58a021508eb821") --Main Windows Depot Cinderia
+setManifestid(4559031, "2699210251144744155", 160550976)
+addappid(4559033, 1, "4f687c3db81bf9b2a5f33ecc95cbd98163e9fe607233f591a11a9eda33827bb3") --Main Windows Depot Cinderia
+setManifestid(4559033, "8280345685188849130", 2925272640)
+--Dlcname Cinderia Soundtrack
+addappid(4559030) --Dlcname Cinderia Soundtrack
