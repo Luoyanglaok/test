aa7e3ddde54f9dcfcd4ace0d2e823d281b376365
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:23:04 +0800

    add 2013360.lua

--- 2013360.lua
+++ 2013360.lua
@@ -1,5 +1,12 @@
-addappid(2013360)
-addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
-setManifestid(2013361,"2544562877228408797")
-setManifestid(2316610,"308827296916478043")
-setManifestid(228988,"6645201662696499616")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2013360.lua
+--Gamename The Oregon Trail
+addappid(2013360) --Mainappid The Oregon Trail
+addappid(2013361, 1, "9643afa3acb621adbeb971d73541c1f34acec3a900acd47fad34fa7f29286cb7") --Main Windows Depot The Oregon Trail
+setManifestid(2013361, "2544562877228408797", 820452448)
+--Dlcname The Oregon Trail — Cowboys and Critters DLC
+addappid(2316610, 1, "6466a3da267a52fe833cef91956d9d65ed9cf45726c3917194970ab040375fae") --Dlcname The Oregon Trail — Cowboys and Critters DLC
+setManifestid(2316610, "308827296916478043", 4816)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
