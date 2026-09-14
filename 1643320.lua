21249035f50eec8928990b0dbadad1bd0d51d579
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 01:30:33 +0800

    add 1643320.lua

--- 1643320.lua
+++ 1643320.lua
@@ -1,6 +1,23 @@
-addappid(1643320)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-addappid(1643321,0,"71c29e58911c13991edea02ac06db5f5991e43e52cdcd76c19126d657c4a38e7")
-setManifestid(1643321,"6288361973254418915")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1643320.lua
+--Gamename S.T.A.L.K.E.R. 2: Heart of Chornobyl
+addappid(1643320, 1, "b783726cbbe01aa0588ea0829164ac9b24665c7394b3d13497e62639b8c083ea") --Mainappid S.T.A.L.K.E.R. 2: Heart of Chornobyl
+addappid(1643321, 1, "71c29e58911c13991edea02ac06db5f5991e43e52cdcd76c19126d657c4a38e7") --Main Windows Depot S.T.A.L.K.E.R. 2: Heart of Chornobyl
+setManifestid(1643321, "4518958509884143477", 155718642624)
+addappid(3765020, 1, "b1b8603692741b1a378078069c67a9908bda61991fdec23100fef15970450358") --Main Windows Depot S.T.A.L.K.E.R. 2: Heart of Chornobyl
+setManifestid(3765020, "4750257545716219112", 0)
+--Dlcname S.T.A.L.K.E.R. 2: Cost of Hope
+addappid(3765021, 1, "e876a04dbb1e5e6acbfd5615bb7fef20660736442090c3eb1454398f00b7dd4f") --Dlc Windows Depot S.T.A.L.K.E.R. 2: Cost of Hope
+setManifestid(3765021, "6104496397739427170", 18751498208)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(1661620) --Dlcname S.T.A.L.K.E.R. 2: Heart of Chornobyl - Deluxe Upgrade
+addappid(1661621) --Dlcname S.T.A.L.K.E.R. 2: Heart of Chornobyl - Ultimate Content
+addappid(1661622) --Dlcname S.T.A.L.K.E.R. 2: Heart of Chornobyl - Season Pass
+addappid(1661623) --Dlcname S.T.A.L.K.E.R. 2: Heart of Chornobyl - Pre-order Bonus
+addappid(3399310) --Dlcname AppID 3399310
