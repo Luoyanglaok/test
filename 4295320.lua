6ed23f56213230e6250dc591639b7d4dac7c28cf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 08:38:23 +0800

    add 4295320.lua

---
+++ 4295320.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4295320.lua
+--Gamename Crown Of Silence
+addappid(4295320) --Mainappid Crown Of Silence
+addappid(4295321, 1, "4a4a6ccc05f22a8196314aa90f6bdeb97609fb095d31015ae3daa1294da6ad3a") --Main Windows Depot Crown Of Silence
+setManifestid(4295321, "6014867777933371762", 152072016)
