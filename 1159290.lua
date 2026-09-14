9bf4b9b0cec365f2c8604f0f718397cd49f83865
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 13:58:15 +0800

    add 1159290.lua

--- 1159290.lua
+++ 1159290.lua
@@ -1,5 +1,11 @@
-addappid(1159290)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1159291,0,"76e3cc8dd03421623d2c8f64513c49d675e0eea185c1bf6795bf9263c8f93535")
-setManifestid(1159291,"2736260411608603330")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1159290.lua
+--Gamename The Bloodline
+addappid(1159290, 1, "a6ea6756fc4be566469565ac0e62cdc5f83ca70f6a84d423d2c3f1b7e1bf2725") --Mainappid The Bloodline
+addappid(1159291, 1, "76e3cc8dd03421623d2c8f64513c49d675e0eea185c1bf6795bf9263c8f93535") --Main Windows Depot The Bloodline
+setManifestid(1159291, "962759323218907421", 18786016288)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
