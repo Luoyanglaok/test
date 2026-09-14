0058fcfbaf2fe2e24e217228e997c934149c599b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 09:47:34 +0800

    add 643610.lua

---
+++ 643610.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 643610.lua
+--Gamename Mortal Blitz
+addappid(643610) --Mainappid Mortal Blitz
+addappid(643611, 1, "7aeb49a1e0556c09c60bb873839fe246522c4b5215331d8111b47bd307f1c326") --Main Windows Depot Mortal Blitz
+setManifestid(643611, "1355344062398317549", 867140192)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
