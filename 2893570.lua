e34cbab00da6b769117a519724805d2c3baea8ec
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:32:32 +0800

    add 2893570.lua

---
+++ 2893570.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2893570.lua
+--Gamename DRAGON QUEST I & II HD-2D Remake
+addappid(2893570, 1, "aa4a4be24d92e54b64cc267674240a5b53b1899a79fa27b8a40ebb3b0cd942aa") --Mainappid DRAGON QUEST I & II HD-2D Remake
+addappid(2893571, 1, "511f158856c1819c10a9d2916437455351f4fde9144e27c85cdbd64769037309") --Main Windows Depot DRAGON QUEST I & II HD-2D Remake
+setManifestid(2893571, "5258844536216943832", 15661540432)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3707290) --Dlcname DRAGON QUEST I & II HD-2D Remake: Trouble-Free Travel Kit
+addappid(3707300) --Dlcname DRAGON QUEST I & II HD-2D Remake: Adventurous Accessory Kit
+addappid(3707310) --Dlcname DRAGON QUEST I & II HD-2D Remake: Reasonably Priced Regalia Kit
+addappid(3904130) --Dlcname DRAGON QUEST I & II HD-2D Remake: Beginner Battler Kit
+addappid(3904140) --Dlcname DRAGON QUEST I & II HD-2D Remake: Bona Fide Battle Kit
+addappid(3904280) --Dlcname DRAGON QUEST I & II HD-2D Remake: Essential Items Kit
