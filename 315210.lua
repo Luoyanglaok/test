4b80ed7a4079bc04ad4b34c7f662f8f467f9722c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 22:02:52 +0800

    add 315210.lua

--- 315210.lua
+++ 315210.lua
@@ -1,19 +1,30 @@
-addappid(315210)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(315211,0,"fabb38272098c3396b2e9f2e47f227b066bb5213920e64570afdac89330de21f")
-setManifestid(315211,"4443834196174197060")
-addappid(315212,0,"e61a5741667835bdbabe5aebc4348b520f97ed4156dbe3fd607e21a0093fcda0")
-setManifestid(315212,"262943211739857545")
-addappid(315213,0,"1e45ecc87c25598487a2fc6aad943085a92d9a220160bf9ed500e24c9669961f")
-setManifestid(315213,"780674397389142294")
-addappid(315214,0,"60dc3874da94039f2347261a4d0a3bd1060c1517abadc1872e54887c945cd494")
-setManifestid(315214,"3390447772311628478")
-addappid(315215,0,"e49e77edadfd05a3d449e867449d8c08a73ca9ff247841043dd594e427ef1c17")
-setManifestid(315215,"6878152482888731645")
-addappid(315216,0,"cc2767e57c58cc4ce064181e68edb5a71427e4bc638e57ab71ac5b4f089c5b3f")
-setManifestid(315216,"7361648533418468010")
-addappid(315217,0,"92f2682dbe683871e849b0dc13ec3e397c03d3683720af203527c36e2bd83ab6")
-setManifestid(315217,"520277345523201444")
-addappid(315218,0,"0a751ccef3454275907a0e4349eb73ddf25f9c4cfadf6574927bf1cc50f4a387")
-setManifestid(315218,"4075749034952776096")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 315210.lua
+--Gamename Suicide Squad: Kill the Justice League
+addappid(315210, 1, "95ec8198f00727372db21a4bf642496b6e37b7800a01e8c36e886da8135049da") --Mainappid Suicide Squad: Kill the Justice League
+addappid(315211, 1, "fabb38272098c3396b2e9f2e47f227b066bb5213920e64570afdac89330de21f") --Main Windows Depot Suicide Squad: Kill the Justice League
+setManifestid(315211, "4443834196174197060", 86664833808)
+addappid(315212, 1, "e61a5741667835bdbabe5aebc4348b520f97ed4156dbe3fd607e21a0093fcda0") --Main Windows Language Depot German Suicide Squad: Kill the Justice League
+setManifestid(315212, "262943211739857545", 5908732448)
+addappid(315213, 1, "1e45ecc87c25598487a2fc6aad943085a92d9a220160bf9ed500e24c9669961f") --Main Windows Language Depot Spanish Suicide Squad: Kill the Justice League
+setManifestid(315213, "780674397389142294", 6175225904)
+addappid(315214, 1, "60dc3874da94039f2347261a4d0a3bd1060c1517abadc1872e54887c945cd494") --Main Windows Language Depot Latam Suicide Squad: Kill the Justice League
+setManifestid(315214, "3390447772311628478", 5850156176)
+addappid(315215, 1, "e49e77edadfd05a3d449e867449d8c08a73ca9ff247841043dd594e427ef1c17") --Main Windows Language Depot French Suicide Squad: Kill the Justice League
+setManifestid(315215, "6878152482888731645", 5705310384)
+addappid(315216, 1, "cc2767e57c58cc4ce064181e68edb5a71427e4bc638e57ab71ac5b4f089c5b3f") --Main Windows Language Depot Italian Suicide Squad: Kill the Justice League
+setManifestid(315216, "7361648533418468010", 5866465632)
+addappid(315217, 1, "92f2682dbe683871e849b0dc13ec3e397c03d3683720af203527c36e2bd83ab6") --Main Windows Language Depot Japanese Suicide Squad: Kill the Justice League
+setManifestid(315217, "520277345523201444", 5880750016)
+addappid(315218, 1, "0a751ccef3454275907a0e4349eb73ddf25f9c4cfadf6574927bf1cc50f4a387") --Main Windows Language Depot Brazilian Suicide Squad: Kill the Justice League
+setManifestid(315218, "4075749034952776096", 5856418128)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2474100) --Dlcname Suicide Squad: Kill the Justice League - Deluxe Edition Content
+addappid(2474110) --Dlcname Suicide Squad: Kill the Justice League - Digital Deluxe Edition Content
+addappid(2716350) --Dlcname Suicide Squad: Kill the Justice League - Digital Deluxe Edition Upgrade
+--Missing Dlcs
+--Dlcname Suicide Squad: Kill the Justice League - Preorder Content
