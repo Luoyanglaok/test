8dea2e6314d2d7ab130188afd34dff737c2365bc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 08:46:46 +0800

    add 3394840.lua

---
+++ 3394840.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3394840.lua
+--Gamename Clawed
+addappid(3394840) --Mainappid Clawed
+addappid(3394841, 1, "34e22bdc0f2bf98f9a7158ae217d5327977e69ee3e731fda1b4bc2639e4f2674") --Main Windows Depot Clawed
+setManifestid(3394841, "1890154721923807997", 16850726480)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
