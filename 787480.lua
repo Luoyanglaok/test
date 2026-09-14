dd5e63b8b00bcd678122b08bf06c211b7196871a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 21:33:26 +0800

    add 787480.lua

--- 787480.lua
+++ 787480.lua
@@ -1,5 +1,67 @@
-addappid(787480)
-addappid(787481,0,"21ec2f837fe1dfd3cb4acfb74871f9989d4d75fb292e7ab5b902cbc58f577a63")
-setManifestid(787481,"6847712794471749420")
-addappid(787482,0,"3c49d038e4a9825a77cb8e9756d0aeb6e4971363b027c52c6477cac104710a40")
-setManifestid(787482,"4929657870284100407")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 787480.lua
+--Gamename Phoenix Wright: Ace Attorney Trilogy
+addappid(787480, 1, "ec62c289573f1dc0dad9bd9cc25aa24b9d7995ea15de5bcfa4c145aa98fd5016") --Mainappid Phoenix Wright: Ace Attorney Trilogy
+addappid(787481, 1, "21ec2f837fe1dfd3cb4acfb74871f9989d4d75fb292e7ab5b902cbc58f577a63") --Main Windows Depot Phoenix Wright: Ace Attorney Trilogy
+setManifestid(787481, "4596784893399743796", 2847929728)
+addappid(787482, 1, "3c49d038e4a9825a77cb8e9756d0aeb6e4971363b027c52c6477cac104710a40") --Main Windows Depot Phoenix Wright: Ace Attorney Trilogy
+setManifestid(787482, "4929657870284100407", 4626768)
+addappid(787483, 1, "c2905c5640fde8855cb314b8a55650eb6625c631a1047fca98dff7a43125b691") --Main Windows Depot Phoenix Wright: Ace Attorney Trilogy
+addappid(787484, 1, "e068c94228149aa99007649f734b768c4ab7a51d4513556df1cf33ee1c2a0126") --Main Windows Depot Phoenix Wright: Ace Attorney Trilogy
+addappid(787485, 1, "db3ba13aeb08c580831ddae67b40c2e92853f73a5e8f88b8a196d1a9d0703959") --Main Windows Depot Phoenix Wright: Ace Attorney Trilogy
+addappid(1335144, 1, "081afcd02c37c56dee05c39146455e0f11349f45eac8f24772b5fbfa37debca1") --Main Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy
+setManifestid(1335144, "7931820672299760975", 119957552)
+addappid(1335145, 1, "acef4cc2dea296691c0d91193fd26f65ddfbd01105c8ffaf61c1c1335cdbcc08") --Main Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy
+setManifestid(1335145, "1862475894113303752", 382734896)
+addappid(1335146, 1, "1bf7f127030c7f32616124d1f1c46b19bb7cbd8faa58651a917b95a43c00f078") --Main Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy
+setManifestid(1335146, "2792814299510378859", 158949872)
+addappid(1335154, 1, "c49ae27b119e5ca9574bf72712db145be5fa44ae09e83611a99c7f4fe32c201a") --Main Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy
+setManifestid(1335154, "358687043244415925", 124408224)
+addappid(1335155, 1, "1184daabc3de5eee8d924834258f9fb2c185d9304b056d4b53d8eba5fddb4e60") --Main Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy
+setManifestid(1335155, "5494670510324976932", 358747776)
+addappid(1335156, 1, "f2dc1db3ebf00833c60688ffa9b6d9cbb98c7ffcdffc2c8516a1a826dfde7517") --Main Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy
+setManifestid(1335156, "3273806374450441459", 157159040)
+addappid(1335164, 1, "d7be3c4367c507e6ebe370092c9e7f34ff43003e663a752d8aed99ea4a03655e") --Main Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy
+setManifestid(1335164, "2810436598558608039", 155481424)
+addappid(1335165, 1, "3977935e565374b625ae89a2951068619bf9b08b7715f17d11fdf01253aba0ca") --Main Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy
+setManifestid(1335165, "5969709782251354894", 445772928)
+addappid(1335166, 1, "77d4d9e72193312bc904e676c1c423efbc9eab94ff20c0f665821731097c5ed1") --Main Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy
+setManifestid(1335166, "9129160447779904163", 197510160)
+--Dlcname Phoenix Wright: Ace Attorney Trilogy - Turnabout Tunes
+addappid(1032760) --Dlcname Phoenix Wright: Ace Attorney Trilogy - Turnabout Tunes
+addappid(1032761, 1, "31e5103081f6718fb15d5632a83a541b24e13599f8c4e18ccb5235300d4f6cf7") --Dlc Windows Depot Phoenix Wright: Ace Attorney Trilogy - Turnabout Tunes
+setManifestid(1032761, "5474027449965208217", 169413232)
+addappid(1032762, 1, "7ba072ffb7c2d7d67aedfb29bddbedf50f9e0bc270ad1b7a63a731a0e7a6f90e") --Dlc Windows Depot Phoenix Wright: Ace Attorney Trilogy - Turnabout Tunes
+setManifestid(1032762, "4219034964509969620", 473836256)
+addappid(1032763, 1, "bd6adedebee07a136b08d92685afdbdf9eede76f6390fb639c6e2ba8dd2d8de7") --Dlc Windows Depot Phoenix Wright: Ace Attorney Trilogy - Turnabout Tunes
+setManifestid(1032763, "4548522945940583456", 215291520)
+addappid(1032764, 1, "7648bb2342ab3fae12c46a51184d2e4d625f5c13dab7666cb48605a04618d591") --Dlc Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy - Turnabout Tunes
+setManifestid(1032764, "4695207193065142738", 169384000)
+addappid(1032765, 1, "026d9df7afaaf5c1d36f42934a843b9329e9d01f17148febd5728268a013c6a0") --Dlc Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy - Turnabout Tunes
+setManifestid(1032765, "5870508246175722387", 473807840)
+addappid(1032766, 1, "011f13d9b8fa09c316b4808e7391ef1508ae14dc8fb2519d1f01bb56f652a792") --Dlc Windows Language Depot Japanese Phoenix Wright: Ace Attorney Trilogy - Turnabout Tunes
+setManifestid(1032766, "7835843548254387927", 215319984)
+--Dlcname Phoenix Wright: Ace Attorney Original Soundtrack
+addappid(1335140) --Dlcname Phoenix Wright: Ace Attorney Original Soundtrack
+addappid(1335141, 1, "3f0f6b8ce32e9e908faffd68ccfa6e315b94a935d698682e6f21666c5eeab3a6") --Dlc Windows Depot Phoenix Wright: Ace Attorney Original Soundtrack
+setManifestid(1335141, "3136377446818984263", 119960352)
+addappid(1335142, 1, "4b6cfd1e718fa9ded6e8aacc6b06bec94d10ce5511537ad583fa278477b6ad7a") --Dlc Windows Depot Phoenix Wright: Ace Attorney Original Soundtrack
+setManifestid(1335142, "3697061898464209202", 382734640)
+addappid(1335143, 1, "2ffdeb68fd05dcada973adb3f8900aa2f1ca5eb88f3a3a7696fe569d7fe447f6") --Dlc Windows Depot Phoenix Wright: Ace Attorney Original Soundtrack
+setManifestid(1335143, "5392304002229925995", 158950336)
+--Dlcname Phoenix Wright: Ace Attorney − Justice for All Original Soundtrack
+addappid(1335150) --Dlcname Phoenix Wright: Ace Attorney − Justice for All Original Soundtrack
+addappid(1335151, 1, "e652a52c38b26af7915e82b354a9c772c338ab3873ace738c9906e20a2db1a33") --Dlc Windows Depot Phoenix Wright: Ace Attorney − Justice for All Original Soundtrack
+setManifestid(1335151, "7370836466425153469", 124410480)
+addappid(1335152, 1, "7fa6bb807f53a5fc236d5fbcd70f68dba86c5d3dad4ed45227832c0e2580a3a5") --Dlc Windows Depot Phoenix Wright: Ace Attorney − Justice for All Original Soundtrack
+setManifestid(1335152, "2900730919138150658", 358748336)
+addappid(1335153, 1, "eeb9d11fbcf8d3edd57167d13b3f28cdc408e0268260df6271db0cd656e11f53") --Dlc Windows Depot Phoenix Wright: Ace Attorney − Justice for All Original Soundtrack
+setManifestid(1335153, "4467657357844279767", 157159408)
+--Dlcname Phoenix Wright: Ace Attorney − Trials and Tribulations Original Soundtrack
+addappid(1335160) --Dlcname Phoenix Wright: Ace Attorney − Trials and Tribulations Original Soundtrack
+addappid(1335161, 1, "93f5df20bc8195154e7cab144cc763117713f5112c22a684d59b14d81720fd46") --Dlc Windows Depot Phoenix Wright: Ace Attorney − Trials and Tribulations Original Soundtrack
+setManifestid(1335161, "571110628509610948", 155482128)
+addappid(1335162, 1, "fac8883468c184389225d0b6edcba85ef2674aff35dac6ad96b18e9e5d1d0dab") --Dlc Windows Depot Phoenix Wright: Ace Attorney − Trials and Tribulations Original Soundtrack
+setManifestid(1335162, "4328365996337572002", 445773984)
+addappid(1335163, 1, "928264b0a396af117df78a2551d4b9582a594efb86d3e26e74efe9e51f4b8b58") --Dlc Windows Depot Phoenix Wright: Ace Attorney − Trials and Tribulations Original Soundtrack
+setManifestid(1335163, "1192987634431533711", 197511264)
