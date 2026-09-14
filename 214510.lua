c0c129a75d8619d2ec9309a23fee1edec631e62f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:11:04 +0800

    add 214510.lua

--- 214510.lua
+++ 214510.lua
@@ -1,3 +1,8 @@
-addappid(214510)
-addappid(214511,0,"b350edf474fc7c130bad45e320962c01c7199096257f9a90bbb82ca8fd4addbf")
-setManifestid(214511,"1362028623425855695")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 214510.lua
+--Gamename LEGO® The Lord of the Rings™
+addappid(214510, 1, "de9e3052e14daa66f3bf02e6ad1c670934a7acaa62b8e850d63e76afd2b8b840") --Mainappid LEGO® The Lord of the Rings™
+addappid(214511, 1, "b350edf474fc7c130bad45e320962c01c7199096257f9a90bbb82ca8fd4addbf") --Main Windows Depot LEGO® The Lord of the Rings™
+setManifestid(214511, "1362028623425855695", 0)
+addappid(214512, 1, "08A91E4952B7307B33C0451A00EE43A2D4D9A690F0586C16DA75EF13A88FE4DB") --Main Windows Depot LEGO® The Lord of the Rings™
+setManifestid(214512, "4622145475954685404", 1796528)
