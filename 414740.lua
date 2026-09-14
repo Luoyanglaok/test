36cfc67c7f0690cae099080ec0fcf4c2bb39bf63
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:30:41 +0800

    add 414740.lua

--- 414740.lua
+++ 414740.lua
@@ -1,7 +1,22 @@
-addappid(414740)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(414741,0,"ff8f400443edb658fefc39c3014ae7265c3fb432677ae79e767f11c98afbef6c")
-setManifestid(414741,"2378251033650905184")
-addappid(707081,0,"f50e65e650663e1e56a87ae26e05a1d7d59d477a85329af1711ec3317c6707c3")
-setManifestid(707081,"3048988680620135682")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 414740.lua
+--Gamename RAID: World War II
+addappid(414740) --Mainappid RAID: World War II
+addappid(414741, 1, "ff8f400443edb658fefc39c3014ae7265c3fb432677ae79e767f11c98afbef6c") --Main Windows Depot RAID: World War II
+setManifestid(414741, "8577464110292793858", 11451132928)
+--Dlcname RAID Special Edition
+addappid(707080) --Dlcname RAID Special Edition
+addappid(707081, 1, "f50e65e650663e1e56a87ae26e05a1d7d59d477a85329af1711ec3317c6707c3") --Dlc Windows Depot RAID Special Edition
+setManifestid(707081, "3048988680620135682", 199114752)
+--Dlcname RAID: World War II Soundtrack
+addappid(720860) --Dlcname RAID: World War II Soundtrack
+addappid(720861, 1, "c36fe6f1ff1de1c920d44086427094c57e4b569881cf06ab4e9dd5acde452548") --Dlc Windows Depot RAID: World War II Soundtrack
+setManifestid(720861, "2233576060323883982", 359787200)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(707070) --Dlcname RAID Pre-order
+addappid(804050) --Dlcname AppID 804050
+addtoken(804050, "3496343535275419338")
