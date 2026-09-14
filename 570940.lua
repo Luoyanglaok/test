f7782fcda323f07295a50a66710a80fcba8a269c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 21:49:15 +0800

    add 570940.lua

--- 570940.lua
+++ 570940.lua
@@ -1,8 +1,13 @@
-addappid(570940)
-setManifestid(228986,"8782296191957114623")
-addappid(570941,0,"e782efcd07a6c765e4393f9b7b6285c09819e62da5f1573e40f15b98303799fe")
-setManifestid(570941,"2900566705832487094")
-addappid(570942,0,"74947c74eb866acac60dc108e9c8144e499da81f312f965cfe46ef6c280afaef")
-setManifestid(570942,"5866652778102292477")
-addappid(570943,0,"b789ea361d9a3e2ff6beb324f2a264fa8db3a49e9ccf390e88332f3850ce5735")
-setManifestid(570943,"2385633508013979570")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 570940.lua
+--Gamename DARK SOULS™: REMASTERED
+addappid(570940, 1, "6d9470246b6832653270865630596846ae5147ee704382c8afa94b1ebd383ff2") --Mainappid DARK SOULS™: REMASTERED
+addappid(570941, 1, "e782efcd07a6c765e4393f9b7b6285c09819e62da5f1573e40f15b98303799fe") --Main Windows Depot DARK SOULS™: REMASTERED
+setManifestid(570941, "2900566705832487094", 7010281424)
+addappid(570942, 1, "74947c74eb866acac60dc108e9c8144e499da81f312f965cfe46ef6c280afaef") --Main Windows Language Depot Japanese DARK SOULS™: REMASTERED
+setManifestid(570942, "5866652778102292477", 382503824)
+addappid(570943, 1, "b789ea361d9a3e2ff6beb324f2a264fa8db3a49e9ccf390e88332f3850ce5735") --Main Windows Depot DARK SOULS™: REMASTERED
+setManifestid(570943, "2385633508013979570", 18578144)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
