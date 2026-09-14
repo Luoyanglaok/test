965ad5681ade8c0a92ea9eb459b0ec18b048d0f6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:23:22 +0800

    add 2114740.lua

--- 2114740.lua
+++ 2114740.lua
@@ -1,8 +1,21 @@
-addappid(2114740)
-setManifestid(228989,"3514306556860204959")
-addappid(2114741,0,"bfa23044329db73bff7359d046baad4e5d546333e7c34042a0065de17eb5350e")
-setManifestid(2114741,"30965641548323185")
-addappid(2500160,0,"503b5732abefe07001d4c001350558da4747a61c52562da4882858a9e2be2235")
-setManifestid(2500160,"1591885379887391557")
-addappid(2114742,0,"a0950a724a09a3bc1a126e4543d910aafa0e83727704db0fd11745547ea0bd66")
-setManifestid(2114742,"8954525284424133301")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2114740.lua
+--Gamename Blasphemous 2
+addappid(2114740, 1, "d0cee23f4516768ad92273125718aba5c92b538394b7f0736ca0fa8370b00b27") --Mainappid Blasphemous 2
+addappid(2114741, 1, "bfa23044329db73bff7359d046baad4e5d546333e7c34042a0065de17eb5350e") --Main Windows Depot Blasphemous 2
+setManifestid(2114741, "3607367436135451382", 2729615376)
+addappid(2114742, 1, "a0950a724a09a3bc1a126e4543d910aafa0e83727704db0fd11745547ea0bd66") --Main Windows Depot Blasphemous 2
+setManifestid(2114742, "3631315998253881066", 28154960)
+--Dlcname Blasphemous 2 - Digital Artbook
+addappid(2500160, 1, "503b5732abefe07001d4c001350558da4747a61c52562da4882858a9e2be2235") --Dlcname Blasphemous 2 - Digital Artbook
+setManifestid(2500160, "1591885379887391557", 130562784)
+--Dlcname Blasphemous 2 - OST
+addappid(2500170) --Dlcname Blasphemous 2 - OST
+addappid(2500171, 1, "b9d3fa57528bb80453d54ddd9ac898b8d48e37176b1eaad3e8474b7b738c533d") --Dlc Windows Depot Blasphemous 2 - OST
+setManifestid(2500171, "4007254842697346922", 388096592)
+addappid(2500172, 1, "ef49e4da832ef9aace023b93a7e0bd9129f58b731032bc2422e64e41a2505370") --Dlc Windows Depot Blasphemous 2 - OST
+setManifestid(2500172, "2569618554006666471", 2645453872)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2879370) --Dlcname Blasphemous 2 - Mea Culpa
