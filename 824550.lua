6c6be30a5cbca09913f77236fb67737e0dbe6bde
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 19:51:17 +0800

    add 824550.lua

--- 824550.lua
+++ 824550.lua
@@ -1,8 +1,26 @@
-addappid(824550)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(824551,0,"d7322acca8725452a82e5121292fd903e830f8a91921e30a735b09ec79c9df51")
-setManifestid(824551,"3110662896725176459")
-addappid(1938455)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 824550.lua
+--Gamename SD GUNDAM BATTLE ALLIANCE
+addappid(824550) --Mainappid SD GUNDAM BATTLE ALLIANCE
+addappid(824551, 1, "d7322acca8725452a82e5121292fd903e830f8a91921e30a735b09ec79c9df51") --Main Windows Depot SD GUNDAM BATTLE ALLIANCE
+setManifestid(824551, "3110662896725176459", 26398963648)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1938440) --Dlcname SD GUNDAM BATTLE ALLIANCE - MS Development Aid: Start Dash Pack and Early Unlock: “Gundam”
+addappid(1938441) --Dlcname SD GUNDAM BATTLE ALLIANCE - Early Unlock: “Knight Gundam”
+addappid(1938442) --Dlcname SD GUNDAM BATTLE ALLIANCE - Early Unlock: Musha Gundam
+addappid(1938443) --Dlcname SD GUNDAM BATTLE ALLIANCE - “SD Gundam World Sangoku Soketsuden” Pack
+addappid(1938444) --Dlcname SD GUNDAM BATTLE ALLIANCE - Early Unlock: “Command Gundam”
+addappid(1938445) --Dlcname SD GUNDAM BATTLE ALLIANCE MS Development - Super Pack Lv1
+addappid(1938446) --Dlcname SD GUNDAM BATTLE ALLIANCE MS Development - Super Pack Lv2
+addappid(1938450) --Dlcname SD GUNDAM BATTLE ALLIANCE MS Development - Super Pack Lv3
+addappid(1938451) --Dlcname SD GUNDAM BATTLE ALLIANCE - Season Pass
+addappid(1938452) --Dlcname SD GUNDAM BATTLE ALLIANCE Unit and Scenario Pack 1: Legend & Succession
+addappid(1938453) --Dlcname SD GUNDAM BATTLE ALLIANCE Unit and Scenario Pack 2: Knights of Moon & Light
+addappid(1938454) --Dlcname SD GUNDAM BATTLE ALLIANCE Unit and Scenario Pack 3: Flash & Rebirth
+addappid(1938456) --Dlcname SD GUNDAM BATTLE ALLIANCE - Limit Break Materials Pack 1
+addappid(2231720) --Dlcname SD GUNDAM BATTLE ALLIANCE Bonus Pack
+addappid(2291030) --Dlcname SD GUNDAM BATTLE ALLIANCE - Mobile Suit Gundam: The Witch from Mercury Pack
