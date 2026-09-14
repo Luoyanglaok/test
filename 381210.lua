65bc0597cf5c9ed2dbde9868c0dd5e3616550fff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 13:44:06 +0800

    add 381210.lua

--- 381210.lua
+++ 381210.lua
@@ -1,46 +1,109 @@
-addappid(381210)
-addappid(5060920)
-addappid(4791830)
-addappid(4318580)
-addappid(4019590)
-addappid(3884020)
-addappid(3796620)
-addappid(3698790)
-addappid(3448670)
-addappid(3261720)
-addappid(3103150)
-addappid(3024580)
-addappid(2958440)
-addappid(2661270)
-addappid(2661250)
-addappid(2922730)
-addappid(2526530)
-addappid(2526540)
-addappid(2526550)
-addappid(2656010)
-addappid(1135280)
-addappid(2515990)
-addappid(2469400)
-addappid(2399750)
-addappid(2102730)
-addappid(1899750)
-addappid(1634040)
-addappid(1622941)
-addappid(1622940)
-addappid(700282)
-addappid(1009821)
-addappid(750381)
-addappid(700280)
-addappid(1089270)
-addappid(1324970)
-addappid(661770)
-addappid(509060)
-addappid(573940)
-addappid(627510)
-addappid(492140)
-addappid(381211,1,"66477a849ed619510d127ff3f05d4d37b38a04fd7074654337053dfe817798ca")
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(381211,"7492657323105270481")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 381210.lua
+--Gamename Dead by Daylight
+addappid(381210, 1, "f17be424bd1dc965706d5527f803a0c4ae2c7aae87449be6f5bab17b7ac3a20c") --Mainappid Dead by Daylight
+addappid(381211, 1, "66477a849ed619510d127ff3f05d4d37b38a04fd7074654337053dfe817798ca") --Main Windows Depot Dead by Daylight
+setManifestid(381211, "7492657323105270481", 63919006752)
+addappid(381213, 1, "525dbbc5d40bca2cd810f8cb48945f893aa16ca7c1ea90895e0d0c498f183ff5") --Main Windows Depot Dead by Daylight
+setManifestid(381213, "6590975724534196178", 78111024)
+--Dlcname Dead by Daylight: Original Soundtrack
+addappid(492140) --Dlcname Dead by Daylight: Original Soundtrack
+--Dlcname Dead by Daylight: Digital Art Book
+addappid(492360) --Dlcname Dead by Daylight: Digital Art Book
+addappid(381212, 1, "d514d170477029b1fc73e157f9550e3591facfc4508a7568deaf4599fc3a54b4") --Dlc Windows Depot Dead by Daylight: Digital Art Book
+setManifestid(381212, "5921550076694751899", 31327728)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(489980) --Dlcname Dead by Daylight: Deluxe Edition
+addappid(509060) --Dlcname Dead by Daylight: The Last Breath Chapter
+addappid(509061) --Dlcname Dead by Daylight: Dweard + Sharp Mustache Jake
+addappid(516780) --Dlcname Dead by Daylight: The Trapper's Mask - Chuckles
+addappid(528320) --Dlcname AppID 528320
+addappid(528321) --Dlcname AppID 528321
+addappid(528322) --Dlcname AppID 528322
+addappid(528323) --Dlcname AppID 528323
+addappid(528324) --Dlcname AppID 528324
+addappid(530710) --Dlcname Dead by Daylight - The 80's Suitcase
+addappid(530711) --Dlcname Dead by Daylight - The Halloween Chapter
+addappid(534130) --Dlcname AppID 534130
+addtoken(534130, "10361710686141983297")
+addappid(534131) --Dlcname AppID 534131
+addtoken(534131, "11636565156979948298")
+addappid(554380) --Dlcname AppID 554380
+addappid(554381) --Dlcname AppID 554381
+addappid(556360) --Dlcname AppID 556360
+addappid(556370) --Dlcname AppID 556370
+addappid(556371) --Dlcname AppID 556371
+addappid(562130) --Dlcname AppID 562130
+addtoken(562130, "11319676841542177788")
+addappid(566500) --Dlcname AppID 566500
+addappid(573240) --Dlcname AppID 573240
+addappid(573940) --Dlcname Dead by Daylight - Left Behind
+addtoken(573940, "18105380312885266784")
+addappid(577900) --Dlcname AppID 577900
+addtoken(577900, "6140623372922911603")
+addappid(577901) --Dlcname Dead by Daylight - Headcase
+addappid(582600) --Dlcname AppID 582600
+addappid(620230) --Dlcname AppID 620230
+addappid(627510) --Dlcname Dead by Daylight - Charity Case
+addappid(639710) --Dlcname AppID 639710
+addtoken(639710, "13687018254219025029")
+addappid(661770) --Dlcname Dead by Daylight - A Lullaby for the Dark
+addappid(700280) --Dlcname Dead by Daylight: LEATHERFACE
+addappid(700282) --Dlcname Dead by Daylight - A Nightmare on Elm Street
+addappid(724650) --Dlcname Dead by Daylight - Feng Golden Exclusive
+addappid(750380) --Dlcname Dead by Daylight - David's Donkey Jacket
+addappid(750381) --Dlcname Dead by Daylight - The Saw Chapter
+addappid(799200) --Dlcname Dead by Daylight - Curtain Call
+addappid(925750) --Dlcname AppID 925750
+addappid(971790) --Dlcname AppID 971790
+addappid(1009820) --Dlcname AppID 1009820
+addappid(1009821) --Dlcname Dead by Daylight - Ash vs Evil Dead
+addappid(1089270) --Dlcname Dead by Daylight: Ghost Face
+addappid(1135280) --Dlcname Dead by Daylight - Stranger Things Chapter
+addappid(1199880) --Dlcname Dead by Daylight - Cursed Legacy
+addappid(1251000) --Dlcname Dead by Daylight - Chains of Hate
+addappid(1324970) --Dlcname Dead By Daylight - Silent Hill Chapter
+addappid(1324971) --Dlcname Dead by Daylight - Silent Hill Cosmetic Pack
+addtoken(1324971, "18081697420483979648")
+addappid(1408020) --Dlcname AppID 1408020
+addappid(1474030) --Dlcname Dead by Daylight - A Binding of Kin Chapter
+addappid(1557310) --Dlcname AppID 1557310
+addappid(1622940) --Dlcname Dead by Daylight - Terror Expansion Pack
+addappid(1622941) --Dlcname Dead by Daylight - Escape Expansion Pack
+addappid(1634040) --Dlcname Dead by Daylight - Resident Evil Chapter
+addappid(1734080) --Dlcname AppID 1734080
+addappid(1763310) --Dlcname Dead by Daylight - Hour of the Witch Chapter
+addappid(1804690) --Dlcname AppID 1804690
+addappid(1899750) --Dlcname Dead by Daylight - Sadako Rising Chapter
+addappid(1985790) --Dlcname Dead by Daylight - Roots of Dread Chapter
+addappid(2102730) --Dlcname Dead by Daylight - Resident Evil: PROJECT W Chapter
+addappid(2198470) --Dlcname Dead by Daylight - Forged in Fog Chapter
+addappid(2294610) --Dlcname Dead by Daylight - Tools of Torment Chapter
+addappid(2399750) --Dlcname Dead by Daylight - End Transmission Chapter
+addappid(2405130) --Dlcname Dead by Daylight x Attack on Titan: Armored Pack
+addappid(2405140) --Dlcname Dead by Daylight x Attack on Titan: War Hammer Pack
+addappid(2469400) --Dlcname Dead by Daylight - Nicolas Cage Chapter Pack
+addappid(2515990) --Dlcname Dead by Daylight - Alien Chapter Pack
+addappid(2526530) --Dlcname Dead by Daylight - Maddening Darkness Pack
+addappid(2526540) --Dlcname Dead by Daylight - Old Wounds Pack
+addappid(2526550) --Dlcname Dead by Daylight - Macabre Tales Pack
+addappid(2656010) --Dlcname Dead by Daylight - Chucky Chapter
+addappid(2661250) --Dlcname Dead by Daylight - Alan Wake Chapter
+addappid(2661270) --Dlcname Dead by Daylight - All Things Wicked Chapter
+addappid(2922730) --Dlcname Dead by Daylight - Endless Hunt Pack
+addappid(2958440) --Dlcname Dead by Daylight - Dungeons & Dragons
+addappid(3024580) --Dlcname Dead by Daylight - Tomb Raider Chapter
+addappid(3103150) --Dlcname Dead by Daylight - Castlevania Chapter
+addappid(3261720) --Dlcname Dead by Daylight - Doomed Course Chapter
+addappid(3448670) --Dlcname Dead by Daylight - Tokyo Ghoul
+addappid(3698790) --Dlcname Dead by Daylight - Steady Pulse
+addappid(3796620) --Dlcname Dead by Daylight: Five Nights at Freddy’s
+addappid(3884020) --Dlcname Dead by Daylight: The Walking Dead
+addappid(4019590) --Dlcname Dead by Daylight - Sinister Grace
+addappid(4318580) --Dlcname Dead by Daylight: Stranger Things Chapter 2
+addappid(4791830) --Dlcname Dead by Daylight: Jason
+addappid(5060920) --Dlcname Dead by Daylight: Chorus of Sin
