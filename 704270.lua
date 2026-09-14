f73ae78714caf05548e80e01f5ea2908409a8dd1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 22:17:09 +0800

    add 704270.lua

--- 704270.lua
+++ 704270.lua
@@ -1,5 +1,38 @@
-addappid(704270)
-setManifestid(228986,"8782296191957114623")
-setManifestid(229004,"5220958916987797232")
-addappid(704271,0,"bec34bd7a4b8b9e13563f62a6dab312a919c164703f0739da3f138a71e64db7c")
-setManifestid(704271,"3510212211800621258")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 704270.lua
+--Gamename Generation Zero®
+addappid(704270, 1, "b5d735b4ebfbeebc9582b143f79a8cecd22170b23d4afc909e16cc5b5ec57cac") --Mainappid Generation Zero®
+addappid(704271, 1, "bec34bd7a4b8b9e13563f62a6dab312a919c164703f0739da3f138a71e64db7c") --Main Windows Depot Generation Zero®
+setManifestid(704271, "3510212211800621258", 41846589392)
+addappid(704272, 1, "c12abbdf59e0442db3e06aefe5d28e5e961d1a74f15d0ae9d1be52bf5bf3b768") --Main Windows Depot Generation Zero®
+setManifestid(704272, "3067257149903567997", 345974592)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(974380) --Dlcname AppID 974380
+addtoken(974380, "14292488639897807766")
+addappid(1062400) --Dlcname Generation Zero® - Tubular Vanity Pack
+addappid(1062420) --Dlcname Generation Zero® - Schweet Vanity Pack
+addappid(1099600) --Dlcname Generation Zero® - Blockbuster Vanity Pack
+addappid(1103830) --Dlcname Generation Zero® - Bikes
+addappid(1157550) --Dlcname Generation Zero® - Alpine Unrest
+addappid(1173930) --Dlcname Generation Zero® - Rivals & Experimental Weapons
+addappid(1252740) --Dlcname Generation Zero® - Apparel Crafting
+addappid(1301740) --Dlcname Generation Zero® - FNIX Rising
+addappid(1450340) --Dlcname Generation Zero® - US Weapons Pack
+addappid(1580920) --Dlcname Generation Zero® - Soviet Weapons Pack
+addappid(1610690) --Dlcname Generation Zero® - US Weapons Pack 2
+addappid(1610720) --Dlcname Generation Zero® - Base Defense Pack
+addappid(1771050) --Dlcname Generation Zero® - Resistance Weapons Pack
+addappid(1850670) --Dlcname Generation Zero® - Base Support Pack
+addappid(2009530) --Dlcname Generation Zero® - Tactical Equipment Pack
+addappid(2144760) --Dlcname Generation Zero® - Camo Weapon Skins Pack
+addappid(2144761) --Dlcname Generation Zero® - Motorbikes Pack
+addappid(2200740) --Dlcname Generation Zero® - Eastern European Weapons Pack
+addappid(2334430) --Dlcname Generation Zero® - Tactical Equipment Pack 2
+addappid(2473770) --Dlcname Generation Zero® - Companion Accessories Pack
+addappid(2591100) --Dlcname Generation Zero® - Advanced Intelligence Cosmetics Pack
+addappid(2689180) --Dlcname Generation Zero® - Reinforced Flakmoped Pack
+addappid(2955520) --Dlcname Generation Zero® - Heavy Weapons Pack
