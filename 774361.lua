9add6bb33568aa91f726d1855a62230e0046d254
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:23:19 +0800

    add 774361.lua

--- 774361.lua
+++ 774361.lua
@@ -1,19 +1,30 @@
-addappid(774361)
-addappid(774362,0,"3c0c23fe108b94cd3807435b2bc40180ca3b94536b58e0c39accee663fed16bb")
-setManifestid(774362,"6869787296347543622")
-addappid(774363,0,"d993ef6a175b97835fad06b6a121a239090b64f1af4301e21a277f7f72e8f89a")
-setManifestid(774363,"413831179119247709")
-addappid(774364,0,"95c96562c2ca7d1a11075a78297194a2012a6b4dc3e795f3604c305717fc6e17")
-setManifestid(774364,"871042798300177276")
-addappid(774365,0,"bab5ef14e351920c47897af01aa779ee7cd40e781f30acf4090c8d7bbbea62a0")
-setManifestid(774365,"1710297333649588908")
-addappid(774366,0,"f4cd54024c406a8d40ad60806ebdab94a7dd83dfd7a6440e044d3906517ab6ea")
-setManifestid(774366,"2489827079019319511")
-addappid(774367,0,"57fe84004caab7f31c8e3aa070d594f60d9fc847aaec1a8279fe5d96e008e66a")
-setManifestid(774367,"175520263097441601")
-addappid(1143850,0,"de17e7ba029ac0ab3018562633b8542f7336481472c8789b26ec665192b68cd4")
-setManifestid(1143850,"3317964550509557174")
-addappid(1143851,0,"5069e7a80c60cb1e87e237739720a7dee49178f81b14c5290401aed931c8b9e1")
-setManifestid(1143851,"6736759184537561598")
-addappid(1143854,0,"7d3b229e73e5756b4152675ce1fd12d0318ca3f4f74269d6b93fa41f1cb6a6c3")
-setManifestid(1143854,"7538235104894025746")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 774361.lua
+--Gamename Blasphemous
+addappid(774361, 1, "43a3d6f20da5979e761222db12b5836304682ef40eae785e8d5c1fea1dcd4e7a") --Mainappid Blasphemous
+addappid(774362, 1, "3c0c23fe108b94cd3807435b2bc40180ca3b94536b58e0c39accee663fed16bb") --Main Windows Depot Blasphemous
+setManifestid(774362, "4978333590109687115", 695370560)
+addappid(774363, 1, "d993ef6a175b97835fad06b6a121a239090b64f1af4301e21a277f7f72e8f89a") --Main Macos Depot Blasphemous
+setManifestid(774363, "6555253313188407979", 697928352)
+addappid(774364, 1, "95c96562c2ca7d1a11075a78297194a2012a6b4dc3e795f3604c305717fc6e17") --Main Linux Depot Blasphemous
+setManifestid(774364, "871042798300177276", 713829024)
+addappid(1143854, 1, "7d3b229e73e5756b4152675ce1fd12d0318ca3f4f74269d6b93fa41f1cb6a6c3") --Main Windows Depot Blasphemous
+setManifestid(1143854, "7538235104894025746", 5884384)
+--Dlcname Blasphemous - Digital Artbook
+addappid(1143850, 1, "de17e7ba029ac0ab3018562633b8542f7336481472c8789b26ec665192b68cd4") --Dlcname Blasphemous - Digital Artbook
+setManifestid(1143850, "3317964550509557174", 31642624)
+--Dlcname Blasphemous - Digital Comic
+addappid(1143851, 1, "5069e7a80c60cb1e87e237739720a7dee49178f81b14c5290401aed931c8b9e1") --Dlcname Blasphemous - Digital Comic
+setManifestid(1143851, "6736759184537561598", 9664992)
+--Dlcname Blasphemous - OST
+addappid(1143852, 1, "17a1ea3d734965e52de14e0340b18cdf6aef3b9c3edffea6e116a504be3faffc") --Dlcname Blasphemous - OST
+setManifestid(1143852, "1919474787724883518", 299656064)
+--Dlcname Blasphemous - 'Alloy of Sin' Character Skin
+addappid(1143853) --Dlcname Blasphemous - 'Alloy of Sin' Character Skin
+addappid(774365, 1, "bab5ef14e351920c47897af01aa779ee7cd40e781f30acf4090c8d7bbbea62a0") --Dlc Windows Depot Blasphemous - 'Alloy of Sin' Character Skin
+setManifestid(774365, "1710297333649588908", 3088)
+addappid(774366, 1, "f4cd54024c406a8d40ad60806ebdab94a7dd83dfd7a6440e044d3906517ab6ea") --Dlc Macos Depot Blasphemous - 'Alloy of Sin' Character Skin
+setManifestid(774366, "2489827079019319511", 0)
+addappid(774367, 1, "57fe84004caab7f31c8e3aa070d594f60d9fc847aaec1a8279fe5d96e008e66a") --Dlc Linux Depot Blasphemous - 'Alloy of Sin' Character Skin
+setManifestid(774367, "175520263097441601", 3904)
+addappid(1146990) --Dlcname AppID 1146990
