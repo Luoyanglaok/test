ae90be89ebb811163400f54ca327102e5c172541
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 08:17:48 +0800

    add 3305830.lua

---
+++ 3305830.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3305830.lua
+--Gamename Koumi-Jima after Training with Konomi-chan live2d
+addappid(3305830, 1, "bf05f5c551faf386bd40b0754521c2a9e135327b7408613f12b53a0b6cbd8601") --Mainappid Koumi-Jima after Training with Konomi-chan live2d
+addappid(3305831, 1, "32a62b3709011bf7c080c0756baa181efa54de01a3dd1f8c6cce0a8fc0e8b879") --Main Windows Language Depot Japanese Koumi-Jima after Training with Konomi-chan live2d
+setManifestid(3305831, "6102033673934503765", 212214080)
+addappid(3305832, 1, "973885fa87f89e8db868b4f7f5325df35cbd1006019ed0db2ee24ee576515a72") --Main Windows Language Depot English Koumi-Jima after Training with Konomi-chan live2d
+setManifestid(3305832, "3081806568283525340", 212363968)
+addappid(3305833, 1, "dceae218b4c561e221327a730eb42604488a880b11a137bf9ef23203ca60fc48") --Main Windows Language Depot Koreana Koumi-Jima after Training with Konomi-chan live2d
+setManifestid(3305833, "6218985165728213592", 212363968)
+addappid(3305834, 1, "9915d12456ea47d89940f7c27e2b41e0d218661f73b35aab2f914235422e29fd") --Main Windows Language Depot Schinese Koumi-Jima after Training with Konomi-chan live2d
+setManifestid(3305834, "5693575335760933544", 212363968)
+addappid(3305835, 1, "bd9b6c4c5b4cae83edd0254244ae040756cebc3ca187091230cb87b7a16e4f59") --Main Windows Language Depot Tchinese Koumi-Jima after Training with Konomi-chan live2d
+setManifestid(3305835, "6880942561764773875", 212363968)
