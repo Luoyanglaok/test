e4d53d0b786c6acfde8cfadc312002d1548ad0c4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:10:07 +0800

    add 2634890.lua

--- 2634890.lua
+++ 2634890.lua
@@ -1,4 +1,9 @@
-addappid(2634890)
-setManifestid(228990,"1829726630299308803")
-addappid(2634891,0,"4c7a817aabbebeea28ec8bb1971e5e91a388c7b617485eb2cf399d782c4124eb")
-setManifestid(2634891,"5391351782055600685")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2634890.lua
+--Gamename MARVEL vs. CAPCOM Fighting Collection: Arcade Classics
+addappid(2634890) --Mainappid MARVEL vs. CAPCOM Fighting Collection: Arcade Classics
+addappid(2634891, 1, "4c7a817aabbebeea28ec8bb1971e5e91a388c7b617485eb2cf399d782c4124eb") --Main Windows Depot MARVEL vs. CAPCOM Fighting Collection: Arcade Classics
+setManifestid(2634891, "5391351782055600685", 3610736160)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
