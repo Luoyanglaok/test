303d89ba727d12d88fd88165c7c36eadc0274af0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 21:36:34 +0800

    add 1216320.lua

--- 1216320.lua
+++ 1216320.lua
@@ -1,5 +1,17 @@
-addappid(1216320)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(1216321,0,"121ffa3e85426ad033d75eabafe829aa9fc20168fff2efc6513554cb11139b6e")
-setManifestid(1216321,"3494197066367392513")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1216320.lua
+--Gamename Shieldwall
+addappid(1216320) --Mainappid Shieldwall
+addappid(1216321, 1, "121ffa3e85426ad033d75eabafe829aa9fc20168fff2efc6513554cb11139b6e") --Main Windows Depot Shieldwall
+setManifestid(1216321, "3494197066367392513", 2632519424)
+--Dlcname Shieldwall Soundtrack
+addappid(1809830) --Dlcname Shieldwall Soundtrack
+addappid(1809831, 1, "a8efbeeb6cd1d8dec1e6b929f2ec057541a343ae7f5741f0baf0b710010b4eaa") --Dlc Windows Depot Shieldwall Soundtrack
+setManifestid(1809831, "5191737808314576929", 173065872)
+addappid(1809832, 1, "5b1fdaa14eb7962e4a50da04f0b7c846d3d72aa5922e37ce7cdd3e4e93f1a73c") --Dlc Windows Depot Shieldwall Soundtrack
+setManifestid(1809832, "8954130840414532316", 733454352)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
