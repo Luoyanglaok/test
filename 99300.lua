96ddc4ffae747d15759ab8a34b357cf74d62aa72
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 22:24:08 +0800

    add 99300.lua

--- 99300.lua
+++ 99300.lua
@@ -1,7 +1,12 @@
-addappid(99300)
-addappid(99301,0,"1d16c379b67ca3c79d574790d94792d9f62e563d253a34fa5ccc63a8c521b465")
-setManifestid(99301,"252506638482246686")
-addappid(200620,0,"ecef370371809d072426fb282ba550282dcbc5c9bdac4cad5040552a7b4228f8")
-setManifestid(200620,"7961037057220653895")
-addappid(200610,0,"561510e21bcae69e7db1ca198b9def77158253f2d6af6d728ecb9f431b5fae50")
-setManifestid(200610,"6178721417446170789")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 99300.lua
+--Gamename Renegade Ops
+addappid(99300, 1, "039d84637892e1b235d823455c5fa183f06af7ef9c350271c473fd1ff88a62d3") --Mainappid Renegade Ops
+addappid(99301, 1, "1d16c379b67ca3c79d574790d94792d9f62e563d253a34fa5ccc63a8c521b465") --Main Windows Depot Renegade Ops
+setManifestid(99301, "252506638482246686", 0)
+--Dlcname Renegade Ops Coldstrike Campaign
+addappid(200610, 1, "561510e21bcae69e7db1ca198b9def77158253f2d6af6d728ecb9f431b5fae50") --Dlcname Renegade Ops Coldstrike Campaign
+setManifestid(200610, "6178721417446170789", 0)
+--Dlcname Renegade Ops Reinforcement Pack
+addappid(200620, 1, "ecef370371809d072426fb282ba550282dcbc5c9bdac4cad5040552a7b4228f8") --Dlcname Renegade Ops Reinforcement Pack
+setManifestid(200620, "7961037057220653895", 0)
