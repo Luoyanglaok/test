b3096a2a14526e9441405325e2765832959cbc12
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:03:53 +0800

    add 2542020.lua

---
+++ 2542020.lua
@@ -0,0 +1,17 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2542020.lua
+--Gamename Duskfade
+addappid(2542020, 1, "8baa61ba648b203f72825f27792a8f0aea234e6cb66182ee37a6884a51b41c8c") --Mainappid Duskfade
+addappid(2542022, 1, "516e0ca255dc9e621518dcf785ec6c5a3e902a6f714b5e9ad99acbb539f42729") --Main Windows Depot Duskfade
+setManifestid(2542022, "293686788113063679", 14477634864)
+--Dlcname Duskfade Soundtrack
+addappid(4934950) --Dlcname Duskfade Soundtrack
+addappid(4934951, 1, "394094ac0702a2f11335251273045cd5df68d9bdc8561ccf6ba2a782c720271b") --Dlc Windows Depot Duskfade Soundtrack
+setManifestid(4934951, "220392031094205543", 267779968)
+addappid(4934952, 1, "4d3408bd34c380751da80408d7373e6924826e4ffb30086ff6e6bc2768030b50") --Dlc Windows Depot Duskfade Soundtrack
+setManifestid(4934952, "872195088478758752", 1529178784)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
