7980ba1cb6910d8197326731698328123149d0c2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 12:27:57 +0800

    add 2701660.lua

--- 2701660.lua
+++ 2701660.lua
@@ -1,5 +1,14 @@
-addappid(2701660)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2701661,0,"8c4c389019c5d2e6c25110711a77cb18558175e329aab3f13151b5ba67f25a79")
-setManifestid(2701661,"9149249826105582092")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2701660.lua
+--Gamename DRAGON QUEST III HD-2D Remake
+addappid(2701660) --Mainappid DRAGON QUEST III HD-2D Remake
+addappid(2701661, 1, "8c4c389019c5d2e6c25110711a77cb18558175e329aab3f13151b5ba67f25a79") --Main Windows Depot DRAGON QUEST III HD-2D Remake
+setManifestid(2701661, "7418431251785207618", 15613785632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2875360) --Dlcname DRAGON QUEST III HD-2D Remake: The trouble-free travel kit
+addappid(2915510) --Dlcname DRAGON QUEST III HD-2D Remake: Adventurous Accessory Kit
+addappid(2915520) --Dlcname DRAGON QUEST III HD-2D Remake: Marvellous Makeover Kit
