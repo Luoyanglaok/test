c603a5194915e59250c0552121dc903f298c2758
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 08:56:41 +0800

    add 1575990.lua

---
+++ 1575990.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1575990.lua
+--Gamename Twisted Tower
+addappid(1575990, 1, "3162980d7c6f7172eb139d44fa2269dd777bbe874ea4a91e25f64e2777f9da31") --Mainappid Twisted Tower
+addappid(1575991, 1, "734ae639f98a4a293b9234c8cb375ab906ef93823d4d6cde34591bd2e107c258") --Main Windows Depot Twisted Tower
+setManifestid(1575991, "2311064680449855647", 3348911952)
+--Missing Dlcs
+--Dlcname Twisted Tower Playtest
