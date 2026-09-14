d1d02a6a76eae620c1514d2a33f27d2334e3820c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 10:38:39 +0800

    add 47800.lua

--- 47800.lua
+++ 47800.lua
@@ -1,3 +1,6 @@
-addappid(47800)
-addappid(47801,0,"692b98128db91f0d9a6cc2bf9fd835491ce6d74008e15d9ee5bce1588ef3f76d")
-setManifestid(47801,"304802541921338775")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 47800.lua
+--Gamename RISK Factions
+addappid(47800) --Mainappid RISK Factions
+addappid(47801, 1, "692b98128db91f0d9a6cc2bf9fd835491ce6d74008e15d9ee5bce1588ef3f76d") --Main Windows Depot RISK Factions
+setManifestid(47801, "304802541921338775", 0)
