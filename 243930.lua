865e31fe4aec598cdf4a9068ecaf4a002e269ee5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:40:52 +0800

    add 243930.lua

--- 243930.lua
+++ 243930.lua
@@ -1,31 +1,35 @@
-addappid(243930)
-addappid(228983)
-setManifestid(228983,"8124929965194586177")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(243937,0,"316f9b9bde1bb4589202307c454f03d4c87bd187a23b98a5785941b498c927c0")
-setManifestid(243937,"5034651474694228870")
-addappid(243931,0,"33d43b0d54dcfe4ed21629463e59ea33c68b713905b32382fe28c5e9e2535c02")
-setManifestid(243931,"1983627373802759059")
-addappid(243932,0,"d4f48404312f8d77e6ff1bcc3995fc57e03f2c76a9cc97aef53195064db01220")
-setManifestid(243932,"7634113848857626635")
-addappid(243933,0,"bb6777e6069634ad53ed90022cfd9201bd0a62c469c8e994abf7d809eb16456d")
-setManifestid(243933,"1332339574673749413")
-addappid(243934,0,"9910ef9aa57c9f0f52f08ce5ca44409a698553aed6fb2b7f897ea7961c7a530f")
-setManifestid(243934,"1305437323032896323")
-addappid(243935,0,"c252ba4be0fd6f97ca257a401cfd302a225a0eda13e04f5d1702c3a115069da5")
-setManifestid(243935,"7350231970552147471")
-addappid(243936,0,"39447dc330e363dc5d7e878f08b28f0faa93fa6a8648f95f8d4c0cec03d6562c")
-setManifestid(243936,"1428038251422889900")
-addappid(243938,0,"e61660fe227a92df2d667a91d89c113abaf8634e51dafd1827acc1d7e350609d")
-setManifestid(243938,"3273908415405461101")
-addappid(243939,0,"043afc42ab4ab45fb146ac18a1d577abd6ff7b2284fcbd6537b1bb9aa04428f0")
-setManifestid(243939,"2050119468007672291")
-addappid(243940,0,"5d686fb1860ea6648c24a7d269ab3f9e6427ab754c87d80cdf26cd849bb4d282")
-setManifestid(243940,"3532451093581426453")
-addappid(243941,0,"7ec104fefe3fb761bb1bc34ce085c3ac8d2249f484b3e592374875ea22e0488b")
-setManifestid(243941,"3004198083028642205")
-addappid(243942,0,"2054b88963693d430683f1c9b494b60b436557624b5d4c164999cca152e6048e")
-setManifestid(243942,"3045381882525192222")
-addappid(243943,0,"8493d0f6736cc053758d60553bc11d2695068a14aad20d6ab833cb734c9e7b55")
-setManifestid(243943,"709744954687781845")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 243930.lua
+--Gamename Bound By Flame
+addappid(243930) --Mainappid Bound By Flame
+addappid(243931, 1, "33d43b0d54dcfe4ed21629463e59ea33c68b713905b32382fe28c5e9e2535c02") --Main Windows Depot Bound By Flame
+setManifestid(243931, "1983627373802759059", 0)
+addappid(243932, 1, "d4f48404312f8d77e6ff1bcc3995fc57e03f2c76a9cc97aef53195064db01220") --Main Windows Language Depot English Bound By Flame
+setManifestid(243932, "7634113848857626635", 0)
+addappid(243933, 1, "bb6777e6069634ad53ed90022cfd9201bd0a62c469c8e994abf7d809eb16456d") --Main Windows Language Depot French Bound By Flame
+setManifestid(243933, "1332339574673749413", 0)
+addappid(243934, 1, "9910ef9aa57c9f0f52f08ce5ca44409a698553aed6fb2b7f897ea7961c7a530f") --Main Windows Language Depot German Bound By Flame
+setManifestid(243934, "1305437323032896323", 0)
+addappid(243935, 1, "c252ba4be0fd6f97ca257a401cfd302a225a0eda13e04f5d1702c3a115069da5") --Main Windows Language Depot Italian Bound By Flame
+setManifestid(243935, "7350231970552147471", 0)
+addappid(243936, 1, "39447dc330e363dc5d7e878f08b28f0faa93fa6a8648f95f8d4c0cec03d6562c") --Main Windows Language Depot Spanish Bound By Flame
+setManifestid(243936, "1428038251422889900", 0)
+addappid(243937, 1, "316f9b9bde1bb4589202307c454f03d4c87bd187a23b98a5785941b498c927c0") --Main Windows Depot Bound By Flame
+setManifestid(243937, "5034651474694228870", 0)
+addappid(243938, 1, "e61660fe227a92df2d667a91d89c113abaf8634e51dafd1827acc1d7e350609d") --Main Windows Language Depot Polish Bound By Flame
+setManifestid(243938, "3273908415405461101", 0)
+addappid(243939, 1, "043afc42ab4ab45fb146ac18a1d577abd6ff7b2284fcbd6537b1bb9aa04428f0") --Main Windows Language Depot Russian Bound By Flame
+setManifestid(243939, "2050119468007672291", 0)
+addappid(243940, 1, "5d686fb1860ea6648c24a7d269ab3f9e6427ab754c87d80cdf26cd849bb4d282") --Main Windows Language Depot Czech Bound By Flame
+setManifestid(243940, "3532451093581426453", 0)
+addappid(243941, 1, "7ec104fefe3fb761bb1bc34ce085c3ac8d2249f484b3e592374875ea22e0488b") --Main Windows Language Depot Koreana Bound By Flame
+setManifestid(243941, "3004198083028642205", 0)
+addappid(243942, 1, "2054b88963693d430683f1c9b494b60b436557624b5d4c164999cca152e6048e") --Main Windows Language Depot Tchinese Bound By Flame
+setManifestid(243942, "3045381882525192222", 7746880)
+addappid(243943, 1, "8493d0f6736cc053758d60553bc11d2695068a14aad20d6ab833cb734c9e7b55") --Main Linux Depot Bound By Flame
+setManifestid(243943, "709744954687781845", 4348561152)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
