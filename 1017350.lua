ccf430747a31224f455591e8c9ab6173e31af4bc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 20:55:10 +0800

    add 1017350.lua

--- 1017350.lua
+++ 1017350.lua
@@ -1,3 +1,6 @@
-addappid(1017350)
-addappid(1017351,0,"63b84f882a7402b3fa6778e3ecf69fde025ffd9b4f5087dfcac711791e3b0999")
-setManifestid(1017351,"2202684434024467374")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1017350.lua
+--Gamename aMAZE St.Patrick
+addappid(1017350) --Mainappid aMAZE St.Patrick
+addappid(1017351, 1, "63b84f882a7402b3fa6778e3ecf69fde025ffd9b4f5087dfcac711791e3b0999") --Main Windows Depot aMAZE St.Patrick
+setManifestid(1017351, "2202684434024467374", 92126544)
