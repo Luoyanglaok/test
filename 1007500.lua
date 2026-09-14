7254daf0e5725edacbd2846bee8de40ef6900c83
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 01:41:42 +0800

    add 1007500.lua

--- 1007500.lua
+++ 1007500.lua
@@ -1,3 +1,6 @@
-addappid(1007500)
-addappid(1007501,0,"7e71329d9c41da07fa8f40a222a07129c962b881850308929f4ed17abd915017")
-setManifestid(1007501,"632214711749284071")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1007500.lua
+--Gamename Gladiator: Blades of Fury Demo
+addappid(1007500) --Mainappid Gladiator: Blades of Fury Demo
+addappid(1007501, 1, "7e71329d9c41da07fa8f40a222a07129c962b881850308929f4ed17abd915017") --Main Windows Depot Gladiator: Blades of Fury Demo
+setManifestid(1007501, "632214711749284071", 438946688)
