10273a30e2c9a965cd2b783c75946325545bf08c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 11:24:35 +0800

    add 1943950.lua

--- 1943950.lua
+++ 1943950.lua
@@ -1,5 +1,11 @@
-addappid(1943950)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1943951,0,"7930e28c405ca5c1ab83cb57ad8f6a96adc288960b0e60acbc764ff1154d8bb3")
-setManifestid(1943951,"4105378663961580580")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1943950.lua
+--Gamename Escape the Backrooms
+addappid(1943950, 1, "67e88d1bb91401a675aea9580536fdc41e7f9ffe2d2f24dbb9720ebd74f6bb16") --Mainappid Escape the Backrooms
+addappid(1943951, 1, "7930e28c405ca5c1ab83cb57ad8f6a96adc288960b0e60acbc764ff1154d8bb3") --Main Windows Depot Escape the Backrooms
+setManifestid(1943951, "9161997191706108505", 26527027888)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
