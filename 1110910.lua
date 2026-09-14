da429d89584956e9a148fbceb3c65df1764ca154
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-14 09:49:16 +0800

    add 1110910.lua

--- 1110910.lua
+++ 1110910.lua
@@ -1,5 +1,12 @@
-addappid(1110910)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1110911,0,"c6a18b263aa9a616ceb8bd5abf647a4c2190ed55748548225a1a44cc4d4c117e")
-setManifestid(1110911,"191700587065152978")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1110910.lua
+--Gamename Mortal Shell
+addappid(1110910, 1, "1842e160705294ccccdec21ad763abcbd79a19d8da682faedbaec5c6ef8a9439") --Mainappid Mortal Shell
+addappid(1110911, 1, "c6a18b263aa9a616ceb8bd5abf647a4c2190ed55748548225a1a44cc4d4c117e") --Main Windows Depot Mortal Shell
+setManifestid(1110911, "191700587065152978", 7464782016)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1672210) --Dlcname Mortal Shell: The Virtuous Cycle
