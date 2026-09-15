1c410e4e450316cd2423f6dd83071ff6ade036b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 12:45:26 +0800

    add 3376850.lua

--- 3376850.lua
+++ 3376850.lua
@@ -1,3 +1,14 @@
-addappid(3376850)
-addappid(3376851,0,"f57959fbb03f93d0003e5886f276d1219007b5c309804b7513cf6bcf1fe9a928")
-setManifestid(3376851,"8654104462921891360")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3376850.lua
+--Gamename Bomb Vehicle The Idle Clicker
+addappid(3376850) --Mainappid Bomb Vehicle The Idle Clicker
+addappid(3376851, 1, "f57959fbb03f93d0003e5886f276d1219007b5c309804b7513cf6bcf1fe9a928") --Main Windows Depot Bomb Vehicle The Idle Clicker
+setManifestid(3376851, "1196859646192657217", 454697296)
+--Missing Dlcs
+--Dlcname Bomb Vehicle The Idle Clicker - Combat Train DLC
+--Dlcname Bomb Vehicle The Idle Clicker - Doom Vanguard Tank
+--Dlcname Bomb Vehicle The Idle Clicker - Game Speed Shifter
+--Dlcname Bomb Vehicle The Idle Clicker - Scarlet Wolf
+--Dlcname Bomb Vehicle The Idle Clicker - DF Express Strategic Transport Train
+--Dlcname Bomb Vehicle The Idle Clicker - Megoliath Armed Reclamation Vehicle
+--Dlcname Bomb Vehicle The Idle Clicker - War Maid
