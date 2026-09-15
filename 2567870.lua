76dcc167ab895e2964977a2ab2416e5a7c9804cd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 17:22:38 +0800

    add 2567870.lua

--- 2567870.lua
+++ 2567870.lua
@@ -1,8 +1,18 @@
-addappid(2567870)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229002,"7260605429366465749")
-setManifestid(229003,"8740933542064151477")
-setManifestid(229007,"4477590687906973371")
-addappid(2567871,0,"84ff26693d69b1f13f653ad7d1a182142a2a5049bf0ce8dc73735d955d3cb186")
-setManifestid(2567871,"2564921531254078552")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2567870.lua
+--Gamename Chained Together
+addappid(2567870, 1, "3eb4ccd36ec5b43258c3051d9614925915db0d976e68e89c79eb4a576ac0bcd9") --Mainappid Chained Together
+addappid(2567871, 1, "84ff26693d69b1f13f653ad7d1a182142a2a5049bf0ce8dc73735d955d3cb186") --Main Windows Depot Chained Together
+setManifestid(2567871, "235745312197097650", 7865091344)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229003, "8740933542064151477", 42914928)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(4948760) --Dlcname Chained Together - Supporter Pack
