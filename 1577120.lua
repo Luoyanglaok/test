123ac4fad353ecdbb06bffcfd6cf95f3e3b09a56
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 13:34:18 +0800

    add 1577120.lua

--- 1577120.lua
+++ 1577120.lua
@@ -1,7 +1,17 @@
-addappid(1577120)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1523211,0,"6111b2e3d21efa5e5b26a91f75ff32d12aa8c5bb547c4d0215eda184a7f4c49b")
-setManifestid(1523211,"5488617169383930545")
-addappid(1577122,0,"d1f173e0a675ae53ba562864c36961f17e2f532af0acd7764099896fe7852d6d")
-setManifestid(1577122,"3441814442032017627")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1577120.lua
+--Gamename The Quarry
+addappid(1577120, 1, "4bd8c29bd6c9801e5af307d2093fdfd297d9e280985916412e8a5770817f8eae") --Mainappid The Quarry
+addappid(1577122, 1, "d1f173e0a675ae53ba562864c36961f17e2f532af0acd7764099896fe7852d6d") --Main Windows Depot The Quarry
+setManifestid(1577122, "3441814442032017627", 48166482272)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1523211, 1, "6111b2e3d21efa5e5b26a91f75ff32d12aa8c5bb547c4d0215eda184a7f4c49b") --Share Windows Depot AppID 1523210
+setManifestid(1523211, "5488617169383930545", 128)
+addappid(1877110) --Dlcname The Quarry - Horror History Visual Filter Pack
+addappid(1877111) --Dlcname The Quarry - Deluxe Edition Content Pack
+addappid(1891950) --Dlcname The Quarry - Full Game Unlock
+addappid(2101870) --Dlcname The Quarry - ‘50s Throwback Character Outfits
