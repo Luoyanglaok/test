f102f922174bd36b57c8d86729065c820addb1c3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 10:40:51 +0800

    add 536270.lua

--- 536270.lua
+++ 536270.lua
@@ -1,3 +1,12 @@
-addappid(536270)
-addappid(536272,0,"cca45dc1fc727e49cb5dd237968fd4c65c0ae75f5ae2993fbbdee2d3e625ffdb")
-setManifestid(536272,"8085501622692399519")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 536270.lua
+--Gamename Ancestors: The Humankind Odyssey
+addappid(536270, 1, "4b212c6c948d3307f6c44ffff76d10375df7564e53cc24f2bbf24cfbabe72440") --Mainappid Ancestors: The Humankind Odyssey
+addappid(536272, 1, "cca45dc1fc727e49cb5dd237968fd4c65c0ae75f5ae2993fbbdee2d3e625ffdb") --Main Windows Depot Ancestors: The Humankind Odyssey
+setManifestid(536272, "8085501622692399519", 8011471824)
+--Dlcname Ancestors: The Humankind Odyssey Official Soundtrack
+addappid(1382040) --Dlcname Ancestors: The Humankind Odyssey Official Soundtrack
+addappid(1382041, 1, "c1eeb79769e90750a0d4732d8f28d207aa090112979b066efa907fb621b256ec") --Dlc Windows Depot Ancestors: The Humankind Odyssey Official Soundtrack
+setManifestid(1382041, "372922912922430349", 105112032)
+addappid(1382042, 1, "daee3f6c7e1f517d707c998cedc1bbe7ded7db22e958d46942fc23d3d7b33ef3") --Dlc Windows Depot Ancestors: The Humankind Odyssey Official Soundtrack
+setManifestid(1382042, "2325658935379667117", 735943776)
