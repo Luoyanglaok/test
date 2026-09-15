073902e6bc2e0dc2b0ceaaa1fcd81f57310efa00
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 02:05:54 +0800

    add 2749380.lua

--- 2749380.lua
+++ 2749380.lua
@@ -3,4 +3,4 @@
 --Gamename Lust Theory Season 3
 addappid(2749380, 1, "784514fdda7204b31a952b8c2e379863b12ff7886aeb0269394195ae1020ca74") --Mainappid Lust Theory Season 3
 addappid(2749381, 1, "6ca78b6b876594fe3b3d667666d648454744911e8a3e6b0e3215d4e7a9d0ceee") --Main Windows Depot Lust Theory Season 3
-setManifestid(2749381, "3415037885276392298", 14285390944)
+setManifestid(2749381, "5392182909138484010", 15201949904)
