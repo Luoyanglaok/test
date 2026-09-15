69122938a1c5701a755f7b375a0911a91759ee74
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 12:44:22 +0800

    add 1909840.lua

--- 1909840.lua
+++ 1909840.lua
@@ -1,3 +1,6 @@
-addappid(1909840)
-addappid(1909841,0,"e5d50367b0e533f638f41c3d0c53ba62d3e6f5832ba684c2a0720932e5b2a63e")
-setManifestid(1909841,"3605359256825980278")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1909840.lua
+--Gamename JIANGHU CHRONICLES
+addappid(1909840, 1, "8c8d49aaf769b747885a38bcac7ab969446e71ef8a73612a6bc2eb35aaf78c01") --Mainappid JIANGHU CHRONICLES
+addappid(1909841, 1, "e5d50367b0e533f638f41c3d0c53ba62d3e6f5832ba684c2a0720932e5b2a63e") --Main Windows Language Depot Schinese JIANGHU CHRONICLES
+setManifestid(1909841, "6847610376555945497", 1463368384)
