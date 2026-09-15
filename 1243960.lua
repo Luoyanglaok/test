92c9b87797e6298044b4299a9278073e3cb3a6ff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 05:37:19 +0800

    add 1243960.lua

--- 1243960.lua
+++ 1243960.lua
@@ -1,5 +1,10 @@
-addappid(1243960)
-addappid(1243961,0,"7aebeffe9f73689183b6674be54492a1f835e508f144bd65d8d558928e23b606")
-setManifestid(1243961,"7477416350522418444")
-addappid(1243962)
-addappid(1243963)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1243960.lua
+--Gamename Unspottable
+addappid(1243960, 1, "b5ca09564948ee3f1fb44797fa5243cdda6f2d3b9ba90d48bc768bf2f56752a0") --Mainappid Unspottable
+addappid(1243961, 1, "7aebeffe9f73689183b6674be54492a1f835e508f144bd65d8d558928e23b606") --Main Windows Depot Unspottable
+setManifestid(1243961, "1541527795211162547", 251401664)
+addappid(1243962, 1, "b7de736e14c4b3206e4b7faf4757123fb5a5319fcdf7345dbac5ecded2962b10") --Main Linux Depot Unspottable
+setManifestid(1243962, "9208468806245634746", 259832080)
+addappid(1243963, 1, "16962182454bff4c706202db29c423e6c47c192b957952788d7516dc9f9e0a43") --Main Macos Depot Unspottable
+setManifestid(1243963, "8736357124907740370", 261885008)
