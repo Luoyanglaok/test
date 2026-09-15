3f03dc668f9b6e509a43b9d0496a8445f35021f6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 23:52:12 +0800

    add 2582140.lua

---
+++ 2582140.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2582140.lua
+--Gamename Seafarer: The Ship Sim
+addappid(2582140) --Mainappid Seafarer: The Ship Sim
+addappid(2582141, 1, "f40d0c88571b28f840178b77be55334a2e94510b18e939af7c2da27f9d855b4e") --Main Windows Depot Seafarer: The Ship Sim
+setManifestid(2582141, "2737646242854437964", 15243599520)
+addappid(4180111, 1, "4f97b3204a97db610ceab83e1a06b0436655051837dd88b83313dfdb1b5b804e") --Main Windows Depot Seafarer: The Ship Sim
+setManifestid(4180111, "1222130291592211207", 44645664)
+--Dlcname Seafarer: The Ship Sim - Soundtrack
+addappid(4180110) --Dlcname Seafarer: The Ship Sim - Soundtrack
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4213410) --Dlcname Seafarer: The Ship Sim - Supporter DLC
