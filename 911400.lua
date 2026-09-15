5882ea105b2a9b73e6bf117c2bc9291acb73aafc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 14:26:59 +0800

    add 911400.lua

--- 911400.lua
+++ 911400.lua
@@ -1,5 +1,11 @@
-addappid(911400)
-addappid(911401,0,"c04740de17320ddf2881d8e88424ccb3850b951c06acdf8de1da26decd287cd9")
-setManifestid(911401,"3670596370283469393")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 911400.lua
+--Gamename Assassin's Creed® III Remastered
+addappid(911400, 1, "d5d33c264be86935ec5c45c5bfc9308191e7e50965d348b61486cc0138999b9b") --Mainappid Assassin's Creed® III Remastered
+addappid(911401, 1, "c04740de17320ddf2881d8e88424ccb3850b951c06acdf8de1da26decd287cd9") --Main Windows Depot Assassin's Creed® III Remastered
+setManifestid(911401, "3670596370283469393", 32835295872)
+--Share Depots
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(985980) --Dlcname AppID 985980
+addtoken(985980, "4734749171725067532")
