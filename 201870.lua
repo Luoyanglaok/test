959720cb177599de88e3c30032830a7189864a09
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 13:06:29 +0800

    add 201870.lua

--- 201870.lua
+++ 201870.lua
@@ -1,14 +1,27 @@
-addappid(201870)
-setManifestid(228981,"7613356809904826842")
-setManifestid(228982,"6413394087650432851")
-setManifestid(228990,"1829726630299308803")
-addappid(201882,0,"9da8a3de98388a48fca6702689b9b4bbbf67af576b632ee04e08b98f303efdd2")
-setManifestid(201882,"7521579372377574937")
-addappid(201871,0,"8174e101496f03e7544b945add7c99908e744b0c0b2b7182f69e5c408692f082")
-setManifestid(201871,"6910264676643594998")
-addappid(201872,0,"9ef78f51c49e94cee4c9aea09dabfc753646cd124809429590a467b53cd232e5")
-setManifestid(201872,"2105489577231626183")
-addappid(201873,0,"e340422eaaff220ae2823c05e9a039d9ba4f8fa68b8e32eff7219def9b6a941a")
-setManifestid(201873,"5532293391770189079")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 201870.lua
+--Gamename Assassin's Creed® Revelations
+addappid(201870, 1, "979712e8290ed0112507493bb952bd6fa53e9d649fd31b2da1aef49052106e24") --Mainappid Assassin's Creed® Revelations
+addappid(201871, 1, "8174e101496f03e7544b945add7c99908e744b0c0b2b7182f69e5c408692f082") --Main Windows Depot Assassin's Creed® Revelations
+setManifestid(201871, "6910264676643594998", 6765522384)
+addappid(201872, 1, "9ef78f51c49e94cee4c9aea09dabfc753646cd124809429590a467b53cd232e5") --Main Windows Language Depot Russian Assassin's Creed® Revelations
+setManifestid(201872, "2105489577231626183", 366088048)
+addappid(201873, 1, "e340422eaaff220ae2823c05e9a039d9ba4f8fa68b8e32eff7219def9b6a941a") --Main Windows Language Depot Czech Assassin's Creed® Revelations
+setManifestid(201873, "5532293391770189079", 1173520)
+--Dlcname Assassin's Creed Revelations - The Lost Archive
+addappid(201882, 1, "9da8a3de98388a48fca6702689b9b4bbbf67af576b632ee04e08b98f303efdd2") --Dlcname Assassin's Creed Revelations - The Lost Archive
+setManifestid(201882, "7521579372377574937", 693013936)
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(201877) --Dlcname The Ancestors Character Pack
+addappid(201878) --Dlcname Assassin's Creed Revelations - Standard Key
+addappid(201879) --Dlcname Assassin's Creed Revelations - Deluxe Key
+addappid(201880) --Dlcname AppID 201880
+addappid(201881) --Dlcname Assassin's Creed Revelations - The Mediterranean Traveler Map Pack
