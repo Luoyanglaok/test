2f1748bff50da562c763c94bd17cd113bf085677
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 03:45:06 +0800

    add 2706020.lua

--- 2706020.lua
+++ 2706020.lua
@@ -2,7 +2,8 @@
 -- Original file: 2706020.lua
 --Gamename ALL WILL FALL
 addappid(2706020, 1, "1e77dbc1e9853e8b3745dc5f83ea2f91a1a6235c8d318ce3b2d16c5396d90ba5") --Mainappid ALL WILL FALL
+addtoken(2706020, "10841314618797467692")
 addappid(2706021, 1, "8ca6191f4f09dcb309688901e511599e604be2d09f060774e028dce04bd063e3") --Main Windows Depot ALL WILL FALL
-setManifestid(2706021, "3519247360334837771", 1920137696)
+setManifestid(2706021, "2620129667642861550", 1920083584)
 --Missing Dlcs
 --Dlcname All Will Fall Soundtrack
