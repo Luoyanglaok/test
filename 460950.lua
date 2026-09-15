b08282bcb64f21e34abe631efb3024434c33f562
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 21:42:51 +0800

    add 460950.lua

--- 460950.lua
+++ 460950.lua
@@ -1,5 +1,8 @@
-addappid(460950)
-addappid(460951,0,"76672e24adb8b313cdc23f79a51982c3fd243414f79426ed46e8bbae264cb202")
-setManifestid(460951,"7302141199497370222")
-addappid(460952,0,"d0715cdb92b3e7017a3e6378d03cee9db4e669dbe34639616706f0a336e1c540")
-setManifestid(460952,"4979081960644650413")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 460950.lua
+--Gamename Katana ZERO
+addappid(460950) --Mainappid Katana ZERO
+addappid(460951, 1, "76672e24adb8b313cdc23f79a51982c3fd243414f79426ed46e8bbae264cb202") --Main Windows Depot Katana ZERO
+setManifestid(460951, "7302141199497370222", 152736384)
+addappid(460952, 1, "d0715cdb92b3e7017a3e6378d03cee9db4e669dbe34639616706f0a336e1c540") --Main Macos Depot Katana ZERO
+setManifestid(460952, "4979081960644650413", 157237296)
