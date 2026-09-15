3c7c9776546ed51e06bfe015749900411e32288d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 11:49:34 +0800

    add 2389170.lua

---
+++ 2389170.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2389170.lua
+--Gamename Huaxia: Warring States
+addappid(2389170) --Mainappid Huaxia: Warring States
+addtoken(2389170, "14488982077635452708")
+addappid(2389171, 1, "0d52bd6fc3de22427af2f6912f4b737fa72560c3cebc810b2b5b238ea58eb797") --Main Windows Depot Huaxia: Warring States
+setManifestid(2389171, "8316938800510129653", 9450207744)
+--Dlcname Rebirth from Flame - Supporter Pack
+addappid(4501300, 1, "0ccc3bc733b899447a5a2586a9c44183bcf8aeac56ed7b9c67f734086fa73485") --Dlcname Rebirth from Flame - Supporter Pack
+setManifestid(4501300, "4292537953090597457", 100794800)
+--Missing Dlcs
+--Dlcname HUAXIA: Warring States - Deserved Flogging - Content Pack
