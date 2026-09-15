eb2f7ca275f6446c9d46d911e0dfb9d97cedb60c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 21:39:28 +0800

    add 1874880.lua

--- 1874880.lua
+++ 1874880.lua
@@ -1,6 +1,33 @@
-addappid(1874880)
-setManifestid(228989,"3514306556860204959")
-addappid(1874881,0,"f1bc308d3a58a8d144a76dc02d552819b433bda49c325060fcab53e8100cead3")
-setManifestid(1874881,"3208659220235311754")
-addappid(1874882,0,"c964efe3e6ee7295beeb474f46e6c78d1f2f62097dc3c793b1474cfab9f2f055")
-setManifestid(1874882,"1650233779375073015")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1874880.lua
+--Gamename Arma Reforger
+addappid(1874880, 1, "b52c7bfff7cf6936c36fdd97b188bc70fe248c77f487c595951876b49d5f33d0") --Mainappid Arma Reforger
+addappid(1874881, 1, "f1bc308d3a58a8d144a76dc02d552819b433bda49c325060fcab53e8100cead3") --Main Windows Depot Arma Reforger
+setManifestid(1874881, "7768706146966449237", 21568984704)
+addappid(1874882, 1, "c964efe3e6ee7295beeb474f46e6c78d1f2f62097dc3c793b1474cfab9f2f055") --Main Windows Depot Arma Reforger
+setManifestid(1874882, "1650233779375073015", 234859456)
+--Dlcname Arma Reforger Soundtrack
+addappid(1982980) --Dlcname Arma Reforger Soundtrack
+addappid(1982981, 1, "fb9b19f4018873d6112446a8991e8911cd8d4452a8c7bd27f4251b07638e89d5") --Dlc Windows Depot Arma Reforger Soundtrack
+setManifestid(1982981, "4663975222407329777", 426926688)
+addappid(1982982, 1, "305012a57de8f65ba31415383f9ef1e29a6e6abcee5d86bcf04cac01b927906e") --Dlc Windows Depot Arma Reforger Soundtrack
+setManifestid(1982982, "8175811065148517847", 1926158384)
+--Dlcname Refrogger Supporter Pack
+addappid(3923950) --Dlcname Refrogger Supporter Pack
+addappid(3923951, 1, "6093cb5032a8d98863652c35177f7653c823c16613b35ac3d61f6814d00e5e89") --Dlc Windows Depot Refrogger Supporter Pack
+setManifestid(3923951, "690578477946807091", 82800032)
+addappid(3923952, 1, "a958403d909fa984ff77e097be93096f01942ed66f2dcd3793e85c7d15ef6fcb") --Dlc Windows Depot Refrogger Supporter Pack
+setManifestid(3923952, "77953387113932170", 4186043456)
+addappid(3923953, 1, "5a8fc21ce8d7116717039f57ee9e017f244fb52890ee401b55a8110a535452ab") --Dlc Windows Depot Refrogger Supporter Pack
+setManifestid(3923953, "3992893852227475416", 107836800)
+addappid(3923954, 1, "7143a76a793670739b7ce21d832695bd14a5b48f4c98a888de32bf6328643bb5") --Dlc Windows Depot Refrogger Supporter Pack
+setManifestid(3923954, "7616342300144661544", 59263136)
+--Dlcname Modder Supporter Pack
+addappid(4631450, 1, "2c4e240a1439793eb509f5f0d5006ac05cd7bcb998c9a9dc7187440e3557be8a") --Dlcname Modder Supporter Pack
+setManifestid(4631450, "3003804867971276946", 3956432)
+addappid(4631451, 1, "f8b33530189694040c628f4b4140043320bb6d1c77fa50976845990309abfef7") --Dlc Windows Depot Modder Supporter Pack
+setManifestid(4631451, "5762110209672128902", 158368)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(5146340) --Dlcname Arma Reforger - Rally
