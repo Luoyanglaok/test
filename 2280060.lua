174fd5df9c26ee9c9cedb44008dc9b19bbb80f18
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 09:08:38 +0800

    add 2280060.lua

--- 2280060.lua
+++ 2280060.lua
@@ -1,3 +1,6 @@
-addappid(2280060)
-addappid(2280061,0,"a8928c3e35f1b486b7e6601779a46aba034470b87b29c84e6035c53ef8da4597")
-setManifestid(2280061,"7596094222599541469")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2280060.lua
+--Gamename Tenebris: Terra Incognita
+addappid(2280060) --Mainappid Tenebris: Terra Incognita
+addappid(2280061, 1, "a8928c3e35f1b486b7e6601779a46aba034470b87b29c84e6035c53ef8da4597") --Main Windows Depot Tenebris: Terra Incognita
+setManifestid(2280061, "7852318086917838412", 3273277200)
