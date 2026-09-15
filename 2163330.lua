22c1c3a60431f17e676a306757a86606dae45f21
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 12:36:46 +0800

    add 2163330.lua

--- 2163330.lua
+++ 2163330.lua
@@ -3,4 +3,10 @@
 --Gamename Yet Another Zombie Survivors
 addappid(2163330, 1, "685bc3c89a6debbe36217fa0abb80a93867699477fb8a5d5d2aee86599f14e79") --Mainappid Yet Another Zombie Survivors
 addappid(2163331, 1, "f83759c39e0a087b6ee076427f28b6fa19f8e6400ef9e3843bc4ea1d0ba5afff") --Main Windows Depot Yet Another Zombie Survivors
-setManifestid(2163331, "6587898556167808074", 4681610944)
+setManifestid(2163331, "8833317469152774742", 4683441312)
+addappid(4377471, 1, "97ea633cdc399e2655d0908cde0ab0e5a3e267b93128f2244037ecfbe184ad01") --Main Windows Depot Yet Another Zombie Survivors
+setManifestid(4377471, "2072104595930746418", 74698544)
+addappid(4377472, 1, "eaff8a987214d3cf19b7e75fb5babc05ab46fe9b8f51a0b8e9e3bacf50a154b6") --Main Windows Depot Yet Another Zombie Survivors
+setManifestid(4377472, "4909519595541108151", 474986208)
+--Dlcname Yet Another Zombie Survivors Soundtrack
+addappid(4377470) --Dlcname Yet Another Zombie Survivors Soundtrack
