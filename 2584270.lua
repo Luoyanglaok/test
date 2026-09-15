d1e3df542acd86f37132ee934935cb21eb6b1a59
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 01:15:00 +0800

    add 2584270.lua

--- 2584270.lua
+++ 2584270.lua
@@ -3,7 +3,7 @@
 --Gamename Mortal Shell II
 addappid(2584270) --Mainappid Mortal Shell II
 addappid(2584271, 1, "bb59e6dc30df8728a4bfcc3129ca9b8284ed3808d42a9a272e65d4904b4bc445") --Main Windows Depot Mortal Shell II
-setManifestid(2584271, "4010694604290439344", 69803772768)
+setManifestid(2584271, "8653917884651199139", 69804630480)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
