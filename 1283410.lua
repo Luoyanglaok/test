51295a10dba66ab3b97e07523d92494e57080073
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 16:57:41 +0800

    add 1283410.lua

--- 1283410.lua
+++ 1283410.lua
@@ -1,5 +1,23 @@
-addappid(1283410)
-addappid(1677120)
-addappid(1792940)
-addappid(2715450)
-setManifestid(1283411,"4204738802457958148")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1283410.lua
+--Gamename Tails of Iron
+addappid(1283410, 1, "8bf061ecbebea51014144f7add86225902190392ee2cb83baea3d39524c6ab32") --Mainappid Tails of Iron
+addappid(1283411, 1, "9088237d7b955a99fcfb88a0fe335ea74903e34559c6e2fdcc8140a7612c9f85") --Main Windows Depot Tails of Iron
+setManifestid(1283411, "4204738802457958148", 1473809152)
+addappid(1283412, 1, "fbed7072cbd0df3d340fc2762582822cdcc9fb17d3b677dcd3729066b30306cf") --Main Macos Depot Tails of Iron
+setManifestid(1283412, "6864782205758173895", 1477627808)
+addappid(1283413, 1, "8c7d457e9140157ca27c50e2801244020606739f29c0a083d3f878603d784d5d") --Main Linux Depot Tails of Iron
+setManifestid(1283413, "5945399409353929535", 1414418928)
+--Dlcname Tails of Iron Soundtrack
+addappid(1792940) --Dlcname Tails of Iron Soundtrack
+addappid(1792941, 1, "fcc70d08b5d7f217d5654394c659258266e3bd73027a189fdf2c0bcff5489ef9") --Dlc Windows Depot Tails of Iron Soundtrack
+setManifestid(1792941, "7416059701691606113", 98020336)
+--Dlcname Tails of Iron - Artbook
+addappid(2715450) --Dlcname Tails of Iron - Artbook
+addappid(2715451, 1, "1d3d4de844ff36ced4d0cbfbdd602fd85261bb7bce68ad8b74fbaf685fd390f8") --Dlc Windows Depot Tails of Iron - Artbook
+setManifestid(2715451, "3644509945029682468", 1100603488)
+addappid(2715452, 1, "cd234b4111626bd609a3d4020cb82a43b8e61450afb126d924f7d5818ffa305e") --Dlc Macos Depot Tails of Iron - Artbook
+setManifestid(2715452, "2706667787080211824", 1106073680)
+addappid(2715453, 1, "76e119fcbb095d15172a13f7f39820b0ee2834821a74d68688052a1c92b16d86") --Dlc Linux Depot Tails of Iron - Artbook
+setManifestid(2715453, "3316619315225757513", 370840064)
+addappid(1677120) --Dlcname Tails of Iron - Crimson Knight DLC
