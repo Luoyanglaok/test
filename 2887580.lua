f705e95f9f243822f707941fa8e4137297e13a1a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 20:37:44 +0800

    add 2887580.lua

---
+++ 2887580.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2887580.lua
+--Gamename Active Matter
+addappid(2887580) --Mainappid Active Matter
+addappid(2887581, 1, "279f63502b38413b7f07b57ac3e32cf4b9c303e3977107d2891cf053b159f5c9") --Main Windows Depot Active Matter
+setManifestid(2887581, "5253383296583988128", 37251015904)
+addappid(2887582, 1, "fe6a84f76a66a813069f11ee823b3e5946abe5a2503bc72302a56c1db2cde372") --Main Windows Depot Active Matter
+setManifestid(2887582, "5826875760740471667", 425618448)
+addappid(4107830) --Dlcname Active Matter - Upgrade to Advanced Edition
+addappid(4107840) --Dlcname Active Matter - Upgrade to Deluxe Edition
+--Missing Dlcs
+--Dlcname Active Matter - Original Game Soundtrack, Vol.1
