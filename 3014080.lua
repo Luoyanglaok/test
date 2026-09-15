dd151183e2db6359d8ae69b99adf02c6fb8274a2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 08:07:07 +0800

    add 3014080.lua

--- 3014080.lua
+++ 3014080.lua
@@ -1,3 +1,48 @@
-addappid(3014080)
-addappid(3014081,0,"9c8b0e729ef1fce35224045a12393cf5a7069e60692bc9b3bf2d64b0bb37b6a3")
-setManifestid(3014081,"3204597251153695972")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3014080.lua
+--Gamename The Hundred Line -Last Defense Academy-
+addappid(3014080, 1, "0bd0349c566b5350b82cde56525e4b321f136e5a64cdd26f210477b9d4cbf060") --Mainappid The Hundred Line -Last Defense Academy-
+addappid(3014081, 1, "9c8b0e729ef1fce35224045a12393cf5a7069e60692bc9b3bf2d64b0bb37b6a3") --Main Windows Depot The Hundred Line -Last Defense Academy-
+setManifestid(3014081, "5529363074240054460", 30224802272)
+addappid(4282301, 1, "3bb523d111d308433cf1237442a5537eae94af6f7a6e3e29c14a4bcc29c1169a") --Main Windows Depot The Hundred Line -Last Defense Academy-
+setManifestid(4282301, "2729221457319718334", 474870400)
+addappid(4282302, 1, "2208d244492daaebffe5c8e9da4453f661ee02f238877bf00d9f948ea391da57") --Main Windows Depot The Hundred Line -Last Defense Academy-
+setManifestid(4282302, "897302265533361316", 3492110752)
+addappid(4282303, 1, "e49536c73ee03ca39d0fbae6d6f77f001c125d73fc2dcb556184c9428c7e4676") --Main Windows Language Depot Japanese The Hundred Line -Last Defense Academy-
+setManifestid(4282303, "5104185891663692057", 474868448)
+addappid(4282304, 1, "52230be9bfdb510b4f9c17c486a15fb8382455c290793bf8cfa2786dbfc508b4") --Main Windows Language Depot Japanese The Hundred Line -Last Defense Academy-
+setManifestid(4282304, "3539567785708280480", 3492110752)
+addappid(4282305, 1, "8c123a99bc8ae71795db6d781789afb6a560aecf00cd509e12cb592bd63d30b4") --Main Windows Language Depot Schinese The Hundred Line -Last Defense Academy-
+setManifestid(4282305, "5675787956733278481", 474865824)
+addappid(4282306, 1, "4469f8101240a75b48e0f19e20927712e8d388344f64e721aae031a0a85eba21") --Main Windows Language Depot Schinese The Hundred Line -Last Defense Academy-
+setManifestid(4282306, "9010061429308007685", 3492110752)
+addappid(4282307, 1, "c92604cb07a3ca77ce638ab9085daa1d01cc7e38982e4deb0ced67e558b7e610") --Main Windows Language Depot Tchinese The Hundred Line -Last Defense Academy-
+setManifestid(4282307, "8648385234630074449", 474866000)
+addappid(4282308, 1, "10f847da110730ae4f060e2049b307b7a6b52e833f6657338c380b3e055bb52d") --Main Windows Language Depot Tchinese The Hundred Line -Last Defense Academy-
+setManifestid(4282308, "3093752262478099799", 3492110752)
+--Dlcname HUNDRED LINE -最終防衛学園- Soundtrack
+addappid(3313470) --Dlcname HUNDRED LINE -最終防衛学園- Soundtrack
+addappid(3313471, 1, "0753f5fb816b44194be0f35cffc6d553028ebc4b682c291bccd9412cf6fa02e3") --Dlc Windows Language Depot Japanese HUNDRED LINE -最終防衛学園- Soundtrack
+setManifestid(3313471, "4633437612164092131", 65206320)
+addappid(3313472, 1, "08759e5214021416b3a891aed8e49a7574288a969e64de476f35a0a39b4ebbc0") --Dlc Windows Language Depot Japanese HUNDRED LINE -最終防衛学園- Soundtrack
+setManifestid(3313472, "2084566027565761264", 366747040)
+addappid(3313473, 1, "7139c9b192d7d27f8735a404af47a898e4218cdd8653933a6680df1ed49013b7") --Dlc Windows Depot HUNDRED LINE -最終防衛学園- Soundtrack
+setManifestid(3313473, "1114607683842095068", 65206080)
+addappid(3313474, 1, "cc700042dbbffb57753bc0af37d5b8f8983c6913ce31f98c42ba3968739eeb22") --Dlc Windows Depot HUNDRED LINE -最終防衛学園- Soundtrack
+setManifestid(3313474, "1129197215123705621", 366747040)
+addappid(3313475, 1, "8b6728f6eac52d923e015280a35003cc3a44368a53126caa4f419e340b4e96e1") --Dlc Windows Language Depot Schinese HUNDRED LINE -最終防衛学園- Soundtrack
+setManifestid(3313475, "2668544798282893607", 65206144)
+addappid(3313476, 1, "62e0bf9e3a7f470967ba2bc5e91a0f92301a1cd96480c8129b2756c2236e91b4") --Dlc Windows Language Depot Schinese HUNDRED LINE -最終防衛学園- Soundtrack
+setManifestid(3313476, "312663337026652661", 366747040)
+addappid(3313477, 1, "e3f18355823cc2d09f0e0f8a9ae9848b27dae93d8c82051cd74ef129c276a4d6") --Dlc Windows Language Depot Tchinese HUNDRED LINE -最終防衛学園- Soundtrack
+setManifestid(3313477, "8131007184588358547", 65206192)
+addappid(3313478, 1, "8a307ea16c6f00697712d08309c0a851afeb2816bb5d192d1686ddf4b038b43d") --Dlc Windows Language Depot Tchinese HUNDRED LINE -最終防衛学園- Soundtrack
+setManifestid(3313478, "4548166260689771597", 366747040)
+--Dlcname HUNDRED LINE -最終防衛学園- Artbook
+addappid(3319760, 1, "fe92de42a7232fcb703844d0f9644a24611561c408aa574d4dba47f3ee44b769") --Dlcname HUNDRED LINE -最終防衛学園- Artbook
+setManifestid(3319760, "6460435187318013314", 879814080)
+--Dlcname HUNDRED LINE -最終防衛学園- 特防隊前日譚ＦＩＬＥ０３ 雫原比留子の初陣
+addtoken(3422790, "15866239174142086161")
+addappid(3422790, 1, "b543c1fe930dd8d79d1b91ef388462de0f4f316083ad2c65482347ed0fcbfd1c") --Dlcname HUNDRED LINE -最終防衛学園- 特防隊前日譚ＦＩＬＥ０３ 雫原比留子の初陣
+--Dlcname The Hundred Line -Last Defense Academy- Original Soundtrack
+addappid(4282300) --Dlcname The Hundred Line -Last Defense Academy- Original Soundtrack
