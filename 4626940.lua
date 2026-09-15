73fb950805967a8935fb56686c0eeb88e3e35700
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 20:58:16 +0800

    add 4626940.lua

---
+++ 4626940.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4626940.lua
+--Gamename Scratch the Ticket
+addappid(4626940) --Mainappid Scratch the Ticket
+addappid(4626941, 1, "7432783b01898bdfc1970bdc0351ec130f3d79f76d599e592ab4493b55628c94") --Main Windows Depot Scratch the Ticket
+setManifestid(4626941, "3064777213644372820", 768912304)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
