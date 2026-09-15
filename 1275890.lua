73805419c48681f9fcf077547531c9383dae8d90
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 20:59:15 +0800

    add 1275890.lua

--- 1275890.lua
+++ 1275890.lua
@@ -1,7 +1,10 @@
-addappid(1275890)
-addappid(1275891,0,"311496f62a59013629f9698b66e74dc8432a2fd9d30b1182891b7a37a9f2113f")
-setManifestid(1275891,"1879844573517131127")
-addappid(1275892,0,"1e23c689edc58019b16cb8dbd4a58a337349ea18d0a52dc77b2882ad0c4dd3e4")
-setManifestid(1275892,"1065639639616012142")
-addappid(1275893,0,"cdd2e002910dcea162209fe5a2b76a05dd46856d6a4b649e09b7b8ccf7ccc1e8")
-setManifestid(1275893,"5245699796921645466")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1275890.lua
+--Gamename Baldi's Basics Plus
+addappid(1275890, 1, "e75b8d91a4b60f356738a5523dd17ad5493f7983e204176e0e9a4f0c18de57e6") --Mainappid Baldi's Basics Plus
+addappid(1275891, 1, "311496f62a59013629f9698b66e74dc8432a2fd9d30b1182891b7a37a9f2113f") --Main Windows Depot Baldi's Basics Plus
+setManifestid(1275891, "6761812723407193713", 73797056)
+addappid(1275892, 1, "1e23c689edc58019b16cb8dbd4a58a337349ea18d0a52dc77b2882ad0c4dd3e4") --Main Linux Depot Baldi's Basics Plus
+setManifestid(1275892, "1648558442256541571", 74802240)
+addappid(1275893, 1, "cdd2e002910dcea162209fe5a2b76a05dd46856d6a4b649e09b7b8ccf7ccc1e8") --Main Macos Depot Baldi's Basics Plus
+setManifestid(1275893, "97270620090631718", 81031904)
