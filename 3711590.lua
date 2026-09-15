064f9a92eac5d3673254c4779d19783818c65137
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 04:25:22 +0800

    add 3711590.lua

--- 3711590.lua
+++ 3711590.lua
@@ -3,7 +3,7 @@
 --Gamename Train 45
 addappid(3711590, 1, "d0fbcba33e35102ec47709617020d7bc9b402c89fa0f1e92369a5972a8738a3b") --Mainappid Train 45
 addappid(3711591, 1, "66e440ffab28f63a155822137b57433dad7a9ddf50009e8d61ff1d48af37019c") --Main Windows Depot Train 45
-setManifestid(3711591, "8882598814287590726", 1302374736)
+setManifestid(3711591, "5264271296513230747", 1302819344)
 --Dlcname Train 45 Artbook
 addappid(4612910) --Dlcname Train 45 Artbook
 addappid(4612911, 1, "45f012daa170c726e8d1234119150130a703e293a9513f461608700811726e63") --Dlc Windows Depot Train 45 Artbook
