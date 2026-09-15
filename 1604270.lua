d7caf7b57dff8eb09f202d556fda8d32ba732ef8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 12:49:30 +0800

    add 1604270.lua

--- 1604270.lua
+++ 1604270.lua
@@ -1,17 +1,39 @@
-addappid(1604270)
-addtoken(1604270,"1075697477965442338")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-setManifestid(229000,"4622705914179893434")
-setManifestid(229001,"4049573910112143457")
-setManifestid(229002,"7260605429366465749")
-setManifestid(229003,"8740933542064151477")
-setManifestid(229004,"5220958916987797232")
-setManifestid(229005,"7992454656023763365")
-setManifestid(229006,"1784011429307107530")
-addappid(1604271,0,"da1386311fdfb5dc5fd71357c294bbce7afd7e86bffd0588249f39807ac91d08")
-setManifestid(1604271,"5953656088507625829")
-addappid(1604272,0,"dec4f347ca36d0293362a8dba7e62b9d0f950872215aa4b70de99549ad402b27")
-setManifestid(1604272,"8016565903785953344")
-addappid(3751200,0,"7da9c745d0ff4b29d80193b0310c863c94993cf694eb5fb9c815e2a808e58e35")
-setManifestid(3751200,"8433294051576507884")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1604270.lua
+--Gamename Broken Arrow
+addappid(1604270, 1, "6a1d71140c5c5a62ae105124fe68479f68eb01b0bd07c51ce511632f9101ec2c") --Mainappid Broken Arrow
+addtoken(1604270, "1075697477965442338")
+addappid(1604271, 1, "da1386311fdfb5dc5fd71357c294bbce7afd7e86bffd0588249f39807ac91d08") --Main Windows Language Depot English Broken Arrow
+setManifestid(1604271, "3623928301832451787", 51991988176)
+addappid(1604272, 1, "dec4f347ca36d0293362a8dba7e62b9d0f950872215aa4b70de99549ad402b27") --Main Windows Depot Broken Arrow
+setManifestid(1604272, "3493301947442336063", 3132816)
+--Dlcname Broken Arrow Soundtrack
+addtoken(3693780, "2878295017911079096")
+addappid(3693780) --Dlcname Broken Arrow Soundtrack
+addappid(3693781, 1, "e0e98c1b997f2cd43ff79d143606267c7a23dce3abceb239f088a33c3923cd63") --Dlc Windows Depot Broken Arrow Soundtrack
+setManifestid(3693781, "8557652084982604654", 501966496)
+--Dlcname AppID 3751200
+addtoken(3751200, "11570295912332125767")
+addappid(3751200, 1, "7da9c745d0ff4b29d80193b0310c863c94993cf694eb5fb9c815e2a808e58e35") --Dlcname AppID 3751200
+setManifestid(3751200, "7329335284774443223", 75208880)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229001, 1, "ec23e1a1e1da6205722291f91bf72d1989cb347245e5a14d8d514c4ad5ba4e97") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229001, "4049573910112143457", 268006912)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229003, "8740933542064151477", 42914928)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(4151720) --Dlcname Broken Arrow: Baltic Battalion
+addappid(4232510) --Dlcname Broken Arrow: Russian Guard
