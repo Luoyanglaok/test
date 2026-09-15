5bb4d71fbd51837f3121da0b31c8266991666ce7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 08:07:58 +0800

    add 1570020.lua

--- 1570020.lua
+++ 1570020.lua
@@ -1,3 +1,6 @@
-addappid(1570020)
-addappid(1570021,0,"e9ca437800a02541ec4f01a35c050be03285f368857b545ed94dae35d8756d4a")
-setManifestid(1570021,"4119791831202850679")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1570020.lua
+--Gamename World's End Club
+addappid(1570020) --Mainappid World's End Club
+addappid(1570021, 1, "e9ca437800a02541ec4f01a35c050be03285f368857b545ed94dae35d8756d4a") --Main Windows Depot World's End Club
+setManifestid(1570021, "4119791831202850679", 3231185568)
