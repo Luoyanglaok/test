5afab232a4f267305b5ebfc7fa61c75b76c37073
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 10:45:15 +0800

    add 4282300.lua

---
+++ 4282300.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4282300.lua
+--Gamename AppID_4282300
+addappid(4282300) --Mainappid AppID_4282300
+addappid(4282301, 1, "3bb523d111d308433cf1237442a5537eae94af6f7a6e3e29c14a4bcc29c1169a") --Main Windows Depot AppID_4282300
+setManifestid(4282301, "2729221457319718334", 474870400)
+addappid(4282302, 1, "2208d244492daaebffe5c8e9da4453f661ee02f238877bf00d9f948ea391da57") --Main Windows Depot AppID_4282300
+setManifestid(4282302, "897302265533361316", 3492110752)
+addappid(4282303, 1, "e49536c73ee03ca39d0fbae6d6f77f001c125d73fc2dcb556184c9428c7e4676") --Main Windows Language Depot Japanese AppID_4282300
+setManifestid(4282303, "5104185891663692057", 474868448)
+addappid(4282304, 1, "52230be9bfdb510b4f9c17c486a15fb8382455c290793bf8cfa2786dbfc508b4") --Main Windows Language Depot Japanese AppID_4282300
+setManifestid(4282304, "3539567785708280480", 3492110752)
+addappid(4282305, 1, "8c123a99bc8ae71795db6d781789afb6a560aecf00cd509e12cb592bd63d30b4") --Main Windows Language Depot Schinese AppID_4282300
+setManifestid(4282305, "5675787956733278481", 474865824)
+addappid(4282306, 1, "4469f8101240a75b48e0f19e20927712e8d388344f64e721aae031a0a85eba21") --Main Windows Language Depot Schinese AppID_4282300
+setManifestid(4282306, "9010061429308007685", 3492110752)
+addappid(4282307, 1, "c92604cb07a3ca77ce638ab9085daa1d01cc7e38982e4deb0ced67e558b7e610") --Main Windows Language Depot Tchinese AppID_4282300
+setManifestid(4282307, "8648385234630074449", 474866000)
+addappid(4282308, 1, "10f847da110730ae4f060e2049b307b7a6b52e833f6657338c380b3e055bb52d") --Main Windows Language Depot Tchinese AppID_4282300
+setManifestid(4282308, "3093752262478099799", 3492110752)
