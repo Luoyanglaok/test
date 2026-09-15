cb072df9645079bc3071f025bc5581d7dbb4ed97
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 20:17:20 +0800

    add 243470.lua

--- 243470.lua
+++ 243470.lua
@@ -1,78 +1,113 @@
-addappid(243470)
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-setManifestid(229001,"4049573910112143457")
-addappid(243437,0,"453ae22778c9a046e28bf731c08bb0af550566ad0cc47fdd6a75e8e589c29807")
-setManifestid(243437,"6543707150037666796")
-addappid(243472,0,"adee09eb2f2f87a7fc5bd5abc7a6daef85889156da92e591b198bb6e7066ed25")
-setManifestid(243472,"1774278393062350330")
-addappid(243473,0,"1d449edba5ff3d393d749ad7ea8bc577c4331c3304077f8f4b5d53dc2930195e")
-setManifestid(243473,"7393497789577191823")
-addappid(243474,0,"8cc4873c48760bd3d472680bfaac98003d53453ae5499106891a145d44d1bf02")
-setManifestid(243474,"2677117606687932389")
-addappid(243475,0,"27a0151c5291c7402580483b0bfdd52f23ca8be8137ada3ff44e214b4a31d4e4")
-setManifestid(243475,"2641619498238537385")
-addappid(243476,0,"594073c6d5066dfb653a5f25e86d8abb71968c8bb8a0dc9b1335333a3628b96d")
-setManifestid(243476,"6893969456469766077")
-addappid(243477,0,"b4f1f4125b68cb58e579b0e9721d1a11b0702e53667376b2077bcf8ad5aa7680")
-setManifestid(243477,"4746735065034948169")
-addappid(243478,0,"4c3dba743034ec7845b0f9275b8b5fb8b6a404ddf6a51fbc14ff910def21ca33")
-setManifestid(243478,"5408765191612297193")
-addappid(243479,0,"8194a59d76827d5c6a853c7d28610826d4ce54d4584ec1c3ed0945c16f7cc5f6")
-setManifestid(243479,"1781482215680542239")
-addappid(243480,0,"2f41dda0871fbd049db13e5b8c93aaed31ec2fa1a4507fb680825bc2c88f3e2f")
-setManifestid(243480,"2163185997145201045")
-addappid(243481,0,"1ad1e106f29b29021045fb5612bc652122ac6ab4706e389668b7e47c7848c658")
-setManifestid(243481,"3669181965975934130")
-addappid(243482,0,"b6b6c54173d11c7c6fbb2d3d227d2367e65be1af56f2bc123fc1f3bd11227714")
-setManifestid(243482,"6839500667345753022")
-addappid(243483,0,"d6f43caf35e3e8daf4e4a862986323a0e8c36054795a60445b4a1641bd01852c")
-setManifestid(243483,"3426855786923651137")
-addappid(243484,0,"832dd18b8c9afe55acf05d8e3e515017d8b514dd65e765b4953b2d96b9216a1d")
-setManifestid(243484,"8917800849169533312")
-addappid(243485,0,"0aeb55622681b144828a6849484dc93183780e441b6b39927beeae067309faea")
-setManifestid(243485,"6736938428494126940")
-addappid(243486,0,"50d971a41af68757304374ae351e21830d32a81826ca7e19d71c2623572bd840")
-setManifestid(243486,"5022705916316509828")
-addappid(243487,0,"0f3838c855b1d540db4881c89acb9da28b8eb4d07c45832100cdb1b8ea9f62cf")
-setManifestid(243487,"4679331382201445970")
-addappid(243488,0,"cfd8e9b779bd2109bbd9012343bc97404613327295698ea49438488e9e374f83")
-setManifestid(243488,"1897496850595183508")
-addappid(293055,0,"07308d946c4eb0c38ed54ddec03e7dc70cbd9905f16aafb23f4ebb1366eeedf4")
-setManifestid(293055,"7966729444078833786")
-addappid(293057,0,"95474de677410428364ae70a1f391406da35f6d14b72dda6a7aa42a5040757e5")
-setManifestid(293057,"3489527419200560368")
-addappid(293059,0,"c3e377b39e27482ab87c3cd9fe1beb790a8252bf15b914898f3382b5a31f231b")
-setManifestid(293059,"6421005004532097582")
-addappid(293061,0,"b6875dd3b7ed1e9c2a8e1ddbb62626e2f4302fc2eefd01b2fbae3b09b5718a8c")
-setManifestid(293061,"4116348827715489844")
-addappid(293063,0,"cc1a8f01eb4a11345eb1426b7dfc217515d7f5024a2eb3f01ad10254fd5fbbc8")
-setManifestid(293063,"40897716262624801")
-addappid(293064,0,"fcb1eb3608df8698384c34215f8f1d5ed286f5d9c61ea603d5eef88e2ff11f41")
-setManifestid(293064,"3858197885764284729")
-addappid(293054,0,"6a07e87977d2328dfb2ca918bf228022d5a6d1d02fb86a29f3e460bd8ce415cf")
-setManifestid(293054,"3534712475932838237")
-addappid(293066,0,"fece6518ba9e73e347f13b81a872b821f63a23d9c912e1a236d3dd4d50c9306e")
-setManifestid(293066,"2536281954435214171")
-addappid(293065,0,"b1f2ed2249e2729518e713e6e3ae3319f4f428d1d506728457fbed4c2b175499")
-setManifestid(293065,"5159588849394621757")
-addappid(293067,0,"024b05c77945604f9c49ffb1be8ae42ffdd68b18cead5dd3061fc0c6783bb72f")
-setManifestid(293067,"6512055718638381686")
-addappid(298789,0,"58ac0b7fc7366ee2845552f479408b4ffc19514d35b552e1ba0b5600101861f1")
-setManifestid(298789,"2879812338883668516")
-addappid(298918,0,"c68273003c364892abf9f9553b801056e90a6b572feccfa9bfb8621d78ce26c5")
-setManifestid(298918,"8075397187365750216")
-addappid(293068,0,"9e71b1e0cde5fb03c88861e9f84ef4f1cbdf6414bdc8050be26a17923c2d4726")
-setManifestid(293068,"8901343776127074727")
-addappid(293069,0,"bd7d9a6a1157b8d20ee3a1730c8b530ba9eb80a33c9e4072efebcbc0543c99a7")
-setManifestid(293069,"1249918633068890571")
-addappid(243489,0,"86697691499c13832dca56937a77ba2d88033449e9fa0ec94184f220ea967f78")
-setManifestid(243489,"362035036454864186")
-addappid(243471,0,"77184dab3ba78bfc47c7a4de0a466dcecc808954c08be694e6772e5efdca2d21")
-setManifestid(243471,"5582561119037069284")
-addappid(300668,0,"10f6f19ea39ad6c757590a877d7c7a6686e479e21af0a7754d942160102d1cb0")
-setManifestid(300668,"6878173766487895011")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 243470.lua
+--Gamename Watch_Dogs™
+addappid(243470, 1, "3390b2efef88a1d26e3579dde5d9b44e24c04b13f4ff845e71eaed03c064a2f4") --Mainappid Watch_Dogs™
+addappid(243437, 1, "453ae22778c9a046e28bf731c08bb0af550566ad0cc47fdd6a75e8e589c29807") --Main Windows Language Depot English Watch_Dogs™
+setManifestid(243437, "6543707150037666796", 1627486464)
+addappid(243471, 1, "77184dab3ba78bfc47c7a4de0a466dcecc808954c08be694e6772e5efdca2d21") --Main Windows Depot Watch_Dogs™
+setManifestid(243471, "5582561119037069284", 12188550272)
+addappid(243472, 1, "adee09eb2f2f87a7fc5bd5abc7a6daef85889156da92e591b198bb6e7066ed25") --Main Windows Language Depot French Watch_Dogs™
+setManifestid(243472, "1774278393062350330", 1559324560)
+addappid(243473, 1, "1d449edba5ff3d393d749ad7ea8bc577c4331c3304077f8f4b5d53dc2930195e") --Main Windows Language Depot German Watch_Dogs™
+setManifestid(243473, "7393497789577191823", 1544030016)
+addappid(243474, 1, "8cc4873c48760bd3d472680bfaac98003d53453ae5499106891a145d44d1bf02") --Main Windows Language Depot Italian Watch_Dogs™
+setManifestid(243474, "2677117606687932389", 1568897488)
+addappid(243475, 1, "27a0151c5291c7402580483b0bfdd52f23ca8be8137ada3ff44e214b4a31d4e4") --Main Windows Language Depot Swedish Watch_Dogs™
+setManifestid(243475, "2641619498238537385", 1627404560)
+addappid(243476, 1, "594073c6d5066dfb653a5f25e86d8abb71968c8bb8a0dc9b1335333a3628b96d") --Main Windows Language Depot Finnish Watch_Dogs™
+setManifestid(243476, "6893969456469766077", 1627411824)
+addappid(243477, 1, "b4f1f4125b68cb58e579b0e9721d1a11b0702e53667376b2077bcf8ad5aa7680") --Main Windows Language Depot Norwegian Watch_Dogs™
+setManifestid(243477, "4746735065034948169", 1627404976)
+addappid(243478, 1, "4c3dba743034ec7845b0f9275b8b5fb8b6a404ddf6a51fbc14ff910def21ca33") --Main Windows Language Depot Dutch Watch_Dogs™
+setManifestid(243478, "5408765191612297193", 1626604288)
+addappid(243479, 1, "8194a59d76827d5c6a853c7d28610826d4ce54d4584ec1c3ed0945c16f7cc5f6") --Main Windows Language Depot Portuguese Watch_Dogs™
+setManifestid(243479, "1781482215680542239", 1628158032)
+addappid(243480, 1, "2f41dda0871fbd049db13e5b8c93aaed31ec2fa1a4507fb680825bc2c88f3e2f") --Main Windows Language Depot Brazilian Watch_Dogs™
+setManifestid(243480, "2163185997145201045", 1568694160)
+addappid(243481, 1, "1ad1e106f29b29021045fb5612bc652122ac6ab4706e389668b7e47c7848c658") --Main Windows Language Depot Polish Watch_Dogs™
+setManifestid(243481, "3669181965975934130", 1626676016)
+addappid(243482, 1, "b6b6c54173d11c7c6fbb2d3d227d2367e65be1af56f2bc123fc1f3bd11227714") --Main Windows Language Depot Japanese Watch_Dogs™
+setManifestid(243482, "6839500667345753022", 1562946768)
+addappid(243483, 1, "d6f43caf35e3e8daf4e4a862986323a0e8c36054795a60445b4a1641bd01852c") --Main Windows Language Depot Russian Watch_Dogs™
+setManifestid(243483, "3426855786923651137", 1567748224)
+addappid(243484, 1, "832dd18b8c9afe55acf05d8e3e515017d8b514dd65e765b4953b2d96b9216a1d") --Main Windows Language Depot Hungarian Watch_Dogs™
+setManifestid(243484, "8917800849169533312", 1627404384)
+addappid(243485, 1, "0aeb55622681b144828a6849484dc93183780e441b6b39927beeae067309faea") --Main Windows Language Depot Czech Watch_Dogs™
+setManifestid(243485, "6736938428494126940", 1627403600)
+addappid(243486, 1, "50d971a41af68757304374ae351e21830d32a81826ca7e19d71c2623572bd840") --Main Windows Language Depot Koreana Watch_Dogs™
+setManifestid(243486, "5022705916316509828", 1627412816)
+addappid(243487, 1, "0f3838c855b1d540db4881c89acb9da28b8eb4d07c45832100cdb1b8ea9f62cf") --Main Windows Language Depot Tchinese Watch_Dogs™
+setManifestid(243487, "4679331382201445970", 1628872640)
+addappid(243488, 1, "cfd8e9b779bd2109bbd9012343bc97404613327295698ea49438488e9e374f83") --Main Windows Language Depot Spanish Watch_Dogs™
+setManifestid(243488, "1897496850595183508", 1576180880)
+addappid(243489, 1, "86697691499c13832dca56937a77ba2d88033449e9fa0ec94184f220ea967f78") --Main Windows Depot Watch_Dogs™
+setManifestid(243489, "362035036454864186", 108579104)
+addappid(293063, 1, "cc1a8f01eb4a11345eb1426b7dfc217515d7f5024a2eb3f01ad10254fd5fbbc8") --Main Windows Language Depot Russian Watch_Dogs™
+setManifestid(293063, "40897716262624801", 100109648)
+addappid(293064, 1, "fcb1eb3608df8698384c34215f8f1d5ed286f5d9c61ea603d5eef88e2ff11f41") --Main Windows Language Depot Danish Watch_Dogs™
+setManifestid(293064, "3858197885764284729", 1627405264)
+addappid(293065, 1, "b1f2ed2249e2729518e713e6e3ae3319f4f428d1d506728457fbed4c2b175499") --Main Windows Depot Watch_Dogs™
+setManifestid(293065, "5159588849394621757", 12174447168)
+addappid(293066, 1, "fece6518ba9e73e347f13b81a872b821f63a23d9c912e1a236d3dd4d50c9306e") --Main Windows Depot Watch_Dogs™
+setManifestid(293066, "2536281954435214171", 101645520)
+addappid(293067, 1, "024b05c77945604f9c49ffb1be8ae42ffdd68b18cead5dd3061fc0c6783bb72f") --Main Windows Depot Watch_Dogs™
+setManifestid(293067, "6512055718638381686", 9913264)
+addappid(293068, 1, "9e71b1e0cde5fb03c88861e9f84ef4f1cbdf6414bdc8050be26a17923c2d4726") --Main Windows Language Depot Japanese Watch_Dogs™
+setManifestid(293068, "8901343776127074727", 1593438064)
+addappid(293069, 1, "bd7d9a6a1157b8d20ee3a1730c8b530ba9eb80a33c9e4072efebcbc0543c99a7") --Main Windows Language Depot Koreana Watch_Dogs™
+setManifestid(293069, "1249918633068890571", 1624082912)
+addappid(298789, 1, "58ac0b7fc7366ee2845552f479408b4ffc19514d35b552e1ba0b5600101861f1") --Main Windows Language Depot Tchinese Watch_Dogs™
+setManifestid(298789, "2879812338883668516", 1625542736)
+addappid(298918, 1, "c68273003c364892abf9f9553b801056e90a6b572feccfa9bfb8621d78ce26c5") --Main Windows Language Depot English Watch_Dogs™
+setManifestid(298918, "8075397187365750216", 1624156560)
+addappid(300668, 1, "10f6f19ea39ad6c757590a877d7c7a6686e479e21af0a7754d942160102d1cb0") --Main Windows Language Depot Schinese Watch_Dogs™
+setManifestid(300668, "6878173766487895011", 1584170816)
+--Dlcname Watch_Dogs Season Pass
+addappid(293054, 1, "6a07e87977d2328dfb2ca918bf228022d5a6d1d02fb86a29f3e460bd8ce415cf") --Dlcname Watch_Dogs Season Pass
+setManifestid(293054, "3534712475932838237", 480)
+--Dlcname Watch_Dogs - Deluxe Edition Exclusive Content
+addappid(293055, 1, "07308d946c4eb0c38ed54ddec03e7dc70cbd9905f16aafb23f4ebb1366eeedf4") --Dlcname Watch_Dogs - Deluxe Edition Exclusive Content
+setManifestid(293055, "7966729444078833786", 27574352)
+--Dlcname Watch_Dogs - Conspiracy
+addappid(293057, 1, "95474de677410428364ae70a1f391406da35f6d14b72dda6a7aa42a5040757e5") --Dlcname Watch_Dogs - Conspiracy
+setManifestid(293057, "3489527419200560368", 30542640)
+--Dlcname Watch_Dogs - Access Granted Pack
+addappid(293059, 1, "c3e377b39e27482ab87c3cd9fe1beb790a8252bf15b914898f3382b5a31f231b") --Dlcname Watch_Dogs - Access Granted Pack
+setManifestid(293059, "6421005004532097582", 656)
+--Dlcname Watch_Dogs Bad Blood
+addtoken(293061, "12130676317065299263")
+addappid(293061, 1, "b6875dd3b7ed1e9c2a8e1ddbb62626e2f4302fc2eefd01b2fbae3b09b5718a8c") --Dlcname Watch_Dogs Bad Blood
+setManifestid(293061, "4116348827715489844", 3574260112)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229001, 1, "ec23e1a1e1da6205722291f91bf72d1989cb347245e5a14d8d514c4ad5ba4e97") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229001, "4049573910112143457", 268006912)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(298781) --Dlcname Watch_Dogs - Uplay Activation
+addappid(298782) --Dlcname Watch_Dogs - Deluxe Edition Preorder Uplay Activation
+addappid(298783) --Dlcname Watch_Dogs - Deluxe Edition Uplay Activation
+addappid(298784) --Dlcname Watch_Dogs - Season Pass Uplay Activation
+addappid(298785) --Dlcname Watch_Dogs - Deluxe Edition Exclusive Content Uplay Activation
+addappid(298786) --Dlcname Watch_Dogs - Conspiracy Uplay Activation
+addappid(298788) --Dlcname Watch_Dogs - Bad Blood Uplay Activation
+addappid(298910) --Dlcname Watch_Dogs - Preorder Uplay Activation (RU)
+addappid(298911) --Dlcname AppID 298911
+addtoken(298911, "3065787327939462993")
+addappid(298913) --Dlcname AppID 298913
+addtoken(298913, "15265792249588161980")
+addappid(300660) --Dlcname Watch_Dogs - Preorder Uplay Activation (ASIA)
+addappid(300661) --Dlcname Watch_Dogs - Uplay Activation (ASIA)
+addappid(300662) --Dlcname Watch_Dogs - Deluxe Edition Preorder Uplay Activation (ASIA)
+addappid(300663) --Dlcname Watch_Dogs - Deluxe Edition Uplay Activation (ASIA)
+addappid(303890) --Dlcname Watch_Dogs - Preorder Uplay Activation
+addappid(303920) --Dlcname Watch_Dogs - Deluxe Edition Preorder Uplay Activation (RU)
+addappid(303960) --Dlcname AppID 303960
+addtoken(303960, "12387094823720109792")
