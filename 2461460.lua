25f4ce30dcb7797fa118fcf21be57ed2490c5699
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 00:24:47 +0800

    add 2461460.lua

--- 2461460.lua
+++ 2461460.lua
@@ -1,5 +1,20 @@
-addappid(2461460)
-addappid(2461461,0,"62dc73ebf2f505a1ec7edc0d23331a4c64c687cfb0534f6023a2f0f8d3d97d4a")
-setManifestid(2461461,"1929603524348093827")
-addappid(2461462)
-addappid(2461463)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2461460.lua
+--Gamename Womanizer
+addappid(2461460, 1, "2ce93767c0a47ad475a3a913496530dba70405871631db5d308aec02e0d6f6c7") --Mainappid Womanizer
+addappid(2461461, 1, "62dc73ebf2f505a1ec7edc0d23331a4c64c687cfb0534f6023a2f0f8d3d97d4a") --Main Windows Depot Womanizer
+setManifestid(2461461, "2647610199020257752", 719158688)
+addappid(2461462, 1, "8a4f8b75c728122620c4934a2ffed73099ad959020fc19f774d1fb29c281c9a1") --Main Linux Depot Womanizer
+setManifestid(2461462, "2565694458858559639", 714626688)
+addappid(2461463, 1, "b52f2fcc2fe5b266c0d9508f4c0ce566d839b84d421f10e1146be645614293c5") --Main Macos Depot Womanizer
+setManifestid(2461463, "295713323041659601", 723621648)
+--Dlcname Womanizer - Adult content
+addappid(2464590) --Dlcname Womanizer - Adult content
+addappid(2464591, 1, "bbfd80d0d4aeb8592a9a6eac714264fceaa7ae035e67309d49a3230a1265e6ff") --Dlc Linux Depot Womanizer - Adult content
+setManifestid(2464591, "8346057519635671389", 224574752)
+addappid(2464592, 1, "26d34e6f771597c56c1f44610e9188070bcb8bf48b9c7e382afc11e9b662e62b") --Dlc Macos Depot Womanizer - Adult content
+setManifestid(2464592, "3252049193066876298", 224574752)
+addappid(2464593, 1, "9ec6ceb90d093a0b0e1a99891744d9bf6d76d4522d8409ab1c59cf30dc59083b") --Dlc Windows Depot Womanizer - Adult content
+setManifestid(2464593, "207692832063391693", 224788112)
+--Missing Dlcs
+--Dlcname Womanizer Soundtrack
