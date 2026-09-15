9d6114ba40409e00e9d11da17f54bfd14923628b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 15:23:46 +0800

    add 2468630.lua

---
+++ 2468630.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2468630.lua
+--Gamename Magic Inn
+addappid(2468630) --Mainappid Magic Inn
+addappid(2468631, 1, "827097914547406b092584f27b9910aeeac6241e10013c5908f89462407611e4") --Main Windows Depot Magic Inn
+setManifestid(2468631, "1110753001623381285", 3497987248)
