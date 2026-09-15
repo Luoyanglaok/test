e2a02cd14d1ad2faa9b5673c2d93021697505aea
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 00:13:16 +0800

    add 3427620.lua

--- 3427620.lua
+++ 3427620.lua
@@ -1,5 +1,10 @@
-addappid(3427620)
-addappid(3427621,0,"80c213b42de11f37c994633ad9882cd7f2d37ae3bfd332d851c4dda7cc034283")
-setManifestid(3427621,"6971981775118530302")
-addappid(3456670)
-addappid(3456430)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3427620.lua
+--Gamename Locked in Temptation
+addappid(3427620) --Mainappid Locked in Temptation
+addappid(3427621, 1, "80c213b42de11f37c994633ad9882cd7f2d37ae3bfd332d851c4dda7cc034283") --Main Windows Depot Locked in Temptation
+setManifestid(3427621, "4365032004158231673", 891257088)
+--Missing Dlcs
+--Dlcname Locked in Temptation - Soundtrack
+--Dlcname Locked in Temptation - Wallpapers
+--Dlcname Locked in Temptation - Walkthrough
