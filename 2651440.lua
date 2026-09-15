713d06cf76afec64b03ffcfebc8919a93e60a7eb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 15:50:45 +0800

    add 2651440.lua

---
+++ 2651440.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2651440.lua
+--Gamename Fallen Elf Freya
+addappid(2651440, 1, "5a6cc18e8981f0acc6ce5a648dd400db0653ae3b26969a38a15a290d09b752f1") --Mainappid Fallen Elf Freya
+addappid(2651441, 1, "9f20b1b76e295cdd391c66998004ddd2c8ebaa8777e96a4b5c067ef1a38bacb4") --Main Windows Depot Fallen Elf Freya
+setManifestid(2651441, "6776615499298707681", 1100178944)
