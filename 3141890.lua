476fbfd7bddc3d40963db1d9754cbe87204b00dc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 19:05:46 +0800

    add 3141890.lua

--- 3141890.lua
+++ 3141890.lua
@@ -1,5 +1,10 @@
-addappid(3141890)
-addappid(3141892,0,"30956106a730200480d1bdbaccc2ea8717e2dd12b640f292cffdd86c666de83e")
-setManifestid(3141892,"1523254154678674531")
-addappid(3141893,0,"85edd720b0c91c2ca0f819fe57d80b19bc370fc2a3eb794d990a2de312ca2a96")
-setManifestid(3141893,"5896485410645470466")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3141890.lua
+--Gamename Night of The Black Goat
+addappid(3141890, 1, "6b4b43ed0133a44810ad048b5ec9c018e5b73975dd595c864dec6f65dcb121d6") --Mainappid Night of The Black Goat
+addappid(3141891, 1, "56e3478d490b318eb59bb4e6fa7b4bbc797d77223bac34482f00bf3dc4f8f419") --Main Windows Language Depot Japanese Night of The Black Goat
+setManifestid(3141891, "3402655135391737376", 3127513648)
+addappid(3141892, 1, "30956106a730200480d1bdbaccc2ea8717e2dd12b640f292cffdd86c666de83e") --Main Windows Language Depot Tchinese Night of The Black Goat
+setManifestid(3141892, "1523254154678674531", 3135204928)
+addappid(3141893, 1, "85edd720b0c91c2ca0f819fe57d80b19bc370fc2a3eb794d990a2de312ca2a96") --Main Windows Language Depot English Night of The Black Goat
+setManifestid(3141893, "7215684426071291128", 3134109648)
