3c05e4b4c8f755bdc2e1088dd2a3b34a375eba12
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 22:25:17 +0800

    add 3216500.lua

---
+++ 3216500.lua
@@ -0,0 +1,27 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3216500.lua
+--Gamename SHUTEN ORDER
+addappid(3216500, 1, "b77eb496770278f0167f0497c7a96405fde936657548b634f11bec77c96fce7f") --Mainappid SHUTEN ORDER
+addappid(3216501, 1, "dc2a9f945722661f9ae1e59db7468dfdb8ec05e296b74c91b03ba7f8fa04985b") --Main Windows Depot SHUTEN ORDER
+setManifestid(3216501, "335653919369989008", 12058580928)
+--Dlcname SHUTEN ORDER Mini Soundtrack
+addappid(3607160) --Dlcname SHUTEN ORDER Mini Soundtrack
+addappid(3607161, 1, "0d474914346ca61f9ebd49c3916d0eac099c97462110f3c8227985241dfb03da") --Dlc Windows Depot SHUTEN ORDER Mini Soundtrack
+setManifestid(3607161, "4566724711491614754", 103417328)
+addappid(3607162, 1, "b8dd7ecd9976bb04eddcb76008817f84f7c954f126cd80e7f387b80294dc3cc6") --Dlc Windows Depot SHUTEN ORDER Mini Soundtrack
+setManifestid(3607162, "2333809494426786504", 814653056)
+addappid(3607163, 1, "d11665b3043499d21ae424aadce3b12ab62379f04f2c480aa972f4e78b1f0680") --Dlc Windows Language Depot Japanese SHUTEN ORDER Mini Soundtrack
+setManifestid(3607163, "8108519165613928051", 103655696)
+addappid(3607164, 1, "b8c66527d4cf7b6ccfb135be7583ce4931be4a6b436dc877a875809eb5177bf7") --Dlc Windows Language Depot Japanese SHUTEN ORDER Mini Soundtrack
+setManifestid(3607164, "4520054213009594206", 814711968)
+--Dlcname SHUTEN ORDER Visual Art Book
+addappid(3662610, 1, "5d3f4ff0a91ad123ad0f1f203acaeac9dacd35c0b4ed8c24d1234a37eba95a02") --Dlcname SHUTEN ORDER Visual Art Book
+setManifestid(3662610, "1529808884715661129", 342454288)
+addappid(3662611, 1, "705a73476cb03feee4089e91420b6f73ca598447210afb329491ec592a88a057") --Dlc Windows Language Depot Japanese SHUTEN ORDER Visual Art Book
+setManifestid(3662611, "7748864325431194148", 342375072)
+addappid(3662612, 1, "d7f020a6790e2893a6758e0e3b4327f0d9e53b41756e6dc8a912f47252b23407") --Dlc Windows Language Depot Tchinese SHUTEN ORDER Visual Art Book
+setManifestid(3662612, "29693345176506549", 344551088)
+addappid(3662613, 1, "28baee7c77badc04f32c07c2783ad456c39c1f5a1db8041208c58190c6830b3b") --Dlc Windows Language Depot Schinese SHUTEN ORDER Visual Art Book
+setManifestid(3662613, "4694599794832704870", 344551088)
+--Dlcname AppID 3830900
+addappid(3830900, 1, "19b4862c443f0922872e53f5a98193ec6815380078edd4b3326f969ec16583a5") --Dlcname AppID 3830900
