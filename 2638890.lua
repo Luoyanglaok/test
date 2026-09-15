360d45b267d719aca5120e5c283de2ff9d87d755
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 08:22:26 +0800

    add 2638890.lua

--- 2638890.lua
+++ 2638890.lua
@@ -90,3 +90,4 @@ setManifestid(228988, "6645201662696499616", 22411856)
 --Dlcname Onimusha: WotS - Materials Pack: Red Souls II
 --Dlcname Onimusha: WotS - Materials Pack: Offerings II
 --Dlcname Onimusha: WotS - Materials Pack: Enhancements III
+--Dlcname Onimusha: Way of the Sword Original Sound Track
