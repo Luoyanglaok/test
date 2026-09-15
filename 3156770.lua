e0f8d498e6fdec552a643fb0af6fd52bb0259947
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 20:35:00 +0800

    add 3156770.lua

--- 3156770.lua
+++ 3156770.lua
@@ -1,5 +1,11 @@
-addappid(3156770)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(3156771,0,"e09d98ec7fb6604ed8c2f44292e3f8cb278dd0add46641f0434375eeb9255e87")
-setManifestid(3156771,"1143178387645886986")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3156770.lua
+--Gamename Witchfire
+addappid(3156770, 1, "a3d625ea35ccb9f6b7f85adb4bc578f136e51437a97ea3277887ca9ed995e2e8") --Mainappid Witchfire
+addappid(3156771, 1, "e09d98ec7fb6604ed8c2f44292e3f8cb278dd0add46641f0434375eeb9255e87") --Main Windows Depot Witchfire
+setManifestid(3156771, "2208463186393063821", 51085548432)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
