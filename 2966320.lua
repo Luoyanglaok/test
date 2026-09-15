d2d8b15c65875ce8fed66b07624185bdc31234d9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 03:37:42 +0800

    add 2966320.lua

--- 2966320.lua
+++ 2966320.lua
@@ -4,7 +4,7 @@
 addappid(2966320, 1, "208ea6f9923a37a7b011f392e50f7f7ab2952497337e184dbd00448c63bd1a50") --Mainappid Starsand Island
 addtoken(2966320, "1152859757700171993")
 addappid(2966321, 1, "ce86c628c5c525587d2aea36d5a745c980584e22eca6389d8bfd7a63c0db1c4c") --Main Windows Depot Starsand Island
-setManifestid(2966321, "7271785676686400459", 7880356400)
+setManifestid(2966321, "2076032856204621983", 7888514176)
 addappid(4391920) --Dlcname 星砂岛 春节国风家具和服装DLC
 addtoken(4391920, "4278437199386008459")
 addappid(4395490) --Dlcname 星砂岛 欧式梦幻家具、服装和载具DLC
