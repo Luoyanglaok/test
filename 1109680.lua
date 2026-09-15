e7389692e081633f7520589866a5c6d0df30b81a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 19:02:06 +0800

    add 1109680.lua

--- 1109680.lua
+++ 1109680.lua
@@ -1,3 +1,7 @@
-addappid(1109680)
-addappid(1109681,0,"edddbcd9ad0d20c27f9dfe5dbc63b54266d67a028f2f5e6ea81960a0925e0bc3")
-setManifestid(1109681,"5066923993795110095")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1109680.lua
+--Gamename Regiments
+addappid(1109680) --Mainappid Regiments
+addappid(1109681, 1, "edddbcd9ad0d20c27f9dfe5dbc63b54266d67a028f2f5e6ea81960a0925e0bc3") --Main Windows Depot Regiments
+setManifestid(1109681, "7821369386820259396", 3897322224)
+addappid(2542770) --Dlcname Regiments - Winds of Change
