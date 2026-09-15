8ef50791d513e60eb33b513a4bf2abc236521677
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 11:46:34 +0800

    add 2689120.lua

--- 2689120.lua
+++ 2689120.lua
@@ -1,3 +1,9 @@
-addappid(2689120)
-addappid(2689121,0,"87cb2be56e4de79d17f5cb52efc521ae4a02a57f2195116339023ad0aa27c4be")
-setManifestid(2689121,"3557783713296357071")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2689120.lua
+--Gamename Gladio Mori
+addappid(2689120, 1, "d0def7a45d0a846547ec3057ca2a0fa8ade238141732f5592dbba40702d925f5") --Mainappid Gladio Mori
+addappid(2689121, 1, "87cb2be56e4de79d17f5cb52efc521ae4a02a57f2195116339023ad0aa27c4be") --Main Windows Depot Gladio Mori
+setManifestid(2689121, "6025382391423264408", 715486096)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
