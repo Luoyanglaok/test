d22c1ce07944387c10ee5e0cec4c5e4838f22465
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 11:22:29 +0800

    add 2646320.lua

---
+++ 2646320.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2646320.lua
+--Gamename 息风谷战略
+addappid(2646320, 1, "72c776dde4e41be4d4d4d23e9bc5e73bdea9fb7386ed44772eaeda0825803aa5") --Mainappid 息风谷战略
+addtoken(2646320, "15908732202951388015")
+addappid(2646321, 1, "788707cb8c67eb748233059f2da076e2b585eb95d64774c323e01295bcc8d968") --Main Windows Depot 息风谷战略
+setManifestid(2646321, "7456128571277095939", 2678749696)
+addappid(4334591, 1, "350670de5174646e5110f12a6f6213e2d9b6438e029dd13b9c27dcff3bcc1334") --Main Windows Depot 息风谷战略
+setManifestid(4334591, "534403546266850802", 613448464)
+addappid(4334592, 1, "2a5b9653f2657ba167ccdca415f5fa93face237e10aa8dd8da1ae7ddceaa5872") --Main Windows Depot 息风谷战略
+setManifestid(4334592, "6618581582143281595", 72270160)
+--Dlcname 息风谷战略 原声音乐集
+addappid(4334590) --Dlcname 息风谷战略 原声音乐集
+--Dlcname 息风谷战略 - 云隐探秘
+addappid(4659120) --Dlcname 息风谷战略 - 云隐探秘
+addappid(4659121, 1, "72163cac8fdb67346b16fdcf7a7771aa07ade887d8547db59c7d4b9ff3103abc") --Dlc Windows Depot 息风谷战略 - 云隐探秘
+setManifestid(4659121, "4940762501709121001", 0)
+addappid(4670450) --Dlcname 息风谷战略 - 风起云涌
