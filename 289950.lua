81f5aafb659da7fa50a6eeb91eb198f4f7c0728b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 17:59:21 +0800

    add 289950.lua

--- 289950.lua
+++ 289950.lua
@@ -1,29 +1,39 @@
-addappid(289950)
-addappid(289951,0,"d10c2955fda3f4474d717f82c302b099f452a1d09565f837260b26fb6c41a26e")
-setManifestid(289951,"6187196982400466117")
-addappid(289952,0,"c63b4a8848ba573b8c5c8283b306965b97f2e17c2b5903fe153cd02324cdd54c")
-setManifestid(289952,"3246692775414292118")
-addappid(289963,0,"e3ddad94bb289fba3fc532dffb777de9ff8d6182a21e9d938e87e8e4d898f672")
-setManifestid(289963,"4230567957551800605")
-addappid(289954)
-addappid(289961)
-addappid(289955)
-addappid(289953)
-addappid(289958)
-addappid(289957)
-addappid(289959)
-addappid(289960)
-addappid(289962)
-addappid(289956)
-addappid(325680)
-addappid(325690)
-addappid(325691)
-addappid(325692)
-addappid(325693)
-addappid(334610)
-addappid(359780)
-addappid(359781)
-addappid(359790)
-addappid(404281)
-addappid(289964)
-addappid(289965)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 289950.lua
+--Gamename Construction Simulator 2015
+addappid(289950, 1, "5b8a65d0bffd65b0a1646ed4cda5926cb21da0c81ff79218be21e8bc5a5fd860") --Mainappid Construction Simulator 2015
+addappid(289951, 1, "d10c2955fda3f4474d717f82c302b099f452a1d09565f837260b26fb6c41a26e") --Main Windows Depot Construction Simulator 2015
+setManifestid(289951, "6187196982400466117", 922228240)
+addappid(289952, 1, "c63b4a8848ba573b8c5c8283b306965b97f2e17c2b5903fe153cd02324cdd54c") --Main Macos Depot Construction Simulator 2015
+setManifestid(289952, "3246692775414292118", 915977008)
+addappid(289953, 1, "01b335dc04d91b3f36c59a670ae5418ee135814003903760610ec42d92836d52") --Main Windows Language Depot German Construction Simulator 2015
+addappid(289958, 1, "dcb1c4e646941d12361a1b0fd8567f61bda561a50d8c6598fe60a66d05c7ed45") --Main Windows Language Depot Hungarian Construction Simulator 2015
+addappid(289959, 1, "501be033c10d3f90f9b03b6cf79e6215b14278aa32045078d5a7c5eafcabba57") --Main Windows Language Depot Japanese Construction Simulator 2015
+addappid(289960, 1, "39556948cd3743d4eb3a6dcf817415dede07ced1103ffbddbdc815feae765399") --Main Windows Language Depot Polish Construction Simulator 2015
+addappid(289961, 1, "f4d7d4eb64d723cfc69f89fcf8b594a8ca05ce8ef6623fffa95085201d834516") --Main Windows Language Depot Czech Construction Simulator 2015
+addappid(289962, 1, "0e206d1cced65d7f2e76d9a5ab2db4480eb566e7b142f12eaf8de05444b8cf73") --Main Windows Language Depot Russian Construction Simulator 2015
+addappid(289963, 1, "e3ddad94bb289fba3fc532dffb777de9ff8d6182a21e9d938e87e8e4d898f672") --Main Linux Depot Construction Simulator 2015
+setManifestid(289963, "4230567957551800605", 909556736)
+addappid(289964, 1, "c72d7d564b7f186b9dc2b6f6a1947a9e6e33335888ed6450579a32fa05ca7340") --Main Windows Language Depot Turkish Construction Simulator 2015
+addappid(289965, 1, "56636e44a5fc0c6b06ab539c4df0dfa2e6b8b1a5319990c9ee3c59ac79f6b058") --Main Windows Language Depot Brazilian Construction Simulator 2015
+addappid(320390) --Dlcname Construction Simulator 2015 - Media Markt Mission Mac
+addappid(329840) --Dlcname Construction Simulator 2015 - Saturn Mission Mac
+addappid(333470) --Dlcname Construction Simulator 2015 - Libro Mission Mac
+addappid(333480) --Dlcname Construction Simulator 2015 - Amazon Mission Mac
+addappid(333481) --Dlcname Construction Simulator 2015 - Steam Mission Mac
+addappid(334611) --Dlcname Construction Simulator 2015 - Zoo Mission Mac
+addappid(404280) --Dlcname Construction Simulator 2015: Vertical Skyline
+addappid(404282) --Dlcname Construction Simulator 2015: Liebherr LTM 1300 6.2
+addappid(404283) --Dlcname Construction Simulator 2015: Liebherr HTM 1204 ZA
+addappid(404284) --Dlcname Construction Simulator 2015: Liebherr A 918
+--Missing Dlcs
+--Dlcname Construction Simulator 2015 - Media Markt Mission
+--Dlcname Construction Simulator 2015 - Saturn Mission
+--Dlcname Construction Simulator 2015 - Libro Mission
+--Dlcname Construction Simulator 2015 - Amazon Mission
+--Dlcname Construction Simulator 2015 - Steam Mission
+--Dlcname Construction Simulator 2015 - Zoo Mission
+--Dlcname Construction Simulator 2015: Liebherr 150 EC-B
+--Dlcname Construction Simulator 2015: Liebherr LB28
+--Dlcname Construction Simulator 2015: Liebherr LR 1300
+--Dlcname Construction Simulator 2015: St. John’s Hospital Fuchsberg
