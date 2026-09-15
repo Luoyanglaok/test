88c472a9c59850c9283c905146ce34563dedb88a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 15:30:11 +0800

    add 2112740.lua

---
+++ 2112740.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2112740.lua
+--Gamename Humanica
+addappid(2112740, 1, "49f96f42327230e92f2140b6bcda8a069fa941a8023edea61ed70d6ffac211b6") --Mainappid Humanica
+addappid(2112741, 1, "05a239aab771f004b53f5783b779852a87d5647830795cf68b26d61d957dadfa") --Main Windows Depot Humanica
+setManifestid(2112741, "2530183555267120925", 112600224)
