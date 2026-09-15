2cc8389aecb8a03abe8f017850eb479794e664e0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 16:24:16 +0800

    add 37420.lua

--- 37420.lua
+++ 37420.lua
@@ -1,3 +1,6 @@
-addappid(37420)
-addappid(37421,0,"0cc4226a10a59b9efbe293a92f52292ef89eda3913b913a5d6f008b3b5512bf7")
-setManifestid(37421,"4669888909886600883")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 37420.lua
+--Gamename Ben There, Dan That!
+addappid(37420, 1, "695d6a6bcfd38931508da4c1ec534514be392bc5b7ec04d05a6ea6b8ec1fff72") --Mainappid Ben There, Dan That!
+addappid(37421, 1, "0cc4226a10a59b9efbe293a92f52292ef89eda3913b913a5d6f008b3b5512bf7") --Main Windows Depot Ben There, Dan That!
+setManifestid(37421, "4669888909886600883", 0)
