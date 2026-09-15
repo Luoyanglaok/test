52dfc189aebca4f942e2dbdbe02f499463852b24
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 10:50:22 +0800

    add 838350.lua

--- 838350.lua
+++ 838350.lua
@@ -20,35 +20,24 @@ setManifestid(2241120, "8929325035393384592", 3543984)
 --Dlcname Scroll Of Taiwu - 五方神龙
 addtoken(2764950, "9097792362658511678")
 addappid(2764950, 1, "c4c231a528c0f4c95a95fceae57a4530982bd307bd8f07c47070e4a7adebff0b") --Dlcname Scroll Of Taiwu - 五方神龙
-setManifestid(2764950, "6279348019110604381", 192421136)
 --Dlcname Scroll Of Taiwu - 新衣贺春
 addtoken(2764960, "10039634942498840576")
 addappid(2764960, 1, "00140a77be9fb80581f68904948537eb5ba3ca04ffc9b2c024d128d98cbf9365") --Dlcname Scroll Of Taiwu - 新衣贺春
-setManifestid(2764960, "4096172026402743863", 1540416)
---Dlcname Scroll Of Taiwu - OST 2
-addtoken(2819320, "2893316619928666294")
-addappid(2819320) --Dlcname Scroll Of Taiwu - OST 2
-addappid(2819321, 1, "cf201c9ac473365627823aa8be965fb9fdd38aa72279ba31180be819e182d994") --Dlc Windows Depot Scroll Of Taiwu - OST 2
-setManifestid(2819321, "7080424961267127743", 183282800)
---Dlcname Scroll Of Taiwu - 碧霄蛇影
+--Dlcname AppID 3464590
 addtoken(3464590, "17260488847850872888")
-addappid(3464590, 1, "34a15558cad10dbf91bbb3f82e56f56bfdce3e80ca105b90fb69296f4c94d94f") --Dlcname Scroll Of Taiwu - 碧霄蛇影
-setManifestid(3464590, "2170511017358734763", 23531136)
+addappid(3464590, 1, "34a15558cad10dbf91bbb3f82e56f56bfdce3e80ca105b90fb69296f4c94d94f") --Dlcname AppID 3464590
 --Dlcname Scroll Of Taiwu - 跃马听香
 addtoken(4395170, "6007455422738143747")
 addappid(4395170, 1, "32426e879f3458c36c7827712232c988b375f53b006c967cade6b279b82719f8") --Dlcname Scroll Of Taiwu - 跃马听香
 setManifestid(4395170, "8007211957462256197", 2345232)
 addappid(4395171, 1, "cd96a820b2c9245d21083263f9571fc227fd0fd087dd6abf6f5b740f2ce6ce47") --Dlc Windows Depot Scroll Of Taiwu - 跃马听香
 setManifestid(4395171, "6990289780772373997", 4645504)
---Dlcname Scroll Of Taiwu - 促织化影
-addappid(4528730, 1, "011923e062968acb50f9d2a992c286143858510fc261adedbc079513df981791") --Dlcname Scroll Of Taiwu - 促织化影
-setManifestid(4528730, "4456568609758365763", 62839264)
---Dlcname Scroll Of Taiwu - 八载同舟
-addappid(4834440, 1, "f8e3794187a1e35aa42731bcea4507938766a7d8a75db3ca6c73e0a0dfb83ece") --Dlcname Scroll Of Taiwu - 八载同舟
-setManifestid(4834440, "6304815620934302627", 3882080)
---Dlcname Scroll Of Taiwu - 青山依旧
-addappid(4834450, 1, "8ff4143536007a730a9eb1deb44329ab822ca0b193ce85943b230cf658929096") --Dlcname Scroll Of Taiwu - 青山依旧
-setManifestid(4834450, "9070015990007851062", 6934704)
+--Dlcname AppID 4528730
+addappid(4528730, 1, "011923e062968acb50f9d2a992c286143858510fc261adedbc079513df981791") --Dlcname AppID 4528730
+--Dlcname AppID 4834440
+addappid(4834440, 1, "f8e3794187a1e35aa42731bcea4507938766a7d8a75db3ca6c73e0a0dfb83ece") --Dlcname AppID 4834440
+--Dlcname AppID 4834450
+addappid(4834450, 1, "8ff4143536007a730a9eb1deb44329ab822ca0b193ce85943b230cf658929096") --Dlcname AppID 4834450
 --Share Depots
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228988, "6645201662696499616", 22411856)
