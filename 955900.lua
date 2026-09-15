7bbbbfa546f988c0e0d150ba827cf2e8d853f04d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 14:09:54 +0800

    add 955900.lua

--- 955900.lua
+++ 955900.lua
@@ -1,9 +1,14 @@
-addappid(955900)
-addappid(955901,0,"7a3f5a92b6422af44c29c94bc4d54cb778ee157e58873e3e7b4e3fd4f25d3956")
-setManifestid(955901,"1652931289589496880")
-addappid(955902,0,"41dddf3f2fdc9cf3c5f99e7b96ca775a8ecb996de611c526fb3e6223a9aa6944")
-setManifestid(955902,"7378292070912114805")
-addappid(1446730,0,"df4bff2f0d54a794ba24b7faa60d334cc2abaf20d7ea625138d15a1bc6ac439d")
-setManifestid(1446730,"8455975255050325821")
-addappid(1701600,0,"d358bbd50b412fd6ccd9a211041aaf9f0b8927f65d503c2026576162a2140dcc")
-setManifestid(1701600,"6746209964332676910")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 955900.lua
+--Gamename Amazing Cultivation Simulator
+addappid(955900, 1, "5424a9ed4a4e466dae7151acf58d8bf0b60a8aa54a3289af037b1e56d36be716") --Mainappid Amazing Cultivation Simulator
+addappid(955901, 1, "7a3f5a92b6422af44c29c94bc4d54cb778ee157e58873e3e7b4e3fd4f25d3956") --Main Windows Depot Amazing Cultivation Simulator
+setManifestid(955901, "1652931289589496880", 895512064)
+addappid(955902, 1, "41dddf3f2fdc9cf3c5f99e7b96ca775a8ecb996de611c526fb3e6223a9aa6944") --Main Windows Depot Amazing Cultivation Simulator
+setManifestid(955902, "7378292070912114805", 892570112)
+--Dlcname Amazing Cultivation Simulator - Deep in the bamboo Forest
+addappid(1446730, 1, "df4bff2f0d54a794ba24b7faa60d334cc2abaf20d7ea625138d15a1bc6ac439d") --Dlcname Amazing Cultivation Simulator - Deep in the bamboo Forest
+setManifestid(1446730, "8455975255050325821", 82128)
+--Dlcname Amazing Cultivation Simulator - Immortal Tales of Wudang
+addappid(1701600, 1, "d358bbd50b412fd6ccd9a211041aaf9f0b8927f65d503c2026576162a2140dcc") --Dlcname Amazing Cultivation Simulator - Immortal Tales of Wudang
+setManifestid(1701600, "6746209964332676910", 63056)
