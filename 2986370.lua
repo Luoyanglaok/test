15961a2cec20fbc460078d54816594ed6c511617
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 06:45:15 +0800

    add 2986370.lua

--- 2986370.lua
+++ 2986370.lua
@@ -1,3 +1,6 @@
-addappid(2986370)
-addappid(2986372,0,"3c33608cb0e40db3a3b700286443175881b1b14cc8fd17721e09a50c570791fc")
-setManifestid(2986372,"4843851254449298907")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2986370.lua
+--Gamename Food Delivery Simulator
+addappid(2986370) --Mainappid Food Delivery Simulator
+addappid(2986372, 1, "3c33608cb0e40db3a3b700286443175881b1b14cc8fd17721e09a50c570791fc") --Main Windows Depot Food Delivery Simulator
+setManifestid(2986372, "4699285840423029472", 950873184)
