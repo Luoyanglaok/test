aa883809fba4581373cf1969329e0cee93a16b30
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 15:27:50 +0800

    add 45760.lua

--- 45760.lua
+++ 45760.lua
@@ -1,15 +1,59 @@
-addappid(45760)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(45761,0,"57e09e51340949bac89d9a3b1be1274426a0a8c44c04288e78aeee7cebd1afdc")
-setManifestid(45761,"7494827315436345520")
-addappid(45762,0,"0f3e423d8ebb2d9c2d875bb9d44f676e13cee2feea0f61448af5778f3f27bf90")
-setManifestid(45762,"9108480795496328509")
-addappid(301660,0,"6bf7af0f3e6c1d6f19ce09adc0fcae00b25d17bc42ad7d30cb0ae095a6e5a89f")
-setManifestid(301660,"1238152240090815273")
-addappid(45763,0,"021020f02f3e53dfb27c215dd29ba45e93912dd1a18c10352f10fbe7c11cbeb4")
-setManifestid(45763,"5759861264066331637")
-addappid(45764,0,"ea6267f4b30c43759f65516977082415e1ad11bf6c0eaa71b513070efc06bbb9")
-setManifestid(45764,"8286823315541266651")
-addappid(45765,0,"ac6275726b29b5307f9c08e9e09aadeab062553c74ecd91405f6fd4f881b34e7")
-setManifestid(45765,"2973503131075698863")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 45760.lua
+--Gamename Ultra Street Fighter® IV
+addappid(45760, 1, "a4840274b161541eae9ac6ca65090a696a3c4a0b99b6bc6b81ed3b47c801fadd") --Mainappid Ultra Street Fighter® IV
+addappid(45761, 1, "57e09e51340949bac89d9a3b1be1274426a0a8c44c04288e78aeee7cebd1afdc") --Main Windows Depot Ultra Street Fighter® IV
+setManifestid(45761, "7494827315436345520", 6962358240)
+addappid(45762, 1, "0f3e423d8ebb2d9c2d875bb9d44f676e13cee2feea0f61448af5778f3f27bf90") --Main Windows Depot Ultra Street Fighter® IV
+setManifestid(45762, "9108480795496328509", 126760800)
+addappid(45763, 1, "021020f02f3e53dfb27c215dd29ba45e93912dd1a18c10352f10fbe7c11cbeb4") --Main Windows Depot Ultra Street Fighter® IV
+setManifestid(45763, "5759861264066331637", 565636016)
+addappid(45764, 1, "ea6267f4b30c43759f65516977082415e1ad11bf6c0eaa71b513070efc06bbb9") --Main Windows Depot Ultra Street Fighter® IV
+setManifestid(45764, "8286823315541266651", 594989920)
+addappid(45765, 1, "ac6275726b29b5307f9c08e9e09aadeab062553c74ecd91405f6fd4f881b34e7") --Main Windows Depot Ultra Street Fighter® IV
+setManifestid(45765, "2973503131075698863", 491451280)
+--Dlcname Ultra SFIV Digital Upgrade
+addappid(301660, 1, "6bf7af0f3e6c1d6f19ce09adc0fcae00b25d17bc42ad7d30cb0ae095a6e5a89f") --Dlcname Ultra SFIV Digital Upgrade
+setManifestid(301660, "1238152240090815273", 6378293658070)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(45790) --Dlcname USFIV: Complete Brawler Pack (2011)
+addappid(45791) --Dlcname USFIV: Complete Femme Fatale Pack (2011)
+addappid(45792) --Dlcname USFIV: Complete Shadoloo Pack (2011)
+addappid(45793) --Dlcname USFIV: Complete Shoryuken Pack (2011)
+addappid(45794) --Dlcname USFIV: Complete Classic Pack (2011)
+addappid(45795) --Dlcname USFIV: Complete Challengers 1 Pack (2011)
+addappid(45796) --Dlcname USFIV: Complete Challengers 2 Pack (2011)
+addappid(45797) --Dlcname USFIV: Arcade Challengers Pack (2011)
+addappid(45798) --Dlcname USFIV: All-in 2011 Costume Pack
+addappid(306610) --Dlcname USFIV: Fantasy 2014 Challengers Pack
+addappid(308261) --Dlcname USFIV: Shoryuken Vacation Pack
+addappid(308262) --Dlcname USFIV: Brawler Vacation Pack
+addappid(308263) --Dlcname USFIV: Shadaloo Vacation Pack
+addappid(308264) --Dlcname USFIV: Classic Vacation Pack
+addappid(308265) --Dlcname USFIV: Femme Fatale Vacation Pack
+addappid(308266) --Dlcname USFIV: Challengers Vacation Pack 1
+addappid(308267) --Dlcname USFIV: Challengers Vacation Pack 2
+addappid(308268) --Dlcname USFIV: Arcade Challengers Vacation Pack
+addappid(308269) --Dlcname USFIV: 2014 Challengers Vacation Pack
+addappid(331350) --Dlcname USFIV: Shoryuken Wild Pack
+addappid(331351) --Dlcname USFIV: Brawler Wild Pack
+addappid(331352) --Dlcname USFIV: Shadaloo Wild Pack
+addappid(331353) --Dlcname USFIV: Classic Wild Pack
+addappid(331354) --Dlcname USFIV: Femme Fatale Wild Pack
+addappid(331355) --Dlcname USFIV: Challengers Wild Pack 1
+addappid(331356) --Dlcname USFIV: Challengers Wild Pack 2
+addappid(331357) --Dlcname USFIV: Arcade Challengers Wild Pack
+addappid(331358) --Dlcname USFIV: 2014 Challengers Wild Pack
+addappid(344170) --Dlcname USFIV: Shoryuken Horror Pack
+addappid(344171) --Dlcname USFIV: Brawler Horror Pack
+addappid(344172) --Dlcname USFIV: Shadaloo Horror Pack
+addappid(344173) --Dlcname USFIV: Classic Horror Pack
+addappid(344174) --Dlcname USFIV: Femme Fatale Horror Pack
+addappid(344175) --Dlcname USFIV: Challengers Horror Pack 1
+addappid(344176) --Dlcname USFIV: Challengers Horror Pack 2
+addappid(344177) --Dlcname USFIV: Arcade Challengers Horror Pack
+addappid(344178) --Dlcname USFIV: 2014 Challengers Horror Pack
