4de69da869a3055f77c0c67707ec2db0e380526d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 00:24:21 +0800

    add 1040200.lua

--- 1040200.lua
+++ 1040200.lua
@@ -1,3 +1,12 @@
-addappid(1040200)
-addappid(1040201,0,"c674332af23f9b6fcc58092e93db43d62fe38a9b004985697e490bde97b45548")
-setManifestid(1040201,"1126666393377343775")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1040200.lua
+--Gamename Crime Scene Cleaner
+addappid(1040200, 1, "3651c051a4a7c98a872929fc9e2994273629bf6724ec207f61fd933dc702bd01") --Mainappid Crime Scene Cleaner
+addappid(1040201, 1, "c674332af23f9b6fcc58092e93db43d62fe38a9b004985697e490bde97b45548") --Main Windows Depot Crime Scene Cleaner
+setManifestid(1040201, "902898025752593275", 24070730048)
+--Dlcname Crime Scene Cleaner Soundtrack
+addappid(3153150) --Dlcname Crime Scene Cleaner Soundtrack
+addappid(3153151, 1, "d9b4073e24a34384ace2e0fec178772df457447a705fcc29d4cfe5cfd084ed11") --Dlc Windows Depot Crime Scene Cleaner Soundtrack
+setManifestid(3153151, "9162474647431108499", 1314857536)
+--Missing Dlcs
+--Dlcname Crime Scene Cleaner - Biohazard
