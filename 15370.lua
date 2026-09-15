04ceaa7a3fd18c0b00bdab8ecb193fd5ce63b318
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 14:27:59 +0800

    add 15370.lua

--- 15370.lua
+++ 15370.lua
@@ -1,19 +1,29 @@
-addappid(15370)
-setManifestid(228989,"3514306556860204959")
-setManifestid(229000,"4622705914179893434")
-addappid(15371,0,"e3718fbe2f8f98351eb260d4ea5d487e7f82ab1ec921d25ed9651c41d9991306")
-setManifestid(15371,"4769518222776312223")
-addappid(15372,0,"222a52489d9bd068bea12e1e737b9f118b0992c0d0e3b935796e3bac2bb09062")
-setManifestid(15372,"7584043978334127484")
-addappid(15373,0,"9f3e2054ab84775aa115213cb622fa37ce87a43b8920d334ce9a40b5331a9376")
-setManifestid(15373,"6276213676925816794")
-addappid(15374,0,"04dee008a9fa6ad531dca377c526947d3bba103cd3ea6d7d940a36ea27ac22a2")
-setManifestid(15374,"4103556589775682415")
-addappid(15375,0,"6327bfffbe1b62aa214bdc961baaef2dab35b45ff9c2bc2f78e306e3a5a106db")
-setManifestid(15375,"1217706121366728914")
-addappid(15376,0,"f21b96ff47e5ef90d6fc2a02030711ba6d30b19d2dce7a531f0a97eb12c0a33e")
-setManifestid(15376,"1394219716505974599")
-addappid(15378,0,"7d769e13ba4ae1654f4ab1f2828b934330a26043b44783db4da71ed1f3124b91")
-setManifestid(15378,"3623150917510837027")
-addappid(3833491,0,"663c7ff25649e9cb6bf7a64f0940b27c7367de86101058d56cb060eb68649328")
-setManifestid(3833491,"7004690534106949914")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 15370.lua
+--Gamename Heroes of Might & Magic V: Tribes of the East
+addappid(15370) --Mainappid Heroes of Might & Magic V: Tribes of the East
+addappid(15371, 1, "e3718fbe2f8f98351eb260d4ea5d487e7f82ab1ec921d25ed9651c41d9991306") --Main Windows Depot Heroes of Might & Magic V: Tribes of the East
+setManifestid(15371, "4769518222776312223", 2000733968)
+addappid(15372, 1, "222a52489d9bd068bea12e1e737b9f118b0992c0d0e3b935796e3bac2bb09062") --Main Windows Language Depot English Heroes of Might & Magic V: Tribes of the East
+setManifestid(15372, "7584043978334127484", 0)
+addappid(15373, 1, "9f3e2054ab84775aa115213cb622fa37ce87a43b8920d334ce9a40b5331a9376") --Main Windows Language Depot French Heroes of Might & Magic V: Tribes of the East
+setManifestid(15373, "6276213676925816794", 0)
+addappid(15374, 1, "04dee008a9fa6ad531dca377c526947d3bba103cd3ea6d7d940a36ea27ac22a2") --Main Windows Language Depot German Heroes of Might & Magic V: Tribes of the East
+setManifestid(15374, "4103556589775682415", 0)
+addappid(15375, 1, "6327bfffbe1b62aa214bdc961baaef2dab35b45ff9c2bc2f78e306e3a5a106db") --Main Windows Language Depot Italian Heroes of Might & Magic V: Tribes of the East
+setManifestid(15375, "1217706121366728914", 0)
+addappid(15376, 1, "f21b96ff47e5ef90d6fc2a02030711ba6d30b19d2dce7a531f0a97eb12c0a33e") --Main Windows Language Depot Spanish Heroes of Might & Magic V: Tribes of the East
+setManifestid(15376, "1394219716505974599", 0)
+addappid(15377, 1, "d39c4ed08652d5e3bc6a70acb5a90ab028b0b800ba76dfb673d492263cde0e81") --Main Windows Language Depot Polish Heroes of Might & Magic V: Tribes of the East
+setManifestid(15377, "7681165509062379757", 2358495088)
+addappid(15378, 1, "7d769e13ba4ae1654f4ab1f2828b934330a26043b44783db4da71ed1f3124b91") --Main Windows Language Depot Russian Heroes of Might & Magic V: Tribes of the East
+setManifestid(15378, "3623150917510837027", 1831542704)
+addappid(15379, 1, "a356e9ba1874fa6e90d04ef27ba92fb1c4df0a36c883c894507da1c714a08d75") --Main Windows Language Depot Czech Heroes of Might & Magic V: Tribes of the East
+setManifestid(15379, "6933323992978238353", 2275780176)
+addappid(3833491, 1, "663c7ff25649e9cb6bf7a64f0940b27c7367de86101058d56cb060eb68649328") --Main Windows Language Depot Schinese Heroes of Might & Magic V: Tribes of the East
+setManifestid(3833491, "7004690534106949914", 1739193504)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
