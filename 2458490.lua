7c07896bcd3200e23459465a24bd10c3e2d26627
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 15:44:25 +0800

    add 2458490.lua

--- 2458490.lua
+++ 2458490.lua
@@ -1,9 +1,14 @@
-addappid(2458490)
-addappid(2458491,0,"27d3d0f1c3e000bba5c84d8fd3c883ee148ec06e067b1d9f9559065ccfb4e529")
-setManifestid(2458491,"7104473547932207080")
-addappid(2458492,0,"3a2ea55a1af4cc2aab188650c6b52a6f7316f42f05127c63838117e5b4598dbe")
-setManifestid(2458492,"9174688445659347098")
-addappid(2458493,0,"aad3f74c786de6ea901bc9e82b20051c0497f8110f4824bdaef44ddebea47ccb")
-setManifestid(2458493,"7595205409312410584")
-addappid(2458494,0,"cc9e5a7872d6cf8739971db347550e95e648899997811ac053cf255d3b2b9e2f")
-setManifestid(2458494,"8450722921082857914")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2458490.lua
+--Gamename Fallen Shinobi SS
+addappid(2458490, 1, "72a5977f498be05db347a2f03c24397a54f78250efe64577f304c8eccf6105a7") --Mainappid Fallen Shinobi SS
+addappid(2458491, 1, "27d3d0f1c3e000bba5c84d8fd3c883ee148ec06e067b1d9f9559065ccfb4e529") --Main Windows Language Depot Japanese Fallen Shinobi SS
+setManifestid(2458491, "7104473547932207080", 1251087392)
+addappid(2458492, 1, "3a2ea55a1af4cc2aab188650c6b52a6f7316f42f05127c63838117e5b4598dbe") --Main Windows Language Depot English Fallen Shinobi SS
+setManifestid(2458492, "9174688445659347098", 1293693904)
+addappid(2458493, 1, "aad3f74c786de6ea901bc9e82b20051c0497f8110f4824bdaef44ddebea47ccb") --Main Windows Language Depot Tchinese Fallen Shinobi SS
+setManifestid(2458493, "7595205409312410584", 1442488592)
+addappid(2458494, 1, "cc9e5a7872d6cf8739971db347550e95e648899997811ac053cf255d3b2b9e2f") --Main Windows Language Depot Schinese Fallen Shinobi SS
+setManifestid(2458494, "8450722921082857914", 1408671632)
+addappid(2458495, 1, "29B8306874EFB069702D4E75D2C5F16E1D16091A3C1CC6B0049F23F95B33B249") --Main Windows Language Depot Koreana Fallen Shinobi SS
+setManifestid(2458495, "6392856804584778592", 1332082976)
