5e425fa3deb7c7ef957db023ccd0c31c65f87d69
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 15:11:56 +0800

    add 1150090.lua

--- 1150090.lua
+++ 1150090.lua
@@ -1,9 +1,21 @@
-addappid(1150090)
-addappid(1150091,0,"665c97034c7ae21a81c9c82e352ed9ff3c7e70626ed7afe9233db0bbed1cd939")
-setManifestid(1150091,"7190344543645704828")
-addappid(1150093,0,"cba65e460e5550e33f89b1becb668701951d6782c4af8ac19d7457030344c294")
-setManifestid(1150093,"7274068230425681312")
-addappid(1150094,0,"81dbda729176b415bd1b74f076ab2fab5490bc35377a56abdf34d05fd7a69384")
-setManifestid(1150094,"630835813530253190")
-addappid(1545770,0,"b1ca321815b0580c756f28ee5ddf0a4386ad33c6f969d59b33b39a43b30fff83")
-setManifestid(1545770,"8025577116302565116")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1150090.lua
+--Gamename Learning Factory
+addappid(1150090, 1, "114295aad80c3a7a9dfef2ce887278ff5d33a6498c2e0342e59280dccf5c5c94") --Mainappid Learning Factory
+addappid(1150091, 1, "665c97034c7ae21a81c9c82e352ed9ff3c7e70626ed7afe9233db0bbed1cd939") --Main Windows Depot Learning Factory
+setManifestid(1150091, "7190344543645704828", 934720528)
+addappid(1150093, 1, "cba65e460e5550e33f89b1becb668701951d6782c4af8ac19d7457030344c294") --Main Macos Depot Learning Factory
+setManifestid(1150093, "7274068230425681312", 940409072)
+addappid(1150094, 1, "81dbda729176b415bd1b74f076ab2fab5490bc35377a56abdf34d05fd7a69384") --Main Linux Depot Learning Factory
+setManifestid(1150094, "630835813530253190", 934022128)
+--Dlcname Learning Factory Soundtrack
+addappid(1545760) --Dlcname Learning Factory Soundtrack
+addappid(1545761, 1, "4d9761626b10e53bb303e503f92fbe3621a902037be9973dcc1e8b8f77789d6c") --Dlc Windows Depot Learning Factory Soundtrack
+setManifestid(1545761, "6873097910497696693", 151924624)
+addappid(1545762, 1, "98b9362c2019882d195709975df1c3111720681464898a1aa9a6f4459642e119") --Dlc Windows Depot Learning Factory Soundtrack
+setManifestid(1545762, "2614516373608449129", 274459552)
+--Dlcname Learning Factory Art Pack
+addappid(1545770, 1, "b1ca321815b0580c756f28ee5ddf0a4386ad33c6f969d59b33b39a43b30fff83") --Dlcname Learning Factory Art Pack
+setManifestid(1545770, "8025577116302565116", 208467168)
+--Missing Dlcs
+--Dlcname Learning Factory Support the Developers & Poster Pack
