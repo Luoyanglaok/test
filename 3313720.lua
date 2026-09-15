13127b654eadd11ba3e3823ea4c58c6787ece5c2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 11:22:15 +0800

    add 3313720.lua

---
+++ 3313720.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3313720.lua
+--Gamename Psychic Kung Fu Master
+addappid(3313720) --Mainappid Psychic Kung Fu Master
+addappid(3313721, 1, "ee11efd6a3f4b69b34662632aa725443ff59d29d7c5f5b17f2c02414af66623b") --Main Windows Depot Psychic Kung Fu Master
+setManifestid(3313721, "5652444297579413506", 7582682992)
+addappid(4599150) --Dlcname 今古群侠传 - 支持者包
