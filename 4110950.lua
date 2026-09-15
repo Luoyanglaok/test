e1edefefdfd7229363005cc6cfbbd35ba596b382
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 17:32:34 +0800

    add 4110950.lua

---
+++ 4110950.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4110950.lua
+--Gamename Split Fiction - Original Soundtrack
+addappid(4110950) --Mainappid Split Fiction - Original Soundtrack
+addappid(4110951, 1, "0359f50b5814950537d5fb52d353c6262fad8e41c720103435de9208b14d7949") --Main Windows Depot Split Fiction - Original Soundtrack
+setManifestid(4110951, "1497142047146784507", 342161616)
+addappid(4110952, 1, "0db626922af091f02ab545eb507f259869dae6a4787c5a5c0681a20c79e16eec") --Main Windows Depot Split Fiction - Original Soundtrack
+setManifestid(4110952, "2424117993464905013", 1086362912)
