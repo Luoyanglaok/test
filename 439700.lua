6fced33c0e52e536c6cecf7bfce648f69d490801
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 19:32:57 +0800

    add 439700.lua

--- 439700.lua
+++ 439700.lua
@@ -1,7 +1,11 @@
-addappid(439700)
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(439701,0,"5467907d5be42494fae9aaaab51d70528d517c130c1d18df5be75274b5244378")
-setManifestid(439701,"9203940625127963753")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 439700.lua
+--Gamename Z1 Battle Royale: Test Server
+addappid(439700) --Mainappid Z1 Battle Royale: Test Server
+addappid(439701, 1, "5467907d5be42494fae9aaaab51d70528d517c130c1d18df5be75274b5244378") --Main Windows Depot Z1 Battle Royale: Test Server
+setManifestid(439701, "9203940625127963753", 16304177376)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
