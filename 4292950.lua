bf0a6576fb8e7d896a266a568ad8697d2b7079ae
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-15 10:13:05 +0800

    add 4292950.lua

---
+++ 4292950.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4292950.lua
+--Gamename Shao Song I
+addappid(4292950) --Mainappid Shao Song I
+addappid(4292951, 1, "d67ee3c314a005c060fc2616ff7d3787bd366d0b33bcc961bb42eab401a4bf8a") --Main Windows Depot Shao Song I
+setManifestid(4292951, "1391489508651968034", 40821877488)
