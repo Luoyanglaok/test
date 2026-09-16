e7d843449275371ae9bd159465cb883e0dde31c2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 00:44:00 +0800

    add 10150.lua

--- 10150.lua
+++ 10150.lua
@@ -1,9 +1,12 @@
-addappid(10150)
-addappid(10151,0,"6a8031835dc05293f181f82838888b8125309337318239b1ddbdb09f0a4caa77")
-setManifestid(10151,"117487646560570439")
-addappid(10152,0,"fe92b92ecf2f65762c8fa0946ed504a468250e5d5bba72d8c22d82dbf4837fd9")
-setManifestid(10152,"2419202093937763642")
-addappid(10153,0,"2d78a62123ebaeaa32c518637004173360e4c771d95d477c473cc15ce4c5d40d")
-setManifestid(10153,"4118699407910273842")
-addappid(10154,0,"18966e09d0c0543581c6fdc1623d6e5666a410dd1a8005eea750d7414e54204a")
-setManifestid(10154,"7389168689420377684")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10150.lua
+--Gamename Prototype™
+addappid(10150, 1, "bb5616d06e04e5b9da902b702242e658563712aef958a634ebbfe19bacabc6d0") --Mainappid Prototype™
+addappid(10151, 1, "6a8031835dc05293f181f82838888b8125309337318239b1ddbdb09f0a4caa77") --Main Windows Depot Prototype™
+setManifestid(10151, "2961055382649058691", 7867635312)
+addappid(10152, 1, "fe92b92ecf2f65762c8fa0946ed504a468250e5d5bba72d8c22d82dbf4837fd9") --Main Windows Language Depot French Prototype™
+setManifestid(10152, "1688785474723655723", 39248)
+addappid(10153, 1, "2d78a62123ebaeaa32c518637004173360e4c771d95d477c473cc15ce4c5d40d") --Main Windows Language Depot Spanish Prototype™
+setManifestid(10153, "1808095436463938303", 39984)
+addappid(10154, 1, "18966e09d0c0543581c6fdc1623d6e5666a410dd1a8005eea750d7414e54204a") --Main Windows Language Depot Italian Prototype™
+setManifestid(10154, "3364852067550674408", 39088)
