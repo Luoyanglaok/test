ad695b0ef2d0e9ea6bdbb0e2686cf635857941a5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 07:22:06 +0800

    add 2174500.lua

--- 2174500.lua
+++ 2174500.lua
@@ -1,3 +1,13 @@
-addappid(2174500)
-addappid(2174503,0,"4f30c557ea6b487f1f13027e1651b44b80169d0540804cf3ba9a6b9dcf59ba0f")
-setManifestid(2174503,"7722247529501339672")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2174500.lua
+--Gamename Parasite Black
+addappid(2174500, 1, "3d3c468da08f5c7d6388f1561a82236ba71dd1963bfc0c86b49e7f401317fdc1") --Mainappid Parasite Black
+addappid(2174503, 1, "4f30c557ea6b487f1f13027e1651b44b80169d0540804cf3ba9a6b9dcf59ba0f") --Main Windows Depot Parasite Black
+setManifestid(2174503, "2481012342489480782", 2204016224)
+--Dlcname Parasite Black - Premium Supporter Pack + Cheats
+addappid(3404970) --Dlcname Parasite Black - Premium Supporter Pack + Cheats
+addappid(3404971, 1, "344300db177c01cfd35b157cadc7d6e8c3ffad27d8ba0e962b5f62a2add040c4") --Dlc Windows Depot Parasite Black - Premium Supporter Pack + Cheats
+setManifestid(3404971, "380120784977558345", 164269344)
+addappid(4636510) --Dlcname Parasite Black - Grim Days Manga Series
+--Missing Dlcs
+--Dlcname Parasite Black Soundtrack
