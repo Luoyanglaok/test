7d8299b00bba7380197a701ee92826dd57949165
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:45:55 +0800

    add 1274570.lua

--- 1274570.lua
+++ 1274570.lua
@@ -1,8 +1,20 @@
-addappid(1274570)
-setManifestid(228988,"6645201662696499616")
-addappid(1274571,0,"b56991abb013052ba7ef6d1e0982f0ca99bbeeb631057347267102c6967c1537")
-setManifestid(1274571,"8592548184546986290")
-addappid(1274573,0,"b67b5c5c44139818a0d7ddb9d1103a724ca404a87bde4d78fe257fc352f0f36d")
-setManifestid(1274573,"7461564844819776433")
-addappid(1966630,0,"9b579f914a2ba495ad1e0b1e1a1fc21c8e578ee933fb6bfabd9f138124244587")
-setManifestid(1966630,"3546735088053658697")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1274570.lua
+--Gamename DEVOUR
+addappid(1274570, 1, "2c8793d85b560d8e445d68641b5b0836e2dd95ef6dc2872eb25eb4ad9c42248a") --Mainappid DEVOUR
+addappid(1274571, 1, "b56991abb013052ba7ef6d1e0982f0ca99bbeeb631057347267102c6967c1537") --Main Windows Depot DEVOUR
+setManifestid(1274571, "2850603824927654821", 8609714768)
+addappid(1274573, 1, "b67b5c5c44139818a0d7ddb9d1103a724ca404a87bde4d78fe257fc352f0f36d") --Main Macos Depot DEVOUR
+setManifestid(1274573, "7186629756615425564", 8680307568)
+--Dlcname DEVOUR: Supporter Edition
+addappid(1966630, 1, "9b579f914a2ba495ad1e0b1e1a1fc21c8e578ee933fb6bfabd9f138124244587") --Dlcname DEVOUR: Supporter Edition
+setManifestid(1966630, "7348617984918166394", 92388272)
+--Dlcname DEVOUR: Soundtrack
+addappid(1977260) --Dlcname DEVOUR: Soundtrack
+addappid(1977261, 1, "b8fe2b413baf1a2f70d13eb0189a16a2113ca9b11dcad56196a5ee8f618c81ac") --Dlc Windows Depot DEVOUR: Soundtrack
+setManifestid(1977261, "3996702687201802107", 231874704)
+addappid(1977262, 1, "a94dd08266eee983f548031945f935947eb7b6148f4af8167ab67e2993aceb0b") --Dlc Windows Depot DEVOUR: Soundtrack
+setManifestid(1977262, "3321894516755209546", 1812635872)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
