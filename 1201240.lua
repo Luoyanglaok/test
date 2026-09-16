951df04072a3cc6bbafcf2a48bf4c76f65911f78
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:57:28 +0800

    add 1201240.lua

--- 1201240.lua
+++ 1201240.lua
@@ -1,11 +1,23 @@
-addappid(1201240)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228987,"4302102680580581867")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229004,"5220958916987797232")
-setManifestid(229005,"7992454656023763365")
-setManifestid(229006,"1784011429307107530")
-addappid(1201241,0,"85566eaa31209ac31dde217a058f58e810356a8df255c9a7c4c8b31749858248")
-setManifestid(1201241,"1738679832283399314")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1201240.lua
+--Gamename BLEACH Brave Souls
+addappid(1201240, 1, "5b888301762e2550573c0f183895f2a40e847bb88dfafed7dcc247dffadf0717") --Mainappid BLEACH Brave Souls
+addappid(1201241, 1, "85566eaa31209ac31dde217a058f58e810356a8df255c9a7c4c8b31749858248") --Main Windows Depot BLEACH Brave Souls
+setManifestid(1201241, "5698208772793357873", 151941680)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
