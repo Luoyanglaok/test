164444a83c14dd05236f30125ebadb052c1f9117
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 16:44:44 +0800

    add 4226370.lua

---
+++ 4226370.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4226370.lua
+--Gamename 我的文字修仙世界
+addappid(4226370, 1, "d9a47b2352cd6790cfad6f05e2f96a5b645016014970db618ebf4021098d2d19") --Mainappid 我的文字修仙世界
+addappid(4226371, 1, "97796a43acebbedb471896bbe624e66f243be2f85d9d99c4f5ebf4a46d539918") --Main Windows Depot 我的文字修仙世界
+setManifestid(4226371, "1018058239352232558", 23453472)
