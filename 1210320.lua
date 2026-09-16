b494e6400a994c83b0b76075a4e45aa785edbe69
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 22:28:12 +0800

    add 1210320.lua

--- 1210320.lua
+++ 1210320.lua
@@ -1,3 +1,6 @@
-addappid(1210320)
-addappid(1210321,0,"d8767749a4bd55e51cd4cc1c210399bbbcb0e728f7caab6cdcdabb84ed18d158")
-setManifestid(1210321,"7634710488886089021")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1210320.lua
+--Gamename Potion Craft: Alchemist Simulator
+addappid(1210320, 1, "be10c61fab43d9989afeb91728041d0f9474fc64b256dd6bcb787dfdb4ed3ab1") --Mainappid Potion Craft: Alchemist Simulator
+addappid(1210321, 1, "d8767749a4bd55e51cd4cc1c210399bbbcb0e728f7caab6cdcdabb84ed18d158") --Main Windows Depot Potion Craft: Alchemist Simulator
+setManifestid(1210321, "7634710488886089021", 1213505808)
