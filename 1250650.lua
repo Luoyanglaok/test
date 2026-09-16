1a1f848cfe74bb94eb6361b563755976ceebe94f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 20:41:14 +0800

    add 1250650.lua

--- 1250650.lua
+++ 1250650.lua
@@ -1,4 +1,10 @@
-addappid(1250650)
-setManifestid(229000,"4622705914179893434")
-addappid(1250651,0,"afa5356481ba8c88343716391d1692b39ad47fc1976c224763ac75179601f367")
-setManifestid(1250651,"2234212590759699552")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1250650.lua
+--Gamename AI＊Shoujo/AI＊少女
+addappid(1250650, 1, "0b11c35594efd568903475411b856b35f4acb6a5a854ca03b63d0cafffa880dc") --Mainappid AI＊Shoujo/AI＊少女
+addappid(1250651, 1, "afa5356481ba8c88343716391d1692b39ad47fc1976c224763ac75179601f367") --Main Windows Depot AI＊Shoujo/AI＊少女
+setManifestid(1250651, "2234212590759699552", 3289166448)
+addappid(1250652, 1, "b0f60ff8a47151656fcfabb62a522d63b073db8368fddebbfd09e9c340e0df90") --Main Linux Depot AI＊Shoujo/AI＊少女
+--Share Depots
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
