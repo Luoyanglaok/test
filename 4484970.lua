d4ef938c26ea3ee3e134ec599a40bc1d0371f6e4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 22:51:56 +0800

    add 4484970.lua

---
+++ 4484970.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4484970.lua
+--Gamename Wallpaper Animator
+addappid(4484970) --Mainappid Wallpaper Animator
+addappid(4484971, 1, "3fba67e423e03a0c88e0e228b0694bd35fa03b47726dde9afb7d6f4dc88cbfb1") --Main Windows Depot Wallpaper Animator
+setManifestid(4484971, "2925981158046265628", 49620240)
+addappid(4484972, 1, "3691df76147879982e8a0b794a4d63418961dd53e4deb93a5865c37e48664e14") --Main Linux Depot Wallpaper Animator
+setManifestid(4484972, "3259108961215702799", 167522864)
