bf6ff247e670e4e60629e9c0f86193f1784efc63
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 14:44:19 +0800

    add 1057090.lua

--- 1057090.lua
+++ 1057090.lua
@@ -1,4 +1,15 @@
-addappid(1057090)
-setManifestid(228988,"6645201662696499616")
-addappid(1057091,0,"f2095fc72f59315e1315914a5a349e23488d3e48897b16bc733e6cad2ca059a1")
-setManifestid(1057091,"1069298212161557632")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1057090.lua
+--Gamename Ori and the Will of the Wisps
+addappid(1057090, 1, "1441906c390a3caf52202420ce4720bfcf215ed097fc191694eea582c4aea1f4") --Mainappid Ori and the Will of the Wisps
+addappid(1057091, 1, "f2095fc72f59315e1315914a5a349e23488d3e48897b16bc733e6cad2ca059a1") --Main Windows Depot Ori and the Will of the Wisps
+setManifestid(1057091, "1069298212161557632", 4020360912)
+--Dlcname Ori and the Will of the Wisps Soundtrack
+addappid(1258740) --Dlcname Ori and the Will of the Wisps Soundtrack
+addappid(1258741, 1, "333cc36da18ee28253c5dcedee37858e449c97d3df6e9618f20f8540712fc39d") --Dlc Windows Depot Ori and the Will of the Wisps Soundtrack
+setManifestid(1258741, "3832800547358284023", 542879584)
+addappid(1258742, 1, "e6c0fe164d33686134aa4baa7a8ae8535a7a3fdcf2da90a1d3f1f4a3df825253") --Dlc Windows Depot Ori and the Will of the Wisps Soundtrack
+setManifestid(1258742, "1750814743816009241", 2020526016)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
