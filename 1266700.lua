b7b9a2458354e4bef2810051e0f059da79d5c596
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 23:29:21 +0800

    add 1266700.lua

--- 1266700.lua
+++ 1266700.lua
@@ -1,9 +1,24 @@
-addappid(1266700)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1266701,0,"ae5c9073f56b7f287b657d279d00ea5f42df953b35a7a6da543a8f68fc51efdb")
-setManifestid(1266701,"1099288554538397849")
-addappid(1266702,0,"b1d9a25d04e79436bcdcc0a8fba11bba11e549f70ef3aa1eccc724e0342b08e8")
-setManifestid(1266702,"4649550142226521669")
-addappid(2080852,0,"e04e6d3dac96e79b745b5240c5998417fcd44e99aa82ecec82aa88854e537f1f")
-setManifestid(2080852,"5939157165078395042")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1266700.lua
+--Gamename Destroy All Humans! 2 - Reprobed
+addappid(1266700) --Mainappid Destroy All Humans! 2 - Reprobed
+addappid(1266701, 1, "ae5c9073f56b7f287b657d279d00ea5f42df953b35a7a6da543a8f68fc51efdb") --Main Windows Depot Destroy All Humans! 2 - Reprobed
+setManifestid(1266701, "9092817942251797382", 33705394192)
+addappid(1266702, 1, "b1d9a25d04e79436bcdcc0a8fba11bba11e549f70ef3aa1eccc724e0342b08e8") --Main Windows Depot Destroy All Humans! 2 - Reprobed
+setManifestid(1266702, "8288362880269411455", 0)
+--Dlcname Destroy All Humans! 2 - Reprobed: Digital Artbook
+addappid(2080852, 1, "e04e6d3dac96e79b745b5240c5998417fcd44e99aa82ecec82aa88854e537f1f") --Dlcname Destroy All Humans! 2 - Reprobed: Digital Artbook
+setManifestid(2080852, "5939157165078395042", 54133920)
+--Dlcname Destroy All Humans! 2 - Reprobed: Official Orchestral Score
+addappid(2082930) --Dlcname Destroy All Humans! 2 - Reprobed: Official Orchestral Score
+addappid(2082931, 1, "968f3de5ddf0ef634bc3da666f3dcd65c1b841662bb19f72189196b9e8bc3912") --Dlc Windows Depot Destroy All Humans! 2 - Reprobed: Official Orchestral Score
+setManifestid(2082931, "5812972070059323312", 123467408)
+addappid(2082932, 1, "80df9992ed68aae4a8511a7ffe7109fe343cf6cafde434220193a577e28924fc") --Dlc Windows Depot Destroy All Humans! 2 - Reprobed: Official Orchestral Score
+setManifestid(2082932, "4294403110007154289", 577978416)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2080850) --Dlcname Destroy All Humans! 2 - Reprobed: Skin Pack
+addappid(2080851) --Dlcname Destroy All Humans! 2 - Reprobed: Challenge Accepted DLC
