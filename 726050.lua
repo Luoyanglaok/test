5e758603aee84fc048b4705b31147a320b824620
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:55:09 +0800

    add 726050.lua

--- 726050.lua
+++ 726050.lua
@@ -1,3 +1,5 @@
-addappid(726050)
-addappid(726050,0,"3866668b1a85adef60c3470be739a60c4764d0fcbd9fd7f7813b703233f9548e")
-setManifestid(726050,"987934740586805841")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 726050.lua
+--Gamename Stick Fight: The Game OST
+addappid(726050, 1, "3866668b1a85adef60c3470be739a60c4764d0fcbd9fd7f7813b703233f9548e") --Mainappid Stick Fight: The Game OST
+setManifestid(726050, "5969593727816611135", 260589216)
