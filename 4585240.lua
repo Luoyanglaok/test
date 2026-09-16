098add568b24b8e814067bf2c5b38597c32a6d57
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:47:14 +0800

    add 4585240.lua

---
+++ 4585240.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4585240.lua
+--Gamename AppID_4585240
+addappid(4585240) --Mainappid AppID_4585240
+addappid(4585241, 1, "518f935078493c639f560c5746abbb87c99fdf618965443eeca50fec3ad305cc") --Main Windows Depot AppID_4585240
+setManifestid(4585241, "6842091866445973736", 2281498000)
