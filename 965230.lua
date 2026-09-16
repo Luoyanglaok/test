e527504977edd058d7c384b75b22ade22c2b62fa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:58:21 +0800

    add 965230.lua

--- 965230.lua
+++ 965230.lua
@@ -1,3 +1,6 @@
-addappid(965230)
-addappid(965231,0,"7991997f37bd388822b50e2b9e2984c14593fd80b32bfbc60baa0e795b037c04")
-setManifestid(965231,"5336583278541251712")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 965230.lua
+--Gamename DORAEMON STORY OF SEASONS
+addappid(965230) --Mainappid DORAEMON STORY OF SEASONS
+addappid(965231, 1, "7991997f37bd388822b50e2b9e2984c14593fd80b32bfbc60baa0e795b037c04") --Main Windows Depot DORAEMON STORY OF SEASONS
+setManifestid(965231, "3090345055232363618", 690511024)
