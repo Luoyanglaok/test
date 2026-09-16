9e817a978b93c0c15edc4143cceb5d75af3e549c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:52:50 +0800

    add 1492730.lua

--- 1492730.lua
+++ 1492730.lua
@@ -1,6 +1,16 @@
-addappid(1492730)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(1492731,0,"7a2c277cce45042f0a1d145eaf9c1d726882d47f61669b5f72ad56d991156276")
-setManifestid(1492731,"6272594670443705729")
-addappid(1492732)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1492730.lua
+--Gamename DORAEMON STORY OF SEASONS: Friends of the Great Kingdom
+addappid(1492730) --Mainappid DORAEMON STORY OF SEASONS: Friends of the Great Kingdom
+addappid(1492731, 1, "7a2c277cce45042f0a1d145eaf9c1d726882d47f61669b5f72ad56d991156276") --Main Windows Depot DORAEMON STORY OF SEASONS: Friends of the Great Kingdom
+setManifestid(1492731, "5403938009098128156", 1701434000)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(2019410) --Dlcname DORAEMON STORY OF SEASONS: Friends of the Great Kingdom Summer Wear Set
+addappid(2062250) --Dlcname DORAEMON STORY OF SEASONS: Friends of the Great Kingdom Premium Crops Seed Set
+addappid(2062251) --Dlcname DORAEMON STORY OF SEASONS: Friends of the Great Kingdom Seasonal Veggies Seeds Set
+addappid(2062252) --Dlcname DORAEMON STORY OF SEASONS: Friends of the Great Kingdom - Winter Tales
+addappid(2062253) --Dlcname DORAEMON STORY OF SEASONS: Friends of the Great Kingdom - The Life of Insects
+addappid(2062254) --Dlcname DORAEMON STORY OF SEASONS: Friends of the Great Kingdom - Together with Animals
+addappid(2062255) --Dlcname DORAEMON STORY OF SEASONS: Friends of the Great Kingdom Season Pass
