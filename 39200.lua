ae5b42386e205c2ebc8f6e63447393d627f4c281
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 16:54:08 +0800

    add 39200.lua

--- 39200.lua
+++ 39200.lua
@@ -1,14 +1,19 @@
-addappid(39200)
-setManifestid(228990,"1829726630299308803")
-addappid(39201,0,"4b6f829d6048e6ea844a5b920b83db19bfc6e736eee9ee0c20500629807ce02d")
-setManifestid(39201,"135602134324201647")
-addappid(39202,0,"1bb2b4828efede9e560539a31c1642bfb59db0729caec84f23685556e14b2325")
-setManifestid(39202,"8579449328975197911")
-addappid(39203,0,"05bbf4f6a2638daaf863031a70045ecacaeb160192a774a433ed7e0f2f2861a6")
-setManifestid(39203,"7815057877649358143")
-addappid(39204,0,"82066a0f0e7791d84d16b6a134ab64960a7ce849da42a227b5b9c7f1ad684fb5")
-setManifestid(39204,"2132330666660005180")
-addappid(39205,0,"d0790852ef8280ec87368944153865509edf673b23212e49ad5e3b012330974e")
-setManifestid(39205,"1822593502552445737")
-addappid(39206,0,"a1fc7750db11837f6fe54dda903b3ccb166346f63aa8ed64f191e9578f234743")
-setManifestid(39206,"4721806995932406306")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 39200.lua
+--Gamename Dungeon Siege II
+addappid(39200) --Mainappid Dungeon Siege II
+addappid(39201, 1, "4b6f829d6048e6ea844a5b920b83db19bfc6e736eee9ee0c20500629807ce02d") --Main Windows Depot Dungeon Siege II
+setManifestid(39201, "135602134324201647", 0)
+addappid(39202, 1, "1bb2b4828efede9e560539a31c1642bfb59db0729caec84f23685556e14b2325") --Main Windows Language Depot French Dungeon Siege II
+setManifestid(39202, "8579449328975197911", 0)
+addappid(39203, 1, "05bbf4f6a2638daaf863031a70045ecacaeb160192a774a433ed7e0f2f2861a6") --Main Windows Language Depot German Dungeon Siege II
+setManifestid(39203, "7815057877649358143", 0)
+addappid(39204, 1, "82066a0f0e7791d84d16b6a134ab64960a7ce849da42a227b5b9c7f1ad684fb5") --Main Windows Language Depot Spanish Dungeon Siege II
+setManifestid(39204, "2132330666660005180", 0)
+addappid(39205, 1, "d0790852ef8280ec87368944153865509edf673b23212e49ad5e3b012330974e") --Main Windows Language Depot Italian Dungeon Siege II
+setManifestid(39205, "1822593502552445737", 0)
+addappid(39206, 1, "a1fc7750db11837f6fe54dda903b3ccb166346f63aa8ed64f191e9578f234743") --Main Windows Language Depot Japanese Dungeon Siege II
+setManifestid(39206, "4721806995932406306", 0)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
