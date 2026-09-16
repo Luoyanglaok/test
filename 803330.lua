94f45ae4312ae394e56c0036d1a173a2aabbdfd0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 23:27:33 +0800

    add 803330.lua

--- 803330.lua
+++ 803330.lua
@@ -1,9 +1,14 @@
-addappid(803330)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(803331,0,"8548ae2dc9ed3193e4ebb386ea566523714b8ca47ab85919c5b0d58b8aa94c02")
-setManifestid(803331,"162929606556726402")
-addappid(803332,0,"69762cce364c47101a23c7cc4fe695d0f3e73acf90abb0a8d4891a52bfa596e0")
-setManifestid(803332,"4386657334275739391")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 803330.lua
+--Gamename Destroy All Humans!
+addappid(803330) --Mainappid Destroy All Humans!
+addappid(803331, 1, "8548ae2dc9ed3193e4ebb386ea566523714b8ca47ab85919c5b0d58b8aa94c02") --Main Windows Depot Destroy All Humans!
+setManifestid(803331, "162929606556726402", 8206772288)
+addappid(803332, 1, "69762cce364c47101a23c7cc4fe695d0f3e73acf90abb0a8d4891a52bfa596e0") --Main Windows Depot Destroy All Humans!
+setManifestid(803332, "4386657334275739391", 7966396320)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1254570) --Dlcname Destroy All Humans! Skin Pack
