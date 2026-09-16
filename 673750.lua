7b28888e1b5246f7e8c0fe24c783ac76bab3f9eb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:50:43 +0800

    add 673750.lua

--- 673750.lua
+++ 673750.lua
@@ -1,7 +1,10 @@
-addappid(673750)
-addappid(673751,0,"ef8e2faa6ce07ab491309abf7236035899b0ffacebb9e67c94c43ef39e9db256")
-setManifestid(673751,"7877590470736542532")
-addappid(673752,0,"ce41cc79d5435eace9ea012608624c9011832aa19b1177636544a8159ddefcf4")
-setManifestid(673752,"6841300154909663411")
-addappid(673753,0,"69b83c4681d49f60187dec02af5734fdfe3f5f0d2bd446662abde2a607cf214e")
-setManifestid(673753,"2406391090775499135")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 673750.lua
+--Gamename Super Bunny Man
+addappid(673750, 1, "9086f1b2ada4e1b684662546b196aad2dafe70705b52a1151c840c002e33ee59") --Mainappid Super Bunny Man
+addappid(673751, 1, "ef8e2faa6ce07ab491309abf7236035899b0ffacebb9e67c94c43ef39e9db256") --Main Windows Depot Super Bunny Man
+setManifestid(673751, "5757779581101073559", 211725712)
+addappid(673752, 1, "ce41cc79d5435eace9ea012608624c9011832aa19b1177636544a8159ddefcf4") --Main Macos Depot Super Bunny Man
+setManifestid(673752, "6868561638579814382", 221002464)
+addappid(673753, 1, "69b83c4681d49f60187dec02af5734fdfe3f5f0d2bd446662abde2a607cf214e") --Main Linux Depot Super Bunny Man
+setManifestid(673753, "8963798596294226565", 210180896)
