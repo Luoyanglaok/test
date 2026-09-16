5b464be906f0e8d74a11b5e401d702a90fdcf66e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 11:57:55 +0800

    add 3812600.lua

--- 3812600.lua
+++ 3812600.lua
@@ -1,4 +1,13 @@
-addappid(3812600)
-addappid(5020360)
-addappid(5034500)
-setManifestid(3812601,"654423896192937661")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3812600.lua
+--Gamename ReStory: Chill Electronics Repairs
+addappid(3812600, 1, "28dcae12b1fc1cc8730d44073f4378f9ae73e32de2639581a693dd0bfcd42163") --Mainappid ReStory: Chill Electronics Repairs
+addappid(3812601, 1, "5f6abd59a61b1a803b5a34eef196a26cd035da18f255212a544d5158dbfc626e") --Main Windows/Linux Depot ReStory: Chill Electronics Repairs
+setManifestid(3812601, "654423896192937661", 623962928)
+addappid(3812602, 1, "6fcf1eb039f557a73d5ed9b5fc840b753c184899a3e48ef44d98bf4bc423c31f") --Main Macos Depot ReStory: Chill Electronics Repairs
+setManifestid(3812602, "6341355155354872355", 652345616)
+--Dlcname Restory: Chill Electronics Repairs - Digital Artbook
+addappid(5034500, 1, "7b50eb279041c0949418aece25a74fc3c349e149ad3cfdfe093f5ea2c6954065") --Dlcname Restory: Chill Electronics Repairs - Digital Artbook
+setManifestid(5034500, "4573138219617442396", 50167840)
+--Missing Dlcs
+--Dlcname ReStory: Chill Electronics Repairs Soundtrack
