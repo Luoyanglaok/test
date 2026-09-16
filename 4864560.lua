600f816e61dfaf4236c0e537181918e7dba0ee35
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:36:14 +0800

    add 4864560.lua

---
+++ 4864560.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4864560.lua
+--Gamename SCRIBBLE HUNT
+addappid(4864560) --Mainappid SCRIBBLE HUNT
+addappid(4864561, 1, "52936e781dc452b446084f9979c58367b30ebdd39659bb334a5249676f5df12a") --Main Windows Depot SCRIBBLE HUNT
+setManifestid(4864561, "4340875279084385730", 577631984)
