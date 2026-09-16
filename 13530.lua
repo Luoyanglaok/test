c6afdaa0ee851ae32716388c96503df3364ca751
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 19:30:27 +0800

    add 13530.lua

--- 13530.lua
+++ 13530.lua
@@ -1,13 +1,16 @@
-addappid(13530)
-addappid(13531,0,"b1cd415ec5ed2a5e3db610e95efcf4e56a87c35a68c849d426ba5a4d983548d0")
-setManifestid(13531,"5961831934542013620")
-addappid(13532,0,"fdc54ccb51553cc6fad5190cec9065d4886f6811aceac12e1dcff030d401a99f")
-setManifestid(13532,"294025028153179304")
-addappid(13533,0,"e44d840e9863ebac4258d1e10b809343dfc8ea7b3308af224607535113603902")
-setManifestid(13533,"4258727741508043262")
-addappid(13534,0,"2450493833e46bc4e51f19874adc888c8d2ce06f3bdab671cf66cdf9d31b7e84")
-setManifestid(13534,"5893073800087704633")
-addappid(13535,0,"a3f2c783a615efb6a83797d359a0a3d62dd26f609ab9bd90b22fa554d16b89da")
-setManifestid(13535,"3325630653161161324")
-addappid(13536,0,"88303f7bd09d7ccbe9d219183ecb9d0302758f5073ff5c8e9bc1d6c21b97b1a6")
-setManifestid(13536,"7432135760558882208")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 13530.lua
+--Gamename Prince of Persia: The Two Thrones™
+addappid(13530, 1, "de480aaa4c303d064ca39050cb744f2352c4d246454e9a88a9141ffa3a27e03e") --Mainappid Prince of Persia: The Two Thrones™
+addappid(13531, 1, "b1cd415ec5ed2a5e3db610e95efcf4e56a87c35a68c849d426ba5a4d983548d0") --Main Windows Depot Prince of Persia: The Two Thrones™
+setManifestid(13531, "5961831934542013620", 0)
+addappid(13532, 1, "fdc54ccb51553cc6fad5190cec9065d4886f6811aceac12e1dcff030d401a99f") --Main Windows Language Depot English Prince of Persia: The Two Thrones™
+setManifestid(13532, "294025028153179304", 0)
+addappid(13533, 1, "e44d840e9863ebac4258d1e10b809343dfc8ea7b3308af224607535113603902") --Main Windows Language Depot French Prince of Persia: The Two Thrones™
+setManifestid(13533, "4258727741508043262", 0)
+addappid(13534, 1, "2450493833e46bc4e51f19874adc888c8d2ce06f3bdab671cf66cdf9d31b7e84") --Main Windows Language Depot German Prince of Persia: The Two Thrones™
+setManifestid(13534, "5893073800087704633", 0)
+addappid(13535, 1, "a3f2c783a615efb6a83797d359a0a3d62dd26f609ab9bd90b22fa554d16b89da") --Main Windows Language Depot Italian Prince of Persia: The Two Thrones™
+setManifestid(13535, "3325630653161161324", 0)
+addappid(13536, 1, "88303f7bd09d7ccbe9d219183ecb9d0302758f5073ff5c8e9bc1d6c21b97b1a6") --Main Windows Language Depot Spanish Prince of Persia: The Two Thrones™
+setManifestid(13536, "7432135760558882208", 0)
