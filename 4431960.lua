b36c92e90c9ec49626f3fb1b3133531dbb3d0280
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 23:45:21 +0800

    add 4431960.lua

---
+++ 4431960.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4431960.lua
+--Gamename Island of mech 机械岛
+addappid(4431960) --Mainappid Island of mech 机械岛
+addappid(4431961, 1, "2fa3b47a8e62717b493ee263698015fdc4fbda15b53fd96aae3d131bf170e394") --Main Windows Depot Island of mech 机械岛
+setManifestid(4431961, "3152913376827605826", 229492464)
