80b78842902225f50a6bd9ee1e306b4bb47aafda
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 11:51:58 +0800

    add 2488370.lua

--- 2488370.lua
+++ 2488370.lua
@@ -1,5 +1,20 @@
-addappid(2488370)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2488372,0,"977e890c85c823ca9de5001068b30e038ade55c019c6e777e357cea4c148f33b")
-setManifestid(2488372,"1941277096523761470")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2488370.lua
+--Gamename Cash Cleaner Simulator
+addappid(2488370, 1, "890f37b45ab85bba704a5807707ae75961262bc0b5f50f44219a0faafaa0bf08") --Mainappid Cash Cleaner Simulator
+addappid(2488372, 1, "977e890c85c823ca9de5001068b30e038ade55c019c6e777e357cea4c148f33b") --Main Windows Depot Cash Cleaner Simulator
+setManifestid(2488372, "3263719484495518344", 2544796480)
+--Dlcname Cash Cleaner Simulator - Soundtrack
+addappid(3660300) --Dlcname Cash Cleaner Simulator - Soundtrack
+addappid(3660301, 1, "52b76fc0741c08617322b98fe08534682e59ccef8bc508ffee14375bdec30d77") --Dlc Windows Depot Cash Cleaner Simulator - Soundtrack
+setManifestid(3660301, "2888902693268649026", 68738080)
+--Dlcname Cash Cleaner Simulator - LOUD & LOADED
+addappid(4037500, 1, "75ac3f64e37085ac5c56984e78af87f5b281aef66b79ffed4cfe2fba6c4916f7") --Dlcname Cash Cleaner Simulator - LOUD & LOADED
+setManifestid(4037500, "7498130913430263497", 16620544)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3660310) --Dlcname Cash Cleaner Simulator - Luxury Loadout
+addappid(3795660) --Dlcname Cash Cleaner Simulator - Slay the Look
