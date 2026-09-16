7b170b47c0bdb9fad79a1e2302c9ce2117935e21
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 19:42:02 +0800

    add 826630.lua

--- 826630.lua
+++ 826630.lua
@@ -1,15 +1,28 @@
-addappid(826630)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228988,"6645201662696499616")
-addappid(826631,0,"d8be3df0a3621fbdc240ad82627e30a49fb44377c9e3853dcedbbc13f688c950")
-setManifestid(826631,"7139336776038494655")
-addappid(826632,0,"2de56d135ce616ca3d7c317cab731d0e7cae6993e3da80bfebc062b3549930c3")
-setManifestid(826632,"9187769269792769144")
-addappid(1341700,0,"e57395b0a6ef343f1cc7270bb3de56cb0c8c5b327c9ee0d8a06de8233bda5050")
-setManifestid(1341700,"2934246979974376637")
-addappid(1341701,0,"3a1b0e7f86ab77de1157f8fd35b197e866b8162d216e225c6e0e0a409d145b38")
-setManifestid(1341701,"2169359315516559275")
-addappid(1341702,0,"cc8401539e1c438a04e72b9349c091e5327c74c15f9e58981e4e0a7cdd77294d")
-setManifestid(1341702,"3256842357118763492")
-addappid(1628070,0,"21f7c10c96c7b8e840a421e65dff7aaa4d6eaacf1efabb3ddb27cadb0337e178")
-setManifestid(1628070,"2889397040427345476")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 826630.lua
+--Gamename Iron Harvest
+addappid(826630) --Mainappid Iron Harvest
+addappid(826631, 1, "d8be3df0a3621fbdc240ad82627e30a49fb44377c9e3853dcedbbc13f688c950") --Main Windows Depot Iron Harvest
+setManifestid(826631, "7139336776038494655", 7880101760)
+addappid(826632, 1, "2de56d135ce616ca3d7c317cab731d0e7cae6993e3da80bfebc062b3549930c3") --Main Windows Depot Iron Harvest
+setManifestid(826632, "6085011661260874995", 1951023168)
+--Dlcname AppID 1341700
+addtoken(1341700, "9060342457478263227")
+addappid(1341700, 1, "e57395b0a6ef343f1cc7270bb3de56cb0c8c5b327c9ee0d8a06de8233bda5050") --Dlcname AppID 1341700
+setManifestid(1341700, "2934246979974376637", 132433872)
+--Dlcname Iron Harvest: - Rusviet Revolution
+addappid(1341701, 1, "3a1b0e7f86ab77de1157f8fd35b197e866b8162d216e225c6e0e0a409d145b38") --Dlcname Iron Harvest: - Rusviet Revolution
+setManifestid(1341701, "2169359315516559275", 390841776)
+--Dlcname Iron Harvest: - Operation Eagle
+addappid(1341702, 1, "cc8401539e1c438a04e72b9349c091e5327c74c15f9e58981e4e0a7cdd77294d") --Dlcname Iron Harvest: - Operation Eagle
+setManifestid(1341702, "3256842357118763492", 2554303104)
+--Dlcname Iron Harvest - Iron Harvest 1920+
+addappid(1628070, 1, "21f7c10c96c7b8e840a421e65dff7aaa4d6eaacf1efabb3ddb27cadb0337e178") --Dlcname Iron Harvest - Iron Harvest 1920+
+setManifestid(1628070, "2889397040427345476", 4342254208)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(1341703) --Dlcname AppID 1341703
+addtoken(1341703, "7096760094847903643")
