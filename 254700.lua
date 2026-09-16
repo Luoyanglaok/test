b4cc2c9f41f7bd406d1ce16cad3f4acdc58711f3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 19:50:28 +0800

    add 254700.lua

--- 254700.lua
+++ 254700.lua
@@ -1,11 +1,23 @@
-addappid(254700)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(254701,0,"138bb1e5f555bb1881214eced9844b83b643695fc8726786a42b42a3f5c27902")
-setManifestid(254701,"6999377187357974260")
-addappid(272261,0,"e899c415aef8a533e80abb6c72b6bf3602f0b9b064c1bbb381a655667c576336")
-setManifestid(272261,"4849919482799769104")
-addappid(272260,0,"887fbc832b41f133430f20d78b365b07c724dd04c51ddcc48f879d907c4fd055")
-setManifestid(272260,"8170509752846139490")
-addappid(254704,0,"870c561ed9cbeb8f5733db64c240b45b609f1b8440890d999af03e206733d31d")
-setManifestid(254704,"7927591321049438693")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 254700.lua
+--Gamename Resident Evil 4 (2005)
+addappid(254700, 1, "ccc758ff227055ea493fd214673a52f7c733298a582dda0430d43b81cbbf5e6a") --Mainappid Resident Evil 4 (2005)
+addappid(254701, 1, "138bb1e5f555bb1881214eced9844b83b643695fc8726786a42b42a3f5c27902") --Main Windows Depot Resident Evil 4 (2005)
+setManifestid(254701, "6999377187357974260", 9681127936)
+addappid(254702, 1, "058a874bbbf796de8ae730aad32795572400bfc1404392b872a3302908e81889") --Main Windows Depot Resident Evil 4 (2005)
+setManifestid(254702, "7568911904646837022", 9677680320)
+addappid(254703, 1, "ed74776e6c965e3350288b1d290aae4e39c94c62e8173e014c085027e5b9e07d") --Main Windows Depot Resident Evil 4 (2005)
+setManifestid(254703, "3343076949474248393", 9653005728)
+addappid(254704, 1, "870c561ed9cbeb8f5733db64c240b45b609f1b8440890d999af03e206733d31d") --Main Windows Depot Resident Evil 4 (2005)
+setManifestid(254704, "7927591321049438693", 9677402592)
+--Dlcname Resident Evil 4 Soundtrack
+addappid(272260, 1, "887fbc832b41f133430f20d78b365b07c724dd04c51ddcc48f879d907c4fd055") --Dlcname Resident Evil 4 Soundtrack
+setManifestid(272260, "8170509752846139490", 0)
+--Dlcname Resident Evil 4 Art Book
+addappid(272261, 1, "e899c415aef8a533e80abb6c72b6bf3602f0b9b064c1bbb381a655667c576336") --Dlcname Resident Evil 4 Art Book
+setManifestid(272261, "4849919482799769104", 0)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
