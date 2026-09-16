f2c414e9fc2155a2b838eef55a5e4e31bd39f11c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:16:43 +0800

    add 1621310.lua

--- 1621310.lua
+++ 1621310.lua
@@ -1,5 +1,82 @@
-addappid(1621310)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1621311,0,"7efa5dc2c1f058948f98a3e2b75be10a0cf423134af8a02f79e9f78dc4602157")
-setManifestid(1621311,"5269180525473962898")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1621310.lua
+--Gamename Atelier Sophie 2: The Alchemist of the Mysterious Dream
+addappid(1621310) --Mainappid Atelier Sophie 2: The Alchemist of the Mysterious Dream
+addappid(1621311, 1, "7efa5dc2c1f058948f98a3e2b75be10a0cf423134af8a02f79e9f78dc4602157") --Main Windows Depot Atelier Sophie 2: The Alchemist of the Mysterious Dream
+setManifestid(1621311, "5269180525473962898", 10659750832)
+--Dlcname AppID 1754680
+addtoken(1754680, "2438579537275917386")
+addappid(1754680, 1, "19f21896c7cbe76801f0112566e96cb32d1c6c5efba7b056d7f24985f29ddf69") --Dlcname AppID 1754680
+--Dlcname AppID 1754683
+addtoken(1754683, "5776473274934807548")
+addappid(1754683, 1, "84ea7914b3a4be9ab29aaaa095d4ecabb421ede988931c9df531e7eb39702655") --Dlcname AppID 1754683
+--Dlcname AppID 1754684
+addtoken(1754684, "12796335806120162501")
+addappid(1754684, 1, "4c4249b7ae1cbad87da903e7b1094b9807fa78daec77ddbdf33e630c4d119478") --Dlcname AppID 1754684
+--Dlcname AppID 1754686
+addtoken(1754686, "11177714381433317960")
+addappid(1754686, 1, "c7c617a143b13671c071b9c68613aee3a3b732e915b9ffa29e226ada8a5a3898") --Dlcname AppID 1754686
+--Dlcname Atelier Sophie 2 - Sophie's Costume "Alchemist of the Mysterious Journey"
+addappid(1754687, 1, "d2234ecbeb4b503ae106df5680d0c6c613b421027bade9352299d6dc6fe432ca") --Dlcname Atelier Sophie 2 - Sophie's Costume "Alchemist of the Mysterious Journey"
+setManifestid(1754687, "4369955370819607665", 6258224)
+--Dlcname Atelier Sophie 2 - Plachta's Costume "Haute Couture"
+addappid(1754688, 1, "3e88a9d47ab78f7bcdbeccd14956e5d593fa48195b16cfeff5ab3dbfe31531a0") --Dlcname Atelier Sophie 2 - Plachta's Costume "Haute Couture"
+setManifestid(1754688, "5712629861566771067", 5108400)
+--Dlcname Atelier Sophie 2 - Ramizel's Costume "Ertona Hunter"
+addappid(1754689, 1, "8e2305c7b006bfa0897d3829a86905e4b2b5c16d0ecdff30ef23ff218b70759e") --Dlcname Atelier Sophie 2 - Ramizel's Costume "Ertona Hunter"
+setManifestid(1754689, "3785983125693932894", 4100464)
+--Dlcname Atelier Sophie 2 - Accessory "25 Glasses"
+addappid(1754690, 1, "2234668fadcf8604d2a6d54d11cb91bc1ac801765a437a97e9d6cbcbe4c4ecde") --Dlcname Atelier Sophie 2 - Accessory "25 Glasses"
+setManifestid(1754690, "703177582357918028", 546704)
+--Dlcname Atelier Sophie 2 - Accessory "Animal Headband"
+addappid(1754691, 1, "bd5a71f908e3be7dac852e56dc93a2ea6a08fe5c9b28ffdaa6b587563cbb12fd") --Dlcname Atelier Sophie 2 - Accessory "Animal Headband"
+setManifestid(1754691, "8062694440485836289", 1278368)
+--Dlcname Atelier Sophie 2 - Sophie/Plachta/Ramizel/Alette's Costume "Bunny-Eared Salesgirl"
+addappid(1754692, 1, "fa1773985960e293ff442f39e4769bae393091b693960bc712350bdefbcdbac9") --Dlcname Atelier Sophie 2 - Sophie/Plachta/Ramizel/Alette's Costume "Bunny-Eared Salesgirl"
+setManifestid(1754692, "1444197957677054757", 18963072)
+--Dlcname Atelier Sophie 2 - Recipe Expansion Pack "The Art of Synthesis"
+addappid(1754693, 1, "2f3ddbd2688abc427f48ab3144b6b3492b54fdb0ea5a08b8e89d74509439d0fb") --Dlcname Atelier Sophie 2 - Recipe Expansion Pack "The Art of Synthesis"
+setManifestid(1754693, "858447598732692701", 373936)
+--Dlcname Atelier Sophie 2 - Recipe Expansion Pack "The Art of Battle"
+addappid(1754694, 1, "7ff85e8497b2526a676d9b7653ba1461045be0785ad93b5a5d310a97b90895cb") --Dlcname Atelier Sophie 2 - Recipe Expansion Pack "The Art of Battle"
+setManifestid(1754694, "4844543110183554031", 2238576)
+--Dlcname Atelier Sophie 2 - Gust Extra BGM Pack
+addappid(1754695, 1, "1b285977b0be3b48b8cf640aa63c0d8b85e853d670aa2acd5d7ccb1a05440fad") --Dlcname Atelier Sophie 2 - Gust Extra BGM Pack
+setManifestid(1754695, "239842428436587579", 1287368912)
+--Dlcname Atelier Sophie 2 - Extra Area "Heartscape"
+addappid(1754696, 1, "5358e8fac5274f01364d3bc414c5953d3fe293f777ce09aeb074d0ad271a1d67") --Dlcname Atelier Sophie 2 - Extra Area "Heartscape"
+setManifestid(1754696, "8589538989680081933", 210384960)
+--Dlcname Atelier Sophie 2 - Sophie's Swimsuit "White Canvas"
+addappid(1754697, 1, "9bd5f83ecc0f987dbc59790df0e37fb82c110f683e53dcc58806ee08e43c4dde") --Dlcname Atelier Sophie 2 - Sophie's Swimsuit "White Canvas"
+setManifestid(1754697, "3944099572505315737", 3717648)
+--Dlcname Atelier Sophie 2 - Plachta's Swimsuit "Clivia Nobilis"
+addappid(1754698, 1, "0f1689965003e30d98b944f77870832ebfa9208f7b1f7bb2563f74aa66cce8f4") --Dlcname Atelier Sophie 2 - Plachta's Swimsuit "Clivia Nobilis"
+setManifestid(1754698, "1490338041946224317", 6277776)
+--Dlcname Atelier Sophie 2 - Ramizel's Swimsuit "Agapanthus Romance"
+addappid(1754699, 1, "1f367420406fb8d4c7e30ecc28f2c72283bcef5df00e584e54645a2828932852") --Dlcname Atelier Sophie 2 - Ramizel's Swimsuit "Agapanthus Romance"
+setManifestid(1754699, "2484368571832440506", 4051280)
+--Dlcname Atelier Sophie 2 - Alette's Swimsuit "Rainmaker"
+addappid(1754700, 1, "5850698dcb737db293d85d928794dceb3455934ba9b1b19e0069d95d37675a28") --Dlcname Atelier Sophie 2 - Alette's Swimsuit "Rainmaker"
+setManifestid(1754700, "4328401927093576770", 3619248)
+--Dlcname Atelier Sophie 2 - Olias's Swimsuit "Vegetable Garden"
+addappid(1754701, 1, "9e0bc7fa0aa1c6657e3cbf6f6aff4d065ceaab93b8c57d5165988610255d9046") --Dlcname Atelier Sophie 2 - Olias's Swimsuit "Vegetable Garden"
+setManifestid(1754701, "2567436537784201991", 4333104)
+--Dlcname Atelier Sophie 2 - Diebold's Swimsuit "Knight Commander"
+addappid(1754702, 1, "ba9d4356ba2c21c741558b8a9686a6a919a0933d888aa8b28f0bbd91aa783dd1") --Dlcname Atelier Sophie 2 - Diebold's Swimsuit "Knight Commander"
+setManifestid(1754702, "4636656465222311258", 4148224)
+--Dlcname Atelier Sophie 2 - Atelier Series Legacy BGM Pack
+addappid(1754703, 1, "d3e7799d9a68e156608d74aa0ef6049e36dd0df1308c016b0dc15f90f59298e7") --Dlcname Atelier Sophie 2 - Atelier Series Legacy BGM Pack
+setManifestid(1754703, "3952985351826138857", 3426383648)
+--Dlcname Atelier Sophie 2 - Extra Scenario/Area "Atelier Plachta"
+addappid(1754704, 1, "669094ba47d811aeb2e10100040bbdb32afa56f42f401920d9b989b5f55bf9ad") --Dlcname Atelier Sophie 2 - Extra Scenario/Area "Atelier Plachta"
+setManifestid(1754704, "2663273283167581852", 481899248)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1754681) --Dlcname AppID 1754681
+addtoken(1754681, "1746634817671374687")
+addappid(1754682) --Dlcname AppID 1754682
+addtoken(1754682, "17015986415343029936")
+addappid(1754685) --Dlcname Atelier Sophie 2 - Season Pass
