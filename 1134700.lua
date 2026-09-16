7a026462f1f72107535ffc412ae7b19e1558e5e4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 20:41:34 +0800

    add 1134700.lua

--- 1134700.lua
+++ 1134700.lua
@@ -1,9 +1,25 @@
-addappid(1134700)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(229007)
-setManifestid(229007,"4477590687906973371")
-addappid(1134701,0,"4760fc3eb52b780e294b15f3c907e77081c0e04233d5495645c3ac1ff3b6dfc8")
-setManifestid(1134701,"5616239145931184406")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1134700.lua
+--Gamename Wild Terra 2: New Lands
+addappid(1134700) --Mainappid Wild Terra 2: New Lands
+addappid(1134701, 1, "4760fc3eb52b780e294b15f3c907e77081c0e04233d5495645c3ac1ff3b6dfc8") --Main Windows Depot Wild Terra 2: New Lands
+setManifestid(1134701, "5289666417006776329", 3904370400)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(1785760) --Dlcname Wild Terra 2 - Halloween Pack
+addappid(1853210) --Dlcname Wild Terra 2 - Christmas Pack
+addappid(2157370) --Dlcname Wild Terra 2 - Bard Pack
+addappid(2157371) --Dlcname Wild Terra 2 - Lord of Pain Pack
+addappid(2250730) --Dlcname Wild Terra 2 - Christmas 2022 Pack
+addappid(2631620) --Dlcname Wild Terra 2 - Witchcraft Pack
+addappid(3371670) --Dlcname Wild Terra 2 - Landowner Pack
+addappid(4162100) --Dlcname Wild Terra 2 - Cultist Pack
+addappid(4280510) --Dlcname Wild Terra 2 - Novice Helpers
+addappid(5058360) --Dlcname Wild Terra 2 - Greek Pack
+--Missing Dlcs
+--Dlcname Wild Terra 2: New Lands Soundtrack
