78bd4ca91bef9120d0334f4de1a05c3a24c62a74
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 09:26:09 +0800

    add 2291680.lua

--- 2291680.lua
+++ 2291680.lua
@@ -4,6 +4,10 @@
 addappid(2291680, 1, "14a51b81f58e18c0558c035d022e2180e58f6e8c0ec757f967eba4c2ae64eda4") --Mainappid The Arrogant Kaiju Princess & The Detective Servant
 addappid(2291681, 1, "d5f249db247fe7b9d64a53304732e9775a8ffc470308d03c4e96fc84f8775f71") --Main Windows Depot The Arrogant Kaiju Princess & The Detective Servant
 setManifestid(2291681, "1585438778104121488", 1133623184)
+--Dlcname The Arrogant Kaiju Princess and The Detective Servant Soundtrack
+addappid(2714810) --Dlcname The Arrogant Kaiju Princess and The Detective Servant Soundtrack
+addappid(2714811, 1, "effac0195a538f873d695a538946b4fc0f02ff6f54eeda3908a2cde70fb889c7") --Dlc Windows Depot The Arrogant Kaiju Princess and The Detective Servant Soundtrack
+setManifestid(2714811, "4205806703297978943", 105430960)
 --Dlcname The Arrogant Kaiju Princess and The Detective Servant ArtBook
 addappid(2714820, 1, "232e0645e594dbe740dbb0bf1557d765dd0444558df6d159de8734b3fb51e255") --Dlcname The Arrogant Kaiju Princess and The Detective Servant ArtBook
 setManifestid(2714820, "4057397915279419981", 144017440)
