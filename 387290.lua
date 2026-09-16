c23e6aadba233d6a82ca9f5dce6e0e638071a5a3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 14:10:07 +0800

    add 387290.lua

--- 387290.lua
+++ 387290.lua
@@ -1,4 +1,15 @@
-addappid(387290)
-setManifestid(228983,"8124929965194586177")
-addappid(387291,0,"e3379e56401649f05bf54aa6ef2b7e61b9603a14484529d84daa9f07ba0b24c5")
-setManifestid(387291,"8629676443251475535")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 387290.lua
+--Gamename Ori and the Blind Forest: Definitive Edition
+addappid(387290, 1, "599380c191242721c4784f2d5313ab154bc6419575141da471220315f1260e5b") --Mainappid Ori and the Blind Forest: Definitive Edition
+addappid(387291, 1, "e3379e56401649f05bf54aa6ef2b7e61b9603a14484529d84daa9f07ba0b24c5") --Main Windows Depot Ori and the Blind Forest: Definitive Edition
+setManifestid(387291, "8629676443251475535", 4211368256)
+--Dlcname Ori and the Blind Forest (Original Soundtrack)
+addappid(465980, 1, "3ebeb57fc575aba316d19e27a787813044dddfb95ee036dab06fdf4f454550c1") --Dlcname Ori and the Blind Forest (Original Soundtrack)
+setManifestid(465980, "1673698342912223461", 769944896)
+--Dlcname Ori and the Blind Forest (Additional Soundtrack)
+addappid(466390, 1, "cbc8339aee28cc9461aa44f0509230f175fbccef2ae0fc1eac84409a060e55e5") --Dlcname Ori and the Blind Forest (Additional Soundtrack)
+setManifestid(466390, "571191038733822378", 565836976)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
