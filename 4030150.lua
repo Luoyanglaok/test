10c0bbef19e48c45044d372c7f6c7aa1c1da0318
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 16:45:25 +0800

    add 4030150.lua

---
+++ 4030150.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4030150.lua
+--Gamename The Legend of Fancy Realm Remake
+addappid(4030150, 1, "5f63e8880f18a4a4ffc412bb77810d7c6bfcb1cbe55b09f79cff18f05834a1ab") --Mainappid The Legend of Fancy Realm Remake
+addappid(4030151, 1, "f5f03f627f092015d6fe7d5add236886b69f806aa25fd3e0c343a070b3a72d3f") --Main Windows Depot The Legend of Fancy Realm Remake
+setManifestid(4030151, "7970017760703922757", 4317274176)
+--Dlcname The Legend of Fancy Realm Remake Demo
+addappid(4778780) --Dlcname The Legend of Fancy Realm Remake Demo
+addappid(4778781, 1, "51265d542ee157163fa31df95df5be3c6febc5b0320dd0f473207447d7be4665") --Dlc Windows Depot The Legend of Fancy Realm Remake Demo
+setManifestid(4778781, "8976619685647091328", 1766497552)
