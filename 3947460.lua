1f8506ee02371efbb5b9a3c4be41b6c4705c45e8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 21:09:23 +0800

    add 3947460.lua

---
+++ 3947460.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3947460.lua
+--Gamename Creator Chronicles
+addappid(3947460) --Mainappid Creator Chronicles
+addappid(3947462, 1, "e67b8ac726503edf627eaecb3cd37bdd7a3ce2834f082918f090c261af1813a9") --Main Windows Depot Creator Chronicles
+setManifestid(3947462, "8715923487143345218", 1409130064)
+addappid(3947463, 1, "c6d3679bc519719046c4e538cb06d7032c049b2608ce35a7f83205cbef5bedf7") --Main Macos Depot Creator Chronicles
+setManifestid(3947463, "3528167736646025248", 1436446800)
