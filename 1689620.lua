ebeaf3ae67fa264d49bea272f6352d72418e6bd1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:58:24 +0800

    add 1689620.lua

--- 1689620.lua
+++ 1689620.lua
@@ -1,6 +1,24 @@
-addappid(1689620)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-addappid(1689621,0,"7ff1d0c9c2aea56e2337ba3c85dbf96f3e436b64f4845764d78eb97ad681181c")
-setManifestid(1689621,"5456125316339713265")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1689620.lua
+--Gamename BLEACH Rebirth of Souls
+addappid(1689620, 1, "2ffa079d850193e2f4fb453873ce85c96ce745d3b83f21f97a60fd138cb268f0") --Mainappid BLEACH Rebirth of Souls
+addappid(1689621, 1, "7ff1d0c9c2aea56e2337ba3c85dbf96f3e436b64f4845764d78eb97ad681181c") --Main Windows Depot BLEACH Rebirth of Souls
+setManifestid(1689621, "4654145826755142404", 66417147040)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3203740) --Dlcname BLEACH Rebirth of Souls - Ichigo Kurosaki (Thousand-Year Blood War Arc)
+addappid(3203760) --Dlcname BLEACH Rebirth of Souls - Retsu Unohana
+addappid(3203770) --Dlcname BLEACH Rebirth of Souls - Ichibe Hyosube
+addappid(3203780) --Dlcname BLEACH Rebirth of Souls - Yhwach
+addappid(3203790) --Dlcname BLEACH Rebirth of Souls - Thousand-Year Blood War Toshiro Hitsugaya & Yoruichi Shihoin Costume Set
+addappid(3203800) --Dlcname BLEACH Rebirth of Souls - Thousand-Year Blood War Costume Set
+addappid(3203820) --Dlcname BLEACH Rebirth of Souls - Rebirth of Souls Costume Set
+addappid(3203830) --Dlcname BLEACH Rebirth of Souls - Two-Type Soul Crystal Set 1
+addappid(3203840) --Dlcname BLEACH Rebirth of Souls - Two-Type Soul Crystal Set 2
+addappid(3203850) --Dlcname BLEACH Rebirth of Souls - Two-Type Soul Crystal Set 3
+addappid(3203940) --Dlcname BLEACH Rebirth of Souls - Season Pass
