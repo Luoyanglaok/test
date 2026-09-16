51d83b70000efb1ae4f87c9e9851589a6a84b637
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 10:48:57 +0800

    add 3365050.lua

---
+++ 3365050.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3365050.lua
+--Gamename Atelier Ryza 3: Alchemist of the End & the Secret Key DX
+addappid(3365050) --Mainappid Atelier Ryza 3: Alchemist of the End & the Secret Key DX
+addappid(3365051, 1, "5f0ac27fa68bdbbb717f526d76c9a8c440ae3cfd217b65f183fa281d0b1cdf85") --Main Windows Depot Atelier Ryza 3: Alchemist of the End & the Secret Key DX
+setManifestid(3365051, "3923075188305520948", 30783205776)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3782470) --Dlcname Atelier Ryza 3: Alchemist of the End & the Secret Key DX - DX Version Pre-order Bonus
