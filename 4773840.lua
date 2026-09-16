b99db373e1fbdc959584da6adf1f303e590fa18c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 23:36:25 +0800

    add 4773840.lua

--- 4773840.lua
+++ 4773840.lua
@@ -3,4 +3,4 @@
 --Gamename City Lights Love Bites Season 1 - The Reset
 addappid(4773840, 1, "81b1f506295c8e1a4114f10e157e7ed8399b395bf172fa1987e1abfad4078c29") --Mainappid City Lights Love Bites Season 1 - The Reset
 addappid(4773842, 1, "f4c1c70bc5cff14bc969f7ed32304d0dae894661fc1d0cf67007ccec6dcaa57d") --Main Windows Depot City Lights Love Bites Season 1 - The Reset
-setManifestid(4773842, "3310363611030079186", 19536531728)
+setManifestid(4773842, "8199117338599327195", 21011964016)
