1f9cf2ba88621b4df09ef65513a72a918b768dd9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 01:05:31 +0800

    add 5027520.lua

--- 5027520.lua
+++ 5027520.lua
@@ -3,4 +3,4 @@
 --Gamename Megastore: Tidy Up Together
 addappid(5027520) --Mainappid Megastore: Tidy Up Together
 addappid(5027521, 1, "0b4bece51fa05789c4172e6faed43d360d0db528622312bc27b424ae4d608651") --Main Windows Depot Megastore: Tidy Up Together
-setManifestid(5027521, "4802343023408455639", 2663703376)
+setManifestid(5027521, "3542759666360127724", 2662461024)
