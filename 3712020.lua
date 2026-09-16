d3bd3a5c615fd03bfb9792589b7b5f1f2f89b504
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 22:25:59 +0800

    add 3712020.lua

--- 3712020.lua
+++ 3712020.lua
@@ -1,5 +1,11 @@
-addappid(3712020)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(3712021,0,"befead842594f096b8b7184b1a12ab7dddcdb21b1beefe7c5737c8647a00e4d2")
-setManifestid(3712021,"8859626808550239280")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3712020.lua
+--Gamename 意识分裂开篇季
+addappid(3712020, 1, "a9c9c324267c47862c1621a64e8250548da1a8f8eef703849b1cf34e4da76d5f") --Mainappid 意识分裂开篇季
+addappid(3712021, 1, "befead842594f096b8b7184b1a12ab7dddcdb21b1beefe7c5737c8647a00e4d2") --Main Windows Depot 意识分裂开篇季
+setManifestid(3712021, "5650619451767384470", 84774368)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
