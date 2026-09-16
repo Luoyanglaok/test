26b20e922f2b63f2291c91084ea534062db0dfc4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 21:22:01 +0800

    add 1486920.lua

--- 1486920.lua
+++ 1486920.lua
@@ -1,5 +1,28 @@
-addappid(1486920)
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-addappid(1486921,0,"26528db8fe19a223ddd4dbc280b52006e6b4aad5884ce084762e313c91d0de46")
-setManifestid(1486921,"5053467103971791561")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1486920.lua
+--Gamename Tempest Rising
+addappid(1486920, 1, "4f3870d90f0e6cedd1a0cc4ca85ebe56d874543cb0f2ff9d6f0ef4d76eb4729e") --Mainappid Tempest Rising
+addappid(1486921, 1, "26528db8fe19a223ddd4dbc280b52006e6b4aad5884ce084762e313c91d0de46") --Main Windows Depot Tempest Rising
+setManifestid(1486921, "3858388093955632009", 45284871952)
+--Dlcname Tempest Rising Soundtrack
+addtoken(2488520, "17731320530619852544")
+addappid(2488520) --Dlcname Tempest Rising Soundtrack
+addappid(2488521, 1, "a2b6d87f86ca0a85031b592070b8a77f88e741eaff207b555ce3dad8eb7d8faa") --Dlc Windows Depot Tempest Rising Soundtrack
+setManifestid(2488521, "8110103133700831113", 359213184)
+addappid(2488522, 1, "a83d7ae843a8ce85ebbb3cde04b685a0be56036671999c3a43b74a4f25874dca") --Dlc Windows Depot Tempest Rising Soundtrack
+setManifestid(2488522, "468187998898867897", 1838425968)
+--Dlcname Tempest Rising - Digital Artbook
+addtoken(3478250, "832652260017266002")
+addappid(3478250, 1, "9e5ff25c95b392c64f062155f91ef29f7378057b6e0f5e2742daf764e74f0aa4") --Dlcname Tempest Rising - Digital Artbook
+setManifestid(3478250, "6423062365868499672", 347529808)
+--Dlcname Tempest Rising - Map Editor
+addappid(4764990, 1, "df414ba7cc33e187046ccd3edd653a0559971467f6a0a1b697a89c352011d84e") --Dlcname Tempest Rising - Map Editor
+setManifestid(4764990, "5758251852664370", 5636155472)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3478280) --Dlcname Tempest Rising - Pre-order DLC
+addappid(3710570) --Dlcname Tempest Rising - Deluxe Pre-order Bonus Pack
+addappid(4114220) --Dlcname Tempest Rising: The Veti’s Wrath
