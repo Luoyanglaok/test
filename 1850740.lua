8a3668262874f0baddbcbf69d7c7b52ee201b753
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-16 13:44:00 +0800

    add 1850740.lua

--- 1850740.lua
+++ 1850740.lua
@@ -1,3 +1,9 @@
-addappid(1850740)
-addappid(1850741,0,"a830d518fb69395dfac8536575b35f141dc0ccb74f84bb08dad20556b7050c1e")
-setManifestid(1850741,"7373609519513095457")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1850740.lua
+--Gamename Ghost Watchers
+addappid(1850740, 1, "3119293ca8283e74497a588b64ec4fa7fc4494ec00ce6c0332d1ecc13bf6b081") --Mainappid Ghost Watchers
+addappid(1850741, 1, "a830d518fb69395dfac8536575b35f141dc0ccb74f84bb08dad20556b7050c1e") --Main Windows Depot Ghost Watchers
+setManifestid(1850741, "4237981273108245415", 10765789840)
+--Dlcname Ghost Watchers - Supporter Pack
+addappid(3876090, 1, "3d86a638743c1845580d2fe8dfbba088e48712581c1f040ad5745c4c590af9ad") --Dlcname Ghost Watchers - Supporter Pack
+setManifestid(3876090, "5303610124632066470", 255172080)
