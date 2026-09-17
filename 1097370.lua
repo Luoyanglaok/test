8e399f77990fa5f53c3b913cc31c52788d5b5ed5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 14:51:47 +0800

    add 1097370.lua

--- 1097370.lua
+++ 1097370.lua
@@ -1,3 +1,6 @@
-addappid(1097370)
-addappid(1097371,0,"ebbd318c18cf0cb59b16e9a854d0bd962807978682eb9a865ddb8ae9c87321b7")
-setManifestid(1097371,"3518189139650934497")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1097370.lua
+--Gamename パラディンリアス
+addappid(1097370, 1, "59e8126019524223d5b556ea790d27ce00dfe826630a41d89df064367d342b13") --Mainappid パラディンリアス
+addappid(1097371, 1, "ebbd318c18cf0cb59b16e9a854d0bd962807978682eb9a865ddb8ae9c87321b7") --Main Windows Depot パラディンリアス
+setManifestid(1097371, "3518189139650934497", 531758976)
