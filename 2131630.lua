fe984715b7598754b3205416ecaf0b286b2a1927
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 10:13:02 +0800

    add 2131630.lua

--- 2131630.lua
+++ 2131630.lua
@@ -1,11 +1,20 @@
-addappid(2131630)
-addappid(2131633,0,"b5534a1dc315beefb2031cfeda2b76f2602e42fcf1556dcd08b36ff166823513")
-setManifestid(2131633,"743655476540832556")
-addappid(2131632,0,"06990dbaa532ef23fc9c5b67dd9f0183e01ad29486b0ed6b7000b5d7e529786e")
-setManifestid(2131632,"6213511035375444915")
-addappid(2314212,0,"c2905c5640fde8855cb314b8a55650eb6625c631a1047fca98dff7a43125b691")
-setManifestid(2314212,"3092029632321159821")
-addappid(2314210,0,"e068c94228149aa99007649f734b768c4ab7a51d4513556df1cf33ee1c2a0126")
-setManifestid(2314210,"9135189822400605037")
-addappid(2314211,0,"db3ba13aeb08c580831ddae67b40c2e92853f73a5e8f88b8a196d1a9d0703959")
-setManifestid(2314211,"6106653665695138588")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2131630.lua
+--Gamename METAL GEAR SOLID - Master Collection Version
+addappid(2131630, 1, "4ff8d8f16301ac754d4b8bf2e22bc337b57e1e903c4ee5b92df4e0e7de6090b8") --Mainappid METAL GEAR SOLID - Master Collection Version
+addappid(2131631, 1, "5611f7eca8917daa3b4503c93aecc8ca3970b442be9d89b2b6e90ef16de6264b") --Main Windows Depot METAL GEAR SOLID - Master Collection Version
+setManifestid(2131631, "7339216024084706552", 5915058640)
+addappid(2131632, 1, "06990dbaa532ef23fc9c5b67dd9f0183e01ad29486b0ed6b7000b5d7e529786e") --Main Windows Depot METAL GEAR SOLID - Master Collection Version
+setManifestid(2131632, "7713525600005167712", 5021952560)
+addappid(2131633, 1, "b5534a1dc315beefb2031cfeda2b76f2602e42fcf1556dcd08b36ff166823513") --Main Windows Depot METAL GEAR SOLID - Master Collection Version
+setManifestid(2131633, "6062097622870190229", 8432542112)
+--Dlcname METAL GEAR SOLID - Master Collection Version Japanese Language Pack
+addappid(2314210, 1, "e068c94228149aa99007649f734b768c4ab7a51d4513556df1cf33ee1c2a0126") --Dlcname METAL GEAR SOLID - Master Collection Version Japanese Language Pack
+setManifestid(2314210, "9135189822400605037", 2104146320)
+--Dlcname METAL GEAR SOLID - Master Collection Version North American Pack
+addappid(2314211, 1, "db3ba13aeb08c580831ddae67b40c2e92853f73a5e8f88b8a196d1a9d0703959") --Dlcname METAL GEAR SOLID - Master Collection Version North American Pack
+setManifestid(2314211, "6106653665695138588", 1211016720)
+--Dlcname METAL GEAR SOLID - Master Collection Version European Pack
+addappid(2314212, 1, "c2905c5640fde8855cb314b8a55650eb6625c631a1047fca98dff7a43125b691") --Dlcname METAL GEAR SOLID - Master Collection Version European Pack
+setManifestid(2314212, "3092029632321159821", 4624827168)
+--Share Depots
