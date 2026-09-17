55a403741825e71bd8800f6d1c783e4fadc5033e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 22:29:44 +0800

    add 2428810.lua

--- 2428810.lua
+++ 2428810.lua
@@ -1,5 +1,13 @@
-addappid(2428810)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2428811,0,"b736dc935b4daf6a78b341a34ecc87e55683c086b7ba22247ebb72fe5194c818")
-setManifestid(2428811,"407163537835292904")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2428810.lua
+--Gamename LEGO® Horizon Adventures™
+addappid(2428810) --Mainappid LEGO® Horizon Adventures™
+addappid(2428811, 1, "b736dc935b4daf6a78b341a34ecc87e55683c086b7ba22247ebb72fe5194c818") --Main Windows Depot LEGO® Horizon Adventures™
+setManifestid(2428811, "407163537835292904", 14199349840)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2971640) --Dlcname LEGO® Horizon Adventures™ - Upgrade to Digital Deluxe Edition
+addappid(2971710) --Dlcname LEGO® Horizon Adventures™ - Pre-Purchase Entitlement
