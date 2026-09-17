005afabe5d41d9f58765a39c3d0a58212f9095b5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 18:34:19 +0800

    add 418730.lua

---
+++ 418730.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 418730.lua
+--Gamename AppID_418730
+addappid(418730) --Mainappid AppID_418730
+addappid(418731, 1, "aa364a0c4c8810e7687625846aa30f55681830b4747a221f4afa82e77e37639c") --Main Windows Depot AppID_418730
