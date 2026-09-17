5d1b72e8917a49e21f2e7e9aa8a8f4a3ec0420e1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 05:57:41 +0800

    add 3393280.lua

--- 3393280.lua
+++ 3393280.lua
@@ -3,6 +3,6 @@
 --Gamename Anime Shop Simulator ✨
 addappid(3393280, 1, "21b15997d07ddfbee815b7682a128e3eeab2ec9ce6dc10e4a2b6bc5416a04798") --Mainappid Anime Shop Simulator ✨
 addappid(3393281, 1, "e0932388cfbfc7e41c07682d366e74f70b2c9f46b5c00af7d316726e7e313a6c") --Main Windows Depot Anime Shop Simulator ✨
-setManifestid(3393281, "9027292350283347397", 1102737824)
+setManifestid(3393281, "7164730959186276014", 1109852752)
 addappid(5175820) --Dlcname Supporter Pack
 addappid(5175830) --Dlcname Naughty Shelf Pack
