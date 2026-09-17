47548e12734151266b17d00c4bbec74993bae606
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 12:01:23 +0800

    add 454650.lua

--- 454650.lua
+++ 454650.lua
@@ -1,7 +1,52 @@
-addappid(454650)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(454651,0,"f42c03be492cf6df28782bcf7630521712bb5425d268fcfc599731056ec1a21c")
-setManifestid(454651,"805057964426462694")
-addappid(540070,0,"3fe54a572bc1b0c0f9f13f3aa0d468cb56bb0faee8eb10df6a992c161943cc21")
-setManifestid(540070,"5501846961741041819")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 454650.lua
+--Gamename DRAGON BALL XENOVERSE 2
+addappid(454650, 1, "a0b608554d4fef2d5ac053ce6fd13408cef28626ddcf7198955127517ce39baa") --Mainappid DRAGON BALL XENOVERSE 2
+addappid(454651, 1, "f42c03be492cf6df28782bcf7630521712bb5425d268fcfc599731056ec1a21c") --Main Windows Depot DRAGON BALL XENOVERSE 2
+setManifestid(454651, "1938671063383174151", 33831861520)
+--Dlcname DRAGON BALL XENOVERSE 2 - Steve Aoki Pack
+addappid(540070, 1, "3fe54a572bc1b0c0f9f13f3aa0d468cb56bb0faee8eb10df6a992c161943cc21") --Dlcname DRAGON BALL XENOVERSE 2 - Steve Aoki Pack
+setManifestid(540070, "5501846961741041819", 11738528)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(507510) --Dlcname DRAGON BALL XENOVERSE 2 - Super Pass
+addappid(508400) --Dlcname DRAGON BALL XENOVERSE 2 - Super Pack 1
+addappid(508401) --Dlcname DRAGON BALL XENOVERSE 2 - Super Pack 2
+addappid(508402) --Dlcname DRAGON BALL XENOVERSE 2 - Super Pack 3
+addappid(508403) --Dlcname DRAGON BALL XENOVERSE 2 - Super Pack 4
+addappid(508404) --Dlcname DRAGON BALL Xenoverse 2 - Future Trunks
+addappid(508405) --Dlcname DRAGON BALL XENOVERSE 2 - Pre-Order Bonus
+addappid(540071) --Dlcname DRAGON BALL Xenoverse 2 - TP Medal
+addappid(556270) --Dlcname DRAGON BALL Xenoverse 2 - TV show TP Medal
+addappid(621100) --Dlcname DRAGON BALL XENOVERSE 2 - Future Trunks (Super)
+addappid(739280) --Dlcname DRAGON BALL XENOVERSE 2 - Extra Pack Set
+addappid(739281) --Dlcname DRAGON BALL XENOVERSE 2 - Extra DLC Pack 1
+addappid(739282) --Dlcname DRAGON BALL XENOVERSE 2 - Extra DLC Pack 2
+addappid(740140) --Dlcname DRAGON BALL XENOVERSE 2 - Anime Music Pack
+addappid(740141) --Dlcname DRAGON BALL XENOVERSE 2 - Extra Pass
+addappid(740142) --Dlcname DRAGON BALL XENOVERSE 2 - Extra DLC Pack 3
+addtoken(740142, "1583159477200805808")
+addappid(740143) --Dlcname DRAGON BALL XENOVERSE 2 - Extra DLC Pack 4
+addappid(740160) --Dlcname DRAGON BALL XENOVERSE 2 - Ultra Pack 1
+addappid(953020) --Dlcname DRAGON BALL XENOVERSE 2 - Anime Music Pack 2
+addappid(1108460) --Dlcname DRAGON BALL XENOVERSE 2 - Ultra Pack Set
+addappid(1108461) --Dlcname DRAGON BALL XENOVERSE 2 - Ultra Pack 2
+addtoken(1108461, "12437570787333637007")
+addappid(1507380) --Dlcname DRAGON BALL XENOVERSE 2 - Legendary Pack 1
+addappid(1544340) --Dlcname DRAGON BALL XENOVERSE 2 - Legendary Pack Set
+addappid(1744080) --Dlcname DRAGON BALL XENOVERSE 2 - Legendary Pack 2
+addappid(1999190) --Dlcname DRAGON BALL XENOVERSE 2 - Conton City Vote Pack
+addappid(2023140) --Dlcname DRAGON BALL XENOVERSE 2 Legend Patrol Pack
+addappid(2161540) --Dlcname DRAGON BALL XENOVERSE 2 - HERO OF JUSTICE Pack 1
+addappid(2161560) --Dlcname DRAGON BALL XENOVERSE 2 - HERO OF JUSTICE Pack Set
+addappid(2377310) --Dlcname DRAGON BALL XENOVERSE 2 - HERO OF JUSTICE Pack 2
+addappid(2814240) --Dlcname DRAGON BALL XENOVERSE 2 - FUTURE SAGA Chapter 1
+addappid(3188680) --Dlcname DRAGON BALL XENOVERSE 2 - FUTURE SAGA Chapter 2
+addappid(3387210) --Dlcname DRAGON BALL XENOVERSE 2 - Dragon Ball DAIMA Pack
+addappid(3581800) --Dlcname DRAGON BALL XENOVERSE 2 - Time Patrol Support Pack
+addappid(3839310) --Dlcname DRAGON BALL XENOVERSE 2 - FUTURE SAGA Chapter 3
+addappid(3905260) --Dlcname DRAGON BALL XENOVERSE 2 - FUTURE SAGA Chapter 4
+addappid(3905290) --Dlcname DRAGON BALL XENOVERSE 2 - FUTURE SAGA Pack Set
