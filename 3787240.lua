491263fb543adf12f0877e34d3bd6dd76e3eac46
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 22:24:08 +0800

    add 3787240.lua

---
+++ 3787240.lua
@@ -0,0 +1,25 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3787240.lua
+--Gamename MARVEL Tōkon: Fighting Souls
+addappid(3787240) --Mainappid MARVEL Tōkon: Fighting Souls
+addappid(3787241, 1, "bf2facc10027681f29ed4be380c37d63e38063a45ae478290aa3eae4da76db6a") --Main Windows Depot MARVEL Tōkon: Fighting Souls
+setManifestid(3787241, "745100765013791340", 25200140768)
+--Dlcname MARVEL Tōkon: Fighting Souls – Standard – Ultimate Edition Upgrade
+addappid(5030920, 1, "a4ce49332b55702216763ab082b9a974b55dc4671f4dcf08373d04d0147d536a") --Dlcname MARVEL Tōkon: Fighting Souls – Standard – Ultimate Edition Upgrade
+--Dlcname MARVEL Tōkon: Fighting Souls – Deluxe – Ultimate Edition Upgrade
+addappid(5030930, 1, "e64759fa1407e4b957f341d71502016b322e370ddfead9f81be2d6e51bd663fc") --Dlcname MARVEL Tōkon: Fighting Souls – Deluxe – Ultimate Edition Upgrade
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4839960) --Dlcname MARVEL Tōkon: Fighting Souls - Pre-purchase Bonus
+addappid(4848500) --Dlcname MARVEL Tōkon: Fighting Souls - Year 1 Characters and Stage Pass
+addappid(4848520) --Dlcname MARVEL Tōkon: Fighting Souls - Set of result screen poses
+addappid(4848530) --Dlcname MARVEL Tōkon: Fighting Souls - Lobby Avatar and Lobby Pet
+addappid(4848540) --Dlcname MARVEL Tōkon: Fighting Souls - Animated Chromatic Color Unlock
+addappid(4848550) --Dlcname MARVEL Tōkon: Fighting Souls - Spider-Man “Advanced Suit 2.0” Costume
+addappid(4848570) --Dlcname MARVEL Tōkon: Fighting Souls - Storm Punk Rock Costume
+addappid(4848580) --Dlcname MARVEL Tōkon: Fighting Souls - Iron Man Classic Costume
+addappid(4848590) --Dlcname MARVEL Tōkon: Fighting Souls - Captain America Classic Costume
+addappid(4848600) --Dlcname MARVEL Tōkon: Fighting Souls - Doctor Doom Supreme Ruler Costume
