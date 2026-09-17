e1f65a6697e64b4bce274a18d4049472cddc11cd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 01:32:34 +0800

    add 3628950.lua

---
+++ 3628950.lua
@@ -0,0 +1,19 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3628950.lua
+--Gamename Succubus Successor: Delilah's Juicy Journey
+addappid(3628950, 1, "c6cb617a81045106ceb205a9c87b664229dd0424c49ea19baea25f0d5855c0c9") --Mainappid Succubus Successor: Delilah's Juicy Journey
+addtoken(3628950, "396372275392605206")
+addappid(3628951, 1, "e3609b671bf2f7a6bac8a1a1400c623a98744aae13e7439da56e6d0671149ec6") --Main Windows/Linux Depot Succubus Successor: Delilah's Juicy Journey
+setManifestid(3628951, "3102471238129722632", 346152848)
+addappid(3628952, 1, "b87ab725b1e81ad396d09037ad31b1ea27ba5d61cbc7ffadb2f8bb7fb7f3a8f6") --Main Macos Depot Succubus Successor: Delilah's Juicy Journey
+setManifestid(3628952, "6283739771057901395", 356231952)
+--Dlcname Succubus Successor - Digital Artbook
+addappid(4644260, 1, "41a885b51961ef2218d73264db11ba87f3edce28e17d081837d0b78f4d22298f") --Dlcname Succubus Successor - Digital Artbook
+setManifestid(4644260, "4295630627713167132", 186149472)
+--Dlcname Succubus Successor - Deluxe Weapon Appearance Pack
+addappid(4718750, 1, "9186347a9f3be8ed1990497511ee6d3363f4a48a60c513a7261cf017523163fd") --Dlcname Succubus Successor - Deluxe Weapon Appearance Pack
+setManifestid(4718750, "8594653028465372818", 592)
+addappid(4718751, 1, "7b316edcb17b5b61df21a8bf5fe913d30c407c8a13ad347fc9adc21b058c92f9") --Dlc Macos Depot Succubus Successor - Deluxe Weapon Appearance Pack
+setManifestid(4718751, "9161438700502255147", 592)
+--Missing Dlcs
+--Dlcname Succubus Successor - Original Soundtrack
