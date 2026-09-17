dce75e64931d8c369f529cf1bd8dd357b385c041
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 11:50:54 +0800

    add 4225980.lua

---
+++ 4225980.lua
@@ -0,0 +1,27 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4225980.lua
+--Gamename Trails in the Sky 2nd Chapter
+addappid(4225980) --Mainappid Trails in the Sky 2nd Chapter
+addappid(4225981, 1, "e2f69dd5a8a01d4cf7d8c36743a5389f87f090207cf209ab2b21e2aa6d048ad5") --Main Windows Depot Trails in the Sky 2nd Chapter
+setManifestid(4225981, "5745377515794956771", 39607553024)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4975590) --Dlcname Trails in the Sky 2nd Chapter - Fluffy Creepy Sheep Costume Set
+addappid(4975600) --Dlcname Trails in the Sky 2nd Chapter - Neo Bracer II Costume (Estelle)
+addappid(4975680) --Dlcname Trails in the Sky 2nd Chapter - Original Cheerleader Costume Set
+addappid(4975710) --Dlcname Trails in the Sky 2nd Chapter - Glasses Set A
+addappid(4975740) --Dlcname Trails in the Sky 2nd Chapter - Pom Pom Head Set
+addappid(4975820) --Dlcname Trails in the Sky 2nd Chapter - Thrilling Stadium Set
+addappid(4975830) --Dlcname Trails in the Sky 2nd Chapter - Exciting Effect Set
+addappid(4975850) --Dlcname Trails in the Sky 2nd Chapter - Shiny Hair Color Set
+addappid(4975870) --Dlcname Trails in the Sky 2nd Chapter - Orbment Cover Set - 2nd Chapter
+addappid(4975910) --Dlcname Trails in the Sky 2nd Chapter - Starter Set
+addappid(4975920) --Dlcname Trails in the Sky 2nd Chapter - Advanced Item Set Vol. 1
+addappid(4975940) --Dlcname Trails in the Sky 2nd Chapter - Advanced Item Set Vol. 2
+addappid(4975950) --Dlcname Trails in the Sky 2nd Chapter - Advanced Item Set Vol. 3
+addappid(4975960) --Dlcname Trails in the Sky 2nd Chapter - Battle Boost Pack
+addappid(4975970) --Dlcname Trails in the Sky 2nd Chapter - Free Sample Set
+addappid(5009480) --Dlcname Trails in the Sky 2nd Chapter Season Pass
