9ee8cb77242c61e09a6aea71dfd196cd6ddf8f69
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 14:26:13 +0800

    add 1284190.lua

--- 1284190.lua
+++ 1284190.lua
@@ -1,3 +1,14 @@
-addappid(1284190)
-addappid(1284192,0,"1f10eb10bfc54e336b5f2e2a5c05001817700491d0c79a7931637a9158c41dc0")
-setManifestid(1284192,"2664903678062864908")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1284190.lua
+--Gamename The Planet Crafter
+addappid(1284190, 1, "d691e043b9b43765cb9dce454231965e93fa7fd5b28ca5c6cfb72af0e785adf9") --Mainappid The Planet Crafter
+addappid(1284192, 1, "1f10eb10bfc54e336b5f2e2a5c05001817700491d0c79a7931637a9158c41dc0") --Main Windows Depot The Planet Crafter
+setManifestid(1284192, "7543045212191035237", 3754368832)
+--Dlcname The Planet Crafter Soundtrack
+addappid(2902050) --Dlcname The Planet Crafter Soundtrack
+addappid(2902051, 1, "7b4e9eae4b3cbb994e3efaff9668dacfacf4dd0442655edb3f0b1113e4dacf8e") --Dlc Windows Depot The Planet Crafter Soundtrack
+setManifestid(2902051, "4820105730894901731", 267345120)
+addappid(2902052, 1, "61747707d2c3cc6d5d6e2600ea2b40f67857afa2af76e21d2d03582b62e5920d") --Dlc Windows Depot The Planet Crafter Soundtrack
+setManifestid(2902052, "7813122267707738327", 2405280928)
+addappid(3142540) --Dlcname The Planet Crafter - Planet Humble
+addappid(4078590) --Dlcname The Planet Crafter - Toxicity
