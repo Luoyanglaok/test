d65d8b1db7f58893067053bae3e51cf14e5a30b7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 16:25:32 +0800

    add 4507930.lua

---
+++ 4507930.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4507930.lua
+--Gamename Succubus Nightmare
+addappid(4507930, 1, "1e3069dafe06e30d341ca24b8d9ba3deced25c80c8cc938b2f79ee7fbe2d3c67") --Mainappid Succubus Nightmare
+addappid(4507931, 1, "9a8f4b03c31155ac29133fa191ac18b5c157eaf4d424c0e7aca32e5eb26b5151") --Main Windows Depot Succubus Nightmare
+setManifestid(4507931, "3592047195389462965", 2801891472)
+addappid(4996421, 1, "c80ef52f774137dc1d64575a50544528a72340731257dc9c66a2cbefae13fe00") --Main Windows Depot Succubus Nightmare
+setManifestid(4996421, "99692359182191734", 46754000)
+--Dlcname Succubus Nightmare Artbook
+addappid(4996420) --Dlcname Succubus Nightmare Artbook
+--Missing Dlcs
+--Dlcname Succubus Nightmare Soundtrack
