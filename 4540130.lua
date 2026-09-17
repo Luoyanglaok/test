6eab0bf0efcabd6d44c1636dbb45bf17a53284c4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 13:37:48 +0800

    add 4540130.lua

---
+++ 4540130.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4540130.lua
+--Gamename Parry Survivor
+addappid(4540130) --Mainappid Parry Survivor
+addappid(4540131, 1, "63953a24ac7ea2163ad575dc8306279a018f05b9ba0de621693da6ef5beaa2e3") --Main Windows Depot Parry Survivor
+setManifestid(4540131, "5968073851385607517", 1898642416)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
