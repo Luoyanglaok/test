dbb62f07146a37830b7fa8764170c4f3dcde0922
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 16:20:35 +0800

    add 2887580.lua

--- 2887580.lua
+++ 2887580.lua
@@ -3,7 +3,7 @@
 --Gamename Active Matter
 addappid(2887580) --Mainappid Active Matter
 addappid(2887581, 1, "279f63502b38413b7f07b57ac3e32cf4b9c303e3977107d2891cf053b159f5c9") --Main Windows Depot Active Matter
-setManifestid(2887581, "5253383296583988128", 37251015904)
+setManifestid(2887581, "8718670431717722752", 37251013648)
 addappid(2887582, 1, "fe6a84f76a66a813069f11ee823b3e5946abe5a2503bc72302a56c1db2cde372") --Main Windows Depot Active Matter
 setManifestid(2887582, "5826875760740471667", 425618448)
 addappid(4107830) --Dlcname Active Matter - Upgrade to Advanced Edition
