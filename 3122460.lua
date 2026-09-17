63a4ce81bfe2eb8d6cf7f7d87c9c20e227b02955
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 11:03:22 +0800

    add 3122460.lua

---
+++ 3122460.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3122460.lua
+--Gamename Desktop Raid
+addappid(3122460, 1, "e85f053bec558ce787bc13ce61e746d32c881ad6cf7a73a5a50b79829296d882") --Mainappid Desktop Raid
+addappid(3122461, 1, "52933d19d80d16a07195093f8cf4540dfabf6a8a6c4df8f066bc8aae48d36880") --Main Windows Depot Desktop Raid
+setManifestid(3122461, "4486529857199498339", 271789568)
+addappid(4463340) --Dlcname Desktop Raid - Supporter Pack
+addappid(4539650) --Dlcname Desktop Raid - Mythic Heroes Pack
+addappid(4539660) --Dlcname Desktop Raid - Eastern Mythology Pack
