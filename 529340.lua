6025c004c5c45bccb4d445f08e50fe5caf4b3ce5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 13:18:12 +0800

    add 529340.lua

--- 529340.lua
+++ 529340.lua
@@ -1,39 +1,75 @@
-addappid(529340)
-addappid(529341,0,"751bcf7f0ff0ba30922a60e32fe2c3e57e717bf3eb81cd5abe517d89a91f666a")
-setManifestid(529341,"5615111875422178600")
-addappid(529342,0,"fbbc0f3f114625d167ca6637a358e37e90ae0e56002ad08d6cd826141487b143")
-setManifestid(529342,"4198924086075322936")
-addappid(529343,0,"f8b6d5cad4b56cdcb290338d680e0b7f5ec029a3a9c0e7801eab102c7057ed45")
-setManifestid(529343,"5992630337827096803")
-addappid(529344,0,"91e2a80038ee5c6b9c10ffde8db28f2da0e1b358d499654416aea646689673a1")
-setManifestid(529344,"5474418411429635610")
-addappid(529345,0,"35b3747b37fea27a9fd293bd654b46bb98d4bf2517f9a13d4c131d01ac0b09a3")
-setManifestid(529345,"5423609435181126207")
-addappid(529346,0,"a2b158572cb0700badd3577c2d65aab9d8401347c178d0a429b7fd4aabf0d608")
-setManifestid(529346,"6652037455944396673")
-addappid(529347,0,"657da71ab5a239544ffed593e0ab6f4d90ecc376989bdd7d3ddea676bed7321b")
-setManifestid(529347,"7736451851756103348")
-addappid(2071470,0,"e46b0335eaf1a6aa7ed5b00b9051735d16d1f034c9ed1dbcc1b5743175f8d1fc")
-setManifestid(2071470,"8346935359547576049")
-addappid(2071471,0,"fd34c3e32f4e327d81e2929351b9ab82f5848549f7ea3173a3266335cbf6b1f5")
-setManifestid(2071471,"9120305042692556702")
-addappid(2348450,0,"8321a5fc485da48cfb6fc6ac66b941c35a979cd982b719fc8facf51d2edba047")
-setManifestid(2348450,"8440722596536826416")
-addappid(2282100,0,"ca755c1431847a9c44d481bb188467c2f96ee0c26720c944acdcd1700cf69771")
-setManifestid(2282100,"5402093386241691583")
-addappid(2366580,0,"8cc68dc93b4685bc2986a3fc4556a2de7f8cf1909bb2f06cd2ed94f48e2350d3")
-setManifestid(2366580,"4098566664589815689")
-addappid(2411230,0,"80fa984d828254137925cac71bd9ebeb795fa5b4bc390ad9983fe8fadbd211a3")
-setManifestid(2411230,"439261979960657494")
-addappid(2591240,0,"773a1ca21d2e9197ce4b1ff852ae3230ac5fbab8a954a68b61ad71a13e4875b2")
-setManifestid(2591240,"9066131419901792073")
-addappid(2800730,0,"4e6ca866a42b0bda030de8c041ff8694d51a8ec73375dbc5e68c4d93749ae07f")
-setManifestid(2800730,"7779536810996294997")
-addappid(2411231,0,"5ba842826fdb9e68071c9771b01adc0e4be9159e92e26de331e7ac5c1d2684f9")
-setManifestid(2411231,"3406759059409215989")
-addappid(3174360,0,"fd9adc2a5fc66fba32852fcc4584ab6ee87ca061d6a03ba3a9c262baed00dd8a")
-setManifestid(3174360,"2849401594948952698")
-addappid(3562940,0,"6fc91d1743fbec84b8887e3092e4c473292d0dbc973bb31ea812805e28f7ea50")
-setManifestid(3562940,"561741218986605312")
-addappid(3450170,0,"10d073b22eb3123a70c012b6b9ebea935c6fff13e7fb6652e7e7636566ba062d")
-setManifestid(3450170,"5691867127414468959")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 529340.lua
+--Gamename Victoria 3
+addappid(529340, 1, "6bcadea95bc4140711cac04610c20ad2857088090ead96424d38bae4505042ce") --Mainappid Victoria 3
+addappid(529341, 1, "751bcf7f0ff0ba30922a60e32fe2c3e57e717bf3eb81cd5abe517d89a91f666a") --Main Windows Depot Victoria 3
+setManifestid(529341, "4498977168532327663", 6154101392)
+addappid(529342, 1, "fbbc0f3f114625d167ca6637a358e37e90ae0e56002ad08d6cd826141487b143") --Main Windows Depot Victoria 3
+setManifestid(529342, "4691919316655930977", 163376560)
+addappid(529343, 1, "f8b6d5cad4b56cdcb290338d680e0b7f5ec029a3a9c0e7801eab102c7057ed45") --Main Macos Depot Victoria 3
+setManifestid(529343, "72255550990920910", 221560208)
+addappid(529344, 1, "91e2a80038ee5c6b9c10ffde8db28f2da0e1b358d499654416aea646689673a1") --Main Linux Depot Victoria 3
+setManifestid(529344, "8986793942031034561", 571299920)
+addappid(529345, 1, "35b3747b37fea27a9fd293bd654b46bb98d4bf2517f9a13d4c131d01ac0b09a3") --Main Windows Depot Victoria 3
+setManifestid(529345, "7414932565382460817", 191976048)
+addappid(529346, 1, "a2b158572cb0700badd3577c2d65aab9d8401347c178d0a429b7fd4aabf0d608") --Main Macos Depot Victoria 3
+setManifestid(529346, "3116744691613779432", 382078784)
+addappid(529347, 1, "657da71ab5a239544ffed593e0ab6f4d90ecc376989bdd7d3ddea676bed7321b") --Main Linux Depot Victoria 3
+setManifestid(529347, "7728389654811035422", 186340416)
+--Dlcname Victoria 3: Victoria II Remastered Songs
+addappid(2071470, 1, "e46b0335eaf1a6aa7ed5b00b9051735d16d1f034c9ed1dbcc1b5743175f8d1fc") --Dlcname Victoria 3: Victoria II Remastered Songs
+setManifestid(2071470, "4163203492406782527", 608302720)
+--Dlcname Victoria 3: American Buildings Pack
+addappid(2071471, 1, "fd34c3e32f4e327d81e2929351b9ab82f5848549f7ea3173a3266335cbf6b1f5") --Dlcname Victoria 3: American Buildings Pack
+setManifestid(2071471, "9120305042692556702", 102896)
+--Dlcname Victoria 3: Voice of the People - Immersion Pack
+addappid(2282100, 1, "ca755c1431847a9c44d481bb188467c2f96ee0c26720c944acdcd1700cf69771") --Dlcname Victoria 3: Voice of the People - Immersion Pack
+setManifestid(2282100, "363659807722140988", 387120928)
+--Dlcname Victoria 3: Melodies for the Masses - Music Pack
+addappid(2348450, 1, "8321a5fc485da48cfb6fc6ac66b941c35a979cd982b719fc8facf51d2edba047") --Dlcname Victoria 3: Melodies for the Masses - Music Pack
+setManifestid(2348450, "4343126498545025373", 725314224)
+--Dlcname Victoria 3: French Agitators Bonus Pack
+addappid(2366580, 1, "8cc68dc93b4685bc2986a3fc4556a2de7f8cf1909bb2f06cd2ed94f48e2350d3") --Dlcname Victoria 3: French Agitators Bonus Pack
+setManifestid(2366580, "4098566664589815689", 115984)
+--Dlcname Victoria 3: Dawn of Wonder - Art Pack
+addappid(2411230, 1, "80fa984d828254137925cac71bd9ebeb795fa5b4bc390ad9983fe8fadbd211a3") --Dlcname Victoria 3: Dawn of Wonder - Art Pack
+setManifestid(2411230, "1461629349360033550", 112047680)
+--Dlcname Victoria 3: Sphere of Influence - Expansion
+addappid(2411231, 1, "5ba842826fdb9e68071c9771b01adc0e4be9159e92e26de331e7ac5c1d2684f9") --Dlcname Victoria 3: Sphere of Influence - Expansion
+setManifestid(2411231, "6778870637804137779", 273442048)
+--Dlcname Victoria 3: Colossus of the South - Region Pack
+addappid(2591240, 1, "773a1ca21d2e9197ce4b1ff852ae3230ac5fbab8a954a68b61ad71a13e4875b2") --Dlcname Victoria 3: Colossus of the South - Region Pack
+setManifestid(2591240, "5896939596283282705", 45027952)
+--Dlcname Victoria 3: Trains - Bonus Pack
+addappid(2800730, 1, "4e6ca866a42b0bda030de8c041ff8694d51a8ec73375dbc5e68c4d93749ae07f") --Dlcname Victoria 3: Trains - Bonus Pack
+setManifestid(2800730, "5050073985265929023", 3867856)
+--Dlcname Victoria 3: Pivot of Empire - Immersion Pack
+addappid(3174360, 1, "fd9adc2a5fc66fba32852fcc4584ab6ee87ca061d6a03ba3a9c262baed00dd8a") --Dlcname Victoria 3: Pivot of Empire - Immersion Pack
+setManifestid(3174360, "1036159294070605185", 161633888)
+--Dlcname Victoria 3: Charters of Commerce - Mechanics Pack
+addappid(3450170, 1, "10d073b22eb3123a70c012b6b9ebea935c6fff13e7fb6652e7e7636566ba062d") --Dlcname Victoria 3: Charters of Commerce - Mechanics Pack
+setManifestid(3450170, "1859415655471258101", 50902720)
+--Dlcname Victoria 3: National Awakening - Immersion Pack
+addappid(3562730, 1, "ed6a84a44c1e43437202fc093e173ce8cb2ead8463df1a694bf43bd2e99a9988") --Dlcname Victoria 3: National Awakening - Immersion Pack
+setManifestid(3562730, "5018939423623213493", 201380032)
+--Dlcname Victoria 3: Songs of the Homeland - Music Pack
+addappid(3562810, 1, "e3554bdbd47ebd858dfd124dfbda63f44ee18ce89de8574e8e7dbdb0d7441a07") --Dlcname Victoria 3: Songs of the Homeland - Music Pack
+setManifestid(3562810, "8968706433718035952", 413758832)
+--Dlcname Victoria 3: Iberian Twilight - Immersion Pack
+addappid(3562890, 1, "a94dbe1edbe6d6570aa59dc67513830a4c712703306c40afcc9238c47b741d26") --Dlcname Victoria 3: Iberian Twilight - Immersion Pack
+setManifestid(3562890, "6177428406241237836", 165012032)
+--Dlcname Victoria 3: Trade Ships - Bonus Pack
+addappid(3562940, 1, "6fc91d1743fbec84b8887e3092e4c473292d0dbc973bb31ea812805e28f7ea50") --Dlcname Victoria 3: Trade Ships - Bonus Pack
+setManifestid(3562940, "4588768850590574248", 5821280)
+--Dlcname Victoria 3: Warships - Bonus Pack
+addappid(4314640, 1, "de6a7f79a9b77028620bdb00a4d9029076f2e378750404aa12420eab405c7288") --Dlcname Victoria 3: Warships - Bonus Pack
+setManifestid(4314640, "6866145312939114949", 22673632)
+--Dlcname Victoria 3: The Great Wave - Expansion
+addappid(4314650, 1, "08f910d38b72a83f8f28d324c6d95fe6514ed4c39ee8b5c8b5e7bb1541218d7e") --Dlcname Victoria 3: The Great Wave - Expansion
+setManifestid(4314650, "1572040182768359", 363803056)
+addappid(2071472) --Dlcname Victoria 3 - Expansion Pass
+addappid(3563050) --Dlcname Victoria 3: Expansion Pass 2
+addappid(3596990) --Dlcname Victoria 3: Ultimate Bundle
+addappid(4314630) --Dlcname Victoria 3: Volume 3
+addappid(4314660) --Dlcname Victoria 3: State and Revolution - Immersion Pack
+addappid(4314670) --Dlcname Victoria 3: Century of Strife - Immersion Pack
