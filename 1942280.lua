8ca23ce9fcb963300188b7469a892cba81feb8b7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 22:45:36 +0800

    add 1942280.lua

--- 1942280.lua
+++ 1942280.lua
@@ -1,5 +1,22 @@
-addappid(1942280)
-addappid(1942281,0,"414bbd1359869bcea73f5e7f3a911ba4a8f487dee2cf0290cd2409f9ae410582")
-setManifestid(1942281,"6327420223269467326")
-addappid(2868390,0,"d7b9838655889932b120a4bd3426b37af101b334e6602a5898a1d54e4dc8f0f8")
-setManifestid(2868390,"1044737294045538329")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1942280.lua
+--Gamename Brotato
+addappid(1942280, 1, "bf5b1b4f28c4d1839b85a86e3bfdabc5435f8cc848ca968aec505c86c6ecb52e") --Mainappid Brotato
+addtoken(1942280, "14512176092569698002")
+addappid(1942281, 1, "414bbd1359869bcea73f5e7f3a911ba4a8f487dee2cf0290cd2409f9ae410582") --Main Windows Depot Brotato
+setManifestid(1942281, "5289220193624029472", 161064320)
+addappid(1942282, 1, "3494683F2E70AFB7AE55048CE6D4C4FD6139904D7D783355A35BBAC4D1FD2A3B") --Main Linux Depot Brotato
+setManifestid(1942282, "4872816150142449642", 160537200)
+addappid(1942283, 1, "15D4A4598C84D88D00A61EF4BAEDBD99BBF62E47402A887EAD1872B8AA5C89E7") --Main Macos Depot Brotato
+setManifestid(1942283, "2748719760099278892", 177672096)
+--Dlcname Brotato: Abyssal Terrors
+addtoken(2868390, "17992809593575622817")
+addappid(2868390, 1, "d7b9838655889932b120a4bd3426b37af101b334e6602a5898a1d54e4dc8f0f8") --Dlcname Brotato: Abyssal Terrors
+setManifestid(2868390, "5627320447442986624", 55272624)
+--Dlcname Brotato Soundtrack
+addappid(3281080) --Dlcname Brotato Soundtrack
+addappid(3281081, 1, "e6d0a71df9e37dbb1c69621d45ab42a35f0bc52e1c716e17bac88ffb29fa6cc6") --Dlc Windows Depot Brotato Soundtrack
+setManifestid(3281081, "5219183446974776720", 83591856)
+addappid(3281082, 1, "0b62d5b0ce0ca5679ccbfafa9035fcbd744dccf03d97e6f2305ef8d28bcb8c58") --Dlc Windows Depot Brotato Soundtrack
+setManifestid(3281082, "8742789198982800516", 424931792)
+addappid(4498220) --Dlcname Brotato: Primal Dread
