57a5c5809373ac3579d71a1259ebf34359c601c1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 11:57:05 +0800

    add 222480.lua

--- 222480.lua
+++ 222480.lua
@@ -1,18 +1,41 @@
-addappid(222480)
-setManifestid(228990,"1829726630299308803")
-addappid(222481,0,"65e75d906613ce658af73b8db0772cf9f6fa3905427161dd1c0482572da72c0d")
-setManifestid(222481,"2624313053854726936")
-addappid(229621,0,"cd908e40d47ae228e942070e0e6e9dd87d874f16434488b0b6d70091915ec999")
-setManifestid(229621,"1729720183785589076")
-addappid(229622,0,"ae8ea828b9a2c97510d5968ce55a9dae3a04346865801ac4757d7cd7ff106d9d")
-setManifestid(229622,"5762540738354433494")
-addappid(229626,0,"1badd29cc032ee6227834ed10f78d0f4e785be725ae708391b1d6083e9d0fd67")
-setManifestid(229626,"1462684019205755272")
-addappid(229627,0,"05fe47e8229993a4957c546b6b11f34f46e75c4344ec653a896e52fcfb8d4343")
-setManifestid(229627,"6146425481277420255")
-addappid(229628,0,"deda58ec7e09d693118e91f904d7e25e08198a746dfe30f5a988ade2caf96dbb")
-setManifestid(229628,"1091544035272724112")
-addappid(229629,0,"c9a80ed80166abc5061fe93f2ca57b2a013075300639e2a7963909215b3cdc57")
-setManifestid(229629,"7610772433233583544")
-addappid(229630,0,"c338077d7ee7ef5a03646b62ae08290d05705e25da98b945e75cc86b5c3da7a3")
-setManifestid(229630,"8821996626474824228")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 222480.lua
+--Gamename Resident Evil Revelations
+addappid(222480, 1, "bd3995e117f1006814935294b80f9cc70b60a3bb40cc0144742f3a2480432298") --Mainappid Resident Evil Revelations
+addappid(222481, 1, "65e75d906613ce658af73b8db0772cf9f6fa3905427161dd1c0482572da72c0d") --Main Windows Depot Resident Evil Revelations
+setManifestid(222481, "2624313053854726936", 7745917872)
+--Dlcname RE Rev. / BH REV. UE: Upgrade Parts: "Resistance Set"
+addtoken(229621, "233459904209267960")
+addappid(229621, 1, "cd908e40d47ae228e942070e0e6e9dd87d874f16434488b0b6d70091915ec999") --Dlcname RE Rev. / BH REV. UE: Upgrade Parts: "Resistance Set"
+setManifestid(229621, "1729720183785589076", 560)
+--Dlcname RE Rev. / BH REV. UE: Upgrade Parts: "Enhancement Set"
+addtoken(229622, "16981799673173157394")
+addappid(229622, 1, "ae8ea828b9a2c97510d5968ce55a9dae3a04346865801ac4757d7cd7ff106d9d") --Dlcname RE Rev. / BH REV. UE: Upgrade Parts: "Enhancement Set"
+setManifestid(229622, "5762540738354433494", 560)
+--Dlcname RE Rev. / BH REV. UE: RAID Outfit: "LADY HUNK"
+addappid(229626, 1, "1badd29cc032ee6227834ed10f78d0f4e785be725ae708391b1d6083e9d0fd67") --Dlcname RE Rev. / BH REV. UE: RAID Outfit: "LADY HUNK"
+setManifestid(229626, "1462684019205755272", 464)
+--Dlcname RE Rev. / BH REV. UE: RAID Outfit: "Rachael Ooze"
+addappid(229627, 1, "05fe47e8229993a4957c546b6b11f34f46e75c4344ec653a896e52fcfb8d4343") --Dlcname RE Rev. / BH REV. UE: RAID Outfit: "Rachael Ooze"
+setManifestid(229627, "6146425481277420255", 464)
+--Dlcname AppID 229628
+addtoken(229628, "15453805459233408151")
+addappid(229628, 1, "deda58ec7e09d693118e91f904d7e25e08198a746dfe30f5a988ade2caf96dbb") --Dlcname AppID 229628
+setManifestid(229628, "1091544035272724112", 480)
+--Dlcname RE Rev. / BH REV. UE: Weapon: Jessica's G18 + Custom Part: "BSAA"
+addappid(229629, 1, "c9a80ed80166abc5061fe93f2ca57b2a013075300639e2a7963909215b3cdc57") --Dlcname RE Rev. / BH REV. UE: Weapon: Jessica's G18 + Custom Part: "BSAA"
+setManifestid(229629, "7610772433233583544", 480)
+--Dlcname RE Rev. / BH REV. UE: Weapon: Jill's Samurai Edge + Custom Part: "S.T.A.R.S."
+addappid(229630, 1, "c338077d7ee7ef5a03646b62ae08290d05705e25da98b945e75cc86b5c3da7a3") --Dlcname RE Rev. / BH REV. UE: Weapon: Jill's Samurai Edge + Custom Part: "S.T.A.R.S."
+setManifestid(229630, "8821996626474824228", 480)
+--Dlcname AppID 229631
+addtoken(229631, "15841673125361043595")
+addappid(229631, 1, "1d5f2f41d6041f72b150c752b7ecf1ddd7d9db77a0c983831b6803105ebd962c") --Dlcname AppID 229631
+setManifestid(229631, "2800660586458870138", 72203632)
+--Dlcname AppID 229632
+addtoken(229632, "8947430856517089733")
+addappid(229632, 1, "31b56ec2f11495352b66ad07ea9df0f95cadec7a23235f6779c25899d192d306") --Dlcname AppID 229632
+setManifestid(229632, "5610201632971379408", 97512512)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
