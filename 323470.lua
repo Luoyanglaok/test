fcf278dd30de6eb72a736b9598429401f5c00195
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 12:03:34 +0800

    add 323470.lua

--- 323470.lua
+++ 323470.lua
@@ -1,7 +1,21 @@
-addappid(323470)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(323471,0,"a92b27f33a671ba1ff67c80c0508d6f8d276ac2936d558fba95d8dc83e5b0765")
-setManifestid(323471,"3461626906485904206")
-addappid(323473,0,"a9fc74b57e30b5d14ca436ebad3a4136d68941ba504102597afbb6410ec4c983")
-setManifestid(323473,"2822964839275909432")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 323470.lua
+--Gamename DRAGON BALL XENOVERSE
+addappid(323470, 1, "0d9ac610662fbabdda7ae300e17719657ad7ad73e5e7ecf049c2dcb9ac9d8d35") --Mainappid DRAGON BALL XENOVERSE
+addappid(323471, 1, "a92b27f33a671ba1ff67c80c0508d6f8d276ac2936d558fba95d8dc83e5b0765") --Main Windows Depot DRAGON BALL XENOVERSE
+setManifestid(323471, "3461626906485904206", 9141862032)
+addappid(323473, 1, "a9fc74b57e30b5d14ca436ebad3a4136d68941ba504102597afbb6410ec4c983") --Main Windows Depot DRAGON BALL XENOVERSE
+setManifestid(323473, "2822964839275909432", 368977648)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(344460) --Dlcname AppID 344460
+addtoken(344460, "13138949577396548177")
+addappid(344461) --Dlcname DRAGON BALL XENOVERSE GT Pack 1
+addappid(344462) --Dlcname DRAGON BALL XENOVERSE GT Pack 2
+addappid(344463) --Dlcname DRAGON BALL XENOVERSE Resurrection ‘F’ pack
+addappid(362600) --Dlcname DRAGON BALL XENOVERSE MOVIE COSTUMES PACK
+--Missing Dlcs
+--Dlcname DRAGON BALL XENOVERSE Pre-order DLC
