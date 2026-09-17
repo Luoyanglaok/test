9ab4ce0d72c309f01ad8e9dcd199120f568c0766
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 10:43:40 +0800

    add 4474950.lua

---
+++ 4474950.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4474950.lua
+--Gamename LIFE EFFECT Survival
+addappid(4474950) --Mainappid LIFE EFFECT Survival
+addappid(4474951, 1, "bb0990c8d7c6374fed27185017b78b78baebb1c20dafd9047406396cd5c4b3ba") --Main Windows Depot LIFE EFFECT Survival
+setManifestid(4474951, "7701864833599887305", 21051511056)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
