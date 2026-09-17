7a349b03b09e5a640ba9ba5d80b5f2cc0dbef752
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 22:36:10 +0800

    add 1151640.lua

--- 1151640.lua
+++ 1151640.lua
@@ -1,8 +1,16 @@
-addappid(1151640)
-setManifestid(228988,"6645201662696499616")
-addappid(1151641,0,"df518a946513a91a56704336384864918ec740df8537ca1812a8f54264355361")
-setManifestid(1151641,"3712644444013817231")
-addappid(1151642,0,"59255c4691e86b0813ddced2314be6ac433c80eacb80b7d716c1cc76ca2032f2")
-setManifestid(1151642,"6403385661621685335")
-addappid(1151644,0,"8a4848c62e8fcd4b79f78195190e4595e6c74b9a81ebe42b60e82bdd9b4b5c17")
-setManifestid(1151644,"7685822332590815529")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1151640.lua
+--Gamename Horizon Zero Dawn™ Complete Edition
+addappid(1151640, 1, "d20e63b8454c95af57e7753bd6816924c08f19ca062022275e9b37f0297d0c79") --Mainappid Horizon Zero Dawn™ Complete Edition
+addappid(1151641, 1, "df518a946513a91a56704336384864918ec740df8537ca1812a8f54264355361") --Main Windows Depot Horizon Zero Dawn™ Complete Edition
+setManifestid(1151641, "3712644444013817231", 58041532464)
+addappid(1151642, 1, "59255c4691e86b0813ddced2314be6ac433c80eacb80b7d716c1cc76ca2032f2") --Main Windows Depot Horizon Zero Dawn™ Complete Edition
+setManifestid(1151642, "6403385661621685335", 15632603936)
+addappid(1151644, 1, "8a4848c62e8fcd4b79f78195190e4595e6c74b9a81ebe42b60e82bdd9b4b5c17") --Main Windows Depot Horizon Zero Dawn™ Complete Edition
+setManifestid(1151644, "7685822332590815529", 17202828752)
+--Dlcname HZD Comic
+addtoken(2107510, "17624319664149755126")
+addappid(2107510, 1, "3b0af1076eca77b7becd91fffc4698281fef99bc06ba98c1dd31f11aa63b5f8c") --Dlcname HZD Comic
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
