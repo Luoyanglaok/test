562146ab2e273d533fa1068effaf639511408500
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 22:37:28 +0800

    add 1677280.lua

--- 1677280.lua
+++ 1677280.lua
@@ -1,10 +1,27 @@
-addappid(1677280)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229006,"1784011429307107530")
-addappid(1677281,0,"946c1091854e4b36a1468dea11adb8fe4fe6db6485976132a50781a70d5a206e")
-setManifestid(1677281,"7693589805437791454")
-addappid(1677282,0,"2dbe642c09395dcb0cfab68ff34eca066c3f47e38d8511d59509fb60cf7c665a")
-setManifestid(1677282,"8161104974131144885")
-addappid(1677283,0,"532412e59c494bd36e8efc9d4e257208e7412c8a70b16f206103f3913d4add50")
-setManifestid(1677283,"1956072617122484754")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1677280.lua
+--Gamename Company of Heroes 3
+addappid(1677280, 1, "3bc2162aacc5a2b9978b30069dffbbd4e0dcedfde3b7db80768525428e963600") --Mainappid Company of Heroes 3
+addappid(1677281, 1, "946c1091854e4b36a1468dea11adb8fe4fe6db6485976132a50781a70d5a206e") --Main Windows Depot Company of Heroes 3
+setManifestid(1677281, "8872311503752965071", 28242424112)
+addappid(1677282, 1, "2dbe642c09395dcb0cfab68ff34eca066c3f47e38d8511d59509fb60cf7c665a") --Main Windows Depot Company of Heroes 3
+setManifestid(1677282, "83851306951263697", 1008)
+addappid(1677283, 1, "532412e59c494bd36e8efc9d4e257208e7412c8a70b16f206103f3913d4add50") --Main Windows Depot Company of Heroes 3
+setManifestid(1677283, "6058592591375980183", 3504)
+addappid(1677289, 1, "6c1e1a6d270910f7b11c528abd36479749f01a68ac03f86cf3c7c56f036eb450") --Main Windows Depot Company of Heroes 3
+setManifestid(1677289, "9152350091812695147", 528)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(2068340) --Dlcname Company of Heroes 3 - Pre Order Content
+addappid(2068341) --Dlcname Company of Heroes 3 - Premium Content
+addappid(2068342) --Dlcname Company of Heroes 3: Hammer & Shield Expansion Pack
+addappid(3300460) --Dlcname Company of Heroes 3: Stealth & Stronghold
+addappid(3469800) --Dlcname Company of Heroes 3: Fire & Steel
+addappid(4095900) --Dlcname Company of Heroes 3: Endure & Defy
+addappid(4523180) --Dlcname Company of Heroes 3: Hammer & Shield Battlegroup DLC
+addappid(4523380) --Dlcname Company of Heroes 3: Dare & Destroy
