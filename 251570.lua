7517736b85ef2e4916ead48c3b7d6a0397d18106
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 13:06:27 +0800

    add 251570.lua

--- 251570.lua
+++ 251570.lua
@@ -1,12 +1,37 @@
-addappid(251570)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-addappid(251576,0,"525d3fda8d2195cf5a8bd10c0c158513d9f870f99a036830287c1465c73c6979")
-setManifestid(251576,"5246245990142249054")
-addappid(251577,0,"53a3478a67db1f8ed8030e845ec1be174a60cea2448401ffd4dda0c4838cbb84")
-setManifestid(251577,"3218769120542899578")
-addappid(251578,0,"4550672ef4a498246530208707b36b490dd3bb73c6b54ba6b832dd48db88f71a")
-setManifestid(251578,"8773487232304353634")
-addappid(251571,0,"2787b64cd24c9e42a114df4d2559813dc0f63405faa2f7e41cc77e6c3cc67bd0")
-setManifestid(251571,"8628486239191323866")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 251570.lua
+--Gamename 7 Days to Die
+addappid(251570, 1, "e53177a4e7b3979bb3284352de1dbcf65f8d3b1b57d5d63971906f5b75c53c86") --Mainappid 7 Days to Die
+addappid(251571, 1, "2787b64cd24c9e42a114df4d2559813dc0f63405faa2f7e41cc77e6c3cc67bd0") --Main Windows Depot 7 Days to Die
+setManifestid(251571, "8628486239191323866", 0)
+addappid(251576, 1, "525d3fda8d2195cf5a8bd10c0c158513d9f870f99a036830287c1465c73c6979") --Main Windows Depot 7 Days to Die
+setManifestid(251576, "369722652848853089", 16359308944)
+addappid(251577, 1, "53a3478a67db1f8ed8030e845ec1be174a60cea2448401ffd4dda0c4838cbb84") --Main Macos Depot 7 Days to Die
+setManifestid(251577, "1155325717857946076", 16251001312)
+addappid(251578, 1, "4550672ef4a498246530208707b36b490dd3bb73c6b54ba6b832dd48db88f71a") --Main Linux Depot 7 Days to Die
+setManifestid(251578, "6197483310039483353", 16333572416)
+--Dlcname 7 Days to Die - Soundtrack
+addappid(2721510) --Dlcname 7 Days to Die - Soundtrack
+addappid(2721511, 1, "41d26a497295b9a2272150763884d4ade69473388e6fa3eb1d62270127c1fe18") --Dlc Windows Depot 7 Days to Die - Soundtrack
+setManifestid(2721511, "5342576852196769776", 232732624)
+addappid(2721512, 1, "2e2cff02218afbdbac08aa587c8fd5a9b82ea9a266eb3953620e6cfa67abcdbe") --Dlc Windows Depot 7 Days to Die - Soundtrack
+setManifestid(2721512, "3713042900877315858", 470360288)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3486400) --Dlcname The Marauder Armor Set
+addappid(3635260) --Dlcname The Desert Armor Set
+addappid(4206290) --Dlcname The Classic Survivor Armor Set
+addappid(4234560) --Dlcname The Butcher Armor Set
+addappid(4234570) --Dlcname The Hellreaver Armor Set
+addappid(4234580) --Dlcname The Holiday Hat Pack
+addappid(4234590) --Dlcname The Jolly Raider Armor Set
+addappid(4767590) --Dlcname The Beach Armor Set
+addappid(4767600) --Dlcname Working Stiff Armor Set
+addappid(4924490) --Dlcname The Big Beak Armor Set
+--Missing Dlcs
+--Dlcname The Hoarder Armor Set
