249f36f7d4571a7e7a424cd5027b82229afc7d43
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 12:02:31 +0800

    add 678950.lua

--- 678950.lua
+++ 678950.lua
@@ -1,5 +1,52 @@
-addappid(678950)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(678951,0,"cd01842cde6ddf8bae5ce50cdaa221bf46f81a94091514fad68425f49bbb028d")
-setManifestid(678951,"8079912243422982324")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 678950.lua
+--Gamename DRAGON BALL FighterZ
+addappid(678950, 1, "2b86e8c2ba07861abfdc8079b82fd79a287244f6d90e81e425c01dda2a640832") --Mainappid DRAGON BALL FighterZ
+addappid(678951, 1, "cd01842cde6ddf8bae5ce50cdaa221bf46f81a94091514fad68425f49bbb028d") --Main Windows Depot DRAGON BALL FighterZ
+setManifestid(678951, "852949778273295123", 6290678048)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(729450) --Dlcname DRAGON BALL FighterZ - FighterZ Pass
+addappid(758770) --Dlcname DRAGON BALL FighterZ - SSGSS Goku and SSGSS Vegeta Unlock
+addappid(758771) --Dlcname DRAGON BALL FighterZ - SSGSS Lobby Avatars
+addappid(758772) --Dlcname AppID 758772
+addtoken(758772, "17417027749599668078")
+addappid(758774) --Dlcname AppID 758774
+addtoken(758774, "13532697273787985900")
+addappid(758780) --Dlcname AppID 758780
+addtoken(758780, "17464801751033731459")
+addappid(779930) --Dlcname DRAGON BALL FighterZ - Anime Music Pack
+addappid(779931) --Dlcname DRAGON BALL FighterZ - Commentator Voice Pack
+addappid(779932) --Dlcname DRAGON BALL FighterZ - Bardock
+addappid(779933) --Dlcname DRAGON BALL FighterZ - Broly
+addappid(779934) --Dlcname DRAGON BALL FighterZ - Zamasu (Fused)
+addappid(779935) --Dlcname DRAGON BALL FighterZ - Vegito (SSGSS)
+addappid(779936) --Dlcname DRAGON BALL FighterZ - Goku
+addappid(779937) --Dlcname DRAGON BALL FighterZ - Vegeta
+addappid(779938) --Dlcname DRAGON BALL FighterZ - Android 17
+addappid(779939) --Dlcname DRAGON BALL FighterZ - Cooler
+addappid(779940) --Dlcname AppID 779940
+addtoken(779940, "5728032048401986557")
+addappid(952080) --Dlcname DRAGON BALL FIGHTERZ – Anime Music Pack 2
+addappid(992170) --Dlcname DRAGON BALL FIGHTERZ - Jiren
+addappid(992171) --Dlcname DRAGON BALL FIGHTERZ - Videl
+addappid(992172) --Dlcname DRAGON BALL FighterZ - Goku (GT)
+addappid(992173) --Dlcname DRAGON BALL FIGHTERZ - Janemba
+addappid(992174) --Dlcname DRAGON BALL FIGHTERZ - Gogeta (SSGSS)
+addappid(992175) --Dlcname DRAGON BALL FIGHTERZ - Broly (DBS)
+addappid(992176) --Dlcname DRAGON BALL FIGHTERZ - FighterZ Pass 2
+addappid(1020570) --Dlcname DRAGON BALL FighterZ - Commentator Voice Pack 2
+addappid(1020571) --Dlcname DRAGON BALL FighterZ - Commentator Voice Pack 3
+addappid(1020572) --Dlcname DRAGON BALL FighterZ - Commentator Voice Pack 4
+addappid(1209880) --Dlcname AppID 1209880
+addtoken(1209880, "13997769959332540213")
+addappid(1248430) --Dlcname DRAGON BALL FIGHTERZ - Goku (Ultra Instinct)
+addappid(1248431) --Dlcname DRAGON BALL FIGHTERZ - Master Roshi
+addappid(1248432) --Dlcname DRAGON BALL FIGHTERZ - Gogeta (SS4)
+addappid(1248433) --Dlcname DRAGON BALL FIGHTERZ - Super Baby 2
+addappid(1248434) --Dlcname DRAGON BALL FIGHTERZ - FighterZ Pass 3
+addappid(1744780) --Dlcname DRAGON BALL FIGHTERZ - Android 21 (Lab Coat)
+addappid(4179010) --Dlcname DRAGON BALL FighterZ - DAIMA Pack
