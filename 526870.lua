14810b5506cd25514b281cea96509f43063355b9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 13:21:20 +0800

    add 526870.lua

--- 526870.lua
+++ 526870.lua
@@ -1,4 +1,16 @@
-addappid(526870)
-setManifestid(228988,"6645201662696499616")
-addappid(526871,0,"ae0ab673da3c9d0f444b1cd61fe8f2235d43e6c0401684a78f4fbdfc8f0d9bc1")
-setManifestid(526871,"4569623158540465987")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 526870.lua
+--Gamename Satisfactory
+addappid(526870, 1, "f8d1243ead37652491a8d284a79ee2b814ad878208ec9276f3d5d8b1528a11e7") --Mainappid Satisfactory
+addappid(526871, 1, "ae0ab673da3c9d0f444b1cd61fe8f2235d43e6c0401684a78f4fbdfc8f0d9bc1") --Main Windows Depot Satisfactory
+setManifestid(526871, "4522661880264054134", 13018822112)
+--Dlcname Satisfactory Soundtrack
+addappid(2873550) --Dlcname Satisfactory Soundtrack
+addappid(2873551, 1, "97dcf4cb06c5be6b0f4831f66bd0dadcfb94a829f8faa423f99e65ead70937c0") --Dlc Windows Depot Satisfactory Soundtrack
+setManifestid(2873551, "7665396372579316180", 451183856)
+addappid(2873552, 1, "016194001abfe403556a728afdf0ce0db0916f34f6fe78789e08f91910c27fb0") --Dlc Windows Depot Satisfactory Soundtrack
+setManifestid(2873552, "7578872653658177168", 1101065472)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3170070) --Dlcname Early Access supporter pack
