25edeac5b7867db2516c95b5c8af2999d35ea763
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 15:25:17 +0800

    add 2357570.lua

--- 2357570.lua
+++ 2357570.lua
@@ -1,35 +1,108 @@
-addappid(2357570)
-addappid(2357571,0,"9aceefd997458ce3f273f21e60d2859c34098efaab9650f9a2496ba845284dfc")
-setManifestid(2357571,"9159345061246005502")
-addappid(2357572,0,"027d6fdfe9cfcf0517776f8eaef6daa4456b7736492196a9413dc92b0f3dd3b5")
-setManifestid(2357572,"3327224823579225774")
-addappid(2357573,0,"dcb477c8eb68b6a67f5f165f64d15359098c232a02da45fd33d31f131134edd9")
-setManifestid(2357573,"658657830278371543")
-addappid(2357574,0,"2913db079c91572b2647d9d35e0411af7d7e24c5fb3d2a9fded9d5bd42aaa22a")
-setManifestid(2357574,"3690638304696572158")
-addappid(2357575,0,"ab9543bae056a3b1fcb6f4084862d3f34d67e4926853044c7e6d978ff521cff1")
-setManifestid(2357575,"396175242996834812")
-addappid(2357576,0,"9eb3f725cc4212fe268441ae4feebf992a75de060eede3811d41308d4002b6c2")
-setManifestid(2357576,"3567474119551949279")
-addappid(2357577,0,"f4a960b56fa1ce855e8997d364386b9de85af9a4fff0259995cd1010457af544")
-setManifestid(2357577,"373391267668338628")
-addappid(2357578,0,"6e141c49f494006f38c3c884adf40a3d4bb6205045ebf1f2c96f94918610a86f")
-setManifestid(2357578,"5104278352850858591")
-addappid(2357579,0,"9466914bed29f6ef13c9305f2fb084b82b60db755d9aff0909264b5523a8eda1")
-setManifestid(2357579,"3292737308701363819")
-addappid(2437281,0,"84ab2cf7a51aa7d4841d0caadd0bfcc1771ae0dc8d3c8a2bacfe5b5b7c7bbc72")
-setManifestid(2437281,"1983617556059008866")
-addappid(2437282,0,"f59297fec578e7da993328cc73a343393f0b401024b1c80b0749b80006cc6496")
-setManifestid(2437282,"4435500679110539867")
-addappid(2437283,0,"e689de82e8f6c9ea07c3b9c07d08a995b3c145b5a067e9b108817b9a79fe302e")
-setManifestid(2437283,"4996608359801623395")
-addappid(2437284,0,"c6f6eec0dcb29cc3c6c27a2d2838fb1b52146884817a177225d73c2d6012c087")
-setManifestid(2437284,"2031921695376890947")
-addappid(2437285,0,"bbf587ccff5fda185651761c457dbbd2768c6a1e6b90522d140723ee113b4c0f")
-setManifestid(2437285,"4794202431314884053")
-addappid(2437286,0,"46126633906fbd8aa4fdb76e4b1b72a30ebaca33a51604e0b40b2b63773ecf00")
-setManifestid(2437286,"2411051575211455076")
-addappid(2437287,0,"1c868a41bb55eeed2f1d3987bcc0a036d507855aff0c68e0c1e09375d01f1a00")
-setManifestid(2437287,"2717258968098960513")
-addappid(2437288,0,"f7d2f55ca1724c78a7c8703d21ea4fafa1bdc2bfc7f04a206d1f040536d6b3fa")
-setManifestid(2437288,"5126589583594333931")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2357570.lua
+--Gamename Overwatch®
+addappid(2357570, 1, "baa66da75a894da3328c6e2a1fe5d01a980e016001bffe2753e04ec81ae4e8ba") --Mainappid Overwatch®
+addappid(2357571, 1, "9aceefd997458ce3f273f21e60d2859c34098efaab9650f9a2496ba845284dfc") --Main Windows Depot Overwatch®
+setManifestid(2357571, "6549880795388805757", 77770307472)
+addappid(2357572, 1, "027d6fdfe9cfcf0517776f8eaef6daa4456b7736492196a9413dc92b0f3dd3b5") --Main Windows Language Depot English Overwatch®
+setManifestid(2357572, "8534450263849644237", 1350345392)
+addappid(2357573, 1, "dcb477c8eb68b6a67f5f165f64d15359098c232a02da45fd33d31f131134edd9") --Main Windows Language Depot Spanish Overwatch®
+setManifestid(2357573, "7153466010516393637", 1557522080)
+addappid(2357574, 1, "2913db079c91572b2647d9d35e0411af7d7e24c5fb3d2a9fded9d5bd42aaa22a") --Main Windows Language Depot Latam Overwatch®
+setManifestid(2357574, "257325868172992212", 1542300784)
+addappid(2357575, 1, "ab9543bae056a3b1fcb6f4084862d3f34d67e4926853044c7e6d978ff521cff1") --Main Windows Language Depot Italian Overwatch®
+setManifestid(2357575, "7648260659241861154", 1486575504)
+addappid(2357576, 1, "9eb3f725cc4212fe268441ae4feebf992a75de060eede3811d41308d4002b6c2") --Main Windows Language Depot Russian Overwatch®
+setManifestid(2357576, "8364510359803059233", 1517486976)
+addappid(2357577, 1, "f4a960b56fa1ce855e8997d364386b9de85af9a4fff0259995cd1010457af544") --Main Windows Language Depot Polish Overwatch®
+setManifestid(2357577, "7160040570026963932", 1541080080)
+addappid(2357578, 1, "6e141c49f494006f38c3c884adf40a3d4bb6205045ebf1f2c96f94918610a86f") --Main Windows Language Depot French Overwatch®
+setManifestid(2357578, "5593084454397002451", 1416628992)
+addappid(2357579, 1, "9466914bed29f6ef13c9305f2fb084b82b60db755d9aff0909264b5523a8eda1") --Main Windows Language Depot German Overwatch®
+setManifestid(2357579, "8012765844200538197", 1537683008)
+addappid(2437281, 1, "84ab2cf7a51aa7d4841d0caadd0bfcc1771ae0dc8d3c8a2bacfe5b5b7c7bbc72") --Main Windows Language Depot Brazilian Overwatch®
+setManifestid(2437281, "611086037797477758", 1473835568)
+addappid(2437282, 1, "f59297fec578e7da993328cc73a343393f0b401024b1c80b0749b80006cc6496") --Main Windows Language Depot Koreana Overwatch®
+setManifestid(2437282, "8853590106640647244", 1536763536)
+addappid(2437283, 1, "e689de82e8f6c9ea07c3b9c07d08a995b3c145b5a067e9b108817b9a79fe302e") --Main Windows Language Depot Tchinese Overwatch®
+setManifestid(2437283, "4234358194730374775", 1448378112)
+addappid(2437284, 1, "c6f6eec0dcb29cc3c6c27a2d2838fb1b52146884817a177225d73c2d6012c087") --Main Windows Language Depot Schinese Overwatch®
+setManifestid(2437284, "5836817185850100911", 1386623664)
+addappid(2437285, 1, "bbf587ccff5fda185651761c457dbbd2768c6a1e6b90522d140723ee113b4c0f") --Main Windows Language Depot Japanese Overwatch®
+setManifestid(2437285, "7745071869806717346", 1404706752)
+addappid(2437286, 1, "46126633906fbd8aa4fdb76e4b1b72a30ebaca33a51604e0b40b2b63773ecf00") --Main Windows Language Depot Turkish Overwatch®
+setManifestid(2437286, "7358751700729590272", 1350380016)
+addappid(2437287, 1, "1c868a41bb55eeed2f1d3987bcc0a036d507855aff0c68e0c1e09375d01f1a00") --Main Windows Language Depot Thai Overwatch®
+setManifestid(2437287, "2084284257028197250", 1350367696)
+addappid(2437288, 1, "f7d2f55ca1724c78a7c8703d21ea4fafa1bdc2bfc7f04a206d1f040536d6b3fa") --Main Windows Depot Overwatch®
+setManifestid(2437288, "2732825187149699374", 322152288)
+addappid(2437280) --Dlcname Overwatch® - Invasion Bundle
+addappid(2443730) --Dlcname AppID 2443730
+addappid(2443731) --Dlcname AppID 2443731
+addappid(2443732) --Dlcname AppID 2443732
+addappid(2443733) --Dlcname Overwatch® 2 - Complete Hero Collection
+addappid(2443734) --Dlcname Overwatch® 2: Complete Hero Collection Upgrade
+addappid(2528660) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season Seven
+addappid(2528670) --Dlcname AppID 2528670
+addappid(2584720) --Dlcname AppID 2584720
+addappid(2584730) --Dlcname Overwatch® 2 - 10000 (+5000 Bonus) Overwatch Coins - Limited Time!
+addappid(2618570) --Dlcname AppID 2618570
+addappid(2618580) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 8
+addappid(2707800) --Dlcname AppID 2707800
+addappid(2809330) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 10
+addappid(2923060) --Dlcname AppID 2923060
+addappid(2923070) --Dlcname Overwatch® 2 Starter Pack: Season 11
+addappid(2961080) --Dlcname Overwatch® 2: Pink Mercy Skin
+addappid(2961090) --Dlcname AppID 2961090
+addappid(2977970) --Dlcname Overwatch® 2: Reinhardt Mythic Weapon Skin Bundle
+addappid(3031620) --Dlcname Overwatch® 2 - 5000 (+2500 Bonus) Overwatch Coins - Limited Time!
+addappid(3031630) --Dlcname Overwatch® 2 - 10000 (+5000 Bonus) Overwatch Coins - Limited Time!
+addappid(3039790) --Dlcname AppID 3039790
+addappid(3039800) --Dlcname Overwatch® 2 Starter Pack: Season 12
+addappid(3039810) --Dlcname AppID 3039810
+addappid(3157030) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 13
+addappid(3157040) --Dlcname Overwatch® 2 Starter Pack: Season 13
+addappid(3157050) --Dlcname AppID 3157050
+addappid(3259290) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 14
+addappid(3259300) --Dlcname Overwatch® 2 Starter Pack: Season 14
+addappid(3259310) --Dlcname Overwatch® 2: Ashe Complete Mythic Weapon Skin Bundle
+addappid(3317060) --Dlcname Overwatch® 2: DPS Essentials Bundle
+addappid(3317070) --Dlcname Overwatch® 2: Tank Essentials Bundle
+addappid(3317080) --Dlcname Overwatch® 2: Support Essentials Bundle
+addappid(3364660) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 15
+addappid(3364670) --Dlcname AppID 3364670
+addappid(3364680) --Dlcname AppID 3364680
+addappid(3526660) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 16
+addappid(3526670) --Dlcname Overwatch® 2 Starter Pack: Season 16
+addappid(3526700) --Dlcname Overwatch® 2: Mercy Complete Mythic Weapon Skin Bundle
+addappid(3665170) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 17
+addappid(3665190) --Dlcname Overwatch® 2 Starter Pack: Season 17
+addappid(3665200) --Dlcname Overwatch® 2: Reaper Complete Mythic Weapon Skin Bundle
+addappid(3813020) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 18
+addappid(3813030) --Dlcname Overwatch® 2 Starter Pack: Season 18
+addappid(3819870) --Dlcname Overwatch® 2: Cassidy Complete Mythic Weapon Skin Bundle
+addappid(3943050) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 19
+addappid(3943060) --Dlcname Overwatch® 2 Starter Pack: Season 19
+addappid(4044330) --Dlcname Overwatch® 2 - Ultimate Battle Pass Bundle: Season 20
+addappid(4044340) --Dlcname Overwatch® 2 Starter Pack: Season 20
+addappid(4186050) --Dlcname Overwatch® - Ultimate Battle Pass Bundle 2026: Season 1
+addappid(4186060) --Dlcname Overwatch® Starter Pack 2026: Season 1
+addappid(4186070) --Dlcname Overwatch®: Juno Complete Mythic Weapon Skin Bundle
+addappid(4186080) --Dlcname Overwatch®: Lunar New Year 2026 Value Bundle
+addappid(4374980) --Dlcname Overwatch® - Ultimate Battle Pass Bundle 2026: Season 2
+addappid(4375000) --Dlcname Overwatch® Starter Pack 2026: Season 2
+addappid(4375010) --Dlcname Overwatch®: Genji Complete Mythic Weapon Skin Bundle
+addappid(4485430) --Dlcname Overwatch®: Ana Complete Mythic Hero Skin Bundle
+addappid(4586820) --Dlcname Overwatch® - Ultimate Battle Pass Bundle 2026: Season 3
+addappid(4586830) --Dlcname Overwatch® Starter Pack 2026: Season 3
+addappid(4586840) --Dlcname Overwatch®: Hanzo Complete Mythic Weapon Skin Bundle
+addappid(4586850) --Dlcname Overwatch® BlizzCon® Bundle
+addappid(4586860) --Dlcname Overwatch®: Mauga Complete Mythic Hero Skin Bundle
+addappid(4815770) --Dlcname Overwatch® - Ultimate Battle Pass Bundle 2026: Season 4
+addappid(4815780) --Dlcname Overwatch® Starter Pack 2026: Season 4
+addappid(4815790) --Dlcname Overwatch®: Sojourn Complete Mythic Weapon Skin Bundle
+addappid(4815800) --Dlcname Overwatch®: Widowmaker Complete Mythic Hero Skin Bundle
+addappid(4921050) --Dlcname Overwatch®: Sweet Brigitte Bundle
+addappid(4921060) --Dlcname Overwatch®: Spooky Winston Bundle
+addappid(4921070) --Dlcname Overwatch®: Starter Pack Collection
+addappid(4921080) --Dlcname Overwatch®: Classic Divinity Mega Bundle
