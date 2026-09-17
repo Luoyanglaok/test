ee7795467b33282eccd1f632fada29a91e1d1d1b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 18:24:18 +0800

    add 3655390.lua

---
+++ 3655390.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3655390.lua
+--Gamename Alien: Rogue Incursion Evolved Edition
+addappid(3655390, 1, "8e6ece68a5f2d49b11d59b278f01baa1d3c2e644c72f9393e4ab5eceaf807d78") --Mainappid Alien: Rogue Incursion Evolved Edition
+addappid(3655391, 1, "4289857b0b55b53b17c6143a6727f746e475c76253e99cff492725e4ad9e3e13") --Main Windows Depot Alien: Rogue Incursion Evolved Edition
+setManifestid(3655391, "6110594964002655128", 20288245808)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3712120) --Dlcname Alien: Rogue Incursion - Evolved - Deluxe Content
+addtoken(3712120, "2021362082053022241")
+addappid(3712140) --Dlcname Alien: Rogue Incursion - Evolved - Preorder Content
+addtoken(3712140, "7891782928624123809")
