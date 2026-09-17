5edcf425ebdb275adecde785e027aa8f511f6890
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 19:50:41 +0800

    add 1272320.lua

--- 1272320.lua
+++ 1272320.lua
@@ -1,6 +1,16 @@
-addappid(1272320)
-addappid(5075270)
-addappid(2941670)
-addappid(2959690)
-addappid(1272321,1,"679b29b2f179a1a412570aecff5f975e3722b084a18733f963832690fcfd6005")
-setManifestid(1272321,"2640365391667109060")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1272320.lua
+--Gamename Diplomacy is Not an Option
+addappid(1272320, 1, "a6e1d14489efd06ce2c35a50e97f75a2c94eabec03dcc91536e1266ee2b1d8ce") --Mainappid Diplomacy is Not an Option
+addappid(1272321, 1, "679b29b2f179a1a412570aecff5f975e3722b084a18733f963832690fcfd6005") --Main Windows Depot Diplomacy is Not an Option
+setManifestid(1272321, "2640365391667109060", 4893515648)
+--Dlcname Diplomacy is Not an Option - Wallpapers
+addappid(2941670, 1, "bec3b6bade013e0df9876a4149398270a2ef588a15164b75a5c21fb2197029d0") --Dlcname Diplomacy is Not an Option - Wallpapers
+setManifestid(2941670, "3197767119738992087", 661341536)
+--Dlcname Diplomacy is Not an Option Soundtrack
+addappid(2959690) --Dlcname Diplomacy is Not an Option Soundtrack
+addappid(2959691, 1, "62339c047c81293d2047d98800c452851e401dad0cd49ab00901ff9486c73014") --Dlc Windows Depot Diplomacy is Not an Option Soundtrack
+setManifestid(2959691, "7457779569162959859", 169182640)
+addappid(2959692, 1, "49e095119ebb6bd6aed0cbf368116aa1e62d56973a7d7b3251bfef30f8ac8504") --Dlc Windows Depot Diplomacy is Not an Option Soundtrack
+setManifestid(2959692, "4519411563231151732", 994525696)
+addappid(5075270) --Dlcname Diplomacy is Not an Option: Fog of War
