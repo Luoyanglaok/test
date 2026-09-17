fbca3329b675265bb6cd17c338e952b493f09d6b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 22:13:17 +0800

    add 3451970.lua

---
+++ 3451970.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3451970.lua
+--Gamename BACKROOMS ANOMALY
+addappid(3451970) --Mainappid BACKROOMS ANOMALY
+addappid(3451971, 1, "6ca4f623e9a908c820bbdda506cff3d4d885efc92a041548849ddc8637127a4f") --Main Windows Depot BACKROOMS ANOMALY
+setManifestid(3451971, "944153701083361815", 1423735088)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4818320) --Dlcname Backrooms Anomaly - Supporter Pack
