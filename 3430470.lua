d69ddc8f65107d862f4a58dad5df5a005174fa1f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 21:50:58 +0800

    add 3430470.lua

--- 3430470.lua
+++ 3430470.lua
@@ -1,3 +1,21 @@
-addappid(3430470)
-addappid(3430471,0,"7f478c739cf9e5b4ccacc2527b6ad86e4bbfc13453783e9bf5db05fd5f371673")
-setManifestid(3430471,"8756833846835852240")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3430470.lua
+--Gamename PLATiNA :: LAB
+addappid(3430470, 1, "b8a27d1c17e32ae2fb9fb63aed48176f5fbbac4361bdd8a0816a9d386c9803f6") --Mainappid PLATiNA :: LAB
+addappid(3430471, 1, "7f478c739cf9e5b4ccacc2527b6ad86e4bbfc13453783e9bf5db05fd5f371673") --Main Windows Depot PLATiNA :: LAB
+setManifestid(3430471, "4028752266456623260", 12929318912)
+addappid(3605951, 1, "14ef1ecd5e7dfdd5d6d4193587f6f483c251853b76993244510c907c9c945ed6") --Main Windows Depot PLATiNA :: LAB
+setManifestid(3605951, "5382488171452293648", 363252944)
+addappid(3605952, 1, "494463164548a3ca774d8301b3bacea5a10362b8727460ac4c8af9632929d185") --Main Windows Depot PLATiNA :: LAB
+setManifestid(3605952, "5062803683148432", 1781119376)
+--Dlcname PLATiNA :: LAB - OST & AQUA BLUE OST
+addappid(3605950) --Dlcname PLATiNA :: LAB - OST & AQUA BLUE OST
+addappid(3900890) --Dlcname PLATiNA :: LAB - AQUA BLUE
+addappid(4143860) --Dlcname PLATiNA :: LAB - sasakure.UK x TJ.hangneil
+addappid(4340790) --Dlcname PLATiNA :: LAB - Zundamon「Songs」
+addappid(4340800) --Dlcname PLATiNA :: LAB - Zundamon「Character」
+addappid(4886890) --Dlcname PLATiNA :: LAB - SWEETIE;S
+addappid(5058960) --Dlcname PLATiNA :: LAB - FLAGSHIP Collection
+--Missing Dlcs
+--Dlcname PLATiNA :: LAB - SWEETIE;S OST
+--Dlcname PLATiNA :: LAB - ADDITIONAL OST
