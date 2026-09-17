7a607ad030308b5c26eae3b88022df8c1f891020
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 22:24:38 +0800

    add 2215200.lua

---
+++ 2215200.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2215200.lua
+--Gamename LEGO® Batman™: Legacy of the Dark Knight
+addappid(2215200, 1, "93ce128163332a2a5e65d676406007b5b04d781c12b900d5fc8b157d9517eede") --Mainappid LEGO® Batman™: Legacy of the Dark Knight
+addappid(2215201, 1, "5d2bafc1381c29ebabb37bbab50e496548be24f6bf5d511e8d471b99d60cfbf3") --Main Windows Depot LEGO® Batman™: Legacy of the Dark Knight
+setManifestid(2215201, "6457737506199836404", 46734091840)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4168610) --Dlcname The Dark Knight Returns Suit
+addtoken(4168610, "9867947645710704055")
+addappid(4168620) --Dlcname Arkham Trilogy Pack
+addtoken(4168620, "9614104586700303235")
+addappid(4168630) --Dlcname Batman Beyond Pack
+addtoken(4168630, "17750122315834084647")
+addappid(4168640) --Dlcname Party Music Pack
+addtoken(4168640, "10837524983767176435")
+addappid(4468750) --Dlcname LEGO® Batman™: Legacy of the Dark Knight Deluxe Edition Upgrade
