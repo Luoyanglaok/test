7573cbcfeb77533215aa7a36f587ca5f669ce293
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 13:08:42 +0800

    add 1956740.lua

--- 1956740.lua
+++ 1956740.lua
@@ -1,5 +1,13 @@
-addappid(1956740)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1956741,0,"3c35220723f93f124d644ef432bf32f526ff0d9a93d0fee435ec78b16f183c0a")
-setManifestid(1956741,"765090532333997966")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1956740.lua
+--Gamename Argentum Online
+addappid(1956740) --Mainappid Argentum Online
+addappid(1956741, 1, "3c35220723f93f124d644ef432bf32f526ff0d9a93d0fee435ec78b16f183c0a") --Main Windows Depot Argentum Online
+setManifestid(1956741, "3852309552821216957", 1268610480)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Argentum 20 Soundtrack
