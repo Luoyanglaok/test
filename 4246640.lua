8a47deddd79a01b6874c7a03459099e778c568fc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 11:47:57 +0800

    add 4246640.lua

--- 4246640.lua
+++ 4246640.lua
@@ -3,6 +3,6 @@
 --Gamename 地下城竞技场
 addappid(4246640, 1, "7d3012ec04087077f25a64d6cb4bb1515af0429361ce69baae0d4476c217f8d3") --Mainappid 地下城竞技场
 addappid(4246641, 1, "38a27266a1ba3a515e0f81afd6604545fc8493c1434a6dc17f3b4e45b836e23a") --Main Windows Depot 地下城竞技场
-setManifestid(4246641, "2081237400991284476", 1020066048)
+setManifestid(4246641, "390952085244190797", 1031192528)
 addappid(4817070) --Dlcname Dungeon Brawls - Supporter Pack
 addappid(4817080) --Dlcname Dungeon Brawls - Wings Pack
