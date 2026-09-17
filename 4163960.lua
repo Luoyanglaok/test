2bb888a2bc70733673d7066961b78db9373d4d3b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 13:35:33 +0800

    add 4163960.lua

---
+++ 4163960.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4163960.lua
+--Gamename Another Eden Begins
+addappid(4163960, 1, "101bf2e40b8ec56bb7c4fae2ac34c0b65b3501838b88fa0bb5091c7ae2de9926") --Mainappid Another Eden Begins
+addappid(4163961, 1, "7559dca7eb457a92f7f272c7fd8c9ea8a69f9e6ea3779eca98151285812832b7") --Main Windows Depot Another Eden Begins
+setManifestid(4163961, "7904816340797369361", 10413330480)
+addappid(4175530, 1, "d15d058c54a718df37052d2cefb650898835ce0526644caae614c6f6aa632242") --Main Windows Depot Another Eden Begins
+setManifestid(4175530, "1135978042187306955", 155912880)
+addappid(4175540, 1, "262a03e0716dea57b4a6972f04ed87c332f42032c9fb51edc5aa70f533c3fea9") --Main Windows Depot Another Eden Begins
+setManifestid(4175540, "344808300139560930", 140138912)
+--Dlcname Another Eden Begins - Character Pack Vol. 1: Thillelille & Tiramisu
+--Dlcname Another Eden Begins - Character Pack Vol. 2: Elseal & Benedict
+addappid(4923920) --Dlcname Another Eden Begins Season Pass
+--Missing Dlcs
+--Dlcname Another Eden Begins Playtest
