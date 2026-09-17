22f8e67c942a6d35b9b998c5b00aaffe360bf4bc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 13:35:54 +0800

    add 2572040.lua

---
+++ 2572040.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2572040.lua
+--Gamename Woodo
+addappid(2572040, 1, "cadd62bbd10331b149c4233a2714241006dec369d83f039e0321e8e997734be9") --Mainappid Woodo
+addappid(2572041, 1, "a8959d947a2817366f38298c03ab8221b823a712d0a952d901f76680cea3233b") --Main Windows Depot Woodo
+setManifestid(2572041, "4574250498666890266", 1210504080)
+addappid(4795611, 1, "76d787bf18dc88e2ec23c1265ee33d28f4c3006c9264c5167633bc5d4ae02656") --Main Windows Depot Woodo
+setManifestid(4795611, "5249958920025715259", 267971616)
+addappid(4795612, 1, "33d2e8b6f2e76291882952b82e7ab7f03cdbb0051b5a8f3cf0afcfa9293c7cf8") --Main Windows Depot Woodo
+setManifestid(4795612, "579421348445038891", 1167940560)
+--Dlcname Woodo - Supporter Pack
+addappid(4795600, 1, "8e5a554fc3fabc5b761e661c59efe00edfe468e9dd029a40fb6a082feb20ee33") --Dlcname Woodo - Supporter Pack
+setManifestid(4795600, "7295233514928971248", 571245856)
+--Dlcname Woodo - Soundtrack Pack
+addappid(4795610) --Dlcname Woodo - Soundtrack Pack
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
