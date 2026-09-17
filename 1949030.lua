b6efc5c9d0166477f3305303572108bf1813633e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 17:39:56 +0800

    add 1949030.lua

--- 1949030.lua
+++ 1949030.lua
@@ -1,12 +1,30 @@
-addappid(1949030)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1949031,0,"9d24669555d74ce46354104ac51cabb0d56b98f2be48de9c9eef2ff5b2a392cc")
-setManifestid(1949031,"1556170711472305898")
-addappid(1949032,0,"f5ed67a9eed700ec2032583ddac5767254f593873bcc340b7b30141eac7d1dc7")
-setManifestid(1949032,"9049401390664358768")
-addappid(2330100,0,"21dd9d672e685c3529683b7c95ec6e28aaae408ef532769282a39c17f9f0d43c")
-setManifestid(2330100,"3767737197284181840")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1949030.lua
+--Gamename Sherlock Holmes The Awakened
+addappid(1949030) --Mainappid Sherlock Holmes The Awakened
+addappid(1949031, 1, "9d24669555d74ce46354104ac51cabb0d56b98f2be48de9c9eef2ff5b2a392cc") --Main Windows Depot Sherlock Holmes The Awakened
+setManifestid(1949031, "1556170711472305898", 26658567184)
+addappid(1949032, 1, "f5ed67a9eed700ec2032583ddac5767254f593873bcc340b7b30141eac7d1dc7") --Main Windows Depot Sherlock Holmes The Awakened
+setManifestid(1949032, "9049401390664358768", 160107664)
+--Dlcname Sherlock Holmes The Awakened Soundtrack
+addappid(2330080) --Dlcname Sherlock Holmes The Awakened Soundtrack
+addappid(2330081, 1, "6a488df0b1fecc0d99123d5e4843a6c22cabb2fbf2e4c8d6209318f90dab2785") --Dlc Windows Depot Sherlock Holmes The Awakened Soundtrack
+setManifestid(2330081, "8738884563905880468", 248754736)
+addappid(2330082, 1, "076d7bdc12cdbcee771feb20ce20f72799a3f0b2a006f9fba62c306dd07f0156") --Dlc Windows Depot Sherlock Holmes The Awakened Soundtrack
+setManifestid(2330082, "5866912014003035058", 404616848)
+--Dlcname Sherlock Holmes The Awakened Artbook
+addappid(2330100, 1, "21dd9d672e685c3529683b7c95ec6e28aaae408ef532769282a39c17f9f0d43c") --Dlcname Sherlock Holmes The Awakened Artbook
+setManifestid(2330100, "3767737197284181840", 92989888)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2208700) --Dlcname Sherlock Holmes The Awakened - The Sleuth's Attire
+addappid(2208701) --Dlcname Sherlock Holmes The Awakened - The Whispered Dreams Side Quest Pack
