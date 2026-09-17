494a46aa50c1049028d601b6433063ac6bb8cd65
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 14:24:30 +0800

    add 3764200.lua

--- 3764200.lua
+++ 3764200.lua
@@ -1,33 +1,10 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 3764200.lua
---Gamename Resident Evil Requiem
-addappid(3764200, 1, "d29b7ad461b8b74dce38b7137fc00d4b5a885c5be1211767a4ecae0eaf8ddd96") --Mainappid Resident Evil Requiem
-addappid(3764201, 1, "bdf443ffde6449192b9863c0fa5e3cda31fdaa5e4d33e5bcded62dc2085b7cb8") --Main Windows Depot Resident Evil Requiem
-setManifestid(3764201, "2990234554016452841", 78360773568)
-addappid(3764202, 1, "3b6b7669a064598e8f48e38776d1862941f78d515a88b89333c2dbde0a07fcff") --Main Windows Depot Resident Evil Requiem
-setManifestid(3764202, "1782915813675562857", 78415401440)
-addappid(3764203, 1, "b2e820287e6e3f32d1ccb3f2f8c95ec3783e8c0a75abab5d311a0a648626fd92") --Main Windows Depot Resident Evil Requiem
-setManifestid(3764203, "1507402407513694495", 3119534720)
-addappid(3764204, 1, "5c0b40c6708cb9d8c0d0d780717a925508eb763c3a0c9bd74f6e76132785bc97") --Main Windows Depot Resident Evil Requiem
-setManifestid(3764204, "49224221358980643", 3206584128)
-addappid(4460243, 1, "3d9be12e9ef64f2554c99b8ec4aaf62efa30c1f254720f4d32f760e6b4af8d8b") --Main Windows Depot Resident Evil Requiem
-setManifestid(4460243, "6992659664139057986", 233522944)
-addappid(4460244, 1, "09854b8a7b7f643c28f451c16daa123a5eeb74091fbabb1c6b4cfd8550b798ff") --Main Windows Depot Resident Evil Requiem
-setManifestid(4460244, "5065662612253623127", 588824496)
---Dlcname Resident Evil Requiem - Grace's Costume: Apocalypse
-addappid(3990800, 1, "f5bf116706491176c9eee46a96fe1faa70b3f771d75ccb34f0c8962097a907e5") --Dlcname Resident Evil Requiem - Grace's Costume: Apocalypse
-addappid(3764205, 1, "25a058f4301da5d231a8e5ccdd648c2fbc5aa686cd4669012901b5f90e4ff1cb") --Dlc Windows Depot Resident Evil Requiem - Grace's Costume: Apocalypse
-setManifestid(3764205, "4436866528626191669", 154125248)
-setManifestid(3990800, "9181787779883827112", 154125168)
---Dlcname Resident Evil Requiem - Deluxe Kit
-addappid(3990820, 1, "9ae4713cb4114effdd34d1cd54dbda8a560f27a05aef06fbaa0370748f69d15c") --Dlcname Resident Evil Requiem - Deluxe Kit
-addappid(3764206, 1, "960c01ef80e38e1993ada744b48c5dd59eabf9de2726f222d579ad5c4160b10e") --Dlc Windows Depot Resident Evil Requiem - Deluxe Kit
-setManifestid(3764206, "136565968785594279", 959898864)
-setManifestid(3990820, "509708311968316285", 960907760)
---Dlcname Resident Evil Requiem Original Soundtrack
-addappid(4460240) --Dlcname Resident Evil Requiem Original Soundtrack
-addappid(4460241, 1, "8a08c4f0a4f659b5068003bcdf72daede745db9a881d5d4021102273fdecfe59") --Dlc Windows Language Depot Japanese Resident Evil Requiem Original Soundtrack
-setManifestid(4460241, "4190523507411811525", 158101936)
---Share Depots
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
-setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3764200)
+addappid(3990820)
+addappid(3990800)
+addappid(4460240)
+addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+setManifestid(3764201,"2990234554016452841")
+setManifestid(3764203,"1507402407513694495")
+setManifestid(3990800,"9181787779883827112")
+setManifestid(3990820,"509708311968316285")
+setManifestid(228989,"5753583882400741046")
