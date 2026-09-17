3539ca7174501b11acc8c556656b1ac465c14bb1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 14:47:46 +0800

    add 232090.lua

--- 232090.lua
+++ 232090.lua
@@ -1,16 +1,52 @@
-addappid(232090)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-setManifestid(229004,"5220958916987797232")
-setManifestid(229033,"2059065101492814639")
-addappid(232091,0,"f46359be90614cd82464602ee51bc2122dc359d6a493844be3a2edfd2a733be7")
-setManifestid(232091,"3780140359303487885")
-addappid(232092,0,"4e45d5cae89bca85c7df390c27343ee8c743dd49404f6bb848c900a1a1ef7dcc")
-setManifestid(232092,"5954956292867565444")
-addappid(232093,0,"15e06cd0dba1b90a3daedffd14e329e4134ec76f3ba808f6f44dc11b32835c0a")
-setManifestid(232093,"2216080875481941448")
-addappid(232094,0,"ff56a2e376c3a8baddd72e9f22d3c896728aa0846076d76806d96e56e7ff3bec")
-setManifestid(232094,"1845006560688334540")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 232090.lua
+--Gamename Killing Floor 2
+addappid(232090, 1, "d50a5c27ad4f493a022571ee34c65609d373b8344825dbe065bff22e893e06e8") --Mainappid Killing Floor 2
+addappid(232091, 1, "f46359be90614cd82464602ee51bc2122dc359d6a493844be3a2edfd2a733be7") --Main Windows Depot Killing Floor 2
+setManifestid(232091, "3780140359303487885", 43638599440)
+--Dlcname KF2 - Soundtrack
+addappid(360930) --Dlcname KF2 - Soundtrack
+addappid(232092, 1, "4e45d5cae89bca85c7df390c27343ee8c743dd49404f6bb848c900a1a1ef7dcc") --Dlc Windows Depot KF2 - Soundtrack
+setManifestid(232092, "5954956292867565444", 879955184)
+--Dlcname KF2 - Artbook
+addappid(360931) --Dlcname KF2 - Artbook
+addappid(232093, 1, "15e06cd0dba1b90a3daedffd14e329e4134ec76f3ba808f6f44dc11b32835c0a") --Dlc Windows Depot KF2 - Artbook
+setManifestid(232093, "2216080875481941448", 52495216)
+--Dlcname AppID 363760
+addtoken(363760, "10580095351703989210")
+addappid(363760) --Dlcname AppID 363760
+addappid(232094, 1, "ff56a2e376c3a8baddd72e9f22d3c896728aa0846076d76806d96e56e7ff3bec") --Dlc Windows Depot AppID 363760
+setManifestid(232094, "1845006560688334540", 2737216)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
+addappid(294850) --Dlcname KF2 - Mr Foster Dosh Jacket!
+addappid(354020) --Dlcname KF2 - Digital Deluxe Edition DLC
+addappid(361240) --Dlcname KF2 - Full Game
+addappid(408560) --Dlcname KF2 - Alienware DLC
+addappid(1368501) --Dlcname AppID 1368501
+addtoken(1368501, "13344653896183276191")
+addappid(1368502) --Dlcname AppID 1368502
+addtoken(1368502, "1461492668364395345")
+addappid(1368503) --Dlcname AppID 1368503
+addtoken(1368503, "7700075408784397171")
+addappid(1524820) --Dlcname KF2 - Season Pass 2021
+addappid(1914490) --Dlcname KF2 - Season Pass 2022
+addappid(1914560) --Dlcname KF2 - Ultimate Edition Upgrade DLC
+addappid(1937240) --Dlcname AppID 1937240
+addtoken(1937240, "15231710637902033995")
+addappid(1937241) --Dlcname AppID 1937241
+addappid(1993760) --Dlcname KF2 - Ultimate Edition Upgrade Bundle DLC
+addappid(2363410) --Dlcname Killing Floor 2 - Cosmetics Season Pass
