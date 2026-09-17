f1566636ed28a3cfa3204e7a6c89e1bcc237b4ad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 16:54:58 +0800

    add 1100910.lua

--- 1100910.lua
+++ 1100910.lua
@@ -1,5 +1,9 @@
-addappid(1100910)
-addappid(1100911,0,"760aeecde28db6521c27b4d765166beed7b2a4bad578c13d86366ccd6825e128")
-setManifestid(1100911,"932633407292066915")
-addappid(3726170,0,"2312b64239d8c91ba9e907e314a92ce207a3bf0cd225f05ec36af06b5c90b418")
-setManifestid(3726170,"7593661029837381308")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1100910.lua
+--Gamename 鬼哭岭
+addappid(1100910) --Mainappid 鬼哭岭
+addappid(1100911, 1, "760aeecde28db6521c27b4d765166beed7b2a4bad578c13d86366ccd6825e128") --Main Windows Depot 鬼哭岭
+setManifestid(1100911, "6898167809263923731", 1430545472)
+--Dlcname 鬼哭岭设定集- Artbook
+addappid(3726170, 1, "2312b64239d8c91ba9e907e314a92ce207a3bf0cd225f05ec36af06b5c90b418") --Dlcname 鬼哭岭设定集- Artbook
+setManifestid(3726170, "7593661029837381308", 29740784)
