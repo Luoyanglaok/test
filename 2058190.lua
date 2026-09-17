d5ceb8c14b68d8994dd6d687289b85d269274d8b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 19:56:34 +0800

    add 2058190.lua

--- 2058190.lua
+++ 2058190.lua
@@ -1,20 +1,28 @@
-addappid(2058190)
-setManifestid(228988,"6645201662696499616")
-addappid(2058191,0,"39536eee50189371b847b8c923772bb0a1914079459c852bac325c55f71bd085")
-setManifestid(2058191,"1108385733889662950")
-addappid(2058192,0,"16020a20ac3bd8d42984905d72924d4923f6db53593fd874f5977dd3b81a5b29")
-setManifestid(2058192,"2356614499278645358")
-addappid(2058193,0,"249f3e81ec2455f6556dcfe0bc2c582a57350c703a2c5018b4a35a52bc30e918")
-setManifestid(2058193,"3023615365333382853")
-addappid(2058194,0,"41a42f75e89e1333aaae4d1f21cd1d40d79d573fb63179ea465f955419f86c31")
-setManifestid(2058194,"5496445866113927429")
-addappid(2058195,0,"7ae46cfc89b26b8575d1ffe8656b2cd947d97782c34c7ffe72618ffc10f13a33")
-setManifestid(2058195,"1709028538040015540")
-addappid(2058196,0,"732d3974d75cfc0e28b47bd192a69fda650dc05a8bf1f6ba52b7d8692a3d7983")
-setManifestid(2058196,"6227221954580559052")
-addappid(2058197,0,"1479564412ea2e8db403c95327009a29fe07a89fc55ebb65576c763870154fa2")
-setManifestid(2058197,"7227965025578574369")
-addappid(2058198,0,"4c49c509bbfb496da9e5d2a48ed82a3e308cf8cccbc6067b40d54a97f2cf7802")
-setManifestid(2058198,"6217176551366386900")
-addappid(2058199,0,"0bfec0fce4a72ac9879db6e54968c03419b4318f4167daf097a5ea22baa97fb8")
-setManifestid(2058199,"2746341661372416133")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2058190.lua
+--Gamename Lost Judgment
+addappid(2058190, 1, "aade91745646d24fd81a0155105faba27359a63cbad2d49f76be35db902dd4e4") --Mainappid Lost Judgment
+addappid(2058191, 1, "39536eee50189371b847b8c923772bb0a1914079459c852bac325c55f71bd085") --Main Windows Depot Lost Judgment
+setManifestid(2058191, "1108385733889662950", 49242419088)
+addappid(2058192, 1, "16020a20ac3bd8d42984905d72924d4923f6db53593fd874f5977dd3b81a5b29") --Main Windows Language Depot Japanese Lost Judgment
+setManifestid(2058192, "2356614499278645358", 249864832)
+addappid(2058193, 1, "249f3e81ec2455f6556dcfe0bc2c582a57350c703a2c5018b4a35a52bc30e918") --Main Windows Language Depot Tchinese Lost Judgment
+setManifestid(2058193, "3023615365333382853", 250109552)
+addappid(2058194, 1, "41a42f75e89e1333aaae4d1f21cd1d40d79d573fb63179ea465f955419f86c31") --Main Windows Language Depot Schinese Lost Judgment
+setManifestid(2058194, "5496445866113927429", 250219488)
+addappid(2058195, 1, "7ae46cfc89b26b8575d1ffe8656b2cd947d97782c34c7ffe72618ffc10f13a33") --Main Windows Language Depot Koreana Lost Judgment
+setManifestid(2058195, "1709028538040015540", 250449792)
+addappid(2058196, 1, "732d3974d75cfc0e28b47bd192a69fda650dc05a8bf1f6ba52b7d8692a3d7983") --Main Windows Language Depot French Lost Judgment
+setManifestid(2058196, "6227221954580559052", 249054400)
+addappid(2058197, 1, "1479564412ea2e8db403c95327009a29fe07a89fc55ebb65576c763870154fa2") --Main Windows Language Depot Italian Lost Judgment
+setManifestid(2058197, "7227965025578574369", 249092112)
+addappid(2058198, 1, "4c49c509bbfb496da9e5d2a48ed82a3e308cf8cccbc6067b40d54a97f2cf7802") --Main Windows Language Depot German Lost Judgment
+setManifestid(2058198, "6217176551366386900", 249037984)
+addappid(2058199, 1, "0bfec0fce4a72ac9879db6e54968c03419b4318f4167daf097a5ea22baa97fb8") --Main Windows Language Depot Spanish Lost Judgment
+setManifestid(2058199, "2746341661372416133", 248986960)
+--Dlcname Lost Judgment - The Kaito Files Story Expansion
+addappid(2060280, 1, "8f210ec51cad794270eddc55022b23a70e688e715909c17bf7778d3fe8ccef71") --Dlcname Lost Judgment - The Kaito Files Story Expansion
+setManifestid(2060280, "4231231069023266225", 3448268864)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
