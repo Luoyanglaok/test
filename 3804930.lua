1a06337668749b396d466abd6a000c3f00c5bedf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 05:44:59 +0800

    add 3804930.lua

---
+++ 3804930.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3804930.lua
+--Gamename Burger Shift: Team Rush
+addappid(3804930) --Mainappid Burger Shift: Team Rush
+addappid(3804931, 1, "1b6e337f02a0b99393fdd6ea2379993ee6bdbb8bf387c7c58e0cdc7540f05e30") --Main Windows Depot Burger Shift: Team Rush
+setManifestid(3804931, "4854079306381536204", 5037174832)
+--Dlcname Burger Shift: Team Rush Playtest
+addappid(4882060) --Dlcname Burger Shift: Team Rush Playtest
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Dlc Windows Depot Burger Shift: Team Rush Playtest
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Dlc Windows Depot Burger Shift: Team Rush Playtest
+setManifestid(228990, "1829726630299308803", 100658080)
+--Share Depots
