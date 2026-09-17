476df6c1e69eec5407bc64638dbd722ffc41c375
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 17:50:06 +0800

    add 2058180.lua

--- 2058180.lua
+++ 2058180.lua
@@ -1,20 +1,25 @@
-addappid(2058180)
-setManifestid(228988,"6645201662696499616")
-addappid(2058181,0,"2c5b08fe950ac2f5fcfc838f3ebcf0f1863bcb65ffc05bb859d1e3f8cd20fb99")
-setManifestid(2058181,"8035707233626484552")
-addappid(2058182,0,"f6aa46579ab78cdede9b725040ae992a922876dc88c3d0b4d51f1de8dca091d7")
-setManifestid(2058182,"2945639909247876092")
-addappid(2058183,0,"362b005a854a2281f863b2b29a2d4cdf14c0038bd1593d80eae2845a023c748a")
-setManifestid(2058183,"6902293912600710880")
-addappid(2058184,0,"8e74fca7757729e2611867385eb27efcdac8eb374d28dd0d6a783c25a852f6d4")
-setManifestid(2058184,"2894483093347645518")
-addappid(2058185,0,"a26ec42bcfcd2d33dfe90f40d4354dcc0e4a5345c55ae1bea3e70d5bd19164b4")
-setManifestid(2058185,"1892906999181499702")
-addappid(2058186,0,"89fd458eaee9f9f95bbd9ebd4198b2ebbdd31e4757ee89dee2de1822755476a0")
-setManifestid(2058186,"532774351515253971")
-addappid(2058187,0,"6392203a125860c340a4115737d0a6f44929bf4def0f4e18db19c1e917078387")
-setManifestid(2058187,"508766208648227258")
-addappid(2058188,0,"94548f8725ca5a1ab85f479f30ab6c987c1f92b16d3a51a1601ea4705db12ba3")
-setManifestid(2058188,"2209049931727278392")
-addappid(2058189,0,"fd84527ae76dc442a8ad74d9293d136400bfe79a532dc323d21790b2b9021bed")
-setManifestid(2058189,"930300468467287281")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2058180.lua
+--Gamename Judgment
+addappid(2058180, 1, "c3f1bcb3f07bb8ea7b5f90877dc5037ad8eb64ced3c6cc2720089e3c7b45322c") --Mainappid Judgment
+addappid(2058181, 1, "2c5b08fe950ac2f5fcfc838f3ebcf0f1863bcb65ffc05bb859d1e3f8cd20fb99") --Main Windows Depot Judgment
+setManifestid(2058181, "8035707233626484552", 28708052976)
+addappid(2058182, 1, "f6aa46579ab78cdede9b725040ae992a922876dc88c3d0b4d51f1de8dca091d7") --Main Windows Language Depot Japanese Judgment
+setManifestid(2058182, "2945639909247876092", 193799104)
+addappid(2058183, 1, "362b005a854a2281f863b2b29a2d4cdf14c0038bd1593d80eae2845a023c748a") --Main Windows Language Depot Tchinese Judgment
+setManifestid(2058183, "6902293912600710880", 193432224)
+addappid(2058184, 1, "8e74fca7757729e2611867385eb27efcdac8eb374d28dd0d6a783c25a852f6d4") --Main Windows Language Depot Schinese Judgment
+setManifestid(2058184, "2894483093347645518", 189436544)
+addappid(2058185, 1, "a26ec42bcfcd2d33dfe90f40d4354dcc0e4a5345c55ae1bea3e70d5bd19164b4") --Main Windows Language Depot Koreana Judgment
+setManifestid(2058185, "1892906999181499702", 192156496)
+addappid(2058186, 1, "89fd458eaee9f9f95bbd9ebd4198b2ebbdd31e4757ee89dee2de1822755476a0") --Main Windows Language Depot French Judgment
+setManifestid(2058186, "532774351515253971", 190093648)
+addappid(2058187, 1, "6392203a125860c340a4115737d0a6f44929bf4def0f4e18db19c1e917078387") --Main Windows Language Depot Italian Judgment
+setManifestid(2058187, "508766208648227258", 190213456)
+addappid(2058188, 1, "94548f8725ca5a1ab85f479f30ab6c987c1f92b16d3a51a1601ea4705db12ba3") --Main Windows Language Depot German Judgment
+setManifestid(2058188, "2209049931727278392", 190535520)
+addappid(2058189, 1, "fd84527ae76dc442a8ad74d9293d136400bfe79a532dc323d21790b2b9021bed") --Main Windows Language Depot Spanish Judgment
+setManifestid(2058189, "930300468467287281", 190402544)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
