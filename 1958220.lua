1cc0e985214c4ae32fd9418b3592c80541e89ef6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 18:21:43 +0800

    add 1958220.lua

--- 1958220.lua
+++ 1958220.lua
@@ -1,3 +1,17 @@
-addappid(1958220)
-addappid(1958221,0,"f4ac13aa686b816d6d28b882d301090c4eb3b9aa14c6802a398402367dc1f612")
-setManifestid(1958221,"1118587852297575919")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1958220.lua
+--Gamename WitchSpring R
+addappid(1958220) --Mainappid WitchSpring R
+addappid(1958221, 1, "f4ac13aa686b816d6d28b882d301090c4eb3b9aa14c6802a398402367dc1f612") --Main Windows Depot WitchSpring R
+setManifestid(1958221, "7941803587227338199", 5810168352)
+--Dlcname WitchSpring R Soundtrack
+addappid(2666070) --Dlcname WitchSpring R Soundtrack
+addappid(2666071, 1, "82943f18543d68a94fd6ff4001289a4ca36a06a5ccb23966a037fdd883b8fb81") --Dlc Windows Depot WitchSpring R Soundtrack
+setManifestid(2666071, "5252024825087965050", 256637552)
+addappid(2666072, 1, "0fcecdb46ab351fdd6d349b018549d0ff24ae5fd8593c152d40560d3a1f5e2e1") --Dlc Windows Depot WitchSpring R Soundtrack
+setManifestid(2666072, "2060437540854225209", 1043716640)
+addappid(2666040) --Dlcname WitchSpring R - Costume - Red Berry Set
+addappid(2666050) --Dlcname WitchSpring R - Costume - Black Pearl Set
+addappid(3228130) --Dlcname WitchSpring R - Costume - Silver Rose Set
+addappid(3228140) --Dlcname WitchSpring R - Costume - Shining Dawn Set
+addappid(3899110) --Dlcname WitchSpring R - Costume - Fluffy Peach Set
