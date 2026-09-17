fa95f9963d6f3d0fce0bf198baa46c8630db2603
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 11:21:23 +0800

    add 1693980.lua

--- 1693980.lua
+++ 1693980.lua
@@ -1,4 +1,10 @@
-addappid(1693980)
-setManifestid(228989,"550968249685141759")
-addappid(1693981,0,"9738ac7c4b941978165b403d5785d6c8c331366f7feb30c0b276401fd71f4ccb")
-setManifestid(1693981,"1038276949129655019")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1693980.lua
+--Gamename Dead Space
+addappid(1693980, 1, "d3b17f005068be7c20ce6a6376d31b5076db5cd3686094f6d546cd0759117904") --Mainappid Dead Space
+addappid(1693981, 1, "9738ac7c4b941978165b403d5785d6c8c331366f7feb30c0b276401fd71f4ccb") --Main Windows Depot Dead Space
+setManifestid(1693981, "1038276949129655019", 37189291824)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2164210) --Dlcname Dead Space Digital Deluxe Edition Upgrade
