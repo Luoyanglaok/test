811bc6096176016d86c2db00be785e2777aaf5ed
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-17 12:08:14 +0800

    add 1790600.lua

--- 1790600.lua
+++ 1790600.lua
@@ -1,18 +1,31 @@
-addappid(1790600)
-addappid(4433900)
-addappid(2957520)
-addappid(2957480)
-addappid(2957490)
-addappid(2957460)
-addappid(3310650)
-addappid(3456600)
-addappid(3456610)
-addappid(2957470)
-addappid(3693250)
-addappid(2957500)
-addappid(2957510)
-setManifestid(1790601,"8617588857786990249")
-setManifestid(2957480,"2926878599274088682")
-setManifestid(2957490,"6336205044388274470")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1790600.lua
+--Gamename DRAGON BALL: Sparking! ZERO
+addappid(1790600, 1, "5c8b5e47bb9da92fe0a31a0d56554bb62851cb9f846cc5f89fac68501d9964ff") --Mainappid DRAGON BALL: Sparking! ZERO
+addtoken(1790600, "4807984869702339794")
+addappid(1790601, 1, "0721a07eb97ba7b4e59e31606ae9f0d67c66309cc467c5da1aa79a924c34e7d8") --Main Windows Depot DRAGON BALL: Sparking! ZERO
+setManifestid(1790601, "8617588857786990249", 35101720464)
+--Dlcname DRAGON BALL: Sparking! ZERO Anime Music Pack 1
+addappid(2957480, 1, "a8bbd2bdc3022cc2edc229e300b7583436b29d0e469662a8a277cf066798aa9f") --Dlcname DRAGON BALL: Sparking! ZERO Anime Music Pack 1
+setManifestid(2957480, "2926878599274088682", 25228400)
+--Dlcname DRAGON BALL: Sparking! ZERO Anime Music Pack 2
+addappid(2957490, 1, "1b1f85ab25eb92147de8a0c1531d9ed57f3e01cfd46c9bb37be377251c2d24cd") --Dlcname DRAGON BALL: Sparking! ZERO Anime Music Pack 2
+setManifestid(2957490, "6336205044388274470", 37553680)
+--Dlcname DRAGON BALL: Sparking! ZERO - Super Limit-Breaking NEO
+addappid(4433900, 1, "ff0bdc811c96b18811728859ee79be7240c2e3d904ec06e077f117ad2235363d") --Dlcname DRAGON BALL: Sparking! ZERO - Super Limit-Breaking NEO
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2957460) --Dlcname DRAGON BALL: Sparking! ZERO Pre-Order Pack
+addappid(2957470) --Dlcname DRAGON BALL: Sparking! ZERO Ultimate Upgrade Pack
+addappid(2957500) --Dlcname DRAGON BALL: Sparking! ZERO Victory Pack
+addappid(2957510) --Dlcname DRAGON BALL: Sparking! ZERO Martial Arts Pack
+addappid(2957520) --Dlcname DRAGON BALL: Sparking! ZERO Season Pass
+addappid(2963440) --Dlcname DRAGON BALL: Sparking! ZERO Ultimate Edition Bonus
+addappid(2963450) --Dlcname DRAGON BALL: Sparking! ZERO Season Pass Bonus
+addappid(3310650) --Dlcname DRAGON BALL: Sparking! ZERO HERO OF JUSTICE Pack
+addappid(3456600) --Dlcname DRAGON BALL: Sparking! ZERO - Dragon Ball DAIMA: Character Pack 1
+addappid(3456610) --Dlcname DRAGON BALL: Sparking! ZERO - Dragon Ball DAIMA: Character Pack 2
+addappid(3693250) --Dlcname DRAGON BALL: Sparking! ZERO - Shallot (DRAGON BALL LEGENDS)
