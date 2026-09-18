65dc41770a89fc6d13927077fd5eeb049a756428
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:51:44 +0800

    add 3343790.lua

--- 3343790.lua
+++ 3343790.lua
@@ -1,5 +1,11 @@
-addappid(3343790)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(3343791,0,"59c2c08614e675395a3b6a2fd990325fb436fe49461df1b2b227401be95655eb")
-setManifestid(3343791,"1441302830186948122")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3343790.lua
+--Gamename Superhero Market
+addappid(3343790) --Mainappid Superhero Market
+addappid(3343791, 1, "59c2c08614e675395a3b6a2fd990325fb436fe49461df1b2b227401be95655eb") --Main Windows Depot Superhero Market
+setManifestid(3343791, "1441302830186948122", 20238651152)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
