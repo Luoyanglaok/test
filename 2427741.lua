889e5fd82234f67411ca1dfea02f9dca2a1d96ea
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 03:26:27 +0800

    add 2427741.lua

--- 2427741.lua
+++ 2427741.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 2427741.lua
-addappid(2427741, 0, "c7c0e08f49fab3d443d26d53189e7b41a35c85a126fc916bbc0c3aaae81c2305")
+addappid(2427741, 0, "60e6171ca90b326a72fa174706d56b459dba6b11502bfc9cd4e723f3a0c49b4f")
