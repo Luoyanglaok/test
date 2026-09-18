b0fd8ef27888ed862bb2e22ae6cbe9f84f5c814b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 23:42:18 +0800

    add 10.lua

--- 10.lua
+++ 10.lua
@@ -1,34 +1,53 @@
-addappid(10)
-setManifestid(228988,"6645201662696499616")
-setManifestid(1,"2667117727132184734")
-addappid(11,0,"dacfb281a0abbecc078e884aef2a69caf7723f4066ce3f493ea02248abc8de71")
-setManifestid(11,"3018093494972814332")
-addappid(95,0,"7ab15ade88539a6bee8195ae0b1a2a44ad68813632ac2dc230169a64893556e2")
-setManifestid(95,"7685864384078520360")
-setManifestid(3,"6081070194444336449")
-setManifestid(8,"1360709870205537514")
-setManifestid(2,"724247850320422500")
-setManifestid(9,"1083345737969856561")
-setManifestid(6,"8518478957342391261")
-setManifestid(72,"491735997354925434")
-setManifestid(73,"3587203030189636184")
-setManifestid(74,"6929797608257779911")
-setManifestid(75,"6599379689843151742")
-setManifestid(77,"8307934900153578333")
-setManifestid(78,"2582370374354695075")
-setManifestid(79,"6488358189498321283")
-setManifestid(141,"3921686991280371259")
-addappid(12,0,"4989c0128f7c8f0194425db0e36c2800e0f0c0e31373bff7661e8a806f79c82c")
-setManifestid(12,"7476629819067189879")
-addappid(13,0,"2f7229ad38de3e01bbf7816c22aa22a239bebbd9eb1347b5b8d48681e53117f3")
-setManifestid(13,"1596103708518313909")
-addappid(14,0,"447273cfeac2d08447c113fd1065235f5abe5a17e6f6487c50393fed0014e131")
-setManifestid(14,"4163740413628378088")
-addappid(15,0,"7b217d6cc4650b0657392c11b8f0a689be2ac7037a17d3a82f8c1a09d3cd386b")
-setManifestid(15,"2951655054604220030")
-addappid(17,0,"67bff47a4f20a2dbfc1e156d84fc6f1508dce43212347723f3bb916619224506")
-setManifestid(17,"8232848132911478974")
-addappid(18,0,"8d2761f52afa196eda68faa7a613e64965fa0ce7ebca0780ba2cbe94c1d6a409")
-setManifestid(18,"9210252686867714338")
-addappid(19,0,"42ed27e3851524839873cc01d0408d2d76ae6d879404f16d48681329efffadad")
-setManifestid(19,"5149037574830114753")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10.lua
+--Gamename Counter-Strike
+addappid(10, 1, "7b83732c00d6cb6178cdd01d4e6b9d858e5c62dc03d15be859d4851486812ebb") --Mainappid Counter-Strike
+addappid(11, 1, "dacfb281a0abbecc078e884aef2a69caf7723f4066ce3f493ea02248abc8de71") --Main Windows Depot Counter-Strike
+setManifestid(11, "3018093494972814332", 149760208)
+addappid(12, 1, "4989c0128f7c8f0194425db0e36c2800e0f0c0e31373bff7661e8a806f79c82c") --Main Windows Language Depot French Counter-Strike
+setManifestid(12, "7476629819067189879", 1451920)
+addappid(13, 1, "2f7229ad38de3e01bbf7816c22aa22a239bebbd9eb1347b5b8d48681e53117f3") --Main Windows Language Depot Italian Counter-Strike
+setManifestid(13, "1596103708518313909", 5369424)
+addappid(14, 1, "447273cfeac2d08447c113fd1065235f5abe5a17e6f6487c50393fed0014e131") --Main Windows Language Depot German Counter-Strike
+setManifestid(14, "4163740413628378088", 1191920)
+addappid(15, 1, "7b217d6cc4650b0657392c11b8f0a689be2ac7037a17d3a82f8c1a09d3cd386b") --Main Windows Language Depot Spanish Counter-Strike
+setManifestid(15, "2951655054604220030", 1137440)
+addappid(17, 1, "67bff47a4f20a2dbfc1e156d84fc6f1508dce43212347723f3bb916619224506") --Main Windows Language Depot Schinese Counter-Strike
+setManifestid(17, "8232848132911478974", 8988336)
+addappid(18, 1, "8d2761f52afa196eda68faa7a613e64965fa0ce7ebca0780ba2cbe94c1d6a409") --Main Windows Language Depot Koreana Counter-Strike
+setManifestid(18, "9210252686867714338", 2251424)
+addappid(19, 1, "42ed27e3851524839873cc01d0408d2d76ae6d879404f16d48681329efffadad") --Main Windows Language Depot Tchinese Counter-Strike
+setManifestid(19, "5149037574830114753", 2010848)
+addappid(95, 1, "7ab15ade88539a6bee8195ae0b1a2a44ad68813632ac2dc230169a64893556e2") --Main Windows Depot Counter-Strike
+setManifestid(95, "7685864384078520360", 14626816)
+--Share Depots
+addappid(1, 1, "b465d45ab2a7c396f7d1c08a6644e68529ec86b14da77e18588abbbcd2412060") --Share Windows Depot Half-Life
+setManifestid(1, "2667117727132184734", 264641152)
+addappid(2, 1, "fd681c065529f5ee95e920321a780a9290f8f31ef51c232ccb3e98d27e132ebc") --Share Windows Depot Half-Life
+setManifestid(2, "724247850320422500", 19297632)
+addappid(3, 1, "d249f7d93387a793ed021f0e2314ad0cea0d6aef0b0881c3b725a33deec37d59") --Share Windows Depot Half-Life
+setManifestid(3, "6081070194444336449", 148112)
+addappid(6, 1, "cd3d8b6f23fb863aea4d6789a53dd58dcf284141860e893e8e580d1988d93d3c") --Share Windows Depot Half-Life
+setManifestid(6, "8518478957342391261", 27950128)
+addappid(8, 1, "20af1043db56d6408ddd965580e1269005b6e04f81c3676d155174f73099da95") --Share Linux Depot Half-Life
+setManifestid(8, "1360709870205537514", 42477296)
+addappid(9, 1, "68c92cf2532cdc6e0b7a9d2783b473b6d16bac72218badf68d7be9b033076096") --Share Macos Depot Half-Life
+setManifestid(9, "1083345737969856561", 19253680)
+addappid(72, 1, "bbc1a14b0c63025aece7d9c3b5a98089205580f321bb5edf6b5f00d368c6face") --Share Windows Language Depot French Half-Life
+setManifestid(72, "491735997354925434", 29617440)
+addappid(73, 1, "9604a5ba9ad4b580269d5362e201d217cdd9d268bf1098e2c6beb0c5ade020d3") --Share Windows Language Depot Italian Half-Life
+setManifestid(73, "3587203030189636184", 36527248)
+addappid(74, 1, "49d573f303205df2316749a6ede6725b74651fae3e845494b88ac3d0022a2857") --Share Windows Language Depot German Half-Life
+setManifestid(74, "6929797608257779911", 35264800)
+addappid(75, 1, "1339202607b6485322859fc1f045c6790bbc470a2d3bb8158aa94b06da31f9bb") --Share Windows Language Depot Spanish Half-Life
+setManifestid(75, "6599379689843151742", 37926368)
+addappid(77, 1, "793780217977cc3a005dc4410e963c980137e4faca4442f92dc41ab56af96983") --Share Windows Language Depot Schinese Half-Life
+setManifestid(77, "8307934900153578333", 32815776)
+addappid(78, 1, "39f04a0cc8cffae8023eff1e3e933680681997409823942a5f0f2899d2d2c4b2") --Share Windows Language Depot Koreana Half-Life
+setManifestid(78, "2582370374354695075", 33761760)
+addappid(79, 1, "019e1cb0950e060fb992001657a444fe03cbc4984081e0d1d9a6505259a7a7e7") --Share Windows Language Depot Tchinese Half-Life
+setManifestid(79, "6488358189498321283", 32815632)
+addappid(141, 1, "0d85b74e163cceabf3651fdd6084585587461f6f7735c37eef875634fc6886cb") --Share Windows Language Depot Russian Half-Life
+setManifestid(141, "3921686991280371259", 10352)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
