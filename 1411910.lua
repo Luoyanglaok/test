f4940796b1ff0d46191da794fcfb79598cb0c8a2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 02:30:02 +0800

    add 1411910.lua

--- 1411910.lua
+++ 1411910.lua
@@ -1,3 +1,6 @@
-addappid(1411910)
-addappid(1411911,0,"8f3ebc25fc2fca88d99ee0a0983db4feed1e6174072e46f94bc59aae57d1445f")
-setManifestid(1411911,"3122881567820050281")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1411910.lua
+--Gamename Fallen Aces
+addappid(1411910, 1, "a04357472b1e27b7d0fff23981487f48f419541db7cb0d5fda8fffd8e651d411") --Mainappid Fallen Aces
+addappid(1411911, 1, "8f3ebc25fc2fca88d99ee0a0983db4feed1e6174072e46f94bc59aae57d1445f") --Main Windows Depot Fallen Aces
+setManifestid(1411911, "5465475253771600813", 8763731520)
