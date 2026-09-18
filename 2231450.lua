ac606589a89a14b3b60d8290e3bf720004cdfcc7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:12:01 +0800

    add 2231450.lua

--- 2231450.lua
+++ 2231450.lua
@@ -1,5 +1,15 @@
-addappid(2231450)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2231451,0,"04bf341f26f9a9b6cc4da858ea57d362a95437570d05b354ac0aff84ffbb243e")
-setManifestid(2231451,"6913777767294465277")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2231450.lua
+--Gamename Pizza Tower
+addappid(2231450, 1, "347037804e17552489f4cbd516cab2a5e617002fcd370b477f1f7b53ffe8b55c") --Mainappid Pizza Tower
+addappid(2231451, 1, "04bf341f26f9a9b6cc4da858ea57d362a95437570d05b354ac0aff84ffbb243e") --Main Windows Depot Pizza Tower
+setManifestid(2231451, "6913777767294465277", 284256592)
+--Dlcname Pizza Tower Soundtrack
+addappid(2250640) --Dlcname Pizza Tower Soundtrack
+addappid(2250641, 1, "827084d8dbac504690f000cfa0620f41bf6e18bbe5c3b5818ca2c25a11d1864b") --Dlc Windows Depot Pizza Tower Soundtrack
+setManifestid(2250641, "7428080939698681446", 284566752)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
