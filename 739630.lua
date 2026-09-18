3d3d2c1aa0b500140e4a73b3731714d69cc5c1d3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 17:35:45 +0800

    add 739630.lua

--- 739630.lua
+++ 739630.lua
@@ -3,7 +3,7 @@
 --Gamename Phasmophobia
 addappid(739630, 1, "6850686ca304da94d7b1a8ebd9203bcf007712610dc92eb9452e22a31b6b823d") --Mainappid Phasmophobia
 addappid(739631, 1, "a0af61717bae449e5db4a1cc6eb68d7604ac3a8e05ff5226faa14b5be67d640c") --Main Windows Depot Phasmophobia
-setManifestid(739631, "9162727472030908328", 17111764368)
+setManifestid(739631, "4491859809819227821", 17111563680)
 --Share Depots
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228988, "6645201662696499616", 22411856)
