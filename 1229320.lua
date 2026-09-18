7dd03fbb56776f34ac28618ef89fee68f26e4b04
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 12:33:10 +0800

    add 1229320.lua

--- 1229320.lua
+++ 1229320.lua
@@ -1,5 +1,8 @@
-addappid(1229320)
-addappid(1229321,0,"aabffdf335260ba3a1609a699afe267f038bd26f104d5080c6c84f4c2f954c5e")
-setManifestid(1229321,"2956293257306390314")
-addappid(1229322,0,"c8f0226c60c667cc2115ac95cc5b2fd10b11f3c924812ed419147a480bfc5e74")
-setManifestid(1229322,"951359575970231120")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1229320.lua
+--Gamename The Witcher 3: Wild Hunt Soundtrack
+addappid(1229320) --Mainappid The Witcher 3: Wild Hunt Soundtrack
+addappid(1229321, 1, "aabffdf335260ba3a1609a699afe267f038bd26f104d5080c6c84f4c2f954c5e") --Main Windows Depot The Witcher 3: Wild Hunt Soundtrack
+setManifestid(1229321, "2956293257306390314", 216737456)
+addappid(1229322, 1, "c8f0226c60c667cc2115ac95cc5b2fd10b11f3c924812ed419147a480bfc5e74") --Main Windows Depot The Witcher 3: Wild Hunt Soundtrack
+setManifestid(1229322, "951359575970231120", 579639456)
