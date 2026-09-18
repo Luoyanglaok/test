cc4bf669f4b06f05577df4f031f37ff817bdeaf4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 06:51:42 +0800

    add 2881150.lua

--- 2881150.lua
+++ 2881150.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 2881150.lua
-addappid(2881150, 0, "bd09ffd4d6c10921352512d4b9fecd1ff4c47372844768a3c20ec7621516c1a0")
+addappid(2881150, 0, "b3761f8ff75b365d4483f1554854f396c1e57a45b41b7af4e4c828d9d66fb5af")
