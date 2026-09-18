a8651b3b33996e16ff48117b8eacaa6da1c89e04
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 19:14:05 +0800

    add 2995560.lua

--- 2995560.lua
+++ 2995560.lua
@@ -1,5 +1,17 @@
-addappid(2995560)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2995561,0,"82e700a5f0601b7e7330daa61fa8dabf30452670a844774860ceae85d6f1fce3")
-setManifestid(2995561,"3140037134109404371")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2995560.lua
+--Gamename Wildkeepers Rising
+addappid(2995560) --Mainappid Wildkeepers Rising
+addappid(2995561, 1, "82e700a5f0601b7e7330daa61fa8dabf30452670a844774860ceae85d6f1fce3") --Main Windows Depot Wildkeepers Rising
+setManifestid(2995561, "865595762587831943", 1426890784)
+--Dlcname Wildkeepers Rising Soundtrack
+addappid(3618440) --Dlcname Wildkeepers Rising Soundtrack
+addappid(3618441, 1, "b79a006ca042d6f76e901079d3fdcc21b60919fe34a10e3f3d854d471e710500") --Dlc Windows Depot Wildkeepers Rising Soundtrack
+setManifestid(3618441, "572559801651708469", 87098256)
+addappid(3618442, 1, "234b9af981e606fef4c0eba41c7849d42b0d6c68c6bf24aad0e9635fbc236b9c") --Dlc Windows Depot Wildkeepers Rising Soundtrack
+setManifestid(3618442, "6044689564286391360", 613228432)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
