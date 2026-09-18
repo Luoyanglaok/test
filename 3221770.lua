720017a52c78048a55a8cac88fbdbce3c3d25f35
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 13:33:50 +0800

    add 3221770.lua

---
+++ 3221770.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3221770.lua
+--Gamename Sin Spire
+addappid(3221770, 1, "17383f9051ad082049fffd5ed7e3e63f3356a1a0608d6a2f914402d596c14529") --Mainappid Sin Spire
+addappid(3221771, 1, "439f434b2a2771d317dc4421495e6536e0c1738576d9203af453becaf06040ef") --Main Windows Depot Sin Spire
+setManifestid(3221771, "5661482734144686800", 1528988688)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
