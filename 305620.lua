56e16bc41189ba675b512431e2df195257be01fd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:42:33 +0800

    add 305620.lua

--- 305620.lua
+++ 305620.lua
@@ -1,22 +1,42 @@
-addappid(305620)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228990,"1829726630299308803")
-addappid(305621,0,"2a658a052de2d030f6031e4aa4835b7b12b6646cb7142fde7f0216b06b9b3d85")
-setManifestid(305621,"8931098350046500768")
-addappid(305622,0,"973dd3d0c6c26bcaaabe742bd507c7d2711d69bbc1f1f5c21650629e93121eca")
-setManifestid(305622,"2505377507520171245")
-addappid(305623,0,"9960f03341efd0f41aa06b12b9df8c42a592b5d1385f29a87531bec7c84709d0")
-setManifestid(305623,"3491776124229082114")
-addappid(2091331,0,"ef5d6edc9a2c7de7d0c7e187558780cc5724114441d0e5cfab386460042b0975")
-setManifestid(2091331,"1890365215997124305")
-addappid(2091332,0,"71e3e4cc7b1539fd8cf9f21f16c4c191d9927f04656a51ab06a8e03051a4b612")
-setManifestid(2091332,"5455100232396703117")
-addappid(2091333,0,"9cdb1d532c9c2a2315abf67c51cc52d39725194ee239616f8cb38ad89e8a697a")
-setManifestid(2091333,"6980442262811045156")
-addappid(2095001,0,"c38c5ee851fd90ea714284f31e69e3adde2d17018bef93398aba3cc91cb7548b")
-setManifestid(2095001,"4774641205750373436")
-addappid(2095002,0,"1eddf8e27184634ad945b1491c042d71f3a157e0df86f36dbe724ceb3333f8b6")
-setManifestid(2095002,"5145346364724428069")
-addappid(2095003,0,"a223ad812dca8bccde9bc8fd91c2baa033dc7b67befff5ea497641a6c24b351f")
-setManifestid(2095003,"1518757556260858728")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 305620.lua
+--Gamename The Long Dark
+addappid(305620, 1, "4b7d17191dfd796a730b8812292b2f9fd3d94f929e22af7b4a423c49b9315e6d") --Mainappid The Long Dark
+addappid(305621, 1, "2a658a052de2d030f6031e4aa4835b7b12b6646cb7142fde7f0216b06b9b3d85") --Main Windows Language Depot English The Long Dark
+setManifestid(305621, "3631420424042153719", 3306910256)
+addappid(305622, 1, "973dd3d0c6c26bcaaabe742bd507c7d2711d69bbc1f1f5c21650629e93121eca") --Main Linux Language Depot English The Long Dark
+setManifestid(305622, "2713194945771619804", 3365051520)
+addappid(305623, 1, "9960f03341efd0f41aa06b12b9df8c42a592b5d1385f29a87531bec7c84709d0") --Main Macos Language Depot English The Long Dark
+setManifestid(305623, "6729403644203628262", 3410444144)
+--Dlcname Music for The Long Dark - Volume One
+addappid(690180, 1, "04a0c3c5e7303a48d4001b88b8699cc9cec30bde0e0c7b92922c916d3b5a6696") --Dlcname Music for The Long Dark - Volume One
+setManifestid(690180, "6787748495731721057", 174210512)
+--Dlcname Music for The Long Dark - Volume Two
+addappid(1328170) --Dlcname Music for The Long Dark - Volume Two
+addappid(1328171, 1, "0e00400899a90654d87d7e0c7bb682372347b7e5fb13c25329af9d9e45ddf610") --Dlc Windows Depot Music for The Long Dark - Volume Two
+setManifestid(1328171, "4607351511778743662", 181264016)
+--Dlcname The Long Dark: Tales from the Far Territory
+addappid(2091330) --Dlcname The Long Dark: Tales from the Far Territory
+addappid(2091331, 1, "ef5d6edc9a2c7de7d0c7e187558780cc5724114441d0e5cfab386460042b0975") --Dlc Windows Depot The Long Dark: Tales from the Far Territory
+setManifestid(2091331, "6934285033560594235", 523696240)
+addappid(2091332, 1, "71e3e4cc7b1539fd8cf9f21f16c4c191d9927f04656a51ab06a8e03051a4b612") --Dlc Macos Depot The Long Dark: Tales from the Far Territory
+setManifestid(2091332, "5519621439095186356", 524289952)
+addappid(2091333, 1, "9cdb1d532c9c2a2315abf67c51cc52d39725194ee239616f8cb38ad89e8a697a") --Dlc Linux Depot The Long Dark: Tales from the Far Territory
+setManifestid(2091333, "8511028114621659541", 529044928)
+--Dlcname The Long Dark: WINTERMUTE
+addappid(2095000) --Dlcname The Long Dark: WINTERMUTE
+addappid(2095001, 1, "c38c5ee851fd90ea714284f31e69e3adde2d17018bef93398aba3cc91cb7548b") --Dlc Windows Depot The Long Dark: WINTERMUTE
+setManifestid(2095001, "8686842080203135951", 12059666592)
+addappid(2095002, 1, "1eddf8e27184634ad945b1491c042d71f3a157e0df86f36dbe724ceb3333f8b6") --Dlc Macos Depot The Long Dark: WINTERMUTE
+setManifestid(2095002, "7110216230137553989", 10747259008)
+addappid(2095003, 1, "a223ad812dca8bccde9bc8fd91c2baa033dc7b67befff5ea497641a6c24b351f") --Dlc Linux Depot The Long Dark: WINTERMUTE
+setManifestid(2095003, "2845013450662144547", 10397895216)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Music for The Long Dark - Volume Three
