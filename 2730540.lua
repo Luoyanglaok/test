6416e61516a0a0824c94437c031c7debb9f280ee
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:58:39 +0800

    add 2730540.lua

--- 2730540.lua
+++ 2730540.lua
@@ -1,5 +1,25 @@
-addappid(2730540)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2730541,0,"bb15eb56e2ff9e3d98cd71816938db84ca3be50a6482bbfdf6c0748d91d2c06e")
-setManifestid(2730541,"3797792820264534466")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2730540.lua
+--Gamename Card-en-Ciel
+addappid(2730540, 1, "4e3f547ac7a808f58b779f87a34e0d98a551a2958a29f4f74d01a8f77822ff51") --Mainappid Card-en-Ciel
+addappid(2730541, 1, "bb15eb56e2ff9e3d98cd71816938db84ca3be50a6482bbfdf6c0748d91d2c06e") --Main Windows Depot Card-en-Ciel
+setManifestid(2730541, "4827811689683470104", 5490432096)
+--Dlcname Card-en-Ciel Soundtrack
+addappid(3306340) --Dlcname Card-en-Ciel Soundtrack
+addappid(3306341, 1, "02b3cbb419684e4c81a880ac7a8f54c6a8b4d89928133d1c8b7a15b1d44a3928") --Dlc Windows Depot Card-en-Ciel Soundtrack
+setManifestid(3306341, "1201790577272217515", 270649568)
+--Dlcname Card-en-Ciel - Neon Additional Card
+addappid(4164630, 1, "dd247ccad3eb8baff680775a45d57ae08b0d7e44b40b1d00bc88e9345e2b1c76") --Dlcname Card-en-Ciel - Neon Additional Card
+setManifestid(4164630, "3609594940054362317", 699344)
+--Dlcname Card-en-Ciel - The World of "Gal Guardians Servants of the Dark"
+addappid(4314030, 1, "9510bf217dd8787960cfb9b01af3f4f39a59814752be23dec078c5e6e6818e43") --Dlcname Card-en-Ciel - The World of "Gal Guardians Servants of the Dark"
+setManifestid(4314030, "5332367816122997643", 66442976)
+--Dlcname Card-en-Ciel - The World of the "Blaster Master Zero Series"
+addappid(4314040, 1, "0c49060f2c3d79948ffeba9b286daea77cc535fc2c0b5cb5d22a88726a087d82") --Dlcname Card-en-Ciel - The World of the "Blaster Master Zero Series"
+setManifestid(4314040, "3996176739430737732", 76476352)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4376020) --Dlcname Card-en-Ciel Expansion Pass
