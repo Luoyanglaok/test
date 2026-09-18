bd4d14d983a9e9e0070738b4b985eca2fa3aa8aa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 12:28:50 +0800

    add 3557750.lua

---
+++ 3557750.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3557750.lua
+--Gamename Hot and Lovely : Seduction
+addappid(3557750) --Mainappid Hot and Lovely : Seduction
+addtoken(3557750, "2665255130802731813")
+addappid(3557751, 1, "c2411a06a6d88aa0ebca19c78061441ed248a41dbeb8cf570c2077c0f15a3a4b") --Main Windows Depot Hot and Lovely : Seduction
