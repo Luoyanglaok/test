7551cb2d8d6b6b7438a746023e5d7bfb946df432
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 19:31:21 +0800

    add 2072450.lua

--- 2072450.lua
+++ 2072450.lua
@@ -1,4 +1,29 @@
-addappid(2072450)
-setManifestid(228989,"3514306556860204959")
-addappid(2072451,0,"77360feef0acf98c8a6727d3cca6f0bd7ede2c3a36fa467945ff217a7067e002")
-setManifestid(2072451,"4882956135694037628")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2072450.lua
+--Gamename Like a Dragon: Infinite Wealth
+addappid(2072450, 1, "b542db2fcb88d550bd6d79d68eb1a3b46a3c87fe799ce18f1f97f0c3cc2b4481") --Mainappid Like a Dragon: Infinite Wealth
+addappid(2072451, 1, "77360feef0acf98c8a6727d3cca6f0bd7ede2c3a36fa467945ff217a7067e002") --Main Windows Depot Like a Dragon: Infinite Wealth
+setManifestid(2072451, "4882956135694037628", 64361885760)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2562820) --Dlcname Like a Dragon: Infinite Wealth - Master Vacation Bundle
+addappid(2562830) --Dlcname Like a Dragon: Infinite Wealth - Assorted Outfit Bundle
+addappid(2562840) --Dlcname Like a Dragon: Infinite Wealth - Yakuza CD Collection Set
+addappid(2562850) --Dlcname Like a Dragon: Infinite Wealth - Sujimon & Resort Bundle
+addappid(2562860) --Dlcname Like a Dragon: Infinite Wealth - Special Job Set
+addappid(2584300) --Dlcname Like a Dragon: Infinite Wealth - Hero's Booster Pack
+addappid(2606390) --Dlcname Like a Dragon: Infinite Wealth - Gearworks Crafting Set (Medium)
+addappid(2606400) --Dlcname Like a Dragon: Infinite Wealth - Gearworks Crafting Set (Large)
+addappid(2606410) --Dlcname Like a Dragon: Infinite Wealth - Self-Improvement Booster Set (Small)
+addappid(2606420) --Dlcname Like a Dragon: Infinite Wealth - Self-Improvement Booster Set (Medium)
+addappid(2606430) --Dlcname Like a Dragon: Infinite Wealth - Self-Improvement Booster Set (Large)
+addappid(2606440) --Dlcname Like a Dragon: Infinite Wealth - Job Leveling Set (Small)
+addappid(2606450) --Dlcname Like a Dragon: Infinite Wealth - Job Leveling Set (Medium)
+addappid(2606460) --Dlcname Like a Dragon: Infinite Wealth - Job Leveling Set (Large)
+addappid(2606470) --Dlcname Like a Dragon: Infinite Wealth - Leveling Set (Small)
+addappid(2606480) --Dlcname Like a Dragon: Infinite Wealth - Leveling Set (Medium)
+addappid(2606490) --Dlcname Like a Dragon: Infinite Wealth - Leveling Set (Large)
+addappid(2606500) --Dlcname Like a Dragon: Infinite Wealth - Leveling Set (Extra Large)
+addappid(2606510) --Dlcname Like a Dragon: Infinite Wealth - Legendary Booster Pack
+addappid(2669420) --Dlcname Like a Dragon: Infinite Wealth - Special Outfit: Hello Work Employee (Ichiban)
