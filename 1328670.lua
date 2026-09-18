cc2cf6d639ace290a17f8f89ce4b36aedd14b1b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 11:01:56 +0800

    add 1328670.lua

--- 1328670.lua
+++ 1328670.lua
@@ -1,23 +1,30 @@
-addappid(1328670)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1328671,0,"1b520c64f75ebe1a7f790333c5853f8b457add2b86420baf131e3745da969f7a")
-setManifestid(1328671,"8624289317820175979")
-addappid(1328672,0,"69d012c8068dd2c4629138cbec801bbb0a734de4599de6fe2bb6474998f28b39")
-setManifestid(1328672,"6245399057144560046")
-addappid(1328673,0,"a221c0fffb7d13defb492a08b2b374f4c7985e350949704da3c5b898281b9b8c")
-setManifestid(1328673,"1093442016851462013")
-addappid(1328674,0,"2cb09b62df0a5695580ee9fee27f68f82ca5da05dc6f7f3bf8a7fdcf4c2d8ec8")
-setManifestid(1328674,"714752994269792427")
-addappid(1328675,0,"9a76443f9117cc6c61740bed586fdbacc37f1e75253263fbf9a1da1b1fa2c79e")
-setManifestid(1328675,"8839585240582289122")
-addappid(1328676,0,"1ddb6edf2f05c57067bd35342fa6d29a565858b6980cdae592d9260b07c1249c")
-setManifestid(1328676,"8330674821888864767")
-addappid(1328677,0,"96579200c174b23b82fef4c3a1c5edf870062c0d3b853fe93ea5b0280aa45f75")
-setManifestid(1328677,"506201563779209150")
-addappid(1328678,0,"e8c1cfa35825cc97c40ee7f2ff2f294a3187d21c9f276f736c1daa8b660335a5")
-setManifestid(1328678,"6913537578765169955")
-addappid(1328679,0,"aa32bd82a4477fea9f805725a12c6514ef11cbc069f1029161abfde1451d9e22")
-setManifestid(1328679,"6021402890254141810")
-addappid(3340991,0,"023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491")
-setManifestid(3340991,"1318423300601359782")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1328670.lua
+--Gamename Mass Effect™ Legendary Edition
+addappid(1328670, 1, "5373b526311b32bd4b9aeb8dd7617c9de6d99ebdcf13a55a8ac689d61eff128b") --Mainappid Mass Effect™ Legendary Edition
+addappid(1328671, 1, "1b520c64f75ebe1a7f790333c5853f8b457add2b86420baf131e3745da969f7a") --Main Windows Depot Mass Effect™ Legendary Edition
+setManifestid(1328671, "8624289317820175979", 104013606224)
+addappid(1328672, 1, "69d012c8068dd2c4629138cbec801bbb0a734de4599de6fe2bb6474998f28b39") --Main Windows Language Depot English Mass Effect™ Legendary Edition
+setManifestid(1328672, "6245399057144560046", 25552)
+addappid(1328673, 1, "a221c0fffb7d13defb492a08b2b374f4c7985e350949704da3c5b898281b9b8c") --Main Windows Language Depot German Mass Effect™ Legendary Edition
+setManifestid(1328673, "1093442016851462013", 24768)
+addappid(1328674, 1, "2cb09b62df0a5695580ee9fee27f68f82ca5da05dc6f7f3bf8a7fdcf4c2d8ec8") --Main Windows Language Depot Spanish Mass Effect™ Legendary Edition
+setManifestid(1328674, "714752994269792427", 27744)
+addappid(1328675, 1, "9a76443f9117cc6c61740bed586fdbacc37f1e75253263fbf9a1da1b1fa2c79e") --Main Windows Language Depot Russian Mass Effect™ Legendary Edition
+setManifestid(1328675, "8839585240582289122", 30816)
+addappid(1328676, 1, "1ddb6edf2f05c57067bd35342fa6d29a565858b6980cdae592d9260b07c1249c") --Main Windows Language Depot Japanese Mass Effect™ Legendary Edition
+setManifestid(1328676, "8330674821888864767", 30768)
+addappid(1328677, 1, "96579200c174b23b82fef4c3a1c5edf870062c0d3b853fe93ea5b0280aa45f75") --Main Windows Language Depot French Mass Effect™ Legendary Edition
+setManifestid(1328677, "506201563779209150", 27456)
+addappid(1328678, 1, "e8c1cfa35825cc97c40ee7f2ff2f294a3187d21c9f276f736c1daa8b660335a5") --Main Windows Language Depot Italian Mass Effect™ Legendary Edition
+setManifestid(1328678, "6913537578765169955", 26368)
+addappid(1328679, 1, "aa32bd82a4477fea9f805725a12c6514ef11cbc069f1029161abfde1451d9e22") --Main Windows Language Depot Polish Mass Effect™ Legendary Edition
+setManifestid(1328679, "6021402890254141810", 28576)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") --Share Windows Depot EA APP Installer
+setManifestid(3340991, "3755387105551220105", 246722480)
+addappid(1393160) --Dlcname Mass Effect™ Legendary Edition - Key
