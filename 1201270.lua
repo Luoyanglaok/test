ba18201cc9ba02e84264879e89f835705dc44a69
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:26:16 +0800

    add 1201270.lua

--- 1201270.lua
+++ 1201270.lua
@@ -1,5 +1,13 @@
-addappid(1201270)
-addappid(1201271,0,"059aab9cc6be1eb88f8bc9f00435944cbb6da3d97bf77c1321194c77f7eb9345")
-setManifestid(1201271,"6189556093350933795")
-addappid(2050830,0,"5406f763ce10cb0c5838e6901d1e20be36452695e30b0c987e870ac1feecaa04")
-setManifestid(2050830,"4653805756932442876")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1201270.lua
+--Gamename A Space for the Unbound
+addappid(1201270) --Mainappid A Space for the Unbound
+addtoken(1201270, "11593083983154065347")
+addappid(1201271, 1, "059aab9cc6be1eb88f8bc9f00435944cbb6da3d97bf77c1321194c77f7eb9345") --Main Windows Depot A Space for the Unbound
+setManifestid(1201271, "6189556093350933795", 313048496)
+--Dlcname A Space for the Unbound - Artbook
+addtoken(2050830, "3173906344167853105")
+addappid(2050830, 1, "5406f763ce10cb0c5838e6901d1e20be36452695e30b0c987e870ac1feecaa04") --Dlcname A Space for the Unbound - Artbook
+setManifestid(2050830, "4653805756932442876", 81179120)
+addappid(2050790) --Dlcname A Space for the Unbound - Original Soundtrack
+addtoken(2050790, "18256121407537156694")
