582fec6d8ecd1ed1254880465077897e1d1bf9c2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 10:55:10 +0800

    add 1259420.lua

--- 1259420.lua
+++ 1259420.lua
@@ -1,5 +1,14 @@
-addappid(1259420)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1259421,0,"3a92a76d80b16038abe5b2831f554a22120535ab039dbde663a70c8b52afe11b")
-setManifestid(1259421,"9008749683399927967")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1259420.lua
+--Gamename Days Gone
+addappid(1259420, 1, "a84c754cff9b4845e7575ad3155bd88d2cacd91bf1cad7137adc1b612b94df3b") --Mainappid Days Gone
+addappid(1259421, 1, "3a92a76d80b16038abe5b2831f554a22120535ab039dbde663a70c8b52afe11b") --Main Windows Depot Days Gone
+setManifestid(1259421, "9008749683399927967", 60209250528)
+addappid(1259422, 1, "95e707b118a213e71a3e7ba7a6ee3a6af3e545fb87206f8219cf7a415c359231") --Main Windows Depot Days Gone
+setManifestid(1259422, "9075941046550830080", 45244380128)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3238470) --Dlcname Days Gone - Broken Road DLC
