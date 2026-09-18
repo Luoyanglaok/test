227b6fa594cd2dbd1fd99fcad240e117b3b41bee
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 17:27:26 +0800

    add 3590.lua

--- 3590.lua
+++ 3590.lua
@@ -1,2 +1,8 @@
-addappid(3590)
-setManifestid(3591,"7598841597413163537")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3590.lua
+--Gamename Plants vs. Zombies GOTY Edition
+addappid(3590, 1, "61d2d35ff8665affa1677a8488ea60016655e678bccaab9098c96ef686362f52") --Mainappid Plants vs. Zombies GOTY Edition
+addappid(3591, 1, "1625fdac425750d0736c420f3877d3b4b4119d1d361629c2d12b360eb3689839") --Main Windows Depot Plants vs. Zombies GOTY Edition
+setManifestid(3591, "7598841597413163537", 0)
+addappid(3593, 1, "2fec966bebbf74b5fad937c61ad06d393d521b64f27d97992ae7ef3775c50c10") --Main Macos Depot Plants vs. Zombies GOTY Edition
+setManifestid(3593, "2896720143485326882", 0)
