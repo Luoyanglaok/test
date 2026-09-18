10a9c7ab841f83069c0b287f11e4346a27e14be6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 12:33:56 +0800

    add 12100.lua

--- 12100.lua
+++ 12100.lua
@@ -1,3 +1,6 @@
-addappid(12100)
-addappid(12101,0,"a6808fc3d1c16fdf788138c0cfbb9fb7602de9d9018b0091f2ab65c8b7f7e82a")
-setManifestid(12101,"1807349195210842305")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12100.lua
+--Gamename Grand Theft Auto III
+addappid(12100) --Mainappid Grand Theft Auto III
+addappid(12101, 1, "a6808fc3d1c16fdf788138c0cfbb9fb7602de9d9018b0091f2ab65c8b7f7e82a") --Main Windows Depot Grand Theft Auto III
+setManifestid(12101, "1807349195210842305", 0)
