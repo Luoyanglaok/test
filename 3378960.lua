6337ae2c0b355a7effa9aae6418dab1a0c91d2e8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 10:50:12 +0800

    add 3378960.lua

---
+++ 3378960.lua
@@ -0,0 +1,26 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3378960.lua
+--Gamename Lost Soul Aside™
+addappid(3378960, 1, "4b96d8ac9da3edf7f6f2180861d547c5aada6f27c922737891be9790abc88e3b") --Mainappid Lost Soul Aside™
+addappid(3378961, 1, "02157efe6285cacaa20730d8f0bd83c4b020ec25cd0c75e88ad7ac83465a570a") --Main Windows Depot Lost Soul Aside™
+setManifestid(3378961, "8724312632676515323", 89296270768)
+--Dlcname Lost Soul Aside™- Digital Artbook
+addappid(3484240) --Dlcname Lost Soul Aside™- Digital Artbook
+addappid(3484241, 1, "850583f98b56a2b21b65c877cf536c83830fa54c21f30ce3c6397935f065b2bc") --Dlc Windows Depot Lost Soul Aside™- Digital Artbook
+setManifestid(3484241, "7238494630064099886", 61334592)
+--Dlcname Lost Soul Aside™: Digital Deluxe Edition - Soundtrack
+addappid(3484250) --Dlcname Lost Soul Aside™: Digital Deluxe Edition - Soundtrack
+addappid(3484251, 1, "4d96b2afbcfcbba0a4e98d2666b43834b25af48aa4f3c58e84e0af4381bd8b48") --Dlc Windows Depot Lost Soul Aside™: Digital Deluxe Edition - Soundtrack
+setManifestid(3484251, "7206341750101004194", 2181491568)
+addappid(3484252, 1, "2417138851dffb147cdb3bed4780fe31f95895762758fb4a2a8e678c29e3e439") --Dlc Windows Depot Lost Soul Aside™: Digital Deluxe Edition - Soundtrack
+setManifestid(3484252, "4835736521149264600", 310047776)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+--Missing Dlcs
+--Dlcname Lost Soul Aside™- Upgrade to Digital Deluxe
+--Dlcname Lost Soul Aside™- Pre-purchase Entitlement
