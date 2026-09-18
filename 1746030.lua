34257e1e98ba5a9ff8c9ebfbb7910b1f1cb01148
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:18:12 +0800

    add 1746030.lua

--- 1746030.lua
+++ 1746030.lua
@@ -1,3 +1,14 @@
-addappid(1746030)
-addappid(2808420)
-setManifestid(1746031,"8409351989732700752")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1746030.lua
+--Gamename 山河旅探 - Murders on the Yangtze River
+addappid(1746030) --Mainappid 山河旅探 - Murders on the Yangtze River
+addappid(1746031, 1, "d2e49cb79445331fa5ecbcd064628f63bb7564a3ee8ca7aaefbcf8034f5722ea") --Main Windows Depot 山河旅探 - Murders on the Yangtze River
+setManifestid(1746031, "8409351989732700752", 5713920848)
+addappid(1746032, 1, "14857b18effdcdcff1ba5225c733118fe29fcf35c5d6d931edd7da5b1f47f7bd") --Main Macos Depot 山河旅探 - Murders on the Yangtze River
+setManifestid(1746032, "6419634032717834803", 5216525168)
+--Dlcname 山河旅探音乐原声集 - Murders on the Yangtze River Soundtrack
+addappid(2808420) --Dlcname 山河旅探音乐原声集 - Murders on the Yangtze River Soundtrack
+addappid(2808421, 1, "55a55c77ef1b81abbc62175ec0df4537e22c15e3c62e3a08b28d3af2a0d7ef3d") --Dlc Windows Depot 山河旅探音乐原声集 - Murders on the Yangtze River Soundtrack
+setManifestid(2808421, "4732325115372692492", 127487040)
+addappid(2808422, 1, "4813bb0d4373c67ab9e087a9fc69f32ca805445e6fbef13bd6220d20a7505bff") --Dlc Windows Depot 山河旅探音乐原声集 - Murders on the Yangtze River Soundtrack
+setManifestid(2808422, "4194744886643376388", 1007819376)
