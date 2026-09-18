9e45797a27939d7b043511c28f901b8ca8645d81
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 12:00:51 +0800

    add 3690030.lua

--- 3690030.lua
+++ 3690030.lua
@@ -3,8 +3,9 @@
 --Gamename The Last Gas Station
 addappid(3690030, 1, "9c831c125283abd62011549d1b427b41304c95601a2bca17eda346863cfa9f93") --Mainappid The Last Gas Station
 addappid(3690031, 1, "6e046d7bf13d154a199fdcd2f9a1bedd5de66dc45496e4db97af27a0f31fc5d9") --Main Windows Depot The Last Gas Station
-setManifestid(3690031, "9002499117642193864", 684978288)
+setManifestid(3690031, "1929039428877998559", 721222096)
 addappid(3690032, 1, "41aaa4bb7e847316b65b3df578c82cae1a6789c35a9c906b776aa156d1802a55") --Main Macos Depot The Last Gas Station
-setManifestid(3690032, "2042864954941438476", 709703744)
+setManifestid(3690032, "4278799811333842373", 755137312)
 --Missing Dlcs
 --Dlcname The Last Gas Station Soundtrack
+--Dlcname The Last Gas Station - Supporter Pack
