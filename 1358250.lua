07e2d811487b89c474a27d994225332592d552ad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 08:49:37 +0800

    add 1358250.lua

--- 1358250.lua
+++ 1358250.lua
@@ -1,9 +1,14 @@
-addappid(1358250)
-addappid(1358251,0,"2c4814db747d2278c49ba72c2001bb39a857bc26f980d9d54b3f31c027e7cc48")
-setManifestid(1358251,"3292884553172540444")
-addappid(1358252,0,"9bd6786218cc737af10384d4404418c59dbee8450b0f820b38cd33c01ecf67ae")
-setManifestid(1358252,"7920955957010170742")
-addappid(1385850,0,"b693ffdfe811030a735b70b587601ce26758aab13cfa40e674c08b117474bbbe")
-setManifestid(1385850,"8888856415849209222")
-addappid(3169990,0,"5a6c1d1472aed2af663041e89fe0b461cc1358c503a0a65aad1554885234c4c8")
-setManifestid(3169990,"7662012409463469317")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1358250.lua
+--Gamename City of Broken Dreamers: Book One
+addappid(1358250) --Mainappid City of Broken Dreamers: Book One
+addappid(1358251, 1, "2c4814db747d2278c49ba72c2001bb39a857bc26f980d9d54b3f31c027e7cc48") --Main Windows Depot City of Broken Dreamers: Book One
+setManifestid(1358251, "3292884553172540444", 4842985184)
+addappid(1358252, 1, "9bd6786218cc737af10384d4404418c59dbee8450b0f820b38cd33c01ecf67ae") --Main Macos Depot City of Broken Dreamers: Book One
+setManifestid(1358252, "7920955957010170742", 4860831344)
+--Dlcname City of Broken Dreamers Game Guide
+addappid(1385850, 1, "b693ffdfe811030a735b70b587601ce26758aab13cfa40e674c08b117474bbbe") --Dlcname City of Broken Dreamers Game Guide
+setManifestid(1385850, "8888856415849209222", 24440592)
+--Dlcname City of Broken Dreamers: Book Two
+addappid(3169990, 1, "5a6c1d1472aed2af663041e89fe0b461cc1358c503a0a65aad1554885234c4c8") --Dlcname City of Broken Dreamers: Book Two
+setManifestid(3169990, "7662012409463469317", 11178985952)
