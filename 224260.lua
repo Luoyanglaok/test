6d0ff1f1151a7c856c621ab0110be282af41300b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 17:35:11 +0800

    add 224260.lua

---
+++ 224260.lua
@@ -0,0 +1,22 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 224260.lua
+--Gamename No More Room in Hell
+addappid(224260, 1, "5d8c4ae23001dbb1c64b949b3b5b922a95e9887db5073bf864e231bd9669fef4") --Mainappid No More Room in Hell
+addappid(224261, 1, "bfd0df36252eee487fc1f04740fe609457da8672f9d0672da0a96026f09109be") --Main Windows Depot No More Room in Hell
+setManifestid(224261, "7439796245749018791", 2974592496)
+addappid(224262, 1, "6c375fbc3957e3f37c6faec07844205e5069a1cdd2ece11b8918e901599f1f7c") --Main Windows Depot No More Room in Hell
+setManifestid(224262, "2096814040346351896", 13811968)
+addappid(224263, 1, "5dc0b6adc1c917ae04472eda3063a2f678b3ae662e02f71428eb98859e539c88") --Main Linux Depot No More Room in Hell
+setManifestid(224263, "5891712091933839480", 20866608)
+addappid(224265, 1, "405e32e56a563553f8c3b22f9b6655e1a871f43b6b4fd95220243cf46e91442b") --Main Windows Depot No More Room in Hell
+setManifestid(224265, "3771566521610106510", 1870912272)
+addappid(224266, 1, "db5c882cdbfa354e9db8b98d5fdec7cb2f631225c5bed500ed5ffab7ef77cb0a") --Main Linux Depot No More Room in Hell
+setManifestid(224266, "9078342065902040751", 1798987520)
+--Dlcname No More Room in Hell - Original Soundtrack Volume 1
+addappid(310660, 1, "44f3049ae1bb29b26b1c535721495f8370ee4afbe571513bd445820746bcdf0c") --Dlcname No More Room in Hell - Original Soundtrack Volume 1
+setManifestid(310660, "7170683187232556788", 134944272)
+addappid(310661, 1, "721f1ffb29533b2278f000d9154dee5f5d34ba53257a31fba77091abed03ccb8") --Dlc Windows Depot No More Room in Hell - Original Soundtrack Volume 1
+setManifestid(310661, "758102933840971922", 538884528)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
