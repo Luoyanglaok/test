03f054ec1811798db1c38c65a14ba9455fdd98a0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 16:06:11 +0800

    add 4317690.lua

---
+++ 4317690.lua
@@ -0,0 +1,18 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4317690.lua
+--Gamename Crimson Moon
+addappid(4317690, 1, "bf0e2d63fd004ffcc5dd1ed38f5a7afe0354a502aa366e1391c2979d0e53e6cb") --Mainappid Crimson Moon
+addappid(4317691, 1, "54374d8fbe8837e5d9bfdb27b4d678cf9905989b976f2e08825bbfd3c5ba96df") --Main Windows Depot Crimson Moon
+setManifestid(4317691, "2024145623675143877", 33139115056)
+addappid(4859731, 1, "3d812f752ca97e58c48b21d1575f516d74cedd2b93b06eb157f0ab4baeedc548") --Main Windows Depot Crimson Moon
+setManifestid(4859731, "6777624950037544041", 317747568)
+addappid(4859732, 1, "523e8ff1b527b9a277a4cb3ebc761d60eebe0eb0adb402c7caa46da752a39dd3") --Main Windows Depot Crimson Moon
+setManifestid(4859732, "6699589182114983616", 1420575056)
+--Dlcname Crimson Moon Soundtrack
+addappid(4859730) --Dlcname Crimson Moon Soundtrack
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4984080) --Dlcname Crimson Moon: Deluxe Edition Upgrade
