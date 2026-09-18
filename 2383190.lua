030bb81a684df1b3a2caeee51b0a3b88b96fb1ae
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 09:49:58 +0800

    add 2383190.lua

---
+++ 2383190.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2383190.lua
+--Gamename Creeping Deck: Pharaoh's Curse Prologue
+addappid(2383190) --Mainappid Creeping Deck: Pharaoh's Curse Prologue
+addappid(2383191, 1, "ee475ba7284b5c14f74075a3828e0add0ee5b23d23ddeb06d0eb48cb9192b594") --Main Windows Depot Creeping Deck: Pharaoh's Curse Prologue
+setManifestid(2383191, "8350899563090206472", 260849344)
