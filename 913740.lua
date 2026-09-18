8c17c2276058a4f85d7ebed2cf75efba00407c68
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:22:52 +0800

    add 913740.lua

--- 913740.lua
+++ 913740.lua
@@ -1,5 +1,14 @@
-addappid(913740)
-addappid(913741,0,"9010c47615015e2605a9d68ebc215d4888b67cc35c6f34d76a181ff1d507a00f")
-setManifestid(913741,"5253961519843226787")
-addappid(913742,0,"2af6cda9c214b4ac3f6b3ced0d979d21c0a3f08f6355c62dd7c48cdc5c806416")
-setManifestid(913742,"8939061719549407918")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 913740.lua
+--Gamename WORLD OF HORROR
+addappid(913740, 1, "a5400ba2dffdd5b9b65b320bd02ba86544edc13a234dd4b1905b6112be882aee") --Mainappid WORLD OF HORROR
+addappid(913741, 1, "9010c47615015e2605a9d68ebc215d4888b67cc35c6f34d76a181ff1d507a00f") --Main Windows Depot WORLD OF HORROR
+setManifestid(913741, "5253961519843226787", 248500432)
+addappid(913742, 1, "2af6cda9c214b4ac3f6b3ced0d979d21c0a3f08f6355c62dd7c48cdc5c806416") --Main Windows Depot WORLD OF HORROR
+setManifestid(913742, "8939061719549407918", 255653008)
+--Dlcname WORLD OF HORROR Soundtrack
+addappid(2658360) --Dlcname WORLD OF HORROR Soundtrack
+addappid(2658361, 1, "778d264aec0362b5d79d05a1ba055b43396883474237bda45519b344482d0fb4") --Dlc Windows Depot WORLD OF HORROR Soundtrack
+setManifestid(2658361, "9037201660799431267", 60966832)
+addappid(2658362, 1, "201556ff0148ccbdc9ae7fff92d085f05de9fd6388d0bcc5aac7fddf36792244") --Dlc Windows Depot WORLD OF HORROR Soundtrack
+setManifestid(2658362, "4505325321846063021", 427560096)
