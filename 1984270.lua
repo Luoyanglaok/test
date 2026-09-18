f765399686b4765b54c185dafda4954b074ad5e4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 13:27:26 +0800

    add 1984270.lua

---
+++ 1984270.lua
@@ -0,0 +1,57 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1984270.lua
+--Gamename Digimon Story Time Stranger
+addappid(1984270, 1, "60f0f52f51592d35101a1678ff221736cee376b3a9db46229f5164dd95b70add") --Mainappid Digimon Story Time Stranger
+addappid(1984271, 1, "b0aa8ddfef2996e3122b0bda1d02f7ceb28f608ac8c1d853e6793790aaab8860") --Main Windows Depot Digimon Story Time Stranger
+setManifestid(1984271, "2791297361435523425", 25955046400)
+--Dlcname Digimon Story Time Stranger - Pre-Order Bonus Pack
+addappid(3524240, 1, "f06dc8a525a2fb5e03453da570b80915bd7c02daae714ac854c86283a2bc4e18") --Dlcname Digimon Story Time Stranger - Pre-Order Bonus Pack
+setManifestid(3524240, "8093752377166947780", 34777712)
+--Dlcname Digimon Story Time Stranger - Farm Item Golden Moai
+addappid(3524250, 1, "b6c098c1cdd8687907373e2a6e59446445aafca92e10fe4da2c59481f0448f24") --Dlcname Digimon Story Time Stranger - Farm Item Golden Moai
+setManifestid(3524250, "6473051103911318870", 106080)
+--Dlcname Digimon Story Time Stranger - Additional Digimon & Episode Pack 1: Alternate Dimension
+addappid(3524260, 1, "0d0cb59fa779a35a600989d6bbbd8a43d4459245ff74f28b125e3089d2c59940") --Dlcname Digimon Story Time Stranger - Additional Digimon & Episode Pack 1: Alternate Dimension
+setManifestid(3524260, "8571881714801507144", 192016400)
+--Dlcname Digimon Story Time Stranger - Additional Digimon & Episode Pack 2: GAKU−RAN
+addappid(3524270, 1, "efc19afeba78735ba1acc7264f8363b01d290e19fd5bbde3c9b6956d6dbbd750") --Dlcname Digimon Story Time Stranger - Additional Digimon & Episode Pack 2: GAKU−RAN
+setManifestid(3524270, "3349932728113880252", 198107840)
+--Dlcname Digimon Story Time Stranger - Additional Digimon & Episode Pack 3: Anti-ParadoX
+addappid(3524290, 1, "06a3cc7ad7dabf95f282c5ef83ac8eef78605e99999eadcf84aeb73ee22b6eee") --Dlcname Digimon Story Time Stranger - Additional Digimon & Episode Pack 3: Anti-ParadoX
+setManifestid(3524290, "7307741484539899956", 174579248)
+--Dlcname Digimon Story Time Stranger - Costume Cyber Sleuth Set
+addappid(3524300, 1, "0ae80de6e7b2f3ec166de6a98b3f6a018af9fba6c54f144916d1f4ab07671b2c") --Dlcname Digimon Story Time Stranger - Costume Cyber Sleuth Set
+setManifestid(3524300, "2166254318281553691", 25348832)
+--Dlcname Digimon Story Time Stranger - Early Unlock: Special Agumon & Gabumon
+addappid(3524310, 1, "5f8a85a36740263fe78affe346fd7b0c18eb7ad2a51537a2c875f64fa080cbf1") --Dlcname Digimon Story Time Stranger - Early Unlock: Special Agumon & Gabumon
+setManifestid(3524310, "6631771504687800132", 2496)
+--Dlcname Digimon Story Time Stranger - Costume Swimwear Set
+addappid(3524320, 1, "0da086161abea2f6ce0863a530fefc41ac12993fb4308ff16301926398803a27") --Dlcname Digimon Story Time Stranger - Costume Swimwear Set
+setManifestid(3524320, "2937343108483999987", 2112)
+--Dlcname Digimon Story Time Stranger - Costume Chosen Children Set
+addappid(3524330, 1, "2358d5ff200cefb1eef6f77277f04089a01daaea4418cd48d530b4de3fe53f61") --Dlcname Digimon Story Time Stranger - Costume Chosen Children Set
+setManifestid(3524330, "6969047721551132609", 1792)
+--Dlcname Digimon Story Time Stranger - Costume Digimon Costume Set
+addappid(3524340, 1, "5441f08ccf7ebad6314bac9d071ba47a41e709f4000e121004752959138bb2dc") --Dlcname Digimon Story Time Stranger - Costume Digimon Costume Set
+setManifestid(3524340, "1407478222003555040", 688)
+--Dlcname Digimon Story Time Stranger - Costume Public Safety Suit & Special Supplies Set
+addappid(3524350, 1, "388eb5d2862631b327cd83e312bc69e3f14ecf6087a6a2ec0860f20df764139d") --Dlcname Digimon Story Time Stranger - Costume Public Safety Suit & Special Supplies Set
+setManifestid(3524350, "8358207152149625529", 206848)
+--Dlcname Digimon Story Time Stranger - Cyber Sleuth BGM Pack
+addappid(3524360, 1, "9ac71aa654d8726aaf514236b138ac5c075854dd8f33abbfeb1b4316302c7f7b") --Dlcname Digimon Story Time Stranger - Cyber Sleuth BGM Pack
+setManifestid(3524360, "7211903957130876538", 361357088)
+--Dlcname Digimon Story Time Stranger - Digimon Anime Song Pack
+addappid(3524370, 1, "a20f41adefa6edd0b1a0644a6e359437cc9476624a4e80b6b31d90c893fb3577") --Dlcname Digimon Story Time Stranger - Digimon Anime Song Pack
+setManifestid(3524370, "7484848285842310833", 47903168)
+--Dlcname Digimon Story Time Stranger - Outer Dungeons The Halls of EXP, Gold, Materials
+addappid(3524380, 1, "4ef7dc743aea30597adbffc2215a364e119c16ab160dc2f20ae358296d12f4bf") --Dlcname Digimon Story Time Stranger - Outer Dungeons The Halls of EXP, Gold, Materials
+setManifestid(3524380, "9165147238607416395", 1616)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(3524390) --Dlcname Digimon Story Time Stranger - Season Pass
+addappid(3524400) --Dlcname Digimon Story Time Stranger - Costume Pack
