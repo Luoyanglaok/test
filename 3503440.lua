759decab09f086e23a14b461c9b0b1a6831616d1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 21:19:24 +0800

    add 3503440.lua

--- 3503440.lua
+++ 3503440.lua
@@ -1,3 +1,6 @@
-addappid(3503440)
-addappid(3503441,0,"3fcd0cd6fb634ba269eb29f2ad970f6753548331145c13247d5626f9a03b4749")
-setManifestid(3503441,"7443355080766888463")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3503440.lua
+--Gamename Astro Prospector
+addappid(3503440) --Mainappid Astro Prospector
+addappid(3503441, 1, "3fcd0cd6fb634ba269eb29f2ad970f6753548331145c13247d5626f9a03b4749") --Main Windows Depot Astro Prospector
+setManifestid(3503441, "8177376706506906119", 93406848)
