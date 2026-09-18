c1d10dc4188719f98675822f85579b6ea7319ed0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 16:01:15 +0800

    add 2947440.lua

---
+++ 2947440.lua
@@ -0,0 +1,26 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2947440.lua
+--Gamename SILENT HILL f
+addappid(2947440, 1, "4f97e37360a346acfc127b9a4cd35dffd337d2f3bde443730f0281eb4943a969") --Mainappid SILENT HILL f
+addappid(2947441, 1, "5329bd39507637997739c33a4b5737f9539ee434a7b79bd875b5fc7d1337c684") --Main Windows Depot SILENT HILL f
+setManifestid(2947441, "8694249231237617336", 41513342640)
+--Dlcname SILENT HILL f - Bonus Content
+addtoken(3282720, "186020997252537705")
+addappid(3282720) --Dlcname SILENT HILL f - Bonus Content
+addappid(3282721, 1, "fd38a70c015f046a880fc4db0487fcccdf05e009b286cc4175abdddf61511342") --Dlc Windows Depot SILENT HILL f - Bonus Content
+setManifestid(3282721, "9044674092896486181", 3143162000)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3282160) --Dlcname SILENT HILL f - White Sailor School Uniform
+addtoken(3282160, "1723734955608826062")
+addappid(3282170) --Dlcname SILENT HILL f - Item Pack
+addtoken(3282170, "8745338579199605697")
+addappid(3282180) --Dlcname SILENT HILL f - Pink Rabbit Costume
+addtoken(3282180, "2435190140460799412")
+addappid(3282190) --Dlcname SILENT HILL f - Omamori: Peony
+addtoken(3282190, "17422305961579312315")
+addappid(3516200) --Dlcname SILENT HILL f - Digital Deluxe Upgrade
+addappid(4733020) --Dlcname SILENT HILL f - Crimson Butterfly Costume
