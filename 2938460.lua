55eb0000fc933ccd473a195b2c9d006009de2ea7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 12:15:39 +0800

    add 2938460.lua

---
+++ 2938460.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2938460.lua
+--Gamename Police Chief Simulator
+addappid(2938460, 1, "c48792206149a539e273312e913212b2c2794f065508ac30160dc37e1b551f59") --Mainappid Police Chief Simulator
+addappid(2938461, 1, "c48792206149a539e273312e913212b2c2794f065508ac30160dc37e1b551f59") --Main Windows Depot Police Chief Simulator
+addappid(2938462, 1, "5b1100926b4ad8336b8a199494bbb34ace4a46a8848d3cb34b6d48aea1e97764") --Main Windows Depot Police Chief Simulator
+setManifestid(2938462, "8298141876261498195", 16956176640)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
