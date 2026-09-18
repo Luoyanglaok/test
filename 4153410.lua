bc501d8925efada1f2e9bdae5db9eeaa0d8b749f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 08:46:03 +0800

    add 4153410.lua

---
+++ 4153410.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4153410.lua
+--Gamename City of Sin - The Nocturne Files
+addappid(4153410, 1, "a9ad3ac519bec1f8bf17ef3504333128adaeca7336baf506ad22996dedf5b3cb") --Mainappid City of Sin - The Nocturne Files
+addappid(4153411, 1, "d0c9c8f30865af48fc2273ede546391a01a6547d3f9856ea4265915d0bcd8404") --Main Windows Depot City of Sin - The Nocturne Files
+setManifestid(4153411, "7647754552736186049", 3407520224)
