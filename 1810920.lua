796df5e602c285b919ab3389358eff07eea98d1a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 11:03:34 +0800

    add 1810920.lua

--- 1810920.lua
+++ 1810920.lua
@@ -1,5 +1,13 @@
-addappid(1810920)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-addappid(1810921,0,"d71c539b125522b912b11034cfd7cccf8a30f3f8269e22a652740504ebf805df")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1810920.lua
+--Gamename Operation Lovecraft: Fallen Doll Playtest
+addappid(1810920, 1, "1cb5b1e4778512321cb82604d5c01adb154aedb644e8ae6afc8a61f6c5d20d6e") --Mainappid Operation Lovecraft: Fallen Doll Playtest
+addappid(1810921, 1, "d71c539b125522b912b11034cfd7cccf8a30f3f8269e22a652740504ebf805df") --Main Windows Depot Operation Lovecraft: Fallen Doll Playtest
+setManifestid(1810921, "4659979950995162968", 29346985456)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
