2c1da6807c5fe954229c15699863e976d279cc9d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 13:14:33 +0800

    add 4449410.lua

---
+++ 4449410.lua
@@ -0,0 +1,47 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4449410.lua
+--Gamename KYOTO XANADU -the Blooming Phantom-
+addappid(4449410, 1, "17cd50ea5f3b0a34b390862fb70b85ccf176ce3b4b0f29ca57d85f38e4169922") --Mainappid KYOTO XANADU -the Blooming Phantom-
+addappid(4449411, 1, "9c5e523988b316ae37432b8f8cadd52e5b2ea2c65409ac6e50ac5ffdd1b4df49") --Main Windows Depot KYOTO XANADU -the Blooming Phantom-
+setManifestid(4449411, "1718079370043415747", 13852358912)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4630720) --Dlcname KYOTO XANADU -the Blooming Phantom- - Support Card: LinoN
+addappid(4630730) --Dlcname KYOTO XANADU -the Blooming Phantom- - Teahouse Costume "Hirasakakomachi AO" for Rei & Fuka
+addappid(4630750) --Dlcname KYOTO XANADU -the Blooming Phantom- - Swimsuit "Kazenone"
+addappid(4789990) --Dlcname KYOTO XANADU -the Blooming Phantom- - Japanese Traditional Clothing Set "Kachofugetsu"
+addappid(4790000) --Dlcname KYOTO XANADU -the Blooming Phantom- - Virtual Singer Costume Set
+addappid(4790010) --Dlcname KYOTO XANADU -the Blooming Phantom- - Original Swimsuit Set "Summer Colors"
+addappid(4790020) --Dlcname KYOTO XANADU -the Blooming Phantom- - Glasses Set A
+addappid(4790030) --Dlcname KYOTO XANADU -the Blooming Phantom- - Glasses Set B
+addappid(4790040) --Dlcname KYOTO XANADU -the Blooming Phantom- - Karasu Tengu Set
+addappid(4790050) --Dlcname KYOTO XANADU -the Blooming Phantom- - Ronin Set
+addappid(4790060) --Dlcname KYOTO XANADU -the Blooming Phantom- - Greed Set
+addappid(4790070) --Dlcname KYOTO XANADU -the Blooming Phantom- - Colorful Effect Set
+addappid(4790080) --Dlcname KYOTO XANADU -the Blooming Phantom- - Banner Set A
+addappid(4790090) --Dlcname KYOTO XANADU -the Blooming Phantom- - Banner Set B
+addappid(4790100) --Dlcname KYOTO XANADU -the Blooming Phantom- - Comment Panel Set
+addappid(4790110) --Dlcname KYOTO XANADU -the Blooming Phantom- - Hero Mask Set
+addappid(4790120) --Dlcname KYOTO XANADU -the Blooming Phantom- - Head Accessory Set A
+addappid(4790130) --Dlcname KYOTO XANADU -the Blooming Phantom- - Head Accessory Set B
+addappid(4790140) --Dlcname KYOTO XANADU -the Blooming Phantom- - Hair Color Set LinoN
+addappid(4790150) --Dlcname KYOTO XANADU -the Blooming Phantom- - Costume Set "Hero Japanese Robe"
+addappid(4790160) --Dlcname KYOTO XANADU -the Blooming Phantom- - Costume Set "Unique Japanese Robe"
+addappid(4790170) --Dlcname KYOTO XANADU -the Blooming Phantom- - Smartphone Wallpaper Set
+addappid(4790180) --Dlcname KYOTO XANADU -the Blooming Phantom- - Money: 100,000 Yen
+addappid(4790200) --Dlcname KYOTO XANADU -the Blooming Phantom- - Phantom Aether 500ml
+addappid(4790210) --Dlcname KYOTO XANADU -the Blooming Phantom- - Phantom Aether 1000ml
+addappid(4790220) --Dlcname KYOTO XANADU -the Blooming Phantom- - Phantom Aether 1500ml
+addappid(4790230) --Dlcname KYOTO XANADU -the Blooming Phantom- - Xanadu Material Set (Rank 1)
+addappid(4790240) --Dlcname KYOTO XANADU -the Blooming Phantom- - Xanadu Material Set (Rank 2)
+addappid(4790250) --Dlcname KYOTO XANADU -the Blooming Phantom- - Xanadu Material Set (Rank 3)
+addappid(4790260) --Dlcname KYOTO XANADU -the Blooming Phantom- - Books for Status Up - Beginner Level
+addappid(4790270) --Dlcname KYOTO XANADU -the Blooming Phantom- - Books for Status Up - Intermediate Level
+addappid(4790280) --Dlcname KYOTO XANADU -the Blooming Phantom- - Books for Status Up - Advanced Level
+addappid(4790290) --Dlcname KYOTO XANADU -the Blooming Phantom- - Useful Mask Set
+addappid(4790310) --Dlcname KYOTO XANADU -the Blooming Phantom- - Guardian Skill Card Set "Attack"
+addappid(4790320) --Dlcname KYOTO XANADU -the Blooming Phantom- - Guardian Skill Card Set "Defense"
+addappid(4790330) --Dlcname KYOTO XANADU -the Blooming Phantom- - Guardian Skill Card Set "Spirit"
