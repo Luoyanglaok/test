d610f835a25bb659eb7e7cc35614cdeb9b806c81
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 17:51:14 +0800

    add 2235020.lua

--- 2235020.lua
+++ 2235020.lua
@@ -1,3 +1,8 @@
-addappid(2235020)
-addappid(2235021,0,"18c08150aebeedf87433615a466009912f918212efb6ac8b1e91cb8fbcd2d7a8")
-setManifestid(2235021,"3864994051288809346")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2235020.lua
+--Gamename Contra: Operation Galuga
+addappid(2235020) --Mainappid Contra: Operation Galuga
+addappid(2235021, 1, "18c08150aebeedf87433615a466009912f918212efb6ac8b1e91cb8fbcd2d7a8") --Main Windows Depot Contra: Operation Galuga
+setManifestid(2235021, "6835101975066503289", 7973104704)
+--Missing Dlcs
+--Dlcname Contra: Operation Galuga Special Soundtrack
