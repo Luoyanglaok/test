31b35f8f6004c47c09ec3ab35ad6a2d0f9656dc6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 16:46:01 +0800

    add 2651580.lua

--- 2651580.lua
+++ 2651580.lua
@@ -1,3 +1,6 @@
-addappid(2651580)
-addappid(2651581,0,"7ac67cb743bd4080f7cb42b25254c425a53b121d02378ce92271c03ac2ad88a4")
-setManifestid(2651581,"7279651821841441841")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2651580.lua
+--Gamename CatBallGravityMaze
+addappid(2651580) --Mainappid CatBallGravityMaze
+addappid(2651581, 1, "7ac67cb743bd4080f7cb42b25254c425a53b121d02378ce92271c03ac2ad88a4") --Main Windows Depot CatBallGravityMaze
+setManifestid(2651581, "7279651821841441841", 24185328)
