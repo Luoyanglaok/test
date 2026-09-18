07c72860af6b260ef0cf5e380c07421a6b364fc0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 16:41:35 +0800

    add 4266960.lua

---
+++ 4266960.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4266960.lua
+--Gamename AI Faith: Compute [>_AIFC]
+addappid(4266960) --Mainappid AI Faith: Compute [>_AIFC]
+addappid(4266961, 1, "320228aecd8ac6917cbc252e6e286a981953944bfccc2b7475a875e3ad5f5f19") --Main Windows Depot AI Faith: Compute [>_AIFC]
+setManifestid(4266961, "7131730996852486521", 290679664)
