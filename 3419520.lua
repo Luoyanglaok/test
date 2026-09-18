f780e6c5095200acc848eb8bdd1214519fd5e3f6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 18:16:26 +0800

    add 3419520.lua

---
+++ 3419520.lua
@@ -0,0 +1,23 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3419520.lua
+--Gamename Quarantine Zone: The Last Check
+addappid(3419520, 1, "9e084611ffbd6bd1c569aa8dcabdc825ca4c326880e19064b50ed1c6a244db7b") --Mainappid Quarantine Zone: The Last Check
+addappid(3419521, 1, "7763e89ed36372394d459530e45e7dab3aca85588166e1c59d3bd1f7993bfac6") --Main Windows Depot Quarantine Zone: The Last Check
+setManifestid(3419521, "1620323060193342486", 9284685664)
+addappid(4312371, 1, "9f7ca62f12edecdd0b93ed5805a70ef7411343ffac4ce6219b8b15f6ab0cd2a7") --Main Windows Depot Quarantine Zone: The Last Check
+setManifestid(4312371, "483435908930075228", 37051936)
+addappid(4312372, 1, "fc387cea1af2810724fadaa78433efed97fd4f15c6093dea39095f5ea1e84480") --Main Windows Depot Quarantine Zone: The Last Check
+setManifestid(4312372, "3596827044863986607", 162868208)
+--Dlcname Quarantine Zone: The Last Check Soundtrack
+addappid(4312370) --Dlcname Quarantine Zone: The Last Check Soundtrack
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3960620) --Dlcname Quarantine Zone Twitch Drop - Rat Skin - Raccoon
+addappid(3960630) --Dlcname Quarantine Zone Twitch Drop - Military Skin
+addappid(3960640) --Dlcname Quarantine Zone Twitch Drop - Scanner Skin
+addappid(3960650) --Dlcname Quarantine Zone Twitch Drop - Hammer Skin
+addappid(3960660) --Dlcname Quarantine Zone Twitch Drop - Thermopulse
+addappid(4312380) --Dlcname Quarantine Zone: The Last Check Supporter Pack
