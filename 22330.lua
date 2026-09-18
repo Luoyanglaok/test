89d4ee7f3e9dbb7feedcc3bcddbfbac504bab280
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 23:01:19 +0800

    add 22330.lua

--- 22330.lua
+++ 22330.lua
@@ -1,13 +1,22 @@
-addappid(22330)
-addappid(22331,0,"3d9d5b4a4675be6981285ac5c1d08fab79e194eff4ade8c11c2788ea2a96cda8")
-setManifestid(22331,"7977703601170745652")
-addappid(22333,0,"244e0256c60f9fe31ae1c8263670595db76091793618ec6e6db7096a84d3ee26")
-setManifestid(22333,"5078222966915536366")
-addappid(22334,0,"7d47110a2a269d519332d43f0daf05c6e1404d29d76e51ed8a36f4490a6a8cb7")
-setManifestid(22334,"6986544622093806923")
-addappid(22335,0,"b8e736e122378c10c199437f7522ef579477619cc99abafbbf656c49e5d0ed42")
-setManifestid(22335,"1772388336463976851")
-addappid(22336,0,"50a2e885cf832658b972a08433eca7135b1c1ae1f76a62f95e0ca142eb61bfd6")
-setManifestid(22336,"4349214922682464414")
-addappid(22337,0,"b48729265bb248aee367daf9b401a81edd86896ae6bbed5c3725e79ba2e5364f")
-setManifestid(22337,"424494866178121373")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 22330.lua
+--Gamename The Elder Scrolls IV: Oblivion® Game of the Year Edition (2009)
+addappid(22330, 1, "8027f4cea05d3c1909838bd8d42ab3907e38d0de1cc76aa3035549d5dae7ed66") --Mainappid The Elder Scrolls IV: Oblivion® Game of the Year Edition (2009)
+addappid(22331, 1, "3d9d5b4a4675be6981285ac5c1d08fab79e194eff4ade8c11c2788ea2a96cda8") --Main Windows Depot The Elder Scrolls IV: Oblivion® Game of the Year Edition (2009)
+setManifestid(22331, "7977703601170745652", 0)
+addappid(22334, 1, "7d47110a2a269d519332d43f0daf05c6e1404d29d76e51ed8a36f4490a6a8cb7") --Main Windows Language Depot Spanish The Elder Scrolls IV: Oblivion® Game of the Year Edition (2009)
+setManifestid(22334, "6986544622093806923", 0)
+addappid(22335, 1, "b8e736e122378c10c199437f7522ef579477619cc99abafbbf656c49e5d0ed42") --Main Windows Language Depot French The Elder Scrolls IV: Oblivion® Game of the Year Edition (2009)
+setManifestid(22335, "1772388336463976851", 0)
+addappid(22336, 1, "50a2e885cf832658b972a08433eca7135b1c1ae1f76a62f95e0ca142eb61bfd6") --Main Windows Language Depot German The Elder Scrolls IV: Oblivion® Game of the Year Edition (2009)
+setManifestid(22336, "4349214922682464414", 0)
+addappid(22337, 1, "b48729265bb248aee367daf9b401a81edd86896ae6bbed5c3725e79ba2e5364f") --Main Windows Language Depot Italian The Elder Scrolls IV: Oblivion® Game of the Year Edition (2009)
+setManifestid(22337, "424494866178121373", 0)
+--Dlcname Oblivion Game of the Year DLC
+addappid(22333, 1, "244e0256c60f9fe31ae1c8263670595db76091793618ec6e6db7096a84d3ee26") --Dlcname Oblivion Game of the Year DLC
+setManifestid(22333, "5078222966915536366", 0)
+--Dlcname Oblivion Soundtrack
+addappid(452070, 1, "28e57eac4572d002602c39ae6d906f302daaeeb469376ee36151283b5ebc9b6e") --Dlcname Oblivion Soundtrack
+setManifestid(452070, "8677345989153827241", 111931200)
+addappid(452071, 1, "366f4fb630bc2e9fb533d8b066f9148238ad42dacf554a0f2cd96eb27b016e2b") --Dlc Windows Depot Oblivion Soundtrack
+setManifestid(452071, "8969255607162370014", 510572864)
