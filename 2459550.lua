ceeec973940129c467f319b553fb9755d0527d70
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 12:14:23 +0800

    add 2459550.lua

--- 2459550.lua
+++ 2459550.lua
@@ -1,3 +1,6 @@
-addappid(2459550)
-addappid(2459551,1,"da804a84952ce187ba4449fd2d61ff14c21ea7bd9bf5c20a3e81beda8423c8c7")
-setManifestid(2459551,"7027192703569946792")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2459550.lua
+--Gamename Emberward
+addappid(2459550) --Mainappid Emberward
+addappid(2459551, 1, "da804a84952ce187ba4449fd2d61ff14c21ea7bd9bf5c20a3e81beda8423c8c7") --Main Windows Depot Emberward
+setManifestid(2459551, "3334959325384983644", 899050544)
