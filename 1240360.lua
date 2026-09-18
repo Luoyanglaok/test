8d43e7052d455a1e9c4b419fdea8c1316fc79a39
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 23:01:16 +0800

    add 1240360.lua

--- 1240360.lua
+++ 1240360.lua
@@ -1,3 +1,6 @@
-addappid(1240360)
-addappid(1240361,0,"772d0427dfa30d9e6004b4d44313bea94f1092a86fc96a646fef452412e6213a")
-setManifestid(1240361,"6976370690920690050")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1240360.lua
+--Gamename The Elder Scrolls V: Skyrim Soundtrack
+addappid(1240360) --Mainappid The Elder Scrolls V: Skyrim Soundtrack
+addappid(1240361, 1, "772d0427dfa30d9e6004b4d44313bea94f1092a86fc96a646fef452412e6213a") --Main Windows Depot The Elder Scrolls V: Skyrim Soundtrack
+setManifestid(1240361, "6976370690920690050", 412333856)
