5102b192b4529e4aaf6ccd35518cb70d08df5074
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 17:38:45 +0800

    add 3399780.lua

--- 3399780.lua
+++ 3399780.lua
@@ -2,6 +2,7 @@
 -- Original file: 3399780.lua
 --Gamename 镇邪Ⅱ
 addappid(3399780, 1, "bf878e37e7bb2b13964b30127db1a76eea42801f5b69072b4d80bd09a7c7e4b9") --Mainappid 镇邪Ⅱ
+addtoken(3399780, "5440596075866465344")
 addappid(3399781, 1, "392f688f96a0afc206c6e69bbe203680b5daf7ffa3b6b18ed94fa517aaf9ca0e") --Main Windows Depot 镇邪Ⅱ
 setManifestid(3399781, "1889676113177068328", 10322491312)
 addappid(4620710) --Dlcname 镇邪Ⅱ - 八荒死境试炼
