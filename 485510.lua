4775e271c1bc9172b23b37a8869550ae2338e79e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 16:39:28 +0800

    add 485510.lua

--- 485510.lua
+++ 485510.lua
@@ -1,10 +1,17 @@
-addappid(485510)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(485511,0,"eebe92a206180d4e31103c85ffa51ad98d44f2be4b697bbeaf237b4657c589c0")
-setManifestid(485511,"3500435105821266774")
-addappid(485512,0,"eb4221176ff254f8b9987132654da11623fda14bcbd0782398d1a4baf1ec0eea")
-setManifestid(485512,"6314810000001929748")
-addappid(485513,0,"6a157f276514bf77af6aee407eacfab5b423e8b8592238722985697cf2306545")
-setManifestid(485513,"776778844353940279")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 485510.lua
+--Gamename Nioh: Complete Edition
+addappid(485510, 1, "4401fb38be028a72a201c075ea87aa8fbe6f606ecb4b6c35d5d99c3f18ed7be7") --Mainappid Nioh: Complete Edition
+addappid(485511, 1, "eebe92a206180d4e31103c85ffa51ad98d44f2be4b697bbeaf237b4657c589c0") --Main Windows Depot Nioh: Complete Edition
+setManifestid(485511, "3500435105821266774", 31352180544)
+addappid(485512, 1, "eb4221176ff254f8b9987132654da11623fda14bcbd0782398d1a4baf1ec0eea") --Main Windows Depot Nioh: Complete Edition
+setManifestid(485512, "6314810000001929748", 30967280)
+addappid(485513, 1, "6a157f276514bf77af6aee407eacfab5b423e8b8592238722985697cf2306545") --Main Windows Depot Nioh: Complete Edition
+setManifestid(485513, "776778844353940279", 30976352)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
