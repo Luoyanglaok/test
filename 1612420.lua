cda578ae3f12044ef602243cad5440c46b27a49e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:25:55 +0800

    add 1612420.lua

--- 1612420.lua
+++ 1612420.lua
@@ -1,5 +1,11 @@
-addappid(1612420)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1612421,0,"94247ea5c273f90c20d1c1269b4a3591ef8b2bca05db931d4c5e42774cf2ab30")
-setManifestid(1612421,"2787060406986761791")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1612420.lua
+--Gamename Suit for Hire
+addappid(1612420, 1, "fe424418c5edee5762d3a3eb9c00a2d42602e2a8a01195848e5b01925113aae4") --Mainappid Suit for Hire
+addappid(1612421, 1, "94247ea5c273f90c20d1c1269b4a3591ef8b2bca05db931d4c5e42774cf2ab30") --Main Windows Depot Suit for Hire
+setManifestid(1612421, "4515482939388620402", 3587607744)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
