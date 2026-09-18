889edeefbee581ea6240c2fa4f7ade018832b397
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 16:22:48 +0800

    add 2806120.lua

---
+++ 2806120.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2806120.lua
+--Gamename Bio Goddess: Doomsday Begins
+addappid(2806120) --Mainappid Bio Goddess: Doomsday Begins
+addappid(2806121, 1, "b2fdbde8cee12f9bfa492ac7083e3b922b06eb750d4ed3133a2ec34d67381ccb") --Main Windows Depot Bio Goddess: Doomsday Begins
+setManifestid(2806121, "2014363960070834581", 7127314192)
