5d1c75a6305c04d5cda3f41d02ab2386cc7df004
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:50:37 +0800

    add 4368640.lua

---
+++ 4368640.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4368640.lua
+--Gamename DIG Raiders: Anomaly Extraction
+addappid(4368640) --Mainappid DIG Raiders: Anomaly Extraction
+addappid(4368641, 1, "44d4744d9afc54049ca7a9cf53fa6172b3cb85c809ff56db680770455615d8b3") --Main Windows Depot DIG Raiders: Anomaly Extraction
+setManifestid(4368641, "572801112308282720", 5786936640)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
