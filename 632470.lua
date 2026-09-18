9569e3c012f9dfb0b68b450e2c9e279f4c0b09b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 10:59:19 +0800

    add 632470.lua

--- 632470.lua
+++ 632470.lua
@@ -1,8 +1,20 @@
-addappid(632470)
-setManifestid(228985,"3966345552745568756")
-addappid(632471,0,"c1f12eca51041182cc2c6d43bf367dad67ca662865691b1beaac315b739fbf8e")
-setManifestid(632471,"4013470289211380146")
-addappid(1173140,0,"012eaa221b0a375c66d62ef1f37f67519f22863c8931e98cfc1517defaae5334")
-setManifestid(1173140,"4219102846429304420")
-addappid(632473,0,"d8f0c338ee47ed810ea03c7b20bea833cdf9eb8862d96cbb39d3f48da62920bf")
-setManifestid(632473,"3514503533443935223")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 632470.lua
+--Gamename Disco Elysium - The Final Cut
+addappid(632470, 1, "774daf3516c59cb2b13773ced0cb52c3ebbddce660ded85b239d4fee899fe0e2") --Mainappid Disco Elysium - The Final Cut
+addappid(632471, 1, "c1f12eca51041182cc2c6d43bf367dad67ca662865691b1beaac315b739fbf8e") --Main Windows Depot Disco Elysium - The Final Cut
+setManifestid(632471, "5267467363497400856", 9328911504)
+addappid(632473, 1, "d8f0c338ee47ed810ea03c7b20bea833cdf9eb8862d96cbb39d3f48da62920bf") --Main Macos Depot Disco Elysium - The Final Cut
+setManifestid(632473, "3514503533443935223", 9333410112)
+--Dlcname Disco Elysium - Soundtrack & Artbook
+addappid(1173140, 1, "012eaa221b0a375c66d62ef1f37f67519f22863c8931e98cfc1517defaae5334") --Dlcname Disco Elysium - Soundtrack & Artbook
+setManifestid(1173140, "5482040944560501642", 1063987200)
+--Dlcname Disco Elysium Soundtrack
+addappid(1233220) --Dlcname Disco Elysium Soundtrack
+addappid(1233221, 1, "c18511718169a4fccb59f0fec3cd617fc8a15992efeba3ad36f858791bb023d2") --Dlc Windows Depot Disco Elysium Soundtrack
+setManifestid(1233221, "1090265799184396815", 259228512)
+addappid(1233222, 1, "c7ccebdd784293d8d0c020fff4166931d5afa69729545ddd6a3b2fc54cf6f25a") --Dlc Windows Depot Disco Elysium Soundtrack
+setManifestid(1233222, "2555027127085393220", 665597424)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
