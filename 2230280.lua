789d53f26d8e7cdd12ffdd5554175744671a2980
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:08:49 +0800

    add 2230280.lua

--- 2230280.lua
+++ 2230280.lua
@@ -1,3 +1,6 @@
-addappid(2230280)
-addappid(2230281,0,"6907470a25cd285b128bcbbb1b183192afd4491ec29a00a72356e2078a33a771")
-setManifestid(2230281,"4733302096696096679")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2230280.lua
+--Gamename 方寸文明
+addappid(2230280) --Mainappid 方寸文明
+addappid(2230281, 1, "6907470a25cd285b128bcbbb1b183192afd4491ec29a00a72356e2078a33a771") --Main Windows Depot 方寸文明
+setManifestid(2230281, "4733302096696096679", 70385440)
