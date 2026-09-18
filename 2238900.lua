e0ad031a252baeda967b9c89de27b22f85df9afc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 17:10:37 +0800

    add 2238900.lua

--- 2238900.lua
+++ 2238900.lua
@@ -1,4 +1,15 @@
-addappid(2238900)
-setManifestid(228989,"3514306556860204959")
-addappid(2238901,0,"67cb73b183e7b5251669ae7065e69ae7e541ae57d56e19105e12323adcc2f42e")
-setManifestid(2238901,"3448252633173787635")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2238900.lua
+--Gamename STAR OCEAN THE SECOND STORY R
+addappid(2238900) --Mainappid STAR OCEAN THE SECOND STORY R
+addappid(2238901, 1, "67cb73b183e7b5251669ae7065e69ae7e541ae57d56e19105e12323adcc2f42e") --Main Windows Depot STAR OCEAN THE SECOND STORY R
+setManifestid(2238901, "2326349097900874324", 17815425504)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(2428850) --Dlcname STAR OCEAN THE SECOND STORY R - Pangalactic Federation Longsword
+addappid(2428851) --Dlcname STAR OCEAN THE SECOND STORY R - Forest Knuckles
+addappid(2428852) --Dlcname STAR OCEAN THE SECOND STORY R - Set of recovery items
+addappid(2428853) --Dlcname STAR OCEAN THE SECOND STORY R - Sunrise Ring
+--Missing Dlcs
+--Dlcname STAR OCEAN THE SECOND STORY R ORIGINAL SOUNDTRACK
