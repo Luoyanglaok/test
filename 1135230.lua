bd26a791047ba913adabfcdafd707cf8c2760f4e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 21:57:52 +0800

    add 1135230.lua

--- 1135230.lua
+++ 1135230.lua
@@ -1,7 +1,24 @@
-addappid(1135230)
-setManifestid(228988,"6645201662696499616")
-setManifestid(229006,"1784011429307107530")
-addappid(1135232,0,"4fb873edabfbd3320be661283c46fdc9dd7d5f9cc7d942b53790673083fe1b66")
-setManifestid(1135232,"44067277920493741")
-addappid(1135231,0,"011afcb4ff95536d7fd9806e3c2b9b08d9dd0f69a69bd7b92076966562010938")
-setManifestid(1135231,"4282928667131848033")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1135230.lua
+--Gamename Ember Knights
+addappid(1135230, 1, "b0c6604202e7ce05440fbcdcf804b20084088d4ea89617589334d563012db949") --Mainappid Ember Knights
+addappid(1135231, 1, "011afcb4ff95536d7fd9806e3c2b9b08d9dd0f69a69bd7b92076966562010938") --Main Windows Depot Ember Knights
+setManifestid(1135231, "4282928667131848033", 71031776)
+addappid(1135232, 1, "4fb873edabfbd3320be661283c46fdc9dd7d5f9cc7d942b53790673083fe1b66") --Main Windows Depot Ember Knights
+setManifestid(1135232, "1019475401278939983", 736141968)
+addappid(1135233, 1, "4d1fcf06977e3d03c39fb17cba2a677245bff774d0d225bf0835cd6a88e79171") --Main Macos Depot Ember Knights
+setManifestid(1135233, "865315344378060855", 113159040)
+addappid(2686341, 1, "99d77be21f2be3e0b35ae0af2c301d9bc9f2a0caa4228a00542fca6937bf4e51") --Main Windows Depot Ember Knights
+setManifestid(2686341, "4875242581127774907", 97736016)
+addappid(2686342, 1, "018d893b46062ec7d9bb575cef0f8f40937a8fdab56dca1be4a05f0496ad5cac") --Main Windows Depot Ember Knights
+setManifestid(2686342, "5161657659099858639", 705728400)
+addappid(2686343, 1, "9fcf43f6feae7eba8ef48c0d01682e2e5857b183dd960370440dd0e7450cb41f") --Main Windows Depot Ember Knights
+setManifestid(2686343, "3112943047227107795", 891097840)
+--Dlcname Ember Knights Soundtrack
+addappid(2686340) --Dlcname Ember Knights Soundtrack
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(2948140) --Dlcname Ember Knights - Wrath of the Architect
