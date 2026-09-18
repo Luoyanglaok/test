030e9c57b13127c1bd008e75e6f996363d4c8753
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 21:16:29 +0800

    add 2966850.lua

--- 2966850.lua
+++ 2966850.lua
@@ -1,3 +1,12 @@
-addappid(2966850)
-addappid(2966851,0,"0c7cc7204dc7a30359eab52bf0a87d45c2f269e869c4807174017100c63df61c")
-setManifestid(2966851,"6262434329076777542")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2966850.lua
+--Gamename Shotgun Cop Man
+addappid(2966850, 1, "2dd155c021764004a75583f7409775bc8135cd07b08c82387daac2c54454dc99") --Mainappid Shotgun Cop Man
+addappid(2966851, 1, "0c7cc7204dc7a30359eab52bf0a87d45c2f269e869c4807174017100c63df61c") --Main Windows Depot Shotgun Cop Man
+setManifestid(2966851, "987581737311244142", 181284384)
+--Dlcname Shotgun Cop Man Soundtrack
+addappid(2966860) --Dlcname Shotgun Cop Man Soundtrack
+addappid(2966861, 1, "32ca75cf20771093f293eae70bba0e660c3c16bdfff60f1e53283c0fb121c8df") --Dlc Windows Depot Shotgun Cop Man Soundtrack
+setManifestid(2966861, "3844005620413418479", 160387392)
+addappid(2966862, 1, "2545f1dbbe942a462d37388600eacd7cf6207425d6b8042d04651ac9cc02faf3") --Dlc Windows Depot Shotgun Cop Man Soundtrack
+setManifestid(2966862, "6806234068615674934", 672558688)
