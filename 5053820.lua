68c91ef0af8b0487ddd4cb40cd578cac68252b1a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 21:31:49 +0800

    add 5053820.lua

--- 5053820.lua
+++ 5053820.lua
@@ -1,9 +1,9 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 5053820.lua
 --Gamename Mimic Party
-addappid(5053820) --Mainappid Mimic Party
+addappid(5053820, 1, "3687a910eb76c9e3eccddcb2ce6cc97c4bc5667ef8e20e668cbedc150283773a") --Mainappid Mimic Party
 addappid(5053821, 1, "3bf0ccc9e3c7c0ff61adcadc28e45680676fe6f9a82c9c4ad55c417c532c35e6") --Main Windows Depot Mimic Party
-setManifestid(5053821, "2486720415097811998", 140582480)
+setManifestid(5053821, "4902613313277987759", 153045632)
 addappid(5168110) --Dlcname All Characters
 --Missing Dlcs
 --Dlcname Mimic Party Playtest
