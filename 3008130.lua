3d9d73bcd2e047ddf310df46fb44ba4a1e297c73
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 21:28:19 +0800

    add 3008130.lua

--- 3008130.lua
+++ 3008130.lua
@@ -3,49 +3,49 @@
 --Gamename Dying Light: The Beast
 addappid(3008130, 1, "5eacefd012dbfb99c51dc01573afeccbeae511575d049dbdb7a796559d455fa0") --Mainappid Dying Light: The Beast
 addappid(3008132, 1, "6028aa5b91f9bf95194ec527c2ebdccd62e9a5df423e65ce32a73145aaa6c6b3") --Main Windows Depot Dying Light: The Beast
-setManifestid(3008132, "1936258611528346410", 14814308672)
+setManifestid(3008132, "1815870566536345197", 14888053552)
 addappid(3008133, 1, "434623d389db7ee032958e856f56adcc7da748791f86963e54523321e2e57ee9") --Main Windows Depot Dying Light: The Beast
-setManifestid(3008133, "3674182751778406835", 196100016)
+setManifestid(3008133, "6044857437230330703", 187018480)
 addappid(3008134, 1, "9c4e2acba05efb8e83b8b84191c8ea0454009ee00e7fc407eb32ad2c16cd5d9e") --Main Windows Depot Dying Light: The Beast
-setManifestid(3008134, "2365756934235829247", 35575147296)
+setManifestid(3008134, "1787702338064217177", 35136740368)
 addappid(3008135, 1, "d47e689c1f7c949b76c2d74d998ef7cf4db0ebb66ef327e68a4e7e1b4bc931dd") --Main Windows Depot Dying Light: The Beast
-setManifestid(3008135, "2409602976524121557", 1761479184)
+setManifestid(3008135, "7660363868404633094", 1761483216)
 addappid(3008136, 1, "4246c4c3f664e9ac92b6f9bdabea8f9d178d64e7b7f78ce2e47d3261e429d21f") --Main Windows Language Depot Polish Dying Light: The Beast
-setManifestid(3008136, "4835321499446250761", 1636476928)
+setManifestid(3008136, "1201098753956099504", 1636481520)
 addappid(3008137, 1, "91d23606572ed7cd2a17f0a53854b43ab17be7f90b9121c2e133df6bf9628b15") --Main Windows Language Depot Arabic Dying Light: The Beast
-setManifestid(3008137, "2203617687536162847", 4799632)
+setManifestid(3008137, "965404590006239293", 4803808)
 addappid(3008138, 1, "b087b304a3080e8bb9b868f067c8b57d6eac1c0018d81a8d563cc7dcaf6ca050") --Main Windows Language Depot Brazilian Dying Light: The Beast
-setManifestid(3008138, "9124659781447740917", 1643282800)
+setManifestid(3008138, "4334113646338191203", 1643287360)
 addappid(3008139, 1, "9ca49a179625bb0a94c6cef85afa175dbf6f70f640eea01565cf29ee9b89b1a4") --Main Windows Language Depot Schinese Dying Light: The Beast
-setManifestid(3008139, "5890478858748856390", 1599616256)
+setManifestid(3008139, "4722956610001378161", 1599625504)
 addappid(3061861, 1, "2f7e9b1d05e7bc30e55829af984f423c7891ecb354db0e336f3cc8f8115aa9a8") --Main Windows Language Depot Tchinese Dying Light: The Beast
-setManifestid(3061861, "80255138283957343", 8144064)
+setManifestid(3061861, "5495857833815638276", 8154944)
 addappid(3061862, 1, "58441f3a3fc23c78ee6ae062b402e98c3881f13b870b2945df538700a9ee98ee") --Main Windows Language Depot Czech Dying Light: The Beast
-setManifestid(3061862, "1536755372458729572", 1224512)
+setManifestid(3061862, "1508396172463784844", 1228928)
 addappid(3061863, 1, "5046b1035705c5b2cb125dcffa5f4e5e2fc859a4be1568f05cb1690cd5d8a2ed") --Main Windows Language Depot German Dying Light: The Beast
-setManifestid(3061863, "7380519459400442377", 1630095456)
+setManifestid(3061863, "8196123726676845138", 1630099952)
 addappid(3061864, 1, "38076b913ef2e4d848b83c972e455b0ed335321b7b3275f956819d56618137cc") --Main Windows Language Depot Latam Dying Light: The Beast
-setManifestid(3061864, "7457702042064894005", 1172928)
+setManifestid(3061864, "1620402360743943651", 1177200)
 addappid(3061865, 1, "062b2caf7c45bb6c993a1ccc89d4241601e83976f43b4d7a4ec73b6e87a23baf") --Main Windows Language Depot Spanish Dying Light: The Beast
-setManifestid(3061865, "7349148459753270435", 1616940512)
+setManifestid(3061865, "4737904536567645461", 1616944832)
 addappid(3061866, 1, "da6a491e78d34f913d3a35f81992ad8b9554a45ad3405eb90104535c2a190a1c") --Main Windows Language Depot French Dying Light: The Beast
-setManifestid(3061866, "5570890624782831149", 1606488320)
+setManifestid(3061866, "1003081175812180771", 1606492816)
 addappid(3061867, 1, "72b42328ca9c827d6c99a336214d08101a2d1ea131aa4c646a506d94d5c25594") --Main Windows Language Depot Italian Dying Light: The Beast
-setManifestid(3061867, "7273726216662452785", 1198112)
+setManifestid(3061867, "507942733485243188", 1202304)
 addappid(3061868, 1, "4a4e46600e98b44593fe9d6c5f32ffaee4984a804a65841da31aa5f3d562e9c5") --Main Windows Language Depot Japanese Dying Light: The Beast
-setManifestid(3061868, "1095149725616413652", 1632633360)
+setManifestid(3061868, "3108800588308239046", 1632637872)
 addappid(3061869, 1, "a3cc7e3956b21127479c97bde2183a0350a0e00b1866e328210cfd18f9449d2a") --Main Windows Language Depot Koreana Dying Light: The Beast
-setManifestid(3061869, "324574316684545675", 2879392)
+setManifestid(3061869, "309817478819974702", 2883744)
 addappid(3592041, 1, "32f10ad2de76688dc0970aebede422b2921593489edc03e6e1e90d447aeb3890") --Main Windows Depot Dying Light: The Beast
-setManifestid(3592041, "2605870998215591638", 585568)
+setManifestid(3592041, "3262466557518633501", 585408)
 addappid(3592042, 1, "55b8d3f1f8f7b6a5285b4c11635a22a47948493c275ac657e6a3e7ec22a0912b") --Main Windows Language Depot Portuguese Dying Light: The Beast
-setManifestid(3592042, "7772054862312324840", 1182000)
+setManifestid(3592042, "4764296542532447629", 1186528)
 addappid(3592043, 1, "d6564bfa4427ac630cf6a83e8a8dd0da310c59f95cb79634b17a8ce1822922ae") --Main Windows Language Depot Russian Dying Light: The Beast
-setManifestid(3592043, "8591703617627218014", 1617566224)
+setManifestid(3592043, "4346687060559182347", 1617570448)
 addappid(3592044, 1, "d36aeb87d7dc3805c50fbbbbd0a592b0e112ba31cca48aaba4a4b12169ab4a06") --Main Windows Language Depot Turkish Dying Light: The Beast
-setManifestid(3592044, "8894426967940888346", 1177232)
+setManifestid(3592044, "1018030140177615046", 1181280)
 addappid(3592047, 1, "a6d2c01076098ca0ecbe70d4ec21e3c3acfe1671974a526ea883096a51067cec") --Main Windows Depot Dying Light: The Beast
-setManifestid(3592047, "1343576803886034979", 585568)
+setManifestid(3592047, "1191636088182991649", 585408)
 --Dlcname AppID 3061860
 addappid(3061860) --Dlcname AppID 3061860
 addappid(3008131, 1, "3c9721275b2d1577cdbc46948aaef80104cd4f45a9bc32c20e03345c54652587") --Dlc Windows Depot AppID 3061860
