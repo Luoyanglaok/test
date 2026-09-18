0ccb81caaea14e0622c4813bc9b766cd1a341bac
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 17:26:35 +0800

    add 4520360.lua

--- 4520360.lua
+++ 4520360.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 4520360.lua
-addappid(4520360, 0, "0852dabd23cbba3969ed57aef9022803ce5fa5cf260ca862de0a899c84686ef8")
+addappid(4520360, 0, "5d3640860b475b0776b55a627caeeaf03e217051454012f23bb3360da858fab2")
