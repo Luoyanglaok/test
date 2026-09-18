4715d42ef13ca6cc03d1676d59cdc013a8cb1c2d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:40:40 +0800

    add 997070.lua

--- 997070.lua
+++ 997070.lua
@@ -1,35 +1,48 @@
-addappid(997070)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(997071,0,"2bbd8910dd110583a6e39ff41e99ea95f5201599ce7e2ef81f86ac1b9db1bc4e")
-setManifestid(997071,"8053825048164294578")
-addappid(997072,0,"d161f2895db8915a566e7a7529706c135f2af468c35dc49d026be4d69c4bd6e1")
-setManifestid(997072,"7372463490027595797")
-addappid(997075,0,"962130471843a61b56890ec4b2084462b505ecbbc8e7bb822d9b8f8470c44ff8")
-setManifestid(997075,"5718314415369291092")
-addappid(997076,0,"e7528fcf5f05436a5414482f8d51a3d9d0b21e85688ecf93185ca4200fcaf470")
-setManifestid(997076,"6062367728168537218")
-addappid(997077,0,"5dbfda03265b5d79d632c7ea02050fce65e3a94baf890b669129fa7266dbb907")
-setManifestid(997077,"1175162434571743379")
-addappid(997078,0,"8e70d9772e5c23007a9a928da56e61d6979a2ae29960bc11896860407ed457e6")
-setManifestid(997078,"3272191448846488929")
-addappid(997079,0,"27f3ca41bb8266ad854530cb1771be77c2e8bf7131efbc6c4a56db3df616c3dc")
-setManifestid(997079,"7464315618070041860")
-addappid(1159061,0,"94e60a75b62790fa5658275a31fddd479b9b8b87ab1e30053822ca458af65914")
-setManifestid(1159061,"6225577583917234196")
-addappid(1159062,0,"f33ce31ca5c03efee36ae5f3543f6dba942449cd9e413d8c5ec45bc845ba0066")
-setManifestid(1159062,"5358078403326263337")
-addappid(1159063,0,"c597019703148f612ff9df722cb1f8652d93751dc5a1177f42e2e25dfbe4a27b")
-setManifestid(1159063,"4443807893247731768")
-addappid(1159064,0,"28b5fadcf3203de49c4393f56994a981df19ed1d348b9eb823baa1612d75edaf")
-setManifestid(1159064,"7604540984728168049")
-addappid(1159065,0,"f316bd18e228d40d9e31737e9439cff2ee09bcd6a92c541f5f91e3272f2b2d39")
-setManifestid(1159065,"8865971486030177410")
-addappid(1159066,0,"d0356cb76483e49d5b402e2cd89f3722417ac1a676c45262a4b81ba48e268647")
-setManifestid(1159066,"5653194137509388496")
-addappid(1159067,0,"f6ce250caa053e730c60655c3faa792f4c2ac759768196dbb50ec286ce746c4b")
-setManifestid(1159067,"3895884170544226645")
-addappid(1159068,0,"6ff59658ce4c5db14e6624987a690e886a3d48ac06b904e2d3fa18943759f46c")
-setManifestid(1159068,"6053345184197026079")
-addappid(1159069,0,"448ff880a42d103957ac2623fab6b46bf3661014c0999cb09b084312a4eab193")
-setManifestid(1159069,"4372720346211009346")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 997070.lua
+--Gamename Marvel's Avengers - The Definitive Edition
+addappid(997070) --Mainappid Marvel's Avengers - The Definitive Edition
+addappid(997071, 1, "2bbd8910dd110583a6e39ff41e99ea95f5201599ce7e2ef81f86ac1b9db1bc4e") --Main Windows Depot Marvel's Avengers - The Definitive Edition
+setManifestid(997071, "8053825048164294578", 99003042816)
+addappid(997072, 1, "d161f2895db8915a566e7a7529706c135f2af468c35dc49d026be4d69c4bd6e1") --Main Windows Depot Marvel's Avengers - The Definitive Edition
+setManifestid(997072, "7372463490027595797", 66566880)
+addappid(997076, 1, "e7528fcf5f05436a5414482f8d51a3d9d0b21e85688ecf93185ca4200fcaf470") --Main Windows Depot Marvel's Avengers - The Definitive Edition
+setManifestid(997076, "6062367728168537218", 3575078896)
+addappid(997077, 1, "5dbfda03265b5d79d632c7ea02050fce65e3a94baf890b669129fa7266dbb907") --Main Windows Language Depot French Marvel's Avengers - The Definitive Edition
+setManifestid(997077, "1175162434571743379", 3509197184)
+addappid(997078, 1, "8e70d9772e5c23007a9a928da56e61d6979a2ae29960bc11896860407ed457e6") --Main Windows Language Depot German Marvel's Avengers - The Definitive Edition
+setManifestid(997078, "3272191448846488929", 3683697472)
+addappid(997079, 1, "27f3ca41bb8266ad854530cb1771be77c2e8bf7131efbc6c4a56db3df616c3dc") --Main Windows Language Depot Italian Marvel's Avengers - The Definitive Edition
+setManifestid(997079, "7464315618070041860", 3635027712)
+addappid(1159061, 1, "94e60a75b62790fa5658275a31fddd479b9b8b87ab1e30053822ca458af65914") --Main Windows Language Depot Spanish Marvel's Avengers - The Definitive Edition
+setManifestid(1159061, "6225577583917234196", 3618416272)
+addappid(1159062, 1, "f33ce31ca5c03efee36ae5f3543f6dba942449cd9e413d8c5ec45bc845ba0066") --Main Windows Language Depot Brazilian Marvel's Avengers - The Definitive Edition
+setManifestid(1159062, "5358078403326263337", 3702007392)
+addappid(1159063, 1, "c597019703148f612ff9df722cb1f8652d93751dc5a1177f42e2e25dfbe4a27b") --Main Windows Language Depot Polish Marvel's Avengers - The Definitive Edition
+setManifestid(1159063, "4443807893247731768", 3681782624)
+addappid(1159064, 1, "28b5fadcf3203de49c4393f56994a981df19ed1d348b9eb823baa1612d75edaf") --Main Windows Language Depot Russian Marvel's Avengers - The Definitive Edition
+setManifestid(1159064, "7604540984728168049", 3754386688)
+addappid(1159065, 1, "f316bd18e228d40d9e31737e9439cff2ee09bcd6a92c541f5f91e3272f2b2d39") --Main Windows Language Depot Tchinese Marvel's Avengers - The Definitive Edition
+setManifestid(1159065, "8865971486030177410", 3662171248)
+addappid(1159066, 1, "d0356cb76483e49d5b402e2cd89f3722417ac1a676c45262a4b81ba48e268647") --Main Windows Language Depot Schinese Marvel's Avengers - The Definitive Edition
+setManifestid(1159066, "5653194137509388496", 3662171248)
+addappid(1159067, 1, "f6ce250caa053e730c60655c3faa792f4c2ac759768196dbb50ec286ce746c4b") --Main Windows Language Depot Latam Marvel's Avengers - The Definitive Edition
+setManifestid(1159067, "3895884170544226645", 3621808576)
+addappid(1159068, 1, "6ff59658ce4c5db14e6624987a690e886a3d48ac06b904e2d3fa18943759f46c") --Main Windows Language Depot Arabic Marvel's Avengers - The Definitive Edition
+setManifestid(1159068, "6053345184197026079", 3888272112)
+addappid(1159069, 1, "448ff880a42d103957ac2623fab6b46bf3661014c0999cb09b084312a4eab193") --Main Windows Language Depot Japanese Marvel's Avengers - The Definitive Edition
+setManifestid(1159069, "4372720346211009346", 3695906832)
+--Dlcname AppID 1206130
+addtoken(1206130, "10257142369031692540")
+addappid(1206130) --Dlcname AppID 1206130
+addappid(997075, 1, "962130471843a61b56890ec4b2084462b505ecbbc8e7bb822d9b8f8470c44ff8") --Dlc Windows Depot AppID 1206130
+setManifestid(997075, "5718314415369291092", 57340898400)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1159060) --Dlcname AppID 1159060
+addappid(1159080) --Dlcname AppID 1159080
+addtoken(1159080, "5971100673234105118")
+addappid(1664020) --Dlcname AppID 1664020
