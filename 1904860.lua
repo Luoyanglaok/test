bc34cff01e9d9e8c48a2c47f71f7e18684251ef3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 20:07:54 +0800

    add 1904860.lua

--- 1904860.lua
+++ 1904860.lua
@@ -1,5 +1,8 @@
-addappid(1904860)
-addappid(1904861,0,"b379a969a4e62c36ab125ebb98b3244cfb4a9f7d2620f2f25030130208ad1078")
-setManifestid(1904861,"7129913876615335627")
-addappid(1904862,0,"ce2749ccaad1c0b1e73c94d7547964a6480921deea5a61843ee360b380e67f13")
-setManifestid(1904862,"4130659401134359145")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1904860.lua
+--Gamename Legends of Kingdom Rush
+addappid(1904860) --Mainappid Legends of Kingdom Rush
+addappid(1904861, 1, "b379a969a4e62c36ab125ebb98b3244cfb4a9f7d2620f2f25030130208ad1078") --Main Windows Depot Legends of Kingdom Rush
+setManifestid(1904861, "3919707457757904984", 544981168)
+addappid(1904862, 1, "ce2749ccaad1c0b1e73c94d7547964a6480921deea5a61843ee360b380e67f13") --Main Macos Depot Legends of Kingdom Rush
+setManifestid(1904862, "3807360431711140302", 552459872)
