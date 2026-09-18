1ae639c8f8d934fb3f35a837079a26f16f452c91
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:58:01 +0800

    add 2507870.lua

---
+++ 2507870.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2507870.lua
+--Gamename Mahjick - The Realm Taker
+addappid(2507870) --Mainappid Mahjick - The Realm Taker
+addappid(2507871, 1, "efd1a409984e9a9e539c5202b5851dc0cf7110b88ec1739faaac61b3ab56a6ca") --Main Windows Depot Mahjick - The Realm Taker
+setManifestid(2507871, "7209000183523386874", 468599008)
