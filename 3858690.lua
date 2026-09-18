4f617aa00fcf154ee486a11813fc4c4cd44cd106
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:53:06 +0800

    add 3858690.lua

---
+++ 3858690.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3858690.lua
+--Gamename Portal to Nowhere Playtest
+addappid(3858690) --Mainappid Portal to Nowhere Playtest
+addappid(3858691, 1, "0DA44D56D13A7E4845FB3C495E9E09CB824A91BCF144DF68B0F77D6AA18B53F3") --Main Windows Depot Portal to Nowhere Playtest
+setManifestid(3858691, "5449682304035444641", 160024848)
