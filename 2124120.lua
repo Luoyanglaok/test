a66474e9fed85ae45ad771b6e87dbc2edf340e70
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 22:51:34 +0800

    add 2124120.lua

--- 2124120.lua
+++ 2124120.lua
@@ -1,3 +1,14 @@
-addappid(2124120)
-addappid(2124121,0,"5a0438ea06459ea27a8da1e417a22ffeee9793b472712d0a2a86179b4554bf36")
-setManifestid(2124121,"2455271787985757371")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2124120.lua
+--Gamename SULFUR
+addappid(2124120, 1, "0fb87751387bed9c55a1939d0f5435a195c260721c2924407154392fa8d9842a") --Mainappid SULFUR
+addappid(2124121, 1, "5a0438ea06459ea27a8da1e417a22ffeee9793b472712d0a2a86179b4554bf36") --Main Windows Depot SULFUR
+setManifestid(2124121, "5723837501163348289", 856626384)
+--Dlcname SULFUR Official Soundtrack
+addappid(3025640) --Dlcname SULFUR Official Soundtrack
+addappid(3025641, 1, "7ad48191395288ba800ae086e58cae4f08aeac8f5c5fe6ca50ef297c1ff7f486") --Dlc Windows Depot SULFUR Official Soundtrack
+setManifestid(3025641, "3751250985851063991", 306517248)
+addappid(3025642, 1, "0f2825db9045fb088177f68a2d2d19a977ac5e2c1f862fb100768227fef37578") --Dlc Windows Depot SULFUR Official Soundtrack
+setManifestid(3025642, "6870134821116426474", 2191124944)
+--Missing Dlcs
+--Dlcname We Wish You A SULFUR Christmas
