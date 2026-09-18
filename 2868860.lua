1487e8c1ea6ebd8f51232ede2ea12acd8d1b51fa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 18:52:54 +0800

    add 2868860.lua

--- 2868860.lua
+++ 2868860.lua
@@ -1,3 +1,8 @@
-addappid(2868860)
-addappid(2868861,0,"184987f74fbd9ce7b4460cf727ca7d3eec737adec7aab9dc0e7e438a0a01841b")
-setManifestid(2868861,"2863107015359944407")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2868860.lua
+--Gamename Card Survival: Fantasy Forest
+addappid(2868860) --Mainappid Card Survival: Fantasy Forest
+addappid(2868861, 1, "184987f74fbd9ce7b4460cf727ca7d3eec737adec7aab9dc0e7e438a0a01841b") --Main Windows Depot Card Survival: Fantasy Forest
+setManifestid(2868861, "1187965129586501956", 226242656)
+addappid(2868862, 1, "80b6ab971d1f90ec323fbf5aedafd84333e63fb6bdb112de35b23df597fc2309") --Main Macos Depot Card Survival: Fantasy Forest
+setManifestid(2868862, "3826143825498186247", 223290096)
