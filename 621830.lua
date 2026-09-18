569530c08cea0fad4c7064864c7108bd4d71a223
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 17:32:32 +0800

    add 621830.lua

--- 621830.lua
+++ 621830.lua
@@ -1,9 +1,14 @@
-addappid(621830)
-addappid(228983)
-setManifestid(228983,"8124929965194586177")
-addappid(228984)
-setManifestid(228984,"2547553897526095397")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(621831,0,"20e3152c0296460f28ce3f5a211affca305a09e6d9b9f6bbcce13bc819d0f462")
-setManifestid(621831,"3996161855180421451")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 621830.lua
+--Gamename WRC 7 FIA World Rally Championship
+addappid(621830, 1, "0bd3e638ff2266dfa1998d70b86e5b1b0e92a4852b19fffcde5be892c1c5c770") --Mainappid WRC 7 FIA World Rally Championship
+addappid(621831, 1, "20e3152c0296460f28ce3f5a211affca305a09e6d9b9f6bbcce13bc819d0f462") --Main Windows Depot WRC 7 FIA World Rally Championship
+setManifestid(621831, "3996161855180421451", 17307342352)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(627390) --Dlcname DLC - WRC 7 Porsche Car
