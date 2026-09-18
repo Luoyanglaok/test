94edd4ddcab041484c05b6d317a7881b42d5791c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 17:15:58 +0800

    add 391540.lua

--- 391540.lua
+++ 391540.lua
@@ -1,7 +1,13 @@
-addappid(391540)
-addappid(391541,0,"b416ce2b64256c530f83775eef790b171047ce48c3795077ebcfd83f9c627cab")
-setManifestid(391541,"3413030246586915448")
-addappid(391542,0,"6b3c4e270f515da237a568230e441e3a28a5984fa2a9224c31b6c3c825ca44fd")
-setManifestid(391542,"1593483711954100372")
-addappid(391544,0,"ba1155a879c71e2f624fe2fe74a0ac1baf68e199cc84e32e507c4c737299d92b")
-setManifestid(391544,"5730345506996883260")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 391540.lua
+--Gamename Undertale
+addappid(391540, 1, "fb8cdff25bb9ba56eb73d10f640db1d2a7fa8846ef6be025bb87186e8bcc53c9") --Mainappid Undertale
+addappid(391541, 1, "b416ce2b64256c530f83775eef790b171047ce48c3795077ebcfd83f9c627cab") --Main Windows Depot Undertale
+setManifestid(391541, "3413030246586915448", 125700320)
+addappid(391542, 1, "6b3c4e270f515da237a568230e441e3a28a5984fa2a9224c31b6c3c825ca44fd") --Main Macos Depot Undertale
+setManifestid(391542, "1593483711954100372", 129710224)
+addappid(391544, 1, "ba1155a879c71e2f624fe2fe74a0ac1baf68e199cc84e32e507c4c737299d92b") --Main Linux Depot Undertale
+setManifestid(391544, "5730345506996883260", 125043808)
+--Dlcname UNDERTALE Soundtrack
+addappid(391570, 1, "379685b5bcb58da172ca9a75fffa16d7cef1420dc239fa3d0a091da80975c798") --Dlcname UNDERTALE Soundtrack
+setManifestid(391570, "4231991240894241559", 304913504)
