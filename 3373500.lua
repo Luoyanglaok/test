bc8c1da32a80671d6f018e6853df62d47a4fd773
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-18 12:06:10 +0800

    add 3373500.lua

--- 3373500.lua
+++ 3373500.lua
@@ -3,7 +3,7 @@
 --Gamename Scavland
 addappid(3373500) --Mainappid Scavland
 addappid(3373501, 1, "8488688d3e01807890f7561cb55e3b254ceabf782ac4b747ef9ca4511bb499a4") --Main Windows Language Depot English Scavland
-setManifestid(3373501, "5930271486488747426", 890107472)
+setManifestid(3373501, "5411352315105560206", 916751632)
 --Share Depots
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228990, "1829726630299308803", 100658080)
