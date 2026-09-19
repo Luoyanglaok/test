3fff356516cdb65a407f1f4dbf1bd812de8e8870
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:25:28 +0800

    add 1555110.lua

--- 1555110.lua
+++ 1555110.lua
@@ -1,7 +1,13 @@
-addappid(1555110)
-addappid(1555111,0,"221315f2c174e695607ae5f8418db75ae8cbaf0972820adfd71d0a9f659ec0e5")
-setManifestid(1555111,"2463859498370261549")
-addappid(1659940,0,"126503a910566524643e9509240a6da0fb79d672cf8d0fc785ed66a5bb4b10f9")
-setManifestid(1659940,"211388395020194440")
-addappid(1717970,0,"b79da1aab50259385dff4e117ae76c8e42ae2130e60259db6e079fa3fbcb8653")
-setManifestid(1717970,"4785525021377128118")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1555110.lua
+--Gamename Chasing Tails ~A Promise in the Snow~
+addappid(1555110, 1, "c821982ec64c9d49e2709ba360b0176d0d8d0644222a064a7a583da118e0ed9b") --Mainappid Chasing Tails ~A Promise in the Snow~
+addappid(1555111, 1, "221315f2c174e695607ae5f8418db75ae8cbaf0972820adfd71d0a9f659ec0e5") --Main Windows Depot Chasing Tails ~A Promise in the Snow~
+setManifestid(1555111, "2077383313461397511", 783617984)
+--Dlcname Chasing Tails - 18+ Adult Only Patch
+addappid(1659940, 1, "126503a910566524643e9509240a6da0fb79d672cf8d0fc785ed66a5bb4b10f9") --Dlcname Chasing Tails - 18+ Adult Only Patch
+setManifestid(1659940, "211388395020194440", 44080592)
+--Dlcname Chasing Tails ~A Promise in the Snow~ Artbook
+addappid(1717970, 1, "b79da1aab50259385dff4e117ae76c8e42ae2130e60259db6e079fa3fbcb8653") --Dlcname Chasing Tails ~A Promise in the Snow~ Artbook
+setManifestid(1717970, "4785525021377128118", 212172960)
+addappid(1731980) --Dlcname Chasing Tails - Tip the Dev
