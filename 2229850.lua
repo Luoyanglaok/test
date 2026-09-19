eb858a5e88201ccaa3b1cdd88372eca49c823f21
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 03:45:48 +0800

    add 2229850.lua

--- 2229850.lua
+++ 2229850.lua
@@ -1,15 +1,21 @@
-addappid(2229850)
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-addappid(2229851,0,"2cb1c174f12c44bec71bc796c767aef1b47a5a476b79292a7bb41af3b03cd511")
-setManifestid(2229851,"4928885831751969588")
-addappid(2229852,0,"ba66f3a0933b3cfd878d8340e59dbaec8359a68b0a525207879f5fe6161a5340")
-setManifestid(2229852,"2191822715159570153")
-addappid(2229853,0,"2b1ab0ac6f851ca3821881bcf715dd376c8625dc5514c6f0f7b1abd3cd3a7eef")
-setManifestid(2229853,"651095031539856178")
-addappid(2229854,0,"961ba56200f2d06dc6abaf473928366f54ed4aaf2ed7abf48d93c19a2857d6af")
-setManifestid(2229854,"2407982433826224240")
-addappid(2229855,0,"96abb4a7dc16cf7f80ab5df7f89cfa8f5e3fab2255b77e6f54b55236fde7e125")
-setManifestid(2229855,"126987739780522527")
-addappid(2229856,0,"e9d8e9953162e17f2a7f6e780c6cfaa05f80099046b39782568763dac32543c1")
-setManifestid(2229856,"6637089396038252853")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2229850.lua
+--Gamename Command & Conquer Red Alert™ 2 and Yuri’s Revenge™
+addappid(2229850, 1, "c77d357feaf80aeffec05b1e927bb30c6fd94bf15a98088112db8416e70d13ee") --Mainappid Command & Conquer Red Alert™ 2 and Yuri’s Revenge™
+addappid(2229851, 1, "2cb1c174f12c44bec71bc796c767aef1b47a5a476b79292a7bb41af3b03cd511") --Main Windows Depot Command & Conquer Red Alert™ 2 and Yuri’s Revenge™
+setManifestid(2229851, "4928885831751969588", 1398249520)
+addappid(2229852, 1, "ba66f3a0933b3cfd878d8340e59dbaec8359a68b0a525207879f5fe6161a5340") --Main Windows Language Depot English Command & Conquer Red Alert™ 2 and Yuri’s Revenge™
+setManifestid(2229852, "2191822715159570153", 1092250352)
+addappid(2229853, 1, "2b1ab0ac6f851ca3821881bcf715dd376c8625dc5514c6f0f7b1abd3cd3a7eef") --Main Windows Language Depot German Command & Conquer Red Alert™ 2 and Yuri’s Revenge™
+setManifestid(2229853, "651095031539856178", 1108797904)
+addappid(2229854, 1, "961ba56200f2d06dc6abaf473928366f54ed4aaf2ed7abf48d93c19a2857d6af") --Main Windows Language Depot French Command & Conquer Red Alert™ 2 and Yuri’s Revenge™
+setManifestid(2229854, "2407982433826224240", 1110469152)
+addappid(2229855, 1, "96abb4a7dc16cf7f80ab5df7f89cfa8f5e3fab2255b77e6f54b55236fde7e125") --Main Windows Language Depot Tchinese Command & Conquer Red Alert™ 2 and Yuri’s Revenge™
+setManifestid(2229855, "126987739780522527", 1092247168)
+addappid(2229856, 1, "e9d8e9953162e17f2a7f6e780c6cfaa05f80099046b39782568763dac32543c1") --Main Windows Language Depot Koreana Command & Conquer Red Alert™ 2 and Yuri’s Revenge™
+setManifestid(2229856, "6637089396038252853", 1105236160)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
