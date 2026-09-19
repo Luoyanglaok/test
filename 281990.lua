839488d37152ea5fdb00b2ace36294b3c0afaed0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 12:38:22 +0800

    add 281990.lua

--- 281990.lua
+++ 281990.lua
@@ -1,86 +1,163 @@
-addappid(281990)
-addappid(4241400)
-addappid(4241420)
-addappid(3417840)
-addappid(3417850)
-addappid(2729490)
-addappid(716670)
-addappid(944290)
-addappid(1140001)
-addappid(1522090)
-addappid(1889490)
-addappid(2380030)
-addappid(2534090)
-addappid(2840100)
-addappid(2863060)
-addappid(518910)
-addappid(844810)
-addappid(1045980)
-addappid(2277860)
-addappid(2863080)
-addappid(498870)
-addappid(1140000)
-addappid(1341520)
-addappid(1749080)
-addappid(2115770)
-addappid(3283220)
-addappid(633310)
-addappid(3417860)
-addappid(2863180)
-addappid(4241410)
-addappid(4241440)
-addappid(4241450)
-addappid(4355700)
-addappid(228983,1,"77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b")
-addappid(228986,1,"51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5")
-addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-addappid(229003,1,"8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63")
-addappid(229006,1,"9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416")
-setManifestid(281991,"3922771984290037884")
-setManifestid(281992,"3306222774754384885")
-setManifestid(447680,"3796462871416536945")
-setManifestid(447681,"4635516424234970299")
-setManifestid(447682,"8126759959369925319")
-setManifestid(447683,"6115058309987442461")
-setManifestid(447684,"4984318181401498898")
-setManifestid(447685,"5616497098841117414")
-setManifestid(447686,"380104706699965241")
-setManifestid(447687,"8750895669882452148")
-setManifestid(462720,"5938010814972666580")
-setManifestid(497660,"8675625826638210993")
-setManifestid(498870,"5289062830139194176")
-setManifestid(518910,"2771826165032019499")
-setManifestid(553280,"94231584423973538")
-setManifestid(554350,"6329361971126473517")
-setManifestid(633310,"3345875144006097969")
-setManifestid(642750,"8487770189849309225")
-setManifestid(716670,"2874777228579213431")
-setManifestid(756010,"7057785433501895820")
-setManifestid(844810,"265938213461916546")
-setManifestid(944290,"5027959061154273567")
-setManifestid(1045980,"6196140321844137812")
-setManifestid(1140000,"1707277670014098752")
-setManifestid(1140001,"6552287492865536968")
-setManifestid(1341520,"3322127761481394644")
-setManifestid(1522090,"1464825983963181123")
-setManifestid(1749080,"2352410094773288579")
-setManifestid(1889490,"1949988533210430480")
-setManifestid(2115770,"1045801664911037736")
-setManifestid(2277860,"4930317276946265521")
-setManifestid(2380030,"6939619431392031129")
-setManifestid(2534090,"3656631936342514829")
-setManifestid(2863180,"9052823765448867156")
-setManifestid(2840100,"5013502338098449837")
-setManifestid(2863060,"8797712741214948916")
-setManifestid(2863080,"8947542081300706521")
-setManifestid(3417860,"3240748767659651608")
-setManifestid(3417840,"8331750570302919154")
-setManifestid(3417850,"8092327340853379843")
-setManifestid(3283220,"4373441752470778623")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229003,"8740933542064151477")
-setManifestid(229006,"1784011429307107530")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 281990.lua
+--Gamename Stellaris
+addappid(281990, 1, "9f3e7feb17a97b529e54c128210775b97eddc78321652f0a7dd42b1774fd18bf") --Mainappid Stellaris
+addappid(281991, 1, "f7e7dc0ebbd8af57b97ebdf6a1c60cab11601c659735dec52b75c2041c253bbd") --Main Windows Depot Stellaris
+setManifestid(281991, "3922771984290037884", 14182704928)
+addappid(281992, 1, "e4875742b695f223ab3bfa079d2b2e33c6785bb1e31b88e82964f1391b63066a") --Main Windows Depot Stellaris
+setManifestid(281992, "3306222774754384885", 178800448)
+addappid(281993, 1, "d91e08fd5eb94b1d5dfe65bc3ff860a28d2142856c6df540c8fcb04e5f358b57") --Main Macos Depot Stellaris
+setManifestid(281993, "3550514751965101572", 369715856)
+addappid(281994, 1, "40a3fc66b54ac44ad3bc479bddd05655e7a3a444003e9d8792fa8309f7184093") --Main Linux Depot Stellaris
+setManifestid(281994, "290069498413929006", 202778640)
+--Dlcname Stellaris: Symbols of Domination
+addappid(447680, 1, "01d10eee648eebfb7b14598ac27e9f2fe08c69218951c65666b6403f76ec4b68") --Dlcname Stellaris: Symbols of Domination
+setManifestid(447680, "3796462871416536945", 77872)
+--Dlcname Stellaris: Sign-up Campaign Bonus
+addappid(447681, 1, "29362c9059b9629893ffe6ab7b54ea3bcb25c79a8e9356747b7f1cb6cd48782c") --Dlcname Stellaris: Sign-up Campaign Bonus
+setManifestid(447681, "4635516424234970299", 77872)
+--Dlcname Stellaris: Digital Artbook
+addappid(447682, 1, "478f379303fd39b46379f3934eeac323a312c222d288f308f4328faa84a43cc3") --Dlcname Stellaris: Digital Artbook
+setManifestid(447682, "8126759959369925319", 105718688)
+--Dlcname Stellaris: Arachnoid Portrait Pack
+addappid(447683, 1, "e07ca6dfe1f7d44a3400171c4f4e12464de3ecf0229dc1e22d9871822b56a0c1") --Dlcname Stellaris: Arachnoid Portrait Pack
+setManifestid(447683, "6115058309987442461", 77872)
+--Dlcname Stellaris: Digital OST
+addappid(447684, 1, "89d6f99b01e90e6d02e690862d7eedf309161c03fc0bc4358c8b8adc70e6e359") --Dlcname Stellaris: Digital OST
+setManifestid(447684, "4984318181401498898", 1143479312)
+--Dlcname Stellaris: Signed High-res Wallpaper
+addappid(447685, 1, "88d963c939e159f0f2d93aaf3469749e0f336cd30f143a85686da78eb9e9bdd1") --Dlcname Stellaris: Signed High-res Wallpaper
+setManifestid(447685, "5616497098841117414", 4683728)
+--Dlcname Stellaris: Novel by Steven Savile
+addappid(447686, 1, "067f654a1c296e0a5ab71d6cdb46d63933bd983d08ec28f9601a25156234bdd9") --Dlcname Stellaris: Novel by Steven Savile
+setManifestid(447686, "380104706699965241", 2147392)
+--Dlcname Stellaris: Ringtones
+addappid(447687, 1, "3f787826d200ce33a6456ef355ae925b18733d56cc04abf3fa4cbc3b2be700e9") --Dlcname Stellaris: Ringtones
+setManifestid(447687, "8750895669882452148", 1257056)
+--Dlcname Stellaris: Creatures of the Void
+addappid(462720, 1, "210684f8095d1e4df92e410915e1bd9e83b156b94a2c86c7a460eebfd76d04ba") --Dlcname Stellaris: Creatures of the Void
+setManifestid(462720, "5938010814972666580", 77872)
+--Dlcname Stellaris: Infinite Frontiers eBook
+addappid(497660, 1, "459e7efe1251b020dc372643a221ebff07634e8178dad5f2da6c6d98cf52e39d") --Dlcname Stellaris: Infinite Frontiers eBook
+setManifestid(497660, "8675625826638210993", 2069904)
+--Dlcname Stellaris: Plantoids Species Pack
+addappid(498870, 1, "f6a9a5906b514eabc056a3acc46c2faeced3b7633efc2c93610476b0878ad291") --Dlcname Stellaris: Plantoids Species Pack
+setManifestid(498870, "5289062830139194176", 98064)
+--Dlcname Stellaris: Leviathans Story Pack
+addappid(518910, 1, "76afa9d8ed0308929d0d908cfa04f50ea3c5d523d4f474ed62b6d5f1b26aa74c") --Dlcname Stellaris: Leviathans Story Pack
+setManifestid(518910, "2771826165032019499", 59824816)
+--Dlcname Stellaris: Utopia
+addappid(553280, 1, "1b61d5fbaddbeaff663e5b3e9ffbde9d12400b117f5c0ed45bbf0e2aa08e3d3d") --Dlcname Stellaris: Utopia
+setManifestid(553280, "94231584423973538", 77042480)
+--Dlcname Stellaris: Horizon Signal
+addappid(554350, 1, "5f56dd0a866a921ad460131939a68cd0362874fc1c074a8fe81f6ba246334bfb") --Dlcname Stellaris: Horizon Signal
+setManifestid(554350, "6329361971126473517", 77856)
+--Dlcname Stellaris: Anniversary Portraits
+addappid(633310, 1, "01acfb94cc06164fd95aef8aa4dd39460a8b1860d235cd105fec4e88711f8538") --Dlcname Stellaris: Anniversary Portraits
+setManifestid(633310, "3345875144006097969", 105632)
+--Dlcname Stellaris: Synthetic Dawn Story Pack
+addappid(642750, 1, "868699813b46aa012731439d87f3f0924de1d3f530b13bc8f35b3f3ecc5b1641") --Dlcname Stellaris: Synthetic Dawn Story Pack
+setManifestid(642750, "8487770189849309225", 49267408)
+--Dlcname Stellaris: Apocalypse
+addappid(716670, 1, "74fe5949da9d7b5c70c4ea0bf21533a2a5aebdff4e23ce7b04316e848123713f") --Dlcname Stellaris: Apocalypse
+setManifestid(716670, "2874777228579213431", 38416464)
+--Dlcname Stellaris: Humanoids Species Pack
+addappid(756010, 1, "0eb62d6b508d6fb887a04ccd1eeabdd1b709a5c15145dbda6b05d1e49f2478ee") --Dlcname Stellaris: Humanoids Species Pack
+setManifestid(756010, "7057785433501895820", 86874064)
+--Dlcname Stellaris: Distant Stars Story Pack
+addappid(844810, 1, "28da49ec9b8c4bdf7d08dc939ee1c30056ee40c3a8ae5f8b9217775a1abc3397") --Dlcname Stellaris: Distant Stars Story Pack
+setManifestid(844810, "265938213461916546", 20828912)
+--Dlcname Stellaris: MegaCorp
+addappid(944290, 1, "3cee1514401df4f5b97ba233e1d180334891bea3c4fd11555fa4ff1c6904460d") --Dlcname Stellaris: MegaCorp
+setManifestid(944290, "5027959061154273567", 92795600)
+--Dlcname Stellaris: Ancient Relics Story Pack
+addappid(1045980, 1, "0a54b9d6d342067cc9e9aa7ef5718eaa6ceb2eb4f15fc45bad8d2ed87e3a784b") --Dlcname Stellaris: Ancient Relics Story Pack
+setManifestid(1045980, "6196140321844137812", 33179920)
+--Dlcname Stellaris: Lithoids Species Pack
+addappid(1140000, 1, "96e7ed9014e0bdad5829b21abf4dd870a6fdb1c6e43fd5827f01ce874753edba") --Dlcname Stellaris: Lithoids Species Pack
+setManifestid(1140000, "1707277670014098752", 25740864)
+--Dlcname Stellaris: Federations
+addappid(1140001, 1, "cce69ab58969ddf670853f847134f99af9153e1ac4733cc6499e3394e55723d8") --Dlcname Stellaris: Federations
+setManifestid(1140001, "6552287492865536968", 14199280)
+--Dlcname Stellaris: Necroids Species Pack
+addappid(1341520, 1, "c39c85b4fbf86b0342c8b8eaa970add1e471c9590de93bcf8b87f86aeb85cc00") --Dlcname Stellaris: Necroids Species Pack
+setManifestid(1341520, "3322127761481394644", 24979312)
+--Dlcname Stellaris: Nemesis
+addappid(1522090, 1, "72c347cf4b8dc0353239cba39e00e4dd1db8cf4185b5cee840a3ea94c25c6faf") --Dlcname Stellaris: Nemesis
+setManifestid(1522090, "1464825983963181123", 39142144)
+--Dlcname Stellaris: Aquatics Species Pack
+addappid(1749080, 1, "f722c721590152236fbf40c17aae16a1cadd7fa322f71923f7661ca1369f3eb7") --Dlcname Stellaris: Aquatics Species Pack
+setManifestid(1749080, "2352410094773288579", 30168112)
+--Dlcname Stellaris: Overlord
+addappid(1889490, 1, "b802bed9a094ed9150c06fa62024c77c973058320c8467a49630d286da40e47e") --Dlcname Stellaris: Overlord
+setManifestid(1889490, "1949988533210430480", 102807936)
+--Dlcname Stellaris: Toxoids Species Pack
+addappid(2115770, 1, "fedc5957c4d134dae2911d531a3dceab797095bdd7befc025509e54996016863") --Dlcname Stellaris: Toxoids Species Pack
+setManifestid(2115770, "1045801664911037736", 21976512)
+--Dlcname Stellaris: First Contact Story Pack
+addappid(2277860, 1, "ddfa43475c92d94ef82e6487618d43d641185f66bf5e387ed689f99f7acf1ab8") --Dlcname Stellaris: First Contact Story Pack
+setManifestid(2277860, "4930317276946265521", 93040)
+--Dlcname Stellaris: Galactic Paragons
+addappid(2380030, 1, "776a224f33c5fc9c5fd5ea7178b6034ffcd074101f356d203df55f57f2206a8f") --Dlcname Stellaris: Galactic Paragons
+setManifestid(2380030, "6939619431392031129", 92720)
+--Dlcname Stellaris: Astral Planes
+addappid(2534090, 1, "fa241287f452f70a1c00b82a578571438300be428b19d75f6f51f4b999305603") --Dlcname Stellaris: Astral Planes
+setManifestid(2534090, "3656631936342514829", 47667824)
+--Dlcname Stellaris: The Machine Age
+addappid(2840100, 1, "16e0b43a13dfd7e098808c8e7f73d40dd4a61f8bedca7ff3a8d713ea4129078e") --Dlcname Stellaris: The Machine Age
+setManifestid(2840100, "5013502338098449837", 103314592)
+--Dlcname Stellaris: Cosmic Storms
+addappid(2863060, 1, "439660ae0dd2b18c4ec7c6f2a6a2d824daa1a5125ddce5b5ce0f1ca14c2840b9") --Dlcname Stellaris: Cosmic Storms
+setManifestid(2863060, "8797712741214948916", 89552)
+--Dlcname Stellaris: Grand Archive Story Pack
+addappid(2863080, 1, "dd2337201fdc00d6a7949ca0315d688d284dba7cd25345b13ef6f5ebd833da66") --Dlcname Stellaris: Grand Archive Story Pack
+setManifestid(2863080, "8947542081300706521", 33094400)
+--Dlcname Stellaris: Rick the Cube Species Portrait
+addappid(2863180, 1, "22a665adae8ef88c495df237d54e8eb3035a0e711d68e7d12da74d242a1e2e4a") --Dlcname Stellaris: Rick the Cube Species Portrait
+setManifestid(2863180, "9052823765448867156", 78656)
+--Dlcname Stellaris: Infernals Species Pack
+addappid(3283220, 1, "87c315bfaa9f935d91135e325b796ddcc14a9d8179a965a7bfac33cd4e489336") --Dlcname Stellaris: Infernals Species Pack
+setManifestid(3283220, "4373441752470778623", 39485424)
+--Dlcname Stellaris: BioGenesis
+addappid(3417840, 1, "c017d1c54035b510479749be4c962ef16af0faffb11a8b9f3e243b9986814f6a") --Dlcname Stellaris: BioGenesis
+setManifestid(3417840, "8331750570302919154", 90969856)
+--Dlcname Stellaris: Shadows of the Shroud
+addappid(3417850, 1, "74081827e0c06682d458ae444bdbec331220a48ca8b1ca57c68d35da9304ffb0") --Dlcname Stellaris: Shadows of the Shroud
+setManifestid(3417850, "8092327340853379843", 75550144)
+--Dlcname Stellaris: Stargazer Species Portrait
+addappid(3417860, 1, "ea43c617350d094f3e4e1bc8f237e397f15d90468eda42799a1563fdca3824c1") --Dlcname Stellaris: Stargazer Species Portrait
+setManifestid(3417860, "3240748767659651608", 52080)
+--Dlcname Vipra the Vapor Species Portrait
+addappid(4241410, 1, "53657c2cf107c24debd9b546fdd8da26650af6fe577171a7da4b3528220474f1") --Dlcname Vipra the Vapor Species Portrait
+setManifestid(4241410, "8704462531518179024", 85952)
+--Dlcname Stellaris: Nomads
+addappid(4241420, 1, "b1316e4a62fec8210e7bb8a0848e599d99bda4e4f5eb50733958d53280b41d7f") --Dlcname Stellaris: Nomads
+setManifestid(4241420, "4308526494459266879", 110816224)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229003, "8740933542064151477", 42914928)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(461071) --Dlcname AppID 461071
+addtoken(461071, "14499378624197210244")
+addappid(461073) --Dlcname AppID 461073
+addtoken(461073, "10913968139036982872")
+addappid(461461) --Dlcname AppID 461461
+addtoken(461461, "4737354047198272564")
+addappid(616190) --Dlcname Stellaris: Nova Edition Upgrade Pack
+addappid(616191) --Dlcname Stellaris: Galaxy Edition Upgrade Pack
+addappid(2729490) --Dlcname Stellaris: Expansion Subscription
+addappid(2863190) --Dlcname Stellaris: Season 08 - Expansion Pass
+addappid(3417870) --Dlcname Stellaris: Season 09 - Expansion Pass
+addappid(4241400) --Dlcname Stellaris: Season 10
+addappid(4241440) --Dlcname Stellaris: Willpower
+addappid(4241450) --Dlcname Stellaris: Scenario Pack 1
+addappid(4355700) --Dlcname Stellaris: Scenario Pack 2
