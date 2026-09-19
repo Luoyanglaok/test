5700a26457caeaac49156cebfe14f844e78e9b0b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 14:26:52 +0800

    add 881510.lua

--- 881510.lua
+++ 881510.lua
@@ -1,7 +1,11 @@
-addappid(881510)
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(881511,0,"4d15217ecebf136f6595a5cbbec74a8c8312d7ed360e04dc9b9bf4fda3b8212f")
-setManifestid(881511,"790036862485054514")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 881510.lua
+--Gamename The Official GamingTaylor Game, Great Job!
+addappid(881510) --Mainappid The Official GamingTaylor Game, Great Job!
+addappid(881511, 1, "4d15217ecebf136f6595a5cbbec74a8c8312d7ed360e04dc9b9bf4fda3b8212f") --Main Windows Depot The Official GamingTaylor Game, Great Job!
+setManifestid(881511, "790036862485054514", 310816464)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
