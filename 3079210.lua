1c66d322baabfb891b34c3f894d7d57fcaa1592a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 22:19:30 +0800

    add 3079210.lua

---
+++ 3079210.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3079210.lua
+--Gamename Hell Let Loose: Vietnam
+addappid(3079210) --Mainappid Hell Let Loose: Vietnam
+addappid(3079211, 1, "f7eb2462eb25f9a05bb91c0a20e77a6f943af632b06af7d4f2e19dd912176e6c") --Main Windows Depot Hell Let Loose: Vietnam
+setManifestid(3079211, "5180943606815003182", 19705029216)
+addappid(4447970) --Dlcname Field Supply: US Army Boat Crew Uniform
+addappid(4447980) --Dlcname Field Supply: NVA Boat Crew Uniform
+addappid(4448010) --Dlcname Hell Let Loose: Vietnam - 2026 Field Supplies
+addappid(5214260) --Dlcname Field Supply: Helicopter Survival Vest & SPH-4 Patriot
