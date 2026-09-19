8e55e0e6b2454aa241ec3074735339997d48bc94
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 16:46:22 +0800

    add 3084280.lua

--- 3084280.lua
+++ 3084280.lua
@@ -1,3 +1,10 @@
-addappid(3084280)
-addappid(3084281,0,"8992e70f6265e3afca1db65339f6132ed758d29b4517fd6e44039e6154082644")
-setManifestid(3084281,"7760939731312360583")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3084280.lua
+--Gamename Threefold Recital 三相奇谈
+addappid(3084280) --Mainappid Threefold Recital 三相奇谈
+addappid(3084281, 1, "8992e70f6265e3afca1db65339f6132ed758d29b4517fd6e44039e6154082644") --Main Windows Depot Threefold Recital 三相奇谈
+setManifestid(3084281, "7760939731312360583", 485527280)
+--Dlcname Threefold Recital Soundtrack
+addappid(3486410) --Dlcname Threefold Recital Soundtrack
+addappid(3486411, 1, "2e76a4c2e623c95f411d7444c0f873f53a21835b24d9733b94de649414e6315b") --Dlc Windows Depot Threefold Recital Soundtrack
+setManifestid(3486411, "9024515880274672779", 1127262528)
