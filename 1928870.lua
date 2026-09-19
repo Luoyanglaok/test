ef1daa2dd15eb5f898d796700522b8a2ea0be539
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 16:34:05 +0800

    add 1928870.lua

--- 1928870.lua
+++ 1928870.lua
@@ -1,3 +1,7 @@
-addappid(1928870)
-addappid(1928871,0,"77e4ff98428ea4b5db197ad78037b2c0b2ab66399777fa157d1bc84e99fdb289")
-setManifestid(1928871,"4592995720016410867")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1928870.lua
+--Gamename Minecraft Legends
+addappid(1928870) --Mainappid Minecraft Legends
+addappid(1928871, 1, "77e4ff98428ea4b5db197ad78037b2c0b2ab66399777fa157d1bc84e99fdb289") --Main Windows Depot Minecraft Legends
+setManifestid(1928871, "4592995720016410867", 10584888400)
+addappid(2187940) --Dlcname Minecraft Legends Deluxe DLC
