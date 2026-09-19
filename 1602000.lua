cda55ee8e2a4fb3fd715789d51e01a7e24fb8db9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 14:50:12 +0800

    add 1602000.lua

--- 1602000.lua
+++ 1602000.lua
@@ -3,7 +3,7 @@
 --Gamename Hotel Architect
 addappid(1602000, 1, "a17e0c50e72b96a8be98c8d7702bb79628b78e3d9532b1cc3bf7b34949259636") --Mainappid Hotel Architect
 addappid(1602001, 1, "0eae087f0db9be78952d7fae0ef01ab55a34cc7d8bae7f18ccc3379d89476fe7") --Main Windows Depot Hotel Architect
-setManifestid(1602001, "5737379820367698382", 1200575408)
+setManifestid(1602001, "3873579947127529239", 1205063312)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
