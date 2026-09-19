ae826883ed11f589392dbace187bc2400d5e6870
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:41:42 +0800

    add 1276790.lua

--- 1276790.lua
+++ 1276790.lua
@@ -1,4 +1,14 @@
-addappid(1276790)
-setManifestid(228990,"1829726630299308803")
-addappid(1276791,0,"517dbfbb44494a9af79aa0e5d1ec7bfeec6b33e7ed68e471f2ea92648e35c58f")
-setManifestid(1276791,"6102622854430292338")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1276790.lua
+--Gamename Ruined King: A League of Legends Story™
+addappid(1276790) --Mainappid Ruined King: A League of Legends Story™
+addappid(1276791, 1, "517dbfbb44494a9af79aa0e5d1ec7bfeec6b33e7ed68e471f2ea92648e35c58f") --Main Windows Depot Ruined King: A League of Legends Story™
+setManifestid(1276791, "6102622854430292338", 13792702400)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1815930) --Dlcname Ruined King: A League of Legends Story™ - Manamune Sword for Yasuo
+addtoken(1815930, "5487224755347259533")
+addappid(1815931) --Dlcname Ruined King: A League of Legends Story™ - Ruined Skin Variants
+addappid(1815932) --Dlcname Ruined King: A League of Legends Story™ - Ruination Starter Pack
+addappid(1815933) --Dlcname Ruined King: A League of Legends Story™ - Lost & Found Weapon Pack
