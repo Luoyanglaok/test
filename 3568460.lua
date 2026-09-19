ba899392dddfb1c8c55fd60befca9559e0195f7d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 18:00:23 +0800

    add 3568460.lua

---
+++ 3568460.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3568460.lua
+--Gamename MustScream
+addappid(3568460, 1, "8e663d1bad58ba2876487c38ea1a9dd6c4522464429da8be26800742720fc996") --Mainappid MustScream
+addappid(3568461, 1, "a8b7d5e3954c4ae3615a0c6547a17c1a3a6de9e02048b0306c1717928bea5896") --Main Windows Depot MustScream
+setManifestid(3568461, "3151096272661936669", 2119703728)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
