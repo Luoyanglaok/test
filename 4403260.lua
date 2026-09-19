b68d3728603915a247f43f56978a2e41c072bae2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 18:29:05 +0800

    add 4403260.lua

---
+++ 4403260.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4403260.lua
+--Gamename Swarm Bunker: Lust Defense
+addappid(4403260, 1, "0b4be040c08e56a350407ac6a1dce55e05edeb12d20bc13e6f3206b8571ea6cd") --Mainappid Swarm Bunker: Lust Defense
+addappid(4403261, 1, "ee00577eff184928522dc551698320ae55fd9ea8602fb92615c60161845f5903") --Main Windows Depot Swarm Bunker: Lust Defense
+setManifestid(4403261, "904650950847234712", 413048992)
