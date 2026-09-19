55d3cc8d42dbb19c4916354e31e55c4957f93c9a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 16:57:26 +0800

    add 1904480.lua

---
+++ 1904480.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1904480.lua
+--Gamename Absolum
+addappid(1904480, 1, "0af2c99f19603642021fffda7d5d95ac03f6b9b5426776add99de302aee3efb0") --Mainappid Absolum
+addappid(1904481, 1, "119d51fd44dd8198b1a6c12a73e3943c8587d93e990f15b7453404b51a8ce4f3") --Main Windows Depot Absolum
+setManifestid(1904481, "4229612714462700351", 4593690704)
+addappid(3812411, 1, "0ac2d57606b5b0e2c778ae4f9f7ade0e01941d73a9fcaa1bee1eec16b3b9d9d1") --Main Windows Depot Absolum
+setManifestid(3812411, "1975252275178417104", 133318080)
+addappid(3812414, 1, "9097694c6e5eb08331c079931bbb94099cd644883674d006270dd26f46ca85dd") --Main Windows Depot Absolum
+setManifestid(3812414, "4601597969254808548", 2342082336)
+--Dlcname Absolum (Original Game Soundtrack)
+addappid(3812410) --Dlcname Absolum (Original Game Soundtrack)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
