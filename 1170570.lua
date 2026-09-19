b5ae5cefc38ee00b46d9e5e290527b7a28d13a10
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 16:48:02 +0800

    add 1170570.lua

--- 1170570.lua
+++ 1170570.lua
@@ -1,7 +1,14 @@
-addappid(1170570)
-addappid(1170571,0,"ff0011f75c7eb759bc12f76299fcac9b69bb93985272bef530616f987c70ec8f")
-setManifestid(1170571,"7083837960242991248")
-addappid(1170572,0,"7d81728c32fd7968ca3485dbed5cb4be71b1409691e3f8b15ecd88f1c6021551")
-setManifestid(1170572,"4989703344661735582")
-addappid(1170573,0,"33cd587b83a1d217fe5fdec1ff8f0b41e45d065bbd303cfd1bbbb80f7432acc8")
-setManifestid(1170573,"58344633207006509")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1170570.lua
+--Gamename The Drifter
+addappid(1170570) --Mainappid The Drifter
+addappid(1170571, 1, "ff0011f75c7eb759bc12f76299fcac9b69bb93985272bef530616f987c70ec8f") --Main Windows Depot The Drifter
+setManifestid(1170571, "9157791965194182650", 603116672)
+addappid(1170572, 1, "7d81728c32fd7968ca3485dbed5cb4be71b1409691e3f8b15ecd88f1c6021551") --Main Macos Depot The Drifter
+setManifestid(1170572, "4359048483106282539", 611192160)
+addappid(1170573, 1, "33cd587b83a1d217fe5fdec1ff8f0b41e45d065bbd303cfd1bbbb80f7432acc8") --Main Linux Depot The Drifter
+setManifestid(1170573, "4609207423110939652", 604154992)
+--Dlcname The Drifter Original Soundtrack
+addappid(3857280) --Dlcname The Drifter Original Soundtrack
+addappid(3857281, 1, "170e007f3a8a97a5b79949ec9b13050ceddaa6e90a4cdf9c987b591afb8885c3") --Dlc Windows Depot The Drifter Original Soundtrack
+setManifestid(3857281, "2149510506183480721", 116653040)
