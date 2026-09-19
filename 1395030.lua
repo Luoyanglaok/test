44cec3de1abdeee87432778c1402b6cf07c21e48
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 16:50:42 +0800

    add 1395030.lua

--- 1395030.lua
+++ 1395030.lua
@@ -1,7 +1,16 @@
-addappid(1395030)
-addappid(1395031,0,"1f91bfa49a2a1c2408480b422395bf9779304e369c0e4e55492dff398064809d")
-setManifestid(1395031,"7713919728306704908")
-addappid(1395032,0,"24ee6fed0724d750cff98fb68c3b92b487820ca52f12e874142b7d331ff3ed57")
-setManifestid(1395032,"2838781748396371315")
-addappid(1395033,0,"2e0987a09fefb49563c2bbae7600a81099ca3764cdc7d2135d5f7e38426afa29")
-setManifestid(1395033,"5641309650353763309")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1395030.lua
+--Gamename Kathy Rain: Director's Cut
+addappid(1395030) --Mainappid Kathy Rain: Director's Cut
+addappid(1395031, 1, "1f91bfa49a2a1c2408480b422395bf9779304e369c0e4e55492dff398064809d") --Main Windows Depot Kathy Rain: Director's Cut
+setManifestid(1395031, "5493145509261332213", 440164144)
+addappid(1395032, 1, "24ee6fed0724d750cff98fb68c3b92b487820ca52f12e874142b7d331ff3ed57") --Main Macos Depot Kathy Rain: Director's Cut
+setManifestid(1395032, "846367865604733419", 439863728)
+addappid(1395033, 1, "2e0987a09fefb49563c2bbae7600a81099ca3764cdc7d2135d5f7e38426afa29") --Main Linux Depot Kathy Rain: Director's Cut
+setManifestid(1395033, "5487684927407467580", 437599136)
+--Dlcname Kathy Rain: Director's Cut Soundtrack
+addappid(1782060) --Dlcname Kathy Rain: Director's Cut Soundtrack
+addappid(1782061, 1, "0727575dbf85594ca2c84193c47cd24276f0ca7af9b5b34c998fd552a25ebd13") --Dlc Windows Depot Kathy Rain: Director's Cut Soundtrack
+setManifestid(1782061, "7233803051386581365", 205837504)
+addappid(1782062, 1, "2d696ac6fa3ea7319ce79b931775777eb62ad44480139891b1a2b04dd5fa8cc3") --Dlc Windows Depot Kathy Rain: Director's Cut Soundtrack
+setManifestid(1782062, "936568186168664657", 759406768)
