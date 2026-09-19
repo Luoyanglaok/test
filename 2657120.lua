98b9cacf9a35ed54de78dc9332f64046ba6301d0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 18:23:04 +0800

    add 2657120.lua

--- 2657120.lua
+++ 2657120.lua
@@ -1,3 +1,6 @@
-addappid(2657120)
-addappid(2657121,0,"7b381e90b749582143ed04410e6af148215b1c2f6a94435a3cc2db4eed0f6fba")
-setManifestid(2657121,"2760031481247223534")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2657120.lua
+--Gamename My Sexy Neighbor 🔞
+addappid(2657120) --Mainappid My Sexy Neighbor 🔞
+addappid(2657121, 1, "7b381e90b749582143ed04410e6af148215b1c2f6a94435a3cc2db4eed0f6fba") --Main Windows Depot My Sexy Neighbor 🔞
+setManifestid(2657121, "2760031481247223534", 2487437808)
