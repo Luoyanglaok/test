a31daa25c2abcad547c361c36c59d5595a39e384
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 13:24:48 +0800

    add 3995190.lua

---
+++ 3995190.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3995190.lua
+--Gamename Obsessed Lucy
+addappid(3995190, 1, "4ec6554966aed2b780d3e8fbbc1e860cbd36774bc3be0a96951dfa44d45a6aa4") --Mainappid Obsessed Lucy
+addappid(3995191, 1, "d51221beb57afe8c73d2f7550b4da163a690e5b288bbed0f8f826b94eceb2f0c") --Main Windows Depot Obsessed Lucy
+setManifestid(3995191, "5632343282501403060", 862295408)
