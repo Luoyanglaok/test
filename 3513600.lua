3cc149e36232c3ad66edbc07f264411bf2327212
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 17:35:27 +0800

    add 3513600.lua

---
+++ 3513600.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3513600.lua
+--Gamename 女吊
+addappid(3513600, 1, "b4704c00031136c900829a7cfd7fd915ac99bd486aaed523aaf8a0abfe7067dc") --Mainappid 女吊
+addappid(3513602, 1, "3f4bf9f70c9efa12c892a12adb82770844ca0f84f900a064c94dcb9b1ff2f817") --Main Windows Depot 女吊
+setManifestid(3513602, "9207214724964112489", 18591893680)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
