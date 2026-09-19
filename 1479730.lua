faf5d072202b5c2402da4715dbee82ba551cbf77
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 17:44:53 +0800

    add 1479730.lua

--- 1479730.lua
+++ 1479730.lua
@@ -1,7 +1,23 @@
-addappid(1479730)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1479731,0,"64205c91ebfac38afeb2011798b9270dc5d1eec20ef0d4b176a42fbf5570de84")
-setManifestid(1479731,"7610793428013477559")
-addappid(3565060,0,"25969d0cfdf09a97590616156414c9996ebf94afe209ed4cb0eb957fa79a2c77")
-setManifestid(3565060,"3073898408723519129")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1479730.lua
+--Gamename Commandos: Origins
+addappid(1479730, 1, "3f21e32d1a8c13a99e27c8d2f78714a163b990c36979ed5febb2280dfb9ae68b") --Mainappid Commandos: Origins
+addappid(1479731, 1, "64205c91ebfac38afeb2011798b9270dc5d1eec20ef0d4b176a42fbf5570de84") --Main Windows Depot Commandos: Origins
+setManifestid(1479731, "4629709270957703942", 15868704608)
+--Dlcname Commandos: Origins - Original Soundtrack
+addappid(3193810) --Dlcname Commandos: Origins - Original Soundtrack
+addappid(3193811, 1, "13bb24aec83dd1d97fb89aa9cde9ec167fe8b5725e5fa5cfd190dbfaf483f85d") --Dlc Windows Depot Commandos: Origins - Original Soundtrack
+setManifestid(3193811, "1987733506382615523", 101718800)
+addappid(3193812, 1, "1da5b04d5bf13510469f27a0d3e622897963d54b82e34d39a2330fc0153b6f49") --Dlc Windows Depot Commandos: Origins - Original Soundtrack
+setManifestid(3193812, "2912969025784508134", 649023440)
+--Dlcname Commandos: Origins - Classified Archives
+addappid(3565060, 1, "25969d0cfdf09a97590616156414c9996ebf94afe209ed4cb0eb957fa79a2c77") --Dlcname Commandos: Origins - Classified Archives
+setManifestid(3565060, "3073898408723519129", 562423344)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3194180) --Dlcname Commandos: Origins - Deluxe Edition Upgrade
+addappid(3933550) --Dlcname Commandos: Origins - Shadows over Crete
+addappid(4172650) --Dlcname Commandos: Origins - No Man Left Behind
