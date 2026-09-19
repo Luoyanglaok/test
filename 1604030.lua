06f04e7eaaf3f2cfb9de45775958577cd7f9a4e1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:43:40 +0800

    add 1604030.lua

--- 1604030.lua
+++ 1604030.lua
@@ -6,6 +6,12 @@ addappid(1604031, 1, "e51d4f407e118a5477cab33cf9cd3340057f5aa111772951f47d76b05b
 setManifestid(1604031, "2621767958913415677", 5050462560)
 addappid(1604032, 1, "05dc87d45e23f53bfdc06512480bf3a8d84941a929034772fba892bf252d9587") --Main Windows Depot V Rising
 setManifestid(1604032, "8225175356744277948", 4321140624)
+--Dlcname V Rising Soundtrack
+addappid(1999270) --Dlcname V Rising Soundtrack
+addappid(1999271, 1, "018b653b9b20cb999529b75c5893c842325de70b50a9d294e1425f2461089604") --Dlc Windows Depot V Rising Soundtrack
+setManifestid(1999271, "873845939608994832", 282881712)
+addappid(1999272, 1, "bc8a0725a46c16535d2b0ea0ca3671bc100e16c5feb496ba92fa7bbfacc89061") --Dlc Windows Depot V Rising Soundtrack
+setManifestid(1999272, "5505416664520345647", 1986502960)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
