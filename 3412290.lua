bfae521705604f52392d895c849bb33fdbdcf565
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:30:57 +0800

    add 3412290.lua

---
+++ 3412290.lua
@@ -0,0 +1,20 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3412290.lua
+--Gamename NTRaholic SS
+addappid(3412290, 1, "0812d4dc18c318478e71838cae640e975eeb31cced21cf007f4e3d283cab942e") --Mainappid NTRaholic SS
+addappid(3412291, 1, "6ffadbf13b82f0bb9451503db899a299c83c0e2289e8bcb117282226421a96cc") --Main Windows Depot NTRaholic SS
+setManifestid(3412291, "4959263568987710276", 592624848)
+--Dlcname NTRaholic SS - ArtBook
+addappid(4234740) --Dlcname NTRaholic SS - ArtBook
+addappid(4234741, 1, "e60f623fd5a69d4c4e53f474549a7e3a3b96b64de85d0fd9f8032d4ed6cf11a3") --Dlc Windows Depot NTRaholic SS - ArtBook
+setManifestid(4234741, "891903234942766292", 28242608)
+addappid(4234742, 1, "d06d3db6b9ed92524202410c2439c5457f4054ecc47d9f4331bc09737d9b1d11") --Dlc Windows Language Depot Schinese NTRaholic SS - ArtBook
+setManifestid(4234742, "2351154192606060678", 28317504)
+addappid(4234743, 1, "916c779d0368bb83f5bad5c2d612fc23a8b56b2b89d6855866a0e51c076cdf7e") --Dlc Windows Language Depot Japanese NTRaholic SS - ArtBook
+setManifestid(4234743, "1573297870613424866", 28294368)
+addappid(4234744, 1, "3e5b674e015dbf5650e852200ddc3737df7c7261048b9a3924ee0a30b3c76e1b") --Dlc Windows Language Depot Koreana NTRaholic SS - ArtBook
+setManifestid(4234744, "4869641985846109073", 28259232)
+addappid(4234745, 1, "5b1bb4866bc7fe509f151aabee9bf56c1b1a0574cf52e76c6d2d8bc29286fe74") --Dlc Windows Language Depot Tchinese NTRaholic SS - ArtBook
+setManifestid(4234745, "5019499892992148050", 28317728)
+--Missing Dlcs
+--Dlcname NTRaholic SS - Soundtrack
