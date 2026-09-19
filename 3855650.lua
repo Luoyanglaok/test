e337d151ecde57b30fe8671e0e48c99973712b47
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 17:21:34 +0800

    add 3855650.lua

---
+++ 3855650.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3855650.lua
+--Gamename 容器
+addappid(3855650) --Mainappid 容器
+addappid(3855651, 1, "c7cf5a8bba0bd852efd2d8440ac367d20345cefa9dff0fedf1729714a44212af") --Main Windows Depot 容器
+setManifestid(3855651, "7367267051770525324", 9082796080)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
