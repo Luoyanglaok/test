cdb1333981eab601325b3d49fd9f6364a6f789fb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 14:35:23 +0800

    add 460930.lua

--- 460930.lua
+++ 460930.lua
@@ -1,56 +1,117 @@
-addappid(460930)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(460932,0,"bde2110e49d13530ba0d862bf38a2f1da77ebca381bb46123dd94992b4566bbf")
-setManifestid(460932,"4925482683944733643")
-addappid(460934,0,"13ce87f397410fb2e07a8eec31bcd08d89ab99d3f69dc55bf992f2438fc85012")
-setManifestid(460934,"4444935984325081985")
-addappid(460935,0,"cb668cc06add491f81676b5b5e74ee1197422be29ce3613f2c74ecd0d1c699b8")
-setManifestid(460935,"9199072077909944392")
-addappid(460936,0,"b544bb4108b7ae8c010295e4fac8a1479bb9e89da5df5af021d42b9c8a9b1d1d")
-setManifestid(460936,"8051295774455126082")
-addappid(460937,0,"d36441c770d9d5c8cf8d772e3b81ef84556fda68e52af96a9b0c5b16b1d04aa0")
-setManifestid(460937,"1905479328869316905")
-addappid(460938,0,"27d9c76eade84acdd844666a43364165287be4f612e8427e02c727d08bdb6576")
-setManifestid(460938,"5496547418910000312")
-addappid(460939,0,"5efd1f1fe70217560d4cff479ea20def14fec7279f1850a12435e7e5ec95e39f")
-setManifestid(460939,"4811991489032470258")
-addappid(527063,0,"de7a5d543800eafbf2935b8ddddd530e74ded8abdcb238dcb4b876eb98ea6532")
-setManifestid(527063,"222418595712558555")
-addappid(527050,0,"f10cb78d153689ea0177527be3d7b93d805333df0d03eb32a724eda5fb62f7fb")
-setManifestid(527050,"6133416095183290669")
-addappid(527051,0,"7de4c108fe2624e7eb85d16984171d5646021b28efefdf0b48a9052fe5545d9b")
-setManifestid(527051,"8704848836597241185")
-addappid(527052,0,"0c465a66d1535d9a0ec9c0593150b6f097cf83cabee96700261ec0bc5d5a012e")
-setManifestid(527052,"7953425400952113538")
-addappid(527053,0,"1d5327ab8926812c42541b648727b19f5c5e4d6fe0b70edb05fed99d398e15fc")
-setManifestid(527053,"3188754202211716325")
-addappid(527054,0,"4c99be4a2d810244c95983f997c9742ed450d274fc3d1753930af59f41c22b9f")
-setManifestid(527054,"2125566137229626735")
-addappid(527055,0,"e0e96bf59ed92e7db65e7c77059b9f141935835f52782d0351c27610a0a60f4d")
-setManifestid(527055,"7137644527826082354")
-addappid(527056,0,"6048699beac55e725b81369d008b8ab8252bc97c45e5eb0b608ef93c7d6f338d")
-setManifestid(527056,"7414914160411395915")
-addappid(527057,0,"546beca62ef655656b5e3e7d7ef648cc78b3ece40c9b184ecd9802420b3e0acc")
-setManifestid(527057,"6207633329687923501")
-addappid(527058,0,"7cc5e490129d7e39967b2f8968c2ec31525f6fb501401f6b16a4e1ae820c627f")
-setManifestid(527058,"2115994499122544238")
-addappid(527059,0,"bff5883e41209a1c1157f58739a8f4da75fec461f22b2a2151a60d575660ca57")
-setManifestid(527059,"5196520543398736486")
-addappid(527060,0,"e231cae9d9d2e2c8211dca82c0586807aaaa57939e81870e366dbf40c9c66f8b")
-setManifestid(527060,"6909220124751877463")
-addappid(527061,0,"532e7576df7da83b13254a9447e52174d79e2e93e3e32a67c9c77057ba4867ca")
-setManifestid(527061,"5809069132872559849")
-addappid(527062,0,"c285d9099dea161bfabdea70627aa3d08883ef105bef6559a05c88c699f5fce1")
-setManifestid(527062,"6854033068166781157")
-addappid(527064,0,"020c6de13a43f818050fdf62802aed3eafa69e14002e4128d4f4598784125700")
-setManifestid(527064,"8291120964054423790")
-addappid(527065,0,"4a4a61de29327f30e4246ffd40b77b6968e40965d4e2dd687b1942016b672276")
-setManifestid(527065,"646189603913890033")
-addappid(573831,0,"b98f4b2691d92850b6dbd670b4259b24b37f748b2019344233308f1ac849ba0d")
-setManifestid(573831,"504287665923200055")
-addappid(573832,0,"8b0e38a0d31e60759feb1870c81f6a1e54b8373e109942f232ac04b886d6edd3")
-setManifestid(573832,"1388355723944228642")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 460930.lua
+--Gamename Tom Clancy's Ghost Recon® Wildlands
+addappid(460930, 1, "c25d8c77b47191a3d41e556d99fe3adae1457c74f6f6bed925f6e57f3541cfda") --Mainappid Tom Clancy's Ghost Recon® Wildlands
+addappid(460932, 1, "bde2110e49d13530ba0d862bf38a2f1da77ebca381bb46123dd94992b4566bbf") --Main Windows Depot Tom Clancy's Ghost Recon® Wildlands
+setManifestid(460932, "804720071482135820", 5733163648)
+addappid(460933, 1, "90d2ad5b74312c5928a1ac0dcf3b30f071f44dc683c546aeb1ec515464aa442b") --Main Windows Depot Tom Clancy's Ghost Recon® Wildlands
+setManifestid(460933, "327746146178771991", 0)
+addappid(460934, 1, "13ce87f397410fb2e07a8eec31bcd08d89ab99d3f69dc55bf992f2438fc85012") --Main Windows Depot Tom Clancy's Ghost Recon® Wildlands
+setManifestid(460934, "7535029440738114717", 14236740432)
+addappid(460935, 1, "cb668cc06add491f81676b5b5e74ee1197422be29ce3613f2c74ecd0d1c699b8") --Main Windows Depot Tom Clancy's Ghost Recon® Wildlands
+setManifestid(460935, "2292654802067055797", 28267170288)
+addappid(460936, 1, "b544bb4108b7ae8c010295e4fac8a1479bb9e89da5df5af021d42b9c8a9b1d1d") --Main Windows Depot Tom Clancy's Ghost Recon® Wildlands
+setManifestid(460936, "617475596838380834", 5365500912)
+addappid(460937, 1, "d36441c770d9d5c8cf8d772e3b81ef84556fda68e52af96a9b0c5b16b1d04aa0") --Main Windows Depot Tom Clancy's Ghost Recon® Wildlands
+setManifestid(460937, "2513463296456267082", 8535805856)
+addappid(460938, 1, "27d9c76eade84acdd844666a43364165287be4f612e8427e02c727d08bdb6576") --Main Windows Depot Tom Clancy's Ghost Recon® Wildlands
+setManifestid(460938, "3670877133173363108", 4435810336)
+addappid(460939, 1, "5efd1f1fe70217560d4cff479ea20def14fec7279f1850a12435e7e5ec95e39f") --Main Windows Depot Tom Clancy's Ghost Recon® Wildlands
+setManifestid(460939, "4258098457727288719", 63568352)
+addappid(527050, 1, "f10cb78d153689ea0177527be3d7b93d805333df0d03eb32a724eda5fb62f7fb") --Main Windows Language Depot French Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527050, "308046467144140790", 602713456)
+addappid(527051, 1, "7de4c108fe2624e7eb85d16984171d5646021b28efefdf0b48a9052fe5545d9b") --Main Windows Language Depot Italian Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527051, "589023180176785721", 518019024)
+addappid(527052, 1, "0c465a66d1535d9a0ec9c0593150b6f097cf83cabee96700261ec0bc5d5a012e") --Main Windows Language Depot German Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527052, "7176192640852328538", 506000704)
+addappid(527053, 1, "1d5327ab8926812c42541b648727b19f5c5e4d6fe0b70edb05fed99d398e15fc") --Main Windows Language Depot Spanish Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527053, "1680101350595557077", 994851264)
+addappid(527054, 1, "4c99be4a2d810244c95983f997c9742ed450d274fc3d1753930af59f41c22b9f") --Main Windows Language Depot Brazilian Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527054, "5092059339118973534", 503720624)
+addappid(527055, 1, "e0e96bf59ed92e7db65e7c77059b9f141935835f52782d0351c27610a0a60f4d") --Main Windows Language Depot Russian Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527055, "3472546661131702440", 662727136)
+addappid(527056, 1, "6048699beac55e725b81369d008b8ab8252bc97c45e5eb0b608ef93c7d6f338d") --Main Windows Language Depot Japanese Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527056, "2116524945520231333", 667564464)
+addappid(527057, 1, "546beca62ef655656b5e3e7d7ef648cc78b3ece40c9b184ecd9802420b3e0acc") --Main Windows Language Depot Dutch Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527057, "3178348416641631674", 2912)
+addappid(527058, 1, "7cc5e490129d7e39967b2f8968c2ec31525f6fb501401f6b16a4e1ae820c627f") --Main Windows Language Depot Polish Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527058, "11003191118063283", 3216)
+addappid(527059, 1, "bff5883e41209a1c1157f58739a8f4da75fec461f22b2a2151a60d575660ca57") --Main Windows Language Depot Czech Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527059, "5950687316439225007", 3120)
+addappid(527060, 1, "e231cae9d9d2e2c8211dca82c0586807aaaa57939e81870e366dbf40c9c66f8b") --Main Windows Language Depot Koreana Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527060, "7134784222768835703", 3184)
+addappid(527061, 1, "532e7576df7da83b13254a9447e52174d79e2e93e3e32a67c9c77057ba4867ca") --Main Windows Language Depot Schinese Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527061, "652648805899187529", 2592)
+addappid(527062, 1, "c285d9099dea161bfabdea70627aa3d08883ef105bef6559a05c88c699f5fce1") --Main Windows Language Depot Tchinese Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527062, "1601785841494574700", 2608)
+addappid(527063, 1, "de7a5d543800eafbf2935b8ddddd530e74ded8abdcb238dcb4b876eb98ea6532") --Main Windows Language Depot English Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527063, "3055372238354968683", 2784)
+addappid(527064, 1, "020c6de13a43f818050fdf62802aed3eafa69e14002e4128d4f4598784125700") --Main Windows Language Depot Arabic Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527064, "6226407495085201173", 3184)
+addappid(527065, 1, "4a4a61de29327f30e4246ffd40b77b6968e40965d4e2dd687b1942016b672276") --Main Windows Language Depot Latam Tom Clancy's Ghost Recon® Wildlands
+setManifestid(527065, "4335583643660653928", 489746544)
+--Dlcname Tom Clancy's Ghost Recon Wildlands - Narco Road
+addtoken(573831, "2843020018421907384")
+addappid(573831, 1, "b98f4b2691d92850b6dbd670b4259b24b37f748b2019344233308f1ac849ba0d") --Dlcname Tom Clancy's Ghost Recon Wildlands - Narco Road
+setManifestid(573831, "3620668370804767189", 5136976000)
+--Dlcname Tom Clancy's Ghost Recon Wildlands - Fallen Ghosts
+addappid(573832, 1, "8b0e38a0d31e60759feb1870c81f6a1e54b8373e109942f232ac04b886d6edd3") --Dlcname Tom Clancy's Ghost Recon Wildlands - Fallen Ghosts
+setManifestid(573832, "4152690107404347390", 4853471824)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(573830) --Dlcname Tom Clancy's Ghost Recon Wildlands - Season Pass
+addappid(594560) --Dlcname Tom Clancy's Ghost Recon Wildlands - WW Uplay Activation
+addappid(594561) --Dlcname Tom Clancy's Ghost Recon Wildlands - RU/CN Uplay Activation
+addappid(601290) --Dlcname Tom Clancy's Ghost Recon Wildlands - WW Pre-order Standard Uplay Activation
+addappid(601291) --Dlcname Tom Clancy's Ghost Recon Wildlands - RU/CN Pre-order Standard Uplay Activation
+addappid(601292) --Dlcname Tom Clancy's Ghost Recon Wildlands - ASIA Pre-order Standard Uplay Activation
+addappid(601293) --Dlcname Tom Clancy's Ghost Recon Wildlands - WW Pre-order Deluxe Uplay Activation
+addappid(601294) --Dlcname Tom Clancy's Ghost Recon Wildlands - RU/CN Pre-order Deluxe Uplay Activation
+addappid(601295) --Dlcname Tom Clancy's Ghost Recon Wildlands - ASIA Pre-order Deluxe Uplay Activation
+addappid(601296) --Dlcname Tom Clancy's Ghost Recon Wildlands - WW Pre-order Gold Uplay Activation
+addappid(601297) --Dlcname Tom Clancy's Ghost Recon Wildlands - RU/CN Pre-order Gold Uplay Activation
+addappid(601298) --Dlcname Tom Clancy's Ghost Recon Wildlands - ASIA Pre-order Gold Uplay Activation
+addappid(601300) --Dlcname Tom Clancy's Ghost Recon Wildlands - WW Deluxe Uplay Activation
+addappid(601301) --Dlcname Tom Clancy's Ghost Recon Wildlands - RU/CN Deluxe Uplay Activation
+addappid(601302) --Dlcname Tom Clancy's Ghost Recon Wildlands - WW Gold Uplay Activation
+addappid(601303) --Dlcname Tom Clancy's Ghost Recon Wildlands - RU/CN Gold Uplay Activation
+addappid(601310) --Dlcname Tom Clancy's Ghost Recon Wildlands - Season Pass Uplay Activation
+addappid(623780) --Dlcname AppID 623780
+addtoken(623780, "6871850729114411039")
+addappid(623781) --Dlcname AppID 623781
+addtoken(623781, "16084786785819420607")
+addappid(729730) --Dlcname AppID 729730
+addappid(733960) --Dlcname AppID 733960
+addappid(733970) --Dlcname Tom Clancy's Ghost Recon® Wildlands Full Game Ownership
+addtoken(733970, "13152026892702008725")
+addappid(750230) --Dlcname Tom Clancy's Ghost Recon Wildlands - Ghost War Pass
+addappid(751960) --Dlcname AppID 751960
+addtoken(751960, "15206237667257876809")
+addappid(832700) --Dlcname Tom Clancy's Ghost Recon Wildlands - Y2
+addappid(832701) --Dlcname AppID 832701
+addtoken(832701, "981625427631188344")
+addappid(931190) --Dlcname AppID 931190
+addtoken(931190, "13750867402684765005")
+addappid(931191) --Dlcname Tom Clancy's Ghost Recon Wildlands - RU/CN Gold Year 2 Uplay Activation
+addappid(931192) --Dlcname AppID 931192
+addtoken(931192, "17287294552728189835")
+addappid(931193) --Dlcname Tom Clancy's Ghost Recon Wildlands - RU/CN Ultimate Year 2 Uplay Activation
+addappid(4784340) --Dlcname Tom Clancy's Ghost Recon Wildlands - Definitive Edition Ubisoft Activation -WW
+addappid(4784350) --Dlcname Tom Clancy's Ghost Recon Wildlands - Definitive Edition Ubisoft Activation - RU/CN
+addappid(4784380) --Dlcname Tom Clancy's Ghost Recon Wildlands: Cold Eye Pack
+addappid(4784390) --Dlcname Tom Clancy's Ghost Recon Wildlands: Cold Eye Pack Ubisoft Activation
+addappid(4784400) --Dlcname Tom Clancy's Ghost Recon Wildlands: Nightfall Protocol Pack
+addappid(4784410) --Dlcname Tom Clancy's Ghost Recon Wildlands: Nightfall Protocol Pack Ubisoft Activation
+addappid(4784420) --Dlcname Tom Clancy's Ghost Recon Wildlands - Predator Premium Pack
+addappid(4784430) --Dlcname Tom Clancy's Ghost Recon Wildlands - Predator Premium Pack Ubisoft Activation
+addappid(4784440) --Dlcname Tom Clancy's Ghost Recon Wildlands: Definitive Edition Upgrade
+addappid(4784450) --Dlcname Tom Clancy's Ghost Recon Wildlands: Definitive Edition Upgrade Ubisoft Activation
+addappid(5192150) --Dlcname Tom Clancy's Ghost Recon Wildlands: Deluxe Pack
+addappid(5192160) --Dlcname Tom Clancy's Ghost Recon Wildlands - Deluxe Pack Ubisoft Activation
