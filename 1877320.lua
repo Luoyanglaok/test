490adeba1bbdd865a20001ca4c0bdbd8b32bf420
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 20:43:14 +0800

    add 1877320.lua

--- 1877320.lua
+++ 1877320.lua
@@ -1,3 +1,6 @@
-addappid(1877320)
-addappid(1877321,0,"6d803475f030b35ac1519ebb89d2fa3d5fc10cee2bcb101a8eaa10e7fd5c869f")
-setManifestid(1877321,"8110858852638175647")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1877320.lua
+--Gamename The Walking Dead: Betrayal
+addappid(1877320) --Mainappid The Walking Dead: Betrayal
+addappid(1877321, 1, "6d803475f030b35ac1519ebb89d2fa3d5fc10cee2bcb101a8eaa10e7fd5c869f") --Main Windows Depot The Walking Dead: Betrayal
+setManifestid(1877321, "8110858852638175647", 3231075504)
