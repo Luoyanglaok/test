3918c3f0ba63147b34b59e586bc1600d6a0e9d09
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 19:00:46 +0800

    add 583810.lua

--- 583810.lua
+++ 583810.lua
@@ -1,21 +1,24 @@
-addappid(583810)
-addappid(583811,0,"36e7b1016079bfb946e7c59b7677949091a517cd03587caa603dcfc20f17b28e")
-setManifestid(583811,"8253221820533898039")
-addappid(583812,0,"3cdde1e07a57728e52fdf3a16629160e3aa1a91141a1c068863a9cbb0d726ed0")
-setManifestid(583812,"966416827336040689")
-addappid(583813,0,"06419bb48cfc02165a531e9b917670688fa0f96ec65913c5b435822b7d7e9bb3")
-setManifestid(583813,"8960716292059363130")
-addappid(583814,0,"3347d5c4d50c459dd2a6c76a4c9681cf06dcbf4476558efaba51c37490587d06")
-setManifestid(583814,"7287749018283110606")
-addappid(583815,0,"aaa2750460ed1409918a573753cf810acc13c670297d8ea6bd8b63a32c3b0f15")
-setManifestid(583815,"1679476974014982504")
-addappid(583816,0,"78ed9f0075ada547c58d79794bd965b4b19627d652196c4e9d1105aa150cff65")
-setManifestid(583816,"1715021259447610288")
-addappid(583817,0,"e775b88948beb9658f34d018a3fd48dac5776f4cbc8b2b0a07a19734fa818c09")
-setManifestid(583817,"7987493839995406587")
-addappid(583818,0,"f4a1c744ce7a4d5ba7144025a7da2f9151a85884548b737d0f2a397440b6fbe1")
-setManifestid(583818,"6399794036426392597")
-addappid(583819,0,"1d8620f6837e7b32f5eece01135b457123c6285f34e1eaa914586e2212a629dd")
-setManifestid(583819,"7943629174887838133")
-addappid(1233910,0,"0d12b58407b173a09f242a91ff4027d998dbc52c93c143fd3f37e83f35163cd6")
-setManifestid(1233910,"4494750808554982374")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 583810.lua
+--Gamename Resident Evil 7 biohazard Original Soundtrack
+addappid(583810) --Mainappid Resident Evil 7 biohazard Original Soundtrack
+addappid(583811, 1, "36e7b1016079bfb946e7c59b7677949091a517cd03587caa603dcfc20f17b28e") --Main Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583811, "8253221820533898039", 313942896)
+addappid(583812, 1, "3cdde1e07a57728e52fdf3a16629160e3aa1a91141a1c068863a9cbb0d726ed0") --Main Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583812, "966416827336040689", 396310656)
+addappid(583813, 1, "06419bb48cfc02165a531e9b917670688fa0f96ec65913c5b435822b7d7e9bb3") --Main Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583813, "8960716292059363130", 691776528)
+addappid(583814, 1, "3347d5c4d50c459dd2a6c76a4c9681cf06dcbf4476558efaba51c37490587d06") --Main Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583814, "7287749018283110606", 1481879456)
+addappid(583815, 1, "aaa2750460ed1409918a573753cf810acc13c670297d8ea6bd8b63a32c3b0f15") --Main Windows Depot Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583815, "1679476974014982504", 399548608)
+addappid(583816, 1, "78ed9f0075ada547c58d79794bd965b4b19627d652196c4e9d1105aa150cff65") --Main Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583816, "1715021259447610288", 311903312)
+addappid(583817, 1, "e775b88948beb9658f34d018a3fd48dac5776f4cbc8b2b0a07a19734fa818c09") --Main Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583817, "7987493839995406587", 394274784)
+addappid(583818, 1, "f4a1c744ce7a4d5ba7144025a7da2f9151a85884548b737d0f2a397440b6fbe1") --Main Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583818, "6399794036426392597", 689735664)
+addappid(583819, 1, "1d8620f6837e7b32f5eece01135b457123c6285f34e1eaa914586e2212a629dd") --Main Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(583819, "7943629174887838133", 1479839920)
+addappid(1233910, 1, "0d12b58407b173a09f242a91ff4027d998dbc52c93c143fd3f37e83f35163cd6") --Main Windows Language Depot Japanese Resident Evil 7 biohazard Original Soundtrack
+setManifestid(1233910, "4494750808554982374", 397509952)
