14600643911c76f307dfcd14ff3d19f964a8716c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 16:58:28 +0800

    add 433340.lua

--- 433340.lua
+++ 433340.lua
@@ -10,8 +10,14 @@ addappid(433343, 1, "5e5f51850ece04988ceff04c7b3bedd6f2e617e1a7e707e9cba06a89f8d
 setManifestid(433343, "1110279965060068321", 536827744)
 addappid(433344, 1, "3fb5cef87b8f8a4684a476b8d7429b100117e1bd76a4961e168c98d036422e10") --Main Linux Depot Slime Rancher
 setManifestid(433344, "7144887116487152453", 539577280)
+--Dlcname Slime Rancher: Original Soundtrack
+addappid(671160, 1, "0538dc5352b0d179f73adc71a527106785c477716a6b24c616a7698d2b3d090c") --Dlcname Slime Rancher: Original Soundtrack
+setManifestid(671160, "955344548331645278", 205412192)
 --Dlcname Slime Rancher: VR Playground
 addappid(939480, 1, "791322ae7ba59474f3e7faea093783d42597e0dc27c5dd3d1c5325f57cccc6b6") --Dlcname Slime Rancher: VR Playground
 setManifestid(939480, "3789258136107597671", 254688352)
+--Dlcname Slime Rancher - Original Soundtrack II
+addappid(1207000, 1, "50bb3ad1c86384222f45bf83b02ac1a94cae03f5d455bde756d5d5f4983de5ee") --Dlcname Slime Rancher - Original Soundtrack II
+setManifestid(1207000, "1193531183166185565", 170165632)
 addappid(982310) --Dlcname Slime Rancher: Galactic Bundle
 addappid(1079180) --Dlcname Slime Rancher: Secret Style Pack
