d002b3d0dc84c4377198685fcaa832a3b3761f8f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:40:12 +0800

    add 1954200.lua

--- 1954200.lua
+++ 1954200.lua
@@ -1,5 +1,18 @@
-addappid(1954200)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1954201,0,"dbc042174e03cba3e4b8b08266abefcc5d6b3aab5926133d77969966a45d8b3e")
-setManifestid(1954201,"1974986714015120124")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1954200.lua
+--Gamename Kena: Bridge of Spirits
+addappid(1954200, 1, "7b05cb467ac784fdb191a021b48a534b7736e3415cbc2809910a40e61eaddc55") --Mainappid Kena: Bridge of Spirits
+addappid(1954201, 1, "dbc042174e03cba3e4b8b08266abefcc5d6b3aab5926133d77969966a45d8b3e") --Main Windows Depot Kena: Bridge of Spirits
+setManifestid(1954201, "1974986714015120124", 24450208240)
+--Dlcname Kena: Bridge of Spirits (Original Soundtrack)
+addappid(2140250) --Dlcname Kena: Bridge of Spirits (Original Soundtrack)
+addappid(2140251, 1, "a169a6c2bfdd785fbef4058e63cf2231895411dbb93168d090b5089ffedad410") --Dlc Windows Depot Kena: Bridge of Spirits (Original Soundtrack)
+setManifestid(2140251, "2197920483126044540", 276700960)
+addappid(2140252, 1, "89f36e7c622c073c746c841c9597ba538b5f2e9c0a1e1d7b9eab3d29482b687a") --Dlc Windows Depot Kena: Bridge of Spirits (Original Soundtrack)
+setManifestid(2140252, "6549629608117006893", 678460160)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1964240) --Dlcname Kena: Bridge of Spirits - Deluxe Pack
