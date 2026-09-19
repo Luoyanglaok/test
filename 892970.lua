51bad8d5233f4f46497c0a6b037b2b293d151f9c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:01:10 +0800

    add 892970.lua

--- 892970.lua
+++ 892970.lua
@@ -3,11 +3,11 @@
 --Gamename Valheim
 addappid(892970, 1, "82c97459c7f36e035c12075be27cc05f5a36a91a9be322b60cb3e9f83b62cf29") --Mainappid Valheim
 addappid(892971, 1, "fb041c748eb3303b0bb18fbad3b813d19052060d14953e0d7d735943075833fd") --Main Linux Depot Valheim
-setManifestid(892971, "6181039652481492267", 3935520976)
+setManifestid(892971, "4125496845945961733", 3951586640)
 addappid(892972, 1, "10d286383a9248b6de0c8a160231b4d6332bce1da1fce424d4156329dda3ecf6") --Main Windows Depot Valheim
-setManifestid(892972, "8420184671320173885", 4337508016)
+setManifestid(892972, "5527549777940605084", 4355231824)
 addappid(892973, 1, "6c4d88d54997dcc4234f7ed792e946c0386a6c9a4f242a2bc8c89b49d50f3975") --Main Macos Depot Valheim
-setManifestid(892973, "6358775106463601863", 4335060704)
+setManifestid(892973, "1139245080464079516", 4352256768)
 --Dlcname Valheim Soundtrack
 addappid(1620250) --Dlcname Valheim Soundtrack
 addappid(1620251, 1, "bc36439dff4e62bb524374697aea37fb663b8620544c71746ae49db548454536") --Dlc Windows Depot Valheim Soundtrack
