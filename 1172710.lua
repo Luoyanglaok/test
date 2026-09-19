a44506b08778cf64b60b0595afb2d16db88c5f56
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 14:46:48 +0800

    add 1172710.lua

--- 1172710.lua
+++ 1172710.lua
@@ -1,7 +1,32 @@
-addappid(1172710)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1172711,0,"fca2f8a025b411ec4d846fe6bf97d1d0589a93bb56a2527fb857a4bfe4aac2b3")
-setManifestid(1172711,"8582420525960215810")
-addappid(1172719,0,"7b18d5460246bac4620753a4ed05b5f4e42c3c6ad1fce01a9508eb6a9fac5e08")
-setManifestid(1172719,"4822273885625674420")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1172710.lua
+--Gamename Dune: Awakening
+addappid(1172710, 1, "69dc68fc6a5bb50adb32e772e7746d731a85cd2e24df94d25f3208cd3526f454") --Mainappid Dune: Awakening
+addappid(1172711, 1, "fca2f8a025b411ec4d846fe6bf97d1d0589a93bb56a2527fb857a4bfe4aac2b3") --Main Windows Depot Dune: Awakening
+setManifestid(1172711, "1624791797683907617", 36066496864)
+--Dlcname Dune: Awakening - Digital Artbook
+addappid(3604190) --Dlcname Dune: Awakening - Digital Artbook
+addappid(3604191, 1, "c572ef768fbab850c416f10be050771ed650c63967a64819d1b61d1a34e3cbaa") --Dlc Windows Depot Dune: Awakening - Digital Artbook
+setManifestid(3604191, "1970619944737853826", 40527760)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3596810) --Dlcname Dune: Awakening - Season Pass
+addappid(3596890) --Dlcname Dune: Awakening - Wildlife of Arrakis
+addappid(3596900) --Dlcname Dune: Awakening - Lost Harvest DLC
+addappid(3596920) --Dlcname Dune: Awakening - Raiders of the Broken Lands DLC
+addappid(3596930) --Dlcname Dune: Awakening - The Water Wars DLC
+addappid(3604150) --Dlcname Dune: Awakening - Terrarium of Muad'Dib
+addappid(3604160) --Dlcname Dune: Awakening - Dune (2021) Film Stillsuit
+addappid(3604170) --Dlcname Dune: Awakening - Sardaukar Bator Armor
+addappid(3604180) --Dlcname Dune: Awakening - Caladan Palace Building Set
+addappid(3604200) --Dlcname Dune: Awakening - Deluxe Edition Upgrade
+addappid(3604210) --Dlcname Dune: Awakening - Sunset Dye Global Swatch
+addtoken(3604210, "5973425123978831209")
+addappid(3604270) --Dlcname Dune: Awakening - Dusk Rider Sandbike Swatch
+addappid(3604280) --Dlcname Dune: Awakening - Blue Dasher Ornithopter Swatch
+addappid(3796170) --Dlcname Dune: Awakening - Ultimate Edition Upgrade
+addappid(4156640) --Dlcname Dune: Awakening - Gift of the Sands Free Pack
+addappid(4857580) --Dlcname Dune: Awakening - Filmic Archive DLC
