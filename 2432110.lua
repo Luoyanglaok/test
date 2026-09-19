49ab3fe33239656af7924e28886baedeb3099360
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 14:23:46 +0800

    add 2432110.lua

--- 2432110.lua
+++ 2432110.lua
@@ -1,9 +1,12 @@
-addappid(2432110)
-addappid(2432111,0,"21f616bc071cd69c97b0436c2558c2369454c3a3bf26c4be58e083594817fbb9")
-setManifestid(2432111,"3118998383619905222")
-addappid(2432112,0,"bb500034e6f19b0aa80b8f91115f598022baae94f1c27092fa0d1940949fa849")
-setManifestid(2432112,"5953665294834866003")
-addappid(2432113,0,"fbab7cef525d7a82233e82e1f087275f7214853697d44a034e3b86d4514b8633")
-setManifestid(2432113,"4486162893703204728")
-addappid(2432114,0,"63837fda42eb7195bf79308d0f6b869f5b3427860c7f2e8a14388508b36445fc")
-setManifestid(2432114,"3097335352710840729")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2432110.lua
+--Gamename WHITE ALBUM: Memories like Falling Snow
+addappid(2432110, 1, "e8daa277d09ba9ea06e776bb40f29d905641defe6ce4c3b272a90e22ec5a2d2e") --Mainappid WHITE ALBUM: Memories like Falling Snow
+addappid(2432111, 1, "21f616bc071cd69c97b0436c2558c2369454c3a3bf26c4be58e083594817fbb9") --Main Windows Depot WHITE ALBUM: Memories like Falling Snow
+setManifestid(2432111, "3118998383619905222", 1442500688)
+addappid(2432112, 1, "bb500034e6f19b0aa80b8f91115f598022baae94f1c27092fa0d1940949fa849") --Main Windows Language Depot English WHITE ALBUM: Memories like Falling Snow
+setManifestid(2432112, "5953665294834866003", 678729632)
+addappid(2432113, 1, "fbab7cef525d7a82233e82e1f087275f7214853697d44a034e3b86d4514b8633") --Main Windows Language Depot Japanese WHITE ALBUM: Memories like Falling Snow
+setManifestid(2432113, "4486162893703204728", 691553520)
+addappid(2432114, 1, "63837fda42eb7195bf79308d0f6b869f5b3427860c7f2e8a14388508b36445fc") --Main Windows Language Depot Tchinese WHITE ALBUM: Memories like Falling Snow
+setManifestid(2432114, "3097335352710840729", 690035008)
