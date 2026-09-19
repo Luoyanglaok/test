23b3c64993af502e9c0420af97c7322edcd8c479
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 11:25:41 +0800

    add 577230.lua

--- 577230.lua
+++ 577230.lua
@@ -1,18 +1,80 @@
-addappid(577230)
-setManifestid(228981,"7613356809904826842")
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-setManifestid(229006,"1784011429307107530")
-setManifestid(229020,"5799761707845834510")
-addappid(577231,0,"9637d76aa413d336de9550b3932653ee37f71ac30d96721a9bcc5e14a87ec788")
-setManifestid(577231,"2061272678340076791")
-addappid(577232,0,"6bf9dba002c2cb8ed0fd82bc69393ca8464cffc170225416f7aee00fa0ad82a3")
-setManifestid(577232,"5885204661128241325")
-addappid(577233,0,"9bb32a976cd5c911f2cf02d5251dc28fbce00f9a991701c09532e03cd630637e")
-setManifestid(577233,"7718062996325764690")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 577230.lua
+--Gamename Three Kingdoms The Last Warlord
+addappid(577230, 1, "43ee897087b5231758d95b8983edc9fd4008038379421c6ff71a28cd01e33d96") --Mainappid Three Kingdoms The Last Warlord
+addappid(577231, 1, "9637d76aa413d336de9550b3932653ee37f71ac30d96721a9bcc5e14a87ec788") --Main Windows Depot Three Kingdoms The Last Warlord
+setManifestid(577231, "6406061093207265030", 944180912)
+addappid(577232, 1, "6bf9dba002c2cb8ed0fd82bc69393ca8464cffc170225416f7aee00fa0ad82a3") --Main Macos Depot Three Kingdoms The Last Warlord
+setManifestid(577232, "7064935290680373039", 0)
+addappid(577233, 1, "9bb32a976cd5c911f2cf02d5251dc28fbce00f9a991701c09532e03cd630637e") --Main Windows Depot Three Kingdoms The Last Warlord
+setManifestid(577233, "4207556517142600435", 1202492880)
+addappid(4539291, 1, "606b8b892eecce31b30c6195d879cd5be06d521b2441230496383d6bb874b7fa") --Main Windows Depot Three Kingdoms The Last Warlord
+setManifestid(4539291, "3587561996471715950", 197532352)
+addappid(4753561, 1, "e85ad7b63fd7ab718a0328019db31d3438367056d93a5fec682e65812bfe8228") --Main Windows Depot Three Kingdoms The Last Warlord
+setManifestid(4753561, "6708439620886889972", 124412192)
+--Dlcname Three Kingdoms: The Last Warlord - Duel Expansion
+addappid(692770, 1, "734864f43752da53d568bad96aa891198e82e6a075216df18913d3133cf15d6f") --Dlcname Three Kingdoms: The Last Warlord - Duel Expansion
+setManifestid(692770, "914501564318341998", 144)
+addappid(692771, 1, "13927b9109396b23d7538c9781babdc0f8ecfe0060d7143afe90837183fe51ba") --Dlc Windows Depot Three Kingdoms: The Last Warlord - Duel Expansion
+setManifestid(692771, "6660365100926482633", 0)
+--Dlcname 三国志汉末霸业-龙腾虎啸
+addappid(1520720) --Dlcname 三国志汉末霸业-龙腾虎啸
+addappid(1520721, 1, "37f87468014942c2b9fdec29b42bd57bb8551e2bddcc23940955e50113704d08") --Dlc Windows Depot 三国志汉末霸业-龙腾虎啸
+setManifestid(1520721, "8926982292276259060", 0)
+--Dlcname 宝物编辑器
+addappid(1863450) --Dlcname 宝物编辑器
+addappid(1863451, 1, "63d1b2554dfb211ce943a07761bccb8efcea133c619daef71f686900ff6270d1") --Dlc Windows Depot 宝物编辑器
+setManifestid(1863451, "7118239781807457815", 240)
+--Dlcname 手谈九州
+addappid(2022570) --Dlcname 手谈九州
+addappid(2022571, 1, "cd91790dd6606771d149a14b0065c513f28bc8d71c0e1dcbd25de3dbf2e52322") --Dlc Windows Depot 手谈九州
+setManifestid(2022571, "8262161245657721530", 240)
+--Dlcname Three Kingdoms The Last Warlord-Art Upgrade Pack
+addappid(2350160) --Dlcname Three Kingdoms The Last Warlord-Art Upgrade Pack
+addappid(2350161, 1, "293246b9dea1db132b7db393d701e46d257e9872cd360125457e02758e3e0978") --Dlc Windows Depot Three Kingdoms The Last Warlord-Art Upgrade Pack
+setManifestid(2350161, "9152618036144355437", 618848464)
+--Dlcname 三国志汉末霸业-群英荟
+addappid(2718740) --Dlcname 三国志汉末霸业-群英荟
+addappid(2718741, 1, "b1a2c27ca53df6f77e224bfdfcf211fd874afea094b01555a6b369ee10c7913d") --Dlc Windows Depot 三国志汉末霸业-群英荟
+setManifestid(2718741, "3321543069171303929", 92064)
+--Dlcname 【苍天之志】番外篇之魏传
+addappid(4330990) --Dlcname 【苍天之志】番外篇之魏传
+addappid(4330991, 1, "311e2a878078d4440a9e62c73dbe628a05e095ad5430533c1e76bfb4544c6383") --Dlc Windows Depot 【苍天之志】番外篇之魏传
+setManifestid(4330991, "1974809077162295842", 113627632)
+--Dlcname 【苍天之志】番外篇之吴传
+addappid(4331000) --Dlcname 【苍天之志】番外篇之吴传
+addappid(4331001, 1, "c2eaaf88b191ba017b869d7aa68b3fc3e0c1a6173772c521fad6c763713f9ae8") --Dlc Windows Depot 【苍天之志】番外篇之吴传
+setManifestid(4331001, "7225235166320053274", 113625504)
+--Dlcname 三国志汉末霸业-山河归一
+addappid(4539290) --Dlcname 三国志汉末霸业-山河归一
+--Dlcname 三国志汉末霸业：华夏九州-水浒英雄传
+addappid(4753560) --Dlcname 三国志汉末霸业：华夏九州-水浒英雄传
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(229020, 1, "efca2304d92ac2bb7ebca174e6e5015fb0daf45d7db8ecfc1db6eaccdc7b27d9") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229020, "5799761707845834510", 591984)
+addappid(2350150) --Dlcname 三国志汉末霸业-【苍天之志】番外篇
+addappid(3171550) --Dlcname 三国志汉末霸业-出类拔萃
+addtoken(3171550, "16905040412407237133")
+addappid(3575830) --Dlcname 三国志汉末霸业-英雄的黎明
+--Missing Dlcs
+--Dlcname Three Kingdoms: The Last Warlord - OST
