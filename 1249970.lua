454e4da90435d4129ac2c3aba7a86b85da4a1561
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:05:54 +0800

    add 1249970.lua

--- 1249970.lua
+++ 1249970.lua
@@ -1,6 +1,17 @@
-addappid(1249970)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1249971,0,"ef894cda9509eea638e332e510f8d089c04994ef8ffafdfd3cc721414b0b4176")
-setManifestid(1249971,"2984559789861827962")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1249970.lua
+--Gamename Test Drive Unlimited Solar Crown
+addappid(1249970, 1, "8f53c68c87b8d2ba4751b028da5e9ccd6057e599aebbac6b8bb30cd148002185") --Mainappid Test Drive Unlimited Solar Crown
+addappid(1249971, 1, "ef894cda9509eea638e332e510f8d089c04994ef8ffafdfd3cc721414b0b4176") --Main Windows Depot Test Drive Unlimited Solar Crown
+setManifestid(1249971, "1948889394914525941", 88397908496)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2515670) --Dlcname Test Drive Unlimited Solar Crown - Ford GT 2006
+addappid(2515680) --Dlcname Test Drive Unlimited Solar Crown - Silver Streets Pack
+addappid(2515690) --Dlcname Test Drive Unlimited Solar Crown - Silver Sharps Pack
+addappid(2515700) --Dlcname Test Drive Unlimited Solar Crown - Solar Content Pack
