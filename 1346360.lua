d44a33a8b9304ea2a2ffcd954cfb7dea3e791b96
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 16:55:53 +0800

    add 1346360.lua

---
+++ 1346360.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1346360.lua
+--Gamename Old Skies
+addappid(1346360) --Mainappid Old Skies
+addappid(1346361, 1, "55dab47b8a95f7c4d5559003459e91432708c60cf269461bf68280da24ffb5e3") --Main Windows Depot Old Skies
+setManifestid(1346361, "6985144641039281479", 4006392896)
+addappid(1346362, 1, "4708d922994bbb7d10894972c9fd5b9211786a5ce082678e0dc78df61aba91eb") --Main Macos Depot Old Skies
+setManifestid(1346362, "878967729156873077", 4011613952)
+addappid(1346363, 1, "ab313b171d749e00f7d493dfcadb38532dbba1a8d8cfd18a211e8a4eeece5842") --Main Linux Depot Old Skies
+setManifestid(1346363, "7609212324657336424", 4024763904)
+addappid(1346364, 1, "0c7441e4357fd0b2f4847597e9898f6861826e13fd9d66dc40175a4988f912d8") --Main Linux Depot Old Skies
+setManifestid(1346364, "7501824817707679458", 4022036240)
+--Missing Dlcs
+--Dlcname Old Skies Official Soundtrack
