8ece429631551a1cbb534c5d0a5d9e4ecf6242d0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:17:56 +0800

    add 702050.lua

--- 702050.lua
+++ 702050.lua
@@ -1,6 +1,11 @@
-addappid(702050)
-setManifestid(228990,"1829726630299308803")
-addappid(702051,0,"50e945a3ff5eb1c7d9767adb0480a08691bcd99c89d4f0c1a6a242eaba7830b3")
-setManifestid(702051,"9197894380468458936")
-addappid(702052,0,"0889c34892f364e5da0379ea0cfa35d218ebb00eea0b675c282eb66490459886")
-setManifestid(702052,"7320958786654552082")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 702050.lua
+--Gamename The Song of Saya
+addappid(702050, 1, "c76c8166a60812c0a3b4a2174e828ba8f52d85ecc6327b8d296f637d0ea7dca3") --Mainappid The Song of Saya
+addappid(702051, 1, "50e945a3ff5eb1c7d9767adb0480a08691bcd99c89d4f0c1a6a242eaba7830b3") --Main Windows Depot The Song of Saya
+setManifestid(702051, "9197894380468458936", 628148560)
+addappid(702052, 1, "0889c34892f364e5da0379ea0cfa35d218ebb00eea0b675c282eb66490459886") --Main Windows Language Depot Schinese The Song of Saya
+setManifestid(702052, "4883549314492224239", 620847760)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
