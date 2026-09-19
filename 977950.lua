40c72dcd97aad4458131b1a7f34041cca06c5718
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 16:48:09 +0800

    add 977950.lua

--- 977950.lua
+++ 977950.lua
@@ -1,9 +1,34 @@
-addappid(977950)
-addappid(977951,0,"5e954d77ef1866e557c00b742437d480a7c03c2303b6a96268fbd1a55014839e")
-setManifestid(977951,"6859266271102102820")
-addappid(977953,0,"cff06fc131de80d2131b228a0ec443303aa7b4fa8bf9b71b02ce9faa59f005c2")
-setManifestid(977953,"4477163158395276796")
-addappid(977952,0,"7161eb622ca30debeb61bc87ecc80dd9a4cf78b7ef999fbc676710acd080565e")
-setManifestid(977952,"1673557748231453356")
-addappid(977954,0,"85480edfffe7849f3714592a16a8fb97e94bb7fdc0773a9d9db0f80a40890bce")
-setManifestid(977954,"376399736570655445")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 977950.lua
+--Gamename A Dance of Fire and Ice
+addappid(977950, 1, "900c284c78f11289033d308ddc33fcb65659b670bd2f3684b07c15180e02e971") --Mainappid A Dance of Fire and Ice
+addappid(977951, 1, "5e954d77ef1866e557c00b742437d480a7c03c2303b6a96268fbd1a55014839e") --Main Windows Depot A Dance of Fire and Ice
+setManifestid(977951, "3822952625857119142", 592592560)
+addappid(977952, 1, "7161eb622ca30debeb61bc87ecc80dd9a4cf78b7ef999fbc676710acd080565e") --Main Macos Depot A Dance of Fire and Ice
+setManifestid(977952, "3606983167934430243", 651422096)
+addappid(977953, 1, "cff06fc131de80d2131b228a0ec443303aa7b4fa8bf9b71b02ce9faa59f005c2") --Main Windows Depot A Dance of Fire and Ice
+setManifestid(977953, "4950993065500607066", 690752256)
+addappid(977954, 1, "85480edfffe7849f3714592a16a8fb97e94bb7fdc0773a9d9db0f80a40890bce") --Main Linux Depot A Dance of Fire and Ice
+setManifestid(977954, "5224208323207550260", 693175008)
+addappid(977955, 1, "7f9c097352c14ed541bff3441a278188a38cc62869383f0ad32f43fe7af4325a") --Main Windows Depot A Dance of Fire and Ice
+setManifestid(977955, "3677132390289850251", 54324368)
+addappid(1977571, 1, "a4ec8a574d6949e27a066e3ef02d87d50cc32e090944d977595132716b46ed40") --Main Windows Depot A Dance of Fire and Ice
+setManifestid(1977571, "430765350164194066", 127433024)
+addappid(1977572, 1, "b0d89adeb9338cee8859eb5596cfad872bdf7a96c8e58f53cb8f214857e5c481") --Main Windows Depot A Dance of Fire and Ice
+setManifestid(1977572, "138381743111006085", 127527136)
+addappid(1977573, 1, "51140ab9601562c890952d5b7022bc09726be1e7fc6d8dbbfc09f5318110f6f7") --Main Macos Depot A Dance of Fire and Ice
+setManifestid(1977573, "9098275973103259955", 127421056)
+addappid(1977574, 1, "b74fb5a309784a778cb5d14cd6b64ee94da0fd481eef93e9b50fc183ca8b3b1f") --Main Linux Depot A Dance of Fire and Ice
+setManifestid(1977574, "2776041074347108297", 127459008)
+addappid(2016421, 1, "70cfd3ffc2b8a4870e164b0166870983973e8eda35d2b84a49d718fadb4bf982") --Main Windows Depot A Dance of Fire and Ice
+setManifestid(2016421, "9126531929037548092", 69644544)
+addappid(2715611, 1, "42f4bbb800d0eb8cd934988ca3178cbf4bc744081d6315553bcc663ead6afb93") --Main Windows Depot A Dance of Fire and Ice
+setManifestid(2715611, "2223140659405808113", 152967056)
+addappid(2715612, 1, "98cf950ac365cf9c0ef0dc7ea2f17257157f65789f3bbd730e30446d3717772a") --Main Windows Depot A Dance of Fire and Ice
+setManifestid(2715612, "992032378083081189", 549939200)
+--Dlcname A Dance of Fire and Ice OST
+addappid(1027880, 1, "43f72b86d08961d480f11173d52519e09d439789a47469235c3bea5f342d6240") --Dlcname A Dance of Fire and Ice OST
+--Dlcname A Dance of Fire and Ice - Neo Cosmos
+addappid(1977570) --Dlcname A Dance of Fire and Ice - Neo Cosmos
+--Dlcname A Dance of Fire and Ice - Neo Cosmos OST
+addappid(2016420) --Dlcname A Dance of Fire and Ice - Neo Cosmos OST
