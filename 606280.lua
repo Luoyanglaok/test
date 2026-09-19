9d3309fbcf64a76121f15c8a78a40b70aea28fc2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 14:56:05 +0800

    add 606280.lua

--- 606280.lua
+++ 606280.lua
@@ -1,7 +1,19 @@
-addappid(606280)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(606281,0,"1006fea4a0c69e799b0e76c8ac156521a2e813579a945a71ff8810b9880d3d0b")
-setManifestid(606281,"2263395985912720228")
-addappid(889900,0,"b7145397071d1789e33b19bbb06dd94f643f3a2fb1b2aa62be3925e2c49e2828")
-setManifestid(889900,"3840230030307644742")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 606280.lua
+--Gamename Darksiders III
+addappid(606280, 1, "8d7796a4ff91e32d76436b2db523e0dab1cec7b91f36effea02d73d37369de7b") --Mainappid Darksiders III
+addappid(606281, 1, "1006fea4a0c69e799b0e76c8ac156521a2e813579a945a71ff8810b9880d3d0b") --Main Windows Depot Darksiders III
+setManifestid(606281, "2263395985912720228", 22793129056)
+--Dlcname Darksiders III - Digital Extras
+addtoken(889900, "12223590723704831934")
+addappid(889900, 1, "b7145397071d1789e33b19bbb06dd94f643f3a2fb1b2aa62be3925e2c49e2828") --Dlcname Darksiders III - Digital Extras
+setManifestid(889900, "3840230030307644742", 2056598368)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(889901) --Dlcname AppID 889901
+addtoken(889901, "1054069495649484218")
+addappid(889902) --Dlcname Darksiders III - The Crucible
+addappid(889903) --Dlcname Darksiders III - Keepers of the Void
