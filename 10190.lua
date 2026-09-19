d3d99fa99e7965b8eeea88f5a809a4365e357774
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 14:46:04 +0800

    add 10190.lua

--- 10190.lua
+++ 10190.lua
@@ -1,28 +1,56 @@
-addappid(10190)
-addappid(10196,0,"0229b7d5cea8696ae4be6f67a42aff53404f7cce39063ebff05da8dc7d10a895")
-setManifestid(10196,"1027296475802215551")
-addappid(10195,0,"ccbd9c0e70f88c30ec8776f3838f2cc1e3529f9660601292a76df918971424bf")
-setManifestid(10195,"4404426848645331640")
-addappid(10184,0,"57b7539e22d6fdf82a9be1382cab21501e8c34f9995efd2aceabf18d23a5f53a")
-setManifestid(10184,"908433931108303658")
-setManifestid(10182,"4063260329988186194")
-setManifestid(10186,"6253063533272841787")
-setManifestid(10183,"8707803059832053468")
-setManifestid(10185,"6960622809953595829")
-setManifestid(10187,"5447045943202156387")
-setManifestid(10188,"6075992503597705494")
-setManifestid(10189,"1493308596121358900")
-setManifestid(10191,"5365446663453490899")
-setManifestid(10193,"5456705176131695752")
-addappid(278300,0,"e61f0098429dfaa4cd4a91e9c247a48e52ff9e5d261700912111949e490cf9a0")
-setManifestid(278300,"385996767344838275")
-addappid(278301,0,"f31c5cb84b9f6c8d60b3963c885a85e68ac34923e3fb2320980ce3bd85a03fd9")
-setManifestid(278301,"7480450085793114503")
-addappid(278302,0,"21523d64d443e41a7e18754347e53a481b0bcee9fd78d7fb505d460f0ad74f78")
-setManifestid(278302,"8173601917745645979")
-setManifestid(278280,"3619648325982091147")
-setManifestid(278282,"3664267140507507825")
-setManifestid(278283,"6313141199869918175")
-setManifestid(278284,"4009187078934820154")
-setManifestid(278285,"4684911959805635804")
-setManifestid(278286,"4006477255675382643")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 10190.lua
+--Gamename Call of Duty: Modern Warfare 2 (2009) - Multiplayer
+addappid(10190, 1, "9d94d9ff7e26486bb7fa34072cc3dbb16aecd96059034ed31e7eabfc50a1a20a") --Mainappid Call of Duty: Modern Warfare 2 (2009) - Multiplayer
+addappid(10184, 1, "57b7539e22d6fdf82a9be1382cab21501e8c34f9995efd2aceabf18d23a5f53a") --Main Windows Depot Call of Duty: Modern Warfare 2 (2009) - Multiplayer
+setManifestid(10184, "8851756244141656055", 2597280)
+addappid(278300, 1, "e61f0098429dfaa4cd4a91e9c247a48e52ff9e5d261700912111949e490cf9a0") --Main Macos Depot Call of Duty: Modern Warfare 2 (2009) - Multiplayer
+setManifestid(278300, "385996767344838275", 7946528)
+addappid(278301, 1, "f31c5cb84b9f6c8d60b3963c885a85e68ac34923e3fb2320980ce3bd85a03fd9") --Main Macos Depot Call of Duty: Modern Warfare 2 (2009) - Multiplayer
+setManifestid(278301, "7480450085793114503", 351494864)
+addappid(278302, 1, "21523d64d443e41a7e18754347e53a481b0bcee9fd78d7fb505d460f0ad74f78") --Main Macos Depot Call of Duty: Modern Warfare 2 (2009) - Multiplayer
+setManifestid(278302, "8173601917745645979", 371366336)
+--Dlcname Call of Duty: Modern Warfare 2 Stimulus Package
+addappid(10195, 1, "ccbd9c0e70f88c30ec8776f3838f2cc1e3529f9660601292a76df918971424bf") --Dlcname Call of Duty: Modern Warfare 2 Stimulus Package
+setManifestid(10195, "6893463534231552504", 353519760)
+--Dlcname Call of Duty: Modern Warfare 2 - Resurgence Pack
+addappid(10196, 1, "0229b7d5cea8696ae4be6f67a42aff53404f7cce39063ebff05da8dc7d10a895") --Dlcname Call of Duty: Modern Warfare 2 - Resurgence Pack
+setManifestid(10196, "1251041877935507292", 373346256)
+--Share Depots
+addappid(10182, 1, "98e730672197195300951a3d2f198c16ddfd4cc6d6f882d51d4451e07e1f56d4") --Share Windows Depot Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10182, "4763212243329962648", 4326016016)
+addappid(10183, 1, "9d6d7892bb063f0994946afbf4e2dbe2376c4e38fd5499197b04d9739e755eab") --Share Windows Language Depot English Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10183, "3746347210828885454", 7724567040)
+addappid(10185, 1, "bfb1650ac051bc56aa5070292ff58e1d7c51cca2490d854920f33304768b9ebc") --Share Windows Language Depot French Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10185, "2916635933940005699", 7685480368)
+addappid(10186, 1, "34794e682272e97d0eaf302225f29e5768eaf13597588eb377b38be229197376") --Share Windows Depot Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10186, "6155459432830166023", 7750445296)
+addappid(10187, 1, "3f43d1151fcabffd59378e8126ea4e3b3822cf052e97c4bded63f9d2dc9c9ba1") --Share Windows Language Depot Italian Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10187, "9178342988569338205", 7830647648)
+addappid(10188, 1, "dec6cdf03b13ce8882acaa2326a5ad546c218e36fe6ea031ff62349b007b74da") --Share Windows Language Depot Spanish Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10188, "2436709362133862153", 7705141552)
+addappid(10189, 1, "7f06a5d7663f4910db0d0c5ad3bbce302b70c1ccb4acf10245a425d1fab3efed") --Share Windows Language Depot Russian Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10189, "928497548174604501", 7801892128)
+addappid(10191, 1, "4b75b8d33e2d902e031e61b3666f029ab98ac47669b527b0a10237de18f4973d") --Share Windows Language Depot Polish Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10191, "845536259906810517", 7580177280)
+addappid(10192, 1, "4921666a58dbd848cabb65af356be86b94ade0dd89b599c9a5d9e14e221e2e64") --Share Windows Language Depot Japanese Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10192, "2226474644407549761", 7673513920)
+addappid(10193, 1, "4fef095309f586b62847fab74e063f606e4e4e645b10b2519deb61a70f595cc4") --Share Windows Language Depot German Call of Duty: Modern Warfare 2 (2009)
+setManifestid(10193, "8794395590069375614", 7750395296)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(278280, 1, "c2388d5b10307263ca1f6dff3abc196a1cf9714164b643f9de97cf9b275ea2a5") --Share Macos Depot Call of Duty: Modern Warfare 2 (2009)
+setManifestid(278280, "3619648325982091147", 4310418656)
+addappid(278282, 1, "4cb28ace16dd3c200f693690509511b2ac50678a1085b25551e09b259331408f") --Share Macos Language Depot English Call of Duty: Modern Warfare 2 (2009)
+setManifestid(278282, "3664267140507507825", 7682603680)
+addappid(278283, 1, "8f071c3dcae93458e00a7a4f1df7a122d95fb434a0a40fc5e3d7778a2818582a") --Share Macos Language Depot French Call of Duty: Modern Warfare 2 (2009)
+setManifestid(278283, "6313141199869918175", 7667516992)
+addappid(278284, 1, "5d57c67bb0c65020f52183653a46fee36f75565706cf5211a90a1bdf05576eda") --Share Macos Language Depot Italian Call of Duty: Modern Warfare 2 (2009)
+setManifestid(278284, "4009187078934820154", 7813348160)
+addappid(278285, 1, "88abe833e24b8116c84a5b0f7b85feafc4f1b9605c80732208b31a3fe856cfdf") --Share Macos Language Depot Spanish Call of Duty: Modern Warfare 2 (2009)
+setManifestid(278285, "4684911959805635804", 7687154768)
+addappid(278286, 1, "7a1dc16d0fb62b22ffec23b424d1244bce11cc56c717dd975c75204f0ef25ff1") --Share Macos Language Depot German Call of Duty: Modern Warfare 2 (2009)
+setManifestid(278286, "4006477255675382643", 7733690592)
+addappid(10197) --Dlcname Call of Duty: Modern Warfare 2 - Resurgence Pack
