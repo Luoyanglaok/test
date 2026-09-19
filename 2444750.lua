a3480decdba0db7f08ee83cd7530b10eb5852815
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 17:00:53 +0800

    add 2444750.lua

--- 2444750.lua
+++ 2444750.lua
@@ -3,9 +3,12 @@
 --Gamename Shape of Dreams
 addappid(2444750, 1, "7dc7c8c175eae2f6798f4e892251914d68ba702f8066c09c71e3e9342d95943c") --Mainappid Shape of Dreams
 addappid(2444751, 1, "ef362189306209d0c11c3b509c34fd001aed76b0fc0ea258001430f6793519a5") --Main Windows Depot Shape of Dreams
-setManifestid(2444751, "306414387089416804", 6750942752)
+setManifestid(2444751, "407160048126861091", 7377723632)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4890080) --Dlcname Shape of Dreams - Royal Guard Lacerta
+--Missing Dlcs
+--Dlcname Shape of Dreams - Original Soundtrack
