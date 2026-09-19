9a54a5d44751343ffffa004a2230ee46b78e6f71
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 16:50:45 +0800

    add 1466390.lua

--- 1466390.lua
+++ 1466390.lua
@@ -1,3 +1,12 @@
-addappid(1466390)
-addappid(1466391,0,"0841982e471c67dfb711963a190794bc72094844ed98a58ab3e97158b4c495ab")
-setManifestid(1466391,"5472365021419225207")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1466390.lua
+--Gamename Kathy Rain 2: Soothsayer
+addappid(1466390) --Mainappid Kathy Rain 2: Soothsayer
+addappid(1466391, 1, "0841982e471c67dfb711963a190794bc72094844ed98a58ab3e97158b4c495ab") --Main Windows Depot Kathy Rain 2: Soothsayer
+setManifestid(1466391, "1377259378741895667", 1179146752)
+--Dlcname Kathy Rain 2: Soothsayer Original Soundtrack
+addappid(3723630) --Dlcname Kathy Rain 2: Soothsayer Original Soundtrack
+addappid(3723631, 1, "1a13ed83c99ffe7db65a9065c1e59ad2a7aa274f79d81bc73528ff156befe20d") --Dlc Windows Depot Kathy Rain 2: Soothsayer Original Soundtrack
+setManifestid(3723631, "6516275637829626192", 315355376)
+addappid(3723632, 1, "00b6eb1b1c93d8bd7655abdd71e0945b4f44f544f9ca01604291f46482f1d7e0") --Dlc Windows Depot Kathy Rain 2: Soothsayer Original Soundtrack
+setManifestid(3723632, "7078692112880389554", 637374000)
