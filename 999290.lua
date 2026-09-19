e211fdcbf14539fa120b812c660b7d38d54fe943
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 11:24:28 +0800

    add 999290.lua

--- 999290.lua
+++ 999290.lua
@@ -1,3 +1,6 @@
-addappid(999290)
-addappid(999291,0,"be906f1dcacd1af50519b731f558350a38f300e96e90d4500dac58724b5a40c8")
-setManifestid(999291,"3992826039436422520")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 999290.lua
+--Gamename Nine Hentai Babes
+addappid(999290) --Mainappid Nine Hentai Babes
+addappid(999291, 1, "be906f1dcacd1af50519b731f558350a38f300e96e90d4500dac58724b5a40c8") --Main Windows Language Depot English Nine Hentai Babes
+setManifestid(999291, "3992826039436422520", 113922688)
