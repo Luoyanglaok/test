5af166d7969b78ec53aff6bffdae37b93d050fc7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:02:12 +0800

    add 2698940.lua

--- 2698940.lua
+++ 2698940.lua
@@ -1,5 +1,52 @@
-addappid(2698940)
-addappid(2698941,0,"d883429e0c229f4e0634265ea029ccf4200bf0a6d171047809fab73ae807ba8c")
-setManifestid(2698941,"6931954782824843628")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2698940.lua
+--Gamename The Crew Motorfest
+addappid(2698940, 1, "0dc63a9c4ead3fdb3ad467c3c1dbbbac237f51ae9d1532ce1ceac086107d4da8") --Mainappid The Crew Motorfest
+addappid(2698941, 1, "d883429e0c229f4e0634265ea029ccf4200bf0a6d171047809fab73ae807ba8c") --Main Windows Depot The Crew Motorfest
+setManifestid(2698941, "3889162474195455966", 75701408000)
+--Dlcname The Crew Motorfest | Year 1 Pass
+addappid(2805000, 1, "902e3d9b9600ed8100fd7ff4e7b9cdd31c115b943e1ff0bb430eb4262ff39b91") --Dlcname The Crew Motorfest | Year 1 Pass
+--Share Depots
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(2716050) --Dlcname The Crew Motorfest - Ubisoft Activation
+addappid(2805010) --Dlcname The Crew Motorfest - Year 1 Pass - Ubisoft Activation
+addappid(2849030) --Dlcname The Crew Motorfest - Welcome Pack
+addappid(2849040) --Dlcname The Crew Motorfest - Welcome Pack - Ubisoft Activation
+addappid(2865990) --Dlcname The Crew Motorfest - Gold Edition Ubisoft Activation
+addappid(2898630) --Dlcname The Crew Motorfest - Deluxe Edition Ubisoft Activation
+addappid(2898640) --Dlcname The Crew Motorfest - Ultimate Edition Ubisoft Activation
+addappid(2934260) --Dlcname The Crew Motorfest - Demo - Ubisoft Activation
+addappid(3251420) --Dlcname The Crew Motorfest - Year 2 Pass
+addappid(3251430) --Dlcname The Crew Motorfest | Year 2 Pass - Ubisoft Activation
+addappid(3251440) --Dlcname The Crew Motorfest Gold Edition Ubisoft Activation
+addappid(3251450) --Dlcname The Crew Motorfest Ultimate Edition Ubisoft Activation
+addappid(3917350) --Dlcname The Crew Motorfest Ultimate Edition - Year 3 Ubisoft Activation
+addappid(3917380) --Dlcname The Crew Motorfest | Year 3 Pass
+addappid(3917390) --Dlcname The Crew Motorfest | Year 3 Pass Ubisoft Activation
+addappid(3917400) --Dlcname The Crew Motorfest | Chase Squad Pack
+addappid(3917420) --Dlcname The Crew Motorfest | Chase Squad Pack Ubisoft Activation
+addappid(3917430) --Dlcname The Crew Motorfest | BMW Double Car Pack
+addappid(3917440) --Dlcname The Crew Motorfest | BMW Double Car Pack Ubisoft Activation
+addappid(3917450) --Dlcname The Crew Motorfest | Alfa Romeo Double Car Pack
+addappid(3917460) --Dlcname The Crew Motorfest | Alfa Romeo Double Car Pack Ubisoft Activation
+addappid(3917470) --Dlcname The Crew Motorfest | Ford Triple Car Pack
+addappid(3917490) --Dlcname The Crew Motorfest | Ford Triple Car Pack Ubisoft Activation
+addappid(3917500) --Dlcname The Crew Motorfest | Porsche Triple Car Pack
+addappid(3917510) --Dlcname The Crew Motorfest | Porsche Triple Car Pack Ubisoft Activation
+addappid(3917520) --Dlcname The Crew Motorfest | JDM Car Pack
+addappid(3917530) --Dlcname The Crew Motorfest | JDM Car Pack Ubisoft Activation
+addappid(4160500) --Dlcname The Crew Motorfest RC Frenzy Pack
+addappid(4160510) --Dlcname The Crew Motorfest RC Frenzy Pack Ubisoft Activation
+addappid(4160650) --Dlcname The Crew Motorfest | Audi Double Car Pack
+addappid(4160660) --Dlcname The Crew Motorfest Audi Double Car Pack Ubisoft Activation
+addappid(4160670) --Dlcname The Crew Motorfest Triple Bike Pack
+addappid(4160680) --Dlcname The Crew Motorfest Triple Bike Pack Ubisoft Activation
+addappid(4160690) --Dlcname The Crew Motorfest | JDM Custom Car Pack
+addappid(4160700) --Dlcname The Crew Motorfest JDM Custom Car Pack Ubisoft Activation
+addappid(4670550) --Dlcname The Crew Motorfest - Drift Pack
+addappid(4670560) --Dlcname The Crew Motorfest - Drift Pack Ubisoft Activation
+addappid(4670570) --Dlcname The Crew Motorfest - Dodge Pack
+addappid(4670580) --Dlcname The Crew Motorfest - Dodge Pack Ubisoft Activation
+addappid(4670590) --Dlcname The Crew Motorfest - Chevrolet Pack
+addappid(4670600) --Dlcname The Crew Motorfest - Chevrolet Pack Ubisoft Activation
