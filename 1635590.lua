a49b0468d63f64c6cb33c07367d99e9a6cf73ad2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:38:53 +0800

    add 1635590.lua

--- 1635590.lua
+++ 1635590.lua
@@ -1,3 +1,7 @@
-addappid(1635590)
-addappid(1635591,0,"ed4326a44d4a8aa99c822d8ff8e2adb9ef9befbb781b0d39172b1757d69cb66b")
-setManifestid(1635591,"7971647519970850138")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1635590.lua
+--Gamename My Friend Peppa Pig
+addappid(1635590) --Mainappid My Friend Peppa Pig
+addappid(1635591, 1, "ed4326a44d4a8aa99c822d8ff8e2adb9ef9befbb781b0d39172b1757d69cb66b") --Main Windows Depot My Friend Peppa Pig
+setManifestid(1635591, "7971647519970850138", 1238960624)
+addappid(1840890) --Dlcname My Friend Peppa Pig: Pirate Adventures
