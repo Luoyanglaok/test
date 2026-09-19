88dd8dc6c86969d89aacff5d20c051e38e02d3db
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 15:07:05 +0800

    add 2138720.lua

--- 2138720.lua
+++ 2138720.lua
@@ -1,3 +1,19 @@
-addappid(2138720)
-addappid(2138721,0,"c1a2b9eea7346395b175b2d8458f566dcabe83b99822111d3446a5ce377bdc52")
-setManifestid(2138721,"5582227186789954162")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2138720.lua
+--Gamename REMATCH
+addappid(2138720, 1, "cde859ea82c9ffebcff7c7b8167aada642ab1b7accc7ee233a8e437fe7778d29") --Mainappid REMATCH
+addappid(2138721, 1, "c1a2b9eea7346395b175b2d8458f566dcabe83b99822111d3446a5ce377bdc52") --Main Windows Depot REMATCH
+setManifestid(2138721, "5336889137450661984", 10790604000)
+addappid(3469160) --Dlcname REMATCH: Pro Edition Cosmetic Pack
+addtoken(3469160, "16879146445141116971")
+addappid(3469170) --Dlcname REMATCH: Elite Edition Cosmetic Pack
+addappid(3469180) --Dlcname Rematch: Captain Pass Upgrade Ticket
+addtoken(3469180, "2605518638704997599")
+addappid(3525370) --Dlcname REMATCH: Pre-Purchase Pack
+addtoken(3525370, "1094641484719082171")
+addappid(3743380) --Dlcname REMATCH - Pro Edition Upgrade Pack
+addappid(3743390) --Dlcname REMATCH - Elite Edition Upgrade Pack
+addappid(4002110) --Dlcname REMATCH - 4500 Quants
+addappid(4002120) --Dlcname REMATCH - 1100 Quants
+addappid(4031620) --Dlcname Rematch - 2400 Quants
+addappid(4768770) --Dlcname REMATCH - NV Infrared Bundle
