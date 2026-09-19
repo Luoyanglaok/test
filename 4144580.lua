acef8d2401c1f7b932ee6efe8ab74cbe194a3e86
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 11:48:38 +0800

    add 4144580.lua

---
+++ 4144580.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4144580.lua
+--Gamename Christian Mother
+addappid(4144580, 1, "40e93e0f52365eb5ccba1493208d7c18c648be97a2ee343fc47036dccb4d64f9") --Mainappid Christian Mother
+addappid(4144581, 1, "95b3e0e70197fcebdaee8bdebcb8d4ecf5f186ebad0386cd9b48e5bce13e91ad") --Main Windows Depot Christian Mother
+setManifestid(4144581, "7806923744263973714", 842811472)
