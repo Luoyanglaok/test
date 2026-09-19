3a832ae13c3eb5a4c7941525365c656273d06e21
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 09:58:41 +0800

    add 2495450.lua

--- 2495450.lua
+++ 2495450.lua
@@ -1,8 +1,11 @@
-addappid(2495450)
-addtoken(2495450,"8113372692598700339")
-addappid(2495451,0,"16af18f5e3fd7f5ad735d7b0778a3034bd74e40f1db8c0512a63361553a217f8")
-setManifestid(2495451,"6068783921631859912")
-addappid(2495452,0,"8cff64f0eaed597ed5e65daf1192bddb7b7f9a921c2175100e5176d34066e8b9")
-setManifestid(2495452,"2427872953181293715")
-addappid(2495453,0,"a9a51a0ddd91f9d9b3fa601ee5fff51a20eb1862d5cd45033dc40878bc305b0b")
-setManifestid(2495453,"4937674085925708867")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2495450.lua
+--Gamename Winter Memories
+addappid(2495450, 1, "39746f016aa27eb5b7fe7791c09706fa2e25661d2c30eacb5cf398ec83c547d2") --Mainappid Winter Memories
+addtoken(2495450, "8113372692598700339")
+addappid(2495451, 1, "16af18f5e3fd7f5ad735d7b0778a3034bd74e40f1db8c0512a63361553a217f8") --Main Windows Language Depot English Winter Memories
+setManifestid(2495451, "6068783921631859912", 926192768)
+addappid(2495452, 1, "8cff64f0eaed597ed5e65daf1192bddb7b7f9a921c2175100e5176d34066e8b9") --Main Windows Language Depot Schinese Winter Memories
+setManifestid(2495452, "2427872953181293715", 941144992)
+addappid(2495453, 1, "a9a51a0ddd91f9d9b3fa601ee5fff51a20eb1862d5cd45033dc40878bc305b0b") --Main Windows Language Depot Japanese Winter Memories
+setManifestid(2495453, "4937674085925708867", 932515184)
