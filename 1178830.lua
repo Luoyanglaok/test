9b7374eb3428609e598f3f2088c6ac42bfb68f81
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 20:48:02 +0800

    add 1178830.lua

--- 1178830.lua
+++ 1178830.lua
@@ -1,5 +1,44 @@
-addappid(1178830)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1554699,0,"bf04ebda1d1572665a129889cd9333e1f272a3fb37c2eb8cc58fe6dab1a08f7b")
-setManifestid(1554699,"1478305740267948183")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1178830.lua
+--Gamename Bright Memory: Infinite
+addappid(1178830, 1, "f065b725994bc8a5e5d08d1949d7b62e2d34de603a5b1bc736a961650b03b06f") --Mainappid Bright Memory: Infinite
+addappid(1554699, 1, "bf04ebda1d1572665a129889cd9333e1f272a3fb37c2eb8cc58fe6dab1a08f7b") --Main Windows Depot Bright Memory: Infinite
+setManifestid(1554699, "1478305740267948183", 10535322416)
+--Dlcname Bright Memory: Infinite Skinny Jeans DLC
+addappid(1554690, 1, "bd354b82558b7b8edf774134a8f91e4b10494edb006a22d0be0c44e4b0c2c122") --Dlcname Bright Memory: Infinite Skinny Jeans DLC
+setManifestid(1554690, "8882247450564853079", 48099152)
+--Dlcname Bright Memory: Infinite Energetic DLC
+addappid(1780970, 1, "2bb1dcda67fd138471381a098a22cf43c0d351f242417fc86af215dd284b9c69") --Dlcname Bright Memory: Infinite Energetic DLC
+setManifestid(1780970, "6049977753286303269", 47306256)
+--Dlcname Bright Memory: Infinite Youthful Days DLC
+addappid(1781000, 1, "08a1542d2b727f6763c23f90748b2bc0cb2ec6081f150c23fdf65692a3777a2b") --Dlcname Bright Memory: Infinite Youthful Days DLC
+setManifestid(1781000, "7174952743125346408", 13021408)
+--Dlcname Bright Memory: Infinite Black Kitten DLC
+addappid(1781070, 1, "b5a46bc2207c721cf73bde0d6190254673d9ba060c2ecd5e9c27d65bf4c38411") --Dlcname Bright Memory: Infinite Black Kitten DLC
+setManifestid(1781070, "4628102701507202926", 15153808)
+--Dlcname Bright Memory: Infinite Bikini DLC
+addappid(1781100, 1, "d071ca80abb766ecb037f9cf11e33c6b04295ae0d651050c2147d7cf090ad1a4") --Dlcname Bright Memory: Infinite Bikini DLC
+setManifestid(1781100, "7465200703516912238", 13046320)
+--Dlcname Bright Memory: Infinite Soundtrack
+addappid(1781130) --Dlcname Bright Memory: Infinite Soundtrack
+addappid(1781131, 1, "93494e81b318050e6676c8d2402b163a0bf2001670ff1758132e712fbf57dc64") --Dlc Windows Depot Bright Memory: Infinite Soundtrack
+setManifestid(1781131, "997663704874756080", 248590528)
+addappid(1781132, 1, "d18e9832e6404391f229f37168aa7354c1ea034f6606f3420f265946d04ee87c") --Dlc Windows Depot Bright Memory: Infinite Soundtrack
+setManifestid(1781132, "9163176267181919232", 31564016)
+--Dlcname Bright Memory: Infinite Cheongsam (New Year) DLC
+addappid(1870120, 1, "d77c489362c1dd7cbb33eb191fe54f5eda6b85d10b0c33d9c7cfb51203e80aeb") --Dlcname Bright Memory: Infinite Cheongsam (New Year) DLC
+setManifestid(1870120, "521217382731079", 12473392)
+--Dlcname Bright Memory: Infinite Cheongsam (Blue Flowers) DLC
+addappid(1870121, 1, "76810c4f743eb2b4867bdcab19d37502a84d09d662a47ef5b35731f4053470d7") --Dlcname Bright Memory: Infinite Cheongsam (Blue Flowers) DLC
+setManifestid(1870121, "5628063841075777893", 14932400)
+--Dlcname Bright Memory: Infinite Cyber Rabbit DLC
+addappid(2218530, 1, "6ff451301fdc04016a02e4beb1b7ef8b956f17d00e1c80d9fc40546d7c3c3e49") --Dlcname Bright Memory: Infinite Cyber Rabbit DLC
+setManifestid(2218530, "3121167526526069483", 2472768)
+--Dlcname Bright Memory: Infinite Rabbit School Uniform DLC
+addappid(2218531, 1, "860dbe71b12e40682fc1e261e624348af447c67f45727add4a2a023a5e77d943") --Dlcname Bright Memory: Infinite Rabbit School Uniform DLC
+setManifestid(2218531, "7648787424888818917", 2472544)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
