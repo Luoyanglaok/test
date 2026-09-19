1d7b06115b6bf1373107d1c99703cb04631e3108
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 18:05:35 +0800

    add 3299570.lua

--- 3299570.lua
+++ 3299570.lua
@@ -1,3 +1,6 @@
-addappid(3299570)
-addappid(3299571,0,"6028a7baa92bb65c9b2368384f218a1e980fea4e3a0c99e11a27c6237d25ee57")
-setManifestid(3299571,"2153203549625353534")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3299570.lua
+--Gamename No Mercy
+addappid(3299570, 1, "7f78974173714c33ca0c916127d54f486ed892dd064e3a37e52f58f01fc17235") --Mainappid No Mercy
+addappid(3299571, 1, "6028a7baa92bb65c9b2368384f218a1e980fea4e3a0c99e11a27c6237d25ee57") --Main Windows Depot No Mercy
+setManifestid(3299571, "2153203549625353534", 10474919696)
