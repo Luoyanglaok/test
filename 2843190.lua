59cf85c390f3238a98a47e377c482a8eab9444f0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 14:41:33 +0800

    add 2843190.lua

--- 2843190.lua
+++ 2843190.lua
@@ -1,5 +1,11 @@
-addappid(2843190)
-addappid(2843191,0,"fba4bbaff05d9c2d954a673410b3ed2468e54afa6403fe20bacfdddd06d0bb0d")
-setManifestid(2843191,"8882026668974333201")
-addappid(3765780,0,"d1cb02413ec970a4b50bd09fb20b345ea7e28ee0ced5fd0e63c2ec2a701dc178")
-setManifestid(3765780,"1717525757443201380")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2843190.lua
+--Gamename Camper Van: Make it Home
+addappid(2843190) --Mainappid Camper Van: Make it Home
+addappid(2843191, 1, "fba4bbaff05d9c2d954a673410b3ed2468e54afa6403fe20bacfdddd06d0bb0d") --Main Windows Depot Camper Van: Make it Home
+setManifestid(2843191, "7665032006728755735", 1093500704)
+--Dlcname Camper Van: Make it Home - Digital Artbook
+addappid(3765780, 1, "d1cb02413ec970a4b50bd09fb20b345ea7e28ee0ced5fd0e63c2ec2a701dc178") --Dlcname Camper Van: Make it Home - Digital Artbook
+setManifestid(3765780, "1717525757443201380", 250949744)
+--Missing Dlcs
+--Dlcname Camper Van: Make it Home Soundtrack
