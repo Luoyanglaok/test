230cc2e6c01f6b05c6e3f9307a385dee35e0e6f4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 18:28:40 +0800

    add 3624140.lua

--- 3624140.lua
+++ 3624140.lua
@@ -3,5 +3,8 @@
 --Gamename Wanderburg
 addappid(3624140) --Mainappid Wanderburg
 addappid(3624141, 1, "00b04798083e0ef0dd0c7d8516c00facdde39e69ddc8cc2751678da934a905cc") --Main Windows Depot Wanderburg
-setManifestid(3624141, "6056570286009835170", 561069696)
+setManifestid(3624141, "6730288121609839482", 561381104)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
 addappid(5026240) --Dlcname Wanderburg - Supporter Pack
