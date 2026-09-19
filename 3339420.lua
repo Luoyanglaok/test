a46e581fca71b75982d6152bf44ce211c0b2a03b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 10:31:17 +0800

    add 3339420.lua

--- 3339420.lua
+++ 3339420.lua
@@ -1,3 +1,6 @@
-addappid(3339420)
-addappid(3339421,0,"1598c7432d564705f346ee3e22d08d65692a8badf2e71f02e88ad3cd003a42f0")
-setManifestid(3339421,"5750760154674937959")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3339420.lua
+--Gamename 你的老婆
+addappid(3339420, 1, "e032e2d324312271d62477593984937d5552e7056fa0461dd7fc0030204eda87") --Mainappid 你的老婆
+addappid(3339421, 1, "1598c7432d564705f346ee3e22d08d65692a8badf2e71f02e88ad3cd003a42f0") --Main Windows Depot 你的老婆
+setManifestid(3339421, "6177461210879423138", 161379232)
