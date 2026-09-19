03ea238ee91ceef34d1ff609d9c5d8027fb07f51
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 11:47:29 +0800

    add 1328840.lua

--- 1328840.lua
+++ 1328840.lua
@@ -1,5 +1,12 @@
-addappid(1328840)
-addappid(1328841,0,"ced632dc70dc2b26d0dd6394025b43c0b54e4e580ee72da795b4ce31ced31b7b")
-setManifestid(1328841,"3575365608479551923")
-addappid(1328842,0,"08f81f199e072973d6313488cd4722619777b0d46545dfe1fa546f824abb32cf")
-setManifestid(1328842,"6104321699913627556")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1328840.lua
+--Gamename Lost in Play
+addappid(1328840) --Mainappid Lost in Play
+addappid(1328841, 1, "ced632dc70dc2b26d0dd6394025b43c0b54e4e580ee72da795b4ce31ced31b7b") --Main Windows Depot Lost in Play
+setManifestid(1328841, "3575365608479551923", 1108305552)
+addappid(1328842, 1, "08f81f199e072973d6313488cd4722619777b0d46545dfe1fa546f824abb32cf") --Main Macos Depot Lost in Play
+setManifestid(1328842, "6104321699913627556", 1113195008)
+--Dlcname Lost in Play Soundtrack
+addappid(2175430) --Dlcname Lost in Play Soundtrack
+addappid(2175431, 1, "c5071bb3d2adf290d82f2ecfac819c08c549c2f622595028c6eeedae4b0ea7d6") --Dlc Windows Depot Lost in Play Soundtrack
+setManifestid(2175431, "4888065186508248826", 130402560)
