5486e0648b0bd9af788821ce23cb2fea7b21914f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 13:17:14 +0800

    add 882790.lua

--- 882790.lua
+++ 882790.lua
@@ -1,7 +1,12 @@
-addappid(882790)
-addappid(882791,0,"d03735cc64f770b0804e29339a745892ffbd4e7c84dc6b59c1426820b618513f")
-setManifestid(882791,"6208994857282794260")
-addappid(953830,0,"c52b5680c77d2e506d41798951965179eb9b106dee68740bf97330360a7149c1")
-setManifestid(953830,"7660376104530810810")
-addappid(2227890,0,"82cae62a803ad9175f3a2c2acf858f6fd7266d9e86cf0434e3fbfd857caba31f")
-setManifestid(2227890,"13225867182200278")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 882790.lua
+--Gamename 天命奇御 Fate Seeker
+addappid(882790) --Mainappid 天命奇御 Fate Seeker
+addappid(882791, 1, "d03735cc64f770b0804e29339a745892ffbd4e7c84dc6b59c1426820b618513f") --Main Windows Depot 天命奇御 Fate Seeker
+setManifestid(882791, "6208994857282794260", 5276578624)
+--Dlcname 天命奇御 Fate Seeker - 伏虎迷蹤
+addappid(953830, 1, "c52b5680c77d2e506d41798951965179eb9b106dee68740bf97330360a7149c1") --Dlcname 天命奇御 Fate Seeker - 伏虎迷蹤
+setManifestid(953830, "7660376104530810810", 5731261424)
+--Dlcname 归途
+addappid(2227890, 1, "82cae62a803ad9175f3a2c2acf858f6fd7266d9e86cf0434e3fbfd857caba31f") --Dlcname 归途
+setManifestid(2227890, "13225867182200278", 5007693888)
