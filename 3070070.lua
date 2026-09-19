be7a9df2a91ad392806c344a2aeeb3f7870e396c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 20:56:03 +0800

    add 3070070.lua

--- 3070070.lua
+++ 3070070.lua
@@ -3,4 +3,4 @@
 --Gamename TCG Card Shop Simulator
 addappid(3070070, 1, "1d1ae53a09c22e79300f60f3a738e1b3bc390c7d9e6ced65e7f6b09a03bf9124") --Mainappid TCG Card Shop Simulator
 addappid(3070071, 1, "5c49dfd1ba36d4e81f59ad3a2cd714ec14bdb40ac41848727aca2dc9ed29c748") --Main Windows Depot TCG Card Shop Simulator
-setManifestid(3070071, "1570456405244817171", 2017829152)
+setManifestid(3070071, "4630660561061755543", 2017829520)
