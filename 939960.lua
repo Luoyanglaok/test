ccf9844c3450fadb819f3af334764b3fa0095311
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 22:23:51 +0800

    add 939960.lua

--- 939960.lua
+++ 939960.lua
@@ -1,29 +1,64 @@
-addappid(939960)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-setManifestid(229004,"5220958916987797232")
-setManifestid(229005,"7992454656023763365")
-addappid(939961,0,"88845aed0d769fc02f05c0a9c295a01ed524a32bd113350bb8e3c43d17cdbb1d")
-setManifestid(939961,"7305824767802991088")
-addappid(939962,0,"87109a483a0905b5dea2962248892c68c6f61b48a8efbb3384e2ab312c3294ea")
-setManifestid(939962,"2541041874356438434")
-addappid(939963,0,"153f8b8e69aea229d2b1571f0c840f9adeb2b6f9ccb3caaf315802bb1bbd47f5")
-setManifestid(939963,"8415750840864874031")
-addappid(939964,0,"ffc2a37f41d3002147bf3401c286df22e3fa4fec6c49fe83c2fb04ec9b46932b")
-setManifestid(939964,"8941130594844356180")
-addappid(939965,0,"e5773c92e8a81d6bd72945202a619fe9638afdbd952a4f772daedec4f1129efd")
-setManifestid(939965,"3748177826769538242")
-addappid(939966,0,"53d01ef80943cd8a431040ee772dc02fc49090fa50be4f59980f70affb1ddc8d")
-setManifestid(939966,"3612550719594251760")
-addappid(939967,0,"a216dd8d2a4539df618435a0dd99f6386435fe7d724c0fd2f845b78607ef5120")
-setManifestid(939967,"4979989929102189933")
-addappid(939968,0,"2b9606a0243678f3c4cbe6a521c08d936a6f7dc538a3911c737211483c11d106")
-setManifestid(939968,"3851229191392906390")
-addappid(939969,0,"b27d04a8ce1cc6e608d4b2924e6036a022eb6dfe8edafe6b22d01e8721ecce88")
-setManifestid(939969,"3600239354209454019")
-addappid(996970,0,"6515432309a59f7f6397d9a4a527b1fced0c1c98160d7036bc412751a5227f1c")
-setManifestid(996970,"9006765081226229638")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 939960.lua
+--Gamename Far Cry® New Dawn
+addappid(939960, 1, "8a51a80bcee0835ab845ecb5a8827b398027a7f02a78273c575bcad09a4681bf") --Mainappid Far Cry® New Dawn
+addappid(939961, 1, "88845aed0d769fc02f05c0a9c295a01ed524a32bd113350bb8e3c43d17cdbb1d") --Main Windows Depot Far Cry® New Dawn
+setManifestid(939961, "7305824767802991088", 16929615648)
+addappid(939962, 1, "87109a483a0905b5dea2962248892c68c6f61b48a8efbb3384e2ab312c3294ea") --Main Windows Depot Far Cry® New Dawn
+setManifestid(939962, "2541041874356438434", 714775712)
+addappid(939963, 1, "153f8b8e69aea229d2b1571f0c840f9adeb2b6f9ccb3caaf315802bb1bbd47f5") --Main Windows Language Depot French Far Cry® New Dawn
+setManifestid(939963, "8415750840864874031", 725393600)
+addappid(939964, 1, "ffc2a37f41d3002147bf3401c286df22e3fa4fec6c49fe83c2fb04ec9b46932b") --Main Windows Language Depot Italian Far Cry® New Dawn
+setManifestid(939964, "8941130594844356180", 778721360)
+addappid(939965, 1, "e5773c92e8a81d6bd72945202a619fe9638afdbd952a4f772daedec4f1129efd") --Main Windows Language Depot German Far Cry® New Dawn
+setManifestid(939965, "3748177826769538242", 715012928)
+addappid(939966, 1, "53d01ef80943cd8a431040ee772dc02fc49090fa50be4f59980f70affb1ddc8d") --Main Windows Language Depot Spanish Far Cry® New Dawn
+setManifestid(939966, "3612550719594251760", 794941168)
+addappid(939967, 1, "a216dd8d2a4539df618435a0dd99f6386435fe7d724c0fd2f845b78607ef5120") --Main Windows Language Depot Russian Far Cry® New Dawn
+setManifestid(939967, "4979989929102189933", 729314896)
+addappid(939968, 1, "2b9606a0243678f3c4cbe6a521c08d936a6f7dc538a3911c737211483c11d106") --Main Windows Language Depot Brazilian Far Cry® New Dawn
+setManifestid(939968, "3851229191392906390", 747343152)
+addappid(939969, 1, "b27d04a8ce1cc6e608d4b2924e6036a022eb6dfe8edafe6b22d01e8721ecce88") --Main Windows Language Depot Japanese Far Cry® New Dawn
+setManifestid(939969, "3600239354209454019", 721982608)
+--Dlcname Far Cry New Dawn - HD Textures Pack
+addappid(996970, 1, "6515432309a59f7f6397d9a4a527b1fced0c1c98160d7036bc412751a5227f1c") --Dlcname Far Cry New Dawn - HD Textures Pack
+setManifestid(996970, "9006765081226229638", 12973229200)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(995530) --Dlcname AppID 995530
+addtoken(995530, "11402523800759818340")
+addappid(995531) --Dlcname AppID 995531
+addtoken(995531, "13494507965165270184")
+addappid(1019480) --Dlcname AppID 1019480
+addtoken(1019480, "4969856164635458594")
+addappid(1019481) --Dlcname AppID 1019481
+addtoken(1019481, "16090072699928660746")
+addappid(1019490) --Dlcname AppID 1019490
+addtoken(1019490, "4957554953299584076")
+addappid(1019491) --Dlcname AppID 1019491
+addtoken(1019491, "9366163911075946111")
+addappid(1019510) --Dlcname Far Cry New Dawn - Unicorn Trike
+addappid(1019511) --Dlcname Far Cry New Dawn - Hurk Legacy Pack
+addappid(1019512) --Dlcname Far Cry New Dawn - Knight Pack
+addappid(1019513) --Dlcname Far Cry New Dawn - Retro Weapon Pack
+addappid(1019514) --Dlcname AppID 1019514
+addtoken(1019514, "10818553932989008240")
+addappid(1019515) --Dlcname AppID 1019515
+addtoken(1019515, "7108056891515433168")
+addappid(1019520) --Dlcname AppID 1019520
+addtoken(1019520, "6932307190549762984")
+addappid(1019521) --Dlcname AppID 1019521
+addtoken(1019521, "3424464728488667009")
