18e1fe9895c8b67a6439d601c6db44627dd53b8f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 13:19:50 +0800

    add 1559390.lua

--- 1559390.lua
+++ 1559390.lua
@@ -6,3 +6,9 @@ addappid(1559391, 1, "58119c93ebe4b940d4b167bb3a1c7a17f5ca47e7202b3eff66071d1936
 setManifestid(1559391, "8335679402156740357", 11993056048)
 addappid(1559392, 1, "693148890c6643aab10a950503afedb4711857adaf435845a469b64df26952de") --Main Windows Language Depot English 天命奇御二 Fate Seeker II
 setManifestid(1559392, "1482936044453347422", 2393606736)
+--Dlcname 天命奇御二 音乐精选集
+addappid(1776180) --Dlcname 天命奇御二 音乐精选集
+addappid(1776181, 1, "fa013fbf14e92bde624f4efcbf2bc4c41594756264d47c3c89de843f7add71bc") --Dlc Windows Depot 天命奇御二 音乐精选集
+setManifestid(1776181, "3652779302410926602", 17504304)
+addappid(1776182, 1, "0dd60fa01c1f2870fb3ea74eada7df0cad42fa79253bf7d98bae5634aaf81866") --Dlc Windows Depot 天命奇御二 音乐精选集
+setManifestid(1776182, "7441115350118126883", 113703360)
