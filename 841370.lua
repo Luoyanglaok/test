132cf15d285e244c208bb9013978f7c9f7b8df5c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-19 14:03:00 +0800

    add 841370.lua

--- 841370.lua
+++ 841370.lua
@@ -1,7 +1,14 @@
-addappid(841370)
-addappid(228984)
-setManifestid(228984,"2547553897526095397")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(841371,0,"16fb529c90bf0ee75500debcf16fb1b67eb02500cc0261675c5e3ecadc1bc5d8")
-setManifestid(841371,"3271073425797755353")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 841370.lua
+--Gamename NBA 2K19
+addappid(841370) --Mainappid NBA 2K19
+addappid(841371, 1, "16fb529c90bf0ee75500debcf16fb1b67eb02500cc0261675c5e3ecadc1bc5d8") --Main Windows Depot NBA 2K19
+setManifestid(841371, "3271073425797755353", 70369177792)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(863680) --Dlcname AppID 863680
+addappid(863681) --Dlcname AppID 863681
+addappid(927700) --Dlcname AppID 927700
