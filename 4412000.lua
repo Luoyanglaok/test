5903c46e196a92df09fb04925902bf9f85a0f0c3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 17:29:26 +0800

    add 4412000.lua

--- 4412000.lua
+++ 4412000.lua
@@ -3,9 +3,9 @@
 --Gamename Zad Archery
 addappid(4412000) --Mainappid Zad Archery
 addappid(4412001, 1, "8431359d1e9c173ced3d18c8313bc5d41b3d1d7969780cc7422a2231b43c9e22") --Main Windows Depot Zad Archery
-setManifestid(4412001, "8715208282891332139", 105883680)
+setManifestid(4412001, "3579229502615548977", 105883728)
 addappid(4412002, 1, "aac1ac9373d25fedcb3e7538dd4aaaf2efb14e11c4a6b38047556a3a5ea79bc3") --Main Macos Depot Zad Archery
-setManifestid(4412002, "6321591623752653738", 114311088)
+setManifestid(4412002, "5245932877405238814", 114310640)
 addappid(4412003, 1, "643ce514b64835ece6e8b2cbf74e85a376dcb4071ab700696809689d324cd4f3") --Main Linux Depot Zad Archery
-setManifestid(4412003, "230652084880353535", 103781760)
+setManifestid(4412003, "8869436218246392875", 103784880)
 addappid(5053390) --Dlcname Zad Archery - Arrowfall Supporter Pack
