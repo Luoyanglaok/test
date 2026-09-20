40fb7fd09d236e3b91d062441aa6698d79fcfb01
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 08:33:42 +0800

    add 304240.lua

--- 304240.lua
+++ 304240.lua
@@ -1,10 +1,17 @@
-addappid(304240)
-setManifestid(228990,"1829726630299308803")
-addappid(304241,0,"628406156243272990c118c3a8497b1bdc8b03a11cd1b21e986ff86c89a45f49")
-setManifestid(304241,"110278523771241710")
-addappid(304242,0,"e86af320324228098bed69cf389ccd1b7af582a1f5725a1952a5815fd7cc1313")
-setManifestid(304242,"9087090834863155228")
-addappid(345190,0,"60ab048988d781e57c9bbc1881b3a4fbc67178b649d758ef782277efc5f073f5")
-setManifestid(345190,"7864329718337415853")
-addappid(345191,0,"eb7b2280a99010609c30169ce5ea98a4052c54a7139feab5e3ffacdac64035fd")
-setManifestid(345191,"7576499935902985330")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 304240.lua
+--Gamename Resident Evil
+addappid(304240, 1, "609907b0e0df3597c0f55b52065573c22ec94c57f9010bd8c743880799f94e78") --Mainappid Resident Evil
+addappid(304241, 1, "628406156243272990c118c3a8497b1bdc8b03a11cd1b21e986ff86c89a45f49") --Main Windows Depot Resident Evil
+setManifestid(304241, "110278523771241710", 21588002672)
+addappid(304242, 1, "e86af320324228098bed69cf389ccd1b7af582a1f5725a1952a5815fd7cc1313") --Main Windows Depot Resident Evil
+setManifestid(304242, "9087090834863155228", 18018271232)
+--Dlcname Resident Evil HD REMASTER - Sountrack selections
+addappid(345190, 1, "60ab048988d781e57c9bbc1881b3a4fbc67178b649d758ef782277efc5f073f5") --Dlcname Resident Evil HD REMASTER - Sountrack selections
+setManifestid(345190, "7864329718337415853", 89358000)
+--Dlcname Resident Evil HD REMASTER - Artbook selections
+addappid(345191, 1, "eb7b2280a99010609c30169ce5ea98a4052c54a7139feab5e3ffacdac64035fd") --Dlcname Resident Evil HD REMASTER - Artbook selections
+setManifestid(345191, "7576499935902985330", 182670720)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
