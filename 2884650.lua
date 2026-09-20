666c12feeee45c86a7005c01eefdc304e5ab0430
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 21:51:49 +0800

    add 2884650.lua

--- 2884650.lua
+++ 2884650.lua
@@ -1,7 +1,11 @@
-addappid(2884650)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(2884651,0,"dc26307acf43372560a41c67f5b684576923d6fa7a466a7ee9816eebe9e96669")
-setManifestid(2884651,"7200020418213017593")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2884650.lua
+--Gamename STRETCHER MEN
+addappid(2884650) --Mainappid STRETCHER MEN
+addappid(2884651, 1, "dc26307acf43372560a41c67f5b684576923d6fa7a466a7ee9816eebe9e96669") --Main Windows Depot STRETCHER MEN
+setManifestid(2884651, "7200020418213017593", 1804508832)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
