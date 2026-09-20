92cbef09f9888057138f8aa51c86da1775ad6334
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 02:28:43 +0800

    add 1001270.lua

--- 1001270.lua
+++ 1001270.lua
@@ -1,3 +1,8 @@
-addappid(1001270)
-addappid(1001271,0,"840a384558d951071f078063810429d5c2943f4389e02a2b992ad4e6cb001088")
-setManifestid(1001271,"3100106547137649291")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1001270.lua
+--Gamename Kebab Chefs! - Restaurant Simulator
+addappid(1001270) --Mainappid Kebab Chefs! - Restaurant Simulator
+addappid(1001271, 1, "840a384558d951071f078063810429d5c2943f4389e02a2b992ad4e6cb001088") --Main Windows Depot Kebab Chefs! - Restaurant Simulator
+setManifestid(1001271, "589298356051494204", 3139698544)
+--Missing Dlcs
+--Dlcname Kebab Chefs - Bakery Shop
