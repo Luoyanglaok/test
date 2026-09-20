66fcd7e33a7402277979de1b6ad00108f4dd8fac
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 11:36:51 +0800

    add 1850510.lua

--- 1850510.lua
+++ 1850510.lua
@@ -1,5 +1,14 @@
-addappid(1850510)
-setManifestid(1850511,"7997162364821712668")
-setManifestid(1931660,"7715259117299882738")
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1850510.lua
+--Gamename TRIANGLE STRATEGY
+addappid(1850510) --Mainappid TRIANGLE STRATEGY
+addappid(1850511, 1, "99eed402af9553dee98bf009dc96fb86d2f69b18d0d475147f36e1b6f8cc00ba") --Main Windows Depot TRIANGLE STRATEGY
+setManifestid(1850511, "7997162364821712668", 8811870592)
+--Dlcname TRIANGLE STRATEGY Mini Artbook (Digital Download)
+addappid(1931660, 1, "bea5c4f508d15aba1f4095d42eb4aa470a112263e7aeb70917ea7550a785b4b0") --Dlcname TRIANGLE STRATEGY Mini Artbook (Digital Download)
+setManifestid(1931660, "7715259117299882738", 21012704)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
