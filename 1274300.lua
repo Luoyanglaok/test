d511e0937702bb3f74b91785430fb01d3ef8e195
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 22:27:19 +0800

    add 1274300.lua

--- 1274300.lua
+++ 1274300.lua
@@ -1,5 +1,9 @@
-addappid(1274300)
-addappid(1274301,0,"15ca4016e83f695eb2b729348bbabe7491f1e6e8e8297b607ecd4f5724f173fa")
-setManifestid(1274301,"6869697459117137751")
-addappid(1333370,0,"0bfda0b1d5d9b05ae27494490d28a4418c746d09d1ebb1e4fd983e24c6d81985")
-setManifestid(1333370,"7880083163248479744")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1274300.lua
+--Gamename Cyber Agent
+addappid(1274300, 1, "294be4e223cd913cbb255613e1d3a8ed24701a08c46bc9e245fbcd5ba93434ab") --Mainappid Cyber Agent
+addappid(1274301, 1, "15ca4016e83f695eb2b729348bbabe7491f1e6e8e8297b607ecd4f5724f173fa") --Main Windows Depot Cyber Agent
+setManifestid(1274301, "6869697459117137751", 840966976)
+--Dlcname Cyber Agent - Mystery DLC
+addappid(1333370, 1, "0bfda0b1d5d9b05ae27494490d28a4418c746d09d1ebb1e4fd983e24c6d81985") --Dlcname Cyber Agent - Mystery DLC
+setManifestid(1333370, "7880083163248479744", 48326864)
