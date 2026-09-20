243158f5dfdb9ce66847cdf1f1ab790a7ecc530b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:19:17 +0800

    add 3865340.lua

---
+++ 3865340.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3865340.lua
+--Gamename Deadcam
+addappid(3865340) --Mainappid Deadcam
+addappid(3865341, 1, "9065431C279D043F6A958E7C72E60DCD882D05694E55975A57CE333EF069D69F") --Main Windows Depot Deadcam
+setManifestid(3865341, "7145034223440450459", 3287418016)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
