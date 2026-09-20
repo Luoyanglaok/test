98f2195c6ba0a92c7354e35d9787827c0a0522e6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 21:23:36 +0800

    add 818320.lua

--- 818320.lua
+++ 818320.lua
@@ -1,11 +1,18 @@
-addappid(818320)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(818321,0,"6ce5dfe98f68fe49ee0ca8464231b09d5d34c71227f41b4a98d2e77fea8a87da")
-setManifestid(818321,"5082200738862381232")
-addappid(818330,0,"d3782143e833f0a2ee1f432490d82e17dedc4b8ec937d351418e7285f5a7fa6e")
-setManifestid(818330,"1680857634589086224")
-addappid(818322,0,"bf53a94edb83cc75dc2db28f848f970ca181b78da71b13728f7a1541b9c62a42")
-setManifestid(818322,"5630417122678099630")
-addappid(818323,0,"f79b5fc4dd1d7295bce13c630425cf08596173103cbba38750a5d6f7dc6850b2")
-setManifestid(818323,"868116497700638192")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 818320.lua
+--Gamename LEGO® The Incredibles
+addappid(818320, 1, "e263c28d2d7355c75461a45ff1e9dd7d9303eccd86c26359378429140f3aed8b") --Mainappid LEGO® The Incredibles
+addappid(818321, 1, "6ce5dfe98f68fe49ee0ca8464231b09d5d34c71227f41b4a98d2e77fea8a87da") --Main Windows Depot LEGO® The Incredibles
+setManifestid(818321, "5082200738862381232", 14629413296)
+addappid(818322, 1, "bf53a94edb83cc75dc2db28f848f970ca181b78da71b13728f7a1541b9c62a42") --Main Macos Depot LEGO® The Incredibles
+setManifestid(818322, "5630417122678099630", 14696752496)
+--Dlcname Parr Family Vacation Character Pack
+addappid(818330, 1, "d3782143e833f0a2ee1f432490d82e17dedc4b8ec937d351418e7285f5a7fa6e") --Dlcname Parr Family Vacation Character Pack
+addappid(818323, 1, "f79b5fc4dd1d7295bce13c630425cf08596173103cbba38750a5d6f7dc6850b2") --Dlc Macos Depot Parr Family Vacation Character Pack
+setManifestid(818323, "868116497700638192", 5633360)
+setManifestid(818330, "1680857634589086224", 5633360)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
