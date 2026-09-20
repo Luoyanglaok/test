e91108dd2e1590cfe61db59d41c335782357ec94
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 13:11:08 +0800

    add 2496920.lua

--- 2496920.lua
+++ 2496920.lua
@@ -1,5 +1,15 @@
-addappid(2496920)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2496921,0,"7987aba77a32baa32ba890bcef0bb200783f71c4985ac918f4eb4afba2518e39")
-setManifestid(2496921,"952745201629702186")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2496920.lua
+--Gamename 开局一只蛋
+addappid(2496920) --Mainappid 开局一只蛋
+addappid(2496921, 1, "7987aba77a32baa32ba890bcef0bb200783f71c4985ac918f4eb4afba2518e39") --Main Windows Depot 开局一只蛋
+setManifestid(2496921, "2040262775387180806", 2685592608)
+--Dlcname 开局一只蛋——宙天龙皇
+addtoken(2892650, "45222185941776881")
+addappid(2892650, 1, "d088c4807a8b71bdf45cc09834e6031a4216476e22a41608236af5a7227c4c40") --Dlcname 开局一只蛋——宙天龙皇
+setManifestid(2892650, "2081090170064090990", 62946096)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
