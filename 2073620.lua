3754ae922cffd78c9586afd204a29ca89c8332e0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 16:33:41 +0800

    add 2073620.lua

--- 2073620.lua
+++ 2073620.lua
@@ -1,4 +1,9 @@
-addappid(2073620)
-addtoken(2073620,"10441616841966771533")
-addappid(2073622,0,"154ad2895e0508329860b5e2cdf50583b1708ce1a37767731393bf2f62bf5560")
-setManifestid(2073622,"3120763415576993894")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2073620.lua
+--Gamename Arena Breakout: Infinite
+addappid(2073620, 1, "55441bce44bbbf493078d6aabaeaaf6c8760cd094eb7fe330325dce4cf36c4fd") --Mainappid Arena Breakout: Infinite
+addtoken(2073620, "10441616841966771533")
+addappid(2073622, 1, "154ad2895e0508329860b5e2cdf50583b1708ce1a37767731393bf2f62bf5560") --Main Windows Depot Arena Breakout: Infinite
+setManifestid(2073622, "8251362946053458782", 94501707728)
+addappid(2073623, 1, "14f6ab8b9cd355425a40b6b6750af882fb7d46c692db430a0bdd19eb80503ccf") --Main Windows Depot Arena Breakout: Infinite
+setManifestid(2073623, "7566159068305000576", 93426494016)
