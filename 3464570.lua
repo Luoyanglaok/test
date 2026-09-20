e636efe33f30983166ad730e80d37367bdf94bab
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 22:48:12 +0800

    add 3464570.lua

---
+++ 3464570.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3464570.lua
+--Gamename Я беременна в 16
+addappid(3464570) --Mainappid Я беременна в 16
+addappid(3464571, 1, "1B02E37E6D8533825A8916829CCAD9424597C459F141B9F1BD002DC74547EA43") --Main Windows Depot Я беременна в 16
+setManifestid(3464571, "2842890506054228679", 8110453056)
+--Dlcname Я беременна в 16: Эпизод 2 — Дополнительные материалы
+addappid(4033390, 1, "c74d8dd8524cf0bee6ea0a206c98887239c6d952abc2c9bf26beaddefa367b6b") --Dlcname Я беременна в 16: Эпизод 2 — Дополнительные материалы
+setManifestid(4033390, "634606305212452107", 1164028768)
+--Share Depots
+addappid(4879090) --Dlcname Я беременна в 16: Эпизод 4
+--Missing Dlcs
+--Dlcname Я беременна в 16: Дополнительные материалы
+--Dlcname Я беременна в 16: Эпизод 2
+--Dlcname Я беременна в 16: Эпизод 3
+--Dlcname Я беременна в 16: Эпизод 3 — Дополнительные материалы
