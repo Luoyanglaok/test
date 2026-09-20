916d9a11146c75822f8387db7d0273f14702cd44
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 21:03:40 +0800

    add 703080.lua

--- 703080.lua
+++ 703080.lua
@@ -1,6 +1,40 @@
-addappid(703080)
-setManifestid(228987,"4302102680580581867")
-addappid(703081,0,"bc33ccdea0df8610185dab43ef1f141daecf265e379acb2feb58fde38212796c")
-setManifestid(703081,"9166227121018179147")
-addappid(1098120,0,"9decae9ea2fe2d9055772fecad34b56835f0464dff8d4159d7457fc3586d2994")
-setManifestid(1098120,"10163085496252241")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 703080.lua
+--Gamename Planet Zoo
+addappid(703080, 1, "519fa2ea9c9a1cca0bbd6cd0c7a7f8c220812ec10ef3f7402764d03bc6e8121c") --Mainappid Planet Zoo
+addappid(703081, 1, "bc33ccdea0df8610185dab43ef1f141daecf265e379acb2feb58fde38212796c") --Main Windows Depot Planet Zoo
+setManifestid(703081, "6607940512589780813", 19838544816)
+--Dlcname Deluxe Edition Extras
+addappid(1098120, 1, "9decae9ea2fe2d9055772fecad34b56835f0464dff8d4159d7457fc3586d2994") --Dlcname Deluxe Edition Extras
+setManifestid(1098120, "10163085496252241", 806056768)
+--Dlcname Planet Zoo Soundtrack
+addappid(1383610) --Dlcname Planet Zoo Soundtrack
+addappid(1383611, 1, "7f6c35897448b21336cece1d72e8d98086e52e1f3321e7f21a0fdcf754b205e5") --Dlc Windows Depot Planet Zoo Soundtrack
+setManifestid(1383611, "920395403518652583", 78876656)
+addappid(1383612, 1, "60e9cb821b764d13f54d002f93c9bac4eda0da0afee5cd41ebe6f4d0304f7e9e") --Dlc Windows Depot Planet Zoo Soundtrack
+setManifestid(1383612, "8061495413215669178", 713593008)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(1098121) --Dlcname AppID 1098121
+addtoken(1098121, "3634592998398443903")
+addappid(1196770) --Dlcname Planet Zoo: Arctic Pack
+addappid(1238440) --Dlcname Planet Zoo: South America Pack
+addappid(1349400) --Dlcname Planet Zoo: Australia Pack
+addappid(1471590) --Dlcname Planet Zoo: Aquatic Pack
+addappid(1567110) --Dlcname Planet Zoo: Southeast Asia Animal Pack
+addappid(1647620) --Dlcname Planet Zoo: Africa Pack
+addappid(1726150) --Dlcname Planet Zoo: Europe Pack
+addappid(1747960) --Dlcname Planet Zoo: North America Animal Pack
+addappid(1934140) --Dlcname Planet Zoo: Wetlands Animal Pack
+addappid(2013290) --Dlcname Planet Zoo: Conservation Pack
+addappid(2150050) --Dlcname Planet Zoo: Twilight Pack
+addappid(2199210) --Dlcname Planet Zoo: Grasslands Animal Pack
+addappid(2346830) --Dlcname Planet Zoo: Tropical Pack
+addappid(2436600) --Dlcname Planet Zoo: Arid Animal Pack
+addappid(2502240) --Dlcname Planet Zoo: Oceania Pack
+addappid(2675740) --Dlcname Planet Zoo: Eurasia Animal Pack
+addappid(2837730) --Dlcname Planet Zoo: Barnyard Animal Pack
+addappid(3146420) --Dlcname Planet Zoo: Zookeepers Animal Pack
+addappid(3473820) --Dlcname Planet Zoo: Americas Animal Pack
+addappid(3586990) --Dlcname Planet Zoo: Asia Animal Pack
