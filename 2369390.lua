51f0fc06d758320771733b87be80d7556902365c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 20:40:25 +0800

    add 2369390.lua

--- 2369390.lua
+++ 2369390.lua
@@ -1,8 +1,34 @@
-addappid(2369390)
-setManifestid(228988,"6645201662696499616")
-addappid(2369391,0,"c3d84384b92e4ab8aa58ab4d8602f408821c5ce3b7a213d07d79926a6198180b")
-setManifestid(2369391,"5783041166523199015")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
-addappid(2432590,0,"3b4498e4cb314f981631df47a5a260f6f0ea14bbbb77e50efa0c2810741435d4")
-setManifestid(2432590,"7910386811588276346")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2369390.lua
+--Gamename Far Cry® 6
+addappid(2369390, 1, "05b36de22513f6419b9d143306abc1ff99261a4d7c944589268dc5c82c20020d") --Mainappid Far Cry® 6
+addappid(2369391, 1, "c3d84384b92e4ab8aa58ab4d8602f408821c5ce3b7a213d07d79926a6198180b") --Main Windows Depot Far Cry® 6
+setManifestid(2369391, "5783041166523199015", 93436614176)
+addappid(2369392, 1, "39cb26249d996a7b6001a56e8ebf11968ce3f346f2342efa3ec20b88e203b405") --Main Windows Depot Far Cry® 6
+setManifestid(2369392, "8390405336387105870", 48)
+--Dlcname Far Cry 6 - HD Textures
+addappid(2432590, 1, "3b4498e4cb314f981631df47a5a260f6f0ea14bbbb77e50efa0c2810741435d4") --Dlcname Far Cry 6 - HD Textures
+setManifestid(2432590, "7910386811588276346", 55158744048)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(2369450) --Dlcname Far Cry 6: Lost Between Worlds
+addappid(2369451) --Dlcname Far Cry 6: Lost Between Worlds - Ubisoft Activation
+addappid(2369452) --Dlcname Far Cry 6 Game of the Year Upgrade Pass
+addappid(2369453) --Dlcname Far Cry 6 Game of the Year Upgrade Pass - Ubisoft Activation
+addappid(2369454) --Dlcname Far Cry 6 DLC 3 Joseph: Collapse
+addappid(2369455) --Dlcname Far Cry 6 DLC 3 Joseph: Collapse - Ubisoft Activation
+addappid(2369456) --Dlcname Far Cry 6 DLC 2 Pagan: Control
+addappid(2369457) --Dlcname Far Cry 6 DLC 2 Pagan: Control - Ubisoft Activation
+addappid(2369458) --Dlcname Far Cry 6 DLC 1 Vaas: Insanity
+addappid(2369459) --Dlcname Far Cry 6 DLC 1 Vaas: Insanity - Ubisoft Activation
+addappid(2369470) --Dlcname Far Cry 6 - Season Pass
+addappid(2369471) --Dlcname Far Cry 6 - Season Pass Ubisoft Activation
+addappid(2369472) --Dlcname Far Cry 6 - Starter Pack
+addappid(2369473) --Dlcname Far Cry 6 Starter Pack - Ubisoft Activation
+addappid(2369530) --Dlcname Far Cry 6 Standard Edition Ubisoft Activation
+addappid(2369531) --Dlcname Far Cry 6 Deluxe Edition Ubisoft Activation
+addappid(2369532) --Dlcname Far Cry 6 Gold Edition Ubisoft Activation
+addappid(2369533) --Dlcname Far Cry 6 Game of the Year Edition Ubisoft Activation
