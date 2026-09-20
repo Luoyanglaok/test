0b17caac85063fc1d4dd8de4348d869449365627
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 22:29:11 +0800

    add 1292690.lua

--- 1292690.lua
+++ 1292690.lua
@@ -1,6 +1,13 @@
-addappid(1292690)
-setManifestid(228989,"3514306556860204959")
-addappid(1292691,0,"72b67b79a37f90150100e29f681d9fcf6724f28885e8e75f6b6b0919e399f225")
-setManifestid(1292691,"5462434374975305260")
-addappid(1292692,0,"e2a36d3b055dadd2081a3699d0c798f57bb05fc69936826dc08982c81d5e3b26")
-setManifestid(1292692,"3252541663986131580")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1292690.lua
+--Gamename Corruption of Champions II
+addappid(1292690, 1, "9454061fc191adc3b8826942f0a26ca4aef1599c65c29c38139c0982f693e4d0") --Mainappid Corruption of Champions II
+addappid(1292691, 1, "72b67b79a37f90150100e29f681d9fcf6724f28885e8e75f6b6b0919e399f225") --Main Windows Depot Corruption of Champions II
+setManifestid(1292691, "7688220167799415861", 699359936)
+addappid(1292692, 1, "e2a36d3b055dadd2081a3699d0c798f57bb05fc69936826dc08982c81d5e3b26") --Main Windows Depot Corruption of Champions II
+setManifestid(1292692, "1624111370901451371", 66615136)
+addappid(1292693, 1, "ce284c55ce086941dbb3443d3380d907f47a0ea70d0c6760e9932254fd6c4ba5") --Main Linux Depot Corruption of Champions II
+setManifestid(1292693, "3054311165550066608", 61484704)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
