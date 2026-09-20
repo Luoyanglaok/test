3a43f6454a610ec773763ab634ec6cd2c4aba6db
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 22:58:55 +0800

    add 3521270.lua

--- 3521270.lua
+++ 3521270.lua
@@ -3,7 +3,7 @@
 --Gamename Phoenix's Faye.飞儿
 addappid(3521270) --Mainappid Phoenix's Faye.飞儿
 addappid(3521271, 1, "e5771a6562a62cb01d42ca73f05eebfe7bb3aaf42a1f79fa13f5496f9bab7011") --Main Windows Language Depot English Phoenix's Faye.飞儿
-setManifestid(3521271, "3156176101800136319", 13212023840)
+setManifestid(3521271, "7700771625726182865", 13208833024)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
