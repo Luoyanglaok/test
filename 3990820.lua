a5ad618badbbbd377c0e77120f33c4cbfe86ded5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 08:22:08 +0800

    add 3990820.lua

--- 3990820.lua
+++ 3990820.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 3990820.lua
-addappid(3990820, 0, "a902537e4fecfd78a767b8ef07521d6e779db2e1382660dba810c4f3aae3d304")
+addappid(3990820, 0, "ea93aa0f3a08707e850d2ad31a09c7eb7e30827eb9fc9edd4ffb97959f69b772")
