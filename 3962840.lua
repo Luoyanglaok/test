8cebd67dd725126fa5f3026f2b6697fbb7cbe4a1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:43:23 +0800

    add 3962840.lua

---
+++ 3962840.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3962840.lua
+--Gamename TACOS - The Adventurous Couple's Original Story Season 1
+addappid(3962840) --Mainappid TACOS - The Adventurous Couple's Original Story Season 1
+addappid(3962841, 1, "c161b78366e634444ccf6d884bdd9780ba8364703f817a1f1ed936f20bbfe112") --Main Windows Depot TACOS - The Adventurous Couple's Original Story Season 1
+setManifestid(3962841, "1897600610036244263", 12929194432)
+--Missing Dlcs
+--Dlcname TACOS - The Adventurous Couple's Original Story Season 1 Soundtrack
