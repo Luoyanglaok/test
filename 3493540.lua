4c7e5e1ff6e3eba948cb76ee01f3fe2c230e2baa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 00:56:56 +0800

    add 3493540.lua

---
+++ 3493540.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3493540.lua
+--Gamename Transport Fever 3
+addappid(3493540) --Mainappid Transport Fever 3
+addtoken(3493540, "12729446192953376118")
+addappid(3493541, 1, "16dc1b7b118cf59c6dd3a61cc28759250c87f755c7c6c3e187862a586799b941") --Main Windows Depot Transport Fever 3
