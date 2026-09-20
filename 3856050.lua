0f6a8d1b3292d2dca0b3f50e3f28257ea72d7738
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:08:47 +0800

    add 3856050.lua

---
+++ 3856050.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3856050.lua
+--Gamename Naked Lust and Burning Passions
+addappid(3856050) --Mainappid Naked Lust and Burning Passions
+addappid(3856051, 1, "9d0b0efa2541a915830d8271439e01164cf9e67c469c433b5c80d43c04f37b53") --Main Windows Depot Naked Lust and Burning Passions
+setManifestid(3856051, "1009256172750816568", 20427312944)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
