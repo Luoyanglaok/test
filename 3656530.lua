e503aa92cefde991698666f17e35c8eba0855eca
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:36:43 +0800

    add 3656530.lua

---
+++ 3656530.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3656530.lua
+--Gamename Sex Hill
+addappid(3656530, 1, "4ea4b0a64f25095f93c79e18f04a15a40318734fcfa7e5f081d6586ec91ec4e2") --Mainappid Sex Hill
+addappid(3656531, 1, "d188e80ffd1e670c10e8c9c75b55484e448a205edad0961a8349723c9efb60ec") --Main Windows Depot Sex Hill
+setManifestid(3656531, "1528109134622107253", 2449116832)
+--Dlcname Sex Hill - Wallpaper Set
+addappid(4892630, 1, "e91b208572b66c5d2d81b4b397508f97108eed924163fbd438f2da4e6a3aa00f") --Dlcname Sex Hill - Wallpaper Set
+setManifestid(4892630, "5313402916593925664", 379158656)
