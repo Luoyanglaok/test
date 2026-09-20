06ed11ed85baed3c67d546267dc8de4552609b98
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:26:29 +0800

    add 4868600.lua

---
+++ 4868600.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4868600.lua
+--Gamename Grim Whispers of Childhood Demo
+addappid(4868600) --Mainappid Grim Whispers of Childhood Demo
+addappid(4868601, 1, "172b84a88cd4e06ddda7f77088a7ad3eaf9e9be62ba9068f459ccf3a18355355") --Main Windows Depot Grim Whispers of Childhood Demo
+setManifestid(4868601, "2395095902470569659", 8246988096)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
