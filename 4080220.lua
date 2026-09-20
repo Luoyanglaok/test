c3aa3b3b684883291edc7536ac0f9f477d97007f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 21:23:41 +0800

    add 4080220.lua

--- 4080220.lua
+++ 4080220.lua
@@ -4,8 +4,30 @@
 addappid(4080220) --Mainappid EA SPORTS FC™ 27
 addappid(4080221, 1, "7049f0ef11b8ebe50c06c4543fe973208ba01b2d8da3069e108627fbf352cce4") --Main Windows Depot EA SPORTS FC™ 27
 setManifestid(4080221, "8878500642775157634", 62117658608)
+addappid(4080223, 1, "0573bfe3d4f8b5d2734b56489765324c3df348252909d8bd0687e15ce907a45b") --Main Windows Language Depot Swedish EA SPORTS FC™ 27
+setManifestid(4080223, "2844633327964375509", 63232)
+addappid(4080224, 1, "fa1acba06d8e53e502007d40cce2f465d0f11e333e3be79dbe670bf7569c133b") --Main Windows Language Depot German EA SPORTS FC™ 27
+setManifestid(4080224, "1522837532170075092", 1459809296)
+addappid(4080225, 1, "6dd81776a48dc671a16191048c046607a79ab4013dadaf3112678d973e3b59c3") --Main Windows Language Depot English EA SPORTS FC™ 27
+setManifestid(4080225, "2830121205034384666", 48336)
+addappid(4080226, 1, "773b9f7b9f5a017021c1210016fa5712f25b1034f6ea32ac5097cc071308c13e") --Main Windows Language Depot French EA SPORTS FC™ 27
+setManifestid(4080226, "5335581693918691469", 1108082176)
+addappid(4080227, 1, "0821c95d0360dfcdc552c2de7118b4fed735bc21f333c678cb09f78bd5bdc591") --Main Windows Language Depot Italian EA SPORTS FC™ 27
+setManifestid(4080227, "388339849221300007", 1417887600)
+addappid(4080229, 1, "847343c0863a7cb9671b65ab73c5a1189b40336f6f730dc4fd59cb5cb80a6aee") --Main Windows Language Depot Spanish EA SPORTS FC™ 27
+setManifestid(4080229, "2678743399729336553", 1522071872)
 addappid(4080261, 1, "82236ba461d7223a5639914e8e3218f4bed8dffa99eaa6e1126eac8302c7d6d8") --Main Windows Language Depot Schinese EA SPORTS FC™ 27
 setManifestid(4080261, "770041676407763391", 1288844224)
+addappid(4080265, 1, "3fb5f73a06b7503c3bd74958ce1321884230ed85b835658e95d9b855cf96d2c4") --Main Windows Language Depot Portuguese EA SPORTS FC™ 27
+setManifestid(4080265, "7544564129171758317", 63120)
+addappid(4080266, 1, "454be933df9a0720926f2edd97d069e879460f5545abfb62169a1924490ba7d5") --Main Windows Language Depot Polish EA SPORTS FC™ 27
+setManifestid(4080266, "7188230184079753568", 1569330608)
+addappid(4160762, 1, "74e0165849379dc39f62c32f14f581cde89df83128e7e8b1f884a5047554d38e") --Main Windows Language Depot Arabic EA SPORTS FC™ 27
+setManifestid(4160762, "6242186738971933662", 1739427776)
+addappid(4160763, 1, "056ab5df670e8a6d546b48cf2f30c6fda08e7c7f78f9a267425bb3c50e2bb2ad") --Main Windows Language Depot Brazilian EA SPORTS FC™ 27
+setManifestid(4160763, "4260402710574580573", 1707277632)
+addappid(4160764, 1, "0631805b76b7d3e0e6a4be26e79a01b9810087c6cd3aea02f77d7e969c7a8c1a") --Main Windows Language Depot Latam EA SPORTS FC™ 27
+setManifestid(4160764, "2393855831714087215", 1501181200)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
