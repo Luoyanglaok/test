d1dc6e2e8d6c06b5e058e8deb680067c608a2ef2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:42:19 +0800

    add 2692440.lua

--- 2692440.lua
+++ 2692440.lua
@@ -1,3 +1,6 @@
-addappid(2692440)
-addappid(2692441,0,"265ff61fb95c6ca10afe9ae21cc3e1bc01bb93754e8d658167e217de34002a00")
-setManifestid(2692441,"3865878717999391881")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2692440.lua
+--Gamename LoveCraft
+addappid(2692440, 1, "b004180e1534d355841b44776fabefda782c8e637fce7759281c6d45f70b283d") --Mainappid LoveCraft
+addappid(2692441, 1, "265ff61fb95c6ca10afe9ae21cc3e1bc01bb93754e8d658167e217de34002a00") --Main Windows Depot LoveCraft
+setManifestid(2692441, "2442493404270377015", 4918581536)
