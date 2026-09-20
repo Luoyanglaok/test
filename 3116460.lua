7bc25a3ff81d1d2c417a9e2d63fef9787e7933ae
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 19:27:36 +0800

    add 3116460.lua

---
+++ 3116460.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3116460.lua
+--Gamename ROOM– 24/7 Total Surveillance
+addappid(3116460) --Mainappid ROOM– 24/7 Total Surveillance
+addappid(3116461, 1, "636babb542b4e26297baa195e1249e831fedc341640e3f47b78d3170f3d4b37d") --Main Windows Depot ROOM– 24/7 Total Surveillance
+setManifestid(3116461, "4803137010830337417", 1965940240)
+addappid(3116462, 1, "05bef0594ff41b8004380e97d9d27365539395039cff807e7fba3f281ef2da84") --Main Windows Language Depot Japanese ROOM– 24/7 Total Surveillance
+setManifestid(3116462, "1154737813948494117", 1978796976)
+addappid(3116463, 1, "10bd52e634d7ff6836c2c27d59943e7ac1e34c598a7fe04757c6fe3052562515") --Main Windows Language Depot Tchinese ROOM– 24/7 Total Surveillance
+setManifestid(3116463, "6393996901874684325", 1966442272)
+addappid(3116464, 1, "06f2609d0c3f191ce7696db864a3c45d69f97bd695c383c81ab4520f899a7083") --Main Windows Language Depot Schinese ROOM– 24/7 Total Surveillance
+setManifestid(3116464, "3048199740210701678", 1966396224)
+addappid(3116465, 1, "2a9873fdbe892f1adf4f8650b45cc50fe0cb1b2e1d2aab1374a04ee8a4cec0df") --Main Windows Language Depot Koreana ROOM– 24/7 Total Surveillance
+setManifestid(3116465, "664819096788307479", 1966034000)
+--Missing Dlcs
+--Dlcname ROOM– 24/7 Total Surveillance ARTBOOK
