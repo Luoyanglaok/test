dabd7e4eaa68bc8cacaf8c3c8afcfde0bfceabb1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:28:26 +0800

    add 2637070.lua

--- 2637070.lua
+++ 2637070.lua
@@ -1,5 +1,11 @@
-addappid(2637070)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2637071,0,"0f6991cf277e81d9f71b651e4e56c3a98f9b61391fac3bd276845bbfe09cf741")
-setManifestid(2637071,"1352293100308278084")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2637070.lua
+--Gamename Crawling Lab
+addappid(2637070) --Mainappid Crawling Lab
+addappid(2637071, 1, "0f6991cf277e81d9f71b651e4e56c3a98f9b61391fac3bd276845bbfe09cf741") --Main Windows Depot Crawling Lab
+setManifestid(2637071, "5811486885780704904", 2009060336)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
