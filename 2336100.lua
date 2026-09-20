a1a3420b308ec8d0c3bff0bf84d37047769004ff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:17:47 +0800

    add 2336100.lua

---
+++ 2336100.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2336100.lua
+--Gamename Garden of Earthly Delights
+addappid(2336100) --Mainappid Garden of Earthly Delights
+addappid(2336101, 1, "d04856d5e5585e58f7ec3f6e8f60e112a525c95450ba195835e745797be7c823") --Main Windows Depot Garden of Earthly Delights
+setManifestid(2336101, "1212639977033410007", 2571140704)
+--Missing Dlcs
+--Dlcname Sperme d’Éros: Garden of Earthly Delights
