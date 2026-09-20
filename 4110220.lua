7d6f70bfa7425b49fee25833ec36282ba47f8650
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 17:35:40 +0800

    add 4110220.lua

---
+++ 4110220.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4110220.lua
+--Gamename Room 404: Erotic Massage
+addappid(4110220) --Mainappid Room 404: Erotic Massage
+addappid(4110221, 1, "ada5470768542cda02e4ef3bc274fc4f806638b2e086cca39d042b7a1f70349c") --Main Windows Depot Room 404: Erotic Massage
+setManifestid(4110221, "7551920560970868840", 1442684528)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
