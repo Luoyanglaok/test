017fcb7d28aa67432aba5ba4753073f5c30627e3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 09:50:23 +0800

    add 3444020.lua

--- 3444020.lua
+++ 3444020.lua
@@ -1,3 +1,18 @@
-addappid(3444020)
-addappid(3444021,0,"390479eb93dcb5aa47faa126ba19b7b0db80bb304eeaedc192215c05deb69b8e")
-setManifestid(3444021,"6383933528369330095")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3444020.lua
+--Gamename 我在地府打麻将 - Demonic Mahjong
+addappid(3444020, 1, "4bf7c24f3401a258a3191843cd2890ce58ac47bc5f3dc1b388be9645e2e31a77") --Mainappid 我在地府打麻将 - Demonic Mahjong
+addappid(3444021, 1, "390479eb93dcb5aa47faa126ba19b7b0db80bb304eeaedc192215c05deb69b8e") --Main Windows Depot 我在地府打麻将 - Demonic Mahjong
+setManifestid(3444021, "5467306086004302364", 3434848928)
+addappid(3444022, 1, "c7d881f2483c870330dfc2b85e02394b14b6257070a99cbe010f2ad1db2b98ba") --Main Macos Depot 我在地府打麻将 - Demonic Mahjong
+setManifestid(3444022, "4355016926266196388", 9993677184)
+--Dlcname 我在地府打麻将 Soundtrack
+addappid(4027650) --Dlcname 我在地府打麻将 Soundtrack
+addappid(4027651, 1, "86f1e0d8e869bed57f6e6f946554663fbafde333e619ee765e6dde93b321823f") --Dlc Windows Depot 我在地府打麻将 Soundtrack
+setManifestid(4027651, "8599386916551382469", 401480560)
+addappid(4027652, 1, "1da1e0c8db0f9603cafbe67f5e4eb88d3f91c3750956bfc8a900dd5ab2aa38cb") --Dlc Windows Depot 我在地府打麻将 Soundtrack
+setManifestid(4027652, "6493222745834112418", 34489376)
+--Dlcname 我在地府打麻将：恼热狂欢
+addappid(4817180) --Dlcname 我在地府打麻将：恼热狂欢
+addappid(4817182, 1, "a8e6dad6ba27a1b4dc901d4f2cc617f04e0ad9dcc0583d83dbba4572f5dae940") --Dlc Windows Depot 我在地府打麻将：恼热狂欢
+setManifestid(4817182, "4432311096106271299", 0)
