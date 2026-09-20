aac6afed23f5236b2b645dc04a35d1878dc540ad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 02:15:08 +0800

    add 1000410.lua

--- 1000410.lua
+++ 1000410.lua
@@ -1,4 +1,9 @@
-addappid(1000410)
-setManifestid(228989,"550968249685141759")
-addappid(1000411,0,"1cbbf1c9d6b1d0aee69b3d4114d5b5b5ebe60f335599cc50eb08bfaffa7b0efc")
-setManifestid(1000411,"8514345898424009472")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1000410.lua
+--Gamename WRATH: Aeon of Ruin
+addappid(1000410, 1, "5fe21717b240e5174b78446abfad61f98cc1a925c119721fd7d98181a02f55cf") --Mainappid WRATH: Aeon of Ruin
+addappid(1000411, 1, "1cbbf1c9d6b1d0aee69b3d4114d5b5b5ebe60f335599cc50eb08bfaffa7b0efc") --Main Windows Depot WRATH: Aeon of Ruin
+setManifestid(1000411, "8790573023365250137", 1569489664)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
