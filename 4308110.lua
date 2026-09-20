e68056124a573e82810d7614fcbeeb324fe4727c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 22:34:46 +0800

    add 4308110.lua

---
+++ 4308110.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4308110.lua
+--Gamename Your Stepmom
+addappid(4308110, 1, "cb048559398a34d23bb0cb7a9fc270903e0ec0c9027aa6bc0a20454e86737ee5") --Mainappid Your Stepmom
+addappid(4308111, 1, "d6fd1f9a7c968912ef85bfeab3c65ffc99decbb8b9322a6a6a1f507703f1f3ce") --Main Windows Depot Your Stepmom
+setManifestid(4308111, "1219140638895748640", 239572512)
+addappid(4346180) --Dlcname Your Stepmom - Free DLC
