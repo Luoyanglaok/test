4b26884b5f2712de1b0c894f76b544f580fee5f9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:13:56 +0800

    add 4840320.lua

---
+++ 4840320.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4840320.lua
+--Gamename Sex Passion and Lust
+addappid(4840320) --Mainappid Sex Passion and Lust
+addappid(4840321, 1, "3bc14c6508c440807f3c48c719b1ae38e0deed9e7f4930bcf7f665a26551a6e3") --Main Windows Depot Sex Passion and Lust
+setManifestid(4840321, "9023504452531564180", 26557856544)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
