ba92e8c4ba450c01fb73e928b71c32d485f00a2a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:06:19 +0800

    add 3170460.lua

---
+++ 3170460.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3170460.lua
+--Gamename 小先生 Little ShiFu
+addappid(3170460) --Mainappid 小先生 Little ShiFu
+addappid(3170461, 1, "b66e39457ae95ef2c9e0ebffd2e731f21da23582cb60530b373ff6bb55a97029") --Main Windows Depot 小先生 Little ShiFu
+setManifestid(3170461, "8229147932288793874", 37002938112)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
