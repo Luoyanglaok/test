ccdce1244a7e433f2ddb1083bfb4bddc78dfddde
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 19:45:23 +0800

    add 4911470.lua

---
+++ 4911470.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4911470.lua
+--Gamename FRESHIE SISTERS
+addappid(4911470) --Mainappid FRESHIE SISTERS
+addappid(4911471, 1, "335043a525447f62f6b85458d00c80d359efb1c8885e6b21b97eee8ec028d9bb") --Main Windows/Linux Depot FRESHIE SISTERS
+setManifestid(4911471, "5041628558896065881", 1174341024)
+addappid(4911472, 1, "7194d204504f939d30efe4c1d6434251ac92c31db832bb3a8d876990d748be21") --Main Macos Depot FRESHIE SISTERS
+setManifestid(4911472, "1737343145343754789", 1167861456)
