cbdb94985c6e6c7d46dd08611b6566254a855ba9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:19:55 +0800

    add 3966480.lua

---
+++ 3966480.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3966480.lua
+--Gamename Lustquake Legion
+addappid(3966480) --Mainappid Lustquake Legion
+addappid(3966481, 1, "7dccb660ccbdf253001b3a86ddc923ae528eebdd6acb3e308d866008593cd77b") --Main Windows Depot Lustquake Legion
+setManifestid(3966481, "3432504955761504303", 519446464)
