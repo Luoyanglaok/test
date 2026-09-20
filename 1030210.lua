d6ae497bee0a71ee05edbfa6ca25ca2c1850bb23
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 14:01:27 +0800

    add 1030210.lua

--- 1030210.lua
+++ 1030210.lua
@@ -1,5 +1,15 @@
-addappid(1030210)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1030211,0,"294931f8d84cd3c62a79c27031c1d407e28875f7fba01c99f8e3fc6d18385280")
-setManifestid(1030211,"79459445296839775")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1030210.lua
+--Gamename Torchlight III
+addappid(1030210) --Mainappid Torchlight III
+addappid(1030211, 1, "294931f8d84cd3c62a79c27031c1d407e28875f7fba01c99f8e3fc6d18385280") --Main Windows Depot Torchlight III
+setManifestid(1030211, "79459445296839775", 6273783008)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1337370) --Dlcname AppID 1337370
+addtoken(1337370, "8674530810891872138")
+addappid(1358460) --Dlcname AppID 1358460
+addappid(1358461) --Dlcname AppID 1358461
