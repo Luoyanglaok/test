facac499335e48a25fe388214784336fdecfd5fd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 22:44:25 +0800

    add 2665250.lua

---
+++ 2665250.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2665250.lua
+--Gamename Rancher: A new life
+addappid(2665250) --Mainappid Rancher: A new life
+addappid(2665251, 1, "0c6b69f8c5c30ae8c21d2a971385e06d0c67e95d25f226de1e45157ca7b54c9b") --Main Windows Depot Rancher: A new life
+setManifestid(2665251, "1954081045274187739", 4508035056)
