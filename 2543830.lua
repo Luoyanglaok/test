a19e043c5342edc99ba96ca31d48a54a50a8119a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 03:14:59 +0800

    add 2543830.lua

---
+++ 2543830.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2543830.lua
+--Gamename Way of the Hunter 2
+addappid(2543830, 1, "208cf0d6ce8c4379a98a3218a9bf954de984fa45ac07066428ee8de121ffa6b0") --Mainappid Way of the Hunter 2
+addappid(2543831, 1, "9f9973eb07d2b8f038773f7600154e138fba08482108cd1271798748e26a2e30") --Main Windows Depot Way of the Hunter 2
+setManifestid(2543831, "4570601823006210023", 29063846592)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
