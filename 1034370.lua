3cab4629c5e2a872c34fe818d7d2fc382d9f33c4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:21:56 +0800

    add 1034370.lua

--- 1034370.lua
+++ 1034370.lua
@@ -1,5 +1,14 @@
-addappid(1034370)
-addappid(1034371,0,"e518cfceaacd574c98fd7706eb0879c8639756f18fe6cfa152adbf579d360a40")
-setManifestid(1034371,"3267352902640248481")
-addappid(1034374,0,"b4c7dd2b492808021969403e6ffba71834f589fa2dae79f221ef2a802ef5fcb1")
-setManifestid(1034374,"7965703830383317969")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1034370.lua
+--Gamename Iron Heart
+addappid(1034370) --Mainappid Iron Heart
+addappid(1034371, 1, "e518cfceaacd574c98fd7706eb0879c8639756f18fe6cfa152adbf579d360a40") --Main Windows Depot Iron Heart
+setManifestid(1034371, "3267352902640248481", 152773152)
+addappid(1034372, 1, "c9da5ffc765c95679ec09aca9b564f9cc53ba32e241476b708110fa0b07af502") --Main Windows Language Depot German Iron Heart
+setManifestid(1034372, "8627999560030202990", 111285520)
+addappid(1034373, 1, "c06941a149b275e97432f54ed6b5a054312cd3708b9709675bd04d34f48491cc") --Main Windows Language Depot French Iron Heart
+setManifestid(1034373, "7699224038446140463", 111285440)
+addappid(1034374, 1, "b4c7dd2b492808021969403e6ffba71834f589fa2dae79f221ef2a802ef5fcb1") --Main Windows Language Depot Russian Iron Heart
+setManifestid(1034374, "7965703830383317969", 109672928)
+addappid(1034375, 1, "38ec3ba9ac3274d0b8dae314a452ac87201a3f967ad3693e0ea139a0c3cc5810") --Main Macos Depot Iron Heart
+setManifestid(1034375, "5376573767084765179", 148229904)
