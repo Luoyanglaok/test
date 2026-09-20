231b4718fe18765f04a94e9b78da4b49ed3e921a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-20 23:01:20 +0800

    add 935490.lua

--- 935490.lua
+++ 935490.lua
@@ -1,5 +1,11 @@
-addappid(935490)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(935491,0,"0ea665642d86210e6ecb9f31207cdbfc59060a94cc7c610705d7c5ec65ebf860")
-setManifestid(935491,"6143478712260273588")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 935490.lua
+--Gamename AREAZ
+addappid(935490) --Mainappid AREAZ
+addappid(935491, 1, "0ea665642d86210e6ecb9f31207cdbfc59060a94cc7c610705d7c5ec65ebf860") --Main Windows Depot AREAZ
+setManifestid(935491, "542093111532296165", 7331113616)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
