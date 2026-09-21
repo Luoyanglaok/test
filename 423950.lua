37ccc1478db1fad6d671b20c9048c7b9c3b85d36
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 00:01:43 +0800

    add 423950.lua

--- 423950.lua
+++ 423950.lua
@@ -1,8 +1,15 @@
-addappid(423950)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229002,"7260605429366465749")
-addappid(423951,0,"d62af4e518f8dd6b6df6d91958cd1448c6cd5da171c661c6fdeadea226fb2081")
-setManifestid(423951,"7751143447122169620")
-addappid(423952,0,"5b8de7c424e59e4217c08e77c91f107cb32421f06d3f2c0603de2c33df442c33")
-setManifestid(423952,"3141589215476958865")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 423950.lua
+--Gamename Deadlight: Director's Cut
+addappid(423950) --Mainappid Deadlight: Director's Cut
+addappid(423951, 1, "d62af4e518f8dd6b6df6d91958cd1448c6cd5da171c661c6fdeadea226fb2081") --Main Windows Depot Deadlight: Director's Cut
+setManifestid(423951, "7751143447122169620", 4247152768)
+addappid(423952, 1, "5b8de7c424e59e4217c08e77c91f107cb32421f06d3f2c0603de2c33df442c33") --Main Windows Depot Deadlight: Director's Cut
+setManifestid(423952, "3141589215476958865", 32564112)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229002, "7260605429366465749", 50365280)
