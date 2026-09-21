aaea2cfe26308b5325d8bd51c7ce7b74e6ddc36b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 17:14:53 +0800

    add 929200.lua

--- 929200.lua
+++ 929200.lua
@@ -1,3 +1,6 @@
-addappid(929200)
-addappid(929201,0,"a5822d626c236205f9e9ebb89124d4977e801ddad11e758e5c127b3ae35939d8")
-setManifestid(929201,"4334701155737295721")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 929200.lua
+--Gamename 吞食孔明传 Tunshi Kongming Legends
+addappid(929200) --Mainappid 吞食孔明传 Tunshi Kongming Legends
+addappid(929201, 1, "a5822d626c236205f9e9ebb89124d4977e801ddad11e758e5c127b3ae35939d8") --Main Windows Depot 吞食孔明传 Tunshi Kongming Legends
+setManifestid(929201, "4334701155737295721", 1204915664)
