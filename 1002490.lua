71cbd550178244cb8c7d3becb257f88596473c58
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 21:27:00 +0800

    add 1002490.lua

--- 1002490.lua
+++ 1002490.lua
@@ -1,3 +1,6 @@
-addappid(1002490)
-addappid(1002491,0,"81a88db650e0076645c5145385116609c93f9eae1cef3a6232768ca61a5a0c2a")
-setManifestid(1002491,"4746075673066636480")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1002490.lua
+--Gamename Roulette Simulator 2
+addappid(1002490) --Mainappid Roulette Simulator 2
+addappid(1002491, 1, "81a88db650e0076645c5145385116609c93f9eae1cef3a6232768ca61a5a0c2a") --Main Windows Depot Roulette Simulator 2
+setManifestid(1002491, "4746075673066636480", 464469888)
