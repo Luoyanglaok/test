d7ed7b0b4340bc1ad05443709666ba9a7f8d8959
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 17:05:50 +0800

    add 222940.lua

--- 222940.lua
+++ 222940.lua
@@ -1,5 +1,11 @@
-addappid(222940)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(222941,0,"a5607756385b019287b875f646124e89fe3085da91f38fadd658f58947730be3")
-setManifestid(222941,"247548660556727058")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 222940.lua
+--Gamename THE KING OF FIGHTERS XIII STEAM EDITION
+addappid(222940) --Mainappid THE KING OF FIGHTERS XIII STEAM EDITION
+addappid(222941, 1, "a5607756385b019287b875f646124e89fe3085da91f38fadd658f58947730be3") --Main Windows Depot THE KING OF FIGHTERS XIII STEAM EDITION
+setManifestid(222941, "247548660556727058", 0)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
