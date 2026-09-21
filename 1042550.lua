ec1bbcd222f316dac8546e1f66472ab9ec571c0a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 17:53:25 +0800

    add 1042550.lua

--- 1042550.lua
+++ 1042550.lua
@@ -1,7 +1,15 @@
-addappid(1042550)
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-addappid(1042551,0,"bd80c7bd28d5cacbe5cc2dfafa6a9464b0e7fd8e769b04750ed0892ce11a162e")
-setManifestid(1042551,"1711281787115726840")
-addappid(1042552,0,"96a03909c7aef73b6e510b6ac8b569f9792d7652e01175a5a374ee275deffda3")
-setManifestid(1042552,"6775586209044059578")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1042550.lua
+--Gamename Digimon Story Cyber Sleuth: Complete Edition
+addappid(1042550, 1, "0f1bf8ab098f867e21a4e7d60c8e1d19c9d7456048daab8a4c6ee200acacbe63") --Mainappid Digimon Story Cyber Sleuth: Complete Edition
+addappid(1042551, 1, "bd80c7bd28d5cacbe5cc2dfafa6a9464b0e7fd8e769b04750ed0892ce11a162e") --Main Windows Depot Digimon Story Cyber Sleuth: Complete Edition
+setManifestid(1042551, "1711281787115726840", 5813258496)
+addappid(1042552, 1, "96a03909c7aef73b6e510b6ac8b569f9792d7652e01175a5a374ee275deffda3") --Main Windows Depot Digimon Story Cyber Sleuth: Complete Edition
+setManifestid(1042552, "6775586209044059578", 10407264)
+addappid(1042553, 1, "a66b35c9a29bb051b81eafb087b0e572410a863c03892e1c9657354944ca0c65") --Main Windows Depot Digimon Story Cyber Sleuth: Complete Edition
+addappid(1042554, 1, "1e4569414c471fda1b328269a0b7c8ca17e0efcf7ab4630d5ac5e51833be548a") --Main Windows Depot Digimon Story Cyber Sleuth: Complete Edition
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
