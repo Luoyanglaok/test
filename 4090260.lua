6cd5cd8f15e47f2fd5d32db48e73727b5f0dfa59
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 17:14:35 +0800

    add 4090260.lua

---
+++ 4090260.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4090260.lua
+--Gamename Lifeguard Holic
+addappid(4090260) --Mainappid Lifeguard Holic
+addappid(4090261, 1, "22e4f65e4323198ea2ecc5f076766d7c7b4e7637f30121c3dd79cdb4c9052eba") --Main Windows Depot Lifeguard Holic
+setManifestid(4090261, "5075648810136899815", 1697317504)
+--Missing Dlcs
+--Dlcname Lifeguard Holic - ArtBook
