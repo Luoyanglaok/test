9e385d15b6fe118f290ebd6a24e3b49301c5a66f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 22:10:34 +0800

    add 212910.lua

--- 212910.lua
+++ 212910.lua
@@ -1,103 +1,123 @@
-addappid(212910)
-addappid(219092,0,"6eb4af9cc7ed432ff402317620d36eaf3db695eed4672a51c58e4ef506236652")
-setManifestid(219092,"3355545260084890161")
-addappid(212911,0,"02867cf7ad0d4e3ceb3956634299a23791265cb6fd4e8c30b2c9531746eee923")
-setManifestid(212911,"2218285080576667048")
-setManifestid(202972,"44054127954755624")
-addappid(212912,0,"f55b43e5a1d467df6ff885bee0ed09b0ef8a7eae2338298ca2fbe0164d8482ad")
-setManifestid(212912,"7173492351802578203")
-setManifestid(202975,"855173471322016522")
-setManifestid(202976,"398756463655539206")
-setManifestid(202977,"245449812372322261")
-setManifestid(202978,"2034345168780887552")
-setManifestid(202979,"1450519586995334306")
-setManifestid(202996,"471429897779379026")
-setManifestid(202997,"724981713647824764")
-addappid(212921,0,"b53847b58f88ea439fc2fac823ab44eb5ca0e5cf54d1f61692d03e476074db0e")
-setManifestid(212921,"8942509767028602311")
-addappid(212922,0,"7eea5dc81255484780ffbd2b5f7f73f8c2a7fa29a45e99bbc00a7df11c2e8a04")
-setManifestid(212922,"8678673089453878920")
-addappid(212923,0,"19fd0e15ad34c678dbc9e0ef5bd72a79e7b6a34d6c14f99deffe7b0bab14841b")
-setManifestid(212923,"4325288223750995811")
-addappid(212924,0,"fa891463c9cb54d8859c10b7753d6629cc059bb63828185d6c8e236c0a2865af")
-setManifestid(212924,"5583935071805196033")
-addappid(212925,0,"b2396bf138d934122d9fb8b05d690d59d210d54e5f54714e2f4a557143dba1f4")
-setManifestid(212925,"3163634041483756440")
-addappid(212927,0,"60af73ef446db71417f4a033c644d9e0a05c88743a88a564b1e2f4a1fbe8b6a0")
-setManifestid(212927,"1615947624091302306")
-addappid(212928,0,"f8912ae16a46395d42c130299f31549b6316c9ec1656c621f62574294ccdfd06")
-setManifestid(212928,"1688978644157572080")
-setManifestid(202998,"216411158385088962")
-setManifestid(219094,"3038711571425126440")
-addappid(219096,0,"8bca81d3b9e7783a19bbfb591e97e63842234fe03b6b5c98194929c039136851")
-setManifestid(219096,"6083944898681218397")
-addappid(233181,0,"17080fe6340aa6eb718f52302aaabdc7af95d0815c0cec5e6f603e756b983bde")
-setManifestid(233181,"4664140994489092100")
-addappid(233182,0,"f490983b891a8ed73704b21e7a6ba2bb74c81b7988b26069d283f93f954ae5a5")
-setManifestid(233182,"4497978254252620785")
-addappid(233183,0,"54de4e47596df5e02ebdefb3d63f36da36d03f719ff06a18bcc465beb0d57378")
-setManifestid(233183,"4764197661310044006")
-addappid(233184,0,"e4a2bd289ff2123f50e9b0966059985e01ee1f1f45c6635186364ca2deb0144f")
-setManifestid(233184,"7493768293233010044")
-addappid(233185,0,"f43401c9fe8b5205471410b10b52a7e0d163f28e80522cfbd91c84889884c1d8")
-setManifestid(233185,"8766002504692926338")
-addappid(233186,0,"a6190bd0beffca18063a8a11d91bba2ca821f9d55ba7924621aad893ff58a60d")
-setManifestid(233186,"6880795305594739422")
-addappid(233187,0,"42fbcf13a365ece93c5e28746a5bf550ac89ffe8df3098aceacf006a45d61140")
-setManifestid(233187,"1300262062483564262")
-addappid(233188,0,"4a235035df7c483c97c0643237dcd371d178d328ee1526c3c373809d3d80f8de")
-setManifestid(233188,"6301789178665746291")
-addappid(219099,0,"acdaf21e07c794f8c5706d216dd4f2c30addc3af117973483a8b13cd81f4ab1b")
-setManifestid(219099,"8312826338200556582")
-addappid(238811,0,"cf87d5b34bdb01f5e3551e10f590eea6d5218476bf4f00341da22f4b102ad694")
-setManifestid(238811,"411175400606548323")
-addappid(238812,0,"7c1cfa986aa9db24833db276696915e56f7950eb56557eb081f7af484aaf6792")
-setManifestid(238812,"2159041321443513187")
-addappid(238813,0,"776e0d0e4cd941c2e18ba579ada713bee685cc0f8fd8195e5e92b49edf958099")
-setManifestid(238813,"5592459654559696995")
-addappid(238814,0,"f2cb35f6b49d7f46e09a50ee51f83aa140db44584426bba533f7dfd77933e232")
-setManifestid(238814,"3218645021804340047")
-addappid(238815,0,"570c98bcb5980d11864e7bb3f311806c90e96a72a79e268da084b7393fe6fd4e")
-setManifestid(238815,"7522532629479561918")
-addappid(238816,0,"38d3f2cbf92ab70fd6cbb96cdbe530903a5ed893b7c6881a88336068ffa2fe41")
-setManifestid(238816,"679746093254754200")
-addappid(238817,0,"a21b17a516048c78dd8b8927274c3452731c089f518b1812efec512c8453a5f6")
-setManifestid(238817,"4583108330490838221")
-addappid(238818,0,"5383ca972ed1cb97274f31ebabbd98bf4c6345473ec6575bb667c998c9ca69b0")
-setManifestid(238818,"3383282731542742831")
-addappid(219102,0,"10211787c026b5152b74ed3a651a0255792b1caffd83b45e14124df7c7c14d1f")
-setManifestid(219102,"5735097321577510919")
-addappid(247169,0,"e951e1b7e5ba5b06ceb322a5bef053c2204c751ff3c2d0a9f8579312375abdb6")
-setManifestid(247169,"1779924028979804934")
-addappid(247170,0,"2d223c94c1140967c219f40b11a0bb8bb57148c927003f27c107cc986e89c79d")
-setManifestid(247170,"3208871003911302185")
-addappid(247171,0,"379263280e27d155e2d066f596040ae9c64ed0f64bc4fc9100f3421b7d572ce7")
-setManifestid(247171,"7536735363313167114")
-addappid(247172,0,"d62668bfee07e7237701c83f9c490d6a4fcd03eaca25216931f96c84d9990560")
-setManifestid(247172,"4631959612606192943")
-addappid(247173,0,"3df3db23931655824cdf25ebdf58fd39d64567e2c34dae191902824821951efd")
-setManifestid(247173,"2400984025967731946")
-addappid(247174,0,"ebad0a5f0af34be40272d844b90467dde80311ed5375bca415e7bb1ac8863bb4")
-setManifestid(247174,"1762353410019682734")
-addappid(247175,0,"7bfe4e7269703f9cf8cf0adb3b9326e334a7062030b3eff4fffe7000678f1e7f")
-setManifestid(247175,"6590545150150140095")
-addappid(247176,0,"3377df1164e1dc707ed1892d73944019abc474dd92d35f1359c35ca5caba6ba4")
-setManifestid(247176,"1045349965220769849")
-addappid(219104,0,"23b29767995776300fe5e7ab05366c9d55691c70b16ab613634c90a8fa0080b5")
-setManifestid(219104,"580718828309419192")
-addappid(255569,0,"a3a9c94204dd5f3baf7d57295036b7b6678480ae2b57b8db42e98f7c9cf8838a")
-setManifestid(255569,"577328094221422745")
-addappid(255570,0,"8f9d37dddb5ce020d3c7ee6b29458927476cc6a920fb94605c547bcd0a97ff78")
-setManifestid(255570,"2634057275195429479")
-addappid(255571,0,"174cff5219ba1f544ae74c44758d49fff72ad5e06061932c1acd4b22a5154f81")
-setManifestid(255571,"2759068461300104961")
-addappid(255572,0,"67c300190125c389242674aad2e2e5692c2810b7637872ece174199a57b269a3")
-setManifestid(255572,"6316468451344225815")
-addappid(255573,0,"19de300ccee76ffc33e9b6f2a149597ad73c4af2142e291988a9a7b2f13a805f")
-setManifestid(255573,"2078522143069019530")
-addappid(255574,0,"9ac6615d48640edb703ee7880f4f576ea04424eb56544759ace2a751ce187d53")
-setManifestid(255574,"4434767707929500186")
-addappid(255575,0,"4f7ccd4d4a1eb968579532dd971abbbeecb2fe0dc7cafd044fc3718b868b652d")
-setManifestid(255575,"3223612125189088869")
-addappid(255576,0,"ef145c8ce1227ef18b00fdde2d0bf5d92bee0f485919fc0276f6d3e0cd575869")
-setManifestid(255576,"4338541736904757697")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 212910.lua
+--Gamename Call of Duty: Black Ops II - Zombies
+addappid(212910) --Mainappid Call of Duty: Black Ops II - Zombies
+addappid(212911, 1, "02867cf7ad0d4e3ceb3956634299a23791265cb6fd4e8c30b2c9531746eee923") --Main Windows Depot Call of Duty: Black Ops II - Zombies
+setManifestid(212911, "8633544871776073130", 8266880)
+addappid(212912, 1, "f55b43e5a1d467df6ff885bee0ed09b0ef8a7eae2338298ca2fbe0164d8482ad") --Main Windows Depot Call of Duty: Black Ops II - Zombies
+setManifestid(212912, "7173492351802578203", 568060144)
+addappid(212921, 1, "b53847b58f88ea439fc2fac823ab44eb5ca0e5cf54d1f61692d03e476074db0e") --Main Windows Language Depot English Call of Duty: Black Ops II - Zombies
+setManifestid(212921, "8942509767028602311", 452713296)
+addappid(212922, 1, "7eea5dc81255484780ffbd2b5f7f73f8c2a7fa29a45e99bbc00a7df11c2e8a04") --Main Windows Language Depot French Call of Duty: Black Ops II - Zombies
+setManifestid(212922, "8678673089453878920", 411386048)
+addappid(212923, 1, "19fd0e15ad34c678dbc9e0ef5bd72a79e7b6a34d6c14f99deffe7b0bab14841b") --Main Windows Language Depot Italian Call of Duty: Black Ops II - Zombies
+setManifestid(212923, "4325288223750995811", 392116176)
+addappid(212924, 1, "fa891463c9cb54d8859c10b7753d6629cc059bb63828185d6c8e236c0a2865af") --Main Windows Language Depot German Call of Duty: Black Ops II - Zombies
+setManifestid(212924, "5583935071805196033", 451482608)
+addappid(212925, 1, "b2396bf138d934122d9fb8b05d690d59d210d54e5f54714e2f4a557143dba1f4") --Main Windows Language Depot Spanish Call of Duty: Black Ops II - Zombies
+setManifestid(212925, "3163634041483756440", 437773120)
+addappid(212927, 1, "60af73ef446db71417f4a033c644d9e0a05c88743a88a564b1e2f4a1fbe8b6a0") --Main Windows Language Depot Polish Call of Duty: Black Ops II - Zombies
+setManifestid(212927, "1615947624091302306", 452745248)
+addappid(212928, 1, "f8912ae16a46395d42c130299f31549b6316c9ec1656c621f62574294ccdfd06") --Main Windows Language Depot Russian Call of Duty: Black Ops II - Zombies
+setManifestid(212928, "1688978644157572080", 402627024)
+addappid(233181, 1, "17080fe6340aa6eb718f52302aaabdc7af95d0815c0cec5e6f603e756b983bde") --Main Windows Language Depot English Call of Duty: Black Ops II - Zombies
+setManifestid(233181, "4664140994489092100", 329132208)
+addappid(233182, 1, "f490983b891a8ed73704b21e7a6ba2bb74c81b7988b26069d283f93f954ae5a5") --Main Windows Language Depot French Call of Duty: Black Ops II - Zombies
+setManifestid(233182, "4497978254252620785", 279219888)
+addappid(233183, 1, "54de4e47596df5e02ebdefb3d63f36da36d03f719ff06a18bcc465beb0d57378") --Main Windows Language Depot Italian Call of Duty: Black Ops II - Zombies
+setManifestid(233183, "4764197661310044006", 268010880)
+addappid(233184, 1, "e4a2bd289ff2123f50e9b0966059985e01ee1f1f45c6635186364ca2deb0144f") --Main Windows Language Depot German Call of Duty: Black Ops II - Zombies
+setManifestid(233184, "7493768293233010044", 287554608)
+addappid(233185, 1, "f43401c9fe8b5205471410b10b52a7e0d163f28e80522cfbd91c84889884c1d8") --Main Windows Language Depot Spanish Call of Duty: Black Ops II - Zombies
+setManifestid(233185, "8766002504692926338", 310376064)
+addappid(233186, 1, "a6190bd0beffca18063a8a11d91bba2ca821f9d55ba7924621aad893ff58a60d") --Main Windows Language Depot Japanese Call of Duty: Black Ops II - Zombies
+setManifestid(233186, "6880795305594739422", 329132256)
+addappid(233187, 1, "42fbcf13a365ece93c5e28746a5bf550ac89ffe8df3098aceacf006a45d61140") --Main Windows Language Depot Polish Call of Duty: Black Ops II - Zombies
+setManifestid(233187, "1300262062483564262", 329132224)
+addappid(233188, 1, "4a235035df7c483c97c0643237dcd371d178d328ee1526c3c373809d3d80f8de") --Main Windows Language Depot Russian Call of Duty: Black Ops II - Zombies
+setManifestid(233188, "6301789178665746291", 280366848)
+addappid(238811, 1, "cf87d5b34bdb01f5e3551e10f590eea6d5218476bf4f00341da22f4b102ad694") --Main Windows Language Depot English Call of Duty: Black Ops II - Zombies
+setManifestid(238811, "411175400606548323", 322004256)
+addappid(238812, 1, "7c1cfa986aa9db24833db276696915e56f7950eb56557eb081f7af484aaf6792") --Main Windows Language Depot French Call of Duty: Black Ops II - Zombies
+setManifestid(238812, "2159041321443513187", 274566096)
+addappid(238813, 1, "776e0d0e4cd941c2e18ba579ada713bee685cc0f8fd8195e5e92b49edf958099") --Main Windows Language Depot Italian Call of Duty: Black Ops II - Zombies
+setManifestid(238813, "5592459654559696995", 267500816)
+addappid(238814, 1, "f2cb35f6b49d7f46e09a50ee51f83aa140db44584426bba533f7dfd77933e232") --Main Windows Language Depot German Call of Duty: Black Ops II - Zombies
+setManifestid(238814, "3218645021804340047", 287672800)
+addappid(238815, 1, "570c98bcb5980d11864e7bb3f311806c90e96a72a79e268da084b7393fe6fd4e") --Main Windows Language Depot Spanish Call of Duty: Black Ops II - Zombies
+setManifestid(238815, "7522532629479561918", 287515824)
+addappid(238816, 1, "38d3f2cbf92ab70fd6cbb96cdbe530903a5ed893b7c6881a88336068ffa2fe41") --Main Windows Language Depot Japanese Call of Duty: Black Ops II - Zombies
+setManifestid(238816, "679746093254754200", 322004480)
+addappid(238817, 1, "a21b17a516048c78dd8b8927274c3452731c089f518b1812efec512c8453a5f6") --Main Windows Language Depot Polish Call of Duty: Black Ops II - Zombies
+setManifestid(238817, "4583108330490838221", 322004448)
+addappid(238818, 1, "5383ca972ed1cb97274f31ebabbd98bf4c6345473ec6575bb667c998c9ca69b0") --Main Windows Language Depot Russian Call of Duty: Black Ops II - Zombies
+setManifestid(238818, "3383282731542742831", 311115696)
+addappid(247169, 1, "e951e1b7e5ba5b06ceb322a5bef053c2204c751ff3c2d0a9f8579312375abdb6") --Main Windows Language Depot English Call of Duty: Black Ops II - Zombies
+setManifestid(247169, "1779924028979804934", 430480816)
+addappid(247170, 1, "2d223c94c1140967c219f40b11a0bb8bb57148c927003f27c107cc986e89c79d") --Main Windows Language Depot French Call of Duty: Black Ops II - Zombies
+setManifestid(247170, "3208871003911302185", 386574208)
+addappid(247171, 1, "379263280e27d155e2d066f596040ae9c64ed0f64bc4fc9100f3421b7d572ce7") --Main Windows Language Depot Italian Call of Duty: Black Ops II - Zombies
+setManifestid(247171, "7536735363313167114", 382441536)
+addappid(247172, 1, "d62668bfee07e7237701c83f9c490d6a4fcd03eaca25216931f96c84d9990560") --Main Windows Language Depot German Call of Duty: Black Ops II - Zombies
+setManifestid(247172, "4631959612606192943", 408325136)
+addappid(247173, 1, "3df3db23931655824cdf25ebdf58fd39d64567e2c34dae191902824821951efd") --Main Windows Language Depot Spanish Call of Duty: Black Ops II - Zombies
+setManifestid(247173, "2400984025967731946", 416645248)
+addappid(247174, 1, "ebad0a5f0af34be40272d844b90467dde80311ed5375bca415e7bb1ac8863bb4") --Main Windows Language Depot Japanese Call of Duty: Black Ops II - Zombies
+setManifestid(247174, "1762353410019682734", 430481040)
+addappid(247175, 1, "7bfe4e7269703f9cf8cf0adb3b9326e334a7062030b3eff4fffe7000678f1e7f") --Main Windows Language Depot Polish Call of Duty: Black Ops II - Zombies
+setManifestid(247175, "6590545150150140095", 430481056)
+addappid(247176, 1, "3377df1164e1dc707ed1892d73944019abc474dd92d35f1359c35ca5caba6ba4") --Main Windows Language Depot Russian Call of Duty: Black Ops II - Zombies
+setManifestid(247176, "1045349965220769849", 395651120)
+addappid(255569, 1, "a3a9c94204dd5f3baf7d57295036b7b6678480ae2b57b8db42e98f7c9cf8838a") --Main Windows Language Depot English Call of Duty: Black Ops II - Zombies
+setManifestid(255569, "577328094221422745", 394659968)
+addappid(255570, 1, "8f9d37dddb5ce020d3c7ee6b29458927476cc6a920fb94605c547bcd0a97ff78") --Main Windows Language Depot French Call of Duty: Black Ops II - Zombies
+setManifestid(255570, "2634057275195429479", 329301088)
+addappid(255571, 1, "174cff5219ba1f544ae74c44758d49fff72ad5e06061932c1acd4b22a5154f81") --Main Windows Language Depot Italian Call of Duty: Black Ops II - Zombies
+setManifestid(255571, "2759068461300104961", 393356352)
+addappid(255572, 1, "67c300190125c389242674aad2e2e5692c2810b7637872ece174199a57b269a3") --Main Windows Language Depot German Call of Duty: Black Ops II - Zombies
+setManifestid(255572, "6316468451344225815", 389790096)
+addappid(255573, 1, "19de300ccee76ffc33e9b6f2a149597ad73c4af2142e291988a9a7b2f13a805f") --Main Windows Language Depot Spanish Call of Duty: Black Ops II - Zombies
+setManifestid(255573, "2078522143069019530", 378615360)
+addappid(255574, 1, "9ac6615d48640edb703ee7880f4f576ea04424eb56544759ace2a751ce187d53") --Main Windows Language Depot Japanese Call of Duty: Black Ops II - Zombies
+setManifestid(255574, "4434767707929500186", 394665008)
+addappid(255575, 1, "4f7ccd4d4a1eb968579532dd971abbbeecb2fe0dc7cafd044fc3718b868b652d") --Main Windows Language Depot Polish Call of Duty: Black Ops II - Zombies
+setManifestid(255575, "3223612125189088869", 394664960)
+addappid(255576, 1, "ef145c8ce1227ef18b00fdde2d0bf5d92bee0f485919fc0276f6d3e0cd575869") --Main Windows Language Depot Russian Call of Duty: Black Ops II - Zombies
+setManifestid(255576, "4338541736904757697", 377379760)
+--Dlcname Call of Duty: Black Ops II - Zombies - Nuketown Zombies
+addappid(219092, 1, "6eb4af9cc7ed432ff402317620d36eaf3db695eed4672a51c58e4ef506236652") --Dlcname Call of Duty: Black Ops II - Zombies - Nuketown Zombies
+setManifestid(219092, "3355545260084890161", 342536624)
+--Dlcname Call of Duty: Black Ops II - Soundtrack
+addappid(219094, 1, "2e5a53ff93c388da9e33c69ad6c761c937508465d431046c220ab3b564409ef3") --Dlcname Call of Duty: Black Ops II - Soundtrack
+setManifestid(219094, "3038711571425126440", 0)
+--Dlcname Call of Duty: Black Ops II - Revolution Map Pack #1 Zombies
+addappid(219096, 1, "8bca81d3b9e7783a19bbfb591e97e63842234fe03b6b5c98194929c039136851") --Dlcname Call of Duty: Black Ops II - Revolution Map Pack #1 Zombies
+setManifestid(219096, "6083944898681218397", 365641728)
+--Dlcname Call of Duty: Black Ops II - Zombies - Uprising DLC
+addappid(219099, 1, "acdaf21e07c794f8c5706d216dd4f2c30addc3af117973483a8b13cd81f4ab1b") --Dlcname Call of Duty: Black Ops II - Zombies - Uprising DLC
+setManifestid(219099, "8312826338200556582", 639370064)
+--Dlcname Call of Duty: Black Ops II - Vengeance Zombies
+addappid(219102, 1, "10211787c026b5152b74ed3a651a0255792b1caffd83b45e14124df7c7c14d1f") --Dlcname Call of Duty: Black Ops II - Vengeance Zombies
+setManifestid(219102, "5735097321577510919", 759629712)
+--Dlcname Call of Duty: Black Ops II - Apocalypse Zombies
+addappid(219104, 1, "23b29767995776300fe5e7ab05366c9d55691c70b16ab613634c90a8fa0080b5") --Dlcname Call of Duty: Black Ops II - Apocalypse Zombies
+setManifestid(219104, "580718828309419192", 1123254880)
+--Share Depots
+addappid(202972, 1, "e8efd4d7d2c05ac03f3bd26bb3e9c8b0e83d58e3541f0e8f8da9f0a39f8fc16f") --Share Windows Depot Call of Duty: Black Ops II
+setManifestid(202972, "44054127954755624", 0)
+addappid(202975, 1, "74ab26ff68cd38526c1f50213fe8f82a58ada56a0cb042c29823442525f0148a") --Share Windows Language Depot English Call of Duty: Black Ops II
+setManifestid(202975, "855173471322016522", 0)
+addappid(202976, 1, "725f44b140834a21ac2bbba5a199e498272a0bbe869a124ae99ef5b15c35cc12") --Share Windows Language Depot French Call of Duty: Black Ops II
+setManifestid(202976, "398756463655539206", 0)
+addappid(202977, 1, "3f8d2a2911e6b8edec42a23a388b45636588e7541d8abd02da2a62209b322be3") --Share Windows Language Depot Italian Call of Duty: Black Ops II
+setManifestid(202977, "245449812372322261", 0)
+addappid(202978, 1, "6c720b0ffae43e2e7584280239773ae1d31877c8b7ced5ba44b85d1cbe06c467") --Share Windows Language Depot German Call of Duty: Black Ops II
+setManifestid(202978, "2034345168780887552", 0)
+addappid(202979, 1, "a111e2527dbecf81028bb41873e9b1ec64eea27a919a4e623ccd10340b6f6754") --Share Windows Language Depot Spanish Call of Duty: Black Ops II
+setManifestid(202979, "1450519586995334306", 0)
+addappid(202996, 1, "a3efe5d0e7b9623c8732437c076280d2a6a8086bfaaf7e21833384a470ab7ff3") --Share Windows Language Depot Polish Call of Duty: Black Ops II
+setManifestid(202996, "471429897779379026", 0)
+addappid(202997, 1, "3688ca0dc337af67df29a0c8ee0b3702ca5db2d1ae69cc757344b10c7a76d135") --Share Windows Language Depot Russian Call of Duty: Black Ops II
+setManifestid(202997, "724981713647824764", 0)
+addappid(202998, 1, "4eae51754f9a91706005d9348c27f01e412062f6ff4db3a4dc7bf94529da60b6") --Share Windows Depot Call of Duty: Black Ops II
+setManifestid(202998, "216411158385088962", 0)
