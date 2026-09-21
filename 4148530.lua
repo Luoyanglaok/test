919d564eeb2202b8b7de78c816b897f8005bdb4a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 14:32:17 +0800

    add 4148530.lua

---
+++ 4148530.lua
@@ -0,0 +1,28 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4148530.lua
+--Gamename City Transport Simulator 2026 - Bus & Tram
+addappid(4148530) --Mainappid City Transport Simulator 2026 - Bus & Tram
+addappid(4148531, 1, "fa46ca95704770be85f6f457e4fa2da91205e6c1ff0b79653473de4651ce3989") --Main Windows Depot City Transport Simulator 2026 - Bus & Tram
+setManifestid(4148531, "9177362200445688208", 20341656768)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4399970) --Dlcname CTS 2026 - Vienna Expansion
+addappid(4427800) --Dlcname CTS 2026 - Expert Upgrade
+--Missing Dlcs
+--Dlcname CTS 2026 - Tram DLC: Flexity Vienna
+--Dlcname CTS 2026 - Tram DLC: StadtRegioTram
+--Dlcname CTS 2026 - Tram DLC: E1 Vienna
+--Dlcname CTS 2026 - Tram DLC: GT8N Mannheim
+--Dlcname CTS 2026 - Tram DLC: GT6
+--Dlcname CTS 2026 - Bus DLC: MAN New Lion's City
+--Dlcname CTS 2026 - Bus DLC: MAN Lion's City A26 & A47
+--Dlcname CTS 2026 - Cosmetic DLC: Basel Style
+--Dlcname CTS 2026 - Bus DLC: Citybus C2
+--Dlcname CTS: 2026 - Tram DLC: KT4D
+--Dlcname CTS 2026 - Map DLC: Tram Depot + Southern-Route
+--Dlcname CTS 2026 - Season Pass
