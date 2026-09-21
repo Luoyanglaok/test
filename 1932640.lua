2de3a79f48af9ba236b30885ed847ca9e87e3513
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 21:44:15 +0800

    add 1932640.lua

--- 1932640.lua
+++ 1932640.lua
@@ -1,3 +1,7 @@
-addappid(1932640)
-addappid(1932641,0,"8cbcc48378f983709b2bd2959cf190421f3e54c57008b7a1f81ba6bfd9aad886")
-setManifestid(1932641,"8661085697874246284")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1932640.lua
+--Gamename Suikoden I&II HD Remaster Gate Rune and Dunan Unification Wars
+addappid(1932640) --Mainappid Suikoden I&II HD Remaster Gate Rune and Dunan Unification Wars
+addappid(1932641, 1, "8cbcc48378f983709b2bd2959cf190421f3e54c57008b7a1f81ba6bfd9aad886") --Main Windows Depot Suikoden I&II HD Remaster Gate Rune and Dunan Unification Wars
+setManifestid(1932641, "6999958055610959056", 9041353824)
+addappid(2148630) --Dlcname Special Item Pack (Suikoden I&II HD Remaster Pre-Order Bonus)
