2227c63bee690db2b6a75918d16e192e88117318
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 17:56:20 +0800

    add 1530160.lua

--- 1530160.lua
+++ 1530160.lua
@@ -1,4 +1,9 @@
-addappid(1530160)
-setManifestid(228988,"6645201662696499616")
-addappid(1530161,0,"79f6668266ed6a85515dbd54406ca18c6268feafc1e8f239ee78bad23087dd8d")
-setManifestid(1530161,"7509106737928463206")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1530160.lua
+--Gamename Digimon World: Next Order
+addappid(1530160, 1, "444f755fe496aed67ace6ce8d210de627fe0b4fa62fc690f9ff6d282475cc38a") --Mainappid Digimon World: Next Order
+addappid(1530161, 1, "79f6668266ed6a85515dbd54406ca18c6268feafc1e8f239ee78bad23087dd8d") --Main Windows Depot Digimon World: Next Order
+setManifestid(1530161, "8662808378398768237", 4441181744)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
