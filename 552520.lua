8ba62c48901f43e0a3f1e305543963cca0383114
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 16:41:31 +0800

    add 552520.lua

--- 552520.lua
+++ 552520.lua
@@ -1,67 +1,108 @@
-addappid(552520)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229000,"4622705914179893434")
-setManifestid(229004,"5220958916987797232")
-setManifestid(229005,"7992454656023763365")
-addappid(552521,0,"941abe7976a0b28d302442645dd45aeff4a92808b351906d5341cc9928dd1fdd")
-setManifestid(552521,"8965279101697150481")
-addappid(643857,0,"fff270ed617a6a03b5c89610cbec4ceac7e4f03658c65ad702e7bab4ab04cc12")
-setManifestid(643857,"5272481886350375515")
-addappid(552522,0,"74a72dd270bdcf9413516071c930c71a093f2bd0ff6f0370619e36dd3af605f7")
-setManifestid(552522,"663453966566387216")
-addappid(552523,0,"337c9883c0abea9d644bc57a39726413db04279c872286fd9cf47b8d03a6d3f7")
-setManifestid(552523,"4051639157773472698")
-addappid(552524,0,"669f40330d7fd6434cce00fed29c04e7e006227b4d423c050b537f5c92341d45")
-setManifestid(552524,"9177661898334819167")
-addappid(552525,0,"15ecba77e47273f7dc186ec750e2660ee90c27c57d9ac81d2fc82403b06b1244")
-setManifestid(552525,"34117454520629984")
-addappid(552526,0,"e1431f7ce02ee881d43bf75fadc76be28f7321d0610dd36aec90ba888892c336")
-setManifestid(552526,"6358593642057803988")
-addappid(552527,0,"7afb60d9946b6e8bee79c5972164b757e0fb09a28c88d818d4b875a2d1f2190c")
-setManifestid(552527,"8698831239768813057")
-addappid(552528,0,"e9ea523b975fa96f51abd42eb284be630b4a79159c4ea37d00d34b5767bb5ab6")
-setManifestid(552528,"8620862225036627409")
-addappid(761820,0,"98bacdde1712e2f8417583fb6b6fb2c2baf9c2a722e7adfb20bb0e60450db9e2")
-setManifestid(761820,"2545214878384794380")
-addappid(761821,0,"db4ce18aba3aa1810b256c19273d764525612689d00707af7e5afbd3c4f8cde7")
-setManifestid(761821,"6786887464288860602")
-addappid(761824,0,"ceb0ccf442fde02470050a9ef2b45f70a948769acf06f5fa0ffbb1a97cfd7218")
-setManifestid(761824,"8243048617070152687")
-addappid(761825,0,"f8a5414239cba2f46cdaf66b0c1da20647abd5c854a302db3486a006389d85a4")
-setManifestid(761825,"7995029197434014273")
-addappid(761826,0,"f6345588472b31561d44479604e64c29b009d5b6a435631182eee3bc80827b22")
-setManifestid(761826,"7879826585126109365")
-addappid(761827,0,"78c1538ef7dc2e18dac6b948ddc8f34824f6f5b4c9da038fc0157f0648c45b34")
-setManifestid(761827,"6175534544942389636")
-addappid(761828,0,"b15ba89cd3288cdb0a57eb409b5d9e44b2418b494e9f24e40c71bf05713ac06f")
-setManifestid(761828,"7820109186986903857")
-addappid(761829,0,"218fd7386ed9d2c9b3e7a14252aa3be843d956e7d44a52458b60c02a8a4ff660")
-setManifestid(761829,"8692547962058359784")
-addappid(643856,0,"32ab422b969508efb491eaab97eac88283831622546a6014053743fb2990c97e")
-setManifestid(643856,"5424890830496165377")
-addappid(643858,0,"771b6bbb7ef79bdaebef1ade3dc8d30463e2ce675d9e1a0601f73b0a027e5efd")
-setManifestid(643858,"7407458576740880998")
-addappid(763821,0,"2fb99e22ccea2c54daf1a28bd4600540495307d24afa2d1627b291063d4c25b3")
-setManifestid(763821,"5798107504181173828")
-addappid(763822,0,"94262fe43fc8135c74a90f7fa0ca23770f2e9e0839855366f6dcc1725de4e456")
-setManifestid(763822,"8610894077677042877")
-addappid(763823,0,"7a422a4a13dc9ae4a9236e0491c16d445e90150dcf954b08c861c64529e4848c")
-setManifestid(763823,"7241860453021330036")
-addappid(763824,0,"b59e6ec4fea275a654946576d9bbbc598894dcee0f14029e320a9ed773a12383")
-setManifestid(763824,"9059379376526556968")
-addappid(763825,0,"3d39f7de6b8f138eea310b73525ecc5761fcf4d534326734e4888b1f18ec3bd1")
-setManifestid(763825,"5615771465837311702")
-addappid(763826,0,"64b8c5a314796e15acf0e80e04275b979bafc82369b85b64ffe765f9025bc878")
-setManifestid(763826,"8220163018283470515")
-addappid(763827,0,"309f85afad901fa8cf8305e14e02f6f9c65639fb93c1ce72cbd972bb67fc971d")
-setManifestid(763827,"7667932027403316147")
-addappid(763828,0,"fcaa0d4b0d9249ce7116a60e2490634f53c9b4b98a5b3bf454fa5796e6c718bb")
-setManifestid(763828,"9123334093001751284")
-addappid(874610,0,"1ea844ce276d065e245e7534d2dc2c248d1b00c6834685fac76471bce45311ee")
-setManifestid(874610,"8539017273179371602")
-addappid(552529,0,"07b602181357ccc09efa0edf2bd9f5543e03e5a9642c82360a22b09db89df7c8")
-setManifestid(552529,"8763824218821042009")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 552520.lua
+--Gamename Far Cry® 5
+addappid(552520, 1, "e1968afb2ae62b29c04377532878e472346cd0689515c5eebab5f93bdca373a4") --Mainappid Far Cry® 5
+addappid(552521, 1, "941abe7976a0b28d302442645dd45aeff4a92808b351906d5341cc9928dd1fdd") --Main Windows Depot Far Cry® 5
+setManifestid(552521, "8965279101697150481", 32745794640)
+addappid(552522, 1, "74a72dd270bdcf9413516071c930c71a093f2bd0ff6f0370619e36dd3af605f7") --Main Windows Language Depot Brazilian Far Cry® 5
+setManifestid(552522, "663453966566387216", 2853917968)
+addappid(552523, 1, "337c9883c0abea9d644bc57a39726413db04279c872286fd9cf47b8d03a6d3f7") --Main Windows Language Depot French Far Cry® 5
+setManifestid(552523, "4051639157773472698", 2714046896)
+addappid(552524, 1, "669f40330d7fd6434cce00fed29c04e7e006227b4d423c050b537f5c92341d45") --Main Windows Language Depot German Far Cry® 5
+setManifestid(552524, "9177661898334819167", 2783735232)
+addappid(552525, 1, "15ecba77e47273f7dc186ec750e2660ee90c27c57d9ac81d2fc82403b06b1244") --Main Windows Language Depot Italian Far Cry® 5
+setManifestid(552525, "34117454520629984", 3064619840)
+addappid(552526, 1, "e1431f7ce02ee881d43bf75fadc76be28f7321d0610dd36aec90ba888892c336") --Main Windows Language Depot Japanese Far Cry® 5
+setManifestid(552526, "6358593642057803988", 2665176880)
+addappid(552527, 1, "7afb60d9946b6e8bee79c5972164b757e0fb09a28c88d818d4b875a2d1f2190c") --Main Windows Language Depot Spanish Far Cry® 5
+setManifestid(552527, "8698831239768813057", 3059544352)
+addappid(552528, 1, "e9ea523b975fa96f51abd42eb284be630b4a79159c4ea37d00d34b5767bb5ab6") --Main Windows Language Depot Russian Far Cry® 5
+setManifestid(552528, "8620862225036627409", 2724117056)
+addappid(643857, 1, "fff270ed617a6a03b5c89610cbec4ceac7e4f03658c65ad702e7bab4ab04cc12") --Main Windows Depot Far Cry® 5
+setManifestid(643857, "5272481886350375515", 2766938464)
+--Dlcname Far Cry 5 - Lost on Mars
+addtoken(761820, "14681857404498689519")
+addappid(761820, 1, "98bacdde1712e2f8417583fb6b6fb2c2baf9c2a722e7adfb20bb0e60450db9e2") --Dlcname Far Cry 5 - Lost on Mars
+setManifestid(761820, "2545214878384794380", 4277080944)
+addappid(763821, 1, "2fb99e22ccea2c54daf1a28bd4600540495307d24afa2d1627b291063d4c25b3") --Dlc Windows Depot Far Cry 5 - Lost on Mars
+setManifestid(763821, "5798107504181173828", 79189072)
+addappid(763822, 1, "94262fe43fc8135c74a90f7fa0ca23770f2e9e0839855366f6dcc1725de4e456") --Dlc Windows Language Depot Brazilian Far Cry 5 - Lost on Mars
+setManifestid(763822, "8610894077677042877", 82398032)
+addappid(763823, 1, "7a422a4a13dc9ae4a9236e0491c16d445e90150dcf954b08c861c64529e4848c") --Dlc Windows Language Depot French Far Cry 5 - Lost on Mars
+setManifestid(763823, "7241860453021330036", 84885568)
+addappid(763824, 1, "b59e6ec4fea275a654946576d9bbbc598894dcee0f14029e320a9ed773a12383") --Dlc Windows Language Depot German Far Cry 5 - Lost on Mars
+setManifestid(763824, "9059379376526556968", 82074816)
+addappid(763825, 1, "3d39f7de6b8f138eea310b73525ecc5761fcf4d534326734e4888b1f18ec3bd1") --Dlc Windows Language Depot Italian Far Cry 5 - Lost on Mars
+setManifestid(763825, "5615771465837311702", 93674256)
+addappid(763826, 1, "64b8c5a314796e15acf0e80e04275b979bafc82369b85b64ffe765f9025bc878") --Dlc Windows Language Depot Japanese Far Cry 5 - Lost on Mars
+setManifestid(763826, "8220163018283470515", 78238608)
+addappid(763827, 1, "309f85afad901fa8cf8305e14e02f6f9c65639fb93c1ce72cbd972bb67fc971d") --Dlc Windows Language Depot Spanish Far Cry 5 - Lost on Mars
+setManifestid(763827, "7667932027403316147", 87708960)
+addappid(763828, 1, "fcaa0d4b0d9249ce7116a60e2490634f53c9b4b98a5b3bf454fa5796e6c718bb") --Dlc Windows Language Depot Russian Far Cry 5 - Lost on Mars
+setManifestid(763828, "9123334093001751284", 88086176)
+--Dlcname Far Cry 5 - Hours of Darkness
+addtoken(761821, "5284535117382526942")
+addappid(761821, 1, "db4ce18aba3aa1810b256c19273d764525612689d00707af7e5afbd3c4f8cde7") --Dlcname Far Cry 5 - Hours of Darkness
+addappid(643856, 1, "32ab422b969508efb491eaab97eac88283831622546a6014053743fb2990c97e") --Dlc Windows Language Depot Spanish Far Cry 5 - Hours of Darkness
+setManifestid(643856, "5424890830496165377", 56476384)
+addappid(643858, 1, "771b6bbb7ef79bdaebef1ade3dc8d30463e2ce675d9e1a0601f73b0a027e5efd") --Dlc Windows Language Depot Russian Far Cry 5 - Hours of Darkness
+setManifestid(643858, "7407458576740880998", 61687680)
+setManifestid(761821, "6786887464288860602", 3640059920)
+addappid(761824, 1, "ceb0ccf442fde02470050a9ef2b45f70a948769acf06f5fa0ffbb1a97cfd7218") --Dlc Windows Depot Far Cry 5 - Hours of Darkness
+setManifestid(761824, "8243048617070152687", 48593776)
+addappid(761825, 1, "f8a5414239cba2f46cdaf66b0c1da20647abd5c854a302db3486a006389d85a4") --Dlc Windows Language Depot Brazilian Far Cry 5 - Hours of Darkness
+setManifestid(761825, "7995029197434014273", 51943824)
+addappid(761826, 1, "f6345588472b31561d44479604e64c29b009d5b6a435631182eee3bc80827b22") --Dlc Windows Language Depot French Far Cry 5 - Hours of Darkness
+setManifestid(761826, "7879826585126109365", 52559600)
+addappid(761827, 1, "78c1538ef7dc2e18dac6b948ddc8f34824f6f5b4c9da038fc0157f0648c45b34") --Dlc Windows Language Depot German Far Cry 5 - Hours of Darkness
+setManifestid(761827, "6175534544942389636", 50675664)
+addappid(761828, 1, "b15ba89cd3288cdb0a57eb409b5d9e44b2418b494e9f24e40c71bf05713ac06f") --Dlc Windows Language Depot Italian Far Cry 5 - Hours of Darkness
+setManifestid(761828, "7820109186986903857", 62357984)
+addappid(761829, 1, "218fd7386ed9d2c9b3e7a14252aa3be843d956e7d44a52458b60c02a8a4ff660") --Dlc Windows Language Depot Japanese Far Cry 5 - Hours of Darkness
+setManifestid(761829, "8692547962058359784", 47278256)
+--Dlcname Far Cry 5 - Zombies
+addtoken(874610, "6597978076628574405")
+addappid(874610, 1, "1ea844ce276d065e245e7534d2dc2c248d1b00c6834685fac76471bce45311ee") --Dlcname Far Cry 5 - Zombies
+setManifestid(874610, "8539017273179371602", 178896064)
+--Dlcname Far Cry 5 - HD textures pack
+addappid(903840) --Dlcname Far Cry 5 - HD textures pack
+addappid(552529, 1, "07b602181357ccc09efa0edf2bd9f5543e03e5a9642c82360a22b09db89df7c8") --Dlc Windows Depot Far Cry 5 - HD textures pack
+setManifestid(552529, "8763824218821042009", 16305626272)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(643850) --Dlcname Far Cry 5 - WW Standard Preorder - Uplay Activation
+addappid(643851) --Dlcname Far Cry 5 - RU-CN Standard Preorder - Uplay Activation
+addappid(643852) --Dlcname Far Cry 5 - WW Deluxe Preorder - Uplay Activation
+addappid(643853) --Dlcname Far Cry 5 - RU-CN Deluxe Preorder - Uplay Activation
+addappid(643854) --Dlcname Far Cry 5 - WW Gold Preorder - Uplay Activation
+addappid(643855) --Dlcname Far Cry 5 - RU-CN Gold Preorder - Uplay Activation
+addappid(761822) --Dlcname AppID 761822
+addtoken(761822, "11023959061941215702")
+addappid(761823) --Dlcname AppID 761823
+addtoken(761823, "11383797425954549672")
+addappid(763820) --Dlcname Far Cry 5 - Season Pass
+addappid(817060) --Dlcname Far Cry 5 - Season Pass - Uplay Activation
+addappid(823960) --Dlcname Far Cry 5 - Standard Edition - Uplay Activation
+addappid(823970) --Dlcname Far Cry 5 - Deluxe Edition - Uplay activation
+addappid(823980) --Dlcname Far Cry 5 - Gold edition - Uplay Activation
+addappid(823981) --Dlcname Far Cry 5 - Deluxe Pack - Uplay Activation
+addappid(823990) --Dlcname Far Cry 5 - Deluxe Pack
+addappid(874611) --Dlcname AppID 874611
+addtoken(874611, "13538510148581544087")
+addappid(1716580) --Dlcname AppID 1716580
+addtoken(1716580, "11036179696305668927")
+addappid(1716581) --Dlcname AppID 1716581
+addtoken(1716581, "12173462599988593947")
