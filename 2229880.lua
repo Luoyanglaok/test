fe9026e832299219346b0d17c318038611b39869
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 22:11:13 +0800

    add 2229880.lua

--- 2229880.lua
+++ 2229880.lua
@@ -1,13 +1,19 @@
-addappid(2229880)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2229881,0,"dfc5c85c7364067d2212d5acc81bf086099f528afe1e117a27cc1d08392679e8")
-setManifestid(2229881,"1937422335606860416")
-addappid(2229882,0,"bef10d4e11921cb3fe06929ee4230398407ebb12c95cadda3c6a861ab63549ac")
-setManifestid(2229882,"240611967032717951")
-addappid(2229883,0,"62223571e4df5555398c6ea1e074270fc48cd5408b81a4762caf2e7d7e24b9a0")
-setManifestid(2229883,"8597166659789824921")
-addappid(2229884,0,"a7086d4091c08171a87e1e97c9634afa98bd66b218b7e18a7b0e87c9fd4b7a13")
-setManifestid(2229884,"1522432102023798179")
-addappid(2229885,0,"1cc7d508a2f9ce1cdbaa42565d5f4ac35b12587ae36eebd492edac35d590f56e")
-setManifestid(2229885,"914918869490219229")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2229880.lua
+--Gamename Command & Conquer™ Tiberian Sun™ and Firestorm™
+addappid(2229880, 1, "72873cbe8c6ba46a665f3b7a8b74f0322ce1b509ed29831a7d727eb6b3977f35") --Mainappid Command & Conquer™ Tiberian Sun™ and Firestorm™
+addappid(2229881, 1, "dfc5c85c7364067d2212d5acc81bf086099f528afe1e117a27cc1d08392679e8") --Main Windows Depot Command & Conquer™ Tiberian Sun™ and Firestorm™
+setManifestid(2229881, "1937422335606860416", 1292604112)
+addappid(2229882, 1, "bef10d4e11921cb3fe06929ee4230398407ebb12c95cadda3c6a861ab63549ac") --Main Windows Language Depot English Command & Conquer™ Tiberian Sun™ and Firestorm™
+setManifestid(2229882, "240611967032717951", 1192433648)
+addappid(2229883, 1, "62223571e4df5555398c6ea1e074270fc48cd5408b81a4762caf2e7d7e24b9a0") --Main Windows Language Depot German Command & Conquer™ Tiberian Sun™ and Firestorm™
+setManifestid(2229883, "8597166659789824921", 1195148112)
+addappid(2229884, 1, "a7086d4091c08171a87e1e97c9634afa98bd66b218b7e18a7b0e87c9fd4b7a13") --Main Windows Language Depot French Command & Conquer™ Tiberian Sun™ and Firestorm™
+setManifestid(2229884, "1522432102023798179", 1195574512)
+addappid(2229885, 1, "1cc7d508a2f9ce1cdbaa42565d5f4ac35b12587ae36eebd492edac35d590f56e") --Main Windows Language Depot Spanish Command & Conquer™ Tiberian Sun™ and Firestorm™
+setManifestid(2229885, "914918869490219229", 1197389856)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
