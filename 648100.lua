4acd8ccb63fbbfb812c153d85a37c4e10336a9f1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 15:25:44 +0800

    add 648100.lua

--- 648100.lua
+++ 648100.lua
@@ -1,3 +1,6 @@
-addappid(648100)
-addappid(648101,0,"30e0ded97b0a63a895b735c25fa3596825fc11b2f5769202d6da4258b3f0e591")
-setManifestid(648101,"8016674546490539091")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 648100.lua
+--Gamename Raging Loop
+addappid(648100) --Mainappid Raging Loop
+addappid(648101, 1, "30e0ded97b0a63a895b735c25fa3596825fc11b2f5769202d6da4258b3f0e591") --Main Windows Depot Raging Loop
+setManifestid(648101, "8016674546490539091", 1488373760)
