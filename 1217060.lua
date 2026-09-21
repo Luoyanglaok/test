4c678494a647dca65f458ced58ebce3df19d4f51
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 10:38:43 +0800

    add 1217060.lua

--- 1217060.lua
+++ 1217060.lua
@@ -1,4 +1,14 @@
-addappid(1217060)
-setManifestid(228988,"6645201662696499616")
-addappid(1217061,0,"572c9d118c2d97b0d21798e01e43f4402b71b563c6aa09942c2ceb46b36d8300")
-setManifestid(1217061,"5445077084822783091")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1217060.lua
+--Gamename Gunfire Reborn
+addappid(1217060, 1, "fff1cbf2476686bc92545a23416621ad6750a66f27b815d76d42591f40473ada") --Mainappid Gunfire Reborn
+addappid(1217061, 1, "572c9d118c2d97b0d21798e01e43f4402b71b563c6aa09942c2ceb46b36d8300") --Main Windows Depot Gunfire Reborn
+setManifestid(1217061, "7432434498697067033", 4435754832)
+addappid(2111850, 1, "1e453bf587b23ac710df84786cc285306e06082b8e2002b87b1c805dbad2fa94") --Main Windows Depot Gunfire Reborn
+--Dlcname Gunfire Reborn - Visitors of Spirit Realm
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(2430400) --Dlcname Gunfire Reborn - Artisan and Magician
+addappid(3063250) --Dlcname Gunfire Reborn - Realm of Frost and Inkwash
+addappid(3920680) --Dlcname Gunfire Reborn - Echoes From Primal Grove
