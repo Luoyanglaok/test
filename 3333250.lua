3d846266c90c7985335d70e48ccac922d3675127
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-21 21:45:55 +0800

    add 3333250.lua

---
+++ 3333250.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3333250.lua
+--Gamename Crimson Oath
+addappid(3333250, 1, "17dc6b70f333ace8197313bc9c4f16f6f84846deaa3a4dbdd9724e6ccee1e697") --Mainappid Crimson Oath
+addappid(3333251, 1, "779ed1c833d58f3bca6a18b0602618c19813e1ab4eab7d1ea2ece9fd90a1f65b") --Main Windows Depot Crimson Oath
+setManifestid(3333251, "5546587255614693232", 8278138048)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
