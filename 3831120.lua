f59249e459cfb66582db7e4f534bc3afd58356d1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 11:36:36 +0800

    add 3831120.lua

---
+++ 3831120.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3831120.lua
+--Gamename 江山北望
+addappid(3831120) --Mainappid 江山北望
+addtoken(3831120, "7390236893300162594")
+addappid(3831121, 1, "9dbcd6e3623fd391c2dd9f7a389d82710a063351a504bc5653bcf9a690cbf784") --Main Windows Depot 江山北望
+setManifestid(3831121, "5180684583447225140", 256603088)
+addappid(3831124, 1, "90f46fb691c6ef16289cfa4b2253ef65800d41a7401310a2acba28fe6caf0c7e") --Main Windows Depot 江山北望
+setManifestid(3831124, "6299951775186984676", 50400457632)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
