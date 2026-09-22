11da7b92be7f849e0ec7fb2ed1c89abcb7685554
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 13:14:10 +0800

    add 1809330.lua

---
+++ 1809330.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1809330.lua
+--Gamename AppID_1809330
+addappid(1809330) --Mainappid AppID_1809330
+addtoken(1809330, "12075204262259712165")
+addappid(1809331, 1, "8a996c3669b5aed4036d13e2ef2a8fdc4958aef699e1763a719e39b503f4b5e9") --Main Windows Depot AppID_1809330
