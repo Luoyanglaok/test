2278a5fdd2a71ce11c5dada809008f214dbf7592
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 14:24:34 +0800

    add 1330430.lua

---
+++ 1330430.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1330430.lua
+--Gamename Celestial Empire
+addappid(1330430, 1, "0df8788cf856410b5a4c9b80954763e6ed1713a368523fb62601607a08950b18") --Mainappid Celestial Empire
+addappid(1330431, 1, "9c9d8c218847fb1a49d3243f5a04d891620b8993b95b25473aa63ec229cffa52") --Main Windows Depot Celestial Empire
+setManifestid(1330431, "1364573520621297738", 22232355952)
+addappid(3992291, 1, "8a0fff7e36187f4593d031d30038c1dca713a070975480925b5e9a91ad06c281") --Main Windows Depot Celestial Empire
+setManifestid(3992291, "3811189937505839737", 164377856)
+--Dlcname Celestial Empire Soundtrack
+addappid(3992290) --Dlcname Celestial Empire Soundtrack
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
