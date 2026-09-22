379d1c75577f23bac5932569321e5a105f33fffb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 14:50:48 +0800

    add 3444080.lua

--- 3444080.lua
+++ 3444080.lua
@@ -1,5 +1,11 @@
-addappid(3444080)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(3444081,0,"11ced1ac7e5218140e0c3802ac74011ca3d2ccc2979f5be29b709cf33ef1175b")
-setManifestid(3444081,"4212306822335833025")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3444080.lua
+--Gamename Bakery Cafe Simulator
+addappid(3444080) --Mainappid Bakery Cafe Simulator
+addappid(3444081, 1, "11ced1ac7e5218140e0c3802ac74011ca3d2ccc2979f5be29b709cf33ef1175b") --Main Windows Depot Bakery Cafe Simulator
+setManifestid(3444081, "5067819264717776035", 3576045904)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
