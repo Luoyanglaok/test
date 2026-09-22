97f58d6d959323df986854c910b0c0fde6fad1f8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 20:57:42 +0800

    add 2526310.lua

--- 2526310.lua
+++ 2526310.lua
@@ -1,5 +1,13 @@
-addappid(2526310)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2526311,0,"4d96d2024ac336424c26d995e45c7d983e97f71552e8d67e3d794e182b21fc57")
-setManifestid(2526311,"1249240611657929242")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2526310.lua
+--Gamename Out of Sight
+addappid(2526310) --Mainappid Out of Sight
+addappid(2526311, 1, "4d96d2024ac336424c26d995e45c7d983e97f71552e8d67e3d794e182b21fc57") --Main Windows Depot Out of Sight
+setManifestid(2526311, "193790797185163340", 4304125184)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+--Missing Dlcs
+--Dlcname Out of Sight: Stage Fright
