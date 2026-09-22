fda9c91855cd0eade310496a1dfcba7176e6c019
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 13:04:36 +0800

    add 2399220.lua

--- 2399220.lua
+++ 2399220.lua
@@ -1,3 +1,6 @@
-addappid(2399220)
-addappid(2399221,0,"f18c3a74fed166b18837f108c3965836402874d288ee4cf23ebcd520dae50049")
-setManifestid(2399221,"469259110240671652")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2399220.lua
+--Gamename NUKITASHI
+addappid(2399220, 1, "758c1067a0fa7ca88f1b986765e3f27e6f713fa66dcc0db20ab7535e46102b03") --Mainappid NUKITASHI
+addappid(2399221, 1, "f18c3a74fed166b18837f108c3965836402874d288ee4cf23ebcd520dae50049") --Main Windows Depot NUKITASHI
+setManifestid(2399221, "469259110240671652", 614472400)
