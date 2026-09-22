68c8b395be4ea90b7ea68b12b2c8f3831cef8b4f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 21:12:50 +0800

    add 2950790.lua

---
+++ 2950790.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2950790.lua
+--Gamename IRON NEST: Heavy Turret Simulator
+addappid(2950790) --Mainappid IRON NEST: Heavy Turret Simulator
+addappid(2950791, 1, "9024fbcca954551a182e4f9158ef6d35f5e2acc4c5f547f48d3ce0f725f76d4f") --Main Windows Depot IRON NEST: Heavy Turret Simulator
+setManifestid(2950791, "5447756454452346366", 1530110192)
