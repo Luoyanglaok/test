b83396c584d72afa943904c83946982ff0c02bb5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 01:04:39 +0800

    add 3830690.lua

---
+++ 3830690.lua
@@ -0,0 +1,14 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3830690.lua
+--Gamename Flee, My Elven Ninja! 3D
+addappid(3830690, 1, "5cb2cdcc8f99109f033680141f166a8c355a65e1480ea0d36fe1d157346edf35") --Mainappid Flee, My Elven Ninja! 3D
+addappid(3830691, 1, "b7dc6defefd989232eaa64bc1b6251b5e1a5ea073241601dd1d7706eb75376f5") --Main Windows Depot Flee, My Elven Ninja! 3D
+setManifestid(3830691, "1736241096064091406", 1697730208)
+addappid(3830692, 1, "ec74050ea5920381bc0eab9189d805b5dd6cbd7cbe429433be92756d849edf57") --Main Windows Language Depot Japanese Flee, My Elven Ninja! 3D
+setManifestid(3830692, "4706614662425489149", 1697565232)
+--Dlcname Flee, My Elven Ninja! 3D - Artbook
+addappid(4209540) --Dlcname Flee, My Elven Ninja! 3D - Artbook
+addappid(4209541, 1, "daba8942e158f08028623cd7393d181a3ae668a5c316fd657d0329d846ac2e91") --Dlc Windows Depot Flee, My Elven Ninja! 3D - Artbook
+setManifestid(4209541, "4414725775365648680", 73700160)
+--Missing Dlcs
+--Dlcname Flee, My Elven Ninja! 3D - Soundtrack
