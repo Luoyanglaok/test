5f744d9f708c856479c376c59357653e91e35737
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 12:31:45 +0800

    add 2244510.lua

--- 2244510.lua
+++ 2244510.lua
@@ -1,3 +1,6 @@
-addappid(2244510)
-addappid(2244511,0,"38deae40f38d52bc0f6356e5a83448433a7c54d4ecb980518abfb19b7085d708")
-setManifestid(2244511,"2216208425088354022")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2244510.lua
+--Gamename En Garde! Soundtrack
+addappid(2244510) --Mainappid En Garde! Soundtrack
+addappid(2244511, 1, "38deae40f38d52bc0f6356e5a83448433a7c54d4ecb980518abfb19b7085d708") --Main Windows Depot En Garde! Soundtrack
+setManifestid(2244511, "2216208425088354022", 95626336)
