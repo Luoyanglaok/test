c5722831aef9fcb8747fda5ef54f4465f0bc8ff6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 00:09:54 +0800

    add 4490120.lua

--- 4490120.lua
+++ 4490120.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 4490120.lua
-addappid(4490120, 0, "a07f7ae6fd36a407bce71d20696a37f7d177377e2454a51040e4802dea4a2e4e")
+addappid(4490120, 0, "c72adedad774fa2cdddc02edaccba4e9230b6ce57217e462fdefcbf31f0ead73")
