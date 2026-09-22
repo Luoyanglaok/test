85fb299754b4ed7b07b57f452189acf38aa27fdb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 00:53:41 +0800

    add 4145320.lua

---
+++ 4145320.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4145320.lua
+--Gamename 異世界樹の巫女〜魔法のチカラでおさわりHやりたい放題〜
+addappid(4145320, 1, "327c98083bb1f2f5f646cae2b0ee1e1b14cf1ec9e4ab77763483fb9b54bedcd8") --Mainappid 異世界樹の巫女〜魔法のチカラでおさわりHやりたい放題〜
+addappid(4145321, 1, "0a626b21b0f08726b37cb2a81d0d5364ddd793a2940a9c14d8b085f4fb896627") --Main Windows Depot 異世界樹の巫女〜魔法のチカラでおさわりHやりたい放題〜
+setManifestid(4145321, "1906328512312800825", 3819503808)
+--Dlcname 異世界樹の巫女～魔法のチカラでおさわりHやりたい放題～【Hシーン全解放DLC】
+addappid(4376120) --Dlcname 異世界樹の巫女～魔法のチカラでおさわりHやりたい放題～【Hシーン全解放DLC】
+addappid(4376121, 1, "f16bd1dfb0c4abb30db36899f2601f3398a0271e5cce749ce7e64e724023b62f") --Dlc Windows Depot 異世界樹の巫女～魔法のチカラでおさわりHやりたい放題～【Hシーン全解放DLC】
+setManifestid(4376121, "4788652119091173120", 64)
