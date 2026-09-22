ce8159534ff51c7e1b06b401a20462315df96c31
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 00:14:51 +0800

    add 1566390.lua

--- 1566390.lua
+++ 1566390.lua
@@ -1,9 +1,30 @@
-addappid(1566390)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229007,"4477590687906973371")
-addappid(1566391,0,"5903c7ae493cd5f1c18b10794a90ca4aac94973bdd75cc92c94abb11d88ec4f0")
-setManifestid(1566391,"3910588189779567709")
-addappid(2452270,0,"de98419d69bd6fcb0603958d0ed38592693a6277bc986be886c3beb5ed63db20")
-setManifestid(2452270,"3036652846645639683")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1566390.lua
+--Gamename Seed of the Dead: Sweet Home
+addappid(1566390, 1, "c1a5861b1d6ebd2406e3f44cd33c51429c6f0ded39d2f23d4b0819f8b96d6dd7") --Mainappid Seed of the Dead: Sweet Home
+addappid(1566391, 1, "5903c7ae493cd5f1c18b10794a90ca4aac94973bdd75cc92c94abb11d88ec4f0") --Main Windows Depot Seed of the Dead: Sweet Home
+setManifestid(1566391, "3910588189779567709", 15463027840)
+--Dlcname Seed of the Dead: Sweet Home Theme Songs
+addappid(1724110) --Dlcname Seed of the Dead: Sweet Home Theme Songs
+addappid(1724111, 1, "d7596ab7ec3ea53fd41602d6fce180db5611dcfc95097a0b82ffb48039b5c438") --Dlc Windows Depot Seed of the Dead: Sweet Home Theme Songs
+setManifestid(1724111, "2437443947495298125", 23410976)
+addappid(1724112, 1, "ced4195af016893eb6c33c44f9c5afd0cbe7dfa1a4026ddcb6e069a1d6f8ce9d") --Dlc Windows Depot Seed of the Dead: Sweet Home Theme Songs
+setManifestid(1724112, "678878947895912041", 217944128)
+--Dlcname Seed of the Dead: Charm Song
+addappid(2452270, 1, "de98419d69bd6fcb0603958d0ed38592693a6277bc986be886c3beb5ed63db20") --Dlcname Seed of the Dead: Charm Song
+setManifestid(2452270, "578223132592610643", 4210049184)
+--Dlcname Seed of the Dead: Charm Song Vocal Album
+addappid(2651960) --Dlcname Seed of the Dead: Charm Song Vocal Album
+addappid(2651961, 1, "6fa8708b42b4a8f7c1f2f0a3252b8855e13b4fe26eaf8e82b0e9cc5a1d1b75ff") --Dlc Windows Depot Seed of the Dead: Charm Song Vocal Album
+setManifestid(2651961, "585947737235554620", 41258800)
+addappid(2651962, 1, "b9560def3ec46632b97b35731f777949598dc8fbf83ae27d601cf0a5e7a6dcf1") --Dlc Windows Depot Seed of the Dead: Charm Song Vocal Album
+setManifestid(2651962, "7911147534174357661", 394202448)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
