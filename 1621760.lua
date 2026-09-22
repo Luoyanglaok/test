cc2cd8d8b4d8da9b22ec16f26f349396a9411150
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 23:47:33 +0800

    add 1621760.lua

--- 1621760.lua
+++ 1621760.lua
@@ -1,5 +1,9 @@
-addappid(1621760)
-addappid(1621761,0,"92cd4bdecb9ccff631034592da24e750456ab846ba04cb648c263dbed076af69")
-setManifestid(1621761,"1050662750236350885")
-addappid(2079040,0,"4db3216ac1548acc15e72f19243824525091198aa2fde37c4b0c688050f11669")
-setManifestid(2079040,"3756315006656032247")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1621760.lua
+--Gamename Decision: Red Daze
+addappid(1621760) --Mainappid Decision: Red Daze
+addappid(1621761, 1, "92cd4bdecb9ccff631034592da24e750456ab846ba04cb648c263dbed076af69") --Main Windows Depot Decision: Red Daze
+setManifestid(1621761, "1050662750236350885", 1093120656)
+--Dlcname Decision: Red Daze Supporter Bundle
+addappid(2079040, 1, "4db3216ac1548acc15e72f19243824525091198aa2fde37c4b0c688050f11669") --Dlcname Decision: Red Daze Supporter Bundle
+setManifestid(2079040, "3756315006656032247", 1002176336)
