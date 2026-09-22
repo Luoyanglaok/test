04db1b085c33660e76762dda14babca556a8c2b6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 13:04:10 +0800

    add 2614070.lua

--- 2614070.lua
+++ 2614070.lua
@@ -1,4 +1,12 @@
-addappid(2614070)
-setManifestid(228990,"1829726630299308803")
-addappid(2614071,0,"85b2e14de3f99250e09830257b8ce9b7344187e52cacf34930d5c218667b6b94")
-setManifestid(2614071,"7287521717339121043")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2614070.lua
+--Gamename NUKITASHI 2
+addappid(2614070, 1, "bc03b2ad27b1e0194333365ebee47cc2f09ecb2949aab4b606d48f73e3941f4f") --Mainappid NUKITASHI 2
+addappid(2614071, 1, "85b2e14de3f99250e09830257b8ce9b7344187e52cacf34930d5c218667b6b94") --Main Windows Depot NUKITASHI 2
+setManifestid(2614071, "3812542003544925509", 744682272)
+--Dlcname NUKITASHI 2 - Append DLC
+addappid(3936700, 1, "70f7f2a45395070764fa6850f0fcde1cba82ee127a0c2bb9f60eb1f7109a9c52") --Dlcname NUKITASHI 2 - Append DLC
+setManifestid(3936700, "396119825101641784", 341934912)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
