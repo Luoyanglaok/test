37e4a5159df8814d22f2a8c81a694aa8be4a32e7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 21:03:27 +0800

    add 2971610.lua

--- 2971610.lua
+++ 2971610.lua
@@ -1,5 +1,11 @@
-addappid(2971610)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2971611,0,"bda4eb90b24a22450e4c9d653dc76a921d76d75528de7dbd5e0b6acaf1d7250a")
-setManifestid(2971611,"5128869389202082730")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2971610.lua
+--Gamename HOLE
+addappid(2971610) --Mainappid HOLE
+addappid(2971611, 1, "bda4eb90b24a22450e4c9d653dc76a921d76d75528de7dbd5e0b6acaf1d7250a") --Main Windows Depot HOLE
+setManifestid(2971611, "5128869389202082730", 438918352)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
