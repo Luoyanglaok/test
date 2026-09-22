a9f8678ab257af83f09257c94af490a8c69feefb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 20:35:02 +0800

    add 2139460.lua

--- 2139460.lua
+++ 2139460.lua
@@ -1,6 +1,13 @@
-addappid(2139460)
-setManifestid(228989,"3514306556860204959")
-addappid(2139461,0,"78a785be75b7bf5ace231ff22287308bed5800f9afecb4b545a180f7f00b9c11")
-setManifestid(2139461,"1829494746786925603")
-addappid(2139463,0,"ad7c624324a12728521ce39b4e13e9a9bd4df554bc77e25484ade5072f77615e")
-setManifestid(2139463,"3154617151518362545")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2139460.lua
+--Gamename Once Human
+addappid(2139460, 1, "a696e84f78512fdb99546b646ba1eff529fdb20263d0913882ac0c52387c4ec0") --Mainappid Once Human
+addappid(2139461, 1, "78a785be75b7bf5ace231ff22287308bed5800f9afecb4b545a180f7f00b9c11") --Main Windows Depot Once Human
+setManifestid(2139461, "1363805858244504688", 113012017696)
+addappid(2139463, 1, "ad7c624324a12728521ce39b4e13e9a9bd4df554bc77e25484ade5072f77615e") --Main Windows Depot Once Human
+setManifestid(2139463, "5817889686985889808", 113013750048)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(3379260) --Dlcname Once Human - Double Agent Theme Pack
+addappid(3816110) --Dlcname Once Human - Ascend Daily Theme Pack
