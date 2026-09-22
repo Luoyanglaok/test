1acdcb1ef48361471c8d6ef0c03b963c3fef9abd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 12:34:11 +0800

    add 1654660.lua

--- 1654660.lua
+++ 1654660.lua
@@ -1,5 +1,15 @@
-addappid(1654660)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1654661,0,"5b40c516f1699847186ea0cf3558f3a94137c7b55f85808a3ed2d88dbe4850a8")
-setManifestid(1654661,"8942731442814496327")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1654660.lua
+--Gamename En Garde!
+addappid(1654660) --Mainappid En Garde!
+addappid(1654661, 1, "5b40c516f1699847186ea0cf3558f3a94137c7b55f85808a3ed2d88dbe4850a8") --Main Windows Language Depot English En Garde!
+setManifestid(1654661, "8942731442814496327", 7032655488)
+--Dlcname En Garde! Soundtrack
+addappid(2244510) --Dlcname En Garde! Soundtrack
+addappid(2244511, 1, "38deae40f38d52bc0f6356e5a83448433a7c54d4ecb980518abfb19b7085d708") --Dlc Windows Depot En Garde! Soundtrack
+setManifestid(2244511, "2216208425088354022", 95626336)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
