43f64a7b5fb04814b47d10ef0bccd505550b5cc5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-22 17:21:45 +0800

    add 629520.lua

--- 629520.lua
+++ 629520.lua
@@ -1,5 +1,9 @@
-addappid(629520)
-addappid(629521,0,"40df4fcb0748caf1d77bff732f597eebb26b3a36539b1646d149f54eb676807f")
-setManifestid(629521,"7342330603713847775")
-addappid(629522,0,"832c15ab1f9cffda454f899b8d69ce24ae020d79dadfcaf97cb9b4494698b4cf")
-setManifestid(629522,"4968508765236736475")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 629520.lua
+--Gamename Soundpad
+addappid(629520, 1, "c437ad2314a89b2d736db77e137dd9afc1a597b34162483465c358a4a39408c2") --Mainappid Soundpad
+addtoken(629520, "6704532945332869425")
+addappid(629521, 1, "40df4fcb0748caf1d77bff732f597eebb26b3a36539b1646d149f54eb676807f") --Main Windows Depot Soundpad
+setManifestid(629521, "1446460963760793101", 6850640)
+addappid(629522, 1, "832c15ab1f9cffda454f899b8d69ce24ae020d79dadfcaf97cb9b4494698b4cf") --Main Windows Depot Soundpad
+setManifestid(629522, "946766300274500046", 6294272)
