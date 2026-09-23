549cd64bd7b85afe4d7e5644b7d2075bede96976
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 21:14:30 +0800

    add 311560.lua

--- 311560.lua
+++ 311560.lua
@@ -1,14 +1,40 @@
-addappid(311560)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(311561,0,"42bdf94957d5fccbb06dae4e835998ffaacee0eb6e08ffbbd77ad7d0be282380")
-setManifestid(311561,"7137976438714669362")
-addappid(311562,0,"2bcc03afbf14f7a2d4121ef3675f20cf48d8038648a0e3187702620bbcdac0cb")
-setManifestid(311562,"5707643107944744968")
-addappid(311563,0,"01ba3fc561b0e872c5abc7c280cc156824389c2cf677290c267ccf412a2cc2dd")
-setManifestid(311563,"865781276612952734")
-addappid(311564,0,"b49726ea1d301607f3707bf48d31d1a5a10be3344879869e99b04544be782e53")
-setManifestid(311564,"2890088491324757649")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 311560.lua
+--Gamename Assassin’s Creed® Rogue
+addappid(311560, 1, "b06163921482154f38801b93d9217c556d4f21ecfa1f1a37b6cc1b8c85f14ee7") --Mainappid Assassin’s Creed® Rogue
+addappid(311561, 1, "42bdf94957d5fccbb06dae4e835998ffaacee0eb6e08ffbbd77ad7d0be282380") --Main Windows Depot Assassin’s Creed® Rogue
+setManifestid(311561, "7137976438714669362", 8754046368)
+addappid(311562, 1, "2bcc03afbf14f7a2d4121ef3675f20cf48d8038648a0e3187702620bbcdac0cb") --Main Windows Depot Assassin’s Creed® Rogue
+setManifestid(311562, "5707643107944744968", 64)
+addappid(311563, 1, "01ba3fc561b0e872c5abc7c280cc156824389c2cf677290c267ccf412a2cc2dd") --Main Windows Depot Assassin’s Creed® Rogue
+setManifestid(311563, "865781276612952734", 64)
+addappid(311564, 1, "b49726ea1d301607f3707bf48d31d1a5a10be3344879869e99b04544be782e53") --Main Windows Depot Assassin’s Creed® Rogue
+setManifestid(311564, "2890088491324757649", 64)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(339030) --Dlcname Assassin's Creed Rogue - Templar Legacy Pack
+addappid(339031) --Dlcname Assassin's Creed Rogue – Activities Pack
+addappid(339032) --Dlcname Assassin's Creed Rogue – Collectibles Pack
+addappid(339033) --Dlcname Assassin's Creed Rogue – Resources Pack
+addappid(339034) --Dlcname Assassin's Creed Rogue – Technology Pack
+addappid(352920) --Dlcname AC Rogue pre-order WW - Uplay activation
+addappid(352921) --Dlcname AC Rogue pre-order RU - Uplay activation
+addappid(352922) --Dlcname AC Rogue pre-order ASIA - Uplay activation
+addappid(352923) --Dlcname AC Rogue Deluxe WW - Uplay activation
+addappid(352924) --Dlcname AC Rogue Deluxe RU - Uplay activation
+addappid(352925) --Dlcname AC Rogue Deluxe ASIA - Uplay activation
+addappid(352926) --Dlcname AC Rogue WW - Uplay activation
+addappid(352927) --Dlcname AC Rogue RU - Uplay activation
+addappid(352928) --Dlcname AC Rogue ASIA - Uplay activation
+addappid(352940) --Dlcname AC Rogue Activities Pack - Uplay Activation
+addappid(352941) --Dlcname AC Rogue Collectibles Pack - Uplay Activation
+addappid(352942) --Dlcname AC Rogue Resources Pack - Uplay Activation
+addappid(352943) --Dlcname AC Rogue Technology Pack - Uplay Activation
+addappid(352944) --Dlcname AC Rogue Templar Legacy Pack - Uplay Activation
