746874694062277d2edb9968139529634a70253f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 15:52:55 +0800

    add 3639070.lua

--- 3639070.lua
+++ 3639070.lua
@@ -3,5 +3,5 @@
 --Gamename BLACKWOOD
 addappid(3639070) --Mainappid BLACKWOOD
 addappid(3639071, 1, "a8bc6cbde85155fc1ca0e66d9af8e245cc341495b27ec7175fa1ac3f7f21df92") --Main Windows Depot BLACKWOOD
-setManifestid(3639071, "4842142721058281602", 11453104976)
+setManifestid(3639071, "3947545620758041571", 11454176864)
 addappid(5237010) --Dlcname Supporter Edition
