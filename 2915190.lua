9fba1c607719e44f770d1adaa6c7eb36330d688b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 21:45:27 +0800

    add 2915190.lua

--- 2915190.lua
+++ 2915190.lua
@@ -1,5 +1,11 @@
-addappid(2915190)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2915191,0,"453ea4d7a13cc5bb00319773cb19ede8435eb7e53ff7f1f5eb990f551d318895")
-setManifestid(2915191,"4952402859784308897")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2915190.lua
+--Gamename 連鎖反響
+addappid(2915190) --Mainappid 連鎖反響
+addappid(2915191, 1, "453ea4d7a13cc5bb00319773cb19ede8435eb7e53ff7f1f5eb990f551d318895") --Main Windows Depot 連鎖反響
+setManifestid(2915191, "4952402859784308897", 1336026592)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
