d6604fea86271f6236869026c34c3c66513d2e2f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 20:46:45 +0800

    add 3319700.lua

---
+++ 3319700.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3319700.lua
+--Gamename 新世界：暗影成双
+addappid(3319700) --Mainappid 新世界：暗影成双
+addappid(3319701, 1, "5e4184593d27fe5400f053ab89e7f4fd86eadfe58c498336df33d548827a4efc") --Main Windows Depot 新世界：暗影成双
+setManifestid(3319701, "5837032937481974742", 23336350304)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
