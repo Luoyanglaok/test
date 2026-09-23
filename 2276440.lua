24949f43ea89c9b701f97728c1551a709683b909
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 15:49:14 +0800

    add 2276440.lua

--- 2276440.lua
+++ 2276440.lua
@@ -1,5 +1,11 @@
-addappid(2276440)
-setManifestid(228989,"550968249685141759")
-setManifestid(228990,"1829726630299308803")
-addappid(2276441,0,"666f16ca1dd0c34bf6659cca548d2214ef2ecb8523ee2e2864446354363de3d5")
-setManifestid(2276441,"2275618559019301991")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2276440.lua
+--Gamename 鞭炮&烟花：春节模拟器Firecrackers&fireworks：china new year simulation
+addappid(2276440) --Mainappid 鞭炮&烟花：春节模拟器Firecrackers&fireworks：china new year simulation
+addappid(2276441, 1, "666f16ca1dd0c34bf6659cca548d2214ef2ecb8523ee2e2864446354363de3d5") --Main Windows Depot 鞭炮&烟花：春节模拟器Firecrackers&fireworks：china new year simulation
+setManifestid(2276441, "7169139170504742929", 5940798864)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
