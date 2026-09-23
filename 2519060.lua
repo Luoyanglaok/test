978d59628505528ac846bc8d8f74298132b5f062
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 08:56:24 +0800

    add 2519060.lua

--- 2519060.lua
+++ 2519060.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 2519060.lua
-addappid(2519060, 0, "22f4bdcf17bef27123fc7be9d56cd5a9de28c4de7e8e1e1a6fa0cbf15770c618")
+addappid(2519060, 0, "a252a4dd002cb3682ada09f6c386671459328f172697a2427541e6f47127f958")
