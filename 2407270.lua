8f5394f6edd17f1caa7e903b943daeda434f21df
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 11:40:41 +0800

    add 2407270.lua

--- 2407270.lua
+++ 2407270.lua
@@ -3,7 +3,7 @@
 --Gamename AI LIMIT
 addappid(2407270, 1, "2615d909118b737d77236fb50a3dd435f355cc2190c1a6ad30a54a3bd11955cc") --Mainappid AI LIMIT
 addappid(2407271, 1, "7fbec0c07311645df5048184f930dab908082e6a96be2f0814a3159eaa8de5a6") --Main Windows Depot AI LIMIT
-setManifestid(2407271, "118013794508952302", 19998821456)
+setManifestid(2407271, "3772203960994697774", 21309543760)
 addappid(3391010) --Dlcname AI LIMIT - Deluxe Edition Upgrade Pack
 --Missing Dlcs
 --Dlcname AI LIMIT - Eirene's Furnace of War
