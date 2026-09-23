36bd217328c2f76c7dd5b6bc2b889a43354e5626
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 23:03:16 +0800

    add 1238840.lua

--- 1238840.lua
+++ 1238840.lua
@@ -1,33 +1,59 @@
-addappid(1238840)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228990,"1829726630299308803")
-addappid(1238841,0,"cb8c758a6b306785d05f63438a35661ee32cdf1d1cff46d1fbd1b2b9ae6c77e5")
-setManifestid(1238841,"1819202721014430225")
-addappid(1238842,0,"01d2655d5a322102278109ba650ef63f0af559005c03c42f488fa9ea838c5360")
-setManifestid(1238842,"8603388110745960936")
-addappid(1238843,0,"ef63e230db178de5ba83fe6db0e126e49ea50b5b013c3a9446c196d76a0a188e")
-setManifestid(1238843,"311816778127462016")
-addappid(1238844,0,"563c64c4a8a03b132f383a1b283f4dede43590fbf82517f56ee0dd7172cafc11")
-setManifestid(1238844,"4895827690700060475")
-addappid(1238845,0,"1d36de8d8f5ca5ae60eeda4a3d622884bd05a8aa462fb98d56e2ec926baedc10")
-setManifestid(1238845,"2329135298461803691")
-addappid(1238846,0,"9f72127c998d8db2f4118827aef531fffd5ad1ff33ce6bdb55249ce4de669f65")
-setManifestid(1238846,"3226547397988750463")
-addappid(1238847,0,"50851f77684e1cab0648488e5630ee614085e5cec453fe5c0f57b97dc31d756e")
-setManifestid(1238847,"8471862724280977916")
-addappid(1238848,0,"7a3effb7b26ecf44ae09dce6df39f2332990f6d0b726aa1ed58b4bed0e4b7fd4")
-setManifestid(1238848,"5857397183589548065")
-addappid(1238849,0,"515d63c59eb085af6028f57b150a01b3328092809a46703898535f551c716c78")
-setManifestid(1238849,"3350782854890882555")
-addappid(1238852,0,"a4f7c4516dc72e16700c4b4deac33c20287074574decf9b49369653662e721eb")
-setManifestid(1238852,"6876244450189232280")
-addappid(1238853,0,"24c8d7cafd56006a415371f91b4a2a6077ecf8099f3fdc99d7740fd026690518")
-setManifestid(1238853,"5281643831187131300")
-addappid(1238854,0,"ca70e1e20dc6794bc88d442fe505eb85c5142cc741a1a7169ccf753678a4e869")
-setManifestid(1238854,"8122760505266740638")
-addappid(1238855,0,"8c35c4d65346b6f733c667251a3ef34e990280a9546b1be048e6f0e4424b0988")
-setManifestid(1238855,"7374486922401189348")
-addappid(1238856,0,"18125f3b57aa45e546e63f41fa0182224fe3a06ce4b0484f58d4769232ac4cfd")
-setManifestid(1238856,"1280352447725441193")
-addappid(3340991,0,"023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491")
-setManifestid(3340991,"1318423300601359782")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1238840.lua
+--Gamename Battlefield™ 1
+addappid(1238840, 1, "70bc92454053004da7a4dcac6819287d445e1f1858550bcd09fdd9c890dccbb8") --Mainappid Battlefield™ 1
+addappid(1238841, 1, "cb8c758a6b306785d05f63438a35661ee32cdf1d1cff46d1fbd1b2b9ae6c77e5") --Main Windows Depot Battlefield™ 1
+setManifestid(1238841, "1819202721014430225", 63628162400)
+addappid(1238842, 1, "01d2655d5a322102278109ba650ef63f0af559005c03c42f488fa9ea838c5360") --Main Windows Language Depot English Battlefield™ 1
+setManifestid(1238842, "8603388110745960936", 34848)
+addappid(1238843, 1, "ef63e230db178de5ba83fe6db0e126e49ea50b5b013c3a9446c196d76a0a188e") --Main Windows Language Depot French Battlefield™ 1
+setManifestid(1238843, "311816778127462016", 39792)
+addappid(1238844, 1, "563c64c4a8a03b132f383a1b283f4dede43590fbf82517f56ee0dd7172cafc11") --Main Windows Language Depot German Battlefield™ 1
+setManifestid(1238844, "4895827690700060475", 35056)
+addappid(1238845, 1, "1d36de8d8f5ca5ae60eeda4a3d622884bd05a8aa462fb98d56e2ec926baedc10") --Main Windows Language Depot Spanish Battlefield™ 1
+setManifestid(1238845, "2329135298461803691", 38736)
+addappid(1238846, 1, "9f72127c998d8db2f4118827aef531fffd5ad1ff33ce6bdb55249ce4de669f65") --Main Windows Language Depot Italian Battlefield™ 1
+setManifestid(1238846, "3226547397988750463", 36800)
+addappid(1238847, 1, "50851f77684e1cab0648488e5630ee614085e5cec453fe5c0f57b97dc31d756e") --Main Windows Language Depot Russian Battlefield™ 1
+setManifestid(1238847, "8471862724280977916", 49040)
+addappid(1238848, 1, "7a3effb7b26ecf44ae09dce6df39f2332990f6d0b726aa1ed58b4bed0e4b7fd4") --Main Windows Language Depot Polish Battlefield™ 1
+setManifestid(1238848, "5857397183589548065", 42576)
+addappid(1238849, 1, "515d63c59eb085af6028f57b150a01b3328092809a46703898535f551c716c78") --Main Windows Language Depot Japanese Battlefield™ 1
+setManifestid(1238849, "3350782854890882555", 53616)
+addappid(1238852, 1, "a4f7c4516dc72e16700c4b4deac33c20287074574decf9b49369653662e721eb") --Main Windows Language Depot Tchinese Battlefield™ 1
+setManifestid(1238852, "6876244450189232280", 53600)
+addappid(1238853, 1, "24c8d7cafd56006a415371f91b4a2a6077ecf8099f3fdc99d7740fd026690518") --Main Windows Language Depot Brazilian Battlefield™ 1
+setManifestid(1238853, "5281643831187131300", 37920)
+addappid(1238854, 1, "ca70e1e20dc6794bc88d442fe505eb85c5142cc741a1a7169ccf753678a4e869") --Main Windows Language Depot Latam Battlefield™ 1
+setManifestid(1238854, "8122760505266740638", 38256)
+addappid(1238855, 1, "8c35c4d65346b6f733c667251a3ef34e990280a9546b1be048e6f0e4424b0988") --Main Windows Language Depot Arabic Battlefield™ 1
+setManifestid(1238855, "7374486922401189348", 55472)
+addappid(1238856, 1, "18125f3b57aa45e546e63f41fa0182224fe3a06ce4b0484f58d4769232ac4cfd") --Main Windows Language Depot Turkish Battlefield™ 1
+setManifestid(1238856, "1280352447725441193", 41968)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") --Share Windows Depot EA APP Installer
+setManifestid(3340991, "3755387105551220105", 246722480)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6") --Share Windows Depot EA Javelin Anticheat Installer
+setManifestid(3893181, "4878189275181659469", 317406032)
+addappid(1238850) --Dlcname Battlefield™ 1 Premium Pass
+addappid(1238851) --Dlcname Battlefield 1 ™ Shortcut Kit: Ultimate Bundle
+addappid(1259490) --Dlcname Battlefield 1 - Key
+addappid(1259491) --Dlcname Battlefield 1 Revolution - Key
+addappid(1259492) --Dlcname Battlefield 1 They Shall Not Pass
+addappid(1259493) --Dlcname Battlefield 1 Turning Tides
+addappid(1259494) --Dlcname Battlefield 1 Apocalypse
+addappid(1259495) --Dlcname Battlefield 1 In the Name of the Tsar
+addappid(1259496) --Dlcname Battlefield 1 Hellfighter Pack
+addappid(1259497) --Dlcname Battlefield 1 Expansion Pack
+addappid(1259498) --Dlcname Battlefield 1 Heroes of the Great War Bundle
+addappid(1259499) --Dlcname Battlefield 1 Deluxe Upgrade
+addappid(1314760) --Dlcname Battlefield 1 Shortcut Kit: Assault Bundle
+addappid(1314761) --Dlcname Battlefield 1 Shortcut Kit: Support Bundle
+addappid(1314762) --Dlcname Battlefield 1 Shortcut Kit: Medic Bundle
+addappid(1314763) --Dlcname Battlefield 1 Shortcut Kit: Scout Bundle
+addappid(1314764) --Dlcname Battlefield 1 Shortcut Kit: Infantry Bundle
+addappid(1314765) --Dlcname Battlefield 1 Shortcut Kit: Vehicle Bundle
