c528c94e3af5583b404d0aa4118c88e0bf578f63
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 15:33:25 +0800

    add 2739590.lua

--- 2739590.lua
+++ 2739590.lua
@@ -1,3 +1,6 @@
-addappid(2739590)
-addappid(2739591,0,"b4965a72b078a66e012d79a8af2548c3201e72b2fc234cc1e562863063dc42b8")
-setManifestid(2739591,"7141466826678395833")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2739590.lua
+--Gamename Mad Island
+addappid(2739590, 1, "cbed7a4927e60f35fa4fc303117841cc0b0480acaedd95fa870f3dc92d474cb5") --Mainappid Mad Island
+addappid(2739591, 1, "b4965a72b078a66e012d79a8af2548c3201e72b2fc234cc1e562863063dc42b8") --Main Windows Language Depot Japanese Mad Island
+setManifestid(2739591, "3045389096088902706", 1232610784)
