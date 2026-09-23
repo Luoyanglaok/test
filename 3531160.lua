595d6af705351eb02ba23a6f1132118967e19f0c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 23:10:45 +0800

    add 3531160.lua

---
+++ 3531160.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3531160.lua
+--Gamename 荒岛求生：逃出美女岛
+addappid(3531160) --Mainappid 荒岛求生：逃出美女岛
+addappid(3531161, 1, "dfe7bdcdc7bd94813f7cbe67c4116bed483d6a8a66fb901f3a6200d363d717d9") --Main Windows/Linux Depot 荒岛求生：逃出美女岛
+setManifestid(3531161, "645640092798702039", 52528345680)
