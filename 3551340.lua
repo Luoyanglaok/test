bd220ffd7ecac48abb75012e357c9dbea5554398
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 21:11:24 +0800

    add 3551340.lua

---
+++ 3551340.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3551340.lua
+--Gamename Football Manager 26
+addappid(3551340, 1, "052016d88278aada4bc94cc5d3fbef162105991acf65634e80e5fb0fb5fd3304") --Mainappid Football Manager 26
+addappid(3551341, 1, "a4f9c46a0797978fa0b278dac08a0f473872fc74c6b6d9bf45b30d56e33a42c7") --Main Windows Depot Football Manager 26
+setManifestid(3551341, "7759922098115499616", 64)
+addappid(3551342, 1, "31edad03a3daebca77afad83adb899f2ff6611d949738ad3d15e708da8f2c7ca") --Main Windows Depot Football Manager 26
+setManifestid(3551342, "4286155556537576831", 7028007552)
+addappid(3551343, 1, "5ff6a006dff3e4ea6364310a8390ffc5f4754c3928150ea5719009c21074a607") --Main Macos Depot Football Manager 26
+setManifestid(3551343, "6975117889499650380", 6976643984)
+addappid(3551410) --Dlcname Football Manager 26 In-Game Editor
