2805dbaa4bfffcf34b3f806438cc25b6cc1be006
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 15:53:10 +0800

    add 876140.lua

---
+++ 876140.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 876140.lua
+--Gamename Police Air Transporter
+addappid(876140) --Mainappid Police Air Transporter
+addappid(876141, 1, "04a8cda35cd4d5d8e6c180eb162793ec30d8c5b50bc2a762cd212643c284e56a") --Main Windows Depot Police Air Transporter
+setManifestid(876141, "8561738150554909459", 136504224)
