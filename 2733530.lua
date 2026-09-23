4ce93301ce26c5329623bb97c802d6d34a265001
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 23:08:06 +0800

    add 2733530.lua

--- 2733530.lua
+++ 2733530.lua
@@ -1,3 +1,6 @@
-addappid(2733530)
-addappid(2733531,0,"1d344432dd091005e000084dde9b60c6722ddeebf3a25b3daca472340de728e5")
-setManifestid(2733531,"8629877682254750407")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2733530.lua
+--Gamename 别跑！美女在追你！
+addappid(2733530) --Mainappid 别跑！美女在追你！
+addappid(2733531, 1, "1d344432dd091005e000084dde9b60c6722ddeebf3a25b3daca472340de728e5") --Main Windows Depot 别跑！美女在追你！
+setManifestid(2733531, "8629877682254750407", 2989450464)
