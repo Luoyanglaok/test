d1874975d4890eb7f98a8b8e3a9c505b5d5c781b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 20:58:33 +0800

    add 316810.lua

--- 316810.lua
+++ 316810.lua
@@ -1,9 +1,12 @@
-addappid(316810)
-addappid(316811,0,"2a80381a49899badbc3933f9da91ef6e6e361d6bfdb5c13f3d492d4ff47023c7")
-setManifestid(316811,"1092829407038564972")
-addappid(316812,0,"5ed4365a7d01c9c6e392e07b99be40a1433b9b7e2eaaff5bf117bcebc86a82c5")
-setManifestid(316812,"1409218656314256486")
-addappid(316813,0,"833774082e4fac92ed071cf7bdee5175dd40e6faddd2c703afc87382f3939d78")
-setManifestid(316813,"6977293308910525183")
-addappid(316814,0,"cd2ce72cfd8eb2ee9f330a5cb43da222718123075cd21220e0337543588dda89")
-setManifestid(316814,"1361561010276718876")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 316810.lua
+--Gamename Shadowcrypt
+addappid(316810) --Mainappid Shadowcrypt
+addappid(316811, 1, "2a80381a49899badbc3933f9da91ef6e6e361d6bfdb5c13f3d492d4ff47023c7") --Main Windows Language Depot English Shadowcrypt
+setManifestid(316811, "1092829407038564972", 0)
+addappid(316812, 1, "5ed4365a7d01c9c6e392e07b99be40a1433b9b7e2eaaff5bf117bcebc86a82c5") --Main Macos Language Depot English Shadowcrypt
+setManifestid(316812, "1409218656314256486", 0)
+addappid(316813, 1, "833774082e4fac92ed071cf7bdee5175dd40e6faddd2c703afc87382f3939d78") --Main Windows Language Depot English Shadowcrypt
+setManifestid(316813, "6977293308910525183", 81558832)
+addappid(316814, 1, "cd2ce72cfd8eb2ee9f330a5cb43da222718123075cd21220e0337543588dda89") --Main Macos Language Depot English Shadowcrypt
+setManifestid(316814, "1361561010276718876", 85802432)
