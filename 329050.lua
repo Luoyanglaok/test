7461d72a699c27a1fd23a6e713a070966e76367f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 13:35:57 +0800

    add 329050.lua

--- 329050.lua
+++ 329050.lua
@@ -1,15 +1,39 @@
-addappid(329050)
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-addappid(329051,0,"4da524a16eb2bb8577d094a37394176b10de35a42d6bcdecdd5c5115a324f2f3")
-setManifestid(329051,"7950608430955718471")
-addappid(329052,0,"8c64bb7a4690a8c03abdbdd37d6b8f2facd53e84b65bee01f5fc4ba7cbf820c4")
-setManifestid(329052,"1243308987442157279")
-addappid(329063,0,"0fa835c37520b9ba38589e7501bcc412d60c892c4e4545dab524cdd986092179")
-setManifestid(329063,"8424973057200794724")
-addappid(329064,0,"fc5e4126a6feb60b108669709281b0f8c7421f717cfbe6c38e2fe20a2c33bee6")
-setManifestid(329064,"8930563273857639221")
-addappid(359496,0,"5eeebd35182e7d89c2849a263992ec597c582ae9ca3b387426064c8981aa8d0f")
-setManifestid(359496,"7575729025602940829")
-addappid(329059,0,"b337f8a7dab3433f577c958de8297930a64e9d4e26afcb89decc17d68842720a")
-setManifestid(329059,"1258899926408907550")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 329050.lua
+--Gamename Devil May Cry 4 Special Edition
+addappid(329050, 1, "6027587a1940a21f3d1ed3a266f9a2228c79deda0ebe4653a3b90f5515c5e7d7") --Mainappid Devil May Cry 4 Special Edition
+addappid(329051, 1, "4da524a16eb2bb8577d094a37394176b10de35a42d6bcdecdd5c5115a324f2f3") --Main Windows Depot Devil May Cry 4 Special Edition
+setManifestid(329051, "2367310862188190194", 11766288)
+addappid(329052, 1, "8c64bb7a4690a8c03abdbdd37d6b8f2facd53e84b65bee01f5fc4ba7cbf820c4") --Main Windows Depot Devil May Cry 4 Special Edition
+setManifestid(329052, "3932042514805072469", 13572776816)
+addappid(329059, 1, "b337f8a7dab3433f577c958de8297930a64e9d4e26afcb89decc17d68842720a") --Main Windows Depot Devil May Cry 4 Special Edition
+setManifestid(329059, "1258899926408907550", 0)
+addappid(329063, 1, "0fa835c37520b9ba38589e7501bcc412d60c892c4e4545dab524cdd986092179") --Main Windows Depot Devil May Cry 4 Special Edition
+setManifestid(329063, "7604912158803741445", 11766432)
+addappid(329064, 1, "fc5e4126a6feb60b108669709281b0f8c7421f717cfbe6c38e2fe20a2c33bee6") --Main Windows Depot Devil May Cry 4 Special Edition
+setManifestid(329064, "3712214782024545292", 501034336)
+--Dlcname Lady & Trish Costumes
+addappid(359496, 1, "5eeebd35182e7d89c2849a263992ec597c582ae9ca3b387426064c8981aa8d0f") --Dlcname Lady & Trish Costumes
+setManifestid(359496, "8396830996349325680", 202862016)
+--Dlcname Devil May Cry 4 Original Soundtrack
+addappid(1426690) --Dlcname Devil May Cry 4 Original Soundtrack
+addappid(1426691, 1, "d02118b3c80d3b01bffd39ee7c50b90dd70099191275ab76facf7b770f9bde60") --Dlc Windows Depot Devil May Cry 4 Original Soundtrack
+setManifestid(1426691, "6487366122747034326", 467033232)
+addappid(1426692, 1, "14ae56906f5b5765b721dbcc0dcd4e9b89ab3cd175ef694af1b83184527ca066") --Dlc Windows Depot Devil May Cry 4 Original Soundtrack
+setManifestid(1426692, "8236156712971791488", 1373023856)
+addappid(1426693, 1, "fa02f3a573c1429ac81389a2f7c7805bc2827ff938fc57408313bc8ec9cf6159") --Dlc Windows Depot Devil May Cry 4 Original Soundtrack
+setManifestid(1426693, "4596843764624354532", 610650944)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(359490) --Dlcname Unlock All Modes
+addappid(359491) --Dlcname S.Nero/Dante/Vergil
+addappid(359492) --Dlcname 1 Blue Orb
+addappid(359493) --Dlcname 2 Blue Orbs
+addappid(359494) --Dlcname 5 Blue Orbs
+addappid(359495) --Dlcname Proud Souls (200,000)
+addappid(359497) --Dlcname Red Orbs (300,000)
+--Missing Dlcs
+--Dlcname Premium Movie (Japan only)
