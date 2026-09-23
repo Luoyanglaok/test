7ceec72e4a9f295c1fa0aa3cc13791c94deee356
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 19:37:50 +0800

    add 3669870.lua

---
+++ 3669870.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3669870.lua
+--Gamename CONTROL Resonant
+addappid(3669870, 1, "e53df1456a5d9cb4244af0790bfecf0b7f24217faf8a20276ce03b302187f51e") --Mainappid CONTROL Resonant
+addtoken(3669870, "18274421760595271558")
+addappid(3669871, 1, "47ad456f9070a97f6206e13818d3588f48e26d8d94675b45f481b32ae2fc0bd5") --Main Windows Depot CONTROL Resonant
