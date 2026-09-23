113740629d7b20782c6840c72c452dceb37979b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 23:06:58 +0800

    add 2753730.lua

--- 2753730.lua
+++ 2753730.lua
@@ -1,3 +1,6 @@
-addappid(2753730)
-addappid(2753731,0,"05e55d53b400c7f91750a8cf8c1a9eac2b74c0ba9c09ce7e7ab75172697bb9a1")
-setManifestid(2753731,"7581072464108515317")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2753730.lua
+--Gamename 美女,应接不暇
+addappid(2753730) --Mainappid 美女,应接不暇
+addappid(2753731, 1, "05e55d53b400c7f91750a8cf8c1a9eac2b74c0ba9c09ce7e7ab75172697bb9a1") --Main Windows Depot 美女,应接不暇
+setManifestid(2753731, "7581072464108515317", 12058411760)
