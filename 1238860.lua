41c0ca883bd236a1dba0a50188f13b70cb98743a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 22:37:02 +0800

    add 1238860.lua

--- 1238860.lua
+++ 1238860.lua
@@ -1,31 +1,157 @@
-addappid(1238860)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(1238861,0,"a70aeb51423fa65eff9fb75092a3bafbedf41c2f0cc4f66caee89c3d31be99d2")
-setManifestid(1238861,"5790550315647961519")
-addappid(1238862,0,"4c0d025b655f6d76c0118b94dfdb05170f5e030dcd87687c1415976908f2b07d")
-setManifestid(1238862,"4134516474567351318")
-addappid(1238863,0,"9abb028584fd39382a972e489d5b0e437664b94458b65aa9cdba5d163f2821f4")
-setManifestid(1238863,"1179971805763842997")
-addappid(1238864,0,"434c27900b081b42c0dbdee98ee7d2096307f6f0c1c3f4d29b0c290506e60f05")
-setManifestid(1238864,"1884370580307172439")
-addappid(1238865,0,"82b5da1d97ace868a8b27c25927ec77928a9ef159c7cb4975995be22a7ab0826")
-setManifestid(1238865,"3561209213128132980")
-addappid(1238866,0,"fd77ebbbd8bc560070ffc11a6f94e1d18de714341cb4e0dad41201d09863d3c3")
-setManifestid(1238866,"5082204154841506598")
-addappid(1238867,0,"2e09ae88bf21599995be746fcf13f5918f4c7d65c17ca8d2edbc00c31bc58ec4")
-setManifestid(1238867,"3874316708377682985")
-addappid(1238868,0,"2e231f6c99fac90432559ae3b51ad05a34c1903704e16c9240e288e03854b6ba")
-setManifestid(1238868,"8042912031614746500")
-addappid(1238869,0,"c68272de8a3e563d022b970329af67fa3c2f0f35313276f085ebad2f3a4684c5")
-setManifestid(1238869,"8139537678996583048")
-addappid(1238872,0,"9743c66c09d3c6e0e52fdb5cfed0341e621e36f25575576de5d9c211cfa58b6c")
-setManifestid(1238872,"7645850408715557464")
-addappid(1238873,0,"776f34401a8172781ef5cc608f59dd20718fb644071a1daed61aa86bfee80813")
-setManifestid(1238873,"7370055068262480597")
-addappid(1238874,0,"4663f85a8f01aa04196e1e39cddf285a44d5f46520dc2c9db18d7c957bafb443")
-setManifestid(1238874,"3410540413806087694")
-addappid(1238875,0,"38f003a48cd1e5bc78b2bac043fa502b896d52c99e66199978cb29b2689f21ff")
-setManifestid(1238875,"6644320650706456549")
-addappid(3340991,0,"023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491")
-setManifestid(3340991,"1318423300601359782")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1238860.lua
+--Gamename Battlefield 4™
+addappid(1238860, 1, "58ddf0a37c6062a924e46290df04dd9e6bfb5ffa55bd639c07a460f0aa4463c7") --Mainappid Battlefield 4™
+addappid(1238861, 1, "a70aeb51423fa65eff9fb75092a3bafbedf41c2f0cc4f66caee89c3d31be99d2") --Main Windows Depot Battlefield 4™
+setManifestid(1238861, "5790550315647961519", 24228554304)
+addappid(1238862, 1, "4c0d025b655f6d76c0118b94dfdb05170f5e030dcd87687c1415976908f2b07d") --Main Windows Language Depot English Battlefield 4™
+setManifestid(1238862, "4134516474567351318", 783073136)
+addappid(1238863, 1, "9abb028584fd39382a972e489d5b0e437664b94458b65aa9cdba5d163f2821f4") --Main Windows Language Depot French Battlefield 4™
+setManifestid(1238863, "1179971805763842997", 1148030480)
+addappid(1238864, 1, "434c27900b081b42c0dbdee98ee7d2096307f6f0c1c3f4d29b0c290506e60f05") --Main Windows Language Depot Italian Battlefield 4™
+setManifestid(1238864, "1884370580307172439", 1167691232)
+addappid(1238865, 1, "82b5da1d97ace868a8b27c25927ec77928a9ef159c7cb4975995be22a7ab0826") --Main Windows Language Depot German Battlefield 4™
+setManifestid(1238865, "3561209213128132980", 1195795200)
+addappid(1238866, 1, "fd77ebbbd8bc560070ffc11a6f94e1d18de714341cb4e0dad41201d09863d3c3") --Main Windows Language Depot Spanish Battlefield 4™
+setManifestid(1238866, "5082204154841506598", 1199435680)
+addappid(1238867, 1, "2e09ae88bf21599995be746fcf13f5918f4c7d65c17ca8d2edbc00c31bc58ec4") --Main Windows Language Depot Polish Battlefield 4™
+setManifestid(1238867, "3874316708377682985", 652141840)
+addappid(1238868, 1, "2e231f6c99fac90432559ae3b51ad05a34c1903704e16c9240e288e03854b6ba") --Main Windows Language Depot Russian Battlefield 4™
+setManifestid(1238868, "8042912031614746500", 710352128)
+addappid(1238869, 1, "c68272de8a3e563d022b970329af67fa3c2f0f35313276f085ebad2f3a4684c5") --Main Windows Language Depot Brazilian Battlefield 4™
+setManifestid(1238869, "8139537678996583048", 1181824080)
+addappid(1238872, 1, "9743c66c09d3c6e0e52fdb5cfed0341e621e36f25575576de5d9c211cfa58b6c") --Main Windows Language Depot Japanese Battlefield 4™
+setManifestid(1238872, "7645850408715557464", 1167847744)
+addappid(1238873, 1, "776f34401a8172781ef5cc608f59dd20718fb644071a1daed61aa86bfee80813") --Main Windows Language Depot Czech Battlefield 4™
+setManifestid(1238873, "7370055068262480597", 783359344)
+addappid(1238874, 1, "4663f85a8f01aa04196e1e39cddf285a44d5f46520dc2c9db18d7c957bafb443") --Main Windows Language Depot Koreana Battlefield 4™
+setManifestid(1238874, "3410540413806087694", 783302944)
+addappid(1238875, 1, "38f003a48cd1e5bc78b2bac043fa502b896d52c99e66199978cb29b2689f21ff") --Main Windows Language Depot Tchinese Battlefield 4™
+setManifestid(1238875, "6644320650706456549", 783283872)
+--Dlcname AppID 1272641
+addtoken(1272641, "6480156903692853383")
+addappid(1272641, 1, "4b7c6e263f412fb775d5e07caf36e78a3a33220b089bcdb013af22e3da1c4f16") --Dlcname AppID 1272641
+--Dlcname AppID 1272642
+addtoken(1272642, "8880021263436484369")
+addappid(1272642, 1, "e8bff1661a34550e276551b56b813fa53f6ed08295cfbf0af1aed53bd6eaea5c") --Dlcname AppID 1272642
+--Dlcname AppID 1272643
+addtoken(1272643, "1744884208708993561")
+addappid(1272643, 1, "a8745a79e0e701e42944ae8f346eb502964c661a1a33521385294b5cd88ad142") --Dlcname AppID 1272643
+--Dlcname AppID 1272644
+addtoken(1272644, "16314643703021354892")
+addappid(1272644, 1, "c9f79a91da01a26b55cada38e571969a2e638070484074d104fdc7bfabb00845") --Dlcname AppID 1272644
+--Dlcname AppID 1272645
+addtoken(1272645, "14007976601830084197")
+addappid(1272645, 1, "04b5db856fa893e2bc3d7fb417d2a3905f6ac73f08f549c238ff9efaf377dd1b") --Dlcname AppID 1272645
+--Dlcname Battlefield 4™ Community Operations
+addappid(1272650, 1, "5c7d38f8e9899a9ed1e2ae951070aa3fbf284b6ce70bd7f3ec60451406e717b3") --Dlcname Battlefield 4™ Community Operations
+setManifestid(1272650, "8189153946608321125", 2181874384)
+addappid(1272658, 1, "bad245d97f801f424c404d867afc245a42283dae04dbe9bbdcbc45e422f34c05") --Dlc Windows Language Depot English Battlefield 4™ Community Operations
+setManifestid(1272658, "1434996877352769812", 192)
+addappid(1272659, 1, "8284bca717638233091b3ccb6b7aaee70b6d811b69ae68ba7a3298b1095e5d18") --Dlc Windows Language Depot French Battlefield 4™ Community Operations
+setManifestid(1272659, "2324364489166023688", 192)
+addappid(1277000, 1, "84b01bf6fa77de1fe055656320e1601d12b025b84049c7bc2ac0028f3a7c55cd") --Dlc Windows Language Depot Italian Battlefield 4™ Community Operations
+setManifestid(1277000, "9008508770248271278", 192)
+addappid(1277001, 1, "59f06e98bb1d9ba7b6bc7f66cce897ea68577816e489b17edab97f4496657a9e") --Dlc Windows Language Depot German Battlefield 4™ Community Operations
+setManifestid(1277001, "5627938216587220944", 192)
+addappid(1277002, 1, "a0cf66721ce66acd390a72f4071436393bc7852ca7b69590f7915b683c226957") --Dlc Windows Language Depot Spanish Battlefield 4™ Community Operations
+setManifestid(1277002, "2845838032828787436", 192)
+addappid(1277003, 1, "7dd848dcdb35d33c664617206211fae04819027f8862d5e12eca6a6005b95fc7") --Dlc Windows Language Depot Polish Battlefield 4™ Community Operations
+setManifestid(1277003, "7485616906029489831", 192)
+addappid(1277004, 1, "491421c524af7a28432f8cb0a92fc39a0e6aca287eb7c2b5104ec7b84300e39d") --Dlc Windows Language Depot Russian Battlefield 4™ Community Operations
+setManifestid(1277004, "6438894928042359864", 192)
+addappid(1277005, 1, "ea3b36ab61cb39a7b1bba9f7e2f58cf441aa3eb0dea22d74141b36201616184c") --Dlc Windows Language Depot Brazilian Battlefield 4™ Community Operations
+setManifestid(1277005, "5643239877949005998", 192)
+addappid(1277006, 1, "1c86872bf31b3b46ad0a733be8a70c0b108219da9837a9e6566cd144ea822a46") --Dlc Windows Language Depot Brazilian Battlefield 4™ Community Operations
+setManifestid(1277006, "5821429518505440230", 192)
+addappid(1277007, 1, "ebb6a7bfa044e6f17726228add11d83210067c172de2b0687ae3dc9db9a25bbd") --Dlc Windows Language Depot Czech Battlefield 4™ Community Operations
+setManifestid(1277007, "3464933931786430342", 192)
+addappid(1277008, 1, "01451622f3b2caac69231c834e0e61309a32d285ae4787573fb2140968eab52a") --Dlc Windows Language Depot Koreana Battlefield 4™ Community Operations
+setManifestid(1277008, "7323057193122362050", 192)
+addappid(1277009, 1, "306ea8cf96104676b2415bb4681509380a72e3e1f4d728208c5b98e17260c395") --Dlc Windows Language Depot Tchinese Battlefield 4™ Community Operations
+setManifestid(1277009, "2981626549977600983", 192)
+--Dlcname Battlefield 4™ Night Operations
+addappid(1272654, 1, "27a19a5f4615a2baf3ffdbdf90599572a51c47a0992616e675efac8c33a103c8") --Dlcname Battlefield 4™ Night Operations
+setManifestid(1272654, "1045052835655380351", 547182176)
+addappid(1277020, 1, "39f9dba08679f98da2e386caffe5436b5be81f6f3a87065f62c6cd91a7b92270") --Dlc Windows Language Depot English Battlefield 4™ Night Operations
+setManifestid(1277020, "673014866185137298", 192)
+addappid(1277021, 1, "9eb05c400091204e951dcf02f96055ba451689ca53a2a53510315997d58d99d2") --Dlc Windows Language Depot French Battlefield 4™ Night Operations
+setManifestid(1277021, "8874310982080589143", 192)
+addappid(1277022, 1, "524b24df75c696b37aed7c97a4ee4644858a2ac25481f881ca91962254ea0694") --Dlc Windows Language Depot Italian Battlefield 4™ Night Operations
+setManifestid(1277022, "8292679587716402855", 192)
+addappid(1277023, 1, "1855b9b4f4de3c69586c4e3802e4080b0bdd33340af947e4fdc2490865b8e9b0") --Dlc Windows Language Depot German Battlefield 4™ Night Operations
+setManifestid(1277023, "4546639915991340860", 192)
+addappid(1277024, 1, "60c2c34d28e5fd7671bb5f69cab6f4e789061aef1a4fd0613e6b390972a10b0d") --Dlc Windows Language Depot Spanish Battlefield 4™ Night Operations
+setManifestid(1277024, "6523170316932347708", 192)
+addappid(1277025, 1, "77106d8cee6f13834191b2757ac656464e228f4a9248336f8598b28a8384ee61") --Dlc Windows Language Depot Polish Battlefield 4™ Night Operations
+setManifestid(1277025, "9193391038746787754", 192)
+addappid(1277026, 1, "37b0be4c91d012e1da27b5d1efc11e5280a9d18496da5609d969f428f7eb19a4") --Dlc Windows Language Depot Russian Battlefield 4™ Night Operations
+setManifestid(1277026, "8412430344268926156", 192)
+addappid(1277027, 1, "85b97e14f41d8e06b6226f6546b6a4699658273264fb12d64fde7150991a005e") --Dlc Windows Language Depot Brazilian Battlefield 4™ Night Operations
+setManifestid(1277027, "738803481736316564", 192)
+addappid(1277028, 1, "d3c2b9167531f5fac9db07ba59174137d2d5cf8148ba4780fc01f0faa231a49c") --Dlc Windows Language Depot Japanese Battlefield 4™ Night Operations
+setManifestid(1277028, "5469913080843878539", 192)
+addappid(1277029, 1, "053515ee9bd88846aa4e0eb6dc006bc0b18470c24570173a3dde3b1d40520c20") --Dlc Windows Language Depot Czech Battlefield 4™ Night Operations
+setManifestid(1277029, "4757173540653704648", 192)
+addappid(1277030, 1, "89623331537c42ea1b73ec6a9be935e5685027437c04d10cc5c6e5e58b49803c") --Dlc Windows Language Depot Koreana Battlefield 4™ Night Operations
+setManifestid(1277030, "8092221007795059891", 192)
+addappid(1277031, 1, "858aedda6f785500f8dbe6ec50fc986d7b5603dcba0db1432f014964873b79e8") --Dlc Windows Language Depot Tchinese Battlefield 4™ Night Operations
+setManifestid(1277031, "2100212834282816064", 192)
+--Dlcname Battlefield 4™ Legacy Operations
+addappid(1272657, 1, "ce870122f2802b867a41de7d8b3ce9aa0de5af61a264ce3de9d09576a10e8176") --Dlcname Battlefield 4™ Legacy Operations
+setManifestid(1272657, "2560882888371150786", 2326607360)
+addappid(1277040, 1, "b8023e0d30afdd3b12bf791d9752a8293558f164f032eb60835dcb5a0079e704") --Dlc Windows Language Depot English Battlefield 4™ Legacy Operations
+setManifestid(1277040, "5925974092013194913", 192)
+addappid(1277041, 1, "483a7a0a347cb69cda006ac4ab78031ba5b12ecc5fef397fe6681245d1fcc9c4") --Dlc Windows Language Depot French Battlefield 4™ Legacy Operations
+setManifestid(1277041, "3080481614081291389", 192)
+addappid(1277042, 1, "e30e30d87dcc5c84943071f273d5903b0963cbb79a577d96d6cd9cc5845a63f3") --Dlc Windows Language Depot Italian Battlefield 4™ Legacy Operations
+setManifestid(1277042, "2352534588065124529", 192)
+addappid(1277043, 1, "3dacfe0f1752fdee16efe85877a22729ab1ed05c75073b3483965bca8bfe1e55") --Dlc Windows Language Depot German Battlefield 4™ Legacy Operations
+setManifestid(1277043, "1024195867706260014", 192)
+addappid(1277044, 1, "8f8e5a3967c86866923f93b03440af59f31672228b8084b1739b68b8edefc9b0") --Dlc Windows Language Depot Spanish Battlefield 4™ Legacy Operations
+setManifestid(1277044, "551413099187343965", 192)
+addappid(1277045, 1, "5c2d068f117af2e78408d58406f034b19e0d3e4aba51165b4ca7abbcf91ce561") --Dlc Windows Language Depot Polish Battlefield 4™ Legacy Operations
+setManifestid(1277045, "1156373570140536123", 192)
+addappid(1277046, 1, "1eaabb6698f1f72f119a3695884152959417f78e7488279d4716ec961ccb0f49") --Dlc Windows Language Depot Russian Battlefield 4™ Legacy Operations
+setManifestid(1277046, "6135512750757700865", 192)
+addappid(1277047, 1, "f10120e161e62053d171dcd2b580d03cbede06795b53e47f781aa234d52728a4") --Dlc Windows Language Depot Brazilian Battlefield 4™ Legacy Operations
+setManifestid(1277047, "8016969571309461966", 192)
+addappid(1277048, 1, "30a013ff004d379d21ec133ec78327f01c4962f17dbbe05cc80712ff558d8ae8") --Dlc Windows Language Depot Japanese Battlefield 4™ Legacy Operations
+setManifestid(1277048, "365876245565934165", 192)
+addappid(1277049, 1, "c16b5ffa1c2c7aa0a74f6cad6f6237ea22bfcfdda0636669c5abe7e381869ea8") --Dlc Windows Language Depot Czech Battlefield 4™ Legacy Operations
+setManifestid(1277049, "5284941833546291750", 192)
+addappid(1277050, 1, "6d6a22c18b7f0a51a3c5789714c83720bd57e4f320ece312ae75ef99307728a2") --Dlc Windows Language Depot Koreana Battlefield 4™ Legacy Operations
+setManifestid(1277050, "7006746036466336266", 192)
+addappid(1277051, 1, "cf331d04e115aa00bf7ddf3a2ef716b2e75d9515fa9648b44dc0649cadb8f257") --Dlc Windows Language Depot Tchinese Battlefield 4™ Legacy Operations
+setManifestid(1277051, "1154277164389555118", 192)
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") --Share Windows Depot EA APP Installer
+setManifestid(3340991, "3755387105551220105", 246722480)
+addappid(1238870) --Dlcname AppID 1238870
+addtoken(1238870, "394572474292069447")
+addappid(1238871) --Dlcname Battlefield 4™ Ultimate Shortcut Bundle
+addappid(1272640) --Dlcname AppID 1272640
+addtoken(1272640, "11249254201869736922")
+addappid(1276020) --Dlcname AppID 1276020
+addappid(1315430) --Dlcname AppID 1315430
+addappid(1315431) --Dlcname AppID 1315431
+addappid(1315432) --Dlcname AppID 1315432
+addappid(1315433) --Dlcname AppID 1315433
+addappid(1315434) --Dlcname AppID 1315434
+addappid(1315435) --Dlcname AppID 1315435
+addappid(1315440) --Dlcname AppID 1315440
+addappid(1315441) --Dlcname AppID 1315441
+addappid(1315442) --Dlcname AppID 1315442
+addtoken(1315442, "17653099291739995246")
+addappid(1315443) --Dlcname AppID 1315443
+addtoken(1315443, "5881263046246690550")
+addappid(1315470) --Dlcname AppID 1315470
+addtoken(1315470, "784439998782220359")
+addappid(1315471) --Dlcname AppID 1315471
+addappid(1315472) --Dlcname AppID 1315472
+addappid(1315480) --Dlcname Battlefield 4™ Soldier Shortcut Bundle
+addtoken(1315480, "1001675746326222669")
