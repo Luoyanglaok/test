8e8cf68c44190f12eb72d17a5c2bdf24a770b699
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 23:10:52 +0800

    add 2744040.lua

--- 2744040.lua
+++ 2744040.lua
@@ -1,3 +1,6 @@
-addappid(2744040)
-addappid(2744041,0,"21e41066e7a4db70931c34b3a82b32a947957c471166a52a0f7b8026a31a60f9")
-setManifestid(2744041,"336483614022272738")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2744040.lua
+--Gamename 没有被美女包围的三角恋
+addappid(2744040) --Mainappid 没有被美女包围的三角恋
+addappid(2744041, 1, "21e41066e7a4db70931c34b3a82b32a947957c471166a52a0f7b8026a31a60f9") --Main Windows Depot 没有被美女包围的三角恋
+setManifestid(2744041, "336483614022272738", 1615961760)
