1191a8352b16a2575e19cb7be023509fd8313e6f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 23:14:39 +0800

    add 233130.lua

--- 233130.lua
+++ 233130.lua
@@ -1,19 +1,30 @@
-addappid(233130)
-addappid(228982)
-setManifestid(228982,"6413394087650432851")
-addappid(228984)
-setManifestid(228984,"2547553897526095397")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(233131,0,"e49efbcac7fadc110b71ffb3bd9beda4c26e9fad773b1908421308fcb6acd436")
-setManifestid(233131,"7524503039981382511")
-addappid(233132,0,"4097c14b856d417fb04cabb6d3ebc7d150030b99e7e14cd5022c24d9b58d56e9")
-setManifestid(233132,"6445195049350112249")
-addappid(254675,0,"9ba715d0a7ad237032ebfb8c8d22c82d40e42552d664d8e381dd69fc2dae8067")
-setManifestid(254675,"1019797616527443974")
-addappid(254676,0,"205d45573cef11347f8acb7411d9ed9e568cf41184e8724cfef2743353d36460")
-setManifestid(254676,"8136379092540298181")
-addappid(254673,0,"a6d47bd12f8f037384afc892b5610560c78a6ae4c1853cd5b31fab4be4b32a3a")
-setManifestid(254673,"6941547528264440065")
-addappid(254674,0,"2f891797235cdd181db04cebe903a75ec5eb840af3f27a01d06107347bd974c0")
-setManifestid(254674,"193857510522357753")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 233130.lua
+--Gamename Shadow Warrior
+addappid(233130) --Mainappid Shadow Warrior
+addappid(233131, 1, "e49efbcac7fadc110b71ffb3bd9beda4c26e9fad773b1908421308fcb6acd436") --Main Windows Depot Shadow Warrior
+setManifestid(233131, "7524503039981382511", 13395544752)
+addappid(233132, 1, "4097c14b856d417fb04cabb6d3ebc7d150030b99e7e14cd5022c24d9b58d56e9") --Main Windows Depot Shadow Warrior
+setManifestid(233132, "6445195049350112249", 74863392)
+addappid(254675, 1, "9ba715d0a7ad237032ebfb8c8d22c82d40e42552d664d8e381dd69fc2dae8067") --Main Macos Depot Shadow Warrior
+setManifestid(254675, "1019797616527443974", 6096609472)
+addappid(254676, 1, "205d45573cef11347f8acb7411d9ed9e568cf41184e8724cfef2743353d36460") --Main Linux Depot Shadow Warrior
+setManifestid(254676, "8136379092540298181", 6095542544)
+--Dlcname Shadow Warrior Soundtrack
+addappid(254673, 1, "a6d47bd12f8f037384afc892b5610560c78a6ae4c1853cd5b31fab4be4b32a3a") --Dlcname Shadow Warrior Soundtrack
+setManifestid(254673, "6941547528264440065", 454547648)
+--Dlcname Shadow Warrior Digital Artbook
+addappid(254674, 1, "2f891797235cdd181db04cebe903a75ec5eb840af3f27a01d06107347bd974c0") --Dlcname Shadow Warrior Digital Artbook
+setManifestid(254674, "193857510522357753", 82582080)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(254670) --Dlcname Shadow Warrior: Zilla Prototype Katana DLC
+addappid(254671) --Dlcname Shadow Warrior: Serious Sam 3 Sledgehammer DLC
+addappid(254672) --Dlcname Shadow Warrior: Hotline Miami Katana DLC
+addappid(255580) --Dlcname Shadow Warrior: Saints Row 4 Penetrator DLC
+addappid(2550000) --Dlcname Shadow Warrior: Special Edition Upgrade
