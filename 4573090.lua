795e20bdd757261b4ceaac14a47d122ff4dbca47
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 21:39:25 +0800

    add 4573090.lua

---
+++ 4573090.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4573090.lua
+--Gamename Pibcremental
+addappid(4573090) --Mainappid Pibcremental
+addappid(4573091, 1, "f64e53ee14737d39d0eaa9c0032b5bbfa13dd86edffb1b57a8fcdd7ec8a20359") --Main Windows Depot Pibcremental
+setManifestid(4573091, "75500160486729881", 195771792)
