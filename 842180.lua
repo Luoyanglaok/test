1a62ce05b7872ac5aa0793c657c72a61614a2e90
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 15:50:22 +0800

    add 842180.lua

--- 842180.lua
+++ 842180.lua
@@ -1,3 +1,9 @@
-addappid(842180)
-addappid(842181,0,"613f14926c14907812b3dafc61eace8ecad7bea31eb7d101181b78dbfdca03fb")
-setManifestid(842181,"8670733382960856505")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 842180.lua
+--Gamename Prison Simulator
+addappid(842180, 1, "e1e7079ff2ff97e5261e211c769b91f3a1cdb4f0423eeecc31512949d8ba36df") --Mainappid Prison Simulator
+addappid(842181, 1, "613f14926c14907812b3dafc61eace8ecad7bea31eb7d101181b78dbfdca03fb") --Main Windows Depot Prison Simulator
+setManifestid(842181, "2872111146962116943", 3862038784)
+addappid(3592050) --Dlcname Prison Simulator: Skins Pack 01
+addappid(3601580) --Dlcname Prison Simulator: Power Wash
+addappid(4126450) --Dlcname Prison Simulator: Skins Pack 02
