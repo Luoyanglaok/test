130407f157c13f58e76f55a84dbe691bb452197d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 08:30:32 +0800

    add 1636440.lua

---
+++ 1636440.lua
@@ -0,0 +1,27 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1636440.lua
+--Gamename SILENT HILL: Townfall
+addappid(1636440, 1, "7b557e5bf973f343233a3b034f3cb02815f94ba267a7c92e0b335b30c4176e5a") --Mainappid SILENT HILL: Townfall
+addappid(1636441, 1, "62c41fd0ab66ae0f82f86b835f333bc9ec55fb5d28ee696b599a745059bb62ce") --Main Windows Depot SILENT HILL: Townfall
+setManifestid(1636441, "5846939894323649224", 57722516672)
+--Dlcname AppID 4126670
+addtoken(4126670, "4534485472292409398")
+addappid(4126670, 1, "7d1a555b80ecea1b26fe05d48beafaf2745f6f3851e6e691bb8d085421892f99") --Dlcname AppID 4126670
+setManifestid(4126670, "2957044820498811960", 23599504)
+--Dlcname AppID 4235620
+addtoken(4235620, "4379559952951711258")
+addappid(4235620, 1, "556a8c2ff138b8d8b54f00f607af240c1bd58e07e3a9ea92f9a1881fcb1ab522") --Dlcname AppID 4235620
+setManifestid(4235620, "1453085193393980489", 52661248)
+--Dlcname AppID 4663210
+addtoken(4663210, "13545024821267551421")
+addappid(4663210, 1, "86c4decc818889e1d669912dd971ff721cc2136ab9fdbb68de06c2f2e919daf1") --Dlcname AppID 4663210
+setManifestid(4663210, "879371378507688417", 1179259040)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
