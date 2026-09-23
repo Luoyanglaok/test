47cfa0182a6c1e5f0b9b55ca56204d9fe21ab32e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 19:28:58 +0800

    add 3398110.lua

---
+++ 3398110.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3398110.lua
+--Gamename The Walking Trade
+addappid(3398110, 1, "2efcc9d4df6bd08ef96f67ee00716b91c2ebb906a9f9001273d8d1f68e1f4ce3") --Mainappid The Walking Trade
+addappid(3398112, 1, "cad843e27a0eace2f72e865fff2696d8e59ebfeffb2fe12eb8833cb1f9cc2d7e") --Main Windows Depot The Walking Trade
+setManifestid(3398112, "6521855592339363103", 512664304)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
