b19a79d7ed7fe22e7d6ffb3a02e92fa44201046d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 20:04:16 +0800

    add 1145350.lua

--- 1145350.lua
+++ 1145350.lua
@@ -8,12 +8,6 @@ addappid(1145354, 1, "71f0364cb7bd2b7d7755605c7983bf2b73101a6e2e0ef6b7082d275a46
 setManifestid(1145354, "3523419235077190492", 44571408)
 addappid(1145355, 1, "67def0278f458945e71a75914c686e5bf4d3f90c8b4320795c3b78ad999afc56") --Main Macos Depot Hades II
 setManifestid(1145355, "4358145244193201792", 10537006496)
---Dlcname Hades II Original Soundtrack
-addappid(2950840) --Dlcname Hades II Original Soundtrack
-addappid(2950841, 1, "e3caba4f78ee373466d01e82c8533e6d858847bd48fefb2cba4724554537a206") --Dlc Windows Depot Hades II Original Soundtrack
-setManifestid(2950841, "4161983320866568293", 5105678976)
-addappid(2950842, 1, "fca5e03ba1607d809bb6410da5a466f2e3c9456b3ef75a1fa4f3b0427ae9fc34") --Dlc Windows Depot Hades II Original Soundtrack
-setManifestid(2950842, "6358127939422595540", 493302096)
 --Share Depots
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228988, "6645201662696499616", 22411856)
