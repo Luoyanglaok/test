5e337796ce4554a925b4aedb3ad8e235dbb1810e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 21:15:48 +0800

    add 260210.lua

--- 260210.lua
+++ 260210.lua
@@ -1,9 +1,18 @@
-addappid(260210)
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229003,"8740933542064151477")
-addappid(260211,0,"6a3490285db760cc79bac0bdecd4f4bb729ee76363f8298ecf13e046213fd845")
-setManifestid(260211,"2084186428532339486")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751,"818295193716041715")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 260210.lua
+--Gamename Assassin’s Creed® Liberation HD
+addappid(260210, 1, "65cfda07c56ae52841b2c373d294637b3d865e39555a91d2cd59607db3518b43") --Mainappid Assassin’s Creed® Liberation HD
+addappid(260211, 1, "6a3490285db760cc79bac0bdecd4f4bb729ee76363f8298ecf13e046213fd845") --Main Windows Depot Assassin’s Creed® Liberation HD
+setManifestid(260211, "2084186428532339486", 2645512032)
+--Share Depots
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229003, "8740933542064151477", 42914928)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") --Share Windows Depot Ubisoft Connect PC Client
+setManifestid(1716751, "6659642105086821873", 264636800)
+addappid(272700) --Dlcname Assassin’s Creed Liberation HD - Bonus Pack
