528851e3ac8c19cadf0e59f85460dd56b3467f7c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 23:09:16 +0800

    add 3898860.lua

---
+++ 3898860.lua
@@ -0,0 +1,29 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3898860.lua
+--Gamename 敏感！恋のアンテナ ビンビン別嬪荘
+addappid(3898860, 1, "e3f395ee9dbcc6a3f200a431239efe02409dbbcf97e318cc088dbab517cbfc27") --Mainappid 敏感！恋のアンテナ ビンビン別嬪荘
+addappid(3898861, 1, "71ad346d898697f9c9b91f93d981fce8aa2981cf1e9b3ac3bb93f28565967c9a") --Main Windows Depot 敏感！恋のアンテナ ビンビン別嬪荘
+setManifestid(3898861, "6542438242789650661", 35669167936)
+addappid(3898862, 1, "f3ffee679e7d14638b91dd98015e087be1dc6491147eddb17a60e0431a5e6a9e") --Main Macos Depot 敏感！恋のアンテナ ビンビン別嬪荘
+setManifestid(3898862, "7047233340054910914", 35697420592)
+--Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -プレミアム特典アルバム-
+addappid(4234230, 1, "88aaa6e98c508dce9c073ea49833ceb5df500c742dd5b8a7b0c390dda3ab07ea") --Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -プレミアム特典アルバム-
+setManifestid(4234230, "1196947010588116500", 728901184)
+--Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -パーティータイム-
+addappid(4251950, 1, "c3f17e92579871e1da88a97077d11bf12a92a3aa080ba5bcc7a8bd4de56dd06c") --Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -パーティータイム-
+setManifestid(4251950, "2596808944728895757", 2509339696)
+--Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -菜緒スペシャルパッケージ-
+addappid(4251960, 1, "4aa5e8a2a869296c21b3bb7ccc4559e837115bf74093d343577864048bcb6866") --Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -菜緒スペシャルパッケージ-
+setManifestid(4251960, "1179715486451469762", 5032550560)
+--Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -凪スペシャルパッケージ-
+addappid(4251970, 1, "9f6fad2694d7fb91d1ad05a55a83983900416f537eb2ab81b2c4377aa3024aa6") --Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -凪スペシャルパッケージ-
+setManifestid(4251970, "8613448515560307952", 4806498800)
+--Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -アノンスペシャルパッケージ-
+addappid(4251980, 1, "a696a54c284e7b9eed57bd224943dd1ae606002006af99a9449ff99af572b32b") --Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -アノンスペシャルパッケージ-
+setManifestid(4251980, "8697786623633373367", 4488516800)
+--Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -流花スペシャルパッケージ-
+addappid(4251990, 1, "68aa4fa58b2c1a08f3c8bcb99b398714dbcf00f9200ab2d6b7166b4e9197a60e") --Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -流花スペシャルパッケージ-
+setManifestid(4251990, "1150940663089962115", 4553894208)
+--Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -美玲スペシャルパッケージ-
+addappid(4252000, 1, "915fcff956e5b1b962e6653c3c256d85f883d1fc9103a7603a219ec677560038") --Dlcname 敏感！恋のアンテナ ビンビン別嬪荘 -美玲スペシャルパッケージ-
+setManifestid(4252000, "2979775162299010810", 4520062608)
