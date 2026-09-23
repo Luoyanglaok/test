0cb0a2ccb7d706930e773106d81e4ddc0b0c9bef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 20:56:40 +0800

    add 1030380.lua

--- 1030380.lua
+++ 1030380.lua
@@ -1,3 +1,6 @@
-addappid(1030380)
-addappid(1030381,0,"b5c902821b9e2a516260277f82b3dc82e20aafd08125b83cb1d046186b0a9dfd")
-setManifestid(1030381,"8937675075800090138")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1030380.lua
+--Gamename Real Fishing VR
+addappid(1030380) --Mainappid Real Fishing VR
+addappid(1030381, 1, "b5c902821b9e2a516260277f82b3dc82e20aafd08125b83cb1d046186b0a9dfd") --Main Windows Depot Real Fishing VR
+setManifestid(1030381, "8937675075800090138", 621530400)
