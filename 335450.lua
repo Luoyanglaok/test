55467334fd02e5c7fc42cd0778920f2e9f4445cc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 21:11:30 +0800

    add 335450.lua

--- 335450.lua
+++ 335450.lua
@@ -1,9 +1,13 @@
-addappid(335450)
-addappid(228985)
-setManifestid(228985,"3966345552745568756")
-addappid(335451,0,"69957a63e2f4f4b677194dec51f644c853c77bcecbcd8c3b4533133d7982ca4e")
-setManifestid(335451,"3829033815167873882")
-addappid(335452,0,"0fde542af82105c21dee6007ebc7db574f6e1dc40a7aa4e59c1d0c5a0f63f958")
-setManifestid(335452,"475391958296515641")
-addappid(335453,0,"3d28dce5231dabc9d76484a09c097c91f416fe7e8993d40b4de53700daddbc67")
-setManifestid(335453,"4330509803742700134")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 335450.lua
+--Gamename Curse of the Assassin
+addappid(335450) --Mainappid Curse of the Assassin
+addappid(335451, 1, "69957a63e2f4f4b677194dec51f644c853c77bcecbcd8c3b4533133d7982ca4e") --Main Linux Depot Curse of the Assassin
+setManifestid(335451, "3829033815167873882", 73821504)
+addappid(335452, 1, "0fde542af82105c21dee6007ebc7db574f6e1dc40a7aa4e59c1d0c5a0f63f958") --Main Macos Depot Curse of the Assassin
+setManifestid(335452, "475391958296515641", 62658560)
+addappid(335453, 1, "3d28dce5231dabc9d76484a09c097c91f416fe7e8993d40b4de53700daddbc67") --Main Windows Depot Curse of the Assassin
+setManifestid(335453, "4330509803742700134", 60056800)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
