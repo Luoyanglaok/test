0e8908404b5f864c7e3531bfedea1ab6f789dd50
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 15:50:56 +0800

    add 1552730.lua

--- 1552730.lua
+++ 1552730.lua
@@ -1,5 +1,11 @@
-addappid(1552730)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1552731,0,"3049f2518709d3499d9d781bf529ff88b61a8367ea251c0c1f449d186c159a89")
-setManifestid(1552731,"57224948826209779")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1552730.lua
+--Gamename Fireworks Simulator: Realistic
+addappid(1552730) --Mainappid Fireworks Simulator: Realistic
+addappid(1552731, 1, "3049f2518709d3499d9d781bf529ff88b61a8367ea251c0c1f449d186c159a89") --Main Windows Depot Fireworks Simulator: Realistic
+setManifestid(1552731, "57224948826209779", 1634022928)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
