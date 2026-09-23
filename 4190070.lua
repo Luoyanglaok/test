7aff06a3e893e236e097c1cfd8afacf3c2fcebe2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 23:05:34 +0800

    add 4190070.lua

---
+++ 4190070.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4190070.lua
+--Gamename Ladies, Don’t Tempt My Immortality
+addappid(4190070) --Mainappid Ladies, Don’t Tempt My Immortality
+addappid(4190071, 1, "aaa04d6b73a9bc61869264f823f5776c2b82e12c8a293f2c83167bf6a2a9b329") --Main Windows Depot Ladies, Don’t Tempt My Immortality
+setManifestid(4190071, "2233787962425619077", 12330661664)
+--Dlcname Ladies, Don’t Tempt My Immortality - 2K DLC
+addappid(4206960, 1, "498585c62142a3682973e9f45350e7569a178754749eaa8f4af4be13e4af04b3") --Dlcname Ladies, Don’t Tempt My Immortality - 2K DLC
+addappid(4206961, 1, "a7cd41b3a8c3912357d6e7f2015db174ab64129beacfe98a65b489940bc9b308") --Dlc Windows Depot Ladies, Don’t Tempt My Immortality - 2K DLC
+setManifestid(4206961, "6674181466638511994", 30577495536)
