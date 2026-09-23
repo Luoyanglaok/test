91f10279abf113bf25dd9e61e57151f9e960e974
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 20:51:33 +0800

    add 730310.lua

--- 730310.lua
+++ 730310.lua
@@ -1,11 +1,214 @@
-addappid(730310)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(730313,0,"308703da76d059553e5f6c9354d62a54caf6f4efc45ce54dcc1a79766a5a6139")
-setManifestid(730313,"1155763608999357695")
-addappid(730315,0,"a90f00f3030ebba61278889b992de81f8fe971fa81252078c10cfab5a14de47f")
-setManifestid(730315,"4334137994461932011")
-addappid(776523,0,"91ca2dae4d9119f53448a6277b5b987bce6ca35b63b6d4444f448b4341f1a558")
-setManifestid(776523,"8991465563649857119")
-addappid(776521,0,"883ee56f3eedbff5d386b4ef6ac92ed215cc5c6428f934d6b8f1a12f9dd2df2f")
-setManifestid(776521,"5853360198103515412")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 730310.lua
+--Gamename DYNASTY WARRIORS 9
+addappid(730310, 1, "00b2a539e97d84621e7e0c20b8865409b7ef84cf70a4be56446383a0531bb6d2") --Mainappid DYNASTY WARRIORS 9
+addappid(730313, 1, "308703da76d059553e5f6c9354d62a54caf6f4efc45ce54dcc1a79766a5a6139") --Main Windows Depot DYNASTY WARRIORS 9
+setManifestid(730313, "1155763608999357695", 1777399296)
+addappid(730315, 1, "a90f00f3030ebba61278889b992de81f8fe971fa81252078c10cfab5a14de47f") --Main Windows Depot DYNASTY WARRIORS 9
+setManifestid(730315, "4334137994461932011", 35818822240)
+addappid(776521, 1, "883ee56f3eedbff5d386b4ef6ac92ed215cc5c6428f934d6b8f1a12f9dd2df2f") --Main Windows Depot DYNASTY WARRIORS 9
+setManifestid(776521, "5853360198103515412", 19464128)
+addappid(776523, 1, "91ca2dae4d9119f53448a6277b5b987bce6ca35b63b6d4444f448b4341f1a558") --Main Windows Depot DYNASTY WARRIORS 9
+setManifestid(776523, "8991465563649857119", 2111155584)
+addappid(956899, 1, "d214d2d9b4d3f1f4ffd95c6528dd745fce72a9985f2d1411547effb1ab3b38b8") --Main Windows Depot DYNASTY WARRIORS 9
+setManifestid(956899, "2724687988885506557", 48)
+addappid(956900, 1, "4561511eabd79853509d2ee3c8682f05f8831fc13627a4f59217e7f2708a271c") --Main Windows Depot DYNASTY WARRIORS 9
+--Dlcname DYNASTY WARRIORS 9/真・三國無双８: Bonus DLC Set
+addappid(776520, 1, "565afb0c5345487abf68ab7ab5aa38d940abc8ad49a655085e70fccba083c503") --Dlcname DYNASTY WARRIORS 9/真・三國無双８: Bonus DLC Set
+setManifestid(776520, "5697968097000470170", 295445504)
+--Dlcname DYNASTY WARRIORS 9:Season Pass/真・三國無双８:シーズンパス
+addappid(776530, 1, "95a02a65c2a411225aa20e646c17a50358fcb0cabc60348896aa0ed14254227e") --Dlcname DYNASTY WARRIORS 9:Season Pass/真・三國無双８:シーズンパス
+setManifestid(776530, "2707467272273053159", 57376)
+--Dlcname Additional Scenarios Pack/追加シナリオパック
+addappid(798860, 1, "038527fbfc4b71919808521fb7105da29f56ff8c8e4db34ae6e6101f3b767229") --Dlcname Additional Scenarios Pack/追加シナリオパック
+setManifestid(798860, "3359927599035665735", 517064032)
+--Dlcname Additional Weapons Pack/追加武器パック
+addappid(798870, 1, "680393c44baee691054a6e27404226e9504ae85eb46fcab200b5ddc6a2288d43") --Dlcname Additional Weapons Pack/追加武器パック
+setManifestid(798870, "5965911049978234692", 24446544)
+--Dlcname Hideaway Customization/隠れ処カスタマイズパック
+addappid(798880, 1, "e93184e1084c21e08fdf30e4ba3bc50ad166d284790b4e8bc8558532ed1be543") --Dlcname Hideaway Customization/隠れ処カスタマイズパック
+setManifestid(798880, "1898485630775876681", 117250272)
+--Dlcname DYNASTY WARRIORS 9 Useful Gems & Materials Set （役立つ宝玉・素材セット）（助益多寶玉・素材組合）
+addappid(835590, 1, "713d67329656d6275eed859cd81329338400d24444ac721fa3812c11353dd55b") --Dlcname DYNASTY WARRIORS 9 Useful Gems & Materials Set （役立つ宝玉・素材セット）（助益多寶玉・素材組合）
+setManifestid(835590, "7434044509878981130", 73648)
+--Dlcname Zhao Yun (Beijing Opera-Style Costume)/趙雲「京劇風コスチューム」
+addappid(892390, 1, "358e1929492d8b81c62dcaf388a8f40a3124be3f182d30df64d59e0cf17613c2") --Dlcname Zhao Yun (Beijing Opera-Style Costume)/趙雲「京劇風コスチューム」
+setManifestid(892390, "4836483747092836281", 47687136)
+--Dlcname Sun Shangxiang (Dudou Costume)/孫尚香「肚兜風コスチューム」
+addappid(892391, 1, "915b8fbf5be6565291b202ce4c1a979acd3d647210687ca5799b1d65297366bd") --Dlcname Sun Shangxiang (Dudou Costume)/孫尚香「肚兜風コスチューム」
+setManifestid(892391, "1214664281619665346", 38843472)
+--Dlcname Diaochan (Dudou Costume)/貂蝉「肚兜風コスチューム」
+addappid(892392, 1, "13c3fc1f15dec088f5391a254ff6d943c7524c995c439e64a4f2ddc4026be8ab") --Dlcname Diaochan (Dudou Costume)/貂蝉「肚兜風コスチューム」
+setManifestid(892392, "5650584209348861618", 41593424)
+--Dlcname Wang Yuanji (Dudou Costume)/王元姫「肚兜風コスチューム」
+addappid(892393, 1, "ed546f4dca38a532e2f8991a3243dabe0c5a6309b60b472a1cbbdedc2759c67e") --Dlcname Wang Yuanji (Dudou Costume)/王元姫「肚兜風コスチューム」
+setManifestid(892393, "5320927185677602639", 44050256)
+--Dlcname Wang Yi (Dudou Costume)/王異「肚兜風コスチューム」
+addappid(892394, 1, "3832dc80a47763d4e04df32f49ee2a7451bae345662a104671223e7f344a784a") --Dlcname Wang Yi (Dudou Costume)/王異「肚兜風コスチューム」
+setManifestid(892394, "1667084834172197705", 41657440)
+--Dlcname Guan Yinping (Dudou Costume)/関銀屏「肚兜風コスチューム」
+addappid(892395, 1, "94212f4d82e1fd418fc5d300898edf2bfb92ed3da9325cb7673da0a4968c354d") --Dlcname Guan Yinping (Dudou Costume)/関銀屏「肚兜風コスチューム」
+setManifestid(892395, "905562769987288738", 43722272)
+--Dlcname Lu Lingqi (Dudou Costume)/呂玲綺「肚兜風コスチューム」
+addappid(892396, 1, "5482f5ecde1d0b051caac191bc329321d49fee4f8b06a296633a3e196d2e0aa3") --Dlcname Lu Lingqi (Dudou Costume)/呂玲綺「肚兜風コスチューム」
+setManifestid(892396, "6780724957482823529", 38756800)
+--Dlcname Xin Xianying (Special Costume)/辛憲英「特製コスチューム」
+addappid(892397, 1, "8320bb19338a719e91c1f744d8351f0ea0fcf4243402edb8546a70f575578543") --Dlcname Xin Xianying (Special Costume)/辛憲英「特製コスチューム」
+setManifestid(892397, "7477705184385591089", 46630768)
+--Dlcname Xin Xianying (Dudou Costume)/辛憲英「肚兜風コスチューム」
+addappid(892398, 1, "100e3c632a058cb029ffed30b38b9a4f5314713a8a52fbff18b853d4b9f69b9e") --Dlcname Xin Xianying (Dudou Costume)/辛憲英「肚兜風コスチューム」
+setManifestid(892398, "8977255476543493728", 42426592)
+--Dlcname Xiahou Dun - Officer Ticket / 夏侯惇使用券
+addappid(956700, 1, "fba263a6935b15c4a7305a039e333f3a451d2457cf8a45ca150142bd0c4a17a9") --Dlcname Xiahou Dun - Officer Ticket / 夏侯惇使用券
+setManifestid(956700, "4819415246826131624", 48)
+--Dlcname Dian Wei - Officer Ticket / 典韋使用券
+addappid(956701, 1, "7a4db1b2a41daca4733b80091831a6b4a173e518a52cc7b5eaac3bc6f70869d7") --Dlcname Dian Wei - Officer Ticket / 典韋使用券
+setManifestid(956701, "7686013331527834974", 48)
+--Dlcname Sima Yi - Officer Ticket / 司馬懿使用券
+addappid(956702, 1, "d3150aea50eb8da6ad4f2609eea552521dc4b4d052f731660b2e5dc918df1cf1") --Dlcname Sima Yi - Officer Ticket / 司馬懿使用券
+setManifestid(956702, "159233116654340601", 48)
+--Dlcname Zhang Liao - Officer Ticket / 張遼使用券
+addappid(956703, 1, "2851d9f234b6772d6f8e0b090f8a810af99f5d55d45d9dcd8de51742d8504ebb") --Dlcname Zhang Liao - Officer Ticket / 張遼使用券
+setManifestid(956703, "6803467039559842646", 48)
+--Dlcname Cao Cao - Officer Ticket / 曹操使用券
+addappid(956704, 1, "1b1b5abe9f0e00edb2a6803eb22c6880c2017f8a37a94240c9cbaa03525e61ac") --Dlcname Cao Cao - Officer Ticket / 曹操使用券
+setManifestid(956704, "2025199154861069529", 48)
+--Dlcname Zhou Yu - Officer Ticket / 周瑜使用券
+addappid(956705, 1, "4084331d93aeef171cd673001ea1e38d6f64f0590e11690cf1246de65310c92c") --Dlcname Zhou Yu - Officer Ticket / 周瑜使用券
+setManifestid(956705, "4080561184013376887", 48)
+--Dlcname Lu Xun - Officer Ticket / 陸遜使用券
+addappid(956706, 1, "7bf13137ec29cd68df774933b81828520eef4c7bdae11388973b392b5d2cac7f") --Dlcname Lu Xun - Officer Ticket / 陸遜使用券
+setManifestid(956706, "1014422720505535876", 48)
+--Dlcname Sun Shangxiang - Officer Ticket / 孫尚香使用券
+addappid(956707, 1, "f30902f9fd904bf59c257b054c35970a56ebda0cb98a7702e8a5baff2bc8630b") --Dlcname Sun Shangxiang - Officer Ticket / 孫尚香使用券
+setManifestid(956707, "7452576658449087706", 48)
+--Dlcname Gan Ning - Officer Ticket / 甘寧使用券
+addappid(956708, 1, "b637468afdf667a31d9e888d0cd16e23b83f80aee8e0522cd42795df4dddb320") --Dlcname Gan Ning - Officer Ticket / 甘寧使用券
+setManifestid(956708, "1022560448600717337", 48)
+--Dlcname Sun Jian - Officer Ticket / 孫堅使用券
+addappid(956709, 1, "0debf4e31570d0e8b57e88a09d8b6e906903449d8bfbbcd67146627aed1718bc") --Dlcname Sun Jian - Officer Ticket / 孫堅使用券
+setManifestid(956709, "7118367770044805609", 48)
+--Dlcname Zhao Yun - Officer Ticket / 趙雲使用券
+addappid(956720, 1, "77e6cf963e320439624402033c0e603f200890aa1c620dce6dce70df7b436e3e") --Dlcname Zhao Yun - Officer Ticket / 趙雲使用券
+setManifestid(956720, "8774962952031478272", 48)
+--Dlcname Guan Yu - Officer Ticket / 関羽使用券
+addappid(956721, 1, "2471ac3b89b7a73517971c658d0c5e3a38ec7375cc7391c5be34561cc6a31e8e") --Dlcname Guan Yu - Officer Ticket / 関羽使用券
+setManifestid(956721, "6770844801949183279", 48)
+--Dlcname Zhang Fei - Officer Ticket / 張飛使用券
+addappid(956722, 1, "477dcd7e8cf7778f1492cbeaadaf0b271b9880c0d2daeee2248e190493577a74") --Dlcname Zhang Fei - Officer Ticket / 張飛使用券
+setManifestid(956722, "7599906271615287889", 48)
+--Dlcname Zhuge Liang - Officer Ticket / 諸葛亮使用券
+addappid(956723, 1, "3464dfdef50d053f7120de7aabfb251b5ab771528e346f3542f81d604e0fbcf8") --Dlcname Zhuge Liang - Officer Ticket / 諸葛亮使用券
+setManifestid(956723, "4987245523377512827", 48)
+--Dlcname Liu Bei - Officer Ticket / 劉備使用券
+addappid(956724, 1, "7579fe01cb58b2b59d64b5ba466ae4be86fca8db30f9954750699f48bc08e4d2") --Dlcname Liu Bei - Officer Ticket / 劉備使用券
+setManifestid(956724, "6825874852978503282", 48)
+--Dlcname Diaochan - Officer Ticket / 貂蝉使用券
+addappid(956725, 1, "9f09c4d4e323e5f909a5b9f2816ffbb9f0417f40bff8efc8b4e6afe9c8f7ddd7") --Dlcname Diaochan - Officer Ticket / 貂蝉使用券
+setManifestid(956725, "1760380274364814758", 48)
+--Dlcname Lu Bu - Officer Ticket / 呂布使用券
+addappid(956726, 1, "78e7c479bd4234984e800008c428673d90d35ca5616773ec98ccebb6e209f0e3") --Dlcname Lu Bu - Officer Ticket / 呂布使用券
+setManifestid(956726, "2356633486926914254", 48)
+--Dlcname Xu Zhu - Officer Ticket / 許褚使用券
+addappid(956727, 1, "189278a8aa472f4f4b83f94f779f481e838e25b39127f67061885fbe08b82ce5") --Dlcname Xu Zhu - Officer Ticket / 許褚使用券
+setManifestid(956727, "1553912176064524592", 48)
+--Dlcname Xiahou Yuan - Officer Ticket / 夏侯淵使用券
+addappid(956728, 1, "20fad3949e7f6362b97cbfeb0f20990d5470272233f23af2aa2cc2dde2a91d90") --Dlcname Xiahou Yuan - Officer Ticket / 夏侯淵使用券
+setManifestid(956728, "1173952465827888613", 48)
+--Dlcname Xu Huang - Officer Ticket / 徐晃使用券
+addappid(956729, 1, "50a8c76d42d7a53e78d0dbe3310962a19f85d88c104a7e760e2450007385a04a") --Dlcname Xu Huang - Officer Ticket / 徐晃使用券
+setManifestid(956729, "7215816080367874386", 48)
+--Dlcname Zhang He - Officer Ticket / 張郃使用券
+addappid(956730, 1, "be22fa953036466ceb8acb51fbf7ce23a6bf51aa5e88350457af91903b140c35") --Dlcname Zhang He - Officer Ticket / 張郃使用券
+setManifestid(956730, "3425635236357350319", 48)
+--Dlcname Cao Ren - Officer Ticket / 曹仁使用券
+addappid(956731, 1, "6319423811b2b1899d428f570a7c73946a88df111fcd9812c970cee624ce5d88") --Dlcname Cao Ren - Officer Ticket / 曹仁使用券
+setManifestid(956731, "4879597688713241961", 48)
+--Dlcname Cao Pi - Officer Ticket / 曹丕使用券
+addappid(956732, 1, "9b5026756fac28b38b5563b28421be97c9621706b766faa642c19cd780926841") --Dlcname Cao Pi - Officer Ticket / 曹丕使用券
+setManifestid(956732, "7297256141416188589", 48)
+--Dlcname Taishi Ci - Officer Ticket / 太史慈使用券
+addappid(956733, 1, "c0c618e65156475cb227c92760466bd2dcaf3c59e3144d9a61298adcf72fa61f") --Dlcname Taishi Ci - Officer Ticket / 太史慈使用券
+setManifestid(956733, "5442574791757402730", 48)
+--Dlcname Lu Meng - Officer Ticket / 呂蒙使用券
+addappid(956734, 1, "0d2d440517018a2cf867f6b86c01bda04f25b015af12c9b4085c240796e5e690") --Dlcname Lu Meng - Officer Ticket / 呂蒙使用券
+setManifestid(956734, "7813537835719030584", 48)
+--Dlcname Huang Gai - Officer Ticket / 黄蓋使用券
+addappid(956735, 1, "852d5b1f0a498b712c1b171aff4b70fde0000935f65220817ab384ee1e12c487") --Dlcname Huang Gai - Officer Ticket / 黄蓋使用券
+setManifestid(956735, "2979234000839939838", 48)
+--Dlcname Zhou Tai - Officer Ticket / 周泰使用券
+addappid(956736, 1, "2b0f1edbe2baa5c9211292f0eb41f4798a7ba0187d9c8098af9f6b93894aee63") --Dlcname Zhou Tai - Officer Ticket / 周泰使用券
+setManifestid(956736, "8794722238965210844", 48)
+--Dlcname Ling Tong - Officer Ticket / 凌統使用券
+addappid(956737, 1, "f561244c46a92200f40a9fc6db007eed57ec0f1e8f97368478242ee7590a91ea") --Dlcname Ling Tong - Officer Ticket / 凌統使用券
+setManifestid(956737, "2611007615407090313", 48)
+--Dlcname Sun Ce - Officer Ticket / 孫策使用券
+addappid(956738, 1, "ba8aff5f0c3c547f05586ff50f725761c074a0e7eb9ff89c158afb2ad35a9614") --Dlcname Sun Ce - Officer Ticket / 孫策使用券
+setManifestid(956738, "5981374600009261920", 48)
+--Dlcname Sun Quan - Officer Ticket / 孫権使用券
+addappid(956739, 1, "afc6557b77caa43ec4d7fb1bdc1dd603dbfca20556458edd04e169ad1bf99aa1") --Dlcname Sun Quan - Officer Ticket / 孫権使用券
+setManifestid(956739, "1115569486379632431", 48)
+--Dlcname Ma Chao - Officer Ticket / 馬超使用券
+addappid(956750, 1, "3eabce7ffde1e12de1088e33a535f5bc18c8996f91599cad25301ac49c42c656") --Dlcname Ma Chao - Officer Ticket / 馬超使用券
+setManifestid(956750, "85089445874551996", 48)
+--Dlcname Huang Zhong - Officer Ticket / 黄忠使用券
+addappid(956751, 1, "ddb3d9acb99288d755d3e1a4ad2358dae5d07a179bb3694956b8dbb1bb8d709e") --Dlcname Huang Zhong - Officer Ticket / 黄忠使用券
+setManifestid(956751, "1395823817913116181", 48)
+--Dlcname Wei Yan - Officer Ticket / 魏延使用券
+addappid(956752, 1, "6ead28ae250a8047a06b6311cca3c245f421d22cee0f1c02c8a33847366cfe11") --Dlcname Wei Yan - Officer Ticket / 魏延使用券
+setManifestid(956752, "5635565543409567753", 48)
+--Dlcname Guan Ping - Officer Ticket / 関平使用券
+addappid(956753, 1, "1bc8ada9d3ed0c04b1bc4dd4f7ee9eba1acfc4ad522596731e2e6e632a9d3c00") --Dlcname Guan Ping - Officer Ticket / 関平使用券
+setManifestid(956753, "7758531324305873507", 48)
+--Dlcname Pang Tong - Officer Ticket / 龐統使用券
+addappid(956760, 1, "f67695cdc5ee74559ae16e8a3a3e7159f45bc14db2e251a008c0e10a989f391e") --Dlcname Pang Tong - Officer Ticket / 龐統使用券
+setManifestid(956760, "3807400565928694230", 48)
+--Dlcname Dong Zhuo - Officer Ticket / 董卓使用券
+addappid(956761, 1, "9faf84c21c8711f93c9be9c26ac1b963d4355cfce3d3da939bda6fc2c070848e") --Dlcname Dong Zhuo - Officer Ticket / 董卓使用券
+setManifestid(956761, "5248348882168469014", 48)
+--Dlcname Yuan Shao - Officer Ticket / 袁紹使用券
+addappid(956762, 1, "7453ac3827589a51c8550fdb4ead2ac1fb46473d729510fc3edb131d9dacf059") --Dlcname Yuan Shao - Officer Ticket / 袁紹使用券
+setManifestid(956762, "1489000546255589588", 48)
+--Dlcname Zhang Jiao - Officer Ticket / 張角使用券
+addappid(956763, 1, "c18484f94f36d1ddb78ec8073ae1a3f5b504ea6c1e94f09670a239de601a7e22") --Dlcname Zhang Jiao - Officer Ticket / 張角使用券
+setManifestid(956763, "2163721598249348883", 48)
+--Dlcname Zhenji - Officer Ticket / 甄姫使用券
+addappid(956764, 1, "70d9dbcfcd2b1d1718c4d48f170b69367ec23d78305629ebbcebb5246689efb8") --Dlcname Zhenji - Officer Ticket / 甄姫使用券
+setManifestid(956764, "4855726910951828285", 48)
+--Dlcname Xiaoqiao - Officer Ticket / 小喬使用券
+addappid(956765, 1, "56f549b3c6c8b21195dad08d3ff9c865edca582b93cedb944793b37c182f7f32") --Dlcname Xiaoqiao - Officer Ticket / 小喬使用券
+setManifestid(956765, "8126335339789530740", 48)
+--Dlcname Yueying - Officer Ticket / 月英使用券
+addappid(956766, 1, "8c5179176440813b88541dbe01713760a3aca81c43e32955166d149a29fc9629") --Dlcname Yueying - Officer Ticket / 月英使用券
+setManifestid(956766, "3177443899621639985", 48)
+--Dlcname Meng Huo - Officer Ticket / 孟獲使用券
+addappid(956767, 1, "a7deea0f66bc85053f33fd4d0d3b72de79bf02cc0bc169eb388f8edcdbcc7c51") --Dlcname Meng Huo - Officer Ticket / 孟獲使用券
+setManifestid(956767, "8444956319008762625", 48)
+--Dlcname Cai Wenji - Officer Ticket / 蔡文姫使用券
+addappid(956768, 1, "b5ee986e890e2db56294be6c938148bcffb618c6db09492630405ed96817c1da") --Dlcname Cai Wenji - Officer Ticket / 蔡文姫使用券
+setManifestid(956768, "6712072337670477569", 48)
+--Dlcname Daqiao - Officer Ticket / 大喬使用券
+addappid(956769, 1, "c8509e10f1301f53c33a3dce54c673c9ce92dcfcc730b162a479342aa21fa3ce") --Dlcname Daqiao - Officer Ticket / 大喬使用券
+setManifestid(956769, "8099359513029942659", 48)
+--Dlcname Jiang Wei - Officer Ticket / 姜維使用券
+addappid(956770, 1, "0519a532a24bb13252554db6c7064719ff1c05f46e0b6c2fad29d59c73ea72f6") --Dlcname Jiang Wei - Officer Ticket / 姜維使用券
+setManifestid(956770, "8109419163953988202", 48)
+--Dlcname Jia Xu - Officer Ticket / 賈詡使用券
+addappid(956771, 1, "dbf25bf323ace20b80d09afc5f0d1e80f6fe0dbd50886218af7759bff8e4ee8e") --Dlcname Jia Xu - Officer Ticket / 賈詡使用券
+setManifestid(956771, "5396171649572192164", 48)
+--Dlcname Sima Shi - Officer Ticket / 司馬師使用券
+addappid(956772, 1, "eae3cfcd478a45cd8627e48c0a251831c00def73d7b6afcbc5fcf3791e39a697") --Dlcname Sima Shi - Officer Ticket / 司馬師使用券
+setManifestid(956772, "6345047753377968458", 48)
+--Dlcname Sima Zhao - Officer Ticket / 司馬昭使用券
+addappid(956860, 1, "9a193de86cf06fdc298d0bbef8e28309b2990ab80adc0f6010f024c29dacf1a6") --Dlcname Sima Zhao - Officer Ticket / 司馬昭使用券
+setManifestid(956860, "8516406290843469121", 48)
+--Dlcname Xu Shu - Officer Ticket / 徐庶使用券
+addappid(956898, 1, "fea9b6a3e14ae60906b4a0824a6b6080d3c0fabb4cb4dd7406a9bbe281ef7b6a") --Dlcname Xu Shu - Officer Ticket / 徐庶使用券
+setManifestid(956898, "5345456718534674949", 48)
+--Dlcname Zuo Ci - Officer Ticket / 左慈使用券
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(892399) --Dlcname Special Costume Set/特製コスチュームセット
