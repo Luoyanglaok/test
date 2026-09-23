6a8be2c620a035e0ef43fc84583c5834dfa25865
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 23:13:04 +0800

    add 2322560.lua

--- 2322560.lua
+++ 2322560.lua
@@ -1,9 +1,35 @@
-addappid(2322560)
-addappid(2670930)
-addappid(2881050)
-addappid(2322561,1,"f8ae938c87b21add3212afdb3a3ff3ce10006f418b44f8ba151efab5fa3645ba")
-addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
-addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
-setManifestid(2322561,"730112106166130411")
-setManifestid(228989,"5753583882400741046")
-setManifestid(228990,"1829726630299308803")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2322560.lua
+--Gamename Love Is All Around
+addappid(2322560) --Mainappid Love Is All Around
+addtoken(2322560, "12887829205471173139")
+addappid(2322561, 1, "f8ae938c87b21add3212afdb3a3ff3ce10006f418b44f8ba151efab5fa3645ba") --Main Windows Depot Love Is All Around
+setManifestid(2322561, "730112106166130411", 15100732960)
+addappid(2322562, 1, "4c7fe6457c86ff503ae1c48175bdf14b66e7aeace01051dd0c2ebe8c193eb826") --Main Macos Depot Love Is All Around
+setManifestid(2322562, "3467759607857190684", 15131677120)
+addappid(2322563, 1, "43e70d7f01def87e3f7da43d52cfab4355d2df1140aaf190894a9b834f97b5dd") --Main Linux Depot Love Is All Around
+setManifestid(2322563, "3637020757380903864", 15108547632)
+addappid(2322564, 1, "ffba65bebbbe85488c3fc3327d568c07b9fd02bdd5c4a8a988e2b6056561b9b8") --Main Windows Language Depot Japanese Love Is All Around
+setManifestid(2322564, "8309993917302559230", 16874724736)
+addappid(2322565, 1, "4e3c98a38e395106c2ae7b7158a0d71d196f67fb233f6ad104c76d6e93c6f9b9") --Main Macos Language Depot Japanese Love Is All Around
+setManifestid(2322565, "6561471360902661099", 16905659968)
+addappid(2322566, 1, "143539fbc7eb0af9ab5d61ab19123942fefcfef35d51e3a3d221db46ffdc39aa") --Main Linux Language Depot Japanese Love Is All Around
+setManifestid(2322566, "2916963593605501110", 16882526288)
+addappid(2322567, 1, "7d0e7a2f4fe4b913552a2e98b1ef0e0b59d0f6704c6aa17f6338b32f998410f4") --Main Windows Depot Love Is All Around
+setManifestid(2322567, "4526584360959775877", 16876682624)
+--Dlcname 完蛋！我被美女包围了！-房间里的心跳
+addtoken(2670930, "15969562894690364165")
+addappid(2670930) --Dlcname 完蛋！我被美女包围了！-房间里的心跳
+addappid(2670931, 1, "d76bd9a7d2ea306052e48ad896cad20c27bd736307756b8cfb8a44d0c0c0d3ee") --Dlc Windows Depot 完蛋！我被美女包围了！-房间里的心跳
+setManifestid(2670931, "4554042183527825033", 12440486000)
+addappid(2670932, 1, "46d474a30b36c9570a5ccd7724956fc97bb3b66f73fb4f498d5baaade5917c03") --Dlc Windows Depot 完蛋！我被美女包围了！-房间里的心跳
+setManifestid(2670932, "6346746536273598413", 10312224304)
+--Dlcname 完蛋！我被美女包围了！-本体日语配音版
+addappid(2881050) --Dlcname 完蛋！我被美女包围了！-本体日语配音版
+addappid(2881051, 1, "a9be0ce20df7d69708fba93e8b4c404678ec74a8ff7fffb2af46e1927a3c53d6") --Dlc Windows Depot 完蛋！我被美女包围了！-本体日语配音版
+setManifestid(2881051, "5557555957041099952", 16577383728)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
