3def26cb6c855d2e6053191b5f403914fa51a4df
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-23 20:26:11 +0800

    add 1863430.lua

--- 1863430.lua
+++ 1863430.lua
@@ -1,5 +1,18 @@
-addappid(1863430)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1863431,0,"328f355333eea5102d7e3e43ead832f03f1005e6b29a00b563799c6db5148602")
-setManifestid(1863431,"4375395848025988819")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1863430.lua
+--Gamename Dragonkin: The Banished
+addappid(1863430, 1, "de70eb391abaca321b8c4b5102031f3f9276c7e044f57db1c6f0ac6db5f0ed15") --Mainappid Dragonkin: The Banished
+addappid(1863431, 1, "328f355333eea5102d7e3e43ead832f03f1005e6b29a00b563799c6db5148602") --Main Windows Depot Dragonkin: The Banished
+setManifestid(1863431, "1246109164889912924", 34094137296)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3462450) --Dlcname Dragonkin: The Banished - Founder Pack 1
+addappid(3594840) --Dlcname Dragonkin: The Banished - Founder Pack 2
+addappid(3594850) --Dlcname Dragonkin: The Banished - Founder Pack 3
+addappid(3594860) --Dlcname Dragonkin: The Banished - Founder Pack 4
+addappid(4130000) --Dlcname Dragonkin: The Banished - Lord Pack
+addappid(4508200) --Dlcname Dragonkin: The Banished - Light of the First Praise
+addappid(4844640) --Dlcname Dragonkin: The Banished - War Ancestors Pack
