d49a6db4176334fd70279e1186829e947f893cef
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 15:28:17 +0800

    add 363970.lua

--- 363970.lua
+++ 363970.lua
@@ -1,5 +1,15 @@
-addappid(363970)
-addappid(363971,0,"c6ab5a86c8608fb4c10769b87bc52da5a37d3eea36baa471c08477061018fbc7")
-setManifestid(363971,"2725217437438061743")
-addappid(363972,0,"993d3566c26eaeeef35ef591deb53f215a8b3af28e939af09b190e7b558072f7")
-setManifestid(363972,"4539000020424571348")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 363970.lua
+--Gamename Clicker Heroes
+addappid(363970) --Mainappid Clicker Heroes
+addappid(363971, 1, "c6ab5a86c8608fb4c10769b87bc52da5a37d3eea36baa471c08477061018fbc7") --Main Windows Depot Clicker Heroes
+setManifestid(363971, "5345633450303305706", 311812544)
+addappid(363972, 1, "993d3566c26eaeeef35ef591deb53f215a8b3af28e939af09b190e7b558072f7") --Main Macos Depot Clicker Heroes
+setManifestid(363972, "4539000020424571348", 339647328)
+addappid(556650) --Dlcname Clicker Heroes: Turkey Auto Clicker
+addappid(565261) --Dlcname Clicker Heroes: Red-Nosed Clickdeer
+addappid(591620) --Dlcname Clicker Heroes: Boxy & Bloop Auto Clicker
+addappid(702750) --Dlcname Clicker Heroes: Unicorn Auto Clicker
+addappid(1282910) --Dlcname Clicker Heroes: Whelpling Auto Clicker
+--Missing Dlcs
+--Dlcname Clicker Heroes: Zombie Auto Clicker
