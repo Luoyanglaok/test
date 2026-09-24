89a327e587ebdae477df00430d5b4904a5038fd5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 16:15:13 +0800

    add 2097570.lua

--- 2097570.lua
+++ 2097570.lua
@@ -1,3 +1,9 @@
-addappid(2097570)
-addappid(2097571,0,"162f5956918437b675aeb87b3b4b3392bb4b253d3d41aeb4701ffa463b0d1f1e")
-setManifestid(2097571,"998426951605128997")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2097570.lua
+--Gamename StarVaders
+addappid(2097570) --Mainappid StarVaders
+addappid(2097571, 1, "162f5956918437b675aeb87b3b4b3392bb4b253d3d41aeb4701ffa463b0d1f1e") --Main Windows Depot StarVaders
+setManifestid(2097571, "6700866053707351676", 596426896)
+addappid(2097572, 1, "8ca3d075c533478f0d8c60f9a89ba97bc2687442bdadd81b7c924295e658f8d0") --Main Macos Depot StarVaders
+setManifestid(2097572, "3997936078100020008", 621476464)
+addappid(3632550) --Dlcname StarVaders - Supporter Pack
