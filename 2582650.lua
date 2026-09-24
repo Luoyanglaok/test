4087bf41ebb3d599119ced079f7b71ed705f94e8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 17:29:06 +0800

    add 2582650.lua

--- 2582650.lua
+++ 2582650.lua
@@ -1,9 +1,13 @@
-addappid(2582650)
-addappid(2582651,0,"8ea2fbad7aa8878bd7e674aa550b35e8d4b750a2bce64c0ffeed92a421157200")
-setManifestid(2582651,"8479124733599582833")
-addappid(2582652,0,"f505ee536ae4c88d3f84bfd824d866a2d385b3cb48580c5ecff9ba06d0ba1de4")
-setManifestid(2582652,"3010774533848893987")
-addappid(2582653,0,"8b1ee3f6676e1efa49bb41c979e61634454ca32fe393f8564c2e71d809422460")
-setManifestid(2582653,"6499582607049636633")
-addappid(3047540,0,"a15775e485b7b50aeb3a6ee4b02313660239f1bb19e27e29ffb3867b20600b65")
-setManifestid(3047540,"964963059502681490")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2582650.lua
+--Gamename Enishia and the Binding Brand
+addappid(2582650, 1, "d84f3118044462c8f8f722153a07817300f7d4397fef4f1bee6cfec74f840af9") --Mainappid Enishia and the Binding Brand
+addappid(2582651, 1, "8ea2fbad7aa8878bd7e674aa550b35e8d4b750a2bce64c0ffeed92a421157200") --Main Windows Depot Enishia and the Binding Brand
+setManifestid(2582651, "8479124733599582833", 708091088)
+addappid(2582652, 1, "f505ee536ae4c88d3f84bfd824d866a2d385b3cb48580c5ecff9ba06d0ba1de4") --Main Windows Language Depot Schinese Enishia and the Binding Brand
+setManifestid(2582652, "3010774533848893987", 767720512)
+addappid(2582653, 1, "8b1ee3f6676e1efa49bb41c979e61634454ca32fe393f8564c2e71d809422460") --Main Windows Language Depot Japanese Enishia and the Binding Brand
+setManifestid(2582653, "6499582607049636633", 753789088)
+--Dlcname Enishia and the Binding Brand - Official Enishia Cosplay by MiMi Chan
+addappid(3047540, 1, "a15775e485b7b50aeb3a6ee4b02313660239f1bb19e27e29ffb3867b20600b65") --Dlcname Enishia and the Binding Brand - Official Enishia Cosplay by MiMi Chan
+setManifestid(3047540, "964963059502681490", 3787872)
