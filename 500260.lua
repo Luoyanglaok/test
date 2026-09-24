934d174562f0aaffd75a744d2a2c7d8991d969cf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 19:06:23 +0800

    add 500260.lua

--- 500260.lua
+++ 500260.lua
@@ -1,9 +1,14 @@
-addappid(500260)
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(500261,0,"79e78eb3295371e7da5cfdfa3281892b714e01796e43584c86eadf0d64fd9626")
-setManifestid(500261,"3704783087721842600")
-addappid(640910,0,"c389a981b78db9c34f29fe57d1b0793970b19107a4267e2fe58181e8940a0a10")
-setManifestid(640910,"8212858085194970595")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 500260.lua
+--Gamename Ben and Ed - Blood Party
+addappid(500260, 1, "a499b422ef9de7c942e7bf5cc341b4ed59a81205c89e45601f83ab876ec43cc5") --Mainappid Ben and Ed - Blood Party
+addappid(500261, 1, "79e78eb3295371e7da5cfdfa3281892b714e01796e43584c86eadf0d64fd9626") --Main Windows Depot Ben and Ed - Blood Party
+setManifestid(500261, "3704783087721842600", 794046704)
+--Dlcname Ben And Ed - Blood Party OST
+addappid(640910, 1, "c389a981b78db9c34f29fe57d1b0793970b19107a4267e2fe58181e8940a0a10") --Dlcname Ben And Ed - Blood Party OST
+setManifestid(640910, "8212858085194970595", 228688560)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
