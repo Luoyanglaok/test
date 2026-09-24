2dad747fbcff194a22ae849c9e0f3dca52057882
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 00:06:30 +0800

    add 2996040.lua

--- 2996040.lua
+++ 2996040.lua
@@ -3,13 +3,7 @@
 --Gamename Teenage Mutant Ninja Turtles: Splintered Fate
 addappid(2996040, 1, "96891d9b2870a84c54bb996262f721b1299c79885486e6feb249653388a2d127") --Mainappid Teenage Mutant Ninja Turtles: Splintered Fate
 addappid(2996041, 1, "e93c99408425422103d746643c24b3bc8c717cd5110c9f0b876225db50cc6671") --Main Windows Depot Teenage Mutant Ninja Turtles: Splintered Fate
-setManifestid(2996041, "138817717960842889", 1977383104)
---Dlcname TMNT: Splintered Fate Soundtrack
-addappid(3247420) --Dlcname TMNT: Splintered Fate Soundtrack
-addappid(3247421, 1, "e0fa92800392485535c849929c56e0f55d0fc0f57ede311b0794448ae9564d69") --Dlc Windows Depot TMNT: Splintered Fate Soundtrack
-setManifestid(3247421, "1508636023682389250", 1297424160)
-addappid(3247422, 1, "83a069ffe8d6e373c15ea06e16652993927d88b27e9846ec9a09221b1d44ed1b") --Dlc Windows Depot TMNT: Splintered Fate Soundtrack
-setManifestid(3247422, "1233438326065711824", 208700480)
+setManifestid(2996041, "2089684329114690375", 1977385536)
 --Dlcname TMNT Splintered Fate - Artbook
 addappid(3255900, 1, "200f77802a15d4f75f7441e2bb4dbbdbd90e6419e42bd8e66715b12bb0452284") --Dlcname TMNT Splintered Fate - Artbook
 setManifestid(3255900, "4587529972681717957", 541521232)
