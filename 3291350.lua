72c680e58927624b2b3a32821194aa911f10fb13
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 20:57:47 +0800

    add 3291350.lua

--- 3291350.lua
+++ 3291350.lua
@@ -1,7 +1,11 @@
-addappid(3291350)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(3291351,0,"8980d24c0f24bd60a9f7cfee3e00621887acba5cd0f77448e6e3b58a1f0f8e37")
-setManifestid(3291351,"6088655736303867629")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3291350.lua
+--Gamename 灭绝虚空
+addappid(3291350) --Mainappid 灭绝虚空
+addappid(3291351, 1, "8980d24c0f24bd60a9f7cfee3e00621887acba5cd0f77448e6e3b58a1f0f8e37") --Main Windows Depot 灭绝虚空
+setManifestid(3291351, "6698106250906006724", 2149524096)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
