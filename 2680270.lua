4f09758dbc7468b45100823cc9866b61a9b2108e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 23:27:28 +0800

    add 2680270.lua

--- 2680270.lua
+++ 2680270.lua
@@ -1,3 +1,6 @@
-addappid(2680270)
-addappid(2680271,0,"fbe9342c087a8749040fef7d66fc818dec7c79787b96c98b326d21511605fc51")
-setManifestid(2680271,"7004318294318620163")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2680270.lua
+--Gamename 大侠立志传：碧血丹心 Soundtrack
+addappid(2680270) --Mainappid 大侠立志传：碧血丹心 Soundtrack
+addappid(2680271, 1, "fbe9342c087a8749040fef7d66fc818dec7c79787b96c98b326d21511605fc51") --Main Windows Depot 大侠立志传：碧血丹心 Soundtrack
+setManifestid(2680271, "7004318294318620163", 85003536)
