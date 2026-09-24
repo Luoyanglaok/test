a874696b8a037e607cef507df520b36f07f8a614
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 09:40:24 +0800

    add 1952520.lua

--- 1952520.lua
+++ 1952520.lua
@@ -1,5 +1,16 @@
-addappid(1952520)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1952521,0,"7f72773724787f8f44bafd5fef370cc606d64f80d6c3a0eabb32f11be0b5e36a")
-setManifestid(1952521,"2294223912850185819")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1952520.lua
+--Gamename PAW Patrol World
+addappid(1952520) --Mainappid PAW Patrol World
+addappid(1952521, 1, "7f72773724787f8f44bafd5fef370cc606d64f80d6c3a0eabb32f11be0b5e36a") --Main Windows Depot PAW Patrol World
+setManifestid(1952521, "2294223912850185819", 3750423568)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2399350) --Dlcname PAW Patrol World - Rescue Knights - Costume Pack
+addappid(2399351) --Dlcname PAW Patrol World - Aqua Pups - Costume Pack
+addappid(2399352) --Dlcname PAW Patrol World - Halloween - Costume Pack
+addappid(2399353) --Dlcname PAW Patrol World - Ultimate Rescue - Costume Pack
+addappid(2399354) --Dlcname PAW Patrol World - Mighty Movie - Costume Pack
