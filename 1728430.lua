9334e8d8b724524ad36bd8b89128cdbc89ab01b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 19:37:23 +0800

    add 1728430.lua

--- 1728430.lua
+++ 1728430.lua
@@ -1,3 +1,6 @@
-addappid(1728430)
-addappid(1728431,0,"a42b1558af5ea91e2cff23732593b2d9bb1ff21ae1ec1e5ba9b1ac7a2779fc01")
-setManifestid(1728431,"4010578488052069121")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1728430.lua
+--Gamename I wanna go home
+addappid(1728430) --Mainappid I wanna go home
+addappid(1728431, 1, "a42b1558af5ea91e2cff23732593b2d9bb1ff21ae1ec1e5ba9b1ac7a2779fc01") --Main Windows Language Depot Schinese I wanna go home
+setManifestid(1728431, "4010578488052069121", 285644416)
