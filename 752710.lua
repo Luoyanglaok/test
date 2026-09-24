d0058630ddceacfb15e2c6845a51ea0244cb6e90
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 14:11:52 +0800

    add 752710.lua

--- 752710.lua
+++ 752710.lua
@@ -1,7 +1,10 @@
-addappid(752710)
-addappid(752711,0,"e52e476b119d010aeea7bebebb521353681b827bc45b5b27b111ef3ad1a502a8")
-setManifestid(752711,"2110914712653199474")
-addappid(752712,0,"c53fb2942e6ccbb2164f366c58d65902ff7a40212a0c9f2e2a6c20920f6f95bb")
-setManifestid(752712,"1929040667267248997")
-addappid(752713,0,"68d44f95fedc4f1246102f36e67162b3d88b15f277c5bda6135acd8f6ba31005")
-setManifestid(752713,"8657581695178774276")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 752710.lua
+--Gamename Infinity Trip
+addappid(752710) --Mainappid Infinity Trip
+addappid(752711, 1, "e52e476b119d010aeea7bebebb521353681b827bc45b5b27b111ef3ad1a502a8") --Main Windows Depot Infinity Trip
+setManifestid(752711, "2110914712653199474", 12801072)
+addappid(752712, 1, "c53fb2942e6ccbb2164f366c58d65902ff7a40212a0c9f2e2a6c20920f6f95bb") --Main Macos Depot Infinity Trip
+setManifestid(752712, "1929040667267248997", 16693280)
+addappid(752713, 1, "68d44f95fedc4f1246102f36e67162b3d88b15f277c5bda6135acd8f6ba31005") --Main Linux Depot Infinity Trip
+setManifestid(752713, "8657581695178774276", 28817088)
