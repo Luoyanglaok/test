062127d623bcbd9832f849dec04db9114050a7fb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 23:09:27 +0800

    add 2517220.lua

--- 2517220.lua
+++ 2517220.lua
@@ -1,5 +1,13 @@
-addappid(2517220)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(2517221,0,"172154d86126f1a6c064d6901fd4f1a346dc8a781f9e3e35eb98973b29574d93")
-setManifestid(2517221,"8653158269615390400")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2517220.lua
+--Gamename ENTITY: THE BLACK DAY
+addappid(2517220, 1, "9a5f389509214a4e03900b61ba4fdc0315e65b6604826babb018355d20996ec9") --Mainappid ENTITY: THE BLACK DAY
+addappid(2517221, 1, "172154d86126f1a6c064d6901fd4f1a346dc8a781f9e3e35eb98973b29574d93") --Main Windows Depot ENTITY: THE BLACK DAY
+setManifestid(2517221, "1249285104132062158", 38809096192)
+addappid(2517222, 1, "a3dd758b8fbc982d31076474680fd8b805f1a9b5efc08d56ede165d188fcb3dd") --Main Windows Depot ENTITY: THE BLACK DAY
+setManifestid(2517222, "791017410246693638", 15700718544)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
