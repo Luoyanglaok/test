b6bd737be05cb205861d0022b050914e743a70db
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 00:22:12 +0800

    add 1274570.lua

--- 1274570.lua
+++ 1274570.lua
@@ -9,12 +9,6 @@ setManifestid(1274573, "7186629756615425564", 8680307568)
 --Dlcname DEVOUR: Supporter Edition
 addappid(1966630, 1, "9b579f914a2ba495ad1e0b1e1a1fc21c8e578ee933fb6bfabd9f138124244587") --Dlcname DEVOUR: Supporter Edition
 setManifestid(1966630, "7348617984918166394", 92388272)
---Dlcname DEVOUR: Soundtrack
-addappid(1977260) --Dlcname DEVOUR: Soundtrack
-addappid(1977261, 1, "b8fe2b413baf1a2f70d13eb0189a16a2113ca9b11dcad56196a5ee8f618c81ac") --Dlc Windows Depot DEVOUR: Soundtrack
-setManifestid(1977261, "3996702687201802107", 231874704)
-addappid(1977262, 1, "a94dd08266eee983f548031945f935947eb7b6148f4af8167ab67e2993aceb0b") --Dlc Windows Depot DEVOUR: Soundtrack
-setManifestid(1977262, "3321894516755209546", 1812635872)
 --Share Depots
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228988, "6645201662696499616", 22411856)
