e11eaf7cb8ddbd84814b078b013e6e55252cc38d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 14:18:41 +0800

    add 1546970.lua

--- 1546970.lua
+++ 1546970.lua
@@ -1,7 +1,13 @@
-addappid(1546970)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1546971,0,"d6dfa8078e89488b58be9d37f6b0e04acd6535686db4037c9036f3ef6b552b84")
-setManifestid(1546971,"5655906003401315274")
-addappid(1899671,0,"b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b")
-setManifestid(1899671,"1313721849634300954")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1546970.lua
+--Gamename Grand Theft Auto III – The Definitive Edition
+addappid(1546970, 1, "1e61fd2273045bb7c6dc9de6b65884b5ab1b005df323bea63f60570b84f09652") --Mainappid Grand Theft Auto III – The Definitive Edition
+addappid(1546971, 1, "d6dfa8078e89488b58be9d37f6b0e04acd6535686db4037c9036f3ef6b552b84") --Main Windows Depot Grand Theft Auto III – The Definitive Edition
+setManifestid(1546971, "5655906003401315274", 4427409968)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1899671, 1, "b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b") --Share Windows Depot RGL/SC
+setManifestid(1899671, "3914969219305676290", 209058416)
