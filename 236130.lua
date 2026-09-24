58126b25d3579b67179c22d0740a4dda437e5c2b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 14:07:03 +0800

    add 236130.lua

--- 236130.lua
+++ 236130.lua
@@ -1,20 +1,25 @@
-addappid(236130)
-addappid(228983)
-setManifestid(228983,"8124929965194586177")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(229000)
-setManifestid(229000,"4622705914179893434")
-addappid(236131,0,"5f435ce3871c1f4c7d3f929a47b9a1ab81bdee77fbe1d81cf36722d8caba6523")
-setManifestid(236131,"6941477267080632860")
-addappid(236132,0,"1da011bb2f315a8d51b37cdd6c380708643d7dad3a6db3906de6e4a5077cc8bf")
-setManifestid(236132,"7548759156487410892")
-addappid(236133,0,"210bc017376d532d426733ab6001214682866cb31ff4dd592171b0b5118aea7c")
-setManifestid(236133,"5519013101181344599")
-addappid(236134,0,"2821c5bcdd044da85bff6de306f6db70c869927aad86196aaaebb15e441e3084")
-setManifestid(236134,"8638358569134013461")
-addappid(236135,0,"e2202998c262f7220848473861f6df98d20cd10db246989d92fd46a919bfea82")
-setManifestid(236135,"734482378083589949")
-addappid(236136,0,"b4038ea2a11aea677a3400fbd15644fa909f5ba64a9ad2f9bb251c2788b7a0d6")
-setManifestid(236136,"7855772952697589795")
-addappid(2586390)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 236130.lua
+--Gamename Horizon
+addappid(236130) --Mainappid Horizon
+addappid(236131, 1, "5f435ce3871c1f4c7d3f929a47b9a1ab81bdee77fbe1d81cf36722d8caba6523") --Main Windows Depot Horizon
+setManifestid(236131, "6941477267080632860", 3547261600)
+addappid(236132, 1, "1da011bb2f315a8d51b37cdd6c380708643d7dad3a6db3906de6e4a5077cc8bf") --Main Windows Language Depot German Horizon
+setManifestid(236132, "7548759156487410892", 358496976)
+addappid(236133, 1, "210bc017376d532d426733ab6001214682866cb31ff4dd592171b0b5118aea7c") --Main Windows Language Depot Polish Horizon
+setManifestid(236133, "5519013101181344599", 358623072)
+addappid(236134, 1, "2821c5bcdd044da85bff6de306f6db70c869927aad86196aaaebb15e441e3084") --Main Windows Language Depot French Horizon
+setManifestid(236134, "8638358569134013461", 357753216)
+addappid(236135, 1, "e2202998c262f7220848473861f6df98d20cd10db246989d92fd46a919bfea82") --Main Windows Language Depot Spanish Horizon
+setManifestid(236135, "734482378083589949", 357334528)
+addappid(236136, 1, "b4038ea2a11aea677a3400fbd15644fa909f5ba64a9ad2f9bb251c2788b7a0d6") --Main Windows Language Depot Russian Horizon
+setManifestid(236136, "7855772952697589795", 352665952)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229000, "4622705914179893434", 242681440)
+--Missing Dlcs
+--Dlcname Horizon - Deluxe Edition Pack
