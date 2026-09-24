31a8185f92c1d676b4aaa67b5e26f334b9d13dc9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 17:37:31 +0800

    add 1666510.lua

--- 1666510.lua
+++ 1666510.lua
@@ -1,7 +1,10 @@
-addappid(1666510)
-addappid(1666511,0,"aea822186f18b67f909bc68d4bdde4afb3979733c71a85c0a030bb3a48ad9d9b")
-setManifestid(1666511,"3985566660234756828")
-addappid(1666512,0,"12858de25781d51a7caf57207fe1a79a35a8ae3b8d5573e48e2f040baa106189")
-setManifestid(1666512,"1695435134193830792")
-addappid(1666513,0,"060601d1af565790d94758aade93cf2c190d013c01c33668d5a7f31afafe63de")
-setManifestid(1666513,"5597867310672706407")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1666510.lua
+--Gamename Magical Valkyrie Lyristia
+addappid(1666510, 1, "a140df235b4c94d86c41216d9ef0f2df24e36cb0ae17f0ec3abbba351bebc82a") --Mainappid Magical Valkyrie Lyristia
+addappid(1666511, 1, "aea822186f18b67f909bc68d4bdde4afb3979733c71a85c0a030bb3a48ad9d9b") --Main Windows Depot Magical Valkyrie Lyristia
+setManifestid(1666511, "3985566660234756828", 605276368)
+addappid(1666512, 1, "12858de25781d51a7caf57207fe1a79a35a8ae3b8d5573e48e2f040baa106189") --Main Windows Language Depot Schinese Magical Valkyrie Lyristia
+setManifestid(1666512, "1695435134193830792", 19210656)
+addappid(1666513, 1, "060601d1af565790d94758aade93cf2c190d013c01c33668d5a7f31afafe63de") --Main Windows Language Depot Japanese Magical Valkyrie Lyristia
+setManifestid(1666513, "5597867310672706407", 13365264)
