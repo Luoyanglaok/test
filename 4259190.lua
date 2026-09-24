585bb12a7a24cfcf24d662524f7733c087d1e932
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 07:47:39 +0800

    add 4259190.lua

--- 4259190.lua
+++ 4259190.lua
@@ -3,9 +3,9 @@
 --Gamename Kingdom Rush 6: Genesis TD
 addappid(4259190) --Mainappid Kingdom Rush 6: Genesis TD
 addappid(4259192, 1, "4dc51c297d7d690b79deea89d6c611bfc1a7f5436ed178d0dbc2ce1ea6430f36") --Main Windows Depot Kingdom Rush 6: Genesis TD
-setManifestid(4259192, "1420456421438013594", 1191578608)
+setManifestid(4259192, "8043278291481853507", 1191567632)
 addappid(4259193, 1, "383091654e01c6c4ee29ee667284bb28f142e92060ec71661bc7d740527ef41a") --Main Macos Depot Kingdom Rush 6: Genesis TD
-setManifestid(4259193, "7426064351090442559", 1195390432)
+setManifestid(4259193, "2033217607701440974", 1195373152)
 --Share Depots
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228988, "6645201662696499616", 22411856)
