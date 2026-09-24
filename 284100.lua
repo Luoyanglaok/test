9ba5185445ae22149b010562a9c818ba8a730fdd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 19:46:57 +0800

    add 284100.lua

--- 284100.lua
+++ 284100.lua
@@ -1,9 +1,14 @@
-addappid(284100)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(229004)
-setManifestid(229004,"5220958916987797232")
-addappid(284101,0,"66152da777fb68f29aa10cbff6b276236b3f4094d715ef3ce41ac2f0fbfed1a5")
-setManifestid(284101,"3159369609451446961")
-addappid(536790,0,"b4a7c188ecd0010f115e11edfb5ed0b56c98efc41da79471493904f192a6b84a")
-setManifestid(536790,"96715669743251879")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 284100.lua
+--Gamename Unclaimed World
+addappid(284100) --Mainappid Unclaimed World
+addappid(284101, 1, "66152da777fb68f29aa10cbff6b276236b3f4094d715ef3ce41ac2f0fbfed1a5") --Main Windows Depot Unclaimed World
+setManifestid(284101, "3159369609451446961", 223021728)
+--Dlcname Unclaimed World - Soundtrack
+addappid(536790, 1, "b4a7c188ecd0010f115e11edfb5ed0b56c98efc41da79471493904f192a6b84a") --Dlcname Unclaimed World - Soundtrack
+setManifestid(536790, "96715669743251879", 103505728)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229004, "5220958916987797232", 70811632)
