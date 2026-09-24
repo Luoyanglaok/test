a6b5f9043c9b31d83c03a8add1f06d7021e0cc3d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 00:15:23 +0800

    add 668580.lua

--- 668580.lua
+++ 668580.lua
@@ -4,12 +4,6 @@
 addappid(668580, 1, "5a1ed8d8d2a8110fde9be6e0ec543d96db83954f381710c70e330b5c010841e7") --Mainappid Atomic Heart
 addappid(668581, 1, "b331448756a6614f7b9b52f56b71e329928370afb8034b8474e264ace0048928") --Main Windows Depot Atomic Heart
 setManifestid(668581, "1879284477080962633", 85777880992)
---Dlcname Atomic Heart - Original Soundtrack
-addappid(2303120) --Dlcname Atomic Heart - Original Soundtrack
-addappid(2303121, 1, "6fd3db1eba411d9ecc68741d7ebf12f38bfb738c8578dd71419af57f7b2f73c4") --Dlc Windows Depot Atomic Heart - Original Soundtrack
-setManifestid(2303121, "3587340790320923187", 835147072)
-addappid(2303122, 1, "49808584cc51f185a98e76678db9045fcfccc7474b9bccb847a92aa223ed8aff") --Dlc Windows Depot Atomic Heart - Original Soundtrack
-setManifestid(2303122, "41228753438295057", 5180338176)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
