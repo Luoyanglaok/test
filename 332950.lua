d2a5f3b292dcee0694a3670b953044884c5ff865
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 01:09:50 +0800

    add 332950.lua

--- 332950.lua
+++ 332950.lua
@@ -1,15 +1,36 @@
-addappid(332950)
-setManifestid(228981,"7613356809904826842")
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229030,"1043465440436835055")
-setManifestid(229031,"7746630274301172884")
-setManifestid(229032,"3616495131483866412")
-setManifestid(229033,"2059065101492814639")
-addappid(332951,0,"6a39198ed2507581c09af1361425691fc64d1fd13d251b5b951e011d2dbaf3cd")
-setManifestid(332951,"179954252936033447")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 332950.lua
+--Gamename Dark Deception
+addappid(332950) --Mainappid Dark Deception
+addappid(332951, 1, "6a39198ed2507581c09af1361425691fc64d1fd13d251b5b951e011d2dbaf3cd") --Main Windows Depot Dark Deception
+setManifestid(332951, "179954252936033447", 8002849184)
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229030, 1, "92f0f88867c8d5f351090eb2d27aeb50e2b621ae8a34708265d2c20c36fbcf5e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229030, "1043465440436835055", 50071472)
+addappid(229031, 1, "98d01f1eaa90ae08f978994e043bcbbf1f23fc72a325528ca7d722c827c0eb54") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229031, "7746630274301172884", 25905472)
+addappid(229032, 1, "15b3308d4e95d1156182211bab1c823b6df0a06fd74a5c0833abed89bc307cfb") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229032, "3616495131483866412", 40038080)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229033, "2059065101492814639", 59325760)
+addappid(1017030) --Dlcname Dark Deception Chapter 2
+addappid(1019930) --Dlcname Dark Deception Chapter 3
+addappid(1019931) --Dlcname Dark Deception Chapter 4
+addappid(1019932) --Dlcname Dark Deception Chapter 5
+addappid(1022850) --Dlcname Dark Deception Complete
