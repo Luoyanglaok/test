855030b4965d82b888e1f264a6962a3c20f649f3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 23:57:42 +0800

    add 3953160.lua

---
+++ 3953160.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3953160.lua
+--Gamename Human Farm : Investigation :
+addappid(3953160, 1, "c9e3f4d23c9d8014d9e787ec21e2b4166bc021781e5a629ec1e5986d4584b088") --Mainappid Human Farm : Investigation :
+addappid(3953161, 1, "6de14ae48206c7b0bd0237d2a58ba47d6da0f4cfd3f7c29051a6517abef0181b") --Main Windows Language Depot Japanese Human Farm : Investigation :
+setManifestid(3953161, "3003183948133214008", 599131328)
+addappid(3953162, 1, "09bf58da01f4a536434ec0a447e1ea79c200109bdf20c754c4916afe31b4a656") --Main Windows Language Depot English Human Farm : Investigation :
+setManifestid(3953162, "294571912678865610", 653471232)
+addappid(3953163, 1, "cd52839eff4a33a62a0c8e5ad29f69180c4dec6a70949e39ff2f8992ed137d8f") --Main Windows Language Depot Tchinese Human Farm : Investigation :
+setManifestid(3953163, "7439386873336868546", 644597504)
+addappid(3953164, 1, "761cbd00757821507006f714b6676029bfbdca767d85bd3d027dc36d1dfc661a") --Main Windows Language Depot Schinese Human Farm : Investigation :
+setManifestid(3953164, "6883790803730251589", 644610000)
