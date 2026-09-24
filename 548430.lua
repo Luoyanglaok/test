27e3cd859c081843c26c22b92a01d74e11249c1e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 12:52:06 +0800

    add 548430.lua

--- 548430.lua
+++ 548430.lua
@@ -3,17 +3,7 @@
 --Gamename Deep Rock Galactic
 addappid(548430, 1, "35e74cda02258a1dc29466af2d3c05f2030830981ead0936e3eb6c9d59284e7d") --Mainappid Deep Rock Galactic
 addappid(548431, 1, "73ef10dc344cb7fb34be23aea35b20fbb2b32b0db50650919c7e3d731e759aad") --Main Windows Depot Deep Rock Galactic
-setManifestid(548431, "5775322591771794477", 3600352304)
---Dlcname Deep Rock Galactic - Original Soundtrack Volume I + II
-addappid(802640) --Dlcname Deep Rock Galactic - Original Soundtrack Volume I + II
-addappid(802641, 1, "4a25bdb0198768cfc3277d4892ad41f45d19762c101711c73f7fd229b60e1df2") --Dlc Windows Depot Deep Rock Galactic - Original Soundtrack Volume I + II
-setManifestid(802641, "3260459802955998328", 239409152)
-addappid(802642, 1, "d448c4f4a1f48362382656c111c5dc7e5f18a01b98d86d4dc9f821781d563ba5") --Dlc Windows Depot Deep Rock Galactic - Original Soundtrack Volume I + II
-setManifestid(802642, "1312881002778069876", 555620304)
-addappid(802643, 1, "3f7c047aaeda45352662542387c5b0c6012529258d3a4ee7bfb72da9395b55f8") --Dlc Windows Depot Deep Rock Galactic - Original Soundtrack Volume I + II
-setManifestid(802643, "2206979629215810349", 160881952)
-addappid(802644, 1, "fb00dd61b77af7a766e93dec241b8d04576c0e1768cff0677448d00e965ce75a") --Dlc Windows Depot Deep Rock Galactic - Original Soundtrack Volume I + II
-setManifestid(802644, "5048846235105489207", 380614720)
+setManifestid(548431, "4605235793608540204", 3608277088)
 --Share Depots
 addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228986, "8782296191957114623", 23045488)
