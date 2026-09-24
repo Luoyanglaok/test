9743280865b691193eb3e191df7a73e102c3c8e6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 00:02:50 +0800

    add 4147820.lua

---
+++ 4147820.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4147820.lua
+--Gamename Your Mom
+addappid(4147820, 1, "c8d270556cdecd8005f1a549a68df9d8e6eae1c46c8ea059fdf4790543039f1a") --Mainappid Your Mom
+addappid(4147821, 1, "c7402a81531af57513c9124867754dfe6be6b039c3e68fa3c8ba1597870a51f1") --Main Windows Depot Your Mom
+setManifestid(4147821, "5556739627576139428", 221208160)
+addappid(4147822, 1, "8f95e66ed22e2cc7ea1e2c7e20d12fe47d7af4f5a4192bb7c89e3820c74a7f4a") --Main Macos Depot Your Mom
+setManifestid(4147822, "2168722774619152276", 226011536)
+--Dlcname Your Mom - DLC
+addappid(4206120, 1, "84c8a7ed06ebda90864c5e66cf33136e50891102fb6796edd7958b922f916cac") --Dlcname Your Mom - DLC
+setManifestid(4206120, "52280185410003384", 197606096)
+addappid(4206121, 1, "a2448a5d10b2e553aada3e48c342ed6d6a4f03d14e7c1500f374444eb9f0762a") --Dlc Macos Depot Your Mom - DLC
+setManifestid(4206121, "6224105634717185247", 201828016)
+--Dlcname Your Mom's DLC
+addappid(4442970, 1, "a291c4a0c9d67d9f8d4b01f16092b9f31b95a412509167a3c38a58fffa7b6e35") --Dlcname Your Mom's DLC
+setManifestid(4442970, "8343712599994125222", 407876576)
