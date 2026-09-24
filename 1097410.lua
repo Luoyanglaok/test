186bd083a06e85e4d0b2932f4d5d104c97984fa1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 15:40:42 +0800

    add 1097410.lua

---
+++ 1097410.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1097410.lua
+--Gamename Kittens Game
+addappid(1097410, 1, "82ee27e72fc3ad3a30d212918a998248b8a8d1a39b15911047f91a3cb3323afd") --Mainappid Kittens Game
+addappid(1097412, 1, "0eb42ff7a71b336c2db741a77b9700923f7f678e47bce3be227f28d59ddbadca") --Main Linux Depot Kittens Game
+setManifestid(1097412, "7361061520059729134", 128747600)
+addappid(1097414, 1, "962a76d07336e1c35ba09fdd9c8f13624f4dacfbd520198bbf15c35a1d5ae3ae") --Main Windows Depot Kittens Game
+setManifestid(1097414, "6039910173829677600", 135098480)
