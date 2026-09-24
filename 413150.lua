0a711b449910cdba5a352915390f256d3086a8b8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 19:51:15 +0800

    add 413150.lua

--- 413150.lua
+++ 413150.lua
@@ -8,10 +8,6 @@ addappid(413152, 1, "c65e7919242bdcb29efd9551af381e9b6f5d828ac7382cec83f1d8459aa
 setManifestid(413152, "2618652301291810021", 456794576)
 addappid(413153, 1, "d1e04015717ef68e43064bd29a027c3309b47bd156d0494c7d4dff56a971e3c7") --Main Linux Depot Stardew Valley
 setManifestid(413153, "6005910083361727734", 457681328)
---Dlcname Stardew Valley Soundtrack
-addappid(440820, 1, "ed2360d39a75d023e9cb2b89cb5e96f9843f575ae8223776874b13a21ca231dd") --Dlcname Stardew Valley Soundtrack
-addappid(440821, 1, "b7326aa9312d03f8acabdcb537792cd63cf4e5b611a7a17a5ae5c3055ccea894") --Dlc Windows Depot Stardew Valley Soundtrack
-setManifestid(440821, "5706279157015252834", 332305904)
 --Share Depots
 addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(229002, "7260605429366465749", 50365280)
