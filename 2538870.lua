e7fd5a6e326488f13d883c1aa9bb303f6913458d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 22:20:41 +0800

    add 2538870.lua

---
+++ 2538870.lua
@@ -0,0 +1,7 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2538870.lua
+--Gamename False Hero
+addappid(2538870) --Mainappid False Hero
+addappid(2538871, 1, "b458e53f61e43a095e434040391dc0302ea65e1f86e0cf29dde938d3fb71077d") --Main Windows Depot False Hero
+setManifestid(2538871, "8216470677462903926", 7376074736)
+addappid(4739180) --Dlcname False Hero - Supporter Offering
