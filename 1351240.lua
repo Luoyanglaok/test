5555ca39913b0aa0923e87038e0fefc74133d21f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 17:58:30 +0800

    add 1351240.lua

--- 1351240.lua
+++ 1351240.lua
@@ -1,5 +1,19 @@
-addappid(1351240)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(1351241,0,"9e79fc452f59614517425d51bae47b42451e0a8aaba07aa8b48b756424d68539")
-setManifestid(1351241,"6805473830722308430")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1351240.lua
+--Gamename Taxi Life: A City Driving Simulator
+addappid(1351240) --Mainappid Taxi Life: A City Driving Simulator
+addappid(1351241, 1, "9e79fc452f59614517425d51bae47b42451e0a8aaba07aa8b48b756424d68539") --Main Windows Depot Taxi Life: A City Driving Simulator
+setManifestid(1351241, "5002496358135747319", 4061096000)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2633670) --Dlcname Taxi Life: A City Driving Simulator - Supporter Pack
+addappid(2633680) --Dlcname Taxi Life: A City Driving Simulator - VIP Vintage Convertible Car
+addappid(3197460) --Dlcname Taxi Life: A City Driving Simulator - Summer Cosmetic Pack
+addappid(3260720) --Dlcname Taxi Life: A City Driving Simulator - Halloween Cosmetic Pack
+addappid(3335520) --Dlcname Taxi Life: A City Driving Simulator - Christmas Cosmetic Pack
+addappid(3384710) --Dlcname Taxi Life: A City Driving Simulator - Delivery Driver
+addappid(3576810) --Dlcname Taxi Life: A City Driving Simulator - VIP City Guide
+addappid(3854360) --Dlcname Taxi Life: A City Driving Simulator - Black Cab
