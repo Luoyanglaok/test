2be7350e2d950e19fd467d00c1dfcfa8c41ac946
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 17:26:16 +0800

    add 1078350.lua

--- 1078350.lua
+++ 1078350.lua
@@ -1,7 +1,10 @@
-addappid(1078350)
-addappid(1078351,0,"f1b7d5bee1dbdcb18667b63b903c20aa60f61bfc0503ad947154d3e5bd571454")
-setManifestid(1078351,"4091815204860947401")
-addappid(1078352,0,"70534fea5710b3898e8dfc255a4643be10016a18fc439c4b7c01c8a4bd1fe10f")
-setManifestid(1078352,"7207689411825628387")
-addappid(1078353,0,"91abff83d9c70dba592a56841491159f0352c31704f7140b36d05d9ad6d49182")
-setManifestid(1078353,"1490217983809068366")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1078350.lua
+--Gamename Ambrosia
+addappid(1078350, 1, "ba3f05afc38a9f120b27a5470f1460ec50c7726add9b4b9b6ee01a7f099ac88b") --Mainappid Ambrosia
+addappid(1078351, 1, "f1b7d5bee1dbdcb18667b63b903c20aa60f61bfc0503ad947154d3e5bd571454") --Main Windows Depot Ambrosia
+setManifestid(1078351, "4091815204860947401", 879701616)
+addappid(1078352, 1, "70534fea5710b3898e8dfc255a4643be10016a18fc439c4b7c01c8a4bd1fe10f") --Main Windows Language Depot Schinese Ambrosia
+setManifestid(1078352, "7207689411825628387", 246750224)
+addappid(1078353, 1, "91abff83d9c70dba592a56841491159f0352c31704f7140b36d05d9ad6d49182") --Main Windows Language Depot Japanese Ambrosia
+setManifestid(1078353, "1490217983809068366", 235299168)
