6c37a1a0a96ece04b11015a3ee974f6ebae4c532
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 23:07:42 +0800

    add 5053820.lua

--- 5053820.lua
+++ 5053820.lua
@@ -3,7 +3,5 @@
 --Gamename Mimic Party
 addappid(5053820, 1, "3687a910eb76c9e3eccddcb2ce6cc97c4bc5667ef8e20e668cbedc150283773a") --Mainappid Mimic Party
 addappid(5053821, 1, "3bf0ccc9e3c7c0ff61adcadc28e45680676fe6f9a82c9c4ad55c417c532c35e6") --Main Windows Depot Mimic Party
-setManifestid(5053821, "4902613313277987759", 153045632)
+setManifestid(5053821, "3712186476495122709", 154625776)
 addappid(5168110) --Dlcname All Characters
---Missing Dlcs
---Dlcname Mimic Party Playtest
