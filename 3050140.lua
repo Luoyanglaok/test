b4f6ebeb7fc7f01b8878bb6c37d97cef22f4f76d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 17:35:11 +0800

    add 3050140.lua

--- 3050140.lua
+++ 3050140.lua
@@ -1,7 +1,10 @@
-addappid(3050140)
-addappid(3050141,0,"686eba539c686cd7e11cf85b49d996cbba080e9b0c57203382f30e09a9d5d5e9")
-setManifestid(3050141,"3103635125461049595")
-addappid(3050142,0,"5383e3ad1ff7172ed456cc3d5d664aa8365fe105e5d061517137694b56e08170")
-setManifestid(3050142,"8135715556116278715")
-addappid(3050143,0,"8f06b578e2debab18ea9ad40c9e44a8ac60a42f3d6abd60d7962d148e075e60c")
-setManifestid(3050143,"6719885662547009481")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3050140.lua
+--Gamename Radiant Princess - Poni Ceres
+addappid(3050140, 1, "4d95a8dd20d033f0e95fce82de1ec7ec6ad4efa21f930633f6201f903a11c4a0") --Mainappid Radiant Princess - Poni Ceres
+addappid(3050141, 1, "686eba539c686cd7e11cf85b49d996cbba080e9b0c57203382f30e09a9d5d5e9") --Main Windows Depot Radiant Princess - Poni Ceres
+setManifestid(3050141, "3103635125461049595", 748243088)
+addappid(3050142, 1, "5383e3ad1ff7172ed456cc3d5d664aa8365fe105e5d061517137694b56e08170") --Main Windows Language Depot Schinese Radiant Princess - Poni Ceres
+setManifestid(3050142, "8135715556116278715", 706679312)
+addappid(3050143, 1, "8f06b578e2debab18ea9ad40c9e44a8ac60a42f3d6abd60d7962d148e075e60c") --Main Windows Language Depot Japanese Radiant Princess - Poni Ceres
+setManifestid(3050143, "6719885662547009481", 702525792)
