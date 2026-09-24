05c95900c1bf17413b0aa79499c423b4ea720107
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 20:02:17 +0800

    add 1299360.lua

--- 1299360.lua
+++ 1299360.lua
@@ -1,9 +1,13 @@
-addappid(1299360)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1299361,0,"ceb18d12e7cb9a34b06e387a69ea822778755935157897637c66f58e671c229b")
-setManifestid(1299361,"497537559640704304")
-addappid(1299362,0,"fed834b69d44f9bb1310d4103b16f20743a42c45c645cc1d9e82b2d46098f5d0")
-setManifestid(1299362,"2881461251181707675")
-addappid(1299363,0,"9118511d91fa92341a5529c12167d36b0221394a6be495ea558acda9df7ca3b7")
-setManifestid(1299363,"2076139861466872795")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1299360.lua
+--Gamename Mighty Goose
+addappid(1299360) --Mainappid Mighty Goose
+addappid(1299361, 1, "ceb18d12e7cb9a34b06e387a69ea822778755935157897637c66f58e671c229b") --Main Windows Depot Mighty Goose
+setManifestid(1299361, "497537559640704304", 140525632)
+addappid(1299362, 1, "fed834b69d44f9bb1310d4103b16f20743a42c45c645cc1d9e82b2d46098f5d0") --Main Macos Depot Mighty Goose
+setManifestid(1299362, "2881461251181707675", 140581664)
+addappid(1299363, 1, "9118511d91fa92341a5529c12167d36b0221394a6be495ea558acda9df7ca3b7") --Main Linux Depot Mighty Goose
+setManifestid(1299363, "2076139861466872795", 145260960)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
