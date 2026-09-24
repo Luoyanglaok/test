bd9cf77b1a3ae5d4a036ca5cd5715ed16365a65a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 14:49:46 +0800

    add 985890.lua

--- 985890.lua
+++ 985890.lua
@@ -1,8 +1,14 @@
-addappid(985890)
-setManifestid(228986,"8782296191957114623")
-addappid(985891,0,"334d196369a500638de8a9a02c66424ef3b3a519c858d3699911fdeb53deba76")
-setManifestid(985891,"3273645082641574196")
-addappid(985892,0,"c1c833c8664eecb7d7d21a056367ce7349b84f6fa25e160498bb581c6728e938")
-setManifestid(985892,"3854442273198102502")
-addappid(985893,0,"6c963ed6d4f98f40848a2b775a16737dc05ac6554c3122b0870140512531f4dd")
-setManifestid(985893,"7379902906825171084")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 985890.lua
+--Gamename Streets of Rage 4
+addappid(985890, 1, "40e5ea9572e449a54359de04aac79ec18043cd386b6b6085513ca25bbaa23f77") --Mainappid Streets of Rage 4
+addappid(985891, 1, "334d196369a500638de8a9a02c66424ef3b3a519c858d3699911fdeb53deba76") --Main Windows Depot Streets of Rage 4
+setManifestid(985891, "3273645082641574196", 6218883600)
+addappid(985892, 1, "c1c833c8664eecb7d7d21a056367ce7349b84f6fa25e160498bb581c6728e938") --Main Linux Depot Streets of Rage 4
+setManifestid(985892, "3854442273198102502", 6223411248)
+addappid(985893, 1, "6c963ed6d4f98f40848a2b775a16737dc05ac6554c3122b0870140512531f4dd") --Main Macos Depot Streets of Rage 4
+setManifestid(985893, "7379902906825171084", 6225916384)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(1585820) --Dlcname Streets Of Rage 4 - Mr. X Nightmare
