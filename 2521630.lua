79189b77fe2fccf3cb0e8b13860479e976052e31
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 20:53:18 +0800

    add 2521630.lua

--- 2521630.lua
+++ 2521630.lua
@@ -1,3 +1,6 @@
-addappid(2521630)
-addappid(2521631,0,"d06bb3d739ba94610c96828ed1642365fa3dfbefbd811f058750a5a33d22c706")
-setManifestid(2521631,"4656086993732340043")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2521630.lua
+--Gamename Mini Settlers
+addappid(2521630) --Mainappid Mini Settlers
+addappid(2521631, 1, "d06bb3d739ba94610c96828ed1642365fa3dfbefbd811f058750a5a33d22c706") --Main Windows Depot Mini Settlers
+setManifestid(2521631, "4656086993732340043", 325056912)
