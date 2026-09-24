1384f15ebb8915d347aab952160b2353548007da
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 17:52:19 +0800

    add 4529120.lua

--- 4529120.lua
+++ 4529120.lua
@@ -1,2 +1,6 @@
-addappid(4529120)
-setManifestid(4529121,"4818786693772077239")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4529120.lua
+--Gamename Alice's Gaze
+addappid(4529120, 1, "f2b35865af0ad8fba58c85f2eebe46d825158cdfb8062e07a82c6e76fcd3b018") --Mainappid Alice's Gaze
+addappid(4529121, 1, "059a8acd1babf801a92d41e4273b5b3b1a023cd35e3ff0ef0480b5db5458776d") --Main Windows Depot Alice's Gaze
+setManifestid(4529121, "8087779114038550757", 93662624)
