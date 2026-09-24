bdfaa3f54f7d9ee4f2b2a009f95b021efb00cbd9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 18:09:00 +0800

    add 1236300.lua

--- 1236300.lua
+++ 1236300.lua
@@ -1,5 +1,29 @@
-addappid(1236300)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1236301,0,"d871901d18077503a33843f0aed308396db2824654f75fe5217f6ed3c91f1de7")
-setManifestid(1236301,"4760446556874430665")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1236300.lua
+--Gamename Resident Evil Re:Verse
+addappid(1236300, 1, "999d2ac43f1c8862e62d1a523efb1018449e26d3a26b00119e60f23bca19468e") --Mainappid Resident Evil Re:Verse
+addappid(1236301, 1, "d871901d18077503a33843f0aed308396db2824654f75fe5217f6ed3c91f1de7") --Main Windows Depot Resident Evil Re:Verse
+setManifestid(1236301, "4760446556874430665", 15699884144)
+--Dlcname AppID 1607011
+addappid(1607011, 1, "dde5c7d921201c6e3a4bf9a1551bb0463bbfaa2b7735075914ff7e2a85503e60") --Dlcname AppID 1607011
+--Dlcname AppID 1607012
+addappid(1607012, 1, "5769c59d7cf4a036fe9287374f3e37b8cd1878b6494e98a015ed637488072525") --Dlcname AppID 1607012
+--Dlcname AppID 1607013
+addappid(1607013, 1, "d8c652a69968a515baed2389fa1348eda69435b3b3e5be229afc0650a2303018") --Dlcname AppID 1607013
+--Dlcname AppID 1607014
+addappid(1607014, 1, "e8730ab2ae2dbb9236976b1a0c274b9f000d1203fbfd735c22692967a0e00b72") --Dlcname AppID 1607014
+--Dlcname AppID 1607015
+addappid(1607015, 1, "5ed76f78b1c19a832c3429e1b6dcf20f47728b9929345c2baf5ab3999efec18d") --Dlcname AppID 1607015
+--Dlcname AppID 1607016
+addappid(1607016, 1, "e9c6ded588d45885902dd7e0c8b7aedca4bc4770b7f66422fa92b8a5cda39a06") --Dlcname AppID 1607016
+--Dlcname AppID 1607017
+addappid(1607017, 1, "11710792035c5bfe1397dcdf892f8ca8d16b72cac8b92580d4c178e5192febe7") --Dlcname AppID 1607017
+--Dlcname AppID 1607018
+addappid(1607018, 1, "c1234f4f8886863bf4af6714d1638827cbf51dd84086cf4d3b484e6e939d99e7") --Dlcname AppID 1607018
+--Dlcname AppID 1607019
+addappid(1607019, 1, "f92749ce89026e45400ce9f76694564f90c49db3cfbe6d81bbd322a36a389369") --Dlcname AppID 1607019
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
