1ec5aa8ee829b85766b14fb20598b7a42cdedec6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 23:04:59 +0800

    add 310950.lua

--- 310950.lua
+++ 310950.lua
@@ -1,17 +1,95 @@
-addappid(310950)
-setManifestid(228985,"3966345552745568756")
-setManifestid(228990,"1829726630299308803")
-addappid(310951,0,"cde3f482aedb4b88421d61be7343f46674d5871746b189c56dc10b10ee3cada9")
-setManifestid(310951,"4281346506506612445")
-addappid(310953,0,"18d1e71626cb0a8e17e55bb098bedea9b4737fc4cb7e52081175c7e03af05c1e")
-setManifestid(310953,"665527672323223830")
-addappid(444680,0,"db42f16a2c7f575888338273f6a57333df09088a593c1f2ef1972d5e3bbe8052")
-setManifestid(444680,"4334708473715125456")
-addappid(475230,0,"d508e856e2373e136a7f87a1756386fcae717c758409c450ed7c7fbf4efdee3e")
-setManifestid(475230,"2258455590697043247")
-addappid(807290,0,"736c56c0dc3b8d554a6301785f90ae2d9ddb41bd02062e39f28a2baedd80d96b")
-setManifestid(807290,"5114511160089983206")
-addappid(1172540,0,"654f536206ec0d43b2d537cca8c9c737ab400ab6978563f4045c0a1a73d3c226")
-setManifestid(1172540,"1561420009549394100")
-addappid(1474240,0,"02e28ddd341496a1e2d6911b524675379f2fd668b033e557317f82247463db73")
-setManifestid(1474240,"7393267258824001410")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 310950.lua
+--Gamename Street Fighter V
+addappid(310950, 1, "178da158b2405aea95ef234f5a620320ba332d35d9f68c38f1224d2d49dbf8c3") --Mainappid Street Fighter V
+addappid(310951, 1, "cde3f482aedb4b88421d61be7343f46674d5871746b189c56dc10b10ee3cada9") --Main Windows Depot Street Fighter V
+setManifestid(310951, "4281346506506612445", 46971752816)
+addappid(310952, 1, "4a147f5a5cbd4194a6eed1dd029b5a5e4fe2289bd588b3523eb881b099363254") --Main Windows Depot Street Fighter V
+setManifestid(310952, "5579393335594350391", 48)
+addappid(310953, 1, "18d1e71626cb0a8e17e55bb098bedea9b4737fc4cb7e52081175c7e03af05c1e") --Main Windows Depot Street Fighter V
+setManifestid(310953, "665527672323223830", 48)
+addappid(444680, 1, "db42f16a2c7f575888338273f6a57333df09088a593c1f2ef1972d5e3bbe8052") --Main Windows Depot Street Fighter V
+setManifestid(444680, "4334708473715125456", 379014512)
+addappid(807290, 1, "736c56c0dc3b8d554a6301785f90ae2d9ddb41bd02062e39f28a2baedd80d96b") --Main Windows Depot Street Fighter V
+setManifestid(807290, "5114511160089983206", 572026496)
+--Dlcname STREET FIGHTER V General Story "A Shadow Falls"
+addappid(475230, 1, "d508e856e2373e136a7f87a1756386fcae717c758409c450ed7c7fbf4efdee3e") --Dlcname STREET FIGHTER V General Story "A Shadow Falls"
+setManifestid(475230, "2258455590697043247", 8039709344)
+--Dlcname Street Fighter V - Champion Edition Special Wallpapers
+addappid(1172540, 1, "654f536206ec0d43b2d537cca8c9c737ab400ab6978563f4045c0a1a73d3c226") --Dlcname Street Fighter V - Champion Edition Special Wallpapers
+setManifestid(1172540, "1561420009549394100", 16515872)
+--Dlcname Street Fighter V - Season 5 Special Wallpapers
+addtoken(1474240, "5674982419633990023")
+addappid(1474240, 1, "02e28ddd341496a1e2d6911b524675379f2fd668b033e557317f82247463db73") --Dlcname Street Fighter V - Season 5 Special Wallpapers
+setManifestid(1474240, "7393267258824001410", 10236160)
+--Share Depots
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(480520) --Dlcname Street Fighter V - Capcom Pro Tour 2016 Pack
+addappid(480530) --Dlcname Street Fighter V - Season 2 Character Pass
+addappid(480540) --Dlcname Street Fighter V - 2016 Holiday Pack
+addappid(480550) --Dlcname Street Fighter V - Original Characters Battle Costume 1 Pack
+addappid(590750) --Dlcname Street Fighter V - Capcom Pro Tour 2017 Premier Pass
+addappid(691300) --Dlcname Street Fighter V - 2016 Summer Costume Bundle
+addappid(700850) --Dlcname Street Fighter V - 2016 Halloween Costume Bundle
+addappid(700851) --Dlcname Street Fighter V - 2017 Holiday Costume Bundle
+addappid(750310) --Dlcname AppID 750310
+addappid(780660) --Dlcname AppID 780660
+addtoken(780660, "2972272352757020185")
+addappid(789460) --Dlcname Street Fighter V - Capcom Pro Tour: 2018 Premier Pass
+addappid(863180) --Dlcname Street Fighter V - 2018 Summer Costume Bundle
+addappid(863181) --Dlcname Street Fighter V - Monster Hunter Bundle
+addappid(863182) --Dlcname Street Fighter V - Devil May Cry & Ghosts 'n Goblins Bundle
+addappid(893320) --Dlcname Street Fighter V - 2017 Halloween Costume Bundle
+addappid(893321) --Dlcname Street Fighter V - Darkstalkers Costume Bundle
+addappid(906440) --Dlcname Street Fighter V - Sports Costumes Bundle
+addappid(971240) --Dlcname Street Fighter V - Extra Battle CAPCOM LEGEND Bundle
+addappid(971241) --Dlcname Street Fighter V - Story Costumes Bundle S1-S3
+addappid(971242) --Dlcname Street Fighter V - Stages Bundle S1-S3
+addappid(971243) --Dlcname Street Fighter V - Work Costumes Bundle
+addappid(971250) --Dlcname Street Fighter V - School Costumes Bundle
+addappid(971251) --Dlcname Street Fighter V - Nostalgia Costumes Bundle S1-S3
+addappid(971252) --Dlcname Street Fighter V - Street Fighter 30th Anniversary Costumes Bundle
+addappid(971260) --Dlcname Street Fighter V - Akiman Costumes Bundle
+addappid(989970) --Dlcname Street Fighter V - Resident Evil Costume Bundle
+addappid(995950) --Dlcname Street Fighter V - Capcom Pro Tour: 2019 Premier Pass
+addappid(1009400) --Dlcname Street Fighter V - Mech Costume Bundle
+addappid(1025060) --Dlcname Street Fighter V - Ryu Costumes Bundle
+addappid(1025061) --Dlcname Street Fighter V - Chun-Li Costumes Bundle
+addappid(1060990) --Dlcname Street Fighter V - Cammy Costumes Bundle
+addappid(1061020) --Dlcname Street Fighter V - Karin Costumes Bundle
+addappid(1061030) --Dlcname Street Fighter V - R. Mika Costumes Bundle
+addappid(1061040) --Dlcname Street Fighter V - Laura Costumes Bundle
+addappid(1061050) --Dlcname Street Fighter V - Ken Costumes Bundle
+addappid(1061060) --Dlcname Street Fighter V - Nash Costumes Bundle
+addappid(1061070) --Dlcname Street Fighter V - Necalli Costumes Bundle
+addappid(1075990) --Dlcname Street Fighter V - 2019 Summer Costume Bundle
+addappid(1075991) --Dlcname Street Fighter V - Mega Man Costume Bundle
+addappid(1075993) --Dlcname Street Fighter V - Summer 2019 Character Bundle
+addappid(1075994) --Dlcname Street Fighter V - M. Bison Costume Bundle / ベガコスチュームパック
+addappid(1075995) --Dlcname Street Fighter V - Zangief Costume Bundle
+addappid(1075996) --Dlcname Street Fighter V - Birdie Costume Bundle
+addappid(1075997) --Dlcname Street Fighter V - Vega Costume Bundle / バルログコスチュームパック
+addappid(1075998) --Dlcname Street Fighter V - Rashid Costume Bundle
+addappid(1075999) --Dlcname Street Fighter V - Dhalsim Costume Bundle
+addappid(1076000) --Dlcname Street Fighter V - F.A.N.G Costume Bundle
+addappid(1117540) --Dlcname Street Fighter V - Extra Battle CAPCOM LEGEND Bundle 2
+addappid(1117541) --Dlcname Street Fighter V - 2018 Halloween Costume Bundle
+addappid(1150190) --Dlcname Street Fighter V - Champion Edition Upgrade Kit
+addappid(1150191) --Dlcname Street Fighter V - 2018 Holiday Costume Bundle
+addappid(1150192) --Dlcname Street Fighter V - Extra Battle CAPCOM LEGEND Bundle 3
+addappid(1150194) --Dlcname AppID 1150194
+addappid(1150195) --Dlcname Street Fighter V - Champion Edition Special Color
+addappid(1233100) --Dlcname Street Fighter V - Capcom Pro Tour: 2020 Premier Pass
+addappid(1405400) --Dlcname Street Fighter V - SFL2020 UYU Costumes Bundle
+addappid(1405401) --Dlcname Street Fighter V - SFL2020 NASR Costumes Bundle
+--Missing Dlcs
+--Dlcname Chun-Li Battle Costume - Pre-purchase bonus
+--Dlcname Street Fighter V Original Soundtrack (FLAC)
+addappid(425110) --Dlcname AppID 425110
+addappid(807400) --Dlcname AppID 807400
+addtoken(807400, "6494171694919082839")
+addappid(807401) --Dlcname AppID 807401
+addtoken(807401, "6237600527739894899")
