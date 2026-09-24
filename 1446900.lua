36cc7ffdd78fd2d55884f7cd2d99cec240515bad
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 15:25:01 +0800

    add 1446900.lua

---
+++ 1446900.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1446900.lua
+--Gamename Fallen Tear: The Ascension
+addappid(1446900, 1, "b34b07d6ed3f7015c4c0471113d4655b7c01c6e8f3e0e621b25630c1b8c1c0a4") --Mainappid Fallen Tear: The Ascension
+addappid(1446901, 1, "b05889536fd66d9f79de3ab1f457a00b6759ef200d54afc807fb1699263d69ba") --Main Windows Depot Fallen Tear: The Ascension
+setManifestid(1446901, "6524676439494937713", 8888391392)
