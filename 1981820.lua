290d59ef399d1e55139bb3d1fab49c09d51d016d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 21:45:49 +0800

    add 1981820.lua

--- 1981820.lua
+++ 1981820.lua
@@ -1,5 +1,11 @@
-addappid(1981820)
-addappid(1981821,0,"503f766da9b250522ec18da5429e65770ce4f9cd9179c1b89c4c034e07f0d3aa")
-setManifestid(1981821,"3910289881625316754")
-addappid(2670380,0,"12fb1fd8f21f7b69ea82d123f2b3dc731e0b963822c23dc2832b058b98dfe946")
-setManifestid(2670380,"741949742884536920")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1981820.lua
+--Gamename Strike Force Heroes
+addappid(1981820, 1, "54bb4f035ac2c0b3509e3afbdb83d30b6d5a9bb34b7a5bb3734770fbf57efec4") --Mainappid Strike Force Heroes
+addappid(1981821, 1, "503f766da9b250522ec18da5429e65770ce4f9cd9179c1b89c4c034e07f0d3aa") --Main Windows Depot Strike Force Heroes
+setManifestid(1981821, "3910289881625316754", 1113476432)
+--Dlcname Strike Force Heroes Digital Artbook & Outfit
+addappid(2670380, 1, "12fb1fd8f21f7b69ea82d123f2b3dc731e0b963822c23dc2832b058b98dfe946") --Dlcname Strike Force Heroes Digital Artbook & Outfit
+setManifestid(2670380, "741949742884536920", 255553296)
+addappid(2588670) --Dlcname Strike Force Heroes Original Flash Outfits
+addappid(2588680) --Dlcname Strike Force Heroes Ninja Class
