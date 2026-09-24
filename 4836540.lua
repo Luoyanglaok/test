ecbc8547744e220d5ccd67ee2bc8c02265fc33d6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 20:20:03 +0800

    add 4836540.lua

--- 4836540.lua
+++ 4836540.lua
@@ -12,7 +12,7 @@ setManifestid(228990, "1829726630299308803", 100658080)
 addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(229006, "1784011429307107530", 84953712)
 addappid(1677281, 1, "946c1091854e4b36a1468dea11adb8fe4fe6db6485976132a50781a70d5a206e") --Share Windows Depot Company of Heroes 3
-setManifestid(1677281, "5724534741138488832", 28242247408)
+setManifestid(1677281, "8872311503752965071", 28242424112)
 addappid(1677282, 1, "2dbe642c09395dcb0cfab68ff34eca066c3f47e38d8511d59509fb60cf7c665a") --Share Windows Depot Company of Heroes 3
 setManifestid(1677282, "83851306951263697", 1008)
 addappid(1677283, 1, "532412e59c494bd36e8efc9d4e257208e7412c8a70b16f206103f3913d4add50") --Share Windows Depot Company of Heroes 3
