5801ee2b27b541d3618ef72660600d3ee02eb805
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 15:32:40 +0800

    add 2001120.lua

--- 2001120.lua
+++ 2001120.lua
@@ -4,12 +4,6 @@
 addappid(2001120, 1, "3ad571280aa49958160c0270aa4a31064e7e917b44b0a0b36d13e03bb7929c01") --Mainappid Split Fiction
 addappid(2001121, 1, "27f6aac2ce9d80ad4ed8fe3710d306ae108f00ddcf005d47ecf7bfa2a258964c") --Main Windows Depot Split Fiction
 setManifestid(2001121, "4899075162680269256", 83385265472)
---Dlcname Split Fiction - Original Soundtrack
-addappid(4110950) --Dlcname Split Fiction - Original Soundtrack
-addappid(4110951, 1, "0359f50b5814950537d5fb52d353c6262fad8e41c720103435de9208b14d7949") --Dlc Windows Depot Split Fiction - Original Soundtrack
-setManifestid(4110951, "1497142047146784507", 342161616)
-addappid(4110952, 1, "0db626922af091f02ab545eb507f259869dae6a4787c5a5c0681a20c79e16eec") --Dlc Windows Depot Split Fiction - Original Soundtrack
-setManifestid(4110952, "2424117993464905013", 1086362912)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
