1d504e14a8bf5cefa52d414caf09354be102a265
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 21:05:02 +0800

    add 2085540.lua

---
+++ 2085540.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2085540.lua
+--Gamename Stick It to the Stickman
+addappid(2085540, 1, "e31245a80abc1578f15dd339a7baa1283cfe0f55cf48c1f5e2a26dc85b02d37c") --Mainappid Stick It to the Stickman
+addappid(2085541, 1, "770f40e40d880628981e51726a0b14c9516704fc8154041d894b7fd0257b4050") --Main Windows Depot Stick It to the Stickman
+setManifestid(2085541, "2707920741953599548", 1107557424)
+addappid(2085542, 1, "f6e651a7cca7bd6445e2b5580aaa0feddaf9a233b8d6ef1b9b8be099bbb65ff8") --Main Macos Depot Stick It to the Stickman
+setManifestid(2085542, "2127075499923588046", 1114623696)
