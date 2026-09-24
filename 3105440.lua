466d39ba247f9570051b43e30d3de538e8c9db94
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 20:34:42 +0800

    add 3105440.lua

---
+++ 3105440.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3105440.lua
+--Gamename Heroes of Might and Magic: Olden Era
+addappid(3105440) --Mainappid Heroes of Might and Magic: Olden Era
+addtoken(3105440, "822347138807159306")
+addappid(3105441, 1, "ed44e10054a9907358f5bd7827b31171c8ba2e2a11b5b8a738ecd73dd62b0424") --Main Windows Depot Heroes of Might and Magic: Olden Era
+setManifestid(3105441, "438542529703190055", 2708889376)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
