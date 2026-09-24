5f58ef6a350f45bfa6d3709c09605517def158bf
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 17:39:31 +0800

    add 3466680.lua

--- 3466680.lua
+++ 3466680.lua
@@ -1,5 +1,10 @@
-addappid(3466680)
-addappid(3466681,0,"8c8dd4c48406b498dbaf8ac181676538e706634a5a8b3ae1a0f03d7374bed733")
-setManifestid(3466681,"9202602952359269751")
-addappid(3466682,0,"24873b61886e11f68303ac52bd9b176683612c605c9a9ba1b578a8ccec6a1170")
-setManifestid(3466682,"1894737515754419902")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3466680.lua
+--Gamename Magical Girl Opal
+addappid(3466680, 1, "cd304135f8abc4a22836a6fc4bdaad4ea0eba78fb3c63634a51ec314dee920c1") --Mainappid Magical Girl Opal
+addappid(3466681, 1, "8c8dd4c48406b498dbaf8ac181676538e706634a5a8b3ae1a0f03d7374bed733") --Main Windows Depot Magical Girl Opal
+setManifestid(3466681, "9202602952359269751", 833318496)
+addappid(3466682, 1, "24873b61886e11f68303ac52bd9b176683612c605c9a9ba1b578a8ccec6a1170") --Main Windows Language Depot Schinese Magical Girl Opal
+setManifestid(3466682, "1894737515754419902", 767349376)
+addappid(3466683, 1, "ba0e1031e5bab9c6896c769d3781a0241ced869834beacf4714917e4ff30bd1a") --Main Windows Language Depot Japanese Magical Girl Opal
+setManifestid(3466683, "9085889585955861932", 772925200)
