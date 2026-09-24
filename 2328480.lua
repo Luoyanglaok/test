0d2d034922171fa58226a7f61457af14065ef29e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 21:35:35 +0800

    add 2328480.lua

--- 2328480.lua
+++ 2328480.lua
@@ -1,5 +1,8 @@
-addappid(2328480)
-addappid(2328481,0,"09c5b59ae7b6e031e179e66a22db7336d6be2108dbd016fa23b5052619ba8f15")
-setManifestid(2328481,"1983403039360280081")
-addappid(2328482,0,"013f7da7c5fa0909c02485f8e2be56d004dff4ef0cb153e8304de20f7c0109d8")
-setManifestid(2328482,"169987011305924391")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2328480.lua
+--Gamename Ascension
+addappid(2328480) --Mainappid Ascension
+addappid(2328481, 1, "09c5b59ae7b6e031e179e66a22db7336d6be2108dbd016fa23b5052619ba8f15") --Main Windows Depot Ascension
+setManifestid(2328481, "1731661745705585833", 4185372704)
+addappid(2328482, 1, "013f7da7c5fa0909c02485f8e2be56d004dff4ef0cb153e8304de20f7c0109d8") --Main Windows Depot Ascension
+setManifestid(2328482, "8629111367049051029", 4189027808)
