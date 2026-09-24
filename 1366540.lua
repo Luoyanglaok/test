8844303c1d625c60f77431c6c51d4dd82ccc2fe2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 22:51:04 +0800

    add 1366540.lua

--- 1366540.lua
+++ 1366540.lua
@@ -1,3 +1,10 @@
-addappid(1366540)
-addappid(1366541,0,"88e374cd6c344868ab9b1591cdf0391483ea487bde467410ba7cac376292c19c")
-setManifestid(1366541,"7835592415243376611")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1366540.lua
+--Gamename Dyson Sphere Program
+addappid(1366540, 1, "9d62fc41e4b15fea9d911f2e33a59cc5646fdac737610152cd6f307beef53726") --Mainappid Dyson Sphere Program
+addappid(1366541, 1, "88e374cd6c344868ab9b1591cdf0391483ea487bde467410ba7cac376292c19c") --Main Windows Depot Dyson Sphere Program
+setManifestid(1366541, "6841942558692949027", 1806451104)
+--Dlcname Dyson Sphere Program - Digital Art Book
+addtoken(1530310, "11371357649252062499")
+addappid(1530310, 1, "d6853b48dc840f9b44fe7a225d1775e48305f3760aa3e8a826ec0d38eafe989f") --Dlcname Dyson Sphere Program - Digital Art Book
+setManifestid(1530310, "5639672823767287494", 209540592)
