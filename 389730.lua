77ec0cbccd5b4729631e7d42296ac2758805497c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 15:32:41 +0800

    add 389730.lua

--- 389730.lua
+++ 389730.lua
@@ -1,5 +1,44 @@
-addappid(389730)
-setManifestid(228986,"8782296191957114623")
-setManifestid(228990,"1829726630299308803")
-addappid(389731,0,"ced260a7f27dd73a1bdb16c642549470d25ee659c70bc35dc9879e23d38e811c")
-setManifestid(389731,"1653672533371481747")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 389730.lua
+--Gamename TEKKEN 7
+addappid(389730, 1, "5bcb1dc4812973c9d7e830c9f5ba3c6a36727adcdbc50155daa11cfbf9608934") --Mainappid TEKKEN 7
+addappid(389731, 1, "ced260a7f27dd73a1bdb16c642549470d25ee659c70bc35dc9879e23d38e811c") --Main Windows Depot TEKKEN 7
+setManifestid(389731, "1653672533371481747", 49328827904)
+--Share Depots
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(580020) --Dlcname TEKKEN 7 - Season Pass
+addappid(592370) --Dlcname TEKKEN 7 - Eliza Pre-order Bonus - DLC
+addtoken(592370, "7629745928377525948")
+addappid(592371) --Dlcname TEKKEN 7 - Metallic costumes Season Pass Bonus - DLC
+addtoken(592371, "18402440993168939028")
+addappid(592372) --Dlcname TEKKEN 7 DLC 1 Ultimate TEKKEN BOWL & Additional Costumes
+addappid(592373) --Dlcname TEKKEN 7 DLC 2 Geese Howard Pack
+addappid(592374) --Dlcname TEKKEN 7 DLC 3 Noctis Lucis Caelum Pack
+addappid(651430) --Dlcname TEKKEN 7 - TAIKO NO TATSUJIN Pack
+addappid(682560) --Dlcname TEKKEN 7 - Character Panel
+addappid(878460) --Dlcname TEKKEN 7 - DLC4: Anna Williams
+addappid(878461) --Dlcname TEKKEN 7 - DLC5: Lei Wulong
+addappid(878462) --Dlcname TEKKEN 7 - Craig Marduk
+addappid(878463) --Dlcname TEKKEN 7 - Armor King
+addappid(878464) --Dlcname TEKKEN 7 - DLC8 Julia Chang
+addappid(878465) --Dlcname TEKKEN 7 - DLC9 Negan
+addappid(878466) --Dlcname TEKKEN 7 - Bonus Character Customization Items
+addtoken(878466, "5682005606814451290")
+addappid(878467) --Dlcname TEKKEN 7 - Season Pass 2
+addappid(1114550) --Dlcname TEKKEN 7 - Season Pass 3
+addappid(1114560) --Dlcname TEKKEN 7 - SP3 Bonus Character Customization Items
+addappid(1114561) --Dlcname TEKKEN 7 - DLC10: Zafina
+addappid(1162600) --Dlcname TEKKEN 7 - DLC11: Ganryu
+addtoken(1162600, "16665772109696247537")
+addappid(1162601) --Dlcname TEKKEN 7 - DLC12: Leroy Smith
+addappid(1162602) --Dlcname TEKKEN 7 - DLC13: Frame Data Display
+addappid(1236500) --Dlcname TEKKEN 7 - DLC14: Fahkumram
+addappid(1236501) --Dlcname TEKKEN 7 - DLC15: CAVE OF ENLIGHTENMENT
+addappid(1289450) --Dlcname TEKKEN 7 - Season Pass 4
+addappid(1289451) --Dlcname TEKKEN 7 - DLC16: Kunimitsu
+addappid(1289452) --Dlcname TEKKEN 7 - DLC17: Vermilion Gates
+addappid(1289453) --Dlcname TEKKEN 7 - DLC18: Lidia Sobieska
+addappid(1289454) --Dlcname TEKKEN 7 - DLC19: Island Paradise
