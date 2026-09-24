6df6bf9fd6277791ed643a1ad736c94d6c06ee17
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 18:02:10 +0800

    add 4148240.lua

---
+++ 4148240.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4148240.lua
+--Gamename Road to Empress II
+addappid(4148240, 1, "830de95c8a255365c7b2308c0b654c7e5c17f6fa60bc786abfb4223775323194") --Mainappid Road to Empress II
+addappid(4148241, 1, "a5eac25188569b44da6f828e35adf08f236dace1c4ed3ad45a4d6c747f5eaaf9") --Main Windows Depot Road to Empress II
+setManifestid(4148241, "1457418422947062538", 8689029248)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(4742160) --Dlcname Road to Empress II: Bloopers Pack
