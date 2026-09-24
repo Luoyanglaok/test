565c6849397d9ce15589021c4f4eb4212de34797
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 09:39:07 +0800

    add 1434310.lua

--- 1434310.lua
+++ 1434310.lua
@@ -1,3 +1,6 @@
-addappid(1434310)
-addappid(1434311,0,"4977cb9a9b46e50971758a0cd55ae5013f7234c125425aa650cd2423b31756d1")
-setManifestid(1434311,"7187117819042592980")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1434310.lua
+--Gamename PAW Patrol The Movie: Adventure City Calls
+addappid(1434310) --Mainappid PAW Patrol The Movie: Adventure City Calls
+addappid(1434311, 1, "4977cb9a9b46e50971758a0cd55ae5013f7234c125425aa650cd2423b31756d1") --Main Windows Depot PAW Patrol The Movie: Adventure City Calls
+setManifestid(1434311, "7187117819042592980", 3031207776)
