5b147aa0a14afd7d09a55af4f06bf3eb1df23bf6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 19:57:28 +0800

    add 4209920.lua

---
+++ 4209920.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4209920.lua
+--Gamename 不问凡尘
+addappid(4209920, 1, "63f6e126c93b93fbc75b1e771bae8e41d5beb18b49327e9ab54f56519f795750") --Mainappid 不问凡尘
+addappid(4209921, 1, "78a552beaaf2314443aa8b020bfeb5c6c511fc129272cd9e4b134d58fc880a20") --Main Windows Depot 不问凡尘
+setManifestid(4209921, "4302083953845150713", 28561319552)
