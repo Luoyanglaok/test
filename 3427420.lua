0504182ca2481b482d54c6766d5030410f774708
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 21:20:29 +0800

    add 3427420.lua

--- 3427420.lua
+++ 3427420.lua
@@ -1,3 +1,8 @@
-addappid(3427420)
-addappid(3427421,0,"dea7d49312dd6c9655dde564f6c4cc02b6384e3acf15a2af8d1d05737bb80e3c")
-setManifestid(3427421,"7581221354996344664")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3427420.lua
+--Gamename BDSM Clicker
+addappid(3427420, 1, "4b2e4f53611cc8acdf6018b962134035c629c2e8161d97df9329d6a3a2fcf915") --Mainappid BDSM Clicker
+addappid(3427421, 1, "dea7d49312dd6c9655dde564f6c4cc02b6384e3acf15a2af8d1d05737bb80e3c") --Main Windows Depot BDSM Clicker
+setManifestid(3427421, "2979778972197448020", 99898496)
+addappid(3427422, 1, "3fae1d906566eed2b8a31cec0aaaf1ed8cf4d334082be41e6999e08b259b5baa") --Main Linux Depot BDSM Clicker
+setManifestid(3427422, "7998740530032726878", 98071872)
