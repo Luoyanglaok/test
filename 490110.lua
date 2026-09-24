6746079502059a8ee775ce5e5a40bab8835f18b0
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 21:45:10 +0800

    add 490110.lua

--- 490110.lua
+++ 490110.lua
@@ -1,3 +1,6 @@
-addappid(490110)
-addappid(3240070)
-setManifestid(490111,"103314763015814443")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 490110.lua
+--Gamename The Precinct
+addappid(490110, 1, "30a2b9f28e2ea177a918b9ffb874641dafe6b9ee69ec5a83d7eeb9d8fd5c00f7") --Mainappid The Precinct
+addappid(490111, 1, "44bcc3870bf156385e37ff47e71ae28824fa905a542cdc8eaf05715799f78fc2") --Main Windows Depot The Precinct
+setManifestid(490111, "428541716305617698", 4561432032)
