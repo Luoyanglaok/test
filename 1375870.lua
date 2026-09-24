00944ef3d28b0a137406643ca63070c40501045c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 20:13:05 +0800

    add 1375870.lua

--- 1375870.lua
+++ 1375870.lua
@@ -1,3 +1,6 @@
-addappid(1375870)
-addappid(1375871,0,"896295cf8e0845d2097d36bad92e970ae6429b02cf7e7cfebb5f064c961753b6")
-setManifestid(1375871,"6904355878022087903")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1375870.lua
+--Gamename 川建国同志想要连任/Comrade Trump's Re-election
+addappid(1375870) --Mainappid 川建国同志想要连任/Comrade Trump's Re-election
+addappid(1375871, 1, "896295cf8e0845d2097d36bad92e970ae6429b02cf7e7cfebb5f064c961753b6") --Main Windows Depot 川建国同志想要连任/Comrade Trump's Re-election
+setManifestid(1375871, "6904355878022087903", 52263440)
