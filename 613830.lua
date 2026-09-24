0510bc9cd231c356879030a9c6336e469b701f9c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 12:34:15 +0800

    add 613830.lua

--- 613830.lua
+++ 613830.lua
@@ -1,11 +1,23 @@
-addappid(613830)
-setManifestid(228981,"7613356809904826842")
-setManifestid(228982,"6413394087650432851")
-setManifestid(228983,"8124929965194586177")
-setManifestid(228984,"2547553897526095397")
-setManifestid(228985,"3966345552745568756")
-setManifestid(228986,"8782296191957114623")
-addappid(613831,0,"1e8869a6b81930b7548235c629efb0dc53a8a8f5a6eb5f7b8524bbca953c8141")
-setManifestid(613831,"6725455135839501871")
-addappid(775290,0,"ef7fc1356ab3d71703a865e920536fa58566c38c00f224f49ecc5c7e60ced83e")
-setManifestid(775290,"3668456529789183270")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 613830.lua
+--Gamename CHRONO TRIGGER®
+addappid(613830, 1, "7c27b9ca0a6056a2b9be94aa121db05e47a11c4760e170547a79520b40eced93") --Mainappid CHRONO TRIGGER®
+addappid(613831, 1, "1e8869a6b81930b7548235c629efb0dc53a8a8f5a6eb5f7b8524bbca953c8141") --Main Windows Depot CHRONO TRIGGER®
+setManifestid(613831, "6725455135839501871", 862380176)
+--Dlcname AppID 775290
+addtoken(775290, "12104657987446382277")
+addappid(775290, 1, "ef7fc1356ab3d71703a865e920536fa58566c38c00f224f49ecc5c7e60ced83e") --Dlcname AppID 775290
+setManifestid(775290, "3668456529789183270", 83551504)
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228982, "6413394087650432851", 9650016)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228986, "8782296191957114623", 23045488)
