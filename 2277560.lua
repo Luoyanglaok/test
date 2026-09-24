9b4f4b738f551e69f6c15707c93131c83b956fe4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 09:42:47 +0800

    add 2277560.lua

--- 2277560.lua
+++ 2277560.lua
@@ -4,12 +4,6 @@
 addappid(2277560, 1, "5a45ebe9e985f6ea27f4be004ba5af9d317bd2242c20008cea7060fb5651844d") --Mainappid WUCHANG: Fallen Feathers
 addappid(2277561, 1, "6db3af8b5be3def24c758a005feb7fa0f65750716392328e2f14321340adb273") --Main Windows Depot WUCHANG: Fallen Feathers
 setManifestid(2277561, "2668839162049698821", 29074348064)
---Dlcname WUCHANG: Fallen Feathers Soundtrack
-addappid(3758190) --Dlcname WUCHANG: Fallen Feathers Soundtrack
-addappid(3758191, 1, "6a02a8415b11cc2d0d5578a0bb93e955abb933fbb378d43149a2105e5f32590a") --Dlc Windows Depot WUCHANG: Fallen Feathers Soundtrack
-setManifestid(3758191, "8536961417533777456", 209843152)
-addappid(3758192, 1, "3c6aafecacfcd5f0ef971f364de81fcf4752115de544aa3a40c8f8a2ce0bcbe7") --Dlc Windows Depot WUCHANG: Fallen Feathers Soundtrack
-setManifestid(3758192, "3796656319495221628", 622257392)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
