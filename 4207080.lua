795428f2971e6a4ca52d7e2a3181aab77cc646b3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 02:33:44 +0800

    add 4207080.lua

---
+++ 4207080.lua
@@ -0,0 +1,13 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4207080.lua
+--Gamename 愛のあるマナー指導
+addappid(4207080) --Mainappid 愛のあるマナー指導
+addtoken(4207080, "5180403315608619237")
+addappid(4207081, 1, "09bf4de1248a26b913ff84327ecf6451a8cee6132950bce9f984c62c3aa90fc4") --Main Windows Language Depot Japanese 愛のあるマナー指導
+setManifestid(4207081, "7348716229870229107", 102427088)
+addappid(4207082, 1, "4fc0e65250cc5fb6da96c0dc6a0e8ad329ddba0408335151b39d0770e47ed3b2") --Main Windows Language Depot English 愛のあるマナー指導
+setManifestid(4207082, "2324060570217018690", 102630144)
+addappid(4207083, 1, "6232203201ef46038e47b731f01f8883a446113d229d8adedb492ad5b9176fec") --Main Windows Language Depot Schinese 愛のあるマナー指導
+setManifestid(4207083, "9059607728862851782", 129614928)
+addappid(4207084, 1, "7d1105dccb69b6c0fa4df0ba04382a0f8eac956388f94c91c79068b3c399781c") --Main Windows Language Depot Tchinese 愛のあるマナー指導
+setManifestid(4207084, "150506218318942791", 115787280)
