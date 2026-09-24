1cccbff56f8f296ba696534f1d617a6649d536c1
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 23:29:54 +0800

    add 2849080.lua

--- 2849080.lua
+++ 2849080.lua
@@ -1,8 +1,16 @@
-addappid(2849080)
-setManifestid(228988,"6645201662696499616")
-addappid(2849081,0,"e00df48b38c967f303c37f758389ff8e15505ab4416c522ea62ef425884d3849")
-setManifestid(2849081,"2986433879215574901")
-addappid(2849082,0,"4982ecd61fd529c34e6519a1a5004121f93ebb0e60d14d161fa95e4851ea8a32")
-setManifestid(2849082,"2908869751550913963")
-addappid(2849083,0,"3d8ca2d9a2a948ec35d8046112d2bf42f03f46ae30dae1b842c8ec4dd26d285f")
-setManifestid(2849083,"4012762618320968222")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2849080.lua
+--Gamename Kingdom Rush 5: Alliance TD
+addappid(2849080, 1, "801898d184b5ad1b3c57c07d4324e25c122aae717f42aa702b1b616396086299") --Mainappid Kingdom Rush 5: Alliance TD
+addappid(2849081, 1, "e00df48b38c967f303c37f758389ff8e15505ab4416c522ea62ef425884d3849") --Main Windows Depot Kingdom Rush 5: Alliance TD
+setManifestid(2849081, "2986433879215574901", 4098672)
+addappid(2849082, 1, "4982ecd61fd529c34e6519a1a5004121f93ebb0e60d14d161fa95e4851ea8a32") --Main Windows Depot Kingdom Rush 5: Alliance TD
+setManifestid(2849082, "4575449103611673528", 1620798960)
+addappid(2849083, 1, "3d8ca2d9a2a948ec35d8046112d2bf42f03f46ae30dae1b842c8ec4dd26d285f") --Main Macos Depot Kingdom Rush 5: Alliance TD
+setManifestid(2849083, "2237783188623953254", 1624814928)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(3368630) --Dlcname Kingdom Rush 5: Alliance TD - Colossal Dwarfare Campaign
+addappid(3732970) --Dlcname Kingdom Rush 5: Alliance TD - Wukong's Journey Campaign
+addappid(4260510) --Dlcname Kingdom Rush 5: Alliance TD - Dragon Wars Campaign
