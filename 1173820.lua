ce4c14fb70e593808ee991748c861ba28441c178
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 20:53:49 +0800

    add 1173820.lua

--- 1173820.lua
+++ 1173820.lua
@@ -1,8 +1,20 @@
-addappid(1173820)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229007,"4477590687906973371")
-addappid(1173821,0,"f14232f6b12f98270c9af46260c0e9ed60c5c3b2e7ea5aa7d7a4a3aad47b27b6")
-setManifestid(1173821,"4898963439881301981")
-addappid(1638420,0,"044ac29b160a56f2451fe51f246515764b172431ebf2871f35f8afad63db50fa")
-setManifestid(1638420,"4312747159132628671")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1173820.lua
+--Gamename FINAL FANTASY VI
+addappid(1173820) --Mainappid FINAL FANTASY VI
+addappid(1173821, 1, "f14232f6b12f98270c9af46260c0e9ed60c5c3b2e7ea5aa7d7a4a3aad47b27b6") --Main Windows Depot FINAL FANTASY VI
+setManifestid(1173821, "7246209900814962606", 1015863776)
+addappid(4417861, 1, "66b015ba0d8043b17355637ea7d7ecdbf8fa71babb14ec289e4f17b172bb9663") --Main Windows Depot FINAL FANTASY VI
+setManifestid(4417861, "270295708232618333", 90097376)
+--Dlcname FINAL FANTASY VI: OST & Wallpaper
+addappid(1638420, 1, "044ac29b160a56f2451fe51f246515764b172431ebf2871f35f8afad63db50fa") --Dlcname FINAL FANTASY VI: OST & Wallpaper
+setManifestid(1638420, "4312747159132628671", 76413184)
+--Dlcname Piano Collections FINAL FANTASY VI
+addappid(4417860) --Dlcname Piano Collections FINAL FANTASY VI
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
