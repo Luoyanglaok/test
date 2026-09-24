b0c4bee63dff597281c9f7d19069bc8684cb7110
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 20:05:42 +0800

    add 3592.lua

--- 3592.lua
+++ 3592.lua
@@ -2,5 +2,7 @@
 -- Original file: 3592.lua
 --Gamename Plants vs. Zombies Demo
 addappid(3592) --Mainappid Plants vs. Zombies Demo
-addappid(3591, 1, "1625fdac425750d0736c420f3877d3b4b4119d1d361629c2d12b360eb3689839") --Main Windows Depot Plants vs. Zombies Demo
 addappid(3593, 1, "2fec966bebbf74b5fad937c61ad06d393d521b64f27d97992ae7ef3775c50c10") --Main Windows Depot Plants vs. Zombies Demo
+--Share Depots
+addappid(3591, 1, "1625fdac425750d0736c420f3877d3b4b4119d1d361629c2d12b360eb3689839") --Share Windows Depot Plants vs. Zombies: Game of the Year
+setManifestid(3591, "7598841597413163537", 0)
