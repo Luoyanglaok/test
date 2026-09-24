ffa220a9e51fd21ea58ee31d4f56fde48eb3743f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 15:23:19 +0800

    add 964930.lua

--- 964930.lua
+++ 964930.lua
@@ -1,3 +1,3 @@
-addappid(964930)
-addappid(964930,0,"8d6e1a8b596db24106b4cd82eece26e010f10b926c0bb21bf5b1e381c3825cfb")
-setManifestid(964930,"7739407067924420362")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 964930.lua
+addappid(964930, 0, "c82c2dda2f02383e0f780066b5ad841e594ab718fb2dad9667472c75f8a1901e")
