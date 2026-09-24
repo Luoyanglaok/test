5017ddc0d9d60a198299034cbdf6e9ea1800f9aa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-24 23:33:27 +0800

    add 3669870.lua

--- 3669870.lua
+++ 3669870.lua
@@ -4,9 +4,18 @@
 addappid(3669870, 1, "e53df1456a5d9cb4244af0790bfecf0b7f24217faf8a20276ce03b302187f51e") --Mainappid CONTROL Resonant
 addtoken(3669870, "18274421760595271558")
 addappid(3669871, 1, "47ad456f9070a97f6206e13818d3588f48e26d8d94675b45f481b32ae2fc0bd5") --Main Windows Depot CONTROL Resonant
-setManifestid(3669871, "8518191233347387188", 99111389616)
+setManifestid(3669871, "5962114018493126939", 99132429744)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228990, "1829726630299308803", 100658080)
+addappid(4403940) --Dlcname CONTROL Resonant Digital Deluxe Upgrade
+addappid(4760150) --Dlcname CONTROL Resonant Digital Deluxe Edition - Original Soundtrack and Digital Artbook
+addtoken(4760150, "11635387767932080005")
+addappid(4760160) --Dlcname CONTROL Resonant Digital Deluxe Edition - Untapped Artifact (Wallet)
+addtoken(4760160, "4968652230313847411")
+addappid(4760170) --Dlcname CONTROL Resonant Digital Deluxe Edition - Starter Resource Bundle
+addtoken(4760170, "7759855539969402306")
+addappid(4760190) --Dlcname CONTROL Resonant Digital Deluxe Edition - AWE Mission Outfit
+addtoken(4760190, "12349664922998478843")
