96d0d79a268295c30377c7c009403e33252eced4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 22:59:48 +0800

    add 1364210.lua

--- 1364210.lua
+++ 1364210.lua
@@ -1,5 +1,32 @@
-addappid(1364210)
-addappid(1364212,0,"e0d499227945f83fa2e9654ea918bc00d4e33990786e4eb6b16a3abefd1f2263")
-setManifestid(1364212,"1857281912899643078")
-addappid(1364211,0,"3748007409a9ce4382259fe6320aff7ea846f18eba5a9c4a9ec7bab1a6864761")
-setManifestid(1364211,"8126239253086192449")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1364210.lua
+--Gamename City Bus Manager
+addappid(1364210, 1, "a2f63ad9fdb4aad94b825ce27fa4b82ea6327ee0eb2fb35cd62da5406b738ae3") --Mainappid City Bus Manager
+addappid(1364211, 1, "3748007409a9ce4382259fe6320aff7ea846f18eba5a9c4a9ec7bab1a6864761") --Main Windows Depot City Bus Manager
+setManifestid(1364211, "3568744885267660566", 2913840048)
+addappid(1364212, 1, "e0d499227945f83fa2e9654ea918bc00d4e33990786e4eb6b16a3abefd1f2263") --Main Macos Depot City Bus Manager
+setManifestid(1364212, "4930145165573020895", 2561886400)
+--Dlcname AppID 1899720
+addappid(1899720, 1, "1ce3a5283959493f196079aa1440bd86ca62ae22795da97935adb56a102789de") --Dlcname AppID 1899720
+--Dlcname AppID 1901800
+addappid(1901800, 1, "dc8552da1e835325cd793b990d9299b4e18dfe90a6e496947cdfb8d316410004") --Dlcname AppID 1901800
+--Dlcname AppID 1901840
+addappid(1901840, 1, "bb563f0df0d9ff0b94bf5d2b300b96e66fd061540809bdc4a71b8a2f0adeb8e9") --Dlcname AppID 1901840
+--Dlcname AppID 1901850
+addappid(1901850, 1, "be591fba8366a658794b84b02ccac6d21e8fd834bf8184b265a923d4899d3573") --Dlcname AppID 1901850
+--Dlcname AppID 1903250
+addappid(1903250, 1, "bd6941a42f699c7d7f1a4f5f7007715113452bf75bc1047e7e26328324f25aca") --Dlcname AppID 1903250
+--Dlcname AppID 1903920
+addappid(1903920, 1, "6a0f40deeecc17b2fb1e3c7c907e5e7b9bd0f3e99893372ffb0305aa9f486e9a") --Dlcname AppID 1903920
+--Dlcname AppID 1903930
+addappid(1903930, 1, "88eab6f7494313d93e78bd83d69d88832a887fcab89cb803896d377aa5dd8e8a") --Dlcname AppID 1903930
+--Dlcname AppID 1904020
+addappid(1904020, 1, "19a8920f40f99f0197ca8f6f7e998e004922aeec71887003c32139b3e4bf5ce9") --Dlcname AppID 1904020
+--Dlcname AppID 1904840
+addappid(1904840, 1, "05d75864b42529960b18433137bd74d425799a0645ccb60d18adb5d93debe352") --Dlcname AppID 1904840
+--Dlcname AppID 1904870
+addappid(1904870, 1, "5087d23a0c50807ef7c6be290cb96a270a6a9812eb6f0cdf415f8ba8a442c27b") --Dlcname AppID 1904870
+addappid(2700180) --Dlcname City Bus Manager - E-Bus & Green Energy
+addappid(3218520) --Dlcname City Bus Manager - Holiday Resorts & Sightseeing Buses
+addappid(3358770) --Dlcname City Bus Manager - International Bus Pack
+addappid(4196890) --Dlcname City Bus Manager - School Bus & Student Transportation
