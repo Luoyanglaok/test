528cc781e1755e4e7f3035381c3e1a18462e54b8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 13:49:28 +0800

    add 2669320.lua

--- 2669320.lua
+++ 2669320.lua
@@ -1,9 +1,68 @@
-addappid(2669320)
-addappid(3170610)
-addappid(3340992,1,"7112f072ac3a0100e541bc9b1556d3dd0697e7641094f3389e811ceb50efaf3e")
-addappid(228988,1,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
-setManifestid(2669321,"481161766096858153")
-setManifestid(2669328,"6688489169711928475")
-setManifestid(3340992,"9157597073741630849")
-setManifestid(3893181,"3685412901435514107")
-setManifestid(228988,"6645201662696499616")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2669320.lua
+--Gamename EA SPORTS FC 25
+addappid(2669320, 1, "ecf0c5e2ea751c406391cce31f37731f58240823183884a39e185cfc465784de") --Mainappid EA SPORTS FC 25
+addappid(2669321, 1, "76cc370335c7af8b640b995822f72e2a526e593b8b0e415ffee6b14269c110f2") --Main Windows Depot EA SPORTS FC 25
+setManifestid(2669321, "481161766096858153", 51206397920)
+addappid(2669322, 1, "dc956f808120a3be1d76100275434df05c8d8cfc823a248be5a43eebec44f640") --Main Windows Language Depot English EA SPORTS FC 25
+setManifestid(2669322, "6262367819949908011", 3553454976)
+addappid(2669323, 1, "120647853dff26407d7fc2abde8c0d0a0ac57092f50fec65af5dc93c740412b9") --Main Windows Language Depot German EA SPORTS FC 25
+setManifestid(2669323, "7111451473676100595", 4764202848)
+addappid(2669324, 1, "0ba16b84bb418aceaeeba17a8040aa0784fb0babb34201b7b474f006e2fcfa1d") --Main Windows Language Depot French EA SPORTS FC 25
+setManifestid(2669324, "1290241065960430622", 4519967920)
+addappid(2669325, 1, "fede776f29a56961ee68ef6293bfaf8aab8aa9a2965b2779853977c78e4e9515") --Main Windows Language Depot Italian EA SPORTS FC 25
+setManifestid(2669325, "5752336469737282639", 4862330608)
+addappid(2669326, 1, "99c851e993373cec85cbde94fc6b81527d64cb7425ec494292ed271bb6698544") --Main Windows Language Depot Koreana EA SPORTS FC 25
+setManifestid(2669326, "8127865575290614383", 4537150064)
+addappid(2669327, 1, "31970a047d64971c2ea8b19bbe117fa974d66424980227a1d7d2d6262b5dbe69") --Main Windows Language Depot Spanish EA SPORTS FC 25
+setManifestid(2669327, "8198328484011367904", 4821824848)
+addappid(2669328, 1, "afda87b077dfe9ffd05d8d16286646f99eefd1d5317eea27397b157a51513773") --Main Windows Language Depot Schinese EA SPORTS FC 25
+setManifestid(2669328, "6688489169711928475", 4759683104)
+addappid(2669329, 1, "cba103955aec236df9b4aa2a729060e69f945913d41fe741d48bd11edfcf6faa") --Main Windows Language Depot Tchinese EA SPORTS FC 25
+setManifestid(2669329, "4100270276454552706", 3553459392)
+addappid(2680691, 1, "7e336f037173d9491be5ab708032468f7873ef3aa10d66c71592c68f0aa9bb28") --Main Windows Language Depot Russian EA SPORTS FC 25
+setManifestid(2680691, "3964038065154161259", 4700324016)
+addappid(2680692, 1, "45bda3ea04f65dab8d6c879a4425a28a7fd425e307d46f3c8fd6a1f54e49e8f7") --Main Windows Language Depot Japanese EA SPORTS FC 25
+setManifestid(2680692, "4008944280510691451", 5002838032)
+addappid(2680693, 1, "3a23a69dc514223a590eead3190031e66bf74fdfb518f4241da1980f9aa3b27d") --Main Windows Language Depot Portuguese EA SPORTS FC 25
+setManifestid(2680693, "1894172545365758947", 3553463040)
+addappid(2680694, 1, "3efa7e40b483eafe6dd1ab0d630a534f4a59efc80f5a93f61d06628210886916") --Main Windows Language Depot Polish EA SPORTS FC 25
+setManifestid(2680694, "2269701458719285588", 4960664592)
+addappid(2680695, 1, "b0e4fd739905fbb7a8ae785a9e8e342b12abc7aa375954786c5e8f25687ccdca") --Main Windows Language Depot Danish EA SPORTS FC 25
+setManifestid(2680695, "5698308937701658977", 3553462304)
+addappid(2680696, 1, "8c434a5108df457d72c7f6472b97d3658e6d727d57ce5c2db7f8fb5676e70845") --Main Windows Language Depot Dutch EA SPORTS FC 25
+setManifestid(2680696, "6198365047874360393", 5076226016)
+addappid(2680697, 1, "e14e95bfcc27a1a58ce4841f1e94b0ab0a351fcf7cf3a4383a3a88f613148d55") --Main Windows Language Depot Norwegian EA SPORTS FC 25
+setManifestid(2680697, "7084185209381154316", 3553461712)
+addappid(2680698, 1, "05b460cb4261fe5841ae3420742eb2d457e4a26534007281ccbad624733331e8") --Main Windows Language Depot Swedish EA SPORTS FC 25
+setManifestid(2680698, "2407673484597783089", 3553462752)
+addappid(2680699, 1, "fd5aafb61688f4719e52cd5424b0541c0f80136f0cf186f1a0c66345ab9534a2") --Main Windows Language Depot Czech EA SPORTS FC 25
+setManifestid(2680699, "5625354530565475879", 3553468160)
+addappid(2729900, 1, "cd5183550e52913bb9bf1d40ff2c4de7e7e577cd230d15ada99473f6c367559f") --Main Windows Language Depot Turkish EA SPORTS FC 25
+setManifestid(2729900, "3992618973168510907", 3553464800)
+addappid(2729901, 1, "21634bae2b1aed165f2d959a5c4b16bcdeabc02b6f57bae0769628aef8708fe2") --Main Windows Language Depot Arabic EA SPORTS FC 25
+setManifestid(2729901, "3623094243050134489", 5193035056)
+addappid(2729902, 1, "1aa3d3f428af74beed3aa55d0688db59e4ca40652a97044e9878762c5d4135e3") --Main Windows Language Depot Brazilian EA SPORTS FC 25
+setManifestid(2729902, "8142272950266837863", 5097827248)
+addappid(2729903, 1, "01425fde4562981e5240f3797c537fc38d9001fd3a17607931c0ec196f129c40") --Main Windows Language Depot Latam EA SPORTS FC 25
+setManifestid(2729903, "7534281391755315430", 5057482464)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(3340992, 1, "7112f072ac3a0100e541bc9b1556d3dd0697e7641094f3389e811ceb50efaf3e") --Share Windows Depot EA APP Installer
+setManifestid(3340992, "6620854805620489428", 246896656)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6") --Share Windows Depot EA Javelin Anticheat Installer
+setManifestid(3893181, "4878189275181659469", 317406032)
+addappid(2680690) --Dlcname EA SPORTS FC™ 25 - Key
+addappid(2898330) --Dlcname EA SPORTS FC™ 25 Ultimate Edition Content
+addappid(2898340) --Dlcname EA SPORTS FC™ 25 Standard Edition Pre-Purchase content
+addappid(2898350) --Dlcname EA SPORTS FC™ 25 Ultimate Edition Pre-Purchase content
+addappid(2898360) --Dlcname EA SPORTS FC™ 25 Ultimate Edition Pre-Purchase
+addappid(2898370) --Dlcname FC 25 - EA Play Trial Key
+addappid(2898380) --Dlcname FC 25 - Press Offer Key
+addappid(2898450) --Dlcname EA SPORTS FC™ 25 SHOWCASE
+addappid(3102940) --Dlcname EA SPORTS FC™ 25
+addappid(3170610) --Dlcname EA SPORTS FC™ 25 - FC Points
+addappid(3654680) --Dlcname EA SPORTS FC™ 25 Football Ultimate Team™ rewards
+addappid(3876160) --Dlcname EA SPORTS FC™ 25 Football Ultimate Team™ 92+ OVR ICON
+addappid(3971400) --Dlcname EA SPORTS FC™ 25 Football Ultimate Team™ reward
