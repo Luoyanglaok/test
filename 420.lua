fde62988f8b4f9b96d46771eb7d1bf1a03e218d7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 22:18:14 +0800

    add 420.lua

--- 420.lua
+++ 420.lua
@@ -1,25 +1,42 @@
-addappid(420)
-setManifestid(221,"4622516436473900521")
-setManifestid(222,"6432688940779947022")
-setManifestid(223,"5765085572202220494")
-setManifestid(224,"2413749032760673540")
-setManifestid(225,"1805911430347137415")
-setManifestid(226,"2714357289061426116")
-setManifestid(227,"7050670928848178928")
-setManifestid(228,"6436857496404272512")
-setManifestid(389,"1776767667551246449")
-setManifestid(380,"2711482073477362647")
-setManifestid(381,"4542662715823929389")
-setManifestid(382,"7879117186494235404")
-setManifestid(383,"5648275856895286705")
-setManifestid(384,"8194814195373219380")
-addappid(420,0,"efa23c50d649235917d96f24e2f8734c7a3197fc5fe1c4a7ad5c966b2a884a16")
-setManifestid(420,"5401366820066204187")
-addappid(421,0,"5afe8a6d938c98a0e2d90a41456af70491194fc5f6b5260a6f3bed29373c7429")
-setManifestid(421,"3785722811354167350")
-addappid(422,0,"e2b5824301e9bd1543eaf2c562f72f5316b49a4dbc32e60490f1bdffd7a0706e")
-setManifestid(422,"3441685239460551354")
-addappid(423,0,"ad29ddede5c35a594e2c05fbd58f8d2632076af98a998dd0b2364cc797319287")
-setManifestid(423,"1912720665726462040")
-addappid(424,0,"e3d554af58e735c2ae0e2ff4e56cddac154795786f969d1fd69790d93daa90be")
-setManifestid(424,"1258244556687904014")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 420.lua
+--Gamename Half-Life 2: Episode Two
+addappid(420, 1, "efa23c50d649235917d96f24e2f8734c7a3197fc5fe1c4a7ad5c966b2a884a16") --Mainappid Half-Life 2: Episode Two
+setManifestid(420, "5401366820066204187", 1914186784)
+addappid(421, 1, "5afe8a6d938c98a0e2d90a41456af70491194fc5f6b5260a6f3bed29373c7429") --Main Windows Language Depot French Half-Life 2: Episode Two
+setManifestid(421, "3785722811354167350", 326500720)
+addappid(422, 1, "e2b5824301e9bd1543eaf2c562f72f5316b49a4dbc32e60490f1bdffd7a0706e") --Main Windows Language Depot German Half-Life 2: Episode Two
+setManifestid(422, "3441685239460551354", 336658896)
+addappid(423, 1, "ad29ddede5c35a594e2c05fbd58f8d2632076af98a998dd0b2364cc797319287") --Main Windows Language Depot Russian Half-Life 2: Episode Two
+setManifestid(423, "1912720665726462040", 182881088)
+addappid(424, 1, "e3d554af58e735c2ae0e2ff4e56cddac154795786f969d1fd69790d93daa90be") --Main Windows Language Depot Spanish Half-Life 2: Episode Two
+setManifestid(424, "1258244556687904014", 337649744)
+--Share Depots
+addappid(221, 1, "f5e105e91fb4354535c3cf3c8951788ab552be70223608c6694363a226133b85") --Share Windows Depot Half-Life 2
+setManifestid(221, "4622516436473900521", 2751430624)
+addappid(222, 1, "3ab884cba0fadaca0c1fd679674ded49751376c9d31601cbf5024df1f82d4506") --Share Windows Depot Half-Life 2
+setManifestid(222, "6432688940779947022", 205502384)
+addappid(223, 1, "9bdd77e77514c4ab1868ccf58a8d8664e65f69ecbe2cfb770769034477b8c86e") --Share Macos Depot Half-Life 2
+setManifestid(223, "5765085572202220494", 181296416)
+addappid(224, 1, "918d917917c2b39588108c8009673f885025a339340afc19a5ef1941d5925595") --Share Linux Depot Half-Life 2
+setManifestid(224, "2413749032760673540", 194427600)
+addappid(225, 1, "60e635e4daf91e2fd671ffc26e4941dab0988ae3f3a2328dfc04ffe4117e26b5") --Share Windows Language Depot Russian Half-Life 2
+setManifestid(225, "1805911430347137415", 706746416)
+addappid(226, 1, "1bd20044a82e9539ac742ac3691df05152904d82342d9255f45b52c0c57e54c6") --Share Windows Language Depot Spanish Half-Life 2
+setManifestid(226, "2714357289061426116", 658763808)
+addappid(227, 1, "f6aa27353df328897edbd51b5a825d70f7c358807397a93eb5abfcb94b183572") --Share Windows Language Depot French Half-Life 2
+setManifestid(227, "7050670928848178928", 646144000)
+addappid(228, 1, "d75d5d119b336a64745a13676bd7006d972c3de556e771e0009661ff7d4dd955") --Share Windows Language Depot German Half-Life 2
+setManifestid(228, "6436857496404272512", 652717808)
+addappid(380, 1, "c7483771aa5cabfa034d86283a17438be34dd95bd3acb5ecc02bf62e527389e4") --Share Windows Depot Half-Life 2: Episode One
+setManifestid(380, "2711482073477362647", 284474240)
+addappid(381, 1, "43cbfef04a73172acde761eb5dfcd8d4ae0a78e6069a5cc5740f83dfb9f352f8") --Share Windows Language Depot French Half-Life 2: Episode One
+setManifestid(381, "4542662715823929389", 388127888)
+addappid(382, 1, "d93a483c0da0814e1da42efc92e12f380240561dc3ff6c017716823795d35d1c") --Share Windows Language Depot German Half-Life 2: Episode One
+setManifestid(382, "7879117186494235404", 431863328)
+addappid(383, 1, "0887582dc2d4bd92c677b3f22cc2e28bb2066c0c00392413f9fcc2fa90a7b43f") --Share Windows Language Depot Russian Half-Life 2: Episode One
+setManifestid(383, "5648275856895286705", 503928176)
+addappid(384, 1, "27ad5f59a417b17931dfc8b56de22c57060ae041d012382a7dfc8b6b9a1ecb24") --Share Windows Language Depot Spanish Half-Life 2: Episode One
+setManifestid(384, "8194814195373219380", 405212960)
+addappid(389, 1, "e8a443d7ad9e767467e32e045d3f57cebe75478eb4faec1ea9f36f098df236c8") --Share Windows Depot Half-Life 2: Episode One
+setManifestid(389, "1776767667551246449", 757078880)
