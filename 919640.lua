d2ea82dfe65a4417bf890bd3ba75f3e50df144dc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 13:54:47 +0800

    add 919640.lua

--- 919640.lua
+++ 919640.lua
@@ -1,9 +1,53 @@
-addappid(919640)
-setManifestid(228987,"4302102680580581867")
-setManifestid(228990,"1829726630299308803")
-addappid(919641,0,"e4accb53bb694864b0f3b1524387a0a4035d482624f09d6d6872b321642d10d3")
-setManifestid(919641,"7166862061550002029")
-addappid(988170,0,"ef1a14771041deccaf7b6e1d0b2a5892cc9729e1cb8f17f89e6a96c0b21fa4a0")
-setManifestid(988170,"2712962817460656790")
-addappid(988172,0,"c0e3624a2143c3fc6e9db28bd40caca3c7187fd628e0e86469ed44f242b6224e")
-setManifestid(988172,"8797564130778543687")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 919640.lua
+--Gamename Steel Division 2
+addappid(919640, 1, "225f2c8e9c94afbd1afe871f88a3b7812cf2e7c99a9a340d6684bc543f435deb") --Mainappid Steel Division 2
+addappid(919641, 1, "e4accb53bb694864b0f3b1524387a0a4035d482624f09d6d6872b321642d10d3") --Main Windows Depot Steel Division 2
+setManifestid(919641, "7926993569140861378", 61809373616)
+--Dlcname Steel Division 2 - Commander Deluxe Pack
+addappid(988170, 1, "ef1a14771041deccaf7b6e1d0b2a5892cc9729e1cb8f17f89e6a96c0b21fa4a0") --Dlcname Steel Division 2 - Commander Deluxe Pack
+setManifestid(988170, "2712962817460656790", 179125664)
+--Dlcname Steel Division 2 - Preorder Pack
+addappid(988172, 1, "c0e3624a2143c3fc6e9db28bd40caca3c7187fd628e0e86469ed44f242b6224e") --Dlcname Steel Division 2 - Preorder Pack
+setManifestid(988172, "8797564130778543687", 3216640)
+--Share Depots
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228987, "4302102680580581867", 22936736)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(988171) --Dlcname Steel Division 2 - History Pass
+addappid(1022830) --Dlcname Steel Division 2 - Back To War Pack
+addappid(1130850) --Dlcname Steel Division 2 - Reinforcement pack #1
+addappid(1165510) --Dlcname Steel Division 2 - Death on the Vistula
+addappid(1167000) --Dlcname Steel Division 2 - Reinforcement Pack #11
+addappid(1167020) --Dlcname Steel Division 2 - Reinforcement Pack #2
+addappid(1167030) --Dlcname Steel Division 2 - Reinforcement Pack #3
+addappid(1167040) --Dlcname Steel Division 2 - Reinforcement Pack #4
+addappid(1167050) --Dlcname Steel Division 2 - Reinforcement Pack #5
+addappid(1236360) --Dlcname Steel Division 2 - The Fate of Finland
+addappid(1254980) --Dlcname Steel Division 2 - Reinforcement Pack #6 - Auto Deployment
+addappid(1254990) --Dlcname Steel Division 2 - Reinforcement Pack #7 - Rules of Engagement
+addappid(1254991) --Dlcname Steel Division 2 - Reinforcement Pack #8 - Destruction Mode
+addappid(1307590) --Dlcname Steel Division 2 - Reinforcement Pack #9
+addappid(1307591) --Dlcname Steel Division 2 - Reinforcement Pack #10
+addappid(1307600) --Dlcname Steel Division 2 - Tribute to D-Day Pack
+addappid(1348670) --Dlcname Steel Division 2 - Black Sunday
+addappid(1405650) --Dlcname Steel Division 2 - Nemesis 1 Sandomierz
+addappid(1418230) --Dlcname Steel Division 2 - Nemesis #2 - Lvov Offensive
+addappid(1418231) --Dlcname Steel Division 2 - Burning Baltics
+addappid(1477780) --Dlcname Steel Division 2 - Nemesis #3 - Battle of Rimini
+addappid(1571580) --Dlcname Steel Division 2 - Turda
+addappid(1571590) --Dlcname Steel Division 2 - Nemesis #4 - Storming Toulon
+addappid(1640470) --Dlcname Tribute to the Liberation of Italy
+addappid(1810030) --Dlcname Steel Division 2 - Reinforcement Pack #12
+addappid(1810031) --Dlcname Steel Division 2 - Reinforcement Pack #13
+addappid(1833830) --Dlcname Steel Division 2 - Nemesis #5 - Raid on Drvar
+addappid(2155260) --Dlcname Steel Division 2 - Nemesis #6 - Siege of Dunkirk
+addappid(2499890) --Dlcname Steel Division 2 - Men of Steel
+addappid(2873780) --Dlcname Steel Division 2 - Tribute to Normandy '44
+addappid(3023320) --Dlcname Steel Division 2 - Reinforcement Pack #14 - Aces
+addappid(3204550) --Dlcname Steel Division 2 - Reinforcement Pack #15 - Valley of Death
+addappid(3210410) --Dlcname Steel Division 2 - Reinforcement Pack #15 - Dukla Pass
+addappid(3302920) --Dlcname Steel Division 2 - Nemesis #7 - Hell's Highway
+addappid(3508600) --Dlcname Steel Division 2 - Reinforcement Pack #17 - War Reserves
+addappid(3761870) --Dlcname Steel Division 2 - Battle of Dukla Pass
