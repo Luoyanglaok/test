871455207d9ee1a92edd0c1b83d49cc49a03f107
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 16:02:46 +0800

    add 2804310.lua

--- 2804310.lua
+++ 2804310.lua
@@ -1,3 +1,6 @@
-addappid(2804310)
-addappid(2804311,0,"1156be0f2d0d8034493e9a232a6b2a9b88956433f10d91c591267342e2fe5cbe")
-setManifestid(2804311,"704910360468490647")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2804310.lua
+--Gamename California Swingers Club - Season 1: Sea Swap
+addappid(2804310) --Mainappid California Swingers Club - Season 1: Sea Swap
+addappid(2804311, 1, "1156be0f2d0d8034493e9a232a6b2a9b88956433f10d91c591267342e2fe5cbe") --Main Windows/Linux Depot California Swingers Club - Season 1: Sea Swap
+setManifestid(2804311, "704910360468490647", 491502416)
