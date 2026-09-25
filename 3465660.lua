71253e47e49e94a85eb29768a670a58de45d540c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 17:42:22 +0800

    add 3465660.lua

---
+++ 3465660.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3465660.lua
+--Gamename Baddies, Inc.
+addappid(3465660, 1, "05883735698f6414c3805311a2d2869753aaeee5d678d4addceb4aa6e6776585") --Mainappid Baddies, Inc.
+addappid(3465661, 1, "32ef8cbd62599ed9309fd8eb94c8de33034029c11b52a58b00dcdd65406a0e5e") --Main Windows Depot Baddies, Inc.
+setManifestid(3465661, "3992674734646297522", 8765618048)
