e18a84e9a799bf4e0868a7ed2e80b20dfa361a3e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 10:13:10 +0800

    add 4753960.lua

---
+++ 4753960.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4753960.lua
+--Gamename Meow Squad: Incremental Fishing
+addappid(4753960) --Mainappid Meow Squad: Incremental Fishing
+addappid(4753961, 1, "a91e042bc2bbd31e5ab92c40356f2c5c9d4c01ab3fe84e61751e44604160c924") --Main Windows Depot Meow Squad: Incremental Fishing
+setManifestid(4753961, "8693985744420820508", 317846512)
