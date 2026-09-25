13bae8ffddd606a72114bc5096086199a0b119d4
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 12:45:54 +0800

    add 2361680.lua

--- 2361680.lua
+++ 2361680.lua
@@ -1,3 +1,10 @@
-addappid(2361680)
-addappid(2361688,0,"07a52ef0c4000ac45e3da0a62c446d7b48ca61f9cc84b39b7f27b2fa3a8350ef")
-setManifestid(2361688,"8997916810374406356")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2361680.lua
+--Gamename Path of Kung Fu
+addappid(2361680, 1, "2a4b729e6af8835c4895a641bcd108277f517579fa0b96c63cd21d65ddb60f0f") --Mainappid Path of Kung Fu
+addtoken(2361680, "17205939894993219909")
+addappid(2361688, 1, "07a52ef0c4000ac45e3da0a62c446d7b48ca61f9cc84b39b7f27b2fa3a8350ef") --Main Windows/Linux Depot Path of Kung Fu
+setManifestid(2361688, "2847853889570080079", 5837079088)
+addappid(4967090) --Dlcname Path of Kung Fu: Bloodstained Desert
+--Missing Dlcs
+--Dlcname Path of Kung Fu-Deluxe Edition DLC
