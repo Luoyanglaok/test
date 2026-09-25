d1471aa154c597a93dcacf95d7964321c4fb435a
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 22:21:12 +0800

    add 2784840.lua

--- 2784840.lua
+++ 2784840.lua
@@ -1,3 +1,6 @@
-addappid(2784840)
-addappid(2784841,0,"42eef758b86a50a86995d956fc50c0fe1940b2600c471c414e5c3d68e4f0b8d6")
-setManifestid(2784841,"8182437270433283834")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2784840.lua
+--Gamename Egg
+addappid(2784840) --Mainappid Egg
+addappid(2784841, 1, "42eef758b86a50a86995d956fc50c0fe1940b2600c471c414e5c3d68e4f0b8d6") --Main Windows Depot Egg
+setManifestid(2784841, "6386906044354573667", 35808992)
