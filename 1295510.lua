0a05d1dfae07f80c0d7cd39ee0207fcb3f0e29a6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 20:10:29 +0800

    add 1295510.lua

--- 1295510.lua
+++ 1295510.lua
@@ -1,5 +1,13 @@
-addappid(1295510)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(1295513,0,"940f6f87ded1c6f17229cc6e898c946f8ae111e0f74214543e0d73384711c0f2")
-setManifestid(1295513,"8306833616847715577")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1295510.lua
+--Gamename DRAGON QUEST® XI S: Echoes of an Elusive Age™ - Definitive Edition
+addappid(1295510, 1, "fcbbfc18d572cffd60b408510f582329da3d88b37372e4b6996889cde4c7dccd") --Mainappid DRAGON QUEST® XI S: Echoes of an Elusive Age™ - Definitive Edition
+addappid(1295512, 1, "c4f1eb51eed857dfea8f5d5cfdd2ebcc210d2497176457360b5c6a61e312d6b6") --Main Windows Depot DRAGON QUEST® XI S: Echoes of an Elusive Age™ - Definitive Edition
+setManifestid(1295512, "3100793236704427557", 30063077664)
+addappid(1295513, 1, "940f6f87ded1c6f17229cc6e898c946f8ae111e0f74214543e0d73384711c0f2") --Main Windows Depot DRAGON QUEST® XI S: Echoes of an Elusive Age™ - Definitive Edition
+setManifestid(1295513, "8306833616847715577", 29912297392)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
