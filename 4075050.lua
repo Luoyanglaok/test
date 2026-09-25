983aa1fe9901a11e19644b140d1e71db8d3547e7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 15:37:47 +0800

    add 4075050.lua

---
+++ 4075050.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4075050.lua
+--Gamename DrainSim Demo
+addappid(4075050) --Mainappid DrainSim Demo
+addappid(4075051, 1, "2695a895183bf1e2e9b4a0669d5a9c1f2c2fe312d44a0491f50e2e9ff72b14ec") --Main Windows Depot DrainSim Demo
+setManifestid(4075051, "5329753612309331371", 3228590880)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
