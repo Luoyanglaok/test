8f66471d9e97c0ff07230f2faa95896967e7b992
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 17:09:02 +0800

    add 4727230.lua

---
+++ 4727230.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4727230.lua
+--Gamename 修仙挂机
+addappid(4727230, 1, "4ec112c069d1d2f6e9c85bec43445196b8ef6f7e040d7e7a67b6324763cc247c") --Mainappid 修仙挂机
+addappid(4727231, 1, "cd3d58029429443f55604c1ae3ecb03626751fe30ee31aa6eec6588ad0ed4a6e") --Main Windows Depot 修仙挂机
+setManifestid(4727231, "2977421548301717957", 179339488)
