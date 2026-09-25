3373026643a427afc0846c4a8875d6d1c5cef096
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 13:32:10 +0800

    add 2445690.lua

--- 2445690.lua
+++ 2445690.lua
@@ -4,14 +4,8 @@
 addappid(2445690, 1, "6e39d0da9d7ac3fbc396e7db8e789a71a59d33412790e89ff97fbf608ee82cae") --Mainappid Lost Castle 2
 addtoken(2445690, "12154854772648009173")
 addappid(2445691, 1, "58aa213ce335109a11490383c49a406f0bb7a5c68f2844e33e7883c4e70dc1b3") --Main Windows Depot Lost Castle 2
-setManifestid(2445691, "3363074757986384016", 1243358144)
+setManifestid(2445691, "2132911329947198732", 1761237536)
 addappid(2445692, 1, "58dbbc802b67fc0b3afdbf2a15ca8c437ac3a2360fc39b5f8843ccd3df398229") --Main Windows Depot Lost Castle 2
-setManifestid(2445692, "7038033071721194819", 2845680960)
+setManifestid(2445692, "3445920796754077227", 3367178800)
 addappid(2445695, 1, "91b6b4a727098238ce348dc1f01e054f447397b059c1523e79f9ebf765484d74") --Main Windows Depot Lost Castle 2
-setManifestid(2445695, "4423532017770817262", 1243327376)
---Dlcname Lost Castle 2: Original Soundtrack
-addappid(3063440) --Dlcname Lost Castle 2: Original Soundtrack
-addappid(3063441, 1, "c1d21ca1858801ffae2c9192fb45d3bdd99237ca7e8618c1219ff376a3a71d2b") --Dlc Windows Depot Lost Castle 2: Original Soundtrack
-setManifestid(3063441, "2819975559896331081", 130288160)
-addappid(3063442, 1, "29d2548be087391509e333ae33590454d6a4cf74613834d2f0f1aad240ec9adb") --Dlc Windows Depot Lost Castle 2: Original Soundtrack
-setManifestid(3063442, "8114837711276185997", 916431488)
+setManifestid(2445695, "8060553020547175448", 1760804128)
