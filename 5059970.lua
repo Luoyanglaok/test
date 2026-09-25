ea0c5603bb580534954d1e77a87f91d3f003070d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 16:27:37 +0800

    add 5059970.lua

---
+++ 5059970.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5059970.lua
+--Gamename In-Security Demo
+addappid(5059970) --Mainappid In-Security Demo
+addappid(5059971, 1, "d89a3a6f730100745073b4db8230b194f88211054570aaa581e55572c50af540") --Main Windows Depot In-Security Demo
+setManifestid(5059971, "5374121019991067454", 1534624624)
