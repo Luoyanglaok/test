792f011bbbf9837c1e4916da1025afd3e63ba79d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 12:47:23 +0800

    add 1859910.lua

--- 1859910.lua
+++ 1859910.lua
@@ -1,3 +1,6 @@
-addappid(1859910)
-addappid(1859911,0,"015b5360486d0725e48b73d3393cd31c90b11b916f093b088bd1e64b6b59d262")
-setManifestid(1859911,"2772355619964001970")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1859910.lua
+--Gamename 活俠傳
+addappid(1859910) --Mainappid 活俠傳
+addappid(1859911, 1, "015b5360486d0725e48b73d3393cd31c90b11b916f093b088bd1e64b6b59d262") --Main Windows Depot 活俠傳
+setManifestid(1859911, "4961350118399877846", 1046087872)
