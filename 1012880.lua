162649c4b96bfffb7cf609fe858a67cbfa25bc0c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 22:28:11 +0800

    add 1012880.lua

--- 1012880.lua
+++ 1012880.lua
@@ -1,5 +1,8 @@
-addappid(1012880)
-addappid(1012881,0,"0f953c110914b4b70a6b6846415d46d0386b0cc1ce3874be245f8b14f494ab6f")
-setManifestid(1012881,"1819672003451388367")
-addappid(1012882,0,"8daf35d709e19c788b82f7b20528bb2633d0ed73b8cebb055b448c3ac4d822f5")
-setManifestid(1012882,"8775241307099537733")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1012880.lua
+--Gamename 60 Seconds! Reatomized
+addappid(1012880, 1, "9604cb48ff5dc92f51032d77feb0ee97e2f4b4f1ebca130c1dd7d0a11984046c") --Mainappid 60 Seconds! Reatomized
+addappid(1012881, 1, "0f953c110914b4b70a6b6846415d46d0386b0cc1ce3874be245f8b14f494ab6f") --Main Windows Depot 60 Seconds! Reatomized
+setManifestid(1012881, "3626135326864412370", 559495168)
+addappid(1012882, 1, "8daf35d709e19c788b82f7b20528bb2633d0ed73b8cebb055b448c3ac4d822f5") --Main Macos Depot 60 Seconds! Reatomized
+setManifestid(1012882, "5165160170026404809", 582626848)
