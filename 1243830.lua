f87c175bd32dc3ead52c930278dd58fcfd1676c3
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 09:55:10 +0800

    add 1243830.lua

--- 1243830.lua
+++ 1243830.lua
@@ -1,6 +1,14 @@
-addappid(1243830)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229007,"4477590687906973371")
-addappid(1243831,0,"ee4fef3dceab1e96eb608bde52106366ade46c83bf1db1432b02b5f5e3d9c858")
-setManifestid(1243831,"3729927846043242105")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1243830.lua
+--Gamename Overcooked! All You Can Eat
+addappid(1243830, 1, "1cd9096dd6f33b08d6b307acd9f93c2e562b305eeb93c87fd36062995990f331") --Mainappid Overcooked! All You Can Eat
+addappid(1243831, 1, "ee4fef3dceab1e96eb608bde52106366ade46c83bf1db1432b02b5f5e3d9c858") --Main Windows Depot Overcooked! All You Can Eat
+setManifestid(1243831, "932225987764535260", 7662565664)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(1528250) --Dlcname AppID 1528250
