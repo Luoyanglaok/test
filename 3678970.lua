467807f39fa5638acb4377f08c0c0e6788af6149
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 10:36:40 +0800

    add 3678970.lua

---
+++ 3678970.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3678970.lua
+--Gamename TBH: Task Bar Hero
+addappid(3678970, 1, "3babc9b9da2b5ab2409cbd2037432aba399d9c12bedfa41269d1ee4a08d29954") --Mainappid TBH: Task Bar Hero
+addappid(3678971, 1, "8d0b4ba64ffc2eaa7b8f853cd4ad4e91d7a57d637bcf3f33aba1da9c6d6360f0") --Main Windows Depot TBH: Task Bar Hero
+setManifestid(3678971, "8822702365882164571", 185385936)
+addappid(4427441, 1, "605b03f0a7c7b13393a8f49eac0b2d6c6d00419e792570eb9e29cbcd09a97a8e") --Main Windows Depot TBH: Task Bar Hero
+setManifestid(4427441, "9021990124814582885", 452454560)
+--Dlcname TBH: Task Bar Hero Soundtrack
+addappid(4427440) --Dlcname TBH: Task Bar Hero Soundtrack
+addappid(4427390) --Dlcname TBH : Task Bar Hero - Supporter Pack
+addappid(4427400) --Dlcname TBH : Task Bar Hero - 3 Stash Pack
+addappid(4427410) --Dlcname TBH : Task Bar Hero - Priest (Class)
+addappid(4427420) --Dlcname TBH : Task Bar Hero - Hunter (Class)
+addappid(4427430) --Dlcname TBH : Task Bar Hero - Slayer (Class)
+addappid(4876910) --Dlcname TBH : Task Bar Hero - Trade Ship Pack
