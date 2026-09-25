ac136703b690c6f354ade70fd64d802986c28eb5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 17:40:03 +0800

    add 1896170.lua

--- 1896170.lua
+++ 1896170.lua
@@ -1,3 +1,12 @@
-addappid(1896170)
-addappid(1896171,0,"b210131310ba6e0161da389a200c7ca8915bdf84aaa88eda3d362f8ed2aa0d82")
-setManifestid(1896171,"1938236171307717634")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1896170.lua
+--Gamename Prince of Suburbia - Part 1
+addappid(1896170, 1, "d44847814427aa29fd71855ccdc2784f49b9cde31392fd5ef86ca846da8e6efc") --Mainappid Prince of Suburbia - Part 1
+addappid(1896171, 1, "b210131310ba6e0161da389a200c7ca8915bdf84aaa88eda3d362f8ed2aa0d82") --Main Windows Depot Prince of Suburbia - Part 1
+setManifestid(1896171, "6536204489339085688", 1583110016)
+--Dlcname Prince of Suburbia - Part 2
+addappid(2149280, 1, "381f8b708871daa38594dffa4e9871100b6c0aa068857d0e7abbc7eadfb6b719") --Dlcname Prince of Suburbia - Part 2
+setManifestid(2149280, "3015389632657195296", 3774352240)
+addappid(4073030) --Dlcname Prince of Suburbia - I'm Pregnant!
+--Missing Dlcs
+--Dlcname Prince of Suburbia - Knock Me Up Again!
