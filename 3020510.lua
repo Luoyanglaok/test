340a40e8ff05be66ce0afac214d50d2ff8ee8b34
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 10:46:43 +0800

    add 3020510.lua

--- 3020510.lua
+++ 3020510.lua
@@ -3,4 +3,4 @@
 --Gamename Heroes' Vow: Three Kingdoms
 addappid(3020510, 1, "532f81ac5c609577203b243849af2a39d38c00f6a29344ee31dea0fcc812beb0") --Mainappid Heroes' Vow: Three Kingdoms
 addappid(3020511, 1, "139eea24aed3d56dc572b215fc4ea1845c3238d246a98ccef727f3eec863f528") --Main Windows Depot Heroes' Vow: Three Kingdoms
-setManifestid(3020511, "1473798629821440152", 4805182352)
+setManifestid(3020511, "1383757808870689869", 5042674688)
