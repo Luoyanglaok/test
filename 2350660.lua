98b64d983caa367ecf5fce236ce9eba242aa6772
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 17:45:19 +0800

    add 2350660.lua

--- 2350660.lua
+++ 2350660.lua
@@ -1,3 +1,6 @@
-addappid(2350660)
-addappid(2350661,0,"ddac9ab1e2c683b7e77099c44b6783a760902a31b0ec6a7249845d42fc51cf05")
-setManifestid(2350661,"1741913419592634070")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2350660.lua
+--Gamename H Girl 2
+addappid(2350660) --Mainappid H Girl 2
+addappid(2350661, 1, "ddac9ab1e2c683b7e77099c44b6783a760902a31b0ec6a7249845d42fc51cf05") --Main Windows Depot H Girl 2
+setManifestid(2350661, "1741913419592634070", 545688384)
