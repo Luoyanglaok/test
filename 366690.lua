d7691c349f3048bb4c04cc8bb31743989fe1ddb7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 22:22:33 +0800

    add 366690.lua

--- 366690.lua
+++ 366690.lua
@@ -1,5 +1,11 @@
-addappid(366690)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-addappid(366691,0,"734a892a39768b841316ffa9314c7ca1a64929a27b3cd386fd745f3f97ca5e36")
-setManifestid(366691,"8577571028466789468")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 366690.lua
+--Gamename Cue Club 2: Pool & Snooker
+addappid(366690, 1, "aed7b18d6af3b58b407413f09293f15d2db94f23291d0f57004a0d75b67f2e65") --Mainappid Cue Club 2: Pool & Snooker
+addappid(366691, 1, "734a892a39768b841316ffa9314c7ca1a64929a27b3cd386fd745f3f97ca5e36") --Main Windows Depot Cue Club 2: Pool & Snooker
+setManifestid(366691, "5848130535047939850", 132567760)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
