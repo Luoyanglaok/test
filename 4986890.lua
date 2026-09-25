8176cbaa6f42b82f4b0c3f822d55ffbb403c5e20
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 23:21:12 +0800

    add 4986890.lua

--- 4986890.lua
+++ 4986890.lua
@@ -3,4 +3,4 @@
 --Gamename Old Market Restored
 addappid(4986890) --Mainappid Old Market Restored
 addappid(4986891, 1, "3e75ac72cc4c4b7d6406c020cce6c3267c4414ee24025905051e591c72892445") --Main Windows Depot Old Market Restored
-setManifestid(4986891, "4163233862464102286", 214128272)
+setManifestid(4986891, "2576301587452573044", 212173408)
