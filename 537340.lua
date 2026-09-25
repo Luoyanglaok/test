3403f4ad4316bf89132dadc60092eae695bccc9d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 22:28:56 +0800

    add 537340.lua

--- 537340.lua
+++ 537340.lua
@@ -1,7 +1,15 @@
-addappid(537340)
-addappid(537341,0,"9666002b803cda0f99f93e3397766e9c6c0cb883c255707baf99c801e84a0925")
-setManifestid(537341,"1945190940842095773")
-addappid(537342,0,"ed469e4612ea75de1c81b19cf544912616276e2ede60b1cfc0cd516fc73725d2")
-setManifestid(537342,"5945138272911753843")
-addappid(537343,0,"4af51fcd9f2fdb0102b86c4d9d96bb6a8d926bdfb5d4980d88585e169183b3ff")
-setManifestid(537343,"29999913221118649")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 537340.lua
+--Gamename Guts and Glory
+addappid(537340, 1, "1b1e75652d752c2eb42202dac0a7d4360bc19afa6ed0a11f7e353a3472aa4a82") --Mainappid Guts and Glory
+addappid(537341, 1, "9666002b803cda0f99f93e3397766e9c6c0cb883c255707baf99c801e84a0925") --Main Windows Depot Guts and Glory
+setManifestid(537341, "1945190940842095773", 1896703968)
+addappid(537342, 1, "ed469e4612ea75de1c81b19cf544912616276e2ede60b1cfc0cd516fc73725d2") --Main Macos Depot Guts and Glory
+setManifestid(537342, "5945138272911753843", 1899425488)
+addappid(537343, 1, "4af51fcd9f2fdb0102b86c4d9d96bb6a8d926bdfb5d4980d88585e169183b3ff") --Main Linux Depot Guts and Glory
+setManifestid(537343, "29999913221118649", 1996724480)
+--Missing Dlcs
+--Dlcname Guts and Glory - Wacky Hats Pack
+--Dlcname Guts and Glory - Golden Prestige
+--Dlcname Guts and Glory - Halls of Glory
+--Dlcname Guts and Glory - Founder's Club
