59861dedd2ed64960895bdc21dc58b26facdf329
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-25 17:44:51 +0800

    add 2649970.lua

--- 2649970.lua
+++ 2649970.lua
@@ -3,10 +3,10 @@
 --Gamename Steps of Debauchery
 addappid(2649970, 1, "7b52a3698163bba57b34233c05589f1208c3f2db8a434ccb4d561d52c83669a5") --Mainappid Steps of Debauchery
 addappid(2649971, 1, "9aa3e4d1f2a413dbec23cbda51fe786f5f55564444b24d8bdbc0e05099d2a483") --Main Windows Language Depot English Steps of Debauchery
-setManifestid(2649971, "4412486912016943004", 11985197232)
+setManifestid(2649971, "2356632281691275154", 11985190704)
 --Dlcname Steps of Debauchery - Bonus Unlock
 addappid(2808680, 1, "76b27b3113a09f716499a01206b3d65237b9bf62d0bba7f3ef5bf798d3eb9881") --Dlcname Steps of Debauchery - Bonus Unlock
-setManifestid(2808680, "960448735335153791", 32063376)
+setManifestid(2808680, "950559707076587586", 32075664)
 --Dlcname Steps of Debauchery - Christmas DLC
 addappid(4111380, 1, "4886cf03723967d86c0211df4d1cf5d53f71784c40e3ca8f0142f2d0392c9a11") --Dlcname Steps of Debauchery - Christmas DLC
 setManifestid(4111380, "5362411706459390446", 2178049728)
