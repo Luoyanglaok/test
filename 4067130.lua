8996859a0461bfb4471a6fa2220ee102c94d12dd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-27 00:21:08 +0800

    add 4067130.lua

---
+++ 4067130.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 4067130.lua
+--Gamename Drifter Star: Evolution
+addappid(4067130) --Mainappid Drifter Star: Evolution
+addappid(4067131, 1, "6bafa449fca946195270e20ff8af3684b2e1175616289ca2563c22d6d554128e") --Main Windows Depot Drifter Star: Evolution
+setManifestid(4067131, "7958210869313536873", 243414272)
