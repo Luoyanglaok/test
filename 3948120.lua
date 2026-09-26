852391dc268e213ee1155ab9ec643691336c8097
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 19:48:02 +0800

    add 3948120.lua

---
+++ 3948120.lua
@@ -0,0 +1,9 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3948120.lua
+--Gamename Scritchy Scratchy
+addappid(3948120, 1, "2972cbb18aae51595ed6a585a9bb56c70060687d21ff3802cecb5c79fcc1de60") --Mainappid Scritchy Scratchy
+addappid(3948121, 1, "4d5ba58100eba881f6f9a66ee5982857f3db6a375f5319bdfddd6d9b71defc07") --Main Windows Depot Scritchy Scratchy
+setManifestid(3948121, "2568898334237591722", 186254368)
+addappid(3948122, 1, "328927d058e2934b073e34b22bcbf5103daa2dea1879877f2ef930f2cf7ef046") --Main Macos Depot Scritchy Scratchy
+setManifestid(3948122, "5819585551787439752", 214521808)
+addappid(4428640) --Dlcname Scritchy Scratchy - Supporter Pack
