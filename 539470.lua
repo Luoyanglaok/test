6d8d9a4c35b233e1a1ffb4b0acc6a9bab7a259dd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 17:27:38 +0800

    add 539470.lua

--- 539470.lua
+++ 539470.lua
@@ -1,10 +1,17 @@
-addappid(539470)
-setManifestid(228990,"1829726630299308803")
-addappid(539471,0,"97aedb03fac2a0fdfc6c50144f1fd57db3bc61e2c2b81f2d74556611635d5651")
-setManifestid(539471,"8869230369253378067")
-addappid(539472,0,"cd75e96b4cfafa6918a4cd75fc58ad80061b3e9a12c5559d138fdd214ee294b2")
-setManifestid(539472,"2521356969084536780")
-addappid(539473,0,"de069b7fb2704f6027aa1890917b246471e650250634876079df7d5fee987905")
-setManifestid(539473,"4451273467272146862")
-addappid(1158270,0,"40f85c8b7b36d0b6345da0ad638c24b53d4806ff660a43b478321fd7a9a8e951")
-setManifestid(1158270,"4825703595502710127")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 539470.lua
+--Gamename Police Stories
+addappid(539470, 1, "60a5b6588d3bb512e55ff3b05710e6a1f312863661f841e542e1711190d5da9f") --Mainappid Police Stories
+addappid(539471, 1, "97aedb03fac2a0fdfc6c50144f1fd57db3bc61e2c2b81f2d74556611635d5651") --Main Windows Depot Police Stories
+setManifestid(539471, "9152910147611349656", 289724096)
+addappid(539472, 1, "cd75e96b4cfafa6918a4cd75fc58ad80061b3e9a12c5559d138fdd214ee294b2") --Main Macos Depot Police Stories
+setManifestid(539472, "2521356969084536780", 237638480)
+addappid(539473, 1, "de069b7fb2704f6027aa1890917b246471e650250634876079df7d5fee987905") --Main Linux Depot Police Stories
+setManifestid(539473, "4451273467272146862", 238987136)
+--Dlcname Police Stories – Supporter Pack
+addappid(1158270, 1, "40f85c8b7b36d0b6345da0ad638c24b53d4806ff660a43b478321fd7a9a8e951") --Dlcname Police Stories – Supporter Pack
+setManifestid(1158270, "4825703595502710127", 742987792)
+--Share Depots
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(1329730) --Dlcname Police Stories - Zombie Case
