64fc0c1f1c5a76aa9d02de87c323faa1fdf4c316
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 19:52:56 +0800

    add 1380070.lua

--- 1380070.lua
+++ 1380070.lua
@@ -1,9 +1,19 @@
-addappid(1380070)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-setManifestid(229006,"1784011429307107530")
-setManifestid(229007,"4477590687906973371")
-addappid(1380071,0,"35bbeb039ac8fcae5ecedb7bf9592c5fbead89187c78e7fca100ccc0b85d39a9")
-setManifestid(1380071,"4304272757104631291")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1380070.lua
+--Gamename Slaves of Rome
+addappid(1380070, 1, "dc44dcfce7358c32686a81984c7badb2b91b0c41833c620cd10d2c0ed9509b32") --Mainappid Slaves of Rome
+addappid(1380071, 1, "35bbeb039ac8fcae5ecedb7bf9592c5fbead89187c78e7fca100ccc0b85d39a9") --Main Windows Depot Slaves of Rome
+setManifestid(1380071, "8915947402842588896", 6523131024)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229006, "1784011429307107530", 84953712)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
