468230903c62812a7f0d85573b5f594047755282
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 13:45:23 +0800

    add 4000.lua

--- 4000.lua
+++ 4000.lua
@@ -3,13 +3,13 @@
 --Gamename Garry's Mod
 addappid(4000, 1, "85c35e0e023c1e99185ca5645fe083568247de0cd3f786655a10845805935393") --Mainappid Garry's Mod
 addappid(4001, 1, "70b50aa1b52c66816a5f3ee61235edc988520ba54e96913f0bf7ff73c37794de") --Main Windows Depot Garry's Mod
-setManifestid(4001, "220404784471597770", 3570926160)
+setManifestid(4001, "2904029089870460835", 3577617904)
 addappid(4002, 1, "a3e3a34828218993dbc70c168dd602ee73fe43b8fef8cd6458be59de953da3d9") --Main Windows Depot Garry's Mod
-setManifestid(4002, "8989514576105529138", 127654320)
+setManifestid(4002, "8456439456790142762", 197676288)
 addappid(4003, 1, "f12729464515b02d079469c178a93188480f07765f21fe5191f3cc35c18378aa") --Main Linux Depot Garry's Mod
-setManifestid(4003, "1075478557630448531", 65058464)
+setManifestid(4003, "5169228716831564971", 65217216)
 addappid(4004, 1, "bda3d73d97ece9ee942f1275a70d9af5d37dcd48e2e47565feb3b00fc6b66dbd") --Main Macos Depot Garry's Mod
-setManifestid(4004, "2562883153234512593", 37967600)
+setManifestid(4004, "5143526130592293759", 38001648)
 --Share Depots
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228990, "1829726630299308803", 100658080)
