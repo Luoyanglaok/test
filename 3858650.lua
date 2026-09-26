2ac043c18e6309ff201729cf40bf69da76c3cc7d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-27 00:19:36 +0800

    add 3858650.lua

---
+++ 3858650.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3858650.lua
+--Gamename Crushed In Time
+addappid(3858650, 1, "3054c69c9ac8b50739c79ad4c8043669e04cf27b77f274fd8adc4ef7d56adef0") --Mainappid Crushed In Time
+addappid(3858651, 1, "d4eb5b5c4289922438a9be4e0dc73319727b396e879cfa7fe29c1a815f9df617") --Main Windows Depot Crushed In Time
+setManifestid(3858651, "8343779905575544618", 1461398112)
+addappid(4820931, 1, "5efaaeaa38f760b90d4405d11fc41012929e2234ee03729bb781c0679c7acc4d") --Main Windows Depot Crushed In Time
+setManifestid(4820931, "4156721038134433000", 176552176)
+addappid(4820932, 1, "5da8ee9f4ebfcc8774508927a1e919c7519063aebaac52c03fe624ae5db7cf0b") --Main Windows Depot Crushed In Time
+setManifestid(4820932, "3941812139378474310", 990378496)
+--Dlcname Crushed In Time Soundtrack
+addappid(4820930) --Dlcname Crushed In Time Soundtrack
