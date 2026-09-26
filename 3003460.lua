c2b71a8bb36e4b1e209b89d3375225c52d6b1b6e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 15:25:10 +0800

    add 3003460.lua

---
+++ 3003460.lua
@@ -0,0 +1,8 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3003460.lua
+--Gamename Bang Bang Barrage
+addappid(3003460, 1, "d58a79a2cc24e0e9fc24c46a8d38e03ea353fec874250c4d891054f4474fbe42") --Mainappid Bang Bang Barrage
+addappid(3003461, 1, "801d21bccaae396c2524bac81ba685f0c8e23a7d8c70f7388fd0a43a9378e748") --Main Windows Depot Bang Bang Barrage
+setManifestid(3003461, "881609683890363002", 203281536)
+addappid(3003462, 1, "FF75A3B34F78A3C5A7702BC9C4D67357E75ED6E043E71A8A57E6D52C27F4D5AE") --Main Macos Depot Bang Bang Barrage
+setManifestid(3003462, "8503393749384732053", 204004336)
