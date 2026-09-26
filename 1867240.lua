81c330433a6b0ef3b4a91250e653ae9830f6a449
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 19:23:33 +0800

    add 1867240.lua

--- 1867240.lua
+++ 1867240.lua
@@ -4,13 +4,9 @@
 addappid(1867240) --Mainappid WARDOGS
 addappid(1867242, 1, "54c641e6fc8cd622a0cd3a2eba3bae0f4b29ba18c88fb4c83c31303d474c04a7") --Main Windows Depot WARDOGS
 setManifestid(1867242, "5232764749909337392", 44790192032)
---Dlcname WARDOGS Playtest
-addappid(4809930) --Dlcname WARDOGS Playtest
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Dlc Windows Depot WARDOGS Playtest
-setManifestid(228989, "5753583882400741046", 25108528)
-addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Dlc Windows Depot WARDOGS Playtest
-setManifestid(228990, "1829726630299308803", 100658080)
-addappid(4809932, 1, "276e216889f9a00dbaa4bed1b78e1ad0f91ab6eaf0095efa06d1d6153f453048") --Dlc Windows Depot WARDOGS Playtest
-setManifestid(4809932, "213166619639388156", 44795982240)
 --Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
 addappid(4809960) --Dlcname WARDOGS - Supporter Pack
