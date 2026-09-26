813696cef7aef2ab23b345da6147458f6ebd5e3b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 13:19:08 +0800

    add 1238880.lua

--- 1238880.lua
+++ 1238880.lua
@@ -1,31 +1,63 @@
-addappid(1238880)
-setManifestid(228984,"2547553897526095397")
-setManifestid(228990,"1829726630299308803")
-addappid(1238881,0,"ca3213c27f45fd919fb3bbcc717d1fc836d56c9cadf6d7c5484aef22a2e9d886")
-setManifestid(1238881,"6826322720323828727")
-addappid(1238882,0,"2ebe691fe34ea5ca9107fd88b53a6f821a6aa045026efc3b7ef232c72ece882a")
-setManifestid(1238882,"5814594304937679643")
-addappid(1238883,0,"e1198986c9ffaab4271866ef20e776ba574e9b8b8040ecadbb3b76b1e424b205")
-setManifestid(1238883,"4310926822665069738")
-addappid(1238884,0,"2d306ab061b8201a93d108fbd2f291f24efd36512d565ba88464ec6c4b121dae")
-setManifestid(1238884,"4534105736785367841")
-addappid(1238885,0,"c5340ac0a0ed7f342cd4f828385b2cb49efb905acec556d75567da2df9e0e3c6")
-setManifestid(1238885,"1245164389321818488")
-addappid(1238886,0,"950e5af714c7dbb4e5bfe0d4ff452d1f928d21837693850f9309ab786768a295")
-setManifestid(1238886,"2017350244556318474")
-addappid(1238887,0,"30a987c78b276c7f506a4ee56651e3a334bde3ff35f7d79d5761c0d5d857b065")
-setManifestid(1238887,"403053245919075471")
-addappid(1238888,0,"2268764d6bc1f2f32a7458b3571487ede7466bca0fdc990f9afc8a3a96394ddc")
-setManifestid(1238888,"5841245290236083918")
-addappid(1238889,0,"2346893aec717451d709a962d67a2e32f10fc08642c7596c358fe9fb9b994773")
-setManifestid(1238889,"3822655056749081611")
-addappid(1238892,0,"82eb2ed4c66d175c26dd9fb2aab0dfae52ad55d50ebfd4c997983be50a4b1867")
-setManifestid(1238892,"6285831484115087703")
-addappid(1238893,0,"7bc37388f1986e9db3f5edf96121383cfc438df8d9f39875786f4ef7392e7494")
-setManifestid(1238893,"1268099092303341209")
-addappid(1238894,0,"382618919bb7d5b0a5b3f73014eccdff8ae21c7cb89f626b5215e7a74af6d103")
-setManifestid(1238894,"6956063495166561242")
-addappid(1238895,0,"888d9571a3d76aaa8c83b15934a902da713d19a9ca98c4f5127304d432b979f6")
-setManifestid(1238895,"6789680548342885196")
-addappid(3340991,0,"023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491")
-setManifestid(3340991,"1318423300601359782")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1238880.lua
+--Gamename Battlefield™ Hardline
+addappid(1238880, 1, "fe296d1d52db3d01d5bc7003ca4c205474fed0b8d6b3e179f70ceb5638e08546") --Mainappid Battlefield™ Hardline
+addappid(1238881, 1, "ca3213c27f45fd919fb3bbcc717d1fc836d56c9cadf6d7c5484aef22a2e9d886") --Main Windows Depot Battlefield™ Hardline
+setManifestid(1238881, "6826322720323828727", 40469872656)
+addappid(1238882, 1, "2ebe691fe34ea5ca9107fd88b53a6f821a6aa045026efc3b7ef232c72ece882a") --Main Windows Language Depot English Battlefield™ Hardline
+setManifestid(1238882, "5814594304937679643", 1252573168)
+addappid(1238883, 1, "e1198986c9ffaab4271866ef20e776ba574e9b8b8040ecadbb3b76b1e424b205") --Main Windows Language Depot Czech Battlefield™ Hardline
+setManifestid(1238883, "4310926822665069738", 819760)
+addappid(1238884, 1, "2d306ab061b8201a93d108fbd2f291f24efd36512d565ba88464ec6c4b121dae") --Main Windows Language Depot German Battlefield™ Hardline
+setManifestid(1238884, "4534105736785367841", 1096436688)
+addappid(1238885, 1, "c5340ac0a0ed7f342cd4f828385b2cb49efb905acec556d75567da2df9e0e3c6") --Main Windows Language Depot Spanish Battlefield™ Hardline
+setManifestid(1238885, "1245164389321818488", 1108651872)
+addappid(1238886, 1, "950e5af714c7dbb4e5bfe0d4ff452d1f928d21837693850f9309ab786768a295") --Main Windows Language Depot French Battlefield™ Hardline
+setManifestid(1238886, "2017350244556318474", 1066200288)
+addappid(1238887, 1, "30a987c78b276c7f506a4ee56651e3a334bde3ff35f7d79d5761c0d5d857b065") --Main Windows Language Depot Italian Battlefield™ Hardline
+setManifestid(1238887, "403053245919075471", 1099657184)
+addappid(1238888, 1, "2268764d6bc1f2f32a7458b3571487ede7466bca0fdc990f9afc8a3a96394ddc") --Main Windows Language Depot Japanese Battlefield™ Hardline
+setManifestid(1238888, "5841245290236083918", 1073635312)
+addappid(1238889, 1, "2346893aec717451d709a962d67a2e32f10fc08642c7596c358fe9fb9b994773") --Main Windows Language Depot Koreana Battlefield™ Hardline
+setManifestid(1238889, "3822655056749081611", 744544)
+addappid(1238892, 1, "82eb2ed4c66d175c26dd9fb2aab0dfae52ad55d50ebfd4c997983be50a4b1867") --Main Windows Language Depot Polish Battlefield™ Hardline
+setManifestid(1238892, "6285831484115087703", 1088213472)
+addappid(1238893, 1, "7bc37388f1986e9db3f5edf96121383cfc438df8d9f39875786f4ef7392e7494") --Main Windows Language Depot Portuguese Battlefield™ Hardline
+setManifestid(1238893, "1268099092303341209", 1115696128)
+addappid(1238894, 1, "382618919bb7d5b0a5b3f73014eccdff8ae21c7cb89f626b5215e7a74af6d103") --Main Windows Language Depot Russian Battlefield™ Hardline
+setManifestid(1238894, "6956063495166561242", 1125794768)
+addappid(1238895, 1, "888d9571a3d76aaa8c83b15934a902da713d19a9ca98c4f5127304d432b979f6") --Main Windows Language Depot Tchinese Battlefield™ Hardline
+setManifestid(1238895, "6789680548342885196", 719232)
+--Dlcname AppID 1279010
+addtoken(1279010, "48877489347043244")
+addappid(1279010, 1, "9f545aec964a9ed79f02aa905661e8b60d9779809df2ad83f5c9ed7b6d5f6505") --Dlcname AppID 1279010
+--Dlcname AppID 1279011
+addtoken(1279011, "17297542848205981231")
+addappid(1279011, 1, "8046b013db12e08839203c6ad72f5d63fae84e30e7cb197671a4b544b03d37da") --Dlcname AppID 1279011
+--Dlcname AppID 1279012
+addtoken(1279012, "2997523365052677686")
+addappid(1279012, 1, "0891832b3961e9961523351db5568d2e69e9512b05c42ff62f17be49f5b5d9f9") --Dlcname AppID 1279012
+--Dlcname AppID 1279013
+addtoken(1279013, "3369056561361956016")
+addappid(1279013, 1, "55a0bdf74159109ccafb4bfc27182e63926264cdbde00ca5edd3fc2fb3d21d31") --Dlcname AppID 1279013
+--Share Depots
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228984, "2547553897526095397", 13436144)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") --Share Windows Depot EA APP Installer
+setManifestid(3340991, "2872608919245318765", 246896672)
+addappid(1238890) --Dlcname AppID 1238890
+addtoken(1238890, "15157183607126784100")
+addappid(1238891) --Dlcname Battlefield™ Hardline Ultimate Shortcut Unlock
+addappid(1279340) --Dlcname AppID 1279340
+addappid(1279341) --Dlcname AppID 1279341
+addtoken(1279341, "4507831183104054246")
+addappid(1279350) --Dlcname AppID 1279350
+addappid(1279351) --Dlcname AppID 1279351
+addappid(1279352) --Dlcname AppID 1279352
+addappid(1279353) --Dlcname AppID 1279353
+addappid(1279354) --Dlcname AppID 1279354
+addappid(1279355) --Dlcname AppID 1279355
+addappid(1279356) --Dlcname AppID 1279356
+addappid(1279357) --Dlcname AppID 1279357
