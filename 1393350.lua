139d5351ef7f656e273a1bf3b10481f2dc574084
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-27 00:30:26 +0800

    add 1393350.lua

--- 1393350.lua
+++ 1393350.lua
@@ -1,5 +1,9 @@
-addappid(1393350)
-addappid(1393351,0,"127ba573c89e588d1ebf1143d5ac398396761ac9872c33a8a0cdf94ba45b9bec")
-setManifestid(1393351,"7588603003840068516")
-addappid(1411520,0,"e586ed20f11f3af27faedba91bbe31da12db4e28ced73c5eb6266186ad8abdba")
-setManifestid(1411520,"2518352327159358412")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1393350.lua
+--Gamename Swaying Girl
+addappid(1393350, 1, "988793834f16bc58c346dabfde29063a2840ab4fc0a9e84357210bf36625d91e") --Mainappid Swaying Girl
+addappid(1393351, 1, "127ba573c89e588d1ebf1143d5ac398396761ac9872c33a8a0cdf94ba45b9bec") --Main Windows Depot Swaying Girl
+setManifestid(1393351, "7588603003840068516", 68800336)
+--Dlcname Swaying Girl - Exciting
+addappid(1411520, 1, "e586ed20f11f3af27faedba91bbe31da12db4e28ced73c5eb6266186ad8abdba") --Dlcname Swaying Girl - Exciting
+setManifestid(1411520, "2518352327159358412", 177519840)
