02f424d22cd0602db55d6445d2c487a4ca1ae4dc
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 16:21:12 +0800

    add 5040600.lua

---
+++ 5040600.lua
@@ -0,0 +1,15 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5040600.lua
+--Gamename Relaxation Massage Studio
+addappid(5040600) --Mainappid Relaxation Massage Studio
+addappid(5040601, 1, "b94be0d411862dd6f0d554a9b688d97705f7a8b881175ec83ba70a008adaaf4b") --Main Windows Depot Relaxation Massage Studio
+setManifestid(5040601, "5862699051341651322", 7826591040)
+--Share Depots
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228985, "3966345552745568756", 13244128)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
