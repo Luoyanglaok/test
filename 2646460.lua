27e6989095e72ba6c1318014b498226b4e727727
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 18:39:37 +0800

    add 2646460.lua

--- 2646460.lua
+++ 2646460.lua
@@ -1,7 +1,20 @@
-addappid(2646460)
-setManifestid(228989,"3514306556860204959")
-setManifestid(228990,"1829726630299308803")
-setManifestid(229005,"7992454656023763365")
-setManifestid(229007,"4477590687906973371")
-addappid(2646461,0,"b888b7fdf975bfc80d87df6beb2c5d56ada854d2adb6537fce281905a5d74d2a")
-setManifestid(2646461,"4799391451692148856")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2646460.lua
+--Gamename Soulmask
+addappid(2646460, 1, "b23ed007512964e7e8555f41e6bb10b5e69ea21eb12bbb01e396db6520b78d59") --Mainappid Soulmask
+addtoken(2646460, "10675985120625290868")
+addappid(2646461, 1, "b888b7fdf975bfc80d87df6beb2c5d56ada854d2adb6537fce281905a5d74d2a") --Main Windows Depot Soulmask
+setManifestid(2646461, "2421147950143870109", 22998891056)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229005, "7992454656023763365", 62722592)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(229007, "4477590687906973371", 118846656)
+addappid(3731990) --Dlcname Soulmask - The Golden Legend
+addappid(3966020) --Dlcname Soulmask: Shifting Sands
+--Missing Dlcs
+--Dlcname Soulmask-Cozy Tribe Theme Pack
