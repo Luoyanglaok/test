ed84ebdf1a15718b2ffb2853e8f7b3bea9415cd2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 17:17:19 +0800

    add 2120900.lua

--- 2120900.lua
+++ 2120900.lua
@@ -1,3 +1,6 @@
-addappid(2120900)
-addappid(2120901,0,"b060afd33253ced6be83479f4afbcaf0e174a108c185c3ce5c6c106e0aaaabc3")
-setManifestid(2120901,"7788332384184121782")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2120900.lua
+--Gamename Fears to Fathom - Carson House
+addappid(2120900, 1, "9f16c7ed77227e4938584e878d735e598bae0fbfdc63c73e0138b6a8bc2e7154") --Mainappid Fears to Fathom - Carson House
+addappid(2120901, 1, "b060afd33253ced6be83479f4afbcaf0e174a108c185c3ce5c6c106e0aaaabc3") --Main Windows Depot Fears to Fathom - Carson House
+setManifestid(2120901, "7788332384184121782", 2690868656)
