ca818f7dda9c72800da85b90be17eb22fcc4e3fb
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 15:12:22 +0800

    add 2593710.lua

--- 2593710.lua
+++ 2593710.lua
@@ -1,3 +1,6 @@
-addappid(2593710)
-addappid(2593711,0,"6761396a32df35143f09e3fb76c7f66c614bb3512bd229faf87844bf13d96dfe")
-setManifestid(2593711,"8689026570724991756")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2593710.lua
+--Gamename Cyberpunk: Red-Light District
+addappid(2593710) --Mainappid Cyberpunk: Red-Light District
+addappid(2593711, 1, "6761396a32df35143f09e3fb76c7f66c614bb3512bd229faf87844bf13d96dfe") --Main Windows Depot Cyberpunk: Red-Light District
+setManifestid(2593711, "8689026570724991756", 3309034784)
