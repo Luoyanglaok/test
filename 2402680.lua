bc38de2eea2df2c595d74dfa3e5c83d4aabdd600
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 23:44:00 +0800

    add 2402680.lua

---
+++ 2402680.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2402680.lua
+--Gamename Dimraeth
+addappid(2402680) --Mainappid Dimraeth
+addappid(2402681, 1, "4f10d1efbb0cab1a8340d1e37c165519ab3ab8d50cea09ecbeff7ce6d3d5b71e") --Main Windows Depot Dimraeth
