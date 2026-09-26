c8e22ebcd986ca88d19ddd0e2c45f3fe1cb1bc34
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-27 00:15:26 +0800

    add 73170.lua

--- 73170.lua
+++ 73170.lua
@@ -1,7 +1,12 @@
-addappid(73170)
-addappid(228981)
-setManifestid(228981,"7613356809904826842")
-addappid(228983)
-setManifestid(228983,"8124929965194586177")
-addappid(73171,0,"f20fbe34b6198780a177833657451d380c4778439c6d38e1a95d6d25b938474b")
-setManifestid(73171,"1625283161133264610")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 73170.lua
+--Gamename Darkest Hour: A Hearts of Iron Game
+addappid(73170) --Mainappid Darkest Hour: A Hearts of Iron Game
+addappid(73171, 1, "f20fbe34b6198780a177833657451d380c4778439c6d38e1a95d6d25b938474b") --Main Windows Depot Darkest Hour: A Hearts of Iron Game
+setManifestid(73171, "1625283161133264610", 752302464)
+--Share Depots
+addappid(228981, 1, "9afdbd47677424be993f6b516a15ebdb5281aa318114631d14b2b23999ae97ac") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228981, "7613356809904826842", 5804736)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228983, "8124929965194586177", 19214528)
+addappid(73179) --Dlcname Darkest Hour - Forum Registration Key
