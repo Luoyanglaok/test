86c728a4075f57f46d3e217a430aaeddc42f090c
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 16:56:30 +0800

    add 1903340.lua

--- 1903340.lua
+++ 1903340.lua
@@ -4,24 +4,6 @@
 addappid(1903340, 1, "302b1b9c8eb04ca24027876370a3072643790a12b835cabccaecb15b226075e8") --Mainappid Clair Obscur: Expedition 33
 addappid(1903341, 1, "8593f9c4cd595f833d5344e55cd6c6fdb73d9a4fa05dab033ba71622265e8c82") --Main Windows Depot Clair Obscur: Expedition 33
 setManifestid(1903341, "3499712487662840491", 45436725120)
---Dlcname Clair Obscur: Expedition 33 – Original Soundtrack
-addappid(3405480) --Dlcname Clair Obscur: Expedition 33 – Original Soundtrack
-addappid(3405510, 1, "d05c5a4e1666b64375d849a60b8f3694548fd5f756513928e1458c033101b848") --Dlc Windows Depot Clair Obscur: Expedition 33 – Original Soundtrack
-setManifestid(3405510, "6886116125216359724", 1190294816)
-addappid(3405511, 1, "8cb3c5d263faf7383c63bcb27fb60e3f0a4de6b3a8c8f1fac33c9d12f41848ee") --Dlc Windows Depot Clair Obscur: Expedition 33 – Original Soundtrack
-setManifestid(3405511, "6241476744323481469", 7933719168)
---Dlcname Clair Obscur: Expedition 33 – Nos Vies En Lumière (Bonus Edition)
-addappid(3873750) --Dlcname Clair Obscur: Expedition 33 – Nos Vies En Lumière (Bonus Edition)
-addappid(3873752, 1, "28F6F00DD0123ED941D1B387D162E94C3A0040474403F053DC5DDFC9B7CCB41F") --Dlc Windows Depot Clair Obscur: Expedition 33 – Nos Vies En Lumière (Bonus Edition)
-setManifestid(3873752, "4557475591349139350", 219683232)
-addappid(3873753, 1, "1AB83DEA5EA61CC95D7370AFBD1CC0C6446E1CE089F595E6066B326713BA128D") --Dlc Windows Depot Clair Obscur: Expedition 33 – Nos Vies En Lumière (Bonus Edition)
-setManifestid(3873753, "3830972184782627826", 922380352)
---Dlcname Clair Obscur: Expedition 33 – Verso's Drafts (Original Soundtrack)
-addappid(4227900) --Dlcname Clair Obscur: Expedition 33 – Verso's Drafts (Original Soundtrack)
-addappid(4227901, 1, "0364d6dfb72281b8afa9a73d686b35f04f47178a4adbd96ad0282ce932dd7078") --Dlc Windows Depot Clair Obscur: Expedition 33 – Verso's Drafts (Original Soundtrack)
-setManifestid(4227901, "2913298083235666604", 64389296)
-addappid(4227902, 1, "db4a32a14f358738396d6ab2200121ffb0b32e8490ace45edf516f670954c302") --Dlc Windows Depot Clair Obscur: Expedition 33 – Verso's Drafts (Original Soundtrack)
-setManifestid(4227902, "3650867446176062803", 207862848)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
