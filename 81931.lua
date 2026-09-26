0afb4b58b91402b2960746c22b2698fdd4dfe565
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 14:21:02 +0800

    add 81931.lua

---
+++ 81931.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 81931.lua
+--Gamename AppID_81931
+addappid(81931) --Mainappid AppID_81931
+addappid(910, 1, "168405455442eaf4e8e75a13b5914f35d7be54b0cdc4220ea1dc464f79711248") --Main Windows Depot AppID_81931
+setManifestid(910, "1332084816209327708", 0)
