820baf4533d6bf37880c6b3682a70f288aca88b5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 12:27:59 +0800

    add 5206350.lua

---
+++ 5206350.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 5206350.lua
+--Gamename 高三最后300天
+addappid(5206350) --Mainappid 高三最后300天
+addappid(5206351, 1, "1a3d2abc866f8278cf9b0529d2786870211166c14e57b2b8c412d1146ad37aea") --Main Windows Depot 高三最后300天
+setManifestid(5206351, "9181656292740106058", 925936832)
