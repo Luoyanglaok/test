179264103c851c59b17296f8dafbc533b77634a8
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 19:54:31 +0800

    add 1745310.lua

--- 1745310.lua
+++ 1745310.lua
@@ -1,3 +1,6 @@
-addappid(1745310)
-addappid(1745311,0,"5cc84c8ab2e0e10deb49332289a8f535f0b5acb2f9ea24fcfb8d585329f02200")
-setManifestid(1745311,"8363935416445473212")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1745310.lua
+--Gamename 與經紀人戀愛是絕對禁止！
+addappid(1745310, 1, "b2cd7e022420a8711a41e38ecf0e772a170a8a2804e1a6f3339ecc8e8029d098") --Mainappid 與經紀人戀愛是絕對禁止！
+addappid(1745311, 1, "5cc84c8ab2e0e10deb49332289a8f535f0b5acb2f9ea24fcfb8d585329f02200") --Main Windows Depot 與經紀人戀愛是絕對禁止！
+setManifestid(1745311, "8363935416445473212", 743984912)
