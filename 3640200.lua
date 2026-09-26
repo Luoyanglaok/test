80f3f557806c88005bc47ee38d6beae099ce4b82
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 08:43:52 +0800

    add 3640200.lua

---
+++ 3640200.lua
@@ -0,0 +1,10 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3640200.lua
+--Gamename Dawson Oaks Trailer Park
+addappid(3640200) --Mainappid Dawson Oaks Trailer Park
+addappid(3640201, 1, "91176a5c38a2768670135a836a21a634a146e1ac9f1342b893a214e3067dc5fb") --Main Windows Depot Dawson Oaks Trailer Park
+setManifestid(3640201, "2710912804868586013", 896914752)
+addappid(4953162, 1, "69c7b6999a15f1e14d17a0827cc1b6fc3cf76c1164ab1afc5ad6e1f482cea932") --Main Windows Depot Dawson Oaks Trailer Park
+setManifestid(4953162, "6218517473219045774", 799282688)
+--Dlcname Dawson Oaks Trailer Park Playtest
+addappid(4953160) --Dlcname Dawson Oaks Trailer Park Playtest
