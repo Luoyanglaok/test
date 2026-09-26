bf02a45dd7b05a4337078fa74752fb8077421e2b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 11:20:34 +0800

    add 3699590.lua

---
+++ 3699590.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3699590.lua
+--Gamename Well Dweller
+addappid(3699590, 1, "fbbcd8506e54b7c13ffca2dec398d47c19b7207cff8fa2301889fb7ff76ff30e") --Mainappid Well Dweller
+addappid(3699591, 1, "2870de187cd57410799ab7bdc148fd27c1f700edcbca664d61dd77f4142cacb4") --Main Windows Depot Well Dweller
+setManifestid(3699591, "7559501529724673527", 736343408)
