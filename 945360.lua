4be0e873b6adde285421b04fda11f623bc61f3a9
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 19:47:10 +0800

    add 945360.lua

--- 945360.lua
+++ 945360.lua
@@ -1,4 +1,18 @@
-addappid(945360)
-setManifestid(228988,"6645201662696499616")
-addappid(945361,0,"cc84ab09a34dfa19f0cf86c30c05345294e788988c95455a7c42d024dc4f33a8")
-setManifestid(945361,"1298083356997541927")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 945360.lua
+--Gamename Among Us
+addappid(945360, 1, "7f0bd1cc5c537189d88186c93ddc7bb29608c8cff6c3e67bb2bb8bd5bab8f752") --Mainappid Among Us
+addappid(945361, 1, "cc84ab09a34dfa19f0cf86c30c05345294e788988c95455a7c42d024dc4f33a8") --Main Windows Depot Among Us
+setManifestid(945361, "1397756378225229500", 656543488)
+addappid(945362, 1, "cc84ab09a34dfa19f0cf86c30c05345294e788988c95455a7c42d024dc4f33a8") --Main Macos Depot Among Us
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(946430) --Dlcname Among Us - Hamster Pet Bundle
+addappid(971924) --Dlcname Among Us - Bedcrab Pet Bundle
+addappid(971925) --Dlcname Among Us - Polus Map
+addappid(971995) --Dlcname Among Us - Brainslug Pet Bundle
+addappid(971996) --Dlcname Among Us - Mini Crewmate Bundle
+addappid(971998) --Dlcname Among Us - MIRA HQ Map
+addappid(971999) --Dlcname Among Us - Stickmin Pet Bundle
+addappid(1579150) --Dlcname Among Us - Airship Skins
