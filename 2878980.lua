662f42d0f76296705ca8093342386e44c0abf411
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 10:58:00 +0800

    add 2878980.lua

--- 2878980.lua
+++ 2878980.lua
@@ -1,7 +1,54 @@
-addappid(2878980)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2878981,0,"df09d8fa7b88da65f6fdce275ab09b85e5c12731cad10c147745c7501acb1f14")
-setManifestid(2878981,"2009120231322524003")
-addappid(3107390,0,"a86b0e16a6d6f9946fc2671411ad8653123970931a5e289b242e3b1cf563381f")
-setManifestid(3107390,"5851584071712106802")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2878980.lua
+--Gamename NBA 2K25
+addappid(2878980) --Mainappid NBA 2K25
+addappid(2878981, 1, "df09d8fa7b88da65f6fdce275ab09b85e5c12731cad10c147745c7501acb1f14") --Main Windows Depot NBA 2K25
+setManifestid(2878981, "1749668799585948014", 144903532880)
+--Dlcname NBA 2K25: Gravity Ball
+addappid(3107390, 1, "a86b0e16a6d6f9946fc2671411ad8653123970931a5e289b242e3b1cf563381f") --Dlcname NBA 2K25: Gravity Ball
+setManifestid(3107390, "5851584071712106802", 1297583792)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
+addappid(2974820) --Dlcname NBA 2K25 Hall of Fame Pass: Season 1
+addappid(2974830) --Dlcname NBA 2K25 - Season 1 Pro Pass
+addappid(2974840) --Dlcname NBA 2K25 Hall of Fame Pass: Season 2
+addappid(2974850) --Dlcname NBA 2K25 Pro Pass: Season 2
+addappid(2974860) --Dlcname NBA 2K25 Hall of Fame Pass: Season 3
+addappid(2974870) --Dlcname NBA 2K25 Pro Pass: Season 3
+addappid(2974880) --Dlcname NBA 2K25 Hall of Fame Pass: Season 4
+addappid(2974890) --Dlcname NBA 2K25 Pro Pass: Season 4
+addappid(2974900) --Dlcname NBA 2K25 Hall of Fame Pass: Season 5
+addappid(2974910) --Dlcname NBA 2K25 Pro Pass: Season 5
+addappid(2974920) --Dlcname NBA 2K25 Hall of Fame Pass: Season 6
+addappid(2974930) --Dlcname NBA 2K25 Pro Pass: Season 6
+addappid(2974940) --Dlcname NBA 2K25 Pro Pass: Season 7
+addappid(2974950) --Dlcname NBA 2K25 Pro Pass: Season 8
+addappid(2974960) --Dlcname NBA 2K25 Pro Pass: Season 9
+addappid(2974970) --Dlcname NBA 2K25 Hall of Fame Pass: Season 7
+addappid(2974980) --Dlcname NBA 2K25 Hall of Fame Pass: Season 8
+addappid(2974990) --Dlcname NBA 2K25 Hall of Fame Pass: Season 9
+addappid(2975000) --Dlcname NBA 2K25 5,000 Virtual Currency Pack
+addappid(2975010) --Dlcname NBA 2K25 Tournament Edition Bonus
+addappid(2975020) --Dlcname NBA 2K25 All-Star Bonus
+addappid(2982770) --Dlcname NBA 2K25 Hall of Fame Bonus
+addappid(2982780) --Dlcname VC MyCAREER Bundle Season 1
+addappid(2982790) --Dlcname VC MyTEAM Bundle Season 1
+addappid(3261610) --Dlcname NBA 2K25 MyTEAM Bonus Offer: Season 2
+addappid(3261620) --Dlcname NBA 2K25 MyCAREER Bonus Offer: Season 2
+addappid(3310490) --Dlcname NBA 2K25 MyTEAM Bonus Offer: Season 3
+addappid(3310500) --Dlcname NBA 2K25 MyCAREER Bonus Offer: Season 3
+addappid(3312950) --Dlcname NBA 2K25 MyTEAM Bonus Offer: Season 4
+addappid(3312960) --Dlcname NBA 2K25 MyCAREER Bonus Offer: Season 4
+addappid(3312970) --Dlcname NBA 2K25 MyTEAM Bonus Offer: Season 5
+addappid(3312980) --Dlcname NBA 2K25 MyCAREER Bonus Offer: Season 5
+addappid(3312990) --Dlcname NBA 2K25 MyTEAM Bonus Offer: Season 6
+addappid(3313000) --Dlcname NBA 2K25 MyCAREER Bonus Offer: Season 6
+addappid(3313010) --Dlcname NBA 2K25 MyTEAM Bonus Offer: Season 7
+addappid(3313020) --Dlcname NBA 2K25 MyCAREER Bonus Offer: Season 7
+addappid(3313030) --Dlcname NBA 2K25 MyTEAM Bonus Offer: Season 8
+addappid(3313040) --Dlcname NBA 2K25 MyCAREER Bonus Offer: Season 8
+addappid(3313050) --Dlcname NBA 2K25 MyTEAM Bonus Offer: Season 9
+addappid(3313060) --Dlcname NBA 2K25 MyCAREER Bonus Offer: Season 9
