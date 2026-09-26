df82cce341d23a72f3e38f0a2aee49a9ab0ec9ff
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 23:54:17 +0800

    add 3743790.lua

---
+++ 3743790.lua
@@ -0,0 +1,16 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3743790.lua
+--Gamename Thomas & Friends™: Wonders of Sodor
+addappid(3743790) --Mainappid Thomas & Friends™: Wonders of Sodor
+addappid(3743791, 1, "319047ea8ff14b6423edf068bcf6e172e2b49ab644eb998e8fd2ad65507deaae") --Main Windows Depot Thomas & Friends™: Wonders of Sodor
+setManifestid(3743791, "4939973808313107399", 7772471568)
+--Dlcname Thomas & Friends™: Wonders of Sodor – James Add-On
+addappid(4262440, 1, "49e074e3f2a4311eba6c8daeefb9520cf9a68038b663af25e52e07fbb7a61548") --Dlcname Thomas & Friends™: Wonders of Sodor – James Add-On
+setManifestid(4262440, "4303264831270393653", 3936)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
