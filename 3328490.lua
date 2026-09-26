e6f687ba74e8974560fb7dac262bd04ffd2a3703
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 13:36:01 +0800

    add 3328490.lua

--- 3328490.lua
+++ 3328490.lua
@@ -3,7 +3,7 @@
 --Gamename Airport Control 27
 addappid(3328490) --Mainappid Airport Control 27
 addappid(3328491, 1, "11bd5c5429c28f1f176fa16e9382be4df1b52e9182b9a7af922b82ca1aca3627") --Main Windows Depot Airport Control 27
-setManifestid(3328491, "3991507574008701897", 8270696720)
+setManifestid(3328491, "3080784626488226518", 8273842240)
 addappid(4004140, 1, "818bfc9d7a3e02ca1aa3e576db4f3dc4a1cb8519dd4a6986d1e0bae1e7e6c727") --Main Windows Depot Airport Control 27
 addappid(4004141, 1, "8c0df506e6af49ac9217269aeb5d8d938a86a615174d8dc8fadf315823231eab") --Main Windows Depot Airport Control 27
 setManifestid(4004141, "254498131905757005", 8407460752)
