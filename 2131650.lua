40e131bf7ebff0f14105f6f5d984539e86ba5fd2
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 15:54:42 +0800

    add 2131650.lua

--- 2131650.lua
+++ 2131650.lua
@@ -1,13 +1,28 @@
-addappid(2131650)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(2131652,0,"65ab5efe73d2ad271b6038a0632621f95755aa433619ed4223eb115634d035ff")
-setManifestid(2131652,"7077042967756280050")
-addappid(2131651,0,"0073fe0583da7d2329aee1439801c51c48ee664444fa0f669155c1e51d331fe8")
-setManifestid(2131651,"3332246131242867228")
-addappid(2314280,0,"1741d8bab672cba92e86f4f476a811e7e9e39685a9269a7040437c907492d334")
-setManifestid(2314280,"271318178683467101")
-addappid(2314281,0,"5267831e9c74001101fdcd5e5987108b01c2c9fc3a3ad44c53f9bce2a3468e77")
-setManifestid(2314281,"5944477904700342265")
-addappid(2131654,0,"5a546c87b8553c36c0b7c7ad9ea7aed11c225ff288a3077f0ce9236dababfa09")
-setManifestid(2131654,"653085675608124350")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2131650.lua
+--Gamename METAL GEAR SOLID 3: Snake Eater - Master Collection Version
+addappid(2131650, 1, "04d785f1b132636a833d300dbe499f0c6ac1c85d9c94ba4c293b4df2844919f6") --Mainappid METAL GEAR SOLID 3: Snake Eater - Master Collection Version
+addappid(2131651, 1, "0073fe0583da7d2329aee1439801c51c48ee664444fa0f669155c1e51d331fe8") --Main Windows Depot METAL GEAR SOLID 3: Snake Eater - Master Collection Version
+setManifestid(2131651, "6695587154341534306", 13440993952)
+addappid(2131652, 1, "65ab5efe73d2ad271b6038a0632621f95755aa433619ed4223eb115634d035ff") --Main Windows Depot METAL GEAR SOLID 3: Snake Eater - Master Collection Version
+setManifestid(2131652, "7645578694842454403", 14248865936)
+addappid(2131653, 1, "987453f27f184d3ffa2cdbdec5ad68315467386a0f05d348b076d3fdc5b072f4") --Main Windows Depot METAL GEAR SOLID 3: Snake Eater - Master Collection Version
+setManifestid(2131653, "2112137862776922236", 13441354208)
+addappid(2131654, 1, "5a546c87b8553c36c0b7c7ad9ea7aed11c225ff288a3077f0ce9236dababfa09") --Main Windows Depot METAL GEAR SOLID 3: Snake Eater - Master Collection Version
+setManifestid(2131654, "1288863419166058256", 14249225952)
+--Dlcname METAL GEAR SOLID 3: Snake Eater - Master Collection Version Japanese Language Pack
+addappid(2314280, 1, "1741d8bab672cba92e86f4f476a811e7e9e39685a9269a7040437c907492d334") --Dlcname METAL GEAR SOLID 3: Snake Eater - Master Collection Version Japanese Language Pack
+setManifestid(2314280, "5445412728386120678", 4515738288)
+--Dlcname METAL GEAR SOLID 3: Snake Eater - Master Collection Version International Language Pack
+addappid(2314281, 1, "5267831e9c74001101fdcd5e5987108b01c2c9fc3a3ad44c53f9bce2a3468e77") --Dlcname METAL GEAR SOLID 3: Snake Eater - Master Collection Version International Language Pack
+setManifestid(2314281, "2259101554323948940", 5324458944)
+--Dlcname METAL GEAR SOLID 3: Snake Eater - Master Collection Version High Resolution Texture Pack
+addappid(2314282, 1, "9584e2a3c4196fe4ad8bb6abefbce60a06710eb351f832abbec0be982d06f789") --Dlcname METAL GEAR SOLID 3: Snake Eater - Master Collection Version High Resolution Texture Pack
+setManifestid(2314282, "4435371496659335891", 24324584496)
+addappid(2314283, 1, "67624392f2d731364ecf93e7b110d97ef1abcb7440355225cb44625b8f6d74cb") --Dlc Windows Depot METAL GEAR SOLID 3: Snake Eater - Master Collection Version High Resolution Texture Pack
+setManifestid(2314283, "4327820012053922733", 64)
+--Share Depots
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228988, "6645201662696499616", 22411856)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
