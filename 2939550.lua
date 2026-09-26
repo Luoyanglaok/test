48cf6b1470371815abec4183b53faa89345aeb71
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 22:50:34 +0800

    add 2939550.lua

--- 2939550.lua
+++ 2939550.lua
@@ -1,3 +1,6 @@
-addappid(2939550)
-addappid(2939551,0,"fc899ac8e575031689655e3a465ea103a741a031b08935cb3014d1da2ca47e1c")
-setManifestid(2939551,"5562859105081775871")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2939550.lua
+--Gamename Urayama | ウラヤマ
+addappid(2939550) --Mainappid Urayama | ウラヤマ
+addappid(2939551, 1, "fc899ac8e575031689655e3a465ea103a741a031b08935cb3014d1da2ca47e1c") --Main Windows Depot Urayama | ウラヤマ
+setManifestid(2939551, "6547716910198742056", 2855724576)
