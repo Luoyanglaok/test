8feb7be6313b566712734b72696337990fd869e5
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 11:40:48 +0800

    add 1216710.lua

--- 1216710.lua
+++ 1216710.lua
@@ -1,7 +1,13 @@
-addappid(1216710)
-addappid(1216711,0,"93a2ef4da7abb3c0a21988e44cc9c7a8abba823766701bbd016a218893bacab8")
-setManifestid(1216711,"797635092224441207")
-addappid(1216712,0,"594356827386d034c3c0bf64a180639b10f2a1f44a2f9c9efd2a5a7c8a3f245c")
-setManifestid(1216712,"8942833740601944566")
-addappid(1216713,0,"7a026571cabf6b38bdb81c7a515f04c4570fb146ab53cac1c3ec18217927c0e0")
-setManifestid(1216713,"6506154307155443600")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1216710.lua
+--Gamename Cyber Manhunt
+addappid(1216710, 1, "35bf78883ce1c3a301829404de8b7aee69e6c03c320484b07c568ef4abf45bfe") --Mainappid Cyber Manhunt
+addappid(1216711, 1, "93a2ef4da7abb3c0a21988e44cc9c7a8abba823766701bbd016a218893bacab8") --Main Windows Depot Cyber Manhunt
+setManifestid(1216711, "797635092224441207", 1623322576)
+addappid(1216712, 1, "594356827386d034c3c0bf64a180639b10f2a1f44a2f9c9efd2a5a7c8a3f245c") --Main Windows Depot Cyber Manhunt
+setManifestid(1216712, "8942833740601944566", 1621077776)
+addappid(1216713, 1, "7a026571cabf6b38bdb81c7a515f04c4570fb146ab53cac1c3ec18217927c0e0") --Main Macos Depot Cyber Manhunt
+setManifestid(1216713, "6506154307155443600", 1628903472)
+addappid(1682230) --Dlcname Cyber Manhunt - Home Sweet Home
+addappid(1840550) --Dlcname Cyber Manhunt - Hello World
+addappid(2078100) --Dlcname Cyber Manhunt - A Company Man of Titan
