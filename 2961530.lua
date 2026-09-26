f5ee5d590a3c12abc52aedcf746f4a6524922a00
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 17:21:57 +0800

    add 2961530.lua

--- 2961530.lua
+++ 2961530.lua
@@ -1,3 +1,6 @@
-addappid(2961530)
-addappid(2961531,0,"dff8b76588bfbefe7aceedf3312c31c7eab25b590d1a95485d82bad8d4ba225b")
-setManifestid(2961531,"3192230322384416859")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2961530.lua
+--Gamename Fears to Fathom - Woodbury Getaway
+addappid(2961530) --Mainappid Fears to Fathom - Woodbury Getaway
+addappid(2961531, 1, "dff8b76588bfbefe7aceedf3312c31c7eab25b590d1a95485d82bad8d4ba225b") --Main Windows Depot Fears to Fathom - Woodbury Getaway
+setManifestid(2961531, "3192230322384416859", 3825948160)
