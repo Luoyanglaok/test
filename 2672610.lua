ed6eb4184361573ef851274069ef7b0802b4ef19
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 11:21:43 +0800

    add 2672610.lua

--- 2672610.lua
+++ 2672610.lua
@@ -1,3 +1,3 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 2672610.lua
-addappid(2672610, 0, "fb0b8fede391fb2e444b9e5774252165d009dcdcc3344488ef25c5601497dce1")
+addappid(2672610, 0, "9478373338d8a21c743f124ce6928c765a0d25d808d1cafe0c72ebbf09e864ff")
