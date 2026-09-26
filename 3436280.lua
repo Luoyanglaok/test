e26646348941f3fdb989b616623be2a411924413
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 20:40:04 +0800

    add 3436280.lua

---
+++ 3436280.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3436280.lua
+--Gamename Glamour Kaleidoscope
+addappid(3436280) --Mainappid Glamour Kaleidoscope
+addappid(3436281, 1, "37b98b0feb7c17c8dfec87b981d89b0684bcb529c7884bb96577603d218889d7") --Main Windows Depot Glamour Kaleidoscope
+setManifestid(3436281, "8549383308992896193", 1025665856)
