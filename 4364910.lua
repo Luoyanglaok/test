d8eb09cee0f907431af25c358a6b9feec4fd541e
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 20:42:01 +0800

    add 4364910.lua

--- 4364910.lua
+++ 4364910.lua
@@ -1,15 +1,6 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 4364910.lua
---Gamename Ruins of Dawn
-addappid(4364910) --Mainappid Ruins of Dawn
-addappid(4364911, 1, "1ca0d151f24d942dd4e67ca79073604c05339db9cacb13362be1e7343aef6f59") --Main Windows Depot Ruins of Dawn
-setManifestid(4364911, "1406217731728474474", 2301516768)
---Dlcname 破晓之墟Demo
-addappid(4743300) --Dlcname 破晓之墟Demo
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Dlc Windows Depot 破晓之墟Demo
-setManifestid(228989, "5753583882400741046", 25108528)
-addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Dlc Windows Depot 破晓之墟Demo
-setManifestid(228990, "1829726630299308803", 100658080)
-addappid(4743301, 1, "3064c3bf23cb650d75a1d53733b2b2ba3a89ec4708593d346703579cf829310e") --Dlc Windows Depot 破晓之墟Demo
-setManifestid(4743301, "1216853109915941474", 2048240848)
---Share Depots
+addappid(4364910)
+addappid(228989,1,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+addappid(228990,1,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+setManifestid(4364911,"1954631530023888339")
+setManifestid(228989,"5753583882400741046")
+setManifestid(228990,"1829726630299308803")
