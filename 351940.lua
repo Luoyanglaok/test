566b2fae4968054817f5b02e8a0405f6692ad05d
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 11:26:20 +0800

    add 351940.lua

--- 351940.lua
+++ 351940.lua
@@ -1,12 +1,19 @@
-addappid(351940)
-addappid(351941,0,"2e3d926007b0f5c6a7f9766983c812330ddeddac70ec2546b1aefb6c40e0e319")
-setManifestid(351941,"9171187132898035325")
-addappid(351943,0,"7ad2d07689da1bfe6a212c2013dbbe6de2fb021dc7d9c05f3c44f721b1b460bc")
-setManifestid(351943,"1549803554007242827")
-addappid(401640,0,"fadb4ae0f8639129bf662b53c909323e2a40b4ab17f56392bbb45c3c7deac65c")
-setManifestid(401640,"4957648283334514402")
-addappid(401642)
-addappid(351948,0,"bc215a2e7f87d829e7701137c452c0b2747be52f1bd668cc5beb52285df7defd")
-setManifestid(351948,"7409204596308832373")
-addappid(401644)
-addappid(401641)
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 351940.lua
+--Gamename The Descendant
+addappid(351940) --Mainappid The Descendant
+addappid(351941, 1, "2e3d926007b0f5c6a7f9766983c812330ddeddac70ec2546b1aefb6c40e0e319") --Main Windows Depot The Descendant
+setManifestid(351941, "9171187132898035325", 2166943088)
+addappid(351943, 1, "7ad2d07689da1bfe6a212c2013dbbe6de2fb021dc7d9c05f3c44f721b1b460bc") --Main Macos Depot The Descendant
+setManifestid(351943, "1549803554007242827", 2184559520)
+--Dlcname The Descendant: Rest of Season
+addappid(401640, 1, "fadb4ae0f8639129bf662b53c909323e2a40b4ab17f56392bbb45c3c7deac65c") --Dlcname The Descendant: Rest of Season
+addappid(351948, 1, "bc215a2e7f87d829e7701137c452c0b2747be52f1bd668cc5beb52285df7defd") --Dlc Windows Depot The Descendant: Rest of Season
+setManifestid(351948, "7409204596308832373", 957497136)
+setManifestid(401640, "4957648283334514402", 5098177472)
+addappid(401641, 1, "262136e38264c68811a6a4735c1fe950d33342729dda4c2da84890a11bbdb9b1") --Dlc Windows Depot The Descendant: Rest of Season
+setManifestid(401641, "3431369727955410585", 5098208768)
+addappid(401642, 1, "9fe6f7e878a8bfc6bcc71d355a3c8e57495d0449f4958f96fc34b11511c06002") --Dlc Macos Depot The Descendant: Rest of Season
+setManifestid(401642, "6719013734413992031", 5131029008)
+addappid(401644, 1, "80b7da0321ac2db81c31c5d7e84c8d72a6412de2eeda6c281b2f2adc841a0db9") --Dlc Macos Depot The Descendant: Rest of Season
+setManifestid(401644, "3003307465952645802", 5131029008)
