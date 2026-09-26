ca7957b47452cbe44b150223cc8e8eb65d5bc13b
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 15:17:20 +0800

    add 3969150.lua

---
+++ 3969150.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3969150.lua
+--Gamename Beauty Store Simulator
+addappid(3969150) --Mainappid Beauty Store Simulator
+addappid(3969151, 1, "b3557f40f37458dc8bd43aee7788aab2932453c7fd9002d139243f4ae14c9f6b") --Main Windows Depot Beauty Store Simulator
+setManifestid(3969151, "4997907250972950802", 816043312)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
