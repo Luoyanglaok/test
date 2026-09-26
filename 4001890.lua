2d4f4e890139b5f59b224c27a981f40b233b407f
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 18:34:18 +0800

    add 4001890.lua

--- 4001890.lua
+++ 4001890.lua
@@ -1,6 +1,6 @@
 -- Downloaded using DepotBox - https://depotbox.org/
 -- Original file: 4001890.lua
 --Gamename How to Fish
-addappid(4001890) --Mainappid How to Fish
+addappid(4001890, 1, "ea507a303e64f6def87ca6899e164350a1a1fb3a4b109542949345340ba47227") --Mainappid How to Fish
 addappid(4001891, 1, "b5c8ad37740a8db6d370a6934bf203cfaeb3343003e6277ffc96c63f4df2b7ca") --Main Windows Depot How to Fish
 setManifestid(4001891, "3889140805796509645", 357575152)
