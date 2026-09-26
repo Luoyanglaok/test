cfae91b030f972cc8a720be3b95394df9953d2c6
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 12:31:54 +0800

    add 3412900.lua

---
+++ 3412900.lua
@@ -0,0 +1,12 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3412900.lua
+--Gamename 代号三国：龙起
+addappid(3412900) --Mainappid 代号三国：龙起
+addappid(3412901, 1, "1b0053e043a4cdd8f620d8ab2263f9d03add55596f64923327c1633cbe8ade64") --Main Windows Depot 代号三国：龙起
+setManifestid(3412901, "2773242664890736697", 91211440)
+addappid(3412902, 1, "7e0c2a3d2f9eb574e066a6848d94f45a64d95fa86deb5b27c7a5cd5655f665f0") --Main Windows Depot 代号三国：龙起
+setManifestid(3412902, "8896959557201659853", 2395237264)
+addappid(3412903, 1, "1c78b9c94482c21c05ba0936eb1daf7bfb82d2917b2ba176a011567ac6d1897b") --Main Windows Depot 代号三国：龙起
+setManifestid(3412903, "6140867560104639640", 10552032)
+addappid(3412904, 1, "e472012bec404331d0a06079441f61ce3eeaf6cbb409f1af9dffa609d21ef815") --Main Windows Depot 代号三国：龙起
+setManifestid(3412904, "5466482407002457299", 77666913744)
