491f88404dc209e897c344d63c044ca9b7f37492
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 22:18:20 +0800

    add 1997480.lua

--- 1997480.lua
+++ 1997480.lua
@@ -1,3 +1,6 @@
-addappid(1997480)
-addappid(1997481,0,"6e01ca386e2eaaa9606f025f59618ddb12818c3b17f5857d2afba781fafe9270")
-setManifestid(1997481,"4671444676535002679")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1997480.lua
+--Gamename Liu Shan Maker
+addappid(1997480, 1, "bc609f5ff8fb98692e848d6caa0600ca79d9b21cf7b765a83027add64da9398c") --Mainappid Liu Shan Maker
+addappid(1997481, 1, "6e01ca386e2eaaa9606f025f59618ddb12818c3b17f5857d2afba781fafe9270") --Main Windows Depot Liu Shan Maker
+setManifestid(1997481, "4671444676535002679", 368733552)
