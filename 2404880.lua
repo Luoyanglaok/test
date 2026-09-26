3747d8af96a6a21e63121ecc32adf96da5fe6c28
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 23:49:30 +0800

    add 2404880.lua

--- 2404880.lua
+++ 2404880.lua
@@ -1,3 +1,14 @@
-addappid(2404880)
-addappid(2404881,0,"dd3322cbddd81741e4bd8f3d9d08fbe6ff172e3f5c36fec42e215f6661d3ea4f")
-setManifestid(2404881,"3170277151318915105")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2404880.lua
+--Gamename Car Dealer Simulator
+addappid(2404880, 1, "4bccd9e25e0e38d304644b82a0c9961cd9d9814d07831f0fa251ddc39a35243f") --Mainappid Car Dealer Simulator
+addappid(2404881, 1, "dd3322cbddd81741e4bd8f3d9d08fbe6ff172e3f5c36fec42e215f6661d3ea4f") --Main Windows Depot Car Dealer Simulator
+setManifestid(2404881, "8195143885353520026", 18933817936)
+--Dlcname Car Dealer Simulator - Up 2 You DLC
+addappid(4046960, 1, "11fd991d7512eb7d72d0d55e65c4b6e7a1851e35949cce2081ece003c2fb9e33") --Dlcname Car Dealer Simulator - Up 2 You DLC
+setManifestid(4046960, "881733391596233437", 940914992)
+--Share Depots
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228989, "5753583882400741046", 25108528)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+setManifestid(228990, "1829726630299308803", 100658080)
