79fcc94257a10ab0c90f0cbf4eb0c22c6fb09e20
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 23:54:47 +0800

    add 490110.lua

--- 490110.lua
+++ 490110.lua
@@ -3,4 +3,4 @@
 --Gamename The Precinct
 addappid(490110, 1, "30a2b9f28e2ea177a918b9ffb874641dafe6b9ee69ec5a83d7eeb9d8fd5c00f7") --Mainappid The Precinct
 addappid(490111, 1, "44bcc3870bf156385e37ff47e71ae28824fa905a542cdc8eaf05715799f78fc2") --Main Windows Depot The Precinct
-setManifestid(490111, "428541716305617698", 4561432032)
+setManifestid(490111, "6212715692624246529", 4563726544)
