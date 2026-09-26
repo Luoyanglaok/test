d8c91362a37143677491934f5a866f07aba25e89
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 18:31:09 +0800

    add 2967800.lua

--- 2967800.lua
+++ 2967800.lua
@@ -3,15 +3,7 @@
 --Gamename Sex on the beach
 addappid(2967800) --Mainappid Sex on the beach
 addappid(2967801, 1, "b7a46e12d41bc63d19fa4e59b973a2ead9e218c69c1e40a79033112a34d899a2") --Main Windows Depot Sex on the beach
-setManifestid(2967801, "7945755867774909969", 24451889296)
---Dlcname Sex on the beach Soundtrack
-addappid(3854070) --Dlcname Sex on the beach Soundtrack
-addappid(3854071, 1, "22CD4760E579235D8C12BE58ECA6F7666A3A384421025F860A0606ED56393186") --Dlc Windows Depot Sex on the beach Soundtrack
-setManifestid(3854071, "4017634049707749828", 7815024)
---Dlcname Sex on the beach (Latin Pop, Rock, Tropical, Soft Adult, Contemporary)
-addappid(3877640) --Dlcname Sex on the beach (Latin Pop, Rock, Tropical, Soft Adult, Contemporary)
-addappid(3877641, 1, "1B02F521B53E9EF9F149B568D953F97FCD141C5C320F1A4E2DCC3D8271E49A83") --Dlc Windows Depot Sex on the beach (Latin Pop, Rock, Tropical, Soft Adult, Contemporary)
-setManifestid(3877641, "108502950761880042", 17278768)
+setManifestid(2967801, "2094862503846602720", 24592597472)
 --Share Depots
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
 setManifestid(228989, "5753583882400741046", 25108528)
