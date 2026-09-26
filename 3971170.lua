6dd58c5da8bb86c226dcc16a8edcbfc455c273aa
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-27 00:29:07 +0800

    add 3971170.lua

---
+++ 3971170.lua
@@ -0,0 +1,6 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 3971170.lua
+--Gamename Charming Girl
+addappid(3971170) --Mainappid Charming Girl
+addappid(3971171, 1, "180870EA23B0B2DC986569769F6A8EEA0403F0F71E1E617E1A9DF23D91BA7790") --Main Windows Depot Charming Girl
+setManifestid(3971171, "8634073687744813320", 874199360)
