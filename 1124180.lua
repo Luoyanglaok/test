34d2e664f02b9d3c670a0c84acc691ce0e297afd
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 23:55:11 +0800

    add 1124180.lua

--- 1124180.lua
+++ 1124180.lua
@@ -1,7 +1,13 @@
-addappid(1124180)
-addappid(1124182,0,"81bb06ebf8b5dd9a399a9d53dcc4757d72d52e79abf196618e56468ecd339e95")
-setManifestid(1124182,"6755555991817505912")
-addappid(1124183,0,"8f80b57f698bd67ec3f502fb703dfca2f502c9223f1e7f6cbd2c43fe809229bc")
-setManifestid(1124183,"8472592078162798878")
-addappid(1124184,0,"b034d6a660d0f077240a0059876177ac31e50ac363f6f328f44eb16ef9d950fd")
-setManifestid(1124184,"2195645209719504254")
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1124180.lua
+--Gamename Rail Route
+addappid(1124180, 1, "dc3cca428261e662f28d43a2c0fea65b846eb96d77a9ffbbac897c9f5ad902b2") --Mainappid Rail Route
+addappid(1124182, 1, "81bb06ebf8b5dd9a399a9d53dcc4757d72d52e79abf196618e56468ecd339e95") --Main Windows Depot Rail Route
+setManifestid(1124182, "3047714192750290938", 379459296)
+addappid(1124183, 1, "8f80b57f698bd67ec3f502fb703dfca2f502c9223f1e7f6cbd2c43fe809229bc") --Main Macos Depot Rail Route
+setManifestid(1124183, "4207249464115109710", 392588432)
+addappid(1124184, 1, "b034d6a660d0f077240a0059876177ac31e50ac363f6f328f44eb16ef9d950fd") --Main Linux Depot Rail Route
+setManifestid(1124184, "4378875375144901593", 376455392)
+addappid(2943430) --Dlcname Rail Route - Happy Passengers
+addappid(3570380) --Dlcname Rail Route - Curated Map Pack
+addappid(4540370) --Dlcname Rail Route - Expect Delays
