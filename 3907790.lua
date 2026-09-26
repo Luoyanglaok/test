5a446da46762b8be7fa65ddecdc6203c6133ec31
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 08:43:23 +0800

    add 3907790.lua

--- 3907790.lua
+++ 3907790.lua
@@ -3,4 +3,4 @@
 --Gamename OxyRail
 addappid(3907790) --Mainappid OxyRail
 addappid(3907791, 1, "22db0cd15dfca4f2c4a944655338c6007b15260252ee974fb5f0bfdf87db60f2") --Main Windows Depot OxyRail
-setManifestid(3907791, "4131504646564013475", 6104694368)
+setManifestid(3907791, "922001158549384299", 6876722896)
