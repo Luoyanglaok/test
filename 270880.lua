7f27c7a4dcdde0f3d6ee427f659223fd859a64c7
Luoyanglaok <155547637+Luoyanglaok@users.noreply.github.com>
2026-09-26 18:27:14 +0800

    add 270880.lua

--- 270880.lua
+++ 270880.lua
@@ -3,13 +3,13 @@
 --Gamename American Truck Simulator
 addappid(270880, 1, "73fb1b0d84405e9e475ea2185cea2dba8667f5971472b506dbcc44bd9c8e0ccb") --Mainappid American Truck Simulator
 addappid(270881, 1, "f94868b2aae2579ce595b4cc9560f649c2db7d93fe280b938ba7fdbe613ad11c") --Main Windows Depot American Truck Simulator
-setManifestid(270881, "3876626237809646374", 23573837184)
+setManifestid(270881, "6713499798466836066", 24180011216)
 addappid(270882, 1, "fce5a3213079ef0488c660325077b4f413d3cf68b11dde841840cc463773a102") --Main Windows Depot American Truck Simulator
-setManifestid(270882, "3741558341835061249", 20365728)
+setManifestid(270882, "3126424983673026360", 20911760)
 addappid(270883, 1, "bf688e332ee96e2934ee8d06b1258435ee48f37be64cd9e14553d2cccf3a4793") --Main Linux Depot American Truck Simulator
-setManifestid(270883, "5761126174135261057", 20793424)
+setManifestid(270883, "9103914692693045874", 21358384)
 addappid(270884, 1, "d801c40a476be58164b180e53ab1ccd0549b628dee728698b66b9c43e89aecd0") --Main Macos Depot American Truck Simulator
-setManifestid(270884, "8477491632550689493", 30069152)
+setManifestid(270884, "3850251596552957889", 30518064)
 --Dlcname American Truck Simulator - Kenworth T680
 addappid(421070, 1, "ef1ca02be68d36f1eb3686d3cbf3fc59ef7efc5b29c720e79808ebf626ad17ea") --Dlcname American Truck Simulator - Kenworth T680
 setManifestid(421070, "8169493434990176546", 0)
@@ -22,7 +22,7 @@ addappid(421081, 1, "ca26b32816a17e091f968eb9d10f501bf282623e91426e6c68b6c62a657
 setManifestid(421081, "515058574934992941", 0)
 --Dlcname American Truck Simulator - Nevada
 addappid(421090, 1, "5d61076dd16db3de635170ca7acd521f8e4a99236787eb9df11a000cde20e23b") --Dlcname American Truck Simulator - Nevada
-setManifestid(421090, "7516117083832359470", 43534528)
+setManifestid(421090, "5010175386759865446", 43536352)
 --Dlcname American Truck Simulator - Kenworth W900
 addappid(422310, 1, "7f4cee26dcc61f7158a2b1a909f18f1a1be24ac3668dfdc0e88272c2d5174fed") --Dlcname American Truck Simulator - Kenworth W900
 setManifestid(422310, "4599348267446784704", 0)
@@ -34,7 +34,7 @@ addappid(450550, 1, "d6cb25845c1e0b94d6257d12b72f33b91219629dcb64b1a89946b7d6293
 setManifestid(450550, "6356452566938046875", 0)
 --Dlcname American Truck Simulator - Arizona
 addappid(463740, 1, "47f6e20374bf50928ac3cbae0e1e8a1342050f75c09548581d09732660f4a91d") --Dlcname American Truck Simulator - Arizona
-setManifestid(463740, "5028841836292205343", 118178976)
+setManifestid(463740, "7120267857950484362", 117987792)
 --Dlcname American Truck Simulator - Steering Creations Pack
 addappid(520550, 1, "9e7cf9b47b389bf24926aec5971f5c51500bb9ee3eb286fe3c00cd2c0a4171c7") --Dlcname American Truck Simulator - Steering Creations Pack
 setManifestid(520550, "6720522774777303374", 0)
@@ -59,10 +59,10 @@ addappid(620610, 1, "a28b6fd765fb0630ea4bac0e2228bbdbfcf77189c716b6e1f723aa40d10
 setManifestid(620610, "8771153689427663235", 0)
 --Dlcname American Truck Simulator - New Mexico
 addappid(684630, 1, "b83ff05e98792262e6dca7054c6cd1df4d02c7db9d008bbed417ea0fa699a93a") --Dlcname American Truck Simulator - New Mexico
-setManifestid(684630, "1896069532633275384", 104863616)
+setManifestid(684630, "8033015021104834047", 104019008)
 --Dlcname American Truck Simulator - Oregon
 addappid(800370, 1, "5b351a472fb8194ec2a3e0bfe9becb3cd1d70b100e22ad457cdc7301df4e3fa1") --Dlcname American Truck Simulator - Oregon
-setManifestid(800370, "6810601345034008007", 55189504)
+setManifestid(800370, "2847412529290909726", 55222592)
 --Dlcname American Truck Simulator - Classic Stripes Paint Jobs Pack
 addappid(951650, 1, "0d8721903e8f156b515ba99af9ceba82fea7aa689f5ed338952deeb567976f54") --Dlcname American Truck Simulator - Classic Stripes Paint Jobs Pack
 setManifestid(951650, "4069996803610743172", 0)
@@ -71,13 +71,13 @@ addappid(962750, 1, "bd33a51e92d970bad4531669bc9ff6ef3b63708e19fa2f1b4dbeacf1819
 setManifestid(962750, "4357515643689176476", 0)
 --Dlcname American Truck Simulator - Washington
 addappid(1015160, 1, "4d72f042bc3be9238ddcbb5b029a2fc2e74f4d2ca1b8c63cc642c365e7f2a560") --Dlcname American Truck Simulator - Washington
-setManifestid(1015160, "3602412537319997708", 64432816)
+setManifestid(1015160, "6731973974915772189", 64441232)
 --Dlcname American Truck Simulator - Forest Machinery
 addappid(1076080, 1, "cc730305dadfaedafd0791be481d8199e60fe3e148008abf97ef2a0f76b96707") --Dlcname American Truck Simulator - Forest Machinery
 setManifestid(1076080, "659986890721159214", 0)
 --Dlcname American Truck Simulator - Utah
 addappid(1104880, 1, "72ff30c730815796e99727200a260dee79f1ec8549c99d6384249fb9a5b18f17") --Dlcname American Truck Simulator - Utah
-setManifestid(1104880, "5963775787066654146", 57573488)
+setManifestid(1104880, "850996711820698908", 57572768)
 --Dlcname American Truck Simulator - Space Paint Jobs Pack
 addappid(1116310, 1, "88b022d1a772eded89dce36f415a145294d9e585b6d7d67fb90eb5c3ea07f522") --Dlcname American Truck Simulator - Space Paint Jobs Pack
 setManifestid(1116310, "348047091569531530", 0)
@@ -90,10 +90,10 @@ addappid(1162160, 1, "9b74baee93d78fa0372f2ebf7bc261abddb299f645b49bcb506c7ab1ef
 setManifestid(1162160, "7223995549485937630", 0)
 --Dlcname American Truck Simulator - Idaho
 addappid(1209470, 1, "757ef85ac1e944b59162a555336c650e4e94c82a14ae01f10680c2c7b5534852") --Dlcname American Truck Simulator - Idaho
-setManifestid(1209470, "8396517117795822806", 64367200)
+setManifestid(1209470, "8709005521734639356", 63459760)
 --Dlcname American Truck Simulator - Colorado
 addappid(1209471, 1, "19b6552a79d070cd8c3ac570e7f170fe01dc0ef12938c7163f9bdf944f6e8c24") --Dlcname American Truck Simulator - Colorado
-setManifestid(1209471, "3993853403282503602", 134810400)
+setManifestid(1209471, "1550921906381601015", 134803504)
 --Dlcname American Truck Simulator - Mack Anthem®
 addappid(1236650, 1, "be9109d017c0382880c3670a499087f7380bf7d3b0308ad47f9d7c1393ac4620") --Dlcname American Truck Simulator - Mack Anthem®
 setManifestid(1236650, "403939628250608492", 0)
@@ -105,10 +105,10 @@ addappid(1415691, 1, "e9de25d429aebc710b728b4e5f4a004105840c3156655cf81220230452
 setManifestid(1415691, "5600695055779905726", 0)
 --Dlcname American Truck Simulator - Wyoming
 addappid(1415692, 1, "c5c09e5aa105c74d91891a4a003e8c8be58e981b7c77eeaa787384cdde0bd5d0") --Dlcname American Truck Simulator - Wyoming
-setManifestid(1415692, "2873400503895218838", 183632480)
+setManifestid(1415692, "7453546725768778286", 165684096)
 --Dlcname American Truck Simulator - Texas
 addappid(1465750, 1, "fb07018fa485ec3752022d570bb1108d687eaac2f82ce8a6cfc62202b7f020e0") --Dlcname American Truck Simulator - Texas
-setManifestid(1465750, "2056748951405925201", 489119488)
+setManifestid(1465750, "3448674196570005935", 488431744)
 --Dlcname American Truck Simulator - Cabin Accessories
 addappid(1477840, 1, "5b5702561860099c649c687721bd638131d48acff77188258b1fdb8cf533a164") --Dlcname American Truck Simulator - Cabin Accessories
 setManifestid(1477840, "2063882711744259797", 0)
@@ -126,7 +126,7 @@ addappid(1784890, 1, "c114883449c3e90aa527d00e39f7adb22ae666956105f2266c6b1cc89b
 setManifestid(1784890, "2966314702172464364", 0)
 --Dlcname American Truck Simulator - Montana
 addappid(1811080, 1, "5c1aceb43768228e4acb34669db372e81d352df4c1a296a5d84d5cdd4c6ca69e") --Dlcname American Truck Simulator - Montana
-setManifestid(1811080, "6977821494591370225", 275638768)
+setManifestid(1811080, "6702860681873088241", 273784944)
 --Dlcname American Truck Simulator - International 9900i
 addappid(1915840, 1, "54beb526ef1e0618d8d76398cec6ba0e52ed50b2aa89132c7649adf4309b9dea") --Dlcname American Truck Simulator - International 9900i
 setManifestid(1915840, "6306668266471803411", 0)
@@ -141,13 +141,13 @@ addappid(2187930, 1, "a07f486a79f2ac080fa7832d372fbea62c6682adc02e7ad4c554e27e6f
 setManifestid(2187930, "3435088043253893516", 0)
 --Dlcname American Truck Simulator - Oklahoma
 addappid(2209650, 1, "6bcbf2ca269cba04495810447920edea09fa3fe52bcdf4bb4d2aafe1e552bd9c") --Dlcname American Truck Simulator - Oklahoma
-setManifestid(2209650, "4754568792671624573", 160242896)
+setManifestid(2209650, "1595690604759568526", 160192544)
 --Dlcname American Truck Simulator - Western Star® 5700XE
 addappid(2257950, 1, "6f063a928ad302afe38c3b66fac2534279ebeda8c4b53f0de80d6baaa65d5f21") --Dlcname American Truck Simulator - Western Star® 5700XE
 setManifestid(2257950, "8688684472672409532", 0)
 --Dlcname American Truck Simulator - Kansas
 addappid(2298430, 1, "078aa0f7624ad82ee97bbe88e2b8fcdc34ad16d46eb548937fca3b9d75b4cff9") --Dlcname American Truck Simulator - Kansas
-setManifestid(2298430, "2023970975279782836", 124205344)
+setManifestid(2298430, "4312153348182320454", 124172976)
 --Dlcname American Truck Simulator - Volvo VNL 2018
 addappid(2386480, 1, "261a79855ff8d122f6e2b198a3099b9250e76fdeb7a7e702f490b5cf0efecc74") --Dlcname American Truck Simulator - Volvo VNL 2018
 setManifestid(2386480, "497598844484243710", 0)
@@ -156,10 +156,10 @@ addappid(2386530, 1, "be39dde039d636df9a80415f5b08ad5327fae617937cf957d9b7e5e483
 setManifestid(2386530, "2958423849438833889", 0)
 --Dlcname American Truck Simulator - Nebraska
 addappid(2543810, 1, "0b920caa9735843508dc845bfc14890b3bff0a9a039f9b390510f0fe23adef7b") --Dlcname American Truck Simulator - Nebraska
-setManifestid(2543810, "1157599248636057931", 138287184)
+setManifestid(2543810, "240888348296497811", 120394256)
 --Dlcname American Truck Simulator - Arkansas
 addappid(2585150, 1, "366a99af7f33693c3f554990db3d0a267fd78e2152f76e810251dc2fa01c2627") --Dlcname American Truck Simulator - Arkansas
-setManifestid(2585150, "2715365742696498961", 130650816)
+setManifestid(2585150, "6350465587402650843", 130577920)
 --Dlcname American Truck Simulator - Farm Machinery
 addappid(2638630, 1, "79dd85a39f3ed2af37aa9976d42b17c487fe8b315d414fa365eeeff265984889") --Dlcname American Truck Simulator - Farm Machinery
 setManifestid(2638630, "534253102025906285", 0)
@@ -171,7 +171,7 @@ addappid(2720080, 1, "1d5108be17616c1294e9deca093b721868c5a7663fa26f8502a4223bfc
 setManifestid(2720080, "5863575045093730890", 0)
 --Dlcname American Truck Simulator - Missouri
 addappid(2730870, 1, "9e1e19ea1811a4b4cec98af8584ff5a7fb13da5c0272d2479afa9d69617fe865") --Dlcname American Truck Simulator - Missouri
-setManifestid(2730870, "7989391398692757753", 81627680)
+setManifestid(2730870, "7982274742100166878", 81024688)
 --Dlcname American Truck Simulator - Sports Paint Jobs Pack
 addappid(2910160, 1, "19b2b6718f6e8962618c27d20350f198aad895292573bc0841fdefae1b6d722f") --Dlcname American Truck Simulator - Sports Paint Jobs Pack
 setManifestid(2910160, "1332365548956288646", 0)
@@ -180,10 +180,10 @@ addappid(2926440, 1, "b627a9bbc7548911abd87d8958ded646bad7986de2d02a9337ca0bf0c5
 setManifestid(2926440, "7921011583343865359", 0)
 --Dlcname American Truck Simulator - Louisiana
 addappid(3012580, 1, "27e47b41c142f937c2443d184c2e320e6ddfa5c6d3a9db1ae52f8ca4291afffa") --Dlcname American Truck Simulator - Louisiana
-setManifestid(3012580, "615651419872714227", 199000080)
+setManifestid(3012580, "1372447214370403221", 195442528)
 --Dlcname American Truck Simulator - Iowa
 addappid(3025440, 1, "906218b19a08b2cbdd039cbd1ff492f836bae78d9b66f0e3e86bdbb84b7d2bbe") --Dlcname American Truck Simulator - Iowa
-setManifestid(3025440, "2759256013499862141", 118296736)
+setManifestid(3025440, "2466561934949791217", 118261872)
 --Dlcname American Truck Simulator - Mack Pinnacle
 addappid(3146090, 1, "da6afa0d92f3d63650f282d26ff457bd3cd38aae1452fedea91c085d63ce8daf") --Dlcname American Truck Simulator - Mack Pinnacle
 setManifestid(3146090, "8340701473470811333", 0)
@@ -192,7 +192,7 @@ addappid(3272290, 1, "890f2186ed56cebb6bd9ebf7268932cb035b7c3ed8982c029062100b29
 setManifestid(3272290, "3284562101833249356", 0)
 --Dlcname American Truck Simulator - Illinois
 addappid(3486960, 1, "e69b61b24609bd32d3436db49c9d75afaeffe18b7b92b22608883053d1723729") --Dlcname American Truck Simulator - Illinois
-setManifestid(3486960, "7960664714597833164", 162930880)
+setManifestid(3486960, "6931480052109030143", 161404304)
 --Dlcname American Truck Simulator - KRONE Agriculture Equipment
 addappid(3872930, 1, "cb2520d903a63702de6165c9c59df18d6269541becfcd40d0805cea67b7f10a0") --Dlcname American Truck Simulator - KRONE Agriculture Equipment
 setManifestid(3872930, "6332296398029117686", 0)
@@ -203,6 +203,3 @@ setManifestid(4348660, "7046043884242444617", 0)
 --Dlcname American Truck Simulator - South Dakota
 --Dlcname American Truck Simulator - RAM & Dodge Car Pack
 --Dlcname American Truck Simulator - FORD Car Pack
---Dlcname American Truck Simulator - British Columbia
---Dlcname American Truck Simulator - North Dakota
---Dlcname American Truck Simulator - Indiana
